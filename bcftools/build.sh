fa1b2c8af6fe7340be2b7ba6eed82e836bed9215
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-09-13 09:13:39 -0400

    Cleanup.

--- bcftools/build.sh
+++ bcftools/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 make -j install prefix=$PREFIX
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
--- bwa/build.sh
+++ bwa/build.sh
@@ -4,9 +4,3 @@ make -j
 
 mkdir -p $PREFIX/bin
 cp bwa $PREFIX/bin
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
