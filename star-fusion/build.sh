b423961232e958fb573e40aa93a42c0f726a9658
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-09-16 22:34:00 -0400

    Added STAR-Fusion.

---
+++ star-fusion/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib/STAR-Fusion
+
+cp -r * $PREFIX/lib/STAR-Fusion
+
+echo "cd $PREFIX/lib/STAR-Fusion" > $PREFIX/bin/STAR-Fusion
+echo "./STAR-Fusion \$@" >> $PREFIX/bin/STAR-Fusion
+chmod +x $PREFIX/bin/STAR-Fusion
