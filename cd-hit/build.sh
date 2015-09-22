8c51a4eef865eac970d95bcedbb8d267b78d19ee
Hyeshik Chang <hyeshik@snu.ac.kr>
2015-09-22 17:24:32 +0900

    Enable building without openmp support in the system.

--- cd-hit/build.sh
+++ cd-hit/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-make -j
+cat > test-openmp.cc <<END
+#include <omp.h>
+END
+
+if g++ -o /dev/null -c test-openmp.cc 2>/dev/null
+then
+  OPENMP_SUPPORTED=yes
+else
+  OPENMP_SUPPORTED=no
+fi
+
+make -j -- openmp=$OPENMP_SUPPORTED
 
 mkdir -p $PREFIX/bin
 make install PREFIX=$PREFIX/bin
