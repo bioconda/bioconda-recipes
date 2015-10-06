29a9dfdf4da52e25274ecd6dfab529a9992cf103
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-06 13:22:19 -0400

    Only upload from master branch.

--- docker/build-packages.sh
+++ docker/build-packages.sh
@@ -9,8 +9,13 @@ do
     # build package
     conda build --no-anaconda-upload --skip-existing $p || errors+=$p"\n"
 done
-# upload all successfully built packages
-anaconda -t $ANACONDA_TOKEN upload /tmp/conda-build/anaconda/conda-bld/linux-64/*.tar.bz2
+
+# if testing master branch
+if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]
+then
+    # upload all successfully built packages
+    anaconda -t $ANACONDA_TOKEN upload /tmp/conda-build/anaconda/conda-bld/linux-64/*.tar.bz2
+fi
 
 # check for build or test errors and return proper exit code
 if [[ -n $errors ]]
