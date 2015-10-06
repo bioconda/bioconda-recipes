4def1794eef7b9a0941bb364c6ac00a6b2fb4ef4
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-06 19:51:57 -0400

    Fix.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,7 +7,7 @@ then
 else
     # install conda
     wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p /tmp/conda-build/anaconda
+    bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /tmp/conda-build/anaconda
     export PATH=/tmp/conda-build/anaconda/bin:$PATH
     mkdir -p /tmp/conda-build/anaconda/conda-bld/linux-64 # workaround for bug in current conda
     conda install -y conda conda-build anaconda-client
