c592d46ac5961e973df79dc8771fc077af206b78
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-06 19:57:46 -0400

    Fix.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,7 +6,7 @@ then
     docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -e CONDA_PY -e CONDA_NPY -v `pwd`:/tmp/conda-recipes bioconda/bioconda-builder /bin/build-packages.sh /tmp/conda-recipes
 else
     # install conda
-    wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
     bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /tmp/conda-build/anaconda
     export PATH=/tmp/conda-build/anaconda/bin:$PATH
     mkdir -p /tmp/conda-build/anaconda/conda-bld/linux-64 # workaround for bug in current conda
