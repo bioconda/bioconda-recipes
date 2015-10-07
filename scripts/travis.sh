33936edfc2b868f31499bbd30a762df47702107c
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-07 11:10:51 -0400

    Refactoring.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,14 +3,14 @@
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -e CONDA_PY -e CONDA_NPY -v `pwd`:/tmp/conda-recipes bioconda/bioconda-builder /bin/build-packages.py /tmp/conda-recipes
+    docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -e CONDA_PY -e CONDA_NPY -v `pwd`:/tmp/conda-recipes bioconda/bioconda-builder /tmp/conda-recipes
     exit $?
 else
     # install conda
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-    bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /tmp/conda-build/anaconda
-    export PATH=/tmp/conda-build/anaconda/bin:$PATH
-    mkdir -p /tmp/conda-build/anaconda/conda-bld/linux-64 # workaround for bug in current conda
+    bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /anaconda
+    export PATH=/anaconda/bin:$PATH
+    mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
     conda install -y conda conda-build anaconda-client pyyaml toolz jinja2 nose
 
     # setup bioconda channel
