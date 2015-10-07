05e589b2f98e8a7fad50f93f3be720bb87858a00
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-06 23:46:52 -0400

    Propagate exit code to travis.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,6 +4,7 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
     docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -e CONDA_PY -e CONDA_NPY -v `pwd`:/tmp/conda-recipes bioconda/bioconda-builder /bin/build-packages.py /tmp/conda-recipes
+    exit $?
 else
     # install conda
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
@@ -17,4 +18,5 @@ else
 
     # build packages
     scripts/build-packages.py .
+    exit $?
 fi
