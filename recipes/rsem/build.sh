34140f0907095cb8648bc201521e224ed72b6085
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-09 14:12:13 +0200

    add wrapper scripts for non-standard installation

--- recipes/rsem/build.sh
+++ recipes/rsem/build.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 pushd $SRC_DIR
 
-mkdir -p $PREFIX/bin
-
 binaries="\
 rsem-build-read-index \
 rsem-synthesis-reference-transcripts \
@@ -20,13 +18,25 @@ rsem-bam2readdepth \
 rsem-preref \
 rsem-run-gibbs \
 rsem_perl_utils.pm \
-WHAT_IS_NEW \
 EBSeq/rsem-for-ebseq-calculate-clustering-info \
 EBSeq/rsem-for-ebseq-find-DE \
 EBSeq/rsem-for-ebseq-generate-ngvector-from-clustering-info \
 "
+WHAT_IS_NEW="WHAT_IS_NEW"
+
+INSTALLDIR=$PREFIX/lib/rsem
+BINDIR=$PREFIX/bin
+mkdir -p $BINDIR
+mkdir -p $INSTALLDIR
 
 make
 make ebseq
 
-for i in $binaries; do cp $i $PREFIX/bin && chmod +x $PREFIX/bin/$(basename $i); done
+for i in $binaries; do cp $i $INSTALLDIR && chmod +x $INSTALLDIR/$(basename $i); done
+cp $WHAT_IS_NEW $INSTALLDIR;
+for i in $binaries; do
+    echo "#! /bin/bash" > $BINDIR/$(basename $i);
+    echo 'DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )' >>  $BINDIR/$(basename $i);
+    echo '$DIR/../lib/rsem/'$(basename $i) >> $BINDIR/$(basename $i);
+    chmod +x $BINDIR/$(basename $i);
+done
