841b6b664cbbe254114425fa29eaf9370dd90ea3
daler <dalerr@niddk.nih.gov>
2015-10-09 15:25:11 -0400

    working bioconductor-scraper

--- scripts/bioconductor-scraper.py
+++ scripts/bioconductor-scraper.py
@@ -1,44 +1,27 @@
 #!/usr/bin/env python
 
+import tempfile
+import configparser
 from textwrap import dedent
-import yaml
+import tarfile
+import pyaml
 import hashlib
 import os
 import re
 import bs4
+import urllib
 from urllib import request
 from urllib import parse
 from collections import OrderedDict
 
-def ordereddict_constructor(loader, node):
-    try:
-        omap = loader.construct_yaml_omap(node)
-        return OrderedDict(*omap)
-    except yaml.constructor.ConstructorError:
-        return loader.construct_yaml_seq(node)
-
-def represent_ordereddict(dumper, data):
-    # From https://bitbucket.org/xi/pyyaml/issues/13/loading-and-then-dumping-an-omap-is-broken
-    values = []
-    node = yaml.SequenceNode(u'tag:yaml.org,2002:seq', values, flow_style=False)
-    if dumper.alias_key is not None:
-        dumper.represented_objects[dumper.alias_key] = node
-    for key, value in data.items():
-        key_item = dumper.represent_data(key)
-        value_item = dumper.represent_data(value)
-        node_item = yaml.MappingNode(u'tag:yaml.org,2002:map', [(key_item, value_item)],
-                                     flow_style=False)
-        values.append(node_item)
-    return node
-
-yaml.add_constructor(u'tag:yaml.org,2002:seq', ordereddict_constructor)
-yaml.add_representer(OrderedDict, represent_ordereddict)
+base_url = 'http://bioconductor.org/packages/release/bioc/html'
 
 
 class BioCProjectPage(object):
-    def __init__(self, package, base_url='http://bioconductor.org/packages/release/bioc/html'):
+    def __init__(self, package):
         """
-        Represents a single Bioconductor package page and provides access to scraped data.
+        Represents a single Bioconductor package page and provides access to
+        scraped data.
 
         >>> x = BioCProjectPage('DESeq2')
         >>> x.tarball
@@ -54,65 +37,120 @@ class BioCProjectPage(object):
         self.details_table = self.soup.find_all(attrs={'class': 'details'})[0]
         self._md5 = None
         self.sanitized_package = 'bioconductor-' + package.lower()
+        self._cached_tarball = None
 
     @property
-    def tarball(self):
+    def tarball_url(self):
         """
-        Return the url to the tarball indicated in the "Package Source" section.
+        Return the url to the tarball indicated in the "Package Source"
+        section of the project's page.
         """
         r = re.compile('{0}.*\.tar.gz'.format(self.package))
+
         def f(href):
             return href and r.search(href)
+
         results = self.soup.find_all(href=f)
-        assert len(results) == 1, "Found {0} tags with '.tar.gz' in href".format(len(results))
+        assert len(results) == 1, (
+            "Found {0} tags with '.tar.gz' in href".format(len(results)))
         s = list(results[0].stripped_strings)
         assert len(s) == 1
         return os.path.join(parse.urljoin(self.url, '../src/contrib'), s[0])
 
     @property
-    def tarball_filename(self):
-        return os.path.basename(self.tarball)
+    def tarball_basename(self):
+        return os.path.basename(self.tarball_url)
+
+    @property
+    def cached_tarball(self):
+        if self._cached_tarball:
+            return self._cached_tarball
+        fn = tempfile.NamedTemporaryFile(delete=False).name
+        with open(fn, 'wb') as fout:
+            fout.write(request.urlopen(self.tarball_url).read())
+        self._cached_tarball = fn
+        return fn
+
+    @property
+    def description(self):
+        t = tarfile.open(self.cached_tarball)
+        d = t.extractfile(os.path.join(self.package, 'DESCRIPTION')).read()
+        self._contents = d
+        c = configparser.ConfigParser()
+        c.read_string('[top]\n' + d.decode('UTF-8'))
+        e = c['top']
+        for k in e.keys():
+            e[k] = e[k].replace('\n', ' ')
+        return dict(e)
 
     @property
     def version(self):
-        return str(self.details_table.findChild(text='Version').findNext().string)
+        return str(
+            self.details_table.findChild(text='Version').findNext().string)
 
     @property
     def license(self):
-        return str(self.details_table.findChild(text='License').findNext().string)
+        return self.description['license']
 
     @property
     def imports(self):
-        return self.details_table.findChild(text='Imports').findNext().findChildren()
-
-
-    @property
-    def dependencies(self):
-        def _versioned_dependencies(children):
-            results = []
-            for c in children:
-                if c.name == 'a':
-                    pkg = c.string
-                else:
-                    continue
-                if c.next_sibling and c.next_sibling.name is None:
-                    version = c.next_sibling.string.replace(' ', '').replace(',', '')
-                    if version:
-                        version = version.replace('(', ' ').replace(')', '')
-                else:
-                    version = ""
-                if c.get('class') == ['cran_package']:
-                    prefix = 'r-'
-                else:
-                    prefix = 'bioconductor-'
-                results.append(prefix + pkg.lower() + version + ' # ' + pkg)
-            return results
-        return list(set(_versioned_dependencies(self.imports) + _versioned_dependencies(self.depends)))
-
+        try:
+            return self.description['imports'].split(', ')
+        except KeyError:
+            return []
 
     @property
     def depends(self):
-        return self.details_table.findChild(text='Depends').findNext()
+        try:
+            return self.description['depends'].split(', ')
+        except KeyError:
+            return []
+
+    def _parse_dependencies(self, items):
+        results = []
+        for item in items:
+            toks = item.split(' (')
+            if len(toks) == 1:
+                results.append((toks[0], ""))
+            elif len(toks) == 2:
+                assert ')' in toks[1]
+                toks[1] = toks[1].replace(')', '').replace(' ', '')
+                results.append(tuple(toks))
+            else:
+                raise ValueError("Found {0} toks: {1}".format(len(toks), toks))
+        return results
+
+    @property
+    def dependencies(self):
+        results = []
+        specific_r_version = False
+        for name, version in list(
+            set(
+                self._parse_dependencies(self.imports) +
+                self._parse_dependencies(self.depends)
+            )
+        ):
+            # Try finding the dependency on the bioconductor site; if it can't
+            # be found then we assume it's in CRAN.
+            try:
+                BioCProjectPage(name)
+                prefix = 'bioconductor-'
+            except urllib.error.HTTPError:
+                prefix = 'r-'
+            if name in ['methods', 'utils']:
+                continue
+            # add padding
+            if version:
+                version = " " + version
+            if name.lower() == 'r':
+                specific_r_version = True
+                results.append(name.lower() + version)
+            else:
+                results.append(prefix + name.lower() + version)
+
+        if not specific_r_version:
+            results.append('r')
+        return results
 
     @property
     def cleaned_package(self):
@@ -121,7 +159,8 @@ class BioCProjectPage(object):
     @property
     def md5(self):
         if self._md5 is None:
-            self._md5 = hashlib.md5(request.urlopen(self.tarball).read()).hexdigest()
+            self._md5 = hashlib.md5(
+                open(self.cached_tarball, 'rb').read()).hexdigest()
         return self._md5
 
     @property
@@ -139,8 +178,8 @@ class BioCProjectPage(object):
             ),
             (
                 'source', OrderedDict((
-                    ('fn', self.tarball_filename),
-                    ('url', self.tarball),
+                    ('fn', self.tarball_basename),
+                    ('url', self.tarball_url),
                     ('md5', self.md5),
                 )),
             ),
@@ -152,25 +191,27 @@ class BioCProjectPage(object):
             ),
             (
                 'requirements', OrderedDict((
-                    ('build', ['r'] + sorted(self.dependencies)),
-                    ('run', ['r'] + sorted(self.dependencies)),
+                    ('build', sorted(self.dependencies)),
+                    ('run', sorted(self.dependencies)),
                 )),
             ),
             (
                 'test', OrderedDict((
-                    ('commands', """$R -e "library('{package}')""".format(package=self.package)),
+                    ('commands',
+                     ['''$R -e "library('{package}')"'''.format(
+                         package=self.package)]),
                 )),
             ),
             (
                 'about', OrderedDict((
                     ('home', self.url),
                     ('license', self.license),
-                    ('summary', self.summary),
+                    ('summary', self.description['description']),
                 )),
             ),
         ))
-        return yaml.dump(d, default_flow_style=False)
 
+        return pyaml.dumps(d).decode('utf-8')
 
 
 def write_recipe(package, recipe_dir, force=False):
@@ -191,17 +232,19 @@ def write_recipe(package, recipe_dir, force=False):
             """
             #!/bin/bash
 
-            # R refuses to build packages that mark themselves as Priority: Recommended
-            # mv DESCRIPTION DESCRIPTION.old
-            # grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+            # R refuses to build packages that mark themselves as
+            # "Priority: Recommended"
+            mv DESCRIPTION DESCRIPTION.old
+            grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
             #
-            # $R CMD INSTALL --build .
+            $R CMD INSTALL --build .
             #
             # # Add more build steps here, if they are necessary.
             #
-            # # See
-            # # http://docs.continuum.io/conda/build.html
-            # # for a list of environment variables that are set during the build process.
+            # See
+            # http://docs.continuum.io/conda/build.html
+            # for a list of environment variables that are set during the build
+            # process.
             # """
             )
         )
@@ -213,6 +256,7 @@ if __name__ == "__main__":
     ap.add_argument('package', help='Bioconductor package name')
     ap.add_argument('--recipe-dir', default='recipes',
                     help='Recipe will be created in <recipe-dir>/<package>')
-    ap.add_argument('--force', action='store_true', help='Overwrite the contents of an existing recipe')
+    ap.add_argument('--force', action='store_true',
+                    help='Overwrite the contents of an existing recipe')
     args = ap.parse_args()
     write_recipe(args.package, args.recipe_dir, args.force)
