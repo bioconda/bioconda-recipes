ba3190c97ea33a54600c91feb9235a1762ae1353
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-09 15:42:13 +0200

    Add tests for macs2

--- recipes/macs2/build.sh
+++ recipes/macs2/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 pushd $SRC_DIR
+
 # From setup.py: User must check GCC, if >= 4.6, use -Ofast, otherwise -O3.
-sed -i'' -e 's/"-Ofast"/"-O3"/g' setup.py
-$PYTHON setup.py install --prefix=$PREFIX
+GCCVERSION=$( gcc -dumpversion )
+GCCVERSION_MAJOR=$(echo $GCCVERSION | awk 'BEGIN {FS="."}; {print $1}')
+GCCVERSION_MINOR=$(echo $GCCVERSION | awk 'BEGIN {FS="."}; {print $2}')
+
+if [ $GCCVERSION_MAJOR <= 4 ]; then
+    if [ $GCCVERSION_MINOR <= 5 ]; then
+	sed -i'' -e 's/"-Ofast"/"-O3"/g' setup.py
+    fi
+fi
+
+$PYTHON setup.py install
+
