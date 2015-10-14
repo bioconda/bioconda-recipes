3f8399bf8a7ca79d2af1668a984d43fda4d08a61
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-14 18:10:31 +0200

    Add echo statements to output

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -4,7 +4,7 @@ mkdir -p $PREFIX/share/ncbi
 ./configure --prefix=$PREFIX --with-ngs-sdk-prefix=$PREFIX --build-prefix=$PREFIX/share/ncbi
 # sra-tools also needs the sources
 rsync -amP --include="*/" --include="*.h" --include="*.c" --include="*.hpp" --include="*.cpp" --exclude="*" $SRC_DIR/ $PREFIX/share/ncbi/ncbi-vdb
+echo "Running make and make install"
 make
 make install
-
-
+echo "Done"
