1f93ebb2801729222871a4c78084588ecd82a508
Per Unneberg <per.unneberg@scilifelab.se>
2015-10-14 16:06:14 +0200

    Add ncbi-vdb

---
+++ recipes/ncbi-vdb/build.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mkdir -p $PREFIX/share/ncbi
+./configure --prefix=$PREFIX --with-ngs-sdk-prefix=$PREFIX --build-prefix=$PREFIX/share/ncbi
+# sra-tools also needs the sources
+rsync -amP --include="*/" --include="*.h" --include="*.c" --include="*.hpp" --include="*.cpp" --exclude="*" $SRC_DIR/ $PREFIX/share/ncbi/ncbi-vdb
+make
+make install
+
+
