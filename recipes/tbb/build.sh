6d1ed22e8fe48820775881a9dad97bb313905595
chapmanb <chapmanb@50mail.com>
2015-10-15 06:13:36 -0400

    Generalize build script to work on Travis/Docker

--- recipes/tbb/build.sh
+++ recipes/tbb/build.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-rsync -aPq "include/tbb" "$INCLUDE_PATH"
-rsync -aPq "include/serial" "$INCLUDE_PATH"
+mkdir -p $PREFIX/include
+cp -r include/tbb include/serial $PREFIX/include
+mkdir -p $PREFIX/lib
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # On Linux, there's more variability, so lets build from scratch
-  make -j$CPU_COUNT prefix=$PREFIX
-  mkdir -p $LIBRARY_PATH && cp build/linux*_release/*.so* $LIBRARY_PATH
+  make -j 1 prefix=$PREFIX
+  cp build/linux*_release/*.so* $PREFIX/lib
 fi
 if [ "$(uname -s)" == "Darwin" ]; then
   # On OSX, just copy the pre-built binaries
   DYNAMIC_EXT="dylib"
-  rsync -aPq lib/*.dylib ${LIBRARY_PATH}
+  cp lib/*.dylib $PREFIX/lib
 fi
 
