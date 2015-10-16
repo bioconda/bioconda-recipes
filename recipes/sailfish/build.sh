b09bf89f3e346d987c844846e429e7aa5ba5a63d
chapmanb <chapmanb@50mail.com>
2015-10-16 05:14:44 -0400

    sailfish: switch to building from source
    The pre-built binaries have library linking issues that require
    manually setting LD_LIBRARY_PATH. Building by source uses
    conda libraries and avoids need to manually adjust this.

--- recipes/sailfish/build.sh
+++ recipes/sailfish/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-
-mkdir -p $outdir
-mkdir -p $PREFIX/bin
-
-cp -r * $outdir
-ln -s $outdir/bin/sailfish $PREFIX/bin
+mkdir -p build
+sed -i 's/Boost_USE_STATIC_LIBS ON/Boost_USE_STATIC_LIBS OFF/' CMakeLists.txt
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
+make install
