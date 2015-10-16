6122879817fa19974cfdbccb199adb84b6d88fc9
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-16 16:21:09 -0400

    Perform topological sort of recipes to build them in the right order.

--- scripts/build-packages.py
+++ scripts/build-packages.py
@@ -7,18 +7,30 @@ import argparse
 import sys
 
 import nose
+from conda_build.metadata import MetaData
+from toposort import toposort_flatten
 
 PYTHON_VERSIONS = ["27", "34"]
 CONDA_NPY = "19"
+
+
+# inspired by conda-build-all from https://github.com/omnia-md/conda-recipes
+def toposort_recipes(recipes):
+    dag = {
+        meta.get_value("package/name"): set(
+            meta.get_value("requirements/build", []))
+        for meta in map(MetaData, recipes)
+    }
+    return toposort_flatten(dag)
 
 
 def build_recipe(recipe):
     try:
         for py in PYTHON_VERSIONS:
             sp_call = sp.check_call if args.verbose else sp.check_output
-            sp_call(["conda", "build", "--no-anaconda-upload",
-                     "--numpy", CONDA_NPY, "--python", py,
-                     "--skip-existing", "--quiet", recipe],
+            sp_call(["conda", "build", "--no-anaconda-upload", "--numpy",
+                     CONDA_NPY, "--python", py, "--skip-existing", "--quiet",
+                     recipe],
                     stderr=sp.STDOUT)
     except sp.CalledProcessError as e:
         print(e.output.decode())
@@ -33,17 +45,22 @@ def test_recipes():
         recipes = list(glob.glob(os.path.join(args.repository, "recipes",
                                               "*")))
 
+    # ensure that packages are build in the right order
+    recipes = toposort_flatten(recipes)
+
+    # build packages
     for recipe in recipes:
         yield build_recipe, recipe
 
+    # upload builds
     if os.environ.get("TRAVIS_BRANCH") == "master" and os.environ.get(
-          "TRAVIS_PULL_REQUEST") == "false":
+        "TRAVIS_PULL_REQUEST") == "false":
         for recipe in recipes:
             packages = set()
             for py in PYTHON_VERSIONS:
                 packages.add(sp.check_output(["conda", "build", "--output",
-                                              "--numpy", CONDA_NPY, "--python", py,
-                                              recipe]).strip())
+                                              "--numpy", CONDA_NPY, "--python",
+                                              py, recipe]).strip())
             for package in packages:
                 if os.path.exists(package):
                     try:
@@ -63,8 +80,10 @@ if __name__ == "__main__":
     p.add_argument("--packages",
                    nargs="+",
                    help="A specific package to build.")
-    p.add_argument("-v", "--verbose", help="Make output more verbose for local debugging",
-                   default=False, action="store_true")
+    p.add_argument("-v", "--verbose",
+                   help="Make output more verbose for local debugging",
+                   default=False,
+                   action="store_true")
 
     global args
     args = p.parse_args()
