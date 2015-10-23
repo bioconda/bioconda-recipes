0f142ad4aa788d1ae98b46574b0c161fafaa891b
Johannes Koester <johannes.koester@tu-dortmund.de>
2015-10-23 11:30:23 -0400

    Updated java recipe.

--- recipes/java-jdk/build.sh
+++ recipes/java-jdk/build.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#/bin/bash -eu
 
-mkdir -p $PREFIX/bin
+# resource: https://github.com/cyclus/ciclus/tree/master/java-jdk
 
-cp bin/* $PREFIX/bin
-chmod +x $PREFIX/bin
-cp -r include jre lib $PREFIX
+# # this must exist because ln does not have the -r option in Mac. Apple, unix - but not!
+relpath(){ python -c "import os.path; print(os.path.relpath('$1','${2:-$PWD}'))" ; }
+LINKLOC="$PREFIX/lib/*/jli"
 
-# TODO the following, more self-contained approach does currently
-# not work because the .so files are not found
-#dir=$PREFIX/lib/java-jdk
-#mkdir -p $dir
-#cp -r * $dir
-#ln -s $dir/jre/lib/amd64 $PREFIX/lib/java
-#
-#cd bin
-#for cmd in *
-#do
-#    s="$PREFIX/bin/$cmd"
-#	cmd="$PREFIX/lib/java-jdk/bin/$cmd"
-#    echo "#!/bin/bash" > $s
-#    echo "$cmd $@" >> $s
-#    chmod +x $s
-#	chmod +x $cmd
-#done
+# clean up
+rm -rf release README readme.txt Welcome.html *jli.* demo sample *.zip
+mv * $PREFIX
+
+# Install
+JLI_REL=$(relpath $LINKLOC/*jli.* $PREFIX/lib)
+ln -s $JLI_REL $PREFIX/lib
+chmod +x $PREFIX/bin/* $PREFIX/jre/bin/*
+chmod +x $PREFIX/lib/jexec $PREFIX/jre/lib/jexec
+find $PREFIX -type f -name '*.so' -exec chmod +x {} \;
+
+# Some clean up
+rm -rf $PREFIX/release $PREFIX/README $PREFIX/Welcome.html
+rm $PREFIX/DISCLAIMER
+rm $PREFIX/LICENSE
+rm $PREFIX/THIRD_PARTY_README
+rm $PREFIX/ASSEMBLY_EXCEPTION
