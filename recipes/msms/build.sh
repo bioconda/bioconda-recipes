b6ba6b149282e25eefd7874610d51b4abe2ebfcb
Alexey Strokach <ostrokach@gmail.com>
2015-10-27 17:49:49 -0400

    Replaced sed scripts with patch files

--- recipes/msms/build.sh
+++ recipes/msms/build.sh
@@ -7,15 +7,6 @@ mkdir -p "$PREFIX/lib/msms"
 sed -i "s|numfile = \"./atmtypenumbers\"|numfile = \"$PREFIX/lib/msms/atmtypenumbers\"|" pdb_to_xyzr
 sed -i "s|numfile = \"./atmtypenumbers\"|numfile = \"$PREFIX/lib/msms/atmtypenumbers\"|" pdb_to_xyzrn
 
-# Change `nawk` to `awk`, since `nawk` is not installed on some platforms
-sed -i 's|nawk|awk|g' pdb_to_xyzrn
-
-# Patch pdb_to_xyzrn so that it includes the chain in its output
-sed -i 's|resnum=substr($0,23,4);|resnum=substr($0,23,4);\n\tchain=substr($0,21,2);|' pdb_to_xyzrn
-sed -i 's|gsub(" ", "", aname);|gsub(" ", "", aname);\n\tgsub(" ", "", resnum);\n\tgsub(" ", "", chain);|' pdb_to_xyzrn
-sed -i 's|"%f %f %f %f %d %s_%s_%d\\n", x, y, z, |"%f %f %f %f %d %s_%s_%d_%s\\n", x, y, z, |g' pdb_to_xyzrn
-sed -i 's|1, aname, resname, resnum);|1, aname, resname, resnum, chain);|g' pdb_to_xyzrn
-
 # Copy files to the conda bin/ folder
 cp ./pdb_to_xyzr $PREFIX/bin
 cp ./pdb_to_xyzrn $PREFIX/bin
