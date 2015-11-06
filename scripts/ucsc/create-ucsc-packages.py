c2d7d914ef0fc0eaffdf1d0f0b8e37de093c5b28
daler <dalerr@niddk.nih.gov>
2015-11-06 10:28:10 -0500

    more improvements to ucsc recipe creation
    - download the source tarball
    - inspect tarball to identify source dir in which each program lives
    - use that to fill the build.sh template
    - build.sh always builds jkOwnLib and hg/lib
    - re-download FOOTER each time
    - handle problematic cases where FOOTER does not match source name

--- scripts/ucsc/create-ucsc-packages.py
+++ scripts/ucsc/create-ucsc-packages.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 import os
 import re
+import tarfile
+from conda.fetch import download
 
-re_summary = re.compile(r'^(?P<program>\w.*?) - (?P<description>.*)$')
-
-# This is the version of the last available tarball visible on
-# http://hgdownload.cse.ucsc.edu/admin/exe/
-VERSION = "324"
 
 def parse_footer(fn):
     for line in open(fn):
@@ -15,56 +12,88 @@ def parse_footer(fn):
             continue
         yield m.groups()
 
-if __name__ == "__main__":
-    meta_template = open('template-meta.yaml').read()
-    build_template = open('template-build.sh').read()
-    test_template = open('template-run_test.sh').read()
 
-    # relative to where this file lives
-    recipes_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'recipes')
+re_summary = re.compile(r'^(?P<program>\w.*?) - (?P<description>.*)$')
 
-    # FOOTER is from:
-    # http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/FOOTER
-    for program, summary in parse_footer('FOOTER'):
+# This is the version of the last available tarball visible on
+# http://hgdownload.cse.ucsc.edu/admin/exe/
+VERSION = "324"
 
-        # some programs -- like bedGraphToBigWig -- have summary lines that
-        # look like this:
-        #
-        #   bedGraphToBigWig v 4 - Convert a bedGraph file to bigWig format
-        #
-        # So just get the first word as the program name.
-        program = program.split()[0]
+tarball = 'http://hgdownload.cse.ucsc.edu/admin/exe/userApps.v{0}.src.tgz'.format(VERSION)
+if not os.path.exists(os.path.basename(tarball)):
+    download(tarball, os.path.basename(tarball))
 
-        # conda package names must be lowercase
-        package = 'ucsc-' + program.lower()
-        recipe_dir = os.path.join(recipes_dir, package)
+# Different programs are built under different subdirectories in the source. So
+# get a directory listing of the tarball
+names = [i for i in tarfile.open((os.path.basename(tarball))).getnames()
+         if i.startswith('./userApps/kent/src')]
 
-        if not os.path.exists(recipe_dir):
-            os.makedirs(recipe_dir)
+def program_subdir(program, names):
+    # shortest one should be the directory entry
+    hits = [i for i in names if program in i]
+    top = sorted(hits)[0]
+    return top.replace('./userApps/', '')
 
-        # Fill in templates and write them to recipe dir
-        with open(os.path.join(recipe_dir, 'meta.yaml'), 'w') as fout:
-            fout.write(
-                meta_template.format(
-                    program=program,
-                    package=package,
-                    summary=summary,
-                    version=VERSION,
-                )
+
+download('http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/FOOTER', 'FOOTER')
+
+meta_template = open('template-meta.yaml').read()
+build_template = open('template-build.sh').read()
+test_template = open('template-run_test.sh').read()
+
+# relative to where this file lives
+recipes_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'recipes')
+
+# Mismatches between what is parsed from FOOTER and where a program lives in
+# the source
+problematic = {
+    'LiftSpec': 'liftSpec',
+}
+
+for program, summary in parse_footer('FOOTER'):
+
+    # some programs -- like bedGraphToBigWig -- have summary lines that
+    # look like this:
+    #
+    #   bedGraphToBigWig v 4 - Convert a bedGraph file to bigWig format
+    #
+    # So just get the first word as the program name.
+    program = program.split()[0]
+
+    program = problematic.get(program, program)
+
+    # conda package names must be lowercase
+    package = 'ucsc-' + program.lower()
+    recipe_dir = os.path.join(recipes_dir, package)
+
+    if not os.path.exists(recipe_dir):
+        os.makedirs(recipe_dir)
+
+    # Fill in templates and write them to recipe dir
+    with open(os.path.join(recipe_dir, 'meta.yaml'), 'w') as fout:
+        fout.write(
+            meta_template.format(
+                program=program,
+                package=package,
+                summary=summary,
+                version=VERSION,
             )
+        )
 
-        with open(os.path.join(recipe_dir, 'build.sh'), 'w') as fout:
-            fout.write(
-                build_template.format(
-                    program=program)
+    with open(os.path.join(recipe_dir, 'build.sh'), 'w') as fout:
+        fout.write(
+            build_template.format(
+                program=program,
+                program_source_dir=program_subdir(program, names),
             )
+        )
 
-        with open(os.path.join(recipe_dir, 'run_test.sh'), 'w') as fout:
-            fout.write(
-                test_template.format(
-                    program=program
-                )
+    with open(os.path.join(recipe_dir, 'run_test.sh'), 'w') as fout:
+        fout.write(
+            test_template.format(
+                program=program
             )
+        )
 
-        with open(os.path.join(recipe_dir, 'include.patch'), 'w') as fout:
-            fout.write(open('include.patch').read())
+    with open(os.path.join(recipe_dir, 'include.patch'), 'w') as fout:
+        fout.write(open('include.patch').read())
--- scripts/ucsc/template-build.sh
+++ scripts/ucsc/template-build.sh
@@ -4,9 +4,9 @@ export MACHTYPE=x86_64
 export BINDIR=$(pwd)/bin
 mkdir -p $BINDIR
 (cd kent/src/lib && make)
-#(cd kent/src/jkOwnLib && make)
-# (cd kent/src/hg/lib && make)
-(cd kent/src/utils/{program} && make)
+(cd kent/src/jkOwnLib && make)
+(cd kent/src/hg/lib && make)
+(cd {program_source_dir} && make)
 mkdir -p $PREFIX/bin
 cp bin/{program} $PREFIX/bin
 chmod +x $PREFIX/bin/{program}
