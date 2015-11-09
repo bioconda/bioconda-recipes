5f87f8d90d850e3ef575fa53cd2de3a4f7155033
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-11-08 19:27:15 -0500

    Debugging output.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,9 +5,11 @@ echo Building on $TRAVIS_OS_NAME
 
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
+    echo Building in CentOS 5 Docker container
     # run CentOS5 based docker container
     docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
 else
+    echo Building on Mac OS X
     export PATH=/anaconda/bin:$PATH
     # build packages
     scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
