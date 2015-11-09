f23f5fd284d522c8cebe847ef5dd8f6ccb8cb912
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-11-09 08:45:28 -0500

    deactivate docker run for debugging.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     echo Building in CentOS 5 Docker container
     # run CentOS5 based docker container
-    docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
+    #docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
 else
     echo Building on Mac OS X
     export PATH=/anaconda/bin:$PATH
