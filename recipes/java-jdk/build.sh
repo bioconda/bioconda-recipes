25dd5d1ef5cfba4d1ee6dfd6a39110998db6843b
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-11-11 13:51:24 -0500

    Unified zulu java for macos and linux.

--- recipes/java-jdk/build.sh
+++ recipes/java-jdk/build.sh
@@ -1,6 +1,20 @@
 #/bin/bash -eu
 
-# resource: https://github.com/cyclus/ciclus/tree/master/java-jdk
+if [[ `uname` == "linux" ]]
+then
+    REFERER=http://www.azul.com/downloads/zulu/zulu-linux/
+    URL=http://cdn.azulsystems.com/zulu/bin/zulu1.8.0_60-8.9.0.4-x86lx64.zip
+    FILE=zulu1.8.0_60-8.9.0.4-x86lx64.zip
+else
+    REFERER=http://www.azul.com/downloads/zulu/zulu-mac/
+    URL=http://cdn.azulsystems.com/zulu/bin/zulu1.8.0_60-8.9.0.4-macosx.zip
+    FILE=zulu1.8.0_60-8.9.0.4-macosx.zip
+fi
+
+wget --referer=$REFERER $URL
+unzip $FILE
+
+# the rest is taken from https://github.com/cyclus/ciclus/tree/master/java-jdk
 
 # # this must exist because ln does not have the -r option in Mac. Apple, unix - but not!
 relpath(){ python -c "import os.path; print(os.path.relpath('$1','${2:-$PWD}'))" ; }
