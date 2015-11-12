1508b3557c240cf63c2d5c407d151bb9c58cf736
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-11-12 22:54:18 +0100

    Cleanup.

--- recipes/java-jdk/build.sh
+++ recipes/java-jdk/build.sh
@@ -16,10 +16,9 @@ unzip $NAME.zip
 mv $NAME/* .
 rm -r $NAME
 
-# the rest is taken from https://github.com/cyclus/ciclus/tree/master/java-jdk
-
 # clean up
 rm -rf release README readme.txt Welcome.html *jli.* demo sample *.zip
+
 if [[ `uname` == "Linux" ]]
 then
     mv lib/amd64/jli/*.so lib
@@ -28,19 +27,7 @@ then
 fi
 mv * $PREFIX
 
-# # this must exist because ln does not have the -r option in Mac. Apple, unix - but not!
-#relpath(){ python -c "import os.path; print(os.path.relpath('$1','${2:-$PWD}'))" ; }
-#LINKLOC="$PREFIX/lib/*/jli"
-
-# Install
-#JLI_REL=$(relpath $LINKLOC/*jli.* $PREFIX/lib)
-#ln -s $JLI_REL $PREFIX/lib
-
-#chmod +x $PREFIX/bin/* $PREFIX/jre/bin/*
-#chmod +x $PREFIX/lib/jexec $PREFIX/jre/lib/jexec
-#find $PREFIX -type f -name '*.so' -exec chmod +x {} \;
-
-# Some clean up
+# more clean up
 rm -rf $PREFIX/release $PREFIX/README $PREFIX/Welcome.html
 rm -f $PREFIX/DISCLAIMER
 rm -f $PREFIX/LICENSE
