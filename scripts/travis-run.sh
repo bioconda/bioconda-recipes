93dddf2b4f3c6d60d7cb93f81cab22db88553ea6
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-11-26 16:16:14 +0100

    Move travis_wait.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,9 +4,9 @@ set -e
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    travis_wait docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
+    docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
 else
     export PATH=/anaconda/bin:$PATH
     # build packages
-    travis_wait scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
+    scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
 fi
