b6dca4ecffd7530f335bb17f910bcdb1431c63f4
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-11-26 16:11:43 +0100

    Prefix builds with travis_wait to avoid timeouts.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,9 +4,9 @@ set -e
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
+    travis_wait docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
 else
     export PATH=/anaconda/bin:$PATH
     # build packages
-    scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
+    travis_wait scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
 fi
