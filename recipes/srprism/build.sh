e6514ca5fe9efb4e79889a00cc22feb5dd4bba15
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-11-30 19:54:30 -0500

    add srprism

--- recipes/srprism/build.sh
+++ recipes/srprism/build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 # cd to location of Makefile and source
-#cd $SRC_DIR/src
+cd $SRC_DIR/gnuac
 
-# export PRFX=$PREFIX
-# make
-
-ls $SRC_DIR
-
-binaries="\
-srprism \
-"
-#chmod +x $SRC_DIR/srprism
-#$SRC_DIR/srprism
-#exit 1
-
-mkdir -p $PREFIX/bin
-for i in $binaries; do cp $SRC_DIR/$i $PREFIX/bin/ && chmod +x $PREFIX/bin/$i; done
+# depends on automake, autoconf
+aclocal
+autoheader
+automake -a -c
+autoconf
+./configure --prefix=$PREFIX
+make
+make install 
