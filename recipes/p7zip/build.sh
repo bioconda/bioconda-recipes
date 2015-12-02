cc7100ca774bcb188d1bb8a8835d7ea4195165ff
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-02 14:31:27 -0500

    Merge branch 'master' into ct-blast-for-osx
    # Conflicts:
    #	osx-whitelist.txt

--- recipes/p7zip/build.sh
+++ recipes/p7zip/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+
+make all_test
+
+sed -i "s|#! /bin/sh|#!/bin/bash|" install.sh
 sed -i "s|DEST_HOME=.*|DEST_HOME=$PREFIX|" install.sh
 ./install.sh
 
