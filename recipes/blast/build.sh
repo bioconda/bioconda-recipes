50c75a097dbe55f6f53e9800b36897e38c39090f
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-02 14:30:15 -0500

    blast is now compiled on linux
    The version of blast previously distributed via bioconda was the NCBI
    binary, which requires GLIBC_2.7. Only 2.5 is available in the CentOS
    5-based bioconda test environment. This changes the blast recipe to
    build blast from source (a lengthy process) on Linux, adding a section
    to use the pre-compiled NCBI binaries on OSX (where previously blast
    was not available for OSX).

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
+set -e -x -o pipefail
 
-cp bin/* ${PREFIX}/bin
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Platform: Mac"
+
+    # export CFLAGS='-O3'
+    # export CXXFLAGS='-O3'
+
+    mkdir -p $PREFIX/bin
+    cp $SRC_DIR/bin/* $PREFIX/bin
+    chmod +x $PREFIX/bin/*
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Platform: Linux"
+
+    cd $SRC_DIR/c++
+    ./configure --prefix=$PREFIX
+
+    make
+
+    mkdir -p $PREFIX/bin
+    cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin
+    chmod +x $PREFIX/bin/*
+fi
+
