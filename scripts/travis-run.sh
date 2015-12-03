043449a0e85bcfc80d2e9d164f6d7411ad819ac9
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-03 10:37:09 -0500

    revert to travis_wait with custom timeout
    The timeout can apparently be specified for travis_wait (see:
    https://github.com/travis-ci/travis-build/blob/7f4f6ca6ad82bd08da746b2f9
    51a660c84fbde32/lib/travis/build/templates/header.sh), so reverting to
    that over our custom wrapper

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -6,9 +6,9 @@ source ./long-wait.sh
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    long_wait docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
+    docker run -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
 else
     export PATH=/anaconda/bin:$PATH
     # build packages
-    long_wait scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
+    scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
 fi
