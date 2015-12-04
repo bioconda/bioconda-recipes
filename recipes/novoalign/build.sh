290fa865ad114c563bfa9134611b27b5bbafc1d3
chapmanb <chapmanb@50mail.com>
2015-12-04 06:23:54 -0500

    New: novoalign; update: vardict-java

---
+++ recipes/novoalign/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eu
+
+mkdir -p $PREFIX/bin
+chmod a+x *novo*
+cp isnovoindex novo2paf novoalign novoalignCS novoalignCSMPI novoalignMPI novobarcode novoindex novomethyl novope2bed.pl novorun.pl novosort novoutil $PREFIX/bin
