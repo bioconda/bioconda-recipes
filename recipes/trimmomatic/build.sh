1afe5226c7889d3c844dfbedcb242bbbc20dedd2
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-04 12:14:43 -0500

    fix trimmomatic jar file name

--- recipes/trimmomatic/build.sh
+++ recipes/trimmomatic/build.sh
@@ -4,6 +4,7 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 cp -R ./* $outdir/
+mv $outdir/trimmomatic-$PKG_VERSION.jar $outdir/trimmomatic.jar
 cp $RECIPE_DIR/trimmomatic.sh $outdir/trimmomatic
 chmod +x $outdir/trimmomatic
 
