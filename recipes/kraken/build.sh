1e20b0f7f9e4cac67f2340d0576e3add26a4fffe
Simon Ye <sye737@gmail.com>
2015-12-07 17:20:33 -0500

    kraken 0.10.6unreleased
    This is based on latest git.

---
+++ recipes/kraken/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd $SRC_DIR
+
+mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
+
+chmod u+x install_kraken.sh
+./install_kraken.sh "$PREFIX/libexec"
+for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kraken-translate; do
+    chmod +x "$PREFIX/libexec/$bin"
+    ln -s "$PREFIX/libexec/$bin" "$PREFIX/bin/$bin"
+done
