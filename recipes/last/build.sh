7f3d794c0243f6eee4ec9869905391b490d9469a
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-08 21:51:37 -0500

    add make-install for scripts

--- recipes/last/build.sh
+++ recipes/last/build.sh
@@ -8,6 +8,7 @@ lastdb \
 last-split \
 last-pair-probs \
 last-merge-batches \
+maf-convert
 "
 
 chmod +x $SRC_DIR/build/*
@@ -15,3 +16,4 @@ make
 
 mkdir -p $PREFIX/bin
 for i in $binaries; do cp $SRC_DIR/src/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
+make install prefix=$PREFIX
