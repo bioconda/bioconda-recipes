4040fcfd475a7af8322c4d7abc68fb9da9d71de9
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-08 21:39:57 -0500

    explicitly copy last, modify mafft to do the same via the makefile

--- recipes/last/build.sh
+++ recipes/last/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-chmod +x $SRC_DIR/build/*
-
 export PRFX=$PREFIX
+
+binaries="\
+lastal \
+lastdb \
+last-split \
+last-pair-probs \
+last-merge-batches \
+"
+
+chmod +x $SRC_DIR/build/*
 make
-make install prefix=$PREFIX
+
+mkdir -p $PREFIX/bin
+for i in $binaries; do cp $SRC_DIR/src/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
