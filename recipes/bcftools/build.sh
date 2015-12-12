64d6e8de30a1682556bcc5b267fb49acb895adda
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2015-12-11 20:53:54 -0800

    Comment out plugins

--- recipes/bcftools/build.sh
+++ recipes/bcftools/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-make plugins install prefix=$PREFIX
+make install prefix=$PREFIX
 mkdir -p $PREFIX/lib
-cp $(pwd)/plugins/*.so $PREFIX/lib
+#cp $(pwd)/plugins/*.so $PREFIX/lib
 
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
+#ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+#DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+#mkdir -p $ACTIVATE_DIR
+#mkdir -p $DEACTIVATE_DIR
 
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/bcftools-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/bcftools-deactivate.sh
+#cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/bcftools-activate.sh
+#cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/bcftools-deactivate.sh
