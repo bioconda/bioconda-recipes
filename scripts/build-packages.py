5118642b003f8cdd17c35e8338720fb61e266078
Johannes Köster <johannes.koester@tu-dortmund.de>
2015-12-13 13:25:31 +0100

    Properly ignore upload that fails because package exists.

--- scripts/build-packages.py
+++ scripts/build-packages.py
@@ -139,10 +139,12 @@ def test_recipes():
                                 os.environ.get("ANACONDA_TOKEN"),
                                 "upload", package], stdout=sp.PIPE, stderr=sp.STDOUT, check=True)
                     except sp.CalledProcessError as e:
+                        print(e.stdout.decode(), file=sys.stderr)
                         if b"already exists" in e.stdout:
                             # ignore error assuming that it is caused by existing package
                             pass
-                        raise e
+                        else:
+                            raise e
 
 
 if __name__ == "__main__":
