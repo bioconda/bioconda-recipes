5d8583328795673c323b9073ddfdea4db194f65b
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-12-16 15:50:48 -0500

    add older Trinity
    This adds a recipe for an older version of Trinity (2011-11-26) that
    uses an older assembly algorithm that works better with highly diverse
    viral genomes. It also restructures the regular trinity build script a
    bit to have the Trinity files in a subdir of $BIN to reduce likelihood
    of conflicts

--- recipes/trinity/2011_11_26/build.sh
+++ recipes/trinity/2011_11_26/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-#mkdir -p $PREFIX/bin
-#cp -R $SRC_DIR/* $PREFIX/bin/
-
-# ===========================
-
 BINARY=Trinity
 BINARY_HOME=$PREFIX/bin
 TRINITY_HOME=$PREFIX/bin/trinity-$PKG_VERSION
 
 cd $SRC_DIR
 
-#make
+make
 
 # remove the sample data
 rm -rf $SRC_DIR/sample_data
@@ -20,5 +15,5 @@ rm -rf $SRC_DIR/sample_data
 mkdir -p $PREFIX/bin
 mkdir -p $TRINITY_HOME
 cp -R $SRC_DIR/* $TRINITY_HOME/
-cd $TRINITY_HOME && chmod +x Trinity
-cd $BINARY_HOME && ln -s trinity-$PKG_VERSION/Trinity $BINARY
+cd $TRINITY_HOME && chmod +x Trinity.pl
+cd $BINARY_HOME && ln -s trinity-$PKG_VERSION/Trinity.pl $BINARY
