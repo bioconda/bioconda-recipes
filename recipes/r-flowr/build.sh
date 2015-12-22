8603a1f7a890287697d9cd79939919e77eafc773
sseth <me@sahilseth.com>
2015-12-22 17:16:47 -0600

    update flowr paths

--- recipes/r-flowr/build.sh
+++ recipes/r-flowr/build.sh
@@ -8,7 +8,13 @@ $R CMD INSTALL --build .
 
 # Add more build steps here, if they are necessary.
 BIN=$PREFIX/bin
-$R -e "flowr::setup(bin='$BIN')"
+
+FLOWR_BASE=$PREFIX/flowr
+FLOWR_RUN=$PREFIX/flowr/runs
+FLOWR_CONF=$PREFIX/flowr/conf
+FLOWR_PIPE=$PREFIX/flowr/pipelines
+
+$R -e "flowr::setup(bin='$BIN', flow_base_path='$FLOWR_BASE', flow_run_path='$FLOWR_RUN', flow_conf_path='$FLOWR_CONF', flow_pipe_path='$FLOWR_PIPE')"
 
 # See
 # http://docs.continuum.io/conda/build.html
