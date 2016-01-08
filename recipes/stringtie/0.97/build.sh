a1802ae96575ea72f970b0c9b33059f6e6e5fa2c
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-01-08 14:17:10 +0100

    fix stringtie builds

--- recipes/stringtie/0.97/build.sh
+++ recipes/stringtie/0.97/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-ln -s $SAMTOOLS_ROOT_DIR/src samtools
-cp $SAMTOOLS_ROOT_DIR/lib/libbam.* samtools/
-make
+make release
 mkdir -p $PREFIX/bin
 mv stringtie $PREFIX/bin
--- recipes/stringtie/1.0.1/build.sh
+++ recipes/stringtie/1.0.1/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+sed -i 's/^LIBS.*/LIBS := -lbam -lz/g' Makefile
+
 make release
 mkdir -p $PREFIX/bin
 mv stringtie $PREFIX/bin
