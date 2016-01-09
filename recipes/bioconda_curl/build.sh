c3c3123cc3b1db2eef7bd9ceff66e556e8d0f04f
Devon Ryan <dpryan79@gmail.com>
2016-01-09 21:32:11 +0100

    libcurl has prefixes that need changed. Copy over pyBigWig-0.2.1b

--- recipes/bioconda_curl/build.sh
+++ recipes/bioconda_curl/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
 mkdir -p $PREFIX/etc/pki/tls/certs
 #use system curl, or whatever comes with anaconda to get the CA certificates
 curl http://curl.haxx.se/ca/cacert.pem -o $PREFIX/etc/pki/tls/certs/cacert.pem
 
 #Actually install curl over the broken anaconda version
-./configure --prefix=$PREFIX --disable-ldap --with-ssl=$PREFIX --with-zlib=$PREFIX --with-ca-path=$PREFIX/etc/pki/tls/certs --with-ca-bundle=$PREFIX/etc/pki/tls/certs/cacert.pem
+./configure --prefix=$PREFIX --disable-ldap --with-ssl=$PREFIX --with-zlib=$PREFIX --with-ca-bundle=$PREFIX/etc/pki/tls/certs/cacert.pem
 make
 make install
