220d60189ed847f1cd02bd09733aafe0871ae37a
dpryan79 <dpryan79@gmail.com>
2016-01-12 10:17:33 +0100

    Fix failing congfigure script for curl

--- recipes/curl/build.sh
+++ recipes/curl/build.sh
@@ -7,7 +7,7 @@ curl http://curl.haxx.se/ca/cacert.pem -o $PREFIX/etc/pki/tls/certs/cacert.pem
 #wget -O $PREFIX/etc/pki/tls/certs/cacert.pem http://curl.haxx.se/ca/cacert.pem
 
 #This won't build on OSX without this
-export DYLD_LIBRARY_PATH=$PREFIX
+export DYLD_LIBRARY_PATH=$PREFIX/lib
 
 #Actually install curl over the broken anaconda version
 ./configure --prefix=$PREFIX --disable-ldap --with-ssl=$PREFIX --with-zlib=$PREFIX --with-ca-bundle=$PREFIX/etc/pki/tls/certs/cacert.pem
