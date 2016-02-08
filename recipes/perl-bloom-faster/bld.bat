5cff3c43ce17a329a75a8b248e47d23f7d79211c
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-02-08 13:58:45 +0100

    Update build script

--- recipes/perl-bloom-faster/bld.bat
+++ recipes/perl-bloom-faster/bld.bat
@@ -1,29 +1,25 @@
-:: If it has Build.PL use that, otherwise use Makefile.PL
-IF exist Build.PL (
+#!/bin/bash
+
+# If it has Build.PL use that, otherwise use Makefile.PL
+if [ -f Build.PL ]; then
     perl Build.PL
-    IF errorlevel 1 exit 1
-    Build
-    IF errorlevel 1 exit 1
-    Build test
-    :: Make sure this goes in site
-    Build install --installdirs site
-    IF errorlevel 1 exit 1
-) ELSE IF exist Makefile.PL (
-    :: Make sure this goes in site
+    ./Build
+    ./Build test
+    # Make sure this goes in site
+    ./Build install --installdirs site
+elif [ -f Makefile.PL ]; then
+    # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
-    IF errorlevel 1 exit 1
     make
-    IF errorlevel 1 exit 1
     make test
-    IF errorlevel 1 exit 1
     make install
-) ELSE (
-    ECHO 'Unable to find Build.PL or Makefile.PL. You need to modify bld.bat.'
+else
+    echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
-)
+fi
 
-:: Add more build steps here, if they are necessary.
+# Add more build steps here, if they are necessary.
 
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
