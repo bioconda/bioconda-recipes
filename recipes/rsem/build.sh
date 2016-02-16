6e8051d0236d2853db9df00a8499ab2cb1b54882
Per Unneberg <per.unneberg@scilifelab.se>
2016-02-16 10:50:14 +0100

    Upgrade RSEM to 1.2.28
    - update list of executables
    - update test to include actual test of parameters
    - pass missing parameters from wrappers to binaries

--- recipes/rsem/build.sh
+++ recipes/rsem/build.sh
@@ -2,27 +2,37 @@
 pushd $SRC_DIR
 
 binaries="\
-rsem-build-read-index \
-rsem-synthesis-reference-transcripts \
-rsem-tbam2gbam \
-rsem-parse-alignments \
-rsem-simulate-reads \
-rsem-calculate-credibility-intervals \
-rsem-scan-for-paired-end-reads \
-rsem-sam-validator \
-rsem-bam2wig \
-rsem-run-em \
-rsem-get-unique \
-rsem-extract-reference-transcripts \
-rsem-bam2readdepth \
-rsem-preref \
-rsem-run-gibbs \
-rsem_perl_utils.pm \
-EBSeq/rsem-for-ebseq-calculate-clustering-info \
 EBSeq/rsem-for-ebseq-find-DE \
 EBSeq/rsem-for-ebseq-generate-ngvector-from-clustering-info \
+convert-sam-for-rsem \
+rsem-bam2readdepth \
+rsem-bam2wig \
+rsem-build-read-index \
+rsem-calculate-credibility-intervals \
+rsem-calculate-expression \
+rsem-control-fdr \
+rsem-extract-reference-transcripts \
+rsem-gen-transcript-plots \
+rsem-generate-data-matrix \
+rsem-generate-ngvector \
+rsem-get-unique \
+rsem-gff3-to-gtf \
+rsem-parse-alignments \
+rsem-plot-model \
+rsem-plot-transcript-wiggles \
+rsem-prepare-reference \
+rsem-preref \
+rsem-refseq-extract-primary-assembly \
+rsem-run-ebseq \
+rsem-run-em \
+rsem-run-gibbs \
+rsem-sam-validator \
+rsem-scan-for-paired-end-reads \
+rsem-simulate-reads \
+rsem-synthesis-reference-transcripts \
+rsem-tbam2gbam \
+rsem_perl_utils.pm \
 "
-WHAT_IS_NEW="WHAT_IS_NEW"
 
 INSTALLDIR=$PREFIX/lib/rsem
 BINDIR=$PREFIX/bin
@@ -33,10 +43,9 @@ make
 make ebseq
 
 for i in $binaries; do cp $i $INSTALLDIR && chmod +x $INSTALLDIR/$(basename $i); done
-cp $WHAT_IS_NEW $INSTALLDIR;
 for i in $binaries; do
     echo "#! /bin/bash" > $BINDIR/$(basename $i);
     echo 'DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )' >>  $BINDIR/$(basename $i);
-    echo '$DIR/../lib/rsem/'$(basename $i) >> $BINDIR/$(basename $i);
+    echo '$DIR/../lib/rsem/'$(basename $i) '$@' >> $BINDIR/$(basename $i);
     chmod +x $BINDIR/$(basename $i);
 done
