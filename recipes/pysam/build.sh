62ce1639312098e58814da32d5d7c97ccb303c1f
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-02-22 08:22:56 -0800

    Back to internal htslib for pysam

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export HTSLIB_LIBRARY_DIR=$PREFIX/lib/
-export HTSLIB_INCLUDE_DIR=$PREFIX/include/
+# Use internal htslib
+#export HTSLIB_LIBRARY_DIR=$PREFIX/lib/
+#export HTSLIB_INCLUDE_DIR=$PREFIX/include/
 $PYTHON setup.py install
