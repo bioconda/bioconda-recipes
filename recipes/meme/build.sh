80cefec42567d3ecdc44c1e8231754f5c3cf0749
Saket Choudhary <saketkc@gmail.com>
2016-02-25 20:18:03 -0800

    Install missing deps

--- recipes/meme/build.sh
+++ recipes/meme/build.sh
@@ -3,6 +3,7 @@
 
 CC=${PREFIX}/bin/gcc
 CXX=${PREFIX}/bin/g++
+perl scripts/dependencies.pl | sed -n -e 's/missing.*//p' | cpanm 
 ./configure --prefix="$PREFIX"
 make AM_CFLAGS='-DNAN="(0.0/0.0)"' && make test && make install
 
