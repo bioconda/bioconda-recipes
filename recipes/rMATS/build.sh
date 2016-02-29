84683db957c94f3f16f294990ec978b55671b518
apastore <alessandro@alexbook.local>
2016-02-29 12:00:24 -0500

    rMAST
    deleting gtf folder

--- recipes/rMATS/build.sh
+++ recipes/rMATS/build.sh
@@ -2,6 +2,6 @@
 
 echo '#!/usr/bin/env python' | cat - RNASeq-MATS.py > tmp
 mv tmp RNASeq-MATS.py
-rm -rf testData test.bam.sh testRun.sh
+rm -rf testData test.bam.sh testRun.sh gtf
 mkdir -p $PREFIX/bin
 cp -R $SRC_DIR $PREFIX/bin/$PKG_NAME
