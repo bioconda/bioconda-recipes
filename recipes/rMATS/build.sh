5378ba78c90c990ffffb052af56127d692581a4c
apastore <alessandro@alexbook.local>
2016-02-29 12:28:42 -0500

    rMATS fix script path
    fix path in rMATS

--- recipes/rMATS/build.sh
+++ recipes/rMATS/build.sh
@@ -2,6 +2,9 @@
 
 echo '#!/usr/bin/env python' | cat - RNASeq-MATS.py > tmp
 mv tmp RNASeq-MATS.py
+rmats_path = $PREFIX/share/$PKG_NAME
+sed -i -e "s/scriptPath +/$rmats_path/g" RNASeq-MATS.py
 rm -rf testData test.bam.sh testRun.sh gtf
 mkdir -p $PREFIX/bin
-cp -R $SRC_DIR $PREFIX/bin/$PKG_NAME
+cp RNASeq-MATS.py $PREFIX/bin/
+cp -R $SRC_DIR/ $PREFIX/share/$PKG_NAME
