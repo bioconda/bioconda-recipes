58582dc25906f82f491a1dcdd8ea7cd0e69b9d22
apastore <alessandro@alexbook.local>
2016-02-29 19:34:39 -0500

    Read Lenght for BAM file is set in bcbio

--- recipes/rMATS/build.sh
+++ recipes/rMATS/build.sh
@@ -4,6 +4,7 @@ echo '#!/usr/bin/env python' | cat - RNASeq-MATS.py > tmp
 mv tmp RNASeq-MATS.py
 rmats_path = $PREFIX/share/$PKG_NAME
 sed -i -e "s/scriptPath +/$rmats_path +/g" RNASeq-MATS.py
+sed -i':a;N;$!ba;s/else: ## bam file was provided.*sys.exit();//g' ~/RNASeq-MATS.py 
 rm -rf testData test.bam.sh testRun.sh gtf
 mkdir -p $PREFIX/bin
 cp RNASeq-MATS.py $PREFIX/bin/
