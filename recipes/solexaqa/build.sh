491fb0e80e55307eb882d859e7c4146eb32962b8
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-04 15:57:24 +1300

    Changed http tp https to fix download error.

--- recipes/solexaqa/build.sh
+++ recipes/solexaqa/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
+if [ `uname` == "Darwin" ]; then
     echo "Installing SolexaQA++ for OSX."
     mkdir -p $PREFIX/bin
-    cp MacOs_10.7+/* $PREFIX/bin
+    mv MacOs_10.7+/* $PREFIX/bin
 else 
     echo "Installing SolexaQA++ for UNIX/Linux."
     mkdir -p $PREFIX/bin
-    cp Linux_x64/* $PREFIX/bin
+    mv Linux_x64/* $PREFIX/bin
 fi
