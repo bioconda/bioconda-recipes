5dc18bdac47ef498721f33d816002a159bbf5a2b
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-06 09:13:17 +1300

    Copied binaries explicitly.

--- recipes/solexaqa/build.sh
+++ recipes/solexaqa/build.sh
@@ -2,9 +2,9 @@
 if [ `uname` == "Darwin" ]; then
     echo "Installing SolexaQA++ for OSX."
     mkdir -p $PREFIX/bin
-    mv MacOs_10.7+/* $PREFIX/bin
+    mv MacOs_10.7+/SolexaQA++ $PREFIX/bin
 else 
     echo "Installing SolexaQA++ for UNIX/Linux."
     mkdir -p $PREFIX/bin
-    mv Linux_x64/* $PREFIX/bin
+    mv Linux_x64/SolexaQA++ $PREFIX/bin
 fi
