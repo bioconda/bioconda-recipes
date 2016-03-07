a417c758a9cda3a0c78b5befc0ae76c89e71fed9
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2016-03-07 13:17:11 +0000

    add comments about linking htslib for future work

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -3,6 +3,9 @@
 # support, from https://github.com/chapmanb/bcbio-conda/blob/master/pysam/build.sh
 sed -i'' -e 's/"-Wno-error=declaration-after-statement",//g' setup.py
 sed -i'' -e 's/"-Wno-error=declaration-after-statement"//g' setup.py
+# linking htslib, see:
+# http://pysam.readthedocs.org/en/latest/installation.html#external
+# https://github.com/pysam-developers/pysam/blob/v0.9.0/setup.py#L79
 #export HTSLIB_LIBRARY_DIR=$PREFIX/lib:$HTSLIB_LIBRARY_DIR
 #export HTSLIB_INCLUDE_DIR=$PREFIX/include:$HTSLIB_INCLUDE_DIR
 sed -i'' -e 's/HTSLIB_MODE = \"shared\"/HTSLIB_MODE = \"separate\"/g' setup.py
