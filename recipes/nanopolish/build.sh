4fcab65ffbba3e4af4b00f22224f0403f9440937
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-03-07 23:41:15 +0100

    Update nanopore recipe

--- recipes/nanopolish/build.sh
+++ recipes/nanopolish/build.sh
@@ -2,9 +2,10 @@
 
 mkdir -p $PREFIX/bin
 
-CFLAGS="-I. -I${PREFIX}/include -L${PREFIX}/lib" PREFIX=${PREFIX} make
-#make
+#CFLAGS="-I. -I${PREFIX}/include -L${PREFIX}/lib" PREFIX=${PREFIX} make
+make
 cp nanopolish $PREFIX/bin
-
-
+cp scripts/nanopolish_makerange.py $PREFIX/bin
+cp scripts/nanopolish_merge.py $PREFIX/bin
+cp scripts/consensus-preprocess.pl $PREFIX/bin
 
