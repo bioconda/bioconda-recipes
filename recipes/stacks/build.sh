b70c67491ff442700e48d3a9015b09d4bcd2af6d
Björn Grüning <bjoern@gruenings.eu>
2016-03-07 22:27:45 +0100

    Update build.sh

--- recipes/stacks/build.sh
+++ recipes/stacks/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #This won't build on OSX without this
-export DYLD_LIBRARY_PATH=$PREFIX/lib
+export DYLD_LIBRARY_PATH=$PREFIX/lib:$PREFIX
 
 export CPPFLAGS="-std=c++11"
 ./configure --prefix=$PREFIX
