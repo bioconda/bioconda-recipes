c2027455a09aa4df9956e4eccd1a24ea81a8f4aa
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2016-03-07 12:23:51 +0000

    update pysam

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -3,4 +3,7 @@
 # support, from https://github.com/chapmanb/bcbio-conda/blob/master/pysam/build.sh
 sed -i'' -e 's/"-Wno-error=declaration-after-statement",//g' setup.py
 sed -i'' -e 's/"-Wno-error=declaration-after-statement"//g' setup.py
+#export HTSLIB_LIBRARY_DIR=$PREFIX/lib:$HTSLIB_LIBRARY_DIR
+#export HTSLIB_INCLUDE_DIR=$PREFIX/include:$HTSLIB_INCLUDE_DIR
+sed -i 's/HTSLIB_MODE = \"shared\"/HTSLIB_MODE = \"separate\"/g' setup.py
 $PYTHON setup.py install
