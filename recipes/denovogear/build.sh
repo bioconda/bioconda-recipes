5283e29062ed7c5b90a252e1e01e761102d5214c
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-03-08 20:42:45 +0100

    Update build script

--- recipes/denovogear/build.sh
+++ recipes/denovogear/build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-#strictly use anaconda build environment
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
-
 mkdir -p $PREFIX/bin
 
 cd $SRC_DIR/build 
 
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
-
-#mv exe/* $PREFIX/bin
-
-
