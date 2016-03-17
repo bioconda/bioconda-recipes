f7d45dfb0df3993a429e996f3e7145152fd0d9ff
Tiago Antao <tiagoantao@gmail.com>
2016-03-17 08:03:12 -0600

    openblas path

--- recipes/eigensoft/build.sh
+++ recipes/eigensoft/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd src
-sed -i -e 's/-lgsl/-llapacke -lgsl/g' Makefile
+#sed -i -e 's/-lgsl/-llapacke -lgsl/g' Makefile
+ls $PREFIX/lib
+export OPENBLAS=$PREFIX
 make 
 cd ..
 mkdir -p $PREFIX/bin
