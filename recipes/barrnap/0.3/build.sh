556cabecae0dd83300a14f2966b05a26808fb355
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-03-21 19:00:16 +0100

    fix barrnap to now copy everything into $PREFIX

--- recipes/barrnap/0.3/build.sh
+++ recipes/barrnap/0.3/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+mkdir -p $PREFIX/share/barrnap
 mkdir -p $PREFIX/bin
-mv ./* $PREFIX/bin
+cp ./binaries $PREFIX/share/barrnap/ -R
+cp ./db $PREFIX/share/barrnap/ -R
+cp ./barrnap $PREFIX/share/barrnap/
+ln -s $PREFIX/share/barrnap/barrnap $PREFIX/bin/
