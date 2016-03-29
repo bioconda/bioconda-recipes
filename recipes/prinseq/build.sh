ff1e58813d4976bd59e2bcd6990de7429c305f8f
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-03-29 15:01:14 +0200

    add test cases

--- recipes/prinseq/build.sh
+++ recipes/prinseq/build.sh
@@ -2,9 +2,8 @@
 
 binaries="\
 prinseq-lite.pl \
-prinseq-graphs.pl \
 prinseq-graphs-noPCA.pl \
 "
 
 mkdir -p $PREFIX/bin
-for i in $binaries; do cp $SRC_DIR/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
+for i in $binaries; do sed -i.bak 's|usr/bin/perl|usr/bin/env perl|' $i && cp $SRC_DIR/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
