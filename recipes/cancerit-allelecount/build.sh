24ad431acea10af11a5f6ebff1f504fbc2bf054f
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-04-01 20:18:10 +0200

    build all perl packages on osx and depend on perl-threaded as recommended by bioconda

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -eu
 
-wget -O htslib-1.2.1.tar.bz2 https://github.com/samtools/htslib/releases/download/1.2.1/htslib-1.2.1.tar.bz2
-tar -xjvpf htslib-1.2.1.tar.bz2
-cd htslib-1.2.1
-patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
+wget -O htslib-1.3.tar.bz2 https://github.com/samtools/htslib/releases/download/1.3/htslib-1.3.tar.bz2
+tar -xjvpf htslib-1.3.tar.bz2
+cd htslib-1.3
 make
 cd ../c
 mkdir bin
-make OPTINC=-I$SRC_DIR/htslib-1.2.1 HTSLOC=$SRC_DIR/htslib-1.2.1
+make OPTINC=-I$SRC_DIR/htslib-1.3 HTSLOC=$SRC_DIR/htslib-1.3
 mkdir -p $PREFIX/bin
-cp bin/* $PREFIX/bin
+cp bin/alleleCounter $PREFIX/bin
