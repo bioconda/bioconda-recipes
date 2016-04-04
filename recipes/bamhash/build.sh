1e72caac8f9b832958f3f1bc3cefd62445c198d7
Remi Marenco <remi.marenco@gmail.com>
2016-04-04 16:49:38 -0400

    Make all out

--- recipes/bamhash/build.sh
+++ recipes/bamhash/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export CFLAGS=-I$PREFIX/include make all
+export CFLAGS=-I$PREFIX/include
 export CXXFLAGS="${CFLAGS}"
+
+make all
 
 mv bamhash_checksum_bam $PREFIX/bin
 mv bamhash_checksum_fastq $PREFIX/bin
