d2370bbfca97a4a110cc2c818e5737da60a531b7
Remi Marenco <remi.marenco@gmail.com>
2016-04-04 15:56:22 -0400

    Create the bin folder to keep the binaries

--- recipes/bamhash/build.sh
+++ recipes/bamhash/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export CFLAGS=-I$PREFIX/include make
 
+if [ ! -d "$PREFIX/bin" ]; then
+    mkdir $PREFIX/bin
+fi
+
 mv bamhash_checksum_bam $PREFIX/bin
 mv bamhash_checksum_fasta $PREFIX/bin
 mv bamhash_checksum_fastq $PREFIX/bin
