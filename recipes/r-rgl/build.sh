a68214891defe2e393b89323a9499611a093206e
nitesh1989 <nitesh.turaga@gmail.com>
2016-04-04 14:24:17 -0400

    add more deps

---
+++ recipes/r-rgl/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# R refuses to build packages that mark themselves as Priority: Recommended
+mv DESCRIPTION DESCRIPTION.old
+grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+
+$R CMD INSTALL --build .
+
+# Add more build steps here, if they are necessary.
+sudo apt-get build-dep r-cran-rgl
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
