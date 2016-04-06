5381a44bd07939a326ee1692c475badcebc724da
Remi Marenco <remi.marenco@gmail.com>
2016-04-06 03:03:02 -0400

    Fix zlib not found on linux

--- recipes/bamhash/build.sh
+++ recipes/bamhash/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export DYLD_LIBRARY_PATH=$PREFIX/lib
-
-export CFLAGS=-I$PREFIX/include
-export CXXFLAGS="${CFLAGS}"
+export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/include
+export LDFLAGS=-L$PREFIX/lib
+export CFLAGS=-03-fPIC-I$PREFIX/include
+export CXXFLAGS=-I$PREFIX/include
 
 make all
 
