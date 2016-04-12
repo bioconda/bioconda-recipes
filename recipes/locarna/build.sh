d80aea4fcec5d505f8f7f7c80228a7660947d7b4
Sebastian Will <swill@csail.mit.edu>
2016-04-13 00:43:20 +0200

    Update locarna recipe

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-./configure --prefix=$PREFIX PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ CXXFLAGS="-I$PREFIX/include"
+./configure --prefix=$PREFIX --with-vrna=$PREFIX
 make install
-
-for p in \
-    locarna \
-    locarna_p \
-    exparna_p \
-    locarnap_fit \
-    locarna_rnafold_pp \
-    ribosum2cc \
-    sparse \
-; do
-  strip $PREFIX/bin/$p
-done
