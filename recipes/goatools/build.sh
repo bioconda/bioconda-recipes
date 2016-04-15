68ad20ade06e59c4172f7802724f8008e4f3e453
bebatut <berenice.batut@gmail.com>
2016-04-15 13:26:37 +0200

    Initiate recipe for goatools

---
+++ recipes/goatools/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+$PYTHON setup.py install
+
+binaries="\
+scripts/fetch_associations.py \
+scripts/find_enrichment.py \
+scripts/map_to_slim.py \
+scripts/plot_go_term.py \
+scripts/write_hierarchy.py
+"
+
+mkdir -p $PREFIX/bin/scripts/
+for i in $binaries; do cp $SRC_DIR/$i $PREFIX/bin/scripts; done
