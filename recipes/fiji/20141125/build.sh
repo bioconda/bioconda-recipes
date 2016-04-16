0178af3ac86da30c0520e1a50d7374985cfa4c65
Greg Von Kuster <ghv2@psu.edu>
2016-04-16 08:13:35 -0400

    Commented out problematic OS X until we have more time

--- recipes/fiji/20141125/build.sh
+++ recipes/fiji/20141125/build.sh
@@ -5,21 +5,21 @@ unamestr=`uname`
 if [ "$unamestr" == 'Linux' ];
 then
     cp -R $SRC_DIR/* $PREFIX
-elif [ "$unamestr" == 'Darwin' ];
-then
-    export DYLD_LIBRARY_PATH=$PREFIX/lib
-    hdiutil attach fiji-macosx-20141125.dmg
-    cp -R /Volumes/Fiji/Fiji.app/ $PREFIX
-    hdiutil detach /Volumes/Fiji
-    if [[ `sw_vers -productVersion | tr -d '.'` -ge 1010 ]];
-    then
-        # The ImageJ launcher contained in this version of Fiji will not
-        # run on Yosemite or newer OS versions due to some operating system
-        # change, so apply the patched version of the ImageJ launcher.
-        cp $RECIPE_DIR/ImageJ-macosx $PREFIX/Contents/MacOS/ImageJ-macosx
-        # Remove the old ImageJ launcher for tiger.
-        rm $PREFIX/Contents/MacOS/ImageJ-tiger
-    fi
+#elif [ "$unamestr" == 'Darwin' ];
+#then
+#    export DYLD_LIBRARY_PATH=$PREFIX/lib
+#    hdiutil attach fiji-macosx-20141125.dmg
+#    cp -R /Volumes/Fiji/Fiji.app/ $PREFIX
+#    hdiutil detach /Volumes/Fiji
+#    if [[ `sw_vers -productVersion | tr -d '.'` -ge 1010 ]];
+#    then
+#        # The ImageJ launcher contained in this version of Fiji will not
+#        # run on Yosemite or newer OS versions due to some operating system
+#        # change, so apply the patched version of the ImageJ launcher.
+#        cp $RECIPE_DIR/ImageJ-macosx $PREFIX/Contents/MacOS/ImageJ-macosx
+#        # Remove the old ImageJ launcher for tiger.
+#        rm $PREFIX/Contents/MacOS/ImageJ-tiger
+#    fi
 fi
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
