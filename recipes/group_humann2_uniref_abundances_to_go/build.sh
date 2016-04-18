014c6e76154ea3b411cc84e33f85b356c066bbd6
bebatut <berenice.batut@gmail.com>
2016-04-18 09:31:51 +0200

    Initiate conda package for group_humann2_uniref_abundances_to_go

---
+++ recipes/group_humann2_uniref_abundances_to_go/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin/
+cp $SRC_DIR/download_datasets.sh $PREFIX/bin/
+cp $SRC_DIR/group_to_GO_abundances.sh $PREFIX/bin/
+cp $SRC_DIR/run_tests.sh $PREFIX/bin/
+
+cp -r $SRC_DIR/src $PREFIX/bin/src
+
+cp -r $SRC_DIR/test-data $PREFIX/bin/test-data
