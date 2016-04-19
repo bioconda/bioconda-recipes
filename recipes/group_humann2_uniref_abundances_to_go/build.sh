1d0644eaaa3c9bcc31e76b3905e31c6c0978a1d5
Bérénice Batut <bebatut@gmail.com>
2016-04-19 14:50:50 +0200

    Take new script names

--- recipes/group_humann2_uniref_abundances_to_go/build.sh
+++ recipes/group_humann2_uniref_abundances_to_go/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin/
-cp $SRC_DIR/download_datasets.sh $PREFIX/bin/
-cp $SRC_DIR/group_to_GO_abundances.sh $PREFIX/bin/
+cp $SRC_DIR/group_humann2_uniref_abundances_to_GO.sh $PREFIX/bin/
 chmod +x $PREFIX/bin/*
 
 cp -r $SRC_DIR/src $PREFIX/bin/src
