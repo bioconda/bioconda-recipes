2061f1d2847d2729d2b6c09aa6dbdce0ff3d5de3
Christian Roth <christian.roth@mpibpc.mpg.de>
2016-04-26 00:22:11 +0200

    samtools: updated to version 1.3.1

--- recipes/samtools/build.sh
+++ recipes/samtools/build.sh
@@ -3,8 +3,10 @@
 sed -i.bak 's/^CPPFLAGS =$//g' Makefile
 sed -i.bak 's/^LDFLAGS  =$//g' Makefile
 
-sed -i.bak 's/^CPPFLAGS =$//g' htslib-1.3.1/Makefile
-sed -i.bak 's/^LDFLAGS  =$//g' htslib-1.3.1/Makefile
+sed -i.bak 's~s/@CURSES_LIB@/-lcurses/g~s|@CURSES_LIB@|-lcurses|g~g' Makefile
+sed -i.bak "s~-lcurses~$(ncurses${CONDA_NCURSES%.*}-config --libs)~g" Makefile
+sed -i.bak 's/^CPPFLAGS =$//g' htslib-$PKG_VERSION/Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' htslib-$PKG_VERSION/Makefile
 
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
