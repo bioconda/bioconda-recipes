d3312fab17ee841a458dbb2c45f0f5af62ec324b
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-04-27 08:52:18 +0200

    Fix deployment.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -12,15 +12,19 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder $testonly
-    # Build package documentation
-    ./scripts/build-docs.sh
 
-    # push to testall branch (replace test-all with master)
-    if [[ $TRAVIS_BRANCH = "test-all" ]]
+    if [[ $SUBDAG = 0 ]]
     then
-      git checkout testall
-      git merge -s theirs test-all
-      git push
+      # push to testall branch (TODO replace test-all with master)
+      if [[ $TRAVIS_BRANCH = "test-all" ]]
+      then
+        git checkout testall
+        git merge -s theirs test-all
+        git push
+      fi
+
+      # Build package documentation
+      ./scripts/build-docs.sh
     fi
 else
     export PATH=/anaconda/bin:$PATH
