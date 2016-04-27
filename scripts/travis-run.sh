2c29610cc25690f4604501e5ed94d6647183edd8
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-04-27 08:17:49 +0200

    Temporarily enable testall outside of master for testing purposes.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,7 +5,8 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder
-    if [ "$TESTALL" = true && $TRAVIS_BRACH = "master" ]
+    #if [ "$TESTALL" = true && $TRAVIS_BRACH = "master" ]
+    if [ "$TESTALL" = true ]
     then
         docker run -v `pwd`:/bioconda-recipes bioconda/bioconda-builder --testonly
     fi
@@ -15,7 +16,8 @@ else
     export PATH=/anaconda/bin:$PATH
     # build packages
     scripts/build-packages.py --repository .
-    if [ "$TESTALL" = true && $TRAVIS_BRACH = "master" ]
+    #if [ "$TESTALL" = true && $TRAVIS_BRACH = "master" ]
+    if [ "$TESTALL" = true ]
     then
         scripts/build-packages.py --repository . --testonly
     fi
