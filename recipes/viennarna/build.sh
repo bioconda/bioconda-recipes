229c4e0ff08450a4a91f5cad45883542c61de370
Sebastian Will <swill@csail.mit.edu>
2016-04-28 00:42:57 +0200

    update viennarna; enable osx build

--- recipes/viennarna/build.sh
+++ recipes/viennarna/build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # ViennaRNA does not support Python 3 yet.
-./configure --prefix=$PREFIX --without-python --without-ruby --disable-openmp --without-forester
+
+if [ `uname` == Darwin ] ; then
+    extra_config_options="--disable-openmp --enable-universal-binary"
+fi
+
+./configure --prefix=$PREFIX \
+            --without-python --without-perl --without-ruby \
+            ${extra_config_options} && \
+make -j${CPU_COUNT} && \
 make install
