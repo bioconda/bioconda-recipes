60359367bf4e6db6cc335c0939ef86373aa21c4d
Sebastian Will <swill@csail.mit.edu>
2016-04-27 17:35:19 +0200

    update carna

--- recipes/carna/build.sh
+++ recipes/carna/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 ./configure --prefix=$PREFIX --with-vrna=$PREFIX GECODE_HOME=$prefix --disable-gist
-
-## libstdc++ is expected in lib64, so link it there
-#ln -s $PREFIX/lib/libstdc++.la $PREFIX/lib64/libstdc++.la 
-#ln -s $PREFIX/lib/libstdc++.so $PREFIX/lib64/libstdc++.so 
-
-make
+make -j${CPU_COUNT}
 make install
