008d6fe3653d491d0724e2877ed40e18a1e5069d
Simon van Heeringen <simon.vanheeringen@gmail.com>
2016-05-02 22:18:25 +0200

    fix cd command

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -4,7 +4,7 @@ NCBI_SRC=$PREFIX/src/ncbi
 
 mkdir -p $NCBI_SRC
 cp -r $SRC_DIR $NCBI_SRC/ncbi-vdb
-cd $NCBI_SRC
+cd $NCBI_SRC/ncbi-vdb
 
 ./configure --prefix=$PREFIX/ \
 	--build-prefix=$PREFIX/share/ncbi \
