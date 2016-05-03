c18839d74302634c78014b710f8ba5db44124dde
chapmanb <chapmanb@50mail.com>
2016-05-03 06:02:13 -0400

    Fix vcf2db for OSX builds
    Switch for sed insert, which is not supported in OSX sed, to use
    perl for inserting python reference at start of file.

--- recipes/vcf2db/build.sh
+++ recipes/vcf2db/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-sed -i.bak '1i#!/opt/anaconda1anaconda2anaconda3/bin/python' vcf2db.py
+perl -pi -e 'print "#!/opt/anaconda1anaconda2anaconda3/bin/python\n" if $. == 1' vcf2db.py
 chmod a+x vcf2db.py
 mkdir -p $PREFIX/bin
 cp vcf2db.py $PREFIX/bin
