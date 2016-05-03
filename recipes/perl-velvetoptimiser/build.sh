0bd5a9317292f51bfbf13ddda387fa686eff665a
Adam Caprez <acaprez2@unl.edu>
2016-05-03 21:03:43 +0000

    Add recipe for VelvetOptimiser 2.2.5

---
+++ recipes/perl-velvetoptimiser/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TMP_DIR=`mktemp -d`
+BUILD_DIR=${TMP_DIR}/VelvetOpt
+mkdir -p ${PREFIX}/bin
+cp VelvetOptimiser.pl ${PREFIX}/bin
+cd ${TMP_DIR}
+h2xs -AX -n VelvetOpt
+rm ${BUILD_DIR}/lib/VelvetOpt.pm
+cp -R ${SRC_DIR}/VelvetOpt ${BUILD_DIR}/lib
+cp ${RECIPE_DIR}/{MANIFEST,Makefile.PL} ${BUILD_DIR}
+cd ${BUILD_DIR}
+perl Makefile.PL INSTALLDIRS=site
+make
+make install
