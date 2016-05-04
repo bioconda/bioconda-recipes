2819a934732782197a2dfd3777c87758d4544a36
Simon van Heeringen <simon.vanheeringen@gmail.com>
2016-05-04 09:47:39 +0200

    monolithic sra-tools package

--- recipes/sra-tools/build.sh
+++ recipes/sra-tools/build.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
+# download extra sources using conda's mechanism
+# see: https://github.com/stuarteberg/conda-multisrc-example
+CONDA_PYTHON=$(conda info --root)/bin/python
+${CONDA_PYTHON} ${RECIPE_DIR}/download-extra-sources.py
+
+WORK_DIR=`dirname $SRC_DIR`
+SRC_SDK=$WORK_DIR/ngs-sdk/ngs-1.2.3
+SRC_VDB=$WORK_DIR/ncbi-vdb/ncbi-vdb-2.6.2
+
+####
+# build ngs-sdk
+####
+
+cd $SRC_SDK
+# First configure fails
+# See: https://github.com/ncbi/ngs/issues/1
+./configure --prefix=$PREFIX/ --build-prefix=$PREFIX/share/ncbi
+make -C ngs-sdk
+
+./configure --prefix=$PREFIX/ --build-prefix=$PREFIX/share/ncbi
+make -C ngs-sdk install
+
+####
+# build ncbi-vdb
+####
+
+cd $SRC_VDB
+./configure --prefix=$PREFIX/ \
+	    --build-prefix=$PREFIX/share/ncbi \
+            --with-ngs-sdk-prefix=$PREFIX
+make install
+
+####
+# build sra-tools
+####
+
+cd $SRC_DIR
 ./configure --prefix=$PREFIX \
 	--build-prefix=$PREFIX/share/ncbi/ \
-	--with-ncbi-vdb-sources=$PREFIX/src/ncbi/ncbi-vdb \
+	--with-ncbi-vdb-sources=$SRC_VDB \
 	--with-ncbi-vdb-build=$PREFIX/share/ncbi/ncbi-vdb \
 	--with-ngs-sdk-prefix=$PREFIX
 
