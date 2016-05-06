ffb992f4cccec102f0ad64099a1752a413858808
daler <dalerr@niddk.nih.gov>
2016-05-06 10:01:45 -0400

    temporarily make build verbose for travis-ci debuggin

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder --env-matrix /bioconda-recipes/scripts/env_matrix.yml
+    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder --env-matrix /bioconda-recipes/scripts/env_matrix.yml -v
 
     if [[ $SUBDAG = 0 ]]
     then
