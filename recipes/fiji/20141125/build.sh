c2c1f586d424fc14fdc07daba636ad7096c60461
Greg Von Kuster <ghv2@psu.edu>
2016-05-10 11:36:29 -0400

    Ehance fiji build script to include a symlink from the fiji executable to /bin

--- recipes/fiji/20141125/build.sh
+++ recipes/fiji/20141125/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -eu -o pipefail
 
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+
 unamestr=`uname`
 if [ "$unamestr" == 'Linux' ];
 then
     cp -R $SRC_DIR/* $PREFIX
+    # Provide a link to the Linux executable from $PREFIX/bin.
+    ln -s $PREFIX/ImageJ-linux32 $PREFIX/bin/ImageJ-linux32
+    ln -s $PREFIX/ImageJ-linux64 $PREFIX/bin/ImageJ-linux64
 elif [ "$unamestr" == 'Darwin' ];
 then
     hdiutil attach fiji-macosx-20141125.dmg
@@ -18,16 +25,19 @@ then
         # run on Yosemite or newer OS versions due to some operating system
         # change, so apply the patched version of the ImageJ launcher.
         cp $RECIPE_DIR/ImageJ-macosx $PREFIX/Contents/MacOS/ImageJ-macosx
-        # Provide a link to the osx executable from the installation dir.
-        ln -s $PREFIX/Contents/MacOS/ImageJ-macosx $PREFIX/ImageJ-macosx
         # Remove the old ImageJ launcher for tiger.
         rm $PREFIX/Contents/MacOS/ImageJ-tiger
+        # Provide a link to the osx executable from the installation dir.
+        ln -s $PREFIX/Contents/MacOS/ImageJ-macosx $PREFIX/ImageJ-macosx
+        # Provide a link to the osx executable from $PREFIX/bin.
+        ln -s $PREFIX/Contents/MacOS/ImageJ-macosx $PREFIX/bin/ImageJ-macosx
+    else
+        # Provide a link to the osx executable from the installation dir.
+        ln -s $PREFIX/Contents/MacOS/ImageJ-tiger $PREFIX/ImageJ-tiger
+        # Provide a link to the osx executable from $PREFIX/bin.
+        ln -s $PREFIX/Contents/MacOS/ImageJ-tiger $PREFIX/bin/ImageJ-tiger
     fi
 fi
-
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
-mkdir -p $PREFIX/bin
 
 cp $RECIPE_DIR/fiji.sh $outdir/fiji
 ln -s $outdir/fiji $PREFIX/bin
