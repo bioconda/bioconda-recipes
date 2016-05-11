7846e3645b112ee46c653eeb56810962e8b22ad1
Björn Grüning <bjoern@gruenings.eu>
2016-05-11 12:04:45 +0200

    Update build.sh

--- recipes/bwa/build.sh
+++ recipes/bwa/build.sh
@@ -2,7 +2,7 @@
 
 sed -i.bak 's/^INCLUDES=$//g' Makefile
 
-sed -i.bak 's|^CC=\s*gcc$|CC=gcc -I$PREFIX/include -L$PREFIX/lib|g' /tmp/Makefile
+sed -i.bak 's|^CC=\s*gcc$|CC=gcc -I$PREFIX/include -L$PREFIX/lib|g' Makefile
 
 export CPPFLAGS="-I$PREFIX/include"
 export INCLUDES="-I$PREFIX/include"
