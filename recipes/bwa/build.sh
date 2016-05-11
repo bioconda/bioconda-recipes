456f9108ba6be1a9ae7724a80bab6742bbfe6881
Björn Grüning <bjoern@gruenings.eu>
2016-05-11 11:51:21 +0200

    force include and lib dir

--- recipes/bwa/build.sh
+++ recipes/bwa/build.sh
@@ -2,6 +2,8 @@
 
 sed -i.bak 's/^INCLUDES=$//g' Makefile
 
+sed -i.bak 's|^CC=\s*gcc$|CC=gcc -I$PREFIX/include -L$PREFIX/lib|g' /tmp/Makefile
+
 export CPPFLAGS="-I$PREFIX/include"
 export INCLUDES="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
