755c78fb31957f96f25fc4f86aa47f9297145bfd
Joerg Fallmann <fall@bioinf.uni-leipzig.de>
2016-05-11 14:59:13 +0200

    Added metilene recipe

---
+++ recipes/metilene/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+make -j ${CPU_COUNT}
+cp metilene ${PREFIX}/bin
+cp metilene_input.pl ${PREFIX}/bin
+cp metilene_output.pl ${PREFIX}/bin
+cp metilene_output.R ${PREFIX}/bin
+install manual.pdf ${PREFIX}/share/doc/metilene
+install README ${PREFIX}/share/doc/metilene
