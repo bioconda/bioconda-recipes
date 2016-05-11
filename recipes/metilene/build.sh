4495461f3bea980e733e1e81ca12effac87d7b57
Joerg Fallmann <fall@bioinf.uni-leipzig.de>
2016-05-11 17:48:44 +0200

    Updated metilene-recipe, thanks to @s-will

--- recipes/metilene/build.sh
+++ recipes/metilene/build.sh
@@ -4,5 +4,7 @@ cp metilene ${PREFIX}/bin
 cp metilene_input.pl ${PREFIX}/bin
 cp metilene_output.pl ${PREFIX}/bin
 cp metilene_output.R ${PREFIX}/bin
+
+install -d ${PREFIX}/share/doc/metilene
 install manual.pdf ${PREFIX}/share/doc/metilene
 install README ${PREFIX}/share/doc/metilene
