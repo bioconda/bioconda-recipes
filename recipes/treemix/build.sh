4ba237f7688e8cafa9387355bb16992528f8e951
Nick Harding <njh@well.ox.ac.uk>
2016-05-13 12:03:17 +0100

    Tidied, added flags for configure
    Added deps

--- recipes/treemix/build.sh
+++ recipes/treemix/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Download gsl: unsure why gsl dep not working
-wget ftp://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz
-tar -xvzf gsl-1.16.tar.gz
-cd gsl-1.16 && ./configure --prefix=${PREFIX}/lib && make && make install
-cd ..
-
-./configure CPPFLAGS=-I${PREFIX}/lib --prefix=$PREFIX
+./configure --with-boost=${PREFIX} CPPFLAGS=-I${PREFIX} --prefix=$PREFIX
 make
 make install
