eb36f2c7d90fc85b8d48d8b1866bacc316ecd9af
Owen Solberg <owen.solberg@gmail.com>
2016-05-12 17:35:10 -0700

    Fixed build.sh. Also added 2to3 for py3 compatibility.

--- recipes/lofreq/build.sh
+++ recipes/lofreq/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -eu
 
-wget -O samtools-1.2.tar.gz https://github.com/samtools/samtools/releases/download/1.2/samtools-1.2.tar.bz2
-tar -xjvpf samtools-1.2.tar.gz
-cd samtools-1.2
-make
-cd ..
-./configure --prefix $PREFIX SAMTOOLS=$SRC_DIR/samtools-1.2 HTSLIB=$SRC_DIR/samtools-1.2/htslib-1.2.1
-make
-make install
+if [[ "${PY_VER}" =~ 3 ]]
+then
+    2to3 -w -n bin/lofreq2_call_pparallel.py
+    2to3 -w -n bin/lofreq2_somatic.py
+fi
+
+mkdir -p $PREFIX/bin
+cp bin/* $PREFIX/bin
