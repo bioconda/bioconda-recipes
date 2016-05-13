f5480a044560457ba4087799bd004ff9c34dd5b2
Owen Solberg <owen.solberg@gmail.com>
2016-05-13 09:28:18 -0700

    using binary dist now, it builds locally on both osx and linux

--- recipes/lofreq/build.sh
+++ recipes/lofreq/build.sh
@@ -3,9 +3,17 @@ set -eu
 
 if [[ "${PY_VER}" =~ 3 ]]
 then
-    2to3 -w -n bin/lofreq2_call_pparallel.py
-    2to3 -w -n bin/lofreq2_somatic.py
+    2to3 -wn .
 fi
+
+# wget -O samtools-1.3.1.tar.bz2 https://github.com/samtools/samtools/releases/download/1.3.1/samtools-1.3.1.tar.bz2
+# tar -xjvpf samtools-1.3.1.tar.bz2
+# cd samtools-1.3.1
+# make
+# cd ..
+# ./configure --prefix $PREFIX SAMTOOLS=$SRC_DIR/samtools-1.3.1 HTSLIB=$SRC_DIR/samtools-1.3.1/htslib-1.3.1
+# make
+# make install
 
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
