d899641d447f27fb561a986e0351857817a0f77b
Sebastian Will <swill@csail.mit.edu>
2016-05-14 10:31:20 +0200

    add ncurses to include path (segemehl)

--- recipes/segemehl/build.sh
+++ recipes/segemehl/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd segemehl
 
-export C_INCLUDE_PATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
 export LIBRARY_PATH=${PREFIX}/lib
+
 make -j 1 ## do not use >1 make threads!
 
 install -t $PREFIX/bin *.x
