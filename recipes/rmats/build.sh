79a012d120f64317d37950a3ad3901dd948e6170
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-05-15 00:25:23 +0200

    add rmats

---
+++ recipes/rmats/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+cp ./* $PREFIX/ -R
+
+echo '#!/usr/bin/env python' > $PREFIX/RNASeq-MATS.py
+cat ./RNASeq-MATS.py >> $PREFIX/RNASeq-MATS.py
+
+bash $PREFIX/test.bam.sh
+
+mv $PREFIX/RNASeq-MATS.py $PREFIX/bin/RNASeq-MATS.py
+
+rm -rf $PREFIX/testData $PREFIX/gtf
+rm $PREFIX/test.bam.sh
+rm $PREFIX/testRun.sh
