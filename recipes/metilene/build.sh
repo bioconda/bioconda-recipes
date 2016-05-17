049ecefce682e2afaf39223966e80af861eef038
Joerg Fallmann <fall@bioinf.uni-leipzig.de>
2016-05-17 13:29:57 +0200

    Removed unset commands from meta.yaml, cleaning environment happens elsewhere

--- recipes/metilene/build.sh
+++ recipes/metilene/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 make -j ${CPU_COUNT}
+
 cp metilene ${PREFIX}/bin
 cp metilene_input.pl ${PREFIX}/bin
 cp metilene_output.pl ${PREFIX}/bin
 cp metilene_output.R ${PREFIX}/bin
-
-#cpanm Errno File::Temp File::Spec File::Path File::Basename List::Util Getopt::Long 
 
 install -d ${PREFIX}/share/doc/metilene
 install manual.pdf ${PREFIX}/share/doc/metilene
