7f1483393a9cc353bcaa59bdb5d2e4a974be4e36
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-05-19 10:29:11 +0200

    update wrapper

--- recipes/openmg/build.sh
+++ recipes/openmg/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp OMG.jar $PREFIX/bin/omg
-chmod 0755 "${PREFIX}/bin/omg"
+cp OMG.jar $outdir/openmg.jar
+cp $RECIPE_DIR/openmg.sh $outdir/openmg
+ls -l $outdir
+ln -s $outdir/openmg $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/openmg"
 
