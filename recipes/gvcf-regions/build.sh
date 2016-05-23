c60f871f282a29832c736230ffdf90592e2a6f17
chapmanb <chapmanb@50mail.com>
2016-05-23 05:56:46 -0400

    Make sed syntax compatible with non-GNU sed for OSX

--- recipes/gvcf-regions/build.sh
+++ recipes/gvcf-regions/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
-sed -i '1i#!/opt/anaconda1anaconda2anaconda3/bin/python' gvcf_regions.py
+sed -i $'1i\\\n#!/opt/anaconda1anaconda2anaconda3/bin/python\n' gvcf_regions.py
 chmod a+x gvcf_regions.py
 cp gvcf_regions.py $PREFIX/bin
