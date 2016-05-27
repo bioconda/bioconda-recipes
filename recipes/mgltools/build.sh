54b97f64965eb708a49907f3ea42bff58e3430a1
leobiscassi <leo.biscassi@gmail.com>
2016-05-27 16:34:21 -0300

    Fix some troubles with mgltools conda package.

--- recipes/mgltools/build.sh
+++ recipes/mgltools/build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-mkdir -p $PREFIX/mgltools
-./install.sh -d $PREFIX/mgltools
-chmod 0755 ${PREFIX}/mgltools/bin/*
-chmod 0755 ${PREFIX}/mgltools/MGLToolsPckgs/AutoDockTools/Utilities24/*.py
-alias script_ligand4=$PREFIX/mgltools/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_ligand4.py
-alias script_receptor4=$PREFIX/mgltools/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py
+./install.sh -d $PREFIX
