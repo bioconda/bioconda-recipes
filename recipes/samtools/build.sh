cccba9b5e09add66fe3c2172f565e14330177c25
chapmanb <chapmanb@50mail.com>
2016-05-29 19:23:41 -0400

    samtools: fix curses, varfilter
    - Ensure install uses ncurses by calling configuration. curses no
      longer in build image and configure resolves.
    - Avoid requiring Python by removing varfilter.py install -- this
      fails on the conda build when resolving shebang since there is no
      python installed to convert to.
    - Incorporates improvements from @croth1 in #1391 for generalizing
      samtools upgrades.

--- recipes/samtools/build.sh
+++ recipes/samtools/build.sh
@@ -3,8 +3,11 @@
 sed -i.bak 's/^CPPFLAGS =$//g' Makefile
 sed -i.bak 's/^LDFLAGS  =$//g' Makefile
 
-sed -i.bak 's/^CPPFLAGS =$//g' htslib-1.3.1/Makefile
-sed -i.bak 's/^LDFLAGS  =$//g' htslib-1.3.1/Makefile
+sed -i.bak 's/^CPPFLAGS =$//g' htslib-$PKG_VERSION/Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' htslib-$PKG_VERSION/Makefile
+
+# varfilter.py in install fails because we don't install Python
+sed -i.bak 's#misc/varfilter.py##g' Makefile
 
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
@@ -13,4 +16,5 @@ cd htslib*
 ./configure --prefix=$PREFIX --enable-libcurl CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make
 cd ..
+./configure --prefix=$PREFIX
 make install prefix=$PREFIX LIBS+=-lcurl LIBS+=-lcrypto
