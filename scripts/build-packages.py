536b8a78bc12af223fd068ade97fc289eb9771f1
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-06-01 15:03:47 +0200

    Use the new stable version of conda-build instead of our fork.

--- scripts/build-packages.py
+++ scripts/build-packages.py
@@ -227,7 +227,7 @@ if __name__ == "__main__":
     args = p.parse_args()
 
     # Use a fixed conda-build from our fork
-    sp.run(["pip", "install", "git+https://github.com/bioconda/conda-build.git@reintroduce-skip-existing"], check=True)
+    #sp.run(["pip", "install", "git+https://github.com/bioconda/conda-build.git@reintroduce-skip-existing"], check=True)
     #sp.run(["pip", "install", "git+https://github.com/bioconda/conda-build.git@20afe8250c617ab5bf3a3dcce89ea6d758b8851a"], check=True)
 
     sp.run(["gcc", "--version"], check=True)
