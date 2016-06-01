a251b6716b07ec54a15512429b130a070d2240f4
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-05-31 23:07:04 -0400

    add recipe for Broad Institute viral-ngs tools
    See: https://github.com/broadinstitute/viral-ngs

---
+++ recipes/viral-ngs/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BINARY_HOME=$PREFIX/bin
+PACKAGE_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+
+cd $SRC_DIR
+
+# remove files duplicated by conda packages
+rm tools/binaries/V-Phaser-2.0/MacOSX/libgomp.1.dylib
+chmod +x tools/scripts/*
+
+# copy source to bin
+mkdir -p $PREFIX/bin
+mkdir -p $PACKAGE_HOME
+cp -R $SRC_DIR/* $PACKAGE_HOME/
+cd $PACKAGE_HOME && chmod a+x *.py
+
+cd $PACKAGE_HOME
+find *.py -type f -exec ln -s $PACKAGE_HOME/{} $BINARY_HOME/{} \;
