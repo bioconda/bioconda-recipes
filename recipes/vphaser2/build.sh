aee518b9e8a0ad44a837c91e68122d1b5d4f1b85
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-06-01 22:07:35 -0400

    rename libgomp.1.dylib used by vphaser so it does not collide for auto rpath tool

--- recipes/vphaser2/build.sh
+++ recipes/vphaser2/build.sh
@@ -13,10 +13,12 @@ mkdir -p $PACKAGE_HOME
 if [ "$(uname)" == "Darwin" ]; then
     echo "Copying in *.dylib for OSX"
     mkdir -p $PREFIX/lib/
-    # store the dylib with the binary so it does not
+    # store the dylib with the binary (with custom name) so it does not
     # collide with the dylibs provided by libgcc
     cp $SRC_DIR/V-Phaser-2.0/MacOSX/*.dylib $PACKAGE_HOME
+    mv $PACKAGE_HOME/libgomp.1.dylib $PACKAGE_HOME/libgomp_custom.1.dylib
     cp $SRC_DIR/V-Phaser-2.0/MacOSX/variant_caller $PACKAGE_HOME
+    install_name_tool -change '@executable_path/libgomp.1.dylib' "@executable_path/libgomp_custom.1.dylib" $PACKAGE_HOME/variant_caller
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Platform: Linux"
     cp $SRC_DIR/V-Phaser-2.0/linux64/variant_caller $PACKAGE_HOME
