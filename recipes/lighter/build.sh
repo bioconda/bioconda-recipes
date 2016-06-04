91f52a795fd2ce6ee8ef3cf4f4c4129709035415
Björn Grüning <bjoern@gruenings.eu>
2016-06-04 17:32:36 +0200

    Update build.sh

--- recipes/lighter/build.sh
+++ recipes/lighter/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 export C_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+export CFLAGS="-I$PREFIX/include"
+
 mkdir -p $PREXIX/bin
 make
 cp lighter $PREFIX/bin
