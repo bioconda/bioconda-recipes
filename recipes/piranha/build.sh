4fcb5738d165d15de91ee8699e5a49c43674baa5
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2016-06-08 09:02:53 +0200

    fix test

--- recipes/piranha/build.sh
+++ recipes/piranha/build.sh
@@ -6,7 +6,6 @@ CXX=${PREFIX}/bin/g++
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-
 export CXXFLAGS="-I${INCLUDE_PATH} -L${LIBRARY_PATH}"
 export LDFLAGS="-L${LIBRARY_PATH}"
 
