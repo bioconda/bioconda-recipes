b0cc90cc79319e98a490cadc5fe61a112220e9c8
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2016-06-08 08:59:42 +0200

    fix compilation with gsl

--- recipes/piranha/build.sh
+++ recipes/piranha/build.sh
@@ -3,11 +3,18 @@
 CC=${PREFIX}/bin/gcc
 CXX=${PREFIX}/bin/g++
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export CXXFLAGS="-I${INCLUDE_PATH} -L${LIBRARY_PATH}"
+export LDFLAGS="-L${LIBRARY_PATH}"
+
 mkdir -p $PREFIX/bin
 
-./configure && \
-make && \
-make test && \
+autoconf
+./configure
+make
+# make test
 make install
-
 cp bin/* $PREFIX/bin
