4647319cdf760dad4fa2cbbb2e0993b8d016c9ca
Adam Caprez <acaprez2@unl.edu>
2016-06-08 10:03:21 -0700

    Add building on OSX.

--- recipes/aria2/build.sh
+++ recipes/aria2/build.sh
@@ -2,6 +2,15 @@
 
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 
-./configure --prefix=${PREFIX} --with-libz --with-libxml2 --with-libssh2 --with-libcares --with-sqlite3
+if [ `uname` == Darwin ]; then
+    # Needed for C++11 support.
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    # Needed to avoid the linker seeing the system-installed libxml2
+    export LD_LIBRARY_PATH=${PREFIX}/lib
+    ./configure --prefix=${PREFIX} --with-appletls --with-libz --with-libxml2 --with-libssh2 --with-libcares --with-sqlite3
+else
+    ./configure --prefix=${PREFIX} --with-openssl --with-libz --with-libxml2 --with-libssh2 --with-libcares --with-sqlite3
+fi
+
 make -j 2
 make install
