e0efd3a95ae36fc9a503b82da116a46b3ac7548d
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2016-06-08 09:13:01 +0200

    fix compile-time tests

--- recipes/piranha/build.sh
+++ recipes/piranha/build.sh
@@ -5,15 +5,13 @@ CXX=${PREFIX}/bin/g++
 
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
 export CXXFLAGS="-I${INCLUDE_PATH} -L${LIBRARY_PATH}"
-export LDFLAGS="-L${LIBRARY_PATH}"
-
-mkdir -p $PREFIX/bin
 
 autoconf
 ./configure
 make
-# make test
+make test
 make install
-cp bin/* $PREFIX/bin
+
+mkdir -p $PREFIX/bin
+cp bin/* $PREFIX/bin/
