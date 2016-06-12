3c615d4d4a092edde9884ad7fef519bf16cb5ee2
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-06-13 00:02:50 +0200

    use new python based wrapper script

--- recipes/searchgui/searchgui.py
+++ recipes/searchgui/searchgui.py
@@ -1,54 +1,87 @@
-#!/bin/sh
+#!/usr/bin/env python
+#
+# Wrapper script for Java Conda packages that ensures that the java runtime
+# is invoked with the right options. Adapted from the bash script (http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128).
+#
 
-# Find original directory of bash script, resovling symlinks
-# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#
+# Program Parameters
+#
+import os
+import sys
+import subprocess
+from os import access, getenv, X_OK
+jar_file = 'SearchGUI-2.1.4.jar'
 
-JAR_DIR=$DIR
-java=java
-if [ -e "$JAVA_HOME/bin/java" ]
-then
-java="$JAVA_HOME/bin/java"
-fi
+default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
 
-# extract memory and system property Java arguments from the list of provided arguments
-# http://java.dzone.com/articles/better-java-shell-script
-default_jvm_mem_opts="-Xms512m -Xmx1g"
-jvm_mem_opts=""
-jvm_prop_opts=""
-pass_args=""
-for arg in "$@"; do
-    case $arg in
-        '-D'*)
-            jvm_prop_opts="$jvm_prop_opts $arg"
-            ;;
-        '-XX'*)
-            jvm_prop_opts="$jvm_prop_opts $arg"
-            ;;
-         '-Xm'*)
-            jvm_mem_opts="$jvm_mem_opts $arg"
-            ;;
-         *)
-            pass_args="$pass_args $arg"
-            ;;
-    esac
-done
+# !!! End of parameter section. No user-serviceable code below this line !!!
 
-if [ "$jvm_mem_opts" == "" ] && [ -z ${_JAVA_OPTIONS+x} ]; then
-    jvm_mem_opts="$default_jvm_mem_opts"
-fi
+def real_dirname(path):
+    """Return the symlink-resolved, canonicalized directory-portion of path."""
+    return os.path.dirname(os.path.realpath(path))
 
-pass_arr=($pass_args)
-if [[ ${pass_arr[0]} == eu* ]]
-then
-    eval "$java" $jvm_mem_opts $jvm_prop_opts -cp "$JAR_DIR/SearchGUI-2.1.4.jar" $pass_args
-else
-    eval "$java" $jvm_mem_opts $jvm_prop_opts -jar "$JAR_DIR/SearchGUI-2.1.4.jar" $pass_args
-fi
-exit
+
+def java_executable():
+    """Return the executable name of the Java interpreter."""
+    java_home = getenv('JAVA_HOME')
+    java_bin = os.path.join('bin', 'java')
+
+    if java_home and access(os.path.join(java_home, java_bin), X_OK):
+        return os.path.join(java_home, java_bin)
+    else:
+        return 'java'
+
+
+def jvm_opts(argv):
+    """Construct list of Java arguments based on our argument list.
+
+    The argument list passed in argv must not include the script name.
+    The return value is a 3-tuple lists of strings of the form:
+      (memory_options, prop_options, passthrough_options)
+    """
+    mem_opts = []
+    prop_opts = []
+    pass_args = []
+
+    for arg in argv:
+        if arg.startswith('-D'):
+            prop_opts.append(arg)
+        elif arg.startswith('-XX'):
+            prop_opts.append(arg)
+        elif arg.startswith('-Xm'):
+            mem_opts.append(arg)
+        else:
+            pass_args.append(arg)
+
+    # In the original shell script the test coded below read:
+    #   if [ "$jvm_mem_opts" == "" ] && [ -z ${_JAVA_OPTIONS+x} ]
+    # To reproduce the behaviour of the above shell code fragment
+    # it is important to explictly check for equality with None
+    # in the second condition, so a null envar value counts as True!
+
+    if mem_opts == [] and getenv('_JAVA_OPTIONS') == None:
+        mem_opts = default_jvm_mem_opts
+
+    return (mem_opts, prop_opts, pass_args)
+
+
+def main():
+    java = java_executable()
+    jar_dir = real_dirname(sys.argv[0])
+    (mem_opts, prop_opts, pass_args) = jvm_opts(sys.argv[1:])
+
+    if pass_args != [] and pass_args[0].startswith('eu'):
+        jar_arg = '-cp'
+    else:
+        jar_arg = '-jar'
+
+    jar_path = os.path.join(jar_dir, jar_file)
+
+    java_args = [java]+ mem_opts + prop_opts + [jar_arg] + [jar_path] + pass_args
+
+    sys.exit(subprocess.call(java_args))
+
+
+if __name__ == '__main__':
+    main()
