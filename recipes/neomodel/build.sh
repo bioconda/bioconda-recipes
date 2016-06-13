88d0a63acb52bc195843f84df280e9f81d4ae42d
Peter van Heusden <pvh@sanbi.ac.za>
2016-06-13 15:31:26 +0200

    Add recipes for neomodel and py2neo Python modules

---
+++ recipes/neomodel/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install ${PKG_NAME}-${PKG_VERSION}-py2-none-any.whl
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
