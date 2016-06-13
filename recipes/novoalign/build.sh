75e99257b9ac84ce8958c6128170e5fec0a7c2fd
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-06-13 16:58:47 -0400

    add script to copy novoalign license file to conda bin/

--- recipes/novoalign/build.sh
+++ recipes/novoalign/build.sh
@@ -4,3 +4,10 @@ set -eu
 mkdir -p $PREFIX/bin
 chmod a+x *novo*
 cp isnovoindex novo2paf novoalign novoalignCS novoalignCSMPI novoalignMPI novobarcode novoindex novomethyl novope2bed.pl novorun.pl novosort novoutil $PREFIX/bin
+
+SOURCE_FILE=$RECIPE_DIR/novoalign-license-register.sh
+DEST_FILE=$PACKAGE_HOME/novoalign-license-register
+
+cp "$SOURCE_FILE" "$DEST_FILE"
+
+chmod +x $DEST_FILE
