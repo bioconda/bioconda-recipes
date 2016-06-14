2d533ef0d0548063b83d79e0bc5630ab8fd833aa
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-06-14 15:33:52 +0200

    Recipe for PROSIC.

--- recipes/prosic/build.sh
+++ recipes/prosic/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -euo
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
-cmake CMakeLists.txt && make
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX CMakeLists.txt && make
 cp bin/prosic-call $PREFIX/bin
 
 $PYTHON setup.py install
