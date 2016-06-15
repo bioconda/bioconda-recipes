dae684473725a9e002694626efd6d5f7db51e689
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-06-14 21:17:58 -0700

    Revert some more changes

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -8,6 +8,5 @@ sed -i'' -e 's/"-Wno-error=declaration-after-statement"//g' setup.py
 # https://github.com/pysam-developers/pysam/blob/v0.9.0/setup.py#L79
 #export HTSLIB_LIBRARY_DIR=$PREFIX/lib
 #export HTSLIB_INCLUDE_DIR=$PREFIX/include
-
 sed -i'' -e 's/HTSLIB_MODE = \"shared\"/HTSLIB_MODE = \"separate\"/g' setup.py
-$PYTHON setup.py install --enable-libcurl
+$PYTHON setup.py install
