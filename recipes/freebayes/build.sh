f3c4cb08c4cf8d3521078657c255fd0d4b0e8d40
chapmanb <chapmanb@50mail.com>
2016-06-16 05:23:24 -0400

    Update: FreeBayes, snpEff, simple_sv_annotation
    - FreeBayes update to latest development with fixes for gVCF output
      (correct reference bases) and VCF-specification ready GQ values
      (integers)
    - snpEff update to latest 4.3 development version with SV fusion
      support.
    - simple_sv_annotation handles problematic exon annotations

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -5,6 +5,10 @@ if [ "$(uname)" == "Darwin" ]; then
    sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' vcflib/smithwaterman/Makefile
 fi
 
+# bamtools/cmake for zlib
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
 mkdir -p bamtools/build
 cd bamtools/build
 cmake ..
@@ -12,7 +16,7 @@ cd ../..
 
 cd src
 make autoversion
-make
+make CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 cd ..
 
 mkdir -p $PREFIX/bin
