852a869cd67becfbea1e9e7a6eeaa3563cfa5926
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-06-16 23:32:28 -0400

    restore non-version specific krona paths

--- recipes/krona/build.sh
+++ recipes/krona/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mkdir -p $PREFIX/opt/$PKG_NAME-$PKG_VERSION
-mv ./* $PREFIX/opt/$PKG_NAME-$PKG_VERSION
-cd $PREFIX/opt/$PKG_NAME-$PKG_VERSION
+mkdir -p $PREFIX/opt/krona
+mv ./* $PREFIX/opt/krona
+cd $PREFIX/opt/krona
 ./install.pl --prefix=$PREFIX
-ln -s $PREFIX/opt/$PKG_NAME-$PKG_VERSION/updateTaxonomy.sh $PREFIX/bin/ktUpdateTaxonomy.sh
-echo 1 > $PREFIX/opt/$PKG_NAME-$PKG_VERSION/taxonomy/placeholder
+ln -s $PREFIX/opt/krona/updateTaxonomy.sh $PREFIX/bin/ktUpdateTaxonomy.sh
+echo 1 > $PREFIX/opt/krona/taxonomy/placeholder
