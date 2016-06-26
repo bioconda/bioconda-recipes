d6c684e42c30e39a4d6d5ee893d92aebddb2788b
Amos Raphenya <raphenar@mcmaster.ca>
2016-06-25 20:19:05 -0400

    adds dependency for zlib and copies gm_key

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # copy MetaGeneMark key to working directory
-#cp ./RGI-3.0.9/package/rgi/mgm/gm_key ~/.gm_key
+cp ./package/rgi/mgm/gm_key ~/.gm_key
 
 # install setup
 $PYTHON setup.py install
