4da273a11d263dd90682c7a135394baa9172b0f1
Chris Tomkins-Tinch <tomkinsc@gmail.com>
2016-06-27 22:12:38 -0400

    omit gatk-register call in viral-ngs post-link script

--- recipes/viral-ngs/post-link.sh
+++ recipes/viral-ngs/post-link.sh
@@ -2,7 +2,8 @@
 
 echo "viral-ngs depends on GATK"
 # call gatk-register which will print its usage statement
-$PREFIX/bin/gatk-register
+# $PREFIX/bin/gatk-register
+echo "Installation must be finalized manually via 'gatk-register'"
 
 # exit 0 so the install is a success
 exit 0 
