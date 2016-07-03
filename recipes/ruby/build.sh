00e648ccf547f8fc357f54ab408e76935ffbf6e9
Björn Grüning <bjoern@gruenings.eu>
2016-07-03 15:37:11 +0200

    Add DYLD fallback

--- recipes/ruby/build.sh
+++ recipes/ruby/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+
 ./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative --with-openssl-dir=$PREFIX
 make
 make install
