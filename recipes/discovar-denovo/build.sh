888bd7bcd0f3c5cde5183f42bbfead15b5466adb
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-04 15:38:48 +0400

    editing makefile for zlib

--- recipes/discovar-denovo/build.sh
+++ recipes/discovar-denovo/build.sh
@@ -8,6 +8,10 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
 ./configure --prefix=$PREFIX
+sed -i 's/CPPFLAGS =/#CPPFLAGS =/' Makefile
 make
 make install
