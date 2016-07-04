75e20b0aa67e9382102f806067871df557e3ddf1
Björn Grüning <bjoern@gruenings.eu>
2016-07-04 03:12:30 +0200

    Update build.sh

--- recipes/ruby/build.sh
+++ recipes/ruby/build.sh
@@ -2,6 +2,6 @@
 
 export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative --with-openssl-dir=$PREFIX
+./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative --with-openssl-dir=$PREFIX CPPFLAGS="-fgnu89-inline"
 make
 make install
