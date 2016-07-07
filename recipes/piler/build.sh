4f570d4eaf3904fa73a34abc5f15d0e38ef51b6e
olivier inizan <oinizan@urgi224.versailles.inra.fr>
2016-07-07 06:13:45 -0700

    piler: osx platforms

--- recipes/piler/build.sh
+++ recipes/piler/build.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+
 mkdir -p $PREFIX/bin
-make
-cp piler2 $PREFIX/bin
+if [ -d "piler" ] # osx platform
+then 
+	cd piler
+	make
+	cp piler $PREFIX/bin
+else  # linux 
+	make
+	cp piler2 $PREFIX/bin
+fi
