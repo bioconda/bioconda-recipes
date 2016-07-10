32453b8c049e9d5d6889f8e3e5518e2d48b3d220
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-10 21:38:05 +0400

    soapdenovo2 from source mostly coopted from biobuilds

--- recipes/soapdenovo2/build.sh
+++ recipes/soapdenovo2/build.sh
@@ -2,7 +2,18 @@
 
 set -x -e
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+export CXXFLAGS="${LDFLAGS} ${CPPFLAGS}"
+
 mkdir -p $PREFIX/bin
 
-make prefix=$PREFIX
-make install
+make
+
+cp SOAPdenovo-127mer $PREFIX/bin
+cp SOAPdenovo-63mer $PREFIX/bin
