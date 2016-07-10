17f21427d36673d38c0e36753c55fc2556dea05a
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-10 20:39:50 +0400

    stupid soapdenovo and its stupid lack of making

--- recipes/soapdenovo2/build.sh
+++ recipes/soapdenovo2/build.sh
@@ -3,7 +3,6 @@
 set -x -e
 
 mkdir -p $PREFIX/bin
-cp -rf SOAP* $PREFIX/bin
 
-#https://sourceforge.net/projects/soapdenovo2/files/SOAPdenovo2/bin/r240/SOAPdenovo2-bin-r240-mac.tgz/download
-# This is the MACOSX version
+make prefix=$PREFIX
+make install
