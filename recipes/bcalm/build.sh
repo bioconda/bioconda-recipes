43729c24902a653c9f88a00d6c97a7f1f1a1b926
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-07-10 13:43:22 -0400

    Added bcalm.

---
+++ recipes/bcalm/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+mkdir -p $PREFIX/bin
+
+mkdir build
+cd build
+cmake -DCMAKE_PREFIX_PATH=${PREFIX} ..
+make
+cp bcalm $PREFIX/bin
