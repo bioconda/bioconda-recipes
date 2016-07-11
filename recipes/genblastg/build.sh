8434d3dd97f5506a9edde713f000e10b80144693
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-11 11:45:38 +0400

    trying to build genblastg from source

--- recipes/genblastg/build.sh
+++ recipes/genblastg/build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+
 set -x -e
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
 mkdir -p $PREFIX/bin
-chmod 777 genblast*
-cp genblast_v138 $PREFIX/bin/genblastG
+
+make all
+#make install
+#chmod 777 genblast*
+#cp genblast_v138 $PREFIX/bin/genblastG
