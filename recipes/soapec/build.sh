19497ade568436fe7e73c052dff8ec0d2eeba2ac
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-12 14:44:31 +0400

    building from source

--- recipes/soapec/build.sh
+++ recipes/soapec/build.sh
@@ -3,5 +3,12 @@ set -x -e
 
 mkdir -p $PREFIX/bin
 
-chmod u+x bin/*
-cp bin/* $PREFIX/bin
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+make
+make install
