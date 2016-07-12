6346c4bf19253b27033146069fec01e69814d3d3
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-12 11:45:44 +0400

    trying to build again

--- recipes/soapdenovo2-prepare/build.sh
+++ recipes/soapdenovo2-prepare/build.sh
@@ -3,11 +3,19 @@ set -x -e
 
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+#export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-export ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future"
+
+if [ "$build_os" == "Darwin" ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+    export ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future"
+    export CFLAGS=-Qunused-arguments
+    export CPPFLAGS="${CPPFLAGS} -Qunused-arguments"
+else
+    export LD_LIBRARY_PATH="${PREFIX}/lib"
+fi
 
 mkdir -p $PREFIX/bin
 
