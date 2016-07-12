8b5785b39cb0f739d9316c67d71b5e01b382c2c1
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-12 14:49:33 +0400

    building from source

--- recipes/soapdenovo2-gapcloser/build.sh
+++ recipes/soapdenovo2-gapcloser/build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 set -x -e
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
 
 mkdir -p $PREFIX/bin
 
-chmod u+x GapCloser
-cp GapCloser $PREFIX/bin
+make clean
+make
 
+cp Release/GapCloser $PREFIX/bin/
