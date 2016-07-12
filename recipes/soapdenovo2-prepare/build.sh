02d8fb9304527cc67ef5d61c07d00ba92ec3f31d
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-12 11:28:30 +0400

    trying something from google

--- recipes/soapdenovo2-prepare/build.sh
+++ recipes/soapdenovo2-prepare/build.sh
@@ -7,6 +7,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
+export ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future"
 
 mkdir -p $PREFIX/bin
 
