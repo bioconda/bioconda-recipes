2f357798381c2a5be4476608c0331706450e5a2c
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2016-07-15 11:47:15 +0200

    Try other path variables

--- recipes/megahit/build.sh
+++ recipes/megahit/build.sh
@@ -5,6 +5,8 @@ CC=${PREFIX}/bin/gcc
 CXX=${PREFIX}/bin/g++
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 make
 mv megahit $PREFIX/bin
