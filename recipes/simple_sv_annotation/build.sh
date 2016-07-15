4ea2ce3db2286185c7047728f796539584a91e3d
chapmanb <chapmanb@50mail.com>
2016-07-15 04:49:26 -0400

    simple_sv_annotation: use anaconda installed python
    Avoids errors in new update due to not pointing at anaconda python
    with pyvcf.

--- recipes/simple_sv_annotation/build.sh
+++ recipes/simple_sv_annotation/build.sh
@@ -3,6 +3,7 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
+sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' simple_sv_annotation.py
 cp simple_sv_annotation.py $outdir
 cp *.txt $outdir
 chmod a+x $outdir/simple_sv_annotation.py
