187c77e1d662b0bb5ea90225ae2ee26278d0e365
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2016-07-18 11:23:22 +0200

    Another try with include paths

--- recipes/megahit/build.sh
+++ recipes/megahit/build.sh
@@ -3,10 +3,8 @@
 #strictly use anaconda build environment
 CC=${PREFIX}/bin/gcc
 CXX=${PREFIX}/bin/g++
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export CPATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
 
 make
 mv megahit $PREFIX/bin
