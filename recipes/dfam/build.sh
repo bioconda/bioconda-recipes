f9d1dbdaafd18d3b55bb63eed4ec8a3dfdbe734d
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-19 20:14:52 +0400

    agan

--- recipes/dfam/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x -e
-
-mkdir -p $PREFIX/bin
-
-cp dfamscan.pl $PREFIX/bin
-cp $RECIPE_DIR/download-dfam.sh $PREFIX/bin
--- recipes/dfam/download-dfam.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget http://dfam.org/web_download/Current_Release/Dfam.hmm.gz
