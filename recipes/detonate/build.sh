788983b4327beba829e7cdc84f1f8319483c8a24
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-20 20:02:30 +0400

    commiting to detonate

--- recipes/detonate/build.sh
+++ recipes/detonate/build.sh
@@ -8,6 +8,10 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
+export CFLAGS_EXTRA="${LDFLAGS} ${CPPFLAGS}"
 mkdir -p $PREFIX/bin
+
+
+sed -e 's@INCLUDES=       -I.@INCLUDES=       -I.$(CPPFLAGS)' ref-eval/sam/Makefile
 
 make
