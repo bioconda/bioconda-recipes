ea71563b8a767d9bec5fd358a0b25f69b38a590c
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-07-20 15:30:16 +0400

    copy it first

--- recipes/dfam/build.sh
+++ recipes/dfam/build.sh
@@ -6,5 +6,5 @@ mkdir -p $PREFIX/bin
 
 chmod 777 dfamscan.pl
 cp dfamscan.pl $PREFIX/bin
-chmod 777 download-dfam.py
 cp $RECIPE_DIR/download-dfam.py $PREFIX/bin
+chmod 777 $PREFIX/bin/download-dfam.py
