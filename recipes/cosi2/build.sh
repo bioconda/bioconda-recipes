5d7d129057dd87178fda97af2bd04da46a22aa02
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-07-27 10:01:00 -0400

    remove cruft

--- recipes/cosi2/build.sh
+++ recipes/cosi2/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#strictly use anaconda build environment
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
