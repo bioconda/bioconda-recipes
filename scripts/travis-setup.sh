406aa2b9f1aa1799425b25d79a3fc2376bd2c2e4
daler <dalerr@niddk.nih.gov>
2016-07-27 19:49:45 -0400

    try adjusting permissions to fix travis error

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
 sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
+sudo chmod +w /anaconda
 export PATH=/anaconda/bin:$PATH
 conda install -y --file scripts/requirements.txt
 pip install git+https://github.com/bioconda/bioconda-utils.git@docker-use-config
