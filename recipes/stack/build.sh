4a83f07616f11fd69e777d31439cb3d29c80b113
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-07-27 15:40:04 +0200

    Updated stack recipe

--- recipes/stack/build.sh
+++ recipes/stack/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if ! [ -x "$(command -v stack)" ]; then
         echo "Stack not available, attempting install"
-	curl -sSL https://get.haskellstack.org/ | sh
-	echo "Setting PATH for $USER"
-        export PATH=/$USER/.local/bin:/usr/local/bin/:$PATH
-        chmod 755 /$USER/.local/bin/stack
+	echo "Setting PATH"
+	cp stack/stack ${PREFIX}/bin
+        export PATH=${PREFIX}/bin:$PATH
+        chmod 755 $PREFIX/stack.
 else
 	echo "Stack available"
 fi
