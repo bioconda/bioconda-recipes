1a0e18d76f13c3c79c7b7361938968db9bec3894
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-07-27 15:54:51 +0200

    Fixed tabs in stack recipe

--- recipes/stack/build.sh
+++ recipes/stack/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if ! [ -x "$(command -v stack)" ]; then
-        echo "Stack not available, attempting install"
+	echo "Stack not available, attempting install"
 	echo "Setting PATH"
 	cp stack/stack ${PREFIX}/bin
-        export PATH=${PREFIX}/bin:$PATH
-        chmod 755 $PREFIX/stack.
+	export PATH=${PREFIX}/bin:$PATH
+	chmod 755 $PREFIX/stack.
 else
 	echo "Stack available"
 fi
