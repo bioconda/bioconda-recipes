3d7a78f69c025d84d64d9fb3b34f000a64f96ca5
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-07-27 15:17:02 +0200

    Removed stack installation from rnalien recipe

--- recipes/rnalien/build.sh
+++ recipes/rnalien/build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-if ! [ -x "$(command -v stack)" ]; then
-	curl -sSL https://get.haskellstack.org/ | sh
-        export PATH=/root/.local/bin:/usr/local/bin/:$PATH
-        chmod 755 /root/.local/bin/stack
-else
-	echo "Stack available"
-fi
 stack setup
 stack build
