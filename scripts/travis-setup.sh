c71464377dcd8a1ab2d14585bc5797c0c3ce4504
daler <dalerr@niddk.nih.gov>
2016-07-28 09:32:55 -0400

    try using /tmp

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -3,9 +3,7 @@ set -euo pipefail
 
 [[ $TRAVIS_OS_NAME = "linux" ]] && tag=Linux || tag=MacOSX
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
-sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
-sudo chown -R $USER /anaconda
-sudo chmod +w /anaconda
-export PATH=/anaconda/bin:$PATH
+bash Miniconda3-latest-${tag}-x86_64.sh -b -p /tmp/anaconda
+export PATH=/tmp/anaconda/bin:$PATH
 conda install -y --file scripts/requirements.txt
-pip install git+https://github.com/bioconda/bioconda-utils.git@docker-use-config
+pip install git+https://github.com/bioconda/bioconda-utils.git
