680fce5f56b2084e5f30a13690afcb7a62690832
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-07-29 17:23:29 +0200

    Fixed stack build.sh

--- recipes/stack/build.sh
+++ recipes/stack/build.sh
@@ -2,9 +2,10 @@
 if ! [ -x "$(command -v stack)" ]; then
 	echo "Stack not available, attempting install"
 	echo "Setting PATH"
-	cp stack/stack ${PREFIX}/bin
-	export PATH=${PREFIX}/bin:$PATH
-	chmod 755 $PREFIX/stack.
+        pwd
+	cp stack ${PREFIX}
+	export PATH=${PREFIX}:$PATH
+	chmod 755 $PREFIX/stack
 else
 	echo "Stack available"
 fi
