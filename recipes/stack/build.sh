423dd3a99d88b95925d4a2f8a8d7a3159061378a
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-07-30 23:01:49 +0200

    small cleanups

--- recipes/stack/build.sh
+++ recipes/stack/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-if ! [ -x "$(command -v stack)" ]; then
-	echo "Stack not available, attempting install"
-	mv stack ${PREFIX}/bin
-	chmod 755 $PREFIX/bin/stack
-else
-	echo "Stack available"
-fi
+
+mkdir -p ${PREFIX}/bin
+mv stack ${PREFIX}/bin
+
+chmod +x ${PREFIX}/bin/stack
