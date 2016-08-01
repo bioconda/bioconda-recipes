0c3dd0d4e3a51cb28a3ac305cb27f139a14e244f
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-08-01 10:06:19 +0200

    update scripts

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[ $TRAVIS_OS_NAME = "linux" ]]
+then
+    sudo apt-get update -qq
+    sudo apt-get install docker-engine --no-install-recommends -y -o Dpkg::Options::="--force-confmiss" -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew"
+
+fi
+
 [[ $TRAVIS_OS_NAME = "linux" ]] && tag=Linux || tag=MacOSX
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
 bash Miniconda3-latest-${tag}-x86_64.sh -b -p /tmp/anaconda
 export PATH=/tmp/anaconda/bin:$PATH
 conda install -y --file scripts/requirements.txt
-pip install git+https://github.com/bioconda/bioconda-utils.git
+pip install git+https://github.com/bioconda/bioconda-utils.git@bgruening
 chmod -R 777 /tmp/anaconda
 ls -lrth /tmp/anaconda
 echo "$UID"
 echo "$USER"
 
+if [[ $TRAVIS_OS_NAME = "linux" ]]
+then
+    sudo chown ubuntu:ubuntu /tmp/anaconda -R
+    sudo chmod 777 /tmp/anaconda -R
+fi
