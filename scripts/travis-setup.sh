67ff788306fcd9eb2057f88a070b16d2a47991d9
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-08-02 03:17:59 +0200

    use travis Docker version and not the latest one

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ $TRAVIS_OS_NAME = "linux" ]]
-then
-    sudo apt-get update -qq
-    sudo apt-get install docker-engine --no-install-recommends -y -o Dpkg::Options::="--force-confmiss" -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew"
-
-fi
-
 [[ $TRAVIS_OS_NAME = "linux" ]] && tag=Linux || tag=MacOSX
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
 bash Miniconda3-latest-${tag}-x86_64.sh -b -p /tmp/anaconda
