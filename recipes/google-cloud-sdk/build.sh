f1bf09cd6fe7f34b6b3ead073d19f935b0512173
chapmanb <chapmanb@50mail.com>
2016-08-09 06:54:42 -0400

    Improve sed for python with OSX support

--- recipes/google-cloud-sdk/build.sh
+++ recipes/google-cloud-sdk/build.sh
@@ -7,6 +7,6 @@ mkdir -p $PREFIX/bin
 cp -r * $outdir
 for FNAME in gcloud gsutil bq
 do
-    sed -i.bak 's$#!/bin/sh$#!/bin/sh\nexport CLOUDSDK_PYTHON=/opt/anaconda1anaconda2anaconda3/bin/python$' $outdir/bin/$FNAME
+    sed -i.bak 's$# <cloud-sdk-sh-preamble>$export CLOUDSDK_PYTHON=/opt/anaconda1anaconda2anaconda3/bin/python$' $outdir/bin/$FNAME
     ln -s $outdir/bin/$FNAME $PREFIX/bin
 done
