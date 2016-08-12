a532e0589619859adafd8ac4e387ae2ab4ab2103
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-08-12 20:41:51 +0300

    Bump nanopolish to v0.5.0

--- recipes/nanopolish/build.sh
+++ recipes/nanopolish/build.sh
@@ -6,5 +6,6 @@ make
 cp nanopolish $PREFIX/bin
 cp scripts/nanopolish_makerange.py $PREFIX/bin
 cp scripts/nanopolish_merge.py $PREFIX/bin
+cp scripts/dropmodel.py  $PREFIX/bin
 # cp scripts/consensus-preprocess.pl $PREFIX/bin # Skipping this pre-processing step at the moment. 
 
