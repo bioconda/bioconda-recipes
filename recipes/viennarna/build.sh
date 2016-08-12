e5ded0b8bf4c8b3ef92879b058249ce0a5eff3de
Sebastian Will <swill@csail.mit.edu>
2016-08-12 12:04:45 +0200

    Disable python3 for macosx

--- recipes/viennarna/build.sh
+++ recipes/viennarna/build.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-# ViennaRNA does not support Python 3 yet.
+# ViennaRNA does not support Python 3 for MacOSX.
 
+## choose extra configure options depending on the operating system
+## (mac or linux)
+##
 if [ `uname` == Darwin ] ; then
     extra_config_options="--disable-openmp 
                           --enable-universal-binary 
                           LDFLAGS=-Wl,-headerpad_max_install_names"
+else ## linux
+    extra_config_options="--with-python3"
 fi
 
 ./configure --prefix=$PREFIX \
-            --without-perl --without-python --with-python3 \
+            --without-perl --without-python \
             ${extra_config_options} && \
 make -j${CPU_COUNT} && \
 make install
