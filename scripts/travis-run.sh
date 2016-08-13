7fbf0610880703addf4f426cc3726f5bc9c1e1fa
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-08-13 15:56:59 +0200

    use pre-compiled image

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -3,9 +3,7 @@ set -euo pipefail
 
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
-    # run CentOS5 based docker container
-    docker build -t bioconda-builder scripts
-    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda-builder bioconda-utils build /bioconda-recipes/recipes /bioconda-recipes/config.yml
+    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bgruening/bioconda-builder bioconda-utils build /bioconda-recipes/recipes /bioconda-recipes/config.yml
 
     if [[ $SUBDAG = 0 ]]
     then
