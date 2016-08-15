19d7dc0e20f1968e9cc345e0423f67685fa495f7
Sebastian Will <swill@csail.mit.edu>
2016-08-15 15:53:12 +0200

    Cleanup, add tests

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -5,13 +5,15 @@ if [ $(uname) == Linux ] ; then
     if [ ! -d $PREFIX/lib64 ] ; then mkdir $PREFIX/lib64 ; fi
     ln -s $PREFIX/lib/libstdc++.la $PREFIX/lib64/libstdc++.la 
     ln -s $PREFIX/lib/libstdc++.so $PREFIX/lib64/libstdc++.so 
-elif [ $(uname) == Darwin ] ; then
-    ## define __restrict as empty, since otherwise clang does not
-    ## compile with C99 or C11 (this is set by locarna for linking the
-    ## Vienna RNAlib, until it offers a cleaner solution)
-    extra_config_options=--enable-STDC=no
 fi
+
+## Currently conda build uses gcc-4.8, which does not turn on C11.
+## Consequnetly, Vienna RNAlib is build without C11 support and we
+## must not set stdc here. (Anyway, C11 would currently not work
+## for the MacOSX build.)
+extra_config_options=--enable-STDC=no
 
 ./configure --prefix=$PREFIX --with-vrna=$PREFIX "${extra_config_options}" && \
 make -j ${CPU_COUNT} && \
+make -j ${CPU_COUNT} check && \
 make install
