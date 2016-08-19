c28ce28a2d198ef3fb5a0e61e2d961b80621c8b4
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-08-19 15:58:07 +0200

    Add perl-bloomfilter recipe

---
+++ recipes/perl-bloomfilter/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+
+cd swig/
+
+PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
+swig -Wall -c++ -perl5 BloomFilter.i
+g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
+g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
+
+cp $SRC_DIR/Build.PL .
+
+perl ./Build.PL
+./Build manifest
+./Build install --installdirs site
