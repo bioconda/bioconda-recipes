1605bb623b671ee5600135486ef63c79a12826fa
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-08-20 07:07:36 +0000

    Fix perl-bloomfilter issues

--- recipes/perl-bloomfilter/build.sh
+++ recipes/perl-bloomfilter/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-
 cd swig/
+
+mkdir -p perl-lib/lib
 
 PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
 swig -Wall -c++ -perl5 BloomFilter.i
 g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
 g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
 
-cp $SRC_DIR/Build.PL .
+cp *.pm perl-lib/lib
+cp *.so perl-lib/lib
 
+cp $RECIPE_DIR/Build.PL perl-lib
+cd perl-lib
 perl ./Build.PL
 ./Build manifest
 ./Build install --installdirs site
