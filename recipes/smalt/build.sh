8d3c3bccf1427aa0be82886b37ba7cb9c8a876a6
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2016-08-22 18:30:26 -0400

    smalt - adding samtools build dependency, trying again to have make check work on osx

--- recipes/smalt/build.sh
+++ recipes/smalt/build.sh
@@ -13,7 +13,5 @@ else
 fi
 
 make
-if [ -z "${OSX_ARCH}" ]; then
-		make check   # fails on macos, even though smalt binary seems ok
-fi
+make -d -d -d check   # fails on macos, even though smalt binary seems ok
 make install
