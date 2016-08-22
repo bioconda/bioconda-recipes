e2c10772c37a5ab847f212777ad165c846d3970c
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2016-08-22 19:23:21 -0400

    smalt - adding debug prints to build; upped build number to force rebuild

--- recipes/smalt/build.sh
+++ recipes/smalt/build.sh
@@ -12,6 +12,10 @@ else
 		./configure --prefix=$PREFIX --build=x86_64-apple-darwin
 fi
 
+echo environment ---------------
+env
+echo end-environment--------------
+
 make
-make -d -d -d check   # fails on macos, even though smalt binary seems ok
+make -d -d -d .SHELLFLAGS=-ecx check   # fails on macos, even though smalt binary seems ok
 make install
