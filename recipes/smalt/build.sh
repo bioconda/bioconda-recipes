4738fef8a0bf8f29429271b04e781ae5830de327
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2016-08-22 20:09:30 -0400

    smalt - disable osx build for now as make check fails there

--- recipes/smalt/build.sh
+++ recipes/smalt/build.sh
@@ -12,10 +12,6 @@ else
 		./configure --prefix=$PREFIX --build=x86_64-apple-darwin
 fi
 
-echo environment ---------------
-env
-echo end-environment--------------
-
 make
-make -d -d -d .SHELLFLAGS=-ecx check   # fails on macos, even though smalt binary seems ok
+make check   # fails on osx
 make install
