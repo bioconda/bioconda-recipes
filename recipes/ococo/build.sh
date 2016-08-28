77a1da8f5d7a9c92e08f4c7358fd4db2b4b6b275
Karel Břinda <karel.brinda@gmail.com>
2016-08-28 14:16:11 +0200

    Fix missing submodule

--- recipes/ococo/build.sh
+++ recipes/ococo/build.sh
@@ -3,6 +3,13 @@
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+(
+	cd ext
+	rm -fr htslib
+	git clone git://github.com/samtools/htslib
+)
+
+
 make
 mkdir -p $PREFIX/bin
 cp ococo $PREFIX/bin
