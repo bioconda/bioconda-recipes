579e14fa285204ccd372dbb38d69218dba75a4a5
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-09-04 10:16:16 +0200

    Use condaforge/linux-anvil container (CentOS 6, same as used by conda-forge).

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     # run CentOS5 based docker container
-    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -v `pwd`:/bioconda-recipes bioconda/bioconda-builder --env-matrix /bioconda-recipes/scripts/env_matrix.yml
+    docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN \
+               -v `pwd`:/bioconda-recipes condaforge/linux-anvil /bin/bash -c \
+               "pip install git+https://github.com/bioconda/bioconda-utils.git; cd /bioconda-recipes; bioconda-utils build recipes config.yml --loglevel=info"
 
     if [[ $SUBDAG = 0 ]]
     then
