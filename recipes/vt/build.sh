86ebd398307996a49656125dc05513bb252064e0
Björn Grüning <bjoern@gruenings.eu>
2016-09-05 17:10:32 +0200

    Update build.sh

--- recipes/vt/build.sh
+++ recipes/vt/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
+export CFLAGS="-I$PREFIX/include"
 export CPATH=${PREFIX}/include
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
 
 make
 mkdir -p $PREFIX/bin
