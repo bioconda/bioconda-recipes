4da73c7d03bb0c7aa501e938d5a1e400acf1726f
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-09-06 10:42:09 +0200

    Use full tag instead of version.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN \
                -v `pwd`:/bioconda-recipes condaforge/linux-anvil /bin/bash -c \
-               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_VER; bioconda-utils build recipes config.yml --loglevel=info"
+               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; bioconda-utils build recipes config.yml --loglevel=info"
 
     if [[ $SUBDAG = 0 ]]
     then
