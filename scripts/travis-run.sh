b97346f1a0a32fa8ed9f94e89ff5ef6d405a8b43
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-09-07 11:00:08 +0200

    Update bioconda-utils and conda-build to fix recipe filtering.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN \
                -v `pwd`:/bioconda-recipes condaforge/linux-anvil /bin/bash -c \
-               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; bioconda-utils build recipes config.yml --loglevel=info"
+               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; pip install git+https://github.com/bioconda/conda-build.git@v2.0.1-fix-output+skip; bioconda-utils build recipes config.yml --loglevel=info"
 
     if [[ $SUBDAG = 0 ]]
     then
@@ -19,5 +19,6 @@ else
     export PATH=/anaconda/bin:$PATH
     # build packages
     #scripts/build-packages.py --repository . --env-matrix scripts/env_matrix.yml
+    pip install git+https://github.com/bioconda/conda-build.git@v2.0.1-fix-output+skip
     bioconda-utils build recipes config.yml --loglevel=info
 fi
