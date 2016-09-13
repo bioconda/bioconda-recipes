aa46a11b87d11fbf0ccd0aac911c05c3cf509ba8
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-09-13 12:11:41 +0200

    More zlib fixes to bfc

--- recipes/bfc/build.sh
+++ recipes/bfc/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#strictly use anaconda build environment
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 
 mkdir -p $PREFIX/bin
 
