b9cafba0127580288e6c72804db89cb440429c15
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-09-13 13:26:49 +0200

    Patch bgt makefile

--- recipes/bgt/build.sh
+++ recipes/bgt/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-export CFLAGS="-I$PREFIX/include"
+export CFLAGS="-I$PREFIX/include -g -Wall -O2 -Wc++-compat -Wno-unused-function"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
