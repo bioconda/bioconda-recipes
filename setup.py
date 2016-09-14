e18c04a3c916dbee1307072b6978ebe9b700811a
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-09-14 09:44:14 +0200

    Restrict requests dependency to avoid version conflict with docker-py.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
             ],
         )
     ],
-    install_requires=["argh", "networkx", "pydotplus", "pyyaml", "conda_build", "docker-py", "requests"],
+    install_requires=["argh", "networkx", "pydotplus", "pyyaml", "conda_build", "docker-py", "requests<2.11"],
     entry_points={"console_scripts": [
         "bioconda-utils = bioconda_utils.cli:main",
         "bioconductor_skeleton = bioconda_utils.bioconductor_skeleton:main"
