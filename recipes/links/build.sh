aa66bbefec9c98ef0aa9e48af79a881f6bcd533e
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-09-15 15:22:06 +0200

    Bump version for links

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -2,3 +2,17 @@ mkdir -p $PREFIX/bin
 
 cp  LINKS $PREFIX/bin
 chmod +x $PREFIX/bin/LINKS
+
+cd lib/bloomfilter/swig/
+
+PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
+swig -Wall -c++ -perl5 BloomFilter.i
+g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
+g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
+
+
+h2xs -n BloomFilter -O  -F -'I ../../../'
+cd BloomFilter
+perl Makefile.PL
+make
+make install
