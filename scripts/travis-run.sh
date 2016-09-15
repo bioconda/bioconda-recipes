f4e2c5ab862a38ddb460645e3449f7b29cb8e822
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-09-15 23:12:33 +0200

    Latest conda-build branch.

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN \
                -v `pwd`:/bioconda-recipes condaforge/linux-anvil /bin/bash -c \
-               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; pip install git+https://github.com/bioconda/conda-build.git@fix-conda-npy; bioconda-utils build recipes config.yml --loglevel=info"
+               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; pip install git+https://github.com/bioconda/conda-build.git@fix-build-skip-existing; bioconda-utils build recipes config.yml --loglevel=info"
 
     if [[ $SUBDAG = 0 ]]
     then
@@ -19,6 +19,6 @@ else
     export PATH=/anaconda/bin:$PATH
     # build packages
     #scripts/build-packages.py --repository . --env-matrix scripts/env_matrix.yml
-    pip install git+https://github.com/bioconda/conda-build.git@fix-conda-npy
+    pip install git+https://github.com/bioconda/conda-build.git@fix-build-skip-existing
     bioconda-utils build recipes config.yml --loglevel=info
 fi
