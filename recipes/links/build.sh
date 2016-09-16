58a700289e0a6503d1068e6b0454fdcd9b4189d8
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-09-16 10:07:38 +0200

    Skip osx build

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -1,8 +1,11 @@
 mkdir -p $PREFIX/bin
 
+#Copying perl script to bin folder
 cp  LINKS $PREFIX/bin
 chmod +x $PREFIX/bin/LINKS
 
+
+#Recompiling C code
 cd lib/bloomfilter/swig/
 
 PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
@@ -10,7 +13,7 @@ swig -Wall -c++ -perl5 BloomFilter.i
 g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
 g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
 
-
+#Installing included perl module 
 h2xs -n BloomFilter -O  -F -'I ../../../'
 cd BloomFilter
 perl Makefile.PL
