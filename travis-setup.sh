020259dae591b2467c86722477129d44f98b1e82
daler <dalerr@niddk.nih.gov>
2016-09-19 18:09:45 -0400

    -y

--- travis-setup.sh
+++ travis-setup.sh
@@ -11,7 +11,7 @@ sudo chown -R $USER /anaconda
 mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
 mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
 export PATH=/anaconda/bin:$PATH
-conda install --file bioconda_utils/bioconda_utils-requirements.txt
+conda install -y --file bioconda_utils/bioconda_utils-requirements.txt
 pip install -r requirements.txt
 pip install -r test-requirements.txt
 python setup.py develop
