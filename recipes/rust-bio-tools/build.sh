8ee61aeae7169df13847aa7e2a9367993b118fe1
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-09-22 10:49:59 +0100

    Add zlib and fix build.

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # build statically linked binary with Rust
-LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
+C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
