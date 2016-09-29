8af3826a8a9b276de4c665f41de07acb49f81a8b
yhoogstrate <y.hoogstrate@gmail.com>
2016-09-29 10:23:27 +0200

    changes

--- recipes/segmentation-fold/build.sh
+++ recipes/segmentation-fold/build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
-cd test
-make
+make clean
+cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
 
-make install
+#make
+#make install
+
+cd scripts/energy-estimation-utility
+
+# setup_requires breaks stuff
+sed -i.bak -E "s/setup_requires.+\],//" setup.py
+sed -i.bak -E "s/install_requires.+\],//" setup.py
+
+cat setup.py > /tmp/setup.py
+
+$PYTHON setup.py install
