242f777780863e91e832760a900af7b6b9868705
aymanm <ayman.yousif@gmail.com>
2016-09-29 11:01:56 +0400

    adding pauda

---
+++ recipes/pauda/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x -e
+
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+mkdir -p $PREFIX
+cp -rf pauda-1.0.1/bin $PREFIX/bin
+cp -rf pauda-1.0.1/data $PREFIX/data
+cp -rf pauda-1.0.1/lib $PREFIX/lib
