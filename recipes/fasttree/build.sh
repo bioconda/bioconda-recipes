03816593a539a0e21d181ba40ed3b0cda6763ca8
Kévin Gravouil <k.gravouil@gmail.com>
2016-09-30 15:37:53 +0200

    add OpenMP support (FastTreeMP)

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 gcc -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTree FastTree.c -lm
+gcc -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTreeMP FastTree.c -lm
 
 mkdir -p $PREFIX/bin
 cp FastTree $PREFIX/bin
-
-#gcc -DNO_SSE -O3 -finline-functions -funroll-loops -Wall -o FastTree FastTree.c -lm
-#gcc -DNO_SSE -O3 -funroll-loops -Wall -o FastTree FastTree.c -lm
+cp FastTreeMP $PREFIX/bin
