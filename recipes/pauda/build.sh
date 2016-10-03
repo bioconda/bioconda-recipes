ab95e1617417144d7353d806d632a578504dc57f
aymanm <ayman.yousif@gmail.com>
2016-10-03 14:39:25 +0400

    add pauda merge script. made it a depency for pauda

--- recipes/pauda/build.sh
+++ recipes/pauda/build.sh
@@ -8,7 +8,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-
+echo 'prefix is ' $PREFIX
 mkdir -p $PREFIX
 cp -rf pauda-1.0.1/bin $PREFIX/bin
 cp -rf pauda-1.0.1/data $PREFIX/data
