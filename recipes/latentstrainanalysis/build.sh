f5bd4d0eafb27b82f58807f6dc0122ee238abaac
Kévin Gravouil <k.gravouil@gmail.com>
2016-10-03 17:28:02 +0200

    recipe for LatentStrainAnalysis

---
+++ recipes/latentstrainanalysis/build.sh
@@ -0,0 +1,10 @@
+mkdir -p $PREFIX/LatentStrainAnalysis/
+cp -r ./* $PREFIX/LatentStrainAnalysis/
+rm $PREFIX/LatentStrainAnalysis/testData.tar.gz
+chmod +x $PREFIX/LatentStrainAnalysis/*.sh
+
+ls -lah $PREFIX/LatentStrainAnalysis/
+
+ln -s $PREFIX/LatentStrainAnalysis/KmerSVDClustering.sh $PREFIX/bin/
+ln -s $PREFIX/LatentStrainAnalysis/ReadPartitioning.sh $PREFIX/bin/
+ln -s $PREFIX/LatentStrainAnalysis/HashCounting.sh $PREFIX/bin/
