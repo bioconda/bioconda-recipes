faba6b84bf0c544cca7d3dd192310d7512d3cc96
lecorguille <lecorguille@users.noreply.github.com>
2016-10-04 10:53:13 +0200

    explicitly include include/

--- recipes/percolator/build.sh
+++ recipes/percolator/build.sh
@@ -4,5 +4,5 @@ make install
 
 cd src/converters
 cmake . -DSERIALIZE="Boost" -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_INCLUDEDIR=$PREFIX/include/boost/ -DBOOST_LIBRARYDIR=$PREFIX/lib -DBOOST_ROOT=$PREFIX -DSQLITE3_INCLUDE_DIR=$PREFIX/include/ -DCMAKE_PREFIX_PATH=$PREFIX/lib
-make
+make -I$PREFIX/include/
 make install
