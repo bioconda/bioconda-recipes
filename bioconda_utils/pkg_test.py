c58d713055403ca1037e47dc95c87d893758d65a
daler <dalerr@niddk.nih.gov>
2016-10-10 14:26:51 -0400

    add module for mulled-build testing

---
+++ bioconda_utils/pkg_test.py
@@ -0,0 +1,104 @@
+import subprocess as sp
+import tempfile
+import tarfile
+import os
+import shlex
+
+from conda_build.metadata import MetaData
+
+'mulled-build build-and-test "gffutils" --test "python -c \'import gffutils\''
+
+
+
+
+
+def get_tests(path):
+    "Extract tests from a built package"
+    tmp = tempfile.mkdtemp()
+    t = tarfile.open(path)
+    t.extractall(tmp)
+    input_dir = os.path.join(tmp, 'info', 'recipe')
+
+    tests = []
+    recipe_meta = MetaData(input_dir)
+
+    tests_commands = recipe_meta.get_value('test/commands')
+    tests_imports = recipe_meta.get_value('test/imports')
+    requirements = recipe_meta.get_value('requirements/run')
+
+    if tests_imports or tests_commands:
+        if tests_commands:
+            tests.append(' && '.join(tests_commands))
+        if tests_imports and 'python' in requirements:
+            tests.append(' && '.join('python -c "import %s"' % imp for imp in tests_imports))
+        elif tests_imports and ('perl' in requirements or 'perl-threaded' in requirements):
+            tests.append(' && '.join('''perl -e "use %s;"''' % imp for imp in tests_imports))
+
+        tests = ' && '.join(tests)
+        tests = tests.replace('$R ', 'Rscript ')
+    else:
+        pass
+    return tests
+
+
+
+def test_package(path, name_override='tmp', channels=['bioconda', 'r', 'conda-forge'], mulled_args=""):
+    """
+    Tests a built package in a minimal docker container.
+
+    Parameters
+    ----------
+    path : str
+        Path to a .tar.bz2 package built by conda-build
+
+    name_override : str
+        Passed as the --name-override argument to mulled-build
+
+    channels : str or list
+        The local channel of the provided package will be added automatically;
+        `channels` are channels to use in addition to the local channel
+
+    mulled_args : str
+        Mechanism for passing arguments to the mulled-build command. They will
+        be split with shlex.split and passed to the mulled-build command. E.g.,
+        mulled_args="--dry-run --involucro-path /opt/involucro"
+
+    """
+    assert path.endswith('.tar.bz2'), "Unrecognized path {0}".format(path)
+    assert os.path.exists(path), '{0} does not exist'.format(path)
+
+    conda_bld_dir = os.path.abspath(os.path.dirname(os.path.dirname(path)))
+
+    sp.check_call(['conda', 'index', os.path.dirname(path)])
+
+    pkg = os.path.basename(path).replace('.tar.bz2', '')
+    toks = pkg.split('-')
+    build_string = toks[-1]
+    version = toks[-2]
+    name = '-'.join(toks[:-2])
+
+    spec = '='.join([name, version, build_string])
+
+    if isinstance(channels, str):
+        channels = [channels]
+    if not channels:
+        channels = []
+
+    channel_args = []
+    channels.append('file:/{0}'.format(conda_bld_dir))
+    for channel in channels:
+        channel_args.extend(['--extra-channel', channel])
+
+    tests = get_tests(path)
+
+
+    cmds = [
+        'mulled-build',
+        'build-and-test',
+        spec,
+        '--name-override', name_override,
+        '--test', tests
+    ]
+    cmds.extend(channel_args)
+    cmds.extend(shlex.split(mulled_args))
+    return sp.check_output(cmds, stderr=sp.STDOUT, universal_newlines=True)
