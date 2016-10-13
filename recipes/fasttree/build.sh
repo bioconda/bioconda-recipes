d9b564817bf238edc57e175dbbad72455b855999
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-10-13 07:02:00 +0200

    Cleaning up build.sh and skipping openmp for osx

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+mkdir -p $PREFIX/bin
 
 gcc -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTree FastTree.c -lm
-gcc -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTreeMP FastTree.c -lm
-
-mkdir -p $PREFIX/bin
 cp FastTree $PREFIX/bin
+
+if [ `uname` != Darwin ]; then
+gcc -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTreeMP FastTree.c -lm
 cp FastTreeMP $PREFIX/bin
+fi
+
