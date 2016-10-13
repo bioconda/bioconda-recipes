f2df6cef9e0a946351e05328719ead0ab0f534e9
daler <dalerr@niddk.nih.gov>
2016-10-13 15:02:41 -0400

    refactor test_pkg_test

--- test/test_pkg_test.py
+++ test/test_pkg_test.py
@@ -10,7 +10,7 @@ from bioconda_utils import utils
 from bioconda_utils import build
 
 
-def test_pkg_test():
+def _build_pkg():
     r = Recipes(dedent(
         """
         one:
@@ -31,29 +31,21 @@ def test_pkg_test():
         recipe=r.recipe_dirs['one'],
         recipe_folder='.',
         env=env_matrix)
+    return built_package
+
+
+def test_pkg_test():
+    """
+    Running a mulled-build test shouldn't cause any errors
+    """
+    built_package = _build_pkg()
     res = pkg_test.test_package(built_package)
 
 
-def test_pkg_test_missing_involucro():
-    r = Recipes(dedent(
-        """
-        one:
-          meta.yaml: |
-            package:
-              name: one
-              version: 0.1
-            test:
-              commands:
-                - "ls -la"
-        """), from_string=True)
-    r.write_recipes()
-    env_matrix = list(utils.EnvMatrix(tmp_env_matrix()))[0]
-    recipe = r.recipe_dirs['one']
-    built_package = utils.built_package_path(recipe)
-    ensure_missing(built_package)
-    build.build(
-        recipe=r.recipe_dirs['one'],
-        recipe_folder='.',
-        env=env_matrix)
+def test_pkg_test_mulled_build_error():
+    """
+    Make sure calling mulled-build with the wrong arg fails correctly
+    """
+    built_package = _build_pkg()
     with pytest.raises(sp.CalledProcessError):
         res = pkg_test.test_package(built_package, mulled_args='--wrong-arg')
