688cc9d52eefa0c8457656829e6a31428f7fe0a0
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-10-14 21:11:04 +0200

    bump graphmap to bring osx build back

--- recipes/graphmap/build.sh
+++ recipes/graphmap/build.sh
@@ -4,9 +4,9 @@ mkdir -p $PREFIX/bin
 
 
 cd $SRC_DIR/codebase
-wget -O seqlib.tar.gz https://github.com/isovic/seqlib/archive/6c00d87.tar.gz
+wget -O seqlib.tar.gz https://github.com/isovic/seqlib/archive/129d39a.tar.gz
 tar -xvf seqlib.tar.gz -C seqlib --strip-components 1
-wget -O argumentparser.tar.gz https://github.com/isovic/argumentparser/archive/d0dae52.tar.gz
+wget -O argumentparser.tar.gz https://github.com/isovic/argumentparser/archive/72af976.tar.gz
 tar -xvf argumentparser.tar.gz -C argumentparser --strip-components 1
 
 cd $SRC_DIR
