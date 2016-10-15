4553fdc825200a69a87a6e41a7bf256deff2e206
Thomas Cokelaer <cokelaer@gmail.com>
2016-10-15 19:38:51 +0200

    update recipes

--- recipes/dsrc/build.sh
+++ recipes/dsrc/build.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 
+yum install glibc-static -y
 
-yum install glibc-static
 sed -i -e  "s/\$(CXXFLAGS)/\$(CXXFLAGS) \$(CFLAGS)/g" src/Makefile
 sed -i -e "s/-lpthread/-lpthread -lrt/g" Makefile
 export CFLAGS=" -I${PREFIX}/include -L${PREFIX}/lib"
 
-
 export CC=${PREFIX}/bin/gcc
 export CXX=${PREFIX}/bin/g++
 
-#export INCLUDE_PATH="${PREFIX}/include"
-#export LIBRARY_PATH="${PREFIX}/lib"
-#export LD_LIBRARY_PATH="${PREFIX}/lib"
-#export BOOST_INCLUDE_DIR=${PREFIX}/include
-#export BOOST_LIBRARY_DIR=${PREFIX}/lib
-#export LIBS='-lboost_system -lboost_program_options -lboost_filesystem -lboost_timer'
-
-#export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
-#export LDFLAGS="-L${BOOST_LIBRARY_DIR} -lboost_filesystem -lboost_system"
-
-
-
 make
-make install
+make bin
+cp bin/dsrc $PREFIX/bin
+
