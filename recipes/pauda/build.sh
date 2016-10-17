ca20a2d80121b228edc58a9039e0565b8ef130ff
aymanm <ayman.yousif@gmail.com>
2016-10-17 10:27:22 +0400

    clean up build script

--- recipes/pauda/build.sh
+++ recipes/pauda/build.sh
@@ -8,7 +8,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-mkdir -p $PREFIX
+
 cp -rf pauda-1.0.1/bin $PREFIX
 cp -rf pauda-1.0.1/data $PREFIX/data
 cp -rf pauda-1.0.1/lib $PREFIX
