274c935af8633d996e2629e32d4bf1485cf2cdb0
aymanm <ayman.yousif@gmail.com>
2016-10-17 09:19:21 +0400

    cleaning up build scripts

--- recipes/tepid/build.sh
+++ recipes/tepid/build.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 set -x -e
 
-#export INCLUDE_PATH="${PREFIX}/include"
-#export LIBRARY_PATH="${PREFIX}/lib"
-#export LD_LIBRARY_PATH="${PREFIX}/lib"
-#export LDFLAGS="-L${PREFIX}/lib"
-#export CPPFLAGS="-I${PREFIX}/include"
-#pip install -r requirements.txt
-ls ${PREFIX}
 $PYTHON setup.py install
-
-
-ls ${PREFIX}
-#mkdir -p $PREFIX
