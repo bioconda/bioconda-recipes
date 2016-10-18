f8f31946665b49f15b3dbe26f1dc580b80bace29
daler <dalerr@niddk.nih.gov>
2016-10-17 21:07:14 -0400

    don't use conda-build fork

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
     docker run -e SUBDAG -e SUBDAGS -e TRAVIS_BRANCH -e TRAVIS_PULL_REQUEST -e ANACONDA_TOKEN -e BIOCONDA_UTILS_ARGS \
                -v `pwd`:/bioconda-recipes condaforge/linux-anvil /bin/bash -c \
-               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; pip install git+https://github.com/bioconda/conda-build.git@fix-build-skip-existing; set -x; bioconda-utils build recipes config.yml $BIOCONDA_UTILS_ARGS; set +x"
+               "cd /bioconda-recipes; conda install --file scripts/requirements.txt; pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG; set -x; bioconda-utils build recipes config.yml $BIOCONDA_UTILS_ARGS; set +x"
 
     if [[ $SUBDAG = 0 ]]
     then
