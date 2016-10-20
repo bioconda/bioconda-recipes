cbd8822c032c726ea386c4a35f986d2376d5b9a1
daler <dalerr@niddk.nih.gov>
2016-10-20 17:43:17 -0400

    update travis-setup

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -5,20 +5,18 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
-    # install conda
-    curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
-    
-    docker pull condaforge/linux-anvil
+    tag=Linux
 else
-    # install conda
-    curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-    sudo bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /anaconda
+    tag=MacOSX
 fi
-    
+
+# install conda
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-$tag-x86_64.sh
+sudo bash Miniconda3-latest-$tag-x86_64.sh -b -p /anaconda
+
 sudo chown -R $USER /anaconda
-mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
-mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
+mkdir -p /anaconda/conda-bld/osx-64
+mkdir -p /anaconda/conda-bld/linux-64
 export PATH=/anaconda/bin:$PATH
 conda install -y --file $SCRIPT_DIR/requirements.txt
 conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
