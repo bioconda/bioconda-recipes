2a27a98a4ad3287cc6986848632c6a5711b4b3ae
Björn Grüning <bjoern@gruenings.eu>
2016-10-21 16:40:37 +0200

    make sure ruby is compiled with zlib

--- recipes/ruby/build.sh
+++ recipes/ruby/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative --with-openssl-dir=$PREFIX CPPFLAGS="-fgnu89-inline"
+./configure --prefix=$PREFIX --disable-install-doc --with-zlib-include=$PREFIX/include --with-zlib-lib=$PREFIX/lib --enable-load-relative --with-openssl-dir=$PREFIX CPPFLAGS="-fgnu89-inline"
 make
 make install
