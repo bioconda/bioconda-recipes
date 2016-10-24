d3b2c9bb513f798a784420b7bd981fafb9038506
Wolfgang Maier <wolfgang.maier@biologie.uni-freiburg.de>
2016-10-24 14:38:24 +0200

    Use CPATH to make zlib.h discoverable

--- recipes/mimodd/build.sh
+++ recipes/mimodd/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export CPATH=${PREFIX}/include
 
 $PYTHON setup.py install
 $PYTHON -m MiModD.__first_run__
