9dd512855f0434eee441021df71af0bd98138903
daler <dalerr@niddk.nih.gov>
2016-10-24 14:30:59 -0400

    install reqs from biconda-utils requirements.txt

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -14,10 +14,8 @@ fi
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-$tag-x86_64.sh
 sudo bash Miniconda3-latest-$tag-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
-mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
-mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
 export PATH=/anaconda/bin:$PATH
-conda install -y --file $SCRIPT_DIR/requirements.txt
+conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/conda-requirements.txt
 conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
 
 # setup bioconda channel
@@ -26,4 +24,5 @@ conda config --add channels r
 conda config --add channels file://anaconda/conda-bld
 
 # setup bioconda-utils
+pip install git+https://github.com/galaxyproject/galaxy-lib.git@871c090
 pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
