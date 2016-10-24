c9b3675966084c00c97f3df5200a9d7f05a5ddd2
Björn Grüning <bjoern@gruenings.eu>
2016-10-24 09:11:03 +0200

    add LD_LIBRARY_PATH and friends

--- recipes/rnalien/build.sh
+++ recipes/rnalien/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
 stack setup
 stack build
