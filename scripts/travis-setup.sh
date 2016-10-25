737fb2e4c5673dad14e886bba5093ff377202fb3
daler <dalerr@niddk.nih.gov>
2016-10-24 21:20:16 -0400

    change order of lines

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -15,6 +15,8 @@ curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-$tag-x86_64.sh
 sudo bash Miniconda3-latest-$tag-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
+conda update -y conda
+conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
 
 # setup bioconda channel
 conda config --add channels anaconda
@@ -23,10 +25,7 @@ conda config --add channels conda-forge
 conda config --add channels bioconda
 conda config --add channels file://anaconda/conda-bld
 
-conda update -y conda
 conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/conda-requirements.txt
-conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
 
 # setup bioconda-utils
-pip install git+https://github.com/galaxyproject/galaxy-lib.git@871c090
 pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
