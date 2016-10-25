eadd7d949d6672fad52eb4e0bda3082873af9afd
molpopgen <krthornt@uci.edu>
2016-10-25 12:53:28 -0700

    docker tests show need for CPPFLAGS and LDFLAGS on Linux

--- recipes/libsequence/build.sh
+++ recipes/libsequence/build.sh
@@ -4,7 +4,7 @@ if [ `uname` == Darwin ] ; then
     MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 echo $MACOSX_DEPLOYMENT_TARGET
-./configure --prefix=$PREFIX
+CPPFLAGS="-I$PREFIX/include $CPPFLAGS" LDFLAGS="-L$PREFIX/lib $LDFLAGS" ./configure --prefix=$PREFIX
 make
 make install
 
