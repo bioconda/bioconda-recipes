f5432fe1124dab32dad3358c7819ec114ae18e25
molpopgen <krthornt@uci.edu>
2016-10-25 15:26:55 -0700

    remove OS X platform stuff

--- recipes/libsequence/build.sh
+++ recipes/libsequence/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ `uname` == Darwin ] ; then
-    MACOSX_DEPLOYMENT_TARGET=10.9
-fi
-echo $MACOSX_DEPLOYMENT_TARGET
 CPPFLAGS="-I$PREFIX/include $CPPFLAGS" LDFLAGS="-L$PREFIX/lib $LDFLAGS" ./configure --prefix=$PREFIX
 make
 make install
