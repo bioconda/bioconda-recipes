d34b81c2dd51e4057ec9e18d35b360f30c50d618
aymanm <ayman.yousif@gmail.com>
2016-10-25 12:27:18 +0400

    removed unnecesary dependencies

--- recipes/pauda/build.sh
+++ recipes/pauda/build.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 set -x -e
 
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-
 cp -rf pauda-1.0.1/bin $PREFIX
-cp -rf pauda-1.0.1/data $PREFIX/data
+cp -rf pauda-1.0.1/data $PREFIX
 cp -rf pauda-1.0.1/lib $PREFIX
-chmod +x $PREFIX/bin/pauda-run
-chmod +x $PREFIX/bin/pauda-build
+chmod a+x $PREFIX/bin/pauda-run
+chmod a+x $PREFIX/bin/pauda-build
