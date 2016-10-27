2b44292b6e5c067fb93a0cfe783ac1b204dcb007
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-10-28 00:05:59 +0200

    Fix typo

--- recipes/mcl/build.sh
+++ recipes/mcl/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kdir -p $PREFIX/bin
+mkdir -p $PREFIX/bin
 ./configure --prefix=$PREFIX --enable-blast
 make
 sed -i.bak 's|/usr/local/bin/perl|/usr/bin/env perl|' src/alien/oxygen/src/mcxdeblast
