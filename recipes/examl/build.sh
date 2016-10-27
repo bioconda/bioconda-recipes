3477370d484262bcc9a92b301806baf94926ff37
Per Unneberg <per.unneberg@scilifelab.se>
2016-10-27 21:43:01 +0200

    Strictly use anaconda build environment

--- recipes/examl/build.sh
+++ recipes/examl/build.sh
@@ -2,6 +2,10 @@
 set -x
 set -e
 
+#strictly use anaconda build environment
+CC=${PREFIX}/bin/gcc
+CXX=${PREFIX}/bin/g++
+
 cd parser && make -f Makefile.SSE3.gcc && mv parse-examl ${PREFIX}/bin
 cd ../examl && make -f Makefile.SSE3.gcc && mv examl ${PREFIX}/bin
 rm -f *.o && make -f Makefile.AVX.gcc && mv examl-AVX ${PREFIX}/bin
