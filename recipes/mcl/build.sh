63b5f9b890e486c364cb3be422f85a332a80d792
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-10-27 23:56:19 +0200

    Enable blast parser for mcl

--- recipes/mcl/build.sh
+++ recipes/mcl/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-./configure --prefix=$PREFIX
-mkdir -p $PREFIX/bin
-
+kdir -p $PREFIX/bin
+./configure --prefix=$PREFIX --enable-blast
+make
+sed -i.bak 's|/usr/local/bin/perl|/usr/bin/env perl|' src/alien/oxygen/src/mcxdeblast
+sed -i.bak 's|/usr/local/bin/perl|perl|' scripts/clxdo
 make install 
 
 
