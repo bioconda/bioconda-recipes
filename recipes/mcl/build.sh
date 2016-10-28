a2081a6ba2f95ccd4ce699f3202073e83db2ec24
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-10-28 20:50:38 +0200

    Change to use patch file

--- recipes/mcl/build.sh
+++ recipes/mcl/build.sh
@@ -2,8 +2,6 @@
 mkdir -p $PREFIX/bin
 ./configure --prefix=$PREFIX --enable-blast
 make
-sed -i.bak 's|/usr/local/bin/perl -w|/usr/bin/env perl|' src/alien/oxygen/src/mcxdeblast
-sed -i.bak 's|/usr/local/bin/perl|perl|' scripts/clxdo
 make install 
 
 
