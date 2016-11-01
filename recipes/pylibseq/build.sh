e684464a0c28ad1479808aaa30399908aa888131
molpopgen <krthornt@uci.edu>
2016-11-01 13:29:10 -0700

    various fixes

--- recipes/pylibseq/build.sh
+++ recipes/pylibseq/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-export CFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib -ltbb"
 #On OS X, we have to force Python 
 #packages to compile with GCC.
 #Conda setup will default to enforcing 
 #clang o/w.
 CC=gcc CXX=g++ $PYTHON setup.py install
-#Run the unit tests:
-python -m unittest discover unit_tests
