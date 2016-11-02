372f0304c5831edee8a743a0e6f56450e7c0e4e8
Peter van Heusden <pvh@sanbi.ac.za>
2016-11-02 11:38:14 +0200

    * Remove reference to boost since source includes boost source * Fix error in build script

--- recipes/gvcftools/build.sh
+++ recipes/gvcftools/build.sh
@@ -1,4 +1,4 @@
 sed -i.bak 's^TABIX_LIB_DIR)$^TABIX_LIB_DIR) -L$(PREFIX)/lib^' src/Makefile
-sed -i.bak 's^TABIX_INCLUDE_DIR)$^TABIX_INCLUDE_DIR) -L$(PREFIX)/include/^ src/Makefile
+sed -i.bak 's^TABIX_INCLUDE_DIR)$^TABIX_INCLUDE_DIR) -L$(PREFIX)/include/^' src/Makefile
 
 make BIN_DIR=$PREFIX/bin
