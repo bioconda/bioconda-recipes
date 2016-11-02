a52c7d3ab713313f561e536e2592cc8e247730f9
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-11-02 22:30:49 +0100

    Fix typo and sed for osx

--- recipes/autoconf/build.sh
+++ recipes/autoconf/build.sh
@@ -3,11 +3,11 @@
 ./configure --prefix=$PREFIX
 make
 
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' bin/autom4te
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoheader
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoreconf
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' bin/ifnames
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoscan
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoupdate
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/autom4te
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoreconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/ifnames
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoscan
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/autoupdate
 
 make install
