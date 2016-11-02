67571f264ed042c2eada3d30c78c2e75961afb13
Peter van Heusden <pvh@sanbi.ac.za>
2016-11-02 17:46:34 +0200

    Replace sed command for tabix

--- recipes/gvcftools/build.sh
+++ recipes/gvcftools/build.sh
@@ -1,5 +1,5 @@
 sed -i.bak 's^TABIX_LIB_DIR)$^TABIX_LIB_DIR) -L$(PREFIX)/lib^' src/Makefile
 sed -i.bak 's^TABIX_INCLUDE_DIR)$^TABIX_INCLUDE_DIR) -L$(PREFIX)/include^' src/Makefile
-sed -i.bak 's@^CFLAGS =@CFLAGS = -I$(PREFIX)/include@' src/redist/tabix/Makefile
+sed -r "s^tabix: (.*)^tabix: \1\n\tsed -i.bak '@CFLAGS =@CFLAGS = -I$\(PREFIX\)/include@ tabix/Makefile'^" redist/Makefile
 
 make BIN_DIR=$PREFIX/bin
