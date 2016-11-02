af4eb486df9ec2add34a4566c947297c9c18d48b
Peter van Heusden <pvh@sanbi.ac.za>
2016-11-02 19:14:19 +0200

    Test with listing

--- recipes/gvcftools/build.sh
+++ recipes/gvcftools/build.sh
@@ -2,4 +2,9 @@ sed -i.bak 's^TABIX_LIB_DIR)$^TABIX_LIB_DIR) -L$(PREFIX)/lib^' src/Makefile
 sed -i.bak 's^TABIX_INCLUDE_DIR)$^TABIX_INCLUDE_DIR) -L$(PREFIX)/include^' src/Makefile
 sed -r -i.bak "s^tabix: (.*)^tabix: \1\n\tsed -i.bak 's@CFLAGS =@CFLAGS = -I$\(PREFIX\)/include@' tabix*/Makefile^" redist/Makefile
 
+echo "LISTING include"
+ls $PREFIX/include
+echo "LISTING lib"
+ls $PREFIX/lib
+
 make BIN_DIR=$PREFIX/bin
