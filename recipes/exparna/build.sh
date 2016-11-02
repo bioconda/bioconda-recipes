b2875d49ebc395984001f360f4581269649af667
Martin Mann <martin-mann@users.noreply.github.com>
2016-11-02 21:31:40 +0100

    Automake fix added, debug out removed

--- recipes/exparna/build.sh
+++ recipes/exparna/build.sh
@@ -1,19 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+# fix automake
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
 
 # fix autoconf
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
+sed -i '' '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
 
-# temp debug
-cat autotools-init.sh
 # create configure file
-sh autotools-init.sh
+bash ./autotools-init.sh
+
 # run configuration
 ./configure --prefix=$PREFIX --with-RNA=$PREFIX
+
 # compile and install
 make clean
 make
