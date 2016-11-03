0ed133275e1da333ecc6d62deb6f17d17b2ede24
blankclemens <blankclemens@gmail.com>
2016-11-03 13:53:00 +0100

    Fixed typo.

--- recipes/sga/build.sh
+++ recipes/sga/build.sh
@@ -6,4 +6,4 @@ pushd $SRC_DIR/src
 export CPATH=${PREFIX}/include
 
 ./autogen.sh
-./configure --prefix=$PREFIX --with-jemalloc=$PREFIX/lib --with-sparsehash=$PREFIX --with-bamtools=$PREFIX  && make && make install
+./configure --prefix=$PREFIX --with-jemalloc=$PREFIX/lib --with-sparsehash=$PREFIX --with-bamtools=$PREFIX && make && make install
