de1ab461eb3751a6a2481ea58761373236539d5a
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-11-04 11:17:29 -0700

    Add GCC and LLVM to meta.yaml for freebayes, specific C++11

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -12,6 +12,7 @@ export CFLAGS="-I$PREFIX/include"
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+
 mkdir -p bamtools/build
 cd bamtools/build
 cmake ..
@@ -19,7 +20,7 @@ cd ../..
 
 cd src
 make autoversion
-make CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
+make CPPFLAGS="-I$PREFIX/include -std=c++11 -stdlib=libc++" LDFLAGS="-L$PREFIX/lib"
 cd ..
 
 pythonfiles="scripts/fasta_generate_regions.py scripts/coverage_to_regions.py"
