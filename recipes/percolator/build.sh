15e566f51e271c19f62db2816f8a3dd839ed1202
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-11-07 22:46:39 +0100

    Some formatting was not conda-compliant

--- recipes/percolator/build.sh
+++ recipes/percolator/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# works with xerces-c 3.1.2/xsd and boost 1.60, not with xerces-x 3.1.4
-# export LD_LIBRARY_PATH=/opt/conda/lib # necessary to actually run!
-cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DXML_SUPPORT=ON -DCMAKE_PREFIX_PATH="$PREFIX;$PREFIX/lib" .
+mkdir percobuild && cd percobuild
+cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DXML_SUPPORT=ON -DCMAKE_PREFIX_PATH="$PREFIX;$PREFIX/lib" $SRC_DIR
 make && make install
+cd ..
 
-cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX -DBOOST_LIBRARYDIR=$PREFIX/lib -DSERIALIZE="TokyoCabinet" -DCMAKE_PREFIX_PATH=$PREFIX ./src/converters
+mkdir converterbuild && cd converterbuild 
+cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX -DBOOST_LIBRARYDIR=$PREFIX/lib -DSERIALIZE="TokyoCabinet" -DCMAKE_PREFIX_PATH=$PREFIX $SRC_DIR/src/converters
 make && make install
