8608a4ff51b92c6105d3ef573e14bc9fa2fff094
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-11-07 21:43:46 +0100

    This works when doing it by hand, now lets see the travis test

--- recipes/percolator/build.sh
+++ recipes/percolator/build.sh
@@ -1,2 +1,9 @@
-cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DXML_SUPPORT=ON -DBOOST_ROOT=$PREFIX -DBOOST_LIBRARYDIR=$PREFIX/lib -DCMAKE_PREFIX_PATH="$PREFIX;$PREFIX/lib" .
-make install
+#!/bin/bash
+
+# works with xerces-c 3.1.2/xsd and boost 1.60, not with xerces-x 3.1.4
+# export LD_LIBRARY_PATH=/opt/conda/lib # necessary to actually run!
+cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DXML_SUPPORT=ON -DCMAKE_PREFIX_PATH="$PREFIX;$PREFIX/lib" .
+make && make install
+
+cmake -DTARGET_ARCH=x86_64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX -DBOOST_LIBRARYDIR=$PREFIX/lib -DSERIALIZE="TokyoCabinet" -DCMAKE_PREFIX_PATH=$PREFIX ./src/converters
+make && make install
