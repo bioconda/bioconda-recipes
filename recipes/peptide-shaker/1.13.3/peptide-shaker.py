dcf9c98869739f66a2747c29ca71818e6915a3f2
James Johnson <jj@javanica.msi.umn.edu>
2016-11-09 16:18:41 -0600

    Add peptide-shaker versions: 1.11.0, 1.13.3, 1.13.6

--- recipes/peptide-shaker/1.13.3/peptide-shaker.py
+++ recipes/peptide-shaker/1.13.3/peptide-shaker.py
@@ -12,7 +12,7 @@ import sys
 from os import access
 from os import getenv
 from os import X_OK
-jar_file = 'PeptideShaker-1.13.6.jar'
+jar_file = 'PeptideShaker-1.13.3.jar'
 
 default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
 
