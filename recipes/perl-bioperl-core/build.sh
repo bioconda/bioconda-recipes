f2ed04f6950bbb5062be2adf791035bcdf8e25d2
Martin Mann <martin-mann@users.noreply.github.com>
2016-11-09 21:20:26 +0100

    bioperl build.sh : tee removed for better debug

--- recipes/perl-bioperl-core/build.sh
+++ recipes/perl-bioperl-core/build.sh
@@ -10,17 +10,21 @@ export PERL_MM_USE_DEFAULT=1
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
-    perl Build.PL 2>&1 | tee configure.log
+#    perl Build.PL 2>&1 | tee configure.log
+    perl Build.PL 
     ./Build
-    ./Build test 2>&1 | tee tests.log
+#    ./Build test 2>&1 | tee tests.log
+    ./Build test 
     # Make sure this goes in site
     ./Build install --installdirs site
     ./Build install --install_path script=site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site 2>&1 | tee configure.log
+#    perl Makefile.PL INSTALLDIRS=site 2>&1 | tee configure.log
+    perl Makefile.PL INSTALLDIRS=site
     make
-    make test 2>&1 | tee tests.log
+#    make test 2>&1 | tee tests.log
+    make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
