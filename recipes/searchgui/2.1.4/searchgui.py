4391bfeda0a745bf3c63ef723827dc02578a632a
James Johnson <jj@javanica.msi.umn.edu>
2016-11-09 16:12:28 -0600

    Add searchgui versions: 2.14, 2.9.0, 3.1.4

--- recipes/searchgui/2.1.4/searchgui.py
+++ recipes/searchgui/2.1.4/searchgui.py
@@ -12,7 +12,7 @@ import sys
 from os import access
 from os import getenv
 from os import X_OK
-jar_file = 'SearchGUI-3.1.4.jar'
+jar_file = 'SearchGUI-2.1.4.jar'
 
 default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
 
