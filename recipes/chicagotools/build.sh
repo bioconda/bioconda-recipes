4255a73c3876f76279244cce9e058222579fcfa7
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2016-11-09 10:31:40 +0100

    Copy now only needed files

--- recipes/chicagotools/build.sh
+++ recipes/chicagotools/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-chmod +x *
-cp -R * ${PREFIX}/bin
+cd chicagoTools
+cp {bam2chicago.sh,fitDistCurve.R,makeDesignFiles.py,makeNBaitsPerBinFile.py,makeNPerBinFile.py,makePeakMatrix.R,makeProxOEFile.py,runChicago.R} ${PREFIX}/bin
+chmod +x {bam2chicago.sh,fitDistCurve.R,makeDesignFiles.py,makeNBaitsPerBinFile.py,makeNPerBinFile.py,makePeakMatrix.R,makeProxOEFile.py,runChicago.R}
+cd ..
+rm -rf chicago
