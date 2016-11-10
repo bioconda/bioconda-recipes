f6847f58147b1363a781732aee57b3cd407fc221
daler <dalerr@niddk.nih.gov>
2016-11-10 16:30:10 -0500

    use new simulate-travis for setting up

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -17,21 +17,8 @@ sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
 conda update -y conda
 
-# channels are added from lowest to highest priority
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
-
-
-conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/conda-requirements.txt
+../simulate-travis.py --set-channel-order
+../simulate-travis.py --install-requirements
 
 conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
 conda config --add channels file://anaconda/conda-bld
-conda config --get
-
-# setup bioconda-utils
-pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
-
-git fetch origin master
-git show FETCH_HEAD:config.yml
