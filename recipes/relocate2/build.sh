4e5f3bebef346813401a9146e52661b84e0abd20
JinfengChen <jinfeng7chen@gmail.com>
2016-11-10 14:34:32 -0800

    Update build.sh

--- recipes/relocate2/build.sh
+++ recipes/relocate2/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir $PREFIX/scripts
-cp ./scripts/*.p* $PREFIX/scripts
-cp ./scripts/*.p* $PREFIX/bin
-cp ./README.md $PREFIX
-cp ./test_data.tar.gz $PREFIX
+mkdir -p $PREFIX/scripts
+mkdir -p $PREFIX/bin
+cp $SRC_DIR/scripts/*.p* $PREFIX/scripts
+cp $SRC_DIR/scripts/*.p* $PREFIX/bin
+cp $SRC_DIR/scripts/*.p* $PREFIX
+cp $SRC_DIR/README.md $PREFIX
+#cp ./test_data.tar.gz $PREFIX
 echo "bwa=$PREFIX/bin/bwa" >> $PREFIX/CONFIG
 echo "bowtie2=$PREFIX/bin/bowtie2" >> $PREFIX/CONFIG
 echo "bowtie2_build=$PREFIX/bin/bowtie2-build" >> $PREFIX/CONFIG
