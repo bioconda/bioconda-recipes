03b5655122c96f65b3199428a45525b6c4f133e7
daler <dalerr@niddk.nih.gov>
2016-11-10 18:14:35 -0500

    clean up setup

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -16,8 +16,7 @@ sudo bash Miniconda3-latest-$tag-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
 conda update -y conda
-python -c 'import sys; print("\n".join(sys.path))'
-ls /anaconda/lib/python3.5/site-packages
+
 $SCRIPT_DIR/../simulate-travis.py --set-channel-order
 $SCRIPT_DIR/../simulate-travis.py --install-requirements
 
