1e851b39271626caaf7a71b6bcbee55aac4a81c1
martin-mann <qyu@gmx.de>
2016-11-10 10:54:45 +0100

    perl-gd : build : extra compiler flag

--- recipes/perl-gd/build.sh
+++ recipes/perl-gd/build.sh
@@ -2,7 +2,7 @@
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
-    perl Build.PL
+    perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
     ./Build
     ./Build test
     # Make sure this goes in site
