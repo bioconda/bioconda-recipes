8c1930772078c4003dc8de6041ee223155f0fd2a
Martin Mann <martin-mann@users.noreply.github.com>
2016-11-14 18:42:40 +0100

    mysqlclient : build: debug output removed

--- recipes/mysqlclient/build.sh
+++ recipes/mysqlclient/build.sh
@@ -4,5 +4,3 @@ export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:/usr/local/mysql/lib:$DYLD_FALL
 
 $PYTHON setup.py install
 
-#debug output if possible
-hash otool 2>/dev/null && otool -L /anaconda/conda-bld/_t_env/lib/python3.4/site-packages/_mysql.so
