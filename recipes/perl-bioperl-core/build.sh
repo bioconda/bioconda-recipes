c4cd6081f1270956aa490208ef54022aee938583
Martin Mann <martin-mann@users.noreply.github.com>
2016-11-14 13:19:48 +0100

    perl-bioperl-code : build : GD test reenabled

--- recipes/perl-bioperl-core/build.sh
+++ recipes/perl-bioperl-core/build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # Make sure pipes to tee don't hide configuration or test failures
-#set -o pipefail
+set -o pipefail
 
 # Accept defaults so we don't get prompted about install options; resuls in:
 #   * building all BioPerl scripts
 #   * skipping test that require network access
 export PERL_MM_USE_DEFAULT=1
 
+echo "##++ PREFIX = $PREFIX";
+
 # ensure perl-gd is loaded properly or die trying
-#perl -e 'die "GD module missing!" unless(eval{require GD});';
+echo "##++ testing for perl GD library";
+perl -e 'use GD;';
+echo "##++ exist status for perl GD test = $?";
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
+    echo "##++ running Build.PL"
 #    perl Build.PL 2>&1 | tee configure.log
     perl Build.PL 
     ./Build
@@ -22,6 +27,7 @@ if [ -f Build.PL ]; then
     ./Build install --installdirs site
 #    ./Build install --install_path script=site
 elif [ -f Makefile.PL ]; then
+    echo "##++ running Makefile.PL"
     # Make sure this goes in site
 #    perl Makefile.PL INSTALLDIRS=site 2>&1 | tee configure.log
     perl Makefile.PL INSTALLDIRS=site
@@ -33,3 +39,6 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
+
+echo "##++ ls PREFIX"
+ls -l $PREFIX
