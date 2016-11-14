0a094d5ff06d62edfed95f3593d2b5574d6c79f8
Martin Mann <martin-mann@users.noreply.github.com>
2016-11-14 17:59:28 +0100

    mysqlclient : debug output

--- recipes/mysqlclient/build.sh
+++ recipes/mysqlclient/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:/usr/local/mysql/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
 $PYTHON setup.py install
+
+#debug output if possible
+hash otool 2>/dev/null && otool -L /anaconda/conda-bld/_t_env/lib/python3.4/site-packages/_mysql.so
