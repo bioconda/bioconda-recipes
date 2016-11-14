3870524c15bae6355b278a4fdb585dee13b4a9bb
Björn Grüning <bjoern@gruenings.eu>
2016-11-14 22:52:37 +0100

    Cleanups

--- recipes/mysql-connector-c/build.sh
+++ recipes/mysql-connector-c/build.sh
@@ -2,17 +2,6 @@
 
 set -o pipefail
 
-#mkdir -p $PREFIX/bin
-#mkdir -p $PREFIX/lib
-#mkdir -p $PREFIX/include
-
-#rsync -a bin/ $PREFIX/bin
-#rsync -a lib/ $PREFIX/lib
-#rsync -a include/ $PREFIX/include
-
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PREFIX
-
 make
-
 make install
-
