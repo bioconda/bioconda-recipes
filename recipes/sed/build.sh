274942d836e355b90057d1260897c0a6d23cc60f
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2016-11-18 14:17:33 +0000

    Add missing build requirements for sed

--- recipes/sed/build.sh
+++ recipes/sed/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i.bak -e '/^rsync .*/d' bootstrap.conf # bootstrap will fall back to wget
 ./bootstrap # This is needed only when the sed sources comes from the Git repo
 ./configure --prefix=$PREFIX
 sed -i.bak -e 's/ -Wmissing-include-dirs//' Makefile
