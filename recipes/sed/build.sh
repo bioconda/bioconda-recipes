c9d9cfdc0770079de8a893b36d8c2c5e0ceaba67
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2016-11-18 10:14:26 +0000

    Use sed from git
    Sandboxing patch has been upstreamed.

--- recipes/sed/build.sh
+++ recipes/sed/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+./bootstrap # This is needed only when the sed sources comes from the Git repo
 ./configure --prefix=$PREFIX
+sed -i.bak -e 's/ -Wmissing-include-dirs//' Makefile
 make
 make install
