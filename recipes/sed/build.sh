bb6d71b9895db3da4d1e82bba4e482c4c3fd05a2
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2016-11-18 19:43:27 +0000

    Fix shebang in autoconf and automake Perl scripts

--- recipes/sed/build.sh
+++ recipes/sed/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# fix autoconf
+sed -i.bak -e '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader \
+    $PREFIX/bin/autom4te $PREFIX/bin/autoreconf $PREFIX/bin/autoscan \
+    $PREFIX/bin/autoupdate $PREFIX/bin/ifnames
+# fix automake
+sed -i.bak -e '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal* \
+    $PREFIX/bin/automake*
+
 sed -i.bak -e '/^rsync .*/d' bootstrap.conf # bootstrap will fall back to wget
 ./bootstrap # This is needed only when the sed sources comes from the Git repo
 ./configure --prefix=$PREFIX
