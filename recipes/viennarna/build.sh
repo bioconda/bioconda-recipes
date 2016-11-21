bec7980faa58de6c898a9633ae144b9d3284a81d
Sebastian Will <swill@csail.mit.edu>
2016-11-21 17:17:47 +0100

    Update ViennaRNA: build Python 2 and 3 bindings

--- recipes/viennarna/build.sh
+++ recipes/viennarna/build.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-# ViennaRNA does not support Python 3 for MacOSX.
+#!/bin/bash
 
-## choose extra configure options depending on the operating system
+## Choose extra configure options depending on the operating system
 ## (mac or linux)
 ##
 if [ `uname` == Darwin ] ; then
@@ -10,14 +9,46 @@ if [ `uname` == Darwin ] ; then
                           --without-python3
                           LDFLAGS=-Wl,-headerpad_max_install_names"
 else ## linux
-    extra_config_options="--with-python3"
+    extra_config_options="--with-python3 --with-python"
+
+    ############################################################
+    ## Vienna requires Python 2 and 3 at run time to generate Swig
+    ## bindings for both. Since conda does not allow installation of both
+    ## python versions in the same environment, we perform this
+    ## workaround: install Python 2 in a different environment and set the
+    ## path accordingly.
+    ##
+    
+    ## install python 2.7 in separate environment
+    conda create -y -n python2 'python<3'
+    
+    ## get path to python2 environment
+    source activate python2
+    PYTHON2_ENV=${CONDA_PREFIX}
+    source deactivate
+    
+    ## Vienna rna expects python-config executable as python2-config,
+    ## unfortunately the python 2.7 conda package does not install it.
+    ## Workaround: set symbolic link
+    ln -sf $PYTHON2_ENV/bin/python-config $PYTHON2_ENV/bin/python2-config
+    
+    ## Add python2 env to search path (don't use activate, since we want
+    ## to stay in the build environment)
+    export PATH=$PYTHON2_ENV/bin:$PATH
+    
+    ##
+    ############################################################
+
 fi
 
+## Configure and make
 ./configure --prefix=$PREFIX \
-            --without-perl --without-python \
+            --without-perl \
             --with-kinwalker \
             --disable-lto \
             --without-doc \
-            ${extra_config_options} && \
-make -j${CPU_COUNT} && \
+            ${extra_config_options} &&\
+make -j${CPU_COUNT}
+
+## Install
 make install
