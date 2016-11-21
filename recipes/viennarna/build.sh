98eb0b2e613899bcd25f157b951ec08d8f9242d5
Sebastian Will <swill@csail.mit.edu>
2016-11-21 19:23:10 +0100

    fixes

--- recipes/viennarna/build.sh
+++ recipes/viennarna/build.sh
@@ -23,14 +23,18 @@ else ## linux
     conda create -y -n python2 'python<3'
     
     ## get path to python2 environment
+    MY_PATH_BACKUP=${PATH}         # backup path
     source activate python2
     PYTHON2_ENV=${CONDA_PREFIX}
     source deactivate
-    
+    export PATH=${MY_PATH_BACKUP}  # restore path
+
     ## Vienna rna expects python-config executable as python2-config,
-    ## unfortunately the python 2.7 conda package does not install it.
+    ## unfortunately some python 2.7 conda packages do not install it.
     ## Workaround: set symbolic link
-    ln -sf $PYTHON2_ENV/bin/python-config $PYTHON2_ENV/bin/python2-config
+    if [ ! -e $PYTHON2_ENV/bin/python2-config ]; then
+      ln -sf $PYTHON2_ENV/bin/python-config $PYTHON2_ENV/bin/python2-config
+    fi
     
     ## Add python2 env to search path (don't use activate, since we want
     ## to stay in the build environment)
