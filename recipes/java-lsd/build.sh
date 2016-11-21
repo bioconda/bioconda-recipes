4335e9753f3dc04a48a0080304d835e16e31cb23
Keith Suderman <suderman@cs.vassar.edu>
2016-11-21 12:08:52 -0500

    Fixed paths and she-bang in build.sh

--- recipes/java-lsd/build.sh
+++ recipes/java-lsd/build.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 TARGET="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
+BIN=${PREFIX}/bin
 [ -d "$TARGET" ] || mkdir -p "$TARGET"
-[ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
+[ -d "$BIN" ] || mkdir -p "$BIN"
 
 cd "${SRC_DIR}"
 mv lsd-$PKG_VERSION.jar ${TARGET}
 
 # Generate the lsd script used to invoke the java program
-cat << EOF > ${PREFIX}/bin/lsd
+cat << EOF > ${TARGET}/lsd
 #/bin/bash
 java -jar -Xmx2G ${TARGET}/lsd-${PKG_VERSION}.jar \$@
 EOF
 
-# Make it executable.
-chmod a+x ${PREFIX}/bin/lsd
+# Create a link to the startup script.
+ln -s ${TARGET}/lsd ${BIN}/lsd
+chmod 0755 ${BIN}/lsd
 
