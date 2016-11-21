afbda106fb1b3aa0ad878631f3472e0ac2de45d8
Keith Suderman <suderman@cs.vassar.edu>
2016-11-21 16:42:17 -0500

    build.sh generates wrapper to handle symlinks

--- recipes/java-lsd/build.sh
+++ recipes/java-lsd/build.sh
@@ -11,7 +11,21 @@ mv lsd-$PKG_VERSION.jar ${TARGET}
 # Generate the lsd script used to invoke the java program
 cat << EOF > ${TARGET}/lsd
 #/usr/bin/env bash
-java -jar -Xmx2G ${TARGET}/lsd-${PKG_VERSION}.jar \$@
+
+# Find original directory of bash script, resovling symlinks
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128
+SOURCE="\${BASH_SOURCE[0]}"
+while [ -h "\$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="\$( cd -P "\$( dirname "\$SOURCE" )" && pwd )"
+    SOURCE="\$(readlink "\$SOURCE")"
+    [[ \$SOURCE != /* ]] && SOURCE="\$DIR/\$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="\$( cd -P "\$( dirname "\$SOURCE" )" && pwd )"
+
+JAR=\${DIR}/lsd-${PKG_VERSION}.jar
+HEAP_SIZE=-Xmx4G
+
+java -jar \${HEAP_SIZE} \${JAR} \$@
 EOF
 
 # Create a link to the startup script.
