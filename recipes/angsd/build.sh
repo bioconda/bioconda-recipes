d903361e3284767a8afbb5883d58b445caa21d75
Per Unneberg <per.unneberg@scilifelab.se>
2016-11-23 20:51:01 +0100

    Add angsd recipe

---
+++ recipes/angsd/build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+binaries="\
+misc/msToGlf \
+misc/contamination \
+misc/contamination2 \
+misc/splitgl \
+misc/thetaStat \
+misc/printIcounts \
+misc/NGSadmix \
+misc/realSFS \
+misc/smartCount \
+misc/supersim \
+angsd \
+"
+BINDIR=$PREFIX/bin
+mkdir -p $BINDIR
+
+
+make LDFLAGS="-L=${PREFIX}/lib" INC="-I=${PREFIX}/include"
+
+for i in $binaries; do cp $i $BINDIR && chmod +x $BINDIR/$(basename $i); done
