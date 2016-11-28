5a757ea91d31d5ed53664e8c0acc8cf5855660de
zwanli <zaherwanli@gmail.com>
2016-11-28 20:29:22 +0100

    added RNASNPPATH env var. fixed missing distParam folder

--- recipes/rnasnp/build.sh
+++ recipes/rnasnp/build.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
 ./configure --prefix=$PREFIX
 make
 make install
+cp lib/distParam $LD_$PREFIX/lib -r 
+
+#Add RNASNPPATH to the enviroment variables
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/gdal-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
+## END BUILD
