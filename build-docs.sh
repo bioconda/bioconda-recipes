49be8af72af9687198b75df512881141d7d980a9
daler <dalerr@niddk.nih.gov>
2016-11-28 10:36:49 -0500

    add -d

--- build-docs.sh
+++ build-docs.sh
@@ -43,7 +43,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $ENCRYPTED_FILE -out key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $ENCRYPTED_FILE -out key -d
 chmod 600 key
 eval `ssh-agent -s`
 ssh-add key
