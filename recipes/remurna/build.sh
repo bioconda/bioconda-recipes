536a406ce837d668683c1de6add985f38fa281e3
zwanli <zaherwanli@gmail.com>
2016-11-30 14:36:45 +0100

    remurna updates

--- recipes/remurna/build.sh
+++ recipes/remurna/build.sh
@@ -5,4 +5,5 @@ make
 #Copy binery file since there is no INSTALL part in the makefile
 cp remuRNA $PREFIX/bin
 #Copy data folder. not sure if putting this folder in the main conda folder is "wise", but this tool won't work otherwise
-cp data/ $PREFIX -r
+cp data/ $PREFIX/lib -r
+ln -s $PREFIX/lib/data $PREFIX/bin/data
