6954fae48f2767a57a2a7b94ef395680b6d1a8aa
zwanli <zaherwanli@gmail.com>
2016-11-30 14:22:38 +0100

    rnasnp updates

--- recipes/rnasnp/build.sh
+++ recipes/rnasnp/build.sh
@@ -10,6 +10,6 @@ DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
 mkdir -p $ACTIVATE_DIR
 mkdir -p $DEACTIVATE_DIR
 
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/gdal-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/rnasnp-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/rnasnp-deactivate.sh
 ## END BUILD
