79f7d8b452ba0f524a4ca466a79fa60cb1c0d1e3
Kévin Gravouil <k.gravouil@gmail.com>
2016-12-02 18:13:15 +0100

    clean build.sh

--- recipes/latentstrainanalysis/build.sh
+++ recipes/latentstrainanalysis/build.sh
@@ -3,8 +3,6 @@ cp -r ./* $PREFIX/LatentStrainAnalysis/
 rm $PREFIX/LatentStrainAnalysis/testData.tar.gz
 chmod +x $PREFIX/LatentStrainAnalysis/*.sh
 
-ls -lah $PREFIX/LatentStrainAnalysis/
-
 ln -s $PREFIX/LatentStrainAnalysis/KmerSVDClustering.sh $PREFIX/bin/
 ln -s $PREFIX/LatentStrainAnalysis/ReadPartitioning.sh $PREFIX/bin/
 ln -s $PREFIX/LatentStrainAnalysis/HashCounting.sh $PREFIX/bin/
