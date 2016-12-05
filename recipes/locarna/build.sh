d3848fe3a773a3288063974d07e5f78b7acdfc11
Sebastian Will <swill@csail.mit.edu>
2016-12-05 13:14:19 +0100

    remove old workaround, which does not work anymore

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if [ $(uname) == Linux ] ; then
-    ## libstdc++ is expected in lib64, so link it there
-    if [ ! -d $PREFIX/lib64 ] ; then mkdir $PREFIX/lib64 ; fi
-    ln -s $PREFIX/lib/libstdc++.la $PREFIX/lib64/libstdc++.la 
-    ln -s $PREFIX/lib/libstdc++.so $PREFIX/lib64/libstdc++.so 
-fi
-
 ./configure --prefix=$PREFIX --with-vrna=$PREFIX "${extra_config_options}" && \
 make -j ${CPU_COUNT} && \
 make install
