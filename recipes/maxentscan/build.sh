741552635289df0b7dfd2c34ff872a4ca2d3f2bd
Matthias De Smet <matthias.desmet@ugent.be>
2016-12-06 11:49:13 +0100

    fixed bin dir pollution in build script, heavily influenced by vep recipe

--- recipes/maxentscan/build.sh
+++ recipes/maxentscan/build.sh
@@ -1,5 +1,15 @@
+target=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $target
 mkdir -p $PREFIX/bin
-cp score3.pl $PREFIX/bin/maxentscan_score3.pl
-cp score5.pl $PREFIX/bin/maxentscan_score5.pl
-cp -r splicemodels $PREFIX/bin
-chmod 0755 $PREFIX/*
+
+#change hardcoded directory of splicemodels
+sed -i.bak "s@\"splicemodels/\"@\"$target/splicemodels/\"@" *.pl
+
+cp score3.pl $target/maxentscan_score3.pl
+cp score5.pl $target/maxentscan_score5.pl
+cp -r splicemodels $target
+
+chmod 0755 $target/*.pl
+ln -s $target/*.pl $PREFIX/bin
+
+
