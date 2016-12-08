d13828e29338e6c67a25d25b8cc717887ac24c5b
mictadlo <mictadlo@gmail.com>
2016-12-08 12:39:44 +1000

    PEP_Scaffolder a new recipe

--- recipes/pep_scaffolder/build.sh
+++ recipes/pep_scaffolder/build.sh
@@ -5,4 +5,5 @@ mkdir -p $pep_scaffolder
 cp -r ./* $pep_scaffolder
 chmod +x $pep_scaffolder/pep_scaffolder
 mkdir -p $PREFIX/bin
-ln -s $pep_scaffolder/pep_scaffolder $PREFIX/bin/pep_scaffolder 
+ln -s $pep_scaffolder/pep_scaffolder $PREFIX/bin/pep_scaffolder
+ 
