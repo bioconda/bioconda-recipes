488903d2d0588225120b3d255601f93ecfdff9f5
Michał T. Lorenc <mictadlo@gmail.com>
2016-12-13 11:46:05 +1000

    Update build.sh

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
+		
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM		
+mkdir -p $outdir		
+mkdir -p $PREFIX/bin		
+cd $SRC_DIR		
+$SRC_DIR
 export DEST=$PREFIX
 ./install.sh
