cff271496307f690fff0e7f1443ad1a4c96fa684
Milad Miladi <miladim@cs.uni-freiburg.de>
2016-12-14 21:00:32 +0100

    Fixing tests for 5 wrappers

--- recipes/graphclust-wrappers/build.sh
+++ recipes/graphclust-wrappers/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #Copy binery file since there is no INSTALL part in the makefile
-cp galaxy_wrappers/*/* $PREFIX/bin/
+chmod +x galaxy_wrappers/*/*.pl
+cp galaxy_wrappers/*/*.pl $PREFIX/bin/
 
