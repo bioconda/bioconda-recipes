1909228af67a7ac498795925d9d7fe73d0b7dd99
hassanamr <hassan.amr89@gmail.com>
2016-12-15 10:39:32 +0100

    modified patch, and now copying files instead of using setup.py

--- recipes/reago/build.sh
+++ recipes/reago/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cp $RECIPE_DIR/setup.py $SRC_DIR/setup.py
-cp $RECIPE_DIR/MANIFEST.in $SRC_DIR/MANIFEST.in
-$PYTHON setup.py install
+mkdir -p $PREFIX/bin/cm
+cp reago.py $PREFIX/bin
+cp filter_input.py $PREFIX/bin
+cp -r cm/* $PREFIX/bin/cm
+
+chmod +x $PREFIX/bin/reago.py
+chmod +x $PREFIX/bin/filter_input.py
