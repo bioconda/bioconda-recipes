158b7a51b75979c58e267f4f16e782f1f7a197dc
mictadlo <mictadlo@gmail.com>
2016-12-17 21:11:58 +1000

    AssemblyUtility new recipes new try

--- recipes/assemblyutility/build.sh
+++ recipes/assemblyutility/build.sh
@@ -3,5 +3,6 @@
 mkdir -p $PREFIX/bin; 
 g++ -O3 -o AssemblyStatistics AssemblyStatistics.cpp; 
 mv AssemblyStatistics $PREFIX/bin
+
 g++ -O3 -o SelectLongestReads SelectLongestReads.cpp;
 mv SelectLongestReads $PREFIX/bin
