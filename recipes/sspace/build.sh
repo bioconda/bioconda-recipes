609b90150e95121bf31f8b37d946464714548b3a
Michal Lorenc <lorencm@sef-eeb-123137.qut.edu.au>
2016-12-22 08:16:57 +1000

    perl module mv to opt

--- recipes/sspace/build.sh
+++ recipes/sspace/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin/
-mv * $PREFIX/bin
-ln -s $PREFIX/bin/SSPACE_Standard_v3.0.pl $PREFIX/bin/SSPACE
-cd $PREFIX/bin/dotlib/
+mkdir -p $PREFIX/opt/sspace
+mv ./* $PREFIX/opt/sspace
+cd $PREFIX/opt/sspace
+ln -s $PREFIX/opt/sspace/SSPACE_Standard_v3.0.pl $PREFIX/bin/SSPACE_Standard_v3.0.pl
+ln -s $PREFIX/opt/sspace/SSPACE_Standard_v3.0.pl $PREFIX/bin/SSPACE
+
+cd $PREFIX/opt/sspace/dotlib/
 wget -c http://cpansearch.perl.org/src/GBARR/perl5.005_03/lib/getopts.pl
 
