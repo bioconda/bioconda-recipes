ddadb8349fd08925cc74ad146e8a85eb2a13bf41
Bérénice Batut <bebatut@gmail.com>
2016-12-22 11:22:05 +0100

    Add automake and autoconf command and variable commands

--- recipes/idba/build.sh
+++ recipes/idba/build.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+aclocal
+autoconf
+automake --add-missing
 
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
-
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-./configure
+./configure --prefix=$PREFIX
 make
 
 
