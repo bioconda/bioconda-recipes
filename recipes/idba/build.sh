4116b676211fa07f0d909a59141895124d2d7c83
Bérénice Batut <bebatut@gmail.com>
2016-12-22 15:13:21 +0100

    Add export variables

--- recipes/idba/build.sh
+++ recipes/idba/build.sh
@@ -5,9 +5,14 @@ aclocal
 autoconf
 automake --add-missing
 
+export CC=${PREFIX}/bin/gcc
+export CXX=${PREFIX}/bin/g++
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
 export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-./configure --prefix=$PREFIX
+
+./configure
 make
 
 
