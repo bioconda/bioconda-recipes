78176598a9707b652fe8f16b804ec1e283a7bbad
Bérénice Batut <bebatut@gmail.com>
2016-12-22 15:48:51 +0100

    Add fixing for automake and autoconf

--- recipes/idba/build.sh
+++ recipes/idba/build.sh
@@ -1,3 +1,16 @@
+# fix automake
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
+
+# fix autoconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
+
+# fix other python and perl scripts
 gawk -i inplace '/usr\/bin\//{ gsub(/python/, "env python");}1' script/*.py
 gawk -i inplace '/usr\/bin\//{ gsub(/perl/, "env perl");}1' script/*
 
