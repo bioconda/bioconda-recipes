e305590506a2d9a0a971baf43ad60c5939e29976
Bérénice Batut <bebatut@gmail.com>
2016-12-22 14:01:39 +0100

    Move the gawk and add perl gawk too

--- recipes/idba/build.sh
+++ recipes/idba/build.sh
@@ -1,3 +1,6 @@
+gawk -i inplace '/usr\/bin\//{ gsub(/python/, "env python");}1' script/*.py
+gawk -i inplace '/usr\/bin\//{ gsub(/perl/, "env perl");}1' script/*.py
+
 aclocal
 autoconf
 automake --add-missing
@@ -7,7 +10,6 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 ./configure --prefix=$PREFIX
 make
 
-gawk -i inplace '/usr\/bin\//{ gsub(/python/, "env python");}1' script/*.py
 
 mkdir -p ${PREFIX}/bin
 
