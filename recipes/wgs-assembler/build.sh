b20f6f010ad7529dc2fa4af65ad178b0c5ed5d07
Bérénice Batut <bebatut@gmail.com>
2016-12-23 09:34:05 +0100

    Add export of environmment variables

--- recipes/wgs-assembler/build.sh
+++ recipes/wgs-assembler/build.sh
@@ -1,3 +1,9 @@
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
 cd $SRC_DIR/kmer
 make install prefix=$PREFIX
 cd $SRC_DIR/src
