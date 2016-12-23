67dbc50640f6defa0c7641b9ae0eb69ebed85c4e
Michal Lorenc <lorencm@sef-eeb-123137.qut.edu.au>
2016-12-23 10:16:41 +1000

    added gsl and modified build.sh

--- recipes/augustus/build.sh
+++ recipes/augustus/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
+
+#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/bamtools"
+
+#make -C auxprogs/filterBam/src BAMTOOLS=${PREFIX}/include/bamtools COMPGENEPRED=true
+export C_INCLUDE_PATH=$PREFIX/include 
+export LIBRARY_PATH=$PREFIX/lib 
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-export BOOST_INCLUDE_DIR=${PREFIX}/include
-export BOOST_LIBRARY_DIR=${PREFIX}/lib
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
 
-export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
-export LDFLAGS="-L${BOOST_LIBRARY_DIR}"
 
-make -C auxprogs/filterBam/src BAMTOOLS=${PREFIX}/include/bamtools COMPGENEPRED=true
-make -C BAMTOOLS=${PREFIX}/include/bamtools COMPGENEPRED=true
+make COMPGENEPRED=true
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/scripts
