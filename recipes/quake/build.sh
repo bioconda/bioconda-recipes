67ddb62c005864ba4c99bd6b1afeccfc4d2e192c
Bérénice Batut <bebatut@gmail.com>
2016-12-23 11:39:37 +0100

    Correct the gawk command to sed command

--- recipes/quake/build.sh
+++ recipes/quake/build.sh
@@ -1,4 +1,4 @@
-gawk -i inplace '/jellyfish_dir = /{ gsub(/quake_dir/, "${PREFIX}/bin");}1' bin/quake.py
+sed -i.bak "s/jellyfish_dir = quake_dir/jellyfish_dir = $PREFIX\/bin/g" bin/quake.py
 
 export CFLAGS="-O2 -fopenmp -I$PREFIX/include -I"
 cd $SRC_DIR/src
