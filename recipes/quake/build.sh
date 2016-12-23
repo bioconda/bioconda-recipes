3c503834837fde778e7076fcc4fbc3478f22945e
Bérénice Batut <bebatut@gmail.com>
2016-12-23 12:58:19 +0100

    Move the sed command

--- recipes/quake/build.sh
+++ recipes/quake/build.sh
@@ -1,9 +1,9 @@
-sed -i.bak "s/jellyfish_dir = quake_dir/jellyfish_dir = $PREFIX\/bin/g" bin/quake.py
-
 export CFLAGS="-O2 -fopenmp -I$PREFIX/include -I"
 cd $SRC_DIR/src
 make
 cd ../
+
+sed -i.bak "s/jellyfish_dir = quake_dir/jellyfish_dir = $PREFIX\/bin/g" bin/quake.py
 
 mkdir -p $PREFIX/bin
 mv bin/* $PREFIX/bin/
