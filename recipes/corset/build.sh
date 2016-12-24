3872da1bbb1a74998e86f9f1c586e21a160b9429
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-12-24 22:37:44 +0200

    Fix build issues for corset

--- recipes/corset/build.sh
+++ recipes/corset/build.sh
@@ -8,5 +8,6 @@ export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/bam"
 
 mkdir -p $PREFIX/bin
 ./configure --prefix=$PREFIX --with-bam_inc=$PREFIX/include/bam --with-bam_lib=$PREFIX/lib
+make clean # Some leftovers needs to be removed
 make 
 make install
