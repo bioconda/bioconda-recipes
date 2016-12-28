af1e7988caf768a0626ded95519b6f34e72ce877
Dilmurat Yusuf <dilmurat.yusuf@gmail.com>
2016-12-28 22:57:41 +0100

    bioconductor-rhtslib/build.sh: fix bioconda autoconf perl configuration

--- recipes/bioconductor-rhtslib/build.sh
+++ recipes/bioconductor-rhtslib/build.sh
@@ -1,6 +1,14 @@
 
 #!/bin/bash
 
+# fix autoconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
+
 # R refuses to build packages that mark themselves as
 # "Priority: Recommended"
 mv DESCRIPTION DESCRIPTION.old
@@ -14,4 +22,4 @@ $R CMD INSTALL --build .
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build
 # process.
-# 
+#
