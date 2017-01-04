cc549cb232df8a9ab8aa17a73b7b8147d970fa40
Bérénice Batut <bebatut@gmail.com>
2017-01-04 16:51:58 +0100

    Try to add -stdlib=libc++ for osx

--- recipes/pbbam/build.sh
+++ recipes/pbbam/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mkdir build
 cd build
-export CXXFLAGS="${CXXFLAGS} -std=c++0x"
+
+if [ `uname` == Darwin ]; then
+    export CXXFLAGS="${CXXFLAGS} -std=c++0x -stdlib=libc++"
+fi
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DPacBioBAM_build_docs=OFF -DPacBioBAM_build_tests=OFF
 make 
 cp lib/* $PREFIX/lib
