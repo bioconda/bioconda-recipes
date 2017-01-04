784f81dcdad06356195994061f32769cd170a5a4
Bérénice Batut <bebatut@gmail.com>
2017-01-04 15:59:35 +0100

    add CXXflags

--- recipes/pbbam/build.sh
+++ recipes/pbbam/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 mkdir build
 cd build
+export CXXFLAGS="${CXXFLAGS} -std=c++0x"
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DPacBioBAM_build_docs=OFF -DPacBioBAM_build_tests=OFF
-make
+make 
 cp lib/* $PREFIX/lib
 cp bin/* $PREFIX/bin
