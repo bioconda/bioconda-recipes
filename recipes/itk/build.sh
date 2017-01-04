861c499e67f0cc195e5cfb622913469f394fd7f8
Thomas Wollmann <thomas.wollmann@bioquant.uni-heidelberg.de>
2017-01-04 12:23:45 +0100

    copied ITK recipe from heimdali channel

---
+++ recipes/itk/build.sh
@@ -0,0 +1,29 @@
+mkdir build
+cd build
+
+if [ -z "$OSX_ARCH" ]
+then
+   config=config.cmake.linux
+else
+   config=config.cmake.osx
+fi
+
+sed s:CONDA_PREFIX:$PREFIX: $RECIPE_DIR/$config > config.cmake
+
+# Configure
+cmake -C config.cmake ..
+
+# Build
+VERBOSE=1 make -j$CPU_COUNT
+
+# Install
+make install
+
+# Fix absolute path to system libaries, that are different for example on
+# Ubuntu.
+for config in $(find $PREFIX -name '*.cmake')
+do
+    sed -i s:/usr/lib64/libm.so:libm.so:g   $config
+    sed -i s:/usr/lib64/libdl.so:libdl.so:g $config
+    sed -i s:/usr/lib64/librt.so:librt.so:g $config
+done
