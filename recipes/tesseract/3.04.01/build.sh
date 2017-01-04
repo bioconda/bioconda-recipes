719d435e79bc296d1bdb2aa28fac7879af970ebf
Björn Grüning <bjoern@gruenings.eu>
2017-01-04 04:47:23 +0100

    Update build.sh

--- recipes/tesseract/3.04.01/build.sh
+++ recipes/tesseract/3.04.01/build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# fix automake
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
+
+# fix autoconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
+
 ./autogen.sh
 LIBLEPT_HEADERSDIR=$PREFIX/include ./configure --prefix=$PREFIX --with-extra-libraries=$PREFIX/lib
 LDFLAGS="-L$PREFIX/lib" CFLAGS="-I$PREFIX/include" make
