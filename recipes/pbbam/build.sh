235667e8d2e390c32c87b1b83820712f91e70c06
Bérénice Batut <bebatut@gmail.com>
2017-01-05 07:48:17 +0100

    Comment lib copy

--- recipes/pbbam/build.sh
+++ recipes/pbbam/build.sh
@@ -7,5 +7,5 @@ if [ `uname` == Darwin ]; then
 fi
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DPacBioBAM_build_docs=OFF -DPacBioBAM_build_tests=OFF
 make 
-cp lib/* $PREFIX/lib
+#cp lib/* $PREFIX/lib
 cp bin/* $PREFIX/bin
