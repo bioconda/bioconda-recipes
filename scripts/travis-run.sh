c6b3cf5e25783d161552149d26813c9cd4736053
Björn Grüning <bjoern@gruenings.eu>
2017-01-06 00:32:49 +0100

    Make travis_run more verbose

--- scripts/travis-run.sh
+++ scripts/travis-run.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 
 export PATH=/anaconda/bin:$PATH
 
-if [[ $TRAVIS_BRANCH = "master" && "$TRAVIS_PULL_REQUEST" = false ]]
+if [[ $TRAVIS_BRANCH = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]
 then
+   echo "Create Container push commands file: ${TRAVIS_BUILD_DIR}/container_push_commands.sh"
    export CONTAINER_PUSH_COMMANDS_PATH=${TRAVIS_BUILD_DIR}/container_push_commands.sh
    touch $CONTAINER_PUSH_COMMANDS_PATH
 fi
@@ -21,7 +22,7 @@ set -x; bioconda-utils build recipes config.yml $USE_DOCKER $BIOCONDA_UTILS_ARGS
 
 if [[ $TRAVIS_OS_NAME = "linux" ]]
 then
-    if [[ $TRAVIS_BRANCH = "master" && "$TRAVIS_PULL_REQUEST" = false ]]
+    if [[ $TRAVIS_BRANCH = "master" && "$TRAVIS_PULL_REQUEST" = "false" ]]
     then
         echo "Push containers to quay.io"
         cat $CONTAINER_PUSH_COMMANDS_PATH
