3c1de018c26b6624395a42501001b6f2caa2a793
Jaime Huerta-Cepas <jhcepas@gmail.com>
2017-01-05 12:07:49 +0100

    try slr compilation

--- recipes/phyml/build.sh
+++ recipes/phyml/build.sh
@@ -4,7 +4,12 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
+# For some reason without this patch compilation fails in OSX. BEAGLE is not
+# used in the default compilation.
+patch configure.ac $RECIPE_DIR/phyml.patch
+./autogen.sh
 ./configure
 make
+
 mkdir -p $PREFIX/bin
-cp phyml $PREFIX/bin
+cp src/phyml $PREFIX/bin
--- recipes/slr/build.sh
+++ recipes/slr/build.sh
@@ -9,7 +9,14 @@ cd src/
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Platform: Linux"
     # modifies makefile to use openblas
-    perl -p -i -e 's/-lblas -llapack/-lopenblas/g' Makefile.linux
+    patch Makefile.linux $RECIPE_DIR/openblas.patch
     make -f Makefile.linux
+    cp Slr $PREFIX/bin
+elif [ "$(uname)" == "Darwin" ]; then
+    echo "Platform: OSX"
+    # modifies makefile to use openblas
+    #patch Makefile.linux $RECIPE_DIR/openblas.patch
+    #make -f Makefile.linux
+    make -f Makefile.osx
     cp Slr $PREFIX/bin
 fi
