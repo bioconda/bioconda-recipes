bff9e2fdcafd3bb5b108f5b13d3e473f8a8583e9
Peter van Heusden <pvh@sanbi.ac.za>
2017-01-10 21:53:31 +0200

    * Add more substantial test * Use REPEATMASKER_DIR, REPEATMASKER_LIB_DIR and REPEATMASKER_MATRICES_DIR to set locations for   the RepeatMasker installation, the Libraries directory and Matrices directory. Required   to use own, externally supplied, Libraries (e.g. latest RepBase). * Suppress complaint about age of library when the minimal library is being used. * Harmonise use of variables in RepeatMasker code: use REPEATMASKER_LIB_DIR   instead of REPEATMASKER_DIR/Libraries.

--- recipes/repeatmasker/build.sh
+++ recipes/repeatmasker/build.sh
@@ -6,22 +6,29 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${RM_DIR}
 cp -r * ${RM_DIR}
 for name in ${RM_PROGRAMS} ; do 
-  perl -i -0pe 's/^#\\!.*perl.*/#\\!\/usr\/bin\/perl/g;' ${RM_DIR}/${name}
+  sed -i 's$^#!.*perl.*$#!/usr/bin/env perl$g;' ${RM_DIR}/${name}
 done
 
 cp ${RECIPE_DIR}/RepeatMaskerConfig.pm ${RM_DIR}/RepeatMaskerConfig.pm
+# pvanheus - disabled this because the library included with RepeatMasker is too old to be usable
+# users will have to provide their own library and point to it using the REPEATMASKER_LIB_DIR environment variable
+
+
 ${RM_DIR}/util/buildRMLibFromEMBL.pl ${RM_DIR}/Libraries/RepeatMaskerLib.embl > ${RM_DIR}/Libraries/RepeatMasker.lib 2>/dev/null
 makeblastdb -dbtype nucl -in ${RM_DIR}/Libraries/RepeatMasker.lib
 makeblastdb -dbtype prot -in ${RM_DIR}/Libraries/RepeatPeps.lib
 
+
 cat <<END >>${PREFIX}/bin/RepeatMasker
 #!/bin/bash
 
-REPEATMASKER_DIR=${PREFIX}/share/RepeatMasker
+REPEATMASKER_DIR=\${REPEATMASKER_DIR:-\${CONDA_PREFIX}/share/RepeatMasker}
+REPEATMASKER_MATRICES_DIR=\${REPEATMASKED_MATRICES_DIR:-\${CONDA_PREFIX}/share/RepeatMasker/Matrices}
+REPEATMASKER_LIB_DIR=\${REPEATMASKER_LIB_DIR:-\${CONDA_PREFIX}/share/RepeatMasker/Libraries}
 TRF_DIR=${PREFIX}/bin
 RMBLAST_DIR=${PREFIX}/bin
 HMMER_DIR=${PREFIX}/bin
-export REPEATMASKER_DIR TRF_DIR RMBLAST_DIR HMMER_DIR
+export REPEATMASKER_DIR REPEATMASKER_LIB_DIR REPEATMASKER_MATRICES_DIR TRF_DIR RMBLAST_DIR HMMER_DIR
 
 NAME=\$(basename \$0)
 
