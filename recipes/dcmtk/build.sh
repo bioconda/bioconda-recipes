1b871d67dd9c4163800bbfc9fb0ff0ca7e2ebf4f
Thomas Wollmann <thomas.wollmann@bioquant.uni-heidelberg.de>
2017-01-11 15:06:25 +0100

    added tests

--- recipes/dcmtk/build.sh
+++ recipes/dcmtk/build.sh
@@ -3,22 +3,12 @@
 mkdir build
 cd build
 
-if [ `uname` == Darwin ]; then
-    DCMTK_HOME=$PREFIX \
-    cmake \
-        -DCMAKE_FIND_ROOT_PATH=$PREFIX \
-        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8                                      \
-        -DCMAKE_CXX_FLAGS="-mmacosx-version-min=10.8 -std=c++11 -stdlib=libc++" \
-        -DCMAKE_SHARED_LINKER_FLAGS="-mmacosx-version-min=10.8 -stdlib=libc++"  \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
-fi
-
-if [ `uname` == Linux ]; then
-    DCMTK_HOME=$PREFIX \
-    cmake \
-        -DCMAKE_FIND_ROOT_PATH=$PREFIX \
-	-DCMAKE_INSTALL_PREFIX=${PREFIX} ..
-fi
+DCMTK_HOME=$PREFIX \
+cmake \
+	-D CMAKE_FIND_ROOT_PATH=${PREFIX} \
+	-D CMAKE_INSTALL_PREFIX=${PREFIX} \
+        -D CMAKE_BUILD_TYPE:STRING=Release \
+	..
 
 make -j${CPU_COUNT}
 make install
