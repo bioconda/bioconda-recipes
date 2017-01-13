780486c19b6ece9d70827f6bb71e15727626ba9a
Thomas Wollmann <thomas.wollmann@bioquant.uni-heidelberg.de>
2017-01-13 09:36:52 +0100

    added Visceral-Project segmentation evaluation tool recipe

---
+++ recipes/visceral-evaluatesegmentation/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "source"
+mkdir build
+cd build
+
+cmake \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+    -D CMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
+    -D CMAKE_BUILD_TYPE=Release \
+    ..
+
+make -j4
+cp EvaluateSegmentation ${PREFIX}/bin/EvaluateSegmentation
