823c1307a656fe6bde97715bbccb0306825f4797
Michal Lorenc <mictadlo@gmail.com>
2017-01-13 15:12:47 +1000

    fixed sed

--- recipes/transcomb/build.sh
+++ recipes/transcomb/build.sh
@@ -3,10 +3,10 @@
 mkdir -p $PREFIX/bin
 
 
-sed -i .bak 's/<Path_To_BOOST_LIB_DIR>/${PREFIX}/lib/g' src/CMakeList.txt
-sed -i .bak 's/<Path_To_BOOST_INCLUDE_DIR>/${PREFIX}/include/g' src/CMakeList.txt
-sed -i .bak 's/<Path_To_BAMTOOLS_INCLUDE_DIR>/${PREFIX}/include/g' src/CMakeList.txt
-sed -i .bak 's/<Path_To_BAMTOOLS_LIB_DIR>/${PREFIX}/lib/g' src/CMakeList.txt
+sed -i.bak 's/<Path_To_BOOST_LIB_DIR>/${PREFIX}\/lib' src/CMakeList.txt
+sed -i.bak 's/<Path_To_BOOST_INCLUDE_DIR>/${PREFIX}\/include' src/CMakeList.txt
+sed -i.bak 's/<Path_To_BAMTOOLS_INCLUDE_DIR>/${PREFIX}\/include' src/CMakeList.txt
+sed -i.bak 's/<Path_To_BAMTOOLS_LIB_DIR>/${PREFIX}\/lib' src/CMakeList.txt
 
 mkdir build
 cd build
