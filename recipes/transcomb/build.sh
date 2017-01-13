a9c39bdd5c87f2df08dfeca91e85193193fec51f
Michal Lorenc <mictadlo@gmail.com>
2017-01-13 12:35:32 +1000

    fixed CmakeList.txt

--- recipes/transcomb/build.sh
+++ recipes/transcomb/build.sh
@@ -2,10 +2,11 @@
 
 mkdir -p $PREFIX/bin
 
-sed -i .bak 's/<Path_To_BOOST_LIB_DIR>/${PREFIX}/lib/g' CMakeList.txt
-sed -i .bak 's/<Path_To_BOOST_INCLUDE_DIR>/${PREFIX}/include/g' CMakeList.txt
-sed -i .bak 's/<Path_To_BAMTOOLS_INCLUDE_DIR>/${PREFIX}/include/g' CMakeList.txt
-sed -i .bak 's/<Path_To_BAMTOOLS_LIB_DIR>/${PREFIX}/lib/g' CMakeList.txt
+
+sed -i .bak 's/<Path_To_BOOST_LIB_DIR>/${PREFIX}/lib/g' src/CMakeList.txt
+sed -i .bak 's/<Path_To_BOOST_INCLUDE_DIR>/${PREFIX}/include/g' src/CMakeList.txt
+sed -i .bak 's/<Path_To_BAMTOOLS_INCLUDE_DIR>/${PREFIX}/include/g' src/CMakeList.txt
+sed -i .bak 's/<Path_To_BAMTOOLS_LIB_DIR>/${PREFIX}/lib/g' src/CMakeList.txt
 
 mkdir build
 cd build
