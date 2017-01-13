515e9b32e07d299d37827ffc79f5f2a9f80c43b6
Ryan Dale <dalerr@niddk.nih.gov>
2017-01-13 12:25:51 -0500

    add pandas to reqs

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
             ],
         )
     ],
-    install_requires=["argh", "networkx", "pydotplus", "pyyaml", "requests<2.11", "jsonschema"],
+    install_requires=["argh", "networkx", "pydotplus", "pyyaml", "requests<2.11", "jsonschema", "pandas"],
     entry_points={"console_scripts": [
         "bioconda-utils = bioconda_utils.cli:main",
         "bioconductor_skeleton = bioconda_utils.bioconductor_skeleton:main"
