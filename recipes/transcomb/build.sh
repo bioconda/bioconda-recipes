9a93c006713f2903da2f187a89757050fd44bf5d
Michal Lorenc <mictadlo@gmail.com>
2017-01-16 07:14:13 +1000

    updated sed command, but still failing

--- recipes/transcomb/build.sh
+++ recipes/transcomb/build.sh
@@ -3,10 +3,10 @@
 mkdir -p $PREFIX/bin
 
 
-sed -i.bak 's/<Path_To_BOOST_LIB_DIR>/${PREFIX}\/lib/' src/CMakeLists.txt
-sed -i.bak 's/<Path_To_BOOST_INCLUDE_DIR>/${PREFIX}\/include/' src/CMakeLists.txt
-sed -i.bak 's/<Path_To_BAMTOOLS_INCLUDE_DIR>/${PREFIX}\/include/' src/CMakeLists.txt
-sed -i.bak 's/<Path_To_BAMTOOLS_LIB_DIR>/${PREFIX}\/lib/' src/CMakeLists.txt
+sed -i .bak 's|<Path_To_BOOST_LIB_DIR>|`echo ${PREFIX}/lib`|' src/CMakeLists.txt
+sed -i .bak 's|<Path_To_BOOST_INCLUDE_DIR>|`echo ${PREFIX}/include`|' src/CMakeLists.txt
+sed -i .bak 's|<Path_To_BAMTOOLS_INCLUDE_DIR>|${PREFIX}/include|' src/CMakeLists.txt
+sed -i .bak 's|<Path_To_BAMTOOLS_LIB_DIR>|${PREFIX}/lib|' src/CMakeLists.txt
 
 mkdir build
 cd build
