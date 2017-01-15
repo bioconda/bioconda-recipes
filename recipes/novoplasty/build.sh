02b61f784e85330fc2a3fe50690ab4e9cdfa3aef
Michal Lorenc <mictadlo@gmail.com>
2017-01-16 07:27:44 +1000

    Fixed test

--- recipes/novoplasty/build.sh
+++ recipes/novoplasty/build.sh
@@ -5,9 +5,7 @@ mkdir -p $PREFIX/bin
 sed -i.bak "s/\/usr\/bin\/perl/\/usr\/bin\/env perl/" NOVOPlasty${PKG_VERSION}.pl
 tail -n+2 NOVOPlasty${PKG_VERSION}.pl > NOVOPlasty${PKG_VERSION}.pl.new
 
-mv NOVOPlasty${PKG_VERSION}.pl.new NOVOPlasty${PKG_VERSION}.pl 
-ln -s NOVOPlasty${PKG_VERSION}.pl novoplasty.pl
-chmod +x NOVOPlasty${PKG_VERSION}.pl
-chmod +x novoplasty.pl
+mv NOVOPlasty${PKG_VERSION}.pl.new NOVOPlasty.pl 
+chmod +x NOVOPlasty.pl
 
 mv *.pl $PREFIX/bin
