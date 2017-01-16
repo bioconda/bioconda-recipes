8404f614cf589982c0f35378f9571b101e3b0926
Michal Lorenc <mictadlo@gmail.com>
2017-01-16 11:51:43 +1000

    strill problems to find perl script

--- recipes/novoplasty/build.sh
+++ recipes/novoplasty/build.sh
@@ -2,10 +2,10 @@
 
 mkdir -p $PREFIX/bin
 
-sed -i.bak "s/\/usr\/bin\/perl/\/usr\/bin\/env perl/" NOVOPlasty${PKG_VERSION}.pl
-tail -n+2 NOVOPlasty${PKG_VERSION}.pl > NOVOPlasty${PKG_VERSION}.pl.new
+sed -i.bak "s|!/usr/bin/perl||" NOVOPlasty${PKG_VERSION}.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g'  NOVOPlasty${PKG_VERSION}.pl
 
-mv NOVOPlasty${PKG_VERSION}.pl.new NOVOPlasty.pl 
-chmod +x NOVOPlasty.pl
+mv *.pl  $PREFIX/bin
 
-mv *.pl $PREFIX/bin
+chmod +x $PREFIX/bin/NOVOPlasty${PKG_VERSION}.pl
+ln -s $PREFIX/bin/NOVOPlasty${PKG_VERSION}.pl $PREFIX/bin/NOVOPlasty.pl
