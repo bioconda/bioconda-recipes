5d8ff9285e0ecc1b0a351eeae8228fc4ddcdf4a9
Anne Fouilloux <annefou@geo.uio.no>
2017-01-18 00:59:06 +0100

    rebuild netcdf-fortran with netCDF 4 enabled

--- recipes/netcdf-fortran/build.sh
+++ recipes/netcdf-fortran/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib ./configure --prefix=$PREFIX
+export CPPFLAGS="-I${PREFIX}/include $CPPFLAGS"
+export CFLAGS="-I${PREFIX}/include $CFLAGS"
+export LDFLAGS="-L${PREFIX}/lib $LDFLAGS"
+export DYLD_LIBRARY_PATH=${PREFIX}/lib
+./configure --prefix=$PREFIX --enable-netcdf4
+
 make
+make check
 make install
