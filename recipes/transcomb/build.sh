9bf11578573c77060f4f2ed874884ca2bea5fd2b
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-01-17 11:40:14 +0400

    adding in icu ==5.6

--- recipes/transcomb/build.sh
+++ recipes/transcomb/build.sh
@@ -9,14 +9,13 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export BOOST_INCLUDE_DIR="${PREFIX}/include"
 export BOOST_LIBRARY_DIR="${PREFIX}/lib"
-
-export LIBS='-lboost_math_c99 -lboost_program_options -lboost_regex -lboost_graph -lboost_thread -lboost_system -lboost_filesystem'
+export LIBS='-lboost_regex -lboost_system -lboost_program_options -lboost_filesystem -lboost_timer'
 
 export BAMTOOLS_INCLUDE_DIR="${PREFIX}/include/bamtools/"
 export BAMTOOLS_LIBRARY_DIR="${PREFIX}/lib/"
 
 export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
-export LDFLAGS="-L${BOOST_LIBRARY_DIR} ${LIBS}"
+export LDFLAGS="-L${BOOST_LIBRARY_DIR} -lboost_regex -lboost_filesystem -lboost_system"
 
 mkdir build
 cd build
