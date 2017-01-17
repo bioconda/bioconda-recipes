a5a9aedba99b0fa59ad8c15c5e9d3c53150cdad1
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-01-17 11:13:57 +0400

    adding libs to boost

--- recipes/transcomb/build.sh
+++ recipes/transcomb/build.sh
@@ -10,13 +10,13 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export BOOST_INCLUDE_DIR="${PREFIX}/include"
 export BOOST_LIBRARY_DIR="${PREFIX}/lib"
 
-#export LIBS='-lboost_system -lboost_program_options -lboost_filesystem -lboost_timer'
+export LIBS='-lboost_regex -lboost_system -lboost_program_options -lboost_filesystem -lboost_timer'
 
 export BAMTOOLS_INCLUDE_DIR="${PREFIX}/include/bamtools/"
 export BAMTOOLS_LIBRARY_DIR="${PREFIX}/lib/"
 
 export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
-export LDFLAGS="-L${BOOST_LIBRARY_DIR} -lboost_filesystem -lboost_system"
+export LDFLAGS="-L${BOOST_LIBRARY_DIR} ${LIBS}"
 
 mkdir build
 cd build
