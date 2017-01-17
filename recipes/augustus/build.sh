18181f9bc4d3b88bb57aef0e3b196f69b61de9cd
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-01-17 12:47:12 +0400

    minor changes

--- recipes/augustus/build.sh
+++ recipes/augustus/build.sh
@@ -10,7 +10,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export BOOST_INCLUDE_DIR=${PREFIX}/include
 export BOOST_LIBRARY_DIR=${PREFIX}/lib
 
-export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
+export CXXFLAGS="-std=c++11 -DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
 export LDFLAGS="-L${BOOST_LIBRARY_DIR}"
 
 make
@@ -18,8 +18,20 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/scripts
 mkdir -p $PREFIX/config
 
-sed -i 's/#!\/usr\/bin\/perl/#!\/usr\/bin\/env perl/g' scripts/*
-sed -i 's/ perl -w$/ perl/' scripts/*
+
+## Build Perl
+
+mkdir perl-build
+find scripts -name "*.pl" | xargs -I {} mv {} perl-build
+cd perl-build
+cp ${RECIPE_DIR}/Build.PL ./
+perl ./Build.PL
+./Build manifest
+./Build install --installdirs site
+
+cd ..
+## End build perl
+
 
 mv bin/* $PREFIX/bin/
 mv scripts/* $PREFIX/bin/
