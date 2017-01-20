3d5f1be85767ef29794bd49325d44a27ff5acab4
rvalieris <rvalieris@users.noreply.github.com>
2017-01-20 16:57:47 -0200

    attempt to compile blast on linux

--- recipes/blast/2.2.21/build.sh
+++ recipes/blast/2.2.21/build.sh
@@ -2,21 +2,25 @@
 
 set -e -x -o pipefail
 
-mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/bin/
 
 if [ "$(uname)" == "Darwin" ]; then
     echo "Platform: Mac"
 
-    # export CFLAGS='-O3'
-    # export CXXFLAGS='-O3'
-
-    cp $SRC_DIR/bin/* $PREFIX/bin
+    cp $SRC_DIR/bin/* $PREFIX/bin/
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Platform: Linux"
-    
-    cp $SRC_DIR/bin/* $PREFIX/bin/
-    
+
+    cd $SRC_DIR/c++/
+    ./configure --prefix=$PREFIX
+
+    make -j${CPU_COUNT}
+
+    cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
 fi
+
+# patch perl shebang line
+sed -i 's/ \/usr\/bin\/perl -w/\/usr\/bin\/env perl/' $PREFIX/bin/legacy_blast.pl $PREFIX/bin/update_blastdb.pl
 
 chmod +x $PREFIX/bin/*
