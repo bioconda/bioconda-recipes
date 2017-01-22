72c96bda6fa9e11beb6a8c381d4b94b276511a6d
Björn Grüning <bjoern@gruenings.eu>
2017-01-22 15:58:51 +0100

    Update build.sh

--- recipes/gdk-pixbuf/build.sh
+++ recipes/gdk-pixbuf/build.sh
@@ -5,6 +5,8 @@ ln -s $PREFIX/lib $PREFIX/lib64
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig"
 export ACLOCAL_FLAGS="-I$PREFIX/share/aclocal"
 
+sed -i.bak 's|#!/usr/bin/perl.*|#!/usr/bin/env perl|' ./gdk-pixbuf/*.pl
+
 # export CFLAGS="-I$PREFIX/include -I$PREFIX/include/glib-2.0 -I$PREFIX/include/glib-2.0/gobject -I$PREFIX/include/glib-2.0/glib -I$PREFIX/include/glib-2.0/gio -I$PREFIX/include/gobject-introspection-1.0 -I$PREFIX/lib/glib-2.0/include -I/usr/include -I/usr/include/X11 -I/usr/include/X11/extensions"
 # export LDFLAGS="-L$PREFIX/lib -L$PREFIX/lib64"
 
