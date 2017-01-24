e7d3d9ef81de32a1cea1894840663e5ccd46eab3
Colin Brislawn <cbrisl@gmail.com>
2017-01-24 14:23:58 -0800

    use same build on OSX and linux

--- recipes/maxbin2/build.sh
+++ recipes/maxbin2/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 cp makefile.new src
-
-if [ `uname` == Darwin ]; then
-    export CXXFLAGS="${CXXFLAGS} -std=c++0x -stdlib=libc++"
-fi
 cd src && make -f makefile.new
 cp MaxBin $PREFIX/bin/
