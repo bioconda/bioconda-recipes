03b23bf6826030120307b790cee9a4aca890c8e1
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2017-01-25 16:26:38 +0100

    remove .exe ext

--- recipes/pash/build.sh
+++ recipes/pash/build.sh
@@ -5,4 +5,9 @@ sed -i.bak "s|INSTALLDIR=|INSTALLDIR=$PREFIX|g" Makefile.include
 make
 make install
 
+mv $PREFIX/bin/keyFreq.exe $PREFIX/bin/keyFreq
+mv $PREFIX/bin/makeIgnoreList.exe $PREFIX/bin/makeIgnoreList
+mv $PREFIX/bin/pash2SAM.exe $PREFIX/bin/pash2SAM
+mv $PREFIX/bin/pash-3.0lx.exe $PREFIX/bin/pash-3.0lx
+
 cp -r rubyLib/* $PREFIX/lib/ruby/2.2.0/
