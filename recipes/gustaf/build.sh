761d24ef57b3cbfeb430bb253892271b91e1b724
eseiler <enrico.seiler@hotmail.de>
2017-01-28 15:58:09 +0100

    Copy binaries directly to /bin

--- recipes/gustaf/build.sh
+++ recipes/gustaf/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-BINARY_HOME=$PREFIX/bin
-PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
-
 cd $SRC_DIR/bin
 
 binaries="\
@@ -10,6 +7,5 @@ gustaf \
 gustaf_mate_joining \
 "
 mkdir -p $PREFIX/bin
-mkdir -p $PKG_HOME
 
-for i in $binaries; do cp $SRC_DIR/bin/$i $PKG_HOME/$i && chmod a+x $PKG_HOME/$i && ln -s $PKG_HOME/$i $BINARY_HOME/$i; done
+for i in $binaries; do cp $SRC_DIR/bin/$i $PREFIX/bin/$i && chmod a+x $PREFIX/bin/$i; done
