28fc20ceafc9d5603d099aca56b4196da67bb7af
eseiler <enrico.seiler@hotmail.de>
2017-01-28 15:54:02 +0100

    Copy binaries directly to /bin

--- recipes/sak/build.sh
+++ recipes/sak/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-BINARY_HOME=$PREFIX/bin
-PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
-
 cd $SRC_DIR/bin
 
 binaries="\
 sak \
 "
-mkdir -p $PREFIX/bin
-mkdir -p $PKG_HOME
 
-for i in $binaries; do cp $SRC_DIR/bin/$i $PKG_HOME/$i && chmod a+x $PKG_HOME/$i && ln -s $PKG_HOME/$i $BINARY_HOME/$i; done
+mkdir -p $PREFIX/bin
+
+for i in $binaries; do cp $SRC_DIR/bin/$i $PREFIX/bin && chmod a+x $PREFIX/bin/$i; done
