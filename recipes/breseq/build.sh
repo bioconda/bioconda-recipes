a7599a0fa3834e05d50f1448e01d65fdb599e57e
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-01-29 18:40:46 +0100

    Add breseq

--- recipes/breseq/build.sh
+++ recipes/breseq/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
 mkdir -p $PREFIX/bin
-
 ./configure --prefix=${PREFIX}
 make 
 make install
