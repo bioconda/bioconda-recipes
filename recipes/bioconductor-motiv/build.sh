7b70c73cfe8e136c74c9b44a46b74e69aa180964
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-01-30 13:39:18 +0100

    Add more paths

--- recipes/bioconductor-motiv/build.sh
+++ recipes/bioconductor-motiv/build.sh
@@ -4,6 +4,8 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
 
 # R refuses to build packages that mark themselves as
 # "Priority: Recommended"
