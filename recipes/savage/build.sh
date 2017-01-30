ccf21293b93366cc89ae9839a1fc9ee5e679f083
Jasmijn <baaijens@cwi.nl>
2017-01-30 12:32:00 +0100

    updated savage recipe

--- recipes/savage/build.sh
+++ recipes/savage/build.sh
@@ -9,5 +9,9 @@ mkdir -p $PREFIX/bin
 PACKAGE_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $PACKAGE_HOME
 
-cp $RECIPE_DIR/* $PACKAGE_HOME/
-ln -s $PACKAGE_HOME/gatk.py $PREFIX/bin/savage # create a symbolic link
+cp -r bin quick-cliques scripts sfo_2011_5 savage.py $PACKAGE_HOME/
+# ln -rs $PACKAGE_HOME/savage.py $PREFIX/bin/savage # create a symbolic link
+WRAPPER=$PREFIX/bin/savage
+echo "#!/bin/sh" > $WRAPPER
+echo "$PACKAGE_HOME/savage.py $@" >> $WRAPPER
+chmod +x $WRAPPER
