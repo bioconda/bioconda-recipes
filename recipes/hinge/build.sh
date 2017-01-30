eff2cb06b3072fe80732b7e459671ba4c71e78d4
Michal Lorenc <mictadlo@gmail.com>
2017-01-30 11:29:21 +1000

    added BOOST_LIBRARYDIR to build.sh

--- recipes/hinge/build.sh
+++ recipes/hinge/build.sh
@@ -5,11 +5,14 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export BOOST_INCLUDE_DIR=${PREFIX}/include
 export BOOST_LIBRARY_DIR=${PREFIX}/lib
+export BOOST_LIBRARYDIR=${PREFIX}/lib
 
 #export CXXFLAGS=" -std=c++11 -stdlib=libstdc++ -stdlib=libc++ -DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
 export CXXFLAGS=" -std=c++11  -DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
 export LDFLAGS="-L${BOOST_LIBRARY_DIR}"
 export Boost_NO_BOOST_CMAKE=TRUE 
+
+
 
 mkdir -p $PREFIX/bin
 
