31ec9adc6244397429f0860203581723a82db4ad
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-01-31 20:14:12 +0100

    Add make install step

--- recipes/mashmap/build.sh
+++ recipes/mashmap/build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # fix automake
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
@@ -21,3 +22,4 @@ mkdir -p $PREFIX/bin
 ./bootstrap.sh
 ./configure --prefix=$PREFIX --with-gsl=$PREFIX 
 make
+make install 
