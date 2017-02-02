9cec3669c0ee20d34e94b0d257d6ae424a4487e7
Colin Brislawn <cbrisl@gmail.com>
2017-02-01 18:13:26 -0800

    Quorum build using only gcc

--- recipes/quorum/build.sh
+++ recipes/quorum/build.sh
@@ -1,4 +1,6 @@
-autoreconf -i
+export JELLYFISH2_0_CFLAGS=-I$PREFIX/include/jellyfish-2.2.6/
+export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.dylib
+
 ./configure --prefix=$PREFIX
 make
 make install
