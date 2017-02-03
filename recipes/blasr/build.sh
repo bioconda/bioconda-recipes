f52cc2a71ecd2a037860525de3e54b62c17e98d9
Bérénice Batut <bebatut@gmail.com>
2017-02-03 07:48:59 +0100

    Change to use submodules

--- recipes/blasr/build.sh
+++ recipes/blasr/build.sh
@@ -2,22 +2,12 @@
 
 mkdir -p $PREFIX/bin
 
-#wget -O libcpp.tar.gz https://github.com/PacificBiosciences/blasr_libcpp/archive/0ae16915b48d1ee77cb7cd4c95764181467c7175.tar.gz
-#tar -xvf libcpp.tar.gz
-#mv blasr_libcpp-0ae16915b48d1ee77cb7cd4c95764181467c7175 libcpp
 
 export HDF5_INCLUDE=$PREFIX/include 
 export HDF5_LIB=$PREFIX/lib
-export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-export LD_LIBRARY_PATH=$PREFIX/lib
-if [ `uname` == Darwin ]; then
-    export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++ -lstdc++"
-fi
-./configure.py --shared --sub --no-pbbam 
+./configure.py --shared --sub --no-pbbam
 
+make build-submodule
 make blasr
 cp blasr $PREFIX/bin
-cp libcpp/alignment/libblasr.* $PREFIX/lib
-cp libcpp/hdf/libpbihdf.* $PREFIX/lib
-cp libcpp/pbdata/libpbdata.* $PREFIX/lib
 
