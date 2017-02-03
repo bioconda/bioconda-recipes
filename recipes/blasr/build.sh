c017c812a68d9d966d5b11a08e993e7b9a8dd3c8
Bérénice Batut <bebatut@gmail.com>
2017-02-03 08:09:06 +0100

    Add the configuration of submodules

--- recipes/blasr/build.sh
+++ recipes/blasr/build.sh
@@ -2,10 +2,11 @@
 
 mkdir -p $PREFIX/bin
 
-
-export HDF5_INCLUDE=$PREFIX/include 
+export HDF5_INCLUDE=$PREFIX/include
 export HDF5_LIB=$PREFIX/lib
+
 ./configure.py --shared --sub --no-pbbam
+make configure-submodule
 
 make build-submodule
 make blasr
