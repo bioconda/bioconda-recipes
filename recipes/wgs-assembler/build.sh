384b5d48a3f5119e8ace2a005a1248b682effa8f
Bérénice Batut <bebatut@gmail.com>
2017-02-03 16:10:49 +0100

    Try new build commands

--- recipes/wgs-assembler/build.sh
+++ recipes/wgs-assembler/build.sh
@@ -5,16 +5,18 @@ find . -type f -name "*.pl" | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/en
 find . -type f -name "*.pm" | xargs sed -i.bak -e 's/usr\/local\/bin\/perl/usr\/bin\/env perl/g'
 find . -type f -name "*.sh" | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g'
 
-cd $SRC_DIR/kmer
-./configure.sh
-
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-make install prefix=$PREFIX
+#cd $SRC_DIR/kmer
+#./configure.sh
+    
+#export INCLUDE_PATH="${PREFIX}/include"
+#export LIBRARY_PATH="${PREFIX}/lib"
+#export LD_LIBRARY_PATH="${PREFIX}/lib"
+#export LDFLAGS="-L${PREFIX}/lib"
+#export CPPFLAGS="-I${PREFIX}/include"
+#make install prefix=$PREFIX
 cd $SRC_DIR/src
+
+export KMER=$PREFIX
 make
 cd ../
 ls
