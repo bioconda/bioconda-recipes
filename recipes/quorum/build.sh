8cef913978bceb279ef323945054719e0dd73543
Colin Brislawn <cbrisl@gmail.com>
2017-02-03 12:51:59 -0800

    Export different libs for osx and linux

--- recipes/quorum/build.sh
+++ recipes/quorum/build.sh
@@ -1,5 +1,11 @@
 export JELLYFISH2_0_CFLAGS=-I$PREFIX/include/jellyfish-2.2.6/
-export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.dylib
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Use *.dylib for OSX"
+    export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.dylib
+else
+    echo "Use *.so for Linux"
+    export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.so
+fi
 
 ./configure --prefix=$PREFIX
 make
