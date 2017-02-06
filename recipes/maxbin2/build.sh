24b109a3e4e0533f58231efaaa066dea3402ad0b
Joe Brown <joe.brown@pnnl.gov>
2017-02-06 12:36:06 -0800

    MaxBin2 fixes; existing leaves out core functionality
    This doesn't build locally and other things fail when using
    `simulate_travis.py`. Committing these changes for testing and comments.

--- recipes/maxbin2/build.sh
+++ recipes/maxbin2/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-set -euo pipefail
-cp makefile.new src
-cd src && make -f makefile.new
-cp MaxBin $PREFIX/bin/
+
+MAXBIN_HOME=$PREFIX/opt/MaxBin-$PKG_VERSION
+
+# make
+cp makefile.new $SRC_DIR/src
+cd $SRC_DIR/src
+make -f makefile.new
+
+mkdir -p $PREFIX/bin
+mkdir -p $MAXBIN_HOME
+cp -R $SRC_DIR/* $MAXBIN_HOME/
+
+cd $MAXBIN_HOME
+# fix perl path
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' run_MaxBin.pl
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' _getabund.pl
+sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' _getmarker.pl
+# fix script's bin dir to follow symlinks
+sed -i.bak 's|\$Bin|\$RealBin|' run_MaxBin.pl
+chmod a+x *.pl
+
+ln -s $MAXBIN_HOME/run_MaxBin.pl $PREFIX/bin/run_MaxBin.pl
