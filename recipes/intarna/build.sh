c4cb4218c66923a88da3dc7e4f8dee4733d76e0d
Martin Mann <martin-mann@users.noreply.github.com>
2017-02-06 19:22:03 +0100

    [linux] -fopenmp to support viennarna OMP build

--- recipes/intarna/build.sh
+++ recipes/intarna/build.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
-./configure --prefix=$PREFIX --with-RNA=$PREFIX --with-boost=$PREFIX --disable-multithreading && \
+#!/bin/bash
+
+## Choose extra configure options depending on the operating system
+## (mac or linux)
+##
+if [ `uname` == Darwin ] ; then
+    extra_config_options=""
+else ## linux
+    # add -fopenmp to compilation due to viennarna setup
+    extra_config_options="CXX_FLAGS=-fopenmp"
+fi
+
+./configure --prefix=$PREFIX \
+            --with-RNA=$PREFIX \
+            --with-boost=$PREFIX \
+            --disable-multithreading \
+            ${extra_config_options} \
+            && \
 make && \
 make install
