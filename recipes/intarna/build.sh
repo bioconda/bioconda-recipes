26476406a2bed103f8d5706cf43b9feee01d6d38
Martin Mann <martin-mann@users.noreply.github.com>
2017-02-06 16:09:33 +0100

    adapted to v2.0

--- recipes/intarna/build.sh
+++ recipes/intarna/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./configure --prefix=$PREFIX --with-RNA=$PREFIX && \
+./configure --prefix=$PREFIX --with-RNA=$PREFIX --with-boost=$PREFIX --disable-multithreading && \
 make && \
 make install
