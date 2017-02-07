039e2986d9a6dcf42b4284d1804adc1c97b5aec4
Martin Mann <martin-mann@users.noreply.github.com>
2017-02-07 12:22:43 +0100

    flags from conda/conda-recipes/boost/

--- recipes/intarna/build.sh
+++ recipes/intarna/build.sh
@@ -5,17 +5,19 @@
 ##
 if [ `uname` == Darwin ] ; then
 # enable c++11 support
-    echo;
+    #echo;
     #echo "PREFIX/lib";
     #ls -l $PREFIX/lib;
-    echo "PREFIX/lib/pkgconfig";
-    ls -l $PREFIX/lib/pkgconfig/boost*;
-    cat $PREFIX/lib/pkgconfig/boost*;
-    cat $PREFIX/lib/pkgconfig/RNAlib*;
-    echo "otool"
-    otool -L $PREFIX/lib/libboost_program_options.dylib
-    echo;
-    extra_config_options="CC=clang CXX=clang++ CXXFLAGS=-stdlib=libc++ LDFLAGS=-stdlib=libc++"
+    #echo "PREFIX/lib/pkgconfig";
+    #ls -l $PREFIX/lib/pkgconfig/boost*;
+    #cat $PREFIX/lib/pkgconfig/boost*;
+    #cat $PREFIX/lib/pkgconfig/RNAlib*;
+    #echo "otool"
+    #otool -L $PREFIX/lib/libboost_program_options.dylib
+    #echo;
+    MACOSX_VERSION_MIN=10.6
+    extra_config_options="CXXFLAGS=\"-stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN}\" LDFLAGS=\"-stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN} -L${LIBRARY_PATH}\""
+#    extra_config_options="CC=clang CXX=clang++ CXXFLAGS=\"-stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN}\" LDFLAGS=\"-stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN} -L${LIBRARY_PATH}\""
 else ## linux
     # add -fopenmp to compilation due to viennarna setup
     extra_config_options="CXXFLAGS=-fopenmp"
