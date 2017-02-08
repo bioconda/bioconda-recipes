00ea29967822197c9e2b8edf815140e8167c806b
Martin Mann <martin-mann@users.noreply.github.com>
2017-02-08 09:16:59 +0100

    otools enabled to check boost 1.60

--- recipes/intarna/build.sh
+++ recipes/intarna/build.sh
@@ -14,18 +14,18 @@ if [ `uname` == Darwin ] ; then
     #ls -l $PREFIX/lib/pkgconfig/boost*;
     #cat $PREFIX/lib/pkgconfig/boost*;
     #cat $PREFIX/lib/pkgconfig/RNAlib*;
-    #echo "otool"
-    #otool -L $PREFIX/lib/libboost_program_options.dylib
+    echo "otool"
+    otool -L $PREFIX/lib/libboost_program_options.dylib
     #echo;
     #MACOSX_VERSION_MIN=10.6
     CC=clang
     CXX=clang++
     #CC=gcc
     #CXX=g++
-    CXXFLAGS="${CXXFLAGS} -stdlib=libstdc++"
-    LDFLAGS="${LDFLAGS} -stdlib=libstdc++ -L${PREFIX}/lib"
-    #CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    #LDFLAGS="${LDFLAGS} -stdlib=libc++ -mmacosx-version-min=${MACOSX_VERSION_MIN} -L${PREFIX}/lib"
+    #CXXFLAGS="${CXXFLAGS} -stdlib=libstdc++"
+    #LDFLAGS="${LDFLAGS} -stdlib=libstdc++ -L${PREFIX}/lib"
+    CXXFLAGS="${CXXFLAGS} -stdlib=libc++"# -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    LDFLAGS="${LDFLAGS} -stdlib=libc++"# -mmacosx-version-min=${MACOSX_VERSION_MIN} -L${PREFIX}/lib"
 else ## linux
     # add -fopenmp to compilation due to viennarna setup
     CC=gcc
