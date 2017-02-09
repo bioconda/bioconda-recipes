b799aaac017e6f855f912c2ad38ac4a45f427363
Thomas Wollmann <thomas.s.wollmann@gmail.com>
2017-02-09 12:11:49 +0100

    changed file encoding

--- recipes/python-omero/setup.py
+++ recipes/python-omero/setup.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 import os
 
 setup(name='Omero Python',
-      version=os.environ['OMERO_VERSION'],
-      description='OME (Open Microscopy Environment) develops open-source software and data format standards for the storage and manipulation of biological light microscopy data.',
-      url='http://www.openmicroscopy.org/',
-      packages=['omero', 'omeroweb', 'pipeline', 'omero_ext'],
-     )
+    version=os.environ['OMERO_VERSION'],
+    description='OME (Open Microscopy Environment) develops open-source software and data format standards for the storage and manipulation of biological light microscopy data.',
+    url='http://www.openmicroscopy.org/',
+    packages=['omero', 'omeroweb', 'pipeline', 'omero_ext']
+)
