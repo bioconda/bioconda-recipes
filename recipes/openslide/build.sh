99995241a049398551cd0da6d9683b363d134faa
Björn Grüning <bjoern@gruenings.eu>
2017-02-09 13:18:43 +0100

    Update build.sh

--- recipes/openslide/build.sh
+++ recipes/openslide/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# fix autoconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
+
 autoreconf -i
 ./configure --prefix=${PREFIX} CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" 
 make -j${CPU_COUNT}
