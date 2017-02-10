bccb1bc459e46b6c1134e9c75a48d9bfc89d53c5
Karel Brinda <karel.brinda@gmail.com>
2017-02-10 14:24:01 -0500

    Typo

--- recipes/nanosim/build.sh
+++ recipes/nanosim/build.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 
 mkdir -p ${PREFIX}/bin
-cp *.py ${PREFIX}/bin/
+cp src/*.py ${PREFIX}/bin/
 
 chmod 0755 "${PREFIX}/bin/read_analysis.py"
 chmod 0755 "${PREFIX}/bin/simulator.py"
