2486edad028cfd450453483f042c70d0eb71fba2
yhoogstrate <y.hoogstrate@gmail.com>
2017-02-16 11:50:26 +0100

    Segmentation-fold: pinning GCC seems to make it work

--- recipes/segmentation-fold/build.sh
+++ recipes/segmentation-fold/build.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
-make clean
-cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
+#cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
+#make clean
+cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX .
 
-#make
-#make install
+make
+make install
 
 cd scripts/energy-estimation-utility
 
-# setup_requires breaks stuff
+# 'setup_requires' and 'install_requires' break setuptools stuff
 sed -i.bak -E "s/setup_requires.+\],//" setup.py
-sed -i.bak -E "s/install_requires.+\],//" setup.py
 
-cat setup.py > /tmp/setup.py
+mv setup.py setup.py.bak ; grep -v numpy setup.py.bak > setup.py
+mv setup.py setup.py.bak ; grep -v HTSeq setup.py.bak > setup.py
+mv setup.py setup.py.bak ; grep -v pysam setup.py.bak > setup.py
+mv setup.py setup.py.bak ; grep -v click setup.py.bak > setup.py
+mv setup.py setup.py.bak ; grep -v install_requires setup.py.bak > setup.py
+mv setup.py setup.py.bak ; grep -v -E '^\s+\],$' setup.py.bak > setup.py
 
 $PYTHON setup.py install
