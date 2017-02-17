aef37901fd14f6a277f4cb21f320262ad89cd3db
Nick <nmapsy@gmail.com>
2017-02-17 13:15:48 -0500

    Du Novo: Fix some dependencies, place .so and .py files in proper directories.

--- recipes/dunovo/build.sh
+++ recipes/dunovo/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 make
+mkdir $PREFIX/include/dunovo
+cp *.py *.so $PREFIX/include/dunovo
+ln -s ../include/dunovo/correct.py $PREFIX/bin
+ln -s ../include/dunovo/align_families.py $PREFIX/bin
+ln -s ../include/dunovo/dunovo.py $PREFIX/bin
