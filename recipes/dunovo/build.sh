b86379fef2a500481534c60ea723ffed6f3026fb
Nick <nmapsy@gmail.com>
2017-02-18 05:07:25 -0500

    Du Novo: Update to 0.6.2, use more standard directory structure.

--- recipes/dunovo/build.sh
+++ recipes/dunovo/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 make
-mkdir $PREFIX/include/dunovo
-cp *.py *.so $PREFIX/include/dunovo
-ln -s ../include/dunovo/correct.py $PREFIX/bin
-ln -s ../include/dunovo/align_families.py $PREFIX/bin
-ln -s ../include/dunovo/dunovo.py $PREFIX/bin
+mv *.so $PREFIX/lib
+mv *.py *.sh $PREFIX/bin
