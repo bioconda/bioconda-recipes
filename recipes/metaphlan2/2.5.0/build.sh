c3662357d7dc2a409c53f25e99dd1b3fb1ff96c6
Bérénice Batut <bebatut@gmail.com>
2017-02-18 10:16:06 +0100

    Add script for db download in 2.5.0 version

--- recipes/metaphlan2/2.5.0/build.sh
+++ recipes/metaphlan2/2.5.0/build.sh
@@ -7,5 +7,5 @@ utils/metaphlan2krona.py
 
 mkdir -p $PREFIX/bin
 for i in $binaries; do cp $SRC_DIR/$i $PREFIX/bin; done
-
+cp "$RECIPE_DIR/download_metaphlan2_db.py" $PREFIX/bin
 #cp -rf db_v20 $PREFIX/bin/db_v20
---
+++ recipes/metaphlan2/2.5.0/download_metaphlan2_db.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import argparse
+import tarfile
+import os
+import urllib2
+import shutil
+
+
+METAPHLAN2_URL = 'https://bitbucket.org/biobakery/metaphlan2/get/2.6.0.tar.gz'
+
+
+def download_file(url):
+    """Download a file from a URL
+    Fetches a file from the specified URL.
+    Returns the name that the file is saved with.
+    """
+    print("Downloading %s" % url)
+    target = os.path.basename(url)
+    print("Saving to %s" % target)
+    open(target, 'wb').write(urllib2.urlopen(url).read())
+    return target
+
+
+def unpack_tar_archive(filen, wd=None):
+    """Extract files from a TAR archive
+    Given a TAR archive (which optionally can be
+    compressed with either gzip or bz2), extract the
+    files it contains and return a list of the
+    resulting file names and paths.
+    'wd' specifies the working directory to extract
+    the files to, otherwise they are extracted to the
+    current working directory.
+    Once all the files are extracted the TAR archive
+    file is deleted from the file system.
+    """
+    file_list = []
+    if not tarfile.is_tarfile(filen):
+        print("%s: not TAR file")
+        return [filen]
+    t = tarfile.open(filen)
+    t.extractall(".")
+    print("Removing %s" % filen)
+    os.remove(filen)
+    return file_list
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Download MetaPhlAn2 database')
+    parser.add_argument('--output', help="Installation directory")
+    args = parser.parse_args()
+
+    if args.output:
+        output = args.output
+    else:
+        output = os.path.dirname(os.path.realpath(__file__))
+        print(output)
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    metaphlan2_tarfile = download_file(METAPHLAN2_URL)
+    file_list = unpack_tar_archive(metaphlan2_tarfile)
+    print(file_list)
+
+    shutil.move("biobakery-metaphlan2-c43e40a443ed/db_v20", output)
