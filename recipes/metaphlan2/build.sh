73578d5b71b16059b1c574db2298bc352cad6c15
Bérénice Batut <bebatut@gmail.com>
2017-02-18 09:55:29 +0100

    Add script to download db

--- recipes/metaphlan2/build.sh
+++ recipes/metaphlan2/build.sh
@@ -13,6 +13,6 @@ utils/plot_bug.py
 mkdir -p $PREFIX/bin
 for i in $binaries; do cp $i $PREFIX/bin; done
 cp -r strainphlan_src $PREFIX/bin
-
+cp "$RECIPE_DIR/download_metaphlan2_db.py" $PREFIX/bin
 
 #cp -rf db_v20 $PREFIX/bin/db_v20
