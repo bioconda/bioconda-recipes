5249f311da9ef1ae44aaae5b4aef33dc492ea79b
Ajay Panyala <ajay.panyala@gmail.com>
2017-02-18 08:09:25 -0800

    Change travis setup to use anaconda instead of miniconda

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -22,11 +22,11 @@ else
 fi
 
 # install conda
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-$tag-x86_64.sh
-sudo bash Miniconda3-latest-$tag-x86_64.sh -b -p /anaconda
+curl -O https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.3.0-Linux-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
-conda install -y conda=4.2.15
+#conda install -y conda=4.2.15
 
 $SCRIPT_DIR/../simulate-travis.py --set-channel-order
 $SCRIPT_DIR/../simulate-travis.py --install-requirements
