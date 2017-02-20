c588b22004eeeb831f62b4743a3a54b3528f112a
Bérénice Batut <bebatut@gmail.com>
2017-02-20 18:05:42 +0100

    Try new sed

--- recipes/wgs-assembler/build.sh
+++ recipes/wgs-assembler/build.sh
@@ -4,7 +4,7 @@ export LANG=C
 cd $SRC_DIR/src
 export KMER=$PREFIX
 sed -i.bak 's/$(LOCAL_OS)/$(PREFIX)/' c_make.gen
-sed -i.bak 's/getBinDirectory()/`which gatekeeper`/' AS_RUN/runCA.pl
 make
 cd ../
+sed -i.bak 's/getBinDirectory()/`which gatekeeper`/' ${PREFIX}/bin/runCA
 sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g' ${PREFIX}/bin/runCA
