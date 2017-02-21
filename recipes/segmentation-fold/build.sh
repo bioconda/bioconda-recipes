06a746c1407c2f0060d06fd0e26a5f23d71eb004
yhoogstrate <y.hoogstrate@gmail.com>
2017-02-21 13:09:44 +0100

    Trying to use osx specific linking flag

--- recipes/segmentation-fold/build.sh
+++ recipes/segmentation-fold/build.sh
@@ -3,13 +3,14 @@
 #cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
 #make clean
 
-if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-	suffix=' -DBOOST_NO_CXX11_RVALUE_REFERENCES';# workaround for weird glitch
-else
-	suffix=' ';
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Compiling segmentation-fold for OSX."
+    cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DBOOST_NO_CXX11_RVALUE_REFERENCES $suffix .
+else 
+    echo "Compiling segmentation-fold for UNIX/Linux."
+    cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON $suffix .
 fi
 
-cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON $suffix .
 
 make VERBOSE=1
 make install
