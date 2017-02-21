c47b48e112b0c9c38e70675d2515cc3bf720eec5
Mataivic <victor.mataigne@wanadoo.fr>
2017-02-21 10:24:55 +0100

    corrected build.sh

--- recipes/blastalign/build.sh
+++ recipes/blastalign/build.sh
@@ -3,8 +3,4 @@
 mkdir -p $PREFIX/bin
 cp BlastAlign* $PREFIX/bin
 
-<<<<<<< HEAD
 #sed -i "s/perl/env perl/" $PREFIX/bin
-=======
-sed -i "s/perl/env perl/" $PREFIX/bin
->>>>>>> 2dd04dd6bcd3a0c74b17d6b6489998b1d53c8a76
