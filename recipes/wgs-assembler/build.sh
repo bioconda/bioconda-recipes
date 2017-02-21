e696061ad52cf3999af7f18d03c9291ce397ea40
Bérénice Batut <bebatut@gmail.com>
2017-02-21 08:12:14 +0100

    Try to find gatekeeper

--- recipes/wgs-assembler/build.sh
+++ recipes/wgs-assembler/build.sh
@@ -6,5 +6,6 @@ export KMER=$PREFIX
 sed -i.bak 's/$(LOCAL_OS)/$(PREFIX)/' c_make.gen
 make
 cd ../
-sed -i.bak 's/getBinDirectory()/`which gatekeeper`/' ${PREFIX}/bin/runCA
+sed -i.bak "s/FileHandle;$/&\\nuse File::Basename;/" ${PREFIX}/bin/runCA
+sed -i.bak 's/getBinDirectory()/dirname(__FILE__)/' ${PREFIX}/bin/runCA
 sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g' ${PREFIX}/bin/runCA
