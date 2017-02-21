80357c2b902f5c62c0ea2e664e105d38988b79b3
yhoogstrate <y.hoogstrate@gmail.com>
2017-02-21 11:20:57 +0100

    attempt to get boost working with std::forward - additional cmake flag

--- recipes/segmentation-fold/build.sh
+++ recipes/segmentation-fold/build.sh
@@ -2,7 +2,14 @@
 
 #cmake -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$PREFIX .
 #make clean
-cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
+
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+	suffix=' -DBOOST_NO_CXX11_RVALUE_REFERENCES';# workaround for weird glitch
+else
+	suffix=' ';
+fi
+
+cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON $suffix .
 
 make VERBOSE=1
 make install
