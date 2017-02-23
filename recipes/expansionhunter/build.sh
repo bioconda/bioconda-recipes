8c2a7f5632ac2b123aaded300e41b8b5cd1a3f65
Maarten <maarten-k@users.noreply.github.com>
2017-02-23 16:07:47 +0100

    Update build.sh

--- recipes/expansionhunter/build.sh
+++ recipes/expansionhunter/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
-
-#cd ExpansionHunter
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
 make
-mv ExpansionHunter $PREFIX/bin
+mv ExpansionHunter ${PREFIX}/bin/.
