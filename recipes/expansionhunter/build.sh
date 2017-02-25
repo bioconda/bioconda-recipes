ade6e7c68ac5fc28581c0ba6f8ab5ffab61cbbf6
maarten-k <maarten.kooyman@surfsara.nl>
2017-02-25 16:53:51 +0100

    trying to fixing osx .... again

--- recipes/expansionhunter/build.sh
+++ recipes/expansionhunter/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
-mkdir build
+mkdir -p build
 cd build
+if [ "$(uname)" == "Darwin" ]; then
+export CXX_FLAGS='-std=c++11 -stdlib=libc++'
+export CXXFLAGS="${CXXFLAGS}  -std=c++11 -stdlib=libc++"
+export CMAKE_CXX_FLAGS='-stdlib=libc++'
+export LDFLAGS=' -stdlib=libc++'
+export LD_FLAGS=' -stdlib=libc++'
+export CMAKE_LDFLAGS=' -stdlib=libc++'
+fi
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
-make
+make VERBOSE=1
 mv ExpansionHunter ${PREFIX}/bin/.
