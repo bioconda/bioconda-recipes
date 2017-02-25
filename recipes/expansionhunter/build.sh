d08e60a0eaa8133aacad100a5a0ffeff190b597b
maarten-k <maarten.kooyman@surfsara.nl>
2017-02-25 23:44:16 +0100

    test back to original state

--- recipes/expansionhunter/build.sh
+++ recipes/expansionhunter/build.sh
@@ -4,12 +4,12 @@ mkdir -p build
 cd build
 if [ "$(uname)" == "Darwin" ]; then
 export CXX_FLAGS='-std=c++11 -stdlib=libc++'
-export CXXFLAGS="${CXXFLAGS}  -std=c++11 -stdlib=libc++"
+export CXXFLAGS="-std=c++11 -stdlib=libc++"
 export CMAKE_CXX_FLAGS='-stdlib=libc++'
-export LDFLAGS=' -stdlib=libc++'
-export LD_FLAGS=' -stdlib=libc++'
-export CMAKE_LDFLAGS=' -stdlib=libc++'
+export LDFLAGS='-stdlib=libc++'
+export LD_FLAGS='-stdlib=libc++'
+export CMAKE_LDFLAGS='-stdlib=libc++'
 fi
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
-make VERBOSE=1
+cmake  -DBoost_DEBUG=ON .. #-DBOOST_ROOT=${PREFIX}
+make VERBOSE=1 
 mv ExpansionHunter ${PREFIX}/bin/.
