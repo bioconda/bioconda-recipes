ff623b8c927d746d820763dc0d5784e0b23e27a8
Bérénice Batut <bebatut@gmail.com>
2017-02-26 18:09:38 +0100

    Add build script

---
+++ recipes/intemap/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd src/mergeassembly
+make
+cd ../../
+make
+
+mkdir -p $PREFIX/bin
+cp *.py $PREFIX/bin
+cp *.sh $PREFIX/bin
+
+cp ctgvalidate $PREFIX/bin
+cp extract_fastqread $PREFIX/bin
+
+binaries="\
+ctgvalidate \
+extract_fastqread \
+fastatrans \
+fastqtofasta \
+FilterCtgCov \
+mergeassembly \
+mergefastq-p \
+rfheader \
+SCFtoCTG
+"
+for i in $binaries; do cp $i $PREFIX/bin; done
