f529d067db8c7074d4cabb18b129a7d53e2df5c3
Bérénice Batut <bebatut@gmail.com>
2017-02-27 15:58:11 +0100

    Test with cmake

--- recipes/ray/build.sh
+++ recipes/ray/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
+
+mkdir build
+cd build
 export MPICXX=$PREFIX/bin
-make 
-make install
-chmod +x Ray
+cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX/bin ..
+make
+ls
+#make install
+#chmod +x Ray
 cp Ray $PREFIX/bin
-ls $PREFIX
+ls $PREFIX/
+ls $PREFIX/bin
