2ca8013a1096d649ae82b1698d5a1b9e74705a16
Adam Caprez <acaprez2@unl.edu>
2017-03-02 23:08:09 +0000

    Add recipe for soapdenovo-trans 1.04.

---
+++ recipes/soapdenovo-trans/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LD_LIBRARY_PATH=${PREFIX}/lib
+
+mkdir -p ${PREFIX}/bin
+sh make.sh
+cp ${SRC_DIR}/{SOAPdenovo-Trans-127mer,SOAPdenovo-Trans-31mer} ${PREFIX}/bin
