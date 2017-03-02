f1bd61a84332339523f11f31c33c4444ba690102
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-03-02 09:40:28 +0400

    stupid sed

--- recipes/perl-crypt-openssl-random/0.11/build.sh
+++ recipes/perl-crypt-openssl-random/0.11/build.sh
@@ -7,8 +7,6 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export LD_RUN_PATH="${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-export CFLAGS="-I${PREFIX}/include"
-export CCFLAGS="-I${PREFIX}/include ${CCFLAGS}"
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
@@ -25,7 +23,7 @@ elif [ -f Makefile.PL ]; then
     sed -i.bak 's|-L/usr/local/lib|-L${PREFIX}/lib|g' Makefile
     sed -i.bak 's|LD_RUN_PATH = /usr/lib/../lib64|LD_RUN_PATH = ${PREFIX}/lib|g' Makefile
     sed -i.bak 's|LD_RUN_PATH = /usr/lib64|LD_RUN_PATH = ${PREFIX}/lib|g' Makefile
-    #sed -i.bak 's|-I/usr/local/include|-I${PREFIX}/include|g' Makefile
+    sed -i.bak 's|-I/usr/local/include|-I${PREFIX}/include|g' Makefile
     make
     make test
     make install
