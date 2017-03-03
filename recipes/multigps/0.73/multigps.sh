97d8584863606558a42d9971df2b5a2ad5a02b5c
Greg Von Kuster <ghv2@psu.edu>
2017-03-03 08:30:59 -0500

    Change text to force build

--- recipes/multigps/0.73/multigps.sh
+++ recipes/multigps/0.73/multigps.sh
@@ -2,7 +2,7 @@
 # Wraps MultiGPS tool
 set -o pipefail
 
-# Find original directory of bash script, resolving symlinks
+# Find original directory of bash script, resolving symlinks.
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
