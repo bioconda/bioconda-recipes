6729eff4d553767699487b6c774c8e1e58009ee7
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-03-03 16:44:19 +0100

    Upgrade for lastz, include lastz_32 binary

--- recipes/lastz/build.sh
+++ recipes/lastz/build.sh
@@ -2,11 +2,14 @@
 
 mkdir -p $PREFIX/bin
 
-make 
+make
+make lastz_32
 
-chmod +x  src/lastz
-chmod +x  src/lastz
+chmod +x src/lastz
+chmod +x src/lastz_D
+chmod +x src/lastz_32
 
 mv src/lastz $PREFIX/bin
 mv src/lastz_D $PREFIX/bin
+mv src/lastz_32 $PREFIX/bin
 
