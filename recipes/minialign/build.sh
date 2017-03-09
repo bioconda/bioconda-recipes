88b603da81a8a247c0a2178cfee1137fc28ff251
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-03-09 15:03:29 +0100

    Adding @ocxtal's patch

--- recipes/minialign/build.sh
+++ recipes/minialign/build.sh
@@ -4,7 +4,7 @@ export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
-sed -i -e 's/march=native/march=corei7/g' Makefile
+sed -i -e 's/-march=native/-msse4.2 -mpopcnt/g' Makefile
 make
 make install PREFIX=$PREFIX
 
