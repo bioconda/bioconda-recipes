27f9ecda22918b5294f658498916060ddd15cf82
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-03-09 15:33:48 +0100

    Adding kenttools v347

---
+++ recipes/kenttools/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup path variables
+BINARY_HOME=$PREFIX/bin
+PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+# Create destination directories
+mkdir -p $BINARY_HOME
+mkdir -p $PACKAGE_HOME
+
+# Build and install
+make
+mv bin/calc bin/kent-tools-calc
+cp bin/* $PREFIX/bin
