071b7fdc4bb74f21a92eb8327846748b7bea3b4d
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-03-10 18:50:27 +0000

    tweak recipe

--- recipes/anaquin/build.sh
+++ recipes/anaquin/build.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
+set -eu -o pipefail
 
-sed -i.bak 's/^CPPFLAGS =$//g' Makefile
-sed -i.bak 's/^LDFLAGS  =$//g' Makefile
+# download and copy required dependencies
+# klib
+wget https://github.com/attractivechaos/klib/archive/spawn-final.tar.gz
+tar xzf spawn-final.tar.gz
+cp -r $SRC_DIR/klib-spawn-final $PREFIX/lib/klib/
 
-# varfilter.py in install fails because we don't install Python
-sed -i.bak 's#misc/varfilter.py##g' Makefile
+# vcflib
+wget https://github.com/student-t/vcflib-forAnaquin/archive/v1.0.0-rc1.tar.gz
+tar xzf v1.0.0-rc1.tar.gz
+cp -r $SRC_DIR/vcflib-forAnaquin-1.0.0-rc1/src $PREFIX/lib/vcflib/
 
-# Remove rdynamic which can cause build issues on OSX
-# https://sourceforge.net/p/samtools/mailman/message/34699333/
-sed -i.bak 's/ -rdynamic//g' Makefile
+# clean Makefile
+sed -i'' -e 's/^BOOST.*//g' Makefile
+sed -i'' -e 's/^EIGEN.*//g' Makefile
+sed -i'' -e 's/^KLIB.*//g' Makefile
+sed -i'' -e 's/^VCFLIB.*//g' Makefile
+sed -i'' -e 's/\/include//g' Makefile
+sed -i'' -e 's/-DBACKWARD_HAS_BFD//g' Makefile
+sed -i'' -e 's/^INCLUDE.*//g' Makefile
 
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-./configure --prefix=$PREFIX --enable-libcurl --without-curses
-make install prefix=$PREFIX LIBS+=-lcrypto LIBS+=-lcurl
+export INCLUDE="$SRC_DIR/src"
+export BOOST="$PREFIX/include/boost"
+export EIGEN="$PREFIX/include/eigen3"
+export KLIB="$PREFIX/lib"
+export VCFLIB="$PREFIX/lib"
+
+make
+mkdir -p $PREFIX/bin
+cp anaquin $PREFIX/bin
+
