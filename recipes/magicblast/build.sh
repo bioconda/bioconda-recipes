3586dd2bc230c527574b27edb7e7275391673df4
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-03-10 17:31:47 -0500

    copy binaries rather than compile

--- recipes/magicblast/build.sh
+++ recipes/magicblast/build.sh
@@ -4,20 +4,6 @@ set -e -x -o pipefail
 
 mkdir -p $PREFIX/bin/
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Platform: Mac"
-
-    cp $SRC_DIR/bin/* $PREFIX/bin/
-
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Platform: Linux"
-
-    cd $SRC_DIR/c++/
-    ./configure --prefix=$PREFIX
-
-    make -j${CPU_COUNT}
-
-    cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
-fi
-
-chmod +x $PREFIX/bin/*
+chmod +x $SRC_DIR/bin/*
+mv $SRC_DIR/bin/makeblastdb $SRC_DIR/bin/magicblast-makeblastdb
+cp $SRC_DIR/bin/* $PREFIX/bin/
