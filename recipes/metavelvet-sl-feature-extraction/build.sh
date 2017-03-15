a1d2669cb246be4945864c19424ccc5eb0bc7d6a
Bérénice Batut <bebatut@gmail.com>
2017-03-15 15:35:46 +0100

    Remove C++ flags and fix find for osx

--- recipes/metavelvet-sl-feature-extraction/build.sh
+++ recipes/metavelvet-sl-feature-extraction/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -x -e
 
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
-
-find -name "*pm~" -delete
+find . -name "*pm~" -delete
 
 mkdir -p perl-build
 mv FeatureExtract.perl perl-build/FeatureExtract.pl
