7137f84f381a06640816c76cdc15dccdc28e642b
Björn Grüning <bjoern@gruenings.eu>
2017-03-16 08:21:41 +0100

    Test inside build-env

--- recipes/prokka/build.sh
+++ recipes/prokka/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-sleep 5
-
 ./bin/prokka --setupdb
 mkdir -p "${PREFIX}/bin" "${PREFIX}/db" "${PREFIX}/share/doc/prokka"
 mv bin/* "${PREFIX}/bin/"
 mv db/* "${PREFIX}/db/"
 mv doc/* "${PREFIX}/share/doc/prokka/"
+
+prokka --listdb
+prokka --version
