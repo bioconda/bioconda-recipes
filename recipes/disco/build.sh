82d3d7b77f45216d5ea5f015d7073af1a4e2d945
Bérénice Batut <bebatut@gmail.com>
2017-03-16 15:26:29 +0100

    Fix the build

--- recipes/disco/build.sh
+++ recipes/disco/build.sh
@@ -5,10 +5,10 @@ make all
 
 mkdir -p $PREFIX/bin
 
-tar xzf Disco_omp_x86-Linux.tar.gz
+ls
+tar xzf Disco_All_x86-Linux.tar.gz
 cp Disco/buildG* $PREFIX/bin
 cp Disco/fullsimplify $PREFIX/bin
 cp Disco/parsimplify $PREFIX/bin
 cp Disco/disco* $PREFIX/bin
 cp Disco/run* $PREFIX/bin
-cp -r Disco/bbmap $PREFIX/bin
