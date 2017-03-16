12abca60b145d0991d3e1f06393d617696a5983d
Bérénice Batut <bebatut@gmail.com>
2017-03-16 17:26:59 +0100

    Change mpic++ for osx

--- recipes/disco/build.sh
+++ recipes/disco/build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [ `uname` == Darwin ]; then
+    if mpic++ --show me | grep -q "clang++"; then
+        # the openmpi package (and particularly the mpic++) from conda-forge is 
+        # compiled to use clang++ (despl)
+        # and the current package need an openmpi version based on gcc to use the
+        # -fopenmpi version
+        sed -i.bak "s|mpic++|g++ -I$PREFIX/include -L$PREFIX/lib -lmpi_cxx -lmpi|g" compiler.mk
+    fi
+fi
+
 make clean
 make all
 
 mkdir -p $PREFIX/bin
 
-ls
 tar xzf Disco_All_x86-Linux.tar.gz
 cp Disco/buildG* $PREFIX/bin
 cp Disco/fullsimplify $PREFIX/bin
