407492cfba59a5f3aa3200c51f961da24a97dee5
ThomasWollmann <ThomasWollmann@users.noreply.github.com>
2017-03-16 15:07:25 +0100

    Update setup.py

--- recipes/python-omero/setup.py
+++ recipes/python-omero/setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
 from distutils.core import setup
-import os
 
-setup(name='Omero Python', version=os.environ['OMERO_VERSION'], description='OME (Open Microscopy Environment) develops open-source software and data format standards for the storage and manipulation of biological light microscopy data.', url='http://www.openmicroscopy.org/', packages=['omero', 'omeroweb', 'pipeline', 'omero_ext'])
+setup(name='Omero Python', version='5.2.7', description='OME (Open Microscopy Environment) develops open-source software and data format standards for the storage and manipulation of biological light microscopy data.', url='http://www.openmicroscopy.org/', packages=['omero', 'omeroweb', 'pipeline', 'omero_ext'])
