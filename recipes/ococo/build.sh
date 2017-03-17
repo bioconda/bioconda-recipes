5df929fe5d25119bd7ea2931fe361594592ca392
Karel Břinda <karel.brinda@gmail.com>
2017-03-17 11:47:13 -0400

    Fix copying manpage

--- recipes/ococo/build.sh
+++ recipes/ococo/build.sh
@@ -24,6 +24,7 @@ export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
 make VERBOSE=1
 mkdir -p ${PREFIX}/bin
-cp ococo ${PREFIX}/bin
-cp ococo.1 ${PREFIX}/share/man/man1
+cp ococo ${PREFIX}/bin/
+mkdir -p ${PREFIX}/share/man/man1
+cp ococo.1 ${PREFIX}/share/man/man1/
 
