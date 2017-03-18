07f944a30256fca1c12031d20d1fac2d371a9ea9
ThomasWollmann <ThomasWollmann@users.noreply.github.com>
2017-03-18 12:14:09 +0100

    Update build.sh

--- recipes/asap/build.sh
+++ recipes/asap/build.sh
@@ -3,7 +3,7 @@ mkdir build
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="-L${PREFIX}/lib -lm"
+export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
 cmake \
