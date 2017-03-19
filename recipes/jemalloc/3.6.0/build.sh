5404ef28a5bdc4a055ffb9e74a0f78b36e547037
Rob Patro <rob@newton>
2017-03-19 16:09:26 -0400

    bump jemalloc version

--- recipes/jemalloc/3.6.0/build.sh
+++ recipes/jemalloc/3.6.0/build.sh
@@ -9,6 +9,6 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --disable-debug --prefix="$PREFIX"
+./configure --prefix="$PREFIX"
 make
 make install
