8993c917b1a2c6bc2135b3045e7dc4065649d775
ThomasWollmann <ThomasWollmann@users.noreply.github.com>
2017-03-19 09:08:10 +0100

    Update build.sh

--- recipes/syntheticwsi/build.sh
+++ recipes/syntheticwsi/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-ant -buildfile syntheticwsi.xml
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
+ant -buildfile syntheticwsi.xml
 cp -R * $outdir/
 cp $RECIPE_DIR/syntheticwsi.py $outdir/syntheticwsi
 ls -l $outdir
