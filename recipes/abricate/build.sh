6b13a9f720f05e5ccabac93f73a339637389bac9
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-03-20 21:02:49 +0100

    Clean up recipe

--- recipes/abricate/build.sh
+++ recipes/abricate/build.sh
@@ -6,6 +6,13 @@ export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
-cp -r * $PREFIX
+#cp -r * $PREFIX
 
+# install in opt/
+mkdir -p $PREFIX/opt/
+cp -r $SRC_DIR $PREFIX/opt/$PKG_NAME
 
+ln -s $PREFIX/opt/$PKG_NAME/bin/abricate $PREFIX/bin/
+ln -s $PREFIX/opt/$PKG_NAME/bin/abricate-get_db $PREFIX/bin/
+ln -s $PREFIX/opt/$PKG_NAME/scripts/abricate-fix_resfinder_fasta $PREFIX/bin/
+ln -s $PREFIX/opt/$PKG_NAME/scripts/abricate-update_db $PREFIX/bin/
