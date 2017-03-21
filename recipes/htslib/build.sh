60d12507f90b1de4d0997cda6533bd840d425ae2
chapmanb <chapmanb@50mail.com>
2017-03-21 13:31:47 -0400

    Update: htslib with libhts.so.1 symlink
    Provides back-compatibility. Fixes chapmanb/bcbio-nextgen#1873

--- recipes/htslib/build.sh
+++ recipes/htslib/build.sh
@@ -2,3 +2,6 @@
 
 ./configure --prefix=$PREFIX --enable-libcurl CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make install prefix=$PREFIX
+# Add back-compatible links for htslib.so
+cd $PREFIX/lib
+ln -s libhts.so libhts.so.1
