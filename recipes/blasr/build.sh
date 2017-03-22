9c4c9cb4e56531ba788f92d0cd81f2ee28475566
Bérénice Batut <bebatut@gmail.com>
2017-03-22 16:36:59 +0100

    Add missing tools

--- recipes/blasr/build.sh
+++ recipes/blasr/build.sh
@@ -9,6 +9,15 @@ export HDF5_LIB=$PREFIX/lib
 make configure-submodule
 
 make build-submodule
-make blasr
+#make blasr
+make
 cp blasr $PREFIX/bin
+
+cp utils/loadPulses $PREFIX/bin
+cp utils/pls2fasta $PREFIX/bin
+cp utils/samtoh5 $PREFIX/bin
+cp utils/samtom4 $PREFIX/bin
+cp utils/samFilter $PREFIX/bin
+cp utils/sawriter $PREFIX/bin
+cp utils/sdpMatcher  $PREFIX/bin
 
