db2830fdb5c5b591427d6ebd467d0313a6dc0d45
Michal Lorenc <mictadlo@gmail.com>
2017-03-30 19:39:29 +1000

    add wrapper

--- recipes/pbsuite/build.sh
+++ recipes/pbsuite/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+PACKAGE_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+
+mkdir -p $PACKAGE_HOME
 mkdir -p $PREFIX/bin
-cp bin/*.py $PREFIX/bin/
+
+cp -r * $PACKAGE_HOME
+
+
+WRAPPER=$PREFIX/bin/Jelly
+echo "#!/bin/sh" > $WRAPPER
+echo "$PACKAGE_HOME/bin/Jelly.py \$@" >> $WRAPPER
+chmod +x $WRAPPER
