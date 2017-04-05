1deeba6f9224c7e3db2fd5bf263938b240dadf45
Dave B <dave@bx.psu.edu>
2017-04-05 10:35:47 -0400

    Build additional object files used by GffCompare

--- recipes/gclib/build.sh
+++ recipes/gclib/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/lib $PREFIX/bin
+mkdir -p $PREFIX/lib $PREFIX/bin $PREFIX/include
 make
-install *.o $PREFIX/lib/
+install *.o -v $PREFIX/lib/
+install *.h -v $PREFIX/include/
 install gtest $PREFIX/bin/
 install threads $PREFIX/bin/
-
