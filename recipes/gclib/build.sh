fb20ed424126ef2c341c1f6537dea66e1b0c9c1a
Dave B <dave@bx.psu.edu>
2017-04-05 12:44:20 -0400

    Also build a shared library.

--- recipes/gclib/build.sh
+++ recipes/gclib/build.sh
@@ -3,6 +3,7 @@
 mkdir -p $PREFIX/lib $PREFIX/bin $PREFIX/include
 make
 install *.o $PREFIX/lib/
+install *.so $PREFIX/lib/
 install *.h $PREFIX/include/
 install gtest $PREFIX/bin/
 install threads $PREFIX/bin/
