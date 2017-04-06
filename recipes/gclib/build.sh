b7d90d8d3e1e726123b1c9d4281069d1d3d15dc7
Dave B <dave@bx.psu.edu>
2017-04-06 15:29:35 -0400

    Build gclib correctly on osx.

--- recipes/gclib/build.sh
+++ recipes/gclib/build.sh
@@ -3,7 +3,12 @@
 mkdir -p $PREFIX/lib $PREFIX/bin $PREFIX/include
 make
 install *.o $PREFIX/lib/
-install *.so $PREFIX/lib/
+if [ -f libgffc.so ] ; then
+    install *.so $PREFIX/lib/
+fi
+if [ -f libgffc.dylib ] ; then
+    install *.dylib $PREFIX/lib/
+fi
 install *.h $PREFIX/include/
 install *.hh $PREFIX/include/
 install gtest $PREFIX/bin/
