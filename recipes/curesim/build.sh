f1e9d730ac4a69552f9d9fe6374ac264651ee21e
Karel Břinda <karel.brinda@gmail.com>
2017-04-13 23:23:58 -0400

    Fix wrapper

--- recipes/curesim/build.sh
+++ recipes/curesim/build.sh
@@ -5,7 +5,12 @@ STRANGEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/bin
 mkdir -p $STRANGEDIR
 
+w=${STRANGEDIR}/curesim
 cp CuReSim.jar $STRANGEDIR
-cp $(dirname "$0")/curesim $STRANGEDIR
 
-(cd ${PREFIX}/bin && ln -s ${STRANGEDIR}/curesim)
+cat /dev/null > $w
+chmod +x $w
+echo "#! /usr/bin/env bash -u -f -e -o pipefail" >> $w
+echo "java -jar $(dirname "$0")/CuReSim.jar $@" >> $w
+
+(cd ${PREFIX}/bin && ln -s $w)
