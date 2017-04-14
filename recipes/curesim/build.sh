ba5e95f783a3b2f0c577d8982d0f3128ad4bd58d
Karel Břinda <kbrinda@hsph.harvard.edu>
2017-04-13 20:44:02 -0400

    Update build.sh

--- recipes/curesim/build.sh
+++ recipes/curesim/build.sh
@@ -5,6 +5,7 @@ STRANGEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/bin
 mkdir -p $STRANGEDIR
 
+unzip CuReSim1.3.zip
 cp CuReSim1.3/CuReSim.jar $STRANGEDIR
 cp curesim $STRANGEDIR
 
