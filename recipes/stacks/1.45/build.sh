0174a881b052cf5b6aa32b91fe55273daeabf918
Björn Grüning <bjoern@gruenings.eu>
2017-04-15 00:59:38 +0200

    Update build.sh

--- recipes/stacks/1.45/build.sh
+++ recipes/stacks/1.45/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export CPATH=${PREFIX}/include
+export CFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
 
 ./configure --prefix=$PREFIX --enable-sparsehash --enable-bam --includedir=${PREFIX}/include/bam --libdir=${PREFIX}/lib
 cp Makefile Makefile.bk
