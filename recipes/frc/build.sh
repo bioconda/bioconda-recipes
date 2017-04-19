c6a0a013e1db5f180b5e050bade123473641ead5
Bérénice Batut <bebatut@gmail.com>
2017-04-19 14:46:59 +0200

    Add condition to libbamtools symlink

--- recipes/frc/build.sh
+++ recipes/frc/build.sh
@@ -13,4 +13,8 @@ cd build
 cmake -DBOOST_INCLUDEDIR=${PREFIX}/include/ -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make install
 cp ../bin/* $PREFIX/bin
-ln -s $PREFIX/lib/bamtools/libbamtools.so.2.3.0 $PREFIX/lib/libbamtools.so.2.3.0
+
+if [ `uname -s` == "Linux" ];
+then
+    ln -s $PREFIX/lib/bamtools/libbamtools.so.2.3.0 $PREFIX/lib/libbamtools.so.2.3.0
+fi
