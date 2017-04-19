acceeee01b4d13e2f1c28f09359833af3070e6da
Bérénice Batut <bebatut@gmail.com>
2017-04-19 17:12:15 +0200

    Fix path to data

--- recipes/metaphyler/build.sh
+++ recipes/metaphyler/build.sh
@@ -7,8 +7,8 @@ cp markers/markers.dna $PREFIX/data
 cp markers/markers.protein $PREFIX/data
 cp markers/tid2name.tab $PREFIX/data
 
-sed -i.bak "s|Bin/markers|PREFIX/data|g" installMetaphyler.pl
-sed -i.bak "s|Bin/markers|PREFIX/data|g" runMetaphyler.pl
+sed -i.bak "s|Bin/markers|Bin/../data|g" installMetaphyler.pl
+sed -i.bak "s|Bin/markers|Bin/../data|g" runMetaphyler.pl
 
 ls *.pl | xargs sed -i.bak -e '1 s|^.*$|#!/usr/bin/env perl|g'
 cp *.pl $PREFIX/bin
