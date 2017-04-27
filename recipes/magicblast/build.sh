4837528e0c8277c6b35fb113d4f22c1671c371ee
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2017-04-27 16:42:01 -0400

    tweak build script

--- recipes/magicblast/build.sh
+++ recipes/magicblast/build.sh
@@ -5,5 +5,6 @@ set -e -x -o pipefail
 mkdir -p $PREFIX/bin/
 
 chmod +x $SRC_DIR/bin/*
-mv $SRC_DIR/bin/makeblastdb $SRC_DIR/bin/magicblast-makeblastdb
-cp $SRC_DIR/bin/* $PREFIX/bin/
+
+cp $SRC_DIR/bin/makeblastdb $PREFIX/bin/magicblast-makeblastdb
+cp $SRC_DIR/bin/magicblast $PREFIX/bin/magicblast
