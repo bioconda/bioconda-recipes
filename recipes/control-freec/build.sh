5800b7a313bd0e2d79a624a2670d99fb96bb25d3
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-05-01 12:33:27 +0200

    Fix scripts.

--- recipes/control-freec/build.sh
+++ recipes/control-freec/build.sh
@@ -6,6 +6,8 @@ mkdir -p $PREFIX/bin
 for f in scripts/*
 do
     chmod +x $f
+    pattern="s/\\/usr\\/bin\\/perl/${PREFIX//\//\\/}\\/bin\\/perl/"
+    sed -i -e $pattern $f
     cp $f $PREFIX/bin
 done
 
