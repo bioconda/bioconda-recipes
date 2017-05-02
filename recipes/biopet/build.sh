8bf8dfe29dd1be62b45075f6098a720c17ed13ff
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-05-02 14:26:51 +0200

    modified build.sh

--- recipes/biopet/build.sh
+++ recipes/biopet/build.sh
@@ -2,9 +2,11 @@
 # Build file is copied from VarScan
 set -eu -o pipefail
 
+jar_file="Biopet-0.9.0-be7838f2.jar"
+
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp VarScan*jar $outdir/VarScan.jar
-cp $RECIPE_DIR/varscan.sh $outdir/varscan
-ln -s $outdir/varscan $PREFIX/bin
+cp $jar_file $outdir/$jar_file
+cp $RECIPE_DIR/biopet.py $outdir/biopet
+ln -s $outdir/biopet $PREFIX/bin
