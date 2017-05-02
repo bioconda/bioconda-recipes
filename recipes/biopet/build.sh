dc32d0e5791d97a67c673960008924e37b9c9f84
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-05-02 14:16:47 +0200

    added biopet-0.9.0 package

---
+++ recipes/biopet/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Build file is copied from VarScan
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+cp VarScan*jar $outdir/VarScan.jar
+cp $RECIPE_DIR/varscan.sh $outdir/varscan
+ln -s $outdir/varscan $PREFIX/bin
