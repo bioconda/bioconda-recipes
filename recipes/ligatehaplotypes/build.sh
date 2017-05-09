716a80a69fe8446a0ee64911b4e4a21c470d3300
Nick Harding <njh@well.ox.ac.uk>
2017-05-09 16:04:18 +0100

    more boost edits

--- recipes/ligatehaplotypes/build.sh
+++ recipes/ligatehaplotypes/build.sh
@@ -3,8 +3,8 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
 sed -i.bak 's/\/usr\/local/${PREFIX}/' makefile
-sed -i.bak 's/\/users\/delaneau\/BOOST\/include/${BOOST_INCLUDE_DIR}/' makefile
-sed -i.bak 's/~\/BOOST/${BOOST_ROOT_DIR}/g' makefile
+sed -i.bak 's/\/users\/delaneau\/BOOST/${PREFIX}/' makefile
+sed -i.bak 's/~\/BOOST/${PREFIX}/g' makefile
 
 make cluster
 make install
