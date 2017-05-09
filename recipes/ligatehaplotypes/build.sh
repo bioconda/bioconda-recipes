d82ca76cf3e96b8c46e05f8091dc1483313fef22
Nick Harding <njh@well.ox.ac.uk>
2017-05-09 15:48:10 +0100

    Patches on makefile

--- recipes/ligatehaplotypes/build.sh
+++ recipes/ligatehaplotypes/build.sh
@@ -3,5 +3,8 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
 sed -i.bak 's/\/usr\/local/${PREFIX}/' makefile
-make static
+sed -i.bak 's/\/users\/delaneau\/BOOST\/include/${BOOST_INCLUDE_DIR}/' makefile
+sed -i.bak 's/~\/BOOST/${BOOST_ROOT_DIR}/g' makefile
+
+make cluster
 make install
