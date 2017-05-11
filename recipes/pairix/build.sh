4ce97f44a14eceef491c6f7ce879824851b8f816
Nezar <nabdennur@gmail.com>
2017-05-10 20:34:34 -0400

    Fix pairix build prefix

--- recipes/pairix/build.sh
+++ recipes/pairix/build.sh
@@ -3,6 +3,6 @@
 # Install both the pairix binaries and the Python extension module
 make
 cp bin/pairix $PREFIX/bin/pairix
-cp bin/pairs_merger $PREFIX/pairs_merger
-cp bin/streamer_1d $PREFIX/streamer_1d
+cp bin/pairs_merger $PREFIX/bin/pairs_merger
+cp bin/streamer_1d $PREFIX/bin/streamer_1d
 $PYTHON setup.py install 
