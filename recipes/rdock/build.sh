18d54c5cfe23aa92a6959c7f8d1e485cc542531f
Nicola Soranzo <nsoranzo@tiscali.it>
2017-05-11 15:11:12 +0100

    rDock fixes (#4653)
    * Fix rDock binaries needing RBT_ROOT env variable
    
    * Use pybel from openbabel

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sed -i.bak -e 's|/usr/bin/g++|g++|' build/tmakelib/linux-g++-64/tmake.conf
-sed -i.bak -e 's|^#!/usr/bin/perl.*$|#!/usr/bin/env perl|' bin/ht_protocol_finder.pl bin/run_rbdock.pl bin/run_rbscreen.pl bin/sdfield bin/sdfilter bin/sdmodify bin/sdreport bin/sdsort bin/sdsplit build/exemake build/p4utils.pl build/progen build/tmake lib/run_rbfuncs.pl
 if [ $PY3K -eq 1 ]; then
     2to3 -w -n build/test/RBT_HOME/check_test.py
 fi
@@ -11,8 +9,17 @@ make linux-g++-64
 make test
 
 cd ..
-cp bin/* "${PREFIX}/bin/"
-cp lib/libRbt.* "${PREFIX}/lib/"
+cp lib/libRbt.so.rDock_2013.1_src "${PREFIX}/lib/"
 PERL_INSTALLSITELIB=$(perl -e 'use Config; print "$Config{installsitelib}"')
-mkdir -p "${PERL_INSTALLSITELIB}"
+mkdir -p "${PERL_INSTALLSITELIB}" "${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin"
 cp lib/*.pl lib/*.pm "${PERL_INSTALLSITELIB}"
+mv data/ "${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/"
+# Create wrappers for binaries that need RBT_ROOT to be in the environment
+for f in rbcalcgrid rbcavity rbdock rblist rbmoegrid; do
+    mv "bin/$f" "${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/"
+    sed -e "s|CHANGEME|${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}|" "$RECIPE_DIR/wrapper.sh" > "${PREFIX}/bin/$f"
+    chmod +x "${PREFIX}/bin/$f"
+done
+# Remove unused to_unix
+rm bin/to_unix
+cp bin/* "${PREFIX}/bin/"
