e1cb673322c5be8675315143575baf1a8fd97ca2
Milad Miladi <miladim@cs.uni-freiburg.de>
2017-05-11 18:28:08 +0200

    cleanup

--- recipes/rnashapes/3.3.0/build.sh
+++ recipes/rnashapes/3.3.0/build.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
-ls $PREFIX
-ls
 cd Misc/Applications/RNAshapes/
-ls
-ls $PREFIX
 PREFIX=$PREFIX && make all 
-ls $PREFIX/bin/
-ls $PREFIX/lib/
 cp x86_*/* $PREFIX/bin/
-cp x86_* $PREFIX/bin/ -r
 cp ../lib/* $PREFIX/lib/ -r
 cp RNAshapes $PREFIX/bin/
 chmod +x $PREFIX/bin/*
-ls
-ls $PREFIX
-ls $PREFIX/bin/
-ls $PREFIX/lib/
-
-
