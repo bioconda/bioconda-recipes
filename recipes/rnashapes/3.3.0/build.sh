9233e497166876b7b29832840a6fa23d67c7487b
Milad Miladi <miladim@cs.uni-freiburg.de>
2017-05-11 17:21:58 +0200

    perl lib to lib

--- recipes/rnashapes/3.3.0/build.sh
+++ recipes/rnashapes/3.3.0/build.sh
@@ -5,11 +5,16 @@ cd Misc/Applications/RNAshapes/
 ls
 ls $PREFIX
 PREFIX=$PREFIX && make all 
-ls
+ls $PREFIX/bin/
+ls $PREFIX/lib/
 cp x86_*/* $PREFIX/bin/
-cp x86_* $PREFIX/bin/ -r 
+cp x86_* $PREFIX/bin/ -r
+cp ../lib/* $PREFIX/lib/
 cp RNAshapes $PREFIX/bin/
 chmod +x $PREFIX/bin/*
 ls
 ls $PREFIX
+ls $PREFIX/bin/
+ls $PREFIX/lib/
+
 
