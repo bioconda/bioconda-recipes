865bd639a43d4807e162355964444c11b0a4aef8
rvalieris <rvalieris@users.noreply.github.com>
2017-05-15 17:18:24 -0300

    duh

--- recipes/fastqc/build.sh
+++ recipes/fastqc/build.sh
@@ -3,7 +3,7 @@
 fastqc=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $fastqc
 cp -r ./* $fastqc
-sed '1 s|^.*$|#!/usr/bin/env perl|g' $fastqc/fastqc
+sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $fastqc/fastqc
 chmod +x $fastqc/fastqc
 mkdir -p $PREFIX/bin
 ln -s $fastqc/fastqc $PREFIX/bin/fastqc 
