2ff5e113cff9d6d291285cc1a3386420ea403135
rvalieris <rvalieris@users.noreply.github.com>
2017-05-15 18:06:39 -0300

    make sed line compatible with osx

--- recipes/fastqc/build.sh
+++ recipes/fastqc/build.sh
@@ -3,7 +3,8 @@
 fastqc=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $fastqc
 cp -r ./* $fastqc
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $fastqc/fastqc
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $fastqc/fastqc
+rm -f $fastqc/fastqc.bak
 chmod +x $fastqc/fastqc
 mkdir -p $PREFIX/bin
 ln -s $fastqc/fastqc $PREFIX/bin/fastqc 
