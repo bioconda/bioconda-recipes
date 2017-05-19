85b56104743fc1bffd61fa3aca23e9852576e419
Peter van Heusden <pvh@sanbi.ac.za>
2017-05-19 21:47:21 +0200

    Hide stderr messages to avoid complaints about job control

--- recipes/udocker/run_test.sh
+++ recipes/udocker/run_test.sh
@@ -11,4 +11,4 @@ sudo -E -u $TEST_USER sh <<"END"
 PATH=$CONDA_PREFIX/bin:$PATH ; export PATH
 udocker help | grep udocker >/dev/null
 udocker pull hello-world && udocker run hello-world | grep 'Hello from Docker' >/dev/null
-END
+END >/dev/null 2>&1
