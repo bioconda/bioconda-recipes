1a1bb71f9f80da068c0fe60e9eaa52f3a2409660
Peter van Heusden <pvh@sanbi.ac.za>
2017-05-19 21:13:23 +0200

    Add test script to run test as non-root-user in Travis

---
+++ recipes/udocker/run_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+UID=`id -u`
+TEST_USER=`id -un`
+if [ $UID -eq 0 ] ; then
+    TEST_USER=testuser
+    useradd $TEST_USER
+fi
+
+sudo -E -u $TEST_USER sh <<"END"
+PATH=$CONDA_PREFIX/bin:$PATH ; export PATH
+udocker help | grep udocker >/dev/null
+udocker pull hello-world && udocker run hello-world | grep 'Hello from Docker' >/dev/null
+END
