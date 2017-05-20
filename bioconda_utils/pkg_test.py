51425e7326f202bc7b7e2286eca3a098316dda39
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-05-20 21:59:23 +0200

    run mulled in its own tmp dir

--- bioconda_utils/pkg_test.py
+++ bioconda_utils/pkg_test.py
@@ -141,4 +141,4 @@ def test_package(path,
     env = os.environ.copy()
     if base_image is not None:
         env["DEST_BASE_IMAGE"] = base_image
-    return utils.run(cmd, env=env)
+    return utils.run(cmd, env=env, cwd='/tmp')
