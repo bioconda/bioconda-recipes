b1fb990d1555fb2dde7745c6e8b73dd455c7d6a0
Björn Grüning <bjoern@gruenings.eu>
2017-05-21 23:46:10 +0200

    Update build.sh

--- recipes/osra/2.0.1/build.sh
+++ recipes/osra/2.0.1/build.sh
@@ -1,5 +1,23 @@
 #!/bin/env bash
 
-./configure --with-tclap-include=$PREFIX/include/ --with-potrace-include=$PREFIX/include/ --with-potrace-lib=$PREFIX/lib/ --with-gocr-include=$PREFIX/include/gocr/ --with-gocr-lib=$PREFIX/lib/ --with-ocrad-include=$PREFIX/include/ --with-ocrad-lib=$INSTALL_DIR/ocrad/build/lib/ --with-openbabel-include=$PREFIX/include/openbabel-2.0/ --with-openbabel-lib=$PREFIX/lib --with-graphicsmagick-lib=$PREFIX/lib/ --with-graphicsmagick-include=$PREFIX/include/GraphicsMagick/ --prefix=$PREFIX
-make
-make install
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+./configure --with-tclap-include=$PREFIX/include/ \
+    --with-potrace-include=$PREFIX/include/ \
+    --with-potrace-lib=$PREFIX/lib/ \
+    --with-gocr-include=$PREFIX/include/gocr/ \
+    --with-gocr-lib=$PREFIX/lib/ \
+    --with-ocrad-include=$PREFIX/include/ \
+    --with-ocrad-lib=$PREFIX/lib/ \
+    --with-openbabel-include=$PREFIX/include/openbabel-2.0/ \
+    --with-openbabel-lib=$PREFIX/lib \
+    --with-graphicsmagick-lib=$PREFIX/lib/ \
+    --with-graphicsmagick-include=$PREFIX/include/GraphicsMagick/ \
+    --prefix=$PREFIX \
+    CXXFLAGS=-Wno-c++11-narrowing
+
+make all install
