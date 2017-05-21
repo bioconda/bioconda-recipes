919773b7f265de5cae7d09d515fabb15fd548296
andrewjpage <andrewjpage@gmail.com>
2017-05-21 14:18:58 +0100

    plasmidtron

---
+++ recipes/plasmidtron/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
+
