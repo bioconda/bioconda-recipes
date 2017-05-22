c91fa0639a52e5d82efc78fcbcfc1749c7f1c485
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-05-22 17:06:12 +0200

    Explicitly set cpanm home directory and modify makefile instead of LDFLAGS/CFLAGS.

--- recipes/perl-vcftools-vcf/840/build.sh
+++ recipes/perl-vcftools-vcf/840/build.sh
@@ -8,5 +8,6 @@ sed -i.bak 's/r840/0.840/' perl-lib/lib/Vcf.pm
 # Fix naming scheme of package to make Builder happy
 sed -i.bak 's/Vcf.pm.  Module/Vcf - Module/' perl-lib/lib/Vcf.pm
 cp $RECIPE_DIR/Build.PL perl-lib
+mkdir cpanm-home
 cd perl-lib
-cpanm -v -i .
+PERL_CPANM_HOME=../cpanm-home cpanm -v -i .
--- recipes/vcftools/0.1.11/build.sh
+++ recipes/vcftools/0.1.11/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
 sed -i.bak -e 's/DIRS = cpp perl/DIRS = cpp/' ./Makefile
+sed -i.bak -e "s/CFLAGS = /CFLAGS = -I$PREFIX\/include /" ./cpp/Makefile
+sed -i.bak -e "s/CPPFLAGS = /CPPFLAGS = -I$PREFIX\/include /" ./cpp/Makefile
+sed -i.bak -e "s/LIB = -lz/LIB = -L$PREFIX\/lib -lz /" ./cpp/Makefile
 make
 make install
