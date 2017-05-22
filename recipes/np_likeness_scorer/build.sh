2c0a6b4c9dfd1ab806200ab049c26f3a12f8e4f1
Hassan Amr <hassan.amr89@gmail.com>
2017-05-22 03:30:04 +0200

    finished python wrapper and fixed the recipe accordingly

--- recipes/np_likeness_scorer/build.sh
+++ recipes/np_likeness_scorer/build.sh
@@ -4,7 +4,8 @@ set -eu -o pipefail
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -R Natural_Product_Likeness_calculator_2.1 $outdir/
+cd Natural_Product_Likeness_calculator_2.1/
+cp NP-Likeness-2.1.jar $outdir/
 cp $RECIPE_DIR/np-likeness.py $outdir/np-likeness
 ls -l $outdir
 ln -s $outdir/np-likeness $PREFIX/bin
--- recipes/np_likeness_scorer/np-likeness.py
+++ recipes/np_likeness_scorer/np-likeness.py
@@ -15,9 +15,7 @@ from os import getenv
 from os import X_OK
 jar_file = 'NP-Likeness-2.1.jar'
 
-default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
-
-# !!! End of parameter section. No user-serviceable code below this line !!!
+default_jvm_pass_args = ['-help']
 
 
 def real_dirname(path):
@@ -37,63 +35,33 @@ def java_executable():
 
 
 def jvm_opts(argv):
-    """Construct list of Java arguments based on our argument list.
-
-    The argument list passed in argv must not include the script name.
-    The return value is a 3-tuple lists of strings of the form:
-      (memory_options, prop_options, passthrough_options)
     """
-    mem_opts = []
-    prop_opts = []
+    Construct list of Java arguments based on our argument list.
+    """
     pass_args = []
-    exec_dir = None
 
     for arg in argv:
-        if arg.startswith('-D'):
-            prop_opts.append(arg)
-        elif arg.startswith('-XX'):
-            prop_opts.append(arg)
-        elif arg.startswith('-Xm'):
-            mem_opts.append(arg)
-        elif arg.startswith('--exec_dir='):
-            exec_dir = arg.split('=')[1].strip('"').strip("'")
-            if not os.path.exists(exec_dir):
-                shutil.copytree(real_dirname(sys.argv[0]), exec_dir, symlinks=False, ignore=None)
-        else:
-            pass_args.append(arg)
+        pass_args.append(arg)
 
-    # In the original shell script the test coded below read:
-    #   if [ "$jvm_mem_opts" == "" ] && [ -z ${_JAVA_OPTIONS+x} ]
-    # To reproduce the behaviour of the above shell code fragment
-    # it is important to explictly check for equality with None
-    # in the second condition, so a null envar value counts as True!
+    if pass_args == []:
+        pass_args = default_jvm_pass_args
 
-    if mem_opts == [] and getenv('_JAVA_OPTIONS') is None:
-        mem_opts = default_jvm_mem_opts
-
-    return (mem_opts, prop_opts, pass_args, exec_dir)
+    return pass_args
 
 
 def main():
     java = java_executable()
     """
-    PeptideShaker updates files relative to the path of the jar file.
-    In a multiuser setting, the option --exec_dir="exec_dir"
-    can be used as the location for the peptide-shaker distribution.
-    If the exec_dir dies not exist,
-    we copy the jar file, lib, and resources to the exec_dir directory.
+    Calling of the tool ensuring that the java runtime is invoked with the right options.
     """
-    (mem_opts, prop_opts, pass_args, exec_dir) = jvm_opts(sys.argv[1:])
-    jar_dir = exec_dir if exec_dir else real_dirname(sys.argv[0])
+    pass_args = jvm_opts(sys.argv[1:])
+    jar_dir = real_dirname(sys.argv[0])
 
-    if pass_args != [] and pass_args[0].startswith('eu'):
-        jar_arg = '-cp'
-    else:
-        jar_arg = '-jar'
+    jar_arg = '-jar'
 
     jar_path = os.path.join(jar_dir, jar_file)
 
-    java_args = [java] + mem_opts + prop_opts + [jar_arg] + [jar_path] + pass_args
+    java_args = [java] + [jar_arg] + [jar_path] + pass_args
 
     sys.exit(subprocess.call(java_args))
 
