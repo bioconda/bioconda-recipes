cbce736595351317469fdf82439f823b78d4d2af
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-05-23 18:46:30 -0400

    gap2seq: enabled osx build (#4751)
    gap2seq: enabled osx build.  Still to do: enable multi-threading on OS X; clean up recipe; separate gatb-core into own recipe; use CONDA_BOOST and CONDA_HDF5 rather than included copies.

--- recipes/gap2seq/build.sh
+++ recipes/gap2seq/build.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
-set -efu -o pipefail
+set -ef -o pipefail
+
+echo BUILDING GAP2SEQ bldvar 1
+echo uname is:
+uname -a
+echo env is:
+env
+echo ======================================== end of env =================================
+
+if [ -z "$CPPFLAGS" ]; then export CPPFLAGS=""; fi
+if [ -z "$LDFLAGS" ]; then export LDFLAGS=""; fi
+
+export CPPFLAGS="$CPPFLAGS -I${PREFIX}/include"
+export LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
 
 mkdir -p build
 pushd build
 
+
 mkdir -p $PREFIX/bin
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBoost_NO_BOOST_CMAKE=TRUE -DBoost_NO_SYSTEM_PATHS=TRUE -DBOOST_ROOT:PATHNAME=$PREFIX -DBoost_LIBRARY_DIRS:FILEPATH=${PREFIX}/lib ${SRC_DIR}
+if [ "$(uname)" == "Darwin" ]; then
+		ln -s ${PREFIX}/include/boost ../thirdparty/gatb-core/thirdparty/
+
+    # c++11 compatibility
+
+		if [ -z "$CXXFLAGS" ]; then export CXXFLAGS=""; fi
+		if [ -z "${CXX_FLAGS}" ]; then export CXX_FLAGS=""; fi
+		if [ -z "${CMAKE_CXX_FLAGS}" ]; then export CMAKE_CXX_FLAGS=""; fi
+		if [ -z "${LDFLAGS}" ]; then export LDFLAGS=""; fi
+		if [ -z "${LD_FLAGS}" ]; then export LD_FLAGS=""; fi
+		if [ -z "${CMAKE_LD_FLAGS}" ]; then export CMAKE_LD_FLAGS=""; fi
+
+		CXXFLAGS="$CXXFLAGS";
+		LDFLAGS="$LDFLAGS -Wl,-rpath ${PREFIX}/lib";
+
+    export CXXFLAGS="$CXXFLAGS -stdlib=libc++ -std=c++11 -I${PREFIX}/include -DSINGLE_THREAD"
+    export CXX_FLAGS="${CXX_FLAGS} -stdlib=libc++ -std=c++11 -I${PREFIX}/include -DSINGLE_THREAD"
+    export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++11 -I${PREFIX}/include -DSINGLE_THREAD"
+    export LDFLAGS="$LDFLAGS -stdlib=libc++"
+    export LD_FLAGS="${LD_FLAGS} -stdlib=libc++"
+    export CMAKE_LDFLAGS="${CMAKE_LDFLAGS} -stdlib=libc++"
+
+		export C_INCLUDE_PATH=${PREFIX}/include
+		export CPLUS_INCLUDE_PATH=${PREFIX}/include
+		export LD_LIBRARY_PATH=${PREFIX}/lib
+		export BOOST_ROOT=${PREFIX}
+
+    export CXX=clang++
+    export CC=clang
+fi
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ${SRC_DIR}
 make Gap2Seq GapCutter GapMerger
 chmod u+x Gap2Seq.sh
 echo copying files: Gap2Seq.sh Gap2Seq GapCutter GapMerger $PREFIX/bin
