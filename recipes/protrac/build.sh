7552807798fd738338b6e3ca356090e5f5fa398d
rvalieris <rvalieris@users.noreply.github.com>
2017-05-26 14:30:16 -0300

    use suffix on sed

--- recipes/protrac/build.sh
+++ recipes/protrac/build.sh
@@ -3,6 +3,6 @@ export LC_ALL=C
 export LANG=C
 mkdir -p $PREFIX/bin
 chmod +x proTRAC_$PKG_VERSION.pl
-sed -i -e 's/\r/\n/g' proTRAC_$PKG_VERSION.pl
-sed -i -e 's/\/perl/\/env perl/' proTRAC_$PKG_VERSION.pl
+sed -i.bak -e 's/\r/\n/g' proTRAC_$PKG_VERSION.pl
+sed -i.bak -e 's/\/perl/\/env perl/' proTRAC_$PKG_VERSION.pl
 cp proTRAC_$PKG_VERSION.pl $PREFIX/bin
