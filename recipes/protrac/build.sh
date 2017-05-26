af03e03e07a8bb8278203f1b08e4e32c46ac6304
rvalieris <rvalieris@users.noreply.github.com>
2017-05-26 14:56:49 -0300

    use perl instead of sed

--- recipes/protrac/build.sh
+++ recipes/protrac/build.sh
@@ -3,6 +3,8 @@ export LC_ALL=C
 export LANG=C
 mkdir -p $PREFIX/bin
 chmod +x proTRAC_$PKG_VERSION.pl
-sed -i.bak -e 's/\r/\n/g' proTRAC_$PKG_VERSION.pl
-sed -i.bak -e 's/\/perl/\/env perl/' proTRAC_$PKG_VERSION.pl
+
+perl -i -wpe 's/\r/\n/g' proTRAC_$PKG_VERSION.pl
+perl -i -wpe 's/\/perl/\/env perl/' proTRAC_$PKG_VERSION.pl
+
 cp proTRAC_$PKG_VERSION.pl $PREFIX/bin
