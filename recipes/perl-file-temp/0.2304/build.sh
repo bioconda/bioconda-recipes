45d520874469971cc16cf836f8d48e345e64a5b8
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-05-29 15:24:51 +0400

    stupid cpanm

--- recipes/perl-file-temp/0.2304/build.sh
+++ recipes/perl-file-temp/0.2304/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # If it has Build.PL use that, otherwise use Makefile.PL
-cpanm --installdeps .
+HOME=/tmp cpanm --installdeps .
+
 if [ -f Build.PL ]; then
     perl Build.PL
     perl ./Build
