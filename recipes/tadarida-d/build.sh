51bb449538058abf9020b932d12012fc514d7cdd
ThimotheeV <thimothee.virgoulay@etu.umontpellier.fr>
2017-05-30 15:22:56 +0200

    Try

--- recipes/tadarida-d/build.sh
+++ recipes/tadarida-d/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_P‌​ATH
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_P‌​ATH"
 make
-mkdir -p $PREFIX/bin
-cp tadaridaD $PREFIX/bin
+mkdir -p "$PREFIX/bin"
+cp tadaridaD "$PREFIX/bin"
