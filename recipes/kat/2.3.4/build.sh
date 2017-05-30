a85d2c8e3a1f0f51687f9401e13c834e1c5e47f2
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-05-30 11:52:54 +0400

    stupid bash

--- recipes/kat/2.3.4/build.sh
+++ recipes/kat/2.3.4/build.sh
@@ -10,7 +10,7 @@ export BOOST_INCLUDE_DIR=${PREFIX}/include
 export BOOST_LIBRARY_DIR=${PREFIX}/lib
 export LIBS='-lboost_system -lboost_program_options -lboost_filesystem -lboost_timer'
 
-export CXXFLAGS= "-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
+export CXXFLAGS=" -std=c++11 -DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
 export LDFLAGS="-L${BOOST_LIBRARY_DIR} -lboost_filesystem -lboost_system"
 
 # fix automake
