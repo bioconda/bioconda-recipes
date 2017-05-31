350e4299aee2db0cf68cfe0b673c47866d1c96bf
ThimotheeV <thimothee.virgoulay@etu.umontpellier.fr>
2017-05-31 11:09:31 +0200

    Test

--- recipes/tadarida-d/build.sh
+++ recipes/tadarida-d/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_P‌​ATH"
+export LD_LIBRARY_PATH="$PREFIX/lib:$PREFIX/lib/x86_64-linux-gnu:$LD_LIBRARY_P‌​ATH"
 make
 mkdir -p "$PREFIX/bin"
 cp tadaridaD "$PREFIX/bin"
