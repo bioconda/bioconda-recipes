f7c93162f4f56f78e7bf586fe1dec1f0aff11c84
ThomasWollmann <ThomasWollmann@users.noreply.github.com>
2017-05-31 10:29:37 +0200

    Update build.sh

--- recipes/gdk-pixbuf/build.sh
+++ recipes/gdk-pixbuf/build.sh
@@ -4,9 +4,8 @@ ln -s $PREFIX/lib $PREFIX/lib64
 
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig"
 export ACLOCAL_FLAGS="-I$PREFIX/share/aclocal"
-export LDFLAGS="-L$PREFIX/lib -Wl,-rpath ${PREFIX}/lib"
 
-./configure  --prefix=$PREFIX --enable-introspection=yes CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" 
+./configure  --prefix=$PREFIX --enable-introspection=yes CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -Wl,-rpath ${PREFIX}/lib" 
 
 sed -i.bak 's|#!/usr/bin/perl.*|#!/usr/bin/env perl|' ./gdk-pixbuf/*.pl
 
