e1ceb5fed9574628113b2410b59107c9ed45e9a6
peterjc <p.j.a.cock@googlemail.com>
2017-06-02 15:37:44 +0100

    Change batchman installation for clinod
    As per advice from Bjoern Gruening, the BioConda installed
    batchman from SNNS should be in /bin/ so assume that.

--- recipes/clinod/build.sh
+++ recipes/clinod/build.sh
@@ -10,5 +10,6 @@ ls -l $outdir
 ln -s $outdir/clinod $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/clinod"
 
-# clinod expects the batchman binary from SNNS in its folder:
-ln -s "`which batchman`" $outdir/batchman
+# clinod expects the batchman binary from SNNS in its folder,
+# it should be installed here by the BioConda SNNS package:
+ln -s $PREFIX/bin/batchman $outdir/batchman
