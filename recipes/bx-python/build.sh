7532b1221cfe7e919fc01b66db384175fa19bdaf
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-06-02 11:33:27 +0200

    Debug bx-python.

--- recipes/bx-python/build.sh
+++ recipes/bx-python/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
+ls $PREFIX/bin
+which python
 $PYTHON setup.py install
 
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
