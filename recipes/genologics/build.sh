9f2b2619d9ac07f1c9082ced83f46f05c3242674
Björn Grüning <bjoern@gruenings.eu>
2017-06-03 16:45:38 +0200

    Update build.sh

--- recipes/genologics/build.sh
+++ recipes/genologics/build.sh
@@ -1,2 +1,4 @@
-$PYTHON setup.py install --single-version-externally-managed
+#!/usr/bin/env bash
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 chmod -R o+r $PREFIX/lib/python*/site-packages/genologics*
