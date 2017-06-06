bioconda/bioconda-recipes

commit abbda6ea25c4342d15d5c494ba1bcdd86cce5de0

update snpeff and snpsift to 4.3m (#4690)
also fix version ordering of snpsift, like snpeff

recipes/snpsift/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
-mkdir -p $PREFIX/bin
-rm -rf snpEff.jar galaxy examples
-cp -R ./* $outdir/
+mkdir -p "$outdir"
+mkdir -p "$PREFIX/bin"
+cd snpEff/
+cp -R SnpSift.jar "$outdir/"
 cp $RECIPE_DIR/snpsift.py $outdir/SnpSift
-chmod +x $outdir/SnpSift
+chmod +x "$outdir/SnpSift"
 
-ln -s $outdir/SnpSift $PREFIX/bin
+ln -s "$outdir/SnpSift" "$PREFIX/bin"

recipes/snpsift/meta.yaml
@@ -1,3 +1,8 @@
+{% set snpeff_ver = "v4_3m" %}
+# NOTE: if the version contains a trailing letter, use the <d>.<d>.1<l> format
+{% set version = "4.3.1m" %}
+{% set sha256 = "b218a3e673cf90c96b00e806a5ea491db48d0085c9a4eda4c57899e08da08154" %}
+
 about:
   home: 'http://snpeff.sourceforge.net/SnpSift.html'
   license: "LGPLv3"
@@ -5,23 +10,26 @@ about:
 
 package:
   name: snpsift
-  version: '4.3'
+  version: {{ version }}
 
 build:
-  number: 2
+  number: 0
   skip: False
 
 source:
-  fn: snpEff_v4_3_2.zip
-  md5: 275cf9a8cc08cebf55c03fdec01eb578
-  url: http://downloads.sourceforge.net/project/snpeff/snpEff_v4_3_core.zip
+  fn: snpEff_{{ snpeff_ver }}_core.zip
+  url: http://downloads.sourceforge.net/project/snpeff/snpEff_{{ snpeff_ver }}_core.zip
+  sha256: {{ sha256 }}
 
 requirements:
   run:
-      - java-jdk
+      - openjdk
       - python
 
 test:
     commands:
       - SnpSift 2>&1 > /dev/null | grep "SnpSift version 4.3"
       - echo | SnpSift filter "(CHROM == '2')" >/dev/null 2>&1
+
+extra:
+  notes: 'Note that the package version is slightly different from upstream, this is to make sure conda will order the package versions correctly.'

recipes/snpeff/meta.yaml
@@ -1,7 +1,7 @@
-{% set snpeff_ver = "v4_3k" %}
+{% set snpeff_ver = "v4_3m" %}
 # NOTE: if the version contains a trailing letter, use the <d>.<d>.1<l> format
-{% set version = "4.3.1k" %}
-{% set sha256 = "396fadbefb4994838713c9a3e96393d16e7ec833254a962834e7cbd699b9f35e" %}
+{% set version = "4.3.1m" %}
+{% set sha256 = "b218a3e673cf90c96b00e806a5ea491db48d0085c9a4eda4c57899e08da08154" %}
 
 about:
   home: 'http://snpeff.sourceforge.net/'

commit b12e45b7e51f48e143d18549e69e8e03b2390eb8

Re-add scripts/ and snpEff.config to snpsift
snpEff.config is needed by SnpSift annotate
scripts/vcfEffOnePerLine.pl is mentioned in the SnpSift documentation

Broken in abbda6ea25c4342d15d5c494ba1bcdd86cce5de0

recipes/snpsift/build.sh
@@ -4,8 +4,8 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p "$outdir"
 mkdir -p "$PREFIX/bin"
 cd snpEff/
-cp -R SnpSift.jar "$outdir/"
-cp $RECIPE_DIR/snpsift.py $outdir/SnpSift
+mv scripts/ snpEff.config SnpSift.jar "$outdir/"
+cp "$RECIPE_DIR/snpsift.py" "$outdir/SnpSift"
 chmod +x "$outdir/SnpSift"
 
 ln -s "$outdir/SnpSift" "$PREFIX/bin"

recipes/snpsift/meta.yaml
@@ -13,7 +13,7 @@ package:
   version: {{ version }}
 
 build:
-  number: 0
+  number: 1
   skip: False
 
 source: