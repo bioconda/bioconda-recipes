657ad07ae9b5cb74d58bf09a91536e0924323ce4
Greg Von Kuster <ghv2@psu.edu>
2017-06-07 08:39:36 -0400

    Add recipe for AssemblyPostProcessor version 1.0.0 (#4903)
    * Add recipe for AssemblyPostProcessor version 1.0.0
    
    * Deprecate AssemblyPostProcessor recipes older than 1.0.0

--- recipes/plant_tribes_assembly_post_processor/1.0.0/build.sh
+++ recipes/plant_tribes_assembly_post_processor/1.0.0/build.sh
@@ -6,8 +6,9 @@ mkdir -p $PREFIX/data
 mkdir -p $PREFIX/pipelines
 mkdir -p $PREFIX/scripts
 
-cp ./pipelines/AssemblyPostProcesser $PREFIX/bin
-ln -s $PREFIX/bin/AssemblyPostProcesser $PREFIX/pipelines/AssemblyPostProcesser
+cp ./pipelines/AssemblyPostProcessor $PREFIX/bin
+chmod +x $PREFIX/bin/AssemblyPostProcessor
+ln -s $PREFIX/bin/AssemblyPostProcessor $PREFIX/pipelines/AssemblyPostProcessor
 cp ./config/* $PREFIX/config
 cp ./data/* $PREFIX/data
 cp ./scripts/* $PREFIX/scripts
