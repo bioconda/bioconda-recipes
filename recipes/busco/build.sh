11d7ea3728591ad73ea3ccc707d3acf6198c8f21
Michal Lorenc <mictadlo@gmail.com>
2017-06-09 14:38:36 +1000

    fixed path

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin/
-cp BUSCO.py $PREFIX/bin
-cp BUSCO_plot.py $PREFIX/bin
-cp generate_plot.py $PREFIX/bin
+cp scripts/BUSCO.py $PREFIX/bin
+cp scripts/generate_plot.py $PREFIX/bin
 
 ln -s $PREFIX/bin/BUSCO.py $PREFIX/bin/busco
-ln -s $PREFIX/bin/BUSCO_plot.py $PREFIX/bin/busco_plot
 ln -s $PREFIX/bin/generate_plot.py $PREFIX/bin/generate_plot
 
