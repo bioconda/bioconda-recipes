b729526e8db70aca062a6a4fad3795b997bb7d1d
Michal Lorenc <mictadlo@gmail.com>
2017-06-09 14:19:11 +1000

    Bump version for busco

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -3,5 +3,9 @@
 mkdir -p $PREFIX/bin/
 cp BUSCO.py $PREFIX/bin
 cp BUSCO_plot.py $PREFIX/bin
+cp generate_plot.py $PREFIX/bin
+
 ln -s $PREFIX/bin/BUSCO.py $PREFIX/bin/busco
 ln -s $PREFIX/bin/BUSCO_plot.py $PREFIX/bin/busco_plot
+ln -s $PREFIX/bin/generate_plot.py $PREFIX/bin/generate_plot
+
