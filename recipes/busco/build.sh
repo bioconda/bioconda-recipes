d1a8dac9d502ea95738b9d5d46b48bd30c983c26
Michal Lorenc <mictadlo@gmail.com>
2017-06-09 22:23:37 +1000

    added python install setup.py

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 mkdir -p $PREFIX/bin/
 cp scripts/run_BUSCO.py $PREFIX/bin
 cp scripts/generate_plot.py $PREFIX/bin
@@ -7,3 +8,4 @@ cp scripts/generate_plot.py $PREFIX/bin
 ln -s $PREFIX/bin/run_BUSCO.py $PREFIX/bin/run_busco
 ln -s $PREFIX/bin/generate_plot.py $PREFIX/bin/generate_plot
 
+python setup.py install
