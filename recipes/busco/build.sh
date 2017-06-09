bf5c1b44c983f44c08f5ed2ff1f2dc7db3dfa7cc
Michal Lorenc <mictadlo@gmail.com>
2017-06-09 18:38:17 +1000

    fix script name

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin/
-cp scripts/BUSCO.py $PREFIX/bin
+cp scripts/run_BUSCO.py $PREFIX/bin
 cp scripts/generate_plot.py $PREFIX/bin
 
-ln -s $PREFIX/bin/BUSCO.py $PREFIX/bin/busco
+ln -s $PREFIX/bin/run_BUSCO.py $PREFIX/bin/run_busco
 ln -s $PREFIX/bin/generate_plot.py $PREFIX/bin/generate_plot
 
