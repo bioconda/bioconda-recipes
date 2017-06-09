1fa06a2cc17f7984014b6381eae3be01f905605b
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-06-09 16:01:19 -0400

    updated building a gem

--- recipes/transrate/build.sh
+++ recipes/transrate/build.sh
@@ -2,4 +2,7 @@
 
 set -ef -o pipefail
 
+wget https://raw.githubusercontent.com/blahah/transrate/master/LICENSE
+
 gem install --install-dir=$PREFIX/lib/ruby/gems/2.1.0 --bindir=$PREFIX/bin transrate
+
