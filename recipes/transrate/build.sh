0c5744073700a1eee94756feb84c87c35c231cca
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-06-11 11:21:40 -0400

    changed pinned versions

--- recipes/transrate/build.sh
+++ recipes/transrate/build.sh
@@ -2,7 +2,4 @@
 
 set -ef -o pipefail
 
-wget https://raw.githubusercontent.com/blahah/transrate/master/LICENSE
-
-gem install --install-dir=$PREFIX/lib/ruby/gems/2.1.0 --bindir=$PREFIX/bin transrate
-
+gem install --install-dir=$PREFIX/lib/ruby/gems/2.2.0 --bindir=$PREFIX/bin transrate
