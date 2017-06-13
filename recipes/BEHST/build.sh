f78256c0db631eba8a5c808ab3a23afd99d13f91
Davide <davide@opennet-33-58.uhnres.utoronto.ca>
2017-06-13 17:50:30 -0400

    edited meta.yaml

--- recipes/BEHST/build.sh
+++ recipes/BEHST/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-#
 #$ -cwd
 #$ -S /bin/bash
 #
 set -o nounset -o pipefail -o errexit
 set -o xtrace
-
-echo "Running build.sh"
 
 echo $(pwd)
 echo $(ls)
@@ -19,27 +16,3 @@ mkdir ../temp/
 
 
 sh project.sh ../data/pressto_LUNG_enhancers.bed DEFAULT_EQ DEFAULT_ET
-
-# sudo yum -y install python
-# 
-# sudo yum -y install python-devel
-# 
-# sudo yum -y install epel-release
-# 
-# sudo yum -y install python-pip
-# 
-# sudo pip install pandas
-# 
-# sudo yum -y group install "Development Tools"
-# 
-# sudo yum -y install zlib-devel
-# 
-# sudo yum -y install BEDTools
-# 
-# sudo pip install pybedtools
-# 
-# sudo yum -y install R
-# 
-# sudo yum -y install curl-devel
-# 
-# sudo Rscript -e 'install.packages(c("RCurl","gProfileR"), repos="https://cran.rstudio.com")'
--- recipes/BEHST/test.sh
+++ recipes/BEHST/test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-#
 #$ -cwd
 #$ -S /bin/bash
 #
 set -o nounset -o pipefail -o errexit
 set -o xtrace
-
-echo "Running test.sh on lung enhancers"
 
 cd ./bin/
 
@@ -14,28 +11,6 @@ mkdir ../results/
 
 mkdir ../temp/
 
-sh project.sh ../data/pressto_LUNG_enhancers.bed DEFAULT_EQ DEFAULT_ET
+sh project.sh ../data/pressto_BLOOD_enhancers.bed DEFAULT_EQ DEFAULT_ET
 
-# sudo yum -y install python
-# 
-# sudo yum -y install python-devel
-# 
-# sudo yum -y install epel-release
-# 
-# sudo yum -y install python-pip
-# 
-# sudo pip install pandas
-# 
-# sudo yum -y group install "Development Tools"
-# 
-# sudo yum -y install zlib-devel
-# 
-# sudo yum -y install BEDTools
-# 
-# sudo pip install pybedtools
-# 
-# sudo yum -y install R
-# 
-# sudo yum -y install curl-devel
-# 
-# sudo Rscript -e 'install.packages(c("RCurl","gProfileR"), repos="https://cran.rstudio.com")'
+sh project.sh ../data/vista_LIMB_sorted.bed DEFAULT_EQ DEFAULT_ET
