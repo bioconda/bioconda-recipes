b868ceeb7df7c63a485081b9bfcf055c0a1563cd
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-06-15 00:31:34 +0200

    r fixes

--- recipes/bioconductor-motiv/build.sh
+++ recipes/bioconductor-motiv/build.sh
@@ -1,6 +1,12 @@
 
 #!/bin/bash
 
+export CFLAGS="$(gsl-config --cflags)"
+export LDFLAGS="$(gsl-config --libs)"
+
+# For whatever reason, it can't link to gsl correctly without this on OS X.
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+
 export LD_LIBRARY_PATH=$PREFIX/lib
 
 mv DESCRIPTION DESCRIPTION.old
