489f24a4f70dbd5d74c19ca4c510091dedf10f86
Thomas Cokelaer <cokelaer@gmail.com>
2017-06-15 11:02:51 +0200

    fix postlink

--- recipes/homer/post-link.sh
+++ recipes/homer/post-link.sh
@@ -1,4 +1,7 @@
-echo"
+#!/bin/bash
+
+cat <<EOF >> ${PREFIX}/.messages.txt
+
 For Mac users: Homer is installed.
 For Linux platforms/users: Once Homer is installed with bioconda, you still 
 need to call the following executable manually:
@@ -6,4 +9,4 @@ need to call the following executable manually:
     configureHomer
 
 
-" > $PREFIX/.messages.txt
+EOF
