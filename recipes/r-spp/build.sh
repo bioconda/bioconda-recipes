caed60b810d177a59e5f51c224a33d4e3e65886a
rvalieris <rvalieris@users.noreply.github.com>
2017-06-19 16:57:18 -0300

    revert spp, try multicool

--- recipes/r-spp/build.sh
+++ recipes/r-spp/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export R_LIBS_USER=':'
-export LDFLAGS=-L${PREFIX}/lib
+if [ `uname` == Darwin ]; then
+	export LDFLAGS=-L${PREFIX}/lib
+fi
 export BOOST_ROOT=${PREFIX}
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
