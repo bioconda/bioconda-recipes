70784ccaec2418c71166b0ae47d39e98562f76fb
Davide <davide@opennet-33-58.uhnres.utoronto.ca>
2017-06-23 16:08:22 -0400

    edited to use download_behst_data.sh

--- recipes/BEHST/build.sh
+++ recipes/BEHST/build.sh
@@ -5,18 +5,6 @@
 set -o nounset -o pipefail -o errexit
 set -o xtrace
 
-
 PREFIX=.
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-echo "outdir: "$outdir
-
-
-cd $PREFIX/bin/
-
-mkdir $PREFIX/../results/
-
-mkdir $PREFIX/../temp/
-
-echo "outdir: "$outdir
-sh project.sh $PREFIX/../data/pressto_LUNG_enhancers.bed DEFAULT_EQ DEFAULT_ET
+./download_behst_data.sh
--- recipes/BEHST/test.sh
+++ recipes/BEHST/test.sh
@@ -7,17 +7,8 @@ set -o xtrace
 
 PREFIX=.
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-echo "outdir: "$outdir
-
-
-
 cd $PREFIX/bin/
 
-mkdir $PREFIX/../results/
+./project.sh ../data/pressto_BLOOD_enhancers.bed DEFAULT_EQ DEFAULT_ET
 
-mkdir $PREFIX/../temp/
-
-sh project.sh ../data/pressto_BLOOD_enhancers.bed DEFAULT_EQ DEFAULT_ET
-
-sh project.sh ../data/vista_LIMB_sorted.bed DEFAULT_EQ DEFAULT_ET
+./project.sh ../data/vista_LIMB_sorted.bed DEFAULT_EQ DEFAULT_ET
