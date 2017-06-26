114d12d277547f2834b2d2fc03fc0c313bc6c7e3
Davide <davide@opennet-33-58.uhnres.utoronto.ca>
2017-06-26 16:54:53 -0400

    reduced lists of duplicate dependencies

--- recipes/BEHST/test.sh
+++ recipes/BEHST/test.sh
@@ -5,8 +5,6 @@
 set -o nounset -o pipefail -o errexit
 set -o xtrace
 
-#mkdir -p $PREFIX/bin
-#cp $SRC_DIR/bin/* $PREFIX/bin
 cd $PREFIX/bin
 
 ./project.sh ../data/pressto_BLOOD_enhancers.bed DEFAULT_EQ DEFAULT_ET
