80f3dbdd1ec736138d72f4ae2ad5ad29104dc9f1
davidechicco <davide.chicco@gmail.com>
2017-06-27 14:40:32 -0400

    final

--- recipes/BEHST/test.sh
+++ recipes/BEHST/test.sh
@@ -2,9 +2,9 @@
 #$ -cwd
 #$ -S /bin/bash
 #
-set -o pipefail -o errexit
+set -o nounset -o pipefail -o errexit
 set -o xtrace
 
 cd $PREFIX/bin
 
-./project.sh --test
+./project.sh --test -1 -1 -1 -1 -1
