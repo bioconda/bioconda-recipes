5251ac0ebc5895d05145d9c4c7f310ee95baf33a
Martin Buchkovich <buchkovm@gmail.com>
2017-06-28 13:39:52 -0400

    Updated build script

--- recipes/kraken-ea/build.sh
+++ recipes/kraken-ea/build.sh
@@ -5,9 +5,13 @@
 #export CPATH=${PREFIX}/include
 
 mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
+export CXXFLAGS="-std=c++0x -Wall -fopenmp  -DGZSTREAM_NAMESPACE=gz -g -I."
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib -L. -lkraken -lz"
 
-chmod u+x install_kraken.sh
-./install_kraken.sh "$PREFIX/libexec"
+
+chmod u+x install_kraken_conda.sh
+./install_kraken_conda.sh "$PREFIX/libexec"
 for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kraken-translate; do
     sed -i.bak 's|#!/usr/bin/perl|#!/usr/bin/env perl|' $PREFIX/libexec/$bin
     rm -f $PREFIX/libexec/$bin.bak
