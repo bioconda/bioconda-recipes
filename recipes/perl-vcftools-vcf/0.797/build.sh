39553658c788750eca7db95b2c1be97a69002706
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-07-06 13:32:02 +0200

    Fixed perl-vcftools-vcf=0.797 and adapted to that of 0.840.

--- recipes/perl-vcftools-vcf/0.797/build.sh
+++ recipes/perl-vcftools-vcf/0.797/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 
 mkdir -p perl-lib/lib
 cp perl/*.pm perl-lib/lib
@@ -8,5 +8,6 @@ sed -i.bak 's/r797/0.797/' perl-lib/lib/Vcf.pm
 # Fix naming scheme of package to make Builder happy
 sed -i.bak 's/Vcf.pm.  Module/Vcf - Module/' perl-lib/lib/Vcf.pm
 cp $RECIPE_DIR/Build.PL perl-lib
+mkdir cpanm-home
 cd perl-lib
-cpanm -v -i .
+PERL_CPANM_HOME=../cpanm-home cpanm -v -i .
