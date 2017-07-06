7ffb648fe0da8f6c4ed331d3c65d27909d12b15e
Marmaduke <jmeppley@gmail.com>
2017-07-06 15:28:06 -0700

    removing the python version restrictions for now. Adding explicit OSX version

--- recipes/anvio/build.sh
+++ recipes/anvio/build.sh
@@ -2,6 +2,9 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+if [ "$(uname)" == "Darwin" ]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
 
 $PYTHON setup.py install 
 
