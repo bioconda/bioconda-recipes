f719999d5ea03be3b30f2d8587828a11c6d72678
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-07-14 13:56:16 +0200

    Try fixing include paths.

--- recipes/vg/build.sh
+++ recipes/vg/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-./source_me.sh
+source source_me.sh
 make static
 
 mkdir -p $PREFIX/bin
