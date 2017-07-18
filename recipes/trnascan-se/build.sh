86ef646e8299b5e206557f230a410761a93bf3df
Joerg Fallmann <fall@bioinf.uni-leipzig.de>
2017-07-18 14:18:11 +0200

    Updating build.sh

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
+export BINDIR=${PREFIX}/bin
+export LIBDIR=${PREFIX}/lib
 
-BINDIR=${PREFIX}/bin
-mkdir -p $BINDIR
-
-LIBDIR=${PREFIX}/lib
-
-make all 
-
-## Build Perl
-
-mkdir perl-build
-find . -name "*.pl" | xargs -I {} mv {} perl-build
-find . -name "*.pm" | xargs -I {} cp {} perl-build/lib
-cd perl-build
-cp ${RECIPE_DIR}/Build.PL ./
-perl ./Build.PL
-perl ./Build manifest
-perl ./Build install --installdirs site
-
-cd ..
-## End build perl
-
-mv coves-SE covels-SE eufindtRNA trnascan-1.4 ${PREFIX}/bin
-mv tRNAscan-SE.src ${PREFIX}/bin/tRNAscan-SE
-
-cd ${PREFIX}/bin
-chmod +x coves-SE 
-chmod +x covels-SE 
-chmod +x eufindtRNA 
-chmod +x trnascan-1.4
-chmod +x tRNAscan-SE
+make -j 1
+make install
 
 
 
+#mkdir -p $BINDIR
+#
+
+#
+#make all 
+#
+### Build Perl
+#
+#mkdir perl-build
+#mkdir perl-build/lib
+#
+#find . -name "*.pl" | xargs -I {} mv {} perl-build
+#find . -name "*.pm" | xargs -I {} cp {} perl-build/lib
+#cd perl-build
+#cp ${RECIPE_DIR}/Build.PL ./
+#perl ./Build.PL
+#perl ./Build manifest
+#perl ./Build install --installdirs site
+#
+#cd ..
+### End build perl
+#
+#mv coves-SE covels-SE eufindtRNA trnascan-1.4 ${PREFIX}/bin
+#mv tRNAscan-SE.src ${PREFIX}/bin/tRNAscan-SE
+#
+#cd ${PREFIX}/bin
+#chmod +x coves-SE 
+#chmod +x covels-SE 
+#chmod +x eufindtRNA 
+#chmod +x trnascan-1.4
+#chmod +x tRNAscan-SE
