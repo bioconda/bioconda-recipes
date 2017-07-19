b8ec37e4d05e569f74208712f4dc28435ba31cdf
Joerg Fallmann <fall@bioinf.uni-leipzig.de>
2017-07-19 14:00:57 +0200

    Regex update

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -1,58 +1,19 @@
 #!/bin/bash
 
-#xport BINDIR=${PREFIX}/bin
-#export PERL5LIB=$PERL5LIB:${PREFIX}/lib
-#LIBDIR=$(/usr/bin/env perl -V|grep -A1 @INC |tail -n+2)
-
 make -j 1
-#make install
-
 mkdir -p ${PREFIX}/bin
-#kdir -p ${PREFIX}/bin/tRNAscan-SE/tRNAscanSE
 
 mv -v tRNAscan-SE coves-SE covels-SE eufindtRNA trnascan-1.4 *.cm -t ${PREFIX}/bin
 mv -v tRNAscanSE ${PREFIX}/bin/.
 mv tRNAscan-SE.src ${PREFIX}/bin/.
 
 cd  ${PREFIX}/bin
-sed -i "/^use Getopt::Long;/c\use Getopt::Long;\\nuse lib ${PREFIX}/bin;" tRNAscan-SE
-sed -i "/^our \$bindir/c\our bindir = ${PREFIX}/bin;" tRNAscan-SE
-sed -i "/^our \$lib_dir/c\our lib_dir = ${PREFIX}/bin/tRNAscanSE;" tRNAscan-SE
+sed -i "/^use Getopt::Long;/c\use Getopt::Long;\\nuse lib ${PREFIX}\/bin;" tRNAscan-SE
+sed -i "/^our \$bindir/c\our bindir = ${PREFIX}\/bin;" tRNAscan-SE
+sed -i "/^our \$lib_dir/c\our lib_dir = ${PREFIX}\/bin\/tRNAscanSE;" tRNAscan-SE
 
 chmod +x coves-SE 
 chmod +x covels-SE 
 chmod +x eufindtRNA 
 chmod +x trnascan-1.4
 chmod +x tRNAscan-SE
-
-#mkdir -p $BINDIR
-#
-
-#
-#make all 
-#
-### Build Perl
-#
-#mkdir perl-build
-#mkdir perl-build/lib
-#
-#find . -name "*.pl" | xargs -I {} mv {} perl-build
-#find . -name "*.pm" | xargs -I {} cp {} perl-build/lib
-#cd perl-build
-#cp ${RECIPE_DIR}/Build.PL ./
-#perl ./Build.PL
-#perl ./Build manifest
-#perl ./Build install --installdirs site
-#
-#cd ..
-### End build perl
-#
-#mv coves-SE covels-SE eufindtRNA trnascan-1.4 ${PREFIX}/bin
-#mv tRNAscan-SE.src ${PREFIX}/bin/tRNAscan-SE
-#
-#cd ${PREFIX}/bin
-#chmod +x coves-SE 
-#chmod +x covels-SE 
-#chmod +x eufindtRNA 
-#chmod +x trnascan-1.4
-#chmod +x tRNAscan-SE
