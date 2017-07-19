a81ef0d6b6592587ebf1ec2a8bdb37798340cbfb
Joerg Fallmann <joerg.fallmann@univie.ac.at>
2017-07-19 15:46:25 +0200

    Fixing Makefile

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-make -j 1
-mkdir -p ${PREFIX}/bin
+sed -i "/^BINDIR/c\BINDIR = ${PREFIX}\/bin" Makefile
+sed -i "/^LIBDIR/c\BINDIR = ${PREFIX}\/bin/tRNAscan-SE" Makefile
+sed -i "/^MANDIR/c\MANDIR = ${PREFIX}\/man" Makefile
 
+make -j 1
+make install
+
+mkdir -p ${PREFIX}/bin
 mv tRNAscan-SE coves-SE covels-SE eufindtRNA trnascan-1.4 *.cm -t ${PREFIX}/bin
 mv tRNAscanSE ${PREFIX}/bin/.
 mv tRNAscan-SE.src ${PREFIX}/bin/.
 
 cd  ${PREFIX}/bin
-sed -i "/^use Getopt::Long/c\use Getopt::Long;\\nuse lib ${PREFIX}\/bin\;" tRNAscan-SE
-sed -i "/^our \$bindir/c\our bindir = ${PREFIX}\/bin\;" tRNAscan-SE
-sed -i "/^our \$lib_dir/c\our lib_dir = ${PREFIX}\/bin\/tRNAscanSE\;" tRNAscan-SE
+#sed -i "/^use Getopt::Long/c\use Getopt::Long;\\nuse lib ${PREFIX}\/bin\;" tRNAscan-SE
+#sed -i "/^our \$bindir/c\our bindir = ${PREFIX}\/bin\;" tRNAscan-SE
+#sed -i "/^our \$lib_dir/c\our lib_dir = ${PREFIX}\/bin\/tRNAscanSE\;" tRNAscan-SE
 
 chmod +x coves-SE
 chmod +x covels-SE
