bf8b71775f67d31cce988385b4742a08e3a52dc3
Joerg Fallmann <joerg.fallmann@univie.ac.at>
2017-07-19 19:07:59 +0200

    Still modulepath fight

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -17,7 +17,7 @@ mv tRNAscan-SE.src ${PREFIX}/bin/.
 
 cd  ${PREFIX}/bin
 export PREFIX=${PREFIX}
-perl -i~ -pe 'BEGIN{ $replace = shift } s/use Getopt::Long;/use Getopt::Long;\n$replace/g' "use lib ${PREFIX}/bin;" tRNAscan-SE
+perl -i~ -pe 'BEGIN{ $replace = shift } s/use Getopt::Long;/use Getopt::Long;\n$replace/g' "use lib '${PREFIX}/bin';" tRNAscan-SE
 #sed -i "/^our \$bindir/c\our bindir = ${PREFIX}\/bin\;" tRNAscan-SE
 #sed -i "/^our \$lib_dir/c\our lib_dir = ${PREFIX}\/bin\/tRNAscanSE\;" tRNAscan-SE
 
