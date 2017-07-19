da09fae86a7b5ef36572c9686d85a21a9aa392bd
Joerg Fallmann <fall@bioinf.uni-leipzig.de>
2017-07-19 14:43:41 +0200

    update

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -3,8 +3,8 @@
 make -j 1
 mkdir -p ${PREFIX}/bin
 
-mv -v tRNAscan-SE coves-SE covels-SE eufindtRNA trnascan-1.4 *.cm -t ${PREFIX}/bin
-mv -v tRNAscanSE ${PREFIX}/bin/.
+mv tRNAscan-SE coves-SE covels-SE eufindtRNA trnascan-1.4 *.cm -t ${PREFIX}/bin
+mv tRNAscanSE ${PREFIX}/bin/.
 mv tRNAscan-SE.src ${PREFIX}/bin/.
 
 cd  ${PREFIX}/bin
@@ -12,8 +12,8 @@ sed -i "/^use Getopt::Long;/c\use Getopt::Long;\\nuse lib ${PREFIX}\/bin;" tRNAs
 sed -i "/^our \$bindir/c\our bindir = ${PREFIX}\/bin;" tRNAscan-SE
 sed -i "/^our \$lib_dir/c\our lib_dir = ${PREFIX}\/bin\/tRNAscanSE;" tRNAscan-SE
 
-chmod +x coves-SE 
-chmod +x covels-SE 
-chmod +x eufindtRNA 
+chmod +x coves-SE
+chmod +x covels-SE
+chmod +x eufindtRNA
 chmod +x trnascan-1.4
 chmod +x tRNAscan-SE
