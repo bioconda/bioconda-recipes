f2fa85dcf9b543d7ecc1a4ccba6359d71351b58c
Patrick R. Wright <wrightp@informatik.uni-freiburg.de>
2017-07-21 10:46:00 +0200

    trying build changes for osx

--- recipes/domclust/1.0/build.sh
+++ recipes/domclust/1.0/build.sh
@@ -2,7 +2,13 @@
 
 make
 # copy binary to prefix folder
-cp -p bin-Linux/domclust $PREFIX/bin
+if [ -d 'bin-Linux' ] # linux
+then
+	cp -p bin-Linux/domclust $PREFIX/bin
+else # osx
+	cp -p bin-Darwin/domclust $PREFIX/bin
+fi
+
 cp -p Script/addtit.pl $PREFIX/bin
 cp -p Script/cmpr.pl $PREFIX/bin
 cp -p Script/convgraph.pl $PREFIX/bin
@@ -10,6 +16,12 @@ cp -p build_input/blast2homfile.pl $PREFIX/bin
 cp -p build_input/fasta2genefile.pl $PREFIX/bin
 
 # test
-bin-Linux/domclust -h
-bin-Linux/domclust tst/test.hom tst/test.gene
+if [ -d 'bin-Linux' ] # linux
+then
+	bin-Linux/domclust -h
+	bin-Linux/domclust tst/test.hom tst/test.gene
+else # osx
+	bin-Darwin/domclust -h
+	bin-Darwin/domclust tst/test.hom tst/test.gene
+fi
 
