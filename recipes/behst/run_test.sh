6913e35af5963598eec31b5438ccf2586211b87f
Ryan Dale <dalerr@niddk.nih.gov>
2017-07-23 15:21:34 -0400

    add timestamps to track example data download time

--- recipes/behst/run_test.sh
+++ recipes/behst/run_test.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 # download script dumps progress to stdout, causing too-large log output on
 # travis. Dump to temp log and only report if it fails.
-echo "Downloading (log in /tmp/download.log)"
+echo "[`date`] Downloading example data (log in /tmp/download.log)"
 download_behst_data.sh /tmp/behst --small &> /tmp/download.log || cat /tmp/download.log
+echo "[`date`] finished downloading example data"
 
 behst.py /tmp/behst/pressto_LUNG_enhancers.bed /tmp/behst
