2a39001266a0e91ebeee5ec0a100d66dd98804f0
Ryan Dale <dalerr@niddk.nih.gov>
2017-07-23 14:56:10 -0400

    hide download log

--- recipes/behst/run_test.sh
+++ recipes/behst/run_test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-download_behst_data.sh /tmp/behst --small
+set -euo pipefail
+
+# download script dumps progress to stdout, causing too-large log output on
+# travis. Dump to temp log and only report if it fails.
+echo "Downloading (log in /tmp/download.log)"
+download_behst_data.sh /tmp/behst --small &> /tmp/download.log || cat /tmp/download.log
 
 behst.py /tmp/behst/pressto_LUNG_enhancers.bed /tmp/behst
