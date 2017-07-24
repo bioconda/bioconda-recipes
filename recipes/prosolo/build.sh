e042889bdd688fa704931cb071014e66615f850f
David Laehnemann <david.laehnemann@hhu.de>
2017-07-24 18:55:50 +0200

    LIBRARY path addition -> .simulate_travis.py works

--- recipes/prosolo/build.sh
+++ recipes/prosolo/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -euo
 
 # build statically linked binary with Rust
-C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
+C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib:/usr/lib64 cargo install --root $PREFIX --verbose --debug
