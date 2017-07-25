8620a5aef3b18c1ac60d3b19495526be93766583
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-07-25 11:53:04 +0100

    fix linting issues

--- recipes/cgat-scripts/build.sh
+++ recipes/cgat-scripts/build.sh
@@ -9,5 +9,7 @@ sed -i'' -e '/# dependencies/,/dependency_links=dependency_links,/d' setup.py
 
 # proceed with actual installation
 #pip install 'bx-python==0.7.3'
-python setup.py install
+
+# https://bioconda.github.io/linting.html#setup-py-install-args
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
