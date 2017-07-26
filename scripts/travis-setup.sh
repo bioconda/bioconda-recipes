e1f5d27c99c4e2c0bcff95b486e2d59de9ac63e2
Ryan Dale <dalerr@niddk.nih.gov>
2017-07-26 08:59:32 -0400

    --force -> --overwrite

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -25,6 +25,6 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 pip install pyyaml
 sudo mkdir /anaconda
 sudo chown -R $USER /anaconda
-$SCRIPT_DIR/../simulate-travis.py --bootstrap /anaconda --force
+$SCRIPT_DIR/../simulate-travis.py --bootstrap /anaconda --overwrite
 /anaconda/bin/conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
 /anaconda/bin/conda config --add channels file://anaconda/conda-bld
