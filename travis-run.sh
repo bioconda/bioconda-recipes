76c166ac0cdd50c4e4ab045e6698a4cdccaf2b60
Ryan Dale <dalerr@niddk.nih.gov>
2017-07-28 11:11:09 -0400

    simplify travis-run

--- travis-run.sh
+++ travis-run.sh
@@ -6,15 +6,14 @@ set +u
 [[ -z $BIOCONDA_UTILS_TEST_TYPE ]] && BIOCONDA_UTILS_TEST_TYPE="all"
 set -u
 
-case $BIOCONDA_UTILS_TEST_TYPE in
-    pytest)
-        py.test test/ -v
-        ;;
-    docs)
-        ./build-docs.sh 2>&1 | grep -v "nonlocal image URI found" | grep -v "reading sources..." | grep -v "writing output..."
-        ;;
-    all)
-        py.test test/ -v
-        ./build-docs.sh 2>&1 | grep -v "nonlocal image URI found" | grep -v "reading sources..." | grep -v "writing output..."
-        ;;
-esac
+if [ $BIOCONDA_UTILS_TEST_TYPE == 'pytest' ] || [ $BIOCONDA_UTILS_TEST_TYPE == 'all' ]; then
+
+  py.test test/ -v
+
+fi
+
+if [ $BIOCONDA_UTILS_TEST_TYPE == 'docs' ] || [ $BIOCONDA_UTILS_TEST_TYPE == 'all' ]; then
+
+  ./build-docs.sh 2>&1 | grep -v "nonlocal image URI found" | grep -v "reading sources..." | grep -v "writing output..."
+
+fi
