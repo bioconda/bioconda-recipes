f738b39f3c2a52c17b110e923630c9ae72eaf264
James Abbott <jamesa@wssb-jamesa.sk.med.ic.ac.uk>
2017-07-28 15:53:11 +0100

    Simplify OSX args...

--- recipes/abyss/1.5.2/build.sh
+++ recipes/abyss/1.5.2/build.sh
@@ -4,9 +4,8 @@ mkdir -p $PREFIX/bin
 
 if [[ $(uname) == "Darwin" ]]; then
 	echo "Configuring for OSX..."
-  	export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include/boost -I$PREFIX/include --stdlib=libstdc++"
-	export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/boost
-	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  	export CPPFLAGS="${CPPFLAGS} --stdlib=libstdc++"
+	export CXXFLAGS="${CXXFLAGS} --stdlib=libc++"
 else
 	echo "Configuring for Linux..."
   	export CPPFLAGS="-I$PREFIX/include" 
