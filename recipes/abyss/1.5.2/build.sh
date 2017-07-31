24547c928f4b8fa59e37685631953c43b989ef96
James Abbott <jamesa@wssb-jamesa.sk.med.ic.ac.uk>
2017-07-31 10:48:36 +0100

    Add CXXFLAGS for OSX since --stdlib=libstdc++ not being passed during linking

--- recipes/abyss/1.5.2/build.sh
+++ recipes/abyss/1.5.2/build.sh
@@ -5,6 +5,7 @@ mkdir -p $PREFIX/bin
 if [[ $(uname) == "Darwin" ]]; then
 	echo "Configuring for OSX..."
   	export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include -I$PREFIX/include/boost --stdlib=libstdc++"
+  	export CXXFLAGS="${CXXFLAGS} --stdlib=libstdc++"
 else
 	echo "Configuring for Linux..."
   	export CPPFLAGS="-I$PREFIX/include" 
