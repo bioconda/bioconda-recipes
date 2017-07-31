0ba2b0802ca822dbbc4168f46a4bed41f6a5f48b
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-07-31 17:22:54 +0300

    Revert changes

--- recipes/hera/build.sh
+++ recipes/hera/build.sh
@@ -2,10 +2,10 @@
 
 mkdir -p $PREFIX/bin
 
-export CFLAGS="-I$PREFIX/include -fgnu89-inline -O2 -D USE_JEMALLOC -w -lrt -lz"
+export CFLAGS="-I$PREFIX/include -fgnu89-inline -O2 -D USE_JEMALLOC  -w -lrt -lz"
 export LIBS="-pthread -lm ${PREFIX}/lib/libz.* -lm ${PREFIX}/lib/libjemalloc.* -lm ${PREFIX}/lib/libhdf5.*  -lm ${PREFIX}/lib/libhdf5_hl.* -ldl -lm ${PREFIX}/lib/libdivsufsort64.*"
 
-./configure --prefix=$PREFIX
+./configure
 
 make
 cp build/hera $PREFIX/bin
