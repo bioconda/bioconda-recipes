ef7b98fcc36ed1a6db37b0eb4afb4d0c322b4ff2
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-07-31 16:46:03 +0300

    Skip osx build

--- recipes/hera/build.sh
+++ recipes/hera/build.sh
@@ -2,10 +2,11 @@
 
 mkdir -p $PREFIX/bin
 
-export CFLAGS="-I$PREFIX/include -fgnu89-inline -O2 -D USE_JEMALLOC  -w -lrt -lz"
-export LIBS="-pthread -lm ${PREFIX}/lib/libz.a -lm ${PREFIX}/lib/libjemalloc.a -lm ${PREFIX}/lib/libhdf5.a  -lm ${PREFIX}/lib/libhdf5_hl.a -ldl -lm ${PREFIX}/lib/libdivsufsort64.*"
+export CFLAGS="-I$PREFIX/include -fgnu89-inline -O2 -D USE_JEMALLOC -w -lrt -lz"
+export LIBS="-pthread -lm ${PREFIX}/lib/libz.* -lm ${PREFIX}/lib/libjemalloc.* -lm ${PREFIX}/lib/libhdf5.*  -lm ${PREFIX}/lib/libhdf5_hl.* -ldl -lm ${PREFIX}/lib/libdivsufsort64.*"
 
-./configure 
+./configure --prefix=$PREFIX
+
 make
 cp build/hera $PREFIX/bin
 cp build/hera_build $PREFIX/bin
