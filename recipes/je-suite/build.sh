93bdc40fb4b98d6261fa297a60e34beffad44396
Jelle Scholtalbers <j.scholtalbers@gmail.com>
2017-08-01 16:24:16 +0200

    add je-suite - a suite to handle barcoded fastq files with (or without) Unique Molecule Identifiers (UMIs) and filter read duplicates using these UMIs

---
+++ recipes/je-suite/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+bindir=$PREFIX/bin
+mkdir -p $bindir
+cp -r ./* $outdir/
+sed -i -r 's@^DIR=.+@DIR='$outdir'@g' $outdir/je
+ln -s $outdir/je $PREFIX/bin/je
+chmod 0755 "${PREFIX}/bin/je"
