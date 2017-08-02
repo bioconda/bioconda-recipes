9e78090ff5d8202fb679ebf02288727c5de2d2ee
Cashalow <sachalau@gmail.com>
2017-08-02 15:00:51 +0200

    adding correct link for python site packages

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+ ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/gt
+
+
 $PYTHON setup.py install
 
-
- ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python${CONDA_PY}/site-packages/gt
 
