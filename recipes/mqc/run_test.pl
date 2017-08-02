eb5176c7d2d7518099b1406f6df646c31cc86263
sfverbru <steven_verbruggen@ugent.be>
2017-08-02 16:16:22 +0200

    add recipe files

---
+++ recipes/mqc/run_test.pl
@@ -0,0 +1,11 @@
+# Test file for testing Perl modules installation
+$|=1;
+
+use strict;
+use warnings;
+use DBI;
+use Data::Dumper;
+use Getopt::Long;
+use v5.10;
+use Parallel::ForkManager;
+use Cwd;
---
+++ recipes/mqc/run_test.py
@@ -0,0 +1,22 @@
+#Test Python modules installation
+import traceback
+import getopt
+from collections import defaultdict
+import os
+import shutil
+import sqlite3
+import csv
+import stat
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from matplotlib.colors import hex2color
+from matplotlib import cm
+import seaborn as sns
+from matplotlib.ticker import ScalarFormatter
+import re
+import time
