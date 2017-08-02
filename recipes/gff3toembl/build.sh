cdab985519a84d07ddd11aac0fffd2457d2e123b
Cashalow <sachalau@gmail.com>
2017-08-02 11:07:32 +0200

    changing python version

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -3,5 +3,5 @@
 $PYTHON setup.py install
 
 
- ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python${TRAVIS_PYTHON_VERSION}/site-packages/gt
+ ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python${CONDA_PY}/site-packages/gt
 
