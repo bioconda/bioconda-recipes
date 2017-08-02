114b9e72bf24b6b0b603da3217b61ed16ad1eede
Jelle Scholtalbers <j.scholtalbers@gmail.com>
2017-08-02 11:45:35 +0200

    je-suite: fix sed cmd for osx

--- recipes/je-suite/build.sh
+++ recipes/je-suite/build.sh
@@ -6,6 +6,7 @@ mkdir -p $outdir
 bindir=$PREFIX/bin
 mkdir -p $bindir
 cp -r ./* $outdir/
-sed -i -r 's@^DIR=.+@DIR='$outdir'@g' $outdir/je
+sed -i'.bak' -E 's@^DIR=.+@DIR='$outdir'@g' $outdir/je
+rm -f $outdir/je.bak
 ln -s $outdir/je $PREFIX/bin/je
 chmod 0755 "${PREFIX}/bin/je"
