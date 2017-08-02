7e252bc3b9973e700749979a5132d16145b803cd
Cashalow <sachalau@gmail.com>
2017-08-02 15:27:58 +0200

    adding fix for python version 34 and 35 of urllib.parse

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
- ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/gt
 
+if [ "CONDA_PY" != "27" ]
+then sed -i "s/urllib/urllib.parse/" gff3toembl/EMBLContig.py
+fi
 
 $PYTHON setup.py install
 
