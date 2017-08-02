976bf032a47de91ca207e55092abc07fcc683a2e
Cashalow <sachalau@gmail.com>
2017-08-02 10:58:45 +0200

    testing with travis version value

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -3,8 +3,5 @@
 $PYTHON setup.py install
 
 
- mv ${PREFIX}/include/genometools/genometools.h ${PREFIX}/include/genometools/gt.h 
-
-
- ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python3.5/site-packages/gt
+ ln -s ${PREFIX}/include/genometools/ ${PREFIX}/lib/python${TRAVIS_PYTHON_VERSION}/site-packages/gt
 
