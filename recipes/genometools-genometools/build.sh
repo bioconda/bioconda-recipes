26ad2847b4f82edcd94b95306453ae977cb55e3a
Cashalow <sachalau@gmail.com>
2017-08-03 16:24:47 +0200

    installing python dependencies the correct way...

--- recipes/genometools-genometools/build.sh
+++ recipes/genometools-genometools/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 
-cp -R gtpython/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/
+#cp -R gtpython/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/
 
 #sed -i.bak "s/DEPLIBS:=/DEPLIBS:=-lsupc++ /g" Makefile
 
 #make -n > log.txt 
 #grep "lsupc++" log.txt
-make useshared=yes
+make
 export prefix=$PREFIX
 make install 
 
-
+cd gtpython
+$PYTHON setup.py install
 
