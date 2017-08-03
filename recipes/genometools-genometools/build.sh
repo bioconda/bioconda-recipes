2c211f6e3f84beb2aa52bd3c55da0e6464112bd1
Cashalow <sachalau@gmail.com>
2017-08-03 14:40:58 +0200

    trying with shared libraries

--- recipes/genometools-genometools/build.sh
+++ recipes/genometools-genometools/build.sh
@@ -3,7 +3,11 @@
 
 cp -R gtpython/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/
 
-make 
+#sed -i.bak "s/DEPLIBS:=/DEPLIBS:=-lsupc++ /g" Makefile
+
+#make -n > log.txt 
+#grep "lsupc++" log.txt
+make useshared=yes
 export prefix=$PREFIX
 make install 
 
