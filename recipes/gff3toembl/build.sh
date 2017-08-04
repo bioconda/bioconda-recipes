fa061d845c706bc6c5cc6649ea13051b21d2e571
Cashalow <sachalau@gmail.com>
2017-08-04 15:20:05 +0200

    adding test on conda_py version and linking of the site packages folder

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
-if [ "CONDA_PY" != "27" ]
-then sed -i "s/urllib/urllib.parse/" gff3toembl/EMBLContig.py
+if [ "${CONDA_PY}" != "27" ]
+then
+    sed -i "s/urllib/urllib.parse/" gff3toembl/EMBLContig.py
+    ln -s ${PREFIX}/lib/python2.7/site-packages/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/gt
 fi
 
+
+
 $PYTHON setup.py install
+
 
 
