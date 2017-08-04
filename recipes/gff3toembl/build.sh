19906a3e297f45b28650fa821cd400ae8329aa88
Cashalow <sachalau@gmail.com>
2017-08-04 15:39:44 +0200

    sed command for mac

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -3,7 +3,7 @@
 
 if [ "${CONDA_PY}" != "27" ]
 then
-    sed -i "s/urllib/urllib.parse/" gff3toembl/EMBLContig.py
+    sed -i.bak "s/urllib/urllib.parse/" gff3toembl/EMBLContig.py
     ln -s ${PREFIX}/lib/python2.7/site-packages/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/gt
 fi
 
