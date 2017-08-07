de9cc17ec3a42e8baaf5b8e965a534e03af081d1
Etienne Kornobis <ekornobis@gmail.com>
2017-08-07 15:32:45 +0200

    First recipe for IRFinder

---
+++ recipes/irfinder/build.sh
@@ -0,0 +1,6 @@
+mv bin/AdaptorDetect.pl $PREFIX/bin
+mv bin/analysisWithLowReplicates.pl $PREFIX/bin
+mv bin/analysisWithNoReplicates.pl $PREFIX/bin
+mv bin/DESeq2Constructor.R $PREFIX/bin
+mv bin/IRFinder $PREFIX/bin
+mv bin/util $PREFIX/bin
