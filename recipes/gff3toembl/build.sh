758620ba88a900469a8f0648d239b6ad9b04bd0e
Cashalow <sachalau@gmail.com>
2017-08-07 11:01:04 +0200

    using six python library to resolve urllib problem

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+sed -i.bak "s/from urllib/from six.moves.urllib.parse/" gff3toembl/EMBLContig.py
 
 if [ "${CONDA_PY}" != "27" ]
 then
-    sed -i.bak "s/urllib/urllib.parse/" gff3toembl/EMBLContig.py
     ln -s ${PREFIX}/lib/python2.7/site-packages/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/gt
 fi
 
