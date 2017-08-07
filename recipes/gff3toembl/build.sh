3cb91c48da2c4c6f6efbef5592c162bbb166559a
Cashalow <sachalau@gmail.com>
2017-08-07 12:11:44 +0200

    removing sed line

--- recipes/gff3toembl/build.sh
+++ recipes/gff3toembl/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sed -i.bak "s/from urllib/from six.moves.urllib.parse/" gff3toembl/EMBLContig.py
-
 if [ "${CONDA_PY}" != "27" ]
 then
     ln -s ${PREFIX}/lib/python2.7/site-packages/gt ${PREFIX}/lib/python${CONDA_PY:0:1}.${CONDA_PY:1:2}/site-packages/gt
