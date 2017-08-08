d770476058154c2ae707cdfaa5fd6533642a644e
Etienne Kornobis <ekornobis@gmail.com>
2017-08-08 12:50:02 +0200

    Other build construct

--- recipes/irfinder/build.sh
+++ recipes/irfinder/build.sh
@@ -1,6 +1,8 @@
-mv bin/AdaptorDetect.pl $PREFIX/bin
-mv bin/analysisWithLowReplicates.pl $PREFIX/bin
-mv bin/analysisWithNoReplicates.pl $PREFIX/bin
-mv bin/DESeq2Constructor.R $PREFIX/bin
-mv bin/IRFinder $PREFIX/bin
-mv bin/util $PREFIX/bin
+#!/bin/sh
+
+progDir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $progDir
+mv bin $progDir
+
+mkdir -p $PREFIX/bin
+ln -s $progDir/bin/IRFinder $PREFIX/bin/IRFinder
