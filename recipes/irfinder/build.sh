d2d46b888b5054eb6e35b4d77d33abc557f1a1b5
Etienne Kornobis <ekornobis@gmail.com>
2017-08-08 16:17:30 +0200

    recipe with Compilation from source when possible

--- recipes/irfinder/build.sh
+++ recipes/irfinder/build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-progDir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $progDir
-mv bin $progDir
+cd $SRC_DIR/src/irfinder && make && mv irfinder $SRC_DIR/bin
+cd $SRC_DIR/src/trim && make && mv trim $SRC_DIR/bin
+cd $SRC_DIR/src/winflat && make && mv winflat $SRC_DIR/bin
+
+cd $PREFIX
+
+irfinder_home=$PREFIX/opt/irfinder-$PKG_VERSION
+mkdir -p $irfinder_home
+cp -r $SRC_DIR/* $irfinder_home
 
 mkdir -p $PREFIX/bin
-ln -s $progDir/bin/IRFinder $PREFIX/bin/IRFinder
+ln -s $irfinder_home/bin/IRFinder $PREFIX/bin/IRFinder
