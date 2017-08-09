612df184eea03d68c6fb4d36847879557355d791
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-09 11:33:50 -0400

    improved config-gen script test

--- recipes/busco/run_test.sh
+++ recipes/busco/run_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -euo pipefail
+
+(generate-busco-config.py -h | grep "usage:") || exit 1
+
 # The directory in which generate-busco-config.py is stored should itself be
 # found in the "path = " lines of its output
 d=$(dirname $(which generate-busco-config.py))
-generate-busco-config.py | grep "path = " | grep $d
+(generate-busco-config.py | grep "path = " | grep $d) || exit 1
