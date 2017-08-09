a858782a2fa306cc4e2bc40d8a7a6b6b5222420e
Cashalow <sachalau@gmail.com>
2017-08-09 17:32:17 +0200

    fixing xtract problem

--- recipes/entrez-direct/build.sh
+++ recipes/entrez-direct/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mv * "$PREFIX/bin/"
+sh "setup.sh"
+mv edirect.pl efetch epost efilter eproxy einfo econtact elink espell enotify esummary entrez-phrase-search xtract "$PREFIX/bin/"
 mkdir -p "$PREFIX/home"
 export HOME="$PREFIX/home"
-sh "$PREFIX/bin/setup.sh"
+
