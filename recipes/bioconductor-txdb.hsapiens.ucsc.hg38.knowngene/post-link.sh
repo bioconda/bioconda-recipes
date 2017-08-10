9a537cac5f76491aed5b0b9e703684900b117eef
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-10 13:19:34 -0400

    add post-link test in txdb.hsapiens.ucsc.hg38.knowngene

---
+++ recipes/bioconductor-txdb.hsapiens.ucsc.hg38.knowngene/post-link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+STAGING=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG-BUILDNUM
+mkdir -p $STAGING
+wget -O- -q http://bioconductor.org/packages/3.5/data/annotation/src/contrib/TxDb.Hsapiens.UCSC.hg38.knownGene_3.4.0.tar.gz > $STAGING/pkg.tar.gz
+R CMD INSTALL --build  $STAGING/pkg.tar.gz
