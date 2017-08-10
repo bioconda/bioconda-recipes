f84d768b0452d1a680d043f388cf22d1737aafd6
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-10 13:21:57 -0400

    clean up downloaded tarball

--- recipes/bioconductor-txdb.hsapiens.ucsc.hg38.knowngene/post-link.sh
+++ recipes/bioconductor-txdb.hsapiens.ucsc.hg38.knowngene/post-link.sh
@@ -2,4 +2,5 @@
 STAGING=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG-BUILDNUM
 mkdir -p $STAGING
 wget -O- -q http://bioconductor.org/packages/3.5/data/annotation/src/contrib/TxDb.Hsapiens.UCSC.hg38.knownGene_3.4.0.tar.gz > $STAGING/pkg.tar.gz
-R CMD INSTALL --build  $STAGING/pkg.tar.gz
+R CMD INSTALL --build $STAGING/pkg.tar.gz
+rm $STAGING/pkg.tar.gz
