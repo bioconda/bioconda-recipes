6285f2860ac8cf680c9f4670b51be04f245f554e
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-10 17:31:21 -0400

    fix post-link

--- recipes/busco/post-link.sh
+++ recipes/busco/post-link.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SHARE=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/config
-sed -i.bak "s|^path = .*$|path = $PREFIX/bin|g" > $PREFIX/config/config.ini
+sed "s|^path = .*$|path = $PREFIX/bin|g" $SHARE/config.ini.default > $PREFIX/config/config.ini
