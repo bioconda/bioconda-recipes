52f8a19b6b5cfe873f62c8517aef04388b5100d8
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-10 17:31:37 -0400

    fix tests

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
 mkdir -p $PREFIX/bin/
 cp scripts/run_BUSCO.py $PREFIX/bin
 cp scripts/generate_plot.py $PREFIX/bin
+
 SHARE=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $SHARE
 cp config/config.ini.default $SHARE/config.ini.default
