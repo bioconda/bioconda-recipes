20225d9b23e843bae9d8d12302043f2d306b2ccb
Amos Raphenya <raphenar@mcmaster.ca>
2017-08-13 08:14:55 -0400

    removes comments

--- recipes/rgi/3.2.1/build.sh
+++ recipes/rgi/3.2.1/build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Platform: Mac"
-
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Platform: Linux"
-fi
-
-# install setup
 $PYTHON setup.py install
