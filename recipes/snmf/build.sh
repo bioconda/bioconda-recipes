82f2223e5e55998a403d5c607738394d03b98c3d
avengerpb <datlequoc96@gmail.com>
2017-08-14 11:35:37 +0700

    Fix recipe

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -2,14 +2,13 @@
 
 snmf=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $snmf
-cp -r ./* $snmf/
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
-rm -f $snmf/Snmf.bak
-chmod +x $snmf/snmf.sh
-chmod +x $snmf/plink
-chmod +x $snmf/install.command
-make $snmf/install.command
-chmod 777 $snmf/Snmf.pl
 mkdir -p $PREFIX/bin
+cp -r ./* $PREFIX/bin
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
+rm -f $PREFIX/Snmf.bak
+chmod +x $PREFIX/snmf.sh
+chmod +x $PREFIX/plink
+chmod +x $PREFIX/install.command
+make $PREFIX/install.command
+chmod 777 $PREFIX/Snmf.pl
 mkdir -p $PREFIX/opt/var
-export PATH=$snmf:$PATH >> ./bash_profile
