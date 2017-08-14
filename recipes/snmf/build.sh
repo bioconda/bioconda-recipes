91b8023fb5accdc102a822ad49a06fd94470b75b
avengerpb <datlequoc96@gmail.com>
2017-08-14 11:53:54 +0700

    fixed

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -4,7 +4,7 @@ snmf=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $snmf
 mkdir -p $PREFIX/bin
 cp -r ./* $PREFIX/bin
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PPREFIX/bin/nmf.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PPREFIX/bin/snmf.pl
 rm -f $PREFIX/bin/Snmf.bak
 chmod +x $PREFIX/bin/snmf.sh
 chmod +x $PREFIX/bin/plink
