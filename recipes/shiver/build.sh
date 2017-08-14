7cb2eb4019082f66830f2da48b5c65f10d8b279a
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-08-14 19:48:45 -0400

    added recipe for SHIVER - Sequences from HIV Easily Reconstructed

---
+++ recipes/shiver/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+mkdir -p $PREFIX/bin
+cp ${SRC_DIR}/config.sh $PREFIX/bin
+cp ${SRC_DIR}/shiver_align_contigs.sh $PREFIX/bin
+cp ${SRC_DIR}/shiver_full_auto.sh $PREFIX/bin
+cp ${SRC_DIR}/shiver_funcs.sh $PREFIX/bin
+cp ${SRC_DIR}/shiver_init.sh $PREFIX/bin
+cp ${SRC_DIR}/shiver_map_reads.sh $PREFIX/bin
+cp ${SRC_DIR}/shiver_reprocess_bam.sh $PREFIX/bin
+cp -r ${SRC_DIR}/tools $PREFIX/bin/shiver_tools
