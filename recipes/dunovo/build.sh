ba450b0a4ff6c469bca9f469201cd61241a0937f
Nick <nmapsy@gmail.com>
2017-08-15 15:30:06 -0400

    Du Novo: Install submodules in build.sh with wget. - bioconda cannot yet use multiple source urls.   - So manually download them in build.sh instead.

--- recipes/dunovo/build.sh
+++ recipes/dunovo/build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Compile binaries and move them to lib.
 make
 mv *.so $PREFIX/lib
-mv ET $PREFIX/lib/ET
-mv utillib $PREFIX/lib/utillib
+
+# Download submodules and move them to lib.
+wget 'https://github.com/NickSto/utillib/archive/v0.1.0.tar.gz'
+tar -zxvpf v0.1.0.tar.gz
+mv utillib-0.1.0 $PREFIX/lib/utillib
+rm v0.1.0.tar.gz
+wget 'https://github.com/NickSto/ET/archive/v0.1.0.tar.gz'
+tar -zxvpf v0.1.0.tar.gz
+mv ET-0.1.0 $PREFIX/lib/ET
+rm v0.1.0.tar.gz
+
+# Move scripts to lib and link to them from bin.
 for script in *.awk *.sh *.py; do
   mv $script $PREFIX/lib
   ln -s ../lib/$script $PREFIX/bin
