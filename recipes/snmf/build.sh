ecf22cdee8f7c367e5f7f7b7431ef1a4b27917da
avengerpb <datlequoc96@gmail.com>
2017-08-15 12:28:13 +0700

    fix build dir

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
-
-snmf=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+ #!/bin/bash
+snmf=$PREFIX/opt/snmf/
 mkdir -p $snmf
-mkdir -p $PREFIX/bin
-cp -r ./* $PREFIX/bin
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/Snmf.pl
-rm -f $PREFIX/bin/Snmf.bak
-chmod +x $PREFIX/bin/snmf.sh
-chmod +x $PREFIX/bin/plink
-chmod +x $PREFIX/bin/install.command
-make $PREFIX/bin/install.command
-chmod 777 $PREFIX/bin/Snmf.pl
-mkdir -p $PREFIX/bin/opt/var
+mkdir -p $PREFIX/bin/
+cp -rf temp/*  $snmf/
+rm -rf temp/
+cp $snmf/sNMF_CL_v1.2.zip .
+unzip *.zip
+cp -rf sNMF_CL_v1.2/* .
+rm -rf sNMF_CL_v1.2/
+chmod +x install.command
+bash install.command
+cp -rf ./* $snmf/
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
+rm -f $snmf/Snmf.bak
+chmod +x $snmf/snmf.sh
+chmod +x $snmf/plink
+chmod 777 $snmf/Snmf.pl
+cp $snmf/snmf.sh $PREFIX/bin/
