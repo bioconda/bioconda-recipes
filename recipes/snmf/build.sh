388ef6b4206d7d11df308deabc067a42c91dc2f7
avengerpb <datlequoc96@gmail.com>
2017-08-16 11:33:06 +0700

    fix bug

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -1,14 +1,8 @@
  #!/bin/bash
+
 snmf=$PREFIX/opt/snmf/
 mkdir -p $snmf
 mkdir -p $PREFIX/bin/
-cp -rf ./*  $snmf/
-cp $snmf/sNMF_CL_v1.2.zip .
-unzip *.zip
-cp -rf sNMF_CL_v1.2/* .
-rm -rf sNMF_CL_v1.2/
-chmod +x install.command
-bash install.command
 cp -rf ./* $snmf/
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
 rm -f $snmf/Snmf.bak
