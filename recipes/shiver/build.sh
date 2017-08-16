2ba54b4f63c82a8fe42cc3ea3f4a15b4a79f5b19
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-08-16 10:29:37 -0400

    shiver -- fixed fastaq invocation

--- recipes/shiver/build.sh
+++ recipes/shiver/build.sh
@@ -11,4 +11,5 @@ cp ${SRC_DIR}/shiver_init.sh $PREFIX/bin
 cp ${SRC_DIR}/shiver_map_reads.sh $PREFIX/bin
 cp ${SRC_DIR}/shiver_reprocess_bam.sh $PREFIX/bin
 cp ${SRC_DIR}/shiver_fastaq $PREFIX/bin
+chmod u+x $PREFIX/bin/shiver_fastaq
 cp -r ${SRC_DIR}/tools $PREFIX/bin/shiver_tools
