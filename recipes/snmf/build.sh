ae25e05612643757c3ea50d3699a12996e89bb7b
avengerpb <datlequoc96@gmail.com>
2017-08-17 10:51:23 +0700

    update recipe

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -8,5 +8,5 @@ sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
 rm -f $snmf/Snmf.bak
 chmod +x $snmf/snmf.sh
 chmod +x $snmf/plink
-chmod 777 $snmf/Snmf.pl
-cp $snmf/snmf.sh $PREFIX/bin/
+chmod +x $snmf/Snmf.pl
+ln -s $snmf/snmf.sh $PREFIX/bin/
