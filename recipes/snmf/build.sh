57d454b47e1a0e4fb816efee3c2b433f823ed056
avengerpb <datlequoc96@gmail.com>
2017-08-17 21:54:39 +0700

    fix bug

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -6,8 +6,8 @@ mkdir -p $PREFIX/bin/
 cp -rf ./* $snmf/
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $snmf/Snmf.pl
 rm -f $snmf/Snmf.bak
-chmod 777 $snmf
 chmod +x $snmf/snmf.sh
 chmod +x $snmf/plink
 chmod +x $snmf/Snmf.pl
+chmod 777 $snmf/
 ln -s $snmf/snmf.sh $PREFIX/bin/
