7a54a1d8ef9108f02660f3f28bffab6eeb06fa21
avengerpb <datlequoc96@gmail.com>
2017-08-18 17:12:28 +0700

    fix

--- recipes/snmf/build.sh
+++ recipes/snmf/build.sh
@@ -4,10 +4,9 @@ snmf=$PREFIX/opt/snmf
 mkdir -p $snmf
 chmod 777 $snmf
 mkdir -p $PREFIX/bin/
-cp -rf ./* $snmf/
+cp -r * $snmf/
 chmod +x $snmf/snmf.sh
 chmod +x $snmf/plink
 chmod +x $snmf/Snmf.pl
-ls -l
-sed -i.bak '$ a\ > bash '$snmf'/snmf.sh vcf output logs best_k_output best_k_logfile kmin kmax groups threshold_group' $snmf/run.sh
+echo "$snmf/snmf.sh vcf output logs best_k_output best_k_logfile kmin kmax groups threshold_group" >> $snmf/run.sh
 ln -s $snmf/run.sh $PREFIX/bin/
