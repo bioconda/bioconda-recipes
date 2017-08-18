77f459bd4a865df75365813821e1f87343696d07
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-18 19:54:32 -0400

    try cppflags for osx

--- recipes/r-spp/build.sh
+++ recipes/r-spp/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
 if [ `uname` == Darwin ]; then
         export LDFLAGS=-L${PREFIX}/lib
+        export CPPFLAGS="${CPPFLAGS} -ferror-limit=0"
 fi
 export BOOST_ROOT=${PREFIX}
-# R refuses to build packages that mark themselves as Priority: Recommended
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-
 $R CMD INSTALL --build .
