8378c11863368b49e6f407e4e1854ba5fc3173eb
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-18 21:42:15 -0400

    try other cflags

--- recipes/r-spp/build.sh
+++ recipes/r-spp/build.sh
@@ -2,6 +2,8 @@
 if [ `uname` == Darwin ]; then
         export LDFLAGS=-L${PREFIX}/lib
         export CPPFLAGS="${CPPFLAGS} -ferror-limit=0"
+        export CFLAGS="${CFLAGS} -ferror-limit=0"
+        export CXXFLAGS="${CXXFLAGS} -ferror-limit=0"
 fi
 export BOOST_ROOT=${PREFIX}
 $R CMD INSTALL --build .
