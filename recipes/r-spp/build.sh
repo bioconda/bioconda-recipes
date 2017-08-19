f98958a1e79c965e0a98049f3591f4b5c39d3510
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-18 21:52:15 -0400

    try always using flags

--- recipes/r-spp/build.sh
+++ recipes/r-spp/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-if [ `uname` == Darwin ]; then
-        export LDFLAGS=-L${PREFIX}/lib
-        export CPPFLAGS="${CPPFLAGS} -ferror-limit=0"
-        export CFLAGS="${CFLAGS} -ferror-limit=0"
-        export CXXFLAGS="${CXXFLAGS} -ferror-limit=0"
-fi
+export LDFLAGS=-L${PREFIX}/lib
+export CPPFLAGS="${CPPFLAGS} -ferror-limit=0"
+export CFLAGS="${CFLAGS} -ferror-limit=0"
+export CXXFLAGS="${CXXFLAGS} -ferror-limit=0"
 export BOOST_ROOT=${PREFIX}
 $R CMD INSTALL --build .
