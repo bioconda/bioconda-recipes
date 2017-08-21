5a44676d9e48b03d1d32863e60999000451514a5
avengerpb <datlequoc96@gmail.com>
2017-08-21 23:50:46 +0700

    fixing

--- recipes/vcf2hapmap/build.sh
+++ recipes/vcf2hapmap/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-v2h=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
-mkdir -p $v2h
-cp -r ./* $v2h
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $v2h/VCF2FastaAndHapmap.pl
-rm -f $v2h/VCF2FastaAndHapma.bak
-chmod +x $v2h/vcf2FastaAndHapmap.sh
-chmod 777 $v2h/VCF2FastaAndHapmap.pl
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/opt/var
-ln -s $v2h/vcf2FastaAndHapmap.sh $PREFIX/bin/vcf2FastaAndHapmap.sh
-ln -s  $v2h/VCF2FastaAndHapmap.pl $PREFIX/bin/VCF2FastaAndHapmap.pl
+chmod +x vcf2FastaAndHapmap.sh
+chmod +x VCF2FastaAndHapmap.pl
+mv VCF2FastaAndHapmap.pl $PREFIX/bin
+mv vcf2FastaAndHapmap.sh $PREFIX/bin
