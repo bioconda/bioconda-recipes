75a04e089be725215e01afd280f1b06d6ee3cf7a
jacorvar <jacorvar@users.noreply.github.com>
2017-08-22 13:50:14 +0200

    Update build.sh
    Enable MPI support

--- recipes/hmmer/build.sh
+++ recipes/hmmer/build.sh
@@ -7,6 +7,6 @@ CXX=${PREFIX}/bin/g++
 # Fix broken configure option
 sed -i.bak -e 's/acx_maxopt_portable=$withval/acx_maxopt_portable=$enableval/' configure
 # --enable-portable-binary should be removed for the next HMMER release where this should be unnecessary
-./configure --enable-portable-binary --prefix=$PREFIX
+./configure --enable-portable-binary --prefix=$PREFIX --enable-mpi
 make -j4
 make install
