f17c6a8c3e980d55cbb9163ce5130bc54b116588
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-08-22 16:20:08 -0400

    use post-link to download the data

---
+++ recipes/antismash/post-link.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# this is a python script form the antismash package that will download the databases
+# needed for a fully functioning antismash.
+# when unistalling ALL data will be removed by pip.
+download-antismash-databases
