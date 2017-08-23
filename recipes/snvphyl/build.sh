603d5c2308367514c53386d581ccc9ea29caa687
takadonet <philipmabon@gmail.com>
2017-08-23 11:19:34 -0500

    removed all bcftools-snvphyl code from meta and build! It is all in the same repo.

--- recipes/snvphyl/build.sh
+++ recipes/snvphyl/build.sh
@@ -3,33 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-echo "Done!"
-
-echo "Fetching and install bcftools"
-
-bcftools='bcftools-1.5'
-
-curl -s -L https://github.com/samtools/bcftools/releases/download/1.5/bcftools-1.5.tar.bz2 > bcftools.tar.bz2
-tar -jxf bcftools.tar.bz2
-
-#clean up
-rm bcftools.tar.bz2
-
-cp bcfplugins/filter_snv_density.c $bcftools/plugins
-cd $bcftools
-
-./configure
-make
-
-#copy the compiled 
-cp plugins/filter_snv_density.so $DIR/bcfplugins/
-
-cd $DIR
-
-#temporary env variable for make test
-export PATH=$bcftools:$PATH
-export BCFTOOLS_PLUGINS=bcfplugins
-
 
 #temporary cpan modules
 cpanm -i Switch -L . || :
@@ -57,7 +30,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
     fi
-
-
-#remove bcftools
-rm -rf $bcftools/
