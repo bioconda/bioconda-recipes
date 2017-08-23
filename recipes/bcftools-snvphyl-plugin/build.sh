ae8a683ed293c176d653217b37aabb6edfe944f5
takadonet <philipmabon@gmail.com>
2017-08-23 12:19:18 -0500

    made a single source download because bioconda not working with conda v3 yet

--- recipes/bcftools-snvphyl-plugin/build.sh
+++ recipes/bcftools-snvphyl-plugin/build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+#downloading outside of meta.yaml because bioconda lint cannot handle multiple list till they more to verison 3 of bioconda
+
+curl -L -s http://gitlab-irida.corefacility.ca/analysis-pipelines/snvphyl-tools/repository/archive.tar.gz?ref=conda-dev > archive.tar.gz
+tar -zxvf archive.tar.gz
+
 #copy over custom bcftools plugin
-cp snvphyl/bcfplugins/filter_snv_density.c bcftools/plugins/
+cp snvphyl*/bcfplugins/filter_snv_density.c plugins/
 
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-cd bcftools/htslib*
+cd htslib*
 ./configure --prefix=$PREFIX --enable-libcurl CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
 make
 cd ..
