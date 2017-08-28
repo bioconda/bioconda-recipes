c8b8395789c3cadd75fe1f2b43f3c9a9a954fd0a
takadonet <philipmabon@gmail.com>
2017-08-28 08:46:10 -0500

    skip osx build for bcftools-snvphyl

--- recipes/snvphyl/build.sh
+++ recipes/snvphyl/build.sh
@@ -4,6 +4,9 @@
 # Make sure this goes in site
 perl Makefile.PL INSTALLDIRS=site
 make
-perl t/consolidate_vcfs.t #only testing script that uses bcftools,
+
+#only testing script that uses bcftools,
 #otherwise we timeout when testing on travis
+perl t/consolidate_vcfs.t 
+
 make install
