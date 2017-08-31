aee5ebb2fd207296d6cf313aa7bc11d61b97c2e2
peterjc <p.j.a.cock@googlemail.com>
2017-08-31 16:56:57 +0100

    Try using wrapper scripts

--- recipes/wolfpsort/build.sh
+++ recipes/wolfpsort/build.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-# Comes with i386 and SPARC binaries,
+# Setup the wrapper scripts under ${PREFIX}/bin/
+mkdir -p "${PREFIX}/bin"
+mv "${RECIPE_DIR}/runWolfPsortSummary.py" "${PREFIX}/bin/runWolfPsortSummary"
+mv "${RECIPE_DIR}/runWolfPsortHtmlTables.py" "${PREFIX}/bin/runWolfPsortHtmlTables"
+chmod 0755 "${PREFIX}/bin/runWolfPsortSummary"
+chmod 0755 "${PREFIX}/bin/runWolfPsortHtmlTables"
+
+# Setup tool itself under ${PREFIX}/opt/
+#
+# WoLF PSORT comes with internal binaries for i386 and SPARC only:
 # - runAddAlignmentToHtml
 # - wolfPredict
 # - WoLFPSORTpredictAndAlign
 cp bin/binByPlatform/binary-i386/* bin/
-
-mv * ${PREFIX}/
+mkdir -p ${PREFIX}/opt
+mv * ${PREFIX}/opt/
---
+++ recipes/wolfpsort/runWolfPsortHtmlTables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#Wrapper script to call WoLF PSORT from its own directory.
+import os
+import sys
+import subprocess
+saved_dir = os.path.abspath(os.curdir)
+# This wrapper will be in ${PREFIX}/bin/
+# and we want to chage to ${PREFIX}/opt/bin/
+os.chdir(os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "../opt/bin"))
+args = ["./runWolfPsortHtmlTables"] + sys.argv[1:]
+return_code = subprocess.call(args)
+os.chdir(saved_dir)
+sys.exit(return_code)
