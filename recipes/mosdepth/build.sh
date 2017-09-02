bfc87cadd07fbc63bc00cf1b5d676316eeb10915
chapmanb <chapmanb@50mail.com>
2017-09-02 05:55:37 -0400

    Update: mosdepth 1.0.5
    - Build in release mode and include bugfixes for chrM and large window
      size problems (brentp/mosdepth#6)

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -4,7 +4,7 @@ export BRANCH=master
 export PATH="nim-$BRANCH/bin/nim/bin:${PATH:+:$PATH}"
 bash ./scripts/install.sh
 export PATH="nim-$BRANCH/bin:${PATH:+:$PATH}"
-nim c -r mosdepth.nim
+nim c -d:release -r mosdepth.nim
 mkdir -p $PREFIX/bin
 chmod a+x mosdepth
 cp mosdepth $PREFIX/bin/mosdepth
