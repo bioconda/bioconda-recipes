2db07c60c4a893196ea71c4948be524ea4ff7175
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2017-09-04 18:25:03 +0200

    Added UROPAs auxillary scripts and switched to new version

--- recipes/uropa/build.sh
+++ recipes/uropa/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p $PREFIX/bin/uropa
+# Main program
 mv uropa.py $PREFIX/bin
+
+mkdir -p $PREFIX/bin/uropa
 mv uropa/* $PREFIX/bin/uropa
+
+# Auxillary scripts
+mv summary.R $PREFIX/bin
+mv reformat_output.R $PREFIX/bin
