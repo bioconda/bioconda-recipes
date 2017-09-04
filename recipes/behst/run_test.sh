9cbd5a864a5f113b02a55524e32a2702157cf70b
davidechicco <davide.chicco@gmail.com>
2017-09-04 14:13:34 -0400

    version 1.6

--- recipes/behst/run_test.sh
+++ recipes/behst/run_test.sh
@@ -7,4 +7,4 @@ echo "[`date`] Downloading example data (log in /tmp/download.log)"
 behst-download-data -d /tmp/behst --small &> /tmp/download.log || cat /tmp/download.log
 echo "[`date`] finished downloading example data"
 
-behst /tmp/behst/pressto_LUNG_enhancers.bed /tmp/behst
+behst /tmp/behst/pressto_LUNG_enhancers.bed -d /tmp/behst
