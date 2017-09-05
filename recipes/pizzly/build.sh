5902b0d9b3ffe584bdc882afa57a9b69b9a3b537
roryk <rory.kirchner@gmail.com>
2017-09-05 17:14:44 -0400

    Add python line to top of pizzly scripts.

--- recipes/pizzly/build.sh
+++ recipes/pizzly/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cp pizzly $PREFIX/bin
 wget https://raw.githubusercontent.com/pmelsted/pizzly/master/scripts/flatten_json.py -O $PREFIX/bin/pizzly_flatten_json.py
+sed -i.bak "1s@^@#!/usr/bin/env python\'$'\n@g" $PREFIX/bin/pizzly_flatten_json.py
 chmod +x $PREFIX/bin/pizzly_flatten_json.py
-wget https://github.com/pmelsted/pizzly/blob/master/scripts/get_fragment_length.py -O $PREFIX/bin/pizzly_get_fragment_length.py
-chmod +x $PREFIX/bin/pizzly_flatten_json.py
+wget https://raw.githubusercontent.com/pmelsted/pizzly/master/scripts/get_fragment_length.py -O $PREFIX/bin/pizzly_get_fragment_length.py
+sed -i.bak "1s@^@#!/usr/bin/env python\'$'\n@g" $PREFIX/bin/pizzly_get_fragment_length.py
+chmod +x $PREFIX/bin/pizzly_get_fragment_length.py
