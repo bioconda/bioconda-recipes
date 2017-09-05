3d58ec032d1c15a26e86df3c798ada7938ceb0a7
peterjc <p.j.a.cock@googlemail.com>
2017-09-05 13:59:39 +0100

    Debugging in wrapper script

--- recipes/wolfpsort/chdir_wrapper.py
+++ recipes/wolfpsort/chdir_wrapper.py
@@ -19,6 +19,9 @@ os.chdir(tool_path)
 if not os.path.isfile(tool_name):
     os.chdir(saved_dir)
     sys.exit("Could not find wrapped tool %s in %s" % (tool_name, tool_path))
+if not os.access(tool_name, os.X_OK):
+    os.chdir(saved_dir)
+    sys.exit("Wrapped tool %s in %s not executable" % (tool_name, tool_path))
 args = ["./" + tool_name] + sys.argv[1:]
 return_code = subprocess.call(args)
 os.chdir(saved_dir)
