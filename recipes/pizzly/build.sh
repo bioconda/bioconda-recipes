ddfb1547861c29d22fc2978e34bd7bd017605800
Rory Kirchner <rory.kirchner@gmail.com>
2017-09-06 03:41:36 -0400

    Add missing pizzly scripts. (#5953)
    This prepends a `pizzly_` prefix onto the scripts to make them more unique,
    since they are generically named.

--- recipes/pizzly/build.sh
+++ recipes/pizzly/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
 cp pizzly $PREFIX/bin
+wget https://raw.githubusercontent.com/pmelsted/pizzly/master/scripts/flatten_json.py -O $PREFIX/bin/pizzly_flatten_json.py
+chmod +x $PREFIX/bin/pizzly_flatten_json.py
+wget https://github.com/pmelsted/pizzly/blob/master/scripts/get_fragment_length.py -O $PREFIX/bin/pizzly_get_fragment_length.py
+chmod +x $PREFIX/bin/pizzly_flatten_json.py
