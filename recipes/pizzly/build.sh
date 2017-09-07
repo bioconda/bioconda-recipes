8ec9018cd87a52e1a6faf3a4f211430b58334688
roryk <rory.kirchner@gmail.com>
2017-09-07 12:18:44 -0400

    Pin pizzly script download to a commit.

--- recipes/pizzly/build.sh
+++ recipes/pizzly/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cp pizzly $PREFIX/bin
-wget https://raw.githubusercontent.com/pmelsted/pizzly/master/scripts/flatten_json.py -O $PREFIX/bin/pizzly_flatten_json.py
+blob=251989979ba2cb39e217a91aa7fefc85c554ebe1
+wget https://raw.githubusercontent.com/pmelsted/pizzly/${blob}/scripts/flatten_json.py -O $PREFIX/bin/pizzly_flatten_json.py
 sed -i.bak '1 i\#!/usr/bin/env python -Es' $PREFIX/bin/pizzly_flatten_json.py
 chmod +x $PREFIX/bin/pizzly_flatten_json.py
-wget https://raw.githubusercontent.com/pmelsted/pizzly/master/scripts/get_fragment_length.py -O $PREFIX/bin/pizzly_get_fragment_length.py
+wget https://raw.githubusercontent.com/pmelsted/pizzly/${blob}/scripts/get_fragment_length.py -O $PREFIX/bin/pizzly_get_fragment_length.py
 chmod +x $PREFIX/bin/pizzly_get_fragment_length.py
 sed -i.bak '1 i\#!/usr/bin/env python -Es' $PREFIX/bin/pizzly_get_fragment_length.py
