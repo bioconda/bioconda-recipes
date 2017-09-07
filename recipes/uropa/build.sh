a1cf58e210a7d1ce3bb0dcd1cdd8b065d0bb828c
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2017-09-07 16:10:16 +0200

    Upgraded to new upstream version 1.2.1

--- recipes/uropa/build.sh
+++ recipes/uropa/build.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-# Main program
-mv uropa.py $PREFIX/bin
-
-mkdir -p $PREFIX/bin/uropa
-mv uropa/* $PREFIX/bin/uropa
-
-# Auxillary scripts
-mv summary.R $PREFIX/bin
-mv reformat_output.R $PREFIX/bin
-mv utils/uropa2gtf.R $PREFIX/bin
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
