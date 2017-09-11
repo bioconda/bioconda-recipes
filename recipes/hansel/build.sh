3e7922a9b0cc1b990b4aebc780dfa3625f7fbabb
Matt <gopezm@myumanitoba.ca>
2017-09-11 09:19:59 -0500

    Removing SED tailoring for now, and adding "which" as a test command.

--- recipes/hansel/build.sh
+++ recipes/hansel/build.sh
@@ -4,4 +4,4 @@
 pip install heidelberg_subtyping
 
 #Change so that the system's python isn't hardcoded, instead uses current global python version.
-sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' ~/anaconda3/bin/heidelberg_subtyping
+#sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' ~/anaconda3/bin/heidelberg_subtyping
