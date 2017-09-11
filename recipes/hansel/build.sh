7c4b1ec6ac1c665bd0e99dbcc0f4817e12e0f11b
Matt <gopezm@myumanitoba.ca>
2017-09-11 08:46:12 -0500

    Fixing the directory within build.sh

--- recipes/hansel/build.sh
+++ recipes/hansel/build.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 #Change so that the system's python isn't hardcoded, instead uses current global python version.
-sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' $HOME/anaconda3/bin/heidelberg_subtyping
+sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' ~/anaconda3/bin/heidelberg_subtyping
 
 #Install Heidelburg Subtyping
 pip install heidelberg_subtyping
