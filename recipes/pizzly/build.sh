694b816e958113a1703d9a9b3a70b33c2dae6c6e
roryk <rory.kirchner@gmail.com>
2017-09-12 12:14:36 -0400

    Insert newline to make i\ POSIX compliant for OSX.

--- recipes/pizzly/build.sh
+++ recipes/pizzly/build.sh
@@ -2,8 +2,11 @@
 cp pizzly $PREFIX/bin
 blob=251989979ba2cb39e217a91aa7fefc85c554ebe1
 wget https://raw.githubusercontent.com/pmelsted/pizzly/${blob}/scripts/flatten_json.py -O $PREFIX/bin/pizzly_flatten_json.py
-sed -i.bak '1 i\#!/usr/bin/env python -Es' $PREFIX/bin/pizzly_flatten_json.py
+sed -i.bak '1 i\
+#!/usr/bin/env python -Es' $PREFIX/bin/pizzly_flatten_json.py
 chmod +x $PREFIX/bin/pizzly_flatten_json.py
+
 wget https://raw.githubusercontent.com/pmelsted/pizzly/${blob}/scripts/get_fragment_length.py -O $PREFIX/bin/pizzly_get_fragment_length.py
 chmod +x $PREFIX/bin/pizzly_get_fragment_length.py
-sed -i.bak '1 i\#!/usr/bin/env python -Es' $PREFIX/bin/pizzly_get_fragment_length.py
+sed -i.bak '1 i\
+#!/usr/bin/env python -Es' $PREFIX/bin/pizzly_get_fragment_length.py
