7aa570a134be3c9cd79828ff476f867a78585e2f
Matt <gopezm@myumanitoba.ca>
2017-09-12 08:32:52 -0500

    Changing repository as we have now migrated to the PHAC NML repository.

--- recipes/bio_hansel/build.sh
+++ recipes/bio_hansel/build.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 #Install Heidelburg Subtyping
-pip install heidelberg_subtyping
+pip install bio_hansel
 
 #Change so that the system's python isn't hardcoded, instead uses current global python version.
-#sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' ~/anaconda3/bin/heidelberg_subtyping
+#sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' ~/anaconda3/bin/bio_hansel
