4fcef8004b2865cbd5cb57dd11901a4cf00f1ac3
Matt <gopezm@myumanitoba.ca>
2017-09-12 11:40:54 -0500

    Fixing requirements and test command.

--- recipes/bio_hansel/build.sh
+++ recipes/bio_hansel/build.sh
@@ -1,7 +1,4 @@
 #!/bin/env bash
 
-#Install Heidelburg Subtyping
+#Install Bio_Hansel
 pip install bio_hansel
-
-#Change so that the system's python isn't hardcoded, instead uses current global python version.
-#sed -i 's|/#!/usr/bin/python|#!/usr/bin/env python|g' ~/anaconda3/bin/bio_hansel
