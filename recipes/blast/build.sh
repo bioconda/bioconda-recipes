81156af51a43409d3f39f47c2d46101f5cbc3f29
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2017-09-12 23:51:13 +0100

    Fix BLAST 2.6.0 when LD_LIBRARY_PATH is defined
    If `--with-hard-runpath` configure option is not used, BLAST programs
    search libraries first in the directories defined by the
    `LD_LIBRARY_PATH` environment variable, instead of using the rpath
    specified by conda.
    
    Also add patch to support Boost 1.64.0 and later.

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -2,7 +2,7 @@
 
 set -e -x -o pipefail
 
-mkdir -p $PREFIX/bin/
+mkdir -p $PREFIX/bin
 
 if [ "$(uname)" == "Darwin" ]; then
     echo "Platform: Mac"
@@ -13,7 +13,10 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Platform: Linux"
 
     cd $SRC_DIR/c++/
-    ./configure --prefix=$PREFIX
+    # --with-hard-runpath is needed otherwise BLAST programs would search
+    # libraries first in the directories defined by the LD_LIBRARY_PATH
+    # environment variable, instead of using the rpath specified by conda
+    ./configure --prefix=$PREFIX --with-hard-runpath
 
     make -j${CPU_COUNT}
 
