1ea48f178dfd58b0ff4b8a960291de69bc58c847
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-09-13 18:33:21 +0200

    Fixed typo

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,8 +3,8 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-#explictitly set dictory for GHC download
-export STACK-ROOT=".stack"
+#explicititly set dictory for GHC download
+export STACK_ROOT=".stack"
 stack setup
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
