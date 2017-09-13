214be938e38e9a23081727d6b9312ab44e5269b8
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-09-13 15:57:01 +0200

    Explicitly set stack root for compatibility with macos

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,9 +3,11 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-
-${PREFIX}/bin/stack setup
-${PREFIX}/bin/stack update
-${PREFIX}/bin/stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+#explictitly set dictory for GHC download
+export STACK-ROOT=".stack"
+stack setup
+stack update
+stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
+rm -r .stack
