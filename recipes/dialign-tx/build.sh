a083f9a606639eaa2bf0cff5cf4003efc91f8fd5
Jonathan Chung <jonathan.chung@regeneron.com>
2017-09-14 11:36:39 -0400

    Cleanup build.sh

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -5,15 +5,6 @@ export LDFLAGS="-L$PREFIX/lib"
 sed -i -e 's/-march=i686/-march=native/' ./source/Makefile
 cd ./source
 
-if [ ! -f Makefile ]; then
-    echo "Makefile not found!"
-fi
-
 make
-
-if [ ! -f dialign-txt ]; then
-    echo "dialign-tx not found!"
-fi
-
 mv dialign-tx $PREFIX/bin/
 
