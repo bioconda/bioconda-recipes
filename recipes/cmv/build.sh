8f51b1ea8968099e36285cd7c2c797810cca7f73
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-09-14 10:51:05 +0200

    Alternate path for stack

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -4,10 +4,10 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 #explicititly set dictory for GHC download
-export STACK_ROOT=".stack"
+export STACK_ROOT="${PREFIX}/stack"
 stack setup
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
-rm -r .stack
+rm -r "${PREFIX}/stack"
