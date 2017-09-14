a3700e091178d8f7a3900f8a08eeb61999a75246
Sasha Mikheyev <sasha [at] homologo.us>
2017-09-14 20:56:48 +1000

    changed added anaconda gcc

--- recipes/kWIP/build.sh
+++ recipes/kWIP/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+CC=${PREFIX}/bin/gcc
+CXX=${PREFIX}/bin/g++
 BINARY_HOME=$PREFIX/bin
 
 cd $SRC_DIR
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=${BINARY_HOME}
+cmake .. \
+	-DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
+    -DCMAKE_INSTALL_PREFIX=${BINARY_HOME}
 make
 ctest --verbose
 make install
