5fb6f01c926cd9cf7d980f4ee75567f4d5820e7e
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-09-15 17:27:56 +0200

    Fixed build.sh syntax

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -4,7 +4,8 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-if [ `uname` == Darwin ]; then
+if [ `uname` == Darwin ] 
+then
     export STACK_ROOT="${SRC_DIR}/s"
     stack setup --local-bin-path ${SRC_DIR}/s
     stack update
@@ -14,6 +15,7 @@ else
     stack setup
     stack update
     stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+fi
 #cleanup
 rm -r .stack-work
 
