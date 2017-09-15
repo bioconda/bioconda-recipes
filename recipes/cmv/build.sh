1b92038e060aad985977dc1dbeca1473b88df02e
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-09-15 12:16:23 +0200

    More fixes for mac os build

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,11 +3,17 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-#explicititly set dictory for GHC download
-export STACK_ROOT="${SRC_DIR}/s"
-stack setup --local-bin-path ${SRC_DIR}/s
-stack update
-stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+
+if [ `uname` == Darwin ]; then
+    export STACK_ROOT="${SRC_DIR}/s"
+    stack setup --local-bin-path ${SRC_DIR}/s
+    stack update
+    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+    rm -r "${SRC_DIR}/s"
+else
+    stack setup
+    stack update
+    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
-rm -r "${PREFIX}/stack"
+
