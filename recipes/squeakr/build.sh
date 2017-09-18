b7e08d88529b49e84008a44d755ea3caabd910f8
rob-p <none@none.com>
2017-09-17 22:24:20 -0400

    add squeakr

--- recipes/squeakr/build.sh
+++ recipes/squeakr/build.sh
@@ -2,7 +2,13 @@
 
 mkdir -p $PREFIX/bin
 
-make
+if [ $UNAME == "Darwin" ]
+then
+    export CFLAGS="-stdlib=libc++"
+    make
+else
+    make
+fi
 
 cp squeakr-count  $PREFIX/bin
 cp squeakr-inner-prod $PREFIX/bin
