e7fcaa27ebfac4d43cc84468ac048ba7de1a79ed
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-09-18 15:05:09 -0600

    Update BLAST recipe
     - build on OSX as well
     - reduce package size
       - build and use blast dynamic libraries
       - use dynamic external libs (zlib, bzip2, openssl, boost)

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -2,25 +2,39 @@
 
 set -e -x -o pipefail
 
-mkdir -p $PREFIX/bin
+cd $SRC_DIR/c++/
 
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Platform: Mac"
+# --with-hard-runpath is needed otherwise BLAST programs would search
+# libraries first in the directories defined by the LD_LIBRARY_PATH
+# environment variable, instead of using the rpath specified by conda
 
-    cp $SRC_DIR/bin/* $PREFIX/bin/
+./configure \
+    --with-dll \
+    --with-mt \
+    --without-autodep \
+    --without-makefile-auto-update \
+    --with-flat-makefile \
+    --without-caution \
+    --without-dbapi \
+    --without-lzo \
+    --with-hard-runpath \
+    --without-debug \
+    --with-strip \
+    --without-downloaded-vdb \
+    --with-z=$PREFIX \
+    --with-bz2=$PREFIX \
+    --with-openssl=$PREFIX \
+    --with-boost=$PREFIX \
+    --with-openssl=$PREFIX
 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Platform: Linux"
+projects="algo/blast/ app/ objmgr/ objtools/align_format/ objtools/blast/"
 
-    cd $SRC_DIR/c++/
-    # --with-hard-runpath is needed otherwise BLAST programs would search
-    # libraries first in the directories defined by the LD_LIBRARY_PATH
-    # environment variable, instead of using the rpath specified by conda
-    ./configure --prefix=$PREFIX --with-hard-runpath
+cd */build
 
-    make -j${CPU_COUNT}
+make -j${CPU_COUNT} -f Makefile.flat all_projects="$projects"
 
-    cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
-fi
+mkdir -p $PREFIX/{bin,lib,include}
+cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
+cp $SRC_DIR/c++/ReleaseMT/lib/* $PREFIX/lib/
 
 chmod +x $PREFIX/bin/*
