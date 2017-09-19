e1f8860bcb1fb8a29b8e76e9500b59bcf25db5cf
rob-p <none@none.com>
2017-09-18 21:26:57 -0400

    add squeakr

--- recipes/squeakr/build.sh
+++ recipes/squeakr/build.sh
@@ -4,7 +4,13 @@ set -x -e
 
 mkdir -p $PREFIX/bin
 
-CXXFLAGS="-stdlib=libc++ -I ${PREFIX}/include" make
+BUILD_OS=$(uname -s)
+
+if [ ${BUILD_OS} == "Darwin" ]
+   CXXFLAGS="-stdlib=libc++ -I ${PREFIX}/include" make
+   else
+   CXXFLAGS="-I ${PREFIX}/include" make
+fi
 
 cp squeakr-count  $PREFIX/bin
 cp squeakr-inner-prod $PREFIX/bin
