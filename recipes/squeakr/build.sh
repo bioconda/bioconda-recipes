79d540775382b51d63587ea43c16458faa8255a8
rob-p <none@none.com>
2017-09-18 22:03:28 -0400

    contributions

--- recipes/squeakr/build.sh
+++ recipes/squeakr/build.sh
@@ -7,7 +7,7 @@ mkdir -p $PREFIX/bin
 BUILD_OS=$(uname -s)
 
 if [ ${BUILD_OS} == "Darwin" ]; then
-   CXXFLAGS="-stdlib=libc++ -I ${PREFIX}/include" make
+   LDFLAGS="-stdlib=libc++" CXXFLAGS="-stdlib=libc++ -I ${PREFIX}/include" make
    else
    CXXFLAGS="-I ${PREFIX}/include" make
 fi
