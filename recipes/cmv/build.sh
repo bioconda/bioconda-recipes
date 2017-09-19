46aa8b2cb449f0e9ceaa0e7e8c53b53257461a65
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-09-19 12:29:11 +0200

    Removed prototype for macosx support from recipe

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -4,18 +4,9 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-if [ `uname` == Darwin ] 
-then
-    export STACK_ROOT="${SRC_DIR}/s"
-    stack setup --local-bin-path ${SRC_DIR}/s
-    stack update
-    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
-    rm -r "${SRC_DIR}/s"
-else
-    stack setup
-    stack update
-    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
-fi
+stack setup
+stack update
+stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
 
