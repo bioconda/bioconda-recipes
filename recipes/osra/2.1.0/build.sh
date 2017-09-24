50515ffa9b2b5672e64412a6cf681b31c9e3db9d
Hassan <hassan.amr89@gmail.com>
2017-09-24 13:59:40 +0200

    added recipes files

---
+++ recipes/osra/2.1.0/build.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+./configure --prefix=$PREFIX \
+    --with-gocr-include=$PREFIX/include/gocr/ \
+    --with-gocr-lib=$PREFIX/lib/ \
+    --with-openbabel-include=$PREFIX/include/openbabel-2.0/ \
+    --with-openbabel-lib=$PREFIX/lib \
+    --with-graphicsmagick-include=$PREFIX/include/ \
+    --with-graphicsmagick-lib=$PREFIX/lib/
+make
+make install
