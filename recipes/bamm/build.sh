5b3e2eddf5cd2d860ba5f58e4302030f298b1030
Mattias <m.dehollander@nioo.knaw.nl>
2017-09-25 17:58:10 +0200

    Disable osx; PRs are welcome

--- recipes/bamm/build.sh
+++ recipes/bamm/build.sh
@@ -3,9 +3,4 @@ export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-if [ "$(uname)" == "Darwin" ]; then
-    rvm get head
-fi
-
-
 $PYTHON setup.py install
