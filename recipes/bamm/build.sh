6c5f74890c74b7189330cacc601345feb0c0b23d
Mattias <m.dehollander@nioo.knaw.nl>
2017-09-25 14:59:42 +0200

    Use embedded htslib and libcfu

--- recipes/bamm/build.sh
+++ recipes/bamm/build.sh
@@ -3,6 +3,7 @@ export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-$PYTHON setup.py install --with-libhts-lib $PREFIX/lib \
- --with-libhts-inc $PREFIX/include --with-libcfu-inc $PREFIX/include \
- --with-libcfu-lib $PREFIX/lib
+$PYTHON setup.py install
+#--with-libhts-lib $PREFIX/lib \
+# --with-libhts-inc $PREFIX/include --with-libcfu-inc $PREFIX/include \
+# --with-libcfu-lib $PREFIX/lib
