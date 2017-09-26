d1ce99187f67e84e7a8bebcbd59c6de142445f41
Cyril MONJEAUD <cyril.monjeaud@gmail.com>
2017-09-26 14:44:29 +0000

    discosnp

---
+++ recipes/discosnp/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}/bin
+
+# decrease RAM needed
+sed -i 's/make -j8/make -j1/' INSTALL
+
+# remove tests because of too much RAM needed
+sed -i 's/. .\/simple_test\.sh/#. .\/simple_test\.sh/' INSTALL
+
+# installation
+sh INSTALL
+
+# copy binaries
+cp run_discoSnp++.sh ${PREFIX}/bin
+chmod +x ${PREFIX}/bin/run_discoSnp++.sh
+
+# copy directories
+cp -R tools ${PREFIX}
+cp -R build ${PREFIX}
+cp -R scripts ${PREFIX}
