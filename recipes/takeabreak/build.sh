1f5347e13efa413c7820113bcebcd90828d494e3
Cyril MONJEAUD <cyril.monjeaud@gmail.com>
2017-09-26 14:57:21 +0000

    fix sed compatibility

--- recipes/takeabreak/build.sh
+++ recipes/takeabreak/build.sh
@@ -3,8 +3,8 @@
 mkdir -p ${PREFIX}/bin
 
 # decrease RAM needed
-sed -i 's/make -j4/make -j1/' INSTALL
-sed -i 's/.\/build\/bin\/TakeABreak/.\/build\/bin\/TakeABreak -max-memory 1000/' INSTALL
+sed -i.bak 's/make -j4/make -j1/' INSTALL
+sed -i.bak 's/.\/build\/bin\/TakeABreak/.\/build\/bin\/TakeABreak -max-memory 1000/' INSTALL
 
 # installation
 sh INSTALL
