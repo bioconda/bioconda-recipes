ae7173371db49b5d3524cb43c30104fe4f70e6fc
Mattias <m.dehollander@nioo.knaw.nl>
2017-09-26 16:31:17 +0200

    Change cfu compilation; add nosetests; increase build number

--- recipes/bamm/build.sh
+++ recipes/bamm/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+#export CFLAGS="-I$PREFIX/include"
+#export CPPFLAGS="-I$PREFIX/include"
+#export LDFLAGS="-L$PREFIX/lib"
+
+(cd c/libcfu-0.03/ && automake --add-missing && make)
 
 $PYTHON setup.py install
