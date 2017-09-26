ceb4f9f3f59b465eb7a550790f4c80ccc3f3dcd4
Matt <gopezm@myumanitoba.ca>
2017-09-26 13:17:30 -0500

    Initial commit for feht's recipe.

---
+++ recipes/feht/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+stack setup
+stack update
+stack install --local-bin-path ${PREFIX}/bin
+mv ~/.local/bin/feht ${PREFIX}/bin/
+
+#Clean-up
+rm -r .stack-work
