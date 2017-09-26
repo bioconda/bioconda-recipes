560259807bc236a72f73e94bef46b8d4392253de
Cyril MONJEAUD <cyril.monjeaud@gmail.com>
2017-09-26 08:47:08 +0000

    test changes and add comments

--- recipes/lordec/build.sh
+++ recipes/lordec/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sed -i 's/GATB_VER=1.0.6/GATB_VER=1.3.0/' Makefile
-sed -i 's/GATB=gatb-core-$(GATB_VER)-Linux/GATB=$(PREFIX)/' Makefile
-
 mkdir -p ${PREFIX}/bin
 
-#mkdir -p build
-#cd build
+# create build directory
+mkdir -p build
+cd build
 
-#cmake ..
+# compilation
+cmake ..
 make
+
+# copy binaries
 cp tools/lordec-* ${PREFIX}/bin
