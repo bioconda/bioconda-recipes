77ba0e34d46fd15c2f7e962f6d000cb2a3c52607
Mattias <m.dehollander@nioo.knaw.nl>
2017-09-27 11:57:54 +0200

    Fix htslib include dir

--- recipes/bamm/build.sh
+++ recipes/bamm/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
 (rm -rf c/htslib-1.3.1/)
 (cd c/ && ./autogen.sh)
 
-$PYTHON setup.py install --with-libhts-lib ${PREFIX}/lib --with-libhts-inc ${PREFIX}/include
+$PYTHON setup.py install --with-libhts-lib ${PREFIX}/lib --with-libhts-inc ${PREFIX}/include/htslib
