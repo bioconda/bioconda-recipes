185280a210741cbc42ac614c770ee1d6266c7259
Matt <gopezm@myumanitoba.ca>
2017-09-27 09:19:01 -0500

    Adding dependencies to fix the compiling linker error.

--- recipes/feht/build.sh
+++ recipes/feht/build.sh
@@ -7,7 +7,7 @@ export CPPFLAGS="-I${PREFIX}/include"
 stack setup
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
-mv ~/.local/bin/feht ${PREFIX}/bin/
-
-#Clean-up
+mv ${PREFIX}/bin/feht ${PREFIX}/bin/feht-bin
+chmod 755 ${PREFIX}/bin/feht
+#cleanup
 rm -r .stack-work
