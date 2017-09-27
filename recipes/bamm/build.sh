6cb99cd9fc4c8fbc33fbd4a262e71c1b865abcc3
Mattias <m.dehollander@nioo.knaw.nl>
2017-09-27 11:24:05 +0200

    Use conda htslib

--- recipes/bamm/build.sh
+++ recipes/bamm/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#export CFLAGS="-I$PREFIX/include"
-#export CPPFLAGS="-I$PREFIX/include"
-#export LDFLAGS="-L$PREFIX/lib"
-
-#(cd c/libcfu-0.03/ && automake --add-missing && make)
+(rm -rf c/htslib-1.3.1/)
 (cd c/ && ./autogen.sh)
 
 $PYTHON setup.py install
