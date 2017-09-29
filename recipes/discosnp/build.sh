543f9c406bbb2d966865195d72bcae925782f528
MONJEAUD Cyril <Cyril.Monjeaud@irisa.fr>
2017-09-29 11:04:57 +0200

    Discosnp fix dependencies (#6182)
    * discosnp
    
    * fix sed compatibility
    
    * pin zlib against {{CONDA_ZLIB}}*
    
    * fix dependencies binaries not found during the execution

--- recipes/discosnp/build.sh
+++ recipes/discosnp/build.sh
@@ -11,11 +11,17 @@ sed -i.bak 's/. .\/simple_test\.sh/#. .\/simple_test\.sh/' INSTALL
 # installation
 sh INSTALL
 
+# change run_discoSnp++.sh deps path
+sed -i.bak 's/\$EDIR\/bin/\$EDIR/' run_discoSnp++.sh
+
 # copy binaries
 cp run_discoSnp++.sh ${PREFIX}/bin
 chmod +x ${PREFIX}/bin/run_discoSnp++.sh
 
-# copy directories
-cp -R tools ${PREFIX}
-cp -R build ${PREFIX}
+cp ext/gatb-core/bin/dbgh5 ${PREFIX}/bin
+cp build/bin/read_file_names ${PREFIX}/bin
+cp build/bin/kissnp2 ${PREFIX}/bin
+cp build/bin/kissreads2 ${PREFIX}/bin
+
+# copy scripts directory
 cp -R scripts ${PREFIX}
