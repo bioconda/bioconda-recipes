25dd79df1ec6761ffa091972c40669345628e936
MONJEAUD Cyril <Cyril.Monjeaud@irisa.fr>
2017-09-29 15:16:33 +0200

    Discosnp fix scripts path in .sh (#6185)
    * discosnp
    
    * fix sed compatibility
    
    * pin zlib against {{CONDA_ZLIB}}*
    
    * fix dependencies binaries not found during the execution
    
    * fix scripts path in .sh
    
    * fix dbh5 path for copy

--- recipes/discosnp/build.sh
+++ recipes/discosnp/build.sh
@@ -13,12 +13,13 @@ sh INSTALL
 
 # change run_discoSnp++.sh deps path
 sed -i.bak 's/\$EDIR\/bin/\$EDIR/' run_discoSnp++.sh
+sed -i.bak 's/scripts\//..\/scripts\//' run_discoSnp++.sh
 
 # copy binaries
 cp run_discoSnp++.sh ${PREFIX}/bin
 chmod +x ${PREFIX}/bin/run_discoSnp++.sh
 
-cp ext/gatb-core/bin/dbgh5 ${PREFIX}/bin
+cp build/ext/gatb-core/bin/dbgh5 ${PREFIX}/bin
 cp build/bin/read_file_names ${PREFIX}/bin
 cp build/bin/kissnp2 ${PREFIX}/bin
 cp build/bin/kissreads2 ${PREFIX}/bin
