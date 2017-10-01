3c5ba1e1b48dc1b513894f16379b41ce25599ff7
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-10-01 17:40:09 +0200

    trying environmental variables in build.sh

--- recipes/mappy/build.sh
+++ recipes/mappy/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
 # Add more build steps here, if they are necessary.
