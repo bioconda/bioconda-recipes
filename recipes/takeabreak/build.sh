87d1261305667b3543c86eb160e21ac4e27017ee
Cyril MONJEAUD <cyril.monjeaud@gmail.com>
2017-10-02 07:06:38 +0000

    delete decrease of memory

--- recipes/takeabreak/build.sh
+++ recipes/takeabreak/build.sh
@@ -2,12 +2,8 @@
 
 mkdir -p ${PREFIX}/bin
 
-# decrease RAM needed
-sed -i.bak 's/make -j4/make -j1/' INSTALL
-sed -i.bak 's/.\/build\/bin\/TakeABreak/.\/build\/bin\/TakeABreak -max-memory 1000/' INSTALL
-
 # installation
 sh INSTALL
 
-# copy binaries
+# copy binary
 cp build/bin/TakeABreak ${PREFIX}/bin
