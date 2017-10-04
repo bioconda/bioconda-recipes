1bb5b441999c3e9d12e64de3e2ddaf33dd134b94
Olivier Sallou <olivier.sallou@irisa.fr>
2017-10-04 13:18:54 +0200

    add LDFLAG to PREFIX/lib

--- recipes/cassiopee/build.sh
+++ recipes/cassiopee/build.sh
@@ -3,9 +3,9 @@
 export C_INCLUDE_PATH=${PREFIX}/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=${PREFIX}/include:$CPLUS_INCLUDE_PATH
 export LIBRARY_PATH=${PREFIX}/lib:$LIBRARY_PATH
+export LDFLAGS="-L$PREFIX/lib"
 
 # DEBUG purpose to find boost related libs on travis
-ldd ${PREFIX}/lib/libboost_regex.so
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX .
 make
 make install
