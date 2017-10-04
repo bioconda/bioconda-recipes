63ab1becfc6c3fc9fb5a48cc5bf940e6669aa989
Jelle Scholtalbers <j.scholtalbers@gmail.com>
2017-10-04 20:03:54 +0200

    samtools: pin requirements and expand CFLAGS

--- recipes/samtools/0.1.14/build.sh
+++ recipes/samtools/0.1.14/build.sh
@@ -2,7 +2,7 @@
 sed -i.bak 's/-lcurses/-lncurses/' Makefile
 sed -i.bak 's/-lz/-lz -ltinfo/' Makefile
 sed -i.bak 's/\(inline void __ks_insertsort\)/static \1/'  ksort.h
-export CFLAGS="-I$PREFIX/include"
+export CFLAGS="$CFLAGS -I$PREFIX/include"
 make
 mkdir -p $PREFIX/bin
 mv samtools $PREFIX/bin
