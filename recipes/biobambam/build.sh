7d6cb4dd226b583b849d3225ae358099de460ce9
Christian Brueffer <christian@brueffer.io>
2017-10-05 15:47:05 +0200

    Update biobambam to 2.0.78.

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu
 
+cd 2.0.78-release-20170927110023/x86_64-etch-linux-gnu
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 cp -r bin $outdir
-cp -r etc $outdir
 cp -r etc $outdir
 cp -r lib $outdir
 cp -r share $outdir
