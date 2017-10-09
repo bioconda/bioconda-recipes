070f0c0d64feb1a1f3113528ffe6af2efeb71471
Ryan Dale <dalerr@niddk.nih.gov>
2017-10-09 10:05:19 -0400

    comment out custom build.sh lines

--- recipes/bioconductor-flowcore/build.sh
+++ recipes/bioconductor-flowcore/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ `uname` == Darwin ]; then
-        export MACOSX_DEPLOYMENT_TARGET=10.9
-fi
+#if [ `uname` == Darwin ]; then
+#        export MACOSX_DEPLOYMENT_TARGET=10.9
+#fi
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 #
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
+#export CXXFLAGS="${CXXFLAGS} -std=c++11"
 $R CMD INSTALL --build .
