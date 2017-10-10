167b9136f6e81830585bd8836cfe976890f021d5
Matt <gopezm@myumanitoba.ca>
2017-10-10 13:19:36 -0500

    Changing requirements

--- recipes/feht/build.sh
+++ recipes/feht/build.sh
@@ -3,7 +3,6 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 
-sudo apt-get install "build-essential"
 stack setup
 stack update
 stack install  --local-bin-path ${PREFIX}/bin
