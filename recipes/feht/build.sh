d56b0e700dfc1a4a243a5174c7cee44a9f2f073f
Matt <gopezm@myumanitoba.ca>
2017-10-10 11:16:53 -0500

    Fixing dependencies

--- recipes/feht/build.sh
+++ recipes/feht/build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-export LIBRARY_PATH="${PREFIX}/usr/lib/"
-export LD_LIBRARY_PATH="${PREFIX}/usr/lib/"
-export LDFLAGS="-L${PREFIX}/usr/lib/"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
 
 stack setup
 stack update
-ldconfig
-stack install --local-bin-path ${PREFIX}/bin
-#Renaming to bin.
-mv ${PREFIX}/bin/feht ${PREFIX}/bin/feht-bin
-#Let everyone read and execute the file.
-chmod 755 ${PREFIX}/bin/feht-bin
+stack install  --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
