7a03958b73022f260ef436d2e3e0ef3e003fd940
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-10-11 16:48:34 +0200

    Minor fix

--- recipes/coprarna/build.sh
+++ recipes/coprarna/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp CopraRNA2.pl  ${PREFIX}/bin
 chmod 755 ${PREFIX}/bin/CopraRNA2.pl
-cp -r coprarna_aux ${PREFIX}/
+cp -r coprarna_aux ${PREFIX}/bin/
 chmod 755 ${PREFIX}/bin/coprarna_aux/*.pl
 chmod 755 ${PREFIX}/bin/coprarna_aux/*.R
 chmod 755 ${PREFIX}/bin/coprarna_aux/*.pl
