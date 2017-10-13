fd83763c3990ca0ba03d64d6431d6bd01dea4f8c
tstrnity <tstrnity@gmail.com>
2017-10-13 09:51:22 -0400

    slncky, adjusting according to reviewer comments.

--- recipes/slncky/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-
-echo "PATH is $PATH"
-# The annotations directory needs to be uploaded by the user.
-# It is too big to be uploaded automatically.
-# test -d $PREFIX/annotations
-slncky --help > /dev/null 2>&1
