f866b5e0a9c35626bc3b7e78707d801639fbc64e
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-10-16 16:48:31 +0200

    Removed phantomjs chmod

--- recipes/coprarna/build.sh
+++ recipes/coprarna/build.sh
@@ -5,5 +5,4 @@ cp -r coprarna_aux ${PREFIX}/bin/
 chmod 755 ${PREFIX}/bin/coprarna_aux/*.pl
 chmod 755 ${PREFIX}/bin/coprarna_aux/*.R
 chmod 755 ${PREFIX}/bin/coprarna_aux/*.pl
-chmod 755 ${PREFIX}/bin/coprarna_aux/phantomjs
 
