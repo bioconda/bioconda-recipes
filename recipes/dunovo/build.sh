c89f6cc1c5e455734997aa20a1c10c43265146f4
Nick <nmapsy@gmail.com>
2017-10-17 15:09:25 -0400

    Du Novo: Update to 2.0.0-rc.3. - Add "kalign" submodule, take care of its .so files. - Use {{ version }} variable. - Specify a version of networkx (1.11). - Use --version printing for script tests. - Update license to GPLv2.

--- recipes/dunovo/build.sh
+++ recipes/dunovo/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Compile binaries and move them to lib.
-make
-mv *.so $PREFIX/lib
-
 # Download submodules and move them to lib.
+## kalign
+kalignver=0.1.1
+wget --no-check-certificate "https://github.com/makrutenko/kalign-dunovo/archive/v$kalignver.tar.gz"
+hash=`sha256sum v$kalignver.tar.gz | tr -s ' ' | cut -d ' ' -f 1`
+[ $hash = c27f7c9ae12751d41f2408cdb038d833fc0b874e0db842ea30f55310bab8c8ba ]
+tar -zxvpf v$kalignver.tar.gz
+rm -rf kalign
+mv kalign-dunovo-$kalignver kalign
+rm v$kalignver.tar.gz
 ## utillib
 utilver=0.1.0
 wget --no-check-certificate "https://github.com/NickSto/utillib/archive/v$utilver.tar.gz"
@@ -14,13 +19,18 @@ tar -zxvpf v$utilver.tar.gz
 mv utillib-$utilver $PREFIX/lib/utillib
 rm v$utilver.tar.gz
 ## ET
-ETver=0.1.1
+ETver=0.2.1
 wget --no-check-certificate "https://github.com/NickSto/ET/archive/v$ETver.tar.gz"
 hash=`sha256sum v$ETver.tar.gz | tr -s ' ' | cut -d ' ' -f 1`
-[ $hash = 552c371f0fe0000b6037038ad1aeae09111d066c362d45655e40381cb0c325e4 ]
+[ $hash = 9c512d755df984c6c662c06515fffe0eab97366fdd111a68112df3aa61f7beaf ]
 tar -zxvpf v$ETver.tar.gz
 mv ET-$ETver $PREFIX/lib/ET
 rm v$ETver.tar.gz
+
+# Compile binaries and move them to lib.
+make
+mv *.so $PREFIX/lib
+mv kalign $PREFIX/lib
 
 # Move scripts to lib and link to them from bin.
 for script in *.awk *.sh *.py; do
