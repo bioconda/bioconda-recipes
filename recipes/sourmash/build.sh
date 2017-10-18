64ef3a9e446e21e3b0ae3e798385caf90352c813
Luiz Irber <luiz.irber@gmail.com>
2017-10-18 16:43:43 -0700

    fixing the setup_py_install_args error

--- recipes/sourmash/build.sh
+++ recipes/sourmash/build.sh
@@ -3,4 +3,4 @@ if [ `uname` == Darwin ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
-$PYTHON setup.py install 
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
