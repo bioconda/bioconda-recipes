58282440775737f1285dd6809a159b9023bd801a
eseiler <enrico.seiler@hotmail.de>
2017-10-18 10:15:31 +0200

    build from source

--- recipes/clustalo/build.sh
+++ recipes/clustalo/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+./configure --prefix=$SRC_DIR
+make
+make install
+
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/clustal* $PREFIX/bin/$PKG_NAME
+cp $SRC_DIR/bin/clustal* $PREFIX/bin/$PKG_NAME
 chmod +x $PREFIX/bin/$PKG_NAME
