50b10a999c2862212cc3d4f45f70c5f92cd31b12
caleb <caleb.easterly@gmail.com>
2017-10-19 16:26:24 -0500

    add moff config file to /bin. This is weird, but must be in same dir as moff.py and other commands

--- recipes/moff/build.sh
+++ recipes/moff/build.sh
@@ -7,3 +7,4 @@ cp -r * $outdir
 ln -s $outdir/moff_mbr.py  $PREFIX/bin/moff_mbr.py
 ln -s $outdir/moff.py $PREFIX/bin/moff.py
 ln -s $outdir/moff_all.py $PREFIX/bin/moff_all.py
+ln -s $outdir/moff_setting.properties $PREFIX/bin/moff_setting.properties
