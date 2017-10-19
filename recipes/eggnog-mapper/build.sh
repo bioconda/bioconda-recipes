46eea033caf2a6bbace69e3e681da8b55ce00ab1
caleb <caleb.easterly@gmail.com>
2017-10-19 10:59:55 -0500

    try setup.py

--- recipes/eggnog-mapper/build.sh
+++ recipes/eggnog-mapper/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
-
-cp -r * $outdir
-ln -s $outdir/emapper.py $PREFIX/bin/emapper.py
-ln -s $outdir/download_eggnog_data.py $PREFIX/bin/download_eggnog_data.py
+#outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+#mkdir -p $outdir
+#
+#cp -r * $outdir
+#ln -s $outdir/emapper.py $PREFIX/bin/emapper.py
+#ln -s $outdir/download_eggnog_data.py $PREFIX/bin/download_eggnog_data.py
+$PYTHON setup.py install
