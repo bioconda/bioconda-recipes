1d78a7f60446acf92697bb5deff9ede2f981e0a9
rvalieris <rvalieris@users.noreply.github.com>
2017-10-19 22:17:41 -0200

    try to fix shortread

--- recipes/bioconductor-shortread/build.sh
+++ recipes/bioconductor-shortread/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export CFLAGS="-I$PREFIX/include"
-# export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 $R CMD INSTALL --build .
