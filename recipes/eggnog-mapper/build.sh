bb26b6aa64033c4935ef76d77546067d481ea2bd
Björn Grüning <bjoern@gruenings.eu>
2017-10-21 11:41:31 +0200

    Update build.sh

--- recipes/eggnog-mapper/build.sh
+++ recipes/eggnog-mapper/build.sh
@@ -1,8 +1,3 @@
 ##!/bin/bash
-#outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-#mkdir -p $outdir
-#cp -r * $outdir
-#ln -s $outdir/emapper.py $PREFIX/bin/emapper.py
-#ln -s $outdir/download_eggnog_data.py $PREFIX/bin/download_eggnog_data.py
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
