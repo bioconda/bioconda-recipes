9cb750b181c2bd5f1fc9f64ea3b1cdec55c75521
Marcel Bargull <marcel.bargull@udo.edu>
2017-10-23 07:06:24 +0200

    concoct: remove superfluous comments

--- recipes/concoct/build.sh
+++ recipes/concoct/build.sh
@@ -4,9 +4,3 @@ export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 $PYTHON setup.py install 
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
