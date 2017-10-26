834374571eb0bd0809923fc303c2004a670f76cc
gurgese <gianvito.urgese@polito.it>
2017-10-26 15:29:25 +0200

    Simplifyed some paths

--- recipes/seqan_tcoffee/build.sh
+++ recipes/seqan_tcoffee/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-set -eu -o pipefail
-
-BINARY_HOME=$PREFIX/bin
-PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 
 cd $SRC_DIR/bin
 
@@ -10,6 +6,5 @@ binaries="\
 seqan_tcoffee \
 "
 mkdir -p $PREFIX/bin
-mkdir -p $PKG_HOME
 
-for i in $binaries; do cp $SRC_DIR/bin/$i $PKG_HOME/$i && chmod a+x $PKG_HOME/$i && ln -s $PKG_HOME/$i $BINARY_HOME/$i; done
+for i in $binaries; do cp $SRC_DIR/bin/$i $PREFIX/bin/$i && chmod a+x $PREFIX/bin/$i; done
