ef3c5eed33359f20190933a823f5b137243eb7c9
tstrnity <tstrnity@gmail.com>
2017-10-26 13:27:57 -0400

    DISCASM: Files now copied to share instead of bin. A script is created in bin that invokes DISCASM from its location in share.

--- recipes/discasm/build.sh
+++ recipes/discasm/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 DISCASM_INSTALL_DIR="$PREFIX/share/DISCASM-0.1.2-0"
 mkdir -p $DISCASM_INSTALL_DIR
-echo "DISCASM_INSTALL_DIR is $DISCASM_INSTALL_DIR"
-echo "about to copy files"
 cp -R $SRC_DIR/DISCASM $SRC_DIR/PerlLib $SRC_DIR/SciEDPipeR $SRC_DIR/testing $SRC_DIR/util $DISCASM_INSTALL_DIR
 chmod a+x $DISCASM_INSTALL_DIR/DISCASM
-ls -la $DISCASM_INSTALL_DIR
 
 # The following shell script is built to invoke DISCASM using its full
 # install path rather than creating a symlink to DISCASM from $PREFIX/bin
