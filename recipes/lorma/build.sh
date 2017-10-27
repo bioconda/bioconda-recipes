5cb67d899651d03328de5db48c924dcb82ae373d
cmonjeau <cyril.monjeaud@gmail.com>
2017-10-27 12:42:42 +0000

    fix LoRMA dependency

--- recipes/lorma/build.sh
+++ recipes/lorma/build.sh
@@ -2,9 +2,8 @@
 
 mkdir -p ${PREFIX}/bin
 
-# change paths for binaries
+# change path for Lordec binaries
 sed -i.bak 's/\$DIR\/LoRDEC/\$DIR/' lorma.sh
-sed -i.bak 's/\$DIR\/LoRMA/\$DIR/' lorma.sh
 
 # copy shell script
 cp lorma.sh ${PREFIX}/bin
