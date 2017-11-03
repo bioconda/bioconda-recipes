0d3c576ee756ca46198b8c09093397c0148e88ba
Martin Mann <mmann@informatik.uni-freiburg.de>
2017-11-03 16:59:26 +0100

    explicit shell and bin path created

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # compile jalview from source
 cd jalview
@@ -5,7 +6,7 @@ ant makedist
 
 # create target directory
 JALVIEWDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir $JALVIEWDIR
+mkdir -p $JALVIEWDIR
 
 # copy files to target
 cp -R dist/* $JALVIEWDIR/.
@@ -15,8 +16,10 @@ echo -e '#!/usr/bin/env bash\n\
 JALVIEWDIR=$JALVIEWDIR;\n\
 java -Djava.awt.headless=true -Djava.ext.dirs=\$JALVIEWDIR -cp \$JALVIEWDIR/jalview.jar jalview.bin.Jalview \${@};\n\
 ' | tr -d '\\' > $JALVIEWDIR/jalview.sh; 
-chmod u+x $JALVIEWDIR/jalview.sh; 
+chmod 0755 $JALVIEWDIR/jalview.sh; 
 
 # link wrapper script
+mkdir -p $PREFIX/bin
 ln -s $JALVIEWDIR/jalview.sh $PREFIX/bin/jalview
+chmod 0755 $PREFIX/bin/jalview; 
 
