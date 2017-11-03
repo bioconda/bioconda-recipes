3b7e8b4d8d6aaa856203cae4b5390956eb7c271e
Cashalow <sachalau@gmail.com>
2017-11-03 17:01:24 +0100

    do not build perl

--- recipes/vcftools/build.sh
+++ recipes/vcftools/build.sh
@@ -4,5 +4,6 @@ export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 ./autogen.sh
 ./configure --prefix=$PREFIX
+sed -i.bak -e 's/SUBDIRS = cpp perl/SUBDIRS = cpp/' ./src/Makefile
 make
 make install
