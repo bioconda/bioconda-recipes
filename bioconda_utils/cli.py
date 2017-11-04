046e91538af0780e40d3afe60a379d200b0fb5a9
Ryan Dale <dalerr@niddk.nih.gov>
2017-11-04 16:36:23 -0400

    warn if both blacklisted and changed

--- bioconda_utils/cli.py
+++ bioconda_utils/cli.py
@@ -70,6 +70,9 @@ def select_recipes(packages, git_range, recipe_folder, config_filename, config, 
     selected_recipes = list(utils.get_recipes(recipe_folder, packages))
     _recipes = []
     for recipe in selected_recipes:
+        if recipes in blacklisted_recipes and recipes in changed_recipes:
+            logger.warning('%s is blackliested but also has changed. Consider '
+                           'removing from blacklist if you want to build it', recipe)
         if force:
             _recipes.append(recipe)
             logger.debug('forced: %s', recipe)
