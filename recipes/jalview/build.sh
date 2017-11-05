634a27bccfccb0ed53c7e5bf92325ab98844fffb
Martin Raden <mmann@informatik.uni-freiburg.de>
2017-11-05 22:37:04 +0100

    extended way to find script directory
    as done in https://github.com/bioconda/bioconda-recipes/blob/master/recipes/snpeff/snpeff.sh

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -13,8 +13,16 @@ cp -R dist/* $JALVIEWDIR/.
 
 # create wrapper script
 echo -e '#!/usr/bin/env bash\n\
-JALVIEWDIR=`dirname "\$(readlink -f "´\$0")"`; # get path of this script file\n\
-JALVIEWDIR=`readlink -f \$JALVIEWDIR`; # back to full abs path\n\
+# Find original directory of bash script, resolving symlinks\
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128\
+SOURCE="\${BASH_SOURCE[0]}"\
+while [ -h "\$SOURCE" ]; do # resolve \$SOURCE until the file is no longer a symlink\
+    DIR="\$( cd -P "\$( dirname "\$SOURCE" )" && pwd )"\
+    SOURCE="\$(readlink "\$SOURCE")"\
+    [[ \$SOURCE != /* ]] && SOURCE="\$DIR/\$SOURCE" # if \$SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located\
+done\
+DIR="\$( cd -P "\$( dirname "\$SOURCE" )" && pwd )"; # get final path of this script\
+JALVIEWDIR=\$DIR`; # set install path of jalview\n\
 echo "JALVIEWDIR=\$JALVIEWDIR"; # debug\n\
 ls \$JALVIEWDIR; # debug\n\
 which jalview; # debug\n\
