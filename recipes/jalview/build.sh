3a4f139808282ab9e8bcdb5d7248e46f2494f2b2
Martin Raden <mmann@informatik.uni-freiburg.de>
2017-11-05 21:24:11 +0100

    full abs path of script directory

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -13,10 +13,14 @@ cp -R dist/* $JALVIEWDIR/.
 
 # create wrapper script
 echo -e '#!/usr/bin/env bash\n\
-JALVIEWDIR=`dirname "\$(readlink -f "´\$0")"`;\n\
-echo \$JALVIEWDIR;\n\
-echo `pwd`;\n\
-java -Djava.awt.headless=true -Djava.ext.dirs=\$JALVIEWDIR -cp \$JALVIEWDIR/jalview.jar jalview.bin.Jalview \${@};\n\
+JALVIEWDIR=`dirname "\$(readlink -f "´\$0")"`; # get path of this script file\n\
+JALVIEWDIR=`readlink -f \$JALVIEWDIR; # back to full abs path\n\
+echo "JALVIEWDIR=\$JALVIEWDIR"; # debug\n\
+ls \$JALVIEWDIR; # debug\n\
+which jalview; # debug\n\
+echo `pwd`; # debug\n\
+echo "java -Djava.awt.headless=true -Djava.ext.dirs=\$JALVIEWDIR -cp \$JALVIEWDIR/jalview.jar jalview.bin.Jalview \${@};"; # debug\n\
+java -Djava.awt.headless=true -Djava.ext.dirs=\$JALVIEWDIR -cp \$JALVIEWDIR/jalview.jar jalview.bin.Jalview \${@}; # forward call\n\
 ' | tr -d '\\' > $JALVIEWDIR/jalview.sh; 
 chmod +x $JALVIEWDIR/jalview.sh; 
 
