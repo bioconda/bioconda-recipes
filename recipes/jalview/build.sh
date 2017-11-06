f9b874a1cb3a6cfc20d6450ae0b919e1db66bc78
Martin Raden <mmann@informatik.uni-freiburg.de>
2017-11-06 09:55:52 +0100

    copy existing wrapper script

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -7,23 +7,10 @@ ant makedist
 JALVIEWDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $JALVIEWDIR
 
-# copy files to target
+# copy jalview files to target
 cp -R dist/* $JALVIEWDIR/.
-
-# create wrapper script
-echo -e '#!/usr/bin/env bash\n\
-# Find original directory of bash script, resolving symlinks\
-# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128\
-SOURCE="\${BASH_SOURCE[0]}"\
-while [ -h "\$SOURCE" ]; do # resolve \$SOURCE until the file is no longer a symlink\
-    DIR="\$( cd -P "\$( dirname "\$SOURCE" )" && pwd )"\
-    SOURCE="\$(readlink "\$SOURCE")"\
-    [[ \$SOURCE != /* ]] && SOURCE="\$DIR/\$SOURCE" # if \$SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located\
-done\
-DIR="\$( cd -P "\$( dirname "\$SOURCE" )" && pwd )"; # get final path of this script\
-JALVIEWDIR=\$DIR; # set install path of jalview\n\
-java -Djava.ext.dirs=\$JALVIEWDIR -cp \$JALVIEWDIR/jalview.jar jalview.bin.Jalview \${@}; # forward call\n\
-' | tr -d '\\' > $JALVIEWDIR/jalview.sh; 
+# copy wrapper and make executable
+cp $RECIPE_DIR/jalview.sh $JALVIEWDIR/.
 chmod +x $JALVIEWDIR/jalview.sh; 
 
 # link wrapper script
