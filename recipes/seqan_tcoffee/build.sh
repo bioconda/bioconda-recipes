a327c9217e466fa959ccd803d7d2de7b2dbe8de3
gurgese <gianvito.urgese@polito.it>
2017-11-06 12:17:29 +0100

    Used the -DCMAKE_INSTALL_PREFIX flag

--- recipes/seqan_tcoffee/build.sh
+++ recipes/seqan_tcoffee/build.sh
@@ -3,6 +3,6 @@
 mkdir -p $PREFIX/bin
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX/bin -DCMAKE_BUILD_TYPE=Release
 make seqan_tcoffee
 cp bin/seqan_tcoffee $PREFIX/bin
