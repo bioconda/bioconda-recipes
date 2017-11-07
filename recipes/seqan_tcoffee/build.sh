a905a1abcdd609a0a0c5d342bcb2d89c084b2aa8
Björn Grüning <bjoern@gruenings.eu>
2017-11-07 09:47:21 +0100

    it does not work

--- recipes/seqan_tcoffee/build.sh
+++ recipes/seqan_tcoffee/build.sh
@@ -3,8 +3,7 @@
 mkdir -p $PREFIX/bin
 mkdir -p build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release
 
 make seqan_tcoffee
-make install
-#cp bin/seqan_tcoffee $PREFIX/bin
+cp bin/seqan_tcoffee $PREFIX/bin
