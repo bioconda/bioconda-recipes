cacfd22c89433aa8188136649889fbf1d0179ae1
Ryan Dale <dalerr@niddk.nih.gov>
2017-11-09 16:53:36 -0500

    update go.db

--- recipes/bioconductor-go.db/post-link.sh
+++ recipes/bioconductor-go.db/post-link.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-FN="GO.db_3.4.2.tar.gz"
+FN="GO.db_3.5.0.tar.gz"
 URLS=(
-  "http://bioconductor.org/packages/3.6/data/annotation/src/contrib/GO.db_3.4.2.tar.gz"
-  "https://depot.galaxyproject.org/software/GO.db/GO.db_3.4.2_src_all.tar.gz"
+  "http://bioconductor.org/packages/3.6/data/annotation/src/contrib/GO.db_3.5.0.tar.gz"
+  "https://depot.galaxyproject.org/software/GO.db/GO.db_3.5.0_src_all.tar.gz"
 )
-    MD5="174bc4c6200d5a0361dd2c76237f86c6"
+    MD5="58b59a93504a33b29652d25b00f212ea"
 
 # Use a staging area in the conda dir rather than temp dirs, both to avoid
 # permission issues as well as to have things downloaded in a predictable
