790a7d2ebf4b9b4640a11bb984a6541ca457b760
Gianvito Urgese <gianvito.urgese@polito.it>
2017-11-09 10:06:42 +0100

    Seqan tools (#6535)
    * Updated yara version
    
    * Included new recipe seqan_tcoffee
    
    * Simplifyed some paths
    
    * razers3 yaml updated
    
    * fixed bzip2
    
    * src version
    
    * New recipe: micro_razers
    
    * start build number with 0
    
    * start build number with 0
    
    * Used the -DCMAKE_INSTALL_PREFIX flag
    
    * Used the -DCMAKE_INSTALL_PREFIX flag
    
    * does this work?
    
    * it does not work

--- recipes/micro_razers/build.sh
+++ recipes/micro_razers/build.sh
@@ -4,6 +4,5 @@ mkdir -p $PREFIX/bin
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-
-make seqan_tcoffee
-cp bin/seqan_tcoffee $PREFIX/bin
+make micro_razers
+cp bin/micro_razers $PREFIX/bin
