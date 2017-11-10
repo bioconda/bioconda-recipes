f5ed25429bb4862fe19a872f63c8c6bbc1244865
James Abbott <jamesabbott@users.noreply.github.com>
2017-11-10 22:21:08 +0000

    Pinned boost to 1.61 to prevent rebuilds against incompatible versions (#6589)
    * Added abyss version dependancy of 1.5.2
    
    * Added recipie for abyss 1.5.2
    
    * Tidied up yaml to make valid
    
    * Added md5 hash since recipe fails linting...
    
    * Set CPPFLAGS to try to find sparsehash since it isn't located during build
    
    * Add OSX specific linker options to stop it using the wrong standard c++ library...
    
    * Rearrange osx args to try to prevent cflags being passed to configure...
    
    * Fixed OS-specific CPPFLAGS setting
    
    * Try not passing configure arguments via CPPFLAGS...might help...
    
    * Add CXXFLAGS so linking uses libstd++
    
    * Add $PREFIX to include path on OSX since it can't find some headers...
    
    * Try specifically adding $PREFIX/include/boost for OSX since it can't find the headers
    
    * Try C_INCLUDE_PATH for OSX since it still can't find boost header
    
    * Simplify OSX args...
    
    * Try again...
    
    * Try adding specifing -I for boost headers...
    
    * Add CXXFLAGS for OSX since --stdlib=libstdc++ not being passed during linking
    
    * Fixed build number...
    
    * Pinned to boost 1.61 to avoid rebuilds with incompatible boost versions
    
    * force rebuild
    
    * force rebuild
    
    * Pinned boost to 1.61 to prevent rebuilds against incompatible versions
    
    * remove abyss from blacklist
    
    * Corrected boost version number
    
    * Pinned boost version to 1.61.0
    
    * Added post-link script to all versions to point shebang line to conda version of make
    Add make as a runtime requirement where missing
    Add abyss-pe test to recipies where missing
    Bump version of latest release to 2.0.2, which resolves compile issues with boost > 1.61
    
    * Move sed command to build.sh and remove post-link.sh
    Make sed syntax OSX compatible...
    
    * Update build.sh to try to get OSX builds to work...

--- recipes/abyss/2.0.2-k128/build.sh
+++ recipes/abyss/2.0.2-k128/build.sh
@@ -8,12 +8,11 @@ if [[ $(uname) == "Darwin" ]]; then
 	echo "Configuring for OSX..."
 	export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include -I$PREFIX/include/boost --stdlib=libstdc++"
 	export CXXFLAGS="${CXXFLAGS} --stdlib=libstdc++"
-	else
+else
 	echo "Configuring for Linux..."
 	export CPPFLAGS="-I$PREFIX/include"
 fi
 
-./configure --prefix=$PREFIX --with-boost=$PREFIX/include --with-mpi=$PREFIX/include --enable-maxk=96
+./configure --prefix=$PREFIX --with-boost=$PREFIX/include --with-mpi=$PREFIX/include --enable-maxk=128
 make AM_CXXFLAGS=-Wall
-make install
-
+make install 
