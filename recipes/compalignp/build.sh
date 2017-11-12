336372999f4435b7b272c43c9c3863bcb7dfc47c
Björn Grüning <bjoern@gruenings.eu>
2017-11-12 10:43:15 +0100

    Update build.sh

--- recipes/compalignp/build.sh
+++ recipes/compalignp/build.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+export CFLAGS="-I$PREFIX/include -L$PREFIX/lib"
+
+
 wget http://www.biophys.uni-duesseldorf.de/bralibase/squid-1.9g.tar.gz
 tar -xzf squid-1.9g.tar.gz
-cd squid-1.9g
+
+pushd squid-1.9g
 
 ./configure --prefix=$PREFIX -q
 make clean
 make
 make install
 
-cd ..
-
-export CFLAGS="-I$PREFIX/include -L$PREFIX/lib"
+popd
 
 make
 cp compalignp $PREFIX/bin
