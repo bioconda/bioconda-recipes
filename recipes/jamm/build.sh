3ffe972480b520f23a35664745b3d4ce0777703d
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2017-11-13 16:15:05 +0100

    cleanup; trigger rebuild

--- recipes/jamm/build.sh
+++ recipes/jamm/build.sh
@@ -9,5 +9,5 @@ mkdir testdata
 curl bioinf.uni-freiburg.de/~maticzkd/jamm_testdata/ctcfRep1.chr21.bed > testdata/ctcfRep1.chr21.bed
 curl bioinf.uni-freiburg.de/~maticzkd/jamm_testdata/ctcfRep2.chr21.bed > testdata/ctcfRep2.chr21.bed
 curl bioinf.uni-freiburg.de/~maticzkd/jamm_testdata/chrSizes21.csize > chrSizes21.csize
-time bash ./JAMM.sh -s testdata -g chrSizes21.csize -o jamm_out
+./JAMM.sh -s testdata -g chrSizes21.csize -o jamm_out
 ls jamm_out/peaks/all.peaks.narrowPeak # non-zero return if file not found
