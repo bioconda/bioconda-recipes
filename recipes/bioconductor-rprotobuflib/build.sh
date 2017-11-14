b45dbc7c3a4bebadc2dfc835740de9bd10f6758b
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-11-14 10:35:34 +0100

    fix autoconf

--- recipes/bioconductor-rprotobuflib/build.sh
+++ recipes/bioconductor-rprotobuflib/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# fix automake
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
+
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 $R CMD INSTALL --build .
