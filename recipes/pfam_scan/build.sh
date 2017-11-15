0b1144ae0b61ffca87904181453fb601f436b92d
peterjc <p.j.a.cock@googlemail.com>
2017-11-15 11:06:59 +0000

    Fix EOF markers; ensure /bin exists

--- recipes/pfam_scan/build.sh
+++ recipes/pfam_scan/build.sh
@@ -2,12 +2,12 @@
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
+mkdir -p $PREFIX/bin
 mkdir -p $outdir
 cp -r * $outdir
 
 # Short wrapper script which sets PERL5LIB while running
 cat > $PREFIX/bin/pfam_scan.pl <<EOF
-EOF
 #!/bin/bash
 export PERL5LIB=$outdir:$outdir/
 $outdir/pfam_scan.pl \$@
