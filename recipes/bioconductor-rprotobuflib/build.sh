561d193967dccb9dbdd1e8675351051f3e3f26ce
Ryan Dale <dalerr@niddk.nih.gov>
2017-11-15 11:45:14 -0500

    fix rprotobuflib

--- recipes/bioconductor-rprotobuflib/build.sh
+++ recipes/bioconductor-rprotobuflib/build.sh
@@ -4,6 +4,8 @@
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal-1.14
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake-1.14
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
 
 # fix autoconf
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
