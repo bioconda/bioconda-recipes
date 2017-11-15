86abf92d66b74022a36a9218c2a52ca74a6c89dc
cmonjeau <cyril.monjeaud@gmail.com>
2017-11-15 14:53:32 +0000

    replace precompiled binary by dsk conda recipe

--- recipes/short-read-connector/build.sh
+++ recipes/short-read-connector/build.sh
@@ -4,6 +4,7 @@ mkdir -p ${PREFIX}/bin
 
 # decrease RAM needed
 sed -i.bak 's/make -j/make -j1/' INSTALL
+sed -i.bak 's/2> log_linker_err//' test/simple_test.sh
 
 # installation
 sh INSTALL
@@ -11,16 +12,9 @@ sh INSTALL
 # change run_discoSnp++.sh deps path
 sed -i.bak 's/\$EDIR\/bin/\$EDIR/' short_read_connector.sh
 
-# copy dsk binary
-if [[ $(uname) == "Darwin" ]]; then
-	echo "Configuring for OSX..."
-    cp thirdparty/dsk/bin/macosx/dsk ${PREFIX}/bin
-    sed -i.bak 's/\$EDIR\/thirdparty\/dsk\/bin\/macosx\/dsk/\$EDIR\/dsk/' short_read_connector.sh
-else
-    echo "Configuring for Linux..."
-    cp thirdparty/dsk/bin/linux/dsk ${PREFIX}/bin
-    sed -i.bak 's/\$EDIR\/thirdparty\/dsk\/bin\/linux\/dsk/\$EDIR\/dsk/' short_read_connector.sh
-fi
+# remove precompiled binary for dsk
+sed -i.bak 's/\$EDIR\/thirdparty\/dsk\/bin\/linux\/dsk/dsk/' short_read_connector.sh
+sed -i.bak 's/\$EDIR\/thirdparty\/dsk\/bin\/macosx\/dsk/dsk/' short_read_connector.sh
 
 # copy binaries
 cp short_read_connector.sh ${PREFIX}/bin
