9869b52505763054903e5d89d70b94f7382b83d9
cmonjeau <cyril.monjeaud@gmail.com>
2017-11-16 08:38:14 +0000

    change sed syntax

--- recipes/short-read-connector/build.sh
+++ recipes/short-read-connector/build.sh
@@ -7,11 +7,11 @@ sed -i.bak 's/make -j/make -j1/' INSTALL
 sed -i.bak 's/2> log_linker_err//' test/simple_test.sh
 
 # change run_discoSnp++.sh deps path
-sed -i.bak 's/\$EDIR\/bin/\$EDIR/' short_read_connector.sh
+sed -i.bak 's|\$EDIR/bin|\$EDIR|' short_read_connector.sh
 
 # remove precompiled binary for dsk
-sed -i.bak 's/\$EDIR\/thirdparty\/dsk\/bin\/linux\/dsk/dsk/' short_read_connector.sh
-sed -i.bak 's/\$EDIR\/thirdparty\/dsk\/bin\/macosx\/dsk/dsk/' short_read_connector.sh
+sed -i.bak 's|\$EDIR/thirdparty/dsk/bin/linux/dsk|dsk|' short_read_connector.sh
+sed -i.bak 's|\$EDIR/thirdparty/dsk/bin/macosx/dsk|dsk|' short_read_connector.sh
 
 # installation
 sh INSTALL
