c66e819ff20c8759b5f661c50609369b9a360107
caleb <caleb.easterly@gmail.com>
2017-11-20 16:06:27 -0600

    remove softlinks to txic and properties

--- recipes/moff/build.sh
+++ recipes/moff/build.sh
@@ -7,5 +7,3 @@ cp -r * $outdir
 ln -s $outdir/moff_mbr.py  $PREFIX/bin/moff_mbr.py
 ln -s $outdir/moff.py $PREFIX/bin/moff.py
 ln -s $outdir/moff_all.py $PREFIX/bin/moff_all.py
-ln -s $outdir/moff_setting.properties $PREFIX/bin/moff_setting.properties
-ln -s $outdir/txic $PREFIX/bin/txic
