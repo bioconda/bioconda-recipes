0da61d0af8aaa49e50c9e1b24f4e4fd9367e1337
Dan Sun <dan.sun@agresearch.co.nz>
2017-11-22 12:48:44 +1300

    Corrected build environment valuables in TASSEL's build script.

--- recipes/tassel/4.3.15/build.sh
+++ recipes/tassel/4.3.15/build.sh
@@ -15,15 +15,15 @@ sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $TASSEL_HOME/*.pl
 
 # Create wrapper script in bin to call TASSEL's launch scripts
 cat >"${BINARY_HOME}/run_pipeline.sh" <<EOF
-exec \$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_pipeline.pl "\$@"
+exec $CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_pipeline.pl "\$@"
 EOF
 
 cat >"${BINARY_HOME}/run_anything.sh" <<EOF
-exec \$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_anything.pl "\$@"
+exec $CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_anything.pl "\$@"
 EOF
 
 cat >"${BINARY_HOME}/start_tassel.sh" <<EOF
-exec \$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/start_tassel.pl "\$@"
+exec $CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/start_tassel.pl "\$@"
 EOF
 
 chmod +x ${BINARY_HOME}/run_pipeline.sh
