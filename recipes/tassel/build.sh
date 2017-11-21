091595f951845e1314fead0dd3a152b93e3caca0
Dan Sun <dan.sun@agresearch.co.nz>
2017-11-21 13:12:53 +1300

    Updated TASSEL5's build recipe. - Don't build for MacOS - Deploy TASSEL binary in the recomemnded locaiton inside a conda environment - Corrected custom deactivation script

--- recipes/tassel/build.sh
+++ recipes/tassel/build.sh
@@ -1,5 +1,5 @@
 BINARY_HOME=$PREFIX/bin
-TASSEL_HOME=$PREFIX/opt/tassel
+TASSEL_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
 # Copy source to bin
 mkdir -p $BINARY_HOME
@@ -9,12 +9,12 @@ cp -R $SRC_DIR/* $TASSEL_HOME/
 # Fix shabang lines
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $TASSEL_HOME/*.pl
 
-# Create custome (de)activate scripts to append or remove TASSEL's top level directory to the PATH
+# Create custom (de)activate scripts to append or remove TASSEL's top level directory to the PATH
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 cat >"${PREFIX}/etc/conda/activate.d/tassel-activate.sh" <<EOF
-export PATH="\$CONDA_PREFIX/opt/tassel:\$PATH"
+export PATH="\$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM:\$PATH"
 EOF
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
 cat >"${PREFIX}/etc/conda/deactivate.d/tassel-deactivate.sh" <<EOF
-export ARBHOME="\$CONDA_PATH_BACKUP"
+export PATH="\$CONDA_PATH_BACKUP"
 EOF
