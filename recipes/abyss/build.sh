61f183ff251ac7946c8d2ac770867ad7d55cfc63
Marcel Bargull <marcel.bargull@udo.edu>
2017-11-21 14:05:33 +0100

    abyss: fix configure parameters (maybe)

--- recipes/abyss/build.sh
+++ recipes/abyss/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-if [[ $(uname) == "Darwin" ]]; then
-	echo "Configuring for OSX..."
-	export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include -I$PREFIX/include/boost --stdlib=libstdc++"
-	export CXXFLAGS="${CXXFLAGS} --stdlib=libstdc++"
-else
-	echo "Configuring for Linux..."
-	export CPPFLAGS="-I$PREFIX/include"
-fi
-
-./configure --prefix=$PREFIX --with-boost=$PREFIX/include --with-mpi=$PREFIX/include --enable-maxk=96
+./configure --prefix=$PREFIX --with-boost=$PREFIX --with-mpi=$PREFIX --enable-maxk=96
 make AM_CXXFLAGS=-Wall
 make install
 
