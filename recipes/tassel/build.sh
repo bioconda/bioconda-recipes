fa66d72c2064c110458fd854af88844a1be2d077
Dan Sun <dan.sun@agresearch.co.nz>
2017-11-22 08:56:28 +1300

    Implemented wrappers to call TASSEL's wrappers to avoid adding TASSEL home directory in the PATH.

--- recipes/tassel/build.sh
+++ recipes/tassel/build.sh
@@ -9,12 +9,19 @@ cp -R $SRC_DIR/* $TASSEL_HOME/
 # Fix shabang lines
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $TASSEL_HOME/*.pl
 
-# Create custom (de)activate scripts to append or remove TASSEL's top level directory to the PATH
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-cat >"${PREFIX}/etc/conda/activate.d/tassel-activate.sh" <<EOF
-export PATH="\$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM:\$PATH"
+# Create wrapper script in bin to call TASSEL's launch scripts
+cat >"${BINARY_HOME}/run_pipeline.sh" <<EOF
+exec \$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_pipeline.pl "\$@"
 EOF
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cat >"${PREFIX}/etc/conda/deactivate.d/tassel-deactivate.sh" <<EOF
-export PATH="\$CONDA_PATH_BACKUP"
+
+cat >"${BINARY_HOME}/run_anything.sh" <<EOF
+exec \$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_anything.pl "\$@"
 EOF
+
+cat >"${BINARY_HOME}/start_tassel.sh" <<EOF
+exec \$CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/start_tassel.pl "\$@"
+EOF
+
+chmod +x ${BINARY_HOME}/run_pipeline.sh
+chmod +x ${BINARY_HOME}/run_anything.sh
+chmod +x ${BINARY_HOME}/start_tassel.sh
