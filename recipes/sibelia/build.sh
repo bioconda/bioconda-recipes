f5f612a7d8038a51e759391a4aa14cbef9d899ad
nickp60 <nickp60@gmail.com>
2017-11-25 23:20:11 +0000

    linux build passing, problem with g++.  removed explicit path setting to cmake

--- recipes/sibelia/build.sh
+++ recipes/sibelia/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd build
 # this was recommended by https://github.com/ContinuumIO/anaconda-issues/issues/483
-cmake ../src -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ -DCMAKE_INSTALL_PREFIX=${PREFIX} 
+cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX} 
 make
 make install
