0b4edb49523e37b99d3e5f898e1560d5eb18072c
nickp60 <nickp60@gmail.com>
2017-11-26 16:49:52 +0000

    downloading prebuilt binaries from sourcefoge

--- recipes/sibelia/build.sh
+++ recipes/sibelia/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd build
-# this was recommended by https://github.com/ContinuumIO/anaconda-issues/issues/483
-cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX} 
-make
-make install
+mkdir -p ${PREFIX}/bin ${PREFIX}/lib ${PREFIX}/share
+cp -r bin/*  ${PREFIX}/bin
+cp -r lib/Sibelia/* ${PREFIX}/lib
+cp -r share/Sibelia/* ${PREFIX}/share
