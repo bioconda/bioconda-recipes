ec396cb3c2faf4b24ecf0310a2db02124092e05f
Dan Sun <dan.sun@agresearch.co.nz>
2017-11-27 08:16:03 +1300

    Revision on TASSEL's build recipes: - Corrected patches for TASSEL3 and TASSEL; - Correct testing commands; - Use TASSEL's current official git repo as source; - Added patches to TASSEL5's build recipe.

--- recipes/tassel/4.3.15/build.sh
+++ recipes/tassel/4.3.15/build.sh
@@ -3,24 +3,9 @@ TASSEL_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
 # Copy source to the conda environment
 mkdir -p $TASSEL_HOME
-cp -R $SRC_DIR/* $TASSEL_HOME/
+cp -R $SRC_DIR/* $TASSEL_HOME//
 
-# Fix shabang lines
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $TASSEL_HOME/*.pl
-
-# Create wrapper script in bin to call TASSEL's launch scripts
-cat >"${BINARY_HOME}/run_pipeline.sh" <<EOF
-exec $CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_pipeline.pl "\$@"
-EOF
-
-cat >"${BINARY_HOME}/run_anything.sh" <<EOF
-exec $CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/run_anything.pl "\$@"
-EOF
-
-cat >"${BINARY_HOME}/start_tassel.sh" <<EOF
-exec $CONDA_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/start_tassel.pl "\$@"
-EOF
-
-chmod +x ${BINARY_HOME}/run_pipeline.sh
-chmod +x ${BINARY_HOME}/run_anything.sh
-chmod +x ${BINARY_HOME}/start_tassel.sh
+# Create symbolic links for TASSEL's wrappers
+ln -s $TASSEL_HOME/run_anything.pl $BINARY_HOME/
+ln -s $TASSEL_HOME/run_pipeline.pl $BINARY_HOME/
+ln -s $TASSEL_HOME/start_tassel.pl $BINARY_HOME/
