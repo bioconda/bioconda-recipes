ec1a4803aced8991e825eca27835b569ac4afbf7
takadonet <philipmabon@gmail.com>
2017-11-28 09:19:43 -0600

    removed boilerplate output

--- recipes/ga4ghmongo/build.sh
+++ recipes/ga4ghmongo/build.sh
@@ -2,9 +2,3 @@
 
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
