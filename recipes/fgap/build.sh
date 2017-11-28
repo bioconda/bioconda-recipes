f42fdcbd22aaa51a840c6fb86096d31fa94ab6bb
nickp60 <nickp60@gmail.com>
2017-11-28 11:48:06 +0000

    remove initialization message from build script

--- recipes/fgap/build.sh
+++ recipes/fgap/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "#!/bin/bash" >> FGAP
-ls
-echo "echo 'starting FGAP'" >> FGAP
 echo "octave --path \"$PREFIX/bin/\" --no-gui --eval \"fgap \$@\"" >> FGAP
 chmod +x FGAP
 cp fgap.m $PREFIX/bin/
