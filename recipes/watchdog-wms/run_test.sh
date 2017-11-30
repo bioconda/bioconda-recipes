e63961e3db7b069cff5980fe06b529765dd66425
Michael Kluge <klugem@users.noreply.github.com>
2017-11-30 14:32:19 +0100

    Update run_test.sh

--- recipes/watchdog-wms/run_test.sh
+++ recipes/watchdog-wms/run_test.sh
@@ -6,6 +6,15 @@ set -eu -o pipefail
 VERSION="${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 OUT="${PREFIX}/share/${VERSION}"
 
+# configure the test examples in fast run mode
+cd "${OUT}/helper_scripts" && configureExamples.sh -f -i "${OUT}"
+# ensure that sleep module is in the right place
+TMP_MODULE_DIR="${OUT}/myCustomFolder/"
+mkdir -p "${TMP_MODULE_DIR}"
+cp -r "${OUT}/modules/sleep" "${TMP_MODULE_DIR}/."
+${OUT}/core_lib/wrapper/sedinline "s#<folder>/home/TODO/additionalModules/</folder>##" "${OUT}/examples/example_include.xml"
+${OUT}/core_lib/wrapper/sedinline "s#/home/TODO/additionalModules#${TMP_MODULE_DIR}#" "${OUT}/examples/example_include.xml"
+
 # basic test
 watchdog-cmd --help 2>&1 1> /dev/null
 exit 0
