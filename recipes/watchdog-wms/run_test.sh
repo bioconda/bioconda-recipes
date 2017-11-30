f539c18affef3b7cb32a01e7881b6cccdc11c830
Michael Kluge <klugem@users.noreply.github.com>
2017-11-30 14:20:36 +0100

    Update run_test.sh

--- recipes/watchdog-wms/run_test.sh
+++ recipes/watchdog-wms/run_test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+# stop on error
+set -eu -o pipefail
+
+# create output folder name
+VERSION="${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
+OUT="${PREFIX}/share/${VERSION}"
+
+# basic test
+watchdog-cmd --help 2>&1 1> /dev/null
 exit 0
