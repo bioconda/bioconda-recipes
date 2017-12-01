aa228970ee4052efc87d83dc1fe6d15696a4cabd
Cashalow <sachalau@gmail.com>
2017-12-01 16:15:14 +0100

    fix for stderr

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -2,6 +2,8 @@
 ./autogen.sh
 ./configure --prefix=${PREFIX}
 make
-sed -i '1i#!/usr/bin/env python\n/' Parsnp.py
+sed -i.bak '1i#!/usr/bin/env python\n' Parsnp.py
+sed -i.bak 's/sys\.stderr\.write(\ BOLDME/sys\.stdout\.write(\ BOLDME/' Parsnp.py
+head Parsnp.py 
 mv Parsnp.py parsnp
 /usr/bin/install parsnp ${PREFIX}/bin/
