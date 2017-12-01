06c1d9cb5e04db4344f4fce1dc8249521d986553
Cashalow <sachalau@gmail.com>
2017-12-01 17:24:42 +0100

    removing useless sed

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -3,6 +3,5 @@
 ./configure --prefix=${PREFIX}
 make
 sed -i.bak '1i#!/usr/bin/env python\n' Parsnp.py
-sed -i.bak 's/sys\.stderr\.write(\ BOLDME/sys\.stdout\.write(\ BOLDME/' Parsnp.py
 mv Parsnp.py parsnp
 /usr/bin/install parsnp ${PREFIX}/bin/
