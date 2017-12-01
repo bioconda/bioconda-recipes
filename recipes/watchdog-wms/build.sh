681627622eb97ef4c1e09fbb933a8a85a93a6e37
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2017-12-01 15:57:09 +0100

    sed call

--- recipes/watchdog-wms/build.sh
+++ recipes/watchdog-wms/build.sh
@@ -14,6 +14,10 @@ cp -R * "${OUT}/"
 mv "${OUT}/watchdog.sh" "${OUT}/watchdog-cmd.sh"
 mv "${OUT}/workflowDesigner.sh" "${OUT}/watchdog-gui.sh"
 
+# modify path of included files
+sed -i 's|$SCRIPT_FOLDER/|$SCRIPT_FOLDER/../share/'${VERSION}'/|g' "${OUT}/watchdog-cmd.sh"
+sed -i 's|$SCRIPT_FOLDER/|$SCRIPT_FOLDER/../share/'${VERSION}'/|g' "${OUT}/watchdog-gui.sh"
+
 # create symbolic links and make them executable
 ln -s "${OUT}/watchdog-cmd.sh" "${PREFIX}/bin/watchdog-cmd"
 chmod 0755 "${PREFIX}/bin/watchdog-cmd"
