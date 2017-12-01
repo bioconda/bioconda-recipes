8b66bbd3de94185933215191c4dc2fbbaa2d2376
Matt <gopezm@myumanitoba.ca>
2017-12-01 15:30:43 -0600

    Removing old sed statements

--- recipes/scanindel/build.sh
+++ recipes/scanindel/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-sed -i -e 's|#!/usr/bin/python|#!/usr/bin/env python|g' ScanIndel.py
-sed -i -e 's|#!/usr/bin/python|#!/usr/bin/env python|g' tools/vcf-combine.py
 cp ScanIndel.py $PREFIX/bin
 cp tools/* $PREFIX/bin
