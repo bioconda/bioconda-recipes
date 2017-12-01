79e7e6b9561fbf6dac478cc21ce1771fa68c2547
Cashalow <sachalau@gmail.com>
2017-12-01 16:37:12 +0100

    modif test

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -4,6 +4,5 @@
 make
 sed -i.bak '1i#!/usr/bin/env python\n' Parsnp.py
 sed -i.bak 's/sys\.stderr\.write(\ BOLDME/sys\.stdout\.write(\ BOLDME/' Parsnp.py
-head Parsnp.py 
 mv Parsnp.py parsnp
 /usr/bin/install parsnp ${PREFIX}/bin/
