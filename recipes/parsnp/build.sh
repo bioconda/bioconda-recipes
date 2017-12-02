6de58443e9e21d25434e3e9e3ea388fe5c3f8bc7
Cashalow <sachalau@gmail.com>
2017-12-02 14:37:34 +0100

    building with correct libmuscle path

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix=${PREFIX}
+./configure --prefix=${PREFIX} --with-libmuscle=${PREFIX}/include/libMUSCLE-3.7/
 make 
+make install
+ls -Srthl src/*
+ls -Srthl bin/*
+
 mv src/parsnp ${PREFIX}/bin/ 
