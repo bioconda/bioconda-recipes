46f0e1285637045b5209c51b62c79bbe1ef8bc76
Björn Grüning <bjoern@gruenings.eu>
2017-12-02 09:38:40 +0100

    fix filename

--- recipes/treetime/0.2.1/build.sh
+++ recipes/treetime/0.2.1/build.sh
@@ -5,4 +5,4 @@ $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 mkdir -p $PREFIX/bin
 cp ancestral_reconstruction.py $PREFIX/bin
 cp timetree_inference.py $PREFIX/bin
-cp temporal_signal.py.py $PREFIX/bin
+cp temporal_signal.py $PREFIX/bin
