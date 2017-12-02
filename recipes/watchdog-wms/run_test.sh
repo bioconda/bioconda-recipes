323c8bc1d2a4b7894e16ec40e6dd1002154c949c
Michael Kluge <klugem@users.noreply.github.com>
2017-12-02 09:34:59 +0100

    Update run_test.sh
    make sed mac compatible

--- recipes/watchdog-wms/run_test.sh
+++ recipes/watchdog-wms/run_test.sh
@@ -5,13 +5,14 @@ set -eu -o pipefail
 # create output folder name
 VERSION="${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 OUT="${PREFIX}/share/${VERSION}"
+PATH="$PATH:$OUT/core_lib/wrapper/"
 
 # basic test
 timeout 15s watchdog-cmd --help 2>&1 1> /dev/null
 
 # prepare workflow test
 TEST_FILE="workflow.test.xml"
-sed -i "s#BASE_DIR#${OUT}/#" "${TEST_FILE}"
+sedinline "s#BASE_DIR#${OUT}/#" "${TEST_FILE}"
 
 # workflow test
 timeout 15s watchdog-cmd -mailWaitTime 0 -x "${TEST_FILE}" -p 8616 2>&1 1> /dev/null
