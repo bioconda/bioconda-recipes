4e1f25f3667bbe1a8e67e285746e8733203bef49
Peter van Heusden <pvh@sanbi.ac.za>
2017-12-07 20:51:10 +0200

    Revert build evolved from 2.4.0 build, add seqtk build, add tests

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -11,8 +11,6 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-
-BINARY=Trinity
 BINARY_HOME=$PREFIX/bin
 TRINITY_HOME=$PREFIX/opt/trinity-$PKG_VERSION
 
@@ -20,6 +18,11 @@ cd $SRC_DIR
 
 make inchworm_target
 make chrysalis_target
+
+cd $SRC_DIR/trinity-plugins/
+#make scaffold_iworm_contigs_target
+make seqtk_target
+cd $SRC_DIR
 
 # remove the sample data
 rm -rf $SRC_DIR/sample_data
@@ -29,13 +32,16 @@ mkdir -p $PREFIX/bin
 mkdir -p $TRINITY_HOME
 cp -R $SRC_DIR/* $TRINITY_HOME/
 cd $TRINITY_HOME && chmod +x Trinity
-cd $BINARY_HOME && ln -s $TRINITY_HOME/Trinity $BINARY
-ln -s $TRINITY_HOME/util/* .
+cd $BINARY_HOME
+ln -s $TRINITY_HOME/Trinity
+ln -s $TRINITY_HOME/util/*.pl .
 ln -s $TRINITY_HOME/Analysis/DifferentialExpression/PtR
 ln -s $TRINITY_HOME/Analysis/DifferentialExpression/run_DE_analysis.pl
 ln -s $TRINITY_HOME/Analysis/DifferentialExpression/analyze_diff_expr.pl
 ln -s $TRINITY_HOME/Analysis/DifferentialExpression/define_clusters_by_cutting_tree.pl
 ln -s $TRINITY_HOME/util/support_scripts/get_Trinity_gene_to_trans_map.pl
+ln -s $TRINITY_HOME/util/misc/contig_ExN50_statistic.pl
+cp $TRINITY_HOME/trinity-plugins/BIN/seqtk-trinity .
 
 # Find real path when executing from a symlink
 export LC_ALL=C
