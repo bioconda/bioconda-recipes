292206a40eaec601f70ed5d214ae73f81eadc19a
Steve Davis <steven.davis@fda.hhs.gov>
2017-12-08 09:52:35 -0500

    snp-pipeline 1.0.1 : update build requirements, entry points, comments.

--- recipes/snp-pipeline/build.sh
+++ recipes/snp-pipeline/build.sh
@@ -2,8 +2,6 @@
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
-# Add more build steps here, if they are necessary.
-
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
