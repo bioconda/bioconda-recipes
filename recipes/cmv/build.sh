776cffcbe5cd5a501c32d0f1fb0efe20da067d13
Florian Eggenhofer <egg@tbi.univie.ac.at>
2017-12-09 14:30:54 +0100

    Explicitly set stack root for cmv

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -4,12 +4,10 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 pwd
-export wdir=`pwd `
-cp -r * /tmp
-cd /tmp
-stack setup
-stack update
-stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+export wdir=`pwd`
+stack --stack-root $wdir setup
+stack --stack-root $wdir update
+stack install --stack-root $wdir --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
 
