dadc087ecbd273dc905782e06dd96d2dd70e8bb5
Florian Eggenhofer <egg@tbi.univie.ac.at>
2017-12-09 13:16:11 +0100

    Added debugging output for osx

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,7 +3,10 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-
+pwd
+export wdir=`pwd `
+cp -r * /tmp
+cd tmp
 stack setup
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
