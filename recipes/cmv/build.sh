48fbc42464d0b8aabf406726e4d6425ea6d360bf
Florian Eggenhofer <egg@tbi.univie.ac.at>
2017-12-09 15:24:34 +0100

    Further fix for osx cmv build

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,11 +3,18 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-pwd
-export wdir=`pwd`
-stack --stack-root $wdir setup
-stack --stack-root $wdir update
-stack install --stack-root $wdir --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+if [ `uname` == Darwin ]
+then
+    export STACK_ROOT="${SRC_DIR}/s"
+    stack setup --local-bin-path ${SRC_DIR}/s
+    stack update
+    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+    rm -r "${SRC_DIR}/s"
+else
+    stack setup
+    stack update
+    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+fi
 #cleanup
 rm -r .stack-work
 
