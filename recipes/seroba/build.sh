0f751da61e82879c802db1cfe2aa57a4422c8e78
Matt <gopezm@myumanitoba.ca>
2017-12-12 09:42:36 -0600

    Adding seroba recipe.

---
+++ recipes/seroba/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Have to download the precompiled binary as we don't have a compiler on conda which supports C++14.
+wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
+tar xzf KMC3.linux.tar.gz
+
+chmod 755 kmc
+chmod 755 kmc_tools
+chmod 755 kmc_dump
+
+cp kmc $PREFIX/bin
+cp kmc_tools $PREFIX/bin
+cp kmc_dump $PREFIX/bin
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
