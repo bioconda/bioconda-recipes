8f97c6e6a7005ea9ffd6bc8bfc003eb1f4dbaf87
Matt <gopezm@myumanitoba.ca>
2017-12-13 10:29:25 -0600

    Adding recipe for seroba.

--- recipes/seroba/build.sh
+++ recipes/seroba/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Have to download the precompiled binary as we don't have a compiler on conda which supports C++14.
+# We don't have a compiler on Conda for C++14, so downloading pre-compiled binary and moving to bin.
 wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
 tar xzf KMC3.linux.tar.gz
 
