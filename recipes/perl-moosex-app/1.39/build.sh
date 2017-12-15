02b392e905410a5d68f816266e9b348bee699fcf
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-12-15 09:52:56 +0400

    stupid cpanm

--- recipes/perl-moosex-app/1.39/build.sh
+++ recipes/perl-moosex-app/1.39/build.sh
@@ -11,10 +11,10 @@ fi
 
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    ./Build test
+    perl ./Build
+    perl ./Build test
     # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
