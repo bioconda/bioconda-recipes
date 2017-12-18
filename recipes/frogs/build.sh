1db65bc667425c0bf1b8fea76b3eff17855dbd69
root <root@vm0011.france-bioinformatique.fr>
2017-12-18 11:42:30 +0100

    add frogs recipe

---
+++ recipes/frogs/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BINARY_HOME=$PREFIX/bin
+LIB_HOME=$PREFIX/lib
+LIBEXEC_HOME=$PREFIX/libexec
+FROGS_HOME=$PREFIX/share/FROGS-$PKG_VERSION
+
+mkdir -p $PREFIX/share/
+mkdir -p $FROGS_HOME
+mkdir -p $BINARY_HOME
+mkdir -p $LIB_HOME
+mkdir -p $LIBEXEC_HOME
+
+
+mv $SRC_DIR/* $FROGS_HOME
+
+ln -s $FROGS_HOME/app/*.py $BINARY_HOME
+ln -s $FROGS_HOME/app/*.html $BINARY_HOME
+ln -s $FROGS_HOME/app/*.Rmd $BINARY_HOME
+ln -s $FROGS_HOME/lib/* $LIB_HOME
+ln -s $FROGS_HOME/libexec/* $LIBEXEC_HOME
+ln -s $PREFIX/share/rdptools-2.0.2-0/classifier.jar $BINARY_HOME
