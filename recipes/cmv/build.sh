e404832ce2714d28af9f3a6811ef8b3f15db621d
Florian Eggenhofer <egg@tbi.univie.ac.at>
2017-12-20 23:22:27 +0100

    Fixed additional nopie flag

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -6,7 +6,7 @@ export CPPFLAGS="-I${PREFIX}/include"
 if [ `uname` == Darwin ]
 then
     export STACK_ROOT="${SRC_DIR}/s"
-    stack setup --ghc-build=nopie --extra-include-dirs ${PREFIX}/include --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
+    stack setup --extra-include-dirs ${PREFIX}/include --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
     stack update
     stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
