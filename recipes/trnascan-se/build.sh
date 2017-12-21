6b8303fca66678be6ccbf1837d9f9b08bf935736
Björn Grüning <bjoern@gruenings.eu>
2017-12-21 20:11:42 +0100

    Update build.sh

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -6,6 +6,3 @@ sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' src/instman.pl
 
 make
 make install
-
-# run a small test
-$PREFIX/bin/tRNAscan-SE --quiet --breakdown --hitsrc --detail Demo/Example1.fa 2>/dev/null | diff Demo/Example1-tRNAs.out -
