61e5af46a4029dee08f02a74d45bdfee486d99c8
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-12-21 21:26:01 +0100

    Added Info.plist

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -7,6 +7,7 @@ if [ `uname` == Darwin ]
 then
     export DYLD_LIBRARY_PATH="${PREFIX}/lib"
     export STACK_ROOT="${SRC_DIR}/s"
+    echo "<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>CFBundleIdentifier</key><string>com.github.eggzilla.cmv</string></dict></plist>" > $SRC_DIR/Info.plist
     stack setup --extra-include-dirs ${PREFIX}/include --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
     stack update
     stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
