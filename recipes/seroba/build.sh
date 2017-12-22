5f1b3456f9ff51b918c16b1a4d250a14a2a9ef70
Marcel Bargull <marcel.bargull@udo.edu>
2017-12-22 17:22:05 +0100

    seroba: require kmc >=3.0 Conda package

--- recipes/seroba/build.sh
+++ recipes/seroba/build.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# We don't have a compiler on Conda for C++14, so downloading pre-compiled binary and moving to bin.
-wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
-tar xzf KMC3.linux.tar.gz
-
-chmod 755 kmc
-chmod 755 kmc_tools
-chmod 755 kmc_dump
-
-cp kmc $PREFIX/bin
-cp kmc_tools $PREFIX/bin
-cp kmc_dump $PREFIX/bin
-
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
