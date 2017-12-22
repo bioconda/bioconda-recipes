2ed22f90dc204127b882d4bb94213281568a8346
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-12-22 18:28:02 +0100

    Added extra lib dirs for macos

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -7,9 +7,9 @@ if [ `uname` == Darwin ]
 then
     #export DYLD_LIBRARY_PATH="/opt/local/lib:${PREFIX}/lib:$DYLD_LIBRARY_PATH"
     export STACK_ROOT="${SRC_DIR}/s"
-    stack setup --extra-include-dirs ${PREFIX}/include --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
+    stack setup --extra-include-dirs [/usr/include,${PREFIX}/include] --extra-lib-dirs [/usr/lib,${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
     stack update
-    stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
+    stack install --extra-include-dirs [/usr/include,${PREFIX}/include] --extra-lib-dirs [/usr/lib,${PREFIX}/lib] --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
 else
     stack setup
