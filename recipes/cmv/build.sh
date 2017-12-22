872924d340150004d967da02257bb97d69a51a58
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-12-22 19:32:23 +0100

    Changed library setup - again

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -5,11 +5,11 @@ export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 if [ `uname` == Darwin ]
 then
-    #export DYLD_LIBRARY_PATH="/opt/local/lib:${PREFIX}/lib:$DYLD_LIBRARY_PATH"
+    export DYLD_LIBRARY_PATH="${PREFIX}/lib"
     export STACK_ROOT="${SRC_DIR}/s"
-    stack setup --extra-include-dirs [/usr/include,${PREFIX}/include] --extra-lib-dirs [/usr/lib,${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
+    stack setup --extra-include-dirs [${PREFIX}/include] --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
     stack update
-    stack install --extra-include-dirs [/usr/include,${PREFIX}/include] --extra-lib-dirs [/usr/lib,${PREFIX}/lib] --local-bin-path ${PREFIX}/bin
+    stack install --extra-include-dirs [${PREFIX}/include] --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
 else
     stack setup
