09688a1629b9fc912457e421384e58c2d9e219c2
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-12-25 15:44:22 +0100

    Added original dynlibpath

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -5,7 +5,7 @@ export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 if [ `uname` == Darwin ]
 then
-    export DYLD_LIBRARY_PATH="/System/Library/Frameworks/ImageIO.framework/Resources/:${PREFIX}/lib"
+    export DYLD_LIBRARY_PATH="/System/Library/Frameworks/ImageIO.framework/Resources/:${PREFIX}/lib:$DYLD_LIBRARY_PATH"
     export STACK_ROOT="${SRC_DIR}/s"
     stack setup --extra-include-dirs [${PREFIX}/include] --extra-lib-dirs [${PREFIX}/lib] --local-bin-path ${SRC_DIR}/s
     stack update
