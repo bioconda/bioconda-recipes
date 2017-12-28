5e688f399bf9a92e95680cd6476bf142060a8771
Cashalow <sachalau@gmail.com>
2017-12-28 18:06:56 +0100

    updates

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -6,4 +6,21 @@ make install
 ls -Srthl src/*
 ls -Srthl bin/*
 
-mv src/parsnp ${PREFIX}/bin/ 
+cp -R * ${PREFIX}/bin/parsnp_folder/
+
+cp src/parsnp ${PREFIX}/bin/
+cp Parsnp.py ${PREFIX}/bin/
+cp template.ini ${PREFIX}/bin/
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    cp bin/harvest_osx ${PREFIX}/bin/harvest
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cp bin/harvest_linux ${PREFIX}/bin/harvest
+fi
+
+
+ln -s ${PREFIX}/bin/fasttree ${PREFIX}/bin/ft
+ln -s ${PREFIX}/bin/Phi ${PREFIX}/bin/phiprofile
+
+
