f7a1cede37b0affc1949b078d71990e0519b4abc
German Uritskiy <guritsk1@jhu.edu@login-node03.cm.cluster>
2017-12-29 23:53:14 -0500

    added metabat2

---
+++ recipes/metabat2/build.sh
@@ -0,0 +1,13 @@
+#tar -xvf metabat-static-binary-linux-x64_v2.12.1.tar.gz
+
+# copying over all necessary files
+mkdir -p $PREFIX/bin/
+
+cp aggregateBinDepths.pl $PREFIX/bin/
+cp aggregateContigOverlapsByBin.pl $PREFIX/bin/
+cp contigOverlaps $PREFIX/bin/
+cp jgi_summarize_bam_contig_depths $PREFIX/bin/
+cp metabat $PREFIX/bin/
+cp metabat1 $PREFIX/bin/
+cp metabat2 $PREFIX/bin/
+cp runMetaBat.sh $PREFIX/bin/
