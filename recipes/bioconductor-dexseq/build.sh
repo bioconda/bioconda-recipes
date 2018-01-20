638621c7a7914c2b3eaddb18286d7cedd76d0f0f
Björn Grüning <bjoern@gruenings.eu>
2018-01-20 21:51:32 +0100

    Update build.sh

--- recipes/bioconductor-dexseq/build.sh
+++ recipes/bioconductor-dexseq/build.sh
@@ -5,8 +5,8 @@ $R CMD INSTALL --build .
 
 python_scripts=$PREFIX/lib/R/library/DEXSeq/python_scripts
 
-sed -i'' -e '1 i\'$'\n''#!/usr/bin/env python' $python_scripts/dexseq_count.py
-sed -i'' -e '1 i\'$'\n''#!/usr/bin/env python' $python_scripts/dexseq_prepare_annotation.py
+sed -i.bak '1 s|^.*$|#!/usr/bin/env python' $python_scripts/dexseq_count.py
+sed -i.bak '1 s|^.*$|#!/usr/bin/env python' $python_scripts/dexseq_prepare_annotation.py
 
 if [ "$PY3K" == 1 ]; then
     2to3 -w $python_scripts/dexseq_count.py
