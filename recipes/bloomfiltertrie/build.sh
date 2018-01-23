1c3d78c2db9eafd0c230957a25336caf20852fe5
Cole Lyman <cole@colelyman.com>
2018-01-23 16:59:30 -0700

    Draft the initial version of the bloomfiltertrie recipe.

---
+++ recipes/bloomfiltertrie/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CC=${PREFIX}/bin/gcc
+export CXX=${PREFIX}/bin/g++
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+mkdir -p $PREFIX/bin
+
+./configure --prefix=$PREFIX/bin
+make
+make install
