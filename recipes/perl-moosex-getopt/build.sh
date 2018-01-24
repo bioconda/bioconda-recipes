4c7e015117e56c8afbe7fee8128d8cfaf1b10901
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-01-24 12:07:13 +0400

    commit

--- recipes/perl-moosex-getopt/build.sh
+++ recipes/perl-moosex-getopt/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # If it has Build.PL use that, otherwise use Makefile.PL
-cpanm --installdep .
+
+cpanm -v Devel::StackTrace
+cpanm -v MooseX::Role::Parameterized
+
+cpanm -v --installdeps .
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    ./Build test
+    perl ./Build
+    perl ./Build test
     # Make sure this goes in site
     ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
