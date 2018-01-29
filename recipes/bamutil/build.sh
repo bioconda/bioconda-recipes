6e4c93fa62bb125bb93104363a98e05aaaa205ee
Marcel Bargull <marcel.bargull@udo.edu>
2018-01-29 01:44:24 +0100

    bamutil: add missing dep for and verify download

--- recipes/bamutil/build.sh
+++ recipes/bamutil/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 VERSION=1.0.14
-wget -O libStatGen-v$VERSION.tar.gz https://github.com/statgen/libStatGen/archive/v$VERSION.tar.gz
+SHA256=70a504c5cc4838c6ac96cdd010644454615cc907df4e3794c999baf958fa734b
+curl -Lo libStatGen-v$VERSION.tar.gz https://github.com/statgen/libStatGen/archive/v$VERSION.tar.gz
+RETRIEVED_SHA256=$(openssl sha256 libStatGen-v$VERSION.tar.gz)
+[[ ${RETRIEVED_SHA256##* } == $SHA256 ]] || exit 1
+
 tar -xzvpf libStatGen-v$VERSION.tar.gz
 make LIB_PATH_GENERAL=./libStatGen-$VERSION
 make install INSTALLDIR=$PREFIX LIB_PATH_GENERAL=./libStatGen-$VERSION
