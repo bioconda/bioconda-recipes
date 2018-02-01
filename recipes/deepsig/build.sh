55d8fb5024dfdec235aca6bb72962fbfb5007ae2
peterjc <p.j.a.cock@googlemail.com>
2018-02-01 15:35:47 +0000

    Replace runDeepSig.sh as primary entry point.
    More consistent over allowing deepsig.py to be executed
    directly.

--- recipes/deepsig/build.sh
+++ recipes/deepsig/build.sh
@@ -5,9 +5,17 @@ cp LICENSE $PREFIX/
 cp README $PREFIX/
 
 mkdir -p "$PREFIX/bin"
-
 cp bin/*.py $PREFIX/bin/
-chmod u+x $PREFIX/bin/*.py
+
+# Short wrapper script (replacement for author provided script)
+# which sets DEEPSIG_ROOT (for finding model files) and
+# PYTHONLIB (for finding the deepsiglib/*.py files).
+cat > $PREFIX/bin/runDeepSig.sh <<EOF
+#!/bin/bash
+export DEEPSIG_ROOT=$PREFIX
+PYTHONPATH=$PREFIX python $PREFIX/bin/deepsig.py "\$@"
+EOF
+chmod a+x $PREFIX/bin/runDeepSig.sh
 
 mkdir -p "$PREFIX/deepsiglib"
 cp deepsig/lib/python2.7/site-packages/deepsiglib/*.py $PREFIX/deepsiglib/
