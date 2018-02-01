ae78df36bde8bbe17ebaac518084e3d51272a5f5
peterjc <p.j.a.cock@googlemail.com>
2018-02-01 14:53:00 +0000

    First attempt to package deepsig
    Still need to sort out making the Python code importable.

---
+++ recipes/deepsig/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+cp LICENSE $PREFIX/
+cp README $PREFIX/
+
+mkdir -p "$PREFIX/bin"
+
+cp bin/*.py $PREFIX/bin/
+chmod u+x $PREFIX/bin/*.py
+
+mkdir -p "$PREFIX/deepsiglib"
+cp deepsig/lib/python2.7/site-packages/deepsiglib/*.py $PREFIX/deepsiglib/
+patch -d $PREFIX/ -p1 < python3.patch
+
+mkdir -p "$PREFIX/models"
+cp -r models/ $PREFIX/models/
