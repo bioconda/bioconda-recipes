e575b3a75b76988f30bec762d9c1d063cead1a67
Philippe Bordron <pbordron@users.noreply.github.com>
2018-02-01 17:47:53 +0100

    Update build.sh

--- recipes/stacks/2.0/build.sh
+++ recipes/stacks/2.0/build.sh
@@ -3,6 +3,6 @@
 # Using --libdir doesn't work.
 export LDFLAGS="-L${CONDA_PREFIX}/lib"
 
-./configure --prefix=$PREFIX --enable-sparsehash --enable-bam --includedir=${CONDA_PREFIX}/include/bam
+./configure --prefix=$PREFIX --enable-bam --includedir=${CONDA_PREFIX}/include/bam
 make
 make install
