db4d236c67e6794d1f9010e67673a3369f8cf7d5
Philippe Bordron <pbordron@users.noreply.github.com>
2018-02-02 11:49:07 +0100

    Update build.sh
    Correct included htslib compilation parameters

--- recipes/stacks/2.0/build.sh
+++ recipes/stacks/2.0/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # Using --libdir doesn't work.
-export LDFLAGS="-L${CONDA_PREFIX}/lib"
+export LDFLAGS="-L${CONDA_PREFIX}/lib" # for both stacks and included htslib dependency
+export CPATH=${PREFIX}/include         # for included htslib dependency
+export CFLAGS="-I${PREFIX}/include"    # for included htslib dependency
 
 ./configure --prefix=$PREFIX --enable-bam --includedir=${CONDA_PREFIX}/include/bam
+
+# Force included htslib library to use conda compiler toolsuite
+sed -i "s|^CC *.*|CC = $CC|" htslib/Makefile
+sed -i "s|^AR *.*|AR = $AR|" htslib/Makefile
+sed -i "s|^RANLIB *.*|RANLIB = $RANLIB|" htslib/Makefile
+sed "s|^\(LIBS *=.*\)$|\1 -I$CONDA_PREFIX/include/ -L$CONDA_PREFIX/lib/ -lz -lgomp|" htslib/Makefile
+
 make
 make install
