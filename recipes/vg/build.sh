b72a12dd0bb8425d104c9e04c3deb0bfb3eef6f8
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-02-05 14:01:39 +0100

    Use gxx. Fix env vars.

--- recipes/vg/build.sh
+++ recipes/vg/build.sh
@@ -5,7 +5,16 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+CC_=$CC
+CXX_=$CXX
+
 source source_me.sh
+
+# overwrite CC and CXX from source_me.sh
+# use conda binaries instead
+export CC=$CC_
+export CXX=$CXX_
+
 make static
 
 mkdir -p $PREFIX/bin
