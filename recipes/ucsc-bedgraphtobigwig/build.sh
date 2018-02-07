d75a5ac924d6f1910b45c8b360a40dd1bbe46f29
Karl Nordström <karl.nordstrom@gmail.com>
2018-02-07 16:55:30 +0100

    updated to v360 and pointed to github instead of hgdownload. Not sure if osx will build as it is

--- recipes/ucsc-bedgraphtobigwig/build.sh
+++ recipes/ucsc-bedgraphtobigwig/build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 mkdir -p "$PREFIX/bin"
-if [ "$(uname)" == "Darwin" ]; then
-    cp bedGraphToBigWig "$PREFIX/bin"
-else
-    export MACHTYPE=x86_64
-    export BINDIR=$(pwd)/bin
-    mkdir -p "$BINDIR"
-    (cd kent/src/lib && make)
-    (cd kent/src/htslib && make)
-    (cd kent/src/jkOwnLib && make)
-    (cd kent/src/hg/lib && make)
-    (cd kent/src/utils/bedGraphToBigWig && make)
-    cp bin/bedGraphToBigWig "$PREFIX/bin"
-fi
+export MACHTYPE=$(uname -m)
+export BINDIR=$(pwd)/bin/$MACHTYPE
+mkdir -p "$BINDIR"
+(cd src/lib && make)
+(cd src/htslib && make)
+(cd src/jkOwnLib && make)
+(cd src/hg/lib && make)
+(cd src/utils/bedGraphToBigWig && make)
+cp $BINDIR/bedGraphToBigWig "$PREFIX/bin"
 chmod +x "$PREFIX/bin/bedGraphToBigWig"
