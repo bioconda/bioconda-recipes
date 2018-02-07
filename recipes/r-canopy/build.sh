c29706c661e037282284fab316b6347fc59f06c6
Philip Reiner Kensche <vinjana@users.noreply.github.com>
2018-02-07 14:12:04 +0100

    R canopy (#7541)
    * Updated r-canopy to 1.3.0.

--- recipes/r-canopy/build.sh
+++ recipes/r-canopy/build.sh
@@ -5,3 +5,9 @@ mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
 $R CMD INSTALL --build .
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
