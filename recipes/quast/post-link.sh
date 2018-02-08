d5119622721955b02d88703d19c8d4899cdb861d
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-08 16:28:05 +0100

    quast: fix output of post-link/pre-unlink scripts

--- recipes/quast/post-link.sh
+++ recipes/quast/post-link.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo 'The default QUAST package does not include:'
-echo '* Manta (needed for structural variants detection)'
-echo '* SILVA 16S rRNA database (needed for reference genome detection in metagenomic datasets)'
-echo ''
-echo 'To be able to use those, please run'
-echo '    quast-download-manta'
-echo '    quast-download-blastdb'
+cat >> "$PREFIX/.messages.txt" <<EOF
+The default QUAST package does not include:
+* Manta (needed for structural variants detection)
+* SILVA 16S rRNA database (needed for reference genome detection in metagenomic datasets)
+
+To be able to use those, please run
+    quast-download-manta
+    quast-download-blastdb
+EOF
 exit 0
