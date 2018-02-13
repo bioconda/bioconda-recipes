07c429b300f895522aae251dc62ec441c3be1d76
Marcel Bargull <mbargull@users.noreply.github.com>
2018-02-13 14:26:58 +0100

    bamutil: use only `install` target to skip debug/profile builds

--- recipes/bamutil/build.sh
+++ recipes/bamutil/build.sh
@@ -7,7 +7,6 @@ RETRIEVED_SHA256=$(openssl sha256 libStatGen-v$VERSION.tar.gz)
 [[ ${RETRIEVED_SHA256##* } == $SHA256 ]] || exit 1
 
 tar -xzvpf libStatGen-v$VERSION.tar.gz
-make LIB_PATH_GENERAL=./libStatGen-$VERSION
 make install INSTALLDIR=$PREFIX LIB_PATH_GENERAL=./libStatGen-$VERSION
 mkdir -p $PREFIX/bin/
 mv $PREFIX/bam $PREFIX/bin/
