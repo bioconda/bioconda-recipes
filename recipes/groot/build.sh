ec6e20a1e29b201c3b4836e82d5ce62fc9122a4b
Will Rowe <will.rowe@stfc.ac.uk>
2018-02-14 14:24:30 +0000

    switching to compile GROOT from source

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
-mkdir -p src/github.com/will-rowe/groot
-mv cmd src main.go  src/github.com/will-rowe/groot
+mkdir groot
+mv cmd src main.go groot/
+mkdir -p src/github.com/will-rowe/
+mv groot src/github.com/will-rowe/
 cd src/github.com/will-rowe/groot
-export GOPATH="$SRC_DIR/"
 
+export GOPATH="$SRC_DIR/"
+go get ./...
 go build -o groot main.go
 mv groot $PREFIX/bin
