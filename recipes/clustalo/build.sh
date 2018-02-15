cd41006be9bda53b26a8924b176975337b3cfee1
Colin Brislawn <cbrisl@gmail.com>
2018-02-14 16:01:48 -0800

    Add OpenMP build to clustalo

--- recipes/clustalo/build.sh
+++ recipes/clustalo/build.sh
@@ -5,7 +5,11 @@ export LDFLAGS="-L$PREFIX/lib"
 
 ./configure --prefix=$SRC_DIR
 make
+./configure --prefix=$SRC_DIR --program-suffix=MP OPENMP_CFLAGS='-fopenmp' CFLAGS='-DHAVE_OPENMP'
+make
 
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/src/clustalo $PREFIX/bin/$PKG_NAME
-chmod +x $PREFIX/bin/$PKG_NAME
+cp $SRC_DIR/src/clustalo $PREFIX/bin/clustalo
+cp $SRC_DIR/src/clustaloMP $PREFIX/bin/clustaloMP
+chmod +x $PREFIX/bin/clustalo
+chmod +x $PREFIX/bin/clustaloMP
