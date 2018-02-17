b8d9d667f83b5c2a75d3d24064034cc76f03dbf4
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-17 13:50:25 +0100

    pplacer: replace hardcoded library paths

--- recipes/pplacer/build.sh
+++ recipes/pplacer/build.sh
@@ -3,11 +3,18 @@
 mkdir -p $PREFIX/bin
 
 cd $SRC_DIR
+
 if [ "$(uname)" == "Darwin" ]; then
-    set -x
-    otool -L ./guppy
-    otool -L ./pplacer
-    otool -L ./rppr
+    for tool in guppy pplacer rppr; do
+        for lib in \
+                /usr/lib/libz.1.dylib \
+                /usr/lib/libsqlite3.dylib \
+                /usr/local/lib/libgsl.0.dylib \
+                /usr/local/lib/libgslcblas.0.dylib
+            do install_name_tool -change "${lib}" "${PREFIX}/lib/${lib##*/}" ./${tool}
+        done
+    done
 fi
+
 cp guppy pplacer rppr $PREFIX/bin
 chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
