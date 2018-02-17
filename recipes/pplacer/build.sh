a8c5b5ea31cf4dca55f752712d12e50e9370adb7
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-17 20:19:49 +0100

    pplacer: exclude `rppr` from macOS package

--- recipes/pplacer/build.sh
+++ recipes/pplacer/build.sh
@@ -4,25 +4,24 @@ mkdir -p $PREFIX/bin
 
 cd $SRC_DIR
 
-if [ "$(uname)" == "Darwin" ]; then
-    # ugly workaround for:
+tools='guppy pplacer rppr'
+if [ "$(uname)" == 'Darwin' ]; then
+    # Exclude `rppr` from package due to:
     #  "install_name_tool: changing install names or rpaths can't be redone for:
     #   ./rppr (for architecture x86_64) because larger updated load commands do
     #   not fit (the program must be relinked, and you may need to use
     #   -headerpad or -headerpad_max_install_names)"
-    gslcblas_src=lib/libgslcblas.0.dylib
-    gslcblas_dst=gslcblas.0.dylib
-    ln -s "${PREFIX}/{gslcblas_src}" "${PREFIX}/{gslcblas_dst}"
-
-    for tool in guppy pplacer rppr; do
+    tools='guppy pplacer'
+    for tool in $tools; do
         for lib in /usr/lib/libz.1.dylib \
                    /usr/lib/libsqlite3.dylib \
-                   /usr/local/lib/libgsl.0.dylib
-            do install_name_tool -change "${lib}" "${PREFIX}/lib/${lib##*/}" ./${tool}
+                   /usr/local/lib/libgsl.0.dylib \
+                   /usr/local/lib/libgslcblas.0.dylib; do
+            install_name_tool -change "${lib}" "${PREFIX}/lib/${lib##*/}" ./${tool}
         done
-        install_name_tool -change "/usr/local/${gslcblas_src}" "${PREFIX}/${gslcblas_dst}" ./${tool}
     done
 fi
 
-cp guppy pplacer rppr $PREFIX/bin
-chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
+cp $tools $PREFIX/bin
+cd $PREFIX/bin
+chmod +x $tools
