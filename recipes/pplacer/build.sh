7d77364b198f44f735ff76f225be4b830c7a404b
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-17 01:44:35 +0100

    pplacer: show library paths

--- recipes/pplacer/build.sh
+++ recipes/pplacer/build.sh
@@ -3,5 +3,11 @@
 mkdir -p $PREFIX/bin
 
 cd $SRC_DIR
+if [ "$(uname)" == "Darwin" ]; then
+    set -x
+    otool -L ./guppy
+    otool -L ./pplacer
+    otool -L ./rppr
+fi
 cp guppy pplacer rppr $PREFIX/bin
 chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
