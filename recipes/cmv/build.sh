1add6bc09cba694c3bb7aa1959a8763073405f74
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-02-18 21:46:01 +0100

    Switched requested ghc variant

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export LIBRARY_PATH="${PREFIX}/lib:/lib64/"
 export LD_LIBRARY_PATH="${PREFIX}/lib:/lib64/"
+export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
+export CPPFLAGS="-I${PREFIX}/include"
 
-stack setup
+stack setup -ghc-build 'standard'
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
