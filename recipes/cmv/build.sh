42cd104e8c8a1362ad354b5875d6a1476d7f17b2
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-02-18 22:23:59 +0100

    Updated - 8

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,10 +3,11 @@ export LIBRARY_PATH="${PREFIX}/lib:/lib64/"
 export LD_LIBRARY_PATH="${PREFIX}/lib:/lib64/"
 export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
 export CPPFLAGS="-I${PREFIX}/include"
+export GCCPATH=`which gcc`
 
-stack setup --ghc-build 'standard'
+stack setup
 stack update
-stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+stack install --with-gcc="$GCCPATH"--extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
 
