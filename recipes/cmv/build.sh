0e72397e9734131974986ba53879c9a2d8dbb06f
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-02-18 22:02:31 +0100

    Updated linker flags - 7

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib:/lib64/"
 export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
 export CPPFLAGS="-I${PREFIX}/include"
 
-stack setup -ghc-build 'standard'
+stack setup --ghc-build 'standard'
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
 #cleanup
