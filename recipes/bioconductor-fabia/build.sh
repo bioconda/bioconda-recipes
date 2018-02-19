13e4a5bb55816c380bb975af522f54b6aa289728
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-19 12:48:44 +0100

    bioconductor-fabia: cleanup

--- recipes/bioconductor-fabia/build.sh
+++ recipes/bioconductor-fabia/build.sh
@@ -3,5 +3,11 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
+if [[ "$(uname)" == "Darwin" ]]; then
+    export CC=clang
+    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
+fi
 autoconf
+
 $R CMD INSTALL --build .
