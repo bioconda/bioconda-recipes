6073722940e4ebcf5779d858091c1512f36189f8
pbordron <philippe.bordron@sb-roscoff.fr>
2018-02-19 14:44:53 +0100

    Update to 2.0Beta8c

--- recipes/stacks/2.0/build.sh
+++ recipes/stacks/2.0/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Using --libdir doesn't work.
-export LDFLAGS="-L${CONDA_PREFIX}/lib" # for both stacks and included htslib dependency
-export CPATH=${CONDA_PREFIX}/include         # for included htslib dependency
-
 ./configure --prefix=$PREFIX --enable-bam
-
-# Force included htslib library to use conda compiler toolsuite
-sed -i "s|^CC *.*|CC = $CC|" htslib/Makefile
-sed -i "s|^AR *.*|AR = $AR|" htslib/Makefile
-sed -i "s|^RANLIB *.*|RANLIB = $RANLIB|" htslib/Makefile
-
 make
 make install
