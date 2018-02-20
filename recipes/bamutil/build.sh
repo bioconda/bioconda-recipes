1d4b5c7278993ed1a2c630e52b16cd59286fd714
Karl Nordström <karl.nordstrom@gmail.com>
2018-02-20 16:03:13 +0100

    Update build.sh
    the makefile to change is located in libStatGen

--- recipes/bamutil/build.sh
+++ recipes/bamutil/build.sh
@@ -5,7 +5,7 @@ curl -o libStatGen-v$VERSION.tar.gz -L https://github.com/statgen/libStatGen/arc
 openssl sha256 libStatGen-v$VERSION.tar.gz | grep 70a504c5cc4838c6ac96cdd010644454615cc907df4e3794c999baf958fa734b
 tar -xzvpf libStatGen-v$VERSION.tar.gz
 # remove profile flag to avoid "ld: cannot find gcrt1.o: No such file or directory"
-sed -i.bak '/OPTFLAG_PROFILE?=/ s/-pg//' Makefiles/Makefile.include
+sed -i.bak '/OPTFLAG_PROFILE?=/ s/-pg//' ./libStatGen-$VERSION/Makefiles/Makefile.include
 make LIB_PATH_GENERAL=./libStatGen-$VERSION
 make install INSTALLDIR=$PREFIX LIB_PATH_GENERAL=./libStatGen-$VERSION
 mkdir -p $PREFIX/bin/
