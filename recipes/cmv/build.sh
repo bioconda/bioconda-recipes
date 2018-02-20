ef1c50c4d519eef2fe256af9f93272205b2e95f6
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-20 19:21:33 +0100

    Simplified recipe 2

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
 export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
+export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-export GCCPATH=$PREFIX/bin/gcc
 
 stack setup --local-bin-path ${PREFIX}/bin
 stack update
