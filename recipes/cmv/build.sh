1fa33c8559771cc8bcadf1e956ab04e8312c0ceb
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-20 17:44:47 +0100

    Switched to /usr/lib64

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-export LIBRARY_PATH="${PREFIX}/lib:/lib:/lib64"
-export LD_LIBRARY_PATH="${PREFIX}/lib:/lib:/lib64"
+export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
 export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
 export CPPFLAGS="-I${PREFIX}/include"
 export GCCPATH=$PREFIX/bin/gcc
 
-stack setup --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
+stack setup --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib:/usr/lib:/usr/lib64 --local-bin-path ${PREFIX}/bin
 stack update
-stack install --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin --ghc-options="-threaded"
+stack install --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib:/usr/lib:/usr/lib64 --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
 
