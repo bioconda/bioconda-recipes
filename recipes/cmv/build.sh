b1251d10394b7c92cbfe50aa873a801b94e9fada
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-02-20 10:18:10 +0100

    Passing threaded option to ghc

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,11 +3,11 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
 export CPPFLAGS="-I${PREFIX}/include"
-export GCCPATH=`which gcc`
+export GCCPATH=$PREFIX/bin/gcc
 
 stack setup
 stack update
-stack install --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
+stack install --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin --ghc-options="-threaded"
 #cleanup
 rm -r .stack-work
 
