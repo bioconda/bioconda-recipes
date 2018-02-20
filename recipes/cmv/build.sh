d7664f897c9d9e24d59a0d5fcae71d52d5e23f9e
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-20 18:43:16 +0100

    Simplifying recipe

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -5,9 +5,9 @@ export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
 export CPPFLAGS="-I${PREFIX}/include"
 export GCCPATH=$PREFIX/bin/gcc
 
-stack setup --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib:/usr/lib:/usr/lib64 --local-bin-path ${PREFIX}/bin
+stack setup --local-bin-path ${PREFIX}/bin
 stack update
-stack install --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib:/usr/lib:/usr/lib64 --local-bin-path ${PREFIX}/bin
+stack install --local-bin-path ${PREFIX}/bin
 #cleanup
 rm -r .stack-work
 
