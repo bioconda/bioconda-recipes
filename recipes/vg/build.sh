74c740d08977a0c494eeaf99939b6a446581110b
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-20 10:00:50 +0100

    vg: show config.log

--- recipes/vg/build.sh
+++ recipes/vg/build.sh
@@ -6,7 +6,7 @@ export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 source source_me.sh
-make static
+make static || (cat config.log ; exit 1)
 
 mkdir -p $PREFIX/bin
 cp bin/vg $PREFIX/bin
