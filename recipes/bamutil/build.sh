8c8bf26e1a3900f533d5cfb9f4c8c9147e9a1e5f
Karl Nordström <karl.nordstrom@gmail.com>
2018-02-20 12:54:34 +0100

    test

--- recipes/bamutil/build.sh
+++ recipes/bamutil/build.sh
@@ -3,7 +3,7 @@
 VERSION=1.0.14
 curl -o libStatGen-v$VERSION.tar.gz -L https://github.com/statgen/libStatGen/archive/v$VERSION.tar.gz
 tar -xzvpf libStatGen-v$VERSION.tar.gz
-sed -i -e 's/-pg//' libStatGen-1.0.14/Makefiles/Makefile.include
+#sed -i -e 's/-pg//' libStatGen-1.0.14/Makefiles/Makefile.include
 make LIB_PATH_GENERAL=./libStatGen-$VERSION
 make install INSTALLDIR=$PREFIX LIB_PATH_GENERAL=./libStatGen-$VERSION
 mkdir -p $PREFIX/bin/
