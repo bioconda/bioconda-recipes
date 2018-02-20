13d17aeb32a7af4cb0e155207e71591435520363
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-20 17:03:11 +0100

    Fixed extra lib path

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -5,7 +5,7 @@ export LDFLAGS="-L${PREFIX}/lib -pthread -lpthread"
 export CPPFLAGS="-I${PREFIX}/include"
 export GCCPATH=$PREFIX/bin/gcc
 
-stack setup --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs=[${PREFIX}/lib,/lib,/lib64] --local-bin-path ${PREFIX}/bin
+stack setup --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
 stack update
 stack install --with-gcc="$GCCPATH" --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin --ghc-options="-threaded"
 #cleanup
