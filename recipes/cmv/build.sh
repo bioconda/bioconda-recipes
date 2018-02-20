b751f67af800c7717ce82d681927dd381897d8d6
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-02-20 21:59:35 +0100

    New attempt for mac build

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,10 +3,19 @@ export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
 export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
-
-stack setup --local-bin-path ${PREFIX}/bin
-stack update
-stack install --local-bin-path ${PREFIX}/bin
+if [ `uname` == Darwin ]
+then
+    export DYLD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+    export STACK_ROOT="${SRC_DIR}/s"
+    stack setup --local-bin-path ${SRC_DIR}/s
+    stack update
+    stack install --local-bin-path ${PREFIX}/bin
+    rm -r "${SRC_DIR}/s"
+else
+  stack setup --local-bin-path ${PREFIX}/bin
+  stack update
+  stack install --local-bin-path ${PREFIX}/bin
+fi
 #cleanup
 rm -r .stack-work
 
