a494cf0c333a882385b42b0724a78b3195e47b18
Karl Nordström <karl.nordstrom@gmail.com>
2018-02-20 11:58:12 +0100

    bamutil: - disabled profiling in makefile as this stopped compilation

--- recipes/bamutil/build.sh
+++ recipes/bamutil/build.sh
@@ -3,6 +3,7 @@
 VERSION=1.0.14
 wget -O libStatGen-v$VERSION.tar.gz https://github.com/statgen/libStatGen/archive/v$VERSION.tar.gz
 tar -xzvpf libStatGen-v$VERSION.tar.gz
+sed -i -e 's/-pg//' libStatGen-1.0.14/Makefiles/Makefile.include
 make LIB_PATH_GENERAL=./libStatGen-$VERSION
 make install INSTALLDIR=$PREFIX LIB_PATH_GENERAL=./libStatGen-$VERSION
 mkdir -p $PREFIX/bin/
