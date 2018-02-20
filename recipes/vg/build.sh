f4ed1302527f1cad0f37513fbd6587040adc52b1
Marcel Bargull <marcel.bargull@udo.edu>
2018-02-20 10:22:32 +0100

    vg: moar logs

--- recipes/vg/build.sh
+++ recipes/vg/build.sh
@@ -6,7 +6,7 @@ export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 source source_me.sh
-make static || (cat config.log ; exit 1)
+make static || (find . -name config.log -exec cat {} \; ; exit 1)
 
 mkdir -p $PREFIX/bin
 cp bin/vg $PREFIX/bin
