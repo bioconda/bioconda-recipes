2344bea59edbb61d001c88cba66d5d4099280f4c
james0032 <chchung.drexel@gmail.com>
2018-02-22 14:18:29 -0500

    specify OpenCL library and header paths

--- recipes/cas-offinder/build.sh
+++ recipes/cas-offinder/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -Wno-dev -G "Unix Makefiles"  
+cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=$PREFIX/lib/libOpenCL.so.1 -DOPENCL_INCLUDE_DIR=$PREFIX/include
 make
 
 mkdir -p $PREFIX/bin
