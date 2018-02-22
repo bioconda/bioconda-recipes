bd90c9a0e4bdd22839af0c617b265f69be7b962c
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-22 16:49:48 +0100

    Added libcxx clangdev llvmdev

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,11 +3,10 @@
 if [ `uname` == Darwin ]
 then
     export LD_LIBRARY_PATH="${PREFIX}/lib"
-    export DYLD_LIBRARY_PATH="${PREFIX}/lib"
     export STACK_ROOT="${SRC_DIR}/s"
-    stack setup --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${SRC_DIR}/s
+    stack setup --local-bin-path ${SRC_DIR}/s
     stack update
-    stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
+    stack install --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
 else
   export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
