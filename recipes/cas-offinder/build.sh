12036bed372f6f8b97cbb2ebbbd0a4aea52fe3fc
james0032 <chchung.drexel@gmail.com>
2018-02-22 16:00:07 -0500

    maybe a curved bracket away

--- recipes/cas-offinder/build.sh
+++ recipes/cas-offinder/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=$PREFIX/lib/libOpenCL.so.1 -DOPENCL_INCLUDE_DIR=$PREFIX/include
+cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL.so.1 -DOPENCL_INCLUDE_DIR=${PREFIX}/include
 make
-
 mkdir -p $PREFIX/bin
 cp cas-offinder $PREFIX/bin
