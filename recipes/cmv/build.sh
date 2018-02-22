c55aad5bbc0cf0c5d4bc88416ee5fc053af752ab
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-22 14:10:52 +0100

    Restricted library path

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -3,11 +3,11 @@
 if [ `uname` == Darwin ]
 then
     export LD_LIBRARY_PATH="${PREFIX}/lib"
-    #export DYLD_LIBRARY_PATH="/System/Library/Frameworks/ImageIO.framework/Versions/A/ImageIO:${PREFIX}/lib:/usr/lib:/usr/lib64"
+    export DYLD_LIBRARY_PATH="${PREFIX}/lib"
     export STACK_ROOT="${SRC_DIR}/s"
-    stack setup --extra-include-dirs ${PREFIX}/include --local-bin-path ${SRC_DIR}/s
+    stack setup --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${SRC_DIR}/s
     stack update
-    stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+    stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
 else
   export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
