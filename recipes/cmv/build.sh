aed259c2e2d4c3ce8a6a3c5007e0962d9f90ba90
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-02-22 09:29:11 +0100

    Updated libray paths in build sh

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
+
 if [ `uname` == Darwin ]
 then
+    export LD_LIBRARY_PATH="${PREFIX}/lib"
     #export DYLD_LIBRARY_PATH="/System/Library/Frameworks/ImageIO.framework/Versions/A/ImageIO:${PREFIX}/lib:/usr/lib:/usr/lib64"
     export STACK_ROOT="${SRC_DIR}/s"
     stack setup --extra-include-dirs ${PREFIX}/include --local-bin-path ${SRC_DIR}/s
@@ -12,6 +10,10 @@ then
     stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
 else
+  export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+  export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+  export LDFLAGS="-L${PREFIX}/lib"
+  export CPPFLAGS="-I${PREFIX}/include"
   stack setup --local-bin-path ${PREFIX}/bin
   stack update
   stack install --local-bin-path ${PREFIX}/bin
