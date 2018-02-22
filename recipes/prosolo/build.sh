14f018ea405f8115572339d88bad3f43f19430ec
David Laehnemann <david.laehnemann@hhu.de>
2018-02-22 19:45:35 +0100

    workaround for conda-build bug
    workaround for the bug that seems to be behind the error that got this
    and other rust recipes blacklisted

--- recipes/prosolo/build.sh
+++ recipes/prosolo/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -euo
 
+unset REQUESTS_CA_BUNDLE ## TODO: remove these `unset` lines, once the following issue in `conda-build` is resolved:
+unset SSL_CERT_FILE      ##       <https://github.com/conda/conda-build/issues/2255>
+
 # build statically linked binary with Rust
 C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib:/usr/lib64 cargo install --root $PREFIX --verbose --debug
