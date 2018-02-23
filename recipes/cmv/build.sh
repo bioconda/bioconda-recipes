42b1ac18f20db124d9781b9768390443080dbf7e
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-23 16:50:31 +0100

    Formatting

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -2,15 +2,15 @@
 
 if [ `uname` == Darwin ]
 then
-    export LD_LIBRARY_PATH="${PREFIX}/lib"
-    export STACK_ROOT="${SRC_DIR}/s"
-    export LDFLAGS="-L${PREFIX}/lib"
-    export CPPFLAGS="-I${PREFIX}/include"
-    echo "${PREFIX}"
-    echo "${SRC_DIR}/s"
-    stack setup --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${SRC_DIR}/s"
-    stack install --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${PREFIX}/bin"
-    rm -r "${SRC_DIR}/s"
+  export LD_LIBRARY_PATH="${PREFIX}/lib"
+  export STACK_ROOT="${SRC_DIR}/s"
+  export LDFLAGS="-L${PREFIX}/lib"
+  export CPPFLAGS="-I${PREFIX}/include"
+  echo "${PREFIX}"
+  echo "${SRC_DIR}/s"
+  stack setup --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${SRC_DIR}/s"
+  stack install --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${PREFIX}/bin"
+  rm -r "${SRC_DIR}/s"
 else
   export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
   export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
