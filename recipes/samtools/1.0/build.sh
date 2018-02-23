672f523b5a3e343a5552fa2a6c80963413eccdae
rvalieris <rvalieris@users.noreply.github.com>
2018-02-23 13:56:56 -0300

    more flags

--- recipes/samtools/1.0/build.sh
+++ recipes/samtools/1.0/build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+set -e
+
+# remove hard-coded *flags
+sed -i.bak 's/^CPPFLAGS =$//g' Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' Makefile
+sed -i.bak 's/^CPPFLAGS =$//g' htslib-$PKG_VERSION/Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' htslib-$PKG_VERSION/Makefile
+
 export CPPFLAGS="-I$PREFIX/include"
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
