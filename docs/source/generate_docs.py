b547f82cc225254f6f75f1ba8cbee414aff0b991
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-02-23 16:08:46 -0700

    Keep package page templates as separate files

--- docs/source/generate_docs.py
+++ docs/source/generate_docs.py
@@ -2,15 +2,26 @@ import os
 import os.path as op
 from collections import defaultdict
 from jinja2 import Template
+from jinja2.sandbox import SandboxedEnvironment
 from bioconda_utils import utils
+from sphinx.util import logging as sphinx_logging
+from sphinx.util import status_iterator
+from sphinx.util.template import SphinxRenderer
+from sphinx.util.rst import escape as rst_escape
+from sphinx.jinja2glue import BuiltinTemplateLoader
+from distutils.version import LooseVersion
+
+try:
+    logger = sphinx_logging.getLogger(__name__)
+except AttributeError:  # not running within sphinx
+    import logging
+    logger = logging.getLogger(__name__)
 
 try:
     from conda_build.metadata import MetaData
-except Exception as e:
-    import traceback
-    traceback.print_exc()
-    raise e
-from distutils.version import LooseVersion
+except Exception:
+    logging.exception("Failed to import MetaData")
+    raise
 
 
 BASE_DIR = op.dirname(op.abspath(__file__))
@@ -18,95 +29,9 @@ RECIPE_DIR = op.join(op.dirname(BASE_DIR), 'bioconda-recipes', 'recipes')
 OUTPUT_DIR = op.join(BASE_DIR, 'recipes')
 
 # jinja2 template for the DataTable of recipes
-RECIPES_TEMPLATE = u"""\
-.. _recipes:
+RECIPES_TEMPLATE = "recipes.rst_t"
+README_TEMPLATE = "readme.rst_t"
 
-Available packages
-==================
-
-.. toctree::
-   :hidden:
-   :maxdepth: 1
-   :glob:
-
-   recipes/*/*
-
-.. raw:: html
-
-    <table id="recipestable" class="display" cellspacing="0" width="100%">
-    <thead>
-        <tr>
-        {% for key in keys %}
-        <th>{{ key }}</th>
-        {% endfor %}
-        </tr>
-    </thead>
-    <tbody>
-    {% for r in recipes %}
-    <tr>
-        {% for k in keys %}
-        <td>{{ r[k] }}</td>
-        {% endfor %}
-    </tr>
-    {% endfor %}
-    </tbody>
-    <tfoot></tfoot>
-    </table>
-"""
-
-
-README_TEMPLATE = u"""\
-.. _`{title}`:
-
-{title}
-{title_underline}
-
-|downloads|
-
-{summary}
-
-======== ===========
-Home     {home}
-Versions {versions}
-License  {license}
-Recipe   {recipe}
-======== ===========
-
-Installation
-------------
-
-.. highlight: bash
-
-With an activated Bioconda channel (see :ref:`set-up-channels`), install with::
-
-   conda install {title}
-
-and update with::
-
-   conda update {title}
-
-{notes}
-
-|docker|
-
-A Docker container is available at https://quay.io/repository/biocontainers/{title}.
-
-Link to this page
------------------
-
-Render an |badge| badge with the following Markdown::
-
-   [![install with bioconda](https://img.shields.io/badge/install%20with-bioconda-brightgreen.svg?style=flat-square)](http://bioconda.github.io/recipes/{title}/README.html)
-
-.. |badge| image:: https://img.shields.io/badge/install%20with-bioconda-brightgreen.svg?style=flat-square
-           :target: http://bioconda.github.io/recipes/{title}/README.html
-.. |downloads| image:: https://anaconda.org/bioconda/{title}/badges/downloads.svg
-               :target: https://anaconda.org/bioconda/{title}
-.. |docker| image:: https://quay.io/repository/biocontainers/{title}/status
-                :target: https://quay.io/repository/biocontainers/{title}
-
-
-"""
 
 def parse_pkgname(p):
     p = p.replace('.tar.bz2', '')
@@ -117,14 +42,35 @@ def parse_pkgname(p):
     return dict(name=name, version=version, build_string=build_string)
 
 
-def setup(*args):
+class Renderer(object):
+    def __init__(self, app):
+        template_loader = BuiltinTemplateLoader()
+        template_loader.init(app.builder)
+        template_env = SandboxedEnvironment(loader=template_loader)
+        template_env.filters['escape'] = rst_escape
+        self.env = template_env
+        self.templates = {}
+
+    def render(self, template_name, context):
+        try:
+            template = self.templates[template_name]
+        except KeyError:
+            template = self.env.get_template(template_name)
+            self.templates[template_name] = template
+
+        return template.render(**context)
+
+
+def generate_recipes(app):
     """
     Go through every folder in the `bioconda-recipes/recipes` dir
     and generate a README.rst file.
     """
-    print('Generating package READMEs...')
 
+    renderer = Renderer(app)
+    render = renderer.render
 
+    logger.info('Loading packages...')
     repodata = defaultdict(lambda: defaultdict(list))
     for platform in ['linux', 'osx']:
         for pkg in utils.get_channel_packages(channel='bioconda', platform=platform):
@@ -141,7 +87,9 @@ def setup(*args):
     summaries = []
     recipes = []
 
-    for folder in os.listdir(RECIPE_DIR):
+    recipe_dirs = os.listdir(RECIPE_DIR)
+    for folder in status_iterator(recipe_dirs, 'Generating package READMEs...',
+                                  "purple", len(recipe_dirs), app.verbosity):
         # Subfolders correspond to different versions
         versions = []
         for sf in os.listdir(op.join(RECIPE_DIR, folder)):
@@ -151,11 +99,12 @@ def setup(*args):
             try:
                 LooseVersion(sf)
             except ValueError:
-                print("'{}' does not look like a proper version!".format(sf))
+                logger.error("'{}' does not look like a proper version!"
+                             "".format(sf))
                 continue
             versions.append(sf)
-        #versions.sort(key=LooseVersion, reverse=True)
-        # Read the meta.yaml file
+
+        # Read the meta.yaml file(s)
         recipe = op.join(RECIPE_DIR, folder, "meta.yaml")
         if op.exists(recipe):
             metadata = MetaData(recipe)
@@ -208,7 +157,8 @@ def setup(*args):
             t['Version'] = version
             recipes.append(t)
 
-        readme = README_TEMPLATE.format(**template_options)
+        readme = render(README_TEMPLATE, template_options)
+
         # Write to file
         try:
             os.makedirs(op.join(OUTPUT_DIR, folder))  # exist_ok=True on Python 3
@@ -226,13 +176,12 @@ def setup(*args):
             ofh.write(readme.encode('utf-8'))
 
     # render the recipes datatable page
-    t = Template(RECIPES_TEMPLATE)
-    recipes_contents = t.render(
-        recipes=recipes,
+    recipes_contents = render(RECIPES_TEMPLATE, {
+        'recipes': recipes,
 
         # order of columns in the table; must be keys in template_options
-        keys=['Package', 'Version', 'License', 'Linux', 'OSX']
-    )
+        'keys': ['Package', 'Version', 'License', 'Linux', 'OSX']
+    })
     recipes_rst = 'source/recipes.rst'
     if not (
         os.path.exists(recipes_rst)
@@ -241,5 +190,6 @@ def setup(*args):
         with open(recipes_rst, 'w') as fout:
             fout.write(recipes_contents)
 
-if __name__ == '__main__':
-    setup()
+
+def setup(app):
+    app.connect('builder-inited', generate_recipes)
