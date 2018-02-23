1663116c2c38b727d4dacfd61c313e787e087da9
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-23 16:21:53 +0100

    Added linker flags

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -4,8 +4,9 @@ if [ `uname` == Darwin ]
 then
     export LD_LIBRARY_PATH="${PREFIX}/lib"
     export STACK_ROOT="${SRC_DIR}/s"
+    export LDFLAGS="-L${PREFIX}/lib"
+    export CPPFLAGS="-I${PREFIX}/include"
     stack setup --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include --local-bin-path ${SRC_DIR}/s
-    stack update
     stack install --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
     rm -r "${SRC_DIR}/s"
 else
