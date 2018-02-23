babf959f4ef9ba78c5c8913ee147ae09026e668b
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-23 16:33:25 +0100

    Debugging output

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -6,8 +6,10 @@ then
     export STACK_ROOT="${SRC_DIR}/s"
     export LDFLAGS="-L${PREFIX}/lib"
     export CPPFLAGS="-I${PREFIX}/include"
-    stack setup --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include --local-bin-path ${SRC_DIR}/s
-    stack install --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
+    echo "${PREFIX}"
+    echo "${SRC_DIR}/s"
+    stack setup --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${SRC_DIR}/s"
+    stack install --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${PREFIX}/bin"
     rm -r "${SRC_DIR}/s"
 else
   export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
