783723ef30d4d2fb16b2856b6fc91117e2056697
james0032 <chchung.drexel@gmail.com>
2018-02-23 20:38:13 -0500

    split cmake by OSTYPE

--- recipes/cas-offinder/build.sh
+++ recipes/cas-offinder/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL.so.1 -DOPENCL_INCLUDE_DIR=${PREFIX}/include
+if [[ $OSTYPE == "linux-gnu" ]]; then
+	cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL.so -DOPENCL_INCLUDE_DIR=${PREFIX}/include
+elif [[ $OSTYPE == "darwin"* ]]; then
+	cmake -Wno-dev -G "Unix Makefiles"
+fi
 make
 mkdir -p $PREFIX/bin
 cp cas-offinder $PREFIX/bin
