dfe209252d7e5458d7f44ce1668261df4cb170ed
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-02-26 12:28:20 +0100

    Added mkdir for stack bin

--- recipes/cmv/build.sh
+++ recipes/cmv/build.sh
@@ -8,14 +8,15 @@ then
   export CPPFLAGS="-I${PREFIX}/include"
   echo "${PREFIX}"
   echo "${SRC_DIR}/s"
+  mkdir "${SRC_DIR}/s"
   export LOCALBIN="${SRC_DIR}/s/bin"
   mkdir "$LOCALBIN"
   export PATH="$LOCALBIN:$PATH"
   stack setup --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "$LOCALBIN"
   stack path
   stack update
-  stack install --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "$LOCALBIN"
-  cp -p $LOCALBIN/* "${PREFIX}/bin"
+  stack install --extra-lib-dirs "${PREFIX}/lib" --extra-include-dirs "${PREFIX}/include" --local-bin-path "${PREFIX}/bin"
+  #cp -p $LOCALBIN/* "${PREFIX}/bin"
   rm -r "${SRC_DIR}/s" 
 else
   export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
