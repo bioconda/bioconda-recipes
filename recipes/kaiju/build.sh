1771f5ea7627ac8133c65fd98ffc04f251978047
Dan Fornika <dfornika@gmail.com>
2018-02-26 21:20:17 -0800

    Update build.sh

--- recipes/kaiju/build.sh
+++ recipes/kaiju/build.sh
@@ -9,17 +9,15 @@ make
 cd $SRC_DIR/bin/
 cp addTaxonNames $PREFIX/bin
 cp convertNR $PREFIX/bin
+cp gbk2faa.pl $PREFIX/bin
 cp kaiju $PREFIX/bin
 cp kaiju2krona $PREFIX/bin
 cp kaijup $PREFIX/bin
 cp kaijuReport $PREFIX/bin
 cp kaijux $PREFIX/bin
+cp makeDB.sh $PREFIX/bin
 cp mergeOutputs $PREFIX/bin
 cp mkbwt $PREFIX/bin
 cp mkfmi $PREFIX/bin
-
-cd $SRC_DIR/util
 cp convert_mar_to_kaiju.py $PREFIX/bin
-cp gbk2faa.pl $PREFIX/bin
-cp makeDB.sh $PREFIX/bin
 cp taxonlist.tsv $PREFIX/bin
