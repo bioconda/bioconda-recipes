d408dd5cd26c37ae840becf7cb1223b43ef48d44
Philippe Bordron <pbordron@users.noreply.github.com>
2018-02-28 21:44:24 +0100

    Stack 2.0: fix missing depencies (#8027)

--- recipes/stacks/2.0/build.sh
+++ recipes/stacks/2.0/build.sh
@@ -5,3 +5,5 @@ export CXXFLAGS="${CXXFLAGS} -std=c++11"
 ./configure --prefix=$PREFIX --enable-bam
 make
 make install
+# copy missing scripts
+cp -rp scripts/{convert_stacks.pl,extract_interpop_chars.pl} $PREFIX/bin/
