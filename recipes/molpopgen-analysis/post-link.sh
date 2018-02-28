d0735673febc413e05011d1c9bf4fcf7fa56f46d
pbordron <philippe.bordron@sb-roscoff.fr>
2018-02-28 16:11:23 +0100

    add message at postprocess and activate steps

--- recipes/molpopgen-analysis/post-link.sh
+++ recipes/molpopgen-analysis/post-link.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-CXXFLAGS="-O3 -DNDEBUG" CPPFLAGS="-I$PREFIX/include $CPPFLAGS" LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib $LDFLAGS" ./configure --prefix=$PREFIX
-make
-make install
-
-mkdir -p $PREFIX/etc/conda/activate.d/
-echo "#!/bin/bash
-echo '
+cat <<EOF >> ${PREFIX}/.messages.txt
 /!\ Warning/!\\
 
 DO NOT USE THIS PACKAGE IN NGS STUDIES.
@@ -15,8 +9,7 @@ This is a RETIRED package for the (pre-NGS-era) analysis of population-genetic d
 These programs were written with high-quality data in mind (e.g. double-pass Sanger sequencing of PCR amplicons).
 Unless you work with Sanger data, RESULTS WILL BE WRONG.
 
-Please check https://github.com/molpopgen/analysis for details.
+Please check https://github.com/molpopgen/analysis for more details.
 
 /!\ Warning/!\\
-
-'" > ${PREFIX}/etc/conda/activate.d/warning.sh
+EOF
