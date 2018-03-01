2d1b3909627fdd711cf82e92e11a2a9e8490e358
Philippe Bordron <pbordron@users.noreply.github.com>
2018-03-01 09:44:59 +0100

    Update post-link.sh

--- recipes/molpopgen-analysis/post-link.sh
+++ recipes/molpopgen-analysis/post-link.sh
@@ -9,7 +9,7 @@ Molpopgen-analysis is a RETIRED package for the (pre-NGS-era) analysis of popula
 These programs were written with high-quality data in mind (e.g. double-pass Sanger sequencing of PCR amplicons).
 Unless you work with Sanger data, RESULTS WILL BE WRONG.
 
-Please check https://github.com/molpopgen/analysis for more details.
+Please check https://github.com/molpopgen/analysis for details.
 
 /!\ Warning/!\\
 EOF
