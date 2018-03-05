a78d0d9daff1e057cf36c5b6e540de45f21c7a31
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-03-05 10:15:34 +0100

    debugging

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
 # build statically linked binary with Rust
 C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --root $PREFIX
