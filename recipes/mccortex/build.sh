a0ee1d3628727c603821c7b8d1204ebeac9b0db2
Karel Břinda <karel.brinda@gmail.com>
2018-03-05 11:24:42 -0500

    Add mccortex

---
+++ recipes/mccortex/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+##strictly use anaconda build environment
+#CXX=${PREFIX}/bin/g++
+
+#to fix problems with zlib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
+export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+
+git clone --recursive https://github.com/mcveanlab/mccortex
+make all VERBOSE=1
+mkdir -p ${PREFIX}/bin
+cp bin/mccortex31 ${PREFIX}/bin
+
