0e3db4704abcda5f00bafc02e928535d5ef541e8
Karel Břinda <karel.brinda@gmail.com>
2018-03-05 12:02:34 -0500

    Add other k's

--- recipes/mccortex/build.sh
+++ recipes/mccortex/build.sh
@@ -11,7 +11,15 @@ export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
 git clone --recursive https://github.com/mcveanlab/mccortex
 cd mccortex
-make VERBOSE=1
+
+git checkout v0.2
+
+make all MAXK=31
+make all MAXK=63
+make all MAXK=127
+
 mkdir -p ${PREFIX}/bin
 cp bin/mccortex31 ${PREFIX}/bin
+cp bin/mccortex63 ${PREFIX}/bin
+cp bin/mccortex127 ${PREFIX}/bin
 
