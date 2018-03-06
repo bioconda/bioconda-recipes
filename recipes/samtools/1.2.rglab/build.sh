8ebdf37c8006fb1d1472028fd4a3fbb16f914dbc
Karl Nordström <karl.nordstrom@gmail.com>
2018-03-06 19:36:11 +0100

    - switched to using the v1.7 build script instead. Still required addition of includes and correction of ncurses

--- recipes/samtools/1.2.rglab/build.sh
+++ recipes/samtools/1.2.rglab/build.sh
@@ -1,6 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+sed -i.bak 's/^CPPFLAGS =$//g' Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' Makefile
+sed -i.bak 's|^INCLUDES=.*|INCLUDES = -I. -I$(HTSDIR) -I'$PREFIX'/include|' Makefile
 sed -i.bak -e 's/-lcurses/-lncurses/' Makefile
-sed -i.bak -e "s|CFLAGS=\s*-g\s*-Wall\s*-O2\s*|CFLAGS= -g -Wall -O2 -I$NCURSES_INCLUDE_PATH/ncurses/ -I$NCURSES_INCLUDE_PATH -L$NCURSES_LIB_PATH -I$ZLIB_INCLUDE_PATH -L$ZLIB_LIB_PATH|g" Makefile htslib-1.2.1/Makefile
+
+sed -i.bak 's/^CPPFLAGS =$//g' htslib-1.2.1/Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' htslib-1.2.1/Makefile
+
+# varfilter.py in install fails because we don't install Python
+sed -i.bak 's#misc/varfilter.py##g' Makefile
+
+# Remove rdynamic which can cause build issues on OSX
+# https://sourceforge.net/p/samtools/mailman/message/34699333/
+sed -i.bak 's/ -rdynamic//g' Makefile
+sed -i.bak 's/ -rdynamic//g' htslib-1.2.1/configure
+
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+
+cd htslib*
+./configure --prefix=$PREFIX --enable-libcurl CFLAGS="-I$PREFIX/include" LDFLAGS="$LDFLAGS"
 make
-mkdir -p $PREFIX/bin
-mv samtools $PREFIX/bin
+cd ..
+# Problem with ncurses from default channel we now get in bioconda so skip tview
+# https://github.com/samtools/samtools/issues/577
+if [[ "$(uname)" == "Linux" ]] ; then
+    export LDFLAGS="$LDFLAGS -Wl,--add-needed"
+fi
+
+#./configure --prefix=$PREFIX --enable-libcurl --enable-plugins --with-plugin-path=$PWD/htslib-$PKG_VERSION LDFLAGS="$LDFLAGS" || (cat config.log ; false)
+make install prefix=$PREFIX
+#LIBS+=-lcrypto LIBS+=-lcurl
