7d997041194afbff9eb9a8f11b845b5e5659d1a6
Karl Nordström <karl.nordstrom@gmail.com>
2018-03-07 11:59:42 +0100

    rsem: - adding workarounds for samtools

--- recipes/rsem/1.2.21/build.sh
+++ recipes/rsem/1.2.21/build.sh
@@ -50,7 +50,7 @@ export CFLAGS="-I${PREFIX}/include"
 ### Prepare Samtools/Htslib 
 ########################################################
 
-sed -i.bak 's/-lcurses/lncurses/g' sam/Makefile
+sed -i.bak 's/-lcurses/-lncurses/g' sam/Makefile
 
 #sed -i.bak 's/^CPPFLAGS =$//g' samtools-1.3/Makefile
 #sed -i.bak 's/^LDFLAGS  =$//g' samtools-1.3/Makefile
@@ -65,6 +65,11 @@ sed -i.bak 's/-lcurses/lncurses/g' sam/Makefile
 # https://sourceforge.net/p/samtools/mailman/message/34699333/
 #sed -i.bak 's/ -rdynamic//g' samtools-1.3/Makefile
 #sed -i.bak 's/ -rdynamic//g' samtools-1.3/htslib-1.3/configure
+
+if [[ "$(uname)" == "Linux" ]] ; then
+    export LDFLAGS="$LDFLAGS -Wl,--add-needed"
+fi
+
 
 ########################################################
 ### Build rsem
