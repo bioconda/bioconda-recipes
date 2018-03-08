90d40ae754dad669b6d7a4ddc9af1c7b1a4748ed
Stefan Hammer <stefan.hammer@uni-leipzig.de>
2018-03-08 13:19:57 +0100

    Adds inital recipe for the RNAblueprint python interface. The program binary and the perl interface are not included.

---
+++ recipes/rnablueprint/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BOOST_ROOT="${CONDA_PREFIX}"
+
+libtoolize --copy --force
+sh autogen.sh
+./configure \
+  --prefix=$PREFIX \
+  --disable-perl \
+  --disable-doxygen-doc \
+  --disable-program \
+  --disable-tests 
+
+make -j
+make install
