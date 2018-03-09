95a2a177067aad86f00c70a94056cb68443a12a1
Karl Nordström <karl.nordstrom@gmail.com>
2018-03-09 16:20:10 +0100

    ucsc tools: - adding legacy version 308 for bedgraphtobigwig, genepredtobed and gtftogenepred

--- recipes/ucsc-gtftogenepred/308/build.sh
+++ recipes/ucsc-gtftogenepred/308/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir -p "$PREFIX/bin"
-
 export MACHTYPE=$(uname -m)
 export BINDIR=$(pwd)/bin
 mkdir -p "$BINDIR"
 (cd src/lib && make)
+(cd src/htslib && make)
 (cd src/jkOwnLib && make)
 (cd src/hg/lib && make)
-(cd src/utils/bedGraphToBigWig && make)
-cp $BINDIR/bedGraphToBigWig "$PREFIX/bin"
-chmod +x "$PREFIX/bin/bedGraphToBigWig"
+(cd src/hg/utils/gtfToGenePred && make)
+cp $BINDIR/gtfToGenePred "$PREFIX/bin"
+chmod +x "$PREFIX/bin/gtfToGenePred"
