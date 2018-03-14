acb75ab043f4bd03c0424a767f4436edfdc96f1e
Cyril Monjeaud <Cyril.Monjeaud@irisa.fr>
2018-03-14 14:04:01 +0000

    delete export encs

--- recipes/feelnc/build.sh
+++ recipes/feelnc/build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/utils
-mkdir -p ${PREFIX}/scripts
-mkdir -p ${PREFIX}/lib
 
+sed -i.bak 's/#!\/usr\/bin\/perl -w/#!\/usr\/bin\/env perl/' scripts/*
+
+cp scripts/* ${PREFIX}/bin/
+cp utils/FEELnc_pipeline.sh ${PREFIX}/bin/
 cp utils/* ${PREFIX}/utils/
-cp scripts/* ${PREFIX}/scripts/
-cp -r lib/* ${PREFIX}/lib/
 
-sed -i.bak 's/#!\/usr\/bin\/perl -w/#!\/usr\/bin\/env perl/' ${PREFIX}/scripts/*
+# copy perl libraries
+cp lib/Bio/SeqFeature/* ${PREFIX}/lib/perl5/site_perl/*/Bio/SeqFeature/
+cp -r lib/Bio/DB/* ${PREFIX}/lib/perl5/site_perl/*/Bio/DB/
+cp lib/*pm ${PREFIX}/lib/perl5/site_perl/*/
 
-# create env file to export all environment variable needed by feelnc scripts
+# create env file to export FEELNCPATH environment variable needed by feelnc scripts
 mkdir -p ${PREFIX}/etc/conda/activate.d/
 
 cat <<EOF >> ${PREFIX}/etc/conda/activate.d/feelnc-env.sh
 export FEELNCPATH=${PREFIX}/
-export PERL5LIB=$PERL5LIB:${PREFIX}/lib/
-export PATH=$PATH:${PREFIX}/scripts/:${PREFIX}/utils/
 EOF
 
 mkdir -p ${PREFIX}/etc/conda/deactivate.d/
