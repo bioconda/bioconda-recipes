cbde6df1352ef564baba4addc0feb7a5da6c43d8
RJMW <r.j.weber@bham.ac.uk>
2018-03-16 13:10:47 +0000

    Add sirius-csifingerid

---
+++ recipes/sirius-csifingerid/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $outdir/bin
+mkdir -p $outdir/lib
+mkdir -p $PREFIX/bin
+
+cp lib/*.jar $outdir/lib
+cp bin/sirius $outdir/bin/sirius
+chmod +x $outdir/bin/sirius
+ln -s $outdir/bin/sirius $PREFIX/bin
+
