69bec43ca23dc93cd57d9506867b64e55fe35fa0
Dan Fornika <dfornika@gmail.com>
2018-03-17 16:03:41 -0700

    Added summary, changed how files are copied during build

--- recipes/julia/build.sh
+++ recipes/julia/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy everything but LICENSE.md
-cp --archive --target-directory=$PREFIX ${PWD}/*/
+cp --archive --target-directory=$PREFIX ${PWD}/*
 
 # Configure juliarc to use conda environment
 cat "${RECIPE_DIR}/juliarc.jl" >> "${PREFIX}/etc/julia/juliarc.jl"
