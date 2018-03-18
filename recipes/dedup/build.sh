3c2a5885f3cdf3759f64cad945ed24e77dd41060
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-03-18 13:51:13 +0100

    Updated buildscript for deDup

--- recipes/dedup/build.sh
+++ recipes/dedup/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu -o pipefail
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
@@ -8,5 +9,3 @@ cp $RECIPE_DIR/dedup.py $outdir/dedup
 ls -l $outdir
 ln -s $outdir/dedup $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/dedup
-ls -l $outdir
-ls -l ${PREFIX}/bin/dedup
