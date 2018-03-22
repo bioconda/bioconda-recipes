991c9de61888249d4026a6be415fb3e2840cca2a
bimbam23 <bommel_23@gmx.net>
2018-03-22 14:40:33 +0100

    export C_INCLUDE_PATH

--- recipes/rcorrector/build.sh
+++ recipes/rcorrector/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export C_INCLUDE_PATH=$PREFIX/include
 make
 mkdir -p $PREFIX/bin
-cp -R rcorrector run_rcorrector.pl jellyfish/ $PREFIX/bin
+cp -R rcorrector run_rcorrector.pl $PREFIX/bin
