1687f90b951e73c039d23d10115cd8875daa6538
Thomas McGowan <mcgo0092@umn.edu>
2018-03-22 10:36:36 -0500

    Mz to sqlite (#8363)
    * Initial commit of new conda package
    
    * Add python as build and run requirement
    
    * Update for 2.0.0 version of mzToSQLite
    
    * Update to version 1.2.0 jdk requirement

--- recipes/mz_to_sqlite/1.2.0/mz_to_sqlite.py
+++ recipes/mz_to_sqlite/1.2.0/mz_to_sqlite.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
+#
+# Wrapper script for Java Conda packages that ensures that the java runtime
+# is invoked with the right options. Adapted from the bash script (http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128).
 
-
+#
+# Program Parameters
+#
 import os
-import sys
 import subprocess
-from os import access, getenv, X_OK
-jar_file = 'mzToSQLite-2.0.0.jar'
+import sys
+import shutil
+from os import access
+from os import getenv
+from os import X_OK
+jar_file = 'mzToSQLite-1.2.0.jar'
 
-default_jvm_mem_opts = ['-Xms1g', '-Xmx2g']
+default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
 
 # !!! End of parameter section. No user-serviceable code below this line !!!
+
 
 def real_dirname(path):
     """Return the symlink-resolved, canonicalized directory-portion of path."""
@@ -29,7 +38,6 @@ def java_executable():
 
 def jvm_opts(argv):
     """Construct list of Java arguments based on our argument list.
-
     The argument list passed in argv must not include the script name.
     The return value is a 3-tuple lists of strings of the form:
       (memory_options, prop_options, passthrough_options)
@@ -37,6 +45,7 @@ def jvm_opts(argv):
     mem_opts = []
     prop_opts = []
     pass_args = []
+    exec_dir = None
 
     for arg in argv:
         if arg.startswith('-D'):
@@ -45,6 +54,10 @@ def jvm_opts(argv):
             prop_opts.append(arg)
         elif arg.startswith('-Xm'):
             mem_opts.append(arg)
+        elif arg.startswith('--exec_dir='):
+            exec_dir = arg.split('=')[1].strip('"').strip("'")
+            if not os.path.exists(exec_dir):
+                shutil.copytree(real_dirname(sys.argv[0]), exec_dir, symlinks=False, ignore=None)
         else:
             pass_args.append(arg)
 
@@ -54,16 +67,23 @@ def jvm_opts(argv):
     # it is important to explictly check for equality with None
     # in the second condition, so a null envar value counts as True!
 
-    if mem_opts == [] and getenv('_JAVA_OPTIONS') == None:
+    if mem_opts == [] and getenv('_JAVA_OPTIONS') is None:
         mem_opts = default_jvm_mem_opts
 
-    return (mem_opts, prop_opts, pass_args)
+    return (mem_opts, prop_opts, pass_args, exec_dir)
 
 
 def main():
     java = java_executable()
-    jar_dir = real_dirname(sys.argv[0])
-    (mem_opts, prop_opts, pass_args) = jvm_opts(sys.argv[1:])
+    """
+    mz_to_sqlite updates files relative to the path of the jar file.
+    In a multiuser setting, the option --exec_dir="exec_dir"
+    can be used as the location for the peptide-shaker distribution.
+    If the exec_dir dies not exist,
+    we copy the jar file, lib, and resources to the exec_dir directory.
+    """
+    (mem_opts, prop_opts, pass_args, exec_dir) = jvm_opts(sys.argv[1:])
+    jar_dir = exec_dir if exec_dir else real_dirname(sys.argv[0])
 
     if pass_args != [] and pass_args[0].startswith('eu'):
         jar_arg = '-cp'
@@ -72,7 +92,7 @@ def main():
 
     jar_path = os.path.join(jar_dir, jar_file)
 
-    java_args = [java]+ mem_opts + prop_opts + [jar_arg] + [jar_path] + pass_args
+    java_args = [java] + mem_opts + prop_opts + [jar_arg] + [jar_path] + pass_args
 
     sys.exit(subprocess.call(java_args))
 
