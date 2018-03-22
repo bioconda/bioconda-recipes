cd0d4815b1ba51eb4d5ea8cbb8d45d1bb00d5662
bimbam23 <bommel_23@gmx.net>
2018-03-22 15:03:20 +0100

    more export tricks

--- recipes/rcorrector/build.sh
+++ recipes/rcorrector/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export C_INCLUDE_PATH=$PREFIX/include
+export OBJC_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+
 make
 mkdir -p $PREFIX/bin
 cp -R rcorrector run_rcorrector.pl $PREFIX/bin
