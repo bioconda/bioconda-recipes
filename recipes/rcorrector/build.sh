b58caf1745f856d9faedef50dbc41760ba5115f0
bimbam23 <bommel_23@gmx.net>
2018-03-22 15:18:09 +0100

    LD_LIBRARY_PATH

--- recipes/rcorrector/build.sh
+++ recipes/rcorrector/build.sh
@@ -2,7 +2,9 @@
 
 export C_INCLUDE_PATH=$PREFIX/include
 export OBJC_INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include 
+LD_LIBRARY_PATH=$PATH/lib
+
 
 make
 mkdir -p $PREFIX/bin
