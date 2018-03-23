f0c23f0d7a0f9fdbc4fe73f0055e07e8459e60b2
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2018-03-23 17:15:26 +0000

    Now libmagic should function properly.

--- recipes/libmagic/build.sh
+++ recipes/libmagic/build.sh
@@ -3,7 +3,7 @@
 cd $SRC_DIR
 
 # rm -f "src/magic.h"
-./configure --prefix=$PREFIX --enable-static --enable-fsect-man5 --disable-silent-rules --disable-dependency-tracking
+./configure --prefix=$PREFIX --datadir=$PREFIX/share --enable-static --enable-fsect-man5 --disable-silent-rules --disable-dependency-tracking
 make
 make install
-cp $PREFIX/share/misc/magic.mgc $HOME/.magic.mgc
+# cp $PREFIX/share/misc/magic.mgc $HOME/.magic.mgc
