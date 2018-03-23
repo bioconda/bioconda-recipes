809a538d2de04a38c9a359afb80df40f3c7d2546
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2018-03-23 17:03:31 +0000

    Upgrade libmagic to 5.32.0, fix missing .magic.mgc file in /usr/users/ga002/venturil

--- recipes/libmagic/build.sh
+++ recipes/libmagic/build.sh
@@ -2,7 +2,8 @@
 
 cd $SRC_DIR
 
-rm -f "src/magic.h"
+# rm -f "src/magic.h"
 ./configure --prefix=$PREFIX --enable-static --enable-fsect-man5 --disable-silent-rules --disable-dependency-tracking
 make
 make install
+cp $PREFIX/share/misc/magic.mgc $HOME/.magic.mgc
