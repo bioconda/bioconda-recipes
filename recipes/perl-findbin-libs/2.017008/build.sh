9d41fc4982f3002a27b77f77416c44ae226865aa
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-03-24 11:05:52 +0100

    perl changes

--- recipes/perl-findbin-libs/2.017008/build.sh
+++ recipes/perl-findbin-libs/2.017008/build.sh
@@ -18,8 +18,4 @@ else
     exit 1
 fi
 
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+#sed -i.bak 's/v2.12.01;/2.017008;/' $PREFIX/lib/perl5/site_perl/5.22.0/FindBin/libs.pm 
