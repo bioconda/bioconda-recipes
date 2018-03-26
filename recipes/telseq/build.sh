eb338b5df3d52f714304c4ae13f0f3f55dbee2b3
Roman Valls Guimera <brainstorm@nopcode.org>
2018-03-27 10:41:00 +1100

    Undo the forced GCC on OSX, follow bamtools way and @dpryan79 recommendations, thanks.

--- recipes/telseq/build.sh
+++ recipes/telseq/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
-export CC=gcc
-export CXX=g++
-
 cd $SRC_DIR/src
 ./autogen.sh
 ./configure --prefix=$PREFIX --with-bamtools=$PREFIX
