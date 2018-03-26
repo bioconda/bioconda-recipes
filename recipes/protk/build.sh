c21f56ac574fcba9ef5971e9fed012c4850f3862
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-03-27 01:50:57 +0200

    update ruby dependency

--- recipes/protk/build.sh
+++ recipes/protk/build.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 set -eu -o pipefail
-
-export LD_LIBRARY_PATH="$PREFIX/lib"
-
-#sed -i.bak "s/s.add_runtime_dependency 'sqlite3'.*/s.add_runtime_dependency 'sqlite3', '~> 1.3'/g" protk.gemspec
-cat protk.gemspec
 gem source -a https://rubygems.org
 gem build protk.gemspec
 gem env gemdir
-gem install --install-dir=$PREFIX/lib/ruby/gems/2.2.0 --bindir=$PREFIX/bin protk -- --with-xml2-lib=$PREFIX/lib --with-xml2-include=$PREFIX/include/libxml2/ --with-opt-include=$PREFIX/include --with-opt-lib=$PREFIX/lib
-
-
+gem install --install-dir=$PREFIX/lib/ruby/gems/2.4.0 --bindir=$PREFIX/bin protk -- --with-xml2-lib=$PREFIX/lib --with-xml2-include=$PREFIX/include/libxml2/ --with-opt-include=$PREFIX/include --with-opt-lib=$PREFIX/lib
