a3732f88f754a2667236cb91e926a891c72eba2a
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-03-27 14:08:52 +0200

    Try setting c++11.

--- recipes/clever-toolkit/build.sh
+++ recipes/clever-toolkit/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # CXX=$PREFIX/bin/x86_64-conda_cos6-linux-gnu-g++ 
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON .
 VERBOSE=TRUE make
 make install
