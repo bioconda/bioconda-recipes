282e1ceac2a753aa41e6eb2aa59758e51d99188c
Martin Steinegger <martin.steinegger@mpibpc.mpg.de>
2018-03-28 22:57:37 +0900

    Fix some problems mmseqs2 recipe

--- recipes/mmseqs2/build.sh
+++ recipes/mmseqs2/build.sh
@@ -5,6 +5,6 @@ export CXX=g++
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DHAVE_TESTS=0 -DHAVE_MPI=0 -DHAVE_SSE4_1=1 ..
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DHAVE_TESTS=0 -DHAVE_MPI=0 -DHAVE_SSE4_1=1 -DVERSION_OVERRIDE="${PKG_VERSION}" ..
 make -j 2
 make install
