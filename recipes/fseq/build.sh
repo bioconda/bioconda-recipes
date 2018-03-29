5aeefb5593ff6a2a66a3de043f8ec88132aece87
Roman Briskine <roman.briskine@gmail.com>
2018-03-29 12:46:15 +0200

    Fix inplace substitution for MacOS sed

--- recipes/fseq/build.sh
+++ recipes/fseq/build.sh
@@ -3,7 +3,8 @@
 fseq=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $fseq
 cp -r ./* $fseq
-sed -i '1 s|^.*$|#!/usr/bin/env perl|g' $fseq/mapviewToBed.pl
+sed -i.orig '1 s|^.*$|#!/usr/bin/env perl|' $fseq/mapviewToBed.pl
+rm -f $fseq/mapviewToBed.pl.orig
 cp $RECIPE_DIR/fseq.py $fseq/bin/fseq
 chmod +x $fseq/bin/fseq
 chmod +x $fseq/mapviewToBed.pl
