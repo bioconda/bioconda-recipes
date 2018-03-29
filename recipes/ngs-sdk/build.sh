2d5c4dd8b09859fcf6276dc6e6d37ae6c1e838c9
Cashalow <sachalau@gmail.com>
2018-03-29 09:02:58 +0200

    well actually it is

--- recipes/ngs-sdk/build.sh
+++ recipes/ngs-sdk/build.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-####
-# build ngs-sdk
-####
-
-# First configure fails
-# See: https://github.com/ncbi/ngs/issues/1
 export ROOT=$PREFIX
 ./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi
 make -C ngs-sdk
