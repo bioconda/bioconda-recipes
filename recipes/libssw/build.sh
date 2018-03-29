4f98fd07b66a1cb98b7e0f75489a66dffccbcb7a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-03-29 15:26:45 -0500

    Remove ls

--- recipes/libssw/build.sh
+++ recipes/libssw/build.sh
@@ -8,9 +8,9 @@ export LDFLAGS="-L$PREFIX/lib"
 cd src
 make
 make java
-ls
 chmod +x *
 cp example_cpp ${PREFIX}/bin
+
 for i in *.py
 do
 sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' $i
