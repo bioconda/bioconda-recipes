4310ffc144148cf3c7476cec70f0b1b5a010017b
Chen Li <danielee0707@gmail.com>
2018-03-29 15:32:10 -0700

    remove comments

--- recipes/perl-dbi/build.sh
+++ recipes/perl-dbi/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
     perl ./Build
@@ -17,9 +15,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
