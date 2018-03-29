cba50e7bbe3bff6a78150e68d82b29df79b3641c
Roman Briskine <roman.briskine@gmail.com>
2018-03-29 11:25:15 +0200

    Enable testing via the wrapper script

--- recipes/fseq/fseq.py
+++ recipes/fseq/fseq.py
@@ -5,10 +5,13 @@
 # The script was taken from peptide-shaker and adapted for fseq.
 # The peptide-shaker script was in turn adapted from the bash script 
 # (http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128).
+# When the program fseq is called with -h, it exits with 1. To make the installation testable,
+# --test flag was added.
 
 #
 # Program Parameters
 #
+import io
 import os
 import subprocess
 import sys
@@ -49,6 +52,8 @@ def jvm_opts(argv):
     mem_opts = []
     prop_opts = []
     pass_args = []
+    usage_req = False
+    is_test = False
 
     for arg in argv:
         if arg.startswith('-D'):
@@ -57,8 +62,12 @@ def jvm_opts(argv):
             prop_opts.append(arg)
         elif arg.startswith('-Xm'):
             mem_opts.append(arg)
+        elif arg == '--test':
+            is_test = True
         else:
             pass_args.append(arg)
+            if arg == '-h' or arg == '--help':
+                usage_req = True
 
     # In the original shell script the test coded below read:
     #   if [ "$jvm_mem_opts" == "" ] && [ -z ${_JAVA_OPTIONS+x} ]
@@ -69,12 +78,12 @@ def jvm_opts(argv):
     if mem_opts == [] and getenv('_JAVA_OPTIONS') is None:
         mem_opts = default_jvm_mem_opts
 
-    return (mem_opts, prop_opts, pass_args)
+    return (mem_opts, prop_opts, pass_args, is_test, usage_req)
 
 
 def main():
     java = java_executable()
-    (mem_opts, prop_opts, pass_args) = jvm_opts(sys.argv[1:])
+    (mem_opts, prop_opts, pass_args, is_test, usage_req) = jvm_opts(sys.argv[1:])
     jar_dir = os.path.join(real_dirname(sys.argv[0]), "..", "lib")
 
     jar_arg = '-cp'
@@ -83,7 +92,27 @@ def main():
 
     java_args = [java] + mem_opts + prop_opts + [jar_arg] + [jar_path] + [main_path] + pass_args
 
-    sys.exit(subprocess.call(java_args))
+    if not is_test:
+        # Note that the preferred way as of v3.5 is subprocess.run
+        exit_code = subprocess.call(java_args)
+        if usage_req:
+            print(
+                '\n'
+                'You can also specify JVM parameters such as -D, -XX, or -Xm.\n'
+                'If you get an "OutOfMemory" error, simply increase the heap size by adding -Xmx '
+                'parameter.\n'
+                'E.g. fseq -Xmx12g input.bed'
+            )
+        sys.exit(exit_code)
+    else:
+        cmd = subprocess.Popen(java_args, stdout=subprocess.PIPE)
+        cmd_out, cmd_err = cmd.communicate()
+        if "usage: fseq [options]" in cmd_out.decode().lower():
+             sys.exit(0)
+        else:
+             print(cmd_out.decode())
+             sys.exit(1)
+
 
 
 if __name__ == '__main__':
