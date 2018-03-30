9aef4941094599d5ebcd0f6b052c9e32643a4f1b
James Abbott <j.abbott@dundee.ac.uk>
2018-03-30 14:08:05 +0100

    Removed unnecessary comments...

--- recipes/perl-file-tee/0.07/build.sh
+++ recipes/perl-file-tee/0.07/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
     perl ./Build
     perl ./Build test
-    # Make sure this goes in site
     perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
-    # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
     make test
@@ -18,8 +15,3 @@ else
     exit 1
 fi
 
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
