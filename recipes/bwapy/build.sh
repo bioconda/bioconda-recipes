bfebdaa203f8cf24718ba64a4535c570728b6150
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2018-04-02 09:59:57 -0700

    Fix lint

--- recipes/bwapy/build.sh
+++ recipes/bwapy/build.sh
@@ -4,5 +4,5 @@ export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-make bwa/libbwa.a 
-$PYTHON setup.py install
+make bwa/libbwa.a
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
