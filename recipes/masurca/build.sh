d5469bda6c01754b3ac7f3b253d1112d6e291445
Marcel Bargull <mbargull@users.noreply.github.com>
2018-04-02 18:09:21 +0200

    masurca: manually download+extract tarball

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -4,5 +4,12 @@ set -x -e
 
 export BOOST_ROOT=${PREFIX}
 
-#../install.sh
+URL="https://github.com/alekseyzimin/masurca/files/1668918/MaSuRCA-${PKG_VERSION}.tar.gz"
+SHA256='759d5b0411b048d996df1ca6daadf1cc49ff88f4436a21cd81d7f191a8bd80b0'
+TAR_FILE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+curl -Lo "${TAR_FILE}" "${URL}"
+openssl sha256 "${TAR_FILE}" | grep -q "${SHA256}"
+tar -xzf "${TAR_FILE}" --strip-components=1
+rm "${TAR_FILE}"
 
+./install.sh
