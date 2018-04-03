689d92326082a45f232fa96f7edab3420960aa5b
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2018-04-03 11:08:55 +0200

    Fixed htslib includes/lib handling; Fixed deployment (libraries to lib/ folder, executables to bin/ folder)

--- recipes/ngs-bits/build.sh
+++ recipes/ngs-bits/build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+#link include and lib folders to allow using htslib
+ln -s $PREFIX/include htslib/include
+ln -s $PREFIX/lib htslib/lib
+
 #build
-CPLUS_INCLUDE_PATH=$PREFIX/include
 make build_tools_release
 
-#deploy
-mkdir -p $PREFIX/bin
+#remove test files from bin folder
 rm -rf bin/out bin/cpp*-TEST bin/tools-TEST
+
+#deploy (lib)
+mkdir -p $PREFIX/lib
+cp -r bin/libcpp* $PREFIX/lib/
+rm -rf bin/libcpp*
+
+#deploy (bin)
+mkdir -p $PREFIX/bin
 cp -r bin/* $PREFIX/bin/
