3269f4cf6f85efba4f0b73cd7420e57273a979dd
Cashalow <sachalau@gmail.com>
2018-04-04 15:25:27 +0200

    no more hdf5

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -1,5 +1,4 @@
 export ROOT=$PREFIX
-./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX --with-xml2-prefix=$PREFIX --with-hdf5-prefix=$PREFIX
+./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX --with-xml2-prefix=$PREFIX 
 make
 make install
-make test
