51a319c37f0a413f0959d30aea9215fac84061e3
Cashalow <sachalau@gmail.com>
2018-04-04 11:59:15 +0200

    fix

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -1,20 +1,4 @@
 export ROOT=$PREFIX
-mkdir -p $PREFIX/usr/include
-./configure --prefix=$PREFIX/ \
-	    --build=$PREFIX/share/ncbi \
-            --with-ngs-sdk-prefix=$PREFIX
+./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX
 make install
-
-####
-# build sra-tools
-####
-
-cd $SRC_DIR
-export VDB_SRCDIR=$SRC_VDB
-./configure --prefix=$PREFIX \
-	--build=$PREFIX/share/ncbi/ \
-	--with-ncbi-vdb-sources=$SRC_VDB \
-	--with-ncbi-vdb-build=$PREFIX/share/ncbi \
-	--with-ngs-sdk-prefix=$PREFIX
-
-make install
+make test
