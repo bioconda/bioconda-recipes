f984bdcb0cce51b84dc5e25e2429807984b2d2ef
Cashalow <sachalau@gmail.com>
2018-04-04 14:35:50 +0200

    adding hdf5 dependency

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -1,5 +1,5 @@
 export ROOT=$PREFIX
-./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX --with-xml2-prefix=$PREFIX
+./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX --with-xml2-prefix=$PREFIX --with-hdf5-prefix=$PREFIX
 make
 make install
 make test
