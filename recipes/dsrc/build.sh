edf379c6dc938cdcbf5de1c057f0e091af506e58
ntraut <ntraut9@gmail.com>
2018-04-06 15:05:58 +0200

    configuring dsrc for mac os

--- recipes/dsrc/build.sh
+++ recipes/dsrc/build.sh
@@ -4,24 +4,28 @@
 
 
 # The CFLAGS are not available in the Maefile, so let us add it
-sed -i -e  "s/\$(CXXFLAGS)/\$(CXXFLAGS) \$(CFLAGS)/g" src/Makefile
+#sed -i -e  "s/\$(CXXFLAGS)/\$(CXXFLAGS) \$(CFLAGS)/g" src/Makefile
 
 # There seem to be a missing library for the linkage (lrt)
 #sed -i -e "s/-lpthread/-lpthread -lrt/g" Makefile
 
 # The static boost_thread library cannot be found during the linkage. Let us
 # comment the -static flag
-sed -i -e "s/CXXFLAGS += -static/#CXXFLAGS += -static/g" Makefile
+#sed -i -e "s/CXXFLAGS += -static/#CXXFLAGS += -static/g" Makefile
 
 # Tell the compiler where to find boost
-export CFLAGS=" -I${PREFIX}/include -L${PREFIX}/lib "
+#export CFLAGS=" -I${PREFIX}/include -L${PREFIX}/lib "
 # -L${CONDA_PREFIX}/lib -I${CONDA_PREFIX}"
 
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
+#export CC=${PREFIX}/bin/gcc
+#export CXX=${PREFIX}/bin/g++
 
 
-make
-make bin
+#make
+if [ "$(uname)" == "Darwin" ]; then
+    make -f Makefile.osx bin
+else
+    make bin
+fi
 cp bin/dsrc $PREFIX/bin
 
