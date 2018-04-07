d89203aad5e0de2d5aded7f31f8fe9a787e9e457
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2018-04-07 15:28:36 -0700

    Commit pysam15 recipe

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -6,8 +6,8 @@ sed -i'' -e 's/"-Wno-error=declaration-after-statement"//g' setup.py
 # linking htslib, see:
 # http://pysam.readthedocs.org/en/latest/installation.html#external
 # https://github.com/pysam-developers/pysam/blob/v0.9.0/setup.py#L79
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
+export CFLAGS="-I$PREFIX/include -DHAVE_LIBDEFLATE"
+export CPPFLAGS="-I$PREFIX/include -DHAVE_LIBDEFLATE"
 export LDFLAGS="-L$PREFIX/lib"
 
 export HTSLIB_LIBRARY_DIR=$PREFIX/lib
