63db11017fce7cd09ab667a58eb852ae4fe270ec
MJ Harvey <m.j.harvey@imperial.ac.uk>
2018-04-10 22:45:57 +0100

    add dummy configure

--- recipes/kallisto/build.sh
+++ recipes/kallisto/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+cd ext/htslib
+touch configure
+chmod +x configure
+cd ../..
+
+
 mkdir -p $PREFIX/bin
 
 mkdir -p build
 cd build
+
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
 make
 make install
