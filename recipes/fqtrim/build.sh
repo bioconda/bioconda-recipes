f021e49de66af1c10556cbbc4caeb4aa811ca18a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-04-13 23:36:21 -0500

    Add variables

--- recipes/fqtrim/build.sh
+++ recipes/fqtrim/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#export C_INCLUDE_PATH=$PREFIX/include
-#export CPLUS_INCLUDE_PATH=$PREFIX/include
-#export CFLAGS="-I$PREFIX/include"
-#export LDFLAGS="-L$PREFIX/lib"
-#export CPATH=${PREFIX}/include
+export C_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
 make release
