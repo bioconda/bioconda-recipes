a368bd38b6d546ab5bf87f904272f872550b20b0
pbordron <philippe.bordron@sb-roscoff.fr>
2018-04-17 10:32:35 +0200

    replace test in order to avoid its timeout

--- recipes/sourcetracker/build.sh
+++ recipes/sourcetracker/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+chmod +x *.r src/*.r
 cp -r sourcetracker_for_qiime.r src "$CONDA_PREFIX/bin"
 mkdir -p $PREFIX/etc/conda/activate.d/
 echo '#!/bin/bash
 export SOURCETRACKER_PATH="$CONDA_PREFIX/bin"
-' > ${PREFIX}/etc/conda/activate.d/sourcetracker_env.sh
+' > ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
 
 mkdir -p $PREFIX/etc/conda/deactivate.d/
 echo '#!/bin/bash
 unset SOURCETRACKER_PATH
-' > ${PREFIX}/etc/conda/deactivate.d/sourcetracker_env.sh
+' > ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
