496af9533d01b8d8a91114e2e51528daf1b1f1e1
pbordron <philippe.bordron@sb-roscoff.fr>
2018-04-17 17:59:55 +0200

    try another way to fix SOURCETRACKER_PATH with circleci

--- recipes/sourcetracker/build.sh
+++ recipes/sourcetracker/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 chmod +x *.r src/*.r
-cp -r sourcetracker_for_qiime.r src "$PREFIX/bin"
+cp -r sourcetracker_for_qiime.r src "${PREFIX}/bin"
 
-mkdir -p $PREFIX/etc/conda/activate.d/
-echo '#!/bin/bash
-export SOURCETRACKER_PATH="$CONDA_PREFIX/bin"
-' > ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
+mkdir -p ${PREFIX}/etc/conda/activate.d/
+cat <<EOF > ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
+#!/bin/bash
+export SOURCETRACKER_PATH="${CONDA_PREFIX}/bin"
+EOF
 chmod +x ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
 
-mkdir -p $PREFIX/etc/conda/deactivate.d/
-echo '#!/bin/bash
+mkdir -p ${PREFIX}/etc/conda/deactivate.d/
+cat <<EOF > ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
+#!/bin/bash
 unset SOURCETRACKER_PATH
-' > ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
+EOF
 chmod +x ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
