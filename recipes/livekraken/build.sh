4224902ada915d26c1be236ea5aed9037f071188
Herr Simon Tausch <tausch@bfr.bund.de>
2018-04-18 13:45:32 +0200

    rename files to livekraken also in build.sh

--- recipes/livekraken/build.sh
+++ recipes/livekraken/build.sh
@@ -4,9 +4,10 @@ mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
 
 chmod u+x install_kraken.sh
 ./install_kraken.sh "$PREFIX/libexec"
-for bin in livekraken livekraken-build livekraken-filter livekraken-mpa-report livekraken-report livekraken-translate; do
+for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kraken-translate; do
+	mv "$PREFIX/libexec/$bin" "$PREFIX/libexec/live$bin"
     chmod +x "$PREFIX/libexec/$bin"
-    ln -s "$PREFIX/libexec/$bin" "$PREFIX/bin/$bin"
+    ln -s "$PREFIX/libexec/live$bin" "$PREFIX/bin/live$bin"
     # Change from double quotes to single in case of special chars
     sed -i.bak "s#my \$KRAKEN_DIR = \"$PREFIX/libexec\";#my \$KRAKEN_DIR = '$PREFIX/libexec';#g" "$PREFIX/libexec/${bin}"
     rm -rf "$PREFIX/libexec/${bin}.bak"
