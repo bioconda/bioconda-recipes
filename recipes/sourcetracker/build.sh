f7ee86c0eec4643841075aca9662aa3465a84c17
pbordron <philippe.bordron@sb-roscoff.fr>
2018-04-18 16:24:31 +0200

    Revert "try another way to fix SOURCETRACKER_PATH with circleci"
    This reverts commit 496af9533d01b8d8a91114e2e51528daf1b1f1e1.

--- recipes/sourcetracker/build.sh
+++ recipes/sourcetracker/build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 chmod +x *.r src/*.r
-cp -r sourcetracker_for_qiime.r src "${PREFIX}/bin"
+cp -r sourcetracker_for_qiime.r src "$PREFIX/bin"
 
-mkdir -p ${PREFIX}/etc/conda/activate.d/
-cat <<EOF > ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
-#!/bin/bash
-export SOURCETRACKER_PATH="${CONDA_PREFIX}/bin"
-EOF
+mkdir -p $PREFIX/etc/conda/activate.d/
+echo '#!/bin/bash
+export SOURCETRACKER_PATH="$CONDA_PREFIX/bin"
+' > ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
 chmod +x ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
 
-mkdir -p ${PREFIX}/etc/conda/deactivate.d/
-cat <<EOF > ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
-#!/bin/bash
+mkdir -p $PREFIX/etc/conda/deactivate.d/
+echo '#!/bin/bash
 unset SOURCETRACKER_PATH
-EOF
+' > ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
 chmod +x ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
