e5ef35348f05508275d9e30de18a855e5b7ed799
Herr Simon Tausch <tausch@bfr.bund.de>
2018-04-18 14:23:31 +0200

    switch to tar.gz, back to start otherwise

--- recipes/livekraken/build.sh
+++ recipes/livekraken/build.sh
@@ -5,9 +5,8 @@ mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
 chmod u+x install_kraken.sh
 ./install_kraken.sh "$PREFIX/libexec"
 for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kraken-translate; do
-	mv "$PREFIX/libexec/$bin" "$PREFIX/libexec/live$bin"
     chmod +x "$PREFIX/libexec/$bin"
-    ln -s "$PREFIX/libexec/live$bin" "$PREFIX/bin/live$bin"
+    ln -s "$PREFIX/libexec/$bin" "$PREFIX/bin/$bin"
     # Change from double quotes to single in case of special chars
     sed -i.bak "s#my \$KRAKEN_DIR = \"$PREFIX/libexec\";#my \$KRAKEN_DIR = '$PREFIX/libexec';#g" "$PREFIX/libexec/${bin}"
     rm -rf "$PREFIX/libexec/${bin}.bak"
