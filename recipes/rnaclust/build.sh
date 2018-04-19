8f20d87234e603aa82e7816cf3bb4e1bdcaadcb3
Peter van Heusden <pvh@sanbi.ac.za>
2018-04-19 07:46:53 +0200

    Remove -w flag from perl

--- recipes/rnaclust/build.sh
+++ recipes/rnaclust/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-find . -name \*.pl -exec sed  -i 's^/usr/bin/perl^/usr/bin/env perl^' {} \;
+# replace perl with /usr/bin/env perl and remove -w flag
+find . -name \*.pl -exec sed  -i 's^/usr/bin/perl -w^/usr/bin/env perl^' {} \;
 ./configure --prefix=$CONDA_PREFIX && make && make install
