aad621fca68d833c214950b3a8e6d4c393e135d2
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-04-22 00:32:56 -0400

    fixed no-cairo build

--- recipes/genometools-genometools/1.5.10-nocairo/build.sh
+++ recipes/genometools-genometools/1.5.10-nocairo/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -e -o pipefile -x
 
-make 
+make cairo=no verbose=yes
 export prefix=$PREFIX
-make cairo=no install 
+make cairo=no verbose=yes install 
 
 cd gtpython
 $PYTHON setup.py install
