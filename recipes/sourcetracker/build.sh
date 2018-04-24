7dfdc7397d52b5b2f0c410143deb8571e3bb004b
Philippe Bordron <pbordron@users.noreply.github.com>
2018-04-24 14:50:06 +0200

    Update build.sh

--- recipes/sourcetracker/build.sh
+++ recipes/sourcetracker/build.sh
@@ -7,10 +7,8 @@ mkdir -p $PREFIX/etc/conda/activate.d/
 echo '#!/bin/bash
 export SOURCETRACKER_PATH="$CONDA_PREFIX/bin"
 ' > ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
-chmod +x ${PREFIX}/etc/conda/activate.d/activate_sourcetracker_env.sh
 
 mkdir -p $PREFIX/etc/conda/deactivate.d/
 echo '#!/bin/bash
 unset SOURCETRACKER_PATH
 ' > ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
-chmod +x ${PREFIX}/etc/conda/deactivate.d/deactivate_sourcetracker_env.sh
