df88e1536eaa6e319d72556f31860db464436032
David Laehnemann <david.laehnemann@hhu.de>
2018-04-24 17:10:02 +0200

    dawg: fix of cmake in-source build error

--- recipes/dawg/build.sh
+++ recipes/dawg/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX .
+# avoid CMake Error "In-source builds not allowed."
+mkdir cmake_build_dir
+cd cmake_build_dir
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make
 make install
