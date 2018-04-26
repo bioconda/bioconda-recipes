98a13c278f298f3ed18582ff446879d30e0d9411
rchikhi <rayan.chikhi@univ-lille1.fr>
2018-04-26 22:59:56 +0200

    can't test locally so using bioconda test platform, ty

--- recipes/bcalm/build.sh
+++ recipes/bcalm/build.sh
@@ -5,6 +5,12 @@ export LIBRARY_PATH=${PREFIX}/lib
 
 mkdir -p $PREFIX/bin
 
+rm -Rf gatb-core
+git clone -n https://github.com/GATB/gatb-core
+cd gatb-core
+git checkout da36e81667f46fd34aa274e792d280a6e4b1e624
+cd ..
+
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
