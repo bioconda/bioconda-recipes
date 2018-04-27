6ad490a1392d56ad72885cd93849566d1b0706c2
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2018-04-27 13:07:25 +0200

    Export C.UTF-8 locale during install

--- recipes/umis/build.sh
+++ recipes/umis/build.sh
@@ -2,4 +2,6 @@
 if [ `uname` == Darwin ]; then
 	export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
