b0586420457a83fcc97f204086c1c7ee7bb6fe9f
Herr Simon Tausch <tausch@bfr.bund.de>
2018-04-30 13:49:46 +0200

    fix bin paths in build.sh

--- recipes/livekraken/build.sh
+++ recipes/livekraken/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-mkdir -p "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec" "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin"
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+mkdir -p "${outdir}/libexec" "$PREFIX/bin"
 
 chmod u+x install_kraken.sh
-./install_kraken.sh "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec"
+./install_kraken.sh "${outdir}/libexec"
 for bin in livekraken livekraken-build livekraken-filter livekraken-mpa-report livekraken-report livekraken-translate; do
-    chmod +x "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec/$bin"
-    ln -s "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec/$bin" "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/$bin"
+    chmod +x "${outdir}/libexec/$bin"
+    ln -s "${outdir}/libexec/$bin" "$PREFIX/bin/$bin"
     # Change from double quotes to single in case of special chars
-    sed -i.bak "s#my \$KRAKEN_DIR = \"$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec\";#my \$KRAKEN_DIR = '$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec';#g" "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec/${bin}"
-    rm -rf "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/libexec/${bin}.bak"
+    sed -i.bak "s#my \$KRAKEN_DIR = \"${outdir}/libexec\";#my \$KRAKEN_DIR = '${outdir}/libexec';#g" "${outdir}/libexec/${bin}"
+    rm -rf "${outdir}/libexec/${bin}.bak"
 done
 
-cp "visualisation/livekraken_sankey_diagram.py" "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/"
+cp "visualisation/livekraken_sankey_diagram.py" "$PREFIX/bin/"
