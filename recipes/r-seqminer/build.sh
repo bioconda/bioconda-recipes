22c2916138fee53221ec0ba07bc703b05c342ae7
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-05-02 05:41:50 +0000

    Use DYLD

--- recipes/r-seqminer/build.sh
+++ recipes/r-seqminer/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # R refuses to build packages that mark themselves as Priority: Recommended
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-$R CMD INSTALL --build .
+unamestr=`uname`
+if [ $unamestr == 'Linux' ];
+then
+    mv DESCRIPTION DESCRIPTION.old
+    grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+    $R CMD INSTALL --build .
+elif [ $unamestr == 'Darwin' ];
+then
+    export DYLD_LIBRARY_PATH=${PREFIX}/lib
+    mv DESCRIPTION DESCRIPTION.old
+    grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+    $R CMD INSTALL --build .
+fi
