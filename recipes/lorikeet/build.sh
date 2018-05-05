53cdfbb4e56e19fab32b6cdad91a317fde2bcdfa
Peter van Heusden <pvh@sanbi.ac.za>
2018-05-05 21:50:01 +0200

    Fix naming of lorikeet jar file

--- recipes/lorikeet/build.sh
+++ recipes/lorikeet/build.sh
@@ -9,7 +9,7 @@ cat <<END >$LORIKEET_DIR/lorikeet
 
 CONDA_BIN_DIR=`dirname \`which conda\``
 JAR_PATH=\$CONDA_BIN_DIR/../share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-java ${JAVA_OPTS:-} -jar \$JAR_PATH/\$PKG_NAME-\$PKG_VERSION.jar \$*
+java ${JAVA_OPTS:-} -jar \$JAR_PATH/lorikeet-$PKG_VERSION.jar \$*
 END
 chmod a+x $LORIKEET_DIR/lorikeet
 ln -s $LORIKEET_DIR/lorikeet $PREFIX/bin
