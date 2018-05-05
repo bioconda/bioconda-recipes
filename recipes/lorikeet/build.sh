a6ab110971e4647b37a0c85a5738084ad8f3cea9
Peter van Heusden <pvh@sanbi.ac.za>
2018-05-05 06:46:54 +0200

    Make jar location comply with bioconda guidelines

--- recipes/lorikeet/build.sh
+++ recipes/lorikeet/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-cp lorikeet-$PKG_VERSION.jar ${PREFIX}/bin
+LORIKEET_DIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir $LORIKEET_DIR
+cp lorikeet-$PKG_VERSION.jar $LORIKEET_DIR
 
-cat <<END >${PREFIX}/bin/lorikeet
+cat <<END >$LORIKEET_DIR/lorikeet
 #!/bin/sh
 
 JAR_PATH=`dirname \`which conda\``
 java ${JAVA_OPTS:-} -jar \$JAR_PATH/lorikeet-${PKG_VERSION}.jar \$*
 END
-chmod a+x ${PREFIX}/bin/lorikeet
+chmod a+x $LORIKEET_DIR/lorikeet
+ln -s $LORIKEET_DIR/lorikeet $PREFIX/bin
