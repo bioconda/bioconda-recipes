1b4ff1754caa957a8870fca2772982f96c442740
Peter van Heusden <pvh@sanbi.ac.za>
2018-05-05 07:10:45 +0200

    Fix jar location in wrapper script

--- recipes/lorikeet/build.sh
+++ recipes/lorikeet/build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 LORIKEET_DIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir $LORIKEET_DIR
+mkdir -p $LORIKEET_DIR
 cp lorikeet-$PKG_VERSION.jar $LORIKEET_DIR
 
 cat <<END >$LORIKEET_DIR/lorikeet
 #!/bin/sh
 
-JAR_PATH=`dirname \`which conda\``
-java ${JAVA_OPTS:-} -jar \$JAR_PATH/lorikeet-${PKG_VERSION}.jar \$*
+CONDA_BIN_DIR=`dirname \`which conda\``
+JAR_PATH=\$CONDA_BIN_DIR/../share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+java ${JAVA_OPTS:-} -jar \$JAR_PATH/\$PKG_NAME-\$PKG_VERSION.jar \$*
 END
 chmod a+x $LORIKEET_DIR/lorikeet
 ln -s $LORIKEET_DIR/lorikeet $PREFIX/bin
