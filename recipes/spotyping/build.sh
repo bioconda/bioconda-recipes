dcd841a066b1d6176b2b702d26817b0b20612a5b
Peter van Heusden <pvh@sanbi.ac.za>
2018-05-05 11:30:45 +0200

    Fully disable plot script

--- recipes/spotyping/build.sh
+++ recipes/spotyping/build.sh
@@ -4,10 +4,10 @@ SPOTYPING_DIR=${PREFIX}/share/SpoTyping
 mkdir $SPOTYPING_DIR
 cp -r SpoTyping-v${PKG_VERSION}-commandLine/* $SPOTYPING_DIR
 
-PLOT_CMD=${PREFIX}/bin/SpoTyping_plot.r
-echo '#!/usr/bin/env Rscript' > $PLOT_CMD
-cat SpoTyping-v${PKG_VERSION}-commandLine/SpoTyping_plot.r >> $PLOT_CMD
-# in the absence of the SITVIT_ONLINE service this script is disabled.
+## in the absence of the SITVIT_ONLINE service this script is disabled.
+#PLOT_CMD=${PREFIX}/bin/SpoTyping_plot.r
+#echo '#!/usr/bin/env Rscript' > $PLOT_CMD
+#cat SpoTyping-v${PKG_VERSION}-commandLine/SpoTyping_plot.r >> $PLOT_CMD
 #chmod a+x $PLOT_CMD
 
 MAIN_CMD=${PREFIX}/bin/SpoTyping.py
