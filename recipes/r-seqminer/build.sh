5fe32a34d2c3d8462a2553c408804c9ceecf5b24
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-05-06 06:25:31 +0000

    test 1

--- recipes/r-seqminer/build.sh
+++ recipes/r-seqminer/build.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-unamestr=`uname`
-export LD_LIBRARY_PATH=${PREFIX}/lib
-
-if [ $unamestr == 'Darwin' ];
-then
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-#    LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
-    LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
-    CXX=clang++;
-    CC=clang;
-fi
-
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+
 $R CMD INSTALL --build .
-cat config.log
