c2b9846e7470b1c76d8bd5bfcb05e5f165a37315
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-05-08 01:22:42 +0000

    Change osx compiler

--- recipes/bsmap/build.sh
+++ recipes/bsmap/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export CPATH=${PREFIX}/include
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I$PREFIX/include"
+
 make
 
 cp *.py ${PREFIX}/bin/
