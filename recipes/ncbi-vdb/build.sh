4536d94c002447fc41cddfbf57724d4396a59ad7
Cashalow <sachalau@gmail.com>
2018-05-08 08:24:17 +0200

    updates

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -1,4 +1,4 @@
 export ROOT=$PREFIX
-./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX --with-xml2-prefix=$PREFIX 
+./configure --prefix=$PREFIX/ --build=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX 
 make
 make install
