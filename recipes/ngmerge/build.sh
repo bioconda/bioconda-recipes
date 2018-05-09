af040843d37ced9cca14fc97496b0f958297eb18
Aaron Kitzmiller <aaron_kitzmiller@harvard.edu>
2018-05-09 14:48:19 -0400

    try to wrangle the path to omp.h

--- recipes/ngmerge/build.sh
+++ recipes/ngmerge/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-make CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+if [[ "$(uname)" == Darwin ]]; then
+    version=`conda info llvm-openmp | grep '^version' | awk -F': ' '{print $2}'`
+    CFLAGS="-I${PREFIX}/lib/clang/${version}/include ${CFLAGS}"
+    LDFLAGS="-lomp $LDFLAGS"
+else
+    CFLAGS="-fopenmp $CFLAGS"
+fi 
+
+make
 make install PREFIX=$PREFIX
