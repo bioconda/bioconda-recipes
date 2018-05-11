51883b2b503e6ef805751ba4b3a9c708f6b554c7
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-05-11 14:45:47 -0600

    cleanup

--- recipes/fastspar/build.sh
+++ recipes/fastspar/build.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 sh autogen.sh
 
+# needed on macos:
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
 
-./configure --prefix=$PREFIX 
+./configure --prefix=$PREFIX
 
 make -j $CPU_COUNT
 make install
