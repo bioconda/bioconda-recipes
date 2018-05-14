ea81ba636636d1ece02170fb65ea62104c544424
CicoZhang <cncico@gmail.com>
2018-05-14 18:37:30 +0200

    Heinz 2.0 rev 1

--- recipes/heinz/build.sh
+++ recipes/heinz/build.sh
@@ -1,2 +1,12 @@
+mkdir build
+cd build
+cmake \
+-DLIBLEMON_ROOT=${PREFIX} \
+-DLIBOGDF_ROOT=${PREFIX}/ogdf \
+-DCPLEX_INC_DIR=../IBM/cplex/include/ \
+-DCPLEX_LIB_DIR=../IBM/cplex/lib/x86-64_linux/static_pic \
+-DCONCERT_LIB_DIR=../IBM/concert/lib/x86-64_linux/static_pic \
+-DCONCERT_INC_DIR=../IBM/concert/include/ ..
+
 chmod +x heinz
 mv heinz ${PREFIX}/bin
