d795a5ff55b563a5a0fbe384f9ba866b09fc7c89
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2018-05-15 23:09:22 +0100

    removed comments

--- recipes/r-sbpiper/build.sh
+++ recipes/r-sbpiper/build.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
 $R CMD INSTALL --build .
-
-
-## conda-build 3
-#if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ #$target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
-#  export DISABLE_AUTOBREW=1
-#  mv DESCRIPTION DESCRIPTION.old
-#  grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-#  $R CMD INSTALL --build .
-#else
-#  mkdir -p $PREFIX/lib/R/library/sbpiper
-#  mv * $PREFIX/lib/R/library/sbpiper
-#fi
