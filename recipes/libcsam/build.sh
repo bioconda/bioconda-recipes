0fbc511d6ae7a13f2ea96a089fefbdf12f2c0cb2
ArneKr <arnekratz@hotmail.de>
2018-05-16 14:17:54 +0200

    Pinned boost version

--- recipes/libcsam/build.sh
+++ recipes/libcsam/build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+
+mkdir -p $PREFIX/bin
+
 make
+
+binaries="\
+CompressSAM \
+DecompressSAM \
+CompressQual \
+DecompressQual \
+CompressSeq \
+DecompressSeq \
+GetIntervalSeq \
+GetIntervalSeqSample \
+CountReadsSample \
+"
+
+for i in $binaries; do cp $i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
