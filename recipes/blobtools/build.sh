9b2ce2bce54a13461c662b51d7076327221dba8e
Daniel Bogema <daniel.bogema@dpi.nsw.gov.au>
2018-05-21 04:25:19 +0000

    Updated blobtools binary to build nodes DB on first run

--- recipes/blobtools/build.sh
+++ recipes/blobtools/build.sh
@@ -5,12 +5,6 @@ mkdir -p $blobtools
 cp -a ./* $blobtools
 cd $blobtools
 
-if [ $PY3K -eq 1 ]
-then
-    echo "Converting for python3..."
-    2to3 --write lib/*
-fi
-
 cd lib
 sed -i "s~LIBDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ''))~LIBDIR = \"$blobtools/lib\"~g" blobtools.py
 sed -i "s~SAMTOOLS = os.path.abspath(os.path.join(MAINDIR, 'samtools/bin/samtools'))~SAMTOOLS = \"$PREFIX/bin/samtools\"~g" blobtools.py
@@ -20,8 +14,16 @@ cd ..
 sed -i -e '4d;16,29d;50d' setup.py
 $PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 
-echo '#!/usr/bin/env bash' > $PREFIX/bin/blobtools
-echo "$PYTHON $blobtools/lib/blobtools.py \"\$@\"" >> $PREFIX/bin/blobtools
+echo "
+#!/usr/bin/env bash
+
+if [ ! -f \"$blobtools/data/nodesDB.txt\" ] && [ $# -ne 1 ]; then
+echo \"Building blobtools nodesDB...\"
+blobtools-build_nodesdb
+fi
+ 
+$PYTHON $blobtools/lib/blobtools.py \"\$@\"
+" > $PREFIX/bin/blobtools
 chmod +x $PREFIX/bin/blobtools
 
 echo '#!/usr/bin/env bash' > $PREFIX/bin/blobtools-build_nodesdb
