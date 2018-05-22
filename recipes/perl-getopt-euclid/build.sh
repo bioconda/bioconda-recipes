ff7860651d9dcdab01a0f5e9478e1967567f4d5e
Nick Youngblut <nyoungb2@gmail.com>
2018-05-22 16:51:58 +0200

    added makefile patches

--- recipes/perl-getopt-euclid/build.sh
+++ recipes/perl-getopt-euclid/build.sh
@@ -10,6 +10,12 @@ if [ -f Build.PL ]; then
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
+    sed -i.bak 's/-fstack-protector-strong//g' Makefile
+    sed -i.bak 's/-fstack-protector//g' Makefile
+    sed -i.bak 's|-L/usr/local/lib|-L${PREFIX}/lib|g' Makefile
+    sed -i.bak 's|LD_RUN_PATH = /usr/lib/../lib64|LD_RUN_PATH = ${PREFIX}/lib|g' Makefile
+    sed -i.bak 's|LD_RUN_PATH = /usr/lib64|LD_RUN_PATH = ${PREFIX}/lib|g' Makefile
+    sed -i.bak 's|-I/usr/local/include|-I${PREFIX}/include|g' Makefile
     make
     make test
     make install
