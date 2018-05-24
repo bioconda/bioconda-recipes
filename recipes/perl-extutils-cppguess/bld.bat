60cc8d2683a3a0fd3f07f40c61684045bfc26202
Michael Knudsen <micknudsen@gmail.com>
2018-05-24 20:57:07 +0200

    Added recipe for perl-extutils-cppguess

---
+++ recipes/perl-extutils-cppguess/bld.bat
@@ -0,0 +1,22 @@
+IF exist Build.PL (
+    perl Build.PL
+    IF %ERRORLEVEL% NEQ 0 exit 1
+    Build
+    IF %ERRORLEVEL% NEQ 0 exit 1
+    Build test
+    :: Make sure this goes in site
+    Build install --installdirs site
+    IF %ERRORLEVEL% NEQ 0 exit 1
+) ELSE IF exist Makefile.PL (
+    :: Make sure this goes in site
+    perl Makefile.PL INSTALLDIRS=site
+    IF %ERRORLEVEL% NEQ 0 exit 1
+    make
+    IF %ERRORLEVEL% NEQ 0 exit 1
+    make test
+    IF %ERRORLEVEL% NEQ 0 exit 1
+    make install
+) ELSE (
+    ECHO 'Unable to find Build.PL or Makefile.PL. You need to modify bld.bat.'
+    exit 1
+)
