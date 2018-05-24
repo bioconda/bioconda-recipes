94d2dc3e93f8fccf1cc98c79bedd7e099a3bea8d
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-05-23 21:21:31 -0400

    finished writing recipe

--- recipes/kmc/build.sh
+++ recipes/kmc/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-export DISABLE_ASMLIB=true
-export CXX=g++
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+set -e -o pipefail
 
-if [ "$(uname)" == "Darwin" ]; then
-    Makefile=makefile_mac
-else
-    Makefile=makefile
-fi
-make -f $Makefile
-
-cp bin/kmc bin/kmc_dump bin/kmc_tools "${PREFIX}/bin/"
+cp kmc kmc_dump kmc_tools "${PREFIX}/bin/"
