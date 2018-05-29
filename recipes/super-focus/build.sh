72c382698a2bb322cc34bac051073ad073852961
Fredrik Boulund <fredrik.boulund@ki.se>
2018-05-29 12:50:54 +0200

    Add SUPER-FOCUS

---
+++ recipes/super-focus/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+superfocus_dir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $superfocus_dir
+cp -r ./* $superfocus_dir
+mkdir -p $PREFIX/bin
+ln -s $superfocus_dir/superfocus.py $PREFIX/bin/superfocus.py
+ln -s $superfocus_dir/superfocus_downloadDB.py $PREFIX/bin/superfocus_downloadDB.py
