e0161011b6a37f77bee8b1d85a1609a54eb6c5a6
rvalieris <rvalieris@users.noreply.github.com>
2018-05-29 17:08:42 -0300

    clean up bcftools

--- recipes/bcftools/build.sh
+++ recipes/bcftools/build.sh
@@ -3,11 +3,6 @@
 export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
-cd htslib*
 ./configure --prefix=$PREFIX --enable-libcurl CPPFLAGS="$CPPFLAGS" LDFLAGS="-L$PREFIX/lib"
-make
-cd ..
-
-./configure --prefix=$PREFIX --enable-libcurl CPPFLAGS="$CPPFLAGS" LDFLAGS="-L$PREFIX/lib"
-make prefix=$PREFIX CPPFLAGS=$CPPFLAGS LDFLAGS=$LDFLAGS
-make prefix=$PREFIX CPPFLAGS=$CPPFLAGS LDFLAGS=$LDFLAGS plugins install
+make all CPPFLAGS=$CPPFLAGS LDFLAGS=$LDFLAGS
+make install
