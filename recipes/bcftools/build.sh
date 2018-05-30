efcdf46182dfa7a148c2dc036e726a85278602b7
rvalieris <rvalieris@users.noreply.github.com>
2018-05-30 10:43:21 -0300

    test

--- recipes/bcftools/build.sh
+++ recipes/bcftools/build.sh
@@ -5,5 +5,5 @@ export CFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
 ./configure --prefix=$PREFIX --enable-libcurl CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
-make all CPPFLAGS=$CPPFLAGS LDFLAGS=$LDFLAGS CFLAGS=$CFLAGS
+make all
 make install
