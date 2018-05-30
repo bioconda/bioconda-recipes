df1382b4df19676205149f6b824cf7a2c0cbc02c
CarlosHorro <carlos.horro@gmail.com>
2018-05-30 11:04:02 +0200

    Pathwaymatcher: new buld.sh

--- recipes/pathwaymatcher/build.sh
+++ recipes/pathwaymatcher/build.sh
@@ -1,1 +1,11 @@
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+#!/bin/bash
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
+mkdir -p $PREFIX/bin
+cp -R ./* $outdir/
+cp $RECIPE_DIR/pathwaymatcher.sh $outdir/pathwaymatcher
+chmod +x $outdir/pathwaymatcher
+
+ln -s $outdir/pathwaymatcher $PREFIX/bin
