410e43414566f6855bc936a68f5b09c32c1c2178
Andreas Sjödin <andreas.sjodin@gmail.com>
2018-06-01 22:33:30 +0200

    Use latest phyx commit

--- recipes/phyx/build.sh
+++ recipes/phyx/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib"
-export CPATH=${PREFIX}/include
+#export CFLAGS="-I$PREFIX/include"
+#export LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib"
+#export CPATH=${PREFIX}/include
 
-sed -i.bak 's/nlopt_cxx/nlopt/' src/configure.ac
 
 mkdir -p $PREFIX/bin
 cd src
