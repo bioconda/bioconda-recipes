6eb7c17dcc74ed66a28fc68888c667e7d88b257b
Andreas Sjödin <andreas.sjodin@gmail.com>
2018-06-01 22:36:03 +0200

    Build fixes

--- recipes/phyx/build.sh
+++ recipes/phyx/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-#export CFLAGS="-I$PREFIX/include"
-#export LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib"
-#export CPATH=${PREFIX}/include
-
-
 mkdir -p $PREFIX/bin
 cd src
 autoreconf -fi
-./configure   --prefix=$PREFIX
+./configure --prefix=$PREFIX
 make
 make install
-
