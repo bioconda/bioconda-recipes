2d18cf8f8e27555712a6f4123ec5cff4b1b1f2ca
Fredrik Boulund <fredrik.boulund@ki.se>
2018-06-01 11:09:15 +0200

    Add focus.py to bin dir

--- recipes/super-focus/build.sh
+++ recipes/super-focus/build.sh
@@ -5,3 +5,4 @@ cp -r ./* $superfocus_dir
 mkdir -p $PREFIX/bin
 ln -s $superfocus_dir/superfocus.py $PREFIX/bin/superfocus.py
 ln -s $superfocus_dir/superfocus_downloadDB.py $PREFIX/bin/superfocus_downloadDB.py
+ln -s $superfocus_dir/focus/focus.py $PREFIX/bin/focus.py
