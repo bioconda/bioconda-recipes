7740dbb85bf2255c02e0acc88022970ce7994255
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-06-03 19:56:29 +0200

    Add recipe for r-ngsplot

---
+++ recipes/r-ngsplot/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export BOOST_ROOT=${PREFIX}
+if [[ $(uname) == "Darwin" ]]; then
+    export LDFLAGS=-L${PREFIX}/lib
+    autoreconf -i
+    $R CMD INSTALL --build . --configure-args="CFLAGS=-ferror-limit=0 CXXFLAGS=-ferror-limit=0"
+else
+    $R CMD INSTALL --build .
+fi
