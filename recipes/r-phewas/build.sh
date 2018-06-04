46428e5e0cd2ce60c1da55bb56d70a9e6059a7fc
slacalle <slacalle@imperial.ac.uk>
2018-06-04 16:27:59 +0100

    Trying jinja2 functions again

--- recipes/r-phewas/build.sh
+++ recipes/r-phewas/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $target_platform =~ linux.* ]]; then
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
   grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
