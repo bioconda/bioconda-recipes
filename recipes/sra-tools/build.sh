6f9cbaae470b15e06b89865842f2ebab30290821
Cashalow <sachalau@gmail.com>
2018-06-05 19:20:33 +0200

    build fix

--- recipes/sra-tools/build.sh
+++ recipes/sra-tools/build.sh
@@ -3,7 +3,7 @@
 NCBI_VDB_VERSION=2.9.0-1
 NGS_VDB_SHA256=b4099e2fc3349eaf487219fbe798b22124949c89ffa1e7e6fbaa73a5178c8aff
 
-curl -L https://github.com/ncbi/ncbi-vdb/archive/${NCBI_VDB_VERSION}.tar.gz ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz
+curl -L https://github.com/ncbi/ncbi-vdb/archive/${NCBI_VDB_VERSION}.tar.gz ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz > ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz
 
 [[ $NGS_VDB_SHA256 == $(cat ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz |shasum -a 256| cut -f1 -d ' ') ]] && echo "NCBI VDB Download success" || exit 1
 
@@ -21,6 +21,7 @@ cd $SRC_VDB
 
 make install
 
+cd $SRC_DIR
 
 ./configure --prefix=$PREFIX --build-prefix=$PREFIX/share/ncbi/ --with-ngs-sdk-prefix=$PREFIX --with-ncbi-vdb-sources=$SRC_VDB --with-ncbi-vdb-build=$PREFIX/share/ncbi
 
