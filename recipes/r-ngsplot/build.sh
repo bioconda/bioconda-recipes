721e3a2cb6933a12d4b1c0621f9f0107df111b11
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-06-06 21:42:39 +0200

    More specific copying

--- recipes/r-ngsplot/build.sh
+++ recipes/r-ngsplot/build.sh
@@ -2,7 +2,7 @@
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -R * $outdir 
+cp -R {bin,database,example,galaxy,lib,webings,LICENSE} ${outdir}/
 #Set up links for 
 for f in ${outdir}/bin/*; do
     ln -s ${f} ${PREFIX}/bin
