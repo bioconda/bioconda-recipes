f3abe55ec047fc8759b4e448dca7fc88074da6cc
CarlosHorro <carlos.horro@gmail.com>
2018-06-06 15:24:03 +0200

    PathwayMatcher: improved build.sh. pathwaymatcher.py is also copied for testing

--- recipes/pathwaymatcher/build.sh
+++ recipes/pathwaymatcher/build.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
-ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
-mkdir -p $PREFIX/bin
-cp -R ./* $outdir/
-cp $RECIPE_DIR/pathwaymatcher.py $outdir/pathwaymatcher
-chmod +x $outdir/pathwaymatcher
+# Setup path variables
+BINARY_HOME=$PREFIX/bin
+PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
-ln -s $outdir/pathwaymatcher $PREFIX/bin
+# Create destination directories
+mkdir -p $PACKAGE_HOME
+mkdir -p $BINARY_HOME
+
+ln -s $PACKAGE_HOME $PREFIX/share/$PKG_NAME
+
+# Copy files into $PACKAGE_HOME
+cp -R ./* $PACKAGE_HOME/
+
+# Create wrapper
+SOURCE_FILE=$RECIPE_DIR/pathwaymatcher.py
+DEST_FILE=$PACKAGE_HOME/pathwaymatcher
+
+cp $SOURCE_FILE $DEST_FILE
+# We also copy pathwaymatcher.py as it is
+cp $SOURCE_FILE $PACKAGE_HOME/pathwaymatcher.py
+
+chmod +x $DEST_FILE
+
+ln -s $DEST_FILE $BINARY_HOME
+ln -s $PACKAGE_HOME/pathwaymatcher.py $BINARY_HOME
