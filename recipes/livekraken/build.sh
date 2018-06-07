d2d550cbaa997cf5b94a09dd930846f2b4915014
simonhtausch <https://simonhtausch@gitlab.com>
2018-06-07 12:58:34 +0200

    link classify to outdir/bin

--- recipes/livekraken/build.sh
+++ recipes/livekraken/build.sh
@@ -15,4 +15,4 @@ for bin in livekraken livekraken-build livekraken-filter livekraken-mpa-report l
 done
 
 cp "visualisation/livekraken_sankey_diagram.py" "$PREFIX/bin/"
-ln -s "libexec/classify" "$PREFIX/bin/classify"
+ln -s "${outdir}/libexec/classify" "$PREFIX/bin/classify"
