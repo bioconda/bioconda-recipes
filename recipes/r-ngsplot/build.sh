05101faf04e7b308a9309d242ff37d3699a50827
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-06-08 16:04:32 +0200

    Add exact name of package

--- recipes/r-ngsplot/build.sh
+++ recipes/r-ngsplot/build.sh
@@ -13,5 +13,5 @@ ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
 mkdir -p $ACTIVATE_DIR
 mkdir -p $DEACTIVATE_DIR
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/ngsplot-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/ngsplot-deactivate.sh
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/${PKG_NAME}-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/${PKG_NAME}-deactivate.sh
