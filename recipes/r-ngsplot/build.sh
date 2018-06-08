9b168e62a31ab8bdc9925d2b68add31835cc11a0
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-06-08 16:26:15 +0200

    Add patch for environment variable in R script

--- recipes/r-ngsplot/build.sh
+++ recipes/r-ngsplot/build.sh
@@ -13,5 +13,13 @@ ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
 mkdir -p $ACTIVATE_DIR
 mkdir -p $DEACTIVATE_DIR
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/${PKG_NAME}-activate.sh
 cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/${PKG_NAME}-deactivate.sh
+#Create activate.sh script
+echo > "${ACTIVATE_DIR}/${PKG_NAME}-activate.sh" <<EOF
+#!/bin/bash
+export NGSPLOT="${outdir}"
+export _CONDA_SET_NGSPLOT_ENV=1
+EOF
+
+#Patch ngs.plot.R to use proper path
+sed -i "s#^progpath.*#progpath=$outdir#g" ${outdir}/bin/ngs.plot.r
