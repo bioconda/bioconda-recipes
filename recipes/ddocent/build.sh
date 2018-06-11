2b86ea8cab930cfd9cc2995f8a34fcabe16a7c93
jpuritz <jpuritz@gmail.com>
2018-06-11 11:50:27 -0400

    Update build.sh

--- recipes/ddocent/build.sh
+++ recipes/ddocent/build.sh
@@ -2,10 +2,6 @@
 
 mkdir -p $PREFIX/bin
 
-git clone https://github.com/lh3/seqtk.git
-cd seqtk
-make
-cp seqtk $PREFIX/bin
-bash ../install_dDocent_requirements $PREFIX/bin
+bash install_dDocent_requirements $PREFIX/bin
 
 chmod +x $PREFIX/bin/dDocent
