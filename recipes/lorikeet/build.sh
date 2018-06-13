638022881a20355a90d6b3bc25a4b15d338bbf35
Peter van Heusden <pvh@sanbi.ac.za>
2018-06-13 21:39:28 +0200

    Compile lorikeet-19 with scala 2.11.8 to match atk build

--- recipes/lorikeet/build.sh
+++ recipes/lorikeet/build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+mkdir lib_compile/scala-2.11.8
+rm -rf lib_compile/scala-2.10.2
+for name in scala-compiler.jar scala-library.jar scala-reflect.jar ; do
+  wget -O lib_compile/scala-2.11.8/$name  https://github.com/AbeelLab/atk/raw/master/lib_compile/scala-2.11.8/$name
+done
+
+ant dist
+unzip lorikeet-development.zip lorikeet-development.jar
 LORIKEET_DIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $LORIKEET_DIR
-cp lorikeet-$PKG_VERSION.jar $LORIKEET_DIR
+cp lorikeet-development.jar $LORIKEET_DIR/lorikeet-$PKG_VERSION.jar
 
 cat <<END >$LORIKEET_DIR/lorikeet
 #!/bin/sh
