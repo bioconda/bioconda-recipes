3f4ac6a162e79bcdec4f6f93ea50d082da5b6a1c
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-06-14 09:51:59 +0400

    Update build.sh

--- recipes/breseq/build.sh
+++ recipes/breseq/build.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -x -e
+
+
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+
+gcc -v
 
 mkdir -p $PREFIX/bin
 ./configure --prefix=${PREFIX}
