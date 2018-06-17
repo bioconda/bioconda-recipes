bc7412fe34e94d4989880df66122b752fb1a0e5a
Marcel Bargull <mbargull@users.noreply.github.com>
2018-06-17 20:45:04 +0200

    orfm: try patching Makefile.am to honor *FLAGS

--- recipes/orfm/build.sh
+++ recipes/orfm/build.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-echo ================================================
-echo PREFIX="${PREFIX}"
-
-echo CC="${CC}"
-echo CXX="${CXX}"
-echo CFLAGS="${CFLAGS}"
-echo CXXFLAGS="${CXXFLAGS}"
-echo CPPFLAGS="${CPPFLAGS}"
-echo LDFLAGS="${LDFLAGS}"
-
-echo INCLUDE_PATH="${INCLUDE_PATH}"
-echo LIBRARY_PATH="${LIBRARY_PATH}"
-echo LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
-echo ================================================
-
 cd $SRC_DIR/ext
 wget -O seqtk.tar.gz https://github.com/lh3/seqtk/archive/d3b53c9.tar.gz
 tar -xvf seqtk.tar.gz -C seqtk --strip-components 1
 cd ..
 
 mkdir -p $PREFIX/bin
+sed -E -i.bak \
+    -e 's/^(CFLAGS|LDFLAGS)=/\1+=/' \
+    -e 's/(\s+)gcc \$\(CFLAGS\)/\1$(CC) $(CPPFLAGS) $(CFLAGS)/' \
+    Makefile.am
+rm Makefile.am.bak
 autoreconf --install
 ./configure
-make
+make \
+    CC="${CC}" \
+    CFLAGS="${CFLAGS}" \
+    CPPFLAGS="${CPPFLAGS}" \
+    LDFLAGS="${LDFLAGS}" \
 mv orfm  $PREFIX/bin
