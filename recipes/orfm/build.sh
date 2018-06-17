e0da715819cae82ec354811d03c3b380ab1c62a9
Marcel Bargull <mbargull@users.noreply.github.com>
2018-06-17 19:29:27 +0200

    orfm: remove outdated automake fixes

--- recipes/orfm/build.sh
+++ recipes/orfm/build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-set -x -e
-
-# I think these were fixed upstream
-# fix automake
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
-#
-## fix autoconf
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
-#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
-
 cd $SRC_DIR/ext
 wget -O seqtk.tar.gz https://github.com/lh3/seqtk/archive/d3b53c9.tar.gz
 tar -xvf seqtk.tar.gz -C seqtk --strip-components 1
