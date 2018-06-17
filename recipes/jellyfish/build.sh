da8907fa41f032f8e5981d09cb34f0c2e6733f83
Marcel Bargull <marcel.bargull@udo.edu>
2018-06-18 01:13:45 +0200

    jellyfish: remove debug code

--- recipes/jellyfish/build.sh
+++ recipes/jellyfish/build.sh
@@ -1,16 +1,6 @@
-#!/usr/bin/env bash
-
-set -x -e
-
-
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-pushd $SRC_DIR
+#!/bin/bash
 
 autoreconf -fi -Im4
 ./configure --prefix=$PREFIX
-cat Makefile
 make
 make install
