022530201d90254fc8ecea4227cb433ff9afc5df
Marcel Bargull <mbargull@users.noreply.github.com>
2018-06-17 20:04:18 +0200

    orfm: debugging

--- recipes/orfm/build.sh
+++ recipes/orfm/build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo ================================================
+echo PREFIX="${PREFIX}"
+
+echo CC="${CC}"
+echo CXX="${CXX}"
+echo CFLAGS="${CFLAGS}"
+echo CXXFLAGS="${CXXFLAGS}"
+echo CPPFLAGS="${CPPFLAGS}"
+echo LDFLAGS="${LDFLAGS}"
+
+echo INCLUDE_PATH="${INCLUDE_PATH}"
+echo LIBRARY_PATH="${LIBRARY_PATH}"
+echo LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
+echo ================================================
+
 cd $SRC_DIR/ext
 wget -O seqtk.tar.gz https://github.com/lh3/seqtk/archive/d3b53c9.tar.gz
 tar -xvf seqtk.tar.gz -C seqtk --strip-components 1
