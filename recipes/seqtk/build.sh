e053d9f2bc96c41d3cae0dbcdac9db497e91a9a7
jpuritz <jpuritz@gmail.com>
2018-06-18 09:50:11 -0400

    Update build.sh

--- recipes/seqtk/build.sh
+++ recipes/seqtk/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-export CFLAGS="-I$PREFIX/include -g -Wall -O2 -Wno-unused-function"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export C_INCLUDE_PATH="${PREFIX}/include"
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CPPLAGS="-I${PREFIX}/include -g -Wall -O2 -Wno-unused-function"
+export CFLAGS="$CPPFLAGS"
+export LDFLAGS="-L${PREFIX}/lib "
+export CPATH="${PREFIX}/include"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export CFLAGS_EXTRA="${LDFLAGS} ${CPPFLAGS}"
+
 
 make -e all
 mkdir -p $PREFIX/bin
