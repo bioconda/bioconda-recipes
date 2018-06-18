6381313f5c10c8f8917b80bb16972a8e07ca11a2
jpuritz <jpuritz@gmail.com>
2018-06-18 09:18:20 -0400

    Update build.sh

--- recipes/seqtk/build.sh
+++ recipes/seqtk/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#export C_INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
+
+export CFLAGS="-I$PREFIX/include -g -Wall -O2 -Wno-unused-function"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 make all
 mkdir -p $PREFIX/bin
 cp -f seqtk $PREFIX/bin/
