76fb0902a7645386d436e4772518a28900a3e0ee
jpuritz <jpuritz@gmail.com>
2018-06-19 08:49:26 -0400

    Update build.sh

--- recipes/seqtk/build.sh
+++ recipes/seqtk/build.sh
@@ -6,6 +6,7 @@ export CPATH=${PREFIX}/include
 
 sed -i.bak 's:CFLAGS=.*:CFLAGS=-I'"${PREFIX}"'\/include -g -Wall -O2 -Wno-unused-function:g' Makefile
 sed -i.bak 's:seqtk.c -o:'"${LDFLAGS}"' seqtk.c -o:g' Makefile
+
 make all
 mkdir -p $PREFIX/bin
 cp -f seqtk $PREFIX/bin/
