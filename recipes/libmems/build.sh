cd4b823164c03e43be7b1a65a96a17366294b87d
James Abbott <j.abbott@dundee.ac.uk>
2018-06-19 21:02:13 +0100

    Moved patching to build.sh. Patch line-endings converted to unix, and added removal of \r\n line endings from files to be patched

--- recipes/libmems/build.sh
+++ recipes/libmems/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cd trunk
+
+# Some patches are required, but the source has DOS line endings which 
+# require the --binary argument to patch, which is not provided through conda 
+# Best option seems to be to convert line endings then apply patches here
+sed -i.bak $'s/\r$//' libMems/ProgressiveAligner.cpp
+sed -i.bak $'s/\r$//' libMems/AbstractMatch.h
+
+patch -p 1 < $RECIPE_DIR/libMems.1.patch
+patch -p 1 < $RECIPE_DIR/libMems.2.patch
+
 ./autogen.sh
 ./configure --prefix=$PREFIX --with-boost=$CONDA_PREFIX
 make
