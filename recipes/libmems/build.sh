049fd663ac10a93f590008a9081b37b3c8deff28
James Abbott <j.abbott@dundee.ac.uk>
2018-06-19 15:11:55 +0100

    Add some debug code to check how pkgconfig thinks we should link against libMems...

--- recipes/libmems/build.sh
+++ recipes/libmems/build.sh
@@ -2,6 +2,10 @@
 
 cd trunk
 ./autogen.sh
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --with-boost=$CONDA_PREFIX
 make
 make install
+
+# let's see if boost -mt libs are listed in the pkgconfig file...
+cat $PREFIX/lib/pkgconfig/libMems-1.6.pc
+
