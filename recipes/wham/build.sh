56741feb49685e1aca55a7099e060be32d6fe340
Björn Grüning <bjoern@gruenings.eu>
2018-06-19 11:02:49 +0200

    Update build.sh

--- recipes/wham/build.sh
+++ recipes/wham/build.sh
@@ -2,6 +2,9 @@
 export CPPFLAGS="-I$PREFIX/include"
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+export C_INCLUDE_PATH=${PREFIX}/include
+
 make
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
