9daa9bc61169fb22a0664e2e40c61e078fb6308b
rvalieris <rvalieris@users.noreply.github.com>
2018-06-19 14:51:23 -0300

    fix stuff

--- recipes/sra-tools/build.sh
+++ recipes/sra-tools/build.sh
@@ -4,10 +4,10 @@
 
 ####
 # downloading ncbi-vdb
-#### 
+####
 
 export ROOT=$PREFIX
-NCBI_VDB_VERSION=2.9.0-1
+NCBI_VDB_VERSION=$PKG_VERSION-1
 NGS_VDB_SHA256=b4099e2fc3349eaf487219fbe798b22124949c89ffa1e7e6fbaa73a5178c8aff
 
 curl -L https://github.com/ncbi/ncbi-vdb/archive/${NCBI_VDB_VERSION}.tar.gz > ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz
@@ -17,34 +17,28 @@ curl -L https://github.com/ncbi/ncbi-vdb/archive/${NCBI_VDB_VERSION}.tar.gz > nc
 
 mkdir -p ncbi-vdb
 
-tar xzf ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz -C ncbi-vdb 
+tar xzf ncbi-vdb-${NCBI_VDB_VERSION}.tar.gz -C ncbi-vdb
 
 SRC_VDB=$SRC_DIR/ncbi-vdb/ncbi-vdb-${NCBI_VDB_VERSION}
+BUILD_OUTDIR=$SRC_DIR/ncbi-outdir
 
 ####
 # building ncbi-vdb
-#### 
-
+####
 
 if [[ $OSTYPE == darwin* ]]; then
      export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 fi
 
-
 cd $SRC_VDB
-./configure --prefix=$PREFIX/ --build-prefix=$PREFIX/share/ncbi --with-ngs-sdk-prefix=$PREFIX
-
-make install
-
-cd $SRC_DIR
+./configure --prefix=$PREFIX --build-prefix=$BUILD_OUTDIR --with-ngs-sdk-prefix=$PREFIX
+make
 
 ####
 # building sra-tools
-#### 
+####
 
-
-./configure --prefix=$PREFIX --build-prefix=$PREFIX/share/ncbi/ --with-ngs-sdk-prefix=$PREFIX --with-ncbi-vdb-sources=$SRC_VDB --with-ncbi-vdb-build=$PREFIX/share/ncbi
-
+cd $SRC_DIR
+./configure --prefix=$PREFIX --build-prefix=$BUILD_OUTDIR --with-ngs-sdk-prefix=$PREFIX --with-ncbi-vdb-sources=$SRC_VDB --with-ncbi-vdb-build=$BUILD_OUTDIR
 make
 make install
-make tests
