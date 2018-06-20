73adc2f9a9911f6edee0108396fdb8644a3dbf7b
Christian Brueffer <christian@brueffer.io>
2018-06-20 12:08:27 +0200

    Remove accidental git merge goo.

--- recipes/fastp/build.sh
+++ recipes/fastp/build.sh
@@ -5,12 +5,9 @@ export CFLAGS=-I$PREFIX/include
 export CXXFLAGS=-I$PREFIX/include
 export LDFLAGS=-L$PREFIX/lib
 
-<<<<<<< HEAD
 # XXX print the linker search paths
 ldconfig -v 2>/dev/null | grep -v ^$'\t'
 
-=======
->>>>>>> 3d7857fd1aeddac4d6e9ca631d3123e7518a2405
 make
 mkdir -p $PREFIX/bin
 cp fastp $PREFIX/bin
