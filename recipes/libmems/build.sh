f366c3c0bd969f7c350c7b733b5d2dc229299932
James Abbott <j.abbott@dundee.ac.uk>
2018-06-20 13:39:27 +0100

    More debug...

--- recipes/libmems/build.sh
+++ recipes/libmems/build.sh
@@ -6,6 +6,7 @@
 
 sed -i.bak $'s/\r$//' trunk/libMems/ProgressiveAligner.cpp
 sed -i.bak $'s/\r$//' trunk/libMems/AbstractMatch.h
+ls -l trunk/libMems
 patch -p 1 < $RECIPE_DIR/patch.1
 patch -p 1 < $RECIPE_DIR/patch.1
 
