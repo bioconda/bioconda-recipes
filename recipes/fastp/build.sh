9674ed89de2b3cb68e1d9fb321f1ac6328cf8a6b
Christian Brueffer <christian@brueffer.io>
2018-06-20 10:21:04 +0200

    Debug: print the linker search paths.

--- recipes/fastp/build.sh
+++ recipes/fastp/build.sh
@@ -5,6 +5,9 @@ export CFLAGS=-I$PREFIX/include
 export CXXFLAGS=-I$PREFIX/include
 export LDFLAGS=-L$PREFIX/lib
 
+# XXX print the linker search paths
+ldconfig -v 2>/dev/null | grep -v ^$'\t'
+
 make
 mkdir -p $PREFIX/bin
 cp fastp $PREFIX/bin
