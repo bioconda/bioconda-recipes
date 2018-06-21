62aaeb39fc2c1dfe5c43813ef6693e5c4f92f2d8
Marcel Bargull <mbargull@users.noreply.github.com>
2018-06-21 18:26:26 +0200

    mauvealigner: pass over LDFLAGS and others to configure

--- recipes/mauvealigner/build.sh
+++ recipes/mauvealigner/build.sh
@@ -3,9 +3,11 @@
 PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${CONDA_PREFIX}/lib/pkgconfig
 
 ./autogen.sh
-./configure --prefix=$PREFIX 
+./configure --prefix=$PREFIX \
+  CC="${CC}" \
+  CFLAGS="${CFLAGS}" \
+  CPPFLAGS="${CPPFLAGS}" \
+  LDFLAGS="${LDFLAGS}"
 make
 find src -maxdepth 1 -perm +111 -exec strip {} \;
 make install
-
-
