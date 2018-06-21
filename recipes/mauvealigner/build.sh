62983be668711b28a04468224ca895aec9d332ec
James Abbott <j.abbott@dundee.ac.uk>
2018-06-21 15:46:50 +0100

    Remove debug

--- recipes/mauvealigner/build.sh
+++ recipes/mauvealigner/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash 
 
 PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${CONDA_PREFIX}/lib/pkgconfig
-for dir in $(ld --verbose|grep SEARCH_DIR|sed -e 's/SEARCH_DIR("//g' -e 's/");//g'); do
-        ret=$(ls -1 ${dir} 2>/dev/null|grep libpthread)
-        if [[ $ret ]]; then
-                >&2 echo libpthread in ${dir}
-                >&2 echo ======================================
-                >&2 echo ${ret}
-        fi
-done; 
 
 ./autogen.sh
 ./configure --prefix=$PREFIX 
