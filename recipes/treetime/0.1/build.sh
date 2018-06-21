af2497bcc4a257fcaa3bab1592bd01d8b90f2b30
dpryan79 <dpryan79@gmail.com>
2018-06-21 12:40:55 +0200

    Don't use sed then

--- recipes/treetime/0.1/build.sh
+++ recipes/treetime/0.1/build.sh
@@ -2,7 +2,7 @@
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 mkdir -p $PREFIX/bin
+echo -e "#!/usr/bin/env python\n$(cat ancestral_reconstruction.py)" > ancestral_reconstruction.py
+echo -e "#!/usr/bin/env python\n$(cat timetree_inference.py)" > timetree_inference.py
 chmod a+x *.py
-sed -i.bak "1i#!/usr/bin/env python" ancestral_reconstruction.py
-sed -i.bak "1i#!/usr/bin/env python" timetree_inference.py
 cp ancestral_reconstruction.py timetree_inference.py $PREFIX/bin
