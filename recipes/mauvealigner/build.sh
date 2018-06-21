c8cf2b7cfa5004cb41ded8b7e53410210c3df0a6
James Abbott <j.abbott@dundee.ac.uk>
2018-06-21 19:16:36 +0100

    Try removing targets for static binaries from src/Makefile

--- recipes/mauvealigner/build.sh
+++ recipes/mauvealigner/build.sh
@@ -3,11 +3,8 @@
 PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${CONDA_PREFIX}/lib/pkgconfig
 
 ./autogen.sh
-./configure --prefix=$PREFIX \
-  CC="${CC}" \
-  CFLAGS="${CFLAGS}" \
-  CPPFLAGS="${CPPFLAGS}" \
-  LDFLAGS="${LDFLAGS}"
+./configure --prefix=$PREFIX 
+sed -i.bak -e 's/ mauveStatic$(EXEEXT)//' -e 's/\tprogressiveMauveStatic$(EXEEXT)/\t/' src/Makefile
 make
 find src -maxdepth 1 -perm +111 -exec strip {} \;
 make install
