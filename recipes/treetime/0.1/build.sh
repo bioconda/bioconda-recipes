059038e2c68d235262114663bbf890b23bd6f72d
dpryan79 <dpryan79@gmail.com>
2018-06-21 11:27:38 +0200

    fix treetime 0.1

--- recipes/treetime/0.1/build.sh
+++ recipes/treetime/0.1/build.sh
@@ -2,6 +2,7 @@
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 mkdir -p $PREFIX/bin
-sed -i "1i#!/usr/bin/env python" ancestral_reconstruction.py
-sed -i "1i#!/usr/bin/env python" timetree_inference.py
+chmod a+x *.py
+sed "1i#!/usr/bin/env python" ancestral_reconstruction.py > ancestral_reconstruction.py
+sed "1i#!/usr/bin/env python" timetree_inference.py > timetree_inference.py
 cp ancestral_reconstruction.py timetree_inference.py $PREFIX/bin
