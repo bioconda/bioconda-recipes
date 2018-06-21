52499544feecdc1c8181d7c3b0546a8d66647071
dpryan79 <dpryan79@gmail.com>
2018-06-21 13:32:15 +0200

    If at first you don't succeed, try and try again

--- recipes/treetime/0.1/build.sh
+++ recipes/treetime/0.1/build.sh
@@ -2,7 +2,8 @@
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 mkdir -p $PREFIX/bin
-echo -e "#!/usr/bin/env python\n$(cat ancestral_reconstruction.py)" > ancestral_reconstruction.py
-echo -e "#!/usr/bin/env python\n$(cat timetree_inference.py)" > timetree_inference.py
-chmod a+x *.py
-cp ancestral_reconstruction.py timetree_inference.py $PREFIX/bin
+echo -e "#!/usr/bin/env python\n" > $PREFIX/bin/ancestral_reconstruction.py
+echo -e "#!/usr/bin/env python\n" > $PREFIX/bin/timetree_inference.py
+cat ancestral_reconstruction.py >> $PREFIX/bin/ancestral_reconstruction.py
+cat timetree_inference.py >>  $PREFIX/bin/timetree_inference.py
+chmod a+x $PREFIX/bin/ancestral_reconstruction.py $PREFIX/bin/timetree_inference.py
