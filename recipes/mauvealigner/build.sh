c5a7268df078f88894d000cbc70cb1176c12eff1
James Abbott <j.abbott@dundee.ac.uk>
2018-06-21 14:28:36 +0100

    Send debug to stderr and see if it actually appears this time...

--- recipes/mauvealigner/build.sh
+++ recipes/mauvealigner/build.sh
@@ -4,15 +4,12 @@ PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${CONDA_PREFIX}/lib/pkgconfig
 for dir in $(ld --verbose|grep SEARCH_DIR|sed -e 's/SEARCH_DIR("//g' -e 's/");//g'); do
         ret=$(ls -1 ${dir} 2>/dev/null|grep libpthread)
         if [[ $ret ]]; then
-                echo libpthread in ${dir}
-                echo ======================================
-                echo ${ret}
+                >&2 echo libpthread in ${dir}
+                >&2 echo ======================================
+                >&2 echo ${ret}
         fi
 done;
 exit 1
-~
-
-
 ./autogen.sh
 ./configure --prefix=$PREFIX 
 make
