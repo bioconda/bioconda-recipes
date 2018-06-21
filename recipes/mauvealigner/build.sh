060c69a4383ee8a097ebb413303128d853d40c5d
James Abbott <j.abbott@dundee.ac.uk>
2018-06-21 19:38:30 +0100

    Try removing static flag from configure.ac...

--- recipes/mauvealigner/build.sh
+++ recipes/mauvealigner/build.sh
@@ -2,9 +2,9 @@
 
 PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${CONDA_PREFIX}/lib/pkgconfig
 
+sed -i.bak 's/-static //' configure.ac
 ./autogen.sh
 ./configure --prefix=$PREFIX 
-sed -i.bak -e 's/ mauveStatic$(EXEEXT)//' -e 's/\tprogressiveMauveStatic$(EXEEXT)/\t/' src/Makefile
 make
 find src -maxdepth 1 -perm +111 -exec strip {} \;
 make install
