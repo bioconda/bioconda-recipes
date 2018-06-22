46e0e71bc2b3dcef0a510fd21f7d7b4dd3c06cb2
James Abbott <j.abbott@dundee.ac.uk>
2018-06-22 10:29:41 +0100

    Disable static builds...

--- recipes/mauvealigner/build.sh
+++ recipes/mauvealigner/build.sh
@@ -2,7 +2,8 @@
 
 PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${CONDA_PREFIX}/lib/pkgconfig
 
-sed -i.bak 's/-static //' configure.ac
+#Disable static builds since these won't link to standard libraries
+sed -i.bak 's/STATIC_FLAG="-static -Wl,--whole-archive -lpthread -Wl,--no-whole-archive"//' configure.ac
 ./autogen.sh
 ./configure --prefix=$PREFIX 
 make
