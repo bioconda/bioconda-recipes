865492389fd55ef9f87245c1b5ac4d75e890b778
dpryan79 <dpryan79@gmail.com>
2018-06-22 14:49:08 +0200

    Update subread and fix real

--- recipes/subread/build.sh
+++ recipes/subread/build.sh
@@ -3,13 +3,21 @@
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/annotation"
 
+export C_INCLUDE_PATH="${PREFIX}/include"
 cd src
-MAKEFILE=Makefile.Linux
 if [ `uname` = "Darwin" ];
 then
   MAKEFILE=Makefile.MacOS
+  sed -i.bak "371" $MAKEFILE
+else
+  MAKEFILE=Makefile.Linux
+  sed -i.bak "37d" $MAKEFILE
 fi
-make -f $MAKEFILE
+cd longread-one
+touch make.version
+make CC_EXEC=$CC LDFLAGS="-lpthread -lz -lm -O3 -DMAKE_FOR_EXON -D MAKE_STANDALONE -L${PREFIX}/lib"
+cd ..
+make -f $MAKEFILE CC_EXEC=$CC STATIC_MAKE="-L${PREFIX}/lib"
 cd ..
 cp bin/utilities/* $PREFIX/bin
 rm -r bin/utilities
