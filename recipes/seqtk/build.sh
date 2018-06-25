184cbef6b217085dd2a3d1e432117483a82caafa
Marcel Bargull <mbargull@users.noreply.github.com>
2018-06-25 21:04:56 +0200

    seqtk: teach Makefile more flags

--- recipes/seqtk/build.sh
+++ recipes/seqtk/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+sed -i.bak 's/\$(CFLAGS)/$(CPPFLAGS) $(CFLAGS) $(LDFLAGS)/' Makefile
 mkdir -p "${PREFIX}/bin"
 make \
   CC="${CC}" \
+  CPPFLAGS="${CPPFLAGS}" \
   CFLAGS="${CFLAGS}" \
+  LDFLAGS="${CFLAGS}" \
   BINDIR="${PREFIX}/bin" \
   install
