4b22f5bfae423fa353d42e21686892f184891c8a
jpuritz <jpuritz@gmail.com>
2018-06-26 09:38:21 -0400

    trying to fix Makefile

--- recipes/seqtk/build.sh
+++ recipes/seqtk/build.sh
@@ -1,4 +1,6 @@
  #!/bin/bash
+
+export LDFLAGS="-L$PREFIX/lib"
   
 sed -i.bak 's/\$(CFLAGS)/$(CPPFLAGS) $(CFLAGS) $(LDFLAGS)/' Makefile
 sed -i.bak 's/^CC=gcc$//' Makefile
@@ -6,5 +8,5 @@ sed -i.bak 's/^CFLAGS=.*//' Makefile
 sed -i.bak 's/^BIND.*//' Makefile
 
 mkdir -p "${PREFIX}/bin"
-make CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" LDFLAGS="${CFLAGS}" BINDIR="${PREFIX}/bin" all
+make CC="${CC}" CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" BINDIR="${PREFIX}/bin" all
 make install
