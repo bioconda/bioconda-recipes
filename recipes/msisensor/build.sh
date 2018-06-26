21c65ad49f97dad3f06d1be04cdd8fcdad7f2e5b
Michael Knudsen <micknudsen@gmail.com>
2018-06-26 14:05:40 +0200

    Alternative approach to samtools dependency

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
-tar -xzf 0.1.19.tar.gz
-cd samtools-0.1.19
-make
-export SAMTOOLS_ROOT=$PWD
-cd ..
+export SAMTOOLS_ROOT=$PREFIX/include
 make
 make install
 mkdir -p $PREFIX/bin
