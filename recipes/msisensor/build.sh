628bdc43d146caa490f505ba7fd23065431eef31
Michael Knudsen <micknudsen@gmail.com>
2018-06-26 15:20:24 +0200

    Starting all over...

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+
 curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
 tar -xzf 0.1.19.tar.gz
-cd samtools-0.1.19
+
+export SAMTOOLS_ROOT=samtools-0.1.19
+
 make
-export SAMTOOLS_ROOT=$PWD
-cd ..
-make
-make install
-mkdir -p $PREFIX/bin
-cp msisensor $PREFIX/bin
