25e668cbc7648c1ae200a4ed95390f77a1f68baa
Michael Knudsen <micknudsen@gmail.com>
2018-06-26 14:39:16 +0200

    Back to first approach

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export SAMTOOLS_ROOT=$PREFIX/include
+curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
+tar -xzf 0.1.19.tar.gz
+cd samtools-0.1.19
+make
+export SAMTOOLS_ROOT=$PWD
+cd ..
 make
 make install
 mkdir -p $PREFIX/bin
