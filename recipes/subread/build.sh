ee4f430f1c3d740e887bdea73e0f745179a8a8fd
dpryan79 <dpryan79@gmail.com>
2018-06-27 08:58:00 +0200

    Use -D MACOS on OSX

--- recipes/subread/build.sh
+++ recipes/subread/build.sh
@@ -8,14 +8,16 @@ cd src
 if [ `uname` = "Darwin" ];
 then
   MAKEFILE=Makefile.MacOS
+  OSX="-D MACOS"
   sed -i.bak "31d" $MAKEFILE
 else
   MAKEFILE=Makefile.Linux
+  OSX=""
   sed -i.bak "37d" $MAKEFILE
 fi
 cd longread-one
 touch make.version
-make CC_EXEC=$CC LDFLAGS="-lpthread -lz -lm -O3 -DMAKE_FOR_EXON -D MAKE_STANDALONE -L${PREFIX}/lib"
+make CC_EXEC=$CC LDFLAGS="-lpthread -lz -lm -O3 ${MACOS} -DMAKE_FOR_EXON -D MAKE_STANDALONE -L${PREFIX}/lib"
 cd ..
 make -f $MAKEFILE CC_EXEC=$CC STATIC_MAKE="-L${PREFIX}/lib"
 cd ..
