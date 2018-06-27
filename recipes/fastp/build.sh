439ef3c772503523d0fcd7c1acd4fed45609d7b8
Jason Travis <JasonTravis@nau.edu>
2018-06-26 21:31:55 -0700

    Add compiler flags

--- recipes/fastp/build.sh
+++ recipes/fastp/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
+
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 make
 mkdir -p $PREFIX/bin
 cp fastp $PREFIX/bin
