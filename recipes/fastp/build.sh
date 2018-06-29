4f46de93171facdd628eeb3300578618ce8f2a39
Jason Travis <JasonTravis@nau.edu>
2018-06-28 19:43:47 -0700

    patch Makefile

--- recipes/fastp/build.sh
+++ recipes/fastp/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -eu -o pipefail
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-
-make
-mkdir -p $PREFIX/bin
-cp fastp $PREFIX/bin
+make INCLUDE_DIRS="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib"
+make install
