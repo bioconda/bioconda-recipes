17623bc41eed7536612d442351d52b6f05f6e3e7
Michael Uhl <uhl.michael@gmx.de>
2018-06-29 21:37:28 +0200

    try with pip install

--- recipes/sshmm/build.sh
+++ recipes/sshmm/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export DATAPATH=$CONDA_PREFIX/share/rnastructure/data_tables/
+cd $CONDA_PREFIX
+mkdir -p ./etc/conda/activate.d
+mkdir -p ./etc/conda/deactivate.d
+echo -e '#!/bin/sh\n\nexport DATAPATH=$CONDA_PREFIX/share/rnastructure/data_tables/' > ./etc/conda/activate.d/env_vars.sh
+echo -e '#!/bin/sh\n\nexport DATAPATH=' > ./etc/conda/deactivate.d/env_vars.sh
 
-$PYTHON setup.py install
-
+pip install sshmm
