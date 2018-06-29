576d453b32568e82a87e1ae97f5a4a279e519a31
Michael Knudsen <micknudsen@gmail.com>
2018-06-29 08:29:14 +0200

    Removed debugging stuff

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -3,19 +3,6 @@
 curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
 tar -xzf 0.1.19.tar.gz
 
-echo "*********************************"
-echo "* START LOOKING FOR zlib.h FILE *"
-echo "*********************************"
-
-find / -name zlib.h
-
-echo "********************************"
-echo "* DONE LOOKING FOR zlib.h FILE *"
-echo "********************************"
-
-# TESTING. ZLIB VERSION SHOULD NOT BE HARDCODED!
-export PATH="/opt/conda/pkgs/zlib-1.2.11-0/include:$PATH"
-
 export SAMTOOLS_ROOT=samtools-0.1.19
 
 make
