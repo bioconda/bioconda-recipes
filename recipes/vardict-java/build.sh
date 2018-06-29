fd9a9b18cb5625ccb0e6b41ba5a4caf69bd38476
Michael Milton <ttmigueltt@gmail.com>
2018-06-29 16:04:37 +1000

    Convert install to use git; add R and perl scripts that are needed for VCF conversion

--- recipes/vardict-java/build.sh
+++ recipes/vardict-java/build.sh
@@ -5,7 +5,16 @@ mkdir -p $outdir/bin
 mkdir -p $outdir/lib
 mkdir -p $PREFIX/bin
 
-cp lib/*.jar $outdir/lib
-cp bin/VarDict $outdir/bin/vardict-java
-chmod +x $outdir/bin/vardict-java
-ln -s $outdir/bin/vardict-java $PREFIX/bin
+# Compile
+./gradlew clean installDist
+
+# Copy into prefix
+cp build/install/VarDict/lib/*.jar $outdir/lib
+cp build/install/VarDict/bin/VarDict $outdir/bin/vardict-java
+cp build/install/VarDict/bin/VarDict.bat $outdir/bin/vardict-java.bat
+cp VarDict/teststrandbias.R $outdir/bin/vardict-strandbias
+cp VarDict/testsomatic.R $outdir/bin/vardict-testsomatic
+cp VarDict/var2vcf_valid.pl $outdir/bin/vardict-var2vcf
+cp VarDict/var2vcf_paired.pl $outdir/bin/vardict-var2vcf-paired
+chmod +x $outdir/bin/*
+ln -s $outdir/bin/* $PREFIX/bin
