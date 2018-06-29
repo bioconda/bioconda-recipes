57ff57537f8bb9c92cfc811af37422d6d16aa02b
Michael Knudsen <micknudsen@gmail.com>
2018-06-29 08:06:51 +0200

    Debugging: where is the zlib.h file?

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -3,6 +3,16 @@
 curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
 tar -xzf 0.1.19.tar.gz
 
+echo "*********************************"
+echo "* START LOOKING FOR zlib.h FILE *"
+echo "*********************************"
+
+find / -name zlib.h
+
+echo "********************************"
+echo "* DONE LOOKING FOR zlib.h FILE *"
+echo "********************************"
+
 export SAMTOOLS_ROOT=samtools-0.1.19
 
 make
