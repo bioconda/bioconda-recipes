6d933e7117c4613a231176c0731091246440f7b6
James Abbott <j.abbott@dundee.ac.uk>
2018-06-30 17:24:13 +0100

    Added debug

--- recipes/mauve/build.sh
+++ recipes/mauve/build.sh
@@ -2,8 +2,15 @@
 
 ant jar
 mauve_dir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-cp Mauve.jar ${mauve_dir}/
-cp -R ext ${mauve_dir}/
-cp Mauve ${mauve_dir}/
+cp -v Mauve.jar ${mauve_dir}/
+echo 'mauve_dir:'
+echo ==============
+ls -l ${mauve_dir}
+cp v-R ext ${mauve_dir}/
+echo ==============
+ls -l ${mauve_dir}
+cp -v Mauve ${mauve_dir}/
+echo ==============
+ls -l ${mauve_dir}
 ln -s ${muave_dir}/Mauve $PREFIX/bin/Mauve
 
