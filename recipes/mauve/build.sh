111777a19c7d29cec489ca341ee59e250acb607b
James Abbott <j.abbott@dundee.ac.uk>
2018-06-30 18:13:46 +0100

    Corrected badly formed variable name

--- recipes/mauve/build.sh
+++ recipes/mauve/build.sh
@@ -7,7 +7,7 @@ cp -v Mauve.jar ${mauve_dir}/Mauve.jar
 echo 'mauve_dir:'
 echo ==============
 ls -l ${mauve_dir}
-mkdir -v {$mauve_dir}/ext
+mkdir -v ${mauve_dir}/ext
 cp -vR ext/* ${mauve_dir}/ext/
 echo ==============
 ls -l ${mauve_dir}
