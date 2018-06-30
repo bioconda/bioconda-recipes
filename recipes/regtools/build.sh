2691d785f328f06643744e8272b2fb9fa8aa4a9a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-06-30 04:36:53 +0000

    Add LIBRARY_PATH

--- recipes/regtools/build.sh
+++ recipes/regtools/build.sh
@@ -2,6 +2,8 @@
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 export CPATH=${PREFIX}/include
 
 cmake -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX .
