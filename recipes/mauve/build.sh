c597f223676b4201eb3b99205acca07613575185
James Abbott <j.abbott@dundee.ac.uk>
2018-06-30 17:13:47 +0100

    Fix paths...

--- recipes/mauve/build.sh
+++ recipes/mauve/build.sh
@@ -2,8 +2,8 @@
 
 ant jar
 mauve_dir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-cp Mauve.jar ${mauve_dir}
-cp -R ext ${mauve_dir}
-cp Mauve ${mauve_dir}
+cp Mauve.jar ${mauve_dir}/
+cp -R ext ${mauve_dir}/
+cp Mauve ${mauve_dir}/
 ln -s ${muave_dir}/Mauve $PREFIX/bin/Mauve
 
