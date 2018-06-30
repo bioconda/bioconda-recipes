7a008a53e3478d6799a0796eea27f805d426df26
James Abbott <j.abbott@dundee.ac.uk>
2018-06-30 17:33:13 +0100

    Specifiy targets when copying files...

--- recipes/mauve/build.sh
+++ recipes/mauve/build.sh
@@ -2,14 +2,15 @@
 
 ant jar
 mauve_dir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-cp -v Mauve.jar ${mauve_dir}/
+cp -v Mauve.jar ${mauve_dir}/Mauve.jar
 echo 'mauve_dir:'
 echo ==============
 ls -l ${mauve_dir}
-cp v-R ext ${mauve_dir}/
+mkdir -v {$mauve_dir}/ext
+cp -vR ext/* ${mauve_dir}/ext/
 echo ==============
 ls -l ${mauve_dir}
-cp -v Mauve ${mauve_dir}/
+cp -v Mauve ${mauve_dir}/Mauve
 echo ==============
 ls -l ${mauve_dir}
 ln -s ${muave_dir}/Mauve $PREFIX/bin/Mauve
