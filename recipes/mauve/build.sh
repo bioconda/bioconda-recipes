6cdf3902c0285d19107ce6c02e8a1c2403daad45
James Abbott <j.abbott@dundee.ac.uk>
2018-07-01 21:34:10 +0100

    Clean up...

--- recipes/mauve/build.sh
+++ recipes/mauve/build.sh
@@ -3,18 +3,9 @@
 ant jar
 mauve_dir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p ${mauve_dir}
-cp -v Mauve.jar ${mauve_dir}/Mauve.jar
-echo 'mauve_dir:'
-echo ==============
-ls -l ${mauve_dir}
+cp Mauve.jar ${mauve_dir}/Mauve.jar
 mkdir -v ${mauve_dir}/ext
-cp -vR ext/* ${mauve_dir}/ext/
-echo ==============
-ls -l ${mauve_dir}
-cp -v linux-x64/Mauve ${mauve_dir}/Mauve
-echo ==============
-ls -l ${mauve_dir}
+cp R ext/* ${mauve_dir}/ext/
+cp  linux-x64/Mauve ${mauve_dir}/Mauve
 ln -s ${mauve_dir}/Mauve $PREFIX/bin/Mauve
-echo ==============
-ls -l ${mauve_dir}
 
