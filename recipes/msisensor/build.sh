6b48e6c8fa62e0c10be0ca93622437d1fb728ccd
Michael Knudsen <micknudsen@gmail.com>
2018-07-03 15:15:34 +0200

    Added samtools as build requirement

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -3,7 +3,8 @@
 curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
 tar -xzf 0.1.19.tar.gz
 
-export SAMTOOLS_ROOT=samtools-0.1.19
+#export SAMTOOLS_ROOT=samtools-0.1.19
+export SAMTOOLS_ROOT=${PREFIX}/include/bam
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
