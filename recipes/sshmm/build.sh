ae5ad50c5c0c83b69968e91a51adcfc8e388634f
Michael Uhl <uhlm@informatik.uni-freiburg.de>
2018-07-04 14:01:13 +0200

    Update build.sh

--- recipes/sshmm/build.sh
+++ recipes/sshmm/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
-echo -e '#!/bin/sh\n\nexport DATAPATH='$CONDA_PREFIX'/share/rnastructure/data_tables/' > $PREFIX/etc/conda/activate.d/env_vars.sh
+echo -e '#!/bin/sh\n\nexport DATAPATH='$PREFIX'/share/rnastructure/data_tables/' > $PREFIX/etc/conda/activate.d/env_vars.sh
 echo -e '#!/bin/sh\n\nexport DATAPATH=' > $PREFIX/etc/conda/deactivate.d/env_vars.sh
 
 pip install sshmm
