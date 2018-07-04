fdf9a94863ba11bd87e35f8ff250193c70178f74
dpryan79 <dpryan79@gmail.com>
2018-07-04 21:53:11 +0200

    Fix various recipes, but not byobu

--- recipes/bxtools/build.sh
+++ recipes/bxtools/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -eu -o pipefail
 
-export CPPFLAGS=-I$PREFIX/include
-export LDFLAGS=-L$PREFIX/lib
-export LIBS=-lpthread
+rm -rf SeqLib
+cd SeqLib
+git checkout f19055cefcd8f41f13450080bbcf453f692278b2
+cd ..
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 ./configure --prefix=$PREFIX
 make
