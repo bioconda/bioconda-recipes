2abffb5eec916da2283acfafa0cfddbccb8dd894
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-07-06 08:53:58 +0200

    ton of fixes

--- recipes/miniasm/build.sh
+++ recipes/miniasm/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
 mkdir -p $PREFIX/bin
 
 make 
