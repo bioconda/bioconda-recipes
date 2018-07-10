4f4de8b98538c607249b60edb640bbf2e800af34
Devon Ryan <dpryan79@users.noreply.github.com>
2018-07-10 10:59:28 +0200

    Fix corset, mir-prefer and disco/1.0 (#9843)
    * corset is cxx
    
    * fix mir-prefer
    
    * fix disco/1.0
    
    * Don't build disco/1.0 on OSX

--- recipes/disco/1.0/build.sh
+++ recipes/disco/1.0/build.sh
@@ -7,6 +7,10 @@ export LIBRARY_PATH=${PREFIX}/lib
 
 
 make clean
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 if [ `uname` == Darwin ]; then
     if mpic++ --show | grep -q "clang++"; then
