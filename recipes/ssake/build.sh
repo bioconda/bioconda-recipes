258d23a26dce62a72338208a7fe0b13a5832c7a4
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-07-10 07:19:03 +0000

    Add path patch

--- recipes/ssake/build.sh
+++ recipes/ssake/build.sh
@@ -2,11 +2,6 @@
 
 mkdir -p ${PREFIX}/bin
 
-sed -i.bak "s:/usr/bin/perl:/usr/bin/env perl:" ssake/SSAKE
-sed -i.bak "s:/usr/bin/perl:/usr/bin/env perl:" ssake/tools/*.pl
-sed -i.bak "s:/home/martink/bin/perl:/usr/bin/env perl:" ssake/tools/getStats.pl
-sed -i.bak "s:/usr/bin/python:/usr/bin/env python:" ssake/tools/*.py
-
 cp ssake/SSAKE ${PREFIX}/bin
 cp ssake/tools/*.pl ${PREFIX}/bin
 cp ssake/tools/*.py ${PREFIX}/bin
