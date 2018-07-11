1cd3e30c2ef42297b72413a09ee16113055d3707
jpuritz <jpuritz@gmail.com>
2018-07-11 13:05:03 -0400

    fix makefile modification

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -13,7 +13,7 @@ sed -i.bak 's/SUBDIRS=./SUBDIRS=.\'$'\n''LOBJS=tabix.o/' tabixpp/Makefile
 sed -i.bak 's/-ltabix//' Makefile
 
 sed -i.bak 's/-Ihtslib\/-Ihtslib -I'$PREFIX'\/include/' tabixpp/Makefile
-sed -i.bak 's/-Lhtslib/-Lhtslib -L"$PREFIX'\/lib/' tabixpp/Makefile
+sed -i.bak 's/-Lhtslib/-Lhtslib -L'$PREFIX'\/lib/' tabixpp/Makefile
 
 make
 
