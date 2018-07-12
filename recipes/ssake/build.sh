8d21ee0e9df4d2262d94ffcaea7a5bf3fea5cf3c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-07-12 01:14:44 +0000

    Force rebuild

--- recipes/ssake/build.sh
+++ recipes/ssake/build.sh
@@ -4,6 +4,6 @@ chmod +x ssake/SSAKE
 chmod +x ssake/tools/*.pl
 chmod +x ssake/tools/*.py
 
-cp ssake/SSAKE ${PREFIX}/bin
-cp ssake/tools/*.pl ${PREFIX}/bin
-cp ssake/tools/*.py ${PREFIX}/bin
+cp ssake/SSAKE ${PREFIX}/bin/
+cp ssake/tools/*.pl ${PREFIX}/bin/
+cp ssake/tools/*.py ${PREFIX}/bin/
