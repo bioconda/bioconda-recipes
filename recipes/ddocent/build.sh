2d964c8129fd88c41c578c31c997d38ba94993cb
jpuritz <jpuritz@gmail.com>
2018-07-14 22:30:04 -0400

    fix trimmomatic install again

--- recipes/ddocent/build.sh
+++ recipes/ddocent/build.sh
@@ -4,7 +4,7 @@ mkdir -p $PREFIX/bin
 
 #bash install_dDocent_requirements $PREFIX/bin
 
-TRIM=$(readlink -f trimmomatic | sed 's/trimmomatic\///' )
+TRIM=$(readlink -f trimmomatic | sed 's/trimmomatic//' )
 
 cp $TRIM/trimmomatic.jar $PREFIX/bin/trimmomatic.jar
 cp $TRIM/adapters/* $PREFIX/bin/
