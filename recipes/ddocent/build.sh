3640268034a12c6c564f0ab8e7e38083730e590d
jpuritz <jpuritz@gmail.com>
2018-07-15 00:47:12 -0400

    Update build.sh

--- recipes/ddocent/build.sh
+++ recipes/ddocent/build.sh
@@ -2,11 +2,7 @@
 
 mkdir -p $PREFIX/bin
 
-#bash install_dDocent_requirements $PREFIX/bin
-
 TRIM=$PREFIX/share
-
-ls -a $TRIM/trimmomatic*
 
 cp $TRIM/trimmomatic*/trim*.jar $PREFIX/bin/trimmomatic.jar
 cp $TRIM/trimmomatic*/adapters/* $PREFIX/bin/
