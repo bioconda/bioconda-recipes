ec1240830f9882e0190704c98d596ce393764342
jpuritz <jpuritz@gmail.com>
2018-07-15 00:04:43 -0400

    fix rainbow build testsA

--- recipes/rainbow/build.sh
+++ recipes/rainbow/build.sh
@@ -8,9 +8,12 @@ make
 
 cp rainbow $PREFIX/bin
 
-chmod +x select_*
 
 cp select_all_rbcontig.pl $PREFIX/bin
 cp select_best_rbcontig.pl $PREFIX/bin
 cp select_sec_rbcontig.pl $PREFIX/bin
 cp select_best_rbcontig_plus_read1.pl $PREFIX/bin
+
+chmod +x $PREFIX/bin/select_*
+
+ll $PREFIX/bin
