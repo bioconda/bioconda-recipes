46358fa97c6e443c48b82d4b4496c0a448939247
Bjoern Gruening <bjoern.gruening@gmail.com>
2018-07-16 08:37:49 +0200

    and some more for a good start in the week

--- recipes/plink2/build.sh
+++ recipes/plink2/build.sh
@@ -3,7 +3,7 @@ sed -i.bak -e "s/shasum/openssl sha1 -r/g" plink_first_compile
 # Remove "make plink" so we can call it with overrides
 sed -i.bak -e "s/make plink//g" plink_first_compile
 # This downloads and builds a local zlib-1.2.8
-#./plink_first_compile
+./plink_first_compile
 
 # Build using Makefile.std as recommended in the README
 if [[ -z "$OSX_ARCH" ]]; then
