e31195099e1255254292c793751b3d348700d425
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-07-17 12:02:50 -0400

    i got nothing

--- recipes/pheniqs/build.sh
+++ recipes/pheniqs/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
+export CFLAGS+="-I$PREFIX/include"
+# export CXXFLAGS="${CXXFLAGS} -std=c++11"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 make all PREFIX=${PREFIX}
