c704be860569933a58ac90edb6395975f10ae5ac
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-07-17 13:31:01 -0400

    different test

--- recipes/perl-compress-raw-zlib/build.sh
+++ recipes/perl-compress-raw-zlib/build.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-cpanm -i .
+
+export PATH=/opt/rh/devtoolset-2/root/usr/bin/:$PATH
+HOME=/tmp cpanm --installdeps .
+
+if [ -f Build.PL ]; then
+    perl Build.PL
+    perl ./Build
+    #perl ./Build test
+    # Make sure this goes in site
+    perl ./Build install --installdirs site
+elif [ -f Makefile.PL ]; then
+    # Make sure this goes in site
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    #make test
+    make install
+else
+    echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
+    exit 1
+fi
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
