8b2c4968c71f1c440e95db63022d6e84a6943ff5
Karl Nordström <karl.nordstrom@gmail.com>
2018-07-17 10:07:10 +0200

    bs_call: tested with circleci

--- recipes/bs_call/build.sh
+++ recipes/bs_call/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+export CPATH=${PREFIX}/include
+
+#This version of bs_call lacks a functioning configure script.
+#  Make sure all dependencies are found (including bzip2 and zlib)
 sed -i.bak '
-    @^GSL_LIB = -L/apps/GSL/2.4/lib/@ s@= -L.*@= '$PREFIX'/lib@
-    @^GSL_INC = -I/apps/GSL/2.4/include/@ s@= -I.*@'$PREFIX'/include@
+    /^GSL_LIB = -L\/apps\/GSL\/2.4\/lib\// s@= -L.*@= -L'$PREFIX'/lib@
+    /^GSL_INC = -I\/apps\/GSL\/2.4\/include\// s@= -I.*@= -I'$PREFIX'/include@
   ' Gsl.mk
+
+sed -i.bak '
+    /^INCLUDE_FLAGS=/ s@$@ -I'$PREFIX'/include@
+    /^LIB_PATH_FLAGS=/ s@$@ -L'$PREFIX'/lib@
+  ' GEMTools/Makefile.mk
+
 
 make all
 mkdir -p $PREFIX/bin
