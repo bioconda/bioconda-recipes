1a36bbf6a85b2578d29956c1a2839feb82f0ad07
Karl Nordström <karl.nordstrom@gmail.com>
2018-07-18 16:23:15 +0200

    bs_call: add gemBS compatible version

---
+++ recipes/bs_call/2.0.1_gemBS/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export CPATH=${PREFIX}/include
+
+#This version of bs_call lacks a functioning configure script.
+#  Make sure all dependencies are found (including bzip2 and zlib)
+sed -i.bak '
+    /^GSL_LIB = -L\/apps\/GSL\/2.4\/lib\// s@= -L.*@= -L'$PREFIX'/lib@
+    /^GSL_INC = -I\/apps\/GSL\/2.4\/include\// s@= -I.*@= -I'$PREFIX'/include@
+  ' Gsl.mk
+
+sed -i.bak '
+    /^INCLUDE_FLAGS=/ s@$@ -I'$PREFIX'/include@
+    /^LIB_PATH_FLAGS=/ s@$@ -L'$PREFIX'/lib@
+  ' GEMTools/Makefile.mk
+
+
+make all
+mkdir -p $PREFIX/bin
+cp bin/bs_call $PREFIX/bin
+cp bin/dbSNP_idx $PREFIX/bin
