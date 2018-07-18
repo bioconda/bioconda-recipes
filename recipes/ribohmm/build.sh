25609955b35fa7f6c0f814b0edb51e4dbb984416
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-07-18 19:19:07 +0200

    Added shebang and made all scripts executable

--- recipes/ribohmm/build.sh
+++ recipes/ribohmm/build.sh
@@ -2,3 +2,13 @@
 $PYTHON setup.py build_ext --inplace --force
 $PYTHON setup.py install
 cp *.py ${PREFIX}/bin/
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/bam_to_tbi.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/compute_mappability.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/construct_synthetic_footprints.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/infer_CDS.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/learn_model.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/load_data.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/plot_model.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/setup.py
+sed -i '#!/usr/bin/env python' ${PREFIX}/bin/utils.py
+chmod 755 ${PREFIX}/bin/*.py
