b2f788aee67d2a0c6ec99994dbc8b5afd0662e5c
Michael Knudsen <micknudsen@gmail.com>
2018-07-18 09:38:34 +0200

    MSIsensor need specific samtools version at build time

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+curl -L https://github.com/samtools/samtools/archive/0.1.19.tar.gz > 0.1.19.tar.gz
+
+tar -xzf 0.1.19.tar.gz
+cd samtools-0.1.19
+make
+export SAMTOOLS_ROOT=$PWD
+
+cd ..
 make
 make install
