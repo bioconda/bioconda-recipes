645202becbaf6789a1428e6520ea3590cf2f57b0
Stefan Hammer <j.4@gmx.at>
2018-07-18 13:39:10 +0200

    Removes too much debugging

--- recipes/barriers/build.sh
+++ recipes/barriers/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./autogen.sh || (cat config.log ; exit 1)
+./autogen.sh
 ./configure --prefix=$PREFIX --with-hash-bits=27 CFLAGS='-mcmodel=large' || (cat config.log ; exit 1)
-make || (cat config.log ; exit 1)
-make install || (cat config.log ; exit 1)
+make
+make install
