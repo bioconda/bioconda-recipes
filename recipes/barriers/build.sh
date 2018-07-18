9ea24a3b845c0c8434dcc556a3589a83318a4aec
Stefan Hammer <j.4@gmx.at>
2018-07-18 13:37:39 +0200

    Adds suggestions by @nsoranzo and orders top level meta according to conda-forge requirements

--- recipes/barriers/build.sh
+++ recipes/barriers/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./autogen.sh && \
-./configure --prefix=$PREFIX --with-hash-bits=27 CFLAGS='-mcmodel=large' && \
-make && \
-make install
+./autogen.sh || (cat config.log ; exit 1)
+./configure --prefix=$PREFIX --with-hash-bits=27 CFLAGS='-mcmodel=large' || (cat config.log ; exit 1)
+make || (cat config.log ; exit 1)
+make install || (cat config.log ; exit 1)
