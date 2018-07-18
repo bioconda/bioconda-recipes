949e6e99430a7641086619b46a522ba10d05db5d
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-07-18 12:08:38 -0400

    pheniqs builds locally wheeee

--- recipes/pheniqs/build.sh
+++ recipes/pheniqs/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export CFLAGS+="-I$PREFIX/include"
-# export CXXFLAGS="${CXXFLAGS} -std=c++11"
+set -x -e
+
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 make all PREFIX=${PREFIX}
+make install PREFIX=${PREFIX}
+
+#This is really a test
+pheniqs demux --config test/000000000-BDGGG/demultiplex.json -VD
