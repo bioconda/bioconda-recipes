129bce812f155af7222649e6fc6bd81756a5a6cf
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-07-19 08:14:31 -0400

    adding perl-text-soundex

--- recipes/perl-image-exiftool/build.sh
+++ recipes/perl-image-exiftool/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -x -e
+
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    ./Build test
-    # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build
+    perl ./Build test
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
