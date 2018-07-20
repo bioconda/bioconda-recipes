a9600c0ac6d8f1b467927ff4ee36b230ce021ce0
dpryan79 <dpryan79@gmail.com>
2018-07-20 16:37:25 +0200

    Try to fix OSX

--- recipes/quorum/build.sh
+++ recipes/quorum/build.sh
@@ -2,6 +2,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     echo "Use *.dylib for OSX"
     export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.dylib
+    export JELLYFISH2_0_CFLAGS=${PREFIX}/include/jellyfish-2.1.10
 else
     echo "Use *.so for Linux"
     export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.so
