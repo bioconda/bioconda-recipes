b9d433d37124a3549ecd338c5b5f7d7ff6eaf2ee
dpryan79 <dpryan79@gmail.com>
2018-07-20 15:55:09 +0200

    Remove blasr_libcpp, quorum, and treemix/1.12 from the blacklist

--- recipes/quorum/build.sh
+++ recipes/quorum/build.sh
@@ -1,4 +1,4 @@
-export JELLYFISH2_0_CFLAGS=-I$PREFIX/include/jellyfish-2.2.6/
+#!/bin/bash
 if [ "$(uname)" == "Darwin" ]; then
     echo "Use *.dylib for OSX"
     export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.dylib
@@ -7,6 +7,10 @@ else
     export JELLYFISH2_0_LIBS=$PREFIX/lib/libjellyfish-2.0.so
 fi
 
+export LIBRARY_PATH=${PREFIX}/lib
+export CPP_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/jellyfish-2.2.10
+export CPLUS_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/jellyfish-2.2.10
+export CXX_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/jellyfish-2.2.10
 ./configure --prefix=$PREFIX
 make
 make install
