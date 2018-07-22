e94d0d01a37482a1b44f768e6e2e2ffec77d9975
Alexander Peltzer <alexander.peltzer@uni-tuebingen.de>
2018-07-22 18:45:51 +0200

    Fix build paths

--- recipes/schmutzi/build.sh
+++ recipes/schmutzi/build.sh
@@ -5,6 +5,8 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 # schmutzi uses non-standard bamtools functions that aren't part of the normal library
+cd lib
+rmdir bamtools
 git clone https://github.com/pezmaster31/bamtools
 mkdir bamtools/build
 cd bamtools/build
@@ -13,6 +15,8 @@ make
 cd ../../
 
 # Build libgab
+cd lib
+rmdir libgab
 git clone https://github.com/grenaud/libgab
 cd libgab
 make BAMTOOLSINC=${PREFIX}/include/bamtools BAMTOOLSLIB=${PREFIX}/lib
