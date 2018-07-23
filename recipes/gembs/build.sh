ebef7a8672d20047db628de767cbf00ecf8c4e26
Karl Nordström <karl.nordstrom@gmail.com>
2018-07-23 15:38:46 +0200

    passes mulled build

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -3,8 +3,8 @@
 #instead of installing software from submodules, let them be dependencies
 
 #install locally included non-python code
-make --directory=tools conda
-cp tools/bin/{gemBS_cat,readNameClean} $PREFIX/bin
+#make --directory=tools conda
+#cp tools/bin/{gemBS_cat,readNameClean} $PREFIX/bin
 
-$PYTHON setup.py install
+$PYTHON setup.py install --minimal
 
