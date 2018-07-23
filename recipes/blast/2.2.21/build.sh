c07fab477b9dd58717e5ade589ebb780cc23cc11
dpryan79 <dpryan79@gmail.com>
2018-07-23 22:55:24 +0200

    Remove blasr, ecoprimers and networkxgmml from the blacklist

--- recipes/blast/2.2.21/build.sh
+++ recipes/blast/2.2.21/build.sh
@@ -6,17 +6,10 @@ mkdir -p $PREFIX/bin
 
 if [ "$(uname)" == "Darwin" ]; then
     echo "Platform: Mac"
-
-    # export CFLAGS='-O3'
-    # export CXXFLAGS='-O3'
-
     cp $SRC_DIR/bin/* $PREFIX/bin
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Platform: Linux"
-    
     cp $SRC_DIR/bin/* $PREFIX/bin/
-    
 fi
-
 chmod +x $PREFIX/bin/*
