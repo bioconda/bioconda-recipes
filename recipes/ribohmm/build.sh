fb87606a65251fe94e3aa323ce87a6b6d4beeb29
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-07-23 13:40:07 +0200

    Updated sed command

--- recipes/ribohmm/build.sh
+++ recipes/ribohmm/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 $PYTHON setup.py build_ext --inplace --force
 cp *.py ${PREFIX}/bin/
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/bam_to_tbi.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/compute_mappability.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/construct_synthetic_footprints.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/infer_CDS.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/learn_model.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/load_data.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/plot_model.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/setup.py
-sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/utils.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/bam_to_tbi.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/compute_mappability.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/construct_synthetic_footprints.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/infer_CDS.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/learn_model.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/load_data.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/plot_model.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/setup.py
+sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/utils.py
 chmod 755 ${PREFIX}/bin/*.py
