42f3ed54bd60462de9175e93a896b83aaa85dd95
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-07-23 12:46:48 +0200

    Improved shebang insertion

--- recipes/ribohmm/build.sh
+++ recipes/ribohmm/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 $PYTHON setup.py build_ext --inplace --force
 cp *.py ${PREFIX}/bin/
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/bam_to_tbi.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/compute_mappability.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/construct_synthetic_footprints.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/infer_CDS.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/learn_model.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/load_data.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/plot_model.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/setup.py
-sed -i '#!/usr/bin/env python' ${PREFIX}/bin/utils.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/bam_to_tbi.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/compute_mappability.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/construct_synthetic_footprints.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/infer_CDS.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/learn_model.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/load_data.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/plot_model.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/setup.py
+sed -i '\#\!\/usr\/bin\/env python\n' ${PREFIX}/bin/utils.py
 chmod 755 ${PREFIX}/bin/*.py
