3d3619209484ea1964da53911b164a25075023a1
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-07-24 00:36:26 +0200

    Updating hard coded path in seq lib

--- recipes/ribohmm/build.sh
+++ recipes/ribohmm/build.sh
@@ -3,7 +3,8 @@ $PYTHON setup.py build_ext --inplace --force
 $PYTHON setup.py install
 cp *.py ${PREFIX}/bin/
 cp *.pyx ${PREFIX}/bin/
-cp -r data $PREFIX/bin
+cp *.pxd ${PREFIX}/bin/
+cp -r data/kozak_model.npz $PREFIX/share
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/bam_to_tbi.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/compute_mappability.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/construct_synthetic_footprints.py
@@ -13,4 +14,5 @@ sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/load_data.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/plot_model.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/setup.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/utils.py
+sed -i 's@data/kozak_model.npz@"$PREFIX"/share/kozak_model.npz@' ${PREFIX}/bin/seq.pyx
 chmod 755 ${PREFIX}/bin/*.py
