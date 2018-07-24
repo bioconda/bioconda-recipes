47e79846897c42b280421b6f153effd5f887a0c8
Dan Leehr <dan.leehr@duke.edu>
2018-07-24 13:54:36 -0400

    Provide prefix to configure call, run tests on build

--- recipes/deploid/build.sh
+++ recipes/deploid/build.sh
@@ -1,6 +1,10 @@
 export CXXFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
+sed -i.bak 's|^./configure$|./configure --prefix="$PREFIX"|g' bootstrap
+
 ./bootstrap
 make
+make unit_tests
+./unit_tests
 make install
