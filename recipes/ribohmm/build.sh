b62d396a34ce639e6a89029a0f318e28d1a067d6
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-07-24 11:57:50 +0200

    Copying data before compilying

--- recipes/ribohmm/build.sh
+++ recipes/ribohmm/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cp -r data/kozak_model.npz $PREFIX/share/kozak_model.npz
+sed -i 's@data/kozak_model.npz@'"$PREFIX"'/share/kozak_model.npz@' seq.pyx
 $PYTHON setup.py build_ext --inplace --force
 $PYTHON setup.py install
 cp *.py ${PREFIX}/bin/
 cp *.pyx ${PREFIX}/bin/
 cp *.pxd ${PREFIX}/bin/
-cp -r data/kozak_model.npz $PREFIX/share/kozak_model.npz
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/bam_to_tbi.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/compute_mappability.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/construct_synthetic_footprints.py
@@ -14,5 +15,4 @@ sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/load_data.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/plot_model.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/setup.py
 sed -i '1s/^/\#!\/usr\/bin\/env python\n/' ${PREFIX}/bin/utils.py
-sed -i 's@data/kozak_model.npz@'"$PREFIX"'/share/kozak_model.npz@' ${PREFIX}/bin/seq.pyx
 chmod 755 ${PREFIX}/bin/*.py
