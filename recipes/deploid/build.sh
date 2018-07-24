126598784d33c8c4d1d427f146dde90ee7b824c0
Dan Leehr <dan.leehr@duke.edu>
2018-07-24 10:56:51 -0400

    Sorta working build script

--- recipes/deploid/build.sh
+++ recipes/deploid/build.sh
@@ -1,3 +1,11 @@
-./bootstrap
+export CFLAGS="-I$PREFIX/include"
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+
+# Moved from bootstrap
+aclocal
+autoconf
+automake -a
+./configure CPPFLAGS="$CPPFLAGS"--prefix=$PREFIX LDFLAGS="$LDFLAGS"
 make
 make install
