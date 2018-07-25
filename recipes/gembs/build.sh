e9e2fcff4fc35379e1bde90bc84234a53b70777f
Karl Nordström <karl.nordstrom@gmail.com>
2018-07-25 18:50:37 +0200

    pointing to the current gem3-mapper instead of a custom version

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -2,5 +2,5 @@
 
 #instead of installing software from submodules, let them be dependencies
 
-$PYTHON setup.py install --minimal
+$PYTHON setup.py install --minimal --single-version-externally-managed --record=record.txt
 
