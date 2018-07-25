c8aa9c75fcb1f7f05849288df63bc7dee3ffc9b4
jfallmann <fall@bioinf.uni-leipzig.de>
2018-07-25 13:12:43 +0200

    working on paths and exports

--- recipes/dialign2/build.sh
+++ recipes/dialign2/build.sh
@@ -3,12 +3,14 @@ set -euo pipefail
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 #sed -i.bak -e 's/-march=i686/-march=native/' ./source/Makefile
-cd ./src
+cd dialign_package/src
 
 mkdir -p $PREFIX/bin/
 mkdir -p $PREFIX/share
 mkdir -p $PREFIX/share/dialign2
+
 make
+
 mv -t $PREFIX/bin/ dialign2-2
-mv -t $PREFIX/share/dialign2 tp400_dna tp400_prot tp400_trans BLOSUM
+mv -t $PREFIX/share/dialign2 dialign2_dir/tp400_dna dialign2_dir/tp400_prot dialign2_dir/tp400_trans dialign2_dir/BLOSUM
 export DIALIGN2_DIR="$PREFIX/share/dialign2"
