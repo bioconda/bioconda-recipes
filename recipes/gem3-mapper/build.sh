5aaa38ebe20d3af7cce87e18625eb07d5ac9419e
Karl Nordström <karl.nordstrom@gmail.com>
2018-07-25 18:16:39 +0200

    gem3-mapper: changing target architecture away from native to x86-64

--- recipes/gem3-mapper/build.sh
+++ recipes/gem3-mapper/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#changing arch
 #adding include and library paths in order to find bzip2
 sed -i.bak '
     /^PATH_INCLUDE=/ s@$@ -I'$PREFIX'/include@
     /^PATH_LIB=/ s@$@ -L'$PREFIX'/lib@
+    /FLAGS_OPT=/ s@-march=native@-march=x86-64 -mtune=generic@
   ' Makefile.mk.in
 
 
