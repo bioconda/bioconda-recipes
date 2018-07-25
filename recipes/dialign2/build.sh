936e3a1c7098347e25c406f4694a1f8460e21a9f
jfallmann <fall@bioinf.uni-leipzig.de>
2018-07-25 13:16:04 +0200

    still working on paths and exports

--- recipes/dialign2/build.sh
+++ recipes/dialign2/build.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 #sed -i.bak -e 's/-march=i686/-march=native/' ./source/Makefile
-cd dialign_package/src
 
 mkdir -p $PREFIX/bin/
 mkdir -p $PREFIX/share
 mkdir -p $PREFIX/share/dialign2
 
+cd src
 make
 
 mv -t $PREFIX/bin/ dialign2-2
