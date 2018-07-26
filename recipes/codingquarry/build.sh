ab3588bf32b04a2902e4d6ae152374dc235fbdd1
Jon Palmer <nextgenusfs@gmail.com>
2018-07-26 09:18:53 -0400

    re-try codingquarry with updated makefile and ananconda compilers

--- recipes/codingquarry/build.sh
+++ recipes/codingquarry/build.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-make
+#modify makefile to use correct compiler c++
+sed -i.bak -e 's/\${CC}/${CXX}/g' -e 's/\${CFLAGS}/${CXXFLAGS}/g' makefile
+
+if [ "$(uname)" == Darwin ] ; then
+  CXXFLAGS="-Wl,-rpath ${PREFIX}/lib -L${PREFIX}/lib -fopenmp"
+else
+  CXXFLAGS="-fopenmp -g -O3"
+fi
+
+#now build
+make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
+
+#link
 mkdir -p $PREFIX/bin
 mv CodingQuarry $PREFIX/bin
 cp -R $SRC_DIR/QuarryFiles ${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}/QuarryFiles
 
+#required ENV variable
 mkdir -p ${PREFIX}/etc/conda/activate.d/
 echo "export QUARRYFILES=${PREFIX}/share/codingquarry/QuarryFiles" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
