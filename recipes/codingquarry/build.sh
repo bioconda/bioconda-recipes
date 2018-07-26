c94428221837e99073042acf4c7fae189cca2288
Jon Palmer <nextgenusfs@gmail.com>
2018-07-26 09:28:01 -0400

    update build to make folders if doesnt exist

--- recipes/codingquarry/build.sh
+++ recipes/codingquarry/build.sh
@@ -15,8 +15,13 @@ make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
 #link
 mkdir -p $PREFIX/bin
 mv CodingQuarry $PREFIX/bin
+mv CufflinksGTF_to_CodingQuarryGFF3.py $PREFIX/bin
+mv run_CQ-PM_mine.sh $PREFIX/bin
+mv run_CQ-PM_stranded.sh $PREFIX/bin
+mv run_CQ-PM_unstranded.sh $PREFIX/bin
+mkdir -p ${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}/QuarryFiles
 cp -R $SRC_DIR/QuarryFiles ${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}/QuarryFiles
 
 #required ENV variable
 mkdir -p ${PREFIX}/etc/conda/activate.d/
-echo "export QUARRYFILES=${PREFIX}/share/codingquarry/QuarryFiles" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
+echo "export QUARRYFILES=${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}/QuarryFiles" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
