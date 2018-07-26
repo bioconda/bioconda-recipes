52d581a238f510a1331f622ab883914ecfa0c783
ZhangYu <zhangy@sccas.cn>
2018-07-26 23:53:56 +0800

    add tinfo

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,5 +1,5 @@
 export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
-export LDFLAGS="-L$PREFIX/lib"
+export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 make
