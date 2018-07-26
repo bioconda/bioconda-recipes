8365df5a1dde35f2a3bbc02b95fe7bf51055676d
ZhangYu <zhangy@sccas.cn>
2018-07-26 22:06:01 +0800

    change include and lib

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,4 +1,4 @@
-export CFLAGS="-I$PREFIX/include"
+export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib"
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
