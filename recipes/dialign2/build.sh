4df33ddaf1e2f318b41bb13e1ab8e93fa2cec052
jfallmann <fall@bioinf.uni-leipzig.de>
2018-07-26 17:09:34 +0200

    Fixing sed command again

--- recipes/dialign2/build.sh
+++ recipes/dialign2/build.sh
@@ -6,7 +6,7 @@ mkdir -p $PREFIX/share
 mkdir -p $PREFIX/share/dialign2
 
 cd src
-sed -i.bak "s/strcpy \( dialign_dir , \"DIALIGN2_DIR\" \);/strcpy \( par_dir , ${PREFIX}\/share\/dialign2 \);/g" dialign.c
+sed -i.bak "s/strcpy ( dialign_dir , \"DIALIGN2_DIR\" );/strcpy ( par_dir , $PREFIX\/share\/dialign2 );/g" dialign.c
 make
 mv dialign2-2 $PREFIX/bin/dialign2-2
 
