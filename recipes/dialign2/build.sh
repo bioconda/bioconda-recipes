c04d9e12844ed08258c4e648b3f86a3e63226be2
jfallmann <fall@bioinf.uni-leipzig.de>
2018-07-26 17:00:28 +0200

    Added patch and sed workaround for env var DIALIGN2_DIR

--- recipes/dialign2/build.sh
+++ recipes/dialign2/build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-#export CPPFLAGS="-I$PREFIX/include"
-#export LDFLAGS="-L$PREFIX/lib"
 
 mkdir -p $PREFIX/bin/
 mkdir -p $PREFIX/share
 mkdir -p $PREFIX/share/dialign2
 
 cd src
+sed -i.bak "s/strcpy ( dialign_dir , \"DIALIGN2_DIR\" );/strcpy ( par_dir , ${PREFIX}\/share\/dialign2 );/g" dialign.c
 make
-mv dialign2-2 $PREFIX/bin/dialign2-2-bin
-echo "export DIALIGN2_DIR=${PREFIX}/share/dialign2 && dialign2-2-bin" > $PREFIX/bin/dialign2-2
+mv dialign2-2 $PREFIX/bin/dialign2-2
 
 cd ../dialign2_dir
 mv -t $PREFIX/share/dialign2 tp400_dna tp400_prot tp400_trans BLOSUM
