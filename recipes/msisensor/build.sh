1e6cc02c9bddba8280ae614501ab8965cd36e9ad
ZhangYu <zhangy@sccas.cn>
2018-07-26 10:02:14 +0800

    change LD_LIBRARY_PATH

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -1,11 +1,13 @@
+export LD_LIBRARY_PATH=${PREFIX}/lib
+
 wget "https://github.com/samtools/samtools/archive/0.1.19.tar.gz"
 tar xvf 0.1.19.tar.gz
 cd samtools-0.1.19
 make INCLUDES="-I. -I$PREFIX/include -I$PREFIX/include/ncurses" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo"
 cd ..
+
 export SAMTOOLS_DIR=samtools-0.1.19/
 export CPPFLAGS="-I$PREFIX/include -I$SAMTOOLS_DIR"
 export LIBS="-L$PREFIX/libi -L$SAMTOOLS_DIR"
-export LD_LIBRARY_PATH=${PREFIX}/lib
 make
 cp msisensor $PREFIX/bin
