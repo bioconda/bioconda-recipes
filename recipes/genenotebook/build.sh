ec7da9d83806ed9b2d1ac8bca08996d78198dac7
Rens Holmer <rens.holmer@wur.nl>
2018-07-27 10:30:52 +0200

    New symlink strategy

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-ln -s genenotebook ${PREFIX}/bin
+mkdir -p $PREFIX/bin/.genenotebook
+cp -r * $PREFIX/bin/.genenotebook
+ln -s $PREFIX/bin/genenotebook/genenotebook ${PREFIX}/bin
