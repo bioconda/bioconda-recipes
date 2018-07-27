b0de30e0994b5c8bb7cb53de7fddf5134c0a0e0b
Matt MacManes <matthew.macmanes@unh.edu>
2018-07-27 14:18:43 -0400

    update to new version (#10015)
    * update to new version
    
    the old version had a bug whereby some reads were dropped. See https://github.com/mourisl/Rcorrector/issues/12
    
    * remove recipe from blacklist
    
    * Update meta.yaml
    
    * try adding flags
    
    per comment from @jpuritz
    
    * fix makefile

--- recipes/rcorrector/build.sh
+++ recipes/rcorrector/build.sh
@@ -4,6 +4,10 @@ export C_INCLUDE_PATH=$PREFIX/include
 export OBJC_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=$PREFIX/include 
 export LD_LIBRARY_PATH=$PATH/lib:$LD_LIBRARY_PATH
+export CFLAGS="-I$PREFIX/include"
+export CXXFLAGS="-Wall -O3 -std=c++0x -L$PREFIX/lib"
+
+sed -i.bak 's/CXXFLAGS=.*//' Makefile
 
 make
 mkdir -p $PREFIX/bin
