3eb1fd05fc3052ee47bc2211e55a074efe51e106
qyou <youqi@yzu.edu.cn>
2018-07-27 15:43:02 +0800

    add build git

---
+++ recipes/genometools-genometools/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e -o pipefail -x
+
+CAIRO_OPT=
+if [[ "$(uname)" == Darwin ]]; then
+		CAIRO_OPT='cairo=no'
+fi
+
+make ${CAIRO_OPT}
+export prefix=$PREFIX
+make ${CAIRO_OPT} install 
+
+cd gtpython
+$PYTHON setup.py install
