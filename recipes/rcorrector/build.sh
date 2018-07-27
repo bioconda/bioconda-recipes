70ba8b8bc0969e4b65a8842e435bdc1928776a2d
jpuritz <jpuritz@gmail.com>
2018-07-27 14:07:54 -0400

    fix makefile

--- recipes/rcorrector/build.sh
+++ recipes/rcorrector/build.sh
@@ -5,7 +5,9 @@ export OBJC_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=$PREFIX/include 
 export LD_LIBRARY_PATH=$PATH/lib:$LD_LIBRARY_PATH
 export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CXXFLAGS="-Wall -O3 -std=c++0x -L$PREFIX/lib"
+
+sed -i.bak 's/CXXFLAGS=.*//' Makefile
 
 make
 mkdir -p $PREFIX/bin
