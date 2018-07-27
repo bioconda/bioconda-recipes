3ae10cfe84187a81f569c0db94fe07c91bc6351a
Matt MacManes <matthew.macmanes@unh.edu>
2018-07-27 07:46:29 -0400

    try adding flags
    per comment from @jpuritz

--- recipes/rcorrector/build.sh
+++ recipes/rcorrector/build.sh
@@ -4,6 +4,8 @@ export C_INCLUDE_PATH=$PREFIX/include
 export OBJC_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=$PREFIX/include 
 export LD_LIBRARY_PATH=$PATH/lib:$LD_LIBRARY_PATH
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 
 make
 mkdir -p $PREFIX/bin
