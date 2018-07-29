2839a93ac86b9c5258915c1181d7f248ac297d33
Devon Ryan <dpryan79@gmail.com>
2018-07-29 21:44:06 +0200

    Remove packages from the blacklist

--- recipes/fermi/build.sh
+++ recipes/fermi/build.sh
@@ -2,6 +2,8 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 mkdir -p $PREFIX/bin
 
