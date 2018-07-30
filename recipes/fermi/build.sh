6231c815326a1b6ce16d0bf5c3566e14002367d6
Devon Ryan <dpryan79@gmail.com>
2018-07-30 08:59:59 +0200

    Ah, only gnu assumes inline functions are static

--- recipes/fermi/build.sh
+++ recipes/fermi/build.sh
@@ -6,6 +6,7 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 mkdir -p $PREFIX/bin
+sed -i.bak "s/inline void ld_set/static inline void ld_set/g" bcr.c
 
 make CC=$CC
 mv fermi  $PREFIX/bin
