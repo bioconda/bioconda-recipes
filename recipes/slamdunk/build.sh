4824c5e36a7bf51978af7c3af558d9166931b29e
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-30 22:47:07 +0200

    conda build checking.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -4,11 +4,15 @@ pip install -r requirements.txt
 mkdir -p slamdunk/plot/Rslamdunk
 export R_LIBS_SITE=${PREFIX}/slamdunk/plot/Rslamdunk
 echo $R_LIBS_SITE
+echo "PREFIX"
 echo $PREFIX
 ls $PREFIX
+echo "RECIPE_DIR"
 echo $RECIPE_DIR
+echo "SP_DIR"
 echo $SP_DIR
 ls $SP_DIR
+echo "SRC_DIR"
 echo $SRC_DIR
 ls $SRC_DIR
 #R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
