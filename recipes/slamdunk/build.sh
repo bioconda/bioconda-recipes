474750b61a5967f7c03600354727c8f5001cde0b
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-30 23:48:08 +0200

    path extensions.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+echo "PWD"
+echo $PWD
 mkdir -p slamdunk/plot/Rslamdunk
 export R_LIBS_SITE=${PREFIX}/slamdunk/plot/Rslamdunk
 echo $R_LIBS_SITE
@@ -15,10 +17,11 @@ ls $SP_DIR
 echo "SRC_DIR"
 echo $SRC_DIR
 ls $SRC_DIR
-#R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
+#$R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
 cd slamdunk/contrib
 ./build-ngm.sh
 ./build-varscan.sh
 ./build-samtools.sh
 ./build-rnaseqreadsimulator.sh
-
+echo "CONTENTS"
+ls -lah *
