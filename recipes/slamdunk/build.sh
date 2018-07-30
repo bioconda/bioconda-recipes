3ab535de9b4477bfd4f25ed05d73fcf1bf9cf81e
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-30 22:25:12 +0200

    wget dependency added.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -5,9 +5,12 @@ mkdir -p slamdunk/plot/Rslamdunk
 export R_LIBS_SITE=${PREFIX}/slamdunk/plot/Rslamdunk
 echo $R_LIBS_SITE
 echo $PREFIX
+ls $PREFIX
 echo $RECIPE_DIR
 echo $SP_DIR
+ls $SP_DIR
 echo $SRC_DIR
+ls $SRC_DIR
 #R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
 cd slamdunk/contrib
 ./build-ngm.sh
