f443d23e2407b051426568a0a7bf87ac3c113941
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-30 22:06:41 +0200

    PREFIX removed from recipe.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-mkdir -p ${PREFIX}/slamdunk/plot/Rslamdunk
+mkdir -p slamdunk/plot/Rslamdunk
 export R_LIBS_SITE=${PREFIX}/slamdunk/plot/Rslamdunk
+echo $R_LIBS_SITE
+echo $PREFIX
+echo $RECIPE_DIR
+echo $SP_DIR
+echo $SRC_DIR
 #R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
-cd ${PREFIX}/slamdunk/contrib
+cd slamdunk/contrib
 ./build-ngm.sh
 ./build-varscan.sh
 ./build-samtools.sh
