598fc0c1eec504b39127edd3e298ebc6618afbab
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-30 21:32:41 +0200

    Java-jdk -> Openjdk.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -3,7 +3,7 @@
 pip install -r requirements.txt
 mkdir -p ${PREFIX}/slamdunk/plot/Rslamdunk
 export R_LIBS_SITE=${PREFIX}/slamdunk/plot/Rslamdunk
-R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
+#R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("${PREFIX}/slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
 cd ${PREFIX}/slamdunk/contrib
 ./build-ngm.sh
 ./build-varscan.sh
