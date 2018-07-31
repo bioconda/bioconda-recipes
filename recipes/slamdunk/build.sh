5512e02da25c400f3b27c03dff2c2d9663a2052c
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-31 09:34:46 +0200

    samtools dependencies added

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-pip install .
-#pip install -r requirements.txt
-#echo "PWD"
-#echo $PWD
-#mkdir -p slamdunk/plot/Rslamdunk
+pip install -r requirements.txt
+echo "PWD"
+echo $PWD
+mkdir -p slamdunk/plot/Rslamdunk
 #export R_LIBS_SITE=slamdunk/plot/Rslamdunk
 #$R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
-#cd slamdunk/contrib
-#echo "BUILDINGGGGGGGGGGGGGGGG"
-#./build-ngm.sh
-#./build-varscan.sh
+cd slamdunk/contrib
+echo "BUILDINGGGGGGGGGGGGGGGG"
+./build-ngm.sh
+./build-varscan.sh
 #./build-samtools.sh
 #./build-rnaseqreadsimulator.sh
