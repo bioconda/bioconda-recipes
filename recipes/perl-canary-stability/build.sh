dd52601ccdbf0ea227a294d18ea122b6027af877
Andreas Sjödin <andreas.sjodin@gmail.com>
2018-07-31 22:13:31 +0200

    Bump perl-canary-stability

--- recipes/perl-canary-stability/build.sh
+++ recipes/perl-canary-stability/build.sh
@@ -3,15 +3,15 @@
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    ./Build test
+    perl ./Build
+    perl ./Build test
     # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
-    make
-    make test
+    PERL_CANARY_STABILITY_NOPROMPT=1 perl Makefile.PL INSTALLDIRS=site
+    PERL_CANARY_STABILITY_NOPROMPT=1 make
+    PERL_CANARY_STABILITY_NOPROMPT=1 make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
