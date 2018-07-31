2f61e64482c50e7dc541378ba3bc5e86dddb022e
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-07-31 09:21:04 +0200

    pip install switch

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-pip install -r requirements.txt
-echo "PWD"
-echo $PWD
-mkdir -p slamdunk/plot/Rslamdunk
-export R_LIBS_SITE=slamdunk/plot/Rslamdunk
-$R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
-cd slamdunk/contrib
-echo "BUILDINGGGGGGGGGGGGGGGG"
-./build-ngm.sh
-./build-varscan.sh
-./build-samtools.sh
-./build-rnaseqreadsimulator.sh
+pip install .
+#pip install -r requirements.txt
+#echo "PWD"
+#echo $PWD
+#mkdir -p slamdunk/plot/Rslamdunk
+#export R_LIBS_SITE=slamdunk/plot/Rslamdunk
+#$R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
+#cd slamdunk/contrib
+#echo "BUILDINGGGGGGGGGGGGGGGG"
+#./build-ngm.sh
+#./build-varscan.sh
+#./build-samtools.sh
+#./build-rnaseqreadsimulator.sh
