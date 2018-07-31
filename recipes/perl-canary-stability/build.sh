65d55ad5f03ae1e1218a8001ff0df0400de713e9
Andreas Sjödin <andreas.sjodin@gmail.com>
2018-07-31 22:58:09 +0200

    Small fixes

--- recipes/perl-canary-stability/build.sh
+++ recipes/perl-canary-stability/build.sh
@@ -9,9 +9,9 @@ if [ -f Build.PL ]; then
     perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
-    PERL_CANARY_STABILITY_NOPROMPT=1 perl Makefile.PL INSTALLDIRS=site
-    PERL_CANARY_STABILITY_NOPROMPT=1 make
-    PERL_CANARY_STABILITY_NOPROMPT=1 make test
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
