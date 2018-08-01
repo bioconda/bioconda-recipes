f085d886cbcba1ce6c01690b4196021ee7556787
Tobias Neumann <tobias.neumann.at@gmail.com>
2018-08-02 00:19:10 +0200

    Slamdunk v0.3.1 bioconda build 0.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 python setup.py install --single-version-externally-managed --record=record.txt
-#mkdir -p slamdunk/plot/Rslamdunk
-#export R_LIBS_SITE=slamdunk/plot/Rslamdunk
-#$R --vanilla -e 'libLoc = .libPaths()[grep("Rslamdunk",.libPaths())]; source("slamdunk/plot/checkLibraries.R"); checkLib(libLoc)'
-#cd slamdunk/contrib
-#./build-ngm.sh
-#./build-varscan.sh
-#./build-samtools.sh
-#./build-rnaseqreadsimulator.sh
