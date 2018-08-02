d2a51fbf2a227319b3f377621e324a348682dc27
Ilya Shlyakhter <ilya@broadinstitute.org>
2018-08-01 22:42:05 -0400

    changed tests to what blast does

---
+++ recipes/bbmap/run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# adapted from recipes/blast/run_test.sh
+
+TMPDIR=$(mktemp -d)
+trap "rm -rf $TMPDIR" 0 INT QUIT ABRT PIPE TERM
+
+cp test*.fa $TMPDIR
+cd $TMPDIR
+
+bbmap.sh in=test.fa ref=testdatabase.fa out=result.sam jni=t nodisk
+diff <(tail -2 result.sam) expected.sam
+
