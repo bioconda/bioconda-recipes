2d9052fd7668d8cc195e28b601a6938c6cad7453
Rens Holmer <rens.holmer@wur.nl>
2018-08-02 15:42:04 +0200

    Build from prebundled tarball

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin/.genenotebook
-cp -r * $PREFIX/bin/.genenotebook
-ln -s $PREFIX/bin/genenotebook/genenotebook ${PREFIX}/bin
+set -exo pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir $PREFIX/bin
+ls -l .
+cp -R * $outdir
+ln -s $outdir/genenotebook $PREFIX/bin/genenotebook
+#npm install --unsafe-perm --force
+#ls -l .
+#ls -l genenotebook_v$PKG_VERSION/
+#cp -R genenotebook_v$PKG_VERSION/* $outdir/
+#ls -l $outdir
+#ln -s $outdir/genenotebook $PREFIX/bin
+#chmod 0755 "${PREFIX}/bin/genenotebook"
