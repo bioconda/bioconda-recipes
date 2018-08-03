5eb48fc4c3cdca3585a706b3528e97fd3ba64a71
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-08-03 13:41:15 +0200

    fix find command

--- recipes/trinotate/build.sh
+++ recipes/trinotate/build.sh
@@ -8,7 +8,7 @@ mv util/auto_Trinotate.txt util/auto_Trinotate.sh
 cp -rf PerlLib perl-build/lib
 cp -rf sample_data ${PREFIX}/trinotate-sample-data
 
-find -name "*.pl" | grep -v sample_data | xargs -I {} cp -rf {} perl-build/scripts/
+find . -name "*.pl" | grep -v sample_data | xargs -I {} cp -rf {} perl-build/scripts/
 cp Trinotate perl-build/scripts
 
 cd perl-build
