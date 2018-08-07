1193fc48b941f66e2067e8d33d5bb2920ea6a370
Chienchi <chienchi@lanl.gov>
2018-08-07 15:23:35 -0600

    provide precomplied binary for MacOSX

--- recipes/faqcs/build.sh
+++ recipes/faqcs/build.sh
@@ -3,6 +3,13 @@
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-make
-mkdir -p $PREFIX/bin
-cp FaQCs $PREFIX/bin
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Installing FaQCs for OSX."
+    mkdir -p $PREFIX/bin
+    cp bin/MacOSX_x86_64/* $PREFIX/bin
+else 
+    echo "Installing FaQCs for UNIX/Linux."
+    make
+    mkdir -p $PREFIX/bin
+    cp FaQCs $PREFIX/bin
+fi
