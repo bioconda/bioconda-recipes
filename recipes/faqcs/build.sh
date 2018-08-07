4b6f6bcecf84a68028f332e4013474958cfa5a4c
Chienchi <chienchi@lanl.gov>
2018-08-07 17:11:49 -0600

    make OPENMP=""

--- recipes/faqcs/build.sh
+++ recipes/faqcs/build.sh
@@ -3,13 +3,14 @@
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+mkdir -p $PREFIX/bin
+
 if [ "$(uname)" == "Darwin" ]; then
     echo "Installing FaQCs for OSX."
-    mkdir -p $PREFIX/bin
-    cp bin/MacOSX_x86_64/* $PREFIX/bin
+    make OPENMP=""
+    cp FaQCs $PREFIX/bin
 else 
     echo "Installing FaQCs for UNIX/Linux."
     make
-    mkdir -p $PREFIX/bin
     cp FaQCs $PREFIX/bin
 fi
