b0fdf1d939e3476b219dcf509e8edd44fc96f263
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-08-08 11:03:32 +0200

    fix

--- recipes/perl-lwp-protocol-https/build.sh
+++ recipes/perl-lwp-protocol-https/build.sh
@@ -6,14 +6,14 @@ export OPENSSL_PREFIX=$PREFIX
 if [ -f Build.PL ]; then
     perl Build.PL
     ./Build
-    #./Build test  # Disabled as it doesn't work on circleci but works locally (network problem probably)
+    ./Build test
     # Make sure this goes in site
     ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
-    make test
+    make test  # Disabled as it doesn't work on circleci but works locally (network problem probably)
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
