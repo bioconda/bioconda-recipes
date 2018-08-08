87571d8d9c0dc8b145bc9f37b10ce95b83646df3
Marcel Bargull <marcel.bargull@udo.edu>
2018-08-08 23:30:16 +0200

    lumpy-sv: also export LIBRARY_PATH...

--- recipes/lumpy-sv/build.sh
+++ recipes/lumpy-sv/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# The project's Makefiles don't use {CPP,C,CXX,LD}FLAGS everywhere.
+# We can try to patch all of those or export the following *_PATH variables.
 export C_INCLUDE_PATH="${PREFIX}/include"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
