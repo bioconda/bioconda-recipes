f9e9ba6b18b65347cfedc9a4cad2028739b2bb0b
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-08-08 15:42:32 +0200

    fix versions

--- recipes/perl-bioperl-run/run_test.pl
+++ recipes/perl-bioperl-run/run_test.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/env perl
 
 my @items = (
-    ["Bio::Tools::Run::Bowtie", undef],
-    ["Bio::Tools::Run::BWA", undef],
-    ["Bio::Tools::Run::EMBOSSApplication", undef],
-    ["Bio::Tools::Run::Hmmer", undef],
-    ["Bio::Tools::Run::Samtools", undef],
-    ["Bio::Tools::Run::StandAloneBlastPlus", undef],
+    ["Bio::Tools::Run::Bowtie", '1.007002'],
+    ["Bio::Tools::Run::BWA", '1.007002'],
+    ["Bio::Tools::Run::EMBOSSApplication", '1.007002'],
+    ["Bio::Tools::Run::Hmmer", '1.007002'],
+    ["Bio::Tools::Run::Samtools", '1.007002'],
+    ["Bio::Tools::Run::StandAloneBlastPlus", '1.007002'],
 );
 
 foreach $item (@items) {
