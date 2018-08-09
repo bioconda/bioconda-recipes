6d5485bab32865385dedc465adbff08754217ebc
Alexey Gurevich <alexeigurevich@gmail.com>
2018-08-09 18:52:47 +0300

    Update to QUAST 5.0.0

--- recipes/quast/build.sh
+++ recipes/quast/build.sh
@@ -3,12 +3,6 @@
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-export BOOST_INCLUDE_DIR=${PREFIX}/include
-export BOOST_LIBRARY_DIR=${PREFIX}/lib
-
-export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
-export LDFLAGS="-L${BOOST_LIBRARY_DIR}"
-export CFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
 
 BINARY_HOME=$PREFIX/bin
 QUAST_HOME=$PREFIX/opt/quast-$PKG_VERSION
@@ -18,7 +12,7 @@ mkdir -p $QUAST_HOME
 
 python "setup.py" install
 
-cp -R $SRC_DIR/*quast.py $QUAST_HOME/
+cp -R $SRC_DIR/*quast*.py $QUAST_HOME/
 cp -R $SRC_DIR/icarus.py $QUAST_HOME/
 
 lib_path=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
