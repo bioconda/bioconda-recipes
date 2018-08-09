7fda8381bff2364a69482e94e2cdf2b886f091ad
Marcel Bargull <marcel.bargull@udo.edu>
2018-08-09 13:39:50 +0200

    spalter: changes for conda-build 3 (^_^)

--- recipes/spalter/build.sh
+++ recipes/spalter/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
 
-# circumvent a bug in conda-build >=2.1.18,<3.0.10
-# https://github.com/conda/conda-build/issues/2255
-[[ -z $REQUESTS_CA_BUNDLE && ${REQUESTS_CA_BUNDLE+x} ]] && unset REQUESTS_CA_BUNDLE
-[[ -z $SSL_CERT_FILE && ${SSL_CERT_FILE+x} ]] && unset SSL_CERT_FILE
-
 # build statically linked binary with Rust
 cargo install --root "${PREFIX}"
