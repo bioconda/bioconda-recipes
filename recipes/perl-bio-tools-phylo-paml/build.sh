9495ea943d75841bf124409ea11cbb606440f486
Anthony Bretaudeau <abretaud@irisa.fr>
2018-08-09 09:09:48 +0200

    Unblacklist bioperl (#10325)
    * fix build
    
    * add missing deps
    
    * update bioperl version
    
    * 3 modules which were moved out of bioperl-run in the latest version
    
    * fix cyclic dep
    
    * move out bio-coordinate + adapt tests everywhere
    
    * unblacklist
    
    * break the (vicious) cycle
    
    * fix tests
    
    * missing dep
    
    * fix versions
    
    * disable temporarily
    
    * disable temporarily
    
    * typos
    
    * fix
    
    * disable this too for now
    
    * disable temp
    
    * good old mac permission problem
    
    * rebuild this one too
    
    * another one
    
    * bump
    
    * bump
    
    * bump
    
    * another one
    
    * bump
    
    * bump
    
    * bump
    
    * bump

--- recipes/perl-bio-tools-phylo-paml/build.sh
+++ recipes/perl-bio-tools-phylo-paml/build.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# Make sure pipes to tee don't hide configuration or test failures
-set -o pipefail
-
-export C_INCLUDE_PATH=${PREFIX}/include
-
-# Tell the build system where to find samtools
-export SAMTOOLS="${PREFIX}"
-
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
-    perl Build.PL 2>&1 | tee configure.log
+    perl Build.PL
     perl ./Build
-    perl ./Build test 2>&1 | tee tests.log
+    perl ./Build test
     # Make sure this goes in site
     perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
-    make test 2>&1
+    make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
