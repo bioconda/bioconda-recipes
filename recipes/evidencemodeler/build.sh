317cdc95463af8dc496fe2435379f1add8796eca
Jon Palmer <nextgenusfs@gmail.com>
2018-08-12 16:33:52 -0700

    build for evidencemodeler

--- recipes/evidencemodeler/build.sh
+++ recipes/evidencemodeler/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-EVM_HOME=$PREFIX/opt/evidencemodeler-$PKG_VERSION
+EVM_HOME=$PREFIX/opt/${PKG_NAME}-${PKG_VERSION}
 
 mkdir -p $EVM_HOME
 
 cp -R $SRC_DIR/EvmUtils $EVM_HOME/EvmUtils
 cp -R $SRC_DIR/PerlLib $FEVM_HOME/PerlLib
 ln -s $EVM_HOME/evidence_modeler.pl $PREFIX/bin/evidence_modeler.pl
+
+#required ENV variable
+mkdir -p ${PREFIX}/etc/conda/activate.d/
+echo "export EVM_HOME=${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
