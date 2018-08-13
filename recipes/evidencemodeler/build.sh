1af2c9a3ac02a727da2ae9e99971f4e4f95ef1f7
Jon Palmer <nextgenusfs@gmail.com>
2018-08-12 18:02:54 -0700

    test is still failing

--- recipes/evidencemodeler/build.sh
+++ recipes/evidencemodeler/build.sh
@@ -8,4 +8,4 @@ cp -R $SRC_DIR/ $EVM_HOME/
 
 #required ENV variable
 mkdir -p ${PREFIX}/etc/conda/activate.d/
-echo "export EVM_HOME=${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
+echo "export EVM_HOME=${EVM_HOME}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
