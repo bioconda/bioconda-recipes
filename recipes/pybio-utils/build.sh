07adf1a2fa625e19eca965aee356130f4560aae9
RickGelhausen <rick.gelhausen@gmail.com>
2018-08-13 09:27:44 +0200

    Minor changes, trying to get pymisc-utils working

--- recipes/pybio-utils/build.sh
+++ recipes/pybio-utils/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sed -i 's/0.2.8/0.2.10/' requirements.txt
 $PYTHON setup.py install
 cp bio_utils/*.py ${PREFIX}/bin/
 cp bio_utils/bio_programs/*.py ${PREFIX}/bin/
 cp bio_utils/bio_programs/plotting/*.py ${PREFIX}/bin/
 cp tests/*.py ${PREFIX}/bin/
-sed -i 's/misc/pymisc-utils/' ${PREFIX}/bin/*.py
+
+sed -i 's/misc/pymisc_utils/' ${PREFIX}/bin/*.py
+sed -i '0,/import pymisc_utils/ s/import pymisc_utils/pymisc_utils = __import__("pymisc-utils")\nimport pymisc_utils/' ${PREFIX}/bin/*.py
+
 chmod 755 ${PREFIX}/bin/*.py
