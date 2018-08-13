4f04862a52fa28b4b85e0bd6dc611eb0911fdf7f
Jon Palmer <nextgenusfs@gmail.com>
2018-08-12 18:25:17 -0700

    test is still failing, try explicitly copying files over2

--- recipes/evidencemodeler/build.sh
+++ recipes/evidencemodeler/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-EVM_HOME=$PREFIX/opt/${PKG_NAME}-${PKG_VERSION}
+set -o errexit -o nounset
 
-mkdir -p $EVM_HOME
+readonly EVM_HOME=${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}
 
-cp -R $SRC_DIR/simple_example $EVM_HOME/simple_example
-cp -R $SRC_DIR/PerlLib $EVM_HOME/PerlLib
-cp -R $SRC_DIR/EvmUtil $EVM_HOME/EvmUtils
-cp $SRC_DIR/evidence_modeler.pl $EVM_HOME/evidence_modeler.pl
+mkdir -p ${EVM_HOME}
+
+cp -Rp simple_example PerlLib EvmUtils evidence_modeler.pl ${EVM_HOME}
 
 #required ENV variable
 mkdir -p ${PREFIX}/etc/conda/activate.d/
 echo "export EVM_HOME=${EVM_HOME}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
+
+mkdir -p ${PREFIX}/etc/conda/deactivate.d/
+echo "unset EVM_HOME" > ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}-${PKG_VERSION}.sh
