044c24cb89baa8454cfba98939db5113be629b39
Jon Palmer <nextgenusfs@gmail.com>
2018-08-12 18:12:37 -0700

    test is still failing, try explicitly copying files over

--- recipes/evidencemodeler/build.sh
+++ recipes/evidencemodeler/build.sh
@@ -4,7 +4,10 @@ EVM_HOME=$PREFIX/opt/${PKG_NAME}-${PKG_VERSION}
 
 mkdir -p $EVM_HOME
 
-cp -R $SRC_DIR/ $EVM_HOME/
+cp -R $SRC_DIR/simple_example $EVM_HOME/simple_example
+cp -R $SRC_DIR/PerlLib $EVM_HOME/PerlLib
+cp -R $SRC_DIR/EvmUtil $EVM_HOME/EvmUtils
+cp $SRC_DIR/evidence_modeler.pl $EVM_HOME/evidence_modeler.pl
 
 #required ENV variable
 mkdir -p ${PREFIX}/etc/conda/activate.d/
