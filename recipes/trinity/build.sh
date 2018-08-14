3a04d063d19856e09d553ca502b9e266cf085717
brianjohnhaas <brianjohnhaas@gmail.com>
2018-08-14 13:53:38 +0000

    bioconda updates for v2.8.1

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -30,21 +30,8 @@ cd $TRINITY_HOME && chmod +x Trinity
 
 # add link to Trinity from bin so in PATH 
 cd $BINARY_HOME
-ln -s $TRINITY_HOME/Trinity
-ln -s $TRINITY_HOME/trinity-plugins/BIN/* .
-
-# add links to regularly used tools:
-ln -s $TRINITY_HOME/util/*.pl .
-ln -s $TRINITY_HOME/Analysis/DifferentialExpression/PtR
-ln -s $TRINITY_HOME/Analysis/DifferentialExpression/run_DE_analysis.pl
-ln -s $TRINITY_HOME/Analysis/DifferentialExpression/analyze_diff_expr.pl
-ln -s $TRINITY_HOME/Analysis/DifferentialExpression/define_clusters_by_cutting_tree.pl
-ln -s $TRINITY_HOME/Analysis/SuperTranscripts/Trinity_gene_splice_modeler.py
-ln -s $TRINITY_HOME/Analysis/SuperTranscripts/extract_supertranscript_from_reference.py
-ln -s $TRINITY_HOME/util/support_scripts/get_Trinity_gene_to_trans_map.pl
-ln -s $TRINITY_HOME/util/misc/contig_ExN50_statistic.pl
-
+ln -sf $TRINITY_HOME/Trinity
 
 # make it easier to find TRINITY_HOME
-ln -s $TRINITY_HOME $PREFIX/opt/TRINITY_HOME
+ln -sf $TRINITY_HOME $PREFIX/opt/TRINITY_HOME
 
--- recipes/trinity/run_test.sh
+++ recipes/trinity/run_test.sh
@@ -6,4 +6,4 @@ ${CONDA_PREFIX}/opt/trinity-*/Inchworm/bin/inchworm --help &>/dev/null || [ $? -
 ${CONDA_PREFIX}/opt/trinity-*/Chrysalis/bin/Chrysalis --help &>/dev/null || [ $? -eq 255 ]
 ${CONDA_PREFIX}/opt/trinity-*/Chrysalis/bin/QuantifyGraph --help &>/dev/null || [ $? -eq 255 ]
 ${CONDA_PREFIX}/opt/trinity-*/Chrysalis/bin/ReadsToTranscripts --help &>/dev/null || [ $? -eq 255 ]
-${CONDA_PREFIX}/bin/ParaFly --help &>/dev/null || [ $? -eq 1 ]
+${CONDA_PREFIX}/opt/trinity-*/trinity-plugins/BIN/ParaFly --help &>/dev/null || [ $? -eq 1 ]
