7a787fb3a9c8c725fb17bd00d18b66a8d9976b1c
Andreas Sjödin <andreas.sjodin@gmail.com>
2018-08-19 22:40:20 +0200

    Bump perl-module-metadata (#10529)

--- recipes/perl-module-metadata/build.sh
+++ recipes/perl-module-metadata/build.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-set -x -e
-
-export CC="${PREFIX}/bin/gcc"
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-export LDFLAGS="-L${PREFIX}/lib"
-export CXXFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I${PREFIX}/include"
-export LC_ALL=C
-
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    ./Build test
+    perl ./Build
+    perl ./Build test
     # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
