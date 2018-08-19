2fdcc189bffe77826713935464657db403ccdc0a
jmoore <josh@glencoesoftware.com>
2018-08-19 12:24:48 +0200

    Add showit

---
+++ recipes/starfish/showit/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+from os import listdir
+
+pyfiles = [f.replace('.py', '') for f in listdir('.') if f.endswith('.py')]
+setup(
+    name='showit',
+    version='1.1.4',
+    description='simple and sensible display of images in python',
+    url='https://github.com/freeman-lab/showit',
+    packages=find_packages(), py_modules=pyfiles)
