168498b26ba06d17952b117d433022381f092c86
Peter van Heusden <pvh@sanbi.ac.za>
2018-08-20 22:10:58 +0200

    Add fallback for $AR to ar

--- recipes/spaln/build.sh
+++ recipes/spaln/build.sh
@@ -1,6 +1,6 @@
 (cd src && \
 bash configure --exec_prefix=$PREFIX/bin --table_dir=$PREFIX/share/spaln/table --alndbs_dir=$PREFIX/share/spaln/alndbs --use_zlib=0 && \
-make AR="$AR rc" && make install)
+make AR="${AR:-ar} rc" && make install)
 
 (cd perl && perl Makefile.PL && \
 sed -i -s '1s^#!/usr/bin/perl^#!/usr/bin/env perl^' *.pl && make && make install && chmod u+w $PREFIX/bin/*.pl
