4bfc53a1578fdde45e0f9eeb08ce38a9ff674723
jmoore <josh@glencoesoftware.com>
2018-08-20 20:14:47 +0200

    bump to 0.0.1

--- recipes/slicedimage/setup.py
+++ recipes/slicedimage/setup.py
@@ -6,7 +6,7 @@ from os import listdir
 pyfiles = [f.replace('.py', '') for f in listdir('.') if f.endswith('.py')]
 setup(
     name='slicedimage',
-    version='0.0.24-dev',
+    version='0.0.1',
     description='sliced imaging data',
     url='https://github.com/spacetx/slicedimage',
     packages=find_packages(), py_modules=pyfiles)
