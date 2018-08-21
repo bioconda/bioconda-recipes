b3ba67f5d1e1e6c5834bc769a5dd297c34740559
Dan Sun <dan.sun@agresearch.co.nz>
2018-08-22 11:45:18 +1200

    masurca: recipe improvements.
             Removed source downloading, verifying and extracting
             logics to meta.yaml. New verison of conda-build can
             handle path prefixed with "/" now.

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -9,12 +9,4 @@ export PERL_EXT_LDFLAGS="-shared -O2 -L/usr/local/lib -fstack-protector"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPATH=${PREFIX}/include
 
-URL="https://github.com/alekseyzimin/masurca/releases/download/${PKG_VERSION}/MaSuRCA-${PKG_VERSION}.tar.gz"
-SHA256='3d3df9276d221551fd190cad3d037d56ce70691592cb28198e7bffc7c07760b2'
-TAR_FILE="${PKG_NAME}-${PKG_VERSION}.tar.gz"
-curl -Lo "${TAR_FILE}" "${URL}"
-openssl sha256 "${TAR_FILE}" | grep -q "${SHA256}"
-tar -xzf "${TAR_FILE}" --strip-components=1
-rm "${TAR_FILE}"
-
 ./install.sh
