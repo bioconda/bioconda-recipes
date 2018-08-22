066e71d1d6c951bf43b386d19834a4a4a2e0f031
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-08-22 16:06:55 +0200

    adapt build.sh

--- recipes/somaticseq/build.sh
+++ recipes/somaticseq/build.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-prog_dir=$PREFIX/opt/SomaticSeq
+mkdir -p $PREFIX/opt/SomaticSeq
+mv ./* $PREFIX/opt/SomaticSeq/
 
-mkdir -p $prog_dir
-mv ./* $prog_dir
-
-cd $prog_dir
+cd $PREFIX/opt/SomaticSeq
 for script in *.sh *.py
 do
-  ln -s $prog_dir/$script $PREFIX/bin/$script
+  ln -s ../opt/SomaticSeq/$script $PREFIX/bin/$script
 done
 
 mkdir $PREFIX/bin/utilities
-for file in $prog_dir/utilities/*
+cd $PREFIX/bin/utilities
+for file in *
 do
-  ln -s $prog_dir/utilities/$file $PREFIX/bin/utilities/$file
+  ln -s ../../opt/SomaticSeq/utilities/$file $PREFIX/bin/utilities/$file
 done
 
 mkdir $PREFIX/bin/r_scripts
 for file in $prog_dir/r_scripts/*
 do
-  ln -s $prog_dir/r_sripts/$file $PREFIX/bin/r_scripts/$file
+  ln -s ../../opt/SomaticSeq/r_sripts/$file $PREFIX/bin/r_scripts/$file
 done
