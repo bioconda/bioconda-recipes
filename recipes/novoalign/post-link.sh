15a736469ca432c216cd72495a669659da2dd3cc
Ilya Shlyakhter <ilya@broadinstitute.org>
2018-08-22 14:41:19 -0400

    added printing of msgs to $PREFIX/.messages.txt

--- recipes/novoalign/post-link.sh
+++ recipes/novoalign/post-link.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-echo 'Novoalign is installed.  Commercial use requires a license; contact sales@novocraft.com.  License also adds'
-echo 'multi-threading and other features.'
+MSGS=$PREFIX/.messages.txt
+touch $MSGS
+echo 'Novoalign is installed.  Commercial use requires a license; contact sales@novocraft.com.  License also adds' >> $MSGS
+echo 'multi-threading and other features.' >> $MSGS
 
-echo 'Once you have a license file, run novoalign-license-register to install it.'
+echo 'Once you have a license file, run novoalign-license-register to install it.' >> $MSGS
 
 
-echo 'Licensing information from Novocraft website:'
-echo
+echo 'Licensing information from Novocraft website:' >> $MSGS
+echo >> $MSGS
+echo '-----------------------------------------------' >> $MSGS
 # Novocraft normally does not permit distribution of novoalign to third parties.
 # They made an exception for Bioconda (and Homebrew), with the condition that
 # each user installation through Bioconda trigger an access to Novocraft's website,
 # so they can track commercial users.
-curl -L http://www.novocraft.com/bioconda/license.txt
+curl -L http://www.novocraft.com/bioconda/license.txt >> $MSGS 2>&1
+echo '-----------------------------------------------' >> $MSGS
