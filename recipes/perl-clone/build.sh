9c38e29cbc8d2c4003e4bbd6f4e0b003d0aed81c
Felix Kühnl <xileF1337@users.noreply.github.com>
2018-08-26 14:05:16 +0200

    Added recipe for Perl package perl-clone. (#10689)

--- recipes/perl-clone/build.sh
+++ recipes/perl-clone/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    ./Build test
-    # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build
+    perl ./Build test
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
-    # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
     make test
@@ -17,3 +14,4 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
+
