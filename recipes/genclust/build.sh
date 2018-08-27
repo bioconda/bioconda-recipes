d048e38fb4c1c991ac685e03c085af12ccce3206
reneechou123 <reneechou123@gmail.com>
2018-08-27 09:50:05 -0400

    Update build.sh

--- recipes/genclust/build.sh
+++ recipes/genclust/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # comment out random seed function srand() on line 454 for reproducibility
-sed -i.bak '454 s|^| //|' $PREFIX/bin/genclustlb.c
+sed -i.bak '454 s|^| //|' genclustlb.c
 
 make
 mkdir -p $PREFIX/bin
