7121edd5fd8a67dd7c8568e895e40cbfe1d6c8c0
Akintunde Emiola <akintunde.emiola@jax.org>
2018-08-29 10:22:33 -0400

    edit build.sh

--- recipes/smeg/build.sh
+++ recipes/smeg/build.sh
@@ -5,18 +5,14 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
-export CXX=${PREFIX}/bin/g++
-
 cd $SRC_DIR
 
 cp *.R $outdir
 cp README.md $outdir
-cp *.cpp $outdir
+cp pileupParser $outdir
+cp uniqueSNPmultithreading $outdir
 cp smeg $outdir
 chmod +x $outdir/smeg
-g++ -std=c++11 $outdir/pileupParser.cpp -o $outdir/pileupParser
-g++ -std=c++11 -pthread $outdir/uniqueSNPmultithreading.cpp -o $outdir/uniqueSNPmultithreading
-
 
 ln -s $outdir/smeg $PREFIX/bin/smeg
 
