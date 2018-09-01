7124b490326af10a0d42d1bb6c1782a26f52609f
Jason Travis <JasonTravis@nau.edu>
2018-09-01 08:50:27 -0700

    patch Makefile to append LDFLAGS

--- recipes/unicycler/build.sh
+++ recipes/unicycler/build.sh
@@ -4,8 +4,11 @@ set -e
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
 
+# Override Makefile -mtune=native
+export CXXFLAGS = -mtune=generic
+
+# TODO: remove when seqan is updated upstream
 rm -rf unicycler/include/seqan
 mv seqan/include/seqan unicycler/include/seqan
 
