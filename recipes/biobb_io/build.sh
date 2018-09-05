77bd14c061918cc1400b916db2b0eedc67a4bb34
Pau Andrio <andriopau@gmail.com>
2018-09-05 17:53:23 +0200

    Softlink instead of copy in /bin

--- recipes/biobb_io/build.sh
+++ recipes/biobb_io/build.sh
@@ -3,6 +3,15 @@
 python setup.py install --single-version-externally-managed --record=record.txt
 
 mkdir -p $PREFIX/bin
-cp  $SP_DIR/biobb_io/mmb_api/pdb.py $PREFIX/bin/
-cp  $SP_DIR/biobb_io/mmb_api/pdb_cluster_zip.py $PREFIX/bin/
-cp  $SP_DIR/biobb_io/mmb_api/pdb_variants.py $PREFIX/bin/
+
+chmod u+x $SP_DIR/biobb_io/mmb_api/pdb.py
+ln -s  $SP_DIR/biobb_io/mmb_api/pdb.py $PREFIX/bin/pdb
+chmod u+x $PREFIX/bin/pdb
+
+chmod u+x $SP_DIR/biobb_io/mmb_api/pdb_cluster_zip.py
+ln -s  $SP_DIR/biobb_io/mmb_api/pdb_cluster_zip.py $PREFIX/bin/pdb_cluster_zip
+chmod u+x $PREFIX/bin/pdb_cluster_zip
+
+chmod u+x $SP_DIR/biobb_io/mmb_api/pdb_variants.py
+ln -s  $SP_DIR/biobb_io/mmb_api/pdb_variants.py $PREFIX/bin/pdb_variants
+chmod u+x $PREFIX/bin/pdb_variants
