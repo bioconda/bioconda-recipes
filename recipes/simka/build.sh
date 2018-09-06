36c0d90e7289d9a71c103ea0f8a787c23b1dc908
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-09-06 18:06:40 +0200

    fixing test

--- recipes/simka/build.sh
+++ recipes/simka/build.sh
@@ -9,5 +9,5 @@ simkaCountProcess \
 simkaMerge \
 "
 
-for i in $binaries; do mv $SRC_DIR/build/bin/$i $PREFIX/bin/$i; done
+for i in $binaries; do cp $SRC_DIR/build/bin/$i $PREFIX/bin/$i; done
 for i in $binaries; do chmod +x $PREFIX/bin/$i; done
