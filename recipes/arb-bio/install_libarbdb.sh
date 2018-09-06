e7f6017cba80d1150dd740fac759bcbd7a64d462
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-09-05 19:49:09 -0600

    typo

--- recipes/arb-bio/install_libarbdb.sh
+++ recipes/arb-bio/install_libarbdb.sh
@@ -2,5 +2,5 @@
 
 mkdir -p $PREFIX/lib/arb/lib
 for fn in libARBDB libCORE arb_tcp.dat; do
-    mv install/lib/arb/lib/lib$LIB* $PREFIX/lib/arb/lib
+    mv install/lib/arb/lib/$LIB* $PREFIX/lib/arb/lib
 done
