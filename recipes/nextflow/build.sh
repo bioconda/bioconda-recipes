49b3a43bbc8cd1c8060018db9aeee433e2d97fdf
Anthony Bretaudeau <abretaud@irisa.fr>
2018-09-06 07:58:54 +0200

    fix NXF_ASSETS

--- recipes/nextflow/build.sh
+++ recipes/nextflow/build.sh
@@ -2,7 +2,7 @@ mkdir -p $PREFIX/bin
 sed "s|^NXF_HOME=.*|NXF_HOME=$PREFIX/share/$PKG_NAME|" nextflow > $PREFIX/bin/nextflow
 
 # Use $HOME for storing some temp files (friendlier for multiuser environments)
-sed -i.bak "s|^  NXF_ASSETS=\${NXF_ASSETS:-\${NXF_HOME:-\$HOME/.nextflow}/assets}| NXF_ASSETS=\$HOME/.nextflow/assets|" $PREFIX/bin/nextflow
+sed -i.bak "s|^  NXF_ASSETS=\${NXF_ASSETS:-\${NXF_HOME:-\$HOME/.nextflow}/assets}| NXF_ASSETS=\${NXF_ASSETS:=\"\$HOME/.nextflow/assets\"}|" $PREFIX/bin/nextflow
 rm -f *.bak
 
 sed -i.bak "s|NXF_LAUNCHER=\${NXF_HOME}/tmp/launcher/nextflow-\${NXF_PACK}_\${NXF_VER}/\${NXF_HOST}|NXF_LAUNCHER=\$HOME/.nextflow/tmp/launcher/nextflow-\${NXF_PACK}_\${NXF_VER}/\${NXF_HOST}|" $PREFIX/bin/nextflow
