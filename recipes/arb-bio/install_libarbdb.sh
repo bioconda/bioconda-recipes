bc1795bde544d3ef03e8f6d14141756a8c17423e
Elmar Pruesse <epruesse@users.noreply.github.com>
2018-09-06 19:48:17 -0600

    Re-Rebuild arb-bio (missing file) (#10833)
    * Fix lib/arb_tcp.dat potentially needed by package libarbdb not just arb-bio

--- recipes/arb-bio/install_libarbdb.sh
+++ recipes/arb-bio/install_libarbdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/lib/arb/lib
-for LIB in ARBDB CORE; do
-    mv install/lib/arb/lib/lib$LIB* $PREFIX/lib/arb/lib
+for fn in libARBDB libCORE arb_tcp.dat; do
+    mv install/lib/arb/lib/$fn* $PREFIX/lib/arb/lib
 done
