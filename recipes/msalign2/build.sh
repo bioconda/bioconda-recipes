82953779bbf06e93b0b25b7da9b5514c2a58b30c
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-09-10 11:12:22 +0200

    no clue

--- recipes/msalign2/build.sh
+++ recipes/msalign2/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export CPATH=$PREFIX/include
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 gcc -o msalign2 base64.c ramp.c msalign2.c -I. -lgd -lm -lz -std=gnu99
 
 cp msalign2 $PREFIX/bin/
