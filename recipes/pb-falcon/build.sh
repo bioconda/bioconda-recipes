2c6c131713dd9d621268d7417385faaeb1344a42
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-09-12 00:01:18 -0700

    pb-falcon/0.2.0 -- Add falcon_phase (#10905)

--- recipes/pb-falcon/build.sh
+++ recipes/pb-falcon/build.sh
@@ -9,3 +9,10 @@ cd ..
 cd falcon_unzip
 $PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 cd ..
+cd falcon_phase
+$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
+cd ..
+cd pb-falcon-phase/bin
+ls -larth
+cp fc_coords2hp.py fc_emit_haplotigs.py fc_filt_hp.py fc_primary_contig_index.pl fc_scrub_names.pl FALCON_headerConverter.pl $PREFIX/bin
+cd ../..
