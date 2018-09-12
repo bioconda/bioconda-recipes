55799a29960b48141ed666d96c45992a3281f580
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-09-12 12:23:10 +0200

    fix gsl depedency

--- recipes/beaconlab_pscan/build.sh
+++ recipes/beaconlab_pscan/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ pscan.cpp -o pscan -O3 -lgsl -lgslcblas
+g++ pscan.cpp -o pscan -O3 -lgsl -lgslcblas -I${PREFIX}/include -L${PREFIX}/lib
 
 mv $SRC_DIR/pscan $PREFIX/bin/
 chmod +x $PREFIX/bin/pscan
