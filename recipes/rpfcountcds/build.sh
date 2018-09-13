0e9f9eb4084b22212f0f1b8cf8e78fa4f084e8a4
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-09-13 16:00:47 +0200

    Added shebang

--- recipes/rpfcountcds/build.sh
+++ recipes/rpfcountcds/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+<<<<<<< HEAD
+sed -i.bak '1s|^|#!/usr/bin/env python\'$'\n|g' RPF_count_CDS.py
+sed -i.bak '1s|^|#!/usr/bin/env python\'$'\n|g' RPF_count_CDS_nonStranded.py
+=======
+>>>>>>> 8b3eaa11327e754845068886a356dbde1c6fdd5c
 cp RPF_count_CDS.py $PREFIX/bin
 cp RPF_count_CDS_nonStranded.py $PREFIX/bin
 chmod 755 $PREFIX/bin/RPF_count_CDS.py
