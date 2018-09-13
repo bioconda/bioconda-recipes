4249dfb37d32debf5ea9740a1b2803df6ff9bd81
aemiol <tunsemi@yahoo.com>
2018-09-13 14:35:20 -0400

    edit meta.yaml

--- recipes/smeg/build.sh
+++ recipes/smeg/build.sh
@@ -8,11 +8,7 @@ mkdir -p $PREFIX/bin
 
 cd $SRC_DIR
 
-cp *.R $outdir
-cp README.md $outdir
-cp pileupParser $outdir
-cp uniqueSNPmultithreading $outdir
-cp smeg $outdir
+cp *.R README.md pileupParser uniqueSNPmultithreading smeg $outdir
 chmod +x $outdir/smeg
 
 ln -s $outdir/smeg $PREFIX/bin/smeg
