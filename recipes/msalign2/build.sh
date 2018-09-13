ce07b5e35cf40dcf3c27d11dcc23fe17471ccdc7
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-09-13 13:09:48 +0200

    fix msalign2

--- recipes/msalign2/build.sh
+++ recipes/msalign2/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export CPATH=$PREFIX/include
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 gcc -o msalign2 base64.c ramp.c msalign2.c -I. -lgd -lm -lz -std=gnu99
 
 cp msalign2 $PREFIX/bin/
