51f91be88239a09b2c44a657cb3e26e8da0f5604
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-09-15 10:26:19 +0200

    add correct path to libraries

--- recipes/gdc/build.sh
+++ recipes/gdc/build.sh
@@ -8,9 +8,11 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 else
         echo "operating system not found or not supported"
 fi
-export CPATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
+#export CPATH=${PREFIX}/include
+#export LIBRARY_PATH=../libs
+#export LD_LIBRARY_PATH=../libs
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 make
 cp gdc2 $PREFIX/bin/
 chmod +x $PREFIX/bin/gdc2
