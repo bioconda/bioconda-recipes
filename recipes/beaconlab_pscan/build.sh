bfc868028eb6654e279e736852d002a49f4a6889
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-09-16 16:07:49 +0200

    trying something else

--- recipes/beaconlab_pscan/build.sh
+++ recipes/beaconlab_pscan/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-g++ pscan.cpp -o pscan -O3 -lgsl -lgslcblas -I$(PREFIX)/include -L$(PREFIX)/lib
+export CPATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+g++ pscan.cpp -o pscan -O3 -lgsl -lgslcblas
 
 mv $SRC_DIR/pscan $PREFIX/bin/
 chmod +x $PREFIX/bin/pscan
