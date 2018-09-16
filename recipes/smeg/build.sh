d66186fd896bb834f4dce3ba958317250881b7a6
aemiol <tunsemi@yahoo.com>
2018-09-16 05:43:51 -0400

    add noarch:generic to meta.yaml

---
+++ recipes/smeg/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+
+cd $SRC_DIR
+
+cp -a . /$outdir/
+chmod +x $outdir/smeg
+
+ln -s $outdir/smeg $PREFIX/bin/smeg
