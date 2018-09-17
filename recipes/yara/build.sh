6bc09ca8c1ca567b5f9b9220dd536500d249529f
Enrico Seiler <enrico.seiler@hotmail.de>
2018-09-17 09:34:18 +0200

    Update yara and build from source

--- recipes/yara/build.sh
+++ recipes/yara/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-BINARY_HOME=$PREFIX/bin
-PKG_HOME=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+mkdir build
+cd build
 
-cd $SRC_DIR/bin
-
-binaries="\
-yara_mapper \
-yara_indexer \
-"
-mkdir -p $PREFIX/bin
-mkdir -p $PKG_HOME
-
-for i in $binaries; do cp $SRC_DIR/bin/$i $PKG_HOME/$i && chmod a+x $PKG_HOME/$i && ln -s $PKG_HOME/$i $BINARY_HOME/$i; done
+cmake .. -DSEQAN_BUILD_SYSTEM=APP:yara -DSEQAN_ARCH_NATIVE=ON -DCMAKE_INSTALL_PREFIX=$PREFIX
+make all
+make test
+make install
