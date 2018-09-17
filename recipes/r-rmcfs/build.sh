3d0729f90a3d747540f7b05f5e6f434ba4695f05
Antônio Pedro Camargo <apcamargo@users.noreply.github.com>
2018-09-17 17:20:52 -0300

    Update build.sh

--- recipes/r-rmcfs/build.sh
+++ recipes/r-rmcfs/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-$R -e "options(java.parameters = '-Xss2560k')"
-$R CMD javareconf
 $R CMD INSTALL --build .
