c618b9cb574c6ff23b088b672ce02e273ba8fca4
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-09-20 17:04:10 +0200

    Add a patch to precompile inline C code for MPI support

--- recipes/maker/build.sh
+++ recipes/maker/build.sh
@@ -2,6 +2,11 @@
 
 cd src/
 
+# We will need this script to precompile inline C code for MPI support
+cp maker_mpi_init $PREFIX/bin/
+rm maker_mpi_init
+chmod a+x $PREFIX/bin/maker_mpi_init
+
 # enable mpi
 echo "yes" | perl Build.PL
 
@@ -17,3 +22,8 @@ mv bin/* $PREFIX/bin
 mkdir -p $PREFIX/perl/lib/
 mv perl/lib/* $PREFIX/perl/lib/
 mv lib/* $PREFIX/lib/
+
+# Run a first time MPI_Init() to pre compile inline C code
+perl $PREFIX/bin/maker_mpi_init
+# This is not needed anymore
+rm $PREFIX/bin/maker_mpi_init
