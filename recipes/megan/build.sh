d0a95c3909c2ae9549938f336343ee3ec68715c2
M Bernt <m.bernt@ufz.de>
2018-09-20 22:46:44 +0200

    megan: doc improvement

--- recipes/megan/build.sh
+++ recipes/megan/build.sh
@@ -6,8 +6,15 @@ MEGAN="$PREFIX/opt/$PKG_NAME-$PKG_VERSION"
 ./MEGAN_Community_unix_6_12_3.sh -q -dir "$MEGAN"
 
 # link to bin/ and fix paths
-# bin_dir is originally specified relative to basename $0, which is in MEGAN/tools .. 
-# we just fix this to the actual file, ie MEGAN/tools/TOOL
+# in all scripts bin_dir is defined as: 
+# ```
+# bin_dir=`dirname "$0"`       # may be relative path
+# bin_dir=`cd "$bin_dir" && pwd`    # ensure absolute path
+# ```
+# all other paths (eg to jars) are defined relative to bin_dir
+# of course this does not work when linking/copying
+# 
+# so we just fix this to the actual file, ie MEGAN/tools/TOOL
 find "$MEGAN"/tools -type f | while read -r file
 do
 	b=$(basename "$file")
