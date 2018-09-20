a03bc3b5ed91b7c8d3823488fb50f95637954187
Akintunde Emiola <akintunde.emiola@jax.org>
2018-09-20 16:59:24 -0400

    bump build number

--- recipes/smeg/build.sh
+++ recipes/smeg/build.sh
@@ -7,5 +7,14 @@ cd $SRC_DIR
 
 cp -a . /$outdir/
 chmod +x $outdir/smeg
+chmod +x $outdir/uniqueSNPmultithreading
+chmod +x $outdir/pileupParser
 
 ln -s $outdir/smeg $PREFIX/bin/smeg
+
+cd $outdir
+wget https://github.com/PathoScope/PathoScope/archive/v2.0.6.tar.gz
+tar xvf v2.0.6.tar.gz
+cd PathoScope-2.0.6/
+python2.7 setup.py install
+
