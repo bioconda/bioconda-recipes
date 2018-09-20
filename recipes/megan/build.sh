1add6fb894a32b04ae2ac6d1f03eabe694893f4e
M Bernt <m.bernt@ufz.de>
2018-09-20 22:36:07 +0200

    megan: made tests work

--- recipes/megan/build.sh
+++ recipes/megan/build.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
+# call installer
 chmod u+x MEGAN_Community_unix_6_12_3.sh
->&2 which java
-MEGAN="$PEFIX/opt/$PKG_NAME-$PKG_VERSION"
+MEGAN="$PREFIX/opt/$PKG_NAME-$PKG_VERSION"
 ./MEGAN_Community_unix_6_12_3.sh -q -dir "$MEGAN"
 
-mkdir -p "$PREFIX"/bin/
-
+# link to bin/ and fix paths
+# bin_dir is originally specified relative to basename $0, which is in MEGAN/tools .. 
+# we just fix this to the actual file, ie MEGAN/tools/TOOL
 find "$MEGAN"/tools -type f | while read -r file
 do
-	ln -s "$file" "$PREFIX"/bin/"$(basename "$file")"
+	b=$(basename "$file")
+	ln -s "$file" "$PREFIX"/bin/"$b"
+        sed -i -e "s@\"\$0\"@\"$file\"@" "$file"
+#	sed -i -e "s@^bin_dir=.*@bin_dir=\"$MEGAN\/tools\"@" "$file"
 done
-ln -s "$MEGAN"/MEGAN "$PREFIX"/bin/
-
-# -varfile [file] responsefile
-# -c console mode
-# -q unattended mode
-# -dir [DIR] installation dir
-# -overwrite 
-# -splash [TITLE] show progress
-# -Dname=value system properties
-
-
-# 
-# fastqc=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
-# mkdir -p $fastqc
-# cp -r ./* $fastqc
-# sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $fastqc/fastqc
-# rm -f $fastqc/fastqc.bak
-# chmod +x $fastqc/fastqc
-# mkdir -p $PREFIX/bin
-# ln -s $fastqc/fastqc $PREFIX/bin/fastqc 
-
+ln -s "$MEGAN"/MEGAN "$PREFIX"/bin/MEGAN
+sed -i -e "s@\"\$0\"@\"$MEGAN/MEGAN\"@" "$MEGAN"/MEGAN
