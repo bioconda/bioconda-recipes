cf8008d5e25f659266100fae3049e4f00d808817
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-09-20 17:49:06 +0200

    fix

--- recipes/maker/build.sh
+++ recipes/maker/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd src/
-
 # We will need this script to precompile inline C code for MPI support
 cp maker_mpi_init $PREFIX/bin/
 rm maker_mpi_init
 chmod a+x $PREFIX/bin/maker_mpi_init
+
+cd src/
 
 # enable mpi
 echo "yes" | perl Build.PL
