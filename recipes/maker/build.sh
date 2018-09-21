71530c72ed0a628032b2b27702fd9759dbf5d253
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-09-21 15:57:29 +0200

    debug problem with mpi

--- recipes/maker/build.sh
+++ recipes/maker/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # We will need this script to precompile inline C code for MPI support
 cp maker_mpi_init $PREFIX/bin/
 rm maker_mpi_init
@@ -24,6 +23,7 @@ mv perl/lib/* $PREFIX/perl/lib/
 mv lib/* $PREFIX/lib/
 
 # Run a first time MPI_Init() to pre compile inline C code
-mpirun --allow-run-as-root -n 1 $PREFIX/bin/maker_mpi_init || true
+mpirun --allow-run-as-root -n 1 $PREFIX/bin/maker_mpi_init
+false
 # This is not needed anymore
 rm $PREFIX/bin/maker_mpi_init
