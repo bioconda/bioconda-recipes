8298f9dbaefa221ed5276fd92146969eaac5d276
dpryan79 <dpryan79@gmail.com>
2018-09-24 12:15:41 +0200

    Don't actually run make in demo

--- recipes/r2r/build.sh
+++ recipes/r2r/build.sh
@@ -4,7 +4,5 @@ make
 #copy binary to bin
 cp -p src/r2r $PREFIX/bin
 #copy test files to share
-cd demo
-make
-cp -p demo1.sto $PREFIX/share/
-cp -p intermediate/demo1.cons.sto $PREFIX/share/
+cp -p demo/demo1.sto $PREFIX/share/
+cp -p demo/intermediate/demo1.cons.sto $PREFIX/share/
