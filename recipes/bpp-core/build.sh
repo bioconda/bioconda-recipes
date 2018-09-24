177eb03a1e560bc723a1e831c002da101f9c1404
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-09-24 13:05:04 -0500

    Add recipe for bpp-core

--- recipes/bpp-core/build.sh
+++ recipes/bpp-core/build.sh
@@ -1,3 +1,11 @@
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX .
+#!/bin/bash
+
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE .
+
 make
 make install
