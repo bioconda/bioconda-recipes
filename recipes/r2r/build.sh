a5eda017d95d8214084df2acc691c606a4b6287f
dpryan79 <dpryan79@gmail.com>
2018-09-24 11:51:05 +0200

    Fix license file, which doesn't exist and build in demo

--- recipes/r2r/build.sh
+++ recipes/r2r/build.sh
@@ -4,6 +4,7 @@ make
 #copy binary to bin
 cp -p src/r2r $PREFIX/bin
 #copy test files to share
-cp -p demo/demo1.sto $PREFIX/share/
-cp -p demo/intermediate/demo1.cons.sto $PREFIX/share/
-
+cd demo
+make
+cp -p demo1.sto $PREFIX/share/
+cp -p intermediate/demo1.cons.sto $PREFIX/share/
