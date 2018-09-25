f1142963a23644cdf5d96ce0c23cd25d310a2e86
dpryan79 <dpryan79@gmail.com>
2018-09-25 14:06:35 +0200

    Remove libBigWig from the blacklist, I didn't even know it was in bioconda...

--- recipes/libbigwig/build.sh
+++ recipes/libbigwig/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 cd $SRC_DIR
 make install prefix=$PREFIX/
