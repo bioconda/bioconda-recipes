944134556a59185155624121610a69c663e43f19
Asma Bankapur <asmariyaz23@gmail.com>
2018-09-25 09:03:53 -0400

    adding new mut recipe

--- recipes/ctat-mutations/build.sh
+++ recipes/ctat-mutations/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# ctat_mutations_DIR_NAME="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
-# The above is an error. The following is correct.
 ctat_mutations_DIR_NAME="$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 ctat_mutations_INSTALL_PATH="$PREFIX/share/$ctat_mutations_DIR_NAME"
 # Make the install directory and move the ctat-mutations files to that location.
 mkdir -p $PREFIX/bin
 mkdir -p $ctat_mutations_INSTALL_PATH
 #copy to INSTALL_PATH
-cp -R $SRC_DIR/ctat_mutations $SRC_DIR/SciEDPipeR $ctat_mutations_INSTALL_PATH
+cp -R $SRC_DIR/ctat_mutations $ctat_mutations_INSTALL_PATH
 #change permissions on ctat_mutations
 chmod a+x $ctat_mutations_INSTALL_PATH/ctat_mutations
-cd $PREFIX/bin 
-echo "#!/bin/bash" > $PREFIX/bin/ctat_mutations
-echo "$ctat_mutations_INSTALL_PATH/ctat_mutations \$@" >> $PREFIX/bin/ctat_mutations
-chmod a+x $PREFIX/bin/ctat_mutations
+cd $PREFIX/bin
+echo '#!/bin/bash' > ctat_mutations
+ln -s $PREFIX/bin/picard $PREFIX/bin/picard.jar
+echo "export PICARD_HOME=$PREFIX/bin" >> ctat_mutations 
+echo "export GATK_HOME=$PREFIX/bin" >> ctat_mutations
+echo "$ctat_mutations_INSTALL_PATH/ctat_mutations \$@" >> ctat_mutations
+chmod a+x ctat_mutations
