86df958d6f5410034a90882b9a67bd90fa15bfac
Asma Bankapur <asmariyaz23@gmail.com>
2018-09-27 07:49:32 -0400

    adding bracket for print

--- recipes/ctat-mutations/build.sh
+++ recipes/ctat-mutations/build.sh
@@ -13,13 +13,13 @@ chmod a+x $ctat_mutations_INSTALL_PATH/ctat_mutations
 cd $PREFIX/bin
 GATK_HOME=$(python -c "\
 import os
-print os.path.realpath(\"/\".join(os.readlink(\"gatk\").split(\"/\")[0:-1]))
+print(os.path.realpath(\"/\".join(os.readlink(\"gatk\").split(\"/\")[0:-1])))
 ")
 echo "%%%%%%%%"
 echo $GATK_HOME
 PICARD_HOME=$(python -c "\
 import os
-print os.path.realpath(\"/\".join(os.readlink(\"picard\").split(\"/\")[0:-1]))
+print(os.path.realpath(\"/\".join(os.readlink(\"picard\").split(\"/\")[0:-1])))
 ")
 echo "%%%%%%%%"
 echo $PICARD_HOME
