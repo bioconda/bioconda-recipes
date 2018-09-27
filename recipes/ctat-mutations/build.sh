93ed243bbf99186379570f40c263122a91e98126
asmariyaz23 <asmariyaz23@users.noreply.github.com>
2018-09-27 00:17:40 -0400

    Ctat mut (#11122)
    * no generic usage
    
    * echo statements
    
    * proper hash
    
    * increasing build number

--- recipes/ctat-mutations/build.sh
+++ recipes/ctat-mutations/build.sh
@@ -11,20 +11,18 @@ cp -R ctat_mutations PyLib mutation_lib_prep plugins src testing LICENSE.txt REA
 #change permissions on ctat_mutations
 chmod a+x $ctat_mutations_INSTALL_PATH/ctat_mutations
 cd $PREFIX/bin
-ACTUAL_GATK=$(python -c "\
-import glob
-print glob.glob(\"$share_path/gatk4-*\")
-")
 GATK_HOME=$(python -c "\
-print \"/\".join(\"$ACTUAL_GATK\".split(\"/\")[0:-1])
+import os
+print os.path.realpath(\"/\".join(os.readlink(\"gatk\").split(\"/\")[0:-1]))
 ")
-ACTUAL_PICARD=$(python -c "\
-import glob
-print glob.glob(\"$share_path/picard-*\")
-")
+echo "%%%%%%%%"
+echo $GATK_HOME
 PICARD_HOME=$(python -c "\
-print \"/\".join(\"$ACTUAL_PICARD\".split(\"/\")[0:-1])
+import os
+print os.path.realpath(\"/\".join(os.readlink(\"picard\").split(\"/\")[0:-1])))
 ")
+echo "%%%%%%%%"
+echo $PICARD_HOME
 echo '#!/bin/bash' > ctat_mutations
 echo "export PICARD_HOME=$PICARD_HOME" >> ctat_mutations 
 echo "export GATK_HOME=$GATK_HOME" >> ctat_mutations
