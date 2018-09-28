3ca615fe91ec1e2c8f388a9051831f18f902ce32
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-09-28 00:02:18 -0500

    Update paml recipe to include dat/

--- recipes/paml/4.9/build.sh
+++ recipes/paml/4.9/build.sh
@@ -2,20 +2,17 @@
 
 set -eu -o pipefail
 
-mkdir -p $PREFIX/bin
+cp -r dat ${PREFIX}/
 
-cd bin
-rm *.exe
-
-cd ../src
+cd src
 make
 
-cp baseml $PREFIX/bin
-cp basemlg $PREFIX/bin
-cp chi2 $PREFIX/bin
-cp codeml $PREFIX/bin
-cp evolver $PREFIX/bin
-cp infinitesites $PREFIX/bin
-cp mcmctree $PREFIX/bin
-cp pamp $PREFIX/bin
-cp yn00 $PREFIX/bin
+cp baseml ${PREFIX}/bin
+cp basemlg ${PREFIX}/bin
+cp chi2 ${PREFIX}/bin
+cp codeml ${PREFIX}/bin
+cp evolver ${PREFIX}/bin
+cp infinitesites ${PREFIX}/bin
+cp mcmctree ${PREFIX}/bin
+cp pamp ${PREFIX}/bin
+cp yn00 ${PREFIX}/bin
