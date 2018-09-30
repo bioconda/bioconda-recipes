59d0124663faab4bcdd32fcda673abcd57436936
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-09-30 20:27:08 +0200

    new attempt to pass gcc error

--- recipes/pygtftk/build.sh
+++ recipes/pygtftk/build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-#cd pygtftk-0.9.1
-#make install
 export C_INCLUDE_PATH=$PREFIX/include
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
-#mkdir -p $PREFIX/bin
-#cp bin/* $PREFIX/bin
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
