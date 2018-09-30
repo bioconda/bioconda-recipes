6dfd035d8de67eb35703952b64d67ef6615a0816
npavlovikj <npavlovikj@hcc-acaprez-02.unl.edu>
2018-09-30 12:31:01 -0500

    Update recipe for paml to include dat/

---
+++ recipes/paml/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+mkdir -p ${PREFIX}/bin
+
+cd src
+make -j 4
+
+cp baseml ${PREFIX}/bin/
+cp basemlg ${PREFIX}/bin/
+cp chi2 ${PREFIX}/bin/
+cp codeml ${PREFIX}/bin/
+cp evolver ${PREFIX}/bin/
+cp infinitesites ${PREFIX}/bin/
+cp mcmctree ${PREFIX}/bin/
+cp pamp ${PREFIX}/bin/
+cp yn00 ${PREFIX}/bin/
+cp -r dat ${PREFIX}/bin/
