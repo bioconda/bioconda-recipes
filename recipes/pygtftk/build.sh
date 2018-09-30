6197015d9244275530e30efcaf84fe6af7367ca7
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-09-30 14:20:56 +0200

    Adding pygtftk

---
+++ recipes/pygtftk/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#cd pygtftk-0.9.1
+#make install
+
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+#mkdir -p $PREFIX/bin
+#cp bin/* $PREFIX/bin
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
