36c67189751962dcfffc14a618a0bd4a05f22ffc
npavlovikj <npavlovikj@hcc-acaprez-02.unl.edu>
2018-09-30 12:13:16 -0500

    Update build.sh

--- recipes/paml/build.sh
+++ recipes/paml/build.sh
@@ -2,17 +2,18 @@
 
 set -eu -o pipefail
 
+mkdir -p ${PREFIX}/bin/
 cp -r dat ${PREFIX}/
 
 cd src
 make
 
-cp baseml ${PREFIX}/bin
-cp basemlg ${PREFIX}/bin
-cp chi2 ${PREFIX}/bin
-cp codeml ${PREFIX}/bin
-cp evolver ${PREFIX}/bin
-cp infinitesites ${PREFIX}/bin
-cp mcmctree ${PREFIX}/bin
-cp pamp ${PREFIX}/bin
-cp yn00 ${PREFIX}/bin
+cp baseml ${PREFIX}/bin/
+cp basemlg ${PREFIX}/bin/
+cp chi2 ${PREFIX}/bin/
+cp codeml ${PREFIX}/bin/
+cp evolver ${PREFIX}/bin/
+cp infinitesites ${PREFIX}/bin/
+cp mcmctree ${PREFIX}/bin/
+cp pamp ${PREFIX}/bin/
+cp yn00 ${PREFIX}/bin/
