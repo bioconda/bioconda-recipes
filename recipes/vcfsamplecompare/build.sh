24e83a9a4adabbad0f00f386a13ec38523216172
hepcat72 <hepcat72@gmail.com>
2018-10-03 10:56:09 -0400

    I have to check in in order for conda build to reflect the changes.
    Files checked in: recipes/vcfsamplecompare/build.sh

--- recipes/vcfsamplecompare/build.sh
+++ recipes/vcfsamplecompare/build.sh
@@ -6,7 +6,8 @@ perl Makefile.PL
 make
 #make test
 echo "RUNNING TESTS"
-perl -e 'use CommandLineInterface;print "Should get an INC error\n\n"'
-perl -Ilib -e 'use CommandLineInterface;' -- --usage
-perl t/run_tests.t --usage
+#perl -e 'use CommandLineInterface;print "Should get an INC error\n\n"'
+#The above does generate an error.
+perl -Ilib -e 'print STDOUT "This is STDOUT\n";print STDERR "This is STDERR\n";use CommandLineInterface;' -- --usage --debug -1
+#perl t/run_tests.t --usage
 make install
