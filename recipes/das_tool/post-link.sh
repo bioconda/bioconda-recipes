2ea52909e986594dda22b79de73d06c4e82f1897
Silas Kieser <silas.kieser@gmail.com>
2018-10-03 09:49:54 +0200

    added v 1.1.1 unoffical from my github test not working

--- recipes/das_tool/post-link.sh
+++ recipes/das_tool/post-link.sh
@@ -4,8 +4,7 @@ echo "
 ####################################################################################
 ${PKG_NAME} version ${PKG_VERSION}-${PKG_BUILDNUM} has been successfully installed!
 
-This software also need USEARCH version 8.1 or more. Bioconda can not provide it
-because of its license.
+This software by default runs with USEARCH. You can install it from the following links or use DIAMOND '--search_engine diamond'
 
  > Download: http://www.drive5.com/usearch/download.html
  > Installation instruction: http://www.drive5.com/usearch/manual/install.html
