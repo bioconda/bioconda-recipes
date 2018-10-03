b2fdda69f0609d1c66a3a155cf91287a3e78ed37
hepcat72 <hepcat72@gmail.com>
2018-10-03 10:51:38 -0400

    Testing lib location.
    Files checked in: recipes/vcfsamplecompare/build.sh

--- recipes/vcfsamplecompare/build.sh
+++ recipes/vcfsamplecompare/build.sh
@@ -6,6 +6,7 @@ perl Makefile.PL
 make
 #make test
 echo "RUNNING TESTS"
+perl -e 'use CommandLineInterface;print "Should get an INC error\n\n"'
 perl -Ilib -e 'use CommandLineInterface;' -- --usage
 perl t/run_tests.t --usage
 make install
