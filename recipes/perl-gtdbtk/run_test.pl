e778423aab4a917ee163439eb73cd8c0dba20d66
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-10-02 19:50:23 -0500

    Add recipe for perl-gtdbtk

---
+++ recipes/perl-gtdbtk/run_test.pl
@@ -0,0 +1,26 @@
+print("import: Bio::Pfam::Active_site::as_search\n");
+use Bio::Pfam::Active_site::as_search;
+
+print("import: Bio::Pfam::HMM::HMMIO\n");
+use Bio::Pfam::HMM::HMMIO;
+
+print("import: Bio::Pfam::HMM::HMMMatch\n");
+use Bio::Pfam::HMM::HMMMatch;
+
+print("import: Bio::Pfam::HMM::HMM\n");
+use Bio::Pfam::HMM::HMM;
+
+print("import: Bio::Pfam::HMM::HMMResults\n");
+use Bio::Pfam::HMM::HMMResults;
+
+print("import: Bio::Pfam::HMM::HMMSequence\n");
+use Bio::Pfam::HMM::HMMSequence;
+
+print("import: Bio::Pfam::HMM::HMMUnit\n");
+use Bio::Pfam::HMM::HMMUnit;
+
+print("import: Bio::Pfam::Scan::PfamScan\n");
+use Bio::Pfam::Scan::PfamScan;
+
+print("import: Bio::Pfam::Scan::Seq\n");
+use Bio::Pfam::Scan::Seq;
