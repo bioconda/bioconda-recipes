77865a54566b47ec3c5727501a5082943e89baa8
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-10-03 11:54:21 -0500

    Change cp for osx build

--- recipes/perl-gtdbtk/build.sh
+++ recipes/perl-gtdbtk/build.sh
@@ -8,10 +8,10 @@ fi
 
 
 BUILD_DIR=${TMP_DIR}/Bio-Pfam
-mkdir -p ${BUILD_DIR}/lib
+mkdir -p ${BUILD_DIR}/lib/
 
 cd ${BUILD_DIR}
-cp -R ${SRC_DIR}/gtdbtk/external/Bio/ ${BUILD_DIR}/lib/
+cp -R ${SRC_DIR}/gtdbtk/external/Bio ${BUILD_DIR}/lib
 cp ${SRC_DIR}/gtdbtk/external/pfam_search.pl ${BUILD_DIR}
 cp ${RECIPE_DIR}/Makefile.PL ${BUILD_DIR}
 
