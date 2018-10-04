0ff4edcce75790eaccac979e682e274961702e3b
dpryan79 <dpryan79@gmail.com>
2018-10-05 00:46:59 +0200

    Try simply avoiding the last error

--- test/test_utils.py
+++ test/test_utils.py
@@ -651,27 +651,27 @@ def test_skip_dependencies():
         one:
           meta.yaml: |
             package:
-              name: one
+              name: skip_dependencies_one
               version: 0.1
         two:
           meta.yaml: |
             package:
-              name: two
+              name: skip_dependencies_two
               version: 0.1
             requirements:
               build:
-                - one
+                - skip_dependencies_one
                 - nonexistent
         three:
           meta.yaml: |
             package:
-              name: three
+              name: skip_dependencies_three
               version: 0.1
             requirements:
               build:
-                - one
+                - skip_dependencies_one
               run:
-                - two
+                - skip_dependencies_two
     """, from_string=True)
     r.write_recipes()
     pkgs = {}
