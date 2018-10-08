5471bf0ba505ac12fb244fa784f7a3bee79f3802
greg <ghv2@psu.edu>
2018-10-08 10:50:52 -0400

    Add path info

--- recipes/bcftools-gtc2vcf-plugin/1.9/build.sh
+++ recipes/bcftools-gtc2vcf-plugin/1.9/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 export VERSION=1.9
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
