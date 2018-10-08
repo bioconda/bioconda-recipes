d90428d4f3bafe5e36619e836d3d6cf29bb7d559
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-10-08 18:06:14 +0200

    update flags

--- recipes/polymutt/build.sh
+++ recipes/polymutt/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 make
 
-cp ${SRC_DIR}/bin/polymutt $PREFIX/bin/polymutt 
+cp ${SRC_DIR}/bin/polymutt $PREFIX/bin/polymutt
