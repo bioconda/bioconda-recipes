983cc7d6dffa09a91532c600c47933f9d4d6bfd5
greg <ghv2@psu.edu>
2018-10-08 11:24:11 -0400

    Add c99 flag for c compiler

--- recipes/bcftools-gtc2vcf-plugin/1.9/build.sh
+++ recipes/bcftools-gtc2vcf-plugin/1.9/build.sh
@@ -8,6 +8,7 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CFLAGS="$CFLAGS -I$PREFIX/include -std=c99"
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/libexec/bcftools
