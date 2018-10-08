35cc7381773d55f8d001488000e6a6c459b5b193
greg <ghv2@psu.edu>
2018-10-08 12:49:27 -0400

    Another try

--- recipes/bcftools-gtc2vcf-plugin/1.9/build.sh
+++ recipes/bcftools-gtc2vcf-plugin/1.9/build.sh
@@ -6,7 +6,7 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include -std=c99"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CFLAGS="$CFLAGS -I$PREFIX/include -std=c99"
 
