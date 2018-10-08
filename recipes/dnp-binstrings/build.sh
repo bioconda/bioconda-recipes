7dc6b8a0bba25bbba859a38fea3abf1673495fec
Erinija <erinija.pranckeviciene@gmail.com>
2018-10-07 20:40:00 -0400

    Dinucleotide patterns analysis tools v1. / Add prefix dnp- to the package names (#11249)
    * Add prefix dnp- to the package names
    
    * Update build.sh
    
    * Update build.sh

--- recipes/dnp-binstrings/build.sh
+++ recipes/dnp-binstrings/build.sh
@@ -10,5 +10,5 @@ CMAKE_PREFIX_PATH="$CONDA_DEFAULT_ENV/share/cmake/seqan"
 cmake  ../ -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" -DSEQAN_INCLUDE_PATH="$SEQAN_INCLUDE_PATH" 
 
 make
-cp binstrings $PREFIX/bin
+cp binstrings $PREFIX/bin/dnp-binstrings
  
