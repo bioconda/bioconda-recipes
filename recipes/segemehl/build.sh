6a73b68591e76de9d7be839b9768717f6e360e25
fall <joerg@fallmann.info>
2018-10-09 13:32:20 +0200

    Added segemehl 0.3.0

--- recipes/segemehl/build.sh
+++ recipes/segemehl/build.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-cd segemehl
-
-export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
-export LIBRARY_PATH=${PREFIX}/lib
-
-make -j 1 ## do not use >1 make threads!
-
-for i in *.x ; do
-    install ${i} ${PREFIX}/bin
-done
+0.3.0/build.sh
