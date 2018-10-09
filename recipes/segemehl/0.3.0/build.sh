1e88ab5f3073c584726ae616badfd5cb3a645e1b
fall <joerg@fallmann.info>
2018-10-09 14:52:20 +0200

    updated makes

--- recipes/segemehl/0.3.0/build.sh
+++ recipes/segemehl/0.3.0/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cd 20181005-161011
-
+#cd 20181005-161011
 export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
 export LIBRARY_PATH=${PREFIX}/lib
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
