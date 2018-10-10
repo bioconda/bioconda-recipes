5846592c83b9d9bce3ae456b7988d93d54aec314
John Lees <jl11@sanger.ac.uk>
2018-10-10 21:35:29 +0100

    Attempt building boost

--- recipes/seer/build.sh
+++ recipes/seer/build.sh
@@ -2,6 +2,11 @@ pushd gzstream
 make clean
 make CXX="$CXX $CXXFLAGS" CPPFLAGS="$CPPFLAGS -I. -I$PREFIX/include" AR="$AR cr"
 popd
+pushd boost
+python2 tools/boostdep/depinst/depinst.py program_options --include example
+./bootstrap.sh --with-libraries=program_options
+./b2 install toolset=gcc-std17
+popd boost
 pushd src
 make CXXFLAGS="$CXXFLAGS"
 make install
