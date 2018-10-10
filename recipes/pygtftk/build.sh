074db40cea577b6ae5782d144bc92c8c334edbb0
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-10-10 19:02:43 +0200

    modified:   pygtftk/build.sh

--- recipes/pygtftk/build.sh
+++ recipes/pygtftk/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-export C_INCLUDE_PATH=$PREFIX/include
+CC=${PREFIX}/bin/gcc
+CXX=${PREFIX}/bin/g++
+export CPATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include
 echo $CC
 #export CC=/usr/local/bin/gcc
 /opt/rh/devtoolset-2/root/usr/bin/gcc -v
