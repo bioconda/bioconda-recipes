76f6aa7a035c8e542754ec54fffbea076ef3089d
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-10-11 10:07:36 +0200

    modified:   recipes/pygtftk/build.sh

--- recipes/pygtftk/build.sh
+++ recipes/pygtftk/build.sh
@@ -4,9 +4,9 @@ CXX=${PREFIX}/bin/g++
 export CPATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export C_INCLUDE_PATH=${PREFIX}/include
-echo $CC
+#echo $CC
 #export CC=/usr/local/bin/gcc
-/opt/rh/devtoolset-2/root/usr/bin/gcc -v
+#/opt/rh/devtoolset-2/root/usr/bin/gcc -v
 #gcc -v
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
