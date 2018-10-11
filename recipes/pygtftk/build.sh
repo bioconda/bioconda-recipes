c4c9a8cd79216e28b5ee29f4cf1e2876e6fb1641
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-10-11 11:01:09 +0200

    modified:   recipes/pygtftk/build.sh

--- recipes/pygtftk/build.sh
+++ recipes/pygtftk/build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
-export CPATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export C_INCLUDE_PATH=${PREFIX}/include
-#echo $CC
-#export CC=/usr/local/bin/gcc
-#/opt/rh/devtoolset-2/root/usr/bin/gcc -v
+#CC=${PREFIX}/bin/gcc
+#CXX=${PREFIX}/bin/g++
+#export CPATH=${PREFIX}/include
+#export CPP_INCLUDE_PATH=${PREFIX}/include
+#export C_INCLUDE_PATH=${PREFIX}/include
+##echo $CC
+##export CC=/usr/local/bin/gcc
+##/opt/rh/devtoolset-2/root/usr/bin/gcc -v
+##gcc -v
+#
+#
+#
+#echo "${PREFIX}/bin/gcc -v"
+#${PREFIX}/bin/gcc -v
+#
+#echo "gcc -v"
 #gcc -v
+#
+#echo "/opt/rh/devtoolset-2/root/usr/bin/gcc -v"
+#/opt/rh/devtoolset-2/root/usr/bin/gcc -v
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
