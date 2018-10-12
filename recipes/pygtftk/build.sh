a6b6c5b47785ddc4cdb381bad25959668541d0bd
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-10-12 12:51:01 +0200

    first cleaning of pygtftk rule after successful build.

--- recipes/pygtftk/build.sh
+++ recipes/pygtftk/build.sh
@@ -1,37 +1,3 @@
-#!/bin/bash
-#CC=${PREFIX}/bin/gcc
-#CXX=${PREFIX}/bin/g++
-#export CPATH=${PREFIX}/include
-#export CPP_INCLUDE_PATH=${PREFIX}/include
-#export C_INCLUDE_PATH=${PREFIX}/include
-echo "CC: $CC"
-echo "PATH: $PATH"
-echo "PREFIX: $PREFIX"
-##export CC=/usr/local/bin/gcc
-##/opt/rh/devtoolset-2/root/usr/bin/gcc -v
-##gcc -v
-#export CC='/opt/rh/devtoolset-2/root/usr/bin/gcc'
-#export PATH='/opt/rh/devtoolset-2/root/usr/bin:$PATH'
-#
-#export C_INCLUDE_PATH=$PREFIX/include
-#export CPP_INCLUDE_PATH=$PREFIX/include
-#export CPATH=${PREFIX}/include
-#
-#
-# Produce this error:
-# Error: bin/gcc is a symlink to a path that may not exist after the build is completed (/opt/conda/conda-bld/pygtftk_1539270111754/_build_env/bin/x86_64-conda_cos6-linux-gnu-cc) compiling .pyc files...
-#ln -s $CC $PREFIX/bin/gcc
 ln -s $CC `dirname $CC`/gcc
-#
-#echo "${PREFIX}/bin/gcc -v"
-#${PREFIX}/bin/gcc -v
-#
-#echo "gcc -v"
-#gcc -v
-#
-#echo "/opt/rh/devtoolset-2/root/usr/bin/gcc -v"
-#/opt/rh/devtoolset-2/root/usr/bin/gcc -v
-
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
 cp bin/* $PREFIX/bin/
