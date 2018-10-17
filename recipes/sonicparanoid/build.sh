f71f22f7373af1bc43bd9e6c5fab3d263735d0d5
Salvo <salvo981@gmail.com>
2018-10-17 15:15:14 +0900

    modified build.sh to compile source codel added make in build section

--- recipes/sonicparanoid/build.sh
+++ recipes/sonicparanoid/build.sh
@@ -1,1 +1,11 @@
-python setup.py install --single-version-externally-managed --record=record.txt
+echo Building module for ortholog groups inference...
+# enter the source directory
+cd ./sonicparanoid/quick_multi_paranoid/
+# build the modules
+make qa
+# go back to root
+cd ../../
+pwd
+# install sonicparanoid
+echo Building sonicparanoid...
+python setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
