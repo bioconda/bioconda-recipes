25d8d121af8636588ee3cded2b01d716e6c448bc
likelet <zhaoqi699@sina.com>
2018-10-25 10:47:01 +0000

    update

--- recipes/r-ggbiplot/build.sh
+++ recipes/r-ggbiplot/build.sh
@@ -1,2 +1,2 @@
-R -e 'httr::set_config(httr::config(ssl_verifypeer = 0L)); install.packages("devtools", repos="http://mirrors.ebi.ac.uk/CRAN/"); library(devtools); install_github("vqv/ggbiplot"); library(ggbiplot)'
-
+#!/bin/bash
+R CMD INSTALL --build .
