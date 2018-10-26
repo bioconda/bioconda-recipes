02df8afd0b0ee8e534b95e42a98603f48e7b4ba4
Michael Knudsen <micknudsen@gmail.com>
2018-10-26 11:24:22 +0200

    Nope!

--- recipes/fqzcomp/build.sh
+++ recipes/fqzcomp/build.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
-export CPATH=${PREFIX}/include
-export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
-export LDFLAGS="-L$PREFIX/lib -ltinfo"
-export LIBRARY_PATH=${PREFIX}/lib
 make
 make install
