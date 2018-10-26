c872a662776bfc42c74b226a41ca6605bd8e614f
John Lees <lees.john6@gmail.com>
2018-10-26 11:52:45 -0400

    Dynamic linking on OS X

--- recipes/ska/build.sh
+++ recipes/ska/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-static -static-libstdc++ -static-libgcc -L$PREFIX/lib -lz"
+if [ "$(uname)" == "Darwin" ]; then
+   make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-L$PREFIX/lib -lz"
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+   make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-static -static-libstdc++ -static-libgcc -L$PREFIX/lib -lz"
+fi
+
 install -d ${PREFIX}/bin
 install bin/ska ${PREFIX}/bin
