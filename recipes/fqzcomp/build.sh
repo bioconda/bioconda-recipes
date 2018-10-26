62ca1e8e01f2c79ddc9712091a4e7e6651522345
Michael Knudsen <micknudsen@gmail.com>
2018-10-26 11:21:37 +0200

    Maybe these exports will do the trick

--- recipes/fqzcomp/build.sh
+++ recipes/fqzcomp/build.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-
+export CPATH=${PREFIX}/include
+export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
+export LDFLAGS="-L$PREFIX/lib -ltinfo"
+export LIBRARY_PATH=${PREFIX}/lib
 make
 make install
