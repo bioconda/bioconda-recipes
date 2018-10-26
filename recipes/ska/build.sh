c8e6cf02e98f4b9462019f224ea2d8557ed29984
John Lees <jl11@sanger.ac.uk>
2018-10-26 16:21:38 +0100

    Try static linking

--- recipes/ska/build.sh
+++ recipes/ska/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS -lz"
-make install
+make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-static -static-libstdc++ -static-libgcc -L$PREFIX/lib -lz"
+install -d ${PREFIX}/bin
+install bin/ska ${PREFIX}/bin
