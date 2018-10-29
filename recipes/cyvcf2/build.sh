6eb7f270b57dc6eaeafa8bd762848aa80ee28d1b
vladsaveliev <vladislav.sav@gmail.com>
2018-10-29 16:33:58 +1100

    cyvcf2: add all src for htslib; Add autoconf deps; Compile htslib in build.sh

--- recipes/cyvcf2/build.sh
+++ recipes/cyvcf2/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+set -x
+
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
+
+cd htslib
+autoheader
+autoconf
+./configure --enable-s3 --disable-lzma --disable-bz2
+make
+cd ..
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
