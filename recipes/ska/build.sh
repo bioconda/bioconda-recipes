ab44e76c9190dc8f2a0993b99a8a7b2e274e5829
John Lees <jl11@sanger.ac.uk>
2018-10-29 15:17:28 +0000

    ldd command test

--- recipes/ska/build.sh
+++ recipes/ska/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-   make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-L$PREFIX/lib -lz"
-fi
-
-if [ "$(uname)" == "Linux" ]; then
-   make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-static -static-libstdc++ -static-libgcc -L$PREFIX/lib -lz"
-fi
+make CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="-L$PREFIX/lib -lz"
 
 install -d ${PREFIX}/bin
 install bin/ska ${PREFIX}/bin
