1d41304d9f0830d64eda0b00b9df30dd6663b611
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-11-01 14:49:22 -0300

    Added make command

--- recipes/sortmerna/3.0.0/build.sh
+++ recipes/sortmerna/3.0.0/build.sh
@@ -3,6 +3,7 @@ mkdir -pv $PREFIX/bin
 mkdir -p build
 cd build
 cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DSRC_ROCKSDB=1 -DSET_ROCKSDB=1 ../
+make
 cp $SMR_HOME/build/src/indexdb $PREFIX/bin
 cp $SMR_HOME/build/src/sortmerna $PREFIX/bin
 cp $PREFIX/scripts/{merge-paired-reads.sh,unmerge-paired-reads.sh}  $PREFIX/bin
