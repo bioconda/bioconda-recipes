c3921b028cb8918cdf4fe8b8d215227dfa02e80b
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-11-01 15:03:39 -0300

    Added building of rockdb

--- recipes/sortmerna/3.0.0/build.sh
+++ recipes/sortmerna/3.0.0/build.sh
@@ -2,8 +2,14 @@ export SMR_HOME=$PREFIX
 mkdir -pv $PREFIX/bin
 mkdir -p build
 cd build
+mdir -p SMR_HOME/3rdparty/rocksdb/build/Release
+pushd SMR_HOME/3rdparty/rocksdb/build/Release
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DPORTABLE=1 -DWITH_ZLIB=1 -DWITH_TESTS=0 -DWITH_TOOLS=0 ../..
+make
+popd
 cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DSRC_ROCKSDB=1 -DSET_ROCKSDB=1 ../
 make
+popd
 cp $SMR_HOME/build/src/indexdb $PREFIX/bin
 cp $SMR_HOME/build/src/sortmerna $PREFIX/bin
 cp $PREFIX/scripts/{merge-paired-reads.sh,unmerge-paired-reads.sh}  $PREFIX/bin
