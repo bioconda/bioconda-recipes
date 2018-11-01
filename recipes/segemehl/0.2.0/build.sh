06f9c6497e96afbea7c8f9499a642d4855dfea9b
Joerg Fallmann <fall@tbi.univie.ac.at>
2018-11-01 18:14:56 +0100

    segemehl3

--- recipes/segemehl/0.2.0/build.sh
+++ recipes/segemehl/0.2.0/build.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
 export LIBRARY_PATH=${PREFIX}/lib
 
-make all -j 1 ## do not use >1 make threads!
+cd segemehl
+make -j${CPU_COUNT} ## do not use >1 make threads!
 
 for i in *.x ; do
     install ${i} ${PREFIX}/bin
--- recipes/segemehl/build.sh
+++ recipes/segemehl/build.sh
@@ -1,1 +1,11 @@
-0.3.0/build.sh
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
+export LIBRARY_PATH=${PREFIX}/lib
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+
+make -j${CPU_COUNT}  ## do not use >1 make threads!
+
+for i in *.x ; do
+    install ${i} ${PREFIX}/bin
+done
