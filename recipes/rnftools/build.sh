84d042b2ef686ed3ea37ec0e4431d7eddb83f0cf
Karel Břinda <karel.brinda@gmail.com>
2018-11-01 12:07:14 -0400

    Fix build

--- recipes/rnftools/build.sh
+++ recipes/rnftools/build.sh
@@ -3,4 +3,4 @@
 set -e
 set -o pipefail
 
-PROPHYLE_PACKBIN=1 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install -vvv --single-version-externally-managed --record=record.txt
