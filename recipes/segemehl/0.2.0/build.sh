c336b151a29a2f7831c8ece62977b720231f3417
Joerg Fallmann <fall@tbi.univie.ac.at>
2018-11-01 19:14:28 +0100

    0.3 running, 0.2 compiler error

--- recipes/segemehl/0.2.0/build.sh
+++ recipes/segemehl/0.2.0/build.sh
@@ -3,8 +3,7 @@
 export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
 export LIBRARY_PATH=${PREFIX}/lib
 
-cd segemehl
-make all -j${CPU_COUNT} ## do not use >1 make threads!
+make -j${CPU_COUNT} ## do not use >1 make threads!
 
 for i in *.x ; do
     install ${i} ${PREFIX}/bin
