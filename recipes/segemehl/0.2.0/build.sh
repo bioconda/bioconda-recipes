cf6c4299e1219cd21a30d53dc0b3d11525b48326
Joerg Fallmann <fall@tbi.univie.ac.at>
2018-11-01 19:32:14 +0100

    Check

--- recipes/segemehl/0.2.0/build.sh
+++ recipes/segemehl/0.2.0/build.sh
@@ -3,6 +3,7 @@
 export C_INCLUDE_PATH=${PREFIX}/include:${PREFIX}/include/ncurses
 export LIBRARY_PATH=${PREFIX}/lib
 
+cd segemehl
 make segemehl.x -j${CPU_COUNT}
 make testrealign.x -j${CPU_COUNT}
 make lack.x -j${CPU_COUNT}
