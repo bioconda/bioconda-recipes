70f86cd6cbe3e748bb220acfe4e8bc1b975a8408
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-11-01 12:05:58 -0300

    Updated to newer build system

--- recipes/sortmerna/3.0.0/build.sh
+++ recipes/sortmerna/3.0.0/build.sh
@@ -1,4 +1,8 @@
+export SMR_HOME=$PREFIX
 mkdir -pv $PREFIX/bin
-bash build.sh
-cp {sortmerna,indexdb_rna} $PREFIX/bin
-cp scripts/{merge-paired-reads.sh,unmerge-paired-reads.sh}  $PREFIX/bin
+mkdir -p build
+cd build
+cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DSRC_ROCKSDB=1 -DSET_ROCKSDB=1 ../
+cp $SMR_HOME/build/src/indexdb $PREFIX/bin
+cp $SMR_HOME/build/src/sortmerna $PREFIX/bin
+cp $PREFIX/scripts/{merge-paired-reads.sh,unmerge-paired-reads.sh}  $PREFIX/bin
