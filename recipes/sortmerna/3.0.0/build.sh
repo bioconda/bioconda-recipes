7f441101c98560356dd27379b9e46f317877bcc9
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-11-01 17:46:27 -0300

    Fix for build

--- recipes/sortmerna/3.0.0/build.sh
+++ recipes/sortmerna/3.0.0/build.sh
@@ -2,10 +2,9 @@ export SMR_HOME=$PREFIX
 mkdir -pv $PREFIX/bin
 mkdir -p build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DPORTABLE=1 -DWITH_ZLIB=1 -DWITH_TESTS=0 -DWITH_TOOLS=0 ../..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DSRC_ROCKSDB=1 -DSRC_RAPIDJSON=1 -DSET_ROCKSDB=1 ..
 mkdir -p $SMR_HOME/3rdparty/rocksdb/build/Release
 pushd $SMR_HOME/3rdparty/rocksdb/build/Release
-cd 
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DPORTABLE=1 -DWITH_ZLIB=1 -DWITH_TESTS=0 -DWITH_TOOLS=0 ..
 make
 popd
