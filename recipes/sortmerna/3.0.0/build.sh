28a1581a9f100bb43314e3d804e82f9449eebe93
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-11-02 20:13:15 -0300

    Fix

--- recipes/sortmerna/3.0.0/build.sh
+++ recipes/sortmerna/3.0.0/build.sh
@@ -7,7 +7,8 @@ ls
 ls ..
 mkdir -p $SMR_HOME/3rdparty/rocksdb
 pushd $SMR_HOME/3rdparty/rocksdb
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DPORTABLE=1 -DWITH_ZLIB=1 -DWITH_TESTS=0 -DWITH_TOOLS=0 ../..
+cd $PREFIX
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DPORTABLE=1 -DWITH_ZLIB=1 -DWITH_TESTS=0 -DWITH_TOOLS=0 .
 ls
 ls ..
 make
