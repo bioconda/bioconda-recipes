e91d63c80015e6ef10290277ded85744a32a4828
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-11-02 18:05:04 -0300

    fixed dependecy path

--- recipes/sortmerna/3.0.0/build.sh
+++ recipes/sortmerna/3.0.0/build.sh
@@ -3,8 +3,8 @@ mkdir -pv $PREFIX/bin
 mkdir -p build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DSRC_ROCKSDB=1 -DSRC_RAPIDJSON=1 -DSET_ROCKSDB=1 ..
-mkdir -p $SMR_HOME/3rdparty/rocksdb/build
-pushd $SMR_HOME/3rdparty/rocksdb/build
+mkdir -p $SMR_HOME/3rdparty/rocksdb
+pushd $SMR_HOME/3rdparty/rocksdb
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DPORTABLE=1 -DWITH_ZLIB=1 -DWITH_TESTS=0 -DWITH_TOOLS=0 ..
 make
 popd
