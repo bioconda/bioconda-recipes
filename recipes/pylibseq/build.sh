3c876a7886c9117da1b923994c4eab4532a20cce
molpopgen <krthornt@uci.edu>
2018-11-06 11:33:33 -0800

    do not link to tbb

--- recipes/pylibseq/build.sh
+++ recipes/pylibseq/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib -ltbb"
+#export LDFLAGS="-L${PREFIX}/lib -ltbb"
 #On OS X, we have to force Python 
 #packages to compile with GCC.
 #Conda setup will default to enforcing 
