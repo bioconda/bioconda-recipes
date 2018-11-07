b5972250d1bd7501a0645fc84eb6f7e42aca2413
Pavankumar Videm <videmp@informatik.uni-freiburg.de>
2018-11-07 20:32:24 +0100

    modify makefile flags using sed

--- recipes/eden/v1.1/build.sh
+++ recipes/eden/v1.1/build.sh
@@ -1,6 +1,6 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-sed -i "s/-c EDeN.cc/$CFLAGS $LDFLAGS -c EDeN.cc/" Makefile
+sed -i "s|-c EDeN.cc|$CFLAGS $LDFLAGS -c EDeN.cc|" Makefile
 make
 mkdir -p ${PREFIX}/bin
 cp EDeN ${PREFIX}/bin
