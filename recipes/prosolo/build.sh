9e50b26289ad3f0a700d29ce49877e01f321e807
David Laehnemann <david.laehnemann@hhu.de>
2018-11-07 15:37:48 +0100

    prosolo: manually set HOME for OSX, which is not set properly and needed by cargo

--- recipes/prosolo/build.sh
+++ recipes/prosolo/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -euo
 
+# taken from yacrd recipe, see: https://github.com/bioconda/bioconda-recipes/blob/2b02c3db6400499d910bc5f297d23cb20c9db4f8/recipes/yacrd/build.sh
+if [ "$(uname)" == "Darwin" ]; then
+    # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
+    export HOME="/Users/distiller"
+fi
+
 # build statically linked binary with Rust
 C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib:/usr/lib64 cargo install --path . --root $PREFIX --verbose --debug
