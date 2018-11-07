5e910b5e455434b2c31c6477eb56fe5a06319604
Björn Grüning <bjoern@gruenings.eu>
2018-11-07 21:07:17 +0100

    Update build.sh

--- recipes/eden/v1.1/build.sh
+++ recipes/eden/v1.1/build.sh
@@ -1,5 +1,8 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+
+export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
+
 sed -i "s|-c EDeN.cc|$CFLAGS $LDFLAGS -c EDeN.cc|" Makefile
 make
 mkdir -p ${PREFIX}/bin
