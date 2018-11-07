c17998693d65afddb21fd4dd9d5d93d68d918d9b
Pavankumar Videm <videmp@informatik.uni-freiburg.de>
2018-11-07 17:41:06 +0100

    add include and lib paths to build.sh

--- recipes/eden/v1.1/build.sh
+++ recipes/eden/v1.1/build.sh
@@ -1,5 +1,5 @@
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CXXFLAGS="-I$PREFIX/include"
+export LIBS="-L$PREFIX/lib"
 make
 mkdir -p ${PREFIX}/bin
 cp EDeN ${PREFIX}/bin
