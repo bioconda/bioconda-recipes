f6c47a7158ac6b1f9326600e6fcee69a07ee2216
Pavankumar Videm <videmp@informatik.uni-freiburg.de>
2018-11-08 17:45:00 +0100

    remove CXX

--- recipes/eden/1.1/build.sh
+++ recipes/eden/1.1/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-sed -i "s|-c EDeN.cc|$CFLAGS -c EDeN.cc|" Makefile
+#sed -i "s|-c EDeN.cc|$CFLAGS -c EDeN.cc|" Makefile
+sed -i.bak "s/CXX=g++//g" Makefile
+cat Makefile
 make
 mkdir -p ${PREFIX}/bin
 cp EDeN ${PREFIX}/bin
