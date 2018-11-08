3c16ed2f68814e60e860c0d9354ac3705bd44b4d
Tara <tfursten@asu.edu>
2018-11-08 10:22:13 -0700

    remove mtsv add license

--- recipes/mtsv/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-bash install.sh --prefix=$PREFIX
-$PYTHON setup.py install
--- recipes/mtsv/install.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash -e -x
-cd mtsv/ext
-#cargo test
-g++ -std=c++11 -pthread -static-libstdc++ ../mtsv_prep/taxidtool.cpp -o mtsv-db-build
-cargo build --release
