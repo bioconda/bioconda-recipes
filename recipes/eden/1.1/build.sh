3692c715c04dcc3f2a3dd273e18356369fb66d65
Björn Grüning <bjoern@gruenings.eu>
2018-11-08 18:23:27 +0100

    add more flags

--- recipes/eden/1.1/build.sh
+++ recipes/eden/1.1/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
+export CPPFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 #sed -i "s|-c EDeN.cc|$CFLAGS -c EDeN.cc|" Makefile
 sed -i.bak "s/CXX=g++//g" Makefile
 cat Makefile
