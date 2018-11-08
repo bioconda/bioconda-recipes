8424f3db7484dff978509be4a06b27175201c3fd
Björn Grüning <bjoern@gruenings.eu>
2018-11-08 21:04:33 +0100

    Update build.sh

--- recipes/eden/1.1/build.sh
+++ recipes/eden/1.1/build.sh
@@ -5,6 +5,8 @@ export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
 
+export OBABEL="${LDFLAGS}"
+
 sed -i.bak "s|-c EDeN.cc|$CPPFLAGS -c EDeN.cc|" Makefile
 sed -i.bak "s/CXX=g++//g" Makefile
 cat Makefile
