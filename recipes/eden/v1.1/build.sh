2e88d45ec55b56da4b6dc28ce1ac853379448f94
Pavankumar Videm <videmp@informatik.uni-freiburg.de>
2018-11-08 16:40:53 +0100

    LDFLAGS not used in makefile

--- recipes/eden/v1.1/build.sh
+++ recipes/eden/v1.1/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
 export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-
-sed -i "s|-c EDeN.cc|$CFLAGS $LDFLAGS -c EDeN.cc|" Makefile
+sed -i "s|-c EDeN.cc|$CFLAGS -c EDeN.cc|" Makefile
 make
 mkdir -p ${PREFIX}/bin
 cp EDeN ${PREFIX}/bin
