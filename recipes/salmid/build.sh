d368d95596861671d50ad99a15c20f0a60ab3073
Peter van Heusden <pvh@sanbi.ac.za>
2018-11-09 12:24:58 +0200

    Fix install step

--- recipes/salmid/build.sh
+++ recipes/salmid/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp SalmID.py version.py invA_mers_dict rpoB_mers_dict $CONDA_PREFIX/bin
-chmod a+x $CONDA_PREFIX/bin/SalmID.py
+cp SalmID.py version.py invA_mers_dict rpoB_mers_dict $PREFIX/bin
+chmod a+x $PREFIX/bin/SalmID.py
