9eb1fac47f09c2e33c4029f92d6e86d83d5b5063
Pavankumar Videm <videmp@informatik.uni-freiburg.de>
2018-11-14 12:15:10 +0100

    removed -static

--- recipes/eden/1.1/build.sh
+++ recipes/eden/1.1/build.sh
@@ -7,8 +7,9 @@ export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
 
 export OBABEL="${LDFLAGS}"
 
+sed -i.bak "s/CXXFLAGS=/CXXFLAGS+=/" Makefile
 sed -i.bak "s|-c EDeN.cc|$CPPFLAGS -c EDeN.cc|" Makefile
-sed -i.bak "s/CXX=g++//g" Makefile
+sed -i.bak "s/CXX=g++\|-static//g" Makefile
 cat Makefile
 make
 mkdir -p ${PREFIX}/bin
