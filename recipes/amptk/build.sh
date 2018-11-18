6759c63a51b8c3cbd0d473997659c56f0348834a
Jon Palmer <nextgenusfs@gmail.com>
2018-11-18 15:41:53 -0800

    update amptk to v1.2.5

--- recipes/amptk/build.sh
+++ recipes/amptk/build.sh
@@ -9,3 +9,5 @@ cp -R $SRC_DIR/util $AMPTK_HOME/util
 cp -R $SRC_DIR/lib $AMPTK_HOME/lib
 cp -R $SRC_DIR/DB $AMPTK_HOME/DB
 ln -s $AMPTK_HOME/bin/amptk $PREFIX/bin/amptk
+ln -s $AMPTK_HOME/util/bold2amptk.py $PREFIX/bin/bold2amptk.py
+ln -s $AMPTK_HOME/util/bold2utax.py $PREFIX/bin/bold2utax.py
