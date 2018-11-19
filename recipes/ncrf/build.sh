478b038046899a740c5eaaf0a5401ec54235199a
Bob Harris <rsharris@bx.psu.edu>
2018-11-19 14:42:47 -0500

    including absolute path in symlinks

--- recipes/ncrf/build.sh
+++ recipes/ncrf/build.sh
@@ -10,9 +10,9 @@ mv NCRF $PREFIX/bin
 
 chmod +x *.py
 cp *.py $PREFIX/bin/
-ln -s ncrf_to_bed.py $PREFIX/bin/ncrf_to_bed
-ln -s ncrf_summary.py $PREFIX/bin/ncrf_summary
-ln -s ncrf_sort.py $PREFIX/bin/ncrf_sort
-ln -s ncrf_cat.py $PREFIX/bin/ncrf_cat
-ln -s ncrf_resolve_overlaps.py $PREFIX/bin/ncrf_resolve_overlaps
-ln -s ncrf_consensus_filter.py $PREFIX/bin/ncrf_consensus_filter
+ln -s $PREFIX/bin/ncrf_to_bed.py $PREFIX/bin/ncrf_to_bed
+ln -s $PREFIX/bin/ncrf_summary.py $PREFIX/bin/ncrf_summary
+ln -s $PREFIX/bin/ncrf_sort.py $PREFIX/bin/ncrf_sort
+ln -s $PREFIX/bin/ncrf_cat.py $PREFIX/bin/ncrf_cat
+ln -s $PREFIX/bin/ncrf_resolve_overlaps.py $PREFIX/bin/ncrf_resolve_overlaps
+ln -s $PREFIX/bin/ncrf_consensus_filter.py $PREFIX/bin/ncrf_consensus_filter
