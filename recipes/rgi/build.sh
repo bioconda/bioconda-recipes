7dffe745ab8662e91bb6f2fea732aa1b4d5d235c
Mary Jo Ramos <mramos@phac5001660.cscscience.ca>
2018-11-21 14:04:54 -0600

    update build number

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
 cd card-data
-
 mv card.json ..
-
 cd ..
-
-./rgi load -i card.json
-
 rm -rf card-data
+./rgi load -i card.json
+rm card.json
