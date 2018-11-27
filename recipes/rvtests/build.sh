d45801e95f1d9bb0a7fded7616f62eb68ca52c65
zhanxw <zhanxw@users.noreply.github.com>
2018-11-27 15:23:56 -0600

    Update build.sh

--- recipes/rvtests/build.sh
+++ recipes/rvtests/build.sh
@@ -1,11 +1,15 @@
-# export these flags so that zlib.h can be found
-export CFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+## export these flags so that zlib.h can be found
+#export CFLAGS="-I$PREFIX/include"
+#export CXXFLAGS="-I$PREFIX/include"
+#export LDFLAGS="-L$PREFIX/lib"
 
 # Build
+(cd third && tar zvxf zlib-1.2.8.tar.gz && cd zlib-1.2.8 && ./configure && make)
+(cd third && tar jvxf tabix-0.2.6.tar.bz2 && cd tabix-0.2.6 && sed -i 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:' Makefile && make)
+(cd third && tar jvxf samtools-0.1.19.tar.bz2 && cd samtools-0.1.19 && sed -i 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:' Makefile bcftools/Makefile && make)
+
 make
 
 # Install
 mkdir -p $PREFIX/bin
-cp executable/rvtests $PREFIX/bin
+find executable -executable -exec cp {} $PREFIX/bin \;
