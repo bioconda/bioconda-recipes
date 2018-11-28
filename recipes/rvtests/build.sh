8d2c1ed70d2767e38cafd7b628360e1af195e420
zhanxw <zhanxw@users.noreply.github.com>
2018-11-28 01:18:57 -0600

    Update build.sh

--- recipes/rvtests/build.sh
+++ recipes/rvtests/build.sh
@@ -9,6 +9,11 @@
 (cd third && tar jxf samtools-0.1.19.tar.bz2 && ln -s -f samtools-0.1.19 samtools && cd samtools-0.1.19 && sed -i 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8 -I../../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8 -L../../zlib-1.2.8:' Makefile bcftools/Makefile && make lib-recur)
 (sed -i 's:CXX_FLAGS = -O2 -DNDEBUG:CXX_FLAGS = -O2 -DNDEBUG -I../third/zlib-1.2.8:' libsrc/Makefile)
 
+#export CFLAGS="-I$PREFIX/include"
+#export LDFLAGS="-L$PREFIX/lib"
+(sed -i 's:^CXX_INCLUDE =:CXX_INCLUDE = -I$(PREFIX)/include:' Makefile.common)
+(sed -i 's:^CXX_LIB =:CXX_LIB = -L$(PREFIX)/lib:' Makefile.common)
+
 make
 
 # Install
