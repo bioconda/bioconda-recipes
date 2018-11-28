d3b8cf3220c9fce408b3193ccd3bbaa6892f3d94
zhanxw <zhanxw@users.noreply.github.com>
2018-11-28 10:11:11 -0600

    Update build.sh

--- recipes/rvtests/build.sh
+++ recipes/rvtests/build.sh
@@ -14,7 +14,7 @@
 (sed -i 's:^CXX_INCLUDE =:CXX_INCLUDE = -I${PREFIX}/include:' Makefile.common)
 (sed -i 's:^CXX_LIB =:CXX_LIB = -L${PREFIX}/lib:' Makefile.common)
 
-make
+make STATIC_FLAG=''
 
 # Install
 mkdir -p $PREFIX/bin
