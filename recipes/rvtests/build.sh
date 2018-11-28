0a06a8342b8912464cdae02354dacdcfd0d1146d
zhanxw <zhanxw@users.noreply.github.com>
2018-11-27 21:46:02 -0600

    Update build.sh

--- recipes/rvtests/build.sh
+++ recipes/rvtests/build.sh
@@ -6,7 +6,7 @@
 # Build
 (cd third && tar zvxf zlib-1.2.8.tar.gz && cd zlib-1.2.8 && ./configure && make)
 (cd third && tar jvxf tabix-0.2.6.tar.bz2 && cd tabix-0.2.6 && sed -i 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8:'  Makefile && make libtabix.a)
-(cd third && tar jvxf samtools-0.1.19.tar.bz2 && cd samtools-0.1.19 && sed -i 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8:' Makefile && sed -i 's:^CFLAGS=:CFLAGS=-I../../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../../zlib-1.2.8:' bcftools/Makefile && make)
+(cd third && tar jvxf samtools-0.1.19.tar.bz2 && cd samtools-0.1.19 && sed -i 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8 -I../../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8 -L../../zlib-1.2.8:' Makefile bcftools/Makefile && make)
 
 make
 
