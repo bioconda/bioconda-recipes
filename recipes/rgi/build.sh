a537c9d753fefc348de039e9ecfd51cf48c510df
Mary Jo Ramos <mramos@phac5001660.cscscience.ca>
2018-11-30 14:29:59 -0600

    fix linting errors

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -3,5 +3,5 @@
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
 # Load the CARD database JSON file
-#python `which rgi` load -i card-data/card.json
-#rm -rf card-data
+python `which rgi` load -i card-data/card.json
+rm -rf card-data
