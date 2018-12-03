7765b0532110e13e3bfb3ba5ed3e20bc808a8ef1
Rob Patro <rob-p@users.noreply.github.com>
2018-12-03 16:26:03 -0500

    Update build.sh

--- recipes/salmon/build.sh
+++ recipes/salmon/build.sh
@@ -48,6 +48,7 @@ mkdir -p $PREFIX/lib
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCONDA_BUILD=TRUE -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON ..
+make
 echo "unit test executable"
 ./src/unitTests
 echo "cmake-powered unit test"
