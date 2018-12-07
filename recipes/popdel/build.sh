fcf6ee267f4f558aa43116c39c1ebfe712a20d6e
Sebastian Roskosch <Sebastian.Roskosch@bihealth.de>
2018-12-07 09:38:48 +0100

    Automatically get version from PKG_VERSION

--- recipes/popdel/build.sh
+++ recipes/popdel/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-VERSION=1.0.5
 DATE=2018-12-06
 
-make CXX=${CXX} VERSION=${VERSION} DATE=${DATE}
+make CXX=${CXX} VERSION=${PKG_VERSION} DATE=${DATE}
 mkdir -p ${PREFIX}/bin
 cp popdel ${PREFIX}/bin
