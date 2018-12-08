39dd094049e7fd1f027dd63fcf8d91f4094d2f40
Devon Ryan <dpryan79@gmail.com>
2018-12-08 23:08:10 +0100

    try src/Makevars

--- recipes/bioconductor-roc/build.sh
+++ recipes/bioconductor-roc/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-mkdir -p ~/.R
 echo -e "CC=$CC
 CXXFLAGS=$CXXFLAGS
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
+CXX14=$CXX" > src/Makevars
 $R CMD INSTALL --build .
