d2da23d2beb20e8569aeb1e039be3b2f1caa134a
zhanxw <zhanxw@users.noreply.github.com>
2018-12-09 01:33:24 -0600

    Update build.sh

--- recipes/rvtests/build.sh
+++ recipes/rvtests/build.sh
@@ -1,25 +1,18 @@
 set -xe
 echo "Start build..."
-if [[ $(uname -s) == Darwin ]]; then
-  echo "test braces"
-  (cd .. && pwd && ls)
-  gcc --version
-  echo "check build script..."
-  cat /Users/distiller/project/miniconda/conda-bld/rvtests_*/work/conda_build.sh
-  echo "done"
-fi
 
 ## export these flags so that zlib.h can be found
 #export CFLAGS="-I$PREFIX/include"
 #export CXXFLAGS="-I$PREFIX/include"
 #export LDFLAGS="-L$PREFIX/lib"
 
-# Build
+# Build zlib, and then tabix, samtools against zlib
 (cd third && tar zxf zlib-1.2.8.tar.gz && cd zlib-1.2.8 && ./configure && make)
 (cd third && tar jxf tabix-0.2.6.tar.bz2 && ln -s -f tabix-0.2.6 tabix && cd tabix-0.2.6 && sed -i.bak 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8:'  Makefile && make libtabix.a)
 (cd third && tar jxf samtools-0.1.19.tar.bz2 && ln -s -f samtools-0.1.19 samtools && cd samtools-0.1.19 && sed -i.bak 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8 -I../../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8 -L../../zlib-1.2.8:' Makefile bcftools/Makefile && make lib-recur)
 (sed -i.bak 's:CXX_FLAGS = -O2 -DNDEBUG:CXX_FLAGS = -O2 -DNDEBUG -I../third/zlib-1.2.8:' libsrc/Makefile)
 
+# Use system zlib
 #export CFLAGS="-I$PREFIX/include"
 #export LDFLAGS="-L$PREFIX/lib"
 (sed -i.bak 's:^CXX_INCLUDE =:CXX_INCLUDE = -I${PREFIX}/include:' Makefile.common)
@@ -38,14 +31,6 @@ if [[ $(uname -s) == Darwin ]]; then
  ' regression/BoltLMM.cpp
 
 
-  ## clang does not support openmp
-  #sed -i.bak 's:-fopenmp::' libBgen/Makefile
-  #sed -i.bak 's:-fopenmp::' libVcf/Makefile
-  #sed -i.bak 's:-fopenmp::' src/Makefile
-  
-  echo "show Makefile"
-  cat src/Makefile
-  echo "start build..."
   make STATIC_FLAG='-fopenmp'
 else
   # hack this flag to link against rt, so clock_gettime can be linked
@@ -57,6 +42,7 @@ echo "Install..."
 mkdir -p $PREFIX/bin
 echo "Copying..."
 if [[ $(uname -s) == Darwin ]]; then
+  # Mac does not have `-executable` option
   find executable/ -perm +111 -type f -exec cp {} $PREFIX/bin \;
 else
   find executable/ -executable -type f -exec cp {} $PREFIX/bin \;
