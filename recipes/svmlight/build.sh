a9173461e9bbb9bff9b3bbb59a5700b06e87241f
Alexander Peltzer <alex.peltzer@gmail.com>
2018-12-12 16:45:41 +0100

    Update build script to copy executables

--- recipes/svmlight/build.sh
+++ recipes/svmlight/build.sh
@@ -1,1 +1,11 @@
-make all
+#!/bin/bash
+set -eu -o pipefail
+
+# ## Binary install with wrappers
+
+mkdir -p "${PREFIX}/bin"
+
+make all 
+
+mv svm_learn "${PREFIX}/bin/"
+mv svm_classify "${PREFIX}/bin/"
