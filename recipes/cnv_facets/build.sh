efcc45679e53f6dfab34911216c8d0d81086a764
dariober <dario.beraldi@gmail.com>
2018-12-14 17:36:31 +0000

    Test fixed requirements

--- recipes/cnv_facets/build.sh
+++ recipes/cnv_facets/build.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-samtools view 
-
-function install_htslib(){
-    # Download and install htslib. Compiled stuff is in `pwd`/htslib 
-    pushd .
-    rm -f htslib-1.8.tar.bz2
-    wget https://github.com/samtools/htslib/releases/download/1.8/htslib-1.8.tar.bz2
-    tar xf htslib-1.8.tar.bz2
-    rm htslib-1.8.tar.bz2
-    mv htslib-1.8 htslib
-    cd htslib
-    ./configure --prefix=`pwd`
-    make -j 4
-    make install
-    popd
-}
-git clone https://github.com/mskcc/facets.git
-pushd .
-cd facets/inst/extcode/
-echo 'INSTALLING HTSLIB'
-install_htslib
-echo 'HTSLIB DONE'
-ln -s `pwd`/htslib/lib/libhts.a `pwd`/htslib/lib/libhts-static.a
-echo 'INSTALLING SNP-PILEUP'
-g++ -std=c++11 -I `pwd`/htslib/include snp-pileup.cpp -L `pwd`/htslib/lib -lhts-static -o snp-pileup -lcurl -lz -lpthread -lcrypto -llzma -lbz2
-./snp-pileup --help
-popd
-
-echo 'SNP-PILEUP DONE'
-
-# $R -e 'library(RColorBrewer); print("DONE")'
-
+mkdir -p $PREFIX/bin
 chmod a+x setup.sh
 ./setup.sh --bin_dir $PREFIX/bin
