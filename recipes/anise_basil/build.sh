8b0c5f33e47962f3701b238f1a3afa20ebdd2207
Björn Grüning <bjoern@gruenings.eu>
2018-12-14 21:46:59 +0100

    Update build.sh

--- recipes/anise_basil/build.sh
+++ recipes/anise_basil/build.sh
@@ -18,4 +18,4 @@ cd ../build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
 make -j 4 anise basil
 
-#cp bin/anise bin/basil ../scripts/filter_basil.py $PREFIX/bin/
+cp bin/anise bin/basil ../scripts/filter_basil.py $PREFIX/bin/
