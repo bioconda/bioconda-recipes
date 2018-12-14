46ed8105a53fc2c1fccd31c15f800d2d4a4de29b
Björn Grüning <bjoern@gruenings.eu>
2018-12-14 21:21:08 +0100

    help cmake

--- recipes/anise_basil/build.sh
+++ recipes/anise_basil/build.sh
@@ -14,7 +14,8 @@ mv ../lemon lemon
 mv ../seqan seqan
 
 cd ../build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+#cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
 make -j 4 anise basil
 
-cp bin/anise bin/basil ../scripts/filter_basil.py $PREFIX/bin/
+#cp bin/anise bin/basil ../scripts/filter_basil.py $PREFIX/bin/
