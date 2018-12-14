a670d5e6b839689715fe9469b7401679f4b74592
dariober <dario.beraldi@gmail.com>
2018-12-14 10:29:50 +0000

    Test compiling snp-pileup

--- recipes/cnv_facets/build.sh
+++ recipes/cnv_facets/build.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+function install_htslib(){
+    # Download and install htslib. Compiled stuff is in `pwd`/htslib 
+    pushd .
+    rm -f htslib-1.8.tar.bz2
+    wget https://github.com/samtools/htslib/releases/download/1.8/htslib-1.8.tar.bz2
+    tar xf htslib-1.8.tar.bz2
+    rm htslib-1.8.tar.bz2
+    mv htslib-1.8 htslib
+    cd htslib
+    ./configure --prefix=`pwd`
+    make -j 4
+    make install
+    popd
+}
+
+git clone https://github.com/mskcc/facets.git
+cd facets/inst/extcode/
+install_htslib
+ln -s `pwd`/htslib/lib/libhts.a `pwd`/htslib/lib/libhts-static.a
+g++ -std=c++11 -I `pwd`/htslib/include snp-pileup.cpp -L `pwd`/htslib/lib -lhts-static -o snp-pileup -lcurl -lz -lpthread -lcrypto -llzma -lbz2
+
 bash setup.sh --bin_dir $PREFIX/bin
