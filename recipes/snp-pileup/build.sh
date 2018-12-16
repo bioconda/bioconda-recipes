c8f6ba4973ccf032e8343391fd0ebeae97d12208
dariober <dario.beraldi@gmail.com>
2018-12-16 16:53:27 +0000

    Retry with local install of htslib

--- recipes/snp-pileup/build.sh
+++ recipes/snp-pileup/build.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 
+function install_htslib(){
+    # Download and install htslib. Compiled stuff is in `pwd`/htslib
+    pushd .
+    rm -f htslib-1.8.tar.bz2
+    wget https://github.com/samtools/htslib/releases/download/1.8/htslib-1.8.tar.bz2
+    tar xf htslib-1.8.tar.bz2
+    rm htslib-1.8.tar.bz2
+    mv htslib-1.8 htslib
+    cd htslib
+    ./configure --prefix=`pwd`
+    make -j 4
+    make install
+    popd
+}
+
 mkdir -p $PREFIX/bin
 
 cd inst/extcode/
-g++ -std=c++11 snp-pileup.cpp -lhts -o snp-pileup
-chmod a+x snp-pileup
+install_htslib
+ln -s `pwd`/htslib/lib/libhts.a `pwd`/htslib/lib/libhts-static.a
+g++ -std=c++11 -I `pwd`/htslib/include snp-pileup.cpp -L `pwd`/htslib/lib \
+    -lhts-static -o snp-pileup -lcurl -lz -lpthread -lcrypto -llzma -lbz2
+
+snp-pileup --help
+
 mv snp-pileup $PREFIX/bin/
-
-# function install_htslib(){
-#     # Download and install htslib. Compiled stuff is in `pwd`/htslib 
-#     pushd .
-#     rm -f htslib-1.8.tar.bz2
-#     wget https://github.com/samtools/htslib/releases/download/1.8/htslib-1.8.tar.bz2
-#     tar xf htslib-1.8.tar.bz2
-#     rm htslib-1.8.tar.bz2
-#     mv htslib-1.8 htslib
-#     cd htslib
-#     ./configure --prefix=`pwd`
-#     make -j 4
-#     make install
-#     popd 
-# }
-# 
-# mkdir -p $PREFIX/bin
-# 
-# cd inst/extcode/
-# 
-# install_htslib
-# ln -s `pwd`/htslib/lib/libhts.a `pwd`/htslib/lib/libhts-static.a
-# 
-# g++ -std=c++11 -I `pwd`/htslib/include snp-pileup.cpp \
-#     -L `pwd`/htslib/lib -lhts-static -o snp-pileup \
-#     -lcurl -lz -lpthread -lcrypto -llzma -lbz2
-# chmod a+x snp-pileup
-# mv snp-pileup $PREFIX/bin/
-# rm -rf `pwd`/htslib
-
