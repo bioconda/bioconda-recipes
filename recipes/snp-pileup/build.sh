219d5122f3e110e6ec4519a1d52f9d8632a84305
dariober <dario.beraldi@gmail.com>
2018-12-16 20:04:28 +0000

    Dynamic compiling

--- recipes/snp-pileup/build.sh
+++ recipes/snp-pileup/build.sh
@@ -23,12 +23,16 @@ mkdir -p $PREFIX/bin
 
 cd inst/extcode/
 
+g++ -std=c++11 -I$PREFIX/include snp-pileup.cpp \
+     -L$PREFIX/lib -lhts -Wl,-rpath=$PREFIX/lib -o snp-pileup
+
 # install_htslib
-ln -s $PREFIX/lib/libhts.a $PREFIX/lib/libhts-static.a
-
-g++ -std=c++11 -o snp-pileup snp-pileup.cpp -I$PREFIX/include -L$PREFIX/lib -lm -lhts-static -lcurl -lz -lpthread -lcrypto -llzma -lbz2
-
+#ln -s $PREFIX/lib/libhts.a $PREFIX/lib/libhts-static.a
+#
+#g++ -std=c++11 -o snp-pileup snp-pileup.cpp -I$PREFIX/include -L$PREFIX/lib -lm -lhts-static -lcurl -lz -lpthread -lcrypto -llzma -lbz2
+#
 ./snp-pileup --help
-mv snp-pileup $PREFIX/bin/
+ln -s `pwd`/snp-pileup $PREFIX/bin/snp-pileup
+# mv snp-pileup $PREFIX/bin/
 cd $PREFIX
 snp-pileup --help
