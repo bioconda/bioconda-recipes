f41e0b0c2ee24f5cd0f356a077c4aa1275f1e436
hepcat72 <hepcat72@gmail.com>
2018-12-17 09:12:58 -0500

    Cleaned up files.
    Files checked in: recipes/anise_basil/build.sh recipes/anise_basil/meta.yaml

--- recipes/anise_basil/build.sh
+++ recipes/anise_basil/build.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-#git submodule init
-#git submodule update --recursive
-
-#Retrieve the submodules, putting them in ./ext
+#Put the submodules (lemon & seqan) in ext
 cd ext
 rmdir */
-
-#Move submodule lemon
 mv ../lemon lemon
-
-#Move submodule seqan
 mv ../seqan seqan
 
 cd ../build
-#cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
 make -j 4 anise basil
 
