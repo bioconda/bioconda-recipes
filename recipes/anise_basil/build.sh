cd554141b55e4217ae1e9081a28eb4868a78146d
hepcat72 <hepcat72@gmail.com>
2018-12-18 14:55:38 -0500

    License name update and removed make test.
    Files checked in: recipes/anise_basil/build.sh recipes/anise_basil/meta.yaml

--- recipes/anise_basil/build.sh
+++ recipes/anise_basil/build.sh
@@ -9,6 +9,5 @@ mv ../seqan seqan
 cd ../build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
 make -j 4 anise basil
-make test
 
 cp bin/anise bin/basil ../scripts/filter_basil.py $PREFIX/bin/
