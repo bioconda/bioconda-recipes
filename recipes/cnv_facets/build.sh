c7dffb69c0eb541e750e15ef23fb5cbbbc82c7a0
dariober <dario.beraldi@gmail.com>
2018-12-18 17:19:10 +0000

    Install without setup.sh

--- recipes/cnv_facets/build.sh
+++ recipes/cnv_facets/build.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+FACETS_GIT_REF=`grep -P ' *^FACETS_REF' install/install_pkgs.R \
+| sed 's/ //g; s/<-//; s/=//;' \
+| tr "'" '"' \
+| sed 's/FACETS_REF"//; s/".*//'`
+
+$R -e "library(devtools)
+devtools::install_github('mskcc/facets', ref= '$FACETS_GIT_REF', lib= NULL, repos= 'https://cran.r-project.org')"
+
+$R -e 'install.packages("argparse", lib= NULL, repos= "https://cran.r-project.org")'
+
 mkdir -p $PREFIX/bin
 chmod a+x bin/cnv_facets.R
 cp bin/cnv_facets.R $PREFIX/bin/
