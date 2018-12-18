b3c0de63e5f9e7df1b8bc25c510c1ba977de6b9a
dariober <dario.beraldi@gmail.com>
2018-12-18 16:21:22 +0000

    Do not use setup script
    If some dependency is missing, edit meta.yaml or explicitly put
    dependecies build.sh

--- recipes/cnv_facets/build.sh
+++ recipes/cnv_facets/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p $PREFIX/bin
-chmod a+x setup.sh
-./setup.sh --bin_dir $PREFIX/bin
+chmod a+x bin/cnv_facets.R
+cp bin/cnv_facets.R $PREFIX/bin/
+
+cnv_facets.R -d 1 8000 \
+    -t test/data/TCRBOA6-T-WEX.sample.bam \
+    -n test/data/TCRBOA6-N-WEX.sample.bam \
+    -vcf test/data/common.sample.vcf.gz \
+    -o test_out/out
