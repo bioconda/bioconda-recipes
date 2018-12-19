d93db7609534686ee9a50c5a4e91cffced1cf5fa
Dario Beraldi <dario.beraldi@gmail.com>
2018-12-19 11:15:39 +0000

    Cnv facets (#12737)
    * First draft
    
    * Restrict to linux
    
    * Add snp-pileup dependency
    
    * Do not use setup script
    
    If some dependency is missing, edit meta.yaml or explicitly put
    dependecies build.sh
    
    * Install without setup.sh
    
    * Fix R command
    
    * Move test to meta.yaml
    
    * Fix yaml line
    
    * Test moved to build script to resolve #38177 (upload-linux)

--- recipes/cnv_facets/build.sh
+++ recipes/cnv_facets/build.sh
@@ -14,3 +14,9 @@ $R -e 'install.packages("argparse", lib= NULL, repos= "https://cran.r-project.or
 mkdir -p $PREFIX/bin
 chmod a+x bin/cnv_facets.R
 cp bin/cnv_facets.R $PREFIX/bin/
+
+cnv_facets.R -d 1 8000 \
+    -t test/data/TCRBOA6-T-WEX.sample.bam \
+    -n test/data/TCRBOA6-N-WEX.sample.bam \
+    -vcf test/data/common.sample.vcf.gz \
+    -o test_out/out
