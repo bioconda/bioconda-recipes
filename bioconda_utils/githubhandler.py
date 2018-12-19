3399fa70dd8f34a65e0452c42e20ed33872795eb
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-12-19 13:17:56 -0700

    Autobump: show changes to PR if loglevel=debug and dryrun=True

--- bioconda_utils/githubhandler.py
+++ bioconda_utils/githubhandler.py
@@ -157,6 +157,13 @@ class GitHubHandler:
 
         if self.dry_run:
             logger.info("Would modify PR %s", number)
+            if title:
+                logger.debug("New title: %s", title)
+            if labels:
+                logger.debug("New labels: %s", labels)
+            if body:
+                logger.debug("New Body:\n%s\n", body)
+
             return {'number': number}
         logger.info("Modifying PR %s", number)
         return await self.api.patch(self.ISSUES, var_data, data=data)
