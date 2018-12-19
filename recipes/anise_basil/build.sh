830ef897bbb1badbfa164cc38e413591be657819
hepcat72 <hepcat72@gmail.com>
2018-12-19 15:32:55 -0500

    Added a commented `make test` with an explanation.
    Files checked in: recipes/anise_basil/build.sh

--- recipes/anise_basil/build.sh
+++ recipes/anise_basil/build.sh
@@ -10,4 +10,8 @@ cd ../build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON ..
 make -j 4 anise basil
 
+#The basil test is broken due to an absolute reference path in the vcf output
+#Uncomment when fixed
+#make test
+
 cp bin/anise bin/basil ../scripts/filter_basil.py $PREFIX/bin/
