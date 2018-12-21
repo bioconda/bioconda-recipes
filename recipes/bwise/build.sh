d26dd7f5d1745ec9b985017e149789db591e22ab
Malfoy <antoine.limasset@gmail.com>
2018-12-21 13:41:37 +0100

    zlib bug S01ep02

--- recipes/bwise/build.sh
+++ recipes/bwise/build.sh
@@ -4,7 +4,25 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
 cd src;
-make;
+g++ -o n50.o -c N50.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o n50 n50.o -flto -fopenmp -lz
+g++ -o sequencesToNumbers.o -c sequencesToNumbers.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o sequencesToNumbers sequencesToNumbers.o -flto -fopenmp -lz
+g++ -o numbersToSequences.o -c numbersToSequences.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o numbersToSequences numbersToSequences.o -flto -fopenmp -lz
+g++ -o numbersFilter.o -c numbersFilter.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o numbersFilter numbersFilter.o -flto -fopenmp -lz
+g++ -o simulator.o -c simulator.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o simulator simulator.o -flto -fopenmp -lz
+g++ -o path_counter.o -c path_counter.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o path_counter path_counter.o -flto -fopenmp -lz
+g++ -o maximal_sr.o -c Maximal_SR.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o maximal_sr maximal_sr.o -flto -fopenmp -lz
+g++ -o crush_bulle.o -c crush_bulle.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o crush_bulle crush_bulle.o -flto -fopenmp -lz
+g++ -o path_to_kmer.o -c path_to_kmer.cpp -Wall  -Ofast -std=c++11  -flto -pipe -funit-at-a-time  -Wfatal-errors -fopenmp -lz
+g++ -o path_to_kmer path_to_kmer.o -flto -fopenmp -lz
+
 
 
 mkdir -p $PREFIX/bin;
