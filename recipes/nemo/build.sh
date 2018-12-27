88e4e6a953964e3d5dc55437d5446bfdf1922bbb
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-12-27 20:50:26 +0100

    define MAC variable

--- recipes/nemo/build.sh
+++ recipes/nemo/build.sh
@@ -1,2 +1,7 @@
-make GSL_PATH=$PREFIX/ CC=$CXX
-make install BIN_INSTALL=$PREFIX/bin/ LIB_INSTALL=$PREFIX/lib/
+MAC=0
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    MAC=1
+fi
+
+make GSL_PATH=$PREFIX/ CC=$CXX MAC=$MAC
+make install BIN_INSTALL=$PREFIX/bin/ LIB_INSTALL=$PREFIX/lib/ MAC=$MAC
