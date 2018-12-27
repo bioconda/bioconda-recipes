81e33d8fe22d9bd89f8488a29553687157a33e45
zhanxw <zhanxw@users.noreply.github.com>
2018-12-26 22:12:25 -0600

    Update build.sh

--- recipes/rvtests/build.sh
+++ recipes/rvtests/build.sh
@@ -6,11 +6,11 @@ echo "Start build..."
 #export CXXFLAGS="-I$PREFIX/include"
 #export LDFLAGS="-L$PREFIX/lib"
 
-# Build zlib, and then tabix, samtools against zlib
-(cd third && tar zxf zlib-1.2.8.tar.gz && cd zlib-1.2.8 && ./configure && make)
-(cd third && tar jxf tabix-0.2.6.tar.bz2 && ln -s -f tabix-0.2.6 tabix && cd tabix-0.2.6 && sed -i.bak 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8:'  Makefile && make libtabix.a)
-(cd third && tar jxf samtools-0.1.19.tar.bz2 && ln -s -f samtools-0.1.19 samtools && cd samtools-0.1.19 && sed -i.bak 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8 -I../../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8 -L../../zlib-1.2.8:' Makefile bcftools/Makefile && make lib-recur)
-(sed -i.bak 's:CXX_FLAGS = -O2 -DNDEBUG:CXX_FLAGS = -O2 -DNDEBUG -I../third/zlib-1.2.8:' libsrc/Makefile)
+## Build zlib, and then tabix, samtools against zlib
+#(cd third && tar zxf zlib-1.2.8.tar.gz && cd zlib-1.2.8 && ./configure && make)
+#(cd third && tar jxf tabix-0.2.6.tar.bz2 && ln -s -f tabix-0.2.6 tabix && cd tabix-0.2.6 && sed -i.bak 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8:'  Makefile && make libtabix.a)
+#(cd third && tar jxf samtools-0.1.19.tar.bz2 && ln -s -f samtools-0.1.19 samtools && cd samtools-0.1.19 && sed -i.bak 's:^CFLAGS=:CFLAGS=-I../zlib-1.2.8 -I../../zlib-1.2.8:;s:^LIBPATH=:LIBPATH=-L../zlib-1.2.8 -L../../zlib-1.2.8:' Makefile bcftools/Makefile && make lib-recur)
+#(sed -i.bak 's:CXX_FLAGS = -O2 -DNDEBUG:CXX_FLAGS = -O2 -DNDEBUG -I../third/zlib-1.2.8:' libsrc/Makefile)
 
 # Use system zlib
 #export CFLAGS="-I$PREFIX/include"
@@ -19,18 +19,6 @@ echo "Start build..."
 (sed -i.bak 's:^CXX_LIB =:CXX_LIB = -L${PREFIX}/lib:' Makefile.common)
 
 if [[ $(uname -s) == Darwin ]]; then
-  sed -i.bak '3i\
-#include <errno.h>
- ' base/Socket.cpp
-  sed -i.bak '4i\
-#ifdef _OPENMP
-;5i\
-#endif' libVcf/VCFRecord.h
-  sed -i.bak '3i\
-#include <omp.h>
- ' regression/BoltLMM.cpp
-
-
   make STATIC_FLAG='-fopenmp'
 else
   # hack this flag to link against rt, so clock_gettime can be linked
