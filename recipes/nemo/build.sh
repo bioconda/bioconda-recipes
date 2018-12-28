0bb2e8128f9e0f83fb813fdbf58f909c30480fc7
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-12-28 13:59:41 +0100

    try forcing bash shell in order to ensure that echo has -n.

--- recipes/nemo/build.sh
+++ recipes/nemo/build.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    make GSL_PATH=$PREFIX/ CC=$CXX MAC=1
+    make GSL_PATH=$PREFIX/ CC=$CXX MAC=1 SHELL=/bin/bash
 else
-    make GSL_PATH=$PREFIX/ CC=$CXX
+    make GSL_PATH=$PREFIX/ CC=$CXX SHELL=/bin/bash
 fi
 
-make install BIN_INSTALL=$PREFIX/bin/ LIB_INSTALL=$PREFIX/lib/
+make install BIN_INSTALL=$PREFIX/bin/ LIB_INSTALL=$PREFIX/lib/ SHELL=/bin/bash
