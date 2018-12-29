0880f592beb93e087bea9cd681316ff07f5cff91
Joerg Fallmann <fall@tbi.univie.ac.at>
2018-12-29 15:53:42 +0100

    fixing

--- recipes/rnaz/build.sh
+++ recipes/rnaz/build.sh
@@ -2,3 +2,5 @@
 ./configure CFLAGS="-std=gnu89 -g -O2" --prefix=$PREFIX --datadir=$PREFIX/share
 make
 make install
+cp $PREFIX/share/RNAz/perl/*.pl $PREFIX/bin/.
+cp $PREFIX/share/RNAz/perl/RNAz.pm $PREFIX/lib/5.26.2/.
