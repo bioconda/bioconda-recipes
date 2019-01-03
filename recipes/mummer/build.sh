83c082ea70b912ae8c120b68d5607fc714363123
thanhleviet <thanh.le-viet@quadram.ac.uk>
2019-01-03 14:04:47 +0000

    Fix typo

--- recipes/mummer/build.sh
+++ recipes/mummer/build.sh
@@ -41,7 +41,7 @@ show-tiling \
 perl -i -pe 's/defined \(%/\(%/' mummerplot
 
 #Fix escaping symbol @ included in the path to the library (mainly for conda virtual env with galaxy)
-to_fix ="exact-tandems dnadiff mapview mummerplot nucmer promer run-mummer1 run-mummer3"
+to_fix="exact-tandems dnadiff mapview mummerplot nucmer promer run-mummer1 run-mummer3"
 
 for i in $to_fix;do
   perl -i -pe 's/(envs\/\_\_.*)(\K\@)/\\@/' $i
