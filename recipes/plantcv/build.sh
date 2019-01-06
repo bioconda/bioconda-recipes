e33d27e754d1b3987fb3324087d0d137da275add
Noah Fahlgren <noahfahlgren@gmail.com>
2019-01-06 12:54:46 -0600

    Fixed sed syntax issues

--- recipes/plantcv/build.sh
+++ recipes/plantcv/build.sh
@@ -2,6 +2,6 @@
 
 sed -i'' -e 's/versioneer.get_version()/"3.0.3"/g' setup.py
 sed -i'' -e 's/cmdclass=versioneer.get_cmdclass(),//g' setup.py
-sed -i'' -e 's/setup_requires=["pytest-runner"],//g' setup.py
-sed -i'' -e "s/tests_require=['pytest'],//g" setup.py
+sed -i'' -e 's/setup_requires=\["pytest-runner"\],//g' setup.py
+sed -i'' -e "s/tests_require=\['pytest'\],//g" setup.py
 $PYTHON -m pip install --no-deps --ignore-installed .
