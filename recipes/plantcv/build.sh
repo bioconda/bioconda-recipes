155e0c3c57c28c67bbffd9178d6a1a6166846921
Noah Fahlgren <noahfahlgren@gmail.com>
2019-01-06 12:18:16 -0600

    Remove host dependencies and install with pip
    * Comment out `build.sh` commands
    * Removed host dependencies other than python and pip
    * Install with $PYTHON -m pip instead of setup.py
    * Swapped noarch and build number

--- recipes/plantcv/build.sh
+++ recipes/plantcv/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i'' -e 's/versioneer.get_version()/"3.0.3"/g' setup.py
-sed -i'' -e 's/cmdclass=versioneer.get_cmdclass(),//g' setup.py
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+# sed -i'' -e 's/versioneer.get_version()/"3.0.3"/g' setup.py
+# sed -i'' -e 's/cmdclass=versioneer.get_cmdclass(),//g' setup.py
+#$PYTHON -m pip install --single-version-externally-managed --record=record.txt
