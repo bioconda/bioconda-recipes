a80810c6f9ec523719e4ea6fd922ecf65a87ffcb
slacalle <slacalle@imperial.ac.uk>
2019-01-08 10:06:18 +0000

    Minor changes to tidy recipe

--- recipes/mixcr/build.sh
+++ recipes/mixcr/build.sh
@@ -4,4 +4,4 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share
 cp -f mixcr.jar $PREFIX/share/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar
 sed "s/jar\/mixcr.jar/share\/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar/g" mixcr > $PREFIX/bin/mixcr
-chmod +x $PREFIX/bin/mixcr
+chmod a+x $PREFIX/bin/mixcr
