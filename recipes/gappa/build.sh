8a79859e00f5282e15104996b6884d90b3536d05
Gavin Douglas <gavinmdouglas@gmail.com>
2019-01-10 14:58:59 +0000

    added conda_build_config.yaml and llvm-openmp requirement

--- recipes/gappa/build.sh
+++ recipes/gappa/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+CXXFLAGS="$CXXFLAGS"
+LDFLAGS="$LDFLAGS"
+if [ "$(uname)" == Darwin ] ; then
+        CXXFLAGS="$CXXFLAGS -Wl,-rpath ${PREFIX}/lib -L${PREFIX}/lib -I${PREFIX}/include -fopenmp"
+else
+        CXXFLAGS="$CXXFLAGS -fopenmp -g -O3"
+        CXX=g++
+fi
+
 make  
 
 mkdir -p $PREFIX/bin
