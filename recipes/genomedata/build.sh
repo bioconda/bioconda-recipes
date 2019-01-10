0f92decea80d43ec5e8f2ee4c00dc0243ff24ba8
Eric Roberts <notthebmovieactor@gmail.com>
2019-01-10 15:47:37 -0500

    Remove pip version check from build

--- recipes/genomedata/build.sh
+++ recipes/genomedata/build.sh
@@ -5,7 +5,7 @@ export HDF5_DIR=$PREFIX
 # See https://bitbucket.org/hoffmanlab/genomedata/issues/47/assert-statements-from
 export CPPFLAGS='-D_FORTIFY_SOURCE=2 -O2'
 
-$PYTHON -m pip install -vv --no-deps --no-cache-dir --ignore-installed . 
+$PYTHON -m pip install -vv --disable-pip-version-check --no-deps --no-cache-dir --ignore-installed . 
 # Work around for no 'source_files' support in test section of meta.yaml
 cp test/run_tests.py $PREFIX/bin/run_genomedata_tests.py
 cp test/test_genomedata.py $PREFIX/bin
