7f7a62d7d735b12cf51849cc158687d3c6cad2a2
Björn Grüning <bjoern@gruenings.eu>
2019-01-12 00:19:02 +0100

    add HOME dir

--- recipes/islandpath/build.sh
+++ recipes/islandpath/build.sh
@@ -4,5 +4,6 @@ cp -r . $PREFIX/islandpath
 mkdir -p $PREFIX/bin
 ln -s $PREFIX/islandpath/Dimob.pl $PREFIX/bin/islandpath
 chmod +x $PREFIX/bin/*
+export HOME=/tmp 
 cpanm --force TAP::Harness::Env ExtUtils::Helpers
 cpanm MooseX::Singleton
