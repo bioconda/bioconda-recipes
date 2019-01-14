98ac43fb7177be1c5c7258f41198d61524c14b33
James Abbott <j.abbott@dundee.ac.uk>
2019-01-14 15:56:03 +0000

    Removed debug statements

--- recipes/readfq/build.sh
+++ recipes/readfq/build.sh
@@ -2,13 +2,6 @@
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-export CPATH="${PREFIX}/include"
-
-echo $CFLAGS
-ls $PREFIX/include
-echo $LDFLAGS
-ls $PREFIX/lib
-echo $CPATH
 
 ${CC} ${CFLAGS} ${LDFLAGS} -lz -o readfq kseq_fastq_base.c
 cp readfq $PREFIX/bin/readfq
