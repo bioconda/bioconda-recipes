8af2b1913aa6548ae53b3098044409a22c68c777
James Abbott <j.abbott@dundee.ac.uk>
2019-01-14 15:38:36 +0000

    Add debug...

--- recipes/readfq/build.sh
+++ recipes/readfq/build.sh
@@ -2,6 +2,11 @@
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+export CPATH="${PREFIX}/include"
+
+echo $CFLAGS
+echo $LDFLAGS
+echo $CPATH
 
 ${CC} -lz -o readfq kseq_fastq_base.c
 cp readfq $PREFIX/bin/readfq
