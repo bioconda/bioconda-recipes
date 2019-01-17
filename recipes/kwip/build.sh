9f633070893fc29e32d47e1dc85566b0396e1fba
phue <patrick.huether@gmail.com>
2019-01-17 15:32:41 +0000

    fix kWIP build and rework meta.yml
    * use compiler macros instead of gcc dependencies
    * fix tests (kwip binary is lowercase)
    * switch to lowercase

--- recipes/kwip/build.sh
+++ recipes/kwip/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
+
 export BINARY_HOME=$PREFIX
 
 export CFLAGS="-I$PREFIX/include"
@@ -9,10 +8,7 @@ export CPATH=${PREFIX}/include
 
 cd $SRC_DIR
 mkdir build && cd build
-cmake .. \
-	-DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
-    -DCMAKE_INSTALL_PREFIX=${BINARY_HOME}
+cmake .. -DCMAKE_INSTALL_PREFIX=${BINARY_HOME}
 make
 ctest --verbose
 make install
