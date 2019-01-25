3cd9f7240a4de2a00a340de09526a1a108c07f0d
Dario Beraldi <dario.beraldi@gmail.com>
2019-01-25 12:21:06 +0000

    Revert to sh

--- recipes/asciigenome/build.sh
+++ recipes/asciigenome/build.sh
@@ -5,7 +5,7 @@ mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
 cp ASCIIGenome.jar $outdir
-echo '#!/bin/bash' > $outdir/ASCIIGenome
+echo '#!/bin/sh' > $outdir/ASCIIGenome
 echo exec java -Djava.net.useSystemProxies=true -Xmx2500m -jar $outdir/ASCIIGenome.jar '"$@"' >> $outdir/ASCIIGenome
 chmod a+x $outdir/ASCIIGenome
 ln -s $outdir/ASCIIGenome $PREFIX/bin
