2bfc9c700c23a63a27fdd3379330348e33cb5eb9
Dario Beraldi <dario.beraldi@gmail.com>
2019-01-26 17:05:28 +0000

    Fix symplink

--- recipes/asciigenome/build.sh
+++ recipes/asciigenome/build.sh
@@ -6,7 +6,9 @@ mkdir -p $PREFIX/bin
 
 # To be removed after shebang is fixed is ASCIIGenome repository
 sed 's|#!/bin/sh|#!/bin/bash|' ASCIIGenome > $outdir/ASCIIGenome
+chmod a+x $outdir/ASCIIGenome
 
 cp ASCIIGenome.jar $outdir
-chmod a+x $outdir/ASCIIGenome
+
 ln -s $outdir/ASCIIGenome $PREFIX/bin
+ln -s $outdir/ASCIIGenome.jar $PREFIX/bin
