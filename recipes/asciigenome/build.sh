90a248885c23e137afbfc49306d304e5f7625421
Dario Beraldi <dario.beraldi@gmail.com>
2019-01-26 16:35:50 +0000

    Fix sed command

--- recipes/asciigenome/build.sh
+++ recipes/asciigenome/build.sh
@@ -4,7 +4,10 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
-sed -i '' 's|#!/bin/sh|#!/bin/bash|' ASCIIGenome
+# To be removed after shebang is fixed is ASCIIGenome repository
+mv ASCIIGenome ASCIIGenome.tmp
+sed 's|#!/bin/sh|#!/bin/bash|' ASCIIGenome.tmp > ASCIIGenome
+rm ASCIIGenome.tmp
 
 cp ASCIIGenome.jar $outdir
 cp ASCIIGenome $outdir
