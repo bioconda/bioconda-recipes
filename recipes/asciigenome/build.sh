8ea9436e77a6ce59115a8f38cec7c5a1a0eab28c
Dario Beraldi <dario.beraldi@gmail.com>
2019-01-26 17:26:51 +0000

    Copy - do not symlink

--- recipes/asciigenome/build.sh
+++ recipes/asciigenome/build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
 # To be removed after shebang is fixed is ASCIIGenome repository
-sed 's|#!/bin/sh|#!/bin/bash|' ASCIIGenome > $outdir/ASCIIGenome
-chmod a+x $outdir/ASCIIGenome
+sed 's|#!/bin/sh|#!/bin/bash|' ASCIIGenome > $PREFIX/bin/ASCIIGenome
+chmod a+x $PREFIX/bin/ASCIIGenome
 
-cp ASCIIGenome.jar $outdir
-
-ln -s $outdir/ASCIIGenome $PREFIX/bin
-ln -s $outdir/ASCIIGenome.jar $PREFIX/bin
+cp ASCIIGenome.jar $PREFIX/bin/
