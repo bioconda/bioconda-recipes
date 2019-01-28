8cf4aaac8550615fb2f02f8b9c7f5760656df53f
Ryan Raaum <ryan.raaum@lehman.cuny.edu>
2019-01-28 17:05:16 -0500

    working on osx

--- recipes/alder/build.sh
+++ recipes/alder/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-make CFLAGS="-Wno-unused-comparison -Wno-return-type -I${PREFIX}/include -Iadmixtools_src/nicksrc" LDFLAGS="-L${PREFIX}/lib -Ladmixtools_src/nicksrc"
+make CXX=clang++ \
+	 CXXFLAGS="-Wall -Wno-return-type -Wno-write-strings -I$PREFIX/include -Iadmixtools_src -Iadmixtools_src/nicksrc" \
+	 CFLAGS="-c -O2 -Wimplicit -Wno-return-type -Wno-write-strings -I$PREFIX/include -I. -I./nicksrc" \
+	 L="-L$PREFIX/lib -lfftw3 -llapack -fopenmp"
 
-mkdir -p ${PREFIX}/bin
-cp alder ${PREFIX}/bin/
+mkdir -p $PREFIX/bin
+cp alder $PREFIX/bin/
