f51c44e3e52cf406af2100aae53083c26be43362
jfallmann <fall@bioinf.uni-leipzig.de>
2019-01-30 15:26:43 +0100

    path fix

--- recipes/igv/build.sh
+++ recipes/igv/build.sh
@@ -3,6 +3,6 @@
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/lib/igv
 cp lib/*.jar $PREFIX/lib/igv
-sed -i 's/"$prefix"\/lib\/igv.jar/${PREFIX}\/..\/lib\/igv\/igv.jar/g' igv.sh
+sed -i 's/"$prefix"\/lib\/igv.jar/${PREFIX}\/lib\/igv\/igv.jar/g' igv.sh
 cp igv.sh $PREFIX/bin/igv
 chmod +x $PREFIX/bin/igv
