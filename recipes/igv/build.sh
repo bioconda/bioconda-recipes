282bef7e95b3fdfb1b4970c23f0814abcfdae0de
jfallmann <fall@bioinf.uni-leipzig.de>
2019-01-30 13:41:08 +0100

    Upgraded version, fixed startscript

--- recipes/igv/build.sh
+++ recipes/igv/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cp lib/*.jar $PREFIX/bin
+cp lib/*.jar $PREFIX/lib/igv
+sed -i 's/"$prefix"\/lib\/igv.jar/"$prefix"\/..\/lib\/igv\/igv.jar/g' igv.sh
 cp igv.sh $PREFIX/bin/igv
 chmod +x $PREFIX/bin/igv
