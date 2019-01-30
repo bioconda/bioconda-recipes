337f8607acfbb671c58547e2e01adb189c3b77cb
Jonathan Manning <jmanning@ebi.ac.uk>
2019-01-30 10:13:55 +0000

    Permissions tweak attempt

--- recipes/irap-components/build.sh
+++ recipes/irap-components/build.sh
@@ -15,8 +15,6 @@ export IRAP_DIR=$PREFIX/bin/irap
 #$SRC_DIR/scripts/irap_install.sh -s $SRC_DIR -c $PREFIX/bin/irap
 
 cp -rp $SRC_DIR $PREFIX/bin/irap
-sh_location=$(which sh)
-ln -s $sh_location /bin/sh
 chmod +x $PREFIX/bin/irap
 
 # Run tests
