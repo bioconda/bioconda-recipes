0581af66aede2ae62d39f466e96cee72f6ae0c66
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-02-01 22:39:29 -0700

    Docs: fix stylesheet name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 def setup(app):
-    app.add_stylesheet("custom.css")
+    app.add_stylesheet("style.css")
 
 
 # -- General configuration ------------------------------------------------
