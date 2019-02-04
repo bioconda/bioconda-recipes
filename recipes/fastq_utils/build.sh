e9c4baaba9cc07712a6f45fe0ddb0d20e7d9dfb1
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-04 11:05:03 +0000

    Attempt to fix curses error for Samtools following that recipe's example

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -10,6 +10,8 @@ export LIBRARY_PATH="${PREFIX}/lib"
 
 tar jxvf deps/samtools-0.1.19.tar.bz2 
 pushd samtools-0.1.19
+sed -i.bak -e 's/-lcurses/-lncurses/' Makefile
+sed -i.bak -e "s|CFLAGS=\s*-g\s*-Wall\s*-O2\s*|CFLAGS= -g -Wall -O2 -I$NCURSES_INCLUDE_PATH/ncurses/ -I$NCURSES_INCLUDE_PATH -L$NCURSES_LIB_PATH|g" Makefile
 make
 make razip
 popd
