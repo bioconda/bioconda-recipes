e75b4385a7ff50d96b28a3f2d1f59de47260aa5a
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-04 15:02:05 +0000

    Probably futile attempt at fix

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -3,9 +3,9 @@
 mkdir -p $PREFIX/bin
 
 export C_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/bam"
-export CPP_INCLUDE_PATH="${PREFIX}/include"
-export CXX_INCLUDE_PATH="${PREFIX}/include"
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+#export CPP_INCLUDE_PATH="${PREFIX}/include"
+#export CXX_INCLUDE_PATH="${PREFIX}/include"
+#export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
 # Run the main install
