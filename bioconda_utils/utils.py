fd01914b4be0a1816120b6e0739b62e1a4352223
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-02-04 14:04:16 -0700

    Load `depends` with RepoData; store as pkl to allow nested data

--- bioconda_utils/utils.py
+++ bioconda_utils/utils.py
@@ -1208,7 +1208,7 @@ class RepoData:
     REPODATA_LABELED_URL = 'https://conda.anaconda.org/{channel}/label/{label}/{subdir}/repodata.json'
     REPODATA_DEFAULTS_URL = 'https://repo.anaconda.com/pkgs/main/{subdir}/repodata.json'
 
-    _load_columns = ['build', 'build_number', 'name', 'version']
+    _load_columns = ['build', 'build_number', 'name', 'version', 'depends']
 
     #: Columns available in internal dataframe
     columns = _load_columns + ['channel', 'subdir', 'platform']
@@ -1264,7 +1264,7 @@ class RepoData:
     def _load_channel_dataframe(self):
         if self.cache_file is not None and os.path.exists(self.cache_file):
             logger.info("Loading repodata from cache %s", self.cache_file)
-            return pd.read_table(self.cache_file)
+            return pd.read_pickle(self.cache_file)
 
         repos = list(product(self.channels, self.platforms))
         urls = [self._make_repodata_url(c, p) for c, p in repos]
@@ -1286,7 +1286,7 @@ class RepoData:
         res = pd.concat(dfs)
 
         if self.cache_file is not None:
-            res.to_csv(self.cache_file, sep='\t')
+            res.to_pickle(self.cache_file)
 
         return res
 
