3764f0f46346aabedc73236aaa10ec36c8ebc7a1
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-04 11:39:50 +0000

    Attempt to resolve samtools compile issue

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -1,20 +1,21 @@
 mkdir -p $PREFIX/bin
 
-export C_INCLUDE_PATH="${PREFIX}/include"
-export CPP_INCLUDE_PATH="${PREFIX}/include"
-export CXX_INCLUDE_PATH="${PREFIX}/include"
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-
 # Download the old Samtools fastq_utils needs for its bam.h
 
 tar jxvf deps/samtools-0.1.19.tar.bz2 
 pushd samtools-0.1.19
-sed -i.bak -e 's/-lcurses/-lncurses/' Makefile
-sed -i.bak -e "s|CFLAGS=\s*-g\s*-Wall\s*-O2\s*|CFLAGS= -g -Wall -O2 -I$NCURSES_INCLUDE_PATH/ncurses/ -I$NCURSES_INCLUDE_PATH -L$NCURSES_LIB_PATH|g" Makefile
+make INCLUDES="-I. -I$PREFIX/include -I$PREFIX/include/ncurses" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo"
+#sed -i.bak -e 's/-lcurses/-lncurses/' Makefile
+#sed -i.bak -e "s|CFLAGS=\s*-g\s*-Wall\s*-O2\s*|CFLAGS= -g -Wall -O2 -I$NCURSES_INCLUDE_PATH/ncurses/ -I$NCURSES_INCLUDE_PATH -L$NCURSES_LIB_PATH|g" Makefile
 make
 make razip
 popd
+
+#export C_INCLUDE_PATH="${PREFIX}/include"
+#export CPP_INCLUDE_PATH="${PREFIX}/include"
+#export CXX_INCLUDE_PATH="${PREFIX}/include"
+#export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+#export LIBRARY_PATH="${PREFIX}/lib"
 
 # Run the main install
 
