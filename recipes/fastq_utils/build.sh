25bfa0bee0ce0389bbad21c6ab6314236b7fe7d3
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-05 08:35:57 +0000

    Replace gcc call with x86_64-apple-darwin13.4.0-clang

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -3,13 +3,12 @@
 mkdir -p $PREFIX/bin
 
 export C_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/bam"
-#export CPP_INCLUDE_PATH="${PREFIX}/include"
 export CXX_INCLUDE_PATH="${PREFIX}/include"
-#export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
-# Run the main install
+# Compile and install. Replace gcc with $CC
 
+sed -i.bak "s/gcc/\$CC/g" Makefile
 make
 make install
 
