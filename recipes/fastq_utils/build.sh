59e5dc4f0c24e2c6f801c498ec882e2e1fff4607
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-05 09:33:39 +0000

    Attempted fix for cannot find -lz

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -3,8 +3,10 @@
 mkdir -p $PREFIX/bin
 
 export C_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/bam"
+export CPP_INCLUDE_PATH="${PREFIX}/include"
 export CXX_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 # Compile and install. Replace gcc with $CC
 
