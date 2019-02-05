082685db756b2295b17c3451b06786ffe27ce712
khanghoj <kristianhanghoej@gmail.com>
2019-02-05 16:43:11 +0100

    test

--- recipes/dammet/build.sh
+++ recipes/dammet/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-make
-make install PREFIX=${PREFIX}
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+make makefile
+make makefile install PREFIX=${PREFIX}
