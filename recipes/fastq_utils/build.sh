45dbe657221eed7c1658609d220a1bd2dd9d053b
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-05 09:40:26 +0000

    Hopefully LDFLAGS is what's needed

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -7,6 +7,7 @@ export CPP_INCLUDE_PATH="${PREFIX}/include"
 export CXX_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
 
 # Compile and install. Replace gcc with $CC
 
