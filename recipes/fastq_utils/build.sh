f948fa17c5e0cbedb0ef0e994f1c8f0b6fbcf56f
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-05 10:09:37 +0000

    Okay, source package doesn't use LDFLAGS, use CFLAGS to pass options

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -7,12 +7,11 @@ export CPP_INCLUDE_PATH="${PREFIX}/include"
 export CXX_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="-L${PREFIX}/lib"
 
 # Compile and install. Replace gcc with $CC
 
 sed -i.bak "s/gcc/\$\(CC\)/g" src/Makefile
-make
+make CFLAGS=-L${PREFIX}/lib
 make install
 
 # Copy executables into prefix
