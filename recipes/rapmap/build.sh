1cf7df8c381c6d683da0beb3fd531f734f1dd222
Rob Patro <rob-p@users.noreply.github.com>
2019-02-05 01:12:53 -0500

    Update build.sh

--- recipes/rapmap/build.sh
+++ recipes/rapmap/build.sh
@@ -5,6 +5,4 @@ mkdir build && cd build
 cmake -DNO_NATIVE_ARCH=TRUE -DCMAKE_CXX_FLAGS=-mno-avx -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_BUILD_TYPE=RELEASE ..
 make
 make install
-mkdir -p $PREFIX/bin
-cp ../bin/rapmap $PREFIX/bin
 CTEST_OUTPUT_ON_FAILURE=1 make test
