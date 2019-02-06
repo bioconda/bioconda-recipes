a42f023a83ad686d736ee6da71e161424cf18947
tobiasrausch <rauschtobi@gmail.com>
2019-02-06 21:23:13 +0100

    tracy v0.5.1

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+export C_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make all
+make STATIC=1 all
 mkdir -p $PREFIX/bin
 cp src/tracy $PREFIX/bin
