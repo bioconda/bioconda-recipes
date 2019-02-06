6235285fdb429ec5d98afb95775bcd63815eb809
wdecoster <decosterwouter@gmail.com>
2019-02-06 13:26:18 +0100

    trying adding C and LDFLAGS

--- recipes/last/build.sh
+++ recipes/last/build.sh
@@ -9,6 +9,8 @@ if [[ "${PY_VER}" =~ 3 ]]
 then
     2to3 -w -n `grep -l python ${SRC_DIR}/scripts/*`
 fi
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 
 cp ${SRC_DIR}/scripts/* ${PREFIX}/bin/
 make install prefix=${PREFIX}
