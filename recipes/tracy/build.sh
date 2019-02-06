00ed0a71f9219a3dfeae95166295e827db4c58be
tobiasrausch <rauschtobi@gmail.com>
2019-02-06 21:49:12 +0100

    tracy v0.5.1

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
