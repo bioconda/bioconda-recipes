c15b4b5ac3d924833902b683ed4c57d9bad8a3a5
Jon Palmer <nextgenusfs@gmail.com>
2019-02-07 19:08:01 -0800

    spaces in post-link.sh

--- recipes/amptk/post-link.sh
+++ recipes/amptk/post-link.sh
@@ -9,9 +9,8 @@ cat > "${PREFIX}"/.messages.txt <<- EOF
     #      binaries and make sure they are in your PATH. They should be          #
     #      softlinked to usearch9 and usearch10 respectively, i.e.               #
     #                                                                            #
-    #       chmod +x /path/to/usearch9.2.64_i86osx32                             #
-    #       ln -s /path/to/usearch9.2.64_i86osx32 /usr/local/bin/usearch9        #
+    #      chmod +x /path/to/usearch9.2.64_i86osx32                              #
+    #      ln -s /path/to/usearch9.2.64_i86osx32 /usr/local/bin/usearch9         #
     #                                                                            #
     ##############################################################################
-
 EOF
