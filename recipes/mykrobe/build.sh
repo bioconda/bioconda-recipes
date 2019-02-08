1d45b635ab30f7cc86e6354c2b063fd38c21797f
Michael Hall <mbhall88@gmail.com>
2019-02-08 21:38:03 +0000

    update mykrobe to v0.6.1
    remove source fn
    
    add wget to build requirements
    
    add zlib to build requirements
    
    move zlib to host requirements
    
    specify zlib location
    
    specify zlib location
    
    move zlib to build requirements
    
    add c compiler
    
    add some more zlib related flags
    
    add some more zlib related flags
    
    add htslib to reqs
    
    add setuptools to reqs
    
    remove htslib to reqs
    
    try pip install instead
    
    try skipping py2
    
    skip py27 and add imports test
    
    see if py27 works now
    
    remove py27 build
    
    create test script
    
    create test script
    
    add tests dir to test reqs
    
    add data dir to test reqs

--- recipes/mykrobe/build.sh
+++ recipes/mykrobe/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+if [[ "$(uname)" == "Linux" ]] ; then
+    export LDFLAGS="$LDFLAGS -Wl,--add-needed"
+fi
+
+# cd "$SRC_DIR"
+
+wget https://bit.ly/2H9HKTU -O - | tar -vxzf  -
+rm -fr src/mykrobe/data
+mv mykrobe-data src/mykrobe/data
+pip install .
+
+# $PYTHON setup.py install --single-version-externally-managed --record=record.txt
