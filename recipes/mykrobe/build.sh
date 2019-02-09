7d1222a0c40e5e60b49987ddc59da98a94e0b048
Michael Hall <mbhall88@gmail.com>
2019-02-09 09:02:37 +0000

    clean up build script. add test source files

--- recipes/mykrobe/build.sh
+++ recipes/mykrobe/build.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
-
-if [[ "$(uname)" == "Linux" ]] ; then
-    export LDFLAGS="$LDFLAGS -Wl,--add-needed"
-fi
-
-# cd "$SRC_DIR"
-
 wget https://bit.ly/2H9HKTU -O - | tar -vxzf  -
 rm -fr src/mykrobe/data
 mv mykrobe-data src/mykrobe/data
 pip install .
-
-# $PYTHON setup.py install --single-version-externally-managed --record=record.txt
