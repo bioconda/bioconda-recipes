e62adc1dff55da078714268102bb7c1067467fe0
Michael Hall <mbhall88@gmail.com>
2019-02-09 22:25:38 +0000

    add post-link script for downloading panel data

--- recipes/mykrobe/build.sh
+++ recipes/mykrobe/build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-wget https://bit.ly/2H9HKTU -O - | tar -vxzf  -
-rm -fr src/mykrobe/data
-mv mykrobe-data src/mykrobe/data
 pip install .
---
+++ recipes/mykrobe/post-link.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cd "$PREFIX"
+TARBALL="mykrobe-data.tar.gz"
+URLS=(
+  "https://bit.ly/2H9HKTU"
+)
+MD5="cce48d0e0a5bc93916007b576bf6ace4"
+
+SUCCESS=0
+for URL in ${URLS[@]}; do
+  wget -O- ${URL} > "$TARBALL"
+  [[ $? == 0 ]] || continue
+
+  # Platform-specific md5sum checks.
+  if [[ $(uname -s) == "Linux" ]]; then
+    if md5sum -c <<<"$MD5  $TARBALL"; then
+      SUCCESS=1
+      break
+    fi
+  else if [[ $(uname -s) == "Darwin" ]]; then
+    if [[ $(md5 $TARBALL | cut -f4 -d " ") == "$MD5" ]]; then
+      SUCCESS=1
+      break
+    fi
+  fi
+fi
+done
+
+if [[ $SUCCESS != 1 ]]; then
+  echo "ERROR: post-link.sh was unable to download any of the following URLs with the md5sum $MD5:" >> "${PREFIX}/.messages.txt" 2>&1
+  printf '%s\n' "${URLS[@]}" >> "${PREFIX}/.messages.txt" 2>&1
+  exit 1
+
+tar -vxzf "$TARBALL"
+rm -fr src/mykrobe/data
+mv mykrobe-data src/mykrobe/data
+rm "$TARBALL"
