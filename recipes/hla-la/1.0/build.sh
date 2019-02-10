ccc2afb88a9b86cff1b49c818f06a5d655b780f1
Alexander Dilthey <alexanderdilthey@outlook.com>
2019-02-10 13:36:57 +0100

    Cleaned up file

--- recipes/hla-la/1.0/build.sh
+++ recipes/hla-la/1.0/build.sh
@@ -23,21 +23,12 @@ cd ../../../src
 sed -i.bak 's/CXX    = g++//g' makefile
 sed -i.bak 's/BOOST_INCLUDE = $(BOOST_PATH)/BOOST_INCLUDE = $(PREFIX)/g' makefile
 sed -i.bak 's/BOOST_LIB = $(BOOST_PATH)/BOOST_LIB = $(PREFIX)/g' makefile
-#sed -i.bak 's/BAMTOOLS_INCLUDE = $(BAMTOOLS_PATH)\/include\/bamtools/BAMTOOLS_INCLUDE = ${PREFIX}\/include\/bamtools/g' makefile
 sed -i.bak 's/BAMTOOLS_INCLUDE = $(BAMTOOLS_PATH)\/include\/bamtools/BAMTOOLS_INCLUDE = ..\/localBamTools\/install\/include\/bamtools\//g' makefile
 sed -i.bak 's/BAMTOOLS_LIB = $(BAMTOOLS_PATH)\/lib64//g' makefile
 sed -i.bak 's/BAMTOOLS_SRC = $(BAMTOOLS_PATH)\/src//g' makefile
-#sed -i.bak 's/-lbamtools/\${PREFIX}\/lib\/libbamtools.a/g' makefile
 sed -i.bak 's/-lbamtools/..\/localBamTools\/install\/lib\/libbamtools.a/g' makefile
 sed -i.bak 's/^workingDir=.*//g' paths.ini
 sed -i.bak 's/^workingDir_HLA_ASM=.*//g' paths.ini
-
-
-#echo "Hello\n\n\n\n\n"
-#echo "${PREFIX}/lib/libbamtools.a"
-#nm "${PREFIX}/lib/libbamtools.a"
-#ls "${PREFIX}/lib"
-#echo "End hello area\n\n\n\n\n"
 
 make all 
 
