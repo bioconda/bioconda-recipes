29d5178bf1b15cda68ced3c5baeedb1aa01647b8
Michael Hall <mbhall88@gmail.com>
2019-02-11 14:49:19 +0000

    fix post-link script

--- recipes/mykrobe/post-link.sh
+++ recipes/mykrobe/post-link.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
-cd "$PREFIX"
+
+# This script downloads the panel data that mykrobe uses for its predictions
+
+cd "$PREFIX" || exit 1
 TARBALL="mykrobe-data.tar.gz"
 URLS=(
-  "https://bit.ly/2H9HKTU"
+    "https://bit.ly/2H9HKTU"
 )
 MD5="cce48d0e0a5bc93916007b576bf6ace4"
-
 SUCCESS=0
-for URL in ${URLS[@]}; do
-  wget -O- ${URL} > "$TARBALL"
-  [[ $? == 0 ]] || continue
 
-  # Platform-specific md5sum checks.
-  if [[ $(uname -s) == "Linux" ]]; then
-    if md5sum -c <<<"$MD5  $TARBALL"; then
-      SUCCESS=1
-      break
+for URL in "${URLS[@]}"; do
+    wget -O - "${URL}" > "$TARBALL"
+    [[ $? == 0 ]] || continue
+    # Platform-specific md5sum checks.
+    if [[ $(uname -s) == "Linux" ]]; then
+        if md5sum -c <<<"$MD5  $TARBALL"; then
+            SUCCESS=1
+            break
+        fi
+    else
+        if [[ $(uname -s) == "Darwin" ]]; then
+            if [[ $(md5 $TARBALL | cut -f4 -d " ") == "$MD5" ]]; then
+                SUCCESS=1
+                break
+            fi
+        fi
     fi
-  else if [[ $(uname -s) == "Darwin" ]]; then
-    if [[ $(md5 $TARBALL | cut -f4 -d " ") == "$MD5" ]]; then
-      SUCCESS=1
-      break
-    fi
-  fi
-fi
 done
 
 if [[ $SUCCESS != 1 ]]; then
-  echo "ERROR: post-link.sh was unable to download any of the following URLs with the md5sum $MD5:" >> "${PREFIX}/.messages.txt" 2>&1
-  printf '%s\n' "${URLS[@]}" >> "${PREFIX}/.messages.txt" 2>&1
-  exit 1
+    echo "ERROR: post-link.sh was unable to download any of the following URLs with the md5sum $MD5:" >> "${PREFIX}/.messages.txt" 2>&1
+    printf '%s\n' "${URLS[@]}" >> "${PREFIX}/.messages.txt" 2>&1
+    exit 1
+fi
 
 tar -vxzf "$TARBALL"
 rm -fr "$PREFIX"/src/mykrobe/data
