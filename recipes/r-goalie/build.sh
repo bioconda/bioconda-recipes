10b014290f3f4ea15545afeb5e661333cfa91ba0
Jonathan Manning <jmanning@ebi.ac.uk>
2019-02-11 23:02:50 +0000

    Remove lines that deal with 3.4.1 issues

--- recipes/r-goalie/build.sh
+++ recipes/r-goalie/build.sh
@@ -1,5 +1,1 @@
-# These lines to replace ...elt() calls with the one from backports  
-#sed -i.bak "s/\.\.\.elt/backports::...elt/g" R/validate.R 
-#sed -i.bak "s/\.\.\.elt/backports::...elt/g" R/assert.R 
-
 $R CMD INSTALL --build .
