0ec7686fd71a44d65b41f4240dd9d4b2fba153d6
alex bishara <abishara@cs.stanford.edu>
2019-02-12 08:18:07 -0800

    initial import

---
+++ recipes/idba_subasm/build.sh
@@ -0,0 +1,26 @@
+# fix automake
+
+# fix other python and perl scripts
+gawk -i inplace '/usr\/bin\//{ gsub(/python/, "env python");}1' script/*.py
+gawk -i inplace '/usr\/bin\//{ gsub(/perl/, "env perl");}1' script/*
+
+aclocal
+autoconf
+automake --add-missing
+
+export CC=${GCC}
+export CXX=${GXX}
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+./configure 
+make
+
+
+mkdir -p ${PREFIX}/bin
+
+cp bin/* ${PREFIX}/bin
+cp script/*py ${PREFIX}/bin
+cp script/validate* ${PREFIX}/bin
