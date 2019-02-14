80e8e68c5bc791af189bf744b5328cdc9a1f4a64
Devon Ryan <dpryan79@users.noreply.github.com>
2019-02-14 11:00:37 +0100

    Update build.sh

--- recipes/methyldackel/build.sh
+++ recipes/methyldackel/build.sh
@@ -9,6 +9,6 @@ export CPPFLAGS="-I${PREFIX}/include"
 export CFLAGS="-I${PREFIX}/include"
 
 export C_INCLUDE_PATH=$PREFIX/include
-make -c htslib/Makefile CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
+cd htslib && make Makefile CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" && cd ..
 make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
 make install prefix=$PREFIX/bin
