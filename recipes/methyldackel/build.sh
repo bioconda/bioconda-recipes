37589739737751105d99bd66860373ee3571f288
Devon Ryan <dpryan79@users.noreply.github.com>
2019-02-14 13:29:17 +0100

    I should really just test this stuff locally.

--- recipes/methyldackel/build.sh
+++ recipes/methyldackel/build.sh
@@ -9,6 +9,6 @@ export CPPFLAGS="-I${PREFIX}/include"
 export CFLAGS="-I${PREFIX}/include"
 
 export C_INCLUDE_PATH=$PREFIX/include
-cd htslib && make Makefile CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" && cd ..
+cd htslib && make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" && cd ..
 make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
 make install prefix=$PREFIX/bin
