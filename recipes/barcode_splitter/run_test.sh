513757911f666c41183847c92125824d6baac48e
hepcat72 <hepcat72@gmail.com>
2019-02-15 13:58:29 -0500

    Removed eval of shell variable in if conditional.
    I suspect that this eval is still related to the security warning and subsequent failure of the test in the remote circleci build.
    
    Files checked in: recipes/barcode_splitter/run_test.sh

--- recipes/barcode_splitter/run_test.sh
+++ recipes/barcode_splitter/run_test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 #Discovered a bug that causes a non-zero exit status for -h and --version, so until that's fixed...
-STDOUTTESTONE=`barcode_splitter --version 2>&1 | cut -d ' ' -f 1,2`
-
-if [ "$STDOUTTESTONE" != "barcode_splitter version" ]; then
+if [ `barcode_splitter --version 2>&1 | cut -d ' ' -f 1,2` != "barcode_splitter version" ]; then
   echo "ERROR: Unexpected output"
   exit 1
 fi
