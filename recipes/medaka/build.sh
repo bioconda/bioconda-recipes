2eb2e49ecc44abf6231c29f4aa4491219d6d333f
Chris Wright <chris.wright@nanoporetech.com>
2019-02-18 11:36:38 +0000

    Building on linux, osx dying on docker

--- recipes/medaka/build.sh
+++ recipes/medaka/build.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-#htsversion=1.3.1
-#htssource=samtools-${htsversion}/htslib-${htsversion}/
-#
-#sed -i.bak 's/^CPPFLAGS =$//g' ${htssource}/Makefile
-#sed -i.bak 's/^LDFLAGS  =$//g' ${htssource}/Makefile 
-#
-#
-## Remove rdynamic which can cause build issues on OSX
-## https://sourceforge.net/p/samtools/mailman/message/34699333/
-#sed -i.bak 's/ -rdynamic//g' ${htssource}/configure
-#
-#export CFLAGS="-I$PREFIX/include"
-#export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
-#export LDFLAGS="-L$PREFIX/lib"
-#
-## https://github.com/samtools/samtools/issues/577
-#if [[ "$(uname)" == "Linux" ]] ; then
-#    export LDFLAGS="$LDFLAGS -Wl,--add-needed"
-#fi
-
 # disable Makefile driven build of htslib.a
 sed -i.bak 's/.*build_ext.*//' setup.py
 
 # just link to htslib
-sed -i.bak 's/^extra_objects.*//' build.py
-sed -i.bak 's/^libraries=\[/libraries=\["lhts",/' build.py
+sed -i.bak 's/extra_objects.*//' build.py
+sed -i.bak 's/^libraries=\[/libraries=\["hts",/' build.py
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
