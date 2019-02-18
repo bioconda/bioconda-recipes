8c40231e3e464b4e12cf297c1843db033177d2d5
dpryan79 <dpryan79@gmail.com>
2019-02-18 15:41:52 +0100

    What if we rerun autogen.sh

--- recipes/igor_vdj/build.sh
+++ recipes/igor_vdj/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#export LIBS="-lrt -lm"
-sed -i.bak -e "s:libs/jemalloc::g" configure
-rm -rf libs/jemalloc
-sed -i.bak -e "s:jemalloc::" libs/Makefile.am
-sed -i.bak -e "s:jemalloc::" libs/Makefile.in
+export LIBS="-lrt -lm"
+#sed -i.bak -e "s:libs/jemalloc::g" configure
+#rm -rf libs/jemalloc
+#sed -i.bak -e "s:jemalloc::" libs/Makefile.am
+#sed -i.bak -e "s:jemalloc::" libs/Makefile.in
 
-./configure CC=$CC CXX=$CXX --prefix=$PREFIX #--with-mangling=aligned_alloc:__aligned_alloc --disable-tls
+./autogen.sh
+./configure CC=$CC CXX=$CXX --prefix=$PREFIX --with-mangling=aligned_alloc:__aligned_alloc --disable-tls
 make
 make install
