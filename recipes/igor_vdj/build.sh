56c46ff8fae96fe87891c4a83a97384f2072c430
dpryan79 <dpryan79@gmail.com>
2019-02-18 16:32:31 +0100

    sigh

--- recipes/igor_vdj/build.sh
+++ recipes/igor_vdj/build.sh
@@ -6,6 +6,7 @@ export LIBS="-lrt -lm"
 #sed -i.bak -e "s:jemalloc::" libs/Makefile.am
 #sed -i.bak -e "s:jemalloc::" libs/Makefile.in
 
+touch configure.ac aclocal.m4 configure Makefile.* */Makefile.* */*/Makefile.*
 ./configure CC=$CC CXX=$CXX --prefix=$PREFIX --with-mangling=aligned_alloc:__aligned_alloc --disable-tls
 make
 make install
