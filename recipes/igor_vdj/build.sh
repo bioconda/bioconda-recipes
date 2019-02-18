7367b8b3e3add4128fd909a6000da34ffd6aefa7
dpryan79 <dpryan79@gmail.com>
2019-02-18 16:46:24 +0100

    Remove old build stuff, it was just a timestamp problem

--- recipes/igor_vdj/build.sh
+++ recipes/igor_vdj/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 export LIBS="-lrt -lm"
-#sed -i.bak -e "s:libs/jemalloc::g" configure
-#rm -rf libs/jemalloc
-#sed -i.bak -e "s:jemalloc::" libs/Makefile.am
-#sed -i.bak -e "s:jemalloc::" libs/Makefile.in
-
 touch configure.ac aclocal.m4 configure Makefile.* */Makefile.* */*/Makefile.*
 ./configure CC=$CC CXX=$CXX --prefix=$PREFIX --with-mangling=aligned_alloc:__aligned_alloc --disable-tls
 make
