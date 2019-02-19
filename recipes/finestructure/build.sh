1a61f532277436109387ec0148de0b9f50e20050
Ryan Raaum <ryan.raaum@lehman.cuny.edu>
2019-02-19 11:37:34 -0500

    install helper scripts

--- recipes/finestructure/build.sh
+++ recipes/finestructure/build.sh
@@ -4,13 +4,7 @@ mkdir -p $PREFIX/bin
 
 ./configure --prefix=$PREFIX && make && make install
 
-# if [[ $(uname -s) == Darwin ]]; then
-# 	cp fs_mac $PREFIX/bin/fs
-# else
-# 	cp fs_linux_glibc2.3 $PREFIX/bin/fs
-# fi
-
-# # support scripts
-# cp {beagle2chromopainter.pl,chromopainter2chromopainterv2.pl,convertrecfile.pl} $PREFIX/bin/
-# cp {impute2chromopainter.pl,makeuniformrecfile.pl,msms2cp.pl,phasescreen.pl} $PREFIX/bin/
-# cp {phasesubsample.pl,plink2chromopainter.pl} $PREFIX/bin/
+# support scripts
+cp scripts/{beagle2chromopainter.pl,chromopainter2chromopainterv2.pl,convertrecfile.pl} $PREFIX/bin/
+cp scripts/{impute2chromopainter.pl,makeuniformrecfile.pl,msms2cp.pl,phasescreen.pl} $PREFIX/bin/
+cp scripts/{phasesubsample.pl,plink2chromopainter.pl} $PREFIX/bin/
