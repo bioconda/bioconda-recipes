f5f726f38808048351d8e71f89b208ce5d50d4ea
M Bernt <m.bernt@ufz.de>
2019-02-19 10:25:18 +0100

    cfm-id: some progress toward a working recipe
    - adapted meta.yml to new conda3 system (host, ...)
    - for now: skip osx build to keep it simple
    - conda's boost seems to have no static libraries hence CMakeLists.txt
      patch
    - removed `-DRDKIT_LIBRARIES="${PREFIX}/lib"` from cmake command line
      since according to FindRDKit.cmake the list of libaries
      (RDKIT_LIBARIES) are not set
    - downgraded boost (1.61 seemed not to work)

--- recipes/cfm-id/build.sh
+++ recipes/cfm-id/build.sh
@@ -9,10 +9,15 @@ export CPPFLAGS="-I${PREFIX}/include"
 mkdir -p $PREFIX/include/rdkit/External/INCHI-API
 cp $PREFIX/include/rdkit/GraphMol/inchi.h $PREFIX/include/rdkit/External/INCHI-API
 
+>&2 ls $PREFIX/lib
+>&2 cat CMakeLists.txt
+
 mkdir build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DLPSOLVE_INCLUDE_DIR="${PREFIX}/include" -DLPSOLVE_LIBRARY_DIR="${PREFIX}/lib" -DBoost_INCLUDE_DIR="${PREFIX}/include" -DBOOST_LIBRARYDIR="${PREFIX}/lib" -DRDKIT_INCLUDE_DIR="${PREFIX}/include/rdkit" -DRDKIT_INCLUDE_EXT_DIR="${PREFIX}/include/rdkit/External" -DRDKIT_LIBRARIES="${PREFIX}/lib"
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DLPSOLVE_INCLUDE_DIR="${PREFIX}/include/lpsolve" -DLPSOLVE_LIBRARY_DIR="${PREFIX}/lib" -DBoost_INCLUDE_DIR="${PREFIX}/include" -DBOOST_LIBRARYDIR="${PREFIX}/lib" -DRDKIT_INCLUDE_DIR="${PREFIX}/include/rdkit" -DRDKIT_INCLUDE_EXT_DIR="${PREFIX}/include/rdkit/External" #-DRDKIT_LIBRARIES="${PREFIX}/lib"
 
-make
+make VERBOSE=1
 make install
+
+# >&2 nm $PREFIX/lib/libRD* 
