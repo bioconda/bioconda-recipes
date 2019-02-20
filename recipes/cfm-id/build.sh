ece6e0f70bd6cb20e0cb60264d5bcf91c71d6ee4
M Bernt <m.bernt@ufz.de>
2019-02-20 21:22:53 +0100

    cfm: finished recipe
    - working test
    - also osx

--- recipes/cfm-id/build.sh
+++ recipes/cfm-id/build.sh
@@ -6,36 +6,27 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-mkdir -p $PREFIX/include/rdkit/External/INCHI-API
-cp $PREFIX/include/rdkit/GraphMol/inchi.h $PREFIX/include/rdkit/External/INCHI-API
+mkdir -p "$PREFIX/include/rdkit/External/INCHI-API"
+cp "$PREFIX/include/rdkit/GraphMol/inchi.h" "$PREFIX/include/rdkit/External/INCHI-API"
 
 mkdir build
 cd build
 
-# TODO remove DCMAKE_INSTALL_PREFIX?
-# TODO include optional cfm-train and cfm-test -- brings additional dependencies for MPI and liblbfgs-1.10
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
-#	-DINCLUDE_TESTS=ON -DINCLUDE_TRAIN=ON \
-        -DLPSOLVE_INCLUDE_DIR="${PREFIX}/include/lpsolve" \
+# TODO include optional cfm-train and cfm-test 
+# - brings additional dependencies for MPI (mpich2) and liblbfgs-1.10 (not yet in conda)
+cmake .. -DLPSOLVE_INCLUDE_DIR="${PREFIX}/include/lpsolve" \
         -DLPSOLVE_LIBRARY_DIR="${PREFIX}/lib" \
         -DBoost_INCLUDE_DIR="${PREFIX}/include" \
 	-DBOOST_LIBRARYDIR="${PREFIX}/lib" \
         -DRDKIT_INCLUDE_DIR="${PREFIX}/include/rdkit" \
 	-DRDKIT_INCLUDE_EXT_DIR="${PREFIX}/include/rdkit/External" 
+#-DINCLUDE_TESTS=ON -DINCLUDE_TRAIN=ON \
 #-DRDKIT_LIBRARIES="${PREFIX}/lib"
 
 make 
 make install
 
->&2 echo "parent dir"
->&2 ls ../
->&2 echo "current dir"
->&2 ls ../
->&2 tree ../
-
 # cp binaries 
 # cfm-annotate, cfm-id, cfm-id-precomputed, cfm-predict, compute-stats, fraggraph-gen, ISOTOPE.DAT
 # TODO what is the DAT file
-cp ../bin/* $PREFIX/bin/
-
-# >&2 nm $PREFIX/lib/libRD* 
+cp ../bin/* "$PREFIX"/bin/
