22af0c8e474a2aca1ee9ab2e29bc7e35bf8ea726
CarlosHorro <carlos.horro@gmail.com>
2019-02-21 11:42:55 +0100

    Removed echo command

--- recipes/rawtools/rawtools.sh
+++ recipes/rawtools/rawtools.sh
@@ -6,6 +6,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-echo "params"
-echo $@
 mono $DIR/RawTools.exe $@
