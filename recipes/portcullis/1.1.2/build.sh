303a043b008bb8cd9b26b007640013f36a6891c8
Luca Venturini <lucventurini@gmail.com>
2019-02-22 12:36:31 +0000

    Trying to follow the troubleshooting advice online

--- recipes/portcullis/1.1.2/build.sh
+++ recipes/portcullis/1.1.2/build.sh
@@ -5,7 +5,9 @@ set -x -e
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
