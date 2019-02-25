ef38cbd99281e8fb89d6e527e1af23500a2531cc
Devon Ryan <dpryan79@gmail.com>
2019-02-25 21:07:07 +0100

    c include path

--- recipes/methyldackel/build.sh
+++ recipes/methyldackel/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export C_INCLUDE_PATH="$PREFIX/include"
 cd htslib && make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" && cd ..
 make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
 make install prefix=$PREFIX/bin
