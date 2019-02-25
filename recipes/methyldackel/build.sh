83b27e5c8b97a419d206305922f0310346a03e2b
Devon Ryan <dpryan79@gmail.com>
2019-02-25 20:54:05 +0100

    update wgbs snakepipes tools

--- recipes/methyldackel/build.sh
+++ recipes/methyldackel/build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-export CFLAGS="-I${PREFIX}/include"
-
-export C_INCLUDE_PATH=$PREFIX/include
-make
+cd htslib && make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" && cd ..
+make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
 make install prefix=$PREFIX/bin
