d93ed4288764c4a0e0a34ac37439906840234f34
Devon Ryan <dpryan79@gmail.com>
2019-02-26 22:48:40 +0100

    Fix stringify builds

--- recipes/ucsc-overlapselect/build.sh
+++ recipes/ucsc-overlapselect/build.sh
@@ -8,6 +8,7 @@ mkdir -p "$BINDIR"
 (cd kent/src/htslib && make)
 (cd kent/src/jkOwnLib && make)
 (cd kent/src/hg/lib && make)
+export LD_LIBRARY_PATH=$PREFIX/lib
 (cd kent/src/utils/stringify && make)
 (cd kent/src/hg/utils/overlapSelect && make)
 cp bin/overlapSelect "$PREFIX/bin"
