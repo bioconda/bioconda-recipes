b84321e66f01b54cdd25a5c2db9e7ebac4603a11
Devon Ryan <dpryan79@gmail.com>
2019-02-26 01:13:09 +0100

    install

--- recipes/methyldackel/build.sh
+++ recipes/methyldackel/build.sh
@@ -2,5 +2,5 @@
 export C_INCLUDE_PATH="$PREFIX/include"
 cd htslib && make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" LDFLAGS="-L$PREFIX/lib" && cd ..
 export LIBRARY_PATH="$PREFIX/lib"
-make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
-make install prefix=$PREFIX/bin
+export LIBRARY_PATH="$PREFIX/lib"
+make install CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread"
