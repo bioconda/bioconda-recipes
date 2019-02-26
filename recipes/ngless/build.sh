dd450dd64b6d869fc6fdc4f9c89bbd3479bc17d3
Luis Pedro Coelho <luis@luispedro.org>
2019-02-26 07:47:58 +0100

    Update ngless recipe to newer conda envs
    The environmental variable $PREFIX should now be $CONDA_PREFIX
    
    Take the opportunity to update the URL of the tool to HTTPS
    (https://ngless.embl.de).
    
    This was reported as https://github.com/ngless-toolkit/ngless/issues/101

--- recipes/ngless/build.sh
+++ recipes/ngless/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+export LIBRARY_PATH="${CONDA_PREFIX}/lib:/usr/lib:/usr/lib64"
+export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib:/usr/lib:/usr/lib64"
 
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-export CFLAGS="-I$PREFIX/include"
-export CPATH=${PREFIX}/include
+export LDFLAGS="-L${CONDA_PREFIX}/lib"
+export CPPFLAGS="-I${CONDA_PREFIX}/include"
+export CFLAGS="-I${CONDA_PREFIX}/include"
+export CPATH=${CONDA_PREFIX}/include
 
 
 # stack relies on $HOME, so fake it:
 mkdir -p fake-home
 export HOME=$PWD/fake-home
 export STACK_ROOT="$HOME/.stack"
-stack setup --local-bin-path ${PREFIX}/bin
-make install WGET="wget --no-check-certificate" prefix=$PREFIX
+stack setup --local-bin-path ${CONDA_PREFIX}/bin
+make install WGET="wget --no-check-certificate" prefix=$CONDA_PREFIX
 
 #cleanup
 rm -r .stack-work
