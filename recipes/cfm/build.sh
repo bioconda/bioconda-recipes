9f4f6ebf2dc3dbe6af1f02c1192d2ba39b4a673b
M Bernt <m.bernt@ufz.de>
2019-02-27 23:21:57 +0100

    cfm: source patch
    - enable building of cfm-train and cfm-test
    - added tests
    - fix the hardcoded file ISOTOPE.DAT and the tests folder which are
      assumed in the working dir (ie it is assumed that working dir =
      install dir):
      added a c-macro to the corresponding source files that gets the
      PREFIX/bin folder during compilation. this path is then handled by
      conda.
    - the patch also contains one bugfix (endl instead of cout needed),
      some tests are commented since the input test files were absent
      in the sources, the example_candidates input for cfm-id is reduced
      to one candidate since quite a bit of memory (5GB) and time is needed

--- recipes/cfm/build.sh
+++ recipes/cfm/build.sh
@@ -12,20 +12,22 @@ cp "$PREFIX/include/rdkit/GraphMol/inchi.h" "$PREFIX/include/rdkit/External/INCH
 mkdir build
 cd build
 
-# TODO include optional cfm-train and cfm-test 
-# - brings additional dependencies for MPI (mpich2) and liblbfgs-1.10 (not yet in conda)
-cmake .. -DLPSOLVE_INCLUDE_DIR="${PREFIX}/include/lpsolve" \
+cmake .. -DCFM_OUTPUT_DIR="${PREFIX}/" \
+        -DLPSOLVE_INCLUDE_DIR="${PREFIX}/include/lpsolve" \
         -DLPSOLVE_LIBRARY_DIR="${PREFIX}/lib" \
         -DBoost_INCLUDE_DIR="${PREFIX}/include" \
         -DBOOST_LIBRARYDIR="${PREFIX}/lib" \
         -DRDKIT_INCLUDE_DIR="${PREFIX}/include/rdkit" \
-        -DRDKIT_INCLUDE_EXT_DIR="${PREFIX}/include/rdkit/External" 
-        -DINCLUDE_TESTS=ON -DINCLUDE_TRAIN=ON \
+        -DRDKIT_INCLUDE_EXT_DIR="${PREFIX}/include/rdkit/External" \
+	-DLBFGS_INCLUDE_DIR="${PREFIX}/include" -DLBFGS_LIBRARY_DIR="${PREFIX}/lib" \
+        -DINCLUDE_TESTS=ON -DINCLUDE_TRAIN=ON
 #-DRDKIT_LIBRARIES="${PREFIX}/lib"
 
-make 
+make -j$CPU_COUNT;
 make install
 
+#>&2 tree ..
+
 # cp binaries 
-# cfm-annotate, cfm-id, cfm-id-precomputed, cfm-predict, compute-stats, fraggraph-gen, ISOTOPE.DAT
-cp ../bin/* "$PREFIX"/bin/
+#cp ../bin/cfm-* ../bin/compute-stats ../bin/fraggraph-gen ../bin/ISOTOPE.DAT "$PREFIX"/bin/
+
