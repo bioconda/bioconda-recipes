9fa45521a070d6878918c24bb714137169447088
Devon Patrick Ryan <ryan@mac884.immunbio.mpg.de>
2019-02-28 23:39:19 +0100

    Try this

--- recipes/ucsc-pslmap/build.sh
+++ recipes/ucsc-pslmap/build.sh
@@ -7,9 +7,8 @@ mkdir -p "$BINDIR"
 (cd kent/src/htslib && make)
 (cd kent/src/jkOwnLib && make)
 (cd kent/src/hg/lib && make)
-export LD_LIBRARY_PATH=$PREFIX/lib
-export LIBRARY_PATH=$PREFIX/lib
-(cd kent/src/utils/stringify && make)
+#(cd kent/src/utils/stringify && make)
+ln -s $PREFIX/bin/stringify $BINDIR
 (cd kent/src/hg/utils/pslMap && make)
 cp bin/pslMap "$PREFIX/bin"
 chmod +x "$PREFIX/bin/pslMap"
