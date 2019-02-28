6b91b3775860732bf20b7009b13b0b140aed2001
Luis Pedro Coelho <luis@luispedro.org>
2019-02-28 09:01:09 +0100

    Use bioconda ghc

--- recipes/ngless/build.sh
+++ recipes/ngless/build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-export LIBRARY_PATH="${CONDA_PREFIX}/lib:/usr/lib:/usr/lib64"
-export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib:/usr/lib:/usr/lib64"
+export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64:${LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64:${LD_LIBRARY_PATH}"
+export LD_RUN_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64:${LD_RUN_PATH}"
 
-export LDFLAGS="-L${CONDA_PREFIX}/lib"
-export CPPFLAGS="-I${CONDA_PREFIX}/include"
-export CFLAGS="-I${CONDA_PREFIX}/include"
-export CPATH=${CONDA_PREFIX}/include
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export CPATH="${CPATH} ${PREFIX}/include"
 
 
 # stack relies on $HOME, so fake it:
 mkdir -p fake-home
 export HOME=$PWD/fake-home
 export STACK_ROOT="$HOME/.stack"
-stack setup --ghc-variant integersimple --local-bin-path ${CONDA_PREFIX}/bin
-make install WGET="wget --no-check-certificate" prefix=$CONDA_PREFIX
+stack setup --system-ghc --local-bin-path ${PREFIX}/bin
+make install WGET="wget --no-check-certificate" prefix=$PREFIX
 
 #cleanup
 rm -r .stack-work
