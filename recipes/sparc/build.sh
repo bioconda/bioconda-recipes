4aca603ad1536a60b8d5564093cc044e532bab04
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-02-28 11:54:34 +0100

    mac fix

--- recipes/sparc/build.sh
+++ recipes/sparc/build.sh
@@ -3,6 +3,6 @@
 mkdir -p $PREFIX/bin
 g++ -O3 -o Sparc *.cpp
 mv Sparc $PREFIX/bin
-sed -i 's|\./||g' utility/split_and_run_sparc.sh
+sed -i.bak 's|\./||g' utility/split_and_run_sparc.sh
 chmod a+x utility/*.sh
 mv utility/* $PREFIX/bin
