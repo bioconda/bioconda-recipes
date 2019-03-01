adda6081e12e7be6e54ae4d9c4a4e672aa65d02b
jpuritz <jpuritz@gmail.com>
2019-03-01 12:43:57 -0500

    test

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -11,7 +11,9 @@ export CXXFLAGS="-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
 #sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' smithwaterman/Makefile
 #sed -i.bak 's/CFLAGS/CXXFLAGS/g' smithwaterman/Makefile
 
-sed -i.bak 's/$</$< $(LDFLAGS)/g' smithwaterman/Makefile
+#sed -i.bak 's/$</$< $(LDFLAGS)/g' smithwaterman/Makefile
+sed -i.bak 's/ld/$(LD)/' smithwaterman/Makefile
+
 
 # MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
 if [ "$(uname)" == "Darwin" ]; then
