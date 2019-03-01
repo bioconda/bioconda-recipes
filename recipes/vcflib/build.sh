ebf8d45ad2a38449be8e68a785c12db021868f8a
jpuritz <jpuritz@gmail.com>
2019-03-01 12:14:27 -0500

    test

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -8,8 +8,8 @@ export INCLUDES="-I . -Ihtslib -I$PREFIX/include -Itabixpp/htslib -I\$(INC_DIR) 
 export LIBPATH="-L. -Lhtslib -L$PREFIX/lib"
 export CXXFLAGS="-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
 
-sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' smithwaterman/Makefile
-sed -i.bak 's/CFLAGS/CFFFLAGS/g' smithwaterman/Makefile
+#sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' smithwaterman/Makefile
+sed -i.bak 's/CFLAGS/CXXFLAGS/g' smithwaterman/Makefile
 
 # MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
 if [ "$(uname)" == "Darwin" ]; then
