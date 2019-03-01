a465debd2bfcd3091eaf9144ea2e7cc4d7055a50
Fabio Cumbo <fabio.cumbo@gmail.com>
2019-03-01 17:50:32 +0100

    update 20190301
    generalizing version;
    removing compilers from requirements/run;
    make howdesbt executable

--- recipes/howdesbt/build.sh
+++ recipes/howdesbt/build.sh
@@ -9,5 +9,7 @@ ln -s jellyfish-2.2.10/jellyfish jellyfish
 cd ${SRC_DIR}
 make
 
-mkdir -p $PREFIX/bin
-cp howdesbt $PREFIX/bin
+mkdir -p ${PREFIX}/bin
+cp howdesbt ${PREFIX}/bin
+
+chmod +x ${PREFIX}/bin/howdesbt
