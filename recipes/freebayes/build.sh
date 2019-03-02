d11cb805d9fcd3257f78d7567a5122e22ce2bc08
jpuritz <jpuritz@gmail.com>
2019-03-01 19:46:43 -0500

    simplify freebayes build

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -48,6 +48,7 @@ export CFLAGS="-I$PREFIX/include"
 export C_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="-L$PREFIX/lib"
 
 # Make autoversion.
 make autoversion
