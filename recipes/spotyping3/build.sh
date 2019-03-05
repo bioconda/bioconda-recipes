53047f2a861c5d1909c5999170c3d74ff3c448da
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-03-05 18:49:15 -0500

    Fixed build

--- recipes/spotyping3/build.sh
+++ recipes/spotyping3/build.sh
@@ -1,3 +1,3 @@
-cp ${SRC_DIR}/SpoTyping.py ${PREFIX}/bin
+cp ${SRC_DIR}/SpoTyping-v3.0-commandLine/SpoTyping.py ${PREFIX}/bin
 mv ${PREFIX}/bin/SpoTyping.py ${PREFIX}/bin/SpoTyping
 chmod +x ${PREFIX}/bin/SpoTyping
