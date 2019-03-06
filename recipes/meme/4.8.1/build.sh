cfd7c986308541a0702907b981a569654c7b9969
Shunhua <sh60271@uga.edu>
2019-03-06 17:17:26 -0500

    debug

--- recipes/meme/4.8.1/build.sh
+++ recipes/meme/4.8.1/build.sh
@@ -9,7 +9,7 @@ MEME_ETC_DIR=${PREFIX}/etc
 #HOME=/tmp cpanm HTML::Parse
 #HOME=/tmp cpanm CGI::Application
 #HOME=/tmp cpanm XML::Parser::Expat --configure-args "EXPATLIBPATH=$PREFIX/lib" --configure-args "EXPATHINCPATH=$PREFIX/include"
-#perl scripts/dependencies.pl
+perl scripts/dependencies.pl
 ./configure --prefix="$PREFIX" #--with-gnu-ld --enable-build-libxml2 --enable-build-libxslt
 make clean
 make AM_CFLAGS='-DNAN="(0.0/0.0)"'
