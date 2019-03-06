d227fc9716cd95bec505c7c9b2f08eae4f545852
dpryan79 <dpryan79@gmail.com>
2019-03-06 17:11:48 +0100

    I hate sed on OSX

--- recipes/vsearch/build.sh
+++ recipes/vsearch/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-sed -i.bak '26i26 AC_CHECK_LIB([m],[cos])' configure.ac
+sed -i.bak '26iAC_CHECK_LIB(\[m\],\[cos\])' configure.ac
 
 ./autogen.sh
 ./configure --prefix=$PREFIX
