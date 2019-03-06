a4dd39f9e7d037b436a3917c4ca51a3175ed363c
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-03-05 19:11:17 -0500

    Fixed testing

--- recipes/spotyping3/build.sh
+++ recipes/spotyping3/build.sh
@@ -1,3 +1,4 @@
 cp ${SRC_DIR}/SpoTyping-v3.0-commandLine/SpoTyping.py ${PREFIX}/bin
+cp ${SRC_DIR}/SpoTyping-v3.0-commandLine/SpoTyping_plot.R ${PREFIX}/bin
 mv ${PREFIX}/bin/SpoTyping.py ${PREFIX}/bin/SpoTyping
 chmod +x ${PREFIX}/bin/SpoTyping
