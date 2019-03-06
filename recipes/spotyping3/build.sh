a52067c4876ba3bb02654abc8536fea232432da4
Matthew Nguyen <matthewnguyen.667@gmail.com>
2019-03-05 19:22:22 -0500

    Changed build

--- recipes/spotyping3/build.sh
+++ recipes/spotyping3/build.sh
@@ -1,4 +1,15 @@
-cp ${SRC_DIR}/SpoTyping-v3.0-commandLine/SpoTyping.py ${PREFIX}/bin
-cp ${SRC_DIR}/SpoTyping-v3.0-commandLine/SpoTyping_plot.r ${PREFIX}/bin
-mv ${PREFIX}/bin/SpoTyping.py ${PREFIX}/bin/SpoTyping
-chmod +x ${PREFIX}/bin/SpoTyping
+#!/bin/sh
+
+SPOTYPING_DIR=${PREFIX}/share/SpoTyping
+mkdir $SPOTYPING_DIR
+cp -r SpoTyping-v${PKG_VERSION}-commandLine/* $SPOTYPING_DIR
+
+PLOT_CMD=${PREFIX}/bin/SpoTyping_plot.r
+echo '#!/usr/bin/env Rscript' > $PLOT_CMD
+cat SpoTyping-v${PKG_VERSION}-commandLine/SpoTyping_plot.r >> $PLOT_CMD
+chmod a+x $PLOT_CMD
+
+MAIN_CMD=${PREFIX}/bin/SpoTyping.py
+echo '#!/usr/bin/env python' >$MAIN_CMD
+cat SpoTyping-v${PKG_VERSION}-commandLine/SpoTyping.py >> $MAIN_CMD
+chmod a+x $MAIN_CMD
