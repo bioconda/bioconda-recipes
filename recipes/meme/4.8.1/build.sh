a99aa1b69b02770c08036dbb6a810d07bcadbb89
Shunhua <sh60271@uga.edu>
2019-03-06 10:53:21 -0500

    test with 4.11.1

--- recipes/meme/4.8.1/build.sh
+++ recipes/meme/4.8.1/build.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
+
 MEME_ETC_DIR=${PREFIX}/etc
-HOME=/tmp cpanm YAML
-HOME=/tmp cpanm HTML::PullParser
-HOME=/tmp cpanm XML::Simple
-HOME=/tmp cpanm CGI
-HOME=/tmp cpanm HTML::Template
-HOME=/tmp cpanm HTML::Parse
-HOME=/tmp cpanm CGI::Application
-HOME=/tmp cpanm XML::Parser::Expat --configure-args "EXPATLIBPATH=$PREFIX/lib" --configure-args "EXPATHINCPATH=$PREFIX/include"
-
-./configure --prefix="$PREFIX" --with-gnu-ld
-
+cpanm YAML
+cpanm HTML::PullParser
+cpanm XML::Simple
+cpanm CGI
+cpanm HTML::Template
+cpanm HTML::Parse
+cpanm CGI::Application
+cpanm XML::Parser::Expat --configure-args "EXPATLIBPATH=$PREFIX/lib" --configure-args "EXPATHINCPATH=$PREFIX/include"
+./configure --prefix="$PREFIX" --with-gnu-ld # --enable-build-libxml2 --enable-build-libxslt
 make clean
 make AM_CFLAGS='-DNAN="(0.0/0.0)"'
-
-# tests will only work inside the build dir, but
-# https://github.com/conda/conda-build/issues/1453
-# so you need `conda build --prefix-length 1`
-# for it to work properly
-# make test
-
 make install
-
