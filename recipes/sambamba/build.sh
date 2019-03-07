a97d8751984d1ebbfe458488858a9d425f431940
Devon Ryan <dpryan79@gmail.com>
2019-03-07 21:24:15 +0100

    Try this

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -7,6 +7,8 @@ export LIBRARY_PATH=${PREFIX}/lib
 if [ $(uname) == "Darwin" ]; then
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
+sed -i.bak 's/ gcc / $(CC) /g' Makefile
+cd htslib && make CC=$CC LDFLAGS="-L$PREFIX/lib" && cd ..
 make CC=$CC LIBRARY_PATH=$PREFIX/lib
 make test
 mkdir -p ${PREFIX}/bin
