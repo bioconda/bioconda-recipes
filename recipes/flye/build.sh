b44e32bc91eb796b520069635ab1159829e00dd1
Mikhail Kolmogorov <fenderglass@gmail.com>
2019-03-06 17:16:03 -0800

    build fix for macos

--- recipes/flye/build.sh
+++ recipes/flye/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #zlib headers for minimap
-sed -i "8i CFLAGS+=-L$PREFIX/lib" lib/minimap2/Makefile
-sed -i "9i INCLUDES+=-I$PREFIX/include" lib/minimap2/Makefile
+sed -i.bak "8i CFLAGS+=-L$PREFIX/lib" lib/minimap2/Makefile
+sed -i.bak "9i INCLUDES+=-I$PREFIX/include" lib/minimap2/Makefile
 
 #zlib headers for flye binaries
 export CXXFLAGS="-I$PREFIX/include"
@@ -10,7 +10,7 @@ export LDFLAGS="-L$PREFIX/lib"
 
 #dynamic flag is needed for backtrace printing, 
 #but it seems it fails OSX build
-sed -i 's/-rdynamic//' src/Makefile
+sed -i.bak 's/-rdynamic//' src/Makefile
 
 python setup.py build
 python setup.py install  --record record.txt.
