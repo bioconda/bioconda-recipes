3a30904baecbd65d94f3c61cb9b7da862cb1e679
dpryan79 <dpryan79@gmail.com>
2019-03-07 10:46:00 +0100

    A few fixes

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -3,6 +3,7 @@ set -eu
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
 
 if [ $(uname) == "Darwin" ]; then
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
