30bdce9dae26d2568862633dfe70e3f33469f743
Shunhua <sh60271@uga.edu>
2019-03-07 13:05:14 -0500

    debug

--- recipes/meme/4.8.1/build.sh
+++ recipes/meme/4.8.1/build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-MEME_ETC_DIR=${PREFIX}/etc
-#HOME=/tmp cpanm YAML
-#HOME=/tmp cpanm HTML::PullParser
-#HOME=/tmp cpanm XML::Simple
-#HOME=/tmp cpanm CGI
-#HOME=/tmp cpanm HTML::Template
-#HOME=/tmp cpanm HTML::Parse
-#HOME=/tmp cpanm CGI::Application
-#HOME=/tmp cpanm XML::Parser::Expat --configure-args "EXPATLIBPATH=$PREFIX/lib" --configure-args "EXPATHINCPATH=$PREFIX/include"
+#MEME_ETC_DIR=${PREFIX}/etc
 perl scripts/dependencies.pl
-./configure --prefix="$PREFIX" --enable-build-libxml2 --enable-build-libxslt #--with-gnu-ld
+./configure --prefix="$PREFIX"
 make clean
-make AM_CFLAGS='-DNAN="(0.0/0.0)"'
+#make AM_CFLAGS='-DNAN="(0.0/0.0)"'
 make install
