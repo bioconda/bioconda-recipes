a55d8568077fdb871905c2fe481525c83f7e359d
Romain Feron <romain.feron.91@gmail.com>
2019-03-07 16:02:39 +0100

    Added building lastz_32 to lastz recipe. This binary is used for sequences bigger than 2Gb

--- recipes/lastz/build.sh
+++ recipes/lastz/build.sh
@@ -2,10 +2,13 @@
 
 mkdir -p $PREFIX/bin
 
-make 
+make
+make lastz_32
 
-chmod +x  src/lastz
-chmod +x  src/lastz_D
+chmod +x src/lastz
+chmod +x src/lastz_D
+chmod +x src/lastz_32
 
 mv src/lastz $PREFIX/bin
 mv src/lastz_D $PREFIX/bin
+mv src/lastz_32 $PREFIX/bin
