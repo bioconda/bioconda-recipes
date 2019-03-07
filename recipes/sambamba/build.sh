20ce435bd6276595cd0f5ebd6d8639ab6a4afa9d
dpryan79 <dpryan79@gmail.com>
2019-03-07 16:19:24 +0100

    Ah, should have read the Makefile

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -3,13 +3,11 @@ set -eu
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="-L$PREFIX/lib"
 
 if [ $(uname) == "Darwin" ]; then
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
-make CC=$CC
+make CC=$CC LIBRARY_PATH=$PREFIX/lib
 make test
 mkdir -p ${PREFIX}/bin
 cp bin/sambamba ${PREFIX}/bin/sambamba
