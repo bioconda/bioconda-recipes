8dd1bb09666da8775a02ddecaf624bba00bbed39
Pau Andrio <andriopau@gmail.com>
2019-03-09 00:00:10 +0100

    Copy to home directory

--- recipes/biobb_wf_mutations/build.sh
+++ recipes/biobb_wf_mutations/build.sh
@@ -4,4 +4,4 @@ python3 setup.py install --single-version-externally-managed --record=record.txt
 
 mkdir -p $HOME/$PKG_NAME
 
-cp -R $SP_DIR/$PKG_NAME/* $HOME/$PKG_NAME
+cp -R $SP_DIR/$PKG_NAME $HOME/$PKG_NAME
