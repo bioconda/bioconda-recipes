48925c4abd0556aa3e69398377da01a77f74a9f5
Pau Andrio <andriopau@gmail.com>
2019-03-10 18:19:38 +0100

    Replacing ~ to $HOME

--- recipes/biobb_wf_mutations/build.sh
+++ recipes/biobb_wf_mutations/build.sh
@@ -3,4 +3,4 @@
 python3 setup.py install --single-version-externally-managed --record=record.txt
 
 mkdir -p ~/mutations
-cp -R notebooks/mutations.ipynb ~/biobb_wf_mutations
+cp -R notebooks/mutations.ipynb ~/mutations/
