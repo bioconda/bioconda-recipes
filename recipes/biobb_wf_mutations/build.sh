d082f739991da404718298b0c3402f0e91db2397
Pau Andrio <andriopau@gmail.com>
2019-03-10 18:12:18 +0100

    Using ~ instead of /Users/pau envvar

--- recipes/biobb_wf_mutations/build.sh
+++ recipes/biobb_wf_mutations/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 python3 setup.py install --single-version-externally-managed --record=record.txt
+
+mkdir -p ~/mutations
+cp -R notebooks/mutations.ipynb ~/biobb_wf_mutations
--- recipes/biobb_wf_mutations/post-link.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p $HOME/biobb_wf_mutations
-
-cp -R $SP_DIR/biobb_wf_mutations/notebooks/mutations.ipynb $HOME/biobb_wf_mutations
