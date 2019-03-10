652dae10f3a62346158d28896d6ee92a173e7795
Pau Andrio <andriopau@gmail.com>
2019-03-10 17:33:41 +0100

    Copying notebook to Home dir in post-link

--- recipes/biobb_wf_mutations/build.sh
+++ recipes/biobb_wf_mutations/build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 python3 setup.py install --single-version-externally-managed --record=record.txt
-
-mkdir -p $HOME/biobb_wf_mutations
-
-cp -R $SP_DIR/biobb_wf_mutations/notebooks/mutations.ipynb $HOME/biobb_wf_mutations
---
+++ recipes/biobb_wf_mutations/post-link.sh
@@ -0,0 +1,3 @@
+mkdir -p $HOME/biobb_wf_mutations
+
+cp -R $SP_DIR/biobb_wf_mutations/notebooks/mutations.ipynb $HOME/biobb_wf_mutations
