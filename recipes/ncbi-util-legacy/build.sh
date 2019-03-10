c968e903e431466cf8e4c94782ba375fd88c89a4
M Bernt <m.bernt@ufz.de>
2019-03-10 23:28:09 +0100

    ncbi dev toolkit: initial commit

--- recipes/ncbi-util-legacy/build.sh
+++ recipes/ncbi-util-legacy/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
->&2 ls 
->&2 ls ncbi/make
-
 # chanhe makedis interpreter to tcsh
 sed -i -e '1s@#!.*@#!/usr/bin/env tcsh@' ./ncbi/make/makedis.csh
-./ncbi/make/makedis.csh
+./ncbi/make/makedis.csh 
 
->&2 ls ncbi/build
+cp -rL ncbi/include/* "$PREFIX/include/"
+cp ncbi/lib/* "$PREFIX/lib/"
+cp ncbi/bin/* "$PREFIX/bin/"
