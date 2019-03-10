da9528040d7a14eb2915d82ca0b13ff641fd2a1b
Pau Andrio <andriopau@gmail.com>
2019-03-10 18:53:04 +0100

    Checking the bioconda test dir structure

--- recipes/biobb_wf_mutations/build.sh
+++ recipes/biobb_wf_mutations/build.sh
@@ -3,4 +3,7 @@
 python3 setup.py install --single-version-externally-managed --record=record.txt
 
 mkdir -p ~/mutations
-cp -R notebooks/mutations.ipynb ~/mutations/
+ls $SP_DIR
+cwd
+ls -R $SP_DIR/biobb_wf_mutations
+cp $SP_DIR/biobb_wf_mutations/notebooks/mutations.ipynb ~/mutations/
