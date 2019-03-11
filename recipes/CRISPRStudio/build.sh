fb506d3d36f85fa1b4a3fb9e769e655c073a1605
klamoure <klamoure@cscscience.ca>
2019-03-11 10:57:03 -0500

    Version 1 of CRISPR Studio

--- recipes/CRISPRStudio/build.sh
+++ recipes/CRISPRStudio/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./Install.sh
+cp CRISPR_Studio_1.0.py $PREFIX/bin
+cp -r tools/ $PREFIX/bin
+ln -s  $PREFIX/bin/CRISPR_Studio_1.0.py $PREFIX/bin/CRISPR_Studio
+chmod +x $PREFIX/bin/CRISPR_Studio
