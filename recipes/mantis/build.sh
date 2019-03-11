bcf9ae7ed9b3ea6baf6c0d94cdee863b8e406e2f
John Lees <lees.john6@gmail.com>
2019-03-11 16:00:19 -0400

    Build sdsl-lite too

--- recipes/mantis/build.sh
+++ recipes/mantis/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# build spydrpick
+# build sdsl-lite for static linking (required as c++17 is required - conda-forge is c++14)
+mkdir sdsl-lite && pushd sdsl-lite
+CXXFLAGS="${CXXFLAGS} std=gnu++1z"
+mkdir build
+wget https://github.com/simongog/sdsl-lite/releases/download/v2.1.1/sdsl-lite-2.1.1.tar.gz.offline.install.gz
+tar xf sdsl-lite-2.1.1.tar.gz.offline.install.gz
+./install.sh build
+popd
+
+# build mantis
 mkdir build && pushd build
-cmake -DCMAKE_BUILD_TYPE=Release .. # pass -DNH=1 to disable Haswell instructions
+cmake -DCMAKE_BUILD_TYPE=Release -DSDSL_INSTALL_PATH=sdsl-lite/build .. # pass -DNH=1 to disable Haswell instructions
 make install VERBOSE=1
 popd
