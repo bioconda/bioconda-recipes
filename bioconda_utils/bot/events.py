5d65d1dde882c7d9058cb5390a76d551350973b1
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-03-11 13:14:27 -0600

    Bot: create check run on check_suite event

--- bioconda_utils/bot/events.py
+++ bioconda_utils/bot/events.py
@@ -7,7 +7,9 @@ import re
 
 import gidgethub.routing
 
-from .commands import command_routes
+from .commands import command_routes, get_pr_info
+from .tasks import lint_check, PRInfo
+from ..githubhandler import CheckRunStatus, CheckRunConclusion
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 event_routes = gidgethub.routing.Router()  # pylint: disable=invalid-name
@@ -38,3 +40,65 @@ async def comment_created(event, ghapi, *args, **_kwargs):
 async def demo_using_status(_event, _ghapi):
     """Test function for status events"""
     logger.warning("Status - not handling")
+
+
+@event_routes.register("check_suite")
+async def check_suite(event, ghapi):
+    """Handle check suite event
+
+    If the action is not ``completed`` but ``requested`` (by a commit)
+    or ``rerequested`` (manually), we create a new **check run** an
+    queue a task to lint the PR
+    """
+
+    action = event.get('action')
+    if action not in ['requested', 'rerequested']:
+        return
+    head_sha = event.get("check_suite/head_sha")
+    if not head_sha:
+        logger.error("Check_suite event has no head_sha?!")
+        return
+    prs = event.get("check_suite/pull_requests", [])
+    if not prs:
+        logger.error("Check_suite event had no associated pull requests (merge?)")
+
+    # As per API, a check run, identified by the commit sha, can have multiple PRs
+    # from different repos associated with it. Not quite sure when that can happen,
+    # so we log it as error for now:
+    if len(prs) > 1:
+        logger.error("Multiple PRs in check run - using only first")
+
+    issue_number = prs[0]['number']
+
+    pr = await ghapi.get_prs(number=int(issue_number))
+    if not pr:
+        logger.error("No PRs with number %s?", pr['number'])
+        return
+
+    files = await ghapi.get_pr_modified_files(number=issue_number)
+    recipes = [item['filename'] for item in files
+               if item['filename'].endswith('/meta.yaml')]
+
+    check_run_number = await ghapi.create_check_run("Linting modified recipes", head_sha)
+    logger.warning("Created check run %s", check_run_number)
+
+    if not recipes:
+        logger.warning("Created check run %s", check_run_number)
+        ghapi.modify_check_run(check_run_number,
+                               status=CheckRunStatus.completed,
+                               conclusion=CheckRunConclusion.success,
+                               output_title="No recipes modified by PR",
+                               output_summary="Didn't do anything",
+                               output_text=
+                               "While it's true that all modified recipes are in "
+                               "good condition, it's also true that all of them are in "
+                               "bad condition. Not having found any problems, I'm waving you "
+                               "past me, so I can ponder that logical conundrum.")
+        return
+
+    user = pr['head']['user']['login']
+    repo = pr['head']['repo']['name']
+    ref = pr['head']['ref']
+    pr_info = PRInfo(event.get('installation/id'), user, repo, ref, recipes, issue_number)
+
+    lint_check.schedule(pr_info, head_sha, check_run_number, ghapi=ghapi)
--- bioconda_utils/bot/tasks.py
+++ bioconda_utils/bot/tasks.py
@@ -13,6 +13,7 @@ from .config import BOT_NAME, BOT_EMAIL
 from .. import utils
 from ..recipe import Recipe
 from ..githandler import TempGitHandler
+from ..githubhandler import CheckRunStatus, CheckRunConclusion
 
 if TYPE_CHECKING:
     from .worker import AsyncTask
@@ -69,19 +70,49 @@ async def bump(self: "AsyncTask", pr_info_dict: "Dict", ghapi_data=None):
             logger.error("Failed to push?!")
 
 
-@celery.task(bind=True, acks_late=True)
-async def lint(self: "AsyncTask", pr_info_dict: "Dict", ghapi_data=None):
-    """Lint each recipe"""
-    pr_info = PRInfo(**pr_info_dict)
-    logger.info("Processing lint command: %s", pr_info)
-
-    async with PrBranch(self.ghappapi, pr_info) as git:
+async def do_lint(ghappapi, pr_info):
+    async with PrBranch(ghappapi, pr_info) as git:
         utils.load_config('config.yml')
         from bioconda_utils.linting import lint as _lint, LintArgs, markdown_report
         recipes = [r[:-len('/meta.yaml')] for r in pr_info.recipes]
         df = _lint(recipes, LintArgs())
         msg = markdown_report(df)
-        await self.ghapi.create_comment(pr_info.issue_number, msg)
+    return df, msg
+
+
+@celery.task(bind=True, acks_late=True)
+async def lint(self: "AsyncTask", pr_info_dict: "Dict", ghapi_data=None):
+    """Lint each recipe"""
+    pr_info = PRInfo(**pr_info_dict)
+    logger.info("Processing lint command: %s", pr_info)
+    df, msg = await do_lint(self.ghappapi, pr_info)
+    await self.ghapi.create_comment(pr_info.issue_number, msg)
+
+
+@celery.task(bind=True, acks_late=True)
+async def lint_check(self: "AsyncTask", pr_info_dict: "Dict", head_sha: str,
+                     check_run_number: int, ghapi_data=None):
+    """Lint each recipe"""
+    pr_info = PRInfo(**pr_info_dict)
+    logger.info("Starting lint check: %s", pr_info)
+    await self.ghapi.modify_check_run(check_run_number,
+                                      status=CheckRunStatus.in_progress)
+    # FIXME: check if current sha is head_sha, if not, cancel
+    df, msg = await do_lint(self.ghappapi, pr_info)
+    if df is None:
+        conclusion = CheckRunConclusion.success
+        title = "All recipes in good condition"
+        summary = "No problems found"
+    else:
+        conclusion = CheckRunConclusion.failure
+        title = "Some recipes had problems"
+        summary = "Please fix the listed issues"
+
+    await self.ghapi.modify_check_run(check_run_number,
+                                      status=CheckRunStatus.completed,
+                                      conclusion=conclusion,
+                                      output_title=title, output_summary=summary,
+                                      output_text=msg)
 
 
 @celery.task(acks_late=True)
