c9e9e1a3c1ff36f0ae042dec5df7c5db8842f130
klamoure <klamoure@cscscience.ca>
2019-03-12 09:39:35 -0500

    Changed path of helper package

--- recipes/CRISPRStudio/build.sh
+++ recipes/CRISPRStudio/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp CRISPR_Studio_1.0.py $PREFIX/bin
-cp -r tools/ $PREFIX/bin
+mkdir $PREFIX/bin/tools
+cp -r fasta-36.3.8g/ $PREFIX/bin/tools
 ln -s  $PREFIX/bin/CRISPR_Studio_1.0.py $PREFIX/bin/CRISPR_Studio
 chmod +x $PREFIX/bin/CRISPR_Studio
