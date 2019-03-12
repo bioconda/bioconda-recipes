11d96c385f070d0616edf5ac8f201468e6ee4bcf
John Lees <lees.john6@gmail.com>
2019-03-12 12:04:24 -0400

    Fix sdsl build dir name

--- recipes/mantis/build.sh
+++ recipes/mantis/build.sh
@@ -4,13 +4,13 @@
 wget https://github.com/simongog/sdsl-lite/releases/download/v2.1.1/sdsl-lite-2.1.1.tar.gz.offline.install.gz
 tar xf sdsl-lite-2.1.1.tar.gz.offline.install.gz
 pushd sdsl-lite-2.1.1
-mkdir static-build
+mkdir $SRC_DIR/sdsl-build
 CXXFLAGS="${CXXFLAGS} -std=gnu++1z"
-./install.sh static-build
+./install.sh $SRC_DIR/sdsl-build
 popd
 
 # build mantis
 mkdir build && pushd build
-cmake -DCMAKE_BUILD_TYPE=Release -DSDSL_INSTALL_PATH=sdsl-lite/static-build .. # pass -DNH=1 to disable Haswell instructions
+cmake -DCMAKE_BUILD_TYPE=Release -DSDSL_INSTALL_PATH=$SRC_DIR/sdsl-build .. # pass -DNH=1 to disable Haswell instructions
 make install VERBOSE=1
 popd
