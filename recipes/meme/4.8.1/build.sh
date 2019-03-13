f0e9acec22e849e5fefcf807d9e5045a5b8b7d74
Shunhua <sh60271@uga.edu>
2019-03-13 10:57:31 -0400

    test

--- recipes/meme/4.8.1/build.sh
+++ recipes/meme/4.8.1/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 MEME_ETC_DIR=${PREFIX}/etc
-perl scripts/dependencies.pl
+#perl scripts/dependencies.pl
 ./configure --prefix="$PREFIX" --enable-build-libxml2 --enable-build-libxslt
-make clean
-#make AM_CFLAGS='-DNAN="(0.0/0.0)"'
+make
+make AM_CFLAGS='-DNAN="(0.0/0.0)"'
 make install
