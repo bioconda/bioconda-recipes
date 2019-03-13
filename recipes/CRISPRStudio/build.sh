1e8ad114806563ebbb86f7bda7e592cc1cc6ea06
klamoure <klamoure@cscscience.ca>
2019-03-13 12:42:36 -0500

    Fixed requirements/path to fast36 install

--- recipes/CRISPRStudio/build.sh
+++ recipes/CRISPRStudio/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+mkdir -p $PREFIX/bin/tools/
 cp CRISPR_Studio_1.0.py $PREFIX/bin
-mkdir $PREFIX/bin/tools
-cp -r fasta-36.3.8g/ $PREFIX/bin/tools
+cp -r fasta-36.3.8g $PREFIX/bin/tools
 ln -s  $PREFIX/bin/CRISPR_Studio_1.0.py $PREFIX/bin/CRISPR_Studio
 chmod +x $PREFIX/bin/CRISPR_Studio
