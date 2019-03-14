fa02f3124ece309b2325a7e4b23336af23020b59
Natasha Pavlovikj <natasha.pavlovikj@yahoo.com>
2019-03-14 03:42:15 -0500

    Test

--- recipes/halla/build.sh
+++ recipes/halla/build.sh
@@ -2,9 +2,11 @@
 
 PY3_BUILD="${PY_VER%.*}"
 
-if [ $PY3_BUILD -eq 3 ]; then
-    for i in halla/*.py; do
-        2to3 -w -n $i
+if [ $PY3_BUILD -eq 3 ]
+then
+    for i in halla/*.py
+    do
+        2to3 --write $i
     done
 fi
 
