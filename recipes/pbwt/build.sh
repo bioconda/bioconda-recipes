6bc7ec9751ea8d9e5efafb419f567b311b27f071
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-03-14 16:02:00 -0400

    better download strategy

--- recipes/pbwt/build.sh
+++ recipes/pbwt/build.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-
-export CXXFLAGS="${LDFLAGS} ${CPPFLAGS}"
-
-git clone https://github.com/richarddurbin/pbwt
-cd pbwt
-git checkout 64c4ffa7880db5f85b53ccbd7e530ec4b95ea4ba
+cd pbwt*
 sed -i.bak 's/$(HTSLIB)/-lhts/' Makefile
 make
 cp pbwt $PREFIX/bin
