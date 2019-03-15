8f303d32eaa224fff7e32a2f76921583d9f3c026
Sebastian Uhrig <s.uhrig@dkfz.de>
2019-03-15 10:47:27 +0100

    Arriba 1.0.1: use HTSlib of bioconda

--- recipes/arriba/build.sh
+++ recipes/arriba/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# compile HTSlib and Arriba
-make -C htslib-1.8 CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" libhts.a
-make CXXFLAGS="-pthread -std=c++0x -O2 -Isource -Ihtslib-1.8/htslib -I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -ldeflate -lz -llzma -lbz2"
+# compile Arriba
+make HTSLIB="$PREFIX/lib" CXXFLAGS="-pthread -std=c++0x -O2 -Isource -I$PREFIX/include/htslib -I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -ldl -ldeflate -lz -llzma -lbz2"
 
 # copy executables
 mkdir -p "$PREFIX/bin"
