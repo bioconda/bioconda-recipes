6f0cd36aae7611897220155f7a165cd9b4733536
Dan Fornika <dfornika@gmail.com>
2019-03-15 15:21:57 -0700

    set INCLUDE_PATH

--- recipes/sts/build.sh
+++ recipes/sts/build.sh
@@ -4,8 +4,8 @@ git submodule init
 git submodule update
 
 export CXXFLAGS="-std=c++11"
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
+export INCLUDE_PATH=${PREFIX}/include
+
 
 make
 mkdir -p $PREFIX/bin
