e865c54934ed0a32c3a04097a1a69cefb0c49ff0
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2019-03-15 22:54:58 -0500

    Apply 2to3 on all python versions

--- recipes/stringtie/build.sh
+++ recipes/stringtie/build.sh
@@ -12,9 +12,7 @@ mv stringtie $PREFIX/bin
 # Prepare prepDE
 # This is equivalent to https://github.com/gpertea/stringtie/blob/master/prepDE.py
 curl -LO http://ccb.jhu.edu/software/stringtie/dl/prepDE.py
-if [ "$PY3K" == 1 ]; then
-    2to3 -w --no-diffs prepDE.py
-fi
+2to3 -w --no-diffs prepDE.py
 sed -i.bak 's|/usr/bin/env python2|/usr/bin/env python|' prepDE.py
 mv prepDE.py $PREFIX/bin
 chmod +x $PREFIX/bin/prepDE.py
