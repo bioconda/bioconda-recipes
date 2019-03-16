e363d2903d072f4514393b80468eae2c46c7a6d5
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-03-16 13:35:23 -0600

    Githubhandler: add repr

--- bioconda_utils/githubhandler.py
+++ bioconda_utils/githubhandler.py
@@ -78,6 +78,9 @@ class GitHubHandler:
     def __str__(self):
         return f"{self.__class__.__name__}({self.user}/{self.repo})"
 
+    def __repr__(self):
+        return str(self)
+
     def for_json(self):
         return {
             '__module__': self.__module__,
