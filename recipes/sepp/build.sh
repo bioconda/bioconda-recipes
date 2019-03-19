c585484abec875ff036a90f8d0a4864348cac238
Stefan Janssen <stefan.m.janssen@gmail.com>
2019-03-19 22:39:47 +0100

    script to build sepp

--- recipes/sepp/build.sh
+++ recipes/sepp/build.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-python setup.py config --prefix=${PREFIX}
-python setup.py install
+python setup.py config -c
+
+# ensure SEPP's configuration file is at the correct location ...
+echo "${PREFIX}/share/sepp/.sepp" > home.path
+mkdir -p $PREFIX/share/sepp/.sepp
+# ... and holds correct path names
+mv -v default.main.config $PREFIX/share/sepp/.sepp/main.config
+patch $PREFIX/share/sepp/.sepp/main.config < relocate.main.config.patch
+
+python setup.py install --prefix=${PREFIX}
+
+# copy bundled binaries into $PREFIX/bin/
+mkdir -p $PREFIX/bin/
+cp `cat $SRC_DIR/.sepp/main.config | grep "^path" -m 1 | cut -d "=" -f 2 | xargs dirname`/* $PREFIX/bin/
+
+# configure run-sepp.sh for qiime2 fragment-insertion
+mv -v sepp-package/run-sepp.sh $PREFIX/bin/run-sepp.sh
+patch $PREFIX/bin/run-sepp.sh < relocate.run-sepp.sh.patch
