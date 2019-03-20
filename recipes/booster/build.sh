07fe0c0f0426f0f2680dfa27a40f0685f8e039e8
Matt <mbfiamenghi@gmail.com>
2019-03-19 22:07:13 -0300

    changed meta.yaml and build.sh files to try and compile on osx

--- recipes/booster/build.sh
+++ recipes/booster/build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+CXXFLAGS="$CXXFLAGS";
+LDFLAGS="$LDFLAGS -Wl,-rpath ${PREFIX}/lib";
+CXX=g++;
+CC=gcc;
+
+if [ `uname` == Darwin ] ; then
+    CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+    LDFLAGS="$LDFLAGS -stdlib=libc++"
+    CXX=clang++;
+    CC=clang;
+else ## linux
+    CXXFLAGS="$CXXFLAGS -fopenmp"
+fi
+
+export CC=${CC}
+export CXX=${CXX}
+export CXXFLAGS=${CXXFLAGS}
+export LDFLAGS=${LDFLAGS}
+            
 cd $SRC_DIR/src
 make
 
