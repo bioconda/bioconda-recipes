a040da6abb3d549d4f1f36d9c4db596c6f7aa42b
Mateus Bernabe Fiamenghi <35854519+Matt-BF@users.noreply.github.com>
2019-03-19 21:49:30 -0300

    Changed build.sh to try and build on osx

--- recipes/booster/build.sh
+++ recipes/booster/build.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+CXXFLAGS="$CXXFLAGS";
+LDFLAGS="$LDFLAGS -Wl,-rpath ${PREFIX}/lib";
+CXX=g++;
+CC=gcc;
+
+if [ `uname` == Darwin ] ; then
+    CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+    LDFLAGS="$LDFLAGS -stdlib=libc++"
+    CXX=clang++;
+    CC=clang;
+else ## linux
+    CXXFLAGS="$CXXFLAGS -fopenmp"
+fi
+
+export CC=${CC}
+export CXX=${CXX}
+export CXXFLAGS=${CXXFLAGS}
+export LDFLAGS=${LDFLAGS}
+            
 cd $SRC_DIR/src
 make
 
 mkdir -p $PREFIX/bin
 cp booster $PREFIX/bin
-
