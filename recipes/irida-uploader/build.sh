7f12357a1dab0d19038405f7cf70c9b896ae94e7
Kurt Hanel <khanel@cscscience.ca>
2019-03-20 15:18:16 -0500

    Remove virtualenv creation

--- recipes/irida-uploader/build.sh
+++ recipes/irida-uploader/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin/irida_uploader
 cp -a * ${PREFIX}/bin/irida_uploader
-python3 -m venv $PREFIX/bin/.virtualenv
 ln -s $PREFIX/bin/irida_uploader/upload_run.py $PREFIX/bin/irida-uploader
 chmod +x $PREFIX/bin/irida-uploader
