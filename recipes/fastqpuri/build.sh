c5c9faeec67f6e437c894693509737c9a9b8dedc
Claudio Lottaz <claudio.lottaz@ur.de>
2019-03-21 10:56:34 +0100

    fix more issues in recipe Changes to be committed: 	modified:   build.sh 	modified:   meta.yaml

--- recipes/fastqpuri/build.sh
+++ recipes/fastqpuri/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
-cmake -H. -Bbuild -DRSCRIPT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=${CC}
+Rscript -e 'install.packages(c("digest", "htmltools", "rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
+cmake -H. -Bbuild -DRSCRIPT=${PREFIX}/bin/Rscript -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=${CC}
 cd build
 make
 make install
