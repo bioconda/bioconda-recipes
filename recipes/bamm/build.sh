1747f90cc8e71a9e05e2a2537931cabd7dc076d0
Andreas Sjödin <andreas.sjodin@gmail.com>
2019-03-21 11:05:53 +0100

    Small fix

--- recipes/bamm/build.sh
+++ recipes/bamm/build.sh
@@ -3,6 +3,6 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 (rm -rf c/htslib-1.3.1/)
-(cd c/ && ./autogen.sh)
+(cd c/ && touch ChangeLog && ./autogen.sh)
 
 $PYTHON setup.py install --with-libhts-lib ${PREFIX}/lib --with-libhts-inc ${PREFIX}/include/htslib
