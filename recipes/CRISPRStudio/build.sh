b91766193d7e8df966c3b95a8baddfedeeacfbc4
klamoure <klamoure@cscscience.ca>
2019-03-22 11:43:40 -0500

    Made the suggested changes

--- recipes/CRISPRStudio/build.sh
+++ recipes/CRISPRStudio/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mkdir -p $PREFIX/bin/tools/
-cp CRISPR_Studio_1.0.py $PREFIX/bin
-cp -r fasta-36.3.8g $PREFIX/bin/tools
-ln -s  $PREFIX/bin/CRISPR_Studio_1.0.py $PREFIX/bin/CRISPR_Studio
+cp CRISPR_Studio_1.0.py $PREFIX/bin/CRISPR_Studio
 chmod +x $PREFIX/bin/CRISPR_Studio
