f9c21a27b377787a4e841cb12c5137cb54deae44
Matt <mbfiamenghi@gmail.com>
2019-03-22 20:03:00 -0300

    added llvm-openmp to build and run on meta.yaml, removed host, simplified build.sh

--- recipes/booster/build.sh
+++ recipes/booster/build.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-CXXFLAGS="$CXXFLAGS";
-CFLAGS="$CFLAGS";
-LDFLAGS="$LDFLAGS -Wl,-rpath ${PREFIX}/lib";
-CXX=g++;
-CC=gcc;
-
-if [ `uname` == Darwin ] ; then
-    CXXFLAGS="$CXXFLAGS -stdlib=libc++"
-    CFLAGS="$CFLAGS -stdlib=libgcc"
-    LDFLAGS="$LDFLAGS -stdlib=libc++"
-    CXX=clang++;
-    CC=clang;
-else ## linux
-    CXXFLAGS="$CXXFLAGS -fopenmp"
-fi
-
-export CC=${CC}
-export CXX=${CXX}
-export CXXFLAGS=${CXXFLAGS}
-export CFLAGS=${CFLAGS}
-export LDFLAGS=${LDFLAGS}
-            
 cd $SRC_DIR/src
 make
 
