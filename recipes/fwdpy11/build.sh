e7b314e6d169a932b43eb81e3bc62d07732e9200
Devon Ryan <dpryan79@gmail.com>
2019-03-23 15:38:58 +0100

    try setting the environment variable

--- recipes/fwdpy11/build.sh
+++ recipes/fwdpy11/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export GSL_INCLUDE_DIR=${PREFIX}/include
 export GSL_LIBRARY=${PREFIX}/lib
+export PKG_CONFIG_EXECUTABLE=${PREFIX}/bin
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
