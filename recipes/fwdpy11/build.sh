cc37fe1eaedc3df906b48c7022d12ab852b8f3c0
molpopgen <krthornt@uci.edu>
2019-03-24 07:50:00 -0700

    GSL_ROOT_DIR

--- recipes/fwdpy11/build.sh
+++ recipes/fwdpy11/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-$PYTHON setup.py install --gcc
+# cmake's "find GSL macro" can barf when conda
+# envs are involved, so we use GSL_ROOT_DIR to 
+# fix that:
+GSL_ROOT_DIR=$PREFIX $PYTHON setup.py install 
