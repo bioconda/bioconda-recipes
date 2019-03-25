27b44dbcb4bae8403628ec64151c10c880082013
dpryan79 <dpryan79@gmail.com>
2019-03-25 19:52:46 +0100

    partial Config.pm fix from conda-forge

--- recipes/perl-net-ssleay/build.sh
+++ recipes/perl-net-ssleay/build.sh
@@ -12,16 +12,36 @@ else
     export LD_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
+perl -V
+wrong_prefix="$(perl -V | sed -rn "s|\s+cc='([^']+)/bin/.*|\1|p")"
+if test -n "$wrong_prefix"; then
+  echo "Perl has broken build env prefix. Fixing..."
+  good_prefix="${CC%/bin/*}"
+  echo "Replacing"
+  echo "  $wrong_prefix"
+  echo "with"
+  echo "  $good_prefix"
+  grep -rlI "$wrong_prefix" $(perl -e 'print "@INC"') | \
+      sort -u |\
+      xargs sed -ibak "s|$wrong_prefix|$good_prefix|g"
+  if perl -V | grep "$wrong_prefix"; then
+     echo "Failed to fix paths - expect breakage below"
+  else
+      echo "Sucesss!"
+  fi
+fi
+perl -V
+
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
-    perl Build.PL CC=$CC
+    perl Build.PL
     ./Build
     ./Build test
     # Make sure this goes in site
     ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site CC=$CC
+    perl Makefile.PL INSTALLDIRS=site
     make
     make test
     make install
