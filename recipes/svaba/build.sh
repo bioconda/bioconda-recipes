873152fec663d62883ee02bd76107db84c20278e
Michael Knudsen <micknudsen@gmail.com>
2019-03-25 15:54:04 +0100

    Use newer SeqLib version

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 rm -rf SeqLib
 git clone --recursive https://github.com/walaj/SeqLib
 cd SeqLib
-git checkout 770cd10c308430e1719d54ebedcfe708db560bec
+git checkout f7a89a127409a3f52fdf725fa74e5438c68e48fb
 cd ..
 
 export C_INCLUDE_PATH=$PREFIX/include
