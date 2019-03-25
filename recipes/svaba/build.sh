650c6b60cc32565ef2d8a36395730e0fc59f1307
Michael Knudsen <micknudsen@gmail.com>
2019-03-25 21:13:41 +0100

    Debugging

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
-set -eu -o pipefail
+
+echo "###################"
+echo "#      DEBUG      #"
+echo "###################"
+
+echo ""
+echo "Current directory:" $(pwd)
+echo ""
+echo "Contents:"
+echo ""
+ls -l
+echo ""
 
 rm -rf SeqLib
 git clone --recursive https://github.com/walaj/SeqLib
 cd SeqLib
 git checkout f7a89a127409a3f52fdf725fa74e5438c68e48fb
 cd ..
+
+echo "##################"
+echo "#   MORE DEBUG   #"
+echo "##################"
+
+echo ""
+ls -l SeqLib
+echo ""
+
+echo "##################"
+echo "#   DEBUG DONE   #"
+echo "##################"
 
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
