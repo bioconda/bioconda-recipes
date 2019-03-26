ca38bcbf7b66786e600b5d689ef0c4980eb43b31
Michael Knudsen <micknudsen@gmail.com>
2019-03-26 07:58:37 +0100

    Try cloning

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
 
-rm -rf SeqLib
-git clone --recursive https://github.com/walaj/SeqLib
-cd SeqLib
-git checkout f7a89a127409a3f52fdf725fa74e5438c68e48fb
 cd ..
+
+rm -fr /opt/conda/conda-bld/svaba_1553545215578/work
+git clone --recursive https://github.com/walaj/svaba.git work
+
+cd work
+
+git checkout 1.1.0
+git submodule update --recursive
 
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
