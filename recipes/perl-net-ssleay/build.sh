c5c8729af9016ebe8518819f2b3ceb95a2a95c4a
dpryan79 <dpryan79@gmail.com>
2019-03-26 15:15:35 +0100

    This might actually build on linux. Don't know about make test

--- recipes/perl-net-ssleay/build.sh
+++ recipes/perl-net-ssleay/build.sh
@@ -19,11 +19,10 @@ fi
 # Config_heavy.pl holds the cflags in ccflags
 # ./lib/5.26.2/x86_64-linux-thread-multi/Config_heavy.pl
 #     ccflags ='${CFLAGS}'
+export CFLAGS="-I/usr/include $CFLAGS"
 dname=`find $PREFIX/lib -name Config_heavy.pl -print | xargs dirname`
 sed -i.bak "s|^    cc => .*$|    cc => '${CC}',|" ${dname}/Config.pm
-sed -i.bak "s|^    ccflags =.*$|    ccflags ='${CFLAGS}'|" ${dname}/Config_heavy.pl
-
-perl -V
+sed -i.bak "s|^ccflags=.*$|ccflags='${CFLAGS}'|;s|^ld=.*$|ld='${CC}'|;s|^cppflags=.*$|cppflags='${CFLAGS}'|;" ${dname}/Config_heavy.pl
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
@@ -35,6 +34,7 @@ if [ -f Build.PL ]; then
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
+    sed -i.bak "s|^LDLOADLIBS = .*$|LDLOADLIBS = -L$PREFIX/lib -lssl -lcrypto -lz -lpthread|;s|/home/conda/feedstock_root/build_artifacts/perl_1550669032175/_build_env|$BUILD_PREFIX|g" Makefile
     make
     make test
     make install
@@ -42,9 +42,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
