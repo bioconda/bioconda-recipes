2929e11f89fb435b08a90a1738227801cd0341e2
Eric Roberts <notthebmovieactor@gmail.com>
2019-03-27 11:46:43 -0400

    Remove functional testing from Segway 2.0.4 recipe

--- recipes/segway/build.sh
+++ recipes/segway/build.sh
@@ -5,5 +5,5 @@ export HDF5_DIR=$PREFIX
 $PYTHON -m pip install -vv --disable-pip-version-check --no-deps --no-cache-dir --ignore-installed . 
 # Work around for no 'source_files' support in test section of meta.yaml
 # Only use test simpleseg as a sanity check
-cp test/test_all.sh $PREFIX/bin/run_segway_tests.sh
-cp -r test/simpleseg $PREFIX/bin/segway_simpleseg_test
+# cp test/test_all.sh $PREFIX/bin/run_segway_tests.sh
+# cp -r test/simpleseg $PREFIX/bin/segway_simpleseg_test
