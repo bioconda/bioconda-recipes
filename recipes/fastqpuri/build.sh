51aea3cf5862651fd0834ac1d04067ae87bccc9e
Claudio Lottaz <claudio.lottaz@ur.de>
2019-03-27 09:48:03 +0100

    avoid not needed packages in R Changes to be committed: 	modified:   build.sh

--- recipes/fastqpuri/build.sh
+++ recipes/fastqpuri/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Rscript -e 'install.packages(c("digest", "htmltools", "rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
+Rscript -e 'install.packages(c("rmarkdown", "pheatmap"), repos="https://stat.ethz.ch/CRAN")'
 cmake -H. -Bbuild -DRSCRIPT=${PREFIX}/bin/Rscript -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=${CC}
 cd build
 make
