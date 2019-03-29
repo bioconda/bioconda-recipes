7fcb8884c4beb1fd747051668f983474f6f2b096
Michael Knudsen <micknudsen@gmail.com>
2019-03-29 10:36:56 +0100

    Starting all over...

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-cd ..
-
-rm -fr /opt/conda/conda-bld/svaba_1553545215578/work
-git clone --recursive https://github.com/walaj/svaba.git work
-
-cd work
-
-git checkout 1.1.0
-git submodule update --recursive
-
-export C_INCLUDE_PATH=$PREFIX/include
-export LIBRARY_PATH=$PREFIX/lib
-
 ./configure --prefix=$PREFIX
 make
 make install
