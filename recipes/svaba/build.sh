cdfd688bb706cacd37722a5effdf4c528f676da8
Michael Knudsen <micknudsen@gmail.com>
2019-03-29 10:59:38 +0100

    Clone SeqLib dependency

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm -fr SeqLib
+git clone --recursive https://github.com/walaj/SeqLib.git
+cd SeqLib
+git checkout d12cf224f7a488b913eabbcf54a215e17238032c
+git submodule update --recursive
+cd ..
+
 ./configure --prefix=$PREFIX
 make
 make install
