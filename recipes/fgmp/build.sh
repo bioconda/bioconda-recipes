aa832f587c63c139002568b6f7be35c1d30e2bb8
chapmanb <chapmanb@50mail.com>
2019-03-30 19:00:06 -0400

    Link fgmp into standard prefix bin directory

--- recipes/fgmp/build.sh
+++ recipes/fgmp/build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-FGMP_HOME=$PREFIX/opt/fgmp-${PKG_VERSION}
+FGMP_HOME="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 mkdir -p $FGMP_HOME
+mkdir -p ${PREFIX}/bin
 
 # why not use rsync?
 cp -R $SRC_DIR/src ${FGMP_HOME}
 cp -R $SRC_DIR/utils ${FGMP_HOME}
 cp $SRC_DIR/README.md ${FGMP_HOME}
 cp -R $SRC_DIR/data ${FGMP_HOME}
+# fgmp.config not in source directory
 # cp $SRC_DIR/fgmp.config ${FGMP_HOME}
 cp -R $SRC_DIR/lib ${FGMP_HOME}
 cp -R $SRC_DIR/sample ${FGMP_HOME}
 cp -R $SRC_DIR/sample_output ${FGMP_HOME}
 
-chmod 755 -R ${FGMP_HOME}/src ${FGMP_HOME}/utils ${FGMP_HOME}/lib ${FGMP_HOME}/bin
+chmod 755 -R ${FGMP_HOME}/src ${FGMP_HOME}/utils ${FGMP_HOME}/lib
 
-ln -s ${FGMP_HOME}/src/fgmp.pl ${FGMP_HOME}/fgmp
+ln -s ${FGMP_HOME}/src/fgmp.pl ${PREFIX}/bin/fgmp
