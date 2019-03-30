2079525d4be06adb22f4bcb6aa2621b31aac8690
Björn Grüning <bjoern@gruenings.eu>
2019-03-30 23:03:04 +0100

    Update build.sh

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -5,6 +5,8 @@ git clone --recursive https://github.com/walaj/SeqLib.git
 
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 
 ./configure CXXFLAGS=-fPIC --prefix=$PREFIX
 make
