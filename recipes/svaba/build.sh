d3d93a36e5d39a0e97fff04930d1122e68a830d4
Michael Knudsen <micknudsen@gmail.com>
2019-03-30 19:04:21 +0100

    Use latest version of SeqLib

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -2,10 +2,6 @@
 
 rm -fr SeqLib
 git clone --recursive https://github.com/walaj/SeqLib.git
-cd SeqLib
-git checkout f7a89a127409a3f52fdf725fa74e5438c68e48fb
-git submodule update --recursive
-cd ..
 
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
