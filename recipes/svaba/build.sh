4c207a924c21d7992fcce8ad1210d541940bbb29
Michael Knudsen <micknudsen@gmail.com>
2019-03-30 18:32:08 +0100

    CXXFLAGS=-fPIC

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -10,6 +10,6 @@ cd ..
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
 
-./configure --prefix=$PREFIX
-make -fPIC
+./configure CXXFLAGS=-fPIC --prefix=$PREFIX
+make
 make install
