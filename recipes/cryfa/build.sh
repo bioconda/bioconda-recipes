b26fb7b8e34f8994a2129a4e185000d1872cd37d
mori <s_morteza_h@yahoo.com>
2019-03-31 19:27:39 +0100

    update

--- recipes/cryfa/build.sh
+++ recipes/cryfa/build.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
-sudo apt-get install cmake
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
+export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+
+apt-get install cmake
 cmake .
-make -j8
+make -j8 VERBOSE=1
