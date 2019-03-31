d5c0091267c1cd9b38195bcd81c2f0e31665160a
Michael Knudsen <micknudsen@gmail.com>
2019-03-31 15:08:21 +0200

    Yet another hack

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -4,6 +4,7 @@ rm -fr SeqLib
 git clone --recursive https://github.com/walaj/SeqLib.git
 
 ln -s $PREFIX/include/zlib.h SeqLib/src/
+ln -s $PREFIX/include/zconf.h SeqLib/src/
 
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
