b3c48708fa3d68080c8e8e6778e5e519d59a4f09
Michael Knudsen <micknudsen@gmail.com>
2019-03-31 14:33:12 +0200

    Hack

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -3,10 +3,10 @@
 rm -fr SeqLib
 git clone --recursive https://github.com/walaj/SeqLib.git
 
+ln -s $PREFIX/include/zlib.h SeqLib/bwa/
+
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
