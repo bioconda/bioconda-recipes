cfe6ed285bacf646b6fb04f94a0ee7ed218c32bc
Michael Knudsen <micknudsen@gmail.com>
2019-04-01 13:07:03 +0200

    Make zlib.h available in all subfolders

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -3,6 +3,8 @@
 rm -fr SeqLib
 git clone --recursive https://github.com/walaj/SeqLib.git
 
+for FOLDER in $(find . -type d); do ln -fs $PREFIX/include/zlib.h $FOLDER/zlib.h; done
+
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
 
