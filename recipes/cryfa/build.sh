dc2fae5d256b3311ef66cb8c35b2215a7949573f
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-04-02 09:57:54 +0100

    update

--- recipes/cryfa/build.sh
+++ recipes/cryfa/build.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
-#export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 #export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
-#export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
 apt-get install cmake
 cmake .
-make -j8 VERBOSE=1
+make -j8
+
+mkdir -p "$PREFIX/bin"
+cp cryfa $PREFIX/bin/
