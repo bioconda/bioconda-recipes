761471211e06796f69f45891cf6b6a301c2d9986
Vitor C. Piro <vitorpiro@gmail.com>
2019-04-03 13:01:25 +0000

    working with submodule libs

--- recipes/ganon/build.sh
+++ recipes/ganon/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 rm -r libs/*
-
 git init
-
+# add submodule for sdsl-lite 3 and specific seqan branch
 git submodule add https://github.com/xxsds/sdsl-lite libs/sdsl-lite
 git submodule add -b SeqAn https://github.com/eseiler/seqan libs/seqan
-#git submodule add https://github.com/jarro2783/cxxopts.git libs/cxxopts
+# those libs are coming from the system
+git submodule add https://github.com/jarro2783/cxxopts.git libs/cxxopts
 git submodule add https://github.com/catchorg/Catch2.git libs/Catch2
+
+set -x -e
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DGANON_OFFSET=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DGANON_OFFSET=OFF -DUSE_MODULES=OFF ..
 make
 
 ctest -VV .
