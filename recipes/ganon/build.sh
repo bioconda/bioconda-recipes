bf34a2266f5046369d233b59e2f0e9170bb2939f
Vitor C. Piro <vitorpiro@gmail.com>
2019-04-03 09:44:21 +0000

    fix dependencies

--- recipes/ganon/build.sh
+++ recipes/ganon/build.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 rm -r libs/*
-git init
-git config -f .gitmodules --get-regexp '^submodule\..*\.path$' | 
-  while read path_key path; do
-    url=$(git config -f .gitmodules --get "$(echo $path_key | sed 's/\.path/.url/')")
-    branch=$(git config -f .gitmodules --get "$(echo $path_key | sed 's/\.path/.branch/')")
-    if [[ ! -z $branch ]]; then branch="-b ${branch}"; fi
-    git submodule add $branch $url $path
-  done
 
+git init
+
+git submodule add https://github.com/xxsds/sdsl-lite libs/sdsl-lite
+git submodule add -b SeqAn https://github.com/eseiler/seqan libs/seqan
+#git submodule add https://github.com/jarro2783/cxxopts.git libs/cxxopts
+git submodule add https://github.com/catchorg/Catch2.git libs/Catch2
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DGANON_OFFSET=OFF ..
 make
 
 ctest -VV .
 
 mkdir -p ${PREFIX}/bin
-cp ganon-build ganon-classify $PREFIX/bin/
+cp ../ganon ganon-build ganon-classify ${PREFIX}/bin/
+
