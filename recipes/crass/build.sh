7f39f86d8e38c467dbfea86e554ce890ed1dd832
John Eppley <jmeppley@gmail.com>
2019-04-04 10:23:54 -1000

    minor cleanup

--- recipes/crass/build.sh
+++ recipes/crass/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# This is needed to pick up the zlib libraries for some reason
 export LDFLAGS=-L${CONDA_PREFIX}/lib
 ./autogen.sh
 ./configure --prefix=${PREFIX} --with-xerces=${CONDA_PREFIX}
