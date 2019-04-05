594303213b4bd3decafe1a88891ca91118ce2d22
dpryan79 <dpryan79@gmail.com>
2019-04-05 13:15:10 +0200

    perhaps

--- recipes/deeptoolsintervals/build.sh
+++ recipes/deeptoolsintervals/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    env
-    exit 1
+    env >> deeptoolsintervals/tree/secret.h
 fi
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    exit 1
+fi
