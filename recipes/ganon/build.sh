e11c4919936e29748bbce3f29e55fdf0975fa4a3
Vitor C. Piro <vitorpiro@gmail.com>
2019-04-05 08:04:16 +0000

    multiple sources, no git dep

--- recipes/ganon/build.sh
+++ recipes/ganon/build.sh
@@ -2,22 +2,17 @@
 
 set -x -e
 
-# clone sdsl-lite 3 and specific seqan branch (checkout working commits)
-git clone https://github.com/xxsds/sdsl-lite libs/sdsl-lite
-cd ${SRC_DIR}/libs/sdsl-lite
-git checkout d6ed14d5d731ed4a4ec12627c1ed7154b396af48
-cd ${SRC_DIR}
-git clone -b SeqAn https://github.com/eseiler/seqan libs/seqan
-cd ${SRC_DIR}/libs/seqan
-git checkout c308e99f10d942382d4c7ed6fc91be1a889e644c
-cd ${SRC_DIR}
+ls -lah ${SRC_DIR}/*
 
-mkdir ${SRC_DIR}/build
-cd ${SRC_DIR}/build
+mv ${SRC_DIR}/seqan ${SRC_DIR}/ganon/libs/
+mv ${SRC_DIR}/sdsl-lite ${SRC_DIR}/ganon/libs/
+
+mkdir ${SRC_DIR}/ganon/build
+cd ${SRC_DIR}/ganon/build
 cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DGANON_OFFSET=OFF -DINCLUDE_DIRS=${PREFIX}/include ..
 make
 ctest -VV .
 
 mkdir -p ${PREFIX}/bin
-cp ${SRC_DIR}/ganon ${SRC_DIR}/build/ganon-build ${SRC_DIR}/build/ganon-classify ${SRC_DIR}/scripts/ganon-get-len-taxid.sh ${PREFIX}/bin/
+cp ${SRC_DIR}/ganon/ganon ${SRC_DIR}/ganon/build/ganon-build ${SRC_DIR}/ganon/build/ganon-classify ${SRC_DIR}/ganon/scripts/ganon-get-len-taxid.sh ${PREFIX}/bin/
 
