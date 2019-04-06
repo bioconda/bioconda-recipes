dd9396dff12af96e67567ca2a5f510ba8bae60b3
Devon Ryan <dpryan79@gmail.com>
2019-04-06 23:35:45 +0200

    patch lumpy, use htslib as a dependency

--- recipes/lumpy-sv/build.sh
+++ recipes/lumpy-sv/build.sh
@@ -6,12 +6,11 @@ set -eu -o pipefail
 export C_INCLUDE_PATH="${PREFIX}/include"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+export ZLIB_PATH="${PREFIX}/lib/"
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $outdir/scripts
-mkdir -p $outdir/scripts/bamkit
 mkdir -p $PREFIX/bin
 
 make \
@@ -23,19 +22,15 @@ make \
     LDFLAGS="${LDFLAGS}" \
     ZLIB_PATH="${PREFIX/lib}"
 
-cp bin/lumpy $PREFIX/bin
-cp bin/lumpy_filter $PREFIX/bin
+cp bin/* $PREFIX/bin
 cp scripts/lumpyexpress $PREFIX/bin
-
 cp scripts/cnvanator_to_bedpes.py $PREFIX/bin
-
 
 cp scripts/*.py $outdir/scripts
 cp scripts/*.sh $outdir/scripts
 cp scripts/*.pl $outdir/scripts
 cp scripts/extractSplitReads* $outdir/scripts
 cp scripts/vcf* $outdir/scripts
-cp scripts/bamkit/* $outdir/scripts/bamkit
 
 cp $RECIPE_DIR/lumpyexpress.config $outdir
 ln -s $outdir/lumpyexpress.config $PREFIX/bin
