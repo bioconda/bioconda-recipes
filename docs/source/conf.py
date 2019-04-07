75c45da08ca7fc966afeb30ef1e3041ce164df0b
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-04-07 13:50:15 -0600

    Docs: document celery tasks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,8 @@ extensions = [
 
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinxcontrib.napoleon'
+    'sphinxcontrib.napoleon',
+    'celery.contrib.sphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
