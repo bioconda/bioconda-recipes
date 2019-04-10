05c29532d1dc84b12dc2d73758daed977e7b8a5b
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-04-10 10:59:19 -0600

    Githandler: honor git insteaOf in get_remote() (used by Circle)

--- bioconda_utils/githandler.py
+++ bioconda_utils/githandler.py
@@ -71,12 +71,29 @@ class GitHandlerBase():
 
     def get_remote(self, desc: str):
         """Finds first remote containing **desc** in one of its URLs"""
+        # try if desc is the name
         if desc in [r.name for r in self.repo.remotes]:
             return self.repo.remotes[desc]
+
+        # perhaps it's an URL. If so, first apply insteadOf config
+        with self.repo.config_reader() as reader:
+            for section in reader.sections():
+                if section.startswith("url "):
+                    new = section.lstrip("url ").strip('"')
+                    try:
+                        old = reader.get(section, 'insteadOf')
+                        desc = desc.replace(old, new)
+                    except KeyError:
+                        pass
+        # now try if any remote matches the url
         remotes = [r for r in self.repo.remotes
                    if any(filter(lambda x: desc in x, r.urls))]
+
         if not remotes:
             raise KeyError(f"No remote matching '{desc}' found")
+        if len(remotes) > 1:
+            logger.warning("Multiple remotes found. Using first")
+
         return remotes[0]
 
     async def branch_is_current(self, branch, path: str, master="master") -> bool:
