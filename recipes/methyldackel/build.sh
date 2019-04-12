93e6c8dca31ddd3e195e31a1c1c76ccf54bed9a2
Devon Ryan <dpryan79@gmail.com>
2019-04-12 21:51:47 +0200

    See how OSX is done

--- recipes/methyldackel/build.sh
+++ recipes/methyldackel/build.sh
@@ -4,3 +4,6 @@ cd htslib && make CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pth
 export LIBRARY_PATH="$PREFIX/lib"
 export LIBRARY_PATH="$PREFIX/lib"
 make install CC=$CC OPTS="-I$PREFIX/include -L$PREFIX/lib -Wall -g -O3 -pthread" prefix=$PREFIX/bin
+if [[ $OSTYPE == "darwin"* ]]; then
+    exit 1
+fi
