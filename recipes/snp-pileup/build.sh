bb2553a0c4e2ed4300c630211fb23000e4cf0543
dpryan79 <dpryan79@gmail.com>
2019-04-16 13:29:45 +0200

    Update build command and dependencies

--- recipes/snp-pileup/build.sh
+++ recipes/snp-pileup/build.sh
@@ -2,9 +2,7 @@
 mkdir -p $PREFIX/bin
 
 cd inst/extcode/
-${CXX} -std=c++11 -I$PREFIX/include -I `pwd`/htslib/include snp-pileup.cpp \
-    -L$PREFIX/lib -L `pwd`/htslib/lib \
-    -lhts -o snp-pileup -lcurl -lz -lpthread -lcrypto -llzma -lbz2
+${CXX} -std=c++11 -I$PREFIX/include -L$PREFIX/lib snp-pileup.cpp -lhts -o snp-pileup
 
 ./snp-pileup --help
 cp snp-pileup $PREFIX/bin/
