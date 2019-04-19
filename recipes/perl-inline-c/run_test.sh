c6936db0140bf81e0e89c95bd65e178b3fd24b68
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-04-19 12:26:19 +0200

    explain what's happening

--- recipes/perl-inline-c/run_test.sh
+++ recipes/perl-inline-c/run_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
+# We run it from run_test.pl as if written in meta.yaml, the hack from
+# https://github.com/conda-forge/perl-feedstock/pull/33 doesn't work
+# (ie $ENV{'_'} returns nothing)
+
 export PERL_INLINE_DIRECTORY=/tmp/ &&  \
-env && \
-ls -l /usr/local/bin && \
-perl -e 'print $ENV{"_"};' && \
 perl -e 'use Inline C=>q{void greet(){printf("Hello, world\n");}};greet'
