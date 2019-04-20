fb9e4d9057c3230f529821977ed74aff8e781d87
Bjoern Gruening <bjoern.gruening@gmail.com>
2019-04-20 09:39:46 +0200

    add cesm recipe

---
+++ recipes/cesm/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sed -i.bak "s/'checkout'/'checkout', '--trust-server-cert'/" ./manage_externals/manic/repository_svn.py
+./manage_externals/checkout_externals
+
+mkdir -p ${PREFIX}/bin
+cp -r cime ${PREFIX}/
+cp -r cime_config ${PREFIX}/
+cp -r components ${PREFIX}/
+
+cd ${PREFIX}/bin/
+ln -s ../cime/scripts/create_* .
+ln -s ../cime/scripts/query_* .
+
