9785b5116e7d626b59b098f3580443f85d33ebd3
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-04-22 16:43:38 -0600

    Update build.sh

--- recipes/rapsearch/build.sh
+++ recipes/rapsearch/build.sh
@@ -4,7 +4,7 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
-make -C Src/
+make -C Src/ CXX=$CXX CXXFLAGS=$CXXFLAGS INC="-I$PREFIX/include -I." LIBS=$PREFIX/lib
 # copy binaries from the bin folder in the
 # rapsearch archive to the conda bin folder
 mv bin/rapsearch ${PREFIX}/bin/
