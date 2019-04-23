8541729d798d7a373c6e5f8045c4170a3725b454
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-04-23 13:34:32 -0600

    Update build.sh

--- recipes/raxml/build.sh
+++ recipes/raxml/build.sh
@@ -9,7 +9,7 @@ esac
 mkdir -p $PREFIX/bin
 
 for PTHREADS in "" .PTHREADS; do
-  for OPT in "" .SSE3 AVX2; do
+  for OPT in "" .SSE3 .AVX2; do
     echo "######## Building Flags opt=$OPT pthread=$PTHREADS os=$SUF ######"
     make -f Makefile${OPT}${PTHREADS}${SUF} CC=$CC
     mv raxmlHPC* $PREFIX/bin
