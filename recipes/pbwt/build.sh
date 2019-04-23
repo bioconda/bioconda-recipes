799951fc00a1599dcb4212ef23540689b90da633
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-04-23 16:09:34 -0400

    no cd

--- recipes/pbwt/build.sh
+++ recipes/pbwt/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd pbwt-64c4ffa7880db5f85b53ccbd7e530ec4b95ea4ba
+#cd pbwt-64c4ffa7880db5f85b53ccbd7e530ec4b95ea4ba
+ls
 sed -i.bak 's/$(HTSLIB)/-lhts/' Makefile
 make
 cp pbwt $PREFIX/bin
