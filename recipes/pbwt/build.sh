a67cb40562d1da0aaa84807df1c72431020eae2a
Tiago Rodrigues Antao <tiagoantao@gmail.com>
2019-04-23 17:01:48 -0400

    zlib

--- recipes/pbwt/build.sh
+++ recipes/pbwt/build.sh
@@ -3,7 +3,7 @@
 #cd pbwt-64c4ffa7880db5f85b53ccbd7e530ec4b95ea4ba
 ls
 sed -i.bak 's/$(HTSLIB)/-lhts/' Makefile
-sed -i.bak 's/\"zlib.h"/\<zlib.h\>/' Makefile
+sed -i.bak 's/\"zlib.h"/\<zlib.h\>/' pbwtPaint.c utils.h
 make
 cp pbwt $PREFIX/bin
 
