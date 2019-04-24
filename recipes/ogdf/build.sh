d5324520f30266eac89e2c018d6d4d15604f6939
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-04-23 18:43:34 -0600

    Update build.sh

--- recipes/ogdf/build.sh
+++ recipes/ogdf/build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-chmod +x makeMakefile.sh
-./makeMakefile.sh
-make
-mkdir ${PREFIX}/ogdf
-mkdir ${PREFIX}/ogdf/lib
-mkdir ${PREFIX}/ogdf/include
-cp _release/libOGDF.a ${PREFIX}/ogdf/lib/
-cp -R ogdf ${PREFIX}/ogdf/include/
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX .
+make -j$CPU_COUNT
+make install
