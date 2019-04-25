aa7c552c36fbf851497e0dba5051a03ca45e0bc1
Paul Klemm <paulklemm@t-online.de>
2019-04-25 23:30:09 +0200

    proteinortho

--- recipes/proteinortho/build.sh
+++ recipes/proteinortho/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-make -j${CPU_COUNT}
+echo "build start"
+
+export C_INCLUDE_PATH="${PREFIX}/include"
+export CPP_INCLUDE_PATH="${PREFIX}/include"
+export CXX_INCLUDE_PATH="${PREFIX}/include"
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+
+ln -s $BUILD_PREFIX/bin/*g++ g++
+export PATH=$PWD:$PATH
+
+make clean
+make all -j${CPU_COUNT}
+mkdir -p $PREFIX/bin
 make install PREFIX=$PREFIX/bin/
