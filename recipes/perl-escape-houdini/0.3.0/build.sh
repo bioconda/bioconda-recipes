84b8d65f21ae6182e1160556434325db02ffc6bc
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-04-25 15:50:33 +0200

    more logs?

--- recipes/perl-escape-houdini/0.3.0/build.sh
+++ recipes/perl-escape-houdini/0.3.0/build.sh
@@ -2,7 +2,7 @@
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
-    perl Build.PL
+    perl Build.PL --verbose
     perl ./Build
     perl ./Build test
     # Make sure this goes in site
