a2f769502d211b7966a104abdebc94f8d2823eb8
Alexander Peltzer <alex.peltzer@gmail.com>
2019-04-26 13:08:42 +0200

    Update allelecount to 4.0.0

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -3,13 +3,14 @@ set -eu
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-1.2.1.tar.bz2 https://github.com/samtools/htslib/releases/download/1.2.1/htslib-1.2.1.tar.bz2
-tar -xjvpf htslib-1.2.1.tar.bz2
-cd htslib-1.2.1
-patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
+export HTSLIB_VERSION=1.7
+wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
+tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
+cd htslib-${HTSLIB_VERSION}
+#patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
 make
 cd ../c
 mkdir bin
-make OPTINC=-I$SRC_DIR/htslib-1.2.1 HTSLOC=$SRC_DIR/htslib-1.2.1
+make OPTINC=-I$SRC_DIR/htslib-${HTSLIB_VERSION} HTSLOC=$SRC_DIR/htslib-${HTSLIB_VERSION}
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
