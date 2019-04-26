d3a166a2603bdee7e13ebfc3260a1ba5dae30f50
dpryan79 <dpryan79@gmail.com>
2019-04-26 11:09:24 +0200

    works locally

--- recipes/r-fateid/build.sh
+++ recipes/r-fateid/build.sh
@@ -1,5 +1,2 @@
-LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64
-export DISABLE_AUTOBREW=1
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-$R CMD INSTALL --build .
+#!/bin/bash
+LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64 $R CMD INSTALL --build .
