03431dddbe86fbee3c5ff98b70d0f093b3e180e2
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2019-04-26 17:51:37 +0200

    add bam2wig.sh

--- recipes/jvarkit-bam2wig/build.sh
+++ recipes/jvarkit-bam2wig/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
-cd jvarkit
 ./gradlew bam2wig
 
+OUT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
+mkdir -p $OUT
+mkdir -p $PREFIX/bin
+cp dist/bam2wig.jar $OUT/
+
+cp $RECIPE_DIR/bam2wig.sh $OUT/bam2wig.sh
+chmod +x $OUT/bam2wig.sh
+ln -s $OUT/bam2wig.sh $PREFIX/bin
