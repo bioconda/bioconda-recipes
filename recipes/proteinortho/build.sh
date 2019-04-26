c37aada39a2eb2be6d563654ba3c0b085e91365a
Paul Klemm <paulklemm@t-online.de>
2019-04-26 18:36:06 +0200

    proteinortho

--- recipes/proteinortho/build.sh
+++ recipes/proteinortho/build.sh
@@ -8,9 +8,9 @@ export CXX_INCLUDE_PATH="${PREFIX}/include"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
-# ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-g++ g++
+#ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-g++ g++
 # #ln -s $GCC gcc
-# export PATH=$PWD:$PATH
+#export PATH=$PWD:$PATH
 # export PATH=$PREFIX:$PATH
 # export PATH=$PREFIX/bin:$PATH
 # ls -las $LIBRARY_PATH
