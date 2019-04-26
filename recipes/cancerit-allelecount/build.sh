ea638153cf0df4af82d44f5293601723be2bedbb
Alexander Peltzer <alex.peltzer@gmail.com>
2019-04-26 13:23:23 +0200

    Stay with 4.0.0 :-)

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -3,11 +3,11 @@ set -eu
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export HTSLIB_VERSION=1.2.1
+export HTSLIB_VERSION=1.7
 wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
 tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
 cd htslib-${HTSLIB_VERSION}
-patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
+#patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
 make
 cd ../c
 mkdir bin
