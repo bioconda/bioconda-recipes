90479002f3ca9013e01e9adb9d162d695ba91e3e
Devon Ryan <dpryan79@users.noreply.github.com>
2019-04-29 20:51:26 +0200

    zlib

--- recipes/dropest/build.sh
+++ recipes/dropest/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=$CXX -DBOOST_ROOT=$PREFIX -DR_ROOT=$PREFIX .. && make DESTDIR=$PREFIX
+cmake -DCMAKE_CXX_COMPILER=$CXX -DBOOST_ROOT=$PREFIX -DR_ROOT=$PREFIX -DZLIB_ROOT=$PREFIX .. && make DESTDIR=$PREFIX
 make install
 
 cd ../dropestr
