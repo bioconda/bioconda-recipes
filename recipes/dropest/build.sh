5943bc3d2888e2489d0b1ea3e1ee6c53096608bf
Devon Ryan <dpryan79@gmail.com>
2019-04-29 23:25:13 +0200

    skip on osx

--- recipes/dropest/build.sh
+++ recipes/dropest/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# This is only needed on OSX, where we're pulling in cmake 3.14 at the moment
-sed -i.bak '6icmake_policy(SET CMP0074 NEW)' CMakeLists.txt
-
 mkdir build
 cd build
 cmake -DCMAKE_CXX_COMPILER=$CXX -DBOOST_ROOT=$PREFIX -DR_ROOT=$PREFIX -DZLIB_ROOT=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX .. && make
