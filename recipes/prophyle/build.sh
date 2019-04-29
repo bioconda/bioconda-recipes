749afb9c2891eb138cb199e23da0666fcff233f9
dpryan79 <dpryan79@gmail.com>
2019-04-29 15:39:00 +0200

    Maybe LD_LIBRARY_PATH will do the trick

--- recipes/prophyle/build.sh
+++ recipes/prophyle/build.sh
@@ -6,5 +6,6 @@ set -o pipefail
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
 make -C prophyle
 PROPHYLE_PACKBIN=1 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
