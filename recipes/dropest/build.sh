2e8c57dca9d5feb0c1ce271bed64bcc371a16434
dpryan79 <dpryan79@gmail.com>
2019-04-29 15:33:47 +0200

    add dropEst

---
+++ recipes/dropest/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir build
+cd build
+cmake -DCMAKE_CXX_COMPILER=$CXX -DBOOST_ROOT=$PREFIX -DR_ROOT=$PREFIX .. && make DESTDIR=$PREFIX
+make install
+
+cd ../dropestr
+R CMD INSTALL --build .
