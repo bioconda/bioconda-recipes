4e75969a5da04beb75cddd92b9b7931fb4d8106f
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-04-29 01:31:43 -0500

    Add extra flags

--- recipes/defiant/build.sh
+++ recipes/defiant/build.sh
@@ -14,9 +14,9 @@ if [ `uname` == Darwin ]; then
 fi
 
 # Compile binaries
-${CC} -O4 -o defiant defiant.c -Wall -pedantic -std=gnu11 -lm -fopenmp -I${PREFIX}/include
+${CC} -O4 -o defiant defiant.c -Wall -pedantic -std=gnu11 -lm -fopenmp -I${PREFIX}/include $CFLAGS $LDFLAGS 
 mv regions_of_interest regions_of_interest.c
-${CC} -o roi regions_of_interest.c -lm -Wall -std=gnu11 -Wextra -pedantic -Wconversion
+${CC} -o roi regions_of_interest.c -lm -Wall -std=gnu11 -Wextra -pedantic -Wconversion $CFLAGS $LDFLAGS
 
 # Copy binaries
 cp defiant ${PREFIX}/bin/defiant
