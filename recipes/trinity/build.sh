5a4a3689d2f24a3b443d8d6441390fd81b344ead
Devon Ryan <dpryan79@gmail.com>
2019-04-29 23:01:59 +0200

    Remove hardcoded compilers

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -2,8 +2,6 @@
 
 set -x -e
 
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
