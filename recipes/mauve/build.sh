db8066a744f6d07183d072574c3e9de9458cccf5
Nolan Woods <nolan_w@sfu.ca>
2019-04-30 13:58:40 -0700

    Add wrapper for Mauve Contig Mover

--- recipes/mauve/build.sh
+++ recipes/mauve/build.sh
@@ -7,5 +7,7 @@ cp Mauve.jar ${mauve_dir}/Mauve.jar
 mkdir -v ${mauve_dir}/ext
 cp -R ext/* ${mauve_dir}/ext/
 cp  linux-x64/Mauve ${mauve_dir}/Mauve
+cp $RECIPE_DIR/MauveCM ${mauve_dir}/MauveCM
 ln -s ${mauve_dir}/Mauve $PREFIX/bin/Mauve
+ln -s ${mauve_dir}/MauveCM $PREFIX/bin/MauveCM
 
