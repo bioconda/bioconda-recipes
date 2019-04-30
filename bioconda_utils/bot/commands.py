5de5becbb9780853a930cb7dc072d0b3197d66f6
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-04-29 19:12:39 -0600

    Bot: add permission system for commands

--- bioconda_utils/bot/commands.py
+++ bioconda_utils/bot/commands.py
@@ -3,7 +3,8 @@ Handlers for user commmands (``@biocondabot do this``)
 """
 
 import logging
-from typing import Callable, Dict
+import functools
+from typing import Callable, Dict, Optional
 
 from .tasks import (
     bump, create_check_run, get_latest_pr_commit, check_circle_artifacts,
@@ -45,6 +46,49 @@ class CommandDispatch:
 command_routes = CommandDispatch()  # pylint: disable=invalid-name
 
 
+def permissions(member: Optional[bool] = None,
+                author: Optional[bool] = None):
+    """Decorator for commands checking for permissions
+
+    Permissions are OR combined. Repeat the decorator to get AND.
+
+    Args:
+      member: if true, user must be organization member
+      author: if true, user must be author, if false user must not be author
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(ghapi, issue_number, user, *args):
+            ok = ""
+            err = []
+            if not ok and member is not None:
+                if await ghapi.is_member(user):
+                    ok = "User is member"
+                elif not await ghapi.is_org():
+                    ok = "Repo is not on Org"
+                else:
+                    err += [f"a member of {ghapi.user.capitalize()}"]
+            if not ok and author is not None:
+                pr = await ghapi.get_prs(number=issue_number)
+                pr_author = pr['user']['login']
+                negate = "" if author else "not "
+                if (pr_author == user) == author:
+                    ok = f"User is {negate}PR author"
+                else:
+                    err += [f"{negate}author of the PR"]
+
+            msg = ok or f"Permission denied. You need to be {' or '.join(err)}."
+
+            logger.warning("Access %s: %s wants to run %s on %s#%s ('%s')",
+                           "GRANTED" if ok else "DENIED",
+                           user, func.__name__, ghapi, issue_number, msg)
+            if ok:
+                return await func(ghapi, issue_number, user, *args)
+            return msg
+        return wrapper
+    return decorator
+
+
 @command_routes.register("hello")
 async def command_hello(ghapi, issue_number, user, *_args):
     """Simple demo function answering to hello"""
@@ -56,6 +100,7 @@ async def command_hello(ghapi, issue_number, user, *_args):
 
 
 @command_routes.register("bump")
+@permissions(member=True, author=True)
 async def command_bump(ghapi, issue_number, _user, *_args):
     """Bump the build number of a recipe    """
     bump.apply_async((issue_number, ghapi))
@@ -86,6 +131,7 @@ async def command_recheck(ghapi, issue_number, _user, *_args):
 
 
 @command_routes.register("rebuild")
+@permissions(member=True, author=True)
 async def command_rebuild(ghapi, issue_number, _user, *_args):
     """Trigger rebuild of PR as of latest sha"""
     trigger_circle_rebuild.s(issue_number, ghapi).apply_async()
@@ -93,6 +139,8 @@ async def command_rebuild(ghapi, issue_number, _user, *_args):
 
 
 @command_routes.register("merge")
+@permissions(member=True)
+@permissions(author=False)
 async def command_merge(ghapi, issue_number, user, *_args):
     """Merge PR"""
     (
