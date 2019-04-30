c069a908517525445836177972f90d522a2aa558
Devon Ryan <dpryan79@gmail.com>
2019-04-30 21:06:18 +0200

    more compiler injection

--- recipes/vt/build.sh
+++ recipes/vt/build.sh
@@ -9,6 +9,9 @@ export CPATH=${PREFIX}/include
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
+#Inject compilers
+sed -i.bak "s#gcc#${CC}#g" lib/pcre2/Makefile
+
 make CXX=$CXX CC=$CC -j${CPU_COUNT}
 make test
 mkdir -p $PREFIX/bin
