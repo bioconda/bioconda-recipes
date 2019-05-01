045ee8c7c812e7b3eb428cf57b4fd14eaa35dfa7
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-05-01 00:17:44 -0500

    Update recipe for defiant

--- recipes/defiant/build.sh
+++ recipes/defiant/build.sh
@@ -6,16 +6,14 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 mkdir -p ${PREFIX}/bin
 
-# Fix defiant.c for OSX
-if [ `uname` == Darwin ]; then
-    sed -i.bak 's/restrict argv\[\]/argv\[\]/g' defiant.c
-fi
-
 # Compile binaries
 ${CC} -O4 -o defiant defiant.c -Wall -pedantic -std=gnu11 -lm -fopenmp -I${PREFIX}/include $CFLAGS $LDFLAGS 
-mv regions_of_interest regions_of_interest.c
-${CC} -o roi regions_of_interest.c -lm -Wall -std=gnu11 -Wextra -pedantic -Wconversion $CFLAGS $LDFLAGS
+${CC} -o roi roi.c -lm -Wall -std=gnu11 -Wextra -pedantic -Wconversion $CFLAGS $LDFLAGS
 
 # Copy binaries
 cp defiant ${PREFIX}/bin/defiant
 cp roi ${PREFIX}/bin/roi
+
+# Copy perl script
+chmod +x plot_results.pl
+cp plot_results.pl ${PREFIX}/bin/plot_results.pl
