a2b06d4bfbbfa77fef7dc1831b326407cf70e02f
Alexander Peltzer <alex.peltzer@gmail.com>
2019-05-02 14:03:51 +0200

    Trying to fix recipe

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -3,18 +3,22 @@ set -eu
 
 export C_INCLUDE_PATH=${PREFIX}/include/htslib
 export LIBRARY_PATH=${PREFIX}/lib
-#export HTSLIB_VERSION=1.7
-#wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
-#tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
-#cd htslib-${HTSLIB_VERSION}
+export INCPATH=${PREFIX}/include
+export HTSLIB_VERSION=1.7
+wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
+tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
+cd htslib-${HTSLIB_VERSION}
 #patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
-#make
-#cd ../c
+./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
+make install 
+cd ../c
 cd $SRC_DIR/c
 mkdir bin
 #Inject CC 
 sed -i 's/CC = gcc /compiler = ${CC} /g' Makefile
 sed -i 's/$(CC) /$(compiler) /g' Makefile
-make OPTINC=-I$C_INCLUDE_PATH HTSLOC=$C_INCLUDE_PATH
+#Get rid of -lhts
+sed -i 's/LIBS=-lhts -lpthread -lz -lbz2 -llzma -lm -ldl/LIBS= -lpthread -lz -lbz2 -llzma -lm -ldl/g' Makefile
+make OPTINC=-I$INCPATH HTSLOC=$C_INCLUDE_PATH
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
