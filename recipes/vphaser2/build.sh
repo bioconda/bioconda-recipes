47dae3b63ac40436fc8a8c3784c77884bfa4a4c4
dpryan79 <dpryan79@gmail.com>
2019-05-02 16:23:10 +0200

    D'oh

--- recipes/vphaser2/build.sh
+++ recipes/vphaser2/build.sh
@@ -4,6 +4,6 @@ mkdir -p $PREFIX/bin
 
 # This is all the makefile does and it's simpler to just recreat it than to patch things
 cd V-Phaser-2.0/src
-$CXX -fopenmp -O3 -I${PREFIX}/include/ -I${BUILD_PREFIX}/include -I${PREFIX}/include/bamtools -L${PREFIX}/lib *.cpp -o $PREFIX/variant_caller -lbamtools -lz -lpthread
+$CXX -fopenmp -O3 -I${PREFIX}/include/ -I${BUILD_PREFIX}/include -I${PREFIX}/include/bamtools -L${PREFIX}/lib *.cpp -o $PREFIX/bin/vphaser2 -lbamtools -lz -lpthread
 
-ln -s $PREFIX/bin/variant_caller $PREFIX/bin/vphaser2
+ln -s $PREFIX/bin/vphaser $PREFIX/bin/variant_caller
