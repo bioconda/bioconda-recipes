9d2cb5af1d959a9cba7e1237e6d99b307beb0a8b
Alexander Peltzer <alex.peltzer@gmail.com>
2019-05-02 14:10:22 +0200

    Bump to 4.0.2

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -4,21 +4,22 @@ set -eu
 export C_INCLUDE_PATH=${PREFIX}/include/htslib
 export LIBRARY_PATH=${PREFIX}/lib
 export INCPATH=${PREFIX}/include
-export HTSLIB_VERSION=1.7
-wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
-tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
-cd htslib-${HTSLIB_VERSION}
+export HTSLIB=${PREFIX}/include/htslib
+#export HTSLIB_VERSION=1.7
+#wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
+#tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
+#cd htslib-${HTSLIB_VERSION}
 #patch -p0 < $SRC_DIR/patches/htslibcramindex.diff
-./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
-make install 
-cd ../c
+#./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
+#make install 
+#cd ../c
 cd $SRC_DIR/c
 mkdir bin
 #Inject CC 
 sed -i 's/CC = gcc /compiler = ${CC} /g' Makefile
 sed -i 's/$(CC) /$(compiler) /g' Makefile
 #Get rid of -lhts
-sed -i 's/LIBS=-lhts -lpthread -lz -lbz2 -llzma -lm -ldl/LIBS= -lpthread -lz -lbz2 -llzma -lm -ldl/g' Makefile
-make OPTINC=-I$INCPATH HTSLOC=$C_INCLUDE_PATH
+#sed -i 's/LIBS=-lhts -lpthread -lz -lbz2 -llzma -lm -ldl/LIBS= -lpthread -lz -lbz2 -llzma -lm -ldl/g' Makefile
+make OPTINC=-I$INCPATH HTSLOC=$HTSLIB
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
