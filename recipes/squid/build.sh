b0543af8c27b67fb90f8e6521dc8e48c0ffd859f
Devon Ryan <dpryan79@users.noreply.github.com>
2019-05-02 19:01:14 +0200

    oops

--- recipes/squid/build.sh
+++ recipes/squid/build.sh
@@ -3,4 +3,4 @@ set -x -e
 mkdir -p "${PREFIX}/bin"
 INCLUDES="-I${PREFIX}/include -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/bamtools"
 LDADD="${BUILD_PREFIX}/lib/libbamtools.a ${BUILD_PREFIX}/lib/libglpk.a -L${PREFIX}/lib"
-${CXX} -std=c++11 ${CXXFLAGS} $INCLUDES -o $PREFIX/squid src/*.cpp $LDADD -lz -lm
+${CXX} -std=c++11 ${CXXFLAGS} $INCLUDES -o $PREFIX/bin/squid src/*.cpp $LDADD -lz -lm
