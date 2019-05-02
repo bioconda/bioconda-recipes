06ce664c351309c0bf52b66280a10d23eb558333
Alexander Peltzer <alex.peltzer@gmail.com>
2019-05-02 14:50:43 +0200

    Clean up recipes

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -eu
-
-export C_INCLUDE_PATH=${PREFIX}/include/htslib
 export LIBRARY_PATH=${PREFIX}/lib
 export INCPATH=${PREFIX}/include
-export HTSLIB=${PREFIX}/include/htslib
 #export HTSLIB_VERSION=1.7
 #wget --ca-certificate=$PREFIX/ssl/cert.pem -O htslib-${HTSLIB_VERSION}.tar.bz2 https://github.com/samtools/htslib/releases/download/${HTSLIB_VERSION}/htslib-${HTSLIB_VERSION}.tar.bz2
 #tar -xjvpf htslib-${HTSLIB_VERSION}.tar.bz2
@@ -20,6 +17,6 @@ sed -i 's/CC = gcc /compiler = ${CC} /g' Makefile
 sed -i 's/$(CC) /$(compiler) /g' Makefile
 #Get rid of -lhts
 #sed -i 's/LIBS=-lhts -lpthread -lz -lbz2 -llzma -lm -ldl/LIBS= -lpthread -lz -lbz2 -llzma -lm -ldl/g' Makefile
-make -I${PREFIX}/include -L${PREFIX}/lib
+make -I${INCPATH} -L${LIBRARY_PATH}
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
