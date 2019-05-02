871517193f890765e42ca64a4fbffb7f8842a337
Eric Roberts <eroberts@uhnresearch.ca>
2019-05-02 16:16:31 -0400

    Update timestamps on configure files for patched OSX GMTK build.

--- recipes/gmtk/build.sh
+++ recipes/gmtk/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# For OSX configuration files are patched and autoconf is not necessary
+touch -c configure.ac aclocal.m4 Makefile.am Makefile.in
 ./configure --with-logp=table --prefix=$PREFIX
 make
 make install
