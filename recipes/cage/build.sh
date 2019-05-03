4dd6e429bef31512413ef5d677537f3744be7384
Devon Ryan <dpryan79@gmail.com>
2019-05-03 23:36:32 +0200

    more bamtools updates

--- recipes/cage/build.sh
+++ recipes/cage/build.sh
@@ -4,7 +4,7 @@ sed -i.bak "s@\$(BAMTOOLS_PATH)lib@$PREFIX/lib@" bamdump_src/Makefile
 sed -i.bak "s@\$(BAMTOOLS_PATH)include@$PREFIX/include/bamtools@" bamdump_src/Makefile
 sed -i.bak "s@INCLUDES = @INCLUDES = -I$PREFIX/include @" cage_src/Makefile
 sed -i.bak "s@LFLAGS = @LFLAGS = -L$PREFIX/lib @" cage_src/Makefile
-make
+make CC=${CXX}
 
 mkdir -p $PREFIX/bin
 cp bin/cage $PREFIX/bin
