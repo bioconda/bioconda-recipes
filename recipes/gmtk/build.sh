1f4fe3d900bd34fb465aa0e21e6ab65fe367b896
Eric Roberts <eroberts@uhnresearch.ca>
2019-05-03 09:30:12 -0400

    Update tksrc configuration timestamps for GMTK OSX build

--- recipes/gmtk/build.sh
+++ recipes/gmtk/build.sh
@@ -3,6 +3,7 @@
 # For OSX configuration files that are patched so autoreconf is not necessary
 touch -c configure.ac aclocal.m4 Makefile.am Makefile.in
 touch -c miscSupport/configure.ac miscSupport/aclocal.m4 miscSupport/Makefile.am miscSupport/Makefile.in
+touch -c tksrc/configure.ac tksrc/aclocal.m4 tksrc/Makefile.am tksrc/Makefile.in
 ./configure --with-logp=table --prefix=$PREFIX
 make
 make install
