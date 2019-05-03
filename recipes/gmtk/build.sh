2e261eb7add7310acfdcfbbc5466e823c7479005
Eric Roberts <eroberts@uhnresearch.ca>
2019-05-03 09:00:08 -0400

    Update miscSupport configuration timestamps for GMTK OSX build

--- recipes/gmtk/build.sh
+++ recipes/gmtk/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# For OSX configuration files are patched and autoconf is not necessary
+# For OSX configuration files that are patched so autoreconf is not necessary
 touch -c configure.ac aclocal.m4 Makefile.am Makefile.in
+touch -c miscSupport/configure.ac miscSupport/aclocal.m4 miscSupport/Makefile.am miscSupport/Makefile.in
 ./configure --with-logp=table --prefix=$PREFIX
 make
 make install
