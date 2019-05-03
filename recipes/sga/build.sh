07b3d4388663a0dc585fcce7152b6f9884de4a1c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-05-03 02:01:30 -0500

    Rebuild sga with bamtools

--- recipes/sga/build.sh
+++ recipes/sga/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+export CXXFLAGS="-I${PREFIX}/include"
+
 pushd $SRC_DIR/src
+
 ./autogen.sh
-./configure --prefix=$PREFIX --with-bamtools=$PREFIX  && make && make install
+./configure --prefix=$PREFIX --with-bamtools=$PREFIX --with-sparsehash=$PREFIX
+make CC=${CC} CXX=${CXX}
+make install
