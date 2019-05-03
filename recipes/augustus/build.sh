11f7c963d0f664e9d9af69e8061d2779cfe5a20b
dpryan79 <dpryan79@gmail.com>
2019-05-03 15:03:08 +0200

    omg it builds locally

--- recipes/augustus/build.sh
+++ recipes/augustus/build.sh
@@ -16,12 +16,20 @@ export BAMTOOLS="${BUILD_PREFIX}"
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/scripts
 mkdir -p ${PREFIX}/config
+#mkdir bin
 
-# The Makefile doesn't propogate BAMTOOLS properly everywhere
-pushd auxprogs/bam2hints/
-${CXX} ${CXXFLAGS} -std=c++11 -DUSE_BOOST -I$BUILD_PREFIX/include/bamtools -DNDEBUG -D_FORTIFY_SOURCE=2 -c bam2hints.cc -o bam2hints.o
-${CXX} -std=c++11 -DUSE_BOOST -I$PREFIX/include -L$PREFIX/lib -I$BUILD_PREFIX/include/bamtools -Wall -O2 -o bam2hints bam2hints.o $BUILD_PREFIX/lib/libbamtools.a -lz
-popd
+# This has been corrected on github, but not in a release
+sed -i.bak "s#PREFIX#BUILD_PREFIX#g" auxprogs/bam2hints/Makefile
+sed -i.bak "s#PREFIX#BUILD_PREFIX#g" auxprogs/filterBam/src/Makefile
+cat auxprogs/filterBam/src/Makefile
+#pushd auxprogs/
+#pushd bam2hints/
+#make CXX="${CXX}" BAMTOOLS="${BUILD_PREFIX}" CFLAGS="${CXXFLAGS} -std=c++11 -DNDEBUG -D_FORTIFY_SOURCE=2"
+#popd
+#pushd filterBam/src/
+#make CXX="${CXX}" BAMTOOLS="${BUILD_PREFIX}" CFLAGS="${CXXFLAGS} -std=c++11 -DNDEBUG -D_FORTIFY_SOURCE=2"
+#popd
+#popd
 
 ## Make the software
 
