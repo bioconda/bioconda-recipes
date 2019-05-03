1def3d5ba6bb800e4b781fe28de6cc3861a95422
Alexander Peltzer <alex.peltzer@gmail.com>
2019-05-03 10:55:10 +0200

    Add changes to build recipe

--- recipes/tiddit/build.sh
+++ recipes/tiddit/build.sh
@@ -7,3 +7,11 @@ make
 cd ..
 cd src
 python setup.py install
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+cp -R * $outdir/ 
+ls -l $outdir
+ln -s $outdir/TIDDIT.py $PREFIX/bin/tiddit
+chmod 0755 ${PREFIX}/bin/tiddit
