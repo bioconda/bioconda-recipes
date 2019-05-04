596634033e13e0290f8dc9a5598403776677bc20
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-05-04 10:21:18 -0600

    Update build.sh

--- recipes/sga/build.sh
+++ recipes/sga/build.sh
@@ -9,5 +9,5 @@ pushd $SRC_DIR/src
 
 ./autogen.sh
 ./configure --prefix=$PREFIX --with-bamtools=$PREFIX --with-sparsehash=$PREFIX
-make CC=${CC} CXX=${CXX}
+make CC=${CC} CXX=${CXX} CXXFLAGS="$CXXFLAGS -O3" CFLAGS="$CFLAGS -O3"
 make install
