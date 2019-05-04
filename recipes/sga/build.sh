7c51a264062859037cdb3b3404e65c0634556501
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-05-04 11:42:26 -0600

    Update build.sh

--- recipes/sga/build.sh
+++ recipes/sga/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
+export CFLAGS="-I$PREFIX/include -O3"
 export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export CXXFLAGS="-I${PREFIX}/include"
+export CPATH="${PREFIX}/include"
+export CXXFLAGS="-I${PREFIX}/include -O3"
 
 pushd $SRC_DIR/src
 
 ./autogen.sh
 ./configure --prefix=$PREFIX --with-bamtools=$PREFIX --with-sparsehash=$PREFIX
-make CC=${CC} CXX=${CXX} CXXFLAGS="$CXXFLAGS -O3" CFLAGS="$CFLAGS -O3"
+make CC=${CC} CXX=${CXX} CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS"
 make install
