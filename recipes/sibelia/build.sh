681066844dfcc16b7beb685baf866c1c582cc76f
Devon Ryan <dpryan79@gmail.com>
2019-05-05 10:29:12 +0200

    this could be easier

--- recipes/sibelia/build.sh
+++ recipes/sibelia/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sed -i.bak "s#gcc#${CC}#g" src/lagan/src/Makefile
-
 cd build
 cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC}
-make
+make CC=${CC} CXX=${CXX}
 make install
