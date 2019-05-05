af0882d43eccf4575eadc5a8dd3829f8b75f6841
Devon Ryan <dpryan79@gmail.com>
2019-05-05 09:51:16 +0200

    inject compilers

--- recipes/sibelia/build.sh
+++ recipes/sibelia/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+sed -i.bak "s#gcc#${CC}#g" src/lagan/src/Makefile
 
 cd build
-cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC}
 make
 make install
