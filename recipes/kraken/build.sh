92f6746a0d7dfec40cfd56599736e4b2ac7bdb6d
Hadrien Gourlé <gourlehadrien@gmail.com>
2019-05-06 07:14:34 +0200

    omp

--- recipes/kraken/build.sh
+++ recipes/kraken/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
 mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
 
 # src/Makefile uses
