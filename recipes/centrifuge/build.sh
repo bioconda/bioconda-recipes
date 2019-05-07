405fabc8b355f4dfd123302345981eb6b730df47
Andreas Sjödin <andreas.sjodin@gmail.com>
2019-05-07 22:13:42 +0200

    Try to fix compiler error

--- recipes/centrifuge/build.sh
+++ recipes/centrifuge/build.sh
@@ -5,8 +5,8 @@ export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
 
-make
-make install prefix=$PREFIX
+make CC=$CC
+make install prefix=$PREFIX CC=$CC
 
 cp centrifuge-kreport $PREFIX/bin
 cp evaluation/{centrifuge_evaluate.py,centrifuge_simulate_reads.py} $PREFIX/bin
