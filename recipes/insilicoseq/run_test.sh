f4cf6eafc9ff5518a334b19b15df0176d6cca21c
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2019-05-10 16:57:40 -0400

    Troubleshooting test

--- recipes/insilicoseq/run_test.sh
+++ recipes/insilicoseq/run_test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+curl -L https://ndownloader.figshare.com/files/15127220 > bogus-genome.fasta
+curl -L https://ndownloader.figshare.com/files/15127223 > checksums.sha
+
 iss generate --model MiSeq --seed 42 --cpus 1 --draft bogus-genome.fasta -n 100 -o bogus-reads
 shasum -c checksums.sha
