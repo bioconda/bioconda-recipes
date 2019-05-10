d9d84f3641a558ec542ab38a9af246d7f39d7634
Luca Venturini <lucventurini@gmail.com>
2019-05-10 17:13:29 +0100

    Modified build.sh to avoid crashing at the install stage

--- recipes/genomethreader/build.sh
+++ recipes/genomethreader/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin/{bssm,gthdata}
-install bin/* $PREFIX/bin/
+install $(ls -d bin/* | grep -v bssm | grep -v gthdata) $PREFIX/bin/
 install bin/bssm/* $PREFIX/bin/bssm/
 install bin/gthdata/* $PREFIX/bin/gthdata/
