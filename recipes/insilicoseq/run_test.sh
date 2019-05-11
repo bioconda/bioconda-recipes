489c1656a2c8a5c8cf0b71d8649a8e9a9f52f1dc
Daniel Standage <daniel.standage@gmail.com>
2019-05-10 22:35:48 -0400

    Try files in situ again

--- recipes/insilicoseq/run_test.sh
+++ recipes/insilicoseq/run_test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-curl -L https://ndownloader.figshare.com/files/15127220 > bogus-genome.fasta
-curl -L https://ndownloader.figshare.com/files/15127661 > checksums.md5
-
+gunzip bogus-genome.fasta.gz
 iss generate --model MiSeq --seed 42 --cpus 1 --draft bogus-genome.fasta -n 100 -o bogus-reads
 md5sum -c checksums.md5
