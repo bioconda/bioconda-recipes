d143afd2b5c77ed3af8da1b1e35612c8287d439b
Daniel Standage <daniel.standage@gmail.com>
2019-05-10 21:41:42 -0400

    shasum command not available on CI build, trying md5sum

--- recipes/insilicoseq/run_test.sh
+++ recipes/insilicoseq/run_test.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 curl -L https://ndownloader.figshare.com/files/15127220 > bogus-genome.fasta
-curl -L https://ndownloader.figshare.com/files/15127223 > checksums.sha
+curl -L https://ndownloader.figshare.com/files/15127661 > checksums.md5
 
 iss generate --model MiSeq --seed 42 --cpus 1 --draft bogus-genome.fasta -n 100 -o bogus-reads
-shasum -c checksums.sha
+md5sum -c checksums.md5
