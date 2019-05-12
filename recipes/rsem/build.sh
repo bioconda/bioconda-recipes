0004067a534bc67275e3e333cedb0f0de15bc05d
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-05-12 09:21:55 -0600

    Fix OSX dylib libname

--- recipes/rsem/build.sh
+++ recipes/rsem/build.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/bash
 set -x
 
+case `uname` in
+    Linux)
+	DSOSUF="so"
+	;;
+    Darwin)
+	DSOSUF="dylib"
+	;;
+    *)
+	echo "Unknown uname '`uname`'" >&2
+	exit 1
+esac
+
 # main binaries
 make \
     CXX=$CXX \
     CXXFLAGS="$CXXFLAGS" \
     CPPFLAGS="$CPPFLAGS -I$PREFIX/include -I." \
-    SAMLIBS=$PREFIX/lib/libhts.so \
+    SAMLIBS=$PREFIX/lib/libhts.$DSOSUF \
     SAMHEADERS=$PREFIX/include/htslib/sam.h \
     LDFLAGS="$LDFLAGS -L$PREFIX/lib" \
     prefix="$PREFIX" \
