153d18cea93fbccfadb30955bafbf3d1a89b88b3
dpryan79 <dpryan79@gmail.com>
2019-05-13 10:03:55 +0200

    old bedtools versions won't build, fix bcftools/1.3.1

--- recipes/bcftools/1.3.1/build.sh
+++ recipes/bcftools/1.3.1/build.sh
@@ -2,5 +2,5 @@
 
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-make all prefix=$PREFIX CC=${CC} CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
-make install
+make all prefix=${PREFIX} CC=${CC} LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
+make install prefix=${PREFIX} CC=${CC} LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
