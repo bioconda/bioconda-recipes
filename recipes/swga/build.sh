8d60481fcaa2a319d6ffd38c1b87962d46767873
Andreas Sjödin <andreas.sjodin@gmail.com>
2019-05-13 09:59:16 +0200

    Rebuild swga

--- recipes/swga/build.sh
+++ recipes/swga/build.sh
@@ -4,4 +4,4 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
-python -m pip install --no-deps --ignore-installed .
+python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
