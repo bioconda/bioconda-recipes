9b535951cdd594026e4e7d28596cdf8abfaf4b5d
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2019-05-13 20:48:33 +0200

    update crispritz to 1.1.0 (testing with osx) (#15347)
    * Add files via upload
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh

--- recipes/crispritz/build.sh
+++ recipes/crispritz/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-make
+make -f Makefile_conda
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/opt/crispritz
-chmod 700 -R *
+chmod -R 700 .
 cp crispritz.py $PREFIX/bin
 cp buildTST $PREFIX/opt/crispritz
 cp searchTST $PREFIX/opt/crispritz
