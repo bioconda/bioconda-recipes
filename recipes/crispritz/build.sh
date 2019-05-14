21fe261f30b9f32c63c8e442c7c980002f933c62
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2019-05-14 16:54:59 +0200

    Add files via upload (#15367)

--- recipes/crispritz/build.sh
+++ recipes/crispritz/build.sh
@@ -8,4 +8,4 @@ cp crispritz.py $PREFIX/bin
 cp buildTST $PREFIX/opt/crispritz
 cp searchTST $PREFIX/opt/crispritz
 cp searchBruteForce $PREFIX/opt/crispritz
-cp -R sourceCode/Python_Scripts/ $PREFIX/opt/crispritz
+cp -R sourceCode/Python_Scripts $PREFIX/opt/crispritz
