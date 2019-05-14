0e3f0000c9d1fa4c7eb835d34449beea7ab5d5c4
John Lees <lees.john6@gmail.com>
2019-05-14 16:15:51 -0400

    No longer supports OS X (due to use of posix_fallocate())

--- recipes/squeakr/build.sh
+++ recipes/squeakr/build.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-BUILD_OS=$(uname -s)
-
-if [ ${BUILD_OS} == "Darwin" ]; then
-   make CC="${CC}" LD="${CXX}" CXX="${CXX}" \
-      LDFLAGS="${LDFLAGS} -stdlib=libc++ -L${PREFIX}/lib -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" \
-      CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
-      CFLAGS="${CFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
-      ARCH="${ARCH}"
-else
-   make CC="${CC}" LD="${CXX}" CXX="${CXX}" \
-      LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" \
-      CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
-      CFLAGS="${CFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
-      ARCH="${ARCH}"
-fi
+make CC="${CC}" LD="${CXX}" CXX="${CXX}" \
+   LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" \
+   CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
+   CFLAGS="${CFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
+   ARCH="${ARCH}"
 
 install -d $PREFIX/bin
 install squeakr $PREFIX/bin
