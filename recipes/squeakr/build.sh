5f100a7fa07b28cfd206fda16838b4cf58eaff85
John Lees <lees.john6@gmail.com>
2019-05-14 12:55:50 -0400

    Override Makefile with conda env vars

--- recipes/squeakr/build.sh
+++ recipes/squeakr/build.sh
@@ -3,9 +3,9 @@
 BUILD_OS=$(uname -s)
 
 if [ ${BUILD_OS} == "Darwin" ]; then
-   LDFLAGS="${LDFLAGS} -stdlib=libc++ -L ${PREFIX}/lib -Ofast -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" CXXFLAGS="-stdlib=libc++ -I ${PREFIX}/include" NH=1 make
+   CXX="${CXX}" LDFLAGS="${LDFLAGS} -stdlib=libc++ -L ${PREFIX}/lib" CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -I ${PREFIX}/include" NH=1 make
    else
-   LDFLAGS="${LDFLAGS} -L ${PREFIX}/lib -Ofast -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" CXXFLAGS="${CXXFLAGS} -L ${PREFIX}/lib -I ${PREFIX}/include" NH=1 make
+   CXX="${CXX}" LDFLAGS="${LDFLAGS} -L ${PREFIX}/lib" CXXFLAGS="${CXXFLAGS} -L ${PREFIX}/lib -I ${PREFIX}/include" NH=1 make
 fi
 
 install -d $PREFIX/bin
