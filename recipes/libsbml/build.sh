78ae165c568b1c394723616a03cc3aca7ee635fe
Devon Ryan <dpryan79@users.noreply.github.com>
2019-05-14 11:16:56 +0200

    Update build.sh

--- recipes/libsbml/build.sh
+++ recipes/libsbml/build.sh
@@ -2,6 +2,7 @@
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_CXX_COMPILER=${CXX} \
       -DCMAKE_C_COMPILER=${CC} \
       -DWITH_SWIG=OFF \
