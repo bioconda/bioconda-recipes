36e622379060c8d284b9c870d0bf8b6f28fe3575
John Lees <lees.john6@gmail.com>
2019-05-14 15:30:39 -0400

    Correctly set make options from conda env

--- recipes/squeakr/build.sh
+++ recipes/squeakr/build.sh
@@ -3,9 +3,17 @@
 BUILD_OS=$(uname -s)
 
 if [ ${BUILD_OS} == "Darwin" ]; then
-   CXX="${CXX}" LDFLAGS="${LDFLAGS} -stdlib=libc++ -L ${PREFIX}/lib" CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -I ${PREFIX}/include" NH=1 make
-   else
-   CXX="${CXX}" LDFLAGS="${LDFLAGS} -L ${PREFIX}/lib" CXXFLAGS="${CXXFLAGS} -L ${PREFIX}/lib -I ${PREFIX}/include" NH=1 make
+   make CC="${CC}" LD="${CXX}" CXX="${CXX}" \
+      LDFLAGS="${LDFLAGS} -stdlib=libc++ -L${PREFIX}/lib -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" \
+      CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
+      CFLAGS="${CFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
+      ARCH="${ARCH}"
+else
+   make CC="${CC}" LD="${CXX}" CXX="${CXX}" \
+      LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lpthread -lboost_system -lboost_thread -lm -lbz2 -lz -lrt" \
+      CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
+      CFLAGS="${CFLAGS} -I${PREFIX}/include -I. -I${SRC_DIR}/include" \
+      ARCH="${ARCH}"
 fi
 
 install -d $PREFIX/bin
