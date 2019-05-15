f75c688561d883456fd39e1f322bede4cd131105
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2019-05-15 14:31:34 +0200

    Added extended base image because of libGL dependency

--- recipes/ngs-bits/build.sh
+++ recipes/ngs-bits/build.sh
@@ -4,21 +4,14 @@
 ln -s $PREFIX/include htslib/include
 ln -s $PREFIX/lib htslib/lib
 
+#qmake bugfix: qmake fails if there is no g++ executable available, even if QMAKE_CXX is explicitly set
+ln -s $CXX $BUILD_PREFIX/bin/g++
+export PATH=$BUILD_PREFIX/bin/:$PATH
+
 #build
 mkdir build
 cd build
-echo "QMAKE version..."
-qmake --version
-echo "cxx ..."
-echo $CXX
-echo "cxx version..."
-$CXX --version
-echo "FIXING..."
-ln -s $CXX $BUILD_PREFIX/bin/g++
-export PATH=$BUILD_PREFIX/bin/:$PATH
-echo "QMAKE..."
 qmake CONFIG-=debug CONFIG+=release DEFINES+=QT_NO_DEBUG_OUTPUT QMAKE_CXX=${CXX} -Wall -d ../src/tools.pro
-echo "MAKE..."
 make
 
 #remove test files from bin folder
