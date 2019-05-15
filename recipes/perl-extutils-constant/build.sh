595f855675b07262f3acc61b6ffa5f68f34a770f
Andreas Sjödin <andreas.sjodin@gmail.com>
2019-05-15 23:40:36 +0200

    Bump perl-extutils-constant (#15404)
    * Bump perl-extutils-constant
    
    * Remove from blacklist

--- recipes/perl-extutils-constant/build.sh
+++ recipes/perl-extutils-constant/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
     perl ./Build
     perl ./Build test
+    # Make sure this goes in site
     perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
+    # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
     make test
@@ -14,4 +17,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
