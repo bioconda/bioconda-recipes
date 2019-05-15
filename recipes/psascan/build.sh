7245ab999fd7ab5fe123402beb9888e3a3ef2f59
dpryan79 <dpryan79@gmail.com>
2019-05-15 12:32:23 +0200

    fix sed on OSX

--- recipes/psascan/build.sh
+++ recipes/psascan/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pushd src
-sed -i.bak '4iCFLAGS += -I$(PREFIX)/include -L$(PREFIX)/lib' Makefile
+sed -i.bak '4i\
+CFLAGS += -I$(PREFIX)/include -L$(PREFIX)/lib' Makefile
 make CC=${CXX}
 mkdir -p ${PREFIX}/bin
 mv psascan ${PREFIX}/bin/
