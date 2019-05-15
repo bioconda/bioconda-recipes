90185a8515bd3abd082fd787cdedc9f8b381fba5
dpryan79 <dpryan79@gmail.com>
2019-05-15 14:21:25 +0200

    try this

--- recipes/psascan/build.sh
+++ recipes/psascan/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-find / -name omp.h -print
 pushd src
-sed -i.bak '4i\
-CFLAGS += -I$(PREFIX)/include -L$(PREFIX)/lib' Makefile
+sed -i.bak "4i\\
+CFLAGS += -I${PREFIX}/include -I${BUILD_PREFIX}/include -L${PREFIX}/lib" Makefile
 make CC=${CXX}
 mkdir -p ${PREFIX}/bin
 mv psascan ${PREFIX}/bin/
