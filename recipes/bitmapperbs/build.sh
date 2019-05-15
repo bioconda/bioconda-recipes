6e92cfed1db65ad8d2b7e8c5060ea726b5153309
Devon Ryan <dpryan79@gmail.com>
2019-05-15 23:24:50 +0200

    copy over the custom htslib

--- recipes/bitmapperbs/build.sh
+++ recipes/bitmapperbs/build.sh
@@ -5,3 +5,5 @@ make -f ${RECIPE_DIR}/newMakefile PREFIX=${PREFIX} CC=${CC} CXX=${CXX}
 
 mkdir -p ${PREFIX}/bin
 mv bitmapperBS ${PREFIX}/bin/
+mkdir -p ${PREFIX}/lib
+mv htslib/libhts.s* ${PREFIX}/lib/
