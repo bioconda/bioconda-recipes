8545f33f89cdd1da26c5410c12d9efc789128e0e
Michael Milton <ttmigueltt@gmail.com>
2019-05-16 13:18:04 +1000

    Pass in variables to make

--- recipes/xs/build.sh
+++ recipes/xs/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/CC =/CC ?=/g' Makefile
-sed -i 's/CFLAGS =/CFLAGS ?=/g' Makefile
-make
+#sed -i 's/CC =/CC ?=/g' Makefile
+#sed -i 's/CFLAGS =/CFLAGS ?=/g' Makefile
+make CC=${CC} CFLAGS=${CFLAGS}
 cp XS $PREFIX/bin
