3efd17518896258b1a7b73b8e65990b2c82bd4f0
Jim Johnson <jj@umn.edu>
2019-05-16 15:01:50 -0500

    opencontactcli fix for MacOSX

--- recipes/opencontactcli/build.sh
+++ recipes/opencontactcli/build.sh
@@ -4,13 +4,8 @@ cp src/main_cli.py src/*.f src/*.pdb src/*j* $PREFIX/bin
 chmod 0766 $PREFIX/bin/main_cli.py
 ln -s $PREFIX/bin/main_cli.py $PREFIX/bin/OpenContactCLI
 cd  $PREFIX/bin
-# cd src
-which f2py
 f2py -c --help-fcompiler
 f2py -c -m it inputgui.f
 f2py -c -m contact contactgui.f
-# cp -p *.so* $PREFIX/bin/
-#cp -p it.p* $PREFIX/bin/
-#cp -p contact.p* $PREFIX/bin/
-cp -p *.so* $PREFIX/lib/
-ls -ltr
+if [ ! -e it.so ]; then ln -s it*.so it.so; fi
+if [ ! -e contact.so ]; then ln -s contact*.so contact.so; fi
