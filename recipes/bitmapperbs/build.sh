b31d6444d0f309ee3b100b104bed557ef3e89e34
dpryan79 <dpryan79@gmail.com>
2019-05-16 17:38:03 +0200

    compile in the modified htslib to avoid issues

--- recipes/bitmapperbs/build.sh
+++ recipes/bitmapperbs/build.sh
@@ -5,6 +5,3 @@ make -f ${RECIPE_DIR}/newMakefile PREFIX=${PREFIX} CC=${CC} CXX=${CXX}
 
 mkdir -p ${PREFIX}/bin
 mv bitmapperBS ${PREFIX}/bin/
-mkdir -p ${PREFIX}/lib
-mv htslib_aim/lib/libhtsBit* ${PREFIX}/lib/
-mv htslib_aim/lib/libhts.so.2to3part2 ${PREFIX}/lib/
