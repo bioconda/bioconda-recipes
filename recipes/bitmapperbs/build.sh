b973a69cf610fd86d17360ba565c35e92fa1a3c6
Devon Ryan <dpryan79@users.noreply.github.com>
2019-05-16 14:25:09 +0200

    debug

--- recipes/bitmapperbs/build.sh
+++ recipes/bitmapperbs/build.sh
@@ -6,4 +6,6 @@ make -f ${RECIPE_DIR}/newMakefile PREFIX=${PREFIX} CC=${CC} CXX=${CXX}
 mkdir -p ${PREFIX}/bin
 mv bitmapperBS ${PREFIX}/bin/
 mkdir -p ${PREFIX}/lib
+ls htslib_aim/lib
 mv htslib_aim/lib/libhtsBit* ${PREFIX}/lib/
+ls ${PREFIX}/lib
