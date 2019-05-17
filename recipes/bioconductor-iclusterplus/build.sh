7f0b4528454a6b874db03ebeafb8d43089f76984
dpryan79 <dpryan79@gmail.com>
2019-05-17 15:53:34 +0200

    Patch configure in iclusterplus

--- recipes/bioconductor-iclusterplus/build.sh
+++ recipes/bioconductor-iclusterplus/build.sh
@@ -2,10 +2,14 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
+ln -s $FC ${PREFIX}/bin/gfortran
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
+sed -i.bak "1652i\\
+  FC=\"gfortran\"" configure
 $R CMD INSTALL --build .
+rm ${PREFIX}/bin/gfortran
