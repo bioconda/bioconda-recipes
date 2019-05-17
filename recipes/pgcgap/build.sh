cef0bec9335accdbf14ea3b1d46452c2fe21faf5
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-05-17 14:38:40 +0800

    update COG

--- recipes/pgcgap/build.sh
+++ recipes/pgcgap/build.sh
@@ -2,15 +2,15 @@
 
 mkdir -p $PREFIX/bin
 
-wget -c -r -nH -np -nd -R index.html -P ./ ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/
-gunzip prot2003-2014.fa.gz
-makeblastdb -parse_seqids -in prot2003-2014.fa -input_type fasta -dbtype prot -out COG_2014
-mv COG_2014.phr $PREFIX/bin/
-mv COG_2014.pin $PREFIX/bin/
-mv COG_2014.psq $PREFIX/bin/
-mv cog2003-2014.csv $PREFIX/bin/
-mv cognames2003-2014.tab $PREFIX/bin/
-mv fun2003-2014.tab $PREFIX/bin/
+#wget -c -r -nH -np -nd -R index.html -P ./ ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/
+#gunzip prot2003-2014.fa.gz
+#makeblastdb -parse_seqids -in prot2003-2014.fa -input_type fasta -dbtype prot -out COG_2014
+#mv COG_2014.phr $PREFIX/bin/
+#mv COG_2014.pin $PREFIX/bin/
+#mv COG_2014.psq $PREFIX/bin/
+#mv cog2003-2014.csv $PREFIX/bin/
+#mv cognames2003-2014.tab $PREFIX/bin/
+#mv fun2003-2014.tab $PREFIX/bin/
 
 cp pgcgap.pl $PREFIX/bin/pgcgap
 cp Functions/Pan/plot_3Dpie.R $PREFIX/bin/
@@ -33,12 +33,12 @@ chmod a+x $PREFIX/bin/get_flag_relative_abundances_table.pl
 chmod a+x $PREFIX/bin/Plot_COG.R
 chmod a+x $PREFIX/bin/get_ANImatrix.pl
 chmod a+x $PREFIX/bin/Plot_ANIheatmap.R
-chmod a+r $PREFIX/bin/COG_2014.phr
-chmod a+r $PREFIX/bin/COG_2014.pin
-chmod a+r $PREFIX/bin/COG_2014.psq
-chmod a+r $PREFIX/bin/cog2003-2014.csv
-chmod a+r $PREFIX/bin/cognames2003-2014.tab
-chmod a+r $PREFIX/bin/fun2003-2014.tab
+#chmod a+r $PREFIX/bin/COG_2014.phr
+#chmod a+r $PREFIX/bin/COG_2014.pin
+#chmod a+r $PREFIX/bin/COG_2014.psq
+#chmod a+r $PREFIX/bin/cog2003-2014.csv
+#chmod a+r $PREFIX/bin/cognames2003-2014.tab
+#chmod a+r $PREFIX/bin/fun2003-2014.tab
 
 
 pgcgap --version
