cc9e21c36fd5e14bef445d9974b89777d772b816
Devon Ryan <dpryan79@gmail.com>
2019-05-17 21:53:29 +0200

    reverse that

--- recipes/bioconductor-iclusterplus/build.sh
+++ recipes/bioconductor-iclusterplus/build.sh
@@ -2,7 +2,7 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" != "darwin"* ]]; then
     ln -s $FC ${PREFIX}/bin/gfortran
 fi
 echo -e "CC=$CC
@@ -14,6 +14,6 @@ CXX14=$CXX" > ~/.R/Makevars
 sed -i.bak "1652i\\
   FC=\"gfortran\"" configure
 $R CMD INSTALL --build .
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" != "darwin"* ]]; then
     rm ${PREFIX}/bin/gfortran
 fi
