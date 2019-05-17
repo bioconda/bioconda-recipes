a1211ce44eacde76705830e72428c0b02f4b64e8
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-05-17 12:04:02 +0800

    update

--- recipes/pgcgap/build.sh
+++ recipes/pgcgap/build.sh
@@ -2,22 +2,26 @@
 
 mkdir -p $PREFIX/bin
 
+wget -c -r -nH -np -nd -R index.html -P ./ ftp://ftp.ncbi.nih.gov/pub/COG/COG2014/data/
+gunzip prot2003-2014.fa.gz
+makeblastdb -parse_seqids -in prot2003-2014.fa -input_type fasta -dbtype prot -out COG_2014
+mv COG_2014.phr $PREFIX/bin/
+mv COG_2014.pin $PREFIX/bin/
+mv COG_2014.psq $PREFIX/bin/
+mv cog2003-2014.csv $PREFIX/bin/
+mv cognames2003-2014.tab $PREFIX/bin/
+mv fun2003-2014.tab $PREFIX/bin/
 
 cp pgcgap.pl $PREFIX/bin/pgcgap
 cp Functions/Pan/plot_3Dpie.R $PREFIX/bin/
 cp Functions/Pan/fmplot.py $PREFIX/bin/
+
 cp Functions/COG/COG.pl $PREFIX/bin/
 cp Functions/COG/get_flag_relative_abundances_table.pl $PREFIX/bin/
 cp Functions/COG/Plot_COG.R $PREFIX/bin/
-cp Functions/COG/COG_2014.phr $PREFIX/bin/
-cp Functions/COG/COG_2014.pin $PREFIX/bin/
-cp Functions/COG/COG_2014.psq $PREFIX/bin/
-cp Functions/COG/cog2003-2014.csv $PREFIX/bin/
-cp Functions/COG/cognames2003-2014.tab $PREFIX/bin/
-cp Functions/COG/fun2003-2014.tab $PREFIX/bin/
+
 cp Functions/ANI/get_ANImatrix.pl $PREFIX/bin/
 cp Functions/ANI/Plot_ANIheatmap.R $PREFIX/bin/
-
 
 
 
