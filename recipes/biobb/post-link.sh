ee527770e92c8ec917a1936ddcdb485441d0f6ee
Pau Andrio <andriopau@gmail.com>
2019-05-20 15:54:53 +0200

    Forcing bioconda channel in post-link.sh

--- recipes/biobb/post-link.sh
+++ recipes/biobb/post-link.sh
@@ -1,2 +1,2 @@
-conda install -y biobb_analysis==1.0.3
-conda install -y biobb_chemistry==1.0.6
+conda install -y -c bioconda biobb_analysis==1.0.3
+conda install -y -c bioconda biobb_chemistry==1.0.6
