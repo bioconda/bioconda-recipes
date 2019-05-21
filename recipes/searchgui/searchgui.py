51053681d6592fb7c39ab63a9178870d2a533e50
CarlosHorro <carlos.horro@gmail.com>
2019-05-21 16:12:05 +0200

    SearchGUI 4.0.1_SNAPSHOT

--- recipes/searchgui/searchgui.py
+++ recipes/searchgui/searchgui.py
@@ -14,7 +14,7 @@ from os import access
 from os import getenv
 from os import X_OK
 
-jar_file = 'SearchGUI-3.3.10.jar'
+jar_file = 'SearchGUI-4.0.1-SNAPSHOT'
 
 default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
 
