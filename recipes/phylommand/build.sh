514b5bdddffb42912b2ff38f5b6b2838786c3c44
brendanf <brendan.furneaux@gmail.com>
2019-05-21 21:39:38 +0200

    Use CXXFLAGS on all invocations of CXX

--- recipes/phylommand/build.sh
+++ recipes/phylommand/build.sh
@@ -4,12 +4,10 @@ cd src
 sed -i.bak 's/argv_parser.o \$(TREEATORLINKFLAGS)$/argv_parser.o/' Makefile 
 sed -i.bak 's/\$(PP) -o treeator \$(OTREEATOR)/\$(PP) -o treeator \$(OTREEATOR) \$(TREEATORLINKFLAGS)/' Makefile
 
-# CFLAGS, CPPFLAGS, and LDFLAGS are missing
-sed -i.bak '/\$(PP).*-c/ s/-c/$(CFLAGS) -c/' Makefile
-sed -i.bak '/\$(CC).*-c/ s/-c/$(CFLAGS) -c/' Makefile
+# LDFLAGS are missing
 sed -i.bak '/\$(PP).*-o/ s/-o/$(LDFLAGS) -o/' Makefile
 
-make PP="$CXX" CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" NLOPT=YES PTHREADS=YES
+make PP="$CXX $CXXFLAGS" CC="$CC $CFLAGS" LDFLAGS="$LDFLAGS" NLOPT=YES PTHREADS=YES
 
 # Makefile has no install target 
 mkdir -p $PREFIX/bin
