9e4e28aa927522f5ed47960c62b07b6a5868bc8a
brendanf <brendan.furneaux@gmail.com>
2019-05-21 15:59:13 +0200

    fix sed syntax

--- recipes/phylommand/build.sh
+++ recipes/phylommand/build.sh
@@ -5,9 +5,9 @@ sed -i.bak 's/argv_parser.o \$(TREEATORLINKFLAGS)$/argv_parser.o/' Makefile
 sed -i.bak 's/\$(PP) -o treeator \$(OTREEATOR)/\$(PP) -o treeator \$(OTREEATOR) \$(TREEATORLINKFLAGS)/' Makefile
 
 # CFLAGS, CPPFLAGS, and LDFLAGS are missing
-sed -i.bak '/\$(PP).*-c/ s/-c/$(CFLAGS) -c/'
-sed -i.bak '/\$(CC).*-c/ s/-c/$(CFLAGS) -c/'
-sed -i.bak '/\$(PP).*-o/ s/-o/$(LDFLAGS) -o/'
+sed -i.bak '/\$(PP).*-c/ s/-c/$(CFLAGS) -c/' Makefile
+sed -i.bak '/\$(CC).*-c/ s/-c/$(CFLAGS) -c/' Makefile
+sed -i.bak '/\$(PP).*-o/ s/-o/$(LDFLAGS) -o/' Makefile
 
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
