043b4a34f75cc37234fab15466d26c0f37ce2cdb
brendanf <brendan.furneaux@gmail.com>
2019-05-21 14:38:29 +0200

    CFLAGS and LDFLAGS

--- recipes/phylommand/build.sh
+++ recipes/phylommand/build.sh
@@ -4,6 +4,9 @@ cd src
 sed -i.bak 's/argv_parser.o \$(TREEATORLINKFLAGS)$/argv_parser.o/' Makefile 
 sed -i.bak 's/\$(PP) -o treeator \$(OTREEATOR)/\$(PP) -o treeator \$(OTREEATOR) \$(TREEATORLINKFLAGS)/' Makefile
 
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+
 make PP="$GXX -std=c++11" CC="$GCC" NLOPT=YES RUDISVG=YES PTHREADS=YES
 
 # Makefile has no install target 
