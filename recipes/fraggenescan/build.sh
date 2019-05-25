51d5614a723537ab7d1e83aca173dc7bf371cafa
Devon Ryan <dpryan79@gmail.com>
2019-05-25 21:24:05 +0200

    Makefile sucks

--- recipes/fraggenescan/build.sh
+++ recipes/fraggenescan/build.sh
@@ -4,6 +4,7 @@ mkdir -p $PREFIX/bin/
 
 make clean  # There are .o files in the tarball!
 make CC=${CC}
+make fgs CC=${CC}
 
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $SRC_DIR/run_FragGeneScan.pl
 chmod a+x $SRC_DIR/*FragGeneScan*
