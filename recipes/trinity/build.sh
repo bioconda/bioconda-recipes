10286f750fda9fb58023e5b436e25a52e44cdb07
Devon Ryan <dpryan79@gmail.com>
2019-05-26 11:01:38 +0200

    grr

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -5,6 +5,7 @@ set -x -e
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
