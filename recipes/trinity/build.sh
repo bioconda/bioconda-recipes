7b1a39f555c8da06bb682ed017481e2cbe4654d2
Devon Ryan <dpryan79@gmail.com>
2019-05-26 09:42:31 +0200

    that should have worked

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -18,7 +18,7 @@ cd $SRC_DIR
 
 # The compilers aren't propogated
 pushd trinity-plugins/seqtk-trinity-0.0.2
-make CC=$CC
+${CC} ${CFLAGS} -o seqtk.c -o seqtk-trinity -lz -lm
 popd
 
 make plugins CC=$CC CXX=$CXX
