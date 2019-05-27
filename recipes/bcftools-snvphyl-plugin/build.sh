f852eac5597ed136c719766000061597059b4b82
Philip.Mabon <philip.mabon@canada.ca>
2019-05-27 14:14:40 -0500

    added gcc exports

--- recipes/bcftools-snvphyl-plugin/build.sh
+++ recipes/bcftools-snvphyl-plugin/build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 export VERSION=1.9
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CFLAGS="$CFLAGS -I$PREFIX/include"
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/libexec/bcftools
