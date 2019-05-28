897537c834f62b4619b2af1fa0f84557337ec404
Warren Kretzschmar <warrenk@kth.se>
2019-05-28 16:05:32 +0200

    Compile and link against static libraries: tabix and samtools

--- recipes/snptools/build.sh
+++ recipes/snptools/build.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-sed -i.bak 's/INCLUDE=-Isamtools-0.1.16 -Itabix-0.2.5//; s/-Lsamtools-0.1.16 -Ltabix-0.2.5//' makefile
+cd tabix-0.2.5
+make lib
+cd -
 
-make all CC=$CXX
+cd samtools-0.1.16
+make lib
+cd -
+
+# This patch has not been fed upstream
+sed -i.bak 's/=/\?=/g' makefile
+
+# This getopt.h appears to be included through sam.h for the other .cpp files
+# Not yet reported to upstream
+sed -i.bak 's/using/#include <getopt.h>\nusing/' poprob.cpp
+
+make all CC=$CXX\
+     INCLUDE="-Wall -Wextra -O3 -fopenmp -I$PREFIX/include -Itabix-0.2.5 -Isamtools-0.1.16" \
+     LIBS="-L$PREFIX/lib ./samtools-0.1.16/libbam.a ./tabix-0.2.5/libtabix.a -lgomp -lpthread -lgsl -lgslcblas -lz -lbz2"
 
 install bamodel poprob probin impute hapfuse $PREFIX/bin
 
