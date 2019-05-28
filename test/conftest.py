4bd6544043a6c1af80f38f06ea196d3d576e8d14
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2019-05-28 13:56:17 -0600

    Move fixtures to conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,26 @@
 import contextlib
 import datetime
+import tempfile
+import os
+import os.path as op
+from copy import deepcopy
 
+from ruamel_yaml import YAML
 import pandas as pd
 import pytest
 
 from bioconda_utils import utils
+
+yaml = YAML(typ="rt")  # pylint: disable=invalid-name
+
+
+# common settings
+TEST_RECIPES_FOLDER = 'recipes'
+TEST_CONFIG_YAML_FNAME = 'config.yaml'
+TEST_CONFIG_YAML = {
+    'blacklists': [],
+    'channels': []
+}
 
 
 def pytest_runtest_makereport(item, call):
@@ -21,16 +37,37 @@ def pytest_runtest_setup(item):
 
 
 @pytest.fixture
-def mock_repodata(repodata_yaml):
-    """Pepares RepoData singleton to contain mock data"""
-    repodata = utils.RepoData()
-    orig_data = repodata._df, repodata._df_ts
+def mock_repodata(repodata, case):
+    """Pepares RepoData singleton to contain mock data
 
-    df = pd.DataFrame(columns = repodata.columns)
-    for channel, packages in repodata_yaml.items():
+    Expects function to be parametrized with ``case`` and ``repodata``,
+    where ``case`` may contain a ``repodata`` key to be added to
+    base ``repodata`` contents.
+
+    ``repodata`` should be of this form::
+
+       <channel>:
+          <package_name>:
+             - <key>: value
+               <key>: value
+
+    E.g.::
+       bioconda:
+         package_one:
+             - version: 0.1
+               build_number: 0
+    """
+    if 'repodata' in case:
+        data = deepcopy(repodata)
+        dict_merge(data, case['repodata'])
+    else:
+        data = repodata
+
+    dataframe = pd.DataFrame(columns = utils.RepoData.columns)
+    for channel, packages in data.items():
         for name, versions in packages.items():
             for item in versions:
-                data = {
+                pkg = {
                     'channel': channel,
                     'name': name,
                     'build': '',
@@ -40,11 +77,74 @@ def mock_repodata(repodata_yaml):
                     'subdir': '',
                     'platform': 'noarch',
                 }
-                data.update(item)
-                df = df.append(data, ignore_index=True)
+                pkg.update(item)
+                dataframe = dataframe.append(pkg, ignore_index=True)
 
-    repodata._df_ts = datetime.datetime.now()
-    repodata._df = df
+    backup = utils.RepoData()._df, utils.RepoData()._df_ts
+    utils.RepoData()._df = dataframe
+    utils.RepoData()._df_ts = datetime.datetime.now()
     yield
-    repodata._df, repodata._df_ts = orig_data
+    utils.RepoData()._df, utils.RepoData()._df_ts = backup
 
+
+@pytest.fixture
+def recipes_folder():
+    """Prepares a temp dir with '/recipes' folder as configured"""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        folder = op.join(tmpdir, TEST_RECIPES_FOLDER)
+        os.mkdir(folder)
+        yield folder
+
+
+@pytest.fixture
+def config_file(recipes_folder):
+    """Prepares a Bioconda config.yaml  in a recipes_folder"""
+    config_fname = op.join(op.dirname(recipes_folder), TEST_CONFIG_YAML_FNAME)
+    with open(config_fname, 'w') as fdes:
+        yaml.dump(TEST_CONFIG_YAML, fdes)
+    yield config_fname
+
+
+
+def dict_merge(base, add):
+    for key, value in add.items():
+        if isinstance(value, dict):
+            base[key] = dict_merge(base.get(key, {}), value)
+        elif isinstance(base, list):
+            for num in range(len(base)):
+                base[num][key] = dict_merge(base[num].get(key, {}), add)
+        else:
+            base[key] = value
+    return base
+
+
+@pytest.fixture
+def recipe(recipes_folder, case, recipe_data):
+    """Prepares a recipe from recipe_data in recipes_folder"""
+    recipe = deepcopy(recipe_data['meta.yaml'])
+    if 'remove' in case:
+        for remove in utils.ensure_list(case['remove']):
+            path = remove.split('/')
+            cont = recipe
+            for p in path[:-1]:
+                cont = cont[p]
+            if isinstance(cont, list):
+                for n in range(len(cont)):
+                    del cont[n][path[-1]]
+            else:
+                del cont[path[-1]]
+    if 'add' in case:
+        dict_merge(recipe, case['add'])
+
+    recipe_folder = op.join(recipes_folder, recipe_data['folder'])
+    os.mkdir(recipe_folder)
+
+    if 'add_files' in case:
+        for fname, data in case['add_files'].items():
+            with open(op.join(recipe_folder, fname), "w") as out:
+                out.write(data)
+
+    with open(op.join(recipe_folder, 'meta.yaml'), "w") as meta_file:
+        yaml.dump(recipe, meta_file)
+
+    yield recipe_folder
--- test/test_lint.py
+++ test/test_lint.py
@@ -1,9 +1,5 @@
 import os.path as op
-import os
-import json
 from ruamel_yaml import YAML
-import tempfile
-from copy import deepcopy
 
 import pytest
 
@@ -16,83 +12,10 @@ yaml = YAML(typ="rt")  # pylint: disable=invalid-name
 with open(op.join(op.dirname(__file__), "lint_cases.yaml")) as data:
     TEST_DATA = yaml.load(data)
 
-TEST_SETUP = TEST_DATA['setup']
-TEST_RECIPES = list(TEST_SETUP['recipes'].values())
-TEST_RECIPE_IDS = list(TEST_SETUP['recipes'].keys())
+TEST_RECIPES = list(TEST_DATA['setup']['recipes'].values())
+TEST_RECIPE_IDS = list(TEST_DATA['setup']['recipes'].keys())
 TEST_CASES = TEST_DATA['tests']
 TEST_CASE_IDS = [case['name'] for case in TEST_CASES]
-TEST_REPODATA = TEST_SETUP['repodata']
-
-
-def dict_merge(base, add):
-    for key, value in add.items():
-        if isinstance(value, dict):
-            base[key] = dict_merge(base.get(key, {}), value)
-        elif isinstance(base, list):
-            for n in range(len(base)):
-                base[n][key] = dict_merge(base[n].get(key, {}), add)
-        else:
-            base[key] = value
-    return base
-
-@pytest.fixture
-def repodata_yaml(case):
-    if 'repodata' in case:
-        data = deepcopy(TEST_REPODATA)
-        dict_merge(data, case['repodata'])
-    else:
-        data = TEST_REPODATA
-    yield data
-
-@pytest.fixture
-def recipes_folder():
-    """Prepares a temp dir with '/recipes' folder as configured"""
-    with tempfile.TemporaryDirectory() as tmpdir:
-        recipes_folder = op.join(tmpdir, TEST_SETUP['recipes_folder'])
-        os.mkdir(recipes_folder)
-        yield recipes_folder
-
-
-@pytest.fixture
-def config_file(recipes_folder, case):
-    """Prepares a Bioconda config.yaml  in a recipes_folder"""
-    config_yaml = TEST_SETUP['config.yaml']
-    config_fname = op.join(op.dirname(recipes_folder), 'config.yaml')
-    with open(config_fname, 'w') as config_file:
-        yaml.dump(config_yaml, config_file)
-    yield config_fname
-
-
-@pytest.fixture
-def recipe(recipes_folder, case, recipe_data):
-    """Prepares a recipe from recipe_data in recipes_folder"""
-    recipe = deepcopy(recipe_data['meta.yaml'])
-    if 'remove' in case:
-        for remove in ensure_list(case['remove']):
-            path = remove.split('/')
-            cont = recipe
-            for p in path[:-1]:
-                cont = cont[p]
-            if isinstance(cont, list):
-                for n in range(len(cont)):
-                    del cont[n][path[-1]]
-            else:
-                del cont[path[-1]]
-    if 'add' in case:
-        dict_merge(recipe, case['add'])
-
-    recipe_folder = op.join(recipes_folder, recipe_data['folder'])
-    os.mkdir(recipe_folder)
-
-    if 'add_files' in case:
-        for fname, data in case['add_files'].items():
-            with open(op.join(recipe_folder, fname), "w") as out:
-                out.write(data)
-
-    with open(op.join(recipe_folder, 'meta.yaml'), "w") as meta_file:
-        yaml.dump(recipe, meta_file)
-
-    yield recipe_folder
 
 
 @pytest.fixture
@@ -102,6 +25,7 @@ def linter(config_file, recipes_folder):
     yield lint.Linter(config, recipes_folder, nocatch=True)
 
 
+@pytest.mark.parametrize('repodata', (TEST_DATA['setup']['repodata'],))
 @pytest.mark.parametrize('recipe_data', TEST_RECIPES, ids=TEST_RECIPE_IDS)
 @pytest.mark.parametrize('case', TEST_CASES, ids=TEST_CASE_IDS)
 def test_lint(linter, recipe, mock_repodata, case):
