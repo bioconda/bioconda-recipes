4f933fe3ff047fc9093a680b9db80f12f7dacdc8
Warren Kretzschmar <warrenk@kth.se>
2019-05-28 15:01:29 +0200

    Add patch to remove tabix, samtools deps

--- recipes/snptools/build.sh
+++ recipes/snptools/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sed -i.bak 's/INCLUDE=-Isamtools-0.1.16 -Itabix-0.2.5//; s/-Lsamtools-0.1.16 -Ltabix-0.2.5//' makefile
+
 make all CC=$CXX
 
 install bamodel poprob probin impute hapfuse $PREFIX/bin
