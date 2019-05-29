5a2f271d1080745ea688cb2eac0c0d9a6fc637ce
Philip.Mabon <philip.mabon@canada.ca>
2019-05-29 08:25:07 -0500

    removed not necessary from build.sh for g++

--- recipes/bcftools-snvphyl-plugin/build.sh
+++ recipes/bcftools-snvphyl-plugin/build.sh
@@ -2,19 +2,11 @@
 
 export VERSION=1.9
 export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CFLAGS="$CFLAGS -I$PREFIX/include"
 
 CC=${CC}
-CXX=${CXX}
 
 ln -s ${CC} ${PREFIX}/bin/gcc
-ln -s ${CXX} ${PREFIX}/bin/g++
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/libexec/bcftools
