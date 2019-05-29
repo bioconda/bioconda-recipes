95455f9899968e6f50bc807b1b556fe34ca5e0ea
John Davey <johnomics@gmail.com>
2019-05-29 12:58:02 +0100

    Remove script copy

--- recipes/tapestry/build.sh
+++ recipes/tapestry/build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-mkdir -p $PREFIX/bin
-cp $SRC_DIR/weave $PREFIX/bin
-cp $SRC_DIR/clean $PREFIX/bin
