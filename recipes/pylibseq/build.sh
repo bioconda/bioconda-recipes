820ada331867adaf7696041d5ff164441d1cff70
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-05-30 11:49:21 -0600

    Update pylibseq to 0.2.1 (#15591)
    Package uploads complete: [ci skip]
     - biocontainers/pylibseq:0.2.1--py37h6bb024c_0
     - linux-64/pylibseq-0.2.1-py37h6bb024c_0.tar.bz2
     - osx-64/pylibseq-0.2.1-py37h04f5b5a_0.tar.bz2
    
    Co-authored-by: Natasha Pavlovikj <npavlovikj2@unl.edu>

--- recipes/pylibseq/build.sh
+++ recipes/pylibseq/build.sh
@@ -2,8 +2,9 @@
 
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib -ltbb"
+
 #On OS X, we have to force Python 
 #packages to compile with GCC.
 #Conda setup will default to enforcing 
 #clang o/w.
-CC=gcc CXX=g++ $PYTHON setup.py install --gcc
+CC="${CC}" CXX="${CXX}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
