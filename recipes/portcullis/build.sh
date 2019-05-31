8da75a1239244120333ae01f96af487c7ac67e58
Luca Venturini <lucventurini@gmail.com>
2019-05-31 14:41:38 +0100

    Trying to remove the CFLAGS etc. hack.

--- recipes/portcullis/build.sh
+++ recipes/portcullis/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -x -e
 
-echo "CFLAGS=$CFLAGS"
-echo "CPPFLAGS=$CPPFLAGS"
-echo "CXXFLAGS=$CXXFLAGS"
-echo "LDFLAGS=$LDFLAGS"
+# echo "CFLAGS=$CFLAGS"
+# echo "CPPFLAGS=$CPPFLAGS"
+# echo "CXXFLAGS=$CXXFLAGS"
+# echo "LDFLAGS=$LDFLAGS"
 
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+# export CFLAGS="-I$PREFIX/include"
+# export CPPFLAGS="-I$PREFIX/include"
+# export CXXFLAGS="-I$PREFIX/include"
+# export LDFLAGS="-L$PREFIX/lib"
 
 ./autogen.sh
 ./configure --disable-silent-rules --disable-dependency-tracking --prefix=$PREFIX
