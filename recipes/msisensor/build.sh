0bbd2624e3b725e22341f2d6936e05c5aff15b2a
Michael Knudsen <micknudsen@gmail.com>
2019-06-01 19:24:48 +0200

    Debugging

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -2,5 +2,7 @@
 # export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
 # export LDFLAGS="-L$PREFIX/lib -ltinfo"
 # export LIBRARY_PATH=${PREFIX}/lib
-make
-cp msisensor $PREFIX/bin
+# make
+# cp msisensor $PREFIX/bin
+
+echo "Hello, World!"
