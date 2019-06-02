caeadf600993364d9c1bbdce9d9ef93857648285
Michael Knudsen <micknudsen@gmail.com>
2019-06-02 07:39:26 +0200

    Back to where we started

--- recipes/msisensor/build.sh
+++ recipes/msisensor/build.sh
@@ -3,7 +3,5 @@ export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
 
-alias gcc=$GCC
-
 make
 cp msisensor $PREFIX/bin
