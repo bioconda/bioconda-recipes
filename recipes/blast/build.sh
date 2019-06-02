4f3da9a05b4e304f75aa1cdd96bd820325ca2dd7
Christiam Camacho <christiam@hotmail.com>
2019-06-01 23:33:05 -0400

    Remove unit tests

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -89,7 +89,6 @@ make -j${CPU_COUNT} -f Makefile.flat $apps
 rm $LIB_INSTALL_DIR
 
 mkdir -p $PREFIX/bin $LIB_INSTALL_DIR
-rm $SRC_DIR/c++/ReleaseMT/bin/*_unit_test
 cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
 cp $SRC_DIR/c++/ReleaseMT/lib/* $LIB_INSTALL_DIR
 
