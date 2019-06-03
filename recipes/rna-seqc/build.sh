8d579481cc8b8b406e62d3a21220bdaedf5b70f7
Stephen Kazakoff <sh.kazakoff@gmail.com>
2019-06-03 04:01:54 +0000

    Initial commit for rna-seqc2 recipe

--- recipes/rna-seqc/build.sh
+++ recipes/rna-seqc/build.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
+set -eu -o pipefail
 
-# Setup path variables
-BINARY_HOME=$PREFIX/bin
-PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+gunzip -Nv *.gz
+mkdir -p "${PREFIX}/bin"
+cp rnaseqc "${PREFIX}/bin"
+chmod +x "${PREFIX}/bin/rnaseqc"
 
-# Create destination directories
-mkdir -p $BINARY_HOME
-mkdir -p $PACKAGE_HOME
-
-# Copy file into $PACKAGE_HOME
-cp RNA-SeQC_v$PKG_VERSION.jar $PACKAGE_HOME
-
-# Create wrapper
-SOURCE_FILE=$RECIPE_DIR/rna_seqc.sh
-DEST_FILE=$PACKAGE_HOME/rna-seqc
-
-echo "#!/bin/bash" > $DEST_FILE
-echo "PKG_NAME=$PKG_NAME" >> $DEST_FILE
-echo "PKG_VERSION=$PKG_VERSION" >> $DEST_FILE
-echo "PACKAGE_HOME=$PACKAGE_HOME" >> $DEST_FILE
-cat $RECIPE_DIR/rna_seqc.sh >> $DEST_FILE
-
-chmod +x $DEST_FILE
-ln -s $DEST_FILE $PREFIX/bin
