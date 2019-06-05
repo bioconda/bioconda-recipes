ed19c2d817d6fc6ed3b1a4057d678a1e5f396f63
Michael Knudsen <micknudsen@gmail.com>
2019-06-05 14:56:42 +0200

    Redefine gcc in build.sh

--- recipes/somalier/build.sh
+++ recipes/somalier/build.sh
@@ -3,6 +3,7 @@
 export BRANCH=v0.19.0
 export PATH="nim-$BRANCH/bin/nim/bin:${PATH:+:$PATH}"
 export LD=$CC
+sed 's|sh build.sh|sed -i "s/gcc/${CC}/g" build.sh \&\& sh build.sh|g' ./scripts/build.sh
 bash ./scripts/build.sh
 export PATH="nim-$BRANCH/bin:${PATH:+:$PATH}"
 nim c -d:release -r src/somalier.nim
