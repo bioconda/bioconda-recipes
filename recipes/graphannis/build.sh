2ba23f367522b98cf8e948250a898ea0710a7f73
Thomas Krause <thomaskrause@posteo.de>
2019-06-05 14:11:56 +0200

    Copy the binaries to the prefixed folder

--- recipes/graphannis/build.sh
+++ recipes/graphannis/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 cargo build --release --features "cli c-api"
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/lib
+
+cp ./target/release/annis ${PREFIX}/bin
+cp ./target/release/libgraphannis.so ${PREFIX}/lib
