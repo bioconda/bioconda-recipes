e67cbf6dc69d5190f6b1b2439757635615be760e
Michael Knudsen <micknudsen@gmail.com>
2019-06-05 12:12:04 +0200

    Fixed broken path

--- recipes/somalier/build.sh
+++ recipes/somalier/build.sh
@@ -3,7 +3,7 @@
 export BRANCH=v0.19.0
 export PATH="nim-$BRANCH/bin/nim/bin:${PATH:+:$PATH}"
 export LD=$CC
-bash ./scripts/install.sh
+bash ./scripts/build.sh
 export PATH="nim-$BRANCH/bin:${PATH:+:$PATH}"
 nim c -d:release -r src/somalier.nim
 mkdir -p $PREFIX/bin
