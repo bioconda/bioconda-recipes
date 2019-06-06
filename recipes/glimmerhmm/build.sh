485dbab4b4dda45b80d41982331f1149acd7d02e
zach charlop-powers <zach.charlop.powers@gmail.com>
2019-06-06 12:18:35 -0400

    correct build to use clang on macos

--- recipes/glimmerhmm/build.sh
+++ recipes/glimmerhmm/build.sh
@@ -6,17 +6,25 @@ if [ `uname` == Darwin ]; then
 fi
 
 
-sed -i.bak "s/^CC/#CC/g" sources/makefile
-sed -i.bak "s/^C /#C/g"  train/makefile
-sed -i.bak "s/^CC/#CC/g" train/makefile
+
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/GlimmerHMM/trained_dir
 
 # cd into the sources
-cd sources && make CC=$GXX 
-cd ../train && make C=$C CC=$GXX
-cd ..
+if [ `uname` == Darwin ]; then
+        cd sources && make CC=$CC
+        cd ../train && make C=$C CC=$CC
+        cd ..
+else
+        cd sources && make CC=$GXX 
+        cd ../train && make C=$C CC=$GXX
+        cd ..
+fi
+
+sed -i.bak "s/^CC/#CC/g" sources/makefile
+sed -i.bak "s/^C /#C/g"  train/makefile
+sed -i.bak "s/^CC/#CC/g" train/makefile
 
 #copy the executables	
 cp bin/glimmhmm.pl $PREFIX/bin/.
