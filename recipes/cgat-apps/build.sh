7e5e2cd0292345aa542e51e52a498301aa01ce07
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2019-06-09 13:04:34 +0200

    add more exports to build.sh

--- recipes/cgat-apps/build.sh
+++ recipes/cgat-apps/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# export required env variables
-#export C_INCLUDE_PATH=$PREFIX/include
+# export compiler flags
+export CFLAGS=${CFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
+export CPPFLAGS=${CPPFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
+export LDFLAGS=${LDFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
+export CPATH=${CPATH}" -I${PREFIX}/include -L${PREFIX}/lib"
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
+export LIBRARY_PATH=${LIBRARY_PATH}:${PREFIX}/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib:${PREFIX}/lib/R/lib
 
 # linking htslib, see:
 # http://pysam.readthedocs.org/en/latest/installation.html#external
