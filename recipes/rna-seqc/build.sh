f34bcccc9566d57def44b94fa0f47e63ae4b614b
Stephen Kazakoff <sh.kazakoff@gmail.com>
2019-06-10 05:45:30 +0000

    Prefer building from source

--- recipes/rna-seqc/build.sh
+++ recipes/rna-seqc/build.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 set -eu -o pipefail
 
-gunzip -Nv *.gz
-mkdir -p "${PREFIX}/bin"
-cp rnaseqc "${PREFIX}/bin"
-chmod +x "${PREFIX}/bin/rnaseqc"
+cd ./travis/build/broadinstitute/rnaseqc
+
+pushd SeqLib/bwa
+sed -i.bak '/^DFLAGS=/s/$/ $(LDFLAGS)/' Makefile
+make CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+popd
+
+pushd SeqLib/fermi-lite
+make CC="$CC" CFLAGS="$CFLAGS $LDFLAGS"
+popd
+
+pushd SeqLib/htslib
+make CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+popd
+
+make \
+    CPPFLAGS="-I$PREFIX/include" \
+    SeqLib/lib/libseqlib.a
+
+make \
+    CC="$CXX" \
+    INCLUDE_DIRS="-I$PREFIX/include -ISeqLib -ISeqLib/htslib" \
+    LIBRARY_PATHS="-L$PREFIX/lib -Wl,-rpath $PREFIX/lib"
+
+mkdir -p $PREFIX/bin
+cp rnaseqc $PREFIX/bin
 
