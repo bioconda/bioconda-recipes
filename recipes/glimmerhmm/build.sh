45764868a72ff2ddc11934253dfc6396a97a0029
zach charlop-powers <zach.charlop.powers@gmail.com>
2019-06-11 13:54:39 -0400

    update build as per https://github.com/bioconda/bioconda-recipes/pull/15746

--- recipes/glimmerhmm/build.sh
+++ recipes/glimmerhmm/build.sh
@@ -5,6 +5,15 @@ if [ `uname` == Darwin ]; then
         sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" train/utils.c
 fi
 
+# fix typo in makefile in train
+sed -i.bak "s~^escoreSTOP2:~scoreSTOP2:~g" train/makefile
+sed -i.bak "s~^rfapp:~erfapp:~g" train/makefile
+
+# fix shebang lines in perl scripts
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' train/trainGlimmerHMM
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/glimmhmm.pl
+
+# make directories for storing training data and binaries
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/GlimmerHMM/trained_dir
 
@@ -23,6 +32,21 @@ fi
 cp bin/glimmhmm.pl $PREFIX/bin/.
 cp sources/glimmerhmm $PREFIX/bin/.
 cp train/trainGlimmerHMM $PREFIX/bin/.
+cp train/build-icm $PREFIX/bin/.
+cp train/build-icm-noframe $PREFIX/bin/.
+cp train/build1 $PREFIX/bin/.
+cp train/build2 $PREFIX/bin/.
+cp train/erfapp $PREFIX/bin/.
+cp train/falsecomp $PREFIX/bin/.
+cp train/findsites $PREFIX/bin/.
+cp train/karlin $PREFIX/bin/.
+cp train/score $PREFIX/bin/.
+cp train/score2 $PREFIX/bin/.
+cp train/scoreATG $PREFIX/bin/.
+cp train/scoreATG2 $PREFIX/bin/.
+cp train/scoreSTOP $PREFIX/bin/.
+cp train/scoreSTOP2 $PREFIX/bin/.
+cp train/splicescore $PREFIX/bin/.
 
 # copy the training data
 cp -R trained_dir $PREFIX/GlimmerHMM/
