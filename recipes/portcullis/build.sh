7740a3eacf87183ac5b388108653398122cdcc7e
Luca Venturini <lucventurini@gmail.com>
2019-06-11 10:20:25 +0100

    Added the suggestions by @ npavlovikj

--- recipes/portcullis/build.sh
+++ recipes/portcullis/build.sh
@@ -10,9 +10,11 @@ export CFLAGS="-I$PREFIX/include $CFLAGS"
 export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
 export CXXFLAGS="-I$PREFIX/include $CXXFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LIBS="-lm ${PREFIX}/lib/libz.a"
 
 ./autogen.sh
-./configure --disable-silent-rules --disable-dependency-tracking --prefix=$PREFIX
+#./configure --disable-silent-rules --disable-dependency-tracking --prefix=$PREFIX
+./configure --disable-silent-rules --disable-dependency-tracking --prefix=${PREFIX} --with-boost=${PREFIX} --with-boost-libdir=${PREFIX}/lib
 make V=1
 make V=1 check
 make install
