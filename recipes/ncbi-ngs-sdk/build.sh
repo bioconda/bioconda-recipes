dfadfccd05b2eaf1faa6301fa77f611f28e761fe
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2019-06-12 17:26:18 -0400

    Bumping ncbi-ngs-sdk to version 2.9.6

--- recipes/ncbi-ngs-sdk/build.sh
+++ recipes/ncbi-ngs-sdk/build.sh
@@ -1,4 +1,23 @@
+#!/usr/bin/env bash
+
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 NCBI_OUTDIR=$SRC_DIR/ncbi-outdir
+
+if [[ $OSTYPE == darwin* ]]; then
+     export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+fi
+
+ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc $BUILD_PREFIX/bin/gcc
+ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++ $BUILD_PREFIX/bin/g++
+ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc $BUILD_PREFIX/bin/cc
+ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++ $BUILD_PREFIX/bin/c++
+ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ar $BUILD_PREFIX/bin/ar
+ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $BUILD_PREFIX/bin/ld
+
+export PATH=$BUILD_PREFIX/bin:$PATH
 
 pushd ngs-sdk
 ./configure \
