5cdf3522c7fbac1abafbeda5ca771511b3680381
Vitor C. Piro <vitorpiro@gmail.com>
2019-06-13 16:17:49 +0200

    pass env. variable https_proxy to the setup.sh call

--- bootstrap.py
+++ bootstrap.py
@@ -106,7 +106,7 @@ use_docker = "true"
 if args.no_docker:
     use_docker = "false"
 
-env = {'WORKSPACE': args.bootstrap, 'BOOTSTRAP': "true", 'USE_DOCKER': use_docker, 'PATH': os.environ['PATH']}
+env = {'WORKSPACE': args.bootstrap, 'BOOTSTRAP': "true", 'USE_DOCKER': use_docker, 'PATH': os.environ['PATH'], 'https_proxy': os.environ['https_proxy']}
 sp.check_call(['.circleci/setup.sh'], env=env)
 _write_custom_activate(args.bootstrap)
 
