4f65ad153f95d8a2ca0e1ff3b68e7b217d6de7ab
Jon Palmer <nextgenusfs@gmail.com>
2019-06-16 20:53:23 -0700

    update with most recent changes

--- recipes/glimmerhmm/build.sh
+++ recipes/glimmerhmm/build.sh
@@ -1,51 +1,59 @@
 if [ `uname` == Darwin ]; then
-        sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" sources/util.c
-        sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" sources/oc1.h
-        sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" train/utils.c
+    sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" sources/util.c
+    sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" sources/oc1.h
+    sed -i.bak "s~#include <malloc.h>~#include <malloc/malloc.h>~g" train/utils.c
 fi
 
-# fix typo in makefile in train
-sed -i.bak "s~^escoreSTOP2:~scoreSTOP2:~g" train/makefile
-sed -i.bak "s~^rfapp:~erfapp:~g" train/makefile
+# fix typos in train makefile
+sed -i.bak "s|^escoreSTOP2:|scoreSTOP2:|g" train/makefile
+sed -i.bak "s|^rfapp:|erfapp:|g" train/makefile
+sed -i.bak "s| trainGlimmerHMM||g" train/makefile
+sed -i.bak "s|all:    build-icm|all:    misc.o build-icm.o build-icm-noframe.o build-icm|g" train/makefile
 
-# fix shebang lines in perl scripts
+# fix perl scripts
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' train/trainGlimmerHMM
+sed -i.bak 's|FindBin;|FindBin qw($RealBin);|g' train/trainGlimmerHMM
+sed -i.bak 's|$FindBin::Bin;|"$RealBin/../share/glimmerhmm/train";|g' train/trainGlimmerHMM
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' bin/glimmhmm.pl
 
 # make directories for storing training data and binaries
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/GlimmerHMM/trained_dir
+mkdir -p $PREFIX/share/glimmerhmm
+mkdir -p $PREFIX/share/glimmerhmm/train
 
 # cd into the sources
 if [ `uname` == Darwin ]; then
         cd sources && make CC=$CXX
-        cd ../train && make C=$C CC=$CXX
+        cd ../train && make clean && make all C=$CC CC=$CXX
         cd ..
 else
         cd sources && make CC=$GXX 
-        cd ../train && make C=$C CC=$GXX
+        cd ../train && make clean && make all C=$CC CC=$CXX
         cd ..
 fi
 
-#copy the executables	
+# copy the executables	
 cp bin/glimmhmm.pl $PREFIX/bin/.
 cp sources/glimmerhmm $PREFIX/bin/.
 cp train/trainGlimmerHMM $PREFIX/bin/.
-cp train/build-icm $PREFIX/bin/.
-cp train/build-icm-noframe $PREFIX/bin/.
-cp train/build1 $PREFIX/bin/.
-cp train/build2 $PREFIX/bin/.
-cp train/erfapp $PREFIX/bin/.
-cp train/falsecomp $PREFIX/bin/.
-cp train/findsites $PREFIX/bin/.
-cp train/karlin $PREFIX/bin/.
-cp train/score $PREFIX/bin/.
-cp train/score2 $PREFIX/bin/.
-cp train/scoreATG $PREFIX/bin/.
-cp train/scoreATG2 $PREFIX/bin/.
-cp train/scoreSTOP $PREFIX/bin/.
-cp train/scoreSTOP2 $PREFIX/bin/.
-cp train/splicescore $PREFIX/bin/.
+cp train/build-icm $PREFIX/share/glimmerhmm/train/.
+cp train/build-icm-noframe $PREFIX/share/glimmerhmm/train/.
+cp train/build1 $PREFIX/share/glimmerhmm/train/.
+cp train/build2 $PREFIX/share/glimmerhmm/train/.
+cp train/erfapp $PREFIX/share/glimmerhmm/train/.
+cp train/falsecomp $PREFIX/share/glimmerhmm/train/.
+cp train/findsites $PREFIX/share/glimmerhmm/train/.
+cp train/karlin $PREFIX/share/glimmerhmm/train/.
+cp train/score $PREFIX/share/glimmerhmm/train/.
+cp train/score2 $PREFIX/share/glimmerhmm/train/.
+cp train/scoreATG $PREFIX/share/glimmerhmm/train/.
+cp train/scoreATG2 $PREFIX/share/glimmerhmm/train/.
+cp train/scoreSTOP $PREFIX/share/glimmerhmm/train/.
+cp train/scoreSTOP2 $PREFIX/share/glimmerhmm/train/.
+cp train/splicescore $PREFIX/share/glimmerhmm/train/.
+
+# copy the perl modules
+cp train/*.pm $PREFIX/share/glimmerhmm/train/.
 
 # copy the training data
-cp -R trained_dir $PREFIX/share/GlimmerHMM/
+cp -R trained_dir $PREFIX/share/glimmerhmm/
