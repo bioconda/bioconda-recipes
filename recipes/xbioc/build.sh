cbc5f6240f138e597fc8ca30fa3d1c42168d86d6
Elmar Pruesse <epruesse@users.noreply.github.com>
2019-06-17 18:59:25 -0600

    minor

--- recipes/xbioc/build.sh
+++ recipes/xbioc/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+sed -i.bak '/^Priority: /d' DESCRIPTION
 mkdir -p ~/.R
-echo -e "CC=$CC
+cat > ~/.R/Makevars <<EOF
+CC=$CC
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
+CXX14=$CXX
+EOF
 $R CMD INSTALL --build .
