95d2241b8423c10df9826ce3415413d9c3f029de
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2019-06-18 21:44:16 +0000

    Added and fixed dependencies

--- recipes/magicblast/build.sh
+++ recipes/magicblast/build.sh
@@ -3,5 +3,8 @@ set -euxo pipefail
 
 mkdir -p $PREFIX/bin
 
+# the binary expects libbz1.so.1, but the available one is libbz2.so.1.0
+patchelf --replace-needed libbz2.so.1 libbz2.so.1.0 bin/magicblast
+
 mv bin/magicblast $PREFIX/bin
 
