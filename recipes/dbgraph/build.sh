60a0ee8ccdabd1a8b8bb5232924f01613a9bc0ec
Praveen Kumar <pravs3683@gmail.com>
2019-06-21 13:09:11 -0500

    Update build.sh

--- recipes/dbgraph/build.sh
+++ recipes/dbgraph/build.sh
@@ -2,10 +2,10 @@
 
 mkdir -p $PREFIX/bin
 
-cd $RECIPE_DIR/Graph2Pro
+cd $RECIPE_DIR/graph2pro-var/Graph2Pro
 make clean
 make
 #make check
 
-cp $RECIPE_DIR/Graph2Pro/DBGraph2Pro $PREFIX/bin/DBGraph2Pro
-cp $RECIPE_DIR/Graph2Pro/DBGraphPep2Pro $PREFIX/bin/DBGraphPep2Pro
+cp $RECIPE_DIR/graph2pro-var/Graph2Pro/DBGraph2Pro $PREFIX/bin/DBGraph2Pro
+cp $RECIPE_DIR/graph2pro-var/Graph2Pro/DBGraphPep2Pro $PREFIX/bin/DBGraphPep2Pro
