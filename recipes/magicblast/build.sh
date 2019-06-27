8309cb64e21ba92f6b24d02237d58d0ece5b09fd
Greg Boratyn <boratyng@ncbi.nlm.nih.gov>
2019-06-27 17:39:25 +0000

    Use patchelf only for Linux

--- recipes/magicblast/build.sh
+++ recipes/magicblast/build.sh
@@ -3,8 +3,10 @@ set -euxo pipefail
 
 mkdir -p $PREFIX/bin
 
-# the binary expects libbz1.so.1, but the available one is libbz2.so.1.0
-patchelf --replace-needed libbz2.so.1 libbz2.so.1.0 bin/magicblast
+# the binary expects libbz1.so.1, but the correct name is libbz2.so.1.0
+if [[ $(uname) = Linux ]] ; then
+    patchelf --replace-needed libbz2.so.1 libbz2.so.1.0 bin/magicblast
+fi
 
 mv bin/magicblast $PREFIX/bin
 
