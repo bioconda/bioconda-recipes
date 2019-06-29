a591851723cfdfeb9e214e005039c2e5bb2f42b3
Nick Waters <nickp60@gmail.com>
2019-06-29 17:30:15 +0100

    updates for including spades binaries

--- recipes/riboseed/build.sh
+++ recipes/riboseed/build.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+set -e -o pipefail
 
 $PYTHON setup.py build_ext --inplace --force
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 # Add more build steps here, if they are necessary.
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM-SPAdes3.9.1
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    THISOS = "linux"
+    wget https://github.com/nickp60/riboSeed/raw/subspades/SPAdes-3.9.1-linux.tar.gz
+        # ...
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    THISOS="osx"
+    wget https://github.com/nickp60/riboSeed/raw/subspades/SPAdes-3.9.1-osx.tar.gz
+else
+    echo "No SPAdes3.9.1 binaries available for $OSTYPE"
+    exit 1
+fi
 
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+tar xzf SPAdes-3.9.1-$THISOS.tar.gz
+# stolen from the SPAdes build.sh
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+
+cp -r $THISOS/bin $outdir
+cp -r $THISOS/share $outdir
+
+ln -s $outdir/bin/* $PREFIX/bin
