ae0b36f17ac88d24b3a1ecf63c30c640bf5eceb5
Devon Ryan <dpryan79@gmail.com>
2019-06-30 23:56:36 +0200

    try flags

--- recipes/spades/3.9.1/build.sh
+++ recipes/spades/3.9.1/build.sh
@@ -3,7 +3,9 @@ mkdir build_spades
 pushd build_spades
 cmake -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_C_COMPILER="$CC $CFLAGS" \
-      -DCMAKE_CXX_COMPILER="$CXX $CXXFLAGS" \
+      -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_CFLAGS="$CFLAGS" \
+      -DCMAKE_CXXFLAGS="$CXXFLAGS" \
       ../src
 make install
