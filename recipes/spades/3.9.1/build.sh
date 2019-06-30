14bcd382644752d30e2ea6572c15ff07eb8c8e4d
Devon Ryan <dpryan79@gmail.com>
2019-06-30 23:43:34 +0200

    maybe inject compiler options

--- recipes/spades/3.9.1/build.sh
+++ recipes/spades/3.9.1/build.sh
@@ -3,7 +3,7 @@ mkdir build_spades
 pushd build_spades
 cmake -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_C_COMPILER=$CC \
-      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_COMPILER="$CC $CFLAGS" \
+      -DCMAKE_CXX_COMPILER="$CXX $CXXFLAGS" \
       ../src
 make install
