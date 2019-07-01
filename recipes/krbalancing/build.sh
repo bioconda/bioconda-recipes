591b28a9081813bb3d3412afbb03ab95c0e05e88
Devon Ryan <dpryan79@users.noreply.github.com>
2019-07-01 23:43:26 +0200

    clean up

--- recipes/krbalancing/build.sh
+++ recipes/krbalancing/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/lib/clang/8.0.0/include"
 sed -i.bak  "s#-std=c++11#-std=c++11\", \"-I$BUILD_PREFIX/lib/clang/8.0.0/include#" setup.py
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
