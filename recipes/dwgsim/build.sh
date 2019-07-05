74617057b602cfbe3268502d5c6315fa1a13457f
clintval <valentine.clint@gmail.com>
2019-07-04 19:08:27 -0700

    Samtools pull in YAML and patch, testing...

--- recipes/dwgsim/build.sh
+++ recipes/dwgsim/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-set -e
 
-(git clone http://github.com/samtools/samtools && cd samtools && git checkout 28391e5898804ce6b805016d8c676fdf61442eb3)
-
-export CFLAGS="-I$PREFIX/include"
-sed -i 's/CFLAGS=/CFLAGS+=/' Makefile
-make CC=$CC LIBCURSES=-lncurses CFLAGS=$CFLAGS
+make CC=$CC LIBCURSES=-lncurses 
 
 mkdir -p $PREFIX/bin
 
