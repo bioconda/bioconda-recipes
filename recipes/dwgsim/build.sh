69428d5f9053dde9abe4e9b5badf69e5f0a6f414
clintval <valentine.clint@gmail.com>
2019-07-04 18:46:43 -0700

    Append to CLAGS, testing...

--- recipes/dwgsim/build.sh
+++ recipes/dwgsim/build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-export CFLAGS="-I$PREFIX/include"
-
 (git clone http://github.com/samtools/samtools && cd samtools && git checkout 28391e5898804ce6b805016d8c676fdf61442eb3)
 
-sed -i.bak 's/CFLAGS=/CFLAGS +=/' Makefile
-
+export CFLAGS="-I$PREFIX/include"
+sed -i 's/CFLAGS=/CFLAGS+=/' Makefile
 make CC=$CC LIBCURSES=-lncurses CFLAGS=$CFLAGS
 
 mkdir -p $PREFIX/bin
 
 cp dwgsim $PREFIX/bin
 cp dwgsim_eval $PREFIX/bin
-
