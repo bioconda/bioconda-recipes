611a164e74023de628f66c516a7219432ea1e11e
PengJia6 <pengjia1110@gmail.com>
2019-07-07 23:19:39 +0800

    add msisensor-pro

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -3,6 +3,7 @@ export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
 export CPPFLAGS="-I$PREFIX/include"
-apt-get install gcc
-make
+#apt-get install gcc
+echo $CC $CXX
+make 
 cp msisensor-pro $PREFIX/bin
