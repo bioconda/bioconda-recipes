72e351254a50aba03d6e255dc3291de5db68678d
PengJia6 <pengjia1110@gmail.com>
2019-07-07 22:22:50 +0800

    add msisensor-pro

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -2,5 +2,7 @@ export CPATH=${PREFIX}/include
 export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
+export CPPFLAGS="-I$PREFIX/include"
+
 make
 cp msisensor-pro $PREFIX/bin
