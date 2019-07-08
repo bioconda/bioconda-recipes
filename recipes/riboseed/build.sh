502e8fc013c70ff647cb89b597ae5e6953b24be6
Nick Waters <nickp60@gmail.com>
2019-07-08 13:44:50 +0100

    update version, pin to SPAdes3.9.1, when its available

--- recipes/riboseed/build.sh
+++ recipes/riboseed/build.sh
@@ -3,26 +3,3 @@ set -e -o pipefail
 
 $PYTHON setup.py build_ext --inplace --force
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-# Add more build steps here, if they are necessary.
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM-SPAdes3.9.1
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    THISOS="linux"
-    curl -LO https://github.com/nickp60/riboSeed/raw/subspades/SPAdes-3.9.1-linux.tar.gz
-        # ...
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    THISOS="osx"
-    curl -LO https://github.com/nickp60/riboSeed/raw/subspades/SPAdes-3.9.1-osx.tar.gz
-else
-    echo "No SPAdes3.9.1 binaries available for $OSTYPE"
-    exit 1
-fi
-
-tar xzf SPAdes-3.9.1-$THISOS.tar.gz
-# stolen from the SPAdes build.sh
-mkdir -p $outdir
-mkdir -p $PREFIX/bin
-
-cp -r $THISOS/bin $outdir
-cp -r $THISOS/share $outdir
-
-ln -s $outdir/bin/* $PREFIX/bin
