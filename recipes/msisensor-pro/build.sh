e28e1a5eeaf919caca1f75699c39df78b59bffd2
PengJia6 <pengjia1110@gmail.com>
2019-07-09 20:36:35 +0800

    add msisensor-pro

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -1,12 +1,12 @@
 export CPATH=${PREFIX}/include
 export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
-export LDFLAGS+="-L$PREFIX/lib -ltinfo -lz"
+export LDFLAGS="-L$PREFIX/lib -ltinfo -lz"
 export LIBRARY_PATH=${PREFIX}/lib
 
 #export CPPFLAGS="-I$PREFIX/include"
 #export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 #export CFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
-export LDFLAGS+="-L$PREFIX/lib -ltinfo -lz"
+#export LDFLAGS+="-L$PREFIX/lib -ltinfo -lz"
 #apt-get install gcc
 #echo $CC $CXX
 
