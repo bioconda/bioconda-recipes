fff77e67110d7dad9ba6714e513ebdee805819ad
PengJia6 <pengjia1110@gmail.com>
2019-07-09 21:07:01 +0800

    xxxtt

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -3,4 +3,4 @@ export INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
 make
-cp msisensor $PREFIX/bin
+cp msisensor-pro $PREFIX/bin
