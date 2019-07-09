7c2cc886b868d7831b7e37c1ceb1c0f29369d528
Antônio Pedro Camargo <apcamargo@users.noreply.github.com>
2019-07-09 19:13:53 -0300

    Use DCMAKE_INSTALL_PREFIX

--- recipes/megahit/build.sh
+++ recipes/megahit/build.sh
@@ -7,18 +7,7 @@ rm -rf build
 mkdir -p build
 cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
 make -j $CPU_COUNT
 
-mv megahit $PREFIX/bin
-mv megahit_core $PREFIX/bin
-mv megahit_core_no_hw_accel $PREFIX/bin
-
-
-#mv megahit $PREFIX/bin
-#mv megahit_asm_core $PREFIX/bin
-#mv megahit_sdbg_build $PREFIX/bin
-#mv megahit_toolkit $PREFIX/bin
-
-#mkdir -p $PREFIX/share/megahit
-#mv example $PREFIX/share/megahit
+make install
