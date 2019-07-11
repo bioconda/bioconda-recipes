b33180c5d7517b619622a18c040ecfa1480fb534
xenograft <xenograft@dfast.nig.ac.jp>
2019-07-11 15:39:47 +0900

    post-line, pre-unlink added

--- recipes/dfast/build.sh
+++ recipes/dfast/build.sh
@@ -1,18 +1,34 @@
 #!/bin/sh
 
 APPROOT=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+
+rm bin/*/hmm*
+rm bin/*/ghost*
+rm -r bin/Darwin/lib  # libs for ghostx (MacOS)
+
+rm bin/*/aragorn
+# rm bin/*/barrnap
+# rm -r bin/Darwin/barrnap-0.8
+
+# rm bin/*/CRT
+# rm bin/common/CRT1.2-CLI.jar
+
+# rm bin/*/mga
+
+# rm bin/*/lastal
+# rm bin/*/lastdb
+
+# rm bin/*/blastp
+# rm bin/*/blastdbcmd
+# rm bin/*/rpsblast
+# rm bin/*/makeblastdb
+
 mkdir -p $APPROOT
-# rm bin/Darwin/lib/libgomp.1.dylib
-# rm bin/Darwin/lib/libgcc_s.1.dylib
-# rm -r bin/Darwin/lib
-rm -r bin/*
+
 cp -r ./* $APPROOT
 
-
-# $APPROOT/scripts/file_downloader.py --protein dfast --cdd Cog --hmm TIGR
-
-echo "Debug ln -s ${APPROOT}/dfast ${PREFIX}/bin/"
 ln -s ${APPROOT}/dfast ${PREFIX}/bin/
-ln -s ${APPROOT}/scripts/file_downloader.py ${PREFIX}/bin/
+ln -s ${APPROOT}/scripts/file_downloader.py ${PREFIX}/bin/dfast_file_downloader.py
+ln -s ${APPROOT}/scripts/file_downloader.py ${PREFIX}/bin
 
 
