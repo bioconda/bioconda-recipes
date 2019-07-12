3d72fcc456a6a68552a10f59a41ef7a1e9433180
Derek Croote <dcroote@users.noreply.github.com>
2019-07-12 12:05:36 -0700

    Update BASIC to v1.5.1 (#16349)
    Package uploads complete: [ci skip]
     - biocontainers/basic:1.5.1--py_0
     - noarch/basic-1.5.1-py_0.tar.bz2

--- recipes/basic/build.sh
+++ recipes/basic/build.sh
@@ -7,9 +7,3 @@ cp -rf db/* $PREFIX/db/basic/
 # replace /db/ path with /../db/basic/
 sed 's;"db");"../db/basic");g' BASIC.py > $PREFIX/bin/BASIC.py
 chmod +x $PREFIX/bin/BASIC.py
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
