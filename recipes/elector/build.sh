02425162d1270cf8cfbbc29885851bf7adbb8d09
Marijon Pierre <pierre.marijon@inria.fr>
2019-07-16 14:15:36 +0200

    [ELECTOR] set makefile to use env CC and CXX

--- recipes/elector/build.sh
+++ recipes/elector/build.sh
@@ -5,18 +5,22 @@ set -x
 mkdir -p $PREFIX/bin/
 # build and install binary tools
 cd src/simulator
+sed -i "s#CC=g++#CC=${CXX}#" makefile
 make
 cp simulator $PREFIX/bin/
 
 cd ../poa-graph
+sed -i "s#CC = gcc#CC=${CC}#" Makefile
 make poa
 cp poa $PREFIX/bin/
 
 cd ../utils
+sed -i "s#CC=g++#CC=${CXX}#" makefile
 make
 cp fq2fa $PREFIX/bin/
 
 cd ../split
+sed -i "s#CC=g++#CC=${CXX}#" makefile
 make
 cp masterSplitter $PREFIX/bin/
 cp Donatello $PREFIX/bin/
