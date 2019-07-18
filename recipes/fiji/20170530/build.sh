d2ad03ab9ed26612433f18b29515536a2acae5c7
Greg Von Kuster <ghv2@psu.edu>
2019-07-18 11:01:32 -0400

    Bump fiji to latest lifeline version

---
+++ recipes/fiji/20170530/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu -o pipefail
+
+sharedir=$PREFIX/share
+outdir=$sharedir/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+
+unamestr=`uname`
+if [ "$unamestr" == 'Linux' ];
+then
+    cp -R $SRC_DIR/* $sharedir
+    # Provide a link to the Linux executable from $PREFIX/bin.
+    ln -s $sharedir/ImageJ-linux64 $PREFIX/bin/ImageJ
+elif [ "$unamestr" == 'Darwin' ];
+then
+    hdiutil attach fiji-macosx-20170530.dmg
+    cp -R /Volumes/Fiji/Fiji.app/ $sharedir
+    hdiutil detach /Volumes/Fiji
+    # Eliminate problematic java3d for osx since we do not use it anyway.
+    rm -rf $sharedir/java
+    # Provide a link to the osx executable from $PREFIX/bin.
+    ln -s $sharedir/Contents/MacOS/ImageJ-macosx $PREFIX/bin/ImageJ
+fi
+
+chmod +x "${PREFIX}/bin/ImageJ"
