b6d269cf06b7bd90649b840ccdf406f93ea54e2f
Bjoern Gruening <bjoern.gruening@gmail.com>
2019-07-20 14:26:49 +0200

    fix location of jellyfish library

--- recipes/jellyfish/1.1.12/build.sh
+++ recipes/jellyfish/1.1.12/build.sh
@@ -4,3 +4,8 @@ autoreconf -fi
 ./configure --prefix=$PREFIX
 make
 make install
+
+ls -l ${PREFIX}/lib/
+
+mv ${PREFIX}/include/jellyfish-1.1.12/jellyfish ${PREFIX}/lib/
+rmdir ${PREFIX}/include/jellyfish-1.1.12/
