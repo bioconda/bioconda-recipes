5915660560612d01ba8cd97836b69f7ca142c811
William L. Close <close.will@gmail.com>
2019-07-21 13:00:03 -0400

    updating options in mothur Makefile to allow full functionality

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -1,7 +1,27 @@
 # the flags the new gxx activate script sets breaks the build, so unset them here
-unset CXXFLAGS
-export CXXFLAGS="-I${PREFIX}/include"
-mkdir -pv $PREFIX/bin/blast/bin
+# unset CXXFLAGS
+# export CXXFLAGS="-I${PREFIX}/include"
+
+### Configuring settings within mothur Makefile
+# Enabling dependencies for full functionality
+sed 's;\(USEBOOST \?\=\) no;\1 yes;' Makefile
+sed 's;\(USEHDF5 \?\=\) no;\1 yes;' Makefile
+sed 's;\(USEGSL \?\=\) no;\1 yes;' Makefile
+# Specifying dependency locations
+sed 's;\(BOOST_LIBRARY_DIR \?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
+sed 's;\(BOOST_INCLUDE_DIR \?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/boost\\\"\";' Makefile
+sed 's;\(HDF5_LIBRARY_DIR \?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
+sed 's;\(HDF5_INCLUDE_DIR \?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include\\\"\";' Makefile
+sed 's;\(GSL_LIBRARY_DIR \?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
+sed 's;\(GSL_INCLUDE_DIR \?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/gsl\\\"\";' Makefile
+
+
+
+
+
+
+
+mkdir -pv "${PREFIX}"/bin/blast/bin
 sed -i 's/g++/$CXX/g' source/uchime_src/mk
 make -j 2
 cp {mothur,uchime} $PREFIX/bin
