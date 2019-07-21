2b12693f0bfacb2819e7e94208d5086563dc955b
William L. Close <close.will@gmail.com>
2019-07-21 17:59:24 -0400

    fixing build script by updating mothur Makefile options before compiling

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -4,25 +4,29 @@
 
 ### Configuring settings within mothur Makefile
 # Enabling dependencies for full functionality
-sed 's;\(USEBOOST \?\=\) no;\1 yes;' Makefile
-sed 's;\(USEHDF5 \?\=\) no;\1 yes;' Makefile
-sed 's;\(USEGSL \?\=\) no;\1 yes;' Makefile
+sed -i 's;\(USEBOOST \?\=\) no;\1 yes;' Makefile
+sed -i 's;\(USEHDF5 \?\=\) no;\1 yes;' Makefile
+sed -i 's;\(USEGSL \?\=\) no;\1 yes;' Makefile
 # Specifying dependency locations
-sed 's;\(BOOST_LIBRARY_DIR \?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
-sed 's;\(BOOST_INCLUDE_DIR \?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/boost\\\"\";' Makefile
-sed 's;\(HDF5_LIBRARY_DIR \?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
-sed 's;\(HDF5_INCLUDE_DIR \?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include\\\"\";' Makefile
-sed 's;\(GSL_LIBRARY_DIR \?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
-sed 's;\(GSL_INCLUDE_DIR \?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/gsl\\\"\";' Makefile
+sed -i 's;\(BOOST_LIBRARY_DIR \?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
+sed -i 's;\(BOOST_INCLUDE_DIR \?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/boost\\\"\";' Makefile
+sed -i 's;\(HDF5_LIBRARY_DIR \?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
+sed -i 's;\(HDF5_INCLUDE_DIR \?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include\\\"\";' Makefile
+sed -i 's;\(GSL_LIBRARY_DIR \?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
+sed -i 's;\(GSL_INCLUDE_DIR \?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/gsl\\\"\";' Makefile
 
-
-
-
-
-
-
-mkdir -pv "${PREFIX}"/bin/blast/bin
+### Reconfiguring compiler for uchime Makefile
 sed -i 's/g++/$CXX/g' source/uchime_src/mk
-make -j 2
-cp {mothur,uchime} $PREFIX/bin
-ln -s $PREFIX/bin/{bl2seq,formatdb,blastall,megablast} $PREFIX/bin/blast/bin/
+
+### Compiling programs
+make
+
+### Cleaning up
+make clean
+
+# Copy executables to bin
+cp {mothur,uchime} "${PREFIX}"/bin/
+
+### Placing BLAST binaries in location required by mothur
+mkdir -pv "${PREFIX}"/bin/blast/bin/
+ln -s "${PREFIX}"/bin/{blastall,formatdb,megablast} "${PREFIX}"/bin/blast/bin/
