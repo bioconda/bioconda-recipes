d1e3216ff279909ffd81391447bb0583fc25b678
belle.carrell <belle.carrell@gmail.com>
2019-07-22 16:09:54 -0400

    source activate root

--- recipes/sesimcmc/build.sh
+++ recipes/sesimcmc/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 cd src/
-mkdir -p $PREFIX/bin
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+source activate root
 
 make
