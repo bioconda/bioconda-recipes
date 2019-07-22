0ef2c32b2c0f89efda393e6ee37876bcfdf56971
William L. Close <close.will@gmail.com>
2019-07-22 14:27:29 -0400

    fixing sed portability issues

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -1,18 +1,18 @@
 ### Configuring settings within mothur Makefile
 # Enabling dependencies for full functionality
-sed -i 's;\(USEBOOST \?\=\) no;\1 yes;' Makefile
-sed -i 's;\(USEHDF5 \?\=\) no;\1 yes;' Makefile
-sed -i 's;\(USEGSL \?\=\) no;\1 yes;' Makefile
+sed 's;\(USEBOOST \?\=\) no;\1 yes;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(USEHDF5 \?\=\) no;\1 yes;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(USEGSL \?\=\) no;\1 yes;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 # Specifying dependency locations
-sed -i 's;\(BOOST_LIBRARY_DIR \?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
-sed -i 's;\(BOOST_INCLUDE_DIR \?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/boost\\\"\";' Makefile
-sed -i 's;\(HDF5_LIBRARY_DIR \?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
-sed -i 's;\(HDF5_INCLUDE_DIR \?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include\\\"\";' Makefile
-sed -i 's;\(GSL_LIBRARY_DIR \?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile
-sed -i 's;\(GSL_INCLUDE_DIR \?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/gsl\\\"\";' Makefile
+sed 's;\(BOOST_LIBRARY_DIR \?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(BOOST_INCLUDE_DIR \?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/boost\\\"\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(HDF5_LIBRARY_DIR \?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(HDF5_INCLUDE_DIR \?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include\\\"\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(GSL_LIBRARY_DIR \?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/lib\\\"\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(GSL_INCLUDE_DIR \?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"\\\"'"${PREFIX}"'\/include/gsl\\\"\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 
 ### Reconfiguring compiler for uchime Makefile
-sed -i 's/g++/$CXX/g' source/uchime_src/mk
+sed 's/g++/$CXX/g' source/uchime_src/mk > source/uchime_src/mk.tmp && mv source/uchime_src/mk.tmp source/uchime_src/mk
 
 ### Cleaning before compiling (just in case)
 make clean
@@ -24,6 +24,6 @@ make
 # Copy mothur and uchime executables to bin
 cp {mothur,uchime} "${PREFIX}"/bin/
 
-# Placing BLAST binaries in default location for mothur
+# Linking BLAST binaries to default location for mothur
 mkdir -pv "${PREFIX}"/bin/blast/bin/
 ln -s "${PREFIX}"/bin/{blastall,formatdb,megablast} "${PREFIX}"/bin/blast/bin/
