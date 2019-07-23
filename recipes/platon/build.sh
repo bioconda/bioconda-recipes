4ba009b982782941337d8e4cd208d1d2b1941dd6
Oliver Schwengers <oliver@schwengers.de>
2019-07-23 23:04:29 +0200

    remove db download from build script

--- recipes/platon/build.sh
+++ recipes/platon/build.sh
@@ -3,10 +3,5 @@ set -e
 
 # install PLATON via pip
 $PYTHON -m pip install --no-deps --ignore-installed --no-cache-dir .
+	
 
-# downloadn, extract and install custom database
-wget https://s3.computational.bio.uni-giessen.de/swift/v1/platon/db.tar.gz
-tar -xzf db.tar.gz
-
-mkdir -p $PREFIX/db
-mv db/* $PREFIX/db/
