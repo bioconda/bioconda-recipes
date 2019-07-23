5f66e97f609c5b14b517adb16b236bdb11067f1a
Jin Li <lijin.abc@gmail.com>
2019-07-23 15:16:25 -0500

    add moabs

---
+++ recipes/moabs/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CPATH="${PREFIX}/include"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib -lstdc++"
+export LIBRARY_PATH="${PREFIX}/lib"
+export BOOSTINC="${PREFIX}/include"
+export BOOSTLIB="${PREFIX}/lib"
+
+make
+make install
+mkdir -p ${PREFIX}
+cp -R ${SRC_DIR}/bin/* ${PREFIX}/bin
