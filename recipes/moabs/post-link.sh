fd8a7b334e2a179e046038c62c710e986b07c992
Jin Li <lijin.abc@gmail.com>
2019-07-25 12:39:07 -0500

    post-link.sh -> pre-link.sh

--- recipes/moabs/post-link.sh
+++ recipes/moabs/post-link.sh
@@ -6,10 +6,8 @@ local url=$2
 local sha256=$3
 
 echo "$PREFIX" >> "$PREFIX/.messages.txt"
-
-mkdir -p "$(dirname "$f")"
-wget -q -O "$f" "$url"
-
+echo wget -q -O "$f" "$url" >> "$PREFIX/.messages.txt"
+curl -s "$url" > "$f"
 if [ -f "$f" ]
 then
 	SUCCESS=0
