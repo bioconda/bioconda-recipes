85ceaf085d509ba0ae9694e59e0182d939ee22be
Jin Li <lijin.abc@gmail.com>
2019-07-25 12:06:10 -0500

    Fix wget fails in PREFIX=/usr/local/bin

--- recipes/moabs/post-link.sh
+++ recipes/moabs/post-link.sh
@@ -4,35 +4,31 @@ function cmd {
 local f=$PREFIX/bin/$1
 local url=$2
 local sha256=$3
-date >> "$PREFIX/.messages.txt"
-ls "$f" "$url" "$sha256" >> "$PREFIX/.messages.txt"
-which wget >> "$PREFIX/.messages.txt"
-echo wget -q -O "$f" "$url" >> "$PREFIX/.messages.txt"
+wget -q -O "$f" "$url"
+if [ -f "$f" ]
+then
+	SUCCESS=0
+	if [[ $(uname -s) == "Linux" ]]; then
+		which sha256sum >> "$PREFIX/.messages.txt"
+		sha256sum "$f" >> "$PREFIX/.messages.txt"
+		sha256sum --quiet -c <<< "$sha256  $f"
+		if (($?==0))
+		then
+			SUCCESS=1
+		fi
+	else if [[ $(uname -s) == "Darwin" ]]; then
+		which shasum >> "$PREFIX/.messages.txt"
+		shasum -a 256 "$f" >> "$PREFIX/.messages.txt"
+		if [[ $(shasum -a 256 $f | awk '{ print $1 }') == "$sha256" ]]; then
+			SUCCESS=1
+		fi
+	fi
 
-# wget -q -O "$f" "$url"
-# ls -lh "$f" >> "$PREFIX/.messages.txt"
-
-# SUCCESS=0
-# if [[ $(uname -s) == "Linux" ]]; then
-# 	which sha256sum >> "$PREFIX/.messages.txt"
-# 	sha256sum "$f" >> "$PREFIX/.messages.txt"
-# 	sha256sum --quiet -c <<< "$sha256  $f"
-# 	if (($?==0))
-# 	then
-# 		SUCCESS=1
-# 	fi
-# else if [[ $(uname -s) == "Darwin" ]]; then
-# 	which shasum >> "$PREFIX/.messages.txt"
-# 	shasum -a 256 "$f" >> "$PREFIX/.messages.txt"
-# 	if [[ $(shasum -a 256 $f | awk '{ print $1 }') == "$sha256" ]]; then
-# 		SUCCESS=1
-# 	fi
-# fi
-# 
-# if [[ $SUCCESS != 1 ]]; then
-# 	echo "ERROR: post-link.sh was unable to download $f with the sha256 $sha256 from $url." >> "$PREFIX/.messages.txt"
-# 	exit -1
-# fi
+	if [[ $SUCCESS != 1 ]]; then
+		echo "ERROR: post-link.sh was unable to download $f with the sha256 $sha256 from $url." >> "$PREFIX/.messages.txt"
+		exit -1
+	fi
+fi
 }
 
 cmd lut_fet.dat 'https://ndownloader.figshare.com/files/16527371?private_link=44c546b05dd9fa0aee3d' 2f9099e79d6a23764b51220362634acd7412a025464001717ae58acca24a8eb3
