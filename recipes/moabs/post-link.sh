7321c6d8269c0f3087d9eda2d9745d659083389a
Jin Li <lijin.abc@gmail.com>
2019-07-24 20:24:19 -0500

    moabs: fix url for wget in post-link.sh

--- recipes/moabs/post-link.sh
+++ recipes/moabs/post-link.sh
@@ -14,15 +14,16 @@ then
 fi
 }
 
-cmd lut_fet.dat \
-	'https://ndownloader.figshare.com/files/16527371?private_link=44c546b05dd9fa0aee3d' \
+cmd \
+	lut_fet.dat \
+	https://ndownloader.figshare.com/files/16527371?private_link=44c546b05dd9fa0aee3d \
 	2f9099e79d6a23764b51220362634acd7412a025464001717ae58acca24a8eb3
-
-cmd lut_pdiffCI.dat \
-	'https://ndownloader.figshare.com/files/16527389?private_link=44c546b05dd9fa0aee3d' \
+cmd \
+	lut_pdiffCI.dat \
+	https://ndownloader.figshare.com/files/16527389?private_link=44c546b05dd9fa0aee3d \
 	98cd92911c9a73267129cb010b1186db7cd248267b58614cc836f78db331902f
-
-cmd lut_pdiffInRegion.dat \
-	'https://ndownloader.figshare.com/files/16527443?private_link=44c546b05dd9fa0aee3d' \
+cmd \
+	lut_pdiffInRegion.dat \
+	https://ndownloader.figshare.com/files/16527443?private_link=44c546b05dd9fa0aee3d \
 	6410f059842c11dc62e9452f36f6869edda25d44afc2bd51f9b495c4c3c128e4
 
