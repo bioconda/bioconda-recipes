567a072fc27daf9552784a9f406ac0b91e665fba
Jin Li <lijin.abc@gmail.com>
2019-07-25 16:35:31 -0500

    post-link.sh: add sha256sum check

--- recipes/moabs/post-link.sh
+++ recipes/moabs/post-link.sh
@@ -8,9 +8,28 @@ local f=$PREFIX/bin/$1
 local url=$2
 echo wget -q -O "$f" "$url" >> "$PREFIX/.messages.txt"
 wget -q -O "$f" "$url"
+SUCCESS=0
+if [[ $(uname -s) == "Linux" ]]; then
+	sha256sum "$f" >> "$PREFIX/.messages.txt"
+	sha256sum --quiet -c <<< "$sha256  $f"
+	if (($?==0))
+	then
+		SUCCESS=1
+	fi
+else if [[ $(uname -s) == "Darwin" ]]; then
+	shasum -a 256 "$f" >> "$PREFIX/.messages.txt"
+	if [[ $(shasum -a 256 "$f" | awk '{ print $1 }') == "$sha256" ]]; then
+		SUCCESS=1
+	fi
+fi
+
+if [[ $SUCCESS != 1 ]]; then
+	echo "ERROR: post-link.sh was unable to download $f with the sha256 $sha256 from $url." >> "$PREFIX/.messages.txt"
+	exit -1
+fi
 }
 
-cmd lut_fet.dat 'https://ndownloader.figshare.com/files/16527371?private_link=44c546b05dd9fa0aee3d'
+cmd lut_fet.dat 'https://ndownloader.figshare.com/files/16527371?private_link=44c546b05dd9fa0aee3d' 2f9099e79d6a23764b51220362634acd7412a025464001717ae58acca24a8eb3
 
 # function cmd {
 # local f=$PREFIX/bin/$1
