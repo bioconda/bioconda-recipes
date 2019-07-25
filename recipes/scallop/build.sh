2b0dcb3cd1247b68f1d276284c4a80d6d7148727
Mingfu Shao <mxs2589@cse-mmsms01.local>
2019-07-25 18:47:08 -0400

    update

--- recipes/scallop/build.sh
+++ recipes/scallop/build.sh
@@ -2,8 +2,7 @@
 
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export LIBS="$LIBS -lhts -lboost"
 
 ./configure --prefix=$PREFIX
-make
+make LIBS+=-lhts LIBS+=-lboost
 make install
