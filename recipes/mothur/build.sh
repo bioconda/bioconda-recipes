7791099d89eb1067b7a2d894b737a026eb68283a
William L. Close <close.will@gmail.com>
2019-07-26 21:12:18 -0400

    compiling boost and hdf5 per mothur INSTALL.md

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -1,25 +1,32 @@
+### Installing and configuring Boost libraries
+tar -xzvf boost*tar.gz && rm boost*tar.gz
+cd boost*
+./bootstrap.sh --prefix=${PREFIX}
+./b2 install
+cd ../
+
+
+### Installing and configuring HDF5 libraries
+tar -xzvf hdf5*tar.gz && rm hdf5*tar.gz
+cd hdf5*
+./configure --prefix=${PREFIX} --enable-cxx --enable-static --disable-shared
+make check
+make install
+cd ../
+
+
 ### Configuring settings within mothur Makefile
 # Enabling dependencies for full functionality
 sed 's;\(USEBOOST ?\=\) no;\1 yes;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 sed 's;\(USEHDF5 ?\=\) no;\1 yes;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 sed 's;\(USEGSL ?\=\) no;\1 yes;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 # Specifying dependency locations
-sed 's;\(BOOST_LIBRARY_DIR ?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"'"${BUILD_PREFIX}"'\/lib/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-sed 's;\(BOOST_INCLUDE_DIR ?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"'"${BUILD_PREFIX}"'\/include/boost/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-sed 's;\(HDF5_LIBRARY_DIR ?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"'"${BUILD_PREFIX}"'\/lib/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-sed 's;\(HDF5_INCLUDE_DIR ?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"'"${BUILD_PREFIX}"'\/include/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-sed 's;\(GSL_LIBRARY_DIR ?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"'"${BUILD_PREFIX}"'\/lib/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-sed 's;\(GSL_INCLUDE_DIR ?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"'"${BUILD_PREFIX}"'\/include/gsl/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-# Fixing link errors for HDF5 libraries by replacing dynamic libraries with static libraries
-sed 's;\(LDFLAGS += -L ${HDF5_LIBRARY_DIR}\) -lhdf5 -lhdf5_cpp;\1;' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
-sed '/^LDFLAGS += -L ${HDF5_LIBRARY_DIR}$/r'<(
-	echo ""
-    echo "LIBS += \${HDF5_LIBRARY_DIR}/libhdf5_hl_cpp.a"
-    echo "LIBS += \${HDF5_LIBRARY_DIR}/libhdf5_cpp.a"
-    echo "LIBS += \${HDF5_LIBRARY_DIR}/libhdf5_hl.a"
-    echo "LIBS += \${HDF5_LIBRARY_DIR}/libhdf5.a"
-    echo ""
-) Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(BOOST_LIBRARY_DIR ?\=\) \"\\\"Enter_your_boost_library_path_here\\\"\";\1 \"'"${PREFIX}"'\/lib/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(BOOST_INCLUDE_DIR ?\=\) \"\\\"Enter_your_boost_include_path_here\\\"\";\1 \"'"${PREFIX}"'\/include/boost/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(HDF5_LIBRARY_DIR ?\=\) \"\\\"Enter_your_HDF5_library_path_here\\\"\";\1 \"'"${PREFIX}"'\/lib/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(HDF5_INCLUDE_DIR ?\=\) \"\\\"Enter_your_HDF5_include_path_here\\\"\";\1 \"'"${PREFIX}"'\/include/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(GSL_LIBRARY_DIR ?\=\) \"\\\"Enter_your_GSL_library_path_here\\\"\";\1 \"'"${PREFIX}"'\/lib/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's;\(GSL_INCLUDE_DIR ?\=\) \"\\\"Enter_your_GSL_include_path_here\\\"\";\1 \"'"${PREFIX}"'\/include/gsl/\";' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 
 
 ### Reconfiguring compiler for uchime Makefile
