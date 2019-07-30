21ea58f51017880aa052b80308a145551514f338
jraysajulga <jraysajulga@gmail.com>
2019-07-30 14:51:11 -0500

    add recursive flag and sudo options

--- recipes/pepquery/build.sh
+++ recipes/pepquery/build.sh
@@ -2,5 +2,8 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/bin/logs
 cp -R * $PREFIX/bin/
 cp -R logs/ $PREFIX/bin/
-chmod 775 $PREFIX/bin/logs
+chmod 775 -R $PREFIX/bin/logs
+chmod 775 -R $USER $PREFIX/bin/logs
+sudo chmod 775 -R $PREFIX/bin/logs
+sudo chmod 775 -R $USER $PREFIX/bin/logs
 
