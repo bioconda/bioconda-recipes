088d0d58729d884fe03cb868d83a0d6dfce59449
jraysajulga <jraysajulga@gmail.com>
2019-07-30 18:03:19 -0500

    delete build.sh

--- recipes/pepquery/build.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/bin/logs
-cp -R * $PREFIX/bin/
-cp -R logs/ $PREFIX/bin/
-chmod 775 -R $PREFIX/bin/logs
-chmod 775 -R $USER $PREFIX/bin/logs
-sudo chmod 775 -R $PREFIX/bin/logs
-sudo chmod 775 -R $USER $PREFIX/bin/logs
-
