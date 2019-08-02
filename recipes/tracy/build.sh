78b762fcb7eed4c7614368adf343f83b458891c6
Tobias Rausch <rauschtobi@gmail.com>
2019-08-02 21:36:46 +0200

    include dir

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -4,6 +4,7 @@ rmdir ${SRC_DIR}/tracy/src/xxsds/
 mv ${SRC_DIR}/sdsl-lite ${SRC_DIR}/tracy/src/xxsds
 cd ${SRC_DIR}/tracy
 
-make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS"
+export SDSL_ROOT=${SRC_DIR}/tracy/src/sdslLite/
+make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/tracy/src/sdslLite/include"
 mkdir -p $PREFIX/bin
 cp src/tracy $PREFIX/bin
