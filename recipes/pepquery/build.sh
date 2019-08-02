26d64481b5114cdbc08f1c15834615c1ec5fc7c9
jraysajulga <jraysajulga@gmail.com>
2019-08-02 11:56:56 -0500

    account for logs/ permission

--- recipes/pepquery/build.sh
+++ recipes/pepquery/build.sh
@@ -10,5 +10,8 @@ cat <<END >$PEPQUERY_DIR/pepquery
 JAR_PATH=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 java ${JAVA_OPTS:-} -jar \$JAR_PATH/pepquery.jar \$*
 END
+mkdir $PEPQUERY_DIR/logs
+touch $PEPQUERY_DIR/logs/log4j.properties
+chmod -R a+x $PEPQUERY_DIR/logs
 chmod a+x $PEPQUERY_DIR/pepquery
 ln -s $PEPQUERY_DIR/pepquery $PREFIX/bin
