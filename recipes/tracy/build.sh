1b53db6a753115da8f3b3b16bb802aa7d120dcb8
Tobias Rausch <rauschtobi@gmail.com>
2019-08-02 21:56:17 +0200

    xxsds

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-rmdir ${SRC_DIR}/src/xxsds/
-mv ${SRC_DIR}/sdsl-lite ${SRC_DIR}/src/xxsds
-
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include"
 mkdir -p $PREFIX/bin
 cp src/tracy $PREFIX/bin
