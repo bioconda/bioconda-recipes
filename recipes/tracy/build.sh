0306fc7375f5a0bad32bd6fbde713a0bbac4e531
Tobias Rausch <rauschtobi@gmail.com>
2019-08-02 13:06:12 +0200

    xxsds

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-cd ${SRC_DIR}
-ls
-git submodule init
-git submodule update --init --recursive
+mv ${SRC_DIR}/sdsl-lite ${SRC_DIR}/tracy/src/xxsds
+cd ${SRC_DIR}/tracy
+ls -lhR
 
 make all CXX=$CXX
 mkdir -p $PREFIX/bin
