98837d195b570ecc3d6e092accc9a878b9cfebb5
Sebastian Will <swill@csail.mit.edu>
2019-08-03 16:03:01 +0200

    add fortran build dependency

--- recipes/treekin/build.sh
+++ recipes/treekin/build.sh
@@ -2,6 +2,8 @@
 
 export CXXFLAGS="${CXXFLAGS/-std=c++17/-std=gnu++17} -Wno-unused-result"
 
+autoreconf -i \
+&& \
 PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH \
   ./configure --prefix=$PREFIX && \
 make ${CPUS_COUNT} && \
