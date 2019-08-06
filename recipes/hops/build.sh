868acbc29b47f44238b51561636cd229494cf6a3
Ron Huebler <huebler@shh.mpg.de>
2019-08-06 18:25:36 +0200

    Added Recipe for HOPS

---
+++ recipes/hops/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir=${PREFIX}/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+echo $outdir
+mkdir -p $outdir
+mkdir -p ${PREFIX}/bin
+cp -R * $outdir/
+cp $RECIPE_DIR/hops.py $outdir/hops
+ls -l $outdir
+ln -s $outdir/hops $PREFIX/bin
+chmod 0755 ${PREFIX}/bin/hops
