41c6fd15057d7d4c4773e1c874706032c6061102
Michael Knudsen <micknudsen@gmail.com>
2019-08-07 15:51:44 +0200

    Debugging

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -5,6 +5,14 @@ CXXFLAGS="${CXXFLAGS} -fPIC"
 
 sed -i '1s/.*/CC=${CC}/' SeqLib/fermi-lite/Makefile
 
+echo "######## DEBUG START ########"
+
+head -n1 SeqLib/fermi-lite/Makefile
+sed -i "1s/.*/CC=${CC}/" SeqLib/fermi-lite/Makefile
+head -n1 SeqLib/fermi-lite/Makefile
+
+echo "######## DEBUG STOP ########"
+
 ./configure
 make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 make install
