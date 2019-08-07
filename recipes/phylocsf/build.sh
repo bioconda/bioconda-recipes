4762451db1dd004f93d04fa1b81c04615daf16db
Romain Feron <romain.feron.91@gmail.com>
2019-08-07 14:15:46 +0200

    Added manual download and compiling of Opam

--- recipes/phylocsf/build.sh
+++ recipes/phylocsf/build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-# Install OCaml dependencies
-opam install batteries ocaml-twt gsl
-
 # Create bin folder
 mkdir -p $PREFIX/bin
+
+# Download and compile Opam to install Ocaml dependencies
+wget https://github.com/ocaml/opam/releases/download/2.0.5/opam-full-2.0.5.tar.gz
+tar -xf opam-full-2.0.5.tar.gz
+cd opam-full-2.0.5
+./configure
+make lib-ext
+make
+cp opam $PREFIX/bin
+
+# Install OCaml dependencies
+opam install batteries ocaml-twt gsl
 
 # Build PhyloCSF
 make
