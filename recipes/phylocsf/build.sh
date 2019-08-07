84eb6a23b4868e4ca4a45a07aa3dd1c48aae47b2
Romain Feron <romain.feron.91@gmail.com>
2019-08-07 16:47:30 +0200

    Added messages for better debugging

--- recipes/phylocsf/build.sh
+++ recipes/phylocsf/build.sh
@@ -3,6 +3,7 @@
 # Create bin folder
 mkdir -p $PREFIX/bin
 
+echo "Downloading and compiling Opam ..."
 # Download and compile Opam to install Ocaml dependencies
 wget https://github.com/ocaml/opam/releases/download/2.0.5/opam-full-2.0.5.tar.gz
 tar -xf opam-full-2.0.5.tar.gz
@@ -12,13 +13,16 @@ make lib-ext
 make
 cp opam $PREFIX/bin
 
+echo "Setting up Opam environment ..."
 # Setup opam environment
 opam init -a --disable-sandboxing
 eval $(opam env)
 
+echo "Installing OCaml dependencies ..."
 # Install OCaml dependencies
 opam install batteries ocaml-twt gsl
 
+echo "Building PhyloCSF ..."
 # Build PhyloCSF
 make
 
