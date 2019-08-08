5e736486443a9400c4e6fc3f24e57c4d950d0815
Michael Knudsen <micknudsen@gmail.com>
2019-08-08 08:56:32 +0200

    Added fPIC to CXXFLAGS

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -3,6 +3,8 @@ set -eu -o pipefail
 
 export LIBS="-lm ${PREFIX}/lib/libz.a"
 
+CXXFLAGS="${CXXFLAGS} -fPIC"
+
 ./configure
 make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 make install
