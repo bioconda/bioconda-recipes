55647cb677331d70299ce84a667de10273d16ac3
dpryan79 <dpryan79@gmail.com>
2019-08-08 15:48:14 +0200

    bump CRAN packages

--- recipes/bioconductor-rdrtoolbox/build.sh
+++ recipes/bioconductor-rdrtoolbox/build.sh
@@ -9,6 +9,4 @@ CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
 export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64
-ls -l $PREFIX/lib/R/library/rgl/libs
-ldd -r $PREFIX/lib/R/library/rgl/libs/rgl.so
 $R CMD INSTALL --build .
