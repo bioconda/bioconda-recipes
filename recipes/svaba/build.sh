b5a202a137e2b30fcfef5be713b083db2d1cad47
Michael Knudsen <micknudsen@gmail.com>
2019-08-08 08:12:48 +0200

    Export lm

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-CXXFLAGS="${CXXFLAGS} -fPIC"
-
-sed -i '1s/.*/CC=${CC}/' SeqLib/fermi-lite/Makefile
-
-echo "######## DEBUG START ########"
-
-head -n1 SeqLib/fermi-lite/Makefile
-sed -i "1s/.*/CC=$CC/" SeqLib/fermi-lite/Makefile
-head -n1 SeqLib/fermi-lite/Makefile
-
-echo "######## DEBUG STOP ########"
+export LIBS="-lm ${PREFIX}/lib/libz.a"
 
 ./configure
 make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
