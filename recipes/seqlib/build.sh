73d9bbba044f186c373ef4c903c4e5c477b0cc53
Michael Knudsen <micknudsen@gmail.com>
2019-08-09 08:52:08 +0200

    Removed all mentions of htslib in Makefiles

--- recipes/seqlib/build.sh
+++ recipes/seqlib/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sed -i 's|SUBDIRS = bwa htslib fermi-lite src|SUBDIRS = htslib fermi-lite src|' Makefile.am
-sed -i 's|SUBDIRS = bwa htslib fermi-lite src|SUBDIRS = htslib fermi-lite src|' Makefile.in
+sed -i 's|SUBDIRS = bwa htslib fermi-lite src|SUBDIRS = fermi-lite src|' Makefile.am
+sed -i 's|SUBDIRS = bwa htslib fermi-lite src|SUBDIRS = fermi-lite src|' Makefile.in
 
-sed -i 's|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bwa/libbwa.a htslib/libhts.a bin|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a htslib/libhts.a bin|' Makefile.am
-sed -i 's|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bwa/libbwa.a htslib/libhts.a bin|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a htslib/libhts.a bin|' Makefile.in
+sed -i 's|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bwa/libbwa.a htslib/libhts.a bin|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bin|' Makefile.am
+sed -i 's|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bwa/libbwa.a htslib/libhts.a bin|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bin|' Makefile.in
 
 ./configure
 make
