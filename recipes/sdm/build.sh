698bf660f0e2e68ef9b35916775e3c0cb46f4a32
andrea@telatin.com <andrea@telatin.com>
2019-08-09 10:36:05 +0100

    sdm

--- recipes/sdm/build.sh
+++ recipes/sdm/build.sh
@@ -5,11 +5,4 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
-# cd to location of Makefile and source
-mkdir build
-cd build
-# assumes autoreconf was run and thus configure.sh is present
-$SRC_DIR/configure --prefix=$PREFIX
 make
-make install
-
