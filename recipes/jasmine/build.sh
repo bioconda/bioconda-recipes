5a86927bb9d9e641f67baec212dfd16568a5a72a
Devon Ryan <dpryan79@users.noreply.github.com>
2019-08-13 19:32:19 +0200

    Update build.sh

--- recipes/jasmine/build.sh
+++ recipes/jasmine/build.sh
@@ -4,8 +4,7 @@ set -eu -o pipefail
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -R * $outdir/
+cp *.jar $outdir/
 cp $RECIPE_DIR/jasmine.py $outdir/jasmine
-ls -l $outdir
 ln -s $outdir/jasmine $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/jasmine"
