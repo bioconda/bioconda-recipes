b44868765279598b50c31be5c7970215d1e09cf7
Devon Ryan <dpryan79@users.noreply.github.com>
2019-08-13 14:34:46 +0200

    Update build.sh

--- recipes/ispcr/build.sh
+++ recipes/ispcr/build.sh
@@ -2,9 +2,10 @@
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/share/doc/ispcr
 
 # based on https://raw.githubusercontent.com/brewsci/homebrew-bio/de917e71b442af197523131406bfcd577221089c/Formula/ispcr.rb
-
 export MACHTYPE=unix
 mkdir -p "bin/$MACHTYPE"
 mkdir -p "lib/$MACHTYPE"
