20980ad4a1aac2f7cb40bbe1c6991bd93cdab281
Romain Feron <romain.feron.91@gmail.com>
2019-08-14 16:04:21 +0200

    Cleanup, candidate for final recipe 1

--- recipes/phylocsf/build.sh
+++ recipes/phylocsf/build.sh
@@ -3,39 +3,26 @@
 # Create bin folder
 mkdir -p $PREFIX/bin
 
-echo " ======================= Downloading and compiling Opam ... ======================="
-
-# Download and compile Opam to install Ocaml dependencies
+# Download and compile Opam (Ocaml package manager) to install Ocaml dependencies
 wget https://github.com/ocaml/opam/releases/download/2.0.5/opam-full-2.0.5.tar.gz
 tar -xf opam-full-2.0.5.tar.gz
 cd opam-full-2.0.5
 ./configure
-make lib-ext
+make lib-ext  # Build dependencies included in the archive
 make
 
-echo " ======================= Setting up Opam environment ... ======================="
 # Setup opam environment
 ./opam init -a --disable-sandboxing
 eval $(./opam env)
 
-echo " ======================= Testing pkg config ... ======================="
+# Export path to pkconfig lib files required to install Ocaml packages
 export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
-# cd $BUILD_PREFIX/lib/pkgconfig
-# pwd
-# ls
-# echo "Listing lib dir"
-# which pkg-config
-# echo "Package config output"
-# pkg-config --debug --list-all
-# pkg-config --debug gsl
 
-echo " ======================= Installing OCaml dependencies ... ======================="
 # Install OCaml dependencies
 ./opam install batteries ocaml-twt gsl ocamlfind -y
 
-cd ..
-echo " ======================= Building PhyloCSF ... ======================="
 # Build PhyloCSF
+cd ..
 make
 
 # Give execution permission to binary
@@ -44,4 +31,4 @@ chmod +x PhyloCSF
 # Move binary to bin folder
 cp PhyloCSF $PREFIX/bin
 cp PhyloCSF.Linux.x86_64 $PREFIX/bin
-cp PhyloCSF_Parameters $PREFIX/bin
+cp -r PhyloCSF_Parameters $PREFIX/bin
