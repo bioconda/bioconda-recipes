01e3b56b7d35f2dee0cf3ce1ba3162658d55f308
dpryan79 <dpryan79@gmail.com>
2019-08-14 13:43:23 +0200

    debug

--- recipes/edd/build.sh
+++ recipes/edd/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+$PYTHON -c "import pysam"
+echo DONE
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
