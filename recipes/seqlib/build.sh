c43a8b852d2c096ab5af8553c4ae1008bfcbf1c3
Michael Knudsen <micknudsen@gmail.com>
2019-08-14 13:16:35 +0200

    Cleaned up

--- recipes/seqlib/build.sh
+++ recipes/seqlib/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sed -i 's|SUBDIRS = bwa htslib fermi-lite src|SUBDIRS = src|' Makefile.am
-sed -i 's|SUBDIRS = bwa htslib fermi-lite src|SUBDIRS = src|' Makefile.in
-
-sed -i 's|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bwa/libbwa.a htslib/libhts.a bin|mkdir -p bin \&\& cp src/libseqlib.a bin|' Makefile.am
-sed -i 's|mkdir -p bin \&\& cp src/libseqlib.a fermi-lite/libfml.a bwa/libbwa.a htslib/libhts.a bin|mkdir -p bin \&\& cp src/libseqlib.a bin|' Makefile.in
-
 mv temp_bwa/* bwa/
 mv temp_fermi-lite/* fermi-lite/
 mv temp_htslib/* htslib/
