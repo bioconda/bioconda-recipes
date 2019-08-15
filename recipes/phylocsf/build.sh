e85c973fbb99b910b20254c8c9ec64b579613bb8
Romain Feron <romain.feron.91@gmail.com>
2019-08-15 10:20:36 +0200

    First attempt at implementing tests properly

--- recipes/phylocsf/build.sh
+++ recipes/phylocsf/build.sh
@@ -3,8 +3,9 @@
 # Export path to pkconfig lib files required to install Ocaml packages
 export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
 
-# Create bin folder
+# Create bin and tests folder
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/tests
 
 # Download and compile Opam (Ocaml package manager) to install Ocaml dependencies
 wget https://github.com/ocaml/opam/releases/download/2.0.5/opam-full-2.0.5.tar.gz
@@ -32,3 +33,4 @@ chmod +x PhyloCSF
 cp PhyloCSF $PREFIX/bin
 cp PhyloCSF.Linux.x86_64 $PREFIX/bin
 cp -r PhyloCSF_Parameters $PREFIX/bin
+cp PhyloCSF/PhyloCSF_Examples/tal-AA.fa  $PREFIX/tests
