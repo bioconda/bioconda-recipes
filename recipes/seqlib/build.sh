02d9024dbc5162158dc559ad38453e0805c07eee
Michael Knudsen <micknudsen@gmail.com>
2019-08-15 15:28:02 +0200

    That did not work

--- recipes/seqlib/build.sh
+++ recipes/seqlib/build.sh
@@ -13,9 +13,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 ./configure
 
-sed -i.bak "s/gcc/\$\(CC\)/g" Makefile
-
-make CC=${CC} CXXFLAGS="${CXXFLAGS} -fPIC -L${PREFIX}/lib" CFLAGS="${CFLAGS} -fPIC -L${PREFIX}/lib"
+make CC=${CC} CXXFLAGS="${CXXFLAGS} -fPIC -L${PREFIX}/lib"
 make install
 make seqtools
 
