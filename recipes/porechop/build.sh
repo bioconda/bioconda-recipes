f8201608cfa28205db123a8961c3b3d6a347df7f
dpryan79 <dpryan79@gmail.com>
2019-08-16 10:53:03 +0200

    Copy in a more recent seqan

--- recipes/porechop/build.sh
+++ recipes/porechop/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -rf porechop/include/seqan
+cp -r $PREFIX/include/seqan porechop/include
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 $PYTHON -m unittest
