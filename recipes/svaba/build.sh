52a57415f7fb0dae2dce2a057b955efda8cc44c1
Michael Knudsen <micknudsen@gmail.com>
2019-08-16 10:49:37 +0200

    Modify Makefile

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -2,6 +2,10 @@
 set -eu -o pipefail
 
 ./configure
+
+sed -i 's/CC = gcc/compiler = ${CC}/g' Makefile
+sed -i 's/$(CC)/$(compiler)/g' Makefile
+
 make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 make install
 
