0792183988366514fdca49b16c3534237fbb48e5
Michael Knudsen <micknudsen@gmail.com>
2019-08-16 09:36:04 +0200

    Are these export statements needed?

--- recipes/seqlib/build.sh
+++ recipes/seqlib/build.sh
@@ -5,11 +5,11 @@ mv temp_bwa/* bwa/
 mv temp_fermi-lite/* fermi-lite/
 mv temp_htslib/* htslib/
 
-export C_INCLUDE_PATH="${PREFIX}/include"
-export CPP_INCLUDE_PATH="${PREFIX}/include"
-export CXX_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+# export C_INCLUDE_PATH="${PREFIX}/include"
+# export CPP_INCLUDE_PATH="${PREFIX}/include"
+# export CXX_INCLUDE_PATH="${PREFIX}/include"
+# export LIBRARY_PATH="${PREFIX}/lib"
+# export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 ./configure
 
