c4f029c0f38276ff002373a0886414d1a16eeb99
Michael Knudsen <micknudsen@gmail.com>
2019-08-16 09:44:10 +0200

    Starting all over. Again!

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
-export LIBS="-lm ${PREFIX}/lib/libz.a"
-
-CXXFLAGS="${CXXFLAGS} -fPIC"
-
 ./configure
-make CXX=${CXX}
+make
 make install
 
 mkdir -p ${PREFIX}/bin
