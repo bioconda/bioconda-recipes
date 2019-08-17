e67fc1382d494ad0677526eaf92f01b1bdf7a1b6
Devon Ryan <dpryan79@gmail.com>
2019-08-17 23:47:39 +0200

    add shebangs

--- recipes/lace/build.sh
+++ recipes/lace/build.sh
@@ -10,7 +10,7 @@ chmod +x STViewer.py
 
 cp BuildSuperTranscript.py $PREFIX/bin
 cp Checker.py  $PREFIX/bin/Lace_Checker.py
-cp  Lace.py $PREFIX/bin
-cp  Mobius.py $PREFIX/bin
-cp  STViewer.py $PREFIX/bin
+cp Lace.py $PREFIX/bin
+cp Mobius.py $PREFIX/bin
+cp STViewer.py $PREFIX/bin
 
