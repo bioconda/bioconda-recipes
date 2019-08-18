b156a8c7f31ae9f983ff32c90577b7d9285bc245
Michael Knudsen <micknudsen@gmail.com>
2019-08-18 13:51:56 +0200

    Do all these exports actually make a difference?

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -3,9 +3,9 @@ set -eu -o pipefail
 
 ./configure
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-export INCPATH=${PREFIX}/include
+# export C_INCLUDE_PATH=${PREFIX}/include
+# export LIBRARY_PATH=${PREFIX}/lib
+# export INCPATH=${PREFIX}/include
 
 make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 make install
