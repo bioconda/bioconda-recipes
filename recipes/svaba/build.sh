979386fbd9aa8e46737133e8f0dbda98299c521e
Michael Knudsen <micknudsen@gmail.com>
2019-08-18 14:19:02 +0200

    Nope, no difference. Removed exports.

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -3,10 +3,6 @@ set -eu -o pipefail
 
 ./configure
 
-# export C_INCLUDE_PATH=${PREFIX}/include
-# export LIBRARY_PATH=${PREFIX}/lib
-# export INCPATH=${PREFIX}/include
-
 make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 make install
 
