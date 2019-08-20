6f931cfd5ca9aac732e7d418b1eabda509f11aaf
dpryan79 <dpryan79@gmail.com>
2019-08-20 12:19:44 +0200

    Build completely from source, start injecting compilers

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -3,16 +3,16 @@ set -e
 set -x
 
 platform="$(uname)"
+unzip comet_source_"$PKG_VERSION".zip
 if [ "$platform" = "Darwin" ]; then
-	unzip comet_source_"$PKG_VERSION".zip
-	sed -i '' -e 's/ -static//' -e 's/ -o / -headerpad_max_install_names&/' Makefile
-	sed -i '' -e "s/sha1Report='..'/sha1Report=NULL/" MSToolkit/include/MSReader.h
-	make
-elif [ "$platform" = "Linux" ]; then
-	mv comet."$PKG_VERSION".linux.exe comet.exe
+    sed -i.bak -e 's/ -static//' -e 's/ -o / -headerpad_max_install_names&/' Makefile
+    sed -i.bak -e "s/sha1Report='..'/sha1Report=NULL/" MSToolkit/include/MSReader.h
 fi
-chmod 755 comet.exe
+sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/CometSearch.cpp
+sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" Makefile
+make CXX=${CXX}
 mkdir -p "$PREFIX"/bin
-cp comet.exe "$PREFIX"/bin/comet
+cp comet.exe ${PREFIX}/bin/comet
+chmod a+x ${PREFIX}/bin/comet
 cd "$PREFIX"/bin/
-ln -s comet comet.exe
+ln -sr comet comet.exe
