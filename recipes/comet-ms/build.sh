86398df8c4167da68f87f4271fece0710d088248
dpryan79 <dpryan79@gmail.com>
2019-08-20 13:33:47 +0200

    ftello and fseeko

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -8,6 +8,8 @@ if [ "$platform" = "Darwin" ]; then
     sed -i.bak -e 's/ -static//' -e 's/ -o / -headerpad_max_install_names&/' Makefile
     sed -i.bak -e "s/sha1Report='..'/sha1Report=NULL/" MSToolkit/include/MSReader.h
     sed -i.bak "s/ off64_t / off_t /g" CometSearch/Common.h
+    sek -i.bak "s/fseeko64/fseeko/g;s/ftello64/ftello/g" MSToolkit/include/mzParser.h
+    sek -i.bak "s/fseeko64/fseeko/g;s/ftello64/ftello/g" CometSearch/Common.h
 fi
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
