8120e73c85eec21d36cb1ac87b835e43ded8ac20
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2019-08-22 09:45:52 -0400

    Updated to fix database issues

--- recipes/ncbi-amrfinderplus/build.sh
+++ recipes/ncbi-amrfinderplus/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX"
+mkdir $PREFIX/share/amrfinderplus/data
+make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" DEFAULT_DB_DIR="$PREFIX/share/amrfinderplus/data"
 make install
