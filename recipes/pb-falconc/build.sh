d0dda55f96645b85c2e38ee07de3dc76afe2f46d
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-08-24 13:16:10 -0700

    pb-falconc-0.0.1

---
+++ recipes/pb-falconc/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -vexu -o pipefail
+
+NIM_VERSION=devel
+# devel, b/c branches are missing.
+#NIM_VERSION=v0.20.2
+#NIMBLE_VERSION=v0.9.0
+export base=$(pwd)
+
+# Install nim
+git clone -b $NIM_VERSION --depth 1 git://github.com/nim-lang/nim nim-$NIM_VERSION/
+cd nim-$NIM_VERSION
+git clone                 --depth 1 git://github.com/nim-lang/csources csources/
+
+cd csources
+# Fix compiler references to use conda set CC
+# https://github.com/brentp/mosdepth/blob/master/scripts/install.sh
+export LINKER=$CC
+sed -i.bak '/^CC=/s/^/#/g' build.sh
+sed -i.bak '/^LINKER=/s/^/#/g' build.sh
+sh build.sh
+cd ..
+rm -rf csources
+bin/nim c koch
+./koch boot -d:release
+
+export PATH=$base/nim-$NIM_VERSION/bin:$PATH
+
+#git clone -b $NIMBLE_VERSION --depth 1 git://github.com/nim-lang/nimble.git
+#cd nimble
+#nim c src/nimble
+
+which nim
+nim -v
+
+cd $base
+ls -larth
+
+cd $base
+
+export NIMBLEDIR=$(pwd)/.nimble
+#nimble install -y
+make build
+
+#mkdir -p $PREFIX/bin
+#chmod a+x falconc
+#cp falconc $PREFIX/bin
+ls -larth src
+make install
