6396967f4165d1191dfbec86d4c13e5eaa26d608
Sergey Koren <sergek@umd.edu>
2019-08-25 17:19:28 -0400

    clean and build, fail on errors, issue #11509

--- recipes/canu/build.sh
+++ recipes/canu/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# fail on all errors
+set -e
+
 mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
 
-# Build Canu into libexec dir
-( cd src; make TARGET_DIR=$PREFIX/libexec )
+# clean and build Canu into libexec dir
+( cd src; make clean TARGET_DIR=$PREFIX/libexec && make TARGET_DIR=$PREFIX/libexec )
 
 # Link all executable files to bin
 find $PREFIX/libexec -type f -perm +111 -exec ln -s {} $PREFIX/bin \;
