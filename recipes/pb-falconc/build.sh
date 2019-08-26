061f655423d36f12adc9695e0d0529cc89449b71
dpryan79 <dpryan79@gmail.com>
2019-08-26 16:11:25 +0200

    further nim compilation

--- recipes/pb-falconc/build.sh
+++ recipes/pb-falconc/build.sh
@@ -1,51 +1,37 @@
 #!/bin/bash
 set -vexu -o pipefail
 
-echo "CC=$CC"
-which $CC
-export CC_PATH=$CC
-#which gcc  # missing in Bioconda
-alias gcc=$CC
-
 NIM_VERSION=devel
 # devel, b/c branches are missing.
-#NIM_VERSION=v0.20.2
+NIM_VERSION=v0.20.2
 #NIMBLE_VERSION=v0.9.0
 export base=$(pwd)
 
 # Install nim
 git clone -b $NIM_VERSION --depth 1 git://github.com/nim-lang/nim nim-$NIM_VERSION/
-cd nim-$NIM_VERSION
-
-#bin/nim c koch
-#./koch boot -d:release
+pushd nim-$NIM_VERSION
 
 export PATH=$base/nim-$NIM_VERSION/bin:$PATH
 mkdir -p $base/nim-$NIM_VERSION/bin
-ln -sf $CC $base/nim-$NIM_VERSION/bin/gcc
-which gcc
-gcc -h
 
-sh build_all.sh
+# Fix compiler references to use conda set CC
+export LINKER=$CC
 
-#git clone -b $NIMBLE_VERSION --depth 1 git://github.com/nim-lang/nimble.git
-#cd nimble
-#nim c src/nimble
+git clone --depth 1 https://github.com/nim-lang/csources.git
+pushd csources
+sed -i.bak '/^CC=/s/^/#/g' build.sh
+sed -i.bak '/^LINKER=/s/^/#/g' build.sh
+sh build.sh
+popd
 
-which nim
-nim -v
+bin/nim c --skipUserCfg --skipParentCfg --gcc.exe:$CC --gcc.linkerexe:$CC koch
+./koch boot --gcc.exe:$CC --gcc.linkerexe:$CC -d:release
+./koch tools --gcc.exe:$CC --gcc.linkerexe:$CC # Compile Nimble and other tools.
 
-cd $base
-ls -larth
+export PATH=$base/nim-$NIM_VERSION/bin/:$PATH:$base/nimble/src:$PATH
 
-cd $base
+popd
 
 export NIMBLEDIR=$(pwd)/.nimble
-#nimble install -y
 make build
-
-#mkdir -p $PREFIX/bin
-#chmod a+x falconc
-#cp falconc $PREFIX/bin
-ls -larth src
 make install
