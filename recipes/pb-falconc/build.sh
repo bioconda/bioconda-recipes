0f808e9e97850eebfb524e0764d40b93aa9df4f9
dpryan79 <dpryan79@gmail.com>
2019-08-27 13:11:05 +0200

    better compiler injection

--- recipes/pb-falconc/build.sh
+++ recipes/pb-falconc/build.sh
@@ -4,12 +4,14 @@ set -vexu -o pipefail
 export base=$(pwd)
 
 pushd nim
+# inject compilers
+echo "gcc.exe = \"$CC\"" >> config/nim.cfg
+echo "gcc.linkerexe = \"$CC\"" >> config/nim.cfg
+echo "clang.exe = \"$CC\"" >> config/nim.cfg
+echo "clang.linkerexe = \"$CC\"" >> config/nim.cfg
 ./build.sh
-bin/nim c --gcc.exe:$CC --gcc.linkerexe:$CC koch
-./koch tools --gcc.exe:$CC --gcc.linkerexe:$CC
-baseCC=`basename $CC`
-echo "gcc.exe = \"$baseCC\"" >> config/nim.cfg
-echo "gcc.linkerexe = \"$baseCC\"" >> config/nim.cfg
+bin/nim c koch
+./koch tools
 popd
 
 export PATH=$base/nim/bin:$PATH
