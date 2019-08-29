d4700f926d4deaaa5c950eb9ee2ddacbb18aed97
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-08-29 17:07:40 +0200

    Fix for path

--- recipes/rnalien/build.sh
+++ recipes/rnalien/build.sh
@@ -4,7 +4,7 @@ ls -l
 ls -l RNAlien
 for t in "${tools[@]}"
 do
-   mv RNAlien/$t ${PREFIX}/bin/${t}-bin
+   mv $t ${PREFIX}/bin/${t}-bin
    echo -e "#!/bin/bash\nexport SYSTEM_CERTIFICATE_PATH=${PREFIX}/ssl/cacert.pem\n${PREFIX}/bin/${t}-bin \"\$@\"\n" > ${PREFIX}/bin/${t}
    chmod 755 ${PREFIX}/bin/${t}
 done
