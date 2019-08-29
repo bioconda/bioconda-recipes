089c14abbf78c03ac47a2eba11e7099b802dad62
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-08-29 14:20:02 +0200

    Fixed path

--- recipes/rnalien/build.sh
+++ recipes/rnalien/build.sh
@@ -2,7 +2,7 @@
 declare -a tools=("RNAlien" "RNAlienStatistics" "cmsearchToBed" "RNAcentralHTTPRequest" "RNAlienScan")
 for t in "${tools[@]}"
 do
-   mv $t ${PREFIX}/bin/${t}-bin
+   mv RNAlien/$t ${PREFIX}/bin/${t}-bin
    echo -e "#!/bin/bash\nexport SYSTEM_CERTIFICATE_PATH=${PREFIX}/ssl/cacert.pem\n${PREFIX}/bin/${t}-bin \"\$@\"\n" > ${PREFIX}/bin/${t}
    chmod 755 ${PREFIX}/bin/${t}
 done
