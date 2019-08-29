c3ac89875f5f10ca20f3c992284d7fa92ad98b6d
William L. Close <close.will@gmail.com>
2019-08-29 19:16:28 -0400

    trying to fix clobber issue

--- recipes/virsorter/build.sh
+++ recipes/virsorter/build.sh
@@ -14,3 +14,4 @@ PKG_OUTDIR="${PREFIX}"/share/"${PKG_NAME}"-"${PKG_VERSION}"-"${PKG_BUILDNUM}"/
 mkdir -pv "${PKG_OUTDIR}"/ "${PREFIX}"/bin/
 cp -r $(ls | grep -v "conda" | grep -v "build") "${PKG_OUTDIR}"/
 ln -s "${PKG_OUTDIR}"/wrapper_phage_contigs_sorter_iPlant.pl "${PREFIX}"/bin/
+conda clean --all
