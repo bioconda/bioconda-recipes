0daf17122d25241154f6279880d5823cb4ab7961
William L. Close <close.will@gmail.com>
2019-08-29 17:40:55 -0400

    debugging build

--- recipes/virsorter/build.sh
+++ recipes/virsorter/build.sh
@@ -1,6 +1,6 @@
 ### Reconfiguring compiler for VirSorter Makefile
 cd Scripts/
-sed 's/gcc/$$GCC/g' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's/gcc/\$\(GCC\)/g' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 
 
 ### Compiling programs
@@ -12,5 +12,7 @@ make
 cd ../
 PKG_OUTDIR="${PREFIX}"/share/"${PKG_NAME}"-"${PKG_VERSION}"-"${PKG_BUILDNUM}"
 mkdir -pv "${PREFIX}"/bin "${PKG_OUTDIR}"
+ls -a
+pwd
 cp -r ./ "${PREFIX}"/share/
 ln -s "${PKG_OUTDIR}"/wrapper_phage_contigs_sorter_iPlant.pl "${PREFIX}"/bin/
