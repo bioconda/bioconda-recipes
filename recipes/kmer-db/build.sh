c4c2cf691e5838b978664c066b74da37131f8479
Devon Ryan <dpryan79@users.noreply.github.com>
2019-08-30 09:30:57 +0200

    Update build.sh

--- recipes/kmer-db/build.sh
+++ recipes/kmer-db/build.sh
@@ -6,12 +6,12 @@ for OPT in AVX AVX2;
 do
  if [[ "AVX2" == "${OPT}" ]]; then
     echo "Working on $OPT..."
-    make CC=${GXX} CFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}"
+    make CC=${CXX} CFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}"
     cp kmer-db $PREFIX/bin/kmer-db-${OPT}
     make clean
  else
     echo "Working on $OPT..."
-    make CC=${GXX} CFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" NO_AVX2=true
+    make CC=${CXX} CFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" NO_AVX2=true
     echo "Trying to copy to $PREFIX"
     cp kmer-db $PREFIX/bin/kmer-db-${OPT}
     make clean
