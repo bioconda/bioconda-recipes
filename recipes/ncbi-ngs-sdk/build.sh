0465911c31a5d1447acea72ce13938df77718ee6
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2019-08-30 11:32:24 -0400

    Creating symlinks for compilers

--- recipes/ncbi-ngs-sdk/build.sh
+++ recipes/ncbi-ngs-sdk/build.sh
@@ -10,12 +10,11 @@ if [[ $OSTYPE == darwin* ]]; then
      export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 fi
 
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc $BUILD_PREFIX/bin/gcc
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++ $BUILD_PREFIX/bin/g++
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc $BUILD_PREFIX/bin/cc
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++ $BUILD_PREFIX/bin/c++
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ar $BUILD_PREFIX/bin/ar
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $BUILD_PREFIX/bin/ld
+ln -s ${GCC} $BUILD_PREFIX/bin/gcc
+ln -s ${CC} $BUILD_PREFIX/bin/cc
+ln -s ${CXX} $BUILD_PREFIX/bin/g++
+ln -s ${AR} $BUILD_PREFIX/bin/ar
+ln -s ${LD} $BUILD_PREFIX/bin/ld
 
 export PATH=$BUILD_PREFIX/bin:$PATH
 
