8ce07d98610b5f5ac8b93f8e2faacb47702121a6
Björn Grüning <bjoern@gruenings.eu>
2019-09-02 07:18:14 +0200

    remove -w from perl scripts

--- recipes/mummer/build.sh
+++ recipes/mummer/build.sh
@@ -18,6 +18,9 @@ pushd ${PREFIX}/bin/
 ln -s ./tcsh ./csh
 popd
 
+# replace perl with /usr/bin/env perl and remove -w flag
+find . -name \*.pl -exec sed  -i 's^/usr/bin/perl -w^/usr/bin/env perl^' {} \;
+
 # cd to location of Makefile and source
 cp -R $SRC_DIR/* $MUMMER_HOME
 
