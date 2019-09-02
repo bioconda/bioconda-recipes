9409ca1f60ecea4ade3c6fc22d8f56c24326e612
Devon Ryan <dpryan79@gmail.com>
2019-09-03 00:04:17 +0200

    I don't think it has an FAI submodule any more

--- recipes/perl-bio-db-hts/build.sh
+++ recipes/perl-bio-db-hts/build.sh
@@ -2,8 +2,6 @@
 export HTSLIB_DIR=$PREFIX
 export LD=$CC
 perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
-echo "A"
 perl ./Build
-echo "B"
 # Make sure this goes in site
 perl ./Build install --installdirs site
