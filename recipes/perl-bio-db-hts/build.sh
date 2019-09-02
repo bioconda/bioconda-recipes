6b442450413172aca1c4e949bce466335be3bf4a
Devon Ryan <dpryan79@gmail.com>
2019-09-02 23:42:28 +0200

    use host section

--- recipes/perl-bio-db-hts/build.sh
+++ recipes/perl-bio-db-hts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+export HTSLIB_DIR=$PREFIX
+export LD=$CC
 perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
+echo "A"
 perl ./Build
+echo "B"
 # Make sure this goes in site
 perl ./Build install --installdirs site
