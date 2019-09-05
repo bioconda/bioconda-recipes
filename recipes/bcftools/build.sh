521df68b908d085f7baaa608edc840a0ae1bfc8e
John Marshall <John.W.Marshall@glasgow.ac.uk>
2019-09-05 20:28:49 +0100

    Update samtools & bcftools to depend on htslib package (#17273)
    * Change samtools & bcftools to use bioconda's htslib package
    
    Depend on htslib and link to its shared libhts.so/.dylib rather than
    have both samtools/bcftools build and statically link their own libhts.a.
    
    Remove dependencies on bzip2/curl/xz/libdeflate/openssl as these are
    the htslib package's responsibility, and remove -DHAVE_LIBDEFLATE etc
    accordingly. Remove the CFLAGS/LIBS/etc overrides, which are unnecessary
    when using configure.
    
    * Make samtools work with either conda-forge or default ncurses
    
    Unlike other platforms, the default channel's libncurses.so/.dylib
    does not automatically pull in libtinfo.so/.dylib. Override CURSES_LIB
    so the samtools executable is explicitly linked against both libraries.

--- recipes/bcftools/build.sh
+++ recipes/bcftools/build.sh
@@ -3,11 +3,7 @@
 # Overwrite plot-vcfstats with updated version
 curl https://raw.githubusercontent.com/samtools/bcftools/44c660490411cbc0611e405a8ebf3faa0d65c544/misc/plot-vcfstats -o misc/plot-vcfstats
 
-export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
-export CFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-
 sed -i.bak 's@^#!/usr/bin/perl -w@#!/opt/anaconda1anaconda2anaconda3/bin/perl@' misc/vcfutils.pl
-./configure --prefix=$PREFIX --enable-libcurl --enable-libgsl CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
+./configure --prefix=$PREFIX --with-htslib=system --enable-libgsl
 make all
 make install
