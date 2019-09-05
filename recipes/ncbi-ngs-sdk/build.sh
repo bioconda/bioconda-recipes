92a5d4cfffd58e7a0435adb80ee02374d09e034f
Devon Ryan <dpryan79@gmail.com>
2019-09-05 22:33:53 +0200

    inject compilers

--- recipes/ncbi-ngs-sdk/build.sh
+++ recipes/ncbi-ngs-sdk/build.sh
@@ -1,6 +1,7 @@
 NCBI_OUTDIR=$SRC_DIR/ncbi-outdir
 
 pushd ngs-sdk
+sed -i.bak "s#gcc#${CC}#g;s#g++#${CXX}#g" setup/konfigure.perl
 ./configure \
     --prefix=$PREFIX \
     --build-prefix=$NCBI_OUTDIR \
