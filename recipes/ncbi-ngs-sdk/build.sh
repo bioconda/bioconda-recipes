3f82383d7ba650228a62eca03584f023fb05bfd3
Devon Ryan <dpryan79@users.noreply.github.com>
2019-09-06 16:22:26 +0200

    Update build.sh

--- recipes/ncbi-ngs-sdk/build.sh
+++ recipes/ncbi-ngs-sdk/build.sh
@@ -3,6 +3,7 @@ NCBI_OUTDIR=$SRC_DIR/ncbi-outdir
 pushd ngs-sdk
 sed -i.bak "349i\
 \$TOOLS = '${CXX}';" setup/konfigure.perl
+sed -i.bak "414,415d" setup/konfigure.perl
 ./configure \
     --prefix=$PREFIX \
     --build-prefix=$NCBI_OUTDIR \
