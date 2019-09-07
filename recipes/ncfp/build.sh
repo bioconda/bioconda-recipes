83011257379c538015a60750e19e435573e897b2
Devon Ryan <dpryan79@users.noreply.github.com>
2019-09-07 17:43:00 +0200

    Update build.sh

--- recipes/ncfp/build.sh
+++ recipes/ncfp/build.sh
@@ -2,4 +2,4 @@
 
 #This isn't packaged with the source code on pypi but is needed by setup.py
 touch README.md
-$PYTHON -m pip install --no-dep --ignore-installed -vv .
+$PYTHON -m pip install --no-deps --ignore-installed -vv .
