1699138c1c47236398207edc6ae05cc5507b4d15
Devon Ryan <dpryan79@gmail.com>
2019-09-08 12:01:31 +0200

    lrt

--- recipes/paladin/build.sh
+++ recipes/paladin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread" INCLUDES="$CFLAGS" -j${CPU_COUNT}
+make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread -lrt" INCLUDES="$CFLAGS" -j${CPU_COUNT}
 install -m 0755 paladin ${PREFIX}/bin
