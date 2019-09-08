93ee30f2aa62b906f4ced7e267b0104e611d766c
Devon Ryan <dpryan79@gmail.com>
2019-09-08 16:29:26 +0200

    typo

--- recipes/sccaller/build.sh
+++ recipes/sccaller/build.sh
@@ -2,6 +2,6 @@
 set -eu -o pipefail
 
 mkdir -p $PREFIX/bin
-echo "#!/opt/anaconda1anaconda2anaconda3/bin/python" $PREFIX/bin/sccaller
+echo "#!/opt/anaconda1anaconda2anaconda3/bin/python" > $PREFIX/bin/sccaller
 cat sccaller_*.py >> $PREFIX/bin/sccaller
 chmod 0755 "${PREFIX}/bin/sccaller"
