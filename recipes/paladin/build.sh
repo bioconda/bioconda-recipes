8e519872c4e9c82998a07b266388da0799b77de8
Devon Ryan <dpryan79@gmail.com>
2019-09-08 11:48:40 +0200

    update paladin build script

--- recipes/paladin/build.sh
+++ recipes/paladin/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make INCLUDES=$PREFIX/include -j${CPU_COUNT}
+make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread" INCLUDES="$CFLAGS" -j${CPU_COUNT}
 install -m 0755 paladin ${PREFIX}/bin
-
