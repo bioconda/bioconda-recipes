3f442460eb66cfc698c7c06d056d70481828505f
Devon Ryan <dpryan79@gmail.com>
2019-09-08 22:06:54 +0200

    inject some more compilers

--- recipes/gvcfgenotyper/build.sh
+++ recipes/gvcfgenotyper/build.sh
@@ -3,9 +3,10 @@ set -eu -o pipefail
 
 cd external/htslib-*
 ./configure --prefix=${PREFIX} --disable-lzma --disable-bz2
+make CC=$CC
 cd ../..
 sed -i.bak 's^-Isrc/c/^-Isrc/c/ -I${PREFIX}/include^' Makefile
 sed -i.bak 's^-lpthread^-lpthread -L$(PREFIX)/lib^' Makefile
-make
+make CC=$CC CXX=$CXX
 mkdir -p $PREFIX/bin
 cp bin/gvcfgenotyper $PREFIX/bin
