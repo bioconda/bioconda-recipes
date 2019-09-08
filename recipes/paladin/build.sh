03e355e8c11687333dd158cf69c824d798d42f12
Devon Ryan <dpryan79@gmail.com>
2019-09-08 12:16:24 +0200

    no -lrt on osx

--- recipes/paladin/build.sh
+++ recipes/paladin/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread -lrt" INCLUDES="$CFLAGS" -j${CPU_COUNT}
+if [[ $(uname -s) == "Darwin" ]]; then
+    make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread" INCLUDES="$CFLAGS" -j${CPU_COUNT}
+else
+    make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread -lrt" INCLUDES="$CFLAGS" -j${CPU_COUNT}
+fi
 install -m 0755 paladin ${PREFIX}/bin
