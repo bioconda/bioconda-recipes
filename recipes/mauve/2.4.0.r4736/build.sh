ba8f41d56dc23ca0dc4594aea9454d16301727d8
Nolan Woods <innovate.invent@gmail.com>
2019-09-11 18:25:12 +0000

    Add wrapper for Mauve Contig Mover (#14997)
    * Add wrapper for Mauve Contig Mover
    
    * Increment build
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Use new wrapper to test contig orderer
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Add headless java argument
    
    * Switch to older snapshot
    
    * Add license
    
    * Fix package version
    
    * Abandon 2016 snapshot
    
    * Remove ant
    
    * Fix build.sh
    
    * Work around jre issue with fontconfig
    
    * Add font dependency
    
    * Fix MauveCM test
    
    * Fix busybox not supporting stderr pipe

--- recipes/mauve/2.4.0.r4736/build.sh
+++ recipes/mauve/2.4.0.r4736/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 
+ant jar
 mauve_dir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p ${mauve_dir}
-cp -R * ${mauve_dir}/
+cp Mauve.jar ${mauve_dir}/Mauve.jar
+mkdir -v ${mauve_dir}/ext
+cp -R ext/* ${mauve_dir}/ext/
+cp  linux-x64/Mauve ${mauve_dir}/Mauve
 cp $RECIPE_DIR/MauveCM ${mauve_dir}/MauveCM
 ln -s ${mauve_dir}/Mauve $PREFIX/bin/Mauve
 ln -s ${mauve_dir}/MauveCM $PREFIX/bin/MauveCM
