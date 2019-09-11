97ae99f828f532680377b52ad79ccebdb0c73daf
Matteo <matteo.massidda@crs4.it>
2019-09-11 15:45:12 +0200

    updated post link

--- recipes/kggseq/post-link.sh
+++ recipes/kggseq/post-link.sh
@@ -6,5 +6,4 @@
 ## exit 0 so the install is a success
 #exit 0
 echo "potrebbe anche funzionare"
-kggseq --lib-update
-wget "grass.cgs.hku.hk/limx/kggseq/download/kggseqhg19min.zip"
+#java -jar $outdir/kggseq.jar
