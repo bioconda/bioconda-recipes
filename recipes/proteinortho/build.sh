878e00d4e0b705cc60a6e8be700ef4dbdbc13c68
dpryan79 <dpryan79@gmail.com>
2019-09-11 15:53:38 +0200

    verbose

--- recipes/proteinortho/build.sh
+++ recipes/proteinortho/build.sh
@@ -4,9 +4,7 @@ if [ `uname` == Darwin ]; then
     export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 fi
 
-export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
 make clean
-make CC=$CC CXX=$CXX LDLIBS="-lgfortran" CXXLIBRARY="-L${PREFIX}/lib" all -j${CPU_COUNT}
+make CC=$CC CXX=$CXX V=1 all -j${CPU_COUNT}
 mkdir -p $PREFIX/bin
 make install PREFIX=$PREFIX/bin/
