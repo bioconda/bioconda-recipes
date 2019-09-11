0d71c642494849efa939e7ef0a2f24347cbbe25a
Stefan Janssen <stefan.m.janssen@gmail.com>
2019-09-11 20:01:44 +0200

    sync dirs

--- recipes/sepp-refgg138/build.sh
+++ recipes/sepp-refgg138/build.sh
@@ -8,10 +8,10 @@ rm -v sepp-package/ref/reference-gg-raxml-bl.tre
 rm -v sepp-package/ref/reference-gg-raxml-bl-rooted.tre
 
 # create conda share directory
-mkdir -p $PREFIX/share/sepp/sepp/ref/
+mkdir -p $PREFIX/share/sepp/ref/
 
 # move reference files to conda location
-cp -v sepp-package/ref/99_otu_taxonomy.txt $PREFIX/share/sepp/sepp/ref/
-cp -v sepp-package/ref/gg_13_5_ssu_align_99_pfiltered.fasta $PREFIX/share/sepp/sepp/ref/
-cp -v sepp-package/ref/RAxML_info-reference-gg-raxml-bl.info $PREFIX/share/sepp/sepp/ref/
-cp -v sepp-package/ref/reference-gg-raxml-bl-rooted-relabelled.tre $PREFIX/share/sepp/sepp/ref/
+cp -v sepp-package/ref/99_otu_taxonomy.txt $PREFIX/share/sepp/ref/
+cp -v sepp-package/ref/gg_13_5_ssu_align_99_pfiltered.fasta $PREFIX/share/sepp/ref/
+cp -v sepp-package/ref/RAxML_info-reference-gg-raxml-bl.info $PREFIX/share/sepp/ref/
+cp -v sepp-package/ref/reference-gg-raxml-bl-rooted-relabelled.tre $PREFIX/share/sepp/ref/
