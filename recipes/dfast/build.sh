0b6d7259b13515ef5fcf05804941e7aa7db96e57
Devon Ryan <dpryan79@gmail.com>
2019-09-13 10:28:18 +0200

    don't vendor last, not sure if this will work then.

--- recipes/dfast/build.sh
+++ recipes/dfast/build.sh
@@ -2,23 +2,11 @@
 
 APPROOT=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 
-rm bin/*/aragorn
-rm bin/*/barrnap
-rm -r bin/Darwin/barrnap-0.8
-rm bin/*/blastdbcmd
-rm bin/*/blastp
-rm bin/*/ghost*
-rm bin/*/hmm*
-rm -r bin/Darwin/lib
-rm bin/*/makeblastdb
-rm bin/*/mga
-rm bin/*/rpsblast
-
+rm -rf bin/Linux
+rm -rf bin/Darwin
 
 mkdir -p $APPROOT
 cp -r ./* $APPROOT
 
 ln -s ${APPROOT}/dfast ${PREFIX}/bin/dfast
-ln -s ${APPROOT}/dfc ${PREFIX}/bin/
-ln -s ${APPROOT}/scripts/file_downloader.py ${PREFIX}/bin/dfast_file_downloader.py
-ln -s ${APPROOT}/scripts/file_downloader.py ${PREFIX}/bin/file_downloader.py
+ln -s ${APPROOT}/scripts/dfast_file_downloader.py ${PREFIX}/bin/dfast_file_downloader.py
