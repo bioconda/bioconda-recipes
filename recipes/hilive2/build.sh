f2e5ba5549f550f9b5cefd61306edb14f521e7a2
Tobias Loka <lokat@rki.de>
2019-09-13 09:23:21 +0200

    Add HiLive2 recipe.

---
+++ recipes/hilive2/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir ${SRC_DIR}/hilive2/build
+cd ${SRC_DIR}/hilive2/build
+
+cmake -E env CXXFLAGS="-pthread" cmake -DCMAKE_BUILD_TYPE=Release -DSEQAN_INCLUDE_PATH="${CONDA_DEFAULT_ENV}/include/" -DCMAKE_PREFIX_PATH="${CONDA_DEFAULT_ENV}/share/cmake/seqan" -DCONDA=ON ..
+
+make
+
+mkdir -p ${PREFIX}/bin
+cp ${SRC_DIR}/hilive2/build/hilive ${PREFIX}/bin/
+cp ${SRC_DIR}/hilive2/build/hilive-build ${PREFIX}/bin/
+cp ${SRC_DIR}/hilive2/build/hilive-out ${PREFIX}/bin/
+
