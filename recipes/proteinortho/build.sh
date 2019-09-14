7543019569e1277ddc20505252bb745a5888541d
Paul Klemm <paulmjklemm@gmail.com>
2019-09-14 23:29:21 +0200

    mac only static comp

--- recipes/proteinortho/build.sh
+++ recipes/proteinortho/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export STATIC="FALSE"
 if [ `uname` == Darwin ]; then
     export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+    export STATIC="TRUE"
 fi
 
 make clean
-make CC=$CC CXX=$CXX all -j${CPU_COUNT}
+make CC=$CC CXX=$CXX STATIC=$STATIC all -j${CPU_COUNT}
 mkdir -p $PREFIX/bin
 make install PREFIX=$PREFIX/bin/
