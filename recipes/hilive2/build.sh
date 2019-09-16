abad66b8d0a04f367f2b535217ce631535df915b
tloka <34157353+tloka@users.noreply.github.com>
2019-09-16 15:58:24 +0200

    Update build.sh
    Adapted copy commands according to the changed location of build directory.

--- recipes/hilive2/build.sh
+++ recipes/hilive2/build.sh
@@ -8,7 +8,6 @@ cmake -E env CXXFLAGS="-pthread" cmake -DCMAKE_BUILD_TYPE=Release -DSEQAN_INCLUD
 make
 
 mkdir -p ${PREFIX}/bin
-cp ${SRC_DIR}/hilive2/build/hilive ${PREFIX}/bin/
-cp ${SRC_DIR}/hilive2/build/hilive-build ${PREFIX}/bin/
-cp ${SRC_DIR}/hilive2/build/hilive-out ${PREFIX}/bin/
-
+cp build/hilive ${PREFIX}/bin/
+cp build/hilive-build ${PREFIX}/bin/
+cp build/hilive-out ${PREFIX}/bin/
