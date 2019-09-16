089831c0fca81365f2a6b1458a0eea3803cadae2
Devon Ryan <dpryan79@users.noreply.github.com>
2019-09-16 23:02:40 +0200

    Update build.sh

--- recipes/encode-blacklist/build.sh
+++ recipes/encode-blacklist/build.sh
@@ -2,6 +2,8 @@
 export LIBRARY_PATH=${PREFIX}/lib
 mkdir -p $PREFIX/bin/
 sed -i.bak "s#g++#${CXX}#" Makefile
+if [ "$(uname)" == Darwin ] ; then
+  sed -i.bak "s#std::fmod#fmod#" blacklist.cpp
+fi
 make
 cp Blacklist $PREFIX/bin/
-
