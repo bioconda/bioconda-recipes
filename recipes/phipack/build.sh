8b1bb56c87e34af7ac2f68ef6b834ee4dc3a5e4d
Fred <fredjaya1@gmail.com>
2019-09-17 14:42:14 +1000

    add Profile function for sequence-wide breakpoint detection

--- recipes/phipack/build.sh
+++ recipes/phipack/build.sh
@@ -5,7 +5,8 @@ cd src/
 make
 cp ../Phi $PREFIX/bin
 cp ../ppma_2_bmp $PREFIX/bin
+cp ../Profile $PREFIX/bin
 
 chmod +x $PREFIX/bin/Phi
 chmod +x $PREFIX/bin/ppma_2_bmp
-
+chmod +x $PREFIX/bin/Profile
