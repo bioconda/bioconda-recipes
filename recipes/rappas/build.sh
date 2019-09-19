05ea35c837ad659bfcefaadfe72c8a4ac1912513
Benjamin Linard <benjamin.linard@lirmm.fr>
2019-09-19 10:22:32 +0200

    - removed stub, now part of RAPPAS sources - improved comments in build.sh

--- recipes/rappas/build.sh
+++ recipes/rappas/build.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
-echo "PREFIX: ${PREFIX}"
-echo "SRC_DIR: ${SRC_DIR}"
-pwd
-#create directory following bioconda recipe
+#create directory following bioconda rules
 rappas_dir="${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}"
 mkdir -p $rappas_dir
+
 #compile
 cd $SRC_DIR
 pwd
 ant -f build-cli.xml
+
+#copy both the jar and the stub version to /opt
+#the jar allows tuning of JVM options,
+#ex: when requiring large RAM amount of RAM
+#the stub concatenate 'rappas' is for normal usage
 cp dist/RAPPAS.jar $rappas_dir
 cat stub.sh dist/RAPPAS.jar > rappas && chmod +x rappas
 cp rappas $rappas_dir
-#link in /bin
+
+#link in /bin, following bioconda rules
 mkdir -p ${PREFIX}/bin
 ln -s ${rappas_dir}/rappas ${PREFIX}/bin/rappas
 ln -s ${rappas_dir}/RAPPAS.jar ${PREFIX}/bin/RAPPAS.jar
--- recipes/rappas/stub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-MYSELF=`which "$0" 2>/dev/null`
-[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
-java=java
-if test -n "$JAVA_HOME"; then
-    java="$JAVA_HOME/bin/java"
-fi
-exec "$java" -Xms4G -jar $MYSELF "$@"
-exit 1 
