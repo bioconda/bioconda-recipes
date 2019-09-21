0ded3433e2d7d1b6611681e71c40e97e8111614a
Devon Ryan <dpryan79@users.noreply.github.com>
2019-09-21 22:49:45 +0200

    Update build.sh

--- recipes/gvcfgenotyper/build.sh
+++ recipes/gvcfgenotyper/build.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 
 cd external/htslib-*
-make CC=$CC CFLAGS="$CFLAGS"
+make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 cd ../..
 sed -i.bak 's^-Isrc/c/^-Isrc/c/ -I${PREFIX}/include^' Makefile
 sed -i.bak 's^-lpthread^-lpthread -L$(PREFIX)/lib^' Makefile
