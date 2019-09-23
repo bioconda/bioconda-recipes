78a63abf8ea5593860f27719897bacc06a65e6ab
Devon Ryan <dpryan79@users.noreply.github.com>
2019-09-23 09:51:55 +0200

    Update build.sh

--- recipes/desalt/build.sh
+++ recipes/desalt/build.sh
@@ -3,6 +3,12 @@
 mkdir -p $PREFIX/bin
 
 cd src
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib"
+#malloc.h is a Linux-ism
+sed -i.bak "s/malloc.h/stdlib.h/" load_unipath_size.c
+sed -i.bak "s/malloc.h/stdlib.h/" graph.c
+sed -i.bak "s/malloc.h/stdlib.h/" deBGA-master/load_input.c
+sed -i.bak "s/malloc.h/stdlib.h/" deBGA-master/index_build.c
+
+make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" LIBS="-lm -lz -lpthread -lomp"
 
 cp deSALT deBGA Annotation_Load.py $PREFIX/bin
