4b428077693ab8e2103ae8cb894742fc75fb0044
dpryan79 <dpryan79@gmail.com>
2019-09-25 08:28:44 +0200

    rename, make generic

--- recipes/whatsgnu/build.sh
+++ recipes/whatsgnu/build.sh
@@ -5,9 +5,3 @@ chmod +x bin/*
 cp bin/* "${PREFIX}/bin/"
 cp test_data/* "${PREFIX}/test_data/"
 cp example_reports/* "${PREFIX}/example_reports/"
-
-WhatsGNU_get_GenBank_genomes.py -h
-WhatsGNU_database_customizer.py -h
-WhatsGNU_main.py -h
-WhatsGNU_main.py -v
-WhatsGNU_plotter.py -h
