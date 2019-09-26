758ed121c2d84b7390da390b627729ff6acd3f27
William L. Close <close.will@gmail.com>
2019-09-26 12:00:47 -0400

    removing unnecessary files

--- recipes/virsorter/build.sh
+++ recipes/virsorter/build.sh
@@ -6,9 +6,10 @@ sed "s;mga_linux_ia64\\\n;mga\\\n;" Scripts/Step_1_contigs_cleaning_and_gene_pre
 sed "s;mga_linux_ia64;mga;" Scripts/Step_first_add_custom_phage_sequence.pl > Scripts/Step_first_add_custom_phage_sequence.pl.tmp && mv Scripts/Step_first_add_custom_phage_sequence.pl.tmp Scripts/Step_first_add_custom_phage_sequence.pl
 
 
-### Reconfiguring compiler for VirSorter Makefile
+### Reconfiguring/fixing VirSorter Makefile
 cd Scripts/
 sed 's/gcc/\$\(CC\)/g' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
+sed 's/\\\.o/*\\\.o/' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 
 
 ### Compiling programs
@@ -16,19 +17,15 @@ make clean
 make
 
 
+### Cleaning up
+rm Makefile Sliding_windows_3.*
+
+
+### Setting permissions
+chmod 775 *
+
+
 ### Organizing executables
 cd ../
-
-PKG_OUTDIR="${PREFIX}"/share/"${PKG_NAME}"-"${PKG_VERSION}"-"${PKG_BUILDNUM}"/
-mkdir -pv "${PKG_OUTDIR}"/ "${PREFIX}"/bin/
-cp -r $(ls | grep -v "conda" | grep -v "build") "${PKG_OUTDIR}"/
-
-ln -s "${PKG_OUTDIR}"/wrapper_phage_contigs_sorter_iPlant.pl "${PREFIX}"/bin/
-ln -s "${PKG_OUTDIR}"/Scripts/ "${PREFIX}"/bin/Scripts
-
-
-
-### Fixing permissions
-
-chmod 644 "${PKG_OUTDIR}"/Scripts/Makefile "${PKG_OUTDIR}"/Scripts/Sliding_windows_3.c "${PKG_OUTDIR}"/Scripts/Sliding_windows_3.o
-chmod 755 $(find "${PKG_OUTDIR}"/Scripts/ -maxdepth 1 \( -regex ".*Step.*" -o -regex ".*Sliding_windows_3" \))
+mkdir -pv "${PREFIX}"/bin/
+mv wrapper_phage_contigs_sorter_iPlant.pl Scripts/ "${PREFIX}"/bin/
