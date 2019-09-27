981357815d9e67eb1e3ca8d99a0ab7a3c3382328
Christian Brueffer <christian@brueffer.io>
2019-09-27 18:04:49 +0100

    Add a license file.

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+curl -O LICENSE.txt https://gitlab.com/german.tischler/biobambam2/blob/master/GPLv3
+
 cd x86_64-linux-gnu/$PKG_VERSION
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
