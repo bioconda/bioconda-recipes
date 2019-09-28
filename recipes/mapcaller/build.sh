e2febb9c7dacb43d66f0d5ef17229958cbc9d9a5
Peter van Heusden <pvh@sanbi.ac.za>
2019-09-28 20:44:19 +0200

    Add Compiler=$CXX

--- recipes/mapcaller/build.sh
+++ recipes/mapcaller/build.sh
@@ -1,4 +1,4 @@
-make CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include" CXXFLAGS="-I${PREFIX}/include"
+make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include" CXXFLAGS="-I${PREFIX}/include"
 if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
