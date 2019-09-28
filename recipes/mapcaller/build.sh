99eb39d000181ccfa7cb854de131d9cdd8992fe0
Peter van Heusden <pvh@sanbi.ac.za>
2019-09-28 18:12:37 +0200

    MapCaller recipe

---
+++ recipes/mapcaller/build.sh
@@ -0,0 +1,6 @@
+make LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include" CXXFLAGS="-I${PREFIX}/include"
+if [ ! -d $PREFIX/bin ] ; then
+  mkdir $PREFIX/bin
+fi
+
+cp MapCaller bwt_index $PREFIX/bin
