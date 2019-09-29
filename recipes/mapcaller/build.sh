3ac3ee54f4fe0a9a81e6107b54aeb0b21e6d5969
Peter van Heusden <pvh@sanbi.ac.za>
2019-09-29 06:32:48 +0200

    Add -L to CXXFLAGS

--- recipes/mapcaller/build.sh
+++ recipes/mapcaller/build.sh
@@ -1,4 +1,4 @@
-make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include"
+make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
