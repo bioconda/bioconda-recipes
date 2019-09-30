06bd6b43cab07daef08ec4ee3a8d6f358a826820
wdecoster <decosterwouter@gmail.com>
2019-09-30 11:32:10 +0200

    changing build cc

--- recipes/htsbox/build.sh
+++ recipes/htsbox/build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 #strictly use anaconda build environment
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 mkdir -p $PREFIX/bin
 
-make 
+make CC=${CC}
 mv htsbox  $PREFIX/bin
 
