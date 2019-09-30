f5fe95a81701aeed6f8c773dd1827cdb0832c387
wdecoster <decosterwouter@gmail.com>
2019-09-30 12:00:03 +0200

    adding quote for cflags

--- recipes/htsbox/build.sh
+++ recipes/htsbox/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #strictly use anaconda build environment
-export CFLAGS="${CFLAGS} -L${PREFIX}/lib
+export CFLAGS="${CFLAGS} -L${PREFIX}/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
