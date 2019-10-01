5156d01b93b507c5936217114b788e5ad56a95d8
Torsten Seemann <torsten.seemann@gmail.com>
2019-10-01 10:23:12 +1000

    vcflib 1.0.1

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
+export C_INCLUDE_PATH="$PREFIX/include"
+export CPLUS_INCLUDE_PATH="$PREFIX/include"
 
-export LDFLAGS="-L$PREFIX/lib -L\$(LIB_DIR) -lvcflib -lhts -lpthread -lz -lm -llzma -lbz2"
-export INCLUDES="-I . -Ihtslib -I$PREFIX/include -Itabixpp/htslib -I\$(INC_DIR) -L. -Ltabixpp/htslib"
-export LIBPATH="-L. -Lhtslib -L$PREFIX/lib"
-export CXXFLAGS="-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
+export INCLUDES="-I. -Itabixpp/htslib -I$C_INCLUDE_PATH"
+export LIBPATH="-L. -Ltabixpp/htslib -L$CPLUS_INCLUDE_PATH"
+export LIBS="-lvcflib -lhts -lpthread -lz -lm -llzma -lbz2"
+export LDFLAGS="$LIBPATH $LIBS"
+export CFLAGS="-Ofast -s -D_FILE_OFFSET_BITS=64"
+export CXXFLAGS="-std=c++0x $CFLAGS"
 
-#sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' smithwaterman/Makefile
-#sed -i.bak 's/CFLAGS/CXXFLAGS/g' smithwaterman/Makefile
-
-#sed -i.bak 's/$</$< $(LDFLAGS)/g' smithwaterman/Makefile
 sed -i.bak 's/ld/$(LD)/' smithwaterman/Makefile
-sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' filevercmp/Makefile
-sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' multichoose/Makefile
-sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' intervaltree/Makefile
 
+#for MOD in multichoose intervaltree tabixpp ; do
+#  sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' $MOD/Makefile
+#done  
+
+for MOD in filevercmp tabixpp fastahack ; do
+  sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' $MOD/Makefile
+done
+
+  
 # MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
 if [ "$(uname)" == "Darwin" ]; then
     sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' smithwaterman/Makefile
-    export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
+    export CXXFLAGS="${CFLAGS} -std=c++11 -stdlib=libc++"
     sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' intervaltree/Makefile
     sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' Makefile
     sed -i.bak 's/if ( n_data/if ( \*n_data/' src/cdflib.cpp
-    
 fi
-# tabix missing library https://github.com/ekg/tabixpp/issues/5
-# Uses newline trick for OSX from: http://stackoverflow.com/a/24299845/252589
-#sed -i.bak 's/SUBDIRS=./SUBDIRS=.\'$'\n''LOBJS=tabix.o/' tabixpp/Makefile
-#sed -i.bak 's/-ltabix//' Makefile
-#sed -i.bak 's/make/make -e/' Makefile
 
-make -e
+# -e lets our exported CXX etc override any Makefile settings
+make --environment-overrides -j${CPU_COUNT}
 
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
