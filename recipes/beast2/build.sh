628bdf26d6d0d371ef32c70e5d8cdaa58c79d6f2
Mattias <m.dehollander@nioo.knaw.nl>
2019-10-01 16:31:47 +0200

    Work on build script and fix license

--- recipes/beast2/build.sh
+++ recipes/beast2/build.sh
@@ -2,9 +2,11 @@
 ant linux
 
 mkdir -p ${PREFIX}/bin ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/lib
+
 for file in applauncher beast beauti densitree loganalyser logcombiner packagemanager treeannotator
-    do envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}' < bin/$file > ${PREFIX}/bin/$file
+    do envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}' < release/Linux/beast/bin/$file > ${PREFIX}/bin/$file
     chmod +x ${PREFIX}/bin/$file
 done
-cp -R lib ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
-cp -R {examples,images,templates} ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+
+cp -R release/Linux/beast/lib/ ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+cp -R release/Linux/beast/{examples,images,templates} ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
