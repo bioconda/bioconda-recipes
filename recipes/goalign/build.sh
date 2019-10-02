17c64fc704adaa84b4359f8a0f799c6323e71e2c
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-10-02 13:15:36 +0200

    goalign v0.3.1

--- recipes/goalign/build.sh
+++ recipes/goalign/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-chmod a+x goalign_*
+# Goalign executable
 mkdir -p $PREFIX/bin
+chmod a+x goalign_*
 cp goalign_* $PREFIX/bin/goalign
+
+# Goalign test script
+wget https://raw.githubusercontent.com/evolbioinfo/goalign/v${PKG_VERSION}/test.sh
+chmod a+x test.sh
+sed 's+GOALIGN=./goalign+GOALIGN=goalign+g' test.sh | sed "s+TESTDATA=\"tests/data\"+TESTDATA=$PREFIX/goalign_test_data+g"  > $PREFIX/bin/goalign_test.sh
+
+# Goalign test data
+mkdir  $PREFIX/goalign_test_data
+wget https://github.com/evolbioinfo/goalign/raw/v${PKG_VERSION}/tests/data/test_distance.phy.gz
+wget https://github.com/evolbioinfo/goalign/raw/v${PKG_VERSION}/tests/data/test_rawdistance.phy.gz
+wget https://github.com/evolbioinfo/goalign/raw/v${PKG_VERSION}/tests/data/test_rawdistance2.phy.gz
+mv test_* $PREFIX/goalign_test_data
