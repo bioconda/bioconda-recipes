b54757f8053a52f8aa7cc2b7c972a3602298f851
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-10-02 17:11:15 +0200

    goalign v0.3.1

--- recipes/goalign/build.sh
+++ recipes/goalign/build.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
-chmod a+x goalign_*
-mkdir -p $PREFIX/bin
-cp goalign_* $PREFIX/bin/goalign
+#!/usr/bin/env bash
+
+export CGO_ENABLED=0
+
+go get -u github.com/golang/dep/cmd/dep
+cd "$GOPATH/src/github.com/evolbioinfo/goalign"
+dep ensure
+go build -o ${PREFIX}/bin/goalign -ldflags "-X github.com/evolbioinfo/goalign/version.Version=${PKG_VERSION}" github.com/evolbioinfo/goalign
+
+# Goalign test data
+cp -r tests/data $PREFIX/goalign_test_data
+
+# Goalign test script
+sed 's+GOALIGN=./goalign+GOALIGN=goalign+g' test.sh | sed "s+TESTDATA=\"tests/data\"+TESTDATA=\$PREFIX/goalign_test_data/+g"  > $PREFIX/bin/goalign_test.sh
+chmod a+x $PREFIX/bin/goalign_test.sh
