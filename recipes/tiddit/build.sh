5336d92390a1748307b2f8315cba5da8e9d06c76
Devon Ryan <dpryan79@gmail.com>
2019-10-07 17:37:19 +0200

    clean up

--- recipes/tiddit/build.sh
+++ recipes/tiddit/build.sh
@@ -2,17 +2,14 @@
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/bin/bin
 echo $PREFIX
 
-cp -r * $outdir/
-cd $outdir
 mkdir build
 cd build
 cmake ..
 make
-cd ..
-cd src
+make DESTDIR=${PREFIX} install
+cd $outdir/src
 python setup.py build_ext --inplace
 
 ln -s $outdir/TIDDIT.py $PREFIX/bin/tiddit
