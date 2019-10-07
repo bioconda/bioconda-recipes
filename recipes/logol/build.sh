89655de9be159ca668a9e2a95e04057fbbb5fcde
dpryan79 <dpryan79@gmail.com>
2019-10-07 13:32:51 +0200

    tests fail though

--- recipes/logol/build.sh
+++ recipes/logol/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# it's unclear if it's possible to inject compilers into swi-prolog
+mkdir -p ${PREFIX}/bin
+ln -s $CC ${PREFIX}/bin/gcc
+
 #to fix problems with zlib
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PREFIX}/lib
-export JAVA_HOME=${PREFIX}/jre
+export JAVA_HOME=${PREFIX}
 
 # skip ruby tests
 export TRAVIS=1
@@ -24,3 +28,4 @@ mkdir -p $PREFIX/lib/logol
 cp -r * $PREFIX/lib/logol/
 cd $PREFIX/bin && ln -s ../lib/logol/LogolExec.sh
 cd $PREFIX/bin && ln -s ../lib/logol/LogolMultiExec.sh
+unlink ${PREFIX}/bin/gcc
