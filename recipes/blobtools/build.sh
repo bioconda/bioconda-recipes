19fa609c365af62d340d778df5c38c301b3430b3
dpryan79 <dpryan79@gmail.com>
2019-10-07 10:20:31 +0200

    patch and simplify

--- recipes/blobtools/build.sh
+++ recipes/blobtools/build.sh
@@ -1,11 +1,7 @@
 #/bin/bash
 
-mkdir -p ${PREFIX}/bin
-rm blobtools
 mv lib blobtools
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
-echo -e "#!/opt/anaconda1anaconda2anaconda3/bin/python\nfrom blobtools.interface import main\nmain()" > ${PREFIX}/bin/blobtools
-chmod +x ${PREFIX}/bin/*
 
 echo '#!/usr/bin/env bash' > $PREFIX/bin/blobtools-build_nodesdb
 echo "blobtools=$blobtools" >> $PREFIX/bin/blobtools-build_nodesdb
