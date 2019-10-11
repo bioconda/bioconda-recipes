79bb7119f332a3df66580a5eab4fca85c6f1e094
dpryan79 <dpryan79@gmail.com>
2019-10-11 11:58:48 +0200

    should compile now

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -3,10 +3,10 @@
 export C_INCLUDE_PATH="$PREFIX/include"
 export CPLUS_INCLUDE_PATH="$PREFIX/include"
 
-export INCLUDES="-I. -Itabixpp/htslib -I$C_INCLUDE_PATH"
+export INCLUDES="-I. -Itabixpp/htslib -Itabixpp -Ismithwaterman -Imultichoose -Ifastahack -Ifilevercmp -Iintervaltree -Ifsom -Igoogletest -IlibVCFH -I$C_INCLUDE_PATH"
 export LIBPATH="-L. -Ltabixpp/htslib -L$CPLUS_INCLUDE_PATH"
 export LIBS="-lvcflib -lhts -lpthread -lz -lm -llzma -lbz2"
-export LDFLAGS="$LIBPATH $LIBS"
+export LDFLAGS="$LIBPATH $LIBS -L${PREFIX}/lib"
 export CFLAGS="-Ofast -s -D_FILE_OFFSET_BITS=64"
 export CXXFLAGS="-std=c++0x $CFLAGS"
 
@@ -17,7 +17,7 @@ sed -i.bak 's/ld/$(LD)/' smithwaterman/Makefile
 #done  
 
 for MOD in filevercmp tabixpp fastahack ; do
-  sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' $MOD/Makefile
+  sed -i.bak 's#gcc#$(CC) $(CFLAGS) -L${PREFIX}/lib#g' $MOD/Makefile
 done
 
   
@@ -29,6 +29,10 @@ if [ "$(uname)" == "Darwin" ]; then
     sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' Makefile
     sed -i.bak 's/if ( n_data/if ( \*n_data/' src/cdflib.cpp
 fi
+
+pushd tabixpp
+make CC=$CC CXX=$CXX CXXFLAGS="$CXXFLAGS -Ihtslib" LIBPATH="-L${PREFIX}/lib -L. -Lhtslib" INCLUDES="-Ihtslib"
+popd
 
 # -e lets our exported CXX etc override any Makefile settings
 make --environment-overrides -j${CPU_COUNT}
