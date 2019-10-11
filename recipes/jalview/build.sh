5036a98ef1730854417fa0810c04a53851bd0908
Jim Procter <jprocter@issues.jalview.org>
2019-10-11 17:05:41 +0100

    removed erroneous $SRC_DIR replacement - still with debugging info for locating artefact

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -2,6 +2,7 @@
 
 # compile jalview from source
 gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar
+|| echo faild build.
 
 # create target directory
 JALVIEWDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
@@ -12,13 +13,13 @@ mkdir -p $JALVIEWDIR
 # debug - is the JAR even generated ???
 ls -lR $SRC_DIR | grep jalview-all
 
-cp -R $SRC_DIR/build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
+cp -R build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
 # copy wrapper and make executable
 cp $RECIPE_DIR/jalview.sh $JALVIEWDIR/.
 chmod +x $JALVIEWDIR/jalview.sh; 
 
 # link wrapper script
 mkdir -p $PREFIX/bin
-ln -s $JALVIEWDIR/jalview.sh $PREFIX/bin/jalview
+ln -fs $JALVIEWDIR/jalview.sh $PREFIX/bin/jalview
 chmod +x $PREFIX/bin/jalview; 
 
