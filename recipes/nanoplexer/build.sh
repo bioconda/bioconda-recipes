c3e7bbabb24c46e3a576cab9118995a61fdc12d6
Devon Ryan <dpryan79@users.noreply.github.com>
2019-10-12 15:09:17 +0200

    Update build.sh

--- recipes/nanoplexer/build.sh
+++ recipes/nanoplexer/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
 mkdir -p $PREFIX/bin
-make
+make CC=$CC CFLAGS="$CFLAGS -L${PREFIX}/lib"
 cp ./nanoplexer $PREFIX/bin/nanoplexer
-
