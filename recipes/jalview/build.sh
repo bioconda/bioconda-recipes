d74efc0e55645b9c9aa44bdb7f50064597c08618
Jim Procter <jprocter@issues.jalview.org>
2019-10-13 14:57:48 +0100

    upstream gradle build didn't set rootProject.name so it defaulted to dirname for CWD which was also used for shadowJar archive name. will be addressed in 2.11.1 - see https://issues.jalview.org/browse/JAL-3421

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# patch jalview 2.11.0 gradle.properties so the shadowJar task generates a jar called jalview-all-2.11.0-j1.8.jar
+echo "rootProject.name = 'jalview'" >> settings.gradle
+
 # compile jalview from source
-gradle --debug -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
+gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
 
 # create target directory
 JALVIEWDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $JALVIEWDIR
 
 # copy jalview files to target
-
-# debug - is the JAR even generated ???
-#ls -lR $SRC_DIR | grep jalview-all
 
 cp -R build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
 # copy wrapper and make executable
