50fbaa447e38b67b6033d060ce0126cbe90b9a04
dpryan79 <dpryan79@gmail.com>
2019-10-14 11:15:26 +0200

    libGL

--- recipes/plantcv/build.sh
+++ recipes/plantcv/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-find / -name libGL.so.1 -print
 sed -i'' -e 's/versioneer.get_version()/"3.3.0"/g' setup.py
 sed -i'' -e 's/cmdclass=versioneer.get_cmdclass(),//g' setup.py
 sed -i'' -e 's/setup_requires=\["pytest-runner"\],//g' setup.py
