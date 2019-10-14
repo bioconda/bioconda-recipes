1e9a69d1e9124f20b3f1d9006a3b20d14df210eb
Jim Procter <foreveremain@gmail.com>
2019-10-14 07:23:14 +0100

    UPDATE jalview to 2.11.0 (#18028)
    * UPDATE jalview to 2.11.0 (gradle java 11 build, java 8 runtime, single executable jar automatically configures memory)
    
    * reset build numbre to 0
    
    * fix conda dependency version numbers
    
    * try better to locate shadowJar build artefact
    
    * removed erroneous $SRC_DIR replacement - still with debugging info for locating artefact
    
    * add debug info for checking if return code of gradle is non-zero
    
    * remove erroneous debug message - fingers crossed..
    
    * Set archiveFileName for gradle
    
    Test according [SO answer](https://stackoverflow.com/a/31588712/6914989), [gradle CLI doc](https://docs.gradle.org/current/userguide/command_line_interface.html#environment_options), [gradle doc](https://docs.gradle.org/current/dsl/org.gradle.api.tasks.bundling.Jar.html#org.gradle.api.tasks.bundling.Jar:archiveFileName).
    
    * Set archiveDirectory for gradle
    
    According to [gradle CLI doc](https://docs.gradle.org/current/dsl/org.gradle.api.tasks.bundling.Jar.html#org.gradle.api.tasks.bundling.Jar:destinationDirectory)
    
    * Reverting
    
    No luck testing gradle parameters...
    
    * gradle debug mode reports archive path so we can work out where its being written too!
    
    * upstream gradle build didn't set rootProject.name so it defaulted to dirname for CWD which was also used for shadowJar archive name. will be addressed in 2.11.1 - see https://issues.jalview.org/browse/JAL-3421
    
    * typo in jar name
    
    * Update meta.yaml

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+# patch jalview 2.11.0 gradle.properties so the shadowJar task generates a jar called jalview-all-2.11.0-j1.8.jar
+echo "rootProject.name = 'jalview'" >> settings.gradle
+
 # compile jalview from source
-ant -DINSTALLATION="bioconda (build $PKG_BUILDNUM)" -DJALVIEW_VERSION="$PKG_VERSION" -Dgit.commit="" -Dgit.branch="" makedist
+gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
 
 # create target directory
 JALVIEWDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $JALVIEWDIR
 
 # copy jalview files to target
-cp -R dist/* $JALVIEWDIR/.
+
+cp -R build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
 # copy wrapper and make executable
 cp $RECIPE_DIR/jalview.sh $JALVIEWDIR/.
 chmod +x $JALVIEWDIR/jalview.sh; 
 
 # link wrapper script
 mkdir -p $PREFIX/bin
-ln -s $JALVIEWDIR/jalview.sh $PREFIX/bin/jalview
+ln -fs $JALVIEWDIR/jalview.sh $PREFIX/bin/jalview
 chmod +x $PREFIX/bin/jalview; 
 
