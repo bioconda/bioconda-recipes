1b76bfa5db21678158753744c472c03d573088e8
emilhaegglund <haegglund.emil@gmail.com>
2019-10-14 10:53:59 +0200

    update meta.yaml

--- recipes/divvier/build.sh
+++ recipes/divvier/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
+make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS} -L${PREFIX}/lib"  CXXFLAGS="${CXXFLAGS} -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 make
+
 mkdir -p ${PREFIX}/bin
-cp divvier ${PrEFIX}/bin/
+cp ./divvier ${PREFIX}/bin/
