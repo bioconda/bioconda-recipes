ad0c0ac3ac114e1ad2746ba7570a7476cc5555cd
dpryan79 <dpryan79@gmail.com>
2019-10-15 15:55:08 +0200

    try this

--- recipes/duphold/build.sh
+++ recipes/duphold/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# Compile nim
+pushd nim_source
+bash build.sh --os linux --cpu x86_64
+bname=`basename $CC`
+echo "gcc.exe = \"${bname}\"" >> config/nim.cfg
+echo "gcc.linkerexe = \"${bname}\"" >> config/nim.cfg
+bin/nim c  koch #--gcc.exe=$CC --gcc.linkerexe=$CC koch 
+./koch tools #--gcc.exe=$CC --gcc.linkerexe=$CC
+popd
+
+export PATH=$SRC_DIR/nim_source/bin:$PATH
+
+# the actual recipe
 mkdir -p $PREFIX/bin
-chmod a+x duphold_static
-cp duphold_static $PREFIX/bin/duphold
+nimble install -y --verbose
+mv duphold ${PREFIX}/bin
