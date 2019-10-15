cdeda24dc732015be3177dc5911c50cb3936f091
Jim Procter <foreveremain@gmail.com>
2019-10-15 07:19:17 +0100

    jalview-2.11.0 runtime support for openjdk>=8 (#18080)
    * UPDATE jalview to 2.11.0 (gradle java 11 build, java 8 runtime, single executable jar automatically configures memory)
    
    * reset build numbre to 0
    
    * fix conda dependency version numbers
    
    * try better to locate shadowJar build artefact
    
    * removed erroneous $SRC_DIR replacement - still with debugging info for locating artefact
    
    * add debug info for checking if return code of gradle is non-zero
    
    * remove erroneous debug message - fingers crossed..
    
    * Set archiveFileName for gradle
    
    Test according [SO answer](https://stackoverflow.com/a/31588712/6914989), [gradle CLI doc](https://docs.gradle.org/current/userguide/command_line_interface.html#environment_options), [gradle doc](https://docs.gradle.org/current/dsl/org.gradle.api.tasks.bundling.Jar.html#org.gradle.api.tasks.bundling.Jar:archiveFileName).
    
    * Set archiveDirectory for gradle
    
    According to [gradle CLI doc](https://docs.gradle.org/current/dsl/org.gradle.api.tasks.bundling.Jar.html#org.gradle.api.tasks.bundling.Jar:destinationDirectory)
    
    * Reverting
    
    No luck testing gradle parameters...
    
    * gradle debug mode reports archive path so we can work out where its being written too!
    
    * upstream gradle build didn't set rootProject.name so it defaulted to dirname for CWD which was also used for shadowJar archive name. will be addressed in 2.11.1 - see https://issues.jalview.org/browse/JAL-3421
    
    * typo in jar name
    
    * jalview-2.11 build & wrapper patch to support openjdk>=8 runtime
    
    * fix broken conditional - testing for openjdk 8 presence
    
    * remember to define deployment directory before copying to it !

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# patch jalview 2.11.0 gradle.properties so the shadowJar task generates a jar called jalview-all-2.11.0-j1.8.jar
-echo "rootProject.name = 'jalview'" >> settings.gradle
-
-# compile jalview from source
-gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
-
 # create target directory
 JALVIEWDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $JALVIEWDIR
 
-# copy jalview files to target
+# patch jalview 2.11.0 gradle.properties so the shadowJar task generates jars called jalview-all-2.11.0-j{11,1.8}.jar
+echo "rootProject.name = 'jalview'" >> settings.gradle
 
-cp -R build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
+# compile jalview from source
+
+# First the Java 1.8 build
+gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
+
+# copy jalview jar to target
+cp -vR build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
+
+# Now the Java 11 build
+gradle -PJAVA_VERSION=11 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
+
+# copy jalview jar to target
+cp -vR build/libs/jalview-all-$PKG_VERSION-j11.jar $JALVIEWDIR/jalview-all-j11.jar
+
 # copy wrapper and make executable
 cp $RECIPE_DIR/jalview.sh $JALVIEWDIR/.
 chmod +x $JALVIEWDIR/jalview.sh; 
