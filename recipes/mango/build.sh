5d20ead6478958d56ece504381f6234e98219d05
Alyssa Morrow <akmorrow@berkeley.edu>
2019-10-16 12:00:17 -0700

    update

--- recipes/mango/build.sh
+++ recipes/mango/build.sh
@@ -6,9 +6,30 @@ PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $BINARY_HOME
 mkdir -p $PACKAGE_HOME
 
-cp -R * $PACKAGE_HOME
+ls $PACKAGE_HOME
+cp -R core/* $PACKAGE_HOME
 
 ln -s $PACKAGE_HOME/bin/mango-notebook $BINARY_HOME
 ln -s $PACKAGE_HOME/bin/mango-submit $BINARY_HOME
 ln -s $PACKAGE_HOME/bin/make_genome $BINARY_HOME
 
+echo "PYTHON VERSION----"
+$PYTHON --version
+echo $PY_VER
+which $PYTHON
+which python3.7
+
+# modin
+$PYTHON -m pip install python_modin/.[dask] --no-deps --ignore-installed -vv
+
+# adam
+$PYTHON -m pip install python_adam/. --no-deps --ignore-installed -vv
+
+# mangos
+$PYTHON -m pip install python/. --no-deps --ignore-installed -vv
+
+$PYTHON -m pip install python_pileup/. --no-deps --ignore-installed -vv
+# # Enable widget
+# jupyter nbextension enable --py widgetsnbextension
+# jupyter nbextension install --py --symlink --user bdgenomics.mango.pileup
+# jupyter nbextension enable bdgenomics.mango.pileup --user --py
