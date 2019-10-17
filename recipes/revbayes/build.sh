e25ad78ed8baffccbbbc59da0d901d7dd4226578
Roman Briskine <roman.briskine@gmail.com>
2019-10-17 15:58:17 +0200

    remove INTERNAL_BOOST flag; add license_file property

--- recipes/revbayes/build.sh
+++ recipes/revbayes/build.sh
@@ -8,16 +8,14 @@ fi
 
 ./generate_version_number.sh
 mv GitVersion.cpp ../../src/revlanguage/utils/
-./regenerate.sh -boost false -mpi true
+./regenerate.sh -mpi true
 
 cd build
 
-# INTERNAL_BOOST=OFF instructs not to use the included version
 # If cmake finds a boost version compiled with cmake, it always links to it unless both
 # Boost_NO_SYSTEM_PATHS=ON and Boost_NO_BOOST_CMAKE=ON (probably a bug in cmake)
 cmake -DCMAKE_PREFIX_PATH=$PREFIX \
 	-DBOOST_ROOT=$PREFIX \
-	-DINTERNAL_BOOST=OFF \
 	-DBoost_NO_SYSTEM_PATHS=ON \
 	-DBoost_NO_BOOST_CMAKE=ON \
 	.
@@ -27,5 +25,4 @@ make
 cd ..
 mkdir -p $PREFIX/bin
 mv rb $PREFIX/bin
-
 
