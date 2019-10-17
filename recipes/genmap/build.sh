ca854b6914f4098def3a36747e8364efd9ba13ae
cpockrandt <cpockrandt@users.noreply.github.com>
2019-10-17 13:46:02 -0400

    GenMap: fixed recipe (#18137)

--- recipes/genmap/build.sh
+++ recipes/genmap/build.sh
@@ -3,6 +3,6 @@
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DGENMAP_NATIVE_BUILD=OFF
 make
 make install
