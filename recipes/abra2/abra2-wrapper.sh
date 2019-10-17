f7fcb9584e0c87204cda9d9d3ca1a4613c2d775d
Michael Knudsen <micknudsen@gmail.com>
2019-10-17 09:57:03 +0200

    Debugging

--- recipes/abra2/abra2-wrapper.sh
+++ recipes/abra2/abra2-wrapper.sh
@@ -4,6 +4,14 @@ jar_name="abra2.jar"
 jar_dir=$(dirname "$(pwd -P "${BASH_SOURCE[0]}")")
 jar_path="${jar_dir}/${jar_name}"
 
+echo "***** DEBUG START *****"
+
+echo $jar_name
+echo $jar_dir
+echo $jar_path
+
+echo "***** DEBUG END *****"
+
 if [[ -z "${JAVA_TOOL_OPTIONS}" ]]; then
     JAVA_TOOL_OPTIONS="-Xmx4G"
     export JAVA_TOOL_OPTIONS
