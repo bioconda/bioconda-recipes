0023ad0869a8827b2a328001c6e506e4d6d69782
dpryan79 <dpryan79@gmail.com>
2019-10-18 09:00:33 +0200

    remove previous hack

--- recipes/mash/build.sh
+++ recipes/mash/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# wrong version string: https://github.com/marbl/Mash/issues/124
-sed -i.bak 's/2.2/2.2.1/' src/mash/version.h
-
 ./bootstrap.sh
 ./configure --with-capnp=$PREFIX --with-gsl=$PREFIX --prefix=$PREFIX
 make
