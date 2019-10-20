c7df6de23369665c8fe968f03acf3a5770899c01
Devon Ryan <dpryan79@users.noreply.github.com>
2019-10-20 13:15:20 +0200

    Update build.sh

--- recipes/cryfa/build.sh
+++ recipes/cryfa/build.sh
@@ -3,9 +3,8 @@ export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 #export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
-apt-get install cmake
 cmake .
-make -j8
+make
 
-mkdir -p "$PREFIX/bin"
+mkdir -p $PREFIX/bin
 cp cryfa $PREFIX/bin/
