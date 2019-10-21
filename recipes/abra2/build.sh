86472d1d3496693368ba57f44497a01cba62937d
Michael Knudsen <micknudsen@gmail.com>
2019-10-21 10:20:21 +0200

    Added ABRA2 recipe (#18093)
    * Added ABRA2 recipe
    
    * Replaced java-jdk with openjdk
    
    * Inject compilers
    
    * Update build.sh
    
    * Removed quotes
    
    * Update meta.yaml
    
    * Added coreutils as host requirement
    
    * Moved coreutils to build requirements
    
    * Try avoiding realpath
    
    * Debugging
    
    * Cleaned up
    
    * Debugging
    
    * Debugging
    
    * Trying with realpath again
    
    * Updated test
    
    * Single quotes
    
    * More quotes
    
    * Maybe direction to /dev/null is needed
    
    * This works for other recipes
    
    * Is it really the test failing?
    
    * Yes, it is the test failing. But why?
    
    * Skip Mac OS builds
    
    * This has worked for other recipes
    
    * ...but it doesn't work here
    
    * Update meta.yaml
    
    * Update abra2-wrapper.sh
    
    * Update abra2-wrapper.sh

---
+++ recipes/abra2/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+sed -i.bak "s#g++#${CXX}#" Makefile
+make
+outdir="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
+mkdir -p "${outdir}"
+mkdir -p "${PREFIX}/bin"
+
+cp target/abra2-2.20-jar-with-dependencies.jar "${outdir}/abra2.jar"
+cp "${RECIPE_DIR}/abra2-wrapper.sh" "${outdir}/abra2"
+chmod +x "${outdir}/abra2"
+ln -s "${outdir}/abra2" "${PREFIX}/bin/abra2"
