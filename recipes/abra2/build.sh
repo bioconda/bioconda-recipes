03eff17bcb8ff23f877f3d0791c5aa1ab72c4c74
dpryan79 <dpryan79@gmail.com>
2019-10-21 11:12:53 +0200

    tweak

--- recipes/abra2/build.sh
+++ recipes/abra2/build.sh
@@ -8,7 +8,7 @@ outdir="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 mkdir -p "${outdir}"
 mkdir -p "${PREFIX}/bin"
 
-cp target/abra2-2.20-jar-with-dependencies.jar "${outdir}/abra2.jar"
+cp target/abra2-${PKG_VERSION}-jar-with-dependencies.jar "${outdir}/abra2.jar"
 cp "${RECIPE_DIR}/abra2-wrapper.sh" "${outdir}/abra2"
 chmod +x "${outdir}/abra2"
 ln -s "${outdir}/abra2" "${PREFIX}/bin/abra2"
