98e16212bb4bb9ec7af43de918a7cc8ec747ac40
Martin Raden <mmann@informatik.uni-freiburg.de>
2019-10-24 23:04:41 +0200

    Dialign tx unblacklist (#18268)
    * dialign-tx removed
    
    * trigger rebuild (unblacklisted)
    
    * corrected sed call
    
    * sed again
    
    * fix source code: remove "inline" from structs
    
    * tuning sed for OSX
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * patch
    
    * update patch

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -2,19 +2,6 @@
 set -euo pipefail
 
 cd ./source
-
-# disable CPPFLAGS in Makefile to
-# - update architecture
-# - disable SSE optimization
-# - disable MMX optimization
-sed -i.bak -e 's/^(CPPFLAGS=.*)$/#\1/g' Makefile
-# disable explicit CC setting
-sed -i -e 's/^(CC=.*)$/#\1/g' Makefile
-
-# set general CPPFLAGS (-funroll-loops taken from original Makefile)
-export CPPFLAGS="-I$PREFIX/include -O3 -march=native -funroll-loops"
-export LDFLAGS="-L$PREFIX/lib"
-
 mkdir -p $PREFIX/bin
-make
+make CC="$CC $CFLAGS $LDFLAGS" CPPFLAGS="$CPPFLAGS"
 mv dialign-tx $PREFIX/bin/
