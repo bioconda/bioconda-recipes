b554f369e39fbb2826efbdb961ef847eacfc2af1
Devon Ryan <dpryan79@users.noreply.github.com>
2019-10-24 22:13:53 +0200

    Update build.sh

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -19,5 +19,5 @@ cd ./source
 sed -i.bak -e 's/inline//g' *.c
 
 mkdir -p $PREFIX/bin
-make CC="$CC $CFLAGS $LDFLAGS"
+make CC="$CC $CFLAGS $LDFLAGS" CPPFLAGS="$CPPFLAGS"
 mv dialign-tx $PREFIX/bin/
