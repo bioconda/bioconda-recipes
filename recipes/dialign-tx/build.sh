d1f090b8a6d6168117273a15061b1f93b458ac94
Devon Ryan <dpryan79@gmail.com>
2019-10-24 22:40:59 +0200

    patch

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -2,10 +2,6 @@
 set -euo pipefail
 
 cd ./source
-
-# fix source code: remove "inline" from structs
-#sed -i.bak -e 's/inline//g' *.c
-
 mkdir -p $PREFIX/bin
 make CC="$CC $CFLAGS $LDFLAGS" CPPFLAGS="$CPPFLAGS"
 mv dialign-tx $PREFIX/bin/
