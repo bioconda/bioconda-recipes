2d273d693b4d877d7f53e3e22fd4d5ed3e720378
Martin Raden <mmann@informatik.uni-freiburg.de>
2019-10-24 13:37:48 +0200

    disable explicit CC setting

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -8,6 +8,8 @@ cd ./source
 # - disable SSE optimization
 # - disable MMX optimization
 sed -i.bak -e 's/^(CPPFLAGS=.*)$/#\1/g' Makefile
+# disable explicit CC setting
+sed -i -e 's/^(CC=.*)$/#\1/g' Makefile
 
 # set general CPPFLAGS (-funroll-loops taken from original Makefile)
 export CPPFLAGS="-I$PREFIX/include -O3 -march=native -funroll-loops"
