8fa6ebc21f3af308e46648786eb2211cd339d514
Martin Raden <mmann@informatik.uni-freiburg.de>
2019-10-24 14:10:41 +0200

    dialign-tx : disable SSE and MMX optimization (#18264)
    * disable SSE and MMX optimization
    
    * bump build number, since rebuild without SSE/MMX
    
    * disable explicit CC setting

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -euo pipefail
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-sed -i.bak -e 's/-march=i686/-march=native/' ./source/Makefile
+
 cd ./source
+
+# disable CPPFLAGS in Makefile to
+# - update architecture
+# - disable SSE optimization
+# - disable MMX optimization
+sed -i.bak -e 's/^(CPPFLAGS=.*)$/#\1/g' Makefile
+# disable explicit CC setting
+sed -i -e 's/^(CC=.*)$/#\1/g' Makefile
+
+# set general CPPFLAGS (-funroll-loops taken from original Makefile)
+export CPPFLAGS="-I$PREFIX/include -O3 -march=native -funroll-loops"
+export LDFLAGS="-L$PREFIX/lib"
 
 mkdir -p $PREFIX/bin
 make
