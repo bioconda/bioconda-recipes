973cc36ea641318f0f89615d330a68bbeec1fe10
Devon Ryan <dpryan79@users.noreply.github.com>
2019-10-24 22:30:32 +0200

    Update build.sh

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -3,20 +3,8 @@ set -euo pipefail
 
 cd ./source
 
-# disable CPPFLAGS in Makefile to
-# - update architecture
-# - disable SSE optimization
-# - disable MMX optimization
-#sed -i.bak -e 's/^CPPFLAGS=/#CPPFLAGS=/g' Makefile
-# disable explicit CC setting
-#sed -i.bak -e 's/^CC=/#CC=/g' Makefile
-
-# set general CPPFLAGS (-funroll-loops taken from original Makefile)
-#export CPPFLAGS="-I$PREFIX/include -O3 -march=native -funroll-loops"
-#export LDFLAGS="-L$PREFIX/lib"
-
 # fix source code: remove "inline" from structs
-sed -i.bak -e 's/inline//g' *.c
+#sed -i.bak -e 's/inline//g' *.c
 
 mkdir -p $PREFIX/bin
 make CC="$CC $CFLAGS $LDFLAGS" CPPFLAGS="$CPPFLAGS"
