2094ca92307ddb24067714340d50447f8df10c93
Martin Raden <mmann@informatik.uni-freiburg.de>
2019-10-24 16:11:07 +0200

    sed again

--- recipes/dialign-tx/build.sh
+++ recipes/dialign-tx/build.sh
@@ -7,9 +7,9 @@ cd ./source
 # - update architecture
 # - disable SSE optimization
 # - disable MMX optimization
-sed -i.bak 's/^(CPPFLAGS=.*)$/#\1/g' Makefile
+sed -i.bak 's/^CPPFLAGS=/#CPPFLAGS=/g' Makefile
 # disable explicit CC setting
-sed -i 's/^(CC=.*)$/#\1/g' Makefile
+sed -i 's/^CC=/#CC=/g' Makefile
 
 # set general CPPFLAGS (-funroll-loops taken from original Makefile)
 export CPPFLAGS="-I$PREFIX/include -O3 -march=native -funroll-loops"
