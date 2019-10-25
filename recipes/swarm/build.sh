a58bea519f2a7297846ebc1e44f8dfb37a126afb
Devon Ryan <dpryan79@users.noreply.github.com>
2019-10-25 07:39:36 +0200

    Update build.sh

--- recipes/swarm/build.sh
+++ recipes/swarm/build.sh
@@ -4,9 +4,5 @@ make -C src CXX=${CXX}
 cp bin/swarm ${PREFIX}/bin
 cp man/swarm.1 ${PREFIX}/share/man/man1
 cp man/swarm_manual.pdf ${PREFIX}/share
-if [[ "${PY_VER}" =~ 3 ]]
-then
-	2to3 -w -n scripts/*
-fi
 chmod +x scripts/*
 cp scripts/* ${PREFIX}/bin
