9faae6bc20c291197af301e17ea0696b70321ef0
Devon Ryan <dpryan79@gmail.com>
2019-10-27 10:25:53 +0100

    more updates

--- recipes/minimap/build.sh
+++ recipes/minimap/build.sh
@@ -4,8 +4,8 @@ mkdir -p $PREFIX/bin
 
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make CC=$CC
-make extra CC=$CC
+make CC=$CC CFLAGS="$CFLAGS" LIBS="-L${PREFIX}/lib -lm -lz -lpthread"
+make extra CC=$CC CFLAGS="$CFLAGS" LIBS="-L${PREFIX}/lib -lm -lz -lpthread"
 
 cp minimap $PREFIX/bin 
 cp minimap-lite $PREFIX/bin
