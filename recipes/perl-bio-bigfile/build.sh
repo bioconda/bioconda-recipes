f4129c8975e03e844adac41a434d42bd76e684e7
vladsaveliev <vladislav.sav@gmail.com>
2019-11-01 19:47:26 +1100

    Bio-BigFile: add kent library

--- recipes/perl-bio-bigfile/build.sh
+++ recipes/perl-bio-bigfile/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export LD=$CC
+
+# Compiling the kent source tree
+wget http://hgdownload.cse.ucsc.edu/admin/jksrc.zip
+unzip jksrc.zip
+(cd kent/src/lib && make)
+export KENT_SRC=$PWD/kent/src
+
 perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
 perl ./Build
 # Make sure this goes in site
 perl ./Build install --installdirs site
+
