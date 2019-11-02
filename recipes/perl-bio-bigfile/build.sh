8b7d97c7628d23f25c78714b0a7b53496c968103
vladsaveliev <vladislav.sav@gmail.com>
2019-11-02 14:04:10 +1100

    Bio-BigFile: add perl-module-build into host section, set CPPFLAGS and PERL5LIB, use make -C for kent lib

--- recipes/perl-bio-bigfile/build.sh
+++ recipes/perl-bio-bigfile/build.sh
@@ -2,9 +2,10 @@
 export LD=$CC
 
 # Compiling the kent source tree
-cd kent/src/lib && make
+export CPPFLAGS=-I$PREFIX/include
+make -C kent/src/lib
 export KENT_SRC=$SRC_DIR/kent/src
-
+export PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0
 perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
 perl ./Build
 # Make sure this goes in site
