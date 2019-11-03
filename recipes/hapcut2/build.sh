fac3f80ba99a9b564ef2f6d3c996bc844799bc9d
Marcel Martin <marcel.martin@scilifelab.se>
2019-11-03 16:11:51 +0100

    Create lowercased "hapcut2" alias only on Linux

--- recipes/hapcut2/build.sh
+++ recipes/hapcut2/build.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 mkdir ${PREFIX}/bin
 cp -p build/HAPCUT2 build/extractHAIRS ${PREFIX}/bin/
-ln -s HAPCUT2 ${PREFIX}/bin/hapcut2
 
+if [ $(uname) == Linux ]; then
+	ln -s HAPCUT2 ${PREFIX}/bin/hapcut2
+fi
 for script in LinkFragments.py calculate_haplotype_statistics.py; do
   cp -p utilities/${script} ${PREFIX}/bin
   chmod +x ${PREFIX}/bin/${script}
