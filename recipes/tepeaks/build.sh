7656a70ea3bb2205dfae7de3105778eb15675e65
dpryan79 <dpryan79@gmail.com>
2019-11-04 09:49:31 +0100

    wrong makefile

--- recipes/tepeaks/build.sh
+++ recipes/tepeaks/build.sh
@@ -4,4 +4,4 @@ pushd src/htslib-1.2.1_src
 make CC=${CC} CFLAGS="$CFLAGS" prefix=${PREFIX} LDFLAGS="$LDFLAGS"
 popd
 
-make CC=${CXX} prefix=${PREFIX}
+make -f Makefile.LINUX CC=${CXX} prefix=${PREFIX}
