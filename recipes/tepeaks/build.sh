3c63f9b6147060da354c266352ff7ad8e9f98a93
dpryan79 <dpryan79@gmail.com>
2019-11-04 11:38:21 +0100

    export BOOST_ROOT

--- recipes/tepeaks/build.sh
+++ recipes/tepeaks/build.sh
@@ -4,4 +4,5 @@ pushd src/htslib-1.2.1_src
 make CC=${CC} CFLAGS="$CFLAGS" prefix=${PREFIX} LDFLAGS="$LDFLAGS"
 popd
 
+export BOOST_ROOT=${PREFIX}/include
 make -f Makefile.LINUX CC=${CXX} prefix=${PREFIX} CFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
