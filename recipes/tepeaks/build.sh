18216912d0af54125b0c31966fbaf9781731a112
dpryan79 <dpryan79@gmail.com>
2019-11-04 09:40:29 +0100

    LDFLAGS

--- recipes/tepeaks/build.sh
+++ recipes/tepeaks/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f TEpeaks *.a
 pushd src/htslib-1.2.1_src
-make CC=${CC} CFLAGS="$CFLAGS" prefix=${PREFIX}
+make CC=${CC} CFLAGS="$CFLAGS" prefix=${PREFIX} LDFLAGS="$LDFLAGS"
 popd
 
 make CC=${CXX} prefix=${PREFIX}
