733d46e4218ec2ee3dbd2d94173db368411bf27b
dpryan79 <dpryan79@gmail.com>
2019-11-04 09:58:36 +0100

    more updates

--- recipes/tepeaks/build.sh
+++ recipes/tepeaks/build.sh
@@ -4,4 +4,4 @@ pushd src/htslib-1.2.1_src
 make CC=${CC} CFLAGS="$CFLAGS" prefix=${PREFIX} LDFLAGS="$LDFLAGS"
 popd
 
-make -f Makefile.LINUX CC=${CXX} prefix=${PREFIX}
+make -f Makefile.LINUX CC=${CXX} prefix=${PREFIX} CFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
