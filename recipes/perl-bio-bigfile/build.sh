289ff84ba37999b7ef0ef5f2e086f42c3ca96a2f
vladsaveliev <vladislav.sav@gmail.com>
2019-11-05 19:44:18 +1100

    BigFile: use VEP docs to install kent binaries https://asia.ensembl.org/info/docs/tools/vep/script/vep_download.html

--- recipes/perl-bio-bigfile/build.sh
+++ recipes/perl-bio-bigfile/build.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
 export LD=$CC
 
+CFLAGS="$CFLAGS -L$PREFIX/lib -I$PREFIX/include"
+LIBS="$LDFLAGS -L$PREFIX/lib"
+CPPFLAGS=$CFLAGS
+
 # Compiling the kent source tree
-export CPPFLAGS=-I$PREFIX/include
-make -C kent/src/lib
-export KENT_SRC=$SRC_DIR/kent/src
-export PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0
-perl Build.PL --extra_compiler_flags "-I$PREFIX/include"
+export MACHTYPE=$(uname -m)
+export KENT_SRC=$SRC_DIR/kent-335_base/src
+export MYSQLINC="$(mysql_config --include | sed -e 's/^-I//g')"
+export MYSQLLIBS="$(mysql_config --libs)"
+echo 'CFLAGS="-fPIC"' > $KENT_SRC/inc/localEnvironment.mk
+make -C $KENT_SRC/lib prefix=$PREFIX/ CC=$CC CFLAGS="$CFLAGS" LIBS="$LIBS"
+
+# Building the module
+export PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0:$PERL5LIB
+perl Build.PL --extra_compiler_flags "$CFLAGS" --extra_linker_flags "$CFLAGS"
 perl ./Build
 # Make sure this goes in site
 perl ./Build install --installdirs site
