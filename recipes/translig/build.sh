732cba2e2ce51474130a32a868353419fa9a8856
Devon Ryan <dpryan79@gmail.com>
2019-11-08 22:56:27 +0100

    updates

--- recipes/translig/build.sh
+++ recipes/translig/build.sh
@@ -3,4 +3,9 @@ export BOOST_ROOT=${PREFIX}
 export CFLAGS="$CFLAGS $LDFLAGS"
 ./configure --prefix=${PREFIX} --with-boost-libdir=${PREFIX}/lib
 make
-make install
+mkdir -p ${PREFIX}/bin
+mv Assemble ${PREFIX}/bin/
+mv refine ${PREFIX}/bin/
+mv connect_graph ${PREFIX}/bin/
+mv TransLiG_iteration ${PREFIX}/bin/
+mv Get_output ${PREFIX}/bin/
