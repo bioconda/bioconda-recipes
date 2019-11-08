3c58bdbfead990e6cf1300a9b1b5763c5a42e504
Devon Ryan <dpryan79@gmail.com>
2019-11-08 23:21:26 +0100

    try this

--- recipes/translig/build.sh
+++ recipes/translig/build.sh
@@ -3,9 +3,11 @@ export BOOST_ROOT=${PREFIX}
 export CFLAGS="$CFLAGS $LDFLAGS"
 ./configure --prefix=${PREFIX} --with-boost-libdir=${PREFIX}/lib
 make
+ls -lh
+ls -lh src
 mkdir -p ${PREFIX}/bin
-mv Assemble ${PREFIX}/bin/
-mv refine ${PREFIX}/bin/
-mv connect_graph ${PREFIX}/bin/
-mv TransLiG_iteration ${PREFIX}/bin/
-mv Get_output ${PREFIX}/bin/
+mv src/Assemble ${PREFIX}/bin/
+mv src/refine ${PREFIX}/bin/
+mv src/connect_graph ${PREFIX}/bin/
+mv src/TransLiG_iteration ${PREFIX}/bin/
+mv src/Get_output ${PREFIX}/bin/
