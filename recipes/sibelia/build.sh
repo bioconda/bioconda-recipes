e01c17fe2816677c0ace6bffff7b6a70686f7121
Devon Ryan <dpryan79@gmail.com>
2019-11-10 21:05:25 +0100

    updates

--- recipes/sibelia/build.sh
+++ recipes/sibelia/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i.bak "s#gcc#$CC#g;s#g++#$CXX#g" src/lagan/src/Makefile
 cd build
 cmake ../src -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CC_COMPILER=$CC
 make
