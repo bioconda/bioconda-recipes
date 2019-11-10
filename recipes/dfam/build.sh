6b20677f81ae97ef47855b9143115a164fbd2158
Devon Ryan <dpryan79@gmail.com>
2019-11-10 16:03:43 +0100

    update dfam

--- recipes/dfam/build.sh
+++ recipes/dfam/build.sh
@@ -5,6 +5,7 @@ mkdir -p $PREFIX/bin
 
 
 chmod 777 dfamscan.pl
+gunzip dfamscan.pl.gz
 cp dfamscan.pl $PREFIX/bin
 cp $RECIPE_DIR/download-dfam.py $PREFIX/bin
 chmod 777 $PREFIX/bin/download-dfam.py
