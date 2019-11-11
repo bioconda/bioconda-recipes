60acf55ca6c4466e7f5ec5bfc26c477b9468de46
Devon Ryan <dpryan79@users.noreply.github.com>
2019-11-11 11:01:55 +0100

    more unblacklisted recipes (#18577)
    * more unblacklisted recipes
    
    * lint
    
    * updates
    
    * updates

--- recipes/bcftools/1.3/build.sh
+++ recipes/bcftools/1.3/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make plugins CC=$CC
-make prefix=$PREFIX CC=$CC install
+make plugins CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+make prefix=$PREFIX CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" install
