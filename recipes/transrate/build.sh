90b17cc5a2b6f36f0111886732c8d4fc90e33a88
dpryan79 <dpryan79@gmail.com>
2019-11-12 10:42:05 +0100

    should work now

--- recipes/transrate/build.sh
+++ recipes/transrate/build.sh
@@ -3,4 +3,9 @@ set -x
 mkdir -p ${PREFIX}/bin
 gem build transrate.gemspec
 gem install -n ${PREFIX}/bin transrate
-#ruby ${PREFIX}/bin/transrate --install-deps ref
+
+# Don't vendor deps like salmon and blast
+#ruby ${PREFIX}/bin/transrate --install-deps all
+
+# Clean up the build a bit
+rm ${PREFIX}/lib/ruby/gems/*/gems/transrate-*/ext/transrate/transrate.o
