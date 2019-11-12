86de63b3aa7db4937e4baf03ce1eae62860f5a2c
dpryan79 <dpryan79@gmail.com>
2019-11-12 10:03:42 +0100

    don't let transrate install its own binary deps

--- recipes/transrate/build.sh
+++ recipes/transrate/build.sh
@@ -3,5 +3,4 @@ set -x
 mkdir -p ${PREFIX}/bin
 gem build transrate.gemspec
 gem install -n ${PREFIX}/bin transrate
-ruby ${PREFIX}/bin/transrate --install-deps ref
-gem install io-console -v 0.4.6
+#ruby ${PREFIX}/bin/transrate --install-deps ref
