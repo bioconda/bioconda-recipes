fa51a882999f318409d81f1df460a01d0aed3efa
dpryan79 <dpryan79@gmail.com>
2019-11-12 10:29:53 +0100

    update blacklist and debug gmtk

--- recipes/gmtk/build.sh
+++ recipes/gmtk/build.sh
@@ -6,5 +6,5 @@ DIR_CC=$(dirname $CXX)
 PATH="$DIR_CC:$PATH"
 
 ./configure --with-cppCmd="$BASE_CC -E -x assembler-with-cpp" --with-logp=table --prefix=$PREFIX
-make
+make SHELL='sh -x'
 make install
