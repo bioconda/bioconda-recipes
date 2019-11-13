3081e5dd343f0f1df59bf3e35435aaf9a9e222cd
dpryan79 <dpryan79@gmail.com>
2019-11-13 23:22:00 +0100

    fix a few recipes

--- recipes/nasp/build.sh
+++ recipes/nasp/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-# Remove when go1.11 is available
-#go get -u golang.org/x/vgo
-
 pushd nasp/nasptool
 echo 'module "github.com/TGenNorth/nasp"' > go.mod
-vgo build -o ../nasptool_linux_64
+go build -o ../nasptool_linux_64
 popd
 
 $PYTHON -m pip install --no-deps --ignore-installed --no-cache-dir -vvv .
