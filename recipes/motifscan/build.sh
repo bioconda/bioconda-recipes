3a1b611081aa59be434c79d1b800a8733cdf3432
dpryan79 <dpryan79@gmail.com>
2019-11-13 10:22:13 +0100

    update flowcraft, motifscan and samsifter

--- recipes/motifscan/build.sh
+++ recipes/motifscan/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export CC=$CXX
-export LDSHARED=$CXX
+# setup.py attempts to override the compilers!
+sed -i.bak "5,6d" setup.py
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
