bcae7cc8ed0ae11bb2de0bca7204e19a602a4ddb
Devon Ryan <dpryan79@gmail.com>
2019-11-16 22:07:36 +0100

    should work now

--- recipes/iced/build.sh
+++ recipes/iced/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 find . -type f -name *_.c -exec rm -f {} \;
-#$PYTHON setup.py build_ext -i
 $PYTHON build_tools/cythonize.py iced
-find . -type f -name *.c -ls
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
