c9e92376b8a661292c472aceee47ec88f80af740
Devon Ryan <dpryan79@gmail.com>
2019-11-16 21:31:31 +0100

    cythonize

--- recipes/iced/build.sh
+++ recipes/iced/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find . -type f -name "*.c" -exec rm -f {} \;
-$PYTHON setup.py build_ext -i
+#$PYTHON setup.py build_ext -i
+$PYTHON build_tools/cythonize.py .
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
