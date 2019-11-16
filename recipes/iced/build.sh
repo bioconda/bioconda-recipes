a135229d07fdf23f217c9fe4e684288a3f140389
Devon Ryan <dpryan79@gmail.com>
2019-11-16 21:44:43 +0100

    don't remove all the C files

--- recipes/iced/build.sh
+++ recipes/iced/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-find . -type f -name "*.c" -exec rm -f {} \;
+find . -type f -name *_.c -exec rm -f {} \;
 #$PYTHON setup.py build_ext -i
-$PYTHON build_tools/cythonize.py .
+$PYTHON build_tools/cythonize.py iced
+find . -type -f -name *.c -ls
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
