ff88edf2a02d08f53b538eb0d450cdfcb2373ff3
Devon Ryan <dpryan79@gmail.com>
2019-11-16 21:55:01 +0100

    typo

--- recipes/iced/build.sh
+++ recipes/iced/build.sh
@@ -2,5 +2,5 @@
 find . -type f -name *_.c -exec rm -f {} \;
 #$PYTHON setup.py build_ext -i
 $PYTHON build_tools/cythonize.py iced
-find . -type -f -name *.c -ls
+find . -type f -name *.c -ls
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
