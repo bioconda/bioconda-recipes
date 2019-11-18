08dd6f9f97e8edeb7cd5398886ce2c318b3dd1cb
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-18 15:28:46 +0800

    modified:   build.sh

--- recipes/kart/build.sh
+++ recipes/kart/build.sh
@@ -3,5 +3,5 @@ if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
 
-make Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
+make Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 cp bin/kart bin/bwt_index $PREFIX/bin
