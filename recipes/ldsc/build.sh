9ed1471d79fe8919a727b3ae9c8d75af8952a4ff
Hervé Ménager <herve.menager@gmail.com>
2019-11-18 23:05:29 +0100

    store 176.patch locally for ldsc build

--- recipes/ldsc/build.sh
+++ recipes/ldsc/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eu -o pipefail
-wget https://patch-diff.githubusercontent.com/raw/bulik/ldsc/pull/176.patch 
-patch -p1 < 176.patch
+patch -p1 < $RECIPE_DIR/176.patch
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
