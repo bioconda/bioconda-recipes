137aac85c8828804d25a049d29079e4597d5d7ea
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-18 11:19:59 +0800

    modified:   kart/build.sh

--- recipes/kart/build.sh
+++ recipes/kart/build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
 make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
