12598ca4f44bc96450a83111eaf3970a98a9701d
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-18 11:43:28 +0800

    modified:   kart/build.sh 	modified:   kart/meta.yaml 	deleted:    kart/src_makefile_flags.patch

--- recipes/kart/build.sh
+++ recipes/kart/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
 
-make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
 
+make
 cp bin/kart bin/bwt_index $PREFIX/bin
