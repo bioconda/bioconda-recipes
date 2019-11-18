043d7b663452e3423b33c182b8c74d2d3fc6d707
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-18 12:21:39 +0800

    modified:   build.sh 	modified:   meta.yaml

--- recipes/kart/build.sh
+++ recipes/kart/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" 
-export CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
-export LDFLAGS="-L${PREFIX}/lib"
-
 if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
 
-make Compiler=$CXX CXX=$CXX CC=$CC
+make Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 cp bin/kart bin/bwt_index $PREFIX/bin
