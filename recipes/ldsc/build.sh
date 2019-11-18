2da913dfecd089c6e38a4fdfc24e2b3df5ab4ee2
Hervé Ménager <herve.menager@gmail.com>
2019-11-18 20:51:15 +0100

    improve recipe for ldsc
    based on recommendations from @abretaud:
    - do not include license
    - apply patch to use PR modifications instead of a mere wget

--- recipes/ldsc/build.sh
+++ recipes/ldsc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu -o pipefail
-wget https://raw.githubusercontent.com/bulik/ldsc/ead047d1ff743e9b09a117393f27790fe3323761/requirements.txt
-wget https://raw.githubusercontent.com/bulik/ldsc/ead047d1ff743e9b09a117393f27790fe3323761/setup.py
+wget https://patch-diff.githubusercontent.com/raw/bulik/ldsc/pull/176.patch 
+patch -p1 < 176.patch
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
