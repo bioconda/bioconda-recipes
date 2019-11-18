ab651efd8b1dbaa13f66c018f670fb0fa5ba350a
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-18 21:47:11 +0800

    modified:   build.sh

--- recipes/dart/build.sh
+++ recipes/dart/build.sh
@@ -3,5 +3,6 @@ if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
 
-make Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
+make 
+#Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 cp bin/dart bin/bwt_index $PREFIX/bin
