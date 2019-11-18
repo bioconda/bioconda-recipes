0df665afbd852639017a9aa0e010e2f3909f14a5
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-18 18:23:06 +0800

    new file:   gsalign/build.sh 	new file:   gsalign/meta.yaml

---
+++ recipes/gsalign/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ ! -d $PREFIX/bin ] ; then
+  mkdir $PREFIX/bin
+fi
+
+make Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
+cp bin/GSAlign bin/bwt_index $PREFIX/bin
