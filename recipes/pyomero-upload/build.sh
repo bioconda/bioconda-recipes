71a2bb7914fc6c52ac1dd1100b5cae22a40ef67c
Johnny Hay <j.hay@epcc.ed.ac.uk>
2019-11-19 15:37:07 +0000

    Add build.sh to create symlinks for SSL 1.0.0 libs required by Zeroc ICE

---
+++ recipes/pyomero-upload/build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Create Symbolic Links to libcrypto and libssl libraries so that OMERO client works
+ln -s /usr/lib/x86_64-linux-gnu/libssl.so.1.0.2 /usr/lib/x86_64-linux-gnu/libssl.so.1.0.0
+ln -s /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.2 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0
