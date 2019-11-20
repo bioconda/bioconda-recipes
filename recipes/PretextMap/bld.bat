b216c746adb45d2a67b7aac9dfda33669c6630b4
Ed Harry <e.f.harry@gmail.com>
2019-11-20 16:00:38 +0000

    PretextMap Version 0.1, changed to zip download

--- recipes/PretextMap/bld.bat
+++ recipes/PretextMap/bld.bat
@@ -1,3 +1,2 @@
-wget https://github.com/wtsi-hpag/PretextMap/releases/download/%PKG_VERSION%/PretextMap_Windows-x86-64.zip
-mkdir %PREFIX%/bin
-unzip PretextMap_Windows-x86-64.zip -d %PREFIX%/bin
+mkdir %PREFIX%\bin
+move PretextMap* %PREFIX%\bin\
--- recipes/PretextMap/build.sh
+++ recipes/PretextMap/build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     file=PretextMap_Linux-x86-64.zip;;
-    Darwin*)    file=PretextMap_MacOS.zip;;
-esac
-
-wget https://github.com/wtsi-hpag/PretextMap/releases/download/$PKG_VERSION/$file
 mkdir -p $PREFIX/bin
-unzip $file -d $PREFIX/bin
-for f in $PREFIX/bin/PretextMap*; do
-    chmod +x $f;
+for file in PretextMap*; do
+    chmod +x $file;
+    mv $file $PREFIX/bin/
 done
