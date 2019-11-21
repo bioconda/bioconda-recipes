9c9be990a354e807ace11f1d4abc8ba360b900c7
Ed Harry <e.f.harry@gmail.com>
2019-11-21 14:23:58 +0000

    PretextMap Version 0.1.1, ...

--- recipes/pretextmap/build.sh
+++ recipes/pretextmap/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-for file in Pretext*; do
+
+chmod +x install
+./install
+
+shopt -s extglob
+for file in $(compgen -G "Pretext!(*cpp)"); do
     chmod +x $file;
     mv $file $PREFIX/bin/
 done
