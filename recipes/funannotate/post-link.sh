45366c0db41954eba1d783846d6ffdd34469982d
Jon Palmer <nextgenusfs@gmail.com>
2019-11-21 20:56:55 -0800

    update post-link and explicitly add a few deps

--- recipes/funannotate/post-link.sh
+++ recipes/funannotate/post-link.sh
@@ -7,11 +7,19 @@ All Users:
   You will need to setup the funannotate databases using funannotate setup.
   The location of these databases on the file system is your decision and the
   location can be defined using the FUNANNOTATE_DB environmental variable.
+  
   To set this up in your conda environment you can run the following:
     echo "export FUNANNOTATE_DB=/your/path" > ${PREFIX}/etc/conda/activate.d/funannotate.sh
     echo "unset FUNANNOTATE_DB" > ${PREFIX}/etc/conda/deactivate.d/funannotate.sh
+  
   You can then run your database setup using funannotate:
     funannotate setup -i all
+    
+  Due to licensing restrictions, if you want to use GeneMark-ES/ET, you will need to install manually:
+  download and follow directions at http://topaz.gatech.edu/GeneMark/license_download.cgi
+  ** note you will likely need to change shebang line for all perl scripts:
+    change: #!/usr/bin/perl to #!/usr/bin/env perl
+     
       
 Mac OSX Users:
   Augustus and Trinity cannot be properly installed via conda/bioconda at this time. However,
