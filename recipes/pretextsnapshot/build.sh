f8140fb411c8e61a051a6f73439a95264d0fef7b
Ed Harry <e.f.harry@gmail.com>
2019-11-22 15:53:56 +0000

    ...

--- recipes/pretextsnapshot/build.sh
+++ recipes/pretextsnapshot/build.sh
@@ -2,6 +2,14 @@
 
 mkdir -p $PREFIX/bin
 
+echo "#include<math.h>" >> test.c
+echo "#include <stdlib.h>" >> test.c
+echo "int main(int argc, char **argv){" >> test.c
+echo "float x=sinf((float)atoi(argv[1]));" >> test.c
+echo "return((int)x);}" >> test.c
+clang -Ofast test.c -lm -o test
+ldd test
+
 chmod +x install
 ./install
 
