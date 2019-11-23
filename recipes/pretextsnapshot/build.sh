e24ee25a96a9230947c0cf16d2fcf0a172c2312d
Ed Harry <e.f.harry@gmail.com>
2019-11-23 18:55:30 +0000

    ...

--- recipes/pretextsnapshot/build.sh
+++ recipes/pretextsnapshot/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+esac
+
 mkdir -p $PREFIX/bin
 
-if [[ -v LD_RUN_PATH ]]; then
+if [ $machine = "Linux" ]; then
 awk -v rep="-static -Wl,-rpath,/usr/lib/x86_64-linux-gnu/ -Wl,-rpath-link,/usr/lib/x86_64-linux-gnu/ -L/usr/lib/x86_64-linux-gnu/ -lstdc++ -lm" '{ gsub(/\-lstdc\+\+ \-lm/, rep); print }' > _install < install
 mv install install_
 mv _install install
