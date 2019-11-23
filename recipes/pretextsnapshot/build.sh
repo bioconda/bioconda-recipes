8d4030fc0b09984aa45c27df7244211b4bd3f22b
Ed Harry <e.f.harry@gmail.com>
2019-11-23 18:43:27 +0000

    ...

--- recipes/pretextsnapshot/build.sh
+++ recipes/pretextsnapshot/build.sh
@@ -3,9 +3,9 @@
 mkdir -p $PREFIX/bin
 
 if [[ -v LD_RUN_PATH ]]; then
-    awk -v rep="-static -Wl,-rpath,/usr/lib/x86_64-linux-gnu/ -Wl,-rpath-link,/usr/lib/x86_64-linux-gnu/ -L/usr/lib/x86_64-linux-gnu/ -lstdc++ -lm" '{ gsub(/\-lstdc\+\+ \-lm/, rep); print }' > _install < install
-    mv install install_
-    mv _install install
+awk -v rep="-static -Wl,-rpath,/usr/lib/x86_64-linux-gnu/ -Wl,-rpath-link,/usr/lib/x86_64-linux-gnu/ -L/usr/lib/x86_64-linux-gnu/ -lstdc++ -lm" '{ gsub(/\-lstdc\+\+ \-lm/, rep); print }' > _install < install
+mv install install_
+mv _install install
 fi
 
 chmod +x install
