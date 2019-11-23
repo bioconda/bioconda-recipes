40af2d7ce918b275765370ae4406e12bcc9ca022
Ed Harry <e.f.harry@gmail.com>
2019-11-23 18:20:55 +0000

    ...

--- recipes/pretextsnapshot/build.sh
+++ recipes/pretextsnapshot/build.sh
@@ -2,13 +2,11 @@
 
 mkdir -p $PREFIX/bin
 
-echo "#include<math.h>" >> test.c
-echo "#include <stdlib.h>" >> test.c
-echo "int main(int argc, char **argv){" >> test.c
-echo "float x=sinf((float)atoi(argv[1]));" >> test.c
-echo "return((int)x);}" >> test.c
-clang -Ofast test.c -lm -o test
-ldd test
+if [[ -v LD_RUN_PATH ]]; then
+    awk -v rep="-static -Wl,-rpath,/usr/lib/x86_64-linux-gnu/ -Wl,-rpath-link,/usr/lib/x86_64-linux-gnu/ -L/usr/lib/x86_64-linux-gnu/ -lstdc++ -lm" '{ gsub(/\-lstdc\+\+ \-lm/, rep); print }' > _install < install
+    mv install install_
+    mv _install install
+fi
 
 chmod +x install
 ./install
