db60f9ab20c4cabc7213b8fee0a949323979a29a
dpryan79 <dpryan79@gmail.com>
2019-11-25 10:54:39 +0100

    inject jar path

--- recipes/mixcr/build.sh
+++ recipes/mixcr/build.sh
@@ -3,5 +3,6 @@
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share
 cp -f mixcr.jar $PREFIX/share/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar
-sed "s/jar\/mixcr.jar/share\/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar/g" mixcr > $PREFIX/bin/mixcr
+sed "190i\
+jar=\"/opt/anaconda1anaconda2anaconda3share/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar\"" mixcr > $PREFIX/bin/mixcr
 chmod +x $PREFIX/bin/mixcr
