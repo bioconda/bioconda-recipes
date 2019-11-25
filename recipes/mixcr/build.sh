9b33ffb1b4be0d0e63c5f95925d152d7690c9205
dpryan79 <dpryan79@gmail.com>
2019-11-25 11:02:07 +0100

    oops

--- recipes/mixcr/build.sh
+++ recipes/mixcr/build.sh
@@ -4,5 +4,5 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share
 cp -f mixcr.jar $PREFIX/share/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar
 sed "190i\
-jar=\"/opt/anaconda1anaconda2anaconda3share/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar\"" mixcr > $PREFIX/bin/mixcr
+jar=\"/opt/anaconda1anaconda2anaconda3/share/mixcr-$PKG_VERSION-$PKG_BUILDNUM.jar\"" mixcr > $PREFIX/bin/mixcr
 chmod +x $PREFIX/bin/mixcr
