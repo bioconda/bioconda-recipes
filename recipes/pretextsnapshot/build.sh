f0758cdba9d1bc5a2237e3d09c95280bc02bab49
Ed Harry <e.f.harry@gmail.com>
2019-11-26 13:50:55 +0000

    ...

--- recipes/pretextsnapshot/build.sh
+++ recipes/pretextsnapshot/build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-esac
-
 mkdir -p $PREFIX/bin
-
-if [ $machine = "Linux" ]; then
-awk -v rep="-fno-builtin-exp -fno-builtin-log -fno-builtin-pow -lstdc++ -lm" '{ gsub(/\-lstdc\+\+ \-lm/, rep); print }' > _install < install
-mv install install_
-mv _install install
-fi
 
 chmod +x install
 ./install
