f24a2b383e9d0b9d6382b7c514a54a4fcfab16d6
Devon Ryan <dpryan79@users.noreply.github.com>
2019-11-27 00:19:25 +0100

    Update build.sh

--- recipes/igphyml/build.sh
+++ recipes/igphyml/build.sh
@@ -2,4 +2,4 @@ mv $( pwd ) $PREFIX/share/igphyml
 ./make_phyml
 mkdir -p $PREFIX/bin
 ln -s $PREFIX/share/igphyml/src/igphyml $PREFIX/bin/igphyml
-rm $PREFIX/share/igphyml/compile $PREFIX/share/igphyml/missing $PREFIX/share/igphyml/decomp $PREFIX/share/igphyml/con* $PREFIX/share/igphyml/install-sh $PREFIX/share/igphyml/INSTALL
+rm $PREFIX/share/igphyml/compile $PREFIX/share/igphyml/missing $PREFIX/share/igphyml/depcomp $PREFIX/share/igphyml/con* $PREFIX/share/igphyml/install-sh $PREFIX/share/igphyml/INSTALL
