88a6b5bcf0a63fb59943f530b912b35166b06cd6
dpryan79 <dpryan79@gmail.com>
2019-11-26 10:42:47 +0100

    Remove patch

--- recipes/mapcaller/build.sh
+++ recipes/mapcaller/build.sh
@@ -1,4 +1,4 @@
-make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
+make Compiler=$CXX CXX=$CXX CC=$CC  LDFLAGS="-L${PREFIX}/lib" CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" CXXFLAGS="-I${PREFIX}/include -L${PREFIX}/lib" FLAGS="$CXXFLAGS -Wall -D NDEBUG -O3 -m64 -msse4.1 -fPIC"
 if [ ! -d $PREFIX/bin ] ; then
   mkdir $PREFIX/bin
 fi
