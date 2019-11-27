4caa3241558d49ee8a87f789602b2f2093401b62
Romain Feron <romain.feron.91@gmail.com>
2019-11-27 09:27:58 +0100

    First attempt at building without CLAPACK on macOS

--- recipes/phast/build.sh
+++ recipes/phast/build.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-echo "COMPILER"
-gcc=$CC
-
 # Create bin folder
 mkdir -p $PREFIX/bin
 
-# PHAST requires CLAPACK to be installed with the default directory structure
+# On Linux, PHAST requires CLAPACK to be installed with the default directory structure.
 # CLAPACK conda package does not follow this structure and it's easier to
 # download and compile CLAPACK from source
-wget http://www.netlib.org/clapack/clapack.tgz
-tar -xf clapack.tgz
-cd CLAPACK-3.2.1
-cp make.inc.example make.inc && make CC=$CC f2clib && make CC=$CC blaslib && make CC=$CC lib
+if [[ "$OSTYPE" != "darwin"* ]]; then
+	wget http://www.netlib.org/clapack/clapack.tgz
+	tar -xf clapack.tgz
+	cd CLAPACK-3.2.1
+	cp make.inc.example make.inc && make CC=$CC f2clib && make CC=$CC blaslib && make CC=$CC lib
+fi
 
 # Installing PHAST
 cd ../src
 
-# PHAST needs a path to Clapack libraries at compile time
-make CC=$CC CLAPACKPATH=$SRC_DIR/CLAPACK-3.2.1
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	make CC=$CC
+else
+	# On linux, PHAST needs a path to Clapack libraries at compile time
+	make CC=$CC CLAPACKPATH=$SRC_DIR/CLAPACK-3.2.1
+fi
 
 # PHAST builds multiple binaries
 cd ..
