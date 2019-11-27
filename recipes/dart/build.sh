8e0165ca3b97debe160869ce5b6b9ff33b14b55f
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-27 19:06:48 +0800

    modified:   recipes/dart/build.sh 	modified:   recipes/dart/meta.yaml

--- recipes/dart/build.sh
+++ recipes/dart/build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-make Compiler=$CXX CXX=$CXX CC=$CC LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
-cp bin/dart bin/bwt_index $PREFIX/bin
+mkdir -p ${PREFIX}/bin
+
+#index
+pushd src/BWT_Index
+make CC=$CC FLAGS="$CFLAGS" LIBS="$LDFLAGS -lm -lz"
+cp bwt_index $PREFIX/bin
+popd
+
+#htslib
+pushd src/htslib
+make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" libhts.a
+popd
+
+#kart
+pushd src
+make CXX=$CXX FLAGS="$CXXFLAGS -Wall -D NDEBUG -O3 -m64 -msse4.1 -fPIC" LIB="$LDFLAGS -lz -lm -lbz2 -llzma -lpthread"
+cp dart $PREFIX/bin
+
