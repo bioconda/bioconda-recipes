3094098b1f3d75969ee5e262d22773dd0f0cfb4e
hsinnan75 <arith@iis.sinica.edu.tw>
2019-11-27 10:27:11 +0800

    modified:   build.sh

--- recipes/kart/build.sh
+++ recipes/kart/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ${PREFIX}/bin
 
-# index
+#index
 pushd src/BWT_Index
 make CC=$CC FLAGS="$CFLAGS" LIBS="$LDFLAGS -lm -lz"
 cp bwt_index $PREFIX/bin
@@ -12,7 +12,7 @@ pushd src/htslib
 make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" libhts.a
 popd
 
-#Mapcaller
+#kart
 pushd src
 make CXX=$CXX FLAGS="$CXXFLAGS -Wall -D NDEBUG -O3 -m64 -msse4.1 -fPIC" LIB="$LDFLAGS -lz -lm -lpthread"
 cp kart $PREFIX/bin
