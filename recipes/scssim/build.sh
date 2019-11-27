403609884a2e6a8848e71fe7c4060f51adcdc66b
Warren Kretzschmar <warrenk@kth.se>
2019-11-27 16:33:19 +0100

    Remove debugging info

--- recipes/scssim/build.sh
+++ recipes/scssim/build.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-# Overwrite plot-vcfstats with updated version
-ls
 cmake .
 make
 make install PREFIX=$PREFIX
-
-#cmake -D CMAKE_AR=$(which x86_64-conda_cos6-linux-gnu-ar)\
-#      .
-# make
-# bin_loc=$(readlink -f bin/scssim)
-# popd
-# popd
-# install $bin_loc bin
-# rm -rf $wd  
-
-
-# sed -i.bak 's@^#!/usr/bin/perl -w@#!/opt/anaconda1anaconda2anaconda3/bin/perl@' misc/vcfutils.pl
-# ./configure --prefix=$PREFIX --with-htslib=system --enable-libgsl
-# make all
-# make install
