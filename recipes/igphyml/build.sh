807160ec93590a83e18161a665cd0a34607d5004
Devon Ryan <dpryan79@gmail.com>
2019-11-27 11:26:46 +0100

    Add igphyml

---
+++ recipes/igphyml/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/share/igphyml
+
+aclocal
+autoheader
+autoconf -f
+automake -f --add-missing
+./configure
+
+pushd src
+cp -r motifs ${PREFIX}/share/igphyml
+
+for f in *.c ; do
+  o=${f%%.c}.o;
+  $CC -DUNIX -DPHYML -DDEBUG -DNBLAS -I. $CFLAGS -fopenmp -DHTABLE="\"../share/igphyml\"" $LDFLAGS -c -o $o $f
+done
+$CC $CFLAGS $LDFLAGS -o ${PREFIX}/bin/igphyml main.o utilities.o optimiz.o lk.o bionj.o models.o free.o help.o simu.o eigen.o pars.o alrt.o controller.o cl.o spr.o stats.o nucle2codon.o io.o -lm -lomp
