3a46d5a7fb6f14d0b84c8b13bf7235d396691d27
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-11-29 03:43:16 +0700

    update

--- recipes/genie/build.sh
+++ recipes/genie/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ls -al
+mkdir -p src/github.com/sakkayaphab/bolt
+cp -r * src/github.com/sakkayaphab/bolt
+cd src/github.com/sakkayaphab/bolt
 go build main.go -o genie
+ls -al
 mkdir -p $PREFIX/bin
 cp genie $PREFIX/bin
