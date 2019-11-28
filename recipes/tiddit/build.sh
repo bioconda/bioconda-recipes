5279e40d953163b59586bb7c4e79bb3d4797a4bd
Devon Ryan <dpryan79@users.noreply.github.com>
2019-11-28 19:26:32 +0100

    Update build.sh

--- recipes/tiddit/build.sh
+++ recipes/tiddit/build.sh
@@ -2,6 +2,7 @@
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
 echo $PREFIX
 
 mkdir build
@@ -9,8 +10,7 @@ cd build
 cmake ..
 make
 cp ../bin/TIDDIT $PREFIX/bin
-echo "../lib"
-ls -l ../lib
+cp ../lib/bamtools $PREFIX/lib  # Yes, this vendors bamtools :(
 cd ../src
 python -m pip install . --ignore-installed --no-deps -vv
 cd ../build
@@ -21,5 +21,4 @@ cd ..
 rm -rf $PREFIX/usr
 
 mv TIDDIT.py ${PREFIX}/bin
-#ln -s ${PREFIX}/bin/TIDDIT.py $PREFIX/bin/TIDDIT
 chmod a+x ${PREFIX}/bin/*
