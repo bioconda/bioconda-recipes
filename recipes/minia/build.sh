38b29840de28e66d7ff1710c4a3441714f9c9e9c
cguyomar <cervin.guyomar@gmail.com>
2019-11-28 13:59:06 +0100

    recipe for minia

---
+++ recipes/minia/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export CPATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS -DUSE_NEW_CXX" ..
+make -j2
+
+mkdir -p $PREFIX/bin/
+
+cp bin/minia ${PREFIX}/bin/
+chmod +x $PREFIX/bin/minia
+chmod +x $PREFIX/bin/merci
+
+cp ext/gatb-core/bin/gatb-h5dump ${PREFIX}/bin/
+chmod +x $PREFIX/bin/gatb-h5dump
+cp ext/gatb-core/bin/dbginfo ${PREFIX}/bin/
+chmod +x $PREFIX/bin/dbginfo
+
