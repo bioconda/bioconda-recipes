b8c62316cbcc5223c1fd43f7c4b19bb9f721acb4
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-11-29 05:13:07 +0700

    update

--- recipes/genie/build.sh
+++ recipes/genie/build.sh
@@ -4,9 +4,10 @@ ls -al
 go version
 pwd
 mkdir -p src/github.com/sakkayaphab/bolt
+mv bed cli fasta gff hts variant vcf main.go go.sum go.mod vendor src/github.com/sakkayaphab/bolt
 cd src/github.com/sakkayaphab/bolt
 pwd
-export GOPATH=$HOME/go
+export GOPATH="$SRC_DIR/"
 GO111MODULE=on go build main.go -o genie
 ls -al
 mkdir -p $PREFIX/bin
