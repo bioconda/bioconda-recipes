7901b495c2bd42bc05545b457dc8cb771abea979
dpryan79 <dpryan79@gmail.com>
2019-11-29 15:23:24 +0100

    updates

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
@@ -13,8 +13,8 @@ export CPATH=${PREFIX}/include
 mkdir -p fake-home
 export HOME=$PWD/fake-home
 export STACK_ROOT="$HOME/.stack"
-stack setup --local-bin-path ${PREFIX}/bin
-stack install --local-bin-path ${PREFIX}/bin
+stack setup --local-bin-path ${PREFIX}/bin --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include --with-gmp-libraries ${PREFIX}/lib
+stack install --local-bin-path ${PREFIX}/bin --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include --with-gmp-libraries ${PREFIX}/lib
 
 #cleanup
 rm -r .stack-work
