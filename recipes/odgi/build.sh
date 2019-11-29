e820ba48272d06a76d7718c3447a944e5fd884f1
Devon Ryan <dpryan79@users.noreply.github.com>
2019-11-29 10:41:53 +0100

    Update build.sh

--- recipes/odgi/build.sh
+++ recipes/odgi/build.sh
@@ -4,11 +4,12 @@ export LD_LIBRARY_PATH=${PREFIX}/lib
 export LIBRARY_PATH=${PREFIX}/lib
 cmake -H. -Bbuild
 ls -l
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-      -DCMAKE_CXX_COMPILER=${CXX} \
-      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
-      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-      --build build
+#cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#      -DCMAKE_CXX_COMPILER=${CXX} \
+#      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
+#      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+#      --build build
+cmake --build build
 echo "bin"
 ls -l bin
 echo "lib"
