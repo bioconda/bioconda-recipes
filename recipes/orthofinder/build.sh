62d3bf0975280362cfd976de5edf5456e9961b62
David Emms <david_emms@hotmail.com>
2019-11-29 10:51:09 +0000

    Update python requirement

--- recipes/orthofinder/build.sh
+++ recipes/orthofinder/build.sh
@@ -4,9 +4,8 @@ mkdir -p $PREFIX/bin
 
 cp orthofinder.py $PREFIX/bin/orthofinder
 
+# scripts_of now contains the config.json file
 cp -r scripts_of $PREFIX/bin/
-
-#cp scripts_of/config.json $PREFIX/bin/scripts_of/
 
 cp -r tools $PREFIX/bin/
 
