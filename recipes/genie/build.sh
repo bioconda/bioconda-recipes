790da9df49bc12cd113ea142f9247abaea87cae7
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-11-29 13:30:49 +0700

    Remove unnecessary commands

--- recipes/genie/build.sh
+++ recipes/genie/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-ls -al
 go version
-pwd
 mkdir -p src/github.com/sakkayaphab/bolt
 mv bed cli fasta gff hts variant vcf main.go go.sum go.mod vendor src/github.com/sakkayaphab/bolt
 cd src/github.com/sakkayaphab/bolt
-pwd
-ls -al
 export GOPATH="$SRC_DIR/"
 go get ./...
 go build -o genie main.go 
-ls -al
 mkdir -p $PREFIX/bin
 cp genie $PREFIX/bin
