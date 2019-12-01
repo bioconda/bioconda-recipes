b0f50a7767b6e3bbcaf63abb2c85299ac04e28ef
Douglas Wu <wckdouglas@gmail.com>
2019-11-30 23:14:48 -0500

    fix gcc? in build

--- recipes/primer3-py/build.sh
+++ recipes/primer3-py/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i.bak 's/gcc/\$CC/g' primer3/src/libprimer3/Makefile
+sed -i.bak 's/gcc/\$\{CC\}/g' primer3/src/libprimer3/Makefile
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
