313b7db1a5b78270be2bda4924146578b76951a0
Devon Ryan <dpryan79@users.noreply.github.com>
2019-12-02 08:58:27 +0100

    Update build.sh

--- recipes/blast/2.2.21/build.sh
+++ recipes/blast/2.2.21/build.sh
@@ -11,6 +11,6 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "Platform: Linux"
     cp $SRC_DIR/bin/* $PREFIX/bin/
-    ln -s ${PREFIX}/lib/libbz2.so.1 ${PREFIX}/lib/libbz2.so
+    ln -s ${PREFIX}/lib/libbz2.so ${PREFIX}/lib/libbz2.so.1
 fi
 chmod +x $PREFIX/bin/*
