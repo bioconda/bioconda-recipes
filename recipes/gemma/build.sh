1f11af95d1b30896abee152442e2dc90bc90ac2d
Niklas Mähler <niklas.mahler@gmail.com>
2019-12-02 14:30:16 +0100

    Flag -no-pie not supported by clang

--- recipes/gemma/build.sh
+++ recipes/gemma/build.sh
@@ -5,9 +5,15 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-LIBS="${PREFIX}/lib/libgsl.a ${PREFIX}/lib/libgslcblas.a -L${PREFIX}/lib -no-pie -pthread -lopenblas -lz -lgfortran -lquadmath"
+LIBS="${PREFIX}/lib/libgsl.a ${PREFIX}/lib/libgslcblas.a -L${PREFIX}/lib -pthread -lopenblas -lz -lgfortran -lquadmath"
 
-make EIGEN_INCLUDE_PATH="${PREFIX}/include/eigen3" WITH_OPENBLAS=1 DEBUG=0 GCC_FLAGS="-Wall" LIBS="${LIBS}"
+if [ $(uname) == "Darwin" ]; then
+    PIE=""
+else
+    PIE="-no-pie"
+fi
+
+make EIGEN_INCLUDE_PATH="${PREFIX}/include/eigen3" WITH_OPENBLAS=1 DEBUG=0 GCC_FLAGS="-Wall" LIBS="${LIBS} ${PIE}"
 
 mkdir -p ${PREFIX}/bin
 cp bin/gemma ${PREFIX}/bin/gemma
