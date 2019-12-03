dd2a2642c997df2e1ff81538df880e5ac598fc6b
Fiona J Whelan <fiona.jane.whelan@gmail.com>
2019-12-03 10:03:33 +0000

    [ci skip] Merge PR 19031
    Merge PR 19031

---
+++ recipes/coinfinder/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX .
+cmake --build .
+mkdir -p ${PREFIX}/bin
+mv coinfinder ${PREFIX}/bin
