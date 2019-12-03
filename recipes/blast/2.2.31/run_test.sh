0b81d86cf1fe26a7271bf3a57f23e6986aa0a9d4
Devon Ryan <dpryan79@users.noreply.github.com>
2019-12-03 15:56:35 +0100

    Update run_test.sh

--- recipes/blast/2.2.31/run_test.sh
+++ recipes/blast/2.2.31/run_test.sh
@@ -22,9 +22,7 @@ blastn \
 echo PASS
 echo -n 'Search and Fetch An Entry From Database... '
 blastdbcmd -db testdb -entry gnl1 -out test_query.fa
-cat test_query.fa
-grep -c ">gnl1" test_query.fa
-test "$(grep -c '>gnl1' test_query.fa)" -eq 1
+test "$(grep -c '>lcl|gnl1' test_query.fa)" -eq 1
 test "$(grep -c '>' test_query.fa)" -eq 1
 test "$(wc -l test_query.fa)" -eq 10
 echo PASS
