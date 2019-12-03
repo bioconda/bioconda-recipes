9fe5b07652d1286480a34c3bfc0f804d56f8ddae
Gil Freund <gilf@sysnet.co.il>
2019-12-03 16:41:56 +0000

    Possible type in build.sh

--- recipes/fastml/build.sh
+++ recipes/fastml/build.sh
@@ -2,7 +2,6 @@
 
 set -e -x -o pipefail
 make CC=$GXX CXX=$GXX
-cp ${SRC_DIR}/www/fastml/FastML_Wrapper.pl $PREFIX/bin
-#echo "perl $PREFIX/bin/FastML_Wrapper.pl" > $PREFIX/bin/fastml
-#chmod +x $PREFIX/bin/fastml
+mkdir -p ${PREFIX}/bin
+cp www/fastml/FastML_Wrapper.pl ${PREFIX}/bin
 
