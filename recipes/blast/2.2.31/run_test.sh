c010be3fb09aa5fbd0b56d6391c5ae1719e896eb
Devon Ryan <dpryan79@users.noreply.github.com>
2019-12-03 15:05:15 +0100

    Update run_test.sh

--- recipes/blast/2.2.31/run_test.sh
+++ recipes/blast/2.2.31/run_test.sh
@@ -17,17 +17,16 @@ echo PASS
 echo -n 'Searching Database for Hits... '
 blastn \
   -query test.fa -db testdb -task blastn -dust no \
-  -outfmt "7 qseqid sseqid evalue bitscore" -max_target_seqs 6
-blastn \
-  -query test.fa -db testdb -task blastn -dust no \
   -outfmt "7 qseqid sseqid evalue bitscore" -max_target_seqs 6 | \
   grep -q " 3 hits found"
 echo PASS
 echo -n 'Search and Fetch An Entry From Database... '
 blastdbcmd -db testdb -entry gnl1 -out test_query.fa
-test "$(grep -c ">gnl1" < test_query.fa)" -eq 1
-test "$(grep -c ">" < test_query.fa)" -eq 1
-test "$(wc -l < test_query.fa)" -eq 10
+cat test_query.fa
+grep -c ">gnl1" test_query.fa
+test "$(grep -c '>gnl1' test_query.fa)" -eq 1
+test "$(grep -c '>' test_query.fa)" -eq 1
+test "$(wc -l test_query.fa)" -eq 10
 echo PASS
 echo
 echo "ALL TESTS PASSED"
