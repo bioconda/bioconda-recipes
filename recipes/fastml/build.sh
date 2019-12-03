fab3dab91080d7ce8d1d8b336d274105baa94b37
Gil Freund <gilf@sysnet.co.il>
2019-12-03 16:15:55 +0000

    Added post-link.sh for Perl wrapper

--- recipes/fastml/build.sh
+++ recipes/fastml/build.sh
@@ -3,6 +3,6 @@
 set -e -x -o pipefail
 make CC=$GXX CXX=$GXX
 cp ${SRC_DIR}/www/fastml/FastML_Wrapper.pl $PREFIX/bin
-echo "perl $PREFIX/bin/FastML_Wrapper.pl" > $PREFIX/bin/fastml
-chmod +x $PREFIX/bin/fastml
+#echo "perl $PREFIX/bin/FastML_Wrapper.pl" > $PREFIX/bin/fastml
+#chmod +x $PREFIX/bin/fastml
 
