b381929e748d1e0670f3b88f00131aabfa7399a4
mvdbeek <m.vandenbeek@gmail.com>
2019-12-04 15:58:19 +0100

    Debug

--- recipes/bandage/build.sh
+++ recipes/bandage/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -ex
 
 # Run qmake to generate a Makefile
 qmake Bandage.pro QMAKE_CXX="$CXX" QMAKE_CC="$CC" QMAKE_CFLAGS="$CFLAGS" QMAKE_CXXFLAGS="$CXXFLAGS"
+# DEBUG: what's in here now ?
+cat Makefile
 
 # fix the makefile
 sed -ie "s/isystem/I/" Makefile
