ee3730f914ae5ced2fd913d1fef7187fc1ae7322
mvdbeek <m.vandenbeek@gmail.com>
2019-12-04 16:42:54 +0100

    Aha, found something in ngs-bits

--- recipes/bandage/build.sh
+++ recipes/bandage/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+#qmake bugfix: qmake fails if there is no g++ executable available, even if QMAKE_CXX is explicitly set
+ln -s $CXX $BUILD_PREFIX/bin/g++
+export PATH=$BUILD_PREFIX/bin/:$PATH
+
 # Run qmake to generate a Makefile
 qmake Bandage.pro QMAKE_CXX="$CXX" QMAKE_CC="$CC" QMAKE_CFLAGS="$CFLAGS" QMAKE_CXXFLAGS="$CXXFLAGS"
 # DEBUG: what's in here now ?
