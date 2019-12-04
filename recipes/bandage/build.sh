6b634ca53595bff624e153b50a07d38b50ce83d7
mvdbeek <m.vandenbeek@gmail.com>
2019-12-04 15:19:52 +0100

    Pass more variables

--- recipes/bandage/build.sh
+++ recipes/bandage/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run qmake to generate a Makefile
-qmake Bandage.pro QMAKE_CXX=$CXX QMAKE_CC=$CC
+qmake Bandage.pro QMAKE_CXX="$CXX" QMAKE_CC="$CC" QMAKE_CFLAGS="$CFLAGS" QMAKE_CXXFLAGS="$CXXFLAGS"
 
 # fix the makefile
 sed -ie "s/isystem/I/" Makefile
