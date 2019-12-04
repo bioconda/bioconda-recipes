7dbd64a613bdd448a033f3c47e314ae8ca756fd9
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-12-04 09:03:45 +0700

    update build.sh

--- recipes/genie/build.sh
+++ recipes/genie/build.sh
@@ -2,7 +2,7 @@
 
 go version
 mkdir -p src/github.com/sakkayaphab/bolt
-mv bed cli fasta gff hts variant vcf main.go go.sum go.mod vendor src/github.com/sakkayaphab/bolt
+rsync -av --progress * src/github.com/sakkayaphab/bolt --exclude src
 cd src/github.com/sakkayaphab/bolt
 export GOPATH="$SRC_DIR/"
 go get ./...
