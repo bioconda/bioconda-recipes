0fcc1d49137540655201da1ca76ead99a08a9d4b
mvdbeek <m.vandenbeek@gmail.com>
2019-12-04 12:36:52 +0100

    Add xorg-libxrender and fonts to Bandage
    Without these containerized startup fails / produces faulty svg files.

--- recipes/bandage/build.sh
+++ recipes/bandage/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Run qmake to generate a Makefile
-qmake Bandage.pro
+qmake Bandage.pro QMAKE_CXX=$CXX QMAKE_CC=$CC
 
 # fix the makefile
 sed -ie "s/isystem/I/" Makefile
-
 # Build the program
 make
 
