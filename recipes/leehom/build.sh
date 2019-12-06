e8a5747ed33be5b7b4b4ff2f0cc247761c96331b
Max <max.schubach@googlemail.com>
2019-12-06 15:14:25 +0100

    remove configure in build

--- recipes/leehom/build.sh
+++ recipes/leehom/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Ensure we run successfully using either conda-forge or defaults ncurses
-# (unlike other platforms, the latter does not automatically pull in libtinfo)
-CURSES_LIB="-ltinfow -lncursesw"
-
-./configure --prefix=$PREFIX --with-htslib=system CURSES_LIB="$CURSES_LIB"
 make all
 make install
