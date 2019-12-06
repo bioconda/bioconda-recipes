1f068f4cdfd3a862cf6b31bc53c8fb265297db2c
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2019-12-06 15:08:39 +0100

    Build with Cxx17 and restructure boost finding.

--- recipes/openms/build.sh
+++ recipes/openms/build.sh
@@ -14,14 +14,31 @@ cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/openms.sh
 cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/openms.sh
 
 cd ..
+
+# Use C++17 rather than C++11 to hopefully better match boost
+sed -i.bak "s/11/17/g" CMakeLists.txt
+
 mkdir build
 cd build
 
+
+## TODO is this really working also on macOS with frameworks?
 ORIGIN='$ORIGIN'
 export ORIGIN
 LDFLAGS='-Wl,-rpath,$${ORIGIN}/../lib'
 
-cmake .. -DOPENMS_CONTRIB_LIBS='../../contrib-build' -DCMAKE_INSTALL_PREFIX=$PREFIX -DHAS_XSERVER=OFF -DENABLE_TUTORIALS=OFF -DENABLE_STYLE_TESTING=OFF -DENABLE_UNITYBUILD=OFF -DWITH_GUI=OFF -DBoost_LIB_VERSION=1.64.0 -DBoost_INCLUDE_DIR=$PREFIX/include -DBoost_LIBRARY_DIRS=$PREFIX/lib -DBoost_LIBRARIES=$PREFIX/lib -DBoost_DEBUG=ON -DBOOST_LIBRARYDIR=$PREFIX/lib/ -DBOOST_USE_STATIC=OFF
+cmake .. 
+  -DOPENMS_CONTRIB_LIBS='../../contrib-build' \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX -DHAS_XSERVER=OFF \
+  -DENABLE_TUTORIALS=OFF \
+  -DENABLE_STYLE_TESTING=OFF \
+  -DENABLE_UNITYBUILD=OFF \
+  -DWITH_GUI=OFF \
+  -DBOOST_USE_STATIC=OFF \
+  -DBoost_NO_BOOST_CMAKE=ON \
+  -DBoost_ARCHITECTURE="-x64"
 
 make -j2 TOPP UTILS OpenMS
+## TODO make install is not working well yet. We neither have installation 
+## RPaths nor CMake exports that work correctly with installation.
 make install
