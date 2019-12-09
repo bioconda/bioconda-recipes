742acc7e998fe4f5704ec935ebc210a528ac4b5b
kiesers <silas.kieser@gmail.com>
2019-12-09 08:03:51 +0100

    explicit destination copy

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin/
-cp scripts/run_BUSCO.py $PREFIX/bin
-cp scripts/generate_plot.py $PREFIX/bin
-cp scripts/busco_configurator.py $PREFIX/bin
+cp scripts/run_BUSCO.py $PREFIX/bin/run_BUSCO.py
+cp scripts/generate_plot.py $PREFIX/bin/generate_plot.py
+cp scripts/busco_configurator.py $PREFIX/bin/busco_configurator.py 
 
 SHARE=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $SHARE
