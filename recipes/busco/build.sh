a9fd4b84acfba372b45183da0fc63381ecae3f94
kiesers <silas.kieser@gmail.com>
2019-12-09 08:04:43 +0100

    isntall at the begining

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+python setup.py install
+
 mkdir -p $PREFIX/bin/
 cp scripts/run_BUSCO.py $PREFIX/bin/run_BUSCO.py
 cp scripts/generate_plot.py $PREFIX/bin/generate_plot.py
-cp scripts/busco_configurator.py $PREFIX/bin/busco_configurator.py 
+cp scripts/busco_configurator.py $PREFIX/bin/busco_configurator.py
 
 SHARE=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $SHARE
@@ -12,5 +14,3 @@ cp config/config.ini.default $SHARE/config.ini.default
 ln -s $PREFIX/bin/run_BUSCO.py $PREFIX/bin/run_busco
 ln -s $PREFIX/bin/run_BUSCO.py $PREFIX/bin/busco
 ln -s $PREFIX/bin/generate_plot.py $PREFIX/bin/generate_plot
-
-python setup.py install
