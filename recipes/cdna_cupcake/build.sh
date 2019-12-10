6da8ad483d4db512fda2b716b82497cb2d6de57b
dpryan79 <dpryan79@gmail.com>
2019-12-10 09:00:21 +0100

    more scripts

--- recipes/cdna_cupcake/build.sh
+++ recipes/cdna_cupcake/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+find . -name *.c -exec rm {} \;
+find . -name *.so -exec rm {} \;
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 cp annotation/*.py ${PREFIX}/bin/
@@ -10,6 +12,7 @@ cp post_isoseq_cluster/*.py ${PREFIX}/bin/
 cp SequelQC/[a-zA-Z]*.py ${PREFIX}/bin
 cp sequence/*.py ${PREFIX}/bin/
 cp simulate/*.py ${PREFIX}/bin/
+cp singlecell/*.py ${PREFIX}/bin/
 cp targeted/* ${PREFIX}/bin/
 
 chmod +x ${PREFIX}/bin/*
