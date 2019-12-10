b5565d30df6bfe698dc5d4e3f24faecccc89e4b4
kiesers <silas.kieser@gmail.com>
2019-12-10 07:58:02 +0100

    activate environmental variables BUSCO_CONFIG_FILE

--- recipes/busco/post-link.sh
+++ recipes/busco/post-link.sh
@@ -8,3 +8,8 @@
 SHARE="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 mkdir -p "$PREFIX/config"
 python3 $PREFIX/bin/busco_configurator.py "$SHARE/config.ini.default" "$PREFIX/config/config.ini"
+
+# This should not be necessary in v4.0.beta2
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat "export BUSCO_CONFIG_FILE=$PREFIX/config/config.ini" >> ${PREFIX}/etc/conda/activate.d/set_busco_variable.sh
+cat "unset BUSCO_CONFIG_FILE" >> ${PREFIX}/etc/conda/deactivate.d/set_busco_variable.sh
