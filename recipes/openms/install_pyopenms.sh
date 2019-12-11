4380807dd5c7dd218a97e78f3bbcd231e8d2376a
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2019-12-11 15:51:54 +0100

    Forgot files

---
+++ recipes/openms/install_pyopenms.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# TODO actually it would be better if we could adapt our MANIFEST.in
+# to not package the openms libs and dependencies again. Same for share.
+$PYTHON -m pip install pyOpenMS/dist/*.whl --ignore-installed --no-deps -vv
