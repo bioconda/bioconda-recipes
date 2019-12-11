5bb913dd13145c6798e931bc87541ff958f5d304
M Bernt <m.bernt@ufz.de>
2019-12-11 13:39:13 +0100

    MaRaCluster: new recipe

---
+++ recipes/maracluster/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p build/maracluster
+cd build/maracluster
+
+cmake -DTARGET_ARCH=amd64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${SRC_DIR}/build/tools" "${SRC_DIR}";
+make -j 4;
+make -j 4 package;
+make install
