3643931a1ad86d8248ee0fe97f3e2f7992d86857
Aaron Mussig <aaronmussig@gmail.com>
2019-12-13 10:58:39 +1000

    Update post-link message.

--- recipes/gtdbtk/post-link.sh
+++ recipes/gtdbtk/post-link.sh
@@ -1,22 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-MSGS=$PREFIX/.messages.txt
-touch $MSGS
+echo "
+     GTDB-Tk v${PKG_VERSION} requires ~25G of external data which needs to be downloaded
+     and unarchived. This can be done automatically, or manually:
 
-function msg {
-    echo $1 >> $MSGS 2>&1
-}
+     1. Run the command download-db.sh to automatically download to:
+        ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/db/
 
-msg
-msg "GTDB-Tk v${PKG_VERSION} requires ~25G of external data which needs to be downloaded"
-msg "and unarchived. This can be done automatically, or manually:"
-msg "1. Run the command download-db.sh to automatically download to:"
-msg "${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/db/"
-msg "2. Manually download the latest reference data:"
-msg "https://github.com/Ecogenomics/GTDBTk#gtdb-tk-reference-data"
-msg "2b. Set the GTDBTK_DATA_PATH environment variable in the file:"
-msg "${PREFIX}/etc/conda/activate.d"
+     2. Manually download the latest reference data:
+        https://github.com/Ecogenomics/GTDBTk#gtdb-tk-reference-data
 
-echo 'line1'
-echo 'line2'
-echo 'line3'
+     2b. Set the GTDBTK_DATA_PATH environment variable in the file:
+         ${PREFIX}/etc/conda/activate.d
+
+" > "${PREFIX}"/.messages.txt
