f79164e6626748128fd4331e05ea3ad31673f4a9
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-12-16 04:33:55 +0700

    add bolt

---
+++ recipes/bolt/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir build
+cd build
+cmake .. -DINSTALL_PREFIX=$(pwd) -DINCLUDE_LIBRARY_PREFIX=${PREFIX}/include -DLIBRARY_LINK_PREFIX=${PREFIX}/lib
+make
+make install
+mkdir -p $PREFIX/bin
+cp bolt $PREFIX/bin
