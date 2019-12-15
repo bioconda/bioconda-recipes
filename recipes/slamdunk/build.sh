ffebb01516fb79eef2f055497a7aa3e76b64d896
tobias.neumann <tobias.neumann@imp.ac.at>
2019-12-15 22:36:25 +0100

    New slamdunk meta.yaml with extended dependencies.

--- recipes/slamdunk/build.sh
+++ recipes/slamdunk/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [[ $OSTYPE == darwin* ]]; then
-     export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-fi
-
-$PYTHON -m pip install --no-deps --ignore-installed .
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
