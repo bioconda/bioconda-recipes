582634f7687724a3430984fee2aad6de13b3d294
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-12-16 06:05:11 +0700

    update

--- recipes/bolt/build.sh
+++ recipes/bolt/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd ${PREFIX}/include && ls
-cd ${PREFIX}/lib && ls
-cd ${PREFIX}/include/htslib && ls
 mkdir build
 cd build
 cmake .. -DINSTALL_BIN_PREFIX=$(pwd) -DINCLUDE_LIBRARY_PREFIX=${PREFIX}/include -DLIBRARY_LINK_PREFIX=${PREFIX}/lib
