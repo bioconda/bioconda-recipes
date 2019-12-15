159d8aaa2c03ef0142f08bd273d5b5a34769a3d0
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-12-16 06:44:17 +0700

    update

--- recipes/bolt/build.sh
+++ recipes/bolt/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake .. -DINSTALL_BIN_PREFIX=$(pwd) -DINCLUDE_LIBRARY_PREFIX=${PREFIX}/include -DLIBRARY_LINK_PREFIX=${PREFIX}/lib
+cmake .. -DINSTALL_BIN_PREFIX=${PREFIX}/bin -DINCLUDE_LIBRARY_PREFIX=${PREFIX}/include -DLIBRARY_LINK_PREFIX=${PREFIX}/lib
 make
 make install
 mkdir -p $PREFIX/bin
