fce30caf74700bc2138d9f827a502b246821a328
Sakkayaphab Piwluang <sakkayaphab@gmail.com>
2019-12-16 07:42:39 +0700

    no message

--- recipes/bolt/build.sh
+++ recipes/bolt/build.sh
@@ -6,6 +6,8 @@ ls $PREFIX/lib
 echo $PREFIX/lib
 ls $PREFIX/include/htslib
 echo $PREFIX/include/htslib
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 mkdir build
 cd build
 cmake .. -DINSTALL_BIN_PREFIX=$PREFIX/bin -DINCLUDE_LIBRARY_PREFIX=$PREFIX/include -DLIBRARY_LINK_PREFIX=$PREFIX/lib
