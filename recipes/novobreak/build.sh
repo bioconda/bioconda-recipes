ee970d2ccc089ce206b66d0eb1da1a3d76376c88
Johannes Köster <johannes.koester@uni-due.de>
2019-12-17 15:41:54 +0100

    add novobreak scripts

--- recipes/novobreak/build.sh
+++ recipes/novobreak/build.sh
@@ -4,3 +4,9 @@ make clean
 make novoBreak LIBPATH=-L$PREFIX/lib INCLUDE=-I$PREFIX/include
 mkdir -p $PREFIX/bin
 cp novoBreak $PREFIX/bin
+cp *.pl *.sh $PREFIX/bin
+
+run_novobreak=$PREFIX/bin/run_novobreak
+echo "#!/bin/sh" > $run_novobreak
+echo "run_novoBreak.sh \`which novoBreak\` \$*" >> $run_novobreak
+chmod +x $run_novobreak
