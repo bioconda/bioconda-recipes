194b8117ca6139999e3703e0aebd260298cd8a7c
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2019-12-19 00:16:11 +0100

    change dir'?

--- recipes/openms/install_pyopenms.sh
+++ recipes/openms/install_pyopenms.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # TODO actually it would be better if we could adapt our MANIFEST.in
 # to not package the openms libs and dependencies again. Same for share.
-$PYTHON -m pip install pyOpenMS/ --ignore-installed --no-deps -vv
+pushd src/pyOpenMS
+$PYTHON -m pip install . --ignore-installed --no-deps -vv
+popd
