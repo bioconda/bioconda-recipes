3faf565c496fb53a58cc93d653f5e12bda49c291
Daniel Nicorici <Daniel.Nicorici@gmail.com>
2019-12-18 10:00:02 +0200

    fixes for FusionCatcher v1.20
    updated the genome files from v95 to v98.

--- recipes/fusioncatcher/download-human-db.sh
+++ recipes/fusioncatcher/download-human-db.sh
@@ -4,14 +4,14 @@ echo "Downloading Human Ensembl v95 database to ${FC_DB_PATH}/current/..."
 
 # Direct download:
 cd ${FC_DB_PATH}
-rm -rf human_v95.tar.gz.*
-wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v95.tar.gz.aa
-wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v95.tar.gz.ab
-wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v95.tar.gz.ac
-wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v95.tar.gz.ad
-cat human_v95.tar.gz.* | tar xz
-ln -s human_v95 current
+rm -rf human_v98.tar.gz.*
+wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v98.tar.gz.aa
+wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v98.tar.gz.ab
+wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v98.tar.gz.ac
+wget http://sourceforge.net/projects/fusioncatcher/files/data/human_v98.tar.gz.ad
+cat human_v98.tar.gz.* | tar xz
+ln -s human_v98 current
 
-echo "Human Ensembl v95 database is downloaded."
+echo "Human Ensembl v98 database is downloaded."
 
 exit 0
