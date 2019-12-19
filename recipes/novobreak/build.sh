00e51450798f87d6fe3766193e86064b847773ff
Johannes Köster <johannes.koester@uni-due.de>
2019-12-19 13:11:26 +0100

    add scripts from nb_distribution

--- recipes/novobreak/build.sh
+++ recipes/novobreak/build.sh
@@ -4,9 +4,17 @@ make clean
 make novoBreak LIBPATH=-L$PREFIX/lib INCLUDE=-I$PREFIX/include
 mkdir -p $PREFIX/bin
 cp novoBreak $PREFIX/bin
-cp *.pl *.sh $PREFIX/bin
 
+# add scripts from nb_distribution
+for script in fetch_discordant.pl filter_sv.bak.pl filter_sv.pl filter_sv2.pl filter_sv_icgc.pl group_bp_reads.pl infer_bp.pl infer_bp_v4.pl infer_sv.pl run_novoBreak.sh run_ssake.pl
+do
+    target=$PREFIX/bin/$script
+    curl -L https://raw.githubusercontent.com/czc/nb_distribution/923c670/$script > $target
+    chmod +x $target
+done
+
+# add a simplified overall wrapper
 run_novobreak=$PREFIX/bin/run_novobreak
 echo "#!/bin/sh" > $run_novobreak
-echo "run_novoBreak.sh \`which novoBreak\` \$*" >> $run_novobreak
+echo "run_novoBreak.sh \$(dirname \`which novoBreak\`) \$@" >> $run_novobreak
 chmod +x $run_novobreak
