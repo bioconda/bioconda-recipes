7d408aba23d97c24c2ad8afd6982c1b2d222c06e
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2019-12-19 12:03:54 +0100

    cleaned obsolete comments in the build file

--- recipes/rsat-core/build.sh
+++ recipes/rsat-core/build.sh
@@ -1,18 +1,9 @@
 
 
-# TODO: better file system layout and entry point command
 RSAT_DEST="$PREFIX/opt/rsat/"
 mkdir -p "$RSAT_DEST"
 
 cp -a perl-scripts python-scripts makefiles bin/rsat share/rsat/rsat.yaml "$RSAT_DEST"
-
-# echo "#!/bin/sh" > $PREFIX/bin/rsat
-# echo "../opt/rsat/rsat" >> $PREFIX/bin/rsat
-# chmod 755 $PREFIX/bin/rsat
-
-## Alternative: 
-## - au opt: placer les scripts perl, le yaml etc dans usr/share/rsat
-## - au dispatcher dans bin: placer dans le bin le script python rsat et lui indiquer où aller pêcher le yaml.
 
 # Build and dispatch compiled binaries
 cd contrib
