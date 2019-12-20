7a2d32b4f26fc6e29b2817729fcbf13f21ec4867
kiesers <silas.kieser@gmail.com>
2019-12-20 12:11:31 +0100

    fill config during build

--- recipes/busco/post-link.sh
+++ recipes/busco/post-link.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
 #create config file
-
-
-
-# BUSCO needs a config file with paths to external dependencies. These deps are
-# installed as runtime deps, but the paths in the config file need to reflect
-# the current environment's $PREFIX/bin dir. BUSCO expects a config file in
-# ../config/config.ini, so we create it here using busco_configurator.py on the default ini
-# stored in the share dir, replacing paths as necessary.
-
-SHARE=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $SHARE
-
-mkdir -p "$PREFIX/config"
-busco_configurator.py "config/config.ini" "$PREFIX/config/config.ini"
-
-# This should not be necessary in v4.0.1
-mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
-cat 'export BUSCO_CONFIG_FILE="$PREFIX/config/config.ini"' > ${PREFIX}/etc/conda/activate.d/set_busco_variable.sh
-cat "unset BUSCO_CONFIG_FILE" > ${PREFIX}/etc/conda/deactivate.d/set_busco_variable.sh
