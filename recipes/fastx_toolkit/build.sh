974bd9a1e7364d11c6c55108d29ac0a8a3133011
mvdbeek <m.vandenbeek@gmail.com>
2019-12-24 13:32:21 +0100

    Use -Wall

--- recipes/fastx_toolkit/build.sh
+++ recipes/fastx_toolkit/build.sh
@@ -2,6 +2,7 @@
 
 export GTEXTUTILS_CFLAGS="-I $PREFIX/include/gtextutils"
 export GTEXTUTILS_LIBS="$PREFIX/lib/libgtextutils.a"
+export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++ -Wall"
 sed -i.bak 's/#pragma pack(1)//g' src/libfastx/fastx.h
 
 if [ "$(uname)" == "Darwin" ]; then
