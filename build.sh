119b338a52c53b1673557ce4cd878b7536dbea53
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-12-24 10:27:27 +0100

    '$PREFIX/build' changed to 'build'

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
 rm -fr build
-mkdir -p $PREFIX/build
-cd $PREFIX/build
+mkdir -p build
+cd build
 cmake ../src
 make -j8
 mv smashpp ..
