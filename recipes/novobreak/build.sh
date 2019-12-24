4f3e7a01289d2cc1bbccec6fe4cbc767e857fd42
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-12-24 11:46:34 +0100

    Add scripts to novobreak recipe (#19420)
    * add novobreak scripts
    
    * descript wrapper.
    
    * bump build number
    
    * add scripts from nb_distribution
    
    * set CC
    
    * try setting -fPIE
    
    * fix flags
    
    * disable PIE.
    
    * disable nopie on osx

--- recipes/novobreak/build.sh
+++ recipes/novobreak/build.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+
+NOPIE="-no-pie"
+if [[ $target_platform == osx-64 ]]
+then
+    # clang does not have this flag and it is not needed there
+    NOPIE=""
+fi
+
+cd samtools
 make clean
-make novoBreak LIBPATH=-L$PREFIX/lib INCLUDE=-I$PREFIX/include
+make lib CC=$CC CFLAGS="-g -Wall -O2 $NOPIE" INCLUDES=-I$PREFIX/include LIBPATH=-L$PREFIX/lib
+cd ..
+
+make clean
+make novoBreak LIBPATH=-L$PREFIX/lib INCLUDE=-I$PREFIX/include CC=$CC CFLAGS="-g -W -Wall -O3 -finline-functions -D_FILE_OFFSET_BITS=64 $NOPIE"
 mkdir -p $PREFIX/bin
 cp novoBreak $PREFIX/bin
+
+# add scripts from nb_distribution
+for script in fetch_discordant.pl filter_sv.bak.pl filter_sv.pl filter_sv2.pl filter_sv_icgc.pl group_bp_reads.pl infer_bp.pl infer_bp_v4.pl infer_sv.pl run_novoBreak.sh run_ssake.pl
+do
+    target=$PREFIX/bin/$script
+    curl -L https://raw.githubusercontent.com/czc/nb_distribution/923c670/$script > $target
+    chmod +x $target
+done
+
+# add a simplified overall wrapper
+run_novobreak=$PREFIX/bin/run_novobreak
+echo "#!/bin/sh" > $run_novobreak
+echo "run_novoBreak.sh \$(dirname \`which novoBreak\`) \$@" >> $run_novobreak
+chmod +x $run_novobreak
