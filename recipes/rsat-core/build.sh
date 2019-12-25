8473c692a56463655b92d86458b967d434b723ce
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2019-12-25 09:13:54 +0100

    added default configuration files to the package + run auto configuration in the build.sh

--- recipes/rsat-core/build.sh
+++ recipes/rsat-core/build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 # RSAT_DEST="$PREFIX/opt/rsat/"
+BASE=`pwd`
 RSAT_DEST="$PREFIX/share/rsat/"
 mkdir -p "$RSAT_DEST"
 mkdir -p "$PREFIX/bin"
 ## mkdir -p "$PREFIX/share/rsat"
 
-cp -a LICENSE.txt perl-scripts python-scripts makefiles R-scripts "$RSAT_DEST"
+cp -a LICENSE.txt \
+   perl-scripts \
+   python-scripts \
+   makefiles \
+   R-scripts \
+   RSAT_config_default.mk \
+   RSAT_config_default.bashrc \
+   RSAT_config_default.props \
+   RSAT_config_default.conf \
+   "$RSAT_DEST"
+
 cp bin/rsat $PREFIX/bin/rsat
 cp share/rsat/rsat.yaml $PREFIX/share/rsat/rsat.yaml
 
@@ -18,12 +29,13 @@ cp share/rsat/rsat.yaml $PREFIX/share/rsat/rsat.yaml
 # cd $PREFIX
 
 ## Make a link from share to opt (not sure this is required)
-ln -s share opt
+# ln -s share opt
 
 # Build and dispatch compiled binaries
 cd contrib
 for dbin in info-gibbs count-words matrix-scan-quick retrieve-variation-seq variation-scan 
 do
+    echo "Compiling C/C++ program: $dbin"
     cd "$dbin"
     make clean && make CC=$CC CXX=$CXX && cp "$dbin" "$PREFIX/bin"
     cd ..
@@ -31,6 +43,15 @@ done
 cd ..
 
 # Build the R package with RSAT functions used by matrix-clustering
+echo "Building R package TFBMclust"
 cd R-scripts
 R CMD INSTALL --no-multiarch --with-keep.source TFBMclust
 cd ..
+
+
+# Run the configuration script
+echo "Running RSAT configuration in automatic mode"
+cd $RSAT_DEST
+perl-scripts/configure_rsat.pl -auto
+cd $BASE
+
