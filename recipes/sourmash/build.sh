dd7f3e11ebf3e78a04734e5e8230e003c85736b6
Luiz Irber <luiz.irber@gmail.com>
2020-01-01 20:42:25 +0000

    fix home dir for macos

---
+++ recipes/sourmash/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -euo
+
+# taken from yacrd recipe, see: https://github.com/bioconda/bioconda-recipes/blob/2b02c3db6400499d910bc5f297d23cb20c9db4f8/recipes/yacrd/build.sh
+if [ "$(uname)" == "Darwin" ]; then
+
+    # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
+    export HOME="/Users/distiller"
+
+    # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
+    git config --global --unset url.ssh://git@github.com.insteadOf
+fi
+
+$PYTHON -m pip install --no-deps --ignore-installed -vv .
