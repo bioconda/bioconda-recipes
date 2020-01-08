5ce200c9d4805067108ca757bacb620cd05c641b
Alyssa Morrow <akmorrow@berkeley.edu>
2020-01-08 15:15:02 -0800

    moved distribution to its own directory

--- recipes/adam/build.sh
+++ recipes/adam/build.sh
@@ -6,7 +6,7 @@ PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $BINARY_HOME
 mkdir -p $PACKAGE_HOME
 
-cp -R * $PACKAGE_HOME
+cp -R distribution/* $PACKAGE_HOME
 
 ln -s $PACKAGE_HOME/bin/adam-shell $BINARY_HOME
 ln -s $PACKAGE_HOME/bin/adam-submit $BINARY_HOME
