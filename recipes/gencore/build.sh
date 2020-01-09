72664ced597716a5235881d87c6b0e6280b88d0f
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-09 08:32:59 +0100

    Update build.sh

--- recipes/gencore/build.sh
+++ recipes/gencore/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu -o pipefail
 
-make INCLUDE_DIRS="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib"
+make INCLUDE_DIRS="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib" CC=$CXX
 make install
