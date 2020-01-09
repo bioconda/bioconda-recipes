bf473867bca739acb25cb62213d28eb36c89e1dc
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-09 08:35:00 +0100

    Update build.sh

--- recipes/genefuse/build.sh
+++ recipes/genefuse/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu -o pipefail
 
-make INCLUDE_DIRS="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib" CC=$CXX BINDIR=$PREFIX
-make install  
+make INCLUDE_DIRS="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib" CC=$CXX
+make install BINDIR=$PREFIX
