af0fe7fe18a8cfa95f7a8d40bcd7e5a3c3398b1c
Alyssa Morrow <akmorrow@berkeley.edu>
2020-01-09 13:06:49 -0800

    added ADAM_HOME

--- recipes/adam/build.sh
+++ recipes/adam/build.sh
@@ -7,6 +7,9 @@ mkdir -p $BINARY_HOME
 mkdir -p $PACKAGE_HOME
 
 cp -R distribution/* $PACKAGE_HOME
+# for now, we download the egg file and package it
+# to the make find_adam_egg.sh happy
+cp -R python_egg/*.egg $PACKAGE_HOME
 
 ln -s $PACKAGE_HOME/bin/adam-shell $BINARY_HOME
 ln -s $PACKAGE_HOME/bin/adam-submit $BINARY_HOME
@@ -15,7 +18,13 @@ ln -s $PACKAGE_HOME/bin/pyadam $BINARY_HOME
 # adam
 $PYTHON -m pip install python_adam/. --no-deps --ignore-installed -vv
 
-mkdir -p $PREFIX/adam-assembly/target
+mkdir -p $PREFIX/repo
+
+
+for f in $PACKAGE_HOME/*.egg; do
+  ln -s $f $PREFIX/repo/
+done
+
 for f in $PACKAGE_HOME/repo/*.jar ; do
-    ln -s $f $PREFIX/adam-assembly/target/
+    ln -s $f $PREFIX/repo/
 done
--- recipes/adam/post-link.py
+++ recipes/adam/post-link.py
@@ -4,3 +4,7 @@
 if [ -z "${SPARK_HOME}" ]; then
     export SPARK_HOME=$( eval "echo ${PREFIX}/lib/python*/site-packages/pyspark" )
 fi
+
+if [ -z "${ADAM_HOME}" ]; then
+    export ADAM_HOME=$( eval "echo ${PREFIX}" )
+fi
