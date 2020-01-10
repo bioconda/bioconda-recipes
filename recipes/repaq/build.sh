d66de00f26076c40a6ff78cc04e36b4196f0a9b1
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-10 09:54:54 +0100

    Update build.sh

--- recipes/repaq/build.sh
+++ recipes/repaq/build.sh
@@ -4,6 +4,6 @@ set -eu -o pipefail
 if [[ $OSTYPE == "darwin"* ]]; then
   LDFLAGS="$LDFLAGS -lc"
 fi
-make DIR_INC="$PREFIX/include" LDFLAGS="$LDFLAGS" CXX=$CXX
+make DIR_INC="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib" CXX=$CXX
 mkdir -p $PREFIX/bin
 make install BINDIR=$PREFIX/bin
