63a89c86d418454b7ed6cadc2e2f5916cfe5657b
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-11 15:56:11 +0100

    Update build.sh

--- recipes/admixtools/build.sh
+++ recipes/admixtools/build.sh
@@ -2,6 +2,6 @@
 cd src
 make clobber
 
-make CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" LDLIBS="-lpthread" all
+make CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" LDLIBS="-lopenblas -lpthread" all
 
 make install TOP=$PREFIX/bin
