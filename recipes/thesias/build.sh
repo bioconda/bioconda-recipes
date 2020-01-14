188e7f00f68271262c7f3126fb8030f083b62551
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-14 10:28:52 +0100

    Update build.sh

--- recipes/thesias/build.sh
+++ recipes/thesias/build.sh
@@ -11,6 +11,7 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib
 
 mv thesias.jar $outdir/
+chmod +x THESIAS.py
 mv $RECIPE_DIR/THESIAS.py $outdir/THESIAS
 ln -s $outdir/THESIAS $PREFIX/bin/THESIAS
 mv libthesiaslib.so.0 $PREFIX/lib/libthesiaslib.so
