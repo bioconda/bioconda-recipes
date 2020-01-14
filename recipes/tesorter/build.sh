6f61382bf9cf4bb18562060e9c4a7a4d1d16650b
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-14 17:43:41 +0100

    add old fashion install

--- recipes/tesorter/build.sh
+++ recipes/tesorter/build.sh
@@ -2,4 +2,10 @@
 set -x -e
 
 sh build_database.sh
+
+TEsorter_DIR=${PREFIX}/share/TEsorter
+mkdir -p ${PREFIX}/bin
+mkdir -p ${TEsorter_DIR}
+cp -r * ${TEsorter_DIR}
+
 ln -s TEsorter.py ${PREFIX}/bin/TEsorter.py
