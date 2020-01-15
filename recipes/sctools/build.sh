65078b2acd39556191275b2c4c626f3853f6ebe1
Elena <grassi.e@gmail.com>
2020-01-15 17:41:09 +0100

    fixed cp to $PREFIX/bin

--- recipes/sctools/build.sh
+++ recipes/sctools/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#cd SCTools-1.0.0
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make
 mkdir -p $PREFIX/bin
-cp apps/sctools $PREFIX/bin
+cp apps/sctools/* $PREFIX/bin
