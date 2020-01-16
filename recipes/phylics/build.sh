f154f108e78aca57470af1cc01566449e87eaf99
Elena <grassi.e@gmail.com>
2020-01-16 16:26:06 +0100

    Working build script (still without ginkgo)

--- recipes/phylics/build.sh
+++ recipes/phylics/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
+# The build system fetches our .tar.gz from github and puts us
+# in its "root" directory
+WD=$(pwd)
 mkdir -p $PREFIX/bin
-cp apps/sctools_demultiplex $PREFIX/bin
+cp $WD/phylics/local/src/*.py $PREFIX/bin
+for p in $PREFIX/bin/*py; do
+	NOPATH=$(basename -- "$p")
+	NOEXT="${NOPATH%.*}"
+	ln -s $p $PREFIX/bin/$NOEXT;
+done
+# and for gingko? its not in the repo now
+#ginkgo_path = os.path.join(BIN_DIR,"ginkgo", "cli", "ginkgo.sh")
+#data@rotpunkt:~/work/phylics$ grep -i ROOT_DIR PhyliCS-1.0.0/local/src/*.py
+# NEED TO FIX:
+#PhyliCS-1.0.0/local/src/constants.py:ROOT_DIR = '/home/mmontemurro/PhyliCS'
+#PhyliCS-1.0.0/local/src/constants.py:BIN_DIR = ROOT_DIR + '/local/bin'
+#PhyliCS-1.0.0/local/src/constants.py:SRC_DIR = ROOT_DIR + '/local/src'
