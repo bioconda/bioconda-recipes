0a658f63155b76e7e9460b9a45b76e4ed211be86
fgiacomoni <franck.giacomoni@inrae.fr>
2020-01-16 16:09:00 +0100

    includes annoying things (based on #12755) to try solving compilation issue...

--- recipes/perl-text-csv_xs/build.sh
+++ recipes/perl-text-csv_xs/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+
+# Fix pollution from the perl build environment
+export CFLAGS="-I/usr/include $CFLAGS"
+dname=`find $PREFIX/lib -name Config_heavy.pl -print | xargs dirname`
+sed -i.bak "s|^    cc => .*$|    cc => '${CC}',|" ${dname}/Config.pm
+sed -i.bak "s|^ccflags=.*$|ccflags='${CFLAGS}'|;s|^ld=.*$|ld='${CC}'|;s|^cppflags=.*$|cppflags='${CFLAGS}'|;" ${dname}/Config_heavy.pl
+
+# clean up
+rm -f ${dname}/Config.pm.bak ${dname}/Config_heavy.pl.bak
+
+
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
@@ -10,6 +22,8 @@ if [ -f Build.PL ]; then
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
+    # Fix pollution from the perl build environment
+    sed -i.bak "s|^LDLOADLIBS = .*$|LDLOADLIBS = -L$PREFIX/lib -lssl -lcrypto -lz -lpthread|;s|/home/conda/feedstock_root/build_artifacts/perl_1550669032175/_build_env|$BUILD_PREFIX|g" Makefile
     make
     make test
     make install
