fbbb5e98b37b1fe00a86abcd227922cf38227a16
Elena <grassi.e@gmail.com>
2020-01-16 19:19:48 +0100

    Possibly ginkgo will run now

--- recipes/phylics/build.sh
+++ recipes/phylics/build.sh
@@ -2,18 +2,16 @@
 
 # The build system fetches our .tar.gz from github and puts us
 # in its "root" directory
-WD=$(pwd)
+#WD=$(pwd)
 mkdir -p $PREFIX/bin
-cp $WD/phylics/local/src/*.py $PREFIX/bin
-for p in $PREFIX/bin/*py; do
+mkdir -p $PREFIX/src
+cp phylics/local/src/*.py $PREFIX/src
+for p in $PREFIX/src/*py; do
 	NOPATH=$(basename -- "$p")
 	NOEXT="${NOPATH%.*}"
 	ln -s $p $PREFIX/bin/$NOEXT;
 done
-# and for gingko? its not in the repo now
-#ginkgo_path = os.path.join(BIN_DIR,"ginkgo", "cli", "ginkgo.sh")
-#data@rotpunkt:~/work/phylics$ grep -i ROOT_DIR PhyliCS-1.0.0/local/src/*.py
-# NEED TO FIX:
-#PhyliCS-1.0.0/local/src/constants.py:ROOT_DIR = '/home/mmontemurro/PhyliCS'
-#PhyliCS-1.0.0/local/src/constants.py:BIN_DIR = ROOT_DIR + '/local/bin'
-#PhyliCS-1.0.0/local/src/constants.py:SRC_DIR = ROOT_DIR + '/local/src'
+
+# tell Marilisa to call ginkgo as ginkgo.sh direclty
+cp phylics/local/src/ginkgo/ginkgo.sh $PREFIX/bin/
+cp -r phylics/local/src/ginkgo $PREFIX/
