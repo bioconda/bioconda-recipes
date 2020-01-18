65c085cfa73400a7b72023157e80676598fd5170
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-18 19:48:09 +0100

    add cp -r *

--- recipes/edta/build.sh
+++ recipes/edta/build.sh
@@ -4,6 +4,8 @@ set -x -e
 EDTA_DIR=${PREFIX}/share/EDTA
 EDTA_PROGRAMS="EDTA.pl EDTA_raw.pl EDTA_processI.pl"
 
+cp -r * ${EDTA_DIR}
+
 for name in ${EDTA_PROGRAMS} ; do
   ln -s ${PREFIX}/bin/$name ${PREFIX}/share/$name
 done
