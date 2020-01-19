76fa9e859058af832cf144d8a640d5e5ea4e3b02
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-19 20:53:28 +0100

    add genericrepeatfinder recipe

---
+++ recipes/genericrepeatfinder/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -x -e
+
+cd src/grf-main
+g++ main.cpp DetectMITE.h DetectMITE.cpp DetectIR.h DetectIR.cpp functions.h functions.cpp -std=c++11 -fopenmp -O3 -o grf-main
+
+for name in bin/* ; do
+  ln -s $name ${PREFIX}/bin/$(basename $name)
+done
