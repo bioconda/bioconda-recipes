24365a2d3f4ff7f7e8dd8e6347d8b011f544020b
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-19 22:27:48 +0100

    gcc instead of g++

--- recipes/genericrepeatfinder/build.sh
+++ recipes/genericrepeatfinder/build.sh
@@ -2,7 +2,7 @@
 set -x -e
 
 cd src/grf-main
-g++ main.cpp DetectMITE.h DetectMITE.cpp DetectIR.h DetectIR.cpp functions.h functions.cpp -std=c++11 -fopenmp -O3 -o grf-main
+gcc main.cpp DetectMITE.h DetectMITE.cpp DetectIR.h DetectIR.cpp functions.h functions.cpp -std=c++11 -fopenmp -O3 -o grf-main
 
 for name in bin/* ; do
   ln -s $name ${PREFIX}/bin/$(basename $name)
