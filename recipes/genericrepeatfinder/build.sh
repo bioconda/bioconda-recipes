5cb4f0dc9248566fdb1db604c656770bf336c47c
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-19 22:37:00 +0100

    replace make

--- recipes/genericrepeatfinder/build.sh
+++ recipes/genericrepeatfinder/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x -e
 
-cd src/grf-main
-gcc main.cpp DetectMITE.h DetectMITE.cpp DetectIR.h DetectIR.cpp functions.h functions.cpp -std=c++11 -fopenmp -O3 -o grf-main
+cd src
+make CC=$CC
 
 for name in bin/* ; do
   ln -s $name ${PREFIX}/bin/$(basename $name)
