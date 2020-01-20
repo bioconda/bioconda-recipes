7325050b5866295720a9d12b97de59a9ce561d77
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-20 22:01:46 +0100

    osx test

--- recipes/genericrepeatfinder/build.sh
+++ recipes/genericrepeatfinder/build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 set -x -e
 
-export C_INCLUDE_PATH=${PREFIX}/include
+export CFLAGS="-I$PREFIX/include"
+export CPPFLAGS="-I$PREFIX/include"
+export CXXFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 cd src
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  make CC=clang++
+  make CC=clang++ 
 else
   make CC=$GXX
 fi
