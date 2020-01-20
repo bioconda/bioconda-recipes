f1dc8f730700396474242819d50e28af0e3c1eb2
Peter van Heusden <pvh@sanbi.ac.za>
2020-01-20 21:51:06 +0200

    tbl2asn update to 25.8

--- recipes/tbl2asn/build.sh
+++ recipes/tbl2asn/build.sh
@@ -5,3 +5,8 @@ gunzip *tbl2asn.gz
 mkdir -p ${PREFIX}/bin
 cp *.tbl2asn ${PREFIX}/bin/tbl2asn
 chmod a+x ${PREFIX}/bin/tbl2asn
+
+if [[ $(uname) == 'Linux' ]] ; then 
+    INTERPRETER=$(patchelf --print-interpreter /bin/ls)
+    patchelf --set-interpreter $INTERPRETER ${PREFIX}/bin/tbl2asn
+fi
