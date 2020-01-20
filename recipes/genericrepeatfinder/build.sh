643e218c8a841c4f4f5af66a390a78656ac92e0d
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-20 16:42:16 +0100

    try compiling

--- recipes/genericrepeatfinder/build.sh
+++ recipes/genericrepeatfinder/build.sh
@@ -7,6 +7,6 @@ make CC=$GXX
 cd ..
 mkdir -p ${PREFIX}/bin
 for name in bin/* ; do
-  ln -s $name ${PREFIX}/bin/$(basename $name)
+  cp $name ${PREFIX}/bin/$(basename $name)
   chmod a+x ${PREFIX}/bin/$(basename $name)
 done
