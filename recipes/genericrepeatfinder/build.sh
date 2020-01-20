153d154f6439c1df0800659b5d06b34c390f68a6
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-20 16:55:04 +0100

    try compiling

--- recipes/genericrepeatfinder/build.sh
+++ recipes/genericrepeatfinder/build.sh
@@ -4,9 +4,12 @@ set -x -e
 cd src
 make CC=$GXX
 
-cd ..
 mkdir -p ${PREFIX}/bin
-for name in bin/* ; do
-  cp $name ${PREFIX}/bin/$(basename $name)
-  chmod a+x ${PREFIX}/bin/$(basename $name)
-done
+cp grf-alignment/grf-alignment ${PREFIX}/bin/
+cp grf-alignment2/grf-alignment2 ${PREFIX}/bin/
+cp grf-dbn/grf-dbn ${PREFIX}/bin/
+cp grf-filter/grf-filter ${PREFIX}/bin/
+cp grf-intersperse/grf-intersperse ${PREFIX}/bin/
+cp grf-main/grf-main ${PREFIX}/bin/
+cp grf-mite-cluster/grf-mite-cluster ${PREFIX}/bin/
+cp grf-nest/grf-nest ${PREFIX}/bin/
