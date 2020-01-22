cf6f71a8043d2f1a206c5f97da129f00afaaecd3
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-22 11:13:37 +0100

    fix shebang

--- recipes/edta/build.sh
+++ recipes/edta/build.sh
@@ -4,11 +4,18 @@ set -x -e
 EDTA_DIR=${PREFIX}/share/EDTA
 EDTA_PROGRAMS="EDTA.pl EDTA_raw.pl EDTA_processI.pl"
 
+for name in ${EDTA_PROGRAMS} ; do
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e 's/^#!.*perl.*$/#!\/usr\/bin\/env perl/g'  ${EDTA_PROGRAMS}
+  else
+    sed -i -e 's/^#!.*perl.*$/#!\/usr\/bin\/env perl/g' ${EDTA_PROGRAMS}
+  fi
+done
+
 mkdir -p ${PREFIX}/bin
 mkdir -p ${EDTA_DIR}
 cp -r * ${EDTA_DIR}
 
 for name in ${EDTA_PROGRAMS} ; do
-  chmod u+x ${EDTA_DIR}/$name
   ln -s ${EDTA_DIR}/$name ${PREFIX}/bin/$name
 done
