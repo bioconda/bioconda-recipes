ce09d962f4e14c461d40dcc016df96dd34ecfe3e
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-22 11:33:15 +0100

    fix call in share folder as repeatmodeler

--- recipes/edta/build.sh
+++ recipes/edta/build.sh
@@ -2,15 +2,7 @@
 set -x -e
 
 EDTA_DIR=${PREFIX}/share/EDTA
-EDTA_PROGRAMS="EDTA.pl EDTA_raw.pl EDTA_processI.pl"
-
-for name in ${EDTA_PROGRAMS} ; do
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -e 's/^#!.*perl.*$/#!\/usr\/bin\/env perl/g'  ${EDTA_PROGRAMS}
-  else
-    sed -i -e 's/^#!.*perl.*$/#!\/usr\/bin\/env perl/g' ${EDTA_PROGRAMS}
-  fi
-done
+EDTA_OTHER_PROGRAMS="EDTA_raw.pl EDTA_processI.pl"
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${EDTA_DIR}
@@ -18,4 +10,15 @@ cp -r * ${EDTA_DIR}
 
 for name in ${EDTA_PROGRAMS} ; do
   ln -s ${EDTA_DIR}/$name ${PREFIX}/bin/$name
+done
+
+cat <<END >>${PREFIX}/bin/EDTA.pl
+#!/bin/bash
+NAME=\$(basename \$0)
+perl ${EDTA_DIR}/\${NAME} \$@
+END
+
+chmod a+x ${PREFIX}/bin/EDTA.pl
+for name in ${RM_OTHER_PROGRAMS} ; do
+  ln -s ${PREFIX}/bin/EDTA.pl ${PREFIX}/bin/$(basename $name)
 done
