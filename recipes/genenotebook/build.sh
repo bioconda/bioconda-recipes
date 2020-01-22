85b62ff0ff691799d98f27a4bea7bf0316312d87
Holmer, Rens <rens.holmer@wur.nl>
2020-01-22 15:08:13 +0100

    missing glibcxx debugging

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -5,11 +5,11 @@ set -exo pipefail
 outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
 mkdir -p $outdir $PREFIX/bin
 pwd
-ls -l .
+ls -lah .
 
 npm install --unsafe-perm
-ll /usr/lib64/libstdc*
-strings /usr/lib64/libstdc++.so.6 | grep GLIBC
+ls -lah /usr/lib64/libstdc*
+strings /usr/lib64/libstdc* | grep GLIBC
 METEOR_ALLOW_SUPERUSER=1 METEOR_DISABLE_OPTIMISTIC_CACHING=1 npm run bundle
 
 cp -R genenotebook_v${PKG_VERSION}/* $outdir
