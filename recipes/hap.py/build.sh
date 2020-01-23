c7a9f91c59d2272bfe03dd7af4842efe147675df
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-23 11:52:51 +0100

    Update build.sh

--- recipes/hap.py/build.sh
+++ recipes/hap.py/build.sh
@@ -6,10 +6,14 @@ sed -i.bak -e '/^configure_files.*bcftools/s/^/#/' CMakeLists.txt
 sed -i.bak -e '/^configure_files.*samtools/s/^/#/' CMakeLists.txt
 # Avoid build errors with samtools/curses by skipping
 sed -i.bak 's/make -j4 -C samtools/#make -j4 -C samtools/' external/make_dependencies.sh
+
+# Do not build "external dependencies". This is wrong to begin with and compilers and such are skipped anyway
+sed -i.bak "26,33d" CMakeLists.txt
+
 mkdir -p build
 cd build
 export BOOST_ROOT=$PREFIX
 cmake ../ -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=${PREFIX}
 make
-rm -f lib/libhts*so
+#rm -f lib/libhts*so
 make install
