c06d4fd64cf595c899d80fa4227c1bc59d40ea7b
Holmer, Rens <rens.holmer@wur.nl>
2020-01-23 13:41:35 +0100

    missing glibcxx debugging

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -14,18 +14,20 @@ ls -lah .
 # ls -lah /usr/lib64/libstdc*
 # strings /usr/lib64/libstdc* | grep GLIBC
 
-# METEOR_NODE_PATH=$(meteor node -e 'console.log(process.execPath)')
 METEOR_PATH=$(readlink -f /root/.meteor/packages/meteor-tool/1.9.0)
-METEOR_NODE_PATH=$(find $METEOR_PATH -name node | grep dev_bundle/bin)
+#METEOR_NODE_PATH=$(find $METEOR_PATH -name node | grep dev_bundle/bin)
 NODE_PATH=$(which node)
 
-echo $METEOR_NODE_PATH
-echo $NODE_PATH
+find $METEOR_PATH -name node -type f | xargs -n 1 -P 1 -I % sh -c \
+    "echo %; mv % %.bak; ln -s $NODE_PATH %"
 
-mv $METEOR_NODE_PATH $METEOR_NODE_PATH.bak
-ln -s $NODE_PATH $METEOR_NODE_PATH
+#echo $METEOR_NODE_PATH
+#echo $NODE_PATH
 
-ls -lah /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/bin/
+#mv $METEOR_NODE_PATH $METEOR_NODE_PATH.bak
+#ln -s $NODE_PATH $METEOR_NODE_PATH
+
+#ls -lah /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/bin/
 
 meteor node -v
 
