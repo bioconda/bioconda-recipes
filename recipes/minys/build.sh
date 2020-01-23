bc8c445bdb67da6f56004820d0c6656159c5648f
cguyomar <cervin.guyomar@gmail.com>
2020-01-23 19:07:36 +0100

    Update of MinYS to v1.1

--- recipes/minys/build.sh
+++ recipes/minys/build.sh
@@ -3,7 +3,9 @@
 
 make -C graph_simplification/nwalign/
 
-mkdir -p $PREFIX/bin/
-cp  MinYS.py  ${PREFIX}/bin/
-cp -r minys_utils  ${PREFIX}/bin/
-cp -r graph_simplification  ${PREFIX}/bin/
+mkdir -p $PREFIX/bin
+ln -s  MinYS.py  ${PREFIX}/bin/
+ln -s graph_simplification/enumerate_paths.py  ${PREFIX}/bin/
+ln -s graph_simplification/filter_coomponents.py  ${PREFIX}/bin/
+ln -s graph_simplification/graph_simplification.py  ${PREFIX}/bin/
+
