16ed2e916c81936a00e0d20cb291c107aa07f659
Holmer, Rens <rens.holmer@wur.nl>
2020-01-23 16:16:19 +0100

    missing glibcxx debugging

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -2,8 +2,6 @@
 
 set -exo pipefail
 
-# sudo yum install libstdc++
-
 outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
 mkdir -p $outdir $PREFIX/bin
 pwd
@@ -11,17 +9,11 @@ ls -lah .
 
 npm install --unsafe-perm
 ls -lah .
-# ls -lah /usr/lib64/libstdc*
-# strings /usr/lib64/libstdc* | grep GLIBC
 
 METEOR_PATH=$(readlink -f /root/.meteor/packages/meteor-tool/1.9.0)
 
 METEOR_NODE_PATH=$(find $METEOR_PATH -name node | grep dev_bundle/bin)
 NODE_PATH=$(which node)
-
-
-#find $METEOR_PATH -name node -type f | xargs -n 1 -P 1 -I % sh -c \
-#    "echo %; mv % %.bak; ln -s $NODE_PATH %"
 
 echo $METEOR_NODE_PATH
 echo $NODE_PATH
@@ -38,11 +30,19 @@ echo $NPM_PATH
 mv $METEOR_NPM_PATH $METEOR_NPM_PATH.bak
 ln -s $NPM_PATH $METEOR_NPM_PATH
 
-npm install -g node-gyp
+pushd /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/lib/node_modules/fibers/
+ls -lah 
+npm install
+popd
 
-node /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/lib/node_modules/fibers/build
+# yum install gcc-c++
+# yum groupinstall 'Development Tools'
 
-#ls -lah /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/bin/
+# npm install -g node-gyp
+
+# node /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/lib/node_modules/fibers/build
+
+# ls -lah /root/.meteor/packages/meteor-tool/.1.9.0.1mtsmz6.qnno++os.linux.x86_64+web.browser+web.browser.legacy+web.cordova/mt-os.linux.x86_64/dev_bundle/bin/
 
 meteor node -v
 
