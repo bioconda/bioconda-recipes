10fc89d811e480e0c4a67ebfa9cf65d38c0b722a
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-24 14:11:17 +0100

    Update build.sh

--- recipes/sts/build.sh
+++ recipes/sts/build.sh
@@ -5,6 +5,13 @@ export INCLUDE_PATH=${PREFIX}/include
 export GSL_INCLUDE_DIRS=${PREFIX}/include
 export GSL_LIBRARY_DIRS=${PREFIX}/lib
 
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_COMPILER=$CC \
+      -DGSL_INCLUDE_DIRS=$PREFIX/include \
+      ..
 make
 mkdir -p $PREFIX/bin
 cp _build/release/bin/sts-online $PREFIX/bin
