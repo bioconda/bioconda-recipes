762ec9ba3a79b0eb5ca74d11e7c1f195a2eaf334
Shubham Chandak <schandak@stanford.edu>
2020-01-27 08:52:24 -0800

    add spring recipe - fix zlib

--- recipes/spring/build.sh
+++ recipes/spring/build.sh
@@ -3,7 +3,10 @@
 mkdir -p $PREFIX/bin
 mkdir build
 cd build
+export CPATH=${PREFIX}/include
+export CXXPATH=${PREFIX}/include
 export CFLAGS="$CFLAGS -I$PREFIX/include"
+export CXXFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 cmake ..
 make
