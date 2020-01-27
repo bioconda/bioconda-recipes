b4048ec43fe9dc72a66cae1059b4b6d82949de8b
Jacques Dainat <jacques.dainat@nbis.se>
2020-01-27 09:59:54 +0100

    Revert "Update build.sh"
    This reverts commit 13226bf02c5d96ada5c4511f0cb045edce9b99f5.

--- recipes/edta/build.sh
+++ recipes/edta/build.sh
@@ -15,10 +15,6 @@ perl ${EDTA_DIR}/\${NAME} \$@
 END
 
 chmod a+x ${PREFIX}/bin/EDTA.pl
-ln -s $PREFIX/share/EDTA/
 for name in ${EDTA_OTHER_PROGRAMS} ; do
-  ln -s ${PREFIX}/share/EDTA/$name ${PREFIX}/bin/$name)
+  ln -s ${PREFIX}/bin/EDTA.pl ${PREFIX}/bin/$(basename $name)
 done
-
-rm $PREFIX/share/EDTA/*.sh
-rm $PREFIX/share/EDTA/*.yaml
