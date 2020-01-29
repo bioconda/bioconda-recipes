332a92d78465b373b02f8e3843d48932524bb281
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-29 09:07:26 -0500

    Simplify recipe to use pip

--- recipes/sicer2/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Eeu -o pipefail
-
-if [[ "$(uname)" == Darwin ]]
-then
-	export MACOSX_DEPLOYMENT_TARGET=10.9
-fi
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
