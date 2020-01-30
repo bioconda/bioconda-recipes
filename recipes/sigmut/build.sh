a7ffb8c8f01f05de27ec41cd5f3b01fd2e2fff8d
lgpdv <leonardopnnz@gmail.com>
2020-01-30 13:40:41 +0100

    add sigmut

--- recipes/sigmut/build.sh
+++ recipes/sigmut/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp -r * ${PREFIX}/bin/
+cp sigprofiler ${PREFIX}/bin/
+cp -r lib_repo ${PREFIX}/bin/
 chmod u+rwx $PREFIX/bin/lib_repo/scripts/*
 chmod u+rwx $PREFIX/bin/lib_repo/SigProfPlot/sigProfilerPlotting/*
 chmod u+rwx $PREFIX/bin/*
