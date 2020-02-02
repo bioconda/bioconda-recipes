2957be789531dc969c15230b66b079b945ed83e9
Luis Pedro Coelho <luis@luispedro.org>
2020-02-02 19:00:44 +0800

    NGLess: 1.0.1 -> 1.1.0
    Attempt a new way to build, using the extended containers

--- recipes/ngless/build.sh
+++ recipes/ngless/build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 
-mkdir -p ${PREFIX}/bin
-mv ngless-1.0.1-static-Linux64 ngless
-chmod +x ngless
-mv ngless ${PREFIX}/bin
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+export CFLAGS="-I$PREFIX/include"
+export CPATH=${PREFIX}/include
+
+# These are picked up by stack setup.
+export fp_prog_ar=${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-ar
+export CONF_LD_LINKER_OPTS_STAGE2=-L${PREFIX}/lib
+export CONF_GCC_LINKER_OPTS_STAGE2=-L${PREFIX}/lib
+
+# stack relies on $HOME, so fake it:
+stack setup --local-bin-path ${PREFIX}/bin
+make install WGET="wget --no-check-certificate" prefix=$PREFIX
+
+rm -r .stack-work
