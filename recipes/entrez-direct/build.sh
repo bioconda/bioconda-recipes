55059799f4242b7382a2dc85f5199f19677134c9
Devon Ryan <dpryan79@gmail.com>
2020-02-02 21:17:35 +0100

    correct the build script

--- recipes/entrez-direct/build.sh
+++ recipes/entrez-direct/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sh build.sh
+go get -u github.com/fatih/color
+go get -u github.com/fiam/gounidecode/unidecode
+go get -u github.com/klauspost/cpuid
+go get -u github.com/pbnjay/memory
+go get -u github.com/surgebase/porter2
+go get -u golang.org/x/text/runes
+go get -u golang.org/x/text/transform
+go get -u golang.org/x/text/unicode/norm
+
+go build -o xtract xtract.go common.go
+go build -o rchive rchive.go common.go
+
 mv * "$PREFIX/bin/"
 mkdir -p "$PREFIX/home"
 export HOME="$PREFIX/home"
