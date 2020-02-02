ce378566e16358b1fada5d5153f6f531c3676d4b
Istvan Albert <istvan.albert@gmail.com>
2020-02-02 17:15:52 -0500

    [ci skip] Merge PR 20084
    Merge PR #20084, commits were: 
     * perform a bit of cleanup
     * is this building anything?
     * correct the build script
     * run build.sh first
     * try building the GO programs
     * Update meta.yaml
     * Update meta.yaml
     * Entrez direct is not architecture independent.
    
    Turns out Entrez direct is not architecture-independent. Some tools are written in go, for example xtract (the author may migrate even more)
    
    https://github.com/bioconda/bioconda-recipes/pull/17783

--- recipes/entrez-direct/build.sh
+++ recipes/entrez-direct/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-mv * "$PREFIX/bin/"
+go get -u github.com/fatih/color
+go get -u github.com/fiam/gounidecode/unidecode
+go get -u github.com/klauspost/cpuid
+go get -u github.com/pbnjay/memory
+go get -u github.com/surgebase/porter2
+go get -u golang.org/x/text/runes
+go get -u golang.org/x/text/transform
+go get -u golang.org/x/text/unicode/norm
+
+go build -o xtract xtract.go common.go
+go build -o rchive rchive.go common.go
+
+mkdir -p $PREFIX/bin
+mv * $PREFIX/bin
 mkdir -p "$PREFIX/home"
 export HOME="$PREFIX/home"
 sh ${PREFIX}/bin/setup.sh
 
+# clean up
+rm -rf $PREFIX/bin/src $PREFIX/bin/*.log $PREFIX/bin/*.go $PREFIX/bin/*.yaml $PREFIX/bin/*.sh
