5e51603acb0507e9cf1e4ed7b63cce24a4e8f424
Devon Ryan <dpryan79@gmail.com>
2020-02-02 22:53:55 +0100

    perform a bit of cleanup

--- recipes/entrez-direct/build.sh
+++ recipes/entrez-direct/build.sh
@@ -9,13 +9,14 @@ go get -u golang.org/x/text/runes
 go get -u golang.org/x/text/transform
 go get -u golang.org/x/text/unicode/norm
 
-ls -l
 go build -o xtract xtract.go common.go
 go build -o rchive rchive.go common.go
-ls -l
 
-mv * "$PREFIX/bin/"
+mkdir -p $PREFIX/bin
+mv * $PREFIX/bin
 mkdir -p "$PREFIX/home"
 export HOME="$PREFIX/home"
 sh ${PREFIX}/bin/setup.sh
 
+# clean up
+rm -rf $PREFIX/bin/src $PREFIX/bin/*.log $PREFIX/bin/*.go $PREFIX/bin/*.yaml $PREFIX/bin/*.sh
