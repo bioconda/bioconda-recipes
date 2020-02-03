3a1bd4ba54c8d738b5c47bf8963d34ab8cc6a83a
dpryan79 <dpryan79@gmail.com>
2020-02-03 14:24:10 +0100

    I think this will need per-python version builds

--- recipes/odgi/build.sh
+++ recipes/odgi/build.sh
@@ -4,16 +4,8 @@ export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
 cmake -H. -Bbuild
-ls -l bin
-#cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-#      -DCMAKE_CXX_COMPILER=${CXX} \
-#      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
-#      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-#      --build build
 cmake --build build
-echo "bin"
-ls -l bin
-echo "lib"
-ls -l lib
-echo "build"
-ls -l build
+mkdir -p $PREFIX/bin
+mv bin/* $PREFIX/bin
+mkdir -p $PREFIX/lib
+mv lib/* $PREFIX/lib
