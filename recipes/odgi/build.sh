e21bf0ec5dc53b13c26b77bbb1a8b7208a78ab7c
dpryan79 <dpryan79@gmail.com>
2020-02-03 13:46:28 +0100

    use the conda branch

--- recipes/odgi/build.sh
+++ recipes/odgi/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-git submodule update --init --recursive
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
 cmake -H. -Bbuild
 ls -l
-#cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-#      -DCMAKE_CXX_COMPILER=${CXX} \
-#      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
-#      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-#      --build build
-cmake --build build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+      --build build
+#cmake --build build
 echo "bin"
 ls -l bin
 echo "lib"
