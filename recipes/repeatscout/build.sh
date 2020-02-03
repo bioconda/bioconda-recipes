d53d6ec7d267836a9e2f84248f8c83767c6440b6
Jacques Dainat <jacques.dainat@nbis.se>
2020-02-03 10:18:25 +0100

    add CC=x86_64-apple-darwin13.4.0-clang

--- recipes/repeatscout/build.sh
+++ recipes/repeatscout/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x -e
 mkdir -p ${PREFIX}/bin
-make
+make CC=$CC
 
 cp RepeatScout build_lmer_table compare-out-to-gff.prl filter-stage-1.prl filter-stage-2.prl merge-lmer-tables.prl ${PREFIX}/bin
