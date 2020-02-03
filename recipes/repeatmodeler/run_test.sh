0117428fbe05ea8dd515213b9b6183e4d2023980
Jacques Dainat <jacques.dainat@nbis.se>
2020-02-03 15:56:33 +0100

    modify test to call rmblast instead of ncbi

--- recipes/repeatmodeler/run_test.sh
+++ recipes/repeatmodeler/run_test.sh
@@ -2,7 +2,7 @@
 set -x -e
 
 # build the db with BuildDatabase
-BuildDatabase -engine ncbi -name db test.fa 2> /dev/null
+BuildDatabase -engine rmblast -name db test.fa 2> /dev/null
 
 # run repeat modeler
 RepeatModeler -engine ncbi -database db 2> /dev/null
