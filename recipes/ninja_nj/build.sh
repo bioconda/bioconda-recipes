9c50b323bfbf9e7935743de3933bc30eb46e18f7
Jacques Dainat <jacques.dainat@nbis.se>
2020-02-04 17:17:25 +0100

    remove FLAGS

--- recipes/ninja_nj/build.sh
+++ recipes/ninja_nj/build.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -x -e
 
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
 export CXXFLAGS="-I$PREFIX/include -lrt -std=gnu++11 -Wall -mssse3 -fopenmp"
-export LDFLAGS="-L$PREFIX/lib"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 mkdir -p ${PREFIX}/bin
 
