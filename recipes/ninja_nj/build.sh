7611515c99a74d596f20d0107c6c2a5d8deec0d9
Jacques Dainat <jacques.dainat@nbis.se>
2020-02-04 17:10:14 +0100

    add FLAGS

--- recipes/ninja_nj/build.sh
+++ recipes/ninja_nj/build.sh
@@ -3,7 +3,7 @@ set -x -e
 
 export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include -lrt"
+export CXXFLAGS="-I$PREFIX/include -lrt -std=gnu++11 -Wall -mssse3 -fopenmp"
 export LDFLAGS="-L$PREFIX/lib"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
@@ -11,5 +11,5 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 mkdir -p ${PREFIX}/bin
 
 cd NINJA/
-make all CXX="$CXX"
+make all CXX="$CXX" CXXFLAGS="$CXXFLAGS"
 cp Ninja ${PREFIX}/bin
