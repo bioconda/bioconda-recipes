0657c2e203d5676b48d9f7e7e475527440be5d0d
Luiz Irber <luiz.irber@gmail.com>
2020-02-05 09:06:23 -0800

    fix extension for lib

--- recipes/sourmash/build.sh
+++ recipes/sourmash/build.sh
@@ -3,7 +3,7 @@
 set -x
 set -o xtrace -o nounset -o pipefail -o errexit
 
-SOEXT=.so
+SOEXT=so
 
 # taken from yacrd recipe, see: https://github.com/bioconda/bioconda-recipes/blob/2b02c3db6400499d910bc5f297d23cb20c9db4f8/recipes/yacrd/build.sh
 if [ "$(uname)" == "Darwin" ]; then
@@ -14,7 +14,7 @@ if [ "$(uname)" == "Darwin" ]; then
     # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
     git config --global --unset url.ssh://git@github.com.insteadOf || true
 
-    SOEXT=.dylib
+    SOEXT=dylib
 fi
 
 export RUSTC_WRAPPER=$(which sccache)
