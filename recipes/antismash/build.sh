bc5181089d0e812c833394bebc401ec504d506a3
Antonio Camargo <antoniop.camargo@gmail.com>
2020-02-05 10:32:42 -0800

    Fix path

--- recipes/antismash/build.sh
+++ recipes/antismash/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "include antismash/detection/genefinding/train_crypto" >> MANIFEST.in
+echo "include antismash/detection/genefinding/data/train_crypto" >> MANIFEST.in
 
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
