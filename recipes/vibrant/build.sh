6e796ca9ae5dfb46e59c02ad2e56727fc06c3590
dpryan79 <dpryan79@gmail.com>
2020-02-10 11:29:05 +0100

    update patch

--- recipes/vibrant/build.sh
+++ recipes/vibrant/build.sh
@@ -3,14 +3,9 @@
 mkdir -p ${PREFIX}/bin
 
 cp VIBRANT_run.py ${PREFIX}/bin
-cp scripts/VIBRANT_annotation.py ${PREFIX}/bin
-cp scripts/VIBRANT_extract_nucleotide.py ${PREFIX}/bin
-cp scripts/VIBRANT_extract_protein.py ${PREFIX}/bin
+cp scripts/* ${PREFIX}/bin
 
-chmod +x ${PREFIX}/bin/VIBRANT_run.py
-chmod +x ${PREFIX}/bin/VIBRANT_annotation.py
-chmod +x ${PREFIX}/bin/VIBRANT_extract_nucleotide.py
-chmod +x ${PREFIX}/bin/VIBRANT_extract_protein.py
+chmod +x ${PREFIX}/bin/*
 
 # copy script to download database
 cp ${RECIPE_DIR}/download-db.sh ${PREFIX}/bin
