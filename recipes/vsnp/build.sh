2af509ab7baa344385435e736d5fab58bfdedfd3
Stuber <tstuber@AAPIAAM1V11222E.usda.net>
2020-02-11 14:39:52 -0600

    build update

--- recipes/vsnp/build.sh
+++ recipes/vsnp/build.sh
@@ -2,8 +2,4 @@
 
 mkdir -p $PREFIX/bin
 mv bin/*.py $PREFIX/bin
-
-#prevent clobbering user's reference_options_paths.txt
-if [[ ! -f $PREFIX/dependencies/reference_options_paths.txt ]]; then
-    mv dependencies $PREFIX # if text file does not exists add dependencies
-fi
+mv dependencies $PREFIX
