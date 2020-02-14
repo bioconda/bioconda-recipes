ea53b9424222ac5703ce0fed9647ad910057d393
Dermot Harnett <zslastman@gmail.com>
2020-02-14 11:43:11 +0100

    commit for testing - seems right, but circleci fails

--- recipes/riboseqc/build.sh
+++ recipes/riboseqc/build.sh
@@ -1,1 +1,11 @@
-$R CMD INSTALL --build .
+#!/bin/bash
+
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  mv DESCRIPTION DESCRIPTION.old
+  grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
+  $R CMD INSTALL --build .
+else
+  mkdir -p $PREFIX/lib/R/library/here
+  mv * $PREFIX/lib/R/library/here
+fi
