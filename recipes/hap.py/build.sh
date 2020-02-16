2d668f199228da27fbdd70e94ee41404094667a4
Brad Langhorst <langhorst@neb.com>
2020-02-16 07:44:44 -0500

    removes extraneous host dependencies

--- recipes/hap.py/build.sh
+++ recipes/hap.py/build.sh
@@ -8,28 +8,19 @@ export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export TOOLSET=gcc
 export BOOST_ROOT=${PREFIX}
 export HTSLIB_ROOT=${PREFIX}
-export HTSLIB_LIBRARY_DIR=${PREFIX}/lib
-export HTSLIB_INCLUDE_DIR=${PREFIX}/include
-export CXXFLAGS="-ldeflate -lrt -lm -lz -llzma -lbz2 -ldl"
+export CXXFLAGS="-ldeflate -lrt -lz -lbz2 -llzma -ldl"
 
-#tricks down stream to skip builds of packages already in bioconda
+#tricks make_dependencies to skip builds of packages already in bioconda, except zlib 
+# (comments allude to needing this specific version)
 mkdir -p $SRC_DIR/build/include/htslib
-#mkdir -p $SRC_DIR/build/scratch/zlib-1.2.8/
-#touch $SRC_DIR/build/scratch/zlib-1.2.8/libz.a
 mkdir -p $SRC_DIR/build/bin
 touch $SRC_DIR/build/bin/samtools
 touch $SRC_DIR/build/bin/bcftools
-
-#if [ ! -d ${ISD}/include/htslib ] ;
-#sed -i.bak -e '/include\/htslib/s/\!//' external/make_dependencies.sh
-
 
 cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DBOOST_ROOT="${PREFIX}" -DBoost_NO_SYSTEM_PATHS=ON -DCMAKE_BUILD_TYPE=Release \
   -DHTSLIB_ROOT="${PREFIX}"
 make
-rm -f lib/libhts*so
 make install
