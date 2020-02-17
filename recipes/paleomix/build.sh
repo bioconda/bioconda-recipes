88a510d8faa653663888aab84608c2bf27333374
gtrichard <26085589+gtrichard@users.noreply.github.com>
2020-02-17 17:36:36 +0100

    fix path

--- recipes/paleomix/build.sh
+++ recipes/paleomix/build.sh
@@ -2,4 +2,4 @@
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/lib/python2.7/site-packages/paleomix
 cp paleomix/main.py $PREFIX/bin/main.py
-cp -r paleomix $PREFIX/lib/python2.7/site-packages/paleomix
+cp -r paleomix $PREFIX/lib/python2.7/site-packages/
