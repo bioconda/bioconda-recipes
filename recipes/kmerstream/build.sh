7d06c3bb0d856c70ad1537886d9808894c109ab7
Fabio <miranda.fmm@gmail.com>
2020-02-18 17:50:23 +0100

    Added kmerstream

---
+++ recipes/kmerstream/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x -e
+
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CXXFLAGS="-I$PREFIX/include"
+export CPPFLAGS="-I${PREFIX}/include"
+export LC_ALL=C
+
+sed -i '4d' Makefile
+
+make
+
+cp KmerStream $PREFIX/bin
+cp KmerStreamEstimate.py $PREFIX/bin
+cp KmerStreamJoin $PREFIX/bin
