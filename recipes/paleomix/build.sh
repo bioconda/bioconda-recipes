cf9b287bd296cbe21a2a25c1e20c656d29eb95eb
gtrichard <26085589+gtrichard@users.noreply.github.com>
2020-02-20 11:03:33 +0100

    using pip

--- recipes/paleomix/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/lib/python2.7/site-packages/paleomix
-cp paleomix/main.py $PREFIX/bin/main.py
-cp -r paleomix $PREFIX/lib/python2.7/site-packages/
