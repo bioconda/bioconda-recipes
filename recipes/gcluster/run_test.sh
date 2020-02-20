407d1107932c2eb37fc0691437e1e73180fd8325
Xiangyang1984 <58356520+Xiangyang1984@users.noreply.github.com>
2020-02-20 16:51:17 +0800

    Update run_test.sh

--- recipes/gcluster/run_test.sh
+++ recipes/gcluster/run_test.sh
@@ -3,6 +3,6 @@
 set -x -e
 
 # run gcluster
-Gcluster -dir test_data/gbk -gene test_data/interested_gene_name.txt -tree test_data/16S_rRNA_tree.nwk -m 3 -PNG F
+Gcluster -dir test_data/gbk -gene test_data/interested_gene_name.txt -tree test_data/16S_rRNA_tree.nwk -m 3 #-PNG F
 
 rm -rf test_data
