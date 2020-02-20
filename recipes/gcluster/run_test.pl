5dbb6f2a7d2cd6ab37e1a69204f6ed1940db82e5
Xiangyang1984 <58356520+Xiangyang1984@users.noreply.github.com>
2020-02-20 17:56:52 +0800

    Update run_test.pl

--- recipes/gcluster/run_test.pl
+++ recipes/gcluster/run_test.pl
@@ -10,4 +10,4 @@ chdir $home_directory;
 system ("Gcluster -dir test_data/gbk -gene test_data/interested_gene_name.txt -tree test_data/16S_rRNA_tree.nwk -m 3");
 
 print "--------$home_directory---------\n";
-rm -rf test_data;
+system ("rm -rf test_data");
