e4ff77ce9a64ab262a31d161ddf000432652a869
Devon Ryan <dpryan79@gmail.com>
2020-02-22 23:03:30 +0100

    update build.sh

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
-mkdir groot
-mv cmd src main.go groot/
-mkdir -p src/github.com/will-rowe/
-mv groot src/github.com/will-rowe/
-cd src/github.com/will-rowe/groot
 
 export GOPATH="$SRC_DIR/"
-go get ./...
+go get -d -t -v ./...
 go build -o groot main.go
 mv groot $PREFIX/bin
