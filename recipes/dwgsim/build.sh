f83fafdf42dffb162f1565f456bc12fabcd0d34e
Lorena Pantano <lorena.pantano@gmail.com>
2020-02-24 14:17:58 -0500

    Try to add the include and lib in build

--- recipes/dwgsim/build.sh
+++ recipes/dwgsim/build.sh
@@ -4,6 +4,11 @@ set -x -e -o pipefail
 
 (git clone http://github.com/samtools/samtools && cd samtools && git checkout 28391e5898804ce6b805016)
 
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+sed -i.bak 's/CFLAGS =/CFLAGS +=/' Makefile
+
 make CC=$CC LIBCURSES=-lncurses
 
 mkdir -p $PREFIX/bin
