6159f20b2fe158ce30f82ebb5626827d32e7d14e
Will Rowe <will.rowe@stfc.ac.uk>
2020-02-24 09:35:44 +0000

    Update build.sh

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
-export GOPATH="$SRC_DIR/"
 
+#export GOPATH="$SRC_DIR/"
 rm go.mod
 
 go get github.com/pierrec/lz4 && cd $GOPATH/src/github.com/pierrec/lz4 && git fetch && git checkout v3.0.1
