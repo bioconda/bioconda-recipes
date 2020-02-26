9f07e3c56e8623ba6505b3f525f246af7165afae
nh3 <nh3@users.noreply.github.com>
2020-02-26 21:57:20 +0000

    Don't install from github as r-monocle3 in deps

--- recipes/monocle3-cli/build.sh
+++ recipes/monocle3-cli/build.sh
@@ -4,7 +4,6 @@ export R_GZIPCMD=$(which gzip)
 export R_BZIPCMD=$(which bzip2)
 export LANG=en_US.UTF-8
 
-Rscript -e 'devtools::install_github("cole-trapnell-lab/monocle3", ref="'$monocle_version'", upgrade="never")'
 mkdir -p $PREFIX/bin
 cp exec/monocle3 $PREFIX/bin
 $R CMD INSTALL --build .
