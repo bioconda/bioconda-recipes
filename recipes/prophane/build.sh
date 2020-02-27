d789c95bda97b8ef155ad147652e8dc69f60ee27
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2020-02-27 09:48:40 +0100

    clean up build.sh from unnecessary comments

--- recipes/prophane/build.sh
+++ recipes/prophane/build.sh
@@ -3,8 +3,5 @@
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/opt/prophane/
 
-# Full path to the Snakefile
-#sed -i "s|Snakefile|$PREFIX/opt/metameta/Snakefile|g" metameta
-
 cp -r * $PREFIX/opt/prophane/
 ln -s $PREFIX/opt/prophane/prophane $PREFIX/bin/
