bc25cb040bdac3690829041c7dbf3cf3ec2f7560
Devon Ryan <dpryan79@gmail.com>
2020-02-27 11:48:06 +0100

    the shebang hasn't been changed yet

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -3,5 +3,5 @@
 $PYTHON -m pip install . --ignore-installed --no-deps
 
 # Load the CARD database JSON file
-$PREFIX/bin/rgi load -i card-data/card.json
+python $PREFIX/bin/rgi load -i card-data/card.json
 rm -rf card-data
