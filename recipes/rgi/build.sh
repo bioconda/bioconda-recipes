43b9bd0dafa7ee9d57c0d32fa71b52d2973062ad
Devon Ryan <dpryan79@gmail.com>
2020-02-27 11:38:03 +0100

    fix the rgi path

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -3,5 +3,5 @@
 $PYTHON -m pip install . --ignore-installed --no-deps
 
 # Load the CARD database JSON file
-python `which rgi` load -i card-data/card.json
+$PREFIX/bin/rgi load -i card-data/card.json
 rm -rf card-data
