75480f91aab00dec564caab3454b4c97dd0e5b35
Michael Hall <mbhall88@gmail.com>
2020-03-01 09:03:08 +0000

    try cargo git fix

--- recipes/sketchy/build.sh
+++ recipes/sketchy/build.sh
@@ -2,6 +2,8 @@
 set -x
 export RUST_BACKTRACE=full
 
-cargo install -v --locked --root "$PREFIX" --path .
+# See https://github.com/rust-lang/cargo/issues/2078
+env CARGO_NET_GIT_FETCH_WITH_CLI=true \
+    cargo install -v --locked --root "$PREFIX" --path .
 
 "$PYTHON" -m pip install --no-deps --ignore-installed -vv .
