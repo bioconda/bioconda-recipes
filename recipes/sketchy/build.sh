a823f133282ed7d3689f4411b5c91deb2c8fa789
Michael Hall <mbhall88@gmail.com>
2020-03-01 12:27:03 +0000

    try no git config unset

--- recipes/sketchy/build.sh
+++ recipes/sketchy/build.sh
@@ -7,7 +7,7 @@ if [ "$(uname)" == "Darwin" ]; then
     export HOME="/Users/distiller/"
     echo "HOME is $HOME"
     # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
-    git config --global --unset url.ssh://git@github.com/.insteadOf
+    # git config --global --unset url.ssh://git@github.com/.insteadOf
 fi
 
 cargo install -v --locked --root "$PREFIX" --path .
