a8a7657fbca1d554a80e99aa824de92ba04be28b
Vivek Rai <mail@raivivek.in>
2020-03-01 21:33:02 +0000

    ...

--- recipes/sra-tools/build.sh
+++ recipes/sra-tools/build.sh
@@ -10,35 +10,27 @@ if [[ $OSTYPE == darwin* ]]; then
      export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 fi
 
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-gcc $BUILD_PREFIX/bin/gcc
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++ $BUILD_PREFIX/bin/g++
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc $BUILD_PREFIX/bin/cc
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++ $BUILD_PREFIX/bin/c++
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ar $BUILD_PREFIX/bin/ar
-ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $BUILD_PREFIX/bin/ld
-
 export PATH=$BUILD_PREFIX/bin:$PATH
 
 pushd ncbi-vdb
 ./configure \
     --prefix=$PREFIX \
     --build-prefix=$NCBI_OUTDIR \
-    --debug
-make
+    --with-ngs-sdk-prefix=$PREFIX
+make -j${CPU_COUNT}
 popd
 
 pushd sra-tools
 
 pushd tools/driver-tool/utf8proc
-make
+make -j${CPU_COUNT}
 popd
 
 ./configure \
     --prefix=$PREFIX \
     --build-prefix=$NCBI_OUTDIR \
-    --with-ngs-sdk-prefix=$PREFIX \
-    --debug
-make
+    --with-ngs-sdk-prefix=$PREFIX
+make -j${CPU_COUNT}
 make install
 popd
 
