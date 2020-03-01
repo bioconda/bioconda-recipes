15397cac1bf47b3f8439551bce5135c4d1a1d197
Michael Hall <mbhall88@gmail.com>
2020-03-01 09:57:09 +0000

    another attempt at fixing mac failure

--- recipes/sketchy/build.sh
+++ recipes/sketchy/build.sh
@@ -2,8 +2,11 @@
 set -x
 export RUST_BACKTRACE=full
 
-# See https://github.com/rust-lang/cargo/issues/2078
-env CARGO_NET_GIT_FETCH_WITH_CLI=true \
-    cargo install -v --locked --root "$PREFIX" --path .
+if [ "$(uname)" == "Darwin" ]; then
+    # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
+    git config --global --unset url.ssh://git@github.com:.insteadof
+fi
+
+cargo install -v --locked --root "$PREFIX" --path .
 
 "$PYTHON" -m pip install --no-deps --ignore-installed -vv .
