fc534793c7aab283b2c739a5e738cb4c93536409
Michael Hall <michael@mbh.sh>
2020-03-02 17:19:31 +0000

    clean up comments

--- recipes/sketchy/build.sh
+++ recipes/sketchy/build.sh
@@ -6,12 +6,9 @@ if [ "$(uname)" == "Darwin" ]; then
     # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
     export HOME="/Users/distiller"
     echo "HOME is $HOME"
-    # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
-    # git config --global --unset url.ssh://git@github.com/.insteadOf
-
-    # https://github.com/rust-lang/cargo/issues/1851#issuecomment-288791836
-    # printf 'Host *\n  UseKeychain yes\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_rsa\n' >> "${HOME}/.ssh/config"
-    # ssh-add -K "${HOME}/.ssh/id_rsa"
+    
+    # Fix for when some packages use ssh to fetch git repos
+    # See https://github.com/rust-lang/cargo/issues/1851#issuecomment-450130685
     mkdir -p "${HOME}/.cargo"
     printf "[net]\ngit-fetch-with-cli = true\n" >> "${HOME}/.cargo/config"
 fi
