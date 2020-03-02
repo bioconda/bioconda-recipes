e25afa259d252bb1ea7f22ca412ca425ae28dd32
Daniel Cameron <d-cameron@github.com>
2020-03-02 16:27:16 +1100

    Switched to using the new gridss wrapper script Added driver scripts for all GRIDSS programs

--- recipes/gridss/build.sh
+++ recipes/gridss/build.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
 TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
-mkdir -p "$TGT"
-mkdir -p "${PREFIX}/bin"
+[ -d "$TGT" ] || mkdir -p "$TGT"
+[ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
 
+echo "PREFIX=${PREFIX}"
+echo "TGT=${TGT}"
+
+cd "${SRC_DIR}"
 cp gridss-*.jar $TGT/gridss.jar
+cp *.R *.sh "${TGT}"
 
-cp $RECIPE_DIR/gridss.py $TGT/gridss
+cp $RECIPE_DIR/gridss $TGT/gridss
+cp $RECIPE_DIR/gridss_r_script $TGT/gridss_r_script
+cp $RECIPE_DIR/gridss_java_entrypoint $TGT/gridss_java_entrypoint
+
 ln -s $TGT/gridss $PREFIX/bin
-chmod 0755 "${PREFIX}/bin/gridss"
+ln -s $TGT/gridss_r_script $PREFIX/bin/gridss_somatic_filter
+ln -s $TGT/gridss_r_script $PREFIX/bin/gridss_annotate_insertions_repeatmaster
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AllocateEvidence
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AnnotateInexactHomology
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AnnotateInexactHomologyBedpe
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AnnotateReferenceCoverage
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AnnotateUntemplatedSequence
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AnnotateVariants
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/AssembleBreakends
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CallVariants
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectGridssMetricsAndExtractFullReads
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectGridssMetricsAndExtractSVReads
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/ComputeSamTags
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/ExtractFullReads
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/ExtractSVReads
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/GeneratePonBedpe
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/IdentifyVariants
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/IndexedExtractFullReads
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/SoftClipsToSplitReads
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectCigarMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectFragmentGCMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectGridssMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectIdsvMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectMapqMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectStructuralVariantReadMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/CollectTagMetrics
+ln -s $TGT/gridss_java_entrypoint $PREFIX/bin/ReportThresholdCoverage
+chmod 0755 "${PREFIX}/bin/*"
