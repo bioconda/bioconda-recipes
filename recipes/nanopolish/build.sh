4b754715044f7bf2e987c40202f38690cec8e39e
Devon Ryan <dpryan79@gmail.com>
2020-03-04 16:43:31 +0100

    need to build minimap2 library

--- recipes/nanopolish/build.sh
+++ recipes/nanopolish/build.sh
@@ -10,7 +10,7 @@ export FAST5_INCLUDE=-I${PREFIX}/include/fast5
 mkdir -p $PREFIX/bin
 
 ls -l minimap2
-make HDF5=noinstall EIGEN=noinstall HTS=noinstall MINIMAP2=noinstall CXXFLAGS="-Iminimap2 -g -O3"
+make HDF5=noinstall EIGEN=noinstall HTS=noinstall CXXFLAGS="-Iminimap2 -g -O3"
 cp nanopolish $PREFIX/bin
 cp scripts/nanopolish_makerange.py $PREFIX/bin
 cp scripts/nanopolish_merge.py $PREFIX/bin
