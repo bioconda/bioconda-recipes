ad180a778d250dec3aac592143b01b4db6374683
Antônio Pedro Camargo <apcamargo@users.noreply.github.com>
2020-03-06 14:55:02 -0800

    Fix build.sh

--- recipes/kcounter/build.sh
+++ recipes/kcounter/build.sh
@@ -2,16 +2,8 @@
 
 set -ex
 
-if [ `uname` == Darwin ]; then
-  export HOME=`mktemp -d`
-fi
-
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile=minimal -y
-
-export PATH="$HOME/.cargo/bin:$PATH"
-
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="$CC"
+rustup --default-toolchain nightly --profile=minimal -y
 
 maturin build --interpreter python --release
 
-$PYTHON -m pip install target/wheels --no-deps --ignore-installed -vv
+$PYTHON -m pip install target/wheels/. --no-deps --ignore-installed -vv
