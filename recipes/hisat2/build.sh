c547fb78507434fc89d98cd1be8f063f42244ccb
Devon Ryan <dpryan79@users.noreply.github.com>
2020-03-07 20:58:16 +0100

    Update build.sh

--- recipes/hisat2/build.sh
+++ recipes/hisat2/build.sh
@@ -22,30 +22,14 @@ for i in \
     hisat2-inspect \
     hisat2-inspect-l \
     hisat2-inspect-s \
-    hisat2_extract_exons.py \
-    hisat2_extract_snps_haplotypes_UCSC.py \
-    hisat2_extract_snps_haplotypes_VCF.py \
-    hisat2_extract_splice_sites.py \
-    hisat2_simulate_reads.py \
-    hisatgenotype_build_genome.py \
-    hisatgenotype_extract_reads.py \
-    hisatgenotype_extract_vars.py \
-    hisatgenotype_hla_cyp.py \
-    hisatgenotype_locus.py \
-    hisatgenotype.py;
+    *.py;
 do
     cp ${i} ${PREFIX}/bin
     chmod +x ${PREFIX}/bin/${i}
 done
 
 # modules needed in PYTHONPATH
-for i in \
-    hisatgenotype_modules/hisatgenotype_assembly_graph.py \
-    hisatgenotype_modules/hisatgenotype_convert_codis.py \
-    hisatgenotype_modules/hisatgenotype_extract_codis_data.py \
-    hisatgenotype_modules/hisatgenotype_extract_cyp_data.py \
-    hisatgenotype_modules/hisatgenotype_gene_typing.py \
-    hisatgenotype_modules/hisatgenotype_typing_common.py ;
+for i in hisatgenotype_modules/*.py;
 do
    cp ${i} ${PREFIX}/lib/python${PY_VER}/site-packages
 done
