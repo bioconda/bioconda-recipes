bdd498323618b04881c50e159995260b85e91b5e
Marcel Martin <marcel.martin@scilifelab.se>
2020-03-08 17:01:43 +0100

    CFLAGS/LDFLAGS
    CFLAGS and LDFLAGS already exist and include `-I$PREFIX/include`, `-L$PREFIX/lib`. Also, add quote marks where needed.

--- recipes/dwgsim/build.sh
+++ recipes/dwgsim/build.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-set -x -e -o pipefail
+set -xeuo pipefail
 
 (git clone http://github.com/samtools/samtools && cd samtools && git checkout 28391e5898804ce6b805016)
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
 sed -i.bak 's/CFLAGS =/CFLAGS +=/' Makefile
 
-make CC=$CC LIBCURSES=-lncurses CFLAGS=$CFLAGS LDFLAGS=$LDFLAGS
+make CC=$CC LIBCURSES=-lncurses CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 
 mkdir -p $PREFIX/bin
 
