728df48ebe8ee3efca3c36706537e470dcf83290
Natasha Pavlovikj <npavlovikj2@unl.edu>
2020-03-09 17:18:07 -0500

    Update build script

--- recipes/hisat2/build.sh
+++ recipes/hisat2/build.sh
@@ -4,11 +4,30 @@ mkdir -p ${PREFIX}/bin
 
 make CC=${CC} CPP=${CXX}
 
+
+# convert Python 2.7 scripts to 3
 if [ $PY3K -eq 1 ]
-then	
-    2to3 --write *.py \	
-        hisatgenotype_modules/*.py \	
-        hisatgenotype_scripts/*.py
+then
+    2to3 --write \
+        extract_exons.py \
+        extract_splice_sites.py \
+        hisat2_extract_exons.py \
+        hisat2_extract_snps_haplotypes_UCSC.py \
+        hisat2_extract_snps_haplotypes_VCF.py \
+        hisat2_extract_splice_sites.py \
+        hisat2_simulate_reads.py \
+        hisatgenotype_build_genome.py \
+        hisatgenotype_extract_reads.py \
+        hisatgenotype_extract_vars.py \
+        hisatgenotype_hla_cyp.py \
+        hisatgenotype_locus.py \
+        hisatgenotype_modules/hisatgenotype_assembly_graph.py \
+        hisatgenotype_modules/hisatgenotype_typing_common.py \
+        hisatgenotype_scripts/compare_HLA_Omixon.py \
+        hisatgenotype_scripts/extract_Omixon_HLA.py \
+        hisatgenotype_scripts/hisatgenotype_HLA_genotyping_PGs.py \
+        hisatgenotype_scripts/hisatgenotype_locus_samples.py \
+        hisatgenotype.py
 fi
 
 # copy binaries and python scripts
@@ -22,14 +41,26 @@ for i in \
     hisat2-inspect \
     hisat2-inspect-l \
     hisat2-inspect-s \
-    *.py;
+    hisat2_extract_exons.py \
+    hisat2_extract_snps_haplotypes_UCSC.py \
+    hisat2_extract_snps_haplotypes_VCF.py \
+    hisat2_extract_splice_sites.py \
+    hisat2_simulate_reads.py \
+    hisatgenotype_build_genome.py \
+    hisatgenotype_extract_reads.py \
+    hisatgenotype_extract_vars.py \
+    hisatgenotype_hla_cyp.py \
+    hisatgenotype_locus.py \
+    hisatgenotype.py;
 do
     cp ${i} ${PREFIX}/bin
     chmod +x ${PREFIX}/bin/${i}
 done
 
 # modules needed in PYTHONPATH
-for i in hisatgenotype_modules/*.py;
+for i in \
+    hisatgenotype_modules/hisatgenotype_assembly_graph.py \
+    hisatgenotype_modules/hisatgenotype_typing_common.py ;
 do
    cp ${i} ${PREFIX}/lib/python${PY_VER}/site-packages
 done
