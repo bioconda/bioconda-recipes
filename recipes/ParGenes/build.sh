f6efaf2a829a1f72b8565ad981a88269633bbe9a
Pierre Barbera <pierre.pbdas@gmail.com>
2020-03-10 10:05:47 +0100

    inital recipe

---
+++ recipes/ParGenes/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make m4 accessible to bison
+# https://github.com/conda-forge/bison-feedstock/issues/7#issuecomment-431602144
+export M4=m4
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+
+# build
+./install.sh
+
+dest=${PREFIX}/bin
+mkdir -p ${dest}
+
+# copy over all relevant files for the package
+for file in raxml-ng modeltest-ng astral.jar mpi-scheduler; do
+  find . -type f -name ${file} -print -exec cp --parents '{}' ${dest} \;
+done
+
+for patt in ".*/pargenes/.*\.py" ".*\.so"; do
+  find . -type f -regextype posix-egrep -regex "${patt}" -print -exec cp --parents '{}' ${dest} \;
+done
