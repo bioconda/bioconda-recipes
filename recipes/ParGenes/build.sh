f6481d09bdecdd3227d3c82181e75674b3a1ea49
Pierre Barbera <pierre.pbdas@gmail.com>
2020-03-10 12:14:34 +0100

    bumped version, added more symlinks in the package path, added more libs to the package

--- recipes/ParGenes/build.sh
+++ recipes/ParGenes/build.sh
@@ -14,15 +14,15 @@ dest=${PREFIX}/bin
 mkdir -p ${dest}
 
 # copy over all relevant files for the package
-for file in raxml-ng modeltest-ng astral.jar mpi-scheduler; do
+for file in raxml-ng modeltest-ng mpi-scheduler; do
   find . -type f -name ${file} -print -exec cp --parents '{}' ${dest} \;
 done
 
-for patt in ".*/pargenes/.*\.py" ".*\.so"; do
+for patt in ".*/pargenes/.*\.py" ".*\.so" ".*\.jar" ".*\.dylib"; do
   find . -type f -regextype posix-egrep -regex "${patt}" -print -exec cp --parents '{}' ${dest} \;
 done
 
 cd ${dest}
 ln -s pargenes/pargenes.py pargenes.py
-
-echo -e "#!/usr/bin/env python\n$(cat pargenes/pargenes.py)" > pargenes/pargenes.py
+ln -s pargenes/pargenes-hpc.py pargenes-hpc.py
+ln -s pargenes/pargenes-hpc-debug.py pargenes-hpc-debug.py
