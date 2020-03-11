cc79f8dbb8c18f451497d0e886cfb08c5c29794b
Devon Ryan <dpryan79@users.noreply.github.com>
2020-03-11 11:06:32 +0100

    Update build.sh

--- recipes/survivor/build.sh
+++ recipes/survivor/build.sh
@@ -5,6 +5,8 @@ sed -i.bak "s#g++#$CXX -I$PREFIX/include -L$PREFIX/lib#g" Debug/makefile
 for f in Debug/src/*/subdir.mk; do
     sed -i.bak "s#g++#$CXX -I$PREFIX/include -L$PREFIX/lib#g" $f
 done
+sed -i.bak "s#g++#$CXX -I$PREFIX/include -L$PREFIX/lib#g" Debug/src/subdir.mk
+
 cd Debug
 make
 cp SURVIVOR $PREFIX/bin
