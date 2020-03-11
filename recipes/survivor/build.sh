64383d6b10f8d5a919f0a72cb727e9cf955f7421
Devon Ryan <dpryan79@users.noreply.github.com>
2020-03-11 10:49:07 +0100

    Update build.sh

--- recipes/survivor/build.sh
+++ recipes/survivor/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export CXX_INCLUDE_PATH=$PREFIX/include
+export CPP_INCLUDE_PATH=$PREFIX/include
+export CPPLUS_INCLUDE_PATH=$PREFIX/include
+
 mkdir -p $PREFIX/bin
 sed -i.bak "s#g++#$CXX#g" Debug/makefile
 for f in Debug/src/*/subdir.mk; do
