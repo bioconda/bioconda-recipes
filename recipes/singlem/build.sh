37dfe7b25aad2a06d4512dbd18ae57dfb5cf1ea4
Aaron Mussig <aaronmussig@gmail.com>
2020-03-12 15:40:54 +1000

    add singlem

--- recipes/singlem/build.sh
+++ recipes/singlem/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install python libraries
+python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+
 # copy main singlem python script
 chmod +x bin/singlem
 cp bin/singlem ${PREFIX}/bin/
