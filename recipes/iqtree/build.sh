93a5cdcdf87f5d08fc11ef0857958a054e053691
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-03-13 13:48:04 -0700

    Clean up build.sh ala shellcheck

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -3,7 +3,7 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
-if [ `uname` == Darwin ]; then
+if [ "$(uname)" == Darwin ]; then
     export CMAKE_C_COMPILER="clang"
     export CMAKE_CXX_COMPILER="clang++"
 fi
@@ -11,7 +11,7 @@ fi
 mkdir build
 cd build
 
-cmake -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX -DIQTREE_FLAGS=omp ..
+cmake -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" -DIQTREE_FLAGS=omp ..
 
-make -j${CPU_COUNT}
+make --jobs "${CPU_COUNT}"
 make install
