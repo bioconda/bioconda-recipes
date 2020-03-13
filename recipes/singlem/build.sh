39195bc9d0768316b9bf5339d54c9a136909c23c
Aaron Mussig <aaronmussig@gmail.com>
2020-03-13 10:55:10 +1000

    Should just need the orator package on conda-forge now

--- recipes/singlem/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install python libraries
-python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-
-# copy main singlem python script
-chmod +x bin/singlem
-cp bin/singlem ${PREFIX}/bin/
