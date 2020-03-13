00a4e9e6f2c135e5d83909d462fc812b78d92eb3
andvides <bernardo.benavides@udea.edu.co>
2020-03-12 23:03:37 -0500

    Changes to be committed: 	modified:   selectfasta/build.sh 	modified:   selectfasta/meta.yaml

--- recipes/selectfasta/build.sh
+++ recipes/selectfasta/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 echo "selectFasta compilation"
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
 make
 mkdir -p $PREFIX/bin
 cp selectFasta $PREFIX/bin
 echo "Installation successful"
-selectFasta -h
+
