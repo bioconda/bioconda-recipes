fd8569925db4bdaba2647e9713859d99ef2a7427
biocodz <biocodz@protonmail.com>
2020-03-14 18:29:58 -0400

    modified per reviewer advice

--- recipes/sortmerna/4.2.0/build.sh
+++ recipes/sortmerna/4.2.0/build.sh
@@ -1,9 +1,2 @@
-#
-# FILE: build.sh
-# Created: Dec 08, 2019 Sun
-#
-# standard Conda build script for building sortmerna v4.2.0
-# Uses pre-built binaries from Sortmerna GitHub Releases
-#
 [ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
 mv ./bin/sortmerna $PREFIX/bin
