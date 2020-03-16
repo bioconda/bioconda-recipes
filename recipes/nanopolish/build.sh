03c457a1900f60ef596d25da3323b30eaeb070ec
Jared Simpson <jared.simpson@gmail.com>
2020-03-16 10:28:08 -0400

    attempt self-contained build

--- recipes/nanopolish/build.sh
+++ recipes/nanopolish/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-export CFLAGS="-Iminimap2 -I$PREFIX/include -std=c99"
-export CPATH=${PREFIX}/include
+#export CFLAGS="-Iminimap2 -I$PREFIX/include -std=c99"
+#export CPATH=${PREFIX}/include
 
-export HTS_LIB=${PREFIX}/lib/libhts.a
-export HTS_INCLUDE=-I${PREFIX}/include
-export FAST5_INCLUDE=-I${PREFIX}/include/fast5
+#export FAST5_INCLUDE=-I${PREFIX}/include/fast5
 
 mkdir -p $PREFIX/bin
 
-make HDF5=noinstall EIGEN=noinstall HTS=noinstall LDFLAGS="$LDFLAGS -pthread -fopenmp"
+# Linker options aren't passed to minimap2
+pushd minimap2
+make CFLAGS="$CFLAGS" LIBS="-L$PREFIX/lib -lm -lz -pthread" libminimap2.a
+popd
+
+make
 cp nanopolish $PREFIX/bin
 cp scripts/nanopolish_makerange.py $PREFIX/bin
 cp scripts/nanopolish_merge.py $PREFIX/bin
