248e9542c801691a16af67c94b5d1ffe6d2b5fcd
tobiasrausch <rauschtobi@gmail.com>
2020-03-17 20:13:56 +0100

    alfred clang

--- recipes/alfred/build.sh
+++ recipes/alfred/build.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-if [ "$(uname)" == "Darwin" ]
-then
-    CXXFLAGS="$CXXFLAGS";
-    export CXXFLAGS="$CXXFLAGS -stdlib=libc++ -std=c++11 -I${PREFIX}/include"
-    export CXX_FLAGS="${CXX_FLAGS} -stdlib=libc++ -std=c++11 -I${PREFIX}/include"
-    export C_INCLUDE_PATH=${PREFIX}/include
-    export CPLUS_INCLUDE_PATH=${PREFIX}/include
-    export BOOST_ROOT=${PREFIX}
-    export CXX=clang++
-    export CC=clang
-fi
-
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib"
 mkdir -p $PREFIX/bin
 cp src/alfred $PREFIX/bin
