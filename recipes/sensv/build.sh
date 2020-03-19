eda40ac906ce2186f639b16285585d2abda929a6
chaklim <chaklim@gmail.com>
2020-03-20 06:59:03 +0800

    Add sensv

---
+++ recipes/sensv/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -pv $PREFIX/bin
+
+# download data files
+curl http://www.bio8.cs.hku.hk/sensv/data.tar.gz --output data.tar.gz
+tar -xf data.tar.gz
+
+cp -rv src modules data $PREFIX/bin
+cp Makefile sensv *.ini $PREFIX/bin/
+
+cd $PREFIX/bin/ && make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" && cd -
+
