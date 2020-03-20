f80c725ea98201b36a9fe3aa5bd157797689a7e2
Chienchi <chienchi@lanl.gov>
2020-03-20 15:05:46 -0600

    add cd to SRC_DIR

--- recipes/faqcs/build.sh
+++ recipes/faqcs/build.sh
@@ -4,7 +4,7 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 mkdir -p $PREFIX/bin
-
+cd $SRC_DIR
 if [ "$(uname)" == "Darwin" ]; then
     echo "Installing FaQCs for OSX."
     make OPENMP=""
