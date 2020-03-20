f6b63230566a38d23c550ed38825699458c22218
Yan Gao <yangao07@hit.edu.cn>
2020-03-20 01:40:02 -0400

    add TideHunter

---
+++ recipes/tidehunter/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+
+make INCLUDE="-I$BUILD_PREFIX/include" CFLAGS="-Wall -O3 -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -L$BUILD_PREFIX/lib"
+
+cp bin/TideHunter $PREFIX/bin
