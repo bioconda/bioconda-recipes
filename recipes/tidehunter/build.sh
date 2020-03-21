94a9664d42a9864f757013d4e81ab4db989aebf5
Devon Ryan <dpryan79@users.noreply.github.com>
2020-03-21 13:56:02 +0100

    Update build.sh

--- recipes/tidehunter/build.sh
+++ recipes/tidehunter/build.sh
@@ -2,6 +2,10 @@
 
 mkdir -p $PREFIX/bin
 
+pushd abPOA
+make INCLUDE="-I$PREFIX/include" CFLAGS="$CFLAGS -L$PREFIX/lib"
+popd
+
 make INCLUDE="-I$PREFIX/include" CFLAGS="-Wall -O3 -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -I$PREFIX/include -L$PREFIX/lib"
 
 cp bin/TideHunter $PREFIX/bin
