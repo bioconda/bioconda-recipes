c1c9c2a58caf7458009a1d00bd4a6ab57ead7734
chaklim <chaklim@gmail.com>
2020-03-21 22:30:37 +0800

    Update requested changes
    - meta.yaml update for download data files
    - removed `which sensv`
    - removed python, only pypy is needed

--- recipes/sensv/build.sh
+++ recipes/sensv/build.sh
@@ -2,10 +2,6 @@
 
 mkdir -pv $PREFIX/bin
 
-# download data files
-curl http://www.bio8.cs.hku.hk/sensv/data.tar.gz --output data.tar.gz
-tar -xf data.tar.gz
-
 cp -rv src modules data $PREFIX/bin
 cp Makefile sensv *.ini $PREFIX/bin/
 
