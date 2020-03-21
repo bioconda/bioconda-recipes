81991d649b2081f4eaf754990eef828069754142
andvides <bernardo.benavides@udea.edu.co>
2020-03-20 21:29:47 -0500

    Changes to be committed: 	modified:   clame/build.sh 	modified:   clame/meta.yaml

--- recipes/clame/build.sh
+++ recipes/clame/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "SDSL compilation"
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
@@ -8,21 +8,28 @@ cd sdsl-lite
 ./install.sh
 cd ..
 
+echo "Binning compilation"
 cd binning
-make
+make CPP=${CXX}
 mkdir -p $PREFIX/bin
 cp binning $PREFIX/bin
 cd ..
 
+echo "genFm9 compilation"
 cd genFm9
-make
+make CPP=${CXX}
 cp genFm9 $PREFIX/bin
 cd ..
 
+echo "mapping compilation"
 cd mapping
-make
+make CPP=${CXX}
 cp mapping $PREFIX/bin
 cd ..
 
-make
+echo "clame compilation"
+make CPP=${CXX}
 cp clame $PREFIX/bin
+
+echo "Installation successful"
+
