4ae9b46b77c5fd8d28bda1cb0f7a37324fdc4ec4
Yan Gao <yangao07@hit.edu.cn>
2020-03-21 09:54:29 -0400

    update TideHunter

--- recipes/tidehunter/build.sh
+++ recipes/tidehunter/build.sh
@@ -3,7 +3,8 @@
 mkdir -p $PREFIX/bin
 
 pushd abPOA
-make INCLUDE="-I$PREFIX/include" CFLAGS="$CFLAGS -L$PREFIX/lib"
+make simd_check INCLUDE="-I$PREFIX/include" CFLAGS="$CFLAGS -L$PREFIX/lib"
+make libabpoa INCLUDE="-I$PREFIX/include" CFLAGS="$CFLAGS -L$PREFIX/lib"
 popd
 
 make INCLUDE="-I$PREFIX/include" CFLAGS="-Wall -O3 -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -I$PREFIX/include -L$PREFIX/lib"
