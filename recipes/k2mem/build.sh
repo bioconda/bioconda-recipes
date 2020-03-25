81a3d1213b5775660e8c9e6855c6724a6ba2153f
davide92 <thechryst92@gmail.com>
2020-03-25 11:58:19 +0100

    Added Makefile file

--- recipes/k2mem/build.sh
+++ recipes/k2mem/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
@@ -7,7 +8,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
 
 chmod u+x install_k2mem.sh
-./install_k2mem.sh "$PREFIX/libexec"
+./install_kraken2.sh "$PREFIX/libexec"
 for bin in k2mem k2mem-build k2mem-inspect; do
     chmod +x "$PREFIX/libexec/$bin"
     ln -s "$PREFIX/libexec/$bin" "$PREFIX/bin/$bin"
