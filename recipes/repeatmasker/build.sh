579c82eb0dd209f58f8786a284232d2d717aa864
Jacques Dainat <jacques.dainat@nbis.se>
2020-03-27 12:28:39 +0100

    try to repeatmasker 4.1.0 with simplification

--- recipes/repeatmasker/build.sh
+++ recipes/repeatmasker/build.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 perl ./configure -trf_prgm ${PREFIX}/bin/trf  -rmblast_dir ${PREFIX}/bin/ -hmmer_dir ${PREFIX}/bin -abblast_dir ${PREFIX}/bin -crossmatch_dir ${PREFIX}/bin
+
+RM_DIR=${PREFIX}/share/RepeatMasker
+mkdir -p ${PREFIX}/bin
+mkdir -p ${RM_DIR}
+cp -r * ${RM_DIR}
+
+
+# ----- add tools within the bin ------
+
+# add RepeatMasker
+ln -s ${RM_DIR}/RepeatMasker
+
+# add other tools
+RM_OTHER_PROGRAMS="DateRepeats DupMasker ProcessRepeats RepeatProteinMask"
+for name in ${RM_OTHER_PROGRAMS} ; do
+  ln -s ${RM_DIR}/${name}
+done
+
+# add all utils
+for name in ${RM_DIR}/util/* ; do
+  ln -s $name
+done
