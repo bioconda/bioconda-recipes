a048b4a5a19206670743583e1fb75adb6ac6a531
Michael L Heuer <heuermh@acm.org>
2020-03-30 13:05:04 -0500

    seqwish 0.2.1

---
+++ recipes/seqwish/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH=${PREFIX}/include
+cmake -H. -Bbuild
+cmake --build build
+mkdir -p $PREFIX/bin
+mv bin/* $PREFIX/bin
+mv lib/* $PREFIX/lib
