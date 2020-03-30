fc903b6b4e2f8d7db55f53695e53b31b9f96e414
roryk <rory.kirchner@gmail.com>
2020-03-30 08:08:15 +0000

    Use @dpryan79s changes to the p/r.

--- recipes/sailfish/build.sh
+++ recipes/sailfish/build.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON -DBoost_USE_STATIC_LIBS=OFFi ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON -DBoost_USE_STATIC_LIBS=OFF ..
 make install
