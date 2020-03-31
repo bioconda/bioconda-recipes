16765ef84cc3a093f1a3a93d3ad69a0bcd2cca04
pedroscampoy <pedroscampoy@gmail.com>
2020-03-31 18:38:02 +0200

    mash 2.2 and circos normal installing

--- recipes/plasmidid/build.sh
+++ recipes/plasmidid/build.sh
@@ -7,19 +7,3 @@ cp bin/* $PREFIX/bin
 cp plasmidID $PREFIX/bin
 
 cp config_files/* $PREFIX/bin/config_files
-
-echo "INSTALING MASH 2"
-echo "#############################################"
-
-wget https://github.com/marbl/Mash/releases/download/v2.2/mash-Linux64-v2.2.tar
-tar -xvf mash-Linux64-v2.2.tar
-cp mash-Linux64-v2.2/* $PREFIX/bin
-
-echo "INSTALING CIRCOS WITH CUSTOM SETTINGS"
-echo "#############################################"
-
-wget http://circos.ca/distribution/circos-0.69-8.tgz
-tar -zxvf circos-0.69-8.tgz
-cd circos-0.69-8
-sed -i "s/max_points_per_track =.*/max_points_per_track = 80000000/g" etc/housekeeping.conf
-cp -r . $PREFIX
