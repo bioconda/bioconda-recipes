0559ea915b0d8b46597b009ace6a0a873a869e62
Michael L Heuer <heuermh@acm.org>
2020-03-31 13:51:02 -0500

    remove LD_LIBRARY_PATH

--- recipes/seqwish/build.sh
+++ recipes/seqwish/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=${PREFIX}/lib
 export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
 cmake -H. -Bbuild
 cmake --build build
