e05dbf99687a15a1dfd5634e9a6567230da446d1
Vini Salazar <viniws@gmail.com>
2020-03-30 23:16:37 -0300

    Copy scripts to env bin

--- recipes/metabolisHMM/build.sh
+++ recipes/metabolisHMM/build.sh
@@ -3,7 +3,10 @@
 # Decompress the HMM database
 tar zxvf metabolisHMM_markers_v1.9.tgz
 
-# install python libraries
+# Install python libraries
 python3 -m pip install .
 
+# Copy scripts to environment bin/
+chmod +x bin/*
+cp bin/* ${PREFIX}/bin/
 
