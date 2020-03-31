c5732586039aae68adaf6b0ed999201b45f8d604
pedroscampoy <pedroscampoy@gmail.com>
2020-03-31 11:41:58 +0200

    mash version reproducibility

--- recipes/plasmidid/build.sh
+++ recipes/plasmidid/build.sh
@@ -11,20 +11,15 @@ cp config_files/* $PREFIX/bin/config_files
 echo "INSTALING MASH 2"
 echo "#############################################"
 
-git clone https://github.com/marbl/Mash.git
-
-cd Mash
-
-./bootstrap.sh
-./configure --with-capnp=$PREFIX --with-gsl=$PREFIX --prefix=$PREFIX
-make
-make install
+wget https://github.com/marbl/Mash/releases/download/v2.2/mash-Linux64-v2.2.tar
+tar -xvf mash-Linux64-v2.2.tar
+cp mash-Linux64-v2.2/* $PREFIX/bin
 
 echo "INSTALING CIRCOS WITH CUSTOM SETTINGS"
 echo "#############################################"
 
 wget http://circos.ca/distribution/circos-0.69-8.tgz
-tar zxvf circos-0.69-8.tgz
+tar -zxvf circos-0.69-8.tgz
 cd circos-0.69-8
 sed -i "s/max_points_per_track =.*/max_points_per_track = 80000000/g" etc/housekeeping.conf
 cp -r . $PREFIX
