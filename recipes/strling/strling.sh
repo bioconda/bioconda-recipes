720fd4b4bad816e568252c21dbab36330d337447
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-03-31 21:30:57 +0000

    patch for non conda systems

--- recipes/strling/strling.sh
+++ recipes/strling/strling.sh
@@ -1,2 +1,6 @@
 #bin/bash
+echo $CONDA_PREFIX
+if [ -z $CONDA_PREFIX ]; then
+  CONDA_PREFIX=$(dirname $(dirname "$(readlink -f "$0")"))
+fi
 LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH $CONDA_PREFIX/share/strling $@
