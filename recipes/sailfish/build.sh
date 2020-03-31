a3acb7c33d7dfe6ca999a8c47ed256825d0f1d57
roryk <rory.kirchner@gmail.com>
2020-03-31 01:27:25 +0000

    Make sure we can find g++.

--- recipes/sailfish/build.sh
+++ recipes/sailfish/build.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_NO_SYSTEM_PATHS=ON -DBoost_DEBUG=ON -DBoost_USE_STATIC_LIBS=OFF ..
-make install
+make CXX=${CXX} install
