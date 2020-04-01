d09a6b0caed93a7be7578118e3b42c295b4b8fe2
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-04-01 08:40:11 +0000

    even less

--- recipes/strling/build.sh
+++ recipes/strling/build.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-# Compile nim
-
 cp "${CC}" "${BUILD_PREFIX}/bin/gcc"
-
-mkdir -p $PREFIX/share
-mkdir -p $PREFIX/bin
-cp "$RECIPE_DIR/strling.sh" "$PREFIX/share/strling.sh"
-ln -s "$PREFIX/share/strling.sh" "$PREFIX/bin/strling"
-chmod +x "$PREFIX/bin/strling"
-
 nimble install -y --verbose
 chmod a+x strling
-cp strling $PREFIX/share/strling
+cp strling $PREFIX/bin/strling
--- recipes/strling/strling.sh
+++
@@ -1,6 +0,0 @@
-#bin/bash
-echo $CONDA_PREFIX
-if [ -z $CONDA_PREFIX ]; then
-  CONDA_PREFIX=$(dirname $(dirname "$(readlink -f "$0")"))
-fi
-LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH $CONDA_PREFIX/share/strling $@
