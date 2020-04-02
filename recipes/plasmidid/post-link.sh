9772e3b2984eacc73a50f606f35dcce22e030fba
pedroscampoy <pedroscampoy@gmail.com>
2020-04-02 16:39:26 +0200

    Adapt to bioconda

--- recipes/plasmidid/post-link.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Modifying max_points_per_track in circos housekeeping.conf"
-echo "##############################################################################################################################"
-
-sed -i "s/max_points_per_track =.*/max_points_per_track = 70000000/g" $PREFIX/etc/housekeeping.conf
