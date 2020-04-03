f03b2926241affccf4de99ae58112882343b0db2
John Lees <lees.john6@gmail.com>
2020-04-03 11:25:11 +0100

    Update with comments from @GuillaumeHolley

--- recipes/bifrost/build.sh
+++ recipes/bifrost/build.sh
@@ -2,7 +2,15 @@
 
 mkdir build
 pushd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+# Cmake is having trouble finding the sysroot with conda so we're giving it a little help...
+if [[ ${HOST} =~ .*darwin.* ]]; then
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}" ..
+else
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" ..
+fi
+
 make VERBOSE=1
 make install
+
 popd
