f7f4dea04670ecd2d8b16c14960a5f6d7db8f963
tobiasrausch <rauschtobi@gmail.com>
2020-04-05 21:30:27 +0200

    alfred osx

--- recipes/alfred/build.sh
+++ recipes/alfred/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
 export CPPFLAGS="${CPPFLAGS} -std=c++11"
 export CFLAGS="$CFLAGS -std=c++11"
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/ -std=c++11"
