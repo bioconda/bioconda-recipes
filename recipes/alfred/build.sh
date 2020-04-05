db17efdea27a59dcded48e886ae198219716c0f2
tobiasrausch <rauschtobi@gmail.com>
2020-04-05 20:29:32 +0200

    alfred osx

--- recipes/alfred/build.sh
+++ recipes/alfred/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib"
+make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/"
 mkdir -p $PREFIX/bin
 cp src/alfred $PREFIX/bin
