2eae979bbfd5719e6a092f7ead9df290815ca5ad
Peter van Heusden <pvh@sanbi.ac.za>
2020-04-06 12:56:27 +0200

    Update to v. 1.2.5 sourced from Github

--- recipes/clusterpicker/build.sh
+++ recipes/clusterpicker/build.sh
@@ -5,7 +5,7 @@ mkdir -p $outdir
 ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
 mkdir -p $PREFIX/bin
 cp -R ./* $outdir/
-mv $outdir/ClusterPicker_$PKG_VERSION.jar $outdir/cluster-picker.jar
+mv release/ClusterPicker_1.2.5.jar $outdir/cluster-picker.jar
 cp $RECIPE_DIR/cluster-picker.sh $outdir/cluster-picker
 chmod +x $outdir/cluster-picker
 
