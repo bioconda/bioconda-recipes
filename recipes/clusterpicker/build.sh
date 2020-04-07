ef077e5e758a607ad0776b7b1fa5a075452adef6
Simon Bray <32272674+simonbray@users.noreply.github.com>
2020-04-07 18:50:28 +0200

    Update build.sh

--- recipes/clusterpicker/build.sh
+++ recipes/clusterpicker/build.sh
@@ -5,7 +5,7 @@ mkdir -p $outdir
 ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
 mkdir -p $PREFIX/bin
 cp -R ./* $outdir/
-mv release/ClusterPicker_1.2.5.jar $outdir/cluster-picker.jar
+mv release/ClusterPicker_$PKG_VERSION.jar $outdir/cluster-picker.jar
 # create both cluster-picker and ClusterPicker wrapper scripts to follow bioconda
 # historical name and Brew convention
 cp $RECIPE_DIR/cluster-picker.sh $outdir/cluster-picker
