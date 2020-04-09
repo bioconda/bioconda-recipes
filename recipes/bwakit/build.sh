6c925fc408b2e8854baa26f1d2d94ae8b29db041
Stephen Kazakoff <sh.kazakoff@gmail.com>
2020-04-09 15:15:28 +0000

    De-vendor bwakit binaries

--- recipes/bwakit/build.sh
+++ recipes/bwakit/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
+
 mkdir -p $PREFIX/bin
 
-cp -r * $outdir
-ln -s $outdir/run-bwamem $PREFIX/bin
+curl -L https://github.com/attractivechaos/k8/releases/download/0.2.5/k8-0.2.5.tar.bz2 |
+  tar jxf - k8-0.2.5/k8-$(uname -s) && cp k8-0.2.5/k8-* $PREFIX/bin/k8
+
+sed -i.bak '1i #!/usr/bin/env k8' *.js
+chmod 0755 *.js
+
+cp -r \
+  resource-* \
+  run-* \
+  *.js \
+  *.sh \
+  $PREFIX/bin
