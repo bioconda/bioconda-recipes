ed14d1dc837381da1c59fd79eb397bb0e5bc3ef2
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-09 14:01:01 +0200

    add cath-tools

--- recipes/cath-tools/build.sh
+++ recipes/cath-tools/build.sh
@@ -10,10 +10,7 @@ export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
 #compile
-#conda install -p $PREFIX libgfortran=3.0 -y
-# backup to "gfortran" in conda GFORTRAN is not set
-#cmake CXX=${CXX} -DCMAKE_CXX_FLAGS="-stdlib=libc++"
-cmake -DGSL_LIBRARIES=${PREFIX}/include -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBOOST_ROOT=$PREFIX -DBOOST_LIBRARYDIR=$PREFIX/lib -DSERIALIZE="Boost" -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_PREFIX_PATH="$PREFIX"
+cmake -DGSL_LIBRARIES=${PREFIX}/include/gsl -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBOOST_ROOT=$PREFIX -DBOOST_LIBRARYDIR=$PREFIX/lib -DSERIALIZE="Boost" -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_PREFIX_PATH="$PREFIX"
 # copy tools in the bin
 #mkdir -p ${PREFIX}/bin
 #for PROGRAM in ${PFTOOLS_PROGRAMS} ; do
