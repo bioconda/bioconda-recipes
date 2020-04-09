bf7edf578a1596ce7d7e4e315f5bc6fe06944b78
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-09 16:16:24 +0200

    add coils

--- recipes/coils/build.sh
+++ recipes/coils/build.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 set -x -e
 
+#list programs
+COILS_PROGRAMS="coils-svr.pl coils-wrap.pl"
+# remove the precompilated one
+rm ncoils-linux
 #compile
-COILS_PROGRAMS="ncoils  coils-svr.pl coils-wrap.pl"
 ${CC} -O2 -I. -o ncoils-osf ncoils.c read_matrix.c -lm
 
+# get the name of the ncoils program build
+NCOILS=$(ls ncoils-*)
+
+# info for debugging
 ls -l
 
 # copy tools in the bin
 mkdir -p ${PREFIX}/bin
+
+# Add main tools
+cp ${NCOILS} ${PREFIX}/bin/ncoils
+
+# Add extra programs
 for PROGRAM in ${COILS_PROGRAMS} ; do
   cp ${PROGRAM} ${PREFIX}/bin
 done
