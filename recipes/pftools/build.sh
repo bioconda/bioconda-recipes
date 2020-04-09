aad4d44a455e3a2290d0b1784cf646ce16a18532
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-09 09:09:53 +0200

    add pftools

--- recipes/pftools/build.sh
+++ recipes/pftools/build.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -x -e
 
-export CFLAGS="-I$PREFIX/include"
-export CPPFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
 #compile
 #conda install -p $PREFIX libgfortran=3.0 -y
 # backup to "gfortran" in conda GFORTRAN is not set
