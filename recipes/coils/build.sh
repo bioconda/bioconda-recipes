53e737ff10316bfc6431838680c86735c458d625
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-09 16:51:06 +0200

    add coils

--- recipes/coils/build.sh
+++ recipes/coils/build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -x -e
 
+# Coils Dir
+COILS_DIR=${PREFIX}/share/coils
 #list programs
 COILS_PROGRAMS="coils-svr.pl coils-wrap.pl"
+
+
+
+
 # remove the precompilated one
 rm ncoils-linux
 #compile
@@ -16,6 +22,8 @@ ls -l
 
 # copy tools in the bin
 mkdir -p ${PREFIX}/bin
+mkdir -p ${COILS_DIR}
+cp -r * ${COILS_DIR}
 
 # Add main tools
 cp ${NCOILS} ${PREFIX}/bin/ncoils
@@ -24,3 +32,15 @@ cp ${NCOILS} ${PREFIX}/bin/ncoils
 for PROGRAM in ${COILS_PROGRAMS} ; do
   cp ${PROGRAM} ${PREFIX}/bin
 done
+
+# You then need to set an environment variable $COILSDIR to the coils folder
+# These coils folder is installed as runtime deps, but the paths to it need to
+# to reflect the current environment's $PREFIX/bin dir.
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat <<EOF >> ${PREFIX}/etc/conda/activate.d/coils.sh
+export COILSDIR=${COILS_DIR}
+EOF
+
+cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/coils.sh
+unset COILSDIR
+EOF
