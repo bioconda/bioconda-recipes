0c643b94ca4a34c772add486cb1462a40bc87089
Stephen Kazakoff <sh.kazakoff@gmail.com>
2020-04-11 09:57:22 +0000

    Make sed command POSIX compatible

--- recipes/bwakit/build.sh
+++ recipes/bwakit/build.sh
@@ -5,7 +5,8 @@ mkdir -p $PREFIX/bin
 curl -L https://github.com/attractivechaos/k8/releases/download/0.2.5/k8-0.2.5.tar.bz2 |
   tar jxf - k8-0.2.5/k8-$(uname -s) && cp k8-0.2.5/k8-* $PREFIX/bin/k8
 
-sed -i.bak '1i #!/usr/bin/env k8' *.js
+sed -i.bak '1i\
+#!/usr/bin/env k8' *.js
 chmod 0755 *.js
 
 cp -r \
