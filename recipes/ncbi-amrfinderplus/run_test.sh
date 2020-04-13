8c1db9442d048910baccb7c4bec9a3f6c289bfd4
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-04-13 12:28:35 -0400

    More debugging changes

--- recipes/ncbi-amrfinderplus/run_test.sh
+++ recipes/ncbi-amrfinderplus/run_test.sh
@@ -6,9 +6,9 @@ set -e
          -O https://raw.githubusercontent.com/ncbi/amr/master/test_prot.gff \
          -O https://raw.githubusercontent.com/ncbi/amr/master/test_both.expected
          
-    echo $PREFIX
+    echo "ls -l $PREFIX"
     ls -l $PREFIX
-    find $PREFIX
+    cat /proc/cpuinfo
     which amrfinder
     amrfinder -u
     amrfinder --plus -n test_dna.fa -p test_prot.fa -g test_prot.gff -O Escherichia > test_both.got
