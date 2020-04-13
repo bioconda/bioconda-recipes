59ce39371c63ce4488fde791646a2410ba17ca20
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-04-13 12:12:26 -0400

    Add debugging commands to run_test.sh

--- recipes/ncbi-amrfinderplus/run_test.sh
+++ recipes/ncbi-amrfinderplus/run_test.sh
@@ -5,6 +5,11 @@ set -e
          -O https://raw.githubusercontent.com/ncbi/amr/master/test_prot.fa \
          -O https://raw.githubusercontent.com/ncbi/amr/master/test_prot.gff \
          -O https://raw.githubusercontent.com/ncbi/amr/master/test_both.expected
+         
+    echo $PREFIX
+    ls -l $PREFIX
+    find $PREFIX
+    which amrfinder
     amrfinder -u
     amrfinder --plus -n test_dna.fa -p test_prot.fa -g test_prot.gff -O Escherichia > test_both.got
     diff test_both.expected test_both.got
