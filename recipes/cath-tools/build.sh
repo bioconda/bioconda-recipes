63b6a3e8d255b969843d045f5e0243f6bfe9d854
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-13 16:36:56 +0200

    built tools in /bin

--- recipes/cath-tools/build.sh
+++ recipes/cath-tools/build.sh
@@ -9,21 +9,15 @@ export CPATH=${PREFIX}/include
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-#list programs
-CATH_PROGRAMS=""
+mkdir -p ${PREFIX}/bin
 
 #compile
-cmake -DGSL_LIBRARIES=${PREFIX}/include -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBoost_DEBUG=ON -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_PREFIX_PATH="$PREFIX" .
-# for debugging
-ls -l
-
+cmake -DGSL_LIBRARIES=${PREFIX}/include -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBoost_DEBUG=ON -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_PREFIX_PATH="$PREFIX" ${PREFIX}/bin
 make
+
 # for debugging
 ls -l
+ls -l $PREFIX/bin
 
-# copy tools in the bin
-mkdir -p ${PREFIX}/bin
-for PROGRAM in ${PFTOOLS_PROGRAMS} ; do
-  cp ${PROGRAM} ${PREFIX}/bin
-	chmod a+x ${PREFIX}/bin/${PROGRAM}
-done
+#make everything executable in case
+chmod +x $PREFIX/bin/*
