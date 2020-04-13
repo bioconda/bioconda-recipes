d72fdb08020bbef701070554831031a5fd7394ef
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-13 16:55:56 +0200

    put built tools in /bin

--- recipes/cath-tools/build.sh
+++ recipes/cath-tools/build.sh
@@ -9,15 +9,19 @@ export CPATH=${PREFIX}/include
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-mkdir -p ${PREFIX}/bin
+#list programs
+CATH_PROGRAMS="build-test cath-assign-domains cath-cluster cath-map-clusters cath-refine-align cath-resolve-hits cath-score-align cath-ssap cath-superpose"
 
 #compile
-cmake -DGSL_LIBRARIES=${PREFIX}/include -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBoost_DEBUG=ON -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_PREFIX_PATH="$PREFIX" ${PREFIX}/bin
+cmake -DGSL_LIBRARIES=${PREFIX}/include -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBoost_DEBUG=ON -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_PREFIX_PATH="$PREFIX" .
 make
 
-# for debugging
-ls -l
-ls -l $PREFIX/bin
+# copy tools in the bin
+mkdir -p ${PREFIX}/bin
+for PROGRAM in ${PFTOOLS_PROGRAMS} ; do
+  cp ${PROGRAM} ${PREFIX}/bin
+	chmod a+x ${PREFIX}/bin/${PROGRAM}
+done
 
 #make everything executable in case
 chmod +x $PREFIX/bin/*
