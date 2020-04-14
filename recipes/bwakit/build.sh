153d1b8f6135f7e925c56463b19dbaaf61f6ac4a
Stephen Kazakoff <sh.kazakoff@gmail.com>
2020-04-14 21:40:42 +1000

    Ensure javascripts get a k8 shebang

--- recipes/bwakit/build.sh
+++ recipes/bwakit/build.sh
@@ -5,9 +5,10 @@ mkdir -p $PREFIX/bin
 curl -L https://github.com/attractivechaos/k8/releases/download/0.2.5/k8-0.2.5.tar.bz2 |
   tar jxf - k8-0.2.5/k8-$(uname -s) && cp k8-0.2.5/k8-* $PREFIX/bin/k8
 
-sed -i.bak '1i\
-#!/usr/bin/env k8' *.js
-chmod 0755 *.js
+for file in *.js ; do
+  { echo '#!/usr/bin/env k8'; cat "${file}"; } > "${file}.tmp"
+  mv "${file}.tmp" "${file}" && chmod 0755 "${file}"
+done
 
 cp -r \
   resource-* \
