9c03ebc2ef154192480eee92b52a8e1bce88c07a
Yan Gao <yangao07@hit.edu.cn>
2020-04-14 09:04:29 -0400

    Add pyabpoa

--- recipes/pyabpoa/build.sh
+++ recipes/pyabpoa/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd python
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
