7d39aaea3d7b84b829c0fa842198f5b335e52f4b
RomainFeron <romainferon91@gmail.com>
2020-04-15 12:28:22 +0200

    First build attempt

---
+++ recipes/r-sgtr/build.sh
@@ -0,0 +1,7 @@
+if [[ $(uname) == "Darwin" ]]; then
+    export LDFLAGS=-L${PREFIX}/lib
+    autoreconf -i
+    $R CMD INSTALL --build . --configure-args="CFLAGS=-ferror-limit=0 CXXFLAGS=-ferror-limit=0"
+else
+    $R CMD INSTALL --build .
+fi
