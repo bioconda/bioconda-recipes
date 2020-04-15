a4355f1011f18968c16a7eaf13f532fbb63b0cb5
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-15 21:30:11 +0200

    change flags add cmake

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -15,7 +15,7 @@ if test x"`uname`" = x"Linux"; then
 fi
 
 if [ `uname` == Darwin ]; then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath -lz -lbz2 -flat_namespace -headerpad_max_install_names"
+    export LDFLAGS="${LDFLAGS} -Wl,-as-needed,-rpath -lz -lbz2 -flat_namespace -headerpad_max_install_names"
 else
     export CPP_FOR_BUILD=$CPP
 fi
@@ -32,7 +32,7 @@ LIB_INSTALL_DIR=$PREFIX/lib/ncbi-blast+
 
 # for degug
 echo "CFLAGS= $CFLAGS"
-echo "CXXFLAGS= $CXXFLAGS"
+echo "CXXFLAGS= $CXXFLAGS -fpermissive"
 echo "CPPFLAGS= $CPPFLAGS"
 echo "LDFLAGS= $LDFLAGS"
 echo "CC_FOR_BUILD= $CC_FOR_BUILD"
