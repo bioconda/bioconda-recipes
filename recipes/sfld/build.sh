626308b959cab00796eb731a61ec2bf689aba88d
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-16 23:29:27 +0200

    fix sed command

--- recipes/sfld/build.sh
+++ recipes/sfld/build.sh
@@ -4,13 +4,7 @@ set -x -e
 PROGRAMS="sfld_preprocess sfld_postprocess sfld_preprocess.py"
 
 # Replace path in EASEL_DIR in Makefile with the 'easel' subdir of the hmmer distribution
-# Hack J. Dainat - fix path to access the tools through the wrapper in the bin otherwise fails
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i'' "s|EASEL_DIR=|EASEL_DIR=${PREFIX}/share/easel|g" Makefile
-else
-	sed -i "s|EASEL_DIR=|EASEL_DIR=${PREFIX}/share/easel|g" Makefile
-fi
-
+sed -i'' -e "s|EASEL_DIR=|EASEL_DIR=${PREFIX}/share/easel|g" Makefile
 
 # for debug
 cat Makefile
