fc2cae863e34c64e571eaa3c81450192072bbbf1
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-17 21:06:05 +0200

    use hmmr3

--- recipes/sfld/build.sh
+++ recipes/sfld/build.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -x -e
 
+
+
 PROGRAMS="sfld_preprocess sfld_postprocess sfld_preprocess.py"
 
-# get easel lib from version 3.1b2 of hmmer
-curl -sL http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz | tar -xz
-ls -l
-ls -l ${SRC_DIR}
 # Replace path in EASEL_DIR in Makefile with the 'easel' subdir of the hmmer distribution
-sed -i.bak 's|EASEL_DIR=|EASEL_DIR=${SRC_DIR}/hmmer-3.1b2/easel|g' Makefile
+sed -i.bak 's|EASEL_DIR=|EASEL_DIR=${SRC_DIR}/hmmer-3.3/easel|g' Makefile
 
 #compile
 make CC=${CC}
