9d2ebb048ea62b1fb034b3f7552f97d51421bc36
RomainFeron <romainferon91@gmail.com>
2020-04-17 11:54:53 +0200

    Tried removing macOS-specific instructions in build script

--- recipes/r-sgtr/build.sh
+++ recipes/r-sgtr/build.sh
@@ -1,7 +1,1 @@
-if [[ $(uname) == "Darwin" ]]; then
-    export LDFLAGS=-L${PREFIX}/lib
-    autoreconf -i
-    $R CMD INSTALL --build . --configure-args="CFLAGS=-ferror-limit=0 CXXFLAGS=-ferror-limit=0"
-else
-    $R CMD INSTALL --build .
-fi
+$R CMD INSTALL --build .
