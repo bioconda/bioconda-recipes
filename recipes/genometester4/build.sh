505dae8980354dbc8a16f3046c6a267b0153cd42
Jiangwei Yao <yao.jiangwei@gmail.com>
2020-04-17 12:45:39 -0400

    GenomeTester4 7

--- recipes/genometester4/build.sh
+++ recipes/genometester4/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
 cd src
-sed -i.bak 's/-lrt//' Makefile
+#sed -i.bak 's/-lrt//' Makefile
 sed -i.bak 's/CXX  = gcc//' Makefile
 
+make clean
 make CXX=$CXX
 make gmer_counter
 make gmer_caller
