d53df75688b4f592be2cabfd3703b0fd990d5f28
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-17 13:45:03 +0200

    fix acces easel

--- recipes/sfld/build.sh
+++ recipes/sfld/build.sh
@@ -5,9 +5,10 @@ PROGRAMS="sfld_preprocess sfld_postprocess sfld_preprocess.py"
 
 # get easel lib from version 3.1b2 of hmmer
 curl -sL http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz | tar -xz
-ls -l ${PREFIX}/hmmer-3.1b2
+ls -l
+ls -l ${SRC_DIR}
 # Replace path in EASEL_DIR in Makefile with the 'easel' subdir of the hmmer distribution
-sed -i.bak 's|EASEL_DIR=|EASEL_DIR=${PREFIX}/hmmer-3.1b2/easel|g' Makefile
+sed -i.bak 's|EASEL_DIR=|EASEL_DIR=${SRC_DIR}/hmmer-3.1b2/easel|g' Makefile
 
 #compile
 make CC=${CC}
