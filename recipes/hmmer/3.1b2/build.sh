5d2728d21f193de7d437f0e802006f87b90a575b
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-17 16:56:16 +0200

    new try

--- recipes/hmmer/3.1b2/build.sh
+++ recipes/hmmer/3.1b2/build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-
 set -e -u -x
 
-# Fix broken configure option
-sed -i.bak -e 's/acx_maxopt_portable=$withval/acx_maxopt_portable=$enableval/' configure
+./configure --prefix=$PREFIX
 
-./configure --enable-portable-binary  --prefix=$PREFIX
 make -j4
 make install
 (cd "${SRC_DIR}/easel" && make install PREFIX=$PREFIX)
