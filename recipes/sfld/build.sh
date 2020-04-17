6e1ef5fe4439d1d5c7e6eff10ba98609fe3aecd2
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-17 11:10:04 +0200

    fix sed command

---
+++ recipes/sfld/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -x -e
+
+PROGRAMS="sfld_preprocess sfld_postprocess sfld_preprocess.py"
+
+# Replace path in EASEL_DIR in Makefile with the 'easel' subdir of the hmmer distribution
+sed -i.bak 's|EASEL_DIR=|EASEL_DIR=${PREFIX}/share/easel|g' Makefile
+
+# for debug
+cat Makefile
+
+ls -l ${PREFIX}
+ls -l ${PREFIX}/share
+ls -l ${PREFIX}/share/easel
+
+#compile
+make CC=${CC}
+
+# copy tools in the bin
+mkdir -p ${PREFIX}/bin
+for PROGRAM in ${PROGRAMS} ; do
+  cp ${PROGRAM} ${PREFIX}/bin
+	chmod a+x ${PREFIX}/bin/${PROGRAM}
+done
