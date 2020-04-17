6167c28ba6e9c08f8e406234ec2bd44e4550c969
Jacques Dainat <jacques.dainat@nbis.se>
2020-04-17 21:11:01 +0200

    fix path

--- recipes/sfld/build.sh
+++ recipes/sfld/build.sh
@@ -8,6 +8,8 @@ PROGRAMS="sfld_preprocess sfld_postprocess sfld_preprocess.py"
 # Replace path in EASEL_DIR in Makefile with the 'easel' subdir of the hmmer distribution
 sed -i.bak 's|EASEL_DIR=|EASEL_DIR=${SRC_DIR}/hmmer-3.3/easel|g' Makefile
 
+ls ${SRC_DIR}
+
 #compile
 make CC=${CC}
 
