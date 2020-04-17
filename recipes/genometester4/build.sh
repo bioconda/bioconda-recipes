7cbb4c6c698b55955b007665e4b7ddbd426d9d23
Jiangwei Yao <yao.jiangwei@gmail.com>
2020-04-17 13:26:33 -0400

    gcc v8

--- recipes/genometester4/build.sh
+++ recipes/genometester4/build.sh
@@ -6,7 +6,7 @@ sed -i.bak 's/CXX  = gcc//' Makefile
 
 make clean
 make CXX=$CC
-make gmer_counter
-make gmer_caller
+make CXX=$CC gmer_counter
+make CXX=$CC gmer_caller
 
 cp glistcompare glistmaker glistquery gmer_counter gmer_caller $PREFIX/bin
