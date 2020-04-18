50bcf87a67520557c5fb3961128a2b022afaeefe
tobiasrausch <rauschtobi@gmail.com>
2020-04-18 13:13:49 +0200

    mac osx

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -std=c++14"
+export CPPFLAGS="${CPPFLAGS} -std=c++14"
+export CFLAGS="$CFLAGS -std=c++14"
+make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib -std=c++14"
 mkdir -p $PREFIX/bin
 cp src/tracy $PREFIX/bin
