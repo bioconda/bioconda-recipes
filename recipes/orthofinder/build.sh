da424facfaef14434cd7c49b2f22f924903e4986
Antonio Camargo <antoniop.camargo@gmail.com>
2020-04-18 21:37:45 -0300

    Add tools to path

--- recipes/orthofinder/build.sh
+++ recipes/orthofinder/build.sh
@@ -9,6 +9,8 @@ mkdir $PREFIX/bin/scripts_of
 cp -r scripts_of/*py $PREFIX/bin/scripts_of/
 cp scripts_of/config.json $PREFIX/bin/scripts_of/config.json
 
-cp -r tools $PREFIX/bin/
+cp tools/convert_orthofinder_tree_ids.py $PREFIX/bin/
+cp tools/make_ultrametric.py $PREFIX/bin/
+cp tools/primary_transcript.py $PREFIX/bin/
 
 chmod a+x $PREFIX/bin/orthofinder
