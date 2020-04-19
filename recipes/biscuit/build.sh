ab5bfba19eda2d70285e961e6ddda95357ae4c70
Devon Ryan <dpryan79@users.noreply.github.com>
2020-04-19 22:26:21 +0200

    Update build.sh

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -2,6 +2,6 @@
 
 BIN=$PREFIX/bin
 mkdir -p $BIN
-sed -i.bak 's/CPPFLAGS = /CPPFLAGS = $(CXXFLAGS) /' Makefile
-make CPP=$CXX
+sed -i.bak 's/CC =/CC ?=/;s/CFLAGS =/CFLAGS ?=/' Makefile
+make CC=$CC CFLAGS="$CFLAGS"
 cp biscuit $BIN
