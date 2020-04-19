d8a5ebb9a6319a0a1275dcd9b28b898f8fc360a4
smortezah <s_morteza_h@yahoo.com>
2020-04-19 16:32:07 +0200

    update cryfa

--- recipes/cryfa/build.sh
+++ recipes/cryfa/build.sh
@@ -3,8 +3,12 @@ export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 #export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
-cmake .
-make
-
-mkdir -p $PREFIX/bin
-cp cryfa $PREFIX/bin/
+mkdir -p $PREFIX/build
+cd $PREFIX/build
+rm CMakeCache.txt
+cmake ..
+make -j4
+mkdir -p ${PREFIX}/bin
+cp cryfa ${PREFIX}/bin
+cp keygen ${PREFIX}/bin
+cd ${PREFIX}
