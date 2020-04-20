b531504cd40593df0f314e2e525aaaa8f1cb8d25
tobiasrausch <rauschtobi@gmail.com>
2020-04-20 18:42:28 +0200

    clang c compiler

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -3,6 +3,15 @@
 export CXXFLAGS="${CXXFLAGS} -std=c++14"
 export CPPFLAGS="${CPPFLAGS} -std=c++14"
 export CFLAGS="$CFLAGS -std=c++14"
+
+if [ "$(uname)" == "Darwin" ]
+then
+    export CMAKE_C_COMPILER="clang"
+    export CMAKE_CXX_COMPILER="clang++"
+    export CXX=clang++
+    export CC=clang
+fi
+
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/ -std=c++14"
 mkdir -p $PREFIX/bin
 cp src/tracy $PREFIX/bin
