3ea9649a6b3edad5a8b52630fecebd2aee173373
tobiasrausch <rauschtobi@gmail.com>
2020-04-20 20:28:34 +0200

    clang issues

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -6,12 +6,10 @@ export CFLAGS="$CFLAGS -std=c++14"
 
 if [ "$(uname)" == "Darwin" ]
 then
-    export CXX_FLAGS="${CXX_FLAGS} -std=c++14"
-    export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++14"
-    export CMAKE_C_COMPILER="clang"
+    export CMAKE_C_COMPILER="clang++"
     export CMAKE_CXX_COMPILER="clang++"
     export CXX=clang++
-    export CC=clang
+    export CC=clang++
 fi
 
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/ -std=c++14"
