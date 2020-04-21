d03afae90b2e49b374f2476732bd6dea697c83fc
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-04-21 14:25:30 -0400

    bash.sh updated

--- recipes/fragbuilder/bash.sh
+++ recipes/fragbuilder/bash.sh
@@ -2,9 +2,9 @@
 
 
 # setup environment variables
-export PYTHONPATH=${PREFIX}/lib/python2.7/site-packages:${PYTHONPATH}
+#export PYTHONPATH=${PREFIX}/lib/python2.7/site-packages:${PYTHONPATH}
 #export PYTHONPATH=/fragbuilder-1.0.1/fragbuilder:$PYTHONPATH
-cp -r /fragbuilder-1.0.1/fragbuilder ${PREFIX}/lib/python2.7/site-packages/
+cp -r /fragbuilder-1.0.1/fragbuilder ${PREFIX}/lib
 
 
 
