c4d7803176728ae9ce068b9a8d6fd5fb3a8f1fcb
Jeffrey Thiessen <JeffreyLThiessen@gmail.com>
2020-04-21 13:14:27 -0500

    change copy structure

--- recipes/irida-uploader/build.sh
+++ recipes/irida-uploader/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
 
-cp iridauploader/upload_run.py $PREFIX/bin/irida-uploader
-cp -r iridauploader/config $PREFIX/bin
-cp -r iridauploader/core $PREFIX/bin
-cp -r iridauploader/api $PREFIX/bin
-cp -r iridauploader/model $PREFIX/bin
-cp -r iridauploader/parsers $PREFIX/bin
-cp -r iridauploader/progress $PREFIX/bin
+cp upload_run.py $PREFIX/bin/irida-uploader
+cp -r iridauploader $PREFIX/bin
 
 chmod +x $PREFIX/bin/irida-uploader
