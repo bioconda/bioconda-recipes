fa9aab6a4f7c3e3101cf4131b38045281078fb61
jaidevjoshi83 <jaidev53ster@gmail.com>
2020-04-21 09:51:38 -0400

    bash.sh fixed

--- recipes/fragbuilder/bash.sh
+++ recipes/fragbuilder/bash.sh
@@ -1,9 +1,8 @@
 #!/bin/env bash
 
-tar xvzf fragbuilder-1.0.1.tar.gz
 
 # setup environment variables
-export PYTHONPATH=${PREFIX}/lib/python2.7/site-packages:${PYTHONPATH}
+#export PYTHONPATH=${PREFIX}/lib/python2.7/site-packages:${PYTHONPATH}
 
 cp -r fragbuilder-1.0.1/fragbuilder ${PREFIX}/lib/python2.7/site-packages
 
