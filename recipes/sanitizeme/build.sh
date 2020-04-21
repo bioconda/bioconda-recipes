4632635f0f4162242c0f586c746bf605abd03291
Jiangwei Yao <yao.jiangwei@gmail.com>
2020-04-21 16:30:35 -0400

    Try 8

--- recipes/sanitizeme/build.sh
+++ recipes/sanitizeme/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/bin/test
+mkdir -p ${PREFIX}/test
 chmod 0755 *.py
 cp *.py ${PREFIX}/bin/
-cp test/* ${PREFIX}/bin/test/
+cp test/* ${PREFIX}/test/
 
