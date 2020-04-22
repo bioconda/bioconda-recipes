b77ff0e4a34c2d4ca76a1eab54da7c9196535e59
tobiasrausch <rauschtobi@gmail.com>
2020-04-22 20:20:11 +0200

    remove c++14

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-export CXXFLAGS="${CXXFLAGS} -std=c++14"
-export CPPFLAGS="${CPPFLAGS} -std=c++14"
-export CFLAGS="$CFLAGS -std=c++14"
-
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/ -std=c++14"
 mkdir -p $PREFIX/bin
 cp src/tracy $PREFIX/bin
