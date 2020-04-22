6256ba46ea29aed9f79d324ff194837e87106b7f
TCLamnidis <thisseass@gmail.com>
2020-04-22 19:06:07 +0200

    Added pMMRCalculator recipe.

---
+++ recipes/pmmrcalculator/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+cp -R * $outdir/ 
+ls -l $outdir
+ln -s $outdir/pMMRCalculator.py $PREFIX/bin/pmmrCalculator
+chmod 0755 ${PREFIX}/bin/pmmrCalculator
