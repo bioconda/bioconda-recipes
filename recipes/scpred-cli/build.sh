4059d6e6bf0d97d36ca9b5c3ecab4d6e09387148
Andrey Solovyev <a.solovyev39@gmail.com>
2020-04-23 18:06:17 +0100

    update scpred-cli to 0.0.7

--- recipes/scpred-cli/build.sh
+++ recipes/scpred-cli/build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-
 mkdir -p $PREFIX/bin
 cp *.R $PREFIX/bin
-<<<<<<< HEAD
 cp *.sh $PREFIX/bin
 cp *.bats $PREFIX/bin
-=======
-cp sc*.sh $PREFIX/bin
->>>>>>> 249cb9b19d09753e72f2cb16904e80ce9bd8251a
