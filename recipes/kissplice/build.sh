e2a88b442647d0158aa6b1dd5d1366778e1b46e4
Devon Ryan <dpryan79@gmail.com>
2020-04-26 12:53:53 +0200

    rearrange

--- recipes/kissplice/build.sh
+++ recipes/kissplice/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
+mkdir -p work/bcalm
+touch work/bcalm/README.md
 
 mkdir -p build
 cd build
@@ -11,6 +13,4 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 # make commands
 make -j1
 #make test
-mkdir -p work/bcalm
-touch work/bcalm/README.md
 make install
