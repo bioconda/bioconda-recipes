a565e5b6c6414b0ca51c2d0d81b61fa1960d3e1c
Devon Ryan <dpryan79@users.noreply.github.com>
2020-04-26 12:20:10 +0200

    Update build.sh

--- recipes/bbmap/build.sh
+++ recipes/bbmap/build.sh
@@ -10,9 +10,7 @@ mkdir -p $BBMAP_HOME
 
 chmod a+x *.sh
 
-find . -type f -name *.o -exec rm -rf {} \;
+#find . -type f -name *.o -exec rm -rf {} \;
 cp -R * $BBMAP_HOME/
 
 find *.sh -type f -exec ln -s $BBMAP_HOME/{} $BINARY_HOME/{} \;
-
-python -c "import sys; print('Platform is %s' % sys.platform)"
