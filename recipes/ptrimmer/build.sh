96ea296f09919f5e8ef16a4274a1fd8300f9d2c8
Felix Mölder <felix.moelder@uni-due.de>
2020-04-27 13:33:21 +0200

    Updated build.sh

--- recipes/ptrimmer/build.sh
+++ recipes/ptrimmer/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-make CC=$CC LIBDIR=$PREFIX/lib INCLUDE=$PREFIX/include
+make CC=$CC LIBDIR=-L$PREFIX/lib INCLUDE=-I$PREFIX/include
+mkdir -p $PREFIX/bin
+mv pTrimmer-* $PREFIX/bin/ptrimmer
