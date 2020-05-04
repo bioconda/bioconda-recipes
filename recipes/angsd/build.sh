cacb938b5f0d3cd11b7efd667a1829b5b8740c50
Alexander Peltzer <alex.peltzer@gmail.com>
2020-05-04 22:26:40 +0200

    Attempt to fix build script

--- recipes/angsd/build.sh
+++ recipes/angsd/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 mkdir -p ${PREFIX}/bin
-make CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" prefix=$PREFIX CC=$CC CXX=$CXX install-all
+make CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" prefix=$PREFIX CC=$CC CXX=$CXX LDFLAGS+="-lz -lbz2 -llzma $LDFLAGS" install-all
