70aa9d26a4fc78fc5a00442d9fe95a56aa2fc997
Marcel Martin <marcel.martin@scilifelab.se>
2020-05-05 00:48:37 +0200

    Update build.sh

--- recipes/gargammel/build.sh
+++ recipes/gargammel/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 sed -i.bak 's/make -C libgab/make -C libgab CXX=$(CC)/' Makefile
@@ -10,11 +8,18 @@ sed -i.bak 's/CXXFLAGS =/CXXFLAGS +=/' src/Makefile
 sed -i.bak 's/LDFLAGS  =/LDFLAGS +=/' src/Makefile
 
 ## Following https://bioconda.github.io/contributor/troubleshooting.html#g-or-gcc-not-found
-make CC=$CXX
+binaries="src/fragSim src/deamSim src/adptSim src/fasta2fastas"
+# We list the targets explicitly to prevent the Makefile from downloading and
+# building the external art_illumina.o dependency (added as a runtime
+# requirement instead)
+make CC=$CXX ${binaries}
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -R * $outdir/
-
-ln -s $outdir/gargammel.pl $PREFIX/bin/gargammel
+cp ${binaries} ${PREFIX}/bin
+# Fix hard-coded paths to external tools
+sed \
+  -e 's|$pathdir."/src/|"|' \
+  -e 's|$pathdir."/art_src_MountRainier/|"|' \
+  -e 's|^fileExists(.*);||' \
+  -e 's|#!/usr/bin/perl|#!/usr/bin/env perl|' \
+  gargammel.pl > ${PREFIX}/bin/gargammel
