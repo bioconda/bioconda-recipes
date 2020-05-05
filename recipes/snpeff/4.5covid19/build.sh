6fd4788a0646117212a8fe517df8dab44d680e79
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-05-05 13:22:17 -0400

    4.5covid19 has a different zip structure; correct cp
    for the 4.5covid19 release, snpEff does not reside in a subdir within the source zip

--- recipes/snpeff/4.5covid19/build.sh
+++ recipes/snpeff/4.5covid19/build.sh
@@ -3,7 +3,7 @@
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p "$outdir"
 mkdir -p "$PREFIX/bin"
-cd snpEff/
+#cd snpEff/ # for the 4.5covid19 release, snpEff does not reside in a subdir within the zip
 mv scripts/ snpEff.config snpEff.jar "$outdir/"
 cp "$RECIPE_DIR/snpeff.py" "$outdir/snpEff"
 chmod +x "$outdir/snpEff"
