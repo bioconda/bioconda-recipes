122ab866abca4f6fbb9b07ccd36e67b916cf2e6d
Antonio Camargo <antoniop.camargo@gmail.com>
2020-05-06 20:34:22 -0300

    Fix build script

--- recipes/macse/build.sh
+++ recipes/macse/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 macse=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $macse
-cp -r ./* $macse
+cp -r $SRC_DIR/* $macse
 chmod +x $macse/macse_v2.03.jar
 mkdir -p $PREFIX/bin
 ln -s $macse/macse_v2.03.jar $PREFIX/bin/macse
