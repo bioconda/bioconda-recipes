db484e4d9d3a9b67d91fbfcbfe2de8003273a04d
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-05-06 14:32:32 -0400

    include data sub-dir
    The snpEff author hasn't made databases available online yet for v4.5, and has instead chosen to distribute SARS-CoV-2 annotations as part of the special covid19 release as if they had already been downloaded. For the bioconda package we usually omit the local data/ cache to save space since the databases are typically on sourceforge anyway and because everyone's needs are different. These changes copy the data/ sub-dir to the conda environment as a special case. If/when databases are available on sourceforge for v4_5 we should once again omit copying the data/ subdir.

--- recipes/snpeff/4.5covid19/build.sh
+++ recipes/snpeff/4.5covid19/build.sh
@@ -4,7 +4,9 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p "$outdir"
 mkdir -p "$PREFIX/bin"
 #cd snpEff/ # for the 4.5covid19 release, snpEff does not reside in a subdir within the zip
-mv scripts/ snpEff.config snpEff.jar "$outdir/"
+# data/ should be omitted once databases are available on sourceforge for v4_5
+# check here: https://sourceforge.net/projects/snpeff/files/databases/
+mv scripts/ data/ snpEff.config snpEff.jar "$outdir/" 
 cp "$RECIPE_DIR/snpeff.py" "$outdir/snpEff"
 chmod +x "$outdir/snpEff"
 
