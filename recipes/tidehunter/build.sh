5eaf6f307bbc316a9516eaac053d91a18b915da6
Yan Gao <yangao07@hit.edu.cn>
2020-05-10 20:13:36 +0800

    Update TideHunter to v1.3.0

--- recipes/tidehunter/build.sh
+++ recipes/tidehunter/build.sh
@@ -6,6 +6,6 @@ pushd abPOA
 make libabpoa INCLUDE="-I$PREFIX/include" CFLAGS="$CFLAGS -L$PREFIX/lib"
 popd
 
-make INCLUDE="-I$PREFIX/include" CFLAGS="-Wall -O3 -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -I$PREFIX/include -L$PREFIX/lib"
+make CFLAGS="-Wall -O3 -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -I$PREFIX/include -L$PREFIX/lib"
 
 cp bin/TideHunter $PREFIX/bin
