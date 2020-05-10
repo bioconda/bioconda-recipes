b08c55ffa415b57b9af1b567685ba2f7d4ba8279
timfallon <tfallon@mit.edu>
2020-05-10 10:42:00 -0700

    new recipe: gFACs

---
+++ recipes/perl-gfacs/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -o errexit -o nounset
+
+readonly GFACS_HOME=${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}
+
+mkdir -p ${GFACS_HOME}
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/etc/conda/activate.d
+mkdir -p ${PREFIX}/etc/conda/deactivate.d
+
+cp -Rp format_scripts support_scripts task_scripts gFACs.pl ${GFACS_HOME}
+
+ln -s ${GFACS_HOME}/gFACs.pl ${PREFIX}/bin/gFACs.pl
+
+chmod +x gFACs.pl
+ln -s gFACs.pl ${PREFIX}/bin/gFACs.pl
+
+#helpful ENV variable
+mkdir -p ${PREFIX}/etc/conda/activate.d/
+echo "export GFACS_HOME=${GFACS_HOME}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
+
+mkdir -p ${PREFIX}/etc/conda/deactivate.d/
+echo "unset GFACS_HOME" > ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}-${PKG_VERSION}.sh
