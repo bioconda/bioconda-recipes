3bc4805228bb823bb093653f371cbf22dd11b9aa
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-05-13 13:23:15 +0000

    cplus include

--- recipes/expansionhunterdenovo/build.sh
+++ recipes/expansionhunterdenovo/build.sh
@@ -5,5 +5,6 @@ cmake -DCMAKE_BUILD_TYPE=Release ../source
 # Build the binary, this is required as a work-around to g++ not being found
 ln -s ${CC} $BUILD_PREFIX/bin/gcc
 ln -s ${CXX} $BUILD_PREFIX/bin/g++
+export CPLUS_INCLUDE_PATH=$PREFIX/include
 make
 cp ExpansionHunterDenovo $PREFIX/bin/ExpansionHunterDenovo
