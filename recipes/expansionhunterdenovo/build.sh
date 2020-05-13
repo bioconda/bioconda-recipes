6e7186147d300b649d4dac607cae16d01c532fda
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-05-13 09:52:25 +0000

    remove gcc error?

--- recipes/expansionhunterdenovo/build.sh
+++ recipes/expansionhunterdenovo/build.sh
@@ -2,5 +2,8 @@
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../source
+# Build the binary, this is required as a work-around to g++ not being found
+ln -s ${CC} $BUILD_PREFIX/bin/gcc
+ln -s ${CXX} $BUILD_PREFIX/bin/g++
 make
 cp ExpansionHunterDenovo $PREFIX/bin/ExpansionHunterDenovo
