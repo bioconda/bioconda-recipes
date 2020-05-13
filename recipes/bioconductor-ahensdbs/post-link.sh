a802fcaf97f50d2c3a30ce895147b6c345f3f4c3
Devon Ryan <dpryan79@gmail.com>
2020-05-13 18:50:45 +0200

    Bump a bunch of r- recipes

--- recipes/bioconductor-ahensdbs/post-link.sh
+++ recipes/bioconductor-ahensdbs/post-link.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FN="AHEnsDbs_1.0.13.tar.gz"
+FN="AHEnsDbs_1.0.14.tar.gz"
 URLS=(
-  "https://bioconductor.org/packages/3.11/data/annotation/src/contrib/AHEnsDbs_1.0.13.tar.gz"
-  "https://bioarchive.galaxyproject.org/AHEnsDbs_1.0.13.tar.gz"
-  "https://depot.galaxyproject.org/software/bioconductor-ahensdbs/bioconductor-ahensdbs_1.0.13_src_all.tar.gz"
+  "https://bioconductor.org/packages/3.11/data/annotation/src/contrib/AHEnsDbs_1.0.14.tar.gz"
+  "https://bioarchive.galaxyproject.org/AHEnsDbs_1.0.14.tar.gz"
+  "https://depot.galaxyproject.org/software/bioconductor-ahensdbs/bioconductor-ahensdbs_1.0.14_src_all.tar.gz"
 )
-MD5="fdc0ea28f53928c75ee0c8ca2ef3a0ad"
+MD5="e129b39719011f34d61675ca26090d0e"
 
 # Use a staging area in the conda dir rather than temp dirs, both to avoid
 # permission issues as well as to have things downloaded in a predictable
