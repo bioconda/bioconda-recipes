4438813c3869ed1b90fb1c28bce2f90ef6abc007
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-05-13 13:37:59 +0000

    more exports

--- recipes/expansionhunterdenovo/build.sh
+++ recipes/expansionhunterdenovo/build.sh
@@ -5,6 +5,10 @@ cmake -DCMAKE_BUILD_TYPE=Release ../source
 # Build the binary, this is required as a work-around to g++ not being found
 ln -s ${CC} $BUILD_PREFIX/bin/gcc
 ln -s ${CXX} $BUILD_PREFIX/bin/g++
-export CPLUS_INCLUDE_PATH=$PREFIX/include
+export CXXFLAGS="$CXXFLAGS -L${PREFIX}/lib"
+export LIBRARY_PATH=${PREFIX}/lib
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
 make
 cp ExpansionHunterDenovo $PREFIX/bin/ExpansionHunterDenovo
