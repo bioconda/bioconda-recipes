af21d0b4bffb1cccbd3fa8b3a8da4933b0019e56
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-05-13 14:37:51 +0000

    desperate

--- recipes/expansionhunterdenovo/build.sh
+++ recipes/expansionhunterdenovo/build.sh
@@ -5,14 +5,14 @@ cmake -DCMAKE_BUILD_TYPE=Release ../source
 # Build the binary, this is required as a work-around to g++ not being found
 ln -s ${CC} $BUILD_PREFIX/bin/gcc
 ln -s ${CXX} $BUILD_PREFIX/bin/g++
-export CXXFLAGS="$CXXFLAGS -L${PREFIX}/lib"
-export LIBRARY_PATH=${PREFIX}/lib
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPATH=${PREFIX}/include
-export CFLAGS="-I${PREFIX}/include"
-export INCLUDE_PATH="${PREFIX}/include"
+export CXXFLAGS="$CXXFLAGS -L${PREFIX}/lib -L${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib:${PREFIX}/include"
+export CPP_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/lib"
+export CPLUS_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/lib"
+export CXX_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/lib"
+export C_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/lib"
+export CPATH="${PREFIX}/include:${PREFIX}/lib"
+export CFLAGS="-I${PREFIX}/include -I${PREFIX}/lib"
+export INCLUDE_PATH="${PREFIX}/include:${PREFIX}/lib"
 make
 cp ExpansionHunterDenovo $PREFIX/bin/ExpansionHunterDenovo
