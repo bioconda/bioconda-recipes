80c0732b56902453e3f85b118d1e8ab7fa9314e1
Vivek Rai <mail@raivivek.in>
2020-05-15 13:01:27 -0400

    Update QoRTs to 1.3.6

--- recipes/qorts/build.sh
+++ recipes/qorts/build.sh
@@ -4,8 +4,13 @@ set -eu -o pipefail
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -R * $outdir/
-cp $RECIPE_DIR/qorts.py $outdir/qorts
-ls -l $outdir
-ln -s $outdir/qorts $PREFIX/bin
+
+cp -t $outdir QoRTs.jar QoRTs.sh qortsGenMultiQC.R
+
+# Symlink the executable wrapper as qorts
+ln -s $outdir/QoRTs.sh $PREFIX/qorts
+
+# Symlink provided script
+ln -s $outdir/qortsGenMultiQC.R $PREFIX/qortsGenMultiQC.R
+
 chmod 0755 "${PREFIX}/bin/qorts"
