6a04c2f89cfa594d6288c9d95592b09225448355
Vivek Rai <mail@raivivek.in>
2020-05-15 13:24:19 -0400

    Fix build script

--- recipes/qorts/build.sh
+++ recipes/qorts/build.sh
@@ -5,12 +5,12 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
-cp -t $outdir QoRTs.jar QoRTs.sh qortsGenMultiQC.R
+cp -t $outdir QoRTs.jar scripts/QoRTs.sh scripts/qortsGenMultiQC.R
 
 # Symlink the executable wrapper as qorts
-ln -s $outdir/QoRTs.sh $PREFIX/qorts
+ln -s $outdir/QoRTs.sh $PREFIX/bin/qorts
 
 # Symlink provided script
-ln -s $outdir/qortsGenMultiQC.R $PREFIX/qortsGenMultiQC.R
+ln -s $outdir/qortsGenMultiQC.R $PREFIX/bin/qortsGenMultiQC.R
 
 chmod 0755 "${PREFIX}/bin/qorts"
