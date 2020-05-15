b81dd39421551e081079e88a1680db1747962890
mvdbeek <m.vandenbeek@gmail.com>
2020-05-15 11:34:23 +0200

    Restore htslib dependency, restore S3/GCS support
    Should fix https://github.com/pysam-developers/pysam/issues/920#issuecomment-628919424
    hopefully.

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -7,4 +7,6 @@ export CFLAGS="-I$PREFIX/include -DHAVE_LIBDEFLATE"
 export CPPFLAGS="-I$PREFIX/include -DHAVE_LIBDEFLATE"
 export LDFLAGS="-L$PREFIX/lib"
 
+export HTSLIB_LIBRARY_DIR=$PREFIX/lib
+export HTSLIB_INCLUDE_DIR=$PREFIX/include
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
