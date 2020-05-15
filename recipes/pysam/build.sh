6ed8b711cd25accedf9a3d7837ae69445e63e7da
John Marshall <John.W.Marshall@glasgow.ac.uk>
2020-05-15 10:47:30 +0100

    Update pysam dependencies
    Continue building pysam with its bundled libhts until there is an
    HTSlib release or bioconda package with samtools/htslib#1072 applied.
    Fix build dependencies (ie add libcurl) to restore pysam's https/S3/etc
    capabilities. Remove unneeded CFLAGS/CPPFLAGS/LDFLAGS overrides.
    
    Move tests to a separate run_test.py script, and add a test checking that
    opening "s3:invalid_url" does not result in "Protocol not supported".
    
    (Remove unused obsolete osx_rpath.patch.)

--- recipes/pysam/build.sh
+++ recipes/pysam/build.sh
@@ -3,8 +3,5 @@
 # linking htslib, see:
 # http://pysam.readthedocs.org/en/latest/installation.html#external
 # https://github.com/pysam-developers/pysam/blob/v0.9.0/setup.py#L79
-export CFLAGS="-I$PREFIX/include -DHAVE_LIBDEFLATE"
-export CPPFLAGS="-I$PREFIX/include -DHAVE_LIBDEFLATE"
-export LDFLAGS="-L$PREFIX/lib"
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
