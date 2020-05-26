a7f5e30a22b9149d1670d565246f6ee4e37357ed
Rhys Newell <rhys.newell@uqconnect.edu.au>
2020-05-26 13:02:00 +1000

    updating libclangpath

--- recipes/lorikeet-genome/build.sh
+++ recipes/lorikeet-genome/build.sh
@@ -3,6 +3,7 @@
 # Build statically linked binary with Rust
 C_INCLUDE_PATH=$PREFIX/include \
 LIBRARY_PATH=$PREFIX/lib \
+LIBCLANG_PATH=$PREFIX/lib/libclang.so \
 cargo build --release
 
 # Install the binaries
