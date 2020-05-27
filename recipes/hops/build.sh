01b104afda9c8efab8da82edb56cefbce4f0c839
Ron Huebler <huebler@shh.mpg.de>
2020-05-27 18:48:20 +0200

    updated build.sh

--- recipes/hops/build.sh
+++ recipes/hops/build.sh
@@ -6,14 +6,15 @@ echo $outdir
 mkdir -p $outdir
 mkdir -p ${PREFIX}/bin
 cp -R * $outdir/
+mv $outdir/$PKG_NAME$PKG_VERSION/* $outdir
+rm $outdir/$PKG_NAME$PKG_VERSION/
 cp $RECIPE_DIR/hops.py $outdir/hops
 cp $RECIPE_DIR/MaltExtract.py $outdir/MaltExtract
 ls -l $RECIPE_DIR
-
 ln -s $outdir/hops ${PREFIX}/bin
 ln -s $outdir/MaltExtract ${PREFIX}/bin
 ls -l $outdir
-mv $outdir/$PKG_NAME$PKG_VERSION/postprocessing.AMPS.r ${PREFIX}/bin/
+mv $outdir/postprocessing.AMPS.r ${PREFIX}/bin/
 chmod 0755 ${PREFIX}/bin/hops
 chmod 0755 ${PREFIX}/bin/MaltExtract
 chmod 0755 ${PREFIX}/bin/postprocessing.AMPS.r
