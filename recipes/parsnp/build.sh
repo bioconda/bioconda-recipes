2a6bb3f1e473ec2411b225e01d5643d8c01e706b
Bryce Lorenz Kille <brycekille@gmail.com>
2020-05-27 21:51:49 -0500

    Full build

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -8,11 +8,11 @@ export ORIGIN=\$ORIGIN
 cd muscle
 ./autogen.sh
 ./configure --prefix=`pwd`
-make install -j ${nproc}
+make install
 cd ..
 ./autogen.sh
 ./configure LDFLAGS='-Wl,-rpath,$$ORIGIN/../muscle/lib'
-make LDADD=-lMUSCLE-3.7 -j ${nproc}
+make LDADD=-lMUSCLE-3.7 
 make install
 
 cp template.ini parsnp Parsnp.py bin/ muscle/ $PREFIX/bin/ -r
