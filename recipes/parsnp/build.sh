07b9bb522e5765e63d2a57d10bc9e7f71dfaf247
Bryce Lorenz Kille <brycekille@gmail.com>
2020-05-27 20:25:31 -0500

    debug

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -2,9 +2,10 @@
 
 
 mkdir -p  "$PREFIX/bin"
+>&2 cat *
 >&2 echo $PWD
 >&2 ls
-./build_parsnp.sh
+#./build_parsnp.sh
 
-cp template.ini parsnp Parsnp.py bin/ muscle/ $PREFIX/bin/ -r
+#cp template.ini parsnp Parsnp.py bin/ muscle/ $PREFIX/bin/ -r
 
