371ae9cc2fa351310cdcf4038d4e03bad6a6b791
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-30 15:44:38 +0200

    manually overwriting CC to g++ in order to use correct binary for archtriple determination

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-make PREFIX=$PREFIX -C Misc/Applications/pKiss all
+make PREFIX=$PREFIX cc=g++ -C Misc/Applications/pKiss all
 make -C Misc/Applications/pKiss install-program
 make -C Misc/Applications/lib install
 chmod +x $PREFIX/bin/*
