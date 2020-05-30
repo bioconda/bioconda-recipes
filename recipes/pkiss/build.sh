7091a6dc39252cf8ff1129461eca8588420bbba1
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-30 22:22:58 +0200

    can we read CC from Perl to avoid this issue?

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-which -a g++
-g++ --version
-env
+which -a g++ || g++ --version || env
 make PREFIX=$PREFIX CC=g++ -C Misc/Applications/pKiss all
 make -C Misc/Applications/pKiss install-program
 make -C Misc/Applications/lib install
