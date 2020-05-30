0a63fd9c3d09410a9d075539d29ed23812c12dab
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-30 22:40:54 +0200

    use env for CC

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 which -a g++ || g++ --version || env
-make PREFIX=$PREFIX CC=g++ -C Misc/Applications/pKiss all
+make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pKiss all
 make -C Misc/Applications/pKiss install-program
 make -C Misc/Applications/lib install
 chmod +x $PREFIX/bin/*
