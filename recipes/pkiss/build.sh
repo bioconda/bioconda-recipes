27d66d1b5aaaf42cf8f592c7d77bc220279ec4de
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-30 22:00:14 +0200

    start debugging

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+which -a g++
+g++ --version
+env
 make PREFIX=$PREFIX CC=g++ -C Misc/Applications/pKiss all
 make -C Misc/Applications/pKiss install-program
 make -C Misc/Applications/lib install
