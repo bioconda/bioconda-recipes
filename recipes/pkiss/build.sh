59dafe5f9931c02e640612b86a10dec1ab993052
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-31 23:07:06 +0200

    correct sed argument order

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sed -i $PREFIX/share/gapc/config_linux-gnu.mf "s/^CXX = /#CXX = /"
-sed -i $PREFIX/share/gapc/config_linux-gnu.mf "s/^CC = /#CC = /"
+sed "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
+sed "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config_linux-gnu.mf 
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pKiss all
 make CC=$CC -C Misc/Applications/pKiss install-program
 make CC=$CC -C Misc/Applications/lib install
