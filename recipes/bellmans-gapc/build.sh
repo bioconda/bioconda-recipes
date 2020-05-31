2241b496082f7acce0d40dea4618df392d695d36
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-31 23:05:23 +0200

    fix sed argument order

--- recipes/bellmans-gapc/build.sh
+++ recipes/bellmans-gapc/build.sh
@@ -4,5 +4,5 @@ make
 make install
 # remove definition of CXX in user space config file, since this will not match users compiler path,
 # instead let conda take care of providing meaningful CXX and CC values.
-sed -i $PREFIX/share/gapc/config_linux-gnu.mf "s/^CXX = /#CXX = /"
-sed -i $PREFIX/share/gapc/config_linux-gnu.mf "s/^CC = /#CC = /"
+sed "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
+sed "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
