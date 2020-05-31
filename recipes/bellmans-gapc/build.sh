6a27d55ef3084600b69f1d7a832092aff0c0d2a8
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-05-31 22:47:21 +0200

    taking care that CC and CXX are passed through via conda and make is available in user space

--- recipes/bellmans-gapc/build.sh
+++ recipes/bellmans-gapc/build.sh
@@ -2,3 +2,7 @@
 ./configure --with-boost-libdir=$PREFIX/lib/ CPPFLAGS=-I${PREFIX} --prefix=$PREFIX --disable-gsltest
 make
 make install
+# remove definition of CXX in user space config file, since this will not match users compiler path,
+# instead let conda take care of providing meaningful CXX and CC values.
+sed -i $PREFIX/share/gapc/config_linux-gnu.mf "s/^CXX = /#CXX = /"
+sed -i $PREFIX/share/gapc/config_linux-gnu.mf "s/^CC = /#CC = /"
