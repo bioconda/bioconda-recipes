409a1846842cba98f1b41fd998fa6e1768bbd4bd
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-06-01 10:43:33 +0200

    bellman's gapc should be fixed now + providing PREFIX also for make install

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-sed "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
-sed "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config_linux-gnu.mf 
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pKiss all
-make CC=$CC -C Misc/Applications/pKiss install-program
-make CC=$CC -C Misc/Applications/lib install
+make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pKiss install-program
+make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
 chmod +x $PREFIX/bin/*
