da46f43befbb1c1088f04f26ac353ae3e8d3b0c8
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-06-01 20:29:14 +0200

    debug infos

--- recipes/rnashapes/3.3.0/build.sh
+++ recipes/rnashapes/3.3.0/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cat $PREFIX/share/gapc/config_linux-gnu.mf || True
+cat Misc/Applications/RNAshapes/makefile || True
+echo $CC || True
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes mfe_nodangle
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
