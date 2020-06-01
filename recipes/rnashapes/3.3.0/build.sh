4046db41e89f990737f653f4fbf7bd87e802b57a
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-06-01 20:28:02 +0200

    debug

--- recipes/rnashapes/3.3.0/build.sh
+++ recipes/rnashapes/3.3.0/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes all
+make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes mfe_nodangle
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
 chmod +x $PREFIX/bin/*
