ec703647f30bf6e2b3120879353ec9d2e93e49cb
Mehmet Tekman <mtekman89@gmail.com>
2020-06-02 13:20:00 +0200

    second stab at zlib

--- recipes/king/build.sh
+++ recipes/king/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #export CPP_INCLUDE_PATH=${PREFIX}/include
-#export CXX_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
 #export CPLUS_INCLUDE_PATH=${PREFIX}/include
-#export LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
 #export CXXFLAGS="$CXXFLAGS "
 
-$CXX -lm -DDYNAMIC_ZLIB -O2 -fopenmp -o king *.cpp
+${CXX} -I${CXX_INCLUDE_PATH} -L${LIBRARY_PATH} -lm -lz -O2 -fopenmp -o king *.cpp
 cp king $PREFIX/bin/king
