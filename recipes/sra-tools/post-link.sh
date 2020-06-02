baff3dccc5b325d205affb01b42f023c50061a30
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-06-02 16:30:42 -0400

    [ci skip] Merge PR 22469
    Merge PR #22469, commits were: 
     * add ossuuid as dependency
    
    add ossuuid as dependency since biocontainers baseimage lacks uuidgen
     * remove patch that skipped config file GUID check; add GUID to file during test and at install time
    
    remove patch that skipped config file GUID check; add GUID to file during test and at install time (if not present) via post-link script
     * update vdb version 2.10.5 -> 2.10.7
     * sra-tools 2.10.3 -> 2.10.7; (& ncbi-vdb 2.10.3 -> 2.10.5; ngs 2.10.2 -> 2.10.5); update patch

---
+++ recipes/sra-tools/post-link.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#  sra-toolkit >= 2.10.2 requires a config dotfile to exist with a GUID
+#
+# See:
+#   https://www.ncbi.nlm.nih.gov/sra/docs/sra-cloud/
+#   https://github.com/ncbi/sra-tools/blob/master/build/docker/Dockerfile
+#   https://github.com/ncbi/sra-tools/issues/291
+#   https://github.com/ncbi/sra-tools/issues/282
+#   https://github.com/ncbi/sra-tools/blob/9ea223545e34f4af7c722759764361a7bee84a45/tools/driver-tool/sratools.cpp#L155
+#   https://github.com/ncbi/sra-tools/blob/9ea223545e34f4af7c722759764361a7bee84a45/tools/driver-tool/config.hpp#L46-L64
+
+if ! grep "/LIBS/GUID" ~/.ncbi/user-settings.mkfg &> /dev/null; then
+    echo "No entry for '/LIBS/GUID' in ~/.ncbi/user-settings.mkfg; adding..."
+    mkdir -p ~/.ncbi
+    printf '/LIBS/GUID = "%s"\n' `uuid` >> ~/.ncbi/user-settings.mkfg
+else
+    echo "~/.ncbi/user-settings.mkfg has entry for '/LIBS/GUID'; continuing..."
+fi
