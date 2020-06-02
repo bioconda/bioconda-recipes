49e1c1af1425b8833437b7555c258dee3afd8773
Mehmet Tekman <mtekman89@gmail.com>
2020-06-02 12:24:36 +0200

    Add king recipe

---
+++ recipes/king/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+c++ -lm -lz -O2 -fopenmp -o king *.cpp
+cp king $PREFIX/bin/king
