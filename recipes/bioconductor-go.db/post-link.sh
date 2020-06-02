9bf72f00b8fed05b20d30ddc9ab5a0d54fb74494
Devon Ryan <dpryan79@users.noreply.github.com>
2020-06-02 21:01:35 +0200

    Bump GO.db so it works again (#22467)

--- recipes/bioconductor-go.db/post-link.sh
+++ recipes/bioconductor-go.db/post-link.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FN="GO.db_3.11.1.tar.gz"
+FN="GO.db_3.11.4.tar.gz"
 URLS=(
-  "https://bioconductor.org/packages/3.11/data/annotation/src/contrib/GO.db_3.11.1.tar.gz"
-  "https://bioarchive.galaxyproject.org/GO.db_3.11.1.tar.gz"
-  "https://depot.galaxyproject.org/software/bioconductor-go.db/bioconductor-go.db_3.11.1_src_all.tar.gz"
+  "https://bioconductor.org/packages/3.11/data/annotation/src/contrib/GO.db_3.11.4.tar.gz"
+  "https://bioarchive.galaxyproject.org/GO.db_3.11.4.tar.gz"
+  "https://depot.galaxyproject.org/software/bioconductor-go.db/bioconductor-go.db_3.11.4_src_all.tar.gz"
 )
-MD5="0d4fbb36b4b8f52af43e55021372eb84"
+MD5="cb4322c45d6ee988fdf9ccee242eae1f"
 
 # Use a staging area in the conda dir rather than temp dirs, both to avoid
 # permission issues as well as to have things downloaded in a predictable
