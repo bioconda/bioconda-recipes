b3d06a805ba45714a634dc90402df2915af5bfda
Mehmet Tekman <mtekman89@gmail.com>
2020-06-02 12:38:13 +0200

    c++ to g++

--- recipes/king/build.sh
+++ recipes/king/build.sh
@@ -5,5 +5,5 @@ export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-c++ -lm -lz -O2 -fopenmp -o king *.cpp
+g++ -lm -lz -O2 -fopenmp -o king *.cpp
 cp king $PREFIX/bin/king
