cb2f2f92caed93b9bfd0c51002d4763c762410d4
Mehmet Tekman <mtekman89@gmail.com>
2020-06-02 14:51:24 +0200

    tidy up

--- recipes/king/build.sh
+++ recipes/king/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
-#export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-#export CXXFLAGS="$CXXFLAGS "
 
 ${CXX} -I${CXX_INCLUDE_PATH} -L${LIBRARY_PATH} -lm -lz -O2 -fopenmp -o king *.cpp
 mkdir -p $PREFIX/bin
