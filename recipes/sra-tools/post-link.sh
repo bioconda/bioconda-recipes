1cc4107ce1c1438c7a3e96ea9475fb5827bf4ab1
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-06-02 17:47:03 -0400

    [ci skip] Merge PR 22473
    Merge PR #22473, commits were: 
     * call vdb-config --interactive if post-link script is run from interactive terminal

--- recipes/sra-tools/post-link.sh
+++ recipes/sra-tools/post-link.sh
@@ -10,10 +10,23 @@
 #   https://github.com/ncbi/sra-tools/blob/9ea223545e34f4af7c722759764361a7bee84a45/tools/driver-tool/sratools.cpp#L155
 #   https://github.com/ncbi/sra-tools/blob/9ea223545e34f4af7c722759764361a7bee84a45/tools/driver-tool/config.hpp#L46-L64
 
+# if the GUID is not in the user-settings file
 if ! grep "/LIBS/GUID" ~/.ncbi/user-settings.mkfg &> /dev/null; then
-    echo "No entry for '/LIBS/GUID' in ~/.ncbi/user-settings.mkfg; adding..."
-    mkdir -p ~/.ncbi
-    printf '/LIBS/GUID = "%s"\n' `uuid` >> ~/.ncbi/user-settings.mkfg
+    # check whether this is via interactive terminal
+    # see: https://www.tldp.org/LDP/abs/html/intandnonint.html
+    fd=0   # stdin
+    if [[ -t "$fd" || -p /dev/stdin ]]; then
+        # if this is an interactive terminal, run the official config tool
+        echo "No entry for '/LIBS/GUID' in ~/.ncbi/user-settings.mkfg; calling 'vdb-config --interactive'..."
+        vdb-config --interactive
+        # if still no GUID, add one
+        if ! grep "/LIBS/GUID" ~/.ncbi/user-settings.mkfg &> /dev/null; then mkdir -p ~/.ncbi && printf '/LIBS/GUID = "%s"\n' `uuid` >> ~/.ncbi/user-settings.mkfg; fi
+    else
+        # if this is an unattended install, just insert a new GUID into the config file
+        echo "No entry for '/LIBS/GUID' in ~/.ncbi/user-settings.mkfg; adding..."
+        mkdir -p ~/.ncbi
+        printf '/LIBS/GUID = "%s"\n' `uuid` >> ~/.ncbi/user-settings.mkfg
+    fi
 else
     echo "~/.ncbi/user-settings.mkfg has entry for '/LIBS/GUID'; continuing..."
 fi
