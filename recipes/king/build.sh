7c094d04adadb6842e95d52fa0d514345a3aeb27
Mehmet Tekman <mtekman89@gmail.com>
2020-06-02 12:47:48 +0200

    relative link

--- recipes/king/build.sh
+++ recipes/king/build.sh
@@ -5,5 +5,5 @@ export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-g++ -lm -lz -O2 -fopenmp -o king *.cpp
+$CXX -lm -lz -O2 -fopenmp -o king *.cpp
 cp king $PREFIX/bin/king
