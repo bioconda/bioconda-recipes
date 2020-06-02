6a3af5604a865d5a3709b938a6957aad1fc1fa49
Mehmet Tekman <mtekman89@gmail.com>
2020-06-02 13:03:10 +0200

    added zlib host and dynamic linking

--- recipes/king/build.sh
+++ recipes/king/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+#export CPP_INCLUDE_PATH=${PREFIX}/include
+#export CXX_INCLUDE_PATH=${PREFIX}/include
+#export CPLUS_INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
+#export CXXFLAGS="$CXXFLAGS "
 
-$CXX -lm -lz -O2 -fopenmp -o king *.cpp
+$CXX -lm -DDYNAMIC_ZLIB -O2 -fopenmp -o king *.cpp
 cp king $PREFIX/bin/king
