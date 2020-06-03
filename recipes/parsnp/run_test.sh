831a61d02de4f12624dfcefbbc2980ec8faac092
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-03 12:28:48 -0500

    dummy commit to rerun ci

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -8,5 +8,5 @@ tar -xvf mers49.tar.gz >/dev/null
 rm mers49/._* -f
 
 parsnp -V >/dev/null
-parsnp -g ./ref/EMC_2012.gbk -d ./mers49/*.fna -C 1000 -c -o test --verbose --use-fasttree >/dev/null
-parsnp -r ! -d ./mers49/*.fna -o test --verbose  >/dev/null
+parsnp -g ref/EMC_2012.gbk -d mers49/*.fna -C 1000 -c -o test --verbose --use-fasttree >/dev/null
+parsnp -r ! -d mers49/*.fna -o test --verbose  >/dev/null
