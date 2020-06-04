6948c284d782b686459d22a536f5d924eaf0b4a4
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-04 03:17:07 -0500

    New test dataset

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+wget https://github.com/marbl/harvest/raw/master/docs/content/parsnp/mers_examples.tar.gz
+tar -xzvf mers_examples.tar.gz
 parsnp -V 
-parsnp -g examples/mers_virus/ref/England1.gbk -d examples/mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
-parsnp -r ! -d examples/mers_virus/genomes/*.fna -o test2 --verbose
+parsnp -g mers_virus/ref/England1.gbk -d mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
+parsnp -r ! -d mers_virus/genomes/*.fna -o test2 --verbose
