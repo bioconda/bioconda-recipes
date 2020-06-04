217914b0c9324067c5ab6e0b0ddcd1a6c250e9c7
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-04 03:41:34 -0500

    no wget on osx ):

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/marbl/harvest/raw/master/docs/content/parsnp/mers_examples.tar.gz
+curl https://github.com/marbl/harvest/raw/master/docs/content/parsnp/mers_examples.tar.gz -L --output mers_examples.tar.gz
 tar -xzvf mers_examples.tar.gz
 parsnp -V 
 parsnp -g mers_virus/ref/England1.gbk -d mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
