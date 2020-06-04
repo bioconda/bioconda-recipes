16d4c898dcb93a565b5bce83e55763b28dccd101
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-04 02:55:58 -0500

    usr/bin/env instead of bash path. no pipefail

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-set -euo pipefail
+#!/usr/bin/env bash
 
-parsnp -V >/dev/null
+parsnp -V 
 parsnp -g examples/mers_virus/ref/England1.gbk -d examples/mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
 parsnp -r ! -d examples/mers_virus/genomes/*.fna -o test2 --verbose
