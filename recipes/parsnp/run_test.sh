0989e1d4e217cac37c9bf6120c8d42dc734ee26d
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-04 01:44:34 -0500

    dummy commit

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 parsnp -V >/dev/null
 parsnp -g ls examples/mers_virus/ref/England1.gbk -d example/mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree >/dev/null
-parsnp -r ! -d examples/mers_virus/genomes/*.fna -o test --verbose  >/dev/null
+parsnp -r ! -d examples/mers_virus/genomes/*.fna -o test2 --verbose  >/dev/null
