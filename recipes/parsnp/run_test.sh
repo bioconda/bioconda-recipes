8ebcb85d2a28b5e32663e3c2e48355177114ed0d
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-04 01:25:47 -0500

    Use provided examples

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-TESTDATA="https://harvest.readthedocs.io/en/latest/_downloads/mers49.tar.gz"
-curl -O $TESTDATA >/dev/null
-tar -xvf mers49.tar.gz >/dev/null
-
-rm mers49/._* -f
-
 parsnp -V >/dev/null
-parsnp -g ref/EMC_2012.gbk -d mers49/*.fna -C 1000 -c -o test --verbose --use-fasttree >/dev/null
-parsnp -r ! -d mers49/*.fna -o test --verbose  >/dev/null
+parsnp -g ls examples/mers_virus/ref/England1.gbk -d example/mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree >/dev/null
+parsnp -r ! -d examples/mers_virus/genomes/*.fna -o test --verbose  >/dev/null
