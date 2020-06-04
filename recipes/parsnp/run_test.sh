ddfbb9a01cdb89677e25a5f52ff34833622c100f
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-04 02:36:39 -0500

    typpo

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 parsnp -V >/dev/null
-parsnp -g examples/mers_virus/ref/England1.gbk -d example/mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
+parsnp -g examples/mers_virus/ref/England1.gbk -d examples/mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
 parsnp -r ! -d examples/mers_virus/genomes/*.fna -o test2 --verbose
