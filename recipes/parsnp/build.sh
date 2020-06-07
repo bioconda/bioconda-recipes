76017814ec1c48474be7556737795bfea61e5d14
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-07 15:13:12 -0500

    LFLAGS

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -7,7 +7,7 @@ cd muscle
 ./autogen.sh
 
 if [ `uname` == Darwin ]; then
-    ./configure --prefix=$PWD CXXFLAGS='-fopenmp' --disable-shared 
+    ./configure --prefix=$PWD LFLAGS='-fopenmp' CXXFLAGS='-fopenmp' --disable-shared 
 else
     ./configure --prefix=$PWD CXXFLAGS='-fopenmp'
 fi
@@ -17,7 +17,7 @@ cd ..
 ./autogen.sh
 export ORIGIN=\$ORIGIN
 if [ `uname` == Darwin ]; then
-    ./configure LDFLAGS='-Wl,-rpath,$$ORIGIN/../muscle/lib'
+    ./configure LDFLAGS='-Wl,-rpath,$$ORIGIN/../muscle/lib' CXXFLAGS='-fopenmp' LFLAGS='-fopenmp'
 else
     ./configure LDFLAGS='-Wl,-rpath,$$ORIGIN/../muscle/lib' CXXFLAGS='-fopenmp'
 fi
