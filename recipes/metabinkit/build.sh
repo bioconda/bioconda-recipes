433624430b5635db716addc214f216c9b431d7ff
Nuno Fonseca <firstname.lastname@gmail.com>
2020-06-08 03:06:10 +0100

    no osx. install glibc 2.14

--- recipes/metabinkit/build.sh
+++ recipes/metabinkit/build.sh
@@ -6,6 +6,8 @@ export CXX_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
+# install glibc 214
+conda install -p ${PREFIX} -c pwwang glibc214 -y
 # run the install script -C
 ./install.sh -i $PREFIX -C
 cp exe/* $PREFIX/bin/
