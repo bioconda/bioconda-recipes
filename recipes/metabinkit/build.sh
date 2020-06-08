4d2fef56d821bd6ff3db49e5e970e40a94946f9a
Nuno Fonseca <firstname.lastname@gmail.com>
2020-06-08 23:23:04 +0100

    add blast as a dependency

--- recipes/metabinkit/build.sh
+++ recipes/metabinkit/build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-export C_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/bam"
-export CPP_INCLUDE_PATH="${PREFIX}/include"
-export CXX_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-# install glibc 214
-conda install -p ${PREFIX} -c pwwang glibc214 -y
-# run the install script -C
+# run the install script with -C (conda mode)
 ./install.sh -i $PREFIX -C
 cp exe/* $PREFIX/bin/
