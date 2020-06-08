8d838c1c7f8921f74c55dab80c0978e69d249bd4
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-08 10:09:09 -0500

    debug

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -9,7 +9,8 @@ mkdir -p  "$PREFIX/bin"
 #>&2 ls ${CONDA_PREFIX}/include/libMUSCLE-3.7/*/*
 #>&2 echo $LD_LIBRARY_PATH
 #export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:$LD_LIBRARY_PATH
->&2 echo ${CONDA_PREFIX}/include
+export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
+>&2 ls ${CONDA_PREFIX}/include
 ./configure CXXFLAGS='-I${CONDA_PREFIX}/include -fopenmp' --with-libmuscle=${CONDA_PREFIX}/include/libMUSCLE-3.7 || true
 >&2 cat config.log
 make LDADD='-lMUSCLE-3.7' 
