130dd65442b1adf9e284695beb0ea08bdf8356ae
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2020-06-09 21:38:13 +0200

    Removed zlib flags

--- recipes/ryuto/build.sh
+++ recipes/ryuto/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Ensure we run successfully using either conda-forge or defaults ncurses
-# (unlike other platforms, the latter does not automatically pull in libtinfo)
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
+#export CFLAGS="$CFLAGS -I$PREFIX/include"
+#export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 ./configure --prefix=$PREFIX --with-htslib="$PREFIX" --with-zlib="$PREFIX"
 make
