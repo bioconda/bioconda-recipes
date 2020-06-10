7383daffd5950e9902522a01489001c1a42d080a
James A. Fellows Yates <jfy133@gmail.com>
2020-06-10 14:25:56 +0200

    Bump MultiVCFAnalyzer JAR version

--- recipes/multivcfanalyzer/multivcfanalyzer.py
+++ recipes/multivcfanalyzer/multivcfanalyzer.py
@@ -11,7 +11,7 @@ import os
 import sys
 import subprocess
 from os import access, getenv, X_OK
-jar_file = 'MultiVCFAnalyzer_0-85-1.jar'
+jar_file = 'MultiVCFAnalyzer_0-85-2.jar'
 
 default_jvm_mem_opts = ['-Xms4g', '-Xmx32g']
 
