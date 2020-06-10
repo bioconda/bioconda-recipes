6f6a34f1a7699b208866ff5609751188d896d623
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2020-06-10 11:39:51 +0200

    Updated config boost path

--- recipes/ryuto/build.sh
+++ recipes/ryuto/build.sh
@@ -3,6 +3,6 @@
 #export CFLAGS="$CFLAGS -I$PREFIX/include"
 #export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-./configure --prefix=$PREFIX --with-htslib="$PREFIX" --with-zlib="$PREFIX"
+./configure --prefix=$PREFIX --with-htslib="$PREFIX" --with-zlib="$PREFIX" --with-boost="$PREFIX"
 make
 make install
