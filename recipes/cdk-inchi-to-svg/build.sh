38d511d7241b0cd859179a64cd894b566dcb8cbd
culpinnis <culpinnis@gmail.com>
2020-06-11 19:16:09 +0200

    Applied bioconda guidelines

--- recipes/cdk-inchi-to-svg/build.sh
+++ recipes/cdk-inchi-to-svg/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd cdk-inchi-to-svg 
 mvn package 
-for i in $(find . -name "*with-dependencies*jar"); do install -m755 "$i" ${PREFIX}/bin/cdk-inchi-to-svg.jar; done
-echo "#!/bin/bash" > ${PREFIX}/bin/start-cdk-inchi-to-svg.sh
-echo "java -jar \`which cdk-inchi-to-svg.jar\` \"\$@\"" >> ${PREFIX}/bin/start-cdk-inchi-to-svg.sh
-chmod +x ${PREFIX}/bin/start-cdk-inchi-to-svg.sh
+mkdir -p $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+for i in $(find . -name "*with-dependencies*jar"); do install -m755 "$i" $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/cdk-inchi-to-svg.jar; done
+#Create wrapper script
+echo "#!/bin/bash" > ${PREFIX}/bin/cdk-inchi-to-svg
+echo "java -jar $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/cdk-inchi-to-svg.jar \"\$@\"" >> ${PREFIX}/bin/cdk-inchi-to-svg
+chmod +x ${PREFIX}/bin/cdk-inchi-to-svg
