4f90d622b37ca0b93f118fa768d1fefd26ea51da
Björn Grüning <bjoern@gruenings.eu>
2020-06-12 15:39:15 +0200

    Update build.sh

--- recipes/lofreq/build.sh
+++ recipes/lofreq/build.sh
@@ -3,6 +3,11 @@ set -eu -o pipefail
 
 CFLAGS="${CFLAGS} -Og -g -pedantic"
 
+pushd src/lofreq/
+rm vcf.c
+wget https://raw.githubusercontent.com/CSB5/lofreq/master/src/lofreq/vcf.c
+popd
+
 ./configure --with-htslib=${PREFIX}/lib  --prefix=${PREFIX}
 
 make
