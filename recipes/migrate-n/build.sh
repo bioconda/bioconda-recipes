0f9b09a9a56f606b221ac133f948f8f59ea1da9a
Devon Ryan <dpryan79@users.noreply.github.com>
2020-06-12 10:13:34 +0200

    Update build.sh

--- recipes/migrate-n/build.sh
+++ recipes/migrate-n/build.sh
@@ -8,8 +8,9 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
+export CC="${CC} -L${PREFIX}/lib"
 ./configure
-make prefix=${PREFIX}
+make prefix=${PREFIX} CC="${CC}"
 
 mkdir -p $PREFIX/bin
 
