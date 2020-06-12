830c5049e2122ced56af500a3164697a82090c66
Baraa Orabi <baraaorabi@gmail.com>
2020-06-11 17:04:51 -0700

    Update to v0.3.3 with extra execuatble added

--- recipes/calib/build.sh
+++ recipes/calib/build.sh
@@ -8,3 +8,12 @@ make
 mkdir -p ${PREFIX}/bin
 cp -f calib ${PREFIX}/bin
 calib --help
+
+mkdir consensus/spoa_v1.1.3/build
+cd consensus/spoa_v1.1.3/build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd ../..
+g++ calib_cons.cc -std=c++11 -O3 -pthread -Ispoa_v1.1.3/include/ -Lspoa_v1.1.3/build/lib/ -lspoa -o calib_cons
+cp -f calib_cons ${PREFIX}/bin
+calib_cons --help
