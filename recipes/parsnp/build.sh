0e205a89dde94b3dfcdbdd38e9054163d335c405
Bryce Kille <brycekille@gmail.com>
2020-06-12 02:58:25 -0500

    [ci skip] Merge PR 22625
    Merge PR #22625, commits were: 
     * Update meta.yaml
     * Update build.sh
     * Building parsnp requires muscle library
     * Update meta.yaml
     * Update meta.yaml
     * extra muscle not needed
     * needed libmuscle in build after all
     * Update python req and printing

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -6,8 +6,8 @@ mkdir -p  "$PREFIX/bin"
 
 
 ./autogen.sh
-export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
-./configure CXXFLAGS='-I${CONDA_PREFIX}/include -fopenmp' --with-libmuscle=${CONDA_PREFIX}/include/libMUSCLE-3.7
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+./configure CXXFLAGS='-I${PREFIX}/include -fopenmp' --with-libmuscle=${PREFIX}/include/libMUSCLE-3.7
 make LDADD='-lMUSCLE-3.7' 
 make install
 
@@ -16,6 +16,5 @@ cp parsnp $PREFIX/bin
 cp Parsnp.py $PREFIX/bin
 cp template.ini $PREFIX/bin
 cp -R bin $PREFIX/bin 
-cp -R muscle $PREFIX/bin 
 cp -R examples $PREFIX/bin
 
