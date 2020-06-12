a47dbdd191233aff8a9ada2f66f39c24c0847b6c
Devon Ryan <dpryan79@users.noreply.github.com>
2020-06-12 09:32:08 +0200

    Update build.sh

--- recipes/migrate-n/build.sh
+++ recipes/migrate-n/build.sh
@@ -2,13 +2,13 @@
 
 
 cd src
-
-./configure
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
+./configure
 make
 
 mkdir -p $PREFIX/bin
