3e03bbd67d2178f7a5501965937272ccb917179c
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-06-12 21:03:56 +0200

    + try with strict conda g++ env

--- recipes/crop/build.sh
+++ recipes/crop/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#strictly use anaconda build environment
+export CXX=${PREFIX}/bin/g++
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
 # compile crop 
-make LDFLAGS="$LDFLAGS" CPP=$CXX
+make CPP=$CXX
 
 # cp executables
 mkdir -p "$PREFIX/bin"
