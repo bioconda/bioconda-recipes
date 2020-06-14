dba64f2c1c6c4d079ddb40318f203d74246d4a57
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-14 15:57:26 -0500

    back to packages ):

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -3,18 +3,5 @@
 
 mkdir -p  "$PREFIX/bin"
 
-
-
-./autogen.sh
-export CPLUS_INCLUDE_PATH=$PREFIX/include
-./configure CXXFLAGS='-I${PREFIX}/include -fopenmp' --with-libmuscle=${PREFIX}/include/libMUSCLE-3.7
-make LDADD='-lMUSCLE-3.7' 
-make install
-
-
-cp parsnp $PREFIX/bin 
-cp Parsnp.py $PREFIX/bin
-cp template.ini $PREFIX/bin
-cp -R bin $PREFIX/bin 
-cp -R examples $PREFIX/bin
+cp * $PREFIX/bin
 
