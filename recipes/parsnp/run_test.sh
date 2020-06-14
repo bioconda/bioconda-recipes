02882d504f68618228359da41a159086fc7ea947
Bryce Lorenz Kille <brycekille@gmail.com>
2020-06-14 15:02:31 -0500

    parallel testing

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -5,3 +5,5 @@ tar -xzvf mers_examples.tar.gz
 parsnp -V 
 parsnp -g mers_virus/ref/England1.gbk -d mers_virus/genomes/*.fna -C 1000 -c -o test --verbose --use-fasttree
 parsnp -r ! -d mers_virus/genomes/*.fna -o test2 --verbose
+parsnp -g mers_virus/ref/England1.gbk -d mers_virus/genomes/*.fna -p 5 -C 1000 -c -o test --verbose --use-fasttree
+parsnp -r ! -d mers_virus/genomes/*.fna -o test2 --verbose -p 5
