7844c07517f7844a291ffcec365605e21596cebd
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-06-15 12:07:32 +0200

    * yeat another hack

--- recipes/crop/build.sh
+++ recipes/crop/build.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-#strictly use anaconda build environment
-export CXX=${PREFIX}/bin/g++
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-
 # compile crop 
-make CPP=$CXX
+$HACK=$CXX" -I${PREFIX}/include"
+make CXX=$HACK
 
 # cp executables
 mkdir -p "$PREFIX/bin"
