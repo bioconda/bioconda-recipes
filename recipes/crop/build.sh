292e64248896593ca9da6e79d20e7e7827ab70b5
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-06-15 16:10:21 +0200

    *yet

--- recipes/crop/build.sh
+++ recipes/crop/build.sh
@@ -4,7 +4,7 @@ export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 
-#export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
 
 # compile crop 
 make  CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/gsl" LDFLAGS="$LDFLAGS" CXX=$CXX
