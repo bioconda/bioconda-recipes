0e0112a50fd4e894235863b8c9b266d7dc2a0983
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-06-15 13:49:02 +0200

    * yet

--- recipes/crop/build.sh
+++ recipes/crop/build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-export INCLUDE_PATH=$PREFIX/include
-export LIBRARY_PATH=$PREFIX/lib
+export INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
-export C_INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
 
 # compile crop 
-echo "$INCLUDE_PATH"
-echo "$LIBRARY_PATH"
-echo "$C_INCLUDE_PATH"
-echo "$CPLUS_INCLUDE_PATH"
-echo "$CXX"
+echo $PREFIX
+echo $INCLUDE_PATH
+echo $LIBRARY_PATH
+echo $C_INCLUDE_PATH
+echo $CPLUS_INCLUDE_PATH
+echo $CXX
 
-make CXX=$CXX
+make CXX=$CXX 
 
 # cp executables
-mkdir -p "$PREFIX/bin"
-cp -pf CROPLinux "$PREFIX/bin/"
+mkdir -p $PREFIX/bin
+cp -pf CROPLinux $PREFIX/bin
