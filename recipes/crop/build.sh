fb9f43c7d2a13d1ec731d9bedf4c3fe3b8a07e13
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-06-15 14:03:02 +0200

    * yet

--- recipes/crop/build.sh
+++ recipes/crop/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+#export INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
+#export C_INCLUDE_PATH=${PREFIX}/include
+#export CPLUS_INCLUDE_PATH=${PREFIX}/include
 
 # compile crop 
-echo $PREFIX
-echo $INCLUDE_PATH
-echo $LIBRARY_PATH
-echo $C_INCLUDE_PATH
-echo $CPLUS_INCLUDE_PATH
+#echo $PREFIX
+#echo $INCLUDE_PATH
+#echo $LIBRARY_PATH
+#echo $C_INCLUDE_PATH
+#echo $CPLUS_INCLUDE_PATH
 echo $CXX
 
-make CXX=$CXX 
+make CPPFLAGS="-I$PREFIX/include -I$PREFIX/include/lgsl" LDFLAGS="$LDFLAGS" CXX=$CXX
 
 # cp executables
 mkdir -p $PREFIX/bin
