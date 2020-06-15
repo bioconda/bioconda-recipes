bioconda/bioconda-recipes

commit 5784c5b2476f1b704858e87f979691448268ebe8

[ci skip] Merge PR 22634
Merge PR #22634, commits were: 
 * disable debug
 * remove test
 * foo
 * crashing lofreq

recipes/lofreq/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-./configure --with-htslib=system --prefix=${PREFIX}
+./configure --with-htslib=${PREFIX}/lib  --prefix=${PREFIX}
+
 make
 make install

recipes/lofreq/meta.yaml
@@ -7,19 +7,21 @@ source:
   url: https://raw.githubusercontent.com/CSB5/lofreq/v{{ version }}/dist/lofreq_star-{{ version }}.tar.gz
   sha256: e0c52ef01ded39e7c2440b097cce11ae278df23d9fca288051e21728b281f082
 build:
-  number: 2
+  number: 3
 requirements:
   build:
-    - {{ compiler('cxx') }}
+    - {{ compiler('c') }}
   host:
     - python
     - htslib
+    - zlib
   run:
     - python
     - samtools
 test:
   commands:
-    - 'lofreq version'
+    - lofreq version
+
 about:
   home: http://csb5.github.io/lofreq/
   license: MIT

commit 80148b17fc1e3c9b23da31b7a7fc6df9f7e41aba

[ci skip] Merge PR 22694
Merge PR #22694, commits were: 
 * Fix --with-htslib, randomly broken in PR #22634

recipes/lofreq/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-./configure --with-htslib=${PREFIX}/lib  --prefix=${PREFIX}
-
+./configure --with-htslib=system --prefix=${PREFIX}
 make
 make install

recipes/lofreq/meta.yaml
@@ -7,9 +7,9 @@ package:
 source:
   url: https://raw.githubusercontent.com/CSB5/lofreq/v{{ version }}/dist/lofreq_star-{{ version }}.tar.gz
   sha256: 43028af07faa23c7ec0e167855492ae7bd31c4e7f8158114e51ec12aba5fd184
- 
+
 build:
-  number: 0
+  number: 1
 
 requirements:
   build: