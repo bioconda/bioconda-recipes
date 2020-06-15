e9141505709623fc3bfe31e7893a669fd96715be
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-06-15 12:43:53 +0200

    * echo time

--- recipes/crop/build.sh
+++ recipes/crop/build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+export INCLUDE_PATH=$PREFIX/include
+export LIBRARY_PATH=$PREFIX/lib
+
 export C_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include
 
 # compile crop 
+echo "$INCLUDE_PATH"
+echo "$LIBRARY_PATH"
+echo "$C_INCLUDE_PATH"
+echo "$CPLUS_INCLUDE_PATH"
+echo "$CXX"
+
 make CXX=$CXX
 
 # cp executables
