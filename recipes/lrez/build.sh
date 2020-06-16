f9ef1916982523555cded93ee8f4ebbe93c1beda
fabrice legeai <flegeai@irisa.fr>
2020-06-16 18:30:23 +0200

    First draft or recipe for lrez

---
+++ recipes/lrez/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export HTSlib=${PREFIX}/htslib_1.10.2
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make -j2
+
+mkdir -p $PREFIX/bin/
+
+cp build/tools/Compare ${PREFIX}/bin/
+chmod +x $PREFIX/bin/Compare
+
+cp build/tools/BamExtractor ${PREFIX}/bin/
+chmod +x $PREFIX/bin/BamExtractor
+
+cp scripts/reads_bx_sqlite3.py ${PREFIX}/bin/
+chmod +x $PREFIX/bin/reads_bx_sqlite3.py
+
+cp scripts/idx_bx_sqlite3.py ${PREFIX}/bin/
+chmod +x $PREFIX/bin/idx_bx_sqlite3.py
