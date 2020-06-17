efea6b875ea47ea3ceec3c2319e991ae77ede586
veitveit <veitveit@gmail.com>
2020-06-17 11:29:45 +0200

    temporary test

--- recipes/polystest/run_app.sh
+++ recipes/polystest/run_app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
-#pathname=$(readlink -f ./)
-echo $0
 pathname=$(dirname $0)
+pathname=$(readlink -f $pathname)
 echo $pathname
 ls $pathname
 R -e "shiny::runApp(\"$pathname\", port=3838)"
