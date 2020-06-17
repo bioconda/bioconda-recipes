a48d7b0574e66d51dd37eb64a12b85c8f2a574d2
Roberto Vera Alvarez <veraalva@ncbi.nlm.nih.gov>
2020-06-17 08:58:27 -0400

    Updating blast build to remove -dbapi

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -30,7 +30,7 @@ LIB_INSTALL_DIR=$PREFIX/lib/ncbi-blast+
 # -makefile-auto-update: no rebuild of makefile (one time build)
 # flat-makefile: use single makefile
 # -caution: disable configure script warnings
-# -dbapi: don't build database connectivity libs
+# -dbapi: don't build database connectivity libs <= configure: WARNING: --with(out)-dbapi is deprecated
 # -lzo: don't add lzo support
 # runpath: set runpath for installed $PREFIX location
 # hard-runpath: disable new dtags (disallow LD_LIBRARY_PATH override on Linux)
@@ -54,7 +54,6 @@ export AR="${AR} rcs"
     --without-makefile-auto-update \
     --with-flat-makefile \
     --without-caution \
-    --without-dbapi \
     --without-lzo \
     --with-hard-runpath \
     --with-runpath=$LIB_INSTALL_DIR \
@@ -71,9 +70,9 @@ export AR="${AR} rcs"
 
 apps="blastp.exe blastn.exe blastx.exe tblastn.exe tblastx.exe psiblast.exe"
 apps="$apps rpsblast.exe rpstblastn.exe makembindex.exe segmasker.exe"
-apps="$apps dustmasker.exe windowmasker.exe deltablast.exe makeblastdb.exe" 
+apps="$apps dustmasker.exe windowmasker.exe deltablast.exe makeblastdb.exe"
 apps="$apps blastdbcmd.exe blastdb_aliastool.exe convert2blastmask.exe"
-apps="$apps blastdbcheck.exe makeprofiledb.exe blast_formatter.exe"
+apps="$apps blastdbcheck.exe makeprofiledb.exe blast_formatter.exe rpsbproc.exe"
 cd ReleaseMT
 
 # The "datatool" binary needs the libs at build time, create
