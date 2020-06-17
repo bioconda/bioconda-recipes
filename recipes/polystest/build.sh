86fb7d824f56a5afd4501ae65477df1463ea7c91
veitveit <veitveit@gmail.com>
2020-06-17 06:58:25 +0200

    new version

--- recipes/polystest/build.sh
+++ recipes/polystest/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # not sure whether needed (and will fail due to missing R-files):
-#mkdir -p $PREFIX/bin
-#ln -s run_app.sh $PREFIX/bin
-#chmod 0755 "${PREFIX}/bin/run_app.sh"
-chmod 0755 run_app.sh
+mkdir -p $PREFIX/bin
+cp run_app.sh $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/run_app.sh"
+
 
