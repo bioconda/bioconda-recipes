90ff397ca4eb180fb19f5631a0b02b5ce1bd4876
Felix Mölder <felix.moelder@uni-due.de>
2020-06-17 16:12:30 +0200

    Use binaries

--- recipes/razers3/build.sh
+++ recipes/razers3/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make razers3
-cp bin/razers3 $PREFIX/bin
+
+cp bin/razers3 $PREFIX/bin/razers3 && chmod a+x $PREFIX/bin/razers3
