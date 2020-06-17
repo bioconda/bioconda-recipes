a6d7059d44ae52a25c33a9b40c0dd9ab26288235
fabrice legeai <flegeai@irisa.fr>
2020-06-17 15:08:48 +0200

    Change the copy of the binaries

--- recipes/lrez/build.sh
+++ recipes/lrez/build.sh
@@ -5,17 +5,19 @@ mkdir build
 cd build
 cmake ..
 make -j2
+cd ..
 
 mkdir -p $PREFIX/bin/
 
-cp $PREFIX/build/tools/Compare ${PREFIX}/bin/
+
+cp build/tools/Compare ${PREFIX}/bin/
 chmod +x $PREFIX/bin/Compare
 
-cp $PREFIX/build/tools/BamExtractor ${PREFIX}/bin/
+cp build/tools/BamExtractor ${PREFIX}/bin/
 chmod +x $PREFIX/bin/BamExtractor
 
-cp $PREFIX/scripts/reads_bx_sqlite3.py ${PREFIX}/bin/
+cp scripts/reads_bx_sqlite3.py ${PREFIX}/bin/
 chmod +x $PREFIX/bin/reads_bx_sqlite3.py
 
-cp $PREFIX/scripts/idx_bx_sqlite3.py ${PREFIX}/bin/
+cp scripts/idx_bx_sqlite3.py ${PREFIX}/bin/
 chmod +x $PREFIX/bin/idx_bx_sqlite3.py
