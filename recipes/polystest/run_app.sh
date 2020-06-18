015336bf691d7f931858dfd34467dc2e22c613ce
veitveit <veitveit@gmail.com>
2020-06-18 12:28:41 +0200

    trying to get the right pathnamne

--- recipes/polystest/run_app.sh
+++ recipes/polystest/run_app.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
+echo $0
 pathname=$(dirname $0)
 echo $pathname
 pathname=$(readlink -f $pathname)
 echo $pathname
-ls $pathname
+#ls $pathname
 R -e "shiny::runApp(\"$pathname\", port=3838)"
 
