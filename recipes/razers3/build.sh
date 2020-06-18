39e94b57662e8f1aa21930ea8034cf8d61bd4d8d
Marcel Bargull <marcel.bargull@udo.edu>
2020-06-18 21:28:19 +0200

    razers3: try macOS source build again

--- recipes/razers3/build.sh
+++ recipes/razers3/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-if [ $(uname -s) = "Darwin" ]; then
-    cp bin/razers3 $PREFIX/bin/razers3 && chmod a+x $PREFIX/bin/razers3
-else
-    mkdir -p build
-    cd build
-    cmake .. -DCMAKE_BUILD_TYPE=Release
-    make razers3
-    cp bin/razers3 $PREFIX/bin
-fi
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make razers3
+cp bin/razers3 $PREFIX/bin
