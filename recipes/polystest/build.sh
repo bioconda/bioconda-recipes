8fcb0913fcb238d11e8f7570956e01aef4a67369
veitveit <veitveit@gmail.com>
2020-06-18 20:29:08 +0200

    trying to get the right pathnamne

--- recipes/polystest/build.sh
+++ recipes/polystest/build.sh
@@ -10,7 +10,7 @@ ls -l $outdir
 mkdir -p $PREFIX/bin
 printf '#!/bin/bash\n' > $PREFIX/bin/run_app.sh
 printf 'cd $PREFIX\n' >> $PREFIX/bin/run_app.sh
-printf "cd share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM\nR -e \"shiny::runApp(\"./\", port=3838)\"" >> $PREFIX/bin/run_app.sh
+printf "cd share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM\nR -e \"shiny::runApp(port=3838)\"" >> $PREFIX/bin/run_app.sh
 #cp $RECIPE_DIR/run_app.sh $outdir/
 #ln -s $outdir/run_app.sh $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/run_app.sh"
