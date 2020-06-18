695ad5b58d9d5f63a4d95d735e4b35c2d2efc450
Felix Mölder <felix.moelder@uni-due.de>
2020-06-18 08:26:10 +0200

    Building for linux only

--- recipes/razers3/build.sh
+++ recipes/razers3/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-
-cp bin/razers3 $PREFIX/bin/razers3 && chmod a+x $PREFIX/bin/razers3
+if [ $(uname -s) = "Darwin" ]; then
+    cp bin/razers3 $PREFIX/bin/razers3 && chmod a+x $PREFIX/bin/razers3
+else
+    mkdir -p build
+    cd build
+    cmake .. -DCMAKE_BUILD_TYPE=Release
+    make razers3
+    cp bin/razers3 $PREFIX/bin
+if
