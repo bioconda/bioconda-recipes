9fbba76db3bd9549745d1de1c6529b6ae8133995
veitveit <veitveit@gmail.com>
2020-06-18 20:33:51 +0200

    trying to get the right pathnamne

--- recipes/polystest/build.sh
+++ recipes/polystest/build.sh
@@ -2,17 +2,8 @@
 
 
 # move all files to outdir and link into it by bin executor
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -R * $outdir/
-ls -l $outdir
-mkdir -p $PREFIX/bin
-printf '#!/bin/bash\n' > $PREFIX/bin/run_app.sh
-printf 'cd $PREFIX\n' >> $PREFIX/bin/run_app.sh
-printf "cd share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM\nR -e \"shiny::runApp(port=3838)\"" >> $PREFIX/bin/run_app.sh
-#cp $RECIPE_DIR/run_app.sh $outdir/
-#ln -s $outdir/run_app.sh $PREFIX/bin
-chmod 0755 "${PREFIX}/bin/run_app.sh"
+cp -R * $PREFIX/bin
+
 
 
