cb9a9f47a29babb03809f7afaab71836439788df
Marcel Bargull <marcel.bargull@udo.edu>
2020-06-19 01:26:49 +0200

    razers3: try/debug -mmacosx-version-min=10.13

--- recipes/razers3/build.sh
+++ recipes/razers3/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$(uname)" = 'Darwin' ] ; then
+    export CPPFLAGS="${CPPFLAGS//-mmacosx-version-min=10.9/-mmacosx-version-min=10.13}"
+fi
+
 mkdir -p $PREFIX/bin
 mkdir -p build
 cd build
