fd50f25f8053d3790910eb7f842a061c8634c96e
veitveit <veitveit@gmail.com>
2020-06-18 06:57:38 +0200

    trying to get the right pathnamne

--- recipes/polystest/build.sh
+++ recipes/polystest/build.sh
@@ -8,7 +8,7 @@ mkdir -p $PREFIX/bin
 cp -R * $outdir/
 ls -l $outdir
 mkdir -p $PREFIX/bin
-cp $REPODIR $outdir/
+cp $REPODIR/run_app.sh $outdir/
 ln -s $outdir/run_app.sh $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/run_app.sh"
 
