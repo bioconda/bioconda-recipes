c75f33217b41798bff93d4a39b4ce11c7e2668da
Marcel Bargull <marcel.bargull@udo.edu>
2020-06-19 18:58:21 +0200

    Revert "more test removal"
    This reverts commit 96f1ea51fdb4d5bfff5aa2af6dfea184a8f8108f.
    
    Revert "is it a memory issue?"
    
    This reverts commit 4214a193a69d7b18eb44840ec4f0990d0f98deb3.

--- recipes/blast/run_test.sh
+++ recipes/blast/run_test.sh
@@ -14,17 +14,17 @@ makeblastdb \
   -in testdatabase.fa -parse_seqids -dbtype nucl -out testdb | \
    grep -q "added 3 sequences"
 echo PASS
-#echo -n 'Searching Database for Hits... '
-#blastn \
-#  -query test.fa -db testdb -task blastn -dust no \
-#  -outfmt "7 qseqid sseqid evalue bitscore" -max_target_seqs 6 | \
-#  grep -q " 3 hits found"
-#echo PASS
-#echo -n 'Search and Fetch An Entry From Database... '
-#blastdbcmd -db testdb -entry gnl1 -out test_query.fa
-#test "$(grep -c ">gnl1" < test_query.fa)" -eq 1
-#test "$(grep -c ">" < test_query.fa)" -eq 1
-#test "$(wc -l < test_query.fa)" -eq 10
-#echo PASS
+echo -n 'Searching Database for Hits... '
+blastn \
+  -query test.fa -db testdb -task blastn -dust no \
+  -outfmt "7 qseqid sseqid evalue bitscore" -max_target_seqs 6 | \
+  grep -q " 3 hits found"
+echo PASS
+echo -n 'Search and Fetch An Entry From Database... '
+blastdbcmd -db testdb -entry gnl1 -out test_query.fa
+test "$(grep -c ">gnl1" < test_query.fa)" -eq 1
+test "$(grep -c ">" < test_query.fa)" -eq 1
+test "$(wc -l < test_query.fa)" -eq 10
+echo PASS
 echo
 echo "ALL TESTS PASSED"
