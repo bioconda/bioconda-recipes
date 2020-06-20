ded33545ba4f44020920a3bc26f633837238da7b
Liu <liuq1@l-1-01.cm.cluster>
2020-06-20 16:20:12 -0400

    update build

--- recipes/longgf/build.sh
+++ recipes/longgf/build.sh
@@ -6,10 +6,3 @@ g++ -g -O3 -std=c++11 -I ./include -L ./lib -Wl,--enable-new-dtags,-rpath,"\$ORI
 
 cp LongGF $PREFIX/
 
-#mkdir -p $PREFIX/bin
-#
-#curl -L https://github.com/attractivechaos/k8/releases/download/0.2.5/k8-0.2.5.tar.bz2 | tar jxf - k8-0.2.5/k8-`uname -s`
-#cp k8-0.2.5/k8-* $PREFIX/bin/k8
-#
-#make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib"
-#cp minimap2 misc/paftools.js $PREFIX/bin
