5272513adf55044df62b148bf64499e05ea9130f
Liu <liuq1@l-1-01.cm.cluster>
2020-06-21 19:00:16 -0400

    fixed mulled-test

--- recipes/longgf/build.sh
+++ recipes/longgf/build.sh
@@ -5,7 +5,8 @@ cd bin
 
 rm -dr lib include
 #$GXX -g -O3 -std=c++11 -I $PREFIX/include -L $PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
-$GXX -g -Wall -O3 -std=c++11 -I$PREFIX/include -L$PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
+#$GXX -g -Wall -O3 -std=c++11 -I$PREFIX/include -L$PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
+$GXX -Wall -O3 -std=c++11 -I$PREFIX/include -L$PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
 #$GXX -g -O3 -std=c++11 $CFLAGS $LDFLAGS -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
 
 cp LongGF $PREFIX/bin/
