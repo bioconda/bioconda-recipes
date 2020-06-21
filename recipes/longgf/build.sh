40c3acfdd350c8999aff7b52bed92fca64ac273d
Liu <liuq1@l-1-01.cm.cluster>
2020-06-20 21:39:18 -0400

    add and test LongGF

--- recipes/longgf/build.sh
+++ recipes/longgf/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd LongGF/bin
+cd bin
 
-g++ -g -O3 -std=c++11 -I ./include -L ./lib -Wl,--enable-new-dtags,-rpath,"\$ORIGIN"/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c -Wl,--no-as-needed
+g++ -g -O3 -std=c++11 -I $PREFIX/include -L $PREFIX/lib -Wl,--enable-new-dtags -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c -Wl,--no-as-needed
 
 cp LongGF $PREFIX/
+
 
