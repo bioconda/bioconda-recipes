fab8bdf9f52c93d1b3c78cd8837407faeb6c262a
Liu <liuq1@l-1-01.cm.cluster>
2020-06-20 23:42:44 -0400

    fixed test

--- recipes/longgf/build.sh
+++ recipes/longgf/build.sh
@@ -2,7 +2,8 @@
 
 cd bin
 
-g++ -g -O3 -std=c++11 -I $PREFIX/include -L $PREFIX/lib -Wl,--enable-new-dtags -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c -Wl,--no-as-needed
+rm -dr lib include
+g++ -g -O3 -std=c++11 -I $PREFIX/include -L $PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
 
 cp LongGF $PREFIX/
 
