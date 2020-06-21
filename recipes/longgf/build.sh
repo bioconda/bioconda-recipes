ea27e151bb368dcbdedda741df6b29cd4aa7ddae
Liu <liuq1@l-1-01.cm.cluster>
2020-06-21 15:12:11 -0400

    fixed g++ issue

--- recipes/longgf/build.sh
+++ recipes/longgf/build.sh
@@ -3,7 +3,8 @@
 cd bin
 
 rm -dr lib include
-gcc -g -O3 -xc++ -lstdc++ -shared-libgcc -std=c++11 -I $PREFIX/include -L $PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
+#GCC -g -O3 -xc++ -lstdc++ -shared-libgcc -std=c++11 -I $PREFIX/include -L $PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
+$GXX -g -O3 -std=c++11 -I $PREFIX/include -L $PREFIX/lib -lhts -o LongGF _com_fun_.c _gtf_struct_.c get_gfFrombam.c
 
 cp LongGF $PREFIX/
 
