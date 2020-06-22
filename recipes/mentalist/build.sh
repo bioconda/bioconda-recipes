a277f9686fac6074ea9abb23f7a2ae2aada10323
Nolan Woods <nolan_w@sfu.ca>
2020-06-22 14:05:17 -0700

    Mentalist: Move environment management upstream to julia package. Bump build number.

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -5,16 +5,13 @@
 [[ -z $REQUESTS_CA_BUNDLE && ${REQUESTS_CA_BUNDLE+x} ]] && unset REQUESTS_CA_BUNDLE
 [[ -z $SSL_CERT_FILE && ${SSL_CERT_FILE+x} ]] && unset SSL_CERT_FILE
 
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-echo "#!/usr/bin/env sh\nexport LAST_JULIA_DEPOT_PATH=\$JULIA_DEPOT_PATH\nexport JULIA_DEPOT_PATH='${PREFIX}/deps/'" > "${PREFIX}/etc/conda/activate.d/mentalist-activate.sh"
-echo "#!/usr/bin/env sh\nexport LAST_JULIA_DEPOT_PATH=\nexport JULIA_DEPOT_PATH=\$LAST_JULIA_DEPOT_PATH" > "${PREFIX}/etc/conda/deactivate.d/mentalist-deactivate.sh"
-chmod +x "${PREFIX}/activate.d/*" "${PREFIX}/deactivate.d/*"
-
 cp -r "${SRC_DIR}/src/*.jl" "${PREFIX}/bin"
 cp -r "${SRC_DIR}/scripts" "${PREFIX}"
 cp "${SRC_DIR}/*.toml" "${PREFIX}/"
-echo "#!/usr/bin/env sh\nexec julia --project='${PREFIX}' -- '${PREFIX}/src/MentaLiST.jl' \"\$@\"" > "${PREFIX}/bin/mentalist"
+cat <<EOF > "${PREFIX}/bin/mentalist"
+#!/usr/bin/env sh
+exec julia --project='${PREFIX}' -- '${PREFIX}/src/MentaLiST.jl' "\$@"
+EOF
 chmod +x "${PREFIX}/bin/mentalist"
 
-JULIA_DEPOT_PATH="${PREFIX}/deps/" julia --project="${PREFIX}" -e 'using Pkg; Pkg.instantiate()'
+julia --project="${PREFIX}" -e 'using Pkg; Pkg.instantiate()'
