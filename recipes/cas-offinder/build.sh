31f4023db32f28404bc91fac9c547c213fc55ac0
Devon Ryan <devon.ryan@genedata.com>
2020-06-23 14:05:43 +0200

    should start compiling

--- recipes/cas-offinder/build.sh
+++ recipes/cas-offinder/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [[ $OSTYPE == "linux-gnu" ]]; then
-	cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL.so -DOPENCL_INCLUDE_DIR=${PREFIX}/include
-elif [[ $OSTYPE == "darwin"* ]]; then
-	cmake -Wno-dev -G "Unix Makefiles"
-fi
+cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL.so -DOPENCL_INCLUDE_DIR=${PREFIX}/include -DCMAKE_MAKE_PROGRAM=make -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
 make
 mkdir -p $PREFIX/bin
 cp cas-offinder $PREFIX/bin
