341c79b1ffb247faa87f2e6dffe68f7f002dbf4b
Devon Ryan <dpryan79@users.noreply.github.com>
2020-06-23 16:32:00 +0200

    Update build.sh

--- recipes/cas-offinder/build.sh
+++ recipes/cas-offinder/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ls -l  # debug
 cmake -Wno-dev -G "Unix Makefiles" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL.so -DOPENCL_INCLUDE_DIR=${PREFIX}/include -DCMAKE_MAKE_PROGRAM=make -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
 make
 mkdir -p $PREFIX/bin
