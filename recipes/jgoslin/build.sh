40174d21ba02994d239c223af8717f0b4b2c6cff
Nils Hoffmann <nils.hoffmann@isas.de>
2020-06-24 12:49:52 +0200

    Added recipe for jgoslin 1.1.2

---
+++ recipes/jgoslin/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+cp -R * $outdir/
+cp $RECIPE_DIR/jgoslin.py $outdir/jgoslin
+ls -l $outdir
+ln -s $outdir/jgoslin $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/jgoslin"
