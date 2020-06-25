a016a28e8fe63e84be7324f04bb798e37590b79b
Devon Ryan <dpryan79@users.noreply.github.com>
2020-06-25 13:59:59 +0200

    Update build.sh

--- recipes/deepmod/build.sh
+++ recipes/deepmod/build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-$PYTHON setup.py install
-
-
-#ls $PREFIX
-#ls $PREFIX/bin/*.py
-#ls $SP_DIR
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
 
 mkdir $SP_DIR/DeepMod
 cp -r train_deepmod $SP_DIR/DeepMod/
-
-#ls $SP_DIR/DeepMod/train_deepmod
-
-
