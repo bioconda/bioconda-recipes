62f1dba3ba8e93ca91fbe302725d7cc5b9e20130
Nils Hoffmann <nils.hoffmann@isas.de>
2020-06-26 13:26:19 +0200

    Adapted paths, zip src is already pulled up one level by conda.

--- recipes/jgoslin/build.sh
+++ recipes/jgoslin/build.sh
@@ -4,9 +4,9 @@ echo -e "Running in directory '$(pwd)'"
 ls -l
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 #mv "download_file?file_path=de%2Fisas%2Flipidomics%2Fjgoslin-cli%2F1.1.2%2Fjgoslin-cli-1.1.2.jar" "$PKG_NAME-cli-$PKG_VERSION.jar"
-zipname="$PKG_NAME-cli-$PKG_VERSION-bin.zip"
-mv "download_file?file_path=de%2Fisas%2Flipidomics%2Fjgoslin-cli%2F1.1.2%2Fjgoslin-cli-1.1.2-bin.zip" "$zipname"
-unzip "$zipname"
+#zipname="$PKG_NAME-cli-$PKG_VERSION-bin.zip"
+#mv "download_file?file_path=de%2Fisas%2Flipidomics%2Fjgoslin-cli%2F1.1.2%2Fjgoslin-cli-1.1.2-bin.zip" "$zipname"
+#unzip "$zipname"
 ls -l
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
