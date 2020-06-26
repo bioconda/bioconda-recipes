37276e5fa3b9bfec8f7a60949211edf22136b69e
Nils Hoffmann <nils.hoffmann@isas.de>
2020-06-26 10:36:16 +0200

    Setting source fn, reverted build.sh changes.

--- recipes/jgoslin/build.sh
+++ recipes/jgoslin/build.sh
@@ -8,5 +8,4 @@ cp -R * $outdir/
 cp $RECIPE_DIR/jgoslin.py $outdir/jgoslin
 ls -l $outdir
 ln -s $outdir/jgoslin $PREFIX/bin
-mv $outdir/download_file\?file_path=de%2Fisas%2Flipidomics%2Fjgoslin-cli%2F${PKG_VERSION}%2Fjgoslin-cli-${PKG_VERSION}.jar $outdir/${PKG_NAME}-${PKG_VERSION}.jar
 chmod 0755 "${PREFIX}/bin/jgoslin"
