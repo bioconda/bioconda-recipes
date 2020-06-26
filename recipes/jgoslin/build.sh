883f59caaaa0af2d1c3686711248728a992b461d
Nils Hoffmann <nils.hoffmann@isas.de>
2020-06-26 13:34:42 +0200

    Cleaning up echos

--- recipes/jgoslin/build.sh
+++ recipes/jgoslin/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
-echo -e "Running in directory '$(pwd)'"
-ls -l
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-#mv "download_file?file_path=de%2Fisas%2Flipidomics%2Fjgoslin-cli%2F1.1.2%2Fjgoslin-cli-1.1.2.jar" "$PKG_NAME-cli-$PKG_VERSION.jar"
-#zipname="$PKG_NAME-cli-$PKG_VERSION-bin.zip"
-#mv "download_file?file_path=de%2Fisas%2Flipidomics%2Fjgoslin-cli%2F1.1.2%2Fjgoslin-cli-1.1.2-bin.zip" "$zipname"
-#unzip "$zipname"
-ls -l
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 cp -R * $outdir/
@@ -15,5 +8,4 @@ cp $RECIPE_DIR/jgoslin.py $outdir/jgoslin
 ls -l $outdir
 ln -s $outdir/jgoslin $PREFIX/bin
 echo "Contents of '$outdir'"
-ls -l
 chmod 0755 "${PREFIX}/bin/jgoslin"
