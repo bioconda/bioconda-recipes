deba251606b3e0d0192176b1e50fb7e52eae12d0
Nils Hoffmann <nils.hoffmann@isas.de>
2020-06-26 10:42:51 +0200

    Reverted changes due to linting failure on fn.

--- recipes/jgoslin/build.sh
+++ recipes/jgoslin/build.sh
@@ -8,4 +8,5 @@ cp -R * $outdir/
 cp $RECIPE_DIR/jgoslin.py $outdir/jgoslin
 ls -l $outdir
 ln -s $outdir/jgoslin $PREFIX/bin
+mv download_file?file_path=de%2Fisas%2Flipidomics%2F${PKG_NAME}-cli%2F${PKG_VERSION}%2F${PKG_NAME}-cli-${PKG_VERSION}.jar ${PKG_NAME}-cli-${PKG_VERSION}.jar
 chmod 0755 "${PREFIX}/bin/jgoslin"
