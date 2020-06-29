82214424fd381e5b4245ae70e00fedeb37fe1e77
tina <15641075+tinavisnovska@users.noreply.github.com>
2020-06-29 18:21:39 +0200

    update of bioconductor-org.Hs.eg.db to fix broken links, related to issue #22806

--- recipes/bioconductor-org.hs.eg.db/post-link.sh
+++ recipes/bioconductor-org.hs.eg.db/post-link.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-FN="org.Hs.eg.db_3.11.1.tar.gz"
+FN="org.Hs.eg.db_3.11.4.tar.gz"
 URLS=(
-  "https://bioconductor.org/packages/3.11/data/annotation/src/contrib/org.Hs.eg.db_3.11.1.tar.gz"
-  "https://bioarchive.galaxyproject.org/org.Hs.eg.db_3.11.1.tar.gz"
-  "https://depot.galaxyproject.org/software/bioconductor-org.hs.eg.db/bioconductor-org.hs.eg.db_3.11.1_src_all.tar.gz"
+  "https://bioconductor.org/packages/3.11/data/annotation/src/contrib/"$FN
 )
-MD5="390e3adf5f73220baad5ab4c3472648b"
+MD5="d41d8cd98f00b204e9800998ecf8427e"
 
 # Use a staging area in the conda dir rather than temp dirs, both to avoid
 # permission issues as well as to have things downloaded in a predictable
