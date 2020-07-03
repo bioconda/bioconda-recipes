b42997a595934b815e90714946d848cd78d3c92c
Peter Beerli <beerli@me.com>
2020-07-03 08:42:53 -0400

    reorder commands in the build.sh file and added a few echo commands

--- recipes/migrate-3x/build.sh
+++ recipes/migrate-3x/build.sh
@@ -3,7 +3,6 @@
 
 cd src
 
-./configure
 
 sed -i.bak 's/#include <xlocale.h>//' data.c
 export C_INCLUDE_PATH=${PREFIX}/include
@@ -11,9 +10,9 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
+./configure
 make
-
+echo "finished Make"
 mkdir -p $PREFIX/bin
-
 cp $SRC_DIR/src/migrate-n $PREFIX/bin
-
+echo "copied migrate-n into the bin directory"
