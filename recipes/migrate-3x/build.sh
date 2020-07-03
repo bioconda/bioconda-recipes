e4dffebf528125fd641ad022aebfd1f8b38b1cb2
Peter Beerli <beerli@me.com>
2020-07-03 11:06:08 -0400

    testing

--- recipes/migrate-3x/build.sh
+++ recipes/migrate-3x/build.sh
@@ -5,16 +5,16 @@ cd src
 
 ## gcc claims that this is old-fashioned!
 sed -i.bak 's/#include <xlocale.h>//' data.c
-#export C_INCLUDE_PATH=${PREFIX}/include
-#export CPP_INCLUDE_PATH=${PREFIX}/include
-#export CPLUS_INCLUDE_PATH=${PREFIX}/include
-#export CXX_INCLUDE_PATH=${PREFIX}/include
-#export LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 echo "============="
-#echo $LIBRARY_PATH
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+echo $LIBRARY_PATH
 ./configure
-#export CFLAGS="$CFLAGS -I$PREFIX/include"
-#export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 make
 echo "finished Make"
 mkdir -p $PREFIX/bin
