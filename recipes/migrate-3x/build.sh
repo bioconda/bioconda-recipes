171df064d5caff6518f1e129c8a1b643918b6e72
Peter Beerli <beerli@me.com>
2020-07-03 11:21:33 -0400

    testing

--- recipes/migrate-3x/build.sh
+++ recipes/migrate-3x/build.sh
@@ -10,11 +10,14 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-echo "============="
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-echo $LIBRARY_PATH
+echo "@@============="
+export CFLAGS="$CFLAGS -I${PREFIX}/include"
+export LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
 ./configure
+echo "%%%%%%%%%%"
+echo $LIBRARY_PATH
+echo $LDFLAGS
+echo "%%%%%%%%%%"
 make
 echo "finished Make"
 mkdir -p $PREFIX/bin
