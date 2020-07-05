24f79210a6c4a2dff401cda75b55b984392a6518
Liu <liuq1@c-2-02.cm.cluster>
2020-07-05 19:07:15 -0400

    add and test RepeatHMM

--- recipes/repeathmm/build.sh
+++ recipes/repeathmm/build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-cd bin/scripts/UnsymmetricPairAlignment/
-make
-cd ../../../
+#cat /tmp/miniconda/miniconda/conda-bld/repeathmm_*/work/conda_build.sh
 
-$PYTHON setup.py install
+#cd bin/RepeatHMM_scripts/UnsymmetricPairAlignment/
+#make
+#cd ../../../
 
-swig -python bin/RepeatHMM_scripts/UnsymmetricPairAlignment/UnsymmetricPairAlignment.i
-python bin/RepeatHMM_scripts/UnsymmetricPairAlignment/setup.py build_ext --inplace
+#$PYTHON -m pip install . --no-deps --ignore-installed -vv
+ls 
+echo $PYTHON
+
+$PYTHON ./setup.py install
+
+#swig -python bin/RepeatHMM_scripts/UnsymmetricPairAlignment/UnsymmetricPairAlignment.i
+#python bin/RepeatHMM_scripts/UnsymmetricPairAlignment/setup.py build_ext --inplace
 
 mkdir $SP_DIR/RepeatHMM
 cp -r bin/reference_sts $SP_DIR/RepeatHMM/
