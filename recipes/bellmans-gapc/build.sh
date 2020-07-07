48e1e50272f478810c5497d5d58c83e5b1dbde78
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-07-07 19:20:55 +0200

    using different system suffices

--- recipes/bellmans-gapc/build.sh
+++ recipes/bellmans-gapc/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 export SED="sed"
-
+export SYSTEM_SUFFIX="_linux-gnu"
 ./configure --with-boost-libdir=$PREFIX/lib/ CPPFLAGS=-I${PREFIX} --prefix=$PREFIX --disable-gsltest
 
 # change compile flags if on OSX
 if [ x"$(uname)" == x"Darwin" ]; then
   export SED="gsed"
+  export SYSTEM_SUFFIX="_darwin17.0.0"
   $SED "s/^17 \-D_XOPEN_SOURCE=500 /11 /" -i src/config.mf
 fi
 
@@ -13,5 +14,5 @@ make
 make install
 # remove definition of CXX in user space config file, since this will not match users compiler path,
 # instead let conda take care of providing meaningful CXX and CC values.
-$SED "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
-$SED "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
+$SED "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config${SYSTEM_SUFFIX}.mf
+$SED "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config${SYSTEM_SUFFIX}.mf
