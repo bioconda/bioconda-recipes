e67bf657e1774cc1760c7d55038e33f0a90000ae
Stefan Janssen <stefan.m.janssen@gmail.com>
2020-07-07 18:11:50 +0200

    new version + osx compilation

--- recipes/bellmans-gapc/build.sh
+++ recipes/bellmans-gapc/build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+export SED = sed
+
 ./configure --with-boost-libdir=$PREFIX/lib/ CPPFLAGS=-I${PREFIX} --prefix=$PREFIX --disable-gsltest
+
+# change compile flags if on OSX
+if [ x"$(uname)" == x"Darwin" ]; then
+  export SED = gsed
+  $SED "s/^17 \-D_XOPEN_SOURCE=500 /11 /" -i src/config.mf
+fi
+
 make
 make install
 # remove definition of CXX in user space config file, since this will not match users compiler path,
 # instead let conda take care of providing meaningful CXX and CC values.
-sed "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
-sed "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
+$SED "s/^CXX = /#CXX = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
+$SED "s/^CC = /#CC = /" -i $PREFIX/share/gapc/config_linux-gnu.mf
