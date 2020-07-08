0721eb6eda4222e75362134dbd9f26b3e230bb8c
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2020-07-08 19:30:46 -0400

    test LDBlockShow emits its help text; remove cruft

--- recipes/ldblockshow/build.sh
+++ recipes/ldblockshow/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#export CFLAGS="$CFLAGS -I$PREFIX/include"
-#export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-#export CPATH=${PREFIX}/include
-#alternative to fix problems with zlib
-# export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
-# export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
-# export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
-
 chmod 755 ./configure
 ./configure --prefix=$PREFIX
 make
