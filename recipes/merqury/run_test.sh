70721f1fa453b06ce311727ca01eaee0dc1f5fc5
Devon Ryan <dpryan79@users.noreply.github.com>
2020-07-09 10:35:36 +0200

    Update run_test.sh

--- recipes/merqury/run_test.sh
+++ recipes/merqury/run_test.sh
@@ -5,6 +5,7 @@ MERQURY=$CONDA_PREFIX/share/merqury
 
 merqury.sh
 meryl 2>&1 | grep 'usage: meryl'
+meryl-lookup
 meryl-lookup 2>&1 | grep 'usage: meryl-lookup'
 for script in $MERQURY/**/*.R; do $script --help; done
 for jar in $MERQURY/**/*.jar; do java -jar -Xmx128m $jar; done
