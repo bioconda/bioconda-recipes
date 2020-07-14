d458067b25972054791af30c3e8d96448554ceb4
Marcel Bargull <marcel.bargull@udo.edu>
2020-07-14 22:38:44 +0200

    bulk: more build fixes

--- recipes/yaha/build.sh
+++ recipes/yaha/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-make CC="${CXX}"
-
-cp bin/yaha ${PREFIX}/bin
-
+make \
+  CC="${CC}" CPP="${CXX}" \
+  CFLAG="${CFLAGS} \$(CCFLAGS) -std=gnu99" \
+  CPPFLAGS="${CXXFLAGS} \$(CCFLAGS)" \
+  LDFLAGS+=-pthread \
+  BDIR="${PREFIX}/bin"
