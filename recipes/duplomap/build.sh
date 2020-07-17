7bf98d97b99315ef1d0be3fcf089191f5a2fbaf9
Timofey Prodanov <timofey.prodanov@gmail.com>
2020-07-17 12:28:17 +0200

    Compilation fixes

--- recipes/duplomap/build.sh
+++ recipes/duplomap/build.sh
@@ -2,6 +2,7 @@
 
 export C_INCLUDE_PATH="$BUILD_PREFIX/include"
 export LIBRARY_PATH="$BUILD_PREFIX/lib"
-export CFLAGS="-L$BUILD_PREFIX/lib -fPIC"
+export CFLAGS="-L$BUILD_PREFIX/lib"
 export RUSTFLAGS="-L$BUILD_PREFIX/lib -lz"
 ./build --install "$PREFIX"
+
