75440f5a3ab2cb801ec88f5a0352dafe735fab3f
Devon Ryan <dpryan79@users.noreply.github.com>
2020-07-20 11:29:58 +0200

    Update build.sh

--- recipes/prinseq-plus-plus/build.sh
+++ recipes/prinseq-plus-plus/build.sh
@@ -3,6 +3,9 @@ set +ex
 
 # Add zlib support
 export CFLAGS="-I$PREFIX/include"
+export CXX_INCLUDE_DIR=$PREFIX/include
+export CPP_INCLUDE_DIR=$PREFIX/include
+export CPPLUS_INCLUDE_DIR=$PREFIX/include
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
