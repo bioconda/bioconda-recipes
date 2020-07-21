d7695c16100cff14caa48ab2849ca4b53d638d97
Devon Ryan <dpryan79@users.noreply.github.com>
2020-07-21 09:46:40 +0200

    Update build.sh

--- recipes/unifrac/build.sh
+++ recipes/unifrac/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set +x
+set +ex
 export USE_CYTHON=True
 export PERFORMING_CONDA_BUILD=True
 export LIBRARY_PATH="${CONDA_PREFIX}/lib"
 export LD_LIBRARY_PATH=${LIBRARY_PATH}:${LD_LIBRARY_PATH}
 export CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include"
 
+ls -l
 if [ "$(uname)" == "Darwin" ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.12
     SED='sed -i '"'"''"'"' '
