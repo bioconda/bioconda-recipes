3b10837b32a664c7f78c02378e64c6379d853c10
Christian Brueffer <christian@brueffer.io>
2020-07-24 22:29:27 +0100

    No py2->3 conversion needed anymore.

--- recipes/hisat2/build.sh
+++ recipes/hisat2/build.sh
@@ -6,36 +6,6 @@ mv VERSION VERSION.txt  # Breaks C++20
 make CC=${CC} CPP=${CXX}
 
 
-# convert Python 2.7 scripts to 3
-if [ $PY3K -eq 1 ]
-then
-    2to3 --write \
-        extract_exons.py \
-        extract_splice_sites.py \
-        hisat2_extract_exons.py \
-        hisat2_extract_snps_haplotypes_UCSC.py \
-        hisat2_extract_snps_haplotypes_VCF.py \
-        hisat2_extract_splice_sites.py \
-        hisat2_read_statistics.py \
-        hisat2_simulate_reads.py \
-        hisatgenotype_build_genome.py \
-        hisatgenotype_extract_reads.py \
-        hisatgenotype_extract_vars.py \
-        hisatgenotype_hla_cyp.py \
-        hisatgenotype_locus.py \
-        hisatgenotype_modules/hisatgenotype_assembly_graph.py \
-        hisatgenotype_modules/hisatgenotype_typing_common.py \
-        hisatgenotype_scripts/compare_HLA_Omixon.py \
-        hisatgenotype_scripts/extract_Omixon_HLA.py \
-        hisatgenotype_scripts/hisatgenotype_HLA_genotyping_PGs.py \
-        hisatgenotype_scripts/hisatgenotype_locus_samples.py \
-        hisatgenotype_scripts/compare_HLA.py \
-        hisatgenotype_scripts/hisatgenotype_convert_codis.py \
-        hisatgenotype_scripts/hisatgenotype_extract_codis_data.py \
-        hisatgenotype_scripts/hisatgenotype_extract_cyp_data.py \
-        hisatgenotype.py
-fi
-
 # copy binaries and python scripts
 for i in \
     hisat2 \
