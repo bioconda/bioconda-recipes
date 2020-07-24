1acfab7b7d250aba6fd8c69bcd234f0fd71877ac
Marcel Bargull <marcel.bargull@udo.edu>
2020-07-24 14:02:50 +0200

    isaac4: use latest boost-cp, adjust patch

--- recipes/isaac4/build.sh
+++ recipes/isaac4/build.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-set +e
-
 export BOOST_ROOT="${PREFIX}"
-export CXXFLAGS="-std=c++11"
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
 
 mkdir build
 cd build
 ../src/configure --prefix=${PREFIX}
 make
-cat CMakeFiles/CMakeOutput.log CMakeFiles/CMakeError.log
-exit 1
 make install
