988e050394041f38d1d7d4c5c5e04a33f59a7398
Marcel Bargull <mbargull@users.noreply.github.com>
2020-07-26 01:23:46 +0200

    Bulk (#22983)
    * bulk: switch back to bioconda-common/master
    
    * bulk: source conda.sh before calling conda activate
    
    * bulk: update-pinnings (w/o python or r-base)
    
    * bulk: update-pinnings (non-variant python-dependent)
    
    * bulk: update-pinnings (reported in previous run)
    
    * bulk: try to fix/skip/blacklist failing recipes
    
    * bulk: more linux compilation fixes
    
    * bulk: more build fixes
    
    * bulk: fix recipe broken from last commit
    
    * bulk: more build fixes
    
    * bulk: more build fixes and build bumps
    
    * bulk: more Linux build bumps/fixes
    
    * bulk: osx (potential) build fixes
    
    * bulk: more osx fixes
    
    * bulk: some more osx fixes
    
    * bulk: few osx fix
    
    * bulk: maybe fix batvi and gatb on osx
    
    * bulk: osx: try to fix batvi, skip octopus
    
    * bulk: osx: skip gargammel
    
    * bulk: osx fixes
    
    * bulk: squid: add explicit -I include for bamtools
    
    * bulk: batvi: unvendor zlib
    
    * bulk: batvi: build number bump
    
    * bulk: batvi: Add 0004-Remove-Linux-specific-Set_Affinity-on-OSX.patch
    
    * bulk: squid: skip on osx
    
    * bulk: batvi: Add 0004-Remove-warning-causing-dead-code.patch
    
    * bulk: batvi: Add 0004-Remove-warning-causing-dead-code.patch

--- recipes/pb-falconc/build.sh
+++ recipes/pb-falconc/build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-set -vexu -o pipefail
+set -eu -o pipefail
 
-export base=$(pwd)
-
-pushd nim
-# inject compilers
-echo "gcc.exe = \"$CC\"" >> config/nim.cfg
-echo "gcc.linkerexe = \"$CC\"" >> config/nim.cfg
-echo "clang.exe = \"$CC\"" >> config/nim.cfg
-echo "clang.linkerexe = \"$CC\"" >> config/nim.cfg
-./build.sh
-bin/nim c koch
-./koch tools
-popd
-
-export PATH=$base/nim/bin:$PATH
 make build
 make install
