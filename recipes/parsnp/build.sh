9d389db13dc12c8efb2be2fb2bec4e200ac37f8b
Bryce Lorenz Kille <brycekille@gmail.com>
2020-07-28 16:38:11 -0500

    parsnp build

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-
 mkdir -p  "$PREFIX/bin"
-mkdir "$PREFIX/bin/bin"
+mkdir -p  "$PREFIX/bin/bin"
+
+./autogen.sh
+export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
+./configure CXXFLAGS='-I${CONDA_PREFIX}/include -fopenmp -Wl,-rpath,${CONDA_PREFIX}/pkgs/libmuscle-3.7-h470a237_1/lib -L${CONDA_PREFIX}/pkgs/libmuscle-3.7-h470a237_1/lib'
+make LDADD='-lMUSCLE-3.7' 
+#make install
 
 cp parsnp $PREFIX/bin 
+cp src/parsnp_core $PREFIX/bin/bin
 cp template.ini $PREFIX/bin
-cp bin/parsnp_core "$PREFIX/bin/bin"
+cp -R bin $PREFIX/bin 
+#cp -R muscle $PREFIX/bin 
+cp -R examples $PREFIX/bin
