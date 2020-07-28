5d4e02d7d29b6f3f1e359746e979f9fa48178fc4
lecorguille <gildas.le.corguille@gmail.com>
2020-07-28 17:28:39 +0200

    Add nemo-age 0.29.0 - fix2

--- recipes/nemo-age/build.sh
+++ recipes/nemo-age/build.sh
@@ -5,6 +5,6 @@ export LDFLAGS="-L${PREFIX}/lib/"
 mkdir -p ${PREFIX}/bin/
 
 mkdir -p bin/
-make GSL_PATH="${PREFIX}/"
+make CC=$CXX GSL_PATH="${PREFIX}/"
 make install BIN_INSTALL=${PREFIX}/bin/
 mv ${PREFIX}/bin/nemoage* ${PREFIX}/bin/nemoage
