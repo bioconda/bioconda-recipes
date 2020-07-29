16e3100720e49802fec7e4394dc7a2dee2c9451d
Marcel Bargull <marcel.bargull@udo.edu>
2020-07-29 16:07:54 +0200

    nemo-age: Fix compiler/linker flags

--- recipes/nemo-age/build.sh
+++ recipes/nemo-age/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-export LDFLAGS="-L${PREFIX}/lib/"
-
 mkdir -p ${PREFIX}/bin/
 
 mkdir -p bin/
-make CC=$CXX GSL_PATH="${PREFIX}/"
+C_OPTS="${CPPFLAGS} ${CXXFLAGS}" make CC=$CXX GSL_PATH="${PREFIX}/"
 make install BIN_INSTALL=${PREFIX}/bin/
 mv ${PREFIX}/bin/nemoage* ${PREFIX}/bin/nemoage
