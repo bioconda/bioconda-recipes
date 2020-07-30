1a974dbd8c34bb3c423a9092e0b4d9ac3450ea7e
Benjamin Linard <benjamin.linard@lirmm.fr>
2020-07-30 17:17:03 +0200

    * corrected paths in build file

--- recipes/sherpas/build.sh
+++ recipes/sherpas/build.sh
@@ -5,12 +5,12 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 
 echo `ls $SRC_DIR`
-cd $SRC_DIR/sherpas  
-mkdir -p $SRC_DIR/sherpas/release-build
-cd $SRC_DIR/sherpas/release-build
+cd $SRC_DIR  
+mkdir -p $SRC_DIR/release-build
+cd $SRC_DIR/release-build
 cmake ..
 make
 
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/sherpas/SHERPAS $PREFIX/bin
+cp $SRC_DIR/release-build/sherpas/SHERPAS $PREFIX/bin
 
