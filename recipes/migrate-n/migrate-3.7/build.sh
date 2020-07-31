2d8254d7524cab2410b51615974f983889666c60
Peter Beerli <beerli@me.com>
2020-07-31 10:57:40 -0400

    second attempt to get conda to accept a newer migrate [still need to fix the sha code for the old 3.7.2 version]

---
+++ recipes/migrate-n/migrate-3.7/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+cd src
+
+sed -i.bak 's/#include <xlocale.h>//' data.c
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+export CFLAGS="$CFLAGS -I${PREFIX}/include"
+export LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
+./configure --prefix=$PREFIX  --with-zlib=shipped
+make thread
+mkdir -p $PREFIX/bin
+cp $SRC_DIR/src/migrate-n $PREFIX/bin
