4d81c9a220767d867e7cb1ad536d3f40d8f0ae4f
Robert Edwards <raedwards@gmail.com>
2020-07-31 18:14:25 -0700

    updating boost paths

--- recipes/prinseq-plus-plus/build.sh
+++ recipes/prinseq-plus-plus/build.sh
@@ -4,10 +4,14 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 
 # build
 ./autogen.sh
-./configure
+./configure --prefix=${PREFIX} 
 make
 make install
