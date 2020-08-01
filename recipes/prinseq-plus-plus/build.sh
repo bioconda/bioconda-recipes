8e7f3291acb32916f541edb0cd4cc7e0e1ccedf9
Robert Edwards <raedwards@gmail.com>
2020-07-31 18:15:41 -0700

    updating boost paths

--- recipes/prinseq-plus-plus/build.sh
+++ recipes/prinseq-plus-plus/build.sh
@@ -3,16 +3,15 @@ set +ex
 
 # Add zlib support
 export CFLAGS="-I$PREFIX/include"
-export CXX_INCLUDE_DIR=$PREFIX/include
-export CPP_INCLUDE_DIR=$PREFIX/include
-export CPPLUS_INCLUDE_DIR=$PREFIX/include
-export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_DIR=$PREFIX/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPPLUS_INCLUDE_DIR=$PREFIX/include
+export CXX_INCLUDE_DIR=$PREFIX/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
+export LDFLAGS="-L$PREFIX/lib"
 export LIBRARY_PATH=${PREFIX}/lib
-
 
 # build
 ./autogen.sh
