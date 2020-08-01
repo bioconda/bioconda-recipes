6a0eea4eb4da3d15d6694fb823b3ce52a78868b7
Robert Edwards <raedwards@gmail.com>
2020-08-01 15:11:55 +1000

    cleaning up build.sh

--- recipes/prinseq-plus-plus/build.sh
+++ recipes/prinseq-plus-plus/build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set +ex
 
-# Add zlib support
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CXXFLAGS="$CXXFLAGS -I$PREFIX/include -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_DIR=$PREFIX/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CPPLUS_INCLUDE_DIR=$PREFIX/include
-export CXX_INCLUDE_DIR=$PREFIX/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
 
 # build
 ./autogen.sh
