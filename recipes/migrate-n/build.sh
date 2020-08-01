93a3da955d2bdbfe9b0e38e1a5179baaddce8514
Peter Beerli <beerli@me.com>
2020-08-01 15:49:35 -0400

    copied a sed changer for -lz

--- recipes/migrate-n/build.sh
+++ recipes/migrate-n/build.sh
@@ -3,6 +3,7 @@
 
 cd $SRC_DIR/src
 sed -i.bak 's/#include <xlocale.h>//' src/data.c
+sed -i "s/MIGRATEGITVERSION = \$(shell . showgit.sh)/MIGRATEGITVERSION = Distribution/" Makefile.in
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
@@ -11,6 +12,7 @@ export LIBRARY_PATH=${PREFIX}/lib
 export CFLAGS="$CFLAGS -I${PREFIX}/include"
 export LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
 ./configure --prefix=$PREFIX
+sed -i.bak 's/-lz/-L${LIBRARY_PATH} -I${C_INCLUDE_PATH} -lz/' Makefile
 make thread
 mkdir -p $PREFIX/bin
 cp $SRC_DIR/src/migrate-n $PREFIX/bin/migrate-n-4.4.4
