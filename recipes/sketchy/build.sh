89eb158bc713fec454fb06748bc5e01adebff4f4
Marcel Bargull <marcel.bargull@udo.edu>
2020-08-11 18:42:39 +0200

    sketchy: CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo

--- recipes/sketchy/build.sh
+++ recipes/sketchy/build.sh
@@ -2,9 +2,10 @@
 set -x
 export RUST_BACKTRACE=full
 
-# Fix for when some packages use ssh to fetch git repos
-# See https://github.com/rust-lang/cargo/issues/1851#issuecomment-450130685
-CARGO_NET_GIT_FETCH_WITH_CLI=true \
-    cargo install -v --locked --root "${PREFIX}" --path .
+# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
+# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
+export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
+
+cargo install -v --locked --root "${PREFIX}" --path .
 
 "${PYTHON}" -m pip install --no-deps --ignore-installed -vv .
