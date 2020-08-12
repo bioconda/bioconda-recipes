e0033e1c19234d6c80402be12dfb43696dc186f4
Yasuhiro Tanizawa <30499043+nigyta@users.noreply.github.com>
2020-08-12 11:24:52 +0900

    Update build.sh

--- recipes/dfast_qc/build.sh
+++ recipes/dfast_qc/build.sh
@@ -7,7 +7,11 @@ mkdir -p $APPROOT
 mkdir -p ${PREFIX}/bin
 cp -r ./* $APPROOT
 
+ls ./ # for debug
+ls $APPROOT # for debug
+
 cd ${PREFIX}/bin
 ln -s ${APPROOT}/dfast_qc
 ln -s ${APPROOT}/dqc_admin_tools.py
 
+ls .  # for debug
