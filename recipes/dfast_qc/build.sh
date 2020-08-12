51f3e5365cd936af4350a8203372b4053b165aed
Yasuhiro Tanizawa <30499043+nigyta@users.noreply.github.com>
2020-08-12 17:38:27 +0900

    Update build.sh

--- recipes/dfast_qc/build.sh
+++ recipes/dfast_qc/build.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-APPROOT=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+APPROOT="${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}"
 
 
-mkdir -p $APPROOT
+# mkdir -p ${APPROOT}
 mkdir -p ${PREFIX}/bin
-cp -r . $APPROOT
+cp -av . ${APPROOT}
 # cp -r dqc $APPROOT/dqc
 # cp dfast_qc $APPROOT
 # cp dqc_admin_tools.py $APPROOT
 # cp initial_setup.sh $APPROOT
 
 
-cd ${PREFIX}/bin
-ln -s ${APPROOT}/dfast_qc
-ln -s ${APPROOT}/dqc_admin_tools.py
+# cd ${PREFIX}/bin
+ln -s ${APPROOT}/dfast_qc ${PREFIX}/bin/dfast_qc
+ln -s ${APPROOT}/dqc_admin_tools.py ${PREFIX}/bin/dqc_admin_tools.py
+
 
