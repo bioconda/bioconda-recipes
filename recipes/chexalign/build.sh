67cc4fb75b4b9872f0a5bccbfebc9a2e685fd3a5
PrashantKuntala <prashantkuntala@gmail.com>
2020-08-13 14:53:20 -0400

    update chexalign to v0.12

--- recipes/chexalign/build.sh
+++ recipes/chexalign/build.sh
@@ -5,7 +5,7 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
-cp ./chexalign_v0.11.jar $outdir
+cp ./chexalign_v0.12.jar $outdir
 cp $RECIPE_DIR/chexalign.sh $outdir/chexalign
 chmod +x $outdir/chexalign
 ln -s $outdir/chexalign $PREFIX/bin
