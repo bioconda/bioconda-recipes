37574fe705359c6f905c564b580467993c0b1860
alienzj <alienchuj@gmail.com>
2020-08-15 18:12:46 +0800

    fix

--- recipes/gfatools/build.sh
+++ recipes/gfatools/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 make INCLUDES="-I$PREFIX/include" CFLAGS="-L$PREFIX/lib"
+
+cd paf2gfa
+make INCLUDES="-I$PREFIX/include -I../" CFLAGS="-L$PREFIX/lib"
+
+cd ..
 mkdir -p $PREFIX/bin
 cp gfatools $PREFIX/bin
+cp paf2gfa/paf2gfa $PREFIX/bin
