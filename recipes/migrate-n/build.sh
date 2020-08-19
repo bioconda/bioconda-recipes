d8d75dd23ed4a5f724268190f5d42e61a9622205
Marcel Bargull <marcel.bargull@udo.edu>
2020-08-19 14:11:23 +0200

    bulk: fix migrate-n install

--- recipes/migrate-n/build.sh
+++ recipes/migrate-n/build.sh
@@ -5,4 +5,7 @@ cd src
 autoreconf -fi
 ./configure --prefix="${PREFIX}"
 make all mpis
-make install
+# Manual install since `make install` wants to install man pages into non-existent path.
+install -d "${PREFIX}/bin"
+install migrate-n migrate-n-mpi "${PREFIX}/bin/"
+# make install
