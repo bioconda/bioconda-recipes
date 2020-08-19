4dc831dbf403b8bd8a659a07f735e0a8df8a8f01
matthiasho <matthias.hortenhuber@ki.se>
2020-08-19 17:37:52 +0200

    fix for incorrect shell script header

--- recipes/paraclu/build.sh
+++ recipes/paraclu/build.sh
@@ -4,4 +4,5 @@ make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
 cp paraclu "${PREFIX}/bin/"
+sed -i -e '1s,^#! */bin/sh,#!/bin/bash,' paraclu-cut.sh
 cp paraclu-cut.sh "${PREFIX}/bin/paraclu-cut"
