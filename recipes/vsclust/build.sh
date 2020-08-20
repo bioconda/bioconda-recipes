91ea6ae16957c466277840edc6ac0519a9a18e31
veitveit <veitveit@gmail.com>
2020-08-20 14:57:48 +0200

    now with most files in share

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -7,6 +7,12 @@ cp -r e1071FuzzVec_Installation/* $PREFIX/lib/R/library/e1071FuzzVec/
 
 # move all files to outdir and link into it by bin executor
 mkdir -p $PREFIX/bin
-cp -R * $PREFIX/bin
+mkdir -p $PREFIX/share/$PKGNAME
+cp *.R $PREFIX/share/$PKGNAME
+mv run_app.R run_vsclust_app.R
+sed -i "1 a setwd(\"\$CONDA_PREFIX/share/$PKGNAME\")" run_vsclust_app.R
+cp runVSClust.R * $PREFIX/bin
+cp run_vsclust_app.R * $PREFIX/bin
 chmod a+x $PREFIX/bin/runVSClust.R
+chmod a+x $PREFIX/bin/run_vsclust_app.R
 
