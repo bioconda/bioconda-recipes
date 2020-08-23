ae5d2b013c979ae7a3c2919b34085c0e314942f7
Will Rowe <w.rowe@bham.ac.uk>
2020-08-23 10:22:16 +0100

    Update build.sh

--- recipes/artic-tools/build.sh
+++ recipes/artic-tools/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/Boost_USE_STATIC_LIBS OFF/Boost_USE_STATIC_LIBS ON/' CMakeLists.txt
+sed -i.bak 's/Boost_USE_STATIC_LIBS OFF/Boost_USE_STATIC_LIBS ON/' CMakeLists.txt
 mkdir build && pushd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make 
