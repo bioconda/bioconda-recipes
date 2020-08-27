38ab7a8322f1649a7cd760251a15db1bcfa13559
veitveit <veitveit@gmail.com>
2020-08-27 18:12:28 +0200

    test

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -10,10 +10,10 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share/vsclust
 # substituting paths for testing 
 cp run_app.sh run_vsclust_app.sh
-sed -i "1 a setwd(\"\$CONDA_PREFIX/share/vsclust\")" run_vsclust_app.sh
-sed -i "s=FcmClustPEst.R=../share/vsclust/FcmClustPEst.R=" runVSClust.R
-sed -i "s=mfuzz.plotpdf.R=../share/vsclust/mfuzz.plotpdf.R=" runVSClust.R
-sed -i "s=HelperFuncs.R=../share/vsclust/HelperFuncs.R=" runVSClust.R
+sed -i'.orig' "1 a setwd(\"\$CONDA_PREFIX/share/vsclust\")" run_vsclust_app.sh
+sed -i'.orig' "s=FcmClustPEst.R=../share/vsclust/FcmClustPEst.R=" runVSClust.R
+sed -i'.orig' "s=mfuzz.plotpdf.R=../share/vsclust/mfuzz.plotpdf.R=" runVSClust.R
+sed -i'.orig' "s=HelperFuncs.R=../share/vsclust/HelperFuncs.R=" runVSClust.R
 cp *.R $PREFIX/share/vsclust/
 cp ProtExample.csv $PREFIX/share/vsclust/
 cp vsclust.yml $PREFIX/share/vsclust/
