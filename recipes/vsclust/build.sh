c15bd1c4dbe678532382f66d2de83c10910c6e17
veitveit <veitveit@gmail.com>
2020-08-27 08:26:34 +0200

    found error

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -10,12 +10,12 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share/$PKGNAME
 echo "sed"
 # substituting paths for testing
+cp run_app.sh run_vsclust_app.sh
 sed -i "1 a setwd(\"\$CONDA_PREFIX/share/$PKGNAME\")" run_vsclust_app.sh
 sed -i "s=ProtExample.csv=\$CONDA_PREFIX/share/$PKGNAME=" vsclust.yml
 echo "copy"
 cp *.R $PREFIX/share/$PKGNAME
 cp ProtExample.csv $PREFIX/share/$PKGNAME
-cp run_app.sh run_vsclust_app.sh
 cp vsclust.yml $PREFIX/share/$PKGNAME  
 cp runVSClust.R $PREFIX/bin
 cp run_vsclust_app.sh $PREFIX/bin
