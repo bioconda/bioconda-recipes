8d908816b749c24e081083d2f22c203a676ef927
veitveit <veitveit@gmail.com>
2020-08-27 19:33:58 +0200

    noarch

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -10,14 +10,17 @@ mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share/vsclust
 # substituting paths for testing 
 cp run_app.sh run_vsclust_app.sh
+echo "before sed"
 sed -i'.orig' "1 a setwd(\"\$CONDA_PREFIX/share/vsclust\")" run_vsclust_app.sh
 sed -i'.orig' "s=FcmClustPEst.R=../share/vsclust/FcmClustPEst.R=" runVSClust.R
 sed -i'.orig' "s=mfuzz.plotpdf.R=../share/vsclust/mfuzz.plotpdf.R=" runVSClust.R
 sed -i'.orig' "s=HelperFuncs.R=../share/vsclust/HelperFuncs.R=" runVSClust.R
+echo "after sed"
 cp *.R $PREFIX/share/vsclust/
 cp ProtExample.csv $PREFIX/share/vsclust/
 cp vsclust.yml $PREFIX/share/vsclust/
 cp runVSClust.R $PREFIX/bin
 cp run_vsclust_app.sh $PREFIX/bin
+echo "before chmod"
 chmod a+x $PREFIX/bin/runVSClust.R
 chmod a+x $PREFIX/bin/run_vsclust_app.sh
