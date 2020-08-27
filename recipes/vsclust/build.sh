6372c50706daa7746216bd29e3f25374a8e9aa16
veitveit <veitveit@gmail.com>
2020-08-27 08:11:04 +0200

    found error

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -8,13 +8,15 @@ cp -r e1071FuzzVec_Installation/* $PREFIX/lib/R/library/e1071FuzzVec/
 # move all files to outdir and link into it by bin executor
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share/$PKGNAME
+echo "sed"
+# substituting paths for testing
+sed -i "1 a setwd(\"\$CONDA_PREFIX/share/$PKGNAME\")" run_vsclust_app.sh
+sed -i "s=ProtExample.csv=\$CONDA_PREFIX/share/$PKGNAME=" vsclust.yml
+echo "copy"
 cp *.R $PREFIX/share/$PKGNAME
 cp ProtExample.csv $PREFIX/share/$PKGNAME
 cp run_app.sh run_vsclust_app.sh
 cp vsclust.yml $PREFIX/share/$PKGNAME  
-echo "sed"
-sed -i "1 a setwd(\"\$CONDA_PREFIX/share/$PKGNAME\")" run_vsclust_app.sh
-echo "copy"
 cp runVSClust.R $PREFIX/bin
 cp run_vsclust_app.sh $PREFIX/bin
 chmod a+x $PREFIX/bin/runVSClust.R
