dc4ed51a8d29544705d5c476dcca80c084927362
veitveit <veitveit@gmail.com>
2020-08-27 13:37:40 +0200

    test

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -7,18 +7,17 @@ cp -r e1071FuzzVec_Installation/* $PREFIX/lib/R/library/e1071FuzzVec/
 
 # move all files to outdir and link into it by bin executor
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/share/$PKGNAME
+mkdir -p $PREFIX/share/$PKG_NAME
 echo "sed"
 # substituting paths for testing
 cp run_app.sh run_vsclust_app.sh
-sed -i "1 a setwd(\"\$CONDA_PREFIX/share/$PKGNAME\")" run_vsclust_app.sh
-head run_vsclust_app.sh
-sed -i "s=ProtExample.csv=\$CONDA_PREFIX/share/${PKGNAME}/ProtExample.csv=" vsclust.yml
+sed -i "1 a setwd(\"\$CONDA_PREFIX/share/$PKG_NAME\")" run_vsclust_app.sh
+sed -i "s=ProtExample.csv=\$CONDA_PREFIX/share/${PKG_NAME}/ProtExample.csv=" vsclust.yml
 cat vsclust.yml
 echo "copy"
-cp *.R $PREFIX/share/$PKGNAME
-cp ProtExample.csv $PREFIX/share/$PKGNAME
-cp vsclust.yml $PREFIX/share/$PKGNAME  
+cp *.R $PREFIX/share/$PKG_NAME
+cp ProtExample.csv $PREFIX/share/$PKG_NAME
+cp vsclust.yml $PREFIX/share/$PKG_NAME  
 cp runVSClust.R $PREFIX/bin
 cp run_vsclust_app.sh $PREFIX/bin
 chmod a+x $PREFIX/bin/runVSClust.R
