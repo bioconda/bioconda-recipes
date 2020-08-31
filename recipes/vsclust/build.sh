ecc25eb0886bc6304cb4cfc88546ba7d3fafaf49
veitveit <veitveit@gmail.com>
2020-08-31 14:49:09 +0200

    added missing r libraries

--- recipes/vsclust/build.sh
+++ recipes/vsclust/build.sh
@@ -9,14 +9,7 @@ cp -r e1071FuzzVec_Installation/* $PREFIX/lib/R/library/e1071FuzzVec/
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/share/vsclust
 # substituting paths for testing 
-cp run_app.sh run_vsclust_app.sh
-echo "before sed"
-tail -n +2 run_vsclust_app.sh > t
-echo '#!/usr/bin/env Rscript' > tt
-echo 'setwd("$CONDA_PREFIX/share/vsclust")' > tt
-cat tt t > run_vsclust_app.sh
-#sed -i'.orig' '1a\ setwd("$CONDA_PREFIX/share/vsclust")' run_vsclust_app.sh
-#sed --posix -i'.orig' "1 a setwd(\"\$CONDA_PREFIX/share/vsclust\")" run_vsclust_app.sh
+cp run_app_conda.R run_vsclust_app.R
 sed -i'.orig' "s=FcmClustPEst.R=../share/vsclust/FcmClustPEst.R=" runVSClust.R
 sed -i'.orig' "s=mfuzz.plotpdf.R=../share/vsclust/mfuzz.plotpdf.R=" runVSClust.R
 sed -i'.orig' "s=HelperFuncs.R=../share/vsclust/HelperFuncs.R=" runVSClust.R
