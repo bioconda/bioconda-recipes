3b7f50519aac2b4441d00382aa939e76104f53fa
jripoll <jripoll@lirmm.fr>
2020-09-01 12:19:41 +0200

    create crac recipe

---
+++ recipes/crac/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+
+# compile Crac 
+make CXX=$CXX
+
+# cp executables
+mkdir -p $PREFIX/bin
+cp -pf CRAC $PREFIX/bin
