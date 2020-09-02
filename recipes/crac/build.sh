4f01694da2e3289656b69981759e7a6d4970b204
jripoll <jripoll@lirmm.fr>
2020-09-02 17:52:20 +0200

    updating build.sh

--- recipes/crac/build.sh
+++ recipes/crac/build.sh
@@ -5,9 +5,15 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 
 # for troubleshooting with zlib
-export CPATH=${PREFIX}/include
+#export CPATH=${PREFIX}/include
 
 # compile Crac 
 ./configure
-make clean
-make -B CFLAGS=-DNDEBUG
+make
+make check
+make install
+make installcheck
+
+# cp executables
+mkdir -p $PREFIX/bin
+cp -pf crac $PREFIX/bin
