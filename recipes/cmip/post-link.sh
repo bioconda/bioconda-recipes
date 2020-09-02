5a053c214c25b2a516e32e506ae5a0c2b9829b8e
Pau Andrio <andriopau@gmail.com>
2020-09-02 16:01:28 +0200

    Adding ascii art to post-link.sh

--- recipes/cmip/post-link.sh
+++ recipes/cmip/post-link.sh
@@ -1,15 +1,34 @@
 echo "
-        #########################################################
-        #          Thank you  for installing CMIP               #
-        #                                                       #
-        #  For help/usage options execute:                      #
-        #  cmip -h                                              #
-        #                                                       #
-        #  To run the tests execute:                            #
-        #  $PREFIX/share/cmip/tests/tests.sh                    #
-        #                                                       #
-        #  To run some predefined scripts check this folder:    #
-        #  $PREFIX/share/cmip/scripts                           #
-        #                                                       #
-        #########################################################
+
+      #######################################################################
+      #                                                                     #
+      #                   ######  ##     ## #### ########                   #
+      #                  ##    ## ###   ###  ##  ##     ##                  #
+      #                  ##       #### ####  ##  ##     ##                  #
+      #                  ##       ## ### ##  ##  ########                   #
+      #                  ##       ##     ##  ##  ##                         #
+      #                  ##    ## ##     ##  ##  ##                         #
+      #                   ######  ##     ## #### ##                         #
+      #                                                                     #
+      #              Classical Molecular Interaction Potential              #
+      #                                                                     #
+      #   For help/usage options execute:                                   #
+      #   cmip -h                                                           #
+      #                                                                     #
+      #   To run the test set execute:                                      #
+      #   $PREFIX/share/cmip/tests/tests.sh                                 #
+      #                                                                     #
+      #   To run some predefined scripts check this folder:                 #
+      #   $PREFIX/share/cmip/scripts                                        #
+      #                                                                     #
+      #######################################################################
+
 " > $PREFIX/.messages.txt
+
+
+
+
+
+
+
+
