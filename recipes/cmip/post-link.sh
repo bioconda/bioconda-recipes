d46aaae2b6d93106dd5180ae4b4c35124c8f5270
Pau Andrio <andriopau@gmail.com>
2020-09-02 14:28:10 +0200

    Modifiying Post-link.sh to print a message after installation

--- recipes/cmip/post-link.sh
+++ recipes/cmip/post-link.sh
@@ -1,16 +1,15 @@
-#!/usr/bin/env bash
-
-echo ""
-echo "******************************"
-echo "Thank you  for installing CMIP"
-echo "******************************"
-echo ""
-echo "For help/usage options execute:"
-echo "cmip -h"
-echo ""
-echo "To run the tests execute:"
-echo "\$CONDA_PREFIX/share/cmip/tests/tests.sh"
-echo ""
-echo "To run some predefined scripts check this folder:"
-echo "\$CONDA_PREFIX/share/cmip/scripts"
-echo ""
+echo "
+        #########################################################
+        #          Thank you  for installing CMIP               #
+        #                                                       #
+        #  For help/usage options execute:                      #
+        #  cmip -h                                              #
+        #                                                       #
+        #  To run the tests execute:                            #
+        #  $PREFIX/share/cmip/tests/tests.sh                    #
+        #                                                       #
+        #  To run some predefined scripts check this folder:    #
+        #  $PREFIX/share/cmip/scripts                           #
+        #                                                       #
+        #########################################################
+" > $PREFIX/.messages.txt
