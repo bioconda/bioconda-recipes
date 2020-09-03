7151ee6a9c225977551201a3b18f1242de6d8bce
Pau Andrio <andriopau@gmail.com>
2020-09-03 15:43:16 +0200

    Update Cmip (#24216)
    * Classical Mollecular Interaction Potential
    
    * Fixing Build script
    
    * Renaming Makefile.gfortran to Makefile
    
    * Makefile: Removing -static-libgfortran and -static-libgcc options from gfortran compilation
    
    * Removing skip = True option for meta.yaml
    
    * Adding 1 to build number
    
    * Adding tests dat and script folders to <env>/share/cmip
    
    * Modifiying Post-link.sh to print a message after installation
    
    * Modifiying Post-link.sh to print a message after installation
    
    * Adding ascii art to post-link.sh
    
    * Post-link.sh replace echo by cat to keep the breaklines in MacOS
    
    * post-link.sh every line in a different echo statement. Removing -x option from test.sh

--- recipes/cmip/post-link.sh
+++ recipes/cmip/post-link.sh
@@ -1,25 +1,22 @@
-cat > "${PREFIX}"/.messages.txt <<- EOF
-
-      #######################################################################
-      #                                                                     #
-      #                   ######  ##     ## #### ########                   #
-      #                  ##    ## ###   ###  ##  ##     ##                  #
-      #                  ##       #### ####  ##  ##     ##                  #
-      #                  ##       ## ### ##  ##  ########                   #
-      #                  ##       ##     ##  ##  ##                         #
-      #                  ##    ## ##     ##  ##  ##                         #
-      #                   ######  ##     ## #### ##                         #
-      #                                                                     #
-      #              Classical Molecular Interaction Potential              #
-      #                                                                     #
-      #   For help/usage options execute:                                   #
-      #   cmip -h                                                           #
-      #                                                                     #
-      #   To run the test set execute:                                      #
-      #   $PREFIX/share/cmip/tests/tests.sh                                 #
-      #                                                                     #
-      #   To run some predefined scripts check this folder:                 #
-      #   $PREFIX/share/cmip/scripts                                        #
-      #                                                                     #
-      #######################################################################
-EOF
+echo "      #######################################################################" >> .messages.txt
+echo "      #                                                                     #" >> .messages.txt
+echo "      #                   ######  ##     ## #### ########                   #" >> .messages.txt
+echo "      #                  ##    ## ###   ###  ##  ##     ##                  #" >> .messages.txt
+echo "      #                  ##       #### ####  ##  ##     ##                  #" >> .messages.txt
+echo "      #                  ##       ## ### ##  ##  ########                   #" >> .messages.txt
+echo "      #                  ##       ##     ##  ##  ##                         #" >> .messages.txt
+echo "      #                  ##    ## ##     ##  ##  ##                         #" >> .messages.txt
+echo "      #                   ######  ##     ## #### ##                         #" >> .messages.txt
+echo "      #                                                                     #" >> .messages.txt
+echo "      #              Classical Molecular Interaction Potential              #" >> .messages.txt
+echo "      #                                                                     #" >> .messages.txt
+echo "      #   For help/usage options execute:                                   #" >> .messages.txt
+echo "      #   cmip -h                                                           #" >> .messages.txt
+echo "      #                                                                     #" >> .messages.txt
+echo "      #   To run the test set execute:                                      #" >> .messages.txt
+echo "      #   \$CONDA_PREFIX/share/cmip/tests/tests.sh                           #" >> .messages.txt
+echo "      #                                                                     #" >> .messages.txt
+echo "      #   To run some predefined scripts check this folder:                 #" >> .messages.txt
+echo "      #   \$CONDA_PREFIX/share/cmip/scripts                                  #" >> .messages.txt
+echo "      #                                                                     #" >> .messages.txt
+echo "      #######################################################################" >> .messages.txt
