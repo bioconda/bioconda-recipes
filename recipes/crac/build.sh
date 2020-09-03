1a1ad92fbd3d0a601ea4ef749e737408da143470
jripoll <jripoll@lirmm.fr>
2020-09-03 11:59:33 +0200

    test for build

--- recipes/crac/build.sh
+++ recipes/crac/build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+### Dumping preset flags because they break the build process on linux
+# Linker flags
+unset LDFLAGS
+export LDFLAGS="-L${PREFIX}/lib"
+# Compiler flags
+unset CXXFLAGS
+export CXXFLAGS="-I${PREFIX}/include"
+# fixed the version before C++11 due to deprecated use of the register keyword
+export CPPFLAGS="-std=c++03"
+
+# if troubleshooting with zlib
+export CPATH=${PREFIX}/include
+
+# Others troubleshooting
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
-export CPPFLAGS="-std=c++11"
-
-# for troubleshooting with zlib
-#export CPATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
 # compile Crac 
 ./configure
-make CXX="$CXX $CXXFLAGS" CPPFLAGS="$CPPFLAGS -I. -I$PREFIX/include"
+make CXX="$CXX $CXXFLAGS" CPPFLAGS="$CPPFLAGS" install ROOT="."
 make check
-make install
+sudo make install
 make installcheck
 
 # cp executables
-mkdir -p $PREFIX/bin
-cp -pf crac $PREFIX/bin
+mkdir -pv "$PREFIX"/bin/
+cp -pf CRAC "$PREFIX"/bin/
