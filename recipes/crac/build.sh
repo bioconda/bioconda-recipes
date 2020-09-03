0cae2337b9815101ce719e6b6f367f0a7aa81651
jripoll <jripoll@lirmm.fr>
2020-09-03 16:12:21 +0200

    test succesful

--- recipes/crac/build.sh
+++ recipes/crac/build.sh
@@ -16,16 +16,18 @@ export CPATH=${PREFIX}/include
 # Others troubleshooting
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+#export CXX_INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
+
+# avoid sudo rights by changing localization
+./configure --prefix="${PREFIX}" #/home/<user>/<some>/<directory>
 
 # compile Crac 
-./configure --prefix="${PREFIX}" #/home/<user>/<some>/<directory>
-make CXX="$CXX $CXXFLAGS" CPPFLAGS="$CPPFLAGS" install ROOT="."
-make check
+make CXX="$CXX $CXXFLAGS" CPPFLAGS="$CPPFLAGS" prefix="$PREFIX"
+# make check
 make install
 make installcheck
 
 # cp executables
-#mkdir -pv "$PREFIX"/bin/
-#cp -pf crac "$PREFIX"/bin/
+#mkdir -p $PREFIX/bin
+#for i in ${PREFIX}/crac/src/; do cp ${PREFIX}/crac/src/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
