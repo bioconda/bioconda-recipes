ce49f2161cb1ccc9346c1abfdb40d38ecac04ebf
Pierre Marijon <pierre.marijon@hhu.de>
2020-09-04 18:52:44 +0200

    Add CONSENT a self long read corrector v2.1

--- recipes/consent/build.sh
+++ recipes/consent/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Fix zlib error
-export CPATH=${PREFIX}/include
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 # Download git submodule dependencie
 rm -rf BMEAN
@@ -21,11 +22,13 @@ if [ "$(uname)" == "Darwin" ]; then
     sed -i '' -e "s#CXX= g++#CXX= ${CXX}#" BMEAN/makefile # Fix c++ compiler in BMEAN
     sed -i '' -e "s#CC = gcc#CC = ${CC}#" BMEAN/Complete-Striped-Smith-Waterman-Library/src/Makefile # Fix c compiler in Complete-Striped-Smith-Waterman-Library
     sed -i '' -e "s#CXX = g++#CXX = ${CXX}#" BMEAN/Complete-Striped-Smith-Waterman-Library/src/Makefile # Fix c++ compiler in Complete-Striped-Smith-Waterman-Library
+    sed -i '' -e "s#CFLAGS := -Wall -pipe -O2#CFLAGS := ${CFLAGS} ${LDFLAGS} -Wall -pipe -O2#" BMEAN/Complete-Striped-Smith-Waterman-Library/src/Makefile # Fix flag errase on in Complete-Striped-Smith-Waterman-Library makefile
     sed -i '' -e "s#/BMEAN/BOA/blosum80.mat#/../share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/blosum80.mat#" src/main.cpp # Fix matrix path
 else
     sed -i'' -e "s#CXX= g++#CXX= ${CXX}#" BMEAN/makefile # Fix c++ compiler in BMEAN
     sed -i'' -e "s#CC = gcc#CC = ${CC}#" BMEAN/Complete-Striped-Smith-Waterman-Library/src/Makefile # Fix c compiler in Complete-Striped-Smith-Waterman-Library
     sed -i'' -e "s#CXX = g++#CXX = ${CXX}#" BMEAN/Complete-Striped-Smith-Waterman-Library/src/Makefile # Fix c++ compiler in Complete-Striped-Smith-Waterman-Library
+    sed -i'' -e "s#CFLAGS := -Wall -pipe -O2#CFLAGS := ${CFLAGS} ${LDFLAGS} -Wall -pipe -O2#" BMEAN/Complete-Striped-Smith-Waterman-Library/src/Makefile # Fix flag errase on in Complete-Striped-Smith-Waterman-Library makefile
     sed -i'' -e "s#/BMEAN/BOA/blosum80.mat#/../share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/blosum80.mat#" src/main.cpp # Fix matrix path
 fi
 
@@ -39,8 +42,11 @@ mv bin/reformatPAF.py bin/CONSENT-reformatPAF.py
 
 # install matrix
 mkdir -p $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/
-cp BMEAN/BOA/blosum80.mat $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/
+cp BMEAN/blosum80.mat $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/
 
 # install bin
+mkdir -p $PREFIX/bin/
 cp bin/* $PREFIX/bin/
+cp CONSENT-correct $PREFIX/bin/
+cp CONSENT-polish $PREFIX/bin/
 
