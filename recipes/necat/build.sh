34bee026aed81c62a4d0237ce2e87298ca5c0589
Pierre Marijon <pierre.marijon@hhu.de>
2020-09-09 10:28:41 +0200

    Add necat

---
+++ recipes/necat/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+# build
+cd src
+make CFLAGS+="${CFLAGS}" LDFLAGS+="${LDFLAGS}"
+cd ..
+
+# create and populate binary file
+mkdir -p $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/
+cp -r Linux-amd64/bin/*  $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/
+
+# create calling script
+mkdir $PREFIX/bin/
+cat <<EOF > $PREFIX/bin/necat
+#!/bin/bash
+
+PATH=$PATH:$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/ necat.pl $@
+EOF
+
+chmod +x $PREFIX/bin/necat
