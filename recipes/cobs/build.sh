6ecfe25503ee51313bc4938c9bd5f74770f82ac2
Luiz Irber <luiz.irber@gmail.com>
2020-09-10 08:18:55 -0700

    working on cobs recipe

---
+++ recipes/cobs/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu -o pipefail
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=RELEASE \
+      -DCONDA_BUILD=TRUE \
+      -DBoost_NO_BOOST_CMAKE=ON \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+      -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+      -DBoost_NO_SYSTEM_PATHS=ON \
+      ..
+make VERBOSE=1
+echo "installing"
+make install CFLAGS="-L${PREFIX}/lib -I${PREFIX}/include"
