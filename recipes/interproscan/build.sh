b32621af0d4829f90a5ee032af7546c1c9e34ffb
Jacques Dainat <jacques.dainat@nbis.se>
2020-09-15 19:19:45 +0200

    fix interproscan.sh path

--- recipes/interproscan/build.sh
+++ recipes/interproscan/build.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-echo "debug1"
-ls -l
-
 # cd into the core directory, where the master pom.xml file (Maven build file) is located.
 cd core
-
-echo "debug2"
-ls -l
 
 # Run mvn clean install to build and install (into your local Maven repository) all of the modules for InterProScan 5.
 mvn clean install
 
-echo "debug3"
-ls -l
-
 # cd into the jms-implementation directory and run mvn clean package
- cd jms-implementation
-
-echo "debug4"
-ls -l
+cd jms-implementation
 
 mvn clean package
 
@@ -28,7 +16,7 @@ ls -l
 
 # mv interproscan.sh in the bin
 mkdir -p ${PREFIX}/bin
-cp interproscan-5-dist/interproscan.sh  ${PREFIX}/bin/
+cp target/interproscan-5-dist/interproscan.sh  ${PREFIX}/bin/
 
 # Add more build steps here, if they are necessary.
 
