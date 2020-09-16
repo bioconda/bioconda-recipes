034adf27c461f11ea8d398aeab59052a36ed4628
Andrea Telatin <andrea@telatin.com>
2020-09-16 12:34:13 +0100

    switch_to_nim

--- recipes/porfast/build.sh
+++ recipes/porfast/build.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
-# Compile nim
-
-pushd nim_source
-if [[ $OSTYPE == "darwin"* ]]; then
-  export HOME="/Users/distiller"
-  bash build.sh --os darwin --cpu x86_64
-else
-  bash build.sh --os linux --cpu x86_64
-fi
-bname=`basename $CC`
-echo "gcc.exe = \"${bname}\"" >> config/nim.cfg
-echo "gcc.linkerexe = \"${bname}\"" >> config/nim.cfg
-echo "clang.exe = \"${bname}\"" >> config/nim.cfg
-echo "clang.linkerexe = \"${bname}\"" >> config/nim.cfg
-bin/nim c  koch
-./koch tools
-popd
 
 set -euxo pipefail
 
-export PATH=$SRC_DIR/nim_source/bin:$PATH
-export LD_LIBRARY_PATH=$PREFIX/lib
-
 mkdir -p $PREFIX/bin
-nimble
 nimble install -y --verbose argparse
 nim c --threads:on -p:lib --opt:speed -o:$PREFIX/bin/porfast src/porfast.nim
 
+
+#pushd nim_source
+#if [[ $OSTYPE == "darwin"* ]]; then
+#  export HOME="/Users/distiller"
+#  bash build.sh --os darwin --cpu x86_64
+#else
+#  bash build.sh --os linux --cpu x86_64
+#fi
+#bname=`basename $CC`
+#echo "gcc.exe = \"${bname}\"" >> config/nim.cfg
+#echo "gcc.linkerexe = \"${bname}\"" >> config/nim.cfg
+#echo "clang.exe = \"${bname}\"" >> config/nim.cfg
+#echo "clang.linkerexe = \"${bname}\"" >> config/nim.cfg
+#bin/nim c  koch
+#./koch tools
+#popd
