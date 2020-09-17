78ce389f932907bebbc0e3ba35908d36ea316c92
Jacques Dainat <jacques.dainat@nbis.se>
2020-09-17 20:17:53 +0200

    comment useless command for test

--- recipes/blast/build.sh
+++ recipes/blast/build.sh
@@ -104,8 +104,8 @@ cp $BLAST_SRC_DIR/c++/ReleaseMT/lib/* $LIB_INSTALL_DIR
 ls -s $PREFIX/bin/
 
 chmod +x $PREFIX/bin/*
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/update_blastdb.pl
+#sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/update_blastdb.pl
 # Patches to enable this script to work better in bioconda
-sed -i.bak 's/mktemp.*/mktemp`/; s/exit 1/exit 0/; s/^export PATH=\/bin:\/usr\/bin:/\#export PATH=\/bin:\/usr\/bin:/g' $PREFIX/bin/get_species_taxids.sh
+#sed -i.bak 's/mktemp.*/mktemp`/; s/exit 1/exit 0/; s/^export PATH=\/bin:\/usr\/bin:/\#export PATH=\/bin:\/usr\/bin:/g' $PREFIX/bin/get_species_taxids.sh
 
 ls -s $PREFIX/bin/
