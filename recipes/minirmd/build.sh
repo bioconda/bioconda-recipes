edade542488809dde4be53d6c0113c397de558ea
Yuansheng Liu <yyuanshengliu@gmail.com>
2020-09-26 07:19:48 +0800

    minirmd

--- recipes/minirmd/build.sh
+++ recipes/minirmd/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make GXX="${GXX}" CPPFLAGS="${CPPFLAGS}" LIBS="${LDFLAGS} -lm -lz -lpthread"
+make CC="${GXX}" CPPFLAGS="${CPPFLAGS}" LIBS="${LDFLAGS} -lm -lz -lpthread"
 
 mkdir -p "${PREFIX}/bin"
 cp minirmd "${PREFIX}/bin/"
