a5640868604170f90b7d1a081b4476abee123652
Simon Bray <32272674+simonbray@users.noreply.github.com>
2020-09-28 21:03:01 +0200

    fix python shebangs

--- recipes/tetools_l-modolo/build.sh
+++ recipes/tetools_l-modolo/build.sh
@@ -2,6 +2,8 @@
 
 mkdir -p $PREFIX/bin
 sed -i 's|#!/usr/bin/Rscript|#!/usr/bin/env Rscript|' TEdiff.R
+sed -i 's|#!/usr/bin/python3|#!/usr/bin/env python|' TEcount.py
+sed -i 's|#!/usr/bin/python3|#!/usr/bin/env python|' PingPong.py
 EXEC='PingPong.py TEcount.py TEdiff.R UrQt pingpongpro/pingpongpro'
 chmod +x $EXEC
 cp $EXEC $PREFIX/bin/
