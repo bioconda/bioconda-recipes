93948d998b97ec4f840476ea424b5da6f2289a80
Matthias Bernt <m.bernt@ufz.de>
2020-09-28 16:05:18 +0200

    remove 1.0 specific patches

--- recipes/chromeister/build.sh
+++ recipes/chromeister/build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# in v1.0 we need to 
-# - remove CC=gcc, 
-# - add -lpthread
-# - make CFLAGS not overwrite globally set CFLAGS
-# - and add a missing include to a .h file (needed for osx build)
-sed -i -e 's/^CC=.*//; s/-lm/-lm -lpthread/; s/CFLAGS=/CFLAGS+=/;' src/Makefile
-sed -i -e '/^#include "structs.h"/a \
-#include <pthread.h>' src/commonFunctions.h
-
 make -C src/ all
 cp -r bin/ "$PREFIX"
