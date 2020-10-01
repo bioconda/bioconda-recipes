8f1802e27cb3d50084e1484870200fd254fe8391
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-10-01 18:40:30 -0400

    Fix linking and requirements

--- recipes/scramble/build.sh
+++ recipes/scramble/build.sh
@@ -3,4 +3,4 @@ set -x -e
 mkdir -p "${PREFIX}/bin"
 
 cd cluster_identifier/src
-${GCC} -I"${BUILD_PREFIX}/include" -L"${BUILD_PREFIX}/lib" -o "${PREFIX}/bin/cluster_identifier" -lz -lpthread -llzma -lbz2 -lcurl -lcrypto -lhts cluster_identifier.c
+${GCC} $CFLAGS $LDFLAGS -o "${PREFIX}/bin/cluster_identifier" cluster_identifier.c -lz -lpthread -llzma -lbz2 -lcurl -lcrypto -lhts
