6356c8cf468ae642d2759c03ab4a18bf16782d87
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-10-06 18:55:45 -0400

    Add a wrapper script for cluster_analysis step

--- recipes/scramble/build.sh
+++ recipes/scramble/build.sh
@@ -5,3 +5,4 @@ mkdir -p "${PREFIX}/bin"
 $GCC $CFLAGS $LDFLAGS -o "${PREFIX}/bin/cluster_identifier" "${SRC_DIR}/cluster_identifier/src/cluster_identifier.c" -lz -lpthread -llzma -lbz2 -lcurl -lcrypto -lhts
 
 cp -R "${SRC_DIR}/cluster_analysis" "${PREFIX}"
+cp "${RECIPE_DIR}/scramble.sh" "${PREFIX}/bin"
