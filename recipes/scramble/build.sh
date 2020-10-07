143e47f167286e36b6f4ec170016fbc328450d2a
Ilya Sytchev <isytchev@hsph.harvard.edu>
2020-10-07 12:16:48 -0400

    Cleanup up build script

--- recipes/scramble/build.sh
+++ recipes/scramble/build.sh
@@ -2,7 +2,9 @@ set -x -e
 
 mkdir -p "${PREFIX}/bin"
 
-$GCC $CFLAGS $LDFLAGS -o "${PREFIX}/bin/cluster_identifier" "${SRC_DIR}/cluster_identifier/src/cluster_identifier.c" -lz -lpthread -llzma -lbz2 -lcurl -lcrypto -lhts
+$GCC $CFLAGS $LDFLAGS -o "${PREFIX}/bin/cluster_identifier" \
+  "${SRC_DIR}/cluster_identifier/src/cluster_identifier.c" \
+  -lz -lpthread -llzma -lbz2 -lcurl -lcrypto -lhts
 
 cp -R "${SRC_DIR}/cluster_analysis" "${PREFIX}"
 cp "${RECIPE_DIR}/scramble.sh" "${PREFIX}/bin"
