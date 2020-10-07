9dcffaf6ae487c4a94f7c66608a8cd6bf878fe11
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-10-07 11:14:29 -0400

    Add some debugging code

--- recipes/ncbi-amrfinderplus/build.sh
+++ recipes/ncbi-amrfinderplus/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+echo "PREFIX=$PREFIX    CONDA_PREFIX=$CONDA_PREFIX"
 # note that for version 3.7 the make command should be:
-make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" CONDA_DB_DIR="$PREFIX/share/amrfinderplus/data"
+make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" CONDA_DB_DIR="$CONDA_PREFIX/share/amrfinderplus/data"
 
 # make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" DEFAULT_DB_DIR="$PREFIX/share/amrfinderplus/data"
 make install bindir=$PREFIX/bin
