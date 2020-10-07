ed21cd95ca149cd6d932b9f15d5bc3bd46fab97d
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-10-07 12:00:12 -0400

    More debugging code

--- recipes/ncbi-amrfinderplus/build.sh
+++ recipes/ncbi-amrfinderplus/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "PREFIX=$PREFIX    CONDA_PREFIX=$CONDA_PREFIX"
+set
 # note that for version 3.7 the make command should be:
 make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" CONDA_DB_DIR="$CONDA_PREFIX/share/amrfinderplus/data"
 
