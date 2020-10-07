a0695695fb7d68748383b48f4f4b53e4a1afdae9
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-10-07 14:25:00 -0400

    patch for conda CI test b/c CONDA_PREFIX not set

--- recipes/ncbi-amrfinderplus/build.sh
+++ recipes/ncbi-amrfinderplus/build.sh
@@ -9,21 +9,23 @@ set
 echo "make CXX=\"$CXX $LDFLAGS\" CPPFLAGS=\"$CXXFLAGS\" PREFIX=\"$PREFIX\" DEFAULT_DB_DIR=\"$PREFIX/share/amrfinderplus/data\""
 echo "END DEBUGGING environment info because PREFIX did not appear to be set"
 
-# try patching with some changes
+# try patching with some changes. This is necessary because CONDA_PREFIX is not set on test system. 
+# it should be set when using conda on the commandline
 cat <<END | patch amrfinder.cpp
-*** amrfinder.cpp        Tue Oct  6 15:52:07 2020
---- amrfinder2.cpp        Wed Oct  7 13:43:11 2020
-***************
-*** 422,427 ****
---- 422,429 ----
-      // we're in condaland
-        if (const char* s = getenv("CONDA_PREFIX")) {
-          defaultDb = string (s) + string ("/share/amrfinderplus/data/latest");
-+       } else if (const char* s = getenv("PREFIX")) {
-+         defaultDB = string (s) + string ("/share/amrfinderplus/data/latest");
-        } else {
-          Warning warning (stderr);
-          stderr << "This was compiled for running under bioconda, but $CONDA_PREFIX was not found" << "\n";
+--- a/amrfinder.cpp
++++ b/amrfinder.cpp
+@@ -422,6 +422,11 @@ struct ThisApplication : ShellApplication
+     // we're in condaland
+       if (const char* s = getenv("CONDA_PREFIX")) {
+         defaultDb = string (s) + string ("/share/amrfinderplus/data/latest");
++      } else if (const char* s = getenv("PREFIX")) {
++        Warning warning (stderr);
++        stderr << "This was compiled for running under bioconda, but $CONDA_PREFIX was not found" << "\n";
++        defaultDb = string (s) + string ("/share/amrfinderplus/data/latest");
++        stderr << "Reverting to $PREFIX: " << defaultDb;
+       } else {
+         Warning warning (stderr);
+         stderr << "This was compiled for running under bioconda, but $CONDA_PREFIX was not found" << "\n";
 END
 
 make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" DEFAULT_DB_DIR="$PREFIX/share/amrfinderplus/data"
