565d2c56e9bfda3a5791078dc4aa04769ddedd4e
Devon Ryan <dpryan79@users.noreply.github.com>
2020-10-14 14:15:28 +0200

    Update build.sh

--- recipes/porfast/build.sh
+++ recipes/porfast/build.sh
@@ -4,11 +4,7 @@ set -euxo pipefail
 
 if [[ $OSTYPE == "darwin"* ]]; then
   export HOME="/Users/distiller"
-  #mv bin/porfast_osx $PREFIX/bin/porfast
-  #chmod +x $PREFIX/bin/porfast
-#else
 fi
-  mkdir -p $PREFIX/bin
-  nimble install -y --verbose argparse
-  nim c --threads:on -p:lib --opt:speed -o:$PREFIX/bin/porfast src/porfast.nim
-#fi
+mkdir -p $PREFIX/bin
+nimble install -y --verbose argparse
+nim c --threads:on -p:lib --opt:speed -o:$PREFIX/bin/porfast src/porfast.nim
