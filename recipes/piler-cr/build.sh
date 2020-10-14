8f55670f766f5c114b2b4e343a12518c7a1e092b
Oliver Schwengers <oschwengers@users.noreply.github.com>
2020-10-14 16:24:38 +0200

    remove additional download/extract

--- recipes/piler-cr/build.sh
+++ recipes/piler-cr/build.sh
@@ -1,10 +1,4 @@
-BIN_FOLDER=$PREFIX/bin
-mkdir -p $BIN_FOLDER
-
-wget $URL
-tar -xzf pilercr${PKG_VERSION}.tar.gz
-
-cd pilercr${PKG_VERSION}
 make
 
-cp pilercr $BIN_FOLDER/pilercr
+mkdir -p $PREFIX/bin
+cp pilercr $$PREFIX/bin/pilercr
