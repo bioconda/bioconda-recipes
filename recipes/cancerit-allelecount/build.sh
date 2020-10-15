28a9c601e3867f2e4f322aba82872e2d377f04e7
Devon Ryan <dpryan79@users.noreply.github.com>
2020-10-15 14:17:42 +0200

    Update build.sh

--- recipes/cancerit-allelecount/build.sh
+++ recipes/cancerit-allelecount/build.sh
@@ -18,6 +18,6 @@ sed -i 's/$(CC) /$(compiler) /g' Makefile
 #Force HTSLIb locations
 sed -i 's#HTSLOC?=$(HTSLIB)#HTSLOC=${PREFIX}/lib#g' Makefile
 sed -i 's#prefix=?/usr/local#prefix=${PREFIX}#g' Makefile
-make OPTINC="-I$PREFIX/include"
+make OPTINC="-I$PREFIX/include -L$PREFIX/lib"
 mkdir -p $PREFIX/bin
 cp bin/* $PREFIX/bin
