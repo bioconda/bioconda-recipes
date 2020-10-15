a5cd7296a637c839be339d6c44a2f4a34fe76e5c
liberjul <34191597+liberjul@users.noreply.github.com>
2020-10-15 03:14:56 -0400

    [ci skip] Merge PR 24861
    Merge PR #24861, commits were: 
     * Update meta.yaml
     * Merge branch 'master' into master
     * Update meta.yaml
     * v2.0.3 with high-level taxonomy, various fixes.
     * Removed exports to preserve environment.
     * Fixed build number
     * URL fix
     * v2.0.1
     * Modified to use conda rdptools
     * Testing output sending to /dev/null.
     * Added openjdk, vsearch, and blast to run requirements.
     * Test outputs redirects to stdout/sterr
     * Put noarch: python in correct place
     * Changed to noarch: python
     * Added noarch: generic and git_url -> url
     * Added package info and sends test output to /dev/null
     * Initial constax commit

--- recipes/constax/build.sh
+++ recipes/constax/build.sh
@@ -4,9 +4,9 @@ mkdir -p $PREFIX/bin
 
 constax=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $constax
-echo "SINTAXPATH=vsearch" > $constax/pathfile.txt
-echo "RDPPATH=classifier" >> $constax/pathfile.txt
-echo "CONSTAXPATH=$constax" >> $constax/pathfile.txt
+echo "export SINTAXPATH=vsearch" > $constax/pathfile.txt
+echo "export RDPPATH=classifier" >> $constax/pathfile.txt
+echo "export CONSTAXPATH=$constax" >> $constax/pathfile.txt
 cp -r ./* $constax
 chmod +x $constax/constax.sh
 chmod +x $constax/fasta_select_by_keyword.py
