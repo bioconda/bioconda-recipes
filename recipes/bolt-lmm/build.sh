f733d53987549f55cfe5eba70e3710bc2cbef7ba
Dylan Aïssi <daissi@debian.org>
2020-10-15 10:02:02 +0200

    Don't hardcode an old glibc version, use the current glibc instead

--- recipes/bolt-lmm/build.sh
+++ recipes/bolt-lmm/build.sh
@@ -6,6 +6,8 @@ sed -i'.bak' 's/${CC}/${CXX}/g' Makefile
 sed -i'.bak' 's/ifeq (${CXX},g++)/ifneq (,$(firstword $(findstring gnu-c++,$(CXX)) $(findstring clang,$(CXX))))/g' Makefile
 sed -i'.bak' 's/${LFLAGS}/${LFLAGS} ${LDFLAGS} -lopenblas/g' Makefile
 
+sed -i'.bak' 's/asm (".symver memcpy, memcpy@GLIBC_2.2.5");//g' memcpy.cpp
+
 make CC=$CC
 
 mkdir -p ${PREFIX}/bin
