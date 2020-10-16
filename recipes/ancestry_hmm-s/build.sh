ac18ac752fd3bbfa88f60e9f06ed09231f5379bc
Jesper Svedberg <jsvedber@ucsc.edu>
2020-10-16 17:12:22 +0200

    New attempt to add OSX specific build instructions

--- recipes/ancestry_hmm-s/build.sh
+++ recipes/ancestry_hmm-s/build.sh
@@ -3,14 +3,14 @@
 # conda on macOS will fall back to libstdc++
 # which lacks a ton of standard C++11 headers
 if [[ $(uname) == Darwin ]]; then
-	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+	sed -i.bak 's/-std=c++11/-std=c++11 -stdlib=libc++/' Makefile
 fi
 
 cd src
 
 sed -i.bak 's#ahmms.cpp#ahmms.cpp -L ${PREFIX}/lib/ -I ${PREFIX}/include/#' Makefile 
 
-make CXX=$CXX CXXFLAGS=$CXXFLAGS
+make CXX=$CXX
 
 mkdir -p $PREFIX/bin
 cp ahmm-s $PREFIX/bin 
