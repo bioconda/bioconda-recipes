c515d0dd2190e9963ec31477ea9d4b53afcc2e85
Jesper Svedberg <jsvedber@ucsc.edu>
2020-10-16 19:08:33 +0200

    Fixed Makefile.

--- recipes/ancestry_hmm-s/build.sh
+++ recipes/ancestry_hmm-s/build.sh
@@ -2,11 +2,13 @@
 
 # conda on macOS will fall back to libstdc++
 # which lacks a ton of standard C++11 headers
+
+cd src
+
 if [[ $(uname) == Darwin ]]; then
 	sed -i.bak 's/-std=c++11/-std=c++11 -stdlib=libc++/' Makefile
 fi
 
-cd src
 
 sed -i.bak 's#ahmms.cpp#ahmms.cpp -L ${PREFIX}/lib/ -I ${PREFIX}/include/#' Makefile 
 sed -i.bak 's/ $(TCFLAGS)//' Makefile
