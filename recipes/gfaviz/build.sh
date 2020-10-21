ae725ca9e1dac4a5194b5bdefc80c701840a1359
Andreas Sjödin <andreas.sjodin@gmail.com>
2020-10-22 00:23:47 +0200

    Update build.sh

--- recipes/gfaviz/build.sh
+++ recipes/gfaviz/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
 
 qmake
-make CC=$(CC) CXX=$(CXX) CXXFLAGS="$(CXXFLAGS)" CFLAGS="$(CFLAGS)"
+make CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"  CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 cp viz.x $PREFIX/bin
 
