71f13cc608c9c0960578e9772c713561ce657207
Nathan Roach <nroach2@jhu.edu>
2020-10-24 11:35:45 -0400

    updating build script

--- recipes/shasta/build.sh
+++ recipes/shasta/build.sh
@@ -5,3 +5,13 @@
 # cmake ../shasta/
 # make all -j
 # make install
+if [[ "$OSTYPE" == "darwin"* ]];then
+  mv shasta-macOS-0.6.0 shasta
+fi
+if [[ "$OSTYPE" == "linux-gnu"* ]];then
+  mv shasta-Linux-0.6.0 shasta
+fi
+
+chmod +x shasta
+mkdir -p $PREFIX/bin
+cp shasta $PREFIX/bin/shasta
