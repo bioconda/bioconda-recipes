fb7d807a2843615cdc96aee0e8b20ab4bfbfc11f
Devon Ryan <dpryan79@users.noreply.github.com>
2020-10-25 14:33:51 +0100

    Update build.sh

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -eu
 
-ls
-cd *-release-*/x86_64-etch-linux-gnu
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $outdir
 mkdir -p $PREFIX/bin
-cp -r bin $outdir
-cp -r etc $outdir
-cp -r lib $outdir
-cp -r share $outdir
-ln -s $outdir/bin/bam* $PREFIX/bin
+
+./configure --with-libmaus2=${PREFIX}/lib	--prefix=${PREFIX}/bin/biobambam2
+make install
