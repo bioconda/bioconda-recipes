c6658422ed58ed972244d0028b2b594a45c798cb
Matus Kosut <matus.kosut@ntnu.no>
2020-10-28 14:44:19 +0100

    try without extra settings

--- recipes/savvy/build.sh
+++ recipes/savvy/build.sh
@@ -2,23 +2,9 @@
 set -e
 set -x
 
-export LIBRARY_PATH="${LIBRARY_PATH}:${PREFIX}/lib:${PREFIX}/lib64"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib:${PREFIX}/lib64"
-#export DYLD_LIBRARY_PATH=${PREFIX}/lib
-
 mkdir -p build
 cd build
 
-if [[ $(uname -s) == Darwin ]]; then
-  RPATH='@loader_path/../lib'
-else
-  ORIGIN='$ORIGIN'
-  export ORIGIN
-  RPATH='$${ORIGIN}/../lib:$${ORIGIN}/../lib64'
-fi
-LDFLAGS='-Wl,-rpath,${RPATH}'
-
-# -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++"
 cmake \
   -DBUILD_SHARED_LIBS:BOOL=ON \
   -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
