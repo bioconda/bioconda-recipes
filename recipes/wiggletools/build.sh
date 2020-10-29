bc4fe69b31d4908d1078e3c1da6eb684771422b3
Devon Ryan <dpryan79@users.noreply.github.com>
2020-10-29 11:30:03 +0100

    Update build.sh

--- recipes/wiggletools/build.sh
+++ recipes/wiggletools/build.sh
@@ -5,7 +5,7 @@ mkdir -p lib
 cp -r $PREFIX/include/* src/
 cp -r $PREFIX/lib/* lib/
 cp $PREFIX/lib/libBigWig.a lib/
-make LIBS="-lwiggletools -l:libBigWig.a -lcurl -l:libhts.a -lgsl  -lgslcblas -lz -lpthread -lm -llzma -lbz2 -ldl"
+make LIBS="-lwiggletools -l:libBigWig.a -lcurl -l:libhts.a -lgsl  -lgslcblas -lz -lpthread -lm -llzma -lbz2 -ldl -ldeflate"
 cp lib/libwiggletools.a $PREFIX/lib/
 cp inc/wiggletools.h $PREFIX/include/
 cp bin/wiggletools $PREFIX/bin/
