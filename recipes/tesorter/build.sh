93748b957b1658fb043487c333e3ff8b7b00250d
Jacques Dainat <jacques.dainat@nbis.se>
2020-10-29 21:18:54 +0100

    add hmmpress

---
+++ recipes/tesorter/build.sh
@@ -0,0 +1,5 @@
+# hmmscan in TEsorter need hmmpress first
+# see here https://github.com/oushujun/EDTA/issues/121
+ls -l
+cd TEsorter/database
+for i in `ls *hmm | sed 's/.hmm//g'`; do hmmpress ${i}.hmm > ../${i}.hmm;done
