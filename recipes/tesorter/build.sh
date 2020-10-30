d29dcdd97b2ec369512a74c069b702c97a618b24
Jacques Dainat <jacques.dainat@nbis.se>
2020-10-30 10:07:38 +0100

    no need to add hmmpress it is actually handle by TEsorter

--- recipes/tesorter/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-$PYTHON -m pip install --no-deps --ignore-installed .
-
-# hmmscan in TEsorter need hmmpress first
-# see here https://github.com/oushujun/EDTA/issues/121
-ls -l
-cd TEsorter/database
-for i in `ls *hmm | sed 's/.hmm//g'`; do hmmpress ${i}.hmm > ../${i}.hmm;done
