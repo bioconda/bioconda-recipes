07e2910eea5c702da6c7a663a6e4879e40048c41
Jacques Dainat <jacques.dainat@nbis.se>
2020-11-02 22:09:35 +0100

    add verbosity

--- recipes/tesorter/build.sh
+++ recipes/tesorter/build.sh
@@ -3,7 +3,12 @@ $PYTHON -m pip install --no-deps --ignore-installed .
 
 # hmmscan in TEsorter need hmmpress first
 # see here https://github.com/oushujun/EDTA/issues/121
+pwd
 ls -l
 echo "Run hmmpress to get the DB ready to use"
 cd TEsorter/database
+echo "database folder before"
+ls -l
 for i in `ls *hmm | sed 's/.hmm//g'`; do hmmpress ${i}.hmm > ../${i}.hmm;done
+echo "database folder after"
+ls -l
