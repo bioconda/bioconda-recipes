2c8cc34722a2d5d51e1e06afc3fa7a82132b3302
Jacques Dainat <jacques.dainat@nbis.se>
2020-11-03 21:32:36 +0100

    remove log

--- recipes/tesorter/build.sh
+++ recipes/tesorter/build.sh
@@ -3,18 +3,6 @@ $PYTHON -m pip install --no-deps --ignore-installed .
 
 # hmmscan in TEsorter need hmmpress first
 # see here https://github.com/oushujun/EDTA/issues/121
-echo "local path"
-pwd
-echo "Python's site-packages location"
-ls -l $SP_DIR
-ls -l $SP_DIR/TEsorter
-ls -l $SP_DIR/TEsorter/database
-echo "local dir"
-ls -l
 echo "Run hmmpress to get the DB ready to use"
 cd $SP_DIR/TEsorter/database
-echo "database folder before"
-ls -l
 for i in `ls *hmm | sed 's/.hmm//g'`; do hmmpress ${i}.hmm > ../${i}.hmm;done
-echo "database folder after"
-ls -l
