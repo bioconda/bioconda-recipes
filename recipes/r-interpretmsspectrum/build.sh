0f85e2b0f63bdd8a81b8f43589fe20f3fc668c7e
Martin Cech <marten@bx.psu.edu>
2020-11-10 14:05:53 +0100

    remove refs to win build

--- recipes/r-interpretmsspectrum/build.sh
+++ recipes/r-interpretmsspectrum/build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o pipefail
 
-if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
+if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
   grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
