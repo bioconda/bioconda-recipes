8b3616cafb11325b930f347c5ed2b0233275d84a
CarlosHorro <carlos.horro@gmail.com>
2020-11-12 13:36:34 +0100

    PeptideShaker version updated to 2.0.2

--- recipes/peptide-shaker/peptide-shaker.py
+++ recipes/peptide-shaker/peptide-shaker.py
@@ -14,7 +14,7 @@ from os import access
 from os import getenv
 from os import X_OK
 
-jar_file = 'PeptideShaker-2.0.0.jar'
+jar_file = 'PeptideShaker-2.0.2.jar'
 
 
 default_jvm_mem_opts = ['-Xms512m', '-Xmx1g']
