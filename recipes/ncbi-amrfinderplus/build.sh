35ff1346079dfdd56d99bb7788471e01f3a92dbc
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-11-12 20:55:37 -0500

    Remove patch b/c it is now part of source
    Patch was added to the AMRFinderPlus source

--- recipes/ncbi-amrfinderplus/build.sh
+++ recipes/ncbi-amrfinderplus/build.sh
@@ -9,24 +9,5 @@ set
 echo "make CXX=\"$CXX $LDFLAGS\" CPPFLAGS=\"$CXXFLAGS\" PREFIX=\"$PREFIX\" DEFAULT_DB_DIR=\"$PREFIX/share/amrfinderplus/data\""
 echo "END DEBUGGING environment info because PREFIX did not appear to be set"
 
-# try patching with some changes. This is necessary because CONDA_PREFIX is not set on test system. 
-# it should be set when using conda on the commandline
-cat <<END | patch amrfinder.cpp
---- a/amrfinder.cpp
-+++ b/amrfinder.cpp
-@@ -422,6 +422,11 @@ struct ThisApplication : ShellApplication
-     // we're in condaland
-       if (const char* s = getenv("CONDA_PREFIX")) {
-         defaultDb = string (s) + string ("/share/amrfinderplus/data/latest");
-+      } else if (const char* s = getenv("PREFIX")) {
-+        Warning warning (stderr);
-+        stderr << "This was compiled for running under bioconda, but $CONDA_PREFIX was not found" << "\n";
-+        defaultDb = string (s) + string ("/share/amrfinderplus/data/latest");
-+        stderr << "Reverting to $PREFIX: " << defaultDb;
-       } else {
-         Warning warning (stderr);
-         stderr << "This was compiled for running under bioconda, but $CONDA_PREFIX was not found" << "\n";
-END
-
 make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" DEFAULT_DB_DIR="$PREFIX/share/amrfinderplus/data"
 make install bindir=$PREFIX/bin
