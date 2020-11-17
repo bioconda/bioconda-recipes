a269da76102c8426e93ef8e6d37a83d2ef26309a
Matthias Bernt <m.bernt@ufz.de>
2020-11-17 21:49:34 +0100

    patch in build

--- recipes/cfm/build.sh
+++ recipes/cfm/build.sh
@@ -6,7 +6,8 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
-find . -type f -exec dos2unix {} \;
+find . -type f -exec sed -i 's/\r$//' {} \;
+patch -p 0 -u < $RECIPE_DIR/source.patch
 
 mkdir -p "$PREFIX/include/rdkit/External/INCHI-API"
 cp "$PREFIX/include/rdkit/GraphMol/inchi.h" "$PREFIX/include/rdkit/External/INCHI-API"
