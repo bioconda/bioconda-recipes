3979cae2ad0e084bb91e1efffbddcf8007d09a9b
Matus Kosut <matus.kosut@ntnu.no>
2020-11-22 10:39:19 +0100

    bump r-saige to 0.43

--- recipes/r-saige/build.sh
+++ recipes/r-saige/build.sh
@@ -2,7 +2,7 @@
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  rm configure
+  # rm configure
   rm src/*.so
   rm src/*.o
 
