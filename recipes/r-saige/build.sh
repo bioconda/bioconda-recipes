b284cfda6d400637ad8bbfad44d9f30051f7b6fc
Matúš Košút <matuskosut@users.noreply.github.com>
2020-11-23 09:54:38 +0100

    [ci skip] Merge PR 25482
    Merge PR #25482, commits were: 
     * libblas and remove from run
     * unite openblas
     * clean up fixed patches
     * reset build number
     * safety remove should not fail if not present
     * bump r-saige to 0.43

--- recipes/r-saige/build.sh
+++ recipes/r-saige/build.sh
@@ -2,9 +2,9 @@
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  rm configure
-  rm src/*.so
-  rm src/*.o
+  # rm -f configure
+  rm -f src/*.so
+  rm -f src/*.o
 
   $R CMD INSTALL --build .
   mkdir -p "${PREFIX}/lib/R/library/${PKG_NAME}/"
