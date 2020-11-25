37921a3690627a56f3dbace7f29103cc60b21fdb
thanhleviet <thanh.le-viet@quadram.ac.uk>
2020-11-25 18:25:41 +0000

    add kaptive

---
+++ recipes/kaptive/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p $PREFIX/opt/kaptip_reference_database
+mv ./reference_database/*.* $PREFIX/opt/kaptive_reference_database
---
+++ recipes/kaptive/post-link.sh
@@ -0,0 +1,4 @@
+echo "Kaptive is installed with its database at...$PREFIX/opt/kaptive_reference_database. When you run kaptive.py, you can specify
+path to one of the database as the following example
+kaptive.py -fa /path/to/a/file.fasta -k $PREFIX/opt/kaptive_reference_database/Klebsiella_k_locus_primary_reference.gbk -o output
+"
