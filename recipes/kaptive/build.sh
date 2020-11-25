ed7725de7490ad31e54d705ba6b2111c1cabf68a
thanhleviet <thanh.le-viet@quadram.ac.uk>
2020-11-25 23:00:10 +0000

    fix

--- recipes/kaptive/build.sh
+++ recipes/kaptive/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-mkdir -p $PREFIX/opt/kaptip_reference_database
-mv ./reference_database/*.* $PREFIX/opt/kaptive_reference_database
+python -m pip install . --ignore-installed --no-deps -vv
+mkdir -p $PREFIX/opt/kaptive_reference_database
+mkdir -p $PREFIX/opt/kaptive_sample_data
+
+mv $SRC_DIR/reference_database/*.* $PREFIX/opt/kaptive_reference_database/
+mv $SRC_DIR/sample_data/*.* $PREFIX/opt/kaptive_reference_database/
