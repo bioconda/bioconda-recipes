19f2bb7453c76742c01c47e85d3905f76ee3230b
Will Rowe <w.rowe@bham.ac.uk>
2020-11-26 20:06:28 +0000

    Update build.sh

--- recipes/artic-tools/build.sh
+++ recipes/artic-tools/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i.bak 's/Boost_USE_STATIC_LIBS OFF/Boost_USE_STATIC_LIBS ON/' CMakeLists.txt
-mkdir -p extlibs/flat_hash_map && extlibs/flat_hash_map
+mkdir -p extlibs/flat_hash_map && pushd extlibs/flat_hash_map
 wget https://raw.githubusercontent.com/skarupke/flat_hash_map/master/bytell_hash_map.hpp
 wget https://raw.githubusercontent.com/skarupke/flat_hash_map/master/flat_hash_map.hpp
 wget https://raw.githubusercontent.com/skarupke/flat_hash_map/master/unordered_map.hpp
