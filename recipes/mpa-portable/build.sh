adb38510e1bc7c48d1155bf8e8dc1b1bd2f29bdd
Matthias Bernt <m.bernt@ufz.de>
2020-11-27 23:16:37 +0100

    mpa: fix "missing" xml files
    - add comet and xtandem requirements
    - outline workaround to use the package
    - add fontconfig explicitly (seems not to come reliably with java>=11)
    
    follow up to https://github.com/bioconda/bioconda-recipes/pull/25508/
    
    - the precompiled comet and xtandem binaries did not work in a
      container due to missing libraries. hence I replace (link)
      them at build time with the ones from the corresponding conda packages
      (unfortunately I could not find out how to configure the paths
      to the binaries in MPA .. see
      https://github.com/compomics/meta-proteome-analyzer/issues/31)
    
    - MPA still complained about:
      `java.io.FileNotFoundException: /usr/local/share/mpa-portable-1.4.1-2/built/X!Tandem/linux/linux_64bit/taxonomy.xml (Permission denied)`
    
      the point is that MPA tries to write the input xml files for xtandem
      into this directory which does not work in a container and is a bad
      idea also for conda environments.
    
      a possible workaround is to copy the whole mpa dir into a temporary
      dir outside of the container and to replace the copies of the xtandem
      and comet binaries with links (I could not find out how to copy the
      links correctly). this is documented in the build script.
    
      I asked upstream for a possibility to configure the input paths to
      xtandem: https://github.com/compomics/meta-proteome-analyzer/issues/31

--- recipes/mpa-portable/build.sh
+++ recipes/mpa-portable/build.sh
@@ -8,8 +8,34 @@ cp -R * $outdir/
 cp $RECIPE_DIR/mpa-portable.py $outdir/mpa-portable
 ln -s $outdir/mpa-portable $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/mpa-portable"
-if [ -f $outdir/built/Comet/linux/comet.exe ] ; then chmod 0755 $outdir/built/Comet/linux/comet.exe ; fi
-if [ -f $outdir/built/X!Tandem/linux/linux_64bit/tandem ] ; then chmod 0755 $outdir/built/X!Tandem/linux/linux_64bit/tandem ; fi
-if [ -f $outdir/built/X!Tandem/linux/linux_32bit/tandem ] ; then chmod 0755 $outdir/built/X!Tandem/linux/linux_32bit/tandem ; fi
-find $outdir/ -type f -exec chmod ugo+r {} \;
-find $outdir/ -type d -exec chmod ugo+rx {} \;
+
+# overwrite the precompiled binaries for comet and sirius
+# that are shipped with mpa-portable by links to the binaries
+# of the conda packages (could theoreticallt be done also 
+# for msgf_plus, but the version seems older than the oldest
+# available in bioconda .. see issue below)
+
+# note when mpa write the input xml files to the folder containing
+# the (x)tandem binary (resp. link). this may fail for multi user
+# installations and containerized deployments (as well as other
+# read only deployments)
+# see https://github.com/compomics/meta-proteome-analyzer/issues/31
+
+# possible workaround: 
+# 
+# jar_dir=$(mpa-portable -get_jar_dir)
+# cp -R $jar_dir/conf .
+# cp -r "$jar_dir" .
+# local_jar_dir="$(basename $jar_dir)"
+# ln -sf "$jar_dir"'/built/X!Tandem/linux/linux_64bit/tandem' "$local_jar_dir"'/built/X!Tandem/linux/linux_64bit/tandem' &&
+# ln -sf "\$jar_dir"'/built/Comet/linux/comet.exe' "\$local_jar_dir"'/built/Comet/linux/comet.exe' &&
+# 
+# "$local_jar_dir"/mpa-portable de.mpa.cli.CmdLineInterface ....
+
+if [ -f "$outdir/built/Comet/linux/comet.exe" ]; then
+    ln -sf $(which comet.exe)  "$outdir/built/Comet/linux/comet.exe"; 
+fi
+
+for i in 32 64; do
+    ln -sf "$(which xtandem)" "$outdir"'/built/X!Tandem/linux/linux_'"$i""bit/tandem"
+done
