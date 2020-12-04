1357592531423f619b5528cb745782eaac60dcf9
Thomas Gatter <thomas@bioinf.uni-leipzig.de>
2020-12-04 11:19:11 +0100

    debug

--- recipes/ryuto/cross-linux.cmake
+++ recipes/ryuto/cross-linux.cmake
@@ -12,10 +12,10 @@ set(CMAKE_CXX_COMPILER $ENV{CXX})
 set(CMAKE_FIND_ROOT_PATH $ENV{PREFIX} $ENV{BUILD_PREFIX}/$ENV{HOST}/sysroot)
 
 # search for programs in the build host directories
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # god-awful hack because it seems to not run correct tests to determine this:
-set(__CHAR_UNSIGNED___EXITCODE 1)
+#set(__CHAR_UNSIGNED___EXITCODE 1)
