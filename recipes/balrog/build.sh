f050c11e95c331089da98c01c116653fde5976c5
Markus Sommer <markusjsommer@gmail.com>
2020-12-07 14:49:56 -0500

    ..

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" .
-cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(str(torch.utils.cmake_prefix_path) + "/Torch")'`
+cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
 cmake --build ./cmake-build-release --target Balrog -- -j 3
 make install
