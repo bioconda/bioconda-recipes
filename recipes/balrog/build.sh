7a2e7f2ba28f82b4d6aeac9c1a28638edf180c17
Markus Sommer <markusjsommer@gmail.com>
2020-12-07 16:03:40 -0500

    use os to fix path

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-export Torch_DIR=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'`
+export Torch_DIR=`python -c 'import torch;import os;print(os.path.join(torch.utils.cmake_prefix_path, "Torch"))'`
 cmake --build . --target Balrog -- -j 3
 make install
