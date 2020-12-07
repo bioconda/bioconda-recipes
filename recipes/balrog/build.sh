be65104e74bf55a41b830a9061c912c13967d7cb
Markus Sommer <markusjsommer@gmail.com>
2020-12-07 13:52:11 -0500

    use python to query pytorch path for cmake

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -3,5 +3,6 @@
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
 cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" .
+cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'`
 cmake --build ./cmake-build-release --target Balrog -- -j 3
 make install
