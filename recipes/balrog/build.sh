e6e865e2d6d94ef7e31b359f51a3a0b70f913c93
Markus Sommer <markusjsommer@gmail.com>
2020-12-07 15:41:07 -0500

    stricter pytorch-cpu version

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake -DCMAKE_MODULE_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
+cmake -DCMAKE_PREFIX_PATH=`python -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
 cmake --build . --target Balrog -- -j 3
 make install
