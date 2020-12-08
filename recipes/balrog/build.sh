b8ff48e9e542429d012202fb8286df4bd0a163ad
Markus Sommer <markusjsommer@gmail.com>
2020-12-08 16:37:33 -0500

    retry zlib troubleshooting

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
 cmake --build . --target Balrog -- -j 3
 make install
