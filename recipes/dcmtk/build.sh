e749fb60487d02bbc460e8a97a824748cad2a64e
Julien Lamy <lamy@unistra.fr>
2020-12-08 10:42:33 +0100

    Fix CMAKE_INSTALL_LIBDIR

--- recipes/dcmtk/build.sh
+++ recipes/dcmtk/build.sh
@@ -5,11 +5,13 @@ cd build
 
 # WARNING: with the default value of USE_COMPILER_HIDDEN_VISIBILITY (TRUE/ON),
 # link problems arise when linking (at least) ofstd.
+# WARNING: CMAKE_INSTALL_LIBDIR defaults to lib64, while conda expects lib
 cmake \
     -G Ninja \
     -D CMAKE_BUILD_TYPE:STRING=Release \
     -D BUILD_SHARED_LIBS:BOOL=TRUE \
     -D USE_COMPILER_HIDDEN_VISIBILITY:BOOL=FALSE \
+    -D CMAKE_INSTALL_LIBDIR:PATH=lib \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
     -D ICU_ROOT="${CONDA_PREFIX}" \
     -D OPENSSL_ROOT_DIR="${CONDA_PREFIX}" \
