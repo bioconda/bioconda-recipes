40253961d9e796118c449100a149a32d366e7c46
Devon Ryan <dpryan79@users.noreply.github.com>
2020-12-08 10:42:23 +0100

    Update build.sh

--- recipes/genotyphi/build.sh
+++ recipes/genotyphi/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -ex
+
 mkdir -p $PREFIX/bin
 cp genotyphi.py $PREFIX/bin/genotyphi.py
 ln $PREFIX/bin/genotyphi.py $PREFIX/bin/genotyphi
 chmod +x $PREFIX/bin/genotyphi.py $PREFIX/bin/genotyphi
-python $PREFIX/bin/genotyphi -h
