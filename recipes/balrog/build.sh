bcebf55505ae939ace068235b9a0e998ba461c09
Markus Sommer <markusjsommer@gmail.com>
2020-12-08 15:04:23 -0500

    add different zlib exports...again

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 
 mkdir build
 cd build
