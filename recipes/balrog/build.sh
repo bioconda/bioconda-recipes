5d25e02e2beaca05d392559c2b95206848c7189a
Markus Sommer <markusjsommer@gmail.com>
2020-12-09 14:41:39 -0500

    change make install

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -7,4 +7,4 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
 make -j${CPU_COUNT} ${VERBOSE_CM}
-make install
+make install prefix=$PREFIX CXX=$CXX CC=$CC LDFLAGS="-L$PREFIX/lib"
