cf66c7efbe16741032c7424cedc4ee24ed945333
Markus Sommer <markusjsommer@gmail.com>
2020-12-09 14:25:51 -0500

    change make install

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -5,6 +5,6 @@ export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
-cmake --build . --target Balrog -- -j 3
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
+make -j${CPU_COUNT} ${VERBOSE_CM}
 make install
