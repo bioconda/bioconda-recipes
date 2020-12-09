459c06c132d460be65d818ad02313868a91edeae
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2020-12-09 11:42:23 +0100

    Update build.sh

--- recipes/crisprme/build.sh
+++ recipes/crisprme/build.sh
@@ -3,5 +3,6 @@
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/opt/crisprme"
 chmod -R 700 .
-cp crisprme.py "${PREFIX}/bin/"
+mv crisprme.py crisprme
+cp crisprme "${PREFIX}/bin/"
 cp -R auto_search_corrected "${PREFIX}/opt/crisprme/"
