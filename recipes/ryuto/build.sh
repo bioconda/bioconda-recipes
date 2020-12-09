771898f5e5ec7f7e460fec93fb96ec71694fead6
Thomas Gatter <thomas@bioinf.uni-leipzig.de>
2020-12-09 22:16:58 +0100

    mac debug

--- recipes/ryuto/build.sh
+++ recipes/ryuto/build.sh
@@ -3,34 +3,10 @@
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-#cd $RECIPE_DIR
+cd extern
+cwd=$(pwd)
 
-#cd omp_test
-
-#./reset_autogen.sh
-#./configure
-#make
-
-#./configure CFLAGS="-fopenmp" CXXFLAGS='-fopenmp'
-#make
-
-#./configure --enable-mac
-#make
-
-#exit 0
-
-# RESET AUTOMAKE just in case for RYUTO
-
-./reset_autogen.sh
-
-# Build static libraries. Sadly Lemon from Forge cannot be linked to CLP correctly. So this is a workaround for now.
-
-echo "Recipe Dir"
-echo $RECIPE_DIR
-echo "Source Dir"
-echo $SRC_DIR
-
-cd $RECIPE_DIR
+mkdir libs
 
 unzip clp_mod.zip
 tar -xvf lemon_mod.tar.gz
@@ -39,42 +15,27 @@ cd clp_mod
 
 echo "========================== CLP =========================="
 
-./configure --enable-static --disable-shared --prefix=`pwd` --disable-bzlib --disable-zlib
+./configure --enable-static --disable-shared --prefix=$cwd/libs --disable-bzlib --disable-zlib
 make
 make install
 
-
-
 cd ../lemon_mod
-
 
 echo "========================== LEMON =========================="
 
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" -DLEMON_DEFAULT_LP=CLP -DCOIN_ROOT_DIR=$RECIPE_DIR/clp_mod -DCMAKE_INSTALL_PREFIX=$RECIPE_DIR/lemon_mod  ..
-echo "========================== cmake done"
+cmake -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" -DLEMON_DEFAULT_LP=CLP -DCOIN_ROOT_DIR=$cwd/libs -DCMAKE_INSTALL_PREFIX=$cwd/libs  ..
 make
-echo "========================== make done"
 make install 
-echo "========================== make install done"
 
-#echo "========================== CMAKE LOG =========================="
-#cat CMakeFiles/CMakeOutput.log
-#echo "========================== CMAKE ERR =========================="
-#cat CMakeFiles/CMakeError.log
-#echo "========================== CMAKE VAL =========================="
-#cat CMakeCache.txt
+echo "========================== DONE =========================="
 
-# RUN RYUTO
+cd ..
 
-cd $SRC_DIR
 echo "========================== RYUTO =========================="
 
-./configure --prefix=$PREFIX --with-htslib="$PREFIX" --with-zlib="$PREFIX" --with-boost="$PREFIX" --with-clp=$RECIPE_DIR/clp_mod --with-staticcoin=$RECIPE_DIR/clp_mod --with-lemon=$RECIPE_DIR/lemon_mod
+./configure --prefix=$PREFIX --with-htslib="$PREFIX" --with-zlib="$PREFIX" --with-boost="$PREFIX" --with-clp=$cwd/libs --with-staticcoin=$cwd/libs --with-lemon=$cwd/libs
 
-
-echo "========================== configure done"
 make LIBS+=-lhts
-echo "========================== make done"
 make install
