05b2e05f20e28cd8d4531eed4dd0851768512a45
SH Tausch <misterioes16@gmail.com>
2020-12-14 18:26:24 +0100

    copy init.py too

--- recipes/chewiesnake/build.sh
+++ recipes/chewiesnake/build.sh
@@ -6,7 +6,9 @@ cp chewieSnake.py $PREFIX/bin/chewiesnake
 
 cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
 
-cp -r scripts/* $PREFIX/bin/
+cp scripts/*.py *.R *.sh $PREFIX/bin/
+
+cp scripts/common/__init__.py $PREFIX/bin/
 
 chmod -R u+x $PREFIX/bin/*.sh $PREFIX/bin/*.R $PREFIX/bin/*.py
 
