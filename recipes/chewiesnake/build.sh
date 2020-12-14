80fba3e2329ce9b1bcbd2ab80a780f466677db9e
SH Tausch <misterioes16@gmail.com>
2020-12-14 18:48:39 +0100

    bugfix for copying

--- recipes/chewiesnake/build.sh
+++ recipes/chewiesnake/build.sh
@@ -6,9 +6,7 @@ cp chewieSnake.py $PREFIX/bin/chewiesnake
 
 cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
 
-cp scripts/*.py *.R *.sh $PREFIX/bin/
-
-cp scripts/common/__init__.py $PREFIX/bin/
+cp scripts/*.py scripts/*.R scripts/*.sh scripts/common/*.py $PREFIX/bin/
 
 chmod -R u+x $PREFIX/bin/*.sh $PREFIX/bin/*.R $PREFIX/bin/*.py
 
