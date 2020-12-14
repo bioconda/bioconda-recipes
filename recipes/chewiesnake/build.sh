6f324edb35bf19588c4f9038ff0d25bff0b228de
SH Tausch <misterioes16@gmail.com>
2020-12-14 15:58:41 +0100

    copy instead of softlinking in build.sh

--- recipes/chewiesnake/build.sh
+++ recipes/chewiesnake/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p $PREFIX/bin
 
-ln -s $PREFIX/chewieSnake.py $PREFIX/bin/chewiesnake
+cp chewieSnake.py $PREFIX/bin/chewiesnake
 
-ln -s $PREFIX/chewieSnake_join.py $PREFIX/bin/chewiesnake_join
+cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
 
-ln -s $PREFIX/scripts/ $PREFIX/bin/
+cp scripts/ $PREFIX/bin/
 
 chmod -R u+x $PREFIX/bin/*
 
