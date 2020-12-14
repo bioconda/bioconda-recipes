77bc8d8d623b3b93536925c0cc7baaf64b73a531
Markus Sommer <markusjsommer@gmail.com>
2020-12-14 13:59:06 -0500

    cmake rpath set

---
+++ recipes/balrog/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
+make -j${CPU_COUNT} ${VERBOSE_CM}
+make install prefix=$PREFIX CXX=$CXX CC=$CC LDFLAGS="-L$PREFIX/lib"
