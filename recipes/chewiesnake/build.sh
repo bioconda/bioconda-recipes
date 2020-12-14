bab9b635ff8c4948629fd38a715f765f26001bd2
SH Tausch <misterioes16@gmail.com>
2020-12-14 17:34:09 +0100

    copy script files instead of folder

--- recipes/chewiesnake/build.sh
+++ recipes/chewiesnake/build.sh
@@ -6,7 +6,7 @@ cp chewieSnake.py $PREFIX/bin/chewiesnake
 
 cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
 
-cp -r scripts/ $PREFIX/bin/
+cp scripts/* $PREFIX/bin/
 
 chmod -R u+x $PREFIX/bin/*
 
