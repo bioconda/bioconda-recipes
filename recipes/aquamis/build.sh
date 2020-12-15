4f9b83493e615c266c6abe614705b76e372027b3
SH Tausch <misterioes16@gmail.com>
2020-12-15 11:58:57 +0100

    fix R suffix to Rmd in scriptfolder

--- recipes/aquamis/build.sh
+++ recipes/aquamis/build.sh
@@ -4,6 +4,6 @@ mkdir -p $PREFIX/bin
 
 cp aquamis.py $PREFIX/bin/aquamis
 
-cp scripts/*.py scripts/*.R scripts/*.sh $PREFIX/bin/
+cp scripts/*.py scripts/*.Rmd scripts/*.sh $PREFIX/bin/
 
 chmod -R u+x $PREFIX/bin/*.sh $PREFIX/bin/*.R $PREFIX/bin/*.py $PREFIX/bin/aquamis 
