61db1c35a39ad5da298cb2d622ed7c0a987a2837
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2020-12-15 23:36:42 +0100

    Fix ParGenes

--- recipes/pargenes/build.sh
+++ recipes/pargenes/build.sh
@@ -3,7 +3,7 @@
 # pthreads
 CXX=mpicxx ./install.sh
 ./checker.sh
-ln -s pargenes/pargenes.py ${PREFIX}/bin/pargenes.py
-ln -s pargenes/pargenes-hpc.py ${PREFIX}/bin/pargenes-hpc.py
-ln -s pargenes/pargenes-hpc-debug.py ${PREFIX}/bin/pargenes-hpc-debug.py
+ln -s "$(pwd)pargenes/pargenes.py" ${PREFIX}/bin/pargenes.py
+ln -s "$(pwd)pargenes/pargenes-hpc.py" ${PREFIX}/bin/pargenes-hpc.py
+ln -s "$(pwd)pargenes/pargenes-hpc-debug.py" ${PREFIX}/bin/pargenes-hpc-debug.py
 
