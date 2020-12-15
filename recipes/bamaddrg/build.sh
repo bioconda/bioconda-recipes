b8ecb96860251b7a681a70a13111545a2a92e328
Jacques Dainat <jacques.dainat@nbis.se>
2020-12-15 16:50:57 +0100

    Update build.sh

--- recipes/bamaddrg/build.sh
+++ recipes/bamaddrg/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-export INCLUDE_PATH="${PREFIX}/include/:${PREFIX}/include/bamtools/"
+export INCLUDE_PATH="${PREFIX}/include/"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+      -DZLIB_ROOT=$PREFIX \
 
 
 sed -i.bak "s#g++#$CXX#g" Makefile
