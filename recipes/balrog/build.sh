8cfaa04479dcf6a7f12469dcf4dddf83371086ea
Marcel Bargull <marcel.bargull@udo.edu>
2020-12-16 16:25:23 +0100

    balrog: Remove redundant flags, use $CPU_COUNT

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
-make -j 4
+make -j "${CPU_COUNT}"
 make install
