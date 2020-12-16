09b48c34aa6cd9c126706ff5585a42be7b0cd6e2
Jacques Dainat <jacques.dainat@nbis.se>
2020-12-16 13:53:08 +0100

    ADd cmake flags to be used in the Makefile

--- recipes/bamaddrg/build.sh
+++ recipes/bamaddrg/build.sh
@@ -6,12 +6,10 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-      -DCMAKE_CXX_COMPILER=${CXX} \
-      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
-      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-      -DZLIB_ROOT=$PREFIX \
+CMAKE_FLAGS="-DZLIB_ROOT=$PREFIX"
 
 
 sed -i.bak "s#g++#$CXX#g" Makefile
+sed -i.bak2 "s#cmake#cmake $CMAKE_FLAGS#g" Makefile
+
 make CXX=${CXX} 
