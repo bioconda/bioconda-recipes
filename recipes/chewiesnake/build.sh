d19b0844bbe108d7b280d0275cfa75c80f5f8ee3
SH Tausch <misterioes16@gmail.com>
2020-12-16 13:09:50 +0100

    copy only exposed scripts to bin

--- recipes/chewiesnake/build.sh
+++ recipes/chewiesnake/build.sh
@@ -4,17 +4,9 @@ mkdir -p $PREFIX/bin
 
 mkdir -p $PREFIX/opt/chewiesnake/
 
-#cp chewieSnake.py $PREFIX/bin/chewiesnake
-
-#cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
-
-#cp scripts/*.py scripts/*.R scripts/*.sh scripts/common/*.py $PREFIX/bin/
-
 cp -r * $PREFIX/opt/chewiesnake/
 
-#chmod -R u+x $PREFIX/bin/*.sh $PREFIX/bin/*.R $PREFIX/bin/*.py $PREFIX/bin/chewiesnake $PREFIX/bin/chewiesnake_join
-
-ln -s $PREFIX/opt/chewiesnake/scripts/*.R $PREFIX/opt/chewiesnake/scripts/*.sh $PREFIX/opt/chewiesnake/scripts/*.py  $PREFIX/bin/
+ln -s $PREFIX/opt/chewiesnake/scripts/Clustering_DistanceMatrix.R $PREFIX/opt/chewiesnake/scripts/alleleprofile_hasher.py $PREFIX/opt/chewiesnake/scripts/hashID.py $PREFIX/opt/chewiesnake/scripts/create_alleledbSheet.sh $PREFIX/opt/chewiesnake/scripts/create_sampleSheet.sh $PREFIX/bin/
 
 ln -s $PREFIX/opt/chewiesnake/chewieSnake.py $PREFIX/bin/chewiesnake
 
