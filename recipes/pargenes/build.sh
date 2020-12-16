ea13b0397a80da87dcda0d1dedef87c88f2cbdf3
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2020-12-16 21:11:05 +0100

    fix pargenes

--- recipes/pargenes/build.sh
+++ recipes/pargenes/build.sh
@@ -3,7 +3,23 @@
 # pthreads
 CXX=mpicxx ./install.sh
 ./checker.sh
-ln -s "$(pwd)pargenes/pargenes.py" ${PREFIX}/bin/pargenes.py
-ln -s "$(pwd)pargenes/pargenes-hpc.py" ${PREFIX}/bin/pargenes-hpc.py
-ln -s "$(pwd)pargenes/pargenes-hpc-debug.py" ${PREFIX}/bin/pargenes-hpc-debug.py
+#ln -s "$(pwd)pargenes/pargenes.py" ${PREFIX}/bin/pargenes.py
+#ln -s "$(pwd)pargenes/pargenes-hpc.py" ${PREFIX}/bin/pargenes-hpc.py
+#ln -s "$(pwd)pargenes/pargenes-hpc-debug.py" ${PREFIX}/bin/pargenes-hpc-debug.py
 
+dest=${PREFIX}/bin
+mkdir -p ${dest}
+
+# copy over all relevant files for the package
+for file in raxml-ng modeltest-ng mpi-scheduler; do
+  find . -type f -name ${file} -print -exec cp --parents '{}' ${dest} \;
+done
+
+for patt in ".*/pargenes/.*\.py" ".*\.so" ".*\.jar" ".*\.dylib"; do
+  find . -type f -regextype posix-egrep -regex "${patt}" -print -exec cp --parents '{}' ${dest} \;
+done
+
+cd ${dest}
+ln -s "$(pwd)pargenes/pargenes.py" "$(pwd)pargenes.py"
+ln -s "$(pwd)pargenes/pargenes-hpc.py" "$(pwd)pargenes-hpc.py"
+ln -s "$(pwd)pargenes/pargenes-hpc-debug.py" "$(pwd)pargenes-hpc-debug.py"
