f68f80ff65b1ef6f9f70d80f9d808b5f0d4a1dcb
SH Tausch <misterioes16@gmail.com>
2020-12-16 12:09:58 +0100

    copy complete repo into opt/ and link to it

--- recipes/chewiesnake/build.sh
+++ recipes/chewiesnake/build.sh
@@ -2,11 +2,22 @@
 
 mkdir -p $PREFIX/bin
 
-cp chewieSnake.py $PREFIX/bin/chewiesnake
+mkdir -p $PREFIX/opt/chewiesnake/
 
-cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
+#cp chewieSnake.py $PREFIX/bin/chewiesnake
 
-cp scripts/*.py scripts/*.R scripts/*.sh scripts/common/*.py $PREFIX/bin/
+#cp chewieSnake_join.py $PREFIX/bin/chewiesnake_join
 
-chmod -R u+x $PREFIX/bin/*.sh $PREFIX/bin/*.R $PREFIX/bin/*.py $PREFIX/bin/chewiesnake $PREFIX/bin/chewiesnake_join
+#cp scripts/*.py scripts/*.R scripts/*.sh scripts/common/*.py $PREFIX/bin/
 
+cp -r * $PREFIX/opt/chewiesnake/
+
+#chmod -R u+x $PREFIX/bin/*.sh $PREFIX/bin/*.R $PREFIX/bin/*.py $PREFIX/bin/chewiesnake $PREFIX/bin/chewiesnake_join
+
+ln -s $PREFIX/opt/chewiesnake/scripts/*.R $PREFIX/opt/chewiesnake/scripts/*.sh $PREFIX/opt/chewiesnake/scripts/*.py  $PREFIX/bin/
+
+ln -s $PREFIX/opt/chewiesnake/chewieSnake.py $PREFIX/bin/chewiesnake
+
+ln -s $PREFIX/opt/chewiesnake/chewieSnake_join.py $PREFIX/bin/chewiesnake_join
+
+chmod -R u+x $PREFIX/bin/*
