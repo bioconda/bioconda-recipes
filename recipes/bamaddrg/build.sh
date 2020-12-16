79a805f69e6d5f394f9016538b7c89a220a7eed6
Jacques Dainat <jacques.dainat@nbis.se>
2020-12-16 17:25:18 +0100

    Update build.sh

--- recipes/bamaddrg/build.sh
+++ recipes/bamaddrg/build.sh
@@ -8,19 +8,20 @@ export INCLUDE_PATH="${PREFIX}/include/"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-which bamtools
-echo "print prefix"
-ls -s ${PREFIX}
-echo "print prefix lib"
-ls -s ${PREFIX}/lib
-echo "print prefix share"
-ls -s ${PREFIX}/share
+echo "print buildprefix"
+ls -s ${BUILD_PREFIX}
+echo "print buildprefix lib"
+ls -s ${BUILD_PREFIX}/lib
+echo "print buildprefix share"
+ls -s ${BUILD_PREFIX}/share
+
+
 
 # prepare lib installed from bamtools
-cp ${PREFIX}/lib/libbamtools.a .
-cp -r ${PREFIX}/include/bamtools/api/ .
+cp ${BUILD_PREFIX}/lib/libbamtools.a .
+cp -r ${BUILD_PREFIX}/include/bamtools/api/ .
 for i in api/*.h;do sed -i.bak "s#api/##g" $i;done
-cp -r ${PREFIX}/include/bamtools/shared/ api/
+cp -r ${BUILD_PREFIX}/include/bamtools/shared/ api/
 for i in api/shared/*.h;do sed -i.bak "s#shared/##g" $i;done
 
 # prepare flag
