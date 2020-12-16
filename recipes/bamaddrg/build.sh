04d61fa3ce88fb364a5c42d9caab6d1a331beb49
Jacques Dainat <jacques.dainat@nbis.se>
2020-12-16 14:21:25 +0100

    Update build.sh

--- recipes/bamaddrg/build.sh
+++ recipes/bamaddrg/build.sh
@@ -4,8 +4,6 @@ export INCLUDE_PATH="${PREFIX}/include/"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-mkdir -p build
-cd build
 CMAKE_FLAGS="-DZLIB_ROOT=$PREFIX"
 
 
