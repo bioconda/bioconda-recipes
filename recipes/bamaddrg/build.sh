9de587f3702db53d9fea3ac39e5a040b85e25a7d
Jacques Dainat <jacques.dainat@nbis.se>
2020-12-16 13:45:21 +0100

    Update build.sh

--- recipes/bamaddrg/build.sh
+++ recipes/bamaddrg/build.sh
@@ -4,6 +4,8 @@ export INCLUDE_PATH="${PREFIX}/include/"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
+mkdir -p build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_CXX_COMPILER=${CXX} \
       -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
