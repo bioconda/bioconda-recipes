afe9d33cebded00d9afd870aa194223c27df09ec
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2020-12-17 20:47:02 +0100

    Fix Pargenes with Pierre's version and the new pargenes tag

--- recipes/pargenes/build.sh
+++ recipes/pargenes/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# pthreads
+# Make m4 accessible to bison
+# https://github.com/conda-forge/bison-feedstock/issues/7#issuecomment-431602144
+export M4=m4
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+
+# build
 CXX=mpicxx ./install.sh
-./checker.sh
-#ln -s "$(pwd)pargenes/pargenes.py" ${PREFIX}/bin/pargenes.py
-#ln -s "$(pwd)pargenes/pargenes-hpc.py" ${PREFIX}/bin/pargenes-hpc.py
-#ln -s "$(pwd)pargenes/pargenes-hpc-debug.py" ${PREFIX}/bin/pargenes-hpc-debug.py
 
 dest=${PREFIX}/bin
 mkdir -p ${dest}
@@ -20,6 +23,6 @@ for patt in ".*/pargenes/.*\.py" ".*\.so" ".*\.jar" ".*\.dylib"; do
 done
 
 cd ${dest}
-ln -s "pargenes/pargenes.py" "pargenes.py"
-ln -s "pargenes/pargenes-hpc.py" "pargenes-hpc.py"
-ln -s "pargenes/pargenes-hpc-debug.py" "pargenes-hpc-debug.py"
+ln -s pargenes/pargenes.py pargenes.py
+ln -s pargenes/pargenes-hpc.py pargenes-hpc.py
+ln -s pargenes/pargenes-hpc-debug.py pargenes-hpc-debug.py
