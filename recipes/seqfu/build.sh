2cc55459e629c6944bfd71e614fe35fceea62b57
Andrea Telatin <andrea@telatin.com>
2020-12-18 16:50:31 +0000

    Update SeqFu [new repository]

--- recipes/seqfu/build.sh
+++ recipes/seqfu/build.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
- 
-mkdir -p ${PREFIX}/bin
-chmod +x ./bin/*
-mv ./bin/* ${PREFIX}/bin/
-mv seqfu ${PREFIX}/bin/
+#!/bin/sh
+
+set -euxo pipefail
+
+if [[ $OSTYPE == "darwin"* ]]; then
+  export HOME="/Users/distiller"
+fi
+
+mkdir -p $PREFIX/bin
+
+nimble install -y --verbose argparse docopt terminaltables
+nim c --threads:on -p:lib --opt:speed -d:release -o:$PREFIX/bin/seqfu src/sfu.nim
+
+for SOURCE in src/fu_*.nim;
+do
+	nim c --threads:on  -p:lib --opt:speed -d:release -o:"$PREFIX/bin/${SOURCE/_/-}" "$SOURCE"
+done
