41dee3c006d5100591b953a8ef564f3eee1e8b7c
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2020-12-19 20:33:19 +0100

    Fix Pargenes

--- recipes/pargenes/build.sh
+++ recipes/pargenes/build.sh
@@ -13,16 +13,20 @@ CXX=mpicxx ./install.sh
 dest=${PREFIX}/bin
 mkdir -p ${dest}
 
+echo "Pargenes installer: copying the three main executables"
 # copy over all relevant files for the package
 for file in raxml-ng modeltest-ng mpi-scheduler; do
   find . -type f -name ${file} -print -exec cp --parents '{}' ${dest} \;
 done
 
+echo "Pargenes installer: copying relevant python and binary files"
 for patt in ".*/pargenes/.*\.py" ".*\.so" ".*\.jar" ".*\.dylib"; do
   find . -type f -regextype posix-egrep -regex "${patt}" -print -exec cp --parents '{}' ${dest} \;
 done
 
+echo "Pargenes installer: before cd ${dest}"
 cd ${dest}
+echo "Pargenes installer: Before ln"
 ln -s pargenes/pargenes.py pargenes.py
 ln -s pargenes/pargenes-hpc.py pargenes-hpc.py
 ln -s pargenes/pargenes-hpc-debug.py pargenes-hpc-debug.py
