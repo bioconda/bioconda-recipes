2a1dacaf0bd4fcff715b819802c86e3e09d2ea74
Li <mourisl@hotmail.com>
2020-12-29 12:04:21 -0500

    Add recipe psiclass

--- recipes/psiclass/build.sh
+++ recipes/psiclass/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#sed -i.bak 's/-f .\/samtools-0.1.19\/libbam.a/1/' Makefile
+sed -i.bak 's/-f .\/samtools-0.1.19\/libbam.a/1/' Makefile
 make \
     CXX="${CXX}" \
     CXXFLAGS="${CXXFLAGS} -Wformat -O3" \
-    LINKPATH="${LDFLAGS}"
+    LINKPATH="${LDFLAGS} -I./samtools-0.1.19"
 install -d "${PREFIX}/bin"
 install \
     psiclass \
