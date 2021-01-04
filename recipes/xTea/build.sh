fdd8e7953dfefa1843ca78fadd7cfca5df82be66
SimonCChu <chong.simon.chu@gmail.com>
2021-01-04 16:39:09 +0800

    xTea push 2

--- recipes/xTea/build.sh
+++ recipes/xTea/build.sh
@@ -2,5 +2,5 @@ mkdir -p $PREFIX/bin
 cp bin/xtea $PREFIX/bin
 cp bin/xtea_hg19 $PREFIX/bin
 mkdir -p $PREFIX/lib/xtea
-cp xTea/*.{py} $PREFIX/lib/xtea/
-cp -r xTea/genotyping $PREFIX/lib/xtea/
+cp xtea/*.{py} $PREFIX/lib/xtea/
+cp -r xtea/genotyping $PREFIX/lib/xtea/
