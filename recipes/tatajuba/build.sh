5dd0d427e2b59730dd3c8a3d16f5404a7f32ca4d
Leonardo de Oliveira Martins (QIB) <leomrtns@gmail.com>
2021-01-05 16:38:47 +0000

    tatajuba software for homopolymeric tract length description

--- recipes/tatajuba/build.sh
+++ recipes/tatajuba/build.sh
@@ -5,8 +5,9 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
+# add biomcmc-lib to proper location and run autoreconf
 cd $SRC_DIR
-autoreconf -i
+sh ./autogen.sh
 # cd to location of Makefile and source
 mkdir build
 cd build
