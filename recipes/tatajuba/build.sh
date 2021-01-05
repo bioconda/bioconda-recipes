cea1a2e899cc6cc9e640849931ad96eeaa695b3d
Leonardo de Oliveira Martins (QIB) <leomrtns@gmail.com>
2021-01-05 10:56:51 +0000

    homopolymer tract finder

---
+++ recipes/tatajuba/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# https://bioconda.github.io/troubleshooting.html#zlib-errors
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
+cd $SRC_DIR
+autoreconf -i
+# cd to location of Makefile and source
+mkdir build
+cd build
+# assumes autoreconf was run and thus configure.sh is present
+$SRC_DIR/configure --prefix=$PREFIX
+make
+make install
+
