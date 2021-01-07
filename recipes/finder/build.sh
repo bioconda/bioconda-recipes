8beb8dc75721c91692807fcbdf4f4dd535cd6c3e
Sagnik Banerjee <sagnik.banerjee@ceres19-compute-55.scinet.local>
2021-01-07 13:50:53 -0600

    Push

--- recipes/finder/build.sh
+++ recipes/finder/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printenv > /project/maizegdb/sagnik/FINDER/whatsprefix
+#printenv > /project/maizegdb/sagnik/FINDER/whatsprefix
 cd "${SRC_DIR}/src/olego/"
 make
 cd "${SRC_DIR}/src/assemblies_psiclass_modified/"
@@ -13,4 +13,4 @@ cp "${SRC_DIR}/finder" "${PREFIX}/bin"
 cp "${SRC_DIR}/scripts/"* "${PREFIX}/bin/scripts"
 #cp "${SRC_DIR}/src/"* "${PREFIX}/src/"
 #ls -lhrt $PREFIX/bin > /project/maizegdb/sagnik/FINDER/prefix_bin
-ls -lhrt "${PREFIX}/bin/scripts" > /project/maizegdb/sagnik/FINDER/prefix_bin
+#ls -lhrt "${PREFIX}/bin/scripts" > /project/maizegdb/sagnik/FINDER/prefix_bin
