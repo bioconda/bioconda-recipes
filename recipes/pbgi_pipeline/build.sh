0ab19a2160f39c65cff9b062d3f6b70cf289e1be
sjlgg <3066268521@qq.com>
2021-01-12 20:52:42 +0800

    addpbgi

--- recipes/pbgi_pipeline/build.sh
+++ recipes/pbgi_pipeline/build.sh
@@ -5,6 +5,6 @@ echo sjlokok
 mkdir -p "${PREFIX}/bin"
 mv ./scripts/* "${PREFIX}/bin/"
 mv ./* "${PREFIX}/bin/"
-pip install datasketch
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple datasketch
 
 exit 0
