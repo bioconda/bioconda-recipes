c7e8b991812aced084d89ec153c74f2b5aafb3c5
Björn Grüning <bjoern@gruenings.eu>
2021-01-12 09:18:13 +0100

    Update build.sh

--- recipes/salsa2/build.sh
+++ recipes/salsa2/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
 sed -i.bak '1d' Makefile
 
 COMPILER=${CXX} make
