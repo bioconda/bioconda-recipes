3a9b64973e01b9751106e328ae24d934c12471bd
sjlgg <3066268521@qq.com>
2021-01-13 14:38:50 +0800

    addpbgi

--- recipes/pbgi_pipeline/build.sh
+++ recipes/pbgi_pipeline/build.sh
@@ -6,6 +6,7 @@ mkdir -p "${PREFIX}/bin"
 mv ./scripts/* "${PREFIX}/bin/"
 mv ./* "${PREFIX}/bin/"
 pip list
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple datasketch
+# pip install -i https://pypi.tuna.tsinghua.edu.cn/simple datasketch
+pip --trusted-host https://pypi.tuna.tsinghua.edu.cn/simple install datasketch
 
 exit 0
