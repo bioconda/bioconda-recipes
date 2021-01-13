2ffd925a3e64bf9f19f27cc3a5ba21ffde4f6f8c
Rhys Newell <rhys.newell@hdr.qut.edu.au>
2021-01-13 16:23:30 +1000

    Potential fixed no executable creation error

--- recipes/rosella/build.sh
+++ recipes/rosella/build.sh
@@ -15,10 +15,10 @@ C_INCLUDE_PATH=$PREFIX/include \
 LIBRARY_PATH=$PREFIX/lib \
 LD_LIBRARY_PATH=$CONDA_PREFIX/lib \
 LIBCLANG_PATH=$PREFIX/lib/libclang.so \
-cargo install --path ./ --force --root $PREFIX
+cargo install --path ./ --force
 # cp target/release/rosella $PREFIX/bin
 # cp target/release/remove_minimap2_duplicated_headers $PREFIX/bin
 
 # Install flight
 # cd flight/ && pip install . && cd ../
-python -m pip install --user git+https://github.com/rhysnewell/flight
+pip install git+https://github.com/rhysnewell/flight
