3cb27022e116323e2a9800e36de0d91cb4766e4b
sjlgg <3066268521@qq.com>
2021-01-13 14:24:52 +0800

    addpbgi

--- recipes/pbgi_pipeline/build.sh
+++ recipes/pbgi_pipeline/build.sh
@@ -5,6 +5,7 @@ echo sjlokok
 mkdir -p "${PREFIX}/bin"
 mv ./scripts/* "${PREFIX}/bin/"
 mv ./* "${PREFIX}/bin/"
+pip list
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple datasketch
 
 exit 0
