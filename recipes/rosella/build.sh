0010826305564905919c7ca10b2f19cc8669c91c
Rhys Newell <42852939+rhysnewell@users.noreply.github.com>
2021-01-13 13:27:34 +1000

    Update build.sh

--- recipes/rosella/build.sh
+++ recipes/rosella/build.sh
@@ -11,11 +11,13 @@ LIBCLANG_PATH=$CONDA_PREFIX/lib/libclang.so \
 cargo build --release
 
 # Install the binaries
-C_INCLUDE_PATH=$PREFIX/include \
-LIBRARY_PATH=$PREFIX/lib \
-LD_LIBRARY_PATH=$CONDA_PREFIX/lib \
-LIBCLANG_PATH=$PREFIX/lib/libclang.so \
-cargo install --force --root $PREFIX
+# C_INCLUDE_PATH=$PREFIX/include \
+# LIBRARY_PATH=$PREFIX/lib \
+# LD_LIBRARY_PATH=$CONDA_PREFIX/lib \
+# LIBCLANG_PATH=$PREFIX/lib/libclang.so \
+# cargo install --force --root $PREFIX
+cp target/release/rosella $PREFIX/bin
+cp target/release/remove_minimap2_duplicated_headers $PREFIX/bin
 
 # Install flight
 # cd flight/ && pip install . && cd ../
