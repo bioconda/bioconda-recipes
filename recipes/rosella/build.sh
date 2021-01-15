74923b89e3a1e4d52308b2a04c8366eb49d0e436
Rhys Newell <rhys.newell@hdr.qut.edu.au>
2021-01-15 20:35:43 +1000

    deleting deprecated install section

--- recipes/rosella/build.sh
+++ recipes/rosella/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-# PREFIX=$CONDA_PREFIX
-echo $PREFIX
-echo $CONDA_PREFIX
-
 # Build statically linked binary with Rust
 C_INCLUDE_PATH=$PREFIX/include \
 LIBRARY_PATH=$PREFIX/lib \
@@ -17,9 +13,3 @@ LIBRARY_PATH=$PREFIX/lib \
 LD_LIBRARY_PATH=$PREFIX/lib \
 LIBCLANG_PATH=$PREFIX/lib/libclang.so \
 cargo install --path ./ --force --root $PREFIX
-# cp target/release/rosella $PREFIX/bin
-# cp target/release/remove_minimap2_duplicated_headers $PREFIX/bin
-
-# Install flight
-# cd flight/ && pip install . && cd ../
-#pip install git+https://github.com/rhysnewell/flight
