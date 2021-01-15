d7245f85778f085e94080d931c84b634a90b5f91
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2021-01-15 11:47:26 +0100

    Update build.sh

--- recipes/rnaeditingindexer/build.sh
+++ recipes/rnaeditingindexer/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+
+# run only for the log to see if path is ok
 ./configure.sh -j=${JAVA_HOME}
+#  . ./ syntax does not work. Lets import direclty what is needed
+        export DEV_ROOT=`pwd`
+        export BEDTOOLS_PATH=bedtools
+        export SAMTOOLS_PATH=samtools
+        export RESOURCES_DIR="${DEV_ROOT}/Resources"
+        export JAVA_HOME=${JAVA_HOME}
+        export BAM_UTILS_PATH=bam
+        export PYTHON27_PATH=python
+        export DONT_DOWNLOAD=false
+        export DONT_WRITE=false
+        export IS_UNIX=true
+
 make
