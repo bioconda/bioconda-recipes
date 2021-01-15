764272c998031fdb5c9c34f00659a26ebd0bc2f2
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2021-01-15 10:24:45 +0100

    JAVA is now fixed, path to make also

--- recipes/rnaeditingindexer/build.sh
+++ recipes/rnaeditingindexer/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "JAVA_HOMEllllll = ${JAVA_HOME}"
-sed -i '158 i echo "RET=${RET}"' configure.sh
-sed -i '159 i echo "JAVA_HOME=${JAVA_HOME}"' configure.sh
 
-./configure.sh -j=${JAVA_HOME}
+. ./configure.sh -j=${JAVA_HOME}
 make
