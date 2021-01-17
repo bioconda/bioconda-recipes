02a672494b1c69dfdafde712fedf40ac387f9865
CarlosHorro <carlos.horro@gmail.com>
2021-01-17 13:51:13 +0100

    SG updated to version 4.0.12 temp folders should be be able to be customised for all search engines but Andromeda.

--- recipes/searchgui/searchgui.py
+++ recipes/searchgui/searchgui.py
@@ -14,7 +14,7 @@ from os import access
 from os import getenv
 from os import X_OK
 
-jar_file = 'SearchGUI-4.0.11.jar'
+jar_file = 'SearchGUI-4.0.12.jar'
 
 default_jvm_mem_opts = ['-Xms512m', '-Xmx4g']
 
