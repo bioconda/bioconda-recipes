ba8a4c4bb4a135547f256ca0630fd78fcdbde153
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-01-21 15:25:37 -0800

    Install the NVIDIA HPC SDK as part of the build

--- recipes/unifrac-gpu/build.sh
+++ recipes/unifrac-gpu/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set +ex
+
+# Temporary solution for 0.20.2
+# In the future, the halper script should be in the source tarball
+wget https://raw.githubusercontent.com/sfiligoi/unifrac/v0.20.2-docs/scripts/install_hpc_sdk.sh
+chmod a+x install_hpc_sdk.sh
+./install_hpc_sdk.sh
+source setup_nv_h5.sh
+
 export USE_CYTHON=True
 export PERFORMING_CONDA_BUILD=True
 export LIBRARY_PATH="${CONDA_PREFIX}/lib"
@@ -7,12 +15,7 @@ export LD_LIBRARY_PATH=${LIBRARY_PATH}:${LD_LIBRARY_PATH}
 export CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include"
 
 ls -l
-if [ "$(uname)" == "Darwin" ]; then
-    export MACOSX_DEPLOYMENT_TARGET=10.12
-    SED="sed -i ''"
-else
-    SED='sed -i'
-fi
+SED='sed -i'
 
 $SED 's/^STATIC_AVAILABLE=.*/STATIC_AVAILABLE="no"/' `which h5c++`
 grep STATIC_AVAILABLE `which h5c++`
