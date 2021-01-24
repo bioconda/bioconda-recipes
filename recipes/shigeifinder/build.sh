2a6c098ab7eee0eeee3863287faf6baa3113c240
mjohnpayne <mjohnpayne@gmail.com>
2021-01-25 09:50:25 +1100

    add shigeifinder Shigella and enteroinvasive E. coli serotyping

--- recipes/shigeifinder/build.sh
+++ recipes/shigeifinder/build.sh
@@ -4,7 +4,6 @@ outdir="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 mkdir -p "${outdir}"
 mkdir -p "${PREFIX}/bin"
 
-cp target/abra2-${PKG_VERSION}-jar-with-dependencies.jar "${outdir}/abra2.jar"
 cp "${RECIPE_DIR}/*" "${outdir}/"
 chmod +x "${outdir}/shigeifinder.py"
 ln -s "${outdir}/shigeifinder.py" "${PREFIX}/bin/shigeifinder.py"
