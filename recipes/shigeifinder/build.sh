5f299b555a0132f5ff646b4f0ad51e7433c746ee
mjohnpayne <mjohnpayne@gmail.com>
2021-01-25 10:49:18 +1100

    add shigeifinder Shigella and enteroinvasive E. coli serotyping

--- recipes/shigeifinder/build.sh
+++ recipes/shigeifinder/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-cp -r * $PREFIX
+#mkdir -p $PREFIX/bin
+#cp -r * $PREFIX
 
-#outdir="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
-#mkdir -p "${outdir}"
-#mkdir -p "${PREFIX}/bin"
-#
-#cp * "${outdir}/"
-#chmod +x "${outdir}/shigeifinder.py"
-#ln -s "${outdir}/shigeifinder.py" "${PREFIX}/bin/shigeifinder.py"
+outdir="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
+mkdir -p "${outdir}"
+mkdir -p "${PREFIX}/bin"
+
+cp -r * "${outdir}/"
+chmod +x "${outdir}/shigeifinder.py"
+ln -s "${outdir}/shigeifinder.py" "${PREFIX}/bin/shigeifinder.py"
