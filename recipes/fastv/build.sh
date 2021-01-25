39f59bbe2b106b3af5e2cecd52d54ede2c4cd2f7
Andreas Sjödin <andreas.sjodin@gmail.com>
2021-01-25 11:35:47 +0100

    More zlib

--- recipes/fastv/build.sh
+++ recipes/fastv/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 
 mkdir -p ${PREFIX}/bin
-make CXX="${CXX}"
+make CXX="${CXX}" 
 make install
