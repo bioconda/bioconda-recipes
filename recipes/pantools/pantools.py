578b53d4070e0e147418c4900cb803eb8aec8185
Thom Griffioen <thomgriffioen@gmail.com>
2021-01-26 18:27:54 +0100

    minor recipe changes to Pantools

--- recipes/pantools/pantools.py
+++ recipes/pantools/pantools.py
@@ -1,4 +1,4 @@
-#!/opt/anaconda1anaconda2anaconda3/bin/python
+#!/usr/bin/env python3
 #
 # Wrapper script for Java Conda packages that ensures that the java runtime
 # is invoked with the right options. Adapted from the bash script (http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128).
