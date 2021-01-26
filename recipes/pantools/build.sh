2a485c53cdb7cd7252449176abb3f303f2c867b3
Thom Griffioen <thomgriffioen@gmail.com>
2021-01-26 23:24:02 +0100

    Change pantools metadata

--- recipes/pantools/build.sh
+++ recipes/pantools/build.sh
@@ -2,11 +2,12 @@
 
 set -eu -o pipefail
 
-PACKAGE_HOME="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}_${PKG_BUILDNUM}"
+PACKAGE_HOME="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PACKAGE_HOME}"
 
 cp -R dist/* "${PACKAGE_HOME}/"
 cp "${RECIPE_DIR}/pantools.py" "${PACKAGE_HOME}/pantools.py"
-chmod +x "${PACKAGE_HOME}/pantools.py"
+
 ln -s "${PACKAGE_HOME}/pantools.py" "${PREFIX}/bin/pantools"
+chmod 0755 "${PREFIX}/bin/pantools"
