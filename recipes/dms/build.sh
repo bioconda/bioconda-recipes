88bf1b26a0a189922e107c61224f7826054f5aab
张玉凤 <zhangyufeng@zhangyufengdeMacBook-Pro.local>
2021-01-29 14:59:46 +0800

    update files

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -6,3 +6,5 @@ CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
 CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
 INCLUDE="-I$PREFIX/include"\ 
 CFLAGS="-Wall -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -I$PREFIX/include -L$PREFIX/lib -fopenmp"
+install -d "${PREFIX}/bin"
+install build/execs/xhmm "${PREFIX}/bin/"
