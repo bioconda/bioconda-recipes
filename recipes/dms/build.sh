27463485f04fe76e37ac676cb6b257b7d88ee06b
张玉凤 <zhangyufeng@zhangyufengdeMacBook-Pro.local>
2021-01-29 16:37:58 +0800

    update files

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -2,7 +2,6 @@
 
 mkdir -p ${PREFIV}/bin
 make 
-CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" 
 OMPFLG=-fopenmp
 HASHFLG=-Wno-deprecated
 BUILDFLG=-w -ffunction-sections -fdata-sections -fmodulo-sched -msse
@@ -11,10 +10,10 @@ EXE_T2S=${PREFIV}/bin/MS-table-to-single
 EXE_CMP=${PREFIV}/bin/MS-comp-taxa
 EXE_CPD=${PREFIV}/bin/MS-comp-taxa-dynamic
 EXE_MMR=${PREFIV}/bin/MS-make-ref
-$(CXX) -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
-$(CXX) -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
-$(CXX) -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
-$(CXX) -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
-$(CXX) -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
+${CXX} -o ${EXE_TAL} src/taxa_sel.cpp ${HASHFLG} ${BUILDFLG} ${OMPFLG}
+${CXX} -o ${EXE_T2S} src/table2single.cpp ${HASHFLG} ${BUILDFLG} ${OMPFLG}
+${CXX} -o ${EXE_CMP} src/comp_sam.cpp ${HASHFLG} ${BUILDFLG} ${OMPFLG}
+${CXX} -o ${EXE_CPD} src/comp_sam_dynamic.cpp ${HASHFLG} ${BUILDFLG} ${OMPFLG}
+${CXX} -o ${EXE_MMR} src/make_ref.cpp ${HASHFLG} ${BUILDFLG} ${OMPFLG}
 
 
