7e30d0b967df79331ff57213c081e29f8fd8885d
张玉凤 <zhangyufeng@zhangyufengdeMacBook-Pro.local>
2021-01-29 08:38:46 +0800

    update files

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -6,4 +6,5 @@ CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
 CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
 INCLUDE="-I$PREFIX/include"\ 
 CFLAGS="-Wall -Wno-unused-variable -Wno-unused-function -Wno-misleading-indentation -I$PREFIX/include -L$PREFIX/lib -fopenmp"
-
+mv bin/* $PREFIX/bin
+rm -rf bin
