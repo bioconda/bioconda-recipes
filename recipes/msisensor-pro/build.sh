6ca59a5d876534ff3584957a06fd4d552b85214e
PengJia6 <pengjia1110@gmail.com>
2021-02-01 00:12:16 +0800

    update msisensor-pro

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -1,10 +1,11 @@
 export CPATH=${PREFIX}/include
-export INCLUDES+="-I$PREFIX/include -I$PREFIX/include/ncurses -Ivendor/htslib-1.11"
+export INCLUDES+="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
-$SRC_DIR/vendor/htslib-1.11/configure
-make -C$SRC_DIR/vendor/htslib-1.11
-make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses -I$SRC_DIR/vendor/htslib-1.11" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
+$SRC_DIR/INSTALL
+#$SRC_DIR/vendor/htslib-1.11/configure
+#make -C$SRC_DIR/vendor/htslib-1.11
+#make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses -I$SRC_DIR/vendor/htslib-1.11" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
 #make CC=$CC
 #$SRC_DIR/INSTALL
 cp $SRC_DIR/cpp/msisensor-pro $PREFIX/bin
