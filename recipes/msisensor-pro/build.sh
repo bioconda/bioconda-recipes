a06213c5175d0341a60f538b7883cec44bc1a9a6
PengJia6 <pengjia1110@gmail.com>
2021-01-31 12:33:51 +0800

    update msisensor-pro

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -2,7 +2,8 @@ export CPATH=${PREFIX}/include
 export INCLUDES+="-I$PREFIX/include -I$PREFIX/include/ncurses -Ivendor/htslib-1.11"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
-#make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses -I$SRC_DIR/vendor/samtools-0.1.19" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
+make -C$SRC_DIR/vendor/htslib-1.11
+make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses -I$SRC_DIR/vendor/htslib-1.11" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
 #make CC=$CC
-$SRC_DIR/INSTALL
+#$SRC_DIR/INSTALL
 cp $SRC_DIR/cpp/msisensor-pro $PREFIX/bin
