db809c06d2ee6df5ded3a5e266a0277ed60605f4
Andrea Telatin <andrea@telatin.com>
2021-02-01 17:33:10 +0000

    bamtocov-1

--- recipes/bamtocov/build.sh
+++ recipes/bamtocov/build.sh
@@ -8,16 +8,14 @@ if [[ $OSTYPE == "darwin"* ]]; then
 else
   bash build.sh --os linux --cpu x86_64
 fi
+
 bname=`basename $CC`
 echo "gcc.exe = \"${bname}\"" >> config/nim.cfg
 echo "gcc.linkerexe = \"${bname}\"" >> config/nim.cfg
 echo "clang.exe = \"${bname}\"" >> config/nim.cfg
 echo "clang.linkerexe = \"${bname}\"" >> config/nim.cfg
-bin/nim c  koch
-./koch tools
-popd
 
-export PATH=$SRC_DIR/nim_source/bin:$PATH
+ 
 export LD_LIBRARY_PATH=$PREFIX/lib
 
 nimble install -y --verbose
