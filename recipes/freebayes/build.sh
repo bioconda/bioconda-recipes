1c29c4229ddbab095850d2003c2513dc8bfb87e1
Jon Puritz <jpuritz@gmail.com>
2021-02-01 13:31:18 -0500

    Remove old export statements

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -7,17 +7,18 @@ if [ "$(uname)" == "Darwin" ]; then
     export CXXFLAGS="${CXXFLAGS} CXX=clang++" 
     export CC=clang 
     export CC_LD=lld 
-    meson build --buildtype debug
 fi
 
 
-export C_INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
+#May not need these any more
 
-export LDFLAGS="-L$PREFIX/lib -L\$(LIB_DIR) -lvcflib -lhts -lpthread -lz -lm -llzma -lbz2"
-export INCLUDES="-I . -Ihtslib -I$PREFIX/include -Itabixpp/htslib -I\$(INC_DIR) -L. -Ltabixpp/htslib"
-export LIBPATH="-L. -Lhtslib -L$PREFIX/lib"
-export CXXFLAGS="-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
+#export C_INCLUDE_PATH=$PREFIX/include
+#export CPLUS_INCLUDE_PATH=$PREFIX/include
+
+#export LDFLAGS="-L$PREFIX/lib -L\$(LIB_DIR) -lvcflib -lhts -lpthread -lz -lm -llzma -lbz2"
+#export INCLUDES="-I . -Ihtslib -I$PREFIX/include -Itabixpp/htslib -I\$(INC_DIR) -L. -Ltabixpp/htslib"
+#export LIBPATH="-L. -Lhtslib -L$PREFIX/lib"
+#export CXXFLAGS="-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
 
 #Updating some commands with inspiration from the pbmm2 build
 mkdir build
