2ab6b6fdbb7f77786862069a5da0e63f638ea2d6
PengJia6 <pengjia1110@gmail.com>
2021-02-01 10:10:40 +0800

    update

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -2,7 +2,11 @@ export CPATH=${PREFIX}/include
 export INCLUDES+="-I$PREFIX/include -I$PREFIX/include/ncurses"
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
-$SRC_DIR/INSTALL
+#$SRC_DIR/INSTALL
+cd $SRC_DIR/cpp
+make CC=$CC
+cd -
+cp -rf $SRC_DIR/cpp/msisensor-pro $SRC_DIR/binary/
 #$SRC_DIR/vendor/htslib-1.11/configure
 #make -C$SRC_DIR/vendor/htslib-1.11
 #make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses -I$SRC_DIR/vendor/htslib-1.11" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
