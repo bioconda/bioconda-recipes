1a7dc6b3519b35cc2d6db91885e5b8135c82c03b
PengJia6 <pengjia1110@gmail.com>
2021-02-01 13:04:10 +0800

    update msisensor-pro

--- recipes/msisensor-pro/build.sh
+++ recipes/msisensor-pro/build.sh
@@ -1,12 +1,17 @@
 export CPATH=${PREFIX}/include
-export INCLUDES+="-I$PREFIX/include -I$PREFIX/include/ncurses"
+export INCLUDES+="-I$PREFIX/include -I$PREFIX/include/ncursesi -I$SRC_DIR/vendor/htslib-1.11"
+export LIBCURSES+="-L$PREFIX/lib -lncurses -ltinfo"
+export LIBPATH+="-L$PREFIX/lib" 
 export LDFLAGS="-L$PREFIX/lib -ltinfo"
 export LIBRARY_PATH=${PREFIX}/lib
+export CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
 #$SRC_DIR/INSTALL
 #cd $SRC_DIR/cpp
 #make CC=$CC
-make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses -I$SRC_DIR/vendor/htslib-1.11" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
+echo =============================
 
+make INCLUDES=${INCLUDES} LIBCURSES=${LIBCURSES} LIBPATH=${LIBPATH} CC=$CC CXX=$CXX -C$SRC_DIR/cpp/ 
+echo ==============================
 #make INCLUDES="-I$PREFIX/include -I$PREFIX/include/ncurses  LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib" -C$SRC_DIR/cpp/ 
 #cd -
 cp -rf $SRC_DIR/cpp/msisensor-pro $SRC_DIR/binary/
