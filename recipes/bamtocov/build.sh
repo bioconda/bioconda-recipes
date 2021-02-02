5c70d1103d3294369cc73a611b47ce3ffed0d86f
Andrea Telatin <andrea@telatin.com>
2021-02-02 10:00:08 +0000

    updated-nimble-file

--- recipes/bamtocov/build.sh
+++ recipes/bamtocov/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -euxo pipefail
 
 if [[ $OSTYPE == "darwin"* ]]; then
   export HOME="/Users/distiller"
@@ -10,7 +11,10 @@ export LD_LIBRARY_PATH=$PREFIX/lib
 nimble install -y --verbose
 mkdir -p $PREFIX/bin
 
-for BIN in $(ls src/* | grep -v '\.');
+pwd
+ls -l bin/* 
+
+for BIN in $(ls bin/* | grep -v '\.');
 do
   if [ ! -e "$PREFIX"/bin/${BIN} ]; then
 	chmod a+x ${BIN}
