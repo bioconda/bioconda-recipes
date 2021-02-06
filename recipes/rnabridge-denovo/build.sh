0b69e8a956a77b493860f053e06fae30a4f97c30
Xiang Li <xpl5168@psu.edu>
2021-02-06 22:55:18 +0000

    update recipe

--- recipes/rnabridge-denovo/build.sh
+++ recipes/rnabridge-denovo/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export C_INCLUDE_PATH=$PREFIX/include/
 export CPLUS_INCLUDE_PATH=$PREFIX/include/
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 cd src
-make CC=$CXX
+make CC=$CXX LDFLAGS="$LDFLAGS -L$PREFIX/lib"
