09abf3787fb4a1708e93b6adddccc0e9e0504a1d
bambam <937721540@qq.com>
2021-02-07 15:27:08 +0800

    modify build.sh

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-chmod u+x install.sh
-./install.sh
+mkdir -p ${PREFIX}/bin
+
+OMPFLG=-fopenmp
+HASHFLG=-Wno-deprecated
+BUILDFLG=-w -ffunction-sections -fdata-sections -fmodulo-sched -msse
+EXE_TAL=${PREFIX}/bin/MS-single-to-table
+EXE_T2S=${PREFIX}/bin/MS-table-to-single
+EXE_CMP=${PREFIX}/bin/MS-comp-taxa
+EXE_CPD=${PREFIX}/bin/MS-comp-taxa-dynamic
+EXE_MMR=${PREFIX}/bin/MS-make-ref
+
+$(CXX) -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
+$(CXX) -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
+$(CXX) -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
+$(CXX) -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
+$(CXX) -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) $(OMPFLG)
