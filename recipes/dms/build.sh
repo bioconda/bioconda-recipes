10ebcf7c6096e5d06f8beed1d84b3c8bbef84d20
张玉凤 <zhangyufeng@mbp.lan>
2021-02-09 00:27:57 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -7,7 +7,16 @@ export DynamicMetaStorms=${PREFIX}
 if [ `uname` == Darwin ]; then
     HASHFLG=-Wno-deprecated
     BUILDFLG=-w -ffunction-sections -fdata-sections -fmodulo-sched -msse    
-    $(CXX) -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -openmp=libomp
+    EXE_TAL=bin/MS-single-to-table
+    EXE_T2S=bin/MS-table-to-single
+    EXE_CMP=bin/MS-comp-taxa
+    EXE_CPD=bin/MS-comp-taxa-dynamic
+    EXE_MMR=bin/MS-make-ref
+    $(CXX) -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    $(CXX) -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    $(CXX) -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    $(CXX) -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    $(CXX) -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
 else
     make
 fi
