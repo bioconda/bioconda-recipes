d9e4b04d29d73a2479f527b39cd53b64425a3927
bambam <937721540@qq.com>
2021-02-08 18:08:43 +0800

    modify build.sh export part

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -3,8 +3,8 @@ mkdir -p ${PREFIX}/bin
 chmod u+x install.sh
 ./install.sh
 
-export DynamicMetaStorms=${PREFIX}/
-export PATH=\$PATH:\$DynamicMetaStorms/bin
+export DynamicMetaStorms=${PREFIX}
+export PATH=$PATH:$DynamicMetaStorms/bin
 
 cp bin/* ${PREFIX}/bin
 cp -r databases ${PREFIX}
