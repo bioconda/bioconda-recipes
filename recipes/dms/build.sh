575e301a58aaced8836497dcbdf80faa6836c840
张玉凤 <zhangyufeng@mbp.lan>
2021-02-09 00:56:16 +0800

    update files

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -2,9 +2,8 @@
 
 mkdir -p ${PREFIX}/bin
 
-export DynamicMetaStorms=${PREFIX}
-
 if [ `uname` == Darwin ]; then
+    echo "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm111111111111111111111111"
     HASHFLG=-Wno-deprecated
     BUILDFLG=-w -ffunction-sections -fdata-sections -fmodulo-sched -msse    
     EXE_TAL=bin/MS-single-to-table
@@ -12,12 +11,13 @@ if [ `uname` == Darwin ]; then
     EXE_CMP=bin/MS-comp-taxa
     EXE_CPD=bin/MS-comp-taxa-dynamic
     EXE_MMR=bin/MS-make-ref
-    $(CXX) -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    $(CXX) -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    $(CXX) -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    $(CXX) -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    $(CXX) -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    libllvm11 -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    libllvm11 -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    libllvm11 -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    libllvm11 -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    libllvm11 -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
 else
+    echo "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn2222222222222222222222222"
     make
 fi
 
