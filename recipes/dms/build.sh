64628eb01ce964ff7ffdc87a8dfab3b96bd83986
bambam <937721540@qq.com>
2021-02-08 17:57:37 +0800

    export environment variable

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -2,6 +2,9 @@
 mkdir -p ${PREFIX}/bin
 chmod u+x install.sh
 ./install.sh
+
 export DynamicMetaStorms=${PREFIX}/
+export PATH=\$PATH:\$DynamicMetaStorms/bin
+
 cp bin/* ${PREFIX}/bin
 cp -r databases ${PREFIX}
