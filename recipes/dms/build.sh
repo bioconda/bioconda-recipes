62548dc1b7ed754b258793c94347dfd5e929ea0b
张玉凤 <zhangyufeng@mbp.lan>
2021-02-09 00:23:01 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -4,7 +4,13 @@ mkdir -p ${PREFIX}/bin
 
 export DynamicMetaStorms=${PREFIX}
 
-make
+if [ `uname` == Darwin ]; then
+    HASHFLG=-Wno-deprecated
+    BUILDFLG=-w -ffunction-sections -fdata-sections -fmodulo-sched -msse    
+    $(CXX) -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -openmp=libomp
+else
+    make
+fi
 
 cp bin/* ${PREFIX}/bin
 cp -r databases ${PREFIX}
