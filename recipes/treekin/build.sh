afb67aa715398f4b6c0f8ee615a7d3d59a3b71c9
Sebastian Will <swill@csail.mit.edu>
2021-02-09 22:26:52 +0100

    Add include path

--- recipes/treekin/build.sh
+++ recipes/treekin/build.sh
@@ -4,7 +4,7 @@ export CXXFLAGS="${CXXFLAGS/-std=c++17/-std=gnu++17} -Wno-unused-result"
 
 autoreconf -i \
 && \
-PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH \
-  ./configure --prefix=$PREFIX && \
+PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH" \
+  ./configure --prefix="$PREFIX" CPPFLAGS="-I $PREFIX/include" && \
 make ${CPUS_COUNT} && \
 make install
