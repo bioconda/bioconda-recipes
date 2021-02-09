9040fe6f46d461e484f9edd6b1288440566fc2e6
Andrea Telatin <andrea@telatin.com>
2021-02-09 14:24:43 +0000

    seqfu-dep

--- recipes/seqfu/build.sh
+++ recipes/seqfu/build.sh
@@ -8,13 +8,26 @@ fi
 
 mkdir -p $PREFIX/bin
 
-nimble install -y --verbose argparse docopt terminaltables
+echo " * Attempt automatic build"
+nimble build --verbose || true
+
+ls -ltr *.nimble bin/*.nim
+
+echo " * Legacy procedure"
+nimble install -y --verbose argparse docopt terminaltables readfq iterutils
+
+echo " * Build SeqFu"
 nim c --threads:on -p:lib --opt:speed -d:release -o:$PREFIX/bin/seqfu src/sfu.nim
 
 
 for SOURCE in src/fu_*.nim;
 do
-	NAME=$(basename $SOURCE | sed 's/_/-/'  |cut -f1 -d.)
-	echo "$SOURCE -> $NAME"
+	NAME=$(basename ${SOURCE%.nim} | sed 's/_/-/' )
+    THREADS=""
+    echo " * Build $NAME"
+    CHECK_THREADS=$(grep thread build.sh | wc -l || true )
+	if [[ "$OK" -ne 0 ]]; then
+	    THREADS=" --threads:on "
+	fi
 	nim c -p:lib --opt:speed -d:release -o:"$PREFIX/bin/${NAME}" "$SOURCE" || true
 done
