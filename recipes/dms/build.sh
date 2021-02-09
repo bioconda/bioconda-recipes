cc49a1976a4fdb20a7afabad1ccacbc9eeb5fed3
张玉凤 <zhangyufeng@mbp.lan>
2021-02-09 22:24:52 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -2,16 +2,7 @@
 
 mkdir -p ${PREFIX}/bin
 
-if [ `uname` == Darwin ]; then
-    echo "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm111111111111111111111111"
-    brew install gcc
-    make
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-else
-    echo "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn2222222222222222222222222"
-    make
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-fi
+make
 
 cp bin/* ${PREFIX}/bin
 cp -r databases ${PREFIX}
