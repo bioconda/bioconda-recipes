10b7f670d15cdc7bd78e2b44b467e60f5b90ab57
张玉凤 <zhangyufeng@mbp.lan>
2021-02-09 10:12:48 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -4,6 +4,7 @@ mkdir -p ${PREFIX}/bin
 
 if [ `uname` == Darwin ]; then
     echo "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm111111111111111111111111"
+    brew install gcc
     HASHFLG=-Wno-deprecated
     BUILDFLG=-w -ffunction-sections -fdata-sections -fmodulo-sched -msse    
     EXE_TAL=bin/MS-single-to-table
@@ -11,11 +12,11 @@ if [ `uname` == Darwin ]; then
     EXE_CMP=bin/MS-comp-taxa
     EXE_CPD=bin/MS-comp-taxa-dynamic
     EXE_MMR=bin/MS-make-ref
-    libllvm11 -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    libllvm11 -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    libllvm11 -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    libllvm11 -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
-    libllvm11 -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) -fopenmp=libomp
+    g++-10 -o $(EXE_TAL) src/taxa_sel.cpp $(HASHFLG) $(BUILDFLG) -fopenmp
+    g++-10 -o $(EXE_T2S) src/table2single.cpp $(HASHFLG) $(BUILDFLG) -fopenmp
+    g++-10 -o $(EXE_CMP) src/comp_sam.cpp $(HASHFLG) $(BUILDFLG) -fopenmp
+    g++-10 -o $(EXE_CPD) src/comp_sam_dynamic.cpp $(HASHFLG) $(BUILDFLG) -fopenmp
+    g++-10 -o $(EXE_MMR) src/make_ref.cpp $(HASHFLG) $(BUILDFLG) -fopenmp
 else
     echo "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn2222222222222222222222222"
     make
