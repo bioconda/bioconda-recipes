7bd2521667a81cad380a56232be881997d89c384
Ryan Zheng <ryan0225@foxmail.com>
2021-02-10 17:41:06 -0800

    fix bug

--- recipes/chips/build.sh
+++ recipes/chips/build.sh
@@ -5,10 +5,10 @@ export CPP_INCLUDE_PATH=$CPP_INCLUDE_PATH:${PREFIX}/include
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
 export CXX_INCLUDE_PATH=$CXX_INCLUDE_PATH:${PREFIX}/include
 
-export LIBRARY_PATH="$PREFIX/lib"
+export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export CPATH=$CPATH:${PREFIX}/include
 
 mkdir build
 cd build
