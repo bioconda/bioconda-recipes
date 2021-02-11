ee63d0fb05bb7ba151048041ab0be7b6efe8cf06
Ryan Zheng <ryan0225@foxmail.com>
2021-02-10 22:41:28 -0800

    fix bug

--- recipes/chips/build.sh
+++ recipes/chips/build.sh
@@ -7,12 +7,12 @@
 
 # export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include -std=c++11"
+export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 # export CPATH=$CPATH:${PREFIX}/include
 
 mkdir build
 cd build
 cmake ..
-make CC=$CC CXX=$CXX CFLAGS="$CFLAGS -DVERSION=1.2.3" LDFLAGS+="-lz"
+make CC=$CC CXX=$CXX CFLAGS="$CFLAGS -DVERSION=1.2.3" CXXFLAGS="$CXXFLAGS -std=c++11" LDFLAGS+="-lz"
 #make CC=$CC CXX=$CXX CFLAGS="$CFLAGS -DVERSION=1.2.3"
