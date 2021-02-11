c9efa9b5abfec9219989ebd4b0efc7dcd71b339a
Ryan Zheng <ryan0225@foxmail.com>
2021-02-11 09:41:37 -0800

    fix bug

--- recipes/chips/build.sh
+++ recipes/chips/build.sh
@@ -6,12 +6,17 @@
 # export CXX_INCLUDE_PATH=$CXX_INCLUDE_PATH:${PREFIX}/include
 
 # export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include -std=c++11 -stdlib=libc++"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CFLAGS="$CFLAGS -I$PREFIX/include -DVERSION=1.2.3"
+export CXXFLAGS="$CXXFLAGS -I$PREFIX/include -std=c++11"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib -lz"
+export CPPFLAGS="$CPPFLAGS -std=c++11"
 # export CPATH=$CPATH:${PREFIX}/include
 
 mkdir build
 cd build
 cmake ..
-make CC=$CC CXX=$CXX CFLAGS="$CFLAGS -DVERSION=1.2.3" CXXFLAGS="$CXXFLAGS -std=c++11" LDFLAGS+="-lz"
+make VERBOSE=1 \
+    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+    CC="${CC} ${CFLAGS} ${LDFLAGS}"
+
+# CC=$CC CXX=$CXX CFLAGS="$CFLAGS -DVERSION=1.2.3" CXXFLAGS="$CXXFLAGS -std=c++11" LDFLAGS+="-lz"
