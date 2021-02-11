de3ef48cb397ed06031cb0cc92ea0bccf2a1d462
Ryan Zheng <ryan0225@foxmail.com>
2021-02-10 18:24:25 -0800

    fix bug

--- recipes/chips/build.sh
+++ recipes/chips/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
-export CPP_INCLUDE_PATH=$CPP_INCLUDE_PATH:${PREFIX}/include
-export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
-export CXX_INCLUDE_PATH=$CXX_INCLUDE_PATH:${PREFIX}/include
+# export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+# export CPP_INCLUDE_PATH=$CPP_INCLUDE_PATH:${PREFIX}/include
+# export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
+# export CXX_INCLUDE_PATH=$CXX_INCLUDE_PATH:${PREFIX}/include
 
-export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=$CPATH:${PREFIX}/include
+# export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+# export CFLAGS="$CFLAGS -I$PREFIX/include"
+# export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+# export CPATH=$CPATH:${PREFIX}/include
 
 mkdir build
 cd build
-cmake LIBS="-L$PREFIX/lib -lhts -lz -lm" LDFLAGS="$LDFLAGS" ..
-make CC=$CC CXX=$CXX
+cmake ..
+make
