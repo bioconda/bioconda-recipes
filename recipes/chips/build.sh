4af16219b33127a4de29741573b75ddf54a3ec49
Ryan Zheng <ryan0225@foxmail.com>
2021-02-10 16:41:50 -0800

    fix bug

--- recipes/chips/build.sh
+++ recipes/chips/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH="$PREFIX/include"
-export LIBRARY_PATH="$PREFIX/lib"
-export LDFLAGS="-L$PREFIX/lib"
+# export C_INCLUDE_PATH="$PREFIX/include"
+# export LIBRARY_PATH="$PREFIX/lib"
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 mkdir build
 cd build
