ef7920bea3ce3c8f733b6679fef4d05cffdc9ea0
Ryan Zheng <ryan0225@foxmail.com>
2021-02-11 17:02:03 -0800

    v2.3

--- recipes/chips/build.sh
+++ recipes/chips/build.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
-# export CPP_INCLUDE_PATH=$CPP_INCLUDE_PATH:${PREFIX}/include
-# export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
-# export CXX_INCLUDE_PATH=$CXX_INCLUDE_PATH:${PREFIX}/include
-
-# export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 export CFLAGS="$CFLAGS -I$PREFIX/include -DVERSION=1.2.3"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include -std=c++11"
+export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -lz"
-export CPPFLAGS="$CPPFLAGS"
-# export CPATH=$CPATH:${PREFIX}/include
 
 mkdir build
 cd build
 cmake ..
-make VERBOSE=1 \
-    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
-    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+make \
+    CXX="${CXX} ${CXXFLAGS} ${LDFLAGS}" \
+    CC="${CC} ${CFLAGS} ${LDFLAGS}"
 
 cp -v chips ${PREFIX}/bin/chips
