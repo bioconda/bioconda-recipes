88677f62e5d675349fb24d60900f90b99af573c9
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2021-02-12 18:52:21 +0000

    Add workarounds for sdm 1.73

--- recipes/sdm/build.sh
+++ recipes/sdm/build.sh
@@ -4,6 +4,9 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
+# Workaround for sdm 1.73, fixed in https://github.com/hildebra/sdm/pull/3
+rm -f *.o
+
 sed -i -e 's/-static //' Makefile
 make
 mkdir -p "${PREFIX}/bin"
