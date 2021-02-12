9e5023a296bad7b86dabdad215b7a433b81f06aa
Vineet Bansal <vineetbansal@protonmail.com>
2021-02-12 16:20:23 -0500

    HATCHet recipe

---
+++ recipes/hatchet/build.sh
@@ -0,0 +1,13 @@
+# Download and compile Gurobi 9.0.2
+wget https://packages.gurobi.com/9.0/gurobi9.0.2_linux64.tar.gz -O gurobi9.0.2_linux64.tar.gz
+tar xvzf gurobi9.0.2_linux64.tar.gz
+(cd gurobi902/linux64/src/build && make)
+(cd gurobi902/linux64/lib && ln -f -s ../src/build/libgurobi_c++.a libgurobi_c++.a)
+
+# Set envvar so our cmake script can find it
+export GUROBI_HOME=$(realpath gurobi902)
+
+export CXXFLAGS=-pthread
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+#$PYTHON setup.py install
+
