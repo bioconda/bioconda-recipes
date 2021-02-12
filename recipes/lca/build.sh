0c745945d2721b6b198871a71875546701c7f659
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2021-02-12 15:13:57 +0000

    Update LCA to 0.21

--- recipes/lca/build.sh
+++ recipes/lca/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# https://bioconda.github.io/troubleshooting.html#zlib-errors
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+# https://bioconda.github.io/contributor/troubleshooting.html#zlib-errors
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 make
-mkdir -p ${PREFIX}/bin
-mv LCA ${PREFIX}/bin/
+mkdir -p "${PREFIX}/bin"
+mv LCA "${PREFIX}/bin/"
