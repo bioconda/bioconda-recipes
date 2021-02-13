4984df36d68456e9e5cb96352fce701bb673ed07
Adam Hospital <adam.hospital@irbbarcelona.org>
2021-02-13 10:52:35 +0100

    GOdMD BioConda 3

--- recipes/godmd/build.sh
+++ recipes/godmd/build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Change folder
+cd lib
+
+# Compile using make
+make \
+    F77="${FC}" \
+    CPP="${CXX}"
+
+# Change folder
+cd ../src
+
 # Compile using make
 make \
     F77="${FC}" \
@@ -8,5 +19,5 @@ make \
 # Copy executable to environment bin dir included in the path
 mkdir -p $PREFIX/bin
 
-chmod u+x discrete/discrete
-cp discrete/discrete $PREFIX/bin/
+chmod u+x src/discrete
+cp src/discrete $PREFIX/bin/
