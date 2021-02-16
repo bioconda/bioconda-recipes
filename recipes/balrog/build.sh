00daeecdbdea516a2f0b6b643498f154bdc9d210
Markus Sommer <markusjsommer@gmail.com>
2021-02-16 14:58:43 -0500

    add dl

--- recipes/balrog/build.sh
+++ recipes/balrog/build.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_STANDARD_LIBRARIES="-ldl" ..
 make -j "${CPU_COUNT}"
 make install
