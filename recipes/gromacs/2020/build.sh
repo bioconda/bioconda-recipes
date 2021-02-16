a11bd07ad6e10689a675355542669fe30dc6597b
Robin Long <robin.long1@hotmail.co.uk>
2021-02-16 08:03:00 +0000

    Reverting to old setup of files.

--- recipes/gromacs/2020/build.sh
+++ recipes/gromacs/2020/build.sh
@@ -3,28 +3,26 @@ mkdir build
 cd build
 
 ## See INSTALL of gromacs distro
-for ARCH in SSE2 AVX_256 AVX2_256 AVX_512; do
-  cmake_args=(
-    -DSHARED_LIBS_DEFAULT=ON
-    -DBUILD_SHARED_LIBS=ON
-    -DGMX_PREFER_STATIC_LIBS=NO
-    -DGMX_BUILD_OWN_FFTW=OFF
-    -DGMX_DEFAULT_SUFFIX=ON
-    -DREGRESSIONTEST_DOWNLOAD=ON
-    -DGMX_GPU=ON
-    -DGMX_USE_OPENCL=ON
-    -DCMAKE_PREFIX_PATH="${PREFIX}"
-    -DGMX_INSTALL_PREFIX="${PREFIX}"
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-    -DGMX_SIMD="${ARCH}"
-    -DCMAKE_INSTALL_BINDIR="bin.${ARCH}"
-    -DCMAKE_INSTALL_LIBDIR="lib.${ARCH}"
-    -DGMX_MPI=OFF
-  )
-  cmake .. "${cmake_args[@]}"
-  make -j "${CPU_COUNT}"
+for ARCH in SSE2 AVX_256 AVX2_256 AVX_512; do \
+  cmake .. \
+  -DSHARED_LIBS_DEFAULT=OFF \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DGMX_PREFER_STATIC_LIBS=YES \
+  -DGMX_BUILD_OWN_FFTW=OFF \
+  -DGMX_DEFAULT_SUFFIX=ON \
+  -DREGRESSIONTEST_DOWNLOAD=ON \
+  -DGMX_MPI=OFF \
+  -DGMX_GPU=ON \
+  -DGMX_USE_OPENCL=ON \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DGMX_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DGMX_SIMD=${ARCH} \
+  -DCMAKE_INSTALL_BINDIR=bin.${ARCH} \
+  -DCMAKE_INSTALL_LIBDIR=lib.${ARCH}
+  make -j${CPU_COUNT}
   make install
-done
+done;
 
 
 #
@@ -41,46 +39,12 @@ ${CXX} -O3 -mavx512f -std=c++11 \
 -o ${PREFIX}/bin.AVX_512/identifyavx512fmaunits \
 ${SRC_DIR}/src/gromacs/hardware/identifyavx512fmaunits.cpp
 
+cp -a ${RECIPE_DIR}/gmx-chooser.bash ${PREFIX}/bin/gmx
+chmod a+x ${PREFIX}/bin/gmx
 
-# Create wrapper and activation scripts
-# Variable declaration from MPI script fewer changes if left in.
 
-gmx='gmx'
+# Copy the activate scripts to $PREFIX/etc/conda/activate.d
 
 mkdir -p "${PREFIX}/etc/conda/activate.d"
-touch "${PREFIX}/bin/${gmx}"
-chmod +x "${PREFIX}/bin/${gmx}"
-
-{ cat <<EOF
-#! /bin/sh
-
-function _gromacs_bin_dir() {
-  local arch
-  arch='SSE2'
-  case \$( grep -m1 '^flags' ) in
-    *\ avx512f\ *)
-      test -d "${PREFIX}/bin.AVX_512" && \
-        "${PREFIX}/bin.AVX_512/identifyavx512fmaunits" | grep -q '2' && \
-        arch='AVX_512'
-    ;;
-    *\ avx2\ *)
-      test -d "${PREFIX}/bin.AVX2_256" && \
-        arch='AVX2_256'
-    ;;
-    *\ avx\ *)
-      test -d "${PREFIX}/bin.AVX_256" && \
-        arch='AVX_256'
-  esac
-  printf '%s' "${PREFIX}/bin.\${arch}"
-}
-
-EOF
-} | tee "${PREFIX}/bin/${gmx}" > "${PREFIX}/etc/conda/activate.d/gromacs_activate.sh"
-
-cat >> "${PREFIX}/etc/conda/activate.d/gromacs_activate.sh" <<EOF
-. "\$( _gromacs_bin_dir )/GMXRC" "\${@}"
-EOF
-
-cat >> "${PREFIX}/bin/${gmx}" <<EOF
-exec "\$( _gromacs_bin_dir )/${gmx}" "\${@}"
-EOF
+cp ${RECIPE_DIR}/activate.sh "${PREFIX}/bin/gromacs_activate.sh" 
+ln -s  ../../../bin/gromacs_activate.sh "${PREFIX}/etc/conda/activate.d/gromacs_activate.sh"
