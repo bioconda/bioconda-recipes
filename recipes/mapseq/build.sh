7d5fe40244c07f4d6ec3e9224cd3611fc4932c5b
Joao F. Matias Rodrigues <joao.rodrigues@imls.uzh.ch>
2021-02-18 14:42:43 +0100

    Included wget in requirements. And added wget of ribosomal RNA reference data at build time.

--- recipes/mapseq/build.sh
+++ recipes/mapseq/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+wget http://www.microbeatlas.org/mapref/mapref-2.2b.tar.gz
+tar -Cdata -xvzf mapref-2.2b.tar.gz && mv data/mapref-2.2b/* data/ && rmdir data/mapref-2.2b && touch data/mapref-2.2b.fna
+
 ./bootstrap
 ./configure --prefix=$PREFIX CXXFLAGS="-O2 -I$CONDA_PREFIX/include"
 make
