ad8dc6e3832bd37b024a6b18847c34373c681ead
Vineet Bansal <vineetbansal@protonmail.com>
2021-02-18 18:05:30 -0500

    HATCHet Recipe

--- recipes/hatchet/build.sh
+++ recipes/hatchet/build.sh
@@ -7,7 +7,8 @@ tar xvzf gurobi9.0.2_linux64.tar.gz
 # Set envvar so our cmake script can find it
 export GUROBI_HOME=$(realpath gurobi902)
 
+mkdir -p $PREFIX/lib
+cp gurobi902/linux64/lib/libgurobi90.so $PREFIX/lib
+
 export CXXFLAGS=-pthread
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-#$PYTHON setup.py install
-
