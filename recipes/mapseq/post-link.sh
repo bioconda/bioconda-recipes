c73ff8101341ca6340afd18b659b2ca46170050c
Joao F. Matias Rodrigues <joao.rodrigues@imls.uzh.ch>
2021-02-19 17:15:31 +0100

    Added test in post-link to check if mapseq finds the downloaded dataset correctly

--- recipes/mapseq/post-link.sh
+++ recipes/mapseq/post-link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# downloading large (4GB) required dataset
 
 FN="mapref-2.2b.tar.gz"
 URLS=(
@@ -15,8 +15,8 @@ mkdir -p $STAGING
 TARBALL=$STAGING/$FN
 
 # debugging info
-ls -lahrt $STAGING
-ldd $PREFIX/bin/mapseq
+#ls -lahrt $STAGING
+#ldd $PREFIX/bin/mapseq
 
 SUCCESS=0
 for URL in ${URLS[@]}; do
@@ -49,7 +49,10 @@ mv $STAGING/mapref-2.2b/* $STAGING/
 rmdir $STAGING/mapref-2.2b
 rm $TARBALL
 
-ls -lahrt $STAGING
+# testing to ensure dataset is correctly installed and found by mapseq
+head -n 2 $STAGING/mapref-2.2b.fna | mapseq - > $STAGING/test.fna.mseq 2>> $PREFIX/.messages.txt || ( echo "ERROR running mapseq"; exit -1 )
+cat $STAGING/test.fna.mseq >> $PREFIX/.messages.txt
+rm $STAGING/test.fna.mseq
 
-#rmdir $STAGING
+#ls -lahrt $STAGING
 
