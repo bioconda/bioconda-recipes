9159ea2fbb598603702ce4eeadfd93e1987b46c4
Lucia Fabio <lfabio@mc-hgomez.mmb.pcb.ub.es>
2021-02-19 14:05:30 +0100

    added curves recipe

---
+++ recipes/curves/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Compile using make
+make 
+
+# Copy executable to environment bin directory. This folder is included in the path when the env is activated
+mkdir -p $PREFIX/bin
+
+chmod u+x Cur+
+cp Cur+ $PREFIX/bin/
