b27c4bdb3ca00fef72fd6dc9d4ca489e0f7e383a
Vineet Bansal <vineetbansal@protonmail.com>
2021-02-22 19:37:05 -0500

    using correct cxx compiler in cmake

--- recipes/machina/build.sh
+++ recipes/machina/build.sh
@@ -8,6 +8,7 @@ GUROBI_HOME=$(realpath gurobi902)
 
 mkdir -p build && cd build
 cmake .. \
+  -DCMAKE_CXX_COMPILER=${CXX} \
   -DGUROBI_HOME=$GUROBI_HOME \
   -DLIBLEMON_ROOT=$CONDA_PREFIX/lib
 make
