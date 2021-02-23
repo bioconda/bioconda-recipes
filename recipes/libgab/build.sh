d4316dad8e83dce1080e90f1948cc00547d9b615
grenaud <gabriel.reno@gmail.com>
2021-02-23 21:39:41 +0100

    added gzstream headers and libs

--- recipes/libgab/build.sh
+++ recipes/libgab/build.sh
@@ -5,9 +5,13 @@ export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 mkdir -p ${PREFIX}/include/libgab/
 mkdir -p ${PREFIX}/lib/libgab/
+mkdir -p ${PREFIX}/include/libgab/gzstream/
+mkdir -p ${PREFIX}/lib/libgab/gzstream/
 
 make  CXX="${CXX}"   all
 cp libgab*a ${PREFIX}/lib/libgab/
 cp     *\.h ${PREFIX}/include/libgab/
+cp     gzstream/*\.h          ${PREFIX}/include/libgab/gzstream/
+cp     gzstream/libgzstream.a ${PREFIX}/include/libgab/gzstream/
 cd ..
 
