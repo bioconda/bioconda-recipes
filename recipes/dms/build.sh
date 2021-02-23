702c658600810a5eb7f972dc8a61e45aab5481b1
qibebt-bioinfo <qibebt_bioinfo@163.com>
2021-02-23 23:53:01 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -2,6 +2,12 @@
 
 mkdir -p ${PREFIX}/bin
 
+echo "~~~~~~~~~~~~~~~~~~"
+echo ${CONDA_PREFIX}
+echo "~~~~~~~~~~~~~~~~~~"
+echo `ls ${CONDA_PREFIX}/lib`
+echo "~~~~~~~~~~~~~~~~~~"
+
 make
 
 cp bin/* ${PREFIX}/bin
