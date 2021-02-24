80d2fe9507638dca025cb97f3c0a6753c209d1ad
grenaud <gabriel.reno@gmail.com>
2021-02-24 22:44:01 +0100

    copy library in lib folder

--- recipes/libgab/build.sh
+++ recipes/libgab/build.sh
@@ -12,6 +12,6 @@ make  CXX="${CXX}"   all
 cp libgab*a ${PREFIX}/lib/libgab/
 cp     *\.h ${PREFIX}/include/libgab/
 cp     gzstream/*\.h          ${PREFIX}/include/libgab/gzstream/
-cp     gzstream/libgzstream.a ${PREFIX}/include/libgab/gzstream/
+cp     gzstream/libgzstream.a ${PREFIX}/lib/libgab/gzstream/
 cd ..
 
