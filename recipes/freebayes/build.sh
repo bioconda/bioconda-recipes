63db437ffb270bd399e886ce7e87aea3ee3cb926
Jon Puritz <jpuritz@gmail.com>
2021-02-25 10:58:33 -0500

    Update build.sh

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -8,10 +8,7 @@ cd build
 ninja -v
 ninja -v install
 
-echo "Ninja finished"
-
 cp -n ../scripts/*.py $PREFIX/bin
-echo "first script copy finished"
 cp -n ../scripts/*.sh $PREFIX/bin
 #cp -n ../scripts/*.pl $PREFIX/bin 
 #cp -n ../scripts/*.R $PREFIX/bin
