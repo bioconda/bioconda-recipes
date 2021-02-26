3f09a3268b3f0217c15d06eaf89fad85dd4bfb69
grenaud <gabriel.reno@gmail.com>
2021-02-26 13:36:06 +0100

    not working yet

--- recipes/leehom/build.sh
+++ recipes/leehom/build.sh
@@ -4,7 +4,7 @@ export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 
-make  CXX="${CXX}" LIBGABINC=${PREFIX}/include/libgab/ LIBGABLIB=${PREFIX}/lib/libgab/ BAMTOOLSINC=${PREFIX}/include/bamtools/ BAMTOOLSLIB=${PREFIX}/lib/bamtools/ all
+make  CXX="${CXX}" LIBGABINC=${PREFIX}/include/libgab/ LIBGABLIB=${PREFIX}/lib/libgab/ BAMTOOLSINC=${PREFIX}/include/bamtools/ BAMTOOLSLIB=${PREFIX}/lib/ all
 cp src/leeHom ${PREFIX}/bin/
 cd ..
 
