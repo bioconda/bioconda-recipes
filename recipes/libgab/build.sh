d2579db05108c08d6ac83d33a2bda888cb935999
grenaud <gabriel.reno@gmail.com>
2021-02-26 13:41:31 +0100

    added object files

--- recipes/libgab/build.sh
+++ recipes/libgab/build.sh
@@ -9,8 +9,11 @@ mkdir -p ${PREFIX}/include/libgab/gzstream/
 mkdir -p ${PREFIX}/lib/libgab/gzstream/
 
 make  CXX="${CXX}"   all
-cp libgab*a ${PREFIX}/lib/libgab/
-cp     *\.h ${PREFIX}/include/libgab/
+cp     libgab*a               ${PREFIX}/lib/libgab/
+cp     PutProgramInHeader.o   ${PREFIX}/lib/libgab/ 2>/dev/null || :
+cp     BamFunctions.o         ${PREFIX}/lib/libgab/ 2>/dev/null || :
+cp     ReconsReferenceBAM.o   ${PREFIX}/lib/libgab/ 2>/dev/null || :
+cp     *\.h                   ${PREFIX}/include/libgab/
 cp     gzstream/*\.h          ${PREFIX}/include/libgab/gzstream/
 cp     gzstream/libgzstream.a ${PREFIX}/lib/libgab/gzstream/
 cd ..
