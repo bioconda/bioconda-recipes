f106d671e9e1bb88f0afed1b08d12fa7242f217e
grenaud <gabriel.reno@gmail.com>
2021-02-27 13:56:35 +0100

    added bamtools variables

--- recipes/libgab/build.sh
+++ recipes/libgab/build.sh
@@ -9,11 +9,8 @@ mkdir -p ${PREFIX}/include/libgab/gzstream/
 mkdir -p ${PREFIX}/lib/libgab/gzstream/
 
 
-make  CXX="${CXX}"   all
+make  CXX="${CXX}" BAMTOOLSINC=${PREFIX}/include/bamtools/ BAMTOOLSLIB=${PREFIX}/lib/   all
 cp     libgab*a               ${PREFIX}/lib/libgab/
-cp     PutProgramInHeader.o   ${PREFIX}/lib/libgab/ 2>/dev/null || :
-cp     BamFunctions.o         ${PREFIX}/lib/libgab/ 2>/dev/null || :
-cp     ReconsReferenceBAM.o   ${PREFIX}/lib/libgab/ 2>/dev/null || :
 cp     *\.h                   ${PREFIX}/include/libgab/
 cp     gzstream/*\.h          ${PREFIX}/include/libgab/gzstream/
 cp     gzstream/libgzstream.a ${PREFIX}/lib/libgab/gzstream/
