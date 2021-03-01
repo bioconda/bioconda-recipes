1bfc5b3ccfa716f88a69923970868f913a95291d
grenaud <gabriel.reno@gmail.com>
2021-03-01 13:30:42 +0100

    zlib3

--- recipes/leehom/build.sh
+++ recipes/leehom/build.sh
@@ -3,7 +3,6 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-
 make  CXX="${CXX}" LIBGABINC=${PREFIX}/include/libgab/ LIBGABLIB=${PREFIX}/lib/libgab/ BAMTOOLSINC=${PREFIX}/include/bamtools/ BAMTOOLSLIB=${PREFIX}/lib/ all
 cp src/leeHom ${PREFIX}/bin/
 cd ..
