7df61f624e75ccb9541dd97039a9c00876b9b1d9
qibebt-bioinfo <qibebt_bioinfo@163.com>
2021-03-01 13:37:36 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -6,14 +6,7 @@
 
 mkdir -p ${PREFIX}/bin
 
-echo "~~~~~~~~~"
-echo `ls ${PREFIX}`
-echo "~~~~~~~~~"
-echo `ls ${PREFIX}/*`
-echo "~~~~~~~~~"
-
 make
 
-cp -r bin/* ${PREFIX}/bin
 cp -r databases ${PREFIX}
 cp -r example ${PREFIX}
