c421c28b65e64d158cdc4e20268b286b59486217
qibebt-bioinfo <qibebt_bioinfo@163.com>
2021-03-02 10:38:38 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#if [ x"$(uname)" == x"Darwin" ]; then
-#        chmod 777 $PREFIX/include/c++
-#fi
-
 mkdir -p ${PREFIX}/bin
 
 make CXX="${CXX} ${CPPFLAGS} ${CXXFLAGS} -fopenmp -DOMP ${LDFLAGS}"
--- recipes/dms/post-link.sh
+++
@@ -1,4 +0,0 @@
-echo """    conda config --add channels defaults
-    conda config --add channels biconda
-    conda config --add channels conda-forge """ >> "${PREFIX}/.messages.txt" 2>&1
-
