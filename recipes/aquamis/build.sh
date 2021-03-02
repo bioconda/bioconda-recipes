da6c5705ee2217af45f23c78bc570deaa73b9fbb
SH Tausch <misterioes16@gmail.com>
2021-03-02 10:00:53 +0100

    select new tag 1.3.0

--- recipes/aquamis/build.sh
+++ recipes/aquamis/build.sh
@@ -6,7 +6,7 @@ mkdir -p $PREFIX/opt/aquamis/
 
 cp -r * $PREFIX/opt/aquamis/
 
-ln -s $PREFIX/opt/aquamis/scripts/aquamis_setup.sh $PREFIX/opt/aquamis/scripts/create_sampleSheet.sh $PREFIX/opt/aquamis/scripts/filter_json.py $PREFIX/opt/aquamis/scripts/parse_json.py $PREFIX/opt/aquamis/scripts/write_QC_report.Rmd $PREFIX/opt/aquamis/scripts/write_report.Rmd $PREFIX/opt/aquamis/scripts/pipeline_test.sh $PREFIX/bin/
+ln -s $PREFIX/opt/aquamis/scripts/aquamis_setup.sh $PREFIX/opt/aquamis/scripts/create_sampleSheet.sh $PREFIX/opt/aquamis/scripts/filter_json.py $PREFIX/opt/aquamis/scripts/parse_json.py $PREFIX/opt/aquamis/scripts/helper_functions.py $PREFIX/opt/aquamis/scripts/write_QC_report.Rmd $PREFIX/opt/aquamis/scripts/write_report.Rmd  $PREFIX/bin/
 
 ln -s $PREFIX/opt/aquamis/aquamis.py $PREFIX/bin/aquamis
 
