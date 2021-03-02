e6132d1906befa756aca6c5f3784839ea687f7de
grenaud <gabriel.reno@gmail.com>
2021-03-02 11:54:01 +0100

    changed recipe

--- recipes/leehom/build.sh
+++ recipes/leehom/build.sh
@@ -3,7 +3,8 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
+cd src/
 make  CXX="${CXX}" LIBGABINC=${PREFIX}/include/libgab/ LIBGABLIB=${PREFIX}/lib/libgab/ BAMTOOLSINC=${PREFIX}/include/bamtools/ BAMTOOLSLIB=${PREFIX}/lib/ all
-cp src/leeHom ${PREFIX}/bin/
+cp leeHom ${PREFIX}/bin/
 cd ..
 
