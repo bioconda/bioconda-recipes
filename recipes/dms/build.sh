e68ee82d0bf44cde8d4604a145bf0fda0f9185d1
qibebt-bioinfo <qibebt_bioinfo@163.com>
2021-03-04 09:34:20 +0800

    update file

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -4,7 +4,7 @@ export DynamicMetaStorms=${PREFIX}
 
 mkdir -p ${PREFIX}/bin
 
-make CXX="${CXX} -fopenmp -O3 -I${PREFIX}/include/ -I${BUILD_PREFIX}/include -I${PREFIX}/include -L${PREFIX}/lib -lz -lpthread"
+make CXX="${CXX} -fopenmp -O3 -I${PREFIX}/include/ -I${BUILD_PREFIX}/include -I${PREFIX}/include -L${PREFIX}/lib -lpthread"
 
 cp -r databases ${PREFIX}
 cp -r example ${PREFIX}
