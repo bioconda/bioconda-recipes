99d6efb75f0198661c24c08d38e13aacba675ddd
Joao F. Matias Rodrigues <joao.rodrigues@imls.uzh.ch>
2021-03-04 22:30:45 +0100

    Trying to fix finding data dir path2

--- recipes/mapseq/post-link.sh
+++ recipes/mapseq/post-link.sh
@@ -45,13 +45,16 @@ if [[ $SUCCESS != 1 ]]; then
 fi
 
 tar -xvzf $TARBALL
+ls $STAGING/mapref-2.2b >> $PREFIX/.messages.txt
 mv $STAGING/mapref-2.2b/* $STAGING/
 rmdir $STAGING/mapref-2.2b
 rm $TARBALL
 
 # testing to ensure dataset is correctly installed and found by mapseq
+ls $STAGING >> $PREFIX/.messages.txt
 head -n 2 $STAGING/mapref-2.2b.fna >> $PREFIX/.messages.txt
-strings `which mapseq` | grep mapref >> $PREFIX/.messages.txt
+echo `which mapseq` >> $PREFIX/.messages.txt
+strings `which mapseq` | grep 'share/' >> $PREFIX/.messages.txt
 
 head -n 2 $STAGING/mapref-2.2b.fna | mapseq - > $STAGING/test.fna.mseq 2>> $PREFIX/.messages.txt || ( echo "ERROR running mapseq"; exit -1 )
 cat $STAGING/test.fna.mseq >> $PREFIX/.messages.txt
