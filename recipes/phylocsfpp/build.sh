06e4cc39b2fa06f84a429990b18a232cb39665ff
Christopher Pockrandt <noreply-github@cpockrandt.de>
2021-03-09 15:08:46 +0100

    pass include and library path to gcc

--- recipes/phylocsfpp/build.sh
+++ recipes/phylocsfpp/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
+
+CFLAGS="$CFLAGS -g -Wall -O3 -Wsign-compare -L$PREFIX/lib -I$PREFIX/include"
+
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release
