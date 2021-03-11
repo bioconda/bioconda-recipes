4674450b9481932afaadb362449ec1a650a98593
Devon Ryan <dpryan79@users.noreply.github.com>
2021-03-11 11:38:06 +0100

    Update build.sh

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -6,6 +6,7 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
 
 #autoreconf -i -f
 #./configure --with-libmaus2=${PREFIX}/lib	--prefix=${PREFIX}/bin/
