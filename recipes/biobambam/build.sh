cab00f5f761feb2ec2de6b14c913f95eda1b68bb
Devon Ryan <dpryan79@users.noreply.github.com>
2021-03-11 11:23:29 +0100

    Try autodetecting libmaus2 path

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -3,7 +3,12 @@ set -eu
 
 mkdir -p $PREFIX/bin
 LDFLAGS="-L$PREFIX/lib"
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export LD_LIBRARY_PATH=${PREFIX}/lib
 
 #autoreconf -i -f
-./configure --with-libmaus2=${PREFIX}/lib	--prefix=${PREFIX}/bin/
+#./configure --with-libmaus2=${PREFIX}/lib	--prefix=${PREFIX}/bin/
+./configure --prefix=${PREFIX}/bin/
 make install
