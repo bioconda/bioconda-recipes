5f988d1bfa6f697daa599dfa0f76e6fcb1a7b79f
Jon Puritz <jpuritz@gmail.com>
2021-03-11 09:21:19 -0500

    Update build.sh

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -14,12 +14,9 @@ ninja -v
 ninja -v install
 
 ##Copy scripts over ## This will likely need to be removed with an updated build
-#cp -n ../scripts/*.py $PREFIX/bin
-#cp -n ../scripts/*.sh $PREFIX/bin
-#cp -n ../scripts/*.pl $PREFIX/bin 
+cp -n ../scripts/*.py $PREFIX/bin
+cp -n ../scripts/*.sh $PREFIX/bin
+cp -n ../scripts/*.pl $PREFIX/bin 
 #cp -n ../scripts/*.R $PREFIX/bin
-#cp -n ../scripts/bgziptabix  $PREFIX/bin
-
-ls ../scripts
 
 cp -n ../scripts/freebayes-parallel $PREFIX/bin
