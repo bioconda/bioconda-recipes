a383479e68b8f96206aecd41c3d1acfabff3c6fc
Matthias De Smet <matthias.desmet@ugent.be>
2021-03-12 13:15:48 +0100

    Update build.sh

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -2,13 +2,8 @@
 set -eu
 
 mkdir -p $PREFIX/lib
-export LDFLAGS="-lstdc++fs"
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export LIBRARY_PATH=${PREFIX}/lib
+export LIBS="-lstdc++fs -lcurl"
 
-./configure --prefix $PREFIX
+./configure --prefix $PREFIX --with-snappy --with-io_lib
 make
 make install
