08f89a0002ac3fa9255a04b7fdcc6ca6e4a80ddc
Matthias De Smet <matthias.desmet@ugent.be>
2021-03-12 15:13:42 +0100

    Update build.sh

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -8,7 +8,5 @@ export CXX_INCLUDE_PATH=${PREFIX}/include
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export LIBRARY_PATH=${PREFIX}/lib
 
-#autoreconf -i -f
-#./configure --with-libmaus2=${PREFIX}/lib	--prefix=${PREFIX}/bin/
-./configure --prefix=${PREFIX}/bin/
+./configure --with-libmaus2=${PREFIX}/lib	--prefix=${PREFIX}/bin/
 make install
