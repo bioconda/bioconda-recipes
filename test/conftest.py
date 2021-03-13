f711786180114cf9c1569ae3613393a643e6e9e3
Devon Ryan <dpryan79@gmail.com>
2021-03-13 23:05:40 +0100

    is it something in the pinning that's the problem?

--- test/conftest.py
+++ test/conftest.py
@@ -65,7 +65,6 @@ def mock_repodata(repodata, case):
     else:
         data = repodata
 
-    print(utils.RepoData.columns)
     dataframe = pd.DataFrame(columns=utils.RepoData.columns)
     for channel, packages in data.items():
         for name, versions in packages.items():
