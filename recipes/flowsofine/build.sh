f4c003a1c6568e355cf0515edec5ff25ae8409d3
Stefan Janssen <stefan.m.janssen@gmail.com>
2021-03-16 13:05:09 +0100

    don't set ENVs

--- recipes/flowsofine/build.sh
+++ recipes/flowsofine/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir -p ~/.R
-echo -e "CC=$CC
-FC=$FC
-CXX=$CXX
-CXX98=$CXX
-CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
+#mkdir -p ~/.R
+#echo -e "CC=$CC
+#FC=$FC
+#CXX=$CXX
+#CXX98=$CXX
+#CXX11=$CXX
+#CXX14=$CXX" > ~/.R/Makevars
 
 # install bioconductor
 #echo 'if (!requireNamespace("BiocManager", quietly = TRUE))' > install.r
