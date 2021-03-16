422610dbe88119f63d02d006f076cf04712bbf18
Björn Grüning <bjoern@gruenings.eu>
2021-03-17 00:42:28 +0100

    [ci skip] Merge PR 27404
    Merge PR #27404, commits were: 
     * add new binaries and tests
     * Update build.sh
     * usher-0.2.0

--- recipes/usher/build.sh
+++ recipes/usher/build.sh
@@ -7,9 +7,11 @@ pushd build
 
 cmake -DTBB_DIR=${PWD}/../oneTBB-2019_U9 -DCMAKE_PREFIX_PATH=${PWD}/../oneTBB-2019_U9/cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 
-make -j
+make -j 1
 
 cp ./usher ${PREFIX}/bin/
+cp ./matUtils ${PREFIX}/bin/
+cp ./matOptimize ${PREFIX}/bin/
 cp ./tbb_cmake_build/tbb_cmake_build_subdir_release/* ${PREFIX}/lib/
 
 popd
