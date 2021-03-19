f71baa145fbf51458ab9ab36f9117488c2f80385
svarona <sarai.var.fer@gmail.com>
2021-03-19 14:16:54 +0100

    Changed symbolic link to cp

--- recipes/abacas/build.sh
+++ recipes/abacas/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod +x abacas.1.3.1.pl
-cp abacas.1.3.1.pl ${PREFIX}/bin
-ln -s ${PREFIX}/bin/abacas.1.3.1.pl ${PREFIX}/bin/abacas.pl
+cp abacas.1.3.1.pl $PREFIX/bin
+cp $PREFIX/bin/abacas.1.3.1.pl $PREFIX/bin/abacas.pl
