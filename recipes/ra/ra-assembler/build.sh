66eb1a1f02395d2192702d954d298b871e2e5f47
Ramon Rivera <riveravicens@gmail.com>
2021-03-19 11:14:18 -0700

    Update build.sh

--- recipes/ra/ra-assembler/build.sh
+++ recipes/ra/ra-assembler/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
 mkdir -p $PREFIX/bin
 rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
-cp ./bin/ra $PREFIX/bin/ra
+make -j
+cp ./bin/* $PREFIX/bin/
+cp ../vendor/minimap2/minimap2 $PREFIX/bin/
+cp ./vendor/rala/bin/rala $PREFIX/bin/
+cp ./vendor/racon/bin/racon $PREFIX/bin/
+sed -i.bak "s|\(__minimap = \)\(.*\)|\1\'${PREFIX}\/bin\/minimap2\'|" $PREFIX/bin/ra
+sed -i.bak "s|\(__rala = \).*|\1\'${PREFIX}\/bin\/rala\'|" $PREFIX/bin/ra
+sed -i.bak "s|\(__racon = \).*|\1\'${PREFIX}\/bin\/racon\'|" $PREFIX/bin/ra
