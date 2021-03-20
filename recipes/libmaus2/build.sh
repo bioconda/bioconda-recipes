eb9c34e4aece30dedc50059bfaad59f0376ecc4f
Matthias De Smet <matthias.desmet@ugent.be>
2021-03-20 11:52:16 +0100

    debug, new io_lib

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -eu
-export LIBS="-lstdc++fs -lcurl"
+export LIBS="-lstdc++fs -lcurl -lio_lib"
 
-./configure --prefix $PREFIX --with-snappy --with-io_lib || cat config.log
+./configure --prefix $PREFIX --with-snappy --with-io_lib
+
+cat config.log
 
 make
 make install
