7a9ab24cc9924b3fc1dc868d1cc65eb035dc9aff
Rohan Shah <rohanshah@uchicago.edu>
2021-03-23 21:12:43 -0500

    updating build.sh to connect zlib

--- recipes/smartmap/build.sh
+++ recipes/smartmap/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-make
+pushd gzstream
+make CXX="$CXX $CXXFLAGS" CPPFLAGS="$CPPFLAGS -I. -I$PREFIX/include" AR="$AR cr"
+popd
+
+pushd Default
+make all
+popd
+
 mkdir -p $PREFIX/bin
 cp SmartMapPrep $PREFIX/bin
 cp SmartMapRNAPrep $PREFIX/bin
