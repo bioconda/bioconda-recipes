8685bc13c990fc30300db5582ddd720aa83a845b
Devon Ryan <dpryan79@gmail.com>
2021-04-01 16:31:47 +0200

    bulk update

--- recipes/mrsfast/build.sh
+++ recipes/mrsfast/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CPATH=${PREFIX}/include
 export CFLAGS="${CFLAGS} -fcommon"
+export CXXFLAGS="${CXXFLAGS} -fcommon"
 make with-sse4=no
 mkdir -p ${PREFIX}/bin
 cp -f mrsfast ${PREFIX}/bin
