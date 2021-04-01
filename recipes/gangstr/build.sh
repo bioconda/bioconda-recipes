92badb3046e2c24d33fe2f7c6561741c57632050
Marcel Bargull <marcel.bargull@udo.edu>
2021-04-01 10:25:11 +0200

    gangstr: cleanup

--- recipes/gangstr/build.sh
+++ recipes/gangstr/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 mkdir build
 cd build
-cmake ..
-make
-cmake --install . --prefix ${PREFIX}
-# TODO: copy python scripts
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
+make -j "${CPU_COUNT}" install
