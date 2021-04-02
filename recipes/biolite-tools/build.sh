ce6c457ca34a3a8ca98a962b50892f925f590afe
Devon Ryan <dpryan79@gmail.com>
2021-04-02 09:48:20 +0200

    bulk updates

--- recipes/biolite-tools/build.sh
+++ recipes/biolite-tools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #error: invalid suffix on literal; C++11 requires a space between literal and identifier [-Wreserved-user-defined-literal]
-sed -i.bak "s/PROGNAME/ PROGNAME /" coverage.cpp
-sed -i.bak "s/PROGNAME/ PROGNAME /" exclude.cpp
+sed -i.bak "s/PROGNAME/ PROGNAME /" tools/src/coverage.cpp
+sed -i.bak "s/PROGNAME/ PROGNAME /" tools/src/exclude.cpp
 ./configure --prefix="${PREFIX}" \
             CXXFLAGS="-I${PREFIX}/include" \
             LDFLAGS="-L${PREFIX}/lib"
--- recipes/gimmemotifs/build.sh
+++ recipes/gimmemotifs/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mv src/utils/bedtools/gzstream/version src/utils/bedtools/gzstream/version.txt
 $PYTHON -m pip install --ignore-installed --no-deps -vv .
