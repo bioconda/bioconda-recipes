b5bfcc0a832af320548b9d27bb635969f5d637b2
Devon Ryan <dpryan79@gmail.com>
2021-04-03 00:02:53 +0200

    update bulk

--- recipes/biolite-tools/build.sh
+++ recipes/biolite-tools/build.sh
@@ -2,6 +2,7 @@
 #error: invalid suffix on literal; C++11 requires a space between literal and identifier [-Wreserved-user-defined-literal]
 sed -i.bak "s/PROGNAME/ PROGNAME /" src/coverage.cpp
 sed -i.bak "s/PROGNAME/ PROGNAME /" src/exclude.cpp
+sed -i.bak "s/PROGNAME/ PROGNAME /" src/fasta2fastq.cpp
 ./configure --prefix="${PREFIX}" \
             CXXFLAGS="-I${PREFIX}/include" \
             LDFLAGS="-L${PREFIX}/lib"
