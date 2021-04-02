bafea8dbbf24a8d09f52143f8b664a2774621e29
Guillermo Dufort y Álvarez <gdufort@fing.edu.uy>
2021-04-02 17:11:32 -0300

    Changes to build.sh

--- recipes/renano/build.sh
+++ recipes/renano/build.sh
@@ -9,7 +9,8 @@ cd renano
 # because we can't get to the `make` call easily.
 sed -i.bak 's/CXX *=/CXX ?=/; s/CXXFLAGS *=/CXXFLAGS +=/' Makefile
 sed -i.bak 's/^LDFLAGS  =$//g' Makefile
-
+CXXFLAGS=""
+CPPFLAGS=""
 # Work around "unknown type name 'mach_port_t'" error
 if [ x"$(uname)" == x"Darwin" ]; then
   CXXFLAGS="$CXXFLAGS -D_DARWIN_C_SOURCE"
@@ -17,10 +18,12 @@ if [ x"$(uname)" == x"Darwin" ]; then
   export CXXFLAGS CPPFLAGS
 fi
 
-export CPATH=${PREFIX}/include
-export CXXPATH=${PREFIX}/include
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+# export CPATH=${PREFIX}/include
+# export CXXPATH=${PREFIX}/include
+# export CFLAGS="$CFLAGS -I$PREFIX/include"
+export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+echo $CXXFLAGS
+echo $LDFLAGS
 make
 cp renano $PREFIX/bin
