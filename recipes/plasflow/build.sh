3f2a65ee96474e3c923860cfa86a3ec69064a3e2
Matthias Bernt <m.bernt@ufz.de>
2021-04-04 10:04:05 +0200

    try to use conda's tensorflow

--- recipes/plasflow/build.sh
+++ recipes/plasflow/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# install tensorflow version 0.10.0rc0
-pip install --no-deps --ignore-installed --no-cache-dir  tensorflow/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
+# # install tensorflow version 0.10.0rc0
+# pip install --no-deps --ignore-installed --no-cache-dir  tensorflow/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
 
 
 # install PlasFlow
