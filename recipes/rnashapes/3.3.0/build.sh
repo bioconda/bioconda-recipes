25ee21573c577aa1ae899e0f7fbbc848d8a63865
Devon Ryan <dpryan79@users.noreply.github.com>
2021-04-04 22:20:09 +0200

    Bulk (#27784)
    * Clean bulk, use the bulk branch of bioconda-common
    
    * set env variable for bulk
    
    * Build bowtie2 and salmon
    
    * Fix salmon build.sh, add make to 1390 recipes
    
    * Fix libgenome spacing
    
    * Fix bpp-core
    
    * fix recipe spacing
    
    * bulk: bioconda-utils update-pinning
    
    * salmon: Add boost-cpp to run requirements
    
    * blacklist failed recipes
    
    * bulk BL updates
    
    * More bulk updates
    
    * bulk updates
    
    * bulk updates
    
    * skip UCSC builds on OSX
    
    * Update bulk
    
    * bulk updates
    
    * bulk updates
    
    * bulk updates
    
    * bulk update
    
    * bulk updates
    
    * bulk update
    
    * bulk updates
    
    * bulk update
    
    * bulk updates
    
    * bulk update
    
    * bulk update
    
    * bulk updates
    
    * update bulk
    
    * bulk update
    
    * oops
    
    * try a few on OSX
    
    * update bulk
    
    * bulk update
    
    * update bulk
    
    * update bulk
    
    * bulk update
    
    * bulk update
    
    * bulk update
    
    * bulk update
    
    * bulk updates
    
    * bulk update
    
    * oops
    
    * update bulk
    
    * bulk update
    
    * bl update
    
    * more bulk updates
    
    * updated updates
    
    * expand blacklist
    
    * nearing the end
    
    * bulk update
    
    * bl update
    
    * bulk update
    
    * clean up the blacklist
    
    * update blacklsit
    
    Co-authored-by: Marcel Bargull <marcel.bargull@udo.edu>
    Co-authored-by: Devon Ryan <devon.ryan@genedata.com>

--- recipes/rnashapes/3.3.0/build.sh
+++ recipes/rnashapes/3.3.0/build.sh
@@ -2,4 +2,4 @@
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAshapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
-chmod +x $PREFIX/bin/*
+chmod +x $PREFIX/bin/RNA*
