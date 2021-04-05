de702f9bfc9bc50521d47d1c078bf6afe6886ffa
Devon Ryan <dpryan79@gmail.com>
2021-04-05 23:16:02 +0200

    update demuxlet

--- recipes/demuxlet/build.sh
+++ recipes/demuxlet/build.sh
@@ -2,15 +2,8 @@
 
 set -x -e
 
-cd htslib
-./configure --prefix=$(pwd) --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
-make
-make install
-
-cd ..
-
 cd demuxlet
-export CFLAGS="${CFLAGS} -I${PREFIX}/include -I ${PREFIX}/include/htslib -ldl -ldeflate -fno-strict-aliasing"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include -I${PREFIX}/include/htslib -ldl -ldeflate -fno-strict-aliasing"
 #export LDFLAGS="-ldl"
 export CXXFLAGS="${CXXFLAGS} -ldl -ldeflate"
 export INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/htslib"
