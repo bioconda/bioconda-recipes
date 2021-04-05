fd5ca241317f69166328f55f96e6ec7457b4ad9c
Matthias Bernt <m.bernt@ufz.de>
2021-04-05 13:57:09 +0200

    debug

--- recipes/plasflow/build.sh
+++ recipes/plasflow/build.sh
@@ -10,4 +10,7 @@ $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 ls -la
 
 # copy models
-cp -r -v PlasFlow/models $PREFIX/bin/
+cp -r -v PlasFlow/models "$PREFIX/bin/"
+
+ls -la "$PREFIX/bin/"
+
