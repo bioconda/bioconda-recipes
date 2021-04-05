0ff3d5ebb4d92403a21d58f48609204e3e0d6057
Matthias Bernt <m.bernt@ufz.de>
2021-04-05 17:22:24 +0200

    Revert "remove space from shebang with sed"
    This reverts commit 0fb47d643fb8f16b79f322d8717d4c24ecff0188.

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -4,9 +4,6 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
 
-# "fix" shebang line (space seems to confuse conda-build)
-find util -name "*pl.in" -exec sed -i -e 's/^\#! @PERL@/\#!@PERL@/' {} \;
-
 ./configure --prefix=${PREFIX} --with-simd-level=sse42
 make -j 2
 make install prefix=${PREFIX}
