050f99766459c9a2cedc0b05f4ccb95843650abf
Devon Ryan <devon.ryan@genedata.com>
2021-04-05 14:01:46 +0200

    cxx fix

--- recipes/gatb/build.sh
+++ recipes/gatb/build.sh
@@ -15,6 +15,7 @@ ln -s ${PREFIX}/include ${PREFIX}/include/hdf5
 mkdir build
 cd build
 
+export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS"
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DZLIB_INCLUDE_DIR:PATH=$PREFIX"/include" ..
 make VERBOSE=1
 make install
