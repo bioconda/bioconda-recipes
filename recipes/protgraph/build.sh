249324cb9fcd6b5cf89f1fd2692fa58fe52dfda9
Luxii <dl94@live.de>
2021-04-06 08:58:21 +0200

    Changed dependencies for protgraph to use them from conda-forge

--- recipes/protgraph/build.sh
+++ recipes/protgraph/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-$PYTHON -m pip install -f https://pypi.org/simple/redis/ -vv redis
-$PYTHON -m pip install -f https://pypi.org/simple/prettytable/ -vv prettytable
-$PYTHON -m pip install -f https://pypi.org/simple/python-igraph/ -vv python-igraph 
 $PYTHON -m pip install -f https://pypi.org/simple/gremlinpython/ -vv gremlinpython
 $PYTHON -m pip install -f https://pypi.org/simple/redisgraph/ -vv redisgraph
-$PYTHON -m pip install -f https://pypi.org/simple/texttable/ -vv texttable
 $PYTHON -m pip install . -vv
