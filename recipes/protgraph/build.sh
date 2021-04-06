741902f429cbc675582b2940dfaa96c969094558
Luxii <dl94@live.de>
2021-04-06 19:07:09 +0200

    Removed build.sh, added missing dependencies into conda-forge

--- recipes/protgraph/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-$PYTHON -m pip install -f https://pypi.org/simple/gremlinpython/ -vv gremlinpython
-$PYTHON -m pip install -f https://pypi.org/simple/redisgraph/ -vv redisgraph
-$PYTHON -m pip install . -vv
