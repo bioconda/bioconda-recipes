542929ef66cfa6b524973121cdfc541e08b0d9ee
Marcel Bargull <marcel.bargull@udo.edu>
2021-04-07 16:05:51 +0200

    rust-bio-tools: Set -march=nehalem

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# Dependency crate librocksdb-sys uses SSE4.1/4.2 unconditionally.
+# => Set architecture compatibility to "nehalem".
+export \
+    CFLAGS="${CFLAGS} -march=nehalem" \
+    CXXFLAGS="${CXXFLAGS} -march=nehalem"
+
 export \
     CARGO_NET_GIT_FETCH_WITH_CLI=true \
     CARGO_HOME="${BUILD_PREFIX}/.cargo" \
