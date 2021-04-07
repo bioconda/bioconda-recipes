43674d1f5f2f61a0d34b351b3f6d87a1134b01e2
Marcel Bargull <marcel.bargull@udo.edu>
2021-04-07 18:36:15 +0200

    rust-bio-tools: Set -march=westmere

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-# Dependency crate librocksdb-sys uses SSE4.1/4.2 unconditionally.
-# => Set architecture compatibility to "nehalem".
+# Dependency crate librocksdb-sys uses SSE4.1/4.2 and PCLMUL unconditionally.
+# => Set architecture compatibility to "westmere".
 export \
-    CFLAGS="${CFLAGS} -march=nehalem" \
-    CXXFLAGS="${CXXFLAGS} -march=nehalem"
+    CFLAGS="${CFLAGS} -march=westmere" \
+    CXXFLAGS="${CXXFLAGS} -march=westmere"
 
 export \
     CARGO_NET_GIT_FETCH_WITH_CLI=true \
