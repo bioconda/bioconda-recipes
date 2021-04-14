aa692b5351d3c9091380ae89e4834e75228497e0
Till Hartmann <till.hartmann@udo.edu>
2021-04-14 13:04:29 +0200

    replace hardcoded perl shebang with env version

--- recipes/simug/build.sh
+++ recipes/simug/build.sh
@@ -4,3 +4,5 @@ mkdir -p $PREFIX/bin/
 
 cp simuG.pl $PREFIX/bin/simuG
 cp vcf2model.pl $PREFIX/bin/vcf2model
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/simuG
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/vcf2model
