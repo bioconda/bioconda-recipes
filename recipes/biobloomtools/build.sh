2aabd4310d227138627e0a3d067726aca6bb529c
Malte Petersen <petersen@ie-freiburg.mpg.de>
2021-04-15 14:39:33 +0200

    Use build script from EXFI README: https://github.com/jlanga/exfi

--- recipes/biobloomtools/build.sh
+++ recipes/biobloomtools/build.sh
@@ -8,6 +8,13 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CFLAGS} -fcommon"
 
+# This script was taken from https://github.com/jlanga/exfi
+
+git clone --recursive https://github.com/bcgsc/biobloom.git
+cd biobloom/
+git submodule update --init
+git checkout 0a42916922d42611a087d4df871e424a8907896e
+./autogen.sh
 ./configure --prefix=${PREFIX}
-make
+make -j 4
 make install
