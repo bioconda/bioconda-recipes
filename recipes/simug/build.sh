73002c9b795c15e89f389e9f9fa309cf3beb2413
Marcel Bargull <mbargull@users.noreply.github.com>
2021-04-16 15:36:02 +0200

    [ci skip] Merge PR 28062
    Merge PR #28062, commits were: 
     * simug: Remove backup files from package

--- recipes/simug/build.sh
+++ recipes/simug/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin/
+mkdir -p "${PREFIX}/bin"
 
-cp simuG.pl $PREFIX/bin/simuG
-cp vcf2model.pl $PREFIX/bin/vcf2model
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/simuG
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/vcf2model
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|' simuG.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|' vcf2model.pl
+
+cp simuG.pl "${PREFIX}/bin/simuG"
+cp vcf2model.pl "${PREFIX}/bin/vcf2model"
