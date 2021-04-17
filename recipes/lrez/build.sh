7812db70d0b23bc0e947e5196a9ac06edd0beaa8
PIerre Morisse <contact@pierremorisse.com>
2021-04-17 10:32:12 +0200

    Try copying libs to the bin folder

--- recipes/lrez/build.sh
+++ recipes/lrez/build.sh
@@ -2,3 +2,8 @@
 
 make -j"${CPU_COUNT}"
 make install
+if [ ${SHLIB_EXT} == ".dylib" ]; then
+	mkdir -p ${PREFIX}/bin/lib
+	cp -a ${PREFIX}/lib/liblrez* ${PREFIX}/bin/lib/
+	cp -a ${PREFIX}/lib/liblrez* ${PREFIX}/bin/
+fi
