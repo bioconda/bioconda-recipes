cdc3ff7e4f1908194e4a7f82f14a1fb3c5e893a3
Daniel Cooke <dcooke@well.ox.ac.uk>
2021-04-17 23:47:34 +0100

    Revert "Try pure CMake build"
    This reverts commit a625cb03300f45c08ad5d5fbec0812fbae40594d.

--- recipes/octopus/build.sh
+++ recipes/octopus/build.sh
@@ -6,20 +6,13 @@ export CPATH=${PREFIX}/include
 export CMAKE_LDFLAGS="-L${PREFIX}/lib"
 export LIBRARY_PATH=${PREFIX}/lib
 
-sed -i.bak 's/ -Werror / /' src/CMakeLists.txt
-
-cd build
-cmake  -DCMAKE_CXX_COMPILER_AR=${AR} \
-       -DCMAKE_CXX_COMPILER_RANLIB=${RANLIB} \
-       -DINSTALL_PREFIX=TRUE \
-       -DCMAKE_INSTALL_PREFIX=${PREFIX}/bin \
-       -DCMAKE_BUILD_TYPE=Release \
-       -DBOOST_ROOT=${PREFIX} \
-       -DBoost_NO_BOOST_CMAKE=TRUE \
-       -DBoost_NO_SYSTEM_PATHS=TRUE \
-       -DHTSLIB_ROOT=${PREFIX} \
-       -DHTSlib_NO_SYSTEM_PATHS=TRUE \
-       -DGMP_ROOT=${PREFIX}
-       ..
-
-make install
+scripts/install.py \
+    -c ${CC_FOR_BUILD} \
+    -cxx ${CXX_FOR_BUILD} \
+    --prefix ${PREFIX}/bin \
+    --gmp ${PREFIX} \
+    --boost ${PREFIX} \
+    --htslib ${PREFIX} \
+    --architecture haswell \
+    --threads 1 \
+    --verbose
