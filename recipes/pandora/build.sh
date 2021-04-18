595ccfae736f389500e0d13a5f756213d361b499
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-04-18 21:25:19 +0100

    Testing if this works on Circle CI build (works locally)

--- recipes/pandora/build.sh
+++ recipes/pandora/build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -eux -o pipefail
 
+# make compilation not be dependent on locale settings
+export LC_ALL=C
+
+# allows boost to find gcc/g++ toolset
+BIN_DIR=$(realpath /opt/conda/conda-bld/pandora_*/_build_env/bin/)
+cp "${BIN_DIR}/x86_64-conda-linux-gnu-gcc" "${BIN_DIR}/gcc"
+cp "${BIN_DIR}/x86_64-conda-linux-gnu-g++" "${BIN_DIR}/g++"
+
+# build pandora
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DPRINT_STACKTRACE=True \
+cmake -DBIOCONDA=True \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DHUNTER_JOBS_NUMBER=4 \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       ..
-make VERBOSE=1
-ctest -VV
+make -j4
+
+# test
+ctest
+
+# install
 make install
