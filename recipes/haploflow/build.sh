7fad81de7c3141cb564fde570e908c1e169da53a
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-04-20 14:52:23 +0200

    fixes and linting

--- recipes/haploflow/build.sh
+++ recipes/haploflow/build.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-echo "Build assumes Ubuntu >= 14.04"
-
-sudo apt install cmake libboost-all-dev ruby-ronn git build-essential
-
-DIR=`pwd`
-
 mkdir -p build
 cd build
 cmake ..
 make
 
-sudo cp haploflow /usr/local/bin
-
-cd ..
-ln -sf README.md haploflow.md
-ronn -r --pipe haploflow.md > haploflow.1
-mkdir -p /usr/local/man/man1
-sudo cp haploflow.1 /usr/local/man/man1
-
-cd /
-tar czvf $DIR/build/haploflow.tar.gz usr/local/bin/haploflow usr/local/man/man1/haploflow.1
+cp haploflow $PREFIX/bin/
