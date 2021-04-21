5991c40ebb6dfd09aea74bed49e542c88c6b0ed8
Devon Ryan <dpryan79@users.noreply.github.com>
2021-04-21 15:24:17 +0200

    Update build.sh

--- recipes/leviathan/build.sh
+++ recipes/leviathan/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -j"${CPU_COUNT}" BUILD_DIR=$PREFIX
-make install BUILD_DIR=$PREFIX
+make -j"${CPU_COUNT}" BUILD_DIR=$PREFIX LREZ_LIBDIR=${PREFIX}/lib
+make install BUILD_DIR=$PREFIX LREZ_LIBDIR=${PREFIX}/lib
