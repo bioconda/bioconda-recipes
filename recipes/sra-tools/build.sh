7f235e87dcd3374cb27d5da79965c11d2fb2edc7
Marcel Bargull <marcel.bargull@udo.edu>
2021-04-27 08:22:00 +0200

    sra-tools: Add some TODO comments

--- recipes/sra-tools/build.sh
+++ recipes/sra-tools/build.sh
@@ -29,6 +29,12 @@ NCBI_OUTDIR=$SRC_DIR/ncbi-outdir
 echo "compiling ncbi-vdb"
 pushd ncbi-vdb
 
+# TODO: Ideally, we don't want the dynamic prefix-dependent patching below.
+#       The following hard-codes the environment's path in binaries such that
+#       those binaries have to be patched upon installation by conda
+#       (with the usual downsides, e.g., being excluded from hard-linking).
+#       This patch applies to a library which is statically linked such that
+#       nearly all binaries of the package are affected by this.
 {
 cat <<end-of-patch
 --- libs/kns/tls.c
