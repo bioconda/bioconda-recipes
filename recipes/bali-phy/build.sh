13d9c0a045b44995fb7c7a400336b00a3e6eceb7
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-04-27 10:39:35 -0400

    New recipe bali-phy 3.6.0

---
+++ recipes/bali-phy/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -vex
+
+export BOOST_ROOT="${PREFIX}"
+export PKG_CONFIG_LIBDIR="${PREFIX}"/lib/pkgconfig
+
+pkg-config --list-all
+
+# configure
+export CC=clang
+export CXX=clang++
+meson \
+    --prefix="$PREFIX" \
+    -Db_ndebug=true \
+    builddir .
+
+cd builddir
+
+# build
+meson compile
+
+# test
+meson test
+
+# install
+meson install
