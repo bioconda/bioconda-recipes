3e7ba8d87506e6310cf193262e1f204abc1031ae
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-04-27 14:40:10 -0400

    Print pkg-config flags for cairo, and meson build logs.

--- recipes/bali-phy/build.sh
+++ recipes/bali-phy/build.sh
@@ -6,6 +6,7 @@ export BOOST_ROOT="${PREFIX}"
 export PKG_CONFIG_LIBDIR="${PREFIX}"/lib/pkgconfig
 
 pkg-config --list-all
+pkg-config --cflags cairo
 
 echo "CC=$CC"
 echo "CXX=$CXX"
@@ -18,6 +19,8 @@ meson \
     builddir .
 
 cd builddir
+
+cat meson-logs/meson-log.txt
 
 # build
 meson compile
