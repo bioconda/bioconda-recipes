85939324cc4e6db7ef0072b1967970d7832697fa
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-04-28 14:15:28 -0400

    Edit CXXFLAGS with sed....

--- recipes/bali-phy/build.sh
+++ recipes/bali-phy/build.sh
@@ -9,7 +9,12 @@ pkg-config --list-all
 pkg-config --cflags cairo
 
 echo "CC=$CC"
+echo "CFLAGS=$CFLAGS"
 echo "CXX=$CXX"
+echo "CXXFLAGS=$CXXFLAGS"
+echo "Removing -O2 and -std=c++14 from CXXFLAGS..."
+CXXFLAGS="$(echo $CXXFLAGS | sed 's/-O2//g' | sed 's/-std=c++14//g')"
+echo "CXXFLAGS=$CXXFLAGS"
 
 # configure
 meson \
