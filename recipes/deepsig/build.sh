9cdb2ecd9ef5005a09fc629d11caf16744c639e8
Oliver Schwengers <oschwengers@users.noreply.github.com>
2021-04-29 21:19:55 +0200

    fix package name var in build.sh

--- recipes/deepsig/build.sh
+++ recipes/deepsig/build.sh
@@ -2,8 +2,8 @@
 
 python -m pip install --no-deps --ignore-installed .
 
-cp -r models/ $PREFIX/opt/PKG_NAME-$PKG_VERSION/
-cp -r tools/ $PREFIX/opt/PKG_NAME-$PKG_VERSION/
+cp -r models/ $PREFIX/opt/$PKG_NAME-$PKG_VERSION/
+cp -r tools/ $PREFIX/opt/$PKG_NAME-$PKG_VERSION/
 
 # add de/activation scripts to set the DEEPSIG_ROOT env var
 for CHANGE in "activate" "deactivate"
