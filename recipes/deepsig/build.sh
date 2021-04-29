5f0293ef5a1c70d855d62a356fd304d17468c927
Oliver Schwengers <oschwengers@users.noreply.github.com>
2021-04-29 16:56:36 +0200

    Create build.sh

---
+++ recipes/deepsig/build.sh
@@ -0,0 +1,7 @@
+
+
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
