89b7a2a5db0e97c79055657777dc1d35265a5d69
Oliver Schwengers <oschwengers@users.noreply.github.com>
2021-04-29 17:07:34 +0200

    copy models/tools in build.sh

--- recipes/deepsig/build.sh
+++ recipes/deepsig/build.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 
+python -m pip install --no-deps --ignore-installed .
 
+cp -r models/ $PREFIX/opt/PKG_NAME-$PKG_VERSION/
+cp -r tools/ $PREFIX/opt/PKG_NAME-$PKG_VERSION/
+
+# add de/activation scripts to set the DEEPSIG_ROOT env var
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
