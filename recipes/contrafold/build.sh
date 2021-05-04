0134c196e760ccdd894640b2779dfdfc50643133
Fabien Pertuy <Fabien.Pertuy@Sanofi.com>
2021-05-04 17:49:35 +0000

    FIX bug in recipe

--- recipes/contrafold/build.sh
+++ recipes/contrafold/build.sh
@@ -6,5 +6,5 @@ mkdir -p $PREFIX/shared/contrafold
 mkdir -p $PREFIX/bin
 
 cp Contrafold.o Defaults.ipp FileDescription.o Options.o SStruct.o ScorePrediction.o Utilities.o contrafold score_prediction $PREFIX/shared/contrafold/
-ln -s $PREFIX/bin/contrafold $PREFIX/shared/contrafold/contrafold
-ln -s $PREFIX/bin/score_prediction $PREFIX/shared/contrafold/score_prediction
+ln -s $PREFIX/shared/contrafold/contrafold $PREFIX/bin/contrafold
+ln -s $PREFIX/shared/contrafold/score_prediction $PREFIX/bin/score_prediction
