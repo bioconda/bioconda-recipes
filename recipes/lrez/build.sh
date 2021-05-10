89e2ef325b6f8195a1e3b124c9412e9902928b11
PIerre Morisse <contact@pierremorisse.com>
2021-05-10 09:48:14 +0200

    Update recipe for LRez 2.1

--- recipes/lrez/build.sh
+++ recipes/lrez/build.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Change path to point to conda install
-if [ "$(uname)" == "Darwin" ]; then
-	sed -i '' -e "s#../barcodes/#..share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/#" src/main.cpp
-else
-	sed -i'' -e "s#../barcodes/#..share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/#" src/main.cpp
-fi
-
 make -j"${CPU_COUNT}" \
     BUILD_BINDIR='$(BUILD_PREFIX)' \
     BUILD_LIBDIR='$(BUILD_PREFIX)'
 make install \
     BUILD_BINDIR='$(BUILD_PREFIX)' \
     BUILD_LIBDIR='$(BUILD_PREFIX)'
-
-# install barcodes files
-mkdir -p $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/
-cp barcodes/* $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/
