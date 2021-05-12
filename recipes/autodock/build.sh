c4013c0e9fb4ef6813a67ab688a61027cb410f2e
Devon Ryan <dpryan79@users.noreply.github.com>
2021-05-12 09:43:04 +0200

    Update build.sh

--- recipes/autodock/build.sh
+++ recipes/autodock/build.sh
@@ -4,5 +4,6 @@ ln -s $BUILD_PREFIX/bin/tcsh $BUILD_PREFIX/bin/csh
 ./configure PREFIX=$PREFIX
 make
 make check
-make install PREFIX=$PREFIX
+mkdir -p $PREFIX/bin
+install -c autodock4 autodock4.omp $PREFIX/bin
 unlink $BUILD_PREFIX/bin/csh
