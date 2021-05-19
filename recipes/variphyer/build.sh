b7f121c077ac01d6b06c5387d9363bf08f29d959
root <root@DESKTOP-2C6G0GI.localdomain>
2021-05-19 11:27:10 -0400

    add a wrapper and a build with only --help

--- recipes/variphyer/build.sh
+++ recipes/variphyer/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo ${PREFIX}/bin
-echo ls ${PREFIX}/bin
-echo ls ${PREFIX}/share/
+VARIPHYER="${PREFIX}/share/${PKG_NAME}"
+mkdir -p ${PREFIX}/bin ${VARIPHYER}
+
+chmod 777 variphyer
+mv variphyer ${PREFIX}/bin
+
+# Move bactopia nextflow
+mv main.nf nextflow.config ${VARIPHYER}
