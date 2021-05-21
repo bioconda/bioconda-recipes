8fb2bfb6f722bec05280b142ce3bf05efc1a56f5
Fabien Pertuy <Fabien.Pertuy@Sanofi.com>
2021-05-21 15:18:32 +0000

    CREATE recipe for linearpartition

---
+++ recipes/linearpartition/build.sh
@@ -0,0 +1,11 @@
+# Build
+CC=$GXX
+make -e
+
+# Install
+mkdir -p $PREFIX/shared/linearpartition
+mkdir -p $PREFIX/bin
+
+cp -r bin/ linearpartition AUTHORS draw_heatmap $PREFIX/shared/linearpartition/
+ln -sr $PREFIX/shared/linearpartition/linearpartition $PREFIX/bin/linearpartition
+ln -sr $PREFIX/shared/linearpartition/draw_heatmap $PREFIX/bin/draw_heatmap
