287b6e0fda5251f4af25fbb4c165f969b0cf3f5d
Fabien Pertuy <PertuyF@users.noreply.github.com>
2021-05-22 05:57:52 +0200

    Trigger rebuild

--- recipes/linearpartition/build.sh
+++ recipes/linearpartition/build.sh
@@ -9,3 +9,4 @@ mkdir -p $PREFIX/bin
 cp -r bin/ linearpartition AUTHORS draw_heatmap $PREFIX/shared/linearpartition/
 ln -sr $PREFIX/shared/linearpartition/linearpartition $PREFIX/bin/linearpartition
 ln -sr $PREFIX/shared/linearpartition/draw_heatmap $PREFIX/bin/draw_heatmap
+
