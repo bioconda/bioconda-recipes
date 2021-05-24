9c40a9c72bda29460fb18bfb24ac6de759af278d
kakatoh <katoh@ifrec.osaka-u.ac.jp>
2021-05-25 00:46:47 +0900

    Re-enabled DASH

--- recipes/mafft/build.sh
+++ recipes/mafft/build.sh
@@ -3,5 +3,5 @@
 # cd to location of Makefile and source
 cd $SRC_DIR/core
 
-make CC="$CC" CFLAGS="$CFLAGS" PREFIX="$PREFIX"
-make install PREFIX="$PREFIX"
+make CC="$CC" CFLAGS="$CFLAGS" PREFIX="$PREFIX" DASH_CLIENT="dash_client"
+make install PREFIX="$PREFIX" DASH_CLIENT="dash_client"
