85cf3b9d4eba3ab11f749ae2138e1a7b3c9ac32e
root <root@DESKTOP-2C6G0GI.localdomain>
2021-05-24 20:32:58 -0400

    add more tools

--- recipes/variphyer/build.sh
+++ recipes/variphyer/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # define and make a src folder
-VARIPHYER="${PREFIX}/share/${PKG_NAME}"
+VARIPHYER=${PREFIX}/share/${PKG_NAME}
 mkdir -p ${VARIPHYER}
 
 # copy everything into src folder
 cp -r ./* ${VARIPHYER}
 
-chmod 777 ${VARIPHYER}/variphyer
+chmod 777 ${VARIPHYER}/vaphy
 
 #make a bin folder
 mkdir -p ${PREFIX}/bin 
 
-ln -s ${VARIPHYER}/variphyer ${PREFIX}/bin/variphyer
+ln -s ${VARIPHYER}/vaphy ${PREFIX}/bin/vaphy
+ln -s ${VARIPHYER}/main.nf ${PREFIX}/bin/main.nf
