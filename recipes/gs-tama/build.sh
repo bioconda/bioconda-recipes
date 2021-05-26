94d01c52a8c99642d026cf7fd5db5ba4d3fdb18d
Sébastien Guizard <sguizard@ed.ac.uk>
2021-05-26 13:43:04 +0100

    👌 IMPROVE: Remove commented code

--- recipes/gs-tama/build.sh
+++ recipes/gs-tama/build.sh
@@ -12,9 +12,3 @@ set -eu -o pipefail
 cd $SRC_DIR
 mkdir -p $PREFIX/bin/
 for i in $(ls); do cp -r $i $PREFIX/bin/; done
-
-#cd $SRC_DIR
-#mkdir -p $PREFIX/bin/
-#cp tama_collapse.py $PREFIX/bin/
-#cp tama_merge.py $PREFIX/bin/
-#for i in $(find tama_go/ -type f -executable); do cp $i $PREFIX/bin/; done  
