18a9d1d1a5798391e6148663396242fe54700d49
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2021-05-27 22:07:22 -0400

    copy ShowLDSVG to $PATH; bump LDBlockShow to v1.40

--- recipes/ldblockshow/build.sh
+++ recipes/ldblockshow/build.sh
@@ -4,4 +4,5 @@ chmod 755 ./configure
 ./configure --prefix=$PREFIX
 make
 mv LDBlockShow $PREFIX/bin/LDBlockShow
+mv ShowLDSVG $PREFIX/bin/ShowLDSVG
 #ln -s $PREFIX/bin/LDBlockShow $PREFIX/bin/ldblockshow
