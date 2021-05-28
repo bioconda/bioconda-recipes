10a64a1e5fbc33e1164c8f5926082c900e4d6f08
Marcel Bargull <marcel.bargull@udo.edu>
2021-05-28 13:52:44 +0200

    rust-bio-tools: Re-enable -march=nocona builds

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-# Dependency crate librocksdb-sys uses SSE4.1/4.2 and PCLMUL unconditionally.
-# => Set architecture compatibility to "westmere".
-export \
-    CFLAGS="${CFLAGS} -march=westmere" \
-    CXXFLAGS="${CXXFLAGS} -march=westmere"
-
 export \
     CARGO_NET_GIT_FETCH_WITH_CLI=true \
     CARGO_HOME="${BUILD_PREFIX}/.cargo" \
