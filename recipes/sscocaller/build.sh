4b869aaf5229738732bf2566b817546539406c00
ruqianl <rlyu@svi.edu.au>
2021-06-02 04:43:50 +0000

    update build

--- recipes/sscocaller/build.sh
+++ recipes/sscocaller/build.sh
@@ -4,18 +4,13 @@ export F77="${FC}"
 export CPP="${CXX}"
 export CC="${CC}"
 
-echo $F77
-echo $CPP
-echo $CC
-echo ${GFORTRAN}
-
 git clone https://github.com/SurajGupta/r-source
 cd r-source
 chmod 755 configure
-./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
+./configure 
 
 cd src/nmath/standalone/
-make CC="${CC}" CFLAGS="$CFLAGS" CXX={$CXX}  
+make   
 cp rmathlib/* $HOME/.nimble/lib/
 
 cd ../../../../
