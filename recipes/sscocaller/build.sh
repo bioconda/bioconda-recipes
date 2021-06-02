08dab188363481df42318e27565563c27f280d7b
ruqianl <rlyu@svi.edu.au>
2021-06-02 23:15:22 +0000

    add CPP env

--- recipes/sscocaller/build.sh
+++ recipes/sscocaller/build.sh
@@ -8,6 +8,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export CFLAGS="-I$PREFIX/include"
 export CPATH="${PREFIX}/include"
 export CC="${CC}"
+export CPP="${CXX}"
 
 git clone https://github.com/SurajGupta/r-source
 cd r-source
