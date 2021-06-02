f644b65e06b1f9efffb86364195e715f17c9e1e0
ruqianl <rlyu@svi.edu.au>
2021-06-02 03:26:19 +0000

    update build sh

--- recipes/sscocaller/build.sh
+++ recipes/sscocaller/build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+git clone https://github.com/SurajGupta/r-source
+cd r-source
+./configure
+cd src/nmath/standalone/
+make
+cp rmathlib/* $HOME/.nimble/lib/
+
+cd ../../../../
+rm r-source
 
 nimble --localdeps build -y --verbose -d:release
 mkdir -p "${PREFIX}/bin"
