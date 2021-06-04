b3a754594ac1ec2b958b6743d4aa9682d5615041
ruqianl <rlyu@svi.edu.au>
2021-06-04 06:29:33 +0000

    rm DYN from configure option

--- recipes/sscocaller/build.sh
+++ recipes/sscocaller/build.sh
@@ -23,7 +23,7 @@ git clone https://github.com/SurajGupta/r-source
 cd r-source
 chmod 775 configure
 sed -i.bak 's/exit(strncmp(ZLIB_VERSION, "1.2.5", 5) < 0);/exit(ZLIB_VERNUM < 0x1250);/g' configure
-./configure --with-readline=no --with-x=no --prefix=$PREFIX  CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" CXXFLAGS="$CXXFLAGS" DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH" 
+./configure --with-readline=no --with-x=no --prefix=$PREFIX  CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" CXXFLAGS="$CXXFLAGS" 
 cd src/nmath/standalone/
 make
 
