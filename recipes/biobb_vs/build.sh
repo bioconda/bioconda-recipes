323ff9fd4878b362db1200bfb97cc3f725cb5c84
Genís Bayarri <genis.bayarri@irbbarcelona.org>
2021-06-04 16:06:09 +0200

    [ci skip] Merge PR 28828
    Merge PR #28828, commits were: 
     * 2021.2

--- recipes/biobb_vs/build.sh
+++ recipes/biobb_vs/build.sh
@@ -4,8 +4,8 @@ python3 setup.py install --single-version-externally-managed --record=record.txt
 
 mkdir -p $PREFIX/bin
 
-chmod u+x $SP_DIR/biobb_vs/vina/autodock_vina.py
-cp $SP_DIR/biobb_vs/vina/autodock_vina.py $PREFIX/bin/autodock_vina
+chmod u+x $SP_DIR/biobb_vs/vina/autodock_vina_run.py
+cp $SP_DIR/biobb_vs/vina/autodock_vina_run.py $PREFIX/bin/autodock_vina_run
 
 chmod u+x $SP_DIR/biobb_vs/utils/bindingsite.py
 cp $SP_DIR/biobb_vs/utils/bindingsite.py $PREFIX/bin/bindingsite
@@ -19,8 +19,8 @@ cp $SP_DIR/biobb_vs/utils/box_residues.py $PREFIX/bin/box_residues
 chmod u+x $SP_DIR/biobb_vs/utils/extract_model_pdbqt.py
 cp $SP_DIR/biobb_vs/utils/extract_model_pdbqt.py $PREFIX/bin/extract_model_pdbqt
 
-chmod u+x $SP_DIR/biobb_vs/fpocket/fpocket.py
-cp $SP_DIR/biobb_vs/fpocket/fpocket.py $PREFIX/bin/fpocket
+chmod u+x $SP_DIR/biobb_vs/fpocket/fpocket_run.py
+cp $SP_DIR/biobb_vs/fpocket/fpocket_run.py $PREFIX/bin/fpocket_run
 
 chmod u+x $SP_DIR/biobb_vs/fpocket/fpocket_filter.py
 cp $SP_DIR/biobb_vs/fpocket/fpocket_filter.py $PREFIX/bin/fpocket_filter
