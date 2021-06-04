999afc1e7cbcd7def4c6fa0bacfaa088077f0613
ruqianl <rlyu@svi.edu.au>
2021-06-04 04:47:55 +0000

    add DYLD_LIBRARY_PATH

--- recipes/sscocaller/build.sh
+++ recipes/sscocaller/build.sh
@@ -14,6 +14,7 @@ export CXXFLAGS="-I$PREFIX/include"
 if [ `uname` == Darwin ] ; then
     CXXFLAGS="$CXXFLAGS -stdlib=libc++"
     LDFLAGS="$LDFLAGS -stdlib=libc++"
+    DYLD_LIBRARY_PATH=${PREFIX}/lib:${DYLD_LIBRARY_PATH}
 else ## linux
     CXXFLAGS="$CXXFLAGS"
 fi
