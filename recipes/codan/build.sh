43443c39d2d1e42c51e3992742332f2ac06e9832
pedronachtigall <32197975+pedronachtigall@users.noreply.github.com>
2021-06-07 10:05:19 -0300

    Update build.sh

--- recipes/codan/build.sh
+++ recipes/codan/build.sh
@@ -4,4 +4,8 @@ mkdir -p $PREFIX/bin
 chmod +x bin/*
 cp bin/* $PREFIX/bin
 
-
+if ["$uname" == "Darwin"]; then
+  unzip for_MacOS_users.zip
+  chmod +x for_MacOS_users/*
+  cp for_MacOS_users/tops-viterbi_decoding $PREFIX/bin
+fi
