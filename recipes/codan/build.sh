4696e9f4de0d7600e84500696ebb7083c11b0d6b
pedronachtigall <32197975+pedronachtigall@users.noreply.github.com>
2021-06-07 14:03:46 -0300

    Update build.sh

--- recipes/codan/build.sh
+++ recipes/codan/build.sh
@@ -10,15 +10,15 @@ if ["$uname" == "Darwin"]; then
   cp for_MacOS_users/tops-viterbi_decoding $PREFIX/bin
 fi
 
-# This will allow them to be run on environment activation.
-for CHANGE in "activate" "deactivate";
-do
+if [ -d "$PREFIX/lib/perl5/site_perl/5.22.0/Bio/" ]; then
+  # This will allow them to be run on environment activation.
+  for CHANGE in "activate" "deactivate";
+  do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
-done
-
-echo "#!/bin/sh" >> "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
-echo "export PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0/" >> "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
-
-echo "#!/bin/sh" >> "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
-echo "unset PERL5LIB" >> "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+    #cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+  done
+  echo "#!/bin/sh" > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+  echo "export PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0/" >> "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+  echo "#!/bin/sh" > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+  echo "unset PERL5LIB" >> "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
+fi
