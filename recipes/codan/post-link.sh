df597b0430f5a0cb7c710d09044fe5460653298e
pedronachtigall <32197975+pedronachtigall@users.noreply.github.com>
2021-06-07 13:26:00 -0300

    Delete post-link.sh

--- recipes/codan/post-link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d "$PREFIX/lib/perl5/site_perl/5.22.0/Bio/" ]; then
-  #conda env config vars set PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0/ -n $(basename $PREFIX)
-  echo "Warning: notice that conda installed Perl 5.26 but all libraries are installed in Perl 5.22 path." >> $PREFIX/.messages.txt
-  echo "Please, use the command below to ensure all libraries will properly work:" >> $PREFIX/.messages.txt
-  echo "conda env config vars set PERL5LIB=$PREFIX/lib/perl5/site_perl/5.22.0/ -n ENV_NAME" >> $PREFIX/.messages.txt
-  echo "    whereas ENV_NAME should be the name indicated in the -n option" >> $PREFIX/.messages.txt
-fi
