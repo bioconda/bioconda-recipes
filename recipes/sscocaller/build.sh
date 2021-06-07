d986babd36f2973c0e7296c25b8ada1f0545a119
ruqianl <rlyu@svi.edu.au>
2021-06-07 04:39:31 +0000

    chagne iconv includes and libs

--- recipes/sscocaller/build.sh
+++ recipes/sscocaller/build.sh
@@ -28,7 +28,7 @@ git clone https://github.com/SurajGupta/r-source
 cd r-source
 chmod 775 configure
 sed -i.bak 's/exit(strncmp(ZLIB_VERSION, "1.2.5", 5) < 0);/exit(ZLIB_VERNUM < 0x1250);/g' configure
-./configure --with-readline=no --with-x=no --prefix=$PREFIX  INCICONV=$INCLUDE_PATH LIBICONV=$LIBRARY_PATH CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" CXXFLAGS="$CXXFLAGS" 
+./configure --with-readline=no --with-x=no --prefix=$PREFIX  INCICONV=$CFLAGS LIBICONV=$LDFLAGS CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" CXXFLAGS="$CXXFLAGS" 
 cd src/nmath/standalone/
 make
 
