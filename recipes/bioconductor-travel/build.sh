183db8a6b4b70470a15129b9a8ee27fc2eecfea1
Devon Ryan <devon.ryan@genedata.com>
2021-06-07 13:03:17 +0200

    bulk update

--- recipes/bioconductor-travel/build.sh
+++ recipes/bioconductor-travel/build.sh
@@ -3,10 +3,10 @@ mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/
-echo -e "CC=$CC
+echo -e "CC=$CC -I$PREFIX/include/fuse3
 FC=$FC
-CXX=$CXX
-CXX98=$CXX
-CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
+CXX=$CXX -I$PREFIX/include/fuse3
+CXX98=$CXX -I$PREFIX/include/fuse3
+CXX11=$CXX -I$PREFIX/include/fuse3
+CXX14=$CXX -I$PREFIX/include/fuse3" > ~/.R/Makevars
 $R CMD INSTALL --build .
