f6acf857597587696ddd2ddb6ab6370e580b52af
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-06-07 23:25:54 +0200

    test comment (also for rebuild since I cannot reproduce the error)

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -2,6 +2,8 @@
 # TODO actually it would be better if we could adapt our MANIFEST.in
 # to not package the openms libs and dependencies again. Same for share.
 pushd build
+
+# TODO FindHDF5 seems to have problems with pre-initialized caches. I cannot reproduce on local bootstrapped docker-based mulled containers :( Everything is fine there.
 cmake -DPYOPENMS=ON .
 # Unfortunately if we would call make, it thinks that OpenMS lib is not built since the build folder
 # might have been copied, so it rebuilds. Skip this by using setup.py directly.
