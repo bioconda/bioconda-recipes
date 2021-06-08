7d903c729199b7a69881e7285e540dd40d7a057b
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-06-08 12:35:25 +0200

    i dont like circleci

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -7,7 +7,7 @@ pushd build
 cmake -DPYOPENMS=ON -DPY_NUM_MODULES=16 .
 # Unfortunately if we would call make, it thinks that OpenMS lib is not built since the build folder
 # might have been copied, so it rebuilds. Skip this by using setup.py directly.
-# TODO check that it actually uses the prebuilt libOpenMS from the prefix.
+# TODO check that it actually uses the prebuilt libOpenMS from the prefix
 #make -j${CPU_COUNT} pyopenms
 pushd pyOpenMS
 $PYTHON create_cpp_extension.py
