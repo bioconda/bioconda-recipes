d5d116f5b4b0dafe203f9381f2c472fea5bdfc42
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-06-08 15:58:27 +0200

    force single threaded although we only have one thread

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -11,6 +11,6 @@ cmake -DPYOPENMS=ON -DPY_NUM_MODULES=16 .
 #make -j${CPU_COUNT} pyopenms
 pushd pyOpenMS
 $PYTHON create_cpp_extension.py
-$PYTHON -m pip install . --ignore-installed --no-deps
+$PYTHON -m pip install . --ignore-installed --no-deps --single-threaded
 popd
 popd
