c983f0857a81a098b347502c928f57c95d82e3fd
Devon Ryan <devon.ryan@genedata.com>
2021-06-21 11:48:21 +0200

    debug

--- recipes/chromap/build.sh
+++ recipes/chromap/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # -msse4.1 needed for _mm_min_epi32
+env
 make cxx=$CXX cxxflags="-O3 -Wall -I$PREFIX/include -std=c++11 -msse4.1 -fopenmp" ldflags="-L$PREFIX/lib -lm -lz"
 mkdir -p $PREFIX/bin
 mv chromap $PREFIX/bin
