53db8ec9414beccacff7e35b4e745c7c2a88228a
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-06 15:14:32 +0200

    Update trye another hack.

--- recipes/srnamapper/build.sh
+++ recipes/srnamapper/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # zlib hack
-echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > Makefile
+echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > MakefileA
 sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
-
-make CC=$CC
+make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib"
 chmod +x srnaMapper
 mkdir -p ${PREFIX}/bin
 cp -f srnaMapper ${PREFIX}/bin
