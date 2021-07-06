5ce840e2700ffbd74f28e749e8ef1b22ca543012
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-06 19:38:17 +0200

    Update remove C++ compatibility.

--- recipes/srnamapper/build.sh
+++ recipes/srnamapper/build.sh
@@ -3,7 +3,7 @@
 # zlib hack
 echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > MakefileA
 sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
-make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib"
+make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
 chmod +x srnaMapper
 mkdir -p ${PREFIX}/bin
 cp -f srnaMapper ${PREFIX}/bin
