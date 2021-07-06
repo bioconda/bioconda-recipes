9446a9e4bbc161adc2a2f16e3c340170a4e6012d
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-06 13:45:35 +0200

    Update in order to compile with zlib.

--- recipes/srnamapper/build.sh
+++ recipes/srnamapper/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Use $(CC), not "gcc"
 sed -i '1d' Makefile
+# zlib hack
+echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > Makefile
 
 make
 chmod +x srnaMapper
