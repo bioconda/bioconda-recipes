bcb227ebe9278830f6405e557d8f8def4ba1e49e
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-06 14:35:28 +0200

    Update to correct -lz bug.

--- recipes/srnamapper/build.sh
+++ recipes/srnamapper/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # zlib hack
-echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > Makefile
-sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
 sed -i 's/-lz//' Makefile
+echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > Makefile
+sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
 
 make CC=$CC
 chmod +x srnaMapper
