57c94fe2bc975ea1c65519819f3028608b0d2940
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-07 15:15:13 +0200

    Update correct typo in build.sh

--- recipes/srnamapper/build.sh
+++ recipes/srnamapper/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # zlib hack
-echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include -Doff64_t=__off64_t\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > MakefileA
+echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include -Doff64_t=__off64_t\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > Makefile
 sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
 make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
 chmod +x srnaMapper
