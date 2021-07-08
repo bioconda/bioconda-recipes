af36741194dff1801b1527df948a7fea0cdbc1be
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-08 10:13:52 +0200

    Update to 1.0.3 release.

--- recipes/srnamapper/build.sh
+++ recipes/srnamapper/build.sh
@@ -2,7 +2,6 @@
 
 
 # zlib hack
-echo -e "export CFLAGS=\"$CFLAGS -I$PREFIX/include -Doff64_t=__off64_t\"\nexport LDFLAGS=\"$LDFLAGS -L$PREFIX/lib -lz\"\nexport CPATH=${PREFIX}/include\n$(cat Makefile)" > Makefile
 sed -i 's/CFLAGS =/CFLAGS +=/' Makefile
 make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
 chmod +x srnaMapper
