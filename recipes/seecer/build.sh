098e38002415545dba078bbf148a0cd57697b654
ddurai <dilipdurai@yahoo.co.in>
2021-07-13 10:38:13 +0200

    updating seecer

--- recipes/seecer/build.sh
+++ recipes/seecer/build.sh
@@ -1,5 +1,6 @@
+tar -zxvf "seecer.tar.gz"
 mkdir -p "${PREFIX}/bin"
-ln -s SEECER/bin/run_seecer.sh "${PREFIX}/bin/run_seecer.sh"
+ln -s "seecer/SEECER/bin/run_seecer.sh" "${PREFIX}/bin/run_seecer.sh"
 echo "${PREFIX}/bin"
 #mkdir -p "$PREFIX/bin"
 
