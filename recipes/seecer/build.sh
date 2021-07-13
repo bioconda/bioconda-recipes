a65b3027aac1b94d7234e5b5a2d7ea4494cedfeb
ddurai <dilipdurai@yahoo.co.in>
2021-07-13 07:43:43 +0200

    updating seecer

--- recipes/seecer/build.sh
+++ recipes/seecer/build.sh
@@ -1,4 +1,5 @@
-#ln -s SEECER/bin/run_seecer.sh run_seecer.sh
+mkdir -p "${PREFIX}/bin"
+ln -s SEECER/bin/run_seecer.sh "${PREFIX}/bin/run_seecer.sh"
 #mkdir -p "$PREFIX/bin"
 
 #ln -s build/bin/ORNA "$PREFIX/bin"
