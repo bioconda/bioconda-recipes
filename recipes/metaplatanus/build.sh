d1338fa42f544bd05f9a146e416672e7b38a0db9
rkajitani <rei.kajitani@gmail.com>
2021-07-15 20:12:20 +0900

    init metaplatanus (v1.3.0)

---
+++ recipes/metaplatanus/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir -p $PREFIX/bin/sub_bin
+
+make CXX=${CXX}
+cp metaplatanus $PREFIX/bin
+cp src/scripts/tgsgapcloser_mod $PREFIX/bin
+cp -r sub_bin/* $PREFIX/bin/sub_bin
+
+wget https://github.com/rkajitani/NextPolish/releases/download/v1.3.1/NextPolish_v1.3.1.tar.gz
+tar xf NextPolish_v1.3.1.tar.gz
+cd NextPolish_v1.3.1
+alias gcc=${CC}
+alias g++=${CXX}
+make
+cp -r nextPolish lib bin $PREFIX/bin/sub_bin
+cd ..
