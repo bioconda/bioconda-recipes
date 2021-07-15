5323b56311128b9bab933f7baac60085c11dca92
rkajitani <rei.kajitani@gmail.com>
2021-07-15 22:07:59 +0900

    add zlib for nextPolish build

--- recipes/metaplatanus/build.sh
+++ recipes/metaplatanus/build.sh
@@ -10,9 +10,8 @@ cp -r sub_bin/* $PREFIX/bin/sub_bin
 wget https://github.com/rkajitani/NextPolish/releases/download/v1.3.1/NextPolish_v1.3.1.tar.gz
 tar xf NextPolish_v1.3.1.tar.gz
 cd NextPolish_v1.3.1
-alias gcc=${CC}
 ln -s `which ${CC}` `dirname \`which ${CC}\``/gcc
-ln -s `which ${CXX}` `dirname \`which ${CC}\``/g++
+ln -s `which ${CXX}` `dirname \`which ${CXX}\``/g++
 make
 cp -r nextPolish lib bin $PREFIX/bin/sub_bin
 cd ..
