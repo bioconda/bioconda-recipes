d3dda1cf1316f77c947a7884a0a97a8ef441ad0d
rkajitani <rei.kajitani@gmail.com>
2021-07-16 03:05:35 +0900

    simplify nextPolish

--- recipes/metaplatanus/build.sh
+++ recipes/metaplatanus/build.sh
@@ -7,15 +7,14 @@ cp metaplatanus $PREFIX/bin
 cp src/scripts/tgsgapcloser_mod $PREFIX/bin
 cp -r sub_bin/* $PREFIX/bin/sub_bin
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 wget https://github.com/rkajitani/NextPolish/releases/download/v1.3.1/NextPolish_v1.3.1.tar.gz
 tar xf NextPolish_v1.3.1.tar.gz
 cd NextPolish_v1.3.1
 ln -s `which ${CC}` `dirname \`which ${CC}\``/gcc
 ln -s `which ${CXX}` `dirname \`which ${CXX}\``/g++
 make
+ln -s $PREFIX/bin/samtools bin/samtools
+ln -s $PREFIX/bin/bwa bin/bwa
+ln -s $PREFIX/bin/minimap2 bin/minimaps
 cp -r nextPolish lib bin $PREFIX/bin/sub_bin
 cd ..
