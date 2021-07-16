5921ba72b7ca569c45b2b2836e76a3d162d48c70
Dave Bouvier <dave@bx.psu.edu>
2021-07-16 09:19:39 -0400

    Symlink bealign references to $PREFIX/usr/share/local

---
+++ recipes/python-bioext/activate.sh
@@ -0,0 +1,2 @@
+export
+export BEALIGN_REFERENCE_DIR=usr/local/share/bealign_references
--- recipes/python-bioext/build.sh
+++ recipes/python-bioext/build.sh
@@ -9,3 +9,15 @@ fi
 export FREETYPE2_ROOT=$PREFIX
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
+
+# Symlink reference data to /usr/local/share, since the site-packages path may vary depending on python version
+mkdir -p "${PREFIX}/usr/local/share/bealign_references"
+ln -s "$SP_DIR/BioExt/data/references/hxb2" "$PREFIX/usr/local/share/bealign_references/HXB2"
+ln -s "$SP_DIR/BioExt/data/references/cov2" "$PREFIX/usr/local/share/bealign_references/CoV2"
+ln -s "$SP_DIR/BioExt/data/references/nl4-3" "$PREFIX/usr/local/share/bealign_references/NL4-3"
