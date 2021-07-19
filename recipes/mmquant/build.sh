da85cf3ad740dfc43382d0d192e654910bc75876
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2021-07-19 12:03:15 +0200

    Update to 1.0.3.

--- recipes/mmquant/build.sh
+++ recipes/mmquant/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # zlib hack
-make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-L$PREFIX/lib"
+make CXX=$CXX INCLUDES="-I$PREFIX/include" CFLAGS+="-L$PREFIX/lib"
 chmod +x mmquant
 mkdir -p ${PREFIX}/bin
 cp -f mmquant ${PREFIX}/bin
