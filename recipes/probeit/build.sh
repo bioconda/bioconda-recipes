6684ca3a12c292bda1dd9650b6e649eef21c4ac2
Woosub-Kim <woosubgo@gmail.com>
2021-07-19 12:05:52 +0900

    sample data

--- recipes/probeit/build.sh
+++ recipes/probeit/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 install -d "${PREFIX}/bin"
 install probeit.py "${PREFIX}/bin/"
+
+install -d "${PREFIX}/bin/sample"
+install sample/positive.fasta "${PREFIX}/bin/sample"
+install sample/negative.fasta "${PREFIX}/bin/sample"
+install sample/ref.fasta "${PREFIX}/bin/sample"
+install sample/ref.gff "${PREFIX}/bin/sample"
+install sample/str.fasta "${PREFIX}/bin/sample"
+
 cd setcover
 make CXXFLAGS="${CXXFLAGS} -I. -O3 -std=c++14" -j $CPU_COUNT
 install setcover "${PREFIX}/bin/"
