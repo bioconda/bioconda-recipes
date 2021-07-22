5be9d7e7580d0316caf9fca2325b594c911ff284
Dave B <dave@bx.psu.edu>
2021-07-22 18:39:54 -0400

    Update recipes/python-bioext/build.sh
    Co-authored-by: Nicola Soranzo <nicola.soranzo@gmail.com>

--- recipes/python-bioext/build.sh
+++ recipes/python-bioext/build.sh
@@ -14,7 +14,7 @@ mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/etc/conda/deactivate.d"
 cp ${RECIPE_DIR}/activate.sh "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
 cp ${RECIPE_DIR}/deactivate.sh "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
 
-export TARGET=$PREFIX/usr/share/$PKG_NAME/reference_data
+TARGET=$PREFIX/share/$PKG_NAME/reference_data
 
 # Symlink reference data to /usr/local/share/$TARGET, since the site-packages
 # path may vary depending on the python version
