14c8ece071987cee029cba9cae081a0390992b83
zhengzhenxian <zxzheng@cs.hku.hk>
2021-07-24 15:30:32 +0800

    add g++

--- recipes/clair3-illumina/build.sh
+++ recipes/clair3-illumina/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -pv $PREFIX/bin
-cp -rv preprocess $PREFIX/bin
+mkdir -pv $PREFIX/bin/preprocess
+cp -rv preprocess/realign $PREFIX/bin/preprocess
 cd $PREFIX/bin/preprocess/realign
 $CXX -std=c++14 -O1 -shared -fPIC -o realigner ssw_cpp.cpp ssw.c realigner.cpp
 $CXX -std=c++11 -shared -fPIC -o debruijn_graph -O3 debruijn_graph.cpp -I $PREFIX/include -L $PREFIX/lib
