785bc484dc151ab782cefe5da3193811de3f7edc
Xiao <vincent@cwi.nl>
2021-07-24 19:47:18 +0200

    update build.sh

--- recipes/vechat/build.sh
+++ recipes/vechat/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# mkdir -p $PREFIX/bin
+mkdir -p ${PREFIX}/bin
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX}  -Dspoa_optimize_for_portability=ON ..
 make
 make install
-ln -fs $PWD/scripts/vechat ${PREFIX}/bin/vechat
+ln -fs ${PWD}/../scripts/vechat ${PREFIX}/bin/vechat
 
