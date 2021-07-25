e924be904b46763bbb9a683f5638415735fd3453
Acribbs <adam.cribbs@ndorms.ox.ac.uk>
2021-07-25 18:51:59 +0100

    try pip install

--- recipes/cgat-apps/build.sh
+++ recipes/cgat-apps/build.sh
@@ -25,4 +25,4 @@ sed -i'' -e '/REPO_REQUIREMENT/,/pass/d' setup.py
 sed -i'' -e '/# dependencies/,/dependency_links=dependency_links,/d' setup.py
 
 # https://bioconda.github.io/linting.html#setup-py-install-args
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . -vv
