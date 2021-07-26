02df1d380b6897dd2ec5b1dacb9c6c1204723b1f
Adam Cribbs <a.p.cribbs@gmail.com>
2021-07-26 07:03:44 +0100

    Update build.sh

--- recipes/cgat-apps/build.sh
+++ recipes/cgat-apps/build.sh
@@ -25,4 +25,4 @@ sed -i'' -e '/REPO_REQUIREMENT/,/pass/d' setup.py
 sed -i'' -e '/# dependencies/,/dependency_links=dependency_links,/d' setup.py
 
 # https://bioconda.github.io/linting.html#setup-py-install-args
-$PYTHON -m pip install . -vv --no-deps --ignore-installed
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
