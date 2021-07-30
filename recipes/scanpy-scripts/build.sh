4627571fc11ec6548701728cb9004ba45c195a75
Jonathan Manning <jmanning@ebi.ac.uk>
2021-07-30 16:28:15 +0100

    replace wget

--- recipes/scanpy-scripts/build.sh
+++ recipes/scanpy-scripts/build.sh
@@ -2,7 +2,7 @@ python -m pip install . --no-deps --ignore-installed -vv
 wget https://github.com/theislab/scanpy/archive/refs/tags/1.8.1.tar.gz
 tar -xvzf 1.8.1.tar.gz
 cd scanpy-1.8.1
-wget https://raw.githubusercontent.com/ebi-gene-expression-group/scanpy-scripts/develop/scrublet.patch
+curl https://raw.githubusercontent.com/ebi-gene-expression-group/scanpy-scripts/develop/scrublet.patch > scrublet.patch
 patch -p1 < scrublet.patch
 python -m pip install . --no-deps --ignore-installed -vv
 
