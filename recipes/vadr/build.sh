06d8c181b8c7a8ec87999e37dc863efca29c3bf6
Robert A. Petit III <robbie.petit@gmail.com>
2021-07-30 23:58:13 +0000

    add recipe for vadr

---
+++ recipes/vadr/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Add scripts
+mkdir -p ${PREFIX}/bin
+cp *.pl ${PREFIX}/bin
+cp miniscripts/*.pl ${PREFIX}/bin
+chmod 755 ${PREFIX}/bin/*.pl
+
+# Setup shared directory
+SHARE_DIR="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+VADR_DIR="${SHARE_DIR}/vadr"
+VADR_MODEL_DIR="${SHARE_DIR}/vadr-models"
+mkdir -p ${VADR_DIR} ${VADR_MODEL_DIR}
+cp -r ./ ${VADR_DIR}/
+touch ${SHARE_DIR}/vadr-models/vadr-models-placeholder.txt
+
+# Setup the VADR env variables
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+echo "export VADRINSTALLDIR=${PREFIX}" > ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRSCRIPTSDIR=${VADR_DIR}" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRMODELDIR=${VADR_MODEL_DIR}" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRINFERNALDIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADREASELDIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRHMMERDIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRBIOEASELDIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRSEQUIPDIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRBLASTDIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export VADRFASTADIR=${PREFIX}/bin" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+echo "export PERL5LIB=${VADR_DIR}:${PERL5LIB}" >> ${PREFIX}/etc/conda/activate.d/vadr.sh
+chmod a+x ${PREFIX}/etc/conda/activate.d/vadr.sh
+
+# Unset them
+echo "unset VADRINSTALLDIR" > ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRSCRIPTSDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRMODELDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRINFERNALDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADREASELDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRHMMERDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRBIOEASELDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRSEQUIPDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRBLASTDIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset VADRFASTADIR" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+echo "unset PERL5LIB" >> ${PREFIX}/etc/conda/deactivate.d/vadr.sh
+chmod a+x ${PREFIX}/etc/conda/deactivate.d/vadr.sh
