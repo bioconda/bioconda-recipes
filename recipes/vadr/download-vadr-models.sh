c873fb4d5633082dba26d21e9471ffd961d5cf59
Robert A. Petit III <robbie.petit@gmail.com>
2021-07-31 03:43:47 +0000

    improve download script

--- recipes/vadr/download-vadr-models.sh
+++ recipes/vadr/download-vadr-models.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
-MODELNAME=$1
-
+set -e
+set -u
 
 # If no model given, print help message
-if [[ -z ${MODELNAME+x} ]]; then
+if [ $# -eq 0 ]; then
     echo "usage: download-vadr-models.sh MODEL_NAME"
     echo ""
-    echo "MODEL_NAME can be one of the following: sarscov2, caliciviridae, coronaviridae, cox1, flaviviridae"
+    echo "MODEL_NAME can be one of the following: sarscov2 caliciviridae coronaviridae cox1 flaviviridae"
     echo ""
     echo "Example: download-vadr-models.sh sarscov2"
     echo ""
@@ -21,8 +21,18 @@ MVERSION=$(echo $VADRVERSION | sed -r 's/([0-9]).([0-9]).([0-9])/\1.\2-\3/')
 CURRENT_DIR=$(pwd)
 mkdir -p ${VADRMODELDIR}/tmp
 cd $VADRMODELDIR/tmp
-wget -O vadr-models-${MODELNAME}.tar.gz https://ftp.ncbi.nlm.nih.gov/pub/nawrocki/vadr-models/${MODELNAME}/${MVERSION}/vadr-models-${MODELNAME}-${MVERSION}.tar.gz
-tar -xfz vadr-models-${MODELNAME}.tar.gz
-mv vadr-models-$v-$MVERSION/* ${VADRMODELDIR}/
-rm -rf $VADRMODELDIR/tmp
-cd $(CURRENT_DIR)
+for model in $*; do
+    echo "Downloaded VADR model for ${model} to ${VADRMODELDIR}"
+    clean_model=${model}
+    if [[ "${model}" == *"viridae"* ]]; then
+        clean_model=${model%viridae}
+    fi
+
+    wget -O vadr-models-${model}.tar.gz https://ftp.ncbi.nlm.nih.gov/pub/nawrocki/vadr-models/${model}/${MVERSION}/vadr-models-${clean_model}-${MVERSION}.tar.gz
+    tar -xzf vadr-models-${model}.tar.gz
+    mv vadr-models-${clean_model}-$MVERSION/* ${VADRMODELDIR}/
+    rm -rf $VADRMODELDIR/tmp/*
+    echo "VADR model for ${model} setup"
+done
+rm -rf $VADRMODELDIR/tmp/
+cd ${CURRENT_DIR}
