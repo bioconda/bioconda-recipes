ff6b3d70593577169a7e26cdc0826c639d33244f
Edward (Henry's) <wwlui@bal31.cs.hku.hk>
2021-08-02 18:11:28 +0800

    place in dir MegaPath-Nano

--- recipes/megapath-nano/build.sh
+++ recipes/megapath-nano/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 mkdir -pv $PREFIX/bin
-cp -rv bin/* db_preparation docs Dockerfile README.md LICENSE $PREFIX/bin
+mkdir -pv $PREFIX/MegaPath-Nano
+cp -rv bin db_preparation docs Dockerfile README.md LICENSE $PREFIX/MegaPath-Nano
+ln -s $PREFIX/MegaPath-Nano/megapath_nano.py $PREFIX/bin
+ln -s $PREFIX/MegaPath-Nano/megapath_nano_amr.py $PREFIX/bin
