48e77758cb5dcca96bde5bbbb74de6eff1a98fcc
Edward (Henry's) <wwlui@bal31.cs.hku.hk>
2021-08-02 18:14:52 +0800

    update path

--- recipes/megapath-nano/build.sh
+++ recipes/megapath-nano/build.sh
@@ -3,5 +3,5 @@
 mkdir -pv $PREFIX/bin
 mkdir -pv $PREFIX/MegaPath-Nano
 cp -rv bin db_preparation docs Dockerfile README.md LICENSE $PREFIX/MegaPath-Nano
-ln -s $PREFIX/MegaPath-Nano/megapath_nano.py $PREFIX/bin
-ln -s $PREFIX/MegaPath-Nano/megapath_nano_amr.py $PREFIX/bin
+ln -s $PREFIX/MegaPath-Nano/bin/megapath_nano.py $PREFIX/bin
+ln -s $PREFIX/MegaPath-Nano/bin/megapath_nano_amr.py $PREFIX/bin
