5bc87e11d4c589312af9f82342d9ea769d41b1ca
Edward (Henry's) <wwlui@bal31.cs.hku.hk>
2021-08-03 14:38:30 +0800

    move amrfinder db download from build.sh to test command

--- recipes/megapath-nano/build.sh
+++ recipes/megapath-nano/build.sh
@@ -5,5 +5,3 @@ mkdir -pv $PREFIX/MegaPath-Nano
 cp -rv bin db_preparation docs Dockerfile README.md LICENSE $PREFIX/MegaPath-Nano
 ln -s $PREFIX/MegaPath-Nano/bin/megapath_nano.py $PREFIX/bin
 ln -s $PREFIX/MegaPath-Nano/bin/megapath_nano_amr.py $PREFIX/bin
-#  build amr tools and db
-amrfinder -u
