9bc5d6f551f5488284a137f15c0dd7f66e94fa2a
Nils Homer <nilshomer@gmail.com>
2021-08-04 17:57:18 -0600

    update CXXFLAG and LDFLAG

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -5,7 +5,8 @@ make \
 	-j 4 \
 	CC="$CC -std=c++11" \
 	DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
-	CXXFLAG=${PREFIX}/lib \
+	CXXFLAG="$CXXFLAGS ${PREFIX}/lib" \
+	LDFLAG="$LDFLAGS" \
 	HTSLIB_INC=${PREFIX}/lib \
 	HTSLIB_LIB=${PREFIX}/lib/libhts.a \
 	BOOST_LIB_IO=${PREFIX}/lib/libboost_iostreams.a \
