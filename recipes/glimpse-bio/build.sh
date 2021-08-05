b42c3b5901819d99b1702f603fdc2b4f941eb5c7
Nils Homer <nilshomer@gmail.com>
2021-08-05 08:12:49 -0600

    try try again

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -9,12 +9,12 @@ do
         -j 4 \
         DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
         CXX="$CXX -std=c++11" \
-        CXXFLAG="$CXXFLAGS ${PREFIX}/lib" \
+        CXXFLAG="$CXXFLAGS ${PREFIX}" \
         LDFLAG="$LDFLAGS" \
-        HTSLIB_INC=${PREFIX}/lib \
-        HTSLIB_LIB=${PREFIX}/lib/libhts.a \
-        BOOST_LIB_IO=${PREFIX}/lib/libboost_iostreams.a \
-        BOOST_LIB_PO=${PREFIX}/lib/libboost_program_options.a \
+        HTSLIB_INC="$PREFIX" \
+        HTSLIB_LIB="-lhts" \
+        BOOST_LIB_IO="-lboost_iostreams" \
+        BOOST_LIB_PO="-lboost_program_options" \
     ;
     # Install the binaries
     install bin/GLIMPSE_${subdir}
