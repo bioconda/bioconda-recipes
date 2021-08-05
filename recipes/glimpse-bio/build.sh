ae48abf6326fe2ad94309da2beb57fe462219a3e
Nils Homer <nilshomer@gmail.com>
2021-08-04 18:13:43 -0600

    build in subdirs directly

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Build the binaries
-make \
-	-j 4 \
-	CC="$CC -std=c++11" \
-	DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
-	CXXFLAG="$CXXFLAGS ${PREFIX}/lib" \
-	LDFLAG="$LDFLAGS" \
-	HTSLIB_INC=${PREFIX}/lib \
-	HTSLIB_LIB=${PREFIX}/lib/libhts.a \
-	BOOST_LIB_IO=${PREFIX}/lib/libboost_iostreams.a \
-	BOOST_LIB_PO=${PREFIX}/lib/libboost_program_options.a \
-;
 
-# Install the binaries
-install ./phase/bin/GLIMPSE_phase
-install ./ligate/bin/GLIMPSE_ligate
-install ./chunk/bin/GLIMPSE_chunk
-install ./sample/bin/GLIMPSE_sample
-install ./snparray/bin/GLIMPSE_snparray
-install ./concordance/bin/GLIMPSE_concordance
+for subdir in chunk concordance ligate phase sample snparray
+do
+    pushd $subdir
+    # Build the binaries
+    make \
+        -j 4 \
+        CC="$CC -std=c++11" \
+        DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
+        CXXFLAG="$CXXFLAGS ${PREFIX}/lib" \
+        LDFLAG="$LDFLAGS" \
+        HTSLIB_INC=${PREFIX}/lib \
+        HTSLIB_LIB=${PREFIX}/lib/libhts.a \
+        BOOST_LIB_IO=${PREFIX}/lib/libboost_iostreams.a \
+        BOOST_LIB_PO=${PREFIX}/lib/libboost_program_options.a \
+    ;
+    # Install the binaries
+    install bin/GLIMPSE_${subdir}
+    popd
+done
