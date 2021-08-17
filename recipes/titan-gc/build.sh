70f67c645fee3d6a2615e39c3abbeb2b3e3823c9
Robert A. Petit III <robbie.petit@gmail.com>
2021-08-17 09:55:54 -0600

    Update build.sh

--- recipes/titan-gc/build.sh
+++ recipes/titan-gc/build.sh
@@ -6,3 +6,12 @@ chmod 755 bin/*
 cp -f bin/* $PREFIX/bin/
 
 mv conf/ tasks/ workflows/ ${TITAN}
+
+# Setup the VADR env variables
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+echo "export TITAN_GC_VERSION=${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d//titan-gc.sh
+chmod a+x ${PREFIX}/etc/conda/activate.d/titan-gc.sh
+
+# Unset them
+echo "unset TITAN_GC_VERSION" > ${PREFIX}/etc/conda/deactivate.d//titan-gc.sh
+chmod a+x ${PREFIX}/etc/conda/deactivate.d//titan-gc.sh
