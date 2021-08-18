e88136dce400d55003e3e5a6abf396e5e0906b30
Abhinav Sharma <abhi18av@gmail.com>
2021-08-18 19:02:34 +0200

    initial draft

---
+++ recipes/rd-analyzer/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+RD_ANALYZER_DIR=${PREFIX}/share/RD-Analyzer
+mkdir -p $RD_ANALYZER_DIR
+cp -r RD-Analyzer/* $RD_ANALYZER_DIR
+
+MAIN_CMD=${PREFIX}/bin/RD-Analyzer.py
+echo '#!/usr/bin/env python' >$MAIN_CMD
+cat RD-Analyzer.py >> $MAIN_CMD
+chmod a+x $MAIN_CMD
