c40265e3be757df14c560d9d0fdff147ca81be59
Woosub-Kim <woosubgo@gmail.com>
2021-08-18 14:28:40 +0900

    probeit v1.4

--- recipes/probeit/build.sh
+++ recipes/probeit/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 install -d "${PREFIX}/bin"
 install probeit.py "${PREFIX}/bin/"
+# setcover
 cd setcover
 make CXXFLAGS="${CXXFLAGS} -I. -O3 -std=c++14" -j $CPU_COUNT
 install setcover "${PREFIX}/bin/"
