054e16f3ca386eee57e17c5e092498d20dd067bc
Edward (Henry's) <rewq142857@hotmail.com>
2021-08-21 01:05:08 +0800

    mp 1

---
+++ recipes/megapath/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -pv $PREFIX/bin
+mkdir -pv $PREFIX/MegaPath
+cp -rv * $PREFIX/MegaPath
+ln -s $PREFIX/MegaPath/bin/runMegaPath.sh $PREFIX/bin
+
+make -C $PREFIX/MegaPath/megahit/
+make -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
+make -C $PREFIX/MegaPath/soap4/
