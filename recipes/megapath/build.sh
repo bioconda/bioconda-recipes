0e17f0e5a048ea56c345c1d73f8f1aa7223ea1b6
Edward (Henry's) <rewq142857@hotmail.com>
2021-08-21 14:38:55 +0800

    add build make

--- recipes/megapath/build.sh
+++ recipes/megapath/build.sh
@@ -5,6 +5,6 @@ mkdir -pv $PREFIX/MegaPath
 cp -rv ac-diamond-0.1-beta-linux64 bbmap calcAccuracy.pl cc cleanup.pl extractFromLSAM.pl extractViralAndUnmap.pl fastq2fasta.pl LICENSE m8_to_lsam.pl m8_to_mapLen_hist.cpp maskLowerWithN.pl megahit merge_dpout.sh r2c_to_r2g.pl README_LSAM.md README.md reassign.pl runBBduk.pl runMegaPath.sh runSOAP3dp.pl sam2cfq.pl soap4 sortedBam2Snapshot splitFasta.pl $PREFIX/MegaPath
 ln -s $PREFIX/MegaPath/runMegaPath.sh $PREFIX/bin
 
-make CC=${CC} -C $PREFIX/MegaPath/megahit/ 
-make CC=${CC} -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
-make CC=${CC} -C $PREFIX/MegaPath/soap4/ 
+make -C $PREFIX/MegaPath/megahit/ 
+make -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
+make -C $PREFIX/MegaPath/soap4/ 
