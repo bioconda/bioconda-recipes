e3e4528e926ef58cae95925bba6d366c1870564f
Edward (Henry's) <rewq142857@hotmail.com>
2021-08-21 23:16:56 +0800

    add CC=$CXX

--- recipes/megapath/build.sh
+++ recipes/megapath/build.sh
@@ -7,5 +7,5 @@ ln -s $PREFIX/MegaPath/runMegaPath.sh $PREFIX/bin
 
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-make CC=${CC} CXX=${CXX} -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
-make CC=${CC} CXX=${CXX} -C $PREFIX/MegaPath/soap4/ 
+make CC=$CXX CXX=${CXX} -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
+make CC=$CXX CXX=${CXX} -C $PREFIX/MegaPath/soap4/ 
