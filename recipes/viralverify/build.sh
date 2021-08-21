3095bdc106c0f8b5ec2c333b5da3b288bef7d881
Mike <mike.rayko@gmail.com>
2021-08-21 14:32:24 +0300

    build fix

--- recipes/viralverify/build.sh
+++ recipes/viralverify/build.sh
@@ -4,10 +4,8 @@ set -e
 echo "Building viralverify..."
 
 cp -r $SRC_DIR/* $PREFIX/
-#mkdir $PREFIX/bin
-cd $PREFIX/
-#ln -s ../viralverify.py viralverify
-#ln -s ../training_script.py viralverify_training
 
-chmod +x viralverify.py
-chmod +x training_script.py
+cd $PREFIX/bin
+
+chmod +x viralverify
+chmod +x training_script
