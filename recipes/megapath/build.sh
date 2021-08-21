a86ca52d23d7828ee093b54f9204b24a626f0104
Edward (Henry's) <rewq142857@hotmail.com>
2021-08-21 22:24:34 +0800

    all compile g++

--- recipes/megapath/build.sh
+++ recipes/megapath/build.sh
@@ -7,5 +7,5 @@ ln -s $PREFIX/MegaPath/runMegaPath.sh $PREFIX/bin
 
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-make CC=${CC} -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
+make CXX=${CXX} -C $PREFIX/MegaPath/soap4/2bwt-lib/ 
 make CXX=${CXX} -C $PREFIX/MegaPath/soap4/ 
