b546f492e3459d02ea80541e458133d8d0b5c7cf
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2021-08-23 14:16:33 +0200

    inject compiler, xref #19479

--- recipes/dragmap/build.sh
+++ recipes/dragmap/build.sh
@@ -1,8 +1,10 @@
 export HAS_GTEST=0
 
 # fix build number in config
-
 sed -i.bak 's/VERSION_STRING.*/VERSION_STRING="${PKG_VERSION}"/' config.mk
+
+# set compiler
+sed -i.bak 's/CXX=g++/CXX=$CXX/' config.mk
 
 make
 make install
