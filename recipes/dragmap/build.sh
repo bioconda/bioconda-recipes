15fb86a4e6933de49dbeb8f979581fd216133e17
Matthias De Smet <11850640+matthdsm@users.noreply.github.com>
2021-08-23 15:42:56 +0200

    fix compiler

--- recipes/dragmap/build.sh
+++ recipes/dragmap/build.sh
@@ -3,10 +3,11 @@ export HAS_GTEST=0
 # fix build number in config
 sed -i.bak 's/VERSION_STRING.*/VERSION_STRING="${PKG_VERSION}"/' config.mk
 
-# set compiler
+# unset compilers
 sed -i.bak 's/CXX=g++//' config.mk
+sed -i.bak 's/CC=gcc//' config.mk
 
-make CXX=$CXX
+make CXX=$CXX CC=$CC
 make install
 mkdir -p "${PREFIX}/bin"
 mv build/release/dragen-os ${PREFIX}/bin/
