ec310513ee87e2a65a3b93bf90267b46682239a3
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-08-24 16:57:55 +1000

    [ci skip] Merge PR 30224
    Merge PR #30224, commits were: 
     * update recepe build number
     * separately build slow5lib
     * updated to f5c 0.7

--- recipes/f5c/0.6/build.sh
+++ recipes/f5c/0.6/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 scripts/install-hts.sh
 ./configure
-cd slow5lib
-make CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -D__STDC_FORMAT_MACROS"
-cd ..
 make CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -std=c++11 -D__STDC_FORMAT_MACROS"
 mkdir -p $PREFIX/bin
 cp f5c $PREFIX/bin/f5c
