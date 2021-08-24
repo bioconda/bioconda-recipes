e378b7c34aeaff4278c178b46025bb0e6273e499
pirovc <4673375+pirovc@users.noreply.github.com>
2021-08-24 21:17:49 +0200

    [ci skip] Merge PR 30227
    Merge PR #30227, commits were: 
     * bzip2 dep.
     * disable online tests
     * fix path
     * specific seqan3 source version
     * add dep. seqan3 for submodules
     * fix move seqan build.sh
     * minimal cmake to 3.14
     * ganon version 1.0.0

--- recipes/ganon/build.sh
+++ recipes/ganon/build.sh
@@ -5,11 +5,10 @@ cd ${SRC_DIR}/ganon/
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
 # Build and install cpp packages
-# Copy libs downloaded from source to their correct location
-mv ${SRC_DIR}/seqan ${SRC_DIR}/ganon/libs/
-mv ${SRC_DIR}/sdsl-lite ${SRC_DIR}/ganon/libs/
+# Seqan3.1.0-rc1 from source since still not available on bioconda
+mv ${SRC_DIR}/seqan3 ${SRC_DIR}/ganon/libs/
 mkdir build_cpp && cd build_cpp
-cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DGANON_OFFSET=ON -DINCLUDE_DIRS=${PREFIX}/include -DCONDA=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DINCLUDE_DIRS=${PREFIX}/include -DCONDA=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
 make install
 
@@ -20,4 +19,4 @@ ctest -VV .
 cd ${SRC_DIR}/ganon/
 $PYTHON -m unittest discover -s tests/ganon/unit/ -v
 $PYTHON -m unittest discover -s tests/ganon/integration/ -v
-$PYTHON -m unittest discover -s tests/ganon/integration_online/ -v
+#$PYTHON -m unittest discover -s tests/ganon/integration_online/ -v
