83f778c15a63dc73e71af339e526e6ae5e2ff4cc
James Fellows Yates <jfy133@gmail.com>
2021-08-26 13:12:39 +0200

    remove lib moving commands for debugging

--- recipes/dashing/build.sh
+++ recipes/dashing/build.sh
@@ -5,12 +5,12 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-pushd bonsai
+#pushd bonsai
 git clone --recursive --single-branch --branch dev https://github.com/facebook/zstd
-pushd zstd
+#pushd zstd
 make CC=$CC lib && mv lib/libzstd.a ..
-popd
-popd
+#popd
+#popd
 
 sed -i.bak "s/ -lzstd//g" Makefile
 make CC=$CC CXX=$CXX
