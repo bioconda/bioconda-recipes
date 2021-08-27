46d0a59749ebc77e3e11337508f87be2cde0ad45
Abhinav Sharma <abhi18av@gmail.com>
2021-08-28 01:22:03 +0530

    manually update the diff file

--- recipes/rd-analyzer/build.sh
+++ recipes/rd-analyzer/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-RD_ANALYZER_DIR=${PREFIX}/share/RD-Analyzer
-mkdir -p $RD_ANALYZER_DIR
-cp RD-Analyzer* $RD_ANALYZER_DIR
-
 MAIN_CMD=${PREFIX}/bin/RD-Analyzer.py
 echo '#!/usr/bin/env python' >$MAIN_CMD
 cat RD-Analyzer.py >> $MAIN_CMD
