9f7348d4954adafbcca2cf660286dfa7d55bcddc
Abhinav Sharma <abhi18av@gmail.com>
2021-08-28 02:36:28 +0530

    update the build.sh

--- recipes/rd-analyzer/build.sh
+++ recipes/rd-analyzer/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-RD_ANALYZER_DIR=${PREFIX}
+RD_ANALYZER_DIR=${PREFIX}/share/RD-Analyzer
+mkdir -p $RD_ANALYZER_DIR
+cp -r *py $RD_ANALYZER_DIR
+
+mkdir -p ${PREFIX}/bin
+
 MAIN_CMD=${PREFIX}/bin/RD-Analyzer.py
 echo '#!/usr/bin/env python' >$MAIN_CMD
 cat RD-Analyzer.py >> $MAIN_CMD
