6f57c79778625e43e24944502e7f95583a7ba569
Abhinav Sharma <abhi18av@gmail.com>
2021-08-28 02:24:15 +0530

    restore the original location inside bin

--- recipes/rd-analyzer/build.sh
+++ recipes/rd-analyzer/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 RD_ANALYZER_DIR=${PREFIX}/RD-Analyzer
-MAIN_CMD=${PREFIX}/RD-Analyzer.py
+MAIN_CMD=${PREFIX}/bin/RD-Analyzer.py
 echo '#!/usr/bin/env python' >$MAIN_CMD
 cat RD-Analyzer.py >> $MAIN_CMD
 chmod a+x $MAIN_CMD
