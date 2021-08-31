7626a0401f4ca958f41dd5fa02129821e9a6ae9f
Matthias Bernt <m.bernt@ufz.de>
2021-08-31 10:32:08 +0200

    skip osx build
    seem not possible with the provided binaries:
    
    ```
    error: /Users/distiller/project/miniconda/conda-bld/sirius-csifingerid_1630397834833/_build_env/bin/x86_64-apple-darwin13.4.0-install_name_tool: changing install names or rpaths can't be redone for: /Users/distiller/project/miniconda/conda-bld/sirius-csifingerid_1630397834833/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_/share/sirius-csifingerid-4.9.3-0/runtime/Contents/Home/lib/libjava.dylib (for architecture x86_64) because larger updated load commands do not fit (the program must be relinked, and you may need to use -headerpad or -headerpad_max_install_names)
    ```

--- recipes/sirius-csifingerid/build.sh
+++ recipes/sirius-csifingerid/build.sh
@@ -4,15 +4,7 @@ mkdir -p $outdir/bin
 mkdir -p $outdir/lib
 mkdir -p $PREFIX/bin
 
-unamestr=`uname`
-if [ "$unamestr" == 'Linux' ];
-then
-    cp -a lib/. $outdir/lib
-    cp -r bin/sirius $outdir/bin/sirius
-    chmod +x $outdir/bin/sirius
-    ln -s $outdir/bin/sirius $PREFIX/bin
-elif [ "$unamestr" == 'Darwin' ];
-then
-    cp -r Contents/* $outdir
-    ln -s $outdir/MacOS/sirius $PREFIX/bin
-fi
+cp -a lib/. $outdir/lib
+cp -r bin/sirius $outdir/bin/sirius
+chmod +x $outdir/bin/sirius
+ln -s $outdir/bin/sirius $PREFIX/bin
