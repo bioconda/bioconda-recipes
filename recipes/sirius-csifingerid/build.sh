048a40c97e34b6f2621024cd042a773f139624bd
Matthias Bernt <m.bernt@ufz.de>
2021-08-31 09:43:34 +0200

    support osx in build script

--- recipes/sirius-csifingerid/build.sh
+++ recipes/sirius-csifingerid/build.sh
@@ -4,7 +4,15 @@ mkdir -p $outdir/bin
 mkdir -p $outdir/lib
 mkdir -p $PREFIX/bin
 
-cp -a lib/. $outdir/lib
-cp -r bin/sirius $outdir/bin/sirius
-chmod +x $outdir/bin/sirius
-ln -s $outdir/bin/sirius $PREFIX/bin
+unamestr=`uname`
+if [ "$unamestr" == 'Linux' ];
+then
+    cp -a lib/. $outdir/lib
+    cp -r bin/sirius $outdir/bin/sirius
+    chmod +x $outdir/bin/sirius
+    ln -s $outdir/bin/sirius $PREFIX/bin
+elif [ "$unamestr" == 'Darwin' ];
+then
+    cp -r Contents/* $outdir
+    ln -s $outdir/MacOS/sirius $PREFIX/bin
+fi
