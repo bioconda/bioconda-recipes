6fc3e522f729bfb834c153811da6f1243a06290b
Matthias Bernt <m.bernt@ufz.de>
2021-09-01 17:50:30 +0200

    no-cache-dir, more modules

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -6,7 +6,7 @@ set -x
 pushd build
 
 # Split in more modules to reduce memory usage (default=8), stay on serial compilation (default PY_NUM_THREADS=1)
-cmake -DPYOPENMS=ON -DPY_NUM_MODULES=8 .
+cmake -DPYOPENMS=ON -DPY_NUM_MODULES=32 .
 # Unfortunately if we would call make, it thinks that OpenMS lib is not built since the build folder
 # might have been copied, so it rebuilds. Skip this by using setup.py directly.
 # TODO check that it actually uses the prebuilt libOpenMS from the prefix.
@@ -18,7 +18,7 @@ $PYTHON create_cpp_extension.py
 tree .
 cat setup.py
 ls -lh
-$PYTHON -m pip install . --ignore-installed --no-deps --verbose --global-option="--single-threaded"
+$PYTHON -m pip --no-cache-dir install . --ignore-installed --no-deps --verbose --global-option="--single-threaded"
 #ulimit -v unlimited
 popd
 popd
