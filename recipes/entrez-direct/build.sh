c29b26b383fa85099fbf2c17f7066d65c2aa2456
Istvan Albert <istvan.albert@gmail.com>
2021-09-07 12:40:35 -0400

    Update build.sh

--- recipes/entrez-direct/build.sh
+++ recipes/entrez-direct/build.sh
@@ -7,7 +7,9 @@ mkdir -p $PREFIX/bin
 mv * $PREFIX/bin
 mkdir -p "$PREFIX/home"
 export HOME="$PREFIX/home"
-sh ${PREFIX}/bin/install.sh
 
-# clean up
-rm -rf $PREFIX/bin/src $PREFIX/bin/*.log $PREFIX/bin/*.go $PREFIX/bin/*.yaml $PREFIX/bin/*.sh
+# Needs to run in the install folder
+cd ${PREFIX}/bin
+
+sh install.sh
+
