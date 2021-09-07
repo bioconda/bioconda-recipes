b18f5959e3080ab6b34d2a2bf744f03f505cbc84
Istvan Albert <istvan.albert@gmail.com>
2021-09-07 10:58:34 -0400

    Update build.sh

--- recipes/entrez-direct/build.sh
+++ recipes/entrez-direct/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Keep track of the process
+set -uex
+
 mkdir -p $PREFIX/bin
 mv * $PREFIX/bin
 mkdir -p "$PREFIX/home"
 export HOME="$PREFIX/home"
-sh ${PREFIX}/bin/setup.sh
+sh ${PREFIX}/bin/install.sh
 
 # clean up
 rm -rf $PREFIX/bin/src $PREFIX/bin/*.log $PREFIX/bin/*.go $PREFIX/bin/*.yaml $PREFIX/bin/*.sh
