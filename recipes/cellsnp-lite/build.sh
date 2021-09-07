e4bf0024b69896342a0a7bee780ffd90f0fa7c82
hxj5 <hxj5@hku.hk>
2021-09-07 15:17:48 +0800

    update cellsnp-lite to v1.2.1

--- recipes/cellsnp-lite/build.sh
+++ recipes/cellsnp-lite/build.sh
@@ -4,13 +4,17 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 
-cflags="-g -Wall -O2 -Wno-unused-function -fgnu89-inline -I${PREFIX}/include"
+autoreconf -iv
+./configure     # Needed for choosing optional functionality
+make
 
-if [ "`uname`" == "Darwin" ]; then
-    make CC=${CC} CFLAGS="-fgnu89-inline -fcommon ${CFLAGS}" LDFLAGS="${LDFLAGS}"
-else
-    make CC=${CC} CFLAGS="${cflags}" htslib_lib_dir=${PREFIX}/lib
-fi
+#cflags="-g -Wall -O2 -Wno-unused-function -I${PREFIX}/include"
+
+#if [ "`uname`" == "Darwin" ]; then
+#    make CC=${CC} CFLAGS="-fcommon ${CFLAGS}" LDFLAGS="${LDFLAGS}"
+#else
+#    make CC=${CC} CFLAGS="${cflags}" htslib_lib_dir=${PREFIX}/lib
+#fi
 
 mkdir -p ${PREFIX}/bin
 cp -f cellsnp-lite ${PREFIX}/bin
