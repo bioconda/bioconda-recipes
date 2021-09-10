bb15bad5738fe432d798718cf3b460459cb108a9
Woosub-Kim <59988419+Woosub-Kim@users.noreply.github.com>
2021-09-10 21:32:30 +0900

    [ci skip] Merge PR 29539
    Merge PR #29539, commits were: 
     * setup.py based installation of probeit6
     * setup.py based installation of probeit5
     * setup.py based installation of probeit4
     * setup.py based installation of probeit3.1
     * Merge branch 'master' of https://github.com/bioconda/bioconda-recipes into probeit
     * setup.py based installation of probeit3
     * setup.py based installation of probeit2
     * setup.py based installation of probeit
     * Merge branch 'bioconda:master' into probeit
     * probeit v1.4
     * v1.4
     * v1.3
     * package data 210722
     * package data path ch
     * sample data 2
     * sample data
     * Merge branch 'probeit' of https://github.com/woosub-kim/bioconda-recipes into probeit
     * Fix dependencies
     * Pass default conda CXXFLAGS to build script
     * Try using pin_compatible('numpy')
     * call setcover from path instead of a subfolder
     * Ignore unnecessary avx and debug flags from Makefile
     * Fix probeit dependencies
     * Initial probeit recipe
     * Merge branch 'probeit' of https://github.com/woosub-kim/bioconda-recipes into probeit
     * Try using pin_compatible('numpy')
     * call setcover from path instead of a subfolder
     * Ignore unnecessary avx and debug flags from Makefile
     * Fix probeit dependencies
     * Initial probeit recipe

---
+++ recipes/probeit/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+$PYTHON -m pip install . --ignore-installed --no-deps -vv
+# setcover
+cd probeit/setcover
+make CXXFLAGS="${CXXFLAGS} -I. -O3 -std=c++14" -j $CPU_COUNT
+install -d "${PREFIX}/bin"
+install setcover "${PREFIX}/bin/"
+cd ../..
