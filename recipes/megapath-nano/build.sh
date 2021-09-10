7c48c16c43e959ad5ada95f6467fe96257f93339
edwwlui <rewq142857@hotmail.com>
2021-09-10 23:40:09 +0800

    update cd path

--- recipes/megapath-nano/build.sh
+++ recipes/megapath-nano/build.sh
@@ -9,5 +9,5 @@ cd $PREFIX/MegaPath-Nano/bin/realignment/realign/
 $CXX -std=c++14 -O1 -shared -fPIC -o realigner ssw_cpp.cpp ssw.c realigner.cpp
 $CXX -std=c++11 -shared -fPIC -o debruijn_graph -O3 debruijn_graph.cpp -I $PREFIX/include -L $PREFIX/lib
 $CC -Wall -O3 -pipe -fPIC -shared -rdynamic -o libssw.so ssw.c ssw.h
-cd $PREFIX/MegaPath-Nano/bin/Clair-ensemble/Clair.beta.ensemble.cpu/clair/ensemble
+cd $PREFIX/MegaPath-Nano/bin/Clair-ensemble/Clair.beta.ensemble.cpu/clair/
 $CXX ensemble.cpp -o ensemble
