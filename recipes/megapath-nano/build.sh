3f0086d606941a675691066798131a527c667a00
edwwlui <rewq142857@hotmail.com>
2021-09-11 13:13:16 +0800

    pip install biopython git

--- recipes/megapath-nano/build.sh
+++ recipes/megapath-nano/build.sh
@@ -11,5 +11,4 @@ $CXX -std=c++11 -shared -fPIC -o debruijn_graph -O3 debruijn_graph.cpp -I $PREFI
 $CC -Wall -O3 -pipe -fPIC -shared -rdynamic -o libssw.so ssw.c ssw.h
 cd $PREFIX/MegaPath-Nano/bin/Clair-ensemble/Clair.beta.ensemble.cpu/clair/
 $CXX ensemble.cpp -o ensemble
-python -m pip install --upgrade pip
-pip install biopython && pip install git+https://github.com/arpcard/rgi.git
+pip install git+https://github.com/biopython/biopython.git && pip install git+https://github.com/arpcard/rgi.git
