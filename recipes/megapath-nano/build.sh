1dceef32318a9949fc338b55f6829eced5f71975
edwwlui <rewq142857@hotmail.com>
2021-09-11 11:38:57 +0800

    upgrade pip

--- recipes/megapath-nano/build.sh
+++ recipes/megapath-nano/build.sh
@@ -11,4 +11,5 @@ $CXX -std=c++11 -shared -fPIC -o debruijn_graph -O3 debruijn_graph.cpp -I $PREFI
 $CC -Wall -O3 -pipe -fPIC -shared -rdynamic -o libssw.so ssw.c ssw.h
 cd $PREFIX/MegaPath-Nano/bin/Clair-ensemble/Clair.beta.ensemble.cpu/clair/
 $CXX ensemble.cpp -o ensemble
+python -m pip install --upgrade pip
 pip install biopython && pip install git+https://github.com/arpcard/rgi.git
