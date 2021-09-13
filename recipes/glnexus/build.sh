2cbf11bfff201272805532659fcfa2ae3f5fd3c4
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2021-09-13 10:18:42 +0200

    add comments

--- recipes/glnexus/build.sh
+++ recipes/glnexus/build.sh
@@ -3,3 +3,7 @@
 chmod +x glnexus_cli
 mkdir -p ${PREFIX}/bin
 cp -f glnexus_cli ${PREFIX}/bin
+
+# #### Work in progress to build from source
+# Building from source requires an updated version of capnproto 
+# which is currently unavailable on conda-forge.  
