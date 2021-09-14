dd284f87089ad1d89184a6db90ac409ff63e5db3
Clara Köhne <clara.koehne@gmx.de>
2021-09-14 10:10:27 +0200

    fix paths for julia trace-compile call

--- recipes/patchwork/build.sh
+++ recipes/patchwork/build.sh
@@ -5,8 +5,8 @@ compile_file="${SRC_DIR}/src/compile.jl"
 # file with precompilation statements: 
 precompiled_file="${SRC_DIR}/src/precompiled.jl"
 # test files for precompilation run, if necessary:
-contigs="${RECIPE_DIR}/test/07673_lcal.fa"
-reference="${RECIPE_DIR}/test/07673_Alitta_succinea.fa"
+contigs="${RECIPE_DIR}/precompile_statements/07673_lcal.fa"
+reference="${RECIPE_DIR}/precompile_statements/07673_Alitta_succinea.fa"
 # directory to contain the finished build:
 build_dir="${SRC_DIR}/build"
 # inside build_dir, contains directories artifacts, bin with executable patchwork, and lib:
