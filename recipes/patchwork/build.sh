e99c547dc23e696eba1bda05796471b34b355181
Clara Köhne <clara.koehne@gmx.de>
2021-09-16 17:50:40 +0200

    workaround for missing libmbedcrypto.so

--- recipes/patchwork/build.sh
+++ recipes/patchwork/build.sh
@@ -27,6 +27,10 @@ cp "${RECIPE_DIR}/dependencies/Manifest.toml" "${SRC_DIR}"
 mkdir -p "${build_dir}/${patchwork_dir}"
 # PackageCompiler.jl get_compiler() searches for "gcc" and "clang", therefore:
 mv "${GCC}" "${BUILD_PREFIX}/bin/gcc"
+libmbedcrypto=$(find "${BUILD_PREFIX}" -name "libmbedcrypto.so")
+echo "${libmbedcrypto}"
+# libmbedcrypto="${BUILD_PREFIX}/bin/lib/julia/libmbedcrypto.so"
+ln -s "${libmbedcrypto}" "${BUILD_PREFIX}/bin/lib/julia/libmbedcrypto.so.5"
 # not sure if this is necessary, but can't have these in the Patchwork module:
 julia -e "import Pkg; Pkg.add([\"ArgParse\", \"PackageCompiler\"])"
 # compile Patchwork: 
