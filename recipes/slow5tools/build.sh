e0de8c29e60c635ce6fee4296c99b9da31481166
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-09-17 00:45:38 +1000

    remove cflags

--- recipes/slow5tools/build.sh
+++ recipes/slow5tools/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scripts/install-zstd.sh
 ./configure  --enable-localzstd
-make  CC=$CC CXX=$CXX CFLAGS="-g -Wall -O2 -D__STDC_FORMAT_MACROS"
+make
 mkdir -p $PREFIX/bin
 cp slow5tools $PREFIX/bin/slow5tools
