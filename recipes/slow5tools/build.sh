bae6a77dc3f94280cd8144052afba6aba7a0681e
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2021-09-17 01:38:03 +1000

    export D__STDC_FORMAT_MACROS

--- recipes/slow5tools/build.sh
+++ recipes/slow5tools/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 scripts/install-zstd.sh
 ./configure  --enable-localzstd
+export CFLAGS="${CFLAGS} -D__STDC_FORMAT_MACROS"
 make  CC=$CC CXX=$CXX 
 mkdir -p $PREFIX/bin
 cp slow5tools $PREFIX/bin/slow5tools
