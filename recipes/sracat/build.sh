3c72f0b940bdec183590a7b6974526b1889ddff0
Luiz Irber <luiz.irber@gmail.com>
2021-09-17 16:36:41 -0700

    fix build command

--- recipes/sracat/build.sh
+++ recipes/sracat/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-c++ -O3 -Wall -I. -I${BUILD_PREFIX}/include -c sracat.cpp
-c++ -o ${PREFIX}/bin/sracat sracat.o -lm -lz -lpthread -L${BUILD_PREFIX}/lib64 \
+$CXX ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -O3 -Wall \
+     -I. -I${PREFIX}/include -c sracat.cpp
+$CXX ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -o ${PREFIX}/bin/sracat sracat.o \
+    -lm -lz -lpthread -L${PREFIX}/lib64 \
     -lncbi-ngs-c++ -lncbi-ngs -lngs-c++ -lncbi-vdb-static -ldl
