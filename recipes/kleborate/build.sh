b7a4ca4127845238046de949dbe2fbccb92af4b6
Robert A. Petit III <robbie.petit@gmail.com>
2021-09-17 13:22:57 -0600

    Update build.sh

--- recipes/kleborate/build.sh
+++ recipes/kleborate/build.sh
@@ -4,4 +4,4 @@
 
 # Make blastdbs of Kleborate data
 # Allows containters to be usable
-kleborate -a kleborate/data Klebsiella_pneumoniae.fasta --all
+ $PREFIX/bin/kleborate -a kleborate/data Klebsiella_pneumoniae.fasta --all
