9e5038f512312c63a7a5c106aaccf675b071b23b
Robert A. Petit III <robbie.petit@gmail.com>
2021-09-21 13:55:41 -0600

    Update build.sh

--- recipes/spatyper/build.sh
+++ recipes/spatyper/build.sh
@@ -5,9 +5,10 @@ $PYTHON -m pip install . --ignore-installed --no-deps -vv
 # Add default databases
 SPATYPER_SHARE="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
 mkdir -p ${SPATYPER_SHARE}
+ls -lh
 chmod 755 download-spatypes.sh
 ./download-spatypes.sh ${SPATYPER_SHARE}
-mv download-spaypes.sh ${PREFIX}/bin
+mv download-spatypes.sh ${PREFIX}/bin
 
 # Setup the spaTyper env variables
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
