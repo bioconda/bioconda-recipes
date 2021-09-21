55f50dcb13275a1bd219f278a23e463e455c7e69
Robert A. Petit III <robbie.petit@gmail.com>
2021-09-21 11:47:07 -0600

    Update build.sh

--- recipes/spatyper/build.sh
+++ recipes/spatyper/build.sh
@@ -4,5 +4,5 @@ $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 # Add default databases
 printf '>1\nAAAA\n' > test.fasta
-$PREFIX/bin/spatyper -f test.fasta
+$PREFIX/bin/spaTyper -f test.fasta
 rm test.fasta
