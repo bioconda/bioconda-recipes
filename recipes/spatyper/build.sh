d9da100a52747d497f88885c82c95c662cc536c7
Robert A. Petit III <robbie.petit@gmail.com>
2021-09-21 12:04:09 -0600

    fix line endings

--- recipes/spatyper/build.sh
+++ recipes/spatyper/build.sh
@@ -3,6 +3,7 @@
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 # Add default databases
+sed -i 's/\r//' $PREFIX/bin/spaTyper
 printf '>1\nAAAA\n' > test.fasta
 $PREFIX/bin/spaTyper -f test.fasta
 rm test.fasta
