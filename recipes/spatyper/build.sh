f577d674b37acb41a7646128330b2f47d60a8405
Robert A. Petit III <robbie.petit@gmail.com>
2021-09-21 12:45:54 -0600

    Update build.sh

--- recipes/spatyper/build.sh
+++ recipes/spatyper/build.sh
@@ -3,7 +3,17 @@
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 # Add default databases
-sed -i 's/\r//' $PREFIX/bin/spaTyper
-printf '>1\nAAAA\n' > test.fasta
-$PREFIX/bin/spaTyper -f test.fasta
-rm test.fasta
+SPATYPER_SHARE="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p ${SPATYPER_SHARE}
+chmod 755 download-spatypes.sh
+./download-spatypes.sh ${SPATYPER_SHARE}
+mv download-spaypes.sh ${PREFIX}/bin
+
+# Setup the spaTyper env variables
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+echo "export SPATYPER_SHARE=${SPATYPER_SHARE}" > ${PREFIX}/etc/conda/activate.d/spatyper.sh
+chmod a+x ${PREFIX}/etc/conda/activate.d/spatyper.sh
+
+# Unset them
+echo "unset SPATYPER_SHARE" > ${PREFIX}/etc/conda/deactivate.d/spatyper.sh
+chmod a+x ${PREFIX}/etc/conda/deactivate.d/spatyper.sh
