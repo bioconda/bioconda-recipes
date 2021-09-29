535dc60a964d24cdd198cedb4033617b14f446c4
Kimmo Palin <kimmo.palin@helsinki.fi>
2021-09-29 10:05:11 +0300

    Update bioawk to maintained fork

--- recipes/bioawk/build.sh
+++ recipes/bioawk/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LIBRARY_PATH="$PREFIX/lib"
 
-make CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
+make CC=$CC CFLAGS="-g -Wall -O2 -DBIO_VERSION='\"$PKG_VERSION\"' -I$PREFIX/include -L$PREFIX/lib"
 
 mkdir -p $PREFIX/bin
 cp bioawk $PREFIX/bin
