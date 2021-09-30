910eaf3d617331c9e1c2cede58826ee921da3dcb
Kimmo Palin <kimmo.palin@helsinki.fi>
2021-09-30 10:30:29 +0300

    Trying to get cross-compiling working in conda

--- recipes/bioawk/build.sh
+++ recipes/bioawk/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LIBRARY_PATH="$PREFIX/lib"
 
-make CC=$CC CFLAGS="-g -Wall -O2 -DBIO_VERSION='\"$PKG_VERSION\"' -I$PREFIX/include -L$PREFIX/lib"
+make CC="${CC}" HOSTCC="${CC}" CFLAGS="-g -Wall -O2 -DBIO_VERSION='\"$PKG_VERSION\"' -I$PREFIX/include -L$PREFIX/lib"
 
-mkdir -p $PREFIX/bin
-cp bioawk $PREFIX/bin
+mkdir -p "$PREFIX/bin"
+cp bioawk "$PREFIX/bin"
