14169bcb7e55bdd6192b2e1fc775fe4dedd5b2b8
Sergey Koren <sergek@umd.edu>
2021-10-02 09:29:27 -0400

    Update run_test.sh

--- recipes/canu/run_test.sh
+++ recipes/canu/run_test.sh
@@ -6,7 +6,7 @@ set -eu -o pipefail
 
 rm -f ./lamba_ont.fasta.gz
 curl -L https://obj.umiacs.umd.edu/sergek/shared/lambda_ont.fasta.gz -o lambda_ont.fasta.gz
-canu -p asm -d asm useGrid=false genomeSize=21k -fast maxMemory=8 maxThreads=4 -nanopore lambda_ont.fasta.gz
+canu -p asm -d asm useGrid=false genomeSize=21k -fast maxMemory=4 maxThreads=2 corThreads=2 redThreads=2 redMemory=4 oeaMemory=4 -nanopore lambda_ont.fasta.gz
 
 if [ ! -s asm/asm.contigs.fasta ] || [ ! -s asm/asm.report ]; then
    echo "Error: canu assembly test failed!"
