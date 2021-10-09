15921469c911a50a9338fbc2d483f5e08d1fd438
Gregor Entzian <gregor.entzian@lexogen.com>
2021-10-10 00:38:43 +0200

    Add lrt

--- recipes/idemuxcpp/build.sh
+++ recipes/idemuxcpp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-./configure --prefix="${PREFIX}" BAMTOOLS_CFLAGS="-I${PREFIX}/include/" BAMTOOLS_LIBS="-L${PREFIX}/lib/ -lbamtools"
+./configure --prefix="${PREFIX}" BAMTOOLS_CFLAGS="-I${PREFIX}/include/" BAMTOOLS_LIBS="-L${PREFIX}/lib/ -lbamtools" LIBS="-lrt"
 make
 make install
 make check
