828d2b57ac2f1d3b07cb6eafe88c784c1862be46
Gregor Entzian <gregor.entzian@lexogen.com>
2021-10-09 22:27:43 +0200

    update dependencies

--- recipes/idemuxcpp/build.sh
+++ recipes/idemuxcpp/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-./configure --prefix="${PREFIX}"
+pkg_config_dir=$(find ${PREFIX}/lib -name "bamtools-*.pc" -exec dirname {} \; | tail -n1)
+if [ ! -z "${pkg_config_dir}" ]; then
+  ./configure --prefix="${PREFIX}" PKG_CONFIG_PATH=${pkg_config_dir}
+else
+  ./configure --prefix="${PREFIX}"
+fi
 make
 make install
 make check
