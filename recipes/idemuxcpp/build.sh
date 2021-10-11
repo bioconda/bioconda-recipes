bd288c6554fdaf537a0cdc75ad58e1b576b0fad5
Gregor Entzian <gregor.entzian@lexogen.com>
2021-10-11 19:37:11 +0200

    Add enable-tests parameter

--- recipes/idemuxcpp/build.sh
+++ recipes/idemuxcpp/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-./configure --prefix="${PREFIX}" BAMTOOLS_CFLAGS="-I${PREFIX}/include/" BAMTOOLS_LIBS="-L${PREFIX}/lib/ -lbamtools" LIBS="-lrt"
+./configure --enable-tests --prefix="${PREFIX}" BAMTOOLS_CFLAGS="-I${PREFIX}/include/" BAMTOOLS_LIBS="-L${PREFIX}/lib/ -lbamtools"
 make
 make install
 make check
