faa56b03114f5f474a3dba21ee005e0fecc63bb2
Marcel Bargull <marcel.bargull@udo.edu>
2021-10-11 09:02:20 +0200

    rust-bio-tools: reenable nocona compat

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
 
-# Dependency crate librocksdb-sys uses SSE4.1/4.2 and PCLMUL unconditionally.
-# => Set architecture compatibility to "westmere".
-export \
-    CFLAGS="${CFLAGS} -march=westmere" \
-    CXXFLAGS="${CXXFLAGS} -march=westmere"
+export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
-export \
-    CARGO_NET_GIT_FETCH_WITH_CLI=true \
-    CARGO_HOME="${BUILD_PREFIX}/.cargo" \
-    BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
-
-cargo install --path . --root "${PREFIX}" --verbose
+cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
