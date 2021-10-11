0764d82f40da289143a0e759004a1384d4dfd2b7
Jacques Dainat <jacques.dainat@nbis.se>
2021-10-11 22:35:43 +0200

    add pfscanV3 pfsearchV3

--- recipes/pftools/build.sh
+++ recipes/pftools/build.sh
@@ -4,7 +4,7 @@ set -x -e
 #compile
 #conda install -p $PREFIX libgfortran=3.0 -y
 # backup to "gfortran" in conda GFORTRAN is not set
-PFTOOLS_PROGRAMS="gtop pfmake pfscan pfw ptoh htop pfscale pfsearch psa2msa 2ft 6ft ptof"
+PFTOOLS_PROGRAMS="pfscanV3 pfsearchV3 gtop pfmake pfscan pfw ptoh htop pfscale pfsearch psa2msa 2ft 6ft ptof"
 make CC=${CC} CXX=${CXX} F77=${GFORTRAN} CFLAGS="$CFLAGS $LDFLAGS" ${PFTOOLS_PROGRAMS}
 
 # copy tools in the bin
