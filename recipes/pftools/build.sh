70f533a9285ff25dbb3e95194189b27720816bae
Jacques Dainat <jacques.dainat@nbis.se>
2021-10-12 16:24:05 +0200

    add dependencies and new way to compile

--- recipes/pftools/build.sh
+++ recipes/pftools/build.sh
@@ -5,7 +5,12 @@ set -x -e
 #conda install -p $PREFIX libgfortran=3.0 -y
 # backup to "gfortran" in conda GFORTRAN is not set
 PFTOOLS_PROGRAMS="pfscanV3 pfsearchV3 gtop pfmake pfscan pfw ptoh htop pfscale pfsearch psa2msa 2ft 6ft ptof"
-make CC=${CC} CXX=${CXX} F77=${GFORTRAN} CFLAGS="$CFLAGS $LDFLAGS" ${PFTOOLS_PROGRAMS}
+mkdir build
+cd build/
+cmake ..
+make CC=${CC} CXX=${CXX} F77=${GFORTRAN} CFLAGS="$CFLAGS $LDFLAGS"
+
+ls -l
 
 # copy tools in the bin
 mkdir -p ${PREFIX}/bin
