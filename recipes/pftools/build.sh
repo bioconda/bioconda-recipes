79fb2958bd1847f5530d17a172bb03c2e244d67e
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2021-10-13 17:20:02 +0200

    Update build.sh

--- recipes/pftools/build.sh
+++ recipes/pftools/build.sh
@@ -3,6 +3,8 @@ set -x -e
 
 # programs intsalled
 # pfscanV3 pfsearchV3 pfcalibrateV3 pfemit pfpam pfdump pfindex gtop htop ptoh ptof pfw 2ft 6ft psa2msa pfscale pfsearch pfscan pfmake
+# ps_scan.pl scramble_fasta.pl sort_fasta.pl split_profile_file.pl make_iupac_cmp.pl fasta_to_fastq.pl compare_2_profiles.pl
+ 
 mkdir build
 cd build/
 cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
