fbd7053af467ca4ef188a3b00c138c18a88bf609
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2021-10-13 16:33:18 +0200

    Update build.sh

--- recipes/pftools/build.sh
+++ recipes/pftools/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -x -e
 
-#compile
-#conda install -p $PREFIX libgfortran=3.0 -y
-# backup to "gfortran" in conda GFORTRAN is not set
-PFTOOLS_PROGRAMS="pfscanV3 pfsearchV3 gtop pfmake pfscan pfw ptoh htop pfscale pfsearch psa2msa 2ft 6ft ptof"
-mkdir build
+# programs intsalled
+# pfscanV3 pfsearchV3 pfcalibrateV3 pfemit pfpam pfdump pfindex gtop htop ptoh ptof pfw 2ft 6ft psa2msa pfscale pfsearch pfscan pfmake
+
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}/bin" ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
 make CC=${CC} CXX=${CXX} F77=${GFORTRAN} CFLAGS="$CFLAGS $LDFLAGS"
 make install
 make test
