7590266205dfd54510c750deb84bae8f047fc86c
Jacques Dainat <jacques.dainat@sys2diag.cnrs.fr>
2021-10-13 16:54:22 +0200

    Update build.sh

--- recipes/pftools/build.sh
+++ recipes/pftools/build.sh
@@ -3,7 +3,7 @@ set -x -e
 
 # programs intsalled
 # pfscanV3 pfsearchV3 pfcalibrateV3 pfemit pfpam pfdump pfindex gtop htop ptoh ptof pfw 2ft 6ft psa2msa pfscale pfsearch pfscan pfmake
-
+mkdir build
 cd build/
 cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
 make CC=${CC} CXX=${CXX} F77=${GFORTRAN} CFLAGS="$CFLAGS $LDFLAGS"
