90d234994cecf800fe3c1631cf835e3d3a8db959
svletana <luciaa.fabio@gmail.com>
2021-10-14 17:28:45 +0200

    added tests to the build recipe

--- recipes/curves/build.sh
+++ recipes/curves/build.sh
@@ -3,9 +3,6 @@
 # Compile using make
 make 
 
-# Copy executable to environment bin directory. This folder is included in the path when the env is activated
-mkdir -p $PREFIX/bin
-
 chmod u+x Cur+
 chmod u+x Canal
 chmod u+x Canion
