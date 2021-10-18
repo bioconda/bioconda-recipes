9fbd7b0ca18a4aad1b406eacda24561e5eac7223
qibebt_bioinfo <qibebt_bioinfo@163.com>
2021-10-18 19:41:27 +0800

    fix the 'missing databases' problem, now databases are downloaded from qibebt server automatically.

--- recipes/meta-apo/build.sh
+++ recipes/meta-apo/build.sh
@@ -9,15 +9,23 @@ cp -r bin/* ${PREFIX}/bin
 PKG_NAME=meta-apo
 PKG_VERSION=v1.1
 
-MetaApo_path=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}
+# path for meta-apo database and example
+MetaApo_path=${HOME}/${PKG_NAME}-${PKG_VERSION}
 mkdir -p ${MetaApo_path}
-
-cp -r databases ${MetaApo_path}/databases
 
 # set VIBRANT DB PATH variable on env activation
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
 cat <<EOF >> ${PREFIX}/etc/conda/activate.d/meta-apo.sh
 export MetaApo=${MetaApo_path}
+
+if [ -f "$MetaApo_path/databases/db.config" ];then
+    echo "11"
+else
+    echo "DB file not exsit, start downloading..."
+    rm -rf "$MetaApo_path"
+    wget -P "$MetaApo_path" "http://bioinfo.single-cell.cn/Released_Software/meta-apo/conda_test.tar.gz"
+    tar -xzvf "$MetaApo_path/conda_test.tar.gz" -C "$MetaApo_path"
+fi
 EOF
 
 cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/meta-apo.sh
