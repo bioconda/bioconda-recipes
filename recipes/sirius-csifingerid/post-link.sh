255f57049040c49e983991325bf4b98d5d9b42c5
fleisch <markus.fleischauer@uni-jena.de>
2021-10-20 00:22:36 +0200

    try preventing lib modification during installation

---
+++ recipes/sirius-csifingerid/post-link.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+outdir="${PREFIX}"/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+# unpack library directory
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    echo "placeholder"
+    tar -xf $outdir/Contents/native.tgz -C $outdir/Contents/
+    tar -xf $outdir/Contents/runtime.tgz -C $outdir/Contents/
+    rm $outdir/Contents/native.tgz
+    rm $outdir/Contents/runtime.tgz
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    echo $outdir
+    tar -xf $outdir/lib.tgz -C $outdir/
+    rm $outdir/lib.tgz
+fi
