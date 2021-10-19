947d6f6fe3fdb4db2b9bf03d2ac5e276e90a89c2
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-10-19 14:29:04 -0700

    UniFrac to 0.20.3

--- recipes/unifrac/build.sh
+++ recipes/unifrac/build.sh
@@ -6,18 +6,48 @@ export LIBRARY_PATH="${CONDA_PREFIX}/lib"
 export LD_LIBRARY_PATH=${LIBRARY_PATH}:${LD_LIBRARY_PATH}
 export CPLUS_INCLUDE_PATH="${CONDA_PREFIX}/include"
 
-ls -l
-if [ "$(uname)" == "Darwin" ]; then
-    export MACOSX_DEPLOYMENT_TARGET=10.12
-    SED="sed -i ''"
-else
-    SED='sed -i'
+if [[ "$(uname -s)" != "Linux" ]];
+then
+  ls -l /Applications/
+  # coonda clang does not like xcode 12
+  sudo xcode-select --switch /Applications/Xcode_11.7.app
+  # the system tools are unusable, hide them
+  sudo mv -f /Library/Developer/CommandLineTools /Library/Developer/CommandLineTools.org
 fi
 
-$SED 's/^STATIC_AVAILABLE=.*/STATIC_AVAILABLE="no"/' `which h5c++`
-grep STATIC_AVAILABLE `which h5c++`
+conda create --yes -n unifrac -c conda-forge -c bioconda python=3.6
+conda activate unifrac
+conda config --add channels conda-forge
+conda config --add channels bioconda
+
+if [[ "$(uname -s)" == "Linux" ]];
+then
+  conda install --yes -c conda-forge -c bioconda gxx_linux-64=7.5.0
+else
+  conda install --yes -c conda-forge -c bioconda clangxx_osx-64=10.0.0
+fi 
+conda install --yes -c conda-forge -c bioconda cython "hdf5>=1.8.17" biom-format numpy "h5py>=2.7.0" "scikit-bio>=0.5.1" flake8 nose
+conda install --yes -c conda-forge -c bioconda mkl-include lz4 hdf5-static
+
+if [[ "$(uname -s)" == "Linux" ]];
+then
+  which x86_64-conda-linux-gnu-gcc
+  x86_64-conda-linux-gnu-gcc -v
+  x86_64-conda-linux-gnu-g++ -v
+else
+  conda install --yes liblapacke
+  which clang
+  clang -v
+fi
+
+if [[ "$(uname -s)" == "Linux" ]];
+then
+  ./scripts/install_hpc_sdk.sh
+  source setup_nv_h5.sh
+fi
 
 pushd sucpp
+make main
 make api
 popd
 
