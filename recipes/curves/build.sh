1a8c1253b2738b437aa9ea93643c44972917616a
svletana <luciaa.fabio@gmail.com>
2021-10-20 17:11:18 +0200

    getting files from upstream source and patching makefile. modified tests

--- recipes/curves/build.sh
+++ recipes/curves/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-make \
-    F77="${FC}"
+make
 
 chmod u+x Cur+
 chmod u+x Canal
---
+++ recipes/curves/data/test_exec.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -ue pipefail;
+
+tar -xf input_files.tar.gz;
+  
+# Curves+ executable
+Cur+ <<!
+&inp 
+ file=mABCs1_stride.trj,
+ ftop=abctop_nowat.prmtop,
+ lis=curout_ions,
+ lib=$CONDA_PREFIX/.curvesplus/standard,
+ line=.t.,
+ fit=.t.,
+ test=.t.,
+ ions=.t.,
+&end        
+2 1 -1 0 0                                               
+1:18 
+36:19     
+!
+
+test -e curout_ions.cda;
+test -e curout_ions.cdi;
+test -e curout_ions.lis;
+ 
+# test Canal executable
+Canal <<! 
+&inp
+  lis=canout,
+  series=.t.,
+&end
+curout_ions.cda GCAACGTGCTATGGAAGC       
+!
+
+test -e canout.lis;
+test $(ls|grep canout|wc -l) = 45;
+
+# test Canion executable
+Canion <<!
+&inp
+ lis=canionout,
+ axfrm=curout_avg,
+ dat=curout_ions.cdi,
+ solute=mABCs1_avg,
+ type=K,
+&end
+!
+
+test -e canionout.lis;
+test $(ls | grep canionout | wc -l) = 9;
+
+# cleanup after last execution 
+rm curout_ions.cda 
+rm curout_ions.cdi 
+rm curout_ions.lis
+rm canout* 
+rm canionout.*
+rm abctop_nowat.prmtop 
+rm mABCs1_avg.pdb 
+rm mABCs1_stride.trj
+rm curout_avg.afr
