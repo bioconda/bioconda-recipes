abd5f9819f326ca3c63e25cd5f89fd640b6f5671
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2021-10-20 12:57:45 -0400

    remove unused code

--- recipes/isafe/build.sh
+++ recipes/isafe/build.sh
@@ -11,22 +11,10 @@ rm -rf example
 
 mv src/isafe.py src/isafe.py.tmp
 echo '#!/usr/bin/env python3' > src/isafe.py
-echo '' >> src/isafe.py
 cat src/isafe.py.tmp >> src/isafe.py
 rm src/isafe.py.tmp
-chmod u+x src/isafe.py
-#SHEBANG='#!/usr/bin/env python3'
-#echo -e "${SHEBANG}\n\n$(cat src/isafe.py)" > src/isafe.py
 
 cp -r * $PACKAGE_HOME
 
 chmod u+x ${PACKAGE_HOME}/src/isafe.py
 ln -s ${PACKAGE_HOME}/src/isafe.py ${BINARY_HOME}/isafe.py
-
-# isafe_script=${BINARY_HOME}/isafe
-# echo '#!/bin/bash' > ${isafe_script}
-# echo '' >> ${isafe_script}
-# echo 'set -eu -o pipefail' >> ${isafe_script}
-# echo '' >> ${isafe_script}
-# echo "python ${PACKAGE_HOME}/src/isafe.py \"\$@\"" >> ${isafe_script}
-# chmod u+x ${isafe_script}
