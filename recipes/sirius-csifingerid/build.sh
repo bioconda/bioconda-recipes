fb836f7c16b84ff921c041ba6c99d499fdbf8a07
fleisch <markus.fleischauer@uni-jena.de>
2021-10-25 16:48:43 +0200

    bump to 4.9.7, workaround for CLP lib problem

--- recipes/sirius-csifingerid/build.sh
+++ recipes/sirius-csifingerid/build.sh
@@ -1,11 +1,16 @@
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
+
 if [[ ${target_platform} =~ linux.* ]] ; then
     mkdir -p $outdir
     mkdir -p $PREFIX/bin
 
     cp -a ./* $outdir/
     chmod +x $outdir/bin/sirius
+    # protecting libs from being modified by conda-build
+    tar czf $outdir/lib.tgz -C $outdir/lib .
+    rm -r $outdir/lib
+
     ln -s $outdir/bin/sirius $PREFIX/bin
 elif [[ ${target_platform} =~ osx.* ]] ; then
      mkdir -p $outdir/Contents
@@ -13,5 +18,12 @@ elif [[ ${target_platform} =~ osx.* ]] ; then
 
      cp -a Contents/. $outdir/Contents
      chmod +x $outdir/Contents/MacOS/sirius
+
+     # protecting libs from being modified by conda-build
+     tar czf $outdir/Contents/runtime.tgz -C $outdir/Contents/runtime .
+     tar czf $outdir/Contents/native.tgz -C $outdir/Contents/native .
+     rm -r $outdir/Contents/runtime
+     rm -r $outdir/Contents/native
+
      ln -s $outdir/Contents/MacOS/sirius $PREFIX/bin
 fi
