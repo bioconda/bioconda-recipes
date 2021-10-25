d5089eff71e6c1e54f4bce13cdd6abbf77086bfe
fleisch <markus.fleischauer@uni-jena.de>
2021-10-25 17:59:44 +0200

    fix postlink extract

--- recipes/sirius-csifingerid/post-link.sh
+++ recipes/sirius-csifingerid/post-link.sh
@@ -4,13 +4,17 @@ outdir="${PREFIX}"/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
+#    echo $outdir
+    mkdir $outdir/Contents/native
+    mkdir $outdir/Contents/runtime
     tar -xf $outdir/Contents/native.tgz -C $outdir/Contents/
     tar -xf $outdir/Contents/runtime.tgz -C $outdir/Contents/
     rm $outdir/Contents/native.tgz
     rm $outdir/Contents/runtime.tgz
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
-    echo $outdir
-    tar -xf $outdir/lib.tgz -C $outdir/
+#    echo $outdir
+    mkdir $outdir/lib/
+    tar -xf $outdir/lib.tgz -C $outdir/lib/
     rm $outdir/lib.tgz
 fi
