0bad4ac5efb69b8a18959d54eaf573e87b7b8a22
fleisch <markus.fleischauer@uni-jena.de>
2021-10-26 18:28:01 +0200

    use external test

--- recipes/sirius-csifingerid/post-link.sh
+++ recipes/sirius-csifingerid/post-link.sh
@@ -7,8 +7,8 @@ if [ "$(uname)" == "Darwin" ]; then
 #    echo $outdir
     mkdir $outdir/Contents/native
     mkdir $outdir/Contents/runtime
-    tar -xf $outdir/Contents/native.tgz -C $outdir/Contents/
-    tar -xf $outdir/Contents/runtime.tgz -C $outdir/Contents/
+    tar -xf $outdir/Contents/native.tgz -C $outdir/Contents/native
+    tar -xf $outdir/Contents/runtime.tgz -C $outdir/Contents/runtime
     rm $outdir/Contents/native.tgz
     rm $outdir/Contents/runtime.tgz
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
