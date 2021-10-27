4944af2bef05255234d4c290ff78b2066e6f3294
kowallus <kowallus@o2.pl>
2021-10-27 20:03:13 +0200

    mbgc recipe - removed unnecessary parts

--- recipes/mbgc/build.sh
+++ recipes/mbgc/build.sh
@@ -3,11 +3,6 @@
 mkdir -p $PREFIX/bin
 mkdir build
 cd build
-export CPATH=${BUILD_PREFIX}/include
-export CXXPATH=${BUILD_PREFIX}/include
-export CFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
-export CXXFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$BUILD_PREFIX/lib"
 echo "$LDFLAGS"
 ls $BUILD_PREFIX/lib/libdeflate*
 cmake ..
