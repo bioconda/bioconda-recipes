afea84c8071e81784078e6d694dbdc77f4f79f2e
kowallus <kowallus@o2.pl>
2021-10-27 18:30:41 +0200

    mbgc recipe - paths

--- recipes/mbgc/build.sh
+++ recipes/mbgc/build.sh
@@ -3,13 +3,13 @@
 mkdir -p $PREFIX/bin
 mkdir build
 cd build
-export CPATH=${PREFIX}/include
-export CXXPATH=${PREFIX}/include
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${BUILD_PREFIX}/include
+export CXXPATH=${BUILD_PREFIX}/include
+export CFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
+export CXXFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$BUILD_PREFIX/lib"
 echo "$LDFLAGS"
-ls $PREFIX/lib/libdeflate*
+ls $BUILD_PREFIX/lib/libdeflate*
 cmake ..
 make VERBOSE=1 mbgc
 cp mbgc $PREFIX/bin
