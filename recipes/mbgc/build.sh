68642f1273efd685163f5b0a4cb8ee65af5a92d7
kowallus <kowallus@o2.pl>
2021-10-28 08:28:35 +0200

    mbgc recipe - libdeflate header file fix

--- recipes/mbgc/build.sh
+++ recipes/mbgc/build.sh
@@ -5,6 +5,8 @@ mkdir build
 cd build
 echo "$LDFLAGS"
 ls $BUILD_PREFIX/lib/libdeflate*
+cp $BUILD_PREFIX/lib/libdeflate.h $BUILD_PREFIX/include/
+cp $BUILD_PREFIX/lib/libdeflate.h $PREFIX/include/
 cmake ..
 make VERBOSE=1 mbgc
 cp mbgc $PREFIX/bin
