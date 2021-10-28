d56bf5d06911fbf504da7a98865879f0e44c47fd
kowallus <kowallus@o2.pl>
2021-10-28 08:58:21 +0200

    mbgc recipe - reverted exports

--- recipes/mbgc/build.sh
+++ recipes/mbgc/build.sh
@@ -3,7 +3,11 @@
 mkdir -p $PREFIX/bin
 mkdir build
 cd build
-echo "$LDFLAGS"
+export CPATH=${BUILD_PREFIX}/include
+export CXXPATH=${BUILD_PREFIX}/include
+export CFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
+export CXXFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$BUILD_PREFIX/lib"
 ls $BUILD_PREFIX/lib/libdeflate*
 cp $BUILD_PREFIX/lib/libdeflate.h $BUILD_PREFIX/include/
 cmake ..
