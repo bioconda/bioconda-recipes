fd0e854a1bf0042de3dd962a9e77fc3d59469ca3
kowallus <kowallus@o2.pl>
2021-10-28 17:17:49 +0200

    mbgc recipe - updates

--- recipes/mbgc/build.sh
+++ recipes/mbgc/build.sh
@@ -8,8 +8,7 @@ export CXXPATH=${BUILD_PREFIX}/include
 export CFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
 export CXXFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$BUILD_PREFIX/lib"
-ls $BUILD_PREFIX/lib/libdeflate*
 cp $BUILD_PREFIX/lib/libdeflate.h $BUILD_PREFIX/include/
 cmake ..
-make VERBOSE=1 mbgc
+make mbgc
 cp mbgc $PREFIX/bin
