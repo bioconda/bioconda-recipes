4994b579cd454e953a8a3faa8bb3bcffdb47bfc0
Robert A. Petit III <robbie.petit@gmail.com>
2021-10-29 08:56:20 -0600

    update vadr build script

--- recipes/vadr/build.sh
+++ recipes/vadr/build.sh
@@ -20,6 +20,12 @@ cp -r ./ ${VADR_DIR}/
 # Install SARS-CoV-2 model (~15mb)
 VADRMODELDIR="${VADRMODELDIR}" BIOCONDA_BUILD=1 bash -c '${PREFIX}/bin/download-vadr-models.sh sarscov2'
 
+# Play nicely with workflow managers (e.g. Nextflow's --entrypoint)
+echo "$!/usr/bin/env bash" > ${PREFIX}/bin/vadr-run.sh
+cat ${PREFIX}/etc/conda/activate.d/vadr.sh >> ${PREFIX}/bin/vadr-run.sh
+echo '"$@"' >> ${PREFIX}/vadr-run.sh
+chmod 755 ${PREFIX}/bin/vadr-run.sh
+
 # Setup the VADR env variables
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
 echo "export VADRVERSION=${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d/vadr.sh
