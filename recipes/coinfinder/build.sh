c3dd727bc5551d790b393aa6bb1fdbbeee76c617
fwhelan <fiona.jane.whelan@gmail.com>
2021-11-02 10:01:06 +0000

    removing lib path, based on other's boost issues (shot in the dark)

--- recipes/coinfinder/build.sh
+++ recipes/coinfinder/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=${PREFIX}/lib
-export LIBRARY_PATH=${PREFIX}/lib
+#export LIBRARY_PATH=${PREFIX}/lib
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBoost_USE_STATIC_LIBS=OFF .
 cmake --build .
