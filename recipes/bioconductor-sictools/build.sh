170896d4f0957298cfad570c2d807f0c4d5e7d9e
Devon Ryan <devon.ryan@genedata.com>
2021-11-05 09:21:41 +0100

    bulk update

--- recipes/bioconductor-sictools/build.sh
+++ recipes/bioconductor-sictools/build.sh
@@ -2,10 +2,10 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
-echo -e "CC=$CC
+echo -e "CC=$CC $CFLAGS $CPPFLAGS $LDFLAGS
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+MAKE="make LIBCURSES=-lncurses" $R CMD INSTALL --build .
