11f9d11ea19a99da389e51d2dfd18a6d3f8e7af9
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-11-09 18:29:06 +0100

    try to add swap space by adding big swap file?

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 # TODO actually it would be better if we could adapt our MANIFEST.in
 # to not package the openms libs and dependencies again. Same for share.
+
+SWAP_SIZE=6G
+
+# Default swap file
+
+SWAP_FILE=/var/lib/swap
+# Checking if swap already exists in ./etc/fstab
+grep -q "swap" /etc/fstab
+if ! grep -q "swap" /etc/fstab; then
+	 fallocate -l "$SWAP_SIZE" "$SWAP_FILE"
+	 chmod 600 "$SWAP_FILE"
+	 mkswap "$SWAP_FILE"
+	 swapon "$SWAP_FILE"	
+	echo "$SWAP_FILE   none    swap    sw    0   0" |  tee /etc/fstab -a
+else
+	echo 'swapfile found. No changes made.'
+fi
+
+echo '----------------------'
+echo 'Checking list of swap'
+echo '----------------------'
+swapon -s
+
+
 pushd build
 cmake -DPYOPENMS=ON -DPY_NUM_THREADS=1 .
 # Unfortunately if we would call make, it thinks that OpenMS lib is not built since the build folder
