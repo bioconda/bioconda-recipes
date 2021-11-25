d300391d3439aa574ecca4aaa7a2007286c9fa33
tobiasrausch <rauschtobi@gmail.com>
2021-11-25 17:10:06 +0100

    compile statically

--- recipes/wally/build.sh
+++ recipes/wally/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/ocv/include -L${SRC_DIR}/src/ocv/lib -I${PREFIX}/include -L${PREFIX}/lib"
+make STATIC=1 all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib"
 mkdir -p $PREFIX/bin
 cp src/wally $PREFIX/bin
