c46e0e2c12cf54b313240b33db25a668931bacf0
tobiasrausch <rauschtobi@gmail.com>
2021-11-26 11:22:55 +0100

    libopencv v4.5.3

--- recipes/wally/build.sh
+++ recipes/wally/build.sh
@@ -2,7 +2,4 @@
 
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib"
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/lib
-cp src/htslib/*.so* $PREFIX/lib
-cp src/ocv/lib*/*.so* $PREFIX/lib
 cp src/wally $PREFIX/bin
