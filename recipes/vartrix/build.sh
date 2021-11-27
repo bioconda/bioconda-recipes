5bd5677e66852073848867eba8d8bb18be82e24f
Vivek Rai <mail@raivivek.in>
2021-11-26 23:01:31 -0500

    rust -> compiler('rust')

--- recipes/vartrix/build.sh
+++ recipes/vartrix/build.sh
@@ -7,5 +7,11 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
+export MACOSX_DEPLOYMENT_TARGET=10.13
+
 cargo build --release
-cp ${SRC_DIR}/target/release/vartrix ${PREFIX}/bin
+
+# try to find where binary is. it seems that the binary
+# uses some other variable here: target/<other-var>/vartrix
+VARTRIX_BIN=$(find . -type f -name "vartrix")
+cp -a $VARTRIX_BIN ${PREFIX}/bin/vartrix
