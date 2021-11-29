78ba7d25fe367dbed6081caa066d923e2472e3dd
Vivek Rai <mail@raivivek.in>
2021-11-29 18:35:41 -0500

    ...

--- recipes/vartrix/build.sh
+++ recipes/vartrix/build.sh
@@ -1,17 +1,12 @@
 #! /bin/bash
 
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
-
-export MACOSX_DEPLOYMENT_TARGET=10.13
+export MACOSX_DEPLOYMENT_TARGET=10.13.4
+export CXXFLAGS="${CXXFLAGS} -isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOY  MENT_TARGET}"
 
 cargo build --release
 
 # try to find where binary is. it seems that the binary
 # uses some other variable here: target/<other-var>/vartrix
 VARTRIX_BIN=$(find . -type f -name "vartrix")
+echo $VARTRIX_BIN
 cp -a $VARTRIX_BIN ${PREFIX}/bin/vartrix
