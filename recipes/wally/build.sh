c0fe47070b0788aecde3ba855bffba3aa097824d
tobiasrausch <rauschtobi@gmail.com>
2021-11-30 22:49:06 +0100

    copy libGL

--- recipes/wally/build.sh
+++ recipes/wally/build.sh
@@ -2,4 +2,6 @@
 
 make all CXX=$CXX CXXFLAGS="-D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib"
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+find ${BUILD_PREFIX}/ -iname "*libGL*" -exec cp -- "{}" $PREFIX/lib/ \;
 cp src/wally $PREFIX/bin
