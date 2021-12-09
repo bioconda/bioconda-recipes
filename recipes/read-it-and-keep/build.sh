a33ea45bf4a98771d0be8140d9740407d3453d76
Devon Ryan <devon.ryan@genedata.com>
2021-12-09 13:48:33 +0100

    include dir too

--- recipes/read-it-and-keep/build.sh
+++ recipes/read-it-and-keep/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd src
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -L${PREFIX}/lib"
+make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -L${PREFIX}/lib -I${PREFIX}/include"
 mkdir -p ${PREFIX}/bin
 cp readItAndKeep ${PREFIX}/bin/
