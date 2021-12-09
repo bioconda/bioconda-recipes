e95bf7c26cf307ec68d64d8ac142d38cd59e0244
Devon Ryan <devon.ryan@genedata.com>
2021-12-09 13:35:12 +0100

    ok

--- recipes/read-it-and-keep/build.sh
+++ recipes/read-it-and-keep/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd src
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}"
+make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -L${PREFIX}/lib"
 mkdir -p ${PREFIX}/bin
 cp readItAndKeep ${PREFIX}/bin/
