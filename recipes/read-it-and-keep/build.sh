974368adccea22fa79cdf63d2e5326a4110efcb9
Devon Ryan <devon.ryan@genedata.com>
2021-12-09 12:44:30 +0100

    patch makefile

--- recipes/read-it-and-keep/build.sh
+++ recipes/read-it-and-keep/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd src
-make -I$PREFIX/ext/minimap2-2.22 -I$PREFIX/ext/gzstream -I$PREFIX/include and -L$PREFIX/lib $PREFIX/ext/minimap2-2.22/libminimap2.a $PREFIX/ext/gzstream/libgzstream.a -lz -lm
+make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}"
 mkdir -p ${PREFIX}/bin
 cp readItAndKeep ${PREFIX}/bin/
