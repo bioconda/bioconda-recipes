a8848730cb87ccab3ce942792b76f00786229a77
Devon Ryan <devon.ryan@genedata.com>
2021-12-09 14:53:51 +0100

    forgot a space

--- recipes/read-it-and-keep/build.sh
+++ recipes/read-it-and-keep/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd src
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -L${PREFIX}/lib -I${PREFIX}/include" CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -I${PREFIX}/include-I. -O"
+make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -L${PREFIX}/lib -I${PREFIX}/include" CPPFLAGS="${CPPFLAGS} -L${PREFIX}/lib -I${PREFIX}/include -I. -O"
 mkdir -p ${PREFIX}/bin
 cp readItAndKeep ${PREFIX}/bin/
