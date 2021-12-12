6599b71b0b56bdd791bb8ac7e0b8a9e85028b54b
Milot Mirdita <milot@mirdita.de>
2021-12-12 23:26:36 +0100

    Fix fasttree to build openmp-enabled binary on macOS

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -3,13 +3,10 @@
 mkdir -p $PREFIX/bin
 
 # build FastTree
-$CC $CFLAGS -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTree $SRC_DIR/FastTree-2.1.10.c -lm
-chmod +x FastTree
-cp ./FastTree $PREFIX/bin/fasttree
+$CC $CFLAGS $LDFLAGS -Wall -O3 -DUSE_DOUBLE -funroll-loops $SRC_DIR/FastTree-2.1.10.c -lm -o $PREFIX/bin/FastTree
+chmod +x $PREFIX/bin/FastTree
+cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree
 
-# Build FastTreeMP on Linux
-if [ "$(uname)" == "Linux" ]; then
-    $CC $CFLAGS -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -finline-functions -funroll-loops -Wall -o FastTreeMP $SRC_DIR/FastTree-2.1.10.c -lm
-    chmod +x FastTreeMP
-    mv -v ./FastTree* $PREFIX/bin
-fi
+# Build FastTreeMP
+$CC $CFLAGS $LDFLAGS -Wall -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -funroll-loops $SRC_DIR/FastTree-2.1.10.c -lm -o $PREFIX/bin/FastTreeMP
+chmod +x $PREFIX/bin/FastTreeMP
