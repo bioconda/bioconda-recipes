2f476f9357ca346210dcd38e4e51ed6ac45a9ce3
Milot Mirdita <milot@mirdita.de>
2021-12-15 13:41:25 +0100

    Try ignoring copy error on case-insensitive OS

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -5,7 +5,8 @@ mkdir -p $PREFIX/bin
 # build FastTree
 $CC $CFLAGS $LDFLAGS -Wall -O3 -DUSE_DOUBLE -funroll-loops $SRC_DIR/FastTree-2.1.10.c -lm -o $PREFIX/bin/FastTree
 chmod +x $PREFIX/bin/FastTree
-cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree
+# some OS are not case-sensitive (macOS, by default), ignore the copy error there
+cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || true
 
 # Build FastTreeMP
 $CC $CFLAGS $LDFLAGS -Wall -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -funroll-loops $SRC_DIR/FastTree-2.1.10.c -lm -o $PREFIX/bin/FastTreeMP
