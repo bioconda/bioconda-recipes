789d4ac59104179e32bb5ee6f4ddee8f17516456
Loraine Brillet-Guéguen <loraine-gueguen@users.noreply.github.com>
2021-12-23 16:12:31 +0100

    remove spdlog package. Remove CMAKE_CXX_FLAGS -static-libstdc++

--- recipes/salmon/0.6.0/build.sh
+++ recipes/salmon/0.6.0/build.sh
@@ -24,6 +24,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cd $SRC_DIR
     mkdir -p build
     # Use the tcmalloc installed above
+    sed -i 's#set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libstdc++")##' CMakeLists.txt
     sed -i 's#set (HAVE_FAST_MALLOC FALSE)#set (HAVE_FAST_MALLOC TRUE)\nset (FAST_MALLOC_LIB ${CMAKE_CURRENT_SOURCE_DIR}/external/install/lib/libtcmalloc.a)#' CMakeLists.txt
     sed -i 's/Boost_USE_STATIC_LIBS ON/Boost_USE_STATIC_LIBS OFF/' CMakeLists.txt
     sed -i 's#set(Boost_ADDITIONAL_VERSIONS "1.53" "1.53.0" "1.54" "1.55" "1.56" "1.57.0" "1.58")#set(Boost_ADDITIONAL_VERSIONS "1.53" "1.53.0" "1.54" "1.55" "1.56" "1.57.0" "1.58" "1.74.0")#' CMakeLists.txt
