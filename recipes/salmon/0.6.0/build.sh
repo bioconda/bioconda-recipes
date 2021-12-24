4e41f28b3e61342a3c617ee724f0d1c4ed53ea04
Loraine Brillet-Guéguen <loraine-gueguen@users.noreply.github.com>
2021-12-24 12:35:50 +0100

    try with gcc-4.8.1 (for c++11 standards)

--- recipes/salmon/0.6.0/build.sh
+++ recipes/salmon/0.6.0/build.sh
@@ -24,12 +24,13 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cd $SRC_DIR
     mkdir -p build
     # Use the tcmalloc installed above
-    sed -i 's/.\/autogen.sh/CFLAGS=-fPIC CPPFLAGS=-fPIC .\/autogen.sh/' CMakeLists.txt
     sed -i 's#set (HAVE_FAST_MALLOC FALSE)#set (HAVE_FAST_MALLOC TRUE)\nset (FAST_MALLOC_LIB ${CMAKE_CURRENT_SOURCE_DIR}/external/install/lib/libtcmalloc.a)#' CMakeLists.txt
     sed -i 's/Boost_USE_STATIC_LIBS ON/Boost_USE_STATIC_LIBS OFF/' CMakeLists.txt
     sed -i 's#set(Boost_ADDITIONAL_VERSIONS "1.53" "1.53.0" "1.54" "1.55" "1.56" "1.57.0" "1.58")#set(Boost_ADDITIONAL_VERSIONS "1.53" "1.53.0" "1.54" "1.55" "1.56" "1.57.0" "1.58" "1.74.0")#' CMakeLists.txt
     sed -i 's#BUILD_COMMAND sh -c "make CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}"#BUILD_COMMAND sh -c "make CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CFLAGS+=-I$PREFIX/include CFLAGS+=-I$PREFIX/bin"#' CMakeLists.txt
     cd build
+    export CMAKE_C_COMPILER=gcc-4.8.1
+    export CMAKE_CXX_COMPILER=g++-4.8.1
     cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBOOST_ROOT=$PREFIX -DBoost_DEBUG=ON -DBoost_NO_BOOST_CMAKE=ON ..
     make install
 fi
