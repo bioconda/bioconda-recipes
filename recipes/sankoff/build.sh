b85014222f5fae356bc0cad66950420bf5051f2d
Hadi <53904259+hforoughmand@users.noreply.github.com>
2021-12-31 16:35:43 +0330

    Sankoff (#32243)
    * update sankoff and parallel option is added
    
    * update sankoff and parallel option is added
    
    * update sankoff and parallel option is added

--- recipes/sankoff/build.sh
+++ recipes/sankoff/build.sh
@@ -6,11 +6,11 @@ mkdir -p ${PREFIX}/bin/ build/
 rm -f build/*
 
 if [ "$(uname)" == "Linux" ]; then
-${CXX} -o build/sankoff src/sankoff-no-dep.cc -std=c++11 -Wall 
+${CXX} -o build/sankoff src/sankoff.cc -std=c++11 -Wall -lboost_program_options -I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib -ltbb
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-${CXX} -o build/sankoff src/sankoff-no-dep.cc -std=c++11 -Wall 
+${CXX} -o build/sankoff src/sankoff.cc -std=c++11 -Wall -lboost_program_options -I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib -ltbb
 fi
 
 cp build/sankoff ${PREFIX}/bin/sankoff
