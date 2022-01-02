1a59a9a92813c614092faf2d1c407026cb181384
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-01-03 00:23:49 +0100

    Try one thread

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -3,7 +3,7 @@
 # to not package the openms libs and dependencies again. Same for share.
 
 pushd build
-cmake -DPYOPENMS=ON -DPY_NUM_THREADS=2 .
+cmake -DPYOPENMS=ON -DPY_NUM_THREADS=1 .
 # Unfortunately if we would call make, it thinks that OpenMS lib is not built since the build folder
 # might have been copied, so it rebuilds. Skip this by using setup.py directly.
 # TODO check that it actually uses the prebuilt libOpenMS from the prefix.
