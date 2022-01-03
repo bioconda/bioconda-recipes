656454ba37ddc61d46e7b6b96b17900704ae74d3
Charles Gregory <charles.gregory940@topper.wku.edu>
2022-01-03 14:56:03 -0500

    fixes for linux binary repackaging and hopefully fixes for building on osx

--- recipes/fade/build.sh
+++ recipes/fade/build.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# download and install parasail
-
 if [ "$(uname)" != "Darwin" ]; then
     echo "Linux: download static binary"
-    wget https://github.com/blachlylab/fade/releases/download/v0.5.3/fade
+    cd $SRC_DIR
     chmod +x ./fade
+    ./fade
+    mkdir -p $PREFIX/bin
     cp fade $PREFIX/bin
 else
     echo "MacOS: so we must build"
     echo "Downloading and installing parasail"
-    # Install parasail
+    # download and install parasail
     wget https://github.com/jeffdaily/parasail/releases/download/v2.4.3/parasail-2.4.3-Darwin-10.13.6.tar.gz
     tar -xzf parasail-2.4.3*.tar.gz
     cp -r parasail-2.4.3*/lib/* $PREFIX/lib
 
+    # download and install dub
     wget https://github.com/dlang/dub/releases/download/v1.23.0/dub-v1.23.0-osx-x86_64.tar.gz
     tar -xzf dub-v1.23.0-osx-x86_64.tar.gz
     chmod +x ./dub
+    cp dub $PREFIX/bin
 
     # build fade binary
     cd $SRC_DIR
@@ -26,12 +28,14 @@ else
     export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
     export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 
-    dub build -b release -c bioconda
-    cp ldc-1.26.0/lib/* $PREFIX/lib
+    dub build --compiler=ldc2 -b release -c bioconda
     deactivate    
 
     # run binary as test and move binary to bin
     ./fade
     cp fade $PREFIX/bin
+
+    # remove dub
+    rm $PREFIX/bin/dub
 fi
 
