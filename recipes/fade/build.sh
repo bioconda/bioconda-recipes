952acd1a3f9efbb973bfa841b0d26d1c612fe034
Charles Gregory <charles.gregory940@topper.wku.edu>
2022-01-03 13:35:42 -0500

    updated build process

--- recipes/fade/build.sh
+++ recipes/fade/build.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 
 # download and install parasail
-echo "Downloading and installing parasail"
+
 if [ "$(uname)" != "Darwin" ]; then
-    wget https://github.com/jeffdaily/parasail/releases/download/v2.4.3/parasail-2.4.3-manylinux1_x86_64.tar.gz
+    echo "Linux: download static binary"
+    wget https://github.com/blachlylab/fade/releases/download/v0.5.3/fade
+    chmod +x ./fade
+    cp fade $PREFIX/bin
 else
+    echo "MacOS: so we must build"
+    echo "Downloading and installing parasail"
+    # Install parasail
     wget https://github.com/jeffdaily/parasail/releases/download/v2.4.3/parasail-2.4.3-Darwin-10.13.6.tar.gz
+    tar -xzf parasail-2.4.3*.tar.gz
+    cp -r parasail-2.4.3*/lib/* $PREFIX/lib
+
+    wget https://github.com/dlang/dub/releases/download/v1.23.0/dub-v1.23.0-osx-x86_64.tar.gz
+    tar -xzf dub-v1.23.0-osx-x86_64.tar.gz
+    chmod +x ./dub
+
+    # build fade binary
+    cd $SRC_DIR
+    echo "building fade binary"
+
+    export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+    export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+
+    dub build -b release -c bioconda
+    cp ldc-1.26.0/lib/* $PREFIX/lib
+    deactivate    
+
+    # run binary as test and move binary to bin
+    ./fade
+    cp fade $PREFIX/bin
 fi
-tar -xzf parasail-2.4.3*.tar.gz
-cp -r parasail-2.4.3*/lib/* $PREFIX/lib
 
-# downloading D compiler
-echo "Downloading ldc2 1.26.0 D compiler"
-curl -fsS https://dlang.org/install.sh -o install.sh
-chmod +x install.sh
-DENV=$(./install.sh ldc-1.26.0 -p $PWD -a)
-
-# activating D compiler env
-. $DENV
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-
-# build fade binary
-cd $SRC_DIR
-echo "building fade binary"
-
-cp $PREFIX/lib/libhts* .
-cp $PREFIX/lib/libparasail* .
-
-dub build -b release -c bioconda
-cp ldc-1.26.0/lib/* $PREFIX/lib
-deactivate
-
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-
-# run binary as test and move binary to bin
-./fade
-cp fade $PREFIX/bin
