0ee29415e05318010e9691c25f7ae69d8f84b533
Thom Griffioen <thomgriffioen@gmail.com>
2022-01-04 12:21:45 +0100

    Update pantools to 3.3.3

--- recipes/pantools/build.sh
+++ recipes/pantools/build.sh
@@ -6,6 +6,8 @@ PACKAGE_HOME="${PREFIX}/share/${PKG_NAME}"
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PACKAGE_HOME}"
 
-cp -R dist/* "${PACKAGE_HOME}/"
+mvn package -DskipTests=true
+
+cp -R target/* "${PACKAGE_HOME}/"
 cp "${RECIPE_DIR}/pantools.py" ${PREFIX}/bin/pantools
 chmod 0755 ${PREFIX}/bin/pantools
