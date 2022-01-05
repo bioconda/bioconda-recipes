dba859af655efb31683925e8890e68263125d9a0
Igor Sfiligoi <isfiligoi@ucsd.edu>
2022-01-05 13:18:43 -0800

    Fix typo

--- recipes/unifrac-binaries/build.sh
+++ recipes/unifrac-binaries/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set +ex
+set -e
+set -x
 export PERFORMING_CONDA_BUILD=True
 export LIBRARY_PATH="${CONDA_PREFIX}/lib"
 export LD_LIBRARY_PATH=${LIBRARY_PATH}:${LD_LIBRARY_PATH}
@@ -24,7 +25,7 @@ which h5c++
 if [[ "$(uname -s)" == "Linux" ]];
 then
           # remove unused pieces to save space
-          cat install_hpc_sdk.sh |sed 's/tar xpzf/tar --exclude hpcx --exclude openmpi4 --exclude 10.2 -xpzf/g' >my_install_hpc_sdk.sh
+          cat scripts/install_hpc_sdk.sh |sed 's/tar xpzf/tar --exclude hpcx --exclude openmpi4 --exclude 10.2 -xpzf/g' >my_install_hpc_sdk.sh
           chmod a+x my_install_hpc_sdk.sh
           ./my_install_hpc_sdk.sh
           source setup_nv_h5.sh
