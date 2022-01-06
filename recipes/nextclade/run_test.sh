95289e0f9d0c30ab24289dfc7367d9f63b767cff
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-01-06 21:25:52 +0100

    fix: use correct path to sequences

--- recipes/nextclade/run_test.sh
+++ recipes/nextclade/run_test.sh
@@ -4,7 +4,7 @@ nextclade dataset get --name 'sars-cov-2' --output-dir 'data/sars-cov-2'
 
 nextclade run \
 --input-dataset 'data/sars-cov-2' \
---input-fasta 'my_sequences.fasta' \
+--input-fasta 'data/sars-cov-2/sequences.fasta' \
 --output-tsv 'output/nextclade.tsv' \
 --output-tree 'output/tree.json' \
 --output-dir 'output/'
