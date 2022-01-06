313bf55f92f30323a480d7ff03b6109ad2b1c32a
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-01-06 20:52:25 +0100

    feat: add run_test.sh

---
+++ recipes/nextclade/run_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+nextclade dataset get --name 'sars-cov-2' --output-dir 'data/sars-cov-2'
+
+nextclade run \
+--input-dataset 'data/sars-cov-2' \
+--input-fasta 'my_sequences.fasta' \
+--output-tsv 'output/nextclade.tsv' \
+--output-tree 'output/tree.json' \
+--output-dir 'output/'
