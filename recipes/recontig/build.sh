f894ec5112246bc9733bfa6641057d91689719ca
Charles Gregory <charles.gregory940@topper.wku.edu>
2022-01-06 06:48:38 -0500

    simplified build script

--- recipes/recontig/build.sh
+++ recipes/recontig/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ "$(uname)" != "Darwin" ]; then
-    echo "Linux: static binary"
-    cd $SRC_DIR
-    mkdir -p $PREFIX/bin
-    cp recontig $PREFIX/bin
-else
-    echo "MacOS: shared binary"
-
-    cd $SRC_DIR
-    mkdir -p $PREFIX/bin
-    cp recontig $PREFIX/bin
-
-fi
+cd $SRC_DIR
+mkdir -p $PREFIX/bin
+cp recontig $PREFIX/bin
