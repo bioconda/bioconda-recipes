09ae6a46d85d2e09daa0aeae1b57efe4e6961baa
Devon Ryan <dpryan79@users.noreply.github.com>
2022-01-10 12:09:30 +0100

    Update build.sh

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -2,11 +2,9 @@
 set -ex
 pushd tools/utils
 ./configure --with-htslib=${PREFIX}
-ls
 popd
 pushd tools
 make setup _utils CC=${CC}
-ls -la
 popd
 
 python -m pip install . -vv --no-deps --install-option="--minimal"
