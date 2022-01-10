1561f28c3522912059e31c64bb162e68022aac78
Devon Ryan <dpryan79@users.noreply.github.com>
2022-01-10 14:20:50 +0100

    Update build.sh

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -2,11 +2,12 @@
 set -ex
 pushd tools/utils
 ./configure --with-htslib=${PREFIX}
+make all CC=${CC} LDFLAGS="${LDFLAGS}"
 popd
-pushd tools
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-make setup _utils CC=${CC} LDFLAGS="${LDFLAGS}"
-popd
+#pushd tools
+#export C_INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
+#make setup _utils CC=${CC} LDFLAGS="${LDFLAGS}"
+#popd
 
 python -m pip install . -vv --no-deps --install-option="--minimal"
