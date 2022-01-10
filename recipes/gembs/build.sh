36b2803c4741ed9d14ce47a06b6a6be30df045f7
Devon Ryan <dpryan79@users.noreply.github.com>
2022-01-10 15:27:59 +0100

    Update build.sh

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 pushd tools/utils
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 ./configure --with-htslib=${PREFIX}
 mkdir ../bin
-make all CC=${CC} LDFLAGS="${LDFLAGS}"
+make CC="${CC}" LDFLAGS="${LDFLAGS}"
 popd
 #pushd tools
-#export C_INCLUDE_PATH=${PREFIX}/include
-#export LIBRARY_PATH=${PREFIX}/lib
 #make setup _utils CC=${CC} LDFLAGS="${LDFLAGS}"
 #popd
 
