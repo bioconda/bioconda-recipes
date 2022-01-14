da075b6ba3c0446b15aaa06615dc2f821e739f3f
Marek Dynowski <marek.dynowski@cruk.manchester.ac.uk>
2022-01-14 17:05:10 +0000

    Cleaned up the build file and trying to find out why I cannot find header files.

--- recipes/bamcmp/build.sh
+++ recipes/bamcmp/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo current working directory: `echo $SRC_DIR`
-echo working directory contents: `ls`
-echo is g++ present:
-g++ --help || true
-echo
-echo "Compiler is: " `echo $CC`
 
 echo conda_build.sh contents:
 cat /opt/conda/conda-bld/bamcmp_*/work/conda_build.sh || true
 
+
+ls -lah
+pwd
 cd bamcmp-2.1 || true
+ls -lah
+pwd 
 make CPP=$CC
+pwd
 cp build/bamcmp $PREFIX/bin
