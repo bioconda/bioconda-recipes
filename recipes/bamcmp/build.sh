53442ed8eaaca7f26e57710e7d5f998d05e2226b
Stephen Kitcatt <stephen.kitcatt@cruk.manchester.ac.uk>
2022-01-14 13:23:34 +0000

    Some quick tests to check build environment

--- recipes/bamcmp/build.sh
+++ recipes/bamcmp/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo current working directory: `pwd`
+echo working directory contents: `ls`
+echo is g++ present:
+g++ --help || true
+echo
+echo conda_build.sh contents:
+cat /opt/conda/conda-bld/bamcmp_1642086533269/work/conda_build.sh || true
+
 cd bamcmp-2.1 || true
 make
 cp build/bamcmp $PREFIX/bin
