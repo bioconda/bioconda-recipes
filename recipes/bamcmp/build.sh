365da29f7287d35de9c7580a1082458dd0a77f11
cruk-mi <93599115+cruk-mi@users.noreply.github.com>
2022-01-14 16:22:17 +0000

    Update build.sh, overriding g++ compiler
    Update build.sh and trying to override the g++ compiler

--- recipes/bamcmp/build.sh
+++ recipes/bamcmp/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo current working directory: $SRC_DIR
+echo current working directory: `echo $SRC_DIR`
 echo working directory contents: `ls`
 echo is g++ present:
 g++ --help || true
 echo
+echo "Compiler is: " `echo $CC`
+
 echo conda_build.sh contents:
 cat /opt/conda/conda-bld/bamcmp_*/work/conda_build.sh || true
 
 cd bamcmp-2.1 || true
-make
+make CPP=$CC
 cp build/bamcmp $PREFIX/bin
