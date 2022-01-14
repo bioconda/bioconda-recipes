61cb3bdfd45b8dabe06a8a5d92265fd186aba871
Michael Cormier <cormiermichaelj@gmail.com>
2022-01-14 12:25:13 -0700

    ConSplice bioconda recipe

---
+++ recipes/consplice/post-link.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eo pipefail -o nounset
+
+## Check config data
+"${PREFIX}/bin/python" -c "$( cat <<'EOF'
+import consplice
+
+cur_path = consplice.__cur_path__
+model_path = os.path.abspath(os.path.join(cur_path,'../../config/ConSpliceML_Model'))
+
+## Check ConspliceML model
+assert os.path.exists(os.path.join(model_path,'trained_ConSpliceML.rf'))
+assert os.path.isfile(os.path.join(model_path,'trained_ConSpliceML.rf'))
+assert os.path.getsize(os.path.join(model_path,'trained_ConSpliceML.rf')) > 0
+
+## Check ConSpliceML metadata
+assert os.path.exists(os.path.join(model_path,'training.yaml'))
+assert os.path.isfile(os.path.join(model_path,'training.yaml'))
+assert os.path.getsize(os.path.join(model_path,'training.yaml')) > 0
+
+## Check ConSplice config
+assert os.path.exists(os.path.join(cur_path,'../../config/config.yml'))
+assert os.path.isfile(os.path.join(cur_path,'../../config/config.yml'))
+assert os.path.getsize(os.path.join(cur_path,'../../config/config.yml')) > 0
+
+print('Config checks passed')
+
+EOF
+)" >> "${PREFIX}/.messages.txt" 2>&1
+
