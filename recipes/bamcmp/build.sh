5ce24f4ccd39f53ff26a13d8ba0daaed6007b905
Stephen Kitcatt <stephen.kitcatt@cruk.manchester.ac.uk>
2022-01-17 14:31:03 +0000

    Updating htslib location

--- recipes/bamcmp/build.sh
+++ recipes/bamcmp/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-echo Contents of /usr/local
-ls -lah /usr/local || true
-echo
-
-echo Searching for sam.h file:
-find / -name sam.h || true
-echo
 
 cd bamcmp-2.1 || true
 make CPP=$CC
