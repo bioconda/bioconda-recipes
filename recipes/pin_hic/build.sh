a8f7b4a6eb2bafba4bcf19685c60c6fed699f44c
Devon Ryan <dpryan79@users.noreply.github.com>
2022-01-18 21:18:31 +0100

    Guess that wasn't part of the release

--- recipes/pin_hic/build.sh
+++ recipes/pin_hic/build.sh
@@ -3,7 +3,4 @@ mkdir bin
 pushd src
 make CC=$CC CFLAGS="$CFLAGS -O2 -Wall -D VERBOSE -D PRINT_COVERAGE" LDFLAGS="$LDFLAGS -lz -lm"
 popd
-pushd util
-make CC=$CC CFLAGS="$CFLAGS -O2 -Wall -D VERBOSE -D PRINT_COVERAGE" LDFLAGS="$LDFLAGS -lz -lm"
-popd
 mv bin/* $PREFIX/bin/
