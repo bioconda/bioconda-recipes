87e2ba7fa41dc6d24514e6703dfa578164008425
Devon Ryan <dpryan79@gmail.com>
2022-01-18 21:34:19 +0100

    more perl updates

--- recipes/rnaz/build.sh
+++ recipes/rnaz/build.sh
@@ -6,5 +6,5 @@ make -j
 make install
 
 mv $PREFIX/share/RNAz/perl/*.pl $PREFIX/bin/.
-mkdir -p $PREFIX/lib/5.32/
-mv $PREFIX/share/RNAz/perl/RNAz.pm $PREFIX/lib/5.32/.
+mkdir -p $PREFIX/lib/perl5/site_perl/
+mv $PREFIX/share/RNAz/perl/RNAz.pm $PREFIX/lib/perl5/site_perl/.
