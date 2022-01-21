4768c5d8f10f8f2dc2f47c6b80370d7feb1ba460
Sergey Koren <sergek@umd.edu>
2022-01-20 20:01:38 -0500

    reduce test memory usage

--- recipes/verkko/run_test.sh
+++ recipes/verkko/run_test.sh
@@ -7,7 +7,7 @@ set -eu -o pipefail
 rm -f ./hifi.fastq.gz ./ont.fastq.gz
 curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_hifi_subset24x.fastq.gz -o hifi.fastq.gz
 curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_ont_subset50x.fastq.gz -o ont.fastq.gz
-verkko -d asm --hifi ./hifi.fastq.gz --nano ./ont.fastq.gz
+verkko -d asm --hifi ./hifi.fastq.gz --nano ./ont.fastq.gz --snakeopts "--resources mem_gb=2" --ovb-run 2 2 1 --red-run 2 2 1 --sto-run 2 2 1
 
 if [ ! -s asm/7-consensus/unitig-popped.fasta ]; then
    echo "Error: verkko assembly test failed!"
