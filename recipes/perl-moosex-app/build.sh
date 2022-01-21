37b0f1d0df988f9795fcb9b7d08e34a6b9360b8f
Devon Ryan <devon.ryan@genedata.com>
2022-01-21 13:24:33 +0100

    Updates, I fear a few of these can't be easily built

--- recipes/perl-moosex-app/build.sh
+++ recipes/perl-moosex-app/build.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+# If it has Build.PL use that, otherwise use Makefile.PL
 if [ "$(uname)" == "Darwin" ]; then
-    HOME=/tmp cpanm  Text::WagnerFischer
     HOME=/tmp cpanm --installdeps .
+    HOME=/tmp cpanm  Text::WagnerFischer
+    HOME=/tmp cpanm  Devel::StackTrace 
 else
-    cpanm  Text::WagnerFischer
     cpanm --installdeps .
+    cpanm  Text::WagnerFischer
+    cpanm  Devel::StackTrace 
 fi
 
-
-# If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    #./Build test
+    perl ./Build
+    perl ./Build test
     # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
-    #make test
+    make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
