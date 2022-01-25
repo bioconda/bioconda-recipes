488bb4ebdc04248346a792abf9f6f0760aac3e95
Devon Ryan <devon.ryan@genedata.com>
2022-01-25 16:56:38 +0100

    fix perl-html-tidyp

--- recipes/perl-html-tidy/build.sh
+++ recipes/perl-html-tidy/build.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# ld on ppc64le is sensitive to the order in which "-l<lib>" arguments are
-# presented, so we have to munge the generated Makefile a little so libtidyp.so
-# is properly linked when creating "Tidy.so".
-cat >fix-link-order.patch <<'EOF'
---- Makefile
-+++ Makefile
-@@ -482,7 +482,7 @@
-
- $(INST_DYNAMIC): $(OBJECT) $(MYEXTLIB) $(INST_ARCHAUTODIR)$(DFSEP).exists $(EXPORT_LIST) $(PERL_ARCHIVEDEP) $(PERL_ARCHIVE_AFTER) $(INST_DYNAMIC_DEP)
- 	$(RM_F) $@
--	$(LD)  $(LDDLFLAGS) $(LDFROM) $(OTHERLDFLAGS) -o $@ $(MYEXTLIB)	\
-+	$(LD) $(LDFROM) $(LDDLFLAGS) $(OTHERLDFLAGS) -o $@ $(MYEXTLIB)	\
- 	  $(PERL_ARCHIVE) $(LDLOADLIBS) $(PERL_ARCHIVE_AFTER) $(EXPORT_LIST)	\
- 	  $(INST_DYNAMIC_FIX)
- 	$(CHMOD) $(PERM_RWX) $@
-EOF
-
-export LD_LIBRARY_PATH="$PREFIX/lib"
-
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
@@ -29,7 +10,8 @@ if [ -f Build.PL ]; then
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site INC="-I${PREFIX}/include/tidyp" LIBS="-L${PREFIX}/lib"
-    patch -p0 < fix-link-order.patch
+    # By default the linked .so files are listed first, which obviously doesn't work for linking against tidyp.so
+    sed -i.bak -e "s/\$(LDDLFLAGS)  \$(LDFROM)/\$(LDFROM) \$(LDDLFLAGS)/g" Makefile
     make
     make test
     make install
