5f55e9e6183e621544b098fb212c88e4ac256e9c
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-01-26 11:13:06 -0600

    Mend what was broken

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -ex
-pushd tools/utils
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-./configure
-mkdir ../bin
-make CC="${CC}" LDFLAGS="${LDFLAGS}"
+pushd tools
+make
 popd
 #pushd tools
 #make setup _utils CC=${CC} LDFLAGS="${LDFLAGS}"
