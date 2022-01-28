aa9e3112a77782d567a4304fd8496879e90f7777
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-01-28 13:06:51 -0600

    Best of both worlds

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -3,7 +3,7 @@ set -ex
 pushd tools
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make CC="${CC}" LDFLAGS="${LDFLAGS}"
+make
 popd
 #pushd tools
 #make setup _utils CC=${CC} LDFLAGS="${LDFLAGS}"
