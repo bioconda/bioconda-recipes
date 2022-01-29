df9e1f38b9b008585286dc8068b4f9d2e66e9c0a
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-01-29 16:11:08 -0600

    Old dawg, new tricks

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -3,7 +3,13 @@ set -ex
 pushd tools
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make CC="${CC}" LDFLAGS="${LDFLAGS}"
+if [[ ${target_platform} =~ linux.* ]] ; then
+    # Workaround for glibc<2.17 where clock_gettime is in librt. (clock_time being used by bs_call.)
+    make -MAKE_BUILD_TYPE=Release -MAKE_INSTALL_PREFIX="${PREFIX}" -MAKE_EXE_LINKER_FLAGS_INIT=-lrt ..
+else
+    make -MAKE_BUILD_TYPE=Release -MAKE_INSTALL_PREFIX="${PREFIX}" ..
+fi
+make CC="${CC}"
 popd
 #pushd tools
 #make setup _utils CC=${CC} LDFLAGS="${LDFLAGS}"
