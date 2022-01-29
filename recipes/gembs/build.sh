b2166bf3a70eecec6319c097c4a75a12e1de2e4a
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-01-29 16:57:01 -0600

    Syntactically challenged

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -5,9 +5,9 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 if [[ ${target_platform} =~ linux.* ]] ; then
     # Workaround for glibc<2.17 where clock_gettime is in librt. (clock_time being used by bs_call.)
-    make -MAKE_BUILD_TYPE=Release -MAKE_INSTALL_PREFIX="${PREFIX}" -MAKE_EXE_LINKER_FLAGS_INIT=-lrt ..
+    make MAKE_BUILD_TYPE=Release MAKE_INSTALL_PREFIX="${PREFIX}" MAKE_EXE_LINKER_FLAGS_INIT=-lrt ..
 else
-    make -MAKE_BUILD_TYPE=Release -MAKE_INSTALL_PREFIX="${PREFIX}" ..
+    make MAKE_BUILD_TYPE=Release MAKE_INSTALL_PREFIX="${PREFIX}" ..
 fi
 make CC="${CC}"
 popd
