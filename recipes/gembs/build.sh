9d7f48e899133b701ccba26908580e96967fcf60
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-01-30 16:10:57 -0600

    More than one way to skin this cat

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -3,11 +3,11 @@ set -ex
 pushd tools
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-if [[ "${target_platform}" =~ "linux-64" ]]; then
+if [[ "${target_platform}" =~ linux* ]]; then
     # Workaround for glibc<2.17 where clock_gettime is in librt. (clock_time being used by bs_call.)
-    make BS_CALL_LIBS:=$(LIBS) -lpthread -lrt
+    sed -i 's/BS_CALL_LIBS:= \$(LIBS) -lpthread/BS_CALL_LIBS:= \$(LIBS) -lpthread -lrt/' bs_call/src/Makefile
 else
-    make BS_CALL_LIBS:=$(LIBS) -lpthread
+    echo Nothing to see here move along
 fi
 make CC="${CC}" LDFLAGS="${LDFLAGS}"
 popd
