261d6bc786b24dda454d9a0e926f1bb8a745e42f
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-01-31 07:20:29 -0600

    gemBS troubleshooting (#32614)
    * bump/gembs
    
    An offering to our science overlords.
    
    * Why do I hope?
    
    * Fools rush in
    
    * This will work I swear
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Please work patchless
    
    * patch between v3.5.1 and v3.5.5
    
    This seems too complicated to work.
    
    * Lesson learned
    
    * taking a step back
    
    * increment build number
    
    * Starting over
    
    * Update meta.yaml
    
    * Starting over from here
    
    * I lied, starting over from here final time
    
    * Just call me "Patch" Adams
    
    * I'm a quack doctor
    
    * Why did the duck get arrested?
    
    * For selling quack
    
    * Patching manually is dumb
    
    This is why you should take computer science in college kids. Don't be like me.
    
    * If this works I should go buy a lotto ticket
    
    * Mend what was broken
    
    * Increment build
    
    * rule to make target gem3-mapper/Makefile.mk
    
    * Look mom, no patches!
    
    * New sha256, who dis?
    
    * Last try for the night
    
    * Reset with morning coffee and coding
    
    * zlib.h fix first try
    
    * Final touches (I hope)
    
    * Fetch-a-file-o-matic
    
    * Best of both worlds
    
    * Little by little
    
    * Make it stop
    
    * Put back what I took out
    
    * Can our past teach us anything?
    
    * First patch of many
    
    * Update meta.yaml
    
    * The triumph of the individual over art
    
    * Time isn't real
    
    * Pushing words around
    
    * Mac users are people too I guess
    
    * Glibc you dog
    
    * Mac attack
    
    * Whatever, I'll fix you later
    
    * Snowballs chance
    
    * Hail Mary pass
    
    * Look the other way
    
    * Reset meta.yaml
    
    * Old dawg, new tricks
    
    * Syntactically challenged
    
    * Penultimate patching
    
    * Workaround for glibc<2.17
    
    * Update patch to remove variable
    
    * Make it work
    
    * Crash rules everything around me CREAM
    
    * I give up for today.
    
    * Fresh eyes in a house made of windows
    
    * Moving conditionality to Makefile patch
    
    * Testing out why this isn't working
    
    * I have you now
    
    * Boss fight
    
    * Push workaround back to the shell script
    
    * Respawn
    
    * Ding dong the witch is dead
    
    * Which old witch? The wicked witch
    
    * You can't run forever
    
    * Rinse and repeat
    
    * More than one way to skin this cat
    
    * Epilogue
    
    * Update meta.yaml
    
    * Return of the patch
    
    * herculean effort
    
    * Testing patch in parts
    
    Co-authored-by: Devon Ryan <dpryan79@users.noreply.github.com>

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -ex
-pushd tools/utils
+pushd tools
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-./configure
-mkdir ../bin
+if [[ "${target_platform}" =~ linux* ]]; then
+    # Workaround for glibc<2.17 where clock_gettime is in librt. (clock_time being used by bs_call.)
+    sed -i 's/BS_CALL_LIBS:= \$(LIBS) -lpthread/BS_CALL_LIBS:= \$(LIBS) -lpthread -lrt/' bs_call/src/Makefile
+else
+    echo Nothing to see here move along
+fi
 make CC="${CC}" LDFLAGS="${LDFLAGS}"
 popd
 #pushd tools
