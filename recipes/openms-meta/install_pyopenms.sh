4c7e4f62afa2848cddde7e27955b27b01be52b8f
jpfeuffer <jpfeuffer@fu-berlin.de>
2022-02-01 13:12:19 +0100

    adapt nightlies to 2.8 dev cycle

--- recipes/openms-meta/install_pyopenms.sh
+++ recipes/openms-meta/install_pyopenms.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # TODO actually it would be better if we could adapt our MANIFEST.in
 # to not package the openms libs and dependencies again. Same for share.
-
 pushd build
-cmake -DPYOPENMS=ON -DPY_NUM_THREADS=1 -DPY_NUM_MODULES=12 .
+cmake -DPYOPENMS=ON . #-DPY_NUM_THREADS=1 -DPY_NUM_MODULES=12 .
 # Unfortunately if we would call make, it thinks that OpenMS lib is not built since the build folder
 # might have been copied, so it rebuilds. Skip this by using setup.py directly.
 # TODO check that it actually uses the prebuilt libOpenMS from the prefix.
@@ -11,7 +10,7 @@ cmake -DPYOPENMS=ON -DPY_NUM_THREADS=1 -DPY_NUM_MODULES=12 .
 pushd pyOpenMS
 $PYTHON create_cpp_extension.py
 # try to separate the extension building from the rest. Let's hope pip install is smart enough to not do it again.
-$PYTHON setup.py build_ext -j 1 --single-threaded
+$PYTHON setup.py build_ext #-j 1 --single-threaded
 $PYTHON -m pip install . --ignore-installed --no-deps
 popd
 popd
