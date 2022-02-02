537d585cb4c7f64bbd8d2e392afbc58bd041ecc7
Guy LEonard <guy.leonard@gmail.com>
2022-02-02 15:33:27 +0000

    remove harcoded g++ but rely on system CC and CXX

--- recipes/gloome/build.sh
+++ recipes/gloome/build.sh
@@ -4,11 +4,11 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 cd libs/phylogeny
-#sed -i.bak 's/CXX=g++//' Makefile
+sed -i.bak 's/CXX=g++//' Makefile
 make #CXX=$CXX CC=$CC
 
 cd ../../programs/gainLoss
-#sed -i.bak 's/CXX=g++//' ../Makefile.generic
+sed -i.bak 's/CXX=g++//' ../Makefile.generic
 make #CXX=$CXX CC=$CC
 
 mkdir -p $PREFIX/bin
