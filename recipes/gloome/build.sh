91d8b6ae0cb2d18f14abc839cbeffca38f9e2ef4
Guy LEonard <guy.leonard@gmail.com>
2022-02-02 14:56:01 +0000

    removing gcc from yaml and reverting changes in build seem to execute okay

--- recipes/gloome/build.sh
+++ recipes/gloome/build.sh
@@ -4,12 +4,12 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 cd libs/phylogeny
-sed -i.bak 's/CXX=g++//' Makefile
-make CXX=$CXX CC=$CC
+#sed -i.bak 's/CXX=g++//' Makefile
+make #CXX=$CXX CC=$CC
 
 cd ../../programs/gainLoss
-sed -i.bak 's/CXX=g++//' ../Makefile.generic
-make CXX=$CXX CC=$CC
+#sed -i.bak 's/CXX=g++//' ../Makefile.generic
+make #CXX=$CXX CC=$CC
 
 mkdir -p $PREFIX/bin
 cp gainLoss $PREFIX/bin
