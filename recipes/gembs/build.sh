89f2266f0f6008bd4f1404a94dbfaa794b542296
Jake Lehle <84940857+JakeLehle@users.noreply.github.com>
2022-02-01 21:48:18 -0600

    Will this build v3.0.0 or v3.2.0?

--- recipes/gembs/build.sh
+++ recipes/gembs/build.sh
@@ -12,7 +12,5 @@ fi
 make CC="${CC}" LDFLAGS="${LDFLAGS}"
 popd
 #python -m pip install . -vv --no-deps --install-option="--minimal"
-python -m pip install . -vv --no-deps
-wget http://statgen.cnag.cat/gemBS/v3/UserGuide/downloads/gemBS_example.tar.gz
-tar -zxvf gemBS_example.tar.gz
-pushd gemBS_example
+#python -m pip install . -vv --no-deps
+python3 setup.py install --single-version-externally-managed --record=record.txt
