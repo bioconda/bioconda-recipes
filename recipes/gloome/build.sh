97d73c510634eef0b669eeb27ff8a2ca9d66bfbe
Guy LEonard <guy.leonard@gmail.com>
2022-02-02 11:18:00 +0000

    modifying make as per https://bioconda.github.io/contributor/troubleshooting.html#g-or-gcc-not-found

--- recipes/gloome/build.sh
+++ recipes/gloome/build.sh
@@ -4,10 +4,10 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 cd libs/phylogeny
-make CC=$CC
+make CC=$CXX
 
 cd ../../programs/gainLoss
-make CC=$CC
+make CC=$CXX
 
 mkdir -p $PREFIX/bin
 cp gainLoss $PREFIX/bin
