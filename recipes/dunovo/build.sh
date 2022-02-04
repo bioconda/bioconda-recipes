643d6eb412ddd1498712f1fd37d2c3ae336224d1
Nick <nmapsy@gmail.com>
2022-02-04 17:31:21 -0500

    Du Novo: Initial version of 3.0 support.

--- recipes/dunovo/build.sh
+++ recipes/dunovo/build.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
 
 # Download submodules and move them to lib.
-get_submodule () {
-  read name version owner repo hash <<< "$@"
+
+cat > submodules.txt <<EOF
+kalign  0.3.0       makovalab-psu kalign-dunovo c0ef2de4a958aed47311ea86591debb3bada871143a6923bc6338ab2f99f2d5b
+utillib 0.1.1-alpha NickSto       utillib       961f5a3481d1c0dbe00c258b9df2de541e5605f3de4ff25bcc3cec22922e7c06
+ET      0.3         NickSto       ET            6b757b3ab3634b949f78692ac0db72e9264a1dceaf7449e921091d3ad0012eea
+bfx     0.5.0       NickSto       bfx           f5cc5c9c8a627a082f8b505135aec10b47c09647269ad92abe3a1fa2f2059048
+EOF
+
+while read name version owner repo hash; do
+  echo "Downloading $name v$version from $owner/$repo"
   wget --no-check-certificate "https://github.com/$owner/$repo/archive/v$version.tar.gz"
   downloaded_hash=`sha256sum "v$version.tar.gz" | tr -s ' ' | cut -d ' ' -f 1`
   if ! [ "$hash" == "$downloaded_hash" ]; then
@@ -11,32 +18,24 @@ get_submodule () {
   fi
   tar -zxvpf "v$version.tar.gz"
   rm -rf "$name"
-  if [ "$name" == kalign ]; then
-    mv "$repo-$version" "$name"
-  else
-    mv "$repo-$version" "$PREFIX/lib/$name"
-  fi
+  mv "$repo-$version" "$name"
   rm "v$version.tar.gz"
-}
-get_submodule kalign  0.2.0 makovalab-psu kalign-dunovo 473dd562f520a218df2dd147c89940422344adad6d8824141bffe6466c6d40e7
-get_submodule utillib 0.1.0 NickSto       utillib       bffe515f7bd98661657c26003c41c1224f405c3a36ddabf5bf961fab86f9651a
-get_submodule ET      0.2.2 NickSto       ET            11dc5cb02521a2260e6c88a83d489c72f819bd759aeff31d66aa40ca2f1358a6
-get_submodule bfx     0.2.0 NickSto       bfx           252d31dc260882f203d04624945c8abb4940f3ae4b03a5050182d23854488ef8
-
-# Inject compilers
-sed -i.bak 's#gcc#${CC}#g' Makefile
+done < submodules.txt
 
 # Compile binaries and move them to lib.
-make CC=$CC
+make
+mkdir -p "$PREFIX/lib"
 mv *.so "$PREFIX/lib"
-mv kalign "$PREFIX/lib"
+while read name rest; do
+  mv "$name" "$PREFIX/lib/$name"
+done < submodules.txt
 
 # Move scripts to lib and link to them from bin.
+mkdir -p "$PREFIX/bin"
 for script in *.awk *.sh *.py; do
   mv "$script" "$PREFIX/lib"
-  ln -s "../lib/$script" "$PREFIX/bin"
+  ln -s "../lib/$script" "$PREFIX/bin/$script"
 done
-
 # Handle special cases.
 mv utils/precheck.py "$PREFIX/lib"
 ln -s ../lib/precheck.py "$PREFIX/bin"
