e40e9f433e6f3e797581ee656339da40a8831979
maxibor <maxime.borry@gmail.com>
2022-02-04 18:44:43 +0100

    add zlib dep

--- recipes/sdust/build.sh
+++ recipes/sdust/build.sh
@@ -1,5 +1,8 @@
-cd $SRC_DIR
-
-make
+#!/bin/bash
 mkdir -p $PREFIX/bin
+
+export CPATH=${PREFIX}/include
+
+make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat" sdust
+
 cp $SRC_DIR/sdust $PREFIX/bin
