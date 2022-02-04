6a8f742fad3a7ead7fe91b4f0182863fb7acbe2d
James Fellows Yates <jfy133@gmail.com>
2022-02-04 16:07:23 +0100

    Get recipe working. Saved by @dpryan79... ONCE AGAIN <3

--- recipes/atlas/build.sh
+++ recipes/atlas/build.sh
@@ -2,7 +2,9 @@
 
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CPATH="${PREFIX}/include"
 
 sed -i 's/^GIT_HEADER/#GIT_HEADER/' makefile
 
