01801169126e162a3ea5d8a7b7b01a9381b04b36
Zilong-Li <zimusen94@gmail.com>
2022-02-07 23:18:58 +0100

    Update build.sh

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -7,6 +7,6 @@ cp PCAone ${PREFIX}/bin
 export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 
 # put runtime lib into user's env
-[ -f $HOME/.bashrc ] && echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$PREFIX/lib\"" >> $HOME/.bashrc
-[ -f $HOME/.zshrc ] && echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$PREFIX/lib\"" >> $HOME/.zshrc
+# [ -f $HOME/.bashrc ] && echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$PREFIX/lib\"" >> $HOME/.bashrc
+# [ -f $HOME/.zshrc ] && echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$PREFIX/lib\"" >> $HOME/.zshrc
 
