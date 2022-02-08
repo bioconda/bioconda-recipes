c8bca051b0a80e27ce686ad6ae726a724f028bff
Zilong-Li <zimusen94@gmail.com>
2022-02-08 08:56:29 +0100

    add rpath for mac

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -5,6 +5,10 @@ cp PCAone ${PREFIX}/bin
 
 # Set up build environment
 export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+# on mac, add conda lib to @rpath
+if [ "$(uname)" == "Darwin" ]; then
+    install_name_tool -add_rpath ${PREFIX}/lib PCAone
+fi
 
 # put runtime lib into user's env
 # [ -f $HOME/.bashrc ] && echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$PREFIX/lib\"" >> $HOME/.bashrc
