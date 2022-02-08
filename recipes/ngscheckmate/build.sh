957dc8c195bd20422aef68c325b8bcff312b431f
Stephen Kitcatt <stephen.kitcatt@cruk.manchester.ac.uk>
2022-02-08 01:40:02 +0000

    Added ncm_fastq.py to PATH, bumped NGSCheckMate version to latest commit, added activate and deactivate scripts to build.sh

--- recipes/ngscheckmate/build.sh
+++ recipes/ngscheckmate/build.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
 cd $PREFIX
-mv $SRC_DIR $PREFIX/NGSCheckMate-1.0.0
+mv $SRC_DIR $PREFIX/NGSCheckMate
 
 mkdir -p $PREFIX/bin
 cat << EOF > $PREFIX/bin/ncm.py
 #!/usr/bin/env bash
 
-python /usr/local/NGSCheckMate-1.0.0/ncm.py "\$@"
+python $PREFIX/NGSCheckMate/ncm.py "\$@"
+EOF
+
+cat << EOF > $PREFIX/bin/ncm_fastq.py
+#!/usr/bin/env bash
+
+python $PREFIX/NGSCheckMate/ncm_fastq.py "\$@"
+EOF
+
+mkdir -p $PREFIX/etc/conda/activate.d
+cat << EOF > $PREFIX/etc/conda/activate.d/activate.sh
+#!/usr/bin/env bash
+
+NCM_HOME=$PREFIX/NGSCheckMate
+EOF
+
+mkdir -p $PREFIX/etc/conda/deactivate.d
+cat << EOF > $PREFIX/etc/conda/deactivate.d/deactivate.sh
+#!/usr/bin/env bash
+
+NCM_HOME=
 EOF
