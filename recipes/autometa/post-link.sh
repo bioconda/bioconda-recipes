0179e74cab6b8bcdf956fed80ecc9673a5e9be04
Evan Rees <erees@wisc.edu>
2022-02-08 16:29:21 -0600

    :art: first attempt with all run requirements

--- recipes/autometa/post-link.sh
+++ recipes/autometa/post-link.sh
@@ -7,5 +7,11 @@ echo "
 #   For more details on how to do this see:                                     #
 #   https://autometa.readthedocs.io/en/latest/databases.html                    #
 #                                                                               #
+#   To run autometa-kmers you will also need to run (in your autometa env):     #
+#                                                                               #
+#   cmd:                                                                        #
+#                                                                               #
+#       python -m pip install trimap                                            #
+#                                                                               #
 #################################################################################
 "
