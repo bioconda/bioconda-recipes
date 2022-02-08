07b19f6e0b07804936dfe1e164ee09fbc5235f76
Wouter De Coster <decosterwouter@gmail.com>
2022-02-08 10:16:16 +0100

    Update sniffles to sniffles2 (#32811)
    * updating to sniffles2
    
    * adding python to run instructions

--- recipes/sniffles/build.sh
+++ recipes/sniffles/build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=${PREFIX} -DSTATIC=OFF -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib ..
-make VERBOSE=1
-
-mkdir -p $PREFIX/bin
-cp ../bin/sniffles-core-${PKG_VERSION}/sniffles* $PREFIX/bin/
-
+$PYTHON setup.py install  --single-version-externally-managed --record=record.txt
