ae60cb898d244b10b3c72fd6db6a40972ad789a6
Evan Rees <erees@wisc.edu>
2022-02-08 13:30:23 -0600

    Add autometa recipe

---
+++ recipes/autometa/post-link.sh
@@ -0,0 +1,11 @@
+echo "
+#################################################################################
+#                                                                               #
+#   Note: To use all of Autometa's features you will need to configure          #
+#   it with a few databases (NCBI and marker databases)                         #
+#                                                                               #
+#   For more details on how to do this see:                                     #
+#   https://autometa.readthedocs.io/en/latest/databases.html                    #
+#                                                                               #
+#################################################################################
+"
