67cff34a77b88abb38fba570e3abcbe2d5c55580
Evan Rees <erees@wisc.edu>
2022-02-09 08:51:15 -0600

    :fire: update post-link message and add setuptools to host requirements

--- recipes/autometa/post-link.sh
+++ recipes/autometa/post-link.sh
@@ -1,17 +1,12 @@
 echo "
 #################################################################################
+# Note:                                                                         #
 #                                                                               #
-#   Note: To use all of Autometa's features you will need to configure          #
-#   it with a few databases (NCBI and marker databases)                         #
+# To use all of Autometa's features you will need to configure a few databases  #
 #                                                                               #
-#   For more details on how to do this see:                                     #
+# For more details on how to do this see:                                       #
+#                                                                               #
 #   https://autometa.readthedocs.io/en/latest/databases.html                    #
 #                                                                               #
-#   To run autometa-kmers you will also need to run (in your autometa env):     #
-#                                                                               #
-#   cmd:                                                                        #
-#                                                                               #
-#       python -m pip install trimap                                            #
-#                                                                               #
 #################################################################################
-"
+" >> ${PREFIX}/.messages.txt
