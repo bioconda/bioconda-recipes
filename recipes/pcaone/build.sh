7d5089f2bdac8418b3218061c09c4dd70021f39d
Zilong-Li <zimusen94@gmail.com>
2022-02-13 13:59:16 +0100

    update

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -7,12 +7,10 @@ if [ "$(uname)" == "Darwin" ]; then
     # install_name_tool -add_rpath ${PREFIX}/lib ${PREFIX}/bin/PCAone
 else 
 # Set up build environment
-    export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
     export C_INCLUDE_PATH="${PREFIX}/include:${C_INCLUDE_PATH}"
     export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
-    export CFLAGS="$CFLAGS -I$PREFIX/include"
-    export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-    awk -v dir=${PREFIX} 'NR==7 {$0="MKLROOT="dir}; NR==41 {$0="LPATHS=-L"dir"/lib"}; 1' Makefile > linux.makefile && make -f linux.makefile && cp PCAone ${PREFIX}/bin
+    export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+    awk -v dir=$PREFIX 'NR==7 {$0="MKLROOT = "dir}; NR==41 {$0="LPATHS = -L"dir"/lib"}; NR==68 {$0="DLIBS += -Wl,-rpath,${MKLROOT}/lib -lz -lzstd"}; 1' Makefile > linux.makefile && make -f linux.makefile && cp PCAone ${PREFIX}/bin
 fi
 
 # put runtime lib into user's env
