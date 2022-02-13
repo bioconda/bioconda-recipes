fe7e52ba899fc5088008a4f4cb20872b434ade21
Devon Ryan <dpryan79@gmail.com>
2022-02-13 10:06:53 +0100

    oops

--- recipes/spades/build.sh
+++ recipes/spades/build.sh
@@ -10,12 +10,10 @@ export CXXFLAGS="${CFLAGS} -fcommon"
 
 # Fails on OSX
 #bash spades_compile.sh -j 4
-BUILD_DIR=build_spades
-BASEDIR="$(normalize_path "$(pwd)"/"$(dirname "$0")")"
-WORK_DIR="$BASEDIR/$BUILD_DIR"
-mkdir -p "$WORK_DIR"
+WORK_DIR="build_spades"
+mkdir -p $WORK_DIR
 
 cd "$WORK_DIR"
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DSPADES_BUILD_INTERNAL=OFF "$BASEDIR/src"
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DSPADES_BUILD_INTERNAL=OFF ../src
 make -j 4
 make install
