4e18761009294083753c9bb7d3b8e0a65409f385
Zilong-Li <zimusen94@gmail.com>
2022-02-13 10:41:22 +0100

    update

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -7,9 +7,9 @@ if [ "$(uname)" == "Darwin" ]; then
     # install_name_tool -add_rpath ${PREFIX}/lib ${PREFIX}/bin/PCAone
 else 
 # Set up build environment
-    # export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-    # export C_INCLUDE_PATH="${PREFIX}/include:${C_INCLUDE_PATH}"
-    # export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
+    export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+    export C_INCLUDE_PATH="${PREFIX}/include:${C_INCLUDE_PATH}"
+    export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
     export CFLAGS="$CFLAGS -I$PREFIX/include"
     export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
     awk -v dir=${PREFIX} 'NR==7 {$0="MKLROOT="dir}; NR==41 {$0="LPATHS=-L"dir"/lib"}; 1' Makefile > linux.makefile && make -f linux.makefile && cp PCAone ${PREFIX}/bin
