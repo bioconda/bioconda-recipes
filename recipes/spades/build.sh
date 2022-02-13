fe29bce90acd6dc73b0514c8b095cd6f02594356
Devon Ryan <dpryan79@gmail.com>
2022-02-13 09:37:05 +0100

    just run cmake

--- recipes/spades/build.sh
+++ recipes/spades/build.sh
@@ -8,4 +8,14 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CFLAGS} -fcommon"
 
-bash spades_compile.sh -j 4
+# Fails on OSX
+#bash spades_compile.sh -j 4
+BUILD_DIR=build_spades
+BASEDIR="$(normalize_path "$(pwd)"/"$(dirname "$0")")"
+WORK_DIR="$BASEDIR/$BUILD_DIR"
+mkdir -p "$WORK_DIR"
+
+cd "$WORK_DIR"
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DSPADES_BUILD_INTERNAL=OFF "$BASEDIR/src"
+make -j 4
+make install
