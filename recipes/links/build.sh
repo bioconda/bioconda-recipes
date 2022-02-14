6a2def71a4b92b4c02339b6b573f98c710499ae4
Tom Harrop <twharrop@gmail.com>
2022-02-15 10:12:55 +1100

    tidy build script

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -eux -o pipefail
 
-./configure && make && make install
-
-echo "#!/bin/bash" > ${PREFIX}/bin/LINKS-make
-echo "make -f $(command -v ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/LINKS-make) \$@" >> ${PREFIX}/bin/LINKS-make
+ ./configure --prefix=${PREFIX}
+make
+make install
