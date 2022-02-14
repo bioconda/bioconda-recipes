226f03f7e341acbcf6b94570be992837f02aa553
Tom Harrop <twharrop@gmail.com>
2022-02-15 09:27:03 +1100

    add perl, use make install

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 set -eux -o pipefail
 
-./configure && make
-
-mkdir -p ${PREFIX}/bin/
-mkdir -p ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin
-mkdir -p ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/src
-
-cp src/LINKS_CPP ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/src
-cp bin/* ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin
-cp bin/LINKS* ${PREFIX}/bin/
-
+./configure && make && make install
 
 echo "#!/bin/bash" > ${PREFIX}/bin/LINKS-make
 echo "make -f $(command -v ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/LINKS-make) \$@" >> ${PREFIX}/bin/LINKS-make
