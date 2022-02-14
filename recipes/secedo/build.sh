b2963f16766362bfe022e16625b156e056e02699
Daniel Danciu <dd@daedalean.ai>
2022-02-14 18:41:20 +0100

    Initial packaging for secedo

---
+++ recipes/secedo/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -i.bak '14i\
+cmake_policy(SET CMP0042 NEW)' CMakeLists.txt
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
+mkdir -p build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+      -DZLIB_ROOT=$PREFIX \
+      ..
+make install
