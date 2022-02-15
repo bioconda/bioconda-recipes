0d8dcf7e6fc4d01294fde5d1872d598e8cb7768e
Tom Harrop <twharrop@gmail.com>
2022-02-15 13:45:08 +1100

    deal with conda make

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -4,3 +4,7 @@ set -eux -o pipefail
  ./configure --prefix=${PREFIX}
 make
 make install
+
+mv ${PREFIX}/bin/LINKS-make ${PREFIX}/bin/LINKS-make-real
+echo "#!/bin/bash" > ${PREFIX}/bin/LINKS-make
+echo "make -f $(command -v ${PREFIX}/bin/LINKS-make-real) \$@" >> ${PREFIX}/bin/LINKS-make
