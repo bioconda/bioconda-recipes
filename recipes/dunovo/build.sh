7a7ba3bf7bd6c0b51e01d7f2a727c78c8d971f14
Nick <nmapsy@gmail.com>
2022-02-15 19:37:58 -0500

    Du Novo: Update to configurable Makefiles.

--- recipes/dunovo/build.sh
+++ recipes/dunovo/build.sh
@@ -5,7 +5,7 @@ cat > submodules.txt <<EOF
 kalign  0.3.0       makovalab-psu kalign-dunovo c0ef2de4a958aed47311ea86591debb3bada871143a6923bc6338ab2f99f2d5b
 utillib 0.1.1-alpha NickSto       utillib       961f5a3481d1c0dbe00c258b9df2de541e5605f3de4ff25bcc3cec22922e7c06
 ET      0.3         NickSto       ET            6b757b3ab3634b949f78692ac0db72e9264a1dceaf7449e921091d3ad0012eea
-bfx     0.5.0       NickSto       bfx           f5cc5c9c8a627a082f8b505135aec10b47c09647269ad92abe3a1fa2f2059048
+bfx     0.5.1       NickSto       bfx           03a251d64f6da5908a5e2502881000fa4154f9d81a667bb4f94f4a86367faac6
 EOF
 
 while read name version owner repo hash; do
@@ -23,8 +23,6 @@ while read name version owner repo hash; do
 done < submodules.txt
 
 # Compile binaries and move them to lib.
-sed -i.bak "s#gcc#${CC}#g" Makefile
-sed -i.bak "s#gcc#${CC}#g" bfx/Makefile
 make
 mkdir -p "$PREFIX/lib"
 mv *.so "$PREFIX/lib"
