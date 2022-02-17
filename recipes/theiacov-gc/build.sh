56f151416b2ec5c73339278084c09dca5a4e7c5e
Robert A. Petit III <robbie.petit@gmail.com>
2022-02-16 20:54:36 -0700

    Create build.sh

---
+++ recipes/theiacov-gc/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+THEIACOV="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p $PREFIX/bin ${THEIACOV}
+
+chmod 755 bin/*
+cp -f bin/* $PREFIX/bin/
+
+mv conf/ tasks/ workflows/ ${THEIACOV}
+
+# Setup the TheiaCoV env variables
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+echo "export THEIACOV_GC_VERSION=${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d/theiacov-gc.sh
+chmod a+x ${PREFIX}/etc/conda/activate.d/titan-gc.sh
+
+# Unset them
+echo "unset THEIACOV_GC_VERSION" > ${PREFIX}/etc/conda/deactivate.d//titan-gc.sh
+chmod a+x ${PREFIX}/etc/conda/deactivate.d/theiacov-gc.sh
