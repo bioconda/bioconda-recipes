84d777000455c2b8d125cf884dfb379875538a10
Robert A. Petit III <robbie.petit@gmail.com>
2022-02-17 15:43:39 -0700

    Update build.sh

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -2,13 +2,5 @@
 
 $PYTHON -m pip install . --ignore-installed --no-deps
 
-# Load the CARD database JSON file
-python $PREFIX/bin/rgi load -i card-data/card.json
-rm -rf card-data
-
-# Run rgi main once to build the necessary DB files
-mkdir temp-rgi
-cd temp-rgi
-python $PREFIX/bin/rgi main -i ${RECIPE_DIR}/tests/inputs/homolog.fasta -o output --debug -a diamond
-cd ..
-rm -rf temp-rgi
+# Load the CARD databases
+python $PREFIX/bin/rgi auto_load
