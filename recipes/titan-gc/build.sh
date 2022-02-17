927e1453648d49aed32a5258a2dcec8f2ddea1a2
Robert A. Petit III <robbie.petit@gmail.com>
2022-02-16 20:51:33 -0700

    Update build.sh

--- recipes/titan-gc/build.sh
+++ recipes/titan-gc/build.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-TITAN="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
-mkdir -p $PREFIX/bin ${TITAN}
+THEIACOV="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p $PREFIX/bin ${THEIACOV}
 
 chmod 755 bin/*
 cp -f bin/* $PREFIX/bin/
 
-mv conf/ tasks/ workflows/ ${TITAN}
+mv conf/ tasks/ workflows/ ${THEIACOV}
 
-# Setup the VADR env variables
+# Setup the TheiaCoV env variables
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
-echo "export TITAN_GC_VERSION=${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d//titan-gc.sh
+echo "export THEIACOV_GC_VERSION=${PKG_VERSION}" > ${PREFIX}/etc/conda/activate.d/theiacov-gc.sh
 chmod a+x ${PREFIX}/etc/conda/activate.d/titan-gc.sh
 
 # Unset them
-echo "unset TITAN_GC_VERSION" > ${PREFIX}/etc/conda/deactivate.d//titan-gc.sh
-chmod a+x ${PREFIX}/etc/conda/deactivate.d//titan-gc.sh
+echo "unset THEIACOV_GC_VERSION" > ${PREFIX}/etc/conda/deactivate.d//titan-gc.sh
+chmod a+x ${PREFIX}/etc/conda/deactivate.d/theiacov-gc.sh
