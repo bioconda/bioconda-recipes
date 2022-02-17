eb85bb61ec6f05bb393805b7bd6c11e2a443ed5e
Robert A. Petit III <robbie.petit@gmail.com>
2022-02-17 13:34:45 -0700

    Update build.sh

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -5,3 +5,11 @@ $PYTHON -m pip install . --ignore-installed --no-deps
 # Load the CARD database JSON file
 python $PREFIX/bin/rgi load -i card-data/card.json
 rm -rf card-data
+
+# Run rgi main once to build the necessary DB files
+mkdir temp-rgi
+cd temp-rgi
+wget --quiet --output-document=homolog.fasta https://raw.githubusercontent.com/arpcard/rgi/master/tests/inputs/homolog.fasta 
+python $PREFIX/bin/rgi main -i homolog.fasta -o output --debug -a diamond --clean
+cd ..
+rm -rf temp-rgi
