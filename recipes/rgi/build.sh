3fdddc7acfd7cac3cd27b433fac444f523944071
Robert A. Petit III <robbie.petit@gmail.com>
2022-02-17 20:07:40 -0700

    Update build.sh

--- recipes/rgi/build.sh
+++ recipes/rgi/build.sh
@@ -2,9 +2,8 @@
 
 $PYTHON -m pip install . --ignore-installed --no-deps
 
-# Load the CARD database JSON file
-python $PREFIX/bin/rgi load -i card-data/card.json
-rm -rf card-data
+# Load the CARD databases
+python $PREFIX/bin/rgi auto_load
 
 # Makesure blast/diamond database is built
 mkdir rgi-temp
