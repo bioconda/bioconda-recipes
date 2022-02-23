e60e21b148faec04d3355a93658517be2d11e442
Max Brown <mb39@sanger.ac.uk>
2022-02-23 16:18:00 +0000

    Shift openssl to host requirement. build.sh add openssl_dir environmental variable.

--- recipes/goat/build.sh
+++ recipes/goat/build.sh
@@ -18,4 +18,5 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 # build statically linked binary with Rust
-C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib cargo install --path . --root $PREFIX
+# add OPENSSL_DIR=$PREFIX
+C_INCLUDE_PATH=$PREFIX/include OPENSSL_DIR=$PREFIX LIBRARY_PATH=$PREFIX/lib cargo install --path . --root $PREFIX
