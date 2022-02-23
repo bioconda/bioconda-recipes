0e4ff3d5f71783eb64d94131a941e81bb8546db4
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2022-02-23 13:17:01 +0100

    remove newlines

--- recipes/openms-meta/package_libopenms.sh
+++ recipes/openms-meta/package_libopenms.sh
@@ -12,5 +12,3 @@ cmake -DCOMPONENT="OpenSwathAlgo_headers" -P build/cmake_install.cmake
 cmake -DCOMPONENT="thirdparty_headers" -P build/cmake_install.cmake
 cmake -DCOMPONENT="share" -P build/cmake_install.cmake
 cmake -DCOMPONENT="cmake" -P build/cmake_install.cmake
-
-
