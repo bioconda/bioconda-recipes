42d327cf80ac7ba58860a3b5eda24a04080de104
Benson, Derek (IM&T, Pullenvale) <Derek.Benson@csiro.au>
2022-02-23 15:02:46 +1100

    masurca new version needs gcc 10

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
-
+#
 set -x -e
-BOOST_ROOT=${PREFIX}
+export BOOST_ROOT=${PREFIX}
 export DEST=${PREFIX}
-export PERL_EXT_CPPFLAGS="-D_REENTRANT -D_GNU_SOURCE -fwrapv -fno-strict-aliasing -pipe -fstack-protector"
-export PERL_EXT_LDFLAGS="-shared -O2 -fstack-protector"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPATH=${PREFIX}/include
-export CFLAGS="$CFLAGS -std=gnu90"
+export CFLAGS="$CFLAGS -std=gnu11"
 export CXXFLAGS="$CXXFLAGS -std=gnu++14"
+
+sed -i -e 's|cd Flye|LDFLAGS="-L $LIBDIR" \&\& cd Flye|' install.sh
+sed -i -e 's|#-Wextra| ${LDFLAGS} #-Wextra|' Flye/lib/minimap2/Makefile
+sed -i -e 's|version 4.0.5|version 4.0.8|' global-1/SuperReads/src/masurca
+
 ./install.sh
+
