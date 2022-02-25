4f7da3e9d21182854211a8edf3795cbdc6c1861c
Hadi Foroughmand <mohammad-hadi.foroughmand-araabi@helmholtz-hzi.de>
2022-02-25 22:55:28 +0330

    scelestial_package_creation

--- recipes/scelestial/build.sh
+++ recipes/scelestial/build.sh
@@ -7,10 +7,14 @@ rm -f bin/*
 
 if [ "$(uname)" == "Linux" ]; then
 ${CXX} -O2 -Wconversion -Wno-sign-conversion -Wno-shorten-64-to-32 -std=c++0x src/scelestial.cc -o bin/scelestial -Wsign-compare
+${CXX} -o bin/scelestial-synthesis src/synthesis.cc -std=c++11 -Wall -lboost_program_options -I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib 
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
 ${CXX} -O2 -Wconversion -Wno-sign-conversion -Wno-shorten-64-to-32 -std=c++0x src/scelestial.cc -o bin/scelestial -Wsign-compare
+${CXX} -o bin/scelestial-synthesis src/synthesis.cc -std=c++11 -Wall -lboost_program_options -I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib 
 fi
 
 cp bin/scelestial ${PREFIX}/bin/scelestial
+cp bin/scelestial-synthesis ${PREFIX}/bin/scelestial-synthesis
+
