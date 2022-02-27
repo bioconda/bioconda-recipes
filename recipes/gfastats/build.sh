be7618c9916f85befa55be0f5d3e52d696cf9153
Bjoern Gruening <bjoern.gruening@gmail.com>
2022-02-27 21:02:42 +0100

    fixed the build

--- recipes/gfastats/build.sh
+++ recipes/gfastats/build.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
+export CFLAGS="-g -Wall -I$PREFIX/include -O3  -I$SRC_DIR/include  -std=c++11 -lstdc++"
+
+sed -i.bak -e '1,4d' Makefile
+
+make CC=$CXX
 
 mkdir -p $PREFIX/bin/
-
-# To run boost linking $CC and $CXX to gcc and g++
-ln -s $CC $PREFIX/bin/gcc
-ln -s $CXX $PREFIX/bin/g++
-
-make
-
 cp build/bin/gfastats $PREFIX/bin/
 chmod +x $PREFIX/bin/gfastats
-
-# Remove the fake links
-unlink $PREFIX/bin/gcc
-unlink $PREFIX/bin/g++
