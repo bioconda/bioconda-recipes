5992f2df0637711d723e70e4187aa1ac2c7cc038
Devon Ryan <devon.ryan@genedata.com>
2022-02-28 14:43:14 +0100

    bulk update

--- recipes/sciphi/build.sh
+++ recipes/sciphi/build.sh
@@ -12,6 +12,7 @@ mkdir -p build
 cd build
 
 cmake -DCMAKE_VERBOSE_MAKEFILE=ON -DDLIB_NO_GUI_SUPPORT=ON -DBUILD_DIAGNOSTICS=OFF -DBOOST_ROOT="${PREFIX}" -DDLIB_INCLUDEDIR="${curdir}/dlib/" ..
+grep "no_warning_for_no_symbols" *
 cmake --build . --config Release
 
 mv sciphi "${PREFIX}/bin"
