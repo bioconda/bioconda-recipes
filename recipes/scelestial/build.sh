6c4255129706e9cc0f92c882199052a45a8541ae
Hadi Foroughmand <mohammad-hadi.foroughmand-araabi@helmholtz-hzi.de>
2022-03-02 01:42:43 +0330

    scelestial_package_creation

--- recipes/scelestial/build.sh
+++ recipes/scelestial/build.sh
@@ -5,15 +5,8 @@ set -x -e
 mkdir -p ${PREFIX}/bin/ bin/
 rm -f bin/*
 
-if [ "$(uname)" == "Linux" ]; then
 ${CXX} -O2 -Wconversion -Wno-sign-conversion -Wno-shorten-64-to-32 -std=c++0x src/scelestial.cc -o bin/scelestial -Wsign-compare
 ${CXX} -o bin/scelestial-synthesis src/synthesis.cc -std=c++11 -Wall -lboost_program_options -I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib 
-fi
-
-if [ "$(uname)" == "Darwin" ]; then
-${CXX} -O2 -Wconversion -Wno-sign-conversion -Wno-shorten-64-to-32 -std=c++0x src/scelestial.cc -o bin/scelestial -Wsign-compare
-${CXX} -o bin/scelestial-synthesis src/synthesis.cc -std=c++11 -Wall -lboost_program_options -I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib 
-fi
 
 cp bin/scelestial ${PREFIX}/bin/scelestial
 cp bin/scelestial-synthesis ${PREFIX}/bin/scelestial-synthesis
