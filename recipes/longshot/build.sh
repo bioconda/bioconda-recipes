a92d978907dc06ffc90039c4c7072efffb0353b9
Devon Ryan <dpryan79@gmail.com>
2022-03-01 22:47:15 +0100

    parsnp, sciphi and longshot

--- recipes/longshot/build.sh
+++ recipes/longshot/build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
 # this build script is taken from the rust-bio-tools recipe
 # https://github.com/bioconda/bioconda-recipes/blob/master/recipes/rust-bio-tools/build.sh
@@ -6,7 +7,8 @@
 # taken from yacrd recipe, see: https://github.com/bioconda/bioconda-recipes/blob/2b02c3db6400499d910bc5f297d23cb20c9db4f8/recipes/yacrd/build.sh
 if [ "$(uname)" == "Darwin" ]; then
     # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
-    export HOME="/Users/distiller"
+    echo $HOME
+    #export HOME="/Users/distiller"
     # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
     git config --global --unset url.ssh://git@github.com.insteadOf
 fi
