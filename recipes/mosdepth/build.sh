2d0b233421cacc97a7b2968bef26fff7e6aaa138
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2022-03-01 21:29:36 +0100

    use correct foldername

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -3,7 +3,7 @@
 if [[ ${target_platform}  == osx-64 ]] ; then
     curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
     tar -xzf mosdepth-latest.tar.gz
-    cd mosdepth-latest
+    cd mosdepth-${PKG_VERSION}
     nimble --localdeps build -y --verbose -d:release
 else
     curl -SL https://github.com/brentp/mosdepth/releases/download/v$PKG_VERSION/mosdepth -o mosdepth
