ff50868dc8548965fe292ecf08e8657d87c469af
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-03-02 13:02:00 -0600

    Update bison version

--- recipes/modeltest-ng/build.sh
+++ recipes/modeltest-ng/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin/
-
 mkdir -p build
-cd build
+cd build 
 
+# Build multithreaded version 
+rm -rf *
+cmake ..
+make 
+cp ../bin/modeltest-ng ${PREFIX}/bin/
+
+# Build MPI version
+rm -rf *
 CXX=mpicxx cmake -DENABLE_MPI=ON ..
 make
-cp ../bin/modeltest-ng ${PREFIX}/bin/
+cp ../bin/modeltest-ng ${PREFIX}/bin/modeltest-ng-mpi
+
+chmod +x ${PREFIX}/bin/modeltest-ng*
