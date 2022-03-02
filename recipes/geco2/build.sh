eb5508ad5f2dbe2e601b88078bc72986b4a37ec3
Devon Ryan <dpryan79@gmail.com>
2022-03-02 16:29:17 +0100

    bulk updates

--- recipes/geco2/build.sh
+++ recipes/geco2/build.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+export CXXFLAGS="$CXXFLAGS -fcommon"
+export CFLAGS="$CFLAGS -fcommon"
 
 cd src 
 cmake .
