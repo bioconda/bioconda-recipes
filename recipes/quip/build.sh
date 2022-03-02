71864e2ff149920f9d8443dcc91db8c6cb872f39
Devon Ryan <dpryan79@gmail.com>
2022-03-02 10:08:12 +0100

    bulk updates

--- recipes/quip/build.sh
+++ recipes/quip/build.sh
@@ -3,5 +3,6 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export LIBS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+autoreconf -i
 ./configure --prefix=$PREFIX
 make install
