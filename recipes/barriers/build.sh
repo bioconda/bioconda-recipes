485b693e6f55134dc9390ed442f75f3346127a0f
Devon Ryan <dpryan79@gmail.com>
2022-03-04 00:40:48 +0100

    bulk update

--- recipes/barriers/build.sh
+++ recipes/barriers/build.sh
@@ -2,5 +2,6 @@
 set -ex
 grep -r automake
 ./configure --prefix=$PREFIX || (cat config.log ; exit 1)
+cat src Makefile
 make
 make install
--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -13,3 +13,5 @@ mv Makefile.new Makefile
 make CC=${CC} LIBRARY_PATH=${PREFIX}/lib prefix=${PREFIX}
 make test CC=${CC}
 make install prefix=${PREFIX}
+# The binaries are versioned for some reason
+mv ${PREFIX}/bin/sambamba-* ${PREFIX}/bin/sambamba
