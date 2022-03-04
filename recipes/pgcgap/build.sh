8418864ea021009368c8d47060418c148acd4395
liaochenlanruo <liaochenlanruo@webmail.hzau.edu.cn>
2022-03-04 08:47:37 +0800

    Update build.sh

--- recipes/pgcgap/build.sh
+++ recipes/pgcgap/build.sh
@@ -7,8 +7,8 @@ cp Functions/Pan/plot_3Dpie.R $PREFIX/bin/
 cp Functions/Pan/fmplot.py $PREFIX/bin/
 cp Functions/Pan/grep_cds_aas_from_gff3.pl $PREFIX/bin/
 
-cp Functions/COG/COG.pl $PREFIX/bin/
-cp Functions/COG/COG2020.pl $PREFIX/bin/
+#cp Functions/COG/COG.pl $PREFIX/bin/
+#cp Functions/COG/COG2020.pl $PREFIX/bin/
 cp Functions/COG/COGdiamond2022.pl $PREFIX/bin/
 cp Functions/COG/get_flag_relative_abundances_table.pl $PREFIX/bin/
 cp Functions/COG/Plot_COG.R $PREFIX/bin/
@@ -28,8 +28,8 @@ chmod a+x $PREFIX/bin/pgcgap
 chmod a+x $PREFIX/bin/plot_3Dpie.R
 chmod a+x $PREFIX/bin/fmplot.py
 chmod a+x $PREFIX/bin/grep_cds_aas_from_gff3.pl
-chmod a+x $PREFIX/bin/COG.pl
-chmod a+x $PREFIX/bin/COG2020.pl
+#chmod a+x $PREFIX/bin/COG.pl
+#chmod a+x $PREFIX/bin/COG2020.pl
 chmod a+x $PREFIX/bin/COGdiamond2022.pl
 chmod a+x $PREFIX/bin/get_flag_relative_abundances_table.pl
 chmod a+x $PREFIX/bin/Plot_COG.R
