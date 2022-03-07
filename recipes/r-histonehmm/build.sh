2a98b5d26d2e168977c4ba57e95424a871c27d3f
Devon Ryan <dpryan79@gmail.com>
2022-03-07 12:36:49 +0100

    bulk update

--- recipes/r-histonehmm/build.sh
+++ recipes/r-histonehmm/build.sh
@@ -1,9 +1,9 @@
 find $PREFIX -name Rcpp.h
 mkdir ~/.R
-echo -e "CC=$CC -I$PREFIX/include
+echo -e "CC=$CC -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
 FC=$FC
-CXX=$CXX -I$PREFIX/include
-CXX98=$CXX -I$PREFIX/include
-CXX11=$CXX -I$PREFIX/include
-CXX14=$CXX -I$PREFIX/include" > ~/.R/Makevars
+CXX=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+CXX98=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+CXX11=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+CXX14=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include" > ~/.R/Makevars
 $R CMD INSTALL --build .
