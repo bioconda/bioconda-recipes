5380068a02158fac6cc8a57d60b579d43d2724b6
Devon Ryan <dpryan79@gmail.com>
2022-03-07 17:50:48 +0100

    bulk update

--- recipes/longshot/build.sh
+++ recipes/longshot/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 # this build script is taken from the rust-bio-tools recipe
 # https://github.com/bioconda/bioconda-recipes/blob/master/recipes/rust-bio-tools/build.sh
@@ -9,6 +9,7 @@ if [ "$(uname)" == "Darwin" ]; then
     # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
     echo $HOME
     #export HOME="/Users/distiller"
+    export HOME=~/
     # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
     git config --global --unset url.ssh://git@github.com.insteadOf
 fi
