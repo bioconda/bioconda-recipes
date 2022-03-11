9e9429bc030c2db08080d49a90ed403a97cb2074
Devon Ryan <dpryan79@gmail.com>
2022-03-11 20:20:48 +0100

    bulk update

--- recipes/r-histonehmm/build.sh
+++ recipes/r-histonehmm/build.sh
@@ -1,9 +1,10 @@
 find $PREFIX -name Rcpp.h
-mkdir /Users/runner/.R
-echo -e "CC=$CC -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
-FC=$FC
-CXX=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
-CXX98=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
-CXX11=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
-CXX14=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include" > /Users/runner/.R/Makevars
+#mkdir /Users/runner/.R
+export CXX_INCLUDE_PATH=$PREFIX/lib/R/library/Rcpp/include
+#echo -e "CC=$CC -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+#FC=$FC
+#CXX=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+#CXX98=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+#CXX11=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include
+#CXX14=$CXX -I$PREFIX/include -I$PREFIX/lib/R/library/Rcpp/include" > /Users/runner/.R/Makevars
 $R CMD INSTALL --build .
