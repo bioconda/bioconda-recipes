50202417c822dda66a8ea90190788a74cb966fc7
Devon Ryan <dpryan79@gmail.com>
2022-03-13 22:14:31 +0100

    CMAKE_C_FLAGS

--- recipes/kissplice/build.sh
+++ recipes/kissplice/build.sh
@@ -8,7 +8,7 @@ cd build
 export CXXFLAGS="$CXXFLAGS -fcommon -I$PREFIX/include"
 export CFLAGS="$CFLAGS -fcommon -I$PREFIX/include"
 # cmake command
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_FLAGS="$CFLAGS" ..
 
 # make commands
 make -j1 VERBOSE=1
