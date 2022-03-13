293aa1a84182e437c0a62f87b7e2570042130277
Devon Ryan <dpryan79@gmail.com>
2022-03-14 00:40:44 +0100

    binaries are in the root directory

--- recipes/ma/build.sh
+++ recipes/ma/build.sh
@@ -5,8 +5,7 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DWITH_DB=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib ..
 make VERBOSE=1 -j 8 maCMD
 
-find . -name maCMD -ls
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib
-cp build/maCMD $PREFIX/bin
-cp build/lib*.so $PREFIX/lib/
+cp maCMD $PREFIX/bin
+cp lib*.so $PREFIX/lib/
