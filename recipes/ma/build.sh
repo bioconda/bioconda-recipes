3d3f1431c7f8d81c9245eb45f4f3fdbca81f5d10
Devon Ryan <dpryan79@gmail.com>
2022-03-13 22:59:03 +0100

    only make install maCMD

--- recipes/ma/build.sh
+++ recipes/ma/build.sh
@@ -4,7 +4,7 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DWITH_DB=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib ..
 make VERBOSE=1 -j 8 maCMD
-make install
+make maCMD install
 
 #mkdir -p $PREFIX/bin
 #mkdir -p $PREFIX/lib
