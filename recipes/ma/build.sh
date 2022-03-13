138613ce10050f883144ec648b3e0e2853b991f8
Devon Ryan <dpryan79@gmail.com>
2022-03-13 22:01:29 +0100

    make install

--- recipes/ma/build.sh
+++ recipes/ma/build.sh
@@ -4,9 +4,9 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DWITH_DB=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib ..
 make VERBOSE=1 -j 8 maCMD
+make install
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/lib
-cp maCMD $PREFIX/bin
-cp libMA.so $PREFIX/lib/
-
+#mkdir -p $PREFIX/bin
+#mkdir -p $PREFIX/lib
+#cp maCMD $PREFIX/bin
+#cp libMA.so $PREFIX/lib/
