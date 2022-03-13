2df54255322dadf7de3e54f04421b9812526260c
Devon Ryan <dpryan79@gmail.com>
2022-03-14 00:15:56 +0100

    debug

--- recipes/ma/build.sh
+++ recipes/ma/build.sh
@@ -5,7 +5,7 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DWITH_DB=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib ..
 make VERBOSE=1 -j 8 maCMD
 
-ls build
+find . -name maCMD -ls
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib
 cp build/maCMD $PREFIX/bin
