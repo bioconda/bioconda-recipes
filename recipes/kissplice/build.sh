49d8cfff716680b18ac338e20fa41ca8bf52d52a
Devon Ryan <dpryan79@gmail.com>
2022-03-13 23:17:04 +0100

    patch CMakeLists.txt

--- recipes/kissplice/build.sh
+++ recipes/kissplice/build.sh
@@ -4,7 +4,9 @@ mkdir -p ${PREFIX}/bin
 
 export CXXFLAGS="$CXXFLAGS -fcommon -I$PREFIX/include"
 export CFLAGS="$CFLAGS -fcommon -I$PREFIX/include"
-# cmake command
+
+# Unfortunately this is not written to pass on appropriate flags
+sed -i.bak "s#fopenmp#fopenmp -I$PREFIX/include#g" CMakeLists.txt
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_LIBRARY_PATH=$PREFIX/lib $DCMAKE_INCLUDE_PATH=$PREFIX/include .
 
 # make commands
