c6e81271a8f3bc4f96e33a3998a89b520a657fd3
Devon Ryan <dpryan79@gmail.com>
2022-03-13 22:57:13 +0100

    changes according to docs

--- recipes/kissplice/build.sh
+++ recipes/kissplice/build.sh
@@ -2,15 +2,11 @@
 
 mkdir -p ${PREFIX}/bin
 
-mkdir -p build
-cd build
-
 export CXXFLAGS="$CXXFLAGS -fcommon -I$PREFIX/include"
 export CFLAGS="$CFLAGS -fcommon -I$PREFIX/include"
 # cmake command
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_FLAGS="$CFLAGS" ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_LIBRARY_PATH=$PREFIX/lib $DCMAKE_INCLUDE_PATH=$PREFIX/include .
 
 # make commands
 make -j1 VERBOSE=1
-make test
 make install
