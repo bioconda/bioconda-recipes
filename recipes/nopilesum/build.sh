a629a5a4d2aba8430b73e0f052426351156a20ba
Devon Ryan <dpryan79@gmail.com>
2022-03-14 07:45:19 +0100

    simplify nopilesum compilation

--- recipes/nopilesum/build.sh
+++ recipes/nopilesum/build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# downloading D compiler
-echo "Downloading ldc2 1.26.0 D compiler"
-curl -fsS --insecure https://dlang.org/install.sh -o install.sh
-chmod +x install.sh
-DENV=$(./install.sh ldc-1.26.0 -p $PWD -a)
+## downloading D compiler
+#echo "Downloading ldc2 1.26.0 D compiler"
+#curl -fsS --insecure https://dlang.org/install.sh -o install.sh
+#chmod +x install.sh
+#DENV=$(./install.sh ldc-1.26.0 -p $PWD -a)
 
-# activating D compiler env
-. $DENV
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+## activating D compiler env
+##. $DENV
+#export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+#export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 
-# build fade binary
-cd $SRC_DIR
+## build fade binary
+#cd $SRC_DIR
 echo "building nopilesum binary"
 
 cp $PREFIX/lib/libhts* .
 
-dub build -b release -c bioconda
-cp ldc-1.26.0/lib/* $PREFIX/lib
-deactivate
+dub build -b release -c standard
+#cp ldc-1.26.0/lib/* $PREFIX/lib
+#deactivate
 
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
