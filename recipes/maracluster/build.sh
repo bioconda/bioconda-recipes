fe3c47dba1bb768768e93ddbbfdcbdb4738171a6
Matthias Bernt <m.bernt@ufz.de>
2022-03-14 14:17:20 +0100

    add CMAKE_C_COMPILER and DCMAKE_CXX_COMPILE

--- recipes/maracluster/build.sh
+++ recipes/maracluster/build.sh
@@ -3,7 +3,7 @@
 mkdir -p build/maracluster
 cd build/maracluster
 
-cmake -DTARGET_ARCH=amd64 -DCMAKE_BUILD_TYPE=Release -DBoost_NO_BOOST_CMAKE=ON -DVENDOR_SUPPORT=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${SRC_DIR}/build/tools" "${SRC_DIR}";
+cmake -DTARGET_ARCH=amd64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DBoost_NO_BOOST_CMAKE=ON -DVENDOR_SUPPORT=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${SRC_DIR}/build/tools" "${SRC_DIR}";
 make -j 4;
 make -j 4 package;
 make install
