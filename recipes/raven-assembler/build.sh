010cdfb1042987002246ea1bfb04e95330df7d18
Devon Ryan <dpryan79@users.noreply.github.com>
2022-03-14 11:01:24 +0100

    Update build.sh

--- recipes/raven-assembler/build.sh
+++ recipes/raven-assembler/build.sh
@@ -6,4 +6,4 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -Dspoa_optimize_for_portability=ON ..
 make
-make install
+cmake --target install -DCMAKE_INSTALL_PREFIX=$PREFIX
