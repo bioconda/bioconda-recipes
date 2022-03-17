609432d13d758f39b36a1b0d6b275b7b3a73caf3
William Fondrie <fondriew@gmail.com>
2022-03-17 10:58:00 -0700

    Update comet and change host to GitHub from SourceForge

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -2,16 +2,7 @@
 set -e
 set -x
 
-platform="$(uname)"
-unzip comet_source_"$PKG_VERSION".zip
-if [ "$platform" = "Darwin" ]; then
-    sed -i.bak -e 's/ -static//' -e 's/ -o / -headerpad_max_install_names&/' Makefile
-    sed -i.bak -e "s/sha1Report='..'/sha1Report=NULL/" MSToolkit/include/MSReader.h
-    sed -i.bak "s/ off64_t / off_t /g" CometSearch/Common.h
-    sed -i.bak "s/fseeko64/fseeko/g;s/ftello64/ftello/g" MSToolkit/include/mzParser.h
-    sed -i.bak "s/fseeko64/fseeko/g;s/ftello64/ftello/g" CometSearch/Common.h
-    sed -i.bak "s/pthread_yield/sched_yield/g;" CometSearch/ThreadPool.h
-fi
+unzip Comet-"$PKG_VERSION".zip
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
 make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large"
