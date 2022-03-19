2b51c48e4d2a9657a822b2e9e3f31527bc0e24ca
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-03-19 23:59:59 +0100

    Specify vindas python executable since on MacOS there is another

--- recipes/pyopenms/build.sh
+++ recipes/pyopenms/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+
 # useless default include directory that is silently added by the compiler packages "to help"...
 # it is not even added with -isystem https://github.com/AnacondaRecipes/aggregate/blob/master/clang/activate-clang%2B%2B.sh#L87
 USELESS="-I${PREFIX}/include"
@@ -22,13 +22,6 @@ else
 fi
 LDFLAGS='-Wl,-rpath,${RPATH}'
 
-echo "Helloooo?????"
-
-$PYTHON -c "import Cython"
-
-$PYTHON -c "import Cython" > log.txt 2>&1
-cat log.txt | echo
-
 cmake ../src/pyOpenMS \
   -DOPENMS_GIT_SHORT_REFSPEC="release/${PKG_VERSION}" \
   -DOPENMS_GIT_SHORT_SHA1="b59e0c3" \
@@ -41,6 +34,7 @@ cmake ../src/pyOpenMS \
   -DCMAKE_INSTALL_NAME_DIR="@rpath" \
   -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
   -DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON \
+  -DPython_EXECUTABLE=$PYTHON \
   -DNO_DEPENDENCIES=ON
 
 # NO_DEPENDENCIES since conda takes over re-linking etc.
