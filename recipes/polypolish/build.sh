11ed234ab8cbb5d11802eb7d7401700db06fe329
thanhleviet <thanh.le-viet@quadram.ac.uk>
2022-03-22 12:02:05 +0000

    update polypolish recipe

--- recipes/polypolish/build.sh
+++ recipes/polypolish/build.sh
@@ -2,7 +2,7 @@
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
+export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="${BUILD_PREFIX}/.cargo"
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
