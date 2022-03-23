644e880c706f024af3f83314d67868e3c0421bf8
Reid Wagner <74672860+reid-wagner@users.noreply.github.com>
2022-03-23 12:08:43 -0500

    [ci skip] Merge PR 30894
    Merge PR #30894, commits were: 
     * Initial commit of MetaNovo recipe.

---
+++ recipes/metanovo/metanovo_activate.sh
@@ -0,0 +1,4 @@
+export SG_PATH=${CONDA_PREFIX}/bio/SearchGUI/SearchGUI-3.2.20
+export DG_PATH=${CONDA_PREFIX}/bio/DeNovoGUI/DeNovoGUI-1.15.11
+export CU_PATH=${CONDA_PREFIX}/bio/utilities/utilities-4.12.0
+export MZIDLIB_PATH=${CONDA_PREFIX}/bio/ProteoAnnotator/ProteoAnnotator-1.7.86/mzidlib-1.7
