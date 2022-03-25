bf104801166d50198c78481d5a8c9601eba0f7f4
Robert A. Petit III <robbie.petit@gmail.com>
2022-03-25 11:06:47 -0600

    update recipe

--- recipes/lmas/build.sh
+++ recipes/lmas/build.sh
@@ -4,9 +4,11 @@ LMAS="${PREFIX}/share/${PKG_NAME}-${LMAS_VERSION}"
 mkdir -p ${PREFIX}/bin ${LMAS}
 
 chmod 775 LMAS get_data.sh
-sed -i 's=main.nf=${LMAS}/main.nf=' LMAS
-cp LMAS ${PREFIX}/bin
 cp get_data.sh ${PREFIX}/bin/get_lmas_data.sh
 
+echo "#!/bin/bash" > $PREFIX/bin/LMAS
+echo "$LMAS/LMAS \"\${@:1}\"" >> $PREFIX/bin/LMAS
+chmod 755 $PREFIX/bin/LMAS
+
 # Move LMAS
-mv conf/ docker/ docs/ lib/ modules/ resources/ templates/ test/ main.nf nextflow.config ${LMAS}
+mv conf/ docker/ docs/ lib/ modules/ resources/ templates/ test/ main.nf nextflow.config LMAS ${LMAS}
