8bf64e2f8e53140105f541191e07dc49a2f872f2
Devon Ryan <dpryan79@gmail.com>
2022-03-27 18:37:03 +0200

    umm, what?

--- recipes/perl-alien-build/build.sh
+++ recipes/perl-alien-build/build.sh
@@ -10,7 +10,7 @@ if [[ -f Build.PL ]]; then
     perl ./Build install --installdirs site
 elif [[ -f Makefile.PL ]]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
+    $PREFIX/bin/perl Makefile.PL INSTALLDIRS=site
     make
     make test
     make install
