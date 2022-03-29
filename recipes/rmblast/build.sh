4b5eb3d8b071436fbeb4f59b50d6b87a5a304109
Devon Ryan <dpryan79@users.noreply.github.com>
2022-03-29 12:06:52 +0200

    Update build.sh

--- recipes/rmblast/build.sh
+++ recipes/rmblast/build.sh
@@ -93,10 +93,10 @@ make -j${CPU_COUNT} -f Makefile.flat all_projects="$projects"
 rm $LIB_INSTALL_DIR
 
 mkdir -p $PREFIX/bin $LIB_INSTALL_DIR
+chmod +x $SRC_DIR/c++/ReleaseMT/bin/*
 cp $SRC_DIR/c++/ReleaseMT/bin/* $PREFIX/bin/
 cp $SRC_DIR/c++/ReleaseMT/lib/* $LIB_INSTALL_DIR
 
-chmod +x $PREFIX/bin/*
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/update_blastdb.pl
 # Patches to enable this script to work better in bioconda
 sed -i.bak 's/mktemp.*/mktemp`/; s/exit 1/exit 0/; s/^export PATH=\/bin:\/usr\/bin:/\#export PATH=\/bin:\/usr\/bin:/g' $PREFIX/bin/get_species_taxids.sh
