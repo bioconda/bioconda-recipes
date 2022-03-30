a450601dd006c18d3ac93271e0dc9e3ae7f87adb
Devon Ryan <dpryan79@users.noreply.github.com>
2022-03-30 11:08:51 +0200

    debug where the lto flags are coming from

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-./configure --prefix=$PREFIX --with-vrna=$PREFIX && \
-make -j ${CPU_COUNT} && \
+env | sort
+./configure --prefix=$PREFIX --with-vrna=$PREFIX
+make -j ${CPU_COUNT}
 make install
