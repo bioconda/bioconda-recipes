b0f59023cd904f0f413d9cbbf6eb7753bed3c951
Devon Ryan <dpryan79@users.noreply.github.com>
2022-03-30 11:48:28 +0200

    Update build.sh

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-env | sort
-./configure --prefix=$PREFIX --with-vrna=$PREFIX
+set -ex
+./configure --prefix=$PREFIX --with-vrna=$PREFIX --vrna-libs="-L$PREFIX/lib -lRNA -lpthread"
 make -j ${CPU_COUNT}
 make install
