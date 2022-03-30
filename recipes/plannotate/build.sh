9da5e0d18ba9ecbc7d0b24bb89c6f690570ed115
Devon Ryan <dpryan79@gmail.com>
2022-03-30 22:31:24 +0200

    Clean up OSX files from plAnnotate

--- recipes/plannotate/build.sh
+++ recipes/plannotate/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
-plannotate setupdb
+
+# Clean up any extraneous OSX files in the DB archive
+rm BLAST_dbs/._*
+# Install databases
+mv BLAST_dbs $PREFIX/lib/python3.*/site-packages/plannotate/data/
