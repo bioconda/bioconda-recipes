41786c46ed5afb1fbbdb37a5bf4b8235e22dfddb
Sebastian Will <swill@csail.mit.edu>
2022-03-31 09:41:22 +0200

    Revert "Update build.sh"
    This reverts commit b0f59023cd904f0f413d9cbbf6eb7753bed3c951.

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
-./configure --prefix=$PREFIX --with-vrna=$PREFIX --vrna-libs="-L$PREFIX/lib -lRNA -lpthread"
+env | sort
+./configure --prefix=$PREFIX --with-vrna=$PREFIX
 make -j ${CPU_COUNT}
 make install
