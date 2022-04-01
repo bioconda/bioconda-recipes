092e2d47575a3ab5f8c2e99b5da3cd82b0aae080
zhenxian <920596906@qq.com>
2022-04-01 15:27:56 +0800

    Update build.sh

--- recipes/clair3/build.sh
+++ recipes/clair3/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -pv $PREFIX/bin
-cp -rv clair3 models preprocess scripts shared $PREFIX/bin
+cp -rv clair3 models preprocess scripts  shared $PREFIX/bin
 cp clair3.py $PREFIX/bin/
 cp run_clair3.sh $PREFIX/bin/
-(make)
+make
