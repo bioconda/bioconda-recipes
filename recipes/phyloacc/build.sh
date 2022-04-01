37ca1750b3b32ad4274fdd049ba1fb64ff01d587
gwct <greggwct@gmail.com>
2022-04-01 12:01:43 -0400

    trying to get the gene tree version to build

--- recipes/phyloacc/build.sh
+++ recipes/phyloacc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-make CXX=${CXX}
-make install
+#make CXX=${CXX}
+make install CXX=${CXX}
 cp src/PhyloAcc-interface/phyloacc.py ${PREFIX}/bin/.
 mkdir -p ${SP_DIR}
 cp -R src/PhyloAcc-interface/phyloacc_lib ${SP_DIR}/.
