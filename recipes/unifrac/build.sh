d06e15bce02052d6e941006ae1fbae3417ddd758
Devon Ryan <dpryan79@users.noreply.github.com>
2022-04-03 22:56:52 +0200

    Update build.sh

--- recipes/unifrac/build.sh
+++ recipes/unifrac/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
+sed -i.bak "s#CONDA_PREFIX#PREFIX#g" setup.py
 
 $PYTHON -m pip install --no-deps --ignore-installed .
