7fbc89204ff18730d0fd3d391bdc3ce1f31adac0
Devon Ryan <dpryan79@users.noreply.github.com>
2022-04-03 22:33:39 +0200

    Update build.sh

--- recipes/unifrac/build.sh
+++ recipes/unifrac/build.sh
@@ -2,5 +2,6 @@
 set -e
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
 
 $PYTHON -m pip install --no-deps --ignore-installed .
