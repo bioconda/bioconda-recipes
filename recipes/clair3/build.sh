a1dd1c45f4fe7cbbad079ba5ca1745c9af4d684d
zhenxian <920596906@qq.com>
2022-04-04 15:53:13 +0800

    Update build.sh

--- recipes/clair3/build.sh
+++ recipes/clair3/build.sh
@@ -4,5 +4,5 @@ mkdir -pv $PREFIX/bin
 cp -rv clair3 models preprocess scripts  shared $PREFIX/bin
 cp clair3.py $PREFIX/bin/
 cp run_clair3.sh $PREFIX/bin/
-make
+make CC=${GCC} CXX=${GXX}  PREFIX=${PREFIX}
 cp  longphase libclair3* $PREFIX/bin
