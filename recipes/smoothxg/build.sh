c8eccd2572d20d57348dffcd4d0a28c68eef3837
AndreaGuarracino <andreaguarracino@outlook.com>
2022-04-06 10:06:53 +0200

    update `smoothxg`, its building mode and its build requirements

--- recipes/smoothxg/build.sh
+++ recipes/smoothxg/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LIBRARY_PATH=${PREFIX}/lib
 export C_INCLUDE_PATH=${PREFIX}/include
-cmake -H. -Bbuild
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic
 cmake --build build
 mkdir -p $PREFIX/bin
 mv bin/* $PREFIX/bin
