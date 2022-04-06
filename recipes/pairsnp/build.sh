5fbb47b5cca734edf44dd5fbfef687d6a0775488
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2022-04-06 08:08:19 +0200

    attempt at fixing injection of compilers

--- recipes/pairsnp/build.sh
+++ recipes/pairsnp/build.sh
@@ -1,17 +1,5 @@
 
 #!/bin/bash
 
-# https://bioconda.github.io/troubleshooting.html#zlib-errors
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-
-# conda on macOS will fall back to libstdc++
-# which lacks a ton of standard C++11 headers
-if [[ $(uname) == Darwin ]]; then
-	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-fi
-
-
-make
-make install
+make CC="${CXX}" LINK="${CXX}" SWITCHES="${CPPFLAGS} ${CXXFLAGS} -std=c++14 ${LDFLAGS}"
+make install PREFIX="${PREFIX}/bin"
