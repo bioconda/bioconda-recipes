0e894d17f2ed84045ef178a96091741a52e305b6
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2022-04-06 08:36:33 +0200

    attempt to fix zlib issue

--- recipes/pairsnp/build.sh
+++ recipes/pairsnp/build.sh
@@ -1,7 +1,17 @@
 
 #!/bin/bash
 
-make CXX="${CXX}" LINK="${CXX}" SWITCHES="${CPPFLAGS} ${CXXFLAGS} -std=c++14 ${LDFLAGS}"
-make install PREFIX="${PREFIX}/bin"
+# https://bioconda.github.io/troubleshooting.html#zlib-errors
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
+# conda on macOS will fall back to libstdc++
+# which lacks a ton of standard C++11 headers
+if [[ $(uname) == Darwin ]]; then
+	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+fi
 
 
+LIBS="${LDFLAGS}" make CXX="${CXX}" PREFIX="${PREFIX}"
+make install
