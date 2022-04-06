027d1cea361245d62ff7c95dbbc18f0f44dbd5b6
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2022-04-06 08:24:41 +0200

    attempt at fixing injection of compilers

--- recipes/pairsnp/build.sh
+++ recipes/pairsnp/build.sh
@@ -1,5 +1,7 @@
 
 #!/bin/bash
 
-make CC="${CXX}" LINK="${CXX}" SWITCHES="${CPPFLAGS} ${CXXFLAGS} -std=c++14 ${LDFLAGS}"
+make CXX="${CXX}" LINK="${CXX}" SWITCHES="${CPPFLAGS} ${CXXFLAGS} -std=c++14 ${LDFLAGS}"
 make install PREFIX="${PREFIX}/bin"
+
+
