fc05325b5cbcc59fa86973db80799397c5438977
afshinfard <a.afshinfard@gmail.com>
2022-04-06 15:00:54 -0700

    v1.4.1

--- recipes/btllib/build.sh
+++ recipes/btllib/build.sh
@@ -6,8 +6,8 @@ mkdir -p ${PREFIX}/bin/
 mkdir -p ${PREFIX}/include/
 mkdir -p ${PREFIX}/lib/
 
-cp bin/* ${PREFIX}/bin/
-cp include/* ${PREFIX}/include/
-cp lib/libbtllib.a ${PREFIX}/lib/libbtllib.a
+cp install/bin/* ${PREFIX}/bin/
+cp install/include/* ${PREFIX}/include/
+cp install/lib/* ${PREFIX}/lib/
 
 # python wrappers:
