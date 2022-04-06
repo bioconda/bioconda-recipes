99732b19ddaac6ba4493fdbd922feedc3071d42a
Ben Soares <b.soares@dundee.ac.uk>
2022-04-06 18:13:34 +0100

    fixed a property setting

--- recipes/jalview/build.sh
+++ recipes/jalview/build.sh
@@ -10,23 +10,23 @@ echo "rootProject.name = 'jalview'" >> settings.gradle
 # compile jalview from source
 
 # First the Java 1.8 build
-gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
+gradle -PJAVA_VERSION=1.8 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch="" shadowJar
 
 # copy jalview jar to target
 cp -vR build/libs/jalview-all-$PKG_VERSION-j1.8.jar $JALVIEWDIR/jalview-all-j1.8.jar
 
 # Now the Java 11 build
-gradle -PJAVA_VERSION=11 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch"" shadowJar 
+gradle -PJAVA_VERSION=11 -PINSTALLATION="bioconda (build $PKG_BUILDNUM)" -PJALVIEW_VERSION="$PKG_VERSION" -Pproject.ext.gitHash="" -Pproject.ext.gitBranch="" shadowJar
 
 # copy jalview jar to target
 cp -vR build/libs/jalview-all-$PKG_VERSION-j11.jar $JALVIEWDIR/jalview-all-j11.jar
 
 # copy wrapper and make executable
 cp $RECIPE_DIR/jalview.sh $JALVIEWDIR/.
-chmod +x $JALVIEWDIR/jalview.sh; 
+chmod +x $JALVIEWDIR/jalview.sh;
 
 # link wrapper script
 mkdir -p $PREFIX/bin
 ln -fs $JALVIEWDIR/jalview.sh $PREFIX/bin/jalview
-chmod +x $PREFIX/bin/jalview; 
+chmod +x $PREFIX/bin/jalview;
 
