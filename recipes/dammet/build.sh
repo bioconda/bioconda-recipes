bafe1a0638e312b7d68b4a68b83ca3bf60926e0d
KHanghoj <kristianhanghoej@gmail.com>
2022-04-07 21:12:56 +0200

    updated build

--- recipes/dammet/build.sh
+++ recipes/dammet/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-
 make -f Makefile
 make -f Makefile install PREFIX=${PREFIX}
 
