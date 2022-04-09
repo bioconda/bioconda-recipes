13e060e29546c350d33a6dfad571b6f68ce00f03
genomewalker <genomewalker@gmail.com>
2022-04-09 13:54:10 +0200

    Added ngsLCA recipe

--- recipes/ngslca/build.sh
+++ recipes/ngslca/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
+mkdir -p "${PREFIX}"/bin
 
-mkdir -p ${PREFIX}/bin
-
-make HTSSRC="systemwide" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" prefix=$PREFIX  CC="$CC" CXX="$CXX" FLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
-mv ./ngsLCA ${PREFIX}/bin/
+make clean
+make CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" prefix="${PREFIX}" CC="${CC}" CXX="${CXX} -L${PREFIX}/lib" LDFLAGS+="-lz -lbz2 -llzma ${LDFLAGS} -L${PREFIX}/lib"
+mv ./ngsLCA "${PREFIX}"/bin/
