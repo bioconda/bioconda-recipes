05edb7f3c04c44a6a08566d82cac8312e7327428
Benoit Valot <bvalot3@hp7490-bv>
2022-04-13 10:33:05 +0200

    Specify g++ compiler and add flag

--- recipes/gassst/build.sh
+++ recipes/gassst/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-make
+# export CPPFLAGS="-I${BUILD_PREFIX}/include"
+
+export INCLUDE_PATH="${BUILD_PREFIX}/include"
+export LIBRARY_PATH="${BUILD_PREFIX}/lib"
+export LD_LIBRARY_PATH="${BUILD_PREFIX}/lib"
+export CFLAGS=" -Wall -O3 -D_REENTRANT -msse2 -mssse3  -Wno-write-strings -DSSE=3 -std=c++11"
+export LDFLAGS=" -lm -Xlinker -zmuldefs -lpthread -O3 -mssse3 -L${BUILD_PREFIX}/lib"
+
+make CC=$GXX LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
 
 mkdir -p "$PREFIX"/bin
 cp Gassst "$PREFIX"/bin/Gassst
