d1edc49bc0fe91cc1258013b76e33216f8495415
Sergey Koren <sergek@umd.edu>
2022-04-19 07:57:24 -0400

    Update run_test.sh

--- recipes/verkko/run_test.sh
+++ recipes/verkko/run_test.sh
@@ -7,7 +7,7 @@ set -eu -o pipefail
 rm -f ./hifi.fastq.gz ./ont.fastq.gz
 curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_hifi_subset24x.fastq.gz -o hifi.fastq.gz
 curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_ont_subset50x.fastq.gz -o ont.fastq.gz
-verkko -d asm --correct-hash-bits 22 --hifi ./hifi.fastq.gz --nano ./ont.fastq.gz
+verkko -d asm --no-correction --hifi ./hifi.fastq.gz --nano ./ont.fastq.gz
 
 if [ ! -s asm/assembly.fasta ]; then
    echo "Error: verkko assembly test failed!"
