1aaf6e57fe786a1e31401bff3d61299a097335bb
Malte Petersen <509369+mptrsen@users.noreply.github.com>
2022-04-21 21:49:28 +0200

    Update build.sh
    Use RECIPE_DIR instead of PREFIX, hopefully this finds the installed dependencies

--- recipes/biobloomtools/build.sh
+++ recipes/biobloomtools/build.sh
@@ -2,9 +2,9 @@
 
 set -e -o pipefail -x
 
-export LIBRARY_PATH=${PREFIX}/lib
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${RECIPE_DIR}/lib
+export C_INCLUDE_PATH=${RECIPE_DIR}/include
+export CPP_INCLUDE_PATH=${RECIPE_DIR}/include
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CFLAGS} -fcommon"
 
@@ -16,5 +16,5 @@ git submodule update --init
 git checkout 0a42916922d42611a087d4df871e424a8907896e
 ./autogen.sh
 ./configure --prefix=${PREFIX}
-make -j 4
+make ${MAKEFLAGS}
 make install
