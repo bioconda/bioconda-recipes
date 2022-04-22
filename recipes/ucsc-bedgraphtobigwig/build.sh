0126b4c8999744d759e0df2107be7cfbaf0c44e7
Angie Hinrichs <angie@soe.ucsc.edu>
2022-04-22 13:54:32 -0700

    Don't build kent/src/hg/lib for bedGraphToBigWig, not needed.

--- recipes/ucsc-bedgraphtobigwig/build.sh
+++ recipes/ucsc-bedgraphtobigwig/build.sh
@@ -8,7 +8,6 @@ mkdir -p "$BINDIR"
 (cd kent/src/lib && make)
 (cd kent/src/htslib && make)
 (cd kent/src/jkOwnLib && make)
-(cd kent/src/hg/lib && make)
 (cd kent/src/utils/bedGraphToBigWig && make)
 cp bin/bedGraphToBigWig "$PREFIX/bin"
 chmod +x "$PREFIX/bin/bedGraphToBigWig"
