38e1a806b511e4def1d1dae9065a2f433b0d43b7
quito418 <quito418.mail@gmail.com>
2022-04-24 16:08:13 +0900

    update

--- recipes/bwa-meme/build.sh
+++ recipes/bwa-meme/build.sh
@@ -5,4 +5,6 @@ LIBS="${LDFLAGS}" make -j4 CC="${CC}" CXX="${CXX}" multi
 mkdir -p $PREFIX/bin
 cp bwa-meme* $PREFIX/bin
 
-
+cd RMI && cargo build --release && cd ..
+cp RMI/target/release/bwa-meme-train-prmi $PREFIX/bin
+cp build_rmis_dna.sh $PREFIX/bin
