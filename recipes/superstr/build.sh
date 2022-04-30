50deed37630709d5e570a35052a06cf5a4b2b648
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2022-04-30 07:34:51 +0000

    python scripts

--- recipes/superstr/build.sh
+++ recipes/superstr/build.sh
@@ -3,3 +3,12 @@ cd C
 cmake .
 make
 cp superstr $PREFIX/bin/superstr
+cd ../Python
+cp multiparse.py $PREFIX/bin/superstr-multiparse.py
+cp outliers.py $PREFIX/bin/superstr-outliers.py
+cp screen.py $PREFIX/bin/superstr-screen.py
+cp visualise.py $PREFIX/bin/visualise.py
+chmod +x $PREFIX/bin/superstr-multiparse.py
+chmod +x $PREFIX/bin/superstr-outliers.py
+chmod +x $PREFIX/bin/superstr-screen.py
+chmod +x $PREFIX/bin/visualise.py
