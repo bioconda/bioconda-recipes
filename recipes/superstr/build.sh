a8a7388a1614ff1b5eda95f4b98aecf2150614fc
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2022-04-30 09:46:59 +0000

    outliers script python add

--- recipes/superstr/build.sh
+++ recipes/superstr/build.sh
@@ -5,10 +5,11 @@ make
 cp superstr $PREFIX/bin/superstr
 cd ../Python
 cp multiparse.py $PREFIX/bin/superstr-multiparse.py
-cp outliers.py $PREFIX/bin/superstr-outliers.py
+echo '#!/usr/bin/env python' > $PREFIX/bin/superstr-outliers.py
+cat outliers.py >> $PREFIX/bin/superstr-outliers.py
 cp screen.py $PREFIX/bin/superstr-screen.py
-cp visualise.py $PREFIX/bin/visualise.py
+cp visualise.py $PREFIX/bin/superstr-visualise.py
 chmod +x $PREFIX/bin/superstr-multiparse.py
 chmod +x $PREFIX/bin/superstr-outliers.py
 chmod +x $PREFIX/bin/superstr-screen.py
-chmod +x $PREFIX/bin/visualise.py
+chmod +x $PREFIX/bin/superstr-visualise.py
