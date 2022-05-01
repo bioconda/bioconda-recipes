652ace41a488715d9c569cc9def78673269fa658
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2022-05-01 07:57:10 +0000

    version 1.0.1

--- recipes/superstr/build.sh
+++ recipes/superstr/build.sh
@@ -5,8 +5,7 @@ make
 cp superstr $PREFIX/bin/superstr
 cd ../Python
 cp multiparse.py $PREFIX/bin/superstr-multiparse.py
-echo '#!/usr/bin/env python' > $PREFIX/bin/superstr-outliers.py
-cat outliers.py >> $PREFIX/bin/superstr-outliers.py
+cp outliers.py >> $PREFIX/bin/superstr-outliers.py
 cp screen.py $PREFIX/bin/superstr-screen.py
 cp visualise.py $PREFIX/bin/superstr-visualise.py
 chmod +x $PREFIX/bin/superstr-multiparse.py
