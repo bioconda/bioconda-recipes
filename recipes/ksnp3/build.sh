94be57647226e181f89b70eb471cdc573885eb2c
Robert A. Petit III <robbie.petit@gmail.com>
2022-05-02 13:09:40 -0600

    Update build.sh

--- recipes/ksnp3/build.sh
+++ recipes/ksnp3/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 mkdir -p ${PREFIX}/bin
 
 # Copy files
@@ -11,3 +12,8 @@ sed -i 's=/bin/tcsh=/usr/bin/env tcsh=' ${PREFIX}/bin/kSNP3
 sed -i 's=/usr/local/kSNP3=$(which kSNP3)=' ${PREFIX}/bin/kSNP3
 sed -i 's=/bin/tcsh=/usr/bin/env tcsh=' ${PREFIX}/bin/select_node_annotations3
 sed -i 's=/bin/tcsh=/usr/bin/env tcsh=' ${PREFIX}/bin/extract_nth_locus3
+
+# Debug
+head ${PREFIX}/bin/kSNP3
+head ${PREFIX}/bin/select_node_annotations3
+head ${PREFIX}/bin/extract_nth_locus3
