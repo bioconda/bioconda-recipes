3e5c6c77be02508120b08e17c3bfa6e8faeb57ea
Robert A. Petit III <robbie.petit@gmail.com>
2022-05-02 13:27:47 -0600

    Update build.sh

--- recipes/ksnp3/build.sh
+++ recipes/ksnp3/build.sh
@@ -9,11 +9,6 @@ cp kSNP${PKG_VERSION}_Linux_package/kSNP3/* ${PREFIX}/bin
 
 # Fix hard coded paths
 sed -i 's=/bin/tcsh=/usr/bin/env tcsh=' ${PREFIX}/bin/kSNP3
-sed -i 's=/usr/local/kSNP3=$(which kSNP3)=' ${PREFIX}/bin/kSNP3
+sed -i 's=/usr/local/kSNP3=`which kSNP3`=' ${PREFIX}/bin/kSNP3
 sed -i 's=/bin/tcsh=/usr/bin/env tcsh=' ${PREFIX}/bin/select_node_annotations3
 sed -i 's=/bin/tcsh=/usr/bin/env tcsh=' ${PREFIX}/bin/extract_nth_locus3
-
-# Debug
-head ${PREFIX}/bin/kSNP3
-head ${PREFIX}/bin/select_node_annotations3
-head ${PREFIX}/bin/extract_nth_locus3
