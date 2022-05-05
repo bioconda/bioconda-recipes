cc0526586b8fddaea9bc9e5664a562c738e782b7
zhangjun <zhangjuncsu@csu.edu.cn>
2022-05-05 15:19:10 +0800

    update build.sh of fec

--- recipes/fec/build.sh
+++ recipes/fec/build.sh
@@ -2,30 +2,53 @@
 
 mkdir -p "${PREFIX}"/bin
 
-export CXXPATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
-make INCLUDES="-I$PREFIX/include" CXXFLAG="-g -Wall -O2 -std=c++11" LDFLAG="-pthread -lm -fopenmp -lz -lstdc++"
+make
 
 OSTYPE=$(shell echo `uname`)
 MACHINETYPE=$(shell echo `uname -m`)
 
-ifeq (${MACHINETYPE}, x86_64)
-  MACHINETYPE=amd64
-endif
+if [ "$(MACHINETYOE)" == "x86_64" ]; then
+	MACHINETYPE=amd64
+fi
 
-ifeq (${MACHINETYPE}, Power Macintosh)
-  MACHINETYPE=ppc
-endif
+if [ "$(MACHINETYPE)" == "Power Macintosh" ]; then
+	MACHINETYPE=ppc
+fi
 
-ifeq (${OSTYPE}, SunOS)
-  MACHINETYPE=${shell echo `uname -p`}
-  ifeq (${MACHINETYPE}, sparc)
-    ifeq (${shell /usr/bin/isainfo -b}, 64)
-      MACHINETYPE=sparc64
-    else
-      MACHINETYPE=sparc32
-    endif
-  endif
-endif
+if [ "$(OSTYPE)" == "SunOS" ]; then
+	MACHINETYPE=$(shell echo `uname -p`)
+	if [ "$(MACHINETYPE)" == "sparc" ]: then
+		if ["$(shell /usr/bin/isainfo -b)" == "64" ]; then
+			MACHINETYPE=sparc64
+		else
+			MACHINETYPE=sparc332
+		fi
+	fi
+fi
+
+#ifeq (${MACHINETYPE}, x86_64)
+#  MACHINETYPE=amd64
+#endif
+
+#ifeq (${MACHINETYPE}, Power Macintosh)
+#  MACHINETYPE=ppc
+#endif
+
+#ifeq (${OSTYPE}, SunOS)
+#  MACHINETYPE=${shell echo `uname -p`}
+#  ifeq (${MACHINETYPE}, sparc)
+#    ifeq (${shell /usr/bin/isainfo -b}, 64)
+#      MACHINETYPE=sparc64
+#    else
+#      MACHINETYPE=sparc32
+#    endif
+#  endif
+#endif
 
 cp $(OSTYPE)-$(MACHINETYPE)/bin/Fec $(PREFIX)/bin/Fec
