7e44ff0be9c94f572eb8044756fb7d27995027ae
zhangjun <zhangjuncsu@csu.edu.cn>
2022-05-05 11:19:11 +0800

    update build.sh of fec

--- recipes/fec/build.sh
+++ recipes/fec/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p "${PREFIX}"/bin
-cp Fec "${PREFIX}"/bin/
-chmod +x "${PREFIX}"/bin/Fec
+
+export CXXPATH=${PREFIX}/include
+
+make INCLUDES="-I$PREFIX/include" CXXFLAG="-g -Wall -O2 -std=c++11" Fec
+cp Fec $PREFIX/bin
