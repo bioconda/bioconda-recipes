42c2722ea9745f762fed68f0c3cfc7127893e47c
zhangjun <zhangjuncsu@csu.edu.cn>
2022-05-09 19:55:31 +0800

    update meta.yaml and build.sh of fec

--- recipes/fec/build.sh
+++ recipes/fec/build.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p "${PREFIX}"/bin
-export CXXPATH=${PREFIX}/include
 
-make INCLUDES="-I$PREFIX/include" CXXFLAG="-g -Wall -O2 -std=c++11" 
+mkdir -p ${PREFIX}/bin
 
-OSTYPE=$(shell echo `uname`)
-MACHINETYPE=$(shell echo `uname -m`)
-
-ifeq(${MACHINETYPE}, x86_64)
-  MACHINETYPE=amd64
-endif
-
-ifeq(${MACHINETYPE}, Power Macintosh)
-  MACHINETYPE=ppc
-endif
-
-ifeq(${OSTYPE}, SunOS)
-  MACHINETYPE=${shell echo `uname -p`}
-  ifeq(${MACHINETYPE}, sparc)
-    ifeq (${shell /usr/bin/isainfo -b}, 64)
-      MACHINETYPE=sparc64
-    else
-      MACHINETYPE=sparc32
-    endif
-  endif
-endif
-
-cp $(PREFIX)/$(OSTYPE)-$(MACHINETYPE)/bin/Fec $(PREFIX)/bin/Fec
+mv Fec ${PREFIX}/bin
+chmod +x ${PREFIX}/bin/Fec
