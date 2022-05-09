47343da26c739239e02ec6c9e9b973a7b435daa5
Celio Santos-Jr <celio.diasjunior@gmail.com>
2022-05-09 15:28:59 -0300

    Update build.sh removing prodigal compilation

--- recipes/macrel/build.sh
+++ recipes/macrel/build.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-mkdir -p $PREFIX/bin
-
-cd prodigal_modified
-make CC=$CC --quiet # conda will add $CC to environment
-chmod +x prodigal
-cd ..
-
-cp $SRC_DIR/prodigal_modified/prodigal $PREFIX/bin/prodigal_sm
-
-$PYTHON -m pip install --disable-pip-version-check --no-cache-dir --ignore-installed --no-deps -vv .
-
+$PYTHON -m pip install --disable-pip-version-check --no-cache-dir --ignore-installed --no-deps --use-feature=in-tree-build -vv .
