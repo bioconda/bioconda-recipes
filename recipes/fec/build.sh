c6bcec470305f8168e451bcb0560c4cf7fa0356e
zhangjun <zhangjuncsu@csu.edu.cn>
2022-05-10 21:33:57 +0800

    update build.sh and meta.yaml of fec

--- recipes/fec/build.sh
+++ recipes/fec/build.sh
@@ -2,5 +2,35 @@
 
 mkdir -p ${PREFIX}/bin
 
-mv Fec ${PREFIX}/bin
-chmod +x ${PREFIX}/bin/Fec
+export CPATH=${PREFIX}/include
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+sed -i.bak "369,375d" Makefile
+make CFLAGS="$CFLAGS -D_GLIBCXX_PARALLEL -pthread -O3 -Wall -std=gnu99" CXXFLAGS="$CXXFLAGS -D_GLIBCXX_PARALLEL -pthread -O3 -Wall -std=c++11"
+
+OSTYPE=`uname`
+MACHINETYPE=`uname -m`
+
+if [ $MACHINETYPE = x86_64 ]; then
+        MACHINETYPE="amd64"
+fi
+
+if [ ${MACHINETYPE} = "Power Macintosh" ]; then
+        MACHINETYPE="ppc"
+fi
+
+if [ ${OSTYPE} = "SunOS" ]; then
+        MACHINETYPE=`uname -p`
+        if [ ${MACHINETYPE} = "sparc" ]; then
+                if [ ${/usr/bin/isainfo -b} = "64" ]; then
+                        MACHINETYPE="sparc64"
+                else
+                        MACHINETYPE="sparc332"
+                fi
+        fi
+fi
+
+cp ${OSTYPE}-${MACHINETYPE}/bin/Fec ${PREFIX}/bin
