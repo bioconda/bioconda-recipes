ae29088001e5548cfea29fb95c9c94cd4b555a1b
Devon Ryan <dpryan79@users.noreply.github.com>
2022-05-10 15:11:35 +0200

    Update build.sh

--- recipes/fec/build.sh
+++ recipes/fec/build.sh
@@ -4,5 +4,6 @@
 
 #mv Fec ${PREFIX}/bin
 #chmod +x ${PREFIX}/bin/Fec
-make
+sed -i.bak "369,375d" Makefile
+make CFLAGS="$CFLAGS -D_GLIBCXX_PARALLEL -pthread -O3 -Wall -std=gnu99" CXXFLAGS="$CXXFLAGS -D_GLIBCXX_PARALLEL -pthread -O3 -Wall -std=c++11"
 ls -l
