ad4f283196cbe0ae2190aa43d6115d815e3b89ce
ShixiangWang <wangshx@shanghaitech.edu.cn>
2022-05-12 00:11:14 +0800

    export cppflags and ldflags

--- recipes/methpipe/build.sh
+++ recipes/methpipe/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+CPPFLAGS+=-I $BUILD_PREFIX/include
+LDFLAGS+=-L  $BUILD_PREFIX/lib
+export CPPFLAGS
+export LDFLAGS
 
+echo "Current path is $PWD"
+ls $PWD
 mkdir build && cd build
-cmake ../ -DBOOST_ROOT=$PREFIX -DAGS_EXAMPLE_CPP_LIB_ROOT=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
-#../configure --prefix ${PREFIX}
+../configure --prefix ${PREFIX}
 make
 make install
