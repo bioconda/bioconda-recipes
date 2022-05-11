bf7caceec10d8219156946f029e69dfa20b3e33f
ShixiangWang <wangshx@shanghaitech.edu.cn>
2022-05-11 23:42:48 +0800

    try following template

--- recipes/methpipe/build.sh
+++ recipes/methpipe/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}
-
 mkdir build && cd build
-../configure --prefix ${PREFIX}
+cmake ../ -DBOOST_ROOT=$PREFIX -DAGS_EXAMPLE_CPP_LIB_ROOT=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+#../configure --prefix ${PREFIX}
 make
 make install
