7ea602f3fcdf286c48cfb9bc540c3a716713e51b
Pierre Chaumeil <uqpchaum@uq.edu.au>
2022-05-13 09:31:25 +1000

    Update download-db.sh

--- recipes/gtdbtk/download-db.sh
+++ recipes/gtdbtk/download-db.sh
@@ -1,13 +1,13 @@
 set -e
-​
+
 # Configuration
 N_FILES_IN_TAR=139919
 DB_URL="https://data.gtdb.ecogenomic.org/releases/release207/207.0/auxillary_files/gtdbtk_r207_v2_data.tar.gz"
 TARGET_TAR_NAME="gtdbtk_r207_v2_data.tar.gz"
-​
+
 # Script variables (no need to configure)
 TARGET_TAR="${GTDBTK_DATA_PATH}/${TARGET_TAR_NAME}"
-​
+
 # Check if this is overriding an existing version
 mkdir -p "$GTDBTK_DATA_PATH"
 n_folders=$(find "$GTDBTK_DATA_PATH" -maxdepth 1 -type d | wc -l)
@@ -24,22 +24,22 @@ if [ "$n_folders" -gt 1 ]; then
     exit 1
   fi
 fi
-​
+
 # Ensure that the GTDB-Tk data directory exists
 mkdir -p "$GTDBTK_DATA_PATH"
-​
+
 # Start the download process
 # Note: When this URL is updated, ensure that the "--total" flag of TQDM below is also updated
 echo "Downloading the GTDB-Tk database to: ${GTDBTK_DATA_PATH}"
 wget $DB_URL -O "$TARGET_TAR"
-​
+
 # Uncompress and pipe output to TQDM
 echo "Extracting archive..."
 tar xvzf "$TARGET_TAR" -C "${GTDBTK_DATA_PATH}" --strip 1 | tqdm --unit=file --total=$N_FILES_IN_TAR --smoothing=0.1 >/dev/null
-​
+
 # Remove the file after successful extraction
 rm "$TARGET_TAR"
-​
+
 # Exit
 echo "GTDB-Tk database has been successfully downloaded."
 exit 0
