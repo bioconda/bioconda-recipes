405a5a74025c0950ab12ad2e5e3636a93f5669a7
ShixiangWang <wangshx@shanghaitech.edu.cn>
2022-05-12 08:52:13 +0800

    try removing build_prefix

--- recipes/methpipe/build.sh
+++ recipes/methpipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export CPPLAGS="$CPPFLAGS -I$PREFIX/include -I$BUILD_PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib -L$BUILD_PREFIX/lib"
+export CPPLAGS="$CPPFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 mkdir build && cd build
 ../configure --prefix ${PREFIX}
