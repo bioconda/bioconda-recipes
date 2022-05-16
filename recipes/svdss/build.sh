73543924a77e9e283e22a90086d9005589517898
Luca Denti <ldenti92@gmail.com>
2022-05-16 21:07:29 +0200

    Updates svdss to v1.0.2 (cmake)

--- recipes/svdss/build.sh
+++ recipes/svdss/build.sh
@@ -1,34 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git clone https://github.com/jeffdaily/parasail.git
-cd parasail
-git checkout v2.5
-mkdir build
+mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCONDAPREFIX=${PREFIX} ..
 make
-cmake --install . --prefix ${PREFIX} 
-cd ../..
-
-git clone https://github.com/maxbachmann/rapidfuzz-cpp.git
-cd rapidfuzz-cpp
-git checkout d1e8237
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build .
-cd ../..
-
-git clone https://github.com/yangao07/abPOA.git
-cd abPOA
-git checkout 42a09de
 cd ..
-
-git clone https://github.com/5cript/interval-tree.git
-
-git clone https://github.com/lh3/ropebwt2.git
-
-make
 
 mkdir -p ${PREFIX}/bin
 cp SVDSS ${PREFIX}/bin
