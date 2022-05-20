d385d3416eac7e67121791cd9750ff01bc1f2fa8
Veronica Marijke Sondervan <vms351@log-2.hpc.nyu.edu>
2022-05-20 15:17:35 -0400

    Refresh build script

--- recipes/orthologid/build.sh
+++ recipes/orthologid/build.sh
@@ -1,2 +1,29 @@
-export OID_HOME=$PREFIX
-export OID_USER_DIR=$PREFIX/testdata/
+#!/bin/sh
+
+mkdir $PREFIX/orthologID
+
+cp *.sh $PREFIX/orthologID
+cp *pbs $PREFIX/orthologID
+cp *txt $PREFIX/orthologID
+cp README $PREFIX/orthologID
+cp readme_troubleshoot $PREFIX/orthologID
+cp testdata $PREFIX/orthologID
+cp VERSION $PREFIX/orthologID
+cp run_snpgenie_oid_EXAMPLE.pl $PREFIX/orthologID
+cp -r bin $PREFIX/orthologID
+cp -r config $PREFIX/orthologID
+cp -r lib $PREFIX/orthologID
+cp -r phylobrowse $PREFIX/orthologID
+cp -r PostProcessing $PREFIX/orthologID
+cp -r testdata $PREFIX/orthologID
+
+chmod a+x $PREFIX/orthologID/bin/orthologid.pl
+chmod a+x $PREFIX/orthologID/bin/topshell.sh
+chmod a+x $PREFIX/orthologID/PostProcessing/*.sh
+chmod a+x $PREFIX/orthologID/PostProcessing/*.py
+chmod a+x $PREFIX/orthologID/PostProcessing/*.pl
+chmod a+x $PREFIX/orthologID/PostProcessing/*.R
+
+export OID_HOME=$PREFIX/orthologID
+
+#export OID_USER_DIR=$PREFIX/testdata/
