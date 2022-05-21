401783ddc28c641c27f88d87d89fde79baa0e7f5
Zilong-Li <zimusen94@gmail.com>
2022-05-21 12:16:28 +0200

    Update build.sh

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ${PREFIX}/bin
 # fix zlib issue
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CFLAGS="-I$PREFIX/include"
+export CXXFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 make -f conda.makefile MKLROOT=${PREFIX}
 mv PCAone ${PREFIX}/bin/
