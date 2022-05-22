9cd6cfdbc93a87fb858315254e9b52e1b6cda410
Zilong-Li <zimusen94@gmail.com>
2022-05-22 11:44:40 +0200

    disable avx2 for outdated server

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -5,5 +5,10 @@ mkdir -p ${PREFIX}/bin
 export CFLAGS="-I$PREFIX/include"
 export CXXFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-make MKLROOT=${PREFIX}
+avx=1
+if [ $(uname -s) == "Linux" ];then
+  grep "avx2" /proc/cpuinfo |grep "fma" &>/dev/null
+  [[ $? != 0 ]] && avx=0
+fi
+make MKLROOT=${PREFIX} AVX=$avx
 mv PCAone ${PREFIX}/bin/
