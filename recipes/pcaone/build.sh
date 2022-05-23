34bff5a4fd3808da3a0f8c1aef1e8b80a2ae7e13
Zilong-Li <zimusen94@gmail.com>
2022-05-23 21:43:54 +0200

    add bash wrapper

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -5,10 +5,17 @@ mkdir -p ${PREFIX}/bin
 export CFLAGS="-I$PREFIX/include"
 export CXXFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
-axv=1
+
 if [ $(uname -s) == "Linux" ];then
-  grep "avx2" /proc/cpuinfo |grep "fma" &>/dev/null
-  [[ $? != 0  ]] && avx=0
+  make MKLROOT=${PREFIX} AVX=0 && mv PCAone ${PREFIX}/bin/PCAone.x64
+  make MKLROOT=${PREFIX} AVX=1 && mv PCAone ${PREFIX}/bin/PCAone.avx2
+	echo "#!/usr/bin/env bash
+PCAone=${PREFIX}/bin/PCAone.avx2
+grep avx2 /proc/cpuinfo |grep fma &>/dev/null
+[[ \$? != 0 ]] && PCAone=${PREFIX}/bin/PCAone.x64
+exec \$PCAone \$@" >${PREFIX}/bin/PCAone && chmod +x ${PREFIX}/bin/PCAone
+
+elif [ $(uname -s) == "Darwin" ];then
+  make MKLROOT=${PREFIX}
+  mv PCAone ${PREFIX}/bin/
 fi
-make MKLROOT=${PREFIX} AVX=${avx}
-mv PCAone ${PREFIX}/bin/
