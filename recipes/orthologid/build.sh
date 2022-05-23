26922ee0e27e1db3e08efd7457c93cdac17cb2eb
Veronica Marijke Sondervan <vms351@log-1.hpc.nyu.edu>
2022-05-23 10:57:34 -0400

    Fix bash

--- recipes/orthologid/build.sh
+++ recipes/orthologid/build.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-mkdir $PREFIX/orthologID
+mkdir $PREFIX/share/orthologID
 
-cp *.sh $PREFIX/orthologID
-cp *pbs $PREFIX/orthologID
-cp *txt $PREFIX/orthologID
-cp README $PREFIX/orthologID
-cp VERSION $PREFIX/orthologID
-cp run_snpgenie_oid_EXAMPLE.pl $PREFIX/orthologID
-cp -r bin $PREFIX/orthologID
-cp -r config $PREFIX/orthologID
-cp -r lib $PREFIX/orthologID
-cp -r PostProcessing $PREFIX/orthologID
-cp -r testdata $PREFIX/orthologID
+cp *.sh $PREFIX/share/orthologID
+cp *pbs $PREFIX/share/orthologID
+cp *txt $PREFIX/share/orthologID
+cp README $PREFIX/share/orthologID
+cp VERSION $PREFIX/share/orthologID
+cp run_snpgenie_oid_EXAMPLE.pl $PREFIX/share/orthologID
+cp -r bin $PREFIX/bin
+cp -r config $PREFIX/config
+cp -r lib $PREFIX/lib
+cp -r PostProcessing $PREFIX/share/orthologID
+cp -r testdata $PREFIX/share/orthologID
 
-chmod a+x $PREFIX/orthologID/bin/orthologid.pl
-chmod a+x $PREFIX/orthologID/bin/topshell.sh
-chmod a+x $PREFIX/orthologID/PostProcessing/*.sh
-chmod a+x $PREFIX/orthologID/PostProcessing/*.py
-chmod a+x $PREFIX/orthologID/PostProcessing/*.pl
-chmod a+x $PREFIX/orthologID/PostProcessing/*.R
+chmod a+x $PREFIX/bin/orthologid.pl
+chmod a+x $PREFIX/bin/topshell.sh
+chmod a+x $PREFIX/share/orthologID/PostProcessing/*.sh
+chmod a+x $PREFIX/share/orthologID/PostProcessing/*.py
+chmod a+x $PREFIX/share/orthologID/PostProcessing/*.pl
+chmod a+x $PREFIX/share/orthologID/PostProcessing/*.R
 
 export OID_HOME="${PREFIX}/orthologID"
 export OID_USER_DIR="${PREFIX}/orthologID/testdata/"
-export PATH="$PATH:${PREFIX}/orthologID/bin"
