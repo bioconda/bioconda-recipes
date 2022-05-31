ee674c981334a607d45d701cbdadc7dda3c87e29
Zilong-Li <zimusen94@gmail.com>
2022-05-31 12:40:37 +0200

    link to libiomp5

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -6,10 +6,12 @@ export CFLAGS="-I$PREFIX/include"
 export CXXFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
+sed -i.bak '/-march=native/d' Makefile
+
 if [ $(uname -s) == "Linux" ];then
-  make MKLROOT=${PREFIX} AVX=0 && mv PCAone ${PREFIX}/bin/PCAone.x64
-  make MKLROOT=${PREFIX} AVX=1 && mv PCAone ${PREFIX}/bin/PCAone.avx2
-	echo "#!/usr/bin/env bash
+  make clean && make MKLROOT=${PREFIX} AVX=0 && mv PCAone ${PREFIX}/bin/PCAone.x64
+  make clean && make MKLROOT=${PREFIX} AVX=1 && mv PCAone ${PREFIX}/bin/PCAone.avx2
+  echo "#!/usr/bin/env bash
 PCAone=${PREFIX}/bin/PCAone.avx2
 grep avx2 /proc/cpuinfo |grep fma &>/dev/null
 [[ \$? != 0 ]] && PCAone=${PREFIX}/bin/PCAone.x64
