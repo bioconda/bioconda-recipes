cadf41d8ea49d0d216d15cccd3f1b980ea7d1ac8
Robert A. Petit III <robbie.petit@gmail.com>
2022-05-31 23:32:20 -0600

    add faidx index

--- recipes/shigatyper/build.sh
+++ recipes/shigatyper/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Build index
+samtools faidx shigatyper/resources/ShigellaRef5.fasta
 minimap2 -d shigatyper/resources/ShigellaRef5.mmi shigatyper/resources/ShigellaRef5.fasta
 
 # ShigaTyper
