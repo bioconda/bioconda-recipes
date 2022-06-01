a1b54202b64523ad71cd5320fdd4a2c00e2ac511
Robert A. Petit III <robbie.petit@gmail.com>
2022-05-31 22:47:37 -0600

    Update build.sh

--- recipes/shigatyper/build.sh
+++ recipes/shigatyper/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# Build index
+minimap2 -d shigatyper/resources/ShigellaRef5.mmi shigatyper/resources/ShigellaRef5.fasta
+
 # ShigaTyper
 $PYTHON -m pip install --no-deps --ignore-installed -vv .
-
-# Run single genome to build index
-minimap2 -d shigatyper/resources/ShigellaRef5.mmi shigatyper/resources/ShigellaRef5.fasta
