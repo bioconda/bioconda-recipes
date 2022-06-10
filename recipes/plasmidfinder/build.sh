863ec55b1ecbca1ac8b62d3d1fc5186e15bc3744
Robert A. Petit III <robbie.petit@gmail.com>
2022-06-10 13:21:11 -0600

    Update build.sh

--- recipes/plasmidfinder/build.sh
+++ recipes/plasmidfinder/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Update default DB location
-PLASMIDFINDER_SHARE=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}
-sed -i "s=BIOCONDA_SED_REPLACE=$PLASMIDFINDER_SHARE=" plasmidfinder.py
+PLASMIDFINDER_DB=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/database
+sed -i "s=BIOCONDA_SED_REPLACE=$PLASMIDFINDER_DB=" plasmidfinder.py
 
 # Copy plasmidfinder
 mkdir -p ${PREFIX}/bin
@@ -14,13 +14,13 @@ chmod +x ${RECIPE_DIR}/download-db.sh
 cp ${RECIPE_DIR}/download-db.sh ${PREFIX}/bin/download-db.sh
 
 # Build database
-mkdir -p ${PLASMIDFINDER_SHARE}/database/
-bash -x ${PREFIX}/bin/download-db.sh ${PLASMIDFINDER_SHARE}/database/
+mkdir -p ${PLASMIDFINDER_DB}
+bash -x ${PREFIX}/bin/download-db.sh ${PLASMIDFINDER_DB}
 
 # set PLASMID_DB variable on env activation
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
 cat <<EOF >> ${PREFIX}/etc/conda/activate.d/plasmidfinder.sh
-export PLASMID_DB=${PLASMIDFINDER_SHARE}/database/
+export PLASMID_DB=${PLASMIDFINDER_DB}
 EOF
 
 cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/plasmidfinder.sh
