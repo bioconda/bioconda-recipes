c9e79849f693cd67cac1e9b20fc53ebe1d91a997
Robert A. Petit III <robbie.petit@gmail.com>
2022-06-14 15:45:32 -0600

    add database to bioconda build

---
+++ recipes/seroba/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update default DB location
+mkdir -p ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}
+cp -r database ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/
+SEROBA_DB=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/database
+sed -i "s=BIOCONDA_SED_REPLACE=$SEROBA_DB=" scripts/seroba
+
+# build seroba
+$PYTHON -m pip install . --ignore-installed --no-deps -vv
