f91c4281d0963cdc10e68dcd273af9290f173516
Robert A. Petit III <robbie.petit@gmail.com>
2022-06-14 17:23:45 -0600

    patch kmc memory requirement

--- recipes/seroba/build.sh
+++ recipes/seroba/build.sh
@@ -8,3 +8,6 @@ sed -i "s=BIOCONDA_SED_REPLACE=$SEROBA_DB=" scripts/seroba
 
 # build seroba
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
+
+# Create database with recommended kmer size (k=71)
+seroba createDBs ${SEROBA_DB} 71
