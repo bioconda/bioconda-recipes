ad6721ae051170ad2fa16d38cc02eb625ff90dc2
karentherkelsen <55749031+karentherkelsen@users.noreply.github.com>
2022-06-15 17:20:03 +0200

    Correct typo

--- recipes/mlst-cge/download-db.sh
+++ recipes/mlst-cge/download-db.sh
@@ -3,7 +3,7 @@
 echo "Downloading MLST database to ${MLST_DB}..."
 
 cd ${MLST_DB}
-# download ResFinder database
+# download MLST database
 git clone https://git@bitbucket.org/genomicepidemiology/mlst_db.git
 cd mlst_db
 python INSTALL.py
