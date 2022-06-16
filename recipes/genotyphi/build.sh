9dc95a24abc8dc36c37c27b9a2f7ca9306e4bf89
Robert A. Petit III <robbie.petit@gmail.com>
2022-06-16 10:08:20 -0600

    Update build.sh

--- recipes/genotyphi/build.sh
+++ recipes/genotyphi/build.sh
@@ -7,6 +7,6 @@ chmod +x $PREFIX/bin/genotyphi.py $PREFIX/bin/genotyphi
 
 # Add script for Mykrobe based analysis
 wget -O parse_typhi_mykrobe.py https://github.com/katholt/genotyphi/raw/main/parse_typhi_mykrobe.py
-echo "#! /usr/bin/env python3" > $PREFIX/bin/parse_typhi_mykrobe.py
+echo "#! /usr/bin/env python" > $PREFIX/bin/parse_typhi_mykrobe.py
 cat parse_typhi_mykrobe.py >> $PREFIX/bin/parse_typhi_mykrobe.py
 chmod 755 $PREFIX/bin/parse_typhi_mykrobe.py
