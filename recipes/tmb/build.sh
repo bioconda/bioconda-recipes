b57ce9b639799cef1fb8df7d21b3519f9cefa4f6
Alexander Peltzer <alex.peltzer@gmail.com>
2022-06-17 09:28:10 +0000

    Add recipe for TMB-curie

---
+++ recipes/tmb/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+mkdir -p  "${PREFIX}/bin" "${PREFIX}/config" "${PREFIX}/test"
+
+mv bin/* "${PREFIX}/bin"
+mv config/* "${PREFIX}/config"
+mv test/* "${PREFIX}/test"
