3423136e08952ffd990ffb4798001a6448e4f1ae
canfirtina <canfirtina@gmail.com>
2022-06-20 17:36:49 +0200

    Simplifying the build

--- recipes/fastremap-bioconda/build.sh
+++ recipes/fastremap-bioconda/build.sh
@@ -4,4 +4,6 @@ mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
 
-make CC=$CC EXECUTABLE="$PREFIX/bin/FastRemap" LDFLAGS="-lz -lpthread -L$PREFIX/lib" INCLUDES="-I$PREFIX/include" CFLAGS="-c -O3 -std=c++2a -pthread -DNDEBUG -DSEQAN_HAS_ZLIB=1 -DSEQAN_DISABLE_VERSION_CHECK=YES -W -Wall -pedantic -L$PREFIX/lib"
+cd zlib && ./configure && make && cd ..
+
+make CC=$CC EXECUTABLE="$PREFIX/bin/FastRemap"
