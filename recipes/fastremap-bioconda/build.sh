b3adbd9b6b2829e065f61f53a0b1d3c5d813d2bb
canfirtina <canfirtina@gmail.com>
2022-06-20 17:04:51 +0200

    LDFLAGS

--- recipes/fastremap-bioconda/build.sh
+++ recipes/fastremap-bioconda/build.sh
@@ -3,5 +3,7 @@
 mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
+export CFLAGS="-c -O3 -std=c++2a -pthread -DNDEBUG -DSEQAN_HAS_ZLIB=1 -DSEQAN_DISABLE_VERSION_CHECK=YES -I$PREFIX/include -L$PREFIX/lib"
+export LDFLAGS="$LDFLAGS -lz -lpthread -L$PREFIX/lib"
 
-make CC=$CC EXECUTABLE="$PREFIX/bin/FastRemap" INCLUDES="-I$PREFIX/include" CFLAGS="-c -O3 -std=c++2a -pthread -O3 -DNDEBUG -DSEQAN_HAS_ZLIB=1 -DSEQAN_DISABLE_VERSION_CHECK=YES -W -Wall -pedantic -L$PREFIX/lib"
+make CC=$CC EXECUTABLE="$PREFIX/bin/FastRemap" LDFLAGS=$LDFLAGS INCLUDES="-I$PREFIX/include" CFLAGS=$CFLAGS
