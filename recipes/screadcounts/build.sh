412b61f014156aaf03ed5bdd7d5c5031a2c92223
Nathan Edwards <edwardsnj@gmail.com>
2022-06-22 00:23:12 -0400

    hope so? or not?

--- recipes/screadcounts/build.sh
+++ recipes/screadcounts/build.sh
@@ -2,10 +2,11 @@
 
 set -eu
 
-TARGET=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+# TARGET=$BUILD_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+TARGET=$BUILD_PREFIX/share/$PKG_NAME
 mkdir -p $TARGET
-mkdir -p $PREFIX/bin
-cp -r bin src docs data $TARGET
+mkdir -p $BUILD_PREFIX/bin
+cp -r $SRC_DIR/{bin,src,docs,data} $TARGET
 chmod -R a+r $TARGET/
 chmod -R a+x $TARGET/bin/
-ln -s $TARGET/bin/* $PREFIX/bin
+ln -s $TARGET/bin/* $BUILD_PREFIX/bin
