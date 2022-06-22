f1df1602946e7b9905965798ba6ba05753cecc32
Nathan Edwards <edwardsnj@gmail.com>
2022-06-21 22:42:02 -0400

    another attempt...

--- recipes/scexecute/build.sh
+++ recipes/scexecute/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-true
+mkdir -p $PREFIX/$PKG_NAME
+cp -r $SRC_DIR/* $PREFIX/$PKG_NAME
+for a in $PREFIX/$PKG_NAME/bin/*; do
+  b=`basename $a`
+  ( cd $PREFIX/bin; ln -s ../$PKG_NAME/bin/$b )
+done
