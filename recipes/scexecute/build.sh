af7d8dec21426b0cb57a587e0dcb06ca693442b4
Nathan Edwards <edwardsnj@gmail.com>
2022-06-21 23:10:47 -0400

    round in circles...

--- recipes/scexecute/build.sh
+++ recipes/scexecute/build.sh
@@ -4,5 +4,5 @@ mkdir -p $PREFIX/$PKG_NAME
 cp -r $SRC_DIR/* $PREFIX/$PKG_NAME
 for a in $PREFIX/$PKG_NAME/bin/*; do
   b=`basename $a`
-  ( cd $PREFIX/bin; ln -s ../$PKG_NAME/bin/$b )
+  ln -s $a $PREFIX/bin/$b
 done
