ceff1c27e0ccb7ca54e431f94ec130a593b0b29f
Amirhossein Afshinfard <31241012+aafshinfard@users.noreply.github.com>
2022-06-22 16:45:00 -0700

    Update build.sh

--- recipes/physlr/build.sh
+++ recipes/physlr/build.sh
@@ -9,4 +9,4 @@ cp -a physlr ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/
 
 cp src/physlr-indexlr src/physlr-makebf src/physlr-filter-bxmx src/physlr-filter-barcodes src/physlr-overlap src/physlr-split-minimizers ${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/src/
 
-printf '#!/bin/bash\nexec make -f $(command -v '"${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/physlr-make"') "$@" >${PREFIX}/bin/physlr
+printf '#!/bin/bash\nexec make -f $(command -v '"${PREFIX}/bin/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/physlr-make"') "$@"' >${PREFIX}/bin/physlr
