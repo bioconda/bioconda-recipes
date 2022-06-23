c3daafb9b3a690bae8ebf92e096f9c581f508633
Nathan Edwards <edwardsnj@gmail.com>
2022-06-23 10:08:16 -0400

    Remove data subdirectory from conda package

--- recipes/screadcounts/build.sh
+++ recipes/screadcounts/build.sh
@@ -4,6 +4,7 @@ TARGET=share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/$TARGET/bin
 mkdir -p $PREFIX/bin
 cp -r * $PREFIX/$TARGET
+rm -rf $PREFIX/$TARGET/data
 chmod -R a+rX $PREFIX/$TARGET
 chmod -R a+rx $PREFIX/$TARGET/bin/
 cd $PREFIX/bin
