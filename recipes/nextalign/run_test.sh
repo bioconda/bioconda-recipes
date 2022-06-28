185ee98397f3b05418a80e358f2a157918b285b2
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-06-28 14:26:56 +0200

    fix(test): sequences now passed with positional arg

--- recipes/nextalign/run_test.sh
+++ recipes/nextalign/run_test.sh
@@ -3,9 +3,8 @@
 set -euxo pipefail
 
 nextalign \
---sequences test-data/sequences.fasta \
+test-data/sequences.fasta \
 --reference test-data/reference.fasta \
 --genemap test-data/genemap.gff \
---genes E,M,N,ORF1a,ORF1b,ORF3a,ORF6,ORF7a,ORF7b,ORF8,ORF9b,S \
 --output-dir output/ \
 --output-basename nextalign
