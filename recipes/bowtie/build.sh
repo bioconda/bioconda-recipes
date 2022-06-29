6fa897d9768f8038cb4dd59ca35be478fc65a1bc
Alex Tate <0xalextate@gmail.com>
2022-06-29 16:13:31 -0700

    Trying an alternative approach. Instead of compiling on OSX, instead get the precompiled binaries from releases on the bowtie repo.

--- recipes/bowtie/build.sh
+++ recipes/bowtie/build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-make prefix="${PREFIX}" install
+if [[ "$OSTYPE" != "darwin"* ]]; then
+
+  make prefix="${PREFIX}" install
+
+else
+  declare -a BIN_LIST=(
+    bowtie-build-s
+    bowtie-build-l
+    bowtie-align-s
+    bowtie-align-l
+    bowtie-inspect-s
+    bowtie-inspect-l
+  )
+
+  declare -a PYTHON_WRAPPER_LIST=(
+    bowtie-inspect
+    bowtie-build
+    bowtie
+  )
+
+  for file in "${BIN_LIST[@]}" "${PYTHON_WRAPPER_LIST[@]}"; do
+    cp -f $file "${PREFIX}/bin/"
+  done
+fi
 
 cp -r scripts "${PREFIX}/bin/"
