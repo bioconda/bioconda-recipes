ab0319a3600923dd133a0d44fbab9a7215be21f2
Thomas Weber <thomas.weber@embl.de>
2022-06-30 16:55:06 +0200

    Update mosaic meta.yaml & correct git url

--- recipes/mosaic/build.sh
+++ recipes/mosaic/build.sh
@@ -3,6 +3,10 @@
 # Ensure we run successfully using either conda-forge or defaults ncurses
 # (unlike other platforms, the latter does not automatically pull in libtinfo)
 
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 mkdir build
 cd build
 cmake ../src
