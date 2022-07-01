9f9744312ed21e7ebee84d5c20e40a72aa06e248
wasineer-dev <wasinees@gmail.com>
2022-07-01 19:22:29 +0700

    Add __init__.py

--- recipes/braid_mrf/setup.py
+++ recipes/braid_mrf/setup.py
@@ -2,12 +2,13 @@
 
 from setuptools import find_packages, setup
 
-setup(name='meanfield',
+setup(name='braid_mrf',
       version='1.0.1',
       description='A ',
       author='Wasinee Rungsarityotin',
       author_email='wasinees@gmail.com',
       url='https://github.com/wasineer-dev/braid',
+      package_dir= {".": "meanfield"},
       packages=find_packages(
         where='.',
         include=['meanfield*'],  # ["*"] by default
