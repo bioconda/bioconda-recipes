dca752632242ba43b18ef7217f2056ea2385aaa9
Thomas Weber <thomas.weber@embl.de>
2022-07-01 16:07:05 +0200

    Update mosaic meta.yaml Change requirements/build step Update compiler -lhts option in make command (build.sh) Package versions pinned

--- recipes/mosaic/build.sh
+++ recipes/mosaic/build.sh
@@ -6,12 +6,21 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+# export CPATH=${BUILD_PREFIX}/bin
+export PKG_CONFIG_EXECUTABLE=${BUILD_PREFIX}/bin/pkg-config
 
 which pkg-config
+which cmake
+echo "$CPATH"
+
+sed -i '25 set(PKG_CONFIG_EXECUTABLE ${PKG_CONFIG_EXECUTABLE})' src/CMakeLists.txt
 
 mkdir build
 cd build
 
-cmake ../src
+cmake \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}
+../src
 
 make CC="${CC}" CXX="${CXX}" LDFLAGS="${LDFLAGS}" -lhts -lboost_iostreams
