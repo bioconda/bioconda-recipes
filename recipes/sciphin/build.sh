12260c919909fe3fa2e3d5f3e4a10a3adeac4a09
Jochen Singer <jochensinger@googlemail.com>
2022-07-03 21:02:46 +0200

    removed some deprecated build instructions and adjusted to a new release version

--- recipes/sciphin/build.sh
+++ recipes/sciphin/build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# conda on macOS will fall back to libstdc++
-# which lacks a ton of standard C++11 headers
-if [[ $(uname) == Darwin ]]; then
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-    grep -r no_warning_for
-fi
-
 curdir=$(pwd)
 
 mkdir -p build
