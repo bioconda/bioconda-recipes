5fb671b46e36e6e937f1fd9ac5957de92f17a7ca
Alex Tate <0xalextate@gmail.com>
2022-07-03 10:19:37 -0700

    Got to the bottom of why GNU patch does not rename files on macOS. This functionality was introduced in patch 2.7 but macOS ships with patch 2.5. I've removed the "rename" line from build.sh and instead made Anaconda's patch package (v2.7.6) a build requirement, selective for osx.

--- recipes/bowtie/build.sh
+++ recipes/bowtie/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# The VERSION file is mistaken for a C++20 version header file
-# The patch does not rename the VERSION file on OSX, so we rename it here
-mv VERSION{,.txt} || true
-
 make prefix="${PREFIX}" install
 
 cp -r scripts "${PREFIX}/bin/"
