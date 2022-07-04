50f9fb258b73b516a7887fbd9a7e334e98b1b933
Thomas Weber <thomas.weber@embl.de>
2022-07-04 13:16:24 +0200

    Update mosaic meta.yaml Change requirements/build step Update compiler -lhts option in make command (build.sh) Package versions pinned bzip2 added make lboost CMakeLists.txt updated v2 (hts external package) DK update

--- recipes/mosaic/build.sh
+++ recipes/mosaic/build.sh
@@ -3,10 +3,9 @@
 # Ensure we run successfully using either conda-forge or defaults ncurses
 # (unlike other platforms, the latter does not automatically pull in libtinfo)
 
-export CFLAGS="-I$PREFIX/include"
-export CXXFLAGS="${CFLAGS}"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+# export CFLAGS="-I$PREFIX/include"
+# export LDFLAGS="-L$PREFIX/lib"
+# export CPATH=${PREFIX}/include
 # export CPATH=${BUILD_PREFIX}/bin
 # export PKG_CONFIG_EXECUTABLE=${BUILD_PREFIX}/bin/pkg-config
 
@@ -16,7 +15,7 @@ export CPATH=${PREFIX}/include
 
 # sed -i '25 i set(PKG_CONFIG_EXECUTABLE ${PKG_CONFIG_EXECUTABLE})' src/CMakeLists.txt
 
-cat src/CMakeLists.txt
+# cat src/CMakeLists.txt
 
 # cmake ../src
 
@@ -32,4 +31,4 @@ cmake ../src \
     -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 
-make -lhts
+make
