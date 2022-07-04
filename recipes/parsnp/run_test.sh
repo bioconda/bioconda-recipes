d537ffc6314374380f10514cfb9ee23b4ebc50e2
Bryce Kille <brycekille@gmail.com>
2022-07-03 20:25:04 -0500

    Update run_test.sh

--- recipes/parsnp/run_test.sh
+++ recipes/parsnp/run_test.sh
@@ -7,3 +7,4 @@ parsnp -g mers_virus/ref/England1.gbk -d mers_virus/genomes/*.fna -C 1000 -c -o 
 parsnp -r ! -d mers_virus/genomes/*.fna -o test2 --verbose
 parsnp -g mers_virus/ref/England1.gbk -d mers_virus/genomes/*.fna -p 2 -C 1000 -c -o test --verbose --use-fasttree
 parsnp -r ! -d mers_virus/genomes/*.fna -o test2 --verbose -p 2
+parsnp -r ! -d mers_virus/genomes/*.fna -o test3 --verbose -p 2 --extend-lcbs
