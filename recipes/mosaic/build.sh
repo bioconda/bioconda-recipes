b82a50306d939aa24815021cc4084a1c2f7b9f4e
Thomas Weber <thomas.weber@embl.de>
2022-07-06 13:30:09 +0200

    Git URL & Name update

--- recipes/mosaic/build.sh
+++ recipes/mosaic/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Ensure we run successfully using either conda-forge or defaults ncurses
-# (unlike other platforms, the latter does not automatically pull in libtinfo)
-
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 
