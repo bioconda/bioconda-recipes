61691c5778a608b45caa68510ee04531c0ba042e
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-07 10:18:36 -0600

    Update build.sh

--- recipes/gfmix/build.sh
+++ recipes/gfmix/build.sh
@@ -8,21 +8,21 @@ cp treecns ${PREFIX}/bin
 cp rert ${PREFIX}/bin
 cp alpha_est_mix_rt ${PREFIX}/bin
 
-mkdir -p ${PREFIX}/gfmix
-cp *dat ${PREFIX}/gfmix
+mkdir -p ${PREFIX}/gfmix-dat
+cp *dat ${PREFIX}/gfmix-dat
 
 mkdir -p ${PREFIX}/etc/conda/activate.d
 cat > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh <<EOF
 #!/bin/bash
 
-if [ -d "$HOME/.gfmix " ]; then rm -Rf $HOME/.gfmix ; fi
+if [ -d "$HOME/.gfmix " ]; then rm -rf $HOME/.gfmix ; fi
 mkdir -p $HOME/.gfmix 
-cp ${PREFIX}/bin/*.dat $HOME/.gfmix 
+cp ${PREFIX}/bin/gfmix-dat/*.dat $HOME/.gfmix 
 EOF
 
 mkdir -p ${PREFIX}/etc/conda/deactivate.d
 cat > ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh <<EOF
 #!/bin/bash
 
-if [ -d "$HOME/.gfmix " ]; then rm -Rf $HOME/.gfmix ; fi
+if [ -d "$HOME/.gfmix " ]; then rm -rf $HOME/.gfmix ; fi
 EOF
