601d8b57390219c95f003e3420d71d39e6781aad
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-08 22:01:35 -0600

    Update build.sh

--- recipes/goldrush/build.sh
+++ recipes/goldrush/build.sh
@@ -9,7 +9,7 @@ ninja
 ninja install
 
 # Make wrapper script for goldrush
-cp bin/goldrush ${PREFIX}/bin/goldrush.make
+mv ${PREFIX}/bin/goldrush ${PREFIX}/bin/goldrush.make
 echo "#!/bin/bash" > ${PREFIX}/bin/goldrush
 echo "make -rRf $(which goldrush.make)" >> ${PREFIX}/bin/goldrush
 chmod 755 ${PREFIX}/bin/goldrush*
