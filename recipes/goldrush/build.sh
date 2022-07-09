7e354d3b0db1ac65bf75c1052ee148aeca9c6503
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-08 20:22:24 -0600

    Update build.sh

--- recipes/goldrush/build.sh
+++ recipes/goldrush/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sed -i '' 's=/usr/bin/make=/usr/bin/env make=' ${RECIPE_DIR}/bin/goldrush
 
 mkdir -p ${PREFIX}
-
 meson --prefix ${PREFIX} build
 cd build
 ninja
 ninja install
 
+if [ `uname` == Darwin ]; then
+    sed -i '' 's=/usr/bin/make=/usr/bin/env make=' ${PREFIX}/bin/goldrush
+else
+    sed -i '' 's=/usr/bin/make=/usr/bin/env make=' ${PREFIX}/bin/goldrush
+fi
