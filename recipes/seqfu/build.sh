3c8f665d91e53a6685f80e0dab55d7fdf6c333cf
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2022-07-09 12:55:59 +0100

    [seqFu] add scripts

--- recipes/seqfu/build.sh
+++ recipes/seqfu/build.sh
@@ -13,8 +13,14 @@ echo "## Automatic build"
 nimble build -y --verbose 
 ./bin/seqfu version || true
 
+if [[ -d scripts ]]; then
+  chmod +x scripts/*
+  cp scripts/* bin/
+fi
+
 echo "## Current dir: $(pwd)"
 mv bin/* "$PREFIX"/bin/
+
 
 echo "## List files in \$PREFIX/bin:"
 ls -ltr "$PREFIX"/bin/
