fa2b23a7535e0dbe0ce2b00a7baebdb635343fb6
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-08 21:48:05 -0600

    Update build.sh

--- recipes/goldrush/build.sh
+++ recipes/goldrush/build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# Build goldrush
 mkdir -p ${PREFIX}/bin
+meson --prefix ${PREFIX} build
+cd build
+ninja
+ninja install
 
 # Make wrapper script for goldrush
 cp bin/goldrush ${PREFIX}/bin/goldrush.make
 echo "#!/bin/bash" > ${PREFIX}/bin/goldrush
 echo "make -rRf $(which goldrush.make)" >> ${PREFIX}/bin/goldrush
 chmod 755 ${PREFIX}/bin/goldrush*
-
-# Build goldrush
-meson --prefix ${PREFIX} build
-cd build
-ninja
-ninja install
