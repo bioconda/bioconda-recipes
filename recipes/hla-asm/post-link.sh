980ac006cbeb322e92d1c3252e338aa2fcbd7a2c
Stephan Scholz <stephan.scholz@hhu.de>
2022-07-12 21:41:20 +0200

    do not download reference automatically and ask the user to do it manually

--- recipes/hla-asm/post-link.sh
+++ recipes/hla-asm/post-link.sh
@@ -1,11 +1,4 @@
 HLA_ASM_DIR=$PREFIX/opt/hla-asm
-REF_DIR=$HLA_ASM_DIR/reference_HLA_ASM 
 DL_CMD="cd $HLA_ASM_DIR && wget 'https://www.dropbox.com/s/mnkig0fhaym43m0/reference_HLA_ASM.tar.gz' && tar -xvzf reference_HLA_ASM.tar.gz"
-MESSAGE=""
-if [ ! -d $REF_DIR ]; then
-	echo $DL_CMD
-	{ eval $DL_CMD && MESSAGE="downloaded reference (~800MB)"; } || { MESSAGE="could not download reference. Please download it manually, like: $DL_CMD"; }
-else
-	MESSAGE="reference folder $REF_DIR found. not downloading reference"
-fi
-echo $MESSAGE >> $PREFIX/.messages.txt 
+echo "HLA-ASM installed. It will not run without the reference file (800MB). Please download it manually, e.g." >> $PREFIX/.messages.txt
+echo DL_CMD >> $PREFIX/.messages.txt
