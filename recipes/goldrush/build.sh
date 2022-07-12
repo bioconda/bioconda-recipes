c60db5d0a664d61768bf28847064a18df5e80b6d
lcoombe <lauren.e.coombe@gmail.com>
2022-07-11 17:07:04 -0700

    Update goldrush wrapper

--- recipes/goldrush/build.sh
+++ recipes/goldrush/build.sh
@@ -11,5 +11,5 @@ ninja install
 # Make wrapper script for goldrush
 mv ${PREFIX}/bin/goldrush ${PREFIX}/bin/goldrush.make
 echo "#!/bin/bash" > ${PREFIX}/bin/goldrush
-echo "make -rRf $(which goldrush.make) $@" >> ${PREFIX}/bin/goldrush
+echo "make -rRf $(which goldrush.make) \$@" >> ${PREFIX}/bin/goldrush
 chmod 755 ${PREFIX}/bin/goldrush*
