f596c69de8af36258eee8a9f9fe38892eddd37fc
Jesper Svedberg <jsvedberg@gmail.com>
2022-07-14 12:00:56 +0200

    changed selam recipe

--- recipes/selam/build.sh
+++ recipes/selam/build.sh
@@ -1,20 +1,7 @@
-
 #!/bin/bash
 
-#export CONDAFLAGS="-L${PREFIX}/lib -I${PREFIX}/include"
-
-# conda on macOS will fall back to libstdc++
-# which lacks a ton of standard C++11 headers
-# if [[ $(uname) == Darwin ]]; then
-# 	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-# fi
-
-
 cd src
-#sed -i.bak 's#ahmms.cpp#ahmms.cpp -L ${PREFIX}/lib/ -I ${PREFIX}/include/#' Makefile 
 make CXX=$CXX
-
-#CONDAFLAGS=$CONDAFLAGS
 
 mkdir -p $PREFIX/bin
 cp SELAM SELAM_STATS $PREFIX/bin 
