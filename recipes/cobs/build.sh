f93c91eae79fa5934a3dfa91f3b87da61c5437db
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-14 20:19:39 +0100

    Retrying now with gcc-11 and g++-11

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -eux -o pipefail
 
-compgen -c # will list all the commands you could run.
-compgen -c | grep gcc # will list all the commands you could run.
-exit 0
-
 # make compilation not be dependent on locale settings
 export LC_ALL=C
 
 # build cobs
 mkdir -p build
 cd build
-cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
+cmake -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
 make -j1
 
 # test
