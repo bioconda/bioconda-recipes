fc7a4c6e0d259c237d68bae1e08146351be3219c
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2022-07-14 13:57:50 +0200

    Initial version of recipe for LDA2

---
+++ recipes/lda/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
+mkdir -p "$outdir"
+cp -R README* LICENSE* cache doc *.xml *.txt examples fattyAcids fragRules lda* *.jar *.so *.properties properties thirdParty tuLibs "$outdir/"
+sed "s/@@JAR_FILE@@/jgoslin-cli-${PKG_VERSION}.jar/" \
+    "$RECIPE_DIR/lda.py" \
+    > "$outdir/lda"
+chmod +x "$outdir/lda"
+
+mkdir -p "$PREFIX/bin"
+ln -s "$outdir/lda" "$PREFIX/bin/lda"
+
+echo "Contents of '$outdir'"
+ls -lA "$outdir"
