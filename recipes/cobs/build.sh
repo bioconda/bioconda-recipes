a80b07dd8890c48ba9a20244288be6ab6dc5f4d2
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-14 23:23:00 +0100

    Checking if clang works

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -7,14 +7,7 @@ export LC_ALL=C
 # build cobs
 mkdir -p build
 cd build
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-      cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-      cmake -DCMAKE_C_COMPILER=x86_64-apple-darwin20-gcc-10 -DCMAKE_CXX_COMPILER=x86_64-apple-darwin20-g++-10 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
-else
-      echo "Unsupported OS"
-      exit 1
-fi
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
 make -j1
 
 # test
