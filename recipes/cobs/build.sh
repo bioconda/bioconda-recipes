93a322130ad30494ecee9cb176b5252548cd8ff2
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-14 20:44:26 +0100

    Trying to fix gcc/g++ issue

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -7,7 +7,14 @@ export LC_ALL=C
 # build cobs
 mkdir -p build
 cd build
-cmake -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+      cmake -DCMAKE_C_COMPILER=x86_64-apple-darwin20-gcc-11 -DCMAKE_CXX_COMPILER=x86_64-apple-darwin20-g++-11 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
+else
+      echo "Unsupported OS"
+      exit 1
+fi
 make -j1
 
 # test
