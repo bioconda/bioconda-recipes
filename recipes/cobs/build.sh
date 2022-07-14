1a08210b0875ce91324242c5dab868a6c40d232c
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-14 14:42:57 +0100

    Updating gcc/g++

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -7,7 +7,7 @@ export LC_ALL=C
 # build cobs
 mkdir -p build
 cd build
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
+cmake -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
 make -j1
 
 # test
