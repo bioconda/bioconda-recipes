229a5451c338a576e9df32188f7843d130acdc30
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-16 14:37:18 +0100

    No need for the sed, skipping linux build

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -eux -o pipefail
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      exit 0
+fi
+
 # make compilation not be dependent on locale settings
 export LC_ALL=C
 
 # build cobs
-sed -i 's/1.42.0/1.60.0/g' CMakeLists.txt
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
