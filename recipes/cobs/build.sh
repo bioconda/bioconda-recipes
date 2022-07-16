6deb697b35d323f5cf9bb7b30abbad57bd4df48b
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-16 13:37:29 +0100

    Trying to build with 1.60.0

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -5,6 +5,7 @@ set -eux -o pipefail
 export LC_ALL=C
 
 # build cobs
+sed -i 's/1.42.0/1.60.0/g' CMakeLists.txt
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
