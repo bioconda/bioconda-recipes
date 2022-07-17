dd04ae63f16b6e0be2a1484969761401e7d93d1d
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-17 21:13:06 +0100

    trying boost

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -5,6 +5,10 @@ set -eux -o pipefail
 export LC_ALL=C
 
 # build cobs
+export BOOST_INCLUDE_DIR="${PREFIX}/include"
+export BOOST_LIBRARY_DIR="${PREFIX}/lib"
+export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
+export LDFLAGS="-L${BOOST_LIBRARY_DIR} -lboost_filesystem -lboost_system"
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
