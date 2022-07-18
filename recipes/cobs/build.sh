8fe9338c1c6a7e95cf8e8826b958366ff24e1541
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-18 15:39:27 +0100

    Refactoring

--- recipes/cobs/build.sh
+++ recipes/cobs/build.sh
@@ -4,18 +4,22 @@ set -eux -o pipefail
 # make compilation not be dependent on locale settings
 export LC_ALL=C
 
-# build cobs
+# set up some variables so that CMake can find boost
 export BOOST_INCLUDE_DIR="${PREFIX}/include"
 export BOOST_LIBRARY_DIR="${PREFIX}/lib"
 export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
 export LDFLAGS="-L${BOOST_LIBRARY_DIR} -lboost_filesystem -lboost_system"
+
+# build cobs
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ..
 make -j1
 
 # test
-# make test
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then  # for some reason, tests fail on OS X
+      make test  
+fi
 
 # install
 make install
