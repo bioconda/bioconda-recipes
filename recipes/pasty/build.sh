343d830004a7a8085630b3fa344f319819f17b81
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-20 10:26:50 -0600

    typo

--- recipes/pasty/build.sh
+++ recipes/pasty/build.sh
@@ -7,5 +7,5 @@ mkdir -p ${PREFIX}/bin
 
 # Make Bioconda compatible
 mv db/OSAdb.fasta ${PASTY_SHARE}/
-sed "s=db/OSAdb.fasta=$SPATYPER_SHARE/OSAdb.fasta=" bin/pasty > ${PREFIX}/bin/pasty
+sed "s=db/OSAdb.fasta=$PASTY_SHARE/OSAdb.fasta=" bin/pasty > ${PREFIX}/bin/pasty
 chmod 755 ${PREFIX}/bin/pasty
