339915a284a3601650bb0a93169d84b86f2c4d5a
Morteza Hosseini <s_morteza_h@yahoo.com>
2022-07-20 16:01:17 +0100

    Update cryfa to 20.04

--- recipes/cryfa/build.sh
+++ recipes/cryfa/build.sh
@@ -1,14 +1,23 @@
 #! /bin/bash
+
+# Export
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 #export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
-mkdir -p $PREFIX/build
-cd $PREFIX/build
-rm CMakeCache.txt
-cmake ..
-make -j4
-mkdir -p ${PREFIX}/bin
-cp cryfa ${PREFIX}/bin
-cp keygen ${PREFIX}/bin
-cd ${PREFIX}
+# Parameters
+BUILD_TYPE=Release
+BUILD=${PREFIX}/build
+PARALLEL=4
+BIN=${PREFIX}/bin
+
+# Configure CMake
+cmake -B $BUILD -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+
+# Build
+cmake --build $BUILD --parallel $PARALLEL --config $BUILD_TYPE
+
+# Copy executables to the bin directory
+mkdir -p $BIN
+cp $BUILD/cryfa $BIN
+cp $BUILD/keygen $BIN
