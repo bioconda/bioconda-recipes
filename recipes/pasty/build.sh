a2183d419065697f418cba0a06cb2477e68802ae
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-20 10:38:05 -0600

    fix path

--- recipes/pasty/build.sh
+++ recipes/pasty/build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Set up defaults
-PASTY_SHARE="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
-mkdir -p ${PASTY_SHARE}
+PASTY_SHARE="share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p ${PREFIX}/${PASTY_SHARE}
 mkdir -p ${PREFIX}/bin
 
 # Make Bioconda compatible
-mv db/OSAdb.fasta ${PASTY_SHARE}/
+mv db/OSAdb.fasta ${PREFIX}/${PASTY_SHARE}/
 sed "s=db/OSAdb.fasta=$PASTY_SHARE/OSAdb.fasta=" bin/pasty > ${PREFIX}/bin/pasty
 chmod 755 ${PREFIX}/bin/pasty
