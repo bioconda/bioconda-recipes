a9c2ec7b4e0bd26019c90864457f9af28678f117
pg42872 <pg42872@alunos.uminho.pt>
2022-07-20 15:45:35 +0100

    Just trying to understand how this works

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-echo ${PREFIX}
+INSTALL_FOLDER=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+BIN_FOLDER=$PREFIX/bin
+mkdir -p $INSTALL_FOLDER
+mkdir -p $BIN_FOLDER
 
-mkdir -p $PREFIX/
-mkdir -p $PREFIX/share
-cp -r * $PREFIX/share/
+cp -r workflow $INSTALL_FOLDER/
+cp -r config $INSTALL_FOLDER/
+cp -r resources $INSTALL_FOLDER/
+cp -r results $INSTALL_FOLDER/
+cp plastedma.py $INSTALL_FOLDER/
+echo "Listing copied files from package to install folder"
+ls -lt $INSTALL_FOLDER
 
-SHARE_FOLDER=$PREFIX/share/
-for entry in "SHARE_FOLDER"/*
-do
-  echo "$entry"
-done
-
-ln -s $PREFIX/share/workflow $PREFIX/bin
-ln -s $PREFIX/share/plastedma.py $PREFIX/bin
-chmod +x $PREFIX/bin/plastedma.py
+ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER
+echo "Listing bin folder links from install folder"
+ls -l $BIN_FOLDER
+chmod +x $BIN_FOLDER/plastedma.py
