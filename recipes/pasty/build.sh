4f7963bf82d0daa9db85d1d4e065965236658f14
Robert A. Petit III <robbie.petit@gmail.com>
2022-07-20 10:03:34 -0600

    add recipe for pasty

---
+++ recipes/pasty/build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Set up defaults
+PASTY_SHARE="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p ${PASTY_SHARE}
+mkdir -p ${PREFIX}/bin
+
+# Make Bioconda compatible
+mv db/OSAdb.fasta ${PASTY_SHARE}/
+sed "s=db/OSAdb.fasta=$SPATYPER_SHARE/OSAdb.fasta=" bin/pasty > ${PREFIX}/bin/pasty
+chmod 755 ${PREFIX}/bin/pasty
