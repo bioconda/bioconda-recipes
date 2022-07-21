42ffe18ee739867e39cdaf82aa0f1ec437037c8a
Jose Freitas <pg42872@alunos.uminho.pt>
2022-07-21 17:08:07 +0100

    Just work allready

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTALL_FOLDER=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+INSTALL_FOLDER=$PREFIX/opt/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 BIN_FOLDER=$PREFIX/bin
 mkdir -p $INSTALL_FOLDER
 mkdir -p $BIN_FOLDER
@@ -13,8 +13,9 @@ cp plastedma.py $INSTALL_FOLDER/
 echo "Listing copied files from package to install folder"
 ls -lt $INSTALL_FOLDER/workflow
 
+cp -r workflow $BIN_FOLDER/
 ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER/
-ln -s $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
+# ln -s $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
 echo "Listing bin folder links from install folder"
 ls -l $BIN_FOLDER
-chmod +x $BIN_FOLDER/plastedma.py
+chmod u+x $BIN_FOLDER/*
