c8056907187f6122a6d83c9f69338db885e1bc02
Jose Freitas <pg42872@alunos.uminho.pt>
2022-07-21 16:16:07 +0100

    Just work allready

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -14,8 +14,7 @@ echo "Listing copied files from package to install folder"
 ls -lt $INSTALL_FOLDER/workflow
 
 ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER/
-ln -s $INSTALL_FOLDER/workflow/ $BIN_FOLDER/
-ls -lt $BIN_FOLDER/workflow
+ln -s $INSTALL_FOLDER/workflow/scripts/* $BIN_FOLDER/
 echo "Listing bin folder links from install folder"
 ls -l $BIN_FOLDER
 chmod +x $BIN_FOLDER/plastedma.py
