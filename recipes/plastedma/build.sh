9d6dadca8f5df73e139cfc0da93839bca07b9ffe
Jose Freitas <pg42872@alunos.uminho.pt>
2022-07-21 22:22:09 +0100

    ...

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -4,6 +4,7 @@ INSTALL_FOLDER=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 BIN_FOLDER=$PREFIX/bin
 mkdir -p $INSTALL_FOLDER
 mkdir -p $BIN_FOLDER
+mkdir -p $BIN_FOLDER/workflow/scripts
 
 cp -r workflow $INSTALL_FOLDER/
 cp -r config $INSTALL_FOLDER/
@@ -13,7 +14,7 @@ cp plastedma.py $INSTALL_FOLDER/
 echo "Listing copied files from package to install folder"
 ls -lt $INSTALL_FOLDER/workflow
 
-cp $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
+cp $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/workflow/scripts
 ls -l $BIN_FOLDER
 ls -l $BIN_FOLDER/workflow/
 ls -l $BIN_FOLDER/workflow/scripts
