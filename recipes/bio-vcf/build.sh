81e80912bf052cf3ac69408b4c64b394a2a736b0
Till Hartmann <till.hartmann@udo.edu>
2022-07-22 15:51:18 +0200

    fix ruby to 2.6.6 in build.sh and meta.yaml

--- recipes/bio-vcf/build.sh
+++ recipes/bio-vcf/build.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 gem source -r https://rubygems.org/
 gem build bio-vcf.gemspec
 gem env gemdir
-gem install --install-dir=$PREFIX/lib/ruby/gems/2.2.0 --bindir=$PREFIX/bin bio-vcf
+gem install --install-dir=$PREFIX/lib/ruby/gems/2.6.6 --bindir=$PREFIX/bin bio-vcf
