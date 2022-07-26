e0ac971a4521c3ab0f77a955d92f4a0ebdb1b797
Simon Bray <32272674+simonbray@users.noreply.github.com>
2022-07-26 14:05:30 +0200

    Update build.sh

--- recipes/dragmap/build.sh
+++ recipes/dragmap/build.sh
@@ -3,7 +3,7 @@ export HAS_GTEST=0
 # fix build number in config
 sed -i.bak 's/VERSION_STRING.*/VERSION_STRING="${PKG_VERSION}"/' config.mk
 
-CXXFLAGS+="-std=c++17 -Wno-unused-result"
+CXXFLAGS="$CXXFLAGS -std=c++17 -Wno-unused-result"
 
 make CXX=$CXX CC=$CC CXXFLAGS=$CXXFLAGS CFLAGS=$CFLAGS
 mkdir -p "${PREFIX}/bin"
