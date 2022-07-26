778536e726dc4a4bdde6f9a0bb0fec2850ae25a6
Simon Bray <32272674+simonbray@users.noreply.github.com>
2022-07-26 13:44:10 +0200

    Update build.sh

--- recipes/dragmap/build.sh
+++ recipes/dragmap/build.sh
@@ -5,6 +5,6 @@ sed -i.bak 's/VERSION_STRING.*/VERSION_STRING="${PKG_VERSION}"/' config.mk
 
 CXXFLAGS+="-std=c++17 -Wno-unused-result"
 
-make CXX=$CXX CC=$CC
+make CXX=$CXX CC=$CC CXXFLAGS=$CXXFLAGS CFLAGS=$CFLAGS
 mkdir -p "${PREFIX}/bin"
 mv build/release/dragen-os ${PREFIX}/bin/
