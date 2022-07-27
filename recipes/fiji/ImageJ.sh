6a63a350f7bd1c06566f424475b1e5209d0c042e
Lucille Delisle <lucille.delisle@epfl.ch>
2022-07-27 18:29:28 +0200

    only modify cfg if necessary

--- recipes/fiji/ImageJ.sh
+++ recipes/fiji/ImageJ.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 dir=$(dirname $(which ImageJ_bin))
-echo ".                                                    
+cfg=${dir}/../share/ImageJ.cfg
+if [ ! -e ${cfg} ]; then
+    echo ".                                                    
 jre/bin/java
 -Djava.util.prefs.userRoot=${dir}/../uprefs/ -cp ij.jar ij.ImageJ
-" > ${dir}/../share/ImageJ.cfg
+" > ${cfg}
+elif ! grep -q "Djava.util.prefs.userRoot" "${cfg}"; then
+    mv ${cfg} ${cfg}.bk
+    cat ${cfg}.bk | awk -v prefsuser="${dir}/../uprefs/" 'NR<3{print}NR==3{print "-Djava.util.prefs.userRoot="prefsuser" "$0}' > ${cfg}
+fi
+
 ImageJ_bin "$@"
