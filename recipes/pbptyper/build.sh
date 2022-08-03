1574bec5f40b334911e8af3ecb5b18f8117a6293
Robert A. Petit III <robbie.petit@gmail.com>
2022-08-03 11:50:32 -0600

    Update build.sh

--- recipes/pbptyper/build.sh
+++ recipes/pbptyper/build.sh
@@ -6,6 +6,6 @@ mkdir -p ${PREFIX}/${PBPTYPER_SHARE}
 mkdir -p ${PREFIX}/bin
 
 # Make Bioconda compatible
-mv db/OSAdb.fasta ${PREFIX}/${PBPTYPER_SHARE}/
-sed "s=\"bin\", \"db\"=\"bin\", \"$PBPTYPER_SHARE\"/a=" bin/pbptyper > ${PREFIX}/bin/pbptyper
+mv db/*.faa ${PREFIX}/${PBPTYPER_SHARE}/
+sed "s=\"bin\", \"db\"=\"bin\", \"$PBPTYPER_SHARE\"=" bin/pbptyper > ${PREFIX}/bin/pbptyper
 chmod 755 ${PREFIX}/bin/pbptyper
