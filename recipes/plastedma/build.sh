cd7d9050b0a07a7e903290378ff86b36c90f7437
pg42872 <pg42872@alunos.uminho.pt>
2022-08-05 14:58:40 +0100

    Maybe

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -4,7 +4,6 @@ INSTALL_FOLDER=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 BIN_FOLDER=$PREFIX/bin
 mkdir -p $INSTALL_FOLDER
 mkdir -p $BIN_FOLDER
-# mkdir -p $BIN_FOLDER/workflow/scripts
 
 cp -r workflow/scripts/* $INSTALL_FOLDER/
 cp -r config/* $INSTALL_FOLDER/
@@ -13,9 +12,9 @@ cp -r results $INSTALL_FOLDER/
 cp plastedma.py $INSTALL_FOLDER/
 #ls -lt $INSTALL_FOLDER/workflow
 
-# cp $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/workflow/scripts
+cp $INSTALL_FOLDER/hmmsearch_run.py $INSTALL_FOLDER/hmm_process.py $BIN_FOLDER
 ls -l $BIN_FOLDER
 ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER/
-#ln -s $INSTALL_FOLDER/workflow $BIN_FOLDER/
+# ln -s $INSTALL_FOLDER/workflow $BIN_FOLDER/
 # ln -s $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
 chmod u+x $BIN_FOLDER/plastedma.py
