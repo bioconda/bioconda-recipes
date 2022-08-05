baa133ced6542c95614888ffb03b2b70fc46f711
pg42872 <pg42872@alunos.uminho.pt>
2022-08-05 10:52:15 +0100

    work

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -14,12 +14,10 @@ cp plastedma.py $INSTALL_FOLDER/
 echo "Listing copied files from package to install folder"
 ls -lt $INSTALL_FOLDER/workflow
 
-# cp $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/workflow/scripts
+cp $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/workflow/scripts
 ls -l $BIN_FOLDER
 ls -l $BIN_FOLDER/workflow/
 ls -l $BIN_FOLDER/workflow/scripts
 ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER/
-ln -s $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
-ls -l $BIN_FOLDER
 # ln -s $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
 chmod u+x $BIN_FOLDER/*
