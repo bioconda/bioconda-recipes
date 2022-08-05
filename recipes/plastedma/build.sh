d3a2cf050fb8b358828dc3bac8da49ecd115f6f3
pg42872 <pg42872@alunos.uminho.pt>
2022-08-05 15:41:16 +0100

    Add plastedma

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -5,14 +5,14 @@ BIN_FOLDER=$PREFIX/bin
 mkdir -p $INSTALL_FOLDER
 mkdir -p $BIN_FOLDER
 
-cp -r workflow/scripts/* $INSTALL_FOLDER/
-cp -r config/* $INSTALL_FOLDER/
+cp -r workflow/scripts $INSTALL_FOLDER/
+cp -r config $INSTALL_FOLDER/
 cp -r resources $INSTALL_FOLDER/
 cp -r results $INSTALL_FOLDER/
 cp plastedma.py $INSTALL_FOLDER/
 #ls -lt $INSTALL_FOLDER/workflow
 
-cp $INSTALL_FOLDER/hmmsearch_run.py $INSTALL_FOLDER/hmm_process.py $BIN_FOLDER
+cp $INSTALL_FOLDER/scripts/* $INSTALL_FOLDER/hmm_process.py $BIN_FOLDER
 ls -l $BIN_FOLDER
 ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER/
 # ln -s $INSTALL_FOLDER/workflow $BIN_FOLDER/
