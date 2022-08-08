821df0c746fd5da19abe5b8c032561ff97a90149
sromain <sandra.romain@inria.fr>
2022-08-08 14:56:02 +0200

    corrected build.sh

--- recipes/svjedi-graph/build.sh
+++ recipes/svjedi-graph/build.sh
@@ -6,6 +6,6 @@ cp svjedi-graph.py  ${PREFIX}/bin/
 cp construct-graph.py ${PREFIX}/bin/
 cp filter-alignments.py ${PREFIX}/bin/
 cp predict-genotype.py ${PREFIX}/bin/
-chmod +x ${PREFIX}/bin/svjedi.py
+chmod +x ${PREFIX}/bin/svjedi-graph.py
 
 
