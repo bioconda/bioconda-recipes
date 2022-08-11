c88323b967c1b7cff3877e95592765d03eca7570
Nick Croucher <n.croucher@imperial.ac.uk>
2022-08-11 10:57:43 +0100

    Update env variables

--- recipes/3seq/build.sh
+++ recipes/3seq/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-cd 3seq-v1.8
+cd 3seq-v${PKG_VERSION}
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
 make
-if [ ! -d "$PREFIX/bin" ]; then
+if [ ! -d "$CONDA_PREFIX/bin" ]; then
         mkdir $PREFIX/bin;
 fi
-cp ./3seq $PREFIX/bin/
-if [ ! -d "$PREFIX/share" ]; then
-	mkdir $PREFIX/share;
+cp ./3seq $CONDA_PREFIX/bin/
+if [ ! -d "$CONDA_PREFIX/share" ]; then
+	mkdir $CONDA_PREFIX/share;
 fi
-mkdir $PREFIX/share/pval_table
-wget -P $PREFIX/share/pval_table/ https://www.dropbox.com/s/zac4wotgdmm3mvb/pvaluetable.2017.700.tgz
-tar xfz $PREFIX/share/pval_table/pvaluetable.2017.700.tgz  -C $PREFIX/share/pval_table/
-3seq -c $PREFIX/share/pval_table/PVT.3SEQ.2017.700
+mkdir $CONDA_PREFIX/share/pval_table
+wget -P $CONDA_PREFIX/share/pval_table/ https://www.dropbox.com/s/zac4wotgdmm3mvb/pvaluetable.2017.700.tgz
+tar xfz $CONDA_PREFIX/share/pval_table/pvaluetable.2017.700.tgz  -C $CONDA_PREFIX/share/pval_table/
+3seq -c $CONDA_PREFIX/share/pval_table/PVT.3SEQ.2017.700
