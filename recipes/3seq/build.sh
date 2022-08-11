0792e03d03d9e381821f8b64e29a6f887077c50a
Nick Croucher <n.croucher@imperial.ac.uk>
2022-08-11 10:28:07 +0100

    Modify dir structure

--- recipes/3seq/build.sh
+++ recipes/3seq/build.sh
@@ -4,8 +4,14 @@ mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
-cp ./3seq $PREFIX/bin
+if [ ! -d "$PREFIX/bin" ]; then
+        mkdir $PREFIX/bin;
+fi
+cp ./3seq $PREFIX/bin/
+if [ ! -d "$PREFIX/share" ]; then
+	mkdir $PREFIX/share;
+fi
 mkdir $PREFIX/share/pval_table
-wget -o $PREFIX/share/pval_table/pvaluetable.2017.700.tgz https://www.dropbox.com/s/zac4wotgdmm3mvb/pvaluetable.2017.700.tgz
-tar xfz $PREFIX/share/pval_table/pvaluetable.2017.700.tgz
+wget -P $PREFIX/share/pval_table/ https://www.dropbox.com/s/zac4wotgdmm3mvb/pvaluetable.2017.700.tgz
+tar xfz $PREFIX/share/pval_table/pvaluetable.2017.700.tgz  -C $PREFIX/share/pval_table/
 3seq -c $PREFIX/share/pval_table/PVT.3SEQ.2017.700
