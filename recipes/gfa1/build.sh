772531f5be717cdfdcbac036fc739fb27428ef00
Devon Ryan <dpryan79@users.noreply.github.com>
2022-08-14 09:04:12 +0200

    Update build.sh

--- recipes/gfa1/build.sh
+++ recipes/gfa1/build.sh
@@ -4,12 +4,12 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 #make -j8 INCLUDES="-I$PREFIX/include" CFLAGS="-L$PREFIX/lib"
-make CFLAGS="${CFLAGS} -fcommon"
+make CFLAGS="${CFLAGS} -fcommon" CC=$CC
 
 cd misc
 
 #make -j8 INCLUDES="-I$PREFIX/include -I../" CFLAGS="-L$PREFIX/lib"
-make CFLAGS="${CFLAGS} -fcommon"
+make CFLAGS="${CFLAGS} -fcommon" CC=$CC
 
 cd ..
 
