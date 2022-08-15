2a47d754db75ee8abb4bd605924a200414914c06
zqfang <fangzq@stanford.edu>
2022-08-15 16:57:22 -0700

    Add haplomap-0.1.0

---
+++ recipes/haplomap/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://bioconda.github.io/troubleshooting.html#zlib-errors
+export CFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include
+
+# cd to location of Makefile and source
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
+make
+make install
