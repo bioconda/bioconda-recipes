a8dd8b516d7d563210afefa68dd54edd85634564
Lucille Delisle <lucille.delisle@epfl.ch>
2022-08-16 07:27:52 +0200

    update build

--- recipes/fiji-max-inscribed_circles/build.sh
+++ recipes/fiji-max-inscribed_circles/build.sh
@@ -2,7 +2,6 @@
 
 outdir=$PREFIX/share/plugins/
 
-mkdir -p $outdir
+mkdir -p "$outdir"
 
-# to change
-cp Max_Inscribed_Circles-1.1.0.jar-20211102124353 $outdir/Max_Inscribed_Circles-1.1.0.jar
+cp MaxInscribedCircles*.jar "$outdir/"
