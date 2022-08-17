12c5db8eb729bf5a2a7ee8e5c740f9477404cb65
Zhuoqing Fang <maxzqfang@stanford.edu>
2022-08-17 13:50:35 -0700

    Update build.sh

--- recipes/haplomap/build.sh
+++ recipes/haplomap/build.sh
@@ -10,6 +10,6 @@ export C_INCLUDE_PATH=${PREFIX}/include
 # cd to location of Makefile and source
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX}/bin -DCMAKE_BUILD_TYPE=Release ..
 make
 make install
