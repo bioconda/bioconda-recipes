196ab707d0329303f65729036d42b6dfe8355695
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-08-17 19:00:39 -0500

    Add -Wno-narrowing

--- recipes/fxtract/build.sh
+++ recipes/fxtract/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-
 mkdir -p ${PREFIX}/bin
 
-make -j4 all CC=${CC} CXX=${CXX}
+# add -Wno-narrowing to avoid compilation errors on osx (non-constant-expression cannot be narrowed from type int to size_t)
+make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CXXFLAGS} -Wno-narrowing"
 
+# copy binary
 chmod +x fxtract
 cp fxtract ${PREFIX}/bin/
