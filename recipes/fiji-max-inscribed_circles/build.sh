2c26aadf7eb9a06835449064ba3da9a04d945731
Lucille Delisle <lucille.delisle@epfl.ch>
2022-08-18 14:09:06 +0200

    remove old recipe

--- recipes/fiji-max-inscribed_circles/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-outdir=$PREFIX/share/plugins/
-
-mkdir -p "$outdir"
-
-cp MaxInscribedCircles*.jar "$outdir/"
--- recipes/fiji-max-inscribed_circles/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ImageJ --ij2 --headless --console --run test.groovy
-echo PASS
