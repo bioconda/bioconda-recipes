27c7a30ca970db366f0f162d48fda24d9b9d1891
Natasha Pavlovikj <natasha.pavlovikj@yahoo.com>
2022-08-18 12:25:59 -0500

    Fix build.sh
    Fix build.sh

--- recipes/fxtract/build.sh
+++ recipes/fxtract/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 mkdir -p ${PREFIX}/bin
 
 # add -Wno-narrowing to avoid compilation errors on osx (non-constant-expression cannot be narrowed from type int to size_t) for OSX
 if [ "$(uname)" == "Darwin" ]; then
-    make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CXXFLAGS} -Wno-narrowing"
+    make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS} -Wno-narrowing"
 else
     make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CXXFLAGS}"
+fi
 
 # copy binary
 chmod +x fxtract
