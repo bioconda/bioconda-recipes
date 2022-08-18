fa6f2dc9a134df403b601e5ebf3249fa3c04907b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-08-18 00:09:59 -0500

    Add condition

--- recipes/fxtract/build.sh
+++ recipes/fxtract/build.sh
@@ -2,8 +2,11 @@
 
 mkdir -p ${PREFIX}/bin
 
-# add -Wno-narrowing to avoid compilation errors on osx (non-constant-expression cannot be narrowed from type int to size_t)
-make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CXXFLAGS} -Wno-narrowing"
+# add -Wno-narrowing to avoid compilation errors on osx (non-constant-expression cannot be narrowed from type int to size_t) for OSX
+if [ "$(uname)" == "Darwin" ]; then
+    make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CXXFLAGS} -Wno-narrowing"
+else
+    make -j4 all CC=${CC} CXX=${CXX} CFLAGS="${CXXFLAGS}"
 
 # copy binary
 chmod +x fxtract
