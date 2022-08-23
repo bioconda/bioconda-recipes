6ef0a21bdac1f38435dfea4774a6e52153c0a553
Robert A. Petit III <robbie.petit@gmail.com>
2022-08-23 13:45:03 -0600

    Update build.sh

--- recipes/nextpolish/build.sh
+++ recipes/nextpolish/build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+# Skip copying of bwa, minimap2, and samtools
+sed -i.backup \
+    -e "s| bwa samtools minimap2||g" \
+    Makefile
+
+# Use conda's gcc and includes, also skip build of bwa, minimap2, and samtools
 sed -i.backup \
     -e "s| bwa_ samtools_ minimap2_||g" \
     -e "s|gcc|$CC|g" \
     -e "s|-lz|-lz -isystem ${PREFIX}/include|g" \
     util/Makefile
 
+# Build
 make
 mkdir ${PREFIX}/bin
 cp bin/* ${PREFIX}/bin
