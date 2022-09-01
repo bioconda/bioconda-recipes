c6f1c3f5fed9d4e7b3b6ff0983be295a5ad2e3b2
czc <chongzechen@gmail.com>
2022-09-01 16:43:13 -0500

    modified:   recipes/debreak/build.sh 	modified:   recipes/debreak/meta.yaml

--- recipes/debreak/build.sh
+++ recipes/debreak/build.sh
@@ -3,6 +3,11 @@
 
 mkdir -p $PREFIX/bin
 
+wget -O -  https://github.com/ruanjue/bsalign/archive/refs/tags/v1.2.1.tar.gz | tar zxvf -
 
+cd bsalign-1.2.1 && make
+chmod +x bsalign
+cp bsalign $PREFIX/bin
+cd ..
+chmod +x debreak
 cp debreak *.py $PREFIX/bin
-chmod +x $PREFIX/bin/debreak
