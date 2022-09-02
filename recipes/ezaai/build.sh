b2f6873ea2ad005d8cd245a5e6d491e1998a1bcf
Aaron Mussig <aaronmussig@gmail.com>
2022-09-02 11:03:56 +1000

    Add EzAAI 1.2.2

--- recipes/ezaai/build.sh
+++ recipes/ezaai/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-EZ_AAI_JAR_NAME="EzAAI_v${PKG_VERSION}.jar"
+EZ_AAI_JAR_NAME="EzAAI.jar"
+EZ_AAI_LICENSE="LICENSE.md"
+
 EZ_AAI_BIN="${PREFIX}/bin/EzAAI"
 
 # Copy the JAR to the resource directory
 TARGET="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 mkdir -p "${TARGET}"
 cp "${EZ_AAI_JAR_NAME}" "${TARGET}"
+cp "${EZ_AAI_LICENSE}" "${TARGET}"
 
 # Alias the JAR in the bin directory
 mkdir -p "${PREFIX}/bin"
