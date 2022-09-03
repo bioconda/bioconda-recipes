95afaa8ebd3f9b0a8db7a82e2db7b06d6f9d194b
Nick Croucher <n.croucher@imperial.ac.uk>
2022-09-03 14:33:02 +0100

    Attempt to indirectly access /Users/ncrouche

--- recipes/3seq/activate.sh
+++ recipes/3seq/activate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PVALUE_TABLE="$PREFIX/share/3seq/PVT.3SEQ.2017.700";
+PVALUE_TABLE="${PREFIX}/share/3seq/PVT.3SEQ.2017.700";
+export HOME=`env | grep HOME | cut -d= -f2`;
 if [[ -e "${HOME}/Library/3seq/3seq.conf" ]]; then
     mv ${HOME}/Library/3seq/3seq.conf ${HOME}/Library/3seq/default.3seq.conf
 else
-    echo "$PVALUE_TABLE" > ${HOME}/Library/3seq/default.3seq.conf
+    echo "${PVALUE_TABLE}" > ${HOME}/Library/3seq/default.3seq.conf
 fi
-echo "$PVALUE_TABLE" > ${HOME}/Library/3seq/3seq.conf
+echo "${PVALUE_TABLE}" > ${HOME}/Library/3seq/3seq.conf
