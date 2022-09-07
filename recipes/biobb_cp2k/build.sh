80ba786ca7845ff385a5e1b8e947a637baa57f4d
gbayarri <genis.bayarri@irbbarcelona.org>
2022-09-07 13:24:12 +0200

    2022.3

--- recipes/biobb_cp2k/build.sh
+++ recipes/biobb_cp2k/build.sh
@@ -3,5 +3,5 @@
 python3 setup.py install --single-version-externally-managed --record=record.txt
 
 mkdir -p $PREFIX/cp2k_aux
-cp biobb_cp2k/cp2k/cp2k_in $PREFIX/cp2k_aux
-cp biobb_cp2k/cp2k/cp2k_data $PREFIX/cp2k_aux
+cp -r biobb_cp2k/cp2k/cp2k_in $PREFIX/cp2k_aux
+cp -r biobb_cp2k/cp2k/cp2k_data $PREFIX/cp2k_aux
