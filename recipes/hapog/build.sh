93559eb3a8d65cde03ee112e68a76aace0ada23e
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2022-09-14 11:35:38 +0200

    instal

--- recipes/hapog/build.sh
+++ recipes/hapog/build.sh
@@ -4,4 +4,10 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 bash build.sh -l $LIBRARY_PATH
-cp -r hapog.py lib build ${PREFIX}/bin
+cp -r hapog.py ${PREFIX}/bin
+cp -r hapog_build/hapog ${PREFIX}/bin
+cp -r hapog ${PREFIX}/lib/python${PYVER}/site-packages/
+
+ln -s ${PREFIX}/bin/hapog ${PREFIX}/bin/hapog_bin  # workaround for some hardcoded stuff
+
+chmod a+x ${PREFIX}/bin/hapog ${PREFIX}/bin/hapog_bin ${PREFIX}/bin/hapog.py
