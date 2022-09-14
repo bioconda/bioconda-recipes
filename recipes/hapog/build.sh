e9e5195be0faf203eb37f530cfd0df01f2db0378
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2022-09-14 15:18:26 +0200

    py install

--- recipes/hapog/build.sh
+++ recipes/hapog/build.sh
@@ -6,10 +6,9 @@ export LIBRARY_PATH=${PREFIX}/lib
 PYVER=`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
 
 bash build.sh -l $LIBRARY_PATH
-cp -r hapog.py ${PREFIX}/bin
-cp -r hapog_build/hapog ${PREFIX}/bin
-cp -r hapog ${PREFIX}/lib/python${PYVER}/site-packages/
 
-ln -s ${PREFIX}/bin/hapog ${PREFIX}/bin/hapog_bin  # workaround for some hardcoded stuff
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
-chmod a+x ${PREFIX}/bin/hapog ${PREFIX}/bin/hapog_bin ${PREFIX}/bin/hapog.py
+cp -r hapog_build/hapog ${PREFIX}/bin/hapog_bin
+
+chmod a+x ${PREFIX}/bin/hapog_bin
