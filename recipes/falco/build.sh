9eaa78b429be2f44386de9e460961a9d57a7e82f
guilhermesena1 <desenabr@usc.edu>
2022-09-15 23:07:14 +0100

    update falco to 1.2.1

--- recipes/falco/build.sh
+++ recipes/falco/build.sh
@@ -3,8 +3,7 @@
 # add Configuration and example files to opt
 falco=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 mkdir -p $falco
-cp -r ./Configuration $falco
-cp -r ./example.fq $falco
+cp -r ./* $falco
 
 #to fix problems with htslib
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
@@ -12,6 +11,9 @@ export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 export LD_LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
-./configure --prefix=$PREFIX --enable-hts
+cd $falco
+./configure --prefix=$falco --enable-hts
 make
 make install
+cd -
+mv $falco/bin/falco ./bin
