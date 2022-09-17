5e3ac2706730b328ddc0896657a8c09f7eafd3f6
ZhouQiangwei <1010170266@qq.com>
2022-09-17 21:52:59 +0800

    fixbug

--- recipes/bmtools/build.sh
+++ recipes/bmtools/build.sh
@@ -3,7 +3,8 @@
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-make CFLAGS="${CFLAGS} "
+make
+#CFLAGS="${CFLAGS} -g -w -O3 -Wsign-compare "
 mkdir -p $PREFIX/bin
 cp bmtools $PREFIX/bin
 cp bmDMR $PREFIX/bin
