2c146a559931eae737841c7f520f9432296018fa
zhaoxiaofei <cndfeifei@hotmail.com>
2022-09-19 09:59:19 -0400

    Skip over debug executables in the build process as there is a conflict between -fPIE and -fsanitize=address. Also re-organized comments.

--- recipes/safesim/build.sh
+++ recipes/safesim/build.sh
@@ -3,6 +3,10 @@
 # We use -with-libdeflate to ensure that htslib does not cause error in the linking stage
 bash -evx ./install-dependencies.sh skip-fastq-tools -with-libdeflate
 make clean || true
-# We added -ldeflate to prevent "undefined symbol" error in the linking stage.
-make CXX=$CXX CXXFLAGS="-static-libstdc++ ext/htslib-1.11-lowdep/libhts.a -I ext/htslib-1.11-lowdep/ -I $PREFIX/include -pthread -lm -lz -lbz2 -llzma -ldeflate -fPIE" LDFLAGS="-L $PREFIX/lib" && make deploy
+# We add -ldeflate to prevent "undefined symbol" error in the linking stage.
+# We add CXX and PREFIX to accomodate for conda build env
+# We add -fPIE to get compatibility with conda libs
+# We do not build debug executables because -fsanitize seems to be in conflict with -fPIE
+make CXX=$CXX CXXFLAGS="-static-libstdc++ ext/htslib-1.11-lowdep/libhts.a -I ext/htslib-1.11-lowdep/ -I $PREFIX/include -pthread -lm -lz -lbz2 -llzma -ldeflate -fPIE" LDFLAGS="-L $PREFIX/lib" safemut safemix 
+make deploy
 
