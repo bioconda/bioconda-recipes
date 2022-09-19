e26c651079d4a03cb53c6b3cbcbeb9e1f91718ef
zhaoxiaofei <cndfeifei@hotmail.com>
2022-09-19 05:09:30 -0400

    Use cross-platform c++ compiler with the newer release of safesim.

--- recipes/safesim/build.sh
+++ recipes/safesim/build.sh
@@ -4,5 +4,5 @@
 bash -evx ./install-dependencies.sh skip-fastq-tools -with-libdeflate
 make clean || true
 # We added -ldeflate to prevent "undefined symbol" error in the linking stage.
-make CXXFLAGS="-static-libstdc++ ext/htslib-1.11-lowdep/libhts.a -I ext/htslib-1.11-lowdep/ -pthread -lm -lz -lbz2 -llzma -ldeflate" && make deploy
+make CXX=$CXX CXXFLAGS="-static-libstdc++ ext/htslib-1.11-lowdep/libhts.a -I ext/htslib-1.11-lowdep/ -pthread -lm -lz -lbz2 -llzma -ldeflate" && make deploy
 
