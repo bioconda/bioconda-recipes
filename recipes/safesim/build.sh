5f502ef10b153d93ff3eb4df95cefd2dae9f2440
zhaoxiaofei <cndfeifei@hotmail.com>
2022-09-18 22:56:26 -0400

    Added fastq-tools in dependencies in the meta.yaml file and added comments into the build.sh file.

--- recipes/safesim/build.sh
+++ recipes/safesim/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# We skip fastq-tools because it is defined in the meta.yaml
+# We use -with-libdeflate to ensure that htslib does not cause error in the linking stage
 bash -evx ./install-dependencies.sh skip-fastq-tools -with-libdeflate
 make clean || true
+# We added -ldeflate to prevent "undefined symbol" error in the linking stage.
 make CXXFLAGS="-static-libstdc++ ext/htslib-1.11-lowdep/libhts.a -I ext/htslib-1.11-lowdep/ -pthread -lm -lz -lbz2 -llzma -ldeflate" && make deploy
 
