5294be9ef932376e0cce0fcfbf9cb699d0975039
Nick Croucher <n.croucher@imperial.ac.uk>
2022-09-19 20:32:33 +0100

    Change activation condition

--- recipes/3seq/activate.sh
+++ recipes/3seq/activate.sh
@@ -5,6 +5,6 @@ declare -i PTAB_COUNT
 PTAB_COUNT=`3seq | grep "no p-value table" | wc -l`
 
 # If no existing p value table exists then link to downloaded table
-if [[ $PTAB_COUNT == 0 ]]; then
+if [[ $PTAB_COUNT > 0 ]]; then
     yes | timeout 1 3seq -c $PREFIX/share/3seq/PVT.3SEQ.2017.700;
 fi
