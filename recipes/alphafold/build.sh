6df53c3aedf1a2a0102e142dd79cc2bf427bb57a
Jillian Rowe <jillian.e.rowe@gmail.com>
2022-09-20 11:19:17 +0300

    removing wget

--- recipes/alphafold/build.sh
+++ recipes/alphafold/build.sh
@@ -2,8 +2,7 @@
 
 pip install . --ignore-installed --no-cache-dir -vvv
 
-wget -q -P ${PREFIX}/lib/python3.7/site-packages/lib/python3.7/site-packages/alphafold/common/ \
-  https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
+cp stereo_chemical_props.txt ${PREFIX}/lib/python3.7/site-packages/lib/python3.7/site-packages/alphafold/common/  
 
 pushd ${PREFIX}/lib/python3.7/site-packages/ && \
     patch -p0 < ./docker/openmm.patch && \
