69b8141ab2ac230f45f44b334d4f075dc716a6b2
Robert A. Petit III <robbie.petit@gmail.com>
2022-09-20 11:26:47 -0600

    add patch files

--- recipes/nextpolish/build.sh
+++ recipes/nextpolish/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Create share directory
+SHARE_DIR="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p ${SHARE_DIR}/
+cp -r ./lib ${SHARE_DIR}/
+
 # Skip copying of bwa, minimap2, and samtools
 sed -i.backup \
     -e "s| bwa samtools minimap2||g" \
@@ -12,10 +17,15 @@ sed -i.backup \
     -e "s|-lz|-lz -isystem ${PREFIX}/include|g" \
     util/Makefile
 
+# Add version info (original required internet connection)
+sed -i.backup "s=BIOCONDA_SED_REPLACE=$PKG_VERSION=" lib/kit.py
+
 # Build
 make
 mkdir -p ${PREFIX}/bin
 cp bin/* ${PREFIX}/bin
 
+# fix hardcoded path
+sed -i "s=BIOCONDA_SED_REPLACE=$SHARE_DIR=" nextPolish
 chmod 755 nextPolish
 cp nextPolish ${PREFIX}/bin
