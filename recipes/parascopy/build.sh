b5373c5afd2713fb0576372fd9be452b80a9b831
Timofey Prodanov <timofey.prodanov@gmail.com>
2022-09-21 18:31:05 +0200

    Copy to prefix/bin

--- recipes/parascopy/build.sh
+++ recipes/parascopy/build.sh
@@ -7,7 +7,7 @@ mkdir build
 meson build/ --buildtype release
 cd build
 ninja -v
-cp freebayes "${PREFIX}/_parascopy_freebayes"
+cp freebayes "${PREFIX}/bin/_parascopy_freebayes"
 cd ../../
 
 $PYTHON -m pip install . --ignore-install --no-deps -vv
