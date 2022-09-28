5df0ed94f5c189d54c7c87a8977baef7fb7e9b4b
Workum, Dirk-Jan van <dirk-jan.vanworkum@wur.nl>
2022-09-28 16:17:20 +0200

    update location binaries

--- recipes/aster/build.sh
+++ recipes/aster/build.sh
@@ -6,9 +6,9 @@ make CC=$GXX
 
 [ ! -d $PREFIX/bin ] && mkdir -p $PREFIX/bin
 
-cp bin/astral $PREFIX/bin
-cp bin/astral-pro $PREFIX/bin
-cp bin/asterisk $PREFIX/bin
+cp bin/astral $PREFIX/bin/
+cp bin/astral-pro $PREFIX/bin/
+cp bin/asterisk $PREFIX/bin/
 
 chmod a+x $PREFIX/bin/astral
 chmod a+x $PREFIX/bin/astral-pro
