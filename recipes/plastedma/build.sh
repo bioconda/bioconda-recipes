4975a6568393ae61c4c9d42ca9dc1264b7183175
José Freitas <73902769+ozefreitas@users.noreply.github.com>
2022-09-28 22:04:22 +0100

    Debug build.sh

--- recipes/plastedma/build.sh
+++ recipes/plastedma/build.sh
@@ -33,6 +33,6 @@ ls -l $BIN_FOLDER
 # ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER/
 # ln -s $INSTALL_FOLDER/workflow $BIN_FOLDER/
 # ln -s $INSTALL_FOLDER/workflow/scripts/hmmsearch_run.py $INSTALL_FOLDER/workflow/scripts/hmm_process.py $BIN_FOLDER/
-chmod u+x $BIN_FOLDER/plastedma.py
+# chmod u+x $BIN_FOLDER/plastedma.py
 
-alias plastedma="plastedma.py"
+# alias plastedma="plastedma.py"
