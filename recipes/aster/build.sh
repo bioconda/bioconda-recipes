9b6bc9ff6ff7d631c2f5bfea981050a80b9927f3
Workum, Dirk-Jan van <dirk-jan.vanworkum@wur.nl>
2022-09-28 12:45:19 +0200

    correct handling binaries for aster

--- recipes/aster/build.sh
+++ recipes/aster/build.sh
@@ -2,13 +2,12 @@
 
 make
 
-if [ ! -d $PREFIX/bin ] ; then
-	mkdir -p $PREFIX/bin
-fi
+[ ! -d $PREFIX/bin ] && mkdir -p $PREFIX/bin
 
-cp astral $PREFIX/bin
-cp astral-pro $PREFIX/bin
-cp asterisk $PREFIX/bin
+cp bin/astral $PREFIX/bin
+cp bin/astral-pro $PREFIX/bin
+cp bin/asterisk $PREFIX/bin
+
 chmod a+x $PREFIX/bin/astral
 chmod a+x $PREFIX/bin/astral-pro
 chmod a+x $PREFIX/bin/asterisk
