d608345420a466bde16f4cd04af68df89a1c74a7
Devon Ryan <devon.ryan@genedata.com>
2022-09-28 13:42:57 +0200

    patch around safestring

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://github.com/intel/safestringlib/issues/14
+if [[ $OSTYPE == "darwin"* ]]; then
+    sed -i.bak -e "s/extern errno_t//g" ext/safestringlib/include/safe_mem_lib.h
+fi
 LIBS="${LDFLAGS}" make CC="${CC}" CXX="${CXX}" multi
 
 mkdir -p $PREFIX/bin
