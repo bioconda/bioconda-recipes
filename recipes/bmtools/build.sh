bd37cc127a4b012001fdc9f916f03c37e89863b9
ZhouQiangwei <1010170266@qq.com>
2022-09-28 19:33:45 +0800

    up2

--- recipes/bmtools/build.sh
+++ recipes/bmtools/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
 cd htslib*
-make
+make CC=$CC CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
 cd ..
 
-make pycd
+make pycd CC=$CC CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
 #CFLAGS="${CFLAGS} -g -w -O3 -Wsign-compare "
 mkdir -p $PREFIX/bin
 cp bmtools $PREFIX/bin
