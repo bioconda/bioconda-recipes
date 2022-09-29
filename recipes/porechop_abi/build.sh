fc680822e2d15a838d73df8be3ab7bd002c482b1
Quentin Bonenfant <quentin.bonenfant@gmail.com>
2022-09-29 18:26:00 +0200

    trying with porehopèrunner as entrypoint

--- recipes/porechop_abi/build.sh
+++ recipes/porechop_abi/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export CXXFLAGS="${CXXFLAGS} -std=c++14"
-python3 -m pip install . --ignore-installed --no-deps -vv
+make release
+# python3 -m pip install . --ignore-installed --no-deps -vv
