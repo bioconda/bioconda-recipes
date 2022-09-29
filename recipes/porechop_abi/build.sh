dc9e23775b1aff130b0a8fb472f11f881dab15fe
Quentin Bonenfant <quentin.bonenfant@gmail.com>
2022-09-29 18:35:03 +0200

    reverting to porechop installer with pip.

--- recipes/porechop_abi/build.sh
+++ recipes/porechop_abi/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "[P_ABI build.sh]: Using c++14."
 export CXXFLAGS="${CXXFLAGS} -std=c++14"
-make release
-# python3 -m pip install . --ignore-installed --no-deps -vv
+# make release
+echo "[P_ABI build.sh]: Using pip to build porechop_abi."
+python3 -m pip install . --ignore-installed --no-deps -vv
