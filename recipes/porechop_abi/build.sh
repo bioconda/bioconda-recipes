b4185d3053849db1162da1115c004c8eeaef00a8
Quentin Bonenfant <quentin.bonenfant@gmail.com>
2022-09-29 17:59:26 +0200

    updated metayaml/ build

--- recipes/porechop_abi/build.sh
+++ recipes/porechop_abi/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 export CXXFLAGS="${CXXFLAGS} -std=c++14"
-export CPATH=${PREFIX}/include
-
-python -m pip install . --ignore-installed --no-deps -vv
+python3 -m pip install . --ignore-installed --no-deps -vv
