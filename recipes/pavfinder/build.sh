3eee3c8fd442384f7c0ec2daf31fb46071fc1fb5
kmnip <kamingnip@gmail.com>
2022-09-30 23:17:26 -0700

    fix bug

--- recipes/pavfinder/build.sh
+++ recipes/pavfinder/build.sh
@@ -2,12 +2,13 @@
 
 set -euxo pipefail
 
-${PYTHON} -m pip install -vv --no-deps --ignore-installed --no-cache-dir .
+${PYTHON} -m pip install -vv --no-deps --ignore-installed .
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin
 
-cp "pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
+ls "${SRC_DIR}/pavfinder/scripts/fusion-bloom"
+cp "${SRC_DIR}/pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
 
 echo "#!/bin/bash" > ${PREFIX}/bin/fusion-bloom
 echo "make -f $(command -v ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/fusion-bloom) \$@" >> ${PREFIX}/bin/fusion-bloom
