fca12c13fd1130539ea7381add16a39f2d58c95b
kmnip <kamingnip@gmail.com>
2022-10-01 16:45:27 -0700

    make script executable

--- recipes/pavfinder/build.sh
+++ recipes/pavfinder/build.sh
@@ -7,7 +7,9 @@ ${PYTHON} -m pip install -vv --no-deps --ignore-installed .
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin
 
-cp "${SRC_DIR}/pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
+cp -f "${SRC_DIR}/pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
 
 echo "#!/bin/bash" > ${PREFIX}/bin/fusion-bloom
 echo "make -f $(command -v ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom) \$@" >> ${PREFIX}/bin/fusion-bloom
+chmod +x ${PREFIX}/bin/fusion-bloom
+
