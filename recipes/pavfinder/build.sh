e7dd5a474a8f827718ac20ab028960dde5fa8b36
kmnip <kamingnip@gmail.com>
2022-10-01 00:56:23 -0700

    fix path

--- recipes/pavfinder/build.sh
+++ recipes/pavfinder/build.sh
@@ -8,6 +8,8 @@ mkdir -p ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin
 cp "${SRC_DIR}/pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
 
 echo "#!/bin/bash" > ${PREFIX}/bin/fusion-bloom
-echo "make -f $(command -v ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/fusion-bloom) \$@" >> ${PREFIX}/bin/fusion-bloom
+echo "make -f $(command -v ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom) \$@" >> ${PREFIX}/bin/fusion-bloom
+
+cat ${PREFIX}/bin/fusion-bloom
 
 ${PYTHON} -m pip install -vv --no-deps --ignore-installed .
