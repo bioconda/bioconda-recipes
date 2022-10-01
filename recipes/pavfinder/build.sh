040895e01029623540ab26050068e0a8916c1d8b
kmnip <kamingnip@gmail.com>
2022-10-01 16:26:40 -0700

    minor edit

--- recipes/pavfinder/build.sh
+++ recipes/pavfinder/build.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+${PYTHON} -m pip install -vv --no-deps --ignore-installed .
+
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin
 
@@ -9,7 +11,3 @@ cp "${SRC_DIR}/pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-
 
 echo "#!/bin/bash" > ${PREFIX}/bin/fusion-bloom
 echo "make -f $(command -v ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom) \$@" >> ${PREFIX}/bin/fusion-bloom
-
-cat ${PREFIX}/bin/fusion-bloom
-
-${PYTHON} -m pip install -vv --no-deps --ignore-installed .
