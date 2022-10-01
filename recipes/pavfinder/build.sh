5a69ca7ef5142c65df03e045e7fe3a543caecee9
kmnip <kamingnip@gmail.com>
2022-09-30 23:43:10 -0700

    fix bug

--- recipes/pavfinder/build.sh
+++ recipes/pavfinder/build.sh
@@ -7,7 +7,6 @@ ${PYTHON} -m pip install -vv --no-deps --ignore-installed .
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin
 
-ls "${SRC_DIR}/pavfinder/scripts/fusion-bloom"
 cp "${SRC_DIR}/pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
 
 echo "#!/bin/bash" > ${PREFIX}/bin/fusion-bloom
