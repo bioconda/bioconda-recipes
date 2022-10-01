9109c1f2d25044940a1fedf6fb76ab9f32ad94ee
kmnip <kamingnip@gmail.com>
2022-09-30 22:56:50 -0700

    add build script

---
+++ recipes/pavfinder/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+${PYTHON} -m pip install -vv --no-deps --ignore-installed --no-cache-dir .
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin
+
+cp "pavfinder/scripts/fusion-bloom" "${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/fusion-bloom"
+
+echo "#!/bin/bash" > ${PREFIX}/bin/fusion-bloom
+echo "make -f $(command -v ${PREFIX}/bin/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/fusion-bloom) \$@" >> ${PREFIX}/bin/fusion-bloom
