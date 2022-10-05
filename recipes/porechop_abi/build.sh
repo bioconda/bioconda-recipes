e607355ed6a1f000e8e2514961284cb1a8c3ba4e
Quentin Bonenfant <quentin.bonenfant@gmail.com>
2022-10-05 14:33:58 +0200

    Changed recipe for mac OS

--- recipes/porechop_abi/build.sh
+++ recipes/porechop_abi/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "[P_ABI build.sh]: Using c++14."
-export CXXFLAGS="${CXXFLAGS} -std=c++14"
+echo "[P_ABI build.sh]: Using c++17."
+export CXXFLAGS="${CXXFLAGS} -std=c++17 -D_LIBCPP_DISABLE_AVAILABILITY "
+echo ${CXX}
+echo ${PREFIX}
+echo ${CXXFLAGS}
 # make release
 echo "[P_ABI build.sh]: Using pip to build porechop_abi."
 python3 -m pip install . --ignore-installed --no-deps -vv
