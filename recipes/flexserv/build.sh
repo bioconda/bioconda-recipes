d95022e2bbfe073234549c59af2ce2465d969d9d
Adam Hospital Gasch <hospital@OROZCO67.local>
2022-10-06 08:39:27 +0200

    FlexServ Tools Conda Package version 1.0

---
+++ recipes/flexserv/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Change folder, compiling BD
+cd bd
+sh compile.sh
+cd ..
+
+# Change folder, compiling DMD
+cd dmd
+sh compile.sh
+cd ..
+
+# Change folder, compiling NMA
+cd nma
+sh compile.sh
+cd ..
+
+# Copy executables to environment bin dir included in the path
+mkdir -p $PREFIX/bin
+
+# BD
+chmod u+x bd/bd
+cp bd/bd $PREFIX/bin/
+
+# DMD
+chmod u+x dmd/dmdgoopt
+cp dmd/dmdgoopt $PREFIX/bin/
+
+# NMA
+chmod u+x nma/diaghess/diaghess
+cp nma/diaghess/diaghess $PREFIX/bin/
+chmod u+x nma/nmanu.pl
+cp nma/nmanu.pl $PREFIX/bin/
+chmod u+x nma/mc-eigen.pl
+cp nma/mc-eigen.pl $PREFIX/bin/
+chmod u+x nma/pca_anim_mc.pl
+cp nma/pca_anim_mc.pl $PREFIX/bin/
