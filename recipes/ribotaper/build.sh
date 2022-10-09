d4a28f1d8db78dbe5142bb0fcfc0bf666a018139
Devon Ryan <dpryan79@gmail.com>
2022-10-09 22:04:19 +0200

    bl update

--- recipes/ribotaper/build.sh
+++ recipes/ribotaper/build.sh
@@ -2,4 +2,10 @@
 
 ./configure --prefix=$PREFIX
 make
+for f in scripts/*.sh; do
+    sed -i.bak "s#/usr/bin/bash#/bin/bash#g" $f
+done
+for f in scripts/*.bash; do
+    sed -i.bak "s#/usr/bin/bash#/bin/bash#g" $f
+done
 make install
