8a67cca85eb3f2e31e2a87759f76da7587e25344
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-10-12 16:53:33 -0500

    Clean recipe

--- recipes/rnahybrid/build.sh
+++ recipes/rnahybrid/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Darwin" ]; then
+    export LDFLAGS="-Wl,--allow-multiple-definition $LDFLAGS"
+else
+    export CFLAGS="$CFLAGS -fcommon"
+fi
+
 ./configure --prefix=${PREFIX}
 make
 make install
