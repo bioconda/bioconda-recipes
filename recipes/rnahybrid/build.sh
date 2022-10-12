27b56e20cb3f71f384613feb81cfe74c40ff8b00
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-10-12 17:35:16 -0500

    Flip flags

--- recipes/rnahybrid/build.sh
+++ recipes/rnahybrid/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    export LDFLAGS="-Wl,--allow-multiple-definition $LDFLAGS"
-else
     export CFLAGS="$CFLAGS -fcommon"
+else
+    export LDFLAGS="-Wl,--allow-multiple-definition $LDFLAGS"
 fi
 
 ./configure --prefix=${PREFIX}
