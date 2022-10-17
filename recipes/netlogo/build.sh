d4c131bb7a09a919edc8a3a3d311719b1ac16628
Luca Cozzuto <lucacozzuto@gmail.com>
2022-10-17 16:51:18 +0200

    fixing paths

--- recipes/netlogo/build.sh
+++ recipes/netlogo/build.sh
@@ -1,1 +1,16 @@
-mv NetLogo* $PREFIX/bin
+#!/bin/bash
+
+BINARY_HOME=$PREFIX/bin
+NETLOGO_HOME=$PREFIX/share/netlogo
+
+mkdir -p $PREFIX/bin
+mkdir -p $NETLOGO_HOME
+
+#sed -i "s|APP_HOME=.*|APP_HOME=\"${PACKAGE_HOME}\"|g" nf-test
+
+cp -r * $NETLOGO_HOME/
+
+chmod +x $NETLOGO_HOME/netlogo-headless.sh 
+
+ln -s $NETLOGO_HOME/netlogo-headless.sh  $PREFIX/bin/netlogo-headless.sh 
+
