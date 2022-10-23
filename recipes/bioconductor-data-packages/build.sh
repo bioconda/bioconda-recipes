ed2fa746abf63dc7d03db7db8cc8e09f7c1d9f20
Devon Ryan <dpryan79@gmail.com>
2022-10-23 20:38:21 +0200

    recipe_dir of course

--- recipes/bioconductor-data-packages/build.sh
+++ recipes/bioconductor-data-packages/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/bioconductor-data-packages
-cp ${SRC_DIR}/installBiocDataPackage.sh  ${PREFIX}/bin
-cp ${SRC_DIR}/dataURLs.json ${PREFIX}/share/bioconductor-data-packages/dataURLs.json
+cp ${RECIPE_DIR}/installBiocDataPackage.sh  ${PREFIX}/bin
+cp ${RECIPE_DIR}/dataURLs.json ${PREFIX}/share/bioconductor-data-packages/dataURLs.json
