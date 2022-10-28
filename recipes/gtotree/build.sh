7fa8da6cd2fd97fa5598c878269b7121eaf17c1a
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-10-28 17:06:00 +0200

    Attempting to debug CI

--- recipes/gtotree/build.sh
+++ recipes/gtotree/build.sh
@@ -17,16 +17,16 @@ touch ${PREFIX}/share/gtotree/gtdb_tax_info/GTDB-arc-and-bac-metadata.tsv
 # kofamscan data is added when used, placeholder so the dir is not deleted on creation
 touch ${PREFIX}/share/gtotree/kofamscan_data/README
 
-## add database paths in variables to activation script
-mkdir -p ${PREFIX}/etc/conda/activate.d/
-echo 'export GToTree_HMM_dir=${CONDA_PREFIX}/share/gtotree/hmm_sets/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
-echo 'export TAXONKIT_DB=${CONDA_PREFIX}/share/gtotree/ncbi_tax_info/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
-echo 'export GTDB_dir=${CONDA_PREFIX}/share/gtotree/gtdb_tax_info/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
-echo 'export KO_data_dir=${CONDA_PREFIX}/share/gtotree/kofamscan_data/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# ## add database paths in variables to activation script
+# mkdir -p ${PREFIX}/etc/conda/activate.d/
+# echo 'export GToTree_HMM_dir=${CONDA_PREFIX}/share/gtotree/hmm_sets/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# echo 'export TAXONKIT_DB=${CONDA_PREFIX}/share/gtotree/ncbi_tax_info/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# echo 'export GTDB_dir=${CONDA_PREFIX}/share/gtotree/gtdb_tax_info/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# echo 'export KO_data_dir=${CONDA_PREFIX}/share/gtotree/kofamscan_data/' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
 
-## adding variables to the conda environment so localization matches what the program expects
-echo 'export LC_ALL="en_US.UTF-8"' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
-echo 'export LANG="en_US.UTF-8"' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# ## adding variables to the conda environment so localization matches what the program expects
+# echo 'export LC_ALL="en_US.UTF-8"' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# echo 'export LANG="en_US.UTF-8"' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
 
 cp bin/* ${PREFIX}/bin/
 cp hmm_sets/* ${PREFIX}/share/gtotree/hmm_sets/
@@ -34,8 +34,8 @@ cp LICENSE ${PREFIX}
 
 ## this is intended to help with users who don't have write access in the conda environment location
 ## it's managed by the gtt-data-locations program
-echo '
-if [ -f ~/.config/gtotree/gtotree.sh ] && [ ! -w ${CONDA_PREFIX}/etc/conda/activate.d/gtotree.sh ]; then
-    . ~/.config/gtotree/gtotree.sh
-fi
-' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
+# echo '
+# if [ -f ~/.config/gtotree/gtotree.sh ] && [ ! -w ${CONDA_PREFIX}/etc/conda/activate.d/gtotree.sh ]; then
+#     . ~/.config/gtotree/gtotree.sh
+# fi
+# ' >> ${PREFIX}/etc/conda/activate.d/gtotree.sh
