520bcc30682f49ea834209781e1cc469f32bcb08
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2022-10-28 15:18:38 -0400

    configure TreeShrink recipe for bioconda

--- recipes/treeshrink/build.sh
+++ recipes/treeshrink/build.sh
@@ -1,3 +1,6 @@
-#$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+#!/bin/bash
+
 R CMD INSTALL -l Rlib dependencies/BMS_0.3.3.tar.gz
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+
+ln -s $PREFIX/bin/run_treeshrink.py $PREFIX/bin/run_treeshrink
