bfe0b790bc38b81e7e7b54eca69543169e5c71b4
Devon Ryan <dpryan79@users.noreply.github.com>
2022-10-31 10:08:52 +0100

    Update build.sh

--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -3,4 +3,4 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build . --config Release
 ctest -C Release
-cmake --install . --prefix $PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake --install . --prefix $PREFIX
