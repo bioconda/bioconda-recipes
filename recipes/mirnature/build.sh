34faac3f89362e3c0aaf91a8534716444dfbd7b4
jfallmann <fallmann.joerg@gmail.com>
2022-11-07 16:16:15 +0100

    added Build.PL, updates meta.yaml and build.sh

--- recipes/mirnature/build.sh
+++ recipes/mirnature/build.sh
@@ -2,7 +2,27 @@
 
 set -xe
 
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/scripts
+mkdir -p ${PREFIX}/config
+
+## Build Perl
+
+mkdir perl-build
+find scripts -name "*.pl" | xargs -I {} mv {} perl-build
+cd perl-build
+
+cp ${RECIPE_DIR}/Build.PL ./
+
 perl Build.PL
 perl ./Build
 perl ./Build test
-perl ./Build install
+perl ./Build install --installdirs site
+
+cd ..
+
+## End build perl
+
+mv bin/* $PREFIX/bin/
+mv scripts/* $PREFIX/bin/
+mv config/* $PREFIX/config/
