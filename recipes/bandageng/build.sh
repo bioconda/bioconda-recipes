cc2faef260078e8af35413d8a2254f59b218730a
Cameron Hyde <chyde@neoformit.com>
2022-11-08 22:11:48 +1100

    Init bandage-ng (build broken)

--- recipes/bandageng/build.sh
+++ recipes/bandageng/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -ex
 
-#qmake bugfix: qmake fails if there is no g++ executable available, even if QMAKE_CXX is explicitly set
-ln -s $CXX $BUILD_PREFIX/bin/g++
-export PATH=$BUILD_PREFIX/bin/:$PATH
-
-# Run qmake to generate a Makefile
-qmake Bandage.pro QMAKE_CXX="$CXX" QMAKE_CC="$CC" QMAKE_CFLAGS="$CFLAGS" QMAKE_CXXFLAGS="$CXXFLAGS"
-
-# fix the makefile
-sed -ie "s/isystem/I/" Makefile
-# Build the program
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib .
 make
 
 # Install
