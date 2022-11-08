3da0924403bb68548baefd7f03f140a2d4c67696
Xiao <vincent@cwi.nl>
2022-11-08 19:55:49 +0800

    update

--- recipes/vechat/build.sh
+++ recipes/vechat/build.sh
@@ -6,5 +6,6 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX}  -Dspoa_optimize_for_portability=ON ..
 make
 make install
+cd ..
 cp scripts/vechat ${PREFIX}/bin
 chmod +x ${PREFIX}/bin/vechat
