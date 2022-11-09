dc6da49bd8d924a5d42a4350176898ca25233b0e
Loraine Gueguen <loraine.gueguen@sb-roscoff.fr>
2022-11-09 20:54:56 +0100

    fix mv

--- recipes/ratatosk/build.sh
+++ recipes/ratatosk/build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/pmelsted/bifrost.git
 cd bifrost
 git checkout 9e6f948ce2c27de32bc687502e777aebc9eab53d
 cd ..
-mv bifrost Bifrost
+mv bifrost/* Bifrost/.
 
 mkdir -p $PREFIX/bin
 mkdir build && cd build
