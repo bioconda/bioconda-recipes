c712331c09a6c6fdb010eca3373bb26711458a88
abueg <linelle.abueg@gmail.com>
2022-11-09 04:17:27 -0500

    fastk

---
+++ recipes/fastk/build.sh
@@ -0,0 +1,7 @@
+export LDFLAGS="-L$SRC_DIR/HTSLIB -L$PREFIX/lib"
+export CFLAGS="-I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$SRC_DIR/HTSLIB -L$PREFIX/lib"
+export CPPFLAGS="-I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$SRC_DIR/HTSLIB -L$PREFIX/lib"
+
+cd HTSLIB ; make lib-static ; cd -
+cd LIBDEFLATE ; make ; cd -
+make ; make install
