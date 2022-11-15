7be7095c4b4fcbf8577968de7a5e901c33974f3c
Devon Ryan <dpryan79@users.noreply.github.com>
2022-11-15 22:52:14 +0100

    Update build.sh

--- recipes/pggb/build.sh
+++ recipes/pggb/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Write version in the script
-GIT_VERSION=$(git describe --always --tags --long)
 n=$(grep '$show_version == true' ./pggb -n | cut -f 1 -d :)
 n=$((n-1))
-sed -i ${n}'a\if [ $show_version == true ]; then echo "pggb '$GIT_VERSION'"; exit; fi' pggb
+sed -i ${n}'a\if [ $show_version == true ]; then echo "pggb '$PKG_VERSION'"; exit; fi' pggb
 
 mkdir -p $PREFIX/bin
 mv pggb $PREFIX/bin
