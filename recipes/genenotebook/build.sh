0e1cfd20700209e91b4035ed32d845fa76c62181
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2022-11-17 16:47:29 +0100

    install meteor earlier

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -5,11 +5,6 @@ set -exo pipefail
 outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
 mkdir -p $outdir $PREFIX/bin
 
-
-npm install --unsafe-perm
-
-export PATH=$PATH:"$HOME/.meteor"
-
 # Fix architecture value
 export ARCH=$(uname -m)
 
@@ -18,6 +13,10 @@ curl "https://install.meteor.com/?release=2.8.0" > meteor.sh
 chmod a+x meteor.sh
 sed -i.bak 's|PREFIX=|#PREFIX=|' meteor.sh
 ./meteor.sh
+
+npm install --unsafe-perm
+
+export PATH=$PATH:"$HOME/.meteor"
 
 # Now run the normal gnb install
 METEOR_ALLOW_SUPERUSER=1 METEOR_DISABLE_OPTIMISTIC_CACHING=1 npm run bundle
