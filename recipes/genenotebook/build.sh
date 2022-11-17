8f6eb45388ad8b54bad98a91fc09917df011c9d2
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2022-11-17 16:34:10 +0100

    make sure meteor is installed in PREFIX

--- recipes/genenotebook/build.sh
+++ recipes/genenotebook/build.sh
@@ -13,6 +13,13 @@ export PATH=$PATH:"$HOME/.meteor"
 # Fix architecture value
 export ARCH=$(uname -m)
 
+# Install meteor here (instead of in npm run bundle) because we need to patch the install script
+curl "https://install.meteor.com/?release=2.8.0" > meteor.sh
+chmod a+x meteor.sh
+sed -i.bak 's|PREFIX=|#PREFIX=|' meteor.sh
+./meteor.sh
+
+# Now run the normal gnb install
 METEOR_ALLOW_SUPERUSER=1 METEOR_DISABLE_OPTIMISTIC_CACHING=1 npm run bundle
 
 cp -R genenotebook_v${PKG_VERSION}/* $outdir
