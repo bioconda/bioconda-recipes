365bc461026d499ad95835b0b2387a695892321c
marrip <marrip@ihelse.net>
2022-11-19 19:03:30 +0100

    test: run ONCOCNV test data from repo

--- recipes/oncocnv/run_test.sh
+++ recipes/oncocnv/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sed -i "s/^TOOLDIR=.*/TOOLDIR=$PREFIX/g" RUNME.sh
+cat testdata/Control.stats.txt | grep -v start | awk '{print $1,$2,$3}' | sed "s/ /\t/g" > target.bed
 
-bash RUNME.sh
+cat $PREFIX/bin/processControl.R | R --slave --args testdata/Control.stats.txt Control.stats.Processed.txt testdata/target.GC.txt
+
+cat $PREFIX/bin/processSamples.R | R --slave --args testdata/Test.stats.txt Control.stats.Processed.txt Test.output.txt cghseg
