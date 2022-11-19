d25c473e4548e1421c55dd33a110e6e1225410b1
marrip <marrip@ihelse.net>
2022-11-19 19:30:53 +0100

    test: fix sample file name :grimacing:

--- recipes/oncocnv/run_test.sh
+++ recipes/oncocnv/run_test.sh
@@ -4,4 +4,4 @@ cat testdata/Control.stats.txt | grep -v start | awk '{print $1,$2,$3}' | sed "s
 
 cat $PREFIX/bin/processControl.R | R --slave --args testdata/Control.stats.txt Control.stats.Processed.txt testdata/target.GC.txt
 
-cat $PREFIX/bin/processSamples.R | R --slave --args testdata/Test.stats.txt Control.stats.Processed.txt Test.output.txt cghseg
+cat $PREFIX/bin/processSamples.R | R --slave --args testdata/Test.stats.samplesA1_A3.txt Control.stats.Processed.txt Test.output.txt cghseg
