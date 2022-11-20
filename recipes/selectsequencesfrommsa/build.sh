f1306213f7ef4f322d87c4f850b73df0426010fc
Devon Ryan <dpryan79@gmail.com>
2022-11-20 14:44:16 +0100

    haskell updates

--- recipes/selectsequencesfrommsa/build.sh
+++ recipes/selectsequencesfrommsa/build.sh
@@ -4,6 +4,12 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
 
+# use the system ghc
+mkdir -p ${HOME}/.stack
+echo "system-ghc: true" > ${HOME}/.stack/config.yaml
+ghc --version
+sed -i.bak "s/lts-10.9/ghc-8.10.7/g" stack.yaml
+
 stack setup
 stack update
 stack install --extra-include-dirs ${PREFIX}/include --local-bin-path ${PREFIX}/bin
