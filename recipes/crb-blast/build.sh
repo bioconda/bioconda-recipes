644ba9859a54c9126072f06fcc96b39ef83b5ee2
Devon Ryan <dpryan79@gmail.com>
2022-11-20 11:31:54 +0100

    change executable directory

--- recipes/crb-blast/build.sh
+++ recipes/crb-blast/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gem env
-gem install --debug --verbose --backtrace $PKG_NAME -v $PKG_VERSION
+gem install --debug --verbose --backtrace --bindir $PREFIX/bin  $PKG_NAME -v $PKG_VERSION
