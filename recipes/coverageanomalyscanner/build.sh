f13569316ec71a46f9a4ce6061084edce062a467
Devon Ryan <dpryan79@users.noreply.github.com>
2022-11-21 13:45:37 +0100

    Update build.sh

--- recipes/coverageanomalyscanner/build.sh
+++ recipes/coverageanomalyscanner/build.sh
@@ -2,7 +2,7 @@
 make -C htslib CC=${CC} CFLAGS="${CFLAGS} -g -Wall -O2 -fvisibility=hidden" LDFLAGS="${LDFLAGS} -fvisibility=hidden"
 
 mkdir -p build
-make print CXX=${CXX} CXXFLAGS="${CXXFLAGS} -Ihtslib" LDLIBS="${LDFLAGS}"
+make print CXX=${CXX} CXXFLAGS="${CXXFLAGS} -Ihtslib -Iargparse/include" LDLIBS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 cp cas ${PREFIX}/bin
