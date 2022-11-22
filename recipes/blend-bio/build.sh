47f6505df2756a9e2d59a92a0a01317558dd91e3
canfirtina <canfirtina@gmail.com>
2022-11-22 21:25:04 +0100

    Adding the CC flag in build.sh

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -3,6 +3,8 @@
 mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" 
+make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" 
 cp blend $PREFIX/bin
