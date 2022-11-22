6c6c04faecbc0f3a16d170a3b62cfe1dd23571ff
canfirtina <canfirtina@gmail.com>
2022-11-23 00:00:33 +0100

    Change of release file

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -3,8 +3,7 @@
 mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" blend sdust
+make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" blend sdust
 cp blend $PREFIX/bin
+cp sdust $PREFIX/bin
