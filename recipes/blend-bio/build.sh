326c73788b66ac3846fa9fca33ecbbc8547b9060
canfirtina <canfirtina@gmail.com>
2022-11-23 00:44:43 +0100

    Enabling sse

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -4,6 +4,6 @@ mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
 
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" blend sdust
+make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" blend sdust
 cp blend $PREFIX/bin
 cp sdust $PREFIX/bin
