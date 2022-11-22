1ef73efd247fb591ab0424e71a633cee3c1f8f98
canfirtina <canfirtina@gmail.com>
2022-11-22 19:51:01 +0100

    Compiles fix

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -3,5 +3,5 @@ mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
 
-make -C $PREFIX/src INCLUDES="-I$PREFIX/include"
+make -C $PREFIX/src INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" 
 mv $PREFIX/src/blend $PREFIX/bin
