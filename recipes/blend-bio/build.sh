59e080d38a238b3891e0613945c1c3b360b89aec
canfirtina <canfirtina@gmail.com>
2022-11-22 20:14:01 +0100

    Updating the release file

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
 
-make -C $PREFIX/src INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" 
-mv $PREFIX/src/blend $PREFIX/bin
+make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" 
+cp blend $PREFIX/bin
