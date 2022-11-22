b4a94098ffa6f90984aebc9efa4a2d5816971096
canfirtina <canfirtina@gmail.com>
2022-11-22 17:38:42 +0100

    Fix make -C

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -3,5 +3,5 @@ mkdir -p $PREFIX/bin
 
 export CPATH=${PREFIX}/include
 
-make -c $PREFIX/src INCLUDES="-I$PREFIX/include"
+make -C $PREFIX/src INCLUDES="-I$PREFIX/include"
 mv $PREFIX/src/blend $PREFIX/bin
