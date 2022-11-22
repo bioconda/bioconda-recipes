10cac675140513a5a001148ff8c11f714638dd6c
canfirtina <canfirtina@gmail.com>
2022-11-22 21:40:58 +0100

    Specfying the target in make

--- recipes/blend-bio/build.sh
+++ recipes/blend-bio/build.sh
@@ -6,5 +6,5 @@ export CPATH=${PREFIX}/include
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -march=native -mavx2 -msse4.1 -L$PREFIX/lib" 
+make CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" blend sdust
 cp blend $PREFIX/bin
