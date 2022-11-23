d4a75ff949571b88e62be362078955d70b317734
Devon Ryan <dpryan79@users.noreply.github.com>
2022-11-23 19:34:31 +0100

    Update build.sh

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -6,4 +6,5 @@ export LIBRARY_PATH=${PREFIX}/lib
 
 cd build
 chmod +x p4utils.pl
+sed -i.bak "s#gcc#${CC}#g;s#g++#${CXX}#g" "tmakelib/linux-g++-64/tmake.conf"
 make linux-g++-64 BINDIR=$PREFIX/bin LIBDIR=$PREFIX/lib CC=$CC CXX=$CXX TMAKE_CXX=$CXX
