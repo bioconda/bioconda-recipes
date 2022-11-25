600abfa1ea18e6a55deac452e72a8ba8ae4e78b8
Krannich479 <krannich479@googlemail.com>
2022-11-25 10:47:07 +0100

    first attempt for OSX fix

--- recipes/coverageanomalyscanner/build.sh
+++ recipes/coverageanomalyscanner/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-make -C htslib CC=${CC} CFLAGS="${CFLAGS} -g -Wall -O2 -fvisibility=hidden" LDFLAGS="${LDFLAGS} -fvisibility=hidden"
+make -C htslib CC=${CC} lib-static #CFLAGS="${CFLAGS} -g -Wall -O2 -fvisibility=hidden" LDFLAGS="${LDFLAGS} -fvisibility=hidden"
 
 mkdir -p build
 make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -O3 -DPRINT" LDLIBS="${LDFLAGS}"
