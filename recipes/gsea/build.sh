b0d07d890131342a8b9477ffe4cbf4f0cbf0510d
Jonathan Manning <jonathan.manning@healx.io>
2022-11-30 14:13:33 +0000

    unpin openjdk, add non-cli, allow memory specification

--- recipes/gsea/build.sh
+++ recipes/gsea/build.sh
@@ -8,12 +8,20 @@ mkdir -p $PACKAGE_HOME
 
 cp -r lib modules gsea.args logging.properties $PACKAGE_HOME 
 
-echo """exec java --module-path="${PACKAGE_HOME}/modules" -Xmx4g \
+echo -e """exec java --module-path="${PACKAGE_HOME}/modules" \${JVM_MEM_OPTS:-"-Xmx4g"} \
     -Djava.awt.headless=true \
     -Djava.util.logging.config.file="${PACKAGE_HOME}/logging.properties" \
     @"${PACKAGE_HOME}/gsea.args" \
     --patch-module="jide.common=${PACKAGE_HOME}/lib/jide-components-3.7.4.jar:${PACKAGE_HOME}/lib/jide-dock-3.7.4.jar:${PACKAGE_HOME}/lib/jide-grids-3.7.4.jar" \
     --module=org.gsea_msigdb.gsea/xapps.gsea.CLI "\$@"
+""" > gsea-cli
+
+echo -e """exec java -showversion --module-path="${PACKAGE_HOME}/modules" \${JVM_MEM_OPTS:-"-Xmx4g"} \
+    @"${PACKAGE_HOME}/gsea.args" \
+    --patch-module="jide.common=${PACKAGE_HOME}/lib/jide-components-3.7.4.jar:${PACKAGE_HOME}/lib/jide-dock-3.7.4.jar:${PACKAGE_HOME}/lib/jide-grids-3.7.4.jar" \
+    -Djava.util.logging.config.file="${PACKAGE_HOME}/logging.properties" \
+    -Dapple.laf.useScreenMenuBar=true \
+    --module=org.gsea_msigdb.gsea/xapps.gsea.GSEA "\$@"
 """ > gsea
 
-cp gsea $PREFIX/bin
+cp gsea gsea-cli $PREFIX/bin
