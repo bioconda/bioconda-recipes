29d0f69cb41c5ef3bf22a5dac30512b3da15a134
Marcel Bargull <marcel.bargull@udo.edu>
2022-12-01 14:15:11 +0100

    rdock: Ensure all our compile/link flags are used

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -1,10 +1,14 @@
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
 cd build
 chmod +x p4utils.pl
-sed -i.bak "s#gcc#${CC}#g;s#g++#${CXX}#g" "tmakelib/linux-g++-64/tmake.conf"
-make linux-g++-64 BINDIR=$PREFIX/bin LIBDIR=$PREFIX/lib CC=$CC CXX=$CXX TMAKE_CXX=$CXX
+
+sed -i \
+  -e "/^TMAKE_CC\s*=/          s|=.*|= ${CC}|" \
+  -e "/^TMAKE_CXX\s*=/         s|=.*|= ${CXX}|" \
+  -e "/^TMAKE_LINK\s*=/        s|=.*|= ${CXX}|" \
+  -e "/^TMAKE_LINK_SHLIB\s*=/  s|=.*|= ${CXX}|" \
+  -e "/^TMAKE_CFLAGS\s*=/      s|=\s*|&${CFLAGS} |" \
+  -e "/^TMAKE_CXXFLAGS\s*=/    s|=\s*|&${CXXFLAGS} |" \
+  -e "/^TMAKE_LFLAGS\s*=/      s|=\s*|&${LDFLAGS} |" \
+  tmakelib/linux-g++-64/tmake.conf
+
+make linux-g++-64 BINDIR=$PREFIX/bin LIBDIR=$PREFIX/lib
