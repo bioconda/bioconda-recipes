8df38a1f2e1bd9319ce45dc922961c2c6d76f1da
Natasha Pavlovikj <natasha.pavlovikj@yahoo.com>
2022-12-02 15:08:38 -0600

    [ci skip] Merge PR 38209
    Merge PR #38209, commits were: 
     * Merge branch 'master' into shasta
     * Add skip linux
     * Update shasta version

--- recipes/shasta/build.sh
+++ recipes/shasta/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "darwin"* ]];then
-  mv ${PKG_NAME}-macOS-${PKG_VERSION} shasta
-fi
-if [[ "$OSTYPE" == "linux-gnu"* ]];then
-  mv ${PKG_NAME}-Linux-${PKG_VERSION} shasta
-fi
-
-chmod +x shasta
 mkdir -p ${PREFIX}/bin
+
+mv ${PKG_NAME}-Linux-${PKG_VERSION} shasta
+chmod +x shasta
 cp shasta ${PREFIX}/bin/shasta
