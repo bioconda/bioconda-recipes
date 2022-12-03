4b68b5733394d873f4e01599732ca42c5af4503f
therealgenna <60659021+therealgenna@users.noreply.github.com>
2022-12-03 17:06:27 -0500

    Update build.sh
    According to https://molpopgen.github.io/fwdpy11/misc/developersguide.html need to install `cbindgen`
    
    Also, need to make `libfwdpy11core.so` available during test stage on linux.

--- recipes/fwdpy11/build.sh
+++ recipes/fwdpy11/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# https://molpopgen.github.io/fwdpy11/misc/developersguide.html
+# https://doc.rust-lang.org/cargo/commands/cargo-install.html
+CARGO_INSTALL_ROOT=$BUILD_PREFIX CARGO_HOME=$BUILD_PREFIX cargo install cbindgen
+#CARGO_INSTALL_ROOT=$PREFIX CARGO_HOME=$PREFIX cargo install cbindgen
+
 # cmake's "find GSL macro" can barf when conda
 # envs are involved, so we use GSL_ROOT_DIR to 
 # fix that:
-GSL_ROOT_DIR=$PREFIX $PYTHON -m pip install --no-deps --ignore-installed -vv .
+#GSL_ROOT_DIR=$PREFIX $PYTHON -m pip install --no-deps --ignore-installed -vv .
+# for mac osx need also to add other vars:
+CARGO_INSTALL_ROOT=$BUILD_PREFIX CARGO_HOME=$BUILD_PREFIX GSL_ROOT_DIR=$PREFIX $PYTHON -m pip install --no-deps --ignore-installed -vv .
+
+# for linux need:
+tmp=`find $PREFIX -name libfwdpy11core.so` # $PREFIX/lib/python3.<n>/site-packages/fwdpy11/libfwdpy11core.so
+[[ -f $tmp ]] && { echo FOUND $tmp; cp $tmp $PREFIX/lib; } || echo COULD NOT FIND libfwdpy11core.so
+# (otherwise this libfwdpy11core.so is not found during test in $RPATH/libfwdpy11core.so and all fails; $RPATH equals $PREFIX/lib)
+# (on mac osx this file extension is `.dylib` instead of `.so`, but there is no need to do this ... it might not be using RPATH ...)
