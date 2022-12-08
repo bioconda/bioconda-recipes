c4106c2a20d31da99e7fdf19078cf9cbab2a3ea4
Matthieu Muffato <mm49@sanger.ac.uk>
2022-12-08 22:16:23 +0000

    Trying without libdeflate too

--- recipes/fastk/build.sh
+++ recipes/fastk/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 export LDFLAGS="-L$PREFIX/lib"
-export CFLAGS="-I$SRC_DIR -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$PREFIX/lib"
-export CPPFLAGS="-I$SRC_DIR -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$PREFIX/lib"
+export CFLAGS="-I$SRC_DIR -I$PREFIX/include -L$PREFIX/lib"
+export CPPFLAGS="-I$SRC_DIR -I$PREFIX/include -L$PREFIX/lib"
 
-# pretend HTSLIB has been built
+# pretend HTSLIB and LIBDEFLATE have been built
+touch HTSLIB/libhts.a
+touch LIBDEFLATE/libdeflate.a
 touch libhts.a
-# build LIBDEFLATE
-cd LIBDEFLATE
-make
-cd -
+touch deflate.lib
 # build fastk
 make
 make install
