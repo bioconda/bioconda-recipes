64231f0873f3b0196f1326024cc9e5af8628a371
Robert A. Petit III <robbie.petit@gmail.com>
2022-12-08 12:09:47 -0700

    try build without htslib and libdeflate

--- recipes/fastk/build.sh
+++ recipes/fastk/build.sh
@@ -4,13 +4,13 @@ export CFLAGS="-I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIB
 export CPPFLAGS="-I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$SRC_DIR/HTSLIB -L$PREFIX/lib"
 
 # build HTSLIB
-cd HTSLIB
-make CC=$CC lib-static
-cd -
+#cd HTSLIB
+#make CC=$CC lib-static
+#cd -
 # build LIBDEFLATE
-cd LIBDEFLATE
-make CC=$CC
-cd -
+#cd LIBDEFLATE
+#make CC=$CC
+#cd -
 # build fastk
 make CC=$CC
 make install
