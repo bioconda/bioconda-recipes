7dc31809194f3d599f439bf0469ee7948c864ba9
Afif Elghraoui <afif@ghraoui.name>
2022-12-10 11:43:47 -0800

    t-coffee: exclude plugins for probconsRNA and prank
    To deal with error
    10:01:23 BIOCONDA INFO (ERR) subprocess.CalledProcessError: Command '['/usr/local/miniconda/envs/bioconda/conda-bld/t-coffee_1670666024246/_build_env/bin/llvm-otool', '-l', '/usr/local/miniconda/envs/bioconda/conda-bld/t-coffee_1670666024246/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_/libexec/t-coffee-13.45.0.4846264-5/plugins/macosx/probconsRNA']' returned non-zero exit status 1.
    also seen with prank after removing only probconsRNA

--- recipes/t-coffee/build.sh
+++ recipes/t-coffee/build.sh
@@ -7,7 +7,7 @@
 # $PKG_VERSION The version of the package
 # $PKG_BUILDNUM The build number of the package
 #
-set -eu -o pipefail
+set -eux -o pipefail
 
 SHARE_DIR="${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 OS=$(./install get_os)
@@ -15,6 +15,15 @@ OS=$(./install get_os)
 mkdir -p "${PREFIX}/bin"
 
 ./install all -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS"
+
+# llvm-otool -l fails for these plugins on macosx
+if [ "$OS" = macosx ]
+then
+    for bad_plug in probconsRNA prank
+    do
+	rm -fv "${SHARE_DIR}/plugins/macosx/${bad_plug}"
+    done
+fi
 
 # The installer may try to update dependencies and install them to bin/,
 # which will cause conflicts with the dependencies as separately packaged.
