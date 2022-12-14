c3d229f05e3dc788431eeac4b764d36cc7f42fd9
Vineet Bansal <vineetbansal@protonmail.com>
2022-12-14 15:20:10 -0500

    [ci skip] Merge PR 38380
    Merge PR #38380, commits were: 
     * Merge branch 'master' into master
     * Correct test
     * Reset build number to 0
     * Removed guidescan from build-fail-blacklist
     * Guidescan version 2.0.0

--- recipes/guidescan/build.sh
+++ recipes/guidescan/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-cd $SRC_DIR/guidescan-source_code
 
-$PYTHON setup.py build # the docs mention this as a step
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+
+make
+cp bin/guidescan $PREFIX/bin
+chmod +x $PREFIX/bin/guidescan
