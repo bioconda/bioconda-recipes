cb1f54da2a9e293982d90fe1d18cd7a54ddc99d3
Ashish Patel <ashish615@gmail.com>
2022-12-15 17:47:27 +0530

    make change

--- recipes/mm2-fast/build.sh
+++ recipes/mm2-fast/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p $PREFIX/bin
-export CPATH=${PREFIX}/include
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib"
+make 
 cp minimap2 $PREFIX/bin 
