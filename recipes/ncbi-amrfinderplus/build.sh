c68c44463fa14ca0e69ccab68e445e5a4c22724b
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2022-12-15 11:11:45 -0500

    Cleaned up fix. Seems to work.

--- recipes/ncbi-amrfinderplus/build.sh
+++ recipes/ncbi-amrfinderplus/build.sh
@@ -9,8 +9,11 @@ echo $PREFIX
 # move patch to meta.yaml
 # patch < patch.3.9.8
 
+# fix error because of gnu++17 features. Suggested by https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
 # note that for version 3.7 the make command should be:
-make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY" PREFIX="$PREFIX" CONDA_DB_DIR="$CONDA_PREFIX/share/amrfinderplus/data"
+make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" CONDA_DB_DIR="$CONDA_PREFIX/share/amrfinderplus/data"
 
 #echo "make CXX=\"$CXX $LDFLAGS\" CPPFLAGS=\"$CXXFLAGS\" PREFIX=\"$PREFIX\" DEFAULT_DB_DIR=\"$PREFIX/share/amrfinderplus/data\""
 
