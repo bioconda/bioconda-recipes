6083c5865239aa1ec7b4602c90423d47d9cb5a7e
Ashish Patel <ashish615@gmail.com>
2022-12-16 09:09:05 +0530

    zlib added

--- recipes/mm2-fast/build.sh
+++ recipes/mm2-fast/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+export CPATH=${PREFIX}/include
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAG:qS -L$PREFIX/lib"
 mkdir -p $PREFIX/bin
-make 
+make $CFLAGS $LDFLAGS
 cp minimap2 $PREFIX/bin
