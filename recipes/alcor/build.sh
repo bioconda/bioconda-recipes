43056ecefd179e1f7456cd6a50262a61b1c99185
Jorge Miguel Silva <jorgeMFS@users.noreply.github.com>
2022-12-17 16:08:12 +0000

    fix alcor

--- recipes/alcor/build.sh
+++ recipes/alcor/build.sh
@@ -10,20 +10,20 @@ set -x
 trap 'echo "Interrupted, cleaning up..."' INT
 
 # Check for required dependencies
-if ! which cmake > /dev/null; then
-    echo "CMake not found, installing..."
-    if [ "$(uname)" == "Darwin" ]; then
-        # Install CMake on Mac using Homebrew
-        brew install cmake
-    elif [ "$(uname)" == "Linux" ]; then
-        # Install CMake on Linux using apt-get
-        apt-get update
-        apt-get install -y cmake
-    else
-        echo "Error: Unsupported operating system"
-        exit 1
-    fi
-fi
+# if ! which cmake > /dev/null; then
+#     echo "CMake not found, installing..."
+#     if [ "$(uname)" == "Darwin" ]; then
+#         # Install CMake on Mac using Homebrew
+#         brew install cmake
+#     elif [ "$(uname)" == "Linux" ]; then
+#         # Install CMake on Linux using apt-get
+#         apt-get update
+#         apt-get install -y cmake
+#     else
+#         echo "Error: Unsupported operating system"
+#         exit 1
+#     fi
+# fi
 
 # Set up the build environment
 cd "$SRC_DIR/src/"
