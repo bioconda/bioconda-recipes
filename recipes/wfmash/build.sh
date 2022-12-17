5e81aacd07b2915cca70274769ea7427cf7a8a14
AndreaGuarracino <andreaguarracino@outlook.com>
2022-12-17 16:42:02 -0600

    fix typo in build.sh

--- recipes/wfmash/build.sh
+++ recipes/wfmash/build.sh
@@ -6,7 +6,7 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
-sed -i 's/-march=native/-march=sandybridge/g -Ofast' src/common/wflign/deps/WFA2-lib/Makefile
+sed -i 's/-march=native/-march=sandybridge/g' src/common/wflign/deps/WFA2-lib/Makefile
 cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
 cmake --build build
 mkdir -p $PREFIX/bin
