5025bd825decbd9d3d563d5a7f7b0a961afbaab0
Ashish Patel <ashish615@gmail.com>
2022-12-20 12:59:50 +0530

    flags

--- recipes/mm2-fast/build.sh
+++ recipes/mm2-fast/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CPATH=${PREFIX}/include
 export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAG:qS -L$PREFIX/lib"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 mkdir -p $PREFIX/bin
 make $CFLAGS $LDFLAGS
 cp minimap2 $PREFIX/bin
