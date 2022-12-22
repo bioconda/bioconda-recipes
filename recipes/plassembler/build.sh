c47e1f7251a0d9c9a9542fc0825cfcb9bd2ccaff
gbouras13 <george.bouras@adelaide.edu.au>
2022-12-22 20:55:43 +1030

    fix build

--- recipes/plassembler/build.sh
+++ recipes/plassembler/build.sh
@@ -1,8 +1,3 @@
-python3 -m pip install . --no-deps -vv 
+python3 -m pip install  
 
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/bin/plassemblerModules/"
-
-cp -r plassembler.py $PREFIX/bin/
-cp -r plassemblerModules/* $PREFIX/bin/plassemblerModules/
 
