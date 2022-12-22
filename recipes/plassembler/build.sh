4e93689c805be604039a9e38a98a42f789253b35
gbouras13 <george.bouras@adelaide.edu.au>
2022-12-22 21:20:09 +1030

    change build

--- recipes/plassembler/build.sh
+++ recipes/plassembler/build.sh
@@ -1,2 +1,7 @@
-python3 -m pip install . --ignore-installed --no-deps -vv
+mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/bin/plassemblerModules"
+
+cp -r plassembler.py $PREFIX/bin/
+cp -r plassemblerModules/* $PREFIX/bin/plassemblerModules
+
 
