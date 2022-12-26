f9bf2302b0bf98f5e12b03e6af10b668f8e024b2
Mike Axtell <mja18@psu.edu>
2022-12-26 10:20:30 -0500

    Update build.sh

--- shorttracks/build.sh
+++ shorttracks/build.sh
@@ -1,1 +1,12 @@
-foobar
+#!/bin/bash
+set -eu -o pipefail
+
+# specify and create /bin/ directory
+BINDIR=$PREFIX/bin
+mkdir -p $BINDIR
+
+# make executable
+chmod +x $SRC_DIR/ShortTracks
+
+# Install the script
+cp $SRC_DIR/ShortTracks $BINDIR
