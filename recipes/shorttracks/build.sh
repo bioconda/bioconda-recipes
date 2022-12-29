d82997aaf12e55981e7377b68225c358091294bb
Mike Axtell <mja18@psu.edu>
2022-12-29 08:22:31 -0500

    Update build.sh

--- recipes/shorttracks/build.sh
+++ recipes/shorttracks/build.sh
@@ -1,1 +1,12 @@
-foo
+#!/bin/bash
+set -eu -o pipefail
+
+# specify and create /bin/ directory
+BINDIR=$PREFIX/bin
+mkdir -p $BINDIR
+
+# make executable
+chmod +x $SRC_DIR/ShortTracks
+
+# Install the script
+cp $SRC_DIR/ShortTracks $BINDIR
