60902225eace81295c8c785020c3b4c558caaffd
timslittle <76944764+timslittle@users.noreply.github.com>
2022-12-30 21:09:55 +0000

    fix perl shebang in gmap script gmst.pl
    Change shebang in gmap/gmst.pl to "#!/usr/bin/env perl", which is more robust than "#!/usr/bin/perl". See: https://bioconda.github.io/contributor/troubleshooting.html#usr-bin-perl-or-usr-bin-python-not-found

--- recipes/rnaquast/build.sh
+++ recipes/rnaquast/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# fix perl shebang in gmap script gmst.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/gmst.pl
+
 OUTDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $OUTDIR
 mkdir -p $PREFIX/bin
