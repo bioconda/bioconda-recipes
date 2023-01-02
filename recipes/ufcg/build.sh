84c3ec33a809c45d7dbda501983c17516a8e6b08
DW Kim <endix1029@gmail.com>
2023-01-02 14:55:15 +0900

    fix tar structure

--- recipes/ufcg/build.sh
+++ recipes/ufcg/build.sh
@@ -3,21 +3,18 @@
 set -x -e
 
 UFCG_JAR="UFCG-latest.jar"
+UFCG_LIC="LICENSE.md"
 UFCG_BIN="${PREFIX}/bin/ufcg"
-UFCG_CFG="ufcg/config"
 
 # Copy JAR to the resource directory
 TARGET="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
-mkdir -p ${TARGET}
-cp ufcg/jar/${UFCG_JAR} ${TARGET}
+mkdir -p "${TARGET}"
+cp "${UFCG_JAR}" "${TARGET}"
+cp "${UFCG_LIC}" "${TARGET}"
 
 # Alias JAR
-mkdir -p ${PREFIX}/bin
-echo "#!/usr/bin/env bash" > ${UFCG_BIN}
-echo "java -jar ${TARGET}/${UFCG_JAR} \$@" >> ${UFCG_BIN}
-chmod +x ${UFCG_BIN}
-
-# Copy resources
-mkdir -p ${PREFIX}/config
-cp ${UFCG_CFG}/* ${PREFIX}/config
+mkdir -p "${PREFIX}/bin"
+echo "#!/usr/bin/env bash" > "${UFCG_BIN}"
+echo "java -jar ${TARGET}/${UFCG_JAR} \$@" >> "${UFCG_BIN}"
+chmod +x "${UFCG_BIN}"
 
