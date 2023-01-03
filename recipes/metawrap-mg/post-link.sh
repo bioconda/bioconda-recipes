f7cf005e703afdee59aa07ab6242087ccdca8f6e
Tom Harrop <twharrop@gmail.com>
2023-01-03 11:06:08 +1100

    add checkm database to build

--- recipes/metawrap-mg/post-link.sh
+++ recipes/metawrap-mg/post-link.sh
@@ -1,3 +1,15 @@
-CHECKM_DIR="$( find "${PREFIX}/lib" -type d -name checkm )"
-find "${CHECKM_DIR}" -type f -name DATA_CONFIG \
-	-exec chmod 777 {} \;
+#!/bin/sh
+
+CHECKM_DIR="${PREFIX}/etc/checkm"
+CHECKM_URL="https://zenodo.org/record/7401545/files/checkm_data_2015_01_16.tar.gz"
+
+wget -O checkm_data.tar.gz \
+	"${CHECKM_URL}"
+
+mkdir -p "${CHECKM_DIR}"
+
+tar -zxf checkm_data.tar.gz \
+	-C "${CHECKM_DIR}" \
+	--strip-components 1
+
+checkm data setRoot	"${CHECKM_DIR}"
