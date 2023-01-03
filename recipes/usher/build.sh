741257676cde8139b710c7e80445bf444c3662c7
Angie Hinrichs <angie@soe.ucsc.edu>
2023-01-03 13:42:01 -0800

    [ci skip] Merge PR 38446
    Merge PR #38446, commits were: 
     * Merge remote-tracking branch 'upstream/master' into usher.v0.6.mac
     * When not building ripples-fast on Mac, add a stub ripples-fast shell script that prints out a message pointing to yatisht/usher#310.
     * Don't skip Mac build for usher; instead, omit ripples-fast when building on Mac.

--- recipes/usher/build.sh
+++ recipes/usher/build.sh
@@ -2,6 +2,7 @@
 #/System/Volumes/Data/System/DriverKit/usr/lib/libSystem.dylib
 mkdir -p $PREFIX/bin
 
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     curl -sSLO https://github.com/oneapi-src/oneTBB/releases/download/2019_U9/tbb2019_20191006oss_mac.tgz
     tar -xzf tbb2019_20191006oss_mac.tgz
@@ -17,7 +18,24 @@ pushd build
 
 cmake -DTBB_DIR=${PWD}/../$tbb_root -DCMAKE_PREFIX_PATH=${PWD}/../$tbb_root/cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 
-make -j 1
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # omit ripples-fast due to problems building on Mac
+    make -j 1 usher matUtils matOptimize usher-sampled ripples
+    cat > ripples-fast <<EOF
+#!/bin/bash
+# This is a placeholder for the program ripples-fast on Mac where the build is currently failing.
+# This is only a temporary workaround until we can fix ripples-fast, so that the rest of the
+# programs in the usher package are not held back.
+
+echo ""
+echo "Sorry, ripples-fast is temporarily unavailable from bioconda for Mac."
+echo "Please chime in on https://github.com/yatisht/usher/issues/310 if this inconveniences you."
+echo ""
+EOF
+    chmod a+x ripples-fast
+else
+    make -j 1
+fi
 
 cp ./usher ${PREFIX}/bin/
 cp ./matUtils ${PREFIX}/bin/
