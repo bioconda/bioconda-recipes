d5165a60c36542ad6ff9a8adbbed149b03acf21c
Wouter De Coster <decosterwouter@gmail.com>
2023-01-04 22:12:26 +0100

    Update build.sh

--- recipes/cramino/build.sh
+++ recipes/cramino/build.sh
@@ -2,7 +2,6 @@
 
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CFLAGS} -fcommon"
-export CPPFLAGS="${CPPLAGS} -mmacosx-version-min=10.12"
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
