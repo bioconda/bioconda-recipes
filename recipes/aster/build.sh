d6aa7e2a8116443a18d27d6057399841f628d532
worku005 <dirk-jan.vanworkum@wur.nl>
2023-01-10 16:11:45 +0100

    asterisk was split in two binaries

--- recipes/aster/build.sh
+++ recipes/aster/build.sh
@@ -14,9 +14,11 @@ fi
 
 cp bin/astral $PREFIX/bin/
 cp bin/astral-pro $PREFIX/bin/
-cp bin/asterisk $PREFIX/bin/
+cp bin/asterisk-hky $PREFIX/bin/
+cp bin/asterisk-pair $PREFIX/bin/
 
 chmod a+x $PREFIX/bin/astral
 chmod a+x $PREFIX/bin/astral-pro
-chmod a+x $PREFIX/bin/asterisk
+chmod a+x $PREFIX/bin/asterisk-hky
+chmod a+x $PREFIX/bin/asterisk-pair
 
