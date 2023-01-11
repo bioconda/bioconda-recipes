ad146b3a964a7de89914fd4964a2534783adb412
Adam Talbot <adam.talbot@nonacus.com>
2023-01-11 15:16:21 +0000

    sentieon: Add LD_LIBRARY_PATH to fix lib error

--- recipes/sentieon/build.sh
+++ recipes/sentieon/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eu
 
+export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
+
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
