941d00a48bd97e86979212254d15fd9f909ef5e9
srubinacci <simone.rubinacci@unil.ch>
2023-01-11 11:38:11 +0100

    Build glimpse2

---
+++ recipes/glimpse2-bio/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+for subdir in chunk concordance ligate phase split_reference
+do
+    pushd $subdir
+    # Build the binaries
+    make \
+        -j 4 \
+        DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
+        CXX="$CXX -std=c++11" \
+        CXXFLAG="$CXXFLAGS ${PREFIX}" \
+        LDFLAG="$LDFLAGS" \
+        HTSLIB_INC="$PREFIX" \
+        HTSLIB_LIB="-lhts" \
+        BOOST_LIB_IO="-lboost_iostreams" \
+        BOOST_LIB_PO="-lboost_program_options" \
+	BOOST_LIB_SE="-lboost_libboost_serialization" \
+    ;
+    # Install the binaries
+    install "bin/GLIMPSE_${subdir}" "${PREFIX}/bin"
+    popd
+done
