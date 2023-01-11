7a8ee841186f93b03d16e7d4369220987891b678
guolidong <guolidong@genomics.cn>
2023-01-11 09:20:10 +0800

    add circle-map-cpp

--- recipes/circle-map-cpp/build.sh
+++ recipes/circle-map-cpp/build.sh
@@ -6,5 +6,14 @@ export CFLAGS=" -L$PREFIX/lib $CFLAGS"
 export CPPFLAGS=" -L$PREFIX/lib $CPPFLAGS"
 export CXXFLAGS=" -L$PREFIX/lib $CXXFLAGS"
 export LD_FLAGS=" -L$PREFIX/lib $LD_FLAGS"
+
+
+if [[ $(uname -s) == Darwin ]]; then
+  RPATH='@loader_path/../lib'
+else
+  ORIGIN='$ORIGIN'
+  export ORIGIN
+  RPATH='$${ORIGIN}/../lib'
+fi
 make condainstall PREFIX=$PREFIX CC=$CC CXX=$CXX
 
