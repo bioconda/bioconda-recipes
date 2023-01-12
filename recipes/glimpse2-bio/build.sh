27e469eb2f127f41a0b8a6a273e3eabb3d3993cd
srubinacci <simone.rubinacci@unil.ch>
2023-01-12 14:32:26 +0100

    fix boost se

--- recipes/glimpse2-bio/build.sh
+++ recipes/glimpse2-bio/build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-
 for subdir in chunk concordance ligate phase split_reference
 do
     pushd $subdir
+    
     # Build the binaries
     make \
         -j 4 \
+        CXX="$CXX -std=c++17" \
         DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
-        CXX="$CXX -std=c++11" \
-        CXXFLAG="$CXXFLAGS ${PREFIX}" \
-        LDFLAG="$LDFLAGS" \
+        COMMIT_VERS="3bed6d9" \
+        COMMIT_DATE="2022-12-07" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
         BOOST_LIB_IO="-lboost_iostreams" \
         BOOST_LIB_PO="-lboost_program_options" \
-	BOOST_LIB_SE="-lboost_libboost_serialization" \
+	BOOST_LIB_SE="-lboost_serialization" \
     ;
     # Install the binaries
     install "bin/GLIMPSE2_${subdir}" "${PREFIX}/bin"
