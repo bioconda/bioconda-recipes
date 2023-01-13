80ef34f44c83869342b7673ba5bf72e923128735
Lee Katz - Aspen <gzu2@cdc.gov>
2023-01-13 12:09:50 -0500

    updated path to source scripts

--- recipes/kalamari/build.sh
+++ recipes/kalamari/build.sh
@@ -4,6 +4,8 @@ mkdir -p $PREFIX/bin
 
 EXES="downloadKalamari.pl  generate_sepia_reference.py  getExactTaxonomy.pl  mobsuiteRepresentativeFasta.pl  validateTaxonomy.pl"
 
-chmod -v 755 $EXES
-cp -f $EXES $PREFIX/bin/
+for i in $EXES; do
+  chmod -v 755 bin/$i
+  cp -vf $i $PREFIX/bin
+done
 
