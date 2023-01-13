ec074e209c44d56834f6fb6d3470d174aaa8b3a6
Lee Katz - Aspen <gzu2@cdc.gov>
2023-01-13 11:07:54 -0500

    build.sh

---
+++ recipes/kalamari/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+
+EXES="downloadKalamari.pl  generate_sepia_reference.py  getExactTaxonomy.pl  mobsuiteRepresentativeFasta.pl  validateTaxonomy.pl"
+
+chmod -v 755 $EXES
+cp -f $EXES $PREFIX/bin/assembly-scan
+
