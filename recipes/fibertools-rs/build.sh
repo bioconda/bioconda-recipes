25979a5d1a00c195e7f2bc069418a970023b7b81
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-01-12 20:28:04 -0800

    add pytorch?

--- recipes/fibertools-rs/build.sh
+++ recipes/fibertools-rs/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+# loading the pytorch c++ libs
+export LIBTORCH=$(python -c 'import torch; from pathlib import Path; print(Path(torch.__file__).parent)')
+export LD_LIBRARY_PATH=${LIBTORCH}/lib:#$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=${LIBTORCH}/lib:#$LD_LIBRARY_PATH
+export LIBTORCH_CXX11_ABI=0
+
 # TODO: Remove the following export when pinning is updated and we use
 #       {{ compiler('rust') }} in the recipe.
 export \
