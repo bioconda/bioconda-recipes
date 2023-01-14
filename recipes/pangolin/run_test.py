304bbd4382ccc52143c7391139b9f34601216ae8
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2023-01-14 21:40:09 +0100

    [ci skip] Merge PR 38830
    Merge PR #38830, commits were: 
     * Require usher >=0.6.2 for pangolin
    
    The new minimal version of usher fixes a bug with input sequences
    without mutations, which makes it possible to revert the previous patch
    of run_test.py.

--- recipes/pangolin/run_test.py
+++ recipes/pangolin/run_test.py
@@ -6,13 +6,6 @@ from subprocess import run, PIPE, STDOUT
 import pangolin
 
 test_data = Path(pangolin.__file__).parent / 'data/reference.fasta'
-refseq = open(test_data).read()
-
-# usher 0.6.1 hangs on input with no mutations
-# see https://github.com/cov-lineages/pangolin/issues/500
-# so we need to engineer suitable test input.
-with open('tmp.fa', 'w') as o:
-    o.write(refseq.replace('ACATGGTTTAGTCAGCGTGG', 'ACATGGTTTAGCCAGCGTGG'))
-cmd = ['pangolin', 'tmp.fa']
+cmd = ['pangolin', str(test_data)]
 pangolin_proc = run(cmd, stdout=PIPE, stderr=STDOUT)
 assert b'Output file written to' in pangolin_proc.stdout, pangolin_proc.stdout
