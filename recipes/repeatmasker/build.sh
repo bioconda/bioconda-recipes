d99fb32e1b0839842bb2341c40db9fe6e80e9efc
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-01-13 16:05:49 -0800

    undo rm changes

--- recipes/repeatmasker/build.sh
+++ recipes/repeatmasker/build.sh
@@ -7,7 +7,8 @@ cp -r * ${RM_DIR}
 
 # configure
 cd ${RM_DIR}
-perl ./configure -libdir ${RM_DIR}/Libraries -trf_prgm ${PREFIX}/bin/trf -rmblast_dir ${PREFIX}/bin/ -hmmer_dir ${PREFIX}/bin -abblast_dir ${PREFIX}/bin -crossmatch_dir ${PREFIX}/bin -default_search_engine rmblast
+perl ./configure -libdir ${RM_DIR}/Libraries -trf_prgm ${PREFIX}/bin/trf  -rmblast_dir ${PREFIX}/bin/ -hmmer_dir ${PREFIX}/bin -abblast_dir ${PREFIX}/bin -crossmatch_dir ${PREFIX}/bin -default_search_engine rmblast
+
 
 # ----- add tools within the bin ------
 
@@ -16,11 +17,11 @@ ln -s ${RM_DIR}/RepeatMasker ${PREFIX}/bin/RepeatMasker
 
 # add other tools
 RM_OTHER_PROGRAMS="DateRepeats DupMasker ProcessRepeats RepeatProteinMask"
-for name in ${RM_OTHER_PROGRAMS}; do
-    ln -s ${RM_DIR}/${name} ${PREFIX}/bin/${name}
+for name in ${RM_OTHER_PROGRAMS} ; do
+  ln -s ${RM_DIR}/${name} ${PREFIX}/bin/${name}
 done
 
 # add all utils
-for name in ${RM_DIR}/util/*; do
-    ln -s $name ${PREFIX}/bin/$(basename $name)
+for name in ${RM_DIR}/util/* ; do
+  ln -s $name ${PREFIX}/bin/$(basename $name)
 done
