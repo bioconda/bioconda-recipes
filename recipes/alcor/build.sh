a16179396f822b7eac97262727a379f7b2eb68be
Jorge Miguel Silva <jorgeMFS@users.noreply.github.com>
2023-01-14 17:46:45 +0000

    update v1.7

--- recipes/alcor/build.sh
+++ recipes/alcor/build.sh
@@ -20,28 +20,3 @@ make CC=$CC
 # Copy the executable to the bin directory
 mkdir -p "$PREFIX/bin"
 cp "$SRC_DIR/src/AlcoR" "$PREFIX/bin/"
-
-
-
-#!/bin/bash
-
-# Stop the script if any command returns a non-zero exit status
-set -e
-
-# Print each command as it is executed
-set -x
-
-# Run cleanup tasks if the script is interrupted
-trap 'echo "Interrupted, cleaning up..."' INT
-
-# Set up the build environment
-cd "$SRC_DIR/src/"
-
-# Build and install the package
-cmake . -DCMAKE_C_COMPILER=$CC
-export CFLAGS="$CFLAGS -fno-common -I$PREFIX/include"
-make CC=$CC
-
-# Copy the executable to the bin directory
-mkdir -p "$PREFIX/bin"
-cp "$SRC_DIR/src/AlcoR" "$PREFIX/bin/"
