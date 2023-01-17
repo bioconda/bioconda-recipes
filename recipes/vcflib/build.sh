453d9efb78ff7e6eea41cd196bde1ca068c101e7
AndreaGuarracino <andreaguarracino@outlook.com>
2023-01-16 20:38:27 -0600

    remove seds

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 set -ex
 
-ls -la
-
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib -lhts -ltabixpp -lpthread -lz -lm -llzma -lbz2"
 export INCLUDES="-I . -Ihtslib -I$PREFIX/include -Itabixpp -I\$(INC_DIR) -L."
 export LIBPATH="-L. -Lhtslib -L$PREFIX/lib -Ltabixpp"
 export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
 
-sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' contrib/smithwaterman/Makefile
-sed -i.bak 's/CFLAGS/CXXFLAGS/g' contrib/smithwaterman/Makefile
+#sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' contrib/smithwaterman/Makefile
+#sed -i.bak 's/CFLAGS/CXXFLAGS/g' contrib/smithwaterman/Makefile
 
-sed -i.bak 's/$</$< $(LDFLAGS)/g' contrib/smithwaterman/Makefile
-sed -i.bak 's/ld/$(LD)/' contrib/smithwaterman/Makefile
-sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' contrib/filevercmp/Makefile
-sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/multichoose/Makefile
-sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/intervaltree/Makefile
+#sed -i.bak 's/$</$< $(LDFLAGS)/g' contrib/smithwaterman/Makefile
+#sed -i.bak 's/ld/$(LD)/' contrib/smithwaterman/Makefile
+#sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' contrib/filevercmp/Makefile
+#sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/multichoose/Makefile
+#sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/intervaltree/Makefile
 
-# MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
-if [ "$(uname)" == "Darwin" ]; then
-    sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' contrib/smithwaterman/Makefile
-    #export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
-    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' contrib/intervaltree/Makefile
-    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' Makefile
-    sed -i.bak 's/if ( n_data/if ( \*n_data/' src/cdflib.cpp
-    
-fi
+## MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
+#if [ "$(uname)" == "Darwin" ]; then
+#    sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' contrib/smithwaterman/Makefile
+#    #export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
+#    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' contrib/intervaltree/Makefile
+#    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' Makefile
+#    sed -i.bak 's/if ( n_data/if ( \*n_data/' src/cdflib.cpp   
+#fi
 # tabix missing library https://github.com/ekg/tabixpp/issues/5
 # Uses newline trick for OSX from: http://stackoverflow.com/a/24299845/252589
 #sed -i.bak 's/SUBDIRS=./SUBDIRS=.\'$'\n''LOBJS=tabix.o/' tabixpp/Makefile
