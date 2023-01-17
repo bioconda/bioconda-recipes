3c5478ac346f3b88af15da5d15a10275de580a27
AndreaGuarracino <andreaguarracino@outlook.com>
2023-01-16 19:58:04 -0600

    ls

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 set -ex
 
+ls -la
+
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib -lhts -ltabixpp -lpthread -lz -lm -llzma -lbz2"
 export INCLUDES="-I . -Ihtslib -I$PREFIX/include -Itabixpp -I\$(INC_DIR) -L."
 export LIBPATH="-L. -Lhtslib -L$PREFIX/lib -Ltabixpp"
 export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64 -std=c++0x"
 
-sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' $PREFIX/contrib/smithwaterman/Makefile
-sed -i.bak 's/CFLAGS/CXXFLAGS/g' $PREFIX/contrib/smithwaterman/Makefile
+sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' contrib/smithwaterman/Makefile
+sed -i.bak 's/CFLAGS/CXXFLAGS/g' contrib/smithwaterman/Makefile
 
-sed -i.bak 's/$</$< $(LDFLAGS)/g' $PREFIX/contrib/smithwaterman/Makefile
-sed -i.bak 's/ld/$(LD)/' $PREFIX/contrib/smithwaterman/Makefile
-sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' $PREFIX/contrib/filevercmp/Makefile
-sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' $PREFIX/contrib/multichoose/Makefile
-sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' $PREFIX/contrib/intervaltree/Makefile
+sed -i.bak 's/$</$< $(LDFLAGS)/g' contrib/smithwaterman/Makefile
+sed -i.bak 's/ld/$(LD)/' contrib/smithwaterman/Makefile
+sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' contrib/filevercmp/Makefile
+sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/multichoose/Makefile
+sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/intervaltree/Makefile
 
 # MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
 if [ "$(uname)" == "Darwin" ]; then
-    sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' $PREFIX/contrib/smithwaterman/Makefile
+    sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' contrib/smithwaterman/Makefile
     #export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
-    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' $PREFIX/contrib/intervaltree/Makefile
-    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' $PREFIX/Makefile
-    sed -i.bak 's/if ( n_data/if ( \*n_data/' $PREFIX/src/cdflib.cpp
+    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' contrib/intervaltree/Makefile
+    sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' Makefile
+    sed -i.bak 's/if ( n_data/if ( \*n_data/' src/cdflib.cpp
     
 fi
 # tabix missing library https://github.com/ekg/tabixpp/issues/5
@@ -35,8 +37,8 @@ fi
 #    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
 pkg-config --list-all
-mkdir -p $PREFIX/build
-cd $PREFIX/build
+mkdir -p build
+cd build
 
 
 #cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DTABIXPP_LOCAL:STRING=$PREFIX/lib
