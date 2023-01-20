36799a09d5a1673dd105f7be179cd2364c384138
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2023-01-20 17:40:49 -0500

    Update build.sh

--- recipes/crisprme/build.sh
+++ recipes/crisprme/build.sh
@@ -3,11 +3,8 @@
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/opt/crisprme"
 chmod -R 700 .
-ls -al ${PREFIX}
-ls -al ${BUILD_PREFIX}
-ls -al ${SRC_DIR}
-unzip PostProcess/pickle_data.zip
-mv CRISTA_predictors.pkl PostProcess/
+unzip ${SRC_DIR}/PostProcess/pickle_data.zip
+mv ${SRC_DIR}/PostProcess/CRISTA_predictors.pkl ${SRC_DIR}/PostProcess/
 cp crisprme.py "${PREFIX}/bin/"
 cp crisprme_test_complete_package.sh "${PREFIX}/bin/"
 cp -R * "${PREFIX}/opt/crisprme/"
