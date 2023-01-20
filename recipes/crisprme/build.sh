dcc8a280ea77e7ade73b8e65632806b8035ec769
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2023-01-20 16:38:20 -0500

    skip OSX test

--- recipes/crisprme/build.sh
+++ recipes/crisprme/build.sh
@@ -3,6 +3,9 @@
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/opt/crisprme"
 chmod -R 700 .
+ls -al ${PREFIX}
+ls -al ${BUILD_PREFIX}
+ls -al ${SRC_DIR}
 unzip PostProcess/pickle_data.zip
 mv CRISTA_predictors.pkl PostProcess/
 cp crisprme.py "${PREFIX}/bin/"
