cdc53cec77b1d62455711011c2e222b1a022afe7
tuncK <19364610+tuncK@users.noreply.github.com>
2023-01-24 15:19:24 +0000

    fixed dependency between packages

--- recipes/ncbi-vdb/build.sh
+++ recipes/ncbi-vdb/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# NCBI_OUTDIR=$SRC_DIR/ncbi-outdir
-
-pushd ncbi-vdb
 
 # TODO: Ideally, we don't want the dynamic prefix-dependent patching below.
 #       The following hard-codes the environment's path in binaries such that
@@ -12,12 +9,13 @@ pushd ncbi-vdb
 #       nearly all binaries of the package are affected by this.
 {
 cat <<end-of-patch
---- libs/kns/tls.c
-+++ libs/kns/tls.c
-@@ -431,4 +431,5 @@
+--- ncbi-vdb/libs/kns/tls.c
++++ ncbi-vdb/libs/kns/tls.c
+@@ -431,4 +431,6 @@
          const char * root_ca_paths [] =
          {
-+            "${PREFIX}/ssl/cacert.pem", /* conda-forge::ca-certificates */
++            "${PREFIX}/ssl/cacert.pem",                          /* conda-forge::ca-certificates */
++            "/usr/local/ssl/cacert.pem",                         /* path in docker */
              "/etc/ssl/certs/ca-certificates.crt",                /* Debian/Ubuntu/Gentoo etc */
              "/etc/pki/tls/certs/ca-bundle.crt",                  /* Fedora/RHEL */
 end-of-patch
@@ -26,10 +24,9 @@ end-of-patch
 
 # Execute Make commands from a separate subdirectory. Else:
 # ERROR: In source builds are not allowed
-popd
 BUILD_DIR=./build_vdb
 mkdir ${BUILD_DIR}
-pushd ${BUILD_DIR}
+cd ${BUILD_DIR}
 
 export CFLAGS="${CFLAGS} -DH5_USE_110_API"
 cmake ../ncbi-vdb/ -DNGS_INCDIR=${PREFIX} \
@@ -37,6 +34,3 @@ cmake ../ncbi-vdb/ -DNGS_INCDIR=${PREFIX} \
          -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 cmake --install .
-#popd
-
-
--- recipes/sra-tools/build.sh
+++ recipes/sra-tools/build.sh
@@ -1,40 +1,6 @@
 #!/bin/bash -e
 
-echo "compiling ncbi-vdb"
-pushd ncbi-vdb
-
-# TODO: Ideally, we don't want the dynamic prefix-dependent patching below.
-#       The following hard-codes the environment's path in binaries such that
-#       those binaries have to be patched upon installation by conda
-#       (with the usual downsides, e.g., being excluded from hard-linking).
-#       This patch applies to a library which is statically linked such that
-#       nearly all binaries of the package are affected by this.
-{
-cat <<end-of-patch
---- libs/kns/tls.c
-+++ libs/kns/tls.c
-@@ -431,4 +431,5 @@
-         const char * root_ca_paths [] =
-         {
-+            "${PREFIX}/ssl/cacert.pem", /* conda-forge::ca-certificates */
-             "/etc/ssl/certs/ca-certificates.crt",                /* Debian/Ubuntu/Gentoo etc */
-             "/etc/pki/tls/certs/ca-bundle.crt",                  /* Fedora/RHEL */
-end-of-patch
-} | patch -p0 -i-
-
-
-# Execute Make commands from a separate subdirectory. Else:
-# ERROR: In source builds are not allowed
-popd
-VDB_BUILD_DIR=./build_vdb
-mkdir ${VDB_BUILD_DIR}
-pushd ${VDB_BUILD_DIR}
-
 export CFLAGS="${CFLAGS} -DH5_USE_110_API"
-cmake ../ncbi-vdb/ -DNGS_INCDIR=${PREFIX} -DCMAKE_BUILD_TYPE=Release
-cmake --build .
-popd
-
 
 echo "compiling sra-tools"
 if [[ $OSTYPE == "darwin"* ]]; then
@@ -44,22 +10,23 @@ fi
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
-mkdir -p sra-tools/obj/ngs/ngs-java/javadoc/ngs-doc  # prevent error on OSX
+mkdir -p obj/ngs/ngs-java/javadoc/ngs-doc  # prevent error on OSX
+
+
 # Execute Make commands from a separate subdirectory. Else:
 # ERROR: In source builds are not allowed
-SRC_DIR=$(pwd)
 SRA_BUILD_DIR=./build_sratools
 mkdir ${SRA_BUILD_DIR}
 pushd ${SRA_BUILD_DIR}
-
-cmake ../sra-tools/ -DVDB_BINDIR=${SRC_DIR}/${VDB_BUILD_DIR}/bin \
-    -DVDB_LIBDIR=${SRC_DIR}/${VDB_BUILD_DIR}/lib \
-    -DVDB_INCDIR=${SRC_DIR}/ncbi-vdb/interfaces \
+cmake ../sra-tools/ -DVDB_BINDIR=${BUILD_PREFIX}/lib64 \
+    -DVDB_LIBDIR=${BUILD_PREFIX}/lib64 \
+    -DVDB_INCDIR=${BUILD_PREFIX}/include \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 cmake --install .
 popd
+
 
 # Strip package version from binary names
 cd "${PREFIX}/bin"
