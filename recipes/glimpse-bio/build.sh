de87bb642cd789e189dcb9646d3c79510e87dfec
LouisBzh <louislenezet@gmail.com>
2023-01-25 18:13:50 +0100

    Improve build.sh with new arguments

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -7,14 +7,16 @@ do
     # Build the binaries
     make \
         -j 4 \
-        DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
+        DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm" \
         CXX="$CXX -std=c++11" \
         CXXFLAG="$CXXFLAGS ${PREFIX}" \
         LDFLAG="$LDFLAGS" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
+        BOOST_INC="/usr/include"\
         BOOST_LIB_IO="-lboost_iostreams" \
         BOOST_LIB_PO="-lboost_program_options" \
+        BOOST_LIB_SE="-lboost_serialization"
     ;
     # Install the binaries
     install "bin/GLIMPSE_${subdir}" "${PREFIX}/bin"
