38e1ef4357ba33406cb42f4bbf2f557cf7601b27
LouisBzh <louislenezet@gmail.com>
2023-01-26 12:31:42 +0100

    Add Commit_ID and Commit_Date to scope

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+COMMIT_VERS=git rev-parse --short HEAD
+COMMIT_DATE=git log -1 --format=%cd --date=short
+__COMMIT_ID__="$COMMIT_VERS"
+__COMMIT_DATE__="$COMMIT_DATE"
 
 for subdir in chunk split_reference phase ligate
+
 do
     pushd $subdir
     # Build the binaries
@@ -9,7 +14,7 @@ do
         -j 4 \
         DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm" \
         CXX="$CXX -std=c++11" \
-        CXXFLAG="$CXXFLAGS ${PREFIX}" \
+        CXXFLAG="$CXXFLAGS ${PREFIX} $COMMIT_VERS $COMMIT_DATE" \
         LDFLAG="$LDFLAGS" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
