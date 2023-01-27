1e5f1e3497b09d0300a3726b088b06649a9b9012
Nils Homer <nilshomer@gmail.com>
2023-01-26 21:55:58 -0700

    Update glimpse-bio to 2.0

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+export __COMMIT_ID__="$(git rev-parse --short HEAD)"
+export __COMMIT_DATE__="$(git show -s --format="%ci" | cut -f 1 -d ' ')"
 
-for subdir in chunk concordance ligate phase sample snparray
+for subdir in chunk split_reference phase ligate
+
 do
     pushd $subdir
     # Build the binaries
     make \
         -j 4 \
-        DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate" \
+        DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm" \
         CXX="$CXX -std=c++11" \
-        CXXFLAG="$CXXFLAGS ${PREFIX}" \
+        CXXFLAG="$CXXFLAGS ${PREFIX} -D__COMMIT_ID__=${__COMMIT_ID__} -D__COMMIT_DATE__=${__COMMIT_DATE__}" \
         LDFLAG="$LDFLAGS" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
+        BOOST_INC="/usr/include"\
         BOOST_LIB_IO="-lboost_iostreams" \
         BOOST_LIB_PO="-lboost_program_options" \
+        BOOST_LIB_SE="-lboost_serialization" \
     ;
     # Install the binaries
     install "bin/GLIMPSE_${subdir}" "${PREFIX}/bin"
