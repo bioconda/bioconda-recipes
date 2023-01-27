df157fa6a1606c07d2ed8cfc0879aa0caf72c3d0
LouisBzh <louislenezet@gmail.com>
2023-01-27 14:18:41 +0100

    Simplify build.sh

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-for subdir in chunk split_reference phase ligate
-
-do
-    pushd $subdir
-    # Build the binaries
-    make \
-        -j 4 \
-        -f "makefile"\
-    ;
-    # Install the binaries
-    install "bin/GLIMPSE2_${subdir}" "${PREFIX}/bin"
-    popd
-done
+make \
+    -j 4 \
+    -f "makefile"\
