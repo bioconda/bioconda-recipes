954eaa0981fc756d89a12cae774ccc34e8ee8600
Rob Patro <rob-p@users.noreply.github.com>
2023-01-28 13:14:29 -0500

    Update build.sh

--- recipes/piscem/build.sh
+++ recipes/piscem/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -euo
 
+unamestr=`uname`
 
-export MACOSX_DEPLOYMENT_TARGET=10.15
-export CFLAGS="${CFLAGS} -fcommon -D_LIBCPP_DISABLE_AVAILABILITY"
-export CXXFLAGS="${CXXFLAGS} -fcommon -D_LIBCPP_DISABLE_AVAILABILITY"
-
+if [ "$unamestr" == 'Darwin' ];
+then
+  export MACOSX_DEPLOYMENT_TARGET=10.15
+  export CFLAGS="${CFLAGS} -fcommon -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+  export CXXFLAGS="${CXXFLAGS} -fcommon -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+else 
+  export CFLAGS="${CFLAGS} -fcommon"
+  export CXXFLAGS="${CXXFLAGS} -fcommon"
+fi
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
