77303e82f1c8dd5c469f4342c7547af5e7556fd8
Rob Patro <rob-p@users.noreply.github.com>
2023-01-29 18:46:57 -0500

    Update build.sh

--- recipes/cuttlefish/build.sh
+++ recipes/cuttlefish/build.sh
@@ -3,6 +3,10 @@
 mkdir build
 cd build
 
+# hack! because bzip2 is broken.
+export CC="${CC} -I ${PREFIX}/include"
+export CXX="${CXX} -I ${PREFIX}/include"
+
 cmake \
     -DINSTANCE_COUNT=64 \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
