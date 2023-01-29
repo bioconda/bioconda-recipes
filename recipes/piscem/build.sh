3edbb06846f20e2aa413f26ee83fab04b6738f15
Rob Patro <rob-p@users.noreply.github.com>
2023-01-29 00:37:41 -0500

    Update build.sh

--- recipes/piscem/build.sh
+++ recipes/piscem/build.sh
@@ -16,7 +16,8 @@ fi
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
+export CARGO_NET_GIT_FETCH_WITH_CLI=true 
+export CARGO_HOME="$(pwd)/.cargo"
 
 if [ "$unamestr" == 'Darwin' ];
 then
