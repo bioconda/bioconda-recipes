48b34408304f6e4bd57d684def44a85af391e8ef
Rob Patro <rob-p@users.noreply.github.com>
2023-01-28 23:28:33 -0500

    Update build.sh

--- recipes/piscem/build.sh
+++ recipes/piscem/build.sh
@@ -18,5 +18,16 @@ fi
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
+if [ "$unamestr" == 'Darwin' ];
+then
+
+cat >$CARGO_HOME/config.toml << EOF
+[build]
+# symbols won't be available until runtime
+rustflags = ["-C", "link-args=-Wl,-undefined,dynamic_lookup"]
+EOF
+
+fi
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path .
