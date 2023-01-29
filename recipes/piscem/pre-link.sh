2d7c16ad50060ac5f8f190ec4d173d2fb9c3f039
Rob Patro <rob-p@users.noreply.github.com>
2023-01-29 10:03:17 -0500

    Delete pre-link.sh

--- recipes/piscem/pre-link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -euo
-
-unamestr=`uname`
-
-if [ "$unamestr" == 'Darwin' ];
-then
-  export CPPFLAGS=$(echo "${CPPFLAGS}" | sed 's|10\.9|10.15|g')
-  export CMAKE_ARGS=$(echo "${CMAKE_ARGS}" | sed 's|10\.9|10.15|g')
-fi
