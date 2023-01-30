692b39be9de4da14958ab55eeb74ee56035b6e42
Rob Patro <rob-p@users.noreply.github.com>
2023-01-29 19:07:14 -0500

    Update build.sh

--- recipes/cuttlefish/build.sh
+++ recipes/cuttlefish/build.sh
@@ -4,10 +4,7 @@ mkdir build
 cd build
 
 # hack! because bzip2 is broken.
-export CC="${CC} -I ${PREFIX}/include"
-export CXX="${CXX} -I ${PREFIX}/include"
-
-cmake \
+CC="${CC} -I ${PREFIX}/include" CXX="${CXX} -I ${PREFIX}/include" cmake \
     -DINSTANCE_COUNT=64 \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCONDA_BUILD=ON \
