3ee2ab72ecad73223f83d2e77f40790f46b10ceb
LouisBzh <louislenezet@gmail.com>
2023-01-30 11:49:28 +0100

    Set as string

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -12,7 +12,7 @@ do
         -j 4 \
         DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm" \
         CXX="$CXX -std=c++17" \
-        CXXFLAG="$CXXFLAGS ${PREFIX} -D__COMMIT_ID__=${__COMMIT_ID__} -D__COMMIT_DATE__=${__COMMIT_DATE__} -Wno-ignored-attributes -O3 -mavx2 -mfma" \
+        CXXFLAG="$CXXFLAGS ${PREFIX} -D__COMMIT_ID__='${__COMMIT_ID__}' -D__COMMIT_DATE__='${__COMMIT_DATE__}' -Wno-ignored-attributes -O3 -mavx2 -mfma" \
         LDFLAG="$LDFLAGS" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
