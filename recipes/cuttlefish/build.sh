92235f360fadda2094123ccd2b8452ed3973fa0c
Rob Patro <rob-p@users.noreply.github.com>
2023-01-29 19:37:09 -0500

    Update build.sh

--- recipes/cuttlefish/build.sh
+++ recipes/cuttlefish/build.sh
@@ -3,13 +3,16 @@
 mkdir build
 cd build
 
-mkdir -p ../external/include
-
-cp ${PREFIX}/include/bzlib.h ../external/include/
-cp ${PREFIX}/include/zlib.h ../external/include/
+#mkdir -p ../external/include
+#cp ${PREFIX}/include/bzlib.h ../external/include/
+#cp ${PREFIX}/include/zlib.h ../external/include/
+#cp ${PREFIX}/include/bzlib.h ../external/include/
+#cp ${PREFIX}/include/zlib.h ../external/include/
 
 # hack! because bzip2 is broken.
 cmake \
+    -DCMAKE_C_COMPILER="${CC} -I${PREFIX}/include" \
+    -DCMAKE_CXX_COMPILER="${CXX} -I${PREFIX}/include" \
     -DINSTANCE_COUNT=64 \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCONDA_BUILD=ON \
