e22d6a1d1030f2cd162c6075802ae6eb026d3620
Rob Patro <rob-p@users.noreply.github.com>
2023-01-29 19:49:39 -0500

    Update build.sh

--- recipes/cuttlefish/build.sh
+++ recipes/cuttlefish/build.sh
@@ -3,18 +3,22 @@
 mkdir build
 cd build
 
-#mkdir -p ../external/include
-#cp ${PREFIX}/include/bzlib.h ../external/include/
-#cp ${PREFIX}/include/zlib.h ../external/include/
-#cp ${PREFIX}/include/bzlib.h ../external/include/
-#cp ${PREFIX}/include/zlib.h ../external/include/
+mkdir -p ../external/include
+cp ${PREFIX}/include/bzlib.h ../external/include/
+cp ${PREFIX}/include/zlib.h ../external/include/
+cp ${PREFIX}/include/bzlib.h ../external/include/
+cp ${PREFIX}/include/zlib.h ../external/include/
 
 # hack! because bzip2 is broken.
 cmake \
-    -DCMAKE_C_COMPILER="${CC} -I${PREFIX}/include" \
-    -DCMAKE_CXX_COMPILER="${CXX} -I${PREFIX}/include" \
     -DINSTANCE_COUNT=64 \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCONDA_BUILD=ON \
     ..
+    
+cp ${PREFIX}/include/bzlib.h ../external/KMC-3.2.1/kmc_core/
+cp ${PREFIX}/include/zlib.h ../external/KMC-3.2.1/kmc_core/
+cp ${PREFIX}/include/bzlib.h ../external/KMC-3.2.1/kmc_core/
+cp ${PREFIX}/include/zlib.h ../external/KMC-3.2.1/kmc_core/
+
 make install
