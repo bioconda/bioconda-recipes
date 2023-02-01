72891a3752d81c721182d1f5d76c6f668f2b077a
jayramr <jayaram161989@gmail.com>
2023-02-01 21:31:35 +0400

    install_reforge

---
+++ recipes/reforge/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export LIBRARY_PATH="$PREFIX/lib"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+mkdir -p $PREFIX/bin
+
+ln  "$CC" "$PREFIX/bin/gcc"
+ln  "$CXX" "$PREFIX/bin/g++"
+
+wget https://github.com/UIUCSinhaLab/Stubb/raw/main/stubb_2.1.tar.gz
+tar xvf stubb_2.1.tar.gz
+wget http://www.robertnz.net/ftp/newmat11.tar.gz
+tar -xvf newmat11.tar.gz -C stubb_2.1/lib/newmat/
+
+patch -p1 < ../REforge/stubb.patch
+cd lib/newmat/
+gmake -f nm_gnu.mak
+cd ../../
+make
+cd ../REforge/
+
+mv *.py $PREFIX/bin/
+mv stubb_2.1/bin/* $PREFIX/bin/
