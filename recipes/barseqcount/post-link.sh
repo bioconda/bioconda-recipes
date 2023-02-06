da11cfcce47bc3f867dfb4ecf1333b1c915aaa46
damienmarsic <daming@SLJS20011.porton.com>
2023-02-06 13:29:12 +0800

    modified:   barseqcount/post-link.sh 	new file:   barseqcount/pre-unlink.sh

--- recipes/barseqcount/post-link.sh
+++ recipes/barseqcount/post-link.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "#!/bin/sh
-python -m barseqcount \$@
-" >  "$CONDA_PREFIX"/bin/barseqcount
-chmod +x "$CONDA_PREFIX"/bin/barseqcount
-
+python -m $PKG_NAME \$@
+" >  $CONDA_PREFIX/bin/$PKG_NAME
+chmod +x $CONDA_PREFIX/bin/$PKG_NAME
