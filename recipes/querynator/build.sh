73d8835eba4f551313d8ab0c98497342d80f075c
mapo9 <mark.polster@uni-tuebingen.de>
2023-02-16 12:52:45 +0000

    civicpy path to wheel

--- recipes/querynator/build.sh
+++ recipes/querynator/build.sh
@@ -9,5 +9,5 @@ do
 done
 echo "civicpy is not available from conda ... ... ..."
 ${PYTHON} -m pip uninstall civicpy
-${PYTHON} -m pip install civicpy -vv
+${PYTHON} -m pip install https://files.pythonhosted.org/packages/01/6f/ffc1989427467eb2f8728387f9377c454f83ec42c57e1a995769d3166c66/civicpy-3.0.0-py3-none-any.whl -vv
 echo " === Build finished"
