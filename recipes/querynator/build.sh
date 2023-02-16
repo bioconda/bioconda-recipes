bf48f330ffe5b0c21a857b5f8dad083694e4d358
mapo9 <mark.polster@uni-tuebingen.de>
2023-02-16 12:34:38 +0000

    removed civicpy from host-deps

--- recipes/querynator/build.sh
+++ recipes/querynator/build.sh
@@ -10,5 +10,4 @@ done
 echo "civicpy is not available from conda ... ... ..."
 ${PYTHON} -m pip uninstall civicpy
 ${PYTHON} -m pip install civicpy -vv
-# ${PYTHON} -m pip install https://files.pythonhosted.org/packages/14/d3/65e304ef565997a57f12d79257beccb57d14efc927069a3be7d3183a8acc/tensorflow-2.7.2-cp38-cp38-manylinux2010_x86_64.whl -vv
 echo " === Build finished"
