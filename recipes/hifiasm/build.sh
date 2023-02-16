34184d267e80c747c6a31a9e01b70d2f88d7075f
chhylp123 <hcheng@jimmy.harvard.edu>
2023-02-16 14:11:08 -0500

    for zlib

--- recipes/hifiasm/build.sh
+++ recipes/hifiasm/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p $PREFIX/bin
 
-make INCLUDES="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -lz" CC=${CC} CXX=${CXX}
+make INCLUDES="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
 cp hifiasm $PREFIX/bin
