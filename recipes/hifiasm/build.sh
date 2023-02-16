8b5b2438b7e8ca6e4218465c4b0b0c97af483755
chhylp123 <hcheng@jimmy.harvard.edu>
2023-02-16 14:31:36 -0500

    update for lz

--- recipes/hifiasm/build.sh
+++ recipes/hifiasm/build.sh
@@ -2,5 +2,6 @@
 
 mkdir -p $PREFIX/bin
 
-make INCLUDES="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
+#make INCLUDES="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
+make CXXFLAGS="-g -I$PREFIX/include -O3 -msse4.2 -mpopcnt -fomit-frame-pointer -Wall" LDFLAGS="-L$PREFIX/lib -lz -lpthread -lm" CC=${CC} CXX=${CXX}
 cp hifiasm $PREFIX/bin
