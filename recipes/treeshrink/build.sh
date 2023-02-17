19fbdb676ff001b00ad49c5bd7138faa90d5d355
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2023-02-17 09:59:16 -0500

    list r-bms as a dependency rather than installing its copy in the treeshrink repo

--- recipes/treeshrink/build.sh
+++ recipes/treeshrink/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-R CMD INSTALL -l Rlib dependencies/BMS_0.3.3.tar.gz
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
 
 ln -s $PREFIX/bin/run_treeshrink.py $PREFIX/bin/run_treeshrink
