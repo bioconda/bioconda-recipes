bbbcdc5daa041d6bc67ff2d09e84046f14f33606
James A. Fellows Yates <jfy133@gmail.com>
2023-02-20 13:07:07 +0100

    Try adding javafx to fix mac problems

--- recipes/malt/build.sh
+++ recipes/malt/build.sh
@@ -5,5 +5,8 @@ chmod u+x MALT_unix_0_6_1.sh
 MALT="$PREFIX/opt/$PKG_NAME-$PKG_VERSION"
 ./MALT_unix_0_6_1.sh -q -dir "$MALT"
 
+# use the javafx library installed by conda
+rm -r "${OUT}/jars/libs/modules/"
+
 ln -s "$MALT"/malt-build "$PREFIX"/bin/malt-build
 ln -s "$MALT"/malt-run "$PREFIX"/bin/malt-run
