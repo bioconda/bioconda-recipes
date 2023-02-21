2d872422b55102f7f397f01481cca4769bdee917
cguyomar <cervin.guyomar@gmail.com>
2023-02-21 08:57:21 +0100

    Change again sed command

--- recipes/phaser/build.sh
+++ recipes/phaser/build.sh
@@ -9,11 +9,11 @@ sed -i.bak "1s|.*|#!/usr/bin/env python2|" phaser.py
 cp phaser.py $PREFIX/bin/phaser.py
 
 cd ..
-sed -i.bak '1i\#!\/usr\/bin\/env python2' phaser_annotate/phaser_annotate.py
+sed -i.bak '1s|^|#/usr/bin/env python2 \'$'\n|' phaser_annotate/phaser_annotate.py
 chmod +x phaser_annotate/phaser_annotate.py
 cp phaser_annotate/phaser_annotate.py ${PREFIX}/bin/
 
-sed -i.bak '1i\#!\/usr\/bin\/env python2' phaser_gene_ae/phaser_gene_ae.py
+sed -i.bak '1s|^|#/usr/bin/env python2 \'$'\n|' phaser_gene_ae/phaser_gene_ae.py
 chmod +x phaser_gene_ae/phaser_gene_ae.py
 cp phaser_gene_ae/phaser_gene_ae.py ${PREFIX}/bin/
 
