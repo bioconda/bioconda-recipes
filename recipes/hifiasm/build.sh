40bb5b2624292d2f9b539f83249e160ae8f4fc3f
Björn Grüning <bjoern@gruenings.eu>
2023-02-23 09:39:35 +0100

    include upstream CXXFLAGS

--- recipes/hifiasm/build.sh
+++ recipes/hifiasm/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p $PREFIX/bin
 
-make INCLUDES="-I$PREFIX/include" CXXFLAGS="-L$PREFIX/lib" CC=${CC} CXX=${CXX}
+make INCLUDES="-I$PREFIX/include" CXXFLAGS="-L$PREFIX/lib -g -O3 -msse4.2 -mpopcnt -fomit-frame-pointer -Wall" CC=${CC} CXX=${CXX}
 cp hifiasm $PREFIX/bin
