6225024f6279e79d5151f98ee63703ee65354b97
Devon Ryan <dpryan79@users.noreply.github.com>
2023-02-23 12:40:36 +0100

    Update build.sh

--- recipes/malt/build.sh
+++ recipes/malt/build.sh
@@ -3,6 +3,7 @@
 # call installer
 chmod u+x MALT_unix_0_6_1.sh
 MALT="$PREFIX/opt/$PKG_NAME-$PKG_VERSION"
+export app_java_home=$PREFIX
 ./MALT_unix_0_6_1.sh -q -dir "$MALT"
 
 ln -s "$MALT"/malt-build "$PREFIX"/bin/malt-build
