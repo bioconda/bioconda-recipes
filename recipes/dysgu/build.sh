e83fcb222b7ac8be94e90c912457d3c1775e654e
kcleal <clealk@cardiff.ac.uk>
2023-02-27 09:50:00 +0000

    Adding dysgu

---
+++ recipes/dysgu/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export C_INCLUDE_PATH=${PREFIX}/include
+
+# build bundled htslib
+cd ./dysgu/htslib
+autoreconf -i
+./configure
+make
+cd ../../
+
+$PYTHON setup.py install
