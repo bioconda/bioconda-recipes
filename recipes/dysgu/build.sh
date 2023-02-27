4e0e468367b9d7e3d6714eb11ec480a0cb350b03
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-02-27 12:03:59 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -4,9 +4,12 @@ export LDFLAGS="-L$PREFIX/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
 
 # build bundled htslib
-cd ./dysgu/htslib
+cd ./dysgu
+wget -O htslib.tar.bz2 https://github.com/samtools/htslib/releases/download/1.17/htslib-1.17.tar.bz2
+tar -xvf htslib.tar.bz2
+rm htslib.tar.bz2 && cd htslib
 autoreconf -i
-./configure
+./configure --enable-libcurl --with-libdeflate --enable-bz2 --enable-lzma
 make
 cd ../../
 
