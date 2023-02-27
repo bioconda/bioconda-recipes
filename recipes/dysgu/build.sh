2029a7054a60faccbdc721849a3bcb612ae04fd0
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-02-27 14:05:31 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -4,10 +4,9 @@ export LDFLAGS="-L$PREFIX/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
 
 cd ./dysgu
-wget -O htslib.tar.bz2 https://github.com/samtools/htslib/releases/download/1.17/htslib-1.17.tar.bz2
-tar -xvf htslib.tar.bz2
-mv ./htslib-1.17 ./htslib
-rm htslib.tar.bz2
+wget https://github.com/samtools/htslib/releases/download/1.17/htslib-1.17.tar.bz2
+tar -xvf htslib-1.17.tar.bz2 && mv htslib-1.17 htslib
+rm htslib-1.17.tar.bz2
 cd ./htslib
 pwd
 ls
