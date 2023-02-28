b8fa46dc0418adee2988a08f47486bd98421e857
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-02-28 14:03:56 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -8,7 +8,10 @@ git clone --recurse-submodules https://github.com/samtools/htslib.git
 cd ./htslib
 autoreconf -i
 ./configure --with-libdeflate
-make
+ls ${BUILD_PREFIX}
+pwd
+${BUILD_PREFIX}/include
+CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/include" make
 cd ../../
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
