19ded61ffd5509badf2ff096ed31e1c071697a9d
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-02-28 14:56:15 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -6,12 +6,13 @@ export C_INCLUDE_PATH=${PREFIX}/include
 cd ./dysgu
 git clone --recurse-submodules https://github.com/samtools/htslib.git
 cd ./htslib
+git clone https://github.com/ebiggers/libdeflate.git
 autoreconf -i
 ./configure --with-libdeflate
 ls ${BUILD_PREFIX}
 pwd
 ${BUILD_PREFIX}/include
-CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/include" make
+CPPFLAGS="${CPPFLAGS} -I./libdeflate" make
 cd ../../
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
