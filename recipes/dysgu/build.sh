30f201fdacc6278864eb960dfda5e2843ea7761f
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-01 11:48:23 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -2,7 +2,6 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
-
 cd ./dysgu
 git clone --recurse-submodules https://github.com/samtools/htslib.git
 cd ./htslib
