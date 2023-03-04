28255912bea0efc1c493ab47db8bc949eef0f8cf
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-03-04 21:17:53 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -2,5 +2,5 @@
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
-touch dysgu/post_call_metrics.pyx
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
