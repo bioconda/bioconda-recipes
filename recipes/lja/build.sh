9cac99fdddeca147282fd212324d7cdc1c0189ef
zilov <zilov@scamt-itmo.ru>
2023-03-07 11:33:34 +0000

    fix: url to archive and make fix

--- recipes/lja/build.sh
+++ recipes/lja/build.sh
@@ -9,15 +9,11 @@ echo "Building LJA..."
 cd $SRC_DIR
 
 echo "cmake -B$BUILD_PREFIX -S$SRC_DIR"
-cmake -B $BUILD_PREFIX -S $SRC_DIR
+cmake -S $SRC_DIR
 
-
-cd $BUILD_PREFIX
 echo "make BUILD_BINDIR=$BUILD_PREFIX BUILD_LIBDIR=$BUILD_PREFIX  $BUILD_PREFIX"
 
-find ./ -type f -exec touch {} +
-make clean
-make .
+make
 
 echo 'dir .'
 ls .
@@ -28,8 +24,6 @@ ls $PREFIX
 echo 'dir build'
 ls $BUILD_PREFIX
 
-mkdir $PREFIX/bin/
-chmod +x ./bin/* 
-ln -s $BUILD_DIR/bin/* $PREFIX/bin/
+mv $SRC_DIR/* $PREFIX
 
 cd $PREFIX/bin
