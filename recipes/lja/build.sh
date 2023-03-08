6a39268bf5b3fded837143b6282c6573f5df2042
zilov <zilov@scamt-itmo.ru>
2023-03-08 13:57:48 +0000

    ls all folders

--- recipes/lja/build.sh
+++ recipes/lja/build.sh
@@ -7,16 +7,24 @@ cd $SRC_DIR
 echo "cmake -S $SRC_DIR"
 cmake -S $SRC_DIR
 
-echo "make"
-make CXX=$CXX INCLUDES="-I$PREFIX/include" CFLAGS+="-L$PREFIX/lib"
-
-mv $SRC_DIR/* $PREFIX
-
 echo 'dir src'
 ls $SRC_DIR
 echo 'dir prefix'
 ls $PREFIX
+echo 'dir prefix/include'
+ls $PREFIX/lib
+echo 'dir prefix/lib'
+ls $PREFIX/lib
 echo 'dir build'
 ls $BUILD_PREFIX
+echo '$BUILD_PREFIX/lib'
+ls $BUILD_PREFIX/lib
+echo '$BUILD_PREFIX/includes'
+ls $BUILD_PREFIX/includes
+
+echo "make"
+make CXX=$CXX INCLUDES="-I$PREFIX/include" CFLAGS+="-L$PREFIX/lib"
+
+mv $SRC_DIR/* $PREFIX
 
 cd $PREFIX/bin
