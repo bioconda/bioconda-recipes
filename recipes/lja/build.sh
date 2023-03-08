e1943e9d7b499ba544a11eb4c73358b2065e9911
zilov <zilov@scamt-itmo.ru>
2023-03-08 13:38:15 +0000

    fix: mmannot zlib.h fix

--- recipes/lja/build.sh
+++ recipes/lja/build.sh
@@ -1,29 +1,22 @@
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-
 set -e
 
 echo "Building LJA..."
 
 cd $SRC_DIR
 
-echo "cmake -B$BUILD_PREFIX -S$SRC_DIR"
+echo "cmake -S $SRC_DIR"
 cmake -S $SRC_DIR
 
-echo "make BUILD_BINDIR=$BUILD_PREFIX BUILD_LIBDIR=$BUILD_PREFIX  $BUILD_PREFIX"
+echo "make"
+make CXX=$CXX INCLUDES="-I$PREFIX/include" CFLAGS+="-L$PREFIX/lib"
 
-make
+mv $SRC_DIR/* $PREFIX
 
-echo 'dir .'
-ls .
 echo 'dir src'
 ls $SRC_DIR
 echo 'dir prefix'
 ls $PREFIX
 echo 'dir build'
 ls $BUILD_PREFIX
-
-mv $SRC_DIR/* $PREFIX
 
 cd $PREFIX/bin
