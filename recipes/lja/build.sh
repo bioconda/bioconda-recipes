6754b8b7c114dd3a6947c47351dd210f52ab7350
zilov <zilov@scamt-itmo.ru>
2023-03-09 12:59:42 +0000

    rm flags

--- recipes/lja/build.sh
+++ recipes/lja/build.sh
@@ -22,14 +22,10 @@ ls $BUILD_PREFIX/lib
 echo '$BUILD_PREFIX/include'
 ls $BUILD_PREFIX/include
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -lrt -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-
 echo "make"
 # fix of gettime bug
 sed -i 's/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -lstdc++fs -ggdb3 ${OpenMP_CXX_FLAGS}" )/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -lstdc++fs -ggdb3 ${OpenMP_CXX_FLAGS} -lrt" )/g' $SRC_DIR/CMakeLists.txt
-make
+make CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
 
 mv $SRC_DIR/* $PREFIX
 
