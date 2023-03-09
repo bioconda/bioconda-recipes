72f417fbcf4b4918fca2e855fee16c2cd9623802
zilov <zilov@scamt-itmo.ru>
2023-03-09 12:25:51 +0000

    fix of gettime bug

--- recipes/lja/build.sh
+++ recipes/lja/build.sh
@@ -27,6 +27,8 @@ export LDFLAGS="$LDFLAGS -lrt -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 echo "make"
+# fix of gettime bug
+sed -i 's/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -lstdc++fs -ggdb3 ${OpenMP_CXX_FLAGS}" )/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -lstdc++fs -ggdb3 ${OpenMP_CXX_FLAGS} -lrt" )/g' $SRC_DIR/CMakeLists.txt
 make
 
 mv $SRC_DIR/* $PREFIX
