fa5578643e0d459dc84c4ca3cfdf5be2bc5c35bb
LouisBzh <louislenezet@gmail.com>
2023-03-10 19:04:53 +0100

    Correct file names

--- recipes/impute5/build.sh
+++ recipes/impute5/build.sh
@@ -3,6 +3,6 @@ set -eu
 
 mkdir -p $PREFIX/bin
 ls
-cp impute5_v1.1.5/impute5 $PREFIX/bin
-cp impute5_v1.1.5/imp5Chunker $PREFIX/bin
-cp impute5_v1.1.5/imp5Converter $PREFIX/bin
+cp impute5_v1.1.5/impute5_1.1.5_static $PREFIX/bin/impute5
+cp impute5_v1.1.5/imp5Chunker_1.1.5_static $PREFIX/bin/imp5Chunker
+cp impute5_v1.1.5/imp5Converter_1.1.5_static $PREFIX/bin/imp5Converter
