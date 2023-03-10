36ce8f65197c721beb1c0449c7504026637afed0
LouisBzh <louislenezet@gmail.com>
2023-03-10 18:51:21 +0100

    Change build command

--- recipes/impute5/build.sh
+++ recipes/impute5/build.sh
@@ -2,6 +2,7 @@
 set -eu
 
 mkdir -p $PREFIX/bin
-cp impute5 $PREFIX/bin
-cp imp5Chunker $PREFIX/bin
-cp imp5Converter $PREFIX/bin
+ls
+cp impute5_v1.1.5/impute5 $PREFIX/bin
+cp impute5_v1.1.5/imp5Chunker $PREFIX/bin
+cp impute5_v1.1.5/imp5Converter $PREFIX/bin
