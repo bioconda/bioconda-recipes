2f3592e8e8bfbd70a7e431f7c14cd2710644d067
alienzj <alienchuj@gmail.com>
2023-03-15 11:37:20 +0800

    update kraken2 build

--- recipes/kraken2/build.sh
+++ recipes/kraken2/build.sh
@@ -4,15 +4,16 @@
 export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
-mkdir -p "$PREFIX/share/kraken2" "$PREFIX/bin"
+mkdir -p "${outdir}/libexec" "$PREFIX/bin"
 
 chmod u+x install_kraken2.sh
-./install_kraken2.sh "$PREFIX/share/kraken2"
+./install_kraken2.sh "${outdir}/libexec"
 for bin in kraken2 kraken2-build kraken2-inspect; do
-    chmod +x "$PREFIX/share/kraken2/$bin"
-    ln -s "$PREFIX/share/kraken2/$bin" "$PREFIX/bin/$bin"
+    chmod +x "${outdir}/libexec/$bin"
+    ln -s "${outdir}/libexec/$bin" "$PREFIX/bin/$bin"
     # Change from double quotes to single in case of special chars
-    sed -i.bak "s#my \$KRAKEN_DIR = \"$PREFIX/share/kraken2\";#my \$KRAKEN_DIR = '$PREFIX/share/kraken2';#g" "$PREFIX/share/kraken2/${bin}"
-    rm -rf "$PREFIX/share/kraken2/${bin}.bak"
+    sed -i.bak "s#my \$KRAKEN_DIR = \"${outdir}/libexec\";#my \$KRAKEN_DIR = '${outdir}/libexec';#g" "${outdir}/libexec/${bin}"
+    rm -rf "${outdir}/libexec/${bin}.bak"
 done
