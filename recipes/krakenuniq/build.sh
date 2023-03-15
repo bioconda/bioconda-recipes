29c858b305be16ef0b5d6397cd04feab703df0c1
alienzj <alienchuj@gmail.com>
2023-03-15 11:08:29 +0800

    update krakenuniq build

--- recipes/krakenuniq/build.sh
+++ recipes/krakenuniq/build.sh
@@ -3,7 +3,7 @@ export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
+mkdir -p "$PREFIX/share/krakenuniq" "$PREFIX/bin"
 
 # src/Makefile uses
 # > CXX = g++
@@ -20,17 +20,17 @@ if [ x"$(uname)" == x"Darwin" ]; then
 fi
 
 chmod u+x install_krakenuniq.sh
-./install_krakenuniq.sh "$PREFIX/libexec"
+./install_krakenuniq.sh "$PREFIX/share/krakenuniq"
 
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/libexec/krakenuniq-extract-reads
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/share/krakenuniq/krakenuniq-extract-reads
 
-sed -i.bak 's#$script_dir/jellyfish-install/bin/jellyfish#jellyfish#g' "$PREFIX/libexec/build_db.sh"
-ln -s "$PREFIX/libexec/build_taxdb" "$PREFIX/bin/build_taxdb"
+sed -i.bak 's#$script_dir/jellyfish-install/bin/jellyfish#jellyfish#g' "$PREFIX/share/krakenuniq/build_db.sh"
+ln -s "$PREFIX/share/krakenuniq/build_taxdb" "$PREFIX/bin/build_taxdb"
 
 for bin in krakenuniq krakenuniq-build krakenuniq-download krakenuniq-extract-reads krakenuniq-filter krakenuniq-mpa-report krakenuniq-report krakenuniq-translate read_merger.pl; do
-    chmod +x "$PREFIX/libexec/$bin"
-    ln -s "$PREFIX/libexec/$bin" "$PREFIX/bin/$bin"
+    chmod +x "$PREFIX/share/krakenuniq/$bin"
+    ln -s "$PREFIX/share/krakenuniq/$bin" "$PREFIX/bin/$bin"
     # Change from double quotes to single in case of special chars
-    sed -i.bak "s#my \$KRAKEN_DIR = \"$PREFIX/libexec\";#my \$KRAKEN_DIR = '$PREFIX/libexec';#g" "$PREFIX/libexec/${bin}"
-    rm -rf "$PREFIX/libexec/${bin}.bak"
+    sed -i.bak "s#my \$KRAKEN_DIR = \"$PREFIX/share/krakenuniq\";#my \$KRAKEN_DIR = '$PREFIX/share/krakenuniq';#g" "$PREFIX/share/krakenuniq/${bin}"
+    rm -rf "$PREFIX/share/krakenuniq/${bin}.bak"
 done
