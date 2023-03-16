dfd5a2f2107958c031fb50924297fdb72689435d
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-03-16 19:42:22 +0000

    change build.sh remove shared.

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+export INCLUDE_PATH="${PREFIX}/include"
+export CPPFLAGS="-I${PREFIX}/include"
+
 make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large -fpic" CFLAGS="${CFLAGS} -mcmodel=large -fpic"
 mkdir -p "$PREFIX"/bin
 cp comet.exe ${PREFIX}/bin/comet
