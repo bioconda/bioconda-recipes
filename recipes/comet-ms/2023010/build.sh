3b5f630b53dc81309647c796678c9d988618c246
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-03-16 11:54:54 +0000

    remove unzip, done by meta.yaml

--- recipes/comet-ms/2023010/build.sh
+++ recipes/comet-ms/2023010/build.sh
@@ -2,8 +2,6 @@
 set -e
 set -x
 
-platform="$(uname)"
-unzip v2023.01.0.zip
 make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large -fpic -shared"
 mkdir -p "$PREFIX"/bin
 cp comet.exe ${PREFIX}/bin/comet
