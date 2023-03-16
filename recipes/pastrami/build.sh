b225f8af883253d271ce62ce2a8b21ed69953255
sgupta1524 <sgupta@asrtinc.com>
2023-03-16 19:39:24 -0400

    fix: fixing meta.yaml

--- recipes/pastrami/build.sh
+++ recipes/pastrami/build.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-PIP_NO_CACHE_DIR=True
+#chmod +x $SRC_DIR/pastrami.py
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install --no-deps .  
+PIP_NO_CACHE_DIR=True
+$PYTHON -m pip install --no-deps .
