37d212a325eddbca6c0c532db83872ce8bd88836
Vincent Ranwez <vincent.ranwez@supagro.fr>
2023-03-16 11:40:20 +0100

    cleaning recipe

--- recipes/macse/build.sh
+++ recipes/macse/build.sh
@@ -6,10 +6,6 @@ TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 [ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
 
 cd "${SRC_DIR}"
-# Do not install Linux specific x86-acceleration libraries
-if [ "$(uname)" == "Darwin" ]; then
-    rm -f libIntel*.so
-fi
 cp -rvp . "${TGT}"
 
 cp $RECIPE_DIR/macse.sh $TGT/macse
