3e2d639b59a652ce2306f1e8d01082b0b22208e8
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-03-16 09:39:57 +0000

    Update recipes/comet-ms/2023010/build.sh
    Co-authored-by: Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>

--- recipes/comet-ms/2023010/build.sh
+++ recipes/comet-ms/2023010/build.sh
@@ -14,7 +14,7 @@ if [ "$platform" = "Darwin" ]; then
 fi
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large"
+make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large -fPIC"
 mkdir -p "$PREFIX"/bin
 cp comet.exe ${PREFIX}/bin/comet
 chmod a+x ${PREFIX}/bin/comet
