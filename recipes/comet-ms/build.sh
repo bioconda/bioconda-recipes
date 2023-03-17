e6cd23687df0c465a4b59d8fef6433b01f8aeb04
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-03-17 10:21:46 +0000

    Update recipes/comet-ms/build.sh
    Co-authored-by: Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -8,7 +8,7 @@ export CPPFLAGS="-I${PREFIX}/MSToolkit/include"
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
 
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large -fPIC -I./include" CFLAGS="${CFLAGS} -static -mcmodel=large -fPIC -I./include -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DGCC -DHAVE_EXPAT_CONFIG_H"
+make CXX=${CXX}
 
 mkdir -p "$PREFIX"/bin
 cp comet.exe ${PREFIX}/bin/comet
