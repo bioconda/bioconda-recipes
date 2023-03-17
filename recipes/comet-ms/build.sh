dea32d8f099b188ad9385a94282c52a361ecc88e
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-03-17 10:20:28 +0000

    Update build.sh

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -6,6 +6,8 @@ export INCLUDE_PATH="${PREFIX}/MSToolkit/include"
 export CPPFLAGS="-I${PREFIX}/MSToolkit/include"
 
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
+sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
+
 make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -mcmodel=large -fPIC -I./include" CFLAGS="${CFLAGS} -static -mcmodel=large -fPIC -I./include -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DGCC -DHAVE_EXPAT_CONFIG_H"
 
 mkdir -p "$PREFIX"/bin
