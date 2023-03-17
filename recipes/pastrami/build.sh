5c616c469e69eedb86099343d5e9c8c416abac7b
sgupta1524 <sgupta@asrtinc.com>
2023-03-17 14:04:13 -0400

    fix: addressing PR comments

--- recipes/pastrami/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#chmod +x $SRC_DIR/pastrami.py
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
-$PYTHON -m pip install --no-deps .  
-PIP_NO_CACHE_DIR=True
-$PYTHON -m pip install --no-deps .
