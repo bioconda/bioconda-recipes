7daa71b378fc1526e69e181ead0e206bde6a9a52
moold <mooldhu@gmail.com>
2023-03-17 18:31:11 +0800

    Fix some dependency issues

--- recipes/nextdenovo/build.sh
+++ recipes/nextdenovo/build.sh
@@ -4,7 +4,7 @@ set -x
 PYVER=`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
 OUTDIR=${PREFIX}/lib/python${PYVER}/site-packages/nextdenovo
 
-make CC=${CC} INCLUDES="-I$PREFIX/include" 
+make CC=${CC} INCLUDES="-I$PREFIX/include -lcurl -lssl -lcrypto"
 
 mkdir -p ${PREFIX}/bin ${OUTDIR}
 cp -r -f ./bin/ ./lib/ ./nextDenovo ./test_data VERSION ${OUTDIR}
