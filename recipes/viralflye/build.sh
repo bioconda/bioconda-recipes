2b8a54c715226fd0e557e362469a4d36b68cd7d9
Tom Harrop <twharrop@gmail.com>
2023-03-21 08:33:41 +1100

    no version tag for viralcomplete

--- recipes/viralflye/build.sh
+++ recipes/viralflye/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-cp -r viralflye $SP_DIR/
-cp viralFlye.py $PREFIX/bin
+# install viralcomplete from source
+(
+	git clone \
+		https://github.com/ablab/viralComplete \
+		$SRC_DIR/viralComplete
+	cd $SRC_DIR/viralComplete || exit 1
+	git checkout -f db9ffa7
+	mv bin/viralcomplete $PREFIX/bin/
+	mv blast_db $PREFIX/
+	mv data $PREFIX/
+)
+
+# install viralflye
+mv viralflye $SP_DIR/
+mv viralFlye.py $PREFIX/bin/
