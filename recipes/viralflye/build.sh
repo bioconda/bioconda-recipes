3d35f5ab9daec46b1df64d70041c2183e64b1188
Tom Harrop <twharrop@gmail.com>
2023-03-20 16:15:44 +1100

    tidy modules

--- recipes/viralflye/build.sh
+++ recipes/viralflye/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # change path so viralflye can find itself
-sed -i '/#entry point/a print(os.getcwd())' viralFlye.py
-sed -i '/#entry point/a os.chdir(viralflye_root)' viralFlye.py
+# sed -i '/#entry point/a print(os.getcwd())' viralFlye.py
+# sed -i '/#entry point/a os.chdir(viralflye_root)' viralFlye.py
 
-cp -r viralflye $PREFIX/opt/
-cp viralFlye.py $PREFIX/opt/
-ln -s $PREFIX/opt/viralFlye.py $PREFIX/bin/viralFlye.py
+cp -r viralflye $SP_DIR/
+cp viralFlye.py $PREFIX/bin
