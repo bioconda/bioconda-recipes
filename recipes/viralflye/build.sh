afab7cd865240201c1274b3ede3397b543fe4fd7
Tom Harrop <twharrop@gmail.com>
2023-03-20 16:23:49 +1100

    tidy test

--- recipes/viralflye/build.sh
+++ recipes/viralflye/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# change path so viralflye can find itself
-# sed -i '/#entry point/a print(os.getcwd())' viralFlye.py
-# sed -i '/#entry point/a os.chdir(viralflye_root)' viralFlye.py
-
 cp -r viralflye $SP_DIR/
 cp viralFlye.py $PREFIX/bin
