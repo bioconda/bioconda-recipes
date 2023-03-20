b5a3ab402578272a419a264a195bf0f596bfe5eb
Tom Harrop <twharrop@gmail.com>
2023-03-21 09:11:11 +1100

    specify error code in tests

--- recipes/viralflye/build.sh
+++ recipes/viralflye/build.sh
@@ -2,14 +2,14 @@
 
 # install viralcomplete from source
 (
-	git clone \
-		https://github.com/ablab/viralComplete \
-		$SRC_DIR/viralComplete
-	cd $SRC_DIR/viralComplete || exit 1
-	git checkout -f db9ffa7
-	mv bin/viralcomplete $PREFIX/bin/
-	mv blast_db $PREFIX/
-	mv data $PREFIX/
+    git clone \
+        https://github.com/ablab/viralComplete \
+        $SRC_DIR/viralComplete
+    cd $SRC_DIR/viralComplete || exit 1
+    git checkout -f db9ffa7
+    mv bin/viralcomplete $PREFIX/bin/
+    mv blast_db $PREFIX/
+    mv data $PREFIX/
 )
 
 # install viralflye
