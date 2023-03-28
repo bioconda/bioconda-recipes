423f6e153254d75daf29a6f45329650e79b82670
Luca Cozzuto <lcozzuto@C02WX1XFHV2Q.local>
2023-03-28 13:16:20 +0200

    adding recipes

--- recipes/netlogo/build.sh
+++ recipes/netlogo/build.sh
@@ -10,6 +10,8 @@ mkdir -p $NETLOGO_HOME
 
 cp -r * $NETLOGO_HOME/
 
+cp $RECIPE_DIR/netlogo-headless.sh $NETLOGO_HOME/
+
 chmod +x $NETLOGO_HOME/netlogo-headless.sh 
 
 ln -s $NETLOGO_HOME/netlogo-headless.sh  $PREFIX/bin/netlogo-headless.sh 
