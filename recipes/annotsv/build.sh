28aea87ddf9c054458f23ae2e43a47cb7ffdd1a5
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2023-03-30 18:18:50 +0200

    update recipe

--- recipes/annotsv/build.sh
+++ recipes/annotsv/build.sh
@@ -2,7 +2,5 @@
 set -euo pipefail
 
 make PREFIX="${PREFIX}" install
-make PREFIX="${PREFIX}" install-human-annotation
-make PREFIX="${PREFIX}" install-mouse-annotation
 
-export ANNOTSV=/path/to/install/AnnotSV
+export ANNOTSV=$(pwd)/bin/AnnotSV
