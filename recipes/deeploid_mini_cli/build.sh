3e09446fccd614b2d6bf6566daac7938b507977e
NikitaVenediktov <n.venediktov@gmail.com>
2023-03-30 17:52:18 +0300

    update

--- recipes/deeploid_mini_cli/build.sh
+++ recipes/deeploid_mini_cli/build.sh
@@ -5,7 +5,10 @@ echo "Building deeploid_mini_cli..."
 cp -r $SRC_DIR/* $PREFIX/
 
 cd $PREFIX/bin
-ln -s $PREFIX/deeploid_mini_cli.py ./deeploid_mini_cli
-chmod +x ./deeploid_mini_cli 
+ls $PREFIX
+ls $SRC_DIR
+
+ln -s $PREFIX/deeploid_mini_cli.py $SRC_DIR/deeploid_mini_cli
 cd
+chmod +x deeploid_mini_cli 
 $PYTHON deeploid_mini_cli -h
