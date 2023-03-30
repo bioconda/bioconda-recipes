c61777dc3db039de0f17875626e900bc2f647c35
NikitaVenediktov <n.venediktov@gmail.com>
2023-03-30 14:41:27 +0300

    add: build and meta for DMC

---
+++ recipes/deeploid_mini_cli/build.sh
@@ -0,0 +1,10 @@
+set -e
+
+echo "Building deeploid_mini_cli..."
+
+cp -r $SRC_DIR/* $PREFIX/
+
+mkdir $PREFIX/bin
+cd $PREFIX/bin
+ln -s  $PREFIX/deeploid_mini_cli.py ./deeploid_mini_cli
+chmod +x ./deeploid_mini_cli
