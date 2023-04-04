95e0c05c09978198e18b605965a877164640106f
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-03 21:35:58 -0500

    Update GMAP to 2023-03-24

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="-L${PREFIX}/lib"
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --prefix=${PREFIX} --with-simd-level=sse42
-make -j 2
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}"
+export CFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}"
+
+./configure --prefix=${PREFIX} CFLAGS="${CFLAGS}" --with-simd-level=sse42
+make CC=${CC} CFLAGS="${CFLAGS}" -j 2
 make install prefix=${PREFIX}
