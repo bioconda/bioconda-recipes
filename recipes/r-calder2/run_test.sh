c53a92afe36260fe2fdaf688c0715059d883a913
lucananni93 <luca.nanni@polimi.it>
2023-04-04 15:46:55 +0200

    added test about feature track

--- recipes/r-calder2/run_test.sh
+++ recipes/r-calder2/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/CSOgroup/CALDER2/main/tests/testthat/data/test.cool
+wget https://raw.githubusercontent.com/CSOgroup/CALDER2/main/tests/testthat/data/test_gene_coverage.bed
 
 calder --input test.cool --type cool --bin_size 50000 --genome hg38 --outpath out
+calder --input test.cool --type cool --bin_size 50000 --genome hg38 --outpath out_feat --feature_track test_gene_coverage.bed
