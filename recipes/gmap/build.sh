93eeda2f63655ddc4e15d3c86b6dfc126d078f9c
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-09 00:08:38 -0500

    add perl back as dependency

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 export INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
 
 autoheader
 autoconf
 ./configure --prefix=${PREFIX} --with-simd-level=sse42
-make CC=${CC} CFLAGS="-I${PREFIX}/include ${LDFLAGS}" -j 2
+make CC=${CC} CPPFLAGS="-I${PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include ${LDFLAGS}" -j 2
 make install
