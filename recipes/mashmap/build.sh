389c2854bfd0352a32e64f1e12a54f5292bfe56e
Bryce Kille <brycekille@gmail.com>
2023-04-09 18:57:19 -0500

    Create build.sh

---
+++ recipes/mashmap/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
+cmake -H. -B${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=1 -DOPTIMIZE_FOR_NATIVE=0
+cmake --build ${PREFIX}
