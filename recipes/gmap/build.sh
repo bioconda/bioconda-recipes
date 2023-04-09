bf9ca30e1f6278ff1b6b7380e891bb7b039dbe7f
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-09 01:01:52 -0500

    try building without modifying perl shebang

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -4,9 +4,6 @@ export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
 
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
-
 autoheader
 autoreconf
 ./configure --prefix=${PREFIX} --with-simd-level=avx2
