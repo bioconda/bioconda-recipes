946236960155ca3716242fb086a5670ab4559423
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-09 00:36:01 -0500

    fix autoconf perl shebang

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -4,8 +4,11 @@ export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
 
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+
 autoheader
-autoconf
-./configure --prefix=${PREFIX} --with-simd-level=sse42
+autoreconf
+./configure --prefix=${PREFIX} --with-simd-level=avx2
 make CC=${CC} CPPFLAGS="-I${PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include ${LDFLAGS}" -j 2
 make install
