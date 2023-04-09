a465943a2192596d87c9cf12f6941cab9b21eda0
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-09 01:46:27 -0500

    try building with perl as build requirement

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+patch -p1 < ${RECIPE_DIR}/single_quote_paths.patch
+
 export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
 
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 autoheader
-autoreconf
+autoconf
 ./configure --prefix=${PREFIX} --with-simd-level=avx2
-make CC=${CC} CPPFLAGS="-I${PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include ${LDFLAGS}" -j 2
+make CC=${CC} CPPFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 2
 make install
