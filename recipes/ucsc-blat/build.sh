bc0eee33f454b9df89b6f7e3e48ef65c9c3fbe60
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-10 18:37:32 -0500

    add BUILD_PREFIX

--- recipes/ucsc-blat/build.sh
+++ recipes/ucsc-blat/build.sh
@@ -5,9 +5,9 @@ export BINDIR=`pwd`/bin
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
-export CFLAGS="-I${PREFIX}/include ${LDFLAGS}"
-export CXXFLAGS="-I${PREFIX}/include ${LDFLAGS}"
-export L="${LDFLAGS}"
+export CFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}"
+export CXXFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}"
+export L="${LDFLAGS} -ldl"
 mkdir -p ${BINDIR}
 cd ${SRC_DIR}/kent/src/lib && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 cd ${SRC_DIR}/kent/src/htslib && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
