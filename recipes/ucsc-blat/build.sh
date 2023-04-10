22c89e55067a98e7e4292062c432c8e62c864ad5
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-09 22:58:23 -0500

    Update ucsc-blat to 445 using a workaround

--- recipes/ucsc-blat/build.sh
+++ recipes/ucsc-blat/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-mkdir -p "$PREFIX/bin"
+mkdir -p "${PREFIX}/bin"
 export MACHTYPE=x86_64
-export BINDIR=$(pwd)/bin
+export BINDIR=`pwd`/bin
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CFLAGS="-I${PREFIX}/include ${LDFLAGS}"
 export L="${LDFLAGS}"
-mkdir -p "$BINDIR"
-(cd kent/src/lib && make)
-(cd kent/src/htslib && make)
-(cd kent/src/jkOwnLib && make)
-(cd kent/src/hg/lib && make)
-(cd kent/src/blat && make)
-cp bin/blat "$PREFIX/bin"
-chmod +x "$PREFIX/bin/blat"
+mkdir -p ${BINDIR}
+(cd kent/src/lib && make CC=${CC} CFLAGS="${CFLAGS}")
+(cd kent/src/htslib && make CC=${CC} CFLAGS="${CFLAGS}")
+(cd kent/src/jkOwnLib && make CC=${CC} CFLAGS="${CFLAGS}")
+(cd kent/src/hg/lib && make CC=${CC} CFLAGS="${CFLAGS}")
+# (cd kent/src/blat && make CC=${CC} CFLAGS="${CFLAGS}")
+cp blat "${PREFIX}/bin"
+chmod +rx "${PREFIX}/bin/blat"
