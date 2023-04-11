c2d0266ff0d56e8c967bc669a422a7a4426ea5e2
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-10 19:15:02 -0500

    do not build /src/hg/lib

--- recipes/ucsc-blat/build.sh
+++ recipes/ucsc-blat/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export USE_HIC=0
 mkdir -p "${PREFIX}/bin"
 export MACHTYPE=x86_64
 export BINDIR=`pwd`/bin
@@ -12,7 +13,6 @@ mkdir -p ${BINDIR}
 cd ${SRC_DIR}/kent/src/lib && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 cd ${SRC_DIR}/kent/src/htslib && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 cd ${SRC_DIR}/kent/src/jkOwnLib && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
-cd ${SRC_DIR}/kent/src/hg/lib && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 cd ${SRC_DIR}/kent/src/blat && make CC=${CC} CXX=${CXX} CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 cp ${SRC_DIR}/bin/blat "${PREFIX}/bin"
 chmod +rx "${PREFIX}/bin/blat"
