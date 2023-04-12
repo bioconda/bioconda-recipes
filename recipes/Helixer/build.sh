398f83f98321e7fc5f5ccdeb1b5caf2d7aaaa5b6
rlibouban <romane.libouban@irisa.fr>
2023-04-12 09:33:09 +0200

    Draft helixer package recipe

--- recipes/Helixer/build.sh
+++ recipes/Helixer/build.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+!/bin/bash
 
-# Crate a data/out file to download the output files obtained with Helixer tool && add authority
-mkdir -p data/out
-chmod o+w data/out
+cd Helixer
+pip install .
+git clone https://github.com/TonyBolger/HelixerPost.git
+cd HelixerPost && cargo build --release
 
-# Build an image from a Dockerfile
-docker build -t helixer_v0.3.1 --rm .
-
-# Run image
-docker run -ti --name helixer_v0.3.1 helixer_v0.3.1:latest
-
-
+#cp helixer_post_bin ~/miniconda3/envs/helixer_environement/bin/helixer_post_bin #
+p=`pwd`
+export PATH="$p/helixer_post_bin:$PATH" #
+echo $PATH
