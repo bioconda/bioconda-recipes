a552f9bec3b1370ab197780a3a41e83cde5bf72a
Matthias Bernt <m.bernt@ufz.de>
2023-04-13 23:17:50 +0200

    mzmine: initial commit

---
+++ recipes/mzmine/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# create target directory
+MZMINEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $MZMINEDIR
+
+cp -r bin $MZMINEDIR/
+cp -r lib $MZMINEDIR/
+
+ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/MZmine
+ln -fs $MZMINEDIR/bin/mzmine $PREFIX/bin/MZmine
