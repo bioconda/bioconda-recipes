d5c2aa32badf1737dde3ad064e0b9d75c270bb58
Matthias Bernt <m.bernt@ufz.de>
2023-04-14 11:36:23 +0200

    just try to roll out the linux build

--- recipes/mzmine/build.sh
+++ recipes/mzmine/build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-./gradlew
+# create target directory
+MZMINEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $MZMINEDIR
 
-# # create target directory
-# MZMINEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-# mkdir -p $MZMINEDIR
+cp -r bin $MZMINEDIR/
+cp -r lib $MZMINEDIR/
 
-# cp -r bin $MZMINEDIR/
-# cp -r lib $MZMINEDIR/
-
-# ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/MZmine
-# ln -fs $MZMINEDIR/bin/mzmine $PREFIX/bin/MZmine
+ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/MZmine
+ln -fs $MZMINEDIR/bin/mzmine $PREFIX/bin/MZmine
