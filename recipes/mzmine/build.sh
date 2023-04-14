1736d3688cddf9797b2455e7347598b9e6675ae8
Matthias Bernt <m.bernt@ufz.de>
2023-04-14 10:32:58 +0200

    try to build from source

--- recipes/mzmine/build.sh
+++ recipes/mzmine/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# create target directory
-MZMINEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $MZMINEDIR
+./gradlew
 
-cp -r bin $MZMINEDIR/
-cp -r lib $MZMINEDIR/
+# # create target directory
+# MZMINEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+# mkdir -p $MZMINEDIR
 
-ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/MZmine
-ln -fs $MZMINEDIR/bin/mzmine $PREFIX/bin/MZmine
+# cp -r bin $MZMINEDIR/
+# cp -r lib $MZMINEDIR/
+
+# ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/MZmine
+# ln -fs $MZMINEDIR/bin/mzmine $PREFIX/bin/MZmine
