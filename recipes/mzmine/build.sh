c980979d7a3a1490f09efbf6cd9af67a82ebf960
Matthias Bernt <m.bernt@ufz.de>
2023-04-14 12:28:01 +0200

    quoting

--- recipes/mzmine/build.sh
+++ recipes/mzmine/build.sh
@@ -4,12 +4,12 @@ set -x
 
 # create target directory
 MZMINEDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $MZMINEDIR
+mkdir -p "$MZMINEDIR"
 
-cp -r bin $MZMINEDIR/
-cp -r lib $MZMINEDIR/
+cp -r bin "$MZMINEDIR"/
+cp -r lib "$MZMINEDIR"/
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX"/bin
 
-ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/MZmine
-ln -fs $MZMINEDIR/bin/MZmine $PREFIX/bin/mzmine
+ln -fs "$MZMINEDIR"/bin/MZmine "$PREFIX"/bin/MZmine
+ln -fs "$MZMINEDIR"/bin/MZmine "$PREFIX"/bin/mzmine
