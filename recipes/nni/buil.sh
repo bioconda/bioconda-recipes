5d1a1ba720a7aa5373106dcfc6373cbe358b8e7c
Rlibouban <127295521+rlibouba@users.noreply.github.com>
2023-04-17 11:02:30 +0200

    Delete buil.sh

--- recipes/nni/buil.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-
-git clone https://github.com/microsoft/nni.git
-cd nni
-pip install --upgrade setuptools pip wheel
-
-export NNI_RELEASE=2.0
-python setup.py build_ts
-python setup.py bdist_wheel
