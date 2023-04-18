5693b3857c20ac583deb09f54bd06aeb9f6d0185
Dr. K.D. Murray <foss@kdmurray.id.au>
2023-04-18 13:13:07 +0200

    Add conda preifx to cmake paths

--- recipes/diamond/build.sh
+++ recipes/diamond/build.sh
@@ -9,11 +9,12 @@ cd build
 cmake .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_LIBRARY_PATH="$CONDA_PREFIX;$PREFIX;" \
       -DBOOST_NO_SYSTEM_PATHS=on \
       -DWITH_ZSTD=on \
-      -DZSTD_LIBRARY=$PREFIX/lib/libzstd.a \
-      -DZSTD_INCLUDE_DIR=$PREFIX/include/ \
-      -DBLAST_LIB_DIR=$PREFIX/lib/ncbi-blast+ \
+      -DZSTD_LIBRARY=$CONDA_PREFIX/lib/libzstd.a \
+      -DZSTD_INCLUDE_DIR=$CONDA_PREFIX/include/ \
+      -DBLAST_LIB_DIR=$CONDA_PREFIX/lib/ncbi-blast+ \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=""
 
 cmake --build . --config Release --target install
