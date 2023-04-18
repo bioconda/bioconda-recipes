1576cf43c2b2c4eadd40cdd5be08dc8ff088d1c6
Dr. K.D. Murray <foss@kdmurray.id.au>
2023-04-18 11:47:37 +0200

    Force zstd path to conda dir

--- recipes/diamond/build.sh
+++ recipes/diamond/build.sh
@@ -11,6 +11,8 @@ cmake .. \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DBOOST_NO_SYSTEM_PATHS=on \
       -DWITH_ZSTD=on \
+      -DZSTD_LIBRARY=$PREFIX/lib/libzstd.so \
+      -DZSTD_INCLUDE_DIR=$PREFIX/include/ \
       -DBLAST_LIB_DIR=$PREFIX/lib/ncbi-blast+ \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=""
 
