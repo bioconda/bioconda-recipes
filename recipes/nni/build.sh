51e24e96d4f3dfefbc539ad077bfa07e29114400
rlibouban <romane.libouban@irisa.fr>
2023-04-26 09:19:53 +0200

    Test

--- recipes/nni/build.sh
+++ recipes/nni/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export NNI_RELEASE=2.0
-$PYTHON setup.py build_ts
-$PYTHON setup.py bdist_wheel
+NNI_RELEASE=2.0 $PYTHON setup.py build_ts
+NNI_RELEASE=2.0 $PYTHON setup.py bdist_wheel
