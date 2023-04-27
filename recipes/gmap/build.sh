ebf05179979edeefea58b39ebec50bf29c7cd6a1
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-26 23:28:13 -0500

    Update GMAP to 2023-04-20

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-patch -p1 < ${RECIPE_DIR}/single_quote_paths.patch
-
 export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
@@ -10,8 +8,9 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-autoheader
 autoconf
-./configure --prefix=${PREFIX} --with-simd-level=sse42
-make CC=${CC} CPPFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 2
+autoheader
+./configure CC=${CC} --prefix=${PREFIX} --with-simd-level=sse42
+make CC=${CC} CPPFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 4
 make install
+make clean
