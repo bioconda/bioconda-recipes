b7e343f6bb46aab12f584dcd767f76d85dc04ad4
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-04-27 02:17:13 -0500

    add LDFLAGS required for building on macOS

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -2,17 +2,13 @@
 
 export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-
-if test x"`uname`" = x"Linux"; then
-    LDFLAGS="${LDFLAGS} -Wl,-as-needed,${PREFIX}/lib"
-fi
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CC_FOR_BUILD=$CC
 
 if [ "$(uname)" == "Darwin" ]; then
     # for Mac OSX
-    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names -Wl,-rpath,${PREFIX}/lib"
+    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-else
-    export CPP_FOR_BUILD=${CPP}
 fi
 
 export LANGUAGE=en_US.UTF-8
@@ -22,6 +18,6 @@ export LC_ALL=en_US.UTF-8
 autoconf
 autoheader
 ./configure CC=${CC} --prefix=${PREFIX} --with-simd-level=sse42
-make CC=${CC} CXX=${CXX} CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include ${LDFLAGS}" CFLAGS="${CFLAGS} -I${PREFIX}/include ${LDFLAGS}" -j 4
+make CC=${CC} CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="${CFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 4
 make install
 make clean
