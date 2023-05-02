f2b657f6002195e801901b17ede152d812be8bd9
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-02 14:47:14 -0500

    Update GMAP to 2023-04-28

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-patch -p1 < ${RECIPE_DIR}/single_quote_paths.patch
-
 export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CC_FOR_BUILD=$CC
+
+if [ "$(uname)" == "Darwin" ]; then
+    # for Mac OSX
+    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
 
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-autoheader
 autoconf
-./configure --prefix=${PREFIX} --with-simd-level=sse42
-make CC=${CC} CPPFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="-I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 2
+autoheader
+./configure CC=${CC} --prefix=${PREFIX} --with-simd-level=sse42
+make CC=${CC} CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="${CFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 4
 make install
+make clean
