f0db28cddc4c5cf7cf248467c83fc4368d02ac7a
Michael Milton <michael.r.milton@gmail.com>
2023-05-03 14:25:32 +1000

    -DCMAKE_CXX_COMPILER="${CXX}"

--- recipes/expansionhunter/build.sh
+++ recipes/expansionhunter/build.sh
@@ -11,7 +11,7 @@ export CXX_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-cmake ..  --trace-source CMakeLists.txt
+cmake ..  --trace-source CMakeLists.txt -DCMAKE_CXX_COMPILER="${CXX}"
 make CXX="$CXX" CC="$CC" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
 
 mv install/bin/ExpansionHunter ${PREFIX}/bin/ExpansionHunter
