15d111c0e80562a47180c894edcd85261dfdc0ca
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-05 16:54:12 -0500

    omit CC_FOR_BUILD

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -2,8 +2,9 @@
 
 export INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CC_FOR_BUILD=$CC
+export LDFLAGS="$-L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include"
 
 if [ "$(uname)" == "Darwin" ]; then
     # for Mac OSX
@@ -15,9 +16,7 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-autoconf
-autoheader
-./configure CC=${CC} --prefix=${PREFIX} --with-simd-level=sse42
-make CC=${CC} CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" CFLAGS="${CFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include ${LDFLAGS}" -j 4
+./configure CC="${CC}" --prefix="${PREFIX}" --with-simd-level=sse42
+make CC="${CC}" CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" -j 4
 make install
 make clean
