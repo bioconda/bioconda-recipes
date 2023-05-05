84bf0f0ad005e5195b45a2209814e4adff0b0d00
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-05 17:17:50 -0500

    add C_INCLUDE_PATH

--- recipes/gmap/build.sh
+++ recipes/gmap/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export INCLUDE_PATH=${PREFIX}/include
+export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="$-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I${BUILD_PREFIX}/include"
 
@@ -16,7 +16,9 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-./configure CC="${CC}" --prefix="${PREFIX}" --with-simd-level=sse42
-make CC="${CC}" CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" -j 4
+autoconf
+autoheader
+./configure CC=${CC} --prefix="${PREFIX}" --with-simd-level=sse42
+make CC=${CC} CFLAGS="${CFLAGS} ${LDFLAGS}" CPPFLAGS="${CPPFLAGS} ${LDFLAGS}" -j 4
 make install
 make clean
