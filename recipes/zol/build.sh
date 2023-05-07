bd83685bdc5c88b5e213d81088737c5c5a11c0a2
raufs <salamzader@gmail.com>
2023-05-07 23:55:34 +0000

    add zol to bioconda

---
+++ recipes/zol/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}/bin
+
+# (re)-compile RBH/InParanoid-esque programs written in C++
+g++ -std=c++11 -o zol/orthologs/runRBH zol/orthologs/runRBH.cpp
+g++ -std=c++11 -o zol/orthologs/splitDiamondResults zol/orthologs/splitDiamondResults.cpp
+g++ -std=c++11 -o zol/splitDiamondResultsForFai zol/splitDiamondResultsForFai.cpp
+
+cp zol/orthologs/runRBH ${PREFIX}/bin/
+cp zol/orthologs/splitDiamondResults ${PREFIX}/bin/
+cp zol/splitDiamondResultsForFai ${PREFIX}/bin/
+cp zol/clusterHeatmap.R ${PREFIX}/bin/
+cp zol/plotSegments.R ${PREFIX}/bin/
+
+chmod +x ${PREFIX}/bin/runRBH
+chmod +x ${PREFIX}/bin/splitDiamondResults
+chmod +x ${PREFIX}/bin/splitDiamondResultsForFai
+
+# create folder for database download
+ZOL_DATA_PATH=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/db/
+ZOL_EXEC_PATH=${PREFIX}/bin/
+mkdir -p ${ZOL_DATA_PATH}
+
+# set VIBRANT DB PATH variable on env activation
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat <<EOF >> ${PREFIX}/etc/conda/activate.d/zol.sh
+export ZOL_DATA_PATH=${ZOL_DATA_PATH}
+export ZOL_EXEC_PATH=${ZOL_EXEC_PATH}
+EOF
+
+cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/zol.sh
+unset ZOL_DATA_PATH
+unset ZOL_EXEC_PATH
+EOF
