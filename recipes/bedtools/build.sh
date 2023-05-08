1c71adef6878d004b54565386f0f434d70e40008
John Marshall <jmarshall@hey.com>
2023-05-09 09:54:55 +1200

    Work around conda/conda-build#4869 (._bedtools2 file)
    Do this conditionally, because bedtools-2.31.0.tar.gz variously
    containing .DS_Store and ._bedtools2 is an anomaly -- future release
    tarballs will likely revert to being cleanly hoistable.

--- recipes/bedtools/build.sh
+++ recipes/bedtools/build.sh
@@ -2,4 +2,5 @@
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export C_INCLUDE_PATH=${PREFIX}/include
 
+if test -d bedtools2; then echo Hoisting prevented by .*; cd bedtools2; fi
 make install prefix=$PREFIX CXX=$CXX CC=$CC LDFLAGS="-L$PREFIX/lib"
