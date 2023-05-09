8c04dfec98d0e92bd3823a278c38c37d563d032e
Charlotte Capitanchik <charlotte.capitanchik@crick.ac.uk>
2023-05-10 08:52:26 +0900

    add java dependencies

--- recipes/umicollapse/build.sh
+++ recipes/umicollapse/build.sh
@@ -7,4 +7,8 @@ mkdir -p $PREFIX/bin
 cp -R * $outdir/
 cp $RECIPE_DIR/umicollapse.py $outdir/umicollapse
 ln -s $outdir/umicollapse $PREFIX/bin
+curl -O -L https://repo1.maven.org/maven2/com/github/samtools/htsjdk/2.19.0/htsjdk-2.19.0.jar
+curl -O -L https://repo1.maven.org/maven2/org/xerial/snappy/snappy-java/1.1.7.3/snappy-java-1.1.7.3.jar
+mv htsjdk-2.19.0.jar $PREFIX/bin
+mv snappy-java-1.1.7.3.jar $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/umicollapse"
