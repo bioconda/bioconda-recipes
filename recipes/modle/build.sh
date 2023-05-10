0d391adf469d6e44be9698c42703b2a20828a1e0
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-05-10 17:42:39 +0200

    Update build.sh

--- recipes/modle/build.sh
+++ recipes/modle/build.sh
@@ -44,21 +44,20 @@ conan install conanfile.txt \
        -s compiler.cppstd=17 \
        --output-folder=build/
 
+# Add bioconda suffix to MoDLE version
+sed -i.bak 's/set(MODLE_PROJECT_VERSION_SUFFIX "")/set(MODLE_PROJECT_VERSION_SUFFIX "bioconda")/' cmake/Versioning.cmake
+
 # https://docs.conda.io/projects/conda-build/en/stable/user-guide/environment-variables.html#environment-variables-set-during-the-build-process
-cmake -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
-      -DCMAKE_PREFIX_PATH="$PWD/build"       \
-      -DENABLE_DEVELOPER_MODE=OFF            \
-      -DMODLE_ENABLE_TESTING=ON              \
-      -DGIT_RETRIEVED_STATE=true             \
-      -DGIT_TAG="v${PKG_VERSION#v}"          \
-      -DGIT_IS_DIRTY=false                   \
-      -DGIT_HEAD_SHA1="$PKG_HASH"            \
-      -DGIT_DESCRIBE="$PKG_BUILD_STRING"     \
-      -DCMAKE_INSTALL_PREFIX="$PREFIX"       \
-      -DCMAKE_C_COMPILER="$CC"               \
-      -DCMAKE_CXX_COMPILER="$CXX"            \
-      "${CMAKE_PLATFORM_FLAGS[@]}"           \
-      -B build/                              \
+cmake -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"  \
+      -DCMAKE_PREFIX_PATH="$PWD/build"        \
+      -DENABLE_DEVELOPER_MODE=OFF             \
+      -DMODLE_ENABLE_TESTING=ON               \
+      -DMODLE_ENABLE_GIT_VERSION_TRACKING=OFF \
+      -DCMAKE_INSTALL_PREFIX="$PREFIX"        \
+      -DCMAKE_C_COMPILER="$CC"                \
+      -DCMAKE_CXX_COMPILER="$CXX"             \
+      "${CMAKE_PLATFORM_FLAGS[@]}"            \
+      -B build/                               \
       -S .
 
 cmake --build build/
