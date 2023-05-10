5d265c84ca6bacb4a371e201a19477f352b3165a
Charlotte Capitanchik <charlotte.capitanchik@crick.ac.uk>
2023-05-10 15:08:45 +0900

    update build

--- recipes/umicollapse/build.sh
+++ recipes/umicollapse/build.sh
@@ -9,7 +9,8 @@ cp $RECIPE_DIR/umicollapse.py $outdir/umicollapse
 ln -s $outdir/umicollapse $PREFIX/bin
 curl -O -L https://repo1.maven.org/maven2/com/github/samtools/htsjdk/2.19.0/htsjdk-2.19.0.jar
 curl -O -L https://repo1.maven.org/maven2/org/xerial/snappy/snappy-java/1.1.7.3/snappy-java-1.1.7.3.jar
-mkdir -p $PREFIX/bin/lib
-mv htsjdk-2.19.0.jar $PREFIX/bin/lib
-mv snappy-java-1.1.7.3.jar $PREFIX/bin/lib
-chmod 0755 "${PREFIX}/bin/umicollapse"
+mkdir -p $PREFIX/lib
+mv htsjdk-2.19.0.jar $PREFIX/lib
+mv snappy-java-1.1.7.3.jar $PREFIX/lib
+chmod 0755 "${PREFIX}/bin"
+chmod 0755 "${PREFIX}/lib"
