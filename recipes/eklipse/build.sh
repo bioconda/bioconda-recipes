4173e1c6e58048b995c25c3cf3c5f85da5a08898
lucia.pena.perez@scilifelab.se <lucia.penaperez@hasta-login.local>
2023-05-10 09:04:50 +0200

    fix copy

--- recipes/eklipse/build.sh
+++ recipes/eklipse/build.sh
@@ -3,7 +3,13 @@
 curl -SL https://github.com/dooguypapua/eKLIPse/archive/refs/heads/master.zip -o eklipse.zip
 
 unzip -d eklipse eklipse.zip
-cd eklipse
-chmod a+x *
+chmod a+x eklipse/*
 mkdir -p "${PREFIX}/bin"
-cp eKLIPse_circos.py eKLIPse_fct.py eKLIPse_init.py eKLIPse_sc.py eKLIPse_threading.py pybam.py spinner.py tabulate.py "${PREFIX}/bin/."
+cp eklipse/eKLIPse_circos.py "${PREFIX}/bin/."
+cp eklipse/eKLIPse_fct.py "${PREFIX}/bin/."
+cp eklipse/eKLIPse_init.py "${PREFIX}/bin/."
+cp eklipse/eKLIPse_sc.py "${PREFIX}/bin/."
+cp eklipse/eKLIPse_threading.py "${PREFIX}/bin/."
+cp eklipse/pybam.py "${PREFIX}/bin/."
+cp eklipse/spinner.py "${PREFIX}/bin/."
+cp eklipse/tabulate.py "${PREFIX}/bin/."
