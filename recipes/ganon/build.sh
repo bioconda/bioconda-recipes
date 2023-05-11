3bf7849ed6e37bb9611bf0d2a012010629be93cb
Vitor C. Piro <pirovc@posteo.net>
2023-05-11 08:58:53 +0200

    back to default

--- recipes/ganon/build.sh
+++ recipes/ganon/build.sh
@@ -17,13 +17,5 @@ ctest -VV .
 # Test python
 cd ${SRC_DIR}/ganon/
 
-genome_updater.sh -Z
-curl --version
-
-$PYTHON -m unittest discover -s tests/ganon/integration/ -v || true
+$PYTHON -m unittest discover -s tests/ganon/integration/ -v 
 #$PYTHON -m unittest discover -s tests/ganon/integration_online/ -v
-
-cat tests/ganon/results/integration/build/test_og_arc_bac_vir.log
-cat tests/ganon/results/integration/build/test_taxid.log
-cat tests/ganon/results/integration/update/test_db_prefix.log
-cat tests/ganon/results/integration/update/test_output_db_prefix.log
