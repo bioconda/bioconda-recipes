bfe34976e16419446d1f7f9ab367152dd8e57a0e
lucia.pena.perez@scilifelab.se <lucia.penaperez@hasta-login.local>
2023-05-16 16:24:29 +0200

    fix recipe

--- recipes/eklipse/build.sh
+++ recipes/eklipse/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 curl -SL https://github.com/dooguypapua/eKLIPse/archive/refs/heads/master.zip -o eklipse.zip
-
 unzip -d eklipse eklipse.zip
-cd eklipse
+cd eklipse/eKLIPse-master
+chmod +x eKLIPse.py
+sed -i '' -e '1s|.*|#!/usr/bin/env python2\n&|' eKLIPse.py
 mkdir -p ${PREFIX}/bin
-cp -r eKLIPse-master/*.py ${PREFIX}/bin/
-chmod +x ${PREFIX}/bin/eKLIPse.py
+cp -r data/* doc/* *py  ${PREFIX}/bin/
