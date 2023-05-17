e5cdc6f3163d05f7029c56d761e8ea9d1ffb8848
Devon Ryan <dpryan79@users.noreply.github.com>
2023-05-17 16:22:16 +0200

    Update build.sh

--- recipes/eklipse/build.sh
+++ recipes/eklipse/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-curl -SL https://github.com/dooguypapua/eKLIPse/archive/refs/heads/master.zip -o eklipse.zip
-unzip -d eklipse eklipse.zip
-cd eklipse/eKLIPse-master
+ls -l
 chmod +x eKLIPse.py
 sed -i '1s|.*|#!/usr/bin/env python2\n&|' eKLIPse.py
 mkdir -p ${PREFIX}/bin
