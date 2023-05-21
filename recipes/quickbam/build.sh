38f4ed4468d37a6ef13f3689c76ec4eef862c5b6
Devon Ryan <dpryan79@gmail.com>
2023-05-21 15:10:24 +0200

    Most headers are there.

--- recipes/quickbam/build.sh
+++ recipes/quickbam/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 mv ${RECIPE_DIR}/instrument.h src/quickbam/
-#cp -r src/quickbam $PREFIX/include
 ./configure --prefix=$PREFIX
 make
 make install
-ls -l $PREFIX/include
+cp -r src/quickbam/instrument.h $PREFIX/include/quickbam/
+ls -l $PREFIX/include/quickbam/
