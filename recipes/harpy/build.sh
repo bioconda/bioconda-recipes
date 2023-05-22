c24f35651e9c0d2705ee4a73e9b298a5505f4add
Pavel V. Dimens <pdimens@live.com>
2023-05-22 10:57:18 -0400

    just move the prebuilt binary instead of building from scratch

--- recipes/harpy/build.sh
+++ recipes/harpy/build.sh
@@ -1,25 +1,27 @@
 #! /usr/bin/env bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+#export C_INCLUDE_PATH=${PREFIX}/include
+#export CPLUS_INCLUDE_PATH=${PREFIX}/include
+#export CPP_INCLUDE_PATH=${PREFIX}/include
+#export CXX_INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
 
 # create conda PATH, if not already existing
 mkdir -p $CONDA_PREFIX/bin
 
 # build and install ema
-rm -rf ema || true
-git clone --recursive https://github.com/EdHarry/ema.git
-cd ema && git checkout haplotag
-git submodule update --remote
-git apply ../harpy/misc/makefile.patch
-git apply ../harpy/misc/remove_native.patch
-make CC="$CC -fcommon" CXX="$CXX -fcommon" LDFLAGS="$LDFLAGS"
-chmod +x ema
-cp ema $CONDA_PREFIX/bin/ema-h
-cd ..
+#rm -rf ema || true
+#git clone --recursive https://github.com/EdHarry/ema.git
+#cd ema && git checkout haplotag
+#git submodule update --remote
+#git apply ../harpy/misc/makefile.patch
+#git apply ../harpy/misc/remove_native.patch
+#make CC="$CC -fcommon" CXX="$CXX -fcommon" LDFLAGS="$LDFLAGS"
+#chmod +x ema
+#cp ema $CONDA_PREFIX/bin/ema-h
+#cd ..
+chmod +x misc/ema-h && cp misc/ema-h $CONDA_PREFIX/bin
+
 
 # Harpy executable
 chmod +x harpy
