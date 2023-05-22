754542205c9cef4637e5a7587bb0083e4d89f09b
Pavel V. Dimens <pdimens@live.com>
2023-05-22 13:31:38 -0400

    try this config

--- recipes/harpy/build.sh
+++ recipes/harpy/build.sh
@@ -1,19 +1,20 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
-mkdir -p $PREFIX/bin
-cp misc/ema-h $PREFIX/bin
+mkdir -p ${PREFIX}/bin
+
+cp misc/ema-h ${PREFIX}/bin
 
 # Harpy executable
-cp harpy $PREFIX/bin/
+cp harpy ${PREFIX}/bin/
 
 # rules
-cp rules/*.smk $PREFIX/bin/
+cp rules/*.smk ${PREFIX}/bin/
 
 # associated scripts
-cp utilities/*.{py,R,pl} $PREFIX/bin/
+cp utilities/*.{py,R,pl} ${PREFIX}/bin/
 
 # reports
-cp reports/*.Rmd $PREFIX/bin/
+cp reports/*.Rmd ${PREFIX}/bin/
 
 
 ### deprecated? ###
