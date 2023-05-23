0e346b58773399eb28c1bb588556c65a3119aecd
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-05-23 15:33:09 +0200

    Bugfix

--- recipes/modle/build.sh
+++ recipes/modle/build.sh
@@ -40,10 +40,9 @@ fi
 
 # Remember to update these profiles when bioconda's compiler toolchains are updated
 mkdir -p "$CONAN_HOME/profiles/"
-sed -i.bak \
-  "s/__CONDA_BUILD_SYSROOT__/${CONDA_BUILD_SYSROOT}/" \
-  "${RECIPE_DIR}/conan_profiles/$conan_profile" \
-  "$CONAN_HOME/profiles/$conan_profile"
+sed "s/__CONDA_BUILD_SYSROOT__/${CONDA_BUILD_SYSROOT}/" \
+    "${RECIPE_DIR}/conan_profiles/$conan_profile" \
+    "$CONAN_HOME/profiles/$conan_profile"
 
 # Build everything from source to avoid ABI issues due to old GLIBC/GLIBCXX
 conan install conanfile.txt \
