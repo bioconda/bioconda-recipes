4ec2781491702b05b57a565f831901f72d594681
Devon Ryan <dpryan79@users.noreply.github.com>
2023-05-23 13:24:01 +0200

    Update build.sh

--- recipes/rgccacmd/build.sh
+++ recipes/rgccacmd/build.sh
@@ -2,4 +2,4 @@
 cp -r ${SRC_DIR}/R ${PREFIX}
 #mkdir -p ${PREFIX}/inst
 #wget -O ${PREFIX}/inst/launcher.R https://raw.githubusercontent.com/BrainAndSpineInstitute/rgcca_ui/master/inst/launcher.R
-$R CMD INSTALL --build .
+$R CMD INSTALL .
