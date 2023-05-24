da40cbbc72b19067e10754d94cf66a35606b0fdc
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-24 01:03:04 -0500

    Add recipe for NRPyS v0.1.1

---
+++ recipes/nrpys/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -euo
+
+# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
+# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
+export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
+
+export HOME=${PREFIX}
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+mkdir ${PREFIX}/bin
+
+# build statically linked binary with Rust
+maturin build --release -f
+${PYTHON} pip install -e . -vv
+cp ${SRC_DIR}/python/nrpys/nrpys.*.so ${PREFIX}/lib/libnrpys.so
