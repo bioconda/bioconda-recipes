917da8c06693a3c9f7dabd7be1407aaaceb8a83b
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-24 17:57:53 -0500

    Add recipe for NNI v2.10.1

---
+++ recipes/nni/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+export NNI_RELEASE=${PKG_VERSION}
+${PYTHON} setup.py build_ts
+${PYTHON} setup.py bdist_wheel
