af9d0f4bad6e549dbe5d513428eb35f447418f58
Tommi Mäklin <tommi@maklin.fi>
2023-05-24 16:59:31 +0100

    Pass C linker to rust via $RUSTFLAGS.

--- recipes/themisto/build.sh
+++ recipes/themisto/build.sh
@@ -5,6 +5,8 @@ export CXXFLAGS="$CXXFLAGS -std=c++20 -no-pie -lrt"
 
 mkdir -p $PREFIX/bin
 
+export RUSTFLAGS="-C linker=$CC"
+
 export RUSTUP_HOME="$HOME/rustup"
 export CARGO_HOME="$HOME/cargo"
 wget https://sh.rustup.rs -O rustup.sh
