f8e042bf2c3f6830d4622201f043274d29807b51
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-24 22:21:00 -0500

    remove --prefix from pip install

--- recipes/nni/build.sh
+++ recipes/nni/build.sh
@@ -7,4 +7,4 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export NNI_RELEASE=${PKG_VERSION}
 ${PYTHON} setup.py build_ts
 ${PYTHON} setup.py bdist_wheel
-pip install -e . --user --prefix ${PREFIX} --use-pep517 -vv
+pip install -e . --user --use-pep517 -vv
