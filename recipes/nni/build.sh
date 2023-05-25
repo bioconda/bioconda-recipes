3c2775ae2619aaeeec68aaec2a255c5fcbc62fd9
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-24 23:17:35 -0500

    change pip install command... again

--- recipes/nni/build.sh
+++ recipes/nni/build.sh
@@ -7,4 +7,4 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export NNI_RELEASE=${PKG_VERSION}
 ${PYTHON} setup.py build_ts
 ${PYTHON} setup.py bdist_wheel
-pip install -e . --user --use-pep517 -vv
+pip install --use-pep517 --prefix ${PREFIX} -vv -e .
