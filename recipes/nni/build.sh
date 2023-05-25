b100fa51c057e0e767ae4ffd9619c90bb6441c00
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-25 16:16:50 -0500

    delete unecessary post-link.sh script

--- recipes/nni/build.sh
+++ recipes/nni/build.sh
@@ -4,7 +4,9 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+export PATH="${PATH}:${PREFIX}/.local/bin"
+
 export NNI_RELEASE=${PKG_VERSION}
 ${PYTHON} setup.py build_ts
 ${PYTHON} setup.py bdist_wheel
-pip install --user -vv -e .
+pip install -vv -e .
--- recipes/nni/post-link.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export PATH="${PATH}:${HOME}/.local/bin"
