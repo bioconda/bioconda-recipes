24d2c746273344348e5f019501d20382b7eef80e
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-05-25 19:48:21 -0500

    remove typeguard requirement completely

--- recipes/nni/build.sh
+++ recipes/nni/build.sh
@@ -4,6 +4,4 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-export PATH="${PATH}:${PREFIX}/.local/bin"
-
 ${PYTHON} setup.py develop
