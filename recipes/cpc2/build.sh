826ec38c5144d8d9eb578d81285881afc9904a05
Devon Ryan <dpryan79@users.noreply.github.com>
2023-05-28 14:43:03 +0200

    Update build.sh

--- recipes/cpc2/build.sh
+++ recipes/cpc2/build.sh
@@ -9,11 +9,11 @@ mkdir -p ${PREFIX}/data
 chmod a+tx data/*
 cp data/* ${PREFIX}/data/
 
-mkdir -p ${PREFIX}/libs/libsvm
-chmod a+x libs/libsvm/*
-cp libs/libsvm/* ${PREFIX}/libs/libsvm
+#mkdir -p ${PREFIX}/libs/libsvm
+#chmod a+x libs/libsvm/*
+#cp libs/libsvm/* ${PREFIX}/libs/libsvm
 
-cd ${PREFIX}/libs/libsvm
-gzip -dc libsvm-3.18.tar.gz | tar xf -
-cd libsvm-3.18
-make clean && make
+#cd ${PREFIX}/libs/libsvm
+#gzip -dc libsvm-3.18.tar.gz | tar xf -
+#cd libsvm-3.18
+#make clean && make
