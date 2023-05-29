3a8d0e61eff20fdcad691b9539a53abdbd8b73cf
John Marshall <john.marshall@populationgenomics.org.au>
2023-05-29 16:56:06 +1200

    no tests for now

--- recipes/gatk4/build_gcnvkernel.sh
+++ recipes/gatk4/build_gcnvkernel.sh
@@ -2,9 +2,3 @@
 
 unzip -q gatkPythonPackageArchive.zip '*gcnvkernel*'
 $PYTHON setup_gcnvkernel.py install --single-version-externally-managed --record=record_gcnvkernel.txt
-
-echo LIST FOOBAR
-conda list || true
-echo GREP FOOBAR
-(cd $CONDA_PREFIX/conda-meta && grep libtiff *) || true
-echo DONE FOOBAR
