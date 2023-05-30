2c1beac1d904c0300ea03df4ebc1e990302fafd1
peterpru <peter.pruisscher@scilifelab.se>
2023-05-30 14:20:16 +0200

    replace license and add to build.sh

--- recipes/perl-retroseq/build.sh
+++ recipes/perl-retroseq/build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-export PERL5LIB="${CONDA_PREFIX}/RetroSeq/
+
+# Move the Perl modules to the appropriate 
+export PERL5LIB=/$PREFIX/bin/:$PERL5LIB
+cp ./RetroSeq/Vcf.pm $PREFIX/bin/
+cp ./RetroSeq/Utilities.pm $PREFIX/bin/
+cp ./bin/retroseq.pl $PREFIX/bin/retroseq.pl
+
+# Set executable permissions for the code.pl file
+chmod +x $PREFIX/bin/retroseq.pl
