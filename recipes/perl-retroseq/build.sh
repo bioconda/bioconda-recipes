747f1955edb49dca616a206c39721f5be6c8ae06
peterpru <peter.pruisscher@scilifelab.se>
2023-05-31 09:45:50 +0200

    fix build errors

--- recipes/perl-retroseq/build.sh
+++ recipes/perl-retroseq/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+set -x -e
 
-# Move the Perl modules to the appropriate 
-export PERL5LIB=/$PREFIX/bin/:$PERL5LIB
-cp ./RetroSeq/Vcf.pm $PREFIX/bin/
-cp ./RetroSeq/Utilities.pm $PREFIX/bin/
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+
+cp -r ./RetroSeq $PREFIX/lib
 cp ./bin/retroseq.pl $PREFIX/bin/retroseq.pl
 
-# Set executable permissions for the code.pl file
-chmod +x $PREFIX/bin/retroseq.pl
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+
+echo "export PERL5LIB=$PERL5LIB:$PREFIX/lib" > ${PREFIX}/etc/conda/activate.d/retroseq-perl5lib.sh
+chmod a+x ${PREFIX}/etc/conda/activate.d/retroseq-perl5lib.sh
+
+#echo "unset PERL5LIB" > ${PREFIX}/etc/conda/deactivate.d/retroseq-perl5lib.sh
+#chmod a+x ${PREFIX}/etc/conda/deactivate.d/retroseq-perl5lib.sh
