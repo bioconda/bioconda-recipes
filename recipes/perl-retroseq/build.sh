cd7649809207a6c4d2ac50451fe8824c8e72d408
peterpru <peter.pruisscher@scilifelab.se>
2023-05-31 11:19:21 +0200

    add unset to build

--- recipes/perl-retroseq/build.sh
+++ recipes/perl-retroseq/build.sh
@@ -12,5 +12,5 @@ mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
 echo "export PERL5LIB=$PERL5LIB:$PREFIX/lib" > ${PREFIX}/etc/conda/activate.d/retroseq-perl5lib.sh
 chmod a+x ${PREFIX}/etc/conda/activate.d/retroseq-perl5lib.sh
 
-#echo "unset PERL5LIB" > ${PREFIX}/etc/conda/deactivate.d/retroseq-perl5lib.sh
-#chmod a+x ${PREFIX}/etc/conda/deactivate.d/retroseq-perl5lib.sh
+echo "unset PERL5LIB" > ${PREFIX}/etc/conda/deactivate.d/retroseq-perl5lib.sh
+chmod a+x ${PREFIX}/etc/conda/deactivate.d/retroseq-perl5lib.sh
