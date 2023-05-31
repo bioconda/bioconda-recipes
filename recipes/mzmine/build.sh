101707aacc4b3340c932cade7ce3f442b7e46bb6
Matthias Bernt <m.bernt@ufz.de>
2023-05-31 11:36:05 +0200

    switch to building again

--- recipes/mzmine/build.sh
+++ recipes/mzmine/build.sh
@@ -2,9 +2,14 @@
 
 set -ex
 
-mkdir -p "$PREFIX/"
-cp -rv bin "$PREFIX"
-cp -rv lib  "$PREFIX"
+# TODO with "runtime" the software is only built
+# if removed also tests are executed, some of them
+# fail unfortunately
+./gradlew runtime
+
+
+mkdir -p "$PREFIX"
+cp -rv build/install/MZmine/* "$PREFIX"
 
 # for those the don't like capital letters in commands
 ln -fs "$PREFIX"/bin/MZmine "$PREFIX"/bin/mzmine
