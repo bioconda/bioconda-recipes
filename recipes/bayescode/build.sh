4c4326b4146e329cbd9f68391d89e0e3dc382c5e
Thibault Latrille <thibault.latrille@ens-lyon.org>
2023-06-01 21:00:59 +0200

    Remove python dependencies

--- recipes/bayescode/build.sh
+++ recipes/bayescode/build.sh
@@ -3,8 +3,8 @@
 # Create bin directory
 mkdir -p "${PREFIX}"/bin/
 
-# Build PSASS specifying CPP compiler and C compiler
-make CXX="${CXX}" CC="${CC}" tiny
+# Build specifying CPP compiler and C compiler
+make CXX="${CXX}" tiny
 
 # Copy all generated binaries to bin
 cp bin/*mutselomega "${PREFIX}"/bin/
