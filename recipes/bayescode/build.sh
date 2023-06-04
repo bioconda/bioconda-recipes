be6f3e4c5588b3924df3d23472ddae4d4c287b05
Thibault Latrille <thibault.latrille@ens-lyon.org>
2023-06-04 16:29:01 +0200

    Update bayescode to v1.2.1

--- recipes/bayescode/build.sh
+++ recipes/bayescode/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+rm -rf data/
 # Create bin directory
 mkdir -p "${PREFIX}"/bin/
 
-# Build PSASS specifying CPP compiler and C compiler
-make CXX="${CXX}" CC="${CC}" tiny
+# Build specifying CPP compiler and C compiler
+make CXX="${CXX}" C="${CC}" tiny
 
 # Copy all generated binaries to bin
-cp bin/*mutselomega "${PREFIX}"/bin/
-cp bin/*nodemutsel "${PREFIX}"/bin/
-cp utils/*.py "${PREFIX}"/bin
+mv bin/*mutselomega "${PREFIX}"/bin/
+mv bin/node* "${PREFIX}"/bin/
+mv bin/readnode* "${PREFIX}"/bin/
+mv utils/*.py "${PREFIX}"/bin/
