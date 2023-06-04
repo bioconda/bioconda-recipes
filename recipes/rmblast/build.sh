a783b8c5de727225ed31029ed28eefab51c25f0e
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-04 12:20:38 -0500

    Update rmblast recipe to build on macOS

--- recipes/rmblast/build.sh
+++ recipes/rmblast/build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-patch -p1 < ${RECIPE_DIR}/isb-2.14.0+-rmblast.patch
-
 cd ${SRC_DIR}/c++/
 
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="-L${PREFIX}/lib"
 
-export CFLAGS="${CFLAGS} -Ofast"
-export CXXFLAGS="${CXXFLAGS} -Ofast"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 if test x"`uname`" = x"Linux"; then
@@ -26,7 +24,7 @@ else
     export CPP_FOR_BUILD=${CPP}
 fi
 
-export LIB_INSTALL_DIR=${PREFIX}/lib/ncbi-blast+
+export LIB_INSTALL_DIR="${PREFIX}/lib/ncbi-blast+"
 
 # with/without options:
 #
@@ -55,64 +53,41 @@ export LIB_INSTALL_DIR=${PREFIX}/lib/ncbi-blast+
 # Fixes building on Linux
 export AR="${AR} rcs"
 
-# not building with boost as it's only used for unit tests
-if [[ $(uname) = Linux ]]; then
-    ./configure \
-        CXX=${CXX} CXXFLAGS="${CXXFLAGS}" \
-        --prefix=${PREFIX} \
-        --without-boost \
-        --with-bin-release \
-        --with-mt \
-        --with-dll \
-        --with-openmp \
-        --without-autodep \
-        --without-makefile-auto-update \
-        --with-flat-makefile \
-        --without-caution \
-        --without-dbapi \
-        --without-lzo \
-        --with-hard-runpath \
-        --with-runpath=${LIB_INSTALL_DIR} \
-        --without-debug \
-        --with-experimental=Int8GI \
-        --without-openssl \
-        --with-strip \
-        --without-vdb \
-        --with-z=${PREFIX} \
-        --with-bz2=${PREFIX} \
-        --with-nettle=${PREFIX} \
-        --without-krb5 \
-        --without-gnutls \
-        --without-sse42 \
-        --without-gcrypt
+if [ `uname` == Linux ]; then
+  export CONFIG_ARGS="--with-openmp --with-hard-runpath"
 else
-    ./configure \
-        CXX=${CXX} CXXFLAGS="${CXXFLAGS}" \
-        --prefix=${PREFIX} \
-        --without-boost \
-        --with-bin-release \
-        --with-mt \
-        --without-openmp \
-        --without-autodep \
-        --without-makefile-auto-update \
-        --with-flat-makefile \
-        --without-caution \
-        --without-dbapi \
-        --without-lzo \
-        --with-runpath=${LIB_INSTALL_DIR} \
-        --without-debug \
-        --with-experimental=Int8GI \
-        --without-openssl \
-        --with-strip \
-        --without-vdb \
-        --with-z=${PREFIX} \
-        --with-bz2=${PREFIX} \
-        --with-nettle=${PREFIX} \
-        --without-krb5 \
-        --without-gnutls \
-        --without-sse42 \
-        --without-gcrypt
+  export CONFIG_ARGS="--without-openmp --with-bundles"
 fi
+
+# not building with boost as it's only used for unit tests
+./configure \
+    CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+    --prefix=${PREFIX} \
+    --with-static-exe \
+    --without-caution \
+    --without-boost \
+    --with-64 \
+    --with-dll \
+    --with-mt \
+    --without-autodep \
+    --without-makefile-auto-update \
+    --with-flat-makefile \
+    --with-runpath=${LIB_INSTALL_DIR} \
+    --without-lzo \
+    --without-zstd \
+    --without-debug \
+    --with-experimental=Int8GI \
+    --without-openssl \
+    --with-strip \
+    --without-vdb \
+    --with-z=${PREFIX} \
+    --with-bz2=${PREFIX} \
+    --without-krb5 \
+    --without-gnutls \
+    --without-sse42 \
+    --without-gcrypt \
+    --without-pcre \
+    --with-projects="scripts/projects/rmblastn/project.lst" ${CONFIG_ARGS}
 
 projects="algo/blast/ app/ objmgr/ objtools/align_format/ objtools/blast/"
 cd ReleaseMT
@@ -122,7 +97,7 @@ cd ReleaseMT
 ln -s ${SRC_DIR}/c++/ReleaseMT/lib ${LIB_INSTALL_DIR}
 
 cd build
-make -j1 -f Makefile.flat all_projects="${projects}"
+make INCLUDES="-I${PREFIX}/include" LIBPATH="-L${PREFIX}/lib" -j1 -f Makefile.flat all_projects="${projects}"
 
 # remove temporary link
 rm ${LIB_INSTALL_DIR}
