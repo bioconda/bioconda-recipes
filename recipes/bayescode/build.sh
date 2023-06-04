372df4532205b04947ad125b97b30db918ac38bc
Thibault Latrille <thibault.latrille@ens-lyon.org>
2023-06-04 17:02:36 +0200

    Test debug env

--- recipes/bayescode/build.sh
+++ recipes/bayescode/build.sh
@@ -4,9 +4,9 @@ rm -rf data/
 # Create bin directory
 mkdir -p "${PREFIX}"/bin/
 
-top -n 1 -b | head -n 5
+top -n 1 -b | head -n 20
 # Build specifying CPP compiler and C compiler
-make CXX="${CXX}" C="${CC}" tiny
+make CXX="${CXX}" tiny
 
 # Copy all generated binaries to bin
 mv bin/*mutselomega "${PREFIX}"/bin/
