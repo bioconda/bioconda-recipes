9f91656c7770deca2b5ab985628b344d4c8813d1
Jason Stajich <jasonstajich.phd@gmail.com>
2023-06-11 14:35:52 -0700

    try again on unzip step

--- recipes/tbl2asn/build.sh
+++ recipes/tbl2asn/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-gunzip *tbl2asn.gz 
+gunzip *tbl2asn*.gz 
 mkdir -p ${PREFIX}/bin
-if [ -f tbl2asn.mac.gz ]; then
-	gunzip tbl2asn.mac.gz
+if [ -f tbl2asn.mac ]; then
 	cp tbl2asn.mac ${PREFIX}/bin/tbl2asn
 else
 	cp *.tbl2asn ${PREFIX}/bin/tbl2asn
