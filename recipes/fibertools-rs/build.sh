920f9b2f571301f20a6df7bb44e75d9bf9953b72
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-06-12 12:47:25 -0700

    trying to get the libraireis linked... but they wont...

--- recipes/fibertools-rs/build.sh
+++ recipes/fibertools-rs/build.sh
@@ -1,30 +1,33 @@
 #!/bin/bash -e
 
+TORCH_VERSION="2.0.1"
+INSTALL_TYPE="cpu" # "cu117" or "cu118" or "cpu"
 if [[ ${target_platform} =~ linux.* ]]; then
-    #file=https://download.pytorch.org/libtorch/cu117/libtorch-shared-with-deps-2.0.1%2Bcu117.zip
-    #file=https://download.pytorch.org/libtorch/cu118/libtorch-shared-with-deps-2.0.1%2Bcu118.zip
-    #file=https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-2.0.1%2Bcpu.zip
-    #curl $file --output ${PREFIX}/libtorch.zip
-    #export LIBTORCH_CXX11_ABI=0
-    export LIBTORCH_USE_PYTORCH=1
+    file=https://download.pytorch.org/libtorch/${INSTALL_TYPE}/libtorch-shared-with-deps-${TORCH_VERSION}%2B${INSTALL_TYPE}.zip
+    curl $file --output ${PREFIX}/libtorch.zip
+    export LIBTORCH_CXX11_ABI=0
+    #export LIBTORCH_USE_PYTORCH=1
 elif [[ ${target_platform} =~ osx.* ]]; then
-    curl https://download.pytorch.org/libtorch/cpu/libtorch-macos-2.0.1.zip \
+    curl https://download.pytorch.org/libtorch/cpu/libtorch-macos-${TORCH_VERSION}.zip \
         --output ${PREFIX}/libtorch.zip
 fi
+
+# safe place to keep libraries?
+OUTDIR=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+mkdir -p ${OUTDIR} ${OUTDIR}/bin
+
+# mv libtorch to OUTDIR
 pushd ${PREFIX}
 unzip libtorch.zip
 rm libtorch.zip
+mv ${PREFIX}/libtorch/* ${OUTDIR}/.
+popd
 
-mkdir -p ${PREFIX}/share ${PREFIX}/lib ${PREFIX}/include
-mv ${PREFIX}/libtorch/lib/* ${PREFIX}/lib/.
-mv ${PREFIX}/libtorch/include/* ${PREFIX}/include/.
-mv ${PREFIX}/libtorch/share/* ${PREFIX}/share/.
-
-export LIBTORCH=${PREFIX}
+# set up environment variables
+export LIBTORCH=${OUTDIR}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIBTORCH}/lib
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${LIBTORCH}/lib
 export RPATH=${RPATH}:${LIBTORCH}/lib
-popd
 
 # TODO: Remove the following export when pinning is updated and we use
 #       {{ compiler('rust') }} in the recipe.
@@ -39,10 +42,16 @@ if [[ -n "$OSX_ARCH" ]]; then
     export RUSTFLAGS="-C link-arg=-undefined -C link-arg=dynamic_lookup"
 fi
 
+# install package
 HOME=$(pwd)
-pushd ${PREFIX}
-cargo install --all-features --no-track --verbose --root "${PREFIX}" --path ${HOME}
+pushd ${OUTDIR}
+cargo install --all-features --no-track --verbose --root "${OUTDIR}" --path ${HOME}
 popd
-ft --help
 
+# link binary
+ln -s ${OUTDIR}/bin/ft ${PREFIX}/bin/ft
+chmod +x ${PREFIX}/bin/ft
+
+# test install
+ft --help
 exit 0
