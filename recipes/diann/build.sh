69bf9a77db79535c333cf7c6d7fea7249a43c375
npinter <npinter@protonmail.com>
2023-06-14 22:06:19 +0200

    libgomp test

--- recipes/diann/build.sh
+++ recipes/diann/build.sh
@@ -7,4 +7,8 @@ tar xvf data.tar.gz
 DIANN_PATH=$(find . -type f -executable -print | grep diann)
 DIANN_DIR=$(dirname $DIANN_PATH)
 
-find $DIANN_DIR -type f -exec install -m 0755 {} $PREFIX/bin/ \;
+find $DIANN_DIR -type f -exec cp {} $PREFIX/bin/ \;
+
+ln -s ${PREFIX}/bin/libgomp.so.1 ${$DIANN_PATH}/libgomp-52f2fd74.so.1
+
+chmod +x $PREFIX/bin/diann-1.8.1
