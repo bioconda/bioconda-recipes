2a93fe8f8f7ce3e67862d1635b40ccb4cbf31e24
Matthias Bernt <m.bernt@ufz.de>
2023-06-15 15:37:31 +0200

    implement build change as patch

--- recipes/mzmine/build.sh
+++ recipes/mzmine/build.sh
@@ -2,15 +2,9 @@
 
 set -ex
 
-# TODO use patch
-sed -i -e 's@// installerType = "deb"@installerType = "rpm"@; s@vendor = JvmVendorSpec.ADOPTIUM@// vendor = JvmVendorSpec.ADOPTIUM@' build.gradle
-
-
 # debug: show available tool chains
 ./gradlew -q javaToolchains
-
 ./gradlew --debug --stacktrace --exclude-task test
-
 
 mkdir -p "$PREFIX"
 cp -rv build/install/MZmine/* "$PREFIX"
@@ -18,5 +12,7 @@ cp -rv build/install/MZmine/* "$PREFIX"
 # for those the don't like capital letters in commands
 ln -fs "$PREFIX"/bin/MZmine "$PREFIX"/bin/mzmine
 
+# MZmine does not run without --enable-preview
+# .. so we sneek in in the command line
 sed -i -e 's/exec "$JAVACMD" "$@"/exec "$JAVACMD" --enable-preview "$@"/' "$PREFIX"/bin/MZmine
 
