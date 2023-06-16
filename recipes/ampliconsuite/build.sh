532af975299b61235f21da5c73c8edc9c8f54a92
Jens Luebeck <jluebeck@eng.ucsd.edu>
2023-06-16 16:29:11 -0700

    adjust recipe based on reviewer feedback

--- recipes/ampliconsuite/build.sh
+++ recipes/ampliconsuite/build.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-# pull source code for AA and move scripts into a library
-TARGET="AmpliconArchitect"
-TARGET_VERSION="1.3.r5"
-wget https://github.com/AmpliconSuite/${TARGET}/archive/refs/tags/v${TARGET_VERSION}.zip
-unzip v${TARGET_VERSION}.zip
-mkdir -p ampliconarchitectlib
-cp ${TARGET}-${TARGET_VERSION}/src/*.py ampliconarchitectlib/
-touch ampliconarchitectlib/__init__.py
-rm v${TARGET_VERSION}.zip
+set -ex
 
-# pull source code for AC and move scripts into a library
-TARGET="AmpliconClassifier"
-TARGET_VERSION="0.5.2"
-wget https://github.com/AmpliconSuite/${TARGET}/archive/refs/tags/v${TARGET_VERSION}.zip
-unzip v${TARGET_VERSION}.zip
-mkdir -p ampliconclassifierlib
-cp ${TARGET}-${TARGET_VERSION}/*.py ampliconclassifierlib/
-cp ${TARGET}-${TARGET_VERSION}/*.sh ampliconclassifierlib/
-cp -r ${TARGET}-${TARGET_VERSION}/resources/ ampliconclassifierlib/resources/
+# add init for ampliconarchitect and ampliconclassifier tools, so they can be imported by AmpliconSuite-pipeline
+touch ampliconarchitectlib/__init__.py
 touch ampliconclassifierlib/__init__.py
-rm v${TARGET_VERSION}.zip
 
 # make the bin dir if it doesn't exist
 mkdir -p $PREFIX/bin
 
 # copy driver scripts
+# setup.py will handle this in the next release
 cp AmpliconSuite-pipeline.py ${PREFIX}/bin/AmpliconSuite-pipeline.py
 cp GroupedAnalysisAmpSuite.py ${PREFIX}/bin/GroupedAnalysisAmpSuite.py
 
