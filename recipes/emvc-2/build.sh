9d621a40b3fdf21f95b7c50b9e6af611aff39a78
Guillermo Dufort <cslcollab-guidufort@idoia-work-01.csl.illinois.edu>
2023-06-17 15:39:41 -0500

    Removed cd

--- recipes/emvc-2/build.sh
+++ recipes/emvc-2/build.sh
@@ -1,19 +1,11 @@
 #! /bin/bash
 mkdir -p $PREFIX/bin
-cd EMVC-2
 
 sed -i.bak 's/CXX *=/CXX ?=/; s/CXXFLAGS *=/CXXFLAGS +=/' Makefile
 
-# Work around "unknown type name 'mach_port_t'" error
-if [ x"$(uname)" == x"Darwin" ]; then
-  CXXFLAGS="$CXXFLAGS -D_DARWIN_C_SOURCE"
-  CPPFLAGS="$CPPFLAGS -D_DARWIN_C_SOURCE"
-  export CXXFLAGS CPPFLAGS
-fi
-
 export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
 export LDFLAGS="-I$PREFIX/include -L$PREFIX/lib"
 make 
 
+cp candidate_variants_finder $PREFIX/bin
 cp emvc-2 $PREFIX/bin
