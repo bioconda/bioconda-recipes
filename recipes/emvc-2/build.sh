ac3e6fd9aecc2ba860d63bc2870716df5642b2fa
Guillermo Dufort <cslcollab-guidufort@idoia-work-01.csl.illinois.edu>
2023-06-18 18:18:45 -0500

    Changed C flags

--- recipes/emvc-2/build.sh
+++ recipes/emvc-2/build.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 mkdir -p $PREFIX/bin
 
-sed -i.bak 's/CXX *=/CXX ?=/; s/CXXFLAGS *=/CXXFLAGS +=/' Makefile
+sed -i.bak 's/CC *=/CC ?=/; s/CCFLAGS *=/CCFLAGS +=/' Makefile
 
 # Work around "unknown type name 'mach_port_t'" error
 if [ x"$(uname)" == x"Darwin" ]; then
-  CXXFLAGS="$CXXFLAGS -D_DARWIN_C_SOURCE"
+  CCFLAGS="$CCFLAGS -D_DARWIN_C_SOURCE"
   CPPFLAGS="$CPPFLAGS -D_DARWIN_C_SOURCE"
-  export CXXFLAGS CPPFLAGS
+  export CCFLAGS CPPFLAGS
 fi
 
 export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
+export CCFLAGS="$CCFLAGS -I$PREFIX/include"
 export LDFLAGS="-I$PREFIX/include -L$PREFIX/lib"
 
 make 
