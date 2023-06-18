1454517a8f38751703aee02c6e4ccc87172e5bd5
npinter <npinter@protonmail.com>
2023-06-18 17:36:09 +0200

    Fix libgomp

--- recipes/diann/build.sh
+++ recipes/diann/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export LDFLAGS="-L${PREFIX}/lib"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib/"
-
 mkdir -p $PREFIX/bin
 
 tar xvf data.tar.gz
@@ -11,8 +8,5 @@ DIANN_PATH=$(find . -type f -executable | grep -P "diann-[^/]*$")
 DIANN_DIR=$(dirname $DIANN_PATH)
 
 cp -R $DIANN_DIR/* $PREFIX/bin/
-
-# symlink from libgomp-52f2fd74.so.1 to libgomp.so.1
-ln -s $PREFIX/lib/libgomp.so.1.0.0 $PREFIX/bin/libgomp.so.1
 
 chmod +x $PREFIX/bin/diann-1.8.1
