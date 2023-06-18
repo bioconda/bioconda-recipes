312cd1f28c4dd837c2d67b4009797f99996fd0b2
npinter <npinter@protonmail.com>
2023-06-18 21:22:03 +0200

    Fix libgomp

--- recipes/diann/build.sh
+++ recipes/diann/build.sh
@@ -10,7 +10,7 @@ tar xvf data.tar.gz
 DIANN_PATH=$(find . -type f -executable | grep -P "diann-[^/]*$")
 DIANN_DIR=$(dirname $DIANN_PATH)
 
-mv -R $DIANN_DIR/diann-1.8.1 $PREFIX/bin/
+mv $DIANN_DIR/diann-1.8.1 $PREFIX/bin/
 cp -R $DIANN_DIR/* $PREFIX/lib/
 
 chmod +x $PREFIX/bin/diann-1.8.1
