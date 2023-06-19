986aa403c189e83cf89484b56e83f34edb4703bd
npinter <npinter@protonmail.com>
2023-06-19 13:19:21 +0200

    Fix libgomp

--- recipes/diann/build.sh
+++ recipes/diann/build.sh
@@ -15,6 +15,7 @@ mv $DIANN_DIR/diann-1.8.1 $PREFIX/bin/
 cp -R $DIANN_DIR/* $PREFIX/lib/
 
 # fix for libgomp
+ls -alh ${PREFIX}/lib/
 rm -f ${PREFIX}/lib/libgomp-52f2fd74.so.1
 ln -s ${PREFIX}/lib/libgomp.so.1.0.0 ${PREFIX}/lib/libgomp-52f2fd74.so.1
 
