ab2c283992c265aa092550bc1cef291c02d7aae5
npinter <npinter@protonmail.com>
2023-06-19 15:16:51 +0200

    Fix libgomp

--- recipes/diann/build.sh
+++ recipes/diann/build.sh
@@ -12,15 +12,15 @@ DIANN_PATH=$(find . -type f -executable | grep -P "diann-[^/]*$")
 DIANN_DIR=$(dirname $DIANN_PATH)
 
 #mv $DIANN_DIR/diann-1.8.1 $PREFIX/bin/
-cp -R $DIANN_DIR/* $PREFIX/lib/
+cp -R $DIANN_DIR/* $PREFIX/bin/
 
 # fix for libgomp
 echo "Remove libgomp-52f2fd74.so.1"
-rm -f ${PREFIX}/lib/libgomp-52f2fd74.so.1
+rm -f ${PREFIX}/bin/libgomp-52f2fd74.so.1
 echo "Create symlink libgomp-52f2fd74.so.1"
-ln -s ${PREFIX}/lib/libgomp.so.1.0.0 ${PREFIX}/lib/libgomp-52f2fd74.so.1
+ln -s ${PREFIX}/bin/libgomp.so.1.0.0 ${PREFIX}/bin/libgomp-52f2fd74.so.1
 
-chmod +x $PREFIX/lib/diann-1.8.1
+chmod +x $PREFIX/bin/diann-1.8.1
 
 #cp -R $BUILD_PREFIX/x86_64-conda-linux-gnu/sysroot/lib64/libm.so.6 ${PREFIX}/lib/libm.so.6
 #cp -R $BUILD_PREFIX/x86_64-conda-linux-gnu/sysroot/lib64/libm.so.6 ${PREFIX}/bin/libm.so.6
