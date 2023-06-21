9c610857f60068de4bcc3e3787869be6edd8e611
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-20 19:06:46 -0500

    Update sage-proteomics to 0.13.2

--- recipes/sage-proteomics/build.sh
+++ recipes/sage-proteomics/build.sh
@@ -1,9 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
-export CFLAGS="${CFLAGS} -fcommon"
-export CXXFLAGS="${CFLAGS} -fcommon"
+export INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+#export CFLAGS="${CFLAGS} -fcommon"
+#export CXXFLAGS="${CXXFLAGS} -fcommon"
 
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
 # build statically linked binary with sage
-RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path crates/sage-cli
+RUST_BACKTRACE=1 cargo install --verbose -j 4 --root ${PREFIX} --path crates/sage-cli
