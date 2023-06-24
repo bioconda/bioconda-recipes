5bf38109be8f78670bccbbae5aa8ae81ab09f889
AndreaGuarracino <andreaguarracino@outlook.com>
2023-06-24 22:48:21 +0200

    fix mv

--- recipes/pggb/build.sh
+++ recipes/pggb/build.sh
@@ -7,7 +7,9 @@ sed -i ${n}'a\if [ $show_version == true ]; then echo "pggb '$PKG_VERSION'"; exi
 
 mkdir -p $PREFIX/bin
 
-cp pggb $PREFIX/bin
-cp partition-before-pggb $PREFIX/bin
+mv pggb $PREFIX/bin
+mv partition-before-pggb $PREFIX/bin
 cp scripts/*.py $PREFIX/bin
-cp scripts $PREFIX/bin
+mv scripts $PREFIX/bin
+
+pggb -i data/HLA/DRB1-3123.fa.gz -p 70 -s 3000 -G 800,900,1100 -n 10 -Z -V 'gi|568815561:#,gi|29124352:#:10000' -M -m -o drib1_spoa
