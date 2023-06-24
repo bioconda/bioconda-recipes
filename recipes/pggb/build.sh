4b774ff05f6b3d92ae0b3e0db2dbdcfe5d3e034d
AndreaGuarracino <andreaguarracino@outlook.com>
2023-06-24 23:01:14 +0200

    comment command line

--- recipes/pggb/build.sh
+++ recipes/pggb/build.sh
@@ -12,4 +12,4 @@ mv partition-before-pggb $PREFIX/bin
 cp scripts/*.py $PREFIX/bin
 mv scripts $PREFIX/bin
 
-pggb -i data/HLA/DRB1-3123.fa.gz -p 70 -s 3000 -G 800,900,1100 -n 10 -Z -V 'gi|568815561:#,gi|29124352:#:10000' -M -m -o drib1_spoa
+#pggb -i data/HLA/DRB1-3123.fa.gz -p 70 -s 3000 -G 800,900,1100 -n 10 -Z -V 'gi|568815561:#,gi|29124352:#:10000' -M -m -o drib1_spoa
