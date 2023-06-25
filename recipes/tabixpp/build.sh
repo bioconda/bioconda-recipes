9973e79705e485555f3fc0c75220f18991cc28bf
AndreaGuarracino <andreaguarracino@outlook.com>
2023-06-25 21:21:19 +0200

    build

--- recipes/tabixpp/build.sh
+++ recipes/tabixpp/build.sh
@@ -2,20 +2,24 @@
 set -x -e
 
 if [ "$(uname)" == "Darwin" ]; then
-    sed -i.bak 's/-Wl,-soname/-Wl,-install_name/g' Makefile
-    sed -i.bak 's/\.so.$(SONUMBER)/.$(SONUMBER).dylib/g' Makefile
+	sed -i.bak 's/-Wl,-soname/-Wl,-install_name/g' Makefile
+	sed -i.bak 's/\.so.$(SONUMBER)/.$(SONUMBER).dylib/g' Makefile
 fi
 
-make 
-cp tabix++ $PREFIX/bin
+mkdir -p ${PREFIX}/bin
 
-if [ "$(uname)" == "Darwin" ]; then
-    cp libtabixpp.*.dylib ${PREFIX}/lib/
-    ln -s ${PREFIX}/lib/libtabixpp.*.dylib ${PREFIX}/lib/libtabixpp.dylib 
-else 
-    cp libtabixpp.so.* ${PREFIX}/lib/
-    ln -s ${PREFIX}/lib/libtabixpp.so.* ${PREFIX}/lib/libtabixpp.so  
-fi
+make \
+	LIBPATH="-L${PREFIX}/lib" \
+	INCLUDES="-I${PREFIX}/include" \
+	-v install
 
-cp *.hpp ${PREFIX}/include
-cp libtabixpp.a ${PREFIX}/lib/
+#if [ "$(uname)" == "Darwin" ]; then
+#	cp libtabixpp.*.dylib ${PREFIX}/lib/
+#	ln -s ${PREFIX}/lib/libtabixpp.*.dylib ${PREFIX}/lib/libtabixpp.dylib
+#fi
+
+#cp tabix++ ${PREFIX}/bin
+#cp *.hpp ${PREFIX}/include
+#cp libtabix.a libtabix.so.* ${PREFIX}/lib
+#cp libtabixpp.a ${PREFIX}/lib
+#cp libtabixpp.so.* ${PREFIX}/lib
