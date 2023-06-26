29ecb48331fec8f1ed64331f662263f7a574aeec
Saliha Zen <saliha.zenboudji@gmail.com>
2023-06-26 09:54:46 +0200

    update bash.sh

--- recipes/rapgreen/build.sh
+++ recipes/rapgreen/build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+BINARY_HOME=$PREFIX/bin 
+RAP_GREEN_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
-rapgreen=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $rapgreen
-mkdir -p $PREFIX/bin
-cp -R * $rapgreen/
-cp $RECIPE_DIR/test_rapgreen $rapgreen/rap-green
-ln -s $rapgreen/rapgreen $PREFIX/bin
-chmod 0755 "${PREFIX}/bin/rap-green"
-ls $rapgreen/resources
-cmod -R a+rw $rapgreen/resources/
+# Copy source to the conda environment
+mkdir -p $RAP_GREEN_HOME
+cp -R $SRC_DIR/* $RAP_GREEN_HOME/
+
+# Create symbolic links for a5_miseq's launch script
+mkdir -p $BINARY_HOME
+ln -s $RAP_GREEN_HOME/bin/RapGreen.jar $BINARY_HOME/ 
+
+
+
+
+
+#Old script
+
+#outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+#mkdir -p $outdir
+#mkdir -p $PREFIX/bin
+#cp "$PREFIX/bin/RapGreen.jar "$outdir/"
+#chmod 0755 "${PREFIX}/bin/RapGreen.jar"
+
+#ln -s "$outdir/rapgreen" "$PREFIX/bin"
