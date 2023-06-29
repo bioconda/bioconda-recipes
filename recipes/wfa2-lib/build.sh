49cc2222eed794dc1e6f4e440dc7c6faf07cc21e
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-29 17:18:26 -0500

    remove -ftree-vectorizer-verbose=5

--- recipes/wfa2-lib/build.sh
+++ recipes/wfa2-lib/build.sh
@@ -9,7 +9,7 @@ cmake -S . -B build \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_FLAGS="-O3 -D_FILE_OFFSET_BITS=64 -I${PREFIX}/include ${LDFLAGS}" \
-	-DEXTRA_FLAGS="-ftree-vectorize -msse2 -mfpmath=sse -ftree-vectorizer-verbose=5" \
+	-DEXTRA_FLAGS="-ftree-vectorize -msse2 -mfpmath=sse" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DBUILD_SHARED_LIBS=ON
