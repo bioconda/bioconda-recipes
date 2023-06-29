060d073e1b0adb95203b3ded2b2256e48ab56157
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-29 15:23:57 -0500

    add -O3 optimization

--- recipes/pyspoa/build.sh
+++ recipes/pyspoa/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CXX="${CXX}" INCLUDES="-I${PREFIX}/include" LIBPATH="-L${PREFIX}/lib" build
-${PYTHON} -m pip install . --no-deps --ignore-install -vv
+make CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3" \
+	INCLUDES="-I${PREFIX}/include" \
+	LIBPATH="-L${PREFIX}/lib" -j4 build
+${PYTHON} -m pip install . --no-deps --ignore-install --use-pep517 -vv
