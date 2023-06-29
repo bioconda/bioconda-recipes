59fd5026468deca4d29379a90143f01f806bd6c1
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-29 18:26:31 -0500

    try i.bak

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -11,7 +11,7 @@ export BINARY_HOME="${PREFIX}/bin"
 export TRINITY_HOME="${PREFIX}/opt/trinity-${PKG_VERSION}"
 
 make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" -j4 plugins
-make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" -j4
+make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
 
 # remove the sample data
 rm -rf ${SRC_DIR}/sample_data
@@ -53,8 +53,8 @@ find ${TRINITY_HOME} -type f -print0 | xargs -0 sed -i.bak 's/FindBin::Bin/FindB
 # Replace hard coded path to deps
 find ${TRINITY_HOME} -type f -print0 | xargs -0 sed -i.bak 's/$JELLYFISH_DIR\/bin\/jellyfish/jellyfish/g'
 sed -i.bak "s/\$ROOTDIR\/trinity-plugins\/Trimmomatic/\/opt\/anaconda1anaconda2anaconda3\/share\/trimmomatic/g" ${TRINITY_HOME}/Trinity
-sed -i 's/my $TRIMMOMATIC = "\([^"]\+\)"/my $TRIMMOMATIC = '"'"'\1'"'"'/' ${TRINITY_HOME}/Trinity
-sed -i 's/my $TRIMMOMATIC_DIR = "\([^"]\+\)"/my $TRIMMOMATIC_DIR = '"'"'\1'"'"'/' ${TRINITY_HOME}/Trinity
+sed -i.bak 's/my $TRIMMOMATIC = "\([^"]\+\)"/my $TRIMMOMATIC = '"'"'\1'"'"'/' ${TRINITY_HOME}/Trinity
+sed -i.bak 's/my $TRIMMOMATIC_DIR = "\([^"]\+\)"/my $TRIMMOMATIC_DIR = '"'"'\1'"'"'/' ${TRINITY_HOME}/Trinity
 
 
 find ${TRINITY_HOME} -type f -name "*.bak" -print0 | xargs -0 rm -f
