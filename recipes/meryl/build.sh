49a7d325441658b3d19b08a3b0dc6aa0308dcbbc
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-29 16:25:21 -0500

    Update meryl to 1.4

--- recipes/meryl/build.sh
+++ recipes/meryl/build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-make -C src \
-  BUILD_DIR="$( pwd )/build" \
-  TARGET_DIR="${PREFIX}" \
-  CC="${CC}" \
-  CXX="${CXX}"
+#make -C src \
+#  BUILD_DIR="$( pwd )/build" \
+#  TARGET_DIR="${PREFIX}" \
+#  CC="${CC}" \
+#  CXX="${CXX}"
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/lib
+
+if [ `uname` == Darwin ]; then
+	cp meryl-${PKG_VERSION}-darwin/bin/* ${PREFIX}/bin/
+	cp meryl-${PKG_VERSION}-darwin/lib/* ${PREFIX}/lib/
+	else
+	cp meryl-${PKG_VERSION}-linux/bin/* ${PREFIX}/bin/
+	cp meryl-${PKG_VERSION}-linux/lib/* ${PREFIX}/lib/
+fi
 
 rm "${PREFIX}/lib/libmeryl.a"
+
+chmod 0755 ${PREFIX}/bin/meryl-*
+chmod 0755 ${PREFIX}/bin/meryl
+chmod 0755 ${PREFIX}/bin/position-lookup
