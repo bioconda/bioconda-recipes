cf096925bef71b74690788b905679b103a06b484
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-06-30 21:17:38 -0500

    remove unecessary spoa patch

--- recipes/pyspoa/build.sh
+++ recipes/pyspoa/build.sh
@@ -6,4 +6,4 @@ make CXX="${CXX}" \
 	CXXFLAGS="${CXXFLAGS} -O3" \
 	INCLUDES="-I${PREFIX}/include" \
 	LIBPATH="-L${PREFIX}/lib" -j4 build
-${PYTHON} -m pip install . --no-deps --ignore-install --use-pep517 -vv
+${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-install -vv
