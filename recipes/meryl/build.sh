6ae3ae6b0f70afa2121702bbfd6eab892b52717e
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-07-01 00:46:02 -0500

    try building from source

--- recipes/meryl/build.sh
+++ recipes/meryl/build.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-#make -C src \
-#  BUILD_DIR="$( pwd )/build" \
-#  TARGET_DIR="${PREFIX}" \
-#  CC="${CC}" \
-#  CXX="${CXX}"
-
-mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/lib
-
-if [ `uname` == Darwin ]; then
-	cp meryl-${PKG_VERSION}-darwin/bin/* ${PREFIX}/bin/
-	cp meryl-${PKG_VERSION}-darwin/lib/* ${PREFIX}/lib/
-	else
-	cp meryl-${PKG_VERSION}-linux/bin/* ${PREFIX}/bin/
-	cp meryl-${PKG_VERSION}-linux/lib/* ${PREFIX}/lib/
-fi
-
-rm "${PREFIX}/lib/libmeryl.a"
-
-chmod 0755 ${PREFIX}/bin/meryl-*
-chmod 0755 ${PREFIX}/bin/meryl
-chmod 0755 ${PREFIX}/bin/position-lookup
+make -C src BUILD_DIR="$(pwd)" \
+	TARGET_DIR="${PREFIX}" \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -fopenmp -L${PREFIX}/lib" -j"${CPU_COUNT}"
