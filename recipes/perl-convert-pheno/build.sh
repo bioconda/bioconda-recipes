42c9745e4fe4140b59aa681a34a80cab15cf3cce
Ivo Leist <ivo.leist@googlemail.com>
2023-07-03 09:56:41 +0200

    add build.sh and meta.yaml for perl-convert-pheno

---
+++ recipes/perl-convert-pheno/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# install dependencies not found in conda channels
+install_deps() {
+    deps=(
+        "File::ShareDir::ProjectDistDir"
+        "JSON::Validator"
+        "Moo"
+        "Path::Tiny"
+        "Test::Deep"
+        "Test::Warn"
+        "Text::CSV_XS"
+        "Text::Similarity"
+        "Types::Standard"
+        "XML::Fast"
+        "YAML::XS"
+    )
+    for dep in "${deps[@]}"; do
+        HOME=/tmp cpanm "$dep" || {
+            echo "Failed to install perl module $dep"
+            exit 1
+        }
+    done
+}
+install_deps
+perl Makefile.PL INSTALLDIRS=site
+make
+# make test
+make install
