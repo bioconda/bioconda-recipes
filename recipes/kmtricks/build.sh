d9d1d9fa6b903c1ce5184c43f9379c263776d126
Teo Lemane <teo.lemane@gmail.com>
2023-07-04 11:48:57 +0200

    add kmtricks

--- recipes/kmtricks/build.sh
+++ recipes/kmtricks/build.sh
@@ -10,30 +10,27 @@ KMER_SIZE_LIST="32 64 96 128 160 192 224 256"
 
 mkdir -p $PREFIX/bin
 
-cmake -s . -B build
-
-mkdir build-conda
-
-cd build-conda
-
-cmake -s . -B build -DCMAKE_BUILD_TYPE=Release \
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
                     -DNATIVE=OFF \
                     -DCONDA_BUILD=ON \
                     -DWITH_MODULES=ON \
                     -DWITH_HOWDE=ON \
                     -DKMER_LIST=${KMER_SIZE_LIST} \
-                    -DWITH_SOCKS=ON
+                    -DWITH_SOCKS=ON \
+                    ${PLATFORM_FLAGS}
 cmake --build ./build
 
 cp -r ./bin/kmtricks $PREFIX/bin
 cp -r ./bin/kmtricks-socks $PREFIX/bin
 
-cmake -s . -B build -DCMAKE_BUILD_TYPE=Release \
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
                     -DNATIVE=OFF \
                     -DWITH_PLUGIN=ON \
                     -DCONDA_BUILD=ON \
                     -DWITH_MODULES=ON \
-                    -DKMER_LIST=${KMER_LIST}
+                    -DKMER_LIST=${KMER_LIST} \
+                    ${PLATFORM_FLAGS}
+
 cmake --build ./build
 
 cp -r ./bin/kmtricks $PREFIX/bin/kmtricksp
