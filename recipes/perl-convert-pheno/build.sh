8f4d0fcc869eb77cf6670f21e8683d0e610cc70d
Ivo <ivo.leist@googlemail.com>
2023-07-04 15:19:07 +0200

    try to install Test::Warn via conda

--- recipes/perl-convert-pheno/build.sh
+++ recipes/perl-convert-pheno/build.sh
@@ -8,7 +8,7 @@ install_deps() {
         "Moo"
         "Path::Tiny"
         "Test::Deep"
-        "Test::Warn"
+        # "Test::Warn"
         "Text::CSV_XS"
         "Text::Similarity"
         "Types::Standard"
