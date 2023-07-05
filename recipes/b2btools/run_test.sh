7357168e9baa1d19491a019bdd2d79613065f108
Adrián Diaz <diaz.adrian.g@gmail.com>
2023-07-05 13:08:52 +0200

    Update b2btools to 3.0.6

--- recipes/b2btools/run_test.sh
+++ recipes/b2btools/run_test.sh
@@ -1,25 +1,134 @@
 #!/bin/bash
 
-echo Testing b2bTools
+# Test locally? Read these articles:
+# - https://bioconda.github.io/contributor/building-locally.html
+# - https://docs.openfree.energy/en/stable/installation.html
 
-cat <<EOT >> input_example.fasta
->SEQ_1
-MEDLNVVDSINGAGSWLVANQALLLSYAVNIVAALAIIIVGLIIARMISNAVNRLMISRK
-IDATVADFLSALVRYGIIAFTLIAALGRVGVQTASVIAVLGAAGLAVGLALQGSLSNLAA
-GVLLVMFRPFRAGEYVDLGGVAGTVLSVQIFSTTMRTADGKIIVIPNGKIIAGNIINFSR
-EPVRRNEFIIGVAYDSDIDQVKQILTNIIQSEDRILKDREMTVRLNELGASSINFVVRVW
-SNSGDLQNVYWDVLERIKREFDAAGISFPYPQMDVNFKRVKEDKAA
-EOT
+echo "1) Testing b2bTools installation"
 
 python -m b2bTools --help
+status=$?
 
-python -m b2bTools -file ./input_example.fasta -identifier TEST -output ./results_b2b.json -dynamine -disomine -efoldmine -agmata
+# Check if the status code is non-zero and exit with an error code
+if [ $status -ne 0 ]; then
+    echo "Error: b2bTools command failed with status code $status"
+    exit $status
+fi
+
+python -m b2bTools --version
+status=$?
+
+# Check if the status code is non-zero and exit with an error code
+if [ $status -ne 0 ]; then
+    echo "Error: b2bTools command failed with status code $status"
+    exit $status
+fi
+
+echo "2) Testing b2bTools for Single Seq mode"
+
+cat <<EOT >> ./input_example.fasta
+>SEQ_1
+MEDLNVVDSINGAGSWLVANQALLLSYAVNIVAALAIIIVGLIIARMISNAVNRLMISRK
+
+>SEQ_2
+EPVRRNEFIIGVAYDSDIDQVKQILTNIIQSEDRILKDREMTVRLNELGASSINFVVRVW
+EOT
+
+
+python -m b2bTools --dynamine --disomine --efoldmine --agmata \
+    --input_file ./input_example.fasta \
+    --output_json_file ./input_example.fasta.json \
+    --output_tabular_file ./input_example.fasta.csv \
+    --metadata_file ./input_example.fasta.meta.csv
+status=$?
+
+# Check if the status code is non-zero and exit with an error code
+if [ $status -ne 0 ]; then
+    echo "Error: b2bTools command failed with status code $status"
+    exit $status
+fi
+
+# Validate the presence and content of output files
+files=("input_example.fasta.json" "input_example.fasta.csv" "input_example.fasta.meta.csv")
+for file in "${files[@]}"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: Output file $file not found"
+        exit 1
+    fi
+
+    head "$file"
+
+    # Check if the file is a CSV and has more than one row
+    if [[ "$file" == *.csv ]]; then
+        row_count=$(wc -l < "$file")
+        if [ $row_count -le 1 ]; then
+            echo "Error: Output file $file is a CSV with less than two rows"
+            exit 1
+        fi
+    fi
+
+    # Cleanup: remove input and output files
+    if [ -f "$file" ]; then
+        rm "$file"
+    fi
+done
 
 rm ./input_example.fasta
+echo "Testing b2bTools for Single Seq mode finished with success"
 
-cat ./results_b2b.json
+echo "3) Testing b2bTools for MSA mode"
 
-rm ./results_b2b.json
+cat <<EOT >> ./small_alignment.clustal
+CLUSTAL O(1.2.4) multiple sequence alignment
 
-echo ""
-echo Test finished
+
+SEQ_1      -VYVGNLGNNG	10
+SEQ_2      RVRCGCLTRG-	10
+            *  * * ..
+EOT
+
+python -m b2bTools \
+  --mode msa \
+  --dynamine --disomine --efoldmine --agmata \
+  --input_file ./small_alignment.clustal \
+  --output_json_file ./small_alignment.clustal.json \
+  --output_tabular_file ./small_alignment.clustal.csv \
+  --metadata_file ./small_alignment.clustal.meta.csv \
+  --distribution_json_file ./small_alignment.clustal.distrib.json \
+  --distribution_tabular_file ./small_alignment.clustal.distrib.csv
+
+status=$?
+
+# Check if the status code is non-zero and exit with an error code
+if [ $status -ne 0 ]; then
+    echo "Error: b2bTools command failed with status code $status"
+    exit $status
+fi
+
+# Validate the presence and content of output files
+files=("small_alignment.clustal.json" "small_alignment.clustal.csv" "small_alignment.clustal.meta.csv" "small_alignment.clustal.distrib.json" "small_alignment.clustal.distrib.csv")
+for file in "${files[@]}"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: Output file $file not found"
+        exit 1
+    fi
+
+    head "$file"
+
+    # Check if the file is a CSV and has more than one row
+    if [[ "$file" == *.csv ]]; then
+        row_count=$(wc -l < "$file")
+        if [ $row_count -le 1 ]; then
+            echo "Error: Output file $file is a CSV with less than two rows"
+            exit 1
+        fi
+    fi
+
+    # Cleanup: remove input and output files
+    if [ -f "$file" ]; then
+        rm "$file"
+    fi
+done
+
+rm ./small_alignment.clustal
+echo "Testing b2bTools for Single Seq mode finished with success"
