40abb27ce8971d46371550871fc5b80cfa1c1957
Sergey Koren <sergek@umd.edu>
2023-07-11 10:57:33 -0400

    debug

--- recipes/graphaligner/build.sh
+++ recipes/graphaligner/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 cd $SRC_DIR
+ls -la $PREFIX/lib/libbost*
+echo $LIBRARY_PATH
+export LIBRARY_PATH=$LIBRARY_PATH:$PREFFIX/lib
+echo $LIBRARY_PATH
 make bin/GraphAligner
 cp bin/GraphAligner $PREFIX/bin
