4397b695882f2c5ee88dc9d99066f1abda67e335
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-07-12 15:05:55 -0500

    Update ratatosk to 0.9.0

--- recipes/ratatosk/build.sh
+++ recipes/ratatosk/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -x
 
-git clone https://github.com/pmelsted/bifrost.git
-cd bifrost
-git checkout 9e6f948ce2c27de32bc687502e777aebc9eab53d
-cd ..
-mv bifrost/* Bifrost/.
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-mkdir -p $PREFIX/bin
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make
-make install
+mkdir -p "${PREFIX}/bin"
+
+cmake -S . -B build \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
+
+cmake --build build/ --target install -j 4 -v
