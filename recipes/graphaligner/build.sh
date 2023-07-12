5dc77ea20abb6b789d413cae35758ce5f2eefeb5
Sergey Koren <sergek@umd.edu>
2023-07-12 09:29:34 -0400

    remove error log and debug ls/echo

--- recipes/graphaligner/build.sh
+++ recipes/graphaligner/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 cd $SRC_DIR
-ls -la $PREFIX/lib/
-echo $LIBRARY_PATH
-export LIBRARY_PATH=$PREFIX/lib
-echo $LIBRARY_PATH
 make bin/GraphAligner
 cp bin/GraphAligner $PREFIX/bin
