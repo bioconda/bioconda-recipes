a844392b17e5a1a5f964b0c3b57a697c4869568a
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-07-12 17:15:53 -0500

    edit cmake options

--- recipes/ratatosk/build.sh
+++ recipes/ratatosk/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -x
 
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
+
+if [ `uname` == Darwin ]; then
+	# See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk for -D_LIBCPP_DISABLE_AVAILABILITY
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+	export CONFIG_ARGS="-DBUILD_SHARED_LIBS=ON -DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+	else
+	export CONFIG_ARGS="-DBUILD_SHARED_LIBS=ON"
+fi
 
 mkdir -p "${PREFIX}/bin"
 
@@ -12,6 +22,7 @@ cmake -S . -B build \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_INSTALL_LIBDIR=lib "${CONFIG_ARGS}"
 
-cmake --build build/ --target install -j 4 -v
+cmake --build build/ --target install -j "${CPU_COUNT}"
