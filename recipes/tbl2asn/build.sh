076697b62c99e56ffb5fea68b9bfc58636fea00b
Jason Stajich <jasonstajich.phd@gmail.com>
2023-07-16 15:18:38 -0700

    simplify to same filenames

--- recipes/tbl2asn/build.sh
+++ recipes/tbl2asn/build.sh
@@ -3,9 +3,5 @@ set -e
 
 gunzip *tbl2asn*.gz 
 mkdir -p ${PREFIX}/bin
-if [ -f tbl2asn.mac ]; then
-	cp tbl2asn.mac ${PREFIX}/bin/tbl2asn
-else
-	cp *.tbl2asn ${PREFIX}/bin/tbl2asn
-fi
+cp tbl2asn.* ${PREFIX}/bin/tbl2asn
 chmod a+x ${PREFIX}/bin/tbl2asn
