5f82eac34365a0262568509d5189d432724c7ccb
Austin Hovland <austin_hovland@vrtx.com>
2023-07-17 10:43:11 -0400

    perf: update pear to v0.9.11

--- recipes/pear/build.sh
+++ recipes/pear/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xeuo pipefail
-./configure --prefix=$PREFIX LDFLAGS="$(pkg-config --libs zlib)" CFLAGS="$(pkg-config --cflags zlib)"
+./configure --prefix=$PREFIX
 make
 make install
 ln -s $PREFIX/bin/pear $PREFIX/bin/pearRM
