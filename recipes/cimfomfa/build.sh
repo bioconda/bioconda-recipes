3b316632fe5e4141201fbaa2847f64c10fbed247
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2023-07-18 16:07:21 +0200

    Remove share directory

--- recipes/cimfomfa/build.sh
+++ recipes/cimfomfa/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
-mkdir -p $PREFIX/share
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CXXFLAGS} -fcommon"
 ./configure --prefix=$PREFIX --disable-shared
