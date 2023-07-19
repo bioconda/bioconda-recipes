47bbc3a24f732cb833be805a021496330b6d42c5
gongyh <gongyh@qibebt.ac.cn>
2023-07-19 12:14:51 +0800

    patch python script to find the correct python interpreter

--- recipes/referee/build.sh
+++ recipes/referee/build.sh
@@ -3,4 +3,5 @@
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/lib
 cp -r ${SRC_DIR} $PREFIX/lib/referee
+sed -i 's#/usr/bin/python#/usr/bin/env python#g' $PREFIX/lib/referee/referee.py
 ln -s $PREFIX/lib/referee/referee.py $PREFIX/bin/referee.py
