62c360a05c708e6d5da07f96262269357307da90
raufs <salamzader@gmail.com>
2023-07-24 23:41:58 +0000

    add recipe

---
+++ recipes/skDER/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PYTHON -m pip install . --ignore-installed --no-deps -vv
+
+mkdir -p ${PREFIX}/bin
+
+# (re)-compile RBH/InParanoid-esque programs written in C++
+${CXX} -std=c++11 -o ${PREFIX}/bin/skDERcore skDERcore.cpp
+chmod +x ${PREFIX}/bin/skDERcore
