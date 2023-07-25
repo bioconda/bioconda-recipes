1b1f6456e4f1a0b16ad850b6535a46210ae30ff6
Rauf Salamzade <salamzader@gmail.com>
2023-07-25 05:10:03 -0500

    Update build.sh

--- recipes/skder/build.sh
+++ recipes/skder/build.sh
@@ -4,6 +4,6 @@ $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 mkdir -p ${PREFIX}/bin
 
-# (re)-compile RBH/InParanoid-esque programs written in C++
+# (re)-compile programs written in C++
 ${CXX} -std=c++11 -o ${PREFIX}/bin/skDERcore skDERcore.cpp
 chmod +x ${PREFIX}/bin/skDERcore
