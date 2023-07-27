ea552b07ffca809f68fff2768c5535b0fb8758ff
sparrow <alevar@gmail.com>
2023-07-27 02:00:24 -0400

    update meta

--- recipes/orfanage/build.sh
+++ recipes/orfanage/build.sh
@@ -10,6 +10,6 @@
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DORFANAGE_BUILD_LIBBIGWIG=OFF
 make
 make install
