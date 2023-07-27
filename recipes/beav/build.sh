205ae03ae58c483176eb17dd53f8a1065922c827
Alexandra Weisberg <weisbeal@onid.oregonstate.edu>
2023-07-27 10:23:50 -0700

    fixed cp

--- recipes/beav/build.sh
+++ recipes/beav/build.sh
@@ -24,7 +24,7 @@ mkdir $BEAV_DIR/software/PaperBLAST/bin/blast
 #TIGER
 curl -v -L -O https://github.com/sandialabs/TIGER/archive/refs/tags/TIGER2.1.tar.gz
 tar xzf TIGER2.1.tar.gz --exclude 'TIGER-TIGER2.1/db/Pfam-A.hmm' --exclude 'TIGER-TIGER2.1/bin/aragorn*' --exclude 'TIGER-TIGER2.1/bin/hmmsearch' --exclude 'TIGER-TIGER2.1/bin/pfscan'
-patch -p 0 -d ./ < scripts/tiger.patch
+patch -p 0 -d ./ < $BEAV_DIR/scripts/tiger.patch
 mv TIGER-TIGER2.1 $BEAV_DIR/software/TIGER
 rm TIGER2.1.tar.gz
 
