6312cf9bf246a8f4f09bae0c10249f9400b3d81d
Robert A. Petit III <robbie.petit@gmail.com>
2023-07-31 16:12:28 -0600

    Use recipe includes

--- recipes/conifer/build.sh
+++ recipes/conifer/build.sh
@@ -2,32 +2,28 @@
 
 # Build conifer
 ${CC} -std=c99 -Wall -Wextra -O3 -D_POSIX_C_SOURCE=200809L \
-    -I $SRC_DIR/include \
-    -I $SRC_DIR/include/zlib \
+    -I $PREFIX/include \
     -I third_party/uthash/src \
     -I . src/utils.c src/kraken_stats.c src/kraken_taxo.c src/main.c \
     -o conifer -lz -lm
 
 # Build is_a_parent_of_b
 ${CC} -std=c99 -Wall -O3 -D_POSIX_C_SOURCE=200809L \
-    -I $SRC_DIR/include \
-    -I $SRC_DIR/include/zlib \
+    -I $PREFIX/include \
     -I third_party/uthash/src \
     -I . src/kraken_stats.c src/kraken_taxo.c utils/is_a_parent_of_b.c \
     -o is_a_parent_of_b -lm
     
 # Build show_ancestors
 ${CC} -std=c99 -Wall -O3 -D_POSIX_C_SOURCE=200809L \
-    -I $SRC_DIR/include \
-    -I $SRC_DIR/include/zlib \
+    -I $PREFIX/include \
     -I third_party/uthash/src \
     -I . src/kraken_stats.c src/kraken_taxo.c utils/show_ancestors.c \
     -o show_ancestors -lm
 
 # Build taxid_name
 ${CC} -std=c99 -Wall -O3 -D_POSIX_C_SOURCE=200809L \
-    -I $SRC_DIR/include \
-    -I $SRC_DIR/include/zlib \
+    -I $PREFIX/include \
     -I third_party/uthash/src \
     -I . src/kraken_stats.c src/kraken_taxo.c utils/taxid_name.c \
     -o taxid_name -lm
