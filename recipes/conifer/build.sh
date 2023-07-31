5954d74f0f0fec789df15f1c34c5de9e83611d4c
Ícaro M. S. Castro <icaromscastro@gmail.com>
2023-07-31 17:45:55 -0300

    Adding thirdy party libraries to recipe

--- recipes/conifer/build.sh
+++ recipes/conifer/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+mv third_party/Unity-5e9acef74faffba86375258d822f49d0b1173b5e third_party/Unity
+mv third_party/uthash-8e67ced1d1c5bd8141c542a22630e6de78aa6b90 third_party/uthash
 make
 mkdir -p $PREFIX/bin
 mv conifer $PREFIX/bin
