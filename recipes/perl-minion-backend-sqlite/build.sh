d7637cc27fcb32b2308a5b12f6e4771adfa7c68e
Khaled196 <56037954+khaled196@users.noreply.github.com>
2023-08-08 17:20:27 +0100

    Update build.sh

--- recipes/perl-minion-backend-sqlite/build.sh
+++ recipes/perl-minion-backend-sqlite/build.sh
@@ -5,12 +5,12 @@ set -o errexit -o pipefail
 find="v{{ version }}"
 replace="{{ version }}"
 
-sed 's/$find/$replace/g' $BUILD_PREFIX/*
+sed 's/$find/$replace/g' $BUILD_PREFIX/*.*
 sed 's/$find/$replace/g' $BUILD_PREFIX/lib/Minion/Backend/SQLite.pm
 
 perl Build.PL INSTALLDIRS=site \
     INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
-sed 's/$find/$replace/g' $BUILD_PREFIX/*
+sed 's/$find/$replace/g' $BUILD_PREFIX/*.*
 perl ./Build
 perl ./Build test
 perl ./Build install --installdirs site
