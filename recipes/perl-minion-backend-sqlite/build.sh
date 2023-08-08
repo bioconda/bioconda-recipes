853d7e5efb4472780628262ab6dad516cff32874
Khaled196 <56037954+khaled196@users.noreply.github.com>
2023-08-08 18:34:26 +0100

    Update build.sh

--- recipes/perl-minion-backend-sqlite/build.sh
+++ recipes/perl-minion-backend-sqlite/build.sh
@@ -2,12 +2,9 @@
 
 set -o errexit -o pipefail
 
-find="v5.0.7"
-replace="5.0.7"
-
-sed 's/$find/$replace/g' $SRC_DIR/META.json
-sed 's/$find/$replace/g' $SRC_DIR/Build.PL
-sed 's/$find/$replace/g' $SRC_DIR/lib/Minion/Backend/SQLite.pm
+sed -i 's/v5.0.7/5.0.7/g' $SRC_DIR/META.json
+sed -i 's/v5.0.7/5.0.7/g' $SRC_DIR/Build.PL
+sed -i 's/v5.0.7/5.0.7/g' $SRC_DIR/lib/Minion/Backend/SQLite.pm
 
 
 echo $find
@@ -15,7 +12,7 @@ echo $replace
 
 perl Build.PL INSTALLDIRS=site \
     INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
-sed 's/$find/$replace/g' $SRC_DIR/Build
+sed -i 's/v5.0.7/5.0.7/g' $SRC_DIR/Build
 perl ./Build
 perl ./Build test
 # Make sure this goes in site
