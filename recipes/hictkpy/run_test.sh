46520d3f323e511a2a44f27a208aa185c319bcd4
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-08-09 12:56:43 +0200

    Debugging

--- recipes/hictkpy/run_test.sh
+++ recipes/hictkpy/run_test.sh
@@ -2,10 +2,10 @@
 
 "$PYTHON" -m site
 
-ls -lah /opt/conda/conda-bld/hictkpy*
-ls -lah /opt/conda/conda-bld/hictkpy*/_test_env*
-ls -lah /opt/conda/conda-bld/hictkpy*/_test_env*/lib/python3.10/site-packages
-ls -lah /opt/conda/conda-bld/hictkpy*/_test_env*/lib/python3.10/site-packages/hictkpy
+# ls -lah /opt/conda/conda-bld/hictkpy*
+# ls -lah /opt/conda/conda-bld/hictkpy*/_test_env*
+# ls -lah /opt/conda/conda-bld/hictkpy*/_test_env*/lib/python3.10/site-packages
+# ls -lah /opt/conda/conda-bld/hictkpy*/_test_env*/lib/python3.10/site-packages/hictkpy
 
 "$PYTHON" -c 'import hictkpy; print(hictkpy.__version__)' || true
 
