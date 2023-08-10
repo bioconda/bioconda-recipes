25e811cd6dafec62171d30561f50cab9fdcd67eb
Sergey Koren <sergek@umd.edu>
2023-08-10 11:39:41 -0400

    Fix OSX zcat error, reduce OSX test size

--- recipes/verkko/run_test.sh
+++ recipes/verkko/run_test.sh
@@ -23,11 +23,13 @@ curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_hifi_subset24x.fastq.gz -
 curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_ont_subset50x.fastq.gz -o ont.fastq.gz
 
 # now start tests
+ONT="--nano ./ont.fastq.gz"
 if [ "$(uname)" == "Darwin" ]; then
-   verkko -d asm --no-correction --hifi ./hifi.fastq.gz
-else
-   verkko -d asm --no-correction --hifi ./hifi.fastq.gz --nano ./ont.fastq.gz
+   ONT=""
 fi
+
+verkko -d asm --no-correction --hifi ./hifi.fastq.gz $ONT
+
 if [ -s asm/assembly.fasta ]; then 
    python $PREFIX/lib/verkko/scripts/circularize_ctgs.py -p 10 -f 0.01 -o asm/assembly_circular.fasta --min-ovl 1000 asm/assembly.fasta
 fi
@@ -38,20 +40,21 @@ if [ ! -s asm/assembly_circular.fasta ]; then
    exit 1
 fi
 
-# test Hi-C
-verkko -d asm --hifi hifi.fastq.gz  --nano ont.fastq.gz --hic1 hifi.fastq.gz --hic2 hifi.fastq.gz
+# test HiC but not on darwin to save time
+if [ "$(uname)" != "Darwin" ]; then
+   verkko -d asm --hifi hifi.fastq.gz $ONT --hic1 hifi.fastq.gz --hic2 hifi.fastq.gz
 
-if [ ! -e asm/assembly.haplotype1.fasta ]; then
-   echo "Error: verkko hic assembly test failed!"
-   tail -n +1 `find asm -name *.err`
-   exit 1
+   if [ ! -e asm/assembly.haplotype1.fasta ]; then
+      echo "Error: verkko hic assembly test failed!"
+      tail -n +1 `find asm -name *.err`
+      exit 1
+   fi
 fi
-
 #now test trio
 rm -rf asm/8-* asm/6-layoutContigs/ asm/7-consensus/ asm/assembly.* asm/6-rukki/
 $PREFIX/lib/verkko/bin/meryl count compress k=21 memory=4 threads=8 output empty1.meryl hifi.fastq.gz
 $PREFIX/lib/verkko/bin/meryl greater-than 100 empty1.meryl/ output empty2.meryl
-verkko -d asm --hifi hifi.fastq.gz  --nano ont.fastq.gz --hap-kmers empty1.meryl empty2.meryl trio
+verkko -d asm --hifi hifi.fastq.gz  $ONT --hap-kmers empty1.meryl empty2.meryl trio
 
 if [ ! -s asm/assembly.haplotype1.fasta ]; then
    echo "Error: verkko trio assembly test failed!"
