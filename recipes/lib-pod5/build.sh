e5ee54e37d63f42bc30dcf002bd4c8d2b0545cc4
Jannes Spangenberg <janspang@gmx.de>
2023-08-14 15:49:21 +0200

    Try to install from downloaded source

--- recipes/lib-pod5/build.sh
+++ recipes/lib-pod5/build.sh
@@ -1,3 +1,3 @@
 VERSION="0.2.4"
 PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3]; print("{0}{1}".format(*version))'`
-pip install https://files.pythonhosted.org/packages/cp$PYTHON_VERSION/l/lib_pod5/lib_pod5-$VERSION-cp$PYTHON_VERSION-cp$PYTHON_VERSION-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+pip install wheels/lib_pod5-$VERSION-cp$PYTHON_VERSION-cp$PYTHON_VERSION-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
