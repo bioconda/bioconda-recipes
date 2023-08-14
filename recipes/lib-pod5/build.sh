23987a4f3ed69cbcf26c3d6121e77e7a6eeda2d1
Jannes Spangenberg <janspang@gmx.de>
2023-08-14 16:37:26 +0200

    Try to install from downloaded source

--- recipes/lib-pod5/build.sh
+++ recipes/lib-pod5/build.sh
@@ -1,3 +1,1 @@
-VERSION="0.2.4"
-PYTHON_VERSION=`python -c 'import sys; version=sys.version_info[:3]; print("{0}{1}".format(*version))'`
-pip install wheels/lib_pod5-$VERSION-cp$PYTHON_VERSION-cp$PYTHON_VERSION-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+pip install .
