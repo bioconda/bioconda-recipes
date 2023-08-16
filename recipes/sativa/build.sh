d212c4975a372c5d042c51b5900cb294f7041091
Alexey Kozlov <alexey.kozlov@h-its.org>
2023-08-16 19:44:51 +0200

    add sativa 11

--- recipes/sativa/build.sh
+++ recipes/sativa/build.sh
@@ -3,6 +3,12 @@
 export USE_AVX=yes
 export USE_AVX2=yes
 
+case `uname` in
+Darwin) export DARWIN=1;;
+Linux) export DARWIN=0;;
+*) echo "Unknown architecture"; exit 1;;
+esac
+
 make -C ./raxml CC="$CC"
 
 install -d ${PREFIX}/tmp
