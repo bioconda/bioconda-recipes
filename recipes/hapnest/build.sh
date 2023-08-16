693fb87b5785623e656ce5c17b45de71ad9c5148
msantorsola <ma.santorsola@gmail.com>
2023-08-16 16:48:18 +0200

    added hapnest build.sh

---
+++ recipes/hapnest/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cp run_program.jl $PREFIX/bin/hapnest
+
+
+julia -e 'Pkg.add("ArgParse")'
+julia -e 'Pkg.add("CSV")'
+julia -e 'Pkg.add("CategoricalArrays")'
+julia -e 'Pkg.add("Conda")'
+julia -e 'Pkg.add("DataFrames")'
+julia -e 'Pkg.add("DelimitedFiles")'
+julia -e 'Pkg.add("Distances")'
+julia -e 'Pkg.add("Distributions")'
+julia -e 'Pkg.add("GpABC")'
+julia -e 'Pkg.add("Impute")'
+julia -e 'Pkg.add("LsqFit")'
+julia -e 'Pkg.add("MendelPlots")'
+julia -e 'Pkg.add("Mmap")'
+julia -e 'Pkg.add("Plots")'
+julia -e 'Pkg.add("Printf")'
+julia -e 'Pkg.add("ProgressMeter")'
+julia -e 'Pkg.add("PyCall")'
+julia -e 'Pkg.add("StatsBase")'
+julia -e 'Pkg.add("StatsPlots")'
+julia -e 'Pkg.add("YAML")'
+julia -e 'Conda.add("bed-reader")' 
+
