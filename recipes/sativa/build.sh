7cef6fdcde8a6f2ea5de80035d01c027a3895f02
Alexey Kozlov <alexey.kozlov@h-its.org>
2023-08-16 16:19:14 +0200

    add sativa 6

--- recipes/sativa/build.sh
+++ recipes/sativa/build.sh
@@ -3,11 +3,7 @@
 export USE_AVX=yes
 export USE_AVX2=yes
 
-shopt -s expand_aliases
-alias gcc=$GCC
-alias
-
-make -C ./raxml CC=$CC
+make -C ./raxml CC="$CC"
 
 install -d ${PREFIX}/tmp
 install -t ${PREFIX} *.py ./raxml/raxmlHPC8* ./raxml/*.sh ./epac/*
