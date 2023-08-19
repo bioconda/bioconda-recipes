11c42fd9d775633c5041ac4b20d99b837aed9d89
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-19 09:32:26 -0400

    Attempt to rework build to use gcc instead of clang on macOS

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -eux
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
-if [ $(uname) == "Darwin" ]; then
-    export LDFLAGS="-headerpad_max_install_names ${LDFLAGS}"
-fi
-
-sed -e "/^CC=/d" Makefile > Makefile.new
-mv Makefile.new Makefile
-make CC=${CC} LIBRARY_PATH=${PREFIX}/lib prefix=${PREFIX}
-make test CC=${CC}
-make install prefix=${PREFIX}
-# The binaries are versioned for some reason
-mv ${PREFIX}/bin/sambamba-* ${PREFIX}/bin/sambamba
+export CC="$CC"
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+make release
+# Note that checks can fail on Apple Silicon.
+make check
+# The 'make install' step doesn't work correctly on macOS currently.
+# > make install prefix="${PREFIX}"
+mkdir -pv "${PREFIX}/bin"
+# The binary contains version number, for some reason.
+cp "bin/sambamba-"* "${PREFIX}/bin/sambamba"
