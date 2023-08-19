a5816de396662b84a3b41b077937bc9e74ef254c
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-19 17:49:42 -0400

    Attemp to fix compiler detection

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -4,7 +4,9 @@ set -eux
 export CC="$CC"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-make release
+sed -e "/^CC=/d" Makefile > Makefile.new
+mv Makefile.new Makefile
+make CC="$CC" LIBRARY_PATH="$LIBRARY_PATH" release
 # Note that checks can fail on Apple Silicon.
 make check
 # The 'make install' step doesn't work correctly on macOS currently.
