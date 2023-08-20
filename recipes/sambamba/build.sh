84d051031d0d18ab67449c6348f914e1fe3a0a72
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-20 11:57:51 -0400

    Attempt to force gcc instead of clang on macOS

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 set -eux
 
-# Can we get this value automatically from 'meta.yaml'?
-VERSION='1.0.1'
 export CC="$CC"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
+export VERSION='1.0.1'
+
 sed -e "/^CC=/d" Makefile > Makefile.new
 mv Makefile.new Makefile
 make CC="$CC" LIBRARY_PATH="$LIBRARY_PATH" release
-# Note that checks can fail on Apple Silicon.
 make check
-# The 'make install' step doesn't work correctly on macOS currently.
-# > make install prefix="${PREFIX}"
 mkdir -pv "${PREFIX}/bin"
 cp "bin/sambamba-${VERSION}" --target-directory="${PREFIX}/bin"
-# The binary contains version number, for some reason.
 (
     cd "${PREFIX}/bin"
     ln -s "sambamba-${VERSION}" 'sambamba'
