b8c9789e0dd55a8444005481e122fb8d00088b85
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-21 08:11:42 -0400

    Rework clang support

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -6,8 +6,14 @@ export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export VERSION='1.0.1'
 
-sed -e "/^CC=/d" Makefile > Makefile.new
-mv Makefile.new Makefile
+sed -e "/^CC=/d" 'Makefile' > 'Makefile.new'
+mv 'Makefile.new' 'Makefile'
+if [[ "$(uname)" == 'Darwin' ]]
+then
+    # Clang doesn't support '-flto=full'.
+    sed -e "/^LDFLAGS/d" 'Makefile' > 'Makefile.new'
+    unset -v LDFLAGS
+fi
 make CC="$CC" LIBRARY_PATH="$LIBRARY_PATH" release
 make check
 mkdir -pv "${PREFIX}/bin"
