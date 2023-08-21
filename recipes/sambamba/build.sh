332ed10fa837c24ad1b055823502913f0499d1bc
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-21 14:06:28 -0400

    Attempt to fix prefix weirdness using a variable

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -eux
 
+version='1.0.1'
+
 export CC="$CC"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export VERSION='1.0.1'
 
 sed -e "/^CC=/d" 'Makefile' > 'Makefile.new'
 mv 'Makefile.new' 'Makefile'
 if [[ "$(uname)" == 'Darwin' ]]
 then
-    ldflags_bak="$LDFLAGS"
     # Clang doesn't support '-flto=full'.
     sed -e "/^LDFLAGS/d" 'Makefile' > 'Makefile.new'
     mv 'Makefile.new' 'Makefile'
@@ -19,13 +19,10 @@ then
 fi
 make CC="$CC" LIBRARY_PATH="$LIBRARY_PATH" release
 make check
-if [[ "$(uname)" == 'Darwin' ]]
-then
-    LDFLAGS="$ldflags_bak"
-fi
-mkdir -pv "${PREFIX}/bin"
-cp "bin/sambamba-${VERSION}" --target-directory="${PREFIX}/bin"
+bin_dir="${PREFIX}/bin"
+mkdir -pv "$bin_dir"
+cp "bin/sambamba-${version}" --target-directory="$bin_dir"
 (
-    cd "${PREFIX}/bin"
-    ln -s "sambamba-${VERSION}" 'sambamba'
+    cd "$bin_dir"
+    ln -s "sambamba-${version}" 'sambamba'
 )
