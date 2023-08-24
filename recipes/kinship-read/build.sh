809f7a9abb56c82b8dd7dafb50489617bf19b949
Thiseas Christos Lamnidis <thisseass@gmail.com>
2023-08-24 16:52:31 +0200

    patch shebang

--- recipes/kinship-read/build.sh
+++ recipes/kinship-read/build.sh
@@ -6,5 +6,6 @@ mkdir -p $outdir
 mkdir -p $PREFIX/bin
 cp -R * $outdir/ 
 ls -l $outdir
+sed -i.bak '1 s|^.*$|#!/usr/bin/env python|g' $outdir/READ.py
 ln -s $outdir/READ.py $PREFIX/bin/kinship-read
 chmod 0755 ${PREFIX}/bin/kinship-read
