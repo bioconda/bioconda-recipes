3d439eb5b4382706931fad661b14fb15e046ad8c
subrata-codeons <subrata.icgc@gmail.com>
2023-08-25 11:17:59 +0530

    Fixed build issues

--- recipes/mapinsights/build.sh
+++ recipes/mapinsights/build.sh
@@ -3,10 +3,6 @@
   curl -SL https://github.com/subrata-codeons/mapinsights/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mapinsights-latest.tar.gz
   tar -xzf mapinsights-latest.tar.gz
   cd mapinsights-${PKG_VERSION}
-  echo "${PKG_VERSION}"
-  echo "I am in desire path : $PWD"
-  ls bgzf.c
-  gcc --version
   make
  
  mkdir -p "${PREFIX}/bin"
