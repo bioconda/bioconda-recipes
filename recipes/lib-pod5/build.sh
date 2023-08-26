c070663690c3ccf7aded2f5ec005220c6f35ad76
Devon Ryan <dpryan79@users.noreply.github.com>
2023-08-26 18:35:25 +0200

    Update build.sh

--- recipes/lib-pod5/build.sh
+++ recipes/lib-pod5/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -ex
+
 $PYTHON -m setuptools_scm
 $PYTHON -m pod5_make_version
 
 mkdir build
 cd build
-conan install --build=missing -s build_type=Release .. 
+conan install --build=missing -s build_type=Release -s compiler=$CXX .. 
 cmake -DENABLE_CONAN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make -j
