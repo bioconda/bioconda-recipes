c1218d0520a91bc08475a5cafb43ae285b2fe754
Devon Ryan <dpryan79@users.noreply.github.com>
2023-08-28 15:43:44 +0200

    debug

--- recipes/wfmash/build.sh
+++ recipes/wfmash/build.sh
@@ -9,6 +9,8 @@ export CXX_INCLUDE_PATH=${PREFIX}/include
 sed -i 's/-march=native/-march=sandybridge/g' src/common/wflign/deps/WFA2-lib/Makefile
 cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
 cmake --build build
+# Libraries aren't getting installed
+find . -name "libwfa2cpp" -ls
 mkdir -p $PREFIX/bin
 mv build/bin/* $PREFIX/bin
 mv scripts/split_approx_mappings_in_chunks.py $PREFIX/bin
