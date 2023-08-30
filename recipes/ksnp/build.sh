270e66001a3153fcf3cd8113ec6add0851976429
i-xiaohu <631751599@qq.com>
2023-08-30 10:42:21 +0800

    ksnp recipe

--- recipes/ksnp/build.sh
+++ recipes/ksnp/build.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-set -euo pipefail
-
-mkdir build; cd build; cmake ..; make; cd ..
-
-mkdir -p ${PREFIX}/bin
-cp build/ksnp ${PREFIX}/bin
+#!/bin/bashset -euo pipefailmkdir build; cd build; cmake ..; make; cd ..mkdir -p ${PREFIX}/bincp build/ksnp ${PREFIX}/bin
