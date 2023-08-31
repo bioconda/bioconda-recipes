4982b05188d7a8276a95fe76bb5567099e34f040
Devon Ryan <dpryan79@users.noreply.github.com>
2023-08-31 12:11:41 +0200

    Update build.sh

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -10,11 +10,19 @@ ln -s $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod +x $PREFIX/bin/mentalist
 
 julia -e 'Pkg.init()'
-#julia -e 'Pkg.add("Bio")'
-#julia -e 'Pkg.add("OpenGene")'
-#julia -e 'Pkg.add("Logging")'
-#julia -e 'Pkg.add("Lumberjack")'
-julia -e 'Pkg.add([ "Distributed", "ArgParse", "BioSequences", "JSON", "DataStructures", "JLD", "GZip", "Blosc", "FileIO", "TextWrap", "LightXML", "JuMP", "Gurobi"])'
+julia -e 'Pkg.add("Distributed")'
+julia -e 'Pkg.add("ArgParse")'
+julia -e 'Pkg.add("BioSequences")'
+julia -e 'Pkg.add("JSON")'
+julia -e 'Pkg.add("DataStructures")'
+julia -e 'Pkg.add("JLD")'
+julia -e 'Pkg.add("GZip")'
+julia -e 'Pkg.add("Blosc")'
+julia -e 'Pkg.add("FileIO")'
+julia -e 'Pkg.add("TextWrap")'
+julia -e 'Pkg.add("LightXML")'
+julia -e 'Pkg.add("JuMP")'
+julia -e 'Pkg.add("Gurobi")'
 
 rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
