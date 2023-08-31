dba84a11d8eba0c6faceeadb5ee29dca13414368
Devon Ryan <devon.ryan@genedata.com>
2023-08-31 11:28:07 +0200

    Add missing dependencies

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -10,14 +10,11 @@ ln -s $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod +x $PREFIX/bin/mentalist
 
 julia -e 'Pkg.init()'
-julia -e 'Pkg.add("ArgParse")'
-julia -e 'Pkg.add("Bio")'
-julia -e 'Pkg.add("OpenGene")'
-julia -e 'Pkg.add("Logging")'
-julia -e 'Pkg.add("Lumberjack")'
-julia -e 'Pkg.add("FastaIO")'
-julia -e 'Pkg.add("JLD")'
-julia -e 'Pkg.add("DataStructures")'
+#julia -e 'Pkg.add("Bio")'
+#julia -e 'Pkg.add("OpenGene")'
+#julia -e 'Pkg.add("Logging")'
+#julia -e 'Pkg.add("Lumberjack")'
+julia -e 'using Pkg; Pkg.add([ "Distributed", "ArgParse", "BioSequences", "JSON", "DataStructures", "JLD", "GZip", "Blosc", "FileIO", "TextWrap", "LightXML", "JuMP", "Gurobi"])'
 
 rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
