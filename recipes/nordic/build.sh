ae93fffbfe6b7c24bd36bbf8180db6e9c8a37e80
REDA Clemence <clemence.reda@inria.fr>
2023-09-01 09:34:53 +0200

    add building step (use conda-build idiomatic approach)

--- recipes/nordic/build.sh
+++ recipes/nordic/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-wget https://github.com/sysbio-curie/MaBoSS-env-2.0/archive/refs/heads/master.tar.gz
-tar xvfz MaBoSS-2.0.tar.gz
 cd MaBoSS-2.0/src
 make install \
     CC="${CC}" \
