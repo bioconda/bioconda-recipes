843f84f24b92e18f8845e80ac5fd42b0eb2e4be2
Devon Ryan <dpryan79@users.noreply.github.com>
2023-09-02 16:50:03 +0200

    Update build.sh

--- recipes/pgr-tk/build.sh
+++ recipes/pgr-tk/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+## TODO: using environmental variables in the libwfa makefile in the future. 
+## The current compiler names are hard-coded in the makefile, so we need these symbolic links
+ln -sf $CC $PREFIX/bin/gcc
+ln -sf $CXX $PREFIX/bin/g++
+export PATH=$PREFIX/bin:$PATH
+
 cd pgr-tk
 bash build.sh
