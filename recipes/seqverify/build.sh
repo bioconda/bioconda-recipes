eb550aed0ce22920a3fcbc5ec960f349438722e4
vpepe <valerio.pepe@gmail.com>
2023-09-06 10:43:53 -0400

    Update build.sh

--- recipes/seqverify/build.sh
+++ recipes/seqverify/build.sh
@@ -5,4 +5,6 @@ mkdir $PREFIX/bin
 chmod +x seqverify
 cp seqverify $PREFIX/bin
 cp seqver_functions.py $PREFIX/bin
-cp seqver_transgenes.py $PREFIX/bin
+cp seqver_plots.py $PREFIX/bin
+cp seqver_lofFinder.py $PREFIX/bin
+cp seqver_genomeupdate.py $PREFIX/bin
