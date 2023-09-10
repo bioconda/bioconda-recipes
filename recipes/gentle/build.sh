a36cb78815afee9f2f67776f6135f51369370f3c
Steffen Möller <steffen_moeller@gmx.de>
2023-09-11 00:17:30 +0200

    gentle: Update recipes/gentle/build.sh
    Removal of early debug output.
    
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>

--- recipes/gentle/build.sh
+++ recipes/gentle/build.sh
@@ -5,25 +5,7 @@ libtoolize --automake --force --copy
 automake --add-missing
 autoconf
 mkdir -p ${PREFIX}
-echo "CXX: $CXX"
-if [ "x$CXX" = "x" ]; then
-  #export CXX="g++"
-  echo "CXX found empty, set to '$CXX'"
-fi
-echo "CXXFLAGS: ${CXXFLAGS}"
-echo "LDFLAGS: ${LDFLAGS}"
-echo "PREFIX: ${PREFIX}"
-echo "PATH: ${PATH}"
-echo "which g++:"
-if ! which g++ > /dev/null; then
-  echo "g++ not in path"
-  #echo " installing it via conda"
-  #conda install gxx
-fi
-echo "which gcc:"
-which gcc || echo "gcc not in path"
-echo "which clang:"
-which clang || echo "clang not in path"
+
 ./configure --prefix=$PREFIX CXXFLAGS="-I${PREFIX}/include"
 make
 make install
