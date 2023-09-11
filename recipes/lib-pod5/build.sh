ca97accf5099def54d40f5543318e410dc4c792a
Devon Ryan <dpryan79@users.noreply.github.com>
2023-09-11 10:12:14 +0200

    Update build.sh

--- recipes/lib-pod5/build.sh
+++ recipes/lib-pod5/build.sh
@@ -6,6 +6,14 @@ $PYTHON -m pod5_make_version
 
 mkdir build
 cd build
+
+# conan apparently requires actual binary names like gcc, which is really unfortunate
+ln -s $CC $PREFIX/bin/gcc
+ln -s $CXX $PREFIX/bin/gxx
+
 conan install --build=missing -s build_type=Release -s compiler=$CXX .. 
 cmake -DENABLE_CONAN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make -j
+
+unlink $PREFIX/bin/gcc
+unlink $PREFIX/bin/gxx
