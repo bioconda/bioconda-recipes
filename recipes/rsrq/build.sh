fcb4e784b99f5df1f2e3cebaf7323b33999f085e
Aaron Mussig <aaronmussig@gmail.com>
2023-09-11 14:18:09 +1000

    add rsrq

--- recipes/rsrq/build.sh
+++ recipes/rsrq/build.sh
@@ -1,3 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash -euo
+export CFLAGS="${CFLAGS} -fcommon"
+export CXXFLAGS="${CFLAGS} -fcommon"
 
-cargo install --no-track --verbose --root "${PREFIX}" --path .
+# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
+# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
+export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
+
+# build statically linked binary with Rust
+RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path .
