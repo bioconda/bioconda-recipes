9d7ce5381d786ad2bff0c734a033a509cd0124f3
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-09-12 13:47:44 +0200

    Version bump

--- recipes/ngsngs/build.sh
+++ recipes/ngsngs/build.sh
@@ -5,5 +5,5 @@ make clean
 make CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" prefix="${PREFIX}" CC="${CC}" CXX="${CXX} -L${PREFIX}/lib" LDFLAGS+="-lz -lpthread ${LDFLAGS} -L${PREFIX}/lib"
 mv ./ngsngs "${PREFIX}"/bin/
 
-make --directory misc CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" prefix="${PREFIX}" CC="${CC}" CXX="${CXX} -L${PREFIX}/lib" LDFLAGS+="-lz -lpthread ${LDFLAGS} -L${PREFIX}/lib" HTSSRC="systemwide"
+make --directory misc CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" prefix="${PREFIX}" CC="${CC}" CXX="${CXX} -L${PREFIX}/lib" LDFLAGS+="-lz -lpthread ${LDFLAGS} -L${PREFIX}/lib"
 mv ./misc/LenConvert  ./misc/MetaMisConvert  ./misc/MisConvert  ./misc/QualConvert  ./misc/RandRef  ./misc/RandVar  ./misc/VCFtoBED "${PREFIX}"/bin/
