2b4dae66e4fdbe883a4c91dad7bcc029fe8f45d1
Lightning Auriga <lightning.auriga@gmail.com>
2023-09-13 13:44:57 -0400

    feat: update expansionhunter to v5.0.0
    patches are restructured and augmented to match v5.0.0 conventions. cmake build process is adjusted
    to rely on conda dependencies. assorted patches handle updates required for more modern versions of
    upstream libraries, with the exception of abseil, which is changed enough that the patches would be
    fairly substantial, and is thus pinned for build.

--- recipes/expansionhunter/build.sh
+++ recipes/expansionhunter/build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-
-set -eu -o pipefail
-
-mkdir -p build
+mkdir build
 cd build
-
-export BOOST_ROOT=${PREFIX}
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
 cmake ..
-make CXX="$CXX" CC="$CC" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
-
-mv ExpansionHunter ${PREFIX}/bin/ExpansionHunter
+make
+mkdir -p $PREFIX/bin
+cp install/bin/ExpansionHunter $PREFIX/bin
+mkdir -p $PREFIX/share/ExpansionHunter/variant_catalog
+cp ../variant_catalog/hg38/variant_catalog.json $PREFIX/share/ExpansionHunter/variant_catalog/variant_catalog_GRCh38.json
