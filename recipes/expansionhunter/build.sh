f710b5909171f7339a9c69cf6c446d320d5ca764
Lightning Auriga <lightning.auriga@gmail.com>
2023-09-15 14:59:32 -0400

    feat: add all variant catalogs

--- recipes/expansionhunter/build.sh
+++ recipes/expansionhunter/build.sh
@@ -5,5 +5,5 @@ cmake ..
 make
 mkdir -p $PREFIX/bin
 cp install/bin/ExpansionHunter $PREFIX/bin
-mkdir -p $PREFIX/share/ExpansionHunter/variant_catalog
-cp ../variant_catalog/hg38/variant_catalog.json $PREFIX/share/ExpansionHunter/variant_catalog/variant_catalog_GRCh38.json
+mkdir -p $PREFIX/share/ExpansionHunter
+cp -R ../variant_catalog $PREFIX/share/ExpansionHunter
