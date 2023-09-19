c329e000ba79f989bb75d524c0791d6b2e202a33
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-09-19 12:48:56 +0200

    Fix missing HTSSRC

--- recipes/metadmg/build.sh
+++ recipes/metadmg/build.sh
@@ -2,5 +2,5 @@
 mkdir -p "${PREFIX}"/bin
 
 make clean
-make CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" prefix="${PREFIX}" CC="${CC}" CXX="${CXX} -L${PREFIX}/lib" LDFLAGS+="-lz -lbz2 -llzma ${LDFLAGS} -L${PREFIX}/lib"
-mv ./metadamage "${PREFIX}"/bin/
+make CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" prefix="${PREFIX}" CC="${CC}" CXX="${CXX} -L${PREFIX}/lib" LDFLAGS+="-lz -lbz2 -llzma ${LDFLAGS} -L${PREFIX}/lib" HTSSRC="systemwide"
+mv ./metaDMG-cpp ./misc/compressbam "${PREFIX}"/bin/
