b32d87829125eedb93cf429339551b756bf8e49a
alienzj <alienchuj@gmail.com>
2023-09-19 12:21:47 +0800

    add GT-Pro

---
+++ recipes/gt-pro/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export CPATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+
+make -j $CPU_COUNT
+
+
+outdir=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+mkdir -p ${outdir}
+mkdir -p ${PREFIX}/bin
+
+cp -r bin ${outdir}/
+cp -r scripts ${outdir}/
+cp GT_Pro ${outdir}/
+
+chmod -R 755 ${outdir}/bin/
+chmod -R 755 ${outdir}/scripts/
+chmod -R 755 ${outdir}/GT_Pro
+
+ln -s ${outdir}/GT_Pro ${PREFIX}/bin/GT_Pro
