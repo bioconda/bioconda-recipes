969ee58392a22c1072061938e162bd8a919db905
dirkjanvw <dirk-janvanworkum@hotmail.nl>
2023-09-22 13:02:05 +0200

    some minor fixes to bash specific things in earlgrey

--- recipes/earlgrey/build.sh
+++ recipes/earlgrey/build.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #Based on https://github.com/TobyBaril/EarlGrey/blob/main/configureForDocker
 
-
 # Put package in share directory
 PACKAGE_HOME=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
 mkdir -p ${PACKAGE_HOME}
 cp -r * ${PACKAGE_HOME}/
 
 
+# Add bash shebang line to earlGrey and make sure help doesn't exit 1
+sed -i.bak "1s|^|#!/bin/bash\n|" ${PACKAGE_HOME}/earlGrey
+sed -i.bak "s|usage; exit 1|usage; exit 0|g" ${PACKAGE_HOME}/earlGrey
+
+
 # Fix SCRIPT_DIR
 SCRIPT_DIR="${PACKAGE_HOME}/scripts/"
-sed -i.bak "s|usage; exit 1|usage; exit 0|g" ${PACKAGE_HOME}/earlGrey
 sed -i.bak "s|SCRIPT_DIR=.*|SCRIPT_DIR=${SCRIPT_DIR}|g" ${PACKAGE_HOME}/earlGrey
 sed -i.bak "s|SCRIPT_DIR=.*|SCRIPT_DIR=${SCRIPT_DIR}|g" ${PACKAGE_HOME}/scripts/rcMergeRepeat*
 sed -i.bak "s|SCRIPT_DIR=.*|SCRIPT_DIR=${SCRIPT_DIR}|g" ${PACKAGE_HOME}/scripts/headSwap.sh
 sed -i.bak "s|SCRIPT_DIR=.*|SCRIPT_DIR=${SCRIPT_DIR}|g" ${PACKAGE_HOME}/scripts/autoPie.sh
 sed -i.bak "s|INSERT_FILENAME_HERE|${SCRIPT_DIR}/TEstrainer/scripts/|g" ${PACKAGE_HOME}/scripts/TEstrainer/TEstrainer_for_earlGrey.sh
 
+
+# Set permissions to files
 chmod +x ${PACKAGE_HOME}/earlGrey
 chmod +x ${PACKAGE_HOME}/modules/trf409.linux64
 chmod +x ${SCRIPT_DIR}/TEstrainer/TEstrainer_for_earlGrey.sh
@@ -24,6 +29,8 @@ chmod +x ${SCRIPT_DIR}/* > /dev/null 2>&1
 chmod +x ${SCRIPT_DIR}/bin/LTR_FINDER.x86_64-1.0.7/ltr_finder
 chmod a+w ${SCRIPT_DIR}/repeatCraft/example
 
+
+# Extract tRNAdb
 tar -zxf ${SCRIPT_DIR}/bin/LTR_FINDER.x86_64-1.0.7/tRNAdb.tar.gz --directory ${SCRIPT_DIR}/bin/LTR_FINDER.x86_64-1.0.7
 
 
@@ -31,3 +38,4 @@ tar -zxf ${SCRIPT_DIR}/bin/LTR_FINDER.x86_64-1.0.7/tRNAdb.tar.gz --directory ${S
 mkdir -p ${PREFIX}/bin
 cd ${PREFIX}/bin
 ln -s ${PACKAGE_HOME}/earlGrey .
+
