92068fc80102d797d3eff5374e3b5af56e098e71
James Holt <matt@Jamess-MacBook-Air.local>
2023-09-24 18:51:45 -0500

    path seems to exist, testing if something changes line to line

--- recipes/hificnv/build.sh
+++ recipes/hificnv/build.sh
@@ -3,6 +3,5 @@
 mkdir -p "${PREFIX}"/bin
 # bioconda auto-extract for us apparently
 # tar -xzf hificnv-*.tar.gz
-ls -l . ${PREFIX}
-md5sum -c hificnv.md5
-cp hificnv "${PREFIX}"/bin/
+ls -l ./hificnv* && md5sum -c ./hificnv.md5
+cp ./hificnv "${PREFIX}"/bin/
