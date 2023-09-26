8c97d39a2e4af57b66c2329243f311e234a7bdfa
yhhan19 <yhhan@umd.edu>
2023-09-26 13:09:46 -0400

    build error fixed

--- recipes/treeqmc/build.sh
+++ recipes/treeqmc/build.sh
@@ -1,7 +1,5 @@
-cd MQLib
 make
-cd ..
-g++ -std=c++11 -O2 -I MQLib/include -o TREE-QMC src/*.cpp MQLib/bin/MQLib.a
+g++ -std=c++11 -O2 -I include -o TREE-QMC src-tqmc/*.cpp bin/MQLib.a
 mkdir -p $PREFIX/bin
 cp TREE-QMC $PREFIX/bin/
 chmod a+x $PREFIX/bin/TREE-QMC
