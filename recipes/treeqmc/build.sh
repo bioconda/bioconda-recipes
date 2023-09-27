2aad932044014c8537768390b247d9ca23a78f9e
yhhan19 <yhhan@umd.edu>
2023-09-27 17:43:48 -0400

    osx compilation fix

--- recipes/treeqmc/build.sh
+++ recipes/treeqmc/build.sh
@@ -1,9 +1,9 @@
 cd MQLib
 make \
-    GXX="${GXX}" \
+    GXX="${CXX}" \
     AR="${AR}"
 cd ..
-$GXX -std=c++11 -O2 -I MQLib/include -o TREE-QMC src/*.cpp MQLib/bin/MQLib.a
+$CXX -std=c++11 -O2 -I MQLib/include -o TREE-QMC src/*.cpp MQLib/bin/MQLib.a
 mkdir -p $PREFIX/bin
 cp TREE-QMC $PREFIX/bin/
 chmod a+x $PREFIX/bin/TREE-QMC
