1c37f5cacd5d445fa206552b14d53973bd05993d
Junyan Dai <jdai1234@terpmail.umd.edu>
2023-09-27 08:36:42 -0400

    4

--- recipes/dollo-cdp/build.sh
+++ recipes/dollo-cdp/build.sh
@@ -3,7 +3,8 @@ ls *.cpp
 #make \
 #	GXX="${GXX}" \
 chmod +x $CXX
-$CXX -DUSE_SHRT -std=c++11 -O3 -funroll-loops -fomit-frame-pointer -I boost_1_80_0 -o dollo-cdp ./driver.cpp ./binary_character_matrix.cpp
+#$CXX -DUSE_SHRT -std=c++11 -O3 -funroll-loops -fomit-frame-pointer -I boost_1_80_0 -o dollo-cdp ./driver.cpp ./binary_character_matrix.cpp
+make GXX = "${CXX}"
 mkdir -p $PREFIX/bin
 cp dollo-cdp $PREFIX/bin/dollo-cdp
 chmod +x $PREFIX/bin/dollo-cdp
