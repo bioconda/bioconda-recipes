cff34c1a03ed097dde5ff6a3db09f1198df81649
yhhan19 <yhhan@umd.edu>
2023-09-27 16:57:56 -0400

    build.sh fix

--- recipes/treeqmc/build.sh
+++ recipes/treeqmc/build.sh
@@ -3,7 +3,7 @@ make \
     GXX="${GXX}" \
     AR="${AR}"
 cd ..
-g++ -std=c++11 -O2 -I MQLib/include -o TREE-QMC src/*.cpp MQLib/bin/MQLib.a
+$GXX -std=c++11 -O2 -I MQLib/include -o TREE-QMC src/*.cpp MQLib/bin/MQLib.a
 mkdir -p $PREFIX/bin
 cp TREE-QMC $PREFIX/bin/
 chmod a+x $PREFIX/bin/TREE-QMC
