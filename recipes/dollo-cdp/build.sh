ca6773b5c92fd4823d5cd48bfab158ca924a4fec
Junyan Dai <jdai1234@terpmail.umd.edu>
2023-09-27 09:46:12 -0400

    linux error

--- recipes/dollo-cdp/build.sh
+++ recipes/dollo-cdp/build.sh
@@ -4,7 +4,7 @@ ls *.cpp
 #	GXX="${GXX}" \
 chmod +x $CXX
 #$CXX -DUSE_SHRT -std=c++11 -O3 -funroll-loops -fomit-frame-pointer -I boost_1_80_0 -o dollo-cdp ./driver.cpp ./binary_character_matrix.cpp
-make GXX = "${CXX}"
+make GXX="${CXX}"
 mkdir -p $PREFIX/bin
 cp dollo-cdp $PREFIX/bin/dollo-cdp
 chmod +x $PREFIX/bin/dollo-cdp
