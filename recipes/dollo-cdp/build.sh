ea99af37c8c12fbc1d57439e2a13185c868a8856
Junyan Dai <jdai1234@terpmail.umd.edu>
2023-09-27 06:41:53 -0400

    g++ not found 2

--- recipes/dollo-cdp/build.sh
+++ recipes/dollo-cdp/build.sh
@@ -1,7 +1,11 @@
 cd ./src
-make \
-	GXX="${CXX}" \
-	AR="${AR}"
+#make \
+#	GXX="${CXX}" \
+#	AR="${AR}"
+
+$CXX -DUSE_SHRT -std=c++11 -O3 -funroll-loops -fomit-frame-pointer -static -I \ 
+	boost_1_80_0 -o dollo-cdp driver.cpp binary_character_matrix.cpp
+
 mkdir -p $PREFIX/bin
 cp dollo-cdp $PREFIX/bin/dollo-cdp
 chmod +x $PREFIX/bin/dollo-cdp
