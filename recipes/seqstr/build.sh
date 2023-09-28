a30f9ed8ec7f68bdcc48828374c589dc21fb1d5a
Chenlai Shi <33204827+shichenlai@users.noreply.github.com>
2023-09-28 12:47:56 -0500

    Add files via upload

--- recipes/seqstr/build.sh
+++ recipes/seqstr/build.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+GENOME_DIR="./"
+
+config_file_path="$HOME/.seqstr.config"
+if [ ! -f "$config_file_path" ]; then
+    touch "$config_file_path"
+fi
+
+echo "GENOME_DIR=$GENOME_DIR" > "$config_file_path"
+
+conda install -c conda-forge -y python=3.8 requests pyfaidx
+
+python setup.py install
+
+mkdir -p "$PREFIX/bin"
+cp "$SRC_DIR/seqstr/CLI_seqstr" "$PREFIX/bin/"
+chmod +x "$PREFIX/bin/CLI_seqstr"
