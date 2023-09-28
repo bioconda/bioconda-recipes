5c9bd80bdf894ce378d29c5201a29d80345b3625
xinehc <xinehc@gmail.com>
2023-09-29 01:18:44 +0800

    update ice-cream recipe

--- recipes/ice-cream/build.sh
+++ recipes/ice-cream/build.sh
@@ -2,8 +2,7 @@
 set -eu -o pipefail
 
 mkdir -p ${PREFIX}/bin
-cp ICEcream.sh ${PREFIX}/bin/ICEcream.sh
-cp ICEfamily_refer/familytools/plotting_script.py ${PREFIX}/bin/plotting_script.py
+cp -r * ${PREFIX}/bin
 
 chmod 0755 "${PREFIX}/bin/plotting_script.py"
 chmod 0755 "${PREFIX}/bin/ICEcream.sh"
