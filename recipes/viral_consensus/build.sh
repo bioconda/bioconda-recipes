ca8c0979b79738b404698d9145b3fea0fe48b354
Niema Moshiri <niemamoshiri@gmail.com>
2023-09-28 14:54:09 -0700

    Patched git submodule issue

--- recipes/viral_consensus/build.sh
+++ recipes/viral_consensus/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i 's/git submodule/#git submodule/g' Makefile
+git clone --recurse-submodules https://github.com/samtools/htslib.git
 make
 if [ ! -d "$PREFIX/bin" ]; then
     mkdir $PREFIX/bin;
