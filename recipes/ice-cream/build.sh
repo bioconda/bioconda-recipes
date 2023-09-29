629fb51a4ee403fa428d4ae1d214876f0fdc09dd
xinehc <xinehc@gmail.com>
2023-09-29 11:29:18 +0800

    make cp files explicit

--- recipes/ice-cream/build.sh
+++ recipes/ice-cream/build.sh
@@ -6,11 +6,14 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
 
-cp -r * $outdir
-cp ICEfamily_refer/familytools/plotting_script.py $outdir/plotting_script.py
+cp -r ICEfamily_refer $outdir/ICEfamily_refer
+cp -r data $outdir/data
+cp -r scripts $outdir/scripts
+cp ICEfinder_modified4_yxl.pl $outdir/ICEfinder_modified4_yxl.pl
+cp ICEcream.sh $outdir/ICEcream.sh
 
-chmod 0755 $outdir/plotting_script.py
+chmod 0755 $outdir/ICEfamily_refer/familytools/plotting_script.py
 chmod 0755 $outdir/ICEcream.sh
 
-ln -s $outdir/plotting_script.py $PREFIX/bin
+ln -s $outdir/ICEfamily_refer/familytools/plotting_script.py $PREFIX/bin
 ln -s $outdir/ICEcream.sh $PREFIX/bin
