3525577ac1ee8d41e70861eb11e78b67ef351906
Niema Moshiri <niemamoshiri@gmail.com>
2023-09-28 21:16:41 -0700

    Attempt to fix ld issue

--- recipes/viral_consensus/build.sh
+++ recipes/viral_consensus/build.sh
@@ -2,21 +2,20 @@
 
 # pull htslib submodule (and its submodules) manually since ViralConsensus tarball isn't git repo
 sed -i.bak 's/git submodule/#git submodule/g' Makefile
+sed -i.bak 's/make /make INCLUDES="-I$PREFIX\/include" /g' Makefile
 git clone --recurse-submodules https://github.com/samtools/htslib.git
 
 # htslib compilation calls gcc, so link bioconda's gcc/g++
 ln -s $CC ${PREFIX}/bin/gcc
 ln -s $CXX ${PREFIX}/bin/g++
 
-# htslib compilation needs zlib and other libraries, so specify bioconda's libraries
+# update environment variables
 export CFLAGS="$CFLAGS -I$PREFIX/include -fpermissive"
+export CPPFLAGS="-fpermissive"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-
-# need fpermissive because ViralConsensus is C++ but htslib is C and uses implicit pointer conversion
-export CPPFLAGS="-fpermissive"
 
 # compile ViralConsensus
 make CXX="${CXX}" CXXFLAGS+='-fpermissive'
