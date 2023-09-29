00bd21df97b15ebaa65e942a52fa1370d203ca89
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2023-09-29 18:20:22 +0200

    minor

--- recipes/assemblycomparator2/build.sh
+++ recipes/assemblycomparator2/build.sh
@@ -11,6 +11,18 @@ cp -r assets profiles report_subpipeline scripts tests $PREFIX
 # I'm not sure if I should pull the docker image so it is available in the cache?
 #apptainer pull docker://cmkobel/assemblycomparator2:v2.5.5
 
+# How do I save the necessary system variables?
+
+# Added since last run:
+# I hope these variables are accessible when the user activates the environment that runs ac2.
+ASSCOM2_BASE=$PREFIX
+ASSCOM2_PROFILE=${ASSCOM2_BASE}/profiles/apptainer/local
+ASSCOM2_DATABASES=${ASSCOM2_BASE}/databases # The user should override this in their .bashrc if they want to use a different dir.
+
+# I have to think about this one: the launcher is already activated right.
+#echo "alias asscom2='conda run --live-stream --name asscom2_launcher snakemake --snakefile \${ASSCOM2_BASE}/snakefile --profile \${ASSCOM2_PROFILE} --configfile \${ASSCOM2_BASE}/config.yaml'" >> ~/.bashrc
+# I think it is better to use backslash-sentinel, because then the user can change the variables after having activated the PREFIX environment, and the changes will have effect
+alias asscom2='snakemake --snakefile \${ASSCOM2_BASE}/snakefile --profile \${ASSCOM2_PROFILE} --configfile \${ASSCOM2_BASE}/config.yaml'
 
 # Add more build steps here, if they are necessary.
 
