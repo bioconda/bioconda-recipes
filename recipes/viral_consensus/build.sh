d8173d09abdefc911108e49921bd4d22dfbf5b6c
Niema Moshiri <niemamoshiri@gmail.com>
2023-09-29 09:51:44 -0700

    Attempt to fix for Mac OS X

--- recipes/viral_consensus/build.sh
+++ recipes/viral_consensus/build.sh
@@ -2,10 +2,12 @@
 
 # update environment variables
 export CFLAGS="$CFLAGS -I$PREFIX/include"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
+export LIBRARY_PATH=${PREFIX}/lib
 
 # compile ViralConsensus
 make CC=$CC CXX="${CXX}"
