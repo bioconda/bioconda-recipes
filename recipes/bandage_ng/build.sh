eb416befbecbb8b10b0ee3a2eb428fc7665fca77
Björn Grüning <bjoern@gruenings.eu>
2023-09-29 23:48:41 +0200

    Update build.sh

--- recipes/bandage_ng/build.sh
+++ recipes/bandage_ng/build.sh
@@ -4,7 +4,13 @@ set -ex
 mkdir -p build && cd build
 cmake \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS} \
+    -D__STDC_FORMAT_MACROS" \
+    -DEGL_INCLUDE_DIR:PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/include \
+    -DEGL_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libEGL.so.1 \
+    -DOPENGL_egl_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libEGL.so.1 \
+    -DEGL_opengl_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so \
+    -DOPENGL_opengl_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so \
     ..
 make
 
