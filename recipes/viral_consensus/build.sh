7df12a2e9f83864cfe31fb8b3f3dc71a0948a45e
Niema Moshiri <niemamoshiri@gmail.com>
2023-09-30 14:21:02 -0700

    Update make command to hopefully fix Mac OS X issue

--- recipes/viral_consensus/build.sh
+++ recipes/viral_consensus/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make CC=$CC CXX="${CXX}" LDFLAGS="$LDFLAGS -L$PREFIX/lib" LIBPATH="-L${PREFIX}/lib" LIBRARY_PATH="${PREFIX}/lib"
+make CC="$CC" CXX="$CXX" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS"
 mkdir -p $PREFIX/bin
 cp viral_consensus $PREFIX/bin/
