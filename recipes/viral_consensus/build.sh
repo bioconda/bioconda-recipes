549307c8664eecbe4ef7cde96a2a0c7de8f789e3
Niema Moshiri <niemamoshiri@gmail.com>
2023-09-29 17:38:14 -0700

    Attempt to fix ld issue after cleaning

--- recipes/viral_consensus/build.sh
+++ recipes/viral_consensus/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make CC=$CC CXX="${CXX}"
+make CC=$CC CXX="${CXX}" LDFLAGS="$LDFLAGS -L$PREFIX/lib" LIBPATH="-L${PREFIX}/lib" LIBRARY_PATH="${PREFIX}/lib"
 mkdir -p $PREFIX/bin
 cp viral_consensus $PREFIX/bin/
