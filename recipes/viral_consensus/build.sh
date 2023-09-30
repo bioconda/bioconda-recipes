3f83e0525a4d26f0fc453b06d72bf9618dbb25d1
Niema Moshiri <niemamoshiri@gmail.com>
2023-09-29 17:00:19 -0700

    Updated based on feedback (simplified build.sh and meta.yaml)

--- recipes/viral_consensus/build.sh
+++ recipes/viral_consensus/build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-# update environment variables
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export INCLUDES="-I${PREFIX}/include"
-export LIBPATH="-L${PREFIX}/lib"
-export LIBRARY_PATH=${PREFIX}/lib
-
-# compile ViralConsensus
 make CC=$CC CXX="${CXX}"
-if [ ! -d "$PREFIX/bin" ]; then
-    mkdir $PREFIX/bin;
-    export PATH=$PREFIX/bin:$PATH;
-fi
+mkdir -p $PREFIX/bin
 cp viral_consensus $PREFIX/bin/
