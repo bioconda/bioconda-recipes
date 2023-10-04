4ffd93ed37769feafdbd2fd4ebf1ed9122f017a3
xiangyang1984 <lixiangyang@fudan.edu.cn>
2023-10-05 02:18:01 +0800

    add perl-child recipes

---
+++ recipes/perl-child/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# If it has Build.PL use that, otherwise use Makefile.PL
+set -x -e
+
+RM_DIR=${PREFIX}/share/perl-child
+mkdir -p ${RM_DIR}
+cp -r perl-child-0.013/* ${RM_DIR}
+
+    cpan -i ExtUtils::MakeMaker
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    make test
+    make install
