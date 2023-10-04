9cda4780999aba00ad2790fd727bb735060fc306
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2023-10-04 12:59:51 -0400

    Add sharg, the argument parser for bio-c++ tools from the seqan3 library

---
+++ recipes/sharg/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "cmake_minimum_required(VERSION 3.14)
+project(sharg NONE)
+include (\${SHARG_CLONE_DIR}/build_system/sharg-install.cmake)" > CMakeLists.txt
+
+cmake -DSHARG_CLONE_DIR="${SRC_DIR}" \
+      -DSHARG_INCLUDE_DIR="${SRC_DIR}/include" \
+      -DSHARG_SUBMODULES_DIR="${SRC_DIR}" \
+      -DSHARG_DEPENDENCY_INCLUDE_DIRS="${SRC_DIR}/submodules/tool_description_lib/src" \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" .
+
+make install
