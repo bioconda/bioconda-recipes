65f01818790a31b0ce76765e556defd250c765b8
Xiangyang1984 <58356520+Xiangyang1984@users.noreply.github.com>
2023-10-05 01:21:45 +0800

    Update build.sh

--- recipes/perl-parallel-runner/build.sh
+++ recipes/perl-parallel-runner/build.sh
@@ -2,7 +2,7 @@
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 set -x -e
-
+conda config --add channels xiangyang1984
 RM_DIR=${PREFIX}/share/perl-parallel-runner
 mkdir -p ${RM_DIR}
 cp -r ${SRC_DIR}/perl-parallel-runner-0.013/* ${RM_DIR}
