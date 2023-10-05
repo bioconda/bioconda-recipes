2e7f7de3fcdcd45617fdbe2763c01e4354cf2e74
Junyan Dai <97316565+junyandai@users.noreply.github.com>
2023-10-04 23:28:16 -0400

    Update build.sh

--- recipes/dollo-cdp/build.sh
+++ recipes/dollo-cdp/build.sh
@@ -4,7 +4,7 @@ ls *.cpp
 #	GXX="${GXX}" \
 chmod +x ${CXX}
 #${CXX} -DUSE_SHRT -std=c++11 -O3 -funroll-loops -fomit-frame-pointer -I ./boost_1_80_0 -o dollo-cdp ./driver.cpp ./binary_character_matrix.cpp
-${CXX} -DUSE_SHRT -std=c++11 -O3 -I ./boost_1_80_0 -o dollo-cdp ./driver.cpp ./binary_character_matrix.cpp
+${CXX} -DUSE_SHRT -std=c++11 -I ./boost_1_80_0 -o dollo-cdp ./driver.cpp ./binary_character_matrix.cpp
 #make GXX="${CXX}"
 mkdir -p $PREFIX/bin
 cp dollo-cdp $PREFIX/bin/dollo-cdp
