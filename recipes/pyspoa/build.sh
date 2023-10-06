9a7c09c2ab78b8035f81365dae07461501e40af8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2023-10-06 09:00:59 -0500

    Update pyspoa to 0.2.1 (#43503)
    * Update pyspoa to 0.2.1
    
    * try pypi source
    
    * remove make command
    
    * edit license_file

--- recipes/pyspoa/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
-make CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3" \
-	INCLUDES="-I${PREFIX}/include" \
-	LIBPATH="-L${PREFIX}/lib" -j4 build
-${PYTHON} -m pip install . --use-pep517 --no-deps -vvv
