666808c9e914fff168f3e39856ea3edbe6ad1d15
REDA Clemence <clemence.reda@inria.fr>
2023-10-07 20:18:01 +0200

    fix build.sh

--- recipes/nordic/build.sh
+++ recipes/nordic/build.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+echo "Step #1"
+pwd
 cd ${SRC_DIR}"/MaBoSS-2.0/"
+echo "Step #2"
+ls
 FILE=$(ls | grep "master*.tar.gz")
+echo "Step #3"
+echo $FILE
 unzip $FILE
+echo "Step #4"
+ls
 cd MaBoSS-env-2.0-master/engine/src
+echo "Step #5"
+ls
 make install \
     CC="${CC}" \
     CXX="${CXX}" \
