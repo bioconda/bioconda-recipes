5eae0a427625e73039ff20ba9cd88d87bcba2ad9
REDA Clemence <clemence.reda@inria.fr>
2023-10-09 20:50:29 +0200

    identified root folder

--- recipes/nordic/build.sh
+++ recipes/nordic/build.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Step #1"
-pwd
-cd ${SRC_DIR}"/MaBoSS-2.0/"
-echo "Step #2"
-ls
-FILE=$(ls | grep "master*.tar.gz")
-echo "Step #3"
-echo $FILE
-unzip $FILE
-echo "Step #4"
-ls
-cd MaBoSS-env-2.0-master/engine/src
-echo "Step #5"
-ls
+cd ${SRC_DIR}"/MaBoSS-2.0/engine/src/"
 make install \
     CC="${CC}" \
     CXX="${CXX}" \
