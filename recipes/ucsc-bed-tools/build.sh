aaeb165a4d6b3aade0f9756a0e15178a817a04a8
Mitchell R. Vollger <mrvollger@gmail.com>
2023-10-14 15:30:18 -0700

    chore: update.

--- recipes/ucsc-bed-tools/build.sh
+++ recipes/ucsc-bed-tools/build.sh
@@ -5,17 +5,18 @@ mkdir -p "$BINDIR"
 ls
 
 if [[ ${target_platform} =~ linux.* ]]; then
-    rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ $BINDIR/
+    SRC="rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/"
 elif [[ ${target_platform} =~ osx.* ]]; then
-    rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/macOSX.x86_64/ $BINDIR/
+    SRC="rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/macOSX.x86_64/"
 fi
 
 #cp -r $BINDIR/* "$PREFIX/bin/."
-pushd $BINDIR
-chmod +x *
-ls *
-mv "bed*" "$PREFIX/bin/."
-mv "*Bed*" "$PREFIX/bin/."
-mv "*wig*" "$PREFIX/bin/."
-mv "*Wig*" "$PREFIX/bin/."
-popd
+rync -ap \
+    --include='*/' --exclude='*' \
+    --include='*bed*' \
+    --include='*Bed*' \
+    --include='*wig*' \
+    --include='*Wig*' \
+    "$SRC" "$PREFIX/bin/."
+
+chmod +x "$PREFIX/bin/"*
