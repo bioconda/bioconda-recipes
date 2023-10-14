be7210a581b8a2e46f4f51b26f0c884e76362164
Mitchell R. Vollger <mrvollger@gmail.com>
2023-10-14 15:57:15 -0700

    chore: update.

--- recipes/ucsc-bed-tools/build.sh
+++ recipes/ucsc-bed-tools/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 mkdir -p "$PREFIX/bin"
-export BINDIR=$(pwd)/bin
-mkdir -p "$BINDIR"
-ls
 
 if [[ ${target_platform} =~ linux.* ]]; then
     SRC="rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/"
@@ -10,7 +7,6 @@ elif [[ ${target_platform} =~ osx.* ]]; then
     SRC="rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/macOSX.x86_64/"
 fi
 
-#cp -r $BINDIR/* "$PREFIX/bin/."
 rsync -ap \
     --include='*/' --exclude='*' \
     --include='*bed*' \
