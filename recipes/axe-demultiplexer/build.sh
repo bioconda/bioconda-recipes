eb00574832447863ea75f2516973423c23a57485
Dr. K.D. Murray <foss@kdmurray.id.au>
2023-10-21 14:23:48 +0200

    no python test on osx

--- recipes/axe-demultiplexer/build.sh
+++ recipes/axe-demultiplexer/build.sh
@@ -2,4 +2,10 @@
 
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DNO_OPENMP=yes
-make all test install
+if [ "$(uname)" == "Linux" ]
+then
+    make all test install
+else
+    # The python-based integration test expects linux so fails on OSX
+    make all install
+fi
