c85a5122c28165e9f33eb773b750128b68be675f
Mariangela Santorsola <santorsola@login01.eos.medooza.lan>
2023-10-23 13:08:26 +0200

    fixing path

--- recipes/gametes/build.sh
+++ recipes/gametes/build.sh
@@ -7,11 +7,12 @@ PACKAGE_HOME=$PREFIX/share/$PKG_NAME
 mkdir -p ${BINARY_HOME}
 mkdir -p ${PACKAGE_HOME}
 
+cd $SRC_DIR
 
 JAR_NAME=GAMETES_2.1.jar
 
 cp ${JAR_NAME} ${PACKAGE_HOME}/GAMETES_2.1.jar
-cp $RECIPE_DIR/gametes.py ${BINARY_HOME}
+cp ${RECIPE_DIR}/gametes.py ${BINARY_HOME}
 #ln -s ${PACKAGE_HOME}/gametes.py ${BINARY_HOME}/gametes
 
 chmod +x ${PACKAGE_HOME}
