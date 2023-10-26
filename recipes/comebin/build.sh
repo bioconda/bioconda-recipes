604e24671e1a131662a776b8b3ae9a7b7c9f975d
王子叶 <2630494503@qq.com>
2023-10-27 02:44:08 +0800

    add COMEBin

--- recipes/comebin/build.sh
+++ recipes/comebin/build.sh
@@ -3,6 +3,6 @@
 mkdir -p $PREFIX/bin/ 
 cp -r COMEBin $PREFIX/bin/
 cp -r auxiliary $PREFIX/bin/
+chmod a+x bin/run_comebin.sh
 cp bin/run_comebin.sh $PREFIX/bin/
-chmod a+x $PREFIX/bin/run_comebin.sh
 
