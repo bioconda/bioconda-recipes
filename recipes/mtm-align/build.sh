f8e24fc364d09aeeb91284f65be487dfdd47cf53
Leon Rauschning <rauschning@cip.ifi.lmu.de>
2023-10-26 17:42:13 +0200

    adjusted build.sh to make makefile use conda compiler

--- recipes/mtm-align/build.sh
+++ recipes/mtm-align/build.sh
@@ -2,7 +2,10 @@
 set -ex
 
 cd src/
-make
+# the makefile directly calls g++, and doesn't use env variables
+# use an alias to redirect to the correct compiler without changing the makefile
+alias "g++"=$CXX
+make # builds the package
 
 mkdir -p $PREFIX/bin
 mv mTM-align $PREFIX/bin/mtm-align
