1c5ec0f087a1eff96ba670a4b6a52f334325d7d9
REDA Clemence <clemence.reda@inria.fr>
2023-11-02 23:06:19 +0100

    fix bonesis mpbn error

--- recipes/nordic/build.sh
+++ recipes/nordic/build.sh
@@ -10,6 +10,4 @@ make install \
     LDFLAGS="${LDFLAGS}" \
     prefix="${PREFIX}"
 
-"${CONDA}" install -c colomoto bonesis
-"${CONDA}" install -c colomoto mpbn
 "${PYTHON}" -m pip install --no-deps --no-build-isolation . -vvv
