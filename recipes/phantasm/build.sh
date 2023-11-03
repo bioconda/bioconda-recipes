8ecbdebc3b3e6985c8d70d2fcb2598e05df6898a
dr-joe-wirth <dr.joe.wirth@gmail.com>
2023-11-03 09:27:54 -0400

    test passing

--- recipes/phantasm/build.sh
+++ recipes/phantasm/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# export variables
+# create variables for this build script
 export TEMP_FN=".tmp"
 export BIN_DIR=$PREFIX/bin
 export PHANTASM_DIR=$PREFIX/phantasm
 export PARAM_FN=$PHANTASM_DIR/param.py
+export ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 export PYVER=$(python -c 'import sys; v=[str(x) for x in sys.version_info[:]]; print(".".join(v[:2]))')
 
 # move phantasm files to the directory
@@ -26,3 +27,20 @@ tail -n 5 $PARAM_FN >> $TEMP_FN
 
 # overwrite param.py with the new file
 mv $TEMP_FN $PARAM_FN
+
+# modify PHANTASM_PY variable so it prints the correct usage
+sed -E "s/^(PHANTASM_PY = ).+$/\1'phantasm'/g" $PHANTASM_DIR/phantasm.py > $TEMP_FN
+mv $TEMP_FN $PHANTASM_DIR/phantasm.py
+
+# modify phantasm so it can be invoked directly from the command line
+echo "#! $(which python3)" | sed "s/ //g" > $TEMP_FN # sed is necessary; cannot say `!$`
+echo "" >> $TEMP_FN
+cat $PHANTASM_DIR/phantasm.py >> $TEMP_FN
+mv $TEMP_FN $PHANTASM_DIR/phantasm
+chmod a+x $PHANTASM_DIR/phantasm
+
+# add phantasm to the user's path
+mkdir -p $ACTIVATE_DIR
+echo "export PHANTASM_DIR=$PHANTASM_DIR" > $ACTIVATE_DIR/phantasm.sh
+echo 'export PATH=$PHANTASM_DIR:$PATH' >> $ACTIVATE_DIR/phantasm.sh
+chmod a+x $ACTIVATE_DIR/phantasm.sh
