3289915572f34c48fb17802127110d982cc3e8d1
Benjamin Linard <benjamin.linard@inrae.fr>
2023-11-09 17:53:09 +0100

    compilation works, now check test

--- recipes/ipk/build.sh
+++ recipes/ipk/build.sh
@@ -12,20 +12,15 @@ export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib
 
-echo "PREFIX:" ${PREFIX}
 cmake -DHASH_MAP=USE_TSL_ROBIN_MAP -DCMAKE_CXX_FLAGS="-O3" 
-echo "CMAKE PASSED"
-echo "SRC_DIR:" ${SRC_DIR}
-pwd
-ls .
-ls ..
-ls ${SRC_DIR}
 cmake --build . --target all
-ls .
 
 cp ipk/ipk-aa $PREFIX/bin
 cp ipk/ipk-aa-pos $PREFIX/bin
 cp ipk/ipk-dna $PREFIX/bin
 cp ipk.py $PREFIX/bin
 
-chmod +x $PREFIX/bin/ipk-*
+chmod +x $PREFIX/bin/ipk-aa
+chmod +x $PREFIX/bin/ipk-dna
+chmod +x $PREFIX/bin/ipk-aa-pos
+chmod +x $PREFIX/bin/ipk.py
