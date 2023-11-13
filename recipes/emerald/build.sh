50ee6ea006b42693b0226a6dfcdd4ddb48e63c26
Andreas Grigorjew <andreas.grigorjew@helsinki.fi>
2023-11-13 12:53:42 +0200

    [emerald] remove std::filesystem from code

--- recipes/emerald/build.sh
+++ recipes/emerald/build.sh
@@ -4,10 +4,6 @@ export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
-export CFLAGS="${CFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-[ "$unamestr" == 'Darwin' ] && export MACOSX_DEPLOYMENT_TARGET=10.15
-
 cmake .
 make
 
