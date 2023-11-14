9c96a11b22164952d7744c098297df5a4e288bdf
dr-joe-wirth <dr.joe.wirth@gmail.com>
2023-11-14 13:49:40 -0500

    trying a new strategy for providing a phantasm executable

--- recipes/phantasm/build.sh
+++ recipes/phantasm/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # create variables for this build script
+export PYVER=$(python3 --version|sed -E "s/^\S+ ([0-9]+\.[0-9]+)\..+$/\1/g")
 export TEMP_FN=".tmp"
 export BIN_DIR=$PREFIX/bin
-export PHANTASM_DIR=$PREFIX/phantasm
-export PARAM_FN=$PHANTASM_DIR/param.py
 export ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-export PYVER=$(python -c 'import sys; v=[str(x) for x in sys.version_info[:]]; print(".".join(v[:2]))')
+export PHANTASM_EXE=$BIN_DIR/phantasm
+export PHANTASM_DIR=$PREFIX/lib/python$PYVER/site-packages/phantasm
+export PARAM_FN=$PHANTASM_DIR/param.py
 
 # move phantasm files to the directory
 mkdir -p $PHANTASM_DIR
@@ -32,15 +33,36 @@ mv $TEMP_FN $PARAM_FN
 sed -E "s/^(PHANTASM_PY = ).+$/\1'phantasm'/g" $PHANTASM_DIR/phantasm.py > $TEMP_FN
 mv $TEMP_FN $PHANTASM_DIR/phantasm.py
 
-# modify phantasm so it can be invoked directly from the command line
-echo "#! $(which python3)" | sed "s/ //g" > $TEMP_FN # sed is necessary; cannot say `!$`
-echo "" >> $TEMP_FN
-cat $PHANTASM_DIR/phantasm.py >> $TEMP_FN
-mv $TEMP_FN $PHANTASM_DIR/phantasm
-chmod a+x $PHANTASM_DIR/phantasm
-
-# add phantasm to the user's path
+# make an activate script
 mkdir -p $ACTIVATE_DIR
-echo "export PHANTASM_DIR=$PHANTASM_DIR" > $ACTIVATE_DIR/phantasm.sh
-echo 'export PATH=$PHANTASM_DIR:$PATH' >> $ACTIVATE_DIR/phantasm.sh
-chmod a+x $ACTIVATE_DIR/phantasm.sh
+
+# export variables for the activate script
+echo "export BIN_DIR=$BIN_DIR" > $ACTIVATE_DIR/phantasm.sh
+echo "export LIB_DIR=$PREFIX/lib/python" >> $ACTIVATE_DIR/phantasm.sh
+echo 'export PYVER=$(python3 --version|sed -E "s/^\S+ ([0-9]+\.[0-9]+)\..+$/\1/g")' >> $ACTIVATE_DIR/phantasm.sh
+echo 'export PHANTASM_DIR=$LIB_DIR$PYVER/site-packages/phantasm' >> $ACTIVATE_DIR/phantasm.sh
+echo "export PHANTASM_EXE=$PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+
+# write instructions to create a phantasm executable
+echo 'echo "#! $(which python3)" | sed "s/ //g" > $PHANTASM_EXE' >> $ACTIVATE_DIR/phantasm.sh
+echo "echo 'import os, sys, subprocess' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo -n 'sys.path.append(' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo 'echo -n "$PHANTASM_DIR" >> $PHANTASM_EXE' >> $ACTIVATE_DIR/phantasm.sh
+echo "echo \"')\" >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo -n 'if __name__ == ' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo \"'__main__':\" >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo -n '    cmd = [' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo \"'python3', os.path.join('$PHANTASM_DIR', 'phantasm.py')]\" >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo '    cmd.extend(sys.argv[1:])' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo '    try:' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo '        subprocess.run(cmd, check=True)' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo '    except subprocess.CalledProcessError as e:' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "echo '        raise RuntimeError(e.stderr)' >> $PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
+echo "chmod a+x $PHANTASM_EXE"
+
+# remove the variables
+echo "unset BIN_DIR" >> $ACTIVATE_DIR/phantasm.sh
+echo "unset LIB_DIR" >> $ACTIVATE_DIR/phantasm.sh
+echo "unset PYVER" >> $ACTIVATE_DIR/phantasm.sh
+echo "unset PHANTASM_DIR" >> $ACTIVATE_DIR/phantasm.sh
+echo "unset PHANTASM_EXE" >> $ACTIVATE_DIR/phantasm.sh
