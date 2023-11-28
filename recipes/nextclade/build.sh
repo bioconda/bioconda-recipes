cd2df7df393569a060ecf8a64b8c630855bf20d3
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-11-28 18:05:13 +0100

    Show how to add nextclade2 to conda path

--- recipes/nextclade/build.sh
+++ recipes/nextclade/build.sh
@@ -3,4 +3,5 @@
 ls "${SRC_DIR}"
 chmod +x "${SRC_DIR}"/nextclade*
 mkdir -p "${PREFIX}/bin"
+cp "${SRC_DIR}"/nextclade* "${PREFIX}"/bin/nextclade2
 mv "${SRC_DIR}"/nextclade* "${PREFIX}"/bin/nextclade
