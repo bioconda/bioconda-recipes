4bce1ba10d63a97d581c2694c6d802041991b579
Mohsen Taheri <mohsen.taheri.1989@gmail.com>
2023-11-30 15:34:09 -0500

    add build.sh

---
+++ recipes/yacht/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
+export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
+
+maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m Cargo.toml
+
+python -m pip install . --ignore-installed --no-cache-dir -vvv
