75fed56e81599692ae22d7c876f8ad8214b98608
Mohsen Taheri <mohsen.taheri.1989@gmail.com>
2023-11-30 16:08:12 -0500

    add libclang path to post-link.sh

--- recipes/yacht/post-link.sh
+++ recipes/yacht/post-link.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 echo "Installing pyo3-branchwater via pip..."
+
+set -ex
+
+export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
+export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
+
 "${PREFIX}/bin/pip" install pyo3-branchwater==0.8.1
