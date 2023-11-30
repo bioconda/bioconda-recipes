2cc4041d8e243b11b1f133da9dbaf0acc5f843fb
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2023-11-30 18:09:07 -0500

    fix copying of files

--- recipes/lrsim/build.sh
+++ recipes/lrsim/build.sh
@@ -6,10 +6,12 @@ rm -f DWGSIMSrc/samtools/bcftools/*.[oa]
 bash make.sh
 
 mkdir -p $PREFIX/share/lrsim
-LRSIM_PROGS="simulateLinkedReads.pl dwgsim SURVIVOR msort extractReads samtools faFilter.pl"
-LRSIM_FILES="${LRSIM_PROGS} 4M-with-alts-february-2016.txt"
-chmod u+x "${LRSIM_PROGS}"
-cp "${LRSIM_FILES}" $PREFIX/share/lrsim/
+for LRSIM_PROG in simulateLinkedReads.pl dwgsim SURVIVOR msort extractReads samtools faFilter.pl
+do
+    cp ${LRSIM_PROG} $PREFIX/share/lrsim/
+    chmod u+x $PREFIX/share/lrsim/${LRSIM_PROG}
+done
+cp 4M-with-alts-february-2016.txt $PREFIX/share/lrsim/
                                     
 mkdir -p $PREFIX/bin
 echo "#! /usr/bin/env bash" >> $PREFIX/bin/lrsim
