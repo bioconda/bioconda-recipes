b0e78072577ad34360969e4385819ac0cbaa77e8
Mohsen Taheri <mohsen.taheri.1989@gmail.com>
2023-11-30 16:04:31 -0500

    delete marturin line

--- recipes/yacht/build.sh
+++ recipes/yacht/build.sh
@@ -5,6 +5,6 @@ set -ex
 export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
 export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
 
-maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m Cargo.toml
+
 
 ${PYTHON} -m pip install . --ignore-installed --no-cache-dir -vvv
