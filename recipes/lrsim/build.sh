adf3b6c128decd524607bded06b73ac71cc8bb69
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2023-11-30 22:00:13 -0500

    run test at end

--- recipes/lrsim/build.sh
+++ recipes/lrsim/build.sh
@@ -4,11 +4,6 @@ set -eux -o pipefail
 
 rm -f DWGSIMSrc/samtools/bcftools/*.[oa]
 bash make.sh
-pushd test
-bash -x test.sh
-popd
-echo "AFTER RUNNING TESTS"
-ls -alt
 
 mkdir -p $PREFIX/share/lrsim
 for LRSIM_PROG in simulateLinkedReads.pl dwgsim SURVIVOR msort extractReads samtools faFilter.pl
@@ -17,10 +12,12 @@ do
     chmod u+x $PREFIX/share/lrsim/${LRSIM_PROG}
 done
 cp 4M-with-alts-february-2016.txt $PREFIX/share/lrsim/
-cp simulateLinkedReads.pl_*.so $PREFIX/share/lrsim/
-cp simulateLinkedReads.pl_*.inl $PREFIX/share/lrsim/
+#cp simulateLinkedReads.pl_*.so $PREFIX/share/lrsim/
+#cp simulateLinkedReads.pl_*.inl $PREFIX/share/lrsim/
 
 mkdir -p $PREFIX/share/lrsim/_Inline
+export PERL_INLINE_DIRECTORY=$PREFIX/share/lrsim/_Inline
+
 mkdir -p $PREFIX/bin 
 echo "#! /usr/bin/env bash" >> $PREFIX/bin/lrsim
 echo "set -eux -o pipefail" >> $PREFIX/bin/lrsim
@@ -30,7 +27,14 @@ chmod +x $PREFIX/bin/lrsim
 
 cat $PREFIX/bin/lrsim
 ./lrsim || true
+
+pushd test
+bash -x test.sh
+popd
+find . -name '*.inl' || true
+find . -name '*.so' || true
+find $PREFIX -name '*.inl' || true
+find $PREFIX -name '*.so' || true
+
 chmod -R u+w $PREFIX/share/lrsim/_Inline
 bash -x clean.sh
-
-
