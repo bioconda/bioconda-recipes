6a927b5f25fd87460121d71ca038d4d059f1c23e
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-12-01 22:54:11 +0100

    Trying to use fat mafft binary, which is much faster (native on arm)

--- recipes/mafft/build.sh
+++ recipes/mafft/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# cd to location of Makefile and source
-cd $SRC_DIR/core
+OS=$(uname)
 
-make CC="$CC" CFLAGS="$CFLAGS" PREFIX="$PREFIX" DASH_CLIENT="dash_client"
-make install PREFIX="$PREFIX" DASH_CLIENT="dash_client"
+if [ "$OS" = "Darwin" ]; then
+
+    pkgutil --expand "$SRC_DIR"/mafft* "$SRC_DIR"/pkg
+
+    tar xvf "$SRC_DIR"/pkg/mafft*/Payload --directory "$PREFIX"
+
+
+elif [ "$OS" = "Linux" ]; then
+    cd "$SRC_DIR"/core || exit 1
+
+    make CC="$CC" CFLAGS="$CFLAGS" PREFIX="$PREFIX" DASH_CLIENT="dash_client"
+    make install PREFIX="$PREFIX" DASH_CLIENT="dash_client"
+fi
