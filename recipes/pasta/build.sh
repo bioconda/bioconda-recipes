074fd7a87e8c0274333b441434e53d6283c8db23
Stefan Janssen <stefan.m.janssen@gmail.com>
2023-12-03 21:44:09 +0100

    complete rewrite to avoid pulling old binaries from github repo, use conda dependencies instead

--- recipes/pasta/build.sh
+++ recipes/pasta/build.sh
@@ -2,48 +2,34 @@
 
 set -eu -o pipefail
 
-work_dir=$PREFIX/pasta-build
-install_dir=$PREFIX/bin
-
-mkdir -p $work_dir
-mkdir -p $work_dir/pasta
-
-cp -R ./* $work_dir/pasta
-cd $work_dir
-
+# obtain opal.jar from Siavash's extra repo
 unamestr=`uname`
 if [ $unamestr == 'Linux' ];
 then
-    wget --no-check-certificate https://github.com/smirarab/sate-tools-linux/archive/a3e6f56.tar.gz
-    tar -xzf a3e6f56.tar.gz
-    mv sate-tools-linux-a3e6f56372599ffacf1bd43adb2d67cf361228e2 sate-tools-linux
-    cd pasta
-    $PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
-    mkdir $install_dir/sate-tools-linux
-    rm -f -v $work_dir/sate-tools-linux/clustalw2  # as we use a bioconda package instead
-    cp -R $work_dir/sate-tools-linux/* $install_dir/sate-tools-linux/
-    # Handle a PASTA bug.
-    cp $install_dir/sate-tools-linux/hmmalign $install_dir/sate-tools-linux/hmmeralign
-    cp $install_dir/sate-tools-linux/hmmbuild $install_dir/sate-tools-linux/hmmerbuild
-    # So tools are available in the bin directory.
-    ln -s $install_dir/sate-tools-linux/* $install_dir/
+    wget --no-check-certificate https://github.com/smirarab/sate-tools-linux/raw/master/opal.jar -O $PREFIX/bin/opal.jar
+    mkdir -p ../sate-tools-linux/mafftdir/bin ../sate-tools-linux/mafftdir/libexec  # fake existens of tool collection
 elif [ $unamestr == 'Darwin' ];
 then
-    export DYLD_LIBRARY_PATH=$PREFIX/lib
-    wget --no-check-certificate https://github.com/smirarab/sate-tools-mac/archive/0712214.tar.gz
-    tar -xzf 0712214.tar.gz
-    mv sate-tools-mac-0712214e20152b2ec989fc102602afa53d3a7b1a sate-tools-mac
-    cd pasta
-    $PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
-    mkdir $install_dir/sate-tools-mac
-    rm -f -v $work_dir/sate-tools-mac/clustalw2  # as we use a bioconda package instead
-    cp -R $work_dir/sate-tools-mac/* $install_dir/sate-tools-mac/
-    # Handle a PASTA bug.
-    cp $install_dir/sate-tools-mac/hmmalign $install_dir/sate-tools-mac/hmmeralign
-    cp $install_dir/sate-tools-mac/hmmbuild $install_dir/sate-tools-mac/hmmerbuild
-    # So tools are available in the bin directory.
-    ln -s $install_dir/sate-tools-mac/* $install_dir/
+    wget --no-check-certificate  https://github.com/smirarab/sate-tools-mac/raw/master/opal.jar -O $PREFIX/bin/opal.jar
+    mkdir -p ../sate-tools-mac/mafftdir/bin ../sate-tools-mac/mafftdir/libexec  # fake existens of tool collection
 fi
 
-mkdir $install_dir/pasta
-cp -R $work_dir/pasta/* $install_dir/pasta/
+# Handle a PASTA bug.
+ln -s $PREFIX/bin/hmmalign $PREFIX/bin/hmmeralign
+ln -s $PREFIX/bin/hmmbuild $PREFIX/bin/hmmerbuild
+
+# "rename" raxml binaries
+ln -s $PREFIX/bin/raxmlHPC $PREFIX/bin/raxml
+ln -s $PREFIX/bin/raxmlHPC-PTHREADS $PREFIX/bin/raxmlp
+
+# copy files for tests to shared conda directory
+mkdir -p $PREFIX/share/pasta/data/
+cp -v data/small.fasta $PREFIX/share/pasta/data/
+
+# install pasta itself
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+ln -s bin/treeshrink $PREFIX/bin/treeshrink
+
+# ensure PASTA environment variable point to the right bin directory
+echo 'export PASTA_TOOLS_RUNDIR=\$PREFIX/bin' > $PREFIX/etc/conda/activate.d/pasta_activate.sh
+echo 'unset PASTA_TOOLS_RUNDIR' > $PREFIX/etc/conda/deactivate.d/pasta_deactivate.sh
