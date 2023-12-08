daf4e2d6ccbc2e845aababb928f1a2fe31f9acf8
Adam Talbot <12817534+adamrtalbot@users.noreply.github.com>
2023-12-08 12:51:46 +0000

    [ci skip] add build failure record for recipe recipes/tower-cli

--- recipes/tower-cli/build.sh
+++ recipes/tower-cli/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euxo pipefail
+OUTDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $OUTDIR
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/tw $PREFIX/bin/tw
+ln -s $OUTDIR/tw $PREFIX/bin
 chmod 755 $PREFIX/bin/tw
