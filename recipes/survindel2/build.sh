7bab815f4e97b382e44567899f043a5f485b3af9
ramesh <ramesh.rajaby@gmail.com>
2023-12-08 12:44:26 +0000

    Add survindel2.

--- recipes/survindel2/build.sh
+++ recipes/survindel2/build.sh
@@ -3,15 +3,13 @@ set -x
 ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} reads_categorizer.cpp -o reads_categorizer -pthread -lhts
 ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} libs/ssw.c libs/ssw_cpp.cpp clip_consensus_builder.cpp -o clip_consensus_builder -pthread -lhts
 ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} merge_identical_calls.cpp -o merge_identical_calls -pthread -lhts
-${CXX} ${CPPFLAGS} ${CXXFLAGS} -fpermissive ${LDFLAGS} libs/ssw.c libs/ssw_cpp.cpp libs/kdtree.c dp_clusterer.cpp -o dp_clusterer -pthread -lhts
+${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} libs/ssw.c libs/ssw_cpp.cpp libs/kdtree.c dp_clusterer.cpp -o dp_clusterer -pthread -lhts
 ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} normalise.cpp -o normalise -pthread -lhts
 
 cp reads_categorizer $PREFIX/bin/
 cp clip_consensus_builder $PREFIX/bin/
-cp call_insertions $PREFIX/bin/
-cp dc_remapper $PREFIX/bin/
-cp add_filtering_info $PREFIX/bin/
-cp filter $PREFIX/bin/
+cp merge_identical_calls $PREFIX/bin/
+cp dp_clusterer $PREFIX/bin/
 cp normalise $PREFIX/bin/
 
 cp survindel2.py $PREFIX/bin/
