d9c03154b3e4a8406849e41a5a2f315e3f09ac56
mencian <joshua.zhuang@yahoo.com>
2023-12-10 15:26:15 -0600

    try CPLUS_INCLUDE_PATH

--- recipes/methylpy/build.sh
+++ recipes/methylpy/build.sh
@@ -4,7 +4,7 @@
 ${PYTHON} -m pip install . --no-deps --no-build-isolation -vvv
 
 # Install RMS
-export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
 cd methylpy
 $CXX -O3 -L${PREFIX}/lib -lgsl -lgslcblas -o run_rms_tests.out rms.cpp
 # run_rms_tests.out needs to be copied to the directory where methylpy is installed
