14fd2f311f091dbaea10893a43b7f56eb965db5f
Jacques Dainat <jacques.dainat@ird.fr>
2023-12-11 12:28:13 +0100

    fix build add link to craq from share

--- recipes/craq/build.sh
+++ recipes/craq/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -x -e
-mkdir -p ${PREFIX}/bin
 
-cp * ${PREFIX}/bin
+# Put craq in share
+CRAQ_DIR=${PREFIX}/share/CRAQ
+mkdir -p ${CRAQ_DIR}
+cp -r * ${CRAQ_DIR}
+chmod +x ${CRAQ_DIR}/bin/craq
+
+# add link in bin
+mkdir -p ${PREFIX}/bin
+ln -s ${CRAQ_DIR}/bin/craq ${PREFIX}/bin/craq
