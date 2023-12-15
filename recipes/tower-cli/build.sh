00f0af40d8cc11127b3fbbc98272f37a45c858f1
Adam Talbot <12817534+adamrtalbot@users.noreply.github.com>
2023-12-15 12:25:34 +0000

    Switch tower-cli to use jar file instead of binaries

--- recipes/tower-cli/build.sh
+++ recipes/tower-cli/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-set -euxo pipefail
-OUTDIR=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-mkdir -p $OUTDIR
-mkdir -p $PREFIX/bin
-ln -s $OUTDIR/tw $PREFIX/bin
-chmod 755 $PREFIX/bin/tw
+
+TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
+[ -d "$TGT" ] || mkdir -p "$TGT"
+[ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
+
+cp -p "$SRC_DIR"/*.jar "$TGT"
+
+cp $RECIPE_DIR/tw.sh $TGT/tw
+ln -s $TGT/tw $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/tw"
