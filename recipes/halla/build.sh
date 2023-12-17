4c4b4521eb4a488cc403135776dcc2a420f0b964
joshuazhuang7 <joshua.zhuang@yahoo.com>
2023-12-17 16:45:13 -0600

    remove redundant script: section in meta.yaml

--- recipes/halla/build.sh
+++ recipes/halla/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 PYVER=`python -V | cut -f 2 -d " " | cut -f 1 -d "."`  # Returns the python major version
-if [ $PYVER -eq 3 ]
+if [ ${PYVER} -eq 3 ]
 then
-    for i in halla/*.py
-    do
-        2to3 --write $i
-    done
+	for i in halla/*.py
+	do
+		2to3 --write ${i}
+	done
 fi
 
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation
