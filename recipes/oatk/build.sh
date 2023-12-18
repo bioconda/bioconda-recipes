48d8cf48e900788537e6fd73865e86053091a53d
Lia Obinu <liaobinu@gmail.com>
2023-12-18 15:23:55 +0000

    adding oatk recipe

---
+++ recipes/oatk/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make
+ls -la
+
+# Copying binaries to the $PREFIX/bin directory for them to be included in the Conda package
+mkdir -p $PREFIX/bin
+
+if [ -f oatk ]; then cp oatk $PREFIX/bin/; fi
+if [ -f path_to_fasta ]; then cp path_to_fasta $PREFIX/bin/; fi
+if [ -f pathfinder ]; then cp pathfinder $PREFIX/bin/; fi
+if [ -f hmm_annotation ]; then cp hmm_annotation $PREFIX/bin/; fi
+if [ -f syncasm ]; then cp syncasm $PREFIX/bin/; fi
+
