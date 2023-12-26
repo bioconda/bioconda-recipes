8655579f439fb1c50bf49c98e216ed886f68a977
ekushele <efratkush@gmail.com>
2023-12-26 09:55:41 +0200

    change python3 to python shebang

--- recipes/wgbs_tools/build.sh
+++ recipes/wgbs_tools/build.sh
@@ -4,6 +4,8 @@ set -eoux pipefail
 git clone https://github.com/nloyfer/wgbs_tools.git
 
 cd wgbs_tools
+sed -i -e '1s::#!/usr/bin/python3 -u::#!/usr/bin/python -u:' src/python//*.py
+
 python setup.py
 
 cp -r . ${PREFIX}/bin/
