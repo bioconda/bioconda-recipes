6c22c6f1a92fc9552fb0ad1add0924c426b78fb9
Jacob Morrison <Jacob.Morrison@vai.org>
2024-01-08 14:44:47 -0500

    update to biscuit v1.4.0

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-BIN=$PREFIX/bin
-mkdir -p $BIN
-make CC="$CC $LDFLAGS" CFLAGS="$CFLAGS"
-cp biscuit $BIN
-cp QC.sh $BIN
-cp build_biscuit_QC_assets.pl $BIN
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $BIN/build_biscuit_QC_assets.pl
+mkdir -p "${PREFIX}/bin"
+mkdir -p build
+cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
+make
+make install
