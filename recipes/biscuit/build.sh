bc04e745de374ab19f061d5aa3a1be3f869316ff
Jacob Morrison <Jacob.Morrison@vai.org>
2024-01-09 11:37:13 -0500

    modify sgsl and utils Makefiles in build

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -4,5 +4,7 @@ mkdir -p "${PREFIX}/bin"
 mkdir -p build
 cd build || exit 1
 cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
+sed -i "s/= gcc/?= gcc/g" ../lib/sgsl/Makefile
+sed -i "s/= gcc/?= gcc/g" ../lib/utils/Makefile
 make
 make install
