3d8840f9a10667d74bf49f1df3a2adde50604aa3
Jacob Morrison <Jacob.Morrison@vai.org>
2024-01-09 13:46:05 -0500

    add CC to make call

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -4,7 +4,5 @@ mkdir -p "${PREFIX}/bin"
 mkdir -p build
 cd build || exit 1
 cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
-sed -i "s/= gcc/?= gcc/g" ../lib/sgsl/Makefile
-sed -i "s/= gcc/?= gcc/g" ../lib/utils/Makefile
-make
+make CC=$CC
 make install
