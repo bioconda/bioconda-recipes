52b7feb0869bcc692d9739cb567118d9b55d2d67
Dan Fornika <dfornika@gmail.com>
2024-01-12 12:56:05 -0800

    Revert adding LIBS

--- recipes/tntblast/build.sh
+++ recipes/tntblast/build.sh
@@ -2,7 +2,6 @@
 
 sed -i.bak 's/-DUSE_MPI//g' Makefile
 sed -i.bak '/BLAST_DIR =/d' Makefile
-sed -i.bak 's/$(CC) $(FLAGS) $(INC)/$(CC) $(FLAGS) $(INC) $(LIBS)/g' Makefile
 
 make CC="${CXX_FOR_BUILD}" all
 
