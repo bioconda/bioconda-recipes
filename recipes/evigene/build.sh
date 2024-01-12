ce9f48e90f332a47455af157b5546d908e4822c2
Usman Rashid <usman@smme.edu.pk>
2024-01-12 15:39:12 +1300

    Added evigene

---
+++ recipes/evigene/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+readonly EVIGENEHOME=${PREFIX}/opt/${PKG_NAME}-${PKG_VERSION}
+
+mkdir -p ${EVIGENEHOME}
+
+cd ${SRC_DIR}
+
+cp -Rp config lib scripts ${EVIGENEHOME}
+
+
+mkdir -p ${PREFIX}/etc/conda/activate.d/
+echo "export EVIGENEHOME=${EVIGENEHOME}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}-${PKG_VERSION}.sh
+
+mkdir -p ${PREFIX}/etc/conda/deactivate.d/
+echo "unset EVIGENEHOME" > ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}-${PKG_VERSION}.sh
