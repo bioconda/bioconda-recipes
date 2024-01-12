7a67520116b464c2cb747e666acc327c88692713
Mariangela Santorsola <santorsola@login01.eos.medooza.lan>
2024-01-12 12:12:05 +0100

    check paths

--- recipes/gametes/build.sh
+++ recipes/gametes/build.sh
@@ -2,12 +2,12 @@
 set -eu -o pipefail
 
 #create target directory
-PACKAGE_HOME=$PREFIX/share/$PKG_NAME
+PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PACKAGE_HOME
 
 #create bin
 BINARY_HOME=$PREFIX/bin
-mkdir -p "$BINARY_HOME"
+mkdir -p $BINARY_HOME
 
 cd $SRC_DIR
 
@@ -15,9 +15,13 @@ JAR_NAME=GAMETES_2.1.jar
 
 cp $JAR_NAME $PACKAGE_HOME/gametes.jar
 
-cp $RECIPE_DIR/gametes.py ${BINARY_HOME}/gametes
+#mv wrapper script to package home 
+cp $RECIPE_DIR/gametes.py $PACKAGE_HOME 
 
-chmod +x ${BINARY_HOME}/gametes
+#and symlink to $PREFIX/bin
+ln -s $PACKAGE_HOME/gametes.py  ${BINARY_HOME}
+
+chmod +x ${BINARY_HOME}
 
 
 
