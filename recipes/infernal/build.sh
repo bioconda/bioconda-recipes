9d030d62b096907f4670851e4352cc414f76f3e4
Peter Kruczkiewicz <peter.kruczkiewicz@gmail.com>
2024-01-15 17:08:52 -0600

    Update build.sh

--- recipes/infernal/build.sh
+++ recipes/infernal/build.sh
@@ -2,9 +2,16 @@
 
 set -ex
 
+NAME=infernal
+VERSION=1.1.5
+TAG="${NAME}-${VERSION}"
+
 grep -l -r "/usr/bin/perl" . | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g'
 
+git clone --depth 1 -b $TAG https://github.com/EddyRivasLab/hmmer
+git clone --depth 1 -b $TAG https://github.com/EddyRivasLab/easel
 
+autoreconf
 ./configure --prefix=$PREFIX
 make -j 2
 make check
