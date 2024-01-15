e8cdcaa7fece14eed3736a271a1e2a884e13a43c
Thanh Lee <thanh.le-viet@quadram.ac.uk>
2024-01-15 20:01:35 +0000

    zlib check

--- recipes/tntblast/build.sh
+++ recipes/tntblast/build.sh
@@ -3,6 +3,13 @@
 sed -i.bak 's/-DUSE_MPI//g' Makefile
 sed -i.bak '/BLAST_DIR =/d' Makefile
 
+export INCLUDE_PATH="${BUILD_PREFIX}/include"
+export LIBRARY_PATH="${BUILD_PREFIX}/lib"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${BUILD_PREFIX}/lib"
+
+export LDFLAGS="-L${BUILD_PREFIX}/lib"
+export CPPFLAGS="-I${BUILD_PREFIX}/include"
+
 make CC="${CXX_FOR_BUILD}" all
 
 cp tntblast $PREFIX/bin
