3ce12b5af9d83509ee973e903497a7f6d3f6ce0e
ekushele <efratkush@gmail.com>
2024-01-16 11:48:59 +0200

    change i to be i.bak

--- recipes/wgbs_tools/build.sh
+++ recipes/wgbs_tools/build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/nloyfer/wgbs_tools.git
 
 cd wgbs_tools
 
-sed -i  ''  '1s/python3/python/' src/python/*.py
+sed -i.bak  '1s/python3/python/' src/python/*.py
 python setup.py
 python --version
 
