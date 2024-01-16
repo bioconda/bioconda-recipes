8551e6bd515afd65bc4164e107b154df80d18777
mencian <joshua.zhuang@yahoo.com>
2024-01-15 21:03:34 -0600

    try make

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Needed for building utils dependency
-export CPATH="${PREFIX}/include"
+export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
 
 mkdir -p "${PREFIX}/bin"
 mkdir -p build
 cd build || exit 1
-cmake -S .. -B . \
-	-DCMAKE_C_FLAGS="${CFLAGS} -O3 ${LDFLAGS}" \
-	-DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}"
-cmake --build . --target install -v
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
+make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS}"
+make install
