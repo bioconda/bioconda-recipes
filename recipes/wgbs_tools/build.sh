48125b6725169ef4da587979a20c8963e0b61e69
ekushele <efratkush@gmail.com>
2024-01-16 09:13:22 +0200

    add empty quotes for mac compiling

--- recipes/wgbs_tools/build.sh
+++ recipes/wgbs_tools/build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/nloyfer/wgbs_tools.git
 
 cd wgbs_tools
 
-sed -i  '1 s/python3/python/' src/python/*.py
+sed -i  ""  '1 s/python3/python/' src/python/*.py
 python setup.py
 python --version
 
