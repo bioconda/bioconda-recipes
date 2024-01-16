54254c2239b1959bcc735f0a6fa17cc98b0eaf32
mencian <joshua.zhuang@yahoo.com>
2024-01-15 19:50:02 -0600

    edit build.sh

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -8,9 +8,7 @@ export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
 mkdir -p "${PREFIX}/bin"
 
 cmake -S . -B build \
-	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS} -O3 ${LDFLAGS}" \
-	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}"
 
 cmake --build build/ --target install -v
