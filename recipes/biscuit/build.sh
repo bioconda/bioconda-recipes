65095e89f11747a621b6ad93c994a02d72185dd2
mencian <joshua.zhuang@yahoo.com>
2024-01-15 19:17:04 -0600

    try C_INCLUDE_PATH

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Needed for building utils dependency
-export CPATH=${PREFIX}/include
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
 
 mkdir -p "${PREFIX}/bin"
-mkdir -p build
-cd build || exit 1
-cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
-make CC=$CC
-make install
+
+cmake -S . -B build \
+	-DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS} -O3 ${LDFLAGS}" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}"
+
+cmake --build build/ --target install -v
