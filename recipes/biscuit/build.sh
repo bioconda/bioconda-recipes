e019468a9c4d13444aa33b2fcf67060a902d3a9d
Jacob Morrison <Jacob.Morrison@vai.org>
2024-01-17 10:58:21 -0500

    add shebang via sed for asset builder

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -11,3 +11,6 @@ cd build || exit 1
 cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
 make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS} -O3"
 make install
+
+# Needed to run asset builder
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${PREFIX}/bin/build_biscuit_QC_assets.pl
