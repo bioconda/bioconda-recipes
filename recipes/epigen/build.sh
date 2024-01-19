3de5e27d092c75e03adc5fcf260780546394648a
Mariangela Santorsola <santorsola@login01.eos.medooza.lan>
2024-01-19 11:13:09 +0100

    add epigen recipe

---
+++ recipes/epigen/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##https://github.com/bioconda/bioconda-recipes/blob/4a90b4a7dedfdaeb04f967573fc676fdf53caee7/recipes/papaa/build.sh
+
+mkdir -p "${PREFIX}/bin"
+mkdir -p ${PREFIX}/share/epigen
+cp -r * ${PREFIX}/share/epigen
+ln -s ${PREFIX}/share/epigen/*.py ${PREFIX}/bin/
+ln -s ${PREFIX}/share/epigen/utils/*.py ${PREFIX}/bin/
+
+chmod +x ${PREFIX}/share/epigen/*.py
+chmod +x ${PREFIX}/share/epigen/utils/*.py
+
+
+
