1351a890a41df326a0f09d64b93553b86825c8b3
Matthias Bernt <m.bernt@ufz.de>
2024-01-20 18:00:57 +0100

    cleanup

--- recipes/revoluzer/build.sh
+++ recipes/revoluzer/build.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
-# export LIBRARY_PATH=${PREFIX}/lib
-# export LD_LIBRARY_PATH=${PREFIX}/lib
-# export CPATH=${PREFIX}/include
-# export C_INCLUDE_PATH=${PREFIX}/include
-# export CPLUS_INCLUDE_PATH=${PREFIX}/include
-# export CPP_INCLUDE_PATH=${PREFIX}/include
-# export CXX_INCLUDE_PATH=${PREFIX}/include
-export CMAKE_INCLUDE_PATH=${BUILD_PREFIX}/include
-export CMAKE_PREFIX_PATH=${BUILD_PREFIX}
 CXXFLAGS="-I${BUILD_PREFIX}/include $CXXFLAGS"
 
-echo ${BUILD_PREFIX}/include
-ls ${BUILD_PREFIX}/include
-echo "===================================="
 
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
 cmake --build build
 mkdir -p $PREFIX/bin
-rm build/src/CMakeFiles build/src/cmake_install.cmake build/src/Makefile
-ls -la build/
-mv build/* $PREFIX/bin
+echo "installing: $(find build/src -type f -executable)"
+mv $(find build/src -type f -executable) $PREFIX/bin
