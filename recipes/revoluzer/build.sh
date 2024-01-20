7199374c71456538af91d23686858f6f2b7ceb72
Matthias Bernt <m.bernt@ufz.de>
2024-01-20 12:20:06 +0100

    BUILD_PREFIX?

--- recipes/revoluzer/build.sh
+++ recipes/revoluzer/build.sh
@@ -7,8 +7,10 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export CMAKE_INCLUDE_PATH=${PREFIX}/include
-echo ${PREFIX}/include
-ls ${PREFIX}/include
+export CMAKE_PREFIX_PATH=${PREFIX}
+
+echo ${BUILD_PREFIX}/include
+ls ${BUILD_PREFIX}/include
 echo "===================================="
 
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
