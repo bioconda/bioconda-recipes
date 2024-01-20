8336fd69004fe5dbfd45dca396d906241678b0d6
Matthias Bernt <m.bernt@ufz.de>
2024-01-20 17:27:51 +0100

    bump and test

--- recipes/revoluzer/build.sh
+++ recipes/revoluzer/build.sh
@@ -17,4 +17,6 @@ echo "===================================="
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
 cmake --build build
 mkdir -p $PREFIX/bin
-mv bin/* $PREFIX/bin
+rm build/src/CMakeFiles build/src/cmake_install.cmake build/src/Makefile
+ls -la build/
+mv build/* $PREFIX/bin
