0eb7ec6be580ed50995898988cd7a46e766b4359
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2024-01-24 16:11:22 +0000

    skip patch on osx

--- recipes/seqfu/build.sh
+++ recipes/seqfu/build.sh
@@ -17,16 +17,20 @@ export CPATH=${PREFIX}/include
 echo "GXX: ${GXX:-'not set'}"
 echo "GCC: ${GCC:-'not set'}"
 echo "----------"
-echo "Patching makefile"
-# Trying to fix build when gcc or g++ are required
-sed -i 's/gcc/$(GCC)/g' Makefile
-sed -i 's/g++/$(GXX)/g' Makefile
-sed -i '1iGCC ?= gcc' Makefile
-sed -i '1iGXX ?= g++' Makefile
+
+
 
 if [[ $OSTYPE == "darwin"* ]]; then
+  echo "OSX"
   export HOME="/Users/distiller"
   export HOME=`pwd`
+else
+  # Trying to fix build when gcc or g++ are required
+  echo "LINUX: Patching makefile"
+  sed -i 's/gcc/$(GCC)/g' Makefile
+  sed -i 's/g++/$(GXX)/g' Makefile
+  sed -i '1iGCC ?= gcc' Makefile
+  sed -i '1iGXX ?= g++' Makefile
 fi
 
 mkdir -p "$PREFIX"/bin
