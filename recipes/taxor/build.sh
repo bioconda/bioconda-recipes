d7d1430f050faaab1402f9d7d7874412a66ed1f2
Jens-Uwe Ulrich <jens-uwe.ulrich@hpi.de>
2024-01-26 11:14:55 +0100

    change source directories

--- recipes/taxor/build.sh
+++ recipes/taxor/build.sh
@@ -2,8 +2,8 @@
 
 mkdir ${SRC_DIR}/build/
 cd ${SRC_DIR}/build/
-cmake ${SRC_DIR}
+cmake ${SRC_DIR}/src
 cmake --build . --config Release
 
-cp main/taxor $PREFIX/bin
+cp ${SRC_DIR}/build/main/taxor $PREFIX/bin
 chmod +x $PREFIX/bin/taxor
