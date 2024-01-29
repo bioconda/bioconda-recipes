50479201ea8cb71bdebc842785e88ba106a621f4
Jens-Uwe Ulrich <jens-uwe.ulrich@hpi.de>
2024-01-29 14:40:55 +0100

    rollback

--- recipes/taxor/build.sh
+++ recipes/taxor/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#mkdir ${SRC_DIR}/build/
-#cd ${SRC_DIR}/build/
-#cmake ${SRC_DIR}/src
-#cmake --build . --config Release
+mkdir ${SRC_DIR}/build/
+cd ${SRC_DIR}/build/
+cmake ${SRC_DIR}/src
+cmake --build . --config Release
 
-#cp ${SRC_DIR}/build/main/taxor $PREFIX/bin
-#chmod +x $PREFIX/bin/taxor
+cp ${SRC_DIR}/build/main/taxor $PREFIX/bin
+chmod +x $PREFIX/bin/taxor
 
-mkdir -p $PREFIX/bin
-cp taxor $PREFIX/bin
