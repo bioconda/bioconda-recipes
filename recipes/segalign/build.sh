bf6cc09623c6e84dcb99adadf81e5bc28b47c226
Richard Burhans <burhans@bx.psu.edu>
2024-02-01 16:19:01 -0500

    bioconda recipe for segalign

---
+++ recipes/segalign/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+BUILD_DIR="$SRC_DIR/build"
+BIN_DIR="$PREFIX/bin"
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make VERBOSE=1
+
+install --mode 0755 --directory "$BIN_DIR"
+install --mode 0755 $SRC_DIR/build/segalign{,_repeat_masker} "$BIN_DIR"
+install --mode 0755 $SRC_DIR/scripts/run_segalign{,_repeat_masker} "$BIN_DIR"
