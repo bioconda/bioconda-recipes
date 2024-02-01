f88394176ada6267ccb29addf930226b64cf85a2
mencian <joshua.zhuang@yahoo.com>
2024-02-01 15:14:10 -0600

    edit build.sh

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Needed for building utils dependency
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
+
 mkdir -p "${PREFIX}/bin"
 
 mkdir build
 pushd build
 cmake ..
-make
+make CXX="${CXX} ${LDFLAGS}" CXXFLAGS="${CXXFLAGS} -O3"
 
 cp bin/bam-readcount "${PREFIX}/bin"
 popd
