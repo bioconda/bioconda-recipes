f96b96f89d0a3e5799ee2bc89af997f3fc1aad26
tpeulen <thomas.peulen@posteo.de>
2024-02-03 20:31:06 +0100

    Use 0.14.3 without AVX

--- recipes/imp.bff/build.sh
+++ recipes/imp.bff/build.sh
@@ -2,8 +2,8 @@
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
-ninja install -k 0 -j 4
+cmake .. -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DWITH_AVX=OFF
+ninja install -k 0 -j 8
 
 # Copy examples
 mkdir $PREFIX/share/doc/IMP/examples/bff
