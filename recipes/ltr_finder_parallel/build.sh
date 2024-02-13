5b2533e931b40ba4a5337ed4f9dcf524d66928ad
Usman Rashid <usman@smme.edu.pk>
2024-02-13 19:02:50 +1300

    Replacing ltr_finder problematic line

--- recipes/ltr_finder_parallel/build.sh
+++ recipes/ltr_finder_parallel/build.sh
@@ -7,6 +7,9 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${LTR_FINDER_PARALLEL_DIR}
 cp -r * ${LTR_FINDER_PARALLEL_DIR}
 
+sed -i.bak 's|\$ltr_finder=~s/ltr_finder\\n?//;|\$ltr_finder=~s/\\bltr_finder\\n?\$//m;|' \
+    ${LTR_FINDER_PARALLEL_DIR}/LTR_FINDER_parallel
+
 cat <<END >>${PREFIX}/bin/LTR_FINDER_parallel
 #!/bin/bash
 perl ${LTR_FINDER_PARALLEL_DIR}/LTR_FINDER_parallel \$@
