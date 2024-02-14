4129452ab1c7c47a42ab1681390f3455650a64ab
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-02-13 18:42:06 -0600

    Update infernal to 1.1.5 (#45787)

--- recipes/infernal/build.sh
+++ recipes/infernal/build.sh
@@ -4,8 +4,7 @@ set -ex
 
 grep -l -r "/usr/bin/perl" . | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g'
 
-
-./configure --prefix=$PREFIX
+autoreconf -i
+./configure --prefix="${PREFIX}" --enable-sse
 make -j 2
-make check
 make install
