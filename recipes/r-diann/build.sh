ffd13eddbbc0c13a935ce64eda9c315b0d773c0f
npinter <npinter@protonmail.com>
2024-02-15 11:27:57 +0100

    Fix compiler issue

--- recipes/r-diann/build.sh
+++ recipes/r-diann/build.sh
@@ -4,4 +4,13 @@ export CPATH="${PREFIX}/lib/R/library/RcppEigen/include:${CPATH}"
 export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 
+echo "RcppEigen Path"
+ls -l ${PREFIX}/lib/R/library/RcppEigen/
+
+echo "R library Path"
+ls -l ${PREFIX}/lib/R/library
+
+echo "R Path"
+ls -l ${PREFIX}/lib/R/
+
 $R CMD INSTALL --build .
