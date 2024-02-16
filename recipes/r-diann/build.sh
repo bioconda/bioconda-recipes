f47829080ec523a372cfe0b4d6cd739d0dc4ec6e
npinter <npinter@protonmail.com>
2024-02-16 10:55:12 +0100

    Fix compiler issue

--- recipes/r-diann/build.sh
+++ recipes/r-diann/build.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-export CPATH="${PREFIX}/lib/R/library/RcppEigen/include:${CPATH}"
-export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
-export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-
-export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
-echo "CPATH: $CPATH"
-echo "LIBRARY_PATH: $LIBRARY_PATH"
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
-echo "CXXFLAGS: $CXXFLAGS"
-echo "LDFLAGS: $LDFLAGS"
+export LDFLAGS="${LDFLAGS} -lstdc++"
 
 $R CMD INSTALL --build .
