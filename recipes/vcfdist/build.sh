d204fa73d78242e1f2296d30a5afd7517188864e
Tim Dunn <13918078+TimD1@users.noreply.github.com>
2024-02-16 10:31:57 -0500

    Add BASH path exports

--- recipes/vcfdist/build.sh
+++ recipes/vcfdist/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd src
-make
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+
+cd src && make
 install -d "${PREFIX}/bin"
 install vcfdist "${PREFIX}/bin"
