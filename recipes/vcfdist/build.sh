c8b6433d52c1f8c4ccb2298b861c8b6268307922
Tim Dunn <13918078+TimD1@users.noreply.github.com>
2024-02-16 10:57:46 -0500

    C++ version of path variables

--- recipes/vcfdist/build.sh
+++ recipes/vcfdist/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH="${PREFIX}/include"
+export CPATH="${PREFIX}/include"
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 cd src && make
