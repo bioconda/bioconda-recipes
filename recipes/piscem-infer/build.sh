2d676d0addee50a2b9dbdb24d7c84e0d2719e11e
Rob Patro <rob-p@users.noreply.github.com>
2024-02-17 12:47:04 -0500

    Update build.sh

--- recipes/piscem-infer/build.sh
+++ recipes/piscem-infer/build.sh
@@ -3,5 +3,14 @@
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
+# because OSX seems to be lacking this?
+unamestr=`uname`
+
+if [ "$unamestr" == 'Darwin' ];
+then
+rustup component add rust-std
+fi
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path .
+
