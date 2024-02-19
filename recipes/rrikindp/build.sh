38c70c79c2c55eda6ede858caf12cff636848275
Maria Waldl <maria@tbi.univie.ac.at>
2024-02-19 13:54:15 +0100

    call pip install and only afterwards cd to src directory

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+python -m pip install . --no-deps --ignore-installed -vv
+
 cd src/RRIkinDP
 make -j ${CPU_COUNT}
 install RRIkinDP "$CONDA_PREFIX/bin"
-
-python -m pip install . --no-deps --ignore-installed -vv
