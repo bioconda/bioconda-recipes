7ebd182d423e3de41f47084a1f2faa04ae75bc22
Maria Waldl <maria@tbi.univie.ac.at>
2024-02-19 14:54:47 +0100

    debugging boost loation

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -2,6 +2,9 @@
 
 python -m pip install . --no-deps --ignore-installed -vv
 
+export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PAT
+export CPATH=$CONDA_PREFIX/include:$CPATH
+
 cd src/RRIkinDP
 make -j ${CPU_COUNT}
 install RRIkinDP "$CONDA_PREFIX/bin"
