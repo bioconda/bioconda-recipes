a383848e21e9b6cbc5dd2aee65616e76fabde290
Maria Waldl <maria@tbi.univie.ac.at>
2024-02-20 08:12:28 +0100

    add compiler flags to make call

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -3,5 +3,6 @@
 python -m pip install . --no-deps --ignore-installed -vv
 
 cd src/RRIkinDP
-make -j ${CPU_COUNT}
+
+make -j ${CPU_COUNT} CXXFLAGS+="-I$(CONDA_PREFIX)/include" LDFLAGS+="-L$(CONDA_PREFIX)/lib "
 install RRIkinDP "$CONDA_PREFIX/bin"
