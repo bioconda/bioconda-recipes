564802577b0a46dca9ce901709f50fe81b3d739b
Maria Waldl <maria@tbi.univie.ac.at>
2024-02-20 11:23:46 +0100

    switch to lower case name space for python module

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -2,7 +2,7 @@
 
 python -m pip install . --no-deps --ignore-installed -vv
 
-cd src/RRIkinDP
+cd src/rrikindp
 
 make -j ${CPU_COUNT} CXXFLAGS+="-I${CONDA_PREFIX}/include" LDFLAGS+="-L${CONDA_PREFIX}/lib"
 install RRIkinDP "$CONDA_PREFIX/bin"
