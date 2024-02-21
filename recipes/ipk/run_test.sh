25d6475079ba4cde35809d437f3fb7d9964aaec1
Benjamin Linard <benjamin.linard@inrae.fr>
2024-02-21 17:24:33 +0100

    bugfix

--- recipes/ipk/run_test.sh
+++ recipes/ipk/run_test.sh
@@ -1,54 +1,56 @@
 #!/usr/bin/env bash
 
-echo $(pwd)
-
+pwd
 mkdir -p tests_output
+ls
+ls tests
 
-echo $(ls -lh)
+PASS=true
 
 # A
 test -f $(which ipk-dna)
-if [ $? -eq 0 ]; then
+if [ $? -ne 0  ]; then
   echo "test A:failed"
-  exit 1
+  PASS=false
 fi
 
 # B
 test -f $(which ipk-aa)
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
   echo "test B:failed"
-  exit 1
+  PASS=false
 fi
 
 # C
 #echo $(ipk-dna) | grep "the option '--ar-binary' is required"
 #if [ $? -eq 0 ]; then
 #  echo "test C:failed"
-#  exit 1
+#  PASS=false
 #fi
 
 # D
 ipk.py build --help
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
   echo "test D:failed"
-  exit 1
+  PASS=false
 fi
 
 # E
 # reference files were copied via meta.yaml instructions
 ipk.py build -r reference.fasta -t tree.rooted.newick -m GTR -k 7 --omega 2.0 -u 1.0 -b raxml-ng -w tests_output
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
   echo "test E:failed"
-  exit 1
+  PASS=false
 fi
 
-echo $(ls -lh)
+ls
+ls tests_output
 
 # F
 test -f tests_output/DB_k7_o2.0.rps
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
   echo "test F:failed"
-  exit 1
+  PASS=false
 fi
 
 exit 0
