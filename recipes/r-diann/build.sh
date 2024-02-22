86be7b30d38a4bb3cdd00c92516e6149b80076d0
npinter <npinter@protonmail.com>
2024-02-22 18:10:57 +0100

    Fix compiler issue

--- recipes/r-diann/build.sh
+++ recipes/r-diann/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-mv DESCRIPTION DESCRIPTION.old
-grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
-mkdir -p ~/.R
-echo -e "CC=$CC
-FC=$FC
-CXX=$CXX
-CXX98=$CXX
-CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
+export LDFLAGS="${LDFLAGS} -lstdc++"
+
 $R CMD INSTALL --build .
