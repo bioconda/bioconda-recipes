a8282b2663e990b676a1fb08ee3543b0cb7a55ea
Benjamin Linard <22132778+blinard-BIOINFO@users.noreply.github.com>
2024-02-23 14:35:12 +0100

    Use phyml instead of raxml-ng in tests, to avoid CPU oversubscription issues in CI docker.

--- recipes/ipk/run_test.sh
+++ recipes/ipk/run_test.sh
@@ -45,7 +45,7 @@ wget -O tree.rooted.newick https://github.com/phylo42/IPK/raw/main/tests/data/ne
 test -s reference.fasta
 test -s tree.rooted.newick
 mkdir -p tests_output
-ipk.py build -r reference.fasta -t tree.rooted.newick -m GTR -k 7 --omega 2.0 -u 1.0 -b $(which raxml-ng) -w tests_output
+ipk.py build -r reference.fasta -t tree.rooted.newick -m GTR -k 7 --omega 2.0 -u 1.0 -b $(which phyml) -w tests_output
 if [ $? -ne 0 ]; then
   echo "failed"
   PASS=false
