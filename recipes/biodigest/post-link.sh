913f0b39792ebfe55f35a758419e7673bdb41598
lspindler2509 <lisa.spindler@hotmail.de>
2024-02-23 11:55:01 +0100

    add prints to messages

--- recipes/biodigest/post-link.sh
+++ recipes/biodigest/post-link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Running setup script..."
+echo "Running setup script..." >> "$PREFIX/.messages.txt"
 python -c "from biodigest import setup; setup.main(setup_type='api')"
-echo "Setup script finished."
+echo "Setup script finished." >> "$PREFIX/.messages.txt"
