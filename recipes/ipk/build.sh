36eb4f970dedb97edd992aef19013a64cbe369b9
Jacques Dainat <jacques.dainat@ird.fr>
2024-02-23 10:38:07 +0100

    transplant @blinard modifications related to IPK only from #45958

--- recipes/ipk/build.sh
+++ recipes/ipk/build.sh
@@ -1,9 +1,28 @@
-#! /bin/bash
+#!/bin/bash
+set -ex
 
-#compile
-cd $SRC_DIR/
-pwd
-cmake -DHASH_MAP=USE_TSL_ROBIN_MAP -DCMAKE_CXX_FLAGS="-O3" -DCMAKE_INSTALL_PREFIX=${PREFIX} ${SRC_DIR}
-make -j${CPU_COUNT} ${VERBOSE_CM} 
-make install -j${CPU_COUNT}
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+export CXX_INCLUDE_PATH=${PREFIX}/include
 
+#to ensure zlib location
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/lib
+
+cmake -B build -DHASH_MAP=USE_TSL_ROBIN_MAP -DCMAKE_CXX_FLAGS="-O3" -DBUILD_SHARED_LIBS=ON --install-prefix=$PREFIX
+cmake --build build --target all
+cmake --install build
+
+ls build/ipk
+
+cp build/ipk/ipk-aa $PREFIX/bin
+cp build/ipk/ipk-aa-pos $PREFIX/bin
+cp build/ipk/ipk-dna $PREFIX/bin
+
+chmod +x $PREFIX/bin/ipk-aa
+chmod +x $PREFIX/bin/ipk-dna
+chmod +x $PREFIX/bin/ipk-aa-pos
+chmod +x $PREFIX/bin/ipk.py
