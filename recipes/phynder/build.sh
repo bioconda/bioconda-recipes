fe57f0fb157bde5315f5b9d918cd0036aadf8925
James A. Fellows Yates <jfy133@gmail.com>
2024-02-27 16:17:02 +1030

    Borrow from amplisim the htslib static lib compliation hack

--- recipes/phynder/build.sh
+++ recipes/phynder/build.sh
@@ -5,14 +5,22 @@ set +e
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-git clone https://github.com/samtools/htslib.git
+# git clone https://github.com/samtools/htslib.git
+# cd htslib
+# git submodule update --init --recursive
+
+# make lib-static htslib_static.mk
+
+## Borrowed from amplisim
+git clone --recursive https://github.com/samtools/htslib.git
 cd htslib
-git submodule update --init --recursive
-
-./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
-
-make lib-static htslib_static.mk
-
+git reset --hard 7f5136f
+cd htscodecs
+git reset --hard 11b5007
+cd ..
+make -C lib/htslib CC=${CC} CFLAGS="${CFLAGS} -g -Wall -O2 -fvisibility=hidden" LDFLAGS="${LDFLAGS} -fvisibility=hidden" lib-static
+mkdir -p build
+make CXX=${CXX} CXXFLAGS="${CXXFLAGS} -O3 -DNDEBUG -Wno-missing-field-initializers -Wno-unused-function" LDLIBS="${LDFLAGS}"
 cd ../
 
 sed -i 's#HTSDIR=../htslib#HTSDIR=./htslib#g' Makefile
