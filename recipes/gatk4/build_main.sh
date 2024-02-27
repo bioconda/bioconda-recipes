191cb70e4e847e99e9c1438dd67a7b4af12c6a61
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2024-02-27 19:29:57 +0100

    adoptium java

--- recipes/gatk4/build_main.sh
+++ recipes/gatk4/build_main.sh
@@ -1,27 +1,46 @@
-#!/bin/bash
 
-BINARY_HOME=$PREFIX/bin
+#!/bin/bash
+JAVA_VERSION=17.0.9_9
 PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+JAVA_DIR=$PREFIX/share/java-$JAVA_VERSION
 
 mkdir -p $PREFIX/bin
 mkdir -p $PACKAGE_HOME
+mkdir -p $JAVA_DIR
 
-#export PYTHONPATH=$PYTHONPATH:$PREFIX/lib/python3.6/site-packages/
-#sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' gatk
-# pip install --ignore-installed numpy==1.17.5
-# pip install scipy==1.0.0
-# pip install pymc3==3.1
-# pip install Theano==1.0.4
-# pip install PyVCF==0.6.8
+#
+# download java - none of the openjdk-17 builds in conda-forge work with gatk, and GATK recommends adoptium's jdk builds so we are using their binaries below. 
+#
+#if [[ ${target_platform} =~ linux.* ]] ; then
+wget -c https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9%2B9/OpenJDK17U-jre_x64_linux_hotspot_$JAVA_VERSION.tar.gz 
+wget -O- -q -T 1 -t 1 https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9%2B9/OpenJDK17U-jre_x64_linux_hotspot_$JAVA_VERSION.tar.gz.sha256.txt | sha256sum -c
+tar -xzf OpenJDK17U-jre_x64_linux_hotspot_$JAVA_VERSION.tar.gz
+#else
+#    echo "operating system not not supported"
+#    exit 1
+#fi
 
+#
+# install java
+#
+mv jdk-17.0.9+9-jre/* $JAVA_DIR
+ls $JAVA_DIR/bin
+ls $JAVA_DIR/lib
+
+#
+# install gatk
+#
 chmod +x gatk
 cp gatk ${PACKAGE_HOME}/gatk
 cp gatk-*-local.jar $PACKAGE_HOME
 
-export GATK_LOCAL_JAR=$PACKAGE_HOME
+#
+# install gatk's python packages 
+#
 unzip gatkPythonPackageArchive.zip -d gatkPythonPackageArchive
 cd gatkPythonPackageArchive
 python setup.py install
 
 # Does not install the spark jars, this is done in the `build_spark.sh`
 ln -s $PACKAGE_HOME/gatk $PREFIX/bin
+ln -s $JAVA_DIR/bin/java $PREFIX/bin
