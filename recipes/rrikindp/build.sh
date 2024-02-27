bb4474dcbf8e26011a08b2f515ed9b6b8b358de8
mwaldl <mwaldl@users.noreply.github.com>
2024-02-27 17:08:14 +0100

    Update to new RRIkinDP release (#46017)
    * remove LDFlags from make call
    
    * update to new release RRIkinDP v0.0.2 with pybind11 wrapper fro core c++ class
    
    * Reset build number
    
    * Add pybind11 dependency
    
    ---------
    
    Co-authored-by: Sebastian Will <swill@csail.mit.edu>

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -4,5 +4,5 @@ $PYTHON -m pip install . --no-deps --ignore-installed -vv
 
 cd src/rrikindp
 
-make -j ${CPU_COUNT} CXXFLAGS+="-I${CONDA_PREFIX}/include" LDFLAGS+="-L${CONDA_PREFIX}/lib"
+make -j ${CPU_COUNT} CXXFLAGS+="-I${CONDA_PREFIX}/include"
 install RRIkinDP "${PREFIX}/bin"
