f8db0493e5ad29bfef37ccd30757b2f02a3b531e
James A. Fellows Yates <jfy133@gmail.com>
2024-02-27 12:11:10 +1030

    Start trying the ZLIB patches

--- recipes/phynder/build.sh
+++ recipes/phynder/build.sh
@@ -2,13 +2,30 @@
 set -x
 set +e
 
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
 git clone https://github.com/samtools/htslib.git
 cd htslib
+git submodule update --init --recursive
+
+sed -i.bak 's/^CPPFLAGS =$//g' Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' Makefile
+
+sed -i.bak 's/^CPPFLAGS =$//g' htslib-$PKG_VERSION/Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' htslib-$PKG_VERSION/Makefile
+
 make lib-static htslib_static.mk
 
-cd -
+cd ../
 
-sed 's#HTSDIR=../htslib#HTSDIR=./htslib#g'
+sed -i.bak 's/^CPPFLAGS =$//g' Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' Makefile
+
+sed -i.bak 's/^CPPFLAGS =$//g' htslib-$PKG_VERSION/Makefile
+sed -i.bak 's/^LDFLAGS  =$//g' htslib-$PKG_VERSION/Makefile
+
+sed -i 's#HTSDIR=../htslib#HTSDIR=./htslib#g' Makefile
+
 make
 make install
-
