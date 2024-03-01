a96ca27b6072fc1b5de2ec4b9a17d305351dafd9
James A. Fellows Yates <jfy133@gmail.com>
2024-03-01 15:56:52 +1030

    Almost there, just need to find out with cnanot find libbz2

--- recipes/phynder/build.sh
+++ recipes/phynder/build.sh
@@ -4,6 +4,7 @@ set +e
 
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 
 git clone https://github.com/samtools/htslib.git
 cd htslib
@@ -20,11 +21,18 @@ autoheader
 autoconf
 
 ./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
+make lib-static htslib_static.mk
 make CC=$CC install
 
 cd ../
 
 sed -i 's#HTSDIR=../htslib#HTSDIR=./htslib#g' Makefile
 
-make
-make install
+## TODO:
+## sed patch LDLIBS to add LDFLAGS to the end of line
+## sed patch Turn off copying
+
+sed -i -e 's#LDLIBS=$(HTSLIB) -lpthread $(HTSLIB_static_LIBS)#LDLIBS=$(HTSLIB) -lpthread $(HTSLIB_static_LIBS)#g' -e 's#cp phynder ~/bin##g' Makefile
+
+make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+make install # Remaining error: ./phynder: error while loading shared libraries: libbz2.so.1.0: cannot open shared object file: No such file or directory
