a5b82f229682f8432a235f6001ac33eabfa692bf
James A. Fellows Yates <jfy133@gmail.com>
2024-03-01 13:56:12 +1030

    Start from beginning, autoconf'd ./configure script isn't working... due to `configure: error: cannot find required auxiliary files: config.guess config.sub`

--- recipes/phynder/build.sh
+++ recipes/phynder/build.sh
@@ -4,41 +4,18 @@ set +e
 
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CXX=${CXX}
 
-# git clone https://github.com/samtools/htslib.git
-# cd htslib
-# git submodule update --init --recursive
-
-# make lib-static htslib_static.mk
-
-## Borrowed from amplisim
-git clone --recursive https://github.com/samtools/htslib.git
+git clone https://github.com/samtools/htslib.git
 cd htslib
-ls -l
-git reset --hard 7f5136f
-ls -l
-cd htscodecs
-ls -l
-git reset --hard 11b5007
-cd ..
-ls -l
-make -C . CC=${CC} CFLAGS="${CFLAGS} -g -Wall -O2 -fvisibility=hidden" LDFLAGS="${LDFLAGS} -fvisibility=hidden" lib-static
-ls -l
-mkdir -p build
-ls -l
-./configure CPPFLAGS="${CPPFLAGS}" \
-    LDFLAGS="${LDFLAGS}" \
-    CXX=${CXX} \
-    CC=${CC}
+git submodule update --init --recursive
 
-## FAILING HERE: THE SAMTOOLS HTSLIB COMPILATION IS NOT WORKING
-## CHECK HTSLIB INSTALLATION DOCS? TRYING CONFIGURE ABOVE...
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDLIBS="${LDFLAGS}"
+autoconf
+./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
+make install
+
 cd ../
-ls -l
-sed -i 's#HTSDIR=../htslib#HTSDIR=./htslib#g' Makefile
-sed -i 's/$@ $(LDLIBS)/$@ $(LDLIBS) $(CFLAGS) $(LDFLAGS)/g' Makefile
 
-make CXX=${CXX} CXXFLAGS="${CXXFLAGS}" LDLIBS="${LDFLAGS}"
+sed -i 's#HTSDIR=../htslib#HTSDIR=./htslib#g' Makefile
+
+make
 make install
