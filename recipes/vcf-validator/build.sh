a10bec0efc279e13d86c6d3684200eb8d045fbe0
tcezard <tcezard@ebi.ac.uk>
2024-03-01 08:44:06 +0000

    Retrieve the prebuilt binaries

--- recipes/vcf-validator/build.sh
+++ recipes/vcf-validator/build.sh
@@ -1,11 +1,13 @@
+
+
 mkdir -p ${PREFIX}/bin
 # Check if linux or osx
 if [ -z ${OSX_ARCH+x} ]; then
-  mv vcf_validator_linux ${PREFIX}/bin/vcf_validator
-  mv vcf_assembly_checker_linux ${PREFIX}/bin/vcf_assembly_checker
+  curl -LJo ${PREFIX}/bin/vcf_validator  https://github.com/EBIvariation/vcf-validator/releases/download/v${PKG_VERSION}/vcf_validator_linux \
+    && curl -LJo ${PREFIX}/bin/vcf_assembly_checker  https://github.com/EBIvariation/vcf-validator/releases/download/v${PKG_VERSION}/vcf_assembly_checker_linux \
+    && chmod 755 ${PREFIX}/bin/vcf_assembly_checker ${PREFIX}/bin/vcf_validator
 else
-  mv vcf_validator_macos ${PREFIX}/bin/vcf_validator
-  mv vcf_assembly_checker_macos ${PREFIX}/bin/vcf_assembly_checker
+  curl -LJo ${PREFIX}/bin/vcf_validator  https://github.com/EBIvariation/vcf-validator/releases/download/v${PKG_VERSION}/vcf_validator_macos \
+    && curl -LJo ${PREFIX}/bin/vcf_assembly_checker  https://github.com/EBIvariation/vcf-validator/releases/download/v${PKG_VERSION}/vcf_assembly_checker_macos \
+    && chmod 755 ${PREFIX}/bin/vcf_assembly_checker ${PREFIX}/bin/vcf_validator
 fi
-
-chmod 755 ${PREFIX}/bin/vcf_assembly_checker ${PREFIX}/bin/vcf_validator
