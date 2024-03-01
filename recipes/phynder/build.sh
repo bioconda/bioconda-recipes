95fbcb2c586669d4d3cf36cf863fa46c42daac6e
James A. Fellows Yates <jfy133@gmail.com>
2024-03-01 14:11:29 +1030

    Try HTSlib developer workaround to missing .sub file

--- recipes/phynder/build.sh
+++ recipes/phynder/build.sh
@@ -9,9 +9,18 @@ git clone https://github.com/samtools/htslib.git
 cd htslib
 git submodule update --init --recursive
 
+## from https://github.com/pachterlab/kallisto/issues/303#issuecomment-884612169
+sed '/AC_PROG_CC/a \
+AC_CANONICAL_HOST \
+AC_PROG_INSTALL \
+' configure.ac >configure.ac2
+mv configure.ac2 configure.ac
+autoreconf -i
+autoheader
 autoconf
+
 ./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
-make install
+make CC=$CC install
 
 cd ../
 
