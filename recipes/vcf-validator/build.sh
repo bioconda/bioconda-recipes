598f521822d597ff0b24db07e35cc9eb853b28ba
tcezard <tcezard@ebi.ac.uk>
2024-03-04 09:54:12 +0000

    add default_ext_library_flag

--- recipes/vcf-validator/build.sh
+++ recipes/vcf-validator/build.sh
@@ -7,9 +7,9 @@ fi
 mkdir build
 cd build
 if [ -z ${OSX_ARCH+x} ]; then
-  cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc ..
+  cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DDEFAULT_EXT_LIB_PATH=${BUILD_PREFIX} ..
 else
-  cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang ..
+  cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DEXT_LIB_PATH=${BUILD_PREFIX} ..
 fi
 make -j2
 cd ..
