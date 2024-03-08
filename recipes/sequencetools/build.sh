d415d2aabf46bdcd81d89940b48ee7d15ca99401
Stephan Schiffels <stephan_schiffels@mac.com>
2024-03-08 16:05:46 +0100

    trying again without ENV-variables set

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
+# export LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+# export LD_LIBRARY_PATH="${PREFIX}/lib:/usr/lib:/usr/lib64"
+# export LDFLAGS="-L${PREFIX}/lib"
+# export CPPFLAGS="-I${PREFIX}/include"
 
 stack setup --local-bin-path ${PREFIX}/bin
 stack update
