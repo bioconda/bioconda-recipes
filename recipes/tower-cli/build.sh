8e7bf2f6dd7adb055049446474442bab8dfd0cf6
Adam Talbot <12817534+adamrtalbot@users.noreply.github.com>
2024-03-11 10:47:02 +0000

    Change build.sh logic slightly

--- recipes/tower-cli/build.sh
+++ recipes/tower-cli/build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-mkdir -p $PREFIX/bin
-mv $SRC_DIR/tw $PREFIX/bin/tw
-chmod 755 $PREFIX/bin/tw
+#!/usr/bin/env bash
+ls "${SRC_DIR}"
+chmod +x "${SRC_DIR}/tw"
+mkdir -p "${PREFIX}/bin"
+mv "${SRC_DIR}"/tw ${PREFIX}/bin/tw
