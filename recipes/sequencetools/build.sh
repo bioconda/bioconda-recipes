902772c874e7657053578c11f6ecdb0d1048e4b3
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-03-11 10:13:06 +0100

    try again self-installling ghc

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -4,7 +4,11 @@
 # export LDFLAGS="-L${PREFIX}/lib"
 # export CPPFLAGS="-I${PREFIX}/include"
 
+wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
+tar xvf ghc-9.4.7-x86_64-centos7-linux.tar.xz
+export PATH="$PATH:~/ghc-9.4.7-x86_64-unknown-linux/bin"
+
 stack install --local-bin-path ${PREFIX}/bin --system-ghc
-#cleanup
+# cleanup
 # rm -r .stack-work
 
