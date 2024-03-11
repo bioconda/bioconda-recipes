e4e850cbf0025f37907e83686b0f977e3f16b9fe
Adam Talbot <12817534+adamrtalbot@users.noreply.github.com>
2024-03-11 10:27:09 +0000

    Use binary for tower-cli, add macOS-arm64

--- recipes/tower-cli/build.sh
+++ recipes/tower-cli/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
-[ -d "$TGT" ] || mkdir -p "$TGT"
-[ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
-
-cp -p "$SRC_DIR"/*.jar "$TGT"
-
-cp $RECIPE_DIR/tw.sh $TGT/tw
-ln -s $TGT/tw $PREFIX/bin
-chmod 0755 "${PREFIX}/bin/tw"
+mkdir -p $PREFIX/bin
+mv $SRC_DIR/tw $PREFIX/bin/tw
+chmod 755 $PREFIX/bin/tw
