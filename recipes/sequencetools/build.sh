0fc5f16ace44cace9b30da8e7bea0ed3cdce579e
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-03-11 08:42:38 +0100

    added ghc as conda-forge dependency

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -4,10 +4,6 @@
 # export LDFLAGS="-L${PREFIX}/lib"
 # export CPPFLAGS="-I${PREFIX}/include"
 
-wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
-tar -xvf ghc-9.4.7-x86_64-centos7-linux.tar.xz
-export PATH="$PATH:$HOME/ghc-9.4.7-x86_64-unknown-linux/bin"
-
 stack install --local-bin-path ${PREFIX}/bin --system-ghc
 #cleanup
 # rm -r .stack-work
