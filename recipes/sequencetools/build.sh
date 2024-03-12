2b03b746bfbd24b4a8a7e74a90ba3d35698061b1
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-03-12 16:39:29 +0100

    no stack call before config.yaml change

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -9,12 +9,12 @@
 # export PATH="$PATH:$PWD/ghc-9.4.7-x86_64-unknown-linux/bin"
 
 
-STACKROOT=$(stack path --stack-root)
-echo "Stack Root: $STACKROOT"
+STACKROOT=/home/conda/.stack # can be evaluated with `stack path --stack-root`
+# echo "Stack Root: $STACKROOT"
 mkdir -p $STACKROOT
 touch $STACKROOT/config.yaml
 
-printf "setup-info:\n  ghc:\n    linux64:\n      9.4.7:\n        url: https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz" >> $STACKROOT/config.yaml
+printf "setup-info:\n  ghc:\n    linux64:\n      9.4.7:\n        url: https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz\n" >> $STACKROOT/config.yaml
 
 cat $STACKROOT/config.yaml
 
