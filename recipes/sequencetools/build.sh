fdd091eca3802bc40b3d630a65a744609e398a59
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-03-12 17:08:07 +0100

    trying downloaded GHC

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -4,21 +4,25 @@
 # export LDFLAGS="-L${PREFIX}/lib"
 # export CPPFLAGS="-I${PREFIX}/include"
 
-# wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
-# tar xf ghc-9.4.7-x86_64-centos7-linux.tar.xz
+wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
+tar xf ghc-9.4.7-x86_64-centos7-linux.tar.xz
+
+echo "GHC version"
+ghc-9.4.7-x86_64-unknown-linux/bin/ghc --version
+
 # export PATH="$PATH:$PWD/ghc-9.4.7-x86_64-unknown-linux/bin"
 
 
-STACKROOT=/home/conda/.stack # can be evaluated with `stack path --stack-root`
-# echo "Stack Root: $STACKROOT"
-mkdir -p $STACKROOT
-touch $STACKROOT/config.yaml
+# STACKROOT=/home/conda/.stack # can be evaluated with `stack path --stack-root`
+# # echo "Stack Root: $STACKROOT"
+# mkdir -p $STACKROOT
+# touch $STACKROOT/config.yaml
 
-printf "setup-info:\n  ghc:\n    linux64:\n      9.4.7:\n        url: https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz\n" >> $STACKROOT/config.yaml
+# printf "setup-info:\n  ghc:\n    linux64:\n      9.4.7:\n        url: https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz\n" >> $STACKROOT/config.yaml
 
-cat $STACKROOT/config.yaml
+# cat $STACKROOT/config.yaml
 
-stack install --local-bin-path ${PREFIX}/bin
-# cleanup
-# rm -r .stack-work
+# stack install --local-bin-path ${PREFIX}/bin
+# # cleanup
+# # rm -r .stack-work
 
