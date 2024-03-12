8bbdb1af9e6fb5fe0c8a84bb5c2ef87f0a84964e
Stephan Schiffels <stephan_schiffels@mac.com>
2024-03-12 10:29:31 +0100

    added patch for stack config

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -4,14 +4,19 @@
 # export LDFLAGS="-L${PREFIX}/lib"
 # export CPPFLAGS="-I${PREFIX}/include"
 
-wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
-tar xf ghc-9.4.7-x86_64-centos7-linux.tar.xz
-export PATH="$PATH:$PWD/ghc-9.4.7-x86_64-unknown-linux/bin"
+# wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
+# tar xf ghc-9.4.7-x86_64-centos7-linux.tar.xz
+# export PATH="$PATH:$PWD/ghc-9.4.7-x86_64-unknown-linux/bin"
 
-echo $PATH
-which ghc
 
-stack install --local-bin-path ${PREFIX}/bin --system-ghc --no-install-ghc
+STACKROOT=$(stack path --stack-root)
+echo "Stack Root: $STACKROOT"
+mkdir -p $STACKROOT
+touch $STACKROOT/config.yaml
+
+echo "setup-info:\n  ghc:\n    linux64:\n      9.4.7:\n        url: https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz" >> $STACKROOT/config.yaml
+
+stack install --local-bin-path ${PREFIX}/bin
 # cleanup
 # rm -r .stack-work
 
