f3f1f57190e36124b14f2ae4a6445afa29389b77
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-03-13 07:13:18 +0100

    copying ghc

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -10,16 +10,10 @@ tar xf ghc-9.4.7-x86_64-centos7-linux.tar.xz
 echo "GHC version"
 ghc-9.4.7-x86_64-unknown-linux/bin/ghc --version
 
-echo "SRC_DIR:"
-echo $SRC_DIR
+cp ghc-9.4.7-x86_64-unknown-linux/bin/ghc ${PREFIX}/bin/ghc
 
-echo "pwd:"
-pwd
-
-export PATH="$PATH:$SRC_DIR/ghc-9.4.7-x86_64-unknown-linux/bin"
-
-echo "PATH:"
-echo $PATH
+echo "PREFIX/bin:"
+echo ${PREFIX}/bin
 
 echo "which ghc":
 which ghc
