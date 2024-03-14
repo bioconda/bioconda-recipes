66c3a6649a8c524d63aeeacf84c8eaa00d73facc
nickjcroucher <n.croucher@imperial.ac.uk>
2024-03-14 20:11:36 +0000

    Update gubbins (#46410)
    * Update python version constraints
    
    * Reset build number
    
    * Limits to python version
    
    * Change otool version for osx

--- recipes/gubbins/build.sh
+++ recipes/gubbins/build.sh
@@ -3,6 +3,14 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
+if [[ $target_platform == osx-* ]]; then
+    for toolname in "otool" "install_name_tool"; do
+        tool=$(find "${BUILD_PREFIX}/bin/" -name "*apple*-$toolname")
+        mv "${tool}" "${tool}.bak"
+        ln -s "/Library/Developer/CommandLineTools/usr/bin/${toolname}" "$tool"
+    done
+fi
+
 autoreconf -i
 ./configure --prefix=$PREFIX
 make
