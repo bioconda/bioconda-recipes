ecff56b7d62a0c35812b692bc3b5c3605dfc2c92
Kirill Bessonov <kbessonov@gmail.com>
2024-03-14 12:53:22 -0400

    updated pandas req

--- recipes/sistr_cmd/build.sh
+++ recipes/sistr_cmd/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
--m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir  -vvv
+python -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir  -vvv
