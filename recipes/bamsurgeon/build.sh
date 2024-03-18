83fea01e4c10784fdbaca5f47f4b5f6b8cda5209
Tim15-tech <tmfy_mail-studium@yahoo.com>
2024-03-18 16:59:12 +0100

    Bamsurgeon successfully building and wokring with bamsurgeon python3 test command available on website. However, test is only ls and seperately finding the readme as pipeline operator for find ... | grep is not working in test within meta.yaml. Furthermore, source_files fails to find data for the python3 command and as it is a standalone python-script, I don't know how to navigate to the folder with the scripts for executing python3 testcommandfromgit. Otherwise, if this is somehow possible, testing for finding the data or if exit code is already 0 is suitable.

--- recipes/bamsurgeon/build.sh
+++ recipes/bamsurgeon/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd $PREFIX
 wget https://github.com/broadinstitute/picard/releases/download/1.131/picard-tools-1.131.zip
 unzip picard-tools-1.131.zip
---
+++ recipes/bamsurgeon/run_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/tim/Bachelorprojekt/TroubleshootEnvs/bamsurgeon_1710524732931/work_moved_bamsurgeon-1.4.1-py312hdfd78af_0_noarch
+pwd
+find -name README.md | grep .
