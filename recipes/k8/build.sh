136868de7b62aa20088dada9dcee6409e6b88d6a
guozhicong <juiceonion@foxmail.com>
2024-03-18 14:03:05 +0800

    add k8-1.0: fix build.sh

--- recipes/k8/build.sh
+++ recipes/k8/build.sh
@@ -5,9 +5,9 @@ mkdir -p $PREFIX/bin
 export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 
-cp /root/v18.17.0.tar.gz .
+#cp /root/v18.17.0.tar.gz .
 #NOTE:Please prepare this "v18.17.0.tar.gz" package,Copy it to this directory,Otherwise,you can execute the following command to download this "v18.17.0.tar.gz" package
-#wget https://github.com/nodejs/node/archive/refs/tags/v18.17.0.tar.gz --no-ch
+wget https://github.com/nodejs/node/archive/refs/tags/v18.17.0.tar.gz --no-ch
 tar xf v18.17.0.tar.gz
 cd node-18.17.0
 ./configure
