3a1e0d51c71ee43ebbf6e2963892b9dfd866ab55
Tim15-tech <tmfy_mail-studium@yahoo.com>
2024-03-18 18:54:03 +0100

    Changed run_test to cd instead locally valid cd PATH. Works locally with build command.

--- recipes/bamsurgeon/run_test.sh
+++ recipes/bamsurgeon/run_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd /home/tim/Bachelorprojekt/TroubleshootEnvs/bamsurgeon_1710524732931/work_moved_bamsurgeon-1.4.1-py312hdfd78af_0_noarch
-pwd
+cd $PREFIX
 find -name README.md | grep .
