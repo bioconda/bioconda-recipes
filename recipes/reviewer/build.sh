26c30e58be441654f1ca85ae471be70658359123
Michael Knudsen <micknudsen@gmail.com>
2024-03-19 08:42:12 +0100

    Unzip precombiled binary

--- recipes/reviewer/build.sh
+++ recipes/reviewer/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ls -laR
-
 mkdir -p ${PREFIX}/bin
-mv REViewer-v${PKG_VERSION}-linux_x86_64 ${PREFIX}/bin/REViewer
+zcat REViewer-v${PKG_VERSION}-linux_x86_64.gz > ${PREFIX}/bin/REViewer
 chmod +x ${PREFIX}/bin/REViewer
