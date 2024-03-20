87e0e8daf8421622cfe16ff8a098012ed11bd22c
Jonas Scheid <43858870+jonasscheid@users.noreply.github.com>
2024-03-20 14:25:20 +0100

    Update recipes/comet-ms/build.sh
    Co-authored-by: Julianus Pfeuffer <julianus.pfeuffer@pfizer.com>

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -7,7 +7,7 @@ export CPPFLAGS="-I${PREFIX}/MSToolkit/include"
 
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
-sed -i.bak 's#$(LIBS)#-lrt $(LIBS)#' Makefile
+sed -i.bak 's#-static##' Makefile
 
 make CXX=${CXX}
 
