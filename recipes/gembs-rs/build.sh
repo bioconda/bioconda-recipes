7a365d0fa73ee5f13742dbfd9fb4d869e1a6fde7
Addimator <44083468+Addimator@users.noreply.github.com>
2024-03-21 12:04:28 +0100

    Create build.sh with Makefile commands

---
+++ recipes/gembs-rs/build.sh
@@ -0,0 +1,7 @@
+cargo build --release $prefix/bin
+cp target/release/gem_bs $prefix/bin/gemBS
+cp target/release/read_filter $prefix/bin/
+cp target/release/bs_call $prefix/bin/
+cp target/release/snpxtr $prefix/bin/
+cp target/release/mextr $prefix/bin/
+cp target/release/dbsnp_index $prefix/bin/
