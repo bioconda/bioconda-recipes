03151266a705e4911e8092fc73a95e4bedbd7fee
Julianus Pfeuffer <julianus.pfeuffer@pfizer.com>
2024-03-22 09:48:12 +0000

    static with glibc 2.17

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -5,7 +5,9 @@ set -x
 export INCLUDE_PATH="${PREFIX}/MSToolkit/include"
 export CPPFLAGS="-I${PREFIX}/MSToolkit/include"
 
-sed -i.bak 's#-static##' Makefile
+# To switch from static to dynamic linking on linux. The Makefile does not have this flag for macOS
+# Dynamic linking does not work with mulled container tests
+#sed -i.bak 's#-static##' Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
 
