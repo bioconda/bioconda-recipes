d3b4392d72cd9fe818e65460a3f24a0a4a1df923
Paolo Ribeca <paolo.ribeca@gmail.com>
2024-03-22 01:01:16 +0000

    Update build.sh

--- recipes/kpop/build.sh
+++ recipes/kpop/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Installing KPop for OSX."
-else 
-    echo "Installing KPop for Linux"
-fi
 
 mkdir -p $PREFIX/bin
 cp $SRC_DIR/KPop* $PREFIX/bin
