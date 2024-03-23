5f477a69093c4b873a3e13f3f0c78c20bf844760
John Marshall <jmarshall@hey.com>
2024-03-23 23:56:01 +1300

    Install the thing by hand

--- recipes/reviewer/build.sh
+++ recipes/reviewer/build.sh
@@ -4,4 +4,5 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
-cmake --install .
+mkdir -p $PREFIX/bin
+install -m 755 install/bin/REViewer $PREFIX/bin
