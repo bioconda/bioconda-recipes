1d7c601218c926eee94f664c650a94cc81c26e62
aliciaaevans <alicia.evans@nih.gov>
2024-03-26 11:49:02 -0400

    try 11.0 sdk

--- recipes/bioawk/build.sh
+++ recipes/bioawk/build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 export LIBRARY_PATH="$PREFIX/lib"
-export ARCHFLAGS="-arch $(uname -m)"
-export MACOSX_DEPLOYMENT_TARGET=14.0
-
-echo "MACOSX_SDK_VERSION: "
-echo $MACOSX_SDK_VERSION
-echo "MACOSX_DEPLOYMENT_TARGET: "
-echo $MACOSX_DEPLOYMENT_TARGET
 
 make CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
 
