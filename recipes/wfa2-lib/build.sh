0dcac29689b4154a9f084007399a2a5ed7337826
mencian <joshua.zhuang@yahoo.com>
2024-03-25 23:33:03 -0500

    edit build.sh

--- recipes/wfa2-lib/build.sh
+++ recipes/wfa2-lib/build.sh
@@ -5,11 +5,16 @@ export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+if [ $(uname -m) == "aarch64" ]; then
+	export EXTRA_FLAGS="-ftree-vectorize"
+else
+	export EXTRA_FLAGS="-ftree-vectorize -msse2 -mfpmath=sse"
+
 cmake -S . -B build \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_FLAGS="-O3 -D_FILE_OFFSET_BITS=64 -I${PREFIX}/include ${LDFLAGS}" \
-	-DEXTRA_FLAGS="-ftree-vectorize -msse2 -mfpmath=sse" \
+	-DEXTRA_FLAGS="${EXTRA_FLAGS}" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DOPENMP=TRUE
