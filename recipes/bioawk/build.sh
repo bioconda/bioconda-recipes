6f341bca3c56cb316b873e56898dd317e70b8c82
aliciaaevans <alicia.evans@nih.gov>
2024-03-26 10:44:28 -0400

    move logging

--- recipes/bioawk/build.sh
+++ recipes/bioawk/build.sh
@@ -2,12 +2,12 @@
 export LIBRARY_PATH="$PREFIX/lib"
 export ARCHFLAGS="-arch $(uname -m)"
 
-make CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
-
-mkdir -p $PREFIX/bin
-cp bioawk $PREFIX/bin
-
 echo "MACOSX_SDK_VERSION: "
 echo $MACOSX_SDK_VERSION
 echo "MACOSX_DEPLOYMENT_TARGET: "
 echo $MACOSX_DEPLOYMENT_TARGET
+
+make CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
+
+mkdir -p $PREFIX/bin
+cp bioawk $PREFIX/bin
