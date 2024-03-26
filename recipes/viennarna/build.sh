dbe26666aba4ffbfabb9665f97809b52446fc50d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-03-26 14:15:29 +0200

    viennarna: Skip build for OSX because of compilation problems with naview
    `--without-naview` breaks with:
    ```
    2024-03-26T09:58:55.7422620Z 09:58:55 ␛[32mBIOCONDA INFO␛[0m (ERR) Undefined symbols for architecture x86_64:␛[0m
    2024-03-26T09:58:55.7424070Z 09:58:55 ␛[32mBIOCONDA INFO␛[0m (ERR)   "_naview_xy_coordinates", referenced from:␛[0m
    2024-03-26T09:58:55.7425890Z 09:58:55 ␛[32mBIOCONDA INFO␛[0m (ERR)       _initStacks in libRNA_special_const.a(ugly_bt.o)␛[0m
    2024-03-26T09:58:55.7427900Z 09:58:55 ␛[32mBIOCONDA INFO␛[0m (ERR) ld: symbol(s) not found for architecture x86_64␛[0m
    2024-03-26T09:58:55.7429230Z 09:58:55 ␛[32mBIOCONDA INFO␛[0m (ERR) clang-15: error: linker command failed with exit code 1 (use -v to see invocation)␛[0m
    ```
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/viennarna/build.sh
+++ recipes/viennarna/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -ex
-
 ## Choose extra configure options depending on the operating system
 ## (mac or linux)
 ##
 if [ `uname` == Darwin ] ; then
-    extra_config_options="--disable-openmp --disable-naview LDFLAGS=-Wl,-headerpad_max_install_names"
+    extra_config_options="LDFLAGS=-Wl,-headerpad_max_install_names"
 fi
 
 ## Configure and make
