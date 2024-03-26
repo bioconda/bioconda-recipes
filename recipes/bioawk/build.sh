6860ca9d06da9f605d49a5404e3728094e08c137
aliciaaevans <alicia.evans@nih.gov>
2024-03-26 10:10:02 -0400

    check sdk versions

--- recipes/bioawk/build.sh
+++ recipes/bioawk/build.sh
@@ -6,3 +6,8 @@ make CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
 
 mkdir -p $PREFIX/bin
 cp bioawk $PREFIX/bin
+
+echo "MACOSX_SDK_VERSION: "
+echo $MACOSX_SDK_VERSION
+echo "MACOSX_DEPLOYMENT_TARGET: "
+echo $MACOSX_DEPLOYMENT_TARGET
