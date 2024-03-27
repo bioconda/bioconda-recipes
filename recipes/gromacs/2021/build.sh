67d91828d10730e345b45fe9634ebc042d8dfe28
HWsunqian0027 <sunny_sunqian@163.com>
2024-03-27 16:35:53 +0800

    fix gromacs

--- recipes/gromacs/2021/build.sh
+++ recipes/gromacs/2021/build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+# gromacs > 2021 requires a non default OSX version
+if [ "$(uname)" = 'Darwin' ] ; then
+    export MACOSX_DEPLOYMENT_TARGET=10.13 
+fi
+
+
 mkdir build
 cd build
 
-if [[ "$HOST" == "arm64-apple-darwin"* ]];
-then
-    # Assume ARM Mac
-    simdflavors=(ARM_NEON_ASIMD)
-else
-    # Assume x86
-    simdflavors=(SSE2 AVX_256 AVX2_256)
-fi
-
 ## See INSTALL of gromacs distro
-for simdflavor in "${simdflavors[@]}" ; do
+for ARCH in SSE2 AVX_256 AVX2_256; do
   cmake_args=(
     -DSHARED_LIBS_DEFAULT=ON
     -DBUILD_SHARED_LIBS=ON
@@ -23,40 +20,21 @@ for simdflavor in "${simdflavors[@]}" ; do
     # Tests are not currently run, so do not download them
     # -DREGRESSIONTEST_DOWNLOAD=ON
     -DCMAKE_PREFIX_PATH="${PREFIX}"
+    -DGMX_INSTALL_PREFIX="${PREFIX}"
     -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-    -DGMX_SIMD="${simdflavor}"
-    -DCMAKE_INSTALL_BINDIR="bin.${simdflavor}"
-    -DCMAKE_INSTALL_LIBDIR="lib.${simdflavor}"
-    -DGMX_VERSION_STRING_OF_FORK="conda-forge"
-    -DGMX_INSTALL_LEGACY_API=ON
-    -DGMX_USE_RDTSCP=OFF
+    -DGMX_SIMD="${ARCH}"
+    -DCMAKE_INSTALL_BINDIR="bin.${ARCH}"
+    -DCMAKE_INSTALL_LIBDIR="lib.${ARCH}"
+    -DGMX_VERSION_STRING_OF_FORK="bioconda"
   )
   # OpenCL header on Mac is not recognized by GROMACS
-  if [[ "$(uname)" != 'Darwin' && "${double}" == "no" ]] ; then
+  if [ "$(uname)" != 'Darwin' ] ; then
       cmake_args+=(-DGMX_GPU=OpenCL)
   fi
   if [[ "${mpi}" == "nompi" ]]; then
-      cmake_args+=(-DGMX_MPI=OFF -DGMX_THREAD_MPI=ON)
+      cmake_args+=(-DGMX_MPI=OFF)
   else
       cmake_args+=(-DGMX_MPI=ON)
-  fi
-  if [[ "${double}" == "yes" ]]; then
-      cmake_args+=(-DGMX_DOUBLE=ON)
-      cmake_args+=(-DGMX_GPU=OFF)
-  else
-      cmake_args+=(-DGMX_DOUBLE=OFF)
-  fi
-  if [[ "${cuda_compiler_version}" != "None" ]]; then
-      cmake_args+=(-DGMX_GPU=CUDA)
-  fi
-  if [[ "$(uname)" == 'Darwin' ]] ; then
-      # The clang compiler used on MacOS assumes the system libc++ is
-      # in use, and because conda targets a very old platform, that
-      # fails tests for C++17. However conda will later provide its
-      # own full-featured libc++, so we can use some magic to make the
-      # compiler checks pass. See
-      # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-      cmake_args+=(-DCMAKE_CXX_FLAGS='-D_LIBCPP_DISABLE_AVAILABILITY')
   fi
   cmake .. "${cmake_args[@]}"
   make -j "${CPU_COUNT}"
@@ -82,30 +60,14 @@ done
 
 
 if [ "${mpi}" = 'nompi' ] ; then
-    if [ "${double}" = 'no' ] ; then
-        gmx='gmx'
-    else
-        gmx='gmx_d'
-    fi
+    gmx='gmx'
 else
-    if [ "${double}" = 'no' ] ; then
-        gmx='gmx_mpi'
-    else
-        gmx='gmx_mpi_d'
-    fi
+    gmx='gmx_mpi'
 fi
 
 mkdir -p "${PREFIX}/etc/conda/activate.d"
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
 touch "${PREFIX}/bin/${gmx}"
 chmod +x "${PREFIX}/bin/${gmx}"
-
-# Copy the deactivate scripts for conda
-cp "${RECIPE_DIR}/gromacs_deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/"
-cp "${RECIPE_DIR}/gromacs_deactivate.csh" "${PREFIX}/etc/conda/deactivate.d/"
-
-
-# Create the activate scripts for conda
 
 # We need to find CPU-type descriptors so we know which GROMACS SIMD flavor
 # to use. On Darwin, `sysctl -a` can have lines that look like
@@ -156,31 +118,23 @@ case "$OSTYPE" in
              ;;
 esac
 
-### Bash script (for activate and for direct access through conda run)
 # Search first for AVX2, then AVX. Fall back on SSE2
 { cat <<EOF
 #! /bin/bash
 
 function _gromacs_bin_dir() {
-  local simdflavor
-  local uname=\$(uname -m)
-  if [[ "\$uname" == "arm" ]]; then
-    # Assume ARM Mac
-    test -d "${PREFIX}/bin.ARM_NEON_ASIMD" && \
-      simdflavor='ARM_NEON_ASIMD'
-  else
-    simdflavor='SSE2'
-    case \$( ${hardware_info_command} ) in
-      *\ avx2\ * | *avx2_0*)
-        test -d "${PREFIX}/bin.AVX2_256" && \
-          simdflavor='AVX2_256'
-      ;;
-      *\ avx\ * | *avx1_0*)
-        test -d "${PREFIX}/bin.AVX_256" && \
-          simdflavor='AVX_256'
-    esac
-  fi
-  printf '%s' "${PREFIX}/bin.\${simdflavor}"
+  local arch
+  arch='SSE2'
+  case \$( ${hardware_info_command} ) in
+    *\ avx2\ * | *avx2_0*)
+      test -d "${PREFIX}/bin.AVX2_256" && \
+        arch='AVX2_256'
+    ;;
+    *\ avx\ * | *avx1_0*)
+      test -d "${PREFIX}/bin.AVX_256" && \
+        arch='AVX_256'
+  esac
+  printf '%s' "${PREFIX}/bin.\${arch}"
 }
 
 EOF
@@ -193,34 +147,3 @@ EOF
 cat >> "${PREFIX}/bin/${gmx}" <<EOF
 exec "\$( _gromacs_bin_dir )/${gmx}" "\${@}"
 EOF
-
-### Tcsh script (only for activate)
-{ cat <<EOF
-#! /bin/tcsh
-
-setenv uname `uname -m`
-if ( `uname -m` == "arm" && -d "${PREFIX}/bin.ARM_NEON_ASIMD" ) then ) then
-   setenv simdflavor ARM_NEON_ASIMD
-else
-
-    setenv hwlist `${hardware_info_command}`
-
-    if ( `echo \$hwlist | grep -c 'avx512f'` > 0 && -d "${PREFIX}/bin.AVX_512" && `"${PREFIX}/bin.AVX_512/identifyavx512fmaunits" | grep -c 2` > 0 ) then
-        setenv simdflavor AVX_512
-    else 
-        if ( `echo \$hwlist | grep -c avx2` > 0 && -d "${PREFIX}/bin.AVX2_256" ) then
-            setenv simdflavor AVX2_256
-        else
-            if ( `echo \$hwlist | grep -c avx` > 0 && -d "${PREFIX}/bin.AVX_256" ) then
-                setenv simdflavor AVX_256
-            else
-                setenv simdflavor SSE2
-            endif
-        endif
-    endif
-endif
-
-source "${PREFIX}/bin.\$simdflavor/GMXRC"
-
-EOF
-} > "${PREFIX}/etc/conda/activate.d/gromacs_activate.csh"
