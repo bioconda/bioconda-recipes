69b866b44bc2b210b3eb2819d4ba1855c91829e6
KristinaGagalova <kristina.gagalova@curtin.edu.au>
2024-03-27 15:02:19 +0800

    recipe ripcal-derip

---
+++ recipes/ripcal/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Extracting the downloaded ZIP archive
+unzip ${SRC_DIR}/{{ name }}_install.zip -d ${SRC_DIR}/extracted_files
+
+# Creating necessary directories
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/share/{{ name }}
+
+# Copying executable files to bin directory
+cp ${SRC_DIR}/extracted_files/perl/* ${PREFIX}/bin/
+
+# Copying manual PDF to share directory
+cp ${SRC_DIR}/RIPCAL_manual_v1_0.pdf ${PREFIX}/share/{{ name }}/
+
+# Fixing permissions
+chmod +x ${PREFIX}/bin/deripcal
+chmod +x ${PREFIX}/bin/ripcal
+chmod +x ${PREFIX}/bin/ripcal_summarise
