842aa165d0c4856be4dab244d2d603196cf91864
KristinaGagalova <kristina.gagalova@curtin.edu.au>
2024-03-27 15:31:47 +0800

    fixing errors

--- recipes/ripcal/build.sh
+++ recipes/ripcal/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Extracting the downloaded ZIP archive
-unzip ${SRC_DIR}/{{ name }}_install.zip -d ${SRC_DIR}/extracted_files
+unzip ${SRC_DIR}/{{ name }}_install.zip -d ${SRC_DIR}/ripcal2
 
 # Creating necessary directories
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/{{ name }}
 
 # Copying executable files to bin directory
-cp ${SRC_DIR}/extracted_files/perl/* ${PREFIX}/bin/
+cp ${SRC_DIR}/ripcal2/perl/* ${PREFIX}/bin/
 
 # Copying manual PDF to share directory
 cp ${SRC_DIR}/RIPCAL_manual_v1_0.pdf ${PREFIX}/share/{{ name }}/
