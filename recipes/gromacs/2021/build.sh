7271c1c4d5a639370b988afd72ddc295311058ad
HWsunqian0027 <sunny_sunqian@163.com>
2024-03-27 17:31:09 +0800

    fix gromacs

--- recipes/gromacs/2021/build.sh
+++ recipes/gromacs/2021/build.sh
@@ -11,48 +11,75 @@ cd build
 
 ## See INSTALL of gromacs distro
 if [ `arch` = 'aarch64' ] ; then
-  cmake_args=(
-    -DSHARED_LIBS_DEFAULT=ON
-    -DBUILD_SHARED_LIBS=ON
-    -DGMX_PREFER_STATIC_LIBS=NO
-    -DGMX_BUILD_OWN_FFTW=OFF
-    -DGMX_DEFAULT_SUFFIX=ON
-    -DREGRESSIONTEST_DOWNLOAD=ON
-    -DGMX_GPU=OpenCL
-    -DCMAKE_PREFIX_PATH="${PREFIX}"
-    -DGMX_INSTALL_PREFIX="${PREFIX}"
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-    -DCMAKE_INSTALL_BINDIR="bin.${ARCH}"
-    -DCMAKE_INSTALL_LIBDIR="lib.${ARCH}"
-    -DGMX_MPI=ON
-  )
-  cmake .. "${cmake_args[@]}"
-  make -j "${CPU_COUNT}"
-  make install
+
+cmake_args=(
+-DSHARED_LIBS_DEFAULT=ON
+-DBUILD_SHARED_LIBS=ON
+-DGMX_PREFER_STATIC_LIBS=NO
+-DGMX_BUILD_OWN_FFTW=OFF
+-DGMX_DEFAULT_SUFFIX=ON
+# Tests are not currently run, so do not download them
+# -DREGRESSIONTEST_DOWNLOAD=ON
+-DCMAKE_PREFIX_PATH="${PREFIX}"
+-DGMX_INSTALL_PREFIX="${PREFIX}"
+-DCMAKE_INSTALL_PREFIX="${PREFIX}"
+-DCMAKE_INSTALL_BINDIR="bin"
+-DCMAKE_INSTALL_LIBDIR="lib"
+-DGMX_VERSION_STRING_OF_FORK="bioconda"
+)
+# OpenCL header on Mac is not recognized by GROMACS
+if [ "$(uname)" != 'Darwin' ] ; then
+  cmake_args+=(-DGMX_GPU=OpenCL)
+fi
+if [[ "${mpi}" == "nompi" ]]; then
+  cmake_args+=(-DGMX_MPI=OFF)
+else
+  cmake_args+=(-DGMX_MPI=ON)
+fi
+cmake .. "${cmake_args[@]}"
+make -j "${CPU_COUNT}"
+make install
+
+
 else
 
-for ARCH in SSE2 AVX_256 AVX2_256 AVX_512; do
+for ARCH in SSE2 AVX_256 AVX2_256; do
   cmake_args=(
     -DSHARED_LIBS_DEFAULT=ON
     -DBUILD_SHARED_LIBS=ON
     -DGMX_PREFER_STATIC_LIBS=NO
     -DGMX_BUILD_OWN_FFTW=OFF
     -DGMX_DEFAULT_SUFFIX=ON
-    -DREGRESSIONTEST_DOWNLOAD=ON
-    -DGMX_GPU=OpenCL
+    # Tests are not currently run, so do not download them
+    # -DREGRESSIONTEST_DOWNLOAD=ON
     -DCMAKE_PREFIX_PATH="${PREFIX}"
     -DGMX_INSTALL_PREFIX="${PREFIX}"
     -DCMAKE_INSTALL_PREFIX="${PREFIX}"
     -DGMX_SIMD="${ARCH}"
     -DCMAKE_INSTALL_BINDIR="bin.${ARCH}"
     -DCMAKE_INSTALL_LIBDIR="lib.${ARCH}"
-    -DGMX_MPI=ON
+    -DGMX_VERSION_STRING_OF_FORK="bioconda"
   )
+  # OpenCL header on Mac is not recognized by GROMACS
+  if [ "$(uname)" != 'Darwin' ] ; then
+      cmake_args+=(-DGMX_GPU=OpenCL)
+  fi
+  if [[ "${mpi}" == "nompi" ]]; then
+      cmake_args+=(-DGMX_MPI=OFF)
+  else
+      cmake_args+=(-DGMX_MPI=ON)
+  fi
   cmake .. "${cmake_args[@]}"
   make -j "${CPU_COUNT}"
   make install
 done
 fi
+
+
+
+
+
+
 
 
 #
