df5971cd651651b85aa3ea0e3bc90b6e913d556d
mencian <joshua.zhuang@yahoo.com>
2024-03-28 20:23:35 -0500

    edit build.sh

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -66,3 +66,5 @@ echo "export TRINITY_HOME=${TRINITY_HOME}" > ${PREFIX}/etc/conda/activate.d/${PK
 
 mkdir -p ${PREFIX}/etc/conda/deactivate.d/
 echo "unset TRINITY_HOME" > ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}-${PKG_VERSION}.sh
+
+cd ${RECIPE_DIR} && make clean
