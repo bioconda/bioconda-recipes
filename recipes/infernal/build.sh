ed595191f4abe5c7f2a521a1d4c60fa7e2aa5038
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-03 16:18:09 +0300

    infernal: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/infernal/build.sh
+++ recipes/infernal/build.sh
@@ -4,7 +4,16 @@ set -ex
 
 grep -l -r "/usr/bin/perl" . | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g'
 
+case $(uname -m) in
+    "x86_64") 
+        ARCH_OPTS="--enable-sse" 
+        ;;
+    *) 
+        ARCH_OPTS="" 
+        ;;
+esac
+
 autoreconf -i
-./configure --prefix="${PREFIX}" --enable-sse
+./configure --prefix="${PREFIX}" "${ARCH_OPTS}"
 make -j 2
 make install
