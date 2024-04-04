86c148200afc498470372675fcf5dce0c78c3d6f
Addimator <44083468+Addimator@users.noreply.github.com>
2024-04-04 11:08:10 +0200

    Update recipes/gembs-rs/build.sh
    Co-authored-by: Felix Wiegand <fxwiegand@wgdnet.de>

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -1,6 +1,8 @@
 pwd
 cd rust
+cp $prefix/bin/gemBS/Cargo.toml.in $prefix/bin/gemBS/Cargo.toml
 ls -ahl
+ls -ahl $prefix/bin/gemBS
 cargo build --release
 cp target/release/gem_bs $prefix/bin/gemBS
 cp target/release/read_filter $prefix/bin/
