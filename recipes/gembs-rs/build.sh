a011f85756fae5483ba7c2ebe7c09d6e8848aa21
Addimator <44083468+Addimator@users.noreply.github.com>
2024-04-04 11:43:49 +0200

    redo changes

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -1,15 +1,12 @@
-# pwd
-# cd rust
-# cp $prefix/bin/gemBS/Cargo.toml.in $prefix/bin/gemBS/Cargo.toml
-# ls -ahl
-# ls -ahl $prefix/bin/gemBS
-# cargo build --release
-# cp target/release/gem_bs $prefix/bin/gemBS
-# cp target/release/read_filter $prefix/bin/
-# cp target/release/bs_call $prefix/bin/
-# cp target/release/snpxtr $prefix/bin/
-# cp target/release/mextr $prefix/bin/
-# cp target/release/dbsnp_index $prefix/bin/
 pwd
-make gemBS_config.mk
-make install
+cd rust
+cp gemBS/Cargo.toml.in gemBS/Cargo.toml
+ls -ahl
+ls -ahl gemBS
+cargo build --release
+cp target/release/gem_bs $prefix/bin/gemBS
+cp target/release/read_filter $prefix/bin/
+cp target/release/bs_call $prefix/bin/
+cp target/release/snpxtr $prefix/bin/
+cp target/release/mextr $prefix/bin/
+cp target/release/dbsnp_index $prefix/bin/
