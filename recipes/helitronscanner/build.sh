e23f54abacee23b1b2460747322858ab98688b04
Usman Rashid <usman@smme.edu.pk>
2024-04-05 10:57:56 +1300

    Added a wrapper for HelitronScanner.jar

--- recipes/helitronscanner/build.sh
+++ recipes/helitronscanner/build.sh
@@ -11,20 +11,18 @@ mkdir -p ${HELITRONSCANNER_TRAININGSET_PATH}
 cp -r HelitronScanner/* ${HELITRONSCANNER_DIR}
 cp -r TrainingSet/* ${HELITRONSCANNER_TRAININGSET_PATH}
 
-ln -s ${HELITRONSCANNER_DIR}/HelitronScanner.jar ${PREFIX}/bin/HelitronScanner.jar
-chmod +x ${PREFIX}/bin/HelitronScanner.jar
+cp ${RECIPE_DIR}/HelitronScanner.sh ${HELITRONSCANNER_DIR}/HelitronScanner
+chmod +x ${HELITRONSCANNER_DIR}/HelitronScanner
+ln -s ${HELITRONSCANNER_DIR}/HelitronScanner ${PREFIX}/bin
 
-ln -s ${HELITRONSCANNER_DIR}/scala-actors.jar ${PREFIX}/bin/scala-actors.jar
-ln -s ${HELITRONSCANNER_DIR}/scala-library.jar ${PREFIX}/bin/scala-library.jar
-ln -s ${HELITRONSCANNER_DIR}/xww.basic.jar ${PREFIX}/bin/xww.basic.jar
-
-## Set HelitronScanner variables on env activation
-
+## Set HelitronScanner variables on env activation for training set access
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
 cat <<EOF >> ${PREFIX}/etc/conda/activate.d/helitronscanner.sh
+export HELITRONSCANNER_PATH=${HELITRONSCANNER_DIR}
 export HELITRONSCANNER_TRAININGSET_PATH=${HELITRONSCANNER_TRAININGSET_PATH}
 EOF
 
 cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/helitronscanner.sh
+unset HELITRONSCANNER_PATH
 unset HELITRONSCANNER_TRAININGSET_PATH
 EOF
