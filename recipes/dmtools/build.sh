546b2549a4b5370413b95f97b70266440e6c8d9c
ZhouQiangwei <1010170266@qq.com>
2024-04-04 22:01:56 +0800

    up

--- recipes/dmtools/build.sh
+++ recipes/dmtools/build.sh
@@ -3,13 +3,12 @@
 export C_INCLUDE_PATH=${PREFIX}/include;${BUILD_PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-#export CFLAGS="-I$BUILD_PREFIX/include"
-#export LDFLAGS="-L$BUILD_PREFIX/lib"
-
 make CXX=$CXX CPP=$CXX CC=$CC CFLAGS="-g -w -O3 -Wsign-compare -I$BUILD_PREFIX/include"
-#CFLAGS="${CFLAGS} -fcommon"
-#LDFLAGS=""
-#make CXX=$CXX CPP=$CXX CC=$CC LDLIBS="-L$PREFIX/lib -lz -lzstd -ltbb -ltbbmalloc -lpthread" WITH_ZSTD=1
+##export CFLAGS="-I$BUILD_PREFIX/include"
+##export LDFLAGS="-L$BUILD_PREFIX/lib"
+##CFLAGS="${CFLAGS} -fcommon"
+##LDFLAGS=""
+##make CXX=$CXX CPP=$CXX CC=$CC LDLIBS="-L$PREFIX/lib -lz -lzstd -ltbb -ltbbmalloc -lpthread" WITH_ZSTD=1
 
 mkdir -p $PREFIX/bin
 cp dmtools $PREFIX/bin
