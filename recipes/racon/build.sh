970a0087249741af67c12c86d6741f7e9d6359e1
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-05 15:48:45 +0300

    Set SPOA options specific for the CPU architecture
    See https://github.com/rvaser/spoa?tab=readme-ov-file#options
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/racon/build.sh
+++ recipes/racon/build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+case $(uname -m) in
+    x86_64)
+        SPOA_OPTS="-Dspoa_optimize_for_portability=ON"
+        ;;
+    aarch64)
+        SPOA_OPTS="-Dspoa_use_simde=ON -Dspoa_use_simde_nonvec=ON -Dspoa_use_simde_openmp=ON -Dspoa_generate_dispatch=ON"
+        ;;
+    *)
+        ;;
+esac
+
 mkdir -p $PREFIX/bin
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -Dracon_build_wrapper=ON -Dspoa_optimize_for_portability=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -Dracon_build_wrapper=ON ${SPOA_OPTS} ..
 make
 chmod +w bin/racon_wrapper
 make install
