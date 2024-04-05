1e72293470369f17b9d552808a4395195d54dc05
Acribbs <a.p.cribbs@gmail.com>
2024-04-05 10:36:31 +0100

    forgot we have a build section and removed require.txt

--- recipes/cgat-apps/build.sh
+++ recipes/cgat-apps/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+2#!/bin/bash
 
 # export compiler flags
 #export CFLAGS=${CFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
@@ -25,4 +25,4 @@ sed -i'' -e '/REPO_REQUIREMENT/,/pass/d' setup.py
 sed -i'' -e '/# dependencies/,/dependency_links=dependency_links,/d' setup.py
 
 # https://bioconda.github.io/linting.html#setup-py-install-args
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install --single-version-externally-managed
