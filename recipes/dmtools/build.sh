1e89b92067c35042cad73802095ea02e9a6e026f
ZhouQiangwei <1010170266@qq.com>
2024-04-07 23:36:08 +0800

    up

--- recipes/dmtools/build.sh
+++ recipes/dmtools/build.sh
@@ -3,6 +3,7 @@
 export C_INCLUDE_PATH=${PREFIX}/include;${BUILD_PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+echo make CXX=$CXX CPP=$CXX CC=$CC CFLAGS="-g -w -O3 -Wsign-compare -I$BUILD_PREFIX/include"
 make CXX=$CXX CPP=$CXX CC=$CC CFLAGS="-g -w -O3 -Wsign-compare -I$BUILD_PREFIX/include"
 ##export CFLAGS="-I$BUILD_PREFIX/include"
 ##export LDFLAGS="-L$BUILD_PREFIX/lib"
