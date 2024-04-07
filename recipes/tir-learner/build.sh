4a6d03c6fb0cc4b270a57ca543f3e6526a8358dc
Usman Rashid <usman@smme.edu.pk>
2024-04-08 09:17:35 +1200

    Updated source to lutianyu2001/TIR-Learner

--- recipes/tir-learner/build.sh
+++ recipes/tir-learner/build.sh
@@ -5,7 +5,7 @@ TIR_LEARNER_DIR=${PREFIX}/share/TIR-Learner3.0
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${TIR_LEARNER_DIR}
-cp -r bin/TIR-Learner3.0/* ${TIR_LEARNER_DIR}
+cp -r TIR-Learner3.0/* ${TIR_LEARNER_DIR}
 
 cat <<END >>${PREFIX}/bin/TIR-Learner
 #!/bin/bash
