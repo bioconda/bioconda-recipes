bff39eaac3f0d58c0f27cd0666a28dcb3c09b9fb
Tim15-tech <tmfy_mail-studium@yahoo.com>
2024-04-08 16:06:27 +0200

    Some testing command to get an overview over the folder structure of the virutal env 2

--- recipes/bamsurgeon/run_test.sh
+++ recipes/bamsurgeon/run_test.sh
@@ -4,11 +4,16 @@
 
 set -ex
 
+pwd
 cd $PREFIX
-cd .. # Prefix is test_tmp folder where only test_data/script is, so go one up
-folder=$(pwd)
-unknown_folder=$(find $folder -type d -name "addsv.tmp" -print)
+pwd
+ls
+unknown_folder=$(find -type d -name "addsv.tmp" -print)
 cd "${unknown_folder}"
+pwd
+ls
 cd .. # go up to be not within addsv.tmp but within bamsurgeon folder
+pwd
+ls
 python3 -O bin/addsv.py -p 1 -v test_data/test_sv.txt -f test_data/testregion_realign.bam -r test_data/Homo_sapiens_chr22_assembly19.fasta -o test_data/testregion_sv_mut.bam --aligner mem --keepsecondary --seed 1234 --inslib test_data/test_inslib.fa
 exit 0
