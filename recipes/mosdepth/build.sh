a58cc469c65b8cd5c5307048dde273832c3972e2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-08 13:35:12 +0300

    mosdepth: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if [[ ${target_platform}  == osx-64 ]] ; then
+if [[ ${target_platform}  == linux-64 ]] ; then
+    curl -SL https://github.com/brentp/mosdepth/releases/download/v$PKG_VERSION/mosdepth -o mosdepth
+    chmod +x mosdepth
+else
     curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
     tar -xzf mosdepth-latest.tar.gz
     cd mosdepth-${PKG_VERSION}
     nimble install -y "docopt@0.7.0"
     nimble build -y --verbose -d:release
-else
-    curl -SL https://github.com/brentp/mosdepth/releases/download/v$PKG_VERSION/mosdepth -o mosdepth
-    chmod +x mosdepth
 fi
 
 mkdir -p "${PREFIX}/bin"
