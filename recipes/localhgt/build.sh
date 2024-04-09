7dafdadad0e5c45be5d166f953a92e0ddc9cc845
wshuai294 <wshuai294@gmail.com>
2024-04-09 18:26:24 +0800

    build5

--- recipes/localhgt/build.sh
+++ recipes/localhgt/build.sh
@@ -4,11 +4,14 @@ set -eu -o pipefail
 mkdir -p $PREFIX/bin
 
 
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+#export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+#export LIBRARY_PATH="${PREFIX}/lib"
+#export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CC="${GXX} ${LDFLAGS}" CFLAGS="${CFLAGS}"
+#make CC="${GXX}" CFLAGS="${CFLAGS}"
+#make CC="${GXX} ${LDFLAGS}" CFLAGS="${CFLAGS}"
+
+make
 
 cp $SRC_DIR/scripts/*py $PREFIX/bin
 cp $SRC_DIR/scripts/*sh $PREFIX/bin
