6316ebb4c370c0ae71b6b7937859de92bc92b28a
wshuai294 <wshuai294@gmail.com>
2024-04-09 16:47:16 +0800

    build

--- recipes/localhgt/build.sh
+++ recipes/localhgt/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#export C_INCLUDE_PATH=${PREFIX}/include
-#export LIBRARY_PATH=${PREFIX}/lib
-
-make
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+make CC="${GXX} ${LDFLAGS}" CFLAGS="${CFLAGS}"
 # make
+
 mkdir -p $PREFIX/bin
 cp $SRC_DIR/scripts/*py $PREFIX/bin
 cp $SRC_DIR/scripts/*sh $PREFIX/bin
