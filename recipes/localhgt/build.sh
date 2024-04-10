1ed58f7a1ece63168ad76f74c1900f64c016bcbf
wshuai294 <wshuai294@gmail.com>
2024-04-10 20:41:50 +0800

    revision as suggested

--- recipes/localhgt/build.sh
+++ recipes/localhgt/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Ensure we run successfully using either conda-forge or defaults ncurses
-# (unlike other platforms, the latter does not automatically pull in libtinfo)
 make 
 
 mkdir -p $PREFIX/bin
