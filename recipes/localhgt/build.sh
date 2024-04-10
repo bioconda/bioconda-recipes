61b7d181af7d864171cad2acd7d8beda65ae1ed3
wshuai294 <wshuai294@gmail.com>
2024-04-10 12:32:30 +0800

    test existing tool

--- recipes/localhgt/build.sh
+++ recipes/localhgt/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -eu -o pipefail
 
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
@@ -7,14 +6,21 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 
+if [ "$(uname)" == "Darwin" ]; then
+  sed -i.bak 's/-Wl,-Bstatic//' Makefile.nongs
+  sed -i.bak 's/-Wl,-Bdynamic -lrt//' Makefile.nongs
+fi
 
-mkdir -p $PREFIX/bin
+LDFLAGS=-L${PREFIX}/lib
 
+make -f Makefile.nongs \
+    BOOST_PATH=${PREFIX} \
+    CC="$CXX $CXXFLAGS" \
+    LDFLAGS=$LDFLAGS
 
-
-make
-#make CC="${CXX}"
-
-cp $SRC_DIR/scripts/*py $PREFIX/bin
-cp $SRC_DIR/scripts/*sh $PREFIX/bin
-cp $SRC_DIR/scripts/extract_ref $PREFIX/bin
+mkdir -p ${PREFIX}/bin
+mv skesa ${PREFIX}/bin/
+mv saute ${PREFIX}/bin/
+mv saute_prot ${PREFIX}/bin/
+mv gfa_connector ${PREFIX}/bin/
+mv kmercounter ${PREFIX}/bin/
