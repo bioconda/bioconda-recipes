23e307c799f90d97f1df74c08c7437a24bb8d9b7
mencian <joshua.zhuang@yahoo.com>
2024-04-09 22:27:44 -0500

    edit build.sh

--- recipes/pairix/build.sh
+++ recipes/pairix/build.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+mkdir -p "${PREFIX}/bin"
+
 # Install both the pairix binaries and the Python extension module
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
-make
-cp bin/pairix $PREFIX/bin/pairix
-cp bin/pairs_merger $PREFIX/bin/pairs_merger
-cp bin/streamer_1d $PREFIX/bin/streamer_1d
-cp util/column_remover.pl $PREFIX/bin/column_remover.pl
-cp util/duplicate_header_remover.pl $PREFIX/bin/duplicate_header_remover.pl
-cp util/fragment_4dnpairs.pl $PREFIX/bin/fragment_4dnpairs.pl
-cp util/juicer_shortform2pairs.pl $PREFIX/bin/juicer_shortform2pairs.pl
-cp util/merge-pairs.sh $PREFIX/bin/merge-pairs.sh
-cp util/merged_nodup2pairs.pl $PREFIX/bin/merged_nodup2pairs.pl
-cp util/old_merged_nodup2pairs.pl $PREFIX/bin/old_merged_nodup2pairs.pl
-cp util/process_merged_nodup.sh $PREFIX/bin/process_merged_nodup.sh
-cp util/process_old_merged_nodup.sh $PREFIX/bin/process_old_merged_nodup.sh
-cp util/bam2pairs/bam2pairs $PREFIX/bin/bam2pairs
-$PYTHON -m pip install . -vvv --no-deps --no-build-isolation
+
+make CC="${CC}" CFLAGS="${CFLAGS} -O3" INCLUDES="-I${PREFIX}/include" LIBPATH="-L${PREFIX}/lib" -j4
+
+cp util/*.pl $PREFIX/bin/
+cp util/*.sh $PREFIX/bin/
+cp util/bam2pairs/bam2pairs $PREFIX/bin/
+
+# Fix perl shebang
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/column_remover.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/duplicate_header_remover.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/fragment_4dnpairs.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/juicer_shortform2pairs.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/merged_nodup2pairs.pl
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/old_merged_nodup2pairs.pl
+
+cp bin/pairix $PREFIX/bin/
+cp bin/pairs_merger $PREFIX/bin/
+cp bin/streamer_1d $PREFIX/bin/
+
+$PYTHON -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
