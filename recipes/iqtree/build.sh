4b2dd43cf863bcebf2e9ac54c6469ff1d1783cd0
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-11 14:26:14 +0300

    Revert some not needed changes
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-set -x
-
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS=$(echo "$CXXFLAGS" | sed 's/-std=c++17//g')
-export CMAKE_C_COMPILER="${CC}"
-export CMAKE_CXX_COMPILER="${CXX}"
 
 if [ "$(uname)" == Darwin ]; then
 	export CMAKE_C_COMPILER="clang"
