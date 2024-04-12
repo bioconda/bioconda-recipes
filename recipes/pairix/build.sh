d346c4fd20139ab1b6a764476aab3f8f4544e9b0
mencian <joshua.zhuang@yahoo.com>
2024-04-11 22:39:15 -0500

    edit build.sh

--- recipes/pairix/build.sh
+++ recipes/pairix/build.sh
@@ -6,7 +6,7 @@ mkdir -p "${PREFIX}/bin"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
-make
+make CC="${CC}"
 
 cp ${SRC_DIR}/util/*.pl ${PREFIX}/bin/
 cp ${SRC_DIR}/util/*.sh ${PREFIX}/bin/
@@ -20,8 +20,8 @@ sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/juicer_shortform2pairs.p
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/merged_nodup2pairs.pl
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/old_merged_nodup2pairs.pl
 
+cp ${SRC_DIR}/bin/pairix ${PREFIX}/bin/
 cp ${SRC_DIR}/bin/pairs_merger ${PREFIX}/bin/
 cp ${SRC_DIR}/bin/streamer_1d ${PREFIX}/bin/
-cp ${SRC_DIR}/bin/pairix ${PREFIX}/bin/
 
 ${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
