457755907d872e2392832383339bf25e25d1624a
mencian <joshua.zhuang@yahoo.com>
2024-04-11 20:35:21 -0500

    edit build.sh

--- recipes/pairix/build.sh
+++ recipes/pairix/build.sh
@@ -3,14 +3,14 @@
 mkdir -p "${PREFIX}/bin"
 
 # Install both the pairix binaries and the Python extension module
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
 
 make CC="${CC}" CFLAGS="${CFLAGS} -O3" INCLUDES="-I${PREFIX}/include" LIBPATH="-L${PREFIX}/lib"
 
-cp util/*.pl $PREFIX/bin/
-cp util/*.sh $PREFIX/bin/
-cp util/bam2pairs/bam2pairs $PREFIX/bin/
+cp ${SRC_DIR}/util/*.pl ${PREFIX}/bin/
+cp ${SRC_DIR}/util/*.sh ${PREFIX}/bin/
+cp ${SRC_DIR}/util/bam2pairs/bam2pairs ${PREFIX}/bin/
 
 # Fix perl shebang
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/column_remover.pl
@@ -20,9 +20,8 @@ sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/juicer_shortform2pairs.p
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/merged_nodup2pairs.pl
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/old_merged_nodup2pairs.pl
 
-$PYTHON -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
 
-BIN_PROGRAMS="pairix pairs_merger streamer_1d"
-for name in ${BIN_PROGRAMS}; do
-  cp ${SRC_DIR}/bin/${name} ${PREFIX}/bin/
-done
+cp ${SRC_DIR}/bin/pairs_merger ${PREFIX}/bin/
+cp ${SRC_DIR}/bin/streamer_1d ${PREFIX}/bin/
+cp ${SRC_DIR}/bin/pairix ${PREFIX}/bin/
