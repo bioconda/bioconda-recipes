fd28f5764946e9808f46f7fb6acc91f3ea5dc4d1
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-12 14:53:23 +0300

    lrzip: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/lrzip/build.sh
+++ recipes/lrzip/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="-O2 -march=native -fomit-frame-pointer -I$PREFIX/include $CFLAGS"
-export CXXFLAGS="-O2 -march=native -fomit-frame-pointer -I$PREFIX/include $CXXFLAGS"
+export CFLAGS="-O2 -fomit-frame-pointer -I$PREFIX/include $CFLAGS"
+export CXXFLAGS="-O2 -fomit-frame-pointer -I$PREFIX/include $CXXFLAGS"
 
 ./configure --prefix=$PREFIX 
 make
