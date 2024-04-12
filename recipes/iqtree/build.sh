473d66a923aad126f23a71ac821877aadc866d86
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-12 11:00:34 +0300

    Use parallel builds on non-aarch64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -17,6 +17,15 @@ cd build
 
 cmake -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" -DUSE_LSD2=ON -DIQTREE_FLAGS=omp ..
 
-make
+case $(uname -m) in
+	aarch64) 
+		JOBS=1 # CircleCI's arm.medium VM runs out of memory with higher values 
+		;;
+	*)
+		JOBS=${CPU_COUNT}
+		;;
+esac
+
+make --jobs=${JOBS}
 make install
 cp "${PREFIX}"/bin/iqtree2 "${PREFIX}"/bin/iqtree
