d7806d4be979e5df3fce25e2c48e814773c48591
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-12 15:44:11 +0300

    Update lrzip to 0.651 that supports Linux ARM64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/lrzip/build.sh
+++ recipes/lrzip/build.sh
@@ -4,6 +4,6 @@ export CFLAGS="-O2 -fomit-frame-pointer -I$PREFIX/include $CFLAGS"
 export CXXFLAGS="-O2 -fomit-frame-pointer -I$PREFIX/include $CXXFLAGS"
 
 ./configure --prefix=$PREFIX 
-make
+make --jobs=${CPU_COUNT}
 make install
 
