7044cbae87fdabd9f41222ced0d7adbfc66afa7d
mencian <joshua.zhuang@yahoo.com>
2024-04-11 21:19:24 -0500

    edit build.sh

--- recipes/pairix/build.sh
+++ recipes/pairix/build.sh
@@ -6,7 +6,7 @@ mkdir -p "${PREFIX}/bin"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
-make CC="${CC}" CFLAGS="${CFLAGS} -O3" INCLUDES="-I${PREFIX}/include" LIBPATH="-L${PREFIX}/lib"
+make
 
 cp ${SRC_DIR}/util/*.pl ${PREFIX}/bin/
 cp ${SRC_DIR}/util/*.sh ${PREFIX}/bin/
@@ -20,8 +20,8 @@ sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/juicer_shortform2pairs.p
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/merged_nodup2pairs.pl
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/old_merged_nodup2pairs.pl
 
-${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
-
 cp ${SRC_DIR}/bin/pairs_merger ${PREFIX}/bin/
 cp ${SRC_DIR}/bin/streamer_1d ${PREFIX}/bin/
 cp ${SRC_DIR}/bin/pairix ${PREFIX}/bin/
+
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
