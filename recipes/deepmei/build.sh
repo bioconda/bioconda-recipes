39295e9c3431e046a3dea44c574fe899e29a3e67
Kanglu123 <64641114+Kanglu123@users.noreply.github.com>
2024-04-15 22:57:45 +0800

    Update build.sh

--- recipes/deepmei/build.sh
+++ recipes/deepmei/build.sh
@@ -5,10 +5,13 @@ mkdir -p $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
 mkdir -p $PREFIX/bin/data_cluster/candidate_locus
 mkdir -p $PREFIX/bin/DeepMEI_model/batch_cdgc
 mkdir -p $PREFIX/bin/DeepMEI_model/reference
-chmod +x $PREFIX/bin/*
 #cd $PREFIX/bin/
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/candidate_locus.zip
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/batch_cdgc.zip
+curl -o $PREFIX/bin/candidate_locus.zip  https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/candidate_locus.zip
+unzip  $PREFIX/bin/candidate_locus.zip
+mv $PREFIX/bin/candidate_locus/* $PREFIX/bin/data_cluster/candidate_locus
+curl -o $PREFIX/bin/batch_cdgc.zip https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/batch_cdgc.zip
+unzip  $PREFIX/bin/batch_cdgc.zip
+mv $PREFIX/bin/batch_cdgc/* $PREFIX/bin/data_cluster/batch_cdgc
 curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/keras_metadata.pb  https://github.com/xuxif/DeepMEI/raw/main/DeepMEI/DeepMEI_model/weights/val_best_model/keras_metadata.pb
 curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/saved_model.pb https://github.com/xuxif/DeepMEI/raw/main/DeepMEI/DeepMEI_model/weights/val_best_model/saved_model.pb
 curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables/variables.data-00000-of-00001aa https://github.com/xuxif/DeepMEI/raw/main/DeepMEI/DeepMEI_model/weights/val_best_model/variables/variables.data-00000-of-00001aa
@@ -18,13 +21,17 @@ curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables/variables.dat
 curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables/variables.data-00000-of-00001ae  https://github.com/xuxif/DeepMEI/raw/main/DeepMEI/DeepMEI_model/weights/val_best_model/variables/variables.data-00000-of-00001ae
 curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables/variables.data-00000-of-00001af  https://github.com/xuxif/DeepMEI/raw/main/DeepMEI/DeepMEI_model/weights/val_best_model/variables/variables.data-00000-of-00001af
 curl -o $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables/variables.index curl https://github.com/xuxif/DeepMEI/raw/main/DeepMEI/DeepMEI_model/weights/val_best_model/variables/variables.index
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.00
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.01
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.02
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.03
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.04
-#curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.05
-echo "the path of variables.data-00000-of-00001aa is `realpath variables.data-00000-of-00001aa`"
+curl -o $PREFIX/bin/reference.00 https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.00
+curl -o $PREFIX/bin/reference.01 https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.01
+curl -o $PREFIX/bin/reference.02 https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.02
+curl -o $PREFIX/bin/reference.03 https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.03
+curl -o $PREFIX/bin/reference.04 https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.04
+curl -o $PREFIX/bin/reference.05 https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.05
+cat $PREFIX/bin/reference.0* > $PREFIX/bin/reference.tar.gz
+tar -zxvf $PREFIX/bin/reference.tar.gz
+mv  $PREFIX/bin/reference/* $PREFIX/bin/DeepMEI_model/reference
+rm -rf $PREFIX/bin/reference.0* $PREFIX/bin/reference.tar.gz 
+chmod +x $PREFIX/bin/*
 echo "check start"
 ls -al ./*
 echo "check end"
