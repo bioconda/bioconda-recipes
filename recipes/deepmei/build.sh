b6e41f7da92af4d0c04e57bd382e69637089da3c
Kanglu123 <64641114+Kanglu123@users.noreply.github.com>
2024-04-15 12:47:46 +0800

    Update build.sh

--- recipes/deepmei/build.sh
+++ recipes/deepmei/build.sh
@@ -18,7 +18,9 @@ curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/referen
 curl https://raw.githubusercontent.com/kanglu123/deepmei/deepmei-v1.6.24/reference.05
 echo "the present dir is $PWD"
 echo `realpath ./variables.data-00000-of-00001aa`
-echo `ls -al ./*`
+echo "check start"
+ls -al ./*
+echo "check end"
 ls $PREFIX/bin/
 cp -r * $PREFIX/bin/
 mkdir -p $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
