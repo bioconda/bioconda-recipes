77f727004b6d41ae5a7c91ec0ff971614ba23d85
Kanglu123 <64641114+Kanglu123@users.noreply.github.com>
2024-04-15 12:32:34 +0800

    Update build.sh

--- recipes/deepmei/build.sh
+++ recipes/deepmei/build.sh
@@ -25,21 +25,21 @@ mkdir -p $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
 mkdir -p $PREFIX/bin/data_cluster/candidate_locus
 mkdir -p $PREFIX/bin/DeepMEI_model/batch_cdgc
 mkdir -p $PREFIX/bin/DeepMEI_model/reference
-mv variables.data-00000-of-00001aa $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv variables.data-00000-of-00001ab $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv variables.data-00000-of-00001ac $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv variables.data-00000-of-00001ad $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv variables.data-00000-of-00001ae $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv variables.data-00000-of-00001af $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv variables.index $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
-mv keras_metadata.pb $PREFIX/bin/DeepMEI_model/weights/val_best_model/
-mv saved_model.pb $PREFIX/bin/DeepMEI_model/weights/val_best_model/
-unzip candidate_locus.zip
+mv ./variables.data-00000-of-00001aa $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./variables.data-00000-of-00001ab $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./variables.data-00000-of-00001ac $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./variables.data-00000-of-00001ad $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./variables.data-00000-of-00001ae $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./variables.data-00000-of-00001af $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./variables.index $PREFIX/bin/DeepMEI_model/weights/val_best_model/variables
+mv ./keras_metadata.pb $PREFIX/bin/DeepMEI_model/weights/val_best_model/
+mv ./saved_model.pb $PREFIX/bin/DeepMEI_model/weights/val_best_model/
+unzip ./candidate_locus.zip
 mv candidate_locus/* $PREFIX/bin/data_cluster/candidate_locus
-unzip batch_cdgc.zip
+unzip ./batch_cdgc.zip
 mv batch_cdgc/* $PREFIX/bin/data_cluster/batch_cdgc
-cat reference.0* > reference.tar.gz
-tar -zxvf reference.tar.gz
+cat ./reference.0* > ./reference.tar.gz
+tar -zxvf ./reference.tar.gz
 mv reference/* $PREFIX/bin/DeepMEI_model/reference
-rm -rf reference.tar.gz reference.0*
+rm -rf ./reference.tar.gz ./reference.0*
 chmod +x $PREFIX/bin/*
