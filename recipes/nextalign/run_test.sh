f269fb830c51ed53faefeec001219da931a5baee
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-23 11:13:41 +0300

    Export LD_LIBRARY_PATH and PATH pointing to the installed sysroot for Linux
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/nextalign/run_test.sh
+++ recipes/nextalign/run_test.sh
@@ -2,18 +2,16 @@
 
 set -euxo pipefail
 
-ldd --version
-which ldd
-ls -laR ${PREFIX}
-export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-
-if [ $(uname -m) != "aarch64" ]; then
-
-    nextalign run \
-        test-data/sequences.fasta \
-        --reference test-data/reference.fasta \
-        --genemap test-data/genemap.gff \
-        --output-all output/ \
-        --output-basename nextalign
-        
+if [ $(uname) == "Linux" ]; then
+    ARCH=$(uname -m)
+    export LD_LIBRARY_PATH="${PREFIX}/${ARCH}-conda-linux-gnu/sysroot/usr/lib64"
+    export PATH="${PREFIX}/${ARCH}-conda-linux-gnu/sysroot/usr/bin:${PATH}"
+    ldd --version
 fi
+
+nextalign run \
+    test-data/sequences.fasta \
+    --reference test-data/reference.fasta \
+    --genemap test-data/genemap.gff \
+    --output-all output/ \
+    --output-basename nextalign
