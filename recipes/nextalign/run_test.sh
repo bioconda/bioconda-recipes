9d486edc7efccc964c6db6853491537c9186ec4e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-23 11:00:24 +0300

    More debug. Disable the tests on Linux aarch64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/nextalign/run_test.sh
+++ recipes/nextalign/run_test.sh
@@ -2,12 +2,18 @@
 
 set -euxo pipefail
 
-ls -la ${PREFIX}/lib
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${PREFIX}/lib"
+ldd --version
+which ldd
+ls -laR ${PREFIX}
+export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 
-nextalign run \
-test-data/sequences.fasta \
---reference test-data/reference.fasta \
---genemap test-data/genemap.gff \
---output-all output/ \
---output-basename nextalign
+if [ $(uname -m) != "aarch64" ]; then
+
+    nextalign run \
+        test-data/sequences.fasta \
+        --reference test-data/reference.fasta \
+        --genemap test-data/genemap.gff \
+        --output-all output/ \
+        --output-basename nextalign
+        
+fi
