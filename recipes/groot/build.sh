1902f0c971c04fe06d7234f664d7392456021f10
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-23 16:17:31 +0300

    Do not run the tests on Linux ARM64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -xe
+
 mkdir -p $PREFIX/bin
 
 go get -d -v ./...
+
+# for some reason some test dependencies cannot be resolved at CircleCI
+if [ $(uname -m) != "aarch64" ]; then
 go test -v ./...
+fi
+
 go build -o groot
 
 mv groot $PREFIX/bin
