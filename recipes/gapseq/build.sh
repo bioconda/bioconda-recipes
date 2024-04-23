27a8ba0ec11bb98abb1bbc80f4250157f5af781a
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-04-23 19:13:39 +0200

    final test

--- recipes/gapseq/build.sh
+++ recipes/gapseq/build.sh
@@ -2,12 +2,10 @@
 
 # Installation instructions taken from https://github.com/jotech/gapseq/blob/master/docs/install.md#conda 2024-04-23 by cmkobel
 
-
 # Copy contents to conda prefix
 mkdir -p ${PREFIX}/gapseq
 cp ISSUE_TEMPLATE.MD LICENSE README.md gapseq gapseq_env.yml ${PREFIX}/gapseq
 cp -r dat/ docs/ src/ toy/ unit/ ${PREFIX}/gapseq
-
 
 # install one additional R-package
 # R -e 'install.packages("CHNOSZ", repos="http://cran.us.r-project.org")' # Is now available on bioconda, simply.
@@ -20,32 +18,23 @@ cp -r dat/ docs/ src/ toy/ unit/ ${PREFIX}/gapseq
 # --with-sbml-include=$CONDA_PREFIX/include \
 # --with-sbml-lib=$CONDA_PREFIX/lib" sybilSBML_3.1.2.tar.gz
 # rm sybilSBML_3.1.2.tar.gz
+# There is a workaround to do this which requires setting the correct lib path, but I think it is not strictl necessary to get the package running. Instead we can maybe think of ways to make these easy to install for the user after conda install.
 
-echo $(pwd) > ~/hat.txt
-echo $(ls) >> ~/hat.txt
-# Download reference sequence data
-#bash ./src/update_sequences.sh
-#
+# We must use the installed file as it uses its directory to set the path for the sequence installation.
+bash ${PREFIX}/gapseq/src/update_sequences.sh
 
-# The test is moved to the test section of meta.yaml
-# ./gapseq test
 
 # Final setup
 
 # Make binary available
 mkdir -p ${PREFIX}/bin
 ln -s ${PREFIX}/gapseq/gapseq ${PREFIX}/bin/gapseq
-# ln -s ${PREFIX}/gapseq/src/update_sequences.sh ${PREFIX}/bin/update_sequences.sh
-
-
-# We must use the correct file as it uses its directory to set the path for the sequence installation.
-bash ${PREFIX}/gapseq/src/update_sequences.sh
 
 
 # --- 
 
 
-# Build at home with (before submitting to azure)
+# Build at home with (before submitting to azure):
 # conda activate bioconda-utils
 # bioconda-utils lint --git-range master
 # bioconda-utils build --mulled-test --git-range master
