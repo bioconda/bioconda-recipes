2d8acea7a8f96840f866b63d555e96bc2c919b44
mencian <joshua.zhuang@yahoo.com>
2024-04-23 22:34:15 -0500

    update to v2.3.3

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -5,17 +5,18 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="$CXXFLAGS -std=c++14"
+export CXXFLAGS="$CXXFLAGS -O3"
 
 if [ "$(uname)" == Darwin ]; then
 	export CMAKE_C_COMPILER="clang"
 	export CMAKE_CXX_COMPILER="clang++"
 fi
 
-mkdir build
-cd build
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
+	-DUSE_LSD2=ON -DIQTREE_FLAGS=omp -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 
-cmake -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" -DUSE_LSD2=ON -DIQTREE_FLAGS=omp ..
+cmake --build build --target install -j "${CPU_COUNT}" -v
 
 case $(uname -m) in
 	aarch64) 
@@ -26,6 +27,4 @@ case $(uname -m) in
 		;;
 esac
 
-make --jobs=${JOBS}
-make install
-cp "${PREFIX}"/bin/iqtree2 "${PREFIX}"/bin/iqtree
+cp -f "${PREFIX}"/bin/iqtree2 "${PREFIX}"/bin/iqtree
