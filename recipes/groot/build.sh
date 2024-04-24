bba9316573ab03147e6e46d5d5cd801f89735f81
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-24 11:54:31 +0300

    Try with GO111MODULE=on for the tests
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -5,9 +5,7 @@ set -xe
 mkdir -p $PREFIX/bin
 
 go get -d -v ./...
-# tests are disabled due to a problem with a test dependency:
-# gopath/pkg/mod/github.com/dgryski/go-minhash@v0.0.0-20190315135803-ad340ca03076/minwise_test.go:6:2: no required module provides package github.com/dgryski/go-metro; to add it:[0m
-# go test -v ./...
+GO111MODULE=on go test -v ./...
 go build -o groot
 
 mv groot $PREFIX/bin
