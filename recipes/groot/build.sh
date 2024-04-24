e504df15b2c2552169edb6c8ded9dd744f1b9835
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-24 11:02:48 +0300

    Try with older version of Go - 1.16
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -5,12 +5,7 @@ set -xe
 mkdir -p $PREFIX/bin
 
 go get -d -v ./...
-
-# for some reason some test dependencies cannot be resolved at CircleCI
-if [ $(uname -m) != "aarch64" ]; then
 go test -v ./...
-fi
-
 go build -o groot
 
 mv groot $PREFIX/bin
