0315b6ded0f2a7f845de27cb9ac97c9e2c24c9cb
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-24 15:04:54 +0300

    gsalign: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gsalign/build.sh
+++ recipes/gsalign/build.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -xe
+
 mkdir -p ${PREFIX}/bin
 
 # index
 pushd src/BWT_Index
-make CC=$CC FLAGS="$CFLAGS" LIBS="$LDFLAGS -lm -lz"
+make CC=$CC FLAGS="$CFLAGS" LIBS="$LDFLAGS -lm -lz" -j ${CPU_COUNT}
 cp bwt_index $PREFIX/bin
 popd
 
+ARCH_FLAGS=""
+case $(uname -m) in
+    x86_64)
+        ARCH_FLAGS="-m64 -msse4.2 -mpopcnt"
+        ;;
+    *)
+        ;;
+esac
+
 # GSAlign
 pushd src
-make CXX=$CXX FLAGS="$CXXFLAGS -I$PREFIX/include -Wall -D NDEBUG -O3 -m64 -msse4.2 -mpopcnt -fPIC" LIB="$LDFLAGS -lz -lm -lpthread"
+make CXX=$CXX FLAGS="$CXXFLAGS -I$PREFIX/include -Wall -D NDEBUG -O3 ${ARCH_FLAGS} -fPIC" LIB="$LDFLAGS -lz -lm -lpthread" -j ${CPU_COUNT}
 cp GSAlign $PREFIX/bin
