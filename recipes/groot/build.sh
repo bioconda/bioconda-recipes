e147b882b29cbaddcd18046d8ecf5fb77041a832
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-04-24 11:44:31 +0300

    Disable the tests due to a problem with an old test dependnecy
    ```
    gopath/pkg/mod/github.com/dgryski/go-minhash@v0.0.0-20190315135803-ad340ca03076/minwise_test.go:6:2: no required module provides package github.com/dgryski/go-metro; to add it:␛[0m
    ```
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/groot/build.sh
+++ recipes/groot/build.sh
@@ -5,7 +5,9 @@ set -xe
 mkdir -p $PREFIX/bin
 
 go get -d -v ./...
-go test -v ./...
+# tests are disabled due to a problem with a test dependency:
+# gopath/pkg/mod/github.com/dgryski/go-minhash@v0.0.0-20190315135803-ad340ca03076/minwise_test.go:6:2: no required module provides package github.com/dgryski/go-metro; to add it:[0m
+# go test -v ./...
 go build -o groot
 
 mv groot $PREFIX/bin
