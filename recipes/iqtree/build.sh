497c247f9e6e4dbb8dee64f908f080e840e9d697
mencian <joshua.zhuang@yahoo.com>
2024-04-23 23:31:03 -0500

    edit build.sh

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -6,7 +6,7 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="$CXXFLAGS -O3 -std=c++14"
+export CXXFLAGS="$CXXFLAGS -std=c++14"
 
 if [ "$(uname)" == Darwin ]; then
 	export CMAKE_C_COMPILER="clang"
@@ -15,9 +15,7 @@ fi
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
 	-DUSE_LSD2=ON -DIQTREE_FLAGS=omp -DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS} ${LDFLAGS}" --no-warn-unused-cli
-
-cmake --build build --target install -j "${CPU_COUNT}" -v
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 
 case $(uname -m) in
 	aarch64) 
@@ -27,5 +25,7 @@ case $(uname -m) in
 		JOBS=${CPU_COUNT}
 		;;
 esac
+
+cmake --build build --target install -j "${JOBS}"
 
 cp -f "${PREFIX}"/bin/iqtree2 "${PREFIX}"/bin/iqtree
