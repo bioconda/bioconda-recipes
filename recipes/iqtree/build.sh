83a619a7230d4bf14060da79a4f8e39e90e58e06
mencian <joshua.zhuang@yahoo.com>
2024-04-24 22:22:52 -0500

    edit build.sh

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -6,7 +6,7 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="$CXXFLAGS -O3 -std=c++14"
+export CXXFLAGS="$CXXFLAGS -std=c++14"
 
 if [ "$(uname)" == Darwin ]; then
 	export CMAKE_C_COMPILER="clang"
