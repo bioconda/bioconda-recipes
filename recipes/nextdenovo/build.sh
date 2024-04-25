0709d28788d35a3b197cc81f7f21e74b6a0f1e6b
mencian <joshua.zhuang@yahoo.com>
2024-04-24 22:00:02 -0500

    edit build.sh

--- recipes/nextdenovo/build.sh
+++ recipes/nextdenovo/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -ex
 
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 OUTDIR="${SP_DIR}/nextdenovo"
 
 mkdir -p ${PREFIX}/bin ${OUTDIR}
 
-make CC="${CC}" CFLAGS="-O3" LDFLAGS="${LDFLAGS}" TOP_DIR="${PREFIX}" -j "${CPU_COUNT}"
-
-cp -rf "${SRC_DIR}/nextDenovo" ${PREFIX}/bin
-chmod a+x ${PREFIX}/bin/nextDenovo
+cp -rf ${SRC_DIR}/* ${OUTDIR}
+chmod 755 ${OUTDIR}/nextDenovo
+ln -sf ${OUTDIR}/nextDenovo ${PREFIX}/bin/nextDenovo
