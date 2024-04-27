6d694a130d51cd167edbed5f6cdcdf916a34c22d
Lee Katz - Aspen <gzu2@cdc.gov>
2024-04-27 18:19:19 -0400

    fix SN perl module

--- recipes/sneakernet-qc/build.sh
+++ recipes/sneakernet-qc/build.sh
@@ -5,16 +5,26 @@ which perl
 echo
 echo ======
 ls -RF
+echo "PERL5LIB: $PERL5LIB"
 echo ======
 echo
 perl Makefile.PL
 make
 make install
 
-mkdir -p ${PREFIX}/bin 
+mkdir -p  ${PREFIX}/bin 
+mkdir -pv ${PREFIX}/lib/perl5
 cp -v SneakerNet.plugins/*.pl ${PREFIX}/bin/
 cp -v SneakerNet.plugins/*.py ${PREFIX}/bin/
 cp -v SneakerNet.plugins/*.sh ${PREFIX}/bin/
 cp -v scripts/*.pl            ${PREFIX}/bin/
+cp -v lib/perl5/SneakerNet.pm ${PREFIX}/lib/perl5
 chmod 775 ${PREFIX}/bin/*
+export PERL5LIB=$PERL5LIB:${PREFIX}/lib/perl5
+
+echo
+echo ======
+echo "new PERL5LIB: $PERL5LIB"
+echo ======
+echo
 
