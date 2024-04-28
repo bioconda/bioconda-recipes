f918fb528218d77131c367408bc054f47f6c371f
AndreaGuarracino <andreaguarracino@outlook.com>
2024-04-28 15:54:53 -0500

    `make`

--- recipes/wgatools/build.sh
+++ recipes/wgatools/build.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-set -ex
-export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export CPATH=${PREFIX}/include
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
+#!/bin/bash -euo
 
 RUST_BACKTRACE=1 CARGO_HOME="${BUILD_PREFIX}/.cargo" cargo build --release
 
