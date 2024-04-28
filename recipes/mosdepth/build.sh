43737327068ed643f87fedecf11a9a773beaa05e
asp8200 <anders@mynucleus.com>
2024-04-28 09:49:40 +0000

    DEBUG2

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -6,6 +6,7 @@ ls
 if [[ ${target_platform}  == osx-64 ]] ; then
     echo "DEBUG2 - osx"
     ls
+    ls nim/
     curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
     tar -xzf mosdepth-latest.tar.gz
     cd mosdepth-${PKG_VERSION}
