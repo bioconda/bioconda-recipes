220c5ae5885d133ff2c25444cfda030c77b80161
mencian <joshua.zhuang@yahoo.com>
2024-04-29 02:28:04 -0500

    Update iqtree to v2.3.4

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -6,7 +6,7 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="$CXXFLAGS -std=c++14"
+export CXXFLAGS="${CXXFLAGS} -std=c++14"
 
 if [ "$(uname)" == Darwin ]; then
 	export CMAKE_C_COMPILER="clang"
@@ -17,15 +17,6 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Releas
 	-DUSE_LSD2=ON -DIQTREE_FLAGS=omp -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 
-case $(uname -m) in
-	aarch64) 
-		JOBS=1 # CircleCI's arm.medium VM runs out of memory with higher values 
-		;;
-	*)
-		JOBS=${CPU_COUNT}
-		;;
-esac
-
-cmake --build build --target install -j 1
+cmake --build build --target install
 
 cp -f "${PREFIX}"/bin/iqtree2 "${PREFIX}"/bin/iqtree
