56a7fd9cffc4d99dbe6f5ab4f72c180dbd36a2a0
Lee Katz - Aspen <gzu2@cdc.gov>
2024-04-29 21:42:22 -0400

    v5.3.1

---
+++ recipes/kalamari/post-link.sh
@@ -0,0 +1,15 @@
+echo "
+===================
+
+Kalamari installed.
+
+To download the files, run downloadKalamari.sh.
+This will load the files into:
+
+* $PREFIX/db/kalamari - the genomic files
+* $PREFIX/db/kraken1.kalamari - the Kraken1 database
+* $PREFIX/db/kraken2.kalamari - the Kraken2 database
+
+===================
+
+" > $PREFIX/.messages.txt
