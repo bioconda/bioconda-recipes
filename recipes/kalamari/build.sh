fc74b964fe41c736c131ef4bc2720c06714b27b9
Lee Katz - Aspen <gzu2@cdc.gov>
2024-04-30 10:49:17 -0400

    include shell script

--- recipes/kalamari/build.sh
+++ recipes/kalamari/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $PREFIX/bin
 
-EXES="downloadKalamari.pl  generate_sepia_reference.py  getExactTaxonomy.pl  mobsuiteRepresentativeFasta.pl  validateTaxonomy.pl"
+EXES="downloadKalamari.pl downloadKalamari.sh generate_sepia_reference.py  getExactTaxonomy.pl  mobsuiteRepresentativeFasta.pl  validateTaxonomy.pl"
 
 for i in $EXES; do
   chmod -v 755 bin/$i
