56d3daf8972709e3a01584e2c1c18046fb5595ea
Lee Katz - Aspen <gzu2@cdc.gov>
2024-04-30 19:32:33 -0400

    add version to downloadKalamari.sh via sed

--- recipes/kalamari/build.sh
+++ recipes/kalamari/build.sh
@@ -2,9 +2,14 @@
 
 mkdir -p $PREFIX/bin
 
+# Add in the correct version for the build
+sed -i "s/VERSION=.\+/VERSION=\"${PKG_VERSION}\"/" bin/downloadKalamari.sh
+
 EXES="downloadKalamari.pl downloadKalamari.sh generate_sepia_reference.py  getExactTaxonomy.pl  mobsuiteRepresentativeFasta.pl  validateTaxonomy.pl"
 
 for i in $EXES; do
   chmod -v 755 bin/$i
   cp -vf bin/$i $PREFIX/bin
 done
+
+
