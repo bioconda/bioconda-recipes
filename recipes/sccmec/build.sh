72d7323d0c3659eaf86df9cabb98bfaeec42ff13
Robert A. Petit III <robbie.petit@gmail.com>
2024-04-30 21:20:44 -0600

    Update build.sh

--- recipes/sccmec/build.sh
+++ recipes/sccmec/build.sh
@@ -4,7 +4,7 @@ mkdir -p $PREFIX/bin ${PREFIX}/share/sccmec
 
 # Copy wrapper
 chmod 755 bin/sccmec-bioconda
-cp -f bin/sccmec-bioconda $PREFIX/bin/sccmec-bioconda
+cp -f bin/sccmec-bioconda $PREFIX/bin/sccmec
 
 # Copy schema (~100kb)
 cp -f data/sccmec.yaml ${PREFIX}/share/sccmec
