b6e6cb2d82e9e0e49f9706384398bbd51701e7dd
PatschD <david.patsch@gmx.at>
2024-05-02 15:36:08 +0200

    add the ability to fix random seed - fix download

--- recipes/mapseq/build.sh
+++ recipes/mapseq/build.sh
@@ -4,7 +4,12 @@
 #tar -Cdata -xvzf mapref-2.2b.tar.gz && mv data/mapref-2.2b/* data/ && rmdir data/mapref-2.2b && touch data/mapref-2.2b.fna
 
 # touch data/mapref-2.2b.fna data/mapref-2.2b.fna.mscluster data/mapref-2.2b.fna.ncbitax data/mapref-2.2b.fna.otutax data/mapref-2.2b.gold.fna data/mapref-2.2b.gold.fna.mscluster data/mapref-2.2b.gold.fna.ncbitax data/mapref-2.2b.fna.otutax.97.ncbitax
-./setup.sh
+#
+wget http://www.microbeatlas.org/mapref/mapref-3.0.full.tar.gz
+# git-lfs pull
+mv mapref-3.0.full.tar.gz data/mapref-3.0.tar.gz
+tar -xvzf data/mapref-3.0.tar.gz && mv mapseq/* data/ && rmdir mapseq && touch data/mapref-3.0.fna
+
 ./bootstrap
 ./configure --prefix=$PREFIX CXXFLAGS="-O2 -I$CONDA_PREFIX/include"
 make
