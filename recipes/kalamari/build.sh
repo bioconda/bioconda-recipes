6bde2f373ee4e0b48a81bc862d8020b4be5cc711
Lee Katz <gzu2@cdc.gov>
2024-05-02 01:10:15 -0400

    Update Kalamari - more standard db location (#47593)
    * kalamari init
    
    * build.sh
    
    * removed assembly-scan artifact
    
    * removed comments
    
    * abbreviate license family
    
    * updated path to source scripts
    
    * m
    
    * bump version of kalamari
    
    * updated shasum
    
    * skip kraken depends in test, also try to retrigger CI
    
    * add run_exports
    
    * Update meta.yaml
    
    * perl-bio-seqfeature
    
    * address errors for bld.bat and having tests
    
    * v5.3.1
    
    * perl-bioperl
    
    * removed bioperl
    
    * added some build.sh instructions per @dr-joe-wirth's advice
    
    * removed other package from this PR
    
    * roll back build.sh
    
    * include shell script
    
    * kraken and kraken2 in the run: section
    
    * more standard db download location
    
    * add version to downloadKalamari.sh via sed
    
    * try noarch
    
    ---------
    
    Co-authored-by: Robert A. Petit III <robbie.petit@gmail.com>
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>
    Co-authored-by: mencian <joshua.zhuang@yahoo.com>

--- recipes/kalamari/build.sh
+++ recipes/kalamari/build.sh
@@ -2,16 +2,12 @@
 
 mkdir -p $PREFIX/bin
 
+# Add in the correct version for the build
+sed -i.bak "s/VERSION=.\+/VERSION=\"${PKG_VERSION}\"/" bin/downloadKalamari.sh
+
 EXES="downloadKalamari.pl downloadKalamari.sh generate_sepia_reference.py  getExactTaxonomy.pl  mobsuiteRepresentativeFasta.pl  validateTaxonomy.pl"
 
 for i in $EXES; do
   chmod -v 755 bin/$i
   cp -vf bin/$i $PREFIX/bin
 done
-
-# install and update locales (required for R to function properly)
-#apt-get -y install locales
-#LC_ALL=C.UTF-8
-#dpkg-reconfigure locales
-#echo 'export LC_ALL=C.UTF-8' >> ~/.bashrc
-
