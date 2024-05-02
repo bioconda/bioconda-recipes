8fe5583a1bef49abb1c3b253920466786201d9da
Lee Katz <gzu2@cdc.gov>
2024-05-02 01:10:15 -0400

    Update Kalamari - more standard db location (#47593)
    * kalamari init
    
    * build.sh
    
    * removed assembly-scan artifact
    
    * removed comments
    
    * abbreviate license family
    
    * updated path to source scripts
    
    * m
    
    * bump version of kalamari
    
    * updated shasum
    
    * skip kraken depends in test, also try to retrigger CI
    
    * add run_exports
    
    * Update meta.yaml
    
    * perl-bio-seqfeature
    
    * address errors for bld.bat and having tests
    
    * v5.3.1
    
    * perl-bioperl
    
    * removed bioperl
    
    * added some build.sh instructions per @dr-joe-wirth's advice
    
    * removed other package from this PR
    
    * roll back build.sh
    
    * include shell script
    
    * kraken and kraken2 in the run: section
    
    * more standard db download location
    
    * add version to downloadKalamari.sh via sed
    
    * try noarch
    
    ---------
    
    Co-authored-by: Robert A. Petit III <robbie.petit@gmail.com>
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>
    Co-authored-by: mencian <joshua.zhuang@yahoo.com>

--- recipes/kalamari/post-link.sh
+++ recipes/kalamari/post-link.sh
@@ -1,15 +1,17 @@
+DB_PREFIX="$PREFIX/share/${PKG_NAME}-${PKG_VERSION}"
 echo "
 ===================
-
 Kalamari installed.
 
-To download the files, run downloadKalamari.sh.
+To download the files, run downloadKalamari.sh
 This will load the files into:
 
-* $PREFIX/db/kalamari - the genomic files
-* $PREFIX/db/kraken1.kalamari - the Kraken1 database
-* $PREFIX/db/kraken2.kalamari - the Kraken2 database
+* $DB_PREFIX/kalamari - the genomic files
+* $DB_PREFIX/kraken1.kalamari - the Kraken1 database
+* $DB_PREFIX/kraken2.kalamari - the Kraken2 database
 
+For more refined options, please start with
+  downloadKalamari.pl --help
 ===================
 
 " > $PREFIX/.messages.txt
