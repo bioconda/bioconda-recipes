41c7e8a98df54b213a4bbfe42b2f5cf5fd94cf94
mencian <joshua.zhuang@yahoo.com>
2024-05-02 22:08:15 -0500

    edit build.sh

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -18,12 +18,12 @@ if [[ ${target_platform}  == "osx-64" ]]; then
     nimble build -y --verbose -d:release
 elif [[ "${ARCH}" == "aarch64" ]]; then
     curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-1-6/linux_arm64.tar.xz -o linux_arm64.tar.xz
-    tar -xzf linux_arm64.tar.xz
+    tar -xf linux_arm64.tar.xz
     cd nim-1.6.*
     export PATH="$PWD/bin:$PATH"
     cd ..
     curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
-    tar -xzf mosdepth-latest.tar.gz
+    tar -xf mosdepth-latest.tar.gz
     cd mosdepth-${PKG_VERSION}
     nimble install -y "docopt@0.7.0"
     nimble build -y --verbose -d:release
