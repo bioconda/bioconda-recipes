820e03aa420df3636226daef4eaab17bf8361d9b
mencian <joshua.zhuang@yahoo.com>
2024-05-02 22:31:18 -0500

    edit build.sh

--- recipes/bayestyper/build.sh
+++ recipes/bayestyper/build.sh
@@ -3,14 +3,13 @@
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include ${LDFLAGS}"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 mkdir -p ${PREFIX}/bin
 mkdir build && cd build
 
 cmake -S .. -B ./ \
-	-DBUILD_SCRIPTS=1 \
-	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_STATIC=1 \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 make -j ${CPU_COUNT}
