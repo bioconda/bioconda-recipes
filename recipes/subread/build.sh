bb919db280266a07dfd0edd724f31d03b6b54336
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-05-03 15:29:26 +0300

    Drop `-mtune=core` from CXXFLAGS for Linux ARM64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/subread/build.sh
+++ recipes/subread/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/annotation"
 
@@ -9,6 +11,12 @@ if [ `uname` = "Darwin" ];
 then
   MAKEFILE=Makefile.MacOS
 fi
+
+if [ $(uname -m) == "aarch64" ]; then
+  sed -i.bak 's/-mtune=core2//' ${MAKEFILE}
+  sed -i.bak 's/-mtune=core2//' ./longread-one/Makefile
+fi
+
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 make -f $MAKEFILE CC_EXEC="$CC -L$PREFIX/lib -fcommon" -j ${CPU_COUNT}
