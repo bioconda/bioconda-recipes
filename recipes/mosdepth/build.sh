de5cab4097bd00be368086d3ef207f5408819c94
mencian <joshua.zhuang@yahoo.com>
2024-05-02 22:00:00 -0500

    add aarch64 build instructions

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -3,9 +3,22 @@ set -x -e
 
 mkdir -p "${PREFIX}/bin"
 
-if [[ ${target_platform}  == osx-64 ]] ; then
+ARCH=$(uname -m)
+
+if [[ ${target_platform}  == "osx-64" ]]; then
     curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-1-6/macosx_x64.tar.xz -o macosx_x64.tar.xz
     tar -xzf macosx_x64.tar.xz
+    cd nim-1.6.*
+    export PATH="$PWD/bin:$PATH"
+    cd ..
+    curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
+    tar -xzf mosdepth-latest.tar.gz
+    cd mosdepth-${PKG_VERSION}
+    nimble install -y "docopt@0.7.0"
+    nimble build -y --verbose -d:release
+elif [[ "${ARCH}" == "aarch64" ]]; then
+    curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-1-6/linux_arm64.tar.xz -o linux_arm64.tar.xz
+    tar -xzf linux_arm64.tar.xz
     cd nim-1.6.*
     export PATH="$PWD/bin:$PATH"
     cd ..
