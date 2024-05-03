43d3be3415e05614d0ec7928c5bbfe8d57431f1b
mencian <joshua.zhuang@yahoo.com>
2024-05-02 20:37:22 -0500

    remove from blacklist

--- recipes/bayestyper/build.sh
+++ recipes/bayestyper/build.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-mkdir build && cd build
-cmake ..
-make
+#!/bin/bash -euo
+
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
 mkdir -p ${PREFIX}/bin
+mkdir build && cd build
 
-cp ../bin/bayesTyper ${PREFIX}/bin
-cp ../bin/bayesTyperTools ${PREFIX}/bin
+cmake -S .. -B ./ \
+	-DBUILD_SCRIPTS=1 \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
+make -j ${CPU_COUNT}
+
+cp -f ${SRC_DIR}/bin/bayesTyper ${PREFIX}/bin
+cp -f ${SRC_DIR}/bin/bayesTyperTools ${PREFIX}/bin
+cp -f ${SRC_DIR}/src/bayesTyperTools/scripts/* ${PREFIX}/bin
