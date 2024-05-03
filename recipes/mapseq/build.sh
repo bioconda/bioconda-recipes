4a38f1aa26b7562cc2d6494e2be44c6573858257
PatschD <david.patsch@gmx.at>
2024-05-03 08:42:17 +0200

    fix download

--- recipes/mapseq/build.sh
+++ recipes/mapseq/build.sh
@@ -3,13 +3,7 @@
 #wget http://www.microbeatlas.org/mapref/mapref-2.2b.tar.gz
 #tar -Cdata -xvzf mapref-2.2b.tar.gz && mv data/mapref-2.2b/* data/ && rmdir data/mapref-2.2b && touch data/mapref-2.2b.fna
 
-# touch data/mapref-2.2b.fna data/mapref-2.2b.fna.mscluster data/mapref-2.2b.fna.ncbitax data/mapref-2.2b.fna.otutax data/mapref-2.2b.gold.fna data/mapref-2.2b.gold.fna.mscluster data/mapref-2.2b.gold.fna.ncbitax data/mapref-2.2b.fna.otutax.97.ncbitax
-#
-wget http://www.microbeatlas.org/mapref/mapref-3.0.full.tar.gz
-# git-lfs pull
-mv mapref-3.0.full.tar.gz data/mapref-3.0.tar.gz
-tar -xvzf data/mapref-3.0.tar.gz && mv mapseq/* data/ && rmdir mapseq && touch data/mapref-3.0.fna
-
+touch data/mapref-3.0.fna data/mapref-3.0.fna.mscluster data/mapref-3.0.fna.ncbitax data/mapref-3.0.fna.otutax data/mapref-3.0.gold.fna data/mapref-3.0.gold.fna.mscluster data/mapref-3.0.gold.fna.ncbitax data/mapref-3.0.fna.otutax.97.ncbitax
 ./bootstrap
 ./configure --prefix=$PREFIX CXXFLAGS="-O2 -I$CONDA_PREFIX/include"
 make
