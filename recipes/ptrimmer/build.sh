9c38d7d4eb5ed3e268b1892675d86f6ee96a3cda
mencian <joshua.zhuang@yahoo.com>
2024-05-05 22:40:13 -0500

    add run_exports

--- recipes/ptrimmer/build.sh
+++ recipes/ptrimmer/build.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-make CC=$CC LIBDIR=-L$PREFIX/lib INCLUDE=-I$PREFIX/include
+make CC="$CC" LIBDIR="-L$PREFIX/lib" INCLUDE="-I$PREFIX/include" -j"${CPU_COUNT}"
 mkdir -p $PREFIX/bin
 mv pTrimmer-* $PREFIX/bin/ptrimmer
