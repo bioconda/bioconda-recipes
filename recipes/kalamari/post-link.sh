ab1691c48d3624e1132f1fa26aefa2a16dcff33c
Lee Katz <gzu2@cdc.gov>
2024-05-05 23:42:15 -0400

    Update Kalamari (#47688)
    * kalamari init
    
    * build.sh
    
    * removed assembly-scan artifact
    
    * removed comments
    
    * abbreviate license family
    
    * updated path to source scripts
    
    * m
    
    * bump version of kalamari
    
    * updated shasum
    
    * skip kraken depends in test, also try to retrigger CI
    
    * add run_exports
    
    * Update meta.yaml
    
    * perl-bio-seqfeature
    
    * address errors for bld.bat and having tests
    
    * v5.3.1
    
    * perl-bioperl
    
    * removed bioperl
    
    * added some build.sh instructions per @dr-joe-wirth's advice
    
    * removed other package from this PR
    
    * roll back build.sh
    
    * include shell script
    
    * kraken and kraken2 in the run: section
    
    * more standard db download location
    
    * add version to downloadKalamari.sh via sed
    
    * try noarch
    
    * fix src dir; fix post message
    
    * added -r to cp
    
    ---------
    
    Co-authored-by: Robert A. Petit III <robbie.petit@gmail.com>
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>
    Co-authored-by: mencian <joshua.zhuang@yahoo.com>

--- recipes/kalamari/post-link.sh
+++ recipes/kalamari/post-link.sh
@@ -7,6 +7,9 @@ To download the files, run downloadKalamari.sh
 This will load the files into:
 
 * $DB_PREFIX/kalamari - the genomic files
+
+Suggested directories if building databases:
+
 * $DB_PREFIX/kraken1.kalamari - the Kraken1 database
 * $DB_PREFIX/kraken2.kalamari - the Kraken2 database
 
