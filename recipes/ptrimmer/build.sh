d49725efb436bd8b167216918477d932c831abb3
mencian <joshua.zhuang@yahoo.com>
2024-05-06 20:00:35 -0500

    edit build.sh

--- recipes/ptrimmer/build.sh
+++ recipes/ptrimmer/build.sh
@@ -6,7 +6,7 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS} -O3" LIBDIR="${PREFIX}/lib" \
+make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS} -O3" LIBDIR="-L${PREFIX}/lib" \
 	INCLUDE="-I${PREFIX}/include" -j"${CPU_COUNT}"
 
 chmod 755 pTrimmer-*
