d49e981740e2e20eee3e98d20fdcb33ca8ca9184
mencian <joshua.zhuang@yahoo.com>
2024-05-06 21:17:15 -0500

    edit build.sh

--- recipes/kmer-jellyfish/build.sh
+++ recipes/kmer-jellyfish/build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -euo
 
-./configure --prefix=${PREFIX} --enable-python-binding=sys --enable-all-static --with-sse \
-	CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+./configure --prefix=${PREFIX} --enable-python-binding --with-sse CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 make -j ${CPU_COUNT}
