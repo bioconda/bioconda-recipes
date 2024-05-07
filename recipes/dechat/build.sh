dd08f89c07c4d07a7b2fc1bee34385d54d4e6a42
yichenli <923781272@qq.com>
2024-05-08 00:33:38 +0800

    add dechat recipes

--- recipes/dechat/build.sh
+++ recipes/dechat/build.sh
@@ -2,6 +2,6 @@
 
 cd $SRC_DIR
 mkdir build &&cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DKSIZE_LIST="32 64 96 128 160 192" ..
-make -j 24
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX  ..
+make
 cp $SRC_DIR/bin/dechat $PREFIX
