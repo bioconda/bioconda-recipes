36b11b719afebb8a27aa8362d13f9e5bd706e46f
Florian Plaza Oñate <florian.plaza-onate@inra.fr>
2024-05-08 18:52:27 +0200

    Disable python bindings on OSX

--- recipes/kmer-jellyfish/build.sh
+++ recipes/kmer-jellyfish/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -euo
 
-./configure --prefix=${PREFIX} --enable-python-binding --with-sse CXX="${CXX}" \
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Python bindings do no work on OSX
+    ./configure --prefix=${PREFIX} --with-sse CXX="${CXX}" \
 	CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+else
+    ./configure --prefix=${PREFIX} --enable-python-binding --with-sse CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+fi
 
 make -j ${CPU_COUNT}
 make check -j ${CPU_COUNT}
