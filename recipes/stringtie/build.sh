f2ceb245d53479248099aa5b6f3f60e56c27e3a9
mencian <joshua.zhuang@yahoo.com>
2024-05-07 22:12:55 -0500

    edit meta.yaml

--- recipes/stringtie/build.sh
+++ recipes/stringtie/build.sh
@@ -1,22 +1,21 @@
-#!/bin/bash -euo
+#!/bin/bash
 
-mkdir -p ${PREFIX}/bin
+ln -sf $PREFIX/lib/libz.so.1 $PREFIX/lib/libz.so
 
-ln -sf ${PREFIX}/lib/libz.so.1 ${PREFIX}/lib/libz.so
+export C_INCLUDE_PATH=$PREFIX/include
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+export LINKER="$CXX"
+export CXXFLAGS="$CPPFLAGS"
 
-export C_INCLUDE_PATH="${PREFIX}/include"
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
-export LINKER="${CXX}"
-export CXXFLAGS="${CPPFLAGS}"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
-make release CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}"
-chmod 755 stringtie && mv stringtie ${PREFIX}/bin
+make release CXX=$CXX
+mkdir -p $PREFIX/bin
+chmod 755 stringtie
+mv stringtie $PREFIX/bin
 
 # Prepare prepDE
 # This is equivalent to https://github.com/gpertea/stringtie/blob/master/prepDE.py
 curl -LO https://ccb.jhu.edu/software/stringtie/dl/prepDE.py
 2to3 -w --no-diffs prepDE.py
 sed -i.bak 's|/usr/bin/env python2|/usr/bin/env python|' prepDE.py
-mv prepDE.py ${PREFIX}/bin
-chmod +x ${PREFIX}/bin/prepDE.py
+mv prepDE.py $PREFIX/bin
+chmod +x $PREFIX/bin/prepDE.py
