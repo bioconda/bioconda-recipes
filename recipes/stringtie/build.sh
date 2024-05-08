fb82e64d3872d47beeb3f235f90d7fa22c4e66ca
mencian <joshua.zhuang@yahoo.com>
2024-05-07 20:55:24 -0500

    test osx build

--- recipes/stringtie/build.sh
+++ recipes/stringtie/build.sh
@@ -1,20 +1,22 @@
-#!/bin/sh
+#!/bin/bash -euo
 
-ln -s $PREFIX/lib/libz.so.1 $PREFIX/lib/libz.so
+mkdir -p ${PREFIX}/bin
 
-export C_INCLUDE_PATH=$PREFIX/include
-export CPLUS_INCLUDE_PATH=$PREFIX/include
-export LINKER="$CXX"
-export CXXFLAGS="$CPPFLAGS"
+ln -sf ${PREFIX}/lib/libz.so.1 ${PREFIX}/lib/libz.so
 
-make release CXX=$CXX
-mkdir -p $PREFIX/bin
-mv stringtie $PREFIX/bin
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LINKER="${CXX}"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+make release CXX="${CXX} ${LDFLAGS}" CXXFLAGS="${CXXFLAGS}" INCDIRS="${INCDIRS} -I${PREFIX}/include" -j${CPU_COUNT}
+chmod 755 stringtie && mv stringtie ${PREFIX}/bin
 
 # Prepare prepDE
 # This is equivalent to https://github.com/gpertea/stringtie/blob/master/prepDE.py
 curl -LO https://ccb.jhu.edu/software/stringtie/dl/prepDE.py
 2to3 -w --no-diffs prepDE.py
 sed -i.bak 's|/usr/bin/env python2|/usr/bin/env python|' prepDE.py
-mv prepDE.py $PREFIX/bin
-chmod +x $PREFIX/bin/prepDE.py
+mv prepDE.py ${PREFIX}/bin
+chmod +x ${PREFIX}/bin/prepDE.py
