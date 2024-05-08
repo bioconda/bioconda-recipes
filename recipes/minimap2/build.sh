7d694dfc97ff9823f98afced53f6827c9dc16b28
mencian <joshua.zhuang@yahoo.com>
2024-05-07 21:59:02 -0500

    Update minimap2 recipe

--- recipes/minimap2/build.sh
+++ recipes/minimap2/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include
+mkdir -p ${PREFIX}/bin ${PREFIX}/lib ${PREFIX}/include
 
-export CPATH=${PREFIX}/include
+export INCLUDE_PATH="${PREFIX}/include"
 
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L$PREFIX/lib" minimap2 sdust
-cp minimap2 misc/paftools.js $PREFIX/bin
-cp sdust $PREFIX/bin
-cp libminimap2.a $PREFIX/lib
-cp *.h $PREFIX/include
+ARCH=$(uname -m)
+case ${ARCH} in
+    aarch64) ARCH_FLAGS="arm_neon=1 aarch64=1" ;;
+    *) ARCH_FLAGS="" ;;
+esac
+
+make INCLUDES="-I${PREFIX}/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L${PREFIX}/lib" \
+	-j"${CPU_COUNT}" "${ARCH_FLAGS}" minimap2 sdust
+
+chmod 755 minimap2 && chmod 755 sdust
+
+cp -f minimap2 misc/paftools.js ${PREFIX}/bin
+cp -f sdust ${PREFIX}/bin
+cp -f libminimap2.a ${PREFIX}/lib
+cp -f *.h ${PREFIX}/include
