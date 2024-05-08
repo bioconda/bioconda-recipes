9f9edf8cdf32c62ead8e099436ca4404725214cd
mencian <joshua.zhuang@yahoo.com>
2024-05-07 22:38:50 -0500

    edit meta.yaml

--- recipes/minimap2/build.sh
+++ recipes/minimap2/build.sh
@@ -6,12 +6,12 @@ export INCLUDE_PATH="${PREFIX}/include"
 
 ARCH=$(uname -m)
 case ${ARCH} in
-    aarch64) ARCH_FLAGS="aarch64=1" ;;
-    *) ARCH_FLAGS="" ;;
+    aarch64) ARCH_FLAGS="aarch64=1 arm_neon=" ;;
+    *) ARCH_FLAGS="aarch64=" ;;
 esac
 
 make INCLUDES="-I${PREFIX}/include" CFLAGS="-g -Wall -O2 -Wc++-compat -L${PREFIX}/lib" \
-	-j"${CPU_COUNT}" "${ARCH_FLAGS}" minimap2 sdust
+	"${ARCH_FLAGS}" -j"${CPU_COUNT}" minimap2 sdust
 
 chmod 755 minimap2 && chmod 755 sdust
 
