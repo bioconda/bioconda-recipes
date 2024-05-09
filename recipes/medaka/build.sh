4b800c1a048dc458b2342af9c8943c788b6ee8a5
mencian <joshua.zhuang@yahoo.com>
2024-05-08 21:40:30 -0500

    clean up recipe

--- recipes/medaka/build.sh
+++ recipes/medaka/build.sh
@@ -4,7 +4,7 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-export CFLAGS="-I${PREFIX}/include ${LDFLAGS}"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include ${LDFLAGS}"
 
 # disable Makefile driven build of htslib.a
 sed -i.bak "s/'build_ext': HTSBuild//" setup.py
@@ -13,4 +13,4 @@ sed -i.bak "s/'build_ext': HTSBuild//" setup.py
 sed -i.bak 's/extra_objects.*//' build.py
 sed -i.bak 's/^libraries=\[/libraries=\["hts",/' build.py
 
-$PYTHON -m pip install . --no-deps --ignore-installed -vvv
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
