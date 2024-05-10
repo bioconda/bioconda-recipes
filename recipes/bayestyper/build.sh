1def8d684c08f598164ceb1d0b7cece81de0ce2f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-05-10 14:41:14 +0300

    Disable CMake finding Boost libraries
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/bayestyper/build.sh
+++ recipes/bayestyper/build.sh
@@ -12,17 +12,19 @@ export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 sed -i.bak "4d;5d;6d;7d;8d;9d" CMakeLists.txt
 
 # include boost from the library path, not from local in all *.cpp/*.hpp files
-find ./ -name "*.?pp" | xargs sed -i.bak -E 's/#include "boost(.*)"/#include <boost\1>/'
+#find ./ -name "*.?pp" | xargs sed -i.bak -E 's/#include "boost(.*)"/#include <boost\1>/'
+
+# Disable CMake finding Boost
+sed -i.bak -E 's/(FIND_PACKAGE\(Boost.*)/#\1/' CMakeLists.txt
 
 mkdir -p ${PREFIX}/bin
-mkdir build && cd build
 
-cmake -S .. -B . \
+cmake -S . -B build \
 	-Wno-dev \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	-DGIT_LAST_COMMIT_HASH=123456
-make -j ${CPU_COUNT} VERBOSE=1
+cmake --build build -j ${CPU_COUNT} --verbose
 
 cp -f ${SRC_DIR}/bin/bayesTyper ${PREFIX}/bin
 cp -f ${SRC_DIR}/bin/bayesTyperTools ${PREFIX}/bin
