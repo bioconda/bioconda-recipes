6cb106e433dba7fd35cb411a769f69e5b3a61853
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-05-09 20:00:30 -0500

    Skip cron script

--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -3,6 +3,6 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCIFPP_DOWNLOAD_CCD=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
-cmake --build . --config Release
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCIFPP_DOWNLOAD_CCD=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCIFPP_INSTALL_UPDATE_SCRIPT=OFF ..
+cmake --build . --config Release -DCIFPP_INSTALL_UPDATE_SCRIPT=OFF
 cmake --install . --prefix ${PREFIX}
