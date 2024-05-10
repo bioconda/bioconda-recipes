7424bbc73a3c7dfed9413d135d9cfe2d9b64325a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-05-09 20:21:18 -0500

    Fix typo

--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCIFPP_DOWNLOAD_CCD=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCIFPP_INSTALL_UPDATE_SCRIPT=OFF ..
-cmake --build . --config Release -DCIFPP_INSTALL_UPDATE_SCRIPT=OFF
+cmake --build . --config Release
 cmake --install . --prefix ${PREFIX}
