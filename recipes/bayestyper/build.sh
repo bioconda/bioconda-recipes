57671fcf362341f3e6df8905024ca994a994b540
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-05-10 14:27:16 +0300

    No need of CMAKE_CXX_FLAGS env var
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/bayestyper/build.sh
+++ recipes/bayestyper/build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -euox
+#!/bin/bash -euo
+
+set -x
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
-export CMAKE_CXX_FLAGS="${CXXFLAGS}"
 
 # Delete call to `git rev-parse HEAD`
 # https://github.com/bioinformatics-centre/BayesTyper/blob/1b04081ca4c64975270d267dbe9d80bd00bddaf9/CMakeLists.txt#L4-L9
@@ -12,8 +13,6 @@ sed -i.bak "4d;5d;6d;7d;8d;9d" CMakeLists.txt
 
 # include boost from the library path, not from local in all *.cpp/*.hpp files
 find ./ -name "*.?pp" | xargs sed -i.bak -E 's/#include "boost(.*)"/#include <boost\1>/'
-
-ls -laR ${PREFIX}/include/boost
 
 mkdir -p ${PREFIX}/bin
 mkdir build && cd build
