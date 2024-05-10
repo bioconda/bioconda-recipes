86bb604a4be993a2b64af7cc337c048dd71f1d8f
wjunLu <wjunlu217@gmail.com>
2024-05-10 09:18:31 +0800

    Merge all commits

--- recipes/necat/build.sh
+++ recipes/necat/build.sh
@@ -5,12 +5,16 @@ export CFLAGS="$CFLAGS  -I$PREFIX/include"
 export CXXFLAGS="$CXXFLAGS  -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
+BIN_ROOT="Linux-amd64/bin"
+case $(uname -m) in
+    arm64|aarch64) BIN_ROOT="Linux-aarch64/bin" ;;
+esac
 # build
 make -C src CFLAGS+="${CFLAGS}" LDFLAGS+="${LDFLAGS}" CXXFLAGS+="${CXXFLAGS}"
 
 # create and populate binary file
 mkdir -p $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/
-cp -r Linux-amd64/bin/*  $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/
+cp -r $BIN_ROOT/*  $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/bin/
 
 # create calling script
 mkdir $PREFIX/bin/
