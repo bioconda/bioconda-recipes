cd46d640919f0b2eeed26beafc6bda813398d69a
Lee Katz - Aspen <gzu2@cdc.gov>
2024-05-10 10:28:23 -0400

    new download instructions; added src tsv and dmp files to build

--- recipes/kalamari/post-link.sh
+++ recipes/kalamari/post-link.sh
@@ -7,8 +7,14 @@ To download the files, run downloadKalamari.sh
 This will load the files into:
 
 * $DB_PREFIX/kalamari - the genomic files
-* $DB_PREFIX/kraken1.kalamari - the Kraken1 database
-* $DB_PREFIX/kraken2.kalamari - the Kraken2 database
+
+The following databases are available:
+* buildTaxonomy.sh  - builds a modified taxonomy directory at $DB_PREFIX/taxonomy
+* filterTaxonomy.sh - filters the taxonomy directory into just taxa that are used in Kalamari into $DB_PREFIX/taxonomy/filtered
+
+The following depend on a built taxonomy from buildTaxonomy.sh
+* buildKraken1.sh - creates a Kraken1 database at $DB_PREFIX/kraken1.kalamari
+* buildKraken2.sh - creates a Kraken1 database at $DB_PREFIX/kraken2.kalamari
 
 For more refined options, please start with
   downloadKalamari.pl --help
