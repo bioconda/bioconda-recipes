49d169b445a3cfa343e01e0b8e41d8361f7a36bb
wjunLu <wjunlu217@gmail.com>
2024-05-11 17:40:09 +0800

    Fix

--- recipes/meryl/build.sh
+++ recipes/meryl/build.sh
@@ -3,5 +3,5 @@
 make -C src BUILD_DIR="$(pwd)" \
 	TARGET_DIR="${PREFIX}" \
 	CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS} -fopenmp -L${PREFIX}/lib -lstdc++" -j"${CPU_COUNT}"
+	CXXFLAGS="${CXXFLAGS} -lstdc++ -O3 -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -fopenmp -L${PREFIX}/lib" -j"${CPU_COUNT}"
