cd28757974f9cbdc0490e610baf7b00dc17971bf
wxicu <xichenwu@outlook.com>
2024-05-15 20:06:23 +0200

    update scripts

--- recipes/souporcell/build.sh
+++ recipes/souporcell/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -euo
+#!/usr/bin/env bash
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CFLAGS} -fcommon"
 
@@ -6,11 +6,11 @@ export CXXFLAGS="${CFLAGS} -fcommon"
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
-# build statically linked binary with Rust
-#RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path .
-cd /opt
-cd ${SRC_DIR}/troublet
-cargo build --release
-cd ${SRC_DIR}/souporcell
-cargo build --release
-mv target/release/* "${PREFIX}/bin"
+# Include scripts
+cp stan_consensus.pickle $PREFIX/stan_consensus.pickle
+cp *py $PREFIX
+chmod +x $PREFIX/souporcell_pipeline.py
+
+# Build statically linked binary with Rust
+RUST_BACKTRACE=1 cargo install --locked --root "$PREFIX" --path troublet
+RUST_BACKTRACE=1 cargo install --locked --root "$PREFIX" --path souporcell
