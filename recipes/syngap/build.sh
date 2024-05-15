b208146689afec3cc6132495c1a1367f8c153845
yanyew <360875601@qq.com>
2024-05-15 11:24:05 +0800

    Update syngap to v1.2.1

--- recipes/syngap/build.sh
+++ recipes/syngap/build.sh
@@ -1,12 +1,19 @@
+#!/usr/bin/env bash
+set -xe
+df -h
+du -sch $CONDA_PREFIX
 mkdir -p $PREFIX/bin
 cp -rf $SRC_DIR/bin $PREFIX/bin
 cp -rf $SRC_DIR/scripts $PREFIX/bin
+df -h
 cp $SRC_DIR/SynGAP.py $PREFIX/bin
 cp $SRC_DIR/initdb.py $PREFIX/bin
 cp $SRC_DIR/master.py $PREFIX/bin
 cp $SRC_DIR/dual.py $PREFIX/bin
 cp $SRC_DIR/triple.py $PREFIX/bin
 cp $SRC_DIR/custom.py $PREFIX/bin
+df -h
 cp $SRC_DIR/genepair.py $PREFIX/bin
 cp $SRC_DIR/evi.py $PREFIX/bin
 cp $SRC_DIR/eviplot.py $PREFIX/bin
+df -h
