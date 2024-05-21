8f8c4d28e8827c91bc3c86f7c6782c781483918f
mencian <joshua.zhuang@yahoo.com>
2024-05-20 23:03:01 -0500

    clean up recipe

--- recipes/bowtie2/build.sh
+++ recipes/bowtie2/build.sh
@@ -1,31 +1,20 @@
-#!/bin/bash
+#!/bin/bash -euo
 
 # Fetch third party dependencies 
 # (Git submodules - https://github.com/BenLangmead/bowtie2/blob/a43fa6f43f54989468a294967898f85b9fe4cefa/.gitmodules)
 git clone --branch master https://github.com/simd-everywhere/simde-no-tests.git third_party/simde
 git clone https://github.com/ch4rr0/libsais third_party/libsais
 
-LDFLAGS=""
-make CXX=$CXX CPP=$CXX CC=$CC LDLIBS="-L$PREFIX/lib -lz -lzstd -ltbb -ltbbmalloc -lpthread" WITH_ZSTD=1
+LDFLAGS="${LDFLAGS} -lz -lzstd -ltbb -ltbbmalloc -lpthread -L${PREFIX}/lib"
 
-binaries="\
-bowtie2 \
-bowtie2-align-l \
-bowtie2-align-s \
-bowtie2-build \
-bowtie2-build-l \
-bowtie2-build-s \
-bowtie2-inspect \
-bowtie2-inspect-l \
-bowtie2-inspect-s \
-"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -L${PREFIX}/lib"
+
+cmake --build build/ --target install -j "${CPU_COUNT}" -v
+
 directories="scripts"
-pythonfiles="bowtie2-build bowtie2-inspect"
-
-for i in $binaries; do
-    cp $i $PREFIX/bin && chmod +x $PREFIX/bin/$i
-done
 
 for d in $directories; do
-    cp -r $d $PREFIX/bin
+    cp -rf $d ${PREFIX}/bin
 done
