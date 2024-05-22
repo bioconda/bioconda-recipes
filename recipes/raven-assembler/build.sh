0cad01f042719652906deefd9a36cb1f627e9b45
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-05-22 11:16:40 +0300

    raven-assembler: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/raven-assembler/build.sh
+++ recipes/raven-assembler/build.sh
@@ -5,5 +5,5 @@ rm -rf build
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DRAVEN_BUILD_EXE=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -Dspoa_optimize_for_portability=ON ..
-make
+make -j ${CPU_COUNT}
 cp bin/raven $PREFIX/bin
