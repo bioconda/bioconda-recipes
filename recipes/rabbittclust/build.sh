95cb200a0150003d759a83dd61da7591c6d6988a
xiaomingxu1995 <xiaoming.xu@mail.sdu.edu.cn>
2024-05-22 08:23:16 -0400

    add the rabbittclust

--- recipes/rabbittclust/build.sh
+++ recipes/rabbittclust/build.sh
@@ -5,6 +5,10 @@ export CXX=${CXX:-g++}
 
 ./install.sh
 
+mkdir -p $PREFIX/bin
+cp clust-mst $PREFIX/bin/
+cp clust-greedy $PREFIX/bin/
+
 #export LIBRARY_DIRS="$LIBRARY_DIRS $PREFIX/lib"
 
 #make BIOCONDA=1
