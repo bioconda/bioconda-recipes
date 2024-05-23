7f137cf8a7275f837dcd90367ece19e61216b4ae
Sergey Koren <sergek@umd.edu>
2024-05-23 11:06:23 -0400

    undo rust change, it puts final binary into unexpected location so build fails

--- recipes/verkko/run_test.sh
+++ recipes/verkko/run_test.sh
@@ -10,15 +10,6 @@ if [ ! -n "${PREFIX-}" -a "${PREFIX+defined}" != defined  ]; then
    export PATH=$PREFIX/bin:$PATH
 fi
 
-# taken from yacrd recipe, see: https://github.com/bioconda/bioconda-recipes/blob/2b02c3db6400499d910bc5f297d23cb20c9db4f8/recipes/yacrd/build.sh
-if [ "$(uname)" == "Darwin" ]; then
-
-    # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
-    export HOME=`pwd`
-    echo "HOME is $HOME"
-    mkdir -p $HOME/.cargo/registry/index/
-fi
-
 # download and run a small assembly, skip alignment of ONT on OSX to save time
 rm -f ./subset.ids ./hifi.fastq.gz ./ont.fastq.gz
 curl -L https://obj.umiacs.umd.edu/sergek/shared/ecoli_hifi_subset24x.fastq.gz -o hifi.fastq.gz
