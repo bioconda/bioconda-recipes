186aac17a20952b8ad85fee97f544e4a9255f693
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-05-24 16:46:45 +0300

    Build hatchet instead of reusing its prebuilt binaries
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/hatchet/build.sh
+++ recipes/hatchet/build.sh
@@ -7,19 +7,21 @@ set -xe
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo installer -pkg gurobi*_macos_universal2.pkg -target /
 
-    ls -la /Library/
     export GUROBI_HOME=/Library/gurobi*
     mkdir -p $PREFIX/lib
-    ls -laR $GUROBI_HOME/
     cp $GUROBI_HOME/mac64/lib/libgurobi*.dylib $PREFIX/lib
 
-elif [[ "$OSTYPE" == "linux"* ]]; then
+else
+
+    # Gurobi Makefile uses a variable called 'C++'
+    # Rename this inline to 'CPP' so we can override it with the correct compiler on 'make'
+    sed -i 's/C++/CPP/g' gurobi*/*linux64/src/build/Makefile
+    (cd gurobi*/*linux64/src/build && make -j ${CPU_COUNT} CPP=${CXX})
+    (cd gurobi*/*linux64/lib && ln -f -s ../src/build/libgurobi_c++.a libgurobi_c++.a)
 
     export GUROBI_HOME=$(cd gurobi* && pwd)
     mkdir -p $PREFIX/lib
-    cp $GUROBI_HOME/*linux64/lib/*.so* $PREFIX/lib
-    cp $GUROBI_HOME/*linux64/lib/*.a $PREFIX/lib
-    ls -la $PREFIX/lib
+    cp $GUROBI_HOME/*linux64/lib/libgurobi*.so $PREFIX/lib
 fi
 
 export CXXFLAGS="-O3 -pthread -I${PREFIX}/include ${LDFLAGS}"
