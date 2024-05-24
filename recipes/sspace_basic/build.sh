e6cb5152c40ba9fbd895bf545f2a2019b0f135b7
Joon-Klaps <joonklaps@gmail.com>
2024-05-24 16:54:39 +0200

    fixing dependencies & better test

--- recipes/sspace_basic/build.sh
+++ recipes/sspace_basic/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin && cp -r bin $PREFIX/bin && cp SSPACE_Basic.pl $PREFIX/bin && cp -r tools $PREFIX/bin
+mkdir -p $PREFIX/bin && cp -r bin/* $PREFIX/bin && cp SSPACE_Basic.pl $PREFIX/bin && cp -r tools/* $PREFIX/bin
 chmod +x $PREFIX/bin/*
 sed -i.bak 's|/usr/bin/perl|/usr/bin/env perl|' $PREFIX/bin/*.pl
 ln -s $PREFIX/bin/SSPACE_Basic.pl $PREFIX/bin/sspace_basic
-mkdir -p $PREFIX/dotlib && cp -r dotlib $PREFIX/dotlib
+mkdir -p $PREFIX/bin/dotlib && cp -r dotlib/* $PREFIX/bin/dotlib
