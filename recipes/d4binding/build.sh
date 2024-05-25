0a488a15adc38de14bbad5f5b9bf18724defe2d3
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2024-05-25 19:46:58 +0200

    update path

--- recipes/d4binding/build.sh
+++ recipes/d4binding/build.sh
@@ -8,7 +8,7 @@ export \
     CARGO_NET_GIT_FETCH_WITH_CLI=true \
     CARGO_HOME="${BUILD_PREFIX}/.cargo"
 
-cp $RECIPE_DIR/build_htslib.sh ${PACKAGE_HOME}/d4-hts/build_htslib.sh
+cp $RECIPE_DIR/build_htslib.sh d4-hts/build_htslib.sh
 
 cd d4binding 
 ./install.sh
