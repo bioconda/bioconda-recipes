7c6bef4b890cc877983aae0ab465600d4d84e089
wxicu <xichenwu@outlook.com>
2024-05-26 20:21:44 +0200

    add resource

--- recipes/souporcell/build.sh
+++ recipes/souporcell/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CFLAGS} -fcommon"
+INSTALL_PREFIX="${PREFIX}" make -j${CPU_COUNT}
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
