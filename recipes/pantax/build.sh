e12265aa3e4c9157e3c950d0c7ce24636f12f766
CG Group <127956711+LuoGroup2023@users.noreply.github.com>
2024-05-27 20:12:33 +0800

    Update build.sh

--- recipes/pantax/build.sh
+++ recipes/pantax/build.sh
@@ -4,8 +4,6 @@ if [ "$(uname)" == "Darwin" ]; then
     cd $SRC_DIR/PanTax
     cp $SRC_DIR/PanTax/* $PREFIX
 else
-    PYTHON=${PYTHON:-$(which python)}
-    $PYTHON -m pip install . -vv --no-dependencies
     cd $SRC_DIR/pantax
     chmod +x pantax
     cp $SRC_DIR/pantax/pantax ${PREFIX}/bin
