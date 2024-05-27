c69f50ad3bedcbb68a3d7b36928566c6fcc7934c
Addimator <44083468+Addimator@users.noreply.github.com>
2024-05-27 23:36:28 +0200

    Update build.sh

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -1,13 +1,3 @@
-# Build and install gemBS
-cargo build --release
-echo test_addimator
-ls -ahl
-pwd
-echo test_addimator
-mkdir -p $PREFIX/bin
-cp target/release/gemBS $PREFIX/bin/
-
-
 echo addi1
 ls -ahl
 cd rust
