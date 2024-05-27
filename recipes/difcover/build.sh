6a30d5c2d14fc95e1a7a520d0d36908c6e9acb4e
nikostr <nikostr@users.noreply.github.com>
2024-05-27 09:49:03 +0200

    Updates difcover to pass tests and build

--- recipes/difcover/build.sh
+++ recipes/difcover/build.sh
@@ -3,7 +3,7 @@
 cd dif_cover_scripts/
 
 rm from_unionbed_to_ratio_per_window_CC0
-make
+make CXX="${GXX} ${LDFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
 cp from_unionbed_to_ratio_per_window_CC0 *.sh "${PREFIX}/bin"
