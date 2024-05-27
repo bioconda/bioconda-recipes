8257a6355f83f0c7d6b4bbdffa77f7be0f55c51a
CG Group <127956711+LuoGroup2023@users.noreply.github.com>
2024-05-27 17:01:56 +0800

    Update build.sh

--- recipes/pantax/build.sh
+++ recipes/pantax/build.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    cd $SRC_DIR
-    cp $SRC_DIR/* $PREFIX
+    cd $SRC_DIR/PanTax
+    cp $SRC_DIR/PanTax/* $PREFIX
 else
+    PYTHON=${PYTHON:-$(which python)}
+    $PYTHON -m pip install . -vv --no-dependencies
+    cd $SRC_DIR/pantax
     chmod +x pantax
+    cp $SRC_DIR/pantax/pantax ${PREFIX}/bin
+    cp $SRC_DIR/pantax/*sh ${PREFIX}/bin
     cd $SRC_DIR/tools
     chmod +x vg
     cd fastix
     cargo install fastix --root ./
+    mkdir -p ${PREFIX}/bin/tools
+    cp $SRC_DIR/tools/fastix/bin/fastix ${PREFIX}/bin/tools
+    cp $SRC_DIR/tools/vg ${PREFIX}/bin/tools
 fi
