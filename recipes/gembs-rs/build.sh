0907064f3333b3db4b1a1551b4798482e92829b0
Addimator <44083468+Addimator@users.noreply.github.com>
2024-05-28 11:32:46 +0200

    Copy recursive

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -8,9 +8,9 @@ ls -ahl target
 echo addi3
 ls -ahl target/release
 echo addi4
-cp gemBS $prefix/bin/gemBS
-cp read_filter $prefix/bin/
-cp bs_call $prefix/bin/
-cp snpxtr $prefix/bin/
-cp mextr $prefix/bin/
-cp dbsnp_index $prefix/bin/
+cp -r gemBS $prefix/bin/gemBS
+# cp read_filter $prefix/bin/
+# cp bs_call $prefix/bin/
+# cp snpxtr $prefix/bin/
+# cp mextr $prefix/bin/
+# cp dbsnp_index $prefix/bin/
