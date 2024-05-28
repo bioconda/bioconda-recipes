bd26178899a34d71a393713d2babed5b44f5c74c
Addimator <44083468+Addimator@users.noreply.github.com>
2024-05-28 11:00:10 +0200

    Update build.sh

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -8,9 +8,9 @@ ls -ahl target
 echo addi3
 ls -ahl target/release
 echo addi4
-cp target/release/gem_bs $prefix/bin/gemBS
-cp target/release/read_filter $prefix/bin/
-cp target/release/bs_call $prefix/bin/
-cp target/release/snpxtr $prefix/bin/
-cp target/release/mextr $prefix/bin/
-cp target/release/dbsnp_index $prefix/bin/
+cp gemBS $prefix/bin/gemBS
+cp read_filter $prefix/bin/
+cp bs_call $prefix/bin/
+cp snpxtr $prefix/bin/
+cp mextr $prefix/bin/
+cp dbsnp_index $prefix/bin/
