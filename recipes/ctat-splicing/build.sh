1401d8203a4e4743d59294e532ab334592c48b68
Michael Knudsen <michaelk@clin.au.dk>
2024-05-29 09:05:25 +0200

    Added missing file dependencies

--- recipes/ctat-splicing/build.sh
+++ recipes/ctat-splicing/build.sh
@@ -8,3 +8,9 @@ chmod +x ${PREFIX}/bin/ctat-splicing-lib-integration.py
 
 ln -s ${PREFIX}/bin/STAR_to_cancer_introns.py ${PREFIX}/bin/STAR_to_cancer_introns
 ln -s ${PREFIX}/bin/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
+
+mkdir -p ${PREFIX}/bin/PyLib
+mkdir -p ${PREFIX}/bin/util
+
+cp PyLib/Pipeliner.py ${PREFIX}/bin/PyLib/
+cp util/intron_occurrence_capture.py ${PREFIX}/bin/util/
