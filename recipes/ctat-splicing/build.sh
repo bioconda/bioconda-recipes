d8fdee8cd20d3dafa93509f36082bd1ce31d9fdb
Michael Knudsen <michaelk@clin.au.dk>
2024-05-29 07:15:54 +0200

    Maybe compiling isn't needed at all. It works on my computer...

--- recipes/ctat-splicing/build.sh
+++ recipes/ctat-splicing/build.sh
@@ -1,1 +1,10 @@
-find .
+mkdir -p ${PREFIX}/bin
+
+cp STAR_to_cancer_introns.py ${PREFIX}/bin/
+cp prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin
+
+chmod +x ${PREFIX}/bin/STAR_to_cancer_introns.py
+chmod +x ${PREFIX}/bin/ctat-splicing-lib-integration.py
+
+ln -s ${PREFIX}/bin/STAR_to_cancer_introns.py ${PREFIX}/bin/STAR_to_cancer_introns
+ln -s ${PREFIX}/bin/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
