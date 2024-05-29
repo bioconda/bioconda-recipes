511a70a093aa2fc6629f33824a0c3b1f509b780a
mencian <joshua.zhuang@yahoo.com>
2024-05-28 19:09:02 -0500

    clean up recipe

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+#!/bin/bash -euo
 
-cd $PREFIX
-cmake $SRC_DIR
-make
+# Needed for building utils dependency
+export INCLUDES="-I{PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
+
+mkdir -p "${PREFIX}/bin"
+
+mkdir build
+pushd build
+cmake ..
+make CXX="${CXX} ${LDFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+
+chmod 755 bin/bam-readcount
+cp -f bin/bam-readcount "${PREFIX}/bin"
+popd
