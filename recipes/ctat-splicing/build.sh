861dd85587c81656624b32a6c2c341e371713110
Michael Knudsen <michaelk@clin.au.dk>
2024-05-30 05:57:51 +0200

    Added missing files

--- recipes/ctat-splicing/build.sh
+++ recipes/ctat-splicing/build.sh
@@ -1,19 +1,21 @@
 mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/bin/prep_genome_lib
 
 cp STAR_to_cancer_introns.py ${PREFIX}/bin/
-cp prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin
+cp prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin/prep_genome_lib/
 
 chmod +x ${PREFIX}/bin/STAR_to_cancer_introns.py
-chmod +x ${PREFIX}/bin/ctat-splicing-lib-integration.py
+chmod +x ${PREFIX}/bin/prep_genome_lib/ctat-splicing-lib-integration.py
 
 ln -s ${PREFIX}/bin/STAR_to_cancer_introns.py ${PREFIX}/bin/STAR_to_cancer_introns
-ln -s ${PREFIX}/bin/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
+ln -s ${PREFIX}/bin/prep_genome_libprep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
 
 mkdir -p ${PREFIX}/bin/PyLib
-cp PyLib/*.py ${PREFIX}/bin/PyLib/
-
 mkdir -p ${PREFIX}/bin/util
-cp util/*.{py,pl} ${PREFIX}/bin/util/
-
 mkdir -p ${PREFIX}/bin/util/PerlLib
+
+cp PyLib/*.py ${PREFIX}/bin/PyLib/
+cp util/*.{py,pl} ${PREFIX}/bin/util/
 cp util/PerlLib/*.pm ${PREFIX}/bin/util/PerlLib/
+
+cp -r prep_genome_lib/util ${PREFIX}/bin/prep_genome_lib/
