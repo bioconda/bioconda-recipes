56cef70c0c0a618385e067a3b49b7be1808a05e4
Michael Knudsen <michaelk@clin.au.dk>
2024-05-30 08:32:06 +0200

    Moved some util files to different location

--- recipes/ctat-splicing/build.sh
+++ recipes/ctat-splicing/build.sh
@@ -2,20 +2,22 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/bin/prep_genome_lib
 
 cp STAR_to_cancer_introns.py ${PREFIX}/bin/
-cp prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin/prep_genome_lib/
+cp prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin/
 
 chmod +x ${PREFIX}/bin/STAR_to_cancer_introns.py
 chmod +x ${PREFIX}/bin/prep_genome_lib/ctat-splicing-lib-integration.py
 
 ln -s ${PREFIX}/bin/STAR_to_cancer_introns.py ${PREFIX}/bin/STAR_to_cancer_introns
-ln -s ${PREFIX}/bin/prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
+ln -s ${PREFIX}/bin/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
 
-mkdir -p ${PREFIX}/bin/PyLib
 mkdir -p ${PREFIX}/bin/util
 mkdir -p ${PREFIX}/bin/util/PerlLib
+mkdir -p ${PREFIX}/bin/PyLib
+
+cp util/*.{py,pl} ${PREFIX}/bin/util/
+cp prep_genome_lib/util/*.pl ${PREFIX}/bin/util/
 
 cp PyLib/*.py ${PREFIX}/bin/PyLib/
-cp util/*.{py,pl} ${PREFIX}/bin/util/
-cp util/PerlLib/*.pm ${PREFIX}/bin/util/PerlLib/
 
-cp -r prep_genome_lib/util ${PREFIX}/bin/prep_genome_lib/
+cp util/PerlLib/*.pm ${PREFIX}/bin/util/PerlLib/
+cp prep_genome_lib/util/PerlLib/*.pm ${PREFIX}/bin/util/PerlLib/
