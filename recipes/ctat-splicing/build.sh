7c244f73465cecfa3d4b080ffc60aec32f3cbf7c
Michael Knudsen <michaelk@clin.au.dk>
2024-05-30 09:10:53 +0200

    Forgot to update path

--- recipes/ctat-splicing/build.sh
+++ recipes/ctat-splicing/build.sh
@@ -5,7 +5,7 @@ cp STAR_to_cancer_introns.py ${PREFIX}/bin/
 cp prep_genome_lib/ctat-splicing-lib-integration.py ${PREFIX}/bin/
 
 chmod +x ${PREFIX}/bin/STAR_to_cancer_introns.py
-chmod +x ${PREFIX}/bin/prep_genome_lib/ctat-splicing-lib-integration.py
+chmod +x ${PREFIX}/bin/ctat-splicing-lib-integration.py
 
 ln -s ${PREFIX}/bin/STAR_to_cancer_introns.py ${PREFIX}/bin/STAR_to_cancer_introns
 ln -s ${PREFIX}/bin/ctat-splicing-lib-integration.py ${PREFIX}/bin/ctat-splicing-lib-integration
