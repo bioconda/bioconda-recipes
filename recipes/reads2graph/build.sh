ac7c1d95f2bdca88806d5eea1038dd03940bc5e1
Jappy0 <ping.pengyao@gmail.com>
2024-06-01 16:47:58 +1000

    update meta.yaml

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -14,9 +14,10 @@ cd build
 
 # Configure the build
 cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=${PREFIX}/include:${PREFIX}/include/seqan3/submodules/sdsl-lite/include:${PREFIX}/include/seqan3/submodules/cereal/include \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_MAKE_PROGRAM=make \
-      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
+      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
 # Build and install
