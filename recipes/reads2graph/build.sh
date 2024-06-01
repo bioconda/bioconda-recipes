92b184c96c27084129e4f6707528c88215400f49
Jappy0 <ping.pengyao@gmail.com>
2024-06-01 11:36:01 +1000

    update cross-linux.cmake

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 # Create build directory
 mkdir -p build
 cd build
@@ -8,7 +15,6 @@ cd build
 # Configure the build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-      -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
       -DCMAKE_MAKE_PROGRAM=make \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
