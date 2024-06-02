f154bc940c6dfb7b8dea22971aa2e284f36db561
Jappy0 <ping.pengyao@gmail.com>
2024-06-02 23:49:35 +1000

    update build.sh

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -34,6 +34,7 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
+make clean
 # Build and install
 make -j${CPU_COUNT}
 make install
