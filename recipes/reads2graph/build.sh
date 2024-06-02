177fe5689db8e916446226386e80794966594e7a
Jappy0 <ping.pengyao@gmail.com>
2024-06-02 22:28:44 +1000

    update build.sh

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -5,16 +5,15 @@ set -ex
 export PATH="${PREFIX}/bin:${PATH}"
 
 #Set the CXX environment variable to the specific g++ version
-# export CXX=$(which g++)
-# export CC=$(which gcc)
-export CXX=${PREFIX}/bin/g++
+export CXX=$(which g++)
+export CC=$(which gcc)
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-else
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
+# declare -a CMAKE_PLATFORM_FLAGS
+# if [[ ${HOST} =~ .*darwin.* ]]; then
+#     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+# else
+#     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+# fi
 
 # Check if build directory exists and delete it if it does
 if [ -d "build" ]; then
@@ -31,6 +30,7 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_MAKE_PROGRAM=make \
       -DCMAKE_CXX_COMPILER=${CXX} \
       -DCMAKE_CXX_STANDARD=20 \
+    #   -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
