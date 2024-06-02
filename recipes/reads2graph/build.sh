39c9bbcedfe9f96c42a19db84a85ea0e72f130a4
Jappy0 <ping.pengyao@gmail.com>
2024-06-02 11:43:45 +1000

    update meta.yaml build.sh

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -28,6 +28,9 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_MAKE_PROGRAM=make \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_CXX_STANDARD=20 \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
