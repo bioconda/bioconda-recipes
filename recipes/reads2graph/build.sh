68b4d243139c92175e4a93231ab25fe2a3e06bea
Jappy0 <ping.pengyao@gmail.com>
2024-06-02 22:44:07 +1000

    update build.sh

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -30,7 +30,7 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_MAKE_PROGRAM=make \
       -DCMAKE_CXX_COMPILER=${CXX} \
       -DCMAKE_CXX_STANDARD=20 \
-    #   -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" \
+      -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
--- recipes/reads2graph/cross-linux.cmake
+++ recipes/reads2graph/cross-linux.cmake
@@ -6,6 +6,7 @@ set(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
 set(CMAKE_CXX_COMPILER $ENV{CXX})
+set(CMAKE_C_COMPILER $ENV{CC})
 
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH $ENV{PREFIX} $ENV{BUILD_PREFIX}/$ENV{HOST}/sysroot)
