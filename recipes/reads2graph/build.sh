0a36c4a358c544bb6755fc25a01635d0002d8a65
Jappy0 <ping.pengyao@gmail.com>
2024-06-02 17:38:26 +1000

    update meta.yaml build.sh

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -5,8 +5,8 @@ set -ex
 export PATH="${PREFIX}/bin:${PATH}"
 
 # Set the CXX environment variable to the specific g++ version
-export CXX=$(which g++)
-export CC=$(which gcc)
+# export CXX=$(which g++)
+# export CC=$(which gcc)
 
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*darwin.* ]]; then
@@ -28,7 +28,6 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_MAKE_PROGRAM=make \
-      -DCMAKE_C_COMPILER=${CC} \
       -DCMAKE_CXX_COMPILER=${CXX} \
       -DCMAKE_CXX_STANDARD=20 \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
