ad1ee88b8b783f95b7cc711c4779ce9e7126f682
Joon-Klaps <joonklaps@gmail.com>
2024-06-03 10:38:22 +0200

    include reviewer suggestions

--- recipes/sspace_basic/build.sh
+++ recipes/sspace_basic/build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Copying the tools bin 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/bin/bin
-cp -r bin/* $PREFIX/bin
-cp -r bin/* $PREFIX/bin/bin  # Tool hardcoded the path of other used scripts to $Bin/bin/<tool>.pl
+cp -r bin $PREFIX
+cp -r bin $PREFIX/bin  # Tool hardcoded the path of other used scripts to $Bin/bin/<tool>.pl
 cp SSPACE_Basic.pl $PREFIX/bin 
 cp -r tools/* $PREFIX/bin
 
 # Copy dotlib 
-mkdir -p $PREFIX/bin/dotlib 
-cp -r dotlib/* $PREFIX/bin/dotlib
+cp dotlib $PREFIX/bin
 
 # Making the scripts executable
 chmod +x $PREFIX/bin/*.pl $PREFIX/bin/bin/*
