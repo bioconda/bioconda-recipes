93c672383b0842a86b279690d37d124024e0056d
Jappy0 <ping.pengyao@gmail.com>
2024-06-03 20:40:11 +1000

    meta.yaml

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -8,13 +8,6 @@ export PATH="${PREFIX}/bin:${PATH}"
 export CXX=$(which g++)
 export CC=$(which gcc)
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-else
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
-
 # Check if build directory exists and delete it if it does
 if [ -d "build" ]; then
     rm -rf build
@@ -24,14 +17,15 @@ fi
 mkdir -p build
 cd build
 
-${CXX} --version
-${CC} --version
+# ${CXX} --version
+# ${CC} --version
 
 # Configure the build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_MAKE_PROGRAM=make \
       -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake"
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=gnu++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
