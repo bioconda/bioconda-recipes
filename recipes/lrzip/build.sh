d76e2b87874654d1a6af00808f8d7d9ee7fdae30
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-03 15:03:12 +0300

    Run autogen.sh for lrzip
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/lrzip/build.sh
+++ recipes/lrzip/build.sh
@@ -3,6 +3,7 @@
 export CFLAGS="-O2 -fomit-frame-pointer -I$PREFIX/include $CFLAGS"
 export CXXFLAGS="-O2 -fomit-frame-pointer -I$PREFIX/include $CXXFLAGS"
 
+./autogen.sh
 ./configure --prefix=$PREFIX 
 make --jobs=${CPU_COUNT}
 make install
