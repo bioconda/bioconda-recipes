815e1b45073539005e987a713365fdf8ec84d774
mencian <joshua.zhuang@yahoo.com>
2024-06-04 00:36:16 -0500

    add docs

--- recipes/assemblycomparator2/build.sh
+++ recipes/assemblycomparator2/build.sh
@@ -8,7 +8,7 @@ export PACKAGE_HOME="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 mkdir -p "${PACKAGE_HOME}"
 chmod 755 asscom2
 cp -f asscom2 workflow/Snakefile config/config.yaml "${PACKAGE_HOME}"
-cp -rf profile workflow/scripts tests "${PACKAGE_HOME}"
+cp -rf docs profile workflow/scripts tests "${PACKAGE_HOME}"
 
 # This is the binary that we wish to be able to run.
 ln -sf ${PACKAGE_HOME}/asscom2 ${PREFIX}/bin/asscom2
