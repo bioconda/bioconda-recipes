24c8d8438c8b62c033ae97b5099c61ff48ceeb63
mencian <joshua.zhuang@yahoo.com>
2024-06-03 20:24:28 -0500

    add C compiler and openmp

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -4,7 +4,9 @@ mkdir -p "${PREFIX}/bin"
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CPP="${CXX} ${LDFLAGS}" CPPFLAGS="${CXXFLAGS} -I${PREFIX}/include"
+./install_muse.sh
+
+# make CPP="${CXX} ${LDFLAGS}" CPPFLAGS="${CXXFLAGS} -I${PREFIX}/include"
 
 chmod 755 MuSE
 cp -f MuSE "${PREFIX}/bin"
