729ffa2c959362d2932c4259e4e97bfd5af563a0
mencian <joshua.zhuang@yahoo.com>
2024-06-03 21:09:34 -0500

    Build vartrix on OSX

--- recipes/vartrix/build.sh
+++ recipes/vartrix/build.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
+#!/bin/bash -euo
+
 cargo build --release
 
 # try to find where binary is. it seems that the binary
 # uses some other variable here: target/<other-var>/vartrix
 VARTRIX_BIN=$(find . -type f -name "vartrix")
-cp -a $VARTRIX_BIN ${PREFIX}/bin/vartrix
+chmod 755 $VARTRIX_BIN
+cp -af $VARTRIX_BIN ${PREFIX}/bin/vartrix
