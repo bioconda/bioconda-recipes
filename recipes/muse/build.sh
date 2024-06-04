9b2c8c50a604bf3e7421908d248113e62823e153
mencian <joshua.zhuang@yahoo.com>
2024-06-03 19:54:19 -0500

    clean up recipe

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
-
-make CPP="${CXX}" CPPFLAGS="${CXXFLAGS}"
+#!/bin/bash -euo
 
 mkdir -p "${PREFIX}/bin"
-cp MuSE "${PREFIX}/bin/"
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+make CPP="${CXX} ${LDFLAGS}" CPPFLAGS="${CXXFLAGS} -I${PREFIX}/include"
+
+chmod 755 MuSE
+cp -f MuSE "${PREFIX}/bin"
