eddb454f609b3e55707ca5df73af72ec33a79dd0
Jappy0 <ping.pengyao@gmail.com>
2024-06-04 10:53:36 +1000

    update build and meta

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -13,10 +13,9 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_MAKE_PROGRAM=make \
       -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=gnu++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
 make -j ${CPU_COUNT}
 cp ../bin/reads2graph $PREFIX/bin/reads2graph
-
-#-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake" \
