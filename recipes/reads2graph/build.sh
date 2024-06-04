78c97fa009e7267ea7d978fe8b541d3490d1f526
Jappy0 <ping.pengyao@gmail.com>
2024-06-04 11:15:29 +1000

    update build and meta

--- recipes/reads2graph/build.sh
+++ recipes/reads2graph/build.sh
@@ -17,5 +17,5 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=gnu++20 -fopenmp -static-libgcc -static-libstdc++ -isystem ${PREFIX}/include -isystem ${PREFIX}/include/seqan3/submodules/sdsl-lite/include -isystem ${PREFIX}/include/seqan3/submodules/cereal/include -fconcepts -g -Wall -lyaml-cpp -Wno-interference-size -Wno-maybe-uninitialized" \
       ..
 
-make -j ${CPU_COUNT}
+make
 cp ../bin/reads2graph $PREFIX/bin/reads2graph
