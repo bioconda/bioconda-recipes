12198b0e7069b4c7045f954dbcfd739be3c2502f
Máté Balajti <balajtimate@gmail.com>
2024-06-04 17:09:15 +0200

    Update zarp to 1.0.0

--- recipes/zarp/built.sh
+++ recipes/zarp/built.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install the package
-$PYTHON -m pip install . -vv
+$PYTHON -m pip install . --no-deps --no-build-isolation -vvv
 
 # Manually copy the data file to the site-packages directory
-mkdir -p $SP_DIR/data
-cp data/genome_assemblies.csv $SP_DIR/data/
+mkdir -p $PREFIX/lib/python${PY_VER}/site-packages/zarp/data
+cp data/genome_assemblies.csv $PREFIX/lib/python${PY_VER}/site-packages/zarp/data/
