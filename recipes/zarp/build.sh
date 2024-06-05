9fdd903197b710fbde6f3a6c16d4de342817eb8a
Máté Balajti <balajtimate@gmail.com>
2024-06-05 04:05:28 +0200

    fix zarp test

--- recipes/zarp/build.sh
+++ recipes/zarp/build.sh
@@ -4,5 +4,5 @@
 $PYTHON -m pip install . --no-deps --no-build-isolation -vvv
 
 # Manually copy the data file to the site-packages directory
-mkdir -p $PREFIX/lib/python${PY_VER}/site-packages/zarp/data
-cp data/genome_assemblies.csv $PREFIX/lib/python${PY_VER}/site-packages/zarp/data/
+mkdir -p $PREFIX/share/zarp
+cp -r $SRC_DIR/data/ $PREFIX/lib/python${PY_VER}/site-packages/
