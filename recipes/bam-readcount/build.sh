0ea52e18b937725c4a75ec509213666be6f6b23e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-06 11:34:57 +0300

    Migrate Python2 to Python3 for Boost vendored sources
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -4,8 +4,16 @@ set -xe
 
 mkdir -p $PREFIX/bin
 
+pushd vendor
+    tar xf boost-1.55-bamrc.tar.gz
+    patch -p0 < ${RECIPE_DIR}/boost-python3.patch
+    tar czf boost-1.55-bamrc.tar.gz boost-1.55-bamrc
+    rm -rf boost-1.55-bamrc
+popd
+
 mkdir build 
 cd build
 cmake ..
+
 make
 cp ./bin/bam-readcount ${PREFIX}/bin/
