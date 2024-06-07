283e1e37602f17586728407df7864331f46b0fd6
Addimator <44083468+Addimator@users.noreply.github.com>
2024-06-07 14:42:01 +0200

    Update build.sh

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -4,5 +4,11 @@ cargo build --release
 
 mkdir -p $PREFIX/bin
 cp -r gemBS $PREFIX/bin/gemBS
+echo addi1
+ls -ahl
+echo addi2
+ls -ahl target
+echo addi3
+ls -ahl target/release
 ls $PREFIX/bin/
 chmod +x $PREFIX/bin/gemBS
