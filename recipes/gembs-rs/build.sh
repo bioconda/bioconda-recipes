6b61d58b367d3d4aa70c5acfca1d38bdeb6368dd
Addimator <44083468+Addimator@users.noreply.github.com>
2024-06-07 10:10:30 +0200

    Update build.sh

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -3,6 +3,6 @@ cp gemBS/Cargo.toml.in gemBS/Cargo.toml
 cargo build --release
 
 mkdir -p $PREFIX/bin
-cp target/release/gemBS $PREFIX/bin/gemBS
+cp -r gemBS $PREFIX/bin/gemBS
 ls $PREFIX/bin/
 chmod +x $PREFIX/bin/gemBS
