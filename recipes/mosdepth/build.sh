6125f2dce3ea8cb2452ff3786ac9c030317450d6
Martin Grigorov <martin-g@users.noreply.github.com>
2024-06-07 14:26:48 +0300

    Also pass `-L$PREFIX/lib` to `--passC:`

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -12,7 +12,7 @@ if [[ ${target_platform}  == osx-64 ]] ; then
     export CFLAGS="$CFLAGS -I$PREFIX/include"
     export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
     nimble install -y "docopt@0.7.0"
-    nimble build -y --verbose -d:release -d:d4 --passC:"-I$PREFIX/include"
+    nimble build -y --verbose -d:release -d:d4 --passC:"-I$PREFIX/include -L$PREFIX/lib"
 else
     #Link to mosdepth_d4 is specified below because of https://github.com/brentp/mosdepth/issues/232 
     curl -SL https://github.com/brentp/mosdepth/releases/download/v$PKG_VERSION/mosdepth_d4 -o mosdepth
