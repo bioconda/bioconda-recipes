b7630c89f552fb0602531424082b9f87ec4ea291
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-10 16:15:40 +0300

    Pass arch specific options to SPOA build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/spoa/build.sh
+++ recipes/spoa/build.sh
@@ -6,13 +6,25 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
+case $(uname -m) in
+    x86_64)
+        SPOA_OPTS="-Dspoa_optimize_for_portability=ON"
+        ;;
+    aarch64)
+        SPOA_OPTS="-Dspoa_use_simde_nonvec=ON -DBUILD_TESTING=OFF"
+        ;;
+    *)
+        ;;
+esac
+
+
 cmake -S . -B build \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_SHARED_LIBS=ON \
 	-Dspoa_install=ON \
 	-Dspoa_build_exe=ON \
-	-Dspoa_optimize_for_portability=ON \
+	${SPOA_OPTS} \
 	-Dspoa_use_simde=ON \
 	-Dspoa_use_simde_openmp=ON \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
