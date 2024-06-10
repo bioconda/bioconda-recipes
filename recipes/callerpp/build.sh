51db3d7d476033a0220351651e99e773ce1c6969
Nils Homer <nilshomer@gmail.com>
2024-06-10 01:07:25 -0700

    update version to 0.1.2 and use CXX flags

--- recipes/callerpp/build.sh
+++ recipes/callerpp/build.sh
@@ -3,8 +3,8 @@
 set -x -e -o pipefail
 
 make \
-  CC="${CC}" \
-  CFLAGS="${CPPFLAGS} ${CFLAGS} -g -Wall -O3 ${LDFLAGS}"
+  CXX="${CXX}" \
+  CXX_FLAGS="${CXX_FLAGS} -g -Wall -O3 ${LDFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
 
