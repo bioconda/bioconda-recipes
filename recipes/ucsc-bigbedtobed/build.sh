320f87d3383923b432676761a1bad1b6f4afd0c9
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-11 14:39:44 +0300

    Update ucsc-bigbedtobed to v465
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/ucsc-bigbedtobed/build.sh
+++ recipes/ucsc-bigbedtobed/build.sh
@@ -10,7 +10,7 @@ mkdir -p "$BINDIR"
 (cd kent/src/lib && make -j ${CPU_COUNT})
 (cd kent/src/htslib && make -j ${CPU_COUNT})
 (cd kent/src/jkOwnLib && make -j ${CPU_COUNT})
-(cd kent/src/hg/lib && make -j ${CPU_COUNT})
+(cd kent/src/hg/lib && USE_HIC=0 make -j ${CPU_COUNT})
 (cd kent/src/utils/bigBedToBed && make -j ${CPU_COUNT})
 cp bin/bigBedToBed "$PREFIX/bin"
 chmod +x "$PREFIX/bin/bigBedToBed"
