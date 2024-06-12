04269b742b3b66be558bd1f96d35bc7736583b7f
Nils Homer <nilshomer@gmail.com>
2024-06-11 21:58:56 -0700

    update to 0.1.4 to fix a seg fault

--- recipes/callerpp/build.sh
+++ recipes/callerpp/build.sh
@@ -3,10 +3,8 @@
 set -x -e -o pipefail
 
 make \
-  INCLUDES="${INCLUDES}" \
-  LIBS="${LIBPATH}" \
   CXX="${CXX} -std=c++11" \
-  CXX_FLAGS="${CXXFLAGS} -I${PREFIX}/include -L${PREFIX}/lib -g -Wall -O2 ${LDFLAGS}"
+  CXX_FLAGS="${CXXFLAGS} -I${PREFIX}/include -g -Wall -O3 ${LDFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
 
