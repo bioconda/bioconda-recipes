0a068d67fa0015f196235f8db3123a402933eb4d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-13 13:06:25 +0300

    centrifuge: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/centrifuge/build.sh
+++ recipes/centrifuge/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+set -xe
+
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
 
-make CXX=$CXX RELEASE_FLAGS="$CXXFLAGS"
+make -j ${CPU_COUNT} CXX=$CXX RELEASE_FLAGS="$CXXFLAGS"
 make install prefix=$PREFIX
 
 cp evaluation/{centrifuge_evaluate.py,centrifuge_simulate_reads.py} $PREFIX/bin
