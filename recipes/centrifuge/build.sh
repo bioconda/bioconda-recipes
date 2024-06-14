ee86554324009e5991f6280255feaf2e15eb75da
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-14 14:51:31 +0300

    Add a patch for fixing the build on linux aarch64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/centrifuge/build.sh
+++ recipes/centrifuge/build.sh
@@ -7,7 +7,17 @@ export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
 
-make -j ${CPU_COUNT} CXX=$CXX RELEASE_FLAGS="$CXXFLAGS"
+case $(uname -m) in
+    aarch64)
+        CXXFLAGS="${CXXFLAGS} -fsigned-char"
+        ARCH_OPTS="SSE_FLAG= POPCNT_CAPABILITY=0"
+        ;;
+    *)
+        ARCH_OPTS=""
+        ;;
+esac
+
+make -j ${CPU_COUNT} CXX=$CXX RELEASE_FLAGS="$CXXFLAGS" ${ARCH_OPTS}
 make install prefix=$PREFIX
 
 cp evaluation/{centrifuge_evaluate.py,centrifuge_simulate_reads.py} $PREFIX/bin
