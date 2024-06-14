a30f800a724792cf1391cacd49aa5858903766ae
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-14 14:44:03 +0300

    Improve the test to deal with the new interactive behavior of the program
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/genepop/build.sh
+++ recipes/genepop/build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 cd src
-${CXX} -DNO_MODULES -o Genepop GenepopS.cpp -O3 -std=c++14
+${CXX} -DNO_MODULES -o Genepop GenepopS.cpp -O3 -std=c++11
 
 mkdir -p $PREFIX/bin
 cp Genepop $PREFIX/bin
