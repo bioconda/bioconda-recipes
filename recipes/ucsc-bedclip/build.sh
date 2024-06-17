f85b15d6372949a65b552f05f528cd7ec6fa47b6
mencian <joshua.zhuang@yahoo.com>
2024-06-17 11:09:21 -0500

    revert ucsc-bedclip

--- recipes/ucsc-bedclip/build.sh
+++ recipes/ucsc-bedclip/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-mkdir -p "$PREFIX/bin"
-export MACHTYPE=x86_64
+
+if [ $(arch) = "aarch64" ]
+then
+    export CFLAGS+=" -O3 "
+    export CPPFLAGS+=" -O3 "
+else
+    export MACHTYPE=x86_64
+fi
+
 export BINDIR=$(pwd)/bin
 export L="${LDFLAGS}"
+
+mkdir -p "$PREFIX/bin"
 mkdir -p "$BINDIR"
 (cd kent/src/lib && make)
 (cd kent/src/htslib && make)
 (cd kent/src/jkOwnLib && make)
 (cd kent/src/hg/lib && make)
-(cd kent/src/hg/bedSort && make)
-cp bin/bedSort "$PREFIX/bin"
-chmod +x "$PREFIX/bin/bedSort"
+(cd kent/src/utils/bedClip && make)
+cp bin/bedClip "$PREFIX/bin"
+chmod +x "$PREFIX/bin/bedClip"
