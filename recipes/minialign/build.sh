19d7302b6755baddb507e0ee3a5296a3b212a41b
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-17 10:06:40 +0300

    minialign: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/minialign/build.sh
+++ recipes/minialign/build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+set -xe
+
 export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 mkdir -p $PREFIX/bin
-sed -i -e 's/-march=native/-msse4.2 -mpopcnt/g' Makefile
-make
+
+case $(uname -m) in
+    x86_64)
+        ARCH_OPTS="-msse4.2 -mpopcnt"
+        ;;
+    *)
+        ARCH_OPTS=""
+        ;;
+esac
+
+sed -i -e "s/-march=native/${ARCH_OPTS}/g" Makefile
+make -j ${CPU_COUNT}
 make install PREFIX=$PREFIX
 
