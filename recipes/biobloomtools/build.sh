e26d59635443c31ea29bb9c174aa1df7fe2a31bc
Lauren Coombe <lauren.e.coombe@gmail.com>
2024-06-18 00:15:28 -0700

    Update biobloomtools - rebuild after bulk failure (#48535)
    * Update biobloomtools - rebuild after bulk failure
    
    * Update CXXFLAGS

--- recipes/biobloomtools/build.sh
+++ recipes/biobloomtools/build.sh
@@ -3,7 +3,7 @@
 export CFLAGS="-I$PREFIX/include -O3"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH="${PREFIX}/include"
-export CXXFLAGS="-I${PREFIX}/include -O3"
+export CXXFLAGS="-I${PREFIX}/include -O3 -std=c++17"
 export CPPFLAGS="-isystem/${PREFIX}/include"
 
 ./configure --prefix=${PREFIX} --with-sparsehash="${PREFIX}" --with-sdsl="${PREFIX}" --disable-dependency-tracking --disable-silent-rules
