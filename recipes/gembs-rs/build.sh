ce4bb8f25c17c7fe19e1c72e8d87ee41f8f4c31d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-18 16:27:52 +0300

    Build only gem_bs Rust package. Fix the name of the file copied into $PREFIX/bin
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -4,17 +4,17 @@ set -xe
 
 cd rust
 cp gemBS/Cargo.toml.in gemBS/Cargo.toml
-cargo build --release
+cargo build --release --package gem_bs
 
 mkdir -p $PREFIX/bin
-cp -r target/release/gem_bs $PREFIX/bin/
+ls -ahl target/release
+echo addi4
+cp target/release/gem_bs $PREFIX/bin/gemBS
 echo addi1
 ls -ahl
 echo addi2
 ls -ahl target
 echo addi3
-ls -ahl target/release
-echo addi4
 ls -ahl target/release/build
 echo "List bin prefix contenets recursively"
 ls -ahlR $PREFIX/bin/
