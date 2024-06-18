f61c5ef057ba883ef0a3c466cd559291cdbd37aa
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-18 11:36:27 +0300

    Use newer config.sub/,guess files for linux-aarch64 support
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/cosi2/build.sh
+++ recipes/cosi2/build.sh
@@ -2,6 +2,10 @@
 
 set -xe
 
+# use newer config.guess and config.sub that support linux-aarch64
+cp ${RECIPE_DIR}/config.* ./build-aux/
+cp ${RECIPE_DIR}/config.* ./devel/miscutil/build-aux/
+
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
