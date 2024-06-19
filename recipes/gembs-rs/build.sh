cfc6ec4c3ec755c141fbaed0b31a3cb9e6e75429
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-19 12:23:17 +0300

    The built binary is in ./target/x86_64-unknown-linux-gnu/release/ folder
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -8,12 +8,5 @@ cargo build --release --package gem_bs
 
 mkdir -p $PREFIX/bin
 
-find ../ -name "*gem_bs*"
-
-ls -ahl target/release
-echo addi4
-cp target/release/gem_bs $PREFIX/bin/gemBS
-
-echo "List bin prefix contenets recursively"
-ls -ahlR $PREFIX/bin/
+cp target/$(uname -m)-unknown-linux-gnu/release/gem_bs $PREFIX/bin/gemBS
 chmod +x $PREFIX/bin/gemBS
