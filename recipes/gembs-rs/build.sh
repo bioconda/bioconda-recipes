5713b3fbcd6de73de2b8e10b6873be093f26d3fc
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-19 11:59:59 +0300

    Try to find the release binary
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -7,15 +7,13 @@ cp gemBS/Cargo.toml.in gemBS/Cargo.toml
 cargo build --release --package gem_bs
 
 mkdir -p $PREFIX/bin
+
+find ../ -name "*gem_bs*"
+
 ls -ahl target/release
 echo addi4
 cp target/release/gem_bs $PREFIX/bin/gemBS
-echo addi1
-ls -ahl
-echo addi2
-ls -ahl target
-echo addi3
-ls -ahl target/release/build
+
 echo "List bin prefix contenets recursively"
 ls -ahlR $PREFIX/bin/
 chmod +x $PREFIX/bin/gemBS
