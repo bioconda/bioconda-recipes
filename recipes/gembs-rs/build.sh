2e48f7b33275e79d31066dfc1b0bcf456fb40efa
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-19 10:00:31 +0300

    Point to ../target. Add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gembs-rs/build.sh
+++ recipes/gembs-rs/build.sh
@@ -9,7 +9,7 @@ cargo build --release --package gem_bs
 mkdir -p $PREFIX/bin
 ls -ahl target/release
 echo addi4
-cp target/release/gem_bs $PREFIX/bin/gemBS
+cp ../target/release/gem_bs $PREFIX/bin/gemBS
 echo addi1
 ls -ahl
 echo addi2
