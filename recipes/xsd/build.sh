974f49e43ee1cb197436171bbaa1591abc18d8d8
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-20 09:08:00 +0300

    Update xsd to 4.2.0. Build with build2 tool
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/xsd/build.sh
+++ recipes/xsd/build.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
-make CC="$CC" CXX="$CXX" AR="$AR" RANLIB="$RANLIB" CPPFLAGS="$CPPFLAGS" CXXFLAGS="$CXXFLAGS -std=c++14" LDFLAGS="$LDFLAGS"
+set -xe
+
+wget https://download.build2.org/0.17.0/build2-install-0.17.0.sh
+
+sh build2-install-0.17.0.sh --yes --trust yes --timeout 6000 --jobs ${CPU_COUNT} ${PREFIX}
+
+
+mkdir xsd-build
+$ cd xsd-build
+
+$ bpkg create -d xsd-gcc-N cc     \
+  config.cxx=${CXX}               \
+  config.cc.coptions=${CFLAGS}    \
+  config.bin.rpath=${PREFIX}/lib  \
+  config.install.root=${PREFIX}   \
+  config.install.sudo=false
+
+make CC="$CC" CXX="$CXX" AR="$AR" RANLIB="$RANLIB" CPPFLAGS="$CPPFLAGS" CXXFLAGS="$CXXFLAGS -std=c++14" LDFLAGS="$LDFLAGS" -j ${CPU_COUNT}
 make install_prefix="$PREFIX" install
+
+
+
+
+
+bpkg create -d xsd-gcc-N cc     \
+  config.cxx=g++                  \
+  config.cc.coptions=-O3          \
+  config.bin.rpath=/usr/local/lib \
+  config.install.root=/tmp/usr/local  \
+  config.install.sudo=sudo
+
+cd xsd-gcc-N
+
+bpkg build --trust-yes --jobs 6 --progress xsd@https://pkg.cppget.org/1/beta
