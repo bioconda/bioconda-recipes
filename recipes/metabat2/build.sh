3365a4a7e0cc6913ea2d85eb181287c1fafba9c5
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-20 10:27:20 +0300

    Manually apply the patch to htscodecs after calling `cmake ..`
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/metabat2/build.sh
+++ recipes/metabat2/build.sh
@@ -5,8 +5,17 @@ set -xe
 #sed -i.bak "s/set(Boost_USE_STATIC_LIBS   ON)/set(Boost_USE_STATIC_LIBS   OFF)/g" src/CMakeLists.txt
 mkdir build
 cd build
+
 export CXXFLAGS=-ldeflate
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX
+
+if [ "$(uname -m)" == "aarch64" ]; then
+    # fetch the htslib dependency
+    make htslib/fast
+    pushd ./contrib/htslib-prefix/src/htslib
+      patch -p1 < ${RECIPE_DIR}/arm_hwcap.patch
+    popd
+fi
 
 # Fix the version
 make check_git_repository
