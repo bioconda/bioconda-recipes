10e3996808c1a9e62a967dba8250fd5f02eb512e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-20 09:15:46 +0300

    xsd: update to 4.2.0 and add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/xsd/build.sh
+++ recipes/xsd/build.sh
@@ -2,35 +2,21 @@
 
 set -xe
 
+# https://build2.org/install.xhtml#unix
 wget https://download.build2.org/0.17.0/build2-install-0.17.0.sh
 
-sh build2-install-0.17.0.sh --yes --trust yes --timeout 6000 --jobs ${CPU_COUNT} ${PREFIX}
+sh build2-install-0.17.0.sh --yes --trust yes --timeout 6000 --jobs ${CPU_COUNT} --cxx ${CXX} ${PREFIX}
 
-
-mkdir xsd-build
-$ cd xsd-build
-
-$ bpkg create -d xsd-gcc-N cc     \
+# https://www.codesynthesis.com/products/xsd/doc/install-build2.xhtml#unix-build2
+bpkg create -d xsd-conda-build cc     \
   config.cxx=${CXX}               \
-  config.cc.coptions=${CFLAGS}    \
-  config.bin.rpath=${PREFIX}/lib  \
-  config.install.root=${PREFIX}   \
-  config.install.sudo=false
-
-make CC="$CC" CXX="$CXX" AR="$AR" RANLIB="$RANLIB" CPPFLAGS="$CPPFLAGS" CXXFLAGS="$CXXFLAGS -std=c++14" LDFLAGS="$LDFLAGS" -j ${CPU_COUNT}
-make install_prefix="$PREFIX" install
-
-
-
-
-
-bpkg create -d xsd-gcc-N cc     \
-  config.cxx=g++                  \
-  config.cc.coptions=-O3          \
-  config.bin.rpath=/usr/local/lib \
-  config.install.root=/tmp/usr/local  \
+  config.cc.coptions=${CXXFLAGS}    \
+  config.bin.rpath=${PREFIX}/lib \
+  config.install.root=${PREFIX}  \
   config.install.sudo=sudo
 
-cd xsd-gcc-N
+cd xsd-conda-build
 
-bpkg build --trust-yes --jobs 6 --progress xsd@https://pkg.cppget.org/1/beta
+bpkg build --trust-yes --jobs ${CPU_COUNT} xsd@https://pkg.cppget.org/1/beta ?sys:libxerces-c
+
+ls -laR ${PREFIX}
