caf3452cb607f05050f3f5cfd6f89ecb4d0258c9
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-21 14:13:55 +0300

    Remove some debug statements
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/pangwes/build.sh
+++ recipes/pangwes/build.sh
@@ -5,11 +5,6 @@ set -xe
 # build and install PAN-GWES
 make -C sw/pangwes -j ${CPU_COUNT}
 
-if [[ $target_platform == osx-* ]]; then
-  which llvm-otool
-  llvm-otool --version
-fi
-
 mkdir -p ${PREFIX}/bin
 cp $SRC_DIR/sw/pangwes/bin/unitig_distance $PREFIX/bin/unitig_distance
 cp $SRC_DIR/sw/pangwes/bin/gfa1_parser $PREFIX/bin/gfa1_parser
