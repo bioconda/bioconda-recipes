85959ce197003130439b04e212c89710ffed6bb3
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-24 09:46:53 +0300

    odgi: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/odgi/build.sh
+++ recipes/odgi/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -xe
+
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export CPATH=${PREFIX}/include
@@ -6,9 +9,19 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
+
+case $(uname -m) in
+    x86_64)
+        ARCH_OPTS="-march=sandybridge"
+        ;;
+    *)
+        ARCH_OPTS=""
+        ;;
+esac
+
 #cmake -H. -Bbuild -DPYTHON_EXECUTABLE:FILEPATH=$PYTHON -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast -Og'
-cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
-cmake --build build
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS="${ARCH_OPTS} -Ofast"
+cmake --build build -j ${CPU_COUNT}
 mkdir -p $PREFIX/bin
 mv bin/* $PREFIX/bin
 PYVER=`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
