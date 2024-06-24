1f140d0a579b3ac75ac11cc09a77d6eef4d5ea93
Marcel Martin <marcel.martin@scilifelab.se>
2024-06-24 14:04:56 +0200

    Update recipes/mason/build.sh
    Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

--- recipes/mason/build.sh
+++ recipes/mason/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cmake -S . -B build -DSEQAN_BUILD_SYSTEM=APP:mason2 -DCMAKE_INSTALL_PREFIX=${PREFIX}
-cmake --build build/
+cmake --build build/ -j ${CPU_COUNT}
 cmake --install build/
