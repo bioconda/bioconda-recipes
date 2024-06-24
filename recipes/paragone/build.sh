93410299c7fb16be0203fe96b5d6a03da1ecb0b6
mencian <joshua.zhuang@yahoo.com>
2024-06-24 13:53:04 -0500

    clean up recipe

--- recipes/paragone/build.sh
+++ recipes/paragone/build.sh
@@ -1,6 +1,11 @@
+#!/bin/bash -euo
+
+mkdir -p ${PREFIX}/bin
+
 # Install ParaGone
-$PYTHON -m pip install --no-deps --ignore-installed .
+$PYTHON -m pip install --no-deps --no-build-isolation --no-cache-dir . -vvv
 
 # Install TAPER:
 git clone https://github.com/chaoszhang/TAPER.git
-cp TAPER/correction_multi.jl ${PREFIX}/bin
+chmod 755 TAPER/correction_multi.jl
+cp -f TAPER/correction_multi.jl ${PREFIX}/bin
