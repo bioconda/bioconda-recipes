c2cb6af108578997dede8ec2cc96b2171278b713
mencian <joshua.zhuang@yahoo.com>
2024-06-25 09:50:58 -0500

    edit python variable

--- recipes/pyslow5/build.sh
+++ recipes/pyslow5/build.sh
@@ -3,4 +3,4 @@
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
 
-{{ PYTHON }} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
