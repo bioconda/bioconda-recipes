57190b3b1cb64853994117141c97907d9bb5848f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-25 14:37:34 +0300

    rscape: copy config.guess/.sub files to R2R lib too
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/rscape/build.sh
+++ recipes/rscape/build.sh
@@ -5,6 +5,8 @@ set -xe
 # use newer config.guess and config.sub that support linux-aarch64
 cp ${RECIPE_DIR}/config.* .
 
+cp ${RECIPE_DIR}/config.* lib/R2R/R2R-current/
+
 ./configure --disable-avx512 --prefix=$PREFIX
 
 make V=1 -j ${CPU_COUNT}
