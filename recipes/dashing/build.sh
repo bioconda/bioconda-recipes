ff02c22014120be446f0a35444016601db035bef
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-27 09:19:10 +0300

    add linux-aarch64 builds for dashing & strobealign
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/dashing/build.sh
+++ recipes/dashing/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
@@ -9,10 +11,10 @@ pushd bonsai
 rm -rf zstd
 git clone --recursive --single-branch --branch dev https://github.com/facebook/zstd
 pushd zstd
-make CC=$CC lib && mv lib/libzstd.a ..
+make -j ${CPU_COUNT} CC=$CC lib && mv lib/libzstd.a ..
 popd
 popd
 
 sed -i.bak "s/ -lzstd//g" Makefile
-make CC=$CC CXX=$CXX
+make -j ${CPU_COUNT} CC=$CC CXX=$CXX
 make install PREFIX=$PREFIX
