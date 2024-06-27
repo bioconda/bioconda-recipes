c535eae1f642b557e446ef3926a85ab2e5d0cef2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-27 09:39:07 +0300

    Use xstd-static as a host/run dependency instead of building it
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/dashing/build.sh
+++ recipes/dashing/build.sh
@@ -7,14 +7,6 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-pushd bonsai
-rm -rf zstd
-git clone --recursive --single-branch --branch dev https://github.com/facebook/zstd
-pushd zstd
-make -j ${CPU_COUNT} CC=$CC lib && mv lib/libzstd.a ..
-popd
-popd
-
 sed -i.bak "s/ -lzstd//g" Makefile
 make -j ${CPU_COUNT} CC=$CC CXX=$CXX
 make install PREFIX=$PREFIX
