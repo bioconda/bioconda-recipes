d7e4156aba5e01ccffe536fc5c27a14d1bf0bb71
Hang Xue <hang_xue@berkeley.edu>
2024-06-26 20:18:58 -0700

    new build

--- recipes/tetrimmer/build.sh
+++ recipes/tetrimmer/build.sh
@@ -1,5 +1,20 @@
+# set -x -e
+# mkdir -p ${PREFIX}/bin
+# cp -rf * ${PREFIX}/bin
+# cd ${PREFIX}/bin/TEtrimmer-main/tetrimmer
+
 #!/bin/sh
 set -x -e
+
+tetrimmer_DIR=${PREFIX}/share/tetrimmer
+
 mkdir -p ${PREFIX}/bin
-cp -rf * ${PREFIX}/bin
-cd ${PREFIX}/bin/TEtrimmer-main/tetrimmer
+mkdir -p ${tetrimmer_DIR}
+cp -r tetrimmer/* ${tetrimmer_DIR}
+
+cat <<END >>${PREFIX}/bin/TEtrimmer
+#!/bin/bash
+python ${tetrimmer_DIR}/TEtrimmer.py \$@
+END
+
+chmod a+x ${PREFIX}/bin/TEtrimmer
