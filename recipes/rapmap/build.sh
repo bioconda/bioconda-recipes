3a3288e9cffff9ad2a8bcfe493752730fd668781
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-06-28 14:16:15 +0300

    rapmap: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/rapmap/build.sh
+++ recipes/rapmap/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-set -eu -o pipefail
+set -exu -o pipefail
+
+case $(uname -m) in
+    x86_64)
+        ARCH_FLAGS="-mno-avx"
+        ;;
+    *)
+        ARCH_FLAGS=""
+        ;;
+esac
 
 mkdir build && cd build
-cmake -DNO_NATIVE_ARCH=TRUE -DCMAKE_CXX_FLAGS=-mno-avx -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_BUILD_TYPE=RELEASE ..
-make
+cmake -DNO_NATIVE_ARCH=TRUE -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${ARCH_FLAGS}" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_BUILD_TYPE=RELEASE ..
+make -j ${CPU_COUNT}
 make install
 CTEST_OUTPUT_ON_FAILURE=1 make test
