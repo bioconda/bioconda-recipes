9fc1c7195036a6b720e5b6db81bcbeaf0734fb58
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-01 10:08:28 +0300

    Copy clermonTyping.sh to $PREFIX/bin
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/clermontyping/build.sh
+++ recipes/clermontyping/build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-cp $SRC_DIR/clermonTyping.sh ${PREFIX}
 mkdir -p ${PREFIX}/bin
+cp $SRC_DIR/clermonTyping.sh ${PREFIX}/bin/
 mkdir -p ${PREFIX}/bin/bin
 cp $SRC_DIR/bin/* ${PREFIX}/bin/bin
