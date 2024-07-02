2b99a1f0e1276b05c08ee9a608b2f6dd31cb0713
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-02 09:51:44 +0300

    Export LINKPATH instead of the standard LDFLAGS
    https://github.com/mourisl/centrifuger/blob/v1.0.4/Makefile#L3
    
    Preserve "-msse4.2" in CXXFLAGS for x86_64
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/centrifuger/build.sh
+++ recipes/centrifuger/build.sh
@@ -2,12 +2,15 @@
 
 set -xe
 
-export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+
+if [ $(uname -m) == "x86_64" ]; then
+    CXXFLAGS="${CXXFLAGS} -msse4.2"
+fi
 
 mkdir -p $PREFIX/bin
 
-make -j ${CPU_COUNT} CXX="$CXX" CXXFLAGS="$CXXFLAGS"
+make -j ${CPU_COUNT} CXX="$CXX" CXXFLAGS="$CXXFLAGS" LINKPATH="-L${PREFIX}/lib"
 chmod +x centrifuger-download
 chmod +x centrifuger-kreport
 chmod +x centrifuger-inspect
