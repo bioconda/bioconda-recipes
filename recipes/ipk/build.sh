d7e7718dfa887654cb06f3eb104748a39834534c
Benjamin Linard <benjamin.linard@inrae.fr>
2024-07-04 13:35:50 +0200

    build in build

--- recipes/ipk/build.sh
+++ recipes/ipk/build.sh
@@ -14,21 +14,21 @@ mkdir -p $PREFIX/lib
 
 cmake -B build -DHASH_MAP=USE_TSL_ROBIN_MAP -DCMAKE_CXX_FLAGS="-O3" -DBUILD_SHARED_LIBS=ON --install-prefix=$PREFIX
 cmake --build build --target all --target diff-dna --target diff-aa --target dump-dna --target dump-aa
-cmake --install $PREFIX
+cmake --install build
 
 ls $PREFIX/*
 
-#cp build/ipk/ipk-aa $PREFIX/bin
-#cp build/ipk/ipk-aa-pos $PREFIX/bin
-#cp build/ipk/ipk-dna $PREFIX/bin
-#cp build/tools/diff-dna $PREFIX/bin
-#cp build/tools/diff-aa $PREFIX/bin
-#cp build/tools/dump-dna $PREFIX/bin
-#cp build/tools/dump-aa $PREFIX/bin
+cp build/ipk/ipk-aa $PREFIX/bin
+cp build/ipk/ipk-aa-pos $PREFIX/bin
+cp build/ipk/ipk-dna $PREFIX/bin
+cp build/tools/diff-dna $PREFIX/bin
+cp build/tools/diff-aa $PREFIX/bin
+cp build/tools/dump-dna $PREFIX/bin
+cp build/tools/dump-aa $PREFIX/bin
 
-#cp build/i2l/libi2l_aa.a $PREFIX/lib
-#cp build/i2l/libi2l_aa_pos.a $PREFIX/lib
-#cp build/i2l/libi2l_dna.a $PREFIX/lib
+cp build/i2l/libi2l_aa.a $PREFIX/lib
+cp build/i2l/libi2l_aa_pos.a $PREFIX/lib
+cp build/i2l/libi2l_dna.a $PREFIX/lib
 
 #chmod +x $PREFIX/bin/ipk-aa
 #chmod +x $PREFIX/bin/ipk-dna
