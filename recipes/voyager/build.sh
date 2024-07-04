a30a32bc0dab1672db75ca1d3d2a97628930b09a
sverre-branders <sverre.branders@protonmail.com>
2024-07-04 15:14:00 +0200

    Minor fixes

--- recipes/voyager/build.sh
+++ recipes/voyager/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 mkdir -p "$PREFIX/bin"
 
-# tar -xzf voyager-*.tar.gz
-
 cp voyager-cli $PREFIX/bin/
 cp voyager-build-cli $PREFIX/bin/
 cp voyager-combine-cli $PREFIX/bin/
--- recipes/voyager/run_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-voyager-cli -v >/dev/null
-voyager-build-cli -v >/dev/null
-voyager-combine-cli -v >/dev/null
-voyager-debug-index -v >/dev/null
