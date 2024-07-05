1e945aff740ea240ab853489d70b8c4c54c3cedb
sverre-branders <sverre.branders@protonmail.com>
2024-07-05 12:49:54 +0200

    updated recipe data

--- recipes/voyager/build.sh
+++ recipes/voyager/build.sh
@@ -5,3 +5,10 @@ cp voyager-cli $PREFIX/bin/
 cp voyager-build-cli $PREFIX/bin/
 cp voyager-combine-cli $PREFIX/bin/
 cp voyager-debug-index $PREFIX/bin/
+
+mkdir -p "$PREFIX/lib"
+cp voyager-monitor-1.0.00-SNAPSHOT.jar $PREFIX/lib/
+
+echo '#!/bin/bash' > ${PREFIX}/bin/voyager-monitor
+echo 'java -jar "'$PREFIX'/lib/voyager-monitor-1.0.00-SNAPSHOT.jar" "$@"' >> $PREFIX/bin/voyager-monitor
+chmod +x "${PREFIX}/bin/voyager-monitor"
---
+++ recipes/voyager/run_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+voyager-cli -v
+voyager-build-cli -v
+voyager-combine-cli -v
+voyager-debug-index -v
