c4eeb268b62a7407f4c47415708e29693507f043
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-08 15:15:16 +0300

    narfmap: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/narfmap/build.sh
+++ recipes/narfmap/build.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+
+set -xe
+
 export HAS_GTEST=0
 
 # fix build number in config
 sed -i.bak 's/VERSION_STRING.*/VERSION_STRING="${PKG_VERSION}"/' config.mk
 
-make CXX=$CXX CC=$CC CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS"
+make -j ${CPU_COUNT} CXX=$CXX CC=$CC CXXFLAGS="$CXXFLAGS" CFLAGS="$CFLAGS"
 mkdir -p "${PREFIX}/bin"
 mv build/release/dragen-os ${PREFIX}/bin/
