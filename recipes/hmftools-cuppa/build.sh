fd505bd34bfb065c61897ad8d08fd4fd2c2eafd0
Stephen Watts <hello@stephen.ac>
2024-07-09 14:52:30 +1000

    Add pycuppa

--- recipes/hmftools-cuppa/build.sh
+++ recipes/hmftools-cuppa/build.sh
@@ -6,6 +6,7 @@ TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 
 cd "${SRC_DIR}"
 mv jar/cuppa*.jar $TGT/cuppa.jar
+${PYTHON} -m pip install --no-build-isolation --no-deps --no-cache-dir -vvv src/cuppa/src/main/python/pycuppa/
 
 cp $RECIPE_DIR/cuppa.sh $TGT/cuppa
 ln -s $TGT/cuppa ${PREFIX}/bin/
