5e131ada2352898922d4718db542122150f12c05
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2024-07-09 15:40:50 +0100

    Update recipes/gw/build.sh
    Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   sed -i 's/-lEGL -lGLESv2/-lGL/' Makefile
 fi
 make prep
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" LDFLAGS="${LDFLAGS} -L${PREFIX}" prefix="${PREFIX}"  make
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" LDFLAGS="${LDFLAGS} -L${PREFIX}" prefix="${PREFIX}"  make -j ${CPU_COUNT}
 mkdir -p $PREFIX/bin
 cp gw $PREFIX/bin/gw
 cp -n .gw.ini $PREFIX/bin/.gw.ini
