b14cc8eca30e56847e1672602f4260f02fb7e70e
Stephen Watts <hello@stephen.ac>
2024-07-09 14:51:04 +1000

    Remove CUPPA chart BASH script

--- recipes/hmftools-cuppa/build.sh
+++ recipes/hmftools-cuppa/build.sh
@@ -6,9 +6,6 @@ TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 
 cd "${SRC_DIR}"
 mv jar/cuppa*.jar $TGT/cuppa.jar
-mv src/cuppa/src/main/resources/cuppa-chart/* $TGT/chart/
 
 cp $RECIPE_DIR/cuppa.sh $TGT/cuppa
-cp $RECIPE_DIR/cuppa-chart.sh $TGT/cuppa-chart
-ln -s $TGT/cuppa{,-chart} ${PREFIX}/bin/
-chmod 0755 ${PREFIX}/bin/cuppa{,-chart}
+ln -s $TGT/cuppa ${PREFIX}/bin/
