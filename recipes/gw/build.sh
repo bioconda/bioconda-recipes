5ff02c7e04d0c86fce85b9070a3dc63ab828cb1f
Martin Grigorov <martin-g@users.noreply.github.com>
2024-07-10 16:47:17 +0300

    Add '-L${PREFIX}/lib' to LDFLAGS

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   sed -i 's/-lEGL -lGLESv2/-lGL/' Makefile
 fi
 make prep
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" LDFLAGS="${LDFLAGS} -Wl,-rpath,$(PREFIX)/lib -Wl,-rpath-link,$(PREFIX)/lib" LDLIBS="-lxcb-glx -lxcb-dri2 -lexpat" prefix="${PREFIX}"  make -j ${CPU_COUNT}
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,$(PREFIX)/lib -Wl,-rpath-link,$(PREFIX)/lib" LDLIBS="-lxcb-glx -lxcb-dri2 -lexpat" prefix="${PREFIX}"  make -j ${CPU_COUNT}
 mkdir -p $PREFIX/bin
 cp gw $PREFIX/bin/gw
 cp -n .gw.ini $PREFIX/bin/.gw.ini
