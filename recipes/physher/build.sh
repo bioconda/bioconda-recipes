8bf550f67d295f170c7beedc654963e6ebffd929
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-10 12:35:59 +0300

    Use sse2neon for SSE support for linux-aarch64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/physher/build.sh
+++ recipes/physher/build.sh
@@ -2,15 +2,6 @@
 
 set -xe
 
-case $(uname -m) in
-    aarch64)
-        ARCH_OPTS="-DUSE_SSE_SUPPORT=OFF"
-        ;;
-    *)
-        ARCH_OPTS=""
-        ;;
-esac
-
-cmake -S . -B build -DBUILD_CPP_WRAPPER=on -DCMAKE_INSTALL_PREFIX=$PREFIX ${ARCH_OPTS}
+cmake -S . -B build -DBUILD_CPP_WRAPPER=on -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 cmake --build build/ --target install -j ${CPU_COUNT}
