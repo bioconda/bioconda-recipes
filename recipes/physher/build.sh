fa200ef7516106981c04a6d30d9fd34f96d70d15
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-10 12:22:56 +0300

    Disable SSE for linux-aarch64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/physher/build.sh
+++ recipes/physher/build.sh
@@ -2,6 +2,15 @@
 
 set -xe
 
-cmake -S . -B build -DBUILD_CPP_WRAPPER=on -DCMAKE_INSTALL_PREFIX=$PREFIX
+case $(uname -m) in
+    aarch64)
+        ARCH_OPTS="-DUSE_SSE_SUPPORT=OFF"
+        ;;
+    *)
+        ARCH_OPTS=""
+        ;;
+esac
+
+cmake -S . -B build -DBUILD_CPP_WRAPPER=on -DCMAKE_INSTALL_PREFIX=$PREFIX ${ARCH_OPTS}
 
 cmake --build build/ --target install -j ${CPU_COUNT}
