48ba84736939744d834a7ac921e3c2b28b5b7531
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-11 00:30:36 +0200

    Disable failing arm64 builds

--- recipes/raxml/build.sh
+++ recipes/raxml/build.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-case `uname` in
-Darwin) SUF=.mac;;
-Linux) SUF=.gcc;;
-*) echo "Unknown architecture"; exit 1;;
+set -euxo pipefail
+
+PREFIX=prefix
+
+case $(uname) in
+Darwin) SUF=.mac ;;
+Linux) SUF=.gcc ;;
+*)
+  echo "Unknown architecture"
+  exit 1
+  ;;
 esac
 
 ARCH=$(uname -m)
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX"/bin
 
 for PTHREADS in "" .PTHREADS; do
   for OPT in "" .SSE3 .AVX2; do
 
-    if [ "${ARCH}" == "aarch64" -a "${OPT}" == ".AVX2" ]; then
+    if [[ "${ARCH}" == "aarch64" && "${OPT}" == ".AVX2" ]]; then
+      continue
+    fi
+
+    if [[ ${ARCH} == "arm64" && ("${OPT}" == ".AVX2" || "${OPT}" == ".SSE3" || "${PTHREADS}" == ".PTHREADS") ]]; then
       continue
     fi
 
@@ -24,8 +35,8 @@ for PTHREADS in "" .PTHREADS; do
     else
       MAKEFILE=${MAKEFILE}.gcc
     fi
-    make -f ${MAKEFILE} CC=$CC
-    mv raxmlHPC* $PREFIX/bin
+    make -f ${MAKEFILE}
+    mv raxmlHPC* "$PREFIX"/bin
     make -f ${MAKEFILE} clean
   done
 done
