fc9aef64b4cc79015f8ed9a600e22801c05c384d
Colin J. Brislawn <cbrisl@gmail.com>
2024-07-10 21:15:14 -0400

    Update build.sh

--- recipes/vsearch/build.sh
+++ recipes/vsearch/build.sh
@@ -5,6 +5,8 @@ sed -i.bak 's/ *CX\?X\?FLAGS/#\0/p' configure.ac
 # Remove MACOS_DEPLOYMENT_TARGET override
 sed -i.bak 's/MACOSX_DEPLOYMENT_TARGET=/#\0/' configure.ac
 sed -i.bak 's/export MACOSX_DEPLOYMENT_TARGET=/#\0/' src/Makefile.am
+# Set PATH to prioritize conda-installed m4 (for osx)
+export PATH="$PREFIX/bin:$PATH" 
 
 ./autogen.sh
 ./configure --prefix=$PREFIX
