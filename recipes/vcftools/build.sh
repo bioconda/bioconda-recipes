d99b66dedd99cbee891ab2da9837cf4f5c42d66a
Milot Mirdita <milot@mirdita.de>
2024-07-11 22:02:49 +0900

    Try C++11

--- recipes/vcftools/build.sh
+++ recipes/vcftools/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -euo pipefail
+
+# uses random_shuffle which was removed in C++17
+export CXXFLAGS="-std=c++11 ${CXXFLAGS}"
+
 ./autogen.sh
 ./configure --prefix=$PREFIX
 #sed -i.bak -e 's/SUBDIRS = cpp perl/SUBDIRS = cpp/' ./src/Makefile
