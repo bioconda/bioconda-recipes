a5eb5cfa991219b0b9972d72ec84864b6a6de89b
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-12 22:10:33 +0200

    adapt chmod

--- recipes/palikiss/build.sh
+++ recipes/palikiss/build.sh
@@ -2,4 +2,4 @@
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pAliKiss all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pAliKiss install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
-chmod +x $PREFIX/bin/RNA*
+chmod +x $PREFIX/bin/pAliKiss*
--- recipes/rnaalishapes/build.sh
+++ recipes/rnaalishapes/build.sh
@@ -2,4 +2,4 @@
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
-chmod +x $PREFIX/bin/RNA*
+chmod +x $PREFIX/bin/RNAali*
