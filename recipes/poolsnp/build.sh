79e2a47433b76d7461d18879cebb56303e90cf07
Abhilesh Dhawanjewar <abhilesh7@gmail.com>
2024-07-12 11:40:32 +0100

    Include test directory

--- recipes/poolsnp/build.sh
+++ recipes/poolsnp/build.sh
@@ -4,15 +4,28 @@ set -ex
 
 BASE_DIR=$(dirname "$0")
 SCRIPTS_DIR="$BASE_DIR/scripts"
+TEST_DATA_DIR="$BASE_DIR/TestData"
 
+# Check for scripts directory
 if [ ! -d "$SCRIPTS_DIR" ]; then
     echo "The scripts directory is missing. It should be placed in the same directory as the PoolSNP.sh script."
     exit 1
 fi
 
+# Check for TestData directory
+if [ ! -d "$TEST_DATA_DIR" ]; then
+    echo "The TestData directory is missing. It should be placed in the same directory as the PoolSNP.sh script."
+    exit 1
+fi
+
 chmod +x PoolSNP.sh
+
+mkdir -p $PREFIX/bin
 
 cp PoolSNP.sh $PREFIX/bin
 cp -r $SCRIPTS_DIR $PREFIX/bin
+
+# Copy the test data to the share directory
+cp -r $TEST_DATA_DIR $PREFIX/share/PoolSNP/TestData
 
 chmod +x $PREFIX/bin/PoolSNP.sh
