660cac10d2938950c512946eaf5ba7ba992a4d2f
Carl M. Kobel <carl.mathias.kobel@nmbu.no>
2024-07-12 16:45:16 +0200

    v2.8.2

--- recipes/comparem2/build.sh
+++ recipes/comparem2/build.sh
@@ -13,3 +13,6 @@ cp -rf config docs dynamic_report profile resources tests workflow "${PACKAGE_HO
 # Make links
 ln -sf ${PACKAGE_HOME}/comparem2 ${PREFIX}/bin/comparem2
 ln -sf ${PACKAGE_HOME}/comparem2 ${PREFIX}/bin/asscom2 # Legacy
+
+# In order to enable the passthrough parameters, hyphens must be allowed. With this oneliner, the regex pattern is modified from [a-zA-Z_]\w*$ to [a-zA-Z_-][\w-]*$ in snakemake's parse_config().
+sed -i 's|valid = re.compile(r"\[a-zA-Z_\]\\w\*\$")|valid = re.compile(r"\[a-zA-Z_-\]\[\\w-\]\*\$")|g' $PREFIX/lib/python3.11/site-packages/snakemake/__init__.py
