2c9d45c8d1b4e6c1c4f0526d7940dd7ebaa5755c
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-12 18:18:08 +0200

    defer recipes that need a compiler at runtime

--- recipes/acms/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/aCMs all
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/aCMs install-program
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
-chmod +x $PREFIX/bin/RNA*
--- recipes/rapidshapes/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RapidShapes all
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RapidShapes install-program
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
-chmod +x $PREFIX/bin/RNA*
