344d6cb58874872ec39ce8c07066b784c1facff8
Abhilesh Dhawanjewar <abhilesh7@gmail.com>
2024-07-12 14:56:46 +0100

    Fix path specifications

--- recipes/poolsnp/build.sh
+++ recipes/poolsnp/build.sh
@@ -6,6 +6,7 @@ BASE_DIR=$(dirname "$0")
 SCRIPTS_DIR="$BASE_DIR/scripts"
 TEST_DATA_DIR="$BASE_DIR/TestData"
 
+
 # Check for scripts directory
 if [ ! -d "$SCRIPTS_DIR" ]; then
     echo "The scripts directory is missing. It should be placed in the same directory as the PoolSNP.sh script."
@@ -18,14 +19,27 @@ if [ ! -d "$TEST_DATA_DIR" ]; then
     exit 1
 fi
 
-chmod +x PoolSNP.sh
 
 mkdir -p $PREFIX/bin
 
-cp PoolSNP.sh $PREFIX/bin
-cp -r $SCRIPTS_DIR $PREFIX/bin
+mkdir -p $PREFIX/bin/scripts
 
 mkdir -p $PREFIX/share/PoolSNP
 
+
+cp PoolSNP.sh $PREFIX/bin
+
+# Copy python scripts
+for script in $SCRIPTS_DIR/*; do
+    if [[ $script == *.py ]]; then
+        cp $script $PREFIX/bin/$(basename $script)
+    else
+        cp $script $PREFIX/bin/scripts/
+    fi
+done
+
 # Copy the test data to the share directory
 cp -r $TEST_DATA_DIR $PREFIX/share/PoolSNP/TestData
+
+chmod +x PoolSNP.sh
+chmod +x $PREFIX/share/PoolSNP/TestData/test.sh
