7f6cdc93009f05fd29288b5deadfe6392a8a8b37
mencian <joshua.zhuang@yahoo.com>
2024-07-12 12:04:22 -0500

    add run_exports

--- recipes/pkiss/build.sh
+++ recipes/pkiss/build.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash -euo
+
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pKiss all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pKiss install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
-chmod +x $PREFIX/bin/pKiss* $PREFIX/bin/addRNA*
+chmod 755 $PREFIX/bin/pKiss* $PREFIX/bin/addRNA*
