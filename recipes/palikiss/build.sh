8b2cfd63ce59262902f03e16493d010b66b677fb
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-15 12:42:45 +0200

    integrating suggestions of @martin-g

--- recipes/palikiss/build.sh
+++ recipes/palikiss/build.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pAliKiss all
+#!/bin/bash
+make -j ${CPU_COUNT} PREFIX=$PREFIX CC=$CC -C Misc/Applications/pAliKiss all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/pAliKiss install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
 chmod +x $PREFIX/bin/pAliKiss*
