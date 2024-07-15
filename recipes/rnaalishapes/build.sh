672b78a91a9c154bdd7719ec137f8834b9c82abe
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-15 20:10:39 +0200

    update upstream + fix test call

--- recipes/rnaalishapes/build.sh
+++ recipes/rnaalishapes/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes all
+make -j ${CPU_COUNT} PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
 chmod +x $PREFIX/bin/RNAali*
