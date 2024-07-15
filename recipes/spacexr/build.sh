e41bf65c37fcfb2565a953841b609e9c4146f6e1
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2024-07-15 22:32:44 +0200

    update build.sh

--- recipes/spacexr/build.sh
+++ recipes/spacexr/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 R -e "library(devtools)"
 R -e "options(timeout = 600000000) ### set this to avoid timeout error"
-R -e "devtools::install_github("dmcable/spacexr", build_vignettes = FALSE)"
+R -e "devtools::install_github('dmcable/spacexr', ref = '0a0861e3d1e16014a20e9b743d0e19d3b42231f3', build_vignettes = FALSE)"
 R CMD INSTALL --build .
