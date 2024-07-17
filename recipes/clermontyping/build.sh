15c351bc9abcb9abf049c8aad22b4b516cc0133b
Robert A. Petit III <robbie.petit@gmail.com>
2024-07-17 16:07:45 -0600

    fix typo

--- recipes/clermontyping/build.sh
+++ recipes/clermontyping/build.sh
@@ -8,7 +8,7 @@ mkdir -p ${PREFIX}/bin ${CLERMONTYPING}
 # Use the share folder of R scripts and reference data
 mv ${SRC_DIR}/bin ${CLERMONTYPING}
 mv ${SRC_DIR}/data ${CLERMONTYPING}
-chmod 755 ${PREFIX}/bin/*
+chmod 755 ${CLERMONTYPING}/bin/*
 
 # Edit Wrapper to use Share Folder
 sed -i 's,^MY_PATH=$(dirname "$0"),MY_PATH=$CLERMONTYPING,' $SRC_DIR/clermonTyping.sh
