2d06562233c37ebfcd4db49649b504a778448479
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-07-17 10:53:34 -0700

    fix

--- recipes/gia/build.sh
+++ recipes/gia/build.sh
@@ -14,12 +14,6 @@ export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 HOME=$(pwd)
 pushd ${PREFIX}
 
-# remove cpu specifc cargo flags
-pushd ${HOME}
-sed 's/native/generic/g' .cargo/config.toml >tmp
-mv tmp .cargo/config.toml
-popd
-
 cargo install --no-track --verbose \
     --root "${PREFIX}" --path "${HOME}"
 popd
