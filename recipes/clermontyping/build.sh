01d7c4d117bda990c2ef9ad6cffdcecdafe08dea
Robert A. Petit III <robbie.petit@gmail.com>
2024-07-17 15:48:56 -0600

    add missing data for clermontyping

--- recipes/clermontyping/build.sh
+++ recipes/clermontyping/build.sh
@@ -2,7 +2,17 @@
 
 set -xe
 
-mkdir -p ${PREFIX}/bin
+CLERMONTYPING="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p ${PREFIX}/bin ${CLERMONTYPING}
+
+# Use the share folder of R scripts and reference data
+mv ${SRC_DIR}/bin ${CLERMONTYPING}
+mv ${SRC_DIR}/data ${CLERMONTYPING}
+chmod 755 ${PREFIX}/bin/*
+
+# Edit Wrapper to use Share Folder
+sed -i 's,^MY_PATH=$(dirname "$0"),MY_PATH=$CLERMONTYPING,' $SRC_DIR/clermonTyping.sh
+
+# Copy wrapper into bin
 cp $SRC_DIR/clermonTyping.sh ${PREFIX}/bin/
-mkdir -p ${PREFIX}/bin/bin
-cp $SRC_DIR/bin/* ${PREFIX}/bin/bin
+chmod 755 ${PREFIX}/bin/clermonTyping.sh
