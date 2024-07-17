d735b86d3426088244b03c3c7c4ad07315160e97
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-17 12:59:40 +0200

    update recipe

--- recipes/rnaalishapes/build.sh
+++ recipes/rnaalishapes/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 make -j ${CPU_COUNT} PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RNAalishapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
