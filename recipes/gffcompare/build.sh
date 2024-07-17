3ed33f4bc17e5138929469248a1cf1ba018d2877
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-17 16:26:14 +0300

    Make gcc available for GCLIB's build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gffcompare/build.sh
+++ recipes/gffcompare/build.sh
@@ -5,7 +5,13 @@ set -xe
 export CXX="$CXX"
 export LINKER="$CXX"
 
+# GCLIB needs gcc to detect its version
+# GCLIB itself is git-cloned by gffcompare's Makefile
+ln -s ${CC} $BUILD_PREFIX/bin/gcc
+
 mkdir -p "$PREFIX"/bin/
 make -j ${CPU_COUNT} release
 cp gffcompare "$PREFIX"/bin/
 cp trmap "$PREFIX"/bin/
+
+unlink $BUILD_PREFIX/bin/gcc
