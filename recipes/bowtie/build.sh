3f76ecfb366ad8402ecaf24f2486f23bb26a152c
Akihiro Kuno <akikuno@users.noreply.github.com>
2024-07-17 15:58:40 +0900

    Add a comment of why POPCNT_CAPABILITY needs to be disabled for arm64 only

--- recipes/bowtie/build.sh
+++ recipes/bowtie/build.sh
@@ -2,9 +2,13 @@
 
 set -xe
 
+# If the environment is arm64 architecture, set POPCNT_CAPABILITY to 0 and run make.
+# This is because the POPCNT instruction is not supported on arm64 architecture.
+# By setting POPCNT_CAPABILITY=0, this instruction is disabled, ensuring build compatibility.
 if [ "$(uname -m)" == "arm64" ]; then
     make POPCNT_CAPABILITY=0 -j ${CPU_COUNT}
 fi
+
 
 make prefix="${PREFIX}"  -j ${CPU_COUNT} install
 
