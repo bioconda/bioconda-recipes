b092f02e3d0a2dc432be4a267390031b830d5b2f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-17 15:56:13 +0300

    Pass PLATFORM=arm8 for aarch64/arm64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/famsa/build.sh
+++ recipes/famsa/build.sh
@@ -2,6 +2,15 @@
 
 set -xe
 
-make famsa -j${CPU_COUNT}
+case $(uname -m) in
+    aarch64 | arm64)
+        ARCH_OPTS="PLATFORM=arm8"
+        ;;
+    *)
+        ARCH_OPTS=""
+        ;;
+esac
+
+make famsa ${ARCH_OPTS} -j${CPU_COUNT}
 install -d "${PREFIX}/bin"
 install famsa "${PREFIX}/bin"
