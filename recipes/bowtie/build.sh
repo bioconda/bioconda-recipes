1a03cb480a9345461461da83167c8f1452905d05
Martin Grigorov <martin-g@users.noreply.github.com>
2024-07-17 09:46:29 +0300

    Use parallel build with `make`

--- recipes/bowtie/build.sh
+++ recipes/bowtie/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -xe
+
 if [ "$(uname -m)" == "arm64" ]; then
-    make POPCNT_CAPABILITY=0
+    make POPCNT_CAPABILITY=0 -j ${CPU_COUNT}
 fi
 
-make prefix="${PREFIX}" install
+make prefix="${PREFIX}"  -j ${CPU_COUNT} install
 
 cp -r scripts "${PREFIX}/bin/"
