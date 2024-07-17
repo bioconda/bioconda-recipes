c7be8446d55a48647e5e95c3f4a20a09aa2d3db5
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-17 09:02:48 +0200

    Update recipes/bowtie/build.sh

--- recipes/bowtie/build.sh
+++ recipes/bowtie/build.sh
@@ -5,6 +5,7 @@ set -xe
 # If the environment is arm64 architecture, set POPCNT_CAPABILITY to 0 and run make.
 # This is because the POPCNT instruction is not supported on arm64 architecture.
 # By setting POPCNT_CAPABILITY=0, this instruction is disabled, ensuring build compatibility.
+# See https://github.com/BenLangmead/bowtie/blob/master/processor_support.h
 if [ "$(uname -m)" == "arm64" ]; then
     make POPCNT_CAPABILITY=0 -j ${CPU_COUNT}
 fi
