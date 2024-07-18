04c6d17fea6413b614187de4efebd1de60033fc2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-07-18 12:42:50 +0300

    Remove CC. It is no more needed since GCLIB's Makefile is not used
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/gffcompare/build.sh
+++ recipes/gffcompare/build.sh
@@ -5,13 +5,7 @@ set -xe
 export CXX="$CXX"
 export LINKER="$CXX"
 
-# GCLIB needs gcc to detect its version
-# GCLIB itself is git-cloned by gffcompare's Makefile
-ln -s ${CC} $BUILD_PREFIX/bin/gcc
-
 mkdir -p "$PREFIX"/bin/
 make release
 cp gffcompare "$PREFIX"/bin/
 cp trmap "$PREFIX"/bin/
-
-unlink $BUILD_PREFIX/bin/gcc
