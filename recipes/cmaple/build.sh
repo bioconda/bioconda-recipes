5eb2c0a5f1a038c4958e0b7e020077b24e93189a
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-18 23:59:27 +0200

    fix test

--- recipes/cmaple/build.sh
+++ recipes/cmaple/build.sh
@@ -9,7 +9,6 @@ if [ "$(uname)" == Darwin ]; then
 	RANLIB=$(which "$RANLIB")
 	DCMAKE_ARGS=(-DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}"
 		-DCMAKE_CXX_COMPILER_AR="${AR}" -DCMAKE_CXX_COMPILER_RANLIB="${RANLIB}")
-
 fi
 
 case $(uname -m) in
@@ -20,7 +19,6 @@ aarch64)
 	JOBS=${CPU_COUNT}
 	;;
 esac
-JOBS=1 # Simplify logs
 
 mkdir build
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -G Ninja \
@@ -30,5 +28,10 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -G Ninja \
 
 VERBOSE=1 cmake --build build --target install -j ${JOBS}
 
-chmod 755 "${PREFIX}/bin/cmaple"
-chmod 755 "${PREFIX}/bin/cmaple-aa"
+chmod 755 "${PREFIX}/bin/cmaple"*
+
+for file in "${PREFIX}/example.maple" "${PREFIX}/tree.nwk"; do
+    if [ -f "$file" ]; then
+        rm "$file"
+    fi
+done
