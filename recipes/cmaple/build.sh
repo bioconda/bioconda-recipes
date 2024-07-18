667f7c2ad49cfdbff7d179306bf1fb81badbe297
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-18 20:31:29 +0200

    Try simple

--- recipes/cmaple/build.sh
+++ recipes/cmaple/build.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 set -ex
 
-export INCLUDES="-I${PREFIX}/include"
-export LIBPATH="-L${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -I${PREFIX}/include"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
-
 # if [ "$(uname)" == Darwin ]; then
 # 	export CMAKE_C_COMPILER="clang"
 # 	export CMAKE_CXX_COMPILER="clang++"
 # fi
-
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_C_FLAGS="${CFLAGS}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-DBUILD_GMOCK=OFF -DINSTALL_GTEST=OFF \
-	-Wno-dev -Wno-deprecated --no-warn-unused-cli
 
 case $(uname -m) in
 aarch64)
@@ -28,7 +15,14 @@ aarch64)
 	;;
 esac
 
-cmake --build build --target install -j "${JOBS}"
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DBUILD_GMOCK=OFF -DINSTALL_GTEST=OFF \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli
+make -j "${JOBS}"
+make install
 
 chmod 755 "${PREFIX}/bin/cmaple"
 chmod 755 "${PREFIX}/bin/cmaple-aa"
