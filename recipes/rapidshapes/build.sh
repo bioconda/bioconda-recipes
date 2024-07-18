be09e12dc0fd459fb47c8ca0718bec6e11f43d22
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-18 17:25:44 +0200

    trigger action

--- recipes/rapidshapes/build.sh
+++ recipes/rapidshapes/build.sh
@@ -4,3 +4,4 @@ make -j ${CPU_COUNT} PREFIX=$PREFIX CC=$CC -C Misc/Applications/RapidShapes all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RapidShapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
 chmod 755 $PREFIX/bin/RapidShapes*
+
