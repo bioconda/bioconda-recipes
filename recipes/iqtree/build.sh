2a7e5c9563ac7f38f4b15031be31d353ada4c09e
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-19 00:59:43 +0200

    Let's see

--- recipes/iqtree/build.sh
+++ recipes/iqtree/build.sh
@@ -12,11 +12,17 @@ if [ "$(uname)" == Darwin ]; then
 	export CMAKE_C_COMPILER="clang"
 	export CMAKE_CXX_COMPILER="clang++"
 	export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=11"
+	CC=$(which "$CC")
+	CXX=$(which "$CXX")
+	AR=$(which "$AR")
+	RANLIB=$(which "$RANLIB")
+	DCMAKE_ARGS=(-DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}"
+		-DCMAKE_CXX_COMPILER_AR="${AR}" -DCMAKE_CXX_COMPILER_RANLIB="${RANLIB}")
 fi
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
-	-DUSE_LSD2=ON -DIQTREE_FLAGS=omp -DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DUSE_CMAPLE=ON \
+	-DUSE_LSD2=ON -DIQTREE_FLAGS=omp -DUSE_CMAPLE=ON \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" "${DCMAKE_ARGS[@]}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli
 
 case $(uname -m) in
@@ -28,7 +34,7 @@ case $(uname -m) in
 		;;
 esac
 
-cmake --build build --target install -j 1
+VERBOSE=1 cmake --build build --target install -j "${JOBS}"
 
 chmod 755 "${PREFIX}/bin/iqtree2"
 cp -f "${PREFIX}"/bin/iqtree2 "${PREFIX}"/bin/iqtree
