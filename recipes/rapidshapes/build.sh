a63c5fd4c2153b788ea6aef025309938e2bfd0ad
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-18 10:50:25 +0200

    use pre-release

--- recipes/rapidshapes/build.sh
+++ recipes/rapidshapes/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# seems like BusyBox v1.36.1's readlink does not have a -m option 
-sed -e "s/ -m \$filename/ -f \$filename/" Misc/Applications/lib/foldGrammars/Utils.pm
-
 make -j ${CPU_COUNT} PREFIX=$PREFIX CC=$CC -C Misc/Applications/RapidShapes all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/RapidShapes install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
