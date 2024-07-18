dd7a8e94123b657048140385f899466c98696817
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-07-18 10:40:27 +0200

    using pre-release

--- recipes/acms/build.sh
+++ recipes/acms/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# flag glasgow-ext is no longer supported in OSX
-sed -e "s/ -fglasgow-exts / /g" Misc/Applications/aCMs  Misc/Applications/makefile
-
 make -j ${CPU_COUNT} PREFIX=$PREFIX CC=$CC -C Misc/Applications/aCMs all
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/aCMs install-program
 make PREFIX=$PREFIX CC=$CC -C Misc/Applications/lib install
