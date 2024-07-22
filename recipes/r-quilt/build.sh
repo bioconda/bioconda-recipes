96b4b53293655affecfa2106c74e2a84791c4f99
mencian <joshua.zhuang@yahoo.com>
2024-07-22 10:36:47 -0500

    add r-RcppEigen

--- recipes/r-quilt/build.sh
+++ recipes/r-quilt/build.sh
@@ -3,8 +3,9 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-cp $SRC_DIR/QUILT.R $PREFIX/bin/
+mkdir -p ${PREFIX}/bin
+
+cp -rf $SRC_DIR/QUILT.R $PREFIX/bin/
 cd $SRC_DIR/QUILT
 
-$R CMD INSTALL --build --install-tests .
-
+$R CMD INSTALL --build --install-tests . ${R_ARGS}
