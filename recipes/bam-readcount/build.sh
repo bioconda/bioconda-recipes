9fc3c67000e735806d5f30062e5a2ccbf6fef485
mencian <joshua.zhuang@yahoo.com>
2024-07-23 23:41:13 -0500

    clean up recipe

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -10,11 +10,22 @@ mkdir -p "${PREFIX}/bin"
 export INCLUDES="-I{PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+
+if [ `uname` == Darwin ]; then
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -lz -lbz2"
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk for -D_LIBCPP_DISABLE_AVAILABILITY
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
 
 mkdir -p build
 pushd build
-cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -Wno-dev -Wno-deprecated --no-warn-unused-cli
-make CXX="${CXX} ${LDFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" -j "${CPU_COUNT}"
+cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli
+
+make CXX="${CXX} ${LDFLAGS}" CXXFLAGS="${CXXFLAGS}" -j "${CPU_COUNT}"
 
 chmod 755 bin/bam-readcount
 cp -f bin/bam-readcount "${PREFIX}/bin"
