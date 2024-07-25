6b1c5523d85fb2ead467f6a9a267729385ad264f
lcoombe <lauren.e.coombe@gmail.com>
2024-07-25 09:22:06 -0700

    Add to CXX flags

--- recipes/ntcard/build.sh
+++ recipes/ntcard/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include -Wno-array-bounds"
+export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
+export CXXFLAGS="$CXXFLAGS -Wno-array-bounds"
 
 export M4="$BUILD_PREFIX/bin/m4"
 
