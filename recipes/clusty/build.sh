755b3cc1b8cab0b562e41943825b78eb1596ebf6
agudys <adam.gudys@gmail.com>
2024-07-26 14:30:56 +0200

    Include Leiden only in Linux package

--- recipes/clusty/build.sh
+++ recipes/clusty/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
+
+ifeq ($(uname_S),Darwin)
+make -j${CPU_COUNT}
+else
 make -j${CPU_COUNT} LEIDEN=true
+endif
+
 install -d "${PREFIX}/bin"
 install clusty "${PREFIX}/bin"
