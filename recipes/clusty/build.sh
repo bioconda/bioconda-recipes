c02cac45d9b966a90da680a2f74ca26736b90bf2
agudys <adam.gudys@gmail.com>
2024-07-26 15:00:28 +0200

    fix

--- recipes/clusty/build.sh
+++ recipes/clusty/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
+uname_S=`uname -s 2>/dev/null || echo not`
 
-ifeq ($(uname_S),Darwin)
-make -j${CPU_COUNT}
-else
-make -j${CPU_COUNT} LEIDEN=true
-endif
+if [ "$uname_S" == "Darwin" ]; then make -j${CPU_COUNT}; else make -j${CPU_COUNT} LEIDEN=true; fi
 
 install -d "${PREFIX}/bin"
 install clusty "${PREFIX}/bin"
