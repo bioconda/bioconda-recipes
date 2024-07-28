2ea0a2f69730d7538745f00cc8f69e91007d7324
agudys <adam.gudys@gmail.com>
2024-07-28 23:01:23 +0200

    Leiden disabled under macOS.

--- recipes/clusty/build.sh
+++ recipes/clusty/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ln -s ${CC} gcc
-ln -s ${CXX} g++
-export PATH=$PATH:$(pwd)
+#ln -s ${CC} gcc
+#ln -s ${CXX} g++
+#export PATH=$PATH:$(pwd)
 uname_S=`uname -s 2>/dev/null || echo not`
 
-if [ "$uname_S" == "Darwin" ]; then make -j${CPU_COUNT} LEIDEN=true; else make -j${CPU_COUNT} LEIDEN=true; fi
+if [ "$uname_S" == "Darwin" ]; then make -j${CPU_COUNT}; else make -j${CPU_COUNT} LEIDEN=true; fi
 
 install -d "${PREFIX}/bin"
 install clusty "${PREFIX}/bin"
