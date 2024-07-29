b83763d223c11e60cd37e3b4671c289514ac7e96
UENO, M. <eunosium-1128@live.jp>
2024-07-29 18:41:17 +0900

    Update build.sh

--- recipes/reduce/build.sh
+++ recipes/reduce/build.sh
@@ -2,6 +2,14 @@
 
 mkdir -p build
 cd build
-cmake ${SRC_DIR} -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+cmake ${SRC_DIR} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DBUILD_SHARED_LIBS=ON \
+    -DENABLE_TESTS=OFF \
+    -DCMAKE_CXX_STANDARD=14 \
+    -DCMAKE_VERBOSE_MAKEFILE=ON
+
 make
 make install
