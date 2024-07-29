5f0e5be52c05db0ed62f83b88d0da8f766e17d8b
Thom Griffioen <ebete@users.noreply.github.com>
2024-07-29 13:30:37 +0200

    MacOS debugging run

--- recipes/rpsbproc/build.sh
+++ recipes/rpsbproc/build.sh
@@ -6,18 +6,36 @@ set -o nounset
 set -o pipefail
 
 
-# source paths
+# For debugging ./configure
+cat << EOF >&2
+ENVIRONMENT
+----------
+uname -a   $(uname -a)
+ KERNEL
+uname -s   $(uname -s)
+uname -r   $(uname -r)
+uname -v   $(uname -v)
+ HARDWARE
+uname -m   $(uname -m)
+ OS
+uname -o   $(uname -o)
+EOF
+
+
+# Source paths
 NCBI_CXX_TOOLKIT="$SRC_DIR/ncbi_cxx/c++"
 RPSBPROC_SRC="$SRC_DIR/rpsbproc"
+# Work directory
+RESULT_PATH="$NCBI_CXX_TOOLKIT/Release"
 
 
 # C/C++ preprocessor header includes paths
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 # Linker library paths
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+# C++ compiler flags
 if [[ "$(uname)" = "Darwin" ]]; then
 	# See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk for -D_LIBCPP_DISABLE_AVAILABILITY
-	# C++ compiler flags
 	export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
@@ -26,14 +44,14 @@ fi
 mkdir "$NCBI_CXX_TOOLKIT/src/app/RpsbProc"
 cp -rf "$RPSBPROC_SRC/src/"* "$NCBI_CXX_TOOLKIT/src/app/RpsbProc/"
 
+
 # Configuration synopsis:
 # https://ncbi.github.io/cxx-toolkit/pages/ch_config.html#ch_config.ch_configget_synopsi
 # Run `./configure --help` for all flags.
+CONFIGURE_FLAGS="--with-build-root=$RESULT_PATH"
 
-# platform-independent flags
-# Description of used options (from ./configure --help):
+# Platform-independent flags
 ## BUILD CHAIN OPTIONS
-CONFIGURE_FLAGS=""
 # --with(out)-bin-release:
 #   Build executables suitable for public release
 CONFIGURE_FLAGS="$CONFIGURE_FLAGS --with-bin-release"
@@ -44,9 +62,16 @@ CONFIGURE_FLAGS="$CONFIGURE_FLAGS --without-debug"
 # --with(out)-strip:
 #   Strip binaries at build time (remove debugging symbols)
 CONFIGURE_FLAGS="$CONFIGURE_FLAGS --with-strip"
-# --with(out)-with-experimental:
-#   Enable named experimental feature:
-#     Int4GI (Use a simple 32-bit type for GI numbers).
+# --with-experimental={ChaosMonkey,Int4GI,Int8GI,StrictGI,PSGLoader,BM64,C++20,C2X}:
+#   Enable named experimental feature (comma-separated list):
+#   - ChaosMonkey  Enable "ChaosMonkey" failure testing.
+#   - Int4GI       Use a simple 32-bit type for GI numbers.
+#   - Int8GI       Use a simple 64-bit type for GI numbers.
+#   - StrictGI     Use a strict 64-bit type for GI numbers.
+#   - PSGLoader    Let the GenBank date loader use PubSeq Gateway (PSG).
+#   - BM64         Use 64-bit bitset indices.
+#   - C++20        Use '-std=gnu++20' compiler flag.
+#   - C2X          Use '-std=gnu2x' compiler flag.
 #   See c++/src/build-system/configure.ac lines 1020:1068 for the named options.
 CONFIGURE_FLAGS="$CONFIGURE_FLAGS --with-experimental=Int4GI"
 # --with(out)-mt:
@@ -96,10 +121,10 @@ CONFIGURE_FLAGS="$CONFIGURE_FLAGS --without-boost"
 # --with(out)-pcre:
 #   Do not use pcre (Perl regex).
 CONFIGURE_FLAGS="$CONFIGURE_FLAGS --without-pcre"
-# static linking
+# Static linking of libraries
 CONFIGURE_FLAGS="$CONFIGURE_FLAGS --without-dll --with-static-exe"
 
-# platform-specific flags
+# Platform-specific flags
 if [[ "$(uname)" = "Linux" ]]; then
 	# --with(out)-64:
 	#   Compile in 64-bit mode instead of 32-bit.
@@ -124,20 +149,19 @@ fi
 # Fixes building on unix (linux and osx)
 export AR="${AR} rcs"
 
+# Run configure script
 cd "$NCBI_CXX_TOOLKIT"
-./configure $CONFIGURE_FLAGS
+./configure.orig $CONFIGURE_FLAGS
 
-cd ReleaseMT/build
+# Run GNU Make
+cd "$RESULT_PATH/build"
 echo "RUNNING MAKE"
 make -j1 -f Makefile.flat rpsbproc.exe
 
+# Copy compiled binaries to the Conda $PREFIX
+mkdir -p "$PREFIX/bin"
+chmod +x "$RESULT_PATH/bin/"*
+cp "$RESULT_PATH/bin/rpsbproc"* "$PREFIX/bin/"
 
-mkdir -p $PREFIX/bin
-chmod +x $NCBI_CXX_TOOLKIT/ReleaseMT/bin/*
-
-
-cp $NCBI_CXX_TOOLKIT/ReleaseMT/bin/rpsbproc* $PREFIX/bin/
-
-
-#extra log to check all exe are present
-ls -s $PREFIX/bin/
+# Extra log to check results
+ls -lhAF "$PREFIX/bin/"
