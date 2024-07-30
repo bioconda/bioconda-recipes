e8df3568b298ae9a90e61d9a2be01cfa59ede615
Stephan Schiffels <stephan_schiffels@mac.com>
2024-07-30 17:36:02 +0200

    commented build script

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# Download GHC
 wget https://downloads.haskell.org/~ghc/9.4.7/ghc-9.4.7-x86_64-centos7-linux.tar.xz
 tar xf ghc-9.4.7-x86_64-centos7-linux.tar.xz
 
+# Compile GHC
 cd ghc-9.4.7-x86_64-unknown-linux
-echo "### CONFIGURING GHC compilation"
 ./configure --prefix=$PREFIX
-
-echo "### MAKE INSTALL GHC"
 make install
 
+# Run Stack
 stack install --local-bin-path ${PREFIX}/bin --system-ghc --no-install-ghc
