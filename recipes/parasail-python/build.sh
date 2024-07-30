b3de947ea31483ad296473b22b05092a82569969
mencian <joshua.zhuang@yahoo.com>
2024-07-30 03:22:56 -0500

    edit build.sh

--- recipes/parasail-python/build.sh
+++ recipes/parasail-python/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 export M4="${BUILD_PREFIX}/bin/m4"
 
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
