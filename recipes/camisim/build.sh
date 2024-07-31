b149722a8960d5e995988c8f6e4ddb0a372fac80
paulzierep <paul.zierep@googlemail.com>
2024-07-31 15:08:02 +0200

    use same scripts as in docker

--- recipes/camisim/build.sh
+++ recipes/camisim/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -xe 
+
 mkdir -p  $PREFIX/bin
-cp -R * $PREFIX/bin
+
+cp *.py $PREFIX/bin/
+cp scripts $PREFIX/bin/scripts
+cp tools $PREFIX/bin/tools
+cp defaults $PREFIX/bin/defaults
 
 
