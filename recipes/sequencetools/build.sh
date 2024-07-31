067f38ce9399c787b698eecb0ffe178d8cace8fa
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2024-07-31 14:57:06 +0200

    further fixed build script

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -3,22 +3,26 @@
 GHC_VERSION=9.4.7
 
 echo "Build Platform"
-echo $build_platform
+echo $build_platform # can be "linux-64, osx-64, osx-arm64"
 
 # Download GHC
-if [ "$build_platform" == "linux-64"]; then
-    GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-centos7-linux.tar.xz
-elif [ "$build_platform" == "osx-64"]; then
-    GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-apple-darwin.tar.xz
+if [ "$build_platform" == "linux-64" ]; then
+    GHC_TAR=ghc-$GHC_VERSION-x86_64-centos7-linux.tar.xz
+elif [ "$build_platform" == "osx-64" ]; then
+    GHC_TAR=ghc-$GHC_VERSION-x86_64-apple-darwin.tar.xz
+elif [ "$build_platform" == "osx-arm64" ]; then
+    GHC_TAR=ghc-$GHC_VERSION-aarch64-apple-darwin.tar.xz    
 fi
+
+GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/$GHC_TAR
 echo "Downloading $GHC_URL"
 wget $GHC_URL
 
-echo "Unpacking $GHC_URL"
-tar xf ghc-$GHC_VERSION-src.tar.xz
+echo "Unpacking $GHC_TAR"
+tar xf $GHC_TAR
 
 # Compile GHC
-DIR=$(ls ghc-$GHC_VERSION*)
+DIR=$(find . -type d -name "ghc-$GHC_VERSION-*" -print -quit) # this just looks for the unpacked GHC directory
 echo "Compiling GHC in $DIR"
 cd $DIR
 echo "Configuring GHC"
