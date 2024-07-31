ca47624fbc3dd6340c3b63781f8c6dbbaca4cf03
Stephan Schiffels <stephan_schiffels@mac.com>
2024-07-31 14:17:55 +0200

    added platform selection

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -2,19 +2,23 @@
 
 GHC_VERSION=9.4.7
 
+echo "Build Platform"
+echo $build_platform
+
 # Download GHC
-GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-src.tar.xz
+if [ "$build_platform" == "linux-64"]; then
+    GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-centos7-linux.tar.xz
+elif [ "$build_platform" == "osx-64"]; then
+    GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-apple-darwin.tar.xz
+fi
 echo "Downloading $GHC_URL"
 wget $GHC_URL
 
 echo "Unpacking $GHC_URL"
 tar xf ghc-$GHC_VERSION-src.tar.xz
 
-echo "ls:"
-ls
-
 # Compile GHC
-DIR=ghc-$GHC_VERSION
+DIR=$(ls ghc-$GHC_VERSION*)
 echo "Compiling GHC in $DIR"
 cd $DIR
 echo "Configuring GHC"
