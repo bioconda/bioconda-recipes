7d3411d58e104c2634275e00d101ddf5401bb809
paulzierep <paul.zierep@googlemail.com>
2024-07-31 13:43:12 +0200

    copy all to bin

--- recipes/camisim/build.sh
+++ recipes/camisim/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-BINARY_HOME=$PREFIX/bin
-PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+cp -R * $PREFIX/bin
 
-mkdir -p $BINARY_HOME
-mkdir -p $PACKAGE_HOME
-
-cp -R * $PACKAGE_HOME
-
-ln -s $PACKAGE_HOME/bin/metagenome_from_profile.py $BINARY_HOME
-ln -s $PACKAGE_HOME/bin/metagenomesimulation.py $BINARY_HOME
 
