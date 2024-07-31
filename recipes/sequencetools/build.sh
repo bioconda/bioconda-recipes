f74967d3a8e192884d66b163b796234048c518cb
Stephan Schiffels <stephan_schiffels@mac.com>
2024-07-31 13:45:37 +0200

    more debug output

--- recipes/sequencetools/build.sh
+++ recipes/sequencetools/build.sh
@@ -3,13 +3,26 @@
 GHC_VERSION=9.4.7
 
 # Download GHC
-wget https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-src.tar.xz
+GHC_URL=https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-src.tar.xz
+echo "Downloading $GHC_URL"
+wget $GHC_URL
+
+echo "Unpacking $GHC_URL"
 tar xf ghc-$GHC_VERSION-src.tar.xz
 
+echo "ls:"
+ls
+
 # Compile GHC
-cd ghc-$GHC_VERSION
+DIR=ghc-$GHC_VERSION
+echo "Compiling GHC in $DIR"
+cd $DIR
+echo "Configuring GHC"
 ./configure --prefix=$PREFIX
+
+echo "building GHC"
 make install
 
 # Run Stack
+echo "Running Stack"
 stack install --local-bin-path ${PREFIX}/bin --system-ghc --no-install-ghc
