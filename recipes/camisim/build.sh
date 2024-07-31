763a3ed7f55e74c29e5dd5660eb6d90022249511
paulzierep <paul.zierep@googlemail.com>
2024-07-31 11:43:46 +0200

    add a script

--- recipes/camisim/build.sh
+++ recipes/camisim/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp metagenome_from_profile.py ${PREFIX}/bin
+cp metagenomesimulation.py ${PREFIX}/bin
 chmod 755 ${PREFIX}/bin/metagenome_from_profile.py
+chmod 755 ${PREFIX}/bin/metagenomesimulation.py
