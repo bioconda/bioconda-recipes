88925d2605ed8c850ce0cd808da53a9f0269283c
paulzierep <paul.zierep@googlemail.com>
2024-07-31 13:25:48 +0200

    copy complete package into share

--- recipes/camisim/build.sh
+++ recipes/camisim/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cp metagenome_from_profile.py ${PREFIX}/bin
-cp metagenomesimulation.py ${PREFIX}/bin
-chmod 755 ${PREFIX}/bin/metagenome_from_profile.py
-chmod 755 ${PREFIX}/bin/metagenomesimulation.py
+BINARY_HOME=$PREFIX/bin
+PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+mkdir -p $BINARY_HOME
+mkdir -p $PACKAGE_HOME
+
+cp -R * $PACKAGE_HOME
+
+ln -s $PACKAGE_HOME/bin/metagenome_from_profile.py $BINARY_HOME
+ln -s $PACKAGE_HOME/bin/metagenomesimulation.py $BINARY_HOME
+
