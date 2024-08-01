9a74a341c906e7ea8a4d73149b087f0a07a57a76
agudys <adam.gudys@gmail.com>
2024-08-01 14:48:20 +0200

    Update kmer-db

--- recipes/kmer-db/build.sh
+++ recipes/kmer-db/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+ln -s ${CC} gcc
+ln -s ${CXX} g++
+export PATH=$PATH:$(pwd)
 
 CFLAGS="$CFLAGS -I${PREFIX}/include"
 LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
 
-make CC="${CXX}" 
+make -j${CPU_COUNT} 
 install -d "${PREFIX}/bin"
 install  kmer-db "${PREFIX}/bin"
