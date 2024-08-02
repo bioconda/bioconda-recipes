cf4f8941f223aec1bd572389268c818af066f1ae
agudys <adam.gudys@gmail.com>
2024-08-03 00:00:42 +0200

    attempt to install g++ on macOS

--- recipes/lz-ani/build.sh
+++ recipes/lz-ani/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ "$uname_S" == "Darwin" ]; then 
+	brew install gcc-11
+	brew install g++-11 
+	CC=gcc-11
+	CXX=g++-11
+fi
+
 ln -s ${CC} gcc
 ln -s ${CXX} g++
 export PATH=$PATH:$(pwd)
