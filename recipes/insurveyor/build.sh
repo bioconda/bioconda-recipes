795578ed9be59966cc00c6874fe5a2598e20f7e5
mencian <joshua.zhuang@yahoo.com>
2024-08-03 22:39:37 -0500

    edit build.sh

--- recipes/insurveyor/build.sh
+++ recipes/insurveyor/build.sh
@@ -5,14 +5,6 @@ export CXXFLAGS="${CXXFLAGS} -O3 -I{PREFIX}/include ${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} reads_categorizer.cpp -o reads_categorizer -pthread -lhts
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} clip_consensus_builder.cpp -o clip_consensus_builder -pthread -lhts
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} libs/ssw.c libs/ssw_cpp.cpp call_insertions.cpp -o call_insertions -pthread -lhts
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} libs/ssw.c libs/ssw_cpp.cpp dc_remapper.cpp -o dc_remapper -pthread -lhts
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} add_filtering_info.cpp -o add_filtering_info -pthread -lhts
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} filter.cpp -o filter -pthread -lhts
-# ${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS} normalise.cpp -o normalise -pthread -lhts
-
 cmake -S . -B build \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
