3cb68455b62b5da1f65d883ea5d086fb887d2679
mencian <joshua.zhuang@yahoo.com>
2024-08-05 03:52:28 -0500

    edit build.sh

--- recipes/yak/build.sh
+++ recipes/yak/build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ${PREFIX}/bin
 
-make INCLUDES="-I${PREFIX}/include" CFLAGS="${CFLAGS} -g -Wall -O2 -L${PREFIX}/lib" -j "{CPU_COUNT}"
+make INCLUDES="-I${PREFIX}/include" CFLAGS="${CFLAGS} -g -Wall -O2 -L${PREFIX}/lib"
 cp -rf yak ${PREFIX}/bin
 chmod 755 ${PREFIX}/bin/yak
