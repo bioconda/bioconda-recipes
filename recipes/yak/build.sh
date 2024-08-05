8dffc57512f1b74f1755a9f321a9c113e7256802
mencian <joshua.zhuang@yahoo.com>
2024-08-05 03:47:58 -0500

    yak: add aarch64/arm64 build

--- recipes/yak/build.sh
+++ recipes/yak/build.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-mkdir -p $PREFIX/bin
+mkdir -p ${PREFIX}/bin
 
-make INCLUDES="-I$PREFIX/include" CFLAGS="-g -Wall -O2 -L$PREFIX/lib"
-cp yak $PREFIX/bin
+make INCLUDES="-I${PREFIX}/include" CFLAGS="${CFLAGS} -g -Wall -O2 -L${PREFIX}/lib" -j "{CPU_COUNT}"
+cp -rf yak ${PREFIX}/bin
+chmod 755 ${PREFIX}/bin/yak
