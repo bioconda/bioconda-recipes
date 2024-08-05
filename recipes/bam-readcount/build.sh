44f02dbb56ed8c6a12fa123d3ca8d664b6ff9657
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-08-05 01:43:36 -0500

    bam-readcount: build on OSX (#49790)
    * bam-readcount: build on OSX
    
    * remove osx-arm64

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -1,6 +1,4 @@
-#!/bin/bash -euo
-
-set -x
+#!/bin/bash -ex
 
 wget https://github.com/boostorg/boost/releases/download/boost-1.85.0/boost-1.85.0-cmake.tar.gz
 
@@ -14,11 +12,12 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
-if [ `uname` == Darwin ]; then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -lz -lbz2"
-    export MACOSX_DEPLOYMENT_TARGET=10.13
-    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk for -D_LIBCPP_DISABLE_AVAILABILITY
-    # export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+if [[ `uname` == Darwin ]]; then
+	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+	export CFLAGS="${CFLAGS} -Wno-unguarded-availability -Wdeprecated-non-prototype"
+	export CMAKE_EXTRA="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CMAKE_EXTRA=""
 fi
 
 mkdir -p build
@@ -28,10 +27,12 @@ cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-Wno-dev -Wno-deprecated --no-warn-unused-cli
+	-DCMAKE_C_FLAGS="${CFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	${CMAKE_EXTRA}
 
 make clean
-make CXX="${CXX} ${LDFLAGS}" CC="${CC}" CXXFLAGS="${CXXFLAGS}"
+make CXX="${CXX} ${LDFLAGS}" CC="${CC}" CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}"
 
 chmod 755 bin/bam-readcount
 cp -f bin/bam-readcount "${PREFIX}/bin"
