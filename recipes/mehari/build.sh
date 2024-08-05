cafe1c7f00f1c9eaa9f3a25732503b7b2f6b4615
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-08-05 15:41:18 +0300

    Try without verbose logging during install
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/mehari/build.sh
+++ recipes/mehari/build.sh
@@ -3,4 +3,4 @@
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 
-cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
+cargo install --no-track --locked --root "${PREFIX}" --path .
