73e7fe9aa8df59511c1a26c1161ae350c4569f80
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-08-06 11:18:51 +0300

    admixtools: Try with -fPIE
    Trying to solve:
    ```
    /opt/conda/conda-bld/admixtools_1722622340511/_build_env/bin/../lib/gcc/x86_64-conda-linux-gnu/12.4.0/../../../../x86_64-conda-linux-gnu/bin/ld: /opt/conda/conda-bld/admixtools_1722622340511/_build_env/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/gcrt1.o: relocation R_X86_64_32S against symbol `__libc_csu_fini' can not be used when making a PIE object; recompile with -fPIE
    ```
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/admixtools/build.sh
+++ recipes/admixtools/build.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -euo
+#!/bin/bash
+
+set -xeuo
 
 # clear out pre-built objects and executables
 cd src
-make CC="${CC}" -j "${CPU_COUNT}" clobber
+make CC="${CC}" CFLAGS="${CFLAGS} -fPIE" -j "${CPU_COUNT}" clobber
 
-make CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" all
+make CC="${CC}" CFLAGS="${CFLAGS} -fPIE" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" all
 
 make install TOP="${PREFIX}/bin"
