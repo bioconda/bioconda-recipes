a0d808bc22b7c02cfe335ad0e4af83b0f8cf8305
Lee Katz - Aspen <gzu2@cdc.gov>
2024-08-08 15:38:33 -0400

    adding third party seqan libs

--- recipes/khmer/build.sh
+++ recipes/khmer/build.sh
@@ -2,12 +2,12 @@
 
 set -x -e
 
-export INCLUDE_PATH="${BUILD_PREFIX}/include"
+export INCLUDE_PATH="${BUILD_PREFIX}/include:${BUILD_PREFIX}/third-party/seqan/core/include"
 export LIBRARY_PATH="${BUILD_PREFIX}/lib"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${BUILD_PREFIX}/lib"
 
-export LDFLAGS="-L${BUILD_PREFIX}/lib"
-export CPPFLAGS="-I${BUILD_PREFIX}/include"
+export LDFLAGS="-L${BUILD_PREFIX}/lib -L${BUILD_PREFIX}/third-party/seqan/core/include"
+export CCPFLAGS="-I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/third-party/seqan/core/include"
 
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/include
@@ -16,5 +16,6 @@ mkdir -p $PREFIX/scripts
 
 cp -v lib/* $PREFIX/include
 cp -v scripts/* $PREFIX/bin
+#cp -vr third-party/seqan/core/include $PREFIX/include/seqan
 
 python3 -m pip install . --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
