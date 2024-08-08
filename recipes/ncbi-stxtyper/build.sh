0566abe09a98f5ab76937b7b8aba223ec847de18
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2024-08-08 12:46:53 -0400

    Gave up trying to get a realistic test to run
    Couldn't figure out where the distribution was to run tests included
    with the distribution

--- recipes/ncbi-stxtyper/build.sh
+++ recipes/ncbi-stxtyper/build.sh
@@ -3,6 +3,15 @@
 # fix error because of gnu++17 features. Suggested by https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 # CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
+echo
+echo "Starting build.sh"
+echo
+echo "SRC_DIR=$SRC_DIR"
+echo "pwd -P"
+pwd -P
+echo "ls -l"
+ls -l
+
 # note that for version 3.7 the make command should be:
 make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX" 
 
@@ -10,3 +19,5 @@ make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX="$PREFIX"
 
 #make CXX="$CXX $LDFLAGS" CPPFLAGS="$CXXFLAGS" PREFIX=$PREFIX DEFAULT_DB_DIR="$PREFIX/share/amrfinderplus/data"
 make install bindir=$PREFIX/bin
+
+echo "build.sh done"
