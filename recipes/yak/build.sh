36cc520a403b13b28b0774eb2e3be7ce7198e28a
mencian <joshua.zhuang@yahoo.com>
2024-08-07 20:14:30 -0500

    edit build.sh

--- recipes/yak/build.sh
+++ recipes/yak/build.sh
@@ -2,13 +2,11 @@
 
 mkdir -p ${PREFIX}/bin
 
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
 #install_name_tool error fix
 if [[ "$(uname)" == Darwin ]]; then
-    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -headerpad_max_install_names"
 fi
 
-make INCLUDES="-I${PREFIX}/include" CFLAGS="${CFLAGS} -g -Wall -O2 -L${PREFIX}/lib"
+make INCLUDES="-I${PREFIX}/include" CFLAGS="${CFLAGS} -g -Wall -O3 -L${PREFIX}/lib"
 cp -rf yak ${PREFIX}/bin
 chmod 755 ${PREFIX}/bin/yak
