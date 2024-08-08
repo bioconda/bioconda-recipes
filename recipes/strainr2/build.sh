d06a649cfffa6e473f8ff950672b99cf2c9c8527
kheber <kerim@heber.org>
2024-08-08 02:07:28 -0400

    update file paths and include zlib in dependencies

--- recipes/strainr2/build.sh
+++ recipes/strainr2/build.sh
@@ -2,17 +2,17 @@
 
 mkdir -p "${PREFIX}/bin"
 
-${CC} ${SRC_DIR}/subcontig.c -o subcontig
-${CC} ${SRC_DIR}/hashcounter.c -o hashcounter
+${CC} ${SRC_DIR}/src/subcontig.c -o subcontig -O3
+${CC} ${SRC_DIR}/src/hashcounter.c -o hashcounter -O3 -lz
 
-chmod +x $SRC_DIR/PreProcessR
+chmod +x $SRC_DIR/src/PreProcessR
 chmod +x $SRC_DIR/subcontig
 chmod +x $SRC_DIR/hashcounter
-chmod +x $SRC_DIR/Plot.R
-chmod +x $SRC_DIR/StrainR
+chmod +x $SRC_DIR/src/Plot.R
+chmod +x $SRC_DIR/src/StrainR
 
-cp $SRC_DIR/PreProcessR ${PREFIX}/bin/
+cp $SRC_DIR/src/PreProcessR ${PREFIX}/bin/
 cp $SRC_DIR/subcontig ${PREFIX}/bin/
 cp $SRC_DIR/hashcounter ${PREFIX}/bin/
-cp $SRC_DIR/Plot.R ${PREFIX}/bin/
-cp $SRC_DIR/StrainR ${PREFIX}/bin/
+cp $SRC_DIR/src/Plot.R ${PREFIX}/bin/
+cp $SRC_DIR/src/StrainR ${PREFIX}/bin/
