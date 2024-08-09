730f476774486a8e447368bdc3687a6acbb4ef5f
Joe Wirth (Joseph S. Wirth) <71667025+dr-joe-wirth@users.noreply.github.com>
2024-08-09 09:32:45 -0400

    cute debug to find `khmer` path

--- recipes/khmer/build.sh
+++ recipes/khmer/build.sh
@@ -2,6 +2,7 @@
 
 set -x -e
 
+# export variables for compiling
 export INCLUDE_PATH="${BUILD_PREFIX}/include:${PWD}/third-party/seqan/core/include"
 export LIBRARY_PATH="${BUILD_PREFIX}/lib"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/lib"
@@ -9,11 +10,13 @@ export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/lib"
 export LDFLAGS="-L${BUILD_PREFIX}/lib -L${PWD}/third-party/seqan/core/include"
 export CCPFLAGS="-I${BUILD_PREFIX}/include -I${PWD}/third-party/seqan/core/include"
 
+# create directories for compiling
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/scripts
 
+# populate compiling directories
 cp -v lib/* $PREFIX/include
 cp -v scripts/* $PREFIX/bin
 
@@ -21,4 +24,9 @@ cp -v scripts/* $PREFIX/bin
 mkdir -pv $PREFIX/include/seqan
 cp -vr third-party/seqan/core/include/seqan/* $PREFIX/include/seqan/
 
+# install khmer
 $PYTHON -m pip install . --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
+
+# debugging line to find where khmer was installed
+echo "here i am! ⊂◉‿◉つ "
+ls -lhR
