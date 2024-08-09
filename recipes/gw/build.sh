1160279a5e98cf0fc741778c075c29ab06470761
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2024-08-09 12:35:09 +0100

    Update build.sh

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -1,40 +1,22 @@
 #!/usr/bin/bash
 set -e
-ldconfig -v | grep -v ^$'\t'
-echo
-ldconfig -p | grep libEGL
-ldconfig -p | grep libGLESv2
 
-#ln -s /lib64/*GL* ./lib
-#ls ./lib
-
-echo "SYSROOT"
-ls $CONDA_BUILD_SYSROOT
-
-echo "SYSROOT/usr"
-ls $CONDA_BUILD_SYSROOT/usr
-
-echo "SYSROOT/usr/lib"
-ls $CONDA_BUILD_SYSROOT/usr/lib
-
-echo "BUILD_PREFIX"
-ls $BUILD_PREFIX
-
-#echo "BUILD_PREFIX/lib64"
-#ls $BUILD_PREFIX/lib64
-
-#export USE_GL=1
+export USE_GL=1
 if [[ "$OSTYPE" != "darwin"* ]]; then
-#  sed -i 's/-lEGL -lGLESv2/-lGL/' Makefile
-  LDLIBS="${LDLIBS} -lGLX"
+  sed -i 's/-lEGL -lGLESv2/-lGL -lGLX/' Makefile
 fi
 make prep > /dev/null 2>&1 
 
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" LDFLAGS="${LDFLAGS} -L${PREFIX} -L$CONDA_BUILD_SYSROOT/usr/lib" prefix="${PREFIX}"  make -j ${CPU_COUNT}
+SYSROOT_FLAGS="--sysroot=${BUILD_PREFIX}/${HOST}/sysroot"
+
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY " \
+CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/${HOST}/sysroot/usr/include ${SYSROOT_FLAGS} " \
+LDFLAGS="${LDFLAGS} -Wl,--verbose -L${PREFIX} -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 ${SYSROOT_FLAGS}" \
+prefix="${PREFIX}" \
+make -j ${CPU_COUNT}
 
 mkdir -p $PREFIX/bin
 cp gw $PREFIX/bin/gw
 cp -n .gw.ini $PREFIX/bin/.gw.ini
 chmod +x $PREFIX/bin/gw
 chmod +rw $PREFIX/bin/.gw.ini
-
