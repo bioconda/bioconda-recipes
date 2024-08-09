f026ea966b1af1545d18b0ff72f455ecd37ae3e5
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2024-08-09 23:26:17 +0100

    Update build.sh

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -2,6 +2,8 @@
 set -e
 
 export USE_GL=1
+
+# Modify Makefile and source files if not on macOS
 if [[ "$OSTYPE" != "darwin"* ]]; then
   sed -i 's/-lEGL -lGLESv2/-lGL -lGLX/' Makefile
   sed -i 's/GLFW_EGL_CONTEXT_API/GLFW_NATIVE_CONTEXT_API/' src/plot_manager.cpp
@@ -9,11 +11,21 @@ fi
 
 make prep > /dev/null 2>&1 
 
-SYSROOT_FLAGS="--sysroot=${BUILD_PREFIX}/${HOST}/sysroot"
+# Set flags conditionally based on the OS type
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  SYSROOT_FLAGS="--sysroot=${BUILD_PREFIX}/${HOST}/sysroot"
+  CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/${HOST}/sysroot/usr/include ${SYSROOT_FLAGS}"
+  LDFLAGS="${LDFLAGS} -L${PREFIX} -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 ${SYSROOT_FLAGS}"
+else
+  # No sysroot settings for macOS
+  SYSROOT_FLAGS=""
+  CPPFLAGS="${CPPFLAGS}"
+  LDFLAGS="${LDFLAGS} -L${PREFIX}"
+fi
 
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY " \
-CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/${HOST}/sysroot/usr/include ${SYSROOT_FLAGS} " \
-LDFLAGS="${LDFLAGS} -L${PREFIX} -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 ${SYSROOT_FLAGS}" \
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" \
+CPPFLAGS="${CPPFLAGS}" \
+LDFLAGS="${LDFLAGS}" \
 prefix="${PREFIX}" \
 make -j ${CPU_COUNT}
 
