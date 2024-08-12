75e9c6334b8aa7cffd841af839d866da45a6ea4d
mencian <joshua.zhuang@yahoo.com>
2024-08-12 00:11:12 -0500

    clean up recipe

--- recipes/dna-nn/build.sh
+++ recipes/dna-nn/build.sh
@@ -6,15 +6,14 @@ export CPATH=${PREFIX}/include
 
 mkdir -p "${PREFIX}/bin"
 
-make
+make -j"${CPU_COUNT}"
 
-chmod +x dna-brnn
-chmod +x dna-cnn
-chmod +x gen-fq
-chmod +x parse-rm.js
+chmod 0755 dna-brnn
+chmod 0755 dna-cnn
+chmod 0755 gen-fq
+chmod 0755 parse-rm.js
 
-cp dna-brnn "${PREFIX}/bin"
-cp dna-cnn "${PREFIX}/bin"
-cp gen-fq "${PREFIX}/bin"
-cp parse-rm.js "${PREFIX}/bin"
-
+cp -rf dna-brnn "${PREFIX}/bin"
+cp -rf dna-cnn "${PREFIX}/bin"
+cp -rf gen-fq "${PREFIX}/bin"
+cp -rf parse-rm.js "${PREFIX}/bin"
