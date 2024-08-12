aaa0f38b4998b56c849f02ea821ce40032080c36
Martin Grigorov <martin-g@users.noreply.github.com>
2024-08-12 10:50:32 +0300

    Update recipes/sdust/build.sh

--- recipes/sdust/build.sh
+++ recipes/sdust/build.sh
@@ -4,7 +4,7 @@ set -x -e
 
 mkdir -p "${PREFIX}/bin"
 
-make CC="$CC" INCLUDES="${PREFIX}/include" -j ${CPU_COUNT}
+make CC="$CC" INCLUDES="-I${PREFIX}/include" -j ${CPU_COUNT}
 
 chmod 0755 sdust
 
