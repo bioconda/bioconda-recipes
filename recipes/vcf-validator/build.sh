d6f7aca85eaddaa2a0e2cdeea0dbc7388b39a40d
tcezard <tcezard@ebi.ac.uk>
2024-08-12 11:17:55 +0100

    testing with new branch

--- recipes/vcf-validator/build.sh
+++ recipes/vcf-validator/build.sh
@@ -1,14 +1,13 @@
 # Install additional dependencies
 if [ -z ${OSX_ARCH+x} ]; then
-  bash install_dependencies.sh linux
-else
-  bash install_dependencies.sh osx
-fi
-mkdir build
-cd build
-if [ -z ${OSX_ARCH+x} ]; then
+  ./install_dependencies.sh linux
+  mkdir build
+  cd build
   cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DDEFAULT_EXT_LIB_PATH=${BUILD_PREFIX} ..
 else
+  ./install_dependencies.sh osx
+  mkdir build
+  cd build
   cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DEXT_LIB_PATH=${BUILD_PREFIX} ..
 fi
 make -j2
