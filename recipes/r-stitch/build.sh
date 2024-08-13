d16f3513c10d595c32e0649a3e986e1194216f84
Zilong-Li <zimusen94@gmail.com>
2024-08-13 22:04:21 +0200

    bump stitch version 1.7.0

--- recipes/r-stitch/build.sh
+++ recipes/r-stitch/build.sh
@@ -5,9 +5,4 @@ set -xe
 mkdir -p $PREFIX/bin 
 cp STITCH.R $PREFIX/bin
 
-# patch a Git submodule
-pushd src/SeqLib/
-patch -p1 < ${RECIPE_DIR}/seqlib-aarch64.patch
-popd
-    
 $R CMD INSTALL --build --install-tests .
