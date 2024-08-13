52e59dfa77c2ccb1d4a566cf00f292a6656d4601
mencian <joshua.zhuang@yahoo.com>
2024-08-13 03:00:39 -0500

    Update build.sh

--- recipes/spades/build.sh
+++ recipes/spades/build.sh
@@ -4,8 +4,7 @@ set -e -o pipefail -x
 
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 -fcommon ${LDFLAGS}"
+export CFLAGS="${CFLAGS} -O3 -fcommon"
 export CXXFLAGS="${CFLAGS} -O3 -fcommon -I${PREFIX}/include"
 
 PREFIX="${PREFIX}" bash spades_compile.sh -rj4 -DSPADES_USE_NCBISDK=ON -DSPADES_ENABLE_PROJECTS="all"
