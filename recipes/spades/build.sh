a94cca1ac9287f620f99485d4589af43a44f8f1d
mencian <joshua.zhuang@yahoo.com>
2024-08-13 02:51:56 -0500

    Update build.sh

--- recipes/spades/build.sh
+++ recipes/spades/build.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -x
 
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -Wunused-command-line-argument -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -fcommon ${LDFLAGS}"
 export CXXFLAGS="${CFLAGS} -O3 -fcommon -I${PREFIX}/include"
 
