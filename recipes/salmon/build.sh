3404c8f9d7966965611212bcef4c5c84c03e56f2
mencian <joshua.zhuang@yahoo.com>
2024-08-14 03:35:31 -0500

    salmon: add arm64 build

--- recipes/salmon/build.sh
+++ recipes/salmon/build.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 set -eu -o pipefail
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/lib
+export INCLUDES="-I{PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
-export CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib -I${PREFIX}/include"
+if [[ `uname` == "Darwin" ]]; then
+	CFLAGS="${CFLAGS} -Wunused-command-line-argument -Wunused-parameter"
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
 cmake -S . -B build \
 	-DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" \
 	-DCONDA_BUILD=TRUE \
 	-DLIBSTADEN_LDFLAGS="-L${PREFIX}/lib" \
 	-DBoost_NO_BOOST_CMAKE=ON \
-	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
 	-DBoost_NO_SYSTEM_PATHS=ON \
-	-DNO_IPO=TRUE
+	-DNO_IPO=TRUE "${CONFIG_ARGS}"
 
 cmake --build build/ --target install -v
