b85288983834a3e687ec43059b5869581eaf31a7
Zilong-Li <zimusen94@gmail.com>
2024-08-18 13:50:13 +0200

    Update build.sh

--- recipes/r-stitch/build.sh
+++ recipes/r-stitch/build.sh
@@ -5,10 +5,5 @@ set -xe
 mkdir -p $PREFIX/bin 
 cp -rf STITCH.R $PREFIX/bin
 
-if [[ $(uname) == "Darwin" ]]; then
-    export LDFLAGS=-L${PREFIX}/lib
-    ${R} CMD INSTALL --build . --configure-args="CFLAGS=-ferror-limit=0 CXXFLAGS=-ferror-limit=0 -stdlib=libstdc++"
-else
-    ${R} CMD INSTALL --build .
-fi
+${R} CMD INSTALL --build . ${R_ARGS}
 
