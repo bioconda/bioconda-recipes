6eadc68eac0756f2abecb988140a91871fd6c12d
Zilong-Li <zimusen94@gmail.com>
2024-08-19 22:13:31 +0200

    Update build.sh

--- recipes/r-quilt/build.sh
+++ recipes/r-quilt/build.sh
@@ -5,7 +5,7 @@ set -xe
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-cp -rf $SRC_DIR/QUILT.R $PREFIX/bin/
+cp $SRC_DIR/QUILT.R $PREFIX/bin/
 cd $SRC_DIR/QUILT
 
 ${R} CMD INSTALL --build --install-tests .
