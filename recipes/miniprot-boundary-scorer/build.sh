547b97cd7f3a4699de03e296c3a7f66e630e27f5
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-08-20 11:08:48 +0300

    Try with LDFLAGS instead of LD_LIBRARY_PATH. Add aarch64/arm64 builds too
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/miniprot-boundary-scorer/build.sh
+++ recipes/miniprot-boundary-scorer/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib
-make CC="${CXX}"
+set -xe
+
+make -j ${CPU_COUNT} CC="${CXX}" LDFLAGS="-L${PREFIX}/lib"
 
 mkdir -p ${PREFIX}/bin
 cp miniprot_boundary_scorer ${PREFIX}/bin
