1a2b62aa4ab89de6145136f128ee277e13c7128d
mencian <joshua.zhuang@yahoo.com>
2024-08-21 01:26:14 -0500

    python-hyphy-python: add aarch64/arm64 build

--- recipes/python-hyphy-python/build.sh
+++ recipes/python-hyphy-python/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sed -i.bak "s#\['cc',#\['${CC}',#" setup.py
-$PYTHON -m pip install --no-deps --no-build-isolation --no-cache-dir . -vvv
+rm -rf *.bak
+${PYTHON} -m pip install --no-deps --no-build-isolation --no-cache-dir . -vvv
