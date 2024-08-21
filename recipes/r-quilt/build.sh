04fdfe75b70c7b074dea2bafd9b8afb0264c2a5e
Zilong-Li <zimusen94@gmail.com>
2024-08-21 08:06:59 +0200

    Update build.sh of quilt 2.0.0

--- recipes/r-quilt/build.sh
+++ recipes/r-quilt/build.sh
@@ -6,7 +6,11 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 cp $SRC_DIR/QUILT.R $PREFIX/bin/
-cd $SRC_DIR/QUILT
+cp $SRC_DIR/QUILT_prepare_reference.R $PREFIX/bin/
+cp $SRC_DIR/QUILT_HLA_prepare_reference.R $PREFIX/bin/
+cp $SRC_DIR/QUILT2.R $PREFIX/bin/
+cp $SRC_DIR/QUILT2_prepare_reference.R $PREFIX/bin/
 
+cd $SRC_DIR/QUILT
 ${R} CMD INSTALL --build --install-tests .
 
