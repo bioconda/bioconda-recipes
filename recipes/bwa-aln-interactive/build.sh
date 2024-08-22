47282afd705daa1c3f85db580f0050452bf2e551
Nils Homer <nh13@users.noreply.github.com>
2024-08-22 13:40:48 -0700

    Apply suggestions from code review
    Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

--- recipes/bwa-aln-interactive/build.sh
+++ recipes/bwa-aln-interactive/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-make CC="$CC" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"
+make CC="$CC" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" -j "${CPU_COUNT}"
 mkdir -p $PREFIX/bin
 cp bwa $PREFIX/bin/bwa-aln-interactive
