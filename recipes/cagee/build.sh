04e47ea9ae0e975e1b04ac55e2b88fd0f554cc16
mencian <joshua.zhuang@yahoo.com>
2024-08-23 17:17:00 -0500

    clean up recipe

--- recipes/cagee/build.sh
+++ recipes/cagee/build.sh
@@ -5,24 +5,18 @@ set -x
 
 if [ "$(uname)" = "Darwin" ]; then
   # LDFLAGS fix: https://github.com/AnacondaRecipes/intel_repack-feedstock/issues/8
-  export LDFLAGS="-Wl,-pie -Wl,-headerpad_max_install_names -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
+  export LDFLAGS="${LDFLAGS} -Wl,-pie -Wl,-headerpad_max_install_names -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
 else
-  export LDFLAGS="-L$PREFIX/lib"
+  export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
   export MKL_THREADING_LAYER="GNU"
 fi
+
 # https://bioconda.github.io/contributor/troubleshooting.html#zlib-errors
-export CFLAGS="-I$PREFIX/include"
-export CPATH=${PREFIX}/include
+export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export INCLUDE_PATH="${PREFIX}/include"
 
-
-mkdir -p build
-cd build
-
-cmake -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
+cmake -S . -B build \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-  -DCMAKE_BUILD_TYPE="Release" \
-  ..
+  -DCMAKE_BUILD_TYPE="Release"
 
-make 
-make install
-
+cmake --build build --target install -j "${CPU_COUNT}" -v
