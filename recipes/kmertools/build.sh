bf59f380b55667f79fa5a4efd5a4f876149e1d26
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2024-08-26 20:04:21 +0930

    Update kmertools to v0.1.1

--- recipes/kmertools/build.sh
+++ recipes/kmertools/build.sh
@@ -5,3 +5,9 @@ cargo build --release
 
 # Install the binaries
 cargo install --path kmertools --root $PREFIX 
+
+# Build with maturin
+maturin build --release --manifest-path ./pykmertools/Cargo.toml
+
+# Install the wheel file
+pip install ./target/wheels/*.whl 
