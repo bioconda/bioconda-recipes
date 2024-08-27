933938682fea8151fa86ca666fadc9f3fffe9f97
Fabian Boehm <Fabian.Boehm@Logartis.de>
2024-08-27 22:35:51 +0200

    removed attempted channel specification

--- recipes/r-eoffice/build.sh
+++ recipes/r-eoffice/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-conda install -c r r-devemf
 export DISABLE_AUTOBREW=1
 $R CMD INSTALL --build .
--- recipes/r-polyatailor/build.sh
+++ recipes/r-polyatailor/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
-conda config --add channels r
-conda config --add channels conda-forge
-conda config --add channels bioconda
 $R CMD INSTALL --build .
