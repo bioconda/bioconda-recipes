22919032e50d4227ff5aab47113e608b1bcd0dd3
Rauf Salamzade <salamzader@gmail.com>
2024-08-31 20:11:55 -0500

    Update build.sh

--- recipes/skder/build.sh
+++ recipes/skder/build.sh
@@ -4,9 +4,13 @@ $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 mkdir -p ${PREFIX}/bin
 
-# (re)-compile C++ programs
-${CXX} -std=c++11 -o ${PREFIX}/bin/skDERcore src/skDER/skDERcore.cpp
-${CXX} -std=c++11 -o ${PREFIX}/bin/skDERsum src/skDER/skDERsum.cpp
+if [ ! -f ${PREFIX}/bin/skDERsum ]; then
+    ${CXX} -std=c++11 -o ${PREFIX}/bin/skDERsum src/skDER/skDERsum.cpp
+fi
+
+if [ ! -f ${PREFIX}/bin/skDERcore ]; then
+    ${CXX} -std=c++11 -o ${PREFIX}/bin/skDERcore src/skDER/skDERcore.cpp
+fi
 
 chmod +x ${PREFIX}/bin/skDERcore
 chmod +x ${PREFIX}/bin/skDERsum
