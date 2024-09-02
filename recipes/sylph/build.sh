50c12c26573f413e2de0c5f6fa7c5ab56850439d
Florian Plaza Oñate <florian.plaza-onate@inrae.fr>
2024-09-02 18:15:25 +0200

    Build sylph for multiple target cpus

--- recipes/sylph/build.sh
+++ recipes/sylph/build.sh
@@ -4,5 +4,12 @@
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
-# build statically linked binary with Rust
-RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX
+for INSTRUCTION_SET in "x86-64" "x86-64-v2" "x86-64-v3" "x86-64-v4"; do
+	# build statically linked binary with Rust
+	RUST_BACKTRACE=1 RUSTFLAGS="-C target-cpu=${INSTRUCTION_SET}" cargo install --verbose --path . --root $PREFIX
+	mv ${PREFIX}/bin/sylph ${PREFIX}/bin/_sylph_${INSTRUCTION_SET}
+done
+
+
+cp "${RECIPE_DIR}/sylph" "${PREFIX}/bin/sylph"
+chmod +x "${PREFIX}/bin/sylph"
