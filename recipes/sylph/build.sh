8303355850284c87bd7eaebc96b6076bf2ad9fc1
Florian Plaza Oñate <florian.plaza-onate@inrae.fr>
2024-09-03 09:41:40 +0200

    Add support for aarch64

--- recipes/sylph/build.sh
+++ recipes/sylph/build.sh
@@ -4,10 +4,16 @@
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
-for INSTRUCTION_SET in "x86-64" "x86-64-v2" "x86-64-v3" "x86-64-v4"; do
-	# build statically linked binary with Rust
-	RUST_BACKTRACE=1 RUSTFLAGS="-C target-cpu=${INSTRUCTION_SET}" cargo install --verbose --path . --root $PREFIX
-	mv ${PREFIX}/bin/sylph ${PREFIX}/bin/_sylph_${INSTRUCTION_SET}
+if [ `uname -m` == "aarch64" ]; then
+    TARGET_CPUS=("aarch64")
+else # x86-64
+    TARGET_CPUS=("x86-64" "x86-64-v2" "x86-64-v3" "x86-64-v4")
+fi
+
+for TARGET_CPU in "${TARGET_CPUS[@]}"; do
+    # build statically linked binary with Rust
+    RUST_BACKTRACE=1 RUSTFLAGS="-C target-cpu=${TARGET_CPU}" cargo install --verbose --path . --root $PREFIX
+    mv ${PREFIX}/bin/sylph ${PREFIX}/bin/_sylph_${TARGET_CPU}
 done
 
 
