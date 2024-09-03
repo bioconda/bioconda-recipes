e2e8597968e0dbfeca5dc1b35812041561d1958b
Jason Shao <Jason.Shao@dal.ca>
2024-09-03 16:54:22 -0300

    feat: updated build to use setup.py

--- recipes/eukfinder/setup.py
+++ recipes/eukfinder/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="eukfinder",
     version="1.2.3",
-    py_modules=["eukfinder"],
+    py_modules=["Eukfinder"],
     entry_points={
         'console_scripts': [
-            'eukfinder = eukfinder:main',
+            'eukfinder = Eukfinder:main',
         ],
     },
 )
