82a4b8e63d37670967e8de8b71511185d7c72587
UENO, M. <eunosium-1128@live.jp>
2024-09-04 01:12:09 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,10 +5,14 @@ export RCSBROOT=${PREFIX}
 cd ${SRC_DIR}/maxit-v10.1/src
 sed -i.bak 's|rcsbroot = getenv("RCSBROOT")|rcsbroot = "'${PREFIX}'"|' maxit.C process_entry.C generate_assembly_cif_file.C
 
-cd ../../cifparse-obj-v7.0
+cd ${SRC_DIR}/cifparse-obj-v7.0
 sed -i.bak 's/mv /cp /' Makefile
 
-cd ${SRC_DIR}/maxit-v10.1
+cd ${SRC_DIR}
+sed -i.bak 's|./data/binary|'"${PREFIX}/data/binary"'|g' binary.csh
+
+
+cd ${SRC_DIR}
 make binary
 
 install -d ${PREFIX}/bin
