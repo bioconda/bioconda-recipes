0d3eb135d68ec8d8d17549a17c0ccf046da3bab3
UENO, M. <eunosium-1128@live.jp>
2024-09-03 17:49:45 +0900

    Update build.sh meta.yaml

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -1,16 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+# 環境変数の設定
+export RCSBROOT="$PREFIX"
 
+# ソースディレクトリに移動
+cd maxit-v${PKG_VERSION}-prod-src
+
+# ビルド用にソースコードを修正
 cd maxit-v10.1/src
-sed -i.bak 's|rcsbroot = getenv("RCSBROOT")|rcsbroot = "${PREFIX}"|' maxit.C process_entry.C generate_assembly_cif_file.C
-
+sed -i.bak 's|rcsbroot = getenv("RCSBROOT")|rcsbroot = "'${PREFIX}'"|' maxit.C process_entry.C generate_assembly_cif_file.C
 cd ../cifparse-obj-v7.0
-sed -i.bak 's/mv /cp /' Makefile  # circumvent CI errors
-
+sed -i.bak 's/mv /install /' Makefile
 cd ../../
 
+# アプリケーションのビルド
+make
+
+# 必要なバイナリデータの生成
 make binary
-cp -r bin/* ${PREFIX}/bin
-cp -r data ${PREFIX}/
+
+# バイナリとデータをインストール
+install -d "$PREFIX/bin"  # 必要なディレクトリを作成
+install bin/* "$PREFIX/bin"  # バイナリをコピー
+install -d "$PREFIX/data"
+install -m 644 data/* "$PREFIX/data"  # データをコピー
