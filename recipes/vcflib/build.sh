b62035959c409342329c96bd0052daead1c9ee98
mencian <joshua.zhuang@yahoo.com>
2024-09-04 04:07:57 -0500

    clean up recipe

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -5,19 +5,19 @@ ARCH=$(uname -m)
 
 if [[ "${OS}" == "Darwin" ]] && [[ "${ARCH}" == "x86_64" ]]; then
         echo $(pwd)/zig-macos-x86_64-*
-        export PATH="$(pwd)/zig-macos-x86_64-0.10.1:${PATH}"
+        export PATH="$(pwd)/zig-macos-x86_64-0.9.1:${PATH}"
 elif [[ "${OS}" == "Darwin" ]] && [[ "${ARCH}" == "arm64" ]]; then
         echo $(pwd)/zig-macos-aarch64-*
-        export PATH="$(pwd)/zig-macos-aarch64-0.10.1:${PATH}"
+        export PATH="$(pwd)/zig-macos-aarch64-0.9.1:${PATH}"
 else
         echo $(pwd)/zig-linux-${ARCH}-*
-        export PATH="$(pwd)/zig-linux-${ARCH}-0.10.1:${PATH}"
+        export PATH="$(pwd)/zig-linux-${ARCH}-0.9.1:${PATH}"
 fi
 
 export INCLUDES="-I${PREFIX}/include -I. -Ihtslib -Itabixpp -Iwfa2 -I\$(INC_DIR)"
 export LIBPATH="-L${PREFIX}/lib -L. -Lhtslib -Ltabixpp -Lwfa2"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lhts -ltabixpp -lpthread -lz -lm -llzma -lbz2 -fopenmp -lwfa2"
-export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
+export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64 -I${PREFIX}/include"
 
 sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64 -std=c++0x/' contrib/smithwaterman/Makefile
 sed -i.bak 's/CFLAGS/CXXFLAGS/g' contrib/smithwaterman/Makefile
@@ -32,9 +32,8 @@ sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/intervaltree/Makefile
 if [[ `uname` == "Darwin" ]]; then
 	sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' contrib/smithwaterman/Makefile
 	sed -i.bak 's/-std=c++0x/-std=c++11 -stdlib=libc++/g' contrib/intervaltree/Makefile
-	#export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-	export LD="${CXX}"
-	#export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_ENABLE_CXX17_REMOVED_FEATURES"
+	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_ENABLE_CXX17_REMOVED_FEATURES"
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
 	export CONFIG_ARGS=""
@@ -47,7 +46,7 @@ cmake -S . -B build \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DBUILD_SHARED_LIBS=ON \
-	-DCMAKE_CXX_COMPILER="${CXX}" \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	"${CONFIG_ARGS}"
 
