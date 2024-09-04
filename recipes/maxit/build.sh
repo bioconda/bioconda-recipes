9541688a2fae3b51f5f74825526e59e87d24a4dc
mencian <joshua.zhuang@yahoo.com>
2024-09-03 21:47:08 -0500

    clean up recipe

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-export PATH=${PREFIX}/bin/gcc:${PATH}
-export RCSBROOT=${PREFIX}
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
+export RCSBROOT="${PREFIX}"
 
 cd ${SRC_DIR}/maxit-v10.1/src
 sed -i.bak 's|rcsbroot = getenv("RCSBROOT")|rcsbroot = "'${PREFIX}'"|' maxit.C process_entry.C generate_assembly_cif_file.C
+rm -rf *.bak
 
 cd ${SRC_DIR}/cifparse-obj-v7.0
 sed -i.bak 's/mv /cp /' Makefile
+rm -rf *.bak
 
 cd ${SRC_DIR}
 sed -i.bak 's|./data/binary|'"${PREFIX}/data/binary"'|g' binary.sh
+rm -rf *.bak
 
-make binary
+make compile -j"${CPU_COUNT}" CC="${CC}" CFLAGS="${CFLAGS}"
+make binary -j"${CPU_COUNT}" CC="${CC}" CFLAGS="${CFLAGS}"
 
 install -d ${PREFIX}/bin
 install bin/* ${PREFIX}/bin
