93d460af5007f0fd085f8369d847c7af63fbd7d8
mencian <joshua.zhuang@yahoo.com>
2024-09-03 23:21:10 -0500

    clean up recipe

--- recipes/orthologer/build.sh
+++ recipes/orthologer/build.sh
@@ -5,7 +5,4 @@
 export SKIP_EXTERNAL=1
 
 # install ORTHOLOGER and BRHCLUS
-# * $PREFIX/bin/ORTHOLOGER-x.x.x/
-# * $PREFIX/bin/orthologer    -> softlink to ORTHOLOGER-x.x.x
-# * $PREFIX/bin/brhclus       -> plus all the other executables of BRHCLUS
 ./install_pkg.sh
