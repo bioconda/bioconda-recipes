20738d252114e2a5ed6230cd696772d922ed0560
LiliyaBioinf <khadeeva.liliya@gmail.com>
2024-09-04 21:43:45 +0200

    Update build.sh

--- recipes/pyscenic/build.sh
+++ recipes/pyscenic/build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-set -ex
+set -e
 
-for package in arboreto-0.1.6 interlap-0.2.7 multiprocessing_on_dill-3.5.0a4 ctxcore-0.2.0; do
-  pushd ${SRC_DIR}/${package}
-  ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
-  popd
-done
+pushd ${SRC_DIR}/arboreto-0.1.6
+if [ ! -f requirements.txt ]; then
+    echo "dask[complete]" > requirements.txt
+    echo "distributed" >> requirements.txt
+    echo "numpy>=1.16.5" >> requirements.txt
+    echo "pandas" >> requirements.txt
+    echo "scikit-learn" >> requirements.txt
+    echo "scipy" >> requirements.txt
+fi
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
+popd
+
+pushd ${SRC_DIR}/interlap-0.2.7
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
+popd
+
+pushd ${SRC_DIR}/multiprocessing_on_dill-3.5.0a4
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
+popd
+
+pushd ${SRC_DIR}/ctxcore-0.2.0
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
+popd
 
 pushd ${SRC_DIR}/pyscenic-0.12.1
 ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
