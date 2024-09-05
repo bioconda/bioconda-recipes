6d6f5cf894248ba5d9b6d5028feacbe286c56d2b
UENO, M. <eunosium-1128@live.jp>
2024-09-05 15:56:50 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -4,21 +4,17 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
-export RCSBROOT=${PREFIX}
+export RCSBROOT=${SRC_DIR}
+export CPU_COUNT=1
 
 ln -s ${CC} ${BUILD_PREFIX}/bin/gcc
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
 ln -s ${CXX} ${BUILD_PREFIX}/bin/cxx
 ln -s ${GXX} ${BUILD_PREFIX}/bin/g++
 
-cd ${SRC_DIR}/cifparse-obj-v7.0
-sed -i 's/mv /cp /g' Makefile
-
-cd ${SRC_DIR}
-sed -i 's|./data/binary|\${RCSBROOT}/data/binary|g' binary.sh
-
-cd ${SRC_DIR}
-make binary
+cd ${SRC_DIR}/cifparse-obj-v7.0 && sed -i 's/mv /cp /g' Makefile
+cd ${SRC_DIR} && sed -i 's|./data/binary|${SRC_DIR}/data/binary|g' binary.sh
+cd ${SRC_DIR} && make binary -j"${CPU_COUNT}"
 
 unlink ${BUILD_PREFIX}/bin/gcc
 unlink ${BUILD_PREFIX}/bin/c++
@@ -26,6 +22,6 @@ unlink ${BUILD_PREFIX}/bin/cxx
 unlink ${BUILD_PREFIX}/bin/g++
 
 install -d ${PREFIX}/bin
-install ${RCSBROOT}/bin/* ${PREFIX}/bin
+install ${SRC_DIR}/bin/* ${PREFIX}/bin
 install -d ${PREFIX}/data
-install -m 644 ${RCSBROOT}/data/* ${PREFIX}/data
+install -m 644 ${SRC_DIR}/data/* ${PREFIX}/data
