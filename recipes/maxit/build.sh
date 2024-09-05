1af225ad4def0f07ebfe3781a71d4621dd691db4
UENO, M. <eunosium-1128@live.jp>
2024-09-05 19:16:59 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -exo pipefail
+set -xo pipefail
 
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
@@ -16,7 +16,7 @@ ln -s ${GXX} ${BUILD_PREFIX}/bin/g++
 alias sed="${BUILD_PREFIX}/bin/sed"
 
 cd ${SRC_DIR}/maxit-v10.1/src && \
-sed -i "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = ${SRC_DIR}|g" maxit.C process_entry.C generate_assembly_cif_file.C
+sed -i "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${SRC_DIR}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
 
 cd ${SRC_DIR}/cifparse-obj-v7.0 && sed -i 's|mv |cp |g' Makefile
 cd ${SRC_DIR} && sed -i "s|./data/binary|${SRC_DIR}/data/binary|g" binary.sh
