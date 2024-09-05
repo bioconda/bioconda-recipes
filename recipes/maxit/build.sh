fcdfab48c43c71d27817db5ff1801388f21bfbd6
UENO, M. <eunosium-1128@live.jp>
2024-09-05 22:57:30 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -7,6 +7,7 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
 export CPU_COUNT=1
+export RCSBROOT=${SRC_DIR}
 
 ln -s ${CC} ${BUILD_PREFIX}/bin/gcc
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
@@ -15,11 +16,11 @@ ln -s ${GXX} ${BUILD_PREFIX}/bin/g++
 
 alias sed="${BUILD_PREFIX}/bin/sed"
 
-cd ${SRC_DIR}/maxit-v10.1/src && \
-sed -i "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${PREFIX}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
+# cd ${SRC_DIR}/maxit-v10.1/src && \
+# sed -i "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${PREFIX}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
 
 cd ${SRC_DIR}/cifparse-obj-v7.0 && sed -i 's|mv |cp |g' Makefile
-cd ${SRC_DIR} && sed -i "s|./data/binary|${PREFIX}/data/binary|g" binary.sh
+# cd ${SRC_DIR} && sed -i "s|./data/binary|${PREFIX}/data/binary|g" binary.sh
 cd ${SRC_DIR} && make binary -j"${CPU_COUNT}"
 
 unlink ${BUILD_PREFIX}/bin/gcc
@@ -27,7 +28,7 @@ unlink ${BUILD_PREFIX}/bin/c++
 unlink ${BUILD_PREFIX}/bin/cxx
 unlink ${BUILD_PREFIX}/bin/g++
 
-# install ${SRC_DIR}/bin/* ${PREFIX}/bin
+install ${SRC_DIR}/bin/* ${PREFIX}/bin
 
-# install -d ${PREFIX}/data
-# cp -r ${SRC_DIR}/data/* ${PREFIX}/data
+install -d ${PREFIX}/data
+cp -r ${SRC_DIR}/data/* ${PREFIX}/data
