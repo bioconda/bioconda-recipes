73f2370df310c4c27debcf3dfea9fe3da9a0268a
LiliyaBioinf <khadeeva.liliya@gmail.com>
2024-09-05 14:14:38 +0200

    Update build.sh

--- recipes/pyscenic/build.sh
+++ recipes/pyscenic/build.sh
@@ -7,11 +7,11 @@ fi
 
 for package in arboreto-0.1.6 interlap-0.2.7 multiprocessing_on_dill-3.5.0a4 ctxcore-0.2.0; do
   pushd ${SRC_DIR}/${package}
-  ${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
+  ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
   popd
 done
 
-
 pushd ${SRC_DIR}/pyscenic-0.12.1
-${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
 popd
+
