f2dd6f7dd00ab5d07f810396ca7c0ced6bfbeeae
UENO, M. <eunosium-1128@live.jp>
2024-09-06 00:31:29 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -xo pipefail
+set -exo pipefail
 
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
-export CPU_COUNT=1
-export RCSBROOT=${SRC_DIR}
+# export CPU_COUNT=1
+export RCSBROOT="${SRC_DIR}"
 
-ln -s ${CC} ${BUILD_PREFIX}/bin/gcc
-ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-ln -s ${CXX} ${BUILD_PREFIX}/bin/cxx
-ln -s ${GXX} ${BUILD_PREFIX}/bin/g++
+ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
+ln -s "${CXX}" "${BUILD_PREFIX}/bin/c++"
+ln -s "${CXX}" "${BUILD_PREFIX}/bin/cxx"
+ln -s "${GXX}" "${BUILD_PREFIX}/bin/g++"
 
 alias sed="${BUILD_PREFIX}/bin/sed"
 
@@ -23,10 +23,10 @@ cd ${SRC_DIR}/cifparse-obj-v7.0 && sed -i 's|mv |cp |g' Makefile
 # cd ${SRC_DIR} && sed -i "s|./data/binary|${PREFIX}/data/binary|g" binary.sh
 cd ${SRC_DIR} && make binary -j"${CPU_COUNT}"
 
-unlink ${BUILD_PREFIX}/bin/gcc
-unlink ${BUILD_PREFIX}/bin/c++
-unlink ${BUILD_PREFIX}/bin/cxx
-unlink ${BUILD_PREFIX}/bin/g++
+# unlink ${BUILD_PREFIX}/bin/gcc
+# unlink ${BUILD_PREFIX}/bin/c++
+# unlink ${BUILD_PREFIX}/bin/cxx
+# unlink ${BUILD_PREFIX}/bin/g++
 
 install ${SRC_DIR}/bin/* ${PREFIX}/bin
 
