75590e4cc5a8eefabbdb0064e83769c885123328
Florian Plaza Oñate <florian.plaza-onate@inrae.fr>
2024-09-06 11:18:41 +0200

    Disable EXTRA_FLAGS that set march=native optimisations

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -40,6 +40,7 @@ cmake -S . -B build \
 	-DZIG=ON -DOPENMP=ON -DWFA_GITMODULE=OFF \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DEXTRA_FLAGS=""
 
 cmake --build build/ --target install -j 4 -v
