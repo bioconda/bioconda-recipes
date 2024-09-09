a60daf45e0c79027d36a85c49b57bcd364dd1717
Florian Plaza Oñate <4588656+fplazaonate@users.noreply.github.com>
2024-09-09 13:32:40 +0200

    Update vcflib (#50531)
    * Disable EXTRA_FLAGS that set march=native optimisations
    
    * Bump build version
    
    * Add comment line about disabling EXTRA_FLAGS
    
    * Update recipes/vcflib/build.sh
    
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>
    
    ---------
    
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -40,6 +40,7 @@ cmake -S . -B build \
 	-DZIG=ON -DOPENMP=ON -DWFA_GITMODULE=OFF \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DEXTRA_FLAGS="" # Disable march=native to build a generic executable, see https://github.com/vcflib/vcflib/issues/401
 
 cmake --build build/ --target install -j 4 -v
