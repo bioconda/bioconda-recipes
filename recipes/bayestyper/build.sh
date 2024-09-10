12aabcb8379de3487e0267c493fb52b85d8764ee
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-09-10 16:07:11 +0300

    Download the sources from the Git tag to fetch the Git history too
    Use Boost from the library path
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/bayestyper/build.sh
+++ recipes/bayestyper/build.sh
@@ -1,29 +1,18 @@
-#!/bin/bash -euo
+#!/bin/bash
 
-set -x
+set -xeuo
 
-export INCLUDES="-I${PREFIX}/include"
-export LIBPATH="-L${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
-
-# Delete call to `git rev-parse HEAD`
-# https://github.com/bioinformatics-centre/BayesTyper/blob/1b04081ca4c64975270d267dbe9d80bd00bddaf9/CMakeLists.txt#L4-L9
-sed -i.bak "4d;5d;6d;7d;8d;9d" CMakeLists.txt
-
-# include boost from the library path, not from local in all *.cpp/*.hpp files
-#find ./ -name "*.?pp" | xargs sed -i.bak -E 's/#include "boost(.*)"/#include <boost\1>/'
-
-# Disable CMake finding Boost
-sed -i.bak -E 's/(FIND_PACKAGE\(Boost.*)/#\1/' CMakeLists.txt
+#export INCLUDES="-I${PREFIX}/include"
+#export LIBPATH="-L${PREFIX}/lib"
+#export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+#export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 mkdir -p ${PREFIX}/bin
 
 cmake -S . -B build \
 	-Wno-dev \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-DGIT_LAST_COMMIT_HASH=123456
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 cmake --build build -j ${CPU_COUNT} --verbose
 
 cp -f ${SRC_DIR}/bin/bayesTyper ${PREFIX}/bin
