842c6c0741e300850f8e711e245ea7d1ceef5d2b
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-09-10 15:43:27 +0300

    Unset paw's arch specific build settings for linux-aarch64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/graphtyper/build.sh
+++ recipes/graphtyper/build.sh
@@ -2,10 +2,19 @@
 
 set -xe
 
+case $(uname -m) in
+    aarch64)
+        ARCH_OPTS="-Dpaw_BUILD_FLAGS=\"-DCMAKE_CXX_FLAGS_ARCH=\""
+        ;;
+    *)
+        ARCH_OPTS=
+        ;;
+esac
+
 # Generate build
 mkdir -p build
 cd build
-cmake -DCFLAGS="${CFLAGS}" -DLDFLAGS="${LDFLAGS}" ..
+cmake -DCFLAGS="${CFLAGS}" -DLDFLAGS="${LDFLAGS}" ${ARCH_OPTS} ..
 
 # Build
 VERBOSE=1 make -j ${CPU_COUNT} graphtyper
