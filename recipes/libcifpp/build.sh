1a790db55c8197018f7b1465b685fe857f7d7085
mencian <joshua.zhuang@yahoo.com>
2024-09-10 06:51:39 -0500

    clean up recipe

--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -5,12 +5,6 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
-if [[ "$(uname)" == "Darwin" ]]; then
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-else
-	export CONFIG_ARGS=""
-fi
-
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
