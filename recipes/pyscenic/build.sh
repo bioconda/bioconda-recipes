222e3a5f8245b116db20fcd6a51caba7a607aa3a
LiliyaBioinf <khadeeva.liliya@gmail.com>
2024-09-13 00:00:10 +0200

    returned multiprocessing_on_dill

--- recipes/pyscenic/build.sh
+++ recipes/pyscenic/build.sh
@@ -5,12 +5,9 @@ if [ -z "${PYTHON}" ]; then
   PYTHON=$(which python)
 fi
 
-for package in multiprocessing_on_dill-3.5.0a4 ctxcore-0.2.0; do
-  pushd ${SRC_DIR}/${package}
-  
-  ${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
-  popd
-done
+pushd ${SRC_DIR}/multiprocessing_on_dill-3.5.0a4
+${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
+popd
 
 pushd ${SRC_DIR}/pyscenic-0.12.1
 ${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
