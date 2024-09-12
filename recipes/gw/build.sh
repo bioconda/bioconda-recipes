db2d09c07d85c49e2da309c63aca358f339d2beb
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2024-09-12 23:11:33 +0100

    Update build.sh

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -8,7 +8,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i.bak 's/skia_use_metal=true//g' ./deps/build_skia.sh
 fi
 # Use gn from conda
-sed -i.bak 's/bin\/gn/gn/g' build_skia.sh
+sed -i.bak 's/bin\/gn/gn/g' ./deps/build_skia.sh
 
 bash ./deps/build_skia.sh
 
