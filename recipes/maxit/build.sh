a3b6e564e6fabb24e8a77fb22c9c44587dcfbb54
UENO, M. <eunosium-1128@live.jp>
2024-09-12 22:45:47 +0900

    Remove conditional statements for platforms in build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -7,24 +7,11 @@ export CPU_COUNT=1
 
 export RCSBROOT="${SRC_DIR}"
 
-case "${OSTYPE}" in
-    darwin*)
-        alias sed="${BUILD_PREFIX}/bin/sed"
-        ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
-        ln -s "${CXX}" "${BUILD_PREFIX}/bin/g++"
-        ;;
+# To pass CI test on amd64 platforms
+ulimit -v 2097152
 
-    linux*)
-        # To pass CI test on amd64 platforms
-        ulimit -v 2097152
-
-        ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
-        ln -s "${CXX}" "${BUILD_PREFIX}/bin/g++"
-        ;;
-    *)
-        exit 1
-        ;;
-esac
+ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
+ln -s "${CXX}" "${BUILD_PREFIX}/bin/g++"
 
 cd ${SRC_DIR}/maxit-v10.1/src && \
 sed -i.bak "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${RCSBROOT}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
