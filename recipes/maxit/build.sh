2301b1d76ff836afd588e950ae9a797aeea6fffa
UENO, M. <eunosium-1128@live.jp>
2024-09-13 01:34:19 +0900

    Limit `ulimit` usage only to Linux-x86_64

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-set -x
+set -exo pipefail
 
 # Disable parallel build
 export CPU_COUNT=1
 
 export RCSBROOT="${SRC_DIR}"
 
-echo '${BUILD_PREFIX}/bin/'
-ls -l ${BUILD_PREFIX}/bin
-
-# To pass CI test on amd64 platforms
-ulimit -v 2097152
+# To pass CI test on Linux-x86_64 platform
+if [[ "$(uname -s)" == "Linux" && "$(uname -m)" == "x86_64" ]]; then
+    ulimit -v 2097152
+fi
 
 ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
@@ -19,7 +18,7 @@ ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 cd ${SRC_DIR}/maxit-v10.1/src && \
 sed -i.bak "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${RCSBROOT}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
 
-cd "${SRC_DIR}/cifparse-obj-v7.0" && sed -i 's|mv |cp |g' Makefile
+cd "${SRC_DIR}/cifparse-obj-v7.0" && sed -i.bak 's|mv |cp |g' Makefile
 cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${RCSBROOT}/data/binary|g" binary.sh
 cd "${SRC_DIR}" && make binary -j${CPU_COUNT}
 
