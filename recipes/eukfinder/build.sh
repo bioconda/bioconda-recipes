de3b9a0307681cf22900de092c491f1594628304
Jason Shao <Jason.Shao@dal.ca>
2024-09-12 11:23:23 -0300

    fix: fixed missing whitespaces around version constraints, moved dependency testing to test in meta.yaml

--- recipes/eukfinder/build.sh
+++ recipes/eukfinder/build.sh
@@ -7,8 +7,7 @@ echo "PLAST downloaded successfully"
 
 # Setup PLAST binaries
 tar -zxf plastbinary_linux_v2.3.2.tar.gz
-plastbinary_linux_v2.3.2/plast -h || { echo 'PLAST installation failed'; exit 1; }
-echo "PLAST installed successfully"
+cp plastbinary_linux_v2.3.2/plast $PREFIX/bin/
 
 # Clone acc2tax repository
 git clone https://github.com/richardmleggett/acc2tax.git
@@ -19,8 +18,10 @@ cd acc2tax
 # According to https://bioconda.github.io/contributor/troubleshooting.html
 # and https://github.com/bioconda/bioconda-recipes/pull/49360#discussion_r1686187284
 $(which "$CC") -o acc2tax acc2tax.c
-./acc2tax --help || { echo 'acc2tax build failed'; exit 1; }
-echo "acc2tax installed successfully"
+cp acc2tax/acc2tax $PREFIX/bin/
 cd ..
+
+chmod +x $PREFIX/bin/plast
+chmod +x $PREFIX/bin/acc2tax
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
