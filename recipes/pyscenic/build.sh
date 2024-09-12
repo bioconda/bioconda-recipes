e541c226258d8beb1a2e656cf78ccb1007d5aae4
LiliyaBioinf <khadeeva.liliya@gmail.com>
2024-09-12 17:10:40 +0200

    deleting packeges from conda

--- recipes/pyscenic/build.sh
+++ recipes/pyscenic/build.sh
@@ -5,17 +5,8 @@ if [ -z "${PYTHON}" ]; then
   PYTHON=$(which python)
 fi
 
-for package in arboreto-0.1.6 interlap-0.2.7 multiprocessing_on_dill-3.5.0a4 ctxcore-0.2.0; do
+for package in multiprocessing_on_dill-3.5.0a4 ctxcore-0.2.0; do
   pushd ${SRC_DIR}/${package}
-  
-  if [ "$package" == "arboreto-0.1.6" ] && [ ! -f requirements.txt ]; then
-    echo "dask[complete]" > requirements.txt
-    echo "distributed" >> requirements.txt
-    echo "numpy>=1.16.5" >> requirements.txt
-    echo "pandas" >> requirements.txt
-    echo "scikit-learn" >> requirements.txt
-    echo "scipy" >> requirements.txt
-  fi
   
   ${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
   popd
@@ -24,3 +15,4 @@ done
 pushd ${SRC_DIR}/pyscenic-0.12.1
 ${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
 popd
+
