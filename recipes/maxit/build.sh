1b41961963ae6e7c9d2ca4c07f6f3be872959538
UENO, M. <eunosium-1128@live.jp>
2024-09-13 17:38:15 +0900

    Revert comment out

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -19,7 +19,7 @@ cd ${SRC_DIR}/maxit-v10.1/src && \
 sed -i.bak "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${RCSBROOT}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
 
 cd "${SRC_DIR}/cifparse-obj-v7.0" && sed -i.bak 's|mv |cp |g' Makefile
-# cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${RCSBROOT}/data/binary|g" binary.sh
+cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${RCSBROOT}/data/binary|g" binary.sh
 
 cd "${SRC_DIR}" && make binary -j${CPU_COUNT}
 
@@ -28,11 +28,6 @@ unlink "${BUILD_PREFIX}/bin/g++"
 
 install -d "${PREFIX}/bin"
 install "${SRC_DIR}/bin/"* "${PREFIX}/bin"
-
-# install -d "${PREFIX}/data"
-# find "${SRC_DIR}/data" -type d -exec install -d "${PREFIX}/data/{}" \;
-# find "${SRC_DIR}/data" -type f -exec install -m 644 "{}" "${PREFIX}/data/{}" \;
-
 cp -r "${SRC_DIR}/data" "${PREFIX}/data"
 
 echo '"${PREFIX}/data"'
