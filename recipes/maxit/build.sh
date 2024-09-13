86684ff0a2b802f8a75443592b10e1a3053c50bd
UENO, M. <eunosium-1128@live.jp>
2024-09-13 20:09:40 +0900

    Change install dir

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,7 +5,8 @@ set -exo pipefail
 # Disable parallel build
 export CPU_COUNT=1
 
-export RCSBROOT="${SRC_DIR}"
+# export RCSBROOT="${SRC_DIR}"
+install -d "${PREFIX}/bin"
 
 # To pass CI test on Linux
 if [[ "$(uname -s)" == "Linux" ]]; then
@@ -16,15 +17,21 @@ ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
 cd ${SRC_DIR}/maxit-v10.1/src && \
-sed -i.bak "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${RCSBROOT}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
+sed -i.bak "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${PREFIX}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
 
 cd "${SRC_DIR}/cifparse-obj-v7.0" && sed -i.bak 's|mv |cp |g' Makefile
-cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${RCSBROOT}/data/binary|g" binary.sh
+cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${PREFIX}/data/binary|g" binary.sh
 cd "${SRC_DIR}" && make binary -j${CPU_COUNT}
 
 unlink "${BUILD_PREFIX}/bin/gcc"
 unlink "${BUILD_PREFIX}/bin/g++"
 
-install -d "${PREFIX}/bin"
-install ${SRC_DIR}/bin/* "${PREFIX}/bin"
-cp -r "${SRC_DIR}/data" "${PREFIX}/data"
+# install -d "${PREFIX}/bin"
+# install ${SRC_DIR}/bin/* "${PREFIX}/bin"
+# cp -r "${SRC_DIR}/data" "${PREFIX}/data"
+
+echo '"${PREFIX}/bin",
+ls -l ${PREFIX}/bin
+
+echo '"${PREFIX}/data"'
+ls -l ${PREFIX}/data
