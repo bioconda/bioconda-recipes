521b89a5a05280151e924e9f6ea881cfc76e5aa3
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-09-13 11:50:43 +0300

    r-facets: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/cnv_facets/build.sh
+++ recipes/cnv_facets/build.sh
@@ -7,8 +7,6 @@ FACETS_GIT_REF=`grep -P ' *^FACETS_REF' install/install_pkgs.R \
 | tr "'" '"' \
 | sed 's/FACETS_REF"//; s/".*//'`
 
-$R -e "devtools::install_github('mskcc/facets', ref= '$FACETS_GIT_REF', lib= NULL, repos= 'https://cran.r-project.org')"
-
 mkdir -p $PREFIX/bin
 chmod a+x bin/cnv_facets.R
 cp bin/cnv_facets.R $PREFIX/bin/
