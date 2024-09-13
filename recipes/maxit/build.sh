e451adae44a57b40a783df46289d923e33930120
UENO, M. <eunosium-1128@live.jp>
2024-09-13 16:12:54 +0900

    Add code for debug

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 # Disable parallel build
 export CPU_COUNT=1
 
-export RCSBROOT="${SRC_DIR}"
+export RCSBROOT="${PREFIX}"
 
 # To pass CI test on Linux-x86_64 platform
 if [[ "$(uname -s)" == "Linux" && "$(uname -m)" == "x86_64" ]]; then
@@ -19,7 +19,7 @@ cd ${SRC_DIR}/maxit-v10.1/src && \
 sed -i.bak "s|rcsbroot = getenv(\"RCSBROOT\")|rcsbroot = \"${RCSBROOT}\"|g" maxit.C process_entry.C generate_assembly_cif_file.C
 
 cd "${SRC_DIR}/cifparse-obj-v7.0" && sed -i.bak 's|mv |cp |g' Makefile
-cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${RCSBROOT}/data/binary|g" binary.sh
+# cd "${SRC_DIR}" && sed -i.bak "s|./data/binary|${RCSBROOT}/data/binary|g" binary.sh
 cd "${SRC_DIR}" && make binary -j${CPU_COUNT}
 
 unlink "${BUILD_PREFIX}/bin/gcc"
@@ -31,3 +31,6 @@ install ${SRC_DIR}/bin/* "${PREFIX}/bin"
 install -d "${PREFIX}/data"
 find "${SRC_DIR}/data" -type d -exec install -d "${PREFIX}/data/{}" \;
 find "${SRC_DIR}/data" -type f -exec install -m 644 "{}" "${PREFIX}/data/{}" \;
+
+echo '"${PREFIX}/data"'
+ls -l "${PREFIX}/data"
