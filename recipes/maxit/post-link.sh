0aeebe76fd9c6743a9f0fc9bdcd9cdd8c7206e2a
UENO, M. <eunosium-1128@live.jp>
2024-09-15 04:44:19 +0900

    Fix post-link.sh

--- recipes/maxit/post-link.sh
+++ recipes/maxit/post-link.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-mkdir -p "${PREFIX}/etc/conda/{activate.d,deactivate.d}"
-if [ -n "${BASH_VERSION}" ] || [ -n "${ZSH_VERSION}" ]; then
-    echo $'#!/bin/sh\n\n'"export RCSBROOT=${PREFIX}"$'\n' > "${PREFIX}/etc/conda/activate.d/env_vars.sh"
-    echo $'#!/bin/sh\n\n'"unset RCSBROOT"$'\n' > "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
-elif [ -n "${FISH_VERSION}" ]; then
-    echo $'#!/usr/bin/env fish\n\n'"set -gx RCSBROOT ${PREFIX}"$'\n' > "${PREFIX}/etc/conda/activate.d/env_vars.fish"
-    echo $'#!/usr/bin/env fish\n\n'"set -e RCSBROOT ${PREFIX}"$'\n' > "${PREFIX}/etc/conda/deactivate.d/env_vars.fish"
-else
-    echo "Unsupported shell: ${SHELL}"
-    exit 1
-fi
+mkdir -p ${PREFIX}/etc/conda/{activate,deactivate}.d
+
+# For POSIX Shell(e.g. Bash, Zsh)
+echo $'#!/bin/sh\n\n'"export RCSBROOT=${PREFIX}"$'\n' > "${PREFIX}/etc/conda/activate.d/env_vars.sh"
+echo $'#!/bin/sh\n\n'"unset RCSBROOT"$'\n' > "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+
+# For Fish
+echo $'#!/usr/bin/env fish\n\n'"set -gx RCSBROOT ${PREFIX}"$'\n' > "${PREFIX}/etc/conda/activate.d/env_vars.fish"
+echo $'#!/usr/bin/env fish\n\n'"set -e RCSBROOT ${PREFIX}"$'\n' > "${PREFIX}/etc/conda/deactivate.d/env_vars.fish"
 
 echo "RCSBROOT will be set to ${PREFIX} when this conda environment is activated."
