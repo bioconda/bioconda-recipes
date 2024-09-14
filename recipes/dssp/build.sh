9f0367d67556f6faa0efcbb63d3899aac7dabc41
berkeucar <bucar.cs@gmail.com>
2024-09-13 23:00:05 -0700

    Added DSSP package

---
+++ recipes/dssp/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Link dynamically
+sed -i -e 's/\-static //' makefile
+
+echo "DEST_DIR=${PREFIX}" > make.config
+echo "MAN_DIR=${PREFIX}/share/man/man1" >> make.config
+
+echo "BOOST_INC_DIR=${BUILD_PREFIX}/include" >> make.config
+echo "BOOST_LIB_DIR=${BUILD_PREFIX}/lib" >> make.config
+# Use conda-provided cxx
+sed -i -e 's/^CXX/#CXX/' makefile
+
+# clang++ won't find std::tuple unless in C++11 mode;
+# don't compile with anything newer than C++11 since we use the removed
+# std::unary_function
+if [ `uname -s` = "Darwin" ]; then
+  CFLAGS="-std=c++11 -Wno-enum-constexpr-conversion ${CFLAGS}"
+fi
+
+make -j4 install
