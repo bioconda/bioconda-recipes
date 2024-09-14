8e5e3c2e5b45380c05d68ba344b39e68c0950534
UENO, M. <eunosium-1128@live.jp>
2024-09-15 01:46:25 +0900

    Update post-link.sh

--- recipes/maxit/post-link.sh
+++ recipes/maxit/post-link.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 mkdir -p "${PREFIX}/etc/conda/{activate.d,deactivate.d}"
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
+if [ -n "${BASH_VERSION}" ] || [ -n "${ZSH_VERSION}" ]; then
     echo $'#!/bin/sh\n\n'"export RCSBROOT=${PREFIX}"$'\n' > "${PREFIX}/etc/conda/activate.d/env_vars.sh"
     echo $'#!/bin/sh\n\n'"unset RCSBROOT"$'\n' > "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
-elif [ -n "$FISH_VERSION" ]; then
+elif [ -n "${FISH_VERSION}" ]; then
     echo $'#!/usr/bin/env fish\n\n'"set -gx RCSBROOT ${PREFIX}"$'\n' > "${PREFIX}/etc/conda/activate.d/env_vars.fish"
     echo $'#!/usr/bin/env fish\n\n'"set -e RCSBROOT ${PREFIX}"$'\n' > "${PREFIX}/etc/conda/deactivate.d/env_vars.fish"
 else
-    echo "Unsupported shell: $SHELL"
+    echo "Unsupported shell: ${SHELL}"
     exit 1
 fi
 
