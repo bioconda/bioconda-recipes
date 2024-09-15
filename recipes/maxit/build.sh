11096640308b548442bf5e864b4df120d7503cfa
UENO, M. <eunosium-1128@live.jp>
2024-09-15 20:09:09 +0900

    Change C/C++ compiler path to build

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,14 +5,14 @@ set -exo pipefail
 # Disable parallel build
 export CPU_COUNT=1
 
-# To pass CI test on Linux
-if [[ "$(uname -s)" == "Linux" ]]; then
-    free -h
-    ulimit -v 2000000000
-fi
+# # To pass CI test on Linux
+# if [[ "$(uname -s)" == "Linux" ]]; then
+#     free -h
+#     ulimit -v 2000000000
+# fi
 
-ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
-ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
+ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
+ln -s "${CXX}" "${BUILD_PREFIX}/bin/g++"
 
 cd "${SRC_DIR}/cifparse-obj-v7.0"
 sed -i.bak 's|mv |cp |g' Makefile
