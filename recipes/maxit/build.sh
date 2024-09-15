cdc2d13825bdbe9d6f6311dc303f33a9c9ad650d
UENO, M. <eunosium-1128@live.jp>
2024-09-15 15:50:14 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -13,10 +13,16 @@ export CPU_COUNT=1
 ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
-cd "${SRC_DIR}/cifparse-obj-v7.0" && sed -i.bak 's|mv |cp |g' Makefile
+cd "${SRC_DIR}/cifparse-obj-v7.0"
+sed -i.bak 's|mv |cp |g' Makefile
 rm *.bak
 
-cd "${SRC_DIR}" && make binary -j${CPU_COUNT}
+cd ${SRC_DIR}
+sed -i.bak "s|./data/binary|${SRC_DIR}/data/binary|g" binary.sh
+rm -rf *.bak
+
+cd "${SRC_DIR}"
+make binary "-j${CPU_COUNT}"
 
 unlink "${BUILD_PREFIX}/bin/gcc"
 unlink "${BUILD_PREFIX}/bin/g++"
@@ -24,4 +30,3 @@ unlink "${BUILD_PREFIX}/bin/g++"
 install -d "${PREFIX}/bin"
 install ${SRC_DIR}/bin/* "${PREFIX}/bin"
 cp -r "${SRC_DIR}/data" "${PREFIX}/data"
-
