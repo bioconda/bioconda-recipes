281f93ab68b49e7457851ad23caec03ac4c02a01
UENO, M. <eunosium-1128@live.jp>
2024-09-15 18:55:30 +0900

    Change `ulimit` target size to unlimited

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -7,7 +7,7 @@ export CPU_COUNT=1
 
 # To pass CI test on Linux
 if [[ "$(uname -s)" == "Linux" ]]; then
-    ulimit -v unlimited
+    ulimit -v 500000000
 fi
 
 ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
