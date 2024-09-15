dcc6fb03d4fdb1c3a1f5051d4758ade75b364dca
wenhai <zhangwenhai0182@outlook.com>
2024-09-15 21:29:08 +0800

    add hylight

---
+++ recipes/hylight/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bashscript
+
+if [ "$(uname)" == "Darwin" ]; then
+    cd ${SRC_DIR}
+    cp ${SRC_DIR}/HyLight ${PREFIX}
+else
+    cd ${SRC_DIR}
+    ${PYTHON} -m pip install . -vvv
+    hylight_conda_dir=${PREFIX}/lib/python3.6/site-packages/HyLight
+    chmod +x $hylight_conda_dir/script/*py
+    cd ${SRC_DIR}/HyLight/tools/miniasm
+    make clean
+    make
+    cp ${SRC_DIR}/HyLight/tools/miniasm/miniasm $hylight_conda_dir/tools/miniasm
+    cd ${SRC_DIR}/HyLight/tools/HaploConduct
+    make clean
+    make CPPFLAGS="-Wall -fopenmp -std=c++11 -g -O2 -L ${PREFIX}/lib -I ${PREFIX}/include"
+    cp bin quick-cliques/bin $hylight_conda_dir/tools/HaploConduct -r
+fi
