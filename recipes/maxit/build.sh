bbdc88c85be9ea8aca59613f13d415d457664836
UENO, M. <eunosium-1128@live.jp>
2024-09-16 12:48:25 +0900

    Add ccache as dependency for build on Linux

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,16 +5,14 @@ set -exo pipefail
 # Disable parallel build
 export CPU_COUNT=1
 
+export PATH="${BUILD_PREFIX}/lib/ccache:$PATH"
+
 ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
 cd "${SRC_DIR}/cifparse-obj-v7.0"
 sed -i.bak 's|mv |cp |g' Makefile
 rm *.bak
-
-# cd "${SRC_DIR}"
-# sed -i.bak "s|./data/binary|${SRC_DIR}/data/binary|g" binary.sh
-# rm *.bak
 
 cd "${SRC_DIR}"
 make binary "-j${CPU_COUNT}"
