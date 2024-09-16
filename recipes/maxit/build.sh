86a2e1125e26b0e8285353b31f3e6bd20058d909
UENO, M. <eunosium-1128@live.jp>
2024-09-16 16:57:28 +0900

    Update build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 # Disable parallel build
 export CPU_COUNT=1
 
-ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
+ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
 cd "${SRC_DIR}/cifparse-obj-v7.0"
