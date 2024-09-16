5ac2a027d89a273839c49510d079bfda47d6dcb1
Jason Shao <Jason.Shao@dal.ca>
2024-09-16 15:16:23 -0300

    feat: switched to stable release for acc2tax

--- recipes/eukfinder/build.sh
+++ recipes/eukfinder/build.sh
@@ -9,12 +9,10 @@ git clone https://github.com/richardmleggett/acc2tax.git
 echo "acc2tax downloaded successfully"
 
 # Build acc2tax
-cd acc2tax
 # According to https://bioconda.github.io/contributor/troubleshooting.html
 # and https://github.com/bioconda/bioconda-recipes/pull/49360#discussion_r1686187284
-$(which "$CC") -o acc2tax acc2tax.c
+$("$CC") -o acc2tax acc2tax.c
 cp acc2tax $PREFIX/bin/
-cd ..
 
 chmod +x $PREFIX/bin/plast
 chmod +x $PREFIX/bin/acc2tax
