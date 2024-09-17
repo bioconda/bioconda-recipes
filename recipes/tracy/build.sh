a753f43fc976a8261ec0418723b6a505359533a8
Tobias Rausch <rauschtobi@gmail.com>
2024-09-17 09:57:55 +0200

    cmake

--- recipes/tracy/build.sh
+++ recipes/tracy/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CXXFLAGS="-D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/ -std=c++17" make -j${CPU_COUNT} CXX="${CXX}" prefix="${PREFIX}" install
+CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS -I${SRC_DIR}/src/sdslLite/include -L${SRC_DIR}/src/sdslLite/lib -I${PREFIX}/include -L${PREFIX}/lib -Isrc/jlib/ -std=c++17" make -j${CPU_COUNT} CXX="${CXX}" prefix="${PREFIX}" install
