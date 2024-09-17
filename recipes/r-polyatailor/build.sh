c04314b6ee79b4aa41d1d1b1f17febf15c3b46e2
Fabian Boehm <boehmf@cip.ifi.lmu.de>
2024-09-18 00:54:08 +0200

    fixed movapa, devemf and eoffice

--- recipes/r-polyatailor/build.sh
+++ recipes/r-polyatailor/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
+
+# Unzip the main polyAtailor source to a temporary directory
+unzip PolyAtailor-master.zip -d /tmp/polyAtailor
+
+# Unzip the ggmsa.zip dependency located inside the polyAtailor package
+unzip /tmp/polyAtailor/patch/ggmsa.zip -d /tmp/ggmsa
+
+# Install the ggmsa package
+$R CMD INSTALL /tmp/ggmsa
+
+# Install the main polyAtailor package
 $R CMD INSTALL --build .
