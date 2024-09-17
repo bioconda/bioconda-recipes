7cc6dc7e63c0ef68ac42d27946acc9d12f5c91c7
UENO, M. <eunosium-1128@live.jp>
2024-09-17 23:44:35 +0900

    Remove unused code in build.sh

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -8,11 +8,6 @@ export CPU_COUNT=1
 ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
-cd "${SRC_DIR}/cifparse-obj-v7.0"
-sed -i.bak 's|mv |cp |g' Makefile
-rm *.bak
-
-cd "${SRC_DIR}"
 make binary "-j${CPU_COUNT}"
 
 unlink "${BUILD_PREFIX}/bin/gcc"
