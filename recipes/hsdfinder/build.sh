7d9a028f836850fa891c303051291adb7e17a7ad
Xi Zhang <13602037587@163.com>
2024-09-18 16:34:28 -0600

    Add files via upload

---
+++ recipes/hsdfinder/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+
+cp HSDFinder.py $PREFIX/bin/hsdfinder
+
+cp HSD_to_KEGG.py $PREFIX/bin/hsd_to_kegg
+cp operation.py $PREFIX/bin/operation.py
+cp pfam.py $PREFIX/bin/pfam.py
+cp KO_database.keg $PREFIX/bin/KO_database.keg
+
+chmod a+x $PREFIX/bin/hsdfinder
+chmod a+x $PREFIX/bin/hsd_to_kegg
+chmod a+x $PREFIX/bin/operation.py
+chmod a+x $PREFIX/bin/pfam.py
+
