2a991ddde6009816820e93250ed8147b24e454e1
Fabian Boehm <boehmf@cip.ifi.lmu.de>
2024-09-18 12:24:47 +0200

    corrected ggmsa build directory. polyAtailor builds correctly now

--- recipes/r-polyatailor/build.sh
+++ recipes/r-polyatailor/build.sh
@@ -2,14 +2,11 @@
 
 export DISABLE_AUTOBREW=1
 
-# Unzip the main polyAtailor source to a temporary directory
-unzip PolyAtailor-master.zip -d /tmp/polyAtailor
-
 # Unzip the ggmsa.zip dependency located inside the polyAtailor package
-unzip /tmp/polyAtailor/patch/ggmsa.zip -d /tmp/ggmsa
+unzip patch/ggmsa.zip -d /tmp/ggmsa
 
 # Install the ggmsa package
-$R CMD INSTALL /tmp/ggmsa
+$R CMD INSTALL /tmp/ggmsa/ggmsa
 
 # Install the main polyAtailor package
 $R CMD INSTALL --build .
