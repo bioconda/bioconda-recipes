5bd4e15e861c30b880edbdf5b8b641a9d5fccd9d
UENO, M. <eunosium-1128@live.jp>
2024-09-18 12:26:59 +0900

    Reduce unlimited value for build on Linux

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -9,7 +9,7 @@ ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
 if [[ $(uname -s) == "Linux" ]]; then
-    ulimit -v 6000000
+    ulimit -v 2000000
 fi
 
 make binary "-j${CPU_COUNT}"
