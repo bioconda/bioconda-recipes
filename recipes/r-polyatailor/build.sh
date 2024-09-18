88044c052d2870c1dd2335a499322890c4d63cea
Fabian Boehm <boehmf@cip.ifi.lmu.de>
2024-09-18 12:50:07 +0200

    removed and added comments, removed all unnecessary version constraints

--- recipes/r-polyatailor/build.sh
+++ recipes/r-polyatailor/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 export DISABLE_AUTOBREW=1
-
-# Unzip the ggmsa.zip dependency located inside the polyAtailor package
-unzip patch/ggmsa.zip -d /tmp/ggmsa
-
-# Install the ggmsa package
-$R CMD INSTALL /tmp/ggmsa/ggmsa
-
-# Install the main polyAtailor package
+unzip patch/ggmsa.zip -d /tmp
+$R CMD INSTALL /tmp/ggmsa
 $R CMD INSTALL --build .
