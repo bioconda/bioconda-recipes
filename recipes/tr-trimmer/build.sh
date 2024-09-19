4add3526dcd9bde4c9d95560e8b37cabe70c0f15
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-09-18 19:27:20 -0700

    Add `--no-track` no `cargo install`

--- recipes/tr-trimmer/build.sh
+++ recipes/tr-trimmer/build.sh
@@ -4,4 +4,4 @@ set -xe
 
 # build statically linked binary with Rust
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-RUST_BACKTRACE=1 cargo install --verbose --locked --root $PREFIX --path .
+RUST_BACKTRACE=1 cargo install --verbose --locked --no-track --root $PREFIX --path .
