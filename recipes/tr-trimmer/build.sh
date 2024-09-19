527826d4a4f705bf67d2caaec27e1fc3f2c29115
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-09-18 19:02:33 -0700

    Add `cargo-bundle-licenses` to build script

--- recipes/tr-trimmer/build.sh
+++ recipes/tr-trimmer/build.sh
@@ -3,4 +3,5 @@
 set -xe
 
 # build statically linked binary with Rust
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 RUST_BACKTRACE=1 cargo install --verbose --locked --root $PREFIX --path .
