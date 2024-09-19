44a5e889f93efd38e1c5454660fdd8213b198f09
UENO, M. <eunosium-1128@live.jp>
2024-09-19 15:28:30 +0900

    Use multiple sources

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -5,12 +5,10 @@ set -exo pipefail
 # Disable parallel build
 export CPU_COUNT=1
 
+ls -l
+
 ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
 ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
-
-# if [[ $(uname -s) == "Linux" ]]; then
-#     ulimit -v 2000000
-# fi
 
 make binary "-j${CPU_COUNT}"
 
