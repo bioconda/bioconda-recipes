61488789be4f18f6b0e231e1ef5da3b0015315bc
Patrik Smeds <patrik.smeds@gmail.com>
2024-09-19 15:17:37 -0400

    Update build.sh

--- recipes/gfastats/build.sh
+++ recipes/gfastats/build.sh
@@ -3,8 +3,6 @@
 export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
 export CXXFLAGS="-g -Wall -I$PREFIX/include -O3  -I$SRC_DIR/include -I$SRC_DIR/include/zlib -std=gnu++14 -lstdc++"
 
-git submodule update --init --recursive
-
 make -j CXX=$CXX
 
 mkdir -p $PREFIX/bin/
