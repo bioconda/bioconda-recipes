a620bd03131ad2f8a0e84c96653bf9cff8b0725d
LiliyaBioinf <khadeeva.liliya@gmail.com>
2024-09-19 15:25:29 +0200

    Update build.sh

--- recipes/pyscenic/build.sh
+++ recipes/pyscenic/build.sh
@@ -5,9 +5,6 @@ if [ -z "${PYTHON}" ]; then
   PYTHON=$(which python)
 fi
 
-pushd ${SRC_DIR}/multiprocessing_on_dill-3.5.0a4
-${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
-popd
 
 pushd ${SRC_DIR}/pyscenic-0.12.1
 ${PYTHON} -m pip install . --use-pep517 --no-deps --ignore-installed -vv
