f9c610a021401824944efcbee205f6c0915d9d96
Nae-Chyun Chen <s93rm6@gmail.com>
2024-09-22 23:16:31 -0700

    Update build.sh

--- recipes/leviosam2/build.sh
+++ recipes/leviosam2/build.sh
@@ -1,5 +1,8 @@
 mkdir -p build;
 cd build;
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ${SRC_DIR}
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_LIBRARY_PATH=${PREFIX}/lib \
+  -DCMAKE_INCLUDE_PATH=${PREFIX}/include \
+  ${SRC_DIR}
 make;
 make install
