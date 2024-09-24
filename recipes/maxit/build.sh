754beb84f750fdd1519d1e6ecc33cb66380cb5aa
UENO, M. <eunosium-1128@live.jp>
2024-09-25 01:57:44 +0900

    Fix bug

--- recipes/maxit/build.sh
+++ recipes/maxit/build.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-export INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
-export RCSBROOT=${SRC_DIR}
+set -exo pipefail
 
-ln -s ${CC} ${BUILD_PREFIX}/bin/gcc
-ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-ln -s ${CXX} ${BUILD_PREFIX}/bin/cxx
-ln -s ${GXX} ${BUILD_PREFIX}/bin/g++
+# Disable parallel build
+export CPU_COUNT=1
 
-cd ${RCSBROOT}/cifparse-obj-v7.0
-sed -i 's/mv /cp /g' Makefile
+ln -s "${CC_FOR_BUILD}" "${BUILD_PREFIX}/bin/gcc"
+ln -s "${CXX_FOR_BUILD}" "${BUILD_PREFIX}/bin/g++"
 
-cd ${RCSBROOT}
-sed -i 's|./data/binary|\${RCSBROOT}/data/binary|g' binary.sh
+make binary "-j${CPU_COUNT}"
 
-cd ${RCSBROOT}
-make binary -j"${CPU_COUNT}"
+unlink "${BUILD_PREFIX}/bin/gcc"
+unlink "${BUILD_PREFIX}/bin/g++"
 
-unlink ${BUILD_PREFIX}/bin/gcc
-unlink ${BUILD_PREFIX}/bin/c++
-unlink ${BUILD_PREFIX}/bin/cxx
-unlink ${BUILD_PREFIX}/bin/g++
+install -d "${PREFIX}/bin"
+install ${SRC_DIR}/bin/* "${PREFIX}/bin"
+cp -r "${SRC_DIR}/data" "${PREFIX}/data"
 
-install -d ${PREFIX}/bin
-install ${RCSBROOT}/bin/* ${PREFIX}/bin
-install -d ${PREFIX}/data
-install -m 644 ${RCSBROOT}/data/* ${PREFIX}/data
