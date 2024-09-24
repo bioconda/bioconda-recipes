5ec8d6a71efa5aba45ad02c1945003c0190fbdc1
mencian <joshua.zhuang@yahoo.com>
2024-09-24 01:17:58 -0500

    update to 2.1.1

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -6,11 +6,10 @@ export INCLUDES="-I{PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-export CFLAGS="${CFLAGS} -O3"
+make CC="${CC} -O3 ${LDFLAGS}" CFLAGS="${CFLAGS}" CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3 -I{PREFIX}/include" \
+	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	INCLUDES="${INCLUDES}" COMMONLIBS="${COMMONLIBS} -L${PREFIX}/lib" -j"${CPU_COUNT}"
 
-make CC="${CC} -O3 ${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS} -O3" \
-	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -I${PREFIX}/include" \
-	INCLUDES="${INCLUDES}" COMMONLIBS="${COMMONLIBS} -L${PREFIX}/lib" -j "${CPU_COUNT}"
-
-chmod 755 MuSE
+chmod 0755 MuSE
 cp -f MuSE "${PREFIX}/bin"
