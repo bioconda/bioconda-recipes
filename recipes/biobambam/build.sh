570e940692b9440753e138a75e18d5735a4f8f5d
mencian <joshua.zhuang@yahoo.com>
2024-09-24 02:18:21 -0500

    Update biobambam to v2.0.185

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
 set -eu
-./configure --prefix=${PREFIX}
+
+./configure --prefix="${PREFIX}" CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 make install
