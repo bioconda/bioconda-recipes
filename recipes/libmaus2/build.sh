fc246fd7699ccc3614396556c7f62d20cbf2119e
mencian <joshua.zhuang@yahoo.com>
2024-09-24 02:32:02 -0500

    Update libmaus2 to v2.0.183

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -eu
+
 export LIBS="-lstdc++fs -lcurl"
 
-./configure --prefix $PREFIX --with-snappy --with-io_lib
+./configure --prefix "${PREFIX}" \
+	CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
+	--with-snappy --with-io_lib
 
 cat config.log
 
