cb0a60176bcc1180a60fe47aa8ad57fe05b40a58
mencian <joshua.zhuang@yahoo.com>
2024-09-24 21:49:17 -0500

    fix arch_opts

--- recipes/kallisto/build.sh
+++ recipes/kallisto/build.sh
@@ -8,6 +8,20 @@ autoreconf --force --install --verbose
 ./configure
 cd ../.. || exit 1
 
+case $(uname -m) in
+    x86_64)
+        ARCH_OPTS="-DENABLE_AVX2=OFF -DCOMPILATION_ARCH=OFF"
+        ;;
+    aarch64)
+        ARCH_OPTS="-DCOMPILATION_ARCH=OFF"
+        ;;
+    arm64)
+	ARCH_OPTS="-DENABLE_AVX2=OFF -DCOMPILATION_ARCH=OFF"
+	;;
+    *)
+        ;;
+esac
+
 if [[ `uname` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
@@ -18,7 +32,7 @@ cmake -S. -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DUSE_HDF5=ON -DUSE_BAM=ON -DBUILD_FUNCTESTING=ON -DMAX_KMER_SIZE=64 \
 	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	-DENABLE_AVX2=OFF -DCOMPILATION_ARCH=OFF \
+	"${ARCH_OPTS}" \
 	"${CONFIG_ARGS}"
 
 cmake --build build --target install -v
