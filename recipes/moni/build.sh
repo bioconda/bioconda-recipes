3b4f21b6acf1952591dbe49a500cc7d17c6e0f18
Martin Grigorov <martin-g@users.noreply.github.com>
2024-09-25 10:06:47 +0300

    Use c++14

--- recipes/moni/build.sh
+++ recipes/moni/build.sh
@@ -3,14 +3,16 @@
 export CPATH=$PREFIX/include:$CPATH
 export LIBRARY_PATH=$PREFIX/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export CXXFLAGS="${CXXFLAGS} -std=c++14"
 
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_CXX_FLAGS=$CXXFLAGS \
       -DCMAKE_INCLUDE_PATH=$PREFIX/include \
       -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
       -DZLIB_ROOT=$PREFIX \
       ..
-make VERBOSE=1
+make -j ${CPU_COUNT} VERBOSE=1
 make install
