69007c9e34d964ecf3e5bcf7afc6ae5519a658d7
AndreaGuarracino <andreaguarracino@outlook.com>
2024-09-24 22:29:53 -0500

    update build.sh

--- recipes/moni/build.sh
+++ recipes/moni/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+export CPATH=$PREFIX/include:$CPATH
+export LIBRARY_PATH=$PREFIX/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+      -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
+      -DZLIB_ROOT=$PREFIX \
+      ..
+make VERBOSE=1
 make install
