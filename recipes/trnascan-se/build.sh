1cc92b8ad22f6f0a91cea8947236652cef39ceed
mencian <joshua.zhuang@yahoo.com>
2024-09-26 15:12:48 -0500

    trnascan-se: add aarch64/arm64 build

--- recipes/trnascan-se/build.sh
+++ recipes/trnascan-se/build.sh
@@ -7,7 +7,10 @@ export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include ${LDFLAGS}"
 
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' tRNAscan-SE.src
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' src/instman.pl
+rm -rf *.bak
+rm -rf src/*.bak
 
+autoreconf -if
 ./configure CC="${CC}" CFLAGS="${CFLAGS}" --prefix="${PREFIX}"
 
 make
