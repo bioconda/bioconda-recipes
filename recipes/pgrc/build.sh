fe8a5a949e80e41beb2c7e26e0db06203d5f5060
kowallus <kowallus@o2.pl>
2024-09-26 17:18:10 +0200

    updates & simplifications

--- recipes/pgrc/build.sh
+++ recipes/pgrc/build.sh
@@ -2,18 +2,13 @@
 
 mkdir -p $PREFIX/bin
 
-if [[ "$(uname)" == "Linux" ]]; then
-
 mkdir build
 cd build
 export CPATH=${BUILD_PREFIX}/include
 export CXXPATH=${BUILD_PREFIX}/include
-export CFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
 export CXXFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$BUILD_PREFIX/lib"
 cmake ..
-make PgRC
-
-fi
+make -j ${CPU_COUNT} PgRC
 
 cp PgRC $PREFIX/bin
