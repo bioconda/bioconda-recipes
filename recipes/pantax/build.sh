15006dd061bd95a1b31a60fc1aeda0c9037eef2f
Wenhai Zhang <128364515+zwh82@users.noreply.github.com>
2024-09-27 12:05:28 +0800

    Update build.sh

--- recipes/pantax/build.sh
+++ recipes/pantax/build.sh
@@ -3,16 +3,21 @@
 if [ "$(uname)" == "Darwin" ]; then
     cp -rf $SRC_DIR $PREFIX
 else
-    cd gurobi11
+    cd ${PREFIX}/gurobi11
     ${PYTHON} -m pip install gurobipy-11.0.2-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl
     rm gurobipy-11.0.2-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl
+    
     mkdir -p ${PREFIX}/bin/tools
-    cd ../vg
+    cd ${PREFIX}/vg
     cp vg* ${PREFIX}/bin/tools/vg
+    chmod +x ${PREFIX}/bin/tools/vg
+    
     cd $SRC_DIR/scripts
     chmod +x pantax
-    cp $SRC_DIR/scripts/pantax ${PREFIX}/bin
+    chmod +x data_preprocessing
+    cp $SRC_DIR/scripts/pantax $SRC_DIR/scripts/data_preprocessing ${PREFIX}/bin
     cp $SRC_DIR/scripts/*py ${PREFIX}/bin
+    
     cd $SRC_DIR/tools/fastix
     cargo install fastix --root ./
     cp $SRC_DIR/tools/fastix/bin/fastix ${PREFIX}/bin/tools
