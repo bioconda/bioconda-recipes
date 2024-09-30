24b8b3cb746d99946bf383fd448847b058ca326d
rlibouban <romane.libouban@irisa.fr>
2024-09-30 11:27:12 +0200

    add learnMSA

--- recipes/tiberius/build.sh
+++ recipes/tiberius/build.sh
@@ -5,3 +5,11 @@ cp -R $SRC_DIR/bin/* ${PREFIX}/bin/
 chmod +x ${PREFIX}/bin/tiberius.py
 ln -s ${PREFIX}/bin/tiberius.py ${PREFIX}/bin/tiberius
 sed -i '1s|^|#!/usr/bin/env python3\n|' ${PREFIX}/bin/tiberius
+
+#Temporary: until the specific branch of learnMSA compatible with Tiberius is merged
+#Adding learnMSA
+git clone https://github.com/Gaius-Augustus/learnMSA
+cd learnMSA
+git checkout parallel
+pip install .
+cd ..
