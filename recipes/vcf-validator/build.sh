fc9abc1ab17f104df63802ec3db0f8afa71ea1f3
tcezard <tcezard@ebi.ac.uk>
2024-09-30 11:36:56 +0100

    Add support for arm64 on OS-X Add additional comment as per review

--- recipes/vcf-validator/build.sh
+++ recipes/vcf-validator/build.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
 # Install additional dependencies
 if [ -z ${OSX_ARCH+x} ]; then
   ./install_dependencies.sh linux
-  mkdir build
-  cd build
-  cmake -G "Unix Makefiles" ..
 else
   ./install_dependencies.sh osx
-  mkdir build
-  cd build
-  cmake -G "Unix Makefiles" ..
 fi
-make -j2
-cd ..
-./build/bin/test_validation_suite
+mkdir build || { echo "Failed to create build directory" >&2; exit 1; }
+cd build || { echo "Failed to go into build directory" >&2; exit 1; }
+cmake -G "Unix Makefiles" ..
+make -j2 || { echo "Build failed" >&2; exit 1; }
+cd .. || { echo "Failed to return to parent directory" >&2; exit 1; }
+if ! ./build/bin/test_validation_suite; then
+  echo "Validation suite failed" >&2
+  exit 1
+fi
 echo "Done with vcf-validator"
