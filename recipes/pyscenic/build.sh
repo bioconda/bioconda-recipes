37f36eee0d799e1cf5e7db1d457dea7084b17b73
LiliyaBioinf <khadeeva.liliya@gmail.com>
2024-10-01 13:06:53 +0200

    Update build.sh

--- recipes/pyscenic/build.sh
+++ recipes/pyscenic/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -e  
+set -e
+echo "Current working directory: $(pwd)"
+echo "Listing files in working directory:"
+ls -l
+echo "Applying patch..."
 patch -p1 < ${RECIPE_DIR}/dill_patch.patch
 
+echo "Patch applied, listing files:"
+ls -l
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
