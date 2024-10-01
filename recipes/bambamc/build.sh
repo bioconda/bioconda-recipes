63d8911c69b319a0c76d7fc5a26636003dd7af9d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-01 13:46:49 +0300

    Use newer config.sub/guess to support OSX ARM64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/bambamc/build.sh
+++ recipes/bambamc/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -xe
+
+# use newer config.guess and config.sub that support osx arm64
+cp ${RECIPE_DIR}/config.* .
+
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 ./configure --prefix=${PREFIX}
