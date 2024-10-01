808f5d6be48fc8b765cc91bbd2834035ff92d47d
Daniel Doerr <mail@danieldoerr.de>
2024-10-01 16:47:02 +0200

    Add $PREFIX to copy command

--- recipes/panacus/build.sh
+++ recipes/panacus/build.sh
@@ -3,5 +3,5 @@
 RUST_BACKTRACE=1 CARGO_HOME="${BUILD_PREFIX}/.cargo" cargo build --release
 
 mkdir -p $PREFIX/bin
-cp target/release/panacus $PREFIX/bin 
-cp scripts/panacus-visualize.py $PREFIX/bin/panacus-visualize
+cp $PREFIX/target/release/panacus $PREFIX/bin 
+cp $PREFIX/scripts/panacus-visualize.py $PREFIX/bin/panacus-visualize
