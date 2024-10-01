396f0584d502ade02915f3918bdda6ce08235b51
xyz1396 <1074082710@qq.com>
2024-10-01 17:43:45 -0500

    add sipros

---
+++ recipes/sipros/build.sh
@@ -0,0 +1,13 @@
+set -e
+
+mkdir -p "$PREFIX/bin"
+chmod u+x bin/*
+cp -r bin/* "$PREFIX/bin"
+
+cp -r EnsembleScripts "$PREFIX"
+cp -r V4Scripts "$PREFIX"
+
+cp -r configTemplates "$PREFIX"
+
+cp $RECIPE_DIR/Raxport.sh "$PREFIX/bin/Raxport"
+chmod +x "$PREFIX/bin/Raxport"
