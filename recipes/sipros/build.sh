f1432ba55c5d87c9fe354c33a87606867d545670
Matthias Bernt <m.bernt@ufz.de>
2024-10-01 10:24:45 +0200

    changes as suggested by author
    xref https://github.com/thepanlab/Sipros4/issues/1#issuecomment-2384745312
    
    Co-authored-by: XYZ(YiXiong)<1074082710@qq.com>

--- recipes/sipros/build.sh
+++ recipes/sipros/build.sh
@@ -4,5 +4,13 @@ mkdir -p "$PREFIX/bin"
 chmod u+x bin/*
 cp -r bin/* "$PREFIX/bin"
 
-cp -r EnsembleScripts "$PREFIX/bin"
-cp -r V4Scripts "$PREFIX/bin"
+cp -r EnsembleScripts "$PREFIX"
+cp -r V4Scripts "$PREFIX"
+
+cp -r configTemplates "$PREFIX"
+
+cp $RECIPE_DIR/Raxport.sh "$PREFIX/bin/Raxport"
+chmod +x "$PREFIX/bin/Raxport"
+
+ln -s "$PREFIX"/EnsembleScripts/sipros_psm_tabulating.py "$PREFIX"/bin/sipros_psm_tabulating.py 
+ln -s "$PREFIX"/V4Scripts/sipros_peptides_filtering.py "$PREFIX"/bin/sipros_peptides_filtering.py
