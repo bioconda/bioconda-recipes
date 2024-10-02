8ff39617940d1767c10c51b98351ead9d865d39a
mencian <joshua.zhuang@yahoo.com>
2024-10-02 13:40:03 -0500

    clean up recipe

--- recipes/verkko/build.sh
+++ recipes/verkko/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -ex
 
-# taken from yacrd recipe, see: https://github.com/bioconda/bioconda-recipes/blob/2b02c3db6400499d910bc5f297d23cb20c9db4f8/recipes/yacrd/build.sh
-if [ "$(uname)" == "Darwin" ]; then
+mkdir -p "$PREFIX/bin"
 
-    # apparently the HOME variable isn't set correctly, and circle ci output indicates the following as the home directory
-    export HOME=`pwd`
-    echo "HOME is $HOME"
-    mkdir -p $HOME/.cargo/registry/index/
-fi
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 # on osx we remove the built-in boost and make sure todepend on the system boost
 pushd src
@@ -18,6 +15,5 @@ fi
 make clean && make -j$CPU_COUNT
 popd
 
-mkdir -p "$PREFIX/bin"
 cp -rf bin/* $PREFIX/bin/
 cp -rf lib/* $PREFIX/lib/
