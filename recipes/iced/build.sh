988cb9e2faa18ffb4d75c4026b2ec21d2ca88b8c
mencian <joshua.zhuang@yahoo.com>
2024-10-01 22:16:26 -0500

    Update iced to v0.513

--- recipes/iced/build.sh
+++ recipes/iced/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 find . -type f -name *_.c -exec rm -f {} \;
 $PYTHON build_tools/cythonize.py iced
-$PYTHON -m pip install . --ignore-installed --no-deps -vv
+$PYTHON -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
