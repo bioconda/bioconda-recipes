5ab5cd395b02f3ec0cb1567effae008eb0346a5c
Daniel Doerr <mail@danieldoerr.de>
2024-10-02 06:59:51 +0200

    Maybe this works?

--- recipes/panacus/build.sh
+++ recipes/panacus/build.sh
@@ -2,8 +2,6 @@
 
 mkdir -p $PREFIX/bin
 
-RUST_BACKTRACE=1
-cargo install --no-track --verbose --root "${PREFIX}" --path .
+RUST_BACKTRACE=1 cargo install --no-track --verbose --root "${PREFIX}" --path .
 
-cp $BUILD_PREFIX/.cargo/bin/panacus $PREFIX/bin 
 cp scripts/panacus-visualize.py $PREFIX/bin/panacus-visualize
