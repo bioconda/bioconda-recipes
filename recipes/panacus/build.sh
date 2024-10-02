421546a11d3987c79cb248c406eb96cb2356dac3
mencian <joshua.zhuang@yahoo.com>
2024-10-01 22:29:02 -0500

    add make

--- recipes/panacus/build.sh
+++ recipes/panacus/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -euo
 
-RUST_BACKTRACE=1 CARGO_HOME="${BUILD_PREFIX}/.cargo" cargo install --no-track --verbose --root "${PREFIX}" --path .
-
 mkdir -p $PREFIX/bin
+
+RUST_BACKTRACE=1
+cargo install --no-track --verbose --root "${PREFIX}" --path .
+
 cp $BUILD_PREFIX/.cargo/bin/panacus $PREFIX/bin 
 cp scripts/panacus-visualize.py $PREFIX/bin/panacus-visualize
