e89b8a37fbf95753d854f6b3030b114e19969004
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-03 10:42:43 +0300

    dbghaplo: use a patch for ARM64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/dbghaplo/build.sh
+++ recipes/dbghaplo/build.sh
@@ -2,7 +2,16 @@
 
 set -xeuo
 
+case $(uname -m) in
+    aarch64 | arm64)
+        FEATURES="--no-default-features --features neon"
+        ;;
+    *)
+        FEATURES=""
+        ;;
+esac
+
 # build statically linked binary with Rust
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-RUST_BACKTRACE=1 cargo install --verbose --locked --no-track --root $PREFIX --path .
+RUST_BACKTRACE=1 cargo install --verbose --locked --no-track --root $PREFIX --path . ${FEATURES}
 cp scripts/* $PREFIX/bin
