f47920405639a922945c9aadb577d5876abef52f
Rauf Salamzade <salamzader@gmail.com>
2024-10-03 18:19:28 -0500

    Update build.sh

--- recipes/skder/build.sh
+++ recipes/skder/build.sh
@@ -4,10 +4,9 @@ $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 mkdir -p ${PREFIX}/bin
 
-#export LDFLAGS=
-# -Wl,-headerpad_max_install_names
-${CXX} -std=c++11 -o ${PREFIX}/bin/skDERsum src/skDER/skDERsum.cpp
-${CXX} -std=c++11 -o ${PREFIX}/bin/skDERcore src/skDER/skDERcore.cpp
+export LDFLAGS=
+${CXX} -std=c++11 -Wl,-headerpad_max_install_names -o ${PREFIX}/bin/skDERsum src/skDER/skDERsum.cpp
+${CXX} -std=c++11 -Wl,-headerpad_max_install_names -o ${PREFIX}/bin/skDERcore src/skDER/skDERcore.cpp
 
 chmod +x ${PREFIX}/bin/skDERcore
 chmod +x ${PREFIX}/bin/skDERsum
