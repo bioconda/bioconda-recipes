f2e29db57e35ce794fe02fd0bac16b43b915f907
agudys <adam.gudys@gmail.com>
2024-10-03 15:10:52 +0200

    fix

--- recipes/kmer-db/build.sh
+++ recipes/kmer-db/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ln -s ${CC} gcc
-ln -s ${CXX} g++
-export PATH=$PATH:$(pwd)
+uname_S=`uname -s 2>/dev/null || echo not`
+if [ "$uname_S" == "Darwin" ]; then brew install g++-12;
 
 CFLAGS="$CFLAGS -I${PREFIX}/include"
 LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
 
-make -j${CPU_COUNT} 
+make -j${CPU_COUNT} CXX=g++-12
 install -d "${PREFIX}/bin"
 install  kmer-db "${PREFIX}/bin"
