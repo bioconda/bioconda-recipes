70e2c62928c21756bf66cbeab1b39d3d986bcae0
obaeza16 <baeza@recerca.clinic.cat>
2024-10-03 10:41:04 +0200

    Changes proposed by coderabbit

--- recipes/ega-cryptor/build.sh
+++ recipes/ega-cryptor/build.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e -x
-set -eu -o pipefail
+# set -eu -o pipefail
 
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
 mkdir -p $PREFIX/bin
 cp -Rf * $outdir/
-mv $outdir/ega-cryptor-$PKG_VERSION.jar $outdir/ega-cryptor.jar
+mv $outdir/EGA-Cryptor-$PKG_VERSION/ega-cryptor-$PKG_VERSION.jar $outdir/ega-cryptor.jar
 cp -f $RECIPE_DIR/ega-cryptor.py $outdir/ega-cryptor
+chmod +x $outdir/ega-cryptor
 ln -sf $outdir/ega-cryptor $PREFIX/bin
-chmod 0755 "${PREFIX}/bin/ega-cryptor"
-
