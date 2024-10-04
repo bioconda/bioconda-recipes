92986d7cfaedfe9f03652030fad0da4365ac40df
Alicia A. Evans <108547992+aliciaaevans@users.noreply.github.com>
2024-10-04 16:32:43 -0400

    Update recipes/test/build.sh
    Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

--- recipes/test/build.sh
+++ recipes/test/build.sh
@@ -3,7 +3,7 @@
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-make CC=${CC} CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" all
+make -j ${CPU_COUNT} CC=${CC} CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" all
 mkdir -p $PREFIX/bin
 cp -f seqtk $PREFIX/bin/
 
