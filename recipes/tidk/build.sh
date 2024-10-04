d86b10ca334046f1d03b8a7d233e67b0274178ba
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-04 14:31:33 +0300

    tidk: add aarch64/arm64 builds
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/tidk/build.sh
+++ recipes/tidk/build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -xe
 
 # build statically linked binary with Rust
-C_INCLUDE_PATH=$PREFIX/include OPENSSL_DIR=$PREFIX LIBRARY_PATH=$PREFIX/lib cargo install --verbose --path . --root "${PREFIX}"
+C_INCLUDE_PATH=$PREFIX/include OPENSSL_DIR=$PREFIX LIBRARY_PATH=$PREFIX/lib cargo install --verbose --path . --root "${PREFIX}" --no-track
