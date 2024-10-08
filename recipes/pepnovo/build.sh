6778f8cfa062c979ab5e2a777d77e80911121f9a
Martin Grigorov <martin-g@users.noreply.github.com>
2024-10-08 14:08:06 +0300

    Add -Wno-register to CXXFLAGS for OSX
    Trying to fix:
    ```
    base64.cpp:124:11: error: ISO C++17 does not allow 'register' storage class specifier [-Wregister][0m
    2024-10-08T08:41:28.5270550Z 08:41:27 [32mBIOCONDA INFO[0m (ERR)       int register a;[0m
    ```

--- recipes/pepnovo/build.sh
+++ recipes/pepnovo/build.sh
@@ -6,7 +6,7 @@ cd src/
 
 # Below we add -Wno-narrowing because of the compilation error:
 #   CumulativeSeqProb.h:10:71: error: narrowing conversion of '999999999' from 'int' to 'float' inside { } [-Wnarrowing]
-make -j ${CPU_COUNT} CC="${CXX}" CFLAGS="${CXXFLAGS} -Wno-narrowing " LDFLAGS="${LDFLAGS}"
+make -j ${CPU_COUNT} CC="${CXX}" CFLAGS="${CXXFLAGS} -Wno-narrowing -Wno-register " LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 cp PepNovo_bin ${PREFIX}/bin/pepnovo
