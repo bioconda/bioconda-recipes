aae89e0e0cd2009e5625dd757f2ff89cbf3e8d13
Till Hartmann <till.hartmann@udo.edu>
2024-10-08 07:32:12 +0000

    fix whitespace issue

--- recipes/mehari/build.sh
+++ recipes/mehari/build.sh
@@ -2,7 +2,7 @@
 
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
-export ROCKSDB_LIB_DIR = "/usr/lib/"
-export SNAPPY_LIB_DIR = "/usr/lib/"
+export ROCKSDB_LIB_DIR="/usr/lib/"
+export SNAPPY_LIB_DIR="/usr/lib/"
 
 cargo install --no-track --locked --root "${PREFIX}" --path .
