0c0c132b115cbd9cad39aee0999a2570b4fd6d22
mencian <joshua.zhuang@yahoo.com>
2024-10-08 17:31:26 -0500

    clean up recipe

--- recipes/tefinder/build.sh
+++ recipes/tefinder/build.sh
@@ -1,6 +1,18 @@
-mkdir -p build
-cd build
-cmake ../
-make install
-cp ../bin/* ${PREFIX}/bin
+#!/bin/bash
 
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	"${CONFIG_ARGS}"
+cmake --build build --target install -j "${CPU_COUNT}" -v
