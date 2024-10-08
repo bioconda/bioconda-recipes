85f5fb8318e507ab89b271d082358d2847ea62d9
mencian <joshua.zhuang@yahoo.com>
2024-10-08 16:19:40 -0500

    Build dialign2 on OSX

--- recipes/dialign2/build.sh
+++ recipes/dialign2/build.sh
@@ -5,11 +5,23 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share
 mkdir -p ${PREFIX}/share/dialign2
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib"
+
+if [[ `uname` == "Darwin" ]]; then
+	export CFLAGS="${CFLAGS} -Wno-implicit-int -Wno-implicit-function-declaration"
+else
+	export CFLAGS="${CFLAGS}"
+fi
+
 cd src
 
 sed -i.bak "s|strcpy ( dialign_dir , \"DIALIGN2_DIR\" );|strcpy ( par_dir , \""${PREFIX}"/share/dialign2\" );|g" dialign.c
+rm -rf *.bak
 
-make CC="${CC}"
+make CC="${CC}" CFLAGS="${CFLAGS}" -j"${CPU_COUNT}"
+chmod 0755 dialign2-2
 mv dialign2-2 ${PREFIX}/bin/dialign2-2
 
 cd ../dialign2_dir
