aa33d443438b0d3e3df7c261d5b172ba47332039
XYZ(YiXiong) <1074082710@qq.com>
2024-10-09 11:44:29 -0500

    Update recipes/sipros/copyConfigTemplate.sh
    Co-authored-by: M Bernt <m.bernt@ufz.de>

--- recipes/sipros/copyConfigTemplate.sh
+++ recipes/sipros/copyConfigTemplate.sh
@@ -6,11 +6,25 @@ else
     OUTPUT_PATH=$2
 fi
 
+if [ -z "$CONDA_PREFIX" ]; then
+    echo "Error: CONDA_PREFIX is not set. Are you running this in a Conda environment?"
+    exit 1
+fi
+
+TEMPLATE_DIR="$CONDA_PREFIX/configTemplates"
+
 if [ "$1" == "-Regular" ]; then
-    cp $CONDA_PREFIX/configTemplates/SiprosEnsembleConfig.cfg $OUTPUT_PATH
+    TEMPLATE="$TEMPLATE_DIR/SiprosEnsembleConfig.cfg"
 elif [ "$1" == "-SIP" ]; then
-    cp $CONDA_PREFIX/configTemplates/SiprosV4Config.cfg $OUTPUT_PATH
+    TEMPLATE="$TEMPLATE_DIR/SiprosV4Config.cfg"
 else
     echo "Usage: copyConfigTemplate {-Regular|-SIP} <output_path>"
     exit 1
 fi
+
+if [ ! -f "$TEMPLATE" ]; then
+    echo "Error: Template file $TEMPLATE not found"
+    exit 1
+fi
+
+cp -v "$TEMPLATE" "$OUTPUT_PATH"
