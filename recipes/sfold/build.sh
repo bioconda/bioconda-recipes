58e55424e7210b46729b537bfdff922e7ab6eb2d
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2024-10-10 00:55:55 +0200

    update build

--- recipes/sfold/build.sh
+++ recipes/sfold/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Run configure script to setup build
+# cp ${RECIPE_DIR}/configure ./configure
+sed -i "s|ac_default_prefix=\$PWD|ac_default_prefix=\${PREFIX}|g" configure
+sed -i "s|SFOLDDIR=@abs_top_builddir@|SFOLDDIR=\${PREFIX}|g" sfoldenv.in
+
 ./configure
 
 cp -r bin/* ${PREFIX}/bin
 cp -r lib/* ${PREFIX}/lib
 cp -r param ${PREFIX}
 cp -r STarMir ${PREFIX}
-source sfoldenv
 cp sfoldenv ${PREFIX}
-export SFOLDDIR=${PREFIX}
-
