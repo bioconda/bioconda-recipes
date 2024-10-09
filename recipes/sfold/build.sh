313715ce3df6c6df52dc6744a3799d568ebef33f
reytakop <rey.ta.kop.biochem@gmail.com>
2024-10-09 16:40:44 +0200

    update build.sh

--- recipes/sfold/build.sh
+++ recipes/sfold/build.sh
@@ -2,3 +2,12 @@
 
 # Run configure script to setup build
 ./configure
+
+cp -r bin/* ${PREFIX}/bin
+cp -r lib/* ${PREFIX}/lib
+cp -r param ${PREFIX}
+cp -r STarMir ${PREFIX}
+source sfoldenv
+cp sfoldenv ${PREFIX}
+export SFOLDDIR=${PREFIX}
+
