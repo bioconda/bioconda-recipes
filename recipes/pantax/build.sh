60d398c4228554fa7fc7b19ea3473c1aefc416e7
Wenhai Zhang <128364515+zwh82@users.noreply.github.com>
2024-10-09 09:44:04 +0800

    Update build.sh

--- recipes/pantax/build.sh
+++ recipes/pantax/build.sh
@@ -3,17 +3,13 @@
 if [ "$(uname)" == "Darwin" ]; then
     cp -rf $SRC_DIR $PREFIX
 else
-    mkdir -p ${PREFIX}/bin/tools
-    cd ${SRC_DIR}/vg
-    cp vg* ${PREFIX}/bin/tools/vg
-    chmod +x ${PREFIX}/bin/tools/vg
-    
     cd ${SRC_DIR}/scripts
     chmod +x pantax
     chmod +x data_preprocessing
     cp ${SRC_DIR}/scripts/pantax ${SRC_DIR}/scripts/data_preprocessing ${PREFIX}/bin
     cp ${SRC_DIR}/scripts/*py ${PREFIX}/bin
-    
+
+    mkdir -p ${PREFIX}/bin/tools
     cd ${SRC_DIR}/tools/fastix
     cargo install fastix --root ./
     cp ${SRC_DIR}/tools/fastix/bin/fastix ${PREFIX}/bin/tools
