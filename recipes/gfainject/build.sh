8dcd6ed843e5becc914f463e3b52a6d0c38c66bf
Martin Grigorov <martin-g@users.noreply.github.com>
2024-10-10 11:33:33 +0300

    add --no-track

--- recipes/gfainject/build.sh
+++ recipes/gfainject/build.sh
@@ -4,4 +4,4 @@ set -xe
 
 # build statically linked binary with Rust
 export RUST_BACKTRACE=1
-cargo install --verbose --path . --root ${PREFIX}
+cargo install --verbose --path . --root ${PREFIX} --no-track
