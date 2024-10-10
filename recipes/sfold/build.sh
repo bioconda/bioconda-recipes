11f8e7e4e4afa57e7a6f42f18a530ef0f16adcd9
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2024-10-10 15:01:39 +0200

    add awk and grep and modify sfoldenv manually

--- recipes/sfold/build.sh
+++ recipes/sfold/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Run configure script to setup build
-# cp ${RECIPE_DIR}/configure ./configure
-sed -i "s|ac_default_prefix=\$PWD|ac_default_prefix=\${PREFIX}|g" configure
-sed -i "s|SFOLDDIR=@abs_top_builddir@|SFOLDDIR=\${PREFIX}|g" sfoldenv.in
-
+# configure
 ./configure
 
-cp -r bin/* ${PREFIX}/bin
-cp -r lib/* ${PREFIX}/lib
-cp -r param ${PREFIX}
-cp -r STarMir ${PREFIX}
+# copy the source code to the build directory
+SFOLD_DIR=${PREFIX}/bin/sfold_dir
+mkdir -p ${SFOLD_DIR}
+cp -r bin ${SFOLD_DIR}
+cp -r lib ${SFOLD_DIR}
+cp -r param ${SFOLD_DIR}
+cp -r STarMir ${SFOLD_DIR}
 
-# Update sfoldenv file
-sed -i "s|SFOLDDIR=.*|SFOLDDIR=\$CONDA_PREFIX|g" sfoldenv
-cp sfoldenv ${PREFIX}
+# modify the sfoldenv file
+sed -i "s|SFOLDDIR=.*|SFOLDDIR=${SFOLD_DIR}|g" sfoldenv
+sed -i "s|PATH_AWK=.*|PATH_AWK=${PREFIX}/bin/awk|g" sfoldenv
+sed -i "s|PATH_GREP=.*|PATH_GREP=${PREFIX}/bin/grep|g" sfoldenv
+sed -i "s|PATH_PERL=.*|PATH_PERL=${PREFIX}/bin/perl|g" sfoldenv
+sed -i "s|PATH_R=.*|PATH_R=${PREFIX}/bin/R|g" sfoldenv
+
+cp sfoldenv ${SFOLD_DIR}/sfoldenv
