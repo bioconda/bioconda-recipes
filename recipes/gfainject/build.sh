7e6d1fe8780ce9bd5a62632a4ad607c52301072c
AndreaGuarracino <andreaguarracino@outlook.com>
2024-10-09 19:38:33 -0500

    edit build.sh

--- recipes/gfainject/build.sh
+++ recipes/gfainject/build.sh
@@ -4,4 +4,4 @@ set -xe
 
 # build statically linked binary with Rust
 export RUST_BACKTRACE=1
-cargo install --verbose --path . --root ${PREFIX} --version=${PKG_VERSION}
+cargo install --verbose --path . --root ${PREFIX}
