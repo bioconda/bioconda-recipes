6a4912b99ab5c39366e58f81f3a80a38310e5225
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2024-10-11 12:09:21 +0200

    clean

--- recipes/sfold/build.sh
+++ recipes/sfold/build.sh
@@ -4,7 +4,7 @@
 ./configure
 
 # copy the source code to the build directory
-SFOLD_DIR=${PREFIX}/bin/sfold_dir
+SFOLD_DIR=${PREFIX}
 mkdir -p ${SFOLD_DIR}
 cp -r bin ${SFOLD_DIR}
 cp -r lib ${SFOLD_DIR}
@@ -13,16 +13,4 @@ cp -r STarMir ${SFOLD_DIR}
 
 # modify the sfoldenv file
 sed -i "s|SFOLDDIR=.*|SFOLDDIR=${SFOLD_DIR}|g" sfoldenv
-sed -i "s|PATH_AWK=.*|PATH_AWK=${PREFIX}/bin/awk|g" sfoldenv
-sed -i "s|PATH_GREP=.*|PATH_GREP=${PREFIX}/bin/grep|g" sfoldenv
-sed -i "s|PATH_PERL=.*|PATH_PERL=${PREFIX}/bin/perl|g" sfoldenv
-sed -i "s|PATH_R=.*|PATH_R=${PREFIX}/bin/R|g" sfoldenv
-
 cp sfoldenv ${SFOLD_DIR}/sfoldenv
-
-# modify sfold
-sed -i "s|LOC=\`dirname \$0\`|LOC=${SFOLD_DIR}/sfoldenv|g" ${SFOLD_DIR}/bin/sfold
-sed -i "s|. \$LOC/../sfoldenv|. \$LOC|g" ${SFOLD_DIR}/bin/sfold
-
-# simlink the sfold
-ln -s ${SFOLD_DIR}/bin/sfold ${PREFIX}/bin/sfold
