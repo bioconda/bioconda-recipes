79b47b0e9d193c755714a72cf08e660da4487d79
Iyad Ba Gari <39617810+bagari@users.noreply.github.com>
2024-10-11 21:44:26 -0700

    Update build.sh

--- recipes/meta/build.sh
+++ recipes/meta/build.sh
@@ -8,7 +8,7 @@ mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
 mkdir -p "$PREFIX/share"
 mkdir -p $SRC_DIR/build
-cd $SRC_DIR/build
+cd $SRC_DIR/build || exit 1
 
 cmake -S $SRC_DIR -B $SRC_DIR/build  \
     -DCMAKE_BUILD_TYPE=Release \
@@ -19,10 +19,10 @@ cmake -S $SRC_DIR -B $SRC_DIR/build  \
     -DCMAKE_INSTALL_RPATH=$CMAKE_INSTALL_RPATH \
     -DCMAKE_BUILD_WITH_INSTALL_RPATH=$CMAKE_BUILD_WITH_INSTALL_RPATH
 
-cmake --build . --config Release
-cmake --install .
+cmake --build . --config Release || exit 1
+cmake --install . || exit 1
 rm -rf $SRC_DIR/build
 
 # Install MeTA package
-cd ${SRC_DIR}
-${PYTHON} -m pip install . --no-deps -vv
+cd ${SRC_DIR} || exit 1
+${PYTHON} -m pip install . --no-deps -vv || exit 1
