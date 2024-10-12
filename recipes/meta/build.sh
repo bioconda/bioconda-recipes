6b7031a132473bd683dc3c095858a81110cd7db9
Iyad Ba Gari <bagari@usc.edu>
2024-10-11 20:33:59 -0700

    add build.sh,  meta.yaml and conda_build_config.yaml

---
+++ recipes/meta/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+unset LD LINK ARCH
+export CMAKE_INSTALL_RPATH='$ORIGIN/../lib'
+export CMAKE_BUILD_WITH_INSTALL_RPATH=ON
+
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/lib"
+mkdir -p "$PREFIX/share"
+mkdir -p $SRC_DIR/build
+cd $SRC_DIR/build
+
+cmake -S $SRC_DIR -B $SRC_DIR/build  \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_MODULE_PATH=$PREFIX/share/cmake \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_RPATH=$CMAKE_INSTALL_RPATH \
+    -DCMAKE_BUILD_WITH_INSTALL_RPATH=$CMAKE_BUILD_WITH_INSTALL_RPATH
+
+cmake --build . --config Release
+cmake --install .
+rm -rf $SRC_DIR/build
+
+# Install MeTA package
+cd ${SRC_DIR}
+${PYTHON} -m pip install . --no-deps -vv
