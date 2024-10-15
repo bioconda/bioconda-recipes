346ec67b41e42e802d5d60f07de45f1dc3441691
mencian <joshua.zhuang@yahoo.com>
2024-10-15 17:57:01 -0500

    add makefile patch

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -6,11 +6,12 @@ mkdir -p "${PREFIX}/bin"
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 
 make CC="${CC} -O3" CFLAGS="${CFLAGS}" CXX="${CXX}" LDFLAGS="${LDFLAGS}" \
 	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
 	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	INCLUDES="${INCLUDES}" COMMONLIBS="${COMMONLIBS} -L${PREFIX}/lib" -j"${CPU_COUNT}"
+	INCLUDES="${INCLUDES}" -j"${CPU_COUNT}"
 
 chmod 0755 MuSE
 cp -f MuSE "${PREFIX}/bin"
