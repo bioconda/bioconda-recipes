814f2fa0470e89527a4cc9c6e810c37fd5b511de
Famke Bäuerle <famke.baeuerle@gmail.com>
2024-10-15 09:39:48 +0200

    coderabbit suggestions

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -1,13 +1,14 @@
-#!/bin/bash -euo
+#!/bin/bash
+set -euo pipefail
 
 mkdir -p "${PREFIX}/bin"
 
-export INCLUDES="-I{PREFIX}/include"
+export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CC="${CC} -O3 ${LDFLAGS}" CFLAGS="${CFLAGS}" CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3 -I{PREFIX}/include" \
+make CC="${CC} -O3" CFLAGS="${CFLAGS}" CXX="${CXX}" LDFLAGS="${LDFLAGS}" \
+	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
 	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
 	INCLUDES="${INCLUDES}" COMMONLIBS="${COMMONLIBS} -L${PREFIX}/lib" -j"${CPU_COUNT}"
 
