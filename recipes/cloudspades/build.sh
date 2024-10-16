af16fd42a41f52a897788f3b61b556977495622c
mencian <joshua.zhuang@yahoo.com>
2024-10-15 22:05:38 -0500

    add case for Darwin

--- recipes/cloudspades/build.sh
+++ recipes/cloudspades/build.sh
@@ -7,5 +7,14 @@ export INCLUDE_PATH="${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -fcommon"
 export CXXFLAGS="${CFLAGS} -O3 -fcommon -I${PREFIX}/include"
 
+case $(uname -m) in
+	Linux)
+		THREADS="-rj${CPU_COUNT}"
+		;;
+	Darwin)
+		THREADS=""
+		;;
+esac
+
 cd assembler
-PREFIX="${PREFIX}" bash spades_compile.sh -rj"${CPU_COUNT}" -DSPADES_ENABLE_PROJECTS="all"
+PREFIX="${PREFIX}" bash spades_compile.sh "${THREADS}" -DSPADES_ENABLE_PROJECTS="all"
