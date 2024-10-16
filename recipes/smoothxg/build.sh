fa74bf86a0a5a8fd80a653e0c205a2412d83cf3b
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-16 10:07:38 +0300

    smoothxg: Use custom arch flags
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/smoothxg/build.sh
+++ recipes/smoothxg/build.sh
@@ -9,10 +9,21 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export CPP_INCLUDE_PATH=${PREFIX}/include
 export CXX_INCLUDE_PATH=${PREFIX}/include
-sed -i 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
-sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
-sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
-cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
+
+case $(uname -m) in
+    aarch64 | arm64)
+        ARCH_FLAGS=""
+        sed -i 's/-msse4.1/-Ofast/g' deps/spoa/CMakeLists.txt
+        ;;
+    *)
+        ARCH_FLAGS="-march=sandybridge"
+        sed -i 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
+        sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
+        sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
+        ;;
+esac
+
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS="${ARCH_FLAGS} -Ofast"
 cmake --build build --verbose -j"${CPU_COUNT}"
 mkdir -p $PREFIX/bin
 mv bin/* $PREFIX/bin
