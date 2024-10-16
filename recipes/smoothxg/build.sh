d38f5e740199a778c4eedce3b27efbf9ef04f142
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-16 09:38:01 +0300

    smoothxg: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/smoothxg/build.sh
+++ recipes/smoothxg/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -xe
+
 export LIBRARY_PATH=${PREFIX}/lib
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export CPATH=${PREFIX}/include
@@ -10,6 +13,6 @@ sed -i 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
 sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
 sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
 cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
-cmake --build build --verbose
+cmake --build build --verbose -j"${CPU_COUNT}"
 mkdir -p $PREFIX/bin
 mv bin/* $PREFIX/bin
