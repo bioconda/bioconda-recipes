b127f4fd779d4c71c09d85f22c3842eef8da89c7
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-16 13:59:56 +0300

    vcf-validator: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/vcf-validator/build.sh
+++ recipes/vcf-validator/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -x
+
 # Set c++ to version 11
 export CXXFLAGS="-std=c++11 ${CXXFLAGS}"
 
 mkdir build || { echo "Failed to create build directory" >&2; exit 1; }
 cd build || { echo "Failed to go into build directory" >&2; exit 1; }
 cmake -G "Unix Makefiles" ..
-make -j2 || { echo "Build failed" >&2; exit 1; }
+make -j"${CPU_COUNT}" || { echo "Build failed" >&2; exit 1; }
 cd .. || { echo "Failed to return to parent directory" >&2; exit 1; }
 if ! ./build/bin/test_validation_suite; then
   echo "Validation suite failed" >&2
