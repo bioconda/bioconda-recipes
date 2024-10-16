f1a7429c1f3a9c499a9e1ca5d16af763a1ad6eda
mencian <joshua.zhuang@yahoo.com>
2024-10-15 23:33:43 -0500

    edit case

--- recipes/cloudspades/build.sh
+++ recipes/cloudspades/build.sh
@@ -7,7 +7,7 @@ export INCLUDE_PATH="${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -fcommon"
 export CXXFLAGS="${CFLAGS} -O3 -fcommon -I${PREFIX}/include"
 
-case $(uname -m) in
+case $(uname) in
 	Linux)
 		THREADS="-rj${CPU_COUNT}"
 		;;
