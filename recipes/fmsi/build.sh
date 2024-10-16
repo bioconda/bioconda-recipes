91a4c62ed6b94d910277c3b6f72193c023af43bc
mencian <joshua.zhuang@yahoo.com>
2024-10-15 20:24:59 -0500

    edit chmod command

--- recipes/fmsi/build.sh
+++ recipes/fmsi/build.sh
@@ -2,11 +2,13 @@
 
 mkdir -p ${PREFIX}/bin
 
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
 	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" \
 	-j"${CPU_COUNT}"
 
-chmod 0755 fmsi
-mv fmsi ${PREFIX}/bin
+chmod 0755 ${PREFIX}/bin/fmsi
