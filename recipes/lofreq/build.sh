c77e8433ea303676e280d4d51ed7216095f98267
mencian <joshua.zhuang@yahoo.com>
2024-10-15 21:32:30 -0500

    lofreq: add osx-arm64 build

--- recipes/lofreq/build.sh
+++ recipes/lofreq/build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -eu -o pipefail
 
-./configure --with-htslib=system --prefix=${PREFIX}
-make -j ${CPU_COUNT}
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
+if [[ "$(uname)" == Darwin ]]; then
+	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+fi
+
+autoreconf -if
+./configure --with-htslib=system --prefix="${PREFIX}" \
+	CC="${CC}" CFLAGS="${CFLAGS}" \
+	LDFLAGS="${LDFLAGS}" CPP="${CPP}" \
+	CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include" \
+	PYTHON="${PYTHON}"
+
+make -j"${CPU_COUNT}"
 make install
