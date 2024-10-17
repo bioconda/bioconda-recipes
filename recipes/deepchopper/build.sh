09b91a60084d213c2df9e2a541f1f420dedfa606
Yangyang Li <yangyang.li@northwestern.edu>
2024-10-17 11:57:01 -0500

    Update build.sh

--- recipes/deepchopper/build.sh
+++ recipes/deepchopper/build.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -ex
 
+# Build the package using Maturin
+maturin build --release --strip --manylinux 2014 --interpreter $PYTHON
+
+# Install the wheel
+$PYTHON -m pip install $WHEEL_PATH --no-deps -vv
