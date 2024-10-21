74171b5e8bf98320392ad97fe2e7c081dd31af57
mencian <joshua.zhuang@yahoo.com>
2024-10-21 03:50:16 -0500

    add run_exports

--- recipes/svdss/build.sh
+++ recipes/svdss/build.sh
@@ -1,10 +1,19 @@
-#!/usr/bin/env bash
-
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCONDAPREFIX=${PREFIX} ..
-make
-cd ..
+#!/bin/bash
 
 mkdir -p ${PREFIX}/bin
-cp SVDSS ${PREFIX}/bin
+
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S. -B build -DCMAKE_BUILD_TYPE=Release \
+	-DCONDAPREFIX="${PREFIX}" -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	"${CONFIG_ARGS}"
+cmake --build build -j "${CPU_COUNT}" -v
+
+cd build
+chmod 0755 SVDSS
+cp -f SVDSS ${PREFIX}/bin
