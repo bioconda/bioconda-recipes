559e3d805836e9bd0234971e9c2e5c98e5adb6d5
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-21 10:04:15 +0300

    centrifuge-core: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/centrifuge-core/build.sh
+++ recipes/centrifuge-core/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -xe
+
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
@@ -7,6 +10,6 @@ mkdir -p $PREFIX/bin
 sed "/^GCC/d;/^CC =/d;/^CPP =/d;/^CXX =/d" < Makefile > Makefile.new
 mv Makefile.new Makefile
 cat Makefile
-make CC=$CC CXX=$CXX RELEASE_FLAGS="$CXXFLAGS"
+make -j"${CPU_COUNT}" CC=$CC CXX=$CXX RELEASE_FLAGS="$CXXFLAGS"
 make install prefix=$PREFIX
 
