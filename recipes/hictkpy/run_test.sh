936253ec16a8bd26c380808fbc90f7c987006a40
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-10-23 01:59:38 +0200

    Debugging

--- recipes/hictkpy/run_test.sh
+++ recipes/hictkpy/run_test.sh
@@ -2,5 +2,7 @@
 
 1>&2 echo "Launching tests..."
 
+sleep 30  # Give the runner a chance to pickup echo's output
+
 "$PYTHON" -c 'import hictkpy; print(hictkpy.__version__)'
 "$PYTHON" -m pytest test
