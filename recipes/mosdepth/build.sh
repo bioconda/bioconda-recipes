854781a59efdbbc3283b5b50838fd3028a535f80
mencian <joshua.zhuang@yahoo.com>
2024-10-22 18:56:56 -0500

    bump mosdepth to 0.3.9

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ ${target_platform}  == osx-64 ]] ; then
-    curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-1-6/macosx_x64.tar.xz -o macosx_x64.tar.xz
+if [[ ${target_platform}  == "osx-64" ]] ; then
+    curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-2-2/macosx_x64.tar.xz -o macosx_x64.tar.xz
     tar -xzf macosx_x64.tar.xz
-    cd nim-1.6.*
+    cd nim-2.2.*
     export PATH="$PWD/bin:$PATH"
     cd ..
     curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
@@ -11,7 +11,7 @@ if [[ ${target_platform}  == osx-64 ]] ; then
     cd mosdepth-${PKG_VERSION}
     export CFLAGS="$CFLAGS -I$PREFIX/include"
     export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-    nimble install -y "docopt@0.7.0"  --passC:"-I$PREFIX/include" --passL:"-L$PREFIX/lib"
+    nimble install -y "docopt@0.7.1"  --passC:"-I$PREFIX/include" --passL:"-L$PREFIX/lib"
     nimble build -y --verbose -d:release -d:d4 --passC:"-I$PREFIX/include" --passL:"-L$PREFIX/lib"
 else
     #Link to mosdepth_d4 is specified below because of https://github.com/brentp/mosdepth/issues/232 
@@ -20,4 +20,5 @@ else
 fi
 
 mkdir -p "${PREFIX}/bin"
-cp mosdepth "${PREFIX}/bin/"
+chmod 0755 mosdepth
+mv mosdepth "${PREFIX}/bin/"
