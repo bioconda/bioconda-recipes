656aaf1034212d0eca606edc04839cb8a7215699
mencian <joshua.zhuang@yahoo.com>
2024-10-24 14:06:36 -0500

    clean up recipe

--- recipes/deepchopper/build.sh
+++ recipes/deepchopper/build.sh
@@ -4,14 +4,8 @@
 # -x = print every executed command
 set -ex
 
-# Use a custom temporary directory as home on macOS.
-# (not sure why this is useful, but people use it in bioconda recipes)
-if [ `uname` == Darwin ]; then
-  export HOME=`mktemp -d`
-fi
-
 # Build the package using maturin - should produce *.whl files.
-maturin build --interpreter $PYTHON --release
+maturin build --interpreter "${PYTHON}" --release --strip
 
 # Install *.whl files using pip
-$PYTHON -m pip install target/wheels/*.whl --no-deps --ignore-installed -vv
+$PYTHON -m pip install target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
