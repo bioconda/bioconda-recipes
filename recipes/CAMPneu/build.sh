a061dd412e7f87e31bc006d9f635cfbad879e98b
Mahika Kadam <109224543+mmkadam@users.noreply.github.com>
2024-10-25 11:11:42 -0500

    Delete recipes/CAMPneu/build.sh

--- recipes/CAMPneu/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- 
-# $PYTHON -m pip install . -vv
- 
-mkdir -p $PREFIX/bin/
- 
-cp CAMPneu.nf nextflow.config $PREFIX/bin/
-
-chmod +x $PREFIX/bin/CAMPneu.nf $PREFIX/bin/nextflow.config
