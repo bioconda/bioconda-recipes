1c99d24f2ef9df71a68538a14bdb031477892567
mencian <joshua.zhuang@yahoo.com>
2024-10-28 01:06:17 -0500

    clean up recipe

--- recipes/kmer-jellyfish/build.sh
+++ recipes/kmer-jellyfish/build.sh
@@ -4,12 +4,18 @@ export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+if [[ `uname` == "Darwin" ]]; then
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+else
+	export CXXFLAGS="${CXXFLAGS}"
+fi
+
 ./configure --prefix="${PREFIX}" --enable-python-binding --with-sse CXX="${CXX}" \
 	CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+	LDFLAGS="${LDFLAGS} -Wl,-rpath -Wl,${PREFIX}/lib"
 
 make -j "${CPU_COUNT}"
 make install
 make check -j "${CPU_COUNT}"
 cd swig/python
-$PYTHON -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
