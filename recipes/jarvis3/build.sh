f0841d5032b8e72d181b655d2ac6b9b22151c131
mirakaya <59024914+mirakaya@users.noreply.github.com>
2024-10-28 14:13:18 -0400

    Add JARVIS3 recipe.

---
+++ recipes/jarvis3/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+cd src 
+make CC=$CC CFLAGS="$CFLAGS -fcommon"
+mkdir -p ${PREFIX}/bin
+make
+mv extra/ ${PREFIX}/bin
+mv JARVIS3.sh ${PREFIX}/bin
+mv JARVIS3 ${PREFIX}/bin
+cd ${PREFIX}/bin
+sed 's/ \.\// /g; s/\"\.\//"/g' JARVIS3.sh > aux.sh
+rm JARVIS3.sh
+mv aux.sh JARVIS3.sh
+chmod +x JARVIS3.sh
+JARVIS3.sh --install
