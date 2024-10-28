6faea296c973f3333bb6bbe86496ba200c972441
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-10-28 16:39:50 +0200

    PretextGraph: Use CMake for building on ARM64
    ... because it just works
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/pretextgraph/build.sh
+++ recipes/pretextgraph/build.sh
@@ -2,14 +2,26 @@
 
 set -xeo pipefail
 
-if [ `uname` == Darwin ]; then
-    meson setup --buildtype=release --prefix=$PREFIX --unity on builddir
-else
-    env CC=clang CXX=clang++ meson setup --buildtype=release --prefix=$PREFIX --unity on builddir
-fi
+mkdir -p "${PREFIX}bin"
 
-cd builddir
-meson compile
-meson test
-meson install
+case $(uname -m) in
+    aarch64 | arm64)
+        cmake -DCMAKE_BUILD_TYPE=Release -S . -B build_cmake
+        cmake --build build_cmake -j"${CPU_COUNT}"
+        install -m 0755 build_cmake/PretextGraph "${PREFIX}/bin"
+        ;;
+    *)
+        if [ `uname` == Darwin ]; then
+            meson setup --buildtype=release --prefix=$PREFIX --unity on builddir
+        else
+            env CC=clang CXX=clang++ meson setup --buildtype=release --prefix=$PREFIX --unity on builddir
+        fi
+
+        cd builddir
+        meson compile
+        meson test
+        meson install
+        ;;
+esac
+
 
