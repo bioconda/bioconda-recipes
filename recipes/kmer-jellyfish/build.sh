42f73469dbc8cd935a45e57b611ff8f118604064
mencian <joshua.zhuang@yahoo.com>
2024-10-28 01:14:56 -0500

    add autoreconf

--- recipes/kmer-jellyfish/build.sh
+++ recipes/kmer-jellyfish/build.sh
@@ -3,19 +3,19 @@
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 if [[ `uname` == "Darwin" ]]; then
 	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-else
-	export CXXFLAGS="${CXXFLAGS}"
 fi
 
+autoreconf -if
 ./configure --prefix="${PREFIX}" --enable-python-binding --with-sse CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS} -Wl,-rpath -Wl,${PREFIX}/lib"
+	CXXFLAGS="${CXXFLAGS}" CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 
-make -j "${CPU_COUNT}"
+make -j"${CPU_COUNT}"
 make install
-make check -j "${CPU_COUNT}"
+make check -j"${CPU_COUNT}"
 cd swig/python
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
