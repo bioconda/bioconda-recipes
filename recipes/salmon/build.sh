fa1dd8f6bd8cd6bf00307a2f4aaf3318c66e7353
mencian <joshua.zhuang@yahoo.com>
2024-10-29 15:43:09 -0500

    clean up recipe

--- recipes/salmon/build.sh
+++ recipes/salmon/build.sh
@@ -2,6 +2,7 @@
 set -eu -o pipefail
 
 export CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ `uname` == "Darwin" ]]; then
 	export CFLAGS="${CFLAGS} -Wunused-command-line-argument -Wunused-parameter"
@@ -11,19 +12,21 @@ else
 fi
 
 cmake -S . -B build \
-	-DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=RELEASE \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" \
+	-DCMAKE_LIBRARY_PATH="${PREFIX}/lib" \
+	-DCMAKE_INCLUDE_PATH="${PREFIX}/include" \
 	-DCONDA_BUILD=TRUE \
-	-DLIBSTADEN_LDFLAGS="-L${PREFIX}/lib" \
 	-DBOOST_ROOT="${PREFIX}" \
 	-DCEREAL_INCLUDE_DIR="${PREFIX}/include" \
 	-DLIB_GFF_INCLUDE_DIR="${PREFIX}/include" \
+	-DLIBSTADEN_LDFLAGS="-L${PREFIX}/lib" \
+	-DSTADEN_INCLUDE_DIR="${PREFIX}/include" \
+	-DSTADEN_LIBRARY="${PREFIX}/lib/libstaden-read.a" \
+	-DSTADEN_VERSION="1.15.0" \
 	-DNO_IPO=TRUE \
- 	-DSTADEN_INCLUDE_DIR="${PREFIX}/include" \
-  	-DSTADEN_VERSION="1.15.0" \
-   	-DSTADEN_LIBRARY="${PREFIX}/lib/libstaden-read.a" \
 	"${CONFIG_ARGS}"
 
 cmake --build build/ --target install -v
