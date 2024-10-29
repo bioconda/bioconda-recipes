e4d2ab8d0b38c16626b63e3ef5ecae57493b5309
Chris Markiewicz <effigies@gmail.com>
2024-10-29 08:40:23 -0400

    Update connectome-workbench recipe

--- recipes/connectome-workbench/build.sh
+++ recipes/connectome-workbench/build.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
-
-export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig \
- OPENMP_HEADER_DIR=${PREFIX}/include \
- OPENMP_LIB_DIR=${PREFIX}/lib \
- FREETYPE_DIR=${PREFIX}
+#!/usr/bin/env bash
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DOPENSSL_ROOT_DIR=${PREFIX} \
- -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-L${PREFIX}/lib" \
- -DWORKBENCH_USE_QT5=TRUE -DZLIB_ROOT=${PREFIX} -DWORKBENCH_MESA_DIR=${PREFIX} \
- -DCMAKE_PREFIX_PATH=${PREFIX} -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=True ../src
-make -j ${CPU_COUNT}
-make install
+
+if [[ "$target_platform" == linux-* ]]; then
+    export CXX_FLAGS="$CXX_FLAGS -lGL -lGLU"
+fi
+
+cmake $CMAKE_ARGS -GNinja \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
+    -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
+    -DCMAKE_CXX_FLAGS="$CXX_FLAGS" \
+    ../src
+
+cmake --build .
+
+ctest --extra-verbose --output-on-failure .
+
+cmake --install .
