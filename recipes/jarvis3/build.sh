be39fbe08d7f3a42604aeedbf18546ef0275af20
mirakaya <59024914+mirakaya@users.noreply.github.com>
2024-10-30 15:30:04 -0400

    Reverted CC

--- recipes/jarvis3/build.sh
+++ recipes/jarvis3/build.sh
@@ -3,8 +3,6 @@
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
-CC=${PREFIX}/bin/gcc
-
 cd src 
 make CC=$CC CFLAGS="$CFLAGS -fcommon"
 mkdir -p ${PREFIX}/bin
