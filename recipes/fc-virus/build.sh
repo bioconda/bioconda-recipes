aa29d2f122195b11ca7d8fef7d53ef6a862d19ad
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-30 20:24:56 +0800

    Update build.sh

--- recipes/fc-virus/build.sh
+++ recipes/fc-virus/build.sh
@@ -12,13 +12,13 @@ echo "克隆代码库..."
 git clone https://github.com/qdu-bioinfo/FC-Virus.git
 
 # 进入目录
-cd FC-Virus || { echo "进入目录失败"; exit 1; }
-ls
+cd FC-Virus/bin || { echo "进入目录失败"; exit 1; }
+ls -l
 # 清理和编译
 #echo "清理旧文件并编译..."
 #make clean && make || { echo "编译失败"; exit 1; }
 
 # 运行编译好的程序
 echo "运行程序..."
-chmod +x ./bin/fc-virus
-./bin/fc-virus -h || { echo "运行程序失败"; exit 1; }
+chmod +x fc-virus
+fc-virus --help || { echo "运行程序失败"; exit 1; }
