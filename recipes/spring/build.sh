750c245bc725621e6d3801c5600ca6940daa0e19
FriederikeHanssen <friederike.hanssen@seqera.io>
2024-10-30 11:33:16 +0000

    apply review comments

--- recipes/spring/build.sh
+++ recipes/spring/build.sh
@@ -8,7 +8,7 @@ export CXXPATH=${PREFIX}/include
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export CXXFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-if [ $(arch) == aarch64 ]; then
+if [ $(arch) == "aarch64" ]; then
    cmake ..
   else
       cmake -Dspring_optimize_for_portability=ON ..
