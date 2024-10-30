866bd6d99e40f3a7e52a428ecb34c817eba7d07d
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-30 22:50:38 +0800

    Create build.sh

---
+++ recipes/fc/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# 编译源代码
+#make
+
+# 运行编译好的程序
+#echo "运行程序..."
+#./bin/fc-virus
+
+echo "克隆代码库..."
+git clone https://github.com/qdu-bioinfo/FC-Virus.git
+
+# 进入目录
+cd FC-Virus/bin || { echo "进入目录失败"; exit 1; }
+
+# 清理和编译
+#echo "清理旧文件并编译..."
+#make clean && make || { echo "编译失败"; exit 1; }
+
+# 运行编译好的程序
+echo "运行程序..."
+chmod 755 fc-virus
+ls -l
+if ! ./fc-virus --help 2>&1; then
+    echo "运行程序失败，错误信息为：$?"
+    exit 1
+fi
