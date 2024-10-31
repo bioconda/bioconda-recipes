ed5e09d635d19a867bbfd947bc72ce17e74e1ab8
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-31 22:31:31 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -1,7 +1,11 @@
-#! /bin/sh
+#!/bin/bash
+
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 
 make \
-  CC="${CC}" \
+  CC="${CXX}" \
   CFLAGS="${CFLAGS} -std=c99"
 
 install -d "${PREFIX}/bin"
