a318fd1fafada8f4e6ffccb40c6b0571a5b2fcc9
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-31 22:53:19 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -3,9 +3,10 @@
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 
 make \
-  CC="${CXX}" \
+  CXX="${CXX}${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
   CFLAGS="${CFLAGS} -std=c99"
 
 install -d "${PREFIX}/bin"
