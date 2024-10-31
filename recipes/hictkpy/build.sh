3c5d745599efef020415e03d912b1be6643141ad
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-10-31 09:56:02 +0100

    Export env variables in build script

--- recipes/hictkpy/build.sh
+++ recipes/hictkpy/build.sh
@@ -8,7 +8,6 @@ export CONAN_HOME="$scratch/conan"
 # shellcheck disable=SC2064
 trap "rm -rf '$scratch'" EXIT
 
-declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*darwin.* ]]; then
   if [[ "$OSX_ARCH" == 'x86_64' ]]; then
     # https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk
@@ -44,6 +43,12 @@ CMAKE_ARGS+=" -DPython_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
 CMAKE_ARGS+=" -DPython3_EXECUTABLE:PATH=${PYTHON}"
 CMAKE_ARGS+=" -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
 CMAKE_ARGS+=" -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
+
+echo "CFLAGS='$CFLAGS'"
+echo "CXXFLAGS='$CXXFLAGS'"
+echo "CMAKE_ARGS='$CMAKE_ARGS'"
+
+export CMAKE_ARGS CMAKE_BUILD_PARALLEL_LEVEL CFLAGS CXXFLAGS
 
 SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION" \
 "$PYTHON" -m pip install "$SRC_DIR" -vv
