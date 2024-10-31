7f27cd4450be3cede12780c7fd0385aaab19084b
FriederikeHanssen <friederike.hanssen@seqera.io>
2024-10-31 10:31:54 +0000

    more changes

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -9,7 +9,13 @@ if [[ $OSTYPE == "darwin"* ]]; then
     sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset_s.c
     sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/wmemset_s.c
 fi
-LIBS="${LDFLAGS}" make CC="${CC}" CXX="${CXX}" multi
+
+case ${ARCH} in
+  x86_64) LIBS=“${LDFLAGS}” make CC=“${CC}” CXX=“${CXX}” multi ;;
+  aarch64) LIBS=“${LDFLAGS}” make CC=“${CC}” CXX=“${CXX}” arch=armv8.1-a ;;
+esac
 
 mkdir -p $PREFIX/bin
 cp bwa-mem2* $PREFIX/bin
+
+
