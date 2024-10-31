a1fbb608a90fefefc8df767d77615d9b48b95761
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-31 16:12:15 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -15,28 +15,28 @@ echo "克隆代码库..."
 git clone https://github.com/qdu-bioinfo/FC-Virus.git
 pwd
 # 进入目录
-cd FC-Virus/code || { echo "进入目录失败"; exit 1; }
-
+cd FC-Virus || { echo "进入目录失败"; exit 1; }
+ls -l
 # 清理和编译
 echo "清理旧文件并编译..."
-#make clean && make
-ls -l
-if ! g++ -o FC-Virus GeneralSet.cpp ReadUtility.cpp KmerUtility.cpp KmerHash.cpp HomoKmer.cpp Consensus.cpp FC-Virus.cpp; then
-    echo "编译失败，错误信息如下："
+make clean && make
+
+#if ! g++ -o FC-Virus GeneralSet.cpp ReadUtility.cpp KmerUtility.cpp KmerHash.cpp HomoKmer.cpp Consensus.cpp FC-Virus.cpp; then
+#    echo "编译失败，错误信息如下："
     # 上一条命令的错误信息会自动输出
-    exit 1
-else
-    echo "编译成功"
-fi
+#    exit 1
+#else
+#    echo "编译成功"
+#fi
 
 
 # 运行编译好的程序
-echo "运行程序..."
-ls -l
-if ! ./FC-Virus --help 2>&1; then
-    echo "运行程序失败，错误信息为：$?"
-    exit 1
-fi
+#echo "运行程序..."
+#ls -l
+#if ! ./FC-Virus --help 2>&1; then
+#    echo "运行程序失败，错误信息为：$?"
+#    exit 1
+#fi
 
 chmod 755 ./bin/fc-virus
 ls -l
@@ -45,7 +45,7 @@ conda list libstdcxx-ng
 ldd ./bin/fc-virus
 conda search libstdcxx-ng
 
-if ! ./fc-virus --help 2>&1; then
+if ! ./bin/fc-virus --help 2>&1; then
     echo "运行程序失败，错误信息为：$?"
     exit 1
 fi
