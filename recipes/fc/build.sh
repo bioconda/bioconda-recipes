4f22236f7400891df13d7ccd384d71d680b63a7e
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-31 09:29:02 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -12,7 +12,7 @@ echo "克隆代码库..."
 git clone https://github.com/qdu-bioinfo/FC-Virus.git
 
 # 进入目录
-cd FC-Virus/bin || { echo "进入目录失败"; exit 1; }
+cd FC-Virus || { echo "进入目录失败"; exit 1; }
 
 # 清理和编译
 echo "清理旧文件并编译..."
@@ -27,11 +27,11 @@ fi
 
 # 运行编译好的程序
 echo "运行程序..."
-chmod 755 fc-virus
+chmod 755 ./bin/fc-virus
 ls -l
 conda list libstdcxx-ng
 #conda install libgcc-ng libstdcxx-ng boost
-ldd ./fc-virus
+ldd ./bin/fc-virus
 conda search libstdcxx-ng
 
 if ! ./fc-virus --help 2>&1; then
