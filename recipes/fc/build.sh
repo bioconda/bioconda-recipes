42458ebec67d1369986e3ec6ab8c7b6444ed934c
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-10-31 18:13:45 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -11,15 +11,15 @@ echo $PATH
 #echo "运行程序..."
 #./bin/fc-virus
 
-echo "克隆代码库..."
-git clone https://github.com/qdu-bioinfo/FC-Virus.git
-pwd
+#echo "克隆代码库..."
+#git clone https://github.com/qdu-bioinfo/FC-Virus.git
+#pwd
 # 进入目录
-cd FC-Virus || { echo "进入目录失败"; exit 1; }
-ls -l
+#cd FC-Virus || { echo "进入目录失败"; exit 1; }
+#ls -l
 # 清理和编译
-echo "清理旧文件并编译..."
-make clean && make
+#echo "清理旧文件并编译..."
+#make clean && make
 
 #if ! g++ -o FC-Virus GeneralSet.cpp ReadUtility.cpp KmerUtility.cpp KmerHash.cpp HomoKmer.cpp Consensus.cpp FC-Virus.cpp; then
 #    echo "编译失败，错误信息如下："
@@ -38,12 +38,12 @@ make clean && make
 #    exit 1
 #fi
 
-chmod 755 ./bin/fc-virus
+#chmod 755 ./bin/fc-virus
 ls -l
-conda list libstdcxx-ng
+#conda list libstdcxx-ng
 #conda install libgcc-ng libstdcxx-ng boost
-ldd ./bin/fc-virus
-conda search libstdcxx-ng
+#ldd ./bin/fc-virus
+#conda search libstdcxx-ng
 
 if ! ./bin/fc-virus --help 2>&1; then
     echo "运行程序失败，错误信息为：$?"
