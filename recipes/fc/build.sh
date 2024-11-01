ba636e578b1298d1c8296f2449109dce6adb4bf8
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-11-01 09:18:54 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -4,10 +4,11 @@ export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+export PATH=$BUILD_PREFIX/bin:$PATH
 
 make \
-  CXX="${CXX}${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
-  CFLAGS="${CFLAGS} -std=c99"
+  CXX="${CXX}" \
+  CFLAGS="${CFLAGS} "
 
 install -d "${PREFIX}/bin"
 install fc-virus "${PREFIX}/bin/"
