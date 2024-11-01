373f19c939537101f5185b5a44b1d7627dfde064
Louis Le Nezet <louislenezet@gmail.com>
2024-11-01 01:04:40 +0100

    Fix linting

--- recipes/eagle2/build.sh
+++ recipes/eagle2/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-export COMMIT_VERS="${PKG_VERSION}"
-export COMMIT_DATE="$(date -Idate -u)"
-
 make \
     -j 4 \
     DYN_LIBS="-lz -lpthread -lbz2 -lblas" \
     CXX="$CXX -std=c++17" \
-    CXXFLAG="$CXXFLAGS ${PREFIX} -D__COMMIT_ID__='\"${COMMIT_VERS}\"' -D__COMMIT_DATE__='\"${COMMIT_DATE}\"' \
+    CXXFLAG="$CXXFLAGS" \
     LDFLAG="$LDFLAGS" \
+    PREFIX="${PREFIX}" \
     HTSLIB_INC="$PREFIX" \
     HTSLIB_LIB="-lhts" \
     BOOST_INC="/usr/include"\
@@ -16,5 +13,5 @@ make \
     BOOST_LIB_PO="-lboost_program_options" \
     BOOST_LIB_SE="-lboost_serialization" \
     ;
-install "bin/eagle" "eagle/bin"
+install
 popd
