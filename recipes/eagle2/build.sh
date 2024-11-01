752b1a59a96daa7d56ca97e60b269cd39ac2bb49
Louis Le Nezet <louislenezet@gamil.com>
2024-11-01 11:59:58 +0100

    Improve build script

--- recipes/eagle2/build.sh
+++ recipes/eagle2/build.sh
@@ -5,17 +5,17 @@ echo "Building Eagle2 version ${PKG_VERSION}"
 
 make \
     -e -j ${CPU_COUNT} \
-    DYN_LIBS="-lz -lpthread -lbz2 -lblas" \
+    DYN_LIBS="-lz -lpthread -lbz2 -lopenblas -lm -msse -msse2 -fopenmp -Wall" \
     CXX="$CXX -std=c++11" \
     CXXFLAG="$CXXFLAGS" \
     LDFLAG="$LDFLAGS" \
     PREFIX="${PREFIX}" \
     HTSLIB_INC="$PREFIX" \
     HTSLIB_LIB="-lhts" \
-    BOOST_INC="$PREFIX/include"\
+    BOOST_INC="$PREFIX/include" \
     BOOST_LIB_IO="-lboost_iostreams" \
     BOOST_LIB_PO="-lboost_program_options" \
-    BOOST_LIB_SE="-lboost_serialization"  || exit 1
+    BOOST_LIB_SE="-lboost_serialization" || exit 1
 
 echo "Installing Eagle2..."
 mkdir -p "${PREFIX}/bin"
