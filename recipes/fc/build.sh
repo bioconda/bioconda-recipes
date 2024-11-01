b11df750aaed65e1c7667be1a6adea601b015de9
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-11-01 09:27:26 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -3,8 +3,8 @@
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
-export PATH=$BUILD_PREFIX/bin:$PATH
+#export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+#export PATH=$BUILD_PREFIX/bin:$PATH
 
 make \
   CXX="${CXX}" \
