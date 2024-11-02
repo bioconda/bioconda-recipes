ec35aad69f8950f038272433698bef6dbe0d242e
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-11-02 16:26:34 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 set -xe
-make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
-chmod +x fc-virus
+#make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
+#chmod +x fc-virus
 mkdir -p ${PREFIX}/bin
 cp -f fc-virus ${PREFIX}/bin
