a7f57a16ec41b68e4170b221cc7950dae76524f2
Louis Le Nezet <louislenezet@gmail.com>
2024-11-02 23:06:37 +0100

    Update files

--- recipes/eagle2/build.sh
+++ recipes/eagle2/build.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-set -euo pipefail
+
+#set -euo pipefail
 
 echo "Building Eagle2 version ${PKG_VERSION}"
 
-make \
+cd src
+
+sed -i.bak '/memcpy.o/d' Makefile
+
+# Set up environment variables for building
+export BOOST_INSTALL_DIR=$PREFIX
+export HTSLIB_DIR=$PREFIX
+export BLAS_DIR=$PREFIX/lib
+export ZLIB_STATIC_DIR=$PREFIX
+
+# Run make
+make CC=${CXX} \
     -e -j ${CPU_COUNT} \
-    DYN_LIBS="-lz -lpthread -lbz2 -lopenblas -lm -msse -msse2 -fopenmp -Wall" \
-    CXX="$CXX -std=c++11" \
-    CXXFLAG="$CXXFLAGS" \
-    LDFLAG="$LDFLAGS" \
-    PREFIX="${PREFIX}" \
-    HTSLIB_INC="$PREFIX" \
-    HTSLIB_LIB="-lhts" \
-    BOOST_INC="$PREFIX/include" \
-    BOOST_LIB_IO="-lboost_iostreams" \
-    BOOST_LIB_PO="-lboost_program_options" \
-    BOOST_LIB_SE="-lboost_serialization" || exit 1
+    CFLAGS="$CXXFLAGS -Wno-unused-result" \
+    BOOST_INSTALL_DIR=$BOOST_INSTALL_DIR \
+    HTSLIB_DIR=$HTSLIB_DIR \
+    BLAS_DIR=$BLAS_DIR \
+    ZLIB_STATIC_DIR=$ZLIB_STATIC_DIR \
+    linking=static-except-glibc
 
 echo "Installing Eagle2..."
 mkdir -p "${PREFIX}/bin"
-install -m 755 bin/eagle "${PREFIX}/bin/"
+install -m 755 eagle "${PREFIX}/bin/"
 echo "Eagle2 installation completed successfully"
