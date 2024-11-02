21de2d8c3f2626ecb858497ab46dc896c714c759
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-11-02 15:02:41 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -1,19 +1,8 @@
+
 #!/bin/bash
-
-#export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-#export CPATH=${PREFIX}/include
-#export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
-#export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
-#export PATH=$BUILD_PREFIX/bin:$PATH
-
-#make \
-#  CXX="${CXX} ${CPPFLAGS} ${CXXFLAGS} " \
-#  CFLAGS="${CFLAGS} "
-
-#install -d "${PREFIX}/bin"
-#install ./bin/fc-virus "${PREFIX}/bin/"
 set -xe
-make  CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
-chmod +x ./bin/fc-virus
+make  CC=$CC INCLUDES="-I$PREFIX/include" CFLAGS+="-g -Wall -O2 -L$PREFIX/lib"
+chmod +x rotate
+chmod +x composition
 mkdir -p ${PREFIX}/bin
-cp -f ./bin/fc-virus ${PREFIX}/bin
+cp -f rotate composition ${PREFIX}/bin
