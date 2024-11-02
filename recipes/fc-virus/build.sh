700ce3d2bcb18da0e781086e935b572b5ad818dd
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-11-02 17:39:02 +0800

    Update build.sh

--- recipes/fc-virus/build.sh
+++ recipes/fc-virus/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
-export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
+#export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+#export CPATH=${PREFIX}/include
+#export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
+#export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 #export PATH=$BUILD_PREFIX/bin:$PATH
 
-make \
-  CXX="${CXX}" \
-  CFLAGS="${CFLAGS} "
+#make \
+#  CXX="${CXX}" \
+#  CFLAGS="${CFLAGS} "
 
-install -d "${PREFIX}/bin"
-install ./bin/fc-virus "${PREFIX}/bin/"
+#install -d "${PREFIX}/bin"
+#install ./bin/fc-virus "${PREFIX}/bin/"
+
+mkdir -p ${PREFIX}/bin
+cp -f fc-virus ${PREFIX}/bin
