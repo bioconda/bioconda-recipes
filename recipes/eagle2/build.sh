b2d54c1b9d0840419bca472ebaf4794a77f018a8
Louis Le Nezet <louislenezet@gmail.com>
2024-11-03 00:28:09 +0100

    Fix directory

--- recipes/eagle2/build.sh
+++ recipes/eagle2/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#set -euo pipefail
+set -euo pipefail
 
 echo "Building Eagle2 version ${PKG_VERSION}"
 
-cd src
+cd src || exit 1
 
 sed -i.bak '/memcpy.o/d' Makefile
 
 # Set up environment variables for building
 export BOOST_INSTALL_DIR=$PREFIX
-export HTSLIB_DIR=$PREFIX
+export HTSLIB_DIR=$PREFIX/lib
 export BLAS_DIR=$PREFIX/lib
-export ZLIB_STATIC_DIR=$PREFIX
+export ZLIB_STATIC_DIR=$PREFIX/lib
 
 # Run make
 make CC=${CXX} \
@@ -22,10 +22,12 @@ make CC=${CXX} \
     HTSLIB_DIR=$HTSLIB_DIR \
     BLAS_DIR=$BLAS_DIR \
     ZLIB_STATIC_DIR=$ZLIB_STATIC_DIR \
-    LIBSTDCXX_STATIC_DIR=${PREFIX}/bin \
-    linking=dynamic
+    LIBSTDCXX_STATIC_DIR=${PREFIX}/lib \
+    linking=static
 
 echo "Installing Eagle2..."
 mkdir -p "${PREFIX}/bin"
 install -m 755 eagle "${PREFIX}/bin/"
 echo "Eagle2 installation completed successfully"
+
+cd .. || exit 1
