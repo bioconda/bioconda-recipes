181a2439b4ea777c7d85c0f580b9d361ef486ad0
Iyad Ba Gari <39617810+bagari@users.noreply.github.com>
2024-11-03 09:27:26 -0800

    Update meta-neuro version 1.0.0 (#51826)
    * update meta.yaml file for meta package version 1.0.0
    
    https://bioconda.github.io/recipes/meta/README.html#package-meta
    
    * Update meta.yaml
    
    * Fix boost version compatibility with ITK
    
    * update ITK version to 5.3.0 to prevent install libitk and version 5.4rc02
    
    * update ITK run section at meta.yaml
    
    * Update meta.yaml
    
    * Add libgl-devel
    
    * Update meta.yaml
    
    * update build_variant
    
    * Include test data into meta folder
    
    Moving test data into the meta folder to solve the input data error does not exist during the container test.
    
    * adding test data
    
    * change and saved test data into $PREFIX/share/meta/example
    
    * add python support 3.8-3.11
    
    Python 3.12 will be supported after ITK updates its release.
    
    * update meta.yaml with skipping python 3.12
    
    * debug "No space left on device" issue
    
    * Drop support python 3.8 to solve storage issue, hopefully
    
    * Update build to rebuild and check storage issue.
    
    * Update meta.yaml
    
    * Build only for python 3.10
    
    * incrementing the build number
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Delete conda_build_config.yaml
    
    * Update meta.yaml
    
    * support apple arm
    
    * trying support vtk with egl variant
    
    * Update meta.yaml
    
    * keep only support VTK with osmesa for linux
    
    * Update meta.yaml
    
    * last attempt to build all python variants, hope no space error
    
    * force to skip python 3.12
    
    * Update conda_build_config.yaml
    
    * Update package name from meta to meta-neuro
    
    * update folder name as well
    
    * update name to meta-neuro

--- recipes/meta-neuro/build.sh
+++ recipes/meta-neuro/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 unset LD LINK ARCH
 export CMAKE_INSTALL_RPATH='$ORIGIN/../lib'
@@ -8,8 +9,12 @@ mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
 mkdir -p "$PREFIX/share"
 mkdir -p $SRC_DIR/build
-cd $SRC_DIR/build || exit 1
 
+## copy small test data:
+mkdir -p $PREFIX/share/meta-neuro/example
+cp $SRC_DIR/resources/test.nii.gz $PREFIX/share/meta-neuro/example/
+
+cd $SRC_DIR/build
 cmake -S $SRC_DIR -B $SRC_DIR/build  \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -19,10 +24,12 @@ cmake -S $SRC_DIR -B $SRC_DIR/build  \
     -DCMAKE_INSTALL_RPATH=$CMAKE_INSTALL_RPATH \
     -DCMAKE_BUILD_WITH_INSTALL_RPATH=$CMAKE_BUILD_WITH_INSTALL_RPATH
 
-cmake --build . --config Release --parallel ${CPU_COUNT} --verbose || exit 1
-cmake --install . || exit 1
+cmake --build . --config Release --parallel ${CPU_COUNT} --verbose
+cmake --install .
+
+# Clean up build directory
 rm -rf $SRC_DIR/build
 
 # Install MeTA package
-cd ${SRC_DIR} || exit 1
-${PYTHON} -m pip install . --no-deps -vv || exit 1
+cd ${SRC_DIR}
+${PYTHON} -m pip install . --no-deps -vv
