fbd22d48589420367e91493a88f92d5241acd707
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2024-11-04 09:21:33 +0800

    Update build.sh

--- recipes/fc-virus/build.sh
+++ recipes/fc-virus/build.sh
@@ -5,14 +5,14 @@
 #export LIBRARY_PATH=$LIBRARY_PATH:${PREFIX}/lib
 #export C_INCLUDE_PATH=$C_INCLUDE_PATH:${PREFIX}/include
 #export PATH=$BUILD_PREFIX/bin:$PATH
-
-#make \
-#  CXX="${CXX}" \
-#  CFLAGS="${CFLAGS} "
 set -xe
-#install -d "${PREFIX}/bin"
-#install ./bin/fc-virus "${PREFIX}/bin/"
-chmod 777 fc-virus
-mkdir -p ${PREFIX}/bin
-cp -f fc-virus ${PREFIX}/bin
+make \
+  CXX="${CXX}" \
+  CFLAGS="${CFLAGS} "
+install -d "${PREFIX}/bin"
+install ./bin/fc-virus "${PREFIX}/bin/"
+
+#chmod 777 fc-virus
+#mkdir -p ${PREFIX}/bin
+#cp -f fc-virus ${PREFIX}/bin
 
