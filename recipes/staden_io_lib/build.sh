631342bf84eb8d744f7b59946a0e79a224fe5ca0
mencian <joshua.zhuang@yahoo.com>
2024-11-05 01:57:04 -0600

    edit build.sh

--- recipes/staden_io_lib/build.sh
+++ recipes/staden_io_lib/build.sh
@@ -3,10 +3,11 @@
 export M4="${BUILD_PREFIX}/bin/m4"
 
 ./configure --prefix="${PREFIX}" \
-	--with-libdeflate="${PREFIX}/lib" \
-	CFLAGS="${CFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-R${PREFIX}/lib"
+	--enable-shared=no --without-libcurl \
+	--with-libdeflate="${PREFIX}" \
+	CFLAGS="-I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-R${PREFIX}/lib -Wl,-rpath -Wl,${PREFIX}/lib"
 make -j"${CPU_COUNT}"
 make install
 
-cp -f io_lib_config.h ${PREFIX}/include/io_lib/
+mv io_lib_config.h ${PREFIX}/include/io_lib/
