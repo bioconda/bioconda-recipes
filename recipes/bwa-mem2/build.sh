87885fd726429aa2e1cd6113cf0d41d79cf3ab2d
Pablo <pablo.aledo@gmail.com>
2024-11-05 15:40:43 +0000

    add .native suffix for cp

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -17,7 +17,7 @@ case ${ARCH} in
       mkdir ext/simde
       wget https://github.com/simd-everywhere/simde-no-tests/archive/refs/tags/v0.8.2.tar.gz -O - | tar -xvz
       mv simde-no-tests-0.8.2/* ext/simde/
-      LIBS="${LDFLAGS}" make arch=native EXE=bwa-mem2 CC="${CC}" CXX="${CXX}" all ;;
+      LIBS="${LDFLAGS}" make arch=native EXE=bwa-mem2.native CC="${CC}" CXX="${CXX}" all ;;
 esac
 
 mkdir -p $PREFIX/bin
