c893857af2c2877351a52eaa6d8b864875935d72
mencian <joshua.zhuang@yahoo.com>
2024-11-05 02:04:03 -0600

    edit build.sh

--- recipes/staden_io_lib/build.sh
+++ recipes/staden_io_lib/build.sh
@@ -5,7 +5,6 @@ export M4="${BUILD_PREFIX}/bin/m4"
 ./configure --prefix="${PREFIX}" \
 	--enable-shared=no --without-libcurl \
 	--with-libdeflate="${PREFIX}" \
-	CFLAGS="-I${PREFIX}/include" \
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-R${PREFIX}/lib -Wl,-rpath -Wl,${PREFIX}/lib"
 make -j"${CPU_COUNT}"
 make install
