7f66c80fa65293320422a239dddfeca821b9a9d8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-05 12:26:24 -0600

    Update snapatac2 to v2.7.1 (#51922)
    * Update snapatac2 to v2.7.1
    
    * clean up recipe
    
    * edit pyproject patch
    
    * edit pip install command
    
    * Update meta.yaml
    
    * add typeguard

--- recipes/snapatac2/build.sh
+++ recipes/snapatac2/build.sh
@@ -6,6 +6,6 @@ RUST_BACKTRACE=1
 maturin build -m snapatac2-python/Cargo.toml -b pyo3 --interpreter "${PYTHON}" --release --strip
 
 # Install *.whl files using pip
-${PYTHON} -m pip install snapatac2-python/target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
+${PYTHON} -m pip install target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
 
 cd ${SRC_DIR}/snapatac2-python/ && cargo clean && rm -rf ${BUILD_PREFIX}/.cargo
