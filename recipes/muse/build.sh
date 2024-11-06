4288a47e26a4333cdf0d1603f3efffa637d24878
Famke Bäuerle <famke.baeuerle@gmail.com>
2024-11-06 17:23:28 +0100

    change to inc

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 
 mkdir -p "${PREFIX}/bin"
 
-export INCLUDES="-I${PREFIX}/include"
+export INCLUDES="-I${PREFIX}/inc"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 
 make CC="${CC} -O3" CFLAGS="${CFLAGS}" CXX="${CXX}" LDFLAGS="${LDFLAGS}" \
-	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/inc" \
+	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/inc" \
 	INCLUDES="${INCLUDES}" -j"${CPU_COUNT}"
 
 chmod 0755 MuSE
