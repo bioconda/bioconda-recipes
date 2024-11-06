7e9c4adf014f3c959e0981a93dce5e84f8046941
Richard Burhans <burhans@bx.psu.edu>
2024-11-06 00:21:05 -0500

    aaa

--- recipes/halfdeep/build.sh
+++ recipes/halfdeep/build.sh
@@ -13,19 +13,12 @@ declare -A files=(
     ["scaffold_lengths.py"]="0755"
 )
 
-<<<<<<< HEAD
 # Install files
 for file in "${!files[@]}"; do
     if [[ ! -f "$SRC_DIR/$file" ]]; then
         echo "Source file $file not found in $SRC_DIR" >&2
         exit 1
     fi
-=======
-if ! install -v -m 0755 "$SRC_DIR/halfdeep.r" "$PREFIX/bin/halfdeep.r"; then
-    echo "Failed to install halfdeep r script" >&2
-    exit 1
-fi
->>>>>>> 0be50793b50dcc5777f9a7ce84e792c6b3e284ff
 
     if ! install -v -m "${files[$file]}" "$SRC_DIR/$file" "$PREFIX/bin/$file"; then
         echo "Failed to install $file" >&2
