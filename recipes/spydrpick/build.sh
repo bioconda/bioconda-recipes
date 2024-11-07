109965dbc17b66206a895b0150950ebbeb913849
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-07 16:24:26 -0600

    Update build.sh

--- recipes/spydrpick/build.sh
+++ recipes/spydrpick/build.sh
@@ -3,13 +3,13 @@
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -std=c++14 -O3 -D_LIBCPP_DISABLE_AVAILABILITY -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 # Set up Apegrunt; it's configured as a submodule to SpydrPick on GitHub, but since
 # Bioconda doesn't recommend using git_url (and 'clone --recursive' that would also
 # clone the submodule), we do it manually instead
 rmdir externals/apegrunt
-git clone --branch v0.5.0 https://github.com/santeripuranen/apegrunt.git externals/apegrunt
+git clone --branch v0.4.2 https://github.com/santeripuranen/apegrunt.git externals/apegrunt
 
 if [[ `uname` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
@@ -21,7 +21,6 @@ rm -rf src/CMakeLists.txt
 mv "${RECIPE_DIR}/CMakeLists.txt" src/
 
 # build and install SpydrPick (but only the SpydrPick make target)
-# export CMAKE_MODULE_PATH="${RECIPE_DIR}"
 mkdir build && pushd build
 
 cmake -S .. -B . -DCMAKE_BUILD_TYPE=Release \
