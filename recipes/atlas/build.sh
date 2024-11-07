5adb356a8858ea3e4043f1c68763b636b2a41e69
Andreas <andreas.fueglistaler@protonmail.com>
2024-11-07 15:48:12 +0100

    fixing openmp for os X

--- recipes/atlas/build.sh
+++ recipes/atlas/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
- cmake .. -GNinja -DCONDA=ON -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib -DCMAKE_INCLUDE_PATH=${CONDA_PREFIX}/include
-ninja
+cmake . -GNinja \
+      -DCONDA=ON \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib \
+      -DCMAKE_INCLUDE_PATH=${CONDA_PREFIX}/include
+
+ninja || exit 1
 
 mkdir -p ${PREFIX}/bin
-cp atlas ${PREFIX}/bin
+cp atlas ${PREFIX}/bin || exit 1
+chmod +x ${PREFIX}/bin/atlas
