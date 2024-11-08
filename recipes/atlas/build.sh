593309d6b4bbbb273a421b91981bbbdee03d610b
Andreas <andreas.fueglistaler@protonmail.com>
2024-11-08 09:41:07 +0100

    mac osX 64-bit path problem

--- recipes/atlas/build.sh
+++ recipes/atlas/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 cmake . -GNinja \
-      -DCONDA=ON \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_COMPILER=${CXX} \
-      -DCMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib \
-      -DCMAKE_INCLUDE_PATH=${CONDA_PREFIX}/include
+    -DCONDA=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" \
+    -DCMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib \
+    -DCMAKE_INCLUDE_PATH=${CONDA_PREFIX}/include \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 ninja || exit 1
 
