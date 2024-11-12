8514c19162b6eee42a4d6c3729a22f0a7b9a63ee
Pablo <pablo.aledo@seqera.io>
2024-11-12 10:15:15 +0100

    fix arch env variable

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -10,21 +10,17 @@ if [[ $OSTYPE == "darwin"* ]]; then
     sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/wmemset_s.c
 fi
 
-case ${ARCH} in
+case "$(arch)" in
   x86_64)
       LIBS="${LDFLAGS}" make CC="${CC}" CXX="${CXX}" multi ;;
   aarch64)
       mkdir ext/simde
       wget https://github.com/simd-everywhere/simde-no-tests/archive/refs/tags/v0.8.2.tar.gz -O - | tar -xvz
       mv simde-no-tests-0.8.2/* ext/simde/
-      LIBS="${LDFLAGS}" make arch=native EXE=bwa-mem2.native CC="${CC}" CXX="${CXX}" all
-      cp bwa-mem2.native bwa-mem2
-      cp bwa-mem2.native bwa-mem2.sse41
-      cp bwa-mem2.native bwa-mem2.sse42
-      cp bwa-mem2.native bwa-mem2.avx
-      cp bwa-mem2.native bwa-mem2.avx2
-      cp bwa-mem2.native bwa-mem2.avx512bw
+      LIBS="${LDFLAGS}" make arch=native EXE=bwa-mem2 CC="${CC}" CXX="${CXX}" all
       ;;
+  *)
+      echo "Error detecting architecture" ;;
 esac
 
 mkdir -p $PREFIX/bin
