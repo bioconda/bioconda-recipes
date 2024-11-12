8231e37ae1c1bd68e41d095cd0166fe2f167c5cf
Richard C. Burhans <rico@bx.psu.edu>
2024-11-12 11:24:09 -0500

    adding test

--- recipes/rdeval/build.sh
+++ recipes/rdeval/build.sh
@@ -8,23 +8,20 @@ make
 
 # mkdir -vp "${PREFIX}/bin"
 
-# # Define installation manifest
-# declare -A files=(
-#     ["bam_depth.sh"]="0755"
-#     ["halfdeep.sh"]="0755"
-#     ["halfdeep.r"]="0755"
-#     ["scaffold_lengths.py"]="0755"
-# )
+# Define installation manifest
+declare -A files=(
+    ["rdeval"]="0755"
+)
 
-# # Install files
-# for file in "${!files[@]}"; do
-#     if [[ ! -f "$SRC_DIR/$file" ]]; then
-#         echo "Source file $file not found in $SRC_DIR" >&2
-#         exit 1
-#     fi
-# 
-#     if ! install -v -m "${files[$file]}" "$SRC_DIR/$file" "$PREFIX/bin/$file"; then
-#         echo "Failed to install $file" >&2
-#         exit 1
-#     fi
-# done
+# Install files
+for file in "${!files[@]}"; do
+    if [[ ! -f "$SRC_DIR/$file" ]]; then
+        echo "Source file $file not found in $SRC_DIR" >&2
+        exit 1
+    fi
+
+    if ! install -v -m "${files[$file]}" "$SRC_DIR/$file" "$PREFIX/bin/$file"; then
+        echo "Failed to install $file" >&2
+        exit 1
+    fi
+done
