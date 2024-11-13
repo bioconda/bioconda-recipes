7a51e11f730925a69afa790ba4566ad7b4e49d83
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-12 18:49:12 -0600

    Update build.sh

--- recipes/rdeval/build.sh
+++ recipes/rdeval/build.sh
@@ -9,5 +9,5 @@ cd "$SRC_DIR"
 export CXXFLAGS="$CXXFLAGS -O3 -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
-make -j"${CPU_COUNT}"
+make -j"${CPU_COUNT}" CXX="${CXX}"
 install -v -m 0755 build/bin/rdeval "$PREFIX/bin/rdeval"
