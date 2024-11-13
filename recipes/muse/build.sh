2fe616f5517d082a202280ee0b441ce36876a935
mencian <joshua.zhuang@yahoo.com>
2024-11-13 00:40:50 -0600

    edit build.sh

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -13,10 +13,12 @@ echo "LIBPATH: ${LIBPATH}"
 echo "LDFLAGS: ${LDFLAGS}"
 echo "CFLAGS: ${CFLAGS}"
 
-make CC="${CC} -O3" CFLAGS="${CFLAGS}" CXX="${CXX}" LDFLAGS="${LDFLAGS}" \
-	CXXFLAGS="${CXXFLAGS} -O3" \
-	CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3" \
-	-j"${CPU_COUNT}"
+./install_muse.sh
+
+#make CC="${CC} -O3" CFLAGS="${CFLAGS}" CXX="${CXX}" LDFLAGS="${LDFLAGS}" \
+	#CXXFLAGS="${CXXFLAGS} -O3" \
+	#CPP="${CXX}" CPPFLAGS="${CXXFLAGS} -O3" \
+	#-j"${CPU_COUNT}"
 
 # List the contents of the PREFIX directory to see where files are placed
 echo "Listing contents of ${PREFIX}:"
