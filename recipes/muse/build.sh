20e095d0f0c1048240e59b10f116399f340b2a1c
mencian <joshua.zhuang@yahoo.com>
2024-11-13 02:57:23 -0600

    edit build.sh

--- recipes/muse/build.sh
+++ recipes/muse/build.sh
@@ -13,6 +13,12 @@ echo "LIBPATH: ${LIBPATH}"
 echo "LDFLAGS: ${LDFLAGS}"
 echo "CFLAGS: ${CFLAGS}"
 
+rm -rf boost_1_70_0/
+
+ln -sf ${CC} ${BUILD_PREFIX}/bin/gcc
+ln -sf ${CXX} ${BUILD_PREFIX}/bin/c++
+ln -sf ${GXX} ${BUILD_PREFIX}/bin/g++
+
 ./install_muse.sh
 
 # List the contents of the PREFIX directory to see where files are placed
