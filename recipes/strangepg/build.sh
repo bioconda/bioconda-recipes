4774c212880651f972f7e439cd67543b519f9025
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-12 19:34:34 -0600

    Update build.sh

--- recipes/strangepg/build.sh
+++ recipes/strangepg/build.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
-export CFLAGS="$CFLAGS -I${PREFIX}/include"
-export LDFLAGS="$LDFLAGS -ldl -lpthread"
-make -j ${CPU_COUNT} install
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="$LDFLAGS -ldl -lpthread -L${PREFIX}/lib"
+export CFLAGS="$CFLAGS -O3 -I${PREFIX}/include"
+
+make CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" PREFIX="${PREFIX}" -j"${CPU_COUNT}" install
