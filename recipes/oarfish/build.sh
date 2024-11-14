586b286d17d79505214a934a794ef11037d40c86
Rob Patro <rob-p@users.noreply.github.com>
2024-11-14 10:23:39 -0500

    Update build.sh

--- recipes/oarfish/build.sh
+++ recipes/oarfish/build.sh
@@ -2,9 +2,18 @@
 
 set -xe
 
+unamestr=`uname`
+if [ "$unamestr" == 'Darwin' ];
+  export CC=clang
+  export CXX=clang++
+else
+  export CC=gcc
+  export CXX=g++
+fi
+
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --profile=minimal -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 
 # build statically linked binary with Rust
-RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path .
+RUST_BACKTRACE=1 RUSTFLAGS=" cargo install --verbose --root $PREFIX --path .
