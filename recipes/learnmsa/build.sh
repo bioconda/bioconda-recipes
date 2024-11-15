b631b2ae3f98cd72e75a5ba1c6d1da93ce833b7f
rlibouban <romane.libouban@irisa.fr>
2024-11-15 10:47:21 +0100

    update build.sh

--- recipes/learnmsa/build.sh
+++ recipes/learnmsa/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 cp $SRC_DIR/*.py ${PREFIX}/
 chmod +x ${PREFIX}/*.py
+sed -i.bak '1s|^|#!/usr/bin/env python3\n|' ${PREFIX}/learnMSA.py
+
+ln -s ${PREFIX}/learnMSA.py ${PREFIX}/learnMSA
 
 mkdir -p ${PREFIX}/learnMSA
 cp -R $SRC_DIR/learnMSA/* ${PREFIX}/learnMSA/
 chmod -R +x ${PREFIX}/learnMSA/
-cp $SRC_DIR/*.py ${PREFIX}/
