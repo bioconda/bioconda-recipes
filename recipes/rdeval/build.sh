83bc1de9e91df7e3880059d4fcfea1353d6da0ac
Richard Burhans <burhans@bx.psu.edu>
2024-11-15 15:32:33 -0500

    reads

--- recipes/rdeval/build.sh
+++ recipes/rdeval/build.sh
@@ -2,7 +2,6 @@
 
 set -o errexit
 set -o nounset
-set -o xtrace
 
 if [ -e "$PREFIX/include" ]; then
     export CPPFLAGS="${CPPFLAGS:+$CPPFLAGS }-I$PREFIX/include"
@@ -17,11 +16,7 @@ echo "LDFLAGS=\"$LDFLAGS\""
 
 cd "$SRC_DIR"
 
-make -j "$CPU_COUNT"
+make
+
 install -d "$PREFIX/bin"
 install -v -m 0755 build/bin/rdeval "$PREFIX/bin/"
-
-
-
-# Compiling C++ programs
-# n.o is made automatically from n.cc, n.cpp, or n.C with a recipe of the form ‘$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c’. We encourage you to use the suffix ‘.cc’ or ‘.cpp’ for C++ source files instead of ‘.C’ to better support case-insensitive file systems.
