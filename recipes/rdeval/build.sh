3b136ab1223524ee808ccb0c2cf5e1478553c38d
Richard Burhans <burhans@bx.psu.edu>
2024-11-14 19:02:24 -0500

    upp

--- recipes/rdeval/build.sh
+++ recipes/rdeval/build.sh
@@ -6,9 +6,6 @@ set -o xtrace
 
 cd "$SRC_DIR"
 
-export CXXFLAGS="$CXXFLAGS -O3 -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
-make -j"${CPU_COUNT}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -Wno-unused-command-line-argument" LDFLAGS="${LDFLAGS}"
+make -j "$CPU_COUNT"
 install -d "$PREFIX/bin"
 install -v -m 0755 build/bin/rdeval "$PREFIX/bin/"
