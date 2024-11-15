fe36f3772ad8c4eaaf5a63492d42dd49be3f191a
rlibouban <romane.libouban@irisa.fr>
2024-11-15 11:01:45 +0100

    fix test

--- recipes/learnmsa/build.sh
+++ recipes/learnmsa/build.sh
@@ -5,8 +5,6 @@ cp $SRC_DIR/*.py ${PREFIX}/
 chmod +x ${PREFIX}/*.py
 sed -i.bak '1s|^|#!/usr/bin/env python3\n|' ${PREFIX}/learnMSA.py
 
-ln -s ${PREFIX}/learnMSA.py ${PREFIX}/learnMSA
-
 mkdir -p ${PREFIX}/learnMSA
 cp -R $SRC_DIR/learnMSA/* ${PREFIX}/learnMSA/
 chmod -R +x ${PREFIX}/learnMSA/
