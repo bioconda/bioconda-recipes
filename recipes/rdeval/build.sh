f2463ca5d575e2b31e2dd3386268e394848ade99
richard-burhans <burhans@bx.psu.edu>
2024-11-16 09:43:57 -0500

    Add rdeval (#52095)
    * initial attempt
    
    * adding make as a dependency
    
    * new hash
    
    * adding test
    
    * fixing build.sh
    
    * using version with submodules
    
    * adding zlib
    
    * updates
    
    * update
    
    * foo
    
    * quote
    
    * u
    
    * a
    
    * patch
    
    * zlib
    
    * zlib
    
    * foo
    
    * clean up recipe
    
    * clean up patches
    
    * Update build.sh
    
    * add gfalibs patch
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * add gfalibs patch
    
    * edit foo.patch
    
    * Update foo.patch
    
    * edit foo.patch
    
    * edit foo.patch
    
    * Update build.sh
    
    * makefile
    
    * makefile
    
    * updating submodule makefile
    
    * bumping build version
    
    * includes
    
    * testing makefile changrs
    
    * make
    
    * patches
    
    * patches
    
    * patches
    
    * patches
    
    * patches
    
    * forgot reads.patch
    
    * upp
    
    * zlib
    
    * zlib
    
    * patch
    
    * patches
    
    * requirememts
    
    * zlib
    
    * zlib
    
    * zlib
    
    * zlib
    
    * reads
    
    * rename patch files
    
    * removed patches
    
    ---------
    
    Co-authored-by: Richard C. Burhans <rico@bx.psu.edu>
    Co-authored-by: mencian <joshua.zhuang@yahoo.com>
    Co-authored-by: Joshua Zhuang <71105179+mencian@users.noreply.github.com>

--- recipes/rdeval/build.sh
+++ recipes/rdeval/build.sh
@@ -2,13 +2,21 @@
 
 set -o errexit
 set -o nounset
-set -o xtrace
+
+if [ -e "$PREFIX/include" ]; then
+    export CPPFLAGS="${CPPFLAGS:+$CPPFLAGS }-I$PREFIX/include"
+fi
+
+if [ -e "$PREFIX/lib" ]; then
+    export LDFLAGS="${LDFLAGS:+$LDFLAGS }-L$PREFIX/lib"
+fi
+
+echo "CPPFLAGS=\"$CPPFLAGS\""
+echo "LDFLAGS=\"$LDFLAGS\""
 
 cd "$SRC_DIR"
 
-export CXXFLAGS="$CXXFLAGS -O3 -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+make
 
-make -j"${CPU_COUNT}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -Wno-unused-command-line-argument" LDFLAGS="${LDFLAGS}"
 install -d "$PREFIX/bin"
 install -v -m 0755 build/bin/rdeval "$PREFIX/bin/"
