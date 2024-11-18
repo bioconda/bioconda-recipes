e627e11c81107b6a715d0bceb9dc2bfea317a4ef
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-18 03:26:59 -0600

    Update build.sh

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -3,8 +3,7 @@ set -eu
 
 autoreconf -if
 ./configure --prefix="${PREFIX}" CXX="${CXX}" \
-	--with-libmaus2 --with-liftingwavelettransform \
-	--with-xerces-c --with-gmp \
+	--with-libmaus2 --with-xerces-c --with-gmp \
 	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 make install
