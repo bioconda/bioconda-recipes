219165feb306e03bfc1f821726bf58b018824701
mencian <joshua.zhuang@yahoo.com>
2024-11-18 01:14:24 -0600

    clean up recipe

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -3,15 +3,12 @@ set -eu
 export LIBS="-lstdc++fs -lcurl -lz -ldeflate"
 
 autoreconf -if
-autoupdate
 ./configure --prefix="${PREFIX}" CXX="${CXX}" CC="${CC}" \
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
 	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
 	--with-snappy --with-io_lib --with-libdeflate \
 	--with-libsecrecy --with-nettle \
 	--with-lzma --with-gmp
-
-cat config.log
 
 make -j"${CPU_COUNT}"
 make install
