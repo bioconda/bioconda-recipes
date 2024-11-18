1deb425541ef0d63e31bdf44df7b914d27fb4d03
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-18 00:48:55 -0600

    Update build.sh

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -eu
-export LIBS="-lstdc++fs -lcurl -lz -ldeflate"
+export LIBS="-lstdc++ -lcurl -lz -ldeflate"
 
 autoreconf -if
 autoupdate
 ./configure --prefix="${PREFIX}" CXX="${CXX}" CC="${CC}" \
+	CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
 	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
 	--with-snappy --with-io_lib --with-libdeflate \
