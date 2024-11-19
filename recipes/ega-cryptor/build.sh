f588054e79218911481ed067d8131919537a6866
obaeza16 <baeza@recerca.clinic.cat>
2024-11-19 11:36:16 +0100

    Recipe builds correctly

--- recipes/ega-cryptor/build.sh
+++ recipes/ega-cryptor/build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-set -e -x
-# set -eu -o pipefail
+set -eu -o pipefail
 
-outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
-ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION $PREFIX/share/$PKG_NAME
 mkdir -p $PREFIX/bin
-cp -Rf * $outdir/
-mv $outdir/EGA-Cryptor-$PKG_VERSION/ega-cryptor-$PKG_VERSION.jar $outdir/ega-cryptor.jar
-cp -f $RECIPE_DIR/ega-cryptor.py $outdir/ega-cryptor
-chmod +x $outdir/ega-cryptor
-ln -sf $outdir/ega-cryptor $PREFIX/bin
+ln -s  ega-cryptor-$PKG_VERSION.jar ega-cryptor.jar
+cp -R * $outdir/
+cp $RECIPE_DIR/ega-cryptor.py $outdir/ega-cryptor
+ln -s $outdir/ega-cryptor $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/ega-cryptor"
