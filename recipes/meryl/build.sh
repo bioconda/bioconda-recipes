cac22afb50b50515adca20f1cdc58224351754bf
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-20 07:59:41 -0600

    Update build.sh

--- recipes/meryl/build.sh
+++ recipes/meryl/build.sh
@@ -10,5 +10,5 @@ esac
 make -C src BUILD_DIR="$(pwd)" \
 	TARGET_DIR="${PREFIX}" \
 	CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3 -Wno-c++20-extensions ${ARCH_BUILD} -I${PREFIX}/include" \
+	CXXFLAGS="${CXXFLAGS} -O3 -Wno-c++20-extensions -Wno-inline-namespace-reopened-noninline -Wno-format ${ARCH_BUILD} -I${PREFIX}/include" \
 	LDFLAGS="${LDFLAGS} -fopenmp -L${PREFIX}/lib" -j"${CPU_COUNT}"
