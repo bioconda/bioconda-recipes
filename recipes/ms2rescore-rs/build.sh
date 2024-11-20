75465c8d96cfcd51cd42d09bacb8b3a5d9fbdfca
mencian <joshua.zhuang@yahoo.com>
2024-11-20 02:47:00 -0600

    clean up recipe

--- recipes/ms2rescore-rs/build.sh
+++ recipes/ms2rescore-rs/build.sh
@@ -4,16 +4,10 @@
 # -x = print every executed command
 set -ex
 
-# Use a custom temporary directory as home on macOS.
-# (not sure why this is useful, but people use it in bioconda recipes)
-if [ `uname` == Darwin ]; then
-  export HOME=`mktemp -d`
-fi
-
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 # Build the package using maturin - should produce *.whl files.
-maturin build --interpreter $PYTHON -b pyo3 --release --strip
+maturin build --interpreter "${PYTHON}" -b pyo3 --release --strip
 
 # Install *.whl files using pip
 $PYTHON -m pip install target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
