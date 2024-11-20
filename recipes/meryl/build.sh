e31eaa2f645e64aa08452ada3b956b6a71ddcd95
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-20 06:48:07 -0600

    Update build.sh

--- recipes/meryl/build.sh
+++ recipes/meryl/build.sh
@@ -2,8 +2,13 @@
 
 set -xe
 
+ARCH_BUILD="-m64 -msse4.1"
+case $(uname -m) in
+    arm64|aarch64) ARCH_BUILD="" ;;
+esac
+
 make -C src BUILD_DIR="$(pwd)" \
 	TARGET_DIR="${PREFIX}" \
 	CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	CXXFLAGS="${CXXFLAGS} -O3 -Wno-c++20-extensions ${ARCH_BUILD} -I${PREFIX}/include" \
 	LDFLAGS="${LDFLAGS} -fopenmp -L${PREFIX}/lib" -j"${CPU_COUNT}"
