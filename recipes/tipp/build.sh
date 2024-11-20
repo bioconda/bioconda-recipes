3ef0795bc18498f4880032606b823b8ea15e0f37
Anupam Gautam <gautam@ibminode05.Cs.Uni-Tuebingen.De>
2024-11-20 21:57:31 +0100

    update

--- recipes/tipp/build.sh
+++ recipes/tipp/build.sh
@@ -3,52 +3,23 @@
 # Exit on any error
 set -ex
 
-# Initialize and update all submodules
-#git submodule update --init
-
 # Define variables for directories
+# define kmc directory  
+
 KMC_DIR="src/kmc3"
 
-# Unset Conda-specific environment variables during KMC build
-OLD_PREFIX=$PREFIX
-unset PREFIX
-unset CONDA_PREFIX
-
-# Enter the KMC directory, initialize and update its submodule
-cd "$KMC_DIR"
-#git submodule update --init
-
-# Explicitly build Cloudflares zlib to ensure it's used in KMC build
-cd 3rd_party/cloudflare
-./configure
-make libz.a
-
-# Go back to KMC directory
-cd ../..
-
-# Set flags to use Cloudflare zlib
-CFLAGS="-I$(pwd)/3rd_party/cloudflare"
-LDFLAGS="-L$(pwd)/3rd_party/cloudflare"
-export CFLAGS LDFLAGS
-
-# Build KMC with the correct zlib
-make  -j "${CPU_COUNT}"
-
-# Unset the flags after the KMC build to prevent issues later
-unset CFLAGS
-unset LDFLAGS
-
-# Restore the environment variables
-export PREFIX=$OLD_PREFIX
-export CONDA_PREFIX=$OLD_PREFIX
-
-# Go back to the src directory
-cd ../..
+mkdir $KMC_DIR/bin
+cp $BUILD_PREFIX/bin/kmc* $KMC_DIR/bin/
+cp $BUILD_PREFIX/lib/libkmc_core.a $KMC_DIR/bin/
 
 # Build seqtk
 SEQTK_DIR="src/seqtk"
 cd "$SEQTK_DIR"
-make  -j "${CPU_COUNT}"
+
+export C_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+make CC=${CC} CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" all -j "${CPU_COUNT}"
 
 # Go back to the src directory
 cd ..
@@ -64,6 +35,11 @@ echo "Copying binaries to PREFIX..."
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/bin/kmc3
 mkdir -p $PREFIX/bin/seqtk
+
+for file in *.pl; do
+  sed -i.bak '1 s|^#!/usr/bin/perl$|#!/usr/bin/env perl|' "$file"
+done
+
 cp graph.plot.r readskmercount TIPP.pl TIPP_polish.pl html2repeatbed.pl MSA.plot.r TIPP_plastid.pl TIPP_telomere_backup.pl readskmercount.cpp telomeres.visulization.r TIPP_plastid.v2.1.pl TIPP_telomere.pl "$PREFIX/bin/" || { echo "Failed to copy specific binaries to PREFIX"; exit 1; }
 cp -r kmc3/bin $PREFIX/bin/kmc3/
 cp seqtk/seqtk $PREFIX/bin/seqtk/
