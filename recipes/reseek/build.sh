626cf356aa530e3278c72b42f62a3b10a59fe102
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-11-23 17:11:13 -0800

    Try using `MACOSX_DEPLOYMENT_TARGET`

--- recipes/reseek/build.sh
+++ recipes/reseek/build.sh
@@ -5,6 +5,10 @@ mkdir -p ${PREFIX}/bin
 cd src || exit 1
 echo "0" > gitver.txt
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.15
+fi  
+
 cp ${RECIPE_DIR}/vcxproj_make.py .
 chmod +x vcxproj_make.py
 ./vcxproj_make.py --openmp --cppcompiler ${CXX} --ccompiler ${CC}
