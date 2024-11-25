1b7a343db9cc3ee3bab65ca054a754875e3384b8
mencian <joshua.zhuang@yahoo.com>
2024-11-25 02:33:08 -0600

    Rebuild star recipe

--- recipes/star/build.sh
+++ recipes/star/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
+
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -std=c++14 -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+if [[ "$(uname)" == "Darwin" ]]; then
     echo "Installing STAR for OSX."
     mkdir -p $PREFIX/bin
-    cp bin/MacOSX_x86_64/* $PREFIX/bin
+    cp -rf bin/MacOSX_x86_64/* $PREFIX/bin
 else 
     echo "Building STAR for Linux"
     mkdir -p $PREFIX/bin
@@ -10,24 +16,24 @@ else
 
     AMD64_SIMD_LEVELS=("avx2" "avx" "sse4.1" "ssse3" "sse3")
 
-    if [ "$(arch)" == "x86_64" ]; then
+    if [[ "$(arch)" == "x86_64" ]]; then
         for SIMD in ${AMD64_SIMD_LEVELS[@]}; do
-            make -j "$(nproc)" CXXFLAGSextra="-m$SIMD" STAR STARlong
-            cp STAR $PREFIX/bin/STAR-$SIMD
-            cp STARlong $PREFIX/bin/STARlong-$SIMD
+            make -j"$(nproc)" CXX="${CXX}" CXXFLAGSextra="-m$SIMD" CXXFLAGS="${CXXFLAGS}" STAR STARlong
+            cp -rf STAR $PREFIX/bin/STAR-$SIMD
+            cp -rf STARlong $PREFIX/bin/STARlong-$SIMD
             make clean
         done
-        make STAR STARlong
-        cp STAR $PREFIX/bin/STAR-plain
-        cp STARlong $PREFIX/bin/STARlong-plain
-        cp ../simd-dispatch.sh $PREFIX/bin/STAR
-        cp ../simd-dispatch.sh $PREFIX/bin/STARlong
+        make -j"${CPU_COUNT}" STAR STARlong
+        cp -rf STAR $PREFIX/bin/STAR-plain
+        cp -rf STARlong $PREFIX/bin/STARlong-plain
+        cp -rf ../simd-dispatch.sh $PREFIX/bin/STAR
+        cp -rf ../simd-dispatch.sh $PREFIX/bin/STARlong
     else
-        make -pj "$(nproc)" STAR STARlong
-        cp STAR $PREFIX/bin/STAR
-        cp STARlong $PREFIX/bin/STARlong
+        make -pj"$(nproc)" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" STAR STARlong
+        install -v -m 0755 STAR $PREFIX/bin
+        install -v -m 0755 STARlong $PREFIX/bin
     fi
 fi
 
-chmod +x $PREFIX/bin/STAR
-chmod +x $PREFIX/bin/STARlong
+chmod 0755 $PREFIX/bin/STAR
+chmod 0755 $PREFIX/bin/STARlong
