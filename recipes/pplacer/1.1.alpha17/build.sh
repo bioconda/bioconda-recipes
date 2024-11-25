0a18f3eb0f899a87de5c5a0535c8cf16e02e8bd2
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-25 18:05:00 +0100

    fake library

--- recipes/pplacer/1.1.alpha17/build.sh
+++ recipes/pplacer/1.1.alpha17/build.sh
@@ -5,3 +5,8 @@ mkdir -p $PREFIX/bin
 cd $SRC_DIR
 cp guppy pplacer rppr $PREFIX/bin
 chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
+
+if [ $unamestr == 'Darwin' ];
+then
+	cd $PREFIX/lib/; ln -s libgsl.25.dylib libgsl.0.dylib
+fi
