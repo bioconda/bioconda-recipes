2aa054f1bdb1736e8f50fe424162009d071aaacf
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-26 21:59:14 +0100

    undo rppr

--- recipes/pplacer/1.1.alpha17/build.sh
+++ recipes/pplacer/1.1.alpha17/build.sh
@@ -16,11 +16,6 @@ install_name_tool -change /usr/local/lib/libgsl.0.dylib $PREFIX/lib/libgsl.25.dy
 install_name_tool -change /usr/local/lib/libgslcblas.0.dylib  $PREFIX/lib/libcblas.3.dylib $PREFIX/bin/guppy
 install_name_tool -change /usr/local/lib/gcc/5/libgcc_s.1.dylib $PREFIX/lib/libgcc_s.1.dylib $PREFIX/bin/guppy
 
-install_name_tool -change /usr/lib/libsqlite3.dylib $PREFIX/lib/libsqlite3.0.dylib $PREFIX/bin/rppr
-install_name_tool -change /usr/local/lib/libgsl.0.dylib $PREFIX/lib/libgsl.25.dylib $PREFIX/bin/rppr
-install_name_tool -change /usr/local/lib/libgslcblas.0.dylib  $PREFIX/lib/libcblas.3.dylib $PREFIX/bin/rppr
-install_name_tool -change /usr/local/lib/gcc/5/libgcc_s.1.dylib $PREFIX/lib/libgcc_s.1.dylib $PREFIX/bin/rppr
-
 #otool -L $PREFIX/bin/pplacer
 #if [ $unamestr == 'Darwin' ];
 #then
