390778abbd9450223419ffd78e8f38487f0ffd93
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-26 12:20:08 +0100

    change lib dirs in pplacer binary and print more ls

--- recipes/pplacer/1.1.alpha17/build.sh
+++ recipes/pplacer/1.1.alpha17/build.sh
@@ -6,6 +6,15 @@ cd $SRC_DIR
 mv guppy pplacer rppr $PREFIX/bin
 chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
 
+#install_name_tool -change old new $PREFIX/bin/pplacer
+
+install_name_tool -change /usr/lib/libz.1.dylib $PREFIX/lib/libz.1.2.13.dylib $PREFIX/bin/pplacer
+install_name_tool -change /usr/lib/libsqlite3.dylib $PREFIX/lib/libsqlite3.0.dylib $PREFIX/bin/pplacer
+install_name_tool -change /usr/local/lib/libgsl.25.dylib $PREFIX/lib/libgsl.25.dylib $PREFIX/bin/pplacer
+install_name_tool -change /usr/local/lib/libgslcblas.0.dylib  $PREFIX/lib/libcblas.3.dylib $PREFIX/bin/pplacer
+#install_name_tool -change /usr/lib/libSystem.B.dylib
+#install_name_tool -change /usr/local/lib/gcc/5/libgcc_s.1.dylib
+
 touch $PREFIX/stefan || true
 ls -la /usr/lib/libz* >> $PREFIX/stefan || true
 ls -la /usr/lib/libsqlite3* >> $PREFIX/stefan || true
@@ -17,8 +26,12 @@ ls -la $PREFIX/lib/libz* >> $PREFIX/stefan || true
 ls -la $PREFIX/lib/libsqlite3* >> $PREFIX/stefan || true
 ls -la $PREFIX/lib/libgsl* >> $PREFIX/stefan || true
 ls -la $PREFIX/lib/libgslcblas* >> $PREFIX/stefan || true
+ls -la $PREFIX/lib/libcblas* >> $PREFIX/stefan || true
 ls -la $PREFIX/lib/libSystem* >> $PREFIX/stefan || true
-ls -la $PREFIX/lib/gcc/5/libgcc_s* >> $PREFIX/stefan || true
+ls -la $PREFIX/lib/gcc/5/libgcc* >> $PREFIX/stefan || true
+ls -la $PREFIX/lib/gcc/libgcc* >> $PREFIX/stefan || true
+ls -la $PREFIX/lib/gcc/* >> $PREFIX/stefan || true
+ls -la $PREFIX/lib/libgcc* >> $PREFIX/stefan || true
 #otool -L $PREFIX/bin/pplacer
 #if [ $unamestr == 'Darwin' ];
 #then
