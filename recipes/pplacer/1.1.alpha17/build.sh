18ccb6ed056f360215d012ecff97fa6bf3f8ee6c
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-26 14:04:41 +0100

    fix version number of libgsl

--- recipes/pplacer/1.1.alpha17/build.sh
+++ recipes/pplacer/1.1.alpha17/build.sh
@@ -10,7 +10,7 @@ chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
 
 install_name_tool -change /usr/lib/libz.1.dylib $PREFIX/lib/libz.1.2.13.dylib $PREFIX/bin/pplacer
 install_name_tool -change /usr/lib/libsqlite3.dylib $PREFIX/lib/libsqlite3.0.dylib $PREFIX/bin/pplacer
-install_name_tool -change /usr/local/lib/libgsl.25.dylib $PREFIX/lib/libgsl.25.dylib $PREFIX/bin/pplacer
+install_name_tool -change /usr/local/lib/libgsl.0.dylib $PREFIX/lib/libgsl.25.dylib $PREFIX/bin/pplacer
 install_name_tool -change /usr/local/lib/libgslcblas.0.dylib  $PREFIX/lib/libcblas.3.dylib $PREFIX/bin/pplacer
 #install_name_tool -change /usr/lib/libSystem.B.dylib
 install_name_tool -change /usr/local/lib/gcc/5/libgcc_s.1.dylib $PREFIX/lib/libgcc_s.1.dylib $PREFIX/bin/pplacer
