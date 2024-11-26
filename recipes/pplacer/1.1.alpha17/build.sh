2d3185b406d371cb808aaa342757dc722cdec857
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-26 15:14:29 +0100

    no dylib for sql

--- recipes/pplacer/1.1.alpha17/build.sh
+++ recipes/pplacer/1.1.alpha17/build.sh
@@ -10,7 +10,6 @@ chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
 if [ $unamestr == 'Darwin' ];
 then
   for binary in guppy pplacer rppr; do
-	install_name_tool -change /usr/lib/libsqlite3.dylib $PREFIX/lib/libsqlite3.0.dylib $PREFIX/bin/$binary;
 	install_name_tool -change /usr/local/lib/libgsl.0.dylib $PREFIX/lib/libgsl.25.dylib $PREFIX/bin/$binary;
 	install_name_tool -change /usr/local/lib/libgslcblas.0.dylib  $PREFIX/lib/libcblas.3.dylib $PREFIX/bin/$binary;
 	install_name_tool -change /usr/local/lib/gcc/5/libgcc_s.1.dylib $PREFIX/lib/libgcc_s.1.dylib $PREFIX/bin/$binary;
