518e67fcdda6402b795988953ef6db71e3b7c6d4
shang-hongyun <shanghonyu@163.com>
2024-11-26 16:33:41 +0800

    1126-2

--- recipes/phytop/build.sh
+++ recipes/phytop/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python -m pip install --no-deps --ignore-installed .
-python -m pip install . --no-deps --ignore-installed -vv
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
