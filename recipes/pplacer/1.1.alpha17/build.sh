af47ba9672b0c2591c2a9c064f319f39520f3b6d
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-26 08:59:35 +0100

    more diagnostics

--- recipes/pplacer/1.1.alpha17/build.sh
+++ recipes/pplacer/1.1.alpha17/build.sh
@@ -3,10 +3,18 @@
 mkdir -p $PREFIX/bin
 
 cd $SRC_DIR
-cp guppy pplacer rppr $PREFIX/bin
+mv guppy pplacer rppr $PREFIX/bin
 chmod +x $PREFIX/bin/{guppy,pplacer,rppr}
 
-if [ $unamestr == 'Darwin' ];
-then
-	cd /usr/local/lib/; ln -s $PREFIX/lib/libgsl.25.dylib ./libgsl.0.dylib
-fi
+touch $PREFIX/stefan
+ls -la /usr/lib/libz* >> /stefan
+ls -la /usr/lib/libsqlite3* >> /stefan
+ls -la /usr/local/lib/libgsl* >> /stefan
+ls -la /usr/local/lib/libgslcblas* >> /stefan
+ls -la /usr/lib/libSystem* >> /stefan
+ls -la /usr/local/lib/gcc/5/libgcc_s* >> /stefan
+#otool -L $PREFIX/bin/pplacer
+#if [ $unamestr == 'Darwin' ];
+#then
+#	cd /usr/local/lib/; ln -s $PREFIX/lib/libgsl.25.dylib ./libgsl.0.dylib
+#fi
