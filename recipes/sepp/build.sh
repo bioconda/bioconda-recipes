aab7af1c25574c365877bad0358371c294c45784
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-27 11:37:03 +0100

    update sepp: pull pplacer from matsen release and seppMerger.json from my sepp PR

--- recipes/sepp/build.sh
+++ recipes/sepp/build.sh
@@ -20,17 +20,27 @@ cp ./.sepp/upp.config $PREFIX/share/sepp/sepp/upp.config
 if [ $unamestr == 'Linux' ];
 then
 	sed -i 's@'"$PREFIX"'@/opt/anaconda1anaconda2anaconda3@g' $PREFIX/share/sepp/sepp/upp.config
+	wget https://github.com/matsen/pplacer/releases/download/v1.1.alpha17/pplacer-linux-v1.1.alpha17.zip
+	unzip pplacer-linux-v1.1.alpha17.zip
+	mv pplacer-Linux-v1.1.alpha17/pplacer $PREFIX/bin/
 elif [ $unamestr == 'Darwin' ];
 then
 	gsed -i 's@'"$PREFIX"'@/opt/anaconda1anaconda2anaconda3@g' $PREFIX/share/sepp/sepp/upp.config
+	wget https://github.com/matsen/pplacer/releases/download/v1.1.alpha17/pplacer-Darwin-v1.1.alpha17.zip
+	unzip pplacer-Darwin-v1.1.alpha17.zip
+	mv pplacer-Darwin-v1.1.alpha17-6-g5cecf99/pplacer $PREFIX/bin/
 fi
 
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
-# copy bundled binaries, but hmmer which should be provided by conda, into $PREFIX/bin/
+# copy bundled binaries, but hmmer which should be provided by conda (and pplacer as long as bioconda PR https://github.com/bioconda/bioconda-recipes/pull/52395 is not merged), into $PREFIX/bin/
 mkdir -p $PREFIX/bin/
-cp `cat $SRC_DIR/.sepp/main.config | grep "^path" -m 1 | grep -v "hmm" | cut -d "=" -f 2 | xargs dirname`/* $PREFIX/bin/
-cp `cat $SRC_DIR/.sepp/upp.config | grep "^path" -m 1 | grep -v "hmm" | cut -d "=" -f 2 | xargs dirname`/* $PREFIX/bin/
+cp `cat $SRC_DIR/.sepp/main.config | grep "^path" -m 1 | grep -v "hmm" | grep -v "pplacer" | cut -d "=" -f 2 | xargs dirname`/* $PREFIX/bin/
+cp `cat $SRC_DIR/.sepp/upp.config | grep "^path" -m 1 | grep -v "hmm" | grep -v "pplacer" | cut -d "=" -f 2 | xargs dirname`/* $PREFIX/bin/
+
+# as long as upstream does not merge my PR https://github.com/smirarab/sepp/pull/138, I manually obtain the fixed seppJsonMerger.jar here
+wget https://raw.githubusercontent.com/jlab/sepp/refs/heads/fix_merger/tools/merge/seppJsonMerger.jar
+mv seppJsonMerger.jar $PREFIX/bin/
 
 # configure run-sepp.sh for qiime2 fragment-insertion
 mv -v sepp-package/run-sepp.sh $PREFIX/bin/run-sepp.sh
