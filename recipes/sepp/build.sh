9b2f8a5475e74ad7c98552eda1a4f101a271ec05
Stefan Janssen <stefan.m.janssen@gmail.com>
2024-11-27 17:20:25 +0100

    also move guppy

--- recipes/sepp/build.sh
+++ recipes/sepp/build.sh
@@ -22,21 +22,21 @@ then
 	sed -i 's@'"$PREFIX"'@/opt/anaconda1anaconda2anaconda3@g' $PREFIX/share/sepp/sepp/upp.config
 	wget https://github.com/matsen/pplacer/releases/download/v1.1.alpha17/pplacer-linux-v1.1.alpha17.zip
 	unzip pplacer-linux-v1.1.alpha17.zip
-	mv pplacer-Linux-v1.1.alpha17/pplacer $PREFIX/bin/
+	mv pplacer-Linux-v1.1.alpha17/{pplacer,guppy} $PREFIX/bin/
 elif [ "$(uname)" == "Darwin" ];
 then
 	gsed -i 's@'"$PREFIX"'@/opt/anaconda1anaconda2anaconda3@g' $PREFIX/share/sepp/sepp/upp.config
 	wget https://github.com/matsen/pplacer/releases/download/v1.1.alpha17/pplacer-Darwin-v1.1.alpha17.zip
 	unzip pplacer-Darwin-v1.1.alpha17.zip
-	mv pplacer-Darwin-v1.1.alpha17-6-g5cecf99/pplacer $PREFIX/bin/
+	mv pplacer-Darwin-v1.1.alpha17-6-g5cecf99/{pplacer,guppy} $PREFIX/bin/
 fi
 
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
 
 # copy bundled binaries, but hmmer which should be provided by conda (and pplacer as long as bioconda PR https://github.com/bioconda/bioconda-recipes/pull/52395 is not merged), into $PREFIX/bin/
 mkdir -p $PREFIX/bin/
-cp -v `cat $SRC_DIR/.sepp/main.config | grep "^path" | grep -v "hmm" | grep -v "pplacer" | cut -d "=" -f 2` $PREFIX/bin/
-cp -v `cat $SRC_DIR/.sepp/upp.config | grep "^path" | grep -v "hmm" | grep -v "pplacer" | grep -v "run_pasta" | cut -d "=" -f 2` $PREFIX/bin/
+cp -v `cat $SRC_DIR/.sepp/main.config | grep "^path" | grep -v "hmm" | grep -v "pplacer" | grep -v "guppy" | cut -d "=" -f 2` $PREFIX/bin/
+cp -v `cat $SRC_DIR/.sepp/upp.config | grep "^path" | grep -v "hmm" | grep -v "pplacer" | grep -v "guppy" | grep -v "run_pasta" | cut -d "=" -f 2` $PREFIX/bin/
 
 # as long as upstream does not merge my PR https://github.com/smirarab/sepp/pull/138, I manually obtain the fixed seppJsonMerger.jar here
 wget https://raw.githubusercontent.com/jlab/sepp/refs/heads/fix_merger/tools/merge/seppJsonMerger.jar
