4b7ed44717cb94e8c46252c7eb0cc93bf1c1a601
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-27 21:07:57 -0600

    Update build.sh

--- recipes/repeatscout/build.sh
+++ recipes/repeatscout/build.sh
@@ -3,7 +3,7 @@ set -x -e
 
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="${{LDFLAGS}} -L${{PREFIX}}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p ${PREFIX}/bin
 make CC="${CC}" -j"${CPU_COUNT}"
