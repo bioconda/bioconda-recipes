d36ab6c7c89b701981fa1e584e468d57350ae03c
Stefan Bienert <stefan.bienert@unibas.ch>
2024-11-28 13:16:04 +0100

    macOS support

--- recipes/openstructure/build.sh
+++ recipes/openstructure/build.sh
@@ -6,6 +6,26 @@ sed -i.tmp 's/lib64/lib/g' cmake_support/OST.cmake
 # Use sed above patch as it should work for multiple OST versions.
 sed -i.tmp 's/"@Python_EXECUTABLE@"/`which python`/' scripts/ost_config.in
 
+if test "${target_platform}" = "osx-64"; then
+    ost_cmake_extra_args="-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
+else if test "${target_platform}" = "linux-64"; then
+         ost_cmake_extra_args="-DEIGEN3_INCLUDE_DIR=${BUILD_PREFIX}/include/eigen3 \
+                     -DSQLITE3_LIBRARY=${PREFIX}/lib/libsqlite3${SHLIB_EXT} \
+                     -DSQLITE3_INCLUDE_DIR=${PREFIX}/include \
+                     -DZLIB_LIBRARY=${PREFIX}/lib/libz${SHLIB_EXT} \
+                     -DZLIB_INCLUDE_DIR=${PREFIX}/include \
+                     -DPNG_LIBRARY=${PREFIX}/lib/libpng${SHLIB_EXT} \
+                     -DPNG_INCLUDE_DIR=${PREFIX}/include \
+                     -DFFTW_LIBRARY=${PREFIX}/lib/libfftw3${SHLIB_EXT} \
+                     -DFFTW_INCLUDE_DIR=${PREFIX}/include \
+                     -DTIFF_LIBRARY=${PREFIX}/lib/libtiff${SHLIB_EXT} \
+                     -DTIFF_INCLUDE_DIR=${PREFIX}/include"
+     else
+         echo "unsupported target platform: '${target_platform}'"
+         exit 1
+     fi
+fi
+
 cmake -Bbuild \
       -G "${CMAKE_GENERATOR}" \
       -DPREFIX="${PREFIX}" \
@@ -18,20 +38,11 @@ cmake -Bbuild \
       -DCMAKE_CXX_COMPILER_VERSION="${CMAKE_CXX_COMPILER_VERSION}" \
       -DPython_EXECUTABLE="${PYTHON}" \
       -DPYTHON_MODULE_PATH="${SP_DIR}" \
-      -DEIGEN3_INCLUDE_DIR="${BUILD_PREFIX}/include/eigen3" \
-      -DSQLITE3_LIBRARY="${PREFIX}/lib/libsqlite3${SHLIB_EXT}" \
-      -DSQLITE3_INCLUDE_DIR="${PREFIX}/include" \
-      -DZLIB_LIBRARY="${PREFIX}/lib/libz${SHLIB_EXT}" \
-      -DZLIB_INCLUDE_DIR="${PREFIX}/include" \
-      -DPNG_LIBRARY="${PREFIX}/lib/libpng${SHLIB_EXT}" \
-      -DPNG_INCLUDE_DIR="${PREFIX}/include" \
-      -DFFTW_LIBRARY="${PREFIX}/lib/libfftw3${SHLIB_EXT}" \
-      -DFFTW_INCLUDE_DIR="${PREFIX}/include" \
-      -DTIFF_LIBRARY="${PREFIX}/lib/libtiff${SHLIB_EXT}" \
-      -DTIFF_INCLUDE_DIR="${PREFIX}/include" \
-      -DUSE_RPATH=1
+      -DUSE_RPATH=1 \
+      ${ost_cmake_extra_args}
+
 cmake --build build --target all ${MAKEFLAGS}
-# TODO: make this curl
+
 curl -OL https://files.wwpdb.org/pub/pdb/data/monomers/components.cif.gz
 ${SRC_DIR}/build/stage/bin/chemdict_tool create ${SRC_DIR}/components.cif.gz compounds.chemlib pdb
 ${SRC_DIR}/build/stage/bin/chemdict_tool update ${SRC_DIR}/modules/conop/data/charmm.cif compounds.chemlib charmm
