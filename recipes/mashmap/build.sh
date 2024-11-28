6d909eff1dfeab70bf19f86286978a042b06f7a8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-27 20:07:59 -0600

    Update build.sh

--- recipes/mashmap/build.sh
+++ recipes/mashmap/build.sh
@@ -9,15 +9,16 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 export CPATH="${PREFIX}/include"
 
 if [[ `uname` == "Darwin" ]]; then
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=10.15 -D_LIBCPP_HAS_NO_LIBRARY_ALIGNED_ALLOCATION=1"
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=10.15 -D_LIBCPP_HAS_NO_LIBRARY_ALIGNED_ALLOCATION=1"
   export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else 
   export CONFIG_ARGS=""
 fi  
 
-cmake -H. -B"${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_VERBOSE_MAKEFILE=1 -DOPTIMIZE_FOR_NATIVE=0 \
   -DUSE_HTSLIB=1 -DCMAKE_CXX_COMPILER="${CXX}" \
   -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
   "${CONFIG_ARGS}"
-cmake --build "${PREFIX}" -j "${CPU_COUNT}" -v
+cmake --build build --target install -j "${CPU_COUNT}" -v
