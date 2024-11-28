4ca859ae334a808151d290d61fc6b2ffd4c21e86
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-11-27 21:07:10 -0600

    Update build.sh

--- recipes/ninja-nj/build.sh
+++ recipes/ninja-nj/build.sh
@@ -3,15 +3,18 @@ set -x -e
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-register -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ $(uname) == "Darwin" ]]; then
-	export CXXFLAGS="${CXXFLAGS}"
-	export CXXFLAGS="${CXXFLAGS} -Wno-deprecated-register -Wno-unused-but-set-variable -Wno-unused-variable -Wno-format -Wno-deprecated-declarations"
+	export CXXFLAGS="${CXXFLAGS} -Wno-unused-but-set-variable -Wno-unused-variable -Wno-format -Wno-deprecated-declarations"
 else
 	export CXXFLAGS="${CXXFLAGS} -lrt"
 fi
+
+case $(uname -m) in
+	x86_64) export CXXFLAGS="${CXXFLAGS} -mssse3" ;;
+esac
 
 mkdir -p ${PREFIX}/bin
 
