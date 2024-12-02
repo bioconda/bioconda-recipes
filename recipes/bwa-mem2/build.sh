8d6fccdd3944a239c9e11febde6123d713e1dcc2
Pablo <pablo.aledo@seqera.io>
2024-12-02 06:59:52 +0100

    revert to test
    Signed-off-by: Pablo <pablo.aledo@seqera.io>

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -9,24 +9,7 @@ if [[ $OSTYPE == "darwin"* ]]; then
     sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset_s.c
     sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/wmemset_s.c
 fi
-
-case "$(uname -m)" in
-  x86_64)
-      LIBS="${LDFLAGS}" make CC="${CC}" CXX="${CXX}" multi ;;
-  aarch64)
-      if [ "$(uname -s)" == Darwin ]
-      then
-        LIBS="${LDFLAGS}" make CC="${CC}" CXX="${CXX}" multi
-      else
-        mkdir ext/simde
-        wget https://github.com/simd-everywhere/simde-no-tests/archive/refs/tags/v0.8.2.tar.gz -O - | tar -xvz
-        mv simde-no-tests-0.8.2/* ext/simde/
-        LIBS="${LDFLAGS}" make arch=native EXE=bwa-mem2 CC="${CC}" CXX="${CXX}" all
-      fi
-      ;;
-  *)
-      echo "Error detecting architecture" ;;
-esac
+LIBS="${LDFLAGS}" make CC="${CC}" CXX="${CXX}" multi
 
 mkdir -p $PREFIX/bin
 cp bwa-mem2* $PREFIX/bin
