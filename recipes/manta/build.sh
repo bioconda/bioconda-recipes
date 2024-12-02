027229eaff1d895b927cc9fcc5a6aa97357f6dcf
David Lecomber <david.lecomber@arm.com>
2024-12-02 14:58:49 +0000

    making build script build from source always

--- recipes/manta/build.sh
+++ recipes/manta/build.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 set -eu
 
-if [[ $(arch) == "aarch64" ]]; then
-    mkdir build ; cd build
-    BOOST_ROOT=${PREFIX} ../configure --prefix=${PREFIX}
-    make -j 
-    make install
+mkdir build ; cd build
+BOOST_ROOT=${PREFIX} ../configure --prefix=${PREFIX}
+make -j 
+make install
     
-else
-    outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-    mkdir -p $outdir
-    mkdir -p $PREFIX/bin
-
-    cp -r * $outdir
-    rm -rf $outdir/share/demo
-    sed -i.bak 's/__file__/os.path.realpath(__file__)/' $outdir/bin/configManta.py
-    ln -s $outdir/bin/configManta.py $PREFIX/bin
-    ln -s $outdir/libexec/convertInversion.py $PREFIX/bin
-    ln -s $outdir/libexec/denovo_scoring.py $PREFIX/bin
-fi
