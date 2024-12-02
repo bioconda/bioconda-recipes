978534269e3436b05f595106aa9da44541042093
David Lecomber <david.lecomber@arm.com>
2024-12-02 16:03:56 +0000

    limit make -j to number of cores

--- recipes/manta/build.sh
+++ recipes/manta/build.sh
@@ -3,6 +3,6 @@ set -eu
 
 mkdir build ; cd build
 BOOST_ROOT=${PREFIX} ../configure --prefix=${PREFIX}
-make -j 
+make -j ${CPU_COUNT}			# fails to build / breaks the infrastructure for default '-j ' with no limit.
 make install
     
