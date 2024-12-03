ff2e3b0bf4a3f40bc7c82d0765cf8c440146ce33
David Lecomber <david.lecomber@arm.com>
2024-12-03 16:10:35 +0000

    Add linux-aarch64

--- recipes/mosdepth/build.sh
+++ recipes/mosdepth/build.sh
@@ -11,6 +11,17 @@ if [[ ${target_platform}  == osx-64 ]] ; then
     cd mosdepth-${PKG_VERSION}
     nimble install -y "docopt@0.7.0"
     nimble build -y --verbose -d:release
+elif [[ ${target_platform}  == linux-aarch64 ]] ; then
+    curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-1-6/linux_arm64.tar.xz
+    tar -xzf linux_arm64.tar.xz
+    cd nim-1.6.*
+    export PATH="$PWD/bin:$PATH"
+    cd ..
+    curl -SL https://github.com/brentp/mosdepth/archive/refs/tags/v${PKG_VERSION}.tar.gz -o mosdepth-latest.tar.gz
+    tar -xzf mosdepth-latest.tar.gz
+    cd mosdepth-${PKG_VERSION}
+    nimble install -y "docopt@0.7.0"
+    nimble build -y --verbose -d:release
 else
     curl -SL https://github.com/brentp/mosdepth/releases/download/v$PKG_VERSION/mosdepth_d4 -o mosdepth
     chmod +x mosdepth
