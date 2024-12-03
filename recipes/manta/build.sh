776e4a61af493e7860170ea2a94ee8d0265fc811
David Lecomber <david.lecomber@arm.com>
2024-12-03 12:24:59 +0000

    Make verbose build for debug purpose

--- recipes/manta/build.sh
+++ recipes/manta/build.sh
@@ -8,7 +8,7 @@ export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make PREFIX="${PREFIX}" CXX="${CXX}" \
+make PREFIX="${PREFIX}" CXX="${CXX}" VERBOSE=1\
   CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" \
   INCLUDE_DIRS="$PREFIX/include" \
   LIBRARY_DIRS="$PREFIX/lib" -j"${CPU_COUNT}"
