f8eac72b383654f115cedebed49280d96bab4592
David Lecomber <david.lecomber@arm.com>
2024-12-03 11:47:53 +0000

    Attempt fix of mac missing std includes

--- recipes/manta/build.sh
+++ recipes/manta/build.sh
@@ -3,6 +3,14 @@ set -eu
 
 mkdir build ; cd build
 BOOST_ROOT=${PREFIX} ../configure --prefix=${PREFIX}
-make -j ${CPU_COUNT}			# fails to build / breaks the infrastructure for default '-j ' with no limit.
+
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+make PREFIX="${PREFIX}" CXX="${CXX}" \
+  CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" \
+  INCLUDE_DIRS="$PREFIX/include" \
+  LIBRARY_DIRS="$PREFIX/lib" -j"${CPU_COUNT}"
 make install
     
