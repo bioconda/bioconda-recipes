949ad25b26a4c19813f0b769aa9f95bb6ada4312
mencian <joshua.zhuang@yahoo.com>
2024-12-03 18:17:19 -0600

    parsnp: add arm64/aarch64 builds

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-mkdir -p  "$PREFIX/bin"
-mkdir -p  "$PREFIX/bin/bin"
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/bin/bin"
+export M4="${BUILD_PREFIX}/bin/m4"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
 cd muscle
 
-./autogen.sh
-if [ `uname` == Darwin ]; then
-    sed -i '' '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
-    ./configure --prefix=$PREFIX CXXFLAGS='-fopenmp' --disable-shared 
+autoreconf -if 
+if [[ `uname` == Darwin ]]; then
+    sed -i.bak '' '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
+    ./configure --prefix="$PREFIX" CXX="${CXX}" \
+	CXXFLAGS='${CXXFLAGS} -O3 -fopenmp -I${PREFIX}/include' \
+	LDFLAGS="${LDFLAGS}" --disable-shared 
 else
-    sed -i '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
-    ./configure --prefix=$PREFIX CXXFLAGS='-fopenmp'
+    sed -i.bak '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
+    ./configure --prefix="$PREFIX" CXX="${CXX}" \
+	CXXFLAGS='${CXXFLAGS} -O3 -fopenmp -I${PREFIX}/include' \
+	LDFLAGS="${LDFLAGS}"
 fi
-make -j
+make -j"${CPU_COUNT}"
 make install
 
 cd ..
 
-# Patch automake update. remove for future release 
-#sed '1 s/.*/AC_INIT([parsnp],[1.6.1])/' configure.ac
-#sed '2 s/.*/AM_INIT_AUTOMAKE([-Wall])/' configure.ac
-
-./autogen.sh
-./configure --with-libmuscle=$PREFIX/include
-make LDADD="$LDADD -lMUSCLE-3.7"
+autoreconf -if
+./configure CXX="${CXX}" CXXFLAGS='${CXXFLAGS} -O3 -I${PREFIX}/include' \
+	LDFLAGS="${LDFLAGS}" --with-libmuscle="$PREFIX/include"
+make LDADD="$LDADD -lMUSCLE-3.7" -j"${CPU_COUNT}"
 make install
 
 rm -R muscle/libMUSCLE
 rm -R muscle/autom4te.cache
 rm -R muscle/m4
 
-cp parsnp $PREFIX/bin 
-cp extend.py $PREFIX/bin
-cp logger.py $PREFIX/bin
-cp partition.py $PREFIX/bin
-cp src/parsnp_core $PREFIX/bin/bin
-cp template.ini $PREFIX/bin
+install -v -m 0755 parsnp extend.py logger.py partition.py template.ini $PREFIX/bin
+install -v -m 0755 src/parsnp_core $PREFIX/bin/bin
 cp -R bin $PREFIX/bin 
 cp -R examples $PREFIX/bin
