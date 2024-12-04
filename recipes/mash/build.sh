fe61e6bc7c9d6931efcadb0b8bc3378f8948a884
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-03 18:57:02 -0600

    Update build.sh

--- recipes/mash/build.sh
+++ recipes/mash/build.sh
@@ -4,10 +4,11 @@ export M4="${BUILD_PREFIX}/bin/m4"
 
 #libkj.a needs to link against librt
 sed -i.bak "s/pthread/pthread -lrt/g" Makefile.in
+rm -rf *.bak
 autoreconf -if
 ./configure CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3" \
-	--with-capnp="$PREFIX" --with-gsl="$PREFIX" --prefix="$PREFIX" \
-	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" \
-	CPPFLAGS="-I${PREFIX}/include"
-make -j"${CPU_COUNT}"
+	--with-capnp="${PREFIX}" --with-gsl="${PREFIX}" --prefix="${PREFIX}" \
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
+	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+make
 make install
