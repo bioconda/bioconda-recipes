c98fe0275f50dbcc24eef5f283d06dafcfaf0b22
mencian <joshua.zhuang@yahoo.com>
2024-12-03 18:45:12 -0600

    Mash: add arm64/aarch64 builds

--- recipes/mash/build.sh
+++ recipes/mash/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export M4="${BUILD_PREFIX}/bin/m4"
+
 #libkj.a needs to link against librt
 sed -i.bak "s/pthread/pthread -lrt/g" Makefile.in
-./bootstrap.sh
-./configure --with-capnp=$PREFIX --with-gsl=$PREFIX --prefix=$PREFIX
-make
+autoreconf -if
+./configure CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3" \
+	--with-capnp="$PREFIX" --with-gsl="$PREFIX" --prefix="$PREFIX" \
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" \
+	CPPFLAGS="-I${PREFIX}/include"
+make -j"${CPU_COUNT}"
 make install
