5465621b3b00e9c77d58d5bb7e1fbefe4789a0bf
dslarm <38504854+dslarm@users.noreply.github.com>
2024-12-04 09:39:27 +0000

    Samtools 0.1.19 and 0.1.18 - linux-aarch64 and osx-arm64 (0.1.18) support (#52557)
    * Bump build number, will this bump a rebuild on linux-aarch64?
    
    * update legacy builds for samtools to support linux-aarch64 and osx-arm64
    
    * don't build for osx - fails with
    
    error: /opt/mambaforge/envs/bioconda/conda-bld/samtools_1733249731471/_build_env/bin/x86_64-apple-darwin13.4.0-install_name_tool: changing install names or rpaths can't be redone for: /opt/mambaforge/envs/bioconda/conda-bld/samtools_1733249731471/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_place/include/bam/misc/ace2sam (for architecture x86_64) because larger updated load commands do not fit (the program must be relinked, and you may need to use -headerpad or -headerpad_max_install_names)

--- recipes/samtools/0.1.18/build.sh
+++ recipes/samtools/0.1.18/build.sh
@@ -3,6 +3,6 @@
 # does not affect the linux build
 # this is has been fixed in newer versions: https://github.com/samtools/samtools/issues/22
 sed -i.bak -E 's/(inline void __ks_insertsort_)/static \1/g' ksort.h
-make INCLUDES="-I. -I$PREFIX/include -I$PREFIX/include/ncurses" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
+make -j ${CPU_COUNT} INCLUDES="-I. -I$PREFIX/include -I$PREFIX/include/ncurses" LIBCURSES="-L$PREFIX/lib -lncurses -ltinfo" LIBPATH="-L$PREFIX/lib" CC=$CC CFLAGS="-g -Wall -O2 -I$PREFIX/include -L$PREFIX/lib"
 mkdir -p $PREFIX/bin
 mv samtools $PREFIX/bin
