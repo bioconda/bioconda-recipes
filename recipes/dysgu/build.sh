152ba194787abcc07e045e6ce7d4793a7c2a80fa
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2024-12-05 11:10:12 +0000

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -3,4 +3,11 @@ export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L{$PREFIX}/lib"
 export C_INCLUDE_PATH=${PREFIX}/include
 
+if [[ "$target_platform" == osx-64 ]]; then
+    SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    echo "-isysroot is ${SDKROOT}"
+    export CFLAGS="${CFLAGS} -isysroot ${SDKROOT}"
+    export LDFLAGS="${LDFLAGS} -isysroot ${SDKROOT}"
+fi
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
