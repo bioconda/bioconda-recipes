d5e23d04831bae40d2fb90f62806aecb73702dd9
Limh616 <liminghao616@163.com>
2024-12-07 10:35:06 +0800

    Add files via upload

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xe
 make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-#chmod +x ./bin/fc-virus
-chmod 777 ./bin/fc-virus
+chmod 777 ./bin/virdig
 mkdir -p ${PREFIX}/bin
-cp -f ./bin/fc-virus ${PREFIX}/bin
+cp -f ./bin/virdig ${PREFIX}/bin
