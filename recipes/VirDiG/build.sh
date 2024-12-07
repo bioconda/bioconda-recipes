f1a947a70dcc7093d0d3c55ff68ca37715bfe12c
Limh616 <liminghao616@163.com>
2024-12-07 10:25:45 +0800

    Delete recipes/VirDiG directory

--- recipes/VirDiG/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-chmod 777 ./bin/VirDiG
-mkdir -p ${PREFIX}/bin
-cp -f ./bin/VirDiG ${PREFIX}/bin
