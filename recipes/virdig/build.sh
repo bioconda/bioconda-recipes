2c46165d8285bc78d872a8b31fb5d0b2c17d0a73
Limh616 <liminghao616@163.com>
2024-12-07 10:38:09 +0800

    Update build.sh

--- recipes/virdig/build.sh
+++ recipes/virdig/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-chmod 777 ./bin/VirDiG
+chmod 777 ./bin/virdig
 mkdir -p ${PREFIX}/bin
-cp -f ./bin/VirDiG ${PREFIX}/bin
+cp -f ./bin/virdig ${PREFIX}/bin
