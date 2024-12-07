ea9ae99dede072119f01b72858b4f8b6033e8030
Limh616 <liminghao616@163.com>
2024-12-07 10:41:17 +0800

    Update build.sh

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-chmod 777 ./bin/virdig
+chmod 777 ./bin/fc-virus
 mkdir -p ${PREFIX}/bin
-cp -f ./bin/virdig ${PREFIX}/bin
+cp -f ./bin/fc-virus ${PREFIX}/bin
