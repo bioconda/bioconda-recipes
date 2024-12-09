dc69438abf77ff96337accf4bdfbf50d69552399
Limh616 <liminghao616@163.com>
2024-12-09 21:12:52 +0800

    Update build.sh

--- virdig/build.sh
+++ virdig/build.sh
@@ -1,1 +1,4 @@
-g++ -o virdig assemble2.cpp load_reads2.cpp transcript.cpp utility.cpp 
+#!/bin/bash
+set -xe
+
+make -j"${CPU_COUNT}" CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
