bd6069bdfe4e15d12492287e57cbab06ce9dfe40
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-09 17:19:24 -0600

    Update build.sh

--- recipes/sylph-utils/build.sh
+++ recipes/sylph-utils/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
-cp -rf eukaryote prokaryote virus ${PREFIX}/bin
+cp -rfv eukaryote/* prokaryote/* virus/* ${PREFIX}/bin
 install -v -m 0755 merge_sylph_taxprof.py sylph_to_taxprof.py ${PREFIX}/bin
