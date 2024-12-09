eb878b87d88a020b82bb3fff729f6b7e14dc4773
Martin Grigorov <martin-g@users.noreply.github.com>
2024-12-09 14:17:41 +0200

    Use cargo-bundle-licenses

--- recipes/mmft/build.sh
+++ recipes/mmft/build.sh
@@ -3,12 +3,6 @@ set -ex
 
 RUST_BACKTRACE=full
 
-if [ "$(uname)" == "Darwin" ]; then
-    export HOME=`pwd`
-    echo "HOME is $HOME"
-
-    # according to https://github.com/rust-lang/cargo/issues/2422#issuecomment-198458960 removing circle ci default configuration solves cargo trouble downloading crates
-    #git config --global --unset url.ssh://git@github.com.insteadOf
-fi
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 cargo install -v --locked --root "$PREFIX" --path . --no-track
