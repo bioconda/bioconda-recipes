6aedb1909efdc29f590a20008bfd4025e695b79a
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-12-09 15:41:55 +0200

    rabbitqcplus: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/rabbitqcplus/build.sh
+++ recipes/rabbitqcplus/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -xe
+
 export CC=${CC:-gcc}
 export CXX=${CXX:-g++}
 
 export LIBRARY_DIRS="$LIBRARY_DIRS $PREFIX/lib"
 
-make BIOCONDA=1
+make -j"${CPU_COUNT}" BIOCONDA=1
 make install
 
