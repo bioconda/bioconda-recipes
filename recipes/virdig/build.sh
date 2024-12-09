7e2d8fdf826d7fddb97d831c9b7985e4d40006b6
Limh616 <liminghao616@163.com>
2024-12-09 19:43:02 +0800

    Update build.sh

--- recipes/virdig/build.sh
+++ recipes/virdig/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
-make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-chmod 777 ./bin/virdig
-mkdir -p ${PREFIX}/bin
-cp -f ./bin/virdig ${PREFIX}/bin
+#make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
+g++ -o virdig assemble2.cpp load_reads2.cpp transcript.cpp utility.cpp 
+chmod 777 ./virdig
+
