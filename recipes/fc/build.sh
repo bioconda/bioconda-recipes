a4d3a98d9bea061b02e3d13b8c28390b797a3e6a
Limh616 <liminghao616@163.com>
2024-12-09 19:31:03 +0800

    Delete recipes/fc/build.sh

--- recipes/fc/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-chmod 777 ./bin/fc-virus
-mkdir -p ${PREFIX}/bin
-cp -f ./bin/fc-virus ${PREFIX}/bin
