37b397d017dc5a9409590254856c984ff19f2032
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-09 10:55:15 -0600

    Update build.sh

--- recipes/sansa/build.sh
+++ recipes/sansa/build.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS" make -j${CPU_COUNT} CXX="${CXX}" prefix="${PREFIX}" install
+CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS -O3 -I${PREFIX}/include" make -j"${CPU_COUNT}" CXX="${CXX}" prefix="${PREFIX}" install
