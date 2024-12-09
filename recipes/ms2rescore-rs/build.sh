55bd49891342f39603b5bec470c7ed2a2decb24b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-09 11:36:43 -0600

    Update build.sh

--- recipes/ms2rescore-rs/build.sh
+++ recipes/ms2rescore-rs/build.sh
@@ -7,7 +7,7 @@ set -ex
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 # Build the package using maturin - should produce *.whl files.
-maturin build --interpreter "${PYTHON}" -b pyo3 --release --strip
+maturin build --interpreter "${PYTHON}" -b pyo3 --release --strip --compatibility "manylinux2014"
 
 # Install *.whl files using pip
 $PYTHON -m pip install target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
