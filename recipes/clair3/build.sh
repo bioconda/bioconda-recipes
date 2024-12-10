e910eb8d474206e60b527b6e936edf4274ba9e2b
mencian <joshua.zhuang@yahoo.com>
2024-12-10 09:43:58 -0600

    clean up patches

--- recipes/clair3/build.sh
+++ recipes/clair3/build.sh
@@ -7,9 +7,9 @@ export CPATH="${PREFIX}/include"
 
 mkdir -pv $PREFIX/bin
 cp -rf clair3 models preprocess postprocess scripts shared $PREFIX/bin
-install -v -m 0755 clair3.py $PREFIX/bin/
-install -v -m 0755 run_clair3.sh $PREFIX/bin/
-make all GCC="${CC}" GXX="${CXX}" PREFIX="${PREFIX}" LDFLAGS="${LDFLAGS}"
+install -v -m 0755 clair3.py $PREFIX/bin
+install -v -m 0755 run_clair3.sh $PREFIX/bin
+make all GCC="${CC}" GXX="${CXX}" PREFIX="${PREFIX}" python="${PYTHON}"
 
 install -v -m 0755 longphase $PREFIX/bin
 cp -rf libclair3* $PREFIX/bin
