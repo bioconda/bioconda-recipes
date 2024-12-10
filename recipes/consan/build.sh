aa186bd702af611570b928462846e5b14afa8231
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-09 22:29:23 -0600

    Update build.sh

--- recipes/consan/build.sh
+++ recipes/consan/build.sh
@@ -7,18 +7,10 @@ cd src
 make clean
 cd ..
 
-make CC="$CC" CFLAGS="$CFLAGS -O3 -Wno-implicit-function-declaration -fcommon -Wno-implicit-int" -j"${CPU_COUNT}"
+make CC="$CC" CFLAGS="$CFLAGS -O3 -fcommon -Wno-implicit-function-declaration -Wno-implicit-int -Wno-pointer-to-int-cast -Wno-header-guard -Wno-deprecated-non-prototype" -j"${CPU_COUNT}"
 make install
 
-binaries="\
-bstats \
-comppair \
-conus_compare   \
-conus_train   \
-pModel   \
-scompare  \
-sfold  \
-strain_ml \
-"
-
-for i in $binaries; do install -v -m 0755 bin/$i $PREFIX/bin; done
+install -v -m 0755 src/boot/bstats $PREFIX/bin
+install -v -m 0755 src/utilities/comppair src/utilities/pModel $PREFIX/bin
+install -v -m 0755 src/conus-1.1/conus_compare src/conus-1.1/conus_train $PREFIX/bin
+install -v -m 0755 src/scompare src/sfold src/strain_ml $PREFIX/bin
