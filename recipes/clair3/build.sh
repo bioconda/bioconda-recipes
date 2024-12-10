26247a5d1ca6d869692350f48f33c30facc24ca4
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-09 18:05:18 -0600

    Update build.sh

--- recipes/clair3/build.sh
+++ recipes/clair3/build.sh
@@ -4,7 +4,7 @@ mkdir -pv $PREFIX/bin
 cp -rf clair3 models preprocess postprocess scripts shared $PREFIX/bin
 install -v -m 0755 clair3.py $PREFIX/bin/
 install -v -m 0755 run_clair3.sh $PREFIX/bin/
-make CC="${CC}" CXX="${CXX}" PREFIX="${PREFIX}" -j"${CPU_COUNT}"
+make all CC="${CC}" CXX="${CXX}" PREFIX="${PREFIX}" -j"${CPU_COUNT}"
 
 install -v -m 0755 longphase $PREFIX/bin
 cp -rf libclair3* $PREFIX/bin
