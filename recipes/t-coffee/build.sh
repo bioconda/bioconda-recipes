d7a9d8b59da3114334726728cebafd38554fd69c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-09 21:23:25 -0600

    Update build.sh

--- recipes/t-coffee/build.sh
+++ recipes/t-coffee/build.sh
@@ -9,15 +9,24 @@
 #
 set -eux -o pipefail
 
-SHARE_DIR="${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
+SHARE_DIR="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 OS=$(./install get_os)
 
 mkdir -p "${PREFIX}/bin"
 
-./install all -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install t_coffee -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install tcoffee -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install rcoffee -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install seq_reformat -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install expresso -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install psicoffee -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install trmsd -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install accurate -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install 3dcoffee -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
+./install mcoffee -tcdir="${SHARE_DIR}" CC="$CXX" CFLAGS="$CFLAGS -O3 -Wno-register -L${PREFIX}/lib"
 
 # llvm-otool -l fails for these plugins on macosx
-if [[ "$OS" = macosx ]]; then
+if [[ `uname` == "Darwin" ]]; then
     for bad_plug in probconsRNA prank
     do
 	rm -f "${SHARE_DIR}/plugins/macosx/${bad_plug}"
