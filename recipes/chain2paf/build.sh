a12771cde8b986e53cb2040e613d3c24eb094a0e
AndreaGuarracino <andreaguarracino@outlook.com>
2024-12-16 10:48:33 -0600

    explicit linking flags for OSX

--- recipes/chain2paf/build.sh
+++ recipes/chain2paf/build.sh
@@ -2,6 +2,11 @@
 
 set -xe
 
+if [[ -n "$OSX_ARCH" ]]; then
+    # Set this so that it doesn't fail with open ssl errors
+    export RUSTFLAGS="-C link-arg=-undefined -C link-arg=dynamic_lookup"
+fi
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 cargo install --verbose --path . --root ${PREFIX}
