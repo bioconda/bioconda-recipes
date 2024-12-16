88605d5c7516e05dc3f3028784c0c610bed02abc
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-12-16 16:51:05 +0200

    Pass `--disable-sse` for beagle-lib on ARM64
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/beagle-lib/3.1.2/build.sh
+++ recipes/beagle-lib/3.1.2/build.sh
@@ -8,7 +8,16 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
+case $(uname -m) in
+	aarch64 | arm64)
+		ARCH_OPTS="--disable-sse"
+		;;
+	*)
+		ARCH_OPTS=""
+		;;
+esac
+
 ./autogen.sh
-./configure CC="${CC}" CXX="${CXX}" --prefix=${PREFIX} --with-jdk=${PREFIX} --disable-march-native
+./configure CC="${CC}" CXX="${CXX}" --prefix=${PREFIX} --with-jdk=${PREFIX} --disable-march-native ${ARCH_OPTS}
 make -j ${CPU_COUNT}
 make install
