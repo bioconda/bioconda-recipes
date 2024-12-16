59cb6cab9571089e36c85fd9181ea7e5b8d4a94d
Jacob Morrison <Jacob.Morrison@vai.org>
2024-12-16 14:45:52 -0500

    try a new cmake build method

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/lib"
+mkdir -p "${PREFIX}/include"
+
 # Needed for building utils dependency
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
 
-mkdir -p "${PREFIX}/bin"
-mkdir -p build
-cd build || exit 1
-cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
-#make CC="${CC}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -O3"
-make CC="${CC}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -O3" install
+cmake -S . -B build \
+    -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_FLAGS="${CFLAGS}"
+
+cmake --build build --target install -v
 
 # Needed to run asset builder
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${PREFIX}/bin/build_biscuit_QC_assets.pl
