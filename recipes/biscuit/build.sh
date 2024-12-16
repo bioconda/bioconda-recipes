eb4a12e34f2ba0bd91866487694d24be188a5c41
Jacob Morrison <Jacob.Morrison@vai.org>
2024-12-16 15:00:59 -0500

    revert to old build with some mods

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -8,14 +8,13 @@ mkdir -p "${PREFIX}/include"
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
+export CFLAGS="${CFLAGS} -O3"
 
-cmake -S . -B build \
-    -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_C_FLAGS="${CFLAGS}"
-
-cmake --build build --target install -v
+mkdir -p build
+cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
+#make CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
+make CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" install
 
 # Needed to run asset builder
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${PREFIX}/bin/build_biscuit_QC_assets.pl
