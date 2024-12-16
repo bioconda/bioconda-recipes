0ab871f531237c1bcb4c6c978992cf2d40e4f22d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-12-16 15:57:01 +0200

    Add compiler('cxx') for beagle-lib
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/beagle-lib/3.1.2/build.sh
+++ recipes/beagle-lib/3.1.2/build.sh
@@ -5,9 +5,10 @@ set -xe
 export INCLUDES="-I{PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
 ./autogen.sh
-./configure --prefix=${PREFIX} --with-jdk=${PREFIX} --disable-march-native
+./configure CC="${CC}" CXX="${CXX}" --prefix=${PREFIX} --with-jdk=${PREFIX} --disable-march-native
 make -j ${CPU_COUNT}
 make install
