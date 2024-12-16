531da1358562fb0c47c5ef3a3522b3fcb11d339a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-16 03:57:25 -0600

    Update build.sh

--- recipes/tiberius/build.sh
+++ recipes/tiberius/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
-cp -R $SRC_DIR/bin/* ${PREFIX}/bin/
+cp -rf $SRC_DIR/bin/dev/* ${PREFIX}/bin
+cp -Rf $SRC_DIR/bin/* ${PREFIX}/bin
 chmod +x ${PREFIX}/bin/tiberius.py
-ln -s ${PREFIX}/bin/tiberius.py ${PREFIX}/bin/tiberius
-sed -i.bak '1s|^|#!/usr/bin/env python3\n|' ${PREFIX}/bin/tiberius
-rm -rf ${PREFIX}/bin/*.bak
+ln -sf ${PREFIX}/bin/tiberius.py ${PREFIX}/bin/tiberius
