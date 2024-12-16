a9ca98f275c4b9eaa9e2fb6257d9d084e642a19a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-16 04:23:55 -0600

    Update build.sh

--- recipes/tiberius/build.sh
+++ recipes/tiberius/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
-cp -rf $SRC_DIR/bin/dev/* ${PREFIX}/bin
+cp -rf $SRC_DIR/bin/dev/convert_weights.py ${PREFIX}/bin
 cp -Rf $SRC_DIR/bin/* ${PREFIX}/bin
 chmod +x ${PREFIX}/bin/tiberius.py
+chmod +x ${PREFIX}/bin/convert_weights.py
 ln -sf ${PREFIX}/bin/tiberius.py ${PREFIX}/bin/tiberius
