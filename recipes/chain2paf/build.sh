3b0708d8ffae7e3627e4cca8ea89de4254427f88
AndreaGuarracino <andreaguarracino@outlook.com>
2024-12-17 09:09:44 -0600

    clean build.sh

--- recipes/chain2paf/build.sh
+++ recipes/chain2paf/build.sh
@@ -2,11 +2,6 @@
 
 set -xe
 
-if [[ -n "$OSX_ARCH" ]]; then
-    # Set this so that it doesn't fail with open ssl errors
-    export RUSTFLAGS="-C link-arg=-undefined -C link-arg=dynamic_lookup"
-fi
-
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 cargo install --verbose --path . --root ${PREFIX}
