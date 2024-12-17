598cf50e240aa87c1ad742a09c452904e403ba36
mencian <joshua.zhuang@yahoo.com>
2024-12-17 16:01:06 -0600

    clean up recipe

--- recipes/canu/build.sh
+++ recipes/canu/build.sh
@@ -8,8 +8,18 @@ export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/lib/perl5/site_perl/canu"
+
+cp -rfv src/pipelines/canu/*.pm "${PREFIX}/lib/perl5/site_perl/canu"
 
 cd src
 make CC="${CC}" CXX="${CXX} -O3 -I${PREFIX}/include" -j"${CPU_COUNT}"
 
-install -v -m 0755 ${SRC_DIR}/build/bin/canu "${PREFIX}/bin"
+cd ../build/bin
+install -v -m 0755 canu canu-time draw-tig canu.defaults dumpBlob ovStoreBuild ovStoreConfig ovStoreBucketizer \
+	ovStoreSorter ovStoreIndexer ovStoreDump ovStoreStats sqStoreCreate sqStoreDumpFASTQ sqStoreDumpMetaData \
+	tgStoreCompress tgStoreDump tgStoreLoad tgTigDisplay loadCorrectedReads loadTrimmedReads loadErates \
+	seqrequester meryl overlapInCore overlapInCorePartition overlapConvert overlapImport overlapPair \
+	edalign mhapConvert mmapConvert filterCorrectionOverlaps generateCorrectionLayouts filterCorrectionLayouts \
+	falconsense errorEstimate splitHaplotype trimReads splitReads mergeRanges overlapAlign findErrors \
+	fixErrors findErrors correctOverlaps bogart layoutReads utgcns layoutToPackage alignGFA "${PREFIX}/bin"
