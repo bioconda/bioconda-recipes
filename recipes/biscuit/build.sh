2567b538fdd5b9f8aedf2432c763a6a0543777c3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-17 22:37:40 -0600

    Update build.sh

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -3,14 +3,22 @@
 # Needed for building utils dependency
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -pthread -lrt -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
+
+if [[ `uname` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
 
 mkdir -p "${PREFIX}/bin"
 mkdir -p build
 cd build || exit 1
-cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" ..
-make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS} -O3"
-make install
+cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="${CC}" \
+  -DCMAKE_C_FLAGS="${CFLAGS} -O3 -lrt" \
+  "${CONFIG_ARGS}"
+cmake --build . --target install -j "${CPU_COUNT}" -v
 
 # Needed to run asset builder
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${PREFIX}/bin/build_biscuit_QC_assets.pl
