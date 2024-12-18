f0c0e79af119fe9120733a2f4103b5d69a8d4c55
mencian <joshua.zhuang@yahoo.com>
2024-12-17 22:13:37 -0600

    Salmon: add arm64 build

--- recipes/salmon/build.sh
+++ recipes/salmon/build.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 set -eu -o pipefail
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/lib
+export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
 
-export CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib -I${PREFIX}/include"
+if [[ `uname` == "Darwin" ]]; then
+	export CFLAGS="${CFLAGS} -Wno-unused-command-line-argument -Wno-unused-parameter"
+	export CONFIG_ARGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
 cmake -S . -B build \
-	-DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" \
 	-DCONDA_BUILD=TRUE \
+	-DBOOST_ROOT="${PREFIX}" \
+	-DCEREAL_INCLUDE_DIR="${PREFIX}/include" \
+	-DLIB_GFF_INCLUDE_DIR="${PREFIX}/include" \
 	-DLIBSTADEN_LDFLAGS="-L${PREFIX}/lib" \
-	-DBoost_NO_BOOST_CMAKE=ON \
-	-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
-	-DBoost_NO_SYSTEM_PATHS=ON \
-	-DNO_IPO=TRUE
+	-DSTADEN_INCLUDE_DIR="${PREFIX}/include" \
+	-DSTADEN_LIBRARY="${PREFIX}/lib/libstaden-read.a" \
+	-DSTADEN_VERSION="1.15.0" \
+	-DNO_IPO=TRUE \
+	"${CONFIG_ARGS}"
 
 cmake --build build/ --target install -v
