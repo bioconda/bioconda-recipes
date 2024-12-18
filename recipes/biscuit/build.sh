204c2cc5a3f97b824846e1c3649f58cb48aa4fe5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-18 00:49:01 -0600

    Update build.sh

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -7,16 +7,19 @@ export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
 
 if [[ `uname` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-	export CFLAGS="${CFLAGS} -O3"
 else
 	export CONFIG_ARGS=""
 	export CFLAGS="${CFLAGS} -O3 -lrt"
 fi
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+mkdir build
+cd build
+cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="${CC}" \
   -DCMAKE_C_FLAGS="${CFLAGS}" "${CONFIG_ARGS}"
-cmake --build build --target install -j 1 -v
+make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS}"
+make install
+#cmake --build build --target install -j 1 -v
 
 # Needed to run asset builder
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${PREFIX}/bin/build_biscuit_QC_assets.pl
