e61aefc7b85bb26b9186140c973198ff351d680f
Michael Hall <michael@mbh.sh>
2024-12-19 09:54:06 +1000

    Delete recipes/needletail/build.sh

--- recipes/needletail/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# Build wheel and write to `dist`
-maturin build --interpreter "${PYTHON}" --features python --release --strip --out dist
-
-# Install Python library
-${PYTHON} -m pip install dist/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
