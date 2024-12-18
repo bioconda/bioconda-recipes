1c345f998c860eed6e9968c61ec0e03884106cef
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2024-12-17 18:54:02 -0600

    Update build.sh

--- recipes/parsnp/build.sh
+++ recipes/parsnp/build.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
-mkdir -p  "$PREFIX/bin"
-mkdir -p  "$PREFIX/bin/bin"
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/bin/bin"
 
 cd muscle
 
 ./autogen.sh
-if [ `uname` == Darwin ]; then
-    sed -i '' '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
-    ./configure --prefix=$PREFIX CXXFLAGS='-fopenmp' --disable-shared 
+autoreconf -if
+if [[ `uname` == "Darwin" ]]; then
+    sed -i.bak '' '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
+    ./configure --prefix="$PREFIX" CXX="${CXX}" CXXFLAGS='${CXXFLAGS} -O3 -fopenmp' --disable-shared 
 else
-    sed -i '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
-    ./configure --prefix=$PREFIX CXXFLAGS='-fopenmp'
+    sed -i.bak '/bin_PROGRAMS/d' ./libMUSCLE/Makefile.am
+    ./configure --prefix="$PREFIX" CXX="${CXX}" CXXFLAGS='${CXXFLAGS} -O3 -fopenmp'
 fi
-make -j
+make -j"${CPU_COUNT}"
 make install
 
 cd ..
 
-# Patch automake update. remove for future release 
-#sed '1 s/.*/AC_INIT([parsnp],[1.6.1])/' configure.ac
-#sed '2 s/.*/AM_INIT_AUTOMAKE([-Wall])/' configure.ac
-
 ./autogen.sh
-./configure --with-libmuscle=$PREFIX/include
-make LDADD="$LDADD -lMUSCLE-3.7"
+autoreconf -if
+./configure CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3" --with-libmuscle="$PREFIX/include"
+make LDADD="$LDADD -lMUSCLE-3.7" -j"${CPU_COUNT}"
 make install
 
 rm -R muscle/libMUSCLE
 rm -R muscle/autom4te.cache
 rm -R muscle/m4
 
-cp parsnp $PREFIX/bin 
+install -v -m 0755 parsnp $PREFIX/bin
 cp extend.py $PREFIX/bin
 cp logger.py $PREFIX/bin
 cp partition.py $PREFIX/bin
-cp src/parsnp_core $PREFIX/bin/bin
+install -v -m 0755 src/parsnp_core $PREFIX/bin/bin
 cp template.ini $PREFIX/bin
 cp -R bin $PREFIX/bin 
 cp -R examples $PREFIX/bin
