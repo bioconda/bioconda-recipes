c9a7ae4beb3c0941f08ae26dce5a418ebee9485f
mencian <joshua.zhuang@yahoo.com>
2024-12-18 05:10:13 -0600

    edit patch

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -4,28 +4,31 @@
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
+export RECIPE_DIR="${RECIPE_DIR}"
+export CC="${CC}"
 
 if [[ `uname` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-	export CFLAGS="${CFLAGS} -O3"
+	export CFLAGS="${CFLAGS} -O3 -Wunused-command-line-argument -L${PREFIX}/lib"
 else
 	export CONFIG_ARGS=""
-	export CFLAGS="${CFLAGS} -O3 -lrt"
+	export CFLAGS="${CFLAGS} -O3 -Wunused-command-line-argument -L${PREFIX}/lib -lrt"
 fi
 
 case $(uname -m) in
-    arm64|aarch64) cp -rfv ${RECIPE_DIR}/sse2neon.h ${SRC_DIR}/lib/aln/ ;;
+	arm64|aarch64) cp -rfv ${RECIPE_DIR}/sse2neon.h ${SRC_DIR}/lib/aln/ ;;
 esac
 
-mkdir build
-cd build
-cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+#mkdir build
+#cd build
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" -DCMAKE_INCLUDE_PATH="${PREFIX}/include" \
 	-DCMAKE_LIBRARY_PATH="${PREFIX}/lib" -DZLIB_ROOT="${PREFIX}" \
 	"${CONFIG_ARGS}"
-make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS}"
-make install
+#make CC="${CC} ${LDFLAGS}" CFLAGS="${CFLAGS}"
+#make install
+cmake --build build --target install -v
 
 # Needed to run asset builder
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${PREFIX}/bin/build_biscuit_QC_assets.pl
