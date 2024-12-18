e53d289b9140aebba85b6b576b547928451ef638
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-12-18 15:10:36 +0200

    Build latest commit. 0.9.13 does no support Go modules
    and new Go compilers do not support non-modules.
    
    `GO111MODULE=off` does not help
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/allhic/build.sh
+++ recipes/allhic/build.sh
@@ -2,10 +2,6 @@
 
 set -xe
 
-# we need go.mod because we use a modern Go compiler and allhic v0.9.13 does not have it yet
-wget https://raw.githubusercontent.com/tanghaibao/allhic/e495b41169fe4d9e3285a959a4ce5a0003cc210f/go.mod
-wget https://raw.githubusercontent.com/tanghaibao/allhic/e495b41169fe4d9e3285a959a4ce5a0003cc210f/go.sum
-
 go get -v -t ./...
 go build -o allhic -v cmd/main.go
 
