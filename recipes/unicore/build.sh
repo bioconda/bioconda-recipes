9809609899added4a2f84b3c84b07422a9917859
Daniel DW Kim <endix1029@gmail.com>
2024-12-19 12:51:39 +0900

    fix typo

--- recipes/unicore/build.sh
+++ recipes/unicore/build.sh
@@ -9,7 +9,7 @@ SOURCE_PY="${PWD}/src/py/predict_3Di_encoderOnly.py"
 TARGET_PY="${PREFIX}/etc/predict_3Di_encoderOnly.py"
 
 mkdir -p "${PREFIX}/etc"
-cp ${SOURCE_CFG} ${TARGET_CFD}
+cp ${SOURCE_CFG} ${TARGET_CFG}
 cp ${SOURCE_PY} ${TARGET_PY}
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
