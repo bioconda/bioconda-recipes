2ddebe00de245723dd57bcf8bc3a188434af2938
Daniel DW Kim <endix1029@gmail.com>
2024-12-19 10:55:29 +0900

    pytorch runtime dependencies

--- recipes/unicore/build.sh
+++ recipes/unicore/build.sh
@@ -5,7 +5,6 @@ set -x -e
 SOURCE_BIN="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/bin/unicore"
 TARGET_BIN="${PREFIX}/bin/unicore"
 
-pip install torch transformers sentencepiece protobuf
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo build --release
 cp ${SOURCE_BIN} ${TARGET_BIN}
