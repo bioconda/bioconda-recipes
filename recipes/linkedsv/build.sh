c298c1e2cfee5c11f8087346747de966ba857567
Pavel V. Dimens <19176506+pdimens@users.noreply.github.com>
2024-12-20 11:42:56 -0500

    add bones of recipe

---
+++ recipes/linkedsv/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}
+
+## cpp ##
+cd src
+
+mkdir -p lib/
+cd htslib-1.3/
+./configure
+make
+make check
+cp libhts.a ../lib/
+cd .. 
+
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cluster_reads cluster_reads.cpp tk.cpp cgranges.cpp -l z -pthread
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o extract_barcode_info extract_barcode_info.cpp -l hts -l z -l m -l pthread
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o output_bam_coreinfo output_bam_coreinfo.cpp -l hts -l z -l m -l pthread
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o remove_sparse_nodes remove_sparse_nodes.cpp tk.cpp cgranges.cpp -lz
+
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cal_hap_read_depth_from_bcd21 cal_hap_read_depth_from_bcd21.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o grid_overlap grid_overlap.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cal_read_depth_from_bcd21 cal_read_depth_from_bcd21.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cal_barcode_depth_from_bcd21 cal_barcode_depth_from_bcd21.cpp tk.cpp cgranges.cpp -l z
+
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cal_twin_win_bcd_cnt cal_twin_win_bcd_cnt.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cal_centroid_from_read_depth cal_centroid_from_read_depth.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cal_2d_overlapping_barcodes cal_2d_overlapping_barcodes.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o small_deletion_detection small_deletion_detection.cpp cnv.cpp tk.cpp cgranges.cpp -l z
+g++ -g -O0 -std=c++11 -I ./include/ -L ./lib/ -o cnv_detection cnv_detection.cpp cnv.cpp tk.cpp cgranges.cpp -l z 
+
+mv cluster_reads extract_barcode_info output_bam_coreinfo remove_sparse_nodes ${PREFIX}
+mv cal_hap_read_depth_from_bcd21 grid_overlap cal_read_depth_from_bcd21 cal_barcode_depth_from_bcd21 ${PREFIX}
+mv cal_twin_win_bcd_cnt cal_centroid_from_read_depth cal_2d_overlapping_barcodes cnv_detection small_deletion_detection ${PREFIX}
