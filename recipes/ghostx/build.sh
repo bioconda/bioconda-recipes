af1eef16c07c535ebd4e3b7f4f53f96fb6cffc2d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2024-12-23 18:21:19 +0200

    Set C++ standard to 14
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/ghostx/build.sh
+++ recipes/ghostx/build.sh
@@ -4,7 +4,7 @@ set -xe
 
 cd ./src/
 
-CC="${CC}" CXX="${CXX}" CC_FLAGS="${CFLAGS} -D_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR" make -j"${CPU_COUNT}"
+CC="${CC}" CXX="${CXX}" CC_FLAGS="${CFLAGS}" make -j"${CPU_COUNT}"
 
 mkdir -p $PREFIX/bin
 install -m 755 ghostx $PREFIX/bin
