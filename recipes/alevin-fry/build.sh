acf204b36bd10c3714e12b7d919d3a90c7ec5452
Rob Patro <rob-p@users.noreply.github.com>
2024-12-24 01:39:08 -0500

    Update build.sh

--- recipes/alevin-fry/build.sh
+++ recipes/alevin-fry/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -euo
+
 export CFLAGS="${CFLAGS} -fcommon"
-export CXXFLAGS="${CFLAGS} -fcommon"
+export CXXFLAGS="${CFXXLAGS} -fcommon"
+
+if [ "$(uname)" == "Darwin" ]; then
+  export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
+  export CXXFLAGS="${CXXFLAGS} -fno-define-target-os-macros"
+fi
+
+
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
