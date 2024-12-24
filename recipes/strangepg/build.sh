862665c86978cba6fc2e7c5658830fd3be2de9fe
Konstantinn Bonnet <50845450+qwx9@users.noreply.github.com>
2024-12-24 09:46:29 +0100

    Update strangepg to 0.8.16, add osx and aarch64 support (#52951)

--- recipes/strangepg/build.sh
+++ recipes/strangepg/build.sh
@@ -2,8 +2,11 @@
 
 set -xe
 
-export C_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LDLIBS="-ldl -lpthread"
+OS=$(uname)
+if [[ ${OS} == "Linux" ]]; then
+	export C_INCLUDE_PATH="${PREFIX}/include"
+	export LIBRARY_PATH="${PREFIX}/lib"
+	export LDLIBS="-ldl -lpthread"
+fi
 
 make CC="${CC}" PREFIX="${PREFIX}" -j ${CPU_COUNT} install
