d4be01cd6837976263ef466b8a2f9af770ac2a7a
aliciaaevans <alicia.evans@nih.gov>
2024-12-30 14:13:54 -0500

    bioconductor-flowcore: set flag to handle binary function

--- recipes/bioconductor-flowcore/build.sh
+++ recipes/bioconductor-flowcore/build.sh
@@ -8,4 +8,8 @@ CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+if [[ "$(uname)" == Darwin ]]; then
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION" $R CMD INSTALL --build .
+else
+    $R CMD INSTALL --build .
+fi
