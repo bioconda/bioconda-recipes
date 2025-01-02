a534c185c9b56320f0d797a45883fb1044a7c1c1
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-01-02 14:16:38 -0800

    Add samstrip

---
+++ recipes/samstrip/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -euo
+
+set -xe
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+# Build statically linked binary with Rust
+RUST_BACKTRACE=1 cargo install -v --locked --no-track --root $PREFIX --path .#!/bin/bash -euo
+
+set -xe
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+# Build statically linked binary with Rust
+RUST_BACKTRACE=1 cargo install -v --locked --no-track --root $PREFIX --path .
