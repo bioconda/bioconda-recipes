4799358a2b0f9177117a904b6fe01662a07c664a
mencian <joshua.zhuang@yahoo.com>
2025-01-07 02:57:17 -0600

    add patches

--- recipes/divvier/build.sh
+++ recipes/divvier/build.sh
@@ -4,5 +4,9 @@ set -eu -o pipefail
 echo ${PREFIX}
 mkdir -p ${PREFIX}/bin
 
+if [[ $(uname) == "Linux" ]] ; then
+	cp -rf ${RECIPE_DIR}/bionj.cxx .
+fi
+
 make CC="$CC -fcommon" CPP="$CXX -fcommon" -j"${CPU_COUNT}"
 install -v -m 0755 divvier ${PREFIX}/bin
