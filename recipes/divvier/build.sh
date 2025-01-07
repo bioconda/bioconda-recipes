93cd3b60fe5e949fce1cb7cc1689f8ae31a14a68
mencian <joshua.zhuang@yahoo.com>
2025-01-07 02:18:05 -0600

    Build divvier on aarch64/arm64

--- recipes/divvier/build.sh
+++ recipes/divvier/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
+
 echo ${PREFIX}
 mkdir -p ${PREFIX}/bin
-make CC="$CC -fcommon" CPP="$CXX -fcommon"
-cp divvier ${PREFIX}/bin
+
+make CC="$CC -fcommon" CPP="$CXX -fcommon" -j"${CPU_COUNT}"
+install -v -m 0755 divvier ${PREFIX}/bin
