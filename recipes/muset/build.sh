a88cce0a3207582bbd3a5d53b30dd3a45f5ce4f0
Camila Duitama González <camiladuitama@gmail.com>
2025-01-07 18:11:38 +0100

    Add muset recipe v0.5.1
    - Added run_exports for dependency pinning
    - Specified build script location
    - Added pkg-config and libstdcxx-ng to dependencies

--- recipes/muset/build.sh
+++ recipes/muset/build.sh
@@ -1,19 +1,25 @@
+# Add more verbose error checking to build.sh
 #!/bin/bash
 set -ex
 
-# Print environment variables
-env
+# Print detailed environment information
+echo "Environment Variables:"
+env | sort
 
-# Print compiler version
+# Compiler and build tool versions
+echo "Compiler Version:"
 $CXX --version
 
-# Print CMake version
+echo "CMake Version:"
 cmake --version
 
-# Create build directory
-mkdir build && cd build
+echo "GCC Version:"
+gcc --version
 
-# Run CMake with verbose output
+# Create build directory
+mkdir -p build && cd build
+
+# Detailed CMake configuration
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCONDA_BUILD=ON \
@@ -21,14 +27,21 @@ cmake .. \
     -DARCH_NATIVE=OFF \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_CXX_STANDARD_REQUIRED=ON \
-    -DCMAKE_VERBOSE_MAKEFILE=ON
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    2>&1 | tee cmake_config.log
 
-# Print contents of build directory
-echo "Contents of build directory:"
+# Check CMake configuration log
+cat cmake_config.log
+
+# Print build directory contents
+echo "Build Directory Contents:"
 ls -R
 
-# Build with verbose output
-make VERBOSE=1
+# Build with maximum verbosity
+make VERBOSE=1 2>&1 | tee build_log.txt
+
+# Check build log
+cat build_log.txt
 
 # Install
 make install
