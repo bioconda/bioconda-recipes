80e5c4afda7df28561bcaab8c509a15ce7716564
aliciaaevans <alicia.evans@nih.gov>
2025-01-09 09:23:27 -0500

    add some more stuff, because why not?

--- recipes/bioconductor-rbowtie2/build.sh
+++ recipes/bioconductor-rbowtie2/build.sh
@@ -8,4 +8,5 @@ CXX=$CXX -I${PREFIX}/include -L${PREFIX}/lib
 CXX98=$CXX -I${PREFIX}/include -L${PREFIX}/lib
 CXX11=$CXX -I${PREFIX}/include -L${PREFIX}/lib
 CXX14=$CXX -I${PREFIX}/include -L${PREFIX}/lib" > ~/.R/Makevars
+export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/lib
 $R CMD INSTALL --build .
