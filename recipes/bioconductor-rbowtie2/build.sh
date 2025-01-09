1f18053db74af27cba5eb2d47f5512877e161e8f
aliciaaevans <alicia.evans@nih.gov>
2025-01-09 08:43:20 -0500

    try settings on all lines

--- recipes/bioconductor-rbowtie2/build.sh
+++ recipes/bioconductor-rbowtie2/build.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.R
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX -I${PREFIX}/include -L${PREFIX}/lib
-CXX98=$CXX
-CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
+CXX98=$CXX -I${PREFIX}/include -L${PREFIX}/lib
+CXX11=$CXX -I${PREFIX}/include -L${PREFIX}/lib
+CXX14=$CXX -I${PREFIX}/include -L${PREFIX}/lib" > ~/.R/Makevars
 $R CMD INSTALL --build .
