2fa37da9e41025b33fa6541ccb8d5eb13fd3b963
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-01-09 22:10:58 -0600

    Update build.sh

--- recipes/htstream/build.sh
+++ recipes/htstream/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+
 if [[ `uname` == "Darwin" ]]; then
   export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
@@ -12,8 +16,6 @@ cmake -S . -B build \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_CXX_COMPILER="${CXX}" \
   -DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3" \
-  -DCMAKE_LIBRARY_PATH="${PREFIX}/lib" \
-  -DCMAKE_INCLUDE_PATH="${PREFIX}/include" \
   "${CONFIG_ARGS}"
 
 cmake --build build --target install -j "${CPU_COUNT}"
