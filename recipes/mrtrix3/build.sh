71f91168aec19a988c28bdf9a008a47432353fd2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-10 12:34:11 +0200

    Pass CXX as an env var to ./configure
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -2,14 +2,14 @@
 
 set -xe
 
-export CFLAGS="-idirafter ${PREFIX}/include" 
-export LDFLAGS="-L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -idirafter ${PREFIX}/include" 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export LINKFLAGS="-L${PREFIX}/lib"
 export EIGEN_CFLAGS="-idirafter ${PREFIX}/include/eigen3"
 
 mkdir -p "${PREFIX}"/{bin,lib,share}
 
-./configure -conda 
+CC=${CC} CXX=${CXX} CFLAGS=${CXXFLAGS} ./configure -conda 
 ./build 
 cp -r bin lib share "${PREFIX}"
 
