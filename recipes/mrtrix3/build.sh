0ed2df29bec96ad9c04f4bbdd7f3b00aceed8eff
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-10 11:23:31 +0200

    Add recipe for mrtrix3
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

---
+++ recipes/mrtrix3/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -xe
+
+export CFLAGS="-idirafter ${PREFIX}/include" 
+export LDFLAGS="-L${PREFIX}/lib"
+export LINKFLAGS="-L${PREFIX}/lib"
+export EIGEN_CFLAGS="-idirafter ${PREFIX}/include/eigen3"
+
+mkdir -p "${PREFIX}"/{bin,lib,share}
+
+./configure -conda 
+./build 
+cp -r bin lib share "${PREFIX}"
