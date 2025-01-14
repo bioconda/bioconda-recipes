73e6c4c70febd34d216e97bc4de5d7c6eebe5f94
David Lecomber <david.lecomber@arm.com>
2025-01-14 13:32:55 +0000

    simplify per review

--- recipes/muscle/3.8.1551/build.sh
+++ recipes/muscle/3.8.1551/build.sh
@@ -2,9 +2,4 @@
 
 make GPP=$CXX -j"${CPU_COUNT}"
 
-binaries="\
-muscle \
-"
-
-mkdir -p $PREFIX/bin
-for i in $binaries; do cp $SRC_DIR/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
+install -m 0755 muscle ${PREFIX}/bin/ 
