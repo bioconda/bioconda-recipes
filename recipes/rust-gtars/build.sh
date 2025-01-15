304416337221f02e341e76e9b70552fddb284a59
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-15 11:30:19 +0200

    cd into gtars/ folder before collecting the third party licences
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/rust-gtars/build.sh
+++ recipes/rust-gtars/build.sh
@@ -6,7 +6,11 @@ if [ "$(uname)" == "Darwin" ]; then
     export HOME=`pwd`
 fi
 
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+pushd gtars
+
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
+
+popd
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1 
