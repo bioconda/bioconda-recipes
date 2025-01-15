35c2f9e684ac813a569530b2f1c875d9d8689886
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-15 11:14:29 +0200

    rust-gtars: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/rust-gtars/build.sh
+++ recipes/rust-gtars/build.sh
@@ -6,6 +6,8 @@ if [ "$(uname)" == "Darwin" ]; then
     export HOME=`pwd`
 fi
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1 
-cargo install --no-track --verbose --root "${PREFIX}" --path ./gtars 
+cargo install --no-track --verbose --root "${PREFIX}" --path ./gtars
