b3654dd21946eb4b753b12de537797d2053fdf63
Joe Angell <joe.d.angell@gmail.com>
2025-01-14 18:08:51 -0800

    trying to fix osx build

--- recipes/htstream/build.sh
+++ recipes/htstream/build.sh
@@ -6,7 +6,7 @@ mkdir build && cd build
 
 cmake \
   -DCMAKE_BUILD_TYPE=RELEASE \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  -DCMAKE_CXX_FLAGS='-Wno-vla' \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_LIBRARY_PATH=${PREFIX}/lib \
   -DCMAKE_INCLUDE_PATH=${PREFIX}/include ..
