df1752e513564c65ccc0c8808623e55cd36bb52f
gbayarri <genis.bayarri@irbbarcelona.org>
2025-01-17 16:10:52 +0100

    [biobb_haddock] update 5.0.0

--- recipes/biobb_haddock/build.sh
+++ recipes/biobb_haddock/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python3 -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-python3 -m pip install haddock3 --no-deps --ignore-installed --no-cache-dir -vvv
+python3 -m pip install haddock3==2024.10.0b7 --no-deps --ignore-installed --no-cache-dir -vvv
