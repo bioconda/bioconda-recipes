d92818ff0c7af5796e6432831dd332b21a2a419c
Camila Duitama González <camiladuitama@gmail.com>
2025-01-17 19:49:55 +0100

    Simplify build.sh script

--- recipes/muset/build.sh
+++ recipes/muset/build.sh
@@ -1,47 +1,29 @@
-# Add more verbose error checking to build.sh
 #!/bin/bash
 set -ex
 
-# Print detailed environment information
-echo "Environment Variables:"
-env | sort
+# Print environment and build information
+echo "Build Environment:"
+env | grep -E "PREFIX|CC|CXX"
 
-# Compiler and build tool versions
-echo "Compiler Version:"
+echo "Compiler Versions:"
+$CC --version
 $CXX --version
-
-echo "CMake Version:"
 cmake --version
 
-echo "GCC Version:"
-gcc --version
-
-# Create build directory
+# Create and navigate to build directory
 mkdir -p build && cd build
 
-# Detailed CMake configuration
+# Configure with CMake
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCONDA_BUILD=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -DARCH_NATIVE=OFF \
-    -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_CXX_STANDARD_REQUIRED=ON \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
-    2>&1 | tee cmake_config.log
+    -DCMAKE_CXX_STANDARD=17
 
-# Check CMake configuration log
-cat cmake_config.log
+# Build the project
+make -j4
 
-# Print build directory contents
-echo "Build Directory Contents:"
-ls -R
-
-# Build with maximum verbosity
-make VERBOSE=1 2>&1 | tee build_log.txt
-
-# Check build log
-cat build_log.txt
-
-# Install
+# Install the binary
 make install
+
+# Verify installation
+muset --help
