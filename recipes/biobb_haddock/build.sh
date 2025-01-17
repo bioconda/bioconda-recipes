18af166e0c9d828199d0da5df87d0b21c475bcbd
gbayarri <genis.bayarri@irbbarcelona.org>
2025-01-17 15:44:19 +0100

    [biobb_haddock] update 5.0.0

--- recipes/biobb_haddock/build.sh
+++ recipes/biobb_haddock/build.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Python version:"
-python --version
-echo "Pip version:"
-pip --version
-echo "Pip index:"
-pip config list
-
 python3 -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-python3 -m pip install haddock3 --no-deps --no-cache-dir
+python3 -m pip install git+https://github.com/haddocking/haddock3.git --no-deps
