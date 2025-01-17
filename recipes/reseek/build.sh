0468dfc8c33cc08971429bf48ad175351b7c88ef
mencian <joshua.zhuang@yahoo.com>
2025-01-16 22:49:59 -0600

    clean up recipe

--- recipes/reseek/build.sh
+++ recipes/reseek/build.sh
@@ -2,12 +2,15 @@
 set -e
 
 mkdir -p ${PREFIX}/bin
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 cd src || exit 1
 echo "0" > gitver.txt
 
 cp ${RECIPE_DIR}/vcxproj_make.py .
 chmod +x vcxproj_make.py
-./vcxproj_make.py --openmp --cppcompiler ${CXX} --ccompiler ${CC}
+./vcxproj_make.py --openmp --lrt --cppcompiler "${CXX}" --ccompiler "${CC}"
 
 # Verify binary exists and is executable
 if [ ! -f ../bin/reseek ]; then
@@ -15,4 +18,4 @@ if [ ! -f ../bin/reseek ]; then
     exit 1
 fi
 
-cp ../bin/reseek ${PREFIX}/bin/reseek
+install -v -m 0755 ../bin/reseek ${PREFIX}/bin
