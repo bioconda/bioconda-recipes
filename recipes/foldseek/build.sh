34b5268a59f1f280ac2e15a78e28b62c8327d02b
Milot Mirdita <milot@mirdita.de>
2025-01-20 21:51:13 +0900

    Fix Mac Metal in Foldseek release 10 (#53330)

--- recipes/foldseek/build.sh
+++ recipes/foldseek/build.sh
@@ -15,6 +15,8 @@ esac
 if [ $(uname -s) = "Darwin" ] && [ $(uname -m) = "arm64" ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.15
     export MACOSX_SDK_VERSION=10.15
+    # make sure MetalKit.framework is not removed
+    export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
 fi
 
 if [ -z "${ARCH_BUILD}" ]; then
@@ -24,7 +26,7 @@ fi
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DHAVE_TESTS=0 -DHAVE_MPI=0 ${ARCH_BUILD} -DVERSION_OVERRIDE="${PKG_VERSION}" \
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DHAVE_TESTS=0 -DHAVE_MPI=0 ${ARCH_BUILD} -DVERSION_OVERRIDE="${PKG_VERSION}" \
     -DCMAKE_CUDA_ARCHITECTURES="75-real;80-real;86-real;89-real;90" -DENABLE_CUDA=${CUDA} ..
 make -j${CPU_COUNT} ${VERBOSE_CM}
 make install
