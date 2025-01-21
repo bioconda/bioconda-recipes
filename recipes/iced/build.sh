0982ab8d2901f3b12b595b9f5076a8f912ed7a3b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-01-21 16:31:42 -0600

    Update build.sh

--- recipes/iced/build.sh
+++ recipes/iced/build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 find . -type f -name *_.c -exec rm -f {} \;
-$PYTHON -m pip install . --ignore-installed --no-deps -vv
+$PYTHON -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
