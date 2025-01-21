fbcfabaf54adcead2380e696743d6f63159d2348
mencian <joshua.zhuang@yahoo.com>
2025-01-21 03:20:46 -0600

    Rebuild staden_io_lib recipe

--- recipes/staden_io_lib/build.sh
+++ recipes/staden_io_lib/build.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
 export M4="${BUILD_PREFIX}/bin/m4"
+export CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib -Wno-unused-command-line-argument"
+export CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ "$(uname)" == "Darwin" ]]; then
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
 
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+	export EXTRA_ARGS="--host=arm64"
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "aarch64" ]]; then
+	export EXTRA_ARGS="--host=aarch64"
+else
+	export EXTRA_ARGS="--host=x86_64"
+fi
+
 cp -rf ${RECIPE_DIR}/config.* .
 
 cd htscodecs
+autoupdate
 autoreconf -if
 
 cd ..
 
+autoupdate
 autoreconf -if
-./configure --prefix="${PREFIX}" --with-libdeflate="${PREFIX}" \
-	--with-libcurl="${PREFIX}" --with-zlib="${PREFIX}" \
-	--with-zstd="${PREFIX}" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS}" CC="${CC}" CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib" \
-	--disable-shared
+./configure --prefix="${PREFIX}" --with-libdeflate="${PREFIX}" --with-libcurl="${PREFIX}" \
+	--with-zlib="${PREFIX}" --with-zstd="${PREFIX}" --disable-warnings \
+	--disable-dependency-tracking --disable-option-checkin --enable-silent-rules \
+	--enable-shared=yes --enable-static=no LDFLAGS="${LDFLAGS}" CC="${CC}" CFLAGS="${CFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" "${EXTRA_ARGS}"
 make -j"${CPU_COUNT}"
 make install
 
-cp -f io_lib_config.h ${PREFIX}/include/io_lib/
+install -v -m 0755 io_lib_config.h "${PREFIX}/include/io_lib"
