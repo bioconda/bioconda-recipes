f7cfe9eda9903fbcea92974be44c601f8220d349
Camila Duitama González <camiladuitama@gmail.com>
2025-01-21 18:19:10 +0100

    Try verbose build to debug patches

--- recipes/muset/build.sh
+++ recipes/muset/build.sh
@@ -2,48 +2,22 @@
 set -e
 set -x
 
-# Extensive debugging for external libraries
-echo "Current working directory:"
-pwd
+# Print patch contents
+echo "Patch contents:"
+cat external_project_fmt.patch
 
-echo "Listing current directory contents:"
-ls -la
+# Attempt to apply patch manually
+patch -p1 < external_project_fmt.patch
 
-echo "Checking external directory:"
-if [ -d "external" ]; then
-    echo "External directory exists"
-    find external -type d
-else
-    echo "ERROR: external directory does not exist!"
-fi
-
-echo "Checking specific library directories:"
-for lib in fmt kff-cpp-api lz4 spdlog xxHash; do
-    if [ -d "external/${lib}" ]; then
-        echo "Contents of external/${lib}:"
-        ls -la "external/${lib}"
-    else
-        echo "Warning: external/${lib} directory not found"
-    fi
-done
-
-# Create build directory
 mkdir -p ${PREFIX}/bin
+
 mkdir build-conda
 cd build-conda
 
-# CMake with verbose output
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCONDA_BUILD=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_VERBOSE_MAKEFILE=ON
-
-# Build with maximum verbosity
-make VERBOSE=1 -j8
-
-# Install
+cmake .. -DCONDA_BUILD=ON
+make -j${CPU_COUNT}
 cd ..
+
 cp ./bin/kmat_tools ${PREFIX}/bin
 cp ./bin/muset ${PREFIX}/bin
 cp ./bin/muset_pa ${PREFIX}/bin
