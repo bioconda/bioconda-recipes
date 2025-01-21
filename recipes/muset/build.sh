bb9fe8e00106bda4e6f1327ccc515857d0c5f402
Camila Duitama González <camiladuitama@gmail.com>
2025-01-21 22:19:03 +0100

    correct build.sh that points to non existing patch

--- recipes/muset/build.sh
+++ recipes/muset/build.sh
@@ -2,21 +2,20 @@
 set -e
 set -x
 
-# Print patch contents
-echo "Patch contents:"
-cat external_project_fmt.patch
-
-# Attempt to apply patch manually
-patch -p1 < external_project_fmt.patch
-
 mkdir -p ${PREFIX}/bin
 
 mkdir build-conda
 cd build-conda
 
+echo "Current directory: ${PWD}"
+ls -lh
+
 cmake .. -DCONDA_BUILD=ON
-make -j${CPU_COUNT}
+make -j8
 cd ..
+
+echo "Current directory: ${PWD}"
+ls -lh
 
 cp ./bin/kmat_tools ${PREFIX}/bin
 cp ./bin/muset ${PREFIX}/bin
