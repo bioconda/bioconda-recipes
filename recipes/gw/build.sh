99bfb6178d7114b2944465b0641f17b5c0ca84ac
kcleal <clealk@cardiff.ac.uk>
2025-01-23 14:20:21 +0000

    Gw v1.1.2

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Get pre-compiled skia from jetbrains
-USE_GL=1 make prep 2> /dev/null
+OLD_SKIA=1 make prep 2> /dev/null
 
 if [[ "$OSTYPE" != "darwin"* ]]; then
   sed -i.bak 's/-lEGL -lGLESv2/-lEGL -lGLESv2 -lGL -lGLX/' Makefile
-  sed -i.bak 's/GLFW_EGL_CONTEXT_API/GLFW_NATIVE_CONTEXT_API/' src/plot_manager.cpp
+  # sed -i.bak 's/GLFW_EGL_CONTEXT_API/GLFW_NATIVE_CONTEXT_API/' src/plot_manager.cpp
   # Let conda set these
   sed -i.bak 's/-mmacosx-version-min=10.15//g' Makefile
   sed -i.bak 's/-mmacosx-version-min=11//g' Makefile
@@ -28,7 +28,7 @@ CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" \
 CPPFLAGS="${CPPFLAGS}" \
 LDFLAGS="${LDFLAGS}" \
 prefix="${PREFIX}" \
-make -j ${CPU_COUNT}
+OLD_SKIA=1 make -j ${CPU_COUNT}
 
 mkdir -p $PREFIX/bin
 cp gw $PREFIX/bin/gw
