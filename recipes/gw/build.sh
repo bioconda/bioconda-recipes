9aedd9044356384d15e5b267e3f2dc44e6f7752b
kcleal <clealk@cardiff.ac.uk>
2025-01-23 15:32:20 +0000

    Gw v1.1.2

--- recipes/gw/build.sh
+++ recipes/gw/build.sh
@@ -4,17 +4,6 @@ set -e
 # Get pre-compiled skia from jetbrains
 OLD_SKIA=1 make prep 2> /dev/null
 
-if [[ "$OSTYPE" != "darwin"* ]]; then
-  sed -i.bak 's/-lEGL -lGLESv2/-lEGL -lGLESv2 -lGL -lGLX/' Makefile
-  # sed -i.bak 's/GLFW_EGL_CONTEXT_API/GLFW_NATIVE_CONTEXT_API/' src/plot_manager.cpp
-  # Let conda set these
-  sed -i.bak 's/-mmacosx-version-min=10.15//g' Makefile
-  sed -i.bak 's/-mmacosx-version-min=11//g' Makefile
-
-  # path-link is not available for macOS
-  sed 's| -Wl,-rpath-link,\$(CONDA_PREFIX)\/lib||' yourfile.mk
-
-fi
 
 # Set flags conditionally based on the OS type
 if [[ "$OSTYPE" != "darwin"* ]]; then
@@ -22,6 +11,8 @@ if [[ "$OSTYPE" != "darwin"* ]]; then
   CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/${HOST}/sysroot/usr/include ${SYSROOT_FLAGS}"
   LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 ${SYSROOT_FLAGS}"
 else
+  sed 's| -Wl,-rpath-link,\$(CONDA_PREFIX)\/lib||' Makefile
+
   # No sysroot settings for macOS
   SYSROOT_FLAGS=""
   CPPFLAGS="${CPPFLAGS}"
