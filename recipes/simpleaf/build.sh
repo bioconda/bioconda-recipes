7be08b2da2725133cbdb0c5e2bea766e60d37be5
Rob Patro <rob-p@users.noreply.github.com>
2025-01-25 16:05:13 -0500

    Update build.sh

--- recipes/simpleaf/build.sh
+++ recipes/simpleaf/build.sh
@@ -9,8 +9,8 @@ if [ "$unamestr" == 'Darwin' ];
 then
   if [[ $(uname -m) == 'x86_64' ]]; then
     echo "OSX x86-64: attempting to fix broken (old) SDK behavior"
-    export CFLAGS="${CFLAGS} -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC"
-    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC"
+    export CFLAGS="${CFLAGS} -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC -UHAVE_ALIGNED_ALLOC -D__APPLE__"
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC -UHAVE_ALIGNED_ALLOC -D__APPLE__"
   fi
   export MACOSX_DEPLOYMENT_TARGET=10.15
   export MACOSX_SDK_VERSION=10.15
