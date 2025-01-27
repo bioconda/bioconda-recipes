6b7bbf1c31a09d4da271ed1f8f6dfbcc0c224d7f
mencian <joshua.zhuang@yahoo.com>
2025-01-27 04:52:58 -0600

    add patch

--- recipes/tgsgapcloser/build.sh
+++ recipes/tgsgapcloser/build.sh
@@ -5,16 +5,15 @@ mkdir -p "${PREFIX}/bin"
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export CFLAGS="$CFLAGS -O3 -L$PREFIX/lib"
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export CPPFLAGS="$CPPFLAGS -L$PREFIX/include"
 export CXXFLAGS="$CXXFLAGS -O3 -Wno-unused-command-line-argument"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 case $(uname -m) in
-	arm64|aarch64) EXTRA_ARGS="arm_neon=1 aarch64=1" ;;
+	arm64) ARCH_BUILD="arm_neon=1 aarch64=1" ;;
+	aarch64) ARCH_BUILD="aarch64=1" ;;
 esac
 
-make PREFIX="${PREFIX}" CC="${CC}" CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS}" LD_FLAGS="${LDFLAGS}" \
-	"${EXTRA_ARGS}"
+make PREFIX="${PREFIX}" CC="${CC}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" LD_FLAGS="${LDFLAGS}" "${ARCH_BUILD}"
 
 install -v -m 0755 tgsgapcloserbin/* "${PREFIX}/bin"
