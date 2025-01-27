5bce984100f27c988c9abd2a41ed0c55a6a739d4
mencian <joshua.zhuang@yahoo.com>
2025-01-27 07:47:08 -0600

    clean up recipe

--- recipes/megahit/build.sh
+++ recipes/megahit/build.sh
@@ -3,7 +3,8 @@
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-ignored-optimization-argument"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -fopenmp"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CPATH="${PREFIX}/include"
 export CPP_INCLUDE_PATH="${PREFIX}/include"
 
@@ -15,7 +16,9 @@ fi
 
 rm -rf build
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DCMAKE_C_COMPILER="${CC}" \
-	-DCMAKE_C_FLAGS="${CFLAGS}" "${CONFIG_ARGS}"
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_C_FLAGS="${CFLAGS}" \
+	-DSTATIC_BUILD=ON -DSANITIZER=ON -DTSAN=ON "${CONFIG_ARGS}"
 cmake --build build --target install -j "${CPU_COUNT}"
