f1e6b2f8a59a97646158c51ca12989944f38a835
Camila Duitama González <camiladuitama@gmail.com>
2025-01-27 17:49:49 +0100

    add cmake path to build.sh

--- recipes/muset/build.sh
+++ recipes/muset/build.sh
@@ -2,20 +2,23 @@
 set -e
 set -x
 
-mkdir -p ${PREFIX}/bin
-
-mkdir build-conda
+# Create a build directory
+mkdir -p build-conda
 cd build-conda
 
+# Configure the build
 cmake .. \
     -DCONDA_BUILD=ON \
     -Dexternal_dir=${SRC_DIR}/external \
     -Dexternal_bindir=${PWD}/external \
-    -Dcmake_path=${SRC_DIR}/cmake
+    -Dcmake_path=${SRC_DIR}/cmake \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
+# Build the project
 make -j${CPU_COUNT}
-cd ..
 
+# Manually copy the binaries to ${PREFIX}/bin
+mkdir -p ${PREFIX}/bin
 cp ./bin/kmat_tools ${PREFIX}/bin
 cp ./bin/muset ${PREFIX}/bin
 cp ./bin/muset_pa ${PREFIX}/bin
