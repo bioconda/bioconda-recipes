8b3e069c6abf20b96187c1b1d95213fb144b23a0
mencian <joshua.zhuang@yahoo.com>
2025-01-27 07:22:23 -0600

    megahit: add arm64 build

--- recipes/megahit/build.sh
+++ recipes/megahit/build.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-export CPATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPATH="${PREFIX}/include"
+export CPP_INCLUDE_PATH="${PREFIX}/include"
+
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
 rm -rf build
-mkdir -p build
-cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
-make -j $CPU_COUNT
-
-make install
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS}" ${CONFIG_ARGS}
+cmake --build build --target install -j "${CPU_COUNT}" -v
