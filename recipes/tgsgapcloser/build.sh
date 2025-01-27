31610979b9a25f23fa7d1adc44e341757f1c4729
mencian <joshua.zhuang@yahoo.com>
2025-01-27 05:58:47 -0600

    clean up recipe

--- recipes/tgsgapcloser/build.sh
+++ recipes/tgsgapcloser/build.sh
@@ -6,14 +6,14 @@ export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export CFLAGS="$CFLAGS -O3 -L$PREFIX/lib"
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-export CXXFLAGS="$CXXFLAGS -O3 -Wno-unused-command-line-argument"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export LD_FLAGS="$LD_FLAGS -L$PREFIX/lib"
 
 case $(uname -m) in
 	arm64) ARCH_BUILD="arm_neon=1 aarch64=1" ;;
 	aarch64) ARCH_BUILD="aarch64=1" ;;
 esac
 
-make PREFIX="${PREFIX}" CC="${CC}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" LD_FLAGS="${LDFLAGS}" "${ARCH_BUILD}"
+make CC="${CC}" CXX="${CXX}" "${ARCH_BUILD}"
 
 install -v -m 0755 tgsgapcloserbin/* "${PREFIX}/bin"
