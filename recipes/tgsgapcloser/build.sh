aec2501224f81119e9544ee108860a1e1c76a440
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-27 13:49:21 +0200

    tgsgapcloser: Add aarch64/arm64 builds
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/tgsgapcloser/build.sh
+++ recipes/tgsgapcloser/build.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -xe
+
 export C_INCLUDE_PATH="$PREFIX/include":$C_INCLUDE_PATH
 export INCLUDE_PATH="$PREFIX/include":$INCLUDE_PATH
 export CPLUS_INCLUDE_PATH="$PREFIX/include":$CPLUS_INCLUDE_PATH
@@ -6,5 +10,5 @@ export CFLAGS=" -L$PREFIX/lib $CFLAGS"
 export CPPFLAGS=" -L$PREFIX/lib $CPPFLAGS"
 export CXXFLAGS=" -L$PREFIX/lib $CXXFLAGS"
 export LD_FLAGS=" -L$PREFIX/lib $LD_FLAGS"
-make condainstall PREFIX=$PREFIX CC=$CC CXX=$CXX
+make -j"${CPU_COUNT}" condainstall PREFIX=$PREFIX CC=$CC CXX=$CXX
 
