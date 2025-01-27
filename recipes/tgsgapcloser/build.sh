34778e882441a0f533fa156d282165881fbff0b8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-01-27 07:31:35 -0600

    Update build.sh

--- recipes/tgsgapcloser/build.sh
+++ recipes/tgsgapcloser/build.sh
@@ -12,4 +12,5 @@ export LD_FLAGS="$LD_FLAGS -L$PREFIX/lib"
 make CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
 
 cp -rf tgsgapcloserbin "${PREFIX}/bin"
+chmod 0755 ${PREFIX}/bin/tgsgapcloserbin/*
 install -v -m 0755 tgsgapcloser "${PREFIX}/bin"
