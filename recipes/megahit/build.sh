0cab4f1a9cdd3d2d4ca5d66d1da0790b46601317
mencian <joshua.zhuang@yahoo.com>
2025-01-27 09:12:07 -0600

    clean up recipe

--- recipes/megahit/build.sh
+++ recipes/megahit/build.sh
@@ -2,14 +2,15 @@
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-ignored-optimization-argument"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -fPIC -fPIE"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -fopenmp"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-ignored-optimization-argument"
 export CPATH="${PREFIX}/include"
 export CPP_INCLUDE_PATH="${PREFIX}/include"
 
 if [[ `uname` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+	export LDFLAGS="${LDFLAGS} -Wl,-headerpad_max_install_names -Wl,-dead_strip_dylibs -Wl,-rpath,${PREFIX}/lib"
 else
 	export CONFIG_ARGS=""
 fi
@@ -20,5 +21,5 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_C_FLAGS="${CFLAGS}" \
-	-DSTATIC_BUILD=ON -DSANITIZER=ON -DTSAN=ON "${CONFIG_ARGS}"
+	"${CONFIG_ARGS}"
 cmake --build build --target install -j "${CPU_COUNT}"
