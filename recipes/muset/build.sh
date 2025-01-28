1eda51cacb9c6a0e4c154a7e3b9de3f2fbcccb5f
Camila Duitama González <camiladuitama@gmail.com>
2025-01-28 13:55:29 +0100

    Navigate muset as subdirectory in build.sh

--- recipes/muset/build.sh
+++ recipes/muset/build.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-set -e
-set -x
+set -e  # Exit on error
+set -x  # Print commands for debugging
+
+# Print the current directory for debugging
+echo "Current directory: ${PWD}"
+ls -lh
+
+# Navigate to the muset directory (if the source code is in a subdirectory)
+cd muset
+
+# Create the output directory
+mkdir -p ${PREFIX}/bin
 
 # Create a build directory
 mkdir -p build-conda
 cd build-conda
 
-# Configure the build
-cmake .. \
-    -DCONDA_BUILD=ON \
-    -Dexternal_dir=${SRC_DIR}/external \
-    -Dexternal_bindir=${PWD}/external \
-    -Dcmake_path=${SRC_DIR}/cmake \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+# Configure the build with CMake
+cmake .. -DCONDA_BUILD=ON
 
-# Build the project
+# Build the software
 make -j${CPU_COUNT}
 
-# Manually copy the binaries to ${PREFIX}/bin
-mkdir -p ${PREFIX}/bin
+# Move back to the source directory
+cd ../..
+
+# Copy the binaries to the output directory
 cp ./bin/kmat_tools ${PREFIX}/bin
+cp ./bin/muset-kmtricks ${PREFIX}/bin
 cp ./bin/muset ${PREFIX}/bin
 cp ./bin/muset_pa ${PREFIX}/bin
