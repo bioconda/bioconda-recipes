3d3449be74686eb8b2beb4775f364217c3433be8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-01-29 03:54:26 -0600

    Update build.sh

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -1,18 +1,17 @@
 ### Dumping preset flags because they break the build process on linux
 # Linker flags
 unset LDFLAGS
-export LDFLAGS="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 # Compiler flags
 unset CXXFLAGS
-export CXXFLAGS="-I${PREFIX}/include -I."
-
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include -I."
 
 ### Compiling mothur
 make clean
-make -j 4
+make -j"${CPU_COUNT}"
 make install
-cp -a uchime ${PREFIX}/bin
+cp -af uchime ${PREFIX}/bin
 
 # Linking BLAST binaries to default location for mothur
 mkdir -pv "${PREFIX}"/bin/blast/bin/
-ln -s "${PREFIX}"/bin/{blastall,formatdb,megablast} "${PREFIX}"/bin/blast/bin/
+ln -sf "${PREFIX}"/bin/{blastall,formatdb,megablast} "${PREFIX}"/bin/blast/bin/
