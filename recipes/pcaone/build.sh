cdcb954a3e332d6110c3be70fd2545e99f1b2fcf
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-01-29 04:24:20 -0600

    Update build.sh

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -1,22 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 mkdir -p ${PREFIX}/bin
 # fix zlib issue
-export CFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="${CFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
-if [ $(uname -s) == "Linux" ];then
-  make clean && make MKLROOT=${PREFIX} AVX=0 && mv PCAone ${PREFIX}/bin/PCAone.x64
-  make clean && make MKLROOT=${PREFIX} AVX=1 && mv PCAone ${PREFIX}/bin/PCAone.avx2
-  export LDFLAGS="-Wl,--no-as-needed -L$PREFIX/lib"
+if [[ $(uname -s) == "Linux" ]]; then
+  make clean && make MKLROOT="${PREFIX}" AVX=0 -j"${CPU_COUNT}" && mv PCAone "${PREFIX}/bin/PCAone.x64"
+  make clean && make MKLROOT="${PREFIX}" AVX=1 -j"${CPU_COUNT}" && mv PCAone "${PREFIX}/bin/PCAone.avx2"
+  export LDFLAGS="${LDFLAGS} -Wl,--no-as-needed -L${PREFIX}/lib"
   echo "#!/usr/bin/env bash
 PCAone=${PREFIX}/bin/PCAone.avx2
 grep avx2 /proc/cpuinfo |grep fma &>/dev/null
 [[ \$? != 0 ]] && PCAone=${PREFIX}/bin/PCAone.x64
-exec \$PCAone \$@" >${PREFIX}/bin/PCAone && chmod +x ${PREFIX}/bin/PCAone
+exec \$PCAone \$@" > ${PREFIX}/bin/PCAone && chmod 0755 ${PREFIX}/bin/PCAone
 
-elif [ $(uname -s) == "Darwin" ];then
-  make MKLROOT=${PREFIX}
-  mv PCAone ${PREFIX}/bin/
+elif [[ $(uname -s) == "Darwin" ]]; then
+  make MKLROOT="${PREFIX}" -j"${CPU_COUNT}"
+  install -v -m 0755 PCAone ${PREFIX}/bin
 fi
