3d363ae4e4031a91f92d73cc76e301cbd321dbe7
David Lecomber <david.lecomber@arm.com>
2025-01-30 19:49:50 +0000

    Workaround a broken autoconf setup with jellyfish. autoconf produces a syntax error in the AS_IF which I have otherwise been unable to resolve.

--- recipes/kmer-jellyfish/1.1.12/build.sh
+++ recipes/kmer-jellyfish/1.1.12/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+case "$(uname -s)" in
+    Darwin)
+	export MD5="md5 -r";;
+    Linux)
+	export MD5="md5sum";;
+    *)
+	echo "Not supported";;
+esac
+	
 autoreconf -fi
 export CXXFLAGS="${CXXFLAGS} -fsigned-char"
 export CFLAGS="${CFLAGS} -fsigned-char"
