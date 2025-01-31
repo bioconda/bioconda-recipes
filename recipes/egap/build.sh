dfefb365ad28998038f0b28976875d5e118ca92e
Ian M. B. <99409346+iPsychonaut@users.noreply.github.com>
2025-01-31 13:41:45 -0700

    Update build.sh added exports for CPPFLAGS and LDFLAGS
    We export CPPFLAGS and LDFLAGS to ensure we can find zlib.h and libz.so.
    In the make command for purge_dups, we explicitly pass CC="$CC" and LDFLAGS="$LDFLAGS" to avoid “cannot find -lz.”
    If purge_dups’s Makefile tries to do something like gcc calcuts.o -lz, it will see -L$PREFIX/lib from $LDFLAGS and properly locate libz.

--- recipes/egap/build.sh
+++ recipes/egap/build.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Build purge_dups from source
+# Ensure we pass the correct compiler and library flags to purge_dups
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+# 1) Build 'purge_dups'
 git clone https://github.com/dfguan/purge_dups.git
 cd purge_dups/src
-make CC="$CC"
-mkdir -p $PREFIX/bin
-cp ../bin/* $PREFIX/bin/
+make CC="$CC" \
+     CFLAGS="$CFLAGS $CPPFLAGS" \
+     LDFLAGS="$LDFLAGS"
+mkdir -p "$PREFIX/bin"
+cp ../bin/* "$PREFIX/bin/"
 cd "$SRC_DIR"
 
-# Build Ratatosk from source
+# 2) Build 'Ratatosk'
 git clone --recursive https://github.com/DecodeGenetics/Ratatosk.git
 cd Ratatosk
 mkdir -p build && cd build
@@ -18,7 +24,7 @@ make
 make install
 cd "$SRC_DIR"
 
-# Install or copy EGAP main script
+# 3) Install or copy EGAP main script
 chmod +x EGAP.py
-mkdir -p $PREFIX/bin
-cp EGAP.py $PREFIX/bin/EGAP
+mkdir -p "$PREFIX/bin"
+cp EGAP.py "$PREFIX/bin/EGAP"
