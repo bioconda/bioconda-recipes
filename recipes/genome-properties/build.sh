957370bf008d35860e9246221054b672ee937a0a
Christian Atallah <chrisata@ebi.ac.uk>
2025-02-03 16:12:28 +0000

    Change PERL5LIB

--- recipes/genome-properties/build.sh
+++ recipes/genome-properties/build.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
+## perl version
+perl_version=$(perl -e 'print $^V');
+perl_version=${perl_version:1}
+
+PERLLIB="${PREFIX}/lib/perl5/${perl_version}/perl_lib"
+mkdir -p $PERLLIB
+
 mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/code"
 mkdir -p "$PREFIX/data"
@@ -12,10 +19,10 @@ cp -r ./code/* "$PREFIX/code"
 cp -r ./data/* "$PREFIX/data"
 cp -r ./flatfiles/* "$PREFIX/flatfiles"
 
-cpanm -l "$PREFIX/code/modules" --force --installdeps DBIx::Class
-cpanm -l "$PREFIX/code/modules" --force --installdeps Data::Printer
+cpanm -l $PERLLIB --force --installdeps DBIx::Class
+cpanm -l $PERLLIB --force --installdeps Data::Printer
 
 # Make scripts executable
 chmod -R a+x $PREFIX/bin/*.pl
 
-export PERL5LIB="$PREFIX/code/modules"
+export PERL5LIB="$PERLLIB:$PREFIX/code/modules:$PERLLIB/lib/perl5""
