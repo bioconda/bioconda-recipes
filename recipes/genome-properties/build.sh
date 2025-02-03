b40b3c14a7b8f203a77fd84af7af924d95a149e7
Christian Atallah <chrisata@ebi.ac.uk>
2025-02-03 11:18:26 +0000

    more changes

--- recipes/genome-properties/build.sh
+++ recipes/genome-properties/build.sh
@@ -2,9 +2,18 @@
 
 set -euxo pipefail
 
-# cpan DBIx::Class
-# cpan Data::Printer
+cpanm DBIx::Class
+cpanm Data::Printer
+
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/data"
+mkdir -p "$PREFIX/flatfiles"
+
+cp -r ./code/scripts/* "$PREFIX/bin"
+cp -r ./data/* "$PREFIX/data"
+cp -r ./flatfiles/* "$PREFIX/flatfiles"
 
 # Make scripts executable
-chmod -R a+x ./code/scripts/*.pl
-export PERL5LIB="./code/modules"
+chmod -R a+x $PREFIX/bin/*.pl
+
+export PERL5LIB="$PREFIX/code/modules"
