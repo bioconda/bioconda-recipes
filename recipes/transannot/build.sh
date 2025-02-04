3b3db48d2195014fa887f8da5d88bf795475df0e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-04 04:50:30 -0600

    Update build.sh

--- recipes/transannot/build.sh
+++ recipes/transannot/build.sh
@@ -2,7 +2,7 @@
 
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -Wno-stringop-overflow"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-array-bounds"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export LC_ALL=en_US.UTF-8
 
@@ -25,17 +25,18 @@ fi
 
 if [[ `uname` == "Darwin" ]]; then
     export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+    export CXXFLAGS="${CXXFLAGS} -Wno-shift-overflow"
 else
     export CONFIG_ARGS=""
 fi
 
 mkdir build
 cd build
-
 cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
     -DHAVE_TESTS=0 -DHAVE_MPI=0 "${ARCH_BUILD}" -DVERSION_OVERRIDE="${PKG_VERSION}" \
     -DCMAKE_CUDA_ARCHITECTURES="75-real;80-real;86-real;89-real;90" -DENABLE_CUDA="${CUDA}" \
     -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DCMAKE_C_COMPILER="${CC}" \
     -DCMAKE_C_FLAGS="${CFLAGS}" "${CONFIG_ARGS}"
-make -j"${CPU_COUNT}" "${VERBOSE_CM}"
+
+make -j"${CPU_COUNT}"
 make install
