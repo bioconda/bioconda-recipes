fb32b23892eb6177a7b553bed0d4e9deca6b7520
mencian <joshua.zhuang@yahoo.com>
2025-02-04 00:59:36 -0600

    add patch

--- recipes/fastx_toolkit/build.sh
+++ recipes/fastx_toolkit/build.sh
@@ -4,25 +4,26 @@ set -xe
 
 cp -rf $BUILD_PREFIX/share/gnuconfig/config.* config/
 
+export M4="${BUILD_PREFIX}/bin/m4"
 export GTEXTUTILS_CFLAGS="-I ${PREFIX}/include/gtextutils"
 export GTEXTUTILS_LIBS="${PREFIX}/lib/libgtextutils.a"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 --disable-dependency-tracking --enable-silent-rules -I${PREFIX}/include -Wall -Wno-implicit-fallthrough -Wno-unused-but-set-variable"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -I${PREFIX}/include -Wall -Wno-implicit-fallthrough -Wno-unused-but-set-variable"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 sed -i.bak 's/#pragma pack(1)//g' src/libfastx/fastx.h
+sed -i.bak '88,90d;58d' src/fastx_artifacts_filter/fastx_artifacts_filter.c
+sed -i.bak 's/ -Werror//' configure.ac
 rm -rf src/libfastx/*.bak
+rm -rf src/fastx_artifacts_filter/*.bak
+rm -rf *.bak
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-    export MACOSX_DEPLOYMENT_TARGET="10.15"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-    sed -i.bak '' '88,90d;58d' src/fastx_artifacts_filter/fastx_artifacts_filter.c
-    sed -i.bak '' 's/-Werror//' configure.ac
-else
-    sed -i.bak '88,90d;58d' src/fastx_artifacts_filter/fastx_artifacts_filter.c
-    sed -i.bak 's/-Werror//' configure.ac
+	export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+	export MACOSX_DEPLOYMENT_TARGET="10.15"
+	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
 fi
 
-./configure --prefix="${PREFIX}"
+./reconf
+./configure --prefix="${PREFIX}" --disable-option-checking
 make -j"${CPU_COUNT}"
 make install
