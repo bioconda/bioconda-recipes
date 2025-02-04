5fbbe3b7ecef7a7a2168383b320b7e12dcee95e7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-03 23:19:43 -0600

    Update build.sh

--- recipes/fastx_toolkit/build.sh
+++ recipes/fastx_toolkit/build.sh
@@ -18,8 +18,6 @@ if [[ "$(uname)" == "Darwin" ]]; then
     export CXXFLAGS="${CXXFLAGS} -std=c++14 -stdlib=libc++"
 fi
 
-autoreconf -if
-
 ./configure --prefix="${PREFIX}"
 make -j"${CPU_COUNT}"
 make install
