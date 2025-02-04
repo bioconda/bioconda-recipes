0d3aeae3f593f992d88cec99cea2847c1107a5e8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-03 20:49:35 -0600

    Update build.sh

--- recipes/fastx_toolkit/build.sh
+++ recipes/fastx_toolkit/build.sh
@@ -2,19 +2,21 @@
 
 set -xe
 
-cp $BUILD_PREFIX/share/gnuconfig/config.* config/
+cp -rf $BUILD_PREFIX/share/gnuconfig/config.* config/
 
 export GTEXTUTILS_CFLAGS="-I $PREFIX/include/gtextutils"
 export GTEXTUTILS_LIBS="$PREFIX/lib/libgtextutils.a"
-export CXXFLAGS="${CXXFLAGS} -std=c++11 -Wall -Wno-implicit-fallthrough"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wall -Wno-implicit-fallthrough -Wno-unused-but-set-variable"
 sed -i.bak 's/#pragma pack(1)//g' src/libfastx/fastx.h
 
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
     export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-    MACOSX_DEPLOYMENT_TARGET=10.7
-    CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
+    MACOSX_DEPLOYMENT_TARGET=10.15
+    CXXFLAGS="${CXXFLAGS} -std=c++14 -stdlib=libc++"
 fi
 
-./configure --prefix=$PREFIX
+autoreconf -if
+
+./configure --prefix="$PREFIX" CC="$CC" CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 make -j"${CPU_COUNT}"
 make install
