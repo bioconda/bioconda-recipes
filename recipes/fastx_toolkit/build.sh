629ef47ca78714a2cb455cb3827a072953b21cf8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-03 23:19:14 -0600

    Update build.sh

--- recipes/fastx_toolkit/build.sh
+++ recipes/fastx_toolkit/build.sh
@@ -4,7 +4,7 @@ set -xe
 
 cp -rf $BUILD_PREFIX/share/gnuconfig/config.* config/
 
-export GTEXTUTILS_CFLAGS="-I${PREFIX}/include/gtextutils"
+export GTEXTUTILS_CFLAGS="-I ${PREFIX}/include/gtextutils"
 export GTEXTUTILS_LIBS="${PREFIX}/lib/libgtextutils.a"
 export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 --disable-dependency-tracking --enable-silent-rules -I${PREFIX}/include -Wall -Wno-implicit-fallthrough -Wno-unused-but-set-variable"
 export CFLAGS="${CFLAGS} -O3"
