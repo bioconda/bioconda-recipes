3585633e9302033d7dfa661360c6ede69a4f212d
David Lecomber <david.lecomber@arm.com>
2025-02-04 14:20:47 +0000

    Add std=c++14 to compile with capnproto on os/x

--- recipes/mash/build.sh
+++ recipes/mash/build.sh
@@ -7,7 +7,8 @@ export LIBPATH="-L${PREFIX}/lib"
 sed -i.bak "s/pthread/pthread -lrt/g" Makefile.in
 rm -rf *.bak
 
+
 autoreconf -i
-./configure  --prefix="${PREFIX}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" --with-capnp="${PREFIX}" --with-gsl="${PREFIX}"
+./configure  --prefix="${PREFIX}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -std=c++14" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" --with-capnp="${PREFIX}" --with-gsl="${PREFIX}"
 make -j"${CPU_COUNT}"
 make install
