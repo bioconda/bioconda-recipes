529a5a8704f0ce83e2a184a1414b2d10f84c358d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-04 01:25:08 -0600

    Update build.sh

--- recipes/ctyper/build.sh
+++ recipes/ctyper/build.sh
@@ -4,13 +4,17 @@ set -xe
 
 mkdir -p $PREFIX/bin
 
-export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=10.15 -I$PREFIX/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export CPATH="${PREFIX}/include"
 export EIGEN_ROOT="${PREFIX}"
 
+if [ "$(uname)" == "Darwin" ]; then
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=10.15"
+fi
+
 cd src
-make -j "${CPU_COUNT}" CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -fPIE" CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -fPIE"
+make -j"${CPU_COUNT}" CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -fPIE" CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -fPIE"
 cd ..
 
 install -v -m 0755 src/ctyper "${PREFIX}/bin"
