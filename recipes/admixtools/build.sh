2fb52f3e0318b08482605b255102f2d69326cb49
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-05 03:45:13 -0600

    Update build.sh

--- recipes/admixtools/build.sh
+++ recipes/admixtools/build.sh
@@ -4,6 +4,6 @@
 cd src
 make CC="${CC}" clobber -j"${CPU_COUNT}"
 
-make CC="${CC}" CFLAGS="$CFLAGS -O3 -fPIE -I${PREFIX}/include" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" all -j"${CPU_COUNT}"
+make CC="${CC}" CFLAGS="$CFLAGS -O3 -fPIC -I${PREFIX}/include" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" all -j"${CPU_COUNT}"
 
 make install TOP="${PREFIX}/bin"
