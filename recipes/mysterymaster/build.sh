1a287dd98dc2349a8a1e2b1c7a05de3619852702
sverre-branders <sverre.branders@protonmail.com>
2025-02-05 14:51:18 +0100

    added mysterymaster

---
+++ recipes/mysterymaster/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p "$PREFIX/bin"
+
+cp FindBarcodes $PREFIX/bin/
+
+mkdir -p "$PREFIX/lib"
+cp -r data/ $PREFIX/lib/
+cp mysterymaster.cfg $PREFIX/lib/
+cp MysteryMasterGUI.jar $PREFIX/lib/
+
+echo '#!/bin/bash' > ${PREFIX}/bin/mysterymaster
+echo 'java -jar "'$PREFIX'/lib/MysteryMasterGUI.jar" "$@"' >> $PREFIX/bin/mysterymaster
+chmod +x "${PREFIX}/bin/mysterymaster"
---
+++ recipes/mysterymaster/run_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+FindBarcodes -h >/dev/null
