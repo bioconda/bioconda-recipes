5c6d691884c22e6e72c9bec47d6b00144ed27135
Martin Grigorov <martin-g@users.noreply.github.com>
2025-02-07 09:38:36 +0200

    collect third party licences

--- recipes/adas/build.sh
+++ recipes/adas/build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -euo
+#!/bin/bash
 
-# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
-# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
+set -xeuo
+
+# collect the licenses of the dependencies
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX
+RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX --no-track
