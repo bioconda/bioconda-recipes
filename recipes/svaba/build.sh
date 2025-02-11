ccdd58457473a649685cc9324e02143edca24c1e
Michael Knudsen <micknudsen@gmail.com>
2025-02-11 14:44:32 +0100

    Added stuff from existing recipe back

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 
 cmake -DHTSLIB_DIR=htslib
-make
+make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 cp bin/svaba ${PREFIX}/bin/
