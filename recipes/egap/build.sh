3e741d2d6ea543c448d6d8116705ffe8c016fd1c
Ian M. B. <99409346+iPsychonaut@users.noreply.github.com>
2025-02-11 14:18:25 -0700

    Update build.sh v2.6
    removed runner from build.sh

--- recipes/egap/build.sh
+++ recipes/egap/build.sh
@@ -2,9 +2,6 @@
 set -eux
 ls -l ..
 
-# Install the runner package from the tarball that is one level up
-pip install ../runner-0.0.0.tar.gz
-
 # Build and install Ratatosk
 tar -xzf Ratatosk-0.9.0.tar.gz
 cd Ratatosk-0.9.0
