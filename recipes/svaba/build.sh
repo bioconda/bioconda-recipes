9eb8feb96816ca47e1d4148522314cdc1770e6d0
Michael Knudsen <micknudsen@gmail.com>
2025-02-11 13:40:34 +0100

    Added htslib

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sed -i.bak "s/-llzma -lbz2 -lz/-llzma -lbz2 -lz -pthread/g" src/svaba/Makefile.in
-./configure
-make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
-make install
+cmake -DHTSLIB_DIR=htslib
+make
 
 mkdir -p ${PREFIX}/bin
 cp bin/svaba ${PREFIX}/bin/
