d7394ce0d4b83707ad28167121d0fc1f5e717a01
sverre-branders <154252137+sverre-branders@users.noreply.github.com>
2025-02-11 10:35:14 +0100

    Update build.sh to add help flag

--- recipes/mysterymaster/build.sh
+++ recipes/mysterymaster/build.sh
@@ -9,5 +9,6 @@ cp mysterymaster.cfg $PREFIX/lib/
 cp MysteryMasterGUI.jar $PREFIX/lib/
 
 echo '#!/bin/bash' > ${PREFIX}/bin/mysterymaster
+echo 'for arg in "$@"; do [[ "$arg" == "-h" || "$arg" == "--help" ]] && echo "Run the mysterymaster GUI" && exit 0; done' >> $PREFIX/bin/mysterymaster
 echo 'java -jar "'$PREFIX'/lib/MysteryMasterGUI.jar" "$@"' >> $PREFIX/bin/mysterymaster
 chmod +x "${PREFIX}/bin/mysterymaster"
