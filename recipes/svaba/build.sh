28a0123a0334f9285c01fccc539a48bd7a23e2d5
Michael Knudsen <micknudsen@gmail.com>
2025-02-11 14:42:27 +0100

    Cleaned up

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-echo "#######################"
-echo "### DEBUGGING START ###"
-echo "#######################"
-
-pwd -P
-
-echo ""
-
-find
-
-echo ""
-
-which gcc
-
-echo "#####################"
-echo "### DEBUGGING END ###"
-echo "#####################"
-
-
 cmake -DHTSLIB_DIR=htslib
 make
 
