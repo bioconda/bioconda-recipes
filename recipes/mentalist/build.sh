a034a890ee9c24efd271e337d1fc810f642e638f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-11 19:29:48 -0600

    Update build.sh

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -4,11 +4,12 @@
 # Julia packages are retrieved externally and compile things with gcc without using CC etc. env variables...
 ln -sf "${GCC}" "${BUILD_PREFIX}/gcc"
 
-cp -r $SRC_DIR/src/*.jl $PREFIX/bin
-cp -r $SRC_DIR/scripts $PREFIX
+cp -rf $SRC_DIR/src/*.jl $PREFIX/bin
+cp -rf $SRC_DIR/scripts $PREFIX
 ln -sf $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod 0755 $PREFIX/bin/mentalist
 
+julia -e 'using Pkg; Pkg.add("Distributed")'
 julia -e 'using Pkg; Pkg.add("ArgParse")'
 julia -e 'using Pkg; Pkg.add("BioSequences")'
 julia -e 'using Pkg; Pkg.add("JSON")'
@@ -20,11 +21,10 @@ julia -e 'using Pkg; Pkg.add("FileIO")'
 julia -e 'using Pkg; Pkg.add("TextWrap")'
 julia -e 'using Pkg; Pkg.add("LightXML")'
 #julia -e 'using Pkg; Pkg.add("JuMP")'
-julia -e 'using Pkg; Pkg.add("Gurobi")'
-julia -e 'using Pkg; Pkg.add("Distributed")'
+# julia -e 'using Pkg; Pkg.add("Gurobi")'
 
-rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
+rm -rf "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
-rm -f "$PREFIX"/share/julia/site/v*/META_BRANCH
+rm -rf "$PREFIX"/share/julia/site/v*/META_BRANCH
 
 rm -rf "${BUILD_PREFIX}/gcc"
