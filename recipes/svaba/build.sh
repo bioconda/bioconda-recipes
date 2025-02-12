4c6c7c2441eb890138504044d9eb1dbd8590ece4
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 13:07:36 +0100

    Debugging

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eu -o pipefail
 
+echo "###################"
+echo "### DEBUG START ###"
+echo "###################"
+
+find . | grep configure
+
+echo "#################"
+echo "### DEBUG END ###"
+echo "#################"
+
 ./configure
 cmake
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
