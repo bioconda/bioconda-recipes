bc7b842162d2c3f93f94dad187d2894a4c7fcf33
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 09:53:00 +0100

    Trying with absolute path

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
-echo "### DEBUG START ###"
-
-find .
-
-echo "### DEBUG CMAKE ###"
-
-cmake -DHTSLIB_DIR=htslib
-
-echo "### DEBUG MAKE ###"
-
+cmake -DHTSLIB_DIR=$(pwd -P)/htslib
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
-
-echo "### DEBUG DONE ###"
 
 mkdir -p ${PREFIX}/bin
 cp bin/svaba ${PREFIX}/bin/
