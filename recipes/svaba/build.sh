2177d9f3f989cecfa5f63810d41245446a78ee7b
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 08:14:57 +0100

    Debugging

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -eu -o pipefail
 
+echo "### DEBUG START ###"
+
+find .
+
+echo "### DEBUG CMAKE ###"
+
 cmake -DHTSLIB_DIR=htslib
+
+echo "### DEBUG MAKE ###"
+
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
+
+echo "### DEBUG DONE ###"
 
 mkdir -p ${PREFIX}/bin
 cp bin/svaba ${PREFIX}/bin/
