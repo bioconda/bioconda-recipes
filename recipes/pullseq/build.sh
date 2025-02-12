3683ee46aeb52af1c9904062b774d798c0bcb300
David Lecomber <david.lecomber@arm.com>
2025-02-12 14:34:06 +0000

    do nothing unusual here

--- recipes/pullseq/build.sh
+++ recipes/pullseq/build.sh
@@ -4,9 +4,6 @@ export CPATH=${PREFIX}/include
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -fcommon"
-if [ "$(uname)" = "Darwin" ]; then
-    export CFLAGS="${CFLAGS} -fcommon -std=c89"
-fi
 ./bootstrap
 ./configure --prefix=${PREFIX}
 make
