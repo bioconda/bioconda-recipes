cd73006a7d37639868358fd646a60dcae69e67ed
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 12:36:32 +0100

    Try running configure

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
+./configure
 cmake
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
