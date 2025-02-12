e888b830bdcf8fda2c057454ee9c3a2f86f33d16
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 10:08:29 +0100

    Try with htslib as build requirement instead

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-cmake -DHTSLIB_DIR=$(pwd -P)/htslib
+cmake
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
