9fdae22499291a3896f8bf0c4bce567b2740ad82
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-11 20:02:54 -0600

    Update build.sh

--- recipes/relion/build.sh
+++ recipes/relion/build.sh
@@ -7,11 +7,11 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 if [[ `uname` == "Darwin" ]]; then
   export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER -DCUDA=OFF"
 else
-  export CONFIG_ARGS=""
+  export CONFIG_ARGS="-DCUDA_TOOLKIT_ROOT_DIR=${PREFIX} -DCUDA_NVCC_EXECUTABLE=${PREFIX}/bin/nvcc -DCUDA_INCLUDE_DIRS=${PREFIX}/include"
 fi
 
-cmake -S . -B build "${CMAKE_ARGS}" \
-  -DCMAKE_BUILD_TYPE=Release -DGUI=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+  -DGUI=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-  -DCUDA_TOOLKIT_ROOT_DIR="${PREFIX}" "${CONFIG_ARGS}"
+  "${CONFIG_ARGS}"
 cmake --build build --target install -j "${CPU_COUNT}" -v
