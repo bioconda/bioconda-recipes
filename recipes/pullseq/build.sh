bef676296a51a571831cd4f46e304f4e8a789dd4
David Lecomber <david.lecomber@arm.com>
2025-02-12 11:24:57 +0000

    only add c89 to OS/X

--- recipes/pullseq/build.sh
+++ recipes/pullseq/build.sh
@@ -3,8 +3,10 @@
 export CPATH=${PREFIX}/include
 export LD_LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="-L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -fcommon -std=c89"
-
+export CFLAGS="${CFLAGS} -fcommon"
+if [ "$(uname)" = "Darwin" ]; then
+    export CFLAGS="${CFLAGS} -fcommon -std=c89"
+fi
 ./bootstrap
 ./configure --prefix=${PREFIX}
 make
