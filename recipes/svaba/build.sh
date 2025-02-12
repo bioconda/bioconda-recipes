13dcfefd851e6e84910837784b1a5192cd3a141f
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 16:27:13 +0100

    Where is htslib located?

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eu -o pipefail
 
+echo "#######################"
+echo "### DEBUGGING START ###"
+echo "#######################"
+
+find . | grep htslib
+
+echo "#####################"
+echo "### DEBUGGING END ###"
+echo "#####################"
+
 mkdir build
 cd build
 cmake ..
