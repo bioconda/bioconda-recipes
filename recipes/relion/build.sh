50e1e0d3d54a271a15cea2482d9353b83389d7ed
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-11 22:56:45 -0600

    Update build.sh

--- recipes/relion/build.sh
+++ recipes/relion/build.sh
@@ -2,25 +2,31 @@
 set -xe
 
 mkdir -p "${PREFIX}/share/torch"
+OS=$(uname)
+ARCH=$(uname -m)
 
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-deprecated-declarations"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-if [[ `uname` == "Darwin" ]]; then
+if [[ "${OS}" == "Darwin" ]]; then
 	export LDFLAGS="${LDFLAGS} -Wl,-headerpad_max_install_names -Wl,-rpath,${PREFIX}/lib"
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 	export ADDITIONAL_ARGS="-DCUDA=OFF"
-else
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "aarch64" ]]; then
 	export CONFIG_ARGS="-DCUDAToolkit_ROOT=${PREFIX}"
 	export ADDITIONAL_ARGS=""
- 	cp -rf "${PREFIX}/targets/*-linux/include/*" "${PREFIX}/include"
+ 	cp -rf ${PREFIX}/targets/sbsa-linux/include/* "${PREFIX}/include"
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "x86_64" ]]; then
+	export CONFIG_ARGS="-DCUDAToolkit_ROOT=${PREFIX}"
+ 	export ADDITIONAL_ARGS=""
+  	cp -rf ${PREFIX}/targets/x86_64-linux/include/* "${PREFIX}/include"
 fi
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
 	-DGUI=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-DPYTHON_EXE_PATH="${PREFIX}/bin/python" -DTORCH_HOME_PATH="${PREFIX}/share/torch" \
+	-DPYTHON_EXE_PATH="${PYTHON}" -DTORCH_HOME_PATH="${PREFIX}/share/torch" \
  	"${CONFIG_ARGS}" \
 	"${ADDITIONAL_ARGS}"
 cmake --build build --target install -j "${CPU_COUNT}" -v
