1380551b8fb99dd0de8d1a7235f861900ec553ae
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-11 19:20:51 -0600

    Update build.sh

--- recipes/relion/build.sh
+++ recipes/relion/build.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -xe
 
-mkdir build
-cd build
-cmake .. -DGUI=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX
-make -j"${CPU_COUNT}"
-make install
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+if [[ `uname` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build "${CMAKE_ARGS}" \
+  -DCMAKE_BUILD_TYPE=Release -DGUI=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  "${CONFIG_ARGS}"
+cmake --build build --target install -j "${CPU_COUNT}" -v
