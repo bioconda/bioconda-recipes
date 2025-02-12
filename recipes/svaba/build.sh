5eb874452ef1fc8e2dcef77ce90013461a45cd68
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 13:55:36 +0100

    Trying something else

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -11,6 +11,12 @@ echo "#################"
 echo "### DEBUG END ###"
 echo "#################"
 
+cd svaba
+mkdir build
+cd build
+cmake ..
+make
+
 cmake
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
