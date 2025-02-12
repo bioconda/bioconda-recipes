4f4220aa722a0fa933d4843cb076ce87a42d293b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-11 20:23:01 -0600

    Update build.sh

--- recipes/fibertools-rs/build.sh
+++ recipes/fibertools-rs/build.sh
@@ -3,8 +3,12 @@
 # Set the desitnation for the libtorch files
 #
 
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
-export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 #
 # install package
@@ -14,11 +18,13 @@ pushd ${PREFIX}
 
 # remove cpu specifc cargo flags
 pushd ${HOME}
-sed 's/native/generic/g' .cargo/config.toml >tmp
+sed -i.bak 's/native/generic/g' .cargo/config.toml >tmp
 mv tmp .cargo/config.toml
 popd
 
-cargo install --no-track --verbose \
+RUST_BACKTRACE=full
+RUSTFLAGS="-C linker=${CC}"
+cargo install -v --locked --no-track --verbose \
     --root "${PREFIX}" --path "${HOME}"
 popd
 
