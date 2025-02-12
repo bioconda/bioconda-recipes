cff1c294734c4e61b1e55a12840725841081a9b5
Michael Knudsen <micknudsen@gmail.com>
2025-02-12 14:20:37 +0100

    New attempt

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-echo "###################"
-echo "### DEBUG START ###"
-echo "###################"
-
-find .
-
-echo "#################"
-echo "### DEBUG END ###"
-echo "#################"
-
-cd svaba
 mkdir build
 cd build
 cmake ..
 make
-
-cmake
-make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 cp bin/svaba ${PREFIX}/bin/
