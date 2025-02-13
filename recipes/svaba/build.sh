cdf5d61e58027c4eef60fdc137f6d73392dc3a31
Michael Knudsen <micknudsen@gmail.com>
2025-02-13 10:48:46 +0100

    Included stuff from original recipe

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -3,8 +3,8 @@ set -eu -o pipefail
 
 mkdir build
 cd build
-cmake -DCMAKE_CC_COMPILER=$CC -DHTSLIB_DIR=../htslib ..
-make
+cmake -DHTSLIB_DIR=../htslib ..
+make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
 cp bin/svaba ${PREFIX}/bin/
