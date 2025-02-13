67f3cc031f5602aecc034777fa81e615e4031e5f
Michael Knudsen <micknudsen@gmail.com>
2025-02-13 10:32:10 +0100

    Specify compiler in cmake

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 
-echo "#######################"
-echo "### DEBUGGING START ###"
-echo "#######################"
-
-which gcc
-
-echo "#####################"
-echo "### DEBUGGING END ###"
-echo "#####################"
-
 mkdir build
 cd build
-cmake -DHTSLIB_DIR=../htslib ..
+cmake -DCMAKE_CC_COMPILER=$CC -DHTSLIB_DIR=../htslib ..
 make
 
 mkdir -p ${PREFIX}/bin
