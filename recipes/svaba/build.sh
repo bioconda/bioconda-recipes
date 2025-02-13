0aeb10bcd0961da1ee96ee36518fa2896c70b311
Michael Knudsen <micknudsen@gmail.com>
2025-02-13 11:04:56 +0100

    Maybe this will work

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-mkdir build
-cd build
-cmake -DHTSLIB_DIR=../htslib ..
+cmake -DHTSLIB_DIR=htslib
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
