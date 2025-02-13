01602f714dcb1c8e0dd449cd4a26d2a418c04dad
Michael Knudsen <micknudsen@gmail.com>
2025-02-13 12:06:04 +0100

    Debugging. Where is the hts.h file located?

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eu -o pipefail
 
+echo "#######################"
+echo "### DEBUGGING START ###"
+echo "#######################"
+
+find . -name hts.h
+
+echo "#####################"
+echo "### DEBUGGING END ###"
+echo "#####################"
+
 cmake -DHTSLIB_DIR=htslib
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
