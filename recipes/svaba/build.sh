0587c5368d6079cdd431371aed9a5bab6169b203
Michael Knudsen <micknudsen@gmail.com>
2025-02-14 10:30:17 +0100

    Found compiled binary

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -11,15 +11,5 @@ cd ..
 cmake -DHTSLIB_DIR=htslib
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
-echo "#######################"
-echo "### DEBUGGING START ###"
-echo "#######################"
-
-find . -name svaba
-
-echo "#####################"
-echo "### DEBUGGING END ###"
-echo "#####################"
-
 mkdir -p ${PREFIX}/bin
-cp bin/svaba ${PREFIX}/bin/
+cp svaba ${PREFIX}/bin/
