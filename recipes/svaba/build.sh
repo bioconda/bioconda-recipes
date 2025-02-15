e7b96945ccf6240927a890d57d3d65f6a1efdda7
Michael Knudsen <micknudsen@gmail.com>
2025-02-15 06:49:23 +0100

    Debugging: Where is htslib?

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# Build htslib which is needed by SeqLib (a submodule of SvABA)
-./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
-make install
+echo "### DEBUGGING ###"
 
-# Make sure that SeqLib can find htslib
-cd SeqLib
-ln -s ../htslib .
-cd ..
+find .
 
-# Build SvABA
-cmake -DHTSLIB_DIR=htslib
-make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
+# # Build htslib which is needed by SeqLib (a submodule of SvABA)
+# ./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
+# make install
 
-mkdir -p ${PREFIX}/bin
-cp svaba ${PREFIX}/bin/
+# # Make sure that SeqLib can find htslib
+# cd SeqLib
+# ln -s ../htslib .
+# cd ..
+
+# # Build SvABA
+# cmake -DHTSLIB_DIR=htslib
+# make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
+
+# mkdir -p ${PREFIX}/bin
+# cp svaba ${PREFIX}/bin/
