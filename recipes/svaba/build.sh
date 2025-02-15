fc5356bb3368ff6f5952d4f771a35b2398926945
Michael Knudsen <micknudsen@gmail.com>
2025-02-15 07:33:34 +0100

    Trying to keep it simple

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 
-echo "### DEBUGGING ###"
+# Build SvABA
+cmake -DHTSLIB_DIR=${PREFIX}/htslib
+make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
-find .
-
-# # Build htslib which is needed by SeqLib (a submodule of SvABA)
-# ./configure --prefix=${PREFIX} --enable-libcurl --with-libdeflate --enable-plugins --enable-gcs --enable-s3
-# make install
-
-# # Make sure that SeqLib can find htslib
-# cd SeqLib
-# ln -s ../htslib .
-# cd ..
-
-# # Build SvABA
-# cmake -DHTSLIB_DIR=htslib
-# make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
-
-# mkdir -p ${PREFIX}/bin
-# cp svaba ${PREFIX}/bin/
+mkdir -p ${PREFIX}/bin
+cp svaba ${PREFIX}/bin/
