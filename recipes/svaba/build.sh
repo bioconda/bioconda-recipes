4e5045c8d4562262fba678595ace615adf67d2bd
Michael Knudsen <micknudsen@gmail.com>
2025-02-15 08:04:19 +0100

    Cleaned up

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# Build SvABA
 cmake -DHTSLIB_DIR=${PREFIX}/htslib
 make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
