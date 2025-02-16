d84cba79c92056da0e7a359f9f6dd959f5b3ecf8
ivartb <ivart07@mail.ru>
2025-02-17 01:37:04 +0300

    Add metafx tool

---
+++ recipes/metafx/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# copying over all necessary files
+mkdir -p $PREFIX/bin/
+chmod a+x bin/metafx
+cp bin/metafx $PREFIX/bin/
+cp -r bin/metafx-modules $PREFIX/bin/
+cp -r bin/metafx-scripts $PREFIX/bin/
+
+ln -s $PREFIX/bin/metafx-modules/* $PREFIX/bin/
+ln -s $PREFIX/bin/metafx-scripts/* $PREFIX/bin/
