1ec0289e0a0d6642a5f50e51395ef16ba3c3baf9
mencian <joshua.zhuang@yahoo.com>
2025-02-17 09:24:43 -0600

    clean up recipe

--- recipes/seqlib/build.sh
+++ recipes/seqlib/build.sh
@@ -8,7 +8,7 @@ else
 	export CONFIG_ARGS=""
 fi
 
-export INCLUDES="-I{PREFIX}/include"
+export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -O3"
@@ -18,6 +18,7 @@ cmake -S . -B build \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_SHARED_LIBS=ON \
 	-DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
+	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -Wno-return-type" \
 	-DHTSLIB_DIR="${PREFIX}/include/htslib" \
