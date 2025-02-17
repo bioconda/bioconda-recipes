9f8febe7adcb6351c2daee541f3398acd1dd0bf4
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-17 09:44:38 +0200

    svaba: add aarch64/arm64 builds
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/svaba/build.sh
+++ recipes/svaba/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -eu -o pipefail
+set -xeu -o pipefail
 
-cmake -DHTSLIB_DIR=${PREFIX}/htslib
-make CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
+mkdir build && cd build
+cmake -DHTSLIB_DIR=${PREFIX}/htslib ..
+make -j"${CPU_COUNT}" CC=${CC} CXX=${CXX} CFLAGS="-fcommon ${CFLAGS} -L${PREFIX}/lib" CXXFLAGS="-fcommon ${CXXFLAGS} -UNDEBUG -L${PREFIX}/lib" LDFLAGS="${LDFLAGS}"
 
 mkdir -p ${PREFIX}/bin
-cp svaba ${PREFIX}/bin/
+install -v -m 0755 svaba ${PREFIX}/bin/
