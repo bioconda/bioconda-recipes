8ed092544749a83af05ffb8faa3d2f9719f4c81d
ivartb <ivart07@mail.ru>
2025-02-17 14:21:05 +0300

    Update dependencies and build

--- recipes/metafx/build.sh
+++ recipes/metafx/build.sh
@@ -7,5 +7,5 @@ cp bin/metafx $PREFIX/bin/
 cp -r bin/metafx-modules $PREFIX/bin/
 cp -r bin/metafx-scripts $PREFIX/bin/
 
-ln -s $PREFIX/bin/metafx-modules/* $PREFIX/bin/
-ln -s $PREFIX/bin/metafx-scripts/* $PREFIX/bin/
+cp $PREFIX/bin/metafx-modules/* $PREFIX/bin/
+cp $PREFIX/bin/metafx-scripts/* $PREFIX/bin/
