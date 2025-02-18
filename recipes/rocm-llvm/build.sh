562aa502ada40decf150ef0c69703978d092d3e4
j34ni <jeani@uio.no>
2025-02-18 09:44:14 +0000

    Build shared libs

--- recipes/rocm-llvm/build.sh
+++ recipes/rocm-llvm/build.sh
@@ -9,11 +9,13 @@ cd build
 cmake -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DBUILD_SHARED_LIBS=ON \
+      -DLLVM_BUILD_LLVM_DYLIB=ON \
       -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt" \
       -DLLVM_TARGETS_TO_BUILD="AMDGPU;X86" \
       -DLLVM_ENABLE_ASSERTIONS=OFF \
       ../llvm
 
-ninja
+ninja -C build -j"${CPU_COUNT}"
 
 ninja install
