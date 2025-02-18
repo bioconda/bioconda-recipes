bc8cddfc8400276842566cda97eed90f3306c39d
j34ni <jeani@uio.no>
2025-02-18 13:18:31 +0000

    As before

--- recipes/rocm-llvm/build.sh
+++ recipes/rocm-llvm/build.sh
@@ -9,8 +9,6 @@ cd build
 cmake -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DBUILD_SHARED_LIBS=ON \
-      -DLLVM_BUILD_LLVM_DYLIB=ON \
       -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt" \
       -DLLVM_TARGETS_TO_BUILD="AMDGPU;X86" \
       -DLLVM_ENABLE_ASSERTIONS=OFF \
