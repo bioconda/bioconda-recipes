88aa9115bc260aebe6e09f1ff735b0f568aeb2ce
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-19 14:08:43 +0200

    glimpse-bio: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 export COMMIT_VERS="${PKG_VERSION}"
 export COMMIT_DATE="$(date -Idate -u)"
 
@@ -9,14 +11,14 @@ do
     pushd $subdir
     # Build the binaries
     make \
-        -j 4 \
+        -j"${CPU_COUNT}" \
         DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm" \
         CXX="$CXX -std=c++17" \
         CXXFLAG="$CXXFLAGS ${PREFIX} -D__COMMIT_ID__='\"${COMMIT_VERS}\"' -D__COMMIT_DATE__='\"${COMMIT_DATE}\"' -Wno-ignored-attributes -O3 -mavx2 -mfma" \
         LDFLAG="$LDFLAGS" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
-        BOOST_INC="/usr/include"\
+        BOOST_INC="${PREFIX}/include"\
         BOOST_LIB_IO="-lboost_iostreams" \
         BOOST_LIB_PO="-lboost_program_options" \
         BOOST_LIB_SE="-lboost_serialization" \
