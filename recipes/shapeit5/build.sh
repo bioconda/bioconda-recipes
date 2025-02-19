74343642a528ba345290c79ee6eeca402a0205c6
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-19 14:02:53 +0200

    shapeit5: add linux-aarch64 build
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/shapeit5/build.sh
+++ recipes/shapeit5/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -xe
+
 export COMMIT_VERS="${PKG_VERSION}"
 export COMMIT_DATE="$(date -Idate -u)"
 
-git clone https://github.com/odelaneau/xcftools.git
+# git clone https://github.com/odelaneau/xcftools.git
 
-ls
+# ls
 
 for subdir in phase_common phase_rare switch ligate
 
@@ -14,14 +16,14 @@ do
 
     # Build the binaries
     make \
-        -j 4 \
+        -j"${CPU_COUNT}" \
         DYN_LIBS="-lz -lpthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm -lcrypto" \
         CXX="$CXX -std=c++17" \
         CXXFLAG="$CXXFLAGS ${PREFIX} -D__COMMIT_ID__='\"${COMMIT_VERS}\"' -D__COMMIT_DATE__='\"${COMMIT_DATE}\"' -Wno-ignored-attributes -O3 -mavx2 -mfma" \
         LDFLAG="$LDFLAGS" \
         HTSLIB_INC="$PREFIX" \
         HTSLIB_LIB="-lhts" \
-        BOOST_INC="/usr/include"\
+        BOOST_INC="${PREFIX}/include"\
         BOOST_LIB_IO="-lboost_iostreams" \
         BOOST_LIB_PO="-lboost_program_options" \
         BOOST_LIB_SE="-lboost_serialization" \
