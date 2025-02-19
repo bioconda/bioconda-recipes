c06df165be506110042a857bdbf6cc214a9cef37
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-18 18:52:42 -0600

    Update build.sh

--- recipes/genmap/build.sh
+++ recipes/genmap/build.sh
@@ -4,7 +4,7 @@ rm -rf include/seqan
 mv ${SRC_DIR}/seqan include/
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 --std=c++14 -I${PREFIX}/include"
 
 if [[ "$(uname)" == "Darwin" ]]; then
 	export MACOSX_DEPLOYMENT_TARGET=10.15
@@ -15,6 +15,6 @@ fi
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release -DGENMAP_NATIVE_BUILD=OFF \
-	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS} -Wno-deprecated-declarations -Wno-alloc-size-larger-than" \
 	"${CONFIG_ARGS}"
 cmake --build build --target install -j "${CPU_COUNT}"
