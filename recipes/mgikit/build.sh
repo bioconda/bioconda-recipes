cbccf17891648d618a6c82e5b2a130f404fef1f6
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-19 22:27:21 -0600

    Update build.sh

--- recipes/mgikit/build.sh
+++ recipes/mgikit/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+
 RUST_BACKTRACE=1
 cargo install --no-track --verbose --root "${PREFIX}" --path .
