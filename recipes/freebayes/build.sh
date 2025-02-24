1c5266de63a5d707eff1869a6f4cdb8296d2a7bd
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-02-24 13:58:52 -0600

    Update freebayes v1.3.9 (#54051)
    * Update freebayes v1.3.9
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * edit macos patch
    
    * Update meta.yaml
    
    * Update build.sh
    
    * add version patch
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * add ldflags
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * edit macos patch
    
    * edit macos patch

--- recipes/freebayes/build.sh
+++ recipes/freebayes/build.sh
@@ -5,12 +5,38 @@ mkdir -p ${PREFIX}/bin
 cd freebayes
 mkdir build
 
-export INCLUDES="-I{PREFIX}/include"
-export LIBPATH="-L${PREFIX}/lib"
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I{PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-deprecated-declarations"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" CC="${CC}" meson setup --buildtype release \
+wget -O src/multipermute.h https://raw.githubusercontent.com/ekg/multipermute/refs/heads/master/multipermute.h
+
+sed -i.bak -e 's|"split.h"|<vcflib/split.h>|' src/*.h
+sed -i.bak -e 's|"split.h"|<vcflib/split.h>|' src/*.cpp
+sed -i.bak -e 's|"convert.h"|<vcflib/convert.h>|' src/*.h
+sed -i.bak -e 's|"convert.h"|<vcflib/convert.h>|' src/*.cpp
+sed -i.bak -e 's|"join.h"|<vcflib/join.h>|' src/*.h
+sed -i.bak -e 's|"join.h"|<vcflib/join.h>|' src/*.cpp
+sed -i.bak -e 's|"multichoose.h"|<vcflib/multichoose.h>|' src/*.h
+sed -i.bak -e 's|"multichoose.h"|<vcflib/multichoose.h>|' src/*.cpp
+sed -i.bak -e 's|<Variant.h>|<vcflib/Variant.h>|' src/*.h
+sed -i.bak -e 's|<intervaltree/IntervalTree.h>|<vcflib/IntervalTree.h>|' src/BedReader.h
+sed -i.bak -e 's|<IntervalTree.h>|<vcflib/IntervalTree.h>|' src/BedReader.cpp
+
+rm -rf src/*.bak
+
+OS=$(uname)
+ARCH=$(uname -m)
+
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "x86_64" ]]; then
+	export CXXFLAGS="${CXXFLAGS} -arch x86_64"
+elif [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+	export CXXFLAGS="${CXXFLAGS} -arch arm64"
+fi
+
+CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" meson setup --buildtype release \
 	--prefix "${PREFIX}" --strip \
 	--includedir "${PREFIX}/include" \
 	--libdir "${PREFIX}/lib" build/
@@ -27,7 +53,7 @@ chmod 0755 ../scripts/*.pl
 cp -nf ../scripts/*.py ${PREFIX}/bin
 cp -nf ../scripts/*.sh ${PREFIX}/bin
 cp -nf ../scripts/*.pl ${PREFIX}/bin
+cp -nf ../scripts/freebayes-parallel ${PREFIX}/bin
 
 chmod 0755 ${PREFIX}/bin/freebayes
-chmod 0755 ../scripts/freebayes-parallel
-cp -nf ../scripts/freebayes-parallel ${PREFIX}/bin
+chmod 0755 ${PREFIX}/bin/freebayes-parallel
