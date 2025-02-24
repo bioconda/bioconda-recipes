4e5a38f755eee0025fa3a9fbfec1defe227b7f35
tm4zza <mazza.tommaso@gmail.com>
2025-02-24 17:58:29 +0100

    removed gsnap folder (mistake)

--- recipes/gsnap/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/gmapdb_$PKG_VERSION
-env MAX_READLENGTH=500 ./configure --prefix=$PREFIX --with-gmapdb=$PREFIX/gmapdb_$PKG_VERSION
-make
-make install prefix=$PREFIX 
-sed "s/gmapdb_loc=null/gmapdb_loc=gmapdb_$PKG_VERSION/" $RECIPE_DIR/gmap_make_ref.sh > $PREFIX/bin/gmap_make_ref.sh
-chmod a+x $PREFIX/bin/gmap_make_ref.sh
