23e51a8dce83309d7bcbd70a68899776379352d1
mencian <joshua.zhuang@yahoo.com>
2025-02-26 01:00:45 -0600

    sambamba: add arm64 build

--- recipes/sambamba/build.sh
+++ recipes/sambamba/build.sh
@@ -2,14 +2,27 @@
 
 set -xe
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+
+OS=$(uname)
+ARCH=$(uname -m)
+
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+	wget https://github.com/ldc-developers/ldc/releases/download/v1.40.0/ldc2-1.40.0-osx-arm64.tar.xz
+	tar -xf ldc2-1.40.0-osx-arm64.tar.xz
+	export PATH="${SRC_DIR}/ldc2-1.40.0-osx-arm64/bin:${PATH}"
+	export LIBRARY_PATH="${SRC_DIR}/ldc2-1.40.0-osx-arm64/lib-ios-arm64"
+else
+	export LIBRARY_PATH="${PREFIX}/lib"
+fi
 
 # Running `make check` recompiles as an unoptimised binary so must be done prior to release compile
-make -j ${CPU_COUNT} check CC=${CC}
+make -j"${CPU_COUNT}" check CC="${CC}"
 
-make -j ${CPU_COUNT} release CC=${CC} LIBRARY_PATH=${PREFIX}/lib
-make install prefix=${PREFIX}
+make -j"${CPU_COUNT}" release CC="${CC}" LIBRARY_PATH="${LIBRARY_PATH}"
+make install prefix="${PREFIX}"
 
 # The binaries are versioned for some reason
 mv ${PREFIX}/bin/sambamba-* ${PREFIX}/bin/sambamba
+chmod 0755 ${PREFIX}/bin/sambamba
