2010351c109c4c8ba8a5b2f80d2d137d5073789a
mencian <joshua.zhuang@yahoo.com>
2025-02-28 17:15:12 -0600

    bam-readcount: add arm64 build

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -1,39 +1,51 @@
-#!/bin/bash -ex
+#!/bin/bash -euo
 
-wget https://github.com/boostorg/boost/releases/download/boost-1.85.0/boost-1.85.0-cmake.tar.gz
+wget https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.gz
 
-mv boost-1.85.0-cmake.tar.gz vendor/boost-1.55-bamrc.tar.gz
+mv boost-1.87.0-cmake.tar.gz vendor/boost-1.55-bamrc.tar.gz
 
 mkdir -p "${PREFIX}/bin"
 
+sed -i.bak -e 's|2.8.3|3.10|' CMakeLists.txt
+rm -rf *.bak
+sed -i.bak -e 's|2.8|3.10|' cmake/BuildSamtools.cmake
+sed -i.bak -e 's|2.8|3.10|' cmake/BuildBoost.cmake
+rm -rf cmake/*.bak
+
 # Needed for building utils dependency
-export INCLUDES="-I{PREFIX}/include"
+export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -pthread -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 if [[ `uname` == Darwin ]]; then
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-	export CFLAGS="${CFLAGS} -Wno-unguarded-availability -Wdeprecated-non-prototype"
-	export CMAKE_EXTRA="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+	# See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk for -D_LIBCPP_DISABLE_AVAILABILITY
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+	export CFLAGS="${CFLAGS} -O3 -fno-define-target-os-macros -Wno-unguarded-availability -Wno-deprecated-non-prototype -Wno-implicit-function-declaration"
 else
-	export CMAKE_EXTRA=""
+	export CONFIG_ARGS=""
 fi
 
-mkdir -p build
-pushd build
-cmake -S .. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+if [[ `uname` == "Darwin" ]]; then
+	ln -sf ${CC} ${BUILD_PREFIX}/bin/clang
+	ln -sf ${CXX} ${BUILD_PREFIX}/bin/clang++
+else
+	ln -sf ${CC} ${BUILD_PREFIX}/bin/gcc
+	ln -sf ${CXX} ${BUILD_PREFIX}/bin/g++
+fi
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
-	${CMAKE_EXTRA}
+	"${CONFIG_ARGS}"
 
-make clean
-make CXX="${CXX} ${LDFLAGS}" CC="${CC}" CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}"
+cmake --build build --clean-first -j 1 -v
 
-chmod 755 bin/bam-readcount
-cp -f bin/bam-readcount "${PREFIX}/bin"
-popd
+install -v -m 0755 build/bin/bam-readcount "${PREFIX}/bin"
