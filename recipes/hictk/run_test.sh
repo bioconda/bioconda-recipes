a1e2943676cb35d6f4323c2738d3ecc842de18b7
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-03-03 18:31:36 +0100

    Always use curly braces around shell variables

--- recipes/hictk/run_test.sh
+++ recipes/hictk/run_test.sh
@@ -6,7 +6,7 @@ set -x
 set -o pipefail
 
 which hictk
-if [[ "$OSTYPE" =~ .*darwin.* ]]; then
+if [[ "${OSTYPE}" =~ .*darwin.* ]]; then
   otool -L "$(which hictk)"
 else
   ldd "$(which hictk)"
@@ -19,7 +19,7 @@ url="$(grep -F 'DOWNLOAD' 'cmake/FetchTestDataset.cmake' | sed -E 's/.*DOWNLOAD[
 checksum="$(grep -F 'EXPECTED_HASH' 'cmake/FetchTestDataset.cmake' | sed 's/.*SHA256=//')"
 
 # Download and extract test datasets
-curl -L "$url" -o hictk_test_dataset.tar.zst
+curl -L "${url}" -o hictk_test_dataset.tar.zst
 echo "$checksum  hictk_test_dataset.tar.zst" > checksum.sha256
 shasum -c checksum.sha256
 
@@ -40,7 +40,7 @@ hictk_integration_suite \
   test/integration/config.toml \
   --data-dir test/data \
   --do-not-copy-binary \
-  --threads "$CPU_COUNT" \
+  --threads "${CPU_COUNT}" \
   --result-file results.json
 
 printf '#####\n#####\n#####\n\n\n'
