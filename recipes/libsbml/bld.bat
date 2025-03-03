1968a2ff37922f949cb9ff11ab4cf482e60bbb70
Vincent Noël <vincent.noel@curie.fr>
2025-03-03 14:38:39 -0300

    Added windows support for libsbml

---
+++ recipes/libsbml/bld.bat
@@ -0,0 +1,19 @@
+echo on
+
+cmake -G"Ninja" -S . -B build ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="%PREFIX%"/lib ^
+    -DCMAKE_CXX_COMPILER="%CXX%" ^
+    -DCMAKE_C_COMPILER="%CC%" ^
+    -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
+    -DWITH_SWIG=OFF ^
+    -DLIBXML_LIBRARY="%PREFIX%"/Library/lib/libxml2.lib ^
+    -DLIBXML_INCLUDE_DIR="%PREFIX%"/Library/include/libxml2 ^
+    -DENABLE_COMP=ON -DENABLE_FBC=ON -DENABLE_GROUPS=ON ^
+    -DENABLE_LAYOUT=ON -DENABLE_MULTI=ON -DENABLE_QUAL=ON ^
+    -DENABLE_RENDER=ON -DENABLE_DISTRIB=ON -DENABLE_ARRAYS=ON ^
+    -DENABLE_DYN=ON -DENABLE_REQUIREDELEMENTS=ON ^
+    -DENABLE_SPATIAL=ON -DWITH_CPP_NAMESPACE=ON
+
+cmake --build build --parallel %CPU_COUNT% --config Release
+cmake --build build --target install
