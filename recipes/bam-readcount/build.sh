95fbfaec580deffa822c16dd16c55f849f2bbc03
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-03-05 01:39:30 -0600

    Update build.sh

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -27,7 +27,7 @@ if [[ "${OS}" == "Darwin" ]]; then
         export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
         # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk for -D_LIBCPP_DISABLE_AVAILABILITY
         #export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-        export CFLAGS="${CFLAGS} -O3 -fno-define-target-os-macros -Wno-unguarded-availability -Wno-deprecated-non-prototype -Wno-implicit-function-declaration"
+        export CFLAGS="${CFLAGS} -O3 -mmacosx-version-min=10.13 -fno-define-target-os-macros -Wno-unguarded-availability -Wno-deprecated-non-prototype -Wno-implicit-function-declaration"
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
         ln -sf ${CC} ${BUILD_PREFIX}/bin/gcc
