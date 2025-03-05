7eb748094b9d038fd91727b2ff7c0ce4cafdb20c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-03-04 22:32:02 -0600

    Update build.sh

--- recipes/bam-readcount/build.sh
+++ recipes/bam-readcount/build.sh
@@ -4,10 +4,6 @@ mkdir -p "${PREFIX}/bin"
 
 wget https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.gz
 mv boost-1.87.0-cmake.tar.gz vendor/boost-1.55-bamrc.tar.gz
-wget https://github.com/tukaani-project/xz/archive/refs/tags/v5.6.4.tar.gz
-mv v5.6.4.tar.gz vendor/xz-5.2.4.tar.gz
-wget https://github.com/Mbed-TLS/mbedtls/releases/download/mbedtls-2.28.9/mbedtls-2.28.9.tar.bz2
-mv mbedtls-2.28.9.tar.bz2 vendor/mbedtls-2.16.4-apache.tgz
 
 sed -i.bak -e 's|2.8.3|3.10|' CMakeLists.txt
 rm -rf *.bak
@@ -33,7 +29,6 @@ if [[ "${OS}" == "Darwin" ]]; then
         #export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
         export CFLAGS="${CFLAGS} -O3 -fno-define-target-os-macros -Wno-unguarded-availability -Wno-deprecated-non-prototype -Wno-implicit-function-declaration"
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-	export LIBS="-lrt"
 else
         ln -sf ${CC} ${BUILD_PREFIX}/bin/gcc
         ln -sf ${CXX} ${BUILD_PREFIX}/bin/g++
@@ -44,6 +39,10 @@ if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
 	wget https://github.com/alexey-lysiuk/macos-sdk/releases/download/13.3/MacOSX13.3.tar.xz
 	tar -xf MacOSX13.3.tar.xz
 	cp -rH MacOSX13.3.sdk /Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
+	wget https://github.com/tukaani-project/xz/archive/refs/tags/v5.6.4.tar.gz
+	mv v5.6.4.tar.gz vendor/xz-5.2.4.tar.gz
+	wget https://github.com/Mbed-TLS/mbedtls/releases/download/mbedtls-2.28.9/mbedtls-2.28.9.tar.bz2
+	mv mbedtls-2.28.9.tar.bz2 vendor/mbedtls-2.16.4-apache.tgz
 	export SDKROOT="/Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk"
 	export CONFIG_ARGS="${CONFIG_ARGS} -DCMAKE_OSX_SYSROOT=/Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk"
 	export MACOSX_DEPLOYMENT_TARGET=13.0
