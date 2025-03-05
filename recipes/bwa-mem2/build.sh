58b651209459dd9a5d02e58e676c391d8b78bbe7
David Lecomber <38504854+dslarm@users.noreply.github.com>
2025-03-05 14:30:44 +0000

    Update build.sh: restore the renaming of memset_s functions on osx-64

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -4,6 +4,12 @@ set -x
 export CXXFLAGS="${CXXFLAGS} -std=c++14"
 
 if [ "$(uname)" = "Darwin" -a "$(uname -m)" = "x86_64" ]; then
+    sed -i.bak "s#extern errno_t memset_s#//xxx extern errno_t memset_s#g" ext/safestringlib/include/safe_mem_lib.h
+    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/include/safe_mem_lib.h
+    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset16_s.c
+    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset32_s.c
+    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset_s.c
+    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/wmemset_s.c
  # for safestringlib to compile
     CFLAGS="${CFLAGS} -Wno-error=implicit-function-declaration"
 fi
