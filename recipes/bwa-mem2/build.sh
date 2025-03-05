6d66a6f6f739ab617303ee4f30f2997aec9f054d
David Lecomber <38504854+dslarm@users.noreply.github.com>
2025-03-05 00:00:39 +0000

    Update build.sh: remove stale Darwin amends; set CXXFLAGS to c++14

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -x
-# https://github.com/intel/safestringlib/issues/14
-if [[ $OSTYPE == "darwin"* ]]; then
-    sed -i.bak "s#extern errno_t memset_s#//xxx extern errno_t memset_s#g" ext/safestringlib/include/safe_mem_lib.h
-    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/include/safe_mem_lib.h
-    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset16_s.c
-    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset32_s.c
-    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/memset_s.c
-    sed -i.bak 's/memset_s/memset8_s/g' ext/safestringlib/safeclib/wmemset_s.c
-fi
+
+export CXXFLAGS="${CXXFLAGS} -std=c++14"
+
 rm -rf ext/sse2neon
 git submodule add https://github.com/DLTcollab/sse2neon ext/sse2neon
 cd ext/sse2neon ; git checkout tags/v1.8.0
