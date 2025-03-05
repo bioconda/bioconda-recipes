6876605229342c70cd7c4cf49fc1d6e63f27769f
mencian <joshua.zhuang@yahoo.com>
2025-03-04 22:32:52 -0600

    add cargo-bundle-licenses

--- recipes/autocycler/build.sh
+++ recipes/autocycler/build.sh
@@ -3,6 +3,8 @@
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 cargo install --verbose --no-track --path . --root "${PREFIX}"
