767b522044c389c1bc417f5356d5219192252520
David Lecomber <38504854+dslarm@users.noreply.github.com>
2025-03-05 15:00:56 +0000

    Update build.sh: tidy up case statement, only grab sse2neon on aarch64/arm64

--- recipes/bwa-mem2/build.sh
+++ recipes/bwa-mem2/build.sh
@@ -14,19 +14,16 @@ if [ "$(uname)" = "Darwin" -a "$(uname -m)" = "x86_64" ]; then
     CFLAGS="${CFLAGS} -Wno-error=implicit-function-declaration"
 fi
 
-rm -rf ext/sse2neon
-git submodule add https://github.com/DLTcollab/sse2neon ext/sse2neon
-cd ext/sse2neon ; git checkout tags/v1.8.0
-cd ../..
 
 case "$(uname -m)" in
   x86_64)
       	LIBS="${LDFLAGS}" make -j${CPU_COUNT} CC="${CC}" CXX="${CXX}" multi ;;
-  aarch64)      
+  arm64|aarch64)      
+        rm -rf ext/sse2neon
+        git submodule add https://github.com/DLTcollab/sse2neon ext/sse2neon
+        cd ext/sse2neon ; git checkout tags/v1.8.0
+        cd ../..
 	LIBS="${LDFLAGS}" make -j${CPU_COUNT} arch="-march=armv8-a" EXE=bwa-mem2 CC="${CC}" CXX="${CXX}" all
-      ;;
-  arm64) # has to be darwin
-        LIBS="${LDFLAGS}" make -j${CPU_COUNT} arch="-march=armv8-a" EXE=bwa-mem2 CC="${CC}" CXX="${CXX}" all
       ;;
   *)
       echo "Not supported architecture: $(uname -m)" ;;
