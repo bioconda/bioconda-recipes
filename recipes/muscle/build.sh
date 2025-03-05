040152b3521a7e68fe7d16d08c9a415d232379ce
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-03-05 03:41:52 -0600

    Update build.sh

--- recipes/muscle/build.sh
+++ recipes/muscle/build.sh
@@ -16,24 +16,17 @@ cp -rf ${RECIPE_DIR}/vcxproj_make.py .
 chmod 0755 vcxproj_make.py
 
 OS=$(uname)
-ARCH=$(uname -m)
 
-if [[ "${OS}" == "Darwin" && "${ARCH}" == "x86_64" ]]; then
+if [[ "${OS}" == "Darwin" ]]; then
 	python ./vcxproj_make.py --openmp --pthread --cppcompiler "${CXX}" --ccompiler "${CC}"
-elif [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
- 	#cp -rfv ${RECIPE_DIR}/sse2neon.h ${SRC_DIR}/src
- 	python ./vcxproj_make.py --openmp --pthread --cppcompiler "${CXX}" --ccompiler "${CC}"
-elif [[ "${OS}" == "Linux" && "${ARCH}" == "aarch64" ]]; then
-	#cp -rfv ${RECIPE_DIR}/sse2neon.h ${SRC_DIR}/src
+elif [[ "${OS}" == "Linux" ]]; then
   	python ./vcxproj_make.py --openmp --lrt --pthread --cppcompiler "${CXX}" --ccompiler "${CC}"
-else
- 	python ./vcxproj_make.py --openmp --lrt --pthread --cppcompiler "${CXX}" --ccompiler "${CC}"
 fi
 
 # Verify binary exists and is executable
-if [[ ! -x ../bin/muscle ]]; then
-	echo "Error: muscle binary not found"
-	exit 1
-fi
+#if [[ ! -x ../bin/muscle ]]; then
+	#echo "Error: muscle binary not found"
+	#exit 1
+#fi
 
 install -v -m 0755 ../bin/muscle ${PREFIX}/bin
