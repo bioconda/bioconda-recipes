235ebdeef567e2673fbe0069a91914dfd251f4a6
tripleldh <tong.z@mail.sdu.edu.cn>
2025-03-06 11:29:19 -0500

    fix bug

--- recipes/rabbitsketch/setup.py
+++ recipes/rabbitsketch/setup.py
@@ -38,7 +38,8 @@ class CMakeBuild(build_ext):
             extdir += os.path.sep
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+                      '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      ]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
