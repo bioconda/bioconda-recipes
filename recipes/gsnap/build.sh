0a8116494fab457e4b0c02a3f49532090a822ed2
tm4zza <mazza.tommaso@gmail.com>
2025-03-08 08:34:36 +0000

    gsnap: clean build.sh

--- recipes/gsnap/build.sh
+++ recipes/gsnap/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export CFLAGS="${CFLAGS:-} -O3 -m64 -I${PREFIX}/include"
-
 mkdir -p $PREFIX/gmapdb_$PKG_VERSION
+
 env MAX_READLENGTH=500 ./configure --prefix=$PREFIX --with-gmapdb=$PREFIX/gmapdb_$PKG_VERSION
 make
 make install prefix=$PREFIX 
+
 sed "s/gmapdb_loc=null/gmapdb_loc=gmapdb_$PKG_VERSION/" $RECIPE_DIR/gmap_make_ref.sh > $PREFIX/bin/gmap_make_ref.sh
 chmod a+x $PREFIX/bin/gmap_make_ref.sh
