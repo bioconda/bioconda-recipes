79246e88da29c84019d24b16933cd074844982bc
Kerim Heber <122597229+kheber@users.noreply.github.com>
2025-03-11 22:09:29 -0400

    Try using make instead to resolve compile errors

--- recipes/strainr2/build.sh
+++ recipes/strainr2/build.sh
@@ -2,17 +2,16 @@
 
 mkdir -p "${PREFIX}/bin"
 
-${CC} ${SRC_DIR}/src/subcontig.c -O3 -o subcontig
-${CC} ${SRC_DIR}/src/hashcounter.c -I$PREFIX/include -L$PREFIX/lib -O3 -o hashcounter -lz -lpthread
+make -C src release
 
 chmod +x $SRC_DIR/src/PreProcessR
-chmod +x $SRC_DIR/subcontig
-chmod +x $SRC_DIR/hashcounter
+chmod +x $SRC_DIR/src/subcontig
+chmod +x $SRC_DIR/src/hashcounter
 chmod +x $SRC_DIR/src/Plot.R
 chmod +x $SRC_DIR/src/StrainR
 
 cp $SRC_DIR/src/PreProcessR ${PREFIX}/bin/
-cp $SRC_DIR/subcontig ${PREFIX}/bin/
-cp $SRC_DIR/hashcounter ${PREFIX}/bin/
+cp $SRC_DIR/src/subcontig ${PREFIX}/bin/
+cp $SRC_DIR/src/hashcounter ${PREFIX}/bin/
 cp $SRC_DIR/src/Plot.R ${PREFIX}/bin/
 cp $SRC_DIR/src/StrainR ${PREFIX}/bin/
