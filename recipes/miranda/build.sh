ccbecef594f71c87892aa72bd34defc93675428f
David Lecomber <david.lecomber@arm.com>
2025-03-12 17:47:35 +0000

    add gnuconfig to get support for recent archs..

--- recipes/miranda/build.sh
+++ recipes/miranda/build.sh
@@ -3,6 +3,9 @@ set -e
 export CFLAGS="${CFLAGS} -fcommon"
 export CXXFLAGS="${CXXFLAGS} -fcommon"
 
+# use newer config.guess and config.sub that support linux-aarch64
+cp -rf ${RECIPE_DIR}/config.* .
+
 ./configure --prefix=$PREFIX
 make
 make check
