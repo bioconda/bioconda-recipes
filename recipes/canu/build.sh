b2b0de336f62fa198f13d2689a410871d1fc76f4
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-03-12 15:27:23 -0500

    Update build.sh

--- recipes/canu/build.sh
+++ recipes/canu/build.sh
@@ -16,9 +16,7 @@ mkdir -p "${PREFIX}/share/java/classes"
 cp -rfv src/pipelines/canu/*.pm "${PREFIX}/lib/perl5/site_perl/canu"
 cp -rfv src/mhap/mhap-2.1.3.jar "${PREFIX}/share/java/classes"
 
-cd src/utility
-git checkout a96ff941a9882f68639bb88d94f7272c653c4ec9
-cd ../
+cd src
 make CC="${CC} -O3" CXX="${CXX} -O3 -I${PREFIX}/include" -j"${CPU_COUNT}"
 
 cp -rfv ${SRC_DIR}/build/lib/libcanu.a "${PREFIX}/lib"
