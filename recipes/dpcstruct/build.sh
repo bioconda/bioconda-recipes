974107fe918565f0eb3267315d4707459d245df1
FedeBar <fedebarone@gmail.com>
2025-03-13 13:56:34 +0100

    Update licensce and clean debug message

--- recipes/dpcstruct/build.sh
+++ recipes/dpcstruct/build.sh
@@ -6,7 +6,3 @@ echo $PREFIX
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_VERBOSE_MAKEFILE=ON ..
 make -j ${CPU_COUNT} ${VERBOSE_CM}
 make install
-
-# Debug: List contents of $PREFIX
-echo "Contents of $PREFIX:"
-ls -R $PREFIX
