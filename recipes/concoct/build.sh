f434f66d216a145103d25dcac9d550e23748c742
ZerryNi <996002763.com>
2025-03-14 16:05:42 +0800

    concoct

--- recipes/concoct/build.sh
+++ recipes/concoct/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export CFLAGS="-I$PREFIX/include"
+export CFLAGS="-I$PREFIX/include -I/usr/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+
