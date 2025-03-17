b5c015ab71c41c12b13bb4e687e03d4d88672e19
David Lecomber <david.lecomber@arm.com>
2025-03-17 18:32:46 +0000

    build from source

--- recipes/plink/build.sh
+++ recipes/plink/build.sh
@@ -2,6 +2,11 @@
 
 set -xe
 
+wget https://github.com/simd-everywhere/simde/archive/refs/tags/v0.8.2.tar.gz
+tar zxvf v0.8.2.tar.gz
+
+export CFLAGS="${CFLAGS} -I simd-0.8.2/simde -Wall -O2"
+
+make CFLAGS=${CFLAGS} PREFIX={$PREFIX} CC=${CC}
+make install 
 # Install as plink
-mkdir -p $PREFIX/bin
-install -v -m 0755 plink $PREFIX/bin/
