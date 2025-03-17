ea02e0b84445ae735b96803084c75666d28e76c6
David Lecomber <david.lecomber@arm.com>
2025-03-17 18:41:17 +0000

    fix typo

--- recipes/plink/build.sh
+++ recipes/plink/build.sh
@@ -7,6 +7,6 @@ tar zxvf v0.8.2.tar.gz
 
 export CFLAGS="${CFLAGS} -I simd-0.8.2/simde -Wall -O2"
 
-make CFLAGS=${CFLAGS} PREFIX={$PREFIX} CC=${CC}
+make CFLAGS=${CFLAGS} PREFIX=${PREFIX} CC=${CC}
 make install 
 # Install as plink
