6255396efb8b99b7b829d24c0fe79c7f2a5d4f7e
David Lecomber <38504854+dslarm@users.noreply.github.com>
2025-03-18 00:10:39 +0000

    Update build.sh: override makefile's link / compile settings

--- recipes/plink/build.sh
+++ recipes/plink/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 set -xe
 
 wget https://github.com/simd-everywhere/simde/archive/refs/tags/v0.8.2.tar.gz
@@ -8,11 +7,11 @@ tar zxvf v0.8.2.tar.gz
 
 export CFLAGS="${CFLAGS} -I ${SRC_DIR}/simde-0.8.2/simde -Wall -O2 -DDYNAMIC_ZLIB"
 export CXXFLAGS="${CXXFLAGS} -I ${SRC_DIR}/simde-0.8.2/simde -Wall -O2"
-
-echo $PWD
+export LDFLAGS="${LDFLAGS} -llapack -lblas -lcblas -lpthread"
 
 echo Building ... 
-make CFLAGS="${CFLAGS}" PREFIX="${PREFIX}" CC="${CC}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
+make -j CFLAGS="${CFLAGS}" DESTDIR="${PREFIX}" PREFIX="" CC="${CC}" CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" ZLIB="-lz" LDFLAGS="${LDFLAGS}" BLASFLAGS=""
 
 echo Installing
-make install 
+make install DESTDIR="${PREFIX}" PREFIX="" 
+
