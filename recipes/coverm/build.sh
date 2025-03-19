c650657fee99254e2e134f1898d14e66789a1f07
root <root@localhost.localdomain>
2025-03-19 17:49:28 +0800

    coverm

--- recipes/coverm/build.sh
+++ recipes/coverm/build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -e
 
 
-if grep -r 'let canonical_ptr = canonical' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs; then
-    echo "存在该行代码"
-else
-    echo "不存在该行代码"
+if ! grep -r 'let canonical_ptr = canonical' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs; then
     sed -i '2318i\            let canonical_ptr = canonical as *mut i8 as *mut u8;' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
     sed -i '2324s/.*/                canonical_ptr,/' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
 fi
 
 
-# Build statically linked binary with Rust
+
+
+
 C_INCLUDE_PATH=$PREFIX/include \
 LIBRARY_PATH=$PREFIX/lib \
 cargo build --release
 
-# Install the binaries
 cargo install --root $PREFIX
