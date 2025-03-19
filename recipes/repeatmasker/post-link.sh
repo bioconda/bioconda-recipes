b0982c18129eb8e028962ce5c3d780ca8491d9c9
Tobias Baril <46785187+TobyBaril@users.noreply.github.com>
2025-03-19 12:21:55 +0100

    Update post-link.sh

--- recipes/repeatmasker/post-link.sh
+++ recipes/repeatmasker/post-link.sh
@@ -9,9 +9,11 @@ gunzip -c Dfam-RepeatMasker.lib.gz > ${PREFIX}/share/RepeatMasker/Libraries/Repe
 rm -rf Dfam-RepeatMasker.lib.gz
 
 EOF
-elif [ "$(${PREFIX}/share/RepeatMasker/RepeatMasker -help | head 1)" != "RepeatMasker version 4.1.8" ] # check if v4.1.8, only get libs if not, as 4.1.8 comes with minimum library
+elif [ "$(grep '$VERSION' ${PREFIX}/share/RepeatMasker/RepeatMaskerConfig.pm)" != '$VERSION = "4.1.8";' ] # check if v4.1.8, only get libs if not, as 4.1.8 comes with minimum library
     echo "Dfam-RepeatMasker.lib.gz from www.dfam.org"
     wget -O Dfam-RepeatMasker.lib.gz https://www.dfam.org/releases/Dfam_3.8/families/Dfam-RepeatMasker.lib.gz
     gunzip -c Dfam-RepeatMasker.lib.gz > ${PREFIX}/share/RepeatMasker/Libraries/RepeatMasker.lib
     rm -rf Dfam-RepeatMasker.lib.gz
+else
+    echo "No need for post-link.sh"
 fi
