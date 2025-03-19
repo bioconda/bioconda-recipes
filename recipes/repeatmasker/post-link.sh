c39013795e3b1bc8bf7d989cca8f958c3f900ad5
Tobias Baril <46785187+TobyBaril@users.noreply.github.com>
2025-03-19 11:21:54 +0100

    Update post-link.sh

--- recipes/repeatmasker/post-link.sh
+++ recipes/repeatmasker/post-link.sh
@@ -9,7 +9,7 @@ gunzip -c Dfam-RepeatMasker.lib.gz > ${PREFIX}/share/RepeatMasker/Libraries/Repe
 rm -rf Dfam-RepeatMasker.lib.gz
 
 EOF
-else
+elif [ "$(RepeatMasker -help | head 1)" != "RepeatMasker version 4.1.8" ]
     echo "Dfam-RepeatMasker.lib.gz from www.dfam.org"
     wget -O Dfam-RepeatMasker.lib.gz https://www.dfam.org/releases/Dfam_3.8/families/Dfam-RepeatMasker.lib.gz
     gunzip -c Dfam-RepeatMasker.lib.gz > ${PREFIX}/share/RepeatMasker/Libraries/RepeatMasker.lib
