5fa1cd1259861d4395195c873168c38005fced27
Simon Roux <siroux1@gmail.com>
2025-03-20 15:13:30 -0700

    add removal of activate/deactivate files in pre-unlink

--- recipes/cctyper/pre-unlink.sh
+++ recipes/cctyper/pre-unlink.sh
@@ -3,3 +3,8 @@ CCT_DB_DIR=${PREFIX}/cct_data
 if [[ -d $CCT_DB_DIR ]]; then
     rm -r $CCT_DB_DIR
 fi
+echo "Also removing the files that were setting up the relevant env variable"
+for CHANGE in "activate" "deactivate"
+do
+    rm "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
