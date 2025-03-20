a4c9abd22ddeb93fc6c84c9e3360dcf06fe92e80
LouisPwr <lokraf@dtu.dk>
2025-03-20 18:40:14 +0100

    lzma

--- recipes/addeam/build.sh
+++ recipes/addeam/build.sh
@@ -2,7 +2,7 @@
 set -e  # Exit on error
 
 export CPATH=${PREFIX}/include
-
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 #export CXXFLAGS="-I${PREFIX}/include -I${PREFIX}/lib"
 #export LDFLAGS="-L${PREFIX}/lib -llzma"
 #export LDFLAGS="-L${PREFIX}/lib -llzma"
