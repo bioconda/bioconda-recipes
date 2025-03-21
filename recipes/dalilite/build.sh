d7fce8b68b476da3ac316d36d189d1cb8c404747
Michael Milton <ttmigueltt@gmail.com>
2025-03-21 19:15:10 +1100

    Use ${} interpolation

--- recipes/dalilite/build.sh
+++ recipes/dalilite/build.sh
@@ -1,6 +1,6 @@
 cd bin
 # Replace the hardcoded gcc and gfortran with ones specified via environment variables
-sed -i 's/gcc/$CC/g' Makefile
-sed -i 's/gfortran/$FC/g' Makefile
+sed -i 's/gcc/${CC}/g' Makefile
+sed -i 's/gfortran/${FC}/g' Makefile
 make
 cp * $PREFIX/bin
