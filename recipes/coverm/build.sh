29e0bc6f4844e31874679abccfe81e961cdfa887
root <root@localhost.localdomain>
2025-03-21 16:45:50 +0800

    coverm

--- recipes/coverm/build.sh
+++ recipes/coverm/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 
-if ! grep -r 'let canonical_ptr = canonical' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs; then
-    sed -i '2318i\            let canonical_ptr = canonical as *mut i8 as *mut u8;' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
-    sed -i '2324s/.*/                canonical_ptr,/' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
-fi
+#if ! grep -r 'let canonical_ptr = canonical' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs; then
+#    sed -i '2318i\            let canonical_ptr = canonical as *mut i8 as *mut u8;' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
+#    sed -i '2324s/.*/                canonical_ptr,/' /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
+#fi
 
 
 
