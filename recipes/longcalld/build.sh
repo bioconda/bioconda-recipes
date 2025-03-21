bc7d29845acc785c054f6c8f1842c00fd59a8918
Yan Gao <yangao07@outlook.com>
2025-03-21 18:21:16 -0400

    Update build.sh

--- recipes/longcalld/build.sh
+++ recipes/longcalld/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $PREFIX/bin
 
-make -f Makefile.conda
+make -f Makefile.conda CFLAGS="${CFLAGS} -I${PREFIX}/include -L${PREFIX}/lib" opt_lib=${PREFIX}/lib portable=1
 
 chmod 0755 bin/longcallD
 cp -f bin/longcallD $PREFIX/bin/
