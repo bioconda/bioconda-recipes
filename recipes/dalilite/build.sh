f4f3ac5e1c5715728af0767b078cfda5b2b53c9b
Michael Milton <ttmigueltt@gmail.com>
2025-03-24 10:05:13 +1100

    Patch the makefile

--- recipes/dalilite/build.sh
+++ recipes/dalilite/build.sh
@@ -1,6 +1,3 @@
 cd bin
-# Replace the hardcoded gcc and gfortran with ones specified via environment variables
-sed -i "s/gcc/${CC}/g" Makefile
-sed -i "s/gfortran/${FC}/g" Makefile
 make
 cp * $PREFIX/bin
