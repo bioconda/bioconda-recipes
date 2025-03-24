65bf7b8bf1caed9a52570920eff97dc6179d166c
root <root@localhost.localdomain>
2025-03-24 16:05:25 +0800

    coverm

--- recipes/coverm/build.sh
+++ recipes/coverm/build.sh
@@ -11,10 +11,8 @@ LIBRARY_PATH=$PREFIX/lib \
 cargo build --release || true
 
 catalogue=$BUILD_PREFIX/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rust-htslib-0.44.1/src/bam/record.rs
-echo "目录：$catalogue"
 
 if [ -f "$catalogue" ]; then
-    echo "目录：进入目录，修改内容"
     if ! grep -r 'let canonical_ptr = canonical' $catalogue; then
         sed -i '2318i\            let canonical_ptr = canonical as *mut i8 as *mut u8;' $catalogue
         sed -i '2325s/.*/                canonical_ptr,/' $catalogue
