04db68dc9891f2c856872feec78ee24cd7f5ca1e
Sang Chul Choi <sangchulchoi@sungshin.ac.kr>
2025-03-28 18:27:45 +0900

    Update polap to v0.4.3.7.3

--- recipes/polap/build.sh
+++ recipes/polap/build.sh
@@ -8,6 +8,7 @@ files=(polap
 	polap-mt.1.c70.3.faa
 	polap-pt.2.c70.3.faa
 	run-polap-ncbitools
+	polap-batch-v2.sh
 	polap-build-msbwt.sh
 	polap-command-completion.sh
 	polap-constants.sh
@@ -127,6 +128,7 @@ files=(polap
 	run-polap-r-test-reads-bar-graph.R
 	polap-data-v1.csv
 	polap-data-v2.csv
+	polap-data-v2-later.csv
 	polap-data-v4.csv
 	polap.sh)
 
