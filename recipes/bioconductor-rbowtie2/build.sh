50f49ae38a2d441d7787f42320136417797b293f
Alicia A. Evans <108547992+aliciaaevans@users.noreply.github.com>
2025-03-28 12:12:03 -0400

    see if stdlib fixes this issue

--- recipes/bioconductor-rbowtie2/build.sh
+++ recipes/bioconductor-rbowtie2/build.sh
@@ -5,8 +5,7 @@ mkdir -p ~/.R
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX -I${PREFIX}/include -L${PREFIX}/lib
-CXX98=$CXX -I${PREFIX}/include -L${PREFIX}/lib
-CXX11=$CXX -I${PREFIX}/include -L${PREFIX}/lib
-CXX14=$CXX -I${PREFIX}/include -L${PREFIX}/lib" > ~/.R/Makevars
-export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/lib
+CXX98=$CXX
+CXX11=$CXX
+CXX14=$CXX" > ~/.R/Makevars
 $R CMD INSTALL --build .
