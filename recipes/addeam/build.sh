71eb41202b444ac2697b1e5af09b836becd26eaa
LouisPwr <lokraf@dtu.dk>
2025-03-28 11:36:09 +0100

    changes

--- recipes/addeam/build.sh
+++ recipes/addeam/build.sh
@@ -9,23 +9,17 @@ export CFLAGS="-I${SRC_DIR}/submodules/lib/htslib -I${SRC_DIR}/submodules/lib/sa
 export CPPFLAGS="-I${SRC_DIR}/submodules/lib/htslib -I${SRC_DIR}/submodules/lib/samtools -I${SRC_DIR}/submodules/lib/libgab -I${PREFIX}/include"
 export CXXFLAGS="-I${SRC_DIR}/submodules/lib/htslib -I${SRC_DIR}/submodules/lib/samtools -I${SRC_DIR}/submodules/lib/libgab -I${PREFIX}/include"
 
+ln -s ${PREFIX}/lib/libncurses.so ${PREFIX}/lib/libcurses.so
+
 mkdir -p "${PREFIX}/bin"
 
-cd submodules/src/
-
-ln -s ${PREFIX}/lib/libncurses.so ${PREFIX}/lib/libcurses.so
+pushd submodules/src/
 
 make clean
 make
-mv bam2prof "${PREFIX}/bin/"
-chmod +x "${PREFIX}/bin/bam2prof"
-cd ../..
+install -v -m 0755 bam2prof "${PREFIX}/bin/"
+popd 
 
-# Ensure Python scripts are installed correctly
-cp addeam-bam2prof.py addeam-cluster.py "${PREFIX}/bin/"
-chmod +x "${PREFIX}/bin/addeam-bam2prof.py" "${PREFIX}/bin/addeam-cluster.py"
-
-# Add ${PREFIX}/bin to PATH so tests can find the executables
-export PATH="${PREFIX}/bin:$PATH"
+install -v -m 0755 addeam-bam2prof.py addeam-cluster.py "${PREFIX}/bin/"
 
 
