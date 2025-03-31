d8861c17ab9b127f029d83dc9510ffa1b5c803e9
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-03-31 17:42:37 +0200

    Try patching

--- recipes/mpboot/build.sh
+++ recipes/mpboot/build.sh
@@ -34,11 +34,7 @@ else
     EXE_NAME=mpboot
 fi
 
-mkdir build
-
-cd build
-
-cmake -S ../ -B . \
+cmake -S . -B . \
     -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
