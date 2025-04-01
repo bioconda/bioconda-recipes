ce942e823c02aa20c1638fcef816dcd91ea00ebd
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-04-01 11:33:46 +0200

    Try to enable LTO

--- recipes/hictk/build.sh
+++ recipes/hictk/build.sh
@@ -46,6 +46,7 @@ CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${PWD}/build"
 # https://docs.conda.io/projects/conda-build/en/stable/user-guide/environment-variables.html#environment-variables-set-during-the-build-process
 cmake -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}"   \
       -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" \
+      -DCMAKE_LINKER_TYPE=LLD                    \
       -DBUILD_SHARED_LIBS=ON                     \
       -DENABLE_DEVELOPER_MODE=OFF                \
       -DHICTK_ENABLE_TESTING=ON                  \
