1a94fa48ebd47c6ae7bbac62c592db5caf96857e
khaled196 <khalled.jooma@yahoo.com>
2025-04-01 13:02:42 +0000

    fix linit

--- recipes/clinCNV/bld.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-mkdir "%PREFIX%\Library\bin\clincnv"
-xcopy /s /e /y "%SRC_DIR%\*" "%PREFIX%\Library\bin\clincnv\"
-
-for %%s in (clinCNV.R mergeFilesFromFolder.R mergeFilesFromFolderDT.R) do (
-    echo @echo off > "%PREFIX%\Scripts\%%s.bat"
-    echo Rscript "%CONDA_PREFIX%\Library\bin\clincnv\%%s" %%* >> "%PREFIX%\Scripts\%%s.bat"
-)
