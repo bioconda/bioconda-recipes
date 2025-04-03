bda3af5ed642380d2e7f41d048328b6d2c59659a
Erin McAuley <erin@fulcrumgenomics.com>
2025-04-03 13:56:51 -0400

    add hisat-3n

---
+++ recipes/hisat-3n/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+git clone https://github.com/simd-everywhere/simde-no-tests simde
+
+make -j ${CPU_COUNT} \
+    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+    CPP="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+
+# copy binaries and python scripts
+mkdir -p "${PREFIX}/bin"
+for i in \
+    hisat-3n-build \
+    hisat-3n \
+    hisat-3n-table \
+    hisat2-align-l \
+    hisat2-align-s
+do
+    cp "${i}" "${PREFIX}/bin/"
+    chmod +x "${PREFIX}/bin/${i}"
+done
