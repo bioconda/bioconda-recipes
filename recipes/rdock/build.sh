11899aca359c0d05525debcdb66222d774ec5bed
UENO, M. <eunosium-1128@live.jp>
2025-04-04 02:08:39 +0000

    rdock: add error handling and ensure activation scripts are copied

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+set -euo pipefail
+
 make -j${CPU_COUNT}
 PREFIX=${PREFIX} make install
+
+mkdir -p ${PREFIX}/etc/conda/activate.d
+mkdir -p ${PREFIX}/etc/conda/deactivate.d
+cp ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/activate.sh
+cp ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/deactivate.sh
