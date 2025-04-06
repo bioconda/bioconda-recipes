99c514ce321a8991dfddea2303abd8fe58375a18
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-06 17:35:54 -0500

    Update build.sh

--- recipes/clam/build.sh
+++ recipes/clam/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -euo
-
 set -xe
 
-RUST_BACKTRACE=full
-cargo install -v --locked --root "$PREFIX" --path .
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+RUST_BACKTRACE=1
+cargo install -v --locked --root "${PREFIX}" --path .
