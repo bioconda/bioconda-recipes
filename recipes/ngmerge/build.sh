9568020ae4845efa05967d0d8b775ca8deefb3dd
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-06 23:15:26 -0500

    Update build.sh

--- recipes/ngmerge/build.sh
+++ recipes/ngmerge/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-if [[ "$(uname)" == Darwin ]]; then
+export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
+
+if [[ "$(uname)" == "Darwin" ]]; then
     version=`conda info llvm-openmp | grep '^version' | awk -F': ' '{print $2}'`
     CFLAGS="-I${PREFIX}/lib/clang/${version}/include ${CFLAGS}"
-    LDFLAGS="-lomp $LDFLAGS"
+    LDFLAGS="-lomp ${LDFLAGS}"
 else
-    CFLAGS="-fopenmp $CFLAGS"
+    CFLAGS="-fopenmp ${CFLAGS}"
 fi 
 
-make
-make install PREFIX=$PREFIX
+make -j"${CPU_COUNT}"
+make install PREFIX="${PREFIX}"
