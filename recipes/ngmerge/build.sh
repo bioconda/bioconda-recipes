1f1ef7ad46f934cbf234cdeebc041421624d1f58
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-07 01:01:33 -0500

    Update build.sh

--- recipes/ngmerge/build.sh
+++ recipes/ngmerge/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CFLAGS="${CFLAGS} -g -Wall -std=gnu99 -O3 -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -g -Wall -std=gnu11 -O3 -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -10,4 +10,4 @@ else
 fi 
 
 make CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
-make install PREFIX="${PREFIX}"
+install -v -m 0755 NGmerge "${PREFIX}/bin"
