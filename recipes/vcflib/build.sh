e377cf90f7db158a9c98499f9d1da9e4c3a98413
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-04-06 20:12:03 -0500

    Rebuild vcflib

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -4,7 +4,7 @@ set -ex
 cp -rf "${RECIPE_DIR}/vcflib.pc.in" "${SRC_DIR}"
 
 export M4="${BUILD_PREFIX}/bin/m4"
-#export PATH="$(which zig):${PATH}"
+export PATH="$(which zig):${PATH}"
 
 export INCLUDES="-I${PREFIX}/include -I. -Ihtslib -Itabixpp -Iwfa2 -I\$(INC_DIR)"
 export LIBPATH="-L${PREFIX}/lib -L. -Lhtslib -Ltabixpp -Lwfa2"
@@ -31,7 +31,7 @@ else
 fi
 
 cmake -S . -B build \
-	-DZIG=OFF -DOPENMP=ON \
+	-DZIG=ON -DOPENMP=ON \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_SHARED_LIBS=ON \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
