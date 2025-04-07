6efaa744a2a20a2e9fe6eef805cabd8d2561a800
Erin McAuley <erin@fulcrumgenomics.com>
2025-04-07 09:53:54 -0400

    incorporate reviewer comments

--- recipes/hisat-3n/build.sh
+++ recipes/hisat-3n/build.sh
@@ -8,11 +8,15 @@ make -j ${CPU_COUNT} \
     CPP="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
 # copy binaries and python scripts
+# TODO: can hisat-3n depend on hisat2 in the meta.yaml?
 mkdir -p "${PREFIX}/bin"
 for i in \
-    hisat-3n-build \
     hisat-3n \
+    hisat-3n-build \
     hisat-3n-table \
+    hisat2-build \
+    hisat2-build-l \
+    hisat2-build-s \
     hisat2-align-l \
     hisat2-align-s
 do
