637b1672624af6864ca401b497d09ef0a04d88f5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-08 15:46:13 -0500

    Update build.sh

--- recipes/reduce/build.sh
+++ recipes/reduce/build.sh
@@ -6,8 +6,7 @@ else
   export CONFIG_ARGS=""
 fi
 
-cmake -S . -B build "${CMAKE_ARGS}" \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_BUILD_TYPE=Release \
   "${CONFIG_ARGS}"
 cmake --build build/ --target install -j "${CPU_COUNT}"
