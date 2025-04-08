ec102510a9104d04bc2279a333c217be779167c2
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-08 15:34:06 -0500

    Update build.sh

--- recipes/reduce/build.sh
+++ recipes/reduce/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
-cmake ${SRC_DIR} ${CMAKE_ARGS}
-make
-make install
+if [[ `uname` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build "${CMAKE_ARGS}" \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  "${CONFIG_ARGS}"
+cmake --build build/ --target install -j "${CPU_COUNT}"
