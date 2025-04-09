8d92a116fe49c5755708e6aa50bfaa8f0b90778f
ryanjameskennedy <ryanjameskennedy@icloud.com>
2025-04-09 14:02:26 +0200

    RM old code from build.sh

--- recipes/virulencefinder/build.sh
+++ recipes/virulencefinder/build.sh
@@ -2,9 +2,6 @@
 
 mkdir -p ${PREFIX}/bin
 
-chmod +x virulencefinder.py
-cp virulencefinder.py ${PREFIX}/bin/virulencefinder.py
-
 # copy script to download database
 chmod +x ${RECIPE_DIR}/download-virulence-db.sh
 cp ${RECIPE_DIR}/download-virulence-db.sh ${PREFIX}/bin/download-virulence-db.sh
