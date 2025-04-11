bfb9ae7155a196af1c97fbff1964fd4d166d2577
Michael L Heuer <heuermh@acm.org>
2025-04-11 13:59:34 -0500

    Add eider recipe (#55279)

---
+++ recipes/eider/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BINARY_HOME=$PREFIX/bin
+PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+mkdir -p $BINARY_HOME
+mkdir -p $PACKAGE_HOME
+
+cp -R * $PACKAGE_HOME
+
+ln -s $PACKAGE_HOME/bin/eider $BINARY_HOME
