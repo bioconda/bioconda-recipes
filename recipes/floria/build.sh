64d0cf2d373c0ee304a65553c9902620a45ec083
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-13 18:52:43 -0500

    Update build.sh

--- recipes/floria/build.sh
+++ recipes/floria/build.sh
@@ -3,6 +3,10 @@
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+if [[ `uname` == "Darwin" ]]; then
+  export CFLAGS="${CFLAGS} -O3 -Wno-int-conversion -Wno-implicit-function-declaration"
+fi
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
