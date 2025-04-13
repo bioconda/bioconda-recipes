9349386a4b8f3edbfcd0ee98cd53d4981cb77e62
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-13 17:58:12 -0500

    Update build.sh

--- recipes/smoothxg/build.sh
+++ recipes/smoothxg/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export CPATH=${PREFIX}/include
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-sed -i 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
-sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
-sed -i 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
-cmake -H. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
-cmake --build build --verbose
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+
 mkdir -p $PREFIX/bin
-mv bin/* $PREFIX/bin
+
+sed -i.bak 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
+sed -i.bak 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
+sed -i.bak 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
+rm -rf deps/spoa/*.bak
+rm -rf deps/abPOA/*.bak
+
+cmake -H. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM="3.5" -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
+cmake --build build -j "${CPU_COUNT}"
+install -v -m 0755 bin/* "${PREFIX}/bin"
