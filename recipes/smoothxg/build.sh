b7e480199d58db4d56390794ecd0b78fc8747ccb
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-13 18:26:42 -0500

    Update build.sh

--- recipes/smoothxg/build.sh
+++ recipes/smoothxg/build.sh
@@ -2,15 +2,21 @@
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
-mkdir -p $PREFIX/bin
+mkdir -p "${PREFIX}/bin"
 
-sed -i.bak 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
-sed -i.bak 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
-sed -i.bak 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
+sed -i.bak -e 's/-msse4.1/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
+sed -i.bak -e 's/-march=native/-march=sandybridge -Ofast/g' deps/spoa/CMakeLists.txt
+sed -i.bak -e 's/-march=native/-march=sandybridge -Ofast/g' deps/abPOA/CMakeLists.txt
+sed -i.bak -e 's|VERSION 3.16 FATAL_ERROR|VERSION 3.5|' CMakeLists.txt
 rm -rf deps/spoa/*.bak
 rm -rf deps/abPOA/*.bak
+rm -rf *.bak
 
-cmake -H. -Bbuild -DCMAKE_POLICY_VERSION_MINIMUM="3.5" -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS='-march=sandybridge -Ofast'
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Generic \
+  -DCMAKE_CXX_COMPILER="${CXX}" \
+  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -DEXTRA_FLAGS="-march=sandybridge -Ofast"
 cmake --build build -j "${CPU_COUNT}"
 install -v -m 0755 bin/* "${PREFIX}/bin"
