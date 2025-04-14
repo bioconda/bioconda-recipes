a99aa8ae227b655bde7ec7d5ee987d9215e0f270
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-14 18:58:11 -0500

    Update build.sh

--- recipes/mpboot/build.sh
+++ recipes/mpboot/build.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 set -ex
 
-export CFLAGS="${CFLAGS} -w"
-export CPPFLAGS="${CPPFLAGS} -w"
-export CXXFLAGS="${CXXFLAGS} -w"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wno-deprecated-declarations -Wno-ignored-attributes -Wno-unused-result -Wno-deprecated"
+
+mkdir -p "${PREFIX}/bin"
 
 # On x86 use -DIQTREE_FLAGS=avx, on arm use -DIQTREE_FLAGS=sse4
 if [[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]]; then
-    # DCMAKE_ARGS+=(-DIQTREE_FLAGS=sse4)
+    DCMAKE_ARGS+=(-DIQTREE_FLAGS=omp)
     EXE_NAME=mpboot
 elif [[ "$(uname -m)" == "x86_64" ]]; then
-    DCMAKE_ARGS+=(-DIQTREE_FLAGS=avx)
+    DCMAKE_ARGS+=(-DIQTREE_FLAGS=avx omp)
     EXE_NAME=mpboot-avx
 else
     echo "Unsupported architecture: $(uname -m)"
     exit 1
 fi
 
-cmake -S . -B . \
-    -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_C_FLAGS="${CFLAGS}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+sed -i.bak -e 's|VERSION 2.4.4|VERSION 3.5|' zlib-1.2.7/CMakeLists.txt
+sed -i.bak -e 's|VERSION 2.8|VERSION 3.5|' CMakeLists.txt
+
+rm -rf *.bak
+rm -rf zlib-1.2.7/*.bak
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_C_FLAGS="${CFLAGS}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${DCMAKE_ARGS[@]}" \
 	"${CONFIG_ARGS}"
 
@@ -29,13 +43,12 @@ cmake -S . -B . \
 # If CPU_COUNT is 4, we are on CircleCI's arm.large VM
 JOBS=${CPU_COUNT}
 if [[ "$(uname -m)" == "aarch64" ]] && [[ "${CPU_COUNT}" -lt 4 ]]; then
-	JOBS=1 # CircleCI's arm.medium VM runs out of memory with higher values
+	JOBS=1  # CircleCI's arm.medium VM runs out of memory with higher values
 fi
 
-
-VERBOSE=1 make -j "${JOBS}" 
+cd build
+VERBOSE=1 make -j"${JOBS}"
+cd ..
 
 # install
-mkdir -p "${PREFIX}/bin"
-cp "${SRC_DIR}/${EXE_NAME}" "${PREFIX}/bin/mpboot"
-chmod 0755 "${PREFIX}/bin/${EXE_NAME}"
+install -v -m 0755 "${SRC_DIR}/${EXE_NAME}" "${PREFIX}/bin"
