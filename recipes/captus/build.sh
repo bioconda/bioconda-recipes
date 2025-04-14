99ab7d361c4cb72cc20e34bc1bac6cbb7cd7325f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-13 19:41:13 -0500

    Delete recipes/captus/build.sh

--- recipes/captus/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ `uname -s` == "Linux" ]];
-then
-    ln -sf "${PREFIX}/lib/libbz2.so.1.0" "${PREFIX}/lib/libbz2.so.1"
-fi
-
-$PYTHON -m pip install . --no-build-isolation --no-cache-dir --no-build-isolation -vvv
