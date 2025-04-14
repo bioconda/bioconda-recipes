4328f23b4138c1aa9f3c457a2e135abbf1a87564
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-04-14 13:50:56 +0200

    Simplify recipe

--- recipes/hictkpy/build.sh
+++ recipes/hictkpy/build.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
-CMAKE_BUILD_PARALLEL_LEVEL=${CPU_COUNT}
+export CONAN_NON_INTERACTIVE=1
+
+export CMAKE_BUILD_PARALLEL_LEVEL="${CPU_COUNT}"
 
 scratch=$(mktemp -d)
-export CONAN_HOME="$scratch/conan"
+export CONAN_HOME="${scratch}/conan"
 
 # shellcheck disable=SC2064
-trap "rm -rf '$scratch'" EXIT
+trap "rm -rf '${scratch}'" EXIT
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  if [[ "$OSX_ARCH" == 'x86_64' ]]; then
-    # https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk
-    CFLAGS+=' -D_LIBCPP_DISABLE_AVAILABILITY'
-    CXXFLAGS+=' -D_LIBCPP_DISABLE_AVAILABILITY'
-  fi
-  CMAKE_ARGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
-  conan_profile='apple-clang'
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ "${OSTYPE}" =~ .*darwin.* ]]; then
+  # https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
 else
-  # Workaround missing LLVMgold.so on Linux
-  CMAKE_ARGS+=' -DCMAKE_LINKER_TYPE=LLD'
-  CMAKE_ARGS+=" -DCMAKE_TOOLCHAIN_FILE=${RECIPE_DIR}/cross-linux.cmake"
-  conan_profile='clang'
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
-
-# Remember to update these profiles when bioconda's compiler toolchains are updated
-mkdir -p "$CONAN_HOME/profiles/"
-ln -s "${RECIPE_DIR}/conan_profiles/$conan_profile" "$CONAN_HOME/profiles/default"
 
 # Remove unnecessary dependencies from conanfile.py
 patch conanfile.py < "${RECIPE_DIR}/conanfile.py.patch"
 
+conan profile detect
+
 # Build hictkpy as a shared library
-patch pyproject.toml < "${RECIPE_DIR}/pyproject.toml.patch"
+HICTKPY_BUILD_SHARED_LIBS=ON
 
 # See https://github.com/conda-forge/conda-forge.github.io/pull/2321
-Python_INCLUDE_DIR="$("$PYTHON" -c 'import sysconfig; print(sysconfig.get_path("include"))')"
-Python_NumPy_INCLUDE_DIR="$("$PYTHON" -c 'import numpy; print(numpy.get_include())')"
+Python_INCLUDE_DIR="$("${PYTHON}" -c 'import sysconfig; print(sysconfig.get_path("include"))')"
+Python_NumPy_INCLUDE_DIR="$("${PYTHON}" -c 'import numpy; print(numpy.get_include())')"
 
 CMAKE_ARGS+=" -DPython_EXECUTABLE:PATH=${PYTHON}"
 CMAKE_ARGS+=" -DPython_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
@@ -44,12 +38,12 @@ CMAKE_ARGS+=" -DPython3_EXECUTABLE:PATH=${PYTHON}"
 CMAKE_ARGS+=" -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
 CMAKE_ARGS+=" -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
 
-echo "CFLAGS='$CFLAGS'"
-echo "CXXFLAGS='$CXXFLAGS'"
-echo "CMAKE_ARGS='$CMAKE_ARGS'"
+echo "CFLAGS='${CFLAGS}'"
+echo "CXXFLAGS='${CXXFLAGS}'"
+echo "CMAKE_ARGS='${CMAKE_ARGS}'"
 
-export CMAKE_ARGS CMAKE_BUILD_PARALLEL_LEVEL CFLAGS CXXFLAGS
+export CMAKE_ARGS CMAKE_BUILD_PARALLEL_LEVEL CFLAGS CXXFLAGS HICTKPY_BUILD_SHARED_LIBS
 
 SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION" \
-"$PYTHON" -m pip install "$SRC_DIR" -vv
+"${PYTHON}" -m pip install "$SRC_DIR" -vv
 
