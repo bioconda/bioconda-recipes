e26f40e0479152910bfedd00740be6c7120adfdb
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-04-13 23:43:43 -0500

    tandemx: add arm64 build

--- recipes/xtandem/build.sh
+++ recipes/xtandem/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 set -xe
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
+
+dos2unix src/*
 
 cd src/
 
 # without -fpermissive, this fails with GCC7 due to bad style
-make -j ${CPU_COUNT} CXX="${CXX}" CXXFLAGS+='-fpermissive' LDFLAGS+='-lpthread -lm -lexpat'
+make CXX="${CXX}" \
+	CXXFLAGS+="${CXXFLAGS} -Wno-conversion-null -Wno-unused-result -Wno-register -Wno-aggressive-loop-optimizations" \
+	LDFLAGS+='${LDFLAGS} -pthread -lm -lexpat' \
+	-j"${CPU_COUNT}"
 
-mkdir -p "${PREFIX}/bin"
-cp ../bin/tandem.exe "${PREFIX}/bin/xtandem"
+install -v -m 0755 ../bin/tandem.exe "${PREFIX}/bin"
+ln -sf "${PREFIX}/bin/tandem.exe" "${PREFIX}/bin/tandem"
