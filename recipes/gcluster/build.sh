68ef82de60cbaec58cf23565063aedf918312d76
xiangyang1984 <lixiangyang@fudan.edu.cn>
2025-04-14 08:44:28 +0800

    add update to gcluster-v2.0.7

--- recipes/gcluster/build.sh
+++ recipes/gcluster/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x -e
 
-RM_DIR=${PREFIX}/share/Gcluster
+RM_DIR=${PREFIX}/share/gcluster
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${RM_DIR}
@@ -18,9 +18,9 @@ BLASTP_P="/usr/bin/blastp"
 MAKEBLASTDB_P="/usr/bin/makeblastdb"
 
 #revise the absolute path for three programs within "Gcluster/Gcluster.pl"
-sed -i -e "s~${MCL_P}~${MCL_DIR}~g" ${RM_DIR}/Gcluster.pl
-sed -i -e "s~${BLASTP_P}~${BLASTP_DIR}~g" ${RM_DIR}/Gcluster.pl
-sed -i -e "s~$MAKEBLASTDB_P~${MAKEBLASTDB_DIR}~g" ${RM_DIR}/Gcluster.pl
+sed -i -e "s~${MCL_P}~${MCL_DIR}~g" ${RM_DIR}/gcluster.pl
+sed -i -e "s~${BLASTP_P}~${BLASTP_DIR}~g" ${RM_DIR}/gcluster.pl
+sed -i -e "s~$MAKEBLASTDB_P~${MAKEBLASTDB_DIR}~g" ${RM_DIR}/gcluster.pl
 
 #revise the absolute path for three programs within "Gcluster/interested_gene_generation.pl"
 sed -i -e "s~${BLASTP_P}~${BLASTP_DIR}~g" ${RM_DIR}/interested_gene_generation.pl
@@ -30,23 +30,17 @@ sed -i -e "s~$MAKEBLASTDB_P~${MAKEBLASTDB_DIR}~g" ${RM_DIR}/interested_gene_gene
 # cp -r ${RM_DIR}/test_data ${RECIPE_DIR}/test_data
 
 # Set a executable file for Gcluster.pl
-cat <<END >>${PREFIX}/bin/Gcluster.pl
+cat <<END >>${PREFIX}/bin/gcluster
 #!/bin/bash
-perl ${RM_DIR}/Gcluster.pl \$@
+perl ${RM_DIR}/gcluster.pl \$@
 END
 
+
 # Set a executable file for interested_gene_generation.pl
-cat <<END >>${PREFIX}/bin/interested_gene_generation.pl
+cat <<END >>${PREFIX}/bin/interested_gene_generation
 #!/bin/bash
 perl ${RM_DIR}/interested_gene_generation.pl \$@
 END
 
-# Set a executable file for test.pl
-cat <<END >>${PREFIX}/bin/test.pl
-#!/bin/bash
-perl ${RM_DIR}/test.pl
-END
-
-chmod a+x ${PREFIX}/bin/Gcluster.pl
-chmod a+x ${PREFIX}/bin/interested_gene_generation.pl
-chmod a+x ${PREFIX}/bin/test.pl
+chmod a+x ${PREFIX}/bin/gcluster
+chmod a+x ${PREFIX}/bin/interested_gene_generation
