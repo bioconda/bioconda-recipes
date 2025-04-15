e779e35d36ae7e1bf11dc0c5fc78bd2a9501f6c1
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-14 20:51:43 -0500

    Update build.sh

--- recipes/mpboot/build.sh
+++ recipes/mpboot/build.sh
@@ -10,10 +10,10 @@ mkdir -p "${PREFIX}/bin"
 
 # On x86 use -DIQTREE_FLAGS=avx, on arm use -DIQTREE_FLAGS=sse4
 if [[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]]; then
-    DCMAKE_ARGS+=(-DIQTREE_FLAGS=omp)
+    DCMAKE_ARGS+=(-DIQTREE_FLAGS="omp")
     EXE_NAME=mpboot
 elif [[ "$(uname -m)" == "x86_64" ]]; then
-    DCMAKE_ARGS+=(-DIQTREE_FLAGS=avx omp)
+    DCMAKE_ARGS+=(-DIQTREE_FLAGS="avx omp")
     EXE_NAME=mpboot-avx
 else
     echo "Unsupported architecture: $(uname -m)"
@@ -29,7 +29,6 @@ fi
 
 sed -i.bak -e 's|VERSION 2.4.4|VERSION 3.5|' zlib-1.2.7/CMakeLists.txt
 sed -i.bak -e 's|VERSION 2.8|VERSION 3.5|' CMakeLists.txt
-
 rm -rf *.bak
 rm -rf zlib-1.2.7/*.bak
 
@@ -37,19 +36,16 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_C_FLAGS="${CFLAGS}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
-	"${DCMAKE_ARGS[@]}" \
 	"${CONFIG_ARGS}"
 
 # Detect if we are running on CircleCI's arm.medium VM
 # If CPU_COUNT is 4, we are on CircleCI's arm.large VM
-JOBS=${CPU_COUNT}
+JOBS="${CPU_COUNT}"
 if [[ "$(uname -m)" == "aarch64" ]] && [[ "${CPU_COUNT}" -lt 4 ]]; then
 	JOBS=1  # CircleCI's arm.medium VM runs out of memory with higher values
 fi
 
-cd build
-VERBOSE=1 make -j"${JOBS}"
-cd ..
+cmake --build build/ -j "${JOBS}" -v
 
 # install
 install -v -m 0755 "${SRC_DIR}/build/${EXE_NAME}" "${PREFIX}/bin"
