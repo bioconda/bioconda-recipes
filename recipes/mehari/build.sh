dc01936fc8170c0f05153ee3ebbe5c10773313da
Till Hartmann <till.hartmann@bih-charite.de>
2025-04-15 17:16:12 +0200

    add ldd command for debugging purposes

--- recipes/mehari/build.sh
+++ recipes/mehari/build.sh
@@ -2,8 +2,10 @@
 
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
+echo "BINDGEN_EXTRA_CLANG_ARGS=${BINDGEN_EXTRA_CLANG_ARGS}"
 
 export ROCKSDB_LIB_DIR="${PREFIX}/lib/"
 export SNAPPY_LIB_DIR="${PREFIX}/lib/"
+echo "ROCKSDB_LIB_DIR=${ROCKSDB_LIB_DIR}"
 
 cargo install --no-track --locked --root "${PREFIX}" --path .
