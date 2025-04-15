5b39e6c9c5e6b007ab7db5f5fb60c4594b10a3a8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-14 23:15:57 -0500

    Update build.sh

--- recipes/pycoverm/build.sh
+++ recipes/pycoverm/build.sh
@@ -7,5 +7,10 @@ export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+sed -i.bak -e 's|0.45.0|0.49.0|' Cargo.toml
+sed -i.bak -e 's|0.20.3|0.21.2|' Cargo.toml
+rm -rf *.bak
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 $PYTHON -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
