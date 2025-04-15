8f7d810cf754f33abaed3c7fc8a4acc343baa22f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-14 23:24:32 -0500

    Update build.sh

--- recipes/pycoverm/build.sh
+++ recipes/pycoverm/build.sh
@@ -8,7 +8,6 @@ export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 sed -i.bak -e 's|0.45.0|0.49.0|' Cargo.toml
-sed -i.bak -e 's|0.20.3|0.21.2|' Cargo.toml
 rm -rf *.bak
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
