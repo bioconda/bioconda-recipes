32c15d334eb825ea5171ffab50eacffd483a2161
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-04-14 21:10:01 -0500

    pycoverm: build on aarch64

--- recipes/pycoverm/build.sh
+++ recipes/pycoverm/build.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="$CC"
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="$CC"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+$PYTHON -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
