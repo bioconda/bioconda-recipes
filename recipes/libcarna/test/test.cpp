cb4f48252052eaff310f34438915fdd8debe256d
Leonid Kostrykin <void@evoid.de>
2025-04-16 22:33:17 +0200

    Test library linking

--- recipes/libcarna/test/test.cpp
+++ recipes/libcarna/test/test.cpp
@@ -1,5 +1,9 @@
-#include <Carna/Carna.h>
+#include <Carna/base/Node.h>
 
 int main()
 {
+    /* Call code that is implemented in the shared library,
+     * to test the linking of the library.
+     */
+    Carna::base::Node root;
 }
