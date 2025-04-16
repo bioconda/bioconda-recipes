635536737a8332bec50fe9b2407f6752dbbea925
inesbmartins02 <inesbrancomartins@ua.pt>
2025-04-16 19:25:36 +0100

    Add HYMET recipe v1.0.0

--- recipes/hymet/build.sh
+++ recipes/hymet/build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-# Cria o setup.py dinamicamente
-cat > setup.py << EOF
-from setuptools import setup
-setup(
-    name="hymet",
-    version="1.0.0",
-    packages=["hymet"],
-)
-EOF
+# Cria a estrutura mínima de pacote Python
 mkdir -p hymet
 touch hymet/__init__.py
-$PYTHON -m pip install . --no-deps -vv
+
+# Gera o setup.py dinamicamente
+cat > setup.py << EOF
+from setuptools import setup
+
+setup(
+    name="hymet",
+    version="{{ version }}",
+    packages=["hymet"],
+    install_requires=[
+        "pandas",
+        "numpy",
+        "biopython",
+        "tqdm"
+    ],
+)
+EOF
+
+# Instala o pacote
+${PYTHON} -m pip install . --no-deps -vv
