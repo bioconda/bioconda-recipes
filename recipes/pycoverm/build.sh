6d1f9b069daa54ef52b5a574e87b3dd2991eeb59
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-15 19:44:10 -0500

    Update build.sh

--- recipes/pycoverm/build.sh
+++ recipes/pycoverm/build.sh
@@ -8,8 +8,10 @@ export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-sed -i.bak -e 's|0.45.0|0.47.0|' Cargo.toml
-rm -rf *.bak
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  sed -i.bak -e 's|0.45.0|0.46.0|' Cargo.toml
+  rm -rf *.bak
+fi
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 RUST_BACKTRACE=1
