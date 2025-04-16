6263d7dcffb17232f1d6698527eea0158ab26f9d
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-04-16 05:07:13 -0500

    aster: add arm64 build

--- recipes/aster/build.sh
+++ recipes/aster/build.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+export CXXFLAGS="${CXXFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 for CHANGE in "activate" "deactivate"
 do
-    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+	mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+	cp -rf "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
-if [ `uname -m` != "aarch64" ]; then
-	sed -i.bak1 's/-march=native/-march=x86-64 -mtune=generic/g' makefile
+
+sed -i.bak 's|-O2|-O3|' makefile
+rm -rf *.bak
+
+if [[ `uname -m` != "aarch64" ]]; then
+	sed -i.bak 's/-march=native/-march=x86-64 -mtune=generic/g' makefile
+	rm -rf *.bak
 fi
-if [ "$(uname)" == "Darwin" ];
-then
-    sed -i.bak2 's/g++/${CXX}/g' makefile
-    make mac
+
+if [[ "$(uname)" == "Darwin" ]]; then
+	sed -i.bak2 's/g++/${CXX}/g' makefile
+	rm -rf *.bak
+	make -j"${CPU_COUNT}" mac
 else
-    sed -i.bak2 's/g++/${GXX}/g' makefile
-    make
+	sed -i.bak2 's/g++/${CXX}/g' makefile
+	rm -rf *.bak
+	make -j"${CPU_COUNT}"
 fi
 
-[ ! -d $PREFIX/bin ] && mkdir -p $PREFIX/bin
+[[ ! -d ${PREFIX}/bin ]] && mkdir -p "${PREFIX}/bin"
 
-cp bin/* $PREFIX/bin/
-chmod a+x $PREFIX/bin/*
+install -v -m 0755 bin/* "${PREFIX}/bin"
