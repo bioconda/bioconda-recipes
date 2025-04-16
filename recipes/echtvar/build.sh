46e3eb6a4761c09e8ef014cc5db70f4de43af83e
fellen31 <felixlenner@gmail.com>
2025-04-16 13:17:55 +0200

    try export RUSTC_BOOTSTRAP=1

--- recipes/echtvar/build.sh
+++ recipes/echtvar/build.sh
@@ -10,5 +10,6 @@ export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
+export RUSTC_BOOTSTRAP=1
 export LD=$CC
 C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path . --no-track
