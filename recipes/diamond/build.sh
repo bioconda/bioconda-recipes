949ad9132a01d02f9cea6ca5466c45020c41b527
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-04-16 07:05:20 -0500

    Rebuild diamond recipe

--- recipes/diamond/build.sh
+++ recipes/diamond/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 if [[ `uname` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
@@ -9,12 +12,13 @@ fi
 cmake -S. -B build -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	-DWITH_ZSTD=ON \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3" \
+	-DWITH_ZSTD=ON -DSTATIC_LIBGCC=ON -DSTATIC_LIBSTDC++=ON \
 	-DZSTD_LIBRARY="${PREFIX}/lib/libzstd.a" \
 	-DZSTD_INCLUDE_DIR="${PREFIX}/include" \
+	-DBLAST_INCLUDE_DIR="${PREFIX}/include/ncbi" \
 	-DBLAST_LIBRARY_DIR="${PREFIX}/lib/ncbi-blast+" \
 	-DCMAKE_OSX_DEPLOYMENT_TARGET="" \
 	"${CONFIG_ARGS}"
 
-cmake --build build --target install -v
+cmake --build build --target install
