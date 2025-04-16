9859b2bc0a8ef55d6a950d08bb4c0243acde61ed
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-15 20:44:33 -0500

    Update build.sh

--- recipes/pycoverm/build.sh
+++ recipes/pycoverm/build.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-#export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
-#export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 if [[ "$(uname -m)" == "aarch64" ]]; then
-  sed -i.bak -e 's|0.45.0|0.49.0|' Cargo.toml
+  sed -i.bak -e 's|0.45.0|0.47.0|' Cargo.toml
   rm -rf *.bak
 fi
 
