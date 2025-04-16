019c6be6912eafe7076ae62a03ade17c902c9287
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-16 01:45:46 -0500

    Update build.sh

--- recipes/mehari/build.sh
+++ recipes/mehari/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -xeuo
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration -Wno-int-conversion"
+
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 echo "BINDGEN_EXTRA_CLANG_ARGS=${BINDGEN_EXTRA_CLANG_ARGS}"
 
-export ROCKSDB_LIB_DIR="${PREFIX}/lib/"
-export SNAPPY_LIB_DIR="${PREFIX}/lib/"
+export ROCKSDB_LIB_DIR="${PREFIX}/lib"
+export SNAPPY_LIB_DIR="${PREFIX}/lib"
 echo "ROCKSDB_LIB_DIR=${ROCKSDB_LIB_DIR}"
 
-cargo install --no-track --locked --root "${PREFIX}" --path .
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+RUST_BACKTRACE=1
+cargo install --verbose --no-track --locked --root "${PREFIX}" --path .
