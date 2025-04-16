787d05644cb0bb555ef0d3107afe776eb77ab2d5
xiangyang1984 <lixiangyang@fudan.edu.cn>
2025-04-16 09:19:03 +0800

    Update gcluster to v2.0.7#1

--- recipes/gcluster/build.sh
+++ recipes/gcluster/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -x -e
 
-RM_DIR=${PREFIX}/share/Gcluster
-
-mkdir -p ${PREFIX}/bin
+RM_DIR=${PREFIX}/share/gcluster
 mkdir -p ${RM_DIR}
-cp -r * ${RM_DIR}
+mkdir -p ${PREFIX}/bin
+
+ls -a
+cp -r $SRC_DIR/* ${RM_DIR}
+cp build_env_setup.sh ${RM_DIR}
+cp conda_build.sh ${RM_DIR}
+cp metadata_conda_debug.yaml ${RM_DIR}
+
 
 MCL_DIR="${PREFIX}/bin/mcl"
 BLASTP_DIR="${PREFIX}/bin/blastp"
@@ -18,9 +23,9 @@ BLASTP_P="/usr/bin/blastp"
 MAKEBLASTDB_P="/usr/bin/makeblastdb"
 
 #revise the absolute path for three programs within "Gcluster/Gcluster.pl"
-sed -i -e "s~${MCL_P}~${MCL_DIR}~g" ${RM_DIR}/Gcluster.pl
-sed -i -e "s~${BLASTP_P}~${BLASTP_DIR}~g" ${RM_DIR}/Gcluster.pl
-sed -i -e "s~$MAKEBLASTDB_P~${MAKEBLASTDB_DIR}~g" ${RM_DIR}/Gcluster.pl
+sed -i -e "s~${MCL_P}~${MCL_DIR}~g" ${RM_DIR}/gcluster.pl
+sed -i -e "s~${BLASTP_P}~${BLASTP_DIR}~g" ${RM_DIR}/gcluster.pl
+sed -i -e "s~$MAKEBLASTDB_P~${MAKEBLASTDB_DIR}~g" ${RM_DIR}/gcluster.pl
 
 #revise the absolute path for three programs within "Gcluster/interested_gene_generation.pl"
 sed -i -e "s~${BLASTP_P}~${BLASTP_DIR}~g" ${RM_DIR}/interested_gene_generation.pl
@@ -30,23 +35,17 @@ sed -i -e "s~$MAKEBLASTDB_P~${MAKEBLASTDB_DIR}~g" ${RM_DIR}/interested_gene_gene
 # cp -r ${RM_DIR}/test_data ${RECIPE_DIR}/test_data
 
 # Set a executable file for Gcluster.pl
-cat <<END >>${PREFIX}/bin/Gcluster.pl
+cat <<END >>${PREFIX}/bin/gcluster
 #!/bin/bash
-perl ${RM_DIR}/Gcluster.pl \$@
+perl ${RM_DIR}/gcluster.pl \$@
 END
 
+
 # Set a executable file for interested_gene_generation.pl
-cat <<END >>${PREFIX}/bin/interested_gene_generation.pl
+cat <<END >>${PREFIX}/bin/interested_gene_generation
 #!/bin/bash
 perl ${RM_DIR}/interested_gene_generation.pl \$@
 END
 
-# Set a executable file for test.pl
-cat <<END >>${PREFIX}/bin/test.pl
-#!/bin/bash
-perl ${RM_DIR}/test.pl
-END
-
-chmod a+x ${PREFIX}/bin/Gcluster.pl
-chmod a+x ${PREFIX}/bin/interested_gene_generation.pl
-chmod a+x ${PREFIX}/bin/test.pl
+chmod a+x ${PREFIX}/bin/gcluster
+chmod a+x ${PREFIX}/bin/interested_gene_generation
