2c4015b31c8184851ed1489872347b54814b7a20
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-04-16 14:24:54 +0200

    Update tests

--- recipes/stripepy-hic/run_test.sh
+++ recipes/stripepy-hic/run_test.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
+
+zenodo_id='14921616'
+
 # Download test datasets
 mkdir -p test/data
 
-curl -L 'https://zenodo.org/records/14517632/files/4DNFI9GMP2J8.stripepy.mcool?download=1' -o test/data/4DNFI9GMP2J8.mcool
-curl -L 'https://zenodo.org/records/14517632/files/results_4DNFI9GMP2J8_v1.hdf5?download=1' -o test/data/results_4DNFI9GMP2J8_v1.hdf5
+curl -L "https://zenodo.org/records/${zenodo_id}/files/4DNFI9GMP2J8.stripepy.mcool?download=1" -o test/data/4DNFI9GMP2J8.mcool
+curl -L "https://zenodo.org/records/${zenodo_id}/files/results_4DNFI9GMP2J8_v1.hdf5?download=1" -o test/data/results_4DNFI9GMP2J8_v1.hdf5
+curl -L "https://zenodo.org/records/${zenodo_id}/files/results_4DNFI9GMP2J8_v2.hdf5?download=1" -o test/data/results_4DNFI9GMP2J8_v2.hdf5
+curl -L "https://zenodo.org/records/${zenodo_id}/files/results_4DNFI9GMP2J8_v3.hdf5?download=1" -o test/data/results_4DNFI9GMP2J8_v3.hdf5
+curl -L "https://zenodo.org/records/${zenodo_id}/files/stripepy-call-result-tables.tar.xz?download=1" -o test/data/stripepy-call-result-tables.tar.xz
 
 # Checksum datasets
-echo 'a17d08460c03cf6c926e2ca5743e4888  test/data/4DNFI9GMP2J8.mcool' > checksums.md5
-echo '632b2a7a6e5c1a24dc3635710ed68a80  test/data/results_4DNFI9GMP2J8_v1.hdf5' >> checksums.md5
+cat << EOF > checksums.md5
+a17d08460c03cf6c926e2ca5743e4888  test/data/4DNFI9GMP2J8.mcool
+03bca8d430191aaf3c90a4bc22a8c579  test/data/results_4DNFI9GMP2J8_v1.hdf5
+dd14a2f69b337c40727d414d85e2f0a4  test/data/results_4DNFI9GMP2J8_v2.hdf5
+0e618f61f35579968105e84bf50a2598  test/data/results_4DNFI9GMP2J8_v3.hdf5
+52c4154c9e974a3130b3ecaa7b729827  test/data/stripepy-call-result-tables.tar.xz
+EOF
 
 md5sum -c checksums.md5
 
@@ -17,5 +30,5 @@ stripepy --help
 stripepy --version
 
 # Run automated test suites
-"$PYTHON" -m pytest test/ -m unit -v --disable-pytest-warnings
-"$PYTHON" -m pytest test/ -m end2end -v -k 'not TestStripePyPlot' --disable-pytest-warnings
+"${PYTHON}" -m pytest test/ -m unit -v --disable-pytest-warnings
+"${PYTHON}" -m pytest test/ -m end2end -v -k 'not TestStripePyPlot' --disable-pytest-warnings
