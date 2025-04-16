c5aa707e0d2f6b15112e4ad206e74d18b9fbb554
Xiangyang1984 <58356520+Xiangyang1984@users.noreply.github.com>
2025-04-16 10:18:16 +0800

    Update build.sh

--- recipes/pneumo-typer/build.sh
+++ recipes/pneumo-typer/build.sh
@@ -5,8 +5,8 @@ RM_DIR=${PREFIX}/share/pneumo-typer
 mkdir -p ${RM_DIR}
 mkdir -p ${PREFIX}/bin
 ls -a
-# 关键修改：仅复制pneumo-typer-2.0.1目录下的内容到目标路径
-cp -r $SRC_DIR/pneumo-typer-2.0.1/* ${RM_DIR}/
+# 关键修改：仅复制pneumo-typer-v2.0.1目录下的内容到目标路径
+cp -r $SRC_DIR/pneumo-typer-v2.0.1/* ${RM_DIR}/
 
 # 复制其他必要的构建文件（如果这些文件在顶层目录）
 cp $SRC_DIR/build_env_setup.sh ${RM_DIR}
