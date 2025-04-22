972e78b12adcb32a4041baea2494893eecf3a9ac
Jason Shao <Jason.Shao@dal.ca>
2025-04-22 20:01:36 -0300

    fix: updated checksum, added no-deps for ete3 install with pip

--- recipes/eukfinder/build.sh
+++ recipes/eukfinder/build.sh
@@ -9,4 +9,4 @@ chmod +x $PREFIX/bin/plast
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
-$PYTHON -m pip install ete3==3.1.3
+$PYTHON -m pip install ete3==3.1.3 --no-deps
