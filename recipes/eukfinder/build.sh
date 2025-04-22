a074ba99db7c0971c4e01fb8316bd8c3f2b17422
Jason Shao <Jason.Shao@dal.ca>
2025-04-22 19:15:23 -0300

    fix: fixed catastrophic formatting error related to pip

--- recipes/eukfinder/build.sh
+++ recipes/eukfinder/build.sh
@@ -8,3 +8,5 @@ cp build/bin/plast $PREFIX/bin/
 chmod +x $PREFIX/bin/plast
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
+$PYTHON -m pip install ete3==3.1.3
