0918aa2b4e2123d1ab5f91bda89ab6b38cfe4e89
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-21 21:53:41 -0500

    Update build.sh

--- recipes/fastk/build.sh
+++ recipes/fastk/build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 export LDFLAGS="${LDFLAGS} -L$SRC_DIR/HTSLIB -L$PREFIX/lib"
-export CFLAGS="${CFLAGS} -I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$SRC_DIR/HTSLIB -L$PREFIX/lib -Wno-unused-result -Wno-format -Wno-implicit-function-declaration"
+export CFLAGS="${CFLAGS} -O3 -I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -I$PREFIX/include -L$SRC_DIR/HTSLIB -L$PREFIX/lib -Wno-unused-result -Wno-format -Wno-implicit-function-declaration"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I$SRC_DIR -I$SRC_DIR/HTSLIB -I$SRC_DIR/LIBDEFLATE -I$SRC_DIR/LIBDEFLATE/common -L$SRC_DIR/HTSLIB -L$PREFIX/lib"
 
 # build HTSLIB
