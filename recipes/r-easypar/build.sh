6eec690bf1a3e215fb80d9ffbe64352def9ac032
Lucrezia Valeriani <valerianilucrezia11@gmail.com>
2025-04-22 13:11:51 +0200

    Update recipes/r-easypar/build.sh
    Co-authored-by: Mervin Fansler <mmfansler@gmail.com>

--- recipes/r-easypar/build.sh
+++ recipes/r-easypar/build.sh
@@ -2,9 +2,3 @@
 export DISABLE_AUTOBREW=1
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
-
-# Add more build steps here, if they are necessary.
-
-# See
-# https://docs.conda.io/projects/conda-build
-# for a list of environment variables that are set during the build process.
