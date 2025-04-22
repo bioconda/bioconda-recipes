97080f73025cafeb25889622c6c240def28bfe27
swalia <swalia@AD.UCSD.EDU@peregrine.ucsd.edu>
2025-04-22 11:01:12 -0700

    adding panman conda recipe

---
+++ recipes/panman/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ---- Exit on error ----
+set -euo pipefail
+
+# ---- Debugging: List TBB include directory ----
+echo "Listing contents of $PREFIX/include/tbb:"
+ls -l "$PREFIX/include/tbb"
+
+
+# ---- Build and install ----
+cp -rf ${RECIPE_DIR}/CMakeLists.txt $SRC_DIR/CMakeLists.txt
+mkdir -p build
+cd build
+
+cmake -S $SRC_DIR -B . \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_PREFIX_PATH="${PREFIX}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DTBB_DIR="${PREFIX}/lib/cmake/TBB" \
+    ${CMAKE_ARGS} -Wno-dev
+
+make -j$(nproc)
+make install
