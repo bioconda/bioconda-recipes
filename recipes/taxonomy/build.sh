092712d92537c1d933a4c5931f4b838a0a4082fb
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-21 20:22:11 -0500

    Update build.sh

--- recipes/taxonomy/build.sh
+++ recipes/taxonomy/build.sh
@@ -2,17 +2,11 @@
 
 set -euo
 
-# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
-# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="${BUILD_PREFIX}/.cargo"
-
-if [ `uname` == Darwin ]; then
-	export HOME=`mktemp -d`
-	export PATH="$CARGO_HOME/bin:$PATH"
-fi
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-maturin build -b cffi --interpreter "${PYTHON}" --release --strip #--cargo-extra-args="--features=python"
+maturin build -b cffi --interpreter "${PYTHON}" --release --strip --frozen -f
 
-${PYTHON} -m pip install . --no-deps --no-build-isolation -vvv
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
