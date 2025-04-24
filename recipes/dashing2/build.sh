941b0f79d0270fb4a3612d0308ea43ee7b40829c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-24 17:34:10 -0500

    Update build.sh

--- recipes/dashing2/build.sh
+++ recipes/dashing2/build.sh
@@ -4,8 +4,9 @@ mkdir -p "${PREFIX}/bin"
 
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
+make CC="${CC}" CXX="${CXX}" CXXSTD="${CXXFLAGS} -std=c++14" -j"${CPU_COUNT}"
 
 install -v -m 0755 dashing2 dashing2-64 dashing2-tmp "${PREFIX}/bin"
