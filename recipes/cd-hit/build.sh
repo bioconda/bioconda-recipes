38f772b5e10e205eed6ebce9be2b349883f1b9d5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-24 16:53:43 -0500

    clean up recipe

--- recipes/cd-hit/build.sh
+++ recipes/cd-hit/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p "${PREFIX}/bin"
 
-export CFLAGS="${CFLAGS} -O3"
-export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -I$PREFIX/include"
-export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 export CPATH="${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
+export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -I$PREFIX/include"
 
 sed -i.bak 's/^CC =$//g' Makefile
 sed -i.bak 's/^#LDFLAGS.*//g' Makefile
@@ -24,18 +24,6 @@ rm -rf psi-cd-hit/*.bak
 rm -rf usecases/Miseq-16S/*.bak
 rm -rf usecases/miRNA-seq/*.bak
 
-#if [[ "$OSTYPE" == "darwin"* ]]; then
-  #Lines below is commented out until fix provided for OPENMP support on OS X for this program
-  #CCFLAGS="$CCFLAGS -Wl,-rpath ${PREFIX}/lib -L${PREFIX}/lib -I${PREFIX}/include -fopenmp"
-  #sed -i.bak 's/CCFLAGS = -fopenmp/CCFLAGS += -fopenmp/g' Makefile
-  #rm -rf *.bak
-  #LDFLAGS="$LDFLAGS -stdlib=libc++"
-
-  #make CC="${CXX}" openmp=no MAX_SEQ=1000000 -j"${CPU_COUNT}"
-#else
-  #make CC="${GXX}" MAX_SEQ=1000000 -j"${CPU_COUNT}"
-#fi
-
 make CC="${CXX}" MAX_SEQ=1000000 -j"${CPU_COUNT}"
 
-make install PREFIX="$PREFIX/bin"
+make install PREFIX="${PREFIX}/bin"
