dfccd1363047fe9e3760d7ea9fed51d63d4b8ad4
riccabolla <bollach@virgilio.it>
2025-04-25 12:15:26 +0200

    Update recipes/pcne/build.sh
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>

--- recipes/pcne/build.sh
+++ recipes/pcne/build.sh
@@ -4,6 +4,3 @@ set -u
 mkdir -p $PREFIX/bin
 install -m 755 bin/pcne $PREFIX/bin/
 install -m 644 bin/PCNE.R $PREFIX/bin/
-echo "Installation script finished."
-echo "Files installed in $PREFIX/bin:"
-ls -l $PREFIX/bin
