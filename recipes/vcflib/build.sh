47a0b36375fb0a1b457d2ce217e47de69ec31c61
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-24 23:50:19 -0500

    Update build.sh

--- recipes/vcflib/build.sh
+++ recipes/vcflib/build.sh
@@ -6,14 +6,12 @@ cp -rf "${RECIPE_DIR}/vcflib.pc.in" "${SRC_DIR}"
 sed -i.bak -e 's|mem.split|mem.splitSequence|' src/zig/samples.zig
 rm -rf src/zig/*.bak
 
-sed -i.bak -e 's|-fPIC|-fPIC -Wno-int-conversion|' CMakeLists.txt
+sed -i.bak -e 's|-fPIC|-fPIC -Wno-int-conversion -Wno-deprecated-declarations -Wno-absolute-value -Wno-unused-comparison|' CMakeLists.txt
 rm -rf *.bak
 
 export M4="${BUILD_PREFIX}/bin/m4"
-#export PATH="$(which zig):${PATH}"
-
 export INCLUDES="-I${PREFIX}/include -I. -Ihtslib -Itabixpp -Iwfa2 -I\$(INC_DIR)"
-export CFLAGS="${CFLAGS} -O3 -Wno-int-conversion"
+export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
@@ -42,33 +40,31 @@ fi
 
 sed -i.bak 's/CFFFLAGS:= -O3/CFFFLAGS=-O3 -D_FILE_OFFSET_BITS=64/' contrib/smithwaterman/Makefile
 sed -i.bak 's/CFLAGS/CXXFLAGS/g' contrib/smithwaterman/Makefile
-
 sed -i.bak 's/$</$< $(LDFLAGS)/g' contrib/smithwaterman/Makefile
 sed -i.bak 's/ld/$(LD)/' contrib/smithwaterman/Makefile
 sed -i.bak 's/gcc/$(CC) $(CFLAGS)/g' contrib/filevercmp/Makefile
 sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/multichoose/Makefile
 sed -i.bak 's/g++/$(CXX) $(CXXFLAGS)/g' contrib/intervaltree/Makefile
 
+rm -rf contrib/filevercmp/*.bak
+rm -rf contrib/multichoose/*.bak
+rm -rf contrib/smithwaterman/*.bak
+rm -rf contrib/intervaltree/*.bak
+
 # MacOSX Build fix: https://github.com/chapmanb/homebrew-cbl/issues/14
 if [[ "${OS}" == "Darwin" ]]; then
 	export LIBPATH="-L${PREFIX}/lib -L. -Lhtslib -Ltabixpp -Lwfa2"
 	export LDFLAGS="${LDFLAGS} -lhts -ltabixpp -pthread -lz -lm -llzma -lbz2 -lcurl -fopenmp -lwfa2"
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER -DWFA_GITMODULE=OFF"
 	sed -i.bak 's/LDFLAGS=-Wl,-s/LDFLAGS=/' contrib/smithwaterman/Makefile
 	sed -i.bak 's/-std=c++0x/-std=c++17 -stdlib=libc++/g' contrib/intervaltree/Makefile
 	rm -rf contrib/smithwaterman/*.bak
 	rm -rf contrib/intervaltree/*.bak
-	rm -rf contrib/filevercmp/*.bak
-	rm -rf contrib/multichoose/*.bak
-	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER -DWFA_GITMODULE=OFF"
 else
 	export LIBPATH="-L${PREFIX}/lib -L. -Lhtslib -Ltabixpp"
 	export LDFLAGS="${LDFLAGS} -lhts -ltabixpp -pthread -lz -lm -llzma -lbz2 -lcurl -fopenmp"
  	export CONFIG_ARGS="-DWFA_GITMODULE=ON"
-	rm -rf contrib/smithwaterman/*.bak
-	rm -rf contrib/intervaltree/*.bak
-	rm -rf contrib/filevercmp/*.bak
-	rm -rf contrib/multichoose/*.bak
 fi
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
@@ -76,6 +72,7 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	-DOPENMP=ON -DPROFILING=ON -DZIG=ON -DBUILD_SHARED_LIBS=ON \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
 cmake --build build --target install -j "${CPU_COUNT}"
