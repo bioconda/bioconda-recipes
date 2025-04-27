456a2d98fa874964e85bfc52c7225c82b80dcaba
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-26 20:24:46 -0500

    try exporting m4 variable

--- recipes/meme/build.sh
+++ recipes/meme/build.sh
@@ -1,29 +1,29 @@
-#!/bin/bash -ex
+#!/bin/bash
+set -ex
 
+export M4="${BUILD_PREFIX}/bin/m4"
 export MEME_ETC_DIR="${PREFIX}/etc"
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
 autoreconf -if
-
-./configure CC="${CC}" \
-	CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include" \
+./configure --prefix="${PREFIX}" CC="${CC}" \
+	CFLAGS="${CFLAGS} -O3" \
 	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
-	--prefix="${PREFIX}" \
-	--enable-build-libxml2 \
-	--enable-build-libxslt
+	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+	--enable-build-libxml2 --enable-build-libxslt
 
+make clean
 make AM_CFLAGS='-DNAN="(0.0/0.0)"' -j"${CPU_COUNT}"
 make install
-make clean
 
 ln -sf ${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}/* ${PREFIX}/bin/
 
 # if building with python3,
 # modify meme-chip script to use python3 version of DREME
-if [ ${PY3K}==1 ]; then
-	sed -i.bak  '994s/dreme/dreme-py3/' ${PREFIX}/bin/meme-chip
-	rm ${PREFIX}/bin/meme-chip.bak
+if [[ ${PY3K}==1 ]]; then
+	sed -i.bak '994s/dreme/dreme-py3/' ${PREFIX}/bin/meme-chip
+	rm -rf ${PREFIX}/bin/meme-chip.bak
 	# Fix for dreme
 	cp -rf scripts/*py3.py ${PREFIX}/lib/${PKG_NAME}-${PKG_VERSION}/python/
 fi
