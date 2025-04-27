88d8114ab6e38e4c2f10d38110aae3e2d962d719
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-27 00:43:16 -0500

    add cargo-bundle-licenses to build

--- recipes/alignoth/build.sh
+++ recipes/alignoth/build.sh
@@ -3,8 +3,9 @@
 export CFLAGS="${CFLAGS} -O3 -fno-rtti"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=1
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
