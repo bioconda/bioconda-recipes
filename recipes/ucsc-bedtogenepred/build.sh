c5382c9a44493e36c3e282db6a11e34a2cfd6e43
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-29 16:05:44 -0500

    Update build.sh

--- recipes/ucsc-bedtogenepred/build.sh
+++ recipes/ucsc-bedtogenepred/build.sh
@@ -2,25 +2,27 @@
 
 set -xe
 
+export MACHTYPE=$(uname -m)
+export BINDIR=$(pwd)/bin
+
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export L="${LDFLAGS}"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include ${LDFLAGS}"
+export COPT="${COPT} ${CFLAGS}"
 export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include ${LDFLAGS}"
 export USE_HIC=0
+
+mkdir -p "${PREFIX}/bin"
+mkdir -p "${BINDIR}"
 
 sed -i.bak -e 's|-ldl -lm -lc|-ldl -lm -lc -lhts -lcurl|' kent/src/inc/common.mk
 rm -rf kent/src/inc/*.bak
 
-mkdir -p "${PREFIX}/bin"
-export MACHTYPE=$(uname -m)
-export BINDIR=$(pwd)/bin
-export L="${LDFLAGS}"
-mkdir -p "${BINDIR}"
-(cd kent/src/lib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
-(cd kent/src/htslib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
-(cd kent/src/jkOwnLib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
-(cd kent/src/hg/lib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
-(cd kent/src/hg/bedToGenePred && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+(cd kent/src/lib && make CC="${CC}" COPT="${COPT}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+(cd kent/src/htslib && make CC="${CC}" COPT="${COPT}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+(cd kent/src/jkOwnLib && make CC="${CC}" COPT="${COPT}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+(cd kent/src/hg/lib && make CC="${CC}" COPT="${COPT}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+(cd kent/src/hg/bedToGenePred && make CC="${CC}" COPT="${COPT}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
 install -v -m 0755 bin/bedToGenePred "${PREFIX}/bin"
