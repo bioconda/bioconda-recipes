3b2423ea9c7d236aac4203c5ed51622abc221849
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-30 15:53:32 -0500

    Update build.sh

--- recipes/kat/build.sh
+++ recipes/kat/build.sh
@@ -1,15 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 set -x -e
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
 mkdir -p ${PREFIX}/bin
 
 #importing matplotlib fails, likely due to X
 sed -i.bak "124d" configure.ac
+sed -i.bak 's|>= '3.5'|>= '3.9'|' configure.ac
+rm -rf *.bak
 
 ./autogen.sh
 export PYTHON_NOVERSION_CHECK="3.7.0"
-./configure --disable-silent-rules --disable-dependency-tracking --prefix=$PREFIX
-make
+./configure --prefix="${PREFIX}" CC="${CC}" CXX="${CXX}" \
+  CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}" \
+  PYTHON="${PYTHON}" --enable-silent-rules --disable-dependency-tracking \
+  --disable-option-checking
+make -j"${CPU_COUNT}"
 make install
 
 # This directory isn't needed and confuses conda
