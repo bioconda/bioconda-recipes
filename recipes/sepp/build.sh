1b04f4107879948885be82ec6721ab9ef2af9228
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-04-30 03:25:00 -0500

    Rebuild sepp recipe

--- recipes/sepp/build.sh
+++ recipes/sepp/build.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 set -ex
-python setup.py config -c
-python setup.py upp -c
+
+${PYTHON} setup.py config -c
+${PYTHON} setup.py upp -c
 
 # ensure SEPP's configuration file is at the correct location ...
 echo "${PREFIX}/share/sepp/sepp" > home.path
 # ensure directory is created ... ($SP_DIR = Python's site-packages location, see https://docs.conda.io/projects/conda-build/en/stable/user-guide/environment-variables.html#environment-variables-set-during-the-build-process)
 mkdir -p $SP_DIR/
 # ... before we copy content into it
-cp -rv home.path $SP_DIR/
+cp -rfv home.path $SP_DIR/
 mkdir -p $PREFIX/share/sepp/sepp
 # ... and holds correct path names
 mv -v sepp-package/sepp/default.main.config $PREFIX/share/sepp/sepp/main.config
 # copy upp config, as it's still needed
-cp ./.sepp/upp.config $PREFIX/share/sepp/sepp/upp.config
+cp -f ./.sepp/upp.config $PREFIX/share/sepp/sepp/upp.config
 
 # replace $PREFIX with /opt/anaconda1anaconda2anaconda3 for later replacement of concrete build PREFIX
 # note: can't apply a patch here, as upp.config is not part of upstream but gets generated during python setup
-if [ x"$(uname)" == x"Linux" ]; then
-	sed -i "s|path=.*/|path=/opt/anaconda1anaconda2anaconda3/bin/|g" $PREFIX/share/sepp/sepp/upp.config
-elif [ x"$(uname)" == x"Darwin" ]; then
+if [[ `uname -s` == "Linux" ]]; then
+	sed -i.bak "s|path=.*/|path=/opt/anaconda1anaconda2anaconda3/bin/|g" $PREFIX/share/sepp/sepp/upp.config
+elif [[ `uname -s` == "Darwin" ]]; then
 	# see https://stackoverflow.com/questions/19456518/error-when-using-sed-with-find-command-on-os-x-invalid-command-code
-	sed -i '' -e "s|path=.*/|path=/opt/anaconda1anaconda2anaconda3/bin/|g" $PREFIX/share/sepp/sepp/upp.config
+	sed -i.bak '' -e "s|path=.*/|path=/opt/anaconda1anaconda2anaconda3/bin/|g" $PREFIX/share/sepp/sepp/upp.config
 fi
 
-$PYTHON -m pip install . --ignore-installed --no-deps -vv
+${PYTHON} -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
 
 # copy bundled binaries, but hmmer which should be provided by conda, into $PREFIX/bin/
 mkdir -p $PREFIX/bin/
@@ -36,7 +37,7 @@ mv -v sepp-package/run-sepp.sh $PREFIX/bin/run-sepp.sh
 
 # copy files for tests to shared conda directory
 mkdir -p $PREFIX/share/sepp/ref/
-cp -v test/unittest/data/q2-fragment-insertion/input_fragments.fasta $PREFIX/share/sepp/ref/
-cp -v test/unittest/data/q2-fragment-insertion/reference_alignment_tiny.fasta $PREFIX/share/sepp/ref/
-cp -v test/unittest/data/q2-fragment-insertion/reference_phylogeny_tiny.nwk $PREFIX/share/sepp/ref/
-cp -v test/unittest/data/q2-fragment-insertion/RAxML_info-reference-gg-raxml-bl.info $PREFIX/share/sepp/ref/
+cp -vf test/unittest/data/q2-fragment-insertion/input_fragments.fasta $PREFIX/share/sepp/ref/
+cp -vf test/unittest/data/q2-fragment-insertion/reference_alignment_tiny.fasta $PREFIX/share/sepp/ref/
+cp -vf test/unittest/data/q2-fragment-insertion/reference_phylogeny_tiny.nwk $PREFIX/share/sepp/ref/
+cp -vf test/unittest/data/q2-fragment-insertion/RAxML_info-reference-gg-raxml-bl.info $PREFIX/share/sepp/ref/
