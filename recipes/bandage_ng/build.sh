01685bc5884f7d4be8816ae78c5458ddd77b1bef
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-29 21:33:11 -0500

    Update build.sh

--- recipes/bandage_ng/build.sh
+++ recipes/bandage_ng/build.sh
@@ -21,5 +21,5 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
   -DOPENGL_egl_LIBRARY:FILEPATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libEGL.so.1" \
   -DEGL_opengl_LIBRARY:FILEPATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so" \
   -DOPENGL_opengl_LIBRARY:FILEPATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so" \
-  "${CONFIG_ARGS}"
+  -DQT_HOST_PATH="${PREFIX}/lib/qt6" "${CONFIG_ARGS}"
 cmake --build build --target install -j "${CPU_COUNT}"
