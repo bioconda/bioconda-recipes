aaff83bf61b6de3110e2fb7cfce535ed2a130e42
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-04-29 22:11:32 -0500

    Update build.sh

--- recipes/ucsc-bedtogenepred/build.sh
+++ recipes/ucsc-bedtogenepred/build.sh
@@ -20,10 +20,22 @@ mkdir -p "${BINDIR}"
 #sed -i.bak -e 's|-ldl -lm -lc|-ldl -lm -lc -lhts -lcurl|' kent/src/inc/common.mk
 #sed -i.bak -e 's|-lstdc++ -lrt|-lstdc++ -lrt -lhts -lcurl|' kent/src/inc/common.mk
 sed -i.bak -e 's|${LINKLIBS} ${L}|${LINKLIBS} ${L} -lcurl -lhts -ldl -lm -lc -liconv|' kent/src/inc/userApp.mk
-sed -i.bak -e 's|${MYSQLLIBS}|${MYSQLLIBS} ${PREFIX}/lib/libhts.a|' kent/src/inc/userApp.mk
+#sed -i.bak -e 's|${MYSQLLIBS}|${MYSQLLIBS} ${PREFIX}/lib/libhts.a|' kent/src/inc/userApp.mk
 sed -i.bak -e 's|-lpthread|-pthread|' kent/src/inc/common.mk
 
 rm -rf kent/src/inc/*.bak
+
+cd kent/src/htslib
+autoreconf -if
+./configure --prefix=`$(pwd)` \
+  --enable-libcurl --enable-plugins --disable-option-checking \
+  CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" \
+  CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+
+make clean
+make -j"${CPU_COUNT}"
+make install
+cd ../../../
 
 (cd kent/src/lib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" COPT="${COPT}" -j"${CPU_COUNT}")
 (cd kent/src/htslib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" COPT="${COPT}" -j"${CPU_COUNT}")
