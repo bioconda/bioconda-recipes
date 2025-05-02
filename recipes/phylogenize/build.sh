47bad18ff7a4b4b05ba726ef9981b305685c2efc
Kathryn Kananen <kekananen@gmail.com>
2025-05-02 14:04:36 -0400

    fixed silent dependancy failure

--- recipes/phylogenize/build.sh
+++ recipes/phylogenize/build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-R -e "devtools::install_bitbucket('pbradz/phylogenize/package/phylogenize')"
+set -e
+
+export R_LIBS_USER=$PREFIX/lib/R/library
+export R_LIBS=$PREFIX/lib/R/library
+
+Rscript -e "install.packages('ezknitr', repos='https://cloud.r-project.org')"
+
+cd package/phylogenize
+
+R CMD INSTALL --library=$PREFIX/lib/R/library .
