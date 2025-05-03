2cc7149c5b3bdbcf034c9ada712e01c2a8945fab
UENO, M. <eunosium-1128@live.jp>
2025-05-03 14:39:08 +0000

    Fix quoting in build script for CMake variables

--- recipes/voronota/build.sh
+++ recipes/voronota/build.sh
@@ -9,9 +9,9 @@ cd build
 
 cmake .. \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
     -DEXPANSION_JS=ON \
@@ -19,5 +19,5 @@ cmake .. \
     -DEXPANSION_GL=ON \
     -DENABLE_MPI=ON
 
-make -j${CPU_COUNT}
+make -j"${CPU_COUNT}"
 make install
