676c01d4e828e663825c9f7e6195ffca27b85563
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-02 21:06:05 -0500

    Update build.sh

--- recipes/glimpse-bio/build.sh
+++ recipes/glimpse-bio/build.sh
@@ -8,11 +8,10 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 ARCH=$(uname -m)
 
 if [[ "${ARCH}" == "arm64" || "${ARCH}" == "aarch64" ]]; then
-	sed -i.bak 's|-mavx2 -mfma||' chunk/makefile
-	sed -i.bak 's|-mavx2 -mfma||' concordance/makefile
-	sed -i.bak 's|-mavx2 -mfma||' split_reference/makefile
 	sed -i.bak 's|-mavx2 -mfma||' phase/makefile
-	sed -i.bak 's|-mavx2 -mfma||' ligate/makefile
+	sed -i.bak 's|g++ -std=c++17|$(CXX) -std=c++17|' */makefile
+	sed -i.bak 's|-lpthread|-pthread|' */makefile
+	rm -rf */*.bak
 fi
 
 for subdir in chunk concordance split_reference phase ligate
@@ -21,17 +20,16 @@ do
     # Build the binaries
     make CXX="${CXX} -std=c++17" \
         DYN_LIBS="-lz -pthread -lbz2 -llzma -lcurl -lhts -ldeflate -lm" \
-        CXXFLAG="${CXXFLAGS} ${PREFIX} -O3 -D__COMMIT_ID__='\"${COMMIT_VERS}\"' -D__COMMIT_DATE__='\"${COMMIT_DATE}\"' -Wno-ignored-attributes -O3 -mavx2 -mfma" \
+        CXXFLAG="${CXXFLAGS} ${PREFIX} -D__COMMIT_ID__='\"${COMMIT_VERS}\"' -D__COMMIT_DATE__='\"${COMMIT_DATE}\"' -Wno-ignored-attributes -O3" \
         LDFLAG="${LDFLAGS}" \
 	HTSSRC="${PREFIX}" \
         HTSLIB_INC="${PREFIX}/include" \
         HTSLIB_LIB="-lhts" \
-        BOOST_INC="${PREFIX}/include"\
+        BOOST_INC="${PREFIX}/include" \
         BOOST_LIB_IO="-lboost_iostreams" \
         BOOST_LIB_PO="-lboost_program_options" \
         BOOST_LIB_SE="-lboost_serialization" \
-        -j"${CPU_COUNT}" \
-    ;
+        -j"${CPU_COUNT}";
     # Install the binaries
     install -v -m 0755 "bin/GLIMPSE2_${subdir}" "${PREFIX}/bin"
     popd
