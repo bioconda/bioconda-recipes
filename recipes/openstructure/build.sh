ba8a11facb27ae0edf1f7adec92ae371de0f4a29
UENO, M. <eunosium-1128@live.jp>
2025-05-03 19:34:18 +0000

    openstructure: Update build script and meta.yaml with improved dependencies and configurations

--- recipes/openstructure/build.sh
+++ recipes/openstructure/build.sh
@@ -1,45 +1,50 @@
 #!/usr/bin/env bash
+
 set -euxo pipefail
 
-sed -i 's|<boost/filesystem/convenience.hpp>|<boost/filesystem.hpp>|' modules/seq/alg/src/hmm_pseudo_counts.cc
-
-mkdir build
-cd build
+mkdir -p build && cd build
 
 PYVER=$($PYTHON -c 'import sys; print(f"{sys.version_info[0]}.{sys.version_info[1]}")')
 
 cmake .. \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DPython_EXECUTABLE=${PYTHON} \
-  -DBoost_INCLUDE_DIR=${PREFIX}/include \
-  -DBOOST_PYTHON_LIBRARIES=${PREFIX}/lib/libboost_python${PYVER}.so \
-  -DOPTIMIZE=1 \
-  -DENABLE_MM=1 \
-  -DOPEN_MM_LIBRARY=${PREFIX}/lib/python${PYVER}/site-packages/OpenMM.libs/lib \
-  -DOPEN_MM_INCLUDE_DIR=${PREFIX}/lib/python${PYVER}/site-packages/OpenMM.libs/include \
-  -DOPEN_MM_PLUGIN_DIR=${PREFIX}/lib/python${PYVER}/site-packages/OpenMM.libs/lib/plugins \
-  -DENABLE_PARASAIL=1 \
-  -DCOMPILE_TMTOOLS=1 \
-  -DENABLE_GFX=1 \
-  -DENABLE_GUI=0 \
-  -DENABLE_INFO=0 \
-  -DCMAKE_VERBOSE_MAKEFILE=ON
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DBOOST_ROOT=${PREFIX} \
+    -DENABLE_GUI=OFF \
+    -DENABLE_GFX=OFF \
+    -DENABLE_INFO=OFF \
+    -DUSE_RPATH=ON
 
-make VERBOSE=1
+make VERBOSE=1 -j"${CPU_COUNT}"
 
 cp ../components/components.cif.gz .
 stage/bin/chemdict_tool create components.cif.gz compounds.chemlib pdb -i
 stage/bin/chemdict_tool update ../modules/conop/data/charmm.cif compounds.chemlib charmm
 
+OMM_LIBDIR=${PREFIX}/lib/python${PYVER}/site-packages/OpenMM.libs
 cmake .. \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DPython_EXECUTABLE=${PYTHON} \
-  -DBOOST_PYTHON_LIBRARIES=${PREFIX}/lib/libboost_python${PYVER}.so \
-  -DCOMPOUND_LIB=${PWD}/compounds.chemlib \
-  -DCMAKE_VERBOSE_MAKEFILE=ON
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DBOOST_ROOT=${PREFIX} \
+    -DCOMPOUND_LIB=${SRC_DIR}/build/compounds.chemlib \
+    -DPARASAIL_INCLUDE_DIR=${PREFIX}/include \
+    -DPARASAIL_LIBRARY=${PREFIX}/lib/libparasail.${SHLIB_EXT} \
+    -DUSE_RPATH=ON \
+    -DOPTIMIZE=ON \
+    -DENABLE_PARASAIL=ON \
+    -DCOMPILE_TMTOOLS=OFF \
+    -DENABLE_GFX=ON \
+    -DENABLE_GUI=ON \
+    -DENABLE_INFO=ON \
+    -DUSE_SHADER=ON \
+    -DUSE_DOUBLE_PRECISION=OFF \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DENABLE_MM=ON \
+    -DOPEN_MM_LIBRARY=${OMM_LIBDIR}/lib/libOpenMM.${SHLIB_EXT} \
+    -DOPEN_MM_INCLUDE_DIR=${OMM_LIBDIR}/include \
+    -DOPEN_MM_PLUGIN_DIR=${OMM_LIBDIR}/lib/plugins \
 
-make VERBOSE=1
+make VERBOSE=1 -j"${CPU_COUNT}"
 make check
 make install
