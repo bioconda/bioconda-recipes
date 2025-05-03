be79a053b6c1d008788358b0852da32b5ed85bf5
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-02 19:44:38 -0500

    regtools: add arm64 build

--- recipes/regtools/build.sh
+++ recipes/regtools/build.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="${CFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-export CPATH=${PREFIX}/include
-export CXXFLAGS="${CXXFLAGS} -std=c++14"
+export CPATH="${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+
+mkdir -p "${PREFIX}/bin"
 mv src/utils/bedtools/gzstream/version src/utils/bedtools/gzstream/version.txt
 
-mkdir build
-cd build
-cmake -D CMAKE_INSTALL_PREFIX:PATH=${PREFIX} -D CMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib ..
-make -j 4
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
-mkdir -p ${PREFIX}/bin
-cp regtools ${PREFIX}/bin
-chmod +x ${PREFIX}/bin/regtools
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_INSTALL_RPATH="${PREFIX}/lib" -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_C_FLAGS="${CFLAGS}" \
+	"${CONFIG_ARGS}"
+cmake --build build -j "${CPU_COUNT}"
+
+install -v -m 0755 build/regtools "${PREFIX}/bin"
