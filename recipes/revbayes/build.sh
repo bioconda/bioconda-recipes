89864f104438d25958f5dcf33a82bf5a1c4cb1de
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-02 23:19:01 -0500

    Update build.sh

--- recipes/revbayes/build.sh
+++ recipes/revbayes/build.sh
@@ -17,6 +17,7 @@ fi
 
 # MPI version
 ./regenerate.sh -mpi true
+
 ./build.sh -mpi true -help2yml true \
 	-DCMAKE_BUILD_TYPE=Release \
  	-DCMAKE_PREFIX_PATH="${PREFIX}" \
@@ -33,10 +34,14 @@ install -v -m 0755 build-mpi/rb-mpi rb-mpi-help2yml "${PREFIX}/bin"
 rm -rf build-mpi
 
 ./regenerate.sh
+
 ./build.sh -mpi false -help2yml true \
-	-DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -boost_root "${PREFIX}" \
-	-j "${CPU_COUNT}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_PREFIX_PATH="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-boost_root "${PREFIX}" \
+	-j "${CPU_COUNT}" \
 	"${CONFIG_ARGS}"
 
 install -v -m 0755 build/rb rb-help2yml "${PREFIX}/bin"
