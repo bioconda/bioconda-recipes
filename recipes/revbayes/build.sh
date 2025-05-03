384d422aa58088ad47e33530bbebc6f99342aaa9
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-02 23:34:00 -0500

    Update build.sh

--- recipes/revbayes/build.sh
+++ recipes/revbayes/build.sh
@@ -18,14 +18,10 @@ fi
 # MPI version
 ./regenerate.sh -mpi true
 
-./build.sh -mpi true -help2yml true \
-	-DCMAKE_BUILD_TYPE=Release \
- 	-DCMAKE_PREFIX_PATH="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-boost_root "${PREFIX}" \
-	-j "${CPU_COUNT}" \
-	"${CONFIG_ARGS}"
+./build.sh -mpi true -ninja true -help2yml true \
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -boost_root "${PREFIX}" \
+	-j "${CPU_COUNT}" "${CONFIG_ARGS}"
 
 install -v -m 0755 build-mpi/rb-mpi rb-mpi-help2yml "${PREFIX}/bin"
 
@@ -35,14 +31,10 @@ rm -rf build-mpi
 
 ./regenerate.sh
 
-./build.sh -mpi false -help2yml true \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_PREFIX_PATH="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-boost_root "${PREFIX}" \
-	-j "${CPU_COUNT}" \
-	"${CONFIG_ARGS}"
+./build.sh -mpi false -ninja true -help2yml true \
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -boost_root "${PREFIX}" \
+	-j "${CPU_COUNT}" "${CONFIG_ARGS}"
 
 install -v -m 0755 build/rb rb-help2yml "${PREFIX}/bin"
 
