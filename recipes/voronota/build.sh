52edb4c6a442e4c9fd17b99befec1d99f1de01c7
UENO Masayoshi <masa_u@macbookpro.lan>
2025-05-05 01:42:47 +0900

    voronota: add default CMake args

--- recipes/voronota/build.sh
+++ recipes/voronota/build.sh
@@ -1,28 +1,23 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -exou pipefail
 
 export CPU_COUNT=1
 
 if [[ "$(uname)" == "Darwin" ]]; then
     # c++11 compatibility
-    export CXX="${BUILD_PREFIX}/bin/clang++"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11 -Xpreprocessor -fopenmp -lomp"
-    export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lomp -L${PREFIX}/lib -lGLEW -L${PREFIX}/lib -lglfw -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo"
+    export CXXFLAGS="${CXXFLAGS} -std=c++11 -Xpreprocessor -fopenmp"
+    export LDFLAGS="${LDFLAGS} -lomp -lGLEW -lglfw -framework OpenGL"
 elif [[ "$(uname)" == "Linux" ]]; then
-    export CXX="${BUILD_PREFIX}/bin/g++"
     export CXXFLAGS="${CXXFLAGS} -fopenmp"
     export LDFLAGS="${LDFLAGS} -lGLEW -lglfw -lGL -lGLU -lOpenGL"
 fi
 
-mkdir build
-cd build
+mkdir build && cd build
 
 cmake .. \
+    "${CMAKE_ARGS}" \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_FIND_FRAMEWORK=FIRST \
     -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
