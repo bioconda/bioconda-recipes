0063ab6de16c3ac9ebe69bc600a29d5ecf0c721f
UENO, M <eunosium-1128@live.jp>
2025-05-05 03:57:27 +0900

    voronota: Add platform-specific OpenGL paths for macOS build

--- recipes/voronota/build.sh
+++ recipes/voronota/build.sh
@@ -18,17 +18,33 @@ fi
 
 mkdir build && cd build
 
-cmake .. \
-    ${CMAKE_ARGS} \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DCMAKE_CXX_COMPILER="${CXX}" \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
-    -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
-    -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
-    -DEXPANSION_JS=ON \
-    -DEXPANSION_LT=ON \
-    -DEXPANSION_GL=ON
+if [[ "$(uname)" == "Darwin" ]]; then
+    cmake .. \
+        ${CMAKE_ARGS} \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+        -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
+        -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
+        -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
+        -DOPENGL_gl_LIBRARY:FILEPATH="/System/Library/Frameworks/OpenGL.framework/OpenGL" \
+        -DOPENGL_INCLUDE_DIR:PATH="/System/Library/Frameworks/OpenGL.framework/Headers" \
+        -DEXPANSION_JS=ON \
+        -DEXPANSION_LT=ON \
+        -DEXPANSION_GL=ON
+elif [[ "$(uname)" == "Linux" ]]; then
+    cmake .. \
+        ${CMAKE_ARGS} \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+        -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
+        -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
+        -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
+        -DEXPANSION_JS=ON \
+        -DEXPANSION_LT=ON \
+        -DEXPANSION_GL=ON
+fi
 
 make -j"${CPU_COUNT}"
 make install
