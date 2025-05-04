4b6b0185ebf9ec23d506481ce03ec8c8b20e6e52
UENO, M <eunosium-1128@live.jp>
2025-05-05 04:52:43 +0900

    voronota: Use CMake's FindOpenGL module and simplify build script
    Replace direct GL library linking with proper OpenGL package discovery.
    Unify platform-specific CMake configurations into a single command.

--- recipes/voronota/build.sh
+++ recipes/voronota/build.sh
@@ -16,35 +16,26 @@ elif [[ "$(uname)" == "Linux" ]]; then
     export LDFLAGS="${LDFLAGS} -lGLEW -lglfw -lGL -lGLU"
 fi
 
+sed -i.bak \
+    -e '/target_link_libraries(voronota-gl m GL GLEW glfw)/i\
+find_package(OpenGL REQUIRED)
+' \
+    -e 's/target_link_libraries(voronota-gl m GL GLEW glfw)/target_link_libraries(voronota-gl m OpenGL::GL GLEW glfw)/' \
+    expansion_gl/CMakeLists.txt
+
 mkdir build && cd build
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    cmake .. \
-        ${CMAKE_ARGS} \
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-        -DCMAKE_CXX_COMPILER="${CXX}" \
-        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-        -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
-        -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
-        -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
-        -DOPENGL_gl_LIBRARY:FILEPATH="/System/Library/Frameworks/OpenGL.framework/OpenGL" \
-        -DOPENGL_INCLUDE_DIR:PATH="/System/Library/Frameworks/OpenGL.framework/Headers" \
-        -DEXPANSION_JS=ON \
-        -DEXPANSION_LT=ON \
-        -DEXPANSION_GL=ON
-elif [[ "$(uname)" == "Linux" ]]; then
-    cmake .. \
-        ${CMAKE_ARGS} \
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-        -DCMAKE_CXX_COMPILER="${CXX}" \
-        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-        -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
-        -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
-        -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
-        -DEXPANSION_JS=ON \
-        -DEXPANSION_LT=ON \
-        -DEXPANSION_GL=ON
-fi
+cmake .. \
+    ${CMAKE_ARGS} \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
+    -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
+    -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
+    -DEXPANSION_JS=ON \
+    -DEXPANSION_LT=ON \
+    -DEXPANSION_GL=ON
 
 make -j"${CPU_COUNT}"
 make install
