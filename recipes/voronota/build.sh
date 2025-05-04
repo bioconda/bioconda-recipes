18f429085ce7aa6099f78f7fb35ff0f32993424d
UENO, M <eunosium-1128@live.jp>
2025-05-05 03:29:14 +0900

    Update voronota build flags and linker configuration

--- recipes/voronota/build.sh
+++ recipes/voronota/build.sh
@@ -9,7 +9,7 @@ fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
     # c++11 compatibility
-    export CXXFLAGS="${CXXFLAGS} -std=c++11 -Xpreprocessor -fopenmp"
+    export CXXFLAGS="${CXXFLAGS} -Xpreprocessor -fopenmp"
     export LDFLAGS="${LDFLAGS} -lomp -lGLEW -lglfw -framework OpenGL"
 elif [[ "$(uname)" == "Linux" ]]; then
     export CXXFLAGS="${CXXFLAGS} -fopenmp"
@@ -24,6 +24,8 @@ cmake .. \
     -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
+    -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
+    -DCMAKE_MODULE_LINKER_FLAGS="${LDFLAGS}" \
     -DEXPANSION_JS=ON \
     -DEXPANSION_LT=ON \
     -DEXPANSION_GL=ON
