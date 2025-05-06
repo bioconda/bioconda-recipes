a95e024c4ddf1545c9060ca87ef850fb8eabfec4
Usman Rashid <usman@smme.edu.pk>
2025-05-06 16:41:10 +1200

    Now moving to bin

--- recipes/hic_qc/build.sh
+++ recipes/hic_qc/build.sh
@@ -6,5 +6,16 @@ set -ex
 
 export VERSION=1.2.913c977
 
-git tag $VERSION
+
+cat <<EOF > pyproject.toml
+[project]
+name = "hic_qc"
+description = "Quality assessment for Hi-C libraries"
+version = "$VERSION"
+EOF
+
 ${PYTHON} -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
+
+mkdir -p ${PREFIX}/bin
+mv hic_qc.py ${PREFIX}/bin
+chmod a+x ${PREFIX}/bin/hic_qc.py
