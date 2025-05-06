7cbdeb42875e5d7960f885b623763cc4c70d2d0e
Usman Rashid <usman@smme.edu.pk>
2025-05-06 16:24:20 +1200

    Added tag and no cache

--- recipes/hic_qc/build.sh
+++ recipes/hic_qc/build.sh
@@ -4,5 +4,7 @@
 # -x = print every executed command
 set -ex
 
+export VERSION=1.2.913c977
 
-$PYTHON -m pip install . --no-deps -vvv
+git tag $VERSION
+${PYTHON} -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
