040a5a269d067c06388ba9a80e7d068b212de81b
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-07 16:29:41 -0500

    fix patch

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -x -e
+
 BOOST_ROOT="${PREFIX}"
 export DEST="${PREFIX}"
 export PERL_EXT_CPPFLAGS="-D_REENTRANT -D_GNU_SOURCE -fwrapv -fno-strict-aliasing -pipe -fstack-protector"
 export PERL_EXT_LDFLAGS="-shared -O3 -fstack-protector"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPATH="${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -std=gnu90 ${LDFLAGS}"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -I${PREFIX}/include ${LDFLAGS}"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 ${LDFLAGS}"
+
 ./install.sh
