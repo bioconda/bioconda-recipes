6c6ebf0cbe5160f7c6a86d6627f8eb5f71f03a12
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-07 15:16:09 -0500

    Update build.sh

--- recipes/anchorwave/build.sh
+++ recipes/anchorwave/build.sh
@@ -2,62 +2,67 @@
 
 set -eux
 
-if [ $TARGET_PLATFORM = "Linux" ]; then
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+if [[ `uname` == "Darwin" ]]; then
+    export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+    export CONFIG_ARGS=""
+fi
+
+if [[ $TARGET_PLATFORM = "Linux" ]]; then
     mkdir -p build/{sse2,sse4.1,avx2,avx512}
 
     # SSE2
     rm CMakeLists.txt
-    ln -s CMakeLists_sse2.txt CMakeLists.txt
+    ln -sf CMakeLists_sse2.txt CMakeLists.txt
     cd build/sse2
-    cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../..
-    make -j"${CPU_COUNT}"
-    make install
+    cmake -S ../.. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" -Wno-dev -Wno-deprecated --no-warn-unused-cli "${CONFIG_ARGS}"
+    cmake --build build --clean-first --target install -j "${CPU_COUNT}"
     mv "${PREFIX}/bin/anchorwave" "${PREFIX}/bin/anchorwave_sse2"
     cd ../..
 
     # SSE4.1
     rm CMakeLists.txt
-    ln -s CMakeLists_sse4.1.txt CMakeLists.txt
+    ln -sf CMakeLists_sse4.1.txt CMakeLists.txt
     cd build/sse4.1
-    cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../..
-    make -j"${CPU_COUNT}"
-    make install
+    cmake -S ../.. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" -Wno-dev -Wno-deprecated --no-warn-unused-cli "${CONFIG_ARGS}"
+    cmake --build build --clean-first --target install -j "${CPU_COUNT}"
     mv "${PREFIX}/bin/anchorwave" "${PREFIX}/bin/anchorwave_sse4.1"
     cd ../..
 
     # AVX2
     rm CMakeLists.txt
-    ln -s CMakeLists_avx2.txt CMakeLists.txt
+    ln -sf CMakeLists_avx2.txt CMakeLists.txt
     cd build/avx2
-    cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../..
-    make -j"${CPU_COUNT}"
-    make install
+    cmake -S ../.. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" -Wno-dev -Wno-deprecated --no-warn-unused-cli "${CONFIG_ARGS}"
+    cmake --build build --clean-first --target install -j "${CPU_COUNT}"
     mv "${PREFIX}/bin/anchorwave" "${PREFIX}/bin/anchorwave_avx2"
     cd ../..
 
     # AVX512
     rm CMakeLists.txt
-    ln -s CMakeLists_avx512.txt CMakeLists.txt
+    ln -sf CMakeLists_avx512.txt CMakeLists.txt
     cd build/avx512
-    cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../..
-    make -j"${CPU_COUNT}"
-    make install
+    cmake -S ../.. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" -Wno-dev -Wno-deprecated --no-warn-unused-cli "${CONFIG_ARGS}"
+    cmake --build build --clean-first --target install -j "${CPU_COUNT}"
     mv "${PREFIX}/bin/anchorwave" "${PREFIX}/bin/anchorwave_avx512"
     cd ../..
 
     # wrapper script
-    cp "${RECIPE_DIR}/anchorwave" "${PREFIX}/bin/anchorwave"
+    cp -f "${RECIPE_DIR}/anchorwave" "${PREFIX}/bin/anchorwave"
     chmod +x "${PREFIX}/bin/anchorwave"
-elif [ $TARGET_PLATFORM = "macOS" ]; then
+elif [[ $TARGET_PLATFORM = "macOS" ]]; then
     mkdir -p build/macOS
 
     # macOS (SSE4.1)
     rm CMakeLists.txt
-    ln -s CMakeLists_MACOSX86.txt CMakeLists.txt
+    ln -sf CMakeLists_MACOSX86.txt CMakeLists.txt
     cd build/macOS
-    cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../..
-    make -j"${CPU_COUNT}"
-    make install
+    cmake -S ../.. -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" -Wno-dev -Wno-deprecated --no-warn-unused-cli "${CONFIG_ARGS}"
+    cmake --build build --clean-first --target install -j "${CPU_COUNT}"
 else
     echo "TARGET_PLATFORM must be Linux or macOS" >&2
     exit 1
