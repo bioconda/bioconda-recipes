8747b0cd236263bd7569dae2bc50bdcb0a46a81b
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-07 19:25:27 -0500

    edit build.sh

--- recipes/snap-aligner/build.sh
+++ recipes/snap-aligner/build.sh
@@ -7,6 +7,9 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
+sed -i.bak -e 's|"stdafx.h"|"../apps/snap/stdafx.h"|' tests/*.cpp
+rm -rf tests/*.bak
+
 make CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -std=c++03" \
 	-j"${CPU_COUNT}"
 install -v -m 0755 snap-aligner "${PREFIX}/bin"
