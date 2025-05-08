b50b4c32bc599844f646db324bf2503d86a228f8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-07 21:20:04 -0500

    Update build.sh

--- recipes/snap-aligner/build.sh
+++ recipes/snap-aligner/build.sh
@@ -11,6 +11,7 @@ sed -i.bak -e 's|"stdafx.h"|"../SNAPLib/stdafx.h"|' tests/*.cpp
 sed -i.bak -e 's|"AffineGap.h"|"../SNAPLib/AffineGap.h"|' tests/*.cpp
 sed -i.bak -e 's|"LandauVishkin.h"|"../SNAPLib/LandauVishkin.h"|' tests/*.cpp
 sed -i.bak -e 's|"AffineGapVectorized.h"|"../SNAPLib/AffineGapVectorized.h"|' tests/*.cpp
+sed -i.bak -e 's|"Compat.h"|"../SNAPLib/Compat.h"|' tests/*.cpp
 rm -rf tests/*.bak
 
 make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}"
