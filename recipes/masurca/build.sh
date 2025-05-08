5b191e0bf78f073e9fd326f4decbce76a7f2bf48
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-07 19:17:52 -0500

    Update build.sh

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -12,6 +12,7 @@ export CPATH="${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -std=gnu90"
 export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export LC_ALL=en_US.UTF-8
 
 ./install.sh
 
