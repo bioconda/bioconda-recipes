eb641eee103db20a25226774980f2654de14aab9
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-07 21:07:39 -0500

    clean up recipe

--- recipes/snap-aligner/build.sh
+++ recipes/snap-aligner/build.sh
@@ -5,13 +5,13 @@ mkdir -p "${PREFIX}/bin"
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -Wno-format -std=c++03"
+export CXXFLAGS="${CXXFLAGS} -g -O3 -Wno-format -std=c++03"
 
-sed -i.bak -e 's|"stdafx.h"|"../apps/snap/stdafx.h"|' tests/*.cpp
-sed -i.bak -e 's|"AffineGap.h"|"../SNAPLib/stdafx.h"|' tests/*.cpp
+sed -i.bak -e 's|"stdafx.h"|"../SNAPLib/stdafx.h"|' tests/*.cpp
+sed -i.bak -e 's|"AffineGap.h"|"../SNAPLib/AffineGap.h"|' tests/*.cpp
+sed -i.bak -e 's|"LandauVishkin.h"|"../SNAPLib/LandauVishkin.h"|' tests/*.cpp
+sed -i.bak -e 's|"AffineGapVectorized.h"|"../SNAPLib/AffineGapVectorized.h"|' tests/*.cpp
 rm -rf tests/*.bak
-sed -i.bak -e 's|"-std=c++98"|"-std=c++03"|' Makefile
-rm -rf *.bak
 
 make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}"
 install -v -m 0755 snap-aligner "${PREFIX}/bin"
