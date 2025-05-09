9083be6b6db423aa0c20d0ad186a926267cf8571
Jacques Dainat <jacques.dainat@gmail.com>
2025-05-09 20:53:57 +0200

    fix sed command to touch the correct file (#56038)

--- recipes/ltr_harvest_parallel/build.sh
+++ recipes/ltr_harvest_parallel/build.sh
@@ -9,4 +9,4 @@ cp -r bin/cut.pl ${PREFIX}/bin
 chmod a+x ${PREFIX}/bin/cut.pl
 
 # change the path to cut.pl in LTR_HARVEST_parallel
-sed -i 's|my \$cut = "\$script_path/bin/cut.pl"; #the program to cut sequence|my \$cut = "cut.pl"; #the program to cut sequence|' ${PREFIX}/bin/cut.pl
+sed -i 's|my \$cut = "\$script_path/bin/cut.pl"; #the program to cut sequence|my \$cut = "cut.pl"; #the program to cut sequence|' ${PREFIX}/bin/LTR_HARVEST_parallel
