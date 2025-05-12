da90bf3873c9c2f0114068beff5e723befd04956
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-12 18:55:43 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -2,11 +2,14 @@
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CFLAGS="${CFLAGS} -O3 -fcommon"
+export CFLAGS="${CFLAGS} -O3"
 export PYO3_PYTHON="${PYTHON}"
 unamestr=`uname`
 
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
+sed -i.bak 's|"0.40.2"|"0.47.0"|' rust/Cargo.toml
+sed -i.bak 's|"0.18.1"|"0.21.2"|' rust/Cargo.toml
+sed -i.bak 's|"0.18.3"|"0.21.2"|' rust/Cargo.toml
 rm -rf rust/*.bak
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --profile=minimal -y
@@ -17,7 +20,7 @@ RUST_BACKTRACE=1
 if [[ "$unamestr" == "Darwin" ]]; then
   RUSTFLAGS="-C link-args=-Wl,-undefined,dynamic_lookup" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
 else
-  RUSTFLAGS="-L ${PREFIX}/lib64" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
+  RUSTFLAGS="-L ${PREFIX}/lib" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
 fi
 
 cd rust
