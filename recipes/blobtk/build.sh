a0c5bfeb388a0260cf097ac2fe022f9e9ef1e959
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-12 17:17:25 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -2,14 +2,15 @@
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export PYO3_PYTHON="${PYTHON}"
 
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
 rm -rf rust/*.bak
 
 RUST_BACKTRACE=full
-cargo install -v --no-track --path rust/ --root "${PREFIX}"
+cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
 
 cd rust
-maturin build --interpreter "${PYTHON}" --release --strip -b pyo3
+maturin build -f --release --strip -b pyo3
 
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
