a30f3a04bc9aa4ecc43568774dd9508c5ad5a81b
y3tseng <146570926+y3tseng@users.noreply.github.com>
2025-05-12 16:25:54 -0700

    Update twilight to v0.1.3 (#56109)
    * add twilight
    
    * add space between package and version
    
    * add pin
    
    * modify package name
    
    * Update meta.yaml
    
    * Update build.sh
    
    * update twilight to v0.1.2
    
    * update twilight to v0.1.2
    
    * modify description
    
    * twilight v0.1.3
    
    * add build num
    
    * twilight v0.1.3
    
    * Update build.sh
    
    ---------
    
    Co-authored-by: y3tseng <y3tseng@AD.UCSD.EDU@raptor.ucsd.edu>
    Co-authored-by: Joshua Zhuang <71105179+mencian@users.noreply.github.com>

--- recipes/twilight/build.sh
+++ recipes/twilight/build.sh
@@ -6,17 +6,16 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 
 # Build
 mkdir -p "${PREFIX}/bin"
-cp -rf ${RECIPE_DIR}/CMakeLists.txt $SRC_DIR/CMakeLists.txt
 
 if [[ `uname` == "Darwin" ]]; then
   export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
-  export CONFIG_ARGS=""
+  CONFIG_ARGS="${CONFIG_ARGS} -DUSE_CUDA=ON"
 fi
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
-  "${CONFIG_ARGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli
+  ${CONFIG_ARGS} -Wno-dev -Wno-deprecated --no-warn-unused-cli
 
 cmake --build build --clean-first -j "${CPU_COUNT}"
-install -v -m 0755 build/twilight ${PREFIX}/bin
+install -v -m 0755 bin/twilight ${PREFIX}/bin
