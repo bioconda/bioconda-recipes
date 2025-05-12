52c054bbaf422dc58f012638df5061b314f6ad2f
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-05-12 14:45:01 -0400

    Add fancy java wrapper script and adhere to directory conventions

--- recipes/reactome-cli/build.sh
+++ recipes/reactome-cli/build.sh
@@ -4,7 +4,7 @@ TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 [ -d "${PREFIX}/bin" ] || mkdir -p "${PREFIX}/bin"
 
 cd "${SRC_DIR}"
-mv reactome-cli*.jar $TGT/reactome-cli.jar
+mv reactome*.jar $TGT/reactome-cli.jar
 
 cp $RECIPE_DIR/reactome.sh $TGT/reactome
 ln -s $TGT/reactome $PREFIX/bin
