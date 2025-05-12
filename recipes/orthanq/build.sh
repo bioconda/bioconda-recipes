711aa7251eb2d4518083330c1fac82f485ee017b
Felix Wiegand <fxwiegand@wgdnet.de>
2025-05-12 15:59:33 +0200

    Update build.sh

--- recipes/orthanq/build.sh
+++ recipes/orthanq/build.sh
@@ -3,6 +3,6 @@
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 export HDF5_DIR=${PREFIX}
-export RUSTFLAGS="-C link-args=-Wl,-rpath,$HDF5_DIR/lib"
+export RUSTFLAGS="-L${PREFIX}/lib -C link-args=-Wl,-rpath,$HDF5_DIR/lib"
 
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
