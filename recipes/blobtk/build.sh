4beeac9a4d4c2ca2c3cb08fafb5abc2eae2fb28d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-12 17:00:27 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -6,13 +6,10 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
 rm -rf rust/*.bak
 
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+RUST_BACKTRACE=full
+cargo install -v --no-track --path rust/ --root "${PREFIX}"
 
 cd rust
-
-RUST_BACKTRACE=full
-cargo install -v --no-track --path . --root "${PREFIX}"
-
 maturin build --interpreter "${PYTHON}" --release --strip -b pyo3
 
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
