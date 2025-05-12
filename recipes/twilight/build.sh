9fc74ab54f1bc7659caa7ed4fdc5968188070d58
y3tseng <y3tseng@AD.UCSD.EDU@raptor.ucsd.edu>
2025-05-12 15:04:20 -0700

    twilight v0.1.3

--- recipes/twilight/build.sh
+++ recipes/twilight/build.sh
@@ -5,23 +5,18 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
 # Build
-IS_GPU_BUILD=${cuda_variant}
 echo "Build variant is: $IS_GPU_BUILD"
 mkdir -p "${PREFIX}/bin"
 
 if [[ `uname` == "Darwin" ]]; then
   export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
-  if [[ "$IS_GPU_BUILD" == "gpu" ]]; then
-    CONFIG_ARGS="${CONFIG_ARGS} -DUSE_CUDA=ON"
-  else
-    CONFIG_ARGS="${CONFIG_ARGS} -DUSE_CUDA=OFF"
-  fi
+  CONFIG_ARGS="${CONFIG_ARGS} -DUSE_CUDA=ON"
 fi
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
-  "${CONFIG_ARGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli
+  ${CONFIG_ARGS} -Wno-dev -Wno-deprecated --no-warn-unused-cli
 
 cmake --build build --clean-first -j "${CPU_COUNT}"
-install -v -m 0755 build/twilight ${PREFIX}/bin
+install -v -m 0755 bin/twilight ${PREFIX}/bin
