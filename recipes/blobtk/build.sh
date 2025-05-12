74029217d31fa5147cfc1d60f184703e88c6015f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-12 18:15:19 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -2,13 +2,22 @@
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export PYO3_PYTHON="${PYTHON}"
+export CFLAGS="${CFLAGS} -O3 -fcommon"
+#export PYO3_PYTHON="${PYTHON}"
+unamestr=`uname`
 
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
 rm -rf rust/*.bak
 
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable --profile=minimal -y
+export PATH="${HOME}/.cargo/bin:${PATH}"
+
 RUST_BACKTRACE=full
-RUSTFLAGS="-C linker=${CC}" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
+if [[ "$unamestr" == 'Darwin' ]]; then
+  RUSTFLAGS="-C link-args=-Wl,-undefined,dynamic_lookup" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
+else
+  RUSTFLAGS="-L ${PREFIX}/lib64 -C linker=${CC}" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
+fi
 
 cd rust
 maturin build -f --release --strip -b pyo3
