fda56aa2ded9b1a457f12c14e1eeddf3a6bc1c02
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-13 03:56:50 -0500

    Update build.sh

--- recipes/pyopenms/build.sh
+++ recipes/pyopenms/build.sh
@@ -1,48 +1,35 @@
-#!/bin/sh
-
-# useless default include directory that is silently added by the compiler packages "to help"...
-#  it is not even added with -isystem https://github.com/AnacondaRecipes/aggregate/blob/master/clang/activate-clang%2B%2B.sh#L87
-USELESS="-I${PREFIX}/include"
-export CXXFLAGS=${CXXFLAGS//${USELESS}/}
+#!/bin/bash
 
 # Not sure if those are needed 
-export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
-#export DYLD_LIBRARY_PATH=${PREFIX}/lib
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${RPATH}"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
 
 mkdir build
 cd build
 
-if [[ $(uname -s) == Darwin ]]; then
+if [[ $(uname -s) == "Darwin" ]]; then
   RPATH='@loader_path/../lib'
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER -DCMAKE_MACOSX_RPATH=ON -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
 else
   ORIGIN='$ORIGIN'
   export ORIGIN
   RPATH='$${ORIGIN}/../lib'
+  export CONFIG_ARGS=""
 fi
 
-LDFLAGS='-Wl,-rpath,${RPATH}'
-
-cmake ../src/pyOpenMS \
-  -DOPENMS_GIT_SHORT_REFSPEC="release/${PKG_VERSION}" \
-  -DOPENMS_GIT_SHORT_SHA1="b59e0c3" \
-  -DCMAKE_BUILD_TYPE="Release" \
-  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
-  -DCMAKE_MACOSX_RPATH=ON \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_INSTALL_RPATH=${RPATH} \
-  -DCMAKE_INSTALL_NAME_DIR="@rpath" \
-  -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-  -DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON \
-  -DPython_EXECUTABLE=$PYTHON \
-  -DPython_FIND_STRATEGY="LOCATION" \
-  -DPY_NUM_MODULES=20 \
-  -DNO_DEPENDENCIES=ON \
-  -DNO_SHARE=ON
+cmake -S ../src/pyOpenMS -B . -DOPENMS_GIT_SHORT_REFSPEC="release/${PKG_VERSION}" \
+  -DOPENMS_GIT_SHORT_SHA1="b59e0c3" -DCMAKE_BUILD_TYPE="Release" \
+  -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -DCMAKE_PREFIX_PATH="${PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_RPATH="${RPATH}" -DCMAKE_INSTALL_NAME_DIR="@rpath" \
+  -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON \
+  -DPython_EXECUTABLE="${PYTHON}" -DPython_FIND_STRATEGY="LOCATION" \
+  -DPY_NUM_MODULES=20 -DNO_DEPENDENCIES=ON -DNO_SHARE=ON \
+  "${CONFIG_ARGS}"
 
 # NO_DEPENDENCIES since conda takes over re-linking etc
 
 # limit parallel jobs to 1 for memory usage since pyopenms has huge cython generated cpp files
-make -j1 pyopenms
-$PYTHON -m pip install ./pyOpenMS/dist/*.whl --ignore-installed --no-deps
+cmake --build . --clean-first --target pyopenms -j 1
+${PYTHON} -m pip install ./pyOpenMS/dist/*.whl --no-binary=pyopenms --no-build-isolation --no-deps --no-cache-dir -vvv
