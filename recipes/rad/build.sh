e0f85f4570e2d090a5cad80809f94d7170afd3e0
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-12 21:54:43 -0500

    Update build.sh

--- recipes/rad/build.sh
+++ recipes/rad/build.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# create and enter build directory
-mkdir -p build
-cd build
+if [[ `uname` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
 
 # configure CMake: install into $PREFIX
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release ..
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
+   "${CONFIG_ARGS}"
 
 # build and install
-cmake --build .
-cmake --install . --prefix "${PREFIX}"
+cmake --build build --clean-first -j "${CPU_COUNT}"
+cmake --install build --prefix "${PREFIX}"
 #super lazy local fix
-cd ../
-cp ./build/rad "${PREFIX}/bin/rad"
+install -v -m 0755 ./build/rad "${PREFIX}/bin/rad"
