20d2f0468f7c777ed5a6f759141b232fdf53323a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-14 16:38:27 -0500

    Update build.sh

--- recipes/spades/build.sh
+++ recipes/spades/build.sh
@@ -7,6 +7,6 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -fcommon"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -fcommon -Wno-deprecated-declarations -Wno-unused-variable -Wno-parentheses -Wno-unused-result"
+export CXXFLAGS="${CXXFLAGS} -O3 -fcommon -Wno-deprecated-declarations -Wno-unused-variable -Wno-parentheses -Wno-unused-result -Wno-unused-but-set-variable -Wno-conversion"
 
 PREFIX="${PREFIX}" ./spades_compile.sh -rj"${CPU_COUNT}"
