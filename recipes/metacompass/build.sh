224f93411422ecac6cede427f875dbe948e606c1
gongyh <gongyh@qibebt.ac.cn>
2025-05-15 20:10:26 +0800

    add MetaCompass to bioconda-recipes

---
+++ recipes/metacompass/build.sh
@@ -0,0 +1,7 @@
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/lib
+cp -r ${SRC_DIR} $PREFIX/lib/metacompass
+sed -i 's/g++/${CXX}/g' $PREFIX/lib/metacompass/install.sh
+$PREFIX/lib/metacompass/install.sh
+ln -s $PREFIX/lib/metacompass/go_metacompass.py $PREFIX/bin/go_metacompass.py
+ln -s $PREFIX/lib/metacompass/bin/kmer-mask $PREFIX/bin/kmer-mask
