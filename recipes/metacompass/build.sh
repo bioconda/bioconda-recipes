568a86aca18fb7da4076bcdb805fe22fffc3ce0e
gongyh <gongyh@qibebt.ac.cn>
2025-05-15 21:50:42 +0800

    try to make it work in macos

--- recipes/metacompass/build.sh
+++ recipes/metacompass/build.sh
@@ -7,6 +7,7 @@ mkdir -p ${PREFIX}/lib
 cp -r ${SRC_DIR} $PREFIX/lib/metacompass
 
 sed -i 's/g++/${CXX}/g' $PREFIX/lib/metacompass/install.sh
+chmod +x $PREFIX/lib/metacompass/install.sh
 $PREFIX/lib/metacompass/install.sh
 
 ln -s $PREFIX/lib/metacompass/go_metacompass.py $PREFIX/bin/go_metacompass.py
