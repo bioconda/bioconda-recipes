c0bafb7ae9471343c9823116aec4e64cd7129cc6
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-14 23:33:52 -0500

    Update build.sh

--- recipes/perl-vcftools-vcf/build.sh
+++ recipes/perl-vcftools-vcf/build.sh
@@ -2,15 +2,14 @@
 set -eu
 
 mkdir -p perl-lib/lib
-mkdir -p blib/script
 
 wget https://raw.githubusercontent.com/vcftools/vcftools/refs/heads/master/src/perl/tab-to-vcf -O src/perl/tab-to-vcf
-cp -f src/perl/vcf-haplotypes blib/script
+wget https://raw.githubusercontent.com/vcftools/vcftools/refs/heads/master/src/perl/vcf-haplotypes -O src/perl/vcf-haplotypes
 
-cp -f "src/perl/*.pm" perl-lib/lib
-cp -f "src/perl/fill-*" perl-lib
-cp -f "src/perl/vcf-*" perl-lib
-cp -f "src/perl/tab-to-vcf" perl-lib
+cp -f src/perl/*.pm perl-lib/lib
+cp -f src/perl/fill-* perl-lib
+cp -f src/perl/vcf-* perl-lib
+cp -f src/perl/tab-to-vcf perl-lib
 # Fix naming scheme of package to make Builder happy
 sed -i.bak 's/Vcf.pm.  Module/Vcf - Module/' perl-lib/lib/Vcf.pm
 rm -rf perl-lib/lib/*.bak
