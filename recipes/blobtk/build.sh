14ab33dfe2abb58d107ab583d054f163607dda9a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-14 23:39:54 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -31,8 +31,7 @@ fi
 RUST_BACKTRACE=1
 cd rust
 if [[ "${OS}" == "Linux" ]]; then
-	export RUSTFLAGS="-C target-feature=+crt-static"
-	RUSTFLAGS="-C target-feature=+crt-static" maturin build --interpreter "${PYTHON}" --release --strip -b pyo3
+	RUSTFLAGS="-C target-feature=-crt-static" maturin build --interpreter "${PYTHON}" --release --strip -b pyo3
 else
 	RUSTFLAGS="-C link-args=-Wl,-undefined,dynamic_lookup" maturin build --interpreter "${PYTHON}" --release --strip -b pyo3 --target "${CARGO_BUILD_TARGET}"
 fi
