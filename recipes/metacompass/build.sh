9c62c54abea361df798f7d0d0c498039d0eaf33e
gongyh <gongyh@qibebt.ac.cn>
2025-05-15 21:12:08 +0800

    try to make it work in macos

--- recipes/metacompass/build.sh
+++ recipes/metacompass/build.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+set -eu -o pipefail
+
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/lib
+
 cp -r ${SRC_DIR} $PREFIX/lib/metacompass
+
 sed -i 's/g++/${CXX}/g' $PREFIX/lib/metacompass/install.sh
 $PREFIX/lib/metacompass/install.sh
+
 ln -s $PREFIX/lib/metacompass/go_metacompass.py $PREFIX/bin/go_metacompass.py
 ln -s $PREFIX/lib/metacompass/bin/kmer-mask $PREFIX/bin/kmer-mask
