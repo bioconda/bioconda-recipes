bd661b08bef954f655be86dfb8762855843a2045
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-14 22:30:22 -0500

    Update build.sh

--- recipes/perl-vcftools-vcf/build.sh
+++ recipes/perl-vcftools-vcf/build.sh
@@ -3,13 +3,12 @@ set -eu
 
 mkdir -p perl-lib/lib
 
-chmod 0755 src/perl/tab-to-vcf
 cp -f src/perl/*.pm perl-lib/lib
 cp -f src/perl/fill-* perl-lib
 cp -f src/perl/vcf-* perl-lib
-cp -f "${SRC_DIR}/src/perl/tab-to-vcf" perl-lib
 # Fix naming scheme of package to make Builder happy
 sed -i.bak 's/Vcf.pm.  Module/Vcf - Module/' perl-lib/lib/Vcf.pm
+rm -rf perl-lib/lib/*.bak
 cp -f "${RECIPE_DIR}/Build.PL" perl-lib
 
 cd perl-lib
@@ -21,4 +20,3 @@ perl ./Build install --installdirs site
 
 chmod 0755 ${PREFIX}/bin/fill-*
 chmod 0755 ${PREFIX}/bin/vcf-*
-chmod 0755 ${PREFIX}/bin/tab-to-vcf
