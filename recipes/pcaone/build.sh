f5c3266d7bd23b878b1be43d6a518192e1de8004
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-14 23:48:28 -0500

    Update build.sh

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -5,18 +5,19 @@ mkdir -p ${PREFIX}/bin
 export CFLAGS="${CFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
-export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ $(uname -s) == "Linux" ]]; then
   make clean && make MKLROOT="${PREFIX}" AVX=0 -j"${CPU_COUNT}" && mv PCAone "${PREFIX}/bin/PCAone.x64"
   make clean && make MKLROOT="${PREFIX}" AVX=1 -j"${CPU_COUNT}" && mv PCAone "${PREFIX}/bin/PCAone.avx2"
+  cp -f "${PREFIX}/bin/PCAone.avx2" "${PREFIX}/bin/PCAone.avx22"
   export LDFLAGS="${LDFLAGS} -Wl,--no-as-needed -L${PREFIX}/lib"
   echo "#!/usr/bin/env bash
-PCAone=${PREFIX}/bin/PCAone.avx2
-grep avx2 /proc/cpuinfo |grep fma &>/dev/null
-[[ \$? != 0 ]] && PCAone=${PREFIX}/bin/PCAone.x64
-exec \$PCAone \$@" > ${PREFIX}/bin/PCAone && chmod 0755 ${PREFIX}/bin/PCAone
-
+  PCAone=${PREFIX}/bin/PCAone.avx2
+  grep avx2 /proc/cpuinfo |grep fma &>/dev/null
+  [[ \$? != 0 ]] && PCAone=${PREFIX}/bin/PCAone.x64
+  exec \$PCAone \$@" > ${PREFIX}/bin/PCAone && chmod 0755 ${PREFIX}/bin/PCAone
+  mv "${PREFIX}/bin/PCAone.avx22" "${PREFIX}/bin/PCAone.avx2"
 elif [[ $(uname -s) == "Darwin" ]]; then
   make MKLROOT="${PREFIX}" -j"${CPU_COUNT}"
   install -v -m 0755 PCAone ${PREFIX}/bin
