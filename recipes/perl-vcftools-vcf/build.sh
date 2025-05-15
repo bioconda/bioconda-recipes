a47ceead937e55a5a16441c447377e32771e44f7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-14 22:18:21 -0500

    Update build.sh

--- recipes/perl-vcftools-vcf/build.sh
+++ recipes/perl-vcftools-vcf/build.sh
@@ -1,40 +1,24 @@
 #!/bin/bash
 set -eu
+
 mkdir -p perl-lib/lib
-cp src/perl/*.pm perl-lib/lib
-cp src/perl/fill-* src/perl/tab-to-vcf src/perl/vcf-* perl-lib
+
+chmod 0755 src/perl/tab-to-vcf
+cp -f src/perl/*.pm perl-lib/lib
+cp -f src/perl/fill-* perl-lib
+cp -f src/perl/vcf-* perl-lib
+cp -f "${SRC_DIR}/src/perl/tab-to-vcf" perl-lib
 # Fix naming scheme of package to make Builder happy
 sed -i.bak 's/Vcf.pm.  Module/Vcf - Module/' perl-lib/lib/Vcf.pm
-cp $RECIPE_DIR/Build.PL perl-lib
+cp -f "${RECIPE_DIR}/Build.PL" perl-lib
+
 cd perl-lib
+
 perl Build.PL
 perl ./Build
-perl ./Build test 
+perl ./Build test
 perl ./Build install --installdirs site
 
-chmod +rx $PREFIX/bin/vcf-compare
-chmod +rx $PREFIX/bin/fill-an-ac
-chmod +rx $PREFIX/bin/vcf-to-tab
-chmod +rx $PREFIX/bin/fill-aa
-chmod +rx $PREFIX/bin/vcf-convert
-chmod +rx $PREFIX/bin/vcf-fix-newlines
-chmod +rx $PREFIX/bin/vcf-merge
-chmod +rx $PREFIX/bin/fill-ref-md5
-chmod +rx $PREFIX/bin/vcf-indel-stats
-chmod +rx $PREFIX/bin/vcf-fix-ploidy
-chmod +rx $PREFIX/bin/vcf-isec
-chmod +rx $PREFIX/bin/vcf-stats
-chmod +rx $PREFIX/bin/vcf-subset
-chmod +rx $PREFIX/bin/vcf-phased-join
-chmod +rx $PREFIX/bin/vcf-query
-chmod +rx $PREFIX/bin/vcf-consensus
-chmod +rx $PREFIX/bin/vcf-shuffle-cols
-chmod +rx $PREFIX/bin/vcf-validator
-chmod +rx $PREFIX/bin/vcf-contrast
-chmod +rx $PREFIX/bin/vcf-haplotypes
-chmod +rx $PREFIX/bin/vcf-tstv
-chmod +rx $PREFIX/bin/vcf-sort
-chmod +rx $PREFIX/bin/vcf-concat
-chmod +rx $PREFIX/bin/vcf-annotate
-chmod +rx $PREFIX/bin/tab-to-vcf
-chmod +rx $PREFIX/bin/fill-fs
+chmod 0755 ${PREFIX}/bin/fill-*
+chmod 0755 ${PREFIX}/bin/vcf-*
+chmod 0755 ${PREFIX}/bin/tab-to-vcf
