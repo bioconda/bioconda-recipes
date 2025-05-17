9a28fcd1f347bcd2e5eaf6d2523436b46e8a826a
UENO, M. <eunosium-1128@live.jp>
2025-05-17 07:30:22 +0000

    MSMS: Refactor installation commands

--- recipes/msms/build.sh
+++ recipes/msms/build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib/msms"
+set -euxo pipefail
+
+mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/lib/msms"
+mkdir -p "${PREFIX}/man/man1"
+mkdir -p "${PREFIX}/doc"
 
 # Look for `atmtypenumbers` in the library folder instead of the current folder
 sed -i.bak "s|numfile = \"./atmtypenumbers\"|numfile = \"$PREFIX/lib/msms/atmtypenumbers\"|" pdb_to_xyzr
 sed -i.bak "s|numfile = \"./atmtypenumbers\"|numfile = \"$PREFIX/lib/msms/atmtypenumbers\"|" pdb_to_xyzrn
 
-# Copy files to the conda bin/ folder
-cp ./pdb_to_xyzr $PREFIX/bin
-cp ./pdb_to_xyzrn $PREFIX/bin
-cp ./msms.*.$PKG_VERSION $PREFIX/bin/msms
-cp ./atmtypenumbers $PREFIX/lib/msms
-
-chmod +x $PREFIX/bin/pdb_to_xyzr
-chmod +x $PREFIX/bin/pdb_to_xyzrn
-
+install -m644 ./atmtypenumbers "${PREFIX}/lib/msms/"
+install -m644 msms.1 "${PREFIX}/man/man1/"
+install -m644 msms.html "${PREFIX}/doc/"
+install -m755 ./pdb_to_xyzr "${PREFIX}/bin/pdb_to_xyzr"
+install -m755 ./pdb_to_xyzrn "${PREFIX}/bin/pdb_to_xyzrn"
+install -m755 ./msms*.${PKG_VERSION} "${PREFIX}/bin/msms"
