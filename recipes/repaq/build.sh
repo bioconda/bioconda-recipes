329eb929c92a6de07d6a8aae44824d62f6c36c0f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-18 17:30:54 -0500

    Update build.sh

--- recipes/repaq/build.sh
+++ recipes/repaq/build.sh
@@ -3,8 +3,12 @@ set -eu -o pipefail
 
 mkdir -p $PREFIX/bin
 
+sed -i.bak 's|-lpthread|-pthread|' Makefile
+sed -i.bak 's|-std=c++11|-std=c++14|' Makefile
+rm -rf *.bak
+
 if [[ $OSTYPE == "darwin"* ]]; then
-  LDFLAGS="$LDFLAGS -L${PREFIX}/lib -lc"
+  LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lc"
 fi
 
 make DIR_INC="${PREFIX}/include" LIBRARY_DIRS="${PREFIX}/lib" CXX="${CXX}" -j"${CPU_COUNT}"
