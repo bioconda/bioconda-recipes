26176d27604b9d67f3c5bac27a52378541f12f83
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-18 17:27:39 -0500

    Update build.sh

--- recipes/repaq/build.sh
+++ recipes/repaq/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-if [[ $OSTYPE == "darwin"* ]]; then
-  LDFLAGS="$LDFLAGS -lc"
-fi
-make DIR_INC="$PREFIX/include" LIBRARY_DIRS="$PREFIX/lib" CXX=$CXX
 mkdir -p $PREFIX/bin
-make install BINDIR=$PREFIX/bin
+
+if [[ $OSTYPE == "darwin"* ]]; then
+  LDFLAGS="$LDFLAGS -L${PREFIX}/lib -lc"
+fi
+
+make DIR_INC="${PREFIX}/include" LIBRARY_DIRS="${PREFIX}/lib" CXX="${CXX}" -j"${CPU_COUNT}"
+make install BINDIR="${PREFIX}/bin"
