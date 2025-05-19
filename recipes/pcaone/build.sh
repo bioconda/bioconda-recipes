67b91ba2f56767c8adeeab410b0c10a76dc47afc
Zilong-Li <zimusen94@gmail.com>
2025-05-19 20:06:12 +0200

    increase build number

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -6,8 +6,5 @@ export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-# sed -i.bak 's|-lpthread|-pthread|' Makefile
-# rm -rf *.bak
-
 make MKLROOT="${PREFIX}" -j"${CPU_COUNT}"
 install -v -m 0755 PCAone ${PREFIX}/bin
