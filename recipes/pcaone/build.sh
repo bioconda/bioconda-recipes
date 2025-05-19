45e31e814b95defd5615ad933946f33ba824ec87
Zilong-Li <zimusen94@gmail.com>
2025-05-19 14:28:21 +0200

    version v0.5.2

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ${PREFIX}/bin
 # fix zlib issue
-export CFLAGS="$CFLAGS -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
