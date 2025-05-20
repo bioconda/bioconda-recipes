0d7910c984452aa86bd204b1853a688f170e8b6f
Neng Huang <neng@ds.dfci.harvard.edu>
2025-05-20 11:54:52 -0400

    Fix install -D for macOS compatibility

--- recipes/longcallr/build.sh
+++ recipes/longcallr/build.sh
@@ -6,5 +6,5 @@ cargo install --locked --path . --root "$PREFIX"
 rm -rf "$PREFIX/.crates*"
 
 # Install Python scripts to $PREFIX/bin
-install -D -m 755 allele_specific/longcallR-asj.py "$PREFIX/bin/longcallR-asj"
-install -D -m 755 allele_specific/longcallR-ase.py "$PREFIX/bin/longcallR-ase"
+install -m 755 allele_specific/longcallR-asj.py "$PREFIX/bin/longcallR-asj"
+install -m 755 allele_specific/longcallR-ase.py "$PREFIX/bin/longcallR-ase"
