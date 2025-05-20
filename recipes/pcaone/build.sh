cf1e0c4e17f0e735cdd33b312433de63a7771993
Zilong-Li <zimusen94@gmail.com>
2025-05-20 03:26:49 +0200

    fix pcaone recipe (#56257)
    * fix pcaone recipe
    
    * fix mkl version
    
    * version v0.5.2
    
    * disable mkl for mac
    
    * increase build number
    
    * reset build number to 0
    
    * update recipe-maintainers

--- recipes/pcaone/build.sh
+++ recipes/pcaone/build.sh
@@ -2,26 +2,9 @@
 
 mkdir -p ${PREFIX}/bin
 # fix zlib issue
-export CFLAGS="${CFLAGS} -O3"
-export CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-sed -i.bak 's|-lpthread|-pthread|' Makefile
-rm -rf *.bak
-
-if [[ $(uname -s) == "Linux" ]]; then
-  make clean && make CXX="${CXX}" MKLROOT="${PREFIX}" AVX=0 -j"${CPU_COUNT}" STATIC=1 && mv PCAone "${PREFIX}/bin/PCAone.x64"
-  make clean && make CXX="${CXX}" MKLROOT="${PREFIX}" AVX=1 -j"${CPU_COUNT}" STATIC=1 && mv PCAone "${PREFIX}/bin/PCAone.avx2"
-  #cp -f "${PREFIX}/bin/PCAone.avx2" "${PREFIX}/bin/PCAone.avx22"
-  export LDFLAGS="${LDFLAGS} -Wl,--no-as-needed -L${PREFIX}/lib"
-  echo "#!/usr/bin/env bash
-  PCAone=${PREFIX}/bin/PCAone.avx2
-  grep avx2 /proc/cpuinfo |grep fma &>/dev/null
-  [[ \$? != 0 ]] && PCAone=${PREFIX}/bin/PCAone.x64
-  exec \$PCAone \$@" > ${PREFIX}/bin/PCAone && chmod 0755 ${PREFIX}/bin/PCAone
-  #mv "${PREFIX}/bin/PCAone.avx22" "${PREFIX}/bin/PCAone.avx2"
-elif [[ $(uname -s) == "Darwin" ]]; then
-  make MKLROOT="${PREFIX}" -j"${CPU_COUNT}"
-  install -v -m 0755 PCAone ${PREFIX}/bin
-fi
+make MKLROOT="${PREFIX}" -j"${CPU_COUNT}"
+install -v -m 0755 PCAone ${PREFIX}/bin
