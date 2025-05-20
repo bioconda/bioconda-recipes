a40e6bfa56e6ab45c5da7ab8014dd42ac0cd70da
gongyh <gongyh@qibebt.ac.cn>
2025-05-20 16:55:58 +0800

    improve requirements and build script

--- recipes/metacompass/build.sh
+++ recipes/metacompass/build.sh
@@ -6,9 +6,10 @@ mkdir -p ${PREFIX}/lib
 
 cp -r ${SRC_DIR} $PREFIX/lib/metacompass
 
-sed -i.bak 's/g++/${CXX}/g' $PREFIX/lib/metacompass/install.sh
-chmod +x $PREFIX/lib/metacompass/install.sh
-$PREFIX/lib/metacompass/install.sh
+cd $PREFIX/lib/metacompass
+${CXX} -Wall -W -O2 -o ./bin/extractSeq ./src/utils/extractSeq.cpp
+${CXX} -Wall -W -O2 -o ./bin/formatFASTA ./src/utils/formatFASTA.cpp
+${CXX} -Wall -W -O2 -o ./bin/buildcontig ./src/buildcontig/buildcontig.cpp ./src/buildcontig/cmdoptions.cpp ./src/buildcontig/memory.cpp ./src/buildcontig/procmaps.cpp ./src/buildcontig/outputfiles.cpp
 
 ln -s $PREFIX/lib/metacompass/go_metacompass.py $PREFIX/bin/go_metacompass.py
 ln -s $PREFIX/lib/metacompass/bin/kmer-mask $PREFIX/bin/kmer-mask
