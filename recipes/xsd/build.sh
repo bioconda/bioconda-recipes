a58038e5d5404e27249e617e0bd9989698a9194e
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-21 08:17:55 -0500

    Rebuild xsd recipe

--- recipes/xsd/build.sh
+++ recipes/xsd/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-make -j ${CPU_COUNT} CC="$CC" CXX="$CXX" AR="$AR" RANLIB="$RANLIB" CPPFLAGS="$CPPFLAGS" CXXFLAGS="$CXXFLAGS -std=c++14" LDFLAGS="$LDFLAGS"
-make install_prefix="$PREFIX" install
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+make CC="${CC}" CXX="${CXX}" AR="${AR} rcs" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+make install_prefix="${PREFIX}" install
