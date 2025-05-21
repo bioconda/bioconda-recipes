1e9433be8ec154598cf47f5a729448d9c492293e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-21 03:31:46 -0500

    Update build.sh

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -8,7 +8,7 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPATH="${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CFLAGS="${CFLAGS} -O3 -std=gnu99"
+export CFLAGS="${CFLAGS} -O3 -std=gnu11"
 export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wno-use-after-free -Wno-misleading-indentation -Wno-unused-result"
 export LC_ALL="en_US.UTF-8"
 
