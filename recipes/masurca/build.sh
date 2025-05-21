dcd0a86f6709a1bffc7b0f384844adf1fccb3467
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-21 02:18:16 -0500

    Update build.sh

--- recipes/masurca/build.sh
+++ recipes/masurca/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -x -e
 
-export BOOST_ROOT="${PREFIX}"
-export DEST="${PREFIX}"
 export PERL_EXT_CPPFLAGS="-D_REENTRANT -D_GNU_SOURCE -fwrapv -fno-strict-aliasing -pipe -fstack-protector"
 export PERL_EXT_LDFLAGS="-shared -O3 -fstack-protector"
 export LIBRARY_PATH="${PREFIX}/lib"
@@ -12,9 +10,38 @@ export CPATH="${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -std=gnu90"
 export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
-export LC_ALL=en_US.UTF-8
+export LC_ALL="en_US.UTF-8"
 
-./install.sh
+ROOT="${PREFIX}"
+[ -z "$DEST" ] && DEST="$ROOT"
+
+###################
+# Check for gmake #
+###################
+mkdir -p dist-bin
+PATH="$PATH:$ROOT/dist-bin"
+ln -sf $(which make) ${ROOT}/dist-bin/gmake
+ln -sf ${ROOT}/PkgConfig.pm ${ROOT}/dist-bin/pkg-config
+
+export NUM_THREADS=`grep -c '^processor' /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1`;
+BINDIR="${PREFIX}/bin"
+LIBDIR="${PREFIX}/lib"
+export PKG_CONFIG_PATH="${LIBDIR}/pkgconfig:${PKG_CONFIG_PATH}"
+
+cd global-1
+
+autoreconf -if
+./configure --prefix="${DEST}" --bindir="${BINDIR}" --libdir="${LIBDIR}" \
+        --enable-swig --enable-silent-rules --disable-option-checking \
+        --disable-dependency-tracking CC="${CC}" CXX="${CXX}" CPPFLAGS="${CPPFLAGS}" \
+        LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" \
+        BOOST_ROOT="${PREFIX}" YAGGO="$(which yaggo)"
+make install-special -j"${CPU_COUNT}"
+
+echo "creating example config file masurca_config_example.txt"
+masurca -g masurca_config_example.txt
+
+(cd Flye && make && cp -af ../Flye "${DEST}");
 
 ls ${SRC_DIR}
 ls ${SRC_DIR}/bin
