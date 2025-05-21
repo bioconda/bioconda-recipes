73bfa760620433e54774f279980fefefd331e61e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-21 08:34:16 -0500

    Update build.sh

--- recipes/xsd/build.sh
+++ recipes/xsd/build.sh
@@ -5,5 +5,5 @@ export LIBPATH="-L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CC="${CC}" CXX="${CXX}" AR="${AR} rcs" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+make CC="${CC}" CXX="${CXX}" AR="${AR}" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 make install_prefix="${PREFIX}" install
