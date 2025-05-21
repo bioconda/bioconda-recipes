b472294512795a19c3842a34c81b7069f134705b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-21 08:37:48 -0500

    Update build.sh

--- recipes/xsd/build.sh
+++ recipes/xsd/build.sh
@@ -2,8 +2,10 @@
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make CC="${CC}" CXX="${CXX}" AR="${AR}" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -std=c++14" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+dos2unix libxsd-frontend/version
+
+make CC="${CC}" CXX="${CXX}" AR="${AR}" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wno-terminate" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 make install_prefix="${PREFIX}" install
