22233b294e7ff2b258a9178dc6693dfa54397a10
UENO, M <eunosium-1128@live.jp>
2025-05-27 02:32:27 +0900

    mmcif: Disable LTO and remove Linux binutils deps
    Remove gcc-ar setup and LTO flags in CMakeLists.
    Drop binutils_impl_linux-64 and gcc_impl_linux-64 from host
    requirements in meta.yaml.

--- recipes/mmcif/build.sh
+++ recipes/mmcif/build.sh
@@ -4,13 +4,9 @@ set -exuo pipefail
 
 export CMAKE_GENERATOR=Ninja
 
-if [[ $(uname) == "Linux" ]]; then
-    export AR=$(command -v gcc-ar)
-fi
+# Disable LTO
+sed -i.bak 's|-flto||g' CMakeLists.txt
 
-sed -i.bak -E \
-    's|SET\(CMAKE_AR[[:space:]]+"gcc-ar"\)|SET(CMAKE_AR "$ENV{AR}" CACHE FILEPATH "")|' \
-    CMakeLists.txt
 sed -i.bak -E \
     's|cmake_minimum_required\(VERSION .*\)|cmake_minimum_required(VERSION 3.5)|' \
     CMakeLists.txt
