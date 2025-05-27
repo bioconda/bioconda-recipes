4e2265a143c2553aac9a2c7075f9375c2e4bf9fa
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-26 21:06:47 -0500

    Update build.sh

--- recipes/metacompass/build.sh
+++ recipes/metacompass/build.sh
@@ -4,12 +4,12 @@ set -eu -o pipefail
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/lib
 
-cp -r ${SRC_DIR} $PREFIX/lib/metacompass
+cp -rf ${SRC_DIR} $PREFIX/lib/metacompass
 
 cd $PREFIX/lib/metacompass
-${CXX} -Wall -W -O2 -o ./bin/extractSeq ./src/utils/extractSeq.cpp
-${CXX} -Wall -W -O2 -o ./bin/formatFASTA ./src/utils/formatFASTA.cpp
-${CXX} -Wall -W -O2 -o ./bin/buildcontig ./src/buildcontig/buildcontig.cpp ./src/buildcontig/cmdoptions.cpp ./src/buildcontig/memory.cpp ./src/buildcontig/procmaps.cpp ./src/buildcontig/outputfiles.cpp
+${CXX} -Wall -W -O3 -o ./bin/extractSeq ./src/utils/extractSeq.cpp
+${CXX} -Wall -W -O3 -o ./bin/formatFASTA ./src/utils/formatFASTA.cpp
+${CXX} -Wall -W -O3 -o ./bin/buildcontig ./src/buildcontig/buildcontig.cpp ./src/buildcontig/cmdoptions.cpp ./src/buildcontig/memory.cpp ./src/buildcontig/procmaps.cpp ./src/buildcontig/outputfiles.cpp
 
-ln -s $PREFIX/lib/metacompass/go_metacompass.py $PREFIX/bin/go_metacompass.py
-ln -s $PREFIX/lib/metacompass/bin/kmer-mask $PREFIX/bin/kmer-mask
+ln -sf $PREFIX/lib/metacompass/go_metacompass.py $PREFIX/bin/go_metacompass.py
+ln -sf $PREFIX/lib/metacompass/bin/kmer-mask $PREFIX/bin/kmer-mask
