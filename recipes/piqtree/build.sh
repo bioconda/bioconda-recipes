bab7e3b2995258aa4ce330b7c14a907dd886b1aa
Robert McArthur <robert.mcarthur@anu.edu.au>
2025-05-27 13:57:40 +1000

    ensure correct ar is used

--- recipes/piqtree/build.sh
+++ recipes/piqtree/build.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p "${BUILD_PREFIX}/fakebin"
+ln -sf "$(which llvm-ar)" "${BUILD_PREFIX}/fakebin/ar"
+export PATH="${BUILD_PREFIX}/fakebin:${PATH}"
+
 ./build_tools/build_iqtree.sh
 
 python setup.py install
