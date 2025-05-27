5214d95c52160b7c16d229641db123c6ec48a759
UENO, M <eunosium-1128@live.jp>
2025-05-28 01:30:21 +0900

    openstructure: Update package list and docs link in post-link.sh

--- recipes/openstructure/post-link.sh
+++ recipes/openstructure/post-link.sh
@@ -6,9 +6,9 @@ You may need to install the following packages to use certain python bindings:
   - bioconda::dockq
   - bioconda::hh-suite
   - bioconda::mmseqs2
-  - bioconda::msms
+  - bioconda::scripps-msms
   - conda-forge::dssp
 
-Refer to https://openstructure.org/docs/2.9.2/bindings/bindings/ for details.
+Refer to https://openstructure.org/docs/2.10/bindings/bindings/ for details.
 ===============================================================================
 EOF
