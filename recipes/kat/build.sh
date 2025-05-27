5ddef1ec38fe92efa95c12db6b185f6df1fe4c23
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-26 23:23:36 -0500

    Update build.sh

--- recipes/kat/build.sh
+++ recipes/kat/build.sh
@@ -5,9 +5,10 @@ export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wno-unused-but-set-variable -Wno-vla-cxx-extension -Wno-sizeof-pointer-div"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-unused-but-set-variable -Wno-vla-cxx-extension -Wno-sizeof-pointer-div"
 export LC_ALL="en_US.UTF-8"
 export PYTHONPATH="${SP_DIR}"
+export PYTHON_NOVERSION_CHECK="${PY_VER}"
 
 mkdir -p ${PREFIX}/bin
 
@@ -19,18 +20,21 @@ sed -i.bak "124d" configure.ac
 sed -i.bak 's|'3.5'|'3.10'|' configure.ac
 rm -rf *.bak
 
-./autogen.sh
-export PYTHON_NOVERSION_CHECK="${PY_VER}"
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "${OS}" == "Linux" ]]; then
+  export CXXFLAGS="${CXXFLAGS} -Wno-catch-value"
+fi
+
+autoreconf -if
 ./configure --prefix="${PREFIX}" CC="${CC}" CXX="${CXX}" \
   CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}" \
   PYTHON="${PYTHON}" PYTHON_VERSION="${PY_VER}" LIBS="-lz -lboost_program_options -lboost_filesystem -lboost_timer" \
   --enable-silent-rules --disable-dependency-tracking --disable-option-checking
+
 make -j"${CPU_COUNT}"
 make install
 
 # This directory isn't needed and confuses conda
 rm -rf $PREFIX/mkspecs
-
-cd ${PREFIX}/lib
-# Something is creating a symlink from ${PREFIX}/lib/\n
-find . -type l -not -name "??*" -ls -delete
