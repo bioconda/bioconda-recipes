209fdf5e48425bbc8390b62a6053d4d5978e8e5f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-26 20:38:19 -0500

    Update build.sh

--- recipes/quaqc/build.sh
+++ recipes/quaqc/build.sh
@@ -2,10 +2,17 @@
 
 set -ex
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/share/man/man1"
 
-make z_dyn=1 hts_dyn=1 release
+sed -i.bak 's|-lpthread|-pthread|' Makefile
+sed -i.bak 's|-std=gnu99|-std=gnu11|' Makefile
+rm -rf *.bak
 
+make CC="${CC}" CFLAGS="${CFLAGS}" PREFIX="${PREFIX}" z_dyn=1 hts_dyn=1 release -j"${CPU_COUNT}"
 make install
-
