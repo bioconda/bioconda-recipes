b485597c6d9a66f0833cce47930e7db3b5ca4b43
Reid Wagner <wagnerr@umn.edu>
2025-05-28 10:11:00 -0500

    Update MSFragger recipe to version 4.2: - Remove academic_install.py script, no longer needed. - Add new permanent download link generated for MSFragger. - Update Python dependency to 3.11

--- recipes/msfragger/build.sh
+++ recipes/msfragger/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# These values are submitted to the MSFragger site when downloading the application zip.
-if [[ -z "$NAME" ]]; then
-    NAME="${USERNAME:-bioconda}";
-fi
-if [[ -z "$EMAIL" ]]; then
-    EMAIL="${NAME}@${HOSTNAME:-bioconda.org}";
-fi
-if [[ -z "$INSTITUTION" ]]; then
-    INSTITUTION="${HOSTNAME:-bioconda.org}";
-fi
-
 # Create directories
 TARGET="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 mkdir -p "$TARGET"
@@ -20,15 +9,8 @@ cp "$RECIPE_DIR/msfragger.py" "$TARGET"
 ln -s "$TARGET/msfragger.py" "$PREFIX/bin/msfragger"
 chmod 0755 "${PREFIX}/bin/msfragger"
 
-# This script automates accepting the academic license agreements in order to download MSFragger and build the package. A user of this package is then expected to accept the terms themselves when they download a license key from the MSFragger site, which is enforced by the wrapper script and MSFragger jar file.
-"${RECIPE_DIR}/academic_install.py" -n galaxy -e "$EMAIL" -o "$INSTITUTION" -m "$PKG_VERSION" -p "$TARGET" --hash "$SHA256SUM"
-if [[ $? -ne 0 ]]; then
-    echo "Problem downloading jar file." > /dev/stderr
-    exit 1;
-fi
-
-# Unzip and link jar.
+# Move and link jar.
+mkdir -p "$TARGET/MSFragger-$PKG_VERSION"
+mv msfragger_download/* "$TARGET/MSFragger-$PKG_VERSION"
 cd "$TARGET"
-unzip "MSFragger-$PKG_VERSION.zip"
 ln -s "MSFragger-$PKG_VERSION/MSFragger-$PKG_VERSION.jar" "MSFragger.jar"
-rm "MSFragger-$PKG_VERSION.zip"
