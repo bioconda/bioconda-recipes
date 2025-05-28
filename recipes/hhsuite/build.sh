87439fc1df8648d13771d712021f762e2de01b12
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-27 21:49:25 -0500

    Update build.sh

--- recipes/hhsuite/build.sh
+++ recipes/hhsuite/build.sh
@@ -25,13 +25,13 @@ fi
 sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' CMakeLists.txt
 rm -rf *.bak
 
-cmake -S build -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release -DCHECK_MPI=1 -DHAVE_MPI=1 \
     -Wno-dev -Wno-deprecated --no-warn-unused-cli -DCMAKE_C_COMPILER="${CC}" \
     -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_C_FLAGS="${CFLAGS}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" "${ARCH_BUILD}" "${CONFIG_ARGS}"
 
-cmake --build . --clean-first --target install -j "${CPU_COUNT}"
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
 
 # Fix perl shebang
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${SRC_DIR}/scripts/*.pl
