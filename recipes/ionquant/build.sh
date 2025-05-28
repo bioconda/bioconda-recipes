168c57d54b62a6f23726d2f39885c9fffbc08b26
Reid Wagner <wagnerr@umn.edu>
2025-05-28 17:05:58 -0500

    Remove fn from meta.yaml as required by linter.

--- recipes/ionquant/build.sh
+++ recipes/ionquant/build.sh
@@ -11,6 +11,7 @@ chmod 0755 "${PREFIX}/bin/ionquant"
 
 # Move and link jar.
 mkdir -p "$TARGET/IonQuant-$PKG_VERSION"
-mv "IonQuant-$PKG_VERSION.jar" "$TARGET/IonQuant-$PKG_VERSION"
+unzip "download.php?token=0000000&download=${PKG_VERSION}%24zip"
+mv "IonQuant-$PKG_VERSION/IonQuant-$PKG_VERSION.jar" "$TARGET/IonQuant-$PKG_VERSION"
 cd "$TARGET"
 ln -s "IonQuant-$PKG_VERSION/IonQuant-$PKG_VERSION.jar" "IonQuant.jar"
