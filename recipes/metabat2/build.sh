0056f90bc31e5e31eeeb9f4edb55b59032bd8182
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-28 14:40:33 -0500

    Update build.sh

--- recipes/metabat2/build.sh
+++ recipes/metabat2/build.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
-
 set -xe 
 
-#sed -i.bak "s/set(Boost_USE_STATIC_LIBS   ON)/set(Boost_USE_STATIC_LIBS   OFF)/g" src/CMakeLists.txt
-mkdir build
-cd build
-
-export CXXFLAGS=-ldeflate
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$PREFIX
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -ldeflate"
 
 # Fix the version
 # make check_git_repository
-sed -i.bak 's/GIT-NOTFOUND/'$PKG_VERSION' (Bioconda)/'  ../version.h
+sed -i.bak 's/GIT-NOTFOUND/'$PKG_VERSION' (Bioconda)/' version.h
+
+sed -i.bak 's|VERSION 3.5.1|VERSION 3.5|' CMakeLists.txt
+rm -rf *.bak
+
+if [[ `uname` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="${PREFIX}" \
+  -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -DCMAKE_C_COMPILER="${CC}" -DCMAKE_C_FLAGS="${CFLAGS}" \
+  -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+  "${CONFIG_ARGS}"
 
 # Build & install
-make VERBOSE=1 -j ${CPU_COUNT}
-make install
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
