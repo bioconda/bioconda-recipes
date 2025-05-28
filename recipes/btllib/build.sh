3fa8ed7111b3af48f6e8a9aefa5a9f1c71c0c7d1
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-28 15:47:28 -0500

    Update build.sh

--- recipes/btllib/build.sh
+++ recipes/btllib/build.sh
@@ -4,10 +4,18 @@ export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations"
+
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
 CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" meson setup --buildtype release \
-	--prefix "${PREFIX}" --strip -Db_coverage=false build/
+	--prefix "${PREFIX}" --strip -Db_coverage=false \
+	-Dcmake_args="${CONFIG_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.5" \
+	build/
 
 cd build
 
