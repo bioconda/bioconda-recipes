62112b743a7a19d172fe9a1b11e0f03adc2318b7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-28 17:48:48 -0500

    Update build.sh

--- recipes/xsd/build.sh
+++ recipes/xsd/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations -Wno-terminate -Wno-deprecated"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ `uname -s` == "Darwin" ]]; then
   mv libxsd-frontend/version libxsd-frontend/VERSION
-  export CXXFLAGS="${CXXFLAGS} -O3 -std=libc++ -std=c++14"
+  export CXXFLAGS="${CXXFLAGS} -O3 -std=libc++"
 else
   export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
 fi
 
-make CC="${CC}" CXX="${CXX}" AR="${AR}" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS} -Wno-terminate -Wno-deprecated" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+make CC="${CC}" CXX="${CXX}" AR="${AR}" RANLIB="${RANLIB}" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 make install_prefix="${PREFIX}" install
