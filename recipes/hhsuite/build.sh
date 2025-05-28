00fb3035628116a9624d93c66433e74958520dab
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-27 22:01:30 -0500

    Update build.sh

--- recipes/hhsuite/build.sh
+++ recipes/hhsuite/build.sh
@@ -24,18 +24,21 @@ fi
 
 sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' CMakeLists.txt
 rm -rf *.bak
-
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_BUILD_TYPE=Release -DCHECK_MPI=1 -DHAVE_MPI=1 \
-    -Wno-dev -Wno-deprecated --no-warn-unused-cli -DCMAKE_C_COMPILER="${CC}" \
-    -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_C_FLAGS="${CFLAGS}" \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" "${ARCH_BUILD}" "${CONFIG_ARGS}"
-
-cmake --build build --clean-first --target install -j "${CPU_COUNT}"
+sed -i.bak 's|VERSION 2.8.12 FATAL_ERROR|VERSION 3.5|' lib/ffindex/CMakeLists.txt
+rm -rf lib/ffindex/*.bak
 
 # Fix perl shebang
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${SRC_DIR}/scripts/*.pl
 rm -rf ${SRC_DIR}/scripts/*.bak
 
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_BUILD_TYPE=Release -DCHECK_MPI=1 -DHAVE_MPI=1 \
+    -Wno-dev -Wno-deprecated --no-warn-unused-cli -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_C_FLAGS="${CFLAGS}" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+    "${ARCH_BUILD}" "${CONFIG_ARGS}"
+
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
+
 # Move the `reformat.pl` script to ${PREFIX}/bin to ensure it's available in the PATH
-mv ${SRC_DIR}/scripts/reformat.pl ${PREFIX}/bin
+install -v -m 0755 ${SRC_DIR}/scripts/reformat.pl ${PREFIX}/bin
