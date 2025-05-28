fefe1b61c22b2ae5a49c62c0f37a813151387ddd
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-28 18:58:29 -0500

    beagle-lib: add arm64 build

--- recipes/beagle-lib/build.sh
+++ recipes/beagle-lib/build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-export INCLUDES="-I{PREFIX}/include"
+export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
-cmake -S . -B build/ \
-	-DCMAKE_BUILD_TYPE=Release \
+if [[ `uname` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
 
-cmake --build build/ --target install -j ${CPU_COUNT}
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
