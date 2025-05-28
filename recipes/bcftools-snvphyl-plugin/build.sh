b62950f7e6023af88ebe2715be1a1ad9caa41cfc
joshuazhuang7 <joshua.zhuang@yahoo.com>
2025-05-28 03:22:28 -0500

    Rebuild bcftools-snvphyl-plugin recipe

--- recipes/bcftools-snvphyl-plugin/build.sh
+++ recipes/bcftools-snvphyl-plugin/build.sh
@@ -1,33 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-export VERSION=1.9
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export VERSION="1.9"
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 
-CC=${CC}
+ln -sf ${CC} ${PREFIX}/bin/gcc
 
-ln -s ${CC} ${PREFIX}/bin/gcc
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/bcftools
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/libexec/bcftools
-
-#copy plugin source to be the bcftools plugin directory 
-cp snvphyl/bcfplugins/filter_snv_density.c bcftools-$VERSION/plugins/
+#copy plugin source to be the bcftools plugin directory
+cp -f snvphyl/bcfplugins/filter_snv_density.c bcftools-${VERSION}/plugins/
 
 # Compile htslib and bcftools with filter_snv_density plugin.
-pushd htslib-$VERSION
-autoheader
-(autoconf || autoconf)
-./configure --disable-bz2 --disable-lzma --prefix=$PREFIX
-make
+pushd htslib-${VERSION}
+autoreconf -if
+./configure --prefix="${PREFIX}" --disable-bz2 \
+	--disable-lzma --enable-libcurl --enable-plugins \
+	--with-libdeflate CC="${CC}" CFLAGS="${CFLAGS}" \
+	LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}"
+make -j"${CPU_COUNT}"
 popd
 
-pushd bcftools-$VERSION
-./configure --disable-bz2 --disable-lzma --prefix=$PREFIX
-make
+pushd bcftools-${VERSION}
+autoreconf -if
+./configure --prefix="${PREFIX}" --disable-bz2 --disable-lzma \
+	CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}"
+make -j"${CPU_COUNT}"
 popd
 
 #Move custom bcftools plugin to the ~/libexec/bcftools directory.
-mv bcftools-$VERSION/plugins/filter_snv_density.so $PREFIX/libexec/bcftools/
+mv bcftools-${VERSION}/plugins/filter_snv_density.so ${PREFIX}/libexec/bcftools/
 
-rm ${PREFIX}/bin/gcc
+rm -f ${PREFIX}/bin/gcc
