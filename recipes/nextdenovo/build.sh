85444bf07a40753ddc3709717f7aa6e3b3d67dd3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-29 18:24:42 -0500

    Update build.sh

--- recipes/nextdenovo/build.sh
+++ recipes/nextdenovo/build.sh
@@ -8,10 +8,14 @@ export CFLAGS="${CFLAGS} -O3"
 export OUTDIR="${SP_DIR}/nextdenovo"
 
 mkdir -p ${PREFIX}/bin ${OUTDIR}
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* lib/htslib/
 
 sed -i.bak 's|-O2|-O3|' lib/htslib/configure
 sed -i.bak 's|-lpthread|-pthread|' lib/htslib/configure
 rm -rf lib/htslib/*.bak
+sed -i.bak 's|-O2|-O3|' minimap2/Makefile
+sed -i.bak 's|-lpthread|-pthread|' minimap2/Makefile
+rm -rf minimap2/*.bak
 
 make CC="${CC}" CXX="${CXX}" INCLUDES="-I${PREFIX}/include" -j"${CPU_COUNT}"
 
