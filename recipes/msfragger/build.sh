2bab65b0951bc796fb66f341ac541f752df45eca
Reid Wagner <wagnerr@umn.edu>
2025-05-29 12:16:37 -0500

    Revert "Remove fn from meta.yaml as required by linter."
    This reverts commit f936e482db3d6b52fdb909f990d0db64cfe73d50.

--- recipes/msfragger/build.sh
+++ recipes/msfragger/build.sh
@@ -11,7 +11,6 @@ chmod 0755 "${PREFIX}/bin/msfragger"
 
 # Move and link jar.
 mkdir -p "$TARGET/MSFragger-$PKG_VERSION"
-unzip "download.php?token=0000000&download=Release ${PKG_VERSION}\$zip"
-mv "MSFragger-${PKG_VERSION}"/* "$TARGET/MSFragger-$PKG_VERSION"
+mv msfragger_download/* "$TARGET/MSFragger-$PKG_VERSION"
 cd "$TARGET"
 ln -s "MSFragger-$PKG_VERSION/MSFragger-$PKG_VERSION.jar" "MSFragger.jar"
