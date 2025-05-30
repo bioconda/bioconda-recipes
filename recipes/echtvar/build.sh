bb1bdfbd8c57b089cdd21e3de53dcb52723f9acf
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-05-29 19:03:36 -0500

    Update build.sh

--- recipes/echtvar/build.sh
+++ recipes/echtvar/build.sh
@@ -4,13 +4,14 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
-export OPENSSL_DIR=${PREFIX}
+export OPENSSL_DIR="${PREFIX}"
 export OPENSSL_NO_VENDOR=1
 export RUSTC_BOOTSTRAP=1
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 
-cargo-bundle-licenses \
-    --format yaml \
-    --output THIRDPARTY.yml
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-cargo install --bins --all-features --no-track --locked --root ${PREFIX} --path .
+cargo install --bins --all-features --no-track --locked --root "${PREFIX}" --path .
