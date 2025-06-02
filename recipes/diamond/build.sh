cabe8d83843cd0949d4c72316da67446e875c19d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-02 15:12:09 -0500

    Update build.sh

--- recipes/diamond/build.sh
+++ recipes/diamond/build.sh
@@ -6,26 +6,28 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
-if [[ `uname` == "Darwin" ]]; then
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-else
-	export CONFIG_ARGS=""
-fi
-
 sed -i.bak 's|VERSION 2.6|VERSION 3.5|' CMakeLists.txt
 rm -rf *.bak
 
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+	export CONFIG_ARGS="-DARM=ON -DX86=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET="11.0" -DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+elif [[ "${OS}" == "Darwin" && "${ARCH}" == "x86_64" ]]; then
+	export CONFIG_ARGS="-DX86=ON -DCMAKE_OSX_DEPLOYMENT_TARGET="10.15" -DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "aarch64" ]]; then
+	export CONFIG_ARGS="-DAARCH64=ON -DX86=OFF -DBUILD_STATIC=ON"
+else
+	export CONFIG_ARGS="-DX86=ON -DBUILD_STATIC=ON -DWITH_AVX512=ON"
+fi
+
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	-DWITH_ZSTD=ON \
-	-DZSTD_LIBRARY="${PREFIX}/lib/libzstd.a" \
-	-DZSTD_INCLUDE_DIR="${PREFIX}/include" \
-	-DBLAST_INCLUDE_DIR="${PREFIX}/include"
-	-DBLAST_LIBRARY_DIR="${PREFIX}/lib/ncbi-blast+" \
-	-DCMAKE_OSX_DEPLOYMENT_TARGET="" \
-	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DWITH_ZSTD=ON \
+	-DZSTD_LIBRARY="${PREFIX}/lib/libzstd.a" -DZSTD_INCLUDE_DIR="${PREFIX}/include" \
+	-DBLAST_INCLUDE_DIR="${PREFIX}/include" -DBLAST_LIBRARY_DIR="${PREFIX}/lib/ncbi-blast+" \
+	-DEXTRA=ON -DDP_STAT=ON -DWITH_DNA=ON -DWITH_MCL=ON -Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
 cmake --build build --clean-first --target install -j "${CPU_COUNT}"
