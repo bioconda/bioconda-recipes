ba595c78c4c70ad322561fed65ff7d98d9e6398f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-02 16:44:26 -0500

    Update build.sh

--- recipes/vamos/build.sh
+++ recipes/vamos/build.sh
@@ -7,6 +7,6 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPATH="${PREFIX}/include"
 
 cd src
-make CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -fPIE" CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -fPIE" -j"${CPU_COUNT}"
+make CC="${CC} ${CFLAGS} ${LDFLAGS} -fPIE" CXX="${CXX} ${CXXFLAGS} ${LDFLAGS} -fPIE" -j"${CPU_COUNT}"
 
 install -v -m 0755 vamos ${PREFIX}/bin
