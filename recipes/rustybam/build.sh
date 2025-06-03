d1f2db3d2c3ac2efdabb49de43d7d3ad0e3be0f8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-02 20:59:27 -0500

    Update build.sh

--- recipes/rustybam/build.sh
+++ recipes/rustybam/build.sh
@@ -5,6 +5,9 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
+sed -i.bak 's|"0.44"|"0.47"|' Cargo.toml
+rm -rf *.bak
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=1
