2b0720453ac8eb09adabec465f6015afe9dc14bd
LouisPwr <lokraf@dtu.dk>
2025-06-03 18:49:41 +0200

    pls

--- recipes/addeam/build.sh
+++ recipes/addeam/build.sh
@@ -1,43 +1,21 @@
 #!/usr/bin/env bash
-set -e  # Exit on error
+set -e
 
-# export PREFIX="$PREFIX"
-# export BUILD_PREFIX="$BUILD_PREFIX"
-# export CONDA_PREFIX="$BUILD_PREFIX"
-# export SRC_DIR="$SRC_DIR"
+export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/htslib"
+export LDFLAGS="-L${PREFIX}/lib -lhts -lgsl -lcurl -lgslcblas -llzma -lbz2 -lz  -lpthread"
 
+pushd submodules/lib/libgab
+git submodule update --init --recursive
+make clean
+make libgab.a
+make -C gzstream
+popd
 
-
-# export CPATH="${BUILD_PREFIX}/include"
-
-# export LDFLAGS="-L${BUILD_PREFIX}/lib"
-
-# export CFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/htslib"
-# export CPPFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/htslib"
-# export CXXFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/htslib"
-
-#ln -s ${BUILD_PREFIX}/lib/libncurses.so ${BUILD_PREFIX}/lib/libcurses.so
-
-mkdir -p "${PREFIX}/bin"
-
-pushd submodules/src/
-
-BUILD_PREFIX="$BUILD_PREFIX" \
-CONDA_PREFIX="$BUILD_PREFIX" \
-PREFIX="$PREFIX" \
-SRC_DIR="$SRC_DIR" \
-CXXFLAGS="-I${SRC_DIR}/submodules/lib/libgab \
-          -I${CONDA_PREFIX}/include      \
-          -I${CONDA_PREFIX}/include/htslib" \
-LDFLAGS="-L${CONDA_PREFIX}/lib" \
-
-make clean all
-
-# make clean
-# make
+pushd submodules/src
+make clean
+make CXX="${CXX}"   
 install -v -m 0755 bam2prof "${PREFIX}/bin/"
-popd 
+install -v -m 0755 diffprof "${PREFIX}/bin/"
+popd
 
 install -v -m 0755 addeam-bam2prof.py addeam-cluster.py "${PREFIX}/bin/"
-
-
