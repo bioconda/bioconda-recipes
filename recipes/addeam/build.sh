e1d3908be6ac93e1d86440220935982bb5a612c2
LouisPwr <lokraf@dtu.dk>
2025-06-03 17:07:05 +0200

    test

--- recipes/addeam/build.sh
+++ recipes/addeam/build.sh
@@ -1,22 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e  # Exit on error
 
-export CPATH="${PREFIX}/include"
+# export PREFIX="$PREFIX"
+# export BUILD_PREFIX="$BUILD_PREFIX"
+# export CONDA_PREFIX="$BUILD_PREFIX"
+# export SRC_DIR="$SRC_DIR"
 
-export LDFLAGS="-L${PREFIX}/lib"
 
-export CFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${PREFIX}/include"
-export CPPFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${PREFIX}/include"
-export CXXFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${PREFIX}/include"
 
-#ln -s ${PREFIX}/lib/libncurses.so ${PREFIX}/lib/libcurses.so
+# export CPATH="${BUILD_PREFIX}/include"
+
+# export LDFLAGS="-L${BUILD_PREFIX}/lib"
+
+# export CFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/htslib"
+# export CPPFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/htslib"
+# export CXXFLAGS="-I${SRC_DIR}/submodules/lib/libgab -I${BUILD_PREFIX}/include -I${BUILD_PREFIX}/include/htslib"
+
+#ln -s ${BUILD_PREFIX}/lib/libncurses.so ${BUILD_PREFIX}/lib/libcurses.so
 
 mkdir -p "${PREFIX}/bin"
 
 pushd submodules/src/
 
-make clean
-make
+BUILD_PREFIX="$BUILD_PREFIX" \
+CONDA_PREFIX="$BUILD_PREFIX" \
+PREFIX="$PREFIX" \
+SRC_DIR="$SRC_DIR" \
+CXXFLAGS="-I${SRC_DIR}/submodules/lib/libgab \
+          -I${CONDA_PREFIX}/include      \
+          -I${CONDA_PREFIX}/include/htslib" \
+LDFLAGS="-L${CONDA_PREFIX}/lib" \
+
+make clean all
+
+# make clean
+# make
 install -v -m 0755 bam2prof "${PREFIX}/bin/"
 popd 
 
