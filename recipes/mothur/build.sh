be1c8ae4ce6901249c9ed144138633d2fb55e29d
mencian <joshua.zhuang@yahoo.com>
2025-06-03 22:32:03 -0500

    clean up recipe

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
 ### Dumping preset flags because they break the build process on linux
 # Linker flags
 unset LDFLAGS
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-# Compiler flags
-unset CXXFLAGS
-export CXXFLAGS="${CXXFLAGS} -O3 -I."
-
-sed -i.bak 's|-std=c++11|-std=c++14 -O3|' Makefile
+export TARGET_ARCH="$(uname -m)"
 
 ### Compiling mothur
 make clean
-make CXX="${CXX}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+make CXX="${CXX}" -j"${CPU_COUNT}"
 make install
-install -v -m 0755 uchime ${PREFIX}/bin
+install -v -m 0755 uchime "${PREFIX}/bin"
 
 # Linking BLAST binaries to default location for mothur
-mkdir -pv "${PREFIX}"/bin/blast/bin/
-ln -sf "${PREFIX}"/bin/{blastall,formatdb,megablast} "${PREFIX}"/bin/blast/bin/
+mkdir -pv "${PREFIX}/bin/blast/bin"
+ln -sf "${PREFIX}"/bin/{blastall,formatdb,megablast} "${PREFIX}/bin/blast/bin/"
