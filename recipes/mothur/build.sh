e3efe32ed869aa6891f9107b0a8a5acf72e666df
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-05 18:07:01 -0500

    Update build.sh

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -6,8 +6,8 @@ export LIBPATH="-L${PREFIX}/lib"
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -headerpad_max_install_names"
 	#-std=c++14
-	sed -i.bak 's/-std=c++14/-stdlib=libc++ -std=c++14/' Makefile
- 	sed -i.bak 's/-std=c++14/-stdlib=libc++ -std=c++14/' source/uchime_src/makefile
+	sed -i.bak 's/-std=c++14/-std=c++14 -stdlib=libc++/' Makefile
+ 	sed -i.bak 's/-std=c++14/-std=c++14 -stdlib=libc++/' source/uchime_src/makefile
 else
 	### Dumping preset flags because they break the build process on linux
 	# Linker flags
@@ -18,6 +18,7 @@ fi
 sed -i.bak 's/OPTIMIZE ?= yes/OPTIMIZE ?= no/' Makefile
 sed -i.bak 's/LDFLAGS += $(subDirLinking)/LDFLAGS += -Lsource $(subDirLinking)/' Makefile
 sed -i.bak 's/$(CXX) $(LDFLAGS)/$(CXX) $(CXXFLAGS) $(LDFLAGS)/' Makefile
+sed -i.bak 's|mv mothur ${INSTALL_DIR}/mothur|install -v -m 0755 mothur ${INSTALL_DIR}|' Makefile
 rm -rf *.bak
 
 ### Compiling mothur
