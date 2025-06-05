1368b006a53448832460159e1e4a616ca4a52074
mencian <joshua.zhuang@yahoo.com>
2025-06-05 16:20:36 -0500

    clean up recipe

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -2,20 +2,24 @@
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-### Dumping preset flags because they break the build process on linux
-# Linker flags
-unset LDFLAGS
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-	sed -i.bak 's/-std=c++14/-std=c++14 -stdlib=libc++/' Makefile
- 	sed -i.bak 's/-std=c++14/-std=c++14 -stdlib=libc++/' source/uchime_src/makefile
-  	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -headerpad_max_install_names"
+	export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -headerpad_max_install_names"
+	sed -i.bak 's/-std=c++14/-stdlib=libc++ -std=c++14/' Makefile
+ 	sed -i.bak 's/-std=c++14/-stdlib=libc++ -std=c++14/' source/uchime_src/makefile
+else
+	### Dumping preset flags because they break the build process on linux
+	# Linker flags
+	unset LDFLAGS
+	export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 fi
+
+sed -i.bak 's/OPTIMIZE ?= yes/OPTIMIZE ?= no/' Makefile
+rm -rf *.bak
 
 ### Compiling mothur
 make clean
-make OPTIMIZE=no CXX="${CXX}" -j"${CPU_COUNT}"
+make CXX="${CXX}" -j"${CPU_COUNT}"
 make install
 install -v -m 0755 uchime "${PREFIX}/bin"
 
