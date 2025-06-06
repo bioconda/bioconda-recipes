7e6126aeac1494c595c2b9f91d35729b4cd3dd55
mencian <joshua.zhuang@yahoo.com>
2025-06-06 01:21:44 -0500

    bcalm: add arm64 build

--- recipes/bcalm/build.sh
+++ recipes/bcalm/build.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-export CPATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export CPATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
 # we can't build bcalm with avaible gatb, link trouble
 rm -rf gatb-core
 git clone https://github.com/GATB/gatb-core.git
 cd gatb-core
-git checkout d053d0dffdfb9d31e45d42a3da49d2f71c8f87b3
+git checkout e80aa72fc91bac58de11341b536c3a94ecb54719
 cd ..
 
+sed -i.bak 's|VERSION 2.6|VERSION 3.5|' CMakeLists.txt
+rm -rf *.bak
+
 # avoid gatb example install
-if [ "$(uname)" == "Darwin" ]; then
-    sed -i '' -e "s/.*INSTALL.*examples.*/#&/" gatb-core/gatb-core/CMakeLists.txt
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	sed -i '' -e "s/.*INSTALL.*examples.*/#&/" gatb-core/gatb-core/CMakeLists.txt
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
-    sed -i'' -e "s/.*INSTALL.*examples.*/#&/" gatb-core/gatb-core/CMakeLists.txt
+	sed -i '' -e "s/.*INSTALL.*examples.*/#&/" gatb-core/gatb-core/CMakeLists.txt
+	export CONFIG_ARGS=""
 fi
 
-# build 
-mkdir build
-cd build
-
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DKSIZE_LIST="32 64 96 128 160 192" ..
-make
-make install
+# build
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DKSIZE_LIST="32 64 96 128 160 192" -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli "${CONFIG_ARGS}"
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
