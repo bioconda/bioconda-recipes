74572383c5493d0dcb8ae1874ec0c86ea17516ba
mencian <joshua.zhuang@yahoo.com>
2025-06-06 15:21:31 -0500

    r-bpcells: add osx build

--- recipes/r-bpcells/build.sh
+++ recipes/r-bpcells/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 export DISABLE_AUTOBREW=1
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export CFLAGS="${CFLAGS} -O3"
+
 pushd r/
 ${R} CMD INSTALL --build --install-tests . ${R_ARGS}
 popd
