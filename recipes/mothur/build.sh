72e8b1f534dd0dd18aa49bc43849b1569c0d2211
mencian <joshua.zhuang@yahoo.com>
2025-06-05 19:18:25 -0500

    edit patch

--- recipes/mothur/build.sh
+++ recipes/mothur/build.sh
@@ -2,7 +2,7 @@
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -headerpad_max_install_names"
