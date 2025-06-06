ede29590f4fc8d831c1ba0b079b87ff0e95bd887
mencian <joshua.zhuang@yahoo.com>
2025-06-06 01:45:25 -0500

    add patch

--- recipes/gfatools/build.sh
+++ recipes/gfatools/build.sh
@@ -7,7 +7,8 @@ mkdir -p "${PREFIX}/bin"
 
 sed -i.bak 's|gcc|$(CC)|' paf2gfa/Makefile
 sed -i.bak 's|-O2|-O3|' paf2gfa/Makefile
-sed -i.bak 's|-I..|-I.. -I. -I$(PREFIX)/include|' paf2gfa/Makefile
+sed -i.bak 's|-std=c99|-std=c11|' paf2gfa/Makefile
+sed -i.bak 's|-I..|-I.. -I$(PREFIX)/include|' paf2gfa/Makefile
 rm -rf paf2gfa/*.bak
 
 make CC="${CC}" LIBS="-L${PREFIX}/lib -lz" -j"${CPU_COUNT}"
