0cf07b9ae1e01e71e2a93f9ef94cbbb25c7441c2
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-06 18:27:47 -0500

    Update build.sh

--- recipes/r-bpcells/build.sh
+++ recipes/r-bpcells/build.sh
@@ -8,7 +8,7 @@ export CFLAGS="${CFLAGS} -O3"
 export LC_ALL="en_US.UTF-8"
 
 if [[ $(uname -s) == "Darwin" ]]; then
-  sed -i.bak 's|-std=c++17|-O3 -std=c++17 -std=libc++ -D_LIBCPP_DISABLE_AVAILABILITY|' r/src/Makevars.in
+  sed -i.bak 's|-std=c++17|-O3 -std=c++17 -D_LIBCPP_DISABLE_AVAILABILITY|' r/src/Makevars.in
   rm -rf r/src/*.bak
 else
   sed -i.bak 's|-std=c++17|-O3 -std=c++17|' r/src/Makevars.in
