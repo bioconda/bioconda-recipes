51a51a1d4d6e34a65e606d361ab6e46cd02b58e7
mencian <joshua.zhuang@yahoo.com>
2025-06-06 22:42:30 -0500

    Rebuild ratatosk

--- recipes/ratatosk/build.sh
+++ recipes/ratatosk/build.sh
@@ -5,10 +5,19 @@ mkdir -p "${PREFIX}/bin"
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
 
-export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64 -I${PREFIX}/include"
+sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' CMakeLists.txt
+sed -i.bak 's|-std=c11|-O3 -std=c11|' CMakeLists.txt
+sed -i.bak 's|-std=c++11|-O3 -std=c++14|' CMakeLists.txt
+sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' bifrost/CMakeLists.txt
+sed -i.bak 's|-std=c11|-O3 -std=c11|' bifrost/CMakeLists.txt
+sed -i.bak 's|-std=c++11|-O3 -std=c++14|' bifrost/CMakeLists.txt
+rm -rf *.bak
+rm -rf bifrost/*.bak
 
-if [[ `uname` == Darwin ]]; then
+if [[ $(uname -s) == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 else
@@ -19,7 +28,9 @@ cmake -S . -B build \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
-cmake --build build/ --target install -j "${CPU_COUNT}" -v
+cmake --build build/ --clean-first --target install -j "${CPU_COUNT}"
