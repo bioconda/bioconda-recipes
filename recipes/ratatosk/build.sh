558a1bad8d634ffa7c5cee5d08220eb0e1cc4e9a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-06 23:23:47 -0500

    Update build.sh

--- recipes/ratatosk/build.sh
+++ recipes/ratatosk/build.sh
@@ -17,15 +17,30 @@ sed -i.bak 's|-std=c++11|-O3 -std=c++14|' Bifrost/CMakeLists.txt
 rm -rf *.bak
 rm -rf Bifrost/*.bak
 
-case $(uname -m) in
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+case ${ARCH} in
 	aarch64|arm64) sed -i.bak 's|-mno-avx2||' Bifrost/CMakeLists.txt && rm -rf Bifrost/*.bak ;;
 esac
 
-if [[ $(uname -s) == "Darwin" ]]; then
+case ${ARCH} in
+    x86_64) ARCH_FLAGS="-DCOMPILATION_ARCH=OFF" ;;
+    aarch64) ARCH_FLAGS="-DCOMPILATION_ARCH=OFF" ;;
+    *) ARCH_FLAGS="-DCOMPILATION_ARCH=ON" ;;
+esac
+
+if [[ ${OS} == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 	export CXXFLAGS="${CXXFLAGS} -std=libc++"
 else
 	export CONFIG_ARGS=""
+fi
+
+if [[ ${OS} == "Linux" && ${ARCH} == "x86_64" ]]; then
+	ARCH_FLAGS="${ARCH_FLAGS} -DENABLE_AVX2=ON"
+else
+ 	ARCH_FLAGS="${ARCH_FLAGS} -DENABLE_AVX2=OFF"
 fi
 
 cmake -S . -B build \
@@ -35,6 +50,6 @@ cmake -S . -B build \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
-	"${CONFIG_ARGS}"
+	"${CONFIG_ARGS}" "${ARCH_FLAGS}"
 
 cmake --build build/ --clean-first --target install -j "${CPU_COUNT}"
