89ee5b9288e7e2affd85d1732e823ded6677c087
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-08 15:35:12 -0500

    Update build.sh

--- recipes/miniprot-boundary-scorer/build.sh
+++ recipes/miniprot-boundary-scorer/build.sh
@@ -5,6 +5,6 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 mkdir -p ${PREFIX}/bin
 
-make CC="${CXX}" CFLAGS="${CFLAGS} -O3 -c -Wall -std=c++14" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" -j"${CPU_COUNT}"
+make miniprot_boundary_scorer CC="${CXX}" CFLAGS="${CFLAGS} -O3 -c -Wall -std=c++14" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" -j"${CPU_COUNT}"
 
 install -v -m 0755 miniprot_boundary_scorer "${PREFIX}/bin"
