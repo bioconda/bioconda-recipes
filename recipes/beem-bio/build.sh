4029c655e855de12b471bb2ad30fae3ef39893a7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-09 00:28:00 -0500

    Update build.sh

--- recipes/beem-bio/build.sh
+++ recipes/beem-bio/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
 set -xe
 
-make CC=${CXX} BeEM
-
 mkdir -p "${PREFIX}/bin"
-cp BeEM "${PREFIX}/bin"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+make BeEM CC="${CXX}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+
+install -v -m 0755 BeEM "${PREFIX}/bin"
