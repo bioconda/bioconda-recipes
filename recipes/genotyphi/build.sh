369cbb01100ad0e67bc07e9b87c2fee7e97c68a7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-09 04:13:10 -0500

    Update build.sh

--- recipes/genotyphi/build.sh
+++ recipes/genotyphi/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-cp mappingbased/genotyphi.py $PREFIX/bin/genotyphi.py
-ln $PREFIX/bin/genotyphi.py $PREFIX/bin/genotyphi
-chmod +x $PREFIX/bin/genotyphi.py $PREFIX/bin/genotyphi
+mkdir -p ${PREFIX}/bin
+
+2to3 -w mappingbased/genotyphi.py
+
+install -v -m 755 mappingbased/genotyphi.py "${PREFIX}/bin"
+ln -sf $PREFIX/bin/genotyphi.py $PREFIX/bin/genotyphi
+chmod +rx $PREFIX/bin/genotyphi
 
 # Add script for Mykrobe based analysis
 wget -O parse_typhi_mykrobe.py https://raw.githubusercontent.com/typhoidgenomics/genotyphi/main/typhimykrobe/parse_typhi_mykrobe.py
