5d901e14186370f7f5cbac2fb9cc0d550cb74732
root <root@localhost.localdomain>
2025-06-09 19:55:36 +0800

    update humman2

--- recipes/humann2/build.sh
+++ recipes/humann2/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-$PYTHON setup.py install \
-    --bypass-dependencies-install \
-    --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
