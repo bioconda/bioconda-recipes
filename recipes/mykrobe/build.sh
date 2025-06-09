025fc5b135c65e5ff52de08eaec03c57fa946f72
mencian <joshua.zhuang@yahoo.com>
2025-06-09 04:43:06 -0500

    mykrobe: add arm64 build

--- recipes/mykrobe/build.sh
+++ recipes/mykrobe/build.sh
@@ -2,9 +2,10 @@
 set -x
 
 # specify zlib location https://bioconda.github.io/troubleshooting.html#zlib-errors
-export CFLAGS="-I$PREFIX/include -fcommon"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH="$PREFIX/include"
+export CFLAGS="${CFLAGS} -O3 -fcommon"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPATH="${PREFIX}/include"
 export CC="${CC} -fcommon -lstdc++"
 export CXX="${CC} -fcommon"
 
@@ -29,10 +30,10 @@ if [ $(arch) != "x86_64" ]; then
     sed -i.bak 's/-m64//' Makefile
 fi
 
-make MAXK=31 -j ${CPU_COUNT}
-cp bin/mccortex31 ../src/mykrobe/cortex/
+make MAXK=31 -j"${CPU_COUNT}"
+cp -f bin/mccortex31 ../src/mykrobe/cortex/
 cd ../ || exit 1
 
-"$PYTHON" -m pip install . -vv
+"${PYTHON}" -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
 mykrobe panels update_metadata
 mykrobe panels update_species all
