86a96be186cc9c7f4867faca3333dfbd51ac29a9
mholt <mholt@pacificbiosciences.com>
2025-06-10 06:50:21 -0700

    patch to compile without .git

--- recipes/aardvark/build.sh
+++ recipes/aardvark/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# no .git folder, so use a tag indicating conda build
+export CUSTOM_VERGEN_GIT_DESCRIBE="conda"
+
 # following recommendations of https://bioconda.github.io/contributor/guidelines.html#rust
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install -v --locked --no-track --root $PREFIX --path .
