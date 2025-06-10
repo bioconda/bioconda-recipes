59f284fb92aedfb615643351e8558a658bdbce8f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-09 20:55:11 -0500

    Update build.sh

--- recipes/m-party/build.sh
+++ recipes/m-party/build.sh
@@ -7,13 +7,13 @@ mkdir -p "$INSTALL_FOLDER/resources/Data/Tables"
 mkdir -p "$INSTALL_FOLDER/resources/Data/FASTA"
 mkdir -p $BIN_FOLDER
 
-cp -rf workflow/scripts $INSTALL_FOLDER/
+cp -rf workflow $INSTALL_FOLDER/
 cp -rf config $INSTALL_FOLDER/
 cp -rf resources/Data/Tables/* $INSTALL_FOLDER/resources/Data/Tables/
 cp -rf resources/Data/FASTA/* $INSTALL_FOLDER/resources/Data/FASTA/
 install -v -m 755 m-party.py "$INSTALL_FOLDER"
 
-install -v -m 755 $INSTALL_FOLDER/scripts/*.py "$BIN_FOLDER"
+install -v -m 755 $INSTALL_FOLDER/workflow/scripts/*.py "$BIN_FOLDER"
 # for testing
 cp -rf resources/Data/FASTA/polymerase_DB.fasta $INSTALL_FOLDER
 cp -rf resources/Data/FASTA/human_gut_metagenome.fasta $INSTALL_FOLDER/resources/Data/FASTA/
