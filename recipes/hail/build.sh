bfd54a823b754b1c3349532b795e1ce28fd8bbea
mencian <joshua.zhuang@yahoo.com>
2025-06-10 01:43:27 -0500

    Rebuild hail recipe

--- recipes/hail/build.sh
+++ recipes/hail/build.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 # Build instructions
 # https://hail.is/docs/0.2/getting_started_developing.html#requirements
-pushd $SRC_DIR/hail
-make install HAIL_COMPILE_NATIVES='build' -j ${CPU_COUNT}
+pushd ${SRC_DIR}/hail
+make install HAIL_COMPILE_NATIVES='build' -j"${CPU_COUNT}"
 popd
