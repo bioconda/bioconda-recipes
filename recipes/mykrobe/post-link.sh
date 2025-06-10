1294abb04d99cb55e1eccf74d4eeb7e66186c0ad
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-09 19:56:23 -0500

    Update post-link.sh

--- recipes/mykrobe/post-link.sh
+++ recipes/mykrobe/post-link.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mykrobe panels update_metadata
-
-mykrobe panels update_species all
+if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
+  mykrobe panels --help
+else
+  mykrobe panels update_metadata
+  mykrobe panels update_species all
+fi
