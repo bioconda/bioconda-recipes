d10a05b39756b73daace3bc0f06a438279ea5269
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-09 22:25:19 -0500

    Update build.sh

--- recipes/m-party/build.sh
+++ recipes/m-party/build.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-INSTALL_FOLDER="$PREFIX/share"
 BIN_FOLDER="$PREFIX/bin"
-mkdir -p $INSTALL_FOLDER
-mkdir -p "$INSTALL_FOLDER/resources/Data/Tables"
-mkdir -p "$INSTALL_FOLDER/resources/Data/FASTA"
-mkdir -p $BIN_FOLDER
+mkdir -p $BIN_FOLDER/workflow
 
-cp -rf workflow $INSTALL_FOLDER/
-cp -rf config $INSTALL_FOLDER/
-cp -rf resources/Data/Tables/* $INSTALL_FOLDER/resources/Data/Tables/
-cp -rf resources/Data/FASTA/* $INSTALL_FOLDER/resources/Data/FASTA/
-install -v -m 755 m-party.py "$INSTALL_FOLDER"
+cp -rf workflow $BIN_FOLDER/
+cp -rf config $BIN_FOLDER/
+cp -rf m-party.py "$BIN_FOLDER"
 
-install -v -m 755 $INSTALL_FOLDER/workflow/scripts/*.py "$BIN_FOLDER"
-# for testing
-cp -rf resources/Data/FASTA/polymerase_DB.fasta $INSTALL_FOLDER
-cp -rf resources/Data/FASTA/human_gut_metagenome.fasta $INSTALL_FOLDER/resources/Data/FASTA/
+echo "print bin folder"
+ls -la $BIN_FOLDER
 
-ls -l $INSTALL_FOLDER
-ls -l $BIN_FOLDER
-cp -f $INSTALL_FOLDER/m-party.py $BIN_FOLDER/m-party
+echo "print workflow folder"
+ls -la $BIN_FOLDER/workflow
+
+echo "print config folder"
+ls -la $BIN_FOLDER/config
+
+chmod 755 $BIN_FOLDER/m-party.py
+ln -sf $BIN_FOLDER/m-party.py $BIN_FOLDER/m-party
