51130a100241db4dc84a724af590aeb7fdfe587c
UENO, M. <eunosium-1128@live.jp>
2025-06-10 17:17:35 +0900

    libccp4: Add gnuconfig for build dependencies and  update build script

--- recipes/libccp4/build.sh
+++ recipes/libccp4/build.sh
@@ -2,13 +2,16 @@
 
 set -exo pipefail
 
+cp ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
 sed -i.bak 's/int putenv ();/extern int putenv (char *);/' ccp4/library_utils.c
 
 ./configure \
     --prefix=${PREFIX} \
     --enable-shared \
+    --enable-fortran \
     --disable-static \
-    --disable-fortran
+    --disable-debug \
+    --disable-dependency-tracking
 
 make -j${CPU_COUNT}
 make install
