bdf98264152e44d65d06e55870d034b6f15fce4c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-09 19:56:35 -0500

    Delete recipes/mykrobe/post-link.sh

--- recipes/mykrobe/post-link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
-  mykrobe panels --help
-else
-  mykrobe panels update_metadata
-  mykrobe panels update_species all
-fi
