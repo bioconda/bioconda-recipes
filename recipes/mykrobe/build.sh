811b234b58917385e08994e8f300ecba3c67db30
mencian <joshua.zhuang@yahoo.com>
2025-06-09 19:07:08 -0500

    edit build.sh

--- recipes/mykrobe/build.sh
+++ recipes/mykrobe/build.sh
@@ -12,9 +12,9 @@ export CXX="${CC} -fcommon"
 # download phelim's fork of mccortex
 MCCORTEX_DIR=mccortex
 MCCORTEX_GIT="https://github.com/phelimb/mccortex"
-mkdir "$MCCORTEX_DIR"
-git clone --recursive -b geno_kmer_count "$MCCORTEX_GIT" "$MCCORTEX_DIR"
-cd "$MCCORTEX_DIR" || exit 1
+mkdir -p "${MCCORTEX_DIR}"
+git clone --recursive -b geno_kmer_count "${MCCORTEX_GIT}" "${MCCORTEX_DIR}"
+cd "${MCCORTEX_DIR}" || exit 1
 
 # comment this out as it overrides my LDFLAGS variable
 sed -i.bak 's/LDFLAGS  =/#LDFLAGS  =/' libs/htslib/Makefile
@@ -31,17 +31,15 @@ for make_file in libs/string_buffer/Makefile $(find libs/seq_file -name Makefile
     sed -i.bak 's/-lz/-lz $(LDFLAGS)/' "$make_file"
 done
 
-if [[ $(arch) != "x86_64" ]]; then
+if [[ "$(uname -m)" != "x86_64" ]]; then
     sed -i.bak 's/-m64//' Makefile
 fi
 
 make MAXK=31 CC="${CC}" -j"${CPU_COUNT}"
-install -v -m 755 bin/mccortex31 ../src/mykrobe/cortex/
+install -v -m 755 bin/mccortex31 "../src/mykrobe/cortex"
 cd ../ || exit 1
 
 sed -i.bak 's|find_packages|find_namespace_packages|' setup.py
 rm -rf *.bak
 
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
-mykrobe panels update_metadata
-mykrobe panels update_species all
---
+++ recipes/mykrobe/post-link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mykrobe panels update_metadata
+
+mykrobe panels update_species all
