081c9b8f394ef8f269c36e52325be65206575e73
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-11 15:27:22 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -3,7 +3,7 @@
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
-export PYO3_PYTHON="${PYTHON}"
+#export PYO3_PYTHON="${PYTHON}"
 unamestr=$(uname -s)
 
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
@@ -12,17 +12,19 @@ sed -i.bak 's|"0.18.1"|"0.21.2"|' rust/Cargo.toml
 sed -i.bak 's|"0.18.3"|"0.21.2"|' rust/Cargo.toml
 rm -rf rust/*.bak
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  export CFLAGS="${CFLAGS} -lstdc++"
-fi
-
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile=minimal -y
 export PATH="${HOME}/.cargo/bin:${PATH}"
 
+cd blobtk/rust
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-if [[ "${unamestr}" == "Darwin" ]]; then
-  RUSTFLAGS="-C link-args=-Wl,-undefined,dynamic_lookup" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
-else
-  RUSTFLAGS="-C target-feature=-crt-static -L ${PREFIX}/lib64" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
-fi
+maturin build --release --strip -b pyo3 --interpreter "${PYTHON}"
+
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
+
+#if [[ "${unamestr}" == "Darwin" ]]; then
+  #RUSTFLAGS="-C link-args=-Wl,-undefined,dynamic_lookup" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
+#else
+  #RUSTFLAGS="-C target-feature=-crt-static -L ${PREFIX}/lib64" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
+#fi
