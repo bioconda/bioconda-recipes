6ef3f15b5a73c42281a863a54c3b01cd0685356e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-11 16:13:29 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -3,7 +3,7 @@
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
-#export PYO3_PYTHON="${PYTHON}"
+export PYO3_PYTHON="${PYTHON}"
 unamestr=$(uname -s)
 
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
