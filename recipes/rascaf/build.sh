91bc06d982a1a87288728864c3fe77d132f91f83
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-12 16:28:08 -0500

    Update build.sh

--- recipes/rascaf/build.sh
+++ recipes/rascaf/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+mkdir -p "${PREFIX}/bin"
+
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -std=c++14 -O3 -Wformat -Wno-reserved-user-defined-literal"
-
-mkdir -p "${PREFIX}/bin"
+export CXXFLAGS="${CXXFLAGS} -std=c++14 -O3 -Wformat -Wno-reserved-user-defined-literal -Wno-writable-strings -Wno-literal-suffix"
 
 sed -i.bak '/char nucToNum/s/^/signed\ /g' defs.h
 sed -i.bak '/char numToNuc/s/^/signed\ /g' defs.h
@@ -12,6 +12,11 @@ sed -i.bak 's/extern char nucToNum/extern signed char nucToNum/g' KmerCode.hpp
 sed -i.bak 's/extern char numToNuc/extern signed char numToNuc/g' KmerCode.hpp
 sed -i.bak 's/extern char nucToNum/extern signed char nucToNum/g' genome.hpp
 sed -i.bak 's/extern char numToNuc/extern signed char numToNuc/g' genome.hpp
+sed -i.bak 's/-lpthread/-pthread/g' Makefile
+
+# Fix perl shebang
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' *.pl
+rm -rf *.bak
 
 make CXX="${CXX}" \
     CXXFLAGS="${CXXFLAGS}" LINKPATH="${LDFLAGS}" \
