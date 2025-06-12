2a19f7f856414901e5efab594d03f24fc941d363
mencian <joshua.zhuang@yahoo.com>
2025-06-12 18:30:12 -0500

    Rebuild mbg recipe

--- recipes/mbg/build.sh
+++ recipes/mbg/build.sh
@@ -1,8 +1,18 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -xe
 
-cd $SRC_DIR
-make -j ${CPU_COUNT} bin/MBG
-mkdir -p $PREFIX/bin
-cp bin/MBG $PREFIX/bin
+mkdir -p "${PREFIX}/bin"
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+if [[ `uname -s` == "Darwin" ]]; then
+	sed -i.bak 's|-Wl,-Bstatic||' makefile
+fi
+
+sed -i.bak 's|-Izstr/src|-I$(PREFIX)/include -Izstr/src|' makefile
+sed -i.bak 's|LIBS=`|LIBS=-L$(PREFIX)/lib `|' makefile
+sed -i.bak 's|-lpthread||' makefile
+rm -rf *.bak
+
+make -j"${CPU_COUNT}" bin/MBG
+install -v -m 755 bin/MBG "${PREFIX}/bin"
