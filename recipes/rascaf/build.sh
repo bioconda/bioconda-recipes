0af33255f6eec345b9504220b40498497a2d1cfa
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-12 16:04:15 -0500

    Update build.sh

--- recipes/rascaf/build.sh
+++ recipes/rascaf/build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-sed -i '/char nucToNum/s/^/signed\ /g' defs.h
-sed -i '/char numToNuc/s/^/signed\ /g' defs.h
-sed -i 's/extern char nucToNum/extern signed char nucToNum/g' KmerCode.hpp
-sed -i 's/extern char numToNuc/extern signed char numToNuc/g' KmerCode.hpp
-sed -i 's/extern char nucToNum/extern signed char nucToNum/g' genome.hpp
-sed -i 's/extern char numToNuc/extern signed char numToNuc/g' genome.hpp
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -std=c++14 -O3 -Wformat -Wno-reserved-user-defined-literal"
 
-make \
-    CXX="${CXX}" \
-    CXXFLAGS="${CXXFLAGS} -Wformat -O3" \
-    LINKPATH="${LDFLAGS}"
-install -d "${PREFIX}/bin"
-install \
+mkdir -p "${PREFIX}/bin"
+
+sed -i.bak '/char nucToNum/s/^/signed\ /g' defs.h
+sed -i.bak '/char numToNuc/s/^/signed\ /g' defs.h
+sed -i.bak 's/extern char nucToNum/extern signed char nucToNum/g' KmerCode.hpp
+sed -i.bak 's/extern char numToNuc/extern signed char numToNuc/g' KmerCode.hpp
+sed -i.bak 's/extern char nucToNum/extern signed char nucToNum/g' genome.hpp
+sed -i.bak 's/extern char numToNuc/extern signed char numToNuc/g' genome.hpp
+
+make CXX="${CXX}" \
+    CXXFLAGS="${CXXFLAGS}" LINKPATH="${LDFLAGS}" \
+    -j"${CPU_COUNT}"
+
+install -v -m 755 \
     *.pl \
     rascaf \
     rascaf-join \
-    "${PREFIX}/bin/"
+    "${PREFIX}/bin"
