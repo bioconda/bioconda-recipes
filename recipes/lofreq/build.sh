f8dc0b3ed5108fadbfbb4bcdcce00b648fdf950f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-12 20:06:16 -0500

    Update build.sh

--- recipes/lofreq/build.sh
+++ recipes/lofreq/build.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 set -eu -o pipefail
 
-<<<<<<< aarch64-dbghaplo
-# use newer config.guess and config.sub that support osx-arm64
-cp ${RECIPE_DIR}/config.* .
-
-./configure --with-htslib=system --prefix=${PREFIX}
-make -j ${CPU_COUNT}
-=======
 export M4="${BUILD_PREFIX}/bin/m4"
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 
-if [[ "$(uname)" == Darwin ]]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
+
+# use newer config.guess and config.sub that support osx-arm64
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* .
 
 autoreconf -if
 ./configure --with-htslib=system --prefix="${PREFIX}" \
 	CC="${CC}" CFLAGS="${CFLAGS}" \
 	LDFLAGS="${LDFLAGS}" \
-	CPPFLAGS="${CPPFLAGS} -O3 -I${PREFIX}/include" \
+	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
 	PYTHON="${PYTHON}"
 
 make -j"${CPU_COUNT}"
->>>>>>> master
 make install
