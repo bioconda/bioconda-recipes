8e885369c90f35808ddc4ea30f0334c84342f2e8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-12 21:30:24 -0500

    Update build.sh

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -5,18 +5,18 @@ set -euo pipefail
 export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -std=c++14"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-sed -i.bak -e "s|INCLUDE\s*:=|INCLUDE := -I${PREFIX}/include |" \
-           -e "s|LIB_DEPENDENCIES\s*:=\s*-lpopt|LIB_DEPENDENCIES := -L${PREFIX}/lib -lpopt|" \
-           -e "s|g++|${CXX}|" \
-           -e "s|-I$(BREW_PREFIX)|-I${PREFIX}|" \
-           -e "s|-L$(BREW_PREFIX)|-L${PREFIX}|" \
+sed -i.bak -e 's|:= $(add|:= -I$(PREFIX)/include $(add|' \
+           -e 's|-lpopt -lm|-L$(PREFIX)/lib -lpopt -lm|' \
+           -e 's|g++|$(CXX)|' \
+           -e 's|-I$(BREW_PREFIX)|-I$(PREFIX)|' \
+           -e 's|-L$(BREW_PREFIX)|-L$(PREFIX)|' \
            Makefile
 rm -rf *.bak
 
 install -d ${PREFIX}/share/rDock
 cp -rf lib data tests ${PREFIX}/share/rDock
 rm -f lib/*
-make CXX="${CXX}" -j"${CPU_COUNT}"
+make CXX="${CXX}" CXX_EXTRA_FLAGS="-I${PREFIX}/include" PREFIX="${PREFIX}" -j"${CPU_COUNT}"
 PREFIX="${PREFIX}" make install
 
 mkdir -p ${PREFIX}/etc/conda/activate.d
