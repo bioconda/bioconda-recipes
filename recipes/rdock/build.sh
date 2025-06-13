0403fa8940a27cd19d30ab2ec31771c403b1698b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-12 21:16:55 -0500

    Update build.sh

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -11,6 +11,7 @@ sed -i.bak -e "s|INCLUDE\s*:=|INCLUDE := -I${PREFIX}/include |" \
            -e "s|-I$(BREW_PREFIX)|-I${PREFIX}|" \
            -e "s|-L$(BREW_PREFIX)|-L${PREFIX}|" \
            Makefile
+rm -rf *.bak
 
 install -d ${PREFIX}/share/rDock
 cp -rf lib data tests ${PREFIX}/share/rDock
@@ -20,5 +21,5 @@ PREFIX="${PREFIX}" make install
 
 mkdir -p ${PREFIX}/etc/conda/activate.d
 mkdir -p ${PREFIX}/etc/conda/deactivate.d
-cp -f ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/activate.sh
-cp -f ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/deactivate.sh
+cp -f ${RECIPE_DIR}/activate.sh "${PREFIX}/etc/conda/activate.d/activate.sh"
+cp -f ${RECIPE_DIR}/deactivate.sh "${PREFIX}/etc/conda/deactivate.d/deactivate.sh"
