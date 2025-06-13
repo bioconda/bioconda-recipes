21457ef111be64c4a51df070832bfff2591d1f4b
mencian <joshua.zhuang@yahoo.com>
2025-06-12 21:06:31 -0500

    Rebuild rdock recipe

--- recipes/rdock/build.sh
+++ recipes/rdock/build.sh
@@ -5,17 +5,20 @@ set -euo pipefail
 export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -std=c++14"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-sed -i.bak -e "s|INCLUDE\s*:=|INCLUDE := -I${PREFIX}/include |" \
-           -e "s|LIB_DEPENDENCIES\s*:=\s*-lpopt|LIB_DEPENDENCIES := -L${PREFIX}/lib -lpopt|" \
+sed -i.bak -e "s|INCLUDE\s*:=|INCLUDE := -I$(PREFIX)/include |" \
+           -e "s|LIB_DEPENDENCIES\s*:=\s*-lpopt|LIB_DEPENDENCIES := -L$(PREFIX)/lib -lpopt|" \
+           -e 's|g++|$(CXX)|' \
+           -e 's|-I$(BREW_PREFIX)|-I$(PREFIX)|' \
+           -e 's|-L$(BREW_PREFIX)|-L$(PREFIX)|' \
            Makefile
 
 install -d ${PREFIX}/share/rDock
-cp -r lib data tests ${PREFIX}/share/rDock
-rm lib/*
-make -j${CPU_COUNT}
-PREFIX=${PREFIX} make install
+cp -rf lib data tests ${PREFIX}/share/rDock
+rm -f lib/*
+make CXX="${CXX}" -j"${CPU_COUNT}"
+PREFIX="${PREFIX}" make install
 
 mkdir -p ${PREFIX}/etc/conda/activate.d
 mkdir -p ${PREFIX}/etc/conda/deactivate.d
-cp ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/activate.sh
-cp ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/deactivate.sh
+cp -f ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/activate.sh
+cp -f ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/deactivate.sh
