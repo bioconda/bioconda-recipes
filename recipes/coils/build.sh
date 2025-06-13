d169a6ea029089df1a225110662f65c4d170b305
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-12 23:24:14 -0500

    Update build.sh

--- recipes/coils/build.sh
+++ recipes/coils/build.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 set -x -e
 
 # Coils Dir
-COILS_DIR=${PREFIX}/share/coils
+COILS_DIR="${PREFIX}/share/coils"
 #list programs
 COILS_PROGRAMS="coils-svr.pl coils-wrap.pl"
 
-
-
+mkdir -p ${PREFIX}/bin
+mkdir -p ${COILS_DIR}
 
 # remove the precompilated one
-rm ncoils-linux
+rm -f ncoils-linux
 
-sed -i 's/^main(/int main(/' ncoils.c
-sed -i '1i#include <string.h>' ncoils.c
-sed -i '1i#include <string.h>' read_matrix.c
-sed -i -r 's/strncmp$([^,]+,[^,]+),([0-9]+)$/strncmp(\1,(size_t)\2)/g' read_matrix.c
+sed -i.bak 's/^main(/int main(/' ncoils.c
+sed -i.bak '1i#include <string.h>' ncoils.c
+sed -i.bak '1i#include <string.h>' read_matrix.c
+sed -i.bak -r 's/strncmp$([^,]+,[^,]+),([0-9]+)$/strncmp(\1,(size_t)\2)/g' read_matrix.c
 
 #compile
-${CC} -O2 -I. -o ncoils-osf ncoils.c read_matrix.c -lm
+${CC} -O3 -I. -I${PREFIX}/include -o ncoils-osf ncoils.c read_matrix.c -lm
 
 # get the name of the ncoils program build
 NCOILS=$(ls ncoils-*)
@@ -27,26 +27,22 @@ NCOILS=$(ls ncoils-*)
 ls -l
 
 # copy tools in the bin
-mkdir -p ${PREFIX}/bin
-mkdir -p ${COILS_DIR}
-cp -r * ${COILS_DIR}
+cp -rf * ${COILS_DIR}
 
 # Add main tools
-cp ${NCOILS} ${PREFIX}/bin/ncoils
-chmod a+x ${PREFIX}/bin/ncoils
+install -v -m 755 ${NCOILS} ${PREFIX}/bin
 
 # Add extra programs
-for PROGRAM in ${COILS_PROGRAMS} ; do
-  cp ${PROGRAM} ${PREFIX}/bin
-	chmod a+x ${PREFIX}/bin/${PROGRAM}
+for PROGRAM in ${COILS_PROGRAMS}; do
+	install -v -m 755 ${PROGRAM} ${PREFIX}/bin
 done
 
 # You then need to set an environment variable $COILSDIR to the coils folder
 # These coils folder is installed as runtime deps, but the paths to it need to
 # to reflect the current environment's $PREFIX/bin dir.
 # export COILSDIR as ENV variable
-mkdir -p ${PREFIX}/etc/conda/activate.d/
-echo "export COILSDIR=${COILS_DIR}" > ${PREFIX}/etc/conda/activate.d/${PKG_NAME}.sh
+mkdir -p ${PREFIX}/etc/conda/activate.d
+echo "export COILSDIR=${COILS_DIR}" > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}.sh"
 
-mkdir -p ${PREFIX}/etc/conda/deactivate.d/
-echo "unset COILSDIR" > ${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}.sh
+mkdir -p ${PREFIX}/etc/conda/deactivate.d
+echo "unset COILSDIR" > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}.sh"
