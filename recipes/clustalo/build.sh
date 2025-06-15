caebdfdafbe31ec5a095d03278db246d243629db
mencian <joshua.zhuang@yahoo.com>
2025-06-15 18:04:40 -0500

    clustalo: add arm64 build

--- recipes/clustalo/build.sh
+++ recipes/clustalo/build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # use newer config.guess and config.sub that support linux-aarch64
-cp ${RECIPE_DIR}/config.* .
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* .
+mkdir -p ${PREFIX}/bin
 
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
     # clang doesn't accept -fopenmp and there's no clear way around that
-    ./configure --prefix=$PREFIX
+    ./configure --prefix="${PREFIX}"
 else
-    ./configure --prefix=$PREFIX OPENMP_CFLAGS='-fopenmp' CFLAGS='-DHAVE_OPENMP'
+    ./configure --prefix="${PREFIX}" OPENMP_CFLAGS='-fopenmp' CFLAGS='-DHAVE_OPENMP'
 fi
-make -j ${CPU_COUNT}
+make -j"${CPU_COUNT}"
 
-mkdir -p $PREFIX/bin
-cp $SRC_DIR/src/clustalo $PREFIX/bin/$PKG_NAME
-chmod +x $PREFIX/bin/$PKG_NAME
+install -v -m 755 $SRC_DIR/src/clustalo "$PREFIX/bin"
