54f34e9795e9a0276ed29d25cd75db7cbea8ca7b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-17 14:17:44 -0500

    Update build.sh

--- recipes/faqcs/build.sh
+++ recipes/faqcs/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p $PREFIX/bin
-cd $SRC_DIR
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
     echo "Installing FaQCs for OSX."
-    make cc=$CC$ CC=$CLANGXX LIBS="-L${PREFIX}/lib -lm -lz" OPENMP=""
+    make cc=$CC$ CC="${CLANGXX}" LIBS="-L${PREFIX}/lib -lm -lz" OPENMP=""
     cp FaQCs $PREFIX/bin
 else 
     echo "Installing FaQCs for UNIX/Linux."
-    make cc=$GCC CC=$CXX LIBS="-L${PREFIX}/lib -lm -lz"
-    cp FaQCs $PREFIX/bin
+    make cc="${GCC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz"
+    install -v -m 755 FaQCs $PREFIX/bin
 fi
