fd431e42b882d25f525ed4820c39064d4fb357b7
mencian <joshua.zhuang@yahoo.com>
2025-06-17 15:23:17 -0500

    add patch

--- recipes/faqcs/build.sh
+++ recipes/faqcs/build.sh
@@ -10,17 +10,19 @@ mkdir -p "${PREFIX}/bin"
 sed -i.bak 's|gcc|$(CC)|' Makefile
 sed -i.bak 's|g++|$(CXX)|' Makefile
 sed -i.bak 's|-I.|-I. -I$(PREFIX)/include|' Makefile
+
 if [[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]]; then
-    sed -i.bak 's|-msse2||' Makefile
+	cp -rf "${RECIPE_DIR}/sse2neon.h" "${SRC_DIR}"
+	sed -i.bak 's|-msse2||' Makefile
 fi
 rm -rf *.bak
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-    echo "Installing FaQCs for OSX."
-    make cc="${CC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz" OPENMP="" -j"${CPU_COUNT}"
-    install -v -m 755 FaQCs "${PREFIX}/bin"
+	echo "Installing FaQCs for OSX."
+	make cc="${CC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz" OPENMP="" -j"${CPU_COUNT}"
+	install -v -m 755 FaQCs "${PREFIX}/bin"
 else
-    echo "Installing FaQCs for UNIX/Linux."
-    make cc="${CC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz" -j"${CPU_COUNT}"
-    install -v -m 755 FaQCs "${PREFIX}/bin"
+	echo "Installing FaQCs for UNIX/Linux."
+	make cc="${CC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz" -j"${CPU_COUNT}"
+	install -v -m 755 FaQCs "${PREFIX}/bin"
 fi
