a494f935d8c2e1c975a427edeb977d668420578c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-17 14:37:35 -0500

    Update build.sh

--- recipes/faqcs/build.sh
+++ recipes/faqcs/build.sh
@@ -5,13 +5,22 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-mkdir -p $PREFIX/bin
+mkdir -p ${PREFIX}/bin
+
+sed -i.bak 's|gcc|$(CC)|' Makefile
+sed -i.bak 's|g++|$(CXX)|' Makefile
+sed -i.bak 's|-I.|-I. -I$(PREFIX)/include|' Makefile
+if [[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]]; then
+    sed -i.bak 's|-msse2||' Makefile
+fi
+rm -rf *.bak
+
 if [[ "$(uname -s)" == "Darwin" ]]; then
     echo "Installing FaQCs for OSX."
-    make cc=$CC$ CC="${CLANGXX}" LIBS="-L${PREFIX}/lib -lm -lz" OPENMP=""
-    cp FaQCs $PREFIX/bin
-else 
+    make cc="${CC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz" OPENMP="" -j"${CPU_COUNT}"
+    install -v -m 755 FaQCs "$PREFIX/bin"
+else
     echo "Installing FaQCs for UNIX/Linux."
-    make cc="${GCC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz"
-    install -v -m 755 FaQCs $PREFIX/bin
+    make cc="${CC}" CC="${CXX}" LIBS="-L${PREFIX}/lib -lm -lz" -j"${CPU_COUNT}"
+    install -v -m 755 FaQCs "$PREFIX/bin"
 fi
