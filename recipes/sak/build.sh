6ef3b3a4c67f7c0c622011ddbec5b144682a0ce2
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-17 20:55:41 -0500

    Update build.sh

--- recipes/sak/build.sh
+++ recipes/sak/build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-if [[ ${target_platform} == "linux-aarch64" ]]; then
-
-mkdir -p $SRC_DIR/install
-
-cd $SRC_DIR
-
-mkdir build && cd build 
-
-cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install
-
-make -j 16 && make install
-
-cd $SRC_DIR/install/bin
+cd bin
 
 binaries="\
 sak \
@@ -20,18 +8,6 @@ sak \
 
 mkdir -p $PREFIX/bin
 
-for i in $binaries; do cp $SRC_DIR/install/bin/$i $PREFIX/bin && chmod a+x $PREFIX/bin/$i; done
-
-else
-
-cd $SRC_DIR/bin
-
-binaries="\
-sak \
-"
-
-mkdir -p $PREFIX/bin
-
-for i in $binaries; do cp $SRC_DIR/bin/$i $PREFIX/bin && chmod a+x $PREFIX/bin/$i; done
-
-fi
+for i in ${binaries}; do
+  install -v -m 755 $SRC_DIR/bin/$i "${PREFIX/bin}";
+done
