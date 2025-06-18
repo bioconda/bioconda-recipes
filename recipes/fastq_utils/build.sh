1e3f28daf2feca8067a8fb42d18206d40cf5be2b
mencian <joshua.zhuang@yahoo.com>
2025-06-18 14:39:14 -0500

    add patches

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -6,10 +6,10 @@ export C_INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/bam"
 export CPP_INCLUDE_PATH="${PREFIX}/include"
 export CXX_INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -L${PREFIX}/lib"
 
-sed -i.bak "s/gcc/\$\(CC\)/g" src/Makefile
-make install
+./install_deps.sh
+
+make install CC="${CC}" -j"${CPU_COUNT}"
 
 install -v -m 755 bin/* "${PREFIX}/bin"
