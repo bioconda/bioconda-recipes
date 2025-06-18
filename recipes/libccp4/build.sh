81657866f03f0dfdd509de379d92735d847798bc
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-17 23:19:34 -0500

    Update build.sh

--- recipes/libccp4/build.sh
+++ recipes/libccp4/build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-
 set -exo pipefail
 
-cp ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
-sed -i.bak 's/int putenv ();/extern int putenv (char *);/' ccp4/library_utils.c
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
+sed -i.bak 's/int putenv ();/extern int putenv (char *);/' ccp4/library_utils.c
+rm -rf ccp4/*.bak
+
+autoreconf -if
 ./configure \
-    --prefix=${PREFIX} \
+    --prefix="${PREFIX}" \
     --enable-shared \
     --enable-fortran \
     --disable-static \
     --disable-debug \
-    --disable-dependency-tracking
+    --disable-dependency-tracking \
+    --enable-silent-rules \
+    --disable-option-checking \
+    CC="${CC}" CFLAGS="${CFLAGS}" \
+    CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" \
+    CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
 
-make -j${CPU_COUNT}
+make -j"${CPU_COUNT}"
 make install
