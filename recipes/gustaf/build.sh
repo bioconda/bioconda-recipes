a24df1f7e4a64bbe44fbbc8535caf92d9b3ca53e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-17 20:58:08 -0500

    Update build.sh

--- recipes/gustaf/build.sh
+++ recipes/gustaf/build.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
-if [[ ${target_platform} == "linux-aarch64" ]]; then
 
-mkdir -p $SRC_DIR/install
-
-cd $SRC_DIR
-
-mkdir build && cd build
-
-cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install
-
-make -j 16 && make install
-
-cd $SRC_DIR/install/bin
+mkdir -p $PREFIX/bin
+cd bin
 
 binaries="\
 gustaf \
 gustaf_mate_joining \
 "
 
-mkdir -p $PREFIX/bin
-
-for i in $binaries; do cp $SRC_DIR/install/bin/$i $PREFIX/bin && chmod a+x $PREFIX/bin/$i; done
-
-else
-
-cd $SRC_DIR/bin
-
-binaries="\
-gustaf \
-gustaf_mate_joining \
-"
-mkdir -p $PREFIX/bin
-
-for i in $binaries; do cp $SRC_DIR/bin/$i $PREFIX/bin/$i && chmod a+x $PREFIX/bin/$i; done
-
-fi
+for i in ${binaries}; do install -v -m 755 $SRC_DIR/bin/$i "$PREFIX/bin"; done
