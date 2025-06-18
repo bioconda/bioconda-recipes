34ddeb83227d078f9ecb4f9924d91ae6ecdcdc02
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-18 15:38:00 -0500

    Update build.sh

--- recipes/fastq_utils/build.sh
+++ recipes/fastq_utils/build.sh
@@ -8,6 +8,10 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
 
+tar -xf deps/samtools-0.1.19.tar.bz2
+
+patch -p1 < ${RECIPE_DIR}/0001-Makefile.patch
+
 ./install_deps.sh
 
 make install CC="${CC}" -j"${CPU_COUNT}"
