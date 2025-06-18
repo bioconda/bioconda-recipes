287969a7161332d39cf8a47c6141ed83afa2a56a
mencian <joshua.zhuang@yahoo.com>
2025-06-17 21:45:01 -0500

    clean up recipe

--- recipes/ema/build.sh
+++ recipes/ema/build.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -xeuo pipefail
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-make CC="$CC -fcommon" CXX="$CXX -fcommon" LDFLAGS="$LDFLAGS"
-mkdir -p $PREFIX/bin
-cp ema $PREFIX/bin
+
+export C_INCLUDE_PATH="${PREFIX}/include"
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+mkdir -p "${PREFIX}/bin"
+
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' Makefile
+	;;
+    arm64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' Makefile
+	;;
+esac
+
+
+make CC="${CC} -fcommon" CXX="${CXX} -fcommon" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+
+install -v -m 755 ema "${PREFIX}/bin"
