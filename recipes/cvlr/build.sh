ec13506b63a373327fd6486c74f94d7a624b2729
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-18 19:34:23 -0500

    Update build.sh

--- recipes/cvlr/build.sh
+++ recipes/cvlr/build.sh
@@ -7,6 +7,6 @@ sed -i.bak 's|gcc|$(CC)|' Makefile
 sed -i.bak 's|-Wall|-Wall -O3|' Makefile
 rm -rf *.bak
 
-make --file=makefile-conda.mk PREFIX="${PREFIX}" -j"${CPU_COUNT}"
+make PREFIX="${PREFIX}" -j"${CPU_COUNT}" --file="makefile-conda.mk"
 
 install -v -m 755 cvlr-cluster cvlr-meth-of-bam *py "${PREFIX}/bin"
