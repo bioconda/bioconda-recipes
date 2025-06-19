7f5818d4e8b464e90a72f73dc14e1d064adb97fe
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-06-19 16:09:12 +0800

    Update build.sh

--- recipes/htslib/1.10/build.sh
+++ recipes/htslib/1.10/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-export CFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
+export CC=aarch64-conda-linux-gnu-gcc
+export AR=aarch64-conda-linux-gnu-ar
+export RANLIB=aarch64-conda-linux-gnu-ranlib
+export CFLAGS="-I$PREFIX/include -O2"
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 EXTRA_ARGS=""
-if [[ "$(uname)" == "Linux" ]]; then
+if [[ "$target_platform" == linux-* ]]; then
   EXTRA_ARGS+=" --enable-gcs --enable-s3"
 fi
+
 ./configure \
-  --prefix=${PREFIX} \
+  --prefix=$PREFIX \
+  --host=aarch64-conda-linux-gnu \  # 关键：指定目标平台
   --enable-libcurl \
   --with-libdeflate \
   --enable-plugins \
-  ${EXTRA_ARGS}
-
+  $EXTRA_ARGS
 make install
