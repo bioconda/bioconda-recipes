1190701d7e39f104e6ecb7058250b97ab08017b4
mencian <joshua.zhuang@yahoo.com>
2025-06-19 16:48:58 -0500

    edit patch

--- recipes/demuxlet/build.sh
+++ recipes/demuxlet/build.sh
@@ -3,12 +3,25 @@ set -x -e
 
 export INCLUDE_PATH="${PREFIX}/include:${PREFIX}/include/htslib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-sign-compare -Wno-format"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -I${PREFIX}/include/htslib -ldl -ldeflate -fno-strict-aliasing"
 export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/htslib -ldl -ldeflate"
+export LC_ALL="en_US.UTF-8"
+
+mkdir -p m4
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* m4/
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	sed -i.bak 's|-lgomp|-lomp|' Makefile.am
+	rm -rf *.bak
+fi
 
 autoreconf -if
-CXXFLAGS="${CXXFLAGS}" ./configure --prefix="${PREFIX}"
+./configure --prefix="${PREFIX}" \
+	CC="${CC}" CFLAGS="${CFLAGS}" \
+	CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" \
+	--disable-option-checking --enable-silent-rules --disable-dependency-tracking
 
-CXXFLAGS="${CXXFLAGS}" PREFIX="${PREFIX}" make -j"${CPU_COUNT}"
+make -j"${CPU_COUNT}"
 make install
