f5280aa6032a422771e44f85b4b08e27c76450f9
mencian <joshua.zhuang@yahoo.com>
2025-06-19 02:08:42 -0500

    Update ucsc-liftover to v482

--- recipes/ucsc-liftover/build.sh
+++ recipes/ucsc-liftover/build.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-
 set -xe
 
 mkdir -p "${PREFIX}/bin"
-export MACHTYPE=$(uname -m)
-export BINDIR=$(pwd)/bin
+export MACHTYPE="$(uname -m)"
+export BINDIR="$(pwd)/bin"
+mkdir -p "$(pwd)/bin"
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 ${LDFLAGS}"
-export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include ${LDFLAGS}"
+export CFLAGS="${CFLAGS} -O3"
+export COPT="${COPT} ${CFLAGS}"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 export L="${LDFLAGS}"
-mkdir -p "${BINDIR}"
-(cd kent/src/lib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j "${CPU_COUNT}")
-(cd kent/src/htslib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j "${CPU_COUNT}")
-(cd kent/src/jkOwnLib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j "${CPU_COUNT}")
-(cd kent/src/hg/lib && make USE_HIC=0 CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j "${CPU_COUNT}")
-(cd kent/src/hg/liftOver && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j "${CPU_COUNT}")
-cp bin/liftOver "${PREFIX}/bin"
-chmod 0755 "${PREFIX}/bin/liftOver"
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+        export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+        export CFLAGS="${CFLAGS} -Wno-unused-command-line-argument"
+fi
+
+if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
+	rsync -aP rsync://hgdownload.cse.ucsc.edu/genome/admin/exe/macOSX.arm64/liftOver .
+	install -v -m 755 liftOver "${PREFIX}/bin"
+elif [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
+	rsync -aP rsync://hgdownload.cse.ucsc.edu/genome/admin/exe/macOSX.x86_64/liftOver .
+	install -v -m 755 liftOver "${PREFIX}/bin"
+elif [[ "$(uname -s)" == "Linux" && "$(uname -m)" == "x86_64" ]]; then
+	rsync -aP rsync://hgdownload.cse.ucsc.edu/genome/admin/exe/linux.x86_64/liftOver .
+	install -v -m 755 liftOver "${PREFIX}/bin"
+fi
+
+#(cd kent/src/lib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+#(cd kent/src/htslib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+#(cd kent/src/jkOwnLib && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+#(cd kent/src/hg/lib && make USE_HIC=0 CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+#(cd kent/src/hg/liftOver && make CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}")
+#install -v -m 0755 bin/liftOver "${PREFIX}/bin"
