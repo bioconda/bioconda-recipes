d445c691541222432b89281db3b9f53f1380e8b5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-18 21:52:46 -0500

    Update build.sh

--- recipes/regenie/build.sh
+++ recipes/regenie/build.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
+set -ex
 
-set -e
-set -x
-
-if [ "$(uname)" = "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
   # LDFLAGS fix: https://github.com/AnacondaRecipes/intel_repack-feedstock/issues/8
-  export LDFLAGS="-Wl,-pie -Wl,-headerpad_max_install_names -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
+  export LDFLAGS="${LDFLAGS} -Wl,-pie -Wl,-headerpad_max_install_names -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
 else
-  export LDFLAGS="-L$PREFIX/lib"
+  export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
   export MKL_THREADING_LAYER="GNU"
 fi
 # https://bioconda.github.io/troubleshooting.html#zlib-errors
-export CFLAGS="-I$PREFIX/include"
-export CPATH=${PREFIX}/include
+export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export CPATH="${PREFIX}/include"
 
 mkdir -p build
 
 if [[ ${target_platform} == "linux-aarch64" ]]; then
 # Conda does not yet support the MKL library for aarch64
-  sed -i '/set(MKLROOT/s/^/#/' CMakeLists.txt
-  sed -i '/include <optional>/a #\ \ \ \ include\ <cstdint>' external_libs/cxxopts/include/cxxopts.hpp
+  sed -i.bak '/set(MKLROOT/s/^/#/' CMakeLists.txt
+  sed -i.bak '/include <optional>/a #\ \ \ \ include\ <cstdint>' external_libs/cxxopts/include/cxxopts.hpp
 fi
 
-cmake \
+cmake -DCMAKE_BUILD_TYPE="Release" \
   -DBUILD_SHARED_LIBS:BOOL=ON \
-  -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-  -DCMAKE_BUILD_TYPE="Release" \
+  -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
+  -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
   -S "${SRC_DIR}" \
   -B build
 
-cmake --build build --target install -j "${CPU_COUNT}"
-
-#make  -C build -j1 regenie
-#make  -C build install
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
