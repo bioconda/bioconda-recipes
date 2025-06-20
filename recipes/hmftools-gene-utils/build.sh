064db9a37948cdf9bc8e013bb42bddbb72309773
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2025-06-20 13:18:42 +1000

    Replace redux typos with gene-utils

--- recipes/hmftools-gene-utils/build.sh
+++ recipes/hmftools-gene-utils/build.sh
@@ -7,6 +7,6 @@ TGT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 cd "${SRC_DIR}"
 mv gene-utils*.jar $TGT/gene-utils.jar
 
-cp $RECIPE_DIR/redux.sh $TGT/gene-utils
-ln -s $TGT/redux $PREFIX/bin
+cp $RECIPE_DIR/gene-utils.sh $TGT/gene-utils
+ln -s $TGT/gene-utils $PREFIX/bin
 chmod 0755 "${PREFIX}/bin/gene-utils"
