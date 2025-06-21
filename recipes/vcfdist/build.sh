88cd376e3ce2abe79966d25dccad7aabc3f58e0d
mencian <joshua.zhuang@yahoo.com>
2025-06-21 15:41:14 -0500

    vcfdist: add aarch64/arm64 builds

--- recipes/vcfdist/build.sh
+++ recipes/vcfdist/build.sh
@@ -2,8 +2,13 @@
 
 export CPATH="${PREFIX}/include"
 export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-cd src && make
+sed -i.bak 's|-lpthread|-pthread|' Makefile
+sed -i.bak 's|g++|$(CXX)|' Makefile
+
+cd src && make CXX="${CXX}" -j"${CPU_COUNT}"
 install -d "${PREFIX}/bin"
-install vcfdist "${PREFIX}/bin"
+install -v -m 755 vcfdist "${PREFIX}/bin"
