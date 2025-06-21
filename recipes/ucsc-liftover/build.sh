da78d863fc94b52d2b7bd209b6bbbecbdb974a7d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-21 18:36:41 -0500

    Update build.sh

--- recipes/ucsc-liftover/build.sh
+++ recipes/ucsc-liftover/build.sh
@@ -16,8 +16,12 @@ export L="${LDFLAGS}"
 
 sed -i.bak 's|g++|$(CXX)|' kent/src/optimalLeaf/makefile
 sed -i.bak 's|-g|-g -O3|' kent/src/optimalLeaf/makefile
+sed -i.bak 's|ar rcus|$(AR) rcs|' kent/src/optimalLeaf/makefile
 sed -i.bak 's|ar rcus|$(AR) rcs|' kent/src/jkOwnLib/makefile
 sed -i.bak 's|ld|$(LD)|' kent/src/hg/lib/straw/makefile
+sed -i.bak 's|ar rcus|$(AR) rcs|' kent/src/lib/makefile
+sed -i.bak 's|ar rcus|$(AR) rcs|' kent/src/hg/cgilib/makefile
+sed -i.bak 's|ar rcus|$(AR) rcs|' kent/src/hg/lib/makefile
 rm -rf kent/src/optimalLeaf/*.bak
 rm -rf kent/src/jkOwnLib/*.bak
 rm -rf kent/src/hg/lib/straw/*.bak
