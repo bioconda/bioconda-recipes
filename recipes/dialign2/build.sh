8af3ed7ba6c1dab2508b9f68092ff7b8f44f9d11
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-06-23 14:19:48 +0800

    Update build.sh

--- recipes/dialign2/build.sh
+++ recipes/dialign2/build.sh
@@ -9,7 +9,6 @@ mkdir -p ${PREFIX}/share/dialign2
 cd src
 
 sed -i.bak "s|strcpy ( dialign_dir , \"DIALIGN2_DIR\" );|strcpy ( par_dir , \""${PREFIX}"/share/dialign2\" );|g" dialign.c
-
 make CC="${CC}"
 mv dialign2-2 ${PREFIX}/bin/dialign2-2
 
