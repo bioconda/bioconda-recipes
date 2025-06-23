58756992f6c5195bc2e76a03c3c4b5ac4eff2639
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-23 18:07:57 -0500

    Update build.sh

--- recipes/bio-ripser/build.sh
+++ recipes/bio-ripser/build.sh
@@ -5,6 +5,6 @@ mkdir -p ${PREFIX}/bin
 sed -i.bak 's/c++ \-std=c++11/\$(CXX) \-std=c++14/g' Makefile
 rm -rf *.bak
 
-make all -j"${CPU_COUNT}"
+make all CXX="${CXX}" -j"${CPU_COUNT}"
 
 install -v -m 755 ripser ripser-debug ripser-coeff "${PREFIX}/bin"
