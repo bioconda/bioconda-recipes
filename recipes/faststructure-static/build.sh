9200ffd0ab760bd589948ddcab74caba105f8fc8
Afonso Santos <inbox@fonors.me>
2025-06-24 21:37:57 +0100

    Change fastStructure to Python 3 before attempting build

--- recipes/faststructure-static/build.sh
+++ recipes/faststructure-static/build.sh
@@ -4,12 +4,10 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PREFIX}/lib
 export CFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
-cp vars/*.h .
-cp vars/*.pxd .
-cp vars/*.pyx .
-cp vars/*.c .
-
+cd vars
+python setup.py build_ext --inplace
+cd ..
 python setup.py build_ext --inplace
 
-mkdir -p ${PREFIX}/bin/fastStructure
-mv * ${PREFIX}/bin/fastStructure
+wget https://gitlab.com/StuntsPT/Structure_threader/-/raw/master/helper_scripts/structure.spec
+pyinstaller structure.spec
