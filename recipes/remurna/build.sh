8400abff2c71ca8903442ff8a8f236509a3f19b8
mencian <joshua.zhuang@yahoo.com>
2025-06-23 22:37:06 -0500

    remove remuRNA from blacklist

--- recipes/remurna/build.sh
+++ recipes/remurna/build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
-mkdir -p ${PREFIX}/lib
-
-cd remuRNA
+mkdir -p ${PREFIX}/share/remurna
 
 if [[ ${target_platform} == "linux-aarch64" ]]; then
-  sed -i 's/\-m64/\-mabi=lp64/g' Makefile
+	sed -i.bak 's/\-m64/\-mabi=lp64/g' Makefile
 fi
-make
+
+make CC="${CC}" -j"${CPU_COUNT}"
 
 # Copy binery file since there is no INSTALL part in the makefile
-cp -R remuRNA ${PREFIX}/bin
+#cp -R remuRNA ${PREFIX}/bin
 # Copy data folder. not sure if putting this folder in the main conda folder is "wise", but this tool won't work otherwise
-cp -R data/ ${PREFIX}/lib
-ln -s ${PREFIX}/lib/data ${PREFIX}/bin/data
+cp -R data/ ${PREFIX}/share/remurna
+ln -s ${PREFIX}/share/remurna/data ${PREFIX}/bin/data
 
 # Preparing test files
 printf ">gi|56682960|ref|NM_000146.3| Homo sapiens ferritin, light polypeptide (FTL), mRNA\nGCAGTTCGGCGGTCCCGCGGGTCTGTCTCTTGCTTCAACAGTGTTTGGACGGAACAGATCCGGGGACTCTCTTCCAGCCTCCGACCGCCCTCCGATTTCCTCTCCGCTTGCAACCTCCGGGACCATCTTCTCGGCCATCTCCTGCTTCTGGGACCTGCCAGCACCGTTTTTGTGGTTAGCTCCTTCTTGCCAACCAACCA\n*A3G" > input.fa
