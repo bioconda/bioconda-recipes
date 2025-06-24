38c97e388bc42512d532ddec78b1b9969c91300c
UENO, M. <eunosium-1128@live.jp>
2025-06-24 16:54:33 +0000

    Update coot-headless build and meta configuration

--- recipes/coot-headless/build.sh
+++ recipes/coot-headless/build.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-cmake -S . -B build \
+cmake -S . -B build -G Ninja \
   ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
   -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
@@ -12,7 +12,11 @@ cmake -S . -B build \
   -DBOOST_ROOT="${PREFIX}" \
   -DEIGEN3_INCLUDE_DIR="${PREFIX}/include/eigen3" \
   -DGSL_ROOT_DIR="${PREFIX}" \
-  -DRDKIT_ROOT="${PREFIX}"
+  -DRDKIT_ROOT="${PREFIX}" \
+  -DFFTW2_INCLUDE_DIRS="${PREFIX}/fftw2/include" \
+  -DFFTW2_LIBRARY="${PREFIX}/fftw2/lib/libfftw.so" \
+  -DRFFTW2_LIBRARY="${PREFIX}/fftw2/lib/librfftw.so" \
+  -DPYTHON_SITE_PACKAGES="${SP_DIR}"
 
-cmake --build build --target coot_headless_api --parallel ${CPU_COUNT}
+cmake --build build --target coot_headless_api
 cmake --install build
