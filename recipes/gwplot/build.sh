8de9d1091ac14721de4f09cd47657ee1eaca7f3b
kcleal <clealk@cardiff.ac.uk>
2025-06-24 21:16:14 +0100

    Adding gwplot library

---
+++ recipes/gwplot/build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+set -e
+
+sed -i.bak 's|$(CONDA_PREFIX)/lib -Wl|$(CONDA_PREFIX)/lib|' Makefile
+
+# Set flags conditionally based on the OS type
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  SYSROOT_FLAGS="--sysroot=${BUILD_PREFIX}/${HOST}/sysroot"
+  CPPFLAGS="${CPPFLAGS} -I${BUILD_PREFIX}/${HOST}/sysroot/usr/include ${SYSROOT_FLAGS}"
+  LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib -L${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64 ${SYSROOT_FLAGS}"
+else
+  # No sysroot settings for macOS
+  SYSROOT_FLAGS=""
+  CPPFLAGS="${CPPFLAGS}"
+  LDFLAGS="${LDFLAGS} -L${PREFIX}"
+fi
+
+export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+export CPPFLAGS="${CPPFLAGS}"
+export LDFLAGS="${LDFLAGS}"
+export prefix="${PREFIX}"
+export SYSROOT_FLAGS="${SYSROOT_FLAGS}"
+
+pip install . -vv --config-settings=setup-args=-Dold_skia=true
