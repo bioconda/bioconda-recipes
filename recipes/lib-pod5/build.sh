17e0faa653b3393b4a1a910ebddff1a8b2170c15
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-24 06:12:20 -0500

    Update build.sh

--- recipes/lib-pod5/build.sh
+++ recipes/lib-pod5/build.sh
@@ -38,7 +38,8 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="$(pwd)" \
 
 cmake --build build --clean-first --target install -j "${CPU_COUNT}"
 
-${PYTHON} -m pip install *.whl --no-deps --no-build-isolation --no-cache-dir -vvv
+${PYTHON} -m pip install "build/c++/pod5_format_pybind/wheel_0.3.27/lib_pod5*.whl" --no-deps --no-build-isolation --no-cache-dir -vvv
 
-install -v lib/* "${PREFIX}/lib"
-install -v include/* "${PREFIX}/include"
+install -v lib/*.a "${PREFIX}/lib"
+install -v include/pod5_format/*.h "${PREFIX}/include"
+cp -rf include/pod5_format/svb16 "${PREFIX}/include"
