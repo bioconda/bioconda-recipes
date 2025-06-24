0220120af32f75c55b975bee54d415a34c5f76c3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-23 21:00:30 -0500

    Update build.sh

--- recipes/blobtk/build.sh
+++ recipes/blobtk/build.sh
@@ -9,7 +9,6 @@ export PYO3_PYTHON="${PYTHON}"
 export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
-unamestr=$(uname -s)
 
 sed -i.bak 's|maturin>=0.13,<0.14|maturin>=1.8.0,<2.0.0|' rust/pyproject.toml
 sed -i.bak 's|"0.40.2"|"0.47.0"|' rust/Cargo.toml
@@ -22,7 +21,7 @@ export PATH="${HOME}/.cargo/bin:${PATH}"
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-if [[ "${unamestr}" == "Darwin" ]]; then
+if [[ "$(uname -s)" == "Darwin" ]]; then
   export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -headerpad_max_install_names"
   RUSTFLAGS="-C link-args=-Wl,-undefined,dynamic_lookup" cargo install -v --no-track --locked --path rust/ --root "${PREFIX}"
 else
