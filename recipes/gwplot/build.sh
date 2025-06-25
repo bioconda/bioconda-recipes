7265e98f8b198f94b7783c180dfc1ff37b4cb90c
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2025-06-25 19:14:54 +0100

    Update build.sh

--- recipes/gwplot/build.sh
+++ recipes/gwplot/build.sh
@@ -29,14 +29,31 @@ else
   export OLD_SKIA=1
 fi
 
-git clone https://github.com/kcleal/gw.git
-cd gw
-sed -i.bak 's|$(CONDA_PREFIX)/lib -Wl|$(CONDA_PREFIX)/lib|' Makefile
-make prep
-make shared -j ${CPU_COUNT}
-ls libgw
-cd ../
-
 export MESONPY_SETUP_ARGS="-Dold_skia=${OLD_SKIA_FLAG}"
 
-$PYTHON -m pip install . -vv --config-settings="setup-args=-Dold_skia=${OLD_SKIA_FLAG}"
+# Initialize the gw submodule (if it's supposed to be a submodule)
+git submodule update --init --recursive
+
+# OR clone it if it's not a submodule
+if [ ! -d "gw" ]; then
+    git clone https://github.com/kcleal/gw.git
+fi
+#git clone https://github.com/kcleal/gw.git
+#cd gw
+#sed -i.bak 's|$(CONDA_PREFIX)/lib -Wl|$(CONDA_PREFIX)/lib|' Makefile
+#make prep
+#make shared -j ${CPU_COUNT}
+#ls libgw
+#cd ../
+
+$PYTHON -m pip wheel . --config-settings="setup-args=-Dold_skia=${OLD_SKIA_FLAG}" --no-deps
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    auditwheel -v repair dist/*.whl
+    $PYTHON -m pip install wheelhouse/*.whl -v
+else
+    delocate-wheel -v dist/*.whl
+    $PYTHON -m pip install dist/*.whl -v
+fi
+
+#$PYTHON -m pip install . -vv --config-settings="setup-args=-Dold_skia=${OLD_SKIA_FLAG}"
