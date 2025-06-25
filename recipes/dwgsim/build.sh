2d3861b5fe56f56b16b26231613695e3a0a3227c
root <root@localhost.localdomain>
2025-06-25 14:15:55 +0800

    update dwgsim

--- recipes/dwgsim/build.sh
+++ recipes/dwgsim/build.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-
 set -x -e -o pipefail
-
-make \
-  CC="${CC}" \
-  CFLAGS="${CPPFLAGS} ${CFLAGS} -g -Wall -O3 ${LDFLAGS}" \
-  LIBCURSES=-lncurses
+sed -i '4c\DFLAGS=         -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -D_USE_KNETFILE -DPACKAGE_VERSION=\\\\\\\"${PACKAGE_VERSION}\\\\\\\" -Dunreachable=dwgsim_unreachable' Makefile
+sed -i 's/unreachable/dwgsim_unreachable/g' src/dwgsim.c
+sed -i '8a CFLAGS += $(DFLAGS)' samtools/Makefile
+make  LIBCURSES=-lncurses
 
 mkdir -p "${PREFIX}/bin"
 
