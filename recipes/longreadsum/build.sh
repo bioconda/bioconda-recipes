12a09fc1861db9fe50e90c86a8bd24fac531a26e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-25 18:33:07 -0500

    Update build.sh

--- recipes/longreadsum/build.sh
+++ recipes/longreadsum/build.sh
@@ -3,19 +3,20 @@
 # Build the C++ library
 
 # Add the library path to the LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PREFIX}/lib
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
 
+make CXX="${CXX}" -j"${CPU_COUNT}"
 # Generate the SWIG files
-swig -c++ -python -outdir "${SRC_DIR}"/lib -I"${SRC_DIR}"/include -I"${PREFIX}"/include -o "${SRC_DIR}"/src/lrst_wrap.cpp "${SRC_DIR}"/src/lrst.i
+#swig -c++ -python -outdir "${SRC_DIR}"/lib -I"${SRC_DIR}"/include -I"${PREFIX}"/include -o "${SRC_DIR}"/src/lrst_wrap.cpp "${SRC_DIR}"/src/lrst.i
 
 # Generate the shared library
-$PYTHON setup.py -I"${PREFIX}"/include -L"${PREFIX}"/lib install
+$PYTHON setup.py -I"${PREFIX}/include" -L"${PREFIX}/lib" install
 
 # Create the src directory
 mkdir -p "${PREFIX}"/src
 
 # Copy source files to the bin directory
-cp -r "${SRC_DIR}"/src/*.py "${PREFIX}"/bin
+cp -r "${SRC_DIR}"/src/*.py "${PREFIX}/bin"
 
 # Copy the SWIG generated library to the lib directory
 cp -r "${SRC_DIR}"/lib/*.py "${PREFIX}"/lib
