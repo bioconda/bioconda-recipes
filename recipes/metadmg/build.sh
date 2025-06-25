73414c7537dfca2b8e3a014ec8129728223d2a23
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-06-25 11:25:02 +0200

    Add extract_reads to recipe

--- recipes/metadmg/build.sh
+++ recipes/metadmg/build.sh
@@ -5,10 +5,7 @@ mkdir -p "${PREFIX}/bin"
 export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-
-make clean
+export LDFLAGS="${LDFLAGS} -lz -lbz2 -llzma -L${PREFIX}/lib"
 
 make CC="${CC}" CXX="${CXX}" HTSSRC="systemwide" -j"${CPU_COUNT}"
-
 install -v -m 755 ./metaDMG-cpp ./misc/compressbam "${PREFIX}/bin"
