4eab9c3bfcf475bfdd26e0882418cedb41f74f3b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-25 13:33:25 -0500

    Update build.sh

--- recipes/dwgsim/build.sh
+++ recipes/dwgsim/build.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 set -x -e -o pipefail
-sed -i '4c\DFLAGS=         -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -D_USE_KNETFILE -DPACKAGE_VERSION=\\\\\\\"${PACKAGE_VERSION}\\\\\\\" -Dunreachable=dwgsim_unreachable' Makefile
-sed -i 's/unreachable/dwgsim_unreachable/g' src/dwgsim.c
-sed -i '8a CFLAGS += $(DFLAGS)' samtools/Makefile
-make  CC="${CC}" CFLAGS="${CPPFLAGS} ${CFLAGS} -g -Wall -O3 ${LDFLAGS}" LIBCURSES=-lncurses
+
+sed -i.bak '4c\DFLAGS=         -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -D_USE_KNETFILE -DPACKAGE_VERSION=\\\\\\\"${PACKAGE_VERSION}\\\\\\\" -Dunreachable=dwgsim_unreachable' Makefile
+sed -i.bak 's/unreachable/dwgsim_unreachable/g' src/dwgsim.c
+sed -i.bak '8a CFLAGS += $(DFLAGS)' samtools/Makefile
+make CC="${CC}" CFLAGS="${CPPFLAGS} ${CFLAGS} -g -Wall -O3 ${LDFLAGS}" LIBCURSES="-lncurses" -j"${CPU_COUNT}"
 
 mkdir -p "${PREFIX}/bin"
 
-cp dwgsim dwgsim_eval "${PREFIX}/bin/"
+install -v -m 755 dwgsim dwgsim_eval "${PREFIX}/bin"
