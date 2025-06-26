f5bdf1d9484ed43faee9d4b7b7f8a7f86843f42d
mencian <joshua.zhuang@yahoo.com>
2025-06-25 23:40:27 -0500

    add aarch64 and arm64 builds

--- recipes/famseq/build.sh
+++ recipes/famseq/build.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-cd FamSeq || true
-cd src
-make \
-    CC="${CXX} ${CXXFLAGS} ${CPPFLAGS}" \
-    LDFLAGS="${LDFLAGS}"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 install -d "${PREFIX}/bin"
-install FamSeq "${PREFIX}/bin/"
+
+cd src
+
+sed -i.bak 's|g++|$(CXX)|' makefile
+sed -i.bak 's|LDFLAGS=|LDFLAGS=-L$(PREFIX)/lib|' makefile
+rm -rf *.bak
+
+make CXX="${CXX}" -j"${CPU_COUNT}"
+
+install -v -m 0755 FamSeq "${PREFIX}/bin"
