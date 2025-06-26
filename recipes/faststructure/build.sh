2149cfb8f49b319fbae19f441d447252d63be75f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-26 18:48:21 -0500

    Update build.sh

--- recipes/faststructure/build.sh
+++ recipes/faststructure/build.sh
@@ -1,20 +1,22 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 # Build vars
 cd vars
-python setup.py build_ext --inplace
+${PYTHON} setup.py build_ext --inplace
 cd ..
 
 # Build main
-python setup.py build_ext --inplace
-cp vars/*.so ./
+${PYTHON} setup.py build_ext --inplace
+cp -f vars/*.so ./
 
 # Make scripts executable
-chmod +x structure.py
-chmod +x distruct.py
-chmod +x chooseK.py
+chmod +x structure.py distruct.py chooseK.py
 
 # Install files
-mkdir -p ${PREFIX}/bin/vars
-install -v -m 755 *.py ${PREFIX}/bin
-install -v -m 755 *.so ${PREFIX}/bin
-install -v -m 755 vars/*.py ${PREFIX}/bin/vars
-install -v -m 755 vars/*.so ${PREFIX}/bin/vars
+mkdir -p "${PREFIX}/bin/vars"
+install -v -m 755 *.py *.so "${PREFIX}/bin"
+install -v -m 755 vars/*.py vars/*.so "${PREFIX}/bin/vars"
