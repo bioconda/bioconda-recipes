34c2f089696b698ed413fc26c1c8aa7ec25ff697
AndreaGuarracino <andreaguarracino@outlook.com>
2025-06-26 09:04:26 +0200

    don't touch gafpack build.sh

--- recipes/gafpack/build.sh
+++ recipes/gafpack/build.sh
@@ -3,4 +3,4 @@ set -xe
 
 # build statically linked binary with Rust
 export RUST_BACKTRACE=1
-cargo install --verbose --path . --root "${PREFIX}" --no-track
+cargo install --verbose --path . --root ${PREFIX} --no-track
