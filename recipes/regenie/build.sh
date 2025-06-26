ac65f610d72086ae5aba19da4e995079715aef1b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-26 01:18:28 -0500

    Update build.sh

--- recipes/regenie/build.sh
+++ recipes/regenie/build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -ex
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  # LDFLAGS fix: https://github.com/AnacondaRecipes/intel_repack-feedstock/issues/8
-  export LDFLAGS="${LDFLAGS} -Wl,-pie -Wl,-headerpad_max_install_names -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
-else
-  export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
-  export MKL_THREADING_LAYER="GNU"
-fi
 # https://bioconda.github.io/troubleshooting.html#zlib-errors
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 export CPATH="${PREFIX}/include"
 
-mkdir -p build
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  # LDFLAGS fix: https://github.com/AnacondaRecipes/intel_repack-feedstock/issues/8
+  export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib -Wl,-pie -Wl,-headerpad_max_install_names"
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+  export MKL_THREADING_LAYER="GNU"
+  export CONFIG_ARGS=""
+fi
 
 if [[ ${target_platform} == "linux-aarch64" ]]; then
 # Conda does not yet support the MKL library for aarch64
@@ -20,11 +24,12 @@ if [[ ${target_platform} == "linux-aarch64" ]]; then
   sed -i.bak '/include <optional>/a #\ \ \ \ include\ <cstdint>' external_libs/cxxopts/include/cxxopts.hpp
 fi
 
-cmake -DCMAKE_BUILD_TYPE="Release" \
+cmake -S "${SRC_DIR}" -B build -DCMAKE_BUILD_TYPE="Release" \
   -DBUILD_SHARED_LIBS:BOOL=ON \
   -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
   -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-  -S "${SRC_DIR}" \
-  -B build
-
+  -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+  "${CONFIG_ARGS}"
+  
 cmake --build build --clean-first --target install -j "${CPU_COUNT}"
