5ed52b2a62bd925a83b3aa46eb25ca27242ba6ee
AndreaGuarracino <andreaguarracino@outlook.com>
2025-06-26 09:02:16 +0200

    revert wrong push to gafpack

--- recipes/gafpack/build.sh
+++ recipes/gafpack/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -euo
 set -xe
 
-export CFLAGS="${CFLAGS} -O3 -std=c11 -Wno-implicit-function-declaration"
-
 # build statically linked binary with Rust
 export RUST_BACKTRACE=1
 cargo install --verbose --path . --root "${PREFIX}" --no-track
