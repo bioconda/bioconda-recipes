3575f5a6e407f5d3701ff5f7a196a97b8e36e604
root <root@localhost.localdomain>
2025-06-26 11:21:40 +0800

    update expansionhunterdenovo

--- recipes/expansionhunterdenovo/build.sh
+++ recipes/expansionhunterdenovo/build.sh
@@ -3,6 +3,9 @@
 cd source
 sed -i.bak "s,add_subdirectory(thirdparty/boost-cmake-1.67.0),find_package(Boost REQUIRED COMPONENTS system program_options filesystem),g" CMakeLists.txt
 sed -i.bak "s,include_directories(thirdparty),#include_directories(thirdparty),g" CMakeLists.txt
+sed -i.bak "12c\find_package(fmt REQUIRED)" CMakeLists.txt
+sed -i.bak "31s/common region profileworkflow mergeworkflow)/common region profileworkflow mergeworkflow\n    fmt::fmt)/" CMakeLists.txt
+sed -i.bak '40s/)$/ fmt::fmt)/' CMakeLists.txt
 sed -i.bak "/ExternalProject_Add/,/)/d" io/CMakeLists.txt
 sed -i.bak "s,add_library(htslib IMPORTED),add_library(htslib IMPORTED)\ninclude_directories(${PREFIX}/include)\nlink_directories(${PREFIX}/lib),g" io/CMakeLists.txt
 sed -i.bak "s,add_dependencies(htslib htslib_project),,g" io/CMakeLists.txt
