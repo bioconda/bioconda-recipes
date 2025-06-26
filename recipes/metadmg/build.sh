bb62799bc078f72825eb4d959083d5c49b8ca971
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-25 22:37:55 -0500

    Update build.sh

--- recipes/metadmg/build.sh
+++ recipes/metadmg/build.sh
@@ -8,4 +8,4 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -lz -lbz2 -llzma -L${PREFIX}/lib"
 
 make CC="${CC}" CXX="${CXX}" HTSSRC="systemwide" -j"${CPU_COUNT}"
-install -v -m 755 ./metaDMG-cpp ./misc/compressbam "${PREFIX}/bin"
+install -v -m 755 ./metaDMG-cpp ./misc/compressbam ./misc/extract_reads "${PREFIX}/bin"
