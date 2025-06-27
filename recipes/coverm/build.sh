4ea5f0655e05aa8fad9c3a6fe56e245a7cf7e018
mencian <joshua.zhuang@yahoo.com>
2025-06-27 02:55:49 -0500

    Rebuild coverm

--- recipes/coverm/build.sh
+++ recipes/coverm/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
-# Build statically linked binary with Rust
-C_INCLUDE_PATH=$PREFIX/include \
-LIBRARY_PATH=$PREFIX/lib \
-cargo build --release
+export C_INCLUDE_PATH="$PREFIX/include"
+export LIBRARY_PATH="$PREFIX/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
-# Install the binaries
-cargo install --root $PREFIX
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+# Build statically linked binary with Rust
+RUST_BACKTRACE=1
+cargo install --no-track --verbose --root "${PREFIX}" --path .
