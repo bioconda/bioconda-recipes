5f5b8fe052b4bab2a7ba8b085ea93bdaaa254cf8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-06-27 04:22:45 -0500

    Update build.sh

--- recipes/dashing/build.sh
+++ recipes/dashing/build.sh
@@ -17,17 +17,30 @@ popd
 case $(uname -m) in
     aarch64)
 	sed -i.bak 's|-march=native|-march=armv8-a|' Makefile
+	sed -i.bak 's|-msse4.2 -mpclmul -march=native|-march=armv8-a|' bonsai/clhash/Makefile
+	sed -i.bak 's|-march=native|-march=armv8-a|' bonsai/Makefile
+	sed -i.bak 's|-march=native|-march=armv8-a|' bonsai/python/Makefile
+	sed -i.bak 's|-march=native|-march=armv8-a|' bonsai/hll/Makefile
 	;;
     arm64)
 	sed -i.bak 's|-march=native|-march=armv8.4-a|' Makefile
+	sed -i.bak 's|-msse4.2 -mpclmul -march=native|-march=armv8.4-a|' bonsai/clhash/Makefile
+	sed -i.bak 's|-march=native|-march=armv8-a|' bonsai/Makefile
+	sed -i.bak 's|-march=native|-march=armv8-a|' bonsai/python/Makefile
+	sed -i.bak 's|-march=native|-march=armv8-a|' bonsai/hll/Makefile
 	;;
     x86_64)
 	sed -i.bak 's|-march=native|-march=x86-64-v3|' Makefile
+	sed -i.bak 's|-msse4.2 -mpclmul -march=native|-march=x86-64-v3|' bonsai/clhash/Makefile
+	sed -i.bak 's|-march=native|-march=x86-64-v3|' bonsai/Makefile
+	sed -i.bak 's|-march=native|-march=x86-64-v3|' bonsai/python/Makefile
+	sed -i.bak 's|-march=native|-march=x86-64-v3|' bonsai/hll/Makefile
 	;;
 esac
 
 sed -i.bak "s/ -lzstd//g" Makefile
 sed -i.bak 's|ar r|$(AR) rcs|' Makefile
+sed -i.bak 's|-std=c99|-std=c11|' bonsai/clhash/Makefile
 rm -rf *.bak
 
 make CC="${CC}" CXX="${CXX}" INCPLUS="-I${PREFIX}/include" EXTRA_LD="-L${PREFIX}/lib" -j"${CPU_COUNT}"
