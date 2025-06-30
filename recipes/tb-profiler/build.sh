f5c47b5ed8b2264999c271b0c9e0f0b8173965d1
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-06-30 08:01:13 +0100

    update build.sh and deps

--- recipes/tb-profiler/build.sh
+++ recipes/tb-profiler/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 python -m pip install --no-deps --ignore-installed .
-# gatk CreateSequenceDictionary -R $PREFIX/share/tbprofiler/tbdb.fasta
-# samtools faidx $PREFIX/share/tbprofiler/tbdb.fasta
+cd db
+tb-profiler load_lib tbdb
 bwa index $PREFIX/share/tbprofiler/tbdb.fasta
-# this downloads Mycobacterium_tuberculosis_h37rv DB to $PREFIX/share/snpeff-SNPEFF_VERSION/data
-snpEff download Mycobacterium_tuberculosis_h37rv
