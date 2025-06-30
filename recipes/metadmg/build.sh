b933fc6378ec58f4d6f2ec937e5d43e69f9b1cc2
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-06-30 21:45:36 +0200

    Update: bump metadmg version (#57227)

--- recipes/metadmg/build.sh
+++ recipes/metadmg/build.sh
@@ -7,8 +7,5 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-make clean
-
 make CC="${CC}" CXX="${CXX}" HTSSRC="systemwide" -j"${CPU_COUNT}"
-
-install -v -m 755 ./metaDMG-cpp ./misc/compressbam "${PREFIX}/bin"
+install -v -m 755 ./metaDMG-cpp ./misc/compressbam ./misc/extract_reads "${PREFIX}/bin"
