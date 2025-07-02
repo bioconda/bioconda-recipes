165ce59bf31b393602928e314dabedb51410da7b
dslarm <david.lecomber@arm.com>
2025-07-02 23:43:24 +0100

    add new configs to sub dir.

--- recipes/rscape/build.sh
+++ recipes/rscape/build.sh
@@ -1,5 +1,8 @@
 
 cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* .
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* lib/R2R/R2R-current/
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* lib/hmmer
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* lib/hmmer/easel
 
 ./configure --disable-avx512 --prefix=$PREFIX
 
