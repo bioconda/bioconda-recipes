f671cb8736c67481f8e6531453db1453fa9cfa86
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-02 17:48:29 -0500

    Update build.sh

--- recipes/domclust/build.sh
+++ recipes/domclust/build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+
 mkdir -p $PREFIX/bin
-sed -i '8c\CFLAGS=-g -O2 $(CFLAGS0) -Xlinker -zmuldefs' Makefile
-make
+
+sed -i.bak '8c\CFLAGS=-g -O2 $(CFLAGS0) -Xlinker -zmuldefs' Makefile
+
+make -j"${CPU_COUNT}"
 # copy binary to prefix folder
-cp -p bin/domclust $PREFIX/bin
-cp -p Script/addtit.pl $PREFIX/bin
-cp -p Script/cmpr.pl $PREFIX/bin
-cp -p Script/convgraph.pl $PREFIX/bin
-cp -p build_input/blast2homfile.pl $PREFIX/bin
-cp -p build_input/fasta2genefile.pl $PREFIX/bin
+install -v -m 0755 bin/domclust Script/*.pl build_input/*.pl "${PREFIX}/bin"
 
 # test
 bin/domclust -h
 bin/domclust tst/test.hom tst/test.gene
-
