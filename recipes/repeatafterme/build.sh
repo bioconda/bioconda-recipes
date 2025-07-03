31367a7b803347096636e36ef3fe5293b7459f69
mencian <joshua.zhuang@yahoo.com>
2025-07-02 20:15:24 -0500

    add another patch

--- recipes/repeatafterme/build.sh
+++ recipes/repeatafterme/build.sh
@@ -8,8 +8,8 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' util/*.pl
 rm -rf util/*.bak
 sed -i.bak 's|ar rcs|$(AR) rcs|' kentsrc/Makefile
-sed -i.bak 's|-mtune=native||' Makefile
-
+sed -i.bak 's|cp RAMExtend $(INSTDIR)/bin|install -v -m 0755 RAMExtend $(INSTDIR)|' Makefile
+sed -i.bak 's|-Iminunit -I.|-Iminunit -I. -Wno-format|' Makefile
 
 make CC="${CC}" -j1
 
