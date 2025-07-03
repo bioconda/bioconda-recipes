c03c24f5a1e23952f403171ff6cac5d36bb8563d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-02 20:04:09 -0500

    Update build.sh

--- recipes/repeatafterme/build.sh
+++ recipes/repeatafterme/build.sh
@@ -5,8 +5,6 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-mkdir -p "${PREFIX}/bin"
-
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' util/*.pl
 rm -rf util/*.bak
 sed -i.bak 's|ar rcs|$(AR) rcs|' kentsrc/Makefile
