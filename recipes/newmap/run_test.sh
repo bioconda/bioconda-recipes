b49c27995e24b1bec9b5a463fe718966f0b39143
Eric Roberts <notthebmovieactor@gmail.com>
2025-07-04 10:33:27 -0400

    Remove use of SRC_DIR for tests in newmap recipe

--- recipes/newmap/run_test.sh
+++ recipes/newmap/run_test.sh
@@ -5,6 +5,7 @@ newmap index --help
 newmap search --help
 newmap track --help
 
-pushd $SRC_DIR/tests
+# Move data copied over from source_files into original directory structure
+mkdir -p data
+mv *.fa data/
 ./run_all.sh
-popd
