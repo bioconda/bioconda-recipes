e02aa76df46882a9274ca71c262bc7ae81c203f5
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-07-04 16:46:29 +0800

    Update build.sh

--- recipes/blat/build.sh
+++ recipes/blat/build.sh
@@ -4,7 +4,7 @@ export HOME=${PREFIX}
 mkdir -pv ${PREFIX}/bin/${MACHTYPE}
 mkdir -pv ${SRC_DIR}/lib/${MACHTYPE}
 (cd lib && make CC=$CC)
-(cd jkOwnLib && make cc=$CC)
+(cd jkOwnLib && make CC=$CC)
 (cd blat && make CC=$CC)
 cp ${PREFIX}/bin/${MACHTYPE}/* ${PREFIX}/bin
 rm -rf ${PREFIX}/bin/${MACHTYPE}
