563a181692fd35e3621e83c5f97faabe538e43ab
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-07-04 14:48:51 +0800

    Update build.sh

--- recipes/blat/build.sh
+++ recipes/blat/build.sh
@@ -3,8 +3,8 @@ export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 export HOME=${PREFIX}
 mkdir -pv ${PREFIX}/bin/${MACHTYPE}
 mkdir -pv ${SRC_DIR}/lib/${MACHTYPE}
-(cd lib && make)
-(cd jkOwnLib && make)
-(cd blat && make)
+(cd lib && make CC=$CC)
+(cd jkOwnLib && make cc=$CC)
+(cd blat && make CC=$CC)
 cp ${PREFIX}/bin/${MACHTYPE}/* ${PREFIX}/bin
 rm -rf ${PREFIX}/bin/${MACHTYPE}
