691242c4b42595bf309622fa06cf2dbe69be6020
Eric Roberts <notthebmovieactor@gmail.com>
2025-07-04 11:23:50 -0400

    Fix directory structure for test running for newmap recipe

--- recipes/newmap/run_test.sh
+++ recipes/newmap/run_test.sh
@@ -5,7 +5,6 @@ newmap index --help
 newmap search --help
 newmap track --help
 
-# Move data copied over from source_files into original directory structure
-mkdir -p data
-mv *.fa data/
+pushd tests
 ./run_all.sh
+popd
