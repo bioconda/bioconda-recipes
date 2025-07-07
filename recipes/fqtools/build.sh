81ddcbd3670bc393769846f858e77618293cb6cb
root <root@localhost.localdomain>
2025-07-07 14:48:19 +0800

    update fqtools

--- recipes/fqtools/build.sh
+++ recipes/fqtools/build.sh
@@ -3,6 +3,9 @@
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-make CC="$CC -fcommon" LIBS="-L$PREFIX/lib -lhts -lz -lm"
+sed -i.bak 's/(gzFile\*)/(gzFile)/g' src/fqfile.c
+sed -i.bak 's/gzFile\*/gzFile/g' src/fqfile.c
+
+make CC="$CC -fcommon" CFLAGS="-I$PREFIX/include" LIBS="-L$PREFIX/lib -lhts -lz -lm"
 mkdir -p $PREFIX/bin
 cp bin/fqtools $PREFIX/bin
