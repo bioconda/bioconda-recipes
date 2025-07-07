c8e7f1b8457c1b9b40adc21c3383baf12003b85f
mencian <joshua.zhuang@yahoo.com>
2025-07-07 18:05:52 -0500

    add another patch

--- recipes/kallisto/0.46.1/build.sh
+++ recipes/kallisto/0.46.1/build.sh
@@ -9,8 +9,10 @@ ARCH=$(uname -m)
 
 if [[ "${ARCH}" == "aarch64" ]]; then
 	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	sed -i.bak 's|--host=x86_64|--host=aarch64|' CMakeLists.txt
 elif [[ "${ARCH}" == "arm64" ]]; then
 	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	sed -i.bak 's|--host=x86_64|--host=arm64|' CMakeLists.txt
 else
 	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
 fi
@@ -21,6 +23,7 @@ cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* ext/htslib/
 
 sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' CMakeLists.txt
 sed -i.bak 's|VERSION 2.8.8|VERSION 3.5|' ext/catch/CMakeLists.txt
+rm -rf *.bak
 
 cd ext/htslib
 autoreconf -if
@@ -38,4 +41,4 @@ cmake -S. -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
-cmake --build build --clean-first --target install -j "${CPU_COUNT}"
+cmake --build build --clean-first --target install -j 1
