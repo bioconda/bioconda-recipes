5b9e2eaa5c85bc35e8d8ef9051a8976516f35dfa
mencian <joshua.zhuang@yahoo.com>
2025-07-08 00:29:59 -0500

    edit patch

--- recipes/snap/build.sh
+++ recipes/snap/build.sh
@@ -5,6 +5,11 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/snap
 mkdir -p ${PREFIX}/share/snap/bin
 
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
 make CC="${CC}" -j"${CPU_COUNT}"
 
 for perl_script in gff3_to_zff.pl hmm-assembler.pl zff2gff3.pl; do
