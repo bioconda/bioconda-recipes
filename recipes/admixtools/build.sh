b10a2dd7233925b0fc2a4ab3060797ae06b7c589
mencian <joshua.zhuang@yahoo.com>
2025-07-08 17:43:11 -0500

    add patch

--- recipes/admixtools/build.sh
+++ recipes/admixtools/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -ex
+
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
 
 # clear out pre-built objects and executables
 cd src
+
 make CC="${CC}" clobber -j"${CPU_COUNT}"
 
-make CC="${CC}" CFLAGS="$CFLAGS -O3 -fPIE -I${PREFIX}/include" LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" all -j"${CPU_COUNT}"
+make CC="${CC}" all -j"${CPU_COUNT}"
 
 make install TOP="${PREFIX}/bin"
