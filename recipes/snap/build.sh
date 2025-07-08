233588498c7f65357e3455a695a54c318affe32e
mencian <joshua.zhuang@yahoo.com>
2025-07-08 00:01:21 -0500

    Update snap to 2017_03_01

--- recipes/snap/build.sh
+++ recipes/snap/build.sh
@@ -1,25 +1,21 @@
-#!/bin/sh
-cd Zoe
-make CC=${CC} CFLAGS="$CFLAGS -fcommon" zoe-loop
-cd ..
-make CC=${CC} CFLAGS="$CFLAGS -fcommon" snap
-make CC=${CC} CFLAGS="$CFLAGS -fcommon" fathom
-make CC=${CC} CFLAGS="$CFLAGS -fcommon" forge
-make CC=${CC} CFLAGS="$CFLAGS -fcommon" hmm-info
-make CC=${CC} CFLAGS="$CFLAGS -fcommon" exonpairs
+#!/bin/bash
+set -ex
 
-mkdir -p $PREFIX/bin
+mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/share/snap
 mkdir -p ${PREFIX}/share/snap/bin
-for perl_script in cds-trainer.pl hmm-assembler.pl noncoding-trainer.pl patch-hmm.pl zff2gff3.pl; do
-  sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${perl_script}
-done
-cp -p snap fathom forge hmm-info exonpairs cds-trainer.pl hmm-assembler.pl noncoding-trainer.pl patch-hmm.pl zff2gff3.pl $PREFIX/share/snap/bin
-cp -pr HMM ${PREFIX}/share/snap
-cp -pr DNA ${PREFIX}/share/snap
 
-for NAME in snap fathom forge hmm-info exonpairs cds-trainer.pl hmm-assembler.pl noncoding-trainer.pl patch-hmm.pl zff2gff3.pl ; do
-  cp $RECIPE_DIR/wrapper ${PREFIX}/bin/${NAME}
+for perl_script in gff3_to_zff.pl hmm-assembler.pl zff2gff3.pl; do
+	sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' ${perl_script}
 done
+rm -rf *.bak
+
+install -v -m 0755 snap fathom forge gff3_to_zff.pl hmm-assembler.pl zff2gff3.pl $PREFIX/share/snap/bin
+cp -prf HMM ${PREFIX}/share/snap
+cp -prf DNA ${PREFIX}/share/snap
+
+for NAME in snap fathom forge gff3_to_zff.pl hmm-assembler.pl zff2gff3.pl; do
+	cp -f $RECIPE_DIR/wrapper ${PREFIX}/bin/${NAME}
+done
+
 chmod a+x ${PREFIX}/bin/*
-chmod a+x ${PREFIX}/share/snap/bin/*
