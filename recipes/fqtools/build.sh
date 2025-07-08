b405ccb14074704ec487f56bf436b8dc049d3001
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-07 23:09:51 -0500

    Update build.sh

--- recipes/fqtools/build.sh
+++ recipes/fqtools/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
 
 sed -i.bak 's/(gzFile\*)/(gzFile)/g' src/fqfile.c
 sed -i.bak 's/gzFile\*/gzFile/g' src/fqfile.c
 
-make CC="$CC -fcommon" CFLAGS="-I$PREFIX/include" LIBS="-L$PREFIX/lib -lhts -lz -lm"
+make CC="$CC -fcommon" CFLAGS="${CFLAGS} -O3 -I$PREFIX/include" LIBS="-L$PREFIX/lib -lhts -lz -lm" -j"${CPU_COUNT}"
 mkdir -p $PREFIX/bin
-cp bin/fqtools $PREFIX/bin
+install -v -m 0755 bin/fqtools "$PREFIX/bin"
