cecb745b4301b29cb127097e691adf8b203ba2d0
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-08 15:07:30 -0500

    Update build.sh

--- recipes/n50/build.sh
+++ recipes/n50/build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -euxo pipefail
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
+
 # Patch
-sed -i 's/^CC = gcc$/CC ?= gcc/' Makefile
+sed -i.bak 's/^CC = gcc$/CC ?= gcc/' Makefile
+sed -i.bak 's|-lpthread|-pthread|' Makefile
 
 # CPPFLAGS is PERFECT for include directories in C programs
-export CPPFLAGS="$CPPFLAGS -I$BUILD_PREFIX/include"
-sed -i 's/$(CC) $(CFLAGS)/$(CC) $(CPPFLAGS) $(CFLAGS)/g' Makefile
+sed -i.bak 's/$(CC) $(CFLAGS)/$(CC) $(CPPFLAGS) $(CFLAGS)/g' Makefile
+rm -rf *.bak
 
-
-make
+make CC="${CC}" -j"${CPU_COUNT}"
 
 ./bin/n50 --version
 
 # Install the binaries
-mkdir -p "$PREFIX/bin"
-cp -v bin/* "$PREFIX/bin"
+install -v -m 0755 bin/* "${PREFIX}/bin"
