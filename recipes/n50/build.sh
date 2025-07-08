1a95fead1b773a02a9e21ec6e99d2660d9c2f383
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2025-07-08 16:16:42 +0100

    fix missing lib

--- recipes/n50/build.sh
+++ recipes/n50/build.sh
@@ -4,9 +4,14 @@ set -euxo
 # Patch
 sed -i 's/^CC = gcc$/CC ?= gcc/' Makefile
 
+# Explicitly set include/library paths
+export CFLAGS="$CFLAGS -I$BUILD_PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$BUILD_PREFIX/lib""
+export CPATH="$BUILD_PREFIX/include"
+
 # Compile the project
-make
+make CC="$CC"
 
 # Install the binaries
 mkdir -p "$PREFIX/bin"
-cp bin/* "$PREFIX/bin"
+cp -v bin/* "$PREFIX/bin"
