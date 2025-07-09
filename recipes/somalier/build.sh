2b74b3a96e4e3103abc75cbf642e4eb4485bf2a8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-09 17:00:16 -0500

    Update build.sh

--- recipes/somalier/build.sh
+++ recipes/somalier/build.sh
@@ -2,9 +2,13 @@
 
 mkdir -p "${PREFIX}/bin"
 
-if [[ "$(uname -m)" == "aarch64" || "$(uname -m)" == "arm64" ]]; then
+if [[ "$(uname -m)" == "aarch64" ]]; then
 	sed -i.bak 's|--passC:"-mpopcnt"||' nim.cfg
 	rm -rf *.bak
+fi
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	sed -i.bak 's|--passC:"-mpopcnt"|--passC:"-Wno-incompatible-function-pointer-types"|' nim.cfg
 fi
 
 nimble --localdeps build -y --verbose -d:release
