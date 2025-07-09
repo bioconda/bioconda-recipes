563148c35e7704279f6b85de7c0afb337575d416
Hongxiang2023 <hongxiang23@ucla.edu>
2025-07-08 20:55:50 -0400

    Fix: use correct compiler for Bioconda build

--- recipes/yame/build.sh
+++ recipes/yame/build.sh
@@ -3,6 +3,10 @@
 export CFLAGS="-I${PREFIX}/include -Ihtslib/htslib"
 export LDFLAGS="-L${PREFIX}/lib"
 
+if [[ $(uname) == "Darwin" ]]; then
+  sed -i.bak 's/-ltinfo//g' Makefile
+fi
+
 make CC="${CC}" CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
 
 mkdir -p "${PREFIX}/bin"
