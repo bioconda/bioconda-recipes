051864e41a3825f106e3da401ec416288e8534d3
mencian <joshua.zhuang@yahoo.com>
2025-07-09 17:13:51 -0500

    Update vcfanno to 0.3.6

--- recipes/vcfanno/build.sh
+++ recipes/vcfanno/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-chmod a+x vcfanno*
-mkdir -p $PREFIX/bin
-cp vcfanno* $PREFIX/bin/vcfanno
+
+export CGO_ENABLED=0
+export GOPATH=$PWD
+export GOCACHE=$PWD/.cache/
+
+mkdir -p "${GOCACHE}"
+mkdir -p "${PREFIX}/bin"
+
+make all -j"${CPU_COUNT}"
+
+install -v -m 0755 vcfanno* "${PREFIX}/bin"
