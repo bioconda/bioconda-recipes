8b2e792ca4c795405ad7e23b063e4fa3511618f1
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-09 16:11:19 -0500

    Update build.sh

--- recipes/somalier/build.sh
+++ recipes/somalier/build.sh
@@ -2,6 +2,17 @@
 
 mkdir -p "${PREFIX}/bin"
 
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|--passC:"-mpopcnt"||' nim.cfg
+  rm -rf *.bak
+	;;
+    arm64)
+	sed -i.bak 's|--passC:"-mpopcnt"||' nim.cfg
+  rm -rf *.bak
+	;;
+esac
+
 nimble --localdeps build -y --verbose -d:release
 
 install -v -m 0755 somalier "${PREFIX}/bin"
