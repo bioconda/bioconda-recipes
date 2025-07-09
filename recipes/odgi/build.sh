735cb8c0a70d0b491e00517ac92898f812c0cefa
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-08 23:45:22 -0500

    Update build.sh

--- recipes/odgi/build.sh
+++ recipes/odgi/build.sh
@@ -13,12 +13,19 @@ export EXTRA_FLAGS="-Ofast -pipe -funroll-all-loops"
 
 case $(uname -m) in
     aarch64)
-	export EXTRA_FLAGS="${EXTRA_FLAGS} -march=armv8-a";;
+	export EXTRA_FLAGS="${EXTRA_FLAGS} -march=armv8-a"
+ 	sed -i.bak 's|arch=x86-64|arch=armv8-a|' src/main.cpp
+ 	;;
     arm64)
-	export EXTRA_FLAGS="${EXTRA_FLAGS} -march=armv8.4-a";;
+	export EXTRA_FLAGS="${EXTRA_FLAGS} -march=armv8.4-a"
+ 	sed -i.bak 's|arch=x86-64|arch=armv8.4-a|' src/main.cpp
+ 	;;
     x86_64)
-	export EXTRA_FLAGS="${EXTRA_FLAGS} -march=x86-64-v3";;
+	export EXTRA_FLAGS="${EXTRA_FLAGS} -march=x86-64-v3"
+	sed -i.bak 's|arch=x86-64|arch=x86-64-v3|' src/main.cpp
+	;;
 esac
+rm -rf src/*.bak
 
 if [[ `uname -s` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DBUILD_STATIC=0 -DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
@@ -35,7 +42,7 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Generic \
 cmake --build build --clean-first --target install -j "${CPU_COUNT}"
 
 PYVER=`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
-mkdir -p "${SP_DIR}"
+mkdir -p "${PREFIX}/lib/python${PYVER}/site-packages"
 
-cp -rf lib/*cpython* "${SP_DIR}"
+cp -rf lib/*cpython* "${PREFIX}/lib/python${PYVER}/site-packages"
 cp -rf lib/* "${PREFIX}/lib"
