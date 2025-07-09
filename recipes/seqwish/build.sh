362146dbf4b1dc21da1a9b7e7e559df868386b76
mencian <joshua.zhuang@yahoo.com>
2025-07-08 21:41:08 -0500

    Rebuild seqwish recipe

--- recipes/seqwish/build.sh
+++ recipes/seqwish/build.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
-
 set -xe
 
-export LIBRARY_PATH=${PREFIX}/lib
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export CPATH=${PREFIX}/include
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH="${PREFIX}/lib"
+export C_INCLUDE_PATH="${PREFIX}/include"
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+
+mkdir -p "${PREFIX}/bin"
 
 EXTRA_FLAGS="-Ofast"
 
 case $(uname -m) in
+    aarch64)
+	EXTRA_FLAGS="${EXTRA_FLAGS} -march=armv8-a"
+	;;
+    arm64)
+	EXTRA_FLAGS="${EXTRA_FLAGS} -march=armv8.4-a"
+	;;
     x86_64)
-        EXTRA_FLAGS="${EXTRA_FLAGS} -march=sandybridge"
-        ;;
-    *) ;;
+	EXTRA_FLAGS="${EXTRA_FLAGS} -march=x86-64-v3"
+	;;
 esac
 
-cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Generic -DEXTRA_FLAGS="${EXTRA_FLAGS}"
-cmake --build build -j ${CPU_COUNT}
-mkdir -p $PREFIX/bin
-mv bin/* $PREFIX/bin
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DBUILD_STATIC=0 -DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS="-DBUILD_STATIC=1"
+fi
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Generic \
+	-DEXTRA_FLAGS="${EXTRA_FLAGS}" -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+cmake --build build --clean-first -j "${CPU_COUNT}"
+
+install -v -m 0755 bin/* "${PREFIX}/bin"
