490f8285b2f949578e19ab7d7252cd84d31687c1
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-08 20:40:00 -0500

    Update build.sh

--- recipes/yame/build.sh
+++ recipes/yame/build.sh
@@ -5,12 +5,12 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p "${PREFIX}/bin"
 
+sed -i.bak 's|-std=gnu99|-std=c11 -I$(PREFIX)/include|' Makefile
+sed -i.bak 's|-lpthread|-pthread|' Makefile
+sed -i.bak 's|CLIB = -ltinfo|CLIB = -L$(PREFIX)/lib -ltinfo|' Makefile
 if [[ "$(uname -s)" == "Darwin" ]]; then
   sed -i.bak 's/-ltinfo//g' Makefile
 fi
-
-sed -i.bak 's|-std=gnu99|-std=c11 -I$(PREFIX)/include|' Makefile
-sed -i.bak 's|-lpthread|-pthread|' Makefile
 rm -rf *.bak
 
 make CC="${CC}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
