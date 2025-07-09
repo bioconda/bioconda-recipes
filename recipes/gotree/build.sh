04e554147f805b8c16e2409787851827dd23d4d0
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-09 16:51:04 -0500

    Update build.sh

--- recipes/gotree/build.sh
+++ recipes/gotree/build.sh
@@ -1,21 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 export CGO_ENABLED=0
 export GOPATH=$PWD
 export GOCACHE=$PWD/.cache/
 
-mkdir -p $GOCACHE
+mkdir -p "${GOCACHE}"
+mkdir -p "${PREFIX}/bin"
+
 cd src/github.com/evolbioinfo/${PKG_NAME}
-go get .
-go build -o ${PREFIX}/bin/${PKG_NAME} -ldflags "-X github.com/evolbioinfo/${PKG_NAME}/cmd.Version=${PKG_VERSION}" github.com/evolbioinfo/${PKG_NAME}
-go test github.com/evolbioinfo/${PKG_NAME}/...
+
+make all -j"${CPU_COUNT}"
+
+install -v -m 0755 gotree "${PREFIX}/bin"
+
+#go get .
+#go build -o ${PREFIX}/bin/${PKG_NAME} -ldflags "-X github.com/evolbioinfo/${PKG_NAME}/cmd.Version=${PKG_VERSION}" github.com/evolbioinfo/${PKG_NAME}
+#go test github.com/evolbioinfo/${PKG_NAME}/...
 
 # Gotree test data
-cp -r tests/data $PREFIX/gotree_test_data
+cp -rf tests/data $PREFIX/gotree_test_data
 
 # Gotree test script
 echo "#!/usr/bin/env bash" > $PREFIX/bin/gotree_test.sh
-sed 's+GOTREE=./gotree+GOTREE=gotree+g' test.sh \
-    | sed "s+TESTDATA=\"tests/data\"+TESTDATA=\$(dirname \$0)/../gotree_test_data/+g" \
+sed -i.bak 's+GOTREE=./gotree+GOTREE=gotree+g' test.sh \
+    | sed -i.bak "s+TESTDATA=\"tests/data\"+TESTDATA=\$(dirname \$0)/../gotree_test_data/+g" \
     >> $PREFIX/bin/gotree_test.sh
 chmod a+x $PREFIX/bin/gotree_test.sh
