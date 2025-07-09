13f78cd73dcd0b0353906db63714b0a11de50928
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-09 16:22:24 -0500

    Update build.sh

--- recipes/slivar/build.sh
+++ recipes/slivar/build.sh
@@ -6,6 +6,24 @@ mkdir -p "${PREFIX}/bin"
 VERSION="0.3.1"
 PSLIVAR_SHA256SUM="4b103abacd83eb0b1c9082fd789f3f0eed78eb5d76710f037a1d3e1749d4f25e"
 
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|--passC:"-mpopcnt"||' nim.cfg
+  rm -rf *.bak
+	;;
+    arm64)
+	sed -i.bak 's|--passC:"-mpopcnt"||' nim.cfg
+  rm -rf *.bak
+	;;
+esac
+
+case $(uname -s) in
+    Darwin)
+	sed -i.bak 's|--passC:"-static -no-pie"||' nim.cfg
+  rm -rf *.bak
+	;;
+esac
+
 nimble --localdeps build -y --verbose -d:release
 
 install -v -m 0755 slivar "${PREFIX}/bin"
