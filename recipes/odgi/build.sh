1dfd826e5dbeb36923b00145cf065293196133ee
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-08 23:02:35 -0500

    Update build.sh

--- recipes/odgi/build.sh
+++ recipes/odgi/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib"
+mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/lib"
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export EXTRA_FLAGS="-Ofast -pipe -funroll-all-loops"
 
@@ -25,15 +26,16 @@ else
 	export CONFIG_ARGS="-DBUILD_STATIC=1"
 fi
 
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Generic -DPIC=OFF \
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Generic \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_CXX_COMPILER="${CXX}" \
-	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DEXTRA_FLAGS="${EXTRA_FLAGS}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DCMAKE_C_COMPILER="${CC}" \
+ 	-DCMAKE_C_FLAGS="${CFLAGS}" -DEXTRA_FLAGS="${EXTRA_FLAGS}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
-ninja -C build -t clean -j "${CPU_COUNT}" install
+ninja install -C build -j "${CPU_COUNT}"
 
 PYVER=`python -c 'import sys; print(str(sys.version_info[0])+"."+str(sys.version_info[1]))'`
-mkdir -p "${PREFIX}/lib/python${PYVER}/site-packages"
+mkdir -p "${SP_DIR}/${PKG_NAME}-${PKG_VERSION}"
 
-cp -rf lib/*cpython* "${PREFIX}/lib/python${PYVER}/site-packages"
+cp -rf lib/*cpython* "${SP_DIR}/${PKG_NAME}-${PKG_VERSION}"
 cp -rf lib/* "${PREFIX}/lib"
