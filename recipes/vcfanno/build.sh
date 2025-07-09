358267b1d5347edabaa9eb53a572227375988301
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-09 17:34:39 -0500

    Update build.sh

--- recipes/vcfanno/build.sh
+++ recipes/vcfanno/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export CGO_ENABLED=0
-export GOPATH=$PWD
-export GOCACHE=$PWD/.cache/
+export GOPATH="${PWD}"
+export GOCACHE="${PWD}/.cache"
 
 mkdir -p "${GOCACHE}"
 mkdir -p "${PREFIX}/bin"
+
+go mod download "github.com/BurntSushi/toml@v0.3.1"
 
 make all -j"${CPU_COUNT}"
 
