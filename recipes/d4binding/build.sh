c0979cec9a0de417c355a96daa6e50a8b36da72e
mencian <joshua.zhuang@yahoo.com>
2025-07-09 21:31:14 -0500

    edit build.sh

--- recipes/d4binding/build.sh
+++ recipes/d4binding/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
+mkdir -p ${PREFIX}/include
+mkdir -p ${PREFIX}/lib
+
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
@@ -9,6 +12,11 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 cp -f ${RECIPE_DIR}/build_htslib.sh d4-hts/build_htslib.sh
 
+cd d4binding
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-cargo install --verbose --no-track --path d4binding --root "${PREFIX}"
+cargo build --release --package=d4binding
+
+install -m 644 include/d4.h ${PREFIX}/include
+install -m 644 target/release/libd4binding.* ${PREFIX}/lib
