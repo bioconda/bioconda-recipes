a15470d6322075a67af2e814492cfa9534a55faa
mencian <joshua.zhuang@yahoo.com>
2025-07-09 20:44:16 -0500

    edit patch

--- recipes/d4tools/build.sh
+++ recipes/d4tools/build.sh
@@ -6,6 +6,8 @@ export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+cp -f ${RECIPE_DIR}/build_htslib.sh d4-hts/build_htslib.sh
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 cargo install --verbose --no-track --path d4tools --root "${PREFIX}"
