3425009dbc62718b1b3511be408980790a8e4c6c
AndreaGuarracino <andreaguarracino@outlook.com>
2025-07-10 14:35:07 +0200

    build: export compiler environment variables for AGC makefile

--- recipes/impg/build.sh
+++ recipes/impg/build.sh
@@ -2,6 +2,10 @@
 
 set -xe
 
+# Export compiler environment variables for AGC makefile
+export CC="${CC:-gcc}"
+export CXX="${CXX:-g++}"
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
