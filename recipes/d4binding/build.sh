b0cbd07a7bde7bc6df79dd543f9c0582d9b678cf
mencian <joshua.zhuang@yahoo.com>
2025-07-09 20:37:15 -0500

    d4binding: add arm64 build

--- recipes/d4binding/build.sh
+++ recipes/d4binding/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
 set -xe
 
-# TODO: Remove the following export when pinning is updated and we use
-#       {{ compiler('rust') }} in the recipe.
-export \
-	CARGO_NET_GIT_FETCH_WITH_CLI=true \
-	CARGO_HOME="${BUILD_PREFIX}/.cargo"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
-cp $RECIPE_DIR/build_htslib.sh d4-hts/build_htslib.sh
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-cd d4binding 
+cp -f ${RECIPE_DIR}/build_htslib.sh d4-hts/build_htslib.sh
+
+cd d4binding
+
+# build statically linked binary with Rust
+RUST_BACKTRACE=1
 ./install.sh
