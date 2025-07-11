102610fc023cb20cafd7018d5d672b9a76b64beb
Alicia A. Evans <108547992+aliciaaevans@users.noreply.github.com>
2025-07-11 15:43:54 -0400

    set all the flags!

--- recipes/bioconductor-alabaster.base/build.sh
+++ recipes/bioconductor-alabaster.base/build.sh
@@ -3,14 +3,6 @@ mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
 
-
-echo -e "CC=$CC
-FC=$FC
-CXX=$CXX
-CXX98=$CXX
-CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
-
 # Handle MacOSX
 if [ $(uname) == 'Darwin' ];
 then
@@ -21,9 +13,22 @@ then
     export LDFLAGS="${LDFLAGS} -isysroot ${SDKROOT}"
   fi
   export MACOSX_DEPLOYMENT_TARGET=10.15
+  export OSX_DEPLOYMENT_TARGET=10.15=10.15
   export MACOSX_SDK_VERSION=10.15
-  export CFLAGS="${CFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -fno-define-target-os-macros"
-  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+  export CFLAGS="${CFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -fno-define-target-os-macros  -mmacosx-version-min=10.15"
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15  -mmacosx-version-min=10.15"
+   
 fi
+
+# I don't think this is right because it's for compiling R packages
+echo -e "CC=$CC
+CFLAGS=$CFLAGS
+LDFLAGS=$LDFLAGS
+FC=$FC
+CXX=$CXX
+CXXFLAGS=$CXXFLAGS
+CXX98=$CXX
+CXX11=$CXX
+CXX14=$CXX" > ~/.R/Makevars
 
 $R CMD INSTALL --build .
