521e4bf4a681bb1b1c78a4dae02952a0e48c5713
Devon Ryan <devon.ryan@genedata.com>
2025-07-11 18:36:13 +0200

    updates

--- recipes/cactus/build.sh
+++ recipes/cactus/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+# 
+cd submodules/abPOA
+make EXTRA_FLAGS="-Wall -Wno-unused-function -Wno-misleading-indentation -DUSE_SIMDE -DSIMDE_ENABLE_NATIVE_ALIASES -I${PREFIX}/include -L${PREFIX}/lib"
+make EXTRA_FLAGS="-Wall -Wno-unused-function -Wno-misleading-indentation -DUSE_SIMDE -DSIMDE_ENABLE_NATIVE_ALIASES -I${PREFIX}/include -L${PREFIX}/lib" src/abpoa_align_simd.o
+make EXTRA_FLAGS="-Wall -Wno-unused-function -Wno-misleading-indentation -DUSE_SIMDE -DSIMDE_ENABLE_NATIVE_ALIASES -I${PREFIX}/include -L${PREFIX}/lib" avx2=1
+cd ../../
+
+cd submodules/FASTGA
+make CFLAGS="${CFLAGS} -L${PREFIX}/lib"
+cd ../../
+
 ${PYTHON} -m pip install .
 make
-mkdir -p ${PREFIX}/bin
-ls -lh
