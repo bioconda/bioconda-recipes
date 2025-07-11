101e39026425fa421535f0563ea1fbe442f76ee0
Alicia A. Evans <108547992+aliciaaevans@users.noreply.github.com>
2025-07-11 12:44:33 -0400

    try some other options

--- recipes/bioconductor-alabaster.base/build.sh
+++ recipes/bioconductor-alabaster.base/build.sh
@@ -2,11 +2,27 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
+
+
 echo -e "CC=$CC
 FC=$FC
-CXX=$CXX -D_LIBCPP_DISABLE_AVAILABILITY
-CXX98=$CXX -D_LIBCPP_DISABLE_AVAILABILITY
-CXX11=$CXX -D_LIBCPP_DISABLE_AVAILABILITY
-CXX14=$CXX -D_LIBCPP_DISABLE_AVAILABILITY
-" > ~/.R/Makevars
+CXX=$CXX
+CXX98=$CXX
+CXX11=$CXX
+CXX14=$CXX" > ~/.R/Makevars
+
+# Handle MacOSX
+if [ $(uname) == 'Darwin' ];
+then
+  if [[ $(uname -m) == 'x86_64' ]]; then
+    echo "OSX x86-64: attempting to fix broken (old) SDK behavior"
+    export CFLAGS="${CFLAGS} -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC"
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_HAS_NO_C11_ALIGNED_ALLOC"
+  fi
+  export MACOSX_DEPLOYMENT_TARGET=10.15
+  export MACOSX_SDK_VERSION=10.15
+  export CFLAGS="${CFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -fno-define-target-os-macros"
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 $R CMD INSTALL --build .
