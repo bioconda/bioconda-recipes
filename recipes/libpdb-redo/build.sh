b28978f4d30529ad8c8050f3cdd505150278a52e
UENO, M. <eunosium-1128@live.jp>
2025-07-13 17:36:10 +0000

    libpdb-redo: Enhance build script to include pthread support in LDFLAGS and CXXFLAGS

--- recipes/libpdb-redo/build.sh
+++ recipes/libpdb-redo/build.sh
@@ -2,9 +2,9 @@
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -pthread"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-int-conversion -Wno-implicit-function-declaration"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -pthread"
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
 export CMAKE_THREAD_LIBS_INIT="-lpthread"
 export CMAKE_USE_PTHREADS_INIT=1
@@ -19,6 +19,8 @@ fi
 cmake -S . -B build -G Ninja \
     ${CMAKE_ARGS} \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
+    -DTHREADS_PREFER_PTHREAD_FLAG=ON \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_TESTING=OFF \
     -DFFTW2_INCLUDE_DIRS="${PREFIX}/fftw2/include" \
