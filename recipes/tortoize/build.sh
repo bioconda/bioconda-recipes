e5b50411265f73a22982b042932440bcdf2496f3
UENO, M. <eunosium-1128@live.jp>
2025-07-15 14:00:39 +0000

    tortoize: Update build script to include optimization flags for C++ compilation

--- recipes/tortoize/build.sh
+++ recipes/tortoize/build.sh
@@ -2,6 +2,8 @@
 
 set -exo pipefail
 
+export CXXFLAGS="${CXXFLAGS} -O3"
+
 # - sed -i.bak '/if (NOT TARGET dssp)/i find_package(dssp QUIET)' CMakeLists.txt
 sed -i.bak 's|if (NOT TARGET dssp)|find_package(dssp QUIET)\nif(NOT TARGET dssp::dssp AND NOT TARGET dssp)|g' CMakeLists.txt
 # - sed -i 's/find_or_fetch_package(cifpp VERSION 7/find_package(cifpp QUIET)\nif(NOT cifpp_FOUND)\n  find_or_fetch_package(cifpp VERSION 7/g' CMakeLists.txt
