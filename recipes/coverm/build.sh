1fef509b67640c15c2f41501f99426f4fd02ffe4
mencian <joshua.zhuang@yahoo.com>
2025-07-15 18:44:15 -0500

    coverm: add arm64 build

--- recipes/coverm/build.sh
+++ recipes/coverm/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-cargo add rust-htslib@0.45
+export INCLUDE_PATH="${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # Build statically linked binary with Rust
-C_INCLUDE_PATH=$PREFIX/include \
-LIBRARY_PATH=$PREFIX/lib \
-cargo build --release
-
-# Install the binaries
-cargo install --root $PREFIX
+cargo install --no-track --verbose --root "${PREFIX}" --path .
