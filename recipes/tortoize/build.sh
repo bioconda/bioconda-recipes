a4385e29d8a704571d2bc278552cd5f48261bab7
UENO, M. <eunosium-1128@live.jp>
2025-07-15 14:15:05 +0000

    tortoize: Remove commented-out sed commands in build script for clarity

--- recipes/tortoize/build.sh
+++ recipes/tortoize/build.sh
@@ -4,10 +4,7 @@ set -exo pipefail
 
 export CXXFLAGS="${CXXFLAGS} -O3"
 
-# - sed -i.bak '/if (NOT TARGET dssp)/i find_package(dssp QUIET)' CMakeLists.txt
 sed -i.bak 's|if (NOT TARGET dssp)|find_package(dssp QUIET)\nif(NOT TARGET dssp::dssp AND NOT TARGET dssp)|g' CMakeLists.txt
-# - sed -i 's/find_or_fetch_package(cifpp VERSION 7/find_package(cifpp QUIET)\nif(NOT cifpp_FOUND)\n  find_or_fetch_package(cifpp VERSION 7/g' CMakeLists.txt
-# - sed -i 's/GIT_TAG v7.0.9 VARIABLES "CIFPP_SHARE_DIR")/GIT_TAG v7.0.9 VARIABLES "CIFPP_SHARE_DIR")\nendif()/g' CMakeLists.txt
 
 if [[ "${target_platform}" == "linux-"* ]]; then
     cmake -S . -B build -G Ninja \
