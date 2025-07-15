9ca5a82a57d73060e0086bbf94c042cf6dac5add
UENO, M. <eunosium-1128@live.jp>
2025-07-15 18:19:39 +0000

    ccp4srs: Update CMake configuration to set zlib directory correctly

--- recipes/ccp4srs/build.sh
+++ recipes/ccp4srs/build.sh
@@ -5,15 +5,15 @@ set -exo pipefail
 export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
+sed -i.bak 's|find_package(zlib REQUIRED)|find_package(ZLIB REQUIRED)|' CMakeLists.txt
+
 cmake -S . -B build -G Ninja \
     ${CMAKE_ARGS} \
     -DCMAKE_C_FLAGS="${CFLAGS}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DZLIB_ROOT="${PREFIX}"
-
-cmake --build build --parallel ${CPU_COUNT}
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
 
-install -m 755 build/srsgen ${PREFIX}/bin/
-install -m 755 build/srsview ${PREFIX}/bin/
+install -m 755 build/srsgen "${PREFIX}/bin/"
+install -m 755 build/srsview "${PREFIX}/bin/"
