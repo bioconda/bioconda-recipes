4b424c25a6a405ae346d5db8de9c991a2ab17a27
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-15 17:29:44 -0500

    Update build.sh

--- recipes/impg/build.sh
+++ recipes/impg/build.sh
@@ -11,29 +11,29 @@ if [[ $(uname) == "Darwin" ]]; then
     # AGC-rs needs actual GCC on macOS, not clang
     # The conda-forge gcc package provides this
     # We need to use the actual gcc/g++ binaries from conda
-    mkdir -p "$PREFIX/bin"
-    ln -sf $CC $PREFIX/bin/clang
-    ln -sf $CXX $PREFIX/bin/clang++
-    export CC="$PREFIX/bin/clang"
-    export CXX="$PREFIX/bin/clang++"
+    mkdir -p "$BUILD_PREFIX/bin"
+    ln -sf $CC $BUILD_PREFIX/bin/clang
+    ln -sf $CXX $BUILD_PREFIX/bin/clang++
+    export CC="$BUILD_PREFIX/bin/clang"
+    export CXX="$BUILD_PREFIX/bin/clang++"
     
     # Set up Rust to use g++ as the linker
-    export RUSTFLAGS="-C linker=$CXX"
+    export RUSTFLAGS="-C linker=${CXX}"
     
     # Set make command for macOS
-    export MAKE=make
+    export MAKE="$(which make)"
 else
     # Create symlinks for standard compiler names that AGC makefile expects
-    mkdir -p "$PREFIX/bin"
-    ln -sf $CC "$PREFIX/bin/gcc"
-    ln -sf $CXX "$PREFIX/bin/g++"
+    mkdir -p "$BUILD_PREFIX/bin"
+    ln -sf $CC "$BUILD_PREFIX/bin/gcc"
+    ln -sf $CXX "$BUILD_PREFIX/bin/g++"
 
     # Ensure the symlinks are in PATH
-    export PATH="$PREFIX/bin:$PATH"
+    export PATH="$BUILD_PREFIX/bin:$PATH"
 
     # Also export the standard names
-    export CC="$PREFIX/bin/gcc"
-    export CXX="$PREFIX/bin/g++"
+    export CC="$BUILD_PREFIX/bin/gcc"
+    export CXX="$BUILD_PREFIX/bin/g++"
 fi
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
