6e59894a7de133c2b1205ccc79503be1c19e3453
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-15 17:04:31 -0500

    Update build.sh

--- recipes/impg/build.sh
+++ recipes/impg/build.sh
@@ -11,8 +11,8 @@ if [[ $(uname) == "Darwin" ]]; then
     # AGC-rs needs actual GCC on macOS, not clang
     # The conda-forge gcc package provides this
     # We need to use the actual gcc/g++ binaries from conda
-    export CC=$BUILD_PREFIX/bin/gcc
-    export CXX=$BUILD_PREFIX/bin/g++
+    export CC="$BUILD_PREFIX/bin/gcc"
+    export CXX="$BUILD_PREFIX/bin/g++"
     
     # Set up Rust to use g++ as the linker
     export RUSTFLAGS="-C linker=$CXX"
@@ -21,9 +21,9 @@ if [[ $(uname) == "Darwin" ]]; then
     export MAKE=make
 else
     # Create symlinks for standard compiler names that AGC makefile expects
-    mkdir -p $BUILD_PREFIX/bin
-    ln -sf $CC $BUILD_PREFIX/bin/gcc
-    ln -sf $CXX $BUILD_PREFIX/bin/g++
+    mkdir -p "$BUILD_PREFIX/bin"
+    ln -sf $CC "$BUILD_PREFIX/bin/gcc"
+    ln -sf $CXX "$BUILD_PREFIX/bin/g++"
 
     # Ensure the symlinks are in PATH
     export PATH="$BUILD_PREFIX/bin:$PATH"
