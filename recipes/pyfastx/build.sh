4bafa7f7b07649352209bb2d4dd0787e9534bc04
Cornelius Roemer <cornelius.roemer@gmail.com>
2025-07-15 05:34:14 +0200

    try adding more modern pip install incantation

--- recipes/pyfastx/build.sh
+++ recipes/pyfastx/build.sh
@@ -2,4 +2,4 @@
 if [[ "$(uname)" == "Darwin" ]]; then
 	export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
 fi
-$PYTHON -m pip install . --no-deps -vvv
+$PYTHON -m pip install . --no-deps --no-build-isolation --use-pep517 --no-cache-dir -vvv
