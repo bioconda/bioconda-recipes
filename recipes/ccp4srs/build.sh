924a771eef4383e0d5532e2ccd00766dabfb244e
UENO, M. <eunosium-1128@live.jp>
2025-07-15 20:52:58 +0900

    ccp4srs: Update build.sh

--- recipes/ccp4srs/build.sh
+++ recipes/ccp4srs/build.sh
@@ -10,7 +10,8 @@ cmake -S . -B build -G Ninja \
     ${CMAKE_ARGS} \
     -DCMAKE_C_FLAGS="${CFLAGS}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    -DBUILD_SHARED_LIBS=ON
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
 cmake --build build --parallel ${CPU_COUNT}
 cmake --install build
