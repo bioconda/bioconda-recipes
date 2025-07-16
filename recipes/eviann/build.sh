699f1965dffd23565cf79f7c20ca64abc5900ea1
Aleksey Zimin <27226909+alekseyzimin@users.noreply.github.com>
2025-07-16 10:51:09 -0400

    Update build.sh

--- recipes/eviann/build.sh
+++ recipes/eviann/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-./configure --prefix=$PREFIX
-make
-make install
+cd global-1 && \
+./configure --prefix=$PREFIX --bindir=$PREFIX/bin --libdir=$PREFIX/lib && \
+make -j && \
+make install-special && \
+cd $PREFIX/bin && \
+tar -xzf TransDecoder-v5.7.1a.tar.gz && \
+rm -f  TransDecoder-v5.7.1a.tar.gz
