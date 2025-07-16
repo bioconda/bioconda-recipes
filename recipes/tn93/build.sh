85c9e5342b96ea2251c1ae873407013bc07f17f2
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-16 16:19:23 -0500

    Update build.sh

--- recipes/tn93/build.sh
+++ recipes/tn93/build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 if [[ `uname -s` == "Darwin" ]]; then
     export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 else
@@ -9,7 +25,7 @@ fi
 sed -i.bak 's|VERSION 3.0|VERSION 3.5|' CMakeLists.txt
 rm -rf *.bak
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DINSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
     "${CONFIG_ARGS}"
