85781b6c7a2e9b44d3fdff6385de4c781088ed26
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-15 23:46:44 -0500

    Update build.sh

--- recipes/mzmine/build.sh
+++ recipes/mzmine/build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -ex
 
-mkdir -p "$PREFIX"
+mkdir -p "${PREFIX}"
 
 # debug: show available tool chains
 ./gradlew -q javaToolchains
 ./gradlew --exclude-task test --debug --stacktrace jpackage
 
-cp -rv build/install/MZmine/* "$PREFIX"
-
-# for those the don't like capital letters in commands
-ln -fs "$PREFIX/bin/MZmine" "$PREFIX/bin/mzmine"
+cp -rfv build/jpackage/mzmine/* "${PREFIX}"
 
 # MZmine does not run without --enable-preview
 # .. so we sneek in in the command line
-sed -i.bak -e 's/exec "$JAVACMD" "$@"/exec "$JAVACMD" --enable-preview "$@"/' "$PREFIX/bin/MZmine"
-rm -rf "$PREFIX/bin/*.bak"
+sed -i.bak -e 's/exec "$JAVACMD" "$@"/exec "$JAVACMD" --enable-preview "$@"/' "${PREFIX}/bin/mzmine"
+rm -rf "${PREFIX}/bin/*.bak"
