959abb50bd9373c0fcd10a5601aadce3842264c8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-15 23:15:33 -0500

    Update build.sh

--- recipes/hyphy/build.sh
+++ recipes/hyphy/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CPPFLAGS="${CPPFLAGS} -fPIC -I${PREFIX}/include -Wno-unused-variable"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -fPIC -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-unused-variable"
 
 if [[ `uname -s` == "Darwin" ]]; then
     export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
@@ -11,19 +11,38 @@ else
 fi
 
 case $(uname -m) in
-    aarch64|arm64) 
-        HYPHY_OPTS=""
-        ;;
-    *)
-        HYPHY_OPTS="-DNOAVX=ON"
-        ;;
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+    export HYPHY_OPTS="-DNOSSE4=ON"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+    export HYPHY_OPTS="-DNOSSE4=ON"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+    export HYPHY_OPTS="-DNOAVX=ON"
+	;;
 esac
 
-cmake -S . -B . -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+    export HYPHY_OPTS="${HYPHY_OPTS}"
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "aarch64" ]]; then
+    export HYPHY_OPTS="${HYPHY_OPTS}"
+elif [[ "${OS}" == "Darwin" && "${ARCH}" == "x86_64" ]]; then
+    export HYPHY_OPTS="${HYPHY_OPTS} -DNONEON=ON"
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "x86_64" ]]; then
+    export HYPHY_OPTS="${HYPHY_OPTS} -DNONEON=ON"
+fi
+
+cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev \
+    -Wno-deprecated --no-warn-unused-cli \
     "${HYPHY_OPTS}" \
     "${CONFIG_ARGS}"
-cmake --build . --target HYPHYMPI -j "${CPU_COUNT}"
-cmake --build . --target install -j "${CPU_COUNT}"
+ninja -C build -j"${CPU_COUNT}" HYPHYMPI
+ninja -C build -j"${CPU_COUNT}" install
