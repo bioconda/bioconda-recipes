4b5da3f58e1d1bceae56a98b139656f47c41bc45
mencian <joshua.zhuang@yahoo.com>
2025-07-16 18:04:00 -0500

    rapmap: add aarch64/arm64 builds

--- recipes/rapmap/build.sh
+++ recipes/rapmap/build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -eu -o pipefail
 
-mkdir build && cd build
-cmake -DNO_NATIVE_ARCH=TRUE -DCMAKE_CXX_FLAGS=-mno-avx -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_BUILD_TYPE=RELEASE ..
-make
-make install
-CTEST_OUTPUT_ON_FAILURE=1 make test
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export CFLAGS="${CFLAGS} -O3"
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -DNO_NATIVE_ARCH=TRUE -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+cmake --build build --clean-first --target install -j 1
