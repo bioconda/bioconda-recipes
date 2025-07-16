c6f148b003fc8c76cb25c622c4b7342b2412dc98
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-15 20:21:04 -0500

    Update build.sh

--- recipes/unicorn/build.sh
+++ recipes/unicorn/build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+# Create the directory where executables should be placed in a conda env
+mkdir -p "$PREFIX/bin"
+# Optional: If you want to install header files for others to use
+mkdir -p "$PREFIX/include"
+# Optional: If you want to install the static library
+mkdir -p "$PREFIX/lib"
+
 # This will run your existing Makefile.
 # conda-build automatically sets up the environment so that the
 # C compiler ($CC) is found and the htslib headers and libraries
 # are available for compilation and linking.
-make PREFIX=$PREFIX
-
-# Create the directory where executables should be placed in a conda env
-mkdir -p $PREFIX/bin
+make CC="${CC}" PREFIX="${PREFIX}" -j"${CPU_COUNT}"
 
 # Copy your compiled unicorn executable into that directory
-cp unicorn $PREFIX/bin/
+install -v -m 0755 unicorn "$PREFIX/bin"
 
-# Optional: If you want to install header files for others to use
-mkdir -p $PREFIX/include
-cp src/unicorn.h $PREFIX/include/
+cp -f src/unicorn.h "$PREFIX/include"
 
-# Optional: If you want to install the static library
-mkdir -p $PREFIX/lib
-cp libunicorn.a $PREFIX/lib/
+cp -f libunicorn.a "$PREFIX/lib"
