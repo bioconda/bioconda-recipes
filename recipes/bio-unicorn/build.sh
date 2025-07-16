43e6bfc2f1451b7fa0591509007f2fb1dfede929
mencian <joshua.zhuang@yahoo.com>
2025-07-15 20:44:07 -0500

    rename recipe to bio-unicorn

--- recipes/bio-unicorn/build.sh
+++ recipes/bio-unicorn/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create the directory where executables should be placed in a conda env
-mkdir -p "$PREFIX/bin"
+mkdir -p "${PREFIX}/bin"
 # Optional: If you want to install header files for others to use
-mkdir -p "$PREFIX/include"
+mkdir -p "${PREFIX}/include"
 # Optional: If you want to install the static library
-mkdir -p "$PREFIX/lib"
+mkdir -p "${PREFIX}/lib"
 
 # This will run your existing Makefile.
 # conda-build automatically sets up the environment so that the
@@ -14,8 +14,8 @@ mkdir -p "$PREFIX/lib"
 make CC="${CC}" PREFIX="${PREFIX}" -j"${CPU_COUNT}"
 
 # Copy your compiled unicorn executable into that directory
-install -v -m 0755 unicorn "$PREFIX/bin"
+install -v -m 0755 unicorn "${PREFIX}/bin"
 
-cp -f src/unicorn.h "$PREFIX/include"
+cp -f src/unicorn.h "${PREFIX}/include"
 
-cp -f libunicorn.a "$PREFIX/lib"
+cp -f libunicorn.a "${PREFIX}/lib"
