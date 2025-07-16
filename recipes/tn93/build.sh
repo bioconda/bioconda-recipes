41daf0557d5a3c285688299b8b6f1b5dd126b1bf
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-16 15:57:37 -0500

    Update build.sh

--- recipes/tn93/build.sh
+++ recipes/tn93/build.sh
@@ -6,6 +6,9 @@ else
     export CONFIG_ARGS=""
 fi
 
+sed -i.bak 's|VERSION 3.0|VERSION 3.5|' CMakeLists.txt
+rm -rf *.bak
+
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated --no-warn-unused-cli \
