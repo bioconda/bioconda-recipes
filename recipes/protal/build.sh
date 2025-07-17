08d2bf391ab0d3f80fac9ccff4e0ce9dd3f09dd2
mencian <joshua.zhuang@yahoo.com>
2025-07-16 21:35:13 -0500

    add patch

--- recipes/protal/build.sh
+++ recipes/protal/build.sh
@@ -4,31 +4,34 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -ldl"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
+mkdir -p ${PREFIX}/bin
+
 rm -rf cmake-build-release
 mkdir -p cmake-build-release
 
 echo "Step 1"
-echo $PWD
+echo ${PWD}
 echo "...SRCDIR= $SRC_DIR"
 export VERBOSE=1
 
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B cmake-build-release -G Ninja \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release \
- 	-DCMAKE_MAKE_PROGRAM=make \
-	-DCMAKE_C_COMPILER="${CC}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-G "CodeBlocks - Unix Makefiles" \
-	-S . \
-	-B cmake-build-release \
-	-DCMAKE_VERBOSE_MAKEFILE=ON
+	-DCMAKE_C_COMPILER="${CC}" -DCMAKE_C_FLAGS="${CFLAGS}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
 
 echo "Step 2"
-echo $PWD
-echo "cmake --build cmake-build-release --target protal -- -j ${CPU_COUNT}"
+echo ${PWD}
+echo "ninja -C cmake-build-release -j ${CPU_COUNT} install"
 
-cmake --build cmake-build-release --clean-first --target protal -- -j ${CPU_COUNT}
-cmake --build cmake-build-release --clean-first --target protal_avx2 -- -j ${CPU_COUNT}
+ninja -C cmake-build-release -j "${CPU_COUNT}" install
 
-cp -f cmake-build-release/protal ${PREFIX}/bin/protal_plain
-cp -f cmake-build-release/protal_avx2 ${PREFIX}/bin/protal_avx2
 cp -f protal_launcher ${PREFIX}/bin/protal
