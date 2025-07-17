140f08fa36a5f1fa11b53d2577b637aa09d5ae33
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-17 00:01:55 -0500

    Update build.sh

--- recipes/cactus-gfa-tools/build.sh
+++ recipes/cactus-gfa-tools/build.sh
@@ -6,6 +6,11 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 mkdir -p "${PREFIX}/bin"
 
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	sed -i.bak 's|-lomp||' Makefile
+	rm -rf *.bak
+fi
+
 make CXX="${CXX}" -j"${CPU_COUNT}"
 
 install -v -m 0755 faprefix.sh gaf2paf gaf2unstable gaffilter \
