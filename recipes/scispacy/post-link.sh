aa36fcbad4a421172ba0e2f3031c674527a76ad9
hechth <helge.hecht@recetox.muni.cz>
2025-07-17 14:18:27 +0200

    trying with post-link script

---
+++ recipes/scispacy/post-link.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+{{ PYTHON }} -m pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bc5cdr_md-0.5.4.tar.gz --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
