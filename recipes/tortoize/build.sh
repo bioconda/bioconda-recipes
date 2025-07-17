fc5a9b6a6aa5efb19a3057a627c78f170f749c1f
UENO, M. <eunosium-1128@live.jp>
2025-07-17 21:47:43 +0900

    tortoize: Remove web server feature

--- recipes/tortoize/build.sh
+++ recipes/tortoize/build.sh
@@ -7,24 +7,11 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 sed -i.bak 's|if (NOT TARGET dssp)|find_package(dssp QUIET)\nif(NOT TARGET dssp::dssp AND NOT TARGET dssp)|g' CMakeLists.txt
 sed -i.bak 's|${CIFPP_SHARE_DIR}|$ENV{PREFIX}/share/libcifpp|g' CMakeLists.txt
 
-if [[ "${target_platform}" == "linux-"* ]]; then
-    cmake -S . -B build -G Ninja \
-        ${CMAKE_ARGS} \
-        -DCMAKE_PREFIX_PATH="${PREFIX}" \
-        -DCMAKE_CXX_STANDARD=20 \
-        -DBUILD_TESTING=ON \
-        -DBUILD_WEBSERVICE=ON \
-        -DUSE_RSRC=ON \
-        -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp" \
-        -Dzeep_DIR="${PREFIX}/lib/cmake/zeep"
-elif [[ "${target_platform}" == "osx-"* ]]; then
-    cmake -S . -B build -G Ninja \
-        ${CMAKE_ARGS} \
-        -DCMAKE_PREFIX_PATH="${PREFIX}" \
-        -DCMAKE_CXX_STANDARD=20 \
-        -DBUILD_TESTING=ON \
-        -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp"
-fi
+cmake -S . -B build -G Ninja \
+    ${CMAKE_ARGS} \
+    -DCMAKE_CXX_STANDARD=20 \
+    -DBUILD_TESTING=ON \
+    -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp"
 
 cmake --build build --config Release --parallel "${CPU_COUNT}"
 ctest -V -C Release --test-dir build
