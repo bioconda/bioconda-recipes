eec0392e6dfe323a7502dcc47fe5c637dd982ab9
UENO, M. <eunosium-1128@live.jp>
2025-07-17 16:17:15 +0000

    reduce: Fix build script for OSX and clean up CMake configuration

--- recipes/reduce/build.sh
+++ recipes/reduce/build.sh
@@ -6,10 +6,20 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
+elif [[ "${target_platform}" == "osx-"* ]]; then
+    export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
+fi
+
+sed -i.bak \
+  's|target_link_libraries(mmtbx_reduceOrig_ext PRIVATE reducelib ${Boost_LIBRARIES} ${PYTHON_LIBRARIES})|target_link_libraries(mmtbx_reduceOrig_ext PRIVATE reducelib ${Boost_LIBRARIES})|' \
+  CMakeLists.txt
+
 # Refer to https://github.com/rlabduke/reduce/issues/60 for `-DHET_DICTIONARY` and `-DHET_DICTOLD` flags
-cmake -S . -B build \
+cmake -S . -B build -G Ninja \
   ${CMAKE_ARGS} \
   -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -DPython_ROOT_DIR="${PREFIX}" \
+  -DPYTHON_SITE_PACKAGES="${SP_DIR}" \
   -DHET_DICTIONARY="${PREFIX}/reduce_wwPDB_het_dict.txt" \
   -DHET_DICTOLD="${PREFIX}/reduce_get_dict.txt"
-cmake --build build --target install -j "${CPU_COUNT}"
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
