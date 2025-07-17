f9aa976b92372aae6ecae064b99a3799ab10e4c9
mencian <joshua.zhuang@yahoo.com>
2025-07-16 23:51:15 -0500

    Add recipe for cactus-gfa-tools

---
+++ recipes/cactus-gfa-tools/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x -e
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+
+mkdir -p "${PREFIX}/bin"
+
+make CXX="${CXX}" -j"${CPU_COUNT}"
+
+install -v -m 0755 faprefix.sh gaf2paf gaf2unstable gaffilter \
+	mzgaf2paf paf2lastz paf2stable pafcoverage pafmask \
+	rgfa-split rgfa2paf "${PREFIX}/bin"
