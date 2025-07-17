f1a8adfce90a4579d9004122df90b5f57745011f
UENO, M. <eunosium-1128@live.jp>
2025-07-17 20:20:05 +0900

    tortoize: Update recipe

--- recipes/tortoize/build.sh
+++ recipes/tortoize/build.sh
@@ -7,12 +7,23 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 sed -i.bak 's|if (NOT TARGET dssp)|find_package(dssp QUIET)\nif(NOT TARGET dssp::dssp AND NOT TARGET dssp)|g' CMakeLists.txt
 sed -i.bak 's|${CIFPP_SHARE_DIR}|$ENV{PREFIX}/share/libcifpp|g' CMakeLists.txt
 
-cmake -S . -B build -G Ninja \
-    ${CMAKE_ARGS} \
-    -DCMAKE_CXX_STANDARD=20 \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    -DBUILD_TESTING=ON \
-    -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp"
+if [[ "${target_platform}" == "linux-"* ]]; then
+    cmake -S . -B build -G Ninja \
+        ${CMAKE_ARGS} \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DCMAKE_CXX_STANDARD=20 \
+        -DBUILD_TESTING=ON \
+        -DBUILD_WEBSERVICE=ON \
+        -DUSE_RSRC=ON \
+        -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp"
+elif [[ "${target_platform}" == "osx-"* ]]; then
+    cmake -S . -B build -G Ninja \
+        ${CMAKE_ARGS} \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DCMAKE_CXX_STANDARD=20 \
+        -DBUILD_TESTING=ON \
+        -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp"
+fi
 
 cmake --build build --config Release --parallel "${CPU_COUNT}"
 ctest -V -C Release --test-dir build
