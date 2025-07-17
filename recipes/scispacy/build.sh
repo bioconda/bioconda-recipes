37e7cb9e1bcee1e52d20e631f970c8e922ceead2
hechth <helge.hecht@recetox.muni.cz>
2025-07-17 10:38:42 +0200

    trrying different install

--- recipes/scispacy/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i.bak 's|find_packages|find_namespace_packages|' setup.py
-rm -rf *.bak
-{{ PYTHON }} -m pip install . --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
-{{ PYTHON }} -m pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bc5cdr_md-0.5.4.tar.gz --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
-{{ PYTHON }} -m pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bionlp13cg_md-0.5.4.tar.gz --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
