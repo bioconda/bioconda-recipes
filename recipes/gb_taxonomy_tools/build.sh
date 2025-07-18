40dfbc9e1efa60c40c9d0095dd38510baa444f15
root <root@localhost.localdomain>
2025-07-18 12:01:47 +0800

    update gb_taxonomy_tools

--- recipes/gb_taxonomy_tools/build.sh
+++ recipes/gb_taxonomy_tools/build.sh
@@ -2,6 +2,9 @@
 
 mkdir -p $PREFIX/bin
 mkdir -p build
+sed -i '1i #include <string.h>' src/taxonomy2tree.c
+sed -i '1s/2.8/3.10/' CMakeLists.txt
+sed -i '10c\add_definitions(-D_GNU_SOURCE)' CMakeLists.txt
 cd build
-cmake -D INSTALL_PREFIX:PATH=$PREFIX ..
+cmake -DINSTALL_PREFIX:PATH=$PREFIX  .. 
 make install
