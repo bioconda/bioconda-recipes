029937db70d7e7da42f671b1dd7896628394259f
mencian <joshua.zhuang@yahoo.com>
2025-07-17 23:24:31 -0500

    rebuild d4binding

--- recipes/d4binding/build.sh
+++ recipes/d4binding/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -xe
 
-mkdir -p "${PREFIX}/include"
-mkdir -p "${PREFIX}/lib"
-
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+mkdir -p "${PREFIX}/include"
+mkdir -p "${PREFIX}/lib"
 
 cp -f ${RECIPE_DIR}/build_htslib.sh d4-hts/build_htslib.sh
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 cd d4binding
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-cargo build --release --package=d4binding --target-dir . -j "${CPU_COUNT}"
+cargo build --release --package=d4binding --lib
 
 install -v -m 644 include/d4.h "${PREFIX}/include"
-#install -v -m 644 release/libd4binding.* "${PREFIX}/lib"
+install -v -m 644 ../target/release/libd4binding.* "${PREFIX}/lib"
