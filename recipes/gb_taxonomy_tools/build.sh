ef42e9a74a8c29f9e3a6ec89fc61f9cd6f6a08a6
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-18 02:54:27 -0500

    Update build.sh

--- recipes/gb_taxonomy_tools/build.sh
+++ recipes/gb_taxonomy_tools/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-mkdir -p build
-sed -i '1i #include <string.h>' src/taxonomy2tree.c
-sed -i '1s/2.8/3.10/' CMakeLists.txt
-sed -i '10c\add_definitions(-D_GNU_SOURCE)' CMakeLists.txt
-cd build
-cmake -DINSTALL_PREFIX:PATH=$PREFIX  .. 
-make install
-cp $PREFIX/bin/taxonomy-reader $PREFIX/bin/taxonomyReader
+
+sed -i.bak '1i #include <string.h>' src/taxonomy2tree.c
+sed -i.bak '1s/2.8/3.10/' CMakeLists.txt
+sed -i.bak '10c\add_definitions(-D_GNU_SOURCE)' CMakeLists.txt
+
+cmake -S . -B build -DINSTALL_PREFIX:PATH="${PREFIX}"
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
+cp -f $PREFIX/bin/taxonomy-reader $PREFIX/bin/taxonomyReader
 chmod 755 $PREFIX/bin/taxonomyReader
