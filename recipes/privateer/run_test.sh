41fc8f6a109e0c612798d805e56d946c6e370c84
UENO, M. <eunosium-1128@live.jp>
2025-07-19 10:45:45 +0000

    privateer: Update database file paths in meta.yaml and run_test.sh

--- recipes/privateer/run_test.sh
+++ recipes/privateer/run_test.sh
@@ -6,6 +6,6 @@ privateer \
     -pdbin tests/test_data/5fjj.pdb \
     -mtzin tests/test_data/5fjj.mtz \
     -glytoucan \
-    -databasein src/privateer/privateer_database.json \
+    -databasein data/glycomics/privateer_glycomics_database.json \
     -debug_output \
     -all_permutations
