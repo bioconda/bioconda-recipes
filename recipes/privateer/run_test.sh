509f255ec963629c9e5eaa5e02f2bd090693b940
UENO, M. <eunosium-1128@live.jp>
2025-07-20 01:36:57 +0000

    privateer: Remove database input option from run_test.sh

--- recipes/privateer/run_test.sh
+++ recipes/privateer/run_test.sh
@@ -6,6 +6,5 @@ privateer \
     -pdbin tests/test_data/5fjj.pdb \
     -mtzin tests/test_data/5fjj.mtz \
     -glytoucan \
-    -databasein data/glycomics/privateer_glycomics_database.json \
     -debug_output \
     -all_permutations
