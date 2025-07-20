6f0e10b6459fccea027af80d17de5ef81d2b94e1
Wenhai Zhang <128364515+zwh82@users.noreply.github.com>
2025-07-20 16:04:05 +0800

    Update build.sh

--- recipes/pantax/build.sh
+++ recipes/pantax/build.sh
@@ -7,10 +7,15 @@ else
     chmod +x pantax data_preprocessing pantax_utils
     cp ${SRC_DIR}/scripts/pantax ${SRC_DIR}/scripts/data_preprocessing ${SRC_DIR}/scripts/pantax_utils ${PREFIX}/bin
     cp ${SRC_DIR}/scripts/*py ${PREFIX}/bin
+
     export GUROBI_HOME="$(cd ${SRC_DIR}/gurobi1103/linux64 && pwd)"
     cp -rf "$GUROBI_HOME/lib/libgurobi110.so" "$PREFIX/lib"
     
     cd ${SRC_DIR}/pantaxr
+    export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+    export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+    export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+    export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
     RUST_BACKTRACE=1 RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib" cargo build -r
     cp ${SRC_DIR}/pantaxr/target/release/pantaxr ${PREFIX}/bin/pantaxr
 fi
