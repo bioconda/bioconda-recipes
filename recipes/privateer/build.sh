95ffb217812898617ce2544514ac45b0b2921fa2
UENO, M. <eunosium-1128@live.jp>
2025-07-21 04:10:11 +0000

    build: Update build.sh and meta.yaml for improved configuration and testing

--- recipes/privateer/build.sh
+++ recipes/privateer/build.sh
@@ -2,13 +2,9 @@
 
 set -exo pipefail
 
-export CXXFLAGS="${CXXFLAGS} -frtti -include cstdint"
 
-if [[ "${target_platform}" == "linux-"* ]]; then
-    export LDFLAGS="${LDFLAGS} -Wl,--allow-shlib-undefined,--export-dynamic"
-elif [[ "${target_platform}" == "osx-"* ]]; then
-    export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup -Wl,-export_dynamic"
-fi
+
+export CXXFLAGS="${CXXFLAGS} -frtti -include cstdint"
 
 if [[ "${target_platform}" == "linux-"* ]]; then
     export LDFLAGS="${LDFLAGS} -Wl,--allow-shlib-undefined -Wl,--export-dynamic -Wl,--unresolved-symbols=ignore-all"
@@ -42,7 +38,6 @@ sed -i '/^project(/a find_package(Python3 COMPONENTS Interpreter Development REQ
 sed -i '/^project(/a find_package(pybind11 CONFIG REQUIRED)' CMakeLists.txt
 sed -i 's|add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/pybind11)||' CMakeLists.txt
 sed -i 's|${CMAKE_SOURCE_DIR}/dependencies/gemmi/include|${GEMMI_INCLUDE_DIR}|' CMakeLists.txt
-# sed -i 's|${PYTHON_LIBRARY}||g' CMakeLists.txt
 
 # Modify installation paths except python modules
 sed -i 's|TARGETS privateer_lib LIBRARY DESTINATION ${PROJECT_SOURCE_DIR}|TARGETS privateer_lib LIBRARY DESTINATION lib|' CMakeLists.txt
