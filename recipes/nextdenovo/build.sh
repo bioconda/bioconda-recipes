dbce1bd6971f5a477053d262ab46afb95447f7c8
mencian <joshua.zhuang@yahoo.com>
2025-07-21 17:42:18 -0500

    nextdenovo: add aarch64/arm64 builds

--- recipes/nextdenovo/build.sh
+++ recipes/nextdenovo/build.sh
@@ -5,6 +5,16 @@ OUTDIR="${SP_DIR}/nextdenovo"
 
 mkdir -p ${PREFIX}/bin ${OUTDIR}
 
-cp -rf ${SRC_DIR}/* ${OUTDIR}
+cd lib/htslib
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+autoreconf -if
+
+cd ../../
+
+make CC="${CC}" -j"${CPU_COUNT}"
+
+install -v -m 755 bin/* "${OUTDIR}"
+cp -rf ${SRC_DIR}/* "${OUTDIR}"
 chmod 755 ${OUTDIR}/nextDenovo
-ln -sf ${OUTDIR}/nextDenovo ${PREFIX}/bin/nextDenovo
+ln -sf ${OUTDIR}/nextDenovo "${PREFIX}/bin/nextDenovo"
