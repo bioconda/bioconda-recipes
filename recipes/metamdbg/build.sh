c0aa7b0cbaf0bfeb21dc0bdf74410e51b4fb9a52
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-21 21:12:26 -0500

    Update build.sh

--- recipes/metamdbg/build.sh
+++ recipes/metamdbg/build.sh
@@ -11,21 +11,21 @@ export CXXFLAGS="${CXXFLAGS} -O3 -Wno-unused-variable"
 case $(uname -m) in
     aarch64)
 	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
-    export HYPHY_OPTS="-DNOSSE4=ON"
+	export arm_neon=1
+	export aarch64=1
 	;;
     arm64)
 	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
-    export HYPHY_OPTS="-DNOSSE4=ON"
+	export arm_neon=1
+	export aarch64=1
 	;;
     x86_64)
 	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
-    export HYPHY_OPTS="-DNOAVX=ON"
 	;;
 esac
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-	export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 else
 	export CONFIG_ARGS=""
 fi
