24358470d21e42e09f7ff75edfc780e953efeb58
LiaOb21 <liaobinu@gmail.com>
2025-07-22 11:53:17 +0100

    updating kmergenie

--- recipes/kmergenie/build.sh
+++ recipes/kmergenie/build.sh
@@ -25,3 +25,19 @@ cp third_party/* $PREFIX/bin
 cp specialk $PREFIX/bin
 cp kmergenie $PREFIX/bin
 cp wrapper.py $PREFIX/bin
+
+# Fix KmerGenie directory structure expectations
+mkdir -p $PREFIX/bin/scripts
+mkdir -p $PREFIX/bin/ntCard
+
+# Create symlinks for expected paths
+ln -sf $PREFIX/bin/decide $PREFIX/bin/scripts/decide
+ln -sf $PREFIX/bin/ntcard $PREFIX/bin/ntCard/ntcard
+
+# Download and install readfq Python module
+mkdir -p $PREFIX/lib/python2.7/site-packages
+wget -q -O $PREFIX/lib/python2.7/site-packages/readfq.py \
+    https://raw.githubusercontent.com/lh3/readfq/master/readfq.py
+
+# Make readfq.py executable
+chmod +x $PREFIX/lib/python2.7/site-packages/readfq.py
