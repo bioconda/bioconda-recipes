01e0be6d74e55902047a3876f12c07c3c16e2fc4
danilotat <danilotatoni@gmail.com>
2025-07-22 11:14:28 +0200

    fix: novel release with f77 compilation in build

--- recipes/excavator2/build.sh
+++ recipes/excavator2/build.sh
@@ -5,6 +5,11 @@ mkdir -p "${DEST_DIR}"
 SOURCE_SUBDIR="${SRC_DIR}/${PKG_NAME}"
 # copy in share
 cp -r "${SOURCE_SUBDIR}"/* "${DEST_DIR}/"
+# compile them 
+lib_files=$(find ${DEST_DIR}/lib/F77 -type f -name "*.f")
+for lib_file in lib_files ; do
+       R CMD SHLIB $lib_file 
+done      
 chmod -R a+rX "${DEST_DIR}"
 # symlink them
 mkdir -p "${PREFIX}/bin"
