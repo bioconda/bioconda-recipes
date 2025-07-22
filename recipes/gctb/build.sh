f9e63d22f668b4b4a6a9b7478834c7716b24e00c
root <root@localhost.localdomain>
2025-07-22 20:49:34 +0800

    update gctb

--- recipes/gctb/build.sh
+++ recipes/gctb/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-cp gctb_2.0_Linux/gctb $PREFIX/bin/
+cd gctb_2.0_scr/scr
+sed -i "15c\EIGEN =$PREFIX/include/eigen3" Makefile
+sed -i "16c\BOOST=$PREFIX/include" Makefile
+if [[ ${target_platform} == "linux-aarch64" ]]; then
+ sed -i "23s/-m64 //" Makefile
+ sed -i "23s/-msse2 //" Makefile
+ sed -i "27s/-static //" Makefile
+ sed -i "27s/-m64 //" Makefile
+fi
+make
+cp gctb $PREFIX/bin/
