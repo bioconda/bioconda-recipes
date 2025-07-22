2ce9c72763525c1fc37f960c8720b73c743306c8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-22 02:40:24 -0500

    Update build.sh

--- recipes/vcfbub/build.sh
+++ recipes/vcfbub/build.sh
@@ -5,7 +5,7 @@ mkdir -p $PREFIX/bin
 export INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CFLAGS="${CFLAGS} -O3"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
 RUST_BACKTRACE=1
 cargo install --verbose --no-track --path . --root "${PREFIX}"
