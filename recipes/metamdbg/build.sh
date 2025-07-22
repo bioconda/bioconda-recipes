46d22bd8cdd70df7d77e30416d8e10bdbb3357d3
mencian <joshua.zhuang@yahoo.com>
2025-07-21 20:57:42 -0500

    Update metamdbg to v1.2

--- recipes/metamdbg/build.sh
+++ recipes/metamdbg/build.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
-
 set -xe
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX/bin"
 
 export CPATH="${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -fPIC -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-unused-variable"
 
-if [[ "$(uname)" == "Darwin" ]]; then
-	cp -rf ${RECIPE_DIR}/ContigPolisher.hpp src/polish/
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+    export HYPHY_OPTS="-DNOSSE4=ON"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+    export HYPHY_OPTS="-DNOSSE4=ON"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+    export HYPHY_OPTS="-DNOAVX=ON"
+	;;
+esac
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 	export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 else
 	export CONFIG_ARGS=""
 fi
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
-cmake --build build -j "${CPU_COUNT}" -v
+ninja -C build -j"${CPU_COUNT}"
 
-install -v -m 0755 build/bin/metaMDBG ${PREFIX}/bin
+install -v -m 755 build/bin/metaMDBG "${PREFIX}/bin"
