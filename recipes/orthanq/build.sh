6c30d2ad8c909496ba4936d59a6c13e44edd2d9d
fxwiegand <fxwiegand@wgdnet.de>
2025-07-23 10:17:23 +0200

    WIP

--- recipes/orthanq/build.sh
+++ recipes/orthanq/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -eu
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
-export HDF5_DIR=${PREFIX}
+export HDF5_DIR="${PREFIX}"
 export RUSTFLAGS="-C link-args=-Wl,-rpath,$HDF5_DIR/lib"
+export PYO3_PYTHON="${PYTHON}"
 
-if [ "${target_platform}" = "${build_platform}" ]; then
-  export PYO3_PYTHON="${PYTHON}"
-fi
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+RUST_BACKTRACE=1
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
