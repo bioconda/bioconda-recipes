35554131666c07ce440f6936c1b499bccf9142e5
mencian <joshua.zhuang@yahoo.com>
2025-07-23 17:09:56 -0500

    Update minialign to v0.6.0

--- recipes/minialign/build.sh
+++ recipes/minialign/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
 
-mkdir -p $PREFIX/bin
-sed -i -e 's/-march=native/-msse4.2 -mpopcnt/g' Makefile
-make
-make install PREFIX=$PREFIX
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPATH="${PREFIX}/include"
 
+mkdir -p "$PREFIX/bin"
+
+sed -i.bak 's|-march=native|-msse4.2 -mpopcnt|' Makefile
+
+make CC="${CC}" -j"${CPU_COUNT}"
+
+make install PREFIX="${PREFIX}"
