071e7b36f9a285741d2333f8304f6d33827297f2
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-07-23 14:56:53 +0800

    Update build.sh

--- recipes/gctb/build.sh
+++ recipes/gctb/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-cd gctb_2.0_scr/scr
-sed -i "15c\EIGEN =$PREFIX/include/eigen3" Makefile
-sed -i "16c\BOOST=$PREFIX/include" Makefile
 if [[ ${target_platform} == "linux-aarch64" ]]; then
+ cd gctb_2.0_scr/scr
+ sed -i "15c\EIGEN =$PREFIX/include/eigen3" Makefile
+ sed -i "16c\BOOST=$PREFIX/include" Makefile
  sed -i "23s/-m64 //" Makefile
  sed -i "23s/-msse2 //" Makefile
  sed -i "27s/-static //" Makefile
  sed -i "27s/-m64 //" Makefile
+ make CC=$CC CXX=$CXX
+ cp gctb $PREFIX/bin/
+else
+ cp gctb_2.0_linux/gctb $PREFIX/bin/
 fi
-make CC=$CC CXX=$CXX
-cp gctb $PREFIX/bin/
+
