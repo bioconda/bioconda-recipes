7b7291d7f681211d65e019c97af0f32487729f2a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-23 17:45:43 -0500

    Update build.sh

--- recipes/minialign/build.sh
+++ recipes/minialign/build.sh
@@ -7,10 +7,10 @@ export CPATH="${PREFIX}/include"
 mkdir -p "$PREFIX/bin"
 
 case $(uname -m) in
-  aarch64|arm64) sed -i.bak 's|-march=native||' Makefile && rm -rf *.bak ;;
-  *) sed -i.bak 's|-march=native|-msse4.2 -mpopcnt|' Makefile && rm -rf *.bak ;;
+  aarch64|arm64) sed -i.bak 's|-march=native|-march=armv8-a|' Makefile && make universal CC="${CC}" -j"${CPU_COUNT}" ;;
+  *) sed -i.bak 's|-march=native|-msse4.2 -mpopcnt|' Makefile && make CC="${CC}" -j"${CPU_COUNT}" ;;
 esac
 
 make CC="${CC}" -j"${CPU_COUNT}"
 
-make install PREFIX="${PREFIX}"
+make install CC="${CC}" PREFIX="${PREFIX}"
