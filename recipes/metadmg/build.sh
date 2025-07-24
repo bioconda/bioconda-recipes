6648bf899bd0527a301f8f75dbf009fa81744d7f
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-07-24 14:47:16 +0200

    Reformat

--- recipes/metadmg/build.sh
+++ recipes/metadmg/build.sh
@@ -3,4 +3,5 @@
 mkdir -p "${PREFIX}/bin"
 
 make CC="${CC}" CXX="${CXX}" FLAGS="-O3" HTSSRC="systemwide" -j"${CPU_COUNT}"
+
 install -v -m 755 ./metaDMG-cpp ./misc/compressbam ./misc/extract_reads "${PREFIX}/bin"
