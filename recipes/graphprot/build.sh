7b7ead39d8c59d5d69759dec00482e7ef1e7132f
mencian <joshua.zhuang@yahoo.com>
2025-07-23 22:45:56 -0500

    Rebuild graphprot recipe

--- recipes/graphprot/build.sh
+++ recipes/graphprot/build.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPATH="${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	sed -i.bak 's|-mtune=native -march=native|-march=armv8-a|' EDeN/Makefile
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	sed -i.bak 's|-mtune=native -march=native|-march=armv8.4-a|' EDeN/Makefile
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	sed -i.bak 's|-mtune=native -march=native|-march=x86-64-v3|' EDeN/Makefile
+	;;
+esac
+rm -rf EDeN/*.bak
 
 # set install paths
-BIN=${PREFIX}/bin/
-LIBEXEC=${PREFIX}/libexec/graphprot/
-SHARE=${PREFIX}/share/graphprot/
+BIN="${PREFIX}/bin"
+LIBEXEC="${PREFIX}/libexec/graphprot"
+SHARE="${PREFIX}/share/graphprot"
+mkdir -p "${BIN}" "${LIBEXEC}" "${SHARE}"
 
 # compile EDeN
-pushd .
 cd EDeN
-make clean
-make CC="${CC}" CXX="${CXX}"
-popd
+make CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
+cd ..
 
 # run build tests
 pytest
 
 # install
-mkdir -p $BIN $LIBEXEC $SHARE
-cp $SRC_DIR/GraphProt.pl $BIN
-cp $SRC_DIR/bin/* $LIBEXEC -r
-cp $SRC_DIR/EDeN/EDeN $LIBEXEC
-cp $SRC_DIR/data $SHARE -r
+install -v -m 0755 GraphProt.pl "${BIN}"
+install -v -m 0755 bin/*.pl "${LIBEXEC}"
+install -v -m 0755 bin/*.R "${LIBEXEC}"
+install -v -m 0755 bin/*.sh "${LIBEXEC}"
+install -v -m 0755 "bin/*.awk" "${LIBEXEC}"
+install -v -m 0755 bin/RNAplfold_context_stdout bin/fastapl "${LIBEXEC}"
+install -v -m 0755 EDeN/EDeN "${LIBEXEC}"
+cp -rf bin/StructureLibrary "${LIBEXEC}"
+cp -rf data "${SHARE}"
