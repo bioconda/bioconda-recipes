b84a019bf41c2ba13fb92e3d2c9df9b5078fd700
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-23 19:18:28 -0500

    Update build.sh

--- recipes/gctb/build.sh
+++ recipes/gctb/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX/bin"
+
 if [[ ${target_platform} == "linux-aarch64" ]]; then
  cd gctb_2.0_scr/scr
- sed -i "15c\EIGEN =$PREFIX/include/eigen3" Makefile
- sed -i "16c\BOOST=$PREFIX/include" Makefile
- sed -i "23s/-m64 //" Makefile
- sed -i "23s/-msse2 //" Makefile
- sed -i "27s/-static //" Makefile
- sed -i "27s/-m64 //" Makefile
- make CC=$CC CXX=$CXX
- cp gctb $PREFIX/bin/
+ sed -i.bak "15c\EIGEN =$PREFIX/include/eigen3" Makefile
+ sed -i.bak "16c\BOOST=$PREFIX/include" Makefile
+ sed -i.bak "23s/-m64 //" Makefile
+ sed -i.bak "23s/-msse2 //" Makefile
+ sed -i.bak "27s/-static //" Makefile
+ sed -i.bak "27s/-m64 //" Makefile
+ make CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
+ cp -f gctb "$PREFIX/bin"
 else
- cp gctb_2.0_scr/gctb $PREFIX/bin/
+ cp -f gctb_2.0_scr/gctb "$PREFIX/bin"
 fi
-
