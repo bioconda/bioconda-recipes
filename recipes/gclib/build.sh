3756aeafe376e5cf336875e446cf5e28744435f7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-24 03:32:32 -0500

    Update build.sh

--- recipes/gclib/build.sh
+++ recipes/gclib/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/lib $PREFIX/bin $PREFIX/include
-make
-install *.o $PREFIX/lib/
+
+make -j"${CPU_COUNT}"
+
 if [ -f libgffc.so ] ; then
     install *.so $PREFIX/lib/
 fi
+
 if [ -f libgffc.dylib ] ; then
     install *.dylib $PREFIX/lib/
 fi
+
+install *.o $PREFIX/lib/
 install *.h $PREFIX/include/
 install *.hh $PREFIX/include/
 install gtest $PREFIX/bin/
