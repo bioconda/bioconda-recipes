d987ba6fc9df9f0d9b4c8e431d305d8d863fe32f
root <root@localhost.localdomain>
2025-07-25 14:36:34 +0800

    add rseg

--- recipes/rseg/build.sh
+++ recipes/rseg/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 make \
-    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
-    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -DHIDE_INLINE_STATIC" \
+    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -Xlinker -zmuldefs"
 make install
 install -d "${PREFIX}/bin"
 install bin/* "${PREFIX}/bin/"
