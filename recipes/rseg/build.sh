0669fb0bc1b9a21110bb216c1b6591da2704702b
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-07-25 18:12:12 +0800

    Update build.sh

--- recipes/rseg/build.sh
+++ recipes/rseg/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [[ ${target_platform} == "linux-aarch64" ]]; then
+#if [[ ${target_platform} == "linux-aarch64" ]]; then
     make \
     CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -DHIDE_INLINE_STATIC" \
     CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -Xlinker -zmuldefs"
     make install
     install -d "${PREFIX}/bin"
     install bin/* "${PREFIX}/bin/"
-else
-    make \
-    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
-    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
-    make install
-    install -d "${PREFIX}/bin"
-    install bin/* "${PREFIX}/bin/"
-fi
+#else
+#    make \
+#    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+#    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+#    make install
+#    install -d "${PREFIX}/bin"
+#    install bin/* "${PREFIX}/bin/"
+#fi
