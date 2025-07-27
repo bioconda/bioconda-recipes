010203c3dde29e9d52d2b15ececb6a24c109cb63
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-27 18:28:20 -0500

    Update build.sh

--- recipes/winnowmap/build.sh
+++ recipes/winnowmap/build.sh
@@ -6,7 +6,11 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 
 install -d "${PREFIX}/bin"
 
-make \
+case $(uname -m) in
+	aarch64|arm64) make CC="${CC}" CXX="${CXX}" CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable ${LDFLAGS}" arm_neon=1 -j"${CPU_COUNT}" ;;
+esac
+
+make CC="${CC}" CXX="${CXX}" \
 	CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable ${LDFLAGS}" \
 	-j"${CPU_COUNT}"
 
