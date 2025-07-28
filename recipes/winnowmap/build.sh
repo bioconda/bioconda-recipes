219200e97ab9cb1ea6a4250e6d9bc19c8ce9273d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-27 19:01:50 -0500

    Update build.sh

--- recipes/winnowmap/build.sh
+++ recipes/winnowmap/build.sh
@@ -8,17 +8,17 @@ export CFLAGS="${CFLAGS} -O3 -Wno-narrowing"
 install -d "${PREFIX}/bin"
 
 case $(uname -m) in
-	aarch64|arm64) make CC="${CC}" CXX="${CXX}" CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable -Wno-narrowing ${LDFLAGS}" arm_neon=1 -j"${CPU_COUNT}" ;;
-esac
-
-case $(uname -m) in
-	aarch64|arm64) sed -i.bak 's|-msse2||' src/Makefile && sed -i.bak 's|-mno-sse4.1||' src/Makefile ;;
+	aarch64|arm64) sed -i.bak 's|-msse2||' src/Makefile && sed -i.bak 's|-mno-sse4.1||' src/Makefile && sed -i.bak 's|-msse4.1||' src/Makefile ;;
 esac
 
 sed -i.bak 's|-lpthread|-pthread|' Makefile
 sed -i.bak 's|-O2|-O3|' Makefile
 rm -rf *.bak
 rm -rf src/*.bak
+
+case $(uname -m) in
+	aarch64|arm64) make CC="${CC}" CXX="${CXX}" CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable -Wno-narrowing ${LDFLAGS}" arm_neon=1 -j"${CPU_COUNT}" ;;
+esac
 
 make CC="${CC}" CXX="${CXX}" \
 	CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable ${LDFLAGS}" \
