50c99ef46594ae34c03ffc9ab407fc25e09d54ce
UENO, M. <eunosium-1128@live.jp>
2025-07-28 13:54:44 +0000

    reduce: Fix installation of `reduce_wwPDB_het_dict.txt` in share directory

--- recipes/reduce/build.sh
+++ recipes/reduce/build.sh
@@ -22,6 +22,7 @@ cmake -S . -B build -G Ninja \
   -DHET_DICTIONARY="${PREFIX}/share/reduce/reduce_wwPDB_het_dict.txt" \
   -DHET_DICTOLD="${PREFIX}/share/reduce/reduce_het_dict.txt"
 cmake --build build --clean-first --target install -j "${CPU_COUNT}"
+install -m 644 reduce_wwPDB_het_dict.txt "${PREFIX}/share/reduce"
 install -m 755 update_het_dict.py "${PREFIX}/share/reduce"
 
 # Install a shared library file and a Python module
