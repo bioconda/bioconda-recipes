c6c4cf3aa5ad25b8d9ef946f7790321ecd04e7f7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-28 02:57:02 -0500

    Update build.sh

--- recipes/antismash/build.sh
+++ recipes/antismash/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+1
 echo "include antismash/detection/genefinding/data/train_crypto" >> MANIFEST.in
 df -h
 ${PYTHON} -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
