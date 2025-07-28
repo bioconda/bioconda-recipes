919f4db1bb664b09c2578721b4e21d8df50906fb
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-28 10:15:14 -0500

    Update build.sh

--- recipes/jvarkit-bamstats04/build.sh
+++ recipes/jvarkit-bamstats04/build.sh
@@ -4,9 +4,9 @@ OUT="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 mkdir -p "$OUT"
 mkdir -p "$PREFIX/bin"
 
-./gradlew bamstats04
+./gradlew jvarkit
 
-install -v -m 0755 dist/bamstats04.jar "$OUT"
+install -v -m 0755 dist/jvarkit.jar "$OUT"
 install -v -m 0755 $RECIPE_DIR/bamstats04.sh "$OUT"
 
 ln -sf $OUT/bamstats04.sh $PREFIX/bin/
