b0deab9cfe21fb33374c3055e89b5e1ae7827ec9
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-27 19:02:48 -0500

    Update build.sh

--- recipes/winnowmap/build.sh
+++ recipes/winnowmap/build.sh
@@ -18,10 +18,7 @@ rm -rf src/*.bak
 
 case $(uname -m) in
 	aarch64|arm64) make CC="${CC}" CXX="${CXX}" CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable -Wno-narrowing ${LDFLAGS}" arm_neon=1 -j"${CPU_COUNT}" ;;
+	*) make CC="${CC}" CXX="${CXX}" CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable ${LDFLAGS}" -j"${CPU_COUNT}" ;;
 esac
-
-make CC="${CC}" CXX="${CXX}" \
-	CPPFLAGS="${CXXFLAGS} ${CPPFLAGS} -g -Wall -O3 -DHAVE_KALLOC -fopenmp -std=c++14 -Wno-sign-compare -Wno-write-strings -Wno-unused-but-set-variable ${LDFLAGS}" \
-	-j"${CPU_COUNT}"
 
 install -v -m 0755 bin/winnowmap "${PREFIX}/bin"
