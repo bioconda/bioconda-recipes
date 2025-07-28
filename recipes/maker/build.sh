8a68f493e024bf5e51f658ecd9ed9616c7f76d75
mencian <joshua.zhuang@yahoo.com>
2025-07-27 21:20:25 -0500

    edit patch

--- recipes/maker/build.sh
+++ recipes/maker/build.sh
@@ -3,6 +3,11 @@
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/perl/lib"
 
+# We will need this script to precompile inline C code for MPI support
+cp -f maker_mpi_init $PREFIX/bin/
+rm -f maker_mpi_init
+chmod a+x $PREFIX/bin/maker_mpi_init
+
 cd src/
 
 # enable mpi
@@ -13,5 +18,10 @@ echo "yes" | perl Build.PL
 cd ..
 
 install -v -m 755 bin/* "${PREFIX}/bin"
-mv src/lib/* "${PREFIX}/perl/lib/"
+mv perl/lib/* "${PREFIX}/perl/lib/"
 mv lib/* "${PREFIX}/lib/"
+
+# Run a first time MPI_Init() to pre compile inline C code
+mpiexec -n 1 $PREFIX/bin/maker_mpi_init || true
+# This is not needed anymore
+rm -f $PREFIX/bin/maker_mpi_init
