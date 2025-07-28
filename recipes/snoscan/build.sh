82ab69ffb5447ee0daad50512d5604155cd5a540
mencian <joshua.zhuang@yahoo.com>
2025-07-27 21:46:09 -0500

    snoscan: add aarch64/arm64 builds

--- recipes/snoscan/build.sh
+++ recipes/snoscan/build.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
-
+#!/bin/bash
 set -eu
 
-make -C squid-1.5.11 CC="$CC" CFLAGS="$CFLAGS"
-make CC="$CC" CFLAGS="$CFLAGS"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+
+make -C squid-1.5.11 CC="${CC}" CFLAGS="${CFLAGS}" -j"${CPU_COUNT}"
+make CC="${CC}" CFLAGS="${CFLAGS}" -j"${CPU_COUNT}"
 
 mv sort-snos sort-snos.pl
 
@@ -14,7 +16,8 @@ mkdir perl-build
 find . -name '*.pl' ! -path './perl-build/*.pl' -exec mv {} perl-build \;
 # find . -name "*.pm`" | xargs -I {} cp {} perl-build/lib
 cd perl-build
-cp ${RECIPE_DIR}/Build.PL ./
+
+cp -f ${RECIPE_DIR}/Build.PL ./
 perl ./Build.PL
 perl ./Build manifest
 perl ./Build install --installdirs site
@@ -22,9 +25,9 @@ perl ./Build install --installdirs site
 cd ..
 ## End build perl
 
-ln -s $PREFIX/bin/sort-snos.pl  $PREFIX/bin/sort-snos
+ln -sf $PREFIX/bin/sort-snos.pl $PREFIX/bin/sort-snos
 
-mv snoscan snoscan[AHY] $PREFIX/bin
+install -v -m 0755 snoscan snoscan[AHY] $PREFIX/bin
 
 chmod +rx $PREFIX/bin/genpept2gsi.pl
 chmod +rx $PREFIX/bin/sort-snos.pl
