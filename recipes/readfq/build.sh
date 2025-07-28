f09184f19b5cd314cf8df97cc86e813ca6f7b0cc
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-27 22:46:08 -0500

    Update build.sh

--- recipes/readfq/build.sh
+++ recipes/readfq/build.sh
@@ -6,4 +6,4 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 
-"${CC}" "${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" -o "${PREFIX}/bin/readfq" kseq_fastq_base.c -lz
+"${CC}" ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o "${PREFIX}/bin/readfq" kseq_fastq_base.c -lz
