5faf7d5aa4ed41f9e1c12b9ef7c531a1414a46a4
UENO, M. <eunosium-1128@live.jp>
2025-07-30 06:41:36 +0900

    coot-headless: Rename activation and deactivation scripts (#58078)
    * coot-headless: Update run_test.py
    
    * coot-headless: Create conda_build_config.yaml
    
    * coot-headless: Increment build number
    
    * coot-headless: Rename activation and deactivation scripts
    
    * coot-headless: Remove constraining CPU usage

--- recipes/coot-headless/deactivate.fish
+++ recipes/coot-headless/deactivate.fish
@@ -1,15 +1,14 @@
-# Unset Coot environment variables
-unset COOT_PREFIX
+set -e COOT_PREFIX
 echo "COOT_PREFIX unset"
 
-unset COOT_REFMAC_LIB_DIR
+set -e COOT_REFMAC_LIB_DIR
 echo "COOT_REFMAC_LIB_DIR unset"
 
-unset CLIBD_MON
+set -e CLIBD_MON
 echo "CLIBD_MON unset"
 
-unset COOT_STANDARD_RESIDUES
+set -e COOT_STANDARD_RESIDUES
 echo "COOT_STANDARD_RESIDUES unset"
 
-unset SYMINFO
+set -e SYMINFO
 echo "SYMINFO unset"
