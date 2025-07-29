301b5bfcc14408c23dc0be3a0c1f91176da07ed3
Devon Ryan <devon.ryan@genedata.com>
2025-07-29 13:41:10 +0200

    Update beast2. It builds, but there's something wonky in the classpaths

--- recipes/beast2/build.sh
+++ recipes/beast2/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+pushd BeastFX
+ant -lib ${BUILD_PREFIX}/lib/javafx-sdk*/lib linux
+popd
 
-git clone https://github.com/CompEvol/BeastFX.git
-
-ln -s
-
-ant linux
-
-mkdir -p ${PREFIX}/bin ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}/lib
+TGT="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
+mkdir -p ${PREFIX}/bin ${TGT}/lib ${TGT}/bin
 
 for file in applauncher beast beauti densitree loganalyser logcombiner packagemanager treeannotator
-    do envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}' < release/Linux/beast/bin/$file > ${PREFIX}/bin/$file
-    chmod +x ${PREFIX}/bin/$file
+    do envsubst '${PREFIX}:${PKG_NAME}:${PKG_VERSION}:${PKG_BUILDNUM}' < beast2/release/Linux/beast/bin/$file > ${TGT}/bin/$file
+    chmod +x ${TGT}/bin/$file
+    ln -s ${TGT}/bin/$file ${PREFIX}/bin
 done
 
-cp -R release/Linux/beast/lib/ ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
-cp -R release/Linux/beast/{examples,images,templates} ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+cp -R beast2/release/Linux/beast/lib/ ${TGT}
+cp -R beast2/release/Linux/beast/{examples,images,fxtemplates} ${TGT}
+cp -R BeastFX/locallib/*.jar ${TGT}
+beast -version
+exit 1
