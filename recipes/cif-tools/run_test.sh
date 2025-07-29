29e66b12f6f8190f5ef4db8b0fa9092e351c8850
UENO, M. <eunosium-1128@live.jp>
2025-07-29 15:11:16 +0000

    cif-tools: Fix syntax for `cif-merge` and `cif-validate` commands in run_test.sh

--- recipes/cif-tools/run_test.sh
+++ recipes/cif-tools/run_test.sh
@@ -6,6 +6,6 @@ pdb2cif 7f95-carb.pdb
 cif2pdb 7f95-carb.cif
 cif-diff 443d_final.cif 7f95-carb.cif
 cif-grep 'STRUCTURES OF M-IODO HOECHST-DNA COMPLEXES' 443d_final.cif
-cif-merge 443d_final.cif 7f95-carb.cif || true
-cif-validate 443d_final.cif || true
+( cif-merge 443d_final.cif 7f95-carb.cif ) || true
+( cif-validate 443d_final.cif ) || true
 mmCQL -f test.cql 443d_final.cif
