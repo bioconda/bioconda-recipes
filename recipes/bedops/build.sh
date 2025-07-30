e4e9bacf7302798569b2fcc55dc6b917df84cc34
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-29 21:24:42 -0500

    Update build.sh

--- recipes/bedops/build.sh
+++ recipes/bedops/build.sh
@@ -8,19 +8,7 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 
 mkdir -p "${PREFIX}/bin"
 
-OS=$(uname -s)
-ARCH=$(uname -m)
-
-if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
-	wget https://github.com/alexey-lysiuk/macos-sdk/releases/download/13.3/MacOSX13.3.tar.xz
-	tar -xf MacOSX13.3.tar.xz
-	cp -rH MacOSX13.3.sdk /Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
-	export SDKROOT="/Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk"
-	export MACOSX_DEPLOYMENT_TARGET="13.3"
-	export MACOSX_SDK_VERSION="13.3"
-fi
-
-make all CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" SFLAGS="" -j"${CPU_COUNT}"
+make all CC="${CC}" CXX="${CXX}" SFLAGS= -j"${CPU_COUNT}"
 make install_all
 
 install -v -m 0755 bin/* "${PREFIX}/bin"
