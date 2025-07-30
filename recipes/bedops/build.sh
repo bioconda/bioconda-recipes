158ad15ca126a345a20e780b3bcc1c8f83bab1f0
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-29 23:49:35 -0500

    Update build.sh

--- recipes/bedops/build.sh
+++ recipes/bedops/build.sh
@@ -12,7 +12,13 @@ if [[ "$(uname -s)" == "Darwin"  ]] && [[ "$(uname -m)" == "arm64" ]]; then
   sed -i.bak 's|-static|-static -O3 -march=armv8.4-a|' applications/bed/starch/src/Makefile
 fi
 
-make all CC="${CC}" CXX="${CXX}" SFLAGS= -j"${CPU_COUNT}"
+if [[ "$(uname -s)" == "Darwin"  ]]; then
+  sed -i.bak 's|-static||' applications/bed/starch/src/Makefile
+  make all CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
+else
+  make all CC="${CC}" CXX="${CXX}" SFLAGS="-static -O3" -j"${CPU_COUNT}"
+fi
+
 make install_all
 
 install -v -m 0755 bin/* "${PREFIX}/bin"
