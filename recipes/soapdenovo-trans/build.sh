57b946fe648287c4354ec7f3cb220396b68fce3d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-30 21:12:22 -0500

    Update build.sh

--- recipes/soapdenovo-trans/build.sh
+++ recipes/soapdenovo-trans/build.sh
@@ -12,14 +12,15 @@ if [[ `uname -s` == "Darwin" ]]; then
 	sed -i.bak 's|-lrt||' src/Makefile
 fi
 
-sed -i.bak 's|1.04|1.05|' src/main.c
-sed -i.bak 's|1.04|1.05|' VERSION
+sed -i.bak 's|1.04|1.0.5|' src/main.c
+sed -i.bak 's|1.04|1.0.5|' VERSION
 rm -rf src/*.bak
 rm -rf *.bak
 
 pushd src
-make CC="${CC} -fcommon" LIBPATH="${LDFLAGS}" -j"${CPU_COUNT}"
-make 127mer=1 CC="${CC} -fcommon" LIBPATH="${LDFLAGS}" -j"${CPU_COUNT}"
+make CC="${CC}" LIBPATH="${LDFLAGS}" -j"${CPU_COUNT}"
+make CC="${CC}" clean
+make 127mer=1 CC="${CC}" LIBPATH="${LDFLAGS}" -j"${CPU_COUNT}"
 popd
 
 "${STRIP}" SOAPdenovo-Trans-*
