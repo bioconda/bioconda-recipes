f7edddb24718a048680dc982e7046991a62abab9
mencian <joshua.zhuang@yahoo.com>
2025-07-31 15:10:13 -0500

    Rebuild genometools-genometools recipe

--- recipes/genometools-genometools/build.sh
+++ recipes/genometools-genometools/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-
 set -e -o pipefail -x
 
-CAIRO_OPT=
-LIBS=
-if [[ "$(uname)" == Darwin ]]; then
-    CAIRO_OPT='cairo=no'
-    LIBS="LIBS=-lc"
-fi
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
-make ${CAIRO_OPT} ${LIBS} errorcheck=no -j ${CPU_COUNT}
-export prefix=$PREFIX
-make ${CAIRO_OPT} install 
+#CAIRO_OPT=
+#LIBS=
+#if [[ "$(uname -s)" == "Darwin" ]]; then
+	#CAIRO_OPT="cairo=no"
+	#LIBS="LIBS=-lc"
+#fi
+
+make "${CAIRO_OPT}" "${LIBS}" CC="${CC}" CXX="${CXX}" errorcheck=no -j"${CPU_COUNT}"
+make "${CAIRO_OPT}" prefix="${PREFIX}" install
 
 cd gtpython
-$PYTHON setup.py install 
+
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir --use-pep517 -vvv
