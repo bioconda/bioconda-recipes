4e8a4bb2cfdce701b2e55f8545cd4039b355fb48
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-30 23:24:13 -0500

    Update build.sh

--- recipes/ghostz/build.sh
+++ recipes/ghostz/build.sh
@@ -5,6 +5,11 @@ mkdir -p "${PREFIX}/bin"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	sed -i.bak 's|tr1/memory|memory|' src/aligner.h
+	sed -i.bak 's|tr1/memory|memory|' ext/seg/src/seg.h
+fi
+
 make -e CC="${CC}" CXX="${CXX}" LAST_CC="${CXX}" \
 	INCLUDES="-I${PREFIX}/include" LDFLAGS="${LDFLAGS}" \
 	-j"${CPU_COUNT}"
