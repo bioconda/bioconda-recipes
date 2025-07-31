0f88cc0111d3f6b735ce887d2c2f091d789f5b8f
mencian <joshua.zhuang@yahoo.com>
2025-07-31 16:18:11 -0500

    add makefile patch

--- recipes/genometools-genometools/build.sh
+++ recipes/genometools-genometools/build.sh
@@ -6,16 +6,8 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-	export CAIRO_OPT="cairo=no"
-	export LIBS="LIBS=-lc"
-else
-	export CAIRO_OPT="cairo=yes"
-	export LIBS=""
-fi
-
-make "${CAIRO_OPT}" errorcheck=no "${LIBS}" -j"${CPU_COUNT}"
-make "${CAIRO_OPT}" prefix="${PREFIX}" install
+make cairo=yes 64bit=yes errorcheck=no -j"${CPU_COUNT}"
+make prefix="${PREFIX}" cairo=yes 64bit=yes errorcheck=no install
 
 cd gtpython
 
