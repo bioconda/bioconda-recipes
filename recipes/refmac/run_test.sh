0193a89b36b4ad2207796c071d602052f8a580a4
UENO, M. <eunosium-1128@live.jp>
2025-07-31 02:01:54 +0000

    refmac: Update run_test script to include hydrogen addition and log output

--- recipes/refmac/run_test.sh
+++ recipes/refmac/run_test.sh
@@ -3,11 +3,12 @@ set -exo pipefail
 
 cd test
 
-refmac5 \
-    HKLIN   rnasa-1.8-all_refmac1.mtz \
-    HKLOUT  rnasa-1.8-all_refmac1-ref.mtz \
-    XYZIN   tutorial-modern.pdb \
-    XYZOUT  tutorial-modern-ref.pdb > test.log
+echo -e "MAKE HYDROGENS YES\nREFI TYPE RESTA\nNCYC 10\nEND" | \
+    refmac5 \
+        HKLIN rnasa-1.8-all_refmac1.mtz \
+        HKLOUT rnasa-1.8-all_refmac1-out.mtz \
+        XYZIN tutorial-modern.pdb \
+        XYZOUT tutorial-modern-ref.pdb 2>&1 || true
 
 libcheck < /dev/null 2>&1 | grep 'LIBCHECK'
 header2matr 7KNT.pdb 7KNT -B
