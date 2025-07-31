baebc38efa8897f26ff56ca9e86157d4490e196c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-31 00:32:56 -0500

    Update build.sh

--- recipes/echtvar/build.sh
+++ recipes/echtvar/build.sh
@@ -1,11 +1,6 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CARGO_PROFILE_RELEASE_STRIP=symbols
-export CARGO_PROFILE_RELEASE_LTO=fat
-export OPENSSL_DIR="${PREFIX}"
-export OPENSSL_NO_VENDOR=1
 export RUSTC_BOOTSTRAP=1
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
