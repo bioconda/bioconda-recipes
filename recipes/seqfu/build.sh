972f977e6e9a0667631a6c93aed807407fdcebf1
mencian <joshua.zhuang@yahoo.com>
2025-08-01 01:57:08 -0500

    seqfu: add arm64 build

--- recipes/seqfu/build.sh
+++ recipes/seqfu/build.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
-
+#!/bin/bash
 set -euxo pipefail
+
+mkdir -p "${PREFIX}/bin"
+
+if [[ "$(uname -m)" == "arm64" ]]; then
+	nim_build="macosx_arm64"
+	curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-2-2/${nim_build}.tar.xz -o ${nim_build}.tar.xz
+	unxz -c ${nim_build}.tar.xz | tar -x
+	cd nim-2.2.*
+	export PATH="${PWD}/bin:${PATH}"
+	cd ..
+fi
 
 echo "--- NIM BUILD ---"
 nim --version
@@ -10,9 +20,9 @@ echo "----------"
 
 echo " Setting environment variables"
 # Fix zlib
-export CFLAGS="$CFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPATH="${PREFIX}/include"
 
 echo "GXX: ${GXX:-'not set'}"
 echo "GCC: ${GCC:-'not set'}"
@@ -20,43 +30,31 @@ echo "----------"
 
 
 
-if [[ $OSTYPE == "darwin"* ]]; then
-  echo "OSX"
-  export HOME="/Users/distiller"
-  export HOME=`pwd`
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	echo "OSX"
+	export HOME="/Users/distiller"
+	export HOME=`pwd`
 else
-  # Trying to fix build when gcc or g++ are required
-  echo "LINUX: Patching makefile"
-  sed -i 's/gcc/gcc $(LDFLAGS)/g' Makefile
-  sed -i 's/g++/g++ $(LDFLAGS)/g' Makefile
-  sed -i 's/gcc/$(GCC)/g' Makefile
-  sed -i 's/g++/$(GXX)/g' Makefile
-  sed -i '1iGCC ?= gcc' Makefile
-  sed -i '1iGXX ?= g++' Makefile
+	# Trying to fix build when gcc or g++ are required
+	echo "LINUX: Patching makefile"
+	sed -i 's/gcc/gcc $(LDFLAGS)/g' Makefile
+	sed -i 's/g++/g++ $(LDFLAGS)/g' Makefile
+	sed -i 's/gcc/$(GCC)/g' Makefile
+	sed -i 's/g++/$(GXX)/g' Makefile
+	sed -i '1iGCC ?= gcc' Makefile
+	sed -i '1iGXX ?= g++' Makefile
 fi
 
-mkdir -p "$PREFIX"/bin
 
 echo "## Automatic build: install deps"
-#nimble build -y --verbose 
 nimble install -y --depsOnly
 echo "## Automatic build: make"
 make
 
 ./bin/seqfu version || true
 
-
-# Not necessary using `make`
-#if [[ -d scripts ]]; then
-#  echo "## Copying utils"
-#  chmod +x scripts/*
-#  cp scripts/* bin/
-#fi
-
 echo "## Current dir: $(pwd)"
-mv bin/* "$PREFIX"/bin/
-
+install -v -m 0755 bin/* "$PREFIX/bin"
 
 echo "## List files in \$PREFIX/bin:"
-ls -ltr "$PREFIX"/bin/
- 
+ls -ltr "$PREFIX/bin"
