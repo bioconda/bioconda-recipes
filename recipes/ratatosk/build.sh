d818d5fc5834d8b695c96142bc784676a83c5b16
mencian <joshua.zhuang@yahoo.com>
2025-08-03 23:30:21 -0500

    Ratatosk: add arm64 build

--- recipes/ratatosk/build.sh
+++ recipes/ratatosk/build.sh
@@ -5,21 +5,38 @@ mkdir -p "${PREFIX}/bin"
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64"
 
-export CXXFLAGS="${CXXFLAGS} -O3 -D_FILE_OFFSET_BITS=64 -I${PREFIX}/include"
-
-if [[ `uname` == Darwin ]]; then
+if [[ `uname -s` == "Darwin" ]]; then
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 else
 	export CONFIG_ARGS=""
 fi
 
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	sed -i.bak 's|-DCOMPILATION_ARCH=OFF|-DCOMPILATION_ARCH=ON|' src/CMakeLists.txt
+	rm -rf src/*.bak
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 cmake -S . -B build \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
-cmake --build build/ --target install -j "${CPU_COUNT}" -v
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
