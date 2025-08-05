9721141fb6e806b91d33bddda3dc6f8f1f526844
mencian <joshua.zhuang@yahoo.com>
2025-08-05 17:42:38 -0500

    phipack: add arm64 build

--- recipes/phipack/build.sh
+++ recipes/phipack/build.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 # There's a . directory that's ignored on OSX but not Linux
-if [[ $(uname -s) == "Darwin" ]]; then
-cd src
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	cd src
 else
-cd PhiPack/src/
+	cd PhiPack/src/
 fi
-mkdir -p $PREFIX/bin
 
-make CXX=$CC CXXFLAGS="$CFLAGS"
-cp Phi $PREFIX/bin
-cp -r ppma_2_bmp $PREFIX/bin
-cp Profile $PREFIX/bin
+mkdir -p "$PREFIX/bin"
 
-chmod -R +x $PREFIX/bin/*
+make CXX="${CC}" CXXFLAGS="${CFLAGS}" -j"${CPU_COUNT}"
+
+install -v -m 0755 Phi Profile "${PREFIX}/bin"
+cp -rf ppma_2_bmp "$PREFIX/bin"
