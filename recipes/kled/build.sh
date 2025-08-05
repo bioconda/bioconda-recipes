66bea493a2f60150fda4cd27cb48c44f0a42b965
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-05 16:26:26 -0500

    Update build.sh

--- recipes/kled/build.sh
+++ recipes/kled/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -fPIE"
+export CXXFLAGS="${CXXFLAGS} -O3 -fPIE"
 
 if [[ `uname -s` == "Darwin" ]]; then
     export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
@@ -15,7 +15,7 @@ fi
 cmake -S . -B build \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_STANDARD=14 \
+    -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -Wno-dev -Wno-deprecated --no-warn-unused-cli \
