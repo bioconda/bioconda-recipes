aace3a967b80405d0746ddc017e27707f37f44e5
mencian <joshua.zhuang@yahoo.com>
2025-08-05 19:42:12 -0500

    gadem: add arm64 build

--- recipes/gadem/build.sh
+++ recipes/gadem/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -ex
-./configure --prefix=$PREFIX 
-ln -s gadem_documentation_v1.3.1.pdf doc/GADEM_documentation.pdf
-make
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-deprecated-declarations"
+
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	CC="${CC}" \
+	CFLAGS="${CFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}" \
+	--disable-option-checking --enable-silent-rules --enable-dependency-tracking
+
+ln -sf gadem_documentation_v1.3.1.pdf doc/GADEM_documentation.pdf
+
+make -j"${CPU_COUNT}"
 make install
