b4c718876689e2d3e0577205b80091bde4d42f28
neng <neng@ds.dfci.harvard.edu>
2025-08-07 21:55:15 +0000

    Update longcallR to v1.12.0

--- recipes/longcallr/build.sh
+++ recipes/longcallr/build.sh
@@ -8,3 +8,4 @@ rm -rf "$PREFIX/.crates*"
 # Install Python scripts to $PREFIX/bin
 install -m 755 allele_specific/longcallR-asj.py "$PREFIX/bin/longcallR-asj"
 install -m 755 allele_specific/longcallR-ase.py "$PREFIX/bin/longcallR-ase"
+install -m 755 allele_specific/asj_to_bed.py "$PREFIX/bin/asj_to_bed"
