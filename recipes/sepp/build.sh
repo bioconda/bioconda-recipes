53646c3fbd5c9f3b9065664915a66b731eb64979
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-09 17:57:07 -0500

    Update build.sh

--- recipes/sepp/build.sh
+++ recipes/sepp/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+${PYTHON} -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
+
+config_sepp -c
+config_upp -c
+
 # ensure SEPP's configuration file is at the correct location ...
 echo "${PREFIX}/share/sepp/sepp" > home.path
 # ensure directory is created ... ($SP_DIR = Python's site-packages location, see https://docs.conda.io/projects/conda-build/en/stable/user-guide/environment-variables.html#environment-variables-set-during-the-build-process)
@@ -11,16 +16,11 @@ mkdir -p "${PREFIX}/share/sepp/sepp"
 # ... and holds correct path names
 mv -v sepp-package/sepp/default.main.config $PREFIX/share/sepp/sepp/main.config
 # copy upp config, as it's still needed
-cp -f ./.sepp/upp.config $PREFIX/share/sepp/sepp/upp.config
+cp -f ${PREFIX}/share/sepp/sepp/.sepp/upp.config $PREFIX/share/sepp/sepp/upp.config
 
 # replace $PREFIX with /opt/anaconda1anaconda2anaconda3 for later replacement of concrete build PREFIX
 # note: can't apply a patch here, as upp.config is not part of upstream but gets generated during python setup
 sed -i.bak "s|path=.*/|path=/opt/anaconda1anaconda2anaconda3/bin/|g" $PREFIX/share/sepp/sepp/upp.config
-
-${PYTHON} -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
-
-config_sepp -c
-config_upp -c
 
 # copy bundled binaries, but hmmer which should be provided by conda, into $PREFIX/bin/
 mkdir -p "${PREFIX}/bin"
