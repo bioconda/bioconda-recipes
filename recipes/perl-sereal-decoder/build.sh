67b33575265051a61b0edba4c015122d4f508d3d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-10 12:54:44 -0500

    Update build.sh

--- recipes/perl-sereal-decoder/build.sh
+++ recipes/perl-sereal-decoder/build.sh
@@ -3,16 +3,16 @@
 export LC_ALL=C
 
 # If it has Build.PL use that, otherwise use Makefile.PL
-if [ -f Build.PL ]; then
+if [[ -f Build.PL ]]; then
     perl Build.PL
     ./Build
     ./Build test
     # Make sure this goes in site
     ./Build install --installdirs site
-elif [ -f Makefile.PL ]; then
+elif [[ -f Makefile.PL ]]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
-    make
+    make -j"${CPU_COUNT}"
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
