217972e53cc15b14cca98b1cf8197758f2181e62
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-10 18:15:11 -0500

    Update build.sh

--- recipes/svmlight/build.sh
+++ recipes/svmlight/build.sh
@@ -7,10 +7,10 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
 
 # ## Binary install with wrappers
-if [[ ${target_platform}  == "linux-aarch64" ]]; then
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
  	sed -i "14c CFLAGS=  \$(SFLAGS) -O3 -Xlinker -zmuldefs                    # release C-Compiler flags " Makefile
 	sed -i "15c LFLAGS=  \$(SFLAGS) -O3 -Xlinker -zmuldefs                    # release linker flags" Makefile
-	make all CC="${CC}" LD="${CC}" LFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+	make all CC="${CC}" LD="${CC}" CFLAGS="${CFLAGS} -fcommon" LFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 else
 	make all CC="${CC}" LD="${CC}" CFLAGS="${CFLAGS} -fcommon" LFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 fi
