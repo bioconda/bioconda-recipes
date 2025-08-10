d9d7ea6a2fe5914bdd0556bfba4d41989db62407
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-10 18:08:24 -0500

    Update build.sh

--- recipes/svmlight/build.sh
+++ recipes/svmlight/build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# ## Binary install with wrappers
+mkdir -p "${PREFIX}/bin"
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
+# ## Binary install with wrappers
 if [[ ${target_platform}  == "linux-aarch64" ]]; then
  	sed -i "14c CFLAGS=  \$(SFLAGS) -O3 -Xlinker -zmuldefs                    # release C-Compiler flags " Makefile
 	sed -i "15c LFLAGS=  \$(SFLAGS) -O3 -Xlinker -zmuldefs                    # release linker flags" Makefile
-	make 
+	make all CC="${CC}" LD="${CC}" LFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 else
-	make CC="${CC}" LD="${CC}" CFLAGS=" -fcommon ${CFLAGS}" LFLAGS="${LDFLAGS}" all
+	make all CC="${CC}" LD="${CC}" CFLAGS="${CFLAGS} -fcommon" LFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
 fi
 
-mkdir -p "${PREFIX}/bin"
-
-mv svm_learn svm_classify "${PREFIX}/bin/"
+install -v -m 0755 svm_learn svm_classify "${PREFIX}/bin"
