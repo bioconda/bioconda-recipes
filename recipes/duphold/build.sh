8f8b17c0c18e925d5ec505b0b70691c7429b8868
mencian <joshua.zhuang@yahoo.com>
2025-08-10 14:04:20 -0500

    Update duphold to v0.2.3

--- recipes/duphold/build.sh
+++ recipes/duphold/build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -eu -o pipefail
 
+mkdir -p "${PREFIX}/bin"
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+
+if [[ "$(uname -m)" == "arm64" ]]; then
+	nim_build="macosx_arm64"
+	curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-2-2/${nim_build}.tar.xz -o ${nim_build}.tar.xz
+	unxz -c ${nim_build}.tar.xz | tar -x
+
+	cd nim-2.2.*
+	export PATH="${PWD}/bin:${PATH}"
+	cd ..
+fi
+
 pushd hts-nim
 nimble install -y --verbose
 popd
@@ -9,7 +25,6 @@ pushd genoiser
 nimble install -y --verbose
 popd
 
-nimble install -y --verbose
+nimble install -y --verbose -d:release
 
-mkdir -p "${PREFIX}/bin"
-mv duphold "${PREFIX}/bin/"
+install -v -m 0755 duphold "${PREFIX}/bin"
