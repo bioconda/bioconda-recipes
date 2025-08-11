d4f14b6719c90671627c11bf98f97fd30bfcd41c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-11 15:37:27 -0500

    Update build.sh

--- recipes/gustaf/1.0.8/build.sh
+++ recipes/gustaf/1.0.8/build.sh
@@ -2,13 +2,35 @@
 
 mkdir -p "$PREFIX/bin"
 
-if [[ "${target_platform}" == "linux-aarch64" ]]; then
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+esac
+
+if [[ "${target_platform}" == "linux-aarch64" || "${target_platform}" == "osx-arm64" ]]; then
+
 mkdir -p install
 
 cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="${SRC_DIR}/install" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
-	-Wno-dev -Wno-deprecated --no-warn-unused-cli
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
 
 ninja -C build -j "${CPU_COUNT}" gustaf
 ninja -C build -j "${CPU_COUNT}" gustaf_mate_joining
