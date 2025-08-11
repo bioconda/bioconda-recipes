bac5ddb2aebacf7172260486820ce86e3f372c8a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-11 16:22:01 -0500

    gustaf v1.0.10: add aarch64/arm64 builds (#58458)

--- recipes/gustaf/build.sh
+++ recipes/gustaf/build.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-cd bin
+mkdir -p "$PREFIX/bin"
 
-binaries="\
-gustaf \
-gustaf_mate_joining \
-"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-copy"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wno-deprecated-copy"
 
-for i in ${binaries}; do install -v -m 755 $SRC_DIR/bin/$i "$PREFIX/bin"; done
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+	export MACOSX_DEPLOYMENT_TARGET=11.3
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+
+ninja -C build -j "${CPU_COUNT}" gustaf
+ninja -C build -j "${CPU_COUNT}" gustaf_mate_joining
+
+install -v -m 755 build/bin/gustaf* "${PREFIX}/bin"
