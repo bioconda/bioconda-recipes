d8db8e5337e70ac78a4051807a2e4f062e7fa1f4
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-11 16:17:26 -0500

    Update build.sh

--- recipes/kraken-ea/build.sh
+++ recipes/kraken-ea/build.sh
@@ -13,6 +13,14 @@ sed -i.bak 's|AR       =|#AR       =|' src/Makefile
 sed -i.bak 's|${AR}|$(AR) rcs|' src/Makefile
 rm -rf src/*.bak
 
+if [[ `uname -s` == "Darwin" ]]; then
+	ln -sf ${CC} ${PREFIX}/bin/clang
+	ln -sf ${CXX} ${PREFIX}/bin/clang++
+else
+	ln -sf ${CC} ${PREFIX}/bin/gcc
+	ln -sf ${CXX} ${PREFIX}/bin/g++
+fi
+
 chmod u+x install_kraken_conda.sh
 
 ./install_kraken_conda.sh "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
@@ -22,3 +30,11 @@ for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kra
     chmod +x "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/$bin"
     ln -s "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/$bin" "$PREFIX/bin/$bin"
 done
+
+if [[ `uname -s` == "Darwin" ]]; then
+	rm -rf ${PREFIX}/bin/clang
+	rm -rf ${PREFIX}/bin/clang++
+else
+	rm -rf ${PREFIX}/bin/gcc
+	rm -rf ${PREFIX}/bin/g++
+fi
