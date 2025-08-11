c67f094271e6085de1a4df3aba18b907f13953f7
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-11 16:32:23 -0500

    Update build.sh

--- recipes/kraken-ea/build.sh
+++ recipes/kraken-ea/build.sh
@@ -8,18 +8,12 @@ export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib -L. -lkraken -lz"
 export CPATH="${PREFIX}/include"
 
-sed -i.bak 's|CXX =|#CXX =|' src/Makefile
+sed -i.bak "s|g++|${CXX}|" src/Makefile
 sed -i.bak 's|AR       =|#AR       =|' src/Makefile
-sed -i.bak 's|${AR}|$(AR) rcs|' src/Makefile
+sed -i.bak 's|${AR}|${AR} rcs|' src/Makefile
+sed -i.bak 's|$(CXX)|${CXX}|' src/Makefile
+sed -i.bak 's|$(CXXFLAGS)|${CXXFLAGS}|' src/Makefile
 rm -rf src/*.bak
-
-if [[ `uname -s` == "Darwin" ]]; then
-	ln -sf ${CC} ${PREFIX}/bin/clang
-	ln -sf ${CXX} ${PREFIX}/bin/clang++
-else
-	ln -sf ${CC} ${PREFIX}/bin/gcc
-	ln -sf ${CXX} ${PREFIX}/bin/g++
-fi
 
 chmod u+x install_kraken_conda.sh
 
@@ -30,11 +24,3 @@ for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kra
     chmod +x "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/$bin"
     ln -s "$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM/$bin" "$PREFIX/bin/$bin"
 done
-
-if [[ `uname -s` == "Darwin" ]]; then
-	rm -rf ${PREFIX}/bin/clang
-	rm -rf ${PREFIX}/bin/clang++
-else
-	rm -rf ${PREFIX}/bin/gcc
-	rm -rf ${PREFIX}/bin/g++
-fi
