095d73bb265cdca7faaec38e05394eba3262be1d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-14 16:43:52 -0500

    Update build.sh

--- recipes/perl-extutils-cppguess/build.sh
+++ recipes/perl-extutils-cppguess/build.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-if [ -f Build.PL ]; then
+export LC_ALL="en_US.UTF-8"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+if [[ -f Build.PL ]]; then
     perl Build.PL
     perl ./Build
     perl ./Build test
     # Make sure this goes in site
     perl ./Build install --installdirs site
-elif [ -f Makefile.PL ]; then
+elif [[ -f Makefile.PL ]]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
-    make -j"${CPU_COUNT}"
+    perl Makefile.PL INSTALLDIRS=site cc="${CC}" ld="${CC}"
+    make
     make test
     make install
 else
