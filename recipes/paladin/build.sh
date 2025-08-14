43557605d00c3c73c05579023f7fd54379e62950
mencian <joshua.zhuang@yahoo.com>
2025-08-13 21:47:38 -0500

    paladin: add aarch64/arm64 builds

--- recipes/paladin/build.sh
+++ recipes/paladin/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-if [[ $(uname -s) == "Darwin" ]]; then
-    make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread" INCLUDES="$CFLAGS" -j${CPU_COUNT}
+
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	make CC="${CC}" LIBS="${LDFLAGS} -lm -lz -pthread" INCLUDES="${CFLAGS}" -j"${CPU_COUNT}"
 else
-    make CC=$CC LIBS="$LDFLAGS -lm -lz -lpthread -lrt" INCLUDES="$CFLAGS" -j${CPU_COUNT}
+	make CC="${CC}" LIBS="${LDFLAGS} -lm -lz -pthread -lrt" INCLUDES="${CFLAGS}" -j"${CPU_COUNT}"
 fi
-install -m 0755 paladin ${PREFIX}/bin
+
+install -m 0755 paladin "${PREFIX}/bin"
