e24178514eaed79d5c26c00012deb99f14e617dc
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-15 17:34:50 -0500

    perl-set-intervaltree: add arm64 build (#58584)

--- recipes/perl-extutils-cppguess/build.sh
+++ recipes/perl-extutils-cppguess/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -f Build.PL ]; then
+export LC_ALL="en_US.UTF-8"
+
+if [[ -f Build.PL ]]; then
     perl Build.PL
     perl ./Build
     perl ./Build test
     # Make sure this goes in site
     perl ./Build install --installdirs site
-elif [ -f Makefile.PL ]; then
+elif [[ -f Makefile.PL ]]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
