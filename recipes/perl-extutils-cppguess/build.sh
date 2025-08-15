2a76e16e1b235f61c7cfa2492996b6e09e0c3d5c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-14 22:39:10 -0500

    Update build.sh

--- recipes/perl-extutils-cppguess/build.sh
+++ recipes/perl-extutils-cppguess/build.sh
@@ -19,9 +19,9 @@ if [[ -f Build.PL ]]; then
     perl ./Build install --installdirs site
 elif [[ -f Makefile.PL ]]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site --config CC="${CXX}"
-    make CC="${CXX}"
-    make test CC="${CXX}" -j"${CPU_COUNT}"
+    perl Makefile.PL INSTALLDIRS=site
+    make
+    make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
