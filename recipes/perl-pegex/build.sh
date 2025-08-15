4fe5b0836b52377d37e15439f107f1b358fbdaae
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-15 03:53:42 -0500

    Update build.sh

--- recipes/perl-pegex/build.sh
+++ recipes/perl-pegex/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# If it has Build.PL use that, otherwise use Makefile.PL
+export LC_ALL="en_US.UTF-8"
+
 if [ -f Build.PL ]; then
     perl Build.PL
     ./Build
@@ -11,15 +12,9 @@ elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
-    make test
+    make test -j"${CPU_COUNT}"
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
