6f5b06e453c840197de6c2bca9e9ae151cfdfe06
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-15 05:03:43 -0500

    Update build.sh

--- recipes/perl-yaml-pp/build.sh
+++ recipes/perl-yaml-pp/build.sh
@@ -2,15 +2,21 @@
 
 export LC_ALL="en_US.UTF-8"
 
-if [ -f Build.PL ]; then
+sed -i.bak 's|v0.39.0|0.39.0|' lib/YAML/*.pm
+rm -rf lib/YAML/*.bak
+sed -i.bak 's|v0.39.0|0.39.0|' META.*
+sed -i.bak 's|v0.39.0|0.39.0|' Makefile.PL
+rm -rf *.bak
+
+if [[ -f Build.PL ]]; then
     perl Build.PL
     perl ./Build
     perl ./Build test
     # Make sure this goes in site
     perl ./Build install --installdirs site
-elif [ -f Makefile.PL ]; then
+elif [[ -f Makefile.PL ]]; then
     # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
+    perl Makefile.PL INSTALLDIRS=site NO_PACKLIST=1 NO_PERLLOCAL=1
     make
     make test -j"${CPU_COUNT}"
     make install
