1715e6429a24597bb84cc4ff6989e751e8c64f29
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-15 06:34:57 -0500

    Update perl-xxx to v0.38 (#58576)

---
+++ recipes/perl-xxx/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export LC_ALL="en_US.UTF-8"
+
+if [[ -f Build.PL ]]; then
+    perl Build.PL
+    perl ./Build
+    perl ./Build test
+    perl ./Build install --installdirs site
+elif [[ -f Makefile.PL ]]; then
+    perl Makefile.PL INSTALLDIRS=site NO_PACKLIST=1 NO_PERLLOCAL=1
+    make
+    make test
+    make install
+else
+    echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
+    exit 1
+fi
