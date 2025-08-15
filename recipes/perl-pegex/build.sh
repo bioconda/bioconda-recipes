174c80e943f301e757cde93095b67a84b514490d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-15 18:33:38 -0500

    Update build.sh

--- recipes/perl-pegex/build.sh
+++ recipes/perl-pegex/build.sh
@@ -2,21 +2,15 @@
 
 export LC_ALL="en_US.UTF-8"
 
-sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' inc/bin/testml-cpan
-rm -rf inc/bin/*.bak
-chmod 755 inc/bin/testml-cpan
-
 if [[ -f Build.PL ]]; then
     perl Build.PL
     ./Build
     ./Build test
-    # Make sure this goes in site
     ./Build install --installdirs site
 elif [[ -f Makefile.PL ]]; then
-    # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
     make
-    make test
+    #make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
