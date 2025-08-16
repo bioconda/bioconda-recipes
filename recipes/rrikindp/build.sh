266804d9740ab33bbeca16a3917e2df2eda8e756
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-15 21:46:27 -0500

    Update build.sh

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -1,12 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ ${target_platform} == osx-64 ]]; then
-  export CONDA_BUILD_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+mkdir -p "${PREFIX}/bin"
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+if [[ "${target_platform}" == "osx-64" ]]; then
+  export CONDA_BUILD_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 fi
 
-$PYTHON -m pip install . --no-deps --ignore-installed -vv
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' setup.py
+	;;
+    arm64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' setup.py
+	;;
+esac
+
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
 
 cd src/rrikindp
 
-make -j ${CPU_COUNT} CXXFLAGS+="-I${CONDA_PREFIX}/include"
-install RRIkinDP "${PREFIX}/bin"
+make CXXFLAGS+="-I${CONDA_PREFIX}/include" -j"${CPU_COUNT}"
+install -v -m 0755 RRIkinDP "${PREFIX}/bin"
