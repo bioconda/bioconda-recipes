b0333d419e5907238b7ea009e3abdced036d3d87
j34ni <jeani@uio.no>
2025-08-16 13:35:33 +0000

    Remove patch

--- recipes/esme/build_hdf5.sh
+++ recipes/esme/build_hdf5.sh
@@ -6,6 +6,10 @@ export CC=mpicc
 export CXX=mpicxx
 export FC=mpifort
 
+# Bypass mpiexec dependency and runtime checks
+export RUNPARALLEL=none
+export cross_compiling=yes  # Skip runtime checks by enabling cross-compilation mode
+
 cd esme_hdf5
 
 ./configure --prefix="${PREFIX}" \
