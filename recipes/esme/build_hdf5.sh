a7021fa4040a862dc25e96a0848e8f80e4883dda
j34ni <jeani@uio.no>
2025-08-16 16:03:26 +0000

    Add explicit Fortran MPI library linking

--- recipes/esme/build_hdf5.sh
+++ recipes/esme/build_hdf5.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -ex
 
 # Set MPI environment variables for PsMPI
@@ -14,12 +13,16 @@ export MPI_C_COMPILER=mpicc
 export MPI_CXX_COMPILER=mpicxx
 export MPI_Fortran_COMPILER=mpifort
 
-# Set compiler flags to ensure proper linking
+# Set compiler flags to ensure proper linking including Fortran MPI libraries
 export CFLAGS="-fPIC $CFLAGS"
 export CXXFLAGS="-fPIC $CXXFLAGS"
 export FCFLAGS="-fPIC $FCFLAGS"
 export FFLAGS="-fPIC $FFLAGS"
 
+# Add explicit Fortran MPI library linking
+export LIBS="-lmpifort $LIBS"
+
+# CRITICAL: Override conda's compiler settings with MPI wrappers
 export CC=mpicc
 export CXX=mpicxx
 export FC=mpifort
@@ -28,20 +31,17 @@ export RUNSERIAL="prterun -n 1"
 export RUNPARALLEL="prterun -n \$\${NPROCS:=6}"
 
 cd esme_hdf5
-
 ./configure --prefix="${PREFIX}" \
             --with-zlib="${PREFIX}" \
             --with-szlib="${PREFIX}" \
             --enable-fortran \
-	    --enable-parallel \
+            --enable-parallel \
             --enable-threadsafe \
-	    --enable-unsupported \
-	    --enable-optimization=high \
+            --enable-unsupported \
+            --enable-optimization=high \
             --enable-build-mode=production \
-	    --disable-dependency-tracking \
+            --disable-dependency-tracking \
             --enable-static=no \
-	    --disable-doxygen-doc
-
+            --disable-doxygen-doc
 make -j ${CPU_COUNT}
-
 make install
