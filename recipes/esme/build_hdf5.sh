0dc676e3de7020ef949a46a38ab521cb4fca8582
j34ni <jeani@uio.no>
2025-08-16 07:38:19 +0000

    Update ESMF to 8.9.0 and bypass MPI-IO check for HDF5

--- recipes/esme/build_hdf5.sh
+++ recipes/esme/build_hdf5.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-
 set -ex
 
 export CC=mpicc
 export FC=mpifort
+export MPICC=mpicc
+export MPIFC=mpifort
+export CPPFLAGS="-I${CONDA_PREFIX}/include"
+export FCFLAGS="-fPIC -fallow-argument-mismatch -I${CONDA_PREFIX}/include"
+export FFLAGS="-fPIC -fallow-argument-mismatch -I${CONDA_PREFIX}/include"
+export RUNSERIAL="prterun -n 1"
+export RUNPARALLEL="prterun -n ${NPROCS:=${CPU_COUNT}}"
 
 cd esme_hdf5
 
+# Configure
 ./configure --prefix="${PREFIX}" \
             --with-zlib="${PREFIX}" \
             --with-szlib="${PREFIX}" \
+            --enable-parallel \
             --enable-fortran \
-	    --enable-parallel \
             --enable-threadsafe \
-	    --enable-unsupported \
-	    --enable-optimization=high \
+            --enable-unsupported \
+            --enable-optimization=high \
             --enable-build-mode=production \
-	    --disable-dependency-tracking \
+            --disable-dependency-tracking \
             --enable-static=no \
-	    --disable-doxygen-doc
+            --disable-doxygen-doc
 
 make -j ${CPU_COUNT}
-
 make install
