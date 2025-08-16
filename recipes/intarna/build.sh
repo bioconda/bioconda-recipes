003b2f6ecb904ca89cbcd364ed74d9f576f899a2
mencian <joshua.zhuang@yahoo.com>
2025-08-15 22:27:18 -0500

    intarna: add arm64 build

--- recipes/intarna/build.sh
+++ recipes/intarna/build.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 
-## Choose extra configure options depending on the operating system
-## (mac or linux)
-##
-
-
 # suppress warnings
 # add -fopenmp to compilation due to viennarna setup
-CXXFLAGS="$CXXFLAGS -w -fopenmp"; 
-LDFLAGS="$LDFLAGS -Wl,-rpath ${PREFIX}/lib";
+export CXXFLAGS="${CXXFLAGS} -O3 -w -fopenmp";
+export LDFLAGS="${LDFLAGS} -Wl,-rpath ${PREFIX}/lib";
 
-if [ `uname` == Darwin ] ; then
-    CXXFLAGS="$CXXFLAGS -stdlib=libc++"
-    LDFLAGS="$LDFLAGS -stdlib=libc++"
-    extra_config_options="$extra_config_options --disable-pkg-config"
-else ## linux
-    CXXFLAGS="$CXXFLAGS"
+if [[ `uname -s` == "Darwin" ]] ; then
+	export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+	export LDFLAGS="${LDFLAGS} -stdlib=libc++"
+	export extra_config_options="${extra_config_options} --disable-pkg-config"
+else  ## linux
+	export CXXFLAGS="${CXXFLAGS}"
 fi
 
-export CC=${CC}
-export CXX=${CXX}
-export CXXFLAGS=${CXXFLAGS}
-export LDFLAGS=${LDFLAGS}
-
-./configure --prefix=$PREFIX \
-            --with-vrna=$PREFIX \
-            --with-boost=$PREFIX \
-            --with-zlib=$PREFIX \
-            --disable-log-coloring \
-            --with-boost-libdir=$PREFIX/lib \
-            --disable-intarnapvalue \
-            ${extra_config_options}
+./configure --prefix="${PREFIX}" \
+	--with-vrna="${PREFIX}" \
+	--with-boost="${PREFIX}" \
+	--with-zlib="${PREFIX}" \
+	--disable-log-coloring \
+	--with-boost-libdir="${PREFIX}/lib" \
+	--disable-intarnapvalue \
+	${extra_config_options}
 
 
-make -j ${CPU_COUNT}
-if [ `uname` != Darwin ] ; then # skip tests for osx to avoid timeout
-    make tests -j ${CPU_COUNT}
+make -j"${CPU_COUNT}"
+if [[ `uname -s` != "Darwin" ]]; then  # skip tests for osx to avoid timeout
+	make tests -j"${CPU_COUNT}"
 fi
 make install
