de00fa47430ecbc84f880e7d585fc12ed2ae8e9b
j34ni <jeani@uio.no>
2025-08-16 18:28:33 +0000

    Add sed -i 's/mpiexec/prterun/g' configure

--- recipes/esme/build_hdf5.sh
+++ recipes/esme/build_hdf5.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ex
 
+sed -i 's/mpiexec/prterun/g' configure
+sed -i 's/mpirun/prterun/g' configure
+
 export CC=mpicc
 export CXX=mpicxx
 export FC=mpifort
+
 export RUNSERIAL="prterun -n 1"
 export RUNPARALLEL="prterun -n \$\${NPROCS:=6}"
 
 cd esme_hdf5
-
-# Replace the failing test with a success condition
-sed -i '/checking whether a simple MPI-IO Fortran program can be linked/,/configure: error: unable to link a simple MPI-IO Fortran program/c\
-echo "checking whether a simple MPI-IO Fortran program can be linked... yes (forced for PsMPI)"' configure
 
 ./configure --prefix="${PREFIX}" \
             --with-zlib="${PREFIX}" \
