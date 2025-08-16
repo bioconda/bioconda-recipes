4b460659a4808478ea0e48dcc721208569bedd62
j34ni <jeani@uio.no>
2025-08-16 07:38:19 +0000

    Update ESMF to 8.9.0 and bypass MPI-IO check for HDF5

--- recipes/esme/build_hdf5.sh
+++ recipes/esme/build_hdf5.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+# Set cache variables to bypass MPI runtime tests
+export ac_cv_lib_mpi_MPI_File_open=yes
+export hdf5_cv_mpi_io=yes
+export hdf5_cv_mpi_special_collective_io_works=yes
+export hdf5_cv_mpi_complex_derived_datatype_works=yes
+
 export CC=mpicc
 export FC=mpifort
 
