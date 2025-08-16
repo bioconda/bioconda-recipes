b76ff26e74228cb2678c45f9fdfe4fc56743fc46
bo1929 <asapci@ucsd.edu>
2025-08-16 14:23:41 -0700

    Changed makefile for openmp on osx

--- recipes/krepp/build.sh
+++ recipes/krepp/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -xe
 
-if [ "$(uname -s)" == Darwin ] ; then
- 	CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
-export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CXXFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
 
 make -j ${CPU_COUNT} COMPILER=$CXX
 chmod 0755 krepp
