6d2433ca4b4d5a52a4ab26a12e6807010080887f
Hyunbin Kim <khb7840@gmail.com>
2025-08-18 01:37:00 +0900

    Add C++ linking flags for macOS in build script and update meta.yaml dependencies

--- recipes/folddisco/build.sh
+++ recipes/folddisco/build.sh
@@ -4,5 +4,11 @@
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
 
+# Set C++ linking flags for macOS to ensure proper C++ standard library linking
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export RUSTFLAGS="-C link-arg=-lc++ -C link-arg=-L${PREFIX}/lib"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+fi
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1 cargo install --features foldcomp --verbose --path . --root $PREFIX
