d9a65e602c2f41baa9cf403b65eca85f87731221
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-18 16:57:12 -0500

    Update build.sh

--- recipes/r-signac/build.sh
+++ recipes/r-signac/build.sh
@@ -2,8 +2,10 @@
 set -o errexit -o pipefail
 
 export DISABLE_AUTOBREW=1
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-if [[ "${target_platform}" =~ linux.* ]] || [[ "${target_platform}" == "win-32" ]] || [[ "${target_platform}" == "win-64" ]] || [[ "${target_platform}" == "osx-64" ]]; then
+if [[ "${target_platform}" =~ linux.* ]] || [[ "${target_platform}" == "win-32" ]] || [[ "${target_platform}" == "win-64" ]] || [[ "${target_platform}" == "osx-64" ]] || [[ "${target_platform}" == "osx-arm64" ]]; then
   mv DESCRIPTION DESCRIPTION.old
   grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
   ${R} CMD INSTALL --build . "${R_ARGS}"
