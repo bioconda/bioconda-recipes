b5889ba23969f606943b465f49e761c02509ebff
mencian <joshua.zhuang@yahoo.com>
2025-08-18 21:10:31 -0500

    fastq-tools: add aarch64/arm64 builds

--- recipes/fastq-tools/build.sh
+++ recipes/fastq-tools/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-./autogen.sh
-./configure --prefix=$PREFIX
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-deprecated-declarations"
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	CC="${CC}" CFLAGS="${CFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" \
+	--enable-silent-rules --disable-dependency-tracking --disable-option-checking
+
+make -j"${CPU_COUNT}"
 make install
