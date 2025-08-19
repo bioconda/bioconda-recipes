adeda875ef65e6a4e04b37fbabeda28fcb18e6df
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-18 23:52:08 -0500

    Update build.sh

--- recipes/angsd/build.sh
+++ recipes/angsd/build.sh
@@ -9,12 +9,18 @@ mkdir -p "${PREFIX}/bin"
 
 cd angsd
 
+sed -i.bak 's|-dm0755|-d|' Makefile
+sed -i.bak 's|-m0755|-m 0755|' Makefile
+rm -rf *.bak
+
 # '-D__STDC_FORMAT_MACROS' fix from https://github.com/ANGSD/angsd/issues/397
-make install-all \
-	HTSSRC="systemwide" \
+make HTSSRC="systemwide" \
 	CFLAGS="${CFLAGS}" \
 	CPPFLAGS="${CPPFLAGS}" \
 	prefix="${PREFIX}" \
 	CC="${CC}" \
 	CXX="${CXX}" \
-	FLAGS="-I${PREFIX}/include -L${PREFIX}/lib -D__STDC_FORMAT_MACROS"
+	FLAGS="-I${PREFIX}/include -L${PREFIX}/lib -D__STDC_FORMAT_MACROS" \
+	-j"${CPU_COUNT}"
+
+make install prefix="${PREFIX}"
