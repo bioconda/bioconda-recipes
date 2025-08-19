81bff59c404ae0f56ffe52792f929296d7c1699e
mencian <joshua.zhuang@yahoo.com>
2025-08-18 23:31:06 -0500

    angsd: add arm64 build

--- recipes/angsd/build.sh
+++ recipes/angsd/build.sh
@@ -1,10 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 
-mkdir -p ${PREFIX}/bin
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
 
 cd angsd
 
 # '-D__STDC_FORMAT_MACROS' fix from https://github.com/ANGSD/angsd/issues/397
-make HTSSRC="systemwide" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" prefix=$PREFIX \
- CC="$CC" CXX="$CXX" FLAGS="-I${PREFIX}/include -L${PREFIX}/lib -D__STDC_FORMAT_MACROS" \
- install-all
+make HTSSRC="systemwide" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" prefix="${PREFIX}" \
+	CC="$CC" CXX="$CXX" FLAGS="-I${PREFIX}/include -L${PREFIX}/lib -D__STDC_FORMAT_MACROS" \
+	install-all -j"${CPU_COUNT}"
