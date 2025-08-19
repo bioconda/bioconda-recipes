7d8e3da05997704f6e6e1b4b9b1c7acde4e74936
mencian <joshua.zhuang@yahoo.com>
2025-08-18 23:24:56 -0500

    Rebuild lra recipe

--- recipes/lra/build.sh
+++ recipes/lra/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-export CFLAGS="$CFLAGS -I$PREFIX/include"
+mkdir -p "$PREFIX/bin"
+
+export CFLAGS="$CFLAGS -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export CPATH="${PREFIX}/include"
+export CFLAGS="$CFLAGS -O3"
+export CXXFLAGS="$CXXFLAGS -O3"
 
-make CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"  CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+make CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"  CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" -j"${CPU_COUNT}"
 
-cp lra $PREFIX/bin
-
+install -v -m 0755 lra "$PREFIX/bin"
