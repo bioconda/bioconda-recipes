e31db32a13a816eb247b5d92232bdea9b661defa
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-08-19 11:39:39 +0800

    update unimap and enabled linux-aarch64 (#58640)
    * linux-aarch64 build for nrpys python <312
    
    * Revert "linux-aarch64 build for nrpys python <312"
    
    This reverts commit f8740fe8927ed0247bbce0cddb609f64c56aa7b4.
    
    * linux-aarch64 build for humann2
    
    * update humman2
    
    * update origin
    
    * Update rascaf and enabled aarch64
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * samtools 0.1.19 does not have osx-arm64; disable osx-arm64
    
    * Update meta.yaml
    
    * unimap
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update meta.yaml
    
    ---------
    
    Co-authored-by: root <root@localhost.localdomain>
    Co-authored-by: ZerryNi <996002763.com>
    Co-authored-by: cfmin09 <147251502+cfmin09@users.noreply.github.com>
    Co-authored-by: cfmin09 <1163105507@qq.com>
    Co-authored-by: Joshua Zhuang <71105179+mencian@users.noreply.github.com>
    Co-authored-by: root <1944474653@qq.com>

--- recipes/unimap/build.sh
+++ recipes/unimap/build.sh
@@ -1,9 +1,27 @@
-#! /bin/sh
+#!/bin/bash
 
-make \
-  CC="${CC}" \
-  CFLAGS="${CFLAGS}" \
-  CPPFLAGS="${CPPFLAGS} -DHAVE_KALLOC"
+export CPPFLAGS="${LDFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 
 install -d "${PREFIX}/bin"
-install unimap "${PREFIX}/bin/"
+
+if [[ "$(uname -m)" == "aarch64" ]] || [[ "$(uname -m)" == "arm64" ]]; then
+  export EXTRA_ARGS="arm_neon=1 aarch64=1"
+  git clone https://github.com/DLTcollab/sse2neon.git
+  cp sse2neon/sse2neon.h .
+  sed -i.bak '9c #include "sse2neon.h"'  ksw2_ll_sse.c
+  sed -i.bak '9c #include "sse2neon.h"'  ksw2_extz2_sse.c
+  sed -i.bak '10c #include "sse2neon.h"' ksw2_extd2_sse.c
+  sed -i.bak '10c #include "sse2neon.h"' ksw2_exts2_sse.c
+else
+  export EXTRA_ARGS=""
+fi
+
+make CC="${CC}" \
+  CFLAGS="${CFLAGS}" \
+  CPPFLAGS="${CPPFLAGS} -DHAVE_KALLOC" \
+  "${EXTRA_ARGS}" \
+  -j"${CPU_COUNT}"
+
+install -v -m 0755 unimap "${PREFIX}/bin"
