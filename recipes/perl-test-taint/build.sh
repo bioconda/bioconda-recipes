e120576ae1735040dcc581211d250378ec44e4a0
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-18 20:25:13 -0500

    Add recipe for perl-test-taint v1.08 (#58633)
    * Add recipe for perl-test-taint v1.08
    
    * remove from blacklist
    
    * Update build.sh

--- recipes/perl-test-taint/build.sh
+++ recipes/perl-test-taint/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -o errexit -o pipefail
 
 export LC_ALL="en_US.UTF-8"
-export OPENSSL_PREFIX="${PREFIX}"
 
 if [[ -f Build.PL ]]; then
     perl Build.PL
@@ -9,7 +9,6 @@ if [[ -f Build.PL ]]; then
     perl ./Build test
     perl ./Build install --installdirs site
 elif [[ -f Makefile.PL ]]; then
-    # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site NO_PACKLIST=1 NO_PERLLOCAL=1
     make
     make test -j"${CPU_COUNT}"
