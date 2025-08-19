99a8079dffcd2d01e5849d95f5f66e10481266f2
root <1944474653@qq.com>
2025-08-19 10:21:57 +0800

    unimap

--- recipes/unimap/build.sh
+++ recipes/unimap/build.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 
-make \
+git clone https://github.com/DLTcollab/sse2neon.git
+cp sse2neon/sse2neon.h .
+sed -i '9c #include "sse2neon.h"'  ksw2_ll_sse.c
+sed -i '9c #include "sse2neon.h"'  ksw2_extz2_sse.c
+sed -i '10c #include "sse2neon.h"' ksw2_extd2_sse.c
+sed -i '10c #include "sse2neon.h"' ksw2_exts2_sse.c
+
+make arm_neon=1 aarch64=1\
   CC="${CC}" \
   CFLAGS="${CFLAGS}" \
   CPPFLAGS="${CPPFLAGS} -DHAVE_KALLOC"
