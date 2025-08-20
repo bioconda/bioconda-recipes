f36d639c2be0615e65a04043b984d40903fddac1
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-20 00:02:53 -0500

    Update build.sh

--- recipes/hi-corrector/build.sh
+++ recipes/hi-corrector/build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -euxo pipefail
 
-cd "$SRC_DIR/src"
-if [[ $target_platform == 'linux-aarch64' ]]; then
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+
+sed -i.bak 's|-O3|-O3 -Wno-deprecated-declarations -Wno-implicit-function-declaration|' src/Makefile
+
+cd src
+
+if [[ "$target_platform" == 'linux-aarch64' ]]; then
   sed -i '1i#include <math.h>' ic.c
   sed -i '1i#include <math.h>' ic_mes.c
   sed -i '1i#include <math.h>' ic_mep.c
   sed -i 's|http:\\/\\/|http://|g' ic_mep.c
-  make all CC=$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-cc CCMPI=mpicc LDFLAGS="-lm"
+  make all CC="${CC}" CCMPI="mpicc" LDFLAGS="${LDFLAGS} -lm" -j"${CPU_COUNT}"
 else
-  make all CC="$CC" CCMPI="mpicc"
+  make all CC="${CC}" CCMPI="mpicc" -j"${CPU_COUNT}"
 fi
-chmod +x "$SRC_DIR/bin/"*
+
+chmod +x "$SRC_DIR/bin/*"
 cp -pr "$SRC_DIR/bin" "$PREFIX"
