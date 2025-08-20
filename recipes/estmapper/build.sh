b525f5c925a492074d995b279d60bee0896aba1b
mencian <joshua.zhuang@yahoo.com>
2025-08-20 04:06:08 -0500

    Rebuild estmapper recipe

--- recipes/estmapper/build.sh
+++ recipes/estmapper/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-grep -l -r "/usr/bin/perl" . | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g'
-CXXFLAGS="${CXXFLAGS} -std=c++03" \
-    make install
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/lib
+mkdir -p ${PREFIX}/include
 
-if [ "$(uname)" = Darwin ]; then
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+grep -l -r "/usr/bin/perl" . | xargs sed -i.bak -e 's/usr\/bin\/perl/usr\/bin\/env perl/g'
+
+CXXFLAGS="${CXXFLAGS} -std=c++03" make install
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
     cp Darwin-amd64/bin/* ${PREFIX}/bin/
     cp Darwin-amd64/include/* ${PREFIX}/include/
     cp Darwin-amd64/lib/* ${PREFIX}/lib/
