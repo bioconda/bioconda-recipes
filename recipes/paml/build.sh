319c04844c3f04e78aee62a2714c2c0e7b5f9ba5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-19 23:43:46 -0500

    Update build.sh

--- recipes/paml/build.sh
+++ recipes/paml/build.sh
@@ -5,10 +5,10 @@ mkdir -p "${PREFIX}/bin"
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CFLAGS="${CFLAGS} -O3 -std=c11"
+export CFLAGS="${CFLAGS} -O3 std=gnu11"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-	sed -i.bak 's|-O3|-O3 -std=c11|' src/Makefile
+	sed -i.bak 's|-O3|-O3 std=gnu11|' src/Makefile
 	rm -rf src/*.bak
 fi
 
