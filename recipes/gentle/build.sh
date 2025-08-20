d0851b39f0c4f9a386c3190774a6a73096a54ae3
mencian <joshua.zhuang@yahoo.com>
2025-08-20 07:54:58 -0500

    Update gentle to v1.9.5.alpha2

--- recipes/gentle/build.sh
+++ recipes/gentle/build.sh
@@ -1,11 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
-aclocal
-libtoolize --automake --force --copy
-automake --add-missing
-autoconf
-mkdir -p ${PREFIX}
 
-./configure --prefix=$PREFIX CXXFLAGS="-I${PREFIX}/include"
-make
+mkdir -p "${PREFIX}"
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include"
+
+cp -rf ${BUILD_PREFIX}/share/gnuconfig/config.* .
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+	export EXTRA_ARGS="--host=arm64"
+elif [[ "${OS}" == "Linux" && "${ARCH}" == "aarch64" ]]; then
+	export EXTRA_ARGS="--host=aarch64"
+else
+	export EXTRA_ARGS="--host=x86_64"
+fi
+
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" \
+	CC="${CC}" CFLAGS="${CFLAGS}" \
+	--disable-option-checking --enable-silent-rules \
+	--disable-dependency-tracking --with-clustalw=yes \
+	"${EXTRA_ARGS}"
+
+make -j"${CPU_COUNT}"
 make install
