8853949b66826176cc162604c98cee843f45cd6c
root <root@localhost.localdomain>
2025-08-20 11:06:07 +0800

    update hi-corrector

--- recipes/hi-corrector/build.sh
+++ recipes/hi-corrector/build.sh
@@ -2,6 +2,10 @@
 set -euxo pipefail
 
 cd "$SRC_DIR/src"
-make all CC="$CC" CCMPI="mpicc"
+sed -i '1i#include <math.h>' ic.c
+sed -i '1i#include <math.h>' ic_mes.c
+sed -i '1i#include <math.h>' ic_mep.c
+sed -i 's|http:\\/\\/|http://|g' ic_mep.c
+make all CC=$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-cc CCMPI=mpicc LDFLAGS="-lm"
 chmod +x "$SRC_DIR/bin/"*
 cp -pr "$SRC_DIR/bin" "$PREFIX"
