175a345994d57bde1032565ef615f122e0cbb90d
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-08-20 11:19:43 +0800

    Update build.sh

--- recipes/hi-corrector/build.sh
+++ recipes/hi-corrector/build.sh
@@ -2,10 +2,14 @@
 set -euxo pipefail
 
 cd "$SRC_DIR/src"
-sed -i '1i#include <math.h>' ic.c
-sed -i '1i#include <math.h>' ic_mes.c
-sed -i '1i#include <math.h>' ic_mep.c
-sed -i 's|http:\\/\\/|http://|g' ic_mep.c
-make all CC=$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-cc CCMPI=mpicc LDFLAGS="-lm"
+if [[ $target_platform == 'linux-aarch64' ]]; then
+  sed -i '1i#include <math.h>' ic.c
+  sed -i '1i#include <math.h>' ic_mes.c
+  sed -i '1i#include <math.h>' ic_mep.c
+  sed -i 's|http:\\/\\/|http://|g' ic_mep.c
+  make all CC=$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-cc CCMPI=mpicc LDFLAGS="-lm"
+else
+  make all CC="$CC" CCMPI="mpicc"
+fi
 chmod +x "$SRC_DIR/bin/"*
 cp -pr "$SRC_DIR/bin" "$PREFIX"
