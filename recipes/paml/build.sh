44eb3bab1b1cdc9ab3185d08c4bbc85dddbc400b
mencian <joshua.zhuang@yahoo.com>
2025-08-20 02:01:06 -0500

    Rebuild paml recipe

--- recipes/paml/build.sh
+++ recipes/paml/build.sh
@@ -4,17 +4,29 @@ set -eu -o pipefail
 mkdir -p "${PREFIX}/bin"
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 -std=c11 -I${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-deprecated-declarations -Wno-implicit-function-declaration -Wno-unused-result"
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-	sed -i.bak 's|-O3|-O3 -std=c11|' src/Makefile
-	rm -rf src/*.bak
-fi
+sed -i.bak 's|CC = gcc|CC ?= $(CC)|' src/Makefile
+rm -rf src/*.bak
 
 cd src
 
-make CC="${CC}" CFLAGS="${CFLAGS}" -j"${CPU_COUNT}"
-"${CC}" -o ds -O3 ds.c tools.c -lm
+if [[ "$(uname -s)" == "Darwin" ]]; then
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o baseml baseml.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o basemlg basemlg.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o codeml codeml.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o pamp pamp.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o mcmctree mcmctree.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o infinitesites -D INFINITESITES mcmctree.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o evolver evolver.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o yn00 yn00.c tools.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o chi2 chi2.c -lm
+	"${CC}" ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -o ds ds.c tools.c -lm
+else
+	make -j"${CPU_COUNT}"
+	"${CC}" -O3 -o ds ds.c tools.c -lm
+fi
 
 install -v -m 0755 baseml basemlg \
 	chi2 codeml ds evolver infinitesites \
@@ -22,4 +34,5 @@ install -v -m 0755 baseml basemlg \
 	"${PREFIX}/bin"
 
 cd ..
+
 cp -rf dat ${PREFIX}/
