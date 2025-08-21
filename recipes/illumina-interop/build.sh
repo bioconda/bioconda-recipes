e17036132d454ae4c423cef8a4a4cce8aabfa936
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-21 00:38:25 -0500

    Update build.sh

--- recipes/illumina-interop/build.sh
+++ recipes/illumina-interop/build.sh
@@ -10,14 +10,12 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 
 case $(uname -m) in
     aarch64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
-	;;
+		export CXXFLAGS="${CXXFLAGS} -march=armv8-a";;
     arm64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
-	;;
+		export CXXFLAGS="${CXXFLAGS} -march=armv8-a";;
+		#export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a";;
     x86_64)
-	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
-	;;
+		export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3";;
 esac
 
 if [[ `uname -s` == "Darwin" ]]; then
