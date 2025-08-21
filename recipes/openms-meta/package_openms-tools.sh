646f6daefb746c7e7a3f2669369e95b582bc3cba
Julianus Pfeuffer <8102638+jpfeuffer@users.noreply.github.com>
2025-08-22 00:27:10 +0200

    remove debug

--- recipes/openms-meta/package_openms-tools.sh
+++ recipes/openms-meta/package_openms-tools.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-command -v readelf >/dev/null 2>&1 && readelf -d build/bin/OpenMSInfo | grep 'R.*PATH' || echo "Readelf.. skip rpath check"
 cmake -DCOMPONENT="Applications" -P build/cmake_install.cmake
-command -v readelf >/dev/null 2>&1 && readelf -d ${PREFIX}/bin/OpenMSInfo | grep 'R.*PATH' || echo "Readelf.. skip rpath check"
