0092a9d7da2792674e62ee5cef26a259b4a2d095
Julianus Pfeuffer <8102638+jpfeuffer@users.noreply.github.com>
2025-08-21 22:07:24 +0200

    Update build.sh

--- recipes/openms-meta/build.sh
+++ recipes/openms-meta/build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Everything from the usual conda compiler defaults except for the rpath settings
-#LDFLAGS="-Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,--allow-shlib-undefined"
-echo "LD_RUN_PATH: $LD_RUN_PATH"
-
 export PLATFORM_CMAKE_EXTRAS=""
 if [[ "$CXX" == *gnu-c++* ]]; then
-  # IS THIS A WRAPPER SCRIPT OR WHAT IS HAPPENING?
-  cat $(which $CXX)
   # For stuff like this GCC bug (especially on ARM) https://gcc.gnu.org/bugzilla/show_bug.cgi?id=111516
   echo "Detected gcc: ignoring some compile warnings."
   export CXXFLAGS="${CXXFLAGS} -Wno-psabi"
-  $CXX -dumpspecs
-  export PLATFORM_CMAKE_EXTRAS="-DCMAKE_LINKER_TYPE=GOLD"
+
+  # If you want GOLD linker (which is faster), try the following next two exports. But you will get warnings because GOLD
+  #  uses RUNPATH by default (even with -disable-new-tags, which conda adds in the default LDFLAGS).
+  #  Usually this should lead to RPATH being used.
+  #export PLATFORM_CMAKE_EXTRAS="-DCMAKE_LINKER_TYPE=GOLD"
   # the gcc spec file uses push-state in the hybrid libgcc linking case, which is not supported by GOLD linker
-  export LDFLAGS="-v -shared-libgcc ${LD_FLAGS}" 
+  #export LDFLAGS="-shared-libgcc ${LD_FLAGS}"
+  # Debug to see what kinf of nonsense gcc does under the hood with rpaths
+  #export LDFLAGS="-v ${LDFLAGS}"
 fi
 
 mkdir build
@@ -25,11 +24,14 @@ cd build
 #  to "build" in your dependencies of the meta.yml recipe.
 #  See also: https://stackoverflow.com/questions/39075040/cmake-cmake-automoc-in-cross-compilation
 # Set INSTALL_RPATH to PREFIX such that there are no warnings during linkage fixing of conda-build
-#  and make sure nothing is added by the compiler with CMAKE_INSTALL_REMOVE_ENVIRONMENT_RPATH
+#  and make sure nothing is added by the compiler with CMAKE_INSTALL_REMOVE_ENVIRONMENT_RPATH.
+# We set the BUILD_RPATH to the BUILD_PREFIX just to make CMake aware that the stupid compiler will add
+#  this RPATH to the end of the link line (visible with -v linker flag). With this CMake can remove it at install time.
 cmake -S .. -B . -G Ninja -DCMAKE_BUILD_TYPE="Release" \
 	-DOPENMS_GIT_SHORT_REFSPEC="release/${PKG_VERSION}" -DOPENMS_GIT_SHORT_SHA1="e88b120" \
  	-DOPENMS_CONTRIB_LIBS="SILENCE_WARNING_SINCE_NOT_NEEDED" \
-	-DCMAKE_PREFIX_PATH="${PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" -DCMAKE_INSTALL_REMOVE_ENVIRONMENT_RPATH=ON \
+	-DCMAKE_PREFIX_PATH="${PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_BUILD_RPATH="$BUILD_PREFIX/lib" -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" -DCMAKE_INSTALL_REMOVE_ENVIRONMENT_RPATH=ON \
 	-DHAS_XSERVER=OFF -DWITH_GUI=OFF -DENABLE_CLASS_TESTING=OFF -DENABLE_TOPP_TESTING=OFF -DBOOST_USE_STATIC=OFF -DBUILD_EXAMPLES=OFF -DENABLE_CWL=OFF -DWITH_HDF5=OFF \
 	-DBoost_NO_BOOST_CMAKE=ON -DBoost_ARCHITECTURE="-x64" \
  	-DQT_HOST_PATH="${BUILD_PREFIX}" -DQT_HOST_PATH_CMAKE_DIR="${PREFIX}" \
