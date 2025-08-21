b8551a3614828ceb73f20dcf515e3e12f22ca8fd
mencian <joshua.zhuang@yahoo.com>
2025-08-20 20:13:00 -0500

    Rebuild bgreat recipe

--- recipes/bgreat/build.sh
+++ recipes/bgreat/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPATH=${PREFIX}/include
+export CPATH="${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
+mkdir -p "$PREFIX/bin"
 
-make zobu="$LDFLAGS" CC=$CXX CFLAGS="$CXXFLAGS -flto -funit-at-a-time -fopenmp -lz"
+make zobu="${LDFLAGS}" CC="${CXX}" CFLAGS="${CXXFLAGS} -flto -funit-at-a-time -fopenmp -lz" -j"${CPU_COUNT}"
 
-mkdir -p $PREFIX/bin
-cp bgreat $PREFIX/bin
+install -v -m 0755 bgreat "$PREFIX/bin"
