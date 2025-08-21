059e021ead9dcd02e572d3c79d19a2f2bd6cee74
mencian <joshua.zhuang@yahoo.com>
2025-08-21 15:02:24 -0500

    Update bioconductor-chromvar to v1.30.1

--- recipes/bioconductor-chromvar/build.sh
+++ recipes/bioconductor-chromvar/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+export DISABLE_AUTOBREW=1
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
@@ -8,4 +14,4 @@ CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+${R} CMD INSTALL --build . "${R_ARGS}"
