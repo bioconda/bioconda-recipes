e80e346926d63eb266017616a326942ec9a96c33
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-26 14:16:02 -0400

    fix symlink

--- recipes/allelecodes/build.sh
+++ recipes/allelecodes/build.sh
@@ -2,8 +2,11 @@
 
 export CONDA_BLD_PATH=/tmp/conda-bld
 
+mkdir -pv $CONDA_BLD_PATH
+chmod 755 $CONDA_BLD_PATH
+
 mkdir -p $PREFIX/bin
 cp assignAlleleCodes_py3.6.py $PREFIX/bin/assignAlleleCodes
 chmod +x $PREFIX/bin/assignAlleleCodes
 # ensure backward compatibility
-ln -sv assignAllelecodes $PREFIX/bin/assignAlleleCodes_py3.6
+ln -sv assignAllelecodes $PREFIX/bin/assignAlleleCodes_py3.6.py
