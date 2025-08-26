59ac17620e82168481afb64810074a2c0c3d23bc
Evelin <evelin.aasna@fu-berlin.de>
2025-08-26 18:08:16 +0200

    Fix path

--- recipes/dream-stellar/build.sh
+++ recipes/dream-stellar/build.sh
@@ -2,14 +2,13 @@
 
 set -eux
 
-
 mkdir -p build
 cd build
-cmake ../.. -DCMAKE_BUILD_TYPE=Release -Wno-interference-size -D__STDC_FORMAT_MACROS" \
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-interference-size -D__STDC_FORMAT_MACROS" \
                 -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 make -j"${CPU_COUNT}"
 make install
-cd ../..
+cd ..
 
 cp "${RECIPE_DIR}/dream-stellar" "${PREFIX}/bin/dream-stellar"
 chmod +x "${PREFIX}/bin/dream-stellar"
