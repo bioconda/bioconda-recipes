88eecaac9bec1116601b11ae62af35c37addb6e9
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-26 15:47:55 -0400

    fix shebang

--- recipes/allelecodes/build.sh
+++ recipes/allelecodes/build.sh
@@ -8,9 +8,13 @@ set -ex
 
 mkdir -p $PREFIX/bin
 
+# Fix python3.6 shebang
+sed -i '1s|.*|#!/usr/bin/env python|' assignAlleleCodes_py3.6.py
+
+# Copy the script to the conda bin directory
+chmod +x assignAlleleCodes_py3.6.py
 cp assignAlleleCodes_py3.6.py $PREFIX/bin/assignAlleleCodes
 cp assignAlleleCodes_py3.6.py $PREFIX/bin/assignAlleleCodes_py3.6.py
-chmod +x $PREFIX/bin/assignAlleleCodes
 
 # ensure backward compatibility with a symlink
 #pushd $PREFIX/bin
