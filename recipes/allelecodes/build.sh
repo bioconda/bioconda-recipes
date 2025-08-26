69281da9ab6fafeb1736b39b69db79fd1337eb49
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-26 15:04:11 -0400

    cp instead of symlink

--- recipes/allelecodes/build.sh
+++ recipes/allelecodes/build.sh
@@ -9,11 +9,12 @@ chmod 755 $CONDA_BLD_PATH
 mkdir -p $PREFIX/bin
 
 cp assignAlleleCodes_py3.6.py $PREFIX/bin/assignAlleleCodes
+cp assignAlleleCodes_py3.6.py $PREFIX/bin/assignAlleleCodes_py3.6.py
 chmod +x $PREFIX/bin/assignAlleleCodes
 
 # ensure backward compatibility with a symlink
-pushd $PREFIX/bin
-ln -sv assignAllelecodes assignAlleleCodes_py3.6.py
-popd
+#pushd $PREFIX/bin
+#ln -sv assignAllelecodes assignAlleleCodes_py3.6.py
+#popd
 
 echo "=== DONE WITH $0 ==="
