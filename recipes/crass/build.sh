2cc2af9b5638d3aad3d1bcfb8af7c2ab8f6910d0
root <root@localhost.localdomain>
2025-08-26 16:28:16 +0800

    update crass and enabled linux-aarch64

--- recipes/crass/build.sh
+++ recipes/crass/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ $target_platform == 'linux-aarch64' ]];then
+	git clone https://github.com/DLTcollab/sse2neon.git
+	cp sse2neon/sse2neon.h .
+	sed -i 's/emmintrin.h/sse2neon.h/' src/crass/ksw.c
+
+fi
+
 ./autogen.sh
 ./configure --prefix=${PREFIX} --with-xerces=${PREFIX} CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS"
 make
