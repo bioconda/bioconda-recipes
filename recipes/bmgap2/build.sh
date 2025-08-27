6af4b58600cf0f1e2ebfc8e9aa1c46c9c8b7cd65
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-27 16:19:51 -0400

    smarter copying of executables; copying of small resources

--- recipes/bmgap2/build.sh
+++ recipes/bmgap2/build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-set -ex
-
 mkdir -pv $PREFIX/bin
 mkdir -pv $PREFIX/src
 
-# Will need to install databases, etc. later
-mkdir -pv $PREFIX/share/${PKG_NAME}-${PKG_VERSION}
-
 cp -rvf analysis_scripts/* $PREFIX/bin/
+find analysis_scripts/ -type f -name '*.py' -exec cp -vf {} $PREFIX/bin/ \;
+
 # scripts in bmgap2 look for scripts in analysis_scripts and so make a simple symlink
 ln -sv $PREFIX/bin $PREFIX/analysis_scripts
 cp -vf BMGAP-RUNNER.sh $PREFIX/bin/
+
+# Will need to install databases, etc. later
+sharedb=$PREFIX/share/${PKG_NAME}-${PKG_VERSION}
+mkdir -pv $sharedb
+
+# I know this could have been done in one command,
+# but I wanted to make each item clear on what is being copied.
+(
+  set -e
+  cd analysis_scripts
+  cp -rvf amr_variants_component $sharedb/
+  cp -rvf locusextractor $sharedb/
+  cp -rvf PMGA $sharedb/
+  cp -rvf SpeciesDB $sharedb/
+)
