4adbb5db0fcae18f47f85f0cc841778a50fd64d3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-28 02:06:00 -0500

    Update build.sh

--- recipes/transrate-tools/build.sh
+++ recipes/transrate-tools/build.sh
@@ -3,8 +3,13 @@ set -ef -o pipefail
 
 mkdir -p "$PREFIX/bin"
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include -L${PREFIX}/lib"
+
 rm -rf bamtools
 mkdir -p bamtools/include
+
 ln -sf $PREFIX/lib bamtools/
 ln -sf $PREFIX/include/bamtools/api bamtools/include/
 ln -sf $PREFIX/include/bamtools/shared bamtools/include/
@@ -12,12 +17,18 @@ ln -sf $PREFIX/include/bamtools/shared bamtools/include/
 if [[ "$(uname -s)" == "Linux" ]]; then
 	sed -i.bak 's|ZLIB REQUIRED STATIC|ZLIB REQUIRED|' CMakeLists.txt
 	rm -rf *.bak
+	export CONFIG_ARGS=""
+else
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 fi
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_STATIC_LIBRARY_PREFIX="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}"
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
 
-cmake --build build -j "${CPU_COUNT}"
+cmake --build build --clean-first -j "${CPU_COUNT}"
 
 install -v -m 0755 build/src/bam-read "$PREFIX/bin"
