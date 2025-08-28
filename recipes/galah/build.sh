5add1f00a1a13ef3249c67deb210d81f5c5cf423
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-28 06:35:28 -0500

    Update build.sh

--- recipes/galah/build.sh
+++ recipes/galah/build.sh
@@ -2,13 +2,14 @@
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
 
-mkdir -p ${PREFIX}/bin
+mkdir -p "${PREFIX}/bin"
 
 # Build statically linked binary with Rust
 C_INCLUDE_PATH="$PREFIX/include" \
-LIBRARY_PATH="$PREFIX/lib" \
-cargo build --release
+  LIBRARY_PATH="$PREFIX/lib" \
+  cargo build --release
 
 # Install the binary
-install -v -m 0755 target/release/galah $PREFIX/bin
+install -v -m 0755 target/release/galah "$PREFIX/bin"
