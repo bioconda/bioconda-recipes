1060ac88eb98513cafe1a6698ac7af558b892ca5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-27 21:23:13 -0500

    Update build.sh

--- recipes/crass/build.sh
+++ recipes/crass/build.sh
@@ -6,7 +6,7 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
 cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* m4/
 
-if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "osx-arm64" ]];then
+if [[ "$target_platform" == "linux-aarch64" || "$target_platform" == "osx-arm64" ]]; then
 	git clone https://github.com/DLTcollab/sse2neon.git
 	cp -f sse2neon/sse2neon.h .
 	sed -i 's/emmintrin.h/sse2neon.h/' src/crass/ksw.c
