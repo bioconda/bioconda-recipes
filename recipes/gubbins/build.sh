b0dc5f38dab1389ba71a37243ce750a475c761c3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-27 19:14:54 -0500

    Update build.sh

--- recipes/gubbins/build.sh
+++ recipes/gubbins/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export CPATH=${PREFIX}/include
 
-if [[ $target_platform == osx-* ]]; then
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
+export CPATH="${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+
+if [[ "$target_platform" == osx-* ]]; then
     for toolname in "otool" "install_name_tool"; do
         tool=$(find "${BUILD_PREFIX}/bin/" -name "*apple*-$toolname")
         mv "${tool}" "${tool}.bak"
@@ -11,9 +13,11 @@ if [[ $target_platform == osx-* ]]; then
     done
 fi
 
-autoreconf -i
+autoreconf -if
 ./configure --prefix=$PREFIX
+
 make -j"${CPU_COUNT}"
 make install
+
 cd python
-$PYTHON -m pip install . --report record.txt
+${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir --use-pep517 --report record.txt
