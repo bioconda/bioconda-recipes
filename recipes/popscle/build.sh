eb02e49259f10802aed2d7ef13605672ca7ecadb
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-27 21:25:52 -0500

    Update build.sh

--- recipes/popscle/build.sh
+++ recipes/popscle/build.sh
@@ -2,12 +2,12 @@
 
 mkdir -p "${PREFIX}/bin"
 
-sed -i 's/cmake_minimum_required(VERSION 2\.8\.4)/cmake_minimum_required(VERSION 3.5)/g' CMakeLists.txt
-sed -i '1i #include <random>' cmd_cram_freemuxlet.cpp
-sed -i '304i     std::random_device rd;\n    std::mt19937 g(rd());' cmd_cram_freemuxlet.cpp
-sed -i 's/std::random_shuffle(orand.begin(), orand.end())/std::shuffle(orand.begin(), orand.end(), g)/g' cmd_cram_freemuxlet.cpp
-sed -i '28i #include <limits>' gtf_interval_tree.h
-sed -i '1i #include <cstdint>' gtf.h
+sed -i.bak 's/cmake_minimum_required(VERSION 2\.8\.4)/cmake_minimum_required(VERSION 3.5)/g' CMakeLists.txt
+sed -i.bak '1i #include <random>' cmd_cram_freemuxlet.cpp
+sed -i.bak '304i     std::random_device rd;\n    std::mt19937 g(rd());' cmd_cram_freemuxlet.cpp
+sed -i.bak 's/std::random_shuffle(orand.begin(), orand.end())/std::shuffle(orand.begin(), orand.end(), g)/g' cmd_cram_freemuxlet.cpp
+sed -i.bak '28i #include <limits>' gtf_interval_tree.h
+sed -i.bak '1i #include <cstdint>' gtf.h
 
 #For libhts:
 #  - $ cmake -DHTS_INCLUDE_DIRS=/hts_absolute_path/include/  -DHTS_LIBRARIES=/hts_absolute_path/lib/libhts.a ..
@@ -26,4 +26,4 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 
 cmake --build build -j "${CPU_COUNT}"
 
-install ../bin/popscle "${PREFIX}/bin"
+install bin/popscle "${PREFIX}/bin"
