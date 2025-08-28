f028de8596e9fac278372112e7d37ee568c358d0
mencian <joshua.zhuang@yahoo.com>
2025-08-28 07:30:16 -0500

    Rebuild bioconductor-diffbind

--- recipes/bioconductor-diffbind/build.sh
+++ recipes/bioconductor-diffbind/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
+
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+
+$R CMD INSTALL --build . "$R_ARGS"
