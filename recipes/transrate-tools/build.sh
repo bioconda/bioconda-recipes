80c37749edb7ae7463e344e23bc7726a874b53e4
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-28 01:32:44 -0500

    Update build.sh

--- recipes/transrate-tools/build.sh
+++ recipes/transrate-tools/build.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -ef -o pipefail
 
+mkdir -p "$PREFIX/bin"
+
 rm -rf bamtools
 mkdir -p bamtools/include
-ln -s $BUILD_PREFIX/lib bamtools/
-ln -s $BUILD_PREFIX/include/bamtools/api bamtools/include/
-ln -s $BUILD_PREFIX/include/bamtools/shared bamtools/include/
+ln -sf $BUILD_PREFIX/lib bamtools/
+ln -sf $BUILD_PREFIX/include/bamtools/api bamtools/include/
+ln -sf $BUILD_PREFIX/include/bamtools/shared bamtools/include/
 
-if [[ ${target_platform}  == "linux-aarch64" ]]; then
+if [[ "${target_platform}"  == "linux-aarch64" ]]; then
 	sed -i "43c \\\  find_package (ZLIB REQUIRED)" CMakeLists.txt
 fi
-mkdir -p build
-pushd build
 
-mkdir -p $PREFIX/bin
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_STATIC_LIBRARY_PREFIX=$PREFIX -DCMAKE_CXX_COMPILER=${CXX} ${SRC_DIR} -DCMAKE_POLICY_VERSION_MINIMUM=3.5 
-make
-cp src/bam-read $PREFIX/bin/
-popd
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_STATIC_LIBRARY_PREFIX="${PREFIX}" \
+	-DCMAKE_CXX_COMPILER="${CXX}"
+
+cmake --build build -j "${CPU_COUNT}"
+
+install -v -m 0755 build/src/bam-read "$PREFIX/bin"
