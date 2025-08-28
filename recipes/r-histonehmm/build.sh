1388f0f856b62f0ba2ef977ce3dc4b3e49b38f91
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-08-28 18:27:50 +0800

    update r-histonehmm and enabled linux-aarch64 (#58696)
    * linux-aarch64 build for nrpys python <312
    
    * Revert "linux-aarch64 build for nrpys python <312"
    
    This reverts commit f8740fe8927ed0247bbce0cddb609f64c56aa7b4.
    
    * linux-aarch64 build for humann2
    
    * update humman2
    
    * update origin
    
    * Update rascaf and enabled aarch64
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * samtools 0.1.19 does not have osx-arm64; disable osx-arm64
    
    * Update meta.yaml
    
    * update r-histonehmm
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update fix-dgemm-call.patch
    
    * Update patch
    
    * Update build_failure.linux-64.yaml
    
    * Delete recipes/r-histonehmm/build_failure.linux-64.yaml
    
    * edit patch
    
    * Update patch
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    ---------
    
    Co-authored-by: root <root@localhost.localdomain>
    Co-authored-by: ZerryNi <996002763.com>
    Co-authored-by: cfmin09 <147251502+cfmin09@users.noreply.github.com>
    Co-authored-by: cfmin09 <1163105507@qq.com>
    Co-authored-by: Joshua Zhuang <71105179+mencian@users.noreply.github.com>
    Co-authored-by: mencian <joshua.zhuang@yahoo.com>

--- recipes/r-histonehmm/build.sh
+++ recipes/r-histonehmm/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export LC_ALL="en_US.UTF-8"
+
 Rscript -e "Rcpp:::CxxFlags()"
-$R CMD INSTALL --build .
+
+$R CMD INSTALL --build . "${R_ARGS}"
