d98a11987857b123d840b730b334f2900c1257ad
mencian <joshua.zhuang@yahoo.com>
2025-08-28 02:53:29 -0500

    add patch

--- recipes/transrate-tools/build.sh
+++ recipes/transrate-tools/build.sh
@@ -14,6 +14,18 @@ ln -sf $PREFIX/lib bamtools/
 ln -sf $PREFIX/include/bamtools/api bamtools/include/
 ln -sf $PREFIX/include/bamtools/shared bamtools/include/
 
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 if [[ "$(uname -s)" == "Linux" ]]; then
 	sed -i.bak 's|ZLIB REQUIRED STATIC|ZLIB REQUIRED|' CMakeLists.txt
 	rm -rf *.bak
