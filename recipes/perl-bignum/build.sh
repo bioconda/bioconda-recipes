1e5affc545ee92824201a955e7b2fbbed01f1abb
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-08-28 14:18:59 -0500

    Update build.sh

--- recipes/perl-bignum/build.sh
+++ recipes/perl-bignum/build.sh
@@ -9,8 +9,8 @@ if [[ -f Build.PL ]]; then
     perl ./Build install --installdirs site
 elif [[ -f Makefile.PL ]]; then
     perl Makefile.PL INSTALLDIRS=site NO_PACKLIST=1 NO_PERLLOCAL=1
-    make
-    make test -j"${CPU_COUNT}"
+    make -j"${CPU_COUNT}"
+    #make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
