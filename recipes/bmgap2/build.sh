0e232bb125964cdbfa48b6cf605fac243ff27b5b
Lee Katz - Aspen <gzu2@cdc.gov>
2025-08-29 10:33:12 -0400

    smarter copy

--- recipes/bmgap2/build.sh
+++ recipes/bmgap2/build.sh
@@ -29,9 +29,35 @@ mkdir -pv $sharedb
 )
 
 # some scripts require local database files and so I unfortunately have to put that into bin
-find $sharedb -type f -name 'species_db_*' -exec cp -vf {} $PREFIX/bin/ \;
-find $sharedb -type f -name 'RefSeqSketchesDefaults.msh*' -exec cp -vf {} $PREFIX/bin/ \;
-find $sharedb -type f -name 'blast_db*' -exec cp -vf {} $PREFIX/bin/ \;
-find $sharedb -type d -name 'sqlite3_db*' -exec cp -rvf {} $PREFIX/bin/ \;
+#find $sharedb -type f -name 'species_db_*' -exec cp -vf {} $PREFIX/bin/ \;
+#find $sharedb -type f -name 'RefSeqSketchesDefaults.msh*' -exec cp -vf {} $PREFIX/bin/ \;
+#find $sharedb -type f -name 'blast_db*' -exec cp -vf {} $PREFIX/bin/ \;
+#find $sharedb -type d -name 'sqlite3_db*' -exec cp -rvf {} $PREFIX/bin/ \;
+
+# There are library files that are expected to be in the binary paths,
+# in a subfolder lib
+set -ex
+mkdir -pv $PREFIX/bin/lib
+cp -vf $sharedb/*/lib/* $PREFIX/bin/lib/
+# Sqlite3 DBS are expected to be in a subfolder sqlite3_db
+mkdir -pv $PREFIX/bin/sqlite3_db
+cp -vf $sharedb/*/sqlite3_db/* $PREFIX/bin/sqlite3_db/
+# some scripts are expected to be in a subfolder bin
+mkdir -pv $PREFIX/bin/bin
+cp -vf $sharedb/*/bin/* $PREFIX/bin/bin/
+# Some scripts at least in PMGA are in a subfolder custom_allele_sets
+# and custom_allele_sets itself has subdirectories
+mkdir -pv $PREFIX/bin/custom_allele_sets/{hinfluenzae,neisseria}
+cp -rvf $sharedb/*/custom_allele_sets/hinfluenzae/* $PREFIX/bin/custom_allele_sets/hinfluenzae/
+cp -rvf $sharedb/*/custom_allele_sets/neisseria/* $PREFIX/bin/custom_allele_sets/neisseria/
+# locusextractor has a lot of subfolders too!
+for dir in $sharedb/locusextractor/*; do
+  if [ -d "$dir" ]; then
+    subdir=$(basename $dir)
+    mkdir -pv $PREFIX/bin/locusextractor/$subdir
+    cp -rvf $sharedb/locusextractor/$subdir/* $PREFIX/bin/locusextractor/$subdir/
+  fi
+done
+
 echo "===== Contents of $PREFIX/bin/:"
 ls -CF $PREFIX/bin/
