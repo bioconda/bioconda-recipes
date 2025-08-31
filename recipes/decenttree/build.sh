6c7cf7600392c1a79c43e0ad3b7dba8a9f1192b1
pentamorfico <amarrod96@gmail.com>
2025-08-31 04:39:23 +0200

    Add decenttree 1.0.0

--- recipes/decenttree/build.sh
+++ recipes/decenttree/build.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-if [[ "$target_platform" == linux-* ]]; then
-  mkdir -p build
-  cd build
-  cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
-  make -j${CPU_COUNT}
-  install -Dm755 decenttree $PREFIX/bin/decenttree
-
-elif [[ "$target_platform" == osx-* ]]; then
-  # prebuilt binary lives under decenttree-<ver>-MacOSX/bin/
-  cd decenttree-*-MacOSX
-  install -Dm755 bin/decenttree $PREFIX/bin/decenttree
-fi
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+make -j${CPU_COUNT}
+install -Dm755 decenttree $PREFIX/bin/decenttree
