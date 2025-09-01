6a1c23f6d44954fb6e442d9546933c3a1c9b1d1a
root <root@localhost.localdomain>
2025-09-01 15:09:45 +0800

    update dms

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/bin
+if [[ $target_platform == 'linux-aarch64' ]]; then
+	sed -i.bak '3s/-msse//g' Makefile	
+fi
 
 make CXX="${CXX} ${CPPFLAGS} ${CXXFLAGS} -fopenmp -DOMP ${LDFLAGS}"
-
+cp bin/* ${PREFIX}/bin/
+chmod +x ${PREFIX}/bin/*
 cp -r databases ${PREFIX}
 cp -r example ${PREFIX}
