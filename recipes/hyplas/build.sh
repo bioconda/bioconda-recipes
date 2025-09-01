e52d1709f3b1ca29be69bd2ef9a750294002f715
f0t1h <fatihkaraoglanoglu@gmail.com>
2025-09-01 13:43:28 -0700

    Fixes

--- recipes/hyplas/build.sh
+++ recipes/hyplas/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-${PYTHON} installer.py ${PREFIX} --tool HyPlAs_bin --skip  pandas numpy packaging platon spades racon minigraph minimap2
+${PYTHON} installer.py ${PREFIX} --tool HyPlAs --skip  pandas numpy packaging platon spades racon minigraph minimap2
 ${PYTHON} -m pip install . --no-deps -vvv
