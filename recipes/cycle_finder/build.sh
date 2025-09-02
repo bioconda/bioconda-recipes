e3ba71d20f6cb954ef61eaa0285e31a0f51e64bd
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-02 17:58:25 -0500

    Update build.sh

--- recipes/cycle_finder/build.sh
+++ recipes/cycle_finder/build.sh
@@ -2,10 +2,10 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CFLAGS="${CXXFLAGS} -O3 -std=c++0x -I${PREFIX}/include -fopenmp -funroll-loops -g -Wall"
 
 mkdir -p "$PREFIX/bin"
 
-make CC="$CXX" -j"${CPU_COUNT}"
+make CC="${CXX}" CFLAGS="${CFLAGS}" -j"${CPU_COUNT}"
 
 install -v -m 0755 cycle_finder "$PREFIX/bin"
