f52ac43571f7875c940935699741dadb15d3efe1
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-02 18:26:59 -0500

    Update build.sh

--- recipes/cycle_finder/build.sh
+++ recipes/cycle_finder/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="-g -Wall -O3 -std=c++0x -I${PREFIX}/include -L${PREFIX}/lib -fopenmp -funroll-loops"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+export CFLAGS="-g -Wall -O3 -std=c++0x -funroll-loops -I${PREFIX}/include -L${PREFIX}/lib -fopenmp"
 
 mkdir -p "$PREFIX/bin"
 
