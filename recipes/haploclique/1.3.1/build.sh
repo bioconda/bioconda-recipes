207eb92dca09334b6fd2e82d1aa8278772da90f5
root <root@localhost.localdomain>
2025-09-02 15:30:54 +0800

    update haploclique

--- recipes/haploclique/1.3.1/build.sh
+++ recipes/haploclique/1.3.1/build.sh
@@ -1,11 +1,15 @@
 #!/use/bin/env sh
 
 # HACK GCC 4.8.5 ships with a broken std::regex (it's only fixed with GCC 4.9.0)
+export CXX=${BUILD_PREFIX}/bin/aarch64-conda-linux-gnu-c++
+export CC=${BUILD_PREFIX}/bin/aarch64-conda-linux-gnu-cc
 if [[ `c++ -dumpversion` == 4.8.* ]]; then
     echo 'GCC version 4.8.x detected, forcing switch of regex engine from C++11 to Boost'
     USE_BOOST_REGEX='-DUSE_BOOST_REGEX=1'
 fi
 
+sed -i '1i#include <stdexcept>' external/docopt.cpp/docopt_value.h
+sed -i '1i#include <stdexcept>' external/docopt.cpp/docopt.h
 mkdir -p build && cd build
 
 export C_INCLUDE_PATH=${PREFIX}/include
@@ -14,7 +18,8 @@ export CPP_INCLUDE_PATH=${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}"   \
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+      -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}"   \
       -DBOOST_ROOT="${PREFIX}" -DBoost_NO_SYSTEM_PATHS=ON "${USE_BOOST_REGEX}"    \
       -DCMAKE_SKIP_BUILD_RPATH=FALSE -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE -DCMAKE_INSTALL_RPATH="${PREFIX}/lib64:${PREFIX}/lib:${PREFIX}/lib/bamtools" -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE    \
       ..   \
