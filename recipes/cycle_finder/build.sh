e9b457b00b589b497ddf72ebba121dd7154bdfca
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-02 17:42:46 -0500

    Update build.sh

--- recipes/cycle_finder/build.sh
+++ recipes/cycle_finder/build.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p $PREFIX/bin
-make CC=$CXX
-cp cycle_finder $PREFIX/bin/
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+mkdir -p "$PREFIX/bin"
+
+make CC="$CXX" -j"${CPU_COUNT}"
+
+install -v -m 0755 cycle_finder "$PREFIX/bin"
