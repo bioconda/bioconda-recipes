8099c6dee25aeba88112f8cb8557d1ff45111338
高文尧 <12963281+gao-wenyao123@user.noreply.gitee.com>
2025-09-03 15:17:49 +0800

    plmc

--- recipes/plmc/build.sh
+++ recipes/plmc/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# 正确方式
+export CFLAGS="-O2 -march=armv8-a"
+export CXXFLAGS="-O2 -march=armv8-a"
+
+
 cat makefile | sed 's/clang/$(CC)/g' | sed 's/=/+=/g'> tmp_makefile
 rm makefile
 mv tmp_makefile Makefile
+sed -i 's/-msse4.2//g' Makefile
+sed -i 's/-march=[^ ]*sse4.2[^ ]*//g' Makefile
 
 mkdir -p "${PREFIX}/bin"
 
@@ -10,11 +17,11 @@ if [ "$(uname)" == "Darwin" ]; then
     # for parallelism build with multicore support
     # see https://github.com/debbiemarkslab/plmc#compilation
     # make all-mac-openmp
-    make all-mac CC="${CC}" CLANGFLAG="${CFLAGS} -lm -Wall -Ofast -msse4.2"
+    make all-mac CC="${CC}" CLANGFLAG="${CFLAGS} -lm -Wall -Ofast"
     cp ./bin/plmc "${PREFIX}/bin/plmc"
 else
     # see https://github.com/debbiemarkslab/plmc/issues/12
     sed  -i.bak '1 i\#include <sys/time.h>' ./src/include/plm.h
-    make all-openmp CC="${CC}" GCCFLAGS="${CFLAGS} -std=c99 -lm -O3 -msse4.2"
+    make all-openmp CC="${CC}" GCCFLAGS="${CFLAGS} -std=c99 -lm -O3"
     cp ./bin/plmc "${PREFIX}/bin/plmc"
 fi
