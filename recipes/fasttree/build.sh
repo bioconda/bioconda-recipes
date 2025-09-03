3f64160f9cdda4255688b786a1719a4be18f6fd8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-03 18:50:57 -0500

    Update build.sh

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 set -ex
 
-export LDFLAGS="-L${PREFIX}/lib"
-
 mkdir -p "${PREFIX}/bin"
 
 case $(uname -m) in
     aarch64)
 	export EXTRA_ARGS=""
-	;;
+		;;
     arm64)
 	export EXTRA_ARGS=""
-	;;
+		;;
     x86_64)
-	export EXTRA_ARGS="-fopen-simd -march=native"
-	;;
+	export EXTRA_ARGS="-fopenmp-simd -march=native"
+		;;
 esac
 
 # build FastTree
-${CC} ${EXTRA_ARGS} -O3 -funsafe-math-optimizations -o FastTree FastTree.c -lm
+${CC} -I${PREFIX}/include -L${PREFIX}/lib ${EXTRA_ARGS} -O3 -funsafe-math-optimizations -o FastTree FastTree.c -lm
 install -v -m 0755 FastTree "${PREFIX}/bin"
 
 # some OS are not case-sensitive (macOS, by default), ignore the copy error there
 cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || True
 
 # Build FastTreeMP
-${CC} ${EXTRA_ARGS} -DOPENMP -O3 -fopenmp -funsafe-math-optimizations -o FastTreeMP FastTree.c -lm
+${CC} -I${PREFIX}/include -L${PREFIX}/lib ${EXTRA_ARGS} -DOPENMP -O3 -fopenmp -funsafe-math-optimizations -o FastTreeMP FastTree.c -lm
 install -v -m 0755 FastTreeMP "${PREFIX}/bin"
