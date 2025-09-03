89a9e5410672115876250c062ef0787459af7f2e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-03 18:32:12 -0500

    Update build.sh

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
 
 mkdir -p "${PREFIX}/bin"
 
@@ -18,12 +18,12 @@ case $(uname -m) in
 esac
 
 # build FastTree
-${CC} ${EXTRA_ARGS} -O3 -funsafe-math-optimizations -o "$PREFIX/bin/FastTree" FastTree.c -lm
-chmod +x $PREFIX/bin/FastTree
+${CC} ${EXTRA_ARGS} -O3 -funsafe-math-optimizations -o FastTree FastTree.c -lm
+install -v -m 0755 FastTree "${PREFIX}/bin"
 
 # some OS are not case-sensitive (macOS, by default), ignore the copy error there
 cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || True
 
 # Build FastTreeMP
-${CC} ${EXTRA_ARGS} -DOPENMP -O3 -fopenmp -funsafe-math-optimizations -o "$PREFIX/bin/FastTreeMP" FastTree.c -lm
-chmod +x $PREFIX/bin/FastTreeMP
+${CC} ${EXTRA_ARGS} -DOPENMP -O3 -fopenmp -funsafe-math-optimizations -o FastTreeMP FastTree.c -lm
+install -v -m 0755 FastTreeMP "${PREFIX}/bin"
