f7b461dd9261ba170bbaae3783c0cc1256373a6b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-03 18:15:39 -0500

    Update build.sh

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -18,12 +18,12 @@ case $(uname -m) in
 esac
 
 # build FastTree
-${CC} "${LDFLAGS}" -O3 -funsafe-math-optimizations "${EXTRA_ARGS}" $SRC_DIR/FastTree.c -lm -o $PREFIX/bin/FastTree
+${CC} -O3 -funsafe-math-optimizations "${EXTRA_ARGS}" $SRC_DIR/FastTree.c -lm -o $PREFIX/bin/FastTree
 chmod +x $PREFIX/bin/FastTree
 
 # some OS are not case-sensitive (macOS, by default), ignore the copy error there
 cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || True
 
 # Build FastTreeMP
-${CC} "${LDFLAGS}" -DOPENMP -O3 -fopenmp -funsafe-math-optimizations "${EXTRA_ARGS}" $SRC_DIR/FastTree.c -lm -o $PREFIX/bin/FastTreeMP
+${CC} -DOPENMP -O3 -fopenmp -funsafe-math-optimizations "${EXTRA_ARGS}" $SRC_DIR/FastTree.c -lm -o $PREFIX/bin/FastTreeMP
 chmod +x $PREFIX/bin/FastTreeMP
