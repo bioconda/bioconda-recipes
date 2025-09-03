743b4472eb7da603b59a1abef8dbb22eda2f61cd
mencian <joshua.zhuang@yahoo.com>
2025-09-03 18:09:13 -0500

    Update fasttree to v2.2.0

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 set -ex
-mkdir -p $PREFIX/bin
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+mkdir -p "${PREFIX}/bin"
+
+case $(uname -m) in
+    aarch64)
+	export EXTRA_ARGS=""
+	;;
+    arm64)
+	export EXTRA_ARGS=""
+	;;
+    x86_64)
+	export EXTRA_ARGS="-fopen-simd -march=native"
+	;;
+esac
 
 # build FastTree
-$CC $CFLAGS $LDFLAGS -Wall -O3 -DUSE_DOUBLE -funroll-loops $SRC_DIR/FastTree-2.1.11.c -lm -o $PREFIX/bin/FastTree
+${CC} "${LDFLAGS}" -O3 -funsafe-math-optimizations "${EXTRA_ARGS}" $SRC_DIR/FastTree.c -lm -o $PREFIX/bin/FastTree
 chmod +x $PREFIX/bin/FastTree
 
 # some OS are not case-sensitive (macOS, by default), ignore the copy error there
-cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || true
+cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || True
 
 # Build FastTreeMP
-$CC $CFLAGS $LDFLAGS -Wall -DOPENMP -fopenmp -O3 -DUSE_DOUBLE -funroll-loops $SRC_DIR/FastTree-2.1.11.c -lm -o $PREFIX/bin/FastTreeMP
+${CC} "${LDFLAGS}" -DOPENMP -O3 -fopenmp -funsafe-math-optimizations "${EXTRA_ARGS}" $SRC_DIR/FastTree.c -lm -o $PREFIX/bin/FastTreeMP
 chmod +x $PREFIX/bin/FastTreeMP
