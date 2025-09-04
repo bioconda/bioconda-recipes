7fa3a41d03e42cefaf6212643eb69226efe90d6f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-03 21:14:22 -0500

    Update build.sh

--- recipes/plmc/build.sh
+++ recipes/plmc/build.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
 # 正确方式
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3"
-export CXXFLAGS="${CXXFLAGS} -O3"
 
 mkdir -p "${PREFIX}/bin"
 
 case $(uname -m) in
-    aarch64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
-	;;
-    arm64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
-	;;
-    x86_64)
-	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
-	;;
-esac
-
-cat makefile | sed 's/clang/$(CC)/g' | sed 's/=/+=/g'> tmp_makefile
-rm -f makefile
-mv tmp_makefile Makefile
-
-case $(uname -m) in
     aarch64|arm64)
-	sed -i 's/-msse4.2//g' Makefile
-    sed -i 's/-march=[^ ]*sse4.2[^ ]*//g' Makefile
+	sed -i.bak 's|-msse4.2||' makefile
 	;;
 esac
+
+sed -i.bak 's|clang|$(CC)|' makefile
+sed -i.bak 's|gcc|$(CC)|' makefile
+rm -rf *.bak
 
 if [ "$(uname -s)" == "Darwin" ]; then
     # for parallelism build with multicore support
     # see https://github.com/debbiemarkslab/plmc#compilation
     # make all-mac-openmp
-    make all-mac-openmp CC="${CC}" CLANGFLAG="${CFLAGS} -lm -Wall -Ofast" -j"${CPU_COUNT}"
+    make all-mac-openmp CC="${CC}" CLANGFLAG="${CFLAGS} -lm -Wall -Ofast" LIBOMP_PREFIX="${PREFIX}" -j"${CPU_COUNT}"
     install -v -m 0755 bin/plmc "${PREFIX}/bin"
 else
     # see https://github.com/debbiemarkslab/plmc/issues/12
