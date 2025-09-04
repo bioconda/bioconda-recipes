7de6a60e78f03fdeaca32b058c6c55264f693587
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-04 03:13:47 -0500

    Update build.sh

--- recipes/dms/build.sh
+++ recipes/dms/build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+
 mkdir -p ${PREFIX}/bin
-if [[ $target_platform == 'linux-aarch64' ]]; then
+
+if [[ "$target_platform" == "linux-aarch64" ]]; then
 	sed -i.bak '3s/-msse//g' Makefile	
 fi
 
-make CXX="${CXX} ${CPPFLAGS} ${CXXFLAGS} -fopenmp -DOMP ${LDFLAGS}"
+make CXX="${CXX} ${CPPFLAGS} ${CXXFLAGS} -fopenmp -DOMP ${LDFLAGS}" -j"${CPU_COUNT}"
 #export DynamicMetaStorms=${PREFIX}/bin
-cp bin/* ${PREFIX}/bin/
-chmod +x ${PREFIX}/bin/*
-cp -r databases ${PREFIX}
-cp -r example ${PREFIX}
+install -v -m 0755 bin/* ${PREFIX}/bin
+
+cp -rf databases ${PREFIX}
+cp -rf example ${PREFIX}
