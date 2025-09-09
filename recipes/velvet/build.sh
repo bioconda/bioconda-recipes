3353c172434c1831654c8272f8001f1b47686f79
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-09 14:00:32 -0500

    Update build.sh

--- recipes/velvet/build.sh
+++ recipes/velvet/build.sh
@@ -6,6 +6,6 @@ mkdir -p "${PREFIX}/bin"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -Wno-implicit-function-declaration -Wno-int-conversion"
 
-make CC="${CC}" CFLAGS="${CFLAGS} 'CATEGORIES=4' 'MAXKMERLENGTH=191' 'OPENMP=1' 'LONGSEQUENCES=1'" -j"${CPU_COUNT}"
+make CC="${CC}" CFLAGS="${CFLAGS}" 'CATEGORIES=4' 'MAXKMERLENGTH=191' 'OPENMP=1' 'LONGSEQUENCES=1' -j"${CPU_COUNT}"
 
 install -v -m 0755 velvetg velveth "${PREFIX}/bin"
