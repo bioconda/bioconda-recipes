64cc15ef6ba7f99e8642b5d3f0b015ced36e600a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-08 22:43:37 -0500

    Update build.sh

--- recipes/vt/build.sh
+++ recipes/vt/build.sh
@@ -12,12 +12,12 @@ cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* lib/htslib
 
 #Inject compilers
 sed -i.bak "s#gcc#${CC}#g" lib/pcre2/Makefile
-sed -i.bak "s|-lz|-L$(PREFIX)/lib -lz|" Makefile
-sed -i.bak "s|-D__STDC_LIMIT_MACROS|-I$(PREFIX)/include -D__STDC_LIMIT_MACROS|" Makefile
+sed -i.bak "s|-lz|-L${PREFIX}/lib -lz|" Makefile
+sed -i.bak "s|-D__STDC_LIMIT_MACROS|-I${PREFIX}/include -D__STDC_LIMIT_MACROS|" Makefile
 rm -f *.bak
 rm -f lib/pcre2/*.bak
 
 make CXX="${CXX}" CC="${CC}" OPTFLAG="-O3 -L${PREFIX}/lib" -j"${CPU_COUNT}"
 make test
 
-install -v -m 0755 vt "$PREFIX/bin"
+install -v -m 0755 vt "${PREFIX}/bin"
