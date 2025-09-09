5ccf252b880ccc028c9e84ea5e830324419e9a08
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-09 00:33:57 -0500

    Update build.sh

--- recipes/portcullis/build.sh
+++ recipes/portcullis/build.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 set -x -e
 
-export CFLAGS="-I$PREFIX/include $CFLAGS -O3"
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-export CXXFLAGS="-I$PREFIX/include $CXXFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export LIBS="-lm ${PREFIX}/lib/libz.a"
+export CFLAGS="$CFLAGS -O3"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export CXXFLAGS="$CXXFLAGS -O3"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export LIBS="${LIBS} -lm ${PREFIX}/lib/libz.a"
 
-./autogen.sh
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+autoreconf -if
 ./configure --enable-silent-rules \
 	--disable-dependency-tracking \
+	--disable-option-checking \
 	--prefix="${PREFIX}" \
 	--with-boost="${PREFIX}" \
-	--with-boost-libdir="${PREFIX}/lib"
+	--with-boost-libdir="${PREFIX}/lib" \
+	CC="${CC}" \
+	CFLAGS="${CFLAGS}" \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}"
 
 make -j"${CPU_COUNT}"
 make -j"${CPU_COUNT}" check
