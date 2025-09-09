917a8d6b4896fe41cd9477b67fd24ca2c23e2349
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-09 18:15:01 -0500

    Update build.sh

--- recipes/longphase/build.sh
+++ recipes/longphase/build.sh
@@ -16,6 +16,9 @@ autoreconf -if
   CPPFLAGS="${CPPFLAGS}" \
   LDFLAGS="${LDFLAGS}"
 
+sed -i.bak 's|$(HTSLIB_LIB)|-L$(PREFIX)/lib $(HTSLIB_LIB)|' Makefile
+rm -f *.bak
+
 make CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
 
 install -v -m 0755 longphase "$PREFIX/bin"
