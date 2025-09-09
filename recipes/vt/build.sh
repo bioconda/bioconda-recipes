d8a6b6901fb344cab3f7af1348931f1d51512cf5
mencian <joshua.zhuang@yahoo.com>
2025-09-08 21:25:14 -0500

    vt: add arm64 build

--- recipes/vt/build.sh
+++ recipes/vt/build.sh
@@ -2,17 +2,17 @@
 
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -I$PREFIX/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-export CFLAGS="-I$PREFIX/include"
-export CPATH=${PREFIX}/include
-export LDFLAGS="-L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
+mkdir -p "$PREFIX/bin"
 
 #Inject compilers
 sed -i.bak "s#gcc#${CC}#g" lib/pcre2/Makefile
+rm -f lib/pcre2/*.bak
 
-make CXX=$CXX CC=$CC OPTFLAG="-O3 -L${PREFIX}/lib" -j${CPU_COUNT}
+make CXX="${CXX}" CC="${CC}" OPTFLAG="-O3 -L${PREFIX}/lib" -j"${CPU_COUNT}"
 make test
-mkdir -p $PREFIX/bin
-cp vt $PREFIX/bin
+
+install -v -m 0755 vt "$PREFIX/bin"
