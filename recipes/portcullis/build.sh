fe8b13ef0a68849e1939c991136a5f672246930f
mencian <joshua.zhuang@yahoo.com>
2025-09-09 00:13:32 -0500

    portcullis: add arm64 build

--- recipes/portcullis/build.sh
+++ recipes/portcullis/build.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -x -e
 
-# echo "CFLAGS=$CFLAGS"
-# echo "CPPFLAGS=$CPPFLAGS"
-# echo "CXXFLAGS=$CXXFLAGS"
-# echo "LDFLAGS=$LDFLAGS"
-
-export CFLAGS="-I$PREFIX/include $CFLAGS"
+export CFLAGS="-I$PREFIX/include $CFLAGS -O3"
 export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
 export CXXFLAGS="-I$PREFIX/include $CXXFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export LIBS="-lm ${PREFIX}/lib/libz.a"
 
 ./autogen.sh
-#./configure --disable-silent-rules --disable-dependency-tracking --prefix=$PREFIX
-./configure --disable-silent-rules --disable-dependency-tracking --prefix=${PREFIX} --with-boost=${PREFIX} --with-boost-libdir=${PREFIX}/lib
-make -j ${CPU_COUNT} V=1
-make -j ${CPU_COUNT} V=1 check
+./configure --enable-silent-rules \
+	--disable-dependency-tracking \
+	--prefix="${PREFIX}" \
+	--with-boost="${PREFIX}" \
+	--with-boost-libdir="${PREFIX}/lib"
+
+make -j"${CPU_COUNT}"
+make -j"${CPU_COUNT}" check
+
 make install
