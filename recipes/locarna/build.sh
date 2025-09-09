f247205a80a510380c70a549ecedce13f2e062e1
mencian <joshua.zhuang@yahoo.com>
2025-09-08 21:53:03 -0500

    locarna: add aarch64/arm64 builds

--- recipes/locarna/build.sh
+++ recipes/locarna/build.sh
@@ -1,5 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-./configure --prefix=$PREFIX --with-vrna=$PREFIX && \
-make -j ${CPU_COUNT} && \
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+mkdir -p config
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* config/
+
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	--with-vrna="${PREFIX}" \
+	--disable-dependency-tracking \
+	--enable-silent-rules \
+	--disable-option-checking \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}" \
+
+make -j"${CPU_COUNT}"
 make install
