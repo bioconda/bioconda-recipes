b15917e5aa6742ab1c7a2667ee37185a1e810962
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-08 19:09:31 -0500

    Rebuild rastair recipe (#59162)

--- recipes/rastair/build.sh
+++ recipes/rastair/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -ex
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install -v --locked --no-track --root $PREFIX --path .
+
+RUST_BACKTRACE=1
+cargo install -v --no-track --root "${PREFIX}" --path .
 
 # Copy R scripts
 mkdir -p $PREFIX/share/rastair/scripts
