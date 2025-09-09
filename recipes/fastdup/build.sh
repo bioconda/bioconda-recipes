0ca688d23c59355b5f587c4730c310ad7c58810b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-08 23:59:22 -0500

    Update build.sh

--- recipes/fastdup/build.sh
+++ recipes/fastdup/build.sh
@@ -5,9 +5,9 @@ export LIBRARY_PATH="${PREFIX}/lib"
 export CPATH="${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O2"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX/bin"
 
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
 	-DEXTRA_FLAGS="${EXTRA_FLAGS}" -DCMAKE_CXX_COMPILER="${CXX}" \
@@ -16,13 +16,4 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Releas
 	"${CONFIG_ARGS}"
 cmake --build build --clean-first --target install -j "${CPU_COUNT}"
 
-# Libraries aren't getting installed
-#mkdir -p $PREFIX/lib
-
-#ls $SRC_DIR/build/lib/* -lh
-
-#cp -f $SRC_DIR/build/lib/libwfa2* $PREFIX/lib
-#cp -f $SRC_DIR/build/lib/libwflign* $PREFIX/lib
-
-install -v -m 0755 build/bin/* $PREFIX/bin
-#cp -f scripts/split_approx_mappings_in_chunks.py $PREFIX/bin
+install -v -m 0755 build/bin/* "$PREFIX/bin"
