91885946fcaae69665f390831c5be3ff0f07e7c9
mencian <joshua.zhuang@yahoo.com>
2025-09-15 18:28:57 -0500

    add patch

--- recipes/rust-mdbg/build.sh
+++ recipes/rust-mdbg/build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -euo
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export RUSTFLAGS="-C linker=${CC}"
+export CARGO_BUILD_TARGET="${CC}"
+
 if [[ "${target_platform}"  == "linux-aarch64" ]]; then
-        export CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu
-        export RUSTFLAGS="-C linker=aarch64-conda-linux-gnu-cc "
-        export CFLAGS="-fno-dse"
-        export CXXFLAGS="${CFLAGS}"
-        cargo fetch
-        git clone https://github.com/markschl/seq_io.git
-        cargo clean
-        sed -i '74,98s|^|//|' .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fasthash-sys-0.3.2/build.rs
-        sed -i.bak 's|-msse4.2||'  .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fasthash-sys-0.3.2/src/smhasher/doc/crcutil
-        sed -i "12c twox-hash = \"1.6.3\" " Cargo.toml
-		sed -i "35c seq_io = \"0.4.0-alpha.0\" " Cargo.toml
+        export CFLAGS="${CFLAGS} -fno-dse"
 fi
 
 RUST_BACKTRACE=1
-cargo install --verbose --no-track --path . --root "$PREFIX"
+cargo install -v --no-track --path . --root "${PREFIX}"
