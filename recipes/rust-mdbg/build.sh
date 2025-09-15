d0cf53de05f101dc7fdc15bfb6d8c621f2d5ed06
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-15 17:54:40 -0500

    Update build.sh

--- recipes/rust-mdbg/build.sh
+++ recipes/rust-mdbg/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -euo
 
-# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
-# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
-
-if [[ ${target_platform}  == "linux-aarch64" ]]; then
+if [[ "${target_platform}"  == "linux-aarch64" ]]; then
         export CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu
         export RUSTFLAGS="-C linker=aarch64-conda-linux-gnu-cc "
         export CFLAGS="-fno-dse"
@@ -15,6 +11,8 @@ if [[ ${target_platform}  == "linux-aarch64" ]]; then
         sed -i '74,98s|^|//|' .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fasthash-sys-0.3.2/build.rs
         sed -i.bak 's|-msse4.2||'  .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fasthash-sys-0.3.2/src/smhasher/doc/crcutil
         sed -i "12c twox-hash = \"1.6.3\" " Cargo.toml
-	sed -i "35c seq_io = \"0.4.0-alpha.0\" " Cargo.toml
+		sed -i "35c seq_io = \"0.4.0-alpha.0\" " Cargo.toml
 fi
-RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX
+
+RUST_BACKTRACE=1
+cargo install --verbose --no-track --path . --root "$PREFIX"
