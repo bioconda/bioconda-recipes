5537409c54665f2c18452fb2366fd6288fd136ab
mencian <joshua.zhuang@yahoo.com>
2025-09-15 19:14:56 -0500

    dream-stellar: add arm64 build

--- recipes/dream-stellar/build.sh
+++ recipes/dream-stellar/build.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
-
 set -eux
 
-mkdir -p build
-cd build
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-interference-size -D__STDC_FORMAT_MACROS" \
-                -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-make -j"${CPU_COUNT}"
-make install
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-interference-size -D__STDC_FORMAT_MACROS"
 
-chmod +x "${PREFIX}/bin/dream-stellar"
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+
+ninja -C build install -j "${CPU_COUNT}"
