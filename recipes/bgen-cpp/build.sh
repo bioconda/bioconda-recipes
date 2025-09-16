05305914463cf2f7c338438f3942f75be003f05d
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2025-09-16 16:40:53 +0100

    fix build script

--- recipes/bgen-cpp/build.sh
+++ recipes/bgen-cpp/build.sh
@@ -2,16 +2,10 @@
 
 set -xe
 
-export INCLUDES="-I${PREFIX}/include"
-export LIBPATH="-L${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -DUSE_BOOST -I${PREFIX}/include"
+mkdir -p $PREFIX/bin
 
-mkdir -p $PREFIX/bin bgen
-
-tar -xf bgen.tgz --strip-components 1 -C bgen
-cd bgen
 ./waf configure
+
 ./waf
 
 cp build/apps/bgenix ${PREFIX}/bin
