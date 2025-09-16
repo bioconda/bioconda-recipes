6b6371dfbed619e4400da2d491768e30b6331382
mencian <joshua.zhuang@yahoo.com>
2025-09-16 17:58:32 -0500

    genomedata: add arm64 build

--- recipes/genomedata/build.sh
+++ recipes/genomedata/build.sh
@@ -3,11 +3,13 @@
 export HDF5_DIR="$PREFIX"
 # Remove -DNDEBUG from CPPFLAGS to keep assertions
 # See https://bitbucket.org/hoffmanlab/genomedata/issues/47/assert-statements-from
-export CPPFLAGS="-I${PREFIX}/include -D_FORTIFY_SOURCE=2 -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -D_FORTIFY_SOURCE=2 -O3"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+mkdir -p "$PREFIX/bin"
 
 $PYTHON -m pip install --disable-pip-version-check --no-deps --no-cache-dir --no-build-isolation --use-pep517 . -vvv
 # Work around for no 'source_files' support in test section of meta.yaml
-cp test/run_tests.py $PREFIX/bin/run_genomedata_tests.py
-cp test/test_genomedata.py $PREFIX/bin
-cp -r test/data $PREFIX/bin
+cp -f test/run_tests.py $PREFIX/bin/run_genomedata_tests.py
+cp -f test/test_genomedata.py $PREFIX/bin
+cp -rf test/data $PREFIX/bin
