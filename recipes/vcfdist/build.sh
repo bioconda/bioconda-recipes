a51cb4035865357b67d18fd18ff7d2d89798ac32
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-16 20:50:07 -0500

    Update build.sh

--- recipes/vcfdist/build.sh
+++ recipes/vcfdist/build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CPATH="${PREFIX}/include"
+export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
 install -d "${PREFIX}/bin"
 
