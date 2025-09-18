711c8ee8a1f774c8020e0540ddb76bc80d60fa10
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-18 18:56:50 -0500

    Update build.sh

--- recipes/srprism/build.sh
+++ recipes/srprism/build.sh
@@ -2,9 +2,10 @@
 set -xe
 
 # https://bioconda.github.io/troubleshooting.html#zlib-errors
-export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export CFLAGS="${CFLAGS} -O3"
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 export CPATH="${PREFIX}/include"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -std=c++14"
 
 # cd to location of Makefile and source
 cd gnuac
@@ -13,7 +14,11 @@ cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
 
 # depends on automake, autoconf
 autoreconf -if
-./configure --prefix="$PREFIX"
+./configure --prefix="$PREFIX" \
+  CFLAGS="${CFLAGS}" \
+  CC="${CC}" \
+  CPPFLAGS="${CPPFLAGS}" \
+  LDFLAGS="${LDFLAGS}"
 
 make -j"${CPU_COUNT}"
 make install
