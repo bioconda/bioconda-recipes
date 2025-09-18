1f124ed930d99b15b399c10069b42408397d634c
mencian <joshua.zhuang@yahoo.com>
2025-09-18 18:35:23 -0500

    desalt: add aarch64/arm64 builds

--- recipes/desalt/build.sh
+++ recipes/desalt/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
 set -xe
 
-mkdir -p $PREFIX/bin
+export CFLAGS="${CFLAGS} -fcommon -g -Wall -O3 -Wc++-compat -L$PREFIX/lib -fopenmp"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+mkdir -p "$PREFIX/bin"
 
 cd src
+
 #malloc.h is a Linux-ism
 sed -i.bak "s/malloc.h/stdlib.h/" load_unipath_size.c
 sed -i.bak "s/malloc.h/stdlib.h/" graph.c
 sed -i.bak "s/malloc.h/stdlib.h/" deBGA-master/load_input.c
 sed -i.bak "s/malloc.h/stdlib.h/" deBGA-master/index_build.c
+rm -f *.bak
 
-make -j ${CPU_COUNT} INCLUDES="-I$PREFIX/include" CFLAGS="-fcommon -g -Wall -O2 -Wc++-compat -L$PREFIX/lib -fopenmp" LIBS="-lm -lz -lpthread"
+make -j"${CPU_COUNT}" INCLUDES="-I$PREFIX/include" CFLAGS="${CFLAGS}" LIBS="-lm -lz -pthread"
 
-cp deSALT deBGA Annotation_Load.py $PREFIX/bin
+install -v -m 0755 deSALT deBGA Annotation_Load.py "$PREFIX/bin"
