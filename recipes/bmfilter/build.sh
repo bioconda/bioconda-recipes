f20a8b22d6be8b1c6fdf0820ac89a53b9ee8a9fd
mencian <joshua.zhuang@yahoo.com>
2025-09-18 18:32:04 -0500

    bmfilter: add arm64 build

--- recipes/bmfilter/build.sh
+++ recipes/bmfilter/build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-
 set -x
 
-if [[ ${target_platform} =~ linux.* ]]; then
-    make -j ${CPU_COUNT} -C general  CC="${CC}" CXX="${CXX}" DEBUG="${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -std=c++03"
-    make -j ${CPU_COUNT} -C bmtagger CC="${CC}" CXX="${CXX}" DEBUG="${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -std=c++03"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+install -d "${PREFIX}/bin"
+
+if [[ "${target_platform}" =~ linux.* ]]; then
+    make -j"${CPU_COUNT}" -C general CC="${CC}" CXX="${CXX}" DEBUG="${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -std=c++03"
+    make -j"${CPU_COUNT}" -C bmtagger CC="${CC}" CXX="${CXX}" DEBUG="${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS} -std=c++03"
     cd bmtagger
 fi
 
-install -d "${PREFIX}/bin"
-install bmfilter "${PREFIX}/bin/"
+install -v -m 0755 bmfilter "${PREFIX}/bin"
