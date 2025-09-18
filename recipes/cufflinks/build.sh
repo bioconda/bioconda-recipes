7155e6a6fbca79c5db9fb47a973ea1708229c6c1
mencian <joshua.zhuang@yahoo.com>
2025-09-18 17:06:10 -0500

    cufflinks: add aarch64/arm64 builds

--- recipes/cufflinks/build.sh
+++ recipes/cufflinks/build.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export CFLAGS="${CFLAGS} -O3"
 
-if [ $PY3K -eq 1 ]
-then
-    2to3 --write cuffmerge
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+mkdir -p "$PREFIX/bin"
+mkdir -p build-aux
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
+
+sed -i.bak 's|-m64||' configure.ac
+rm -f *.bak
+
+if [[ $PY3K -eq 1 ]]; then
+	2to3 --write cuffmerge
 fi
 
-cp cufflinks $PREFIX/bin
-cp cuffcompare $PREFIX/bin
-cp cuffdiff $PREFIX/bin
-cp cuffmerge $PREFIX/bin
-cp gffread $PREFIX/bin
-cp gtf_to_sam $PREFIX/bin
-cp cuffnorm $PREFIX/bin
-cp cuffquant $PREFIX/bin
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	--with-boost="${PREFIX}" \
+	--with-zlib="${PREFIX}" \
+	--with-eigen="${PREFIX}" \
+	--disable-option-checking \
+	--enable-dependency-tracking \
+	--enable-silent-rules \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS}" \
+	CC="${CC}" \
+	CFLAGS="${CFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}"
+
+make -j"${CPU_COUNT}"
+make install
+
+#cp cufflinks $PREFIX/bin
+#cp cuffcompare $PREFIX/bin
+#cp cuffdiff $PREFIX/bin
+#cp cuffmerge $PREFIX/bin
+#cp gffread $PREFIX/bin
+#cp gtf_to_sam $PREFIX/bin
+#cp cuffnorm $PREFIX/bin
+#cp cuffquant $PREFIX/bin
