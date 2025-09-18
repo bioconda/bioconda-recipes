d01757dec887dea299eb19bf92f723a7284d9a26
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-18 18:50:32 -0500

    Update build.sh

--- recipes/desalt/build.sh
+++ recipes/desalt/build.sh
@@ -6,6 +6,10 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p "$PREFIX/bin"
 
+case $(uname -m) in
+	aarch64|arm64) sed -i.bak 's|-msse2||' src/Makefile && sed -i.bak 's|-mno-sse4.1||' src/Makefile && sed -i.bak 's|-msse4.1||' src/Makefile ;;
+esac
+
 cd src
 
 #malloc.h is a Linux-ism
@@ -15,6 +19,9 @@ sed -i.bak "s/malloc.h/stdlib.h/" deBGA-master/load_input.c
 sed -i.bak "s/malloc.h/stdlib.h/" deBGA-master/index_build.c
 rm -f *.bak
 
-make -j"${CPU_COUNT}" INCLUDES="-I$PREFIX/include" CFLAGS="${CFLAGS}" LIBS="-lm -lz -pthread"
+case $(uname -m) in
+	aarch64|arm64) make INCLUDES="-I$PREFIX/include" CFLAGS="${CFLAGS}" LIBS="-lm -lz -pthread" arm_neon=1 -j"${CPU_COUNT}" ;;
+	*) make INCLUDES="-I$PREFIX/include" CFLAGS="${CFLAGS}" LIBS="-lm -lz -pthread" -j"${CPU_COUNT}" ;;
+esac
 
 install -v -m 0755 deSALT deBGA Annotation_Load.py "$PREFIX/bin"
