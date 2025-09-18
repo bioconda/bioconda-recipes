59a2eded871f746662cebe8e241a48bd92b5c818
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-18 17:23:10 -0500

    Update build.sh

--- recipes/cufflinks/build.sh
+++ recipes/cufflinks/build.sh
@@ -30,6 +30,7 @@ autoreconf -if
 	--with-boost="${PREFIX}" \
 	--with-zlib="${PREFIX}" \
 	--with-eigen="${PREFIX}" \
+	--with-bam="${PREFIX}" \
 	--disable-option-checking \
 	--enable-dependency-tracking \
 	--enable-silent-rules \
