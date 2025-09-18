466c71ebbb9774e20d8d3207d184b08379a69dea
mencian <joshua.zhuang@yahoo.com>
2025-09-18 16:02:35 -0500

    rust-bio-tools: add arm64 build

--- recipes/rust-bio-tools/build.sh
+++ recipes/rust-bio-tools/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=1
 cargo install --no-track --verbose --root "${PREFIX}" --path .
+
+"${STRIP}" "$PREFIX/bin/rbt"
