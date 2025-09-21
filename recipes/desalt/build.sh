eadbe718a29f976cde7727388801396d080aaa58
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-20 22:47:55 -0500

    Update build.sh

--- recipes/desalt/build.sh
+++ recipes/desalt/build.sh
@@ -6,6 +6,16 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p "$PREFIX/bin"
 
+if [[ "$(uname -m)" == "aarch64" ]] || [[ "$(uname -m)" == "arm64" ]]; then
+	git clone https://github.com/DLTcollab/sse2neon.git
+ 	cp -f sse2neon/sse2neon.h .
+  	sed -i.bak '9c #include "sse2neon.h"'  ksw2_ll_sse.c
+   	sed -i.bak '9c #include "sse2neon.h"'  ksw2_extz2_sse.c
+	sed -i.bak '10c #include "sse2neon.h"' ksw2_extd2_sse.c
+ 	sed -i.bak '10c #include "sse2neon.h"' ksw2_exts2_sse.c
+  	rm -f *.bak
+fi
+
 case $(uname -m) in
 	aarch64|arm64) sed -i.bak 's|-msse2||' src/Makefile && sed -i.bak 's|-mno-sse4.1||' src/Makefile && sed -i.bak 's|-msse4.1||' src/Makefile ;;
 esac
