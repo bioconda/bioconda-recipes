abe79532a4bbd0ce242d6aa54f90300370901b16
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-20 22:42:21 -0500

    Update build.sh

--- recipes/gangstr/build.sh
+++ recipes/gangstr/build.sh
@@ -4,6 +4,28 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
-cmake --build build --target install -j "${CPU_COUNT}"
+if [[ `uname -s` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
+  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+  "${CONFIG_ARGS}"
+
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
