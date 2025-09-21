6b1c3b1a27d276e385b0d4ef9310b8b7d941561c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-20 23:10:42 -0500

    Update build.sh

--- recipes/longtr/build.sh
+++ recipes/longtr/build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -O3 -g -D__STDC_LIMIT_MACROS -D_FILE_OFFSET_BITS=64 -std=c++0x -DMACOSX -pthread"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 mkdir -p "${PREFIX}/bin"
 
-make LongTR DenovoFinder PhasingChecker
+make LongTR DenovoFinder PhasingChecker -j"${CPU_COUNT}"
 
-cp LongTR "${PREFIX}/bin/"
+install -v -m 0755 LongTR DenovoFinder PhasingChecker "${PREFIX}/bin"
