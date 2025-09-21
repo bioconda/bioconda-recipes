36da99899c0ecbe58a50b1d082d41daad78167ba
mencian <joshua.zhuang@yahoo.com>
2025-09-20 23:32:13 -0500

    rustybam: add aarch64 build

--- recipes/rustybam/build.sh
+++ recipes/rustybam/build.sh
@@ -5,6 +5,9 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
+sed -i.bak 's|"0.44"|"0.47.1"|' Cargo.toml
+rm -f *.bak
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=1
