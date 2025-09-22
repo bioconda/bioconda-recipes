546ae7c035253ab9af50802bf6620ca0047cc87f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-22 14:45:47 -0500

    Update build.sh

--- recipes/visceral-evaluatesegmentation/build.sh
+++ recipes/visceral-evaluatesegmentation/build.sh
@@ -8,10 +8,10 @@ sed -i.bak 's/VERSION 2.8/VERSION 3.5/g' CMakeLists.txt
 rm -f *.bak
 
 cmake -S . -B build \
-    -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -D CMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" \
-    -D CMAKE_BUILD_TYPE=Release \
-    -D ITK_DIR="${PREFIX}/lib"
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DITK_DIR="${PREFIX}/lib"
 
 cmake --build build -j "${CPU_COUNT}"
 
