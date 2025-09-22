027885a750b323858dcfae861fa646d4676a9bde
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-22 16:41:44 -0500

    Update build.sh

--- recipes/r-stitch/build.sh
+++ recipes/r-stitch/build.sh
@@ -10,4 +10,10 @@ cp -rf STITCH.R $PREFIX/bin
 
 cd $SRC_DIR/STITCH
 
+if [[ "$(uname -m)" == "arm64" ]]; then
+    # hard gfortran paths in R build(?) break the build on osx-arm64
+    sed -i.bak 's|$(FLIBS)|-L$(PREFIX)/lib/gcc/arm64-apple-darwin20.0.0/13.3.0 -lgfortran -lquadmath -lm -L$(PREFIX)/lib/R/lib -lR -Wl,-framework -Wl,CoreFoundation|' src/Makevars
+    rm -f src/*.bak
+fi
+
 ${R} CMD INSTALL --build --install-tests . "${R_ARGS}"
