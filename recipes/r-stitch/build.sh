e7c1c28dbb54981f93ff2c9f6fecb7effdc7e8af
mencian <joshua.zhuang@yahoo.com>
2025-09-22 15:58:09 -0500

    r-stitch: add arm64 build

--- recipes/r-stitch/build.sh
+++ recipes/r-stitch/build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-
 set -xe
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+export DISABLE_AUTOBREW=1
+export LC_ALL="en_US.UTF-8"
 
-mkdir -p $PREFIX/bin 
+mkdir -p "$PREFIX/bin"
+
 cp -rf STITCH.R $PREFIX/bin
 
 cd $SRC_DIR/STITCH
-${R} CMD INSTALL --build --install-tests . ${R_ARGS}
+
+${R} CMD INSTALL --build --install-tests . "${R_ARGS}"
