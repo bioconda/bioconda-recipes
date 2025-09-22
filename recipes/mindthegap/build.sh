7c77bc68e4d0c643605e9b6192646af756e2b12d
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-09-23 04:59:22 +0800

    update mindthegap and enabled linux-aarch64  (#59515)
    * s
    mindthegap
    
    mindthegap
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * fix typo
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    ---------
    
    Co-authored-by: 高文尧 <12963281+gao-wenyao123@user.noreply.gitee.com>
    Co-authored-by: Joshua Zhuang <71105179+mencian@users.noreply.github.com>

--- recipes/mindthegap/build.sh
+++ recipes/mindthegap/build.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
-export CPATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -DH5_USE_110_API=1"
+export CXXFLAGS="${CXXFLAGS} -O3 -D_HDF5USEDLL_=1 -DUSE_NEW_CXX"
+
+mkdir -p $PREFIX/bin
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+sed -i.bak 's/cmake_minimum_required(VERSION 3.1)/cmake_minimum_required(VERSION 3.5...3.28)/g' CMakeLists.txt
+rm -f *.bak
+
+if [[ `uname -s` == "Linux" ]]; then
+  sed -i '1i #include "H5public.h"\n#if defined(H5_HAVE_114_API) || H5_VERSION_GE(1,12,0)\n#include "H5CXpublic.h"\n#else\n#define H5CX_set_apl(...) (0)\n#endif\n' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Odeprec.c
+
+  sed -i 's/H5P__encode_chunk_cache_nslots(const void \*value, void \*\*_pp, size_t \*size)/H5P__encode_chunk_cache_nslots(const void \*value, void \*\*_pp, size_t \*size, void \*udata)/g' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pdapl.c
+  sed -i '117s/)/, void *udata)/' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pdapl.c
+  sed -i 's/H5P__encode_chunk_cache_nbytes(const void \*value, void \*\*_pp, size_t \*size)/H5P__encode_chunk_cache_nbytes(const void \*value, void \*\*_pp, size_t \*size, void \*udata)/g' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pdapl.c
+  sed -i '120s/)/, void *udata)/' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pdapl.c
+  sed -i 's/H5P__dapl_vds_file_pref_enc(const void \*value, void \*\*_pp, size_t \*size)/H5P__dapl_vds_file_pref_enc(const void \*value, void \*\*_pp, size_t \*size, void \*udata)/g' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pdapl.c
+  sed -i 's/H5P__facc_libver_type_enc(const void \*value, void \*\*_pp, size_t \*size)/H5P__facc_libver_type_enc(const void \*value, void \*\*_pp, size_t \*size, void \*udata)/g' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pfapl.c
+  sed -i 's/H5P__ocpy_merge_comm_dt_list_enc(const void \*value, void \*\*_pp, size_t \*size)/H5P__ocpy_merge_comm_dt_list_enc(const void \*value, void \*\*_pp, size_t \*size, void \*udata)/g' thirdparty/gatb-core/gatb-core/thirdparty/hdf5/src/H5Pocpypl.c
+  sed -i 's/this->m_solid_stretch_size()/this->m_solid_stretch_size/g' src/FindBreakpoints.hpp
+fi
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+if [[ `uname -s` == "Darwin" ]]; then
+  export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+  export CONFIG_ARGS=""
+fi
+
 rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_CXX_FLAGS="$CXXFLAGS -DUSE_NEW_CXX" ..
-make -j2
 
-mkdir -p $PREFIX/bin/
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER="${CXX}" \
+  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -Wno-dev -Wno-deprecated --no-warn-unused-cli \
+  "${CONFIG_ARGS}"
 
-cp bin/MindTheGap ${PREFIX}/bin/
-chmod +x $PREFIX/bin/MindTheGap
+cmake --build build -j "${CPU_COUNT}"
 
-cp ext/gatb-core/bin/dbgh5 ${PREFIX}/bin/
-chmod +x $PREFIX/bin/dbgh5
-cp ext/gatb-core/bin/dbginfo ${PREFIX}/bin/
-chmod +x $PREFIX/bin/dbginfo
-
+install -v -m 0755 build/bin/MindTheGap "${PREFIX}/bin"
