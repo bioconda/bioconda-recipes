751b8c119d765ecca2a13fad4aedd8d91ea95802
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2025-09-23 08:36:32 +0930

    fix(longtr): update build script and override a make variable

--- recipes/longtr/build.sh
+++ recipes/longtr/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 mkdir -p "${PREFIX}/bin"
 
-make LongTR DenovoFinder PhasingChecker
+export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/htslib -I${PREFIX}/include/spoa"
+export LDFLAGS="-L${PREFIX}/lib -lm -lhts -lz -llzma -lbz2 -lcurl -lcrypto -lspoa"
+
+make LIBS='-L./ -L$(CEPHES_ROOT)/' \
+    LongTR DenovoFinder PhasingChecker
+
+install -v -m 0755 LongTR DenovoFinder PhasingChecker "${PREFIX}/bin"
