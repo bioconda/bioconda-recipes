371a39b86fbbba7aff72e4f3ef3513389d178e48
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2025-09-22 13:27:21 +0930

    fix(longtr): try patching Makefile to remove locally compiled htslib and spoa and use bioconda versions instead. Build only the required targets with make

--- recipes/longtr/build.sh
+++ recipes/longtr/build.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -O3 -g -D__STDC_LIMIT_MACROS -D_FILE_OFFSET_BITS=64 -std=c++0x -DMACOSX -pthread"
-
-case $(uname -m) in
-    aarch64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
-	;;
-    arm64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
-	;;
-    x86_64)
-	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
-	;;
-esac
-
 mkdir -p "${PREFIX}/bin"
 
-make CXXFLAGS="${CXXFLAGS}" -j"${CPU_COUNT}"
-
-install -v -m 0755 LongTR DenovoFinder PhasingChecker "${PREFIX}/bin"
+make LongTR DenovoFinder PhasingChecker
