434fa18909e7997bf60189085c0b8981db936ac5
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2025-09-22 09:54:37 +0100

    set clang version in build script

--- recipes/bgen-cpp/build.sh
+++ recipes/bgen-cpp/build.sh
@@ -8,6 +8,9 @@ export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++ -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+# use an older version of clang
+export CC=clang-17 CXX=clang++-17
+
 ./waf configure \
     --prefix="${PREFIX}" \
     --bindir="${PREFIX}/bin" \
