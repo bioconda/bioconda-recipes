86a9faadd8fb93c01818c735d36c8468424f53a8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-22 14:21:13 -0500

    Update build.sh

--- recipes/visceral-evaluatesegmentation/build.sh
+++ recipes/visceral-evaluatesegmentation/build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
+mkdir -p "${PREFIX}/bin"
 
 cd "source"
-if [ $target_platform == "linux-aarch64" ];then
 
-sed -i "s/VERSION 2.8/VERSION 3.5/g" CMakeLists.txt
+sed -i.bak 's/VERSION 2.8/VERSION 3.5/g' CMakeLists.txt
+rm -f *.bak
 
-fi
-#sed -i "s/LESS 4 /LESS 5 /g" CMakeLists.txt
-mkdir build
-cd build
+cmake -S . -B build \
+    -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -D CMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D ITK_DIR="${PREFIX}/lib"
 
-cmake \
-    -D CMAKE_INSTALL_PREFIX=${PREFIX} 		\
-    -D CMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
-    -D CMAKE_BUILD_TYPE=Release 		\
-    -D ITK_DIR=${PREFIX}/lib 			\
-    ..
+cmake --build build -j "${CPU_COUNT}"
 
-make -j
-
-cp EvaluateSegmentation ${PREFIX}/bin/EvaluateSegmentation
+install -v -m 0755 build/EvaluateSegmentation "${PREFIX}/bin"
