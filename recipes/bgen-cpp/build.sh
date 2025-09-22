fba06ec6ab1d4d380d7f61566e9432750b76e840
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2025-09-22 13:02:36 +0100

    set clang config for macs only

--- recipes/bgen-cpp/build.sh
+++ recipes/bgen-cpp/build.sh
@@ -1,23 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-set -euxo pipefail
-
-mkdir -p $PREFIX/bin
+mkdir -p "${PREFIX}/bin"
 
 export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++ -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-# use an older version of clang
-export CC=clang-17 CXX=clang++-17
+# use an older version of clang for macs
+if [[ "${OS}" == "Darwin" ]]; then
+    export CC=clang-17 CXX=clang++-17
+fi
 
-./waf configure \
-    --prefix="${PREFIX}" \
-    --bindir="${PREFIX}/bin" \
-    --libdir="${PREFIX}/lib"
-
-./waf
-
-cp build/apps/bgenix ${PREFIX}/bin
-cp build/apps/cat-bgen ${PREFIX}/bin
-cp build/apps/edit-bgen ${PREFIX}/bin
+./waf configure build install \
+      --prefix=${PREFIX} \
+      --bindir=${PREFIX}/bin \
+      --libdir=${PREFIX}/lib \
+      --jobs=${CPU_COUNT} \
+      CFLAGS="${CFLAGS} -I${PREFIX}/include -I${PREFIX}/include/boost" \
+      LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
