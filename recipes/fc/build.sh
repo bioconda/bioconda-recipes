0777d0bd71d4560a9f4354ada57918682122c172
mencian <joshua.zhuang@yahoo.com>
2025-09-22 20:42:43 -0500

    fc: add arm64 build

--- recipes/fc/build.sh
+++ recipes/fc/build.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 set -xe
 
-make -j"${CPU_COUNT}" CXX="${CXX}" INCLUDES="-I$PREFIX/include" CFLAGS+="${CFLAGS} -g -Wall -O2 -L$PREFIX/lib"
-chmod 755 ./bin/fc-virus
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-implicit-function-declaration"
+
 mkdir -p ${PREFIX}/bin
-cp -f ./bin/fc-virus ${PREFIX}/bin
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+make CXX="${CXX}" INCLUDES="-I$PREFIX/include" CXXFLAGS="${CXXFLAGS}" -j1
+
+install -v -m 0755 bin/fc-virus ${PREFIX}/bin
