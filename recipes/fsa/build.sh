b2af54d9300b12f79d4b23dd2316540c0a51e47c
mencian <joshua.zhuang@yahoo.com>
2025-09-22 20:56:41 -0500

    fsa: add arm64 build

--- recipes/fsa/build.sh
+++ recipes/fsa/build.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
-#
-# CONDA build script variables 
-# 
-# $PREFIX The install prefix
-# $PKG_NAME The name of the package
-# $PKG_VERSION The version of the package
-# $PKG_BUILDNUM The build number of the package
-#
 
-mkdir -p $PREFIX/bin
+export CFLAGS="${CFLAGS} -O3"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
-./configure --prefix=$PREFIX
+mkdir -p "$PREFIX/bin"
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+./configure --prefix="${PREFIX}" \
+	--disable-option-checking --enable-silent-rules --disable-dependency-tracking \
+	--with-mummer=yes \
+	--with-exonerate=yes \
+	CC="${CC}" \
+	CFLAGS="${CFLAGS}" \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}"
+
 make
 make install
