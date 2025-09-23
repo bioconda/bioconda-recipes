ca7a4bbb83aaed470503169afc5dbebcf866c595
mencian <joshua.zhuang@yahoo.com>
2025-09-22 22:35:26 -0500

    sga: add aarch64/arm64 builds

--- recipes/sga/build.sh
+++ recipes/sga/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include -O3"
-export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="${CFLAGS} -O3"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 export CPATH="${PREFIX}/include"
-export CXXFLAGS="-I${PREFIX}/include -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+
+mkdir -p build-aux
 
 pushd $SRC_DIR/src
 
-./autogen.sh
-./configure --prefix=$PREFIX --with-bamtools=$PREFIX --with-sparsehash=$PREFIX
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* build-aux/
+
+autoreconf -if
+./configure --prefix="$PREFIX" --with-bamtools="$PREFIX" --with-sparsehash="$PREFIX" \
+	CC="${CC}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" CXX="${CXX}" \
+	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}"
+
 make
 make install
