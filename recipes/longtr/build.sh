16bffa906202200382a3456d574af4e2f2bb9a58
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2025-09-23 11:23:05 +0930

    fix(longtr): PhasingChecker code is broken, so don't build

--- recipes/longtr/build.sh
+++ recipes/longtr/build.sh
@@ -7,6 +7,6 @@ mkdir -p "${PREFIX}/bin"
 make LIBS="-L./ -L\$(CEPHES_ROOT)/ -L${PREFIX}/lib -lm -lhts -lspoa" \
     INCLUDE="-Ilib -I${PREFIX}/include -I${PREFIX}/include/htslib -I${PREFIX}/include/spoa" \
     HTSLIB_LIB='' \
-    LongTR DenovoFinder PhasingChecker
+    LongTR DenovoFinder
 
-install -v -m 0755 LongTR DenovoFinder PhasingChecker "${PREFIX}/bin"
+install -v -m 0755 LongTR DenovoFinder "${PREFIX}/bin"
