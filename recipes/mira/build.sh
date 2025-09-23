66f038366450f446c51ecd5da53dfb4aae7fb978
mencian <joshua.zhuang@yahoo.com>
2025-09-23 18:24:05 -0500

    Update mira to v5.0.0rc2

--- recipes/mira/build.sh
+++ recipes/mira/build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/bin
-cp bin/* ${PREFIX}/bin
-cp scripts/* ${PREFIX}/bin
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I$PREFIX/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	--disable-option-checking --enable-silent-rules --disable-dependency-tracking \
+	--with-boost="${PREFIX}" \
+	--with-expat="${PREFIX}" \
+	--with-zlib="${PREFIX}" \
+	CC="${CC}" CFLAGS="${CFLAGS}" \
+	CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}"
+
+make -j"${CPU_COUNT}"
+make install
