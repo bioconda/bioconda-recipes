f7cbc53b625bfd9652a191455facee7650c0c4b9
Arthur Gymer <24782660+awgymer@users.noreply.github.com>
2025-09-23 11:01:02 +0930

    fix(longtr): remove unneeded linker args

--- recipes/longtr/build.sh
+++ recipes/longtr/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p "${PREFIX}/bin"
 
-export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/htslib -I${PREFIX}/include/spoa"
-export LDFLAGS="-L${PREFIX}/lib -lm -lhts -lz -llzma -lbz2 -lcurl -lcrypto -lspoa"
+#export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/htslib -I${PREFIX}/include/spoa"
+#export LDFLAGS="-L${PREFIX}/lib -lm -lhts -lz -llzma -lbz2 -lcurl -lcrypto -lspoa"
 
-make LIBS="-L./ -L\$(CEPHES_ROOT)/ -L${PREFIX}/lib -lm -lhts -lz -llzma -lbz2 -lcurl -lcrypto -lspoa" \
+make LIBS="-L./ -L\$(CEPHES_ROOT)/ -L${PREFIX}/lib -lm -lhts -lspoa" \
     INCLUDE="-Ilib -I${PREFIX}/include -I${PREFIX}/include/htslib -I${PREFIX}/include/spoa" \
     HTSLIB_LIB='' \
     LongTR DenovoFinder PhasingChecker
