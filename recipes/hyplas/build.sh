dfbec174b201dc60d55cd15b4ef5548dcc45ad0c
f0t1h <fatihkaraoglanoglu@gmail.com>
2025-09-24 15:09:48 -0700

    Fix missing executable hyplas

--- recipes/hyplas/build.sh
+++ recipes/hyplas/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export CXXFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-${PYTHON} installer.py ${PREFIX} --tool HyPlAs --skip  pandas numpy packaging platon spades racon minigraph minimap2
+${PYTHON} installer.py ${PREFIX} --tool HyPlAs_bin --skip pandas numpy packaging platon spades racon minigraph minimap2
 ${PYTHON} -m pip install . --no-deps -vvv
+${PYTHON} -m pip install external/unicycler-modified-for-hyplas/ --no-deps -vvv
