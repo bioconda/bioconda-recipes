c645a0dd9e31e6f2b1c22998ce384bdd6e73ba02
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-24 02:32:27 -0500

    Update build.sh

--- recipes/wgsim/build.sh
+++ recipes/wgsim/build.sh
@@ -6,6 +6,6 @@ export CFLAGS="${CFLAGS} -O3"
 
 "${CC}" ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o wgsim wgsim.c -lz -lm
 
-mkdir "-p ${PREFIX}/bin"
+mkdir -p "${PREFIX}/bin"
 
 install -v -m 0755 wgsim wgsim_eval.pl "${PREFIX}/bin"
