5ba5a10061f70af06c07828016b00e6dbe519ed3
ZerryNi <996002763@qq.com>
2025-09-24 16:18:28 +0800

    update genomeconstellation

--- recipes/genomeconstellation/build.sh
+++ recipes/genomeconstellation/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/include
+if [[ $(uname -m) == 'aarch64' ]]; then
 
+git clone https://github.com/DLTcollab/sse2neon.git
+cp sse2neon/sse2neon.h $PREFIX/include 
+fi
 cd src
 make CC="$CC" CFLAGS="$CFLAGS $LDFLAGS"
 cp jgi_gc $PREFIX/bin
