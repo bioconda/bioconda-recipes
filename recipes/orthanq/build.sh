8cb4077a62bfc5da4ef2f916f1960c915bb01f4f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-25 18:46:48 -0500

    Update build.sh

--- recipes/orthanq/build.sh
+++ recipes/orthanq/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -eu
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
-export HDF5_DIR=${PREFIX}
+export HDF5_DIR="${PREFIX}"
 export RUSTFLAGS="-C link-args=-Wl,-rpath,$HDF5_DIR/lib"
 
-if [ "${target_platform}" = "${build_platform}" ]; then
+if [[ "${target_platform}" = "${build_platform}" ]]; then
   export PYO3_PYTHON="${PYTHON}"
 fi
 
