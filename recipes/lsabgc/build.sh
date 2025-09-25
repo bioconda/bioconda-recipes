b2e990c7d1ea3ad554adf67ff282d9afb43a2eec
Rauf Salamzade <salamzader@gmail.com>
2025-09-25 15:21:20 -0700

    Delete recipes/lsabgc/build.sh

--- recipes/lsabgc/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -xe
-
-mkdir -p ${PREFIX}/bin
-
-$PYTHON -m pip install . --no-build-isolation --no-deps --no-cache-dir -vvv
-$PYTHON -m pip install git+https://github.com/gtonkinhill/panaroo
