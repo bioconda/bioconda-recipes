27b34691a5f00716ccdc207396ec33a01ba182ba
mencian <joshua.zhuang@yahoo.com>
2025-09-25 17:43:43 -0500

    Rebuild nextgenmap recipe

--- recipes/nextgenmap/build.sh
+++ recipes/nextgenmap/build.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_EXE_LINKER_FLAGS=-L${PREFIX}/lib ..
-make
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export CFLAGS="${CFLAGS} -O3"
 
-mkdir -p $PREFIX/bin
-cp -r ../bin/ngm-${PKG_VERSION}/* $PREFIX/bin/
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+mkdir -p "$PREFIX/bin"
+
+cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+
+ninja -C build install -j "${CPU_COUNT}"
+
+cp -rf bin/ngm-${PKG_VERSION}/opencl $PREFIX/bin/
+rm -rf bin/ngm-${PKG_VERSION}/opencl
+
+install -v -m 0755 bin/ngm-${PKG_VERSION}/* "$PREFIX/bin"
