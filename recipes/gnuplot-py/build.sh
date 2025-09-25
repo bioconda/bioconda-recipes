0c30a554dd4d57011471ea29bca0e5805fce94ba
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-25 16:26:03 -0500

    Delete recipes/gnuplot-py/build.sh

--- recipes/gnuplot-py/build.sh
+++
@@ -1,7 +0,0 @@
-
-sed -i '20s/cStringIO/io/' PlotItems.py
-sed -i '22s/StringIO/io/' PlotItems.py
-2to3 -w utils.py
-2to3 -w test.py
-sed -i "40a py_modules=['gp','gp_unix','Errors','PlotItems','utils','_Gnuplot','termdefs']," setup.py
-$PYTHON -m pip install . --ignore-installed --no-deps -vv
