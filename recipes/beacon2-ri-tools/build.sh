a142b65a459141b47ae2a93b2dd2b7d8e4c18997
khaled196 <khalled.jooma@yahoo.com>
2025-09-25 13:48:23 +0000

    update

--- recipes/beacon2-ri-tools/build.sh
+++ recipes/beacon2-ri-tools/build.sh
@@ -2,6 +2,7 @@
 set -exo pipefail
 
 # Copy all source files from SRC_DIR into the bin folder
+# This will copy the python scripts and the 'files' directory
 mkdir -p "$PREFIX/bin/beacon2-ri-tools/"
 cp -r "$SRC_DIR"/{*.py,scripts,ref_schemas,conf,files,validators} "$PREFIX/bin/beacon2-ri-tools/"
 
@@ -12,12 +13,12 @@ scripts=("analyses_csv.py" "biosamples_csv.py" "cohorts_csv.py" "convert_csvTObf
 for script in "${scripts[@]}"; do
   # Define the wrapper script path
   WRAPPER="$PREFIX/bin/${script}"
-  cat > "$WRAPPER" <<EOF
-#!/bin/bash
-# wrapper: change to the install dir so relative file paths inside scripts work
-cd "\$CONDA_PREFIX/bin/beacon2-ri-tools" || exit 1
-exec "\$PYTHON" "\$CONDA_PREFIX/bin/beacon2-ri-tools/${script}" "\$@"
-EOF
+
+  # Create the wrapper script that changes directory before running the Python script
+  # This ensures that relative paths work correctly.
+  echo '#!/bin/bash' > "$WRAPPER"
+  echo "cd \"\$CONDA_PREFIX/bin/beacon2-ri-tools/\"" >> "$WRAPPER"
+  echo "python \"./${script}\" \"\$@\"" >> "$WRAPPER"
 
   # Make the wrapper and the original python script executable
   chmod +x "$WRAPPER"
