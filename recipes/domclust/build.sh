6917b3dc3a558e4dc6e643de3da5baf1f50a5d05
mencian <joshua.zhuang@yahoo.com>
2025-09-26 00:15:23 -0500

    domclust: add arm64 build

--- recipes/domclust/build.sh
+++ recipes/domclust/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-deprecated-non-prototype -Wno-implicit-function-declaration -Wno-implicit-int"
 
-sed -i.bak 's|-g -O2 $(CFLAGS0)|-g -O3 $(CFLAGS0) -Xlinker -zmuldefs -Wno-implicit-int -Wno-implicit-function-declaration|' Makefile
+mkdir -p "$PREFIX/bin"
+
+sed -i.bak 's|-g -O2 $(CFLAGS0)|-g -O3 $(CFLAGS0) -Xlinker -zmuldefs -Wno-deprecated-non-prototype -Wno-implicit-function-declaration -Wno-implicit-int|' Makefile
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  sed -i.bak 's|-zmuldefs||' Makefile
+	sed -i.bak 's|-Xlinker -zmuldefs||' Makefile
 fi
 
-rm -rf *.bak
+rm -f *.bak
 
-make CC="${CC}" -j"${CPU_COUNT}"
+make CC="${CC}" -j1
+
 # copy binary to prefix folder
 install -v -m 0755 bin/domclust Script/*.pl build_input/*.pl "${PREFIX}/bin"
 
