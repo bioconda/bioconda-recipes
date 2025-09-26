d75c170938520b2be8ec11954dfe2825a648e447
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-25 21:35:34 -0500

    Update build.sh

--- recipes/orthanq/build.sh
+++ recipes/orthanq/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -ldeflate"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
@@ -8,7 +8,7 @@ export PYO3_PYTHON="${PYTHON}"
 
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
-export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib -L ${PREFIX}/lib"
+#export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib -L ${PREFIX}/include"
 
 sed -i.bak 's|"0.3.3"|{ version = "0.3.7", features = ["ab_glyph", "fontconfig-dlopen"] }|' Cargo.toml
 rm -f *.bak
@@ -16,6 +16,6 @@ rm -f *.bak
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=1
-cargo install -v --no-track --locked --root "${PREFIX}" --path .
+RUSTFLAGS="-L ${PREFIX}/include" cargo install -v --no-track --locked --root "${PREFIX}" --path .
 
 "${STRIP}" "${PREFIX}/bin/orthanq"
