be585ca5becca1e30d6c5cc1ffc0a5c4dca511ce
mencian <joshua.zhuang@yahoo.com>
2025-09-26 18:27:53 -0500

    Rebuild spades recipe

--- recipes/spades/build.sh
+++ recipes/spades/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e -o pipefail -x
 
 export LIBRARY_PATH="${PREFIX}/lib"
@@ -10,3 +9,5 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3 -fcommon -Wno-deprecated-declarations -Wno-unused-variable -Wno-parentheses -Wno-unused-result -Wno-unused-but-set-variable -Wno-conversion"
 
 PREFIX="${PREFIX}" ./spades_compile.sh -rj"${CPU_COUNT}"
+
+"${STRIP}" ${PREFIX}/bin/*
