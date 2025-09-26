68ee82d7e8f8dfb57d9671451194dd6d81f87354
mencian <joshua.zhuang@yahoo.com>
2025-09-26 14:39:14 -0500

    edit build.sh

--- recipes/corset/build.sh
+++ recipes/corset/build.sh
@@ -6,6 +6,18 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 
 mkdir -p "$PREFIX/bin"
 
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
 
 autoreconf -if
