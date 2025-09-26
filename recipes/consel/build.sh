b08444ba81419f6a276c425cde2c12dd1d327c9b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-25 20:28:32 -0500

    Update build.sh

--- recipes/consel/build.sh
+++ recipes/consel/build.sh
@@ -5,7 +5,9 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
+mkdir -p "${PREFIX}/bin"
+
 cd src
 
-make CC="${CC}" CFLAGS="${CFLAGS} -O3 -std=gnu89 -Wno-old-style-definition" -j"${CPU_COUNT}"
+make CC="${CC}" CFLAGS="${CFLAGS} -O3 -std=gnu99 -Wno-old-style-definition" -j"${CPU_COUNT}"
 make install bindir="${PREFIX}/bin"
