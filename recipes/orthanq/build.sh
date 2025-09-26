40c947fc0ce74ba12cf3a21a97e2eee52c2b867b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-09-25 19:06:27 -0500

    Update build.sh

--- recipes/orthanq/build.sh
+++ recipes/orthanq/build.sh
@@ -6,11 +6,15 @@ export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
-export HDF5_DIR="${PREFIX}"
-export RUSTFLAGS="-C link-args=-Wl,-rpath,$HDF5_DIR/lib"
+export RUSTFLAGS="-L ${PREFIX}/include -C link-args=-Wl,-rpath,${PREFIX}/lib"
 
-if [[ "${target_platform}" = "${build_platform}" ]]; then
+if [[ "${target_platform}" == "${build_platform}" ]]; then
   export PYO3_PYTHON="${PYTHON}"
 fi
 
-cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+RUST_BACKTRACE=1
+cargo install -v --no-track --root "${PREFIX}" --path .
+
+"${STRIP}" "${PREFIX}/bin/orthanq"
