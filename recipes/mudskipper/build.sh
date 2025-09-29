f1df2db7ba2841446ccd0cbef3499ca9649c5491
mencian <joshua.zhuang@yahoo.com>
2025-09-29 17:41:15 -0500

    Rebuild mudskipper recipe

--- recipes/mudskipper/build.sh
+++ recipes/mudskipper/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -euo
-export CFLAGS="${CFLAGS} -fcommon"
-export CXXFLAGS="${CFLAGS} -fcommon"
 
-# Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
-# We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="$(pwd)/.cargo"
-
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -ldeflate"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration -fcommon"
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
+sed -i.bak 's|"0.16.2"|"0.18.0"|' Cargo.toml
+sed -i.bak 's|"0.39.5"|"0.47.1"|' Cargo.toml
+rm -f *.bak
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
-RUST_BACKTRACE=1 cargo install --verbose --root $PREFIX --path .
+RUST_BACKTRACE=1
+cargo install -v --no-track --root "${PREFIX}" --path .
+
+"${STRIP}" "${PREFIX}/bin/mudskipper"
