0a889c3e33746a2ce4a157a325a3de1dfae6b3b8
mencian <joshua.zhuang@yahoo.com>
2025-10-01 18:45:59 -0500

    Rebuild bioconductor-rsbml recipe

--- recipes/bioconductor-rsbml/build.sh
+++ recipes/bioconductor-rsbml/build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export DISABLE_AUTOBREW=1
+
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
+
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+
+${R} CMD INSTALL --build . "${R_ARGS}"
