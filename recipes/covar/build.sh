5153b8a13426df654215615861c604d3b62d170f
dylanpilz <dpilz@ucsd.edu>
2025-10-02 14:10:26 -0700

    fix build/ metadata yaml

--- recipes/covar/build.sh
+++ recipes/covar/build.sh
@@ -2,13 +2,10 @@
 
 set -xeuo
 
-pushd covar
-
 cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
 
-popd
 
 # build statically linked binary with Rust:
 RUST_BACKTRACE=1 
-cargo install --no-track --verbose --root "${PREFIX}" --path ./covar --all-features
+cargo install --no-track --verbose --root "${PREFIX}" --path . --all-features
 
