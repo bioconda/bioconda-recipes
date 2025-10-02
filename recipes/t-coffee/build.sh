7fe541fe9d985e92353be142516656472a3c7213
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-10-02 09:00:29 -0500

    Update build.sh

--- recipes/t-coffee/build.sh
+++ recipes/t-coffee/build.sh
@@ -14,21 +14,24 @@ mkdir -p "${PREFIX}/bin"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 # -fsigned-char is needed for aarch64; register needs to be hidden for os-x's C++ compiler
-export CXXFLAGS="${CXXFLAGS} -O3 -fsigned-char -Wno-write-strings -Wno-return-type -Dregister=''"
+export CFLAGS="${CFLAGS} -O3 -fsigned-char -Wno-write-strings -Dregister=''"
+export CXXFLAGS="${CXXFLAGS} -O3 -fsigned-char -Wno-write-strings -Wno-return-type"
 # silence some LANG perl warning messages:
 export LC_ALL="en_US.UTF-8"
+export SHARE_DIR="${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
+
+OS="$(./install get_os)"
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-	export CFLAGS="${CFLAGS} -fno-define-target-os-macros"
 fi
-
-export SHARE_DIR="${PREFIX}/libexec/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
-OS=$(./install get_os)
 
 sed -i.bak 's|CC=g++|CC=$(CXX)|' t_coffee_source/makefile
 sed -i.bak 's|$(FCC)|$(FC)|' t_coffee_source/makefile
 sed -i.bak 's|-O3 -Wno-write-strings|-O3 -fpermissive -fsigned-char -Wno-write-strings -Wno-register -Wno-return-type -march=x86-64-v3|' t_coffee_source/makefile
+
+sed -i.bak 's|our $CXX="g++"|our $CXX=$ENV{"CXX"}|' install
+sed -i.bak 's|our $FC="f77"|our $FC=$ENV{"FC"}|' install
 
 case $(uname -m) in
     aarch64)
@@ -51,7 +54,7 @@ cd ..
 # the t-coffee home only has plugins with x86_64 support; let's not
 # download them. Instead use only bioconda's own installs.
 # the t_coffee application is the only one from the set required by Bio::Tools::Run::Alignment::TCoffee
-./install t_coffee -tcdir="${SHARE_DIR}" CC="${CXX}" CFLAGS="${CXXFLAGS}"
+./install t_coffee -tcdir="${SHARE_DIR}" CC="${CC}" CXX="${CXX}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" FCC="${FC}"
 
 # The installer may try to update dependencies and install them to bin/,
 # which will cause conflicts with the dependencies as separately packaged.
