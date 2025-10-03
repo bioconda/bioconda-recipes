28bd03a16d553bd9193875729527edcf5649e24c
FriederikeHanssen <friederike.hanssen@seqera.io>
2025-10-03 13:03:18 +0200

    tryand fix the seg fault issue

--- recipes/msisensor2/build.sh
+++ recipes/msisensor2/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -eu -o pipefail
+
 mkdir -p $PREFIX/bin
+
+# Copy the pre-built binary
 cp msisensor2 $PREFIX/bin/msisensor2
 chmod +x $PREFIX/bin/msisensor2
+
+# Fix RPATH to use relative path from binary to lib directory
+patchelf --set-rpath '$ORIGIN/../lib' $PREFIX/bin/msisensor2
