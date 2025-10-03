be10fafab8940dca9c2bec9cd284fc5a8c192bf3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-10-02 20:50:16 -0500

    Update build.sh

--- recipes/wtdbg/build.sh
+++ recipes/wtdbg/build.sh
@@ -5,12 +5,17 @@ export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
 mkdir -p "$PREFIX/bin"
 
-sed -i.bak 's|-O4|-O4 -std=gnu11|' Makefile
+sed -i.bak 's|-O4|-O4 -std=gnu11 -I$(PREFIX)/include -Wno-implicit-function-declaration -Wno-int-conversion|' Makefile
 sed -i.bak 's|-lpthread|-pthread|' Makefile
 
 case $(uname -m) in
     aarch64|arm64)
+        git clone https://github.com/DLTcollab/sse2neon.git
+        cp -f sse2neon/sse2neon.h .
+        
         sed -i.bak 's|-mpopcnt -msse4.2||' Makefile
+        sed -i.bak 's|#include <emmintrin.h>|#include "sse2neon.h"|' ksw.c
+        sed -i.bak 's|#include <emmintrin.h>|#include "sse2neon.h"|' poacns.h
         ;;
 esac
 
