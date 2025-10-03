47c6f69bf8d6ca8fe8fe74fe873193e9686c46ad
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-10-03 00:00:06 -0500

    Update build.sh

--- recipes/crispritz/build.sh
+++ recipes/crispritz/build.sh
@@ -2,12 +2,31 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -D_GLIBCXX_PARALLEL"
 
 mkdir -p "${PREFIX}/bin"
 mkdir -p "${PREFIX}/opt/crispritz"
 
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 #sed -i.bak 's|#include <parallel/algorithm>|#include <algorithm>|' sourceCode/CRISPR-Cas-Tree/mainParallel.cpp
+case $(uname -s) in
+    "Darwin")
+        export CXX="${CXX} -stdlib=libstdc++"
+		export LDFLAGS="${LDFLAGS} -lstdc++"
+        ;;
+esac
+rm -f *.bak
 
 make -f Makefile_conda \
     CXX="${CXX} ${CPPFLAGS} ${CXXFLAGS} ${LDFLAGS}" \
