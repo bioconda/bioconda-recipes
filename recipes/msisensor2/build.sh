d6ea996d9e9462d955c3c397e3183905592a8bf4
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-10-05 21:21:50 -0500

    Update build.sh

--- recipes/msisensor2/build.sh
+++ recipes/msisensor2/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX/bin"
 
 # Copy the pre-built binary
-cp msisensor2 $PREFIX/bin/msisensor2
-chmod +x $PREFIX/bin/msisensor2
+install -v -m 0755 msisensor2 "$PREFIX/bin"
 
 # Fix RPATH to use relative path from binary to lib directory
-patchelf --set-rpath '$ORIGIN/../lib' $PREFIX/bin/msisensor2
+patchelf --set-rpath '$ORIGIN/../lib' "$PREFIX/bin/msisensor2"
