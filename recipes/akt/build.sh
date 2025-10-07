4ac2995bfc63866f356e1ba1471464e8129d404f
mencian <joshua.zhuang@yahoo.com>
2025-10-07 18:25:01 -0500

    akt: add arm64 build

--- recipes/akt/build.sh
+++ recipes/akt/build.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
-if [ `uname -m` == "aarch64" ]; then
-sed -i 's/\-mpopcnt//g' Makefile
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export LC_ALL=C
+
+install -d "${PREFIX}/bin"
+
+if [[ `uname -m` == "aarch64" || `uname -m` == "arm64" ]]; then
+	sed -i'.bak' 's/\-mpopcnt//g' Makefile
 fi
 
-make \
-    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
-    CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
-install -d "${PREFIX}/bin"
-install ./akt "${PREFIX}/bin/"
+sed -i.bak 's|-lpthread|-pthread|g' Makefile
+sed -i.bak 's|-std=c++11 -O2|-std=c++14 -O3 -march=x86-64-v3 -Wno-implicit-function-declaration|' Makefile
+sed -i.bak 's|-O2|-O3|g' Makefile
+
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' Makefile
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' Makefile
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+rm -f *.bak
+
+make CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
+
+install -v -m 0755 akt "${PREFIX}/bin"
