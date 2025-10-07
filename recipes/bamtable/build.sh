4f603164b77de4d2cc2b251ab13e943c4dad686a
Thiseas Christos Lamnidis <thisseass@gmail.com>
2025-10-07 11:06:11 +0200

    Revert "WIP"
    This reverts commit 7227f957d12af90b55bf27eb511678684e3fe4a0.

--- recipes/bamtable/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure we run successfully using either conda-forge or defaults ncurses
-# (unlike other platforms, the latter does not automatically pull in libtinfo)
-tar -vxzf BamTable-*.tar.gz
-cd BamTable-*/
-
-make all
-make install
