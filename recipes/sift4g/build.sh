b44ec390aec0f20ae27a4369280f8401c4bb08c3
mencian <joshua.zhuang@yahoo.com>
2025-10-08 16:07:45 -0500

    sift4g: add arm64 build

--- recipes/sift4g/build.sh
+++ recipes/sift4g/build.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-make -j 2 \
-    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
-    CP="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
 install -d "${PREFIX}/bin"
-install bin/* "${PREFIX}/bin/"
+
+make CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+	CP="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+	-j"${CPU_COUNT}"
+
+install -v -m 0755 bin/* "${PREFIX}/bin"
