7036c35e1a19316acfdd5402520079bf3c9eea4e
mencian <joshua.zhuang@yahoo.com>
2025-10-08 17:44:24 -0500

    transtermhp: add arm64 build

--- recipes/transtermhp/build.sh
+++ recipes/transtermhp/build.sh
@@ -1,9 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-make
-mv transterm 2ndscore calibrate.sh make_expterm.py mfold_rna.sh $PREFIX/bin
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
 mkdir -p $PREFIX/data
+
+sed -i.bak 's|2.08|2.09|' transterm.cc
+sed -i.bak 's|-O3|-O3 -std=c++03 -march=x86-64-v3|' Makefile
+
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' Makefile
+	;;
+    arm64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' Makefile
+	;;
+esac
+rm -f *.bak
+
+make
+
+install -v -m 0755 transterm 2ndscore calibrate.sh make_expterm.py mfold_rna.sh random_fasta.py "$PREFIX/bin"
+
 mv expterm.dat $PREFIX/data
 
 mkdir -p $PREFIX/etc/conda/activate.d/
