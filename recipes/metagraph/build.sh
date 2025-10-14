83498df21e18f938a4d00536b589f3e1fa249200
Mikhail Karasikov <karasikov@users.noreply.github.com>
2025-10-14 17:16:46 +0200

    Update build.sh

--- recipes/metagraph/build.sh
+++ recipes/metagraph/build.sh
@@ -33,7 +33,6 @@ if [[ "${OS}" == "Linux" ]]; then
 	export CXXFLAGS="${CXXFLAGS} -Wno-attributes -Wno-narrowing -Wno-type-limits"
 	export CONFIG_ARGS=""
 	sed -i.bak 's|Boost_USE_STATIC_LIBS ON|Boost_USE_STATIC_LIBS OFF|' metagraph/CMakeLists.txt
-	rm -f metagraph/external-libraries/htslib/htscodecs/htscodecs/*.bak
 elif [[ "${OS}" == "Darwin" ]]; then
 	sed -i.bak 's|/usr/local/gcc-6.3.0/bin/g++|${CXX}|' metagraph/external-libraries/KMC/makefile_mac
  	sed -i.bak 's|-m64||' metagraph/external-libraries/KMC/makefile_mac
@@ -41,7 +40,6 @@ elif [[ "${OS}" == "Darwin" ]]; then
 	sed -i.bak 's|Boost_USE_STATIC_LIBS ON|Boost_USE_STATIC_LIBS OFF|' metagraph/CMakeLists.txt
 	sed -i.bak 's|link_directories(/opt/homebrew/opt/icu4c/lib)|link_directories(${PREFIX}/lib)|' metagraph/CMakeLists.txt
 	sed -i.bak 's|link_directories(/usr/local/opt/icu4c/lib)|link_directories(${PREFIX}/lib)|' metagraph/CMakeLists.txt
-	rm -f metagraph/external-libraries/htslib/htscodecs/htscodecs/*.bak
 	CMAKE_PLATFORM_FLAGS="-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
 	export CXXFLAGS="${CXXFLAGS} -Wno-implicit-function-declaration -Wno-suggest-destructor-override -Wno-error=deprecated-copy -D_LIBCPP_DISABLE_AVAILABILITY"
 	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
