402d05ba35507d58e1b1afd60310355f782f50fa
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-10-14 15:30:55 +0800

    Update build.sh

--- recipes/harvesttools/build.sh
+++ recipes/harvesttools/build.sh
@@ -12,8 +12,10 @@ sed -i '1s/-std=c++11/-std=c++14 -g/' Makefile.in
 sed -i '8s/-Wl,--wrap=memcpy//' Makefile.in
 sed -i "53s/compile/compile -I@capnp@\/include/" Makefile.in
 sed -i '210s/INT_MAX, //' src/harvest/HarvestIO.cpp
-sed -i "1s/2.2.5/2.17/" src/harvest/memcpyLink.h
 sed -i 's/ln -sf/cp -f/g' Makefile.in 
+if [[ $(uname) == 'aarch64' ]];then
+ 	sed -i "1s/2.2.5/2.17/" src/harvest/memcpyLink.h
+fi
 ./configure --prefix="${PREFIX}" \
 	--with-protobuf="${BUILD_PREFIX}" \
 	--with-capnp="${PREFIX}" \
