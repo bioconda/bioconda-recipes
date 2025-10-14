3bfcc5b56b586526bde2a994542e37da153ce8db
mencian <joshua.zhuang@yahoo.com>
2025-10-13 21:23:02 -0500

    bcov: add arm64 build

--- recipes/bcov/build.sh
+++ recipes/bcov/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+
 autoreconf -fi
-./configure --exec-prefix=$PREFIX
+./configure --prefix="${PREFIX}"
+
 make
 make install
