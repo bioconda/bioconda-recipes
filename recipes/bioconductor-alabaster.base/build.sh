90a6a25174d9e13a609940f38b0fac02cddba4b3
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-10-15 15:17:07 +0200

    manually inject 10.15 sdk

--- recipes/bioconductor-alabaster.base/build.sh
+++ recipes/bioconductor-alabaster.base/build.sh
@@ -4,7 +4,7 @@ grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
 # TODO: this workaround can be dropped once R v4.4.3 build 4+ are used
 if [ "$(uname)" == "Darwin" ]; then
-  sed -i 's/-mmacosx-version-min=10.13//g' ${PREFIX}/lib/R/etc/Makeconf
+  sed -i 's/-mmacosx-version-min=10.13/-mmacosx-version-min=10.15/g' ${PREFIX}/lib/R/etc/Makeconf
 fi
 
 mkdir -p ~/.R
