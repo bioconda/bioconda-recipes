8ec40c25acfc872f8c244dfb4a9a1d0d7971d95a
rlibouban <romane.libouban@irisa.fr>
2025-10-29 17:26:34 +0100

    fix mvn error

--- recipes/interproscan/build.sh
+++ recipes/interproscan/build.sh
@@ -16,9 +16,12 @@ make
 cp ncoils ../../../../bin/ncoils/2.2.1/
 cd ${current_dir}
 
+# Run mvn clean install to build and install (into your local Maven repository) all of the modules for InterProScan 5.
+mvn clean install
+
 # cd into the jms-implementation directory and run mvn clean package
 cd jms-implementation
-mvn -DskipTests -o install
+mvn clean package
 
 # copy result into the share folder
 cp -r target/interproscan-5-dist/* ${IPR_DIR}/
