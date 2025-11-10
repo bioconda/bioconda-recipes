1597ec4909a3f0f2940fae320a3f1b0e79b7c108
Mikko Rautiainen <m_rautiainen@hotmail.com>
2025-11-10 10:18:55 +0200

    typo

--- recipes/isoformcheck/build.sh
+++ recipes/isoformcheck/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p "${PREFIX}/bin"
-mkdir -p "${PREFIX}/bin/isoformcheck_lib"
+mkdir -p "${PREFIX}/bin/isoformchecklib"
 
 cp src/IsoformCheck "${PREFIX}/bin"
-cp src/isoformcheck_lib/*.py "${PREFIX}/bin/isoformcheck_lib"
+cp src/isoformchecklib/*.py "${PREFIX}/bin/isoformchecklib"
