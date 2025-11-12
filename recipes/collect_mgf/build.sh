30ca8855e65f80374604256dcf8bf6f5e86768b2
root <root@localhost.localdomain>
2025-11-12 10:30:25 +0800

    update collect_mgf and enable linux-aarch64

--- recipes/collect_mgf/build.sh
+++ recipes/collect_mgf/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+sed -i '12c\#include <unistd.h>' collect_mgf.c
+sed -i '58s/close/fclose/' collect_mgf.c
+sed -i '85s/close/fclose/' collect_mgf.c
+sed -i '89s/close/fclose/' collect_mgf.c
+
 "${CC}" ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o collect_mgf collect_mgf.c
 install -d "${PREFIX}/bin"
 install collect_mgf "${PREFIX}/bin/"
