163f962e5ac1db3995bc823a36fd300c5efc3ab6
ZerryNi <47553636+ZerryNi@users.noreply.github.com>
2025-11-12 10:55:14 +0800

    Update build.sh

--- recipes/collect_mgf/build.sh
+++ recipes/collect_mgf/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+if [[ "$(uname -m )" == 'aarch64']];then
 sed -i '12c\#include <unistd.h>' collect_mgf.c
 sed -i '58s/close/fclose/' collect_mgf.c
 sed -i '85s/close/fclose/' collect_mgf.c
 sed -i '89s/close/fclose/' collect_mgf.c
-
+fi
 "${CC}" ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o collect_mgf collect_mgf.c
 install -d "${PREFIX}/bin"
 install collect_mgf "${PREFIX}/bin/"
