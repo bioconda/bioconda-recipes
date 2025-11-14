038a2840d503de03e722e12cf0d47197f0eaf969
Mikhail Karasikov <karasikov@users.noreply.github.com>
2025-11-14 02:28:44 +0100

    Update metagraph to 0.4.5 (#59714)
    * Update build.sh
    
    * enable osx
    
    * Update meta.yaml
    
    * Update 0001-boost-cmake.patch
    
    * Delete recipes/metagraph/0001-boost-cmake.patch
    
    * Update build.sh
    
    * add icu for osx
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    cleanup
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update build.sh
    
    * removed htslib cp command
    
    * Update meta.yaml
    
    * download from tar and check sha256sum
    
    * download from releases
    
    * remove htslib patch lines
    
    * Update build.sh
    
    * Update meta.yaml
    
    * create version.h
    
    * removed jemalloc dirs
    
    * removed jemalloc
    
    * cleanup
    
    * Update metagraph dependencies and build configuration
    
    * removed -Wno-invalid-specialization from clang
    
    * don't wait for linux CI to finish
    
    * added  -Wno-invalid-specialization
    
    * install jemalloc
    
    * revert
    
    * link against jemalloc on Darwin
    
    * minor
    
    * without jemalloc on macos
    
    * cleanup
    
    * added -Wno-attributes
    
    * added jemalloc patch
    
    * moved
    
    * libgomp also for macos
    
    * try libomp
    
    * try llvm-openmp
    
    * try
    
    * try
    
    * added OSX_SYSROOT
    
    * added OMP_ROOT
    
    * try
    
    * Temporarily disable Linux Tests to focus on macOS build issues
    
    * Remove temporary files
    
    * test
    
    * /lib
    
    * try
    
    * cleanup
    
    * cleanup
    
    * also compile for linux
    
    * try with jemalloc
    
    * patch with jemalloc
    
    * osx without jemalloc
    
    * revert minor changes, also added adamant-pwn
    
    * update
    
    ---------
    
    Co-authored-by: Joshua Zhuang <71105179+mencian@users.noreply.github.com>

--- recipes/metagraph/build.sh
+++ recipes/metagraph/build.sh
@@ -2,64 +2,15 @@
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -Wno-deprecated-declarations"
-export CFLAGS="${CFLAGS} -O3 -Wno-unused-but-set-variable"
-export JEMALLOC_LIBRARY="${PREFIX}/lib"
-export JEMALLOC_INCLUDE_DIR="${PREFIX}/include"
-export BOOST_INCLUDEDIR="${PREFIX}/include"
-export BOOST_LIBRARYDIR="${PREFIX}/lib"
-
-sed -i.bak 's|VERSION 2.8.2|VERSION 3.5|' metagraph/CMakeLists.txt.in
-sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' metagraph/CMakeListsKMC.txt.in
-sed -i.bak 's|VERSION 2.8.11|VERSION 3.5|' metagraph/external-libraries/sdsl-lite/CMakeLists.txt
-sed -i.bak 's|VERSION 2.4.4|VERSION 3.5|' metagraph/external-libraries/sdsl-lite/external/libdivsufsort/CMakeLists.txt
-sed -i.bak 's|VERSION 2.6.4|VERSION 3.5|' metagraph/external-libraries/sdsl-lite/external/googletest/CMakeLists.txt
-sed -i.bak 's|VERSION 2.6.4|VERSION 3.5|' metagraph/external-libraries/sdsl-lite/external/googletest/googletest/CMakeLists.txt
-sed -i.bak 's|VERSION 2.6.4|VERSION 3.5|' metagraph/external-libraries/sdsl-lite/external/googletest/googlemock/CMakeLists.txt
-sed -i.bak 's|VERSION 2.6|VERSION 3.5|' metagraph/external-libraries/DYNAMIC/CMakeLists.txt
-sed -i.bak 's|VERSION 2.8.12|VERSION 3.5|' metagraph/external-libraries/zlib/CMakeLists.txt
-sed -i.bak 's|VERSION 3.1.3|VERSION 3.5|' metagraph/external-libraries/caches/CMakeLists.txt
-sed -i.bak 's|VERSION 2.8.11|VERSION 3.5|' metagraph/external-libraries/eigen/CMakeLists.txt
-sed -i.bak 's|VERSION 3.0.2|VERSION 3.5|' metagraph/external-libraries/folly/CMakeLists.txt
-sed -i.bak 's|VERSION 3.1|VERSION 3.5|' metagraph/external-libraries/hopscotch-map/CMakeLists.txt
-sed -i.bak 's|VERSION 3.1|VERSION 3.5|' metagraph/external-libraries/ordered-map/CMakeLists.txt
+export CXXFLAGS="${CXXFLAGS} -Wno-deprecated-declarations -Wno-attributes"
 
 ARCH=$(uname -m)
 OS=$(uname -s)
 
-if [[ "${ARCH}" == "arm64" || "${ARCH}" == "aarch64" ]]; then
-	sed -i.bak 's|"-mavx"|""|' metagraph/CMakeListsKMC.txt.in
-	sed -i.bak 's|-mavx2||' metagraph/CMakeListsKMC.txt.in
-	sed -i.bak 's|-mfma||' metagraph/CMakeListsKMC.txt.in
-	sed -i.bak 's|"-msse2"|""|' metagraph/CMakeListsKMC.txt.in
-	sed -i.bak 's|"-msse4.1"|""|' metagraph/CMakeListsKMC.txt.in
-	sed -i.bak 's|-m64||' metagraph/CMakeListsKMC.txt.in
-	sed -i.bak 's|g++|${CXX}|' metagraph/external-libraries/KMC/makefile
-	sed -i.bak 's|-m64||' metagraph/external-libraries/KMC/makefile
-	rm -rf metagraph/external-libraries/KMC/*.bak
-	sed -i.bak 's|-mcx16||' metagraph/CMakeLists.txt
-	rm -rf metagraph/*.bak
-fi
+# set version manually since we're installing from source and not from a checked out git repo
+echo '#define HTSCODECS_VERSION_TEXT "1.6.4"' > metagraph/external-libraries/htslib/htscodecs/htscodecs/version.h
 
-if [[ "${OS}" == "Linux" ]]; then
-	CMAKE_PLATFORM_FLAGS=""
-	export CXXFLAGS="${CXXFLAGS} -Wno-attributes -Wno-narrowing -Wno-type-limits"
-	export CONFIG_ARGS=""
-elif [[ "${OS}" == "Darwin" ]]; then
-	sed -i.bak 's|/usr/local/gcc-6.3.0/bin/g++|${CXX}|' metagraph/external-libraries/KMC/makefile_mac
- 	sed -i.bak 's|-m64||' metagraph/external-libraries/KMC/makefile_mac
-	rm -rf metagraph/external-libraries/KMC/*.bak
-	CMAKE_PLATFORM_FLAGS="-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
-	export CXXFLAGS="${CXXFLAGS} -Wno-implicit-function-declaration -Wno-suggest-destructor-override -Wno-error=deprecated-copy -D_LIBCPP_DISABLE_AVAILABILITY"
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-fi
-
-sed -i.bak 's|-O2|-O3|' metagraph/CMakeLists.txt
-sed -i.bak 's|-lpthread|-pthread|' metagraph/CMakeLists.txt
-sed -i.bak 's|-std=c++11|-std=c++14|' metagraph/CMakeListsKMC.txt.in
-rm -rf metagraph/*.bak
-sed -i.bak 's|-std=c++11|-std=c++14|' metagraph/external-libraries/sdsl-lite/CMakeLists.txt
-rm -rf metagraph/external-libraries/sdsl-lite/*.bak
+sed -i.bak 's|Boost_USE_STATIC_LIBS ON|Boost_USE_STATIC_LIBS OFF|' metagraph/CMakeLists.txt
 
 pushd metagraph/external-libraries/sdsl-lite
 ./install.sh "${PWD}"
@@ -74,21 +25,11 @@ export PIP_NO_INDEX=False
 
 CMAKE_PARAMS="-DCMAKE_BUILD_TYPE=Release \
             -DBOOST_ROOT=${PREFIX} \
-            -DJEMALLOC_ROOT=${PREFIX} \
-            -DOMP_ROOT=${PREFIX} \
-            -DCMAKE_PREFIX_PATH=${PREFIX} \
             -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-            -DCMAKE_CXX_COMPILER=${CXX} \
-            -DCMAKE_C_COMPILER=${CC} \
             -DCMAKE_INSTALL_PREFIX=${PREFIX} \
             -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=1 \
-            -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DBUILD_KMC=OFF \
-	    -DWITH_AVX=OFF -Wno-dev -Wno-deprecated --no-warn-unused-cli \
-            ${CMAKE_PLATFORM_FLAGS} ${CONFIG_ARGS}"
-
-if [[ "${ARCH}" == "arm64" || "${ARCH}" == "aarch64" ]]; then
-	CMAKE_PARAMS="${CMAKE_PARAMS} -DWITH_MSSE42=OFF"
-fi
+            -DBUILD_KMC=OFF \
+            "
 
 cmake -S .. -B . ${CMAKE_PARAMS}
 
