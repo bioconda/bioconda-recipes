d1cfe9c2c91c3f6d17cc7cfa23c44a250c2ccd3b
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2025-11-14 09:42:01 +0100

    Update post-link.sh

--- recipes/crisprhawk/post-link.sh
+++ recipes/crisprhawk/post-link.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$PREFIX/bin/python -m pip install --no-deps charset-normalizer==3.4.2 torch==2.2.2 rs3==0.0.16 sglearn==1.2.5 seqfold==0.7.18
+$PREFIX/bin/python -m pip install charset-normalizer==3.4.2 torch==2.2.2 rs3==0.0.16 sglearn==1.2.5 seqfold==0.7.18
