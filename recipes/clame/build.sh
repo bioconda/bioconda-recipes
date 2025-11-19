8741ccc31d2e47d288ea25332de0a47404adaa1e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-18 18:48:55 -0600

    Update build.sh

--- recipes/clame/build.sh
+++ recipes/clame/build.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
-echo $PREFIX
 
-sed -i '1c\CPP ?= $(CXX)' Makefile
-sed -i '1c\CPP ?= $(CXX)' binning/Makefile
-sed -i '1c\CPP ?= $(CXX)' genFm9/Makefile
-sed -i '1c\CPP ?= $(CXX)' mapping/Makefile
-echo "Binning compilation"
-cd binning
-make all CXX=${CXX}  ILIB="-I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib"
 mkdir -p $PREFIX/bin
-cp binning $PREFIX/bin
+
+sed -i.bak 's|CPP=g++|CPP?=$(CXX)|' Makefile
+	sed -i.bak 's|CPP=g++|CPP?=$(CXX)|' binning/Makefile
+	sed -i.bak 's|CPP=g++|CPP?=$(CXX)|' genFm9/Makefile
+	sed -i.bak 's|CPP=g++|CPP?=$(CXX)|' mapping/Makefile
+echo "Binning compilation"
+
+cd binning
+make all CXX=${CXX} ILIB="-I${PREFIX}/include -L${PREFIX}/lib"
+
+install -v -m 0755 binning $PREFIX/bin
 cd ..
 
 echo "genFm9 compilation"
 cd genFm9
-make all CPP=${CXX}  ILIB="-I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib"
-cp genFm9 $PREFIX/bin
+make all CPP=${CXX} ILIB="-I${PREFIX}/include -L${PREFIX}/lib"
+install -v -m 0755 genFm9 $PREFIX/bin
 cd ..
 
 echo "mapping compilation"
 cd mapping
-make all CPP=${CXX}  ILIB="-I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib"
-cp mapping $PREFIX/bin
+make all CPP=${CXX} ILIB="-I${PREFIX}/include -L${PREFIX}/lib"
+install -v -m 0755 mapping $PREFIX/bin
 cd ..
 
 echo "clame compilation"
-make all CPP=${CXX}  ILIB="-I${BUILD_PREFIX}/include -L${BUILD_PREFIX}/lib"
-cp clame $PREFIX/bin
+make all CPP=${CXX} ILIB="-I${PREFIX}/include -L${PREFIX}/lib"
+install -v -m 0755 clame $PREFIX/bin
 
 echo "Installation successful"
-
