e870d824df465b64912a4785ae807a8865952a8c
Nate Johnson <jax523@gmail.com>
2025-11-18 22:33:26 -0300

    Fixes for conda add
    Completed suggested changes in meta.yaml, including removing build.sh.
    
    Also pushed a new version of yasma to reflect minor changes shown here.

--- recipes/yasma/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# install using pip from the whl file provided by PyPi
-
-pip install https://files.pythonhosted.org/packages/75/45/54bb2d8d4138964a94bef6e9afe48b0be4705ba66ac442ae7d8a8dc4ffef/click_option_group-0.5.9-py3-none-any.whl
-
