e6feac5ad014bd5cbce5d70e3413a9b3a6d03f52
xfengnefx <61363437+xfengnefx@users.noreply.github.com>
2025-11-23 23:02:56 -0500

    Update build.sh
    not sure if -march was the reason but try borrowing the hifiasm build script here

--- recipes/hifiasm_meta/build.sh
+++ recipes/hifiasm_meta/build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-set -xe
 
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -L$PREFIX/lib"
+mkdir -p $PREFIX/bin
 
-mkdir -p "$PREFIX/bin"
-
-sed -i.bak 's|-lpthread|-pthread|' Makefile
+ARCH_OPTS=""
 case $(uname -m) in
     x86_64)
-	sed -i.bak 's|-msse4.2 -mpopcnt|-msse4.2 -mpopcnt -march=x86-64-v3|' Makefile
-  ;;
-    aarch64)
-	sed -i.bak 's|-msse4.2 -mpopcnt|-march=armv8-a|' Makefile
-	;;
-    arm64)
-	sed -i.bak 's|-msse4.2 -mpopcnt|-march=armv8.4-a|' Makefile
-	;;
+        ARCH_OPTS="-msse4.2 -mpopcnt"
+        ;;
+    *)
+        ;;
 esac
-rm -f *.bak
 
-make INCLUDES="-I$PREFIX/include" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS}" CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
-
+sed -i.bak 's/CXXFLAGS=.*//' Makefile
+rm -rf *.bak
+make INCLUDES="-I$PREFIX/include" CXXFLAGS="${CXXFLAGS} -L$PREFIX/lib -g -O3 ${ARCH_OPTS} -fomit-frame-pointer -Wall" CC="${CC}" "CXX=${CXX}"
 install -v -m 0755 hifiasm_meta "$PREFIX/bin"
