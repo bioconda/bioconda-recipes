cd85f00393aede678a98fa1ccb5798cac9c8c021
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-23 21:16:39 -0600

    Update build.sh

--- recipes/hifiasm_meta/build.sh
+++ recipes/hifiasm_meta/build.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-
 set -xe
 
-mkdir -p $PREFIX/bin
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -L$PREFIX/lib"
 
-make -j ${CPU_COUNT} INCLUDES="-I$PREFIX/include" CXXFLAGS="-L$PREFIX/lib" CC=${CC} CXX=${CXX}
-cp hifiasm_meta $PREFIX/bin
+mkdir -p "$PREFIX/bin"
+
+sed -i.bak 's|-lpthread|-pthread|' Makefile
+case $(uname -m) in
+    x86_64)
+	sed -i.bak 's|-msse4.2 -mpopcnt|-msse4.2 -mpopcnt -march=x86-64-v3|' Makefile
+  ;;
+    aarch64)
+	sed -i.bak 's|-msse4.2 -mpopcnt|-march=armv8-a|' Makefile
+	;;
+    arm64)
+	sed -i.bak 's|-msse4.2 -mpopcnt|-march=armv8.4-a|' Makefile
+	;;
+esac
+rm -f *.bak
+
+make INCLUDES="-I$PREFIX/include" CPPFLAGS="${CPPFLAGS}" CXXFLAGS="${CXXFLAGS}" CC="${CC}" CXX="${CXX}" -j"${CPU_COUNT}"
+
+install -v -m 0755 hifiasm_meta "$PREFIX/bin"
