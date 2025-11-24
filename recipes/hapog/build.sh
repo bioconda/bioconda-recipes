e9f839ac3d1c0d32d288fc7f1668f8cbdb82212c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-24 11:41:17 -0600

    hapog: add aarc64/arm64 builds (#60880)
    * hapog: add aarc64/arm64 builds
    
    * Update meta.yaml
    
    * Update meta.yaml

--- recipes/hapog/build.sh
+++ recipes/hapog/build.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 
 python setup.py install --single-version-externally-managed --record=record.txt
 
-mkdir hapog_build && cd hapog_build
-export HTSLIB_ROOT=${LIBRARY_PATH}
+mkdir -p build
+
+export HTSLIB_ROOT="${LIBRARY_PATH}"
 CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  ${CMAKE_PLATFORM_FLAGS[@]} \
-  ../src
-make
-cd ..
-cp -r hapog_build/hapog ${PREFIX}/bin/hapog_bin
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S src -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_COMPILER="$CC" \
+	-DCMAKE_CXX_COMPILER="$CXX" \
+	-DCMAKE_C_FLAGS="${CFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	${CMAKE_PLATFORM_FLAGS[@]} \
+	"${CONFIG_ARGS}"
+
+cmake --build build -j "${CPU_COUNT}"
+
+install -v -m 0755 build/hapog ${PREFIX}/bin/hapog_bin
