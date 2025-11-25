f86281a1041db6da6ab48992e9c667cea3ecdce8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-24 19:42:38 -0600

    Update build.sh

--- recipes/seqprep/build.sh
+++ recipes/seqprep/build.sh
@@ -3,7 +3,9 @@
 mkdir -p "${PREFIX}/bin"
 
 case $(uname -m) in
-	aarch64|arm64) sed -i.bak "s|-mssse3||g" Makefile && rm -f *.bak ;;
+	aarch64) sed -i.bak "s|-march=core2 -mssse3|-march=armv8-a|g" Makefile && rm -f *.bak ;;
+	arm64) sed -i.bak "s|-march=core2 -mssse3|-march=armv8.4-a|g" Makefile && rm -f *.bak ;;
+	x86_64) sed -i.bak "s|-march=core2 -mssse3|-march=x86-64 -mssse3|g" Makefile && rm -f *.bak ;;
 esac
 
 make CC="${CC}"
