2370764a57455baf374b86e6f86d8f0f8e42330b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-25 14:20:14 -0600

    Update build.sh

--- recipes/fasttree/build.sh
+++ recipes/fasttree/build.sh
@@ -16,14 +16,14 @@ case $(uname -m) in
 esac
 
 # build FastTree
-${CC} "-I${PREFIX}/include" "-L${PREFIX}/lib" "${EXTRA_ARGS}" -O3 -funsafe-math-optimizations -o FastTree FastTree.c -lm
+${CC} -I${PREFIX}/include -L${PREFIX}/lib ${EXTRA_ARGS} -O3 -funsafe-math-optimizations -o FastTree FastTree.c -lm
 install -v -m 0755 FastTree "${PREFIX}/bin"
 
 # some OS are not case-sensitive (macOS, by default), ignore the copy error there
 cp -f -- $PREFIX/bin/FastTree $PREFIX/bin/fasttree || True
 
 # Build FastTreeMP
-${CC} "-I${PREFIX}/include" "-L${PREFIX}/lib" "${EXTRA_ARGS}" -DOPENMP -O3 -fopenmp -funsafe-math-optimizations -o FastTreeMP FastTree.c -lm
+${CC} -I${PREFIX}/include -L${PREFIX}/lib ${EXTRA_ARGS} -DOPENMP -O3 -fopenmp -funsafe-math-optimizations -o FastTreeMP FastTree.c -lm
 install -v -m 0755 FastTreeMP "${PREFIX}/bin"
 
 # https://github.com/bioconda/bioconda-recipes/pull/60340#issuecomment-3560431036
