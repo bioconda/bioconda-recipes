4a412db41af09d230e992d6128f13738a259fc77
mencian <joshua.zhuang@yahoo.com>
2025-11-24 19:32:26 -0600

    Rebuild seqprep recipe

--- recipes/seqprep/build.sh
+++ recipes/seqprep/build.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p ${PREFIX}/bin
-make
-cp SeqPrep ${PREFIX}/bin
+mkdir -p "${PREFIX}/bin"
+
+case $(uname -m) in
+	aarch64|arm64) sed -i.bak "s|-mssse3||g" Makefile && rm -f *.bak ;;
+esac
+
+make CC="${CC}"
+
+install -v -m 0755 SeqPrep "${PREFIX}/bin"
