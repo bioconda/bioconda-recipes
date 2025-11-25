b415e1bfd99b118bd54784e936bdfd1c981b2075
mencian <joshua.zhuang@yahoo.com>
2025-11-25 13:30:13 -0600

    edit patch

--- recipes/smudgeplot/build.sh
+++ recipes/smudgeplot/build.sh
@@ -2,12 +2,12 @@
 
 export CPATH="${PREFIX}/include"
 
-mkdir -p $PREFIX/bin
+mkdir -p "$PREFIX/bin"
 
 make -s INSTALL_PREFIX="${PREFIX}" CC="${CC}" -j"${CPU_COUNT}"
 
 # Install executables
-install -C exec/smudgeplot.py $PREFIX/bin
-install -C exec/hetmers $PREFIX/bin
-install -C exec/smudgeplot_plot.R $PREFIX/bin
-install -C exec/centrality_plot.R $PREFIX/bin
+install -v -m 0755 -C exec/smudgeplot.py "$PREFIX/bin"
+install -v -m 0755 -C exec/hetmers "$PREFIX/bin"
+install -v -m 0755 -C exec/smudgeplot_plot.R "$PREFIX/bin"
+install -v -m 0755 -C exec/centrality_plot.R "$PREFIX/bin"
