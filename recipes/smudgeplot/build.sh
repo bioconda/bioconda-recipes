f4c02cc8e018ec0844233ed4e77dd7ae723ec34b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-26 21:49:38 -0600

    Update build.sh

--- recipes/smudgeplot/build.sh
+++ recipes/smudgeplot/build.sh
@@ -5,6 +5,8 @@ export CPATH="${PREFIX}/include"
 mkdir -p "$PREFIX/bin"
 mkdir -p exec
 
+${PYTHON} -m pip install . -vvv --no-cache-dir --no-build-isolation --no-cache-dir
+
 make -s INSTALL_PREFIX="${PREFIX}" CC="${CC}" -j"${CPU_COUNT}" install
 
 # Install executables
