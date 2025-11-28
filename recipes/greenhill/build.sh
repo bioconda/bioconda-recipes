853d959b5c62dc4b2b3c985f6fd585eaa75861af
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-11-28 00:08:33 -0600

    Update build.sh

--- recipes/greenhill/build.sh
+++ recipes/greenhill/build.sh
@@ -8,10 +8,10 @@ mkdir -p "$PREFIX/bin"
 
 case $(uname -m) in
     aarch64)
-	sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' src/Makefile && rm -f src/*.bak
+	sed -i.bak 's|-march=x86-64|-march=armv8-a|' src/Makefile && rm -f src/*.bak
 	;;
     arm64)
-	sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' src/Makefile && rm -f src/*.bak
+	sed -i.bak 's|-march=x86-64|-march=armv8.4-a|' src/Makefile && rm -f src/*.bak
 	;;
 esac
 
