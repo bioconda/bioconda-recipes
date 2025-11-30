74b94637e0e77340305594ea26152835d18cd1eb
lcoombe <lauren.e.coombe@gmail.com>
2025-11-29 16:08:41 -0800

    remove added ldflag command

--- recipes/ntsynt/build.sh
+++ recipes/ntsynt/build.sh
@@ -2,8 +2,6 @@
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
-export LDFLAGS="${LDFLAGS} -Wl,-headerpad_max_install_names"
-
 # Build ntSynt
 mkdir -p ${PREFIX}/bin
 meson setup build --prefix ${PREFIX}
