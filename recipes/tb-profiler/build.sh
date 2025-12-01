1b619a20e5b4d51940cbd4497078e8ab09516dfd
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-12-01 21:04:49 +0100

    fix build

--- recipes/tb-profiler/build.sh
+++ recipes/tb-profiler/build.sh
@@ -4,6 +4,6 @@ python -m pip install --no-deps --ignore-installed .
 cd db
 samtools  dict tbdb/genome.fasta -o tbdb/genome.dict
 samtools faidx tbdb/genome.fasta
-bwa index faidx tbdb/genome.fasta
+bwa index tbdb/genome.fasta
 python ../tb-profiler load_library tbdb --force
 bwa index $PREFIX/share/tbprofiler/tbdb.fasta
