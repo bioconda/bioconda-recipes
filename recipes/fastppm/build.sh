4b3e7a2cd4f629c109b5c3b72a207c1b6e741c95
Mohammed El-Kebir <melkebir@illinois.edu>
2025-12-04 16:50:13 -0600

    Fixing linking issues

--- recipes/fastppm/build.sh
+++ recipes/fastppm/build.sh
@@ -5,9 +5,13 @@ cd build
 
 # We pass -DFASTPPM_VERSION=$PKG_VERSION so CMake knows the version
 # without needing a git history (which conda builds often lack).
+# Fix for macOS: explicitly pass the AR and RANLIB tools set by Conda
+# so CMake doesn't find broken ones in the package cache.
 cmake -DPYTHON_EXECUTABLE=$PYTHON \
       -DCMAKE_BUILD_TYPE=RELEASE \
       -DFASTPPM_VERSION=$PKG_VERSION \
+      -DCMAKE_AR="${AR}" \
+      -DCMAKE_RANLIB="${RANLIB}" \
       ..
 
 make -j${CPU_COUNT}
