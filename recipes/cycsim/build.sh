97589a0c4c04eb3f3e40a88ab613113c968a302a
huj <mooldhu@gmail.com>
2025-12-05 21:53:07 +0800

    remove PKG_CONFIG_PATH

--- recipes/cycsim/build.sh
+++ recipes/cycsim/build.sh
@@ -3,7 +3,6 @@
 export CARGO_HOME="${SRC_DIR}/.cargo"
 export CMAKE_POLICY_VERSION_MINIMUM=3.5
 
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
 export INCLUDE_PATH="${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
@@ -14,13 +13,8 @@ if [ ! -f Cargo.lock ]; then
     cargo generate-lockfile
 fi
 
-cargo fetch --locked --verbose
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-cargo build --release --locked --verbose
-cargo install --path . --root "${PREFIX}" --locked --verbose
-
+RUST_BACKTRACE=1 cargo install --no-track --locked --verbose --path . --root "${PREFIX}"
 
 rm -rf "${CARGO_HOME}"
-
-
