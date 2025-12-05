c34ec7d47b545f8a5f28be3734b266307c6f61ca
Mohammed El-Kebir <melkebir@illinois.edu>
2025-12-05 07:34:49 -0600

    Update fastppm to 1.1.1 (#61129)
    * Update fastppm to v1.1.0
    
    * Updated meta.yaml to include run_exports and use manual tarball
    
    * Removing 'script:'
    
    * Added ARM
    
    * Added ARM support
    
    * Fixing linking issues
    
    * Pinning to Clang version 16
    
    * Remove clashing llvm-ar and llvm-ranlib

--- recipes/fastppm/build.sh
+++ recipes/fastppm/build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# FIX for broken Clang 18 on macOS:
+# The llvm-tools package provides broken 'llvm-ar' and 'llvm-ranlib' binaries
+# that crash with dyld errors. We delete them so CMake finds the standard 'ar'
+# and 'ranlib' provided by cctools instead.
+if [[ "$(uname)" == "Darwin" ]]; then
+    rm -f $BUILD_PREFIX/bin/llvm-ar
+    rm -f $BUILD_PREFIX/bin/llvm-ranlib
+fi
+
 mkdir -p build
 cd build
 
