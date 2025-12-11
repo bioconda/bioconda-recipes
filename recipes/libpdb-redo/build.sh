fc478f826edd010f9aa0f8c373880a2a4e56b681
UENO, M. <eunosium-1128@live.jp>
2025-12-11 23:37:43 +0900

    Refactor build.sh for platform-specific configurations

--- recipes/libpdb-redo/build.sh
+++ recipes/libpdb-redo/build.sh
@@ -12,11 +12,10 @@ export CMAKE_THREAD_LIBS_INIT="-lpthread"
 export CMAKE_USE_PTHREADS_INIT=ON
 export CMAKE_HAVE_LIBC_PTHREAD=ON
 
-if [[ "${target_platform}" == "osx-"* ]]; then
-    export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-else
+if [[ "${target_platform}" == "linux-"* ]]; then
     export CONFIG_ARGS=""
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+elif [[ "${target_platform}" == "osx-"* ]]; then
+    export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
 fi
 
 cmake -S . -B build -G Ninja \
@@ -24,6 +23,8 @@ cmake -S . -B build -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
+    -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
     -DFETCHCONTENT_FULLY_DISCONNECTED=ON \
     -DTHREADS_PREFER_PTHREAD_FLAG=ON \
     -DBUILD_SHARED_LIBS=ON \
