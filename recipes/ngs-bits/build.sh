d6e01df6789e7d7d4c2b2bf3be038ea0648edca0
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2025-12-11 10:31:24 +0100

    refactoring

--- recipes/ngs-bits/build.sh
+++ recipes/ngs-bits/build.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 set -x
 
-#link global folders in to ngs-bits htslib folders (there the htslib is installed by conda)
+#link global include/lib folders, where htslib is installed by conda, into ngs-bits htslib folder
 ln -s $PREFIX/include htslib/include
 ln -s $PREFIX/lib htslib/lib
 
 # Ensure pkg-config can find libxml2 (conda-forge installs .pc into $BUILD_PREFIX)
-export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig:${PREFIX}/lib/pkgconfig"
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${BUILD_PREFIX}/lib/pkgconfig"
 pkg-config --cflags libxml-2.0
 pkg-config --libs libxml-2.0
 
