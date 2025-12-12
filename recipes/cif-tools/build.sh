5892372470a10acbfeabbfc625c416d1ff6dc870
UENO, M. <eunosium-1128@live.jp>
2025-12-12 11:26:46 +0900

    Update CMake configuration for mcfp package

--- recipes/cif-tools/build.sh
+++ recipes/cif-tools/build.sh
@@ -12,15 +12,12 @@ else
   export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
 fi
 
-sed -i.bak 's|find_package(libmcfp REQUIRED)|find_package(mcfp REQUIRED)|' CMakeLists.txt
-sed -i.bak 's|libmcfp::libmcfp|mcfp::mcfp|g' CMakeLists.txt
-
 cmake -S . -B build -G Ninja \
   ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
   -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
   -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-  -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp" \
+  -Dmcfp_DIR="${PREFIX}/lib/cmake/mcfp" \
   -Dcifpp_DIR="${PREFIX}/lib/cmake/cifpp"
 
 cmake --build build --parallel "${CPU_COUNT}"
