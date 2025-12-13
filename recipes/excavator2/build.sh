aef8b35037a184701faf878ad23cea74552a9ef9
danilotat <danilotatoni@gmail.com>
2025-12-13 17:07:27 +0100

    upd: version update, fortran compiling

--- recipes/excavator2/build.sh
+++ recipes/excavator2/build.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+
 DEST_DIR="${PREFIX}/share/${PKG_NAME}"
 mkdir -p "${DEST_DIR}"
-SOURCE_SUBDIR="${SRC_DIR}/${PKG_NAME}"
-# copy in share
+
+SOURCE_SUBDIR="excavator2"
+
+find "${SOURCE_SUBDIR}" -name "*.so" -delete
+find "${SOURCE_SUBDIR}" -name "*.o" -delete
+
 cp -r "${SOURCE_SUBDIR}"/* "${DEST_DIR}/"
-for lib_file in ${DEST_DIR}/lib/F77/*.f ; do
-       R CMD SHLIB $lib_file 
+
+cd "${DEST_DIR}/lib/F77"
+
+for lib_file in *.f; do
+    echo "Compiling ${lib_file}..."
+    R CMD SHLIB "${lib_file}"
 done
+
+cd -
+
 chmod -R a+rX "${DEST_DIR}"
-# symlink them
+
 mkdir -p "${PREFIX}/bin"
+
 for exe in EXCAVATORDataAnalysis.pl EXCAVATORDataPrepare.pl TargetPerla.pl; do
-  # Make the source files executable *before* linking
   chmod +x "${DEST_DIR}/${exe}"
   ln -s "${DEST_DIR}/${exe}" "${PREFIX}/bin/${exe}"
 done
