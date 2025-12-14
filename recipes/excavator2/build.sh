1079bab5716de2fe4354f0f2df155bb75261be54
danilotat <danilotatoni@gmail.com>
2025-12-14 12:00:04 +0100

    fix: minor update

--- recipes/excavator2/build.sh
+++ recipes/excavator2/build.sh
@@ -2,16 +2,14 @@
 set -euxo pipefail
 
 DEST_DIR="${PREFIX}/share/${PKG_NAME}"
-mkdir -p "${DEST_DIR}"
 SOURCE_SUBDIR="excavator2"
 
+mkdir -p "${DEST_DIR}"
 mkdir -p ~/.R
 
-# Create Makevars - don't set FLIBS in Makevars, let R handle it
+# Let R/conda manage compilers, only add legacy flags
 cat <<EOF > ~/.R/Makevars
-F77 = \${FC}
-FC = \${FC}
-FFLAGS = \${FFLAGS} -std=legacy -ffixed-line-length-none -w
+FFLAGS += -std=legacy -ffixed-line-length-none -w
 EOF
 
 find "${SOURCE_SUBDIR}" -name "*.so" -delete
@@ -21,19 +19,20 @@ cp -r "${SOURCE_SUBDIR}"/* "${DEST_DIR}/"
 
 cd "${DEST_DIR}/lib/F77"
 
-# Only override FFLAGS to add Fortran 77 specific flags
 export FFLAGS="${FFLAGS} -std=legacy -ffixed-line-length-none -w"
 
-# compile libs
 for lib_file in *.f; do
     echo "Compiling ${lib_file}..."
     R CMD SHLIB "${lib_file}"
 done
+
 cd -
 
 chmod -R a+rX "${DEST_DIR}"
+
 mkdir -p "${PREFIX}/bin"
 for exe in EXCAVATORDataAnalysis.pl EXCAVATORDataPrepare.pl TargetPerla.pl; do
   chmod +x "${DEST_DIR}/${exe}"
   ln -s "${DEST_DIR}/${exe}" "${PREFIX}/bin/${exe}"
 done
+
