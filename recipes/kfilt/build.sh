0d8e3e39323ac4b59222546b80c59d5c04af725d
AndreaGuarracino <andreaguarracino@outlook.com>
2025-12-24 13:35:20 -0700

    simplify build.sh for kfilt

--- recipes/kfilt/build.sh
+++ recipes/kfilt/build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-set -o xtrace -o nounset -o pipefail -o errexit
-
-export CGO_ENABLED=0
-export GOPATH=$PWD
-export GOCACHE=$PWD/.cache/
-
-mkdir -p "${GOCACHE}"
-mkdir -p "${PREFIX}/bin"
-
+mkdir -p $PREFIX/bin
 go mod init kfilt
 go mod tidy
-go build -trimpath -o=${PREFIX}/bin/kfilt -ldflags="-extldflags '-static' -s -w" -tags netgo
+go get -d ./...
+CGO_ENABLED=0 go build -o $PREFIX/bin/kfilt .
