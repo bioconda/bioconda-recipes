8ef41fd2604030c6507e9f3073750affd4a98b7f
JiaQHo <a582749990@gmail.com>
2026-01-16 10:38:41 +0800

    Add hquate version 1.0.6

--- recipes/hquate/build.sh
+++ recipes/hquate/build.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# if ! [ -z "${CONDA_BUILD+x}" ]; then
-#    echo "### exec ! if"    
-#    find /share/home/zju_hjy/conda-bld/ -name "build_env_setup.sh" -type f -exec ls -t {} +
-#    find /share/home/zju_hjy/conda-bld/ -name "build_env_setup.sh" -type f -exec ls -t {} + | head -1
-#    sourcefile=$(find /share/home/zju_hjy/conda-bld/ -name "build_env_setup.sh" -type f -exec ls -t {} + | head -1)
-#    echo ${sourcefile}
-#    source ${sourcefile}
-# fi
-
-# echo "###" ${CONDA_BUILD+x}
-# echo "SRC_DIR (源码位置_260114): ${SRC_DIR}"
-# echo "PREFIX (安装目标260114): ${PREFIX}"
-
-# export PREFIX=/share/home/zju_hjy/conda-bld/hquate_1768380444575/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placeho
-# export SRC_DIR=/share/home/zju_hjy/conda-bld/hquate_1768380444575/work
-
-
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/examples
 
@@ -28,7 +11,6 @@ cp bin/ByJointProbability.sh ${PREFIX}/bin/
 cp bin/ByTEAnnotation.sh ${PREFIX}/bin/
 cp bin/ByTEConsensus_SubFamily.sh ${PREFIX}/bin/
 cp bin/ByTESubfamily.sh ${PREFIX}/bin/
-cp bin/ByTETranscript.sh ${PREFIX}/bin/
 cp bin/ERVFamily ${PREFIX}/bin/
 cp bin/expTEsub_cattle ${PREFIX}/bin/
 cp bin/expTEsub_chicken ${PREFIX}/bin/
@@ -37,13 +19,12 @@ cp bin/expTEsub_pig ${PREFIX}/bin/
 cp bin/expTEsub_sheep ${PREFIX}/bin/
 cp bin/filterGFF ${PREFIX}/bin/
 cp bin/insideConnection.sh ${PREFIX}/bin/
-cp bin/LocaTE ${PREFIX}/bin/
-# cp bin/locaTE ${PREFIX}/bin/
-cp bin/LocaTE ${PREFIX}/bin/locaTE
+cp bin/HQuaTE ${PREFIX}/bin/HQuaTE
+cp bin/HQuaTE ${PREFIX}/bin/hquate
 cp examples/test.bam ${PREFIX}/examples/
 cp examples/testTE.gff ${PREFIX}/examples/
 
-chmod +x ${PREFIX}/bin/locaTE
-chmod +x ${PREFIX}/bin/LocaTE
+chmod +x ${PREFIX}/bin/HQuaTE
+chmod +x ${PREFIX}/bin/hquate
 chmod +x ${PREFIX}/bin/filterGFF
 
