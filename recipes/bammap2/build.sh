86efa6532aabe7415b70777c6b1da220b66c519e
wangyibin <738339463@qq.com>
2026-01-18 14:48:52 +0800

    Update recipe for bammap2

--- recipes/bammap2/build.sh
+++ recipes/bammap2/build.sh
@@ -6,8 +6,9 @@ export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
 export BINDGEN_EXTRA_CLANG_ARGS="${CFLAGS} ${CPPFLAGS} ${LDFLAGS}"
 
-# cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+unset MAKEFLAGS
 RUST_BACKTRACE=1
 cargo install --no-track -v --root "${PREFIX}" --path .
 
