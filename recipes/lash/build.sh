0645f1f054a80585c2a8cb4fc550e5114108748f
clairewowo <claire.ze.wang@gmail.com>
2026-01-22 17:54:50 -0800

    nightly rust

--- recipes/lash/build.sh
+++ recipes/lash/build.sh
@@ -4,4 +4,15 @@ set -xeuo
 
 # cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+export PATH=$PREFIX/bin:$PATH
+
+# install rustup
+if ! command -v rustup &> /dev/null; then
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    export PATH=$HOME/.cargo/bin:$PATH
+fi
+
+rustup install nightly
+rustup default nightly
+
 RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX --no-track
