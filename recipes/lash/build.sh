ee3522595a1e8d49fa78021c9f872541a747dc83
clairewowo <claire.ze.wang@gmail.com>
2026-01-22 17:32:48 -0800

    fixing build.sh

--- recipes/lash/build.sh
+++ recipes/lash/build.sh
@@ -2,6 +2,6 @@
 
 set -xeuo
 
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+# cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX --no-track
