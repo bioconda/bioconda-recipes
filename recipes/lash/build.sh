6bd87805df9887df1c63588d2e95fd58b7e43df1
clairewowo <claire.ze.wang@gmail.com>
2026-01-22 18:32:27 -0800

    fix another nightly rust issue

--- recipes/lash/build.sh
+++ recipes/lash/build.sh
@@ -6,13 +6,6 @@ set -xeuo
 
 export PATH=$PREFIX/bin:$PATH
 
-# install rustup
-if ! command -v rustup &> /dev/null; then
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
-    export PATH=$HOME/.cargo/bin:$PATH
-fi
-
-rustup install nightly
-rustup default nightly
+export RUSTUP_TOOLCHAIN=nightly
 
 RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX --no-track
