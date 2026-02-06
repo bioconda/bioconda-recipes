08e373889c3d06827a61ac695afcea10ac368465
mkiyer <matthew.iyer@gmail.com>
2026-02-06 16:57:08 -0500

    add v0.1.7

--- recipes/zna/build.sh
+++ recipes/zna/build.sh
@@ -9,4 +9,4 @@ ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
 
 # Verify the installation
 ${PYTHON} -c "import zna; print(f'ZNA version: {zna.__version__ if hasattr(zna, \"__version__\") else \"0.1.0\"}')"
-${PYTHON} -c "from zna.core import is_accelerated; print(f'C++ acceleration: {is_accelerated()}')"
+${PYTHON} -c "from zna import is_accelerated; print(f'C++ acceleration: {is_accelerated()}')"
