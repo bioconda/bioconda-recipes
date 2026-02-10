87366b8d5a3c817e67e98447bfc77ece570a80cc
Enrico Seiler <enrico.seiler@hotmail.de>
2026-02-10 18:54:43 +0100

    review

--- recipes/ganon/build.sh
+++ recipes/ganon/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# Install python packages
-cd ${SRC_DIR}/ganon/
-# Link installed genome_updater.sh in the submodule dir to properly install script with pip
-ln -s $(whereis genome_updater.sh | cut -d " " -f 2) ${SRC_DIR}/ganon/libs/genome_updater/genome_updater.sh
-$PYTHON -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
+set -xe
+
+# Link installed genome_updater.sh (dependency) into ganon's submodule dir
+# to properly install script with pip
+ln -s "$(command -v genome_updater.sh)" libs/genome_updater/genome_updater.sh
+${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
 
 # Build and install cpp packages
-mv ${SRC_DIR}/robin-hood-hashing ${SRC_DIR}/ganon/libs/
-mkdir build_cpp && cd build_cpp
-cmake -DCMAKE_BUILD_TYPE=Release -DVERBOSE_CONFIG=ON -DCONDA=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
-make
-make install
+cmake -S . \
+      -B build_cpp \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DVERBOSE_CONFIG=ON \
+      -DCONDA=ON \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+
+make -C build_cpp -j "${CPU_COUNT}"
+make -C build_cpp install
 
 # Test cpp
-ctest -VV .
+ctest --test-dir build_cpp \
+      --output-on-failure \
+      --no-tests=error \
+      --parallel "${CPU_COUNT}"
+
 # Test python
-cd ${SRC_DIR}/ganon/
-$PYTHON -m unittest discover -s tests/ganon/integration/ -v
+${PYTHON} -m unittest discover -s tests/ganon/integration/ -v
