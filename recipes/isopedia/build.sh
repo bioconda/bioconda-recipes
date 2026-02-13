9fdb5fcf596569216c852e7ff2d822e6046bfd64
Xinchang Zheng <16239108+zhengxinchang@users.noreply.github.com>
2026-02-13 11:45:48 -0600

    Fix chmod command for isopedia-splice-viz.py

--- recipes/isopedia/build.sh
+++ recipes/isopedia/build.sh
@@ -7,6 +7,6 @@ cp isopedia ${PREFIX}/bin
 cp isopedia-tools ${PREFIX}/bin
 cp isopedia-splice-viz.py ${PREFIX}/bin
 cp isopedia-splice-viz-temp.html ${PREFIX}/bin
-chmod +x ${$PREFIX}/bin/isopedia-splice-viz.py
+chmod +x ${PREFIX}/bin/isopedia-splice-viz.py
 chmod +x ${PREFIX}/bin/isopedia
 chmod +x ${PREFIX}/bin/isopedia-tools
