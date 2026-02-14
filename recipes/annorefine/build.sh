4d6571c78b758ba41f2c9740e34c03d09f977957
Jon Palmer <nextgenusfs@gmail.com>
2026-02-14 14:27:29 -0800

    \Use maturin build directly instead of pip install to fix cross-compilation for linux-aarch64\

--- recipes/annorefine/build.sh
+++ recipes/annorefine/build.sh
@@ -13,9 +13,15 @@ export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${PKG_
 export C_INCLUDE_PATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
 
+# Set Rust backtrace for better error messages
+export RUST_BACKTRACE=1
+
 # Bundle licenses for Rust dependencies
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-# Install using pip
-${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
+# Build the package using maturin - should produce *.whl files
+maturin build --interpreter "${PYTHON}" -b pyo3 --release --strip --manylinux off
+
+# Install *.whl files using pip
+${PYTHON} -m pip install target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vv
 
