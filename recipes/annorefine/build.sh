49fb6249b444fa99ccea0360358b81b34b73fd0a
Jon Palmer <nextgenusfs@gmail.com>
2026-02-14 13:52:53 -0800

    Add PKG_CONFIG_PATH and C_INCLUDE_PATH/LIBRARY_PATH to build.sh for better OpenSSL detection

--- recipes/annorefine/build.sh
+++ recipes/annorefine/build.sh
@@ -7,6 +7,11 @@ export OPENSSL_NO_VENDOR=1
 export OPENSSL_DIR=$PREFIX
 export OPENSSL_INCLUDE_DIR=$PREFIX/include
 export OPENSSL_LIB_DIR=$PREFIX/lib
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${PKG_CONFIG_PATH:-}"
+
+# Set C include and library paths for Rust build
+export C_INCLUDE_PATH=$PREFIX/include
+export LIBRARY_PATH=$PREFIX/lib
 
 # Bundle licenses for Rust dependencies
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
