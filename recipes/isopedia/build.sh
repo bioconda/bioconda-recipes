3979540e0a5dba8428dc2784dfce47cf785d260f
xinchang zheng <zhengxc93@gmail.com>
2026-02-13 20:29:47 -0600

    update

--- recipes/isopedia/build.sh
+++ recipes/isopedia/build.sh
@@ -4,5 +4,5 @@ mkdir -p ${PREFIX}/bin
 install -m 755 isopedia "$PREFIX/bin/"
 install -m 755 isopedia-tools "$PREFIX/bin/"
 install -m 755 isopedia-splice-viz.py "$PREFIX/bin/"
-mkdir -p "$PREFIX/share/isopedia"
+mkdir -p "$PREFIX/share/isopedia" 
 install -m 644 isopedia-splice-viz-temp.html "$PREFIX/share/isopedia/"
