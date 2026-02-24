b5aaa3392a4595b4f3e29d58561a5f8b48405c0b
RunpengLuo <runpeng.luo@princeton.edu>
2026-02-24 03:16:58 -0500

    Update Gurobi version to 12.0.3 and adjust build scripts and metadata accordingly.

--- recipes/hatchet/build.sh
+++ recipes/hatchet/build.sh
@@ -5,21 +5,21 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 # Compile Gurobi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	sudo installer -pkg gurobi9.0.2_mac64.pkg -target /
+	sudo installer -pkg gurobi12.0.3_macos_universal2.pkg -target /
 
-	export GUROBI_HOME=/Library/gurobi902
+	export GUROBI_HOME=/Library/gurobi1203
 	mkdir -p $PREFIX/lib
-	cp -rf "$GUROBI_HOME/mac64/lib/libgurobi90.dylib" "$PREFIX/lib"
+	cp -rf "$GUROBI_HOME/macos_universal2/lib/libgurobi120.dylib" "$PREFIX/lib"
 else
 	# Gurobi Makefile uses a variable called 'C++'
 	# Rename this inline to 'CPP' so we can override it with the correct compiler on 'make'
-	sed -i.bak 's/C++/CPP/g' gurobi902/linux64/src/build/Makefile
-	(cd gurobi902/linux64/src/build && make CPP=${CXX})
-	(cd gurobi902/linux64/lib && ln -fs ../src/build/libgurobi_c++.a libgurobi_c++.a)
+	sed -i.bak 's/C++/CPP/g' gurobi1203/linux64/src/build/Makefile
+	(cd gurobi1203/linux64/src/build && make CPP=${CXX})
+	(cd gurobi1203/linux64/lib && ln -fs ../src/build/libgurobi_c++.a libgurobi_c++.a)
 
-	export GUROBI_HOME="$(cd gurobi902 && pwd)"
+	export GUROBI_HOME="$(cd gurobi1203 && pwd)"
 	mkdir -p $PREFIX/lib
-	cp -rf "$GUROBI_HOME/linux64/lib/libgurobi90.so" "$PREFIX/lib"
+	cp -rf "$GUROBI_HOME/linux64/lib/libgurobi120.so" "$PREFIX/lib"
 fi
 
 export CXXFLAGS="${CXXFLAGS} -O3 -pthread -I${PREFIX}/include ${LDFLAGS}"
