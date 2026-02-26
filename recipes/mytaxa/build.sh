f4fe37769f2f2b4851cefe7f0bb3362124a04c6a
Luis M Rodriguez-R <miguel@rodriguez-r.com>
2026-02-26 18:03:12 +0100

    Fix Linux build: use conda CXX compiler instead of hardcoded g++

--- recipes/mytaxa/build.sh
+++ recipes/mytaxa/build.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+
+# Make sure the conda-provided C++ compiler is used
+# Replace any hard-coded 'g++' with '$(CXX)' in the Makefile
+# (portable sed usage for both GNU and BSD sed)
+if grep -qE '(^|\s)g\+\+(\s|$)' Makefile; then
+  sed -E -i.bak 's/(^|[[:space:]])g\+\+([[:space:]]|$)/\1\$(CXX)\2/g' Makefile
+fi
+
 # Build MyTaxa
-make
+make CXX="${CXX:-c++}"
 
 # Install MyTaxa binaries into the conda prefix
 mkdir -p "${PREFIX}/bin"
-
-# MyTaxa builds a binary named "mytaxa"
-# If this changes later, adjust this line accordingly
 cp -v MyTaxa "${PREFIX}/bin/"
+
+# Install utils
 mkdir -p "${PREFIX}/share/mytaxa"
 cp -rv utils "${PREFIX}/share/mytaxa/"
 
