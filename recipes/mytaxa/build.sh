1339dcffdea7a733be6fcc908254eb03d983bd0c
Luis M Rodriguez-R <miguel@rodriguez-r.com>
2026-02-26 18:18:37 +0100

    Fix Linux build: pass CC=${CXX} to make

--- recipes/mytaxa/build.sh
+++ recipes/mytaxa/build.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-
-# Make sure the conda-provided C++ compiler is used
-# Replace any hard-coded 'g++' with '$(CXX)' in the Makefile
-# (portable sed usage for both GNU and BSD sed)
-if grep -qE '(^|\s)g\+\+(\s|$)' Makefile; then
-  sed -E -i.bak 's/(^|[[:space:]])g\+\+([[:space:]]|$)/\1\$(CXX)\2/g' Makefile
-fi
-
 # Build MyTaxa
-make CXX="${CXX:-c++}"
+make CC="${CXX:-c++}"
 
 # Install MyTaxa binaries into the conda prefix
 mkdir -p "${PREFIX}/bin"
