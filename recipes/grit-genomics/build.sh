044c81e0425ab0272fcadc6c5fea70b2f3e94bd5
manish59 <manishkumar.bobbili3@gmail.com>
2026-02-26 17:29:45 -0700

    yup

--- recipes/grit-genomics/build.sh
+++ recipes/grit-genomics/build.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-set -ex
-
-# Build the release binary
-cargo build --release
-
-# Install the binary
-mkdir -p $PREFIX/bin
-cp target/release/grit $PREFIX/bin/
+                                                                                                                                                                                                           
+  #!/bin/bash                                                                                                                                                                                              
+  set -ex                                                                                                                                                                                                  
+                                                                                                                                                                                                           
+  cargo install --no-track --locked --root "$PREFIX" --path .   
