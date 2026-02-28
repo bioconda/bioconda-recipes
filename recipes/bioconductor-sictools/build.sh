3530571c6e5cfc1d21f0950de07d2812fd03cfe3
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-02-28 20:46:24 +0100

    fix

--- recipes/bioconductor-sictools/build.sh
+++ recipes/bioconductor-sictools/build.sh
@@ -2,6 +2,9 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPATH=${PREFIX}/include
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX
