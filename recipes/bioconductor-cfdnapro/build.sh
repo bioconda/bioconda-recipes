f323493fd28dabf0481bf882494928f0e12cce33
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-03-03 22:32:41 +0100

    add more fixes

--- recipes/bioconductor-cfdnapro/build.sh
+++ recipes/bioconductor-cfdnapro/build.sh
@@ -2,6 +2,10 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
+# plyranges no longer exports filter; use dplyr::filter for this call site.
+sed -i '/importFrom(plyranges,filter)/d' NAMESPACE
+sed -i 's/plyranges::filter/dplyr::filter/g' R/read_bam_insert_metrics.R
+
 echo -e "CC=$CC
 FC=$FC
 CXX=$CXX
