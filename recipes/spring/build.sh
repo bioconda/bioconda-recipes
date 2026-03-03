3801f66d494be7cc90067f2d521589581020f0a9
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-03-03 22:07:46 +0100

    fixes

--- recipes/spring/build.sh
+++ recipes/spring/build.sh
@@ -8,6 +8,7 @@ export CXXPATH=${PREFIX}/include
 export CFLAGS="$CFLAGS -I$PREFIX/include"
 export CXXFLAGS="$CFLAGS -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+sed -i '/#include <fstream>/a #include <cstdint>' src/util.h
 if [ $(arch) == "aarch64" ]; then
    cmake ..
   else
