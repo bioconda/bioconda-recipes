ea9cf87264e55b13dbc1730299e7b0d7a017e255
Khaled196 <56037954+khaled196@users.noreply.github.com>
2026-03-03 12:14:52 +0000

    Update build.sh

--- recipes/beacon2-ri-tools/build.sh
+++ recipes/beacon2-ri-tools/build.sh
@@ -10,7 +10,6 @@ cp -r "$SRC_DIR"/{*.py,ref_schemas,conf,files,validators} \
 
 # Ensure package recognition
 touch "$PREFIX/bin/beacon2-ri-tools/validators/__init__.py"
-touch "$PREFIX/bin/beacon2-ri-tools/validators/update/__init__.py"
 
 # List of supported v2 entrypoints
 scripts=(
