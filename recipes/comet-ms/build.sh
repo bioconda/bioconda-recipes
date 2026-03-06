28b6c0f606b1c65b5b55887f5b800ecb73d77551
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-03-06 21:22:55 +0100

    new R packages

--- recipes/comet-ms/build.sh
+++ recipes/comet-ms/build.sh
@@ -2,14 +2,15 @@
 set -e
 set -x
 
-export INCLUDE_PATH="${PREFIX}/MSToolkit/include"
-export CPPFLAGS="-I${PREFIX}/MSToolkit/include"
+export INCLUDE_PATH="${SRC_DIR}/MSToolkit/include"
+export CPPFLAGS="${CPPFLAGS} -I${SRC_DIR}/MSToolkit/include -I${SRC_DIR}/MSToolkit/src/expat-2.2.9/lib -I${SRC_DIR}/MSToolkit/src/zlib-1.2.11"
 
 # To switch from static to dynamic linking on linux. The Makefile does not have this flag for macOS
 # Dynamic linking does not work with mulled container tests
 #sed -i.bak 's#-static##' Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" MSToolkit/Makefile
 sed -i.bak "s#gcc#${CC}#;s#g++#${CXX}#" CometSearch/Makefile
+sed -i.bak "s|-I\\./include|-I./include ${CPPFLAGS}|" MSToolkit/Makefile
 
 make CXX=${CXX} -j ${CPU_COUNT}
 
