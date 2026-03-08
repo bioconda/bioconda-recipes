5d257bbe0cdb21950273ae26b14903776d985a05
Frederik Lassen <frederik.heymann@gmail.com>
2026-03-08 18:09:01 +0000

    Fix quoting of -D macros in build.sh

--- recipes/arcade/build.sh
+++ recipes/arcade/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-make CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -std=c++11 -DVERSION=\"${PKG_VERSION}\" -DGIT_COMMIT=\"conda\" -DGIT_DATE=\"2026-03-08\"" INCLUDES="-I./src -I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz -lhts"
+make CXX="${CXX}" \
+  CXXFLAGS="${CXXFLAGS} -std=c++11 -DVERSION='\"${PKG_VERSION}\"' -DGIT_COMMIT='\"conda\"' -DGIT_DATE='\"${PKG_VERSION}\"'" \
+  INCLUDES="-I./src -I${PREFIX}/include" \
+  LIBS="-L${PREFIX}/lib -lz -lhts"
 make install PREFIX="${PREFIX}"
