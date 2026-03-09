cfd839f59ab82ebb0e2ea2c4905faf76365ddc97
Khaled196 <56037954+khaled196@users.noreply.github.com>
2026-03-09 11:37:57 +0000

    Modify build script to use new update source
    Updated the build script to copy update files from a new source directory.

--- recipes/beacon2-ri-tools/build.sh
+++ recipes/beacon2-ri-tools/build.sh
@@ -7,8 +7,12 @@ mkdir -p "$PREFIX/bin/beacon2-ri-tools"
 # Copy required project structure
 cp -r "$SRC_DIR"/{*.py,ref_schemas,conf,files,validators} \
     "$PREFIX/bin/beacon2-ri-tools/"
+
+
+# Copy the whole missing update directory from the fixed commit
 mkdir -p "$PREFIX/bin/beacon2-ri-tools/validators/update"
-cp -r "$SRC_DIR"/extra_update/* "$PREFIX/bin/beacon2-ri-tools/validators/update/"
+cp -r "$UPDATE_SRC/validators/update/"* \
+    "$PREFIX/bin/beacon2-ri-tools/validators/update/"
 
 # Ensure package recognition
 touch "$PREFIX/bin/beacon2-ri-tools/validators/__init__.py"
