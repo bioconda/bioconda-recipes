bc1bafcdf07c33e1e116c0e6734932d66b833048
eliott <eliottob6@gmail.com>
2026-03-10 15:15:44 +0100

    Remove comments and correct version

--- recipes/himito/build.sh
+++ recipes/himito/build.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 export CARGO_HOME="${BUILD_PREFIX}/.cargo"
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install --locked --no-track --root "${PREFIX}" --path .
