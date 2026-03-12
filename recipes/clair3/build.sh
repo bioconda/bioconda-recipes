4f8bb9e46b1841d7e3c80afb3682e59c18c70705
EfraMP <aefrainmp@protonmail.com>
2026-03-12 16:54:31 -0600

    Update clair3 v1.2 to v2.0

--- recipes/clair3/build.sh
+++ recipes/clair3/build.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+set -euo pipefail
+
 ARCH=$(uname -m)
+OS=$(uname -s)
+PYPY_VER="3.11-v7.3.20"
 
 mkdir -pv $PREFIX/bin
 cp -rv clair3 models preprocess postprocess scripts shared $PREFIX/bin
-cp clair3.py $PREFIX/bin/
+cp clair3.py run_clair3.py $PREFIX/bin/
 cp run_clair3.sh $PREFIX/bin/
-if [ "$ARCH" = "x86_64" ]; then
-    cp -rv pypy3.10-v7.3.19-linux64 $PREFIX/bin
-    cp -P pypy3 pypy $PREFIX/bin
-elif [ "$ARCH" = "aarch64" ]; then
-    cp -rv pypy3.10-v7.3.19-aarch64 $PREFIX/bin
-    cp -P pypy3-aarch64 $PREFIX/bin/pypy3
-    cp -P pypy-aarch64 $PREFIX/bin/pypy
-else
-    cp -rv pypy3.10-v7.3.19-linux64 $PREFIX/bin
-    cp -P pypy3 pypy $PREFIX/bin
+
+if [ "$OS" = "Linux" ]; then
+    if [ "$ARCH" = "x86_64" ]; then
+        PYPY_DIR="${SRC_DIR}/pypy-linux64/pypy${PYPY_VER}-linux64"
+    elif [ "$ARCH" = "aarch64" ]; then
+        PYPY_DIR="${SRC_DIR}/pypy-aarch64/pypy${PYPY_VER}-aarch64"
+    fi
+    cp -rv ${PYPY_DIR} $PREFIX/bin/pypy3.11
+    ln -s $PREFIX/bin/pypy3.11/bin/pypy $PREFIX/bin/pypy3
+    ln -s $PREFIX/bin/pypy3.11/bin/pypy $PREFIX/bin/pypy
 fi
+
+$PREFIX/bin/pypy3 -m ensurepip
+$PREFIX/bin/pypy3 -m pip install mpmath==1.2.1
+
+pip install --no-deps --no-build-isolation .
 make CC=${GCC} CXX=${GXX}  PREFIX=${PREFIX}
 cp  longphase libclair3* $PREFIX/bin
+
+mkdir -p $PREFIX/bin/models
