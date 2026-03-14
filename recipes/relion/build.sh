eabaebb55854bde01b383201e408332d51a96644
Michael Milton <michael.r.milton@gmail.com>
2026-03-14 23:54:53 +1100

    Use bash array in build script; remove explicit python

--- recipes/relion/build.sh
+++ recipes/relion/build.sh
@@ -9,7 +9,7 @@ export CXXFLAGS="${CXXFLAGS} -O3 -Wno-deprecated-declarations"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-case $(uname -m) in
+case "${ARCH}" in
     aarch64)
 	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
 	;;
@@ -21,24 +21,31 @@ case $(uname -m) in
 	;;
 esac
 
+CMAKE_ARGS=(
+	-DCMAKE_BUILD_TYPE=Release
+	-DGUI=OFF
+	-DCUDA=OFF
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}"
+	-DCMAKE_CXX_COMPILER="${CXX}"
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
+	-DTORCH_HOME_PATH="${PREFIX}/share/.cache/torch"
+	-DFETCH_WEIGHTS=OFF
+	-Wno-dev -Wno-deprecated
+	--no-warn-unused-cli
+)
+
 if [[ "${OS}" == "Darwin" ]]; then
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-else
-	export CONFIG_ARGS=""
+	CMAKE_ARGS+=(
+		-DCMAKE_FIND_FRAMEWORK=NEVER
+		-DCMAKE_FIND_APPBUNDLE=NEVER
+	)
 fi
 
 if [[ "${ARCH}" == "x86_64" ]]; then
-	export ADDITIONAL_FLAGS="-DALTCPU=ON"
-else
- 	export ADDITIONAL_FLAGS=""
+	CMAKE_ARGS+=(
+		-DALTCPU=ON
+	)
 fi
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
-	-DGUI=OFF -DCUDA=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-DPYTHON_EXE_PATH="${PYTHON}" -DTORCH_HOME_PATH="${PREFIX}/share/.cache/torch" \
-	-DFETCH_WEIGHTS=OFF \
-	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
-	"${ADDITIONAL_FLAGS}" \
-	"${CONFIG_ARGS}"
+cmake -S . -B build "${CMAKE_ARGS[@]}"
 cmake --build build --target install -j "${CPU_COUNT}"
