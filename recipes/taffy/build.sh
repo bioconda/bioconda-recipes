64483efd09e8b0379dedaf9109f245888daf5bd0
Maed0x <32331424+Maed0x@users.noreply.github.com>
2026-03-16 13:13:29 +0100

    Fix hard links

--- recipes/taffy/build.sh
+++ recipes/taffy/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -ex
 
+# Patch the Makefile to use cp instead of hard links
+sed -i.bak 's/ln -f/cp -f/g' Makefile
+
+# Build and install the binary
 make -j ${CPU_COUNT}
 mkdir -p ${PREFIX}/bin
 install -v -m 755 bin/taffy ${PREFIX}/bin/
 
+# Build and install the Python package bindings
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
