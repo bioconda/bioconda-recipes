e39ada38f377d1a268c6f6c295bd3832f38260bc
Yuxuan Liu <1281096224@qq.com>
2026-03-16 20:45:24 +0800

    fix installation of hybsuite executable

--- recipes/hybsuite/build.sh
+++ recipes/hybsuite/build.sh
@@ -6,14 +6,16 @@ mkdir -p $PREFIX/share/${PKG_NAME}/bin
 mkdir -p $PREFIX/share/${PKG_NAME}/config
 mkdir -p $PREFIX/share/${PKG_NAME}/dependencies
 
-# Copy script files
-cp bin/HybSuite.sh $PREFIX/bin/
-cp -r bin/*.py $PREFIX/share/${PKG_NAME}/bin/
-cp -r bin/*.R $PREFIX/share/${PKG_NAME}/bin/
-cp -r config/* $PREFIX/share/${PKG_NAME}/config/
-cp -r dependencies/* $PREFIX/share/${PKG_NAME}/dependencies/
+# main script
+cp $SRC_DIR/bin/HybSuite.sh $PREFIX/bin/hybsuite
 
-# Set execute permissions
-chmod +x $PREFIX/bin/HybSuite.sh
+# other scripts
+cp -r $SRC_DIR/bin/*.py $PREFIX/share/${PKG_NAME}/bin/ || true
+cp -r $SRC_DIR/bin/*.R $PREFIX/share/${PKG_NAME}/bin/ || true
+
+cp -r $SRC_DIR/config/* $PREFIX/share/${PKG_NAME}/config/
+cp -r $SRC_DIR/dependencies/* $PREFIX/share/${PKG_NAME}/dependencies/
+
+chmod +x $PREFIX/bin/hybsuite
 chmod -R 755 $PREFIX/share/${PKG_NAME}/
 
