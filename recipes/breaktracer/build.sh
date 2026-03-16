dbdc586c64811d215c135a61d2c43edb7d2793ee
Tobias Rausch <rauschtobi@gmail.com>
2026-03-16 15:47:24 +0100

    add libs

--- recipes/breaktracer/build.sh
+++ recipes/breaktracer/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CXXFLAGS="${CXXFLAGS} -O3 -D__STDC_FORMAT_MACROS" make -j${CPU_COUNT} CXX="${CXX}" prefix="${PREFIX}" install
+CXXFLAGS="${CXXFLAGS} -O3 -D__STDC_FORMAT_MACROS -I${PREFIX}/include -L${PREFIX}/lib -std=c++17" make -j${CPU_COUNT} CXX="${CXX}" prefix="${PREFIX}" install
