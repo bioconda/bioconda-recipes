08a12c35feca9cd93bfcf0e38fefa9dc72061c4f
David Laehnemann <1379875+dlaehnemann@users.noreply.github.com>
2026-03-19 18:20:39 +0100

    fix: apply change suggested in separate pull request: https://github.com/bioconda/bioconda-recipes/pull/63448

--- recipes/raxml-ng/build.sh
+++ recipes/raxml-ng/build.sh
@@ -14,7 +14,7 @@ if [ ! "$(uname)" = 'Darwin' ]
 then
   mkdir build_mpi
   pushd build_mpi
-     CXX=mpicxx cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DUSE_MPI=ON ..
+     CXX=mpicxx cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DUSE_MPI=ON -DRAXML_BINARY_NAME=raxml-ng-mpi ..
      make -j ${CPU_COUNT}
      install -d ${PREFIX}/bin
      install ../bin/raxml-ng-mpi ${PREFIX}/bin
