1572eebfbb6b90081fdc00719de1c6c2270747f5
Felix Lenner <felix.lenner@scilifelab.se>
2026-03-19 13:44:49 +0100

    sucessfull build?

--- recipes/echtvar/build.sh
+++ recipes/echtvar/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-set -o xtrace -o nounset -o pipefail -o errexit
-
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile=minimal -y
-export PATH="${HOME}/.cargo/bin:${PATH}"
+set -eou pipefail
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
@@ -10,9 +7,8 @@ export CFLAGS="${CFLAGS} -O3"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-sed -i.bak 's|"0.38.2"|"0.47.1"|' Cargo.toml
-rm -rf *.bak
-
-# build statically linked binary with Rust
-RUST_BACKTRACE=1
+export RUSTC_BOOTSTRAP=1
+export RUST_BACKTRACE=1
+export RUSTFLAGS="${RUSTFLAGS:-}"
+export RUSTFLAGS="${RUSTFLAGS//-C target-feature=+crt-static/}"
 cargo install -v --no-track --locked --root "${PREFIX}" --path .
