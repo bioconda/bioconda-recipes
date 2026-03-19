e1acda4065aef0cb1104d9d85568c3c8a48878e0
Maed0x <32331424+Maed0x@users.noreply.github.com>
2026-03-19 11:22:16 +0100

    Added HAL support

--- recipes/taffy/build.sh
+++ recipes/taffy/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -ex
 
-export C_INCLUDE_PATH="${PREFIX}/include"
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-
 export CFLAGS="${CFLAGS} -DUSE_HTSLIB=1"
 export CXXFLAGS="${CXXFLAGS} -DUSE_HTSLIB=1"
 
+export LDLIBS="${LDLIBS} -lhdf5_cpp -lhdf5 -lhts"
+export LIBRARY_PATH="${PREFIX}/lib"
+
+cd taffy/submodules/abPOA
+make CC="${CC}" CXX="${CXX}" PREFIX=$(pwd) EXTRA_FLAGS="-I${PREFIX}/include -L${PREFIX}/lib" -j"${CPU_COUNT}"
+cd ../../../
+
 # Build and install the binary
-make CC="${CC}" CXX="${CXX}" AR="${AR}"
+make CC="${CC}" CXX="${CXX}" HALDIR="${SRC_DIR}/taffy/submodules/hal" sonLibRootDir="${SRC_DIR}/taffy/submodules/sonLib" -j"${CPU_COUNT}"
 mkdir -p ${PREFIX}/bin
-install -v -m 755 bin/taffy ${PREFIX}/bin/
+install -v -m 755 bin/taffy ${PREFIX}/bin/ 
 
 # Build and install the Python package bindings
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation --no-cache-dir
