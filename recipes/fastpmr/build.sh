c81932529465aa59c78512b4671e9a85b03dab65
ehuangc <edwardhuangc@gmail.com>
2026-03-19 12:56:07 +0100

    Add --locked flag to cargo install command

--- recipes/fastpmr/build.sh
+++ recipes/fastpmr/build.sh
@@ -4,4 +4,4 @@ export CARGO_NET_GIT_FETCH_WITH_CLI=true
 export CARGO_HOME=./.cargo
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --verbose --root "$PREFIX" --path .
+cargo install --locked --verbose --root "$PREFIX" --path .
