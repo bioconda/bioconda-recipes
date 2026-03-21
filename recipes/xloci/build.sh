363add6d1a0dfb87c56aac4221f6c66c6e717a26
Alejandro Gonzales-Irribarren <alejandrxgzi@gmail.com>
2026-03-21 12:28:15 +0100

    Modify build.sh for cargo installation and backtrace
    Updated paths for cargo commands and added RUST_BACKTRACE export.

--- recipes/xloci/build.sh
+++ recipes/xloci/build.sh
@@ -2,11 +2,13 @@
 
 # Add workaround for SSH-based Git connections from Rust/cargo.  See https://github.com/rust-lang/cargo/issues/2078 for details.
 # We set CARGO_HOME because we don't pass on HOME to conda-build, thus rendering the default "${HOME}/.cargo" defunct.
-export CARGO_NET_GIT_FETCH_WITH_CLI=true CARGO_HOME="${BUILD_PREFIX}/.cargo"
+export CARGO_NET_GIT_FETCH_WITH_CLI=true 
+export CARGO_HOME="${BUILD_PREFIX}/.cargo"
+export RUST_BACKTRACE=1
 
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+cd xloci
+cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
 
 # build statically linked binary with Rust
-RUST_BACKTRACE=1
-cargo install --verbose --locked --path ./xloci --no-track --root ${PREFIX}
+cargo install --verbose --locked --path . --no-track --root "${PREFIX}"
 
