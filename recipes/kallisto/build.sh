4106153e9eb7e9d0a6c76226fa9eeaf1e6481ce3
Michael Steinbaugh <mike@steinbaugh.com>
2026-03-21 15:32:47 -0400

    Update build.sh

--- recipes/kallisto/build.sh
+++ recipes/kallisto/build.sh
@@ -27,13 +27,13 @@ cd ../.. || exit 1
 
 case $(uname -m) in
     x86_64)
-	ARCH_OPTS="-DENABLE_AVX2=OFF -DCOMPILATION_ARCH=OFF"
+	ARCH_OPTS='-DENABLE_AVX2=OFF -DCOMPILATION_ARCH=OFF'
 	;;
     aarch64)
-	ARCH_OPTS="-DCOMPILATION_ARCH=OFF"
+	ARCH_OPTS='-DCOMPILATION_ARCH=OFF'
 	;;
     arm64)
-	ARCH_OPTS="-DCOMPILATION_ARCH=ON"
+	ARCH_OPTS='-DCOMPILATION_ARCH=ON'
 	;;
     *)
 	;;
@@ -54,12 +54,13 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-DCMAKE_CXX_STANDARD_LIBRARIES='-lbz2 -llzma -lcurl -lcrypto -lpthread' \
 	-DCMAKE_C_COMPILER="${CC}" \
 	-DCMAKE_C_FLAGS="${CFLAGS}" \
 	-DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
 	-DUSE_HDF5=ON -DUSE_BAM=ON -DBUILD_FUNCTESTING=ON -DMAX_KMER_SIZE=64 \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
-	"${ARCH_OPTS}" \
+	${ARCH_OPTS} \
 	"${CONFIG_ARGS}"
 
 cmake --build build --clean-first --target install
