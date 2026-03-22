c1e7f62c86b66c27549a112315b0006904c402ca
ayobi <obrien.aaron.g@gmail.com>
2026-03-21 23:06:05 -0300

    add CARGO_HOME and verbose build output

--- recipes/emits/build.sh
+++ recipes/emits/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
-cargo build --release
-mkdir -p $PREFIX/bin
-cp target/release/emits $PREFIX/bin/
+export CARGO_HOME="${BUILD_PREFIX}/.cargo"
+export CARGO_TARGET_DIR="${SRC_DIR}/target"
+
+cargo build --release --verbose 2>&1
+
+mkdir -p "${PREFIX}/bin"
+install -m 755 "${CARGO_TARGET_DIR}/release/emits" "${PREFIX}/bin/emits"
