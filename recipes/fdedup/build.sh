26a9c826f6173c3f3ae9400d58780887055b0557
raphael <raphael.ribes@gmail.com>
2026-03-23 15:25:15 +0100

    Gemini fix for osx-arm64 failed test

--- recipes/fdedup/build.sh
+++ recipes/fdedup/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Explicitly tell Rust where to find macOS system libraries
+if [[ "${target_platform}" == osx-* ]]; then
+    export RUSTFLAGS="${RUSTFLAGS} -L ${CONDA_BUILD_SYSROOT}/usr/lib"
+fi
+
 # Bundle all dependency licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
