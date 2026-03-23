b7bb4700a1c8c1060dda28bd7435822edbb06cc3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-03-23 17:10:36 -0500

    Update build script for OpenMP support

--- recipes/cgmlst-dists/build.sh
+++ recipes/cgmlst-dists/build.sh
@@ -5,13 +5,13 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-export CFLAGS="-I$PREFIX/include -Wall -Wextra -Ofast"
-export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="-I$PREFIX/include -O3 -Wall -Wextra -Ofast"
+export LDFLAGS="-L$PREFIX/lib -lomp"
 
 sed -i.bak "/^PREFIX.*$/d" Makefile
 sed -i.bak "/^CFLAGS.*$/d" Makefile
 
-make CC=$CC LIBS="-L${PREFIX}/lib -lm"
+make CC=$CC LIBS="-L${PREFIX}/lib -lm -lomp"
 
 mkdir -p "$PREFIX"/bin
 
