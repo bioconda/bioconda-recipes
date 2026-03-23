aabafe6a216a7e81daab9019758cef2ffa840600
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-03-23 17:24:32 -0500

    Fix build script for environment variables and make command

--- recipes/rtk2/build.sh
+++ recipes/rtk2/build.sh
@@ -7,10 +7,10 @@ echo "Setting environment variables"
 # Fix zlib
 export CFLAGS="$CFLAGS -O3 -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export C_INCLUDE_PATH=${PREFIX}/include
+export C_INCLUDE_PATH="${PREFIX}/include"
 
 echo "RUN MAKE"
-make test
+make test CC="${CC}"
 
 chmod 755 rtk2
 mv rtk2 "$PREFIX"/bin/
