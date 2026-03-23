9bfa50ed3a479d71e5e7fbe43eb1380acab8a21a
raphael <raphael.ribes@gmail.com>
2026-03-23 16:23:13 +0100

    Align with Bioconda Rust guidelines: add --no-track and host C-libs

--- recipes/fdedup/build.sh
+++ recipes/fdedup/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Explicitly tell Rust where to find macOS system libraries
-if [[ "${target_platform}" == osx-* ]]; then
-    export RUSTFLAGS="${RUSTFLAGS} -L ${CONDA_BUILD_SYSROOT}/usr/lib"
-fi
-
 # Bundle all dependency licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-# Install the binary
-cargo install --locked --root "$PREFIX" --path .
+# Install the binary (Bioconda requires --no-track)
+cargo install --no-track --locked --root "$PREFIX" --path .
