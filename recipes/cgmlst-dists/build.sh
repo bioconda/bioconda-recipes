8f3c1aabc590fb145ae120e494ef6f7d1cfaad8f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-03-26 16:24:44 -0500

    Update build.sh

--- recipes/cgmlst-dists/build.sh
+++ recipes/cgmlst-dists/build.sh
@@ -3,16 +3,15 @@
 #strictly use anaconda build environment
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-export CFLAGS="-I$PREFIX/include -O3 -Wall -Wextra -Ofast"
-export LDFLAGS="-L$PREFIX/lib -lomp"
+export CFLAGS="${CFLAGS} -I$PREFIX/include -O3 -Wall -Wextra -Ofast "
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
 sed -i.bak "/^PREFIX.*$/d" Makefile
 sed -i.bak "/^CFLAGS.*$/d" Makefile
 
-make CC=$CC LIBS="-L${PREFIX}/lib -lm -lomp"
+make CC=$CC LIBS="-L${PREFIX}/lib -lm"
 
 mkdir -p "$PREFIX"/bin
 
-cp cgmlst-dists "$PREFIX"/bin/
+install -v -m 0755 cgmlst-dists "$PREFIX/bin"
