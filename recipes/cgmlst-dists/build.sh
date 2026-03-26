70fc96bc5c28b704a6a77b51845dd8db81e182f8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-03-26 16:37:46 -0500

    Update build.sh

--- recipes/cgmlst-dists/build.sh
+++ recipes/cgmlst-dists/build.sh
@@ -4,13 +4,13 @@
 export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
 
-export CFLAGS="${CFLAGS} -I$PREFIX/include -O3 -Wall -Wextra -Ofast "
+export CFLAGS="${CFLAGS} -I$PREFIX/include -O3 -Wall -Wextra -Ofast -fopenmp"
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
 sed -i.bak "/^PREFIX.*$/d" Makefile
 sed -i.bak "/^CFLAGS.*$/d" Makefile
 
-make CC=$CC LIBS="-L${PREFIX}/lib -lm"
+make CC=$CC LIBS="-L${PREFIX}/lib -lm -lomp"
 
 mkdir -p "$PREFIX"/bin
 
