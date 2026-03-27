c48b6cadf4868e82522cf02171a7a9af393aacab
UENO, M. <eunosium-1128@live.jp>
2026-03-27 13:46:47 +0000

    requirements: Add environment variable exports to run_test.sh

--- recipes/acedrg/run_test.sh
+++ recipes/acedrg/run_test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -exo pipefail
 
+export CCP4=$CONDA_PREFIX
+export CBIN=$CONDA_PREFIX/bin
+export CLIBD_MON=$CONDA_PREFIX/share/ccp4/lib/data/
+export LIBMOL_ROOT=$CONDA_PREFIX
+
 TMPDIR_TEST=$(mktemp -d)
 cp ligand.smi "$TMPDIR_TEST/"
 cd "$TMPDIR_TEST"
 acedrg -i ligand.smi -o AIN-acedrg
-cd -
-rm -rf "$TMPDIR_TEST"
