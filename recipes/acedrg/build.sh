f2b70217a7be90c4adf0c497b1173ceaea3553b0
UENO, M. <eunosium-1128@live.jp>
2026-03-27 08:59:23 +0000

    Fix grep command in build script and remove run_test script

--- recipes/acedrg/build.sh
+++ recipes/acedrg/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Patch upstream CMake and replace ccp4-python references to use conda python
 # sed -i '/find_package(Python/a set(Python_SITELIB "${SP_DIR}")' CMakeLists.txt
-grep -rlIZ --exclude-dir=.git 'ccp4-python' . | xargs -0 sed -i 's/ccp4-python/python/g' || true
+grep -rlIZ --exclude-dir=.git 'ccp4-python' . | xargs -0 sed -i.bak 's/ccp4-python/python/g'
 
 cmake -S . -B build -G Ninja \
     ${CMAKE_ARGS} \
