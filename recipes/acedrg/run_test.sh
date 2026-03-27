a28667ecdeb05723b32af9061fbdd6ccec4df0af
UENO, M. <eunosium-1128@live.jp>
2026-03-27 15:36:58 +0000

    requirements: Update CLIBD_MON path and change directory commands in run_test.sh

--- recipes/acedrg/run_test.sh
+++ recipes/acedrg/run_test.sh
@@ -3,10 +3,11 @@ set -exo pipefail
 
 export CCP4=$CONDA_PREFIX
 export CBIN=$CONDA_PREFIX/bin
-export CLIBD_MON=$CONDA_PREFIX/share/ccp4/lib/data/
+export CLIBD_MON=$CONDA_PREFIX/share/acedrg/tables/
 export LIBMOL_ROOT=$CONDA_PREFIX
 
 TMPDIR_TEST=$(mktemp -d)
 cp ligand.smi "$TMPDIR_TEST/"
-cd "$TMPDIR_TEST"
+pushd "$TMPDIR_TEST"
 acedrg -i ligand.smi -o AIN-acedrg
+popd
