72e3e59d64fabda2f7f4084a94863bfdc46f3b6e
LuiisLaranjeira <luiscarloslaranjeira@hotmail.com>
2026-03-30 22:20:10 +0100

    Update recipes/falcon2/build.sh
    Co-authored-by: Thanh Lee <thanh@cloudbinfies.com>

--- recipes/falcon2/build.sh
+++ recipes/falcon2/build.sh
@@ -4,8 +4,8 @@ set -e
 
 cd src
 
-cmake .
-make
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release .
+make -j${CPU_COUNT}
 
-mkdir -p $PREFIX/bin
-cp FALCON2 $PREFIX/bin/falcon2
+mkdir -p ${PREFIX}/bin
+cp FALCON2 ${PREFIX}/bin/falcon2
