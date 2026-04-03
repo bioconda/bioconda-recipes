a5b4ffd6cc0e666e6325a21d138a8432f10eb787
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-02 20:36:33 -0500

    Update gen_patch_json.py

--- recipes/bioconda-repodata-patches/gen_patch_json.py
+++ recipes/bioconda-repodata-patches/gen_patch_json.py
@@ -11,7 +11,7 @@ import sys
 import tqdm
 import re
 import requests
-import packaging
+from packaging import version
 
 CHANNEL_NAME = "bioconda"
 CHANNEL_ALIAS = "https://conda.anaconda.org"
@@ -85,7 +85,7 @@ def has_dep(record, name):
 def parse_version(version):
     if not isinstance(version, str):
         version = '.'.join(version)
-    return packaging.version.parse(version)
+    return version.parse(version)
 
 
 changes = set([])
