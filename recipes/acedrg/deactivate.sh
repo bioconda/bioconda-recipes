89e8ebee35aa578ee941d1e5b6d2360c440ad9f2
UENO, M. <eunosium-1128@live.jp>
2026-04-03 22:25:04 +0900

    Modify deactivate.sh shebang and clean up code
    Updated shebang and removed echo statements.

--- recipes/acedrg/deactivate.sh
+++ recipes/acedrg/deactivate.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 unset LIBMOL_ROOT
 unset CCP4
 unset CBIN
 unset CLIBD_MON
-
-echo "LIBMOL_ROOT unset"
-echo "CCP4 unset"
-echo "CBIN unset"
-echo "CLIBD_MON unset"
