eac7ec682932d227a4297c9b9267f2d46e514815
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-05 17:01:50 -0500

    Update build script for Metabat2 with new flags

--- recipes/metabat2/build.sh
+++ recipes/metabat2/build.sh
@@ -2,8 +2,8 @@
 set -xe
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -ldeflate"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-deprecated-declarations -Wno-c++11-narrowing-const-reference -Wno-non-pod-varargs"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-deprecated-declarations -Wno-c++11-narrowing-const-reference -Wno-non-pod-varargs"
 export CFLAGS="${CFLAGS} -O3"
 
 # Fix the version
