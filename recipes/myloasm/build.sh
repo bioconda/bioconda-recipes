e1ca70d801ea68d2c83a5c047283d37b03375c70
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-05 14:49:33 -0500

    Enhance build script with flags and architecture support
    Added flags for linker and compiler, updated build process for architecture-specific installation.

--- recipes/myloasm/build.sh
+++ recipes/myloasm/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -euo
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-cpp -Wno-unused-function -Wno-implicit-function-declaration -Wno-int-conversion"
+
 git submodule update --init --recursive
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
-RUST_BACKTRACE=1 cargo install --verbose --path . --root $PREFIX
+RUST_BACKTRACE=1
+
+case $(uname -m) in
+    aarch64|arm64) cargo install -v --no-track --path . --root "$PREFIX" --features=neon --no-default-features ;;
+    *) cargo install -v --no-track --path . --root "$PREFIX" ;;
+esac
+
+"${STRIP}" "$PREFIX/bin/myloasm"
