d28f81f399fcea16b180fddf08d6be25be78c64f
mencian <joshua.zhuang@yahoo.com>
2026-04-06 16:58:57 -0500

    Update tmalign to 20240303

--- recipes/tmalign/build.sh
+++ recipes/tmalign/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/bin
+mkdir -p "${PREFIX}/bin"
 
-export FFLAGS="-O3 -ffast-math -lm -Wno-deprecated-declarations"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -ffast-math -Wno-deprecated-declarations"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
-if [[ `uname` == "Darwin" ]]; then
-	rm -f TMalign.cpp TMscore.cpp
-	cp -rf ${RECIPE_DIR}/TMalign.cpp .
-	wget https://zhanggroup.org/TM-score/TMscore.cpp
-	${CXX} ${FFLAGS} -o "${PREFIX}/bin/TMalign" TMalign.cpp
-	${CXX} ${FFLAGS} -o "${PREFIX}/bin/TMscore" TMscore.cpp
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+if [[ `uname -s` == "Darwin" ]]; then
+	make TMalign CC="${CXX}" CFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+	make TMscore CC="${CXX}" CFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}"
+	install -v -m 0755 TMalign TMscore "${PREFIX}/bin"
 else
-	rm -f TMalign.cpp TMscore.cpp
-	wget https://seq2fun.dcmb.med.umich.edu//TM-align/TMalign.cpp
-	wget https://zhanggroup.org/TM-score/TMscore.cpp
-	${CXX} ${FFLAGS} -static -o "${PREFIX}/bin/TMalign" TMalign.cpp
-	${CXX} ${FFLAGS} -static -o "${PREFIX}/bin/TMscore" TMscore.cpp
+	"${CXX}" "${CXXFLAGS}" -static -o "${PREFIX}/bin/TMalign" TMalign.cpp
+	"${CXX}" "${CXXFLAGS}" -static -o "${PREFIX}/bin/TMscore" TMscore.cpp
 fi
