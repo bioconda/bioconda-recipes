ecf26b84b5662d5dcf7f57b10ce3f7ce029e7c5f
mencian <joshua.zhuang@yahoo.com>
2026-04-06 11:41:15 -0500

    edit patch

--- recipes/snapatac2/build.sh
+++ recipes/snapatac2/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -ex
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-cpp -Wno-unused-function -Wno-implicit-function-declaration -Wno-int-conversion"
 export MACOSX_DEPLOYMENT_TARGET="10.15"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
 # Build the package using maturin - should produce *.whl files.
-maturin build -m snapatac2-core/Cargo.toml -b pyo3 --interpreter "${PYTHON}" --release --strip
+maturin build -m Cargo.toml -b pyo3 --interpreter "${PYTHON}" --release --strip
 
 # Install *.whl files using pip
 ${PYTHON} -m pip install target/wheels/*.whl --no-deps --no-build-isolation --no-cache-dir -vvv
 
-cd ${SRC_DIR}/snapatac2-python/ && cargo clean && rm -rf ${BUILD_PREFIX}/.cargo
+cd ${SRC_DIR}/snapatac2-contrib/ && cargo clean && rm -rf ${BUILD_PREFIX}/.cargo
