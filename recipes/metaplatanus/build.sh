21a8ff6609fc464ffcffdb506b4b3e99c5ddb585
mencian <joshua.zhuang@yahoo.com>
2026-04-06 12:10:08 -0500

    Rebuild metaplatanus recipe

--- recipes/metaplatanus/build.sh
+++ recipes/metaplatanus/build.sh
@@ -1,18 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p $PREFIX/bin/sub_bin
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
-make CXX=${CXX}
-cp metaplatanus $PREFIX/bin
-cp src/scripts/tgsgapcloser_mod $PREFIX/bin
-cp -r sub_bin/* $PREFIX/bin/sub_bin
+mkdir -p "$PREFIX/bin/sub_bin"
+
+make CXX="${CXX}"
+
+install -v -m 0755 metaplatanus \
+	src/scripts/tgsgapcloser_mod "$PREFIX/bin"
+install -v -m 0755 sub_bin/* "$PREFIX/bin/sub_bin"
 
 cd src/nextpolish
-ln -s `which ${CC}` `dirname \`which ${CC}\``/gcc
-ln -s `which ${CXX}` `dirname \`which ${CXX}\``/g++
+
+if [[ `uname -s` == "Darwin" ]]; then
+	ln -s `which ${CC}` `dirname \`which ${CC}\``/clang
+	ln -s `which ${CXX}` `dirname \`which ${CXX}\``/clang++
+else
+	ln -s `which ${CC}` `dirname \`which ${CC}\``/gcc
+	ln -s `which ${CXX}` `dirname \`which ${CXX}\``/g++
+fi
+
 make
+
 ln -s $PREFIX/bin/samtools bin/samtools
 ln -s $PREFIX/bin/bwa bin/bwa
 ln -s $PREFIX/bin/minimap2 bin/minimaps
-cp -r nextPolish lib bin $PREFIX/bin/sub_bin
+cp -rf nextPolish lib bin "$PREFIX/bin/sub_bin"
 cd ../..
