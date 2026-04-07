5d65d0693d84bfce33285f70e7ba70eca8aab81c
Miranda PG Zalusky <mgaley@uw.edu>
2026-04-07 10:30:28 -0700

    needLR tested locally

---
+++ recipes/needlr/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu -o pipefail
+
+refloc=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $refloc
+mkdir -p $PREFIX/bin
+
+cd $SRC_DIR
+
+cp needLR $PREFIX/bin/
+cp -r src/ $PREFIX/bin/
+cp -r backend_files/* $refloc/
+chmod +x $PREFIX/bin/needLR
+
+sed -i "s?REFLOCPATH?$refloc?g" $PREFIX/bin/needLR
