4c51029651f697590c9d45e75cfa0d5369850e9d
Miranda PG Zalusky <mgaley@uw.edu>
2026-04-07 12:53:29 -0700

    added a check for no terminal output and removed tput

--- recipes/needlr/build.sh
+++ recipes/needlr/build.sh
@@ -12,4 +12,11 @@ cp -r src/ $PREFIX/bin/
 cp -r backend_files/* $refloc/
 chmod +x $PREFIX/bin/needLR
 
+
+# if there is no terminal then don't attempt fancy printing
 sed -i "s?REFLOCPATH?$refloc?g" $PREFIX/bin/needLR
+if [[ -z ${TERM+x} ]]
+then
+    sed -i 's/bold=$(tput bold)/bold=""/g' $PREFIX/bin/needLR
+    sed -i 's/normal=$(tput normal)/normal=""/g' $PREFIX/bin/needLR
+fi
