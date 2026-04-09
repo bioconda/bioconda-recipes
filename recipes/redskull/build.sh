ca212c61501667a5581898af2d5f3fc5d328800b
Nils Homer <nilshomer@gmail.com>
2026-04-08 18:47:59 -0700

    redskull: export LD/DYLD library paths for build scripts

--- recipes/redskull/build.sh
+++ recipes/redskull/build.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 export RUST_BACKTRACE=1
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+export LD_LIBRARY_PATH="${PREFIX}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib${DYLD_FALLBACK_LIBRARY_PATH:+:${DYLD_FALLBACK_LIBRARY_PATH}}"
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
