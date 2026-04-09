1411ec8ef13d7e2ba7e532d47f8141ced123b3eb
mencian <joshua.zhuang@yahoo.com>
2026-04-08 20:49:31 -0500

    add patch

--- recipes/rasbhari/build.sh
+++ recipes/rasbhari/build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 set -eux
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
 install -d "${PREFIX}/bin"
-make -j "${CPU_COUNT}" CC="${CXX}"
-install -v -m 0755 rasbhari"${PREFIX}/bin"
+
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-std=c++11|-std=c++14 -march=armv8-a|' Makefile
+	;;
+    arm64)
+	sed -i.bak 's|-std=c++11|-std=c++14 -march=armv8.4-a|' Makefile
+	;;
+    x86_64)
+	sed -i.bak 's|-std=c++11|-std=c++14 -march=x86-64-v3|' Makefile
+	;;
+esac
+
+rm -f *.bak
+
+make -j"${CPU_COUNT}" CC="${CXX}"
+
+install -v -m 0755 rasbhari "${PREFIX}/bin"
