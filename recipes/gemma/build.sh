4fa86410bfd97418330e0c53ded7767544f2ffa8
mencian <joshua.zhuang@yahoo.com>
2026-04-09 01:33:34 -0500

    Rebuild gemma recipe

--- recipes/gemma/build.sh
+++ recipes/gemma/build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -eu
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -DHAVE_INLINE -pthread -Wall -std=c++14"
 
-make release
+mkdir -p "${PREFIX}/bin"
 
-mkdir -p ${PREFIX}/bin
-cp bin/gemma ${PREFIX}/bin/gemma
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+make GCC_FLAGS="${CXXFLAGS}" release
+
+install -v -m 0755 bin/gemma "${PREFIX}/bin"
