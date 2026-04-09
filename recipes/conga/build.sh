e629eeabfb8ffed4d1c2bf65526cc7a6eed54a98
asylvz <asoylev@gmail.com>
2026-04-09 13:42:59 +0300

    Fix build date quoting in build script

--- recipes/conga/build.sh
+++ recipes/conga/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -ex
 
+BUILD_DATE_STR=$(date +"%Y-%m-%d")
+
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -I${PREFIX}/include/htslib -I src"
-export CFLAGS="${CFLAGS} -DCONGA_VERSION=\"${PKG_VERSION}\" -DBUILD_DATE=\"$(date)\" -DCONGA_UPDATE=\"April 9, 2026\""
 
 SOURCES="src/svdepth.c src/cmdline.c src/common.c src/bam_data.c src/read_distribution.c src/free.c src/likelihood.c src/svs.c src/split_read.c src/genome.c"
 
 for src in ${SOURCES}; do
-    ${CC} -c ${CFLAGS} ${src} -o ${src%.c}.o
+    ${CC} -c ${CFLAGS} -DCONGA_VERSION="\"${PKG_VERSION}\"" -DBUILD_DATE="\"${BUILD_DATE_STR}\"" -DCONGA_UPDATE="\"April 9, 2026\"" ${src} -o ${src%.c}.o
 done
 
 ${CC} src/*.o -o conga -L${PREFIX}/lib -lhts -lz -lm -lpthread -llzma -lbz2 -lcurl
