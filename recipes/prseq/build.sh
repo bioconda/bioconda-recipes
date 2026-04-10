e6edc36e71ab1153f7fc771cc527981280dc60c2
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-10 00:53:54 -0500

    Update build.sh

--- recipes/prseq/build.sh
+++ recipes/prseq/build.sh
@@ -3,12 +3,16 @@
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-cpp -Wno-unused-function -Wno-implicit-function-declaration -Wno-int-conversion"
+export MACOSX_DEPLOYMENT_TARGET="10.15"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+if [[ `uname -s` == "Darwin" ]]; then
+  export MACOSX_DEPLOYMENT_TARGET="11.0"
+fi
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-
 # Build the package using maturin - should produce *.whl files.
 maturin build -m python/Cargo.toml -b pyo3 --interpreter "${PYTHON}" --release --strip
 
