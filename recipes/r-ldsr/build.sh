75b1398d1e528762ead3d8adacb2b18eb0e33910
lyh970817 <32429705+lyh970817@users.noreply.github.com>
2026-04-11 04:18:03 +0800

    Harden r-ldsr build script for conda builds

--- recipes/r-ldsr/build.sh
+++ recipes/r-ldsr/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -eux -o pipefail
 
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
 grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-# Install the package
-$R CMD INSTALL --build . ${R_ARGS}
+# Dependencies are supplied by Conda; this does not resolve packages from CRAN.
+"${R}" CMD INSTALL --build . ${R_ARGS:-}
