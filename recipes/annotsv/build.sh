9961c6b747b6475dcc1f3822170a1d3a71004eb5
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-12 16:04:29 -0500

    Update build.sh

--- recipes/annotsv/build.sh
+++ recipes/annotsv/build.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 
 mkdir -p "${PREFIX}/bin"
+mkdir -p "${PREFIX}/share/tcl/AnnotSV"
+
+cp -f "share/tcl/AnnotSV/*" ${PREFIX}/share/tcl/AnnotSV/
 
 install -v -m 0755 bin/INSTALL_annotations.sh "${PREFIX}/bin"
 
