4d6c43fea6d1ed7fba83106a0686918ae433b3c2
Zeynep Dila Deniz <zeynepdiladeniz2010@gmail.com>
2026-04-12 17:41:35 +0300

    Update build.sh

--- recipes/biofastq-a/build.sh
+++ recipes/biofastq-a/build.sh
@@ -4,7 +4,4 @@ set -euxo pipefail
 export CARGO_PROFILE_RELEASE_STRIP=false
 export CARGO_HOME="${SRC_DIR}/.cargo-home"
 
-cargo build --release
-
-install -Dm755 "${SRC_DIR}/target/release/biofastq-a" "${PREFIX}/bin/biofastq-a"
-
+cargo install --path . --root "${PREFIX}"
