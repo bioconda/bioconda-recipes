b45e71ebb38919b5cbe4cacb06de7f5f9a81b13e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-12 18:36:36 -0500

    Update build.sh

--- recipes/nextdenovo/build.sh
+++ recipes/nextdenovo/build.sh
@@ -8,15 +8,11 @@ OUTDIR="${SP_DIR}/nextdenovo"
 
 mkdir -p "${PREFIX}/bin" "${OUTDIR}"
 
-#case $(uname -s) in
-	#"Darwin") sed -i.bak 's|-lcrypto||' util/Makefile && sed -i.bak 's|-lcrypto||' lib/Makefile ;;
-#esac
-
 cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* lib/htslib/
 
 case $(uname -m) in
-	aarch64|arm64) export arm_neon=1; export aarch64=1; make arm_neon=1 aarch64=1 CC="${CC}" -j1 ;;
-	*) make CC="${CC}" -j1 ;;
+	aarch64|arm64) export arm_neon=1; export aarch64=1; make arm_neon=1 aarch64=1 CC="${CC}" -j"${CPU_COUNT}" ;;
+	*) make CC="${CC}" -j"${CPU_COUNT}" ;;
 esac
 
 install -v -m 755 bin/* "${OUTDIR}"
