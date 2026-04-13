c7e418e6218b75f70b021d382f994f6445d89f98
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-13 15:39:37 -0500

    Update build.sh

--- recipes/ksw/build.sh
+++ recipes/ksw/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
 install -d "${PREFIX}/bin"
+
 make \
     CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
     CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
     PREFIX="${PREFIX}/bin" \
     PKG_VERSION="${PKG_VERSION}" \
     -j"${CPU_COUNT}" \
-    clean all install
+    all install
