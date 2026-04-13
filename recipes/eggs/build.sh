4dd263f0933cad4404d96c565a361e97173abd31
TQ-Smith <thomasqsmith2000@gmail.com>
2026-04-13 18:11:05 -0400

    EGGS

--- recipes/eggs/build.sh
+++ recipes/eggs/build.sh
@@ -17,6 +17,6 @@ $CC $CFLAGS -c src/Interface.c -o src/Interface.o
 $CC $CFLAGS -DHAVE_INLINE -c src/Main.c -o src/Main.o
 mkdir -p bin
 $CC $LFLAGS -g -o bin/eggs src/*.o lib/*.o lib/gsl/*.o -lz -lm
-mkdir -p $PREFIX/bin
-cp bin/eggs $PREFIX/bin
-chmod +x $PREFIX/bin/eggs
+mkdir -p "$PREFIX/bin"
+cp bin/eggs "$PREFIX/bin"
+chmod +x "$PREFIX/bin/eggs"
