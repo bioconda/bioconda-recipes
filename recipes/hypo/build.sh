52d8c2239eae580d9c2a803ff79f2b0af1e83a62
mencian <joshua.zhuang@yahoo.com>
2026-04-13 00:14:40 -0500

    edit patch

--- recipes/hypo/build.sh
+++ recipes/hypo/build.sh
@@ -2,7 +2,7 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
 mkdir -p "$PREFIX/bin"
@@ -25,13 +25,26 @@ else
 	export CONFIG_ARGS=""
 fi
 
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-march=nocona -mtune=haswell|-march=armv8-a|' CMakeLists.txt
+	;;
+    arm64)
+	sed -i.bak 's|-march=nocona -mtune=haswell|-march=armv8.4-a|' CMakeLists.txt
+	;;
+    x86_64)
+	sed -i.bak 's|-march=nocona -mtune=haswell|-march=x86-64-v3|' CMakeLists.txt
+	;;
+esac
+
+rm -f *.bak
 rm -rf build
 
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Conda \
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Conda \
 	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
-ninja -C build install -j "${CPU_COUNT}"
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
