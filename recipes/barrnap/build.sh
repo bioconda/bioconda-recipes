e9f0efa1edf996594c740e38e15eeb1ae3e7249b
Torsten Seemann <torsten.seemann@gmail.com>
2026-04-13 18:36:25 +1000

    Update barrnap to 1.10.6

--- recipes/barrnap/build.sh
+++ recipes/barrnap/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-EXE="barrnap"
-DESTDIR="$PREFIX/lib/$EXE"
-mkdir -p "$DESTDIR"
-mkdir -p "$PREFIX/bin"
-cp -av * "$DESTDIR/"
-ln -s "$DESTDIR/bin/$EXE" "$PREFIX/bin/$EXE"
+bin/barrnap --updatedb
+cp -av * "$PREFIX/"
