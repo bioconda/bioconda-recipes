c875f0e59e2ce891fdb5f3386a9e0a19e8e09219
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-13 14:27:48 -0500

    Update build.sh

--- recipes/rtk2/build.sh
+++ recipes/rtk2/build.sh
@@ -9,6 +9,10 @@ export CFLAGS="$CFLAGS -O3 -I$PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 export C_INCLUDE_PATH="${PREFIX}/include"
 
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  sed -i.bak "s|-static||g" Makefile && rm -f *.bak
+fi
+
 echo "RUN MAKE"
 make test CC="${CC}"
 
