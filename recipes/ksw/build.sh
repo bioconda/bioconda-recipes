26185151b5419023eff57833059f9b391878e422
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-13 15:53:10 -0500

    Update build.sh

--- recipes/ksw/build.sh
+++ recipes/ksw/build.sh
@@ -2,14 +2,24 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -g -Wall -Wno-unused-function"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
 install -d "${PREFIX}/bin"
 
-make \
-    CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+case $(uname -m) in
+    aarch64|arm64)
+        export EXTRA_ARGS="aarch64=1" ;;
+    *)
+        export EXTRA_ARGS="" ;;
+esac
+
+make CC="${CC} ${CFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
     CXX="${CXX} ${CXXFLAGS} ${CPPFLAGS} ${LDFLAGS}" \
+    INCLUDES="${PREFIX}/include" \
     PREFIX="${PREFIX}/bin" \
     PKG_VERSION="${PKG_VERSION}" \
-    -j"${CPU_COUNT}" \
-    all install
+    "${EXTRA_ARGS}" \
+    -j"${CPU_COUNT}"
+
+make install
