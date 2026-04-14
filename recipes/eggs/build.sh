5684ce14d0a2e7fb24a9f735817aa4a320ffb388
TQ-Smith <thomasqsmith2000@gmail.com>
2026-04-14 14:49:42 -0400

    EGGS

--- recipes/eggs/build.sh
+++ recipes/eggs/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-make CC="${CC}" CFLAGS="-I${PREFIX}/include -c -Wall -g -I lib" LFLAGS="-L${PREFIX}/lib -g -o"
+make CC="${CC}" CFLAGS="-I${PREFIX}/include -c -Wall -I lib" LFLAGS="-L${PREFIX}/lib -o"
 mkdir -p "$PREFIX/bin"
 cp bin/eggs "$PREFIX/bin"
-chmod +x "$PREFIX/bin/eggs"
