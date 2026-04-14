b6b45dc7f548805b78caac1b9489793514efc50e
TQ-Smith <thomasqsmith2000@gmail.com>
2026-04-14 12:14:40 -0400

    EGGS

--- recipes/eggs/build.sh
+++ recipes/eggs/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
-CFLAGS="$CFLAGS -I lib"
+export SOURCE_DATE_EPOCH=0
+export CFLAGS="${CFLAGS} -fdebug-prefix-map=$PWD=. -I lib"
+export LDFLAGS="${LDFALGS} -W1 --build-id=none -I ${PREFIX}/include -g -o"
 $CC $CFLAGS -DHAVE_INLINE -c lib/gsl/error.c -o lib/gsl/error.o
 $CC $CFLAGS -DHAVE_INLINE -c lib/gsl/message.c -o lib/gsl/message.o
 $CC $CFLAGS -DHAVE_INLINE -c lib/gsl/stream.c -o lib/gsl/stream.o
@@ -17,7 +19,7 @@ $CC $CFLAGS -c src/GenotypeParser.c -o src/GenotypeParser.o
 $CC $CFLAGS -c src/Interface.c -o src/Interface.o
 $CC $CFLAGS -DHAVE_INLINE -c src/Main.c -o src/Main.o
 mkdir -p bin
-$CC -I "$PREFIX/include" -g -o bin/eggs src/*.o lib/*.o lib/gsl/*.o -lz -lm
+$CC $LDFLAGS bin/eggs src/*.o lib/*.o lib/gsl/*.o -lz -lm
 mkdir -p "$PREFIX/bin"
 cp bin/eggs "$PREFIX/bin"
 chmod +x "$PREFIX/bin/eggs"
