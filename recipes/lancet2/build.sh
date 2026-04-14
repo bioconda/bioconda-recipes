e81fa42d8c78887fe0f3bb5237112fe89824b049
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-13 21:06:07 -0500

    Update build.sh

--- recipes/lancet2/build.sh
+++ recipes/lancet2/build.sh
@@ -3,6 +3,7 @@
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3"
+export CFLAGS="${CFLAGS} -O3"
 
 case $(uname -m) in
     aarch64)
@@ -17,17 +18,17 @@ case $(uname -m) in
 esac
 
 if [[ `uname -s` == "Darwin" ]]; then
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER -DLANCET_BUILD_STATIC=OFF -DLANCET_ENABLE_CLOUD_IO=ON"
 else
-	export CONFIG_ARGS=""
+	export CONFIG_ARGS="-DLANCET_BUILD_STATIC=ON"
 fi
 
-cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-	-DLANCET_BUILD_STATIC=OFF \
-	-DLANCET_ENABLE_CLOUD_IO=ON \
+	-DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS}" \
 	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
 	"${CONFIG_ARGS}"
 
-ninja -C build install -j "${CPU_COUNT}"
+cmake --build build --clean-first --target install -j "${CPU_COUNT}"
