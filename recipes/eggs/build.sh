81d8306cfad5c27337696caaef891b33e26bfd58
TQ-Smith <thomasqsmith2000@gmail.com>
2026-04-14 15:23:48 -0400

    EGGS

--- recipes/eggs/build.sh
+++ recipes/eggs/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "BUILD SCRIPT IS RUNNING"
 make CC="${CC}" CFLAGS="-I${PREFIX}/include -c -Wall -I lib" LFLAGS="-L${PREFIX}/lib -o"
 mkdir -p "$PREFIX/bin"
 cp bin/eggs "$PREFIX/bin"
