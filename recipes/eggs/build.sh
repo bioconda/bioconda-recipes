0d0127a3fda91b560e9bda33ce41829c8d1680f7
TQ-Smith <thomasqsmith2000@gmail.com>
2026-04-14 12:44:31 -0400

    EGGS

--- recipes/eggs/build.sh
+++ recipes/eggs/build.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 export SOURCE_DATE_EPOCH=0
 export CFLAGS="${CFLAGS} -fdebug-prefix-map=$PWD=. -I lib"
-export LDFLAGS="${LDFLAGS} -W --build-id=none -I ${PREFIX}/include -g -o"
+export LDFLAGS="${LDFLAGS} -W -I ${PREFIX}/include -g -o"
 $CC $CFLAGS -DHAVE_INLINE -c lib/gsl/error.c -o lib/gsl/error.o
 $CC $CFLAGS -DHAVE_INLINE -c lib/gsl/message.c -o lib/gsl/message.o
 $CC $CFLAGS -DHAVE_INLINE -c lib/gsl/stream.c -o lib/gsl/stream.o
