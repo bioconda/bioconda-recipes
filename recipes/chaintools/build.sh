98e37257ab2c43ecaa74106c747bca6c8a6707a0
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-13 22:45:08 -0500

    Update build.sh

--- recipes/chaintools/build.sh
+++ recipes/chaintools/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 set -xe
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
-RUST_BACKTRACE=1 cargo install --verbose --path . --root ${PREFIX}
+RUST_BACKTRACE=1 cargo install --locked --no-track --verbose --path . --root "${PREFIX}"
