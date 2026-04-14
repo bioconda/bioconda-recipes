ebec0403c12a8d2b5dc6150bf229621e1817953c
mencian <joshua.zhuang@yahoo.com>
2026-04-14 00:09:24 -0500

    Update bayescan to v2.1

--- recipes/bayescan/build.sh
+++ recipes/bayescan/build.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-cd source
-make CXXFLAGS="${CXXFLAGS} -std=c++14"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 mkdir -p "${PREFIX}/bin"
-cp bayescan2 "${PREFIX}/bin/"
+
+if [[ `uname -s` == "Darwin" ]]; then
+	sed -i.bak 's|-static||' source/Makefile
+fi
+
+sed -i.bak 's|BayeScan 2.0|BayeScan 2.1|' source/start.cpp
+sed -i.bak 's|strcpy (long_opt_prefix,  _prefix)|strncpy (long_opt_prefix,  _prefix, MAX_LONG_PREFIX_LENGTH)|' source/anyoption.cpp
+sed -i.bak 's|strcpy( long_opt_prefix , "--" )|strncpy( long_opt_prefix , "--", MAX_LONG_PREFIX_LENGTH )|' source/anyoption.cpp
+rm -f source/*.bak
+
+cd source
+
+make CXX="${CXX}" CXXFLAGS="${CXXFLAGS}"
+
+install -v -m 0755 bayescan_2.1 "${PREFIX}/bin"
