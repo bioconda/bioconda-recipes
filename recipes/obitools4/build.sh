49d93b8950a4243d6e52e179ff0cb74ba9ad6a61
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-15 15:53:03 -0500

    Update build.sh

--- recipes/obitools4/build.sh
+++ recipes/obitools4/build.sh
@@ -4,6 +4,8 @@ set -xe
 export GOPATH="$PWD"
 export GOCACHE="$PWD/.cache"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CGO_LDFLAGS="-L${PREFIX}/lib -lz"
+export CGO_CFLAGS="$CGO_CFLAGS -I${PREFIX}/include"
 
 mkdir -p "${GOCACHE}"
 mkdir -p "${PREFIX}/bin"
@@ -13,9 +15,10 @@ rm -f *.bak
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
     export MACOSX_DEPLOYMENT_TARGET="12.0"
-    make CGO_CFLAGS="$CGO_CFLAGS -I${PREFIX}/include"
+    export CGO_LDFLAGS="-mmacosx-version-min=12.0"
+    make CGO_CFLAGS="$CGO_CFLAGS" LDFLAGS="${LDFLAGS}"
 else
-    make CGO_CFLAGS="$CGO_CFLAGS -L$PREFIX/lib -I$PREFIX/include"
+    make CGO_CFLAGS="$CGO_CFLAGS -L$PREFIX/lib" LDFLAGS="${LDFLAGS}"
 fi
 
 install -v -m 0755 "build/obi*" "${PREFIX}/bin"
