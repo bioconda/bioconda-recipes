7895c9828cf1422ce46d520458489084f8f86f3b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-15 00:29:37 -0500

    Update build.sh

--- recipes/pydmtools/build.sh
+++ recipes/pydmtools/build.sh
@@ -2,4 +2,4 @@
 
 export C_INCLUDE_PATH=$PREFIX/include
 
-python3 setup.py install --single-version-externally-managed --record=record.txt
+python3 setup.py install --single-version-externally-managed --record=record.txt1
