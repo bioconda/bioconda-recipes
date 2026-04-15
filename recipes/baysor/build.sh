2b0a69823cdffe5b61779387b0b4776f81b05c0f
Quentin Blampey <quentin.blampey@gmail.com>
2026-04-15 15:28:40 +0200

    update build

--- recipes/baysor/build.sh
+++ recipes/baysor/build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Create the bin directory in the conda environment prefix
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/opt/baysor"
+mkdir -p $PREFIX/bin
 
-# Copy the contents of the zip (already extracted by conda-build into $SRC_DIR)
-# The zip contains a 'bin' folder and potentially 'lib' folders.
-cp -r ./* "$PREFIX/opt/baysor/"
+ln -s bin/baysor $PREFIX/bin/baysor
 
-# Create a symbolic link so 'baysor' is available in the user's PATH
-ln -s "$PREFIX/opt/baysor/bin/baysor" "$PREFIX/bin/baysor"
-
-# Ensure the binary is executable
-chmod +x "$PREFIX/bin/baysor"
+chmod +x $PREFIX/bin/baysor
