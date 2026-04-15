873a7cc463b6c2c8c2d258448cc7be9070a47748
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-15 14:45:37 -0500

    Update build.sh

--- recipes/obitools4/build.sh
+++ recipes/obitools4/build.sh
@@ -1,7 +1,6 @@
 #!/bin bash
 set -xe
 
-export CGO_ENABLED=0
 export GOPATH="$PWD"
 export GOCACHE="$PWD/.cache"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
@@ -19,34 +18,4 @@ else
     GOFLAGS="-buildvcs=false" make "CGO_CFLAGS=$CGO_CFLAGS -L$PREFIX/lib -I$PREFIX/include"
 fi
 
-install -v -m 0755 \
-    build/obiannotate \
-    build/obiclean \
-    build/obicleandb \
-    build/obicomplement \
-    build/obiconsensus \
-    build/obiconvert \
-    build/obicount \
-    build/obicsv \
-    build/obidemerge \
-    build/obidistribute \
-    build/obigrep \
-    build/obijoin \
-    build/obikmermatch \
-    build/obikmersimcount \
-    build/obilandmark \
-    build/obimatrix \
-    build/obimicrosat \
-    build/obimultiplex \
-    build/obipairing \
-    build/obipcr \
-    build/obireffamidx \
-    build/obirefidx \
-    build/obiscript \
-    build/obisplit \
-    build/obisummary \
-    build/obitag \
-    build/obitagpcr \
-    build/obitaxonomy \
-    build/obiuniq \
-    "${PREFIX}/bin"
+install -v -m 0755 "build/obi*" "${PREFIX}/bin"
