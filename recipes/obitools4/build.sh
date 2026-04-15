be0c2414724a1b3f5d39cdd9cdb7e290997df6f3
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-15 16:31:43 -0500

    Update build.sh

--- recipes/obitools4/build.sh
+++ recipes/obitools4/build.sh
@@ -14,8 +14,13 @@ sed -i.bak 's|GOFLAGS=|GOFLAGS=-buildvcs=false|' Makefile
 rm -f *.bak
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-    export MACOSX_DEPLOYMENT_TARGET="12.0"
-    export CGO_LDFLAGS="-mmacosx-version-min=12.0"
+    wget https://github.com/alexey-lysiuk/macos-sdk/releases/download/13.3/MacOSX13.3.tar.xz
+	tar -xf MacOSX13.3.tar.xz
+	cp -rH MacOSX13.3.sdk /Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
+    export SDKROOT="/Applications/Xcode-15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk"
+    export MACOSX_DEPLOYMENT_TARGET="13.0"
+    export MACOSX_SDK_VERSION="13.0"
+    export CGO_LDFLAGS="-mmacosx-version-min=13.0"
     make CGO_CFLAGS="$CGO_CFLAGS"
 else
     make CGO_CFLAGS="$CGO_CFLAGS -L$PREFIX/lib"
