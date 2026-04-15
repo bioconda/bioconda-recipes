b339567bd952b46e43295264f9c43d02f0a2c2e6
Michael Hall <mhaldini@icloud.com>
2026-04-15 11:17:06 +1000

    uncomment license and try liblzma

--- recipes/nohuman/build.sh
+++ recipes/nohuman/build.sh
@@ -5,7 +5,7 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
 
-# cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 RUST_BACKTRACE=full
 cargo install -v --locked --no-track --root "${PREFIX}" --path .
