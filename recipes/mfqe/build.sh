3626eb4f943ba161663941bc9038ef0c795b8ff3
mencian <joshua.zhuang@yahoo.com>
2026-04-15 23:38:15 -0500

    mfqe: add aarch64/arm64 builds

--- recipes/mfqe/build.sh
+++ recipes/mfqe/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -e
 
+export C_INCLUDE_PATH="$PREFIX/include"
+export LIBRARY_PATH="$PREFIX/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # Build statically linked binary with Rust
-C_INCLUDE_PATH=$PREFIX/include \
-LIBRARY_PATH=$PREFIX/lib \
-cargo build --release
+RUST_BACKTRACE=1
+cargo build --release -j "${CPU_COUNT}"
 
 # Install the binary
-mkdir -p ${PREFIX}/bin
-cp target/release/mfqe $PREFIX/bin
+install -v -m 0755 target/release/mfqe "$PREFIX/bin"
+"${STRIP}" "$PREFIX/bin/mfqe"
