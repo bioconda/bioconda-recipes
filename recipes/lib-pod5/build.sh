4074a937cbf72ddab661860938a61339aac4c164
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-15 21:20:05 -0500

    Update build.sh

--- recipes/lib-pod5/build.sh
+++ recipes/lib-pod5/build.sh
@@ -16,20 +16,20 @@ ${PYTHON} -m pod5_make_version
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 
-if [[ "${OS}" == "Darwin" ]]; then
-	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
-fi
-
-if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
-	export CONFIG_ARGS="${CONFIG_ARGS} -DCMAKE_OSX_ARCHITECTURES=arm64"
-fi
-
 if [[ "${ARCH}" == "aarch64" ]]; then
 	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
 elif [[ "${ARCH}" == "arm64" ]]; then
 	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
 else
 	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+fi
+
+if [[ "${OS}" == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+fi
+
+if [[ "${OS}" == "Darwin" && "${ARCH}" == "arm64" ]]; then
+	export CONFIG_ARGS="${CONFIG_ARGS} -DCMAKE_OSX_ARCHITECTURES=arm64"
 fi
 
 cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -41,16 +41,4 @@ cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 
 ninja -C build install -j "${CPU_COUNT}"
 
-if [[ "${OS}" == "Darwin" ]]; then
-	${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
-	install -v lib/*.a "${PREFIX}/lib"
-else
-	${PYTHON} -m pip install *.whl --no-deps --no-build-isolation --no-cache-dir -vvv
-	install -v lib64/*.a "${PREFIX}/lib"
-fi
-
-install -v include/pod5_format/*.h "${PREFIX}/include"
-cp -rf include/pod5_format "${PREFIX}/include"
-
-cd python/lib_pod5/
-${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
+${PYTHON} -m pip install *.whl --no-deps --no-build-isolation --no-cache-dir -vvv
