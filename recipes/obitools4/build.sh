2a1c95228e2d9f0c5579d915abfb112708aafbed
kpayet26 <kevin.payet261101@gmail.com>
2026-04-16 10:36:08 +0200

    Update build.sh to export CGO_CFLAGS
    Refactor build script to set CGO_CFLAGS using export.

--- recipes/obitools4/build.sh
+++ recipes/obitools4/build.sh
@@ -1,19 +1,17 @@
 #!/bin bash
 
 set -xe
-
 mkdir -p $PREFIX/bin
 
+export GOFLAGS="-buildvcs=false"
+
 if [ "$(uname)" == "Darwin" ]; then
-    make \
-        GOFLAGS="-buildvcs=false" \
-        CGO_CFLAGS="$CGO_CFLAGS -I${CONDA_PREFIX}/include"
+    export CGO_CFLAGS="${CGO_CFLAGS} -I${CONDA_PREFIX}/include"
 else
-    make \
-        GOFLAGS="-buildvcs=false" \ 
-        CGO_CFLAGS="$CGO_CFLAGS -L$CONDA_PREFIX/lib -I$CONDA_PREFIX/include"
+    export CGO_CFLAGS="${CGO_CFLAGS} -L${CONDA_PREFIX}/lib -I${CONDA_PREFIX}/include"
 fi
 
+make
 
 cp \
     build/obiannotate \
