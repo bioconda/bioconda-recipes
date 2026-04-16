5b19930c3fd3bceaf04755a52731d24a09e37f91
kpayet26 <kevin.payet261101@gmail.com>
2026-04-16 15:07:13 +0200

    Update build script for cross-platform compatibility

--- recipes/obitools4/build.sh
+++ recipes/obitools4/build.sh
@@ -3,13 +3,14 @@
 set -xe
 mkdir -p $PREFIX/bin
 
+export GOFLAGS="-buildvcs=false"
+
 if [ "$(uname)" == "Darwin" ]; then
-    export CGO_CFLAGS="${CGO_CFLAGS} -I${CONDA_PREFIX}/include"
+    make "CGO_CFLAGS=$CGO_CFLAGS -I${CONDA_PREFIX}/include"
 else
-    export CGO_CFLAGS="${CGO_CFLAGS} -L${CONDA_PREFIX}/lib -I${CONDA_PREFIX}/include"
+    make "CGO_CFLAGS=$CGO_CFLAGS -L$CONDA_PREFIX/lib -I$CONDA_PREFIX/include"
 fi
 
-make GOFLAGS="-buildvcs=false"
 
 cp \
     build/obiannotate \
