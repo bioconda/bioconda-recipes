31aa384b82470978ccc90acaa0884b9339a51e33
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-16 14:19:17 -0500

    Update build.sh

--- recipes/segway/build.sh
+++ recipes/segway/build.sh
@@ -2,7 +2,7 @@
 
 export HDF5_DIR=$PREFIX
 
-$PYTHON -m pip install -vv --disable-pip-version-check --no-deps --no-cache-dir --ignore-installed . 
+$PYTHON -m pip install -vv --disable-pip-version-check --no-deps --no-cache-dir --ignore-installed . 1
 # Work around for no 'source_files' support in test section of meta.yaml
 # Only use test simpleseg as a sanity check
 # cp test/test_all.sh $PREFIX/bin/run_segway_tests.sh
