997fbdcf4791a9d7bfd760fde53b19f0d16e7762
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-16 03:29:31 -0500

    Update build.sh

--- recipes/dysgu/build.sh
+++ recipes/dysgu/build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++17"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export C_INCLUDE_PATH="${PREFIX}/include"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
 if [[ "$target_platform" == "osx-64" || "$target_platform" == "osx-arm64" ]]; then
     SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
