05e2b52da382aa3987a471921308221063835021
Johannes Koester <johannes.koester@uni-due.de>
2026-04-17 12:07:02 +0200

    fix bioconductor-rgraphviz and bioconductor-msa

--- recipes/bioconductor-rgraphviz/build.sh
+++ recipes/bioconductor-rgraphviz/build.sh
@@ -7,5 +7,6 @@ FC=$FC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
-CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+CXX14=$CXX
+CC17=$CC" > ~/.R/Makevars
+$R CMD INSTALL --build . --configure-args="--with-graphviz=${PREFIX}"
