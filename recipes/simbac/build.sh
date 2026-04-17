ffee463c863db97bfbcffb2a396419460ee8fddd
mencian <joshua.zhuang@yahoo.com>
2026-04-16 21:14:01 -0500

    simbac: add aarch64/arm64 builds

--- recipes/simbac/build.sh
+++ recipes/simbac/build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-${CC} *.cpp -lgsl -lgslcblas -lm -O2 -lstdc++ -L$PREFIX/lib -I$PREFIX/include -o $PREFIX/bin/SimBac
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+mkdir -p "$PREFIX/bin"
+
+${CXX} *.cpp -lgsl -lgslcblas -lm "${CXXFLAGS}" -lstdc++ -L$PREFIX/lib -I$PREFIX/include -o $PREFIX/bin/SimBac
