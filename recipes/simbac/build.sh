c7b4fabb404932ca14fd22ba6c972d9575c18a4e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-16 21:22:13 -0500

    Update build.sh

--- recipes/simbac/build.sh
+++ recipes/simbac/build.sh
@@ -2,20 +2,19 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3"
 
 case $(uname -m) in
     aarch64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	export ARCH="-march=armv8-a"
 	;;
     arm64)
-	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	export ARCH="-march=armv8.4-a"
 	;;
     x86_64)
-	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	export ARCH="-march=x86-64-v3"
 	;;
 esac
 
 mkdir -p "$PREFIX/bin"
 
-${CXX} *.cpp -lgsl -lgslcblas -lm "${CXXFLAGS}" -lstdc++ -L$PREFIX/lib -I$PREFIX/include -o $PREFIX/bin/SimBac
+${CXX} *.cpp -lgsl -lgslcblas -lm ${ARCH} -O3 -lstdc++ -L$PREFIX/lib -I$PREFIX/include -o $PREFIX/bin/SimBac
