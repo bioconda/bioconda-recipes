378d9ab7d5beee6ec07e98dbeac4f7149af1e841
Till Hartmann <till.hartmann@tu-dortmund.de>
2026-04-17 10:39:26 +0200

    feat: add mehari-python

---
+++ recipes/mehari-python/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xeuo
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration -Wno-int-conversion"
+
+# Make sure bindgen passes on our compiler flags.
+export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
+
+sed -i.bak 's|"0.23"|"0.24.0"|' Cargo.toml
+rm -f *.bak
+
+rm .cargo/config.toml  # remove custom config.toml for now
+# export ROCKSDB_LIB_DIR="${PREFIX}/lib"
+# export SNAPPY_LIB_DIR="${PREFIX}/lib"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+python -m pip install ./mehari-python --no-deps --no-build-isolation --disable-pip-version-check -vv
