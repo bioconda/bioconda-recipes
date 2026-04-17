62f38a3e130192c015fdacbfbe2c53b3875ec199
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-17 00:12:12 -0500

    Update build.sh

--- recipes/filo/build.sh
+++ recipes/filo/build.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
 # Add include paths for filo's internal headers (gzstream.h, tabFile.h, etc.)
 # which are referenced with angle brackets in the source files.
 export FILO_INCLUDES="-I${PREFIX}/include -I${SRC_DIR}/src/common/gzstream -I${SRC_DIR}/src/common/tabFile -I${SRC_DIR}/src/common/fileType -I${SRC_DIR}/src/common/version"
@@ -22,7 +22,7 @@ case $(uname -m) in
 	;;
 esac
 
-make CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -Wall ${FILO_INCLUDES}" LIBS="-L${PREFIX}/lib -lz"
+make CXX="${CXX}" CXXFLAGS="${CXXFLAGS} -Wall ${FILO_INCLUDES}" LIBS="-L${PREFIX}/lib -lz" -j"${CPU_COUNT}"
 
 # Install binaries
 install -v -m 0755 bin/stats bin/groupBy "${PREFIX}/bin"
