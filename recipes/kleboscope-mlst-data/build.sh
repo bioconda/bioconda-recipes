d4cf59402aebcea6b1377208d5b5b222a32b61c4
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-18 13:23:21 +0000

    Add debug to build.sh for kleboscope-mlst-data

--- recipes/kleboscope-mlst-data/build.sh
+++ recipes/kleboscope-mlst-data/build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -e
 
+echo "=== Debug: SRC_DIR = $SRC_DIR"
+ls -la $SRC_DIR
+
 cd "$SRC_DIR"
 
-# If there is exactly one subdirectory, step into it
-subdir=$(find . -maxdepth 1 -type d ! -name . | head -1)
-if [ -n "$subdir" ]; then
+if [ $(ls -1 | wc -l) -eq 1 ] && [ -d "$(ls -1)" ]; then
+    subdir=$(ls -1)
+    echo "=== Entering subdirectory: $subdir"
     cd "$subdir"
 fi
 
+echo "=== Current directory contents:"
+ls -la
+
 DEST="$SP_DIR/kleboscope/modules/kleb_mlst_module"
+echo "=== Destination: $DEST"
 mkdir -p "$DEST"
-cp -r ./* "$DEST/"
+
+cp -rv ./* "$DEST/"
+
+echo "=== Final contents of DEST:"
+ls -la "$DEST"
