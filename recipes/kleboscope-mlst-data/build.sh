a8802c5596c857266fcff62f8f665764e4833cd0
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-18 13:12:03 +0000

    Fix build.sh to handle tarball extraction structure

--- recipes/kleboscope-mlst-data/build.sh
+++ recipes/kleboscope-mlst-data/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+cd "$SRC_DIR"
+
+# If there is exactly one subdirectory, step into it
+subdir=$(find . -maxdepth 1 -type d ! -name . | head -1)
+if [ -n "$subdir" ]; then
+    cd "$subdir"
+fi
+
 DEST="$SP_DIR/kleboscope/modules/kleb_mlst_module"
 mkdir -p "$DEST"
-cp -r $SRC_DIR/kleb_mlst_data/* "$DEST/"
+cp -r ./* "$DEST/"
