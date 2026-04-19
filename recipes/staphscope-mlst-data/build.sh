923d65749c86705722c0abfcb4f2e44ac4cedbc6
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-19 16:03:10 +0000

    Final fix: use find to locate module dirs and robust test commands

--- recipes/staphscope-mlst-data/build.sh
+++ recipes/staphscope-mlst-data/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+MLST_DIR=$(find "$SRC_DIR" -type d -name "mlst_module" | head -1)
+if [ -z "$MLST_DIR" ]; then
+    echo "ERROR: mlst_module not found in $SRC_DIR"
+    exit 1
+fi
+
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -r $SRC_DIR/* "$DEST/"
+cp -r "$MLST_DIR" "$DEST/"
--- recipes/staphscope-sccmec-data/build.sh
+++ recipes/staphscope-sccmec-data/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+SCCMEC_DIR=$(find "$SRC_DIR" -type d -name "sccmec_module" | head -1)
+if [ -z "$SCCMEC_DIR" ]; then
+    echo "ERROR: sccmec_module not found in $SRC_DIR"
+    exit 1
+fi
+
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -r $SRC_DIR/* "$DEST/"
+cp -r "$SCCMEC_DIR" "$DEST/"
