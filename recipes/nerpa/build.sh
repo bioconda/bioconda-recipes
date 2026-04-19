65d994cb71f40bbd3fa0a2fb64d4a2449835ab75
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-19 03:34:03 -0500

    Update build.sh

--- recipes/nerpa/build.sh
+++ recipes/nerpa/build.sh
@@ -24,14 +24,6 @@ else
 	export CONFIG_ARGS=""
 fi
 
-paras_model_link='https://zenodo.org/records/13165500/files/model.paras?download=1'
-# q: if ./external_tools/paras/model.paras doesn't exist, download it
-if [ ! -f "./external_tools/paras/model.paras" ]; then
-    echo "Downloading model.paras from $paras_model_link..."
-    mkdir -p ./external_tools/paras
-    wget -O ./external_tools/paras/model.paras $paras_model_link
-fi
-
 cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
 	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
