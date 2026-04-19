4eb1eec65c750f1ffd048605bb6b54e79a4e905e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-18 23:23:33 -0500

    Update build.sh

--- recipes/illumina-interop/build.sh
+++ recipes/illumina-interop/build.sh
@@ -27,7 +27,7 @@ cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX="${PREFIX}/interop" \
 	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
 	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" -Wno-dev -Wno-deprecated \
 	--no-warn-unused-cli -DENABLE_PYTHON=ON -DPython_EXECUTABLE="${PYTHON}" \
-	-DENABLE_SWIG=ON \
+	-DENABLE_SWIG=ON -DGTEST_INCLUDE_DIR="${PREFIX}/include/gtest" -DGTEST_LIBRARY="${PREFIX}/lib" \
 	-DPython_ROOT_DIR="${PREFIX}/bin" -DPYTHON_VERSION=$(python -c 'import platform; print(platform.python_version())') \
 	"${CONFIG_ARGS}"
 
