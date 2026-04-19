ffb11d09774c923c6baa325a8f5cab16b8fd357d
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-19 15:01:18 +0000

    staphscope: install data packages into share/ (build 2) and update orchestrator (build 1)

--- recipes/staphscope-mlst-data/build.sh
+++ recipes/staphscope-mlst-data/build.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "=== Debug: SRC_DIR = $SRC_DIR"
-ls -la $SRC_DIR
-
-DEST="$SP_DIR/staphscope/modules/mlst_module"
+DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-echo "=== Debug: DEST = $DEST"
-
-if [ -d "$SRC_DIR/mlst_module" ]; then
-    echo "=== Found mlst_module directory, copying its contents"
-    cp -rv "$SRC_DIR/mlst_module/." "$DEST/"
-else
-    echo "=== No mlst_module directory, copying all contents"
-    cp -rv "$SRC_DIR/." "$DEST/"
-fi
-
-echo "=== Debug: Final contents of $DEST"
-ls -la "$DEST"
+cp -r $SRC_DIR/mlst_module "$DEST/"
