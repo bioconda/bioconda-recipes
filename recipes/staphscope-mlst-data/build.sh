8bf4c06023f83d8e8eb1bee4b93c113a9d986fe6
BECKLEY BROWN <brownbeckley94@gmail.com>
2026-04-19 07:06:46 +0000

    Fix database installation path: use SP_DIR, bump build number to 1 (#64551)

--- recipes/staphscope-mlst-data/build.sh
+++ recipes/staphscope-mlst-data/build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
-# Destination directory for mlst_module
-DEST="$PREFIX/share/staphscope/modules/mlst_module"
-mkdir -p "$DEST"
+echo "=== Debug: SRC_DIR = $SRC_DIR"
+ls -la $SRC_DIR
 
-# Copy all contents from SRC_DIR (these are the mlst_module files) into DEST
-cp -r $SRC_DIR/* "$DEST/"
+DEST="$SP_DIR/staphscope/modules/mlst_module"
+mkdir -p "$DEST"
+echo "=== Debug: DEST = $DEST"
+
+if [ -d "$SRC_DIR/mlst_module" ]; then
+    echo "=== Found mlst_module directory, copying its contents"
+    cp -rv "$SRC_DIR/mlst_module/." "$DEST/"
+else
+    echo "=== No mlst_module directory, copying all contents"
+    cp -rv "$SRC_DIR/." "$DEST/"
+fi
+
+echo "=== Debug: Final contents of $DEST"
+ls -la "$DEST"
