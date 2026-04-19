ed32e668232d61205ce6d147a0c876d3ff7dfc22
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-18 21:24:21 -0500

    Update recipes/csem/build.sh
    Co-authored-by: Thanh Lee <thanh@cloudbinfies.com>

--- recipes/csem/build.sh
+++ recipes/csem/build.sh
@@ -40,9 +40,9 @@ make
 mkdir -p "${PREFIX}/bin"
 cp csem csem-bam2wig csem-bam-processor csem-generate-input run-csem csem_perl_utils.pm "${PREFIX}/bin/"
 
-# --- Fix Perl shebangs to use conda environment perl ---
-sed -i "1s|#!/usr/bin/perl|#!${PREFIX}/bin/perl|" "${PREFIX}/bin/run-csem"
-sed -i "1s|#!/usr/bin/perl|#!${PREFIX}/bin/perl|" "${PREFIX}/bin/csem-generate-input"
+# --- Fix Perl shebangs to use env-based lookup ---
+sed -i "1s|#!/usr/bin/perl|#!/usr/bin/env perl|" "${PREFIX}/bin/run-csem"
+sed -i "1s|#!/usr/bin/perl|#!/usr/bin/env perl|" "${PREFIX}/bin/csem-generate-input"
 
 # --- Install bundled samtools (old 0.1.x API) used by run-csem and csem-generate-input ---
 # The Perl wrappers call "$FindBin::Bin/sam/samtools" with pre-1.0 syntax
