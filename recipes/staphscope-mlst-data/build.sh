c7230b6cf38114b0c2f83476e561f6b97bbb5ab8
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-19 16:22:56 +0000

    Use minimal build.sh: copy everything from SRC_DIR

--- recipes/staphscope-mlst-data/build.sh
+++ recipes/staphscope-mlst-data/build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 set -e
-set -x
-
-echo "=== SRC_DIR = $SRC_DIR"
-ls -la "$SRC_DIR"
-
-# Find mlst_module
-MLST_DIR=$(find "$SRC_DIR" -type d -name "mlst_module" | head -1)
-echo "MLST_DIR = $MLST_DIR"
-
-if [ -z "$MLST_DIR" ]; then
-    echo "ERROR: mlst_module not found"
-    exit 1
-fi
-
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -rv "$MLST_DIR" "$DEST/"
-echo "=== Copy completed"
+cp -r $SRC_DIR/* "$DEST/"
--- recipes/staphscope-sccmec-data/build.sh
+++ recipes/staphscope-sccmec-data/build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 set -e
-set -x
-
-echo "=== SRC_DIR = $SRC_DIR"
-ls -la "$SRC_DIR"
-
-SCCMEC_DIR=$(find "$SRC_DIR" -type d -name "sccmec_module" | head -1)
-echo "SCCMEC_DIR = $SCCMEC_DIR"
-
-if [ -z "$SCCMEC_DIR" ]; then
-    echo "ERROR: sccmec_module not found"
-    exit 1
-fi
-
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -rv "$SCCMEC_DIR" "$DEST/"
-echo "=== Copy completed"
+cp -r $SRC_DIR/* "$DEST/"
