837885651d408e39f5cae2fbd4b95ce27a04fdb9
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-19 15:54:53 -0500

    Update build.sh

--- recipes/amplirust/build.sh
+++ recipes/amplirust/build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -ex
 
-# Build with cargo
-RUST_BACKTRACE=1 cargo install --no-track --locked --root "${PREFIX}" --path .
+case $(uname -m) in
+    aarch64)
+        export RUSTFLAGS="-C target-cpu=aarch64-unknown-linux-gnu"
+        ;;
+    arm64)
+        export RUSTFLAGS="-C target-cpu=aarch64-apple-darwin"
+        ;;
+    x86_64)
+        export RUSTFLAGS="-C target-cpu=x86-64-v3"
+        ;;
+esac
 
 # Bundle third-party licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+# Build with cargo
+RUST_BACKTRACE=1
+RUSTFLAGS="${RUSTFLAGS}" cargo install --no-track --locked --root "${PREFIX}" --path .
