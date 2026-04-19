da6f5a73b61c6dddbe9cb232d65b4921d8d22d88
Till Hartmann <till.hartmann@udo.edu>
2026-04-19 13:05:03 +0200

    remove legacy sed replacement altogether

--- recipes/mehari-python/build.sh
+++ recipes/mehari-python/build.sh
@@ -7,8 +7,6 @@ export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration -Wno-int-convers
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 
-sed -i 's|"0.23"|"0.24.0"|' Cargo.toml
-
 rm .cargo/config.toml  # remove custom config.toml for now
 # export ROCKSDB_LIB_DIR="${PREFIX}/lib"
 # export SNAPPY_LIB_DIR="${PREFIX}/lib"
