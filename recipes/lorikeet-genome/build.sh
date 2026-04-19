662b9a45b6d4c7993ca9bde3eaa22b364baf6aa8
mencian <joshua.zhuang@yahoo.com>
2026-04-19 16:56:44 -0500

    Rebuild lorikeet-genome recipe

--- recipes/lorikeet-genome/build.sh
+++ recipes/lorikeet-genome/build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -e
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -Wno-implicit-function-declaration"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+RUST_BACKTRACE=1
 # Build statically linked binary with Rust
 C_INCLUDE_PATH=$PREFIX/include \
 LIBRARY_PATH=$PREFIX/lib \
-LIBCLANG_PATH=$PREFIX/lib/libclang.so \
+LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang.so \
 OPENSSL_DIR=$PREFIX \
 cargo build --release
 
 # Install the binaries
 C_INCLUDE_PATH=$PREFIX/include \
 LIBRARY_PATH=$PREFIX/lib \
-LIBCLANG_PATH=$PREFIX/lib/libclang.so \
+LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang.so \
 OPENSSL_DIR=$PREFIX \
 cargo install --path ./ --force --root $PREFIX
