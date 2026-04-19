22252deec3a474df280c3dc8a9091b6963992945
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-18 23:52:11 -0500

    Update build.sh

--- recipes/bioconductor-rgraphviz/build.sh
+++ recipes/bioconductor-rgraphviz/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+export DISABLE_AUTOBREW=1
+
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
 echo -e "CC=$CC
 FC=$FC
+CC17=$CC -std=gnu11 -fPIC
 CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-$R CMD INSTALL --build .
+$R CMD INSTALL --build . "${R_ARGS}"
