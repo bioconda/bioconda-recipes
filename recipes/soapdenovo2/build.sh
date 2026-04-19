9bd93f4116a01e453c10eade9152797d5e0ea1c8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-19 05:42:18 -0500

    Update build.sh

--- recipes/soapdenovo2/build.sh
+++ recipes/soapdenovo2/build.sh
@@ -9,11 +9,16 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3 ${LDFLAGS} ${CPPFLAGS}"
 
+sed -i.bak 's|#include "spinLock.h"|#include <os/lock.h>|' sparsePregraph/inc/global.h
+rm -f sparsePregraph/inc/*.bak
+
 cp -f ${PREFIX}/lib/libbam.a standardPregraph/inc/
 cp -f ${PREFIX}/lib/libbam.a sparsePregraph/inc/
 
-make SOAPdenovo-63mer
-make SOAPdenovo-127mer
+make SOAPdenovo-63mer;
+make clean;
+make SOAPdenovo-127mer;
+make clean;
 
 install -v -m 0755 SOAPdenovo-127mer "$PREFIX/bin"
 install -v -m 0755 SOAPdenovo-63mer "$PREFIX/bin"
