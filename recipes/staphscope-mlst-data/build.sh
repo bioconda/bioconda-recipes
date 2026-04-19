4f0c27195cc1765573cf7c250e0799700d21db32
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-19 15:30:22 +0000

    Fix build.sh to locate module directories using find

--- recipes/staphscope-mlst-data/build.sh
+++ recipes/staphscope-mlst-data/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+MLST_DIR=$(find "$SRC_DIR" -type d -name "mlst_module" | head -1)
+if [ -z "$MLST_DIR" ]; then
+    echo "ERROR: Could not find mlst_module directory in $SRC_DIR"
+    exit 1
+fi
+
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -r $SRC_DIR/mlst_module "$DEST/"
+cp -r "$MLST_DIR" "$DEST/"
--- recipes/staphscope-sccmec-data/build.sh
+++ recipes/staphscope-sccmec-data/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+SCCMEC_DIR=$(find "$SRC_DIR" -type d -name "sccmec_module" | head -1)
+if [ -z "$SCCMEC_DIR" ]; then
+    echo "ERROR: Could not find sccmec_module directory in $SRC_DIR"
+    exit 1
+fi
+
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -r $SRC_DIR/sccmec_module "$DEST/"
+cp -r "$SCCMEC_DIR" "$DEST/"
