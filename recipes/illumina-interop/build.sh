c9e96ac7202b40c52498093a4d3e8608dd223c40
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-18 23:02:52 -0500

    Update build.sh

--- recipes/illumina-interop/build.sh
+++ recipes/illumina-interop/build.sh
@@ -11,8 +11,8 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 case $(uname -m) in
     aarch64)
 		export CXXFLAGS="${CXXFLAGS} -march=armv8-a";;
-    arm64)
-		export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a";;
+    #arm64)
+		#export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a";;
     x86_64)
 		export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3";;
 esac
