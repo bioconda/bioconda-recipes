cc1bdc098287874cb6468a31723ded9beb7f98a2
T. Quinn Smith <89670487+TQ-Smith@users.noreply.github.com>
2026-04-19 12:51:12 -0400

    Revert "Add EGGSv1 (#64407)"
    This reverts commit 82251e096df9b040503330fdcf8586126cfbc521.

--- recipes/eggs/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p "$PREFIX/bin"
-
-echo "BUILD SCRIPT IS RUNNING"
-make CC="${CC}" CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -c -Wall -I lib" LFLAGS="${LDFLAGS} -L${PREFIX}/lib -o"
-
-install -v -m 0755 bin/eggs "$PREFIX/bin"
