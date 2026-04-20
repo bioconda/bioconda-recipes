8faf6e69284288ca62ddb1b477ac47f790bc5b89
gallardoalba <gallardoalbac@gmail.com>
2026-04-20 20:15:15 +0200

    Fix recipe 2

--- recipes/tsenat/build.sh
+++ recipes/tsenat/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
 
 # Install gamsel from CRAN since it's not available as a conda package
 Rscript -e "install.packages('gamsel', repos='https://stat.ethz.ch/CRAN')"
+
+# Install the TSENAT R package
+$R CMD INSTALL --library=$PREFIX/lib/R/library $SRC_DIR
