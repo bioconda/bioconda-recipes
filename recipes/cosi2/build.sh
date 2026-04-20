397221d40a39d7c1503b8dcff8e615ffb8ac90c4
mencian <joshua.zhuang@yahoo.com>
2026-04-20 01:32:41 -0500

    Update cosi2 to v2.4.0

--- recipes/cosi2/build.sh
+++ recipes/cosi2/build.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -Wno-array-bounds -Wno-narrowing -Wno-deprecated-declarations"
+export CFLAGS="${CFLAGS} -O3"
 
-./configure --prefix=$PREFIX
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* m4/
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+sed -i.bak 's|2.3.0rc1|2.4.0|' configure.ac
+sed -i.bak 's|2.3.0rc1|2.4.0|' configure
+rm -f *.bak
+sed -i.bak 's|2.3.0rc4|2.4.0|' cosi/cositop.cc
+rm -f cosi/*.bak
+
+autoreconf -if
+./configure --prefix="${PREFIX}" \
+	--disable-option-checking --enable-silent-rules \
+	--disable-dependency-tracking \
+	CC="${CC}" CFLAGS="${CFLAGS}" \
+	CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}"
+
 make
 make install
