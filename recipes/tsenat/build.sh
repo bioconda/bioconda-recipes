734f216f9d740a20eb4472822ef554ff3c3cd0c5
gallardoalba <gallardoalbac@gmail.com>
2026-04-20 23:13:06 +0200

    set MACOSX_DEPLOYMENT_TARGET to 11.0

--- recipes/tsenat/build.sh
+++ recipes/tsenat/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Set macOS deployment target to match dependencies (built for 11.0+)
+export MACOSX_DEPLOYMENT_TARGET=11.0
+
 # Install gamsel from CRAN since it's not available as a conda package
 Rscript -e "install.packages('gamsel', repos='https://stat.ethz.ch/CRAN')"
 
