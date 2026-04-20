4829344c43abca945aa1c2a524a834637a66ab64
UENO, M. <eunosium-1128@live.jp>
2026-04-20 14:44:00 +0900

    acedrg: add `{{ stdlib("c") }}` to build deps (#64271)
    * Add initial recipe for acedrg including build configuration and scripts
    
    * Add ligand file and update test commands in meta.yaml
    
    * Add license and version bump
    
    * acedrg: Refactor conda build configuration to consolidate zip_keys and update Python versions
    
    * Update source URL and commit reference in meta.yaml
    
    * Update license family from MPL to MOZILLA
    
    * Fix grep command in build script and remove run_test script
    
    * Add networkx to run requirements in meta.yaml
    
    * requirements: Add gemmi and servalcat to run requirements in meta.yaml
    
    * requirements: Update run_constrained section and clean up TODO comments in meta.yaml
    
    * requirements: Add numpy to run requirements in meta.yaml
    
    * requirements: Update run_exports to use max_pin=None in meta.yaml
    
    * requirements: Remove version pinning for numpy in meta.yaml
    
    * requirements: Remove test commands from meta.yaml and add run_test.sh script
    
    * requirements: Remove conda_build_config.yaml file
    
    * requirements: Update version to 330 and adjust run_exports to use exact pinning
    
    * requirements: Add environment variable exports to run_test.sh
    
    * requirements: Add build script to meta.yaml
    
    * requirements: Specify bash for build script execution in meta.yaml
    
    * requirements: Remove build script specification from meta.yaml
    
    * requirements: Pin servalcat to version 0.4.126 in meta.yaml
    
    * requirements: Update sed command in build.sh to remove backup file creation
    
    * requirements: Update CLIBD_MON path and change directory commands in run_test.sh
    
    * requirements: Remove version pinning for servalcat in meta.yaml
    
    * requirements: Replace sed with gsed for compatibility in build.sh
    
    * requirements: Replace gsed with sed for consistency in build.sh
    
    * requirements: Update build.sh to use system sed and add grep dependency in meta.yaml
    
    * requirements: Add activation and deactivation scripts for acedrg
    
    * requirements: Update CXXFLAGS settings for target platforms in build.sh
    
    * Add build skip conditions
    
    * Add pdbecif package to meta.yaml dependencies
    
    * Update recipes/acedrg/activate.sh
    
    Co-authored-by: Thanh Lee <thanh@cloudbinfies.com>
    
    * Remove echo statements from activate.fish
    
    Remove echo statements for environment variable settings
    
    * Clean up deactivate.fish by removing echo statements
    
    Remove echo statements for unset environment variables.
    
    * Modify deactivate.sh shebang and clean up code
    
    Updated shebang and removed echo statements.
    
    * Change variable export syntax to Fish shell format and remove unnecessary shebangs
    
    * Add PowerShell and Xonsh activation and deactivation scripts for acedrg
    
    * Add C shell activation and deactivation scripts for acedrg
    
    * Move activation and deactivation scripts to `scripts` dir
    
    * Update URLs in the about section of meta.yaml for acedrg
    
    * Remove activation and deactivation scripts for acedrg
    
    * Update build number to 2 and add C standard library to build requirements for acedrg
    
    * Uncomment metalcoordanalysis dependency in meta.yaml
    
    ---------
    
    Co-authored-by: Thanh Lee <thanh@cloudbinfies.com>
    Co-authored-by: Björn Grüning <bjoern@gruenings.eu>

--- recipes/acedrg/activate.csh
+++
@@ -1,4 +0,0 @@
-setenv LIBMOL_ROOT "${CONDA_PREFIX}"
-setenv CCP4 "${CONDA_PREFIX}"
-setenv CBIN "${CONDA_PREFIX}/bin"
-setenv CLIBD_MON "${CONDA_PREFIX}/share/acedrg/tables/"
--- recipes/acedrg/activate.sh
+++
@@ -1,4 +0,0 @@
-export LIBMOL_ROOT="${CONDA_PREFIX}"
-export CCP4="${CONDA_PREFIX}"
-export CBIN="${CONDA_PREFIX}/bin"
-export CLIBD_MON="${CONDA_PREFIX}/share/acedrg/tables/"
