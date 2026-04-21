2f82c51e79ec8d1a77307321b9fc1130bd67314f
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-21 10:30:38 +0000

    Add staphscope v1.2.1 with bundled MLST data and separate SCCmec data package

--- recipes/staphscope-sccmec-data/build.sh
+++ recipes/staphscope-sccmec-data/build.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "=== Debug: SRC_DIR = $SRC_DIR"
-ls -la $SRC_DIR
-
-DEST="$SP_DIR/staphscope/modules/sccmec_module/database"
+DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-echo "=== Debug: DEST = $DEST"
-
-if [ -d "$SRC_DIR/database" ]; then
-    echo "=== Found database directory, copying its contents"
-    cp -rv "$SRC_DIR/database/." "$DEST/"
-else
-    echo "=== No database directory, copying all contents"
-    cp -rv "$SRC_DIR/." "$DEST/"
-fi
-
-echo "=== Debug: Final contents of $DEST"
-ls -la "$DEST"
+cp -r $SRC_DIR/sccmec_module "$DEST/"
