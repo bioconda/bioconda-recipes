6b3ff15b07ea0c9b5fe87f44b9237e44c0f97489
bbeckley-hub <brownbeckley94@gmail.com>
2026-04-21 11:44:17 +0000

    Copy entire SRC_DIR content

--- recipes/staphscope-sccmec-data/build.sh
+++ recipes/staphscope-sccmec-data/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+cd "$SRC_DIR"
+# If there is exactly one subdirectory, step into it
+if [ $(ls -1 | wc -l) -eq 1 ] && [ -d "$(ls -1)" ]; then
+    cd "$(ls -1)"
+fi
 DEST="$PREFIX/share/staphscope/modules"
 mkdir -p "$DEST"
-cp -r $SRC_DIR/* "$DEST/"
+cp -r ./* "$DEST/"
