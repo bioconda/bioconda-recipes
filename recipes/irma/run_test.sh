b4072b4216cc01515a03f9e588ac1d9467d473ce
bakeronit <Jia.Zhang@health.qld.gov.au>
2026-04-22 17:27:20 +1000

    update irma to v1.3.1

--- recipes/irma/run_test.sh
+++ recipes/irma/run_test.sh
@@ -1,27 +1,19 @@
-#!/bin/bash
-
-# Adapted from https://github.com/bioconda/bioconda-recipes/blob/master/recipes/blast/run_test.sh
+#!/bin/sh
+set -e
 
 TMPDIR=$(mktemp -d)
+export TMP=$TMPDIR
 trap "rm -rf $TMPDIR" 0 INT QUIT ABRT PIPE TERM
 
-TEST_FASTA=test1.fa
-TEST_FASTQ=test2.fastq
-
-cp $TEST_FASTA $TEST_FASTQ $TMPDIR
+TEST_FASTQ=test.fastq
+cp $TEST_FASTQ $TMPDIR
 cd $TMPDIR
 
-echo -n "Testing LABEL... "
-LABEL $TEST_FASTA label-test H9v2011
-grep -q "GQ373074" label-test_final.txt
-grep -q "EF154979" label-test_final.txt
-echo PASS
-
+# test IRMA runs and produces output
 echo -n "Testing IRMA... "
-IRMA FLU test2.fastq irma-test
-# fasta files for each of the 8 segments of influenza
+IRMA FLU $TEST_FASTQ irma-test
 test "$(ls irma-test/amended_consensus/*.fa | wc -l)" -eq 8
 echo PASS
-echo 
+
 echo "ALL TESTS PASSED"
 exit 0
