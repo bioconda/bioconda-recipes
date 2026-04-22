d22e2b0dafbe82a9004a4763c524425683684cc8
mencian <joshua.zhuang@yahoo.com>
2026-04-21 23:28:14 -0500

    Update sts-smctc to v2.0

--- recipes/sts-smctc/build.sh
+++ recipes/sts-smctc/build.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
-make \
-    CXX="${CXX}" \
-    CXXFLAGS="${CXXFLAGS} -O3 -std=c++11 -I ../include" \
-    libraries
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
 mkdir -p "${PREFIX}/"{lib,include}
-cp lib/libsmctc.a "${PREFIX}/lib/"
-cp include/*.hh "${PREFIX}/include/"
+
+sed -i.bak 's|2.8.1|3.5|' CMakeLists.txt
+rm -f *.bak
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+
+cmake --build build --target install -j "${CPU_COUNT}"
+
+cp -f include/*.hh "${PREFIX}/include/"
