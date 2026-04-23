e987238c35dc6a9fd07362b836a3f8218341ac07
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-23 01:53:56 -0500

    Update build.sh

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -9,15 +9,18 @@ cp -r $SRC_DIR/scripts $PREFIX
 ln -s $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod +x $PREFIX/bin/mentalist
 
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("ArgParse")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Bio")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("OpenGene")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Logging")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Lumberjack")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("FastaIO")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("JLD")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("DataStructures")' >> "${PREFIX}/.messages.txt" 2>&1
-"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.instantiate()' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Distributed")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("ArgParse")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("BioSequences")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("JSON")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("JLD")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("DataStructures")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("GZip")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Blosc")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("FileIO")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("TextWrap")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("LightXML")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.instantiate()';
 
 rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
