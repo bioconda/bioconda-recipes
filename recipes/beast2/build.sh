e2226a1e25f2c9a599fda97ec022fcb658e5dd2c
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2026-04-23 15:58:39 -0500

    Focus only on updating to 2.7.7

--- recipes/beast2/build.sh
+++ recipes/beast2/build.sh
@@ -5,12 +5,8 @@ BEAST_DIR_SUFFIX="opt/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 BEAST_DIR="$PREFIX/${BEAST_DIR_SUFFIX}"
 mkdir -p $BEAST_DIR
 
-# First copy the full release contents over
-cp -r $SRC_DIR/beast2_full/* $BEAST_DIR
-
-# Next, copy the package release contents into the full release directory
-cp -r $SRC_DIR/beast2_base_package/* $BEAST_DIR
-cp -r $SRC_DIR/beast2_app_package/* $BEAST_DIR
+# Copy full beast2 installation directory
+cp -r $SRC_DIR/beast2/* $BEAST_DIR
 
 # Setup symlinks in conda "bin/" directory to the beast2 install directory
 ln -s ../${BEAST_DIR_SUFFIX}/bin/{applauncher,beast,beauti,densitree,loganalyser,logcombiner,packagemanager,treeannotator} $PREFIX/bin
