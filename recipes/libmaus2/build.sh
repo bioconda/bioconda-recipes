a704153e15f0f40ee6e409bc4d1be98bdb942b9a
mencian <joshua.zhuang@yahoo.com>
2026-04-22 21:37:56 -0500

    Rebuild libmaus2 recipe

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 set -eu
-export LIBS="-lstdc++fs -lcurl -lz -ldeflate"
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export CFLAGS="${CFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
 
 autoreconf -if
 ./configure --prefix="${PREFIX}" CXX="${CXX}" CC="${CC}" \
-	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
-	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}" \
+	CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}" \
 	--with-snappy --with-io_lib --with-libdeflate \
 	--with-libsecrecy --with-nettle \
-	--with-lzma --with-gmp
+	--with-lzma --with-gmp --with-parasail \
+	--enable-shared-libmaus2 --disable-option-checking \
+	--disable-dependency-tracking --enable-silent-rules
+
+make clean
 
 make -j"${CPU_COUNT}"
 make install
