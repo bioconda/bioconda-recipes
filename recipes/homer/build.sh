0054cc8d96b8087f25b51714ac58882ff53674de
mencian <joshua.zhuang@yahoo.com>
2026-04-22 19:55:00 -0500

    Rebuild homer recipe

--- recipes/homer/build.sh
+++ recipes/homer/build.sh
@@ -1,19 +1,36 @@
-#! /bin/bash
+#!/bin/bash
 
-outdir=$PREFIX/share/$PKG_NAME
-mkdir -p $outdir
-mkdir -p $outdir/bin
-mkdir -p $PREFIX/bin
-chmod +x configureHomer.pl
-cp configureHomer.pl $outdir/
+outdir="$PREFIX/share/$PKG_NAME"
+mkdir -p "$outdir"
+mkdir -p "$outdir/bin"
+mkdir -p "$PREFIX/bin"
 
-ln -s ${CC} $BUILD_PREFIX/bin/gcc
-ln -s ${CXX} $BUILD_PREFIX/bin/g++
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+sed -i.bak 's|-C "$homeDir/cpp/"|-C "$homeDir/cpp/" CXX="$CXX" DEBUG="${CXXFLAGS}"|' configureHomer.pl
+rm -f *.bak
+
+chmod +rwx configureHomer.pl
+cp -f configureHomer.pl $outdir/
 
 ( cd $outdir && perl configureHomer.pl -keepScript -install )
 
 ls -1 $outdir/bin/ | grep -v old | sed -e "s/\*/ \\\/g" | while read id;
 do
-    chmod +x $outdir/bin/$id;
-    ln -s $outdir/bin/$id $PREFIX/bin/$id;
+	chmod +x $outdir/bin/$id;
+	ln -s $outdir/bin/$id $PREFIX/bin/$id;
 done
