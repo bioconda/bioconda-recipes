a4319913e35b5c0034e2afd9f0b0eb287b03f24d
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-23 01:33:21 -0500

    Update build.sh

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -2,22 +2,22 @@
 
 # Please don't do the following symlinking elsewhere. It is not nice.
 # Julia packages are retrieved externally and compile things with gcc without using CC etc. env variables...
-ln -s "${GCC}" "${BUILD_PREFIX}/gcc"
+ln -s "${CC}" "${BUILD_PREFIX}/gcc"
 
 cp -r $SRC_DIR/src/*.jl $PREFIX/bin
 cp -r $SRC_DIR/scripts $PREFIX
 ln -s $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod +x $PREFIX/bin/mentalist
 
-julia -e 'Pkg.init()'
-julia -e 'Pkg.add("ArgParse")'
-julia -e 'Pkg.add("Bio")'
-julia -e 'Pkg.add("OpenGene")'
-julia -e 'Pkg.add("Logging")'
-julia -e 'Pkg.add("Lumberjack")'
-julia -e 'Pkg.add("FastaIO")'
-julia -e 'Pkg.add("JLD")'
-julia -e 'Pkg.add("DataStructures")'
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("ArgParse")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Bio")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("OpenGene")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Logging")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Lumberjack")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("FastaIO")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("JLD")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("DataStructures")' >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.instantiate()' >> "${PREFIX}/.messages.txt" 2>&1
 
 rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
