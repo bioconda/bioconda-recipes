3f63d3bf3f6a332bb0f5a1801966e16f5cc18342
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2026-04-23 14:55:01 -0500

    Updated to include full and patch release

--- recipes/beast2/build.sh
+++ recipes/beast2/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -xe
 
-# Copies full beast2 distribution to opt/
 BEAST_DIR_SUFFIX="opt/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 BEAST_DIR="$PREFIX/${BEAST_DIR_SUFFIX}"
-
 mkdir -p $BEAST_DIR
-cp -r $SRC_DIR/* $BEAST_DIR
 
+# First copy the full release contents over
+cp -r $SRC_DIR/beast2_full/* $BEAST_DIR
+
+# Next, copy the patch release contents into the full release directory
+cp -r $SRC_DIR/beast2_base_patch/* $BEAST_DIR
+cp -r $SRC_DIR/beast2_app_patch/* $BEAST_DIR
+
+# Setup symlinks in conda "bin/" directory to the beast2 install directory
 ln -s ../${BEAST_DIR_SUFFIX}/bin/{applauncher,beast,beauti,densitree,loganalyser,logcombiner,packagemanager,treeannotator} $PREFIX/bin
