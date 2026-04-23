9da3df3f21bbaeac00a0240cde1433cd44eac703
EfraMP <aefrainmp@protonmail.com>
2026-04-22 21:33:16 -0600

    Add py310.patch

--- recipes/eklipse/build.sh
+++ recipes/eklipse/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sed -i 's/from collections import namedtuple, Iterable/from collections import namedtuple\nfrom collections.abc import Iterable/' tabulate.py
-sed -i 's/from itertools import zip_longest as izip_longest/from itertools import zip_longest/' tabulate.py
 
 chmod +x eKLIPse.py
 sed -i '1s|.*|#!/usr/bin/env python\n&|' eKLIPse.py
