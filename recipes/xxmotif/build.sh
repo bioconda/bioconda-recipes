125d96878f25c6e4fdd30a3d5073ff9bf201fa5a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-23 01:29:45 -0500

    Update build.sh

--- recipes/xxmotif/build.sh
+++ recipes/xxmotif/build.sh
@@ -2,7 +2,7 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=gnu++11"
 export CFLAGS="${CFLAGS} -O3"
 export SHARE_DIR="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
 
@@ -32,7 +32,7 @@ cp -rf XXmasker $SHARE_DIR/
 ln -s $SHARE_DIR/XXmasker/*.pl ${PREFIX}/bin/
 
 sed -i.bak 's|VERSION 2.8|VERSION 3.5|' CMakeLists.txt
-sed -i.bak 's|-std=c99|-std=c++14 -march=x86-64-v3|' CMakeLists.txt
+sed -i.bak 's|-std=c99|-std=gnu++11 -march=x86-64-v3|' CMakeLists.txt
 
 case $(uname -m) in
 	arm64) sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' CMakeLists.txt ;;
