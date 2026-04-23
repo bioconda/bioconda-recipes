5f75858c441e1c1d016b1f8feb8f62092fe0882f
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2026-04-23 15:07:34 -0500

    Switch name patch to package

--- recipes/beast2/build.sh
+++ recipes/beast2/build.sh
@@ -8,9 +8,9 @@ mkdir -p $BEAST_DIR
 # First copy the full release contents over
 cp -r $SRC_DIR/beast2_full/* $BEAST_DIR
 
-# Next, copy the patch release contents into the full release directory
-cp -r $SRC_DIR/beast2_base_patch/* $BEAST_DIR
-cp -r $SRC_DIR/beast2_app_patch/* $BEAST_DIR
+# Next, copy the package release contents into the full release directory
+cp -r $SRC_DIR/beast2_base_package/* $BEAST_DIR
+cp -r $SRC_DIR/beast2_app_package/* $BEAST_DIR
 
 # Setup symlinks in conda "bin/" directory to the beast2 install directory
 ln -s ../${BEAST_DIR_SUFFIX}/bin/{applauncher,beast,beauti,densitree,loganalyser,logcombiner,packagemanager,treeannotator} $PREFIX/bin
