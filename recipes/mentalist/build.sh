49db06364302f407033e0d02c5d9ae0933bf4858
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-23 19:19:03 -0500

    Update build.sh

--- recipes/mentalist/build.sh
+++ recipes/mentalist/build.sh
@@ -9,7 +9,20 @@ cp -r $SRC_DIR/scripts $PREFIX
 ln -s $PREFIX/bin/MentaLiST.jl $PREFIX/bin/mentalist
 chmod +x $PREFIX/bin/mentalist
 
-julia --project=. -e 'using Pkg; Pkg.instantiate()'
+#julia --project=. --threads ${CPU_COUNT} -e 'using Pkg; Pkg.instantiate()'
+
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Distributed")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("ArgParse")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("BioSequences")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("JSON")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("JLD")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("DataStructures")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("GZip")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("Blosc")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("FileIO")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("TextWrap")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.add("LightXML")';
+"${PREFIX}/bin/julia" -e 'using Pkg; Pkg.instantiate()';
 
 rm -f "$PREFIX"/share/julia/site/lib/v*/*.ji
 rm -rf "$PREFIX"/share/julia/site/v*/METADATA
