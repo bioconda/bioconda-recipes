b767f67f6eb7893a7328f9d6c7bb39405369e38f
gongyh <gongyh@qibebt.ac.cn>
2026-04-24 20:50:18 +0800

    patch Makefile

--- recipes/pandepth/build.sh
+++ recipes/pandepth/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu -o pipefail
 
+sed -i 's/g++/${CXX}/g' Makefile
+
 make CXX="${CXX}" \
      CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -O3" \
      LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lhts -lz"
