5071bf0ebbefda212bf00e0da10e7f60d68a2118
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-24 21:44:53 -0500

    Update build.sh

--- recipes/molemap/build.sh
+++ recipes/molemap/build.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+case $(uname -s) in
+    Darwin)
+    sed -i.bak "s|g++|${CXX}|" Makefile
+		sed -i.bak 's|-lrt||g' Makefile
+    rm -f *.bak ;;
+esac
+
 mkdir -p "${PREFIX}/bin"
 
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-
-make CXX="${CXX}" -j"${CPU_COUNT}"
+make -j"${CPU_COUNT}"
 
 install -v -m 0755 molemap "${PREFIX}/bin"
