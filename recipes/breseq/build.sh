7e9d40ea2ec0dd7ebf0eefa1c091e0ba024ed99e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-24 20:17:43 -0500

    Update build.sh

--- recipes/breseq/build.sh
+++ recipes/breseq/build.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-
 set -eux
 
 export INCLUDES="-I${PREFIX}/include"
 export LIBPATH="-L${PREFIX}/lib"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export M4="${BUILD_PREFIX}/bin/m4"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
 autoreconf -if
 ./configure --prefix="${PREFIX}" \
 	CXX="${CXX}" \
-	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS}"
-make -j "${CPU_COUNT}"
+	CXXFLAGS="${CXXFLAGS} -O3" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}" \
+	--disable-option-checking --disable-dependency-tracking --enable-silent-rules
+
+make -j"${CPU_COUNT}"
 make install
