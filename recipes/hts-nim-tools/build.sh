e7847cee483557a2f2e32ca16d5b01ef39d9baa4
mencian <joshua.zhuang@yahoo.com>
2026-04-24 20:34:55 -0500

    Update hts-nim-tools to v0.3.31

--- recipes/hts-nim-tools/build.sh
+++ recipes/hts-nim-tools/build.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 set -eu -o pipefail
 
+export CFLAGS="${CFLAGS} -O3"
+export LDFLAGS="${LDFLAGS}"
+
+mkdir -p "${PREFIX}/bin"
+
+if [[ "$(uname -m)" == "arm64" ]]; then
+	nim_build="macosx_arm64"
+	curl -SL https://github.com/nim-lang/nightlies/releases/download/latest-version-2-2/${nim_build}.tar.xz -o ${nim_build}.tar.xz
+	unxz -c ${nim_build}.tar.xz | tar -x
+
+	cd nim-2.2.*
+	export PATH="${PWD}/bin:${PATH}"
+	cd ..
+
+	echo "gcc.exe = \"${CC}\"" >> nim-2.2.*/config/nim.cfg
+	echo "gcc.linkerexe =\"${CC}\"" >>  nim-2.2.*/config/nim.cfg
+	echo "gcc.options.linker %= \"\${gcc.options.linker} ${LDFLAGS}\"" >>  nim-2.2.*/config/nim.cfg
+	cat nim-2.2.*/config/nim.cfg
+fi
+
 pushd hts-nim
 nimble install -y
 popd
 
 nimble install -y
 
-chmod +x hts_nim_tools
-mkdir -p "${PREFIX}/bin"
-cp hts_nim_tools "${PREFIX}/bin/"
+install -v -m 0755 hts_nim_tools "${PREFIX}/bin"
