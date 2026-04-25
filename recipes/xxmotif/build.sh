fa82c42a0ff95d6793ee2d89ea35c33a06f7fa8a
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-04-25 03:58:13 -0500

    Update build.sh

--- recipes/xxmotif/build.sh
+++ recipes/xxmotif/build.sh
@@ -17,7 +17,7 @@ case $(uname -m) in
 	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
 	;;
     x86_64)
-	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64"
 	;;
 esac
 
@@ -32,11 +32,11 @@ cp -rf XXmasker $SHARE_DIR/
 ln -s $SHARE_DIR/XXmasker/*.pl ${PREFIX}/bin/
 
 sed -i.bak 's|VERSION 2.8|VERSION 3.5|' CMakeLists.txt
-sed -i.bak 's|-std=c99|-std=gnu++11 -march=x86-64-v3|' CMakeLists.txt
+sed -i.bak 's|-std=c99|-std=gnu++11 -march=x86-64|' CMakeLists.txt
 
 case $(uname -m) in
-	arm64) sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' CMakeLists.txt ;;
-	aarch64) sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' CMakeLists.txt ;;
+	arm64) sed -i.bak 's|-march=x86-64|-march=armv8.4-a|' CMakeLists.txt ;;
+	aarch64) sed -i.bak 's|-march=x86-64|-march=armv8-a|' CMakeLists.txt ;;
 esac
 
 rm -f *.bak
