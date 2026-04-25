1f2ab131a8375e7406dd54eb1e4723535858d5ac
gongyh <gongyh@qibebt.ac.cn>
2026-04-25 17:25:20 +0800

    compile in build.sh

--- recipes/pandepth/build.sh
+++ recipes/pandepth/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
 
-sed -i.bak 's/g++/${CXX}/g' Makefile
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i.bak 's/-rpath=/-rpath,/g' Makefile
-fi
+${CXX} ${CXXFLAGS} ${LDFLAGS} --std=c++11 -g -O3 src/PanDepth.cpp \
+    -lhts -ldeflate -lz -pthread \
+    -I${PREFIX}/include -L${PREFIX}/lib -o pandepth
 
-make CXX="${CXX}" \
-     CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -O3" \
-     LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lhts -ldeflate -lz"
-
-mkdir -p ${PREFIX}/bin
-cp bin/pandepth ${PREFIX}/bin/
+cp pandepth ${PREFIX}/bin/
 
 chmod +x ${PREFIX}/bin/pandepth
