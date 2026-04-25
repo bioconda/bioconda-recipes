d34346fbb5f138028ccebfb8046328e94c66e2f2
mencian <joshua.zhuang@yahoo.com>
2026-04-24 22:56:51 -0500

    Update nemo to v2.4.0

--- recipes/nemo/build.sh
+++ recipes/nemo/build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+
+mkdir -p "${PREFIX}/bin"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    make GSL_PATH=$PREFIX/ CC=$CXX MAC=1 SHELL=/bin/bash
+	make GSL_PATH="$PREFIX/" CC="$CXX" MAC=1 SHELL="/bin/bash" -j"${CPU_COUNT}"
 else
-    make GSL_PATH=$PREFIX/ CC=$CXX SHELL=/bin/bash
+	make GSL_PATH="$PREFIX/" CC="$CXX" SHELL="/bin/bash" -j"${CPU_COUNT}"
 fi
 
-make install BIN_INSTALL=$PREFIX/bin/ LIB_INSTALL=$PREFIX/lib/ SHELL=/bin/bash
+make install BIN_INSTALL="$PREFIX/bin/" LIB_INSTALL="$PREFIX/lib/" SHELL="/bin/bash"
