dd001e28e7eadb669f0135677afc8469fe695263
gallardoalba <gallardoalbac@gmail.com>
2026-04-27 19:02:44 +0200

    Fix r-gamsel dependency

--- recipes/tsenat/build.sh
+++ recipes/tsenat/build.sh
@@ -4,8 +4,5 @@ set -e
 # Set macOS deployment target to match dependencies (built for 11.0+)
 export MACOSX_DEPLOYMENT_TARGET=11.0
 
-# Install gamsel from CRAN since it's not available as a conda package
-Rscript -e "install.packages('gamsel', repos='https://stat.ethz.ch/CRAN')"
-
 # Install the TSENAT R package
 $R CMD INSTALL --library=$PREFIX/lib/R/library $SRC_DIR
