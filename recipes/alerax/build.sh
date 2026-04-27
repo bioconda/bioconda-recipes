b309f8bf2eeb83b2ae323bbac4bc4e4d49bf47a3
Deepti Varshney <deepti.varshney12@gmail.com>
2026-04-27 16:39:13 +0200

    updated

--- recipes/alerax/build.sh
+++ recipes/alerax/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-mkdir build
+mkdir -p build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release
 make -j"${CPU_COUNT}"
 
 mkdir -p "${PREFIX}/bin"
