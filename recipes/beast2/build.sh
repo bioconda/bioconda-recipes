196884a814083b66ef3c284bd50cad329e1e3c1b
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2026-04-27 10:51:28 -0500

    Updated build script

--- recipes/beast2/build.sh
+++ recipes/beast2/build.sh
@@ -3,10 +3,11 @@ set -xe
 
 BEAST_DIR_SUFFIX="opt/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}"
 BEAST_DIR="$PREFIX/${BEAST_DIR_SUFFIX}"
+
 mkdir -p $BEAST_DIR
 
 # Copy full beast2 installation directory
 cp -r $SRC_DIR/beast2/* $BEAST_DIR
 
 # Setup symlinks in conda "bin/" directory to the beast2 install directory
-ln -s ../${BEAST_DIR_SUFFIX}/bin/{applauncher,beast,beauti,densitree,loganalyser,logcombiner,packagemanager,treeannotator} $PREFIX/bin
+ln -f -s ../${BEAST_DIR_SUFFIX}/bin/{applauncher,beast,beauti,densitree,loganalyser,logcombiner,packagemanager,treeannotator} $PREFIX/bin
