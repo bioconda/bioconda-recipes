d5c2ea1af6a140dbe27be29a14e66132c703c1b3
Deepti Varshney <deepti.varshney12@gmail.com>
2026-04-27 15:26:48 +0200

    updated build file

--- recipes/alerax/build.sh
+++ recipes/alerax/build.sh
@@ -2,13 +2,16 @@
 set -ex
 
 
+git submodule update --init --recursive
+
 mkdir build
 cd build
 
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release \
+  -DMPI_C_COMPILER=$PREFIX/bin/mpicc \
+  -DMPI_CXX_COMPILER=$PREFIX/bin/mpicxx
 
 make -j${CPU_COUNT}
-
 make install
