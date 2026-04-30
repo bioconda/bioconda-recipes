4d3eb3ddce6fe71397e3d8c761a2f42e9f7ca728
Ali Osman Berk Şapcı <asapci@ucsd.edu>
2026-04-30 14:19:09 -0700

    Update build.sh

--- recipes/krepp/build.sh
+++ recipes/krepp/build.sh
@@ -7,6 +7,9 @@ export CXXFLAGS="${CXXFLAGS} -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
 
-make COMPILER="${CXX}" -j"${CPU_COUNT}"
+make COMPILER="${CXX}" \
+    LDFLAGS="${LDFLAGS}" \
+    INC="${CPPFLAGS} -Iexternal/CLI11/include/CLI -Iexternal/parallel-hashmap -Iexternal/boost/libs/math/include" \
+    -j"${CPU_COUNT}"
 
 install -v -m 0755 krepp "$PREFIX/bin"
