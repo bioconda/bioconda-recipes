f94f3957d7c854b0f919742f4b5e5fb7cc94f404
Felix Krueger <fkrueger@altoslabs.com>
2026-05-01 17:47:53 +0100

    trim-galore: drop glob in install path (tarball top-level dir is hoisted)

--- recipes/trim-galore/build.sh
+++ recipes/trim-galore/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 
+# conda_build hoists the single top-level directory out of our tarball,
+# so trim_galore + LICENSE land directly in $SRC_DIR.
 mkdir -p "${PREFIX}/bin"
-install -m 0755 trim_galore-*/trim_galore "${PREFIX}/bin/trim_galore"
+install -m 0755 trim_galore "${PREFIX}/bin/trim_galore"
