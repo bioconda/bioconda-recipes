9f402a247f84282ee49dab1516baea04e87d737f
TQ-Smith <thomasqsmith2000@gmail.com>
2026-05-01 15:55:40 -0400

    LODESTAR

---
+++ recipes/lodestar/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p "$PREFIX/bin"
+
+#echo "BUILD SCRIPT IS RUNNING"
+make CC="${CC}" CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include -c -Wall -I lib" LFLAGS="${LDFLAGS} -L${PREFIX}/lib -o"
+
+install -v -m 0755 bin/lodestar "$PREFIX/bin"
