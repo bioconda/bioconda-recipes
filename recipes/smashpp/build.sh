2b8db2580b42ac42afef65d722a6afec42d67adf
Morteza Hosseini <s_morteza_h@yahoo.com>
2026-05-04 18:54:30 +0100

    build(smashpp): update version to 26.05 and add version override in build script

--- recipes/smashpp/build.sh
+++ recipes/smashpp/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DSMASHPP_VERSION_OVERRIDE="${PKG_VERSION}"
 
 cmake --build build --parallel "${CPU_COUNT:-1}" --config Release
 cmake --install build --config Release
