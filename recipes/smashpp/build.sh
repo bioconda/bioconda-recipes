cdce7cca7279a042bbc50726e03004330bf2e04c
Morteza Hosseini <s_morteza_h@yahoo.com>
2026-05-04 20:14:10 +0100

    Fix smashpp macOS builds

--- recipes/smashpp/build.sh
+++ recipes/smashpp/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
+if [[ "${target_platform:-}" == osx-* || "$(uname)" == "Darwin" ]]; then
+    export CXXFLAGS="${CXXFLAGS:-} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake -S . -B build \
+    ${CMAKE_ARGS:-} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DSMASHPP_VERSION_OVERRIDE="${PKG_VERSION}"
