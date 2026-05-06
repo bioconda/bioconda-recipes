0baf268e82d47fcd984ba01d50f48e51e393da31
Victor Lev <89986042+vicLeva@users.noreply.github.com>
2026-05-06 14:45:21 +0200

    conda: switch to zlib, bundle kff-cpp-api as offline source

--- recipes/tuna-kmer/build.sh
+++ recipes/tuna-kmer/build.sh
@@ -11,7 +11,8 @@ cmake .. \
     -DTUNA_USE_ZLIB_NG=OFF \
     -DCMAKE_CXX_FLAGS="-O3 -march=x86-64" \
     -DCMAKE_THREAD_PREFER_PTHREAD=ON \
-    -DTHREADS_PREFER_PTHREAD_FLAG=ON
+    -DTHREADS_PREFER_PTHREAD_FLAG=ON \
+    -DFETCHCONTENT_SOURCE_DIR_KFF_SRC="${SRC_DIR}/../kff_src"
 
 cmake --build . --target tuna -j"${CPU_COUNT}"
 
