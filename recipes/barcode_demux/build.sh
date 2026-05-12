ab7a5bfec5b15eae7d9b9f1fdf03ff4d9f39a0ea
adam <hui.chen@geneus-tech.com>
2026-05-12 11:35:55 +0800

    fix license problem

--- recipes/barcode_demux/build.sh
+++ recipes/barcode_demux/build.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 export CARGO_HOME="${BUILD_PREFIX}/cargo"
 mkdir -p "${CARGO_HOME}"
 
-cargo install --locked --root "${PREFIX}" --path .
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+cargo install -v --locked --no-track --root "${PREFIX}" --path .
 
 rm -f "${PREFIX}/.crates.toml" "${PREFIX}/.crates2.json"
