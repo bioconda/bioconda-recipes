f09e5f6aaf0a776a31db0103bb8070929379826b
Khaled196 <56037954+khaled196@users.noreply.github.com>
2026-05-14 10:21:43 +0100

    Add 'vrs' to the copied files in build script

--- recipes/beacon2-ri-tools/build.sh
+++ recipes/beacon2-ri-tools/build.sh
@@ -6,7 +6,7 @@ MAIN_SRC="${SRC_DIR}/main_src"
 mkdir -p "$PREFIX/bin/beacon2-ri-tools"
 
 # Copy main release contents
-cp -r "$MAIN_SRC"/{*.py,ref_schemas,conf,files,validators,pipelines} \
+cp -r "$MAIN_SRC"/{*.py,ref_schemas,conf,files,validators,pipelines,vrs} \
     "$PREFIX/bin/beacon2-ri-tools/"
 
 # Ensure package recognition
