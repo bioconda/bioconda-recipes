fc03f79ddbf6c8c46b87698425bf3b7b37190ece
Lee Katz <lskatz@gmail.com>
2026-05-16 19:51:22 -0400

    install perl library

--- recipes/annapotater/build.sh
+++ recipes/annapotater/build.sh
@@ -6,6 +6,7 @@ export PERL5LIB="$PREFIX/lib/perl5:${PERL5LIB:-}"
 
 # Array::IntSpan is not available on conda-forge/bioconda
 cpanm --notest --local-lib "$PREFIX" Array::IntSpan@2.003
+cp -rv lib/perl5/* "$PREFIX/lib/perl5"
 
 # Use INSTALLDIRS=site so that EXE_FILES (annapotater.pl) are installed to $PREFIX/bin
 perl Makefile.PL INSTALLDIRS=site NO_PERLLOCAL=1 NO_PACKLIST=1
