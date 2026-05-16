018ac551b587bf0e790549b2be5c7744ba72c540
alxndrdiaz <ra.ramos.diaz@gmail.com>
2026-05-16 15:22:55 -0600

    do not use rm

--- recipes/rf-net2/build.sh
+++ recipes/rf-net2/build.sh
@@ -6,10 +6,8 @@ wget https://github.com/flu-crew/rf-net-2/archive/refs/heads/main.zip
 # unzip .jar file
 unzip main.zip -d ./
 unzip rf-net-2-main/RF-Net-2.0.4.zip -d ./
-rm -r rf-net-2-main/
 
 # copy .jar file and dependencies to a bin folder
 mkdir -p $PREFIX/bin/
 cp RF-Net-2.0.4/RF-Net-2.0.4.jar $PREFIX/bin/
 cp -r RF-Net-2.0.4/dependencies $PREFIX/bin/
-rm -r RF-Net-2.0.4/ __MACOSX/
