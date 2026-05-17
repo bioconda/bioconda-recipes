b79da17991a0056014e98aeceb9750a2f2ab075e
Lee Katz <lskatz@gmail.com>
2026-05-16 21:39:54 -0400

    cleaned up build.sh and hopefully that works instead

--- recipes/annapotater/build.sh
+++ recipes/annapotater/build.sh
@@ -2,25 +2,15 @@
 
 set -euo pipefail
 
-export PERL5LIB="$PREFIX/lib/perl5:${PERL5LIB:-}"
+# Array::IntSpan is not on conda-forge/bioconda; PERL_MM_OPT ensures cpanm
+# installs to the versioned site_perl path that Perl's @INC already searches
+export PERL_MM_OPT="INSTALLDIRS=site"
+cpanm --notest Array::IntSpan@2.003
 
-# Array::IntSpan is not available on conda-forge/bioconda
-cpanm --notest --local-lib "$PREFIX" Array::IntSpan@2.003
-mkdir -pv "$PREFIX/lib/perl5/Array"
-#find $PREFIX -name 'IntSpan.pm' -exec cp -v {} "$PREFIX/lib/perl5/" \;
-#find $PREFIX -name 'Fields.pm' -exec cp -v {} "$PREFIX/lib/perl5/Array/" \;
-#find $PREFIX -name 'IP.pm' -exec cp -v {} "$PREFIX/lib/perl5/Array/" \;
-
-export PERL5LIB="$PREFIX/lib/perl5:${PERL5LIB:-}"
-
-# Use INSTALLDIRS=site so that EXE_FILES (annapotater.pl) are installed to $PREFIX/bin
 perl Makefile.PL INSTALLDIRS=site NO_PERLLOCAL=1 NO_PACKLIST=1
 make
 make test
 make install
-
-echo "Finding annapotater executable(s) in $PREFIX or else listing files in $PREFIX with annapotater in the name:"
-#which annapotater.pl || find "$PREFIX" -name 'annapotater*' -executable -type f -print
 
 cp scripts/annapotater.pl "$PREFIX/bin/annapotater.pl"
 chmod +x "$PREFIX/bin/annapotater.pl"
