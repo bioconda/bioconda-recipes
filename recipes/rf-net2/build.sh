19fcf6b37952d8cf0b30f0014eaa7f7de1c22f53
alxndrdiaz <ra.ramos.diaz@gmail.com>
2026-05-17 12:28:47 -0600

    start from scratch

--- recipes/rf-net2/build.sh
+++ recipes/rf-net2/build.sh
@@ -5,9 +5,3 @@ wget https://github.com/flu-crew/rf-net-2/archive/refs/heads/main.zip
 
 # unzip .jar file
 unzip main.zip -d ./
-unzip rf-net-2-main/RF-Net-2.0.4.zip -d ./
-
-# copy .jar file and dependencies to a bin folder
-mkdir -p $PREFIX/bin/
-cp RF-Net-2.0.4/RF-Net-2.0.4.jar $PREFIX/bin/
-cp -r RF-Net-2.0.4/dependencies $PREFIX/bin/
