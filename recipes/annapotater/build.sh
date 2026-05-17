1c60c65fb672ac5bc8a485f3a0917ba6c7ae96aa
Lee Katz <lskatz@gmail.com>
2026-05-17 14:57:21 -0400

    use Array::IntSpan

--- recipes/annapotater/build.sh
+++ recipes/annapotater/build.sh
@@ -2,14 +2,6 @@
 
 set -euo pipefail
 
-# Install Array::IntSpan from bundled source (not on conda-forge/bioconda)
-cd "$SRC_DIR/Array-IntSpan"
-perl Makefile.PL INSTALLDIRS=site
-make
-make install
-
-# Build annapotater
-cd "$SRC_DIR/annapotater"
 perl Makefile.PL INSTALLDIRS=site NO_PERLLOCAL=1 NO_PACKLIST=1
 make
 make test
