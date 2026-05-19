e8f7e7d781ff3a7c1847130c3d7fd2c4d300db11
Miranda PG Zalusky <mgaley@uw.edu>
2026-05-19 15:35:16 -0700

    updated release files, changed how awk is called to be compatible with busybox version of awk

--- recipes/needlr/build.sh
+++ recipes/needlr/build.sh
@@ -17,10 +17,3 @@ chmod +x $PREFIX/bin/needLR
 
 
 sed -i "s?REFLOCPATH?$refloc?g" $PREFIX/bin/needLR
-
-#temporary shebang fix
-srcfiles=( $(ls $PREFIX/bin/src/*.awk) )
-for file in ${srcfiles[@]}
-do
-    sed -i 's?/bin/awk -f?/usr/bin/env -S awk -f?g' $file
-done
