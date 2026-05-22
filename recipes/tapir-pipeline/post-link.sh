d691d31f489d280959e101a7da6af9387d7c43a9
LymF <lucasmelobiomed@gmail.com>
2026-05-22 19:24:13 -0300

    Fix post-link: install viralquest with all dependencies

--- recipes/tapir-pipeline/post-link.sh
+++ recipes/tapir-pipeline/post-link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Install pip-only dependencies not available in bioconda/conda-forge
-"${PREFIX}/bin/pip" install --no-deps viralquest 2>&1 || true
+"${PREFIX}/bin/pip" install viralquest 2>&1 || true
