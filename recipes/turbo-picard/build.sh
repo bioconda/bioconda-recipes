c5b8950a28e35c5e4a6aa8232f0baf9fd2df2abf
Donncha O'Toole <dnncha@gmail.com>
2026-06-01 11:35:37 +0100

    Patch rust-htslib bindgen during build

--- recipes/turbo-picard/build.sh
+++ recipes/turbo-picard/build.sh
@@ -6,6 +6,31 @@ export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+cargo fetch --locked
+
+python - <<'PY'
+import os
+import pathlib
+
+cargo_home = pathlib.Path(os.environ.get("CARGO_HOME", pathlib.Path.home() / ".cargo"))
+manifest = next(cargo_home.glob("registry/src/*/rust-htslib-1.0.0/Cargo.toml"))
+text = manifest.read_text()
+old = (
+    '[dependencies.hts-sys]\n'
+    'version = "2.2.0"\n'
+    'features = ["bindgen"]\n'
+    'default-features = false'
+)
+new = (
+    '[dependencies.hts-sys]\n'
+    'version = "2.2.0"\n'
+    'default-features = false'
+)
+if old not in text:
+    raise SystemExit("rust-htslib hts-sys dependency block was not found")
+manifest.write_text(text.replace(old, new))
+PY
+
 cargo install \
   --locked \
   --no-track \
