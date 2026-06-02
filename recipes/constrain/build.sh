806d5c3ed3c462d05a4952a1e1b917150e766923
Olesia <luna7053@yandex.ru>
2026-06-02 11:42:53 +0200

    Fix Bioconda review comments

--- recipes/constrain/build.sh
+++ recipes/constrain/build.sh
@@ -3,14 +3,9 @@ set -euxo pipefail
 
 cd ConSTRain
 
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="${CC}"
-
 cargo build --release --bin ConSTRain
 
 mkdir -p "${PREFIX}/bin"
 cp target/release/ConSTRain "${PREFIX}/bin/ConSTRain"
-
-mkdir -p "${PREFIX}/share/licenses/${PKG_NAME}"
-cp ../LICENSE "${PREFIX}/share/licenses/${PKG_NAME}/LICENSE"
 
 "${STRIP}" "${PREFIX}/bin/ConSTRain"
