71527fe1d337c336bc56c37f64c334cd9d0d680a
Olesia <luna7053@yandex.ru>
2026-06-02 22:56:20 +0200

    Add macos support & parallel make

--- recipes/hipstr/build.sh
+++ recipes/hipstr/build.sh
@@ -3,11 +3,11 @@
 set -euxo pipefail
 
 # Fix build with modern GCC
-sed -i '/#include <stdlib.h>/a #include <cstdint>' src/region.h
+perl -0pi -e 's/#include <stdlib.h>/#include <stdlib.h>\n#include <cstdint>/' src/region.h
 
 # Make bundled htslib find conda-provided headers and libraries
 export CPATH="${PREFIX}/include:${CPATH:-}"
 export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH:-}"
 
-make
+make -j${CPU_COUNT}
 install -m 755 HipSTR "${PREFIX}/bin/"
