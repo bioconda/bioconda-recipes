46f42ae69954e42f42e864ebf630f2336aea4dc8
Tobias Baril <46785187+TobyBaril@users.noreply.github.com>
2026-06-11 11:38:34 +0200

    Add build script for famdb installation
    Create a build script to set up famdb directory and symlinks.

--- recipes/famdb/build.sh
+++ recipes/famdb/build.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+FDB_DIR="${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}"
+mkdir -p $FDB_DIR
+mkdir -p "${PREFIX}/bin"
+mv * ${FDB_DIR}
+
+export LC_ALL="en_US.UTF-8"
+
+# ----- add tools within the bin ------
+
+# add famdb.py
+ln -sf ${FDB_DIR}/famdb.py ${PREFIX}/bin/famdb.py
+
+# add all utils
+for name in ${FDB_DIR}/util/*; do
+	ln -sf $name ${PREFIX}/bin/$(basename $name)
+done
