bb429a04c4a2e5b398f3da2e8a9d594893d712b0
Donncha O'Toole <dnncha@gmail.com>
2026-06-16 17:23:26 +0100

    Patch rust-htslib original manifest

--- recipes/turbo-picard-picard-shim/build.sh
+++ recipes/turbo-picard-picard-shim/build.sh
@@ -13,22 +13,28 @@ import os
 import pathlib
 
 cargo_home = pathlib.Path(os.environ.get("CARGO_HOME", pathlib.Path.home() / ".cargo"))
-manifest = next(cargo_home.glob("registry/src/*/rust-htslib-1.0.0/Cargo.toml"))
-text = manifest.read_text()
-old = (
-    '[dependencies.hts-sys]\n'
-    'version = "2.2.0"\n'
-    'features = ["bindgen"]\n'
-    'default-features = false'
-)
-new = (
-    '[dependencies.hts-sys]\n'
-    'version = "2.2.0"\n'
-    'default-features = false'
-)
-if old not in text:
-    raise SystemExit("rust-htslib hts-sys dependency block was not found")
-manifest.write_text(text.replace(old, new))
+crate_dir = next(cargo_home.glob("registry/src/*/rust-htslib-1.0.0"))
+replacements = {
+    "Cargo.toml": (
+        '[dependencies.hts-sys]\n'
+        'version = "2.2.0"\n'
+        'features = ["bindgen"]\n'
+        'default-features = false',
+        '[dependencies.hts-sys]\n'
+        'version = "2.2.0"\n'
+        'default-features = false',
+    ),
+    "Cargo.toml.orig": (
+        'hts-sys = {version = "2.2.0", default-features = false, features = ["bindgen"]}',
+        'hts-sys = {version = "2.2.0", default-features = false}',
+    ),
+}
+for name, (old, new) in replacements.items():
+    manifest = crate_dir / name
+    text = manifest.read_text()
+    if old not in text:
+        raise SystemExit(f"rust-htslib hts-sys dependency block was not found in {name}")
+    manifest.write_text(text.replace(old, new))
 PY
 
 cargo install \
