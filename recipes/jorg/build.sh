4eb046056d068d11553f61ca31153fda10e5acd8
Sanchita Kamath <kamath.sanchita95@gmail.com>
2026-07-08 12:56:02 +0200

    Fix sed delimiter in jorg build script

--- recipes/jorg/build.sh
+++ recipes/jorg/build.sh
@@ -19,4 +19,4 @@ sed -i 's|if \[ ! -e "manifest_template.conf" \]|if [ ! -f "$MANIFEST_TEMPLATE" 
 sed -i 's|echo "Missing manifest_template.conf file.  Copy it into current directory."|echo "Missing manifest template: $MANIFEST_TEMPLATE"|g' "${PREFIX}/bin/jorg"
 
 # Replace manifest generation line to use packaged template
-sed -i 's|cat manifest_template.conf | sed "s/XXX/$binID/g" > manifest.conf|sed "s/XXX/$binID/g" "$MANIFEST_TEMPLATE" > manifest.conf|g' "${PREFIX}/bin/jorg"
+sed -i 's#cat manifest_template.conf | sed "s/XXX/$binID/g" > manifest.conf#sed "s/XXX/$binID/g" "$MANIFEST_TEMPLATE" > manifest.conf#g' "${PREFIX}/bin/jorg"
