a803dcb6ddbf27370545d25460a0d0faa0689a7a
Felicia Sandberg <felicia.sandberg@pm.me>
2026-07-24 15:32:47 +0200

    Handle OpenBLAS symlinks during app build

--- recipes/patchwork/build.sh
+++ recipes/patchwork/build.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 export JULIA_CPU_TARGET=generic
 export JULIA_CC="${CC}"
 
+julia --project="${SRC_DIR}/build" -e 'using Pkg; Pkg.instantiate()'
+sed -i.bak 's/cp(match, destpath)$/cp(match, destpath; follow_symlinks=true)/' \
+    "${BUILD_PREFIX}"/share/julia/packages/PackageCompiler/*/src/PackageCompiler.jl
+
 julia --project="${SRC_DIR}/build" "${SRC_DIR}/build/build_app.jl"
 
 install -d "${PREFIX}/libexec" "${PREFIX}/bin"
