2d1a80df0560d75a337941f74ef4c19f764e2a70
Zoe Rudnick <zoe.rudnick@yahoo.com>
2026-07-25 12:23:14 -0400

    Add recipe for bramble v0.1.6

---
+++ recipes/bramble/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -xe
+
+# Ensure C compiler / libclang environment variables are active for bindgen
+export LIBCLANG_PATH="${PREFIX}/lib"
+
+# Bundle Rust dependencies licenses (Bioconda compliance check)
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+# Build and install the Rust binary using cargo
+cargo install --no-track --verbose --root "${PREFIX}" --path bramble-cli
