9fb27b452fc611a2188e1477079f035d52a68222
mencian <joshua.zhuang@yahoo.com>
2026-07-25 21:30:38 -0500

    mfold: add aarch64/arm64 builds

--- recipes/mfold/build.sh
+++ recipes/mfold/build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX}
-make 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -Wno-register"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+./configure --prefix="${PREFIX}" \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
+	LDFLAGS="${LDFLAGS}" \
+	--disable-dependency-tracking --disable-silent-rules
+
+make -j"${CPU_COUNT}"
+
 make install
