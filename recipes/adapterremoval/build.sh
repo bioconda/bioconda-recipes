1a14354c5cf5247894e18216b1857cabefb8b687
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-07-25 21:13:52 -0500

    Add locale setting to build.sh
    Set locale to en_US.UTF-8 for build script.

--- recipes/adapterremoval/build.sh
+++ recipes/adapterremoval/build.sh
@@ -4,6 +4,7 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -O3 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2"
+export LC_ALL="en_US.UTF-8"
 
 sed -i.bak -e 's/install -d/mkdir -p/' Makefile
 rm -f *.bak
