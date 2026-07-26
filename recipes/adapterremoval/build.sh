3d432009ffecbbd72061e21b4eeccc89b6456195
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-07-25 21:03:19 -0500

    Enhance build script with optimization flags

--- recipes/adapterremoval/build.sh
+++ recipes/adapterremoval/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLAGS} -O3"
+export CXXFLAGS="${CXXFLAGS} -O3 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2"
 
 sed -i.bak -e 's/install -d/mkdir -p/' Makefile
 rm -f *.bak
 
 make
+
 make install PREFIX="${PREFIX}"
