6ca9970670216d58e7e33ab6e813943df6497f0d
Pavel V. Dimens <19176506+pdimens@users.noreply.github.com>
2026-07-27 15:02:54 -0400

    more build update

--- recipes/arachne/build.sh
+++ recipes/arachne/build.sh
@@ -3,6 +3,9 @@
 export GOPATH=$PWD
 export GOCACHE=$PWD/.cache/
 export CGO_LDFLAGS="-L${GOPATH}/gobwa/bwa -L${GOPATH}/jemalloc/lib"
+export CFLAGS="${CFLAGS} -g -Wall -Wno-unused-function -O3"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 mkdir -p "${GOCACHE}"
 mkdir -p "${PREFIX}/bin"
@@ -11,13 +14,14 @@ mkdir -p "${PREFIX}/bin"
 cd jemalloc
 ./autogen.sh && ./configure --disable-shared --enable-static
 cd ..
-make -j 4 -C jemalloc build_lib_static
+make -j 4 CC="${CC}" -C jemalloc build_lib_static
+
 
 # build bwa
-make -j 4 -C gobwa/bwa libbwa.a bwa
+make CC="${CC}" CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}" -C gobwa/bwa libbwa.a bwa
+#make -j 4 CC="${CC}" -C gobwa/bwa libbwa.a bwa
 
 # build arachne
 go build -ldflags "-X arachne/aligner.VERSION=${$PKG_VERSION}" -o $PREFIX/bin/arachne
 chmod +x $PREFIX/bin/arachne
-cp gobwa/bwa/bwa $PREFIX/bin/
-
+install -v -m 0755 gobwa/bwa/bwa "${PREFIX}/bin"
