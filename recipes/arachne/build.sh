88b034023e815c2a82010731bbd0f87f8f044e85
Pavel V. Dimens <19176506+pdimens@users.noreply.github.com>
2026-07-28 16:16:14 -0400

    macosx build fixes

--- recipes/arachne/build.sh
+++ recipes/arachne/build.sh
@@ -3,7 +3,7 @@
 export GOCACHE="$PWD/.cache"
 export CGO_ENABLED=1
 export GO111MODULE=on
-export CGO_LDFLAGS="-L${SRC_DIR}/gobwa/bwa"
+export CGO_LDFLAGS="-L${SRC_DIR}/gobwa/bwa -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -g -Wall -Wno-unused-function -O3"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
@@ -11,21 +11,13 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 mkdir -p "${GOCACHE}"
 mkdir -p "${PREFIX}/bin"
 
-# build jemalloc
-#cd jemalloc
-#./autogen.sh && ./configure --disable-shared --enable-static
-#cd ..
-#make -j 4 CC="${CC}" -C jemalloc build_lib_static
-
-
 # build bwa
 make CC="${CC}" CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}" -C gobwa/bwa libbwa.a bwa
-#make -j 4 CC="${CC}" -C gobwa/bwa libbwa.a bwa
 
 # build arachne
-echo "GOOS=$(go env GOOS)"
-echo "GOARCH=$(go env GOARCH)"
-echo "CGO_ENABLED=$(go env CGO_ENABLED)"
+echo "jemalloc files:"
+ls -l ${PREFIX}/lib/*jemalloc*
+
 go build -ldflags "-X arachne/aligner.VERSION=${PKG_VERSION}" -o $PREFIX/bin/arachne
 chmod +x $PREFIX/bin/arachne
 install -v -m 0755 gobwa/bwa/bwa "${PREFIX}/bin"
