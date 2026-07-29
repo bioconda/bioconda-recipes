a4aa7550353eb25f0e64fc9683e63956dda9e9f2
Pavel V. Dimens <19176506+pdimens@users.noreply.github.com>
2026-07-29 11:43:29 -0400

    force as cgo

--- recipes/arachne/build.sh
+++ recipes/arachne/build.sh
@@ -16,10 +16,6 @@ mkdir -p "${PREFIX}/bin"
 make CC="${CC}" CFLAGS="${CFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS}" -j"${CPU_COUNT}" -C gobwa/bwa libbwa.a bwa
 
 # build arachne
-go version
-go env
-echo "MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET"
-
 go build -ldflags "-X arachne/aligner.VERSION=${PKG_VERSION}" -o $PREFIX/bin/arachne
 chmod +x $PREFIX/bin/arachne
 install -v -m 0755 gobwa/bwa/bwa "${PREFIX}/bin"
