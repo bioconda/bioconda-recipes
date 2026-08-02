5f89ac862f6657f7eaf66c18fd650c5499d8f512
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-02 15:52:02 -0500

    Update build script for neffy with symlink changes

--- recipes/neffy/build.sh
+++ recipes/neffy/build.sh
@@ -1,18 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 set -x
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 
-if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64"]]; then
-	sed -i.bak 's|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|' Makefile && rm -f *.bak
+if [[ "$(uname -s)" == "Darwin" ]] && [[ "$(uname -m)" == "x86_64"]]; then
+	sed -i.bak "s|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|" Makefile
+	rm -f *.bak
 fi
 
+ln -sf ${CXX} ${PREFIX}/bin/g++
+
 # If you are making a g++ symlink:
-mkdir -p ./mybin
-ln -s "$(command -v $CXX)" ./mybin/g++
-export PATH="$(pwd)/mybin:$PATH"
+#mkdir -p ./mybin
+#ln -s "$(command -v $CXX)" ./mybin/g++
+#export PATH="$(pwd)/mybin:$PATH"
 
 # Now build and install
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
