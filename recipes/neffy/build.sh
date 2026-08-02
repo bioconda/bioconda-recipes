81e326510f1592e1c29aed74dd9275b8b44ec0b8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-02 04:34:04 -0500

    Update build script to modify installation flags

--- recipes/neffy/build.sh
+++ recipes/neffy/build.sh
@@ -2,10 +2,13 @@
 set -e
 set -x
 
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
+
 # If you are making a g++ symlink:
 mkdir -p ./mybin
 ln -s "$(command -v $CXX)" ./mybin/g++
 export PATH="$(pwd)/mybin:$PATH"
 
 # Now build and install
-$PYTHON -m pip install . --no-deps --ignore-installed -vv
+$PYTHON -m pip install . --no-deps --no-build-isolation --no-cache-dir -vv
