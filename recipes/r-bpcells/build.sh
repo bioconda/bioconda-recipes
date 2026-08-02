72b05af56b67ba783f6a6bb8b6b580171f34b1ef
mencian <joshua.zhuang@yahoo.com>
2026-08-02 14:58:38 -0500

    Rebuild r-bpcells recipe

--- recipes/r-bpcells/build.sh
+++ recipes/r-bpcells/build.sh
@@ -8,11 +8,11 @@ export CFLAGS="${CFLAGS} -O3"
 export LC_ALL="en_US.UTF-8"
 
 if [[ $(uname -s) == "Darwin" ]]; then
-  sed -i.bak 's|-std=c++17|-O3 -std=c++17 -D_LIBCPP_DISABLE_AVAILABILITY|' r/src/Makevars.in
-  rm -rf r/src/*.bak
+	sed -i.bak 's|-std=c++17|-O3 -std=c++17 -D_LIBCPP_DISABLE_AVAILABILITY|' r/src/Makevars.in
+	rm -f r/src/*.bak
 else
-  sed -i.bak 's|-std=c++17|-O3 -std=c++17|' r/src/Makevars.in
-  rm -rf r/src/*.bak
+	sed -i.bak 's|-std=c++17|-O3 -std=c++17|' r/src/Makevars.in
+	rm -f r/src/*.bak
 fi
 
 pushd r/
