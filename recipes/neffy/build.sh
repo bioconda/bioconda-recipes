3d250f46e2012bd57f98c99d6ec5198e1e75a412
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-02 14:27:09 -0500

    Refactor build.sh for g++ symlink and optimization flags
    Refactor build script to enable g++ symlink creation and update optimization flags based on architecture.

--- recipes/neffy/build.sh
+++ recipes/neffy/build.sh
@@ -5,26 +5,12 @@ set -x
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 # If you are making a g++ symlink:
-#mkdir -p ./mybin
-#ln -s "$(command -v $CXX)" ./mybin/g++
-#export PATH="$(pwd)/mybin:$PATH"
+mkdir -p ./mybin
+ln -s "$(command -v $CXX)" ./mybin/g++
+export PATH="$(pwd)/mybin:$PATH"
 
 sed -i.bak 's|find_packages|find_namespace_packages|' setup.py
 sed -i.bak 's|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|' Makefile
-sed -i.bak "s|CC=g++|CC=${CXX}|" Makefile
-
-case $(uname -m) in
-    aarch64)
-    sed -i.bak 's|-O3|-O3 -march=armv8-a|' Makefile
-    ;;
-    arm64)
-    sed -i.bak 's|-O3|-O3 -march=armv8.4-a|' Makefile
-    ;;
-    x86_64)
-    sed -i.bak 's|-O3|-O3 -march=x86-64-v3|' Makefile
-    ;;
-esac
-
 rm -f *.bak
 
 # Now build and install
