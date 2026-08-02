e41841d1d93f99bd8b44677e1b66cfa84f7321c8
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-02 16:08:29 -0500

    Update build.sh

--- recipes/neffy/build.sh
+++ recipes/neffy/build.sh
@@ -8,12 +8,10 @@ export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 sed -i.bak "s|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|" Makefile
 rm -f *.bak
 
-ln -sf ${CXX} ${PREFIX}/bin/g++
-
 # If you are making a g++ symlink:
-#mkdir -p ./mybin
-#ln -s "$(command -v $CXX)" ./mybin/g++
-#export PATH="$(pwd)/mybin:$PATH"
+mkdir -p ./mybin
+ln -s "$(command -v $CXX)" ./mybin/g++
+export PATH="$(pwd)/mybin:$PATH"
 
 # Now build and install
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
