e9431d650f576804373be7ce3d9546372034a1f6
mencian <joshua.zhuang@yahoo.com>
2026-08-02 15:33:24 -0500

    delete patch

--- recipes/neffy/build.sh
+++ recipes/neffy/build.sh
@@ -3,18 +3,16 @@ set -e
 set -x
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 
-case $(uname -m) in
-    aarch64)
-    sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' Makefile
-    ;;
-    arm64)
-    sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' Makefile
-    ;;
-esac
-rm -f *.bak
+if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64"]]; then
+	sed -i.bak 's|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|' Makefile && rm -f *.bak
+fi
 
-make CC="${CXX}" -j"${CPU_COUNT}"
+# If you are making a g++ symlink:
+mkdir -p ./mybin
+ln -s "$(command -v $CXX)" ./mybin/g++
+export PATH="$(pwd)/mybin:$PATH"
 
 # Now build and install
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --no-cache-dir -vvv
