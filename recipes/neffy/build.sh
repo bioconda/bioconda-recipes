da8aa0d2ed2ca9578c4bb6f85227dfe16afd6b4f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-02 16:01:14 -0500

    Refactor build script to simplify flags handling

--- recipes/neffy/build.sh
+++ recipes/neffy/build.sh
@@ -5,10 +5,8 @@ set -x
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -D_LIBCPP_DISABLE_AVAILABILITY"
 
-if [[ "$(uname -s)" == "Darwin" ]] && [[ "$(uname -m)" == "x86_64"]]; then
-	sed -i.bak "s|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|" Makefile
-	rm -f *.bak
-fi
+sed -i.bak "s|-O3|-O3 -D_LIBCPP_DISABLE_AVAILABILITY|" Makefile
+rm -f *.bak
 
 ln -sf ${CXX} ${PREFIX}/bin/g++
 
