22ca05086832dc7ac7d3fc36bf86ec9606f41a2b
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2026-08-04 10:55:34 +0800

    Bundle third party license, test

--- recipes/skiver/build.sh
+++ recipes/skiver/build.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 export CFLAGS="${CFLAGS} -O3 -Wno-cpp -Wno-unused-function -Wno-implicit-function-declaration -Wno-int-conversion"
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 # build statically linked binary with Rust
 RUST_BACKTRACE=1 cargo install \
     --verbose \
