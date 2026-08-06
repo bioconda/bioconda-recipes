69941a991519a5665892ee0babe33535217683f2
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-05 22:41:31 -0500

    Uncomment build commands in build.sh

--- recipes/igphyml/build.sh
+++ recipes/igphyml/build.sh
@@ -19,25 +19,23 @@ automake -f --add-missing
 	CC="${CC}" CFLAGS="${CFLAGS}" \
 	LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}"
 
-make clean
-make -j"${CPU_COUNT}"
+#make -j"${CPU_COUNT}"
 
 cp -r examples ${PREFIX}/share/igphyml
 pushd src
 cp -r motifs ${PREFIX}/share/igphyml
 
-#for f in *.c ; do
-	#o=${f%%.c}.o;
-	#$CC -DUNIX -DPHYML -DOMP -DDEBUG -DNBLAS -I. $CFLAGS -fopenmp -DHTABLE="\"../share/igphyml\"" $LDFLAGS -c -o $o $f
-#done
+for f in *.c ; do
+	o=${f%%.c}.o;
+	$CC -DUNIX -DPHYML -DOMP -DDEBUG -DNBLAS -I. $CFLAGS -fopenmp -DHTABLE="\"../share/igphyml\"" $LDFLAGS -c -o $o $f
+done
 
-#$CC -fopenmp $CFLAGS $LDFLAGS -o ${PREFIX}/bin/igphyml main.o utilities.o optimiz.o lk.o bionj.o models.o free.o help.o simu.o eigen.o pars.o alrt.o controller.o cl.o spr.o stats.o nucle2codon.o io.o -lm
+$CC -fopenmp $CFLAGS $LDFLAGS -o ${PREFIX}/bin/igphyml main.o utilities.o optimiz.o lk.o bionj.o models.o free.o help.o simu.o eigen.o pars.o alrt.o controller.o cl.o spr.o stats.o nucle2codon.o io.o -lm
 
 mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
 cat <<EOF >> ${PREFIX}/etc/conda/activate.d/igphyml.sh
 export IGPHYML_PATH=${PREFIX}/share/igphyml/motifs/
 EOF
-
 cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/igphyml.sh
 unset IGPHYML_PATH
 EOF
