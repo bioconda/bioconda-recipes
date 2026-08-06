717358cf85e60e00814553b670b088efb623c9e5
Heng Li <lh3@me.com>
2026-08-06 19:16:39 -0400

    create man/man1

--- recipes/minibwa/build.sh
+++ recipes/minibwa/build.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-mkdir -p "${PREFIX}/bin" "${PREFIX}/lib" "${PREFIX}/include"
+mkdir -p "${PREFIX}/bin" "${PREFIX}/lib" "${PREFIX}/include" "${PREFIX}/man/man1"
 
 make \
     CC="${CC}" \
