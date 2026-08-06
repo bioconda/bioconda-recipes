ba9a1f823153c6ccd31a2d727d5439e4f9499401
Ma Wenxin <mawenxin@stu.henau.edu.cn>
2026-08-06 10:16:09 +0800

    Add vcftools-ng 0.13.0

---
+++ recipes/vcftools-ng/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
+    -DBUILD_TESTING=OFF \
+    -DHTSLIB_ROOT="${PREFIX}"
+cmake --build build --parallel "${CPU_COUNT}"
+cmake --install build
