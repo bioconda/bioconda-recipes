fb048beeb99cbd08fd48f289068f106d9706a05a
mencian <joshua.zhuang@yahoo.com>
2026-08-08 18:52:37 -0500

    add conda_build_config.yaml

--- recipes/genion/build.sh
+++ recipes/genion/build.sh
@@ -6,6 +6,6 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export CONDA_PREFIX="${PREFIX}"
 
-make CXX="${CXX}" OPT="-O3 -I${PREFIX}/include" -j"${CPU_COUNT}"
+make CXX="${CXX}" OPT="-O3" -j"${CPU_COUNT}"
 
 install -v -m 0755 genion "${PREFIX}/bin"
