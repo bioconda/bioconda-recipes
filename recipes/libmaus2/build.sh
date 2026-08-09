e824a68c322f92ffc0a6e753bf87cb94146fbfa6
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-08 22:46:43 -0500

    Uncomment LIBS export in build script

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -5,7 +5,7 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-unused-variable -Wno-unuse
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CXXFLAGS="${CXXFLAGS} -O3 -Wno-unused-variable -Wno-unused-but-set-variable"
 export CFLAGS="${CFLAGS} -O3"
-#export LIBS="-lstdc++fs -lcurl -lz -ldeflate"
+export LIBS="-lstdc++fs -lcurl -lz -ldeflate"
 
 case $(uname -m) in
     aarch64)
@@ -24,7 +24,7 @@ cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
 autoreconf -if;
 ./configure --prefix="${PREFIX}" CXX="${CXX}" CC="${CC}" \
 	LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}" \
-	CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}" \
+	CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}" LIBS="${LIBS}" \
 	--with-snappy --with-io_lib --with-libdeflate \
 	--with-libsecrecy --with-nettle --enable-fast \
 	--with-lzma --with-gmp --disable-compile-testprograms --enable-shared-libmaus2 \
