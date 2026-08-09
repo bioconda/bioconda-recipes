5d6ae375ca3aeaea6c1030e0598a3bc16010d90f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-08 23:46:40 -0500

    Update build.sh

--- recipes/biobambam/build.sh
+++ recipes/biobambam/build.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 set -eu
 
-autoreconf -if
-./configure --prefix="${PREFIX}" CXX="${CXX}" \
-	--with-libmaus2 --with-xerces-c --with-gmp \
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -Wno-unused-variable -Wno-unused-but-set-variable"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++17 -Wno-unused-variable -Wno-unused-but-set-variable"
+export CFLAGS="${CFLAGS} -O3"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
+
+cp -f ${BUILD_PREFIX}/share/gnuconfig/config.* .
+
+autoreconf -if;
+./configure --prefix="${PREFIX}" CXX="${CXX}" CC="${CC}" \
+	LDFLAGS="${LDFLAGS}" CPPFLAGS="${CPPFLAGS}" \
+	CXXFLAGS="${CXXFLAGS}" CFLAGS="${CFLAGS}" \
+	--with-libmaus2="${PREFIX}/include" \
+	--with-xerces-c="${PREFIX}/include" --with-gmp \
+	--disable-option-checking --enable-silent-rules --disable-dependency-tracking \
 	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
-	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-make install
+	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
+
+make clean
+
+make install -j"${CPU_COUNT}"
