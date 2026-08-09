50d0e0c208b8cd036855914751e2566cd1c9c4a3
mencian <joshua.zhuang@yahoo.com>
2026-08-09 03:46:35 -0500

    Rebuild Ray recipe

--- recipes/ray/build.sh
+++ recipes/ray/build.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-
-mkdir build
+mkdir -p "$PREFIX/bin"
+mkdir -p build
 cd build
-export MPICXX=$PREFIX/bin
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
+export MPICXX="$PREFIX/bin"
 export OMPI_MCA_mpi_show_handle_leaks=1
-cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX/bin ..
-make
-cp Ray $PREFIX/bin
+export share_dir="${PREFIX}/share/Ray-${PKG_VERSION}-${PKG_BUILDNUM}"
+
+mkdir -p "${share_dir}"
+
+case $(uname -m) in
+    aarch64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8-a|' Makefile && rm -f *.bak
+	;;
+    arm64)
+	sed -i.bak 's|-march=x86-64-v3|-march=armv8.4-a|' Makefile && rm -f *.bak
+	;;
+esac
+
+make HAVE_LIBZ="y" HAVE_LIBBZ2="y" -j"${CPU_COUNT}"
+
+"${STRIP}" "install-prefix/Ray"
+
+cp -rf install-prefix/* "${share_dir}"
+
+ln -sf "${share_dir}/Ray" "${PREFIX}/bin/"
