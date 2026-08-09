e07497e1eaeca1685c68c7f8e8dd6883035ab5c0
mencian <joshua.zhuang@yahoo.com>
2026-08-08 20:51:05 -0500

    Rebuild libmaus2 recipe

--- recipes/libmaus2/build.sh
+++ recipes/libmaus2/build.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 set -eu
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
 export LIBS="-lstdc++fs -lcurl -lz -ldeflate"
+
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
 autoreconf -if
 ./configure --prefix="${PREFIX}" CXX="${CXX}" CC="${CC}" \
-	LDFLAGS="${LDFLAGS} -L${PREFIX}/lib" \
-	CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS}" \
+	CPPFLAGS="${CPPFLAGS}" \
 	--with-snappy --with-io_lib --with-libdeflate \
 	--with-libsecrecy --with-nettle \
-	--with-lzma --with-gmp
+	--with-lzma --with-gmp \
+	--disable-option-checking --enable-silent-rules --disable-dependency-tracking
+
+make clean
 
 make -j"${CPU_COUNT}"
+
 make install
