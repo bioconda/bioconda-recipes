9efe90f277934167affda74d680df9fb76a43a8b
David Laehnemann <1379875+dlaehnemann@users.noreply.github.com>
2026-08-12 17:29:37 +0200

    ci: try copy-pasting vignette example, which seems to be self-contained, as test script

---
+++ recipes/bioconductor-flames/run_test.r
@@ -0,0 +1,36 @@
+library(FLAMES)
+
+outdir <- tempfile()
+dir.create(outdir)
+# some example data
+# known cell barcodes, e.g. from coupled short-read sequencing
+bc_allow <- file.path(outdir, "bc_allow.tsv")
+R.utils::gunzip(
+  filename = system.file("extdata", "bc_allow.tsv.gz", package = "FLAMES"),
+  destname = bc_allow, remove = FALSE
+)
+# reference genome
+genome_fa <- file.path(outdir, "rps24.fa")
+R.utils::gunzip(
+  filename = system.file("extdata", "rps24.fa.gz", package = "FLAMES"),
+  destname = genome_fa, remove = FALSE
+)
+
+pipeline <- SingleCellPipeline(
+  # use the default configs
+  config_file = create_config(
+    outdir,
+    pipeline_parameters.demultiplexer = "flexiplex"
+  ),
+  outdir = outdir,
+  # the input fastq file
+  fastq = system.file("extdata", "fastq", "musc_rps24.fastq.gz", package = "FLAMES"),
+  # reference annotation file
+  annotation = system.file("extdata", "rps24.gtf.gz", package = "FLAMES"),
+  genome_fa = genome_fa,
+  barcodes_file = bc_allow
+)
+
+pipeline <- run_FLAMES(pipeline)
+
+pipeline
