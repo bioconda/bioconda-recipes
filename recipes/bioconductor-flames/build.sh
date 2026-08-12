c6f92344cb8ca1598a0258e6f0b2ae7b194de631
David Laehnemann <1379875+dlaehnemann@users.noreply.github.com>
2026-08-12 17:22:49 +0200

    fix: try removal of submodule in build.sh

--- recipes/bioconductor-flames/build.sh
+++ recipes/bioconductor-flames/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# remove full `src/submodule` directory to avoid trying to vendor non-compiling version of minimap2
+rm -r src/submodule
+
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 mkdir -p ~/.R
