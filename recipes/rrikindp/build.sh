0ca734a9c4dcbf97f6b6521619a3d90c6a0b9148
maria <code@waldl.org>
2026-08-14 20:40:06 +0200

    fix compability with macOS

--- recipes/rrikindp/build.sh
+++ recipes/rrikindp/build.sh
@@ -3,7 +3,8 @@ set -xe
 
 # the command line tool is not installed by pip and is built separately
 make -C src/rrikindp -j"${CPU_COUNT}" CXXFLAGS="${CXXFLAGS} -O3"
-install -D -m 0755 src/rrikindp/RRIkinDP "${PREFIX}/bin/RRIkinDP"
+mkdir -p "${PREFIX}/bin"
+install -m 0755 src/rrikindp/RRIkinDP "${PREFIX}/bin"
 
 # the python module, including the pybind11 extension
 CXXFLAGS="${CXXFLAGS} -O3" ${PYTHON} -m pip install . \
