50d7b2e2b3a468979cf061bfd36c1ca820cd740b
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-15 15:50:51 -0500

    Update build.sh

--- recipes/gfainject/build.sh
+++ recipes/gfainject/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -euo
-
 set -xe
 
-export CFLAGS="${CFLAGS} -std=c11"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -std=c11 -O3 -Wno-int-conversion -Wno-implicit-function-declaration"
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+cargo generate-lockfile
+cargo update --package console --precise "0.15.11"
 
 # build statically linked binary with Rust
 export RUST_BACKTRACE=1
-cargo install --verbose --path . --root "${PREFIX}" --no-track
+cargo install --root "${PREFIX}" --no-track --locked --verbose --path .
