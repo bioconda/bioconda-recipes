4c6ea7f91579a683c9f23e5bab11687775f73b9c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-15 20:11:24 -0500

    Update build.sh

--- recipes/gfainject/build.sh
+++ recipes/gfainject/build.sh
@@ -7,9 +7,9 @@ export CFLAGS="${CFLAGS} -std=c11 -O3 -Wno-int-conversion -Wno-implicit-function
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-cargo generate-lockfile
-cargo update --package console --precise "0.15.11"
-cargo update --package rust-htslib --precise "0.47.1"
+#cargo generate-lockfile
+#cargo update --package console --precise "0.15.11"
+#cargo update --package rust-htslib --precise "0.47.1"
 
 # build statically linked binary with Rust
 export RUST_BACKTRACE=1
