8e9c4b68c332bb534d3983b35f4fc2d16d83d809
AndreaGuarracino <andreaguarracino@outlook.com>
2026-08-17 09:20:40 +0200

    gfainject: use cargo --locked instead of Cargo.toml patch

--- recipes/gfainject/build.sh
+++ recipes/gfainject/build.sh
@@ -1,16 +1,11 @@
-#!/bin/bash -euo
-set -xe
+#!/bin/bash
+set -xeuo pipefail
 
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CFLAGS="${CFLAGS} -std=c11 -O3 -Wno-int-conversion -Wno-implicit-function-declaration"
+export CFLAGS="${CFLAGS} -std=c11 -Wno-int-conversion -Wno-implicit-function-declaration"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-#cargo generate-lockfile
-#cargo update --package console --precise "0.15.11"
-#cargo update --package rust-htslib --precise "0.47.1"
-
-# build statically linked binary with Rust
 export RUST_BACKTRACE=1
-cargo install --root "${PREFIX}" --no-track --verbose --path .
+cargo install --locked --root "${PREFIX}" --no-track --verbose --path .
