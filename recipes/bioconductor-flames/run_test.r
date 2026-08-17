187144bcc6e5d2e943ba578384aaa5036b806a2d
David Laehnemann <1379875+dlaehnemann@users.noreply.github.com>
2026-08-17 11:56:32 +0200

    fix: properly use unexported functions from reticulate

--- recipes/bioconductor-flames/run_test.r
+++ recipes/bioconductor-flames/run_test.r
@@ -33,9 +33,9 @@ pipeline <- SingleCellPipeline(
 
 python <- Sys.getenv("BASILISK_CUSTOM_PYTHON_3_11_9") 
 modules <- c("pip", "venv")
-print(stringr::str_c("Python version: ", reticulate::python_version(python)))
-print(stringr::str_c("Python has modules 'pip' and 'venv': ", reticulate::python_has_modules(python, modules)))
-print(stringr::str_c("Python config is: ", reticulate::python_config(python, modules)))
+print(stringr::str_c("Python version: ", reticulate:::python_version(python)))
+print(stringr::str_c("Python has modules 'pip' and 'venv': ", reticulate:::python_has_modules(python, modules)))
+print(stringr::str_c("Python config is: ", reticulate:::python_config(python, modules)))
 
 # print warnings when they occur
 options(warn=1)
