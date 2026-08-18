4bfbebfaa703a66fec987571b93fd93e95892537
Pauline <mrspaulinehans@gmail.com>
2026-08-18 16:42:14 +0200

    fix native library search path

--- recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
+++ recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+
+export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 exec dotnet "$CONDA_PREFIX/lib/dotnet/tools/PSMBasedQuantificationTIMs/ProteomIQon.PSMBasedQuantificationTIMs.dll" "$@"
