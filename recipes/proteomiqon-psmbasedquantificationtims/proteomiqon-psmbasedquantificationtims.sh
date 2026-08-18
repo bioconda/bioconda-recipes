3db745f0b1479c8548e386b6517a8edf6b248b9c
Pauline <mrspaulinehans@gmail.com>
2026-08-18 17:38:19 +0200

    add zlib runtime dependency and library path

--- recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
+++ recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-
+LD_LIBRARY_PATH=/usr/local/lib ldd .../SQLite.Interop.dll
 exec dotnet "$CONDA_PREFIX/lib/dotnet/tools/PSMBasedQuantificationTIMs/ProteomIQon.PSMBasedQuantificationTIMs.dll" "$@"
