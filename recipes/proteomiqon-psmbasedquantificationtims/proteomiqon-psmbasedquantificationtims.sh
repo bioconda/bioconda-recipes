0ed43ac3d98b91db3c360270483aeeeabdd92f6a
paulineHans <phans@rptu.de>
2026-08-19 14:26:23 +0200

    fix windows bug

--- recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
+++ recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+
 exec dotnet "$CONDA_PREFIX/lib/dotnet/tools/PSMBasedQuantificationTIMs/ProteomIQon.PSMBasedQuantificationTIMs.dll" "$@"
