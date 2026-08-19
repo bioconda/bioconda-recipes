905d79d35d7ca9c805a6db4781e38eefe805c04e
Björn Grüning <bjoern@gruenings.eu>
2026-08-19 16:32:19 +0200

    Remove redundant PREFIX variable assignment

--- recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
+++ recipes/proteomiqon-psmbasedquantificationtims/proteomiqon-psmbasedquantificationtims.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-PREFIX="${CONDA_PREFIX:-/usr/local}"
-
-export LD_LIBRARY_PATH="${PREFIX}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-
 exec "${PREFIX}/lib/dotnet/dotnet" \
     "${PREFIX}/lib/dotnet/tools/PSMBasedQuantificationTIMs/ProteomIQon.PSMBasedQuantificationTIMs.dll" "$@"
