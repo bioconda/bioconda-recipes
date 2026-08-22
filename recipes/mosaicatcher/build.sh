ef99da7fd7e82a9b21ce35c9a7b5dc0951e36f3d
mencian <joshua.zhuang@yahoo.com>
2026-08-22 01:03:04 -0500

    Rebuild mosacatcher recipe

--- recipes/mosaicatcher/build.sh
+++ recipes/mosaicatcher/build.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -O3 -std=c++14 -DBOOST_TIMER_ENABLE_DEPRECATED"
 
-mkdir build
-cd build
-cmake ../src \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    \
-    -DCMAKE_CXX_LINK_FLAGS="${LDFLAGS}" \
-    -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS}"
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+    x86_64)
+	export CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"
+	;;
+esac
 
-make
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
-mkdir -p "$PREFIX"/bin
-cp mosaicatcher "$PREFIX"/bin/mosaicatcher
+mkdir -p "$PREFIX/bin"
+
+cmake -S src -B build -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${CXX}" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+
+cmake --build build -j "${CPU_COUNT}"
+
+install -v -m 0755 build/mosaicatcher "$PREFIX/bin"
