81a3120e8c0083d3471df1688a58447adb05ba3f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-08-21 23:07:14 -0500

    Update build.sh

--- recipes/tesseract-assembler/build.sh
+++ recipes/tesseract-assembler/build.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 make -j"${CPU_COUNT:-4}" CXX="${CXX:-g++}"
 
 mkdir -p "${PREFIX}/bin"
-install -m 0755 tessera "${PREFIX}/bin/tessera"
-install -m 0755 tessera-model "${PREFIX}/bin/tessera-model"
+install -m 0755 tesseract-asm "${PREFIX}/bin/tessera"
+install -m 0755 tesseract-model "${PREFIX}/bin/tessera-model"
+install -m 0755 tesseract-klebsiella "${PREFIX}/bin/tesseract-klebsiella"
