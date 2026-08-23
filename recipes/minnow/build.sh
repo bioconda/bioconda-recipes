005ff81e7f830fe7aa3193d4bdc3ded107c93c48
mencian <joshua.zhuang@yahoo.com>
2026-08-22 20:18:23 -0500

    Update minnow to v1.6

--- recipes/minnow/build.sh
+++ recipes/minnow/build.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 set -ex
-export CFLAGS="-I$PREFIX/include"
-#export LDFLAGS="-L$PREFIX/lib"
 
-export CPATH=${PREFIX}/include
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3 -I$PREFIX/include"
+export LDFLAGS="${LDFLAGS} -L$PREFIX/lib"
+export CXXFLAGS="${CXXFLAGS} -O3"
 
+case $(uname -m) in
+    aarch64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8-a"
+	;;
+    arm64)
+	export CXXFLAGS="${CXXFLAGS} -march=armv8.4-a"
+	;;
+esac
 
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/lib
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
 
-mkdir -p build
-cd build
-cmake -D CMAKE_C_COMPILER="${CC}" -D CMAKE_CXX_COMPILER="${CXX}" ..
-make VERBOSE=1
-cp src/minnow $PREFIX/bin/ 
-cp src/fixfasta $PREFIX/bin/ 
+mkdir -p "$PREFIX/bin"
+mkdir -p "$PREFIX/lib"
 
-cp src/libfixfastalib.a $PREFIX/lib/
-cp src/libminnowlib.a $PREFIX/lib/
+cmake -S . -B build -DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+cmake --build build --clean-first -j "${CPU_COUNT}"
+
+"${STRIP}" build/src/minnow
+"${STRIP}" build/src/constructgraph
+"${STRIP}" build/src/validate
+
+install -v -m 0755 build/src/minnow "$PREFIX/bin"
+install -v -m 0755 build/src/constructgraph "$PREFIX/bin"
+install -v -m 0755 build/src/validate "$PREFIX/bin"
+
+cp -f build/src/libgraphlib.a "$PREFIX/lib"
+cp -f build/src/libminnowlib.a "$PREFIX/lib"
+cp -f build/src/libvalidatelib.a "$PREFIX/lib"
