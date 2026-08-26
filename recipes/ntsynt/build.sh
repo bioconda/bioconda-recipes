0e2f4ca4bc973afd6334915a3d9e5093ec6e8122
lcoombe <lauren.e.coombe@gmail.com>
2026-08-26 10:11:45 -0700

    Debugging

--- recipes/ntsynt/build.sh
+++ recipes/ntsynt/build.sh
@@ -4,6 +4,13 @@ export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 unset MACOSX_DEPLOYMENT_TARGET  # avoid meson re-adding a conflicting -mmacosx-version-min
 
+echo "=== DEBUG: incoming flags ==="
+echo "CXXFLAGS=$CXXFLAGS"
+echo "CFLAGS=$CFLAGS"
+echo "LDFLAGS=$LDFLAGS"
+echo "MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET"
+echo "=============================="
+
 # Build ntSynt
 mkdir -p ${PREFIX}/bin
 meson setup build --prefix ${PREFIX}  \
