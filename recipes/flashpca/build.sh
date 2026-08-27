eed4fddf1f985e477ea7753445cbd51c3d14e14a
Donald Baku <141358602+dbaku42@users.noreply.github.com>
2026-08-27 12:24:36 +0000

    Add flashpca 2.0 bioconda recipe

---
+++ recipes/flashpca/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+make -j"${CPU_COUNT}" \
+    CXX="${CXX}" \
+    EIGEN_INC="${PREFIX}/include/eigen3" \
+    SPECTRA_INC="${SRC_DIR}/spectra-0.8.1/include" \
+    BOOST_INC="${PREFIX}/include" \
+    BOOST_LIB="${PREFIX}/lib"
+
+install -d "${PREFIX}/bin"
+install -m 755 flashpca "${PREFIX}/bin/flashpca"
