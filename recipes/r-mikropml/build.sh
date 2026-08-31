f2a98979c9ccc6d6c189c9a16af581af96af6d79
Kelly Sovacool <kelly@sovacool.dev>
2026-08-30 20:06:08 -0400

    create recipe for mikropml

---
+++ recipes/r-mikropml/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DISABLE_AUTOBREW=1
+
+# R refuses to build packages that mark themselves as Priority: Recommended
+mv DESCRIPTION DESCRIPTION.old
+grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
+# shellcheck disable=SC2086
+${R} CMD INSTALL --build . ${R_ARGS}
