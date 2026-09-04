ba75f95ece6556bd21bf67786e5520783e059dfb
paulineHans <phans@rptu.de>
2026-09-04 11:04:48 +0200

    bug fixing

--- recipes/proteomiqon-joinquantpepionswithproteins/build.sh
+++ recipes/proteomiqon-joinquantpepionswithproteins/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 unzip -q "$SRC_DIR/$PKG_VERSION" -d "$SRC_DIR"
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
-tool_root="$PREFIX/lib/dotnet/tools/JoinQuantPepionsWithProteins"
+tool_root="$PREFIX/lib/dotnet/tools/JoinQuantPepIonsWithProteins"
 
 mkdir -p "$PREFIX/bin" "$tool_root"
 cp -R "$SRC_DIR/tools/net10.0/any/." "$tool_root/"
