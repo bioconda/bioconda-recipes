803bf83ff6d15f71cc291944efca282b3508434c
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-09-06 19:20:10 -0500

    Update build.sh

--- recipes/repeatafterme/build.sh
+++ recipes/repeatafterme/build.sh
@@ -8,15 +8,19 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' util/*.pl
 rm -f util/*.bak
 sed -i.bak 's|ar rcs|$(AR) rcs|' c/kentsrc/Makefile
-sed -i.bak 's|cp RAMExtend $(INSTDIR)/bin|install -v -m 0755 RAMExtend $(INSTDIR)|' c/Makefile
+#sed -i.bak 's|cp RAMExtend $(INSTDIR)/bin|install -v -m 0755 RAMExtend $(INSTDIR)|' c/Makefile
 sed -i.bak 's|-Iminunit -I.|-Iminunit -I. -Wno-format|' c/Makefile
 
-make CC="${CC}" -j1
+sed -i.bak 's|0.1.0|0.2.0|' Makefile
+sed -i.bak "s|/usr/local/RepeatAfterMe-$(VERSION)|$(PREFIX)/bin|" Makefile
+rm -f *.bak
+
+make CC="${CC}" -j"${CPU_COUNT}"
 
 make install
 
 install -v -m 0755 util/*.pl "${PREFIX}/bin"
 
-cd c/kentsrc && make twoBitToFa CC="${CC}" -j1
+cd c/kentsrc && make twoBitToFa CC="${CC}" -j"${CPU_COUNT}"
 
 install -v -m 0755 twoBitToFa "${PREFIX}/bin"
