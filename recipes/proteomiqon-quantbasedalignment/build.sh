468a67a0c9ad5c30281a54192aed7bd4298944b1
paulineHans <phans@rptu.de>
2026-09-15 11:27:32 +0200

    fix

--- recipes/proteomiqon-quantbasedalignment/build.sh
+++ recipes/proteomiqon-quantbasedalignment/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 unzip -q "$SRC_DIR/$PKG_VERSION" -d "$SRC_DIR"
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
-tool_root="$PREFIX/lib/dotnet/tools/QuantBasedAlignment"
+tool_root="$PREFIX/lib/dotnet/tools/QuantBasedAlignment_linux_x64"
 
 mkdir -p "$PREFIX/bin" "$tool_root"
 cp -R "$SRC_DIR/tools/net10.0/any/." "$tool_root/"
