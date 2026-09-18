29508d2c593ca34b4cb6ec5968594dcd2cb327c4
Shiyi Yin <yinshiyi2020@gmail.com>
2026-09-18 11:22:21 -0700

    add support to JAFFAL

--- recipes/jaffa/build.sh
+++ recipes/jaffa/build.sh
@@ -11,6 +11,21 @@ mkdir -p ${PREFIX}/bin
 # Copy files over into $PACKAGE_HOME
 cp -aR * $PACKAGE_HOME
 
+# Compile JAFFA's helper programs into the conda environment. These binaries
+# must match the Groovy pipeline from the same JAFFA release.
+for tool in \
+    process_transcriptome_align_table \
+    make_3_gene_fusion_table \
+    make_count_table \
+    make_final_table \
+    extract_seq_from_fasta \
+    make_simple_read_table \
+    make_simple_read_table_assembly \
+    compile_results \
+    split_fusion_reads; do
+    $CXX -std=c++11 -O3 -o "$PREFIX/bin/$tool" "$PACKAGE_HOME/src/$tool.c++"
+done
+
 # Create wrappers
 SOURCE_FILE=$RECIPE_DIR/run-jaffa.sh
 for suffix in direct assembly hybrid jaffal; do
@@ -24,5 +39,9 @@ for suffix in direct assembly hybrid jaffal; do
     cat $SOURCE_FILE >> $DEST_FILE
 
     chmod +x $DEST_FILE
-    ln -s $DEST_FILE $PREFIX/bin
+    if [ "$suffix" = "jaffal" ]; then
+        ln -s $DEST_FILE $PREFIX/bin/jaffal
+    else
+        ln -s $DEST_FILE $PREFIX/bin/jaffa-$suffix
+    fi
 done
--- recipes/jaffa/run-jaffa.sh
+++ recipes/jaffa/run-jaffa.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Jaffa wrapper script for Bioconda
 #
 # This script creates a copy of the Jaffa package files into an automatically
@@ -5,11 +6,19 @@
 # because of the somewhat non-general way of installing Jaffa.
 #
 # Author: Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
+# Edit: Shiyi Yin <github.com/yinshiyi>
 #
 # Usage: jaffa-{runmode} <any argument to pass to Jaffa pipeline>
 
 # Unofficial Bash Strict Mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
 set -euo pipefail
+
+# Ensure the wrapper strictly uses the Conda environment binaries (e.g. OpenJDK 8)
+# regardless of any system PATH or JAVA_HOME set in the user's shell profile.
+if [ -n "${CONDA_PREFIX:-}" ]; then
+    export JAVA_HOME="$CONDA_PREFIX"
+    export PATH="$CONDA_PREFIX/bin:$PATH"
+fi
 
 # setup auto-cleaned temporary directory
 export TMPDIR=$(mktemp -d)
@@ -19,11 +28,15 @@ trap "rm -rf $TMPDIR" EXIT
 cp -a $PACKAGE_HOME/* $TMPDIR
 
 # generate tools.groovy
-COMMANDS="bpipe velveth velvetg oases trimmomatic samtools bowtie2 blat dedupe.sh reformat.sh R minimap2"
+COMMANDS="bpipe velveth velvetg oases trimmomatic samtools bowtie2 blat dedupe.sh reformat.sh blastn bedtools minimap2 gffread_bin gtfToGenePred process_transcriptome_align_table make_3_gene_fusion_table make_count_table make_final_table extract_seq_from_fasta make_simple_read_table make_simple_read_table_assembly compile_results split_fusion_reads"
 for command in $COMMANDS; do
     echo "${command%.sh}=\"$(which $command)\"" >>$TMPDIR/tools.groovy
 done
 
 # actually launch Jaffa
 set -x
-bpipe run $TMPDIR/JAFFA_$RUNMODE.groovy $*
+if [ "$RUNMODE" != "jaffal" ]; then
+    bpipe run $TMPDIR/JAFFA_$RUNMODE.groovy "$@"
+else
+    bpipe run $TMPDIR/JAFFAL.groovy "$@"
+fi
