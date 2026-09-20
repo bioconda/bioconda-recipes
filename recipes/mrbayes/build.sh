2f2f67cb55d49f360c1c6a9159307d252cb1f29e
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-09-20 15:58:55 -0500

    Enhance build script with optimization flags and readline
    Updated build script to include additional flags for optimization and support for readline.

--- recipes/mrbayes/build.sh
+++ recipes/mrbayes/build.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-
 set -xe
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CFLAGS="${CFLAGS} -O3"
 
 # use newer config.guess and config.sub that support osx-arm64
 cp -f ${RECIPE_DIR}/config.* ./am-aux/
 
 # build version with MPI & Beagle
-./configure \
-    --prefix=$PREFIX \
-    --disable-debug \
-    --with-beagle=$PREFIX \
-    --with-mpi
+./configure --prefix="$PREFIX" \
+    --with-readline --with-mpi --with-beagle="$PREFIX" \
+    CC="$CC" CFLAGS="$CFLAGS" \
+    LDFLAGS="$LDFLAGS" CPPFLAGS="$CPPFLAGS" \
+    --disable-debug --disable-option-checking --enable-silent-rules --disable-dependency-tracking
 
-make -j$CPU_COUNT
+make -j"$CPU_COUNT"
 make install
 make clean
+
 mv $PREFIX/bin/mb{,-mpi}
 
-
 # build version with Beagle only
-./configure \
-    --prefix=$PREFIX \
-    --disable-debug \
-    --with-beagle=$PREFIX
+./configure --prefix="$PREFIX" \
+    --with-readline --with-beagle="$PREFIX" \
+    CC="$CC" CFLAGS="$CFLAGS" \
+    LDFLAGS="$LDFLAGS" CPPFLAGS="$CPPFLAGS" \
+    --disable-debug --disable-option-checking --enable-silent-rules --disable-dependency-tracking
 
-make -j$CPU_COUNT
+make -j"$CPU_COUNT"
 make install
