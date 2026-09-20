8ccd20594c3d007c88e152a943db07ecd6bc497c
mencian <joshua.zhuang@yahoo.com>
2026-09-20 17:50:59 -0500

    add patch

--- recipes/floria/build.sh
+++ recipes/floria/build.sh
@@ -2,13 +2,26 @@
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3"
 
 if [[ `uname -s` == "Darwin" ]]; then
-  export CFLAGS="${CFLAGS} -O3 -Wno-int-conversion -Wno-implicit-function-declaration"
+	export CFLAGS="${CFLAGS} -Wno-int-conversion -Wno-implicit-function-declaration"
 fi
+
+case $(uname -m) in
+    aarch64)
+	export CONFIG_ARGS="--features=neon --no-default-features"
+	;;
+    arm64)
+	export CONFIG_ARGS="--features=neon --no-default-features"
+	;;
+    x86_64)
+	export CONFIG_ARGS=""
+	;;
+esac
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
 RUST_BACKTRACE=1
-cargo install --verbose --no-track --path . --root "${PREFIX}"
+cargo install --path . --root "${PREFIX}" --verbose --no-track "${CONFIG_ARGS}"
