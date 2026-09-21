5319716d046b74fa00f824af6206bf789f70b430
Janne Sinivirta <janne.sinivirta@gmail.com>
2026-09-21 11:49:55 +0300

    Enable native Apple Silicon builds and correct umi-tools-rs version

--- recipes/umi-tools-rs/build.sh
+++ recipes/umi-tools-rs/build.sh
@@ -1,6 +1,5 @@
 
 export LIBCLANG_PATH="$BUILD_PREFIX/lib"
+export RELEASE_VERSION="$PKG_VERSION"
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --path . --root $PREFIX
-UMI_TOOLS=$(find $(pwd) -type f -executable -name "umi-tools-rs")
-install -Dm755 "$UMI_TOOLS" $PREFIX/bin/umi-tools-rs
+cargo install --locked --no-track --path . --root "$PREFIX"
