a11a742eee9379bf5ba9f678dc3b4fcc1651900f
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2026-09-20 20:11:16 -0500

    Update build script for installation and compilation

--- recipes/repeatafterme/build.sh
+++ recipes/repeatafterme/build.sh
@@ -8,7 +8,6 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' util/*.pl
 rm -f util/*.bak
 sed -i.bak 's|ar rcs|$(AR) rcs|' c/kentsrc/Makefile
-sed -i.bak 's|cp RAMExtend $(INSTDIR)/bin|install -v -m 0755 RAMExtend $(INSTDIR)|' c/Makefile
 sed -i.bak 's|-Iminunit -I.|-Iminunit -I. -Wno-format|' c/Makefile
 rm -f c/*.bak
 
